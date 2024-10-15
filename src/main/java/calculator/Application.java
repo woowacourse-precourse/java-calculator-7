b1bb28342c8a6bdce61352784af6bd739d2ed6c1
ddddabi <ii887742@gmail.com>
2024-10-15 19:56:59 +0900

    refactor: 1번, 2번 기능 refactoring
    - 01. 사용자 입력 기능
    return문 삼항연산자로 변경
    
    - 02. 기본 구분자 처리 기능
    공통적인 숫자 덧셈기능 함수
    추가
    함수명 변경 basicCalculateSum -> defaultCalculateSum

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,7 +7,7 @@ public class Application {
         String input = getUserInput();
 
         try {
-            int result = basicCalculateSum(input);
+            int result = defaultCalculateSum(input);
             System.out.println("결과: " + result);
         } catch (IllegalArgumentException e) {
             System.out.println("IllegalArgumentException: " + e.getMessage());
@@ -20,30 +20,22 @@ public class Application {
         System.out.print("덧셈할 문자열을 입력해 주세요.\n");
         String input = scanner.nextLine().trim();
 
-        // null 또는 비어있는 문자열 처리
-        if (input.isEmpty()) {
-            System.out.println("결과 : 0");
-            return ""; // 빈 문자열 반환
-        }
-
-        return input; // 유효한 입력 반환
+        return input.isEmpty() ? "" : input;
     }
 
     // 02. 기본 구분자 처리 기능
-    private static int basicCalculateSum(String input) {
-        if (input.isEmpty()) {
-            return 0;
-        }
-
-        // 쉼표(,) 또는 콜론(:)을 기준으로 문자열 분리
+    private static int defaultCalculateSum(String input) {
         String[] numbers = input.split("[,:]");
+        return sumNumbers(numbers);
+    }
 
+    // 공통적인 숫자 덧셈 기능
+    private static int sumNumbers(String[] numbers) {
         int sum = 0;
         for (String number : numbers) {
             try {
-                sum += Integer.parseInt(number);  // 숫자로 변환하여 합산
+                sum += Integer.parseInt(number);
             } catch (NumberFormatException e) {
-                // 숫자가 아닌 값이 있을 경우 예외 발생
                 throw new IllegalArgumentException("잘못된 숫자가 포함되어 있습니다.");
             }
         }
