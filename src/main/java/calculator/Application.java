5038ca47f9f7731d0ead2593dcefb03001df1b1d
imjaehyeong <dlawogud517@gmail.com>
2024-10-15 20:02:24 +0900

    refactor: 숫자들의 합을 구하는 함수 구현
    getSum 함수를 구현하여 기존 합 관련  중복된 코드를 함수로 대체하였습니다. 이 함수는
    String타입의 배열을 인자로 받아 배열의 요소를 int타입의 숫자로 바꾸고
    숫자들의 합을 반환하는 함수입니다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,35 +8,30 @@ public class Application {
         String input = scanner.nextLine();
         String[] splitedNumber;
         int sum = 0;
-        // //;\n1;2;3
+        //  //;\n1;2;3
         try {
             if (input.startsWith("//") && input.startsWith("\\n", 3)) {
                 char sperator = input.charAt(2);
                 String sparatorString = String.valueOf(sperator);
                 splitedNumber = input.substring(5).split(sparatorString);
 
-                System.out.println(splitedNumber);
-
-                for (String number : splitedNumber) {
-                    number.trim();
-                    int num = Integer.valueOf(number);
-                    sum += num;
-                }
             } else {
                 splitedNumber = input.split("[:,]");
-                for (String number : splitedNumber) {
-                    int num = Integer.parseInt(number);
-                    sum += num;
-                }
+
             }
         } catch (NumberFormatException e) {
-            System.out.println("error");
             throw new RuntimeException(e);
         }
-
-        System.out.println(sum);
+        System.out.println(getSum(splitedNumber));
 
     }
-
+    static int getSum(String[] array){
+        int sum=0;
+        for (String number : array) {
+            int num = Integer.valueOf(number);
+            sum += num;
+        }
+        return sum;
+    }
 
 }
