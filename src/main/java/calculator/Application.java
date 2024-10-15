b565cb37acba2298fbf7aa67360aea0434fd082f
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-10-15 23:12:19 +0900

    feat: 계산기를 실행하는 main method 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,17 +1,11 @@
 package calculator;
 
-import calculator.service.CalculatorService;
-import calculator.service.CalculatorServiceImpl;
-import java.util.List;
+import calculator.controller.CalculatorController;
 
 public class Application {
+
     public static void main(String[] args) {
-        CalculatorService calculatorService = new CalculatorServiceImpl();
-        List<String> validNumber = calculatorService.findValidNumber("1,2:3");
-
-        for (String s : validNumber) {
-            System.out.println(s);
-        }
-
+        CalculatorController controller = new CalculatorController();
+        controller.start();
     }
 }
