8c53450f45c6392880b67b94d49164ce1f2bd622
Bobae Kim <rlaqhqo0418@naver.com>
2024-10-16 07:06:30 +0900

    feat: 계산로직 수정 및 함수 분리
    다양한 경우를 처리하도록 패턴매칭, 계산로직 수정
    
    구분자빌드, 계산함수 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -7,7 +8,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String separator = ",|:";
+        String separators = ",|:";
         int sum = 0;
         Application app = new Application();
 
@@ -15,22 +16,38 @@ public class Application {
         String input = readLine();
 
         String pattern = "^(//.*\\\\n)*([0-9]+(.+[0-9]+)*|)$";
-        if(!Pattern.matches(pattern, input)) {
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(input);
+        if (!m.find()) {
             throw new IllegalArgumentException();
         }
 
-        String[] separatedInput = input.split("\\\\n");
-        for (int i = 0; i < (separatedInput.length - 1); i++) {
-            separator += "|";
-            separator += separatedInput[i].substring(2);
+        if (!(m.group(1) == null)) {
+            separators = app.buildSeparators(m.group(1));
         }
-
-        String[] calcValue = separatedInput[separatedInput.length-1].split(separator);
-        for (String num : calcValue) {
-            sum += app.stringToInt(num);
-        }
+        sum = app.sumNums(m.group(2), separators);
         System.out.println("결과 : " + sum);
     }
+
+    private String buildSeparators(String input) {
+        String separators = ",|:";
+        String[] inputSeparators = input.split("\\\\n");
+        for (String separator : inputSeparators) {
+            separators += "|";
+            separators += separator.substring(2);
+        }
+        return separators;
+    }
+
+    private int sumNums(String input, String separators) {
+        int sum = 0;
+        String[] inputNums = input.split(separators);
+        for (String num : inputNums) {
+            sum += stringToInt(num);
+        }
+        return sum;
+    }
+
     private int stringToInt(String num) {
         if (num.isBlank()) {
             return 0;
