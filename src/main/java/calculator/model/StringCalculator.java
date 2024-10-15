ef7c783ea81b0deb15b026465f321dd25d6d298e
leejunnyeop <daffodil8620@naver.com>
2024-10-15 23:07:58 +0900

    [feat] 입력값이 틀렸을 경우 예외처리 구현

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -2,27 +2,32 @@ package calculator.model;
 
 public class StringCalculator {
 
-    // TODO 커스텀 구분자 구현
+    // TODO 입력값이 유효 처리
     public int add(String input) {
         if (input != null || input.isEmpty()) {
             return 0;
         }
         String delimiter = ",|:";
 
-        if (input.startsWith("//")){
+        if (input.startsWith("//")) {
             String[] parts = input.split("\n", 2);
             delimiter = parts[0].substring(2);
             input = parts[1];
         }
 
-       String[] numbers = input.split(delimiter);
+        String[] numbers = input.split(delimiter);
         return sum(numbers);
     }
 
     private int sum(String[] numbers) {
         int total = 0;
-        for (String number : numbers){
-            total += Integer.parseInt(number);
+        for (String number : numbers) {
+            try {
+                total += Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("유효하지 않은 숫자입니다 : " + number);
+            }
+
         }
         return total;
     }
