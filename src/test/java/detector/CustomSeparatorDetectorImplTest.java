84623964623d2c15dbec5153fde630e906599275
ouvir <kilhyunii@naver.com>
2024-10-15 23:07:13 +0900

    test: 테스트 추가
    CustomSeparatorDetectorImplTest:
    - 커스텀_구분자 2글자 이상 테스트 추가
    - 커스텀 구분자 순서 바뀐 경우 테스트 추가
    - 커스텀 구분자 안 커스텀 구분자 테스트 1,2 추가

--- src/test/java/detector/CustomSeparatorDetectorImplTest.java
+++ src/test/java/detector/CustomSeparatorDetectorImplTest.java
@@ -1,11 +1,9 @@
 package detector;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CustomSeparatorDetectorImplTest {
     static final CustomSeparatorDetector detector = new CustomSeparatorDetectorImpl();
@@ -22,7 +20,7 @@ class CustomSeparatorDetectorImplTest {
     }
 
     @Test
-    @DisplayName("커스텀구분자가 없는 경우")
+    @DisplayName("커스텀 구분자가 없는 경우")
     void detectSeparator_X() {
         // given
         String input = "1,2,3";
@@ -30,5 +28,49 @@ class CustomSeparatorDetectorImplTest {
         String customSeparator = detector.detectSeparator(input);
         // then
         assertThat(customSeparator).isEqualTo("");
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자가 2자리 이상")
+    void 커스텀_구분자_2글자_이상() {
+        // given
+        String input = "//abcde\n1,2,3";
+        // when
+        String customSeparator = detector.detectSeparator(input);
+        // then
+        assertThat(customSeparator).isEqualTo("abcde");
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 순서가 바뀐 경우")
+    void 커스텀_구분자_순서_바뀐_경우() {
+        // given
+        String input = "\n;//1,2,3";
+        // when
+        String customSeparator = detector.detectSeparator(input);
+        // then
+        assertThat(customSeparator).isEqualTo("");
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 안에 커스텀 구분자 \\n")
+    void 커스텀_구분자_안_커스텀_구분자_1() {
+        // given
+        String input = "//\n\n1,2,3";
+        // when
+        String customSeparator = detector.detectSeparator(input);
+        // then
+        assertThat(customSeparator).isEqualTo("\n");
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 안에 커스텀 구분자 //")
+    void 커스텀_구분자_안_커스텀_구분자_2() {
+        // given
+        String input = "////\n1,2,3";
+        // when
+        String customSeparator = detector.detectSeparator(input);
+        // then
+        assertThat(customSeparator).isEqualTo("//");
     }
 }
