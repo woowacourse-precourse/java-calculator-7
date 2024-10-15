24c6987aa8f624a0d29200d7469e16f3bbd4f3a0
Jhzlo <kjh0010703@naver.com>
2024-10-15 22:50:54 +0900

    fix(output-view): "결과 : " 메시지 출력

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,6 +2,6 @@ package calculator.view;
 
 public class OutputView {
     public void printResult(Integer result){
-        System.out.println(result);
+        System.out.println("결과 : "+result);
     }
 }
