457485982a4395db2cfcb5d09472f97b5b7c2c50
woogym <kwj9294@gmail.com>
2024-10-16 06:03:54 +0900

    feat(view): implement calculator inputView

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,13 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputExpressionString() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String expression = Console.readLine();
+
+        return expression;
+    }
+}
