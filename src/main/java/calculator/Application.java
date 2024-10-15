8817799315fbbcb51ba137898adedfc3b37736f2
jiyun <tomatoziiilll@gmail.com>
2024-10-15 17:53:46 +0900

    feat: extracted numbers with specified separators

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,10 +2,19 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         String input = Console.readLine();
+        List<Integer> numbers = Arrays.stream(input.split("[,:]"))
+                .filter(s -> !s.isEmpty())
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
 
-        System.out.println(input);
+        System.out.println(numbers);
     }
 }
