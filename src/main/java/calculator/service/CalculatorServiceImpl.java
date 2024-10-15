4e244e73f1d288e5e7a444d5a46a7822703d4206
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-10-16 01:32:42 +0900

    refactor: 매직 리터럴 제거

--- src/main/java/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/calculator/service/CalculatorServiceImpl.java
@@ -5,25 +5,29 @@ import java.util.List;
 
 public class CalculatorServiceImpl implements CalculatorService {
 
+    private final String startCustomDelimiter = "//";
+    private final String endCustomDelimiter = "\\n";
+    private final String basicDelimiter = ",|:";
+
     @Override
     public List<String> findValidNumber(String userInput) {
 
         List<String> numbers;
         String customDelimiter = findCustomDelimiter(userInput);
         if (customDelimiter == null || customDelimiter.isEmpty()) {
-            numbers = Arrays.asList(userInput.split(",|\\:"));
+            numbers = Arrays.asList(userInput.split(basicDelimiter));
             return numbers;
         }
-        String changedUserInput = userInput.substring(userInput.lastIndexOf("\\n") + 2,
+        String changedUserInput = userInput.substring(userInput.lastIndexOf(endCustomDelimiter) + 2,
                 userInput.length());
-        numbers = Arrays.asList(changedUserInput.split(",|\\:|\\"+ customDelimiter));
+        numbers = Arrays.asList(changedUserInput.split(basicDelimiter + "|" + customDelimiter));
         return numbers;
     }
 
     private String findCustomDelimiter(String userInput) {
-        if (userInput.contains("//") && userInput.contains("\\n")) {
-            int idx1 = userInput.indexOf("//")+2;
-            int idx2 = userInput.lastIndexOf("\\n");
+        if (userInput.contains(startCustomDelimiter) && userInput.contains(endCustomDelimiter)) {
+            int idx1 = userInput.indexOf(startCustomDelimiter) + 2;
+            int idx2 = userInput.lastIndexOf(endCustomDelimiter);
 
             StringBuilder sb = new StringBuilder();
             for (int i = idx1; i < idx2; i++) {
