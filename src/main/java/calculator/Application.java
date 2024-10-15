42f066e43e0752da539569da2082d2cee5bc41fb
imjaehyeong <dlawogud517@gmail.com>
2024-10-15 19:56:26 +0900

    feat: 커스텀 구분자 기능 추가
    ","와 ":" 뿐만 아니라 //와 \n 을 사용하여 커스텀 구분자를 사용할 수 있는
    기능을 추가하였습니다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,18 +1,42 @@
 package calculator;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input  = scanner.nextLine();
-        String[] spiltedNumber = input.split("[:,]");
-        int sum=0;
-        for(String number:spiltedNumber){
-            int num = Integer.parseInt(number);
-            sum +=num;
+        String input = scanner.nextLine();
+        String[] splitedNumber;
+        int sum = 0;
+        // //;\n1;2;3
+        try {
+            if (input.startsWith("//") && input.startsWith("\\n", 3)) {
+                char sperator = input.charAt(2);
+                String sparatorString = String.valueOf(sperator);
+                splitedNumber = input.substring(5).split(sparatorString);
+
+                System.out.println(splitedNumber);
+
+                for (String number : splitedNumber) {
+                    number.trim();
+                    int num = Integer.valueOf(number);
+                    sum += num;
+                }
+            } else {
+                splitedNumber = input.split("[:,]");
+                for (String number : splitedNumber) {
+                    int num = Integer.parseInt(number);
+                    sum += num;
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("error");
+            throw new RuntimeException(e);
         }
+
         System.out.println(sum);
+
     }
+
+
 }
