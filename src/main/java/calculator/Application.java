aa16bb8ef6ade4a2bf0898015f3bb173fd44c298
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-10-16 01:12:02 +0900

    chore: 공백

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,12 +4,15 @@ import calculator.controller.CalculatorController;
 import calculator.service.Adder;
 import calculator.service.Extractor;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         CalculatorController calculatorController = new CalculatorController(
                 new InputView(),
+                new OutputView(),
                 new Extractor(),
                 new Adder()
         );
