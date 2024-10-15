e5cde850b65b33ed52addc4a0a1e17f63f88f0c7
Jeonhui <48307153+Jeonhui@users.noreply.github.com>
2024-10-15 18:12:36 +0900

    feat: print result of addition

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,8 +8,15 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputText = Console.readLine();
         List<Integer> values = Arrays.stream(inputText.split("[,:]"))
                 .map(Integer::parseInt).toList();
+        Integer sum = 0;
+
+        for (Integer value : values)
+            sum += value;
+
+        System.out.println(sum);
     }
 }
