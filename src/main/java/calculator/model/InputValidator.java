c845e49fcae98633e5c8e3c7bda216ae56bda71b
jae woo Jung <awfjol2008@gmail.com>
2024-10-15 20:43:50 +0900

    feat: 입력값에 숫자가 포함되지 않은 경우, 예외를 반환하는 기능 구현

--- src/main/java/calculator/model/InputValidator.java
+++ src/main/java/calculator/model/InputValidator.java
@@ -1,4 +1,18 @@
 package calculator.model;
 
+import java.util.List;
+
 public class InputValidator {
+	List<String> delimiters;
+
+	public void validateHasNumber(String input) {
+		if (!hasNumber(input)) {
+			throw new IllegalArgumentException("숫자가 없습니다.");
+		}
+	}
+
+	private boolean hasNumber(String input) {
+		return input.chars()
+			.anyMatch(Character::isDigit);
+	}
 }
