93c8224c406922c9bf6da1186abfcfa12116f7c0
gyuho <kyuhokim12@gmail.com>
2024-10-15 16:40:24 +0900

    feat(input): 특정 문자열 입력 기능 추가
    문자열 입력 전 "덧셈할 문자열을 입력해 주세요." 문구 출력 후 사용자로부터 문자열을 입력 받는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,9 +2,13 @@ package calculator;
 
 import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         ArrayList<String> parser = new ArrayList<String>(List.of(",", ":"));
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
     }
 }
