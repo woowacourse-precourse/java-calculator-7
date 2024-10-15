854f56f6d460e946332d767a4ed989b4c9a525ce
sc@t0a14 <gusdn6671@gmail.com>
2024-10-16 01:36:26 +0900

    feat(DefaultDelimiter) : 기본 구분자 클래스 생성

---
+++ src/main/java/calculator/DefaultDelimiter.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class DefaultDelimiter {
+    String defaultDelimiter;
+
+    public DefaultDelimiter() {
+        this.defaultDelimiter = "[,:]";
+    }
+
+    public String getDefaultDelimiter() {
+        return defaultDelimiter;
+    }
+}
