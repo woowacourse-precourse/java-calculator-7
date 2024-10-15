175f637afcb47f6ac5c7e127cdad4d82df32d033
ddddabi <ii887742@gmail.com>
2024-10-15 20:25:09 +0900

    feat(input): 04. 출력 기능 / 05. 잘못된 입력 처리 기능 추가
    - 잘못된 숫자가 포함되어 있을 시 해당 문자열도 같이 출력해 더 나은 에러 확인 가능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -69,7 +69,7 @@ public class Application {
             try {
                 sum += Integer.parseInt(number);
             } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("잘못된 숫자가 포함되어 있습니다.");
+                throw new IllegalArgumentException("잘못된 숫자가 포함되어 있습니다: "+number);
             }
         }
         return sum;
