41e636b1a1941c26801434cc29ab287c7905c457
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-10-15 20:35:02 +0900

    fix: 숫자 추출하는 기능 중에서 이스케이프 문자 처리 수정

--- src/main/java/calculator/service/Extractor.java
+++ src/main/java/calculator/service/Extractor.java
@@ -15,12 +15,12 @@ public class Extractor {
         // TODO 여기서 중간에 validate 들어가야 함
 
         // TODO 커스텀으로 "'//', '\n'으로 입력받는 경우 delimeter를 해당 입력 값으로 변경할 것
-        if (input.contains("//") && input.contains("\n")) {
-            String[] tokens = input.split("//|\\n");
+        if (input.contains("//") && input.contains("\\n")) {
+            String[] tokens = input.split("//|\\\\n");
             // TODO tokens에 전부 다 들어 있으니 첫 번째 요소는 구분자로 쓰고 나머지는 통째로 뒤에 들어 있을 테니까
             //  나머지 부분을 구분자인 첫 번째 요소로 전부 잘라내면 그게 바로 숫자 ??
-            String delimeter = tokens[0];
-            String[] numberTokens = tokens[1].split(delimeter);
+            String delimeter = tokens[1];
+            String[] numberTokens = tokens[2].split(delimeter);
 
             for (String numberToken : numberTokens) {
                 integers.add(Integer.parseInt(numberToken));
