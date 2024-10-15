2590249cf5a329327fe4eac2235663d44d7692b2
pakaheo <k70923@konkuk.ac.kr>
2024-10-15 23:50:59 +0900

    test : test the empty input, input included basic delimiter, input included custom delimiter

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,2 +1,35 @@
-package calculator;public class StringCalculatorTest {
+package calculator;
+
+import calculator.domain.StringCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+
+    @Test
+    void 빈_문자열() {
+        assertThat(StringCalculator.calculate("")).isEqualTo(0);
+    }
+
+    @Test
+    void 구분자_콤마() {
+        assertThat(StringCalculator.calculate("1,2,3")).isEqualTo(6);
+    }
+
+    @Test
+    void 구분자_콜론() {
+        assertThat(StringCalculator.calculate("1:2:3")).isEqualTo(6);
+    }
+
+    @Test
+    void 구분자_컴마_콜론() {
+        assertThat(StringCalculator.calculate("1,2:3")).isEqualTo(6);
+    }
+
+    @Test
+    void 커스텀_구분자() {
+        assertThat(StringCalculator.calculate("//;\\n1;2;3")).isEqualTo(6);
+    }
+
 }
