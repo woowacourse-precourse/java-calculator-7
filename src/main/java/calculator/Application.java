a746ad518cff454735572349d0f3a97d5b950274
cux-maks <volcano020716@gmail.com>
2024-10-15 20:15:54 +0900

    feat(Application): 메인 실행 코드 작성
    메인 실행 코드를 작성했습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import calculator.StringCalculator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+        StringCalculator calculator = new StringCalculator();
+        try {
+            Integer sum = calculator.calculate(input);
+            System.out.println("결과 : " + sum.toString());
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+
+
     }
 }
