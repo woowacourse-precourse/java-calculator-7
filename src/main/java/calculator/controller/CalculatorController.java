4a86a31d09d86e65ab5ac16126751b62972df6c4
승엽 <tmdduqflfl@naver.com>
2024-10-15 23:06:16 +0900

    Feat: 컨트롤러 구현

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,30 @@
+package calculator.controller;
+
+import calculator.domain.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+import java.util.List;
+
+public class CalculatorController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final StringCalculator stringCalculator;
+
+    public CalculatorController(final InputView inputView, final OutputView outputView, final StringCalculator stringCalculator) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.stringCalculator = stringCalculator;
+    }
+
+    public void applicationRun() {
+        String value = inputView.receiveValue();
+        String customValue = stringCalculator.extractCustom(value);
+        String rawValue = stringCalculator.removeValue(value);
+        String[] pureValue = stringCalculator.createNewValue(rawValue, customValue);
+        List<Integer> saveValue = stringCalculator.saveValue(pureValue);
+        int result = stringCalculator.calculateSum(saveValue);
+        outputView.outputResult(result);
+    }
+}
