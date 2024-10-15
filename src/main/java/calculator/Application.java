7d623dd633517120c5e53a26b25fbcdddddfc55e
sc@t0a14 <gusdn6671@gmail.com>
2024-10-15 23:31:51 +0900

    feat(Procedural) : 값이 "" 일 경우 0으로 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,9 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,20 +12,26 @@ public class Application {
         String customDelimiter = "";
         String input = Console.readLine();
 
-        if(input.startsWith("//")){
+        if (input.startsWith("//")) {
             int delimiterEndIndex = input.indexOf("\\n");
-            customDelimiter = input.substring(2,delimiterEndIndex);
-            input = input.substring(delimiterEndIndex +2);
+            customDelimiter = input.substring(2, delimiterEndIndex);
+            input = input.substring(delimiterEndIndex + 2);
         }
 
-        String regex = delimiter.substring(0,delimiter.length()-1)+customDelimiter+"]";
+        String regex = delimiter.substring(0, delimiter.length() - 1) + customDelimiter + "]";
         System.out.println(regex);
-        String[] numbers = input.split(regex);
+        String[] inputs = input.split(regex);
 
-        for (String number : numbers) {
-            System.out.print(number);
+        // //;\n1,2:"";4
+        int sum = 0;
+        for (String number : inputs) {
+            number = number.trim();
+            if (number.isEmpty() || number.equals("\"\"")) {
+                sum += 0;
+            }
         }
 
+        System.out.println(sum);
 
     }
 }
