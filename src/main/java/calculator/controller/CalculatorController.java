4d795d95088b8598b903c5b94947478f313ad66e
slay <palgalow@gmail.com>
2024-10-16 00:10:52 +0900

    [feat]: 컨트롤러 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,20 +1,26 @@
 package calculator.controller;
 
+import calculator.domain.Calculator;
 import calculator.service.StringSeparator;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class CalculatorController {
     private StringSeparator stringSeparator;
     private InputView inputView;
+    private OutputView outputView;
+    private Calculator calculator;
 
     public CalculatorController() {
-      this.stringSeparator = new StringSeparator();
-      this.inputView = new InputView();
+        this.stringSeparator = new StringSeparator();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.calculator = new Calculator();
     }
 
     public void run() {
         String str = inputView.inputString();
         stringSeparator.divideSeparator(str);
-
+        outputView.outputResult(calculator);
     }
 }
