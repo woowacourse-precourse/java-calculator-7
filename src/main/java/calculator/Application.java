5a44a4b5c310c0eff5acfc062461e9afc29f40a7
leejunnyeop <daffodil8620@naver.com>
2024-10-16 02:23:50 +0900

    [feat] main 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+
 public class Application {
     public static void main(String[] args) {
+        CalculatorController controller = new CalculatorController();
+        controller.run();
         // TODO: 프로그램 구현
     }
 }
