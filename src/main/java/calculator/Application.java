6e200b3fea259ed76ef52bf2f0e1a836d748fecc
HyeonsuLee <leehyeonsu4888@naver.com>
2024-10-15 19:50:02 +0900

    feat: 기능 테스트

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Calculator calculator = new Calculator();
+        List<Integer> numbers = inputView.readNumbers();
+        int result = calculator.sum(numbers);
+        outputView.printResult(result);
     }
 }
