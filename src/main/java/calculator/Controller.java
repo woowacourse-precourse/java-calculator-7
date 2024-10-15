7989b05e2ece667d9e013abf820deb08603f512a
downfa11 <downfa11@naver.com>
2024-10-15 20:21:20 +0900

    feat: CheckCustomSeparator 메소드 구현

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -9,6 +9,10 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Controller {
 
     private static final int INITIAL_NUMBER = 0;
+    private static final int STRING_LAST_INDEX = 2;
+    private static final String START_CUSTOM_SEPARATE = "//";
+    private static final String END_CUSTOM_SEPARATE = "\\n";
+    private static final String CUSTOM_SEPARATE_ADD = "|";
     private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
     private static final String OUTPUT_MESSAGE = "결과 : ";
     private static final String BLANK_STRING = "";
@@ -26,13 +30,12 @@ public class Controller {
     private String input(){
         System.out.println(INPUT_MESSAGE);
         String inputField = readLine();
-        return inputField;
+        return checkCustomSeparator(inputField);
     }
 
     private Integer calc(String input){
         List<Integer> intList = parseToIntList(input);
-        System.out.println(intList); // todo. TESTCODE
-        return null;
+        return intList.isEmpty() ? INITIAL_NUMBER : null; //todo. TESTCODE
     }
 
     private void output(Integer result){
@@ -56,6 +59,25 @@ public class Controller {
                     return Integer.parseInt(number);
                 })
                 .toList();
+    }
+
+    private String checkCustomSeparator(String input){
+        if(input.startsWith(START_CUSTOM_SEPARATE) && input.contains(END_CUSTOM_SEPARATE)) {
+            try {
+                int begin = input.indexOf(START_CUSTOM_SEPARATE);
+                int end = input.lastIndexOf(END_CUSTOM_SEPARATE);
+
+                String customSeparator = input.substring(begin + STRING_LAST_INDEX, end);
+                REGEX = REGEX + CUSTOM_SEPARATE_ADD + customSeparator;
+
+                input = input.substring(end + STRING_LAST_INDEX);
+            }
+            catch(IllegalArgumentException e){
+                throw new IllegalArgumentException(e);
+            }
+        }
+
+        return input;
     }
 
 
