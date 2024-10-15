32b6de1f5ac3a11e7e134374ea795bce56b52f5b
yush <yush2202@gmail.com>
2024-10-16 06:04:57 +0900

    refactor($DelimiterConstants): 구분자 상수 추가
    구분자에서 활용할 상수 추가

---
+++ src/main/java/calculator/Constants/DelimiterConstants.java
@@ -0,0 +1,9 @@
+package calculator.Constants;
+
+public class DelimiterConstants {
+    public final static String COMMA = ",";
+    public final static String SEMICOLONS = ";";
+    public final static String BASIC_DELIMITER = COMMA + "|" + SEMICOLONS;
+    public final static String CUSTOM_DELIMITER_START = "//";
+    public final static String CUSTOM_DELIMITER_END = "\\n";
+}
