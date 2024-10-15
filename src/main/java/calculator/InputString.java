b7b2dbe88ae02f77f9b4b275db74810297ede306
imscow11253 <seaweed8759@naver.com>
2024-10-15 21:33:07 +0900

    feat : InputString 클래스 구현

---
+++ src/main/java/calculator/InputString.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import java.util.List;
+
+public class InputString {
+    private String input;
+    private String separator;
+    private List<Integer> operands;
+
+    public InputString(String input){
+        this.input = input;
+    }
+}
