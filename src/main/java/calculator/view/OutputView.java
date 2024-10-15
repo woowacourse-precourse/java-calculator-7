7430c9fcdc8f892f3654c656d2dbe3b5eff54aff
woogym <kwj9294@gmail.com>
2024-10-16 06:06:26 +0900

    feat(view): implement calculator outputView

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+
+    public void printCalculateResult(int result) {
+        System.out.println();
+        System.out.println("결과 : " + result);
+    }
+}
