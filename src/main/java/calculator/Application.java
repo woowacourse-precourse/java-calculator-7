7a359fb3b0e3ef7623615c5ce2e271b2a6fc9ec8
JunHyuk Yu <ericyoo0107@gmail.com>
2024-10-15 19:46:42 +0900

    feat: 커스텀 구분자 추출 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,30 @@
 package calculator;
 
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+
+    private Integer result = 0;
+    private static List<String> delimiterList = List.of(",", ":");
+    private static final String startCustom = "//";
+    private static final String endCustom = "\n";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = readLine();
+        if (input.startsWith(startCustom)) {
+            addCustomDelimiter(input);
+        }
+    }
+
+    private static void addCustomDelimiter(String input) {
+        Integer startIdx = input.indexOf(startCustom) + 2;
+        Integer endIdx = input.indexOf(endCustom);
+        if (startIdx != -1 && endIdx != -1 && startIdx < endIdx) {
+            delimiterList.add(input.substring(startIdx, endIdx));
+        } else {
+            throw new IllegalArgumentException("잘못된 input 값 : " + input);
+        }
     }
 }
