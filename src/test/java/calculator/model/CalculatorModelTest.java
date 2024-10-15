18e6ee65c4c8fe68231ebf94660d385ccff179c8
msk226 <kminseok5167@gmail.com>
2024-10-15 19:43:08 +0900

    test(test): 테스트 코드에 Calculator 생성자 통해 메서드 호출하도록 로직 수정

--- src/test/java/calculator/model/CalculatorModelTest.java
+++ src/test/java/calculator/model/CalculatorModelTest.java
@@ -11,42 +11,48 @@ class CalculatorModelTest extends NsTest {
     @Test
     public void 정상_계산한_경우_1(){
         String input = "1,2,3";
-        Integer result = Calculator.add(input);
+        Calculator calculator = new Calculator();
+        Integer result = calculator.add(input);
         assertEquals(result, 6);
     }
 
     @Test
     public void 정상_계산한_경우_2(){
         String input = "1;2;3;4";
-        Integer result = Calculator.add(input);
+        Calculator calculator = new Calculator();
+        Integer result = calculator.add(input);
         assertEquals(result, 10);
     }
 
     @Test
     public void 정상_계산한_경우_3(){
         String input = "1,2,3;4;5";
-        Integer result = Calculator.add(input);
+        Calculator calculator = new Calculator();
+        Integer result = calculator.add(input);
         assertEquals(result, 15);
     }
 
     @Test
     public void 커스텀_지정자_포함_정상_계산한_경우_1(){
         String input = "//;\n1;2;3";
-        Integer result = Calculator.addWithCustom(input);
+        Calculator calculator = new Calculator();
+        Integer result = calculator.add(input);
         assertEquals(result, 6);
     }
 
     @Test
     public void 커스텀_지정자_포함_정상_계산한_경우_2(){
         String input = "//*\n1*2;13";
-        Integer result = Calculator.addWithCustom(input);
+        Calculator calculator = new Calculator();
+        Integer result = calculator.add(input);
         assertEquals(result, 16);
     }
 
     @Test
     public void 커스텀_지정자_포함_정상_계산한_경우_3(){
         String input = "//~\n1~2~3~4";
-        Integer result = Calculator.addWithCustom(input);
+        Calculator calculator = new Calculator();
+        Integer result = calculator.add(input);
         assertEquals(result, 10);
     }
 
