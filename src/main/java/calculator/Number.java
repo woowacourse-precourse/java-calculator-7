1cc6c209a3782c4f31acdea066f112a10efda636
JongKyeong Kim <oo7bangjk@naver.com>
2024-10-15 16:07:58 +0900

    feat: 리스트에 숫자 추가, 합 구하기 메소드 작성

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -9,4 +9,17 @@ public class Number {
     public Number() {
         this.numbers = new ArrayList<>();
     }
+
+    public void addNumber(int number) {
+        numbers.add(number);
+    }
+
+    public int getSum() {
+        int sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
+        }
+
+        return sum;
+    }
 }
