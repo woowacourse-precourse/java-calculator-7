09b6cf84ae301c8515eeabf994f7dc089ca84b1a
정재민 <jeongjaemin@jeongjaemin-ui-noteubug.local>
2024-10-16 03:12:37 +0900

    feat: handle empty string input in StringCalculator - Implemented functionality in  method to return 0 for empty string input. - Ensured that the calculator does not perform unnecessary operations when input is empty.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringCalculator calculator = new StringCalculator();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        int result = calculator.calculate(input);
+        System.out.println("결과 : " + result);
+    }
+}
+
+class StringCalculator {
+    public int calculate(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+        return 1; // 빈문자열이 아닐 떄 임시 리턴값
     }
 }
