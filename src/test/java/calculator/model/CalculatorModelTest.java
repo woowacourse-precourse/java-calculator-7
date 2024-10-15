bf255c17012d7973d414293d472d23a6b04e9324
msk226 <kminseok5167@gmail.com>
2024-10-15 16:20:15 +0900

    test(test) : Model 테스트 코드 작성

---
+++ src/test/java/calculator/model/CalculatorModelTest.java
@@ -0,0 +1,57 @@
+package calculator.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import calculator.Application;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+class CalculatorModelTest extends NsTest {
+
+    @Test
+    public void 정상_계산한_경우_1(){
+        String input = "1,2,3";
+        Integer result = Calculator.add(input);
+        assertEquals(result, 6);
+    }
+
+    @Test
+    public void 정상_계산한_경우_2(){
+        String input = "1;2;3;4";
+        Integer result = Calculator.add(input);
+        assertEquals(result, 10);
+    }
+
+    @Test
+    public void 정상_계산한_경우_3(){
+        String input = "1,2,3;4;5";
+        Integer result = Calculator.add(input);
+        assertEquals(result, 15);
+    }
+
+    @Test
+    public void 커스텀_지정자_포함_정상_계산한_경우_1(){
+        String input = "//;\n1;2;3";
+        Integer result = Calculator.addWithCustom(input);
+        assertEquals(result, 6);
+    }
+
+    @Test
+    public void 커스텀_지정자_포함_정상_계산한_경우_2(){
+        String input = "//*\n1*2;13";
+        Integer result = Calculator.addWithCustom(input);
+        assertEquals(result, 16);
+    }
+
+    @Test
+    public void 커스텀_지정자_포함_정상_계산한_경우_3(){
+        String input = "//~\n1~2~3~4";
+        Integer result = Calculator.addWithCustom(input);
+        assertEquals(result, 10);
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
