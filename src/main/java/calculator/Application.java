429a0d907ae7e432d49bac1de1277b142aad1748
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-10-15 20:41:21 +0900

    feat(parser) input string을 Long List로 변환하는 parser개발

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
+
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputStringParser parser = new InputStringParser();
+        String inputString = Console.readLine();
+
+        try {
+            List<Long> list = parser.parse(inputString);
+
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
---
+++ src/main/java/calculator/InputStringParser.java
@@ -0,0 +1,46 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputStringParser {
+
+    public List<Long> parse(String input) throws IllegalStateException{
+
+        Optional<String> customIdentifier = parseCustomIdentifier(input);
+        try {
+            if (customIdentifier.isPresent()) {
+                String resizedSTring = resizeString(input);
+                return convertStringToList(resizedSTring, customIdentifier.get());
+            } else {
+                return convertStringToList(input, ", | :");
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    private List<Long> convertStringToList(String input, String regex) {
+        return Arrays
+                .stream(input.split(regex))
+                .map(Long::parseLong)
+                .toList();
+    }
+
+    private String resizeString(String input) {
+        Pattern pattern = Pattern.compile("^//(.*)\\\\n");
+        Matcher matcher = pattern.matcher(input);
+        return input.substring(matcher.end());
+    }
+
+    private Optional<String> parseCustomIdentifier(String input) {
+        Pattern pattern = Pattern.compile("^//(.*)\\\\n");
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.find()) return Optional.of(matcher.group(1));
+        else return Optional.empty();
+    }
+}
