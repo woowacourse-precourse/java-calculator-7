31436e5e004c6e3be8cc1fe32adbbef0640b96f4
yush <yush2202@gmail.com>
2024-10-16 05:36:45 +0900

    refactor($StringSplitter): 사용하지 않는 기능 제거
    불필요한 변수와 메서드 제거

--- src/main/java/calculator/Utils/StringSplitter.java
+++ src/main/java/calculator/Utils/StringSplitter.java
@@ -4,17 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StringSplitter {
-    private static final String COMMA = ",";
-    private static final String SEMICOLONS = ";";
-    private static final String DELIMITER = COMMA + "|" + SEMICOLONS;
-
-    public static List<String> splitByDelimiter(String userInput) {
-        String[] splittedInput = userInput.split(DELIMITER);
-        return Arrays.stream(splittedInput).toList();
-    }
-
-    public static List<String> splitByCumstomDelimiter(String userInput, String customDelimiter) {
-        String[] splittedInput = userInput.split(customDelimiter);
-        return Arrays.stream(splittedInput).toList();
+    public static List<String> splitByDelimiter(String userInput, String delimiter) {
+        String[] splitedInput = userInput.split(delimiter);
+        return Arrays.stream(splitedInput).toList();
     }
 }
