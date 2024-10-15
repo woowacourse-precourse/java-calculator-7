297dcc8d0e921e9ec7233d3cec3d697ee77c55f6
kimgoat <kimgoatlove@gmail.com>
2024-10-15 21:34:19 +0900

    feat: 문자열 입력하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
+    public static String userInput;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application app = new Application();
+        app.run();
+    }
+
+    public void run() {
+        System.out.println(Message.START.getMessage());
+        getUserInput();
+    }
+
+    /**
+     * 사용자가 콘솔에 입력한 값을 저장하는 함수
+     */
+    public void getUserInput() {
+        String input = Console.readLine();
+        userInput = input.replace("\\n", "\n");
     }
 }
