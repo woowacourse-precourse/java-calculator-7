68fb44b68d4e6744aad1f92f6fa90ad09b88b772
youngjuKim <kjuzoojuk@naver.com>
2024-10-15 22:46:17 +0900

    checkDelimiter 메서드와 입력부

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,25 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputStr = Console.readLine();
 
+    }
+
+    private static String[] checkDelimiter(String inputStr){
+        String[] values = {",|:", inputStr};
+
+        if(inputStr.contains("//") && inputStr.contains("\\n")){
+            int newLineIdx = inputStr.lastIndexOf("\\n");
+
+            values[0] += "|"+inputStr.substring(2,newLineIdx);
+            values[1] = inputStr.substring(newLineIdx+2, inputStr.length());
+        }
+
+        return values;
     }
 
 }
