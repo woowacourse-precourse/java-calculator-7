74b2dfe8582c0dc78c8bfc09de348761b328b84a
gitubanana <ptyolo666@gmail.com>
2024-10-15 23:15:18 +0900

    feat: init main method
    각 클래스를 활용해, 메인 코드를 작성한다.
    1. CalculatorInput 클래스를 통해 사용자의 입력을 문자열로 가져온다.
    2. 위의 문자열을 통해, CalculatorParser를 생성한다.
    3. 위의 CalculatorParser의 getCustomDelimiter 메서드를 통해 Calculator를 생성한다.
    4. Calculator의 getResult메서드를 통해 수식의 결과값을 구한다.
    5. CalculatorOutput 클래스를 통해 결과값을 형식에 맞게 출력한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,11 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String inputLine = CalculatorInput.getString();
+        CalculatorParser parser = new CalculatorParser(inputLine);
+        Calculator casio = new Calculator(parser.getCustomDelimiter());
+        double result = casio.getResult(parser.getExpression());
+
+        CalculatorOutput.print(result);
     }
 }
