d667f2c2c2f895d40fcf7630f9480ea83cbb984e
Firedrago95 <yonghwa95@naver.com>
2024-10-15 19:06:56 +0900

    feat : 단위 기능 통합하는 컨트롤러 작성, 애플리케이션 완성

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,21 @@
+package calculator.controller;
+
+import calculator.model.InputParser;
+import calculator.model.SumCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+import java.util.List;
+
+public class CalculatorController {
+
+    private InputParser inputParser = new InputParser();
+    private SumCalculator sumCalculator = new SumCalculator();
+
+    public void run() {
+        String input = InputView.readNumbers();
+        List<Integer> numList = inputParser.checkAndParseInput(input);
+        int sum = sumCalculator.sum(numList);
+        OutputView.printResult(sum);
+    }
+
+}
