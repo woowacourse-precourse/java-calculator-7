dd6bd86c83ec168c8ad1fa78d20f7f5414a9aed1
ouvir <kilhyunii@naver.com>
2024-10-15 19:36:39 +0900

    feat(application): 기능 흐름 주석 추가 및 IO 코드 추가
    - 기능 흐름에 대한 주석 추가
    - IO 에 대한 코드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    public static final String OUTPUT_MESSAGE = "결과 : ";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //1. 입력 받기
+        System.out.println(INPUT_MESSAGE);
+        String input = Console.readLine();
+        //2. 입력에 대해, 커스텀 구분자를 확인하여, 구분자에 추가해줌
+
+        //3. 기본 구분자와 커스텀 구분자를 통해, 숫자를 구분함
+
+        //4. 숫자가 아닌 문자에 대한 예외 처리 진행
+
+        //5. 각각의 숫자에 연산자를 통해, 결과값 얻기
+
+        //6. 결과 출력
+        System.out.println(OUTPUT_MESSAGE + input);
     }
 }
