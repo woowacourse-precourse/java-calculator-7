b1782114b067d074b13d56ca1ebb9041d85f91e2
Jhzlo <kjh0010703@naver.com>
2024-10-15 20:48:08 +0900

    feat(calculator): calculator 모델 객체 생성

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,5 +1,6 @@
 package calculator.controller;
 
+import calculator.model.Calculator;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
@@ -13,7 +14,8 @@ public class CalculatorController {
     }
 
     public void run() {
-        String input = inputAddition();
+        String expression = inputAddition();
+        Calculator calculator = new Calculator(expression);
     }
 
     private String inputAddition() { // 덧셈 문자열을 입력 받는다.
---
+++ src/main/java/calculator/model/Calculator.java
@@ -0,0 +1,11 @@
+package calculator.model;
+
+public class Calculator {
+    private final String expression;
+
+    public Calculator(String expression){
+        this.expression = expression;
+    }
+
+
+}
