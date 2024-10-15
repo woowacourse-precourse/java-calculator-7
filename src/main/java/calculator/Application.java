e7f555301b6844e9bb27eeb365dfbc49f21fa6d0
장혜민 <gpals5987@gmail.com>
2024-10-15 17:40:44 +0900

    [feat] inputView 작성
    - printStart 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView.printStart();
     }
 }
--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,4 +1,9 @@
 package calculator.view;
 
+import calculator.constant.ViewConstants;
+
 public class InputView {
+    public static void printStart(){
+        System.out.println(ViewConstants.INPUT_STRING.getMessage());
+    }
 }
