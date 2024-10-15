65037a4a4478bd835c5f437ee866ccdd864e5040
hwan2-99 <thh1233123@gmail.com>
2024-10-16 01:08:58 +0900

    feat: 커스텀 구분자 구분기능 구현

--- src/main/java/calculator/domain/StringAddCalculator.java
+++ src/main/java/calculator/domain/StringAddCalculator.java
@@ -1,11 +1,14 @@
 package calculator.domain;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class StringAddCalculator {
     private static final String DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER_START = "//";
+    private static final String CUSTOM_DELIMITER_END = "\\n";
     private static final int DEFAULT_VALUE = 0;
+
+    private static final String NUMBER_FORMAT_ERROR = "숫자값만 입력가능합니다.";
 
     public StringAddCalculator() {
     }
@@ -14,19 +17,43 @@ public class StringAddCalculator {
         if (inputValue.isBlank()) {
             return DEFAULT_VALUE;
         }
-        return sum(splitToInteger(inputValue));
+        return sum(inputValue);
     }
 
-    private List<Integer> splitToInteger(final String inputValue) {
-        List<String> values = Arrays.asList(inputValue.split(DELIMITER));
-        return values.stream()
-                .map(Integer::parseInt)
-                .toList();
+    private String[] splitString(final String inputValue) {
+        if (inputValue.startsWith(CUSTOM_DELIMITER_START) && inputValue.contains(CUSTOM_DELIMITER_END)) {
+            int delimiterEnd = inputValue.indexOf(CUSTOM_DELIMITER_END);
+            String customDelimiter = inputValue.substring(CUSTOM_DELIMITER_START.length(), delimiterEnd);
+            return inputValue.substring(delimiterEnd + CUSTOM_DELIMITER_END.length())
+                    .split(customDelimiter);
+        }
+        return inputValue.split(DELIMITER);
     }
 
-    private int sum(final List<Integer> values) {
-        return values.stream()
-                .mapToInt(Integer::intValue)
-                .sum();
+    private int sum(final String inputValue) {
+        int[] values = convertToInt(inputValue);
+        int sum = 0;
+        for (int value : values) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    private int[] convertToInt(final String inputValue) {
+        String[] values = splitString(inputValue);
+        int[] result = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            validateNumberValue(values[i]);
+            result[i] = Integer.parseInt(values[i]);
+        }
+        return result;
+    }
+
+    private void validateNumberValue(final String value) {
+        try {
+            Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR);
+        }
     }
 }
