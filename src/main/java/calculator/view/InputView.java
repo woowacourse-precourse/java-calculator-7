f6a2a589b38b167ea7ff3e55bff57d8e1c00c96b
jaeyeong <zzz26lk@gmail.com>
2024-10-15 18:10:31 +0900

    feat: addInput Console.readline() 메서드를 사용해서 사용자의 입력을 받는다.

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String readString(){
+        String input = Console.readLine();
+        return input;
+    }
+}
