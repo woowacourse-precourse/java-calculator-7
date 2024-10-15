012caa78eba6cd62b347db62ef0465ad2fa29bdf
ouvir <kilhyunii@naver.com>
2024-10-15 23:12:45 +0900

    feat: CustomSeparatorDetectorImpl 구현
    테스트를 만족하도록 구현체 추가

---
+++ src/main/java/detector/CustomSeparatorDetectorImpl.java
@@ -0,0 +1,16 @@
+package detector;
+
+public class CustomSeparatorDetectorImpl implements CustomSeparatorDetector {
+    private static final String START_TEXT = "//";
+    private static final String END_TEXT = "\n";
+
+    @Override
+    public String detectSeparator(String text) {
+        int s = text.indexOf(START_TEXT);
+        int e = text.lastIndexOf(END_TEXT);
+        if (s < e && s != -1) {
+            return text.substring(s + START_TEXT.length(), e);
+        }
+        return "";
+    }
+}
