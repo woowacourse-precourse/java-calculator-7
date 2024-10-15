9d1d35e91a6a55def14ff91c0d2ef9b94583ef5f
min <m2insapp@gmail.com>
2024-10-15 17:10:55 +0900

    feat(Calculator.java): 숫자 추출 및 합산을 위한 parseInput 메서드 추가
    - Calculator 생성자에서 입력값을 초기화
    - 입력값을 파싱하는 parseInput 메서드 구성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+//        String input = Console.readLine();
+        String input = "3;2";
+
+        Calculator calculator = new Calculator(input);
+        int result = calculator.calculate();
+        System.out.println(result);
     }
 }
---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class Calculator {
+
+    public Calculator(String input){
+        parseInput(input); // 숫자 추출 및 합산을 메서드 호출
+    }
+
+
+    private void parseInput(String input){
+
+    }
+
+}
