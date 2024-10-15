f283b3a87bab163a6a6a13bfd1d59456601748ff
sc@t0a14 <gusdn6671@gmail.com>
2024-10-16 03:15:41 +0900

    feat(CustomDelimiter) : 커스텀 구분자에 문자만 입력하는 예외사항 구현.

--- src/main/java/calculator/CustomDelimiter.java
+++ src/main/java/calculator/CustomDelimiter.java
@@ -1,11 +1,15 @@
 package calculator;
 
 public class CustomDelimiter {
-    String customDelimiter;
+    private final String customDelimiter;
 
     public CustomDelimiter(String customDelimiter) {
+        if (customDelimiter.matches("^[a-zA-Z]+$")) {
+            throw new IllegalArgumentException("커스텀 구분자는 문자만 입력가능합니다");
+        }
         this.customDelimiter = customDelimiter;
     }
+
 
     public String getCustomDelimiter() {
         return customDelimiter;
