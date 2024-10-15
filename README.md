d82e6aa0283af8d553de84609fc2033d826db1ed
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2024-10-16 00:24:06 +0900

    feat(Application): add catching custom separator
    Added new feature to Application:
    - Make an array containing ":" and custom separator.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # java-calculator-precourse
+1. 커스텀 구분자의 지정 여부 확인
+2. 입력한 문자열에서 콤마를 제외한 구분자를 콤마로 대체
+3. 문자열을 콤마를 기준으로 나누어 배열 만듦
+4. 잘못된 값 입력 시 IllegalArgumentException 에러 발생
+5. 정상적인 값 입력 시 합을 반환
--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,29 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    public static boolean catchCustomSeparator(String input) {
+        String left = input.substring(0,2);
+        String right = input.substring(3,5);
+
+        return left.equals("//") && right.equals("\\n");
+    }
+
+    public static String[] separators(String input) {
+        String[] separators = new String[]
+                {":"};
+        if (catchCustomSeparator(input)) {
+            separators =
+                    new String[]{":", input.substring(2, 3)};
+        }
+
+        return separators;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = camp.nextstep.edu.
+                missionutils.Console.readLine();
+
     }
 }
