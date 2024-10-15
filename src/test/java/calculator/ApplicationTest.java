4aee669ea65a47ccbccc6059b240bb106df5cdbc
cux-maks <volcano020716@gmail.com>
2024-10-15 20:49:07 +0900

    test(calculator): 커스텀 구분자 다중 사용에 대한 케이스 추가
    커스텀 구분자 다중 사용에 대한 테스트 케이스를 추가하였습니다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -49,6 +49,22 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_다중사용1() {
+        assertSimpleTest(() -> {
+            run("//<sep>|<SEP>\\n1<sep>2<SEP>3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_다중사용2() {
+        assertSimpleTest(() -> {
+            run("//<sep>|<SEP>|sep\\n1<sep>2<SEP>3sep4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
     void 예외_테스트1() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1,2,,3"))
