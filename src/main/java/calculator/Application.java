be0d1a7de431af252694ebe081e0800c804d864a
imjaehyeong <dlawogud517@gmail.com>
2024-10-15 18:47:37 +0900

    feat: "," 와 ":" 를 구분자로 하여 나누고 숫자의 합을 반환하는 프로그램 구현
    정규표현식을 사용하여 입력된 문자를 구분하였고 String타입의 문자를
    숫자로 형 변환 한 뒤 합을 구하였습니다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        String input  = scanner.nextLine();
+        String[] spiltedNumber = input.split("[:,]");
+        int sum=0;
+        for(String number:spiltedNumber){
+            int num = Integer.parseInt(number);
+            sum +=num;
+        }
+        System.out.println(sum);
     }
 }
