78df41db9a3a2e8d12c161fb386e03b4a374d981
rjswjddn <rjswjddn@gmail.com>
2024-10-16 00:06:37 +0900

    feat: 커스텀 구분자 저장 findCustomSeparator 메서드 구현
    String의 indexOf, substring을 이용해 커스텀 구분자를 찾아냄
    커스텀 구분자 부분을 없앤 문자열, 커스텀 구분자를 리스트로 만들어 반환함 return[0] : 커스텀 구분자 부분을 없앤 문자열, return[1] : //와 \n을 없앤 커스텀 구분자

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,12 +2,17 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<String> separator = List.of(",", ":");
         String input;
 
         input = inputString();
+        List<String> numberStringAndCustomSeparator = findCustomSeparator(input);
+        System.out.println(numberStringAndCustomSeparator);
     }
 
     // 문자열 입력
@@ -16,6 +21,25 @@ public class Application {
     }
 
     // 커스텀 구분자 저장
+    // return[0] : 커스텀 구분자 부분을 없앤 문자열
+    // return[1] : //와 \n을 없앤 커스텀 구분자
+    public static List<String> findCustomSeparator(String input) {
+        // //구분자\n
+        int customSeparatorStart = input.indexOf("//");
+        int customSeparatorEnd = input.indexOf("\\n");
+
+        if(customSeparatorStart == -1) {
+            return List.of(input, "");
+        } else if(customSeparatorStart != 0 || customSeparatorEnd == -1) {
+            throw(new IllegalArgumentException());
+        }
+
+        String customSeparator = input.substring(2, customSeparatorEnd);
+        String numberString = input.substring(customSeparatorEnd+2);
+
+        return List.of(numberString, customSeparator);
+    }
+
     // 구분자로 문자열에서 숫자 추출
     // 숫자 합 계산
     // 결과 반환
