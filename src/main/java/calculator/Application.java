cb546222666894a8106daf84d82cd97f1846146f
youngjuKim <kjuzoojuk@naver.com>
2024-10-15 22:31:01 +0900

    Changes

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,5 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
         System.out.println("test");
