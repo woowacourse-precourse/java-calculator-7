47184817c96ee4851f64eb08eb72e96efee1592f
junhyunlim73 <limjh0703@gmail.com>
2024-10-15 17:17:52 +0900

    Feat: 커스텀 구분자 사용 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,7 +6,44 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String name = Console.readLine();
-        System.out.println("Result: " + name);
+        String input = Console.readLine();
+        try {
+            // 입력받은 문자열로 덧셈 계산
+            int result = StringCalculate(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("예외 발생: " + e.getMessage());
+        }
     }
+
+    private static int StringCalculate(String input) {
+        int sum = 0;
+
+        String delimiter = ",|:";
+
+        if (input.startsWith("//")) {
+            delimiter = parseDelimiter(input);  // 커스텀 구분자 추출
+            input = input.substring(input.indexOf("\\n") + 2);
+        }
+
+        String[] temps = input.split(delimiter);
+
+        for(String num : temps) {
+            sum += Integer.parseInt(num);
+        }
+
+        return sum;
+    }
+
+    private static String parseDelimiter(String input) {
+        int startIndex = input.indexOf("//") + 2;
+        int endIndex = input.indexOf("\\n");
+
+        if (endIndex == -1) {
+            throw new IllegalArgumentException("구분자가 잘못되었습니다.");
+        }
+
+        return input.substring(startIndex, endIndex); // "//"와 "\n" 사이의 구분자 추출
+    }
+
 }
