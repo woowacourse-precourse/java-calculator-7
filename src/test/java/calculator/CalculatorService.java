9520cfd83c19bd3049780e5823afb5fd048fbe9d
LJM\eueo8 <eueo8259@gmail.com>
2024-10-15 19:24:18 +0900

    feat: 기본 구분자 처리

--- src/test/java/calculator/CalculatorService.java
+++ src/test/java/calculator/CalculatorService.java
@@ -7,4 +7,13 @@ public class CalculatorService {
         }
         return -1;
     }
+
+    public int [] splitByDefaultDelimiter(String answer){
+        String [] stringArrays = answer.split(",|:");
+        int [] intArrays = new int[stringArrays.length];
+        for (int i = 0; i < intArrays.length; i++){
+            intArrays[i] = Integer.parseInt(stringArrays[i]);
+        }
+        return intArrays;
+    }
 }
--- src/test/java/calculator/CalculatorServiceTest.java
+++ src/test/java/calculator/CalculatorServiceTest.java
@@ -2,15 +2,30 @@ package calculator;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorServiceTest {
     private int answer;
+    private CalculatorService cal = new CalculatorService();
+
     @Test
     void 빈_문자열_처리(){
-        CalculatorService cal = new CalculatorService();
         String input = "";
         answer = cal.returnAnswer(input);
         assertEquals(0, answer);
     }
+
+    @Test
+    void 기본_구분자_처리(){
+        String input = "1,2,3";
+        int [] expected = {1,2,3};
+        int [] actual = cal.splitByDefaultDelimiter(input);
+        assertArrayEquals(expected, actual);
+
+        String input2 = "1:2,3";
+        int [] expected2 = {1,2,3};
+        int [] actual2 = cal.splitByDefaultDelimiter(input2);
+        assertArrayEquals(expected2, actual2);
+    }
+
 }
