17a54b778b5b1bfe96d013ac389fc34aec4a4970
MeangSung <aoddlsgh98@gmail.com>
2024-10-15 17:29:55 +0900

    Add Calculate feature
    입력받은 문자열에 대해 구분하고, 잘못된 입력에 대해
    예외처리(IllegaArgumentException) 및 구분자로 구분된 숫자에 대한 합을
    구할 수 있도록 구현
    
    Closes #2

---
+++ src/main/java/calculator/Calculate.java
@@ -0,0 +1,41 @@
+package calculator;
+
+public class Calculate {
+
+    private static String input;
+    private static String delimiter;
+
+    public Calculate(String input) {
+        Calculate.input = input;
+        delimiter = ",;";
+    }
+
+    private static String[] toArray() {
+
+        if (input.startsWith("//") && input.indexOf("\n") == 3) {
+            int delimiterIndex = input.indexOf("\n");
+            delimiter = input.substring(2, delimiterIndex);
+            input = input.substring(delimiterIndex + 1);
+        }
+
+        return input.split(delimiter);
+    }
+
+    public int cal() {
+        int sum = 0;
+
+        String[] arr = toArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            try {
+                int num = Integer.parseInt(arr[i]);
+                sum += num;
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sum;
+    }
+
+}
