a55e7d110cbb2294c22f317ee9b5ee8973a54925
HyeonsuLee <leehyeonsu4888@naver.com>
2024-10-15 19:14:44 +0900

    feat: 커스텀 구분자로 분리하는 기능 작성

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -7,13 +7,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String readCommand() {
+    public Command readCommand() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine();
+        return Command.from(Console.readLine());
     }
 
     public List<Integer> readNumbers() {
-        String command = readCommand();
-        return Arrays.stream(command.split("[,:]")).mapToInt(Integer::parseInt).boxed().toList();
+        Command command = readCommand();
+        String splitter = String.format("[%s,:]", command.header());
+        return Arrays.stream(command.content().split(splitter)).mapToInt(Integer::parseInt).boxed().toList();
     }
 }
