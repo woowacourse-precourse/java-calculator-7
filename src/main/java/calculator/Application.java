d274247565a7827bb6786a82bb76859773927e5b
KORyounho <dordyd@naver.com>
2024-10-16 02:00:00 +0900

    chore() change methodname as is getResult to be calc

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,7 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class Application {
 
@@ -10,30 +11,42 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String input = Console.readLine();
+        String input = "";
+        try{
+            input = Console.readLine();
+        } catch (NoSuchElementException e){
+        }
 
         int result = customProcess(input);
 
         System.out.println("결과 : " + result);
     }
 
+
     private static int customProcess(String input) {
         int result = 0;
         initDelimiter();
 
-        if (input.startsWith("//") && input.charAt(3) == 92 && input.charAt(4) == 'n') {
+        if (isCustom(input)) {
             char customDelimiter = input.charAt(2);
             addDelimiter(customDelimiter);
 
             input = input.substring(input.indexOf("\n") + 1);
         }
-        result = getResult(input, result);
+        result = calc(input, result);
 
         return result;
     }
 
-    private static int getResult(String input, int result) {
-        String[] numbers = splitInput(input.substring(5));
+    private static boolean isCustom(String input) {
+        return input.startsWith("//") && input.charAt(3) == 92 && input.charAt(4) == 'n';
+    }
+
+    private static int calc(String input, int result) {
+        String[] numbers = splitInput(input);
+        if (isCustom(input)) {
+            numbers = splitInput(input.substring(5));
+        }
 
         for (String number : numbers) {
             if (!number.isEmpty()) {
@@ -73,9 +86,9 @@ public class Application {
         throw new IllegalArgumentException("잘못된 기호가 들어갔습니다.");
     }
 
-    public static int validValue(String value){
+    public static int validValue(String value) {
         int integerValue = Integer.parseInt(value);
-        if(integerValue >= 0){
+        if (integerValue >= 0) {
             return integerValue;
         }
         throw new IllegalArgumentException("음수의 값이 들어왔습니다.");
