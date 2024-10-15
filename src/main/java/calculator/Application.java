20a04c8340cd698061009164279f813f43ed5ab0
MeangSung <aoddlsgh98@gmail.com>
2024-10-15 19:17:36 +0900

    Add start feature in Application.java
    2024.10.15
    애플리케이션이 동작하도록 main으로 합침

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+        Calculate calculate;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = input.nextLine();
+        calculate = new Calculate(str);
+
+        int res = calculate.cal();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("결과 : ").append(res);
     }
 }
