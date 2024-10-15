5d2eb139ce1e8c18e43978acbd580eac7f7efdd1
msk226 <kminseok5167@gmail.com>
2024-10-15 22:02:06 +0900

    docs(view): OutputView 메서드 주석 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,6 @@ import calculator.controller.CalculatorController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         CalculatorController controller = new CalculatorController();
 
         controller.startCalculation();
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -4,12 +4,15 @@ import calculator.model.Calculator;
 
 public class OutputView {
     private final static String OUTPUT_MESSAGE = "결과 : ";
-    private Integer result;
+    private final Integer result;
 
     public OutputView(Integer result){
         this.result = result;
     }
 
+    /**
+     * 계산 결과를 출력합니다.
+     */
     public void printResult(){
         System.out.println(OUTPUT_MESSAGE + result);
     }
