a0bd4563bd4eb7ffe14a65224b4dc16c83f8cde9
Taekgil <taekgil273@gmail.com>
2024-10-16 06:29:24 +0900

    feat: sum number
    입력받은 숫자의 합계산 및 출력 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,11 +2,13 @@ package calculator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import java.util.Arrays;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        // 결과값 변수 생성
+        long answer = 0L;
 
         // camp.nextstep.edu.missionutils.Console의 readLine()을 사용해 문자열을 String으로 저장
         String input = readLine();
@@ -14,8 +16,12 @@ public class Application {
         //쉼표(,)와 콜론(:)을 구분자로 하여 각 숫자로 분리한다.
         String[] strArr = input.split("[,:]");
 
+        // strArr의 숫자들을 더한 결과를 answer에 할당
+        for (String str : strArr) {
+            answer += Long.parseLong(str);
+        }
+
         // 테스트 결과 출력
-        System.out.println("input = " + input);
-        System.out.println("strArr = " + Arrays.toString(strArr));
+        System.out.println("결과 : " + answer);
     }
 }
