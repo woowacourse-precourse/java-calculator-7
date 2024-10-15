a7e545d166714a339cf95a1db008432d67c4086d
Jaehwi Song <wotns0319@gmail.com>
2024-10-16 00:56:18 +0900

    fix(calculator): handle incorrect input with IllegalArgumentException
    Implemented Exception handling logic to detect and handle incorrect inputs by throwing
    IllegalArgumentException. The program terminates when the input contains incorrect input, which is negative number or unknown delimiter.
    
    Closes #3

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,7 +9,7 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = camp.nextstep.edu.missionutils.Console.readLine();
 
-        // 기능 1,2 : basic & custom delimiters
+        // 기능 1,2,3 : basic & custom delimiters + handling incorrect input exception
         if (!input.isEmpty()) {
             //custom delimiter
             if (input.contains("//")) {
@@ -22,7 +22,17 @@ public class Application {
             String[] input_array = input.split(delimiters);
 
             for (String operand: input_array) {
-                result += Integer.parseInt(operand);
+                // handling incorrect input exception with IllegalArgumentException
+                try {
+                    int item = Integer.parseInt(operand);
+                    if (item < 0) {
+                        throw new IllegalArgumentException("음수 값은 허용되지 않습니다 : " + item);
+                    }
+                    result += item;
+                }
+                catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("잘못된 입력 값입니다 : " + input, e);
+                }
             }
         }
         System.out.println("결과 : " + result);
