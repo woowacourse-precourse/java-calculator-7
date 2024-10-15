ab42730ff8bfd6ec6b3834684472585a01f596aa
Ek, Kyungbin <eukyungbin0@gaml.com>
2024-10-15 18:38:05 +0900

    feat : Handle empty input to return 0

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = sc.nextLine();
+
+        try {
+            int result = calculate(input);
+            System.out.println("결과 : " + result);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("잘못된 입력입니다." + e.getMessage());
+        }
+    }
+    public static int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
     }
 }
