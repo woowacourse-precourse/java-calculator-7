284776fb331dac2472eff3065aa04dad60854b9d
tjfwltn <tjfwltn1234@gmail.com>
2024-10-15 18:41:34 +0900

    A
    `

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller();
+        controller.run();
     }
 }
