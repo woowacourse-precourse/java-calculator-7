13cd61329c3831bc38f3deddafbaed174da25472
haebyun <bhk4392@gmail.com>
2024-10-15 23:20:36 +0900

    feat(ErrorMessage): NOT_NUMBER_ERROR 추가
    - NOT_NUMBER_ERROR 메시지를 추가하여 유효한 숫자가 아닌 값 입력 시 처리할 수 있도록 추가

--- src/main/java/calculator/domain/exception/ErrorMessage.java
+++ src/main/java/calculator/domain/exception/ErrorMessage.java
@@ -2,7 +2,8 @@ package calculator.domain.exception;
 
 public enum ErrorMessage {
     INVALID_CUSTOM_DELIMITER_ERROR("커스텀 구분자 형식이 잘못되었습니다."),
-    INVALID_RANGE_ERROR("계산할 숫자는 양수로 입력해주세요.");
+    INVALID_RANGE_ERROR("계산할 숫자는 양수로 입력해주세요."),
+    NOT_NUMBER_ERROR("유효한 숫자를 입력해주세요.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
