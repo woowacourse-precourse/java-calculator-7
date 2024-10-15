ae9ad2d42ea7e63182e5e27664cc80908f97b5cf
Sumin <new.sumyang@gmail.com>
2024-10-15 18:00:43 +0900

    refactor(view): 입력기능 static으로 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -4,7 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String input() {
+    public static String input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 }
