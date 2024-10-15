d72d8c646353f93aecef7699f20e0e2bdfc05f48
slay <palgalow@gmail.com>
2024-10-15 23:59:26 +0900

    [feat]: 구분자 커스텀 구분하는 메소드 구현

--- src/main/java/calculator/service/StringSeparator.java
+++ src/main/java/calculator/service/StringSeparator.java
@@ -10,15 +10,25 @@ public class StringSeparator {
 
     public void divideSeparator(String str) {
         String number = "";
+        String divide = "";
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                if (!divide.isEmpty()) {
+                    if (divide.charAt(0) != '/' || divide.charAt(1) != '/' || divide.charAt(divide.length() - 1) != 'n'|| divide.charAt(divide.length() - 2) != '\\') {
+                        IllegalArgumentException exception = new IllegalArgumentException("잘못된 입력입니다.");
+                    }
+                }
                 number += str.charAt(i);
             } else {
-                if (!number.isEmpty()) {
-                    calculator.addNumber(Integer.parseInt(number));
+                if (calculator.getSeparators().contains(str.charAt(i))) {
+                    if (!number.isEmpty()) {
+                        calculator.addNumber(Integer.parseInt(number));
+                    }
+                } else {
+                    divide += str.charAt(i)
                 }
-                if (calculator)
             }
         }
     }
+
 }
