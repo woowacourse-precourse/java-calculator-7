d2f04dd958f62cc3de84af8135a5530e5d5496e2
posite <su84053@gmail.com>
2024-10-15 23:01:35 +0900

    feat: 계산 결과 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,29 @@
 package calculator;
 
+import java.util.List;
+
+import static calculator.NumberCalculator.calculateNumber;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String userInput = readLine();
+        StringSpliter stringSpliter = new StringSpliter(userInput);
+        List<String> splitUserInput;
+        if (UserInputValidator.validateInputs(userInput)) {
+            System.out.println("결과 : " + 0);
+            return;
+        }
+        if (UserInputValidator.containCustomValidator(userInput)) {
+            stringSpliter = new StringSpliter(userInput.substring(userInput.indexOf("\\") + 2), userInput.substring(2, userInput.indexOf("\\")));
+            splitUserInput = stringSpliter.splitCustomDelUserInput();
+        } else {
+            splitUserInput = stringSpliter.splitUserInput();
+        }
+        if (UserInputValidator.isNumber(splitUserInput)) {
+            System.out.println("결과 : " + calculateNumber(splitUserInput));
+        }
+        
     }
 }
