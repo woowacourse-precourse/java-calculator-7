2d13f542c4d7b222c0fc33076ad662a1d6722f91
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-10-15 17:58:18 +0900

    feat(calculator): 입력하는 문자열 앞에 커스텀 구분자를 지정하게 하라

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,9 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
@@ -11,11 +14,20 @@ public class Application {
     }
 
     public static int calculate(String input) {
+        String delimiter = "[,|:]";
         int result = 0;
         if ("".equals(input)) {
             return result;
         }
-        String[] numbers = input.split("[,:]");
+
+        Pattern pattern = Pattern.compile("//(.)\\\\n(.*)");
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            delimiter = matcher.group(1);
+        }
+        input = matcher.group(2);
+        String[] numbers = input.split(delimiter);
+
         for (String number : numbers) {
             result += Integer.parseInt(number);
         }
