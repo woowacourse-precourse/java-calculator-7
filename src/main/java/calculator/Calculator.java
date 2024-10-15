64346b54c3138953a537e803935a580698e1cc22
B-efore <ljw010512@naver.com>
2024-10-15 19:56:05 +0900

    feat: 입력된 문자열에서 숫자 추출 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,7 +5,9 @@ public class Calculator {
     private Regex regex;
 
     public void calculate(){
-
+        regex = new Regex();
+        regex.inputExpression();
+        regex.setNumbers();
     }
 
 
--- src/main/java/calculator/Regex.java
+++ src/main/java/calculator/Regex.java
@@ -3,14 +3,16 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Regex {
 
     private static final String CUSTOM_DELIMITER_REGEX = "^//(.)\\\\n(.*)";
-    private static final String[] BASIC_DELIMITER = {",", ":"};
+    private static final String BASIC_DELIMITER = ",|:";
     private String customDelimiter;
     private String expression;
     private List<Integer> numbers = new ArrayList<>();
@@ -29,5 +31,30 @@ public class Regex {
         }
 
         Console.close();
+    }
+
+    public void setNumbers(){
+
+        if(customDelimiter == null){
+            numbers = getList(BASIC_DELIMITER);
+        } else {
+            customDelimiter = customDelimiter + "|" + BASIC_DELIMITER;
+            numbers = getList(customDelimiter);
+        }
+    }
+
+    private List<Integer> getList(String delimiter) {
+        return Arrays.stream(expression.split(delimiter))
+                .map(value -> {
+                    if (value.isEmpty()) {
+                        return 0;
+                    }
+                    try {
+                        return Integer.parseInt(value);
+                    } catch (IllegalArgumentException e) {
+                        throw new IllegalArgumentException("잘못된 입력값입니다.");
+                    }
+                })
+                .collect(Collectors.toList());
     }
 }
