8ccda27bd1a6ad46bec68725a791ddacc36ba943
Lys <redspinyant@gmail.com>
2024-10-16 07:35:29 +0900

    feat: 콜론(:) 구분자 추가
    - 쉼표(,)와 콜론(:)을 동시에 구분자로 사용

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,11 +4,14 @@ import java.util.Arrays;
 
 public class Calculator {
 
+	private static final String SEPARATOR = "[,:]";
+
 	public int sum(String input) {
 		if (input == null || input.isEmpty()) {
 			return 0;
 		}
-		String[] number = input.split(",");
+
+		String[] number = input.split(SEPARATOR);
 
 		return Arrays.stream(number).mapToInt(Integer::parseInt).sum();
 	}
