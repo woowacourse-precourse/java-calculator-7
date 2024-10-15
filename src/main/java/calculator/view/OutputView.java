69643e9694ed6f42bbf044b66fb1911418380d9b
Sumin <new.sumyang@gmail.com>
2024-10-15 17:58:50 +0900

    feat(view): 출력 기능 추가

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+
+    public static void printResult(int calculateResult) {
+        System.out.println("결과 : " + calculateResult);
+    }
+
+}
