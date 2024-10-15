adf62c0f296a3b3861870398804d7ca02111dd17
jumdo12 <jumdo12@gmail.com>
2024-10-16 00:16:10 +0900

    feat(controller): StringCalculatorController 구현
    - 사용자 입력을 처리하고 계산을 수행하는 StringCalculatorController 추가.
    - Console을 사용해 입력을 받고, StringCalculator를 통해 입력 문자열을 처리.
    - 기본적인 run 메서드를 포함하여 계산기 실행 가능.

--- src/main/java/controller/StringCalculatorController.java
+++ src/main/java/controller/StringCalculatorController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import domain.StringCalculator;
+
 public class StringCalculatorController {
+    private final StringCalculator calculator;
+
+    public StringCalculatorController() {
+        this.calculator = new StringCalculator();
+    }
+
+    public void run(){
+        System.out.println("덧셈할 문자열을 입력해 주세요:");
+        String input = Console.readLine();
+
+        int result = calculator.add(input);
+        System.out.println("결과 : " + result);
+    }
 }
