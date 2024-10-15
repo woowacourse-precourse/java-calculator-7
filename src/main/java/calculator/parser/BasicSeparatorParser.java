2f9a565b1230a68180149228a0e26aa265e039f8
changuii <rhljh7410@gmail.com>
2024-10-15 20:17:15 +0900

    feat(separatorParser) : 1.커스텀 구분자와 수식을 분리하고 구분자들의 집합을 만든다.
    - SeparatorParser 인터페이스 : 구분자를 처리하기위한 parser 인터페이스
    - BasicSeparatorParser 클래스 : separatorParser를 구현한 구현체
    - BasicSeparatorParser.isValid() : 커스텀 구분자들의 형식이 올바른지 검증하는 private 메서드
    - BasicSeparatorParser.parse() : 입력값에서 커스텀 구분자들을 분리하고 구분자 집합에 추가, 마지막 값은 수식에 해당하므로 수식을 반환

---
+++ src/main/java/calculator/parser/BasicSeparatorParser.java
@@ -0,0 +1,34 @@
+package calculator.parser;
+
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BasicSeparatorParser implements SeparatorParser{
+
+
+	private boolean isValid(String text) {
+		if (text.length() > 2 && text.startsWith("//")) {
+			return true;
+		} else {
+			throw new IllegalArgumentException("잘못된 구분자 형식입니다.");
+		}
+	}
+
+	@Override
+	public String parse(String text, Set<String> separators) {
+		StringTokenizer tokenizer = new StringTokenizer(text, "\\n");
+		while (tokenizer.countTokens() > 1){
+			String now = tokenizer.nextToken();
+
+			if(isValid(now)){
+				String separator = now.substring(2, now.length());
+				if(!separators.add(separator)){
+					throw new IllegalArgumentException("중복된 구분자입니다.");
+				}
+			}
+		}
+
+		String mathematicalExpression = tokenizer.nextToken();
+		return mathematicalExpression;
+	}
+}
---
+++ src/main/java/calculator/parser/SeparatorParser.java
@@ -0,0 +1,8 @@
+package calculator.parser;
+
+import java.util.Set;
+
+public interface SeparatorParser {
+
+	public String parse(String text, Set<String> separators);
+}
