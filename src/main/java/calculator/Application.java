abf3c7ee8fb3e124be236a2883656fb5471b9e75
Etwashoeren <sungmin0609@nate.com>
2024-10-15 17:41:22 +0900

    feat.Application.java 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,15 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputMethod inputMethod = new InputMethod();
+        SplitMethod splitMethod = new SplitMethod();
+
+        System.out.println("덧셈할 문자열을 입력.");
+
+        String input = inputMethod.input();
+
+        int result = splitMethod.split(input);
+
+        System.out.println("결과 : " + result);
     }
 }
