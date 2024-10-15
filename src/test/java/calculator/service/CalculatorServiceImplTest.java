247202a0e874b1b7d9b99f7bf413e2df2a8dab76
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-10-15 23:24:10 +0900

    test: 커스텀 구분자를 사용했을 때 여러 경우 테스트 코드 작성

--- src/test/java/calculator/service/CalculatorServiceImplTest.java
+++ src/test/java/calculator/service/CalculatorServiceImplTest.java
@@ -2,6 +2,7 @@ package calculator.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,4 +19,32 @@ public class CalculatorServiceImplTest {
         List<String> testNumber = List.of("1", "2", "3");
         assertThat(validNumber).containsAll(testNumber);
     }
+
+    @DisplayName("커스텀 구분자와 쉼표, 콜론: 을 함께 사용할 경우의 값 테스트")
+    @Test
+    void custom_delimiter_with_comma_and_colon_test(){
+        String inputString = "//;\\n1;2;3,4:5";
+        List<String> validNumber = calculatorService.findValidNumber(inputString);
+        List<String> testNumber = Arrays.asList("1","2","3","4","5");
+        assertThat(validNumber).containsAll(testNumber);
+    }
+
+    @DisplayName("쉼표, 커스텀구분자, 콜론: 을 함께 사용할 경우의 값 테스트")
+    @Test
+    void comma_with_custom_delimiter_colon_test(){
+        String inputString = "//;\\n1,2,3;4:5";
+        List<String> validNumber = calculatorService.findValidNumber(inputString);
+        List<String> testNumber = Arrays.asList("1","2","3","4","5");
+        assertThat(validNumber).containsAll(testNumber);
+    }
+
+    @DisplayName("커스텀 구분자가 한 자리가 아닐 경우 테스트")
+    @Test
+    void custom_delimiter_is_not_one_digit(){
+        String inputString = "//;;;;;;!!!!!\\n1;;;;;;!!!!!2;;;;;;!!!!!3;;;;;;!!!!!4";
+        List<String> validNumber = calculatorService.findValidNumber(inputString);
+        List<String> testNumber = Arrays.asList("1","2","3","4");
+        assertThat(validNumber).containsAll(testNumber);
+    }
+
 }
