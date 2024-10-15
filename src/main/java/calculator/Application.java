de451f16fccf10d4b7e5e90c0dc647dc5af62d07
JunHyuk Yu <ericyoo0107@gmail.com>
2024-10-15 20:00:10 +0900

    feat: 커스텀 구분자 추출 시 Valid 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,7 +9,7 @@ public class Application {
     private Integer result = 0;
     private static List<String> delimiterList = List.of(",", ":");
     private static final String startCustom = "//";
-    private static final String endCustom = "\n";
+    private static final String endCustom = "\\n";
 
     public static void main(String[] args) {
         String input = readLine();
@@ -24,7 +24,12 @@ public class Application {
         if (startIdx != -1 && endIdx != -1 && startIdx < endIdx) {
             delimiterList.add(input.substring(startIdx, endIdx));
         } else {
-            throw new IllegalArgumentException("잘못된 input 값 : " + input);
+            if (startIdx == -1 || endIdx == -1) {
+                throw new IllegalArgumentException("커스텀 구분자를 추가 하기 위해선 //구분자\\n 형식 으로 입력 하세요.");
+            }
+            if (startIdx >= endIdx) {
+                throw new IllegalArgumentException("커스텀 구분자를 입력 하세요.");
+            }
         }
     }
 }
