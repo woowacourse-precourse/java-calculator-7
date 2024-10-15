cd9b21cda7ef8a4c09eb730f6823a343cafb9c19
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-10-16 02:54:32 +0900

    feat : OutputView 출력 로직 추가

--- src/main/java/calculator/OutputView.java
+++ src/main/java/calculator/OutputView.java
@@ -4,6 +4,6 @@ public class OutputView {
     private final String outputMessage = "결과 : ";
 
     public void printResult(Long resultNumber) {
-
+        System.out.println(outputMessage + resultNumber);
     }
 }
