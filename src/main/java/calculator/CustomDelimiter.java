9acfe036019659293c9139d335fe7b7f6f3b73c9
sc@t0a14 <gusdn6671@gmail.com>
2024-10-16 01:49:27 +0900

    feat(CustomDelimiter) : 커스텀 구분자 클래스 생성

---
+++ src/main/java/calculator/CustomDelimiter.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class CustomDelimiter {
+    String customDelimiter;
+
+    public CustomDelimiter(String customDelimiter) {
+        this.customDelimiter = customDelimiter;
+    }
+
+    public String getCustomDelimiter() {
+        return customDelimiter;
+    }
+}
