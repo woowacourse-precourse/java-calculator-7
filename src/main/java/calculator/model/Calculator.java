b7c2be4e970b4e79a47cc14aa06684021f3f1d4f
woogym <kwj9294@gmail.com>
2024-10-16 04:44:48 +0900

    feat(model): implement calculator model

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -2,14 +2,12 @@ package calculator.model;
 
 public class Calculator {
 
-    private String[] extractNumbers(String input) {
-        String delimiter = "[,:]";
-        if (input.startsWith("//")) {
-            int delimiterIndex = input.indexOf("/n");
-            delimiter = input.substring(2, delimiterIndex);
-            input = input.substring(delimiterIndex + 1);
+    public int sum(int[] input) {
+        int total = 0;
+        for (int i : input) {
+            total += i;
         }
 
-        return input.split(delimiter);
+        return total;
     }
 }
