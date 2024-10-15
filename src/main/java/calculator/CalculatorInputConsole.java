e27ae472c44117e9d5595737b38af11ac3a29e02
Sy P <yongsa0221@gmail.com>
2024-10-16 01:50:53 +0900

    feat: 사용자 입력을 위한 CalculatorInputConsole 추가

---
+++ src/main/java/calculator/CalculatorInputConsole.java
@@ -0,0 +1,13 @@
+package calculator;
+import camp.nextstep.edu.missionutils.Console;
+
+public class CalculatorInputConsole {
+
+    private CalculatorInputConsole() {
+
+    }
+
+    public static String readLine() {
+        return Console.readLine();
+    }
+}
