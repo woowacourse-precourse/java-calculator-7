1be1a442168a64465172a7be754f1a1e19775354
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-15 20:11:56 +0900

    feat: 기본 구분자를 사용한 덧셈 기능 구현
    - ','와 ':'를 구분자로 사용하여 입력값을 분리
    - 분리된 값들을 숫자로 변환한 후 합산하여 결과 반환
    - 빈 문자열 입력 시 0을 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,6 +7,13 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        System.out.println("결과 : " + 6);
+        int result = 0;
+        if(!("".equals(input))) {
+            String[] splitNumbers = input.split(",|:");
+            for (String number : splitNumbers) {
+                result += Integer.parseInt(number);
+            }
+        }
+        System.out.println("결과 : " + result);
     }
 }
