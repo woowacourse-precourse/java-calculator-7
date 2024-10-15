4990fe5ba0bb314df53dde7bfea09738625195ee
yush <yush2202@gmail.com>
2024-10-16 05:08:27 +0900

    feat($StringSplitter): 문자열 구분 구현
    문자열을 구분을 위한 유틸리티 클래스와 메서드 구현

---
+++ src/main/java/calculator/Utils/StringSplitter.java
@@ -0,0 +1,16 @@
+package calculator.Utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringSplitter {
+    private static final String COMMA = ",";
+    private static final String SEMICOLONS = ";";
+    private static final String DELIMITER = COMMA + "|" + SEMICOLONS;
+
+    public static List<String> splitByDelimiter(String userInput) {
+        String[] splittedInput = userInput.split(DELIMITER);
+        return Arrays.stream(splittedInput).toList();
+    }
+
+}
