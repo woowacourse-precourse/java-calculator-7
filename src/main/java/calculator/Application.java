c2acc8aa3679c10b27d577dac61df82b9d277c51
touhou09 <yuseungju@yuseungjuui-MacBookAir.local>
2024-10-15 16:32:06 +0900

    feat($Application): 입력 구현
    $Application에 입력 추가:
    - 기초적인 문자열 입력 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 입력
+        System.out.println("덧셈할 문자열을 입력해 주세요.\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String str;
+        str = scanner.nextLine();
+        // 문자열 str 입력받음
+
+
+        // 커스텀 구분자 찾기
+
+        // 잘못된 문자열인지 오류 탐색 or 합 출력
     }
 }
