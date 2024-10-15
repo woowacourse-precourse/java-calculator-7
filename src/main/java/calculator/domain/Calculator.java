7ac7534788b9ed4604decfdef4c64e50e56aa388
slay <palgalow@gmail.com>
2024-10-15 23:38:27 +0900

    [feat]: 계산기 엔티티 숫자 리스트 반환 메소드 구현

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -23,4 +23,8 @@ public class Calculator {
     public List<String> getSeparators() {
         return this.separators;
     }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
 }
