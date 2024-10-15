664fa3e448c521437f83a1e9cac756defd741ea8
GaBaljaintheroom <junsu1222@naver.com>
2024-10-15 23:34:39 +0900

    feat(Caculator): 덧셈 범위 검증 로직 추가

--- src/main/java/calculator/SumCalculator.java
+++ src/main/java/calculator/SumCalculator.java
@@ -8,10 +8,17 @@ public class SumCalculator implements Calculator {
     public long sum(List<Integer> numbers) {
         long sum = 0;
         for(int number : numbers) {
+            validateRange(sum, number);
             sum += number;
         }
 
         return sum;
     }
 
+    private void validateRange(long sum, int number) {
+        long MAX_SUM = Long.MAX_VALUE;
+        if(sum > MAX_SUM - number) {
+            throw new IllegalArgumentException("합계가 최대값을 초과했습니다.");
+        }
+    }
 }
