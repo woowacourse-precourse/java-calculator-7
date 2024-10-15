369c7464b24fe3efef2282ae71e349c4548d6108
Zepelown <dbswjdqhd12@gmail.com>
2024-10-16 02:37:13 +0900

    feat: add input feature for addition

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,10 +1,13 @@
 package calculator.controller;
 
+import calculator.view.CalculatorInputView;
 import calculator.view.CalculatorOutputView;
 
 public class CalculatorController {
+    CalculatorInputView calculatorInputView = new CalculatorInputView();
     CalculatorOutputView calculatorOutputView = new CalculatorOutputView();
     public void joinStrings(){
         calculatorOutputView.printAddGreetingMessage();
+        String additionString = calculatorInputView.getAdditionString();
     }
 }
---
+++ src/main/java/calculator/view/CalculatorInputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CalculatorInputView {
+    public String getAdditionString(){
+        return Console.readLine();
+    }
+}
