c08b545cb1d3c87320a86313a552926f5e73242e
OSEUK <tpdnrzmzm3@naver.com>
2024-10-15 23:10:07 +0900

    feat(main): 문자열 분리
    쉼표(,)와 콜론(:)을 split과 정규 표현식을 사용하여 분리함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String s = Console.readLine();
+        String s = Console.readLine();  // 문자열 입력
+
+        String[] strings = s.split("[,:]"); // 쉼표와 콜론으로 문자열 분리
     }
 }
