ed44eaec231b418f304958148a11872fbed7f936
yesjuhee <juhee0924@g.skku.edu>
2024-10-16 08:54:22 +0900

    feat: 문자열을 입력 받아서 저장할 수 있다.

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+    private String inputStr;
+//    private char[] delimiters;
+//    private int result;
+
+    public Calculator(String inputStr) {
+        this.inputStr = inputStr;
+    }
+}
