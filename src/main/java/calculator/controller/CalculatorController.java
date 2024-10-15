abdad32d1ac2bca0ecd21ca211c78fd5e86d2b61
ryan-dia <cjfdnjs158@naver.com>
2024-10-15 18:39:09 +0900

    feat: 형식에 맞는 시작 문구 출력

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,10 @@
+package calculator.controller;
+
+import calculator.view.OutputView;
+
+public class CalculatorController {
+
+    public void run() {
+        OutputView.printStartMessage();
+    }
+}
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+public class OutputView {
+
+    private OutputView() {
+    }
+
+    public static void printStartMessage() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+}
