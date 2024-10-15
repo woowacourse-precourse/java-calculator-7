a21aa155e5d37080768cb71b387a2fdd58fef09d
SeongbinPark <hyucse22@gmail.com>
2024-10-15 17:31:23 +0900

    feat: sum input numbers with default delimiters

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // Get input
+        String input = readLine();
+
+        // 1-1. 기본 구분자만 입력된 경우
+        String defaultDelimiter = ",|:";
+        String[] tokens = input.split(defaultDelimiter);
+        int sum = 0;
+
+        for (String token : tokens) {
+            sum += Integer.parseInt(token);
+        }
+
+        System.out.println(sum);
     }
 }
