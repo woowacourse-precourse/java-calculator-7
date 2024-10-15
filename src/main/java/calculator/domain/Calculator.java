3a66279e6cfc5c11c2f236e883bdc5519b0abad4
slay <palgalow@gmail.com>
2024-10-15 23:10:41 +0900

    [feat]: 계산기 엔티티 생성

---
+++ src/main/java/calculator/domain/Calculator.java
@@ -0,0 +1,15 @@
+package calculator.domain;
+
+import java.util.List;
+
+public class Calculator {
+
+    private List<String> separator;
+
+    public Calculator() {
+        this.separator.add(",");
+        this.separator.add(";");
+    }
+
+
+}
