c75496ea32373ee05a731e7abd950f02dda768e2
leejunnyeop <daffodil8620@naver.com>
2024-10-15 22:53:14 +0900

    [feat] 입력값이 빈 문자열이거나 null 인 경우 0 으로 반환

---
+++ src/main/java/calculator/model/StringCalculator.java
@@ -0,0 +1,12 @@
+package calculator.model;
+
+public class StringCalculator {
+
+    // TODO 임시 -1 로 반환
+    public int add(String input) {
+        if (input != null || input.isEmpty()) {
+            return 0;
+        }
+        return -1;
+    }
+}
