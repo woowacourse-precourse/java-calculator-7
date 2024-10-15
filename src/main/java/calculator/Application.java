399daa224d1e8f827107e9a31963eb57125d0c05
byj7176@gmail.com <byj7176@gmail.com>
2024-10-16 01:06:54 +0900

    feat: 빈 문자열 또는 null 입력 처리 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,29 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 문자열 입력 받기
+        String input = Console.readLine();
+        try{
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            int result = StringCalculator.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            // 잘못된 문자열 입력 시 오류 메시지 출력
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class StringCalculator{
+    public static int add(String input){
+        // 빈 문자열 또는 null 입력 시 0 반환
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        int sum = 1;
+        return sum;
     }
 }
