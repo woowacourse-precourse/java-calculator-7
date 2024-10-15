b8c79f110a3c6de54a93e4376690b04a23d62da1
Jeonhui <48307153+Jeonhui@users.noreply.github.com>
2024-10-15 18:10:05 +0900

    feat: extract values as integers in text

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String inputText = Console.readLine();
+        List<Integer> values = Arrays.stream(inputText.split("[,:]"))
+                .map(Integer::parseInt).toList();
     }
 }
