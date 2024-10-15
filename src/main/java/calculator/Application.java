e4743bbc04f85a83551815f421a3f9fcfa15bf0b
gyuho <kyuhokim12@gmail.com>
2024-10-15 16:37:33 +0900

    feat(parser): 기본 구분자를 정의하는 기능 추가
    기본 구분자로 콤마(,)와 콜론(:)을 정의

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ArrayList<String> parser = new ArrayList<String>(List.of(",", ":"));
     }
 }
