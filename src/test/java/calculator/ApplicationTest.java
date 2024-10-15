701d3e36bfb4c8811efaa7c5eb2fb75ae6541df8
gitubanana <ptyolo666@gmail.com>
2024-10-15 22:56:40 +0900

    test(ApplicationTest): fix wrong delimiter in 실수_테스트 method
    ;를 :로 바꾼다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,15 +19,15 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
     @Test
     void 실수_테스트() {
         assertSimpleTest(() -> {
-            run("1.2;1.3,1"); // 실수도 계산 가능
+            run("1.2:1.3,1"); // 실수도 계산 가능
             assertThat(output()).contains("결과 : 3.5");
         });
     }
