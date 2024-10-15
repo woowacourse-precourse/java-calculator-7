c9dba4b180a4e905b44b01e30a0af32c9b91d902
making-a-scene <4936joo@naver.com>
2024-10-15 20:09:29 +0900

    feat: 빈 문자열이 입력으로 주어질 경우 0을 반환하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,5 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String input = Console.readLine();
+
+        if (input.isBlank()) {
+            System.out.println(0);
+            return;
+        }
     }
 }
