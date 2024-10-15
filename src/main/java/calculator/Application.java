a00bac4843d132cbff6734c62c794a410673bc59
khyunse0 <rjdls2549@donga.ac.kr>
2024-10-15 17:17:51 +0900

    String Split

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,5 +7,17 @@ public class Application {
         // 문자열 입력
         System.out.print("덧셈할 문자열을 입력해 주세요.");
         String FullLine = Console.readLine();
+        // 커스텀 구분자 및 기본 구분자로 문자열 분리
+        String[] SplitWord = MakeSplitWordList(FullLine);
+    }
+    public static String[] MakeSplitWordList(String FullLine) {
+        if (FullLine.startsWith("//")) {
+            int EndIndex = FullLine.indexOf("\n");
+            String CustomSplitWord = FullLine.substring(2, EndIndex);
+            String RemainLine = FullLine.substring(EndIndex+1);
+            return RemainLine.split("[,|:]" + CustomSplitWord);
+        } else {
+            return FullLine.split("[,|:]");
+        }
     }
 }
