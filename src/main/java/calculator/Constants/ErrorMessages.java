35df623c7f27b785291e04b2327962b643b10b0d
yush <yush2202@gmail.com>
2024-10-16 04:36:45 +0900

    feat($ErrorMessages): 새 에러 메시지 구현
    Numbers 가 비어있을 때 출력할 메시지 구현

--- src/main/java/calculator/Constants/ErrorMessages.java
+++ src/main/java/calculator/Constants/ErrorMessages.java
@@ -5,6 +5,7 @@ public enum ErrorMessages {
     INVALID_STRING_MESSAGE("문자열만 입력할 수 있습니다."),
     INVALID_BLANK_MESSAGE("공백은 입력할 수 없습니다."),
     INVALID_NUMBER_RANGE_MESSAGE("양수만 입력할 수 있습니다."),
+    INVALID_NUMBERS_EMPTY_MESSAGE("숫자들은 비어있을 수 없습니다."),
     INVALID_SEPARATOR_MESSAGE("잘못된 기본 구분자 입니다."),
     INVALID_CUSTOM_INDICATOR_MESSAGE("// 및 \\n 이외의 커스텀 구분자 지시자를 사용할 수 없습니다."),
     INVALID_CUSTOM_SEPARATOR_MESSAGE("잘못된 커스텀 구분자 입니다.");
