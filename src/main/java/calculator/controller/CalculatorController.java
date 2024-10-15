fcd5984e78b6ec464779df2af3579daed9c4f6de
ryan-dia <cjfdnjs158@naver.com>
2024-10-15 19:09:18 +0900

    feat: 사용자에게 덧셈할 문자열 입력받

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,10 +1,12 @@
 package calculator.controller;
 
+import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class CalculatorController {
 
     public void run() {
         OutputView.printStartMessage();
+        String inputStringForAddition = InputView.readInputString();
     }
 }
