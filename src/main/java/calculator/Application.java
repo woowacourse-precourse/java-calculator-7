0d6b1be29a1d68a8bb22cab01bb74c0533d0c30f
min <m2insapp@gmail.com>
2024-10-15 17:41:06 +0900

    feat(Calculator.java): 기본기능) 숫자 추출 및 합산 로직 추가 - 입력받은 문자열에서 쉼표(,) 및 콜론(:) 제거 - 남은 값을 getNumericValue로 변환 후 result에 합산 추가
    Closes #3

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,12 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
 //        String input = Console.readLine();
         String input = "3;2";
 
         Calculator calculator = new Calculator(input);
-        int result = calculator.calculate();
-        System.out.println(result);
+        System.out.println(calculator.getresult());
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,32 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Calculator {
+
+    private int result;
 
     public Calculator(String input){
         parseInput(input); // 숫자 추출 및 합산을 메서드 호출
     }
 
 
-    private void parseInput(String input){
 
+    private void parseInput(String input){
+        // 쉼표와 콜론 제거해서 list에 추가하기.
+        String cleanedInput = input.replace(",", "").replace(";", "");
+
+
+
+        for (char c :cleanedInput.toCharArray()) {
+            result += Character.getNumericValue(c);
+        }
+    }
+
+    public int getresult() {
+        return result;
     }
 
 }
