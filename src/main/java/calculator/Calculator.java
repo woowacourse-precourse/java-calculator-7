a3820a4f7bca05f0543c963986eaec19d31f91b7
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-10-15 20:23:19 +0900

    refactor(calculator): 의미가 있고 고정적인 변수들을 상수화하여라

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -7,25 +7,31 @@ public class Calculator {
     private static String delimiter = "[,|:]";
     private static int result = 0;
 
+    private static final String EMPTY_STRING = "";
+    private static final String WHITESPACE = " ";
+    private static final int CUSTOM_DELIMITER_ORDER = 1;
+    private static final int ARITHMETIC_EXPRESSION_ORDER = 2;
+    private static final String CUSTOM_REGEX = "//(.)\\\\n(.*)";
+
     public static int calculate(String input) {
-        if ("".equals(input)) {
+        if (EMPTY_STRING.equals(input)) {
             return result;
         }
 
-        if (input.contains(" ")) {
+        if (input.contains(WHITESPACE)) {
             throw new IllegalArgumentException("입력하신 문자열에는 공백이 포함될 수 없습니다.");
         }
 
-        Pattern pattern = Pattern.compile("//(.)\\\\n(.*)");
+        Pattern pattern = Pattern.compile(CUSTOM_REGEX);
         Matcher matcher = pattern.matcher(input);
         if (matcher.find()) {
-            delimiter = matcher.group(1);
-            input = matcher.group(2);
+            delimiter = matcher.group(CUSTOM_DELIMITER_ORDER);
+            input = matcher.group(ARITHMETIC_EXPRESSION_ORDER);
         }
 
         String[] numbers = input.split(delimiter);
         for (String number : numbers) {
-            if ("".equals(number)) {
+            if (EMPTY_STRING.equals(number)) {
                 throw new IllegalArgumentException("구분자 사이에는 숫자가 입력되어야 합니다.");
             }
 
