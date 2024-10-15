029b72612ae83744af7b85c5ec2c8c84ac2dce42
백재우 <9712jw@gmail.com>
2024-10-15 16:01:27 +0900

    docs : README.md 우아한테크코스 logo add

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# 문자열 덧셈 계산기
+# 프리코스 1주차 미션 - 문자열 덧셈 계산기
+<p align="center">
+    <img src="src/main/resource/logo.png" alt="우아한테크코스" width="250px">
+</p>
 
-기능 명세서
+---
+
+# 문자열 덧셈 계산기
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
