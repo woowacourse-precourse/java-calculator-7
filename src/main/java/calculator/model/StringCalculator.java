306cb116a5bf74bb60601fd9d20d41cdee56d4c6
leejunnyeop <daffodil8620@naver.com>
2024-10-15 22:59:00 +0900

    [feat] 쉼표 또는 콜론 기준으로 문자열 분리하고 그 숫자 합을 반환

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -2,11 +2,20 @@ package calculator.model;
 
 public class StringCalculator {
 
-    // TODO 임시 -1 로 반환
+    // TODO 숫자 더하는 함수 구현
     public int add(String input) {
         if (input != null || input.isEmpty()) {
             return 0;
         }
-        return -1;
+       String[] numbers = input.split(",|:");
+        return sum(numbers);
+    }
+
+    private int sum(String[] numbers) {
+        int total = 0;
+        for (String number : numbers){
+            total += Integer.parseInt(number);
+        }
+        return total;
     }
 }
