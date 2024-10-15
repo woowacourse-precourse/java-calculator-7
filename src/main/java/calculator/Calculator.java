4c737b242cb0a7b0036fa8bfbccb9a5db0d37dbc
new_dragon <tkddyd1126@gmail.com>
2024-10-15 23:25:05 +0900

    feat: 기본, 커스텀 구분자 구분 추가
    - isCustomDelimiters 함수 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,13 +3,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
 
+    static boolean customDelimiters = false;    // 기본 구분자, 커스텀 구분자 인지
+
+    // 기본 구분자 or 커스텀 구분자 구분하는 함수
+    private static void isCustomDelimiters(String formula) {
+        if (formula.startsWith("//")) {
+            customDelimiters = true;
+        }
+    }
+
     private static String input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 
     public static void run() {
-        String formula = input();
-
+        String formula = input();   // 사용자 입력
+        isCustomDelimiters(formula);    // 기본 구분자, 커스텀 구분자 구분하기
     }
 }
