5062069e1dee8bd919b8d01f8e638184b93eee79
yush <yush2202@gmail.com>
2024-10-16 05:55:01 +0900

    refactor($StringSplitter): 파라미터 수정
    String 구분자를 Delimiter로 수정

--- src/main/java/calculator/Utils/StringSplitter.java
+++ src/main/java/calculator/Utils/StringSplitter.java
@@ -1,11 +1,12 @@
 package calculator.Utils;
 
+import calculator.Model.Delimiter;
 import java.util.Arrays;
 import java.util.List;
 
 public class StringSplitter {
-    public static List<String> splitByDelimiter(String userInput, String delimiter) {
-        String[] splitedInput = userInput.split(delimiter);
+    public static List<String> splitByDelimiter(String userInput, Delimiter delimiter) {
+        String[] splitedInput = userInput.split(delimiter.getDelimiter());
         return Arrays.stream(splitedInput).toList();
     }
 }
