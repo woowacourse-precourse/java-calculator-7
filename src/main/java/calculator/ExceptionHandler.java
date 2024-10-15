941b6a15b5a91c9e2f09462d2abfaa93192209db
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-15 23:40:24 +0900

    feat(ExceptionHandler.java): 예외 처리 기능 중 첫번째 구현
    - 슬래시와 역슬래시 그리고 n 이외의 문자가 포함되어 있는 문자열에 대한 예외를 출력하는 메서드 checkIncorrectDelimGenerateInput을 구현하였다.
    - 쉼표와 콜론 그리고 커스텀 구분자로 지정하지 않은 문자가 포함되어 있는 문자열에 대한 예외를 출력하는 함수 checkIncorrectInput을 구현하였다.

---
+++ src/main/java/calculator/ExceptionHandler.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import java.util.List;
+
+public class ExceptionHandler {
+    private static ExceptionHandler instance;
+
+    private ExceptionHandler() {
+    }
+
+    public static ExceptionHandler getInstance() {
+        if (instance == null) {
+            instance = new ExceptionHandler();
+        }
+        return instance;
+    }
+
+    public void checkIncorrectDelimGenerateInput(char elem) {
+        if (elem != '/' && elem != 'n' && elem != '\\') {
+            throw new IllegalArgumentException(
+                    "지정된 구분자인 쉼표와 콜론, 양의 정수, 커스텀 구분자를 지정하기 위한 슬래시와 역슬래시 그리고 n 이외에는 입력하실 수 없습니다.");
+        }
+    }
+
+    public void checkIncorrectInput(char[] elems, char customDelim) {
+        for (char elem : elems) {
+            if (elem != ',' && elem != ':' && elem != customDelim && !Character.isDigit(elem)) {
+                throw new IllegalArgumentException(
+                        "지정된 구분자인 쉼표와 콜론, 양의 정수, 커스텀 구분자를 지정하기 위한 슬래시와 역슬래시 그리고 n 이외에는 입력하실 수 없습니다.");
+            }
+        }
+    }
+}
