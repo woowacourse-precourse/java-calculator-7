709ba0401564a93996634d2c4b75af9a40fff623
Firedrago95 <yonghwa95@naver.com>
2024-10-15 17:48:33 +0900

    feat : null 이면 List(0,0) 반환하는 기능 구현

---
+++ src/main/java/calculator/model/InputParser.java
@@ -0,0 +1,14 @@
+package calculator.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputParser {
+
+    public List<Integer> checkAndParseInput(String input) throws IllegalArgumentException{
+        if (input == null) {
+            return List.of(0,0);
+        }
+    }
+    
+}
