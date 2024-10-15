71321b7facf9da2b70ca09f78151b2b92fac255e
juhyeongKim <zxcvbm804@gmail.com>
2024-10-16 00:19:21 +0900

    feat: 문자열을 입력받는 기능을 추가
    main 함수에서 Calculator 객체를 생성
    Calculator 생성자로 문자열을 입력받게 하였음

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,30 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Calculator calc = new Calculator();
+    }
+}
+
+class Calculator {
+
+    static Str str;
+
+    Calculator() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        str = new Str(Console.readLine());
+    }
+}
+
+class Str {
+
+    private static String string;
+
+    Str(String string) {
+        this.string = string;
     }
 }
