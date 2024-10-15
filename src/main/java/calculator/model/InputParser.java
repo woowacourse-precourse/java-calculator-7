24676f9cebaa30754c21be576bd784cbeecb8c50
jae woo Jung <awfjol2008@gmail.com>
2024-10-15 21:15:01 +0900

    feat: 커스텀 구분자를 입력값에서 잘라내어 구분자 목록에 포함시키는 기능 구현

--- src/main/java/calculator/model/InputParser.java
+++ src/main/java/calculator/model/InputParser.java
@@ -6,6 +6,13 @@ import java.util.List;
 public class InputParser {
 	List<String> delimiters = Arrays.asList(",", ":");
 
+	private String addCustomDelimiter(String input) {
+		int delimiterStart = input.indexOf("//") + 2;
+		int delimiterEnd = input.indexOf("\n");
+
+		return input.substring(delimiterStart, delimiterEnd);
+	}
+
 	private boolean hasCustomDelimiter(String input) {
 		return input.startsWith("//");
 	}
