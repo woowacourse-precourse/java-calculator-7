f44e379c782f98aaa55523974017e3b4fb6fa411
kimgoat <kimgoatlove@gmail.com>
2024-10-15 21:36:06 +0900

    feat: 구분자를 기준으로 숫자 추출하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.*;
 
 public class Application {
     public static String userInput;
+    public static String[] numbers;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -14,6 +15,7 @@ public class Application {
     public void run() {
         System.out.println(Message.START.getMessage());
         getUserInput();
+        extractNumber();
     }
 
     /**
@@ -22,5 +24,25 @@ public class Application {
     public void getUserInput() {
         String input = Console.readLine();
         userInput = input.replace("\\n", "\n");
+    }
+
+    /**
+     * 구분자를 기준으로 숫자 추출하여 배열에 저장하는 함수
+     */
+    public void extractNumber(){
+        // 커스텀 구분자를 지정한 경우
+        if(userInput.matches("//[\\D]+(\r?\n)[\\s\\S]+")){
+            String separator ="";
+            int idx = 2;
+            while(true){
+                separator += userInput.charAt(idx++);
+                if(userInput.charAt(idx) == '\n') break;
+            }
+            numbers = userInput.substring(idx+1).split(separator);
+        }
+        // 커스텀 구분자를 지정하지 않은 경우 - 기본 구분자: 쉼표(,), 콜론(:)
+        else{
+            numbers = userInput.split(",|:");
+        }
     }
 }
