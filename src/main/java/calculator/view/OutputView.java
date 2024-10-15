e08cfc9327f03a17a5a72fa292082b411ae044fa
장혜민 <gpals5987@gmail.com>
2024-10-15 17:42:53 +0900

    [feat] outputView 작성
    - printResult 작성

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,4 +1,9 @@
 package calculator.view;
 
+import calculator.constant.ViewConstants;
+
 public class OutputView {
+    public static void printResult(int answer){
+        System.out.println(String.format(ViewConstants.OUTPUT_STRING.getMessage() , answer));
+    }
 }
