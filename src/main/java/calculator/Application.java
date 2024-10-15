381e3451c4d0cb65665b2a8e82146d4c53b88492
Jaehwi Song <wotns0319@gmail.com>
2024-10-16 02:48:10 +0900

    feat(calculator): structure implementation into functions
    Refactored the implementation of features into structured functions for better manageability.
    The following functions have been added:
    - public static String delimiter
    - public static String handleCustomDelimiter(String input);
    - public static void calculateResult(String input);
    
    Closes #4

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,40 +1,58 @@
 package calculator;
 
 public class Application {
+    public static String delimiters = ",|:";
+
+    // handle custom delimiter
+    public static String handleCustomDelimiter(String input) {
+        int custom_end_idx = input.indexOf("\\n");
+        String custom_delimiter = input.substring(2, custom_end_idx);
+        delimiters = delimiters + "|" + custom_delimiter;
+
+        return input.substring(custom_end_idx + 2);
+    }
+
+    // incorrect input exception handling & calculate result (for both custom, basic delimiter)
+    public static void calculateResult(String input) {
+        int result = 0;
+
+        String[] input_array = input.split(delimiters);
+
+        for (String operand: input_array) {
+            // handling incorrect input exception with IllegalArgumentException
+            try {
+                int item = Integer.parseInt(operand);
+                if (item < 0) {
+                    throw new IllegalArgumentException("음수 값은 허용되지 않습니다 : " + item);
+                }
+                result += item;
+            }
+            catch (NumberFormatException e) {
+                throw new IllegalArgumentException("잘못된 입력 값입니다 : " + input, e);
+            }
+        }
+        System.out.println("결과 : " + result);
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int result = 0;
-        String delimiters = ",|:";
-
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = camp.nextstep.edu.missionutils.Console.readLine();
 
         // 기능 1,2,3 : basic & custom delimiters + handling incorrect input exception
         if (!input.isEmpty()) {
-            //custom delimiter
+            // custom delimiter
             if (input.contains("//")) {
-                int custom_end_idx = input.indexOf("\\n");
-                String custom_delimiter = input.substring(2, custom_end_idx);
-                input = input.substring(custom_end_idx + 2);
-                delimiters = delimiters + "|" + custom_delimiter;
+                input = handleCustomDelimiter(input);
             }
-            // basic delimiters
-            String[] input_array = input.split(delimiters);
 
-            for (String operand: input_array) {
-                // handling incorrect input exception with IllegalArgumentException
-                try {
-                    int item = Integer.parseInt(operand);
-                    if (item < 0) {
-                        throw new IllegalArgumentException("음수 값은 허용되지 않습니다 : " + item);
-                    }
-                    result += item;
-                }
-                catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("잘못된 입력 값입니다 : " + input, e);
-                }
-            }
+            calculateResult(input);
         }
-        System.out.println("결과 : " + result);
+        // 빈 문자열이 입력된 경우
+        else {
+            System.out.println("결과 : 0");
+        }
+
     }
 }
