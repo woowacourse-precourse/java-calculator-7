6c80b350c31acef567cd8a6ef6d8c72ba5bc0ce7
DK <laancer4@gmail.com>
2024-10-15 16:59:59 +0900

    feat : 쉼표와 숫자가 포함된 문자열이 제공되면, 쉼표를 기준으로 숫자를 분리하여 더한 값을 리턴한다.

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,38 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Calculator {
+    private static final int EMPTY_VALUE = 0;
+
+    public int calculate(String input) {
+        validateInput(input);
+
+        if (input.isBlank()){
+            return EMPTY_VALUE;
+        }
+
+        int[] inputNumbers = Arrays.stream(input.split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        validateNumbers(inputNumbers);
+
+        return Arrays.stream(inputNumbers).sum();
+    }
+
+    private void validateInput(String input) {
+        // 여기서 validate 해야 split 때 넘버포맷익셉션 안난다.
+        if (Objects.isNull(input)){
+            throw new IllegalArgumentException("널 값은 허용되지 않는다.");
+        }
+    }
+
+
+    private void validateNumbers(int[] inputNumbers) {
+        if (Arrays.stream(inputNumbers).anyMatch(n -> n < 0)) {
+            throw new IllegalArgumentException("음수는 허용되지 않습니다: " + Arrays.toString(inputNumbers));
+        }
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,55 @@
+package calculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("생성자를 테스트한다.")
+    void constructorTest() {
+        Calculator calculator = new Calculator();
+        Assertions.assertThat(calculator).isNotNull();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "   "})
+    @DisplayName("빈 값이 들어오면 0을 반환한다.")
+    void whenEmptyStringIsGivenThenReturnZero(String emptyInput) {
+        Calculator calculator = new Calculator();
+        int calculated = calculator.calculate(emptyInput);
+        Assertions.assertThat(calculated).isZero();
+    }
+
+    @Test
+    @DisplayName("null 이 들어오면 예외가 발생한다.")
+    void whenNullStringIsGivenThenThrowsException() {
+        Calculator calculator = new Calculator();
+        assertThatThrownBy(() -> calculator.calculate(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("쉼표를 포함한 문자열이 주어지면, 쉼표를 기준으로 구분하여 숫자로 변환 후 더한다.")
+    @CsvSource(delimiter = '=', value = {"1,2,3=6", "0,1,2=3", "100,200,300=600", "1,2=3"})
+    void calculateByRest(String input, int expected) {
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate(input);
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("음수가 주어지면 예외가 발생한다.")
+    @ValueSource(strings = {"-1,-2,-3", "-1,5"})
+    void throwsExceptionWhenInputContainsNegative(String input) {
+        Calculator calculator = new Calculator();
+        assertThatThrownBy(() -> calculator.calculate(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
