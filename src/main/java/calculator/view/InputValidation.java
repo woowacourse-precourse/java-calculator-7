f1c77e6ec97e26e5c78327639677da74f92a3662
jaeyeong <zzz26lk@gmail.com>
2024-10-15 19:15:14 +0900

    feat: 양수가 아니면 예외 발생 사용자 입력의 숫자가 0 또는 음수이면 양수가 아니므로 예외를 발생한다.

--- src/main/java/calculator/view/InputValidation.java
+++ src/main/java/calculator/view/InputValidation.java
@@ -5,7 +5,7 @@ public class InputValidation {
     public static String readStringValidation(String input) {
         String[] inputStrings = input.split("");
         for (String inputString : inputStrings) {
-            if (isAlphabet(inputString)) {
+            if (isAlphabet(inputString) || isZeroOrNegativeNumber(inputString)) {
                 throw new IllegalArgumentException("구분자와 양수만 입력 가능합니다.");
             }
         }
@@ -14,5 +14,17 @@ public class InputValidation {
 
     private static boolean isAlphabet(String input) {
         return input.matches("[a-zA-Z]");
+    }
+
+    private static boolean isZeroOrNegativeNumber(String input) {
+        if (isNumber(input)) {
+            int inputNumber = Integer.parseInt(input);
+            return inputNumber <= 0;
+        }
+        return false;
+    }
+
+    private static boolean isNumber(String input) {
+        return input.matches("[0-9]");
     }
 }
