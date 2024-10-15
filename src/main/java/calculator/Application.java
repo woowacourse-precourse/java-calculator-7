21f9da8bb90c404f61f55b5b8065f544b6998da7
rjswjddn <rjswjddn@gmail.com>
2024-10-15 22:57:11 +0900

    feat: 문자열 입력 inputString() 메서드 구현
    camp.nextstep.edu.missionutils.Console import 하여 Console.readLine()으로 입력 받음

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input;
+
+        input = inputString();
     }
+
+    // 문자열 입력
+    public static String inputString() {
+        return Console.readLine();
+    }
+
+    // 커스텀 구분자 저장
+    // 구분자로 문자열에서 숫자 추출
+    // 숫자 합 계산
+    // 결과 반환
 }
