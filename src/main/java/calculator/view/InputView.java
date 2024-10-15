6d7fd21400147f21a0ba7e9739122e6d5b669e4b
pakaheo <k70923@konkuk.ac.kr>
2024-10-15 23:49:20 +0900

    feat : create Inputview / print start message

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,2 +1,10 @@
-package calculator.view;public class InputView {
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static void promptForAdditionInput() {
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+    }
 }
