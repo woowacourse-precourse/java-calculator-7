97bb7a28010d205aac4fa54965f29c265c52bb1a
JunBe <wnsqowid0901@gmail.com>
2024-10-15 18:16:25 +0900

    feat: 구분자를 통한 합 계산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,38 @@
 package calculator;
-
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        calStart();
+    }
+
+    private static void calStart() {
+        String[] digits = getDigits();
+        getResult(digits);
+    }
+
+    private static String input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine(); //한 줄 입력 받음
+    }
+
+    private static String[] getDigits() {
+        String input = input();
+        String seperator = ",:";
+        return input.split("[" + seperator + "]");
+    }
+
+    private static void getResult(String[] digits) {
+        int result = 0;
+        for (String val : digits) {
+            int intVal;
+            if (val.isEmpty()) {
+                intVal = 0;
+            } else {
+                intVal = Integer.parseInt(val);
+            }
+            result += intVal;
+        }
+        System.out.println("결과 : " + result);
     }
 }
