bc4b7fa055fc195eeb448d81fcf9f20cf8c0ebe1
JongKyeong Kim <oo7bangjk@naver.com>
2024-10-15 15:56:27 +0900

    feat: 분리할 숫자들을 저장할 1급 컬렉션 리스트 생성

---
+++ src/main/java/calculator/Number.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Number {
+    private final List<Integer> numbers;
+
+    public Number() {
+        this.numbers = new ArrayList<>();
+    }
+}
