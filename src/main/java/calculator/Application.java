014f424f79f5b22b42a95a1392be6d630cf6f2ad
Jaehwi Song <wotns0319@gmail.com>
2024-10-16 00:29:47 +0900

    feat(calculator): add custom delimiter support
    Added functionality to define a custom delimiter. The custom delimiter can be specified
    between '//' and '\n' at the start of the input string. The sum of the numbers will be
    calculated based on this delimiter.
    
    Closes #2

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,19 +4,27 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int result = 0;
+        String delimiters = ",|:";
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = camp.nextstep.edu.missionutils.Console.readLine();
 
-        // 기능 1: basic separator
+        // 기능 1,2 : basic & custom delimiters
         if (!input.isEmpty()) {
-            String[] input_array = input.split(",|:");
+            //custom delimiter
+            if (input.contains("//")) {
+                int custom_end_idx = input.indexOf("\\n");
+                String custom_delimiter = input.substring(2, custom_end_idx);
+                input = input.substring(custom_end_idx + 2);
+                delimiters = delimiters + "|" + custom_delimiter;
+            }
+            // basic delimiters
+            String[] input_array = input.split(delimiters);
 
             for (String operand: input_array) {
                 result += Integer.parseInt(operand);
             }
-
-            System.out.println("결과 : " + result);
         }
+        System.out.println("결과 : " + result);
     }
 }
