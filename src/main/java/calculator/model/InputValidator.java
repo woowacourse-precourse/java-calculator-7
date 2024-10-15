d08f26238100f81d41533de2b46b31fb2cf468fe
jae woo Jung <awfjol2008@gmail.com>
2024-10-15 20:51:25 +0900

    feat: 입력값에 기본 구분자가 포함되어 있지 않은 경우, 예외를 반환하는 기능 구현

--- src/main/java/calculator/model/InputValidator.java
+++ src/main/java/calculator/model/InputValidator.java
@@ -1,18 +1,30 @@
 package calculator.model;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class InputValidator {
-	List<String> delimiters;
+	List<String> delimiters = Arrays.asList(",", ":");
 
 	public void validateHasNumber(String input) {
 		if (!hasNumber(input)) {
-			throw new IllegalArgumentException("숫자가 없습니다.");
+			throw new IllegalArgumentException("입력값에 숫자가 포함되어 있지 않습니다.");
 		}
 	}
 
 	private boolean hasNumber(String input) {
 		return input.chars()
 			.anyMatch(Character::isDigit);
+	}
+
+	public void validateHasDelimiter(String input) {
+		if (!hasDelimiter(input)) {
+			throw new IllegalArgumentException("입력값에 구분자가 포함되어 있지 않습니다.");
+		}
+	}
+
+	private boolean hasDelimiter(String input) {
+		return delimiters.stream()
+			.anyMatch(input::contains);
 	}
 }
