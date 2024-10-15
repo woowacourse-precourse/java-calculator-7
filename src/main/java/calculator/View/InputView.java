88fa1520d1d4d555d7c613b1b2c83bc028e82607
yush <yush2202@gmail.com>
2024-10-16 04:12:30 +0900

    feat($InputView): 입력 메시지 출력 구현
    입력 받기 전 UI 출력 기능 구현

---
+++ src/main/java/calculator/View/InputView.java
@@ -0,0 +1,13 @@
+package calculator.View;
+
+import calculator.Constants.InputMessages;
+
+public class InputView {
+    InputView() {
+    }
+
+    public static void printInsertMessage() {
+        System.out.println(InputMessages.INSERT_STRING);
+    }
+
+}
