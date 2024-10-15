0768312d626956abf790447eb6f32e581022aba2
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-10-15 18:00:10 +0900

    feat: 커스텀 구분자 저장하기

---
+++ src/main/java/calculator/domain/InputStringSeparator.java
@@ -0,0 +1,30 @@
+package calculator.domain;
+
+public class InputStringSeparator {
+    private String inputString;
+    private String customSeparator = null;
+    private int[] inputNumber; // int vs integear ??
+
+    public InputStringSeparator(String inputString) {
+        this.inputString = inputString;
+        setCustomSeparator(this.inputString);
+    }
+
+    private void validateInputString(String inputString){
+
+    }
+
+    private void setCustomSeparator(String inputString){
+        int startIndex = 0 ;
+        int lastIndex = 0;
+        if(isDefaultSeparatorContained()){ //this.inputString 으로 판단하는게 좋은가?
+            startIndex = inputString.indexOf("//");
+            lastIndex = inputString.indexOf("\n");
+            this.customSeparator = inputString.substring(startIndex+2, lastIndex); //커스텀 구분자는 그럼 한 개만인가?
+        }
+    }
+
+    private boolean isDefaultSeparatorContained(){
+        return inputString.contains("//") && inputString.contains("\n");
+    }
+}
