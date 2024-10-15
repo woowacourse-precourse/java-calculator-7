a169e7245aeba5a3fb9b7d7a89ef569d2a1c1f18
seophohoho <seop0937@gmail.com>
2024-10-15 22:16:02 +0900

    feat(Application): 문자열 입력 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,8 @@ package calculator;
 import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     /** 구분자 목록 */
     public static List<String> separators = new ArrayList<>();
@@ -10,10 +12,17 @@ public class Application {
     public static void main(String[] args) {
         Application app = new Application();
         app.initSeparator();
+
+        String input = Console.readLine();
+        app.getCustomSeparator(input);
     }
 
     private void initSeparator(){
         separators.add(",");
         separators.add(":");
+    }
+
+    private void getCustomSeparator(String input){
+        System.out.println(input);
     }
 }
