144b22382d4e44ae193013abe16db2ee6e4e5169
joshinhyeok <shtree07@naver.com>
2024-10-15 19:17:37 +0900

    fix: 0 예외처리로 10도 예외처리되는 것 수정

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -3,21 +3,46 @@ package calculator;
 import java.util.regex.Pattern;
 
 public class Calculator {
-    int add (String str) {
-        if(str == "") return 0;
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String NEGATIVE_OR_ZERO_ERROR = "음수 또는 0은 입력되면 안됩니다.";
 
-        if(str.contains("-") || str.contains("0")){
-            throw new IllegalArgumentException("음수 또는 0이 입력되었습니다.");
+    int add (String inputString) {
+        if(inputString.isEmpty()) return 0; // 공백 예외처리
+
+        String delimiter = extractDelimiter(inputString); //조건 만족시 구분자 추가
+        inputString = removeCustomDelimiterInit(inputString); //조건 만족시 커스텀구분자 생성식 제거
+
+        negativeOrZeroCheck(inputString,delimiter); // -값, 0 예외처리
+
+        return calculateAdd(inputString, delimiter);
+    }
+
+    void negativeOrZeroCheck(String inputString, String delimiter) {
+        String[] split = inputString.split(delimiter);
+        for (String number : split) {
+            if(number.contains("-") || number.equals("0")) {
+                throw new IllegalArgumentException(NEGATIVE_OR_ZERO_ERROR);
+            }
         }
+    }
 
-        String delimiter = ",|:";
-        if(str.startsWith("//")){
-            String customDelimiter = str.substring(2, str.indexOf("\n"));
-            delimiter += "|" + Pattern.quote(customDelimiter);
-            str = str.substring(str.indexOf("\n") + 1);
+    String extractDelimiter(String inputString) {
+        if(inputString.startsWith("//")){
+            String customDelimiter = inputString.substring(2, inputString.indexOf("\n"));
+            return DEFAULT_DELIMITER + "|" + Pattern.quote(customDelimiter);
         }
+        return DEFAULT_DELIMITER;
+    }
 
-        String[] split = str.split(delimiter);
+    String removeCustomDelimiterInit(String inputString) {
+        if(inputString.startsWith("//")){
+            return inputString.substring(inputString.indexOf("\n") + 1);
+        }
+        return inputString;
+    }
+
+    int calculateAdd(String inputString, String delimiter) {
+        String[] split = inputString.split(delimiter);
         int sum = 0;
         for (String number : split) {
             sum += Integer.parseInt(number);
