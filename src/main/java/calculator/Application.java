9f94527bb0fae32b3cd1e80b89dbd94e729d3afd
suhyun9764 <suhyun9764@naver.com>
2024-10-16 04:01:48 +0900

    feat(init setting): 메세지, 고정 값 등 정적 변수로 선언

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,18 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+
+    // 1. 초기 세팅
+    public static final List<String> DEFAULT_SEPARATORS = Arrays.asList(",", ":");
+    public static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    public static final String OUTPUT_MESSAGE = "결과 : ";
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\n";
+    public static final Long MAXIMUM_NUMBER = Long.MAX_VALUE;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
