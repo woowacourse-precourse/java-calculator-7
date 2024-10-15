ff7f206af0dd2b03688f6da14bba70b33eaf80a8
Jaehwi Song <wotns0319@gmail.com>
2024-10-15 22:18:48 +0900

    feat(calculator): add support for adding numbers with comma and colon delimiters
    Implemented code to sum numbers in a string separated by commas (,) or colons (:).
    Returns result as 0 for an empty string and calculates the sum for non-empty input.
    
    Closes #1

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,20 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int result = 0;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = camp.nextstep.edu.missionutils.Console.readLine();
+
+        // 기능 1: basic separator
+        if (!input.isEmpty()) {
+            String[] input_array = input.split(",|:");
+
+            for (String operand: input_array) {
+                result += Integer.parseInt(operand);
+            }
+
+            System.out.println("결과 : " + result);
+        }
     }
 }
