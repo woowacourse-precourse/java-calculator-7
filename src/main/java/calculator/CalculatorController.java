5763be168abfce3d45737b314602355b868a58e7
BaSak0630 <babamba125@gmail.com>
2024-10-15 16:13:07 +0900

    feat : Add read String 사용자로 부터 문자열 입력 받기 기능 추가

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -1,16 +1,20 @@
 package calculator;
 
+import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class CalculatorController {
 
     private OutputView outputView;
+    private InputView inputView;
 
     public CalculatorController() {
         this.outputView = new OutputView();
+        this.inputView = new InputView();
     }
 
     public void calculate() {
         outputView.printStartMessage();
+        inputView.readString();
     }
 }
