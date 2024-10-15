52610160b8818709601a3446ee7d8bd400cf8fa6
sc@t0a14 <gusdn6671@gmail.com>
2024-10-16 03:20:59 +0900

    fix (CustomDelimiter) : 커스텀 구분자에 문자만 입력하는 예외사항 수정.

--- src/main/java/calculator/CustomDelimiter.java
+++ src/main/java/calculator/CustomDelimiter.java
@@ -4,7 +4,7 @@ public class CustomDelimiter {
     private final String customDelimiter;
 
     public CustomDelimiter(String customDelimiter) {
-        if (customDelimiter.matches("^[a-zA-Z]+$")) {
+        if (!customDelimiter.matches("^[a-zA-Z]+$")) {
             throw new IllegalArgumentException("커스텀 구분자는 문자만 입력가능합니다");
         }
         this.customDelimiter = customDelimiter;
