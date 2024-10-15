cbaf14189c130dde1b06e294f6c5314659fbe1ea
LSH <koreas9408@gmail.com>
2024-10-16 00:17:45 +0900

    feat: 쉼표(,) 또는 콜론(:)을 구분자를 기준으로 분리한 각 숫자의 합 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,30 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.platform.commons.util.StringUtils;
+
 public class Application {
+    private static final String 구분자 = "([,:])";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.printf("결과: " + execute(Console.readLine()));
+    }
+
+    private static String execute(String input) {
+        int sum = 0;
+        if (StringUtils.isBlank(input)) {
+            return String.valueOf(sum);
+        }
+
+        return String.valueOf(sum(input.split(구분자)));
+    }
+
+    private static int sum(String[] strings) {
+        int sum = 0;
+        for (String str : strings) {
+            sum += Integer.parseUnsignedInt(str);
+        }
+
+        return sum;
     }
 }
