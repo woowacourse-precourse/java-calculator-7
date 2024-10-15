0b440d5f40d7d3d749ee371ef117ef30973ec801
DanMac <dan@DanMacui-MacBookPro.local>
2024-10-16 04:04:13 +0900

    feat: add a basic addtion feature

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String[] numbers = Console.readLine().split("[,:]+");
+
+        int sum = Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
+
+        System.out.println("결과 : " + sum);
     }
 }
