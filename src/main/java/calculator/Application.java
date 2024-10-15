400f8fe1dfe0eedcb8db63239712c6177aeb98a7
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-15 21:15:32 +0900

    feat: 커스텀 구분자 계산 로직 구현
    - 계산 시에 일반 구분자와 커스텀 구분자 모두 사용
    - 커스텀 구분자는 여러 글자를 사용할 수 있도록 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,13 +3,23 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        input = input.replace("\\n", "\n");
         int result = 0;
         if(!("".equals(input))) {
-            String[] splitNumbers = input.split(",|:");
+            String[] splitNumbers;
+            if(input.startsWith("//") && input.contains("\n")) {
+                int delimiterIdx = input.indexOf("\n");
+                String delimiter = input.substring(2, delimiterIdx);
+                String numbersWithDelimiter = input.substring(delimiterIdx + 1);
+                splitNumbers = numbersWithDelimiter.split(",|:|" + delimiter);
+            } else {
+                splitNumbers = input.split(",|:");
+            }
             for (String number : splitNumbers) {
                 result += Integer.parseInt(number);
             }
