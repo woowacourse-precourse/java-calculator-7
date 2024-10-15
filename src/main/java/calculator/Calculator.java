8892b9e8d110c9479acf01b2b016f01f63c3bbb3
Chanwoo yang <moomsss@naver.com>
2024-10-15 21:45:12 +0900

    feat(계산): 커스텀 구분자를 추가해서 계산하는 기능 구현.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -10,12 +10,25 @@ public class Calculator {
         if(input.isEmpty()){
             return sum;
         }
+        boolean isNewDelimiter = checkNewDelimiter(input);
 
-        String[] numbers = input.split(delimiter);
+        String[] numbers;
+        if(isNewDelimiter){
+            delimiter = Character.toString(input.charAt(2));
+            input = input.substring(5);
+            numbers = input.split(delimiter);
+        }else{
+            numbers = input.split(delimiter);
+        }
+
 
         for(String number : numbers){
             sum += Integer.parseInt(number);
         }
         return sum;
+    }
+
+    private boolean checkNewDelimiter(String input){
+        return input.startsWith("//");
     }
 }
