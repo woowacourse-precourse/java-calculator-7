c4a24ab98d560d9f005911deccaeb947f9cc3db6
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-15 20:06:09 +0900

    feat : 계산 문자열 입력받기

---
+++ src/main/java/calculator/view/Input.java
@@ -0,0 +1,10 @@
+package calculator.view;
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public static String inputStringToAdd(){
+        return Console.readLine();
+    }
+
+}
