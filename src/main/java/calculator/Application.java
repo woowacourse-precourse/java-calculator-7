bafc13e1f1e5594ec537303a8336a6de09949f45
Bobae Kim <rlaqhqo0418@naver.com>
2024-10-16 06:54:20 +0900

    feat: 기본 계산기능 구현
    문자열을 입력받아 기본구분자로 구분
    
    구분한 문자를 숫자로 형변환하여 계산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,27 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String separator = ",|:";
+        int sum = 0;
+        Application app = new Application();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = readLine();
+
+        String[] separatedInput = input.split(separator);
+        for (String num : separatedInput) {
+            sum += app.stringToInt(num);
+        }
+        System.out.println("결과 : " + sum);
+    }
+    private int stringToInt(String num) {
+        if (num.isBlank()) {
+            return 0;
+        }
+        return Integer.parseInt(num);
     }
 }
