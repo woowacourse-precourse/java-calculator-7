20bea29c70b1f150a182a00913136dd4e7c46cca
hlmg <devhlmg@gmail.com>
2024-10-16 07:00:16 +0900

    feat: 계산할 문자 입력받는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        String userInput = "//1\n2,3:4";
+        String userInput = new InputHandler().getUserInput();
 
         UserInputConverter userInputConverter = new UserInputConverter();
         List<String> split = userInputConverter.split(userInput);
