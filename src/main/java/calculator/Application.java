1531f300d6ea5cc1b2fbb948b5f1a442a87b92e9
hlmg <devhlmg@gmail.com>
2024-10-16 05:17:08 +0900

    feat: 문자열 분리 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,23 +1,16 @@
 package calculator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    private static final List<String> DEFAULT_DELIMITERS = java.util.List.of(",", ":");
-    private static final List<String> DELIMITERS = new ArrayList<>(DEFAULT_DELIMITERS);
 
     public static void main(String[] args) {
         String userInput = "//1\n2,3:4";
 
-        CustomDelimiterCalculator customDelimiterCalculator = new CustomDelimiterCalculator();
-        if (customDelimiterCalculator.hasCustomDelimiter(userInput)) {
-            String customDelimiter = customDelimiterCalculator.extractCustomDelimiterFrom(userInput);
-            DELIMITERS.add(customDelimiter);
-            userInput = customDelimiterCalculator.removeCustomDelimiterPrefix(userInput);
-        }
+        UserInputConverter userInputConverter = new UserInputConverter();
+        List<String> split = userInputConverter.split(userInput);
 
         System.out.println(userInput);
-        System.out.println(DELIMITERS);
+        System.out.println(split);
     }
 }
---
+++ src/main/java/calculator/UserInputConverter.java
@@ -0,0 +1,35 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInputConverter {
+    private static final List<String> DEFAULT_DELIMITERS = java.util.List.of(",", ":");
+
+    private final CustomDelimiterCalculator customDelimiterCalculator = new CustomDelimiterCalculator();
+    private final List<String> delimiters = new ArrayList<>(DEFAULT_DELIMITERS);
+
+    public List<String> split(String str) {
+        if (customDelimiterCalculator.hasCustomDelimiter(str)) {
+            addCustomDelimiter(str);
+            str = customDelimiterCalculator.removeCustomDelimiterPrefix(str);
+        }
+
+        return List.of(splitByDelimiters(str));
+    }
+
+    private void addCustomDelimiter(String str) {
+        String customDelimiter = customDelimiterCalculator.extractCustomDelimiterFrom(str);
+        delimiters.add(customDelimiter);
+    }
+
+    private String[] splitByDelimiters(String str) {
+        String regex = getRegex();
+        return str.split(regex);
+    }
+
+    private String getRegex() {
+        String joinedDelimiters = String.join("", delimiters);
+        return String.format("[%s]", joinedDelimiters);
+    }
+}
