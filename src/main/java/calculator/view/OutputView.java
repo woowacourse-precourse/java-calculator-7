71a7c1cc18e9f2bef49adf97e1866a0d778f2c3f
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-10-15 17:16:38 +0900

    feat: 시작 메시지 출력함

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,7 +1,11 @@
 package calculator.view;
 
 public class OutputView {
-    public void printResult(Integer result){
-        System.out.println(result);
+    public void printStartMessage() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public void printResult(Integer result) {
+        System.out.println("결과 : " + result);
     }
 }
