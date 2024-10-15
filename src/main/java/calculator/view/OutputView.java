4fee0f446ea745b91ec9b5e43808e480d7a2bf77
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-15 23:13:29 +0900

    feat: 입출력 클래스 추가

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,10 @@
+package calculator.view;
+
+public class OutputView {
+
+    private static final String OUTPUT_MESSAGE = "결과 : ";
+
+    public static void printResult(String result) {
+        System.out.println(OUTPUT_MESSAGE + result);
+    }
+}
