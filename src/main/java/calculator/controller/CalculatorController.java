8bb71ee89a921c86f9e5ea437e3918d0c6ae52da
digitpic <jkw5033@gmail.com>
2024-10-15 19:31:12 +0900

    feat(input-view): 사용자로부터 문자열을 입력받는 기능 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,15 +1,22 @@
 package calculator.controller;
 
+import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class CalculatorController {
     private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
 
     public void run() {
         requestInput();
+        String input = receiveInput();
     }
 
     private void requestInput() {
         outputView.requestInput();
+    }
+
+    private String receiveInput() {
+        return inputView.receiveInput();
     }
 }
