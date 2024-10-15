3d5eccca0ff650cf28afdb2736c175bb049b6b99
Sumin <new.sumyang@gmail.com>
2024-10-15 18:05:05 +0900

    delete(controller): 필요 없는 기능 삭제

--- src/main/java/calculator/controller/ParsingInputString.java
+++ src/main/java/calculator/controller/ParsingInputString.java
@@ -7,24 +7,10 @@ import java.util.List;
 
 public class ParsingInputString {
 
-    private InputString inputString;
+    private final InputString inputString;
 
     public ParsingInputString(InputString inputString) {
         this.inputString = inputString;
-    }
-
-    public String setInputString(String initString) {
-        try {
-            if (initString.charAt(0) != '"'
-                || initString.charAt(initString.length() - 1) != '"') {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
-            }
-        } catch (IllegalArgumentException e) {
-            System.exit(0);
-        }
-        inputString.setInput(initString.substring(1, initString.length() - 1));
-
-        return inputString.getInput();
     }
 
     public List<Integer> parsingInput() {
