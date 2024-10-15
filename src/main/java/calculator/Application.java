64153c19d01eb44bc6ea398549af485cc674f2df
Taekgil <taekgil273@gmail.com>
2024-10-16 06:15:01 +0900

    feat: input string
    입력된 문자열 저장

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // camp.nextstep.edu.missionutils.Console의 readLine()을 사용해 문자열을 String으로 저장
+        String input = readLine();
+        // 테스트 결과 출력
+        System.out.println("input = " + input);
     }
 }
