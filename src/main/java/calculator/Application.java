c17a2be26c3447289e6e81b45421a9248fdb1b09
KORyounho <dordyd@naver.com>
2024-10-16 02:09:01 +0900

    fix(compile) empty input as is exception to be 0

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,7 +12,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = "";
-        try{
+        try {
             input = Console.readLine();
         } catch (NoSuchElementException e){
         }
@@ -21,7 +21,6 @@ public class Application {
 
         System.out.println("결과 : " + result);
     }
-
 
     private static int customProcess(String input) {
         int result = 0;
@@ -33,7 +32,7 @@ public class Application {
 
             input = input.substring(input.indexOf("\n") + 1);
         }
-        result = calc(input, result);
+        result = getResult(input, result);
 
         return result;
     }
@@ -42,9 +41,9 @@ public class Application {
         return input.startsWith("//") && input.charAt(3) == 92 && input.charAt(4) == 'n';
     }
 
-    private static int calc(String input, int result) {
+    private static int getResult(String input, int result) {
         String[] numbers = splitInput(input);
-        if (isCustom(input)) {
+        if(isCustom(input)) {
             numbers = splitInput(input.substring(5));
         }
 
@@ -86,9 +85,9 @@ public class Application {
         throw new IllegalArgumentException("잘못된 기호가 들어갔습니다.");
     }
 
-    public static int validValue(String value) {
+    public static int validValue(String value){
         int integerValue = Integer.parseInt(value);
-        if (integerValue >= 0) {
+        if(integerValue >= 0){
             return integerValue;
         }
         throw new IllegalArgumentException("음수의 값이 들어왔습니다.");
