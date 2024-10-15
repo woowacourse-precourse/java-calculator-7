eb9085ee04de1f9f55c7fb691d2efe1fa072720c
woowabrie <buyong.jo@woowahan.com>
2024-10-15 09:30:11 +0900

    feat: setup project

---
+++ src/test/java/calculator/ApplicationTest.java
@@ -0,0 +1,31 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ApplicationTest extends NsTest {
+	@Test
+	void 커스텀_구분자_사용() {
+		assertSimpleTest(() -> {
+			run("//;\\n1");
+			assertThat(output()).contains("결과 : 1");
+		});
+	}
+
+	@Test
+	void 예외_테스트() {
+		assertSimpleTest(() ->
+			assertThatThrownBy(() -> runException("-1,2,3"))
+				.isInstanceOf(IllegalArgumentException.class)
+		);
+	}
+
+	@Override
+	public void runMain() {
+		Application.main(new String[]{});
+	}
+}
