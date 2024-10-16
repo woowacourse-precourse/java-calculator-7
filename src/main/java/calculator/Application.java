971b96da60c75caf91bfe34fece795df722ce370
Jungwoo Chu <cjw980930@gmail.com>
2024-10-16 20:48:54 +0900

    feat(입력): 문자열 입력받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,11 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        //문자열 입력받기
+        String input = Console.readLine();
 
     }
 }
