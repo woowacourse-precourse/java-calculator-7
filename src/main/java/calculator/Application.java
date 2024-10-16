ddfdc2fff2d2ec65acf49ab1dbbaf913a4ecdf30
pakaheo <k70923@konkuk.ac.kr>
2024-10-16 12:14:19 +0900

    refactor : call startStringCalculator method of StringCalculatorController class

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,9 @@
 package calculator;
 
+import calculator.controller.StringCalculatorController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringCalculatorController.startStringCalculator();
     }
 }
