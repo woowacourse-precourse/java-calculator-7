7071524190eefdd1b0693fd5936e1c529e896ca3
jiffyin7 <jiffyin7@gmail.com>
2024-10-17 01:51:02 +0900

    feat(DelimiterParser): 커스텀 구분자 처리
    이슈 : #7

--- src/main/java/calculator/stringAdder/delimiterParser/DelimiterParser.java
+++ src/main/java/calculator/stringAdder/delimiterParser/DelimiterParser.java
@@ -1,12 +1,29 @@
 package calculator.stringAdder.delimiterParser;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author : jiffyin7@gmail.com
  * @since : 24. 10. 16.
  */
 public class DelimiterParser {
   private static final String[] DEFAULT_DELIMITERS = {",", ":"};
+  private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)n(.*)");
+
   public String[] getDelimiters(String input) {
+    if (input.startsWith("//")){
+      Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+      if (matcher.matches()) {
+        String[] customDelimiters = new String[]{ DEFAULT_DELIMITERS[0],
+                                                      DEFAULT_DELIMITERS[1],
+                                                      matcher.group(1)};
+        return customDelimiters;
+      }
+      else {
+        throw new IllegalArgumentException("커스텀 구분자를 찾을 수 없습니다.");
+      }
+    }
     return DEFAULT_DELIMITERS;
   }
 }
