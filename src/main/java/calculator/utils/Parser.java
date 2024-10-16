9e1ed99760acb7306ddb1a4ec775c1dd0f161ba2
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-16 17:53:22 +0900

    feat(Parser): 쉼표 또는 콜론을 구분자로 파싱한 리스트 반환하는 메서드 구현

--- src/main/java/calculator/utils/Parser.java
+++ src/main/java/calculator/utils/Parser.java
@@ -1,10 +1,25 @@
 package calculator.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parser {
     private final String userInput;
+    private final List<String> numbers = new ArrayList<>();
 
     public Parser(String userInput) {
         this.userInput = userInput;
+    }
+
+    private List<String> parseWithDefaultDelimiter() {
+        String defaultDelimiter = "[,:]";
+        String[] tokens = userInput.split(defaultDelimiter);
+
+        for (String token : tokens) {
+            numbers.add(token);
+        }
+
+        return numbers;
     }
 
     private boolean isCustomDelimiter() {
