4935e9c049cf40c79711f7b58ccec32b24cdfb00
gikhoon <ghikhoon@naver.com>
2024-10-16 18:38:46 +0900

    feat(calculator): 계산기 서비스 클래스 생성자 구현
    - 디폴티 구분자인 쉼표(,) 콜론(:)를 구분자 리스트 필드에 추가

---
+++ src/main/java/calculator/CalculatorService.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalculatorService {
+    private static final List<String> DEFAULT_SEPARATOR = List.of(":", ",");
+
+    private final List<String> separators;
+
+    public CalculatorService() {
+        separators = new ArrayList<>(DEFAULT_SEPARATOR);
+    }
+}
