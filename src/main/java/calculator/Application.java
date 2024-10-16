1f9f0bf2c3cbd99afb517696558050c956172903
songkyung <min.songkyung@gmail.com>
2024-10-16 15:01:43 +0900

    기능 목록 정리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
