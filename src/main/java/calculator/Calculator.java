db82ab87b19ec58f389b2fc339b532eb85610b7b
choign3123 <choign3123@naver.com>
2024-10-16 22:20:26 +0900

    Rename(Separation -> Separator): '구분자'의 의미를 더 잘 나타내도록 변경

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,14 +5,14 @@ import java.util.regex.Pattern;
 
 public class Calculator {
 
-    private final SeparationProcessor separationProcessor;
-    private final Pattern customSeparationCmdPattern;
-    private final int customSeparationIdx;
+    private final SeparatorProcessor separatorProcessor;
+    private final Pattern customSeparatorCmdPattern;
+    private final int customSeparatorIdx;
 
     public Calculator (){
-        this.separationProcessor = new SeparationProcessor(',', ':');
-        this.customSeparationCmdPattern = Pattern.compile("^(\\/\\/.\\\\n)");
-        this.customSeparationIdx = 2;
+        this.separatorProcessor = new SeparatorProcessor(',', ':');
+        this.customSeparatorCmdPattern = Pattern.compile("^(\\/\\/.\\\\n)");
+        this.customSeparatorIdx = 2;
     }
 
     public int sum(String cmd){
@@ -20,17 +20,17 @@ public class Calculator {
             return 0;
         }
 
-        Matcher matcher = customSeparationCmdPattern.matcher(cmd);
-        boolean containsCustomSeparationCmd = matcher.find();
-        if(containsCustomSeparationCmd){
-            registerCustomSeparation(matcher);
+        Matcher matcher = customSeparatorCmdPattern.matcher(cmd);
+        boolean containsCustomSeparatorCmd = matcher.find();
+        if(containsCustomSeparatorCmd){
+            registerCustomSeparator(matcher);
 
             // 구분자 등록에 사용됐던 명령어 제거
             cmd = cmd.replace(matcher.group(), "");
         }
 
         int result = 0;
-        String[] rawNumbers = separationProcessor.split(cmd);
+        String[] rawNumbers = separatorProcessor.split(cmd);
         for(String rawNum : rawNumbers){
             result += Integer.parseInt(rawNum);
         }
@@ -38,8 +38,8 @@ public class Calculator {
         return result;
     }
 
-    private void registerCustomSeparation(Matcher matcher){
-        Character customSeparation = matcher.group().charAt(customSeparationIdx);
-        separationProcessor.addSeparation(customSeparation);
+    private void registerCustomSeparator(Matcher matcher){
+        Character customSeparator = matcher.group().charAt(customSeparatorIdx);
+        separatorProcessor.addSeparator(customSeparator);
     }
 }
--- src/main/java/calculator/SeparatorProcessor.java
+++ src/main/java/calculator/SeparatorProcessor.java
@@ -3,14 +3,14 @@ package calculator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SeparationProcessor {
+public class SeparatorProcessor {
 
-    private final List<Character> separationList;
+    private final List<Character> separatorList;
 
-    public SeparationProcessor(Character... separations){
-        separationList = new ArrayList<>();
-        for(Character c : separations){
-            separationList.add(c);
+    public SeparatorProcessor(Character... separator){
+        separatorList = new ArrayList<>();
+        for(Character c : separator){
+            separatorList.add(c);
         }
     }
 
@@ -18,15 +18,15 @@ public class SeparationProcessor {
         return str.split(getRegex());
     }
 
-    public void addSeparation(Character c){
-        separationList.add(c);
+    public void addSeparator(Character c){
+        separatorList.add(c);
     }
 
     private String getRegex(){
         StringBuilder regex = new StringBuilder();
 
         regex.append("[");
-        for(char c : separationList){
+        for(char c : separatorList){
             regex.append(c);
         }
         regex.append("]");
