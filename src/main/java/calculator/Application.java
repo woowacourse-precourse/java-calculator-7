4c0f98ebd79453069ad99eb04eba9b30915879c4
mjk <mjk3220@ajou.ac.kr>
2024-10-16 11:57:33 +0900

    feat: calculate method
    기본 구분자 기준으로 계산을 위해 메소드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,24 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
 
+    public static int calculateValue(String user_input) {
+        String num = "";
+        int sum = 0;
+        for (int i = 0; i < user_input.length(); i++) {
+            char ch = user_input.charAt(i);
+            if (ch == ',' || ch == ':') {
+                if (num != "")
+                    sum += Integer.parseInt(num);
+                num = "";
+            } else {
+                num = num + ch;
+            }
+        }
+        if (num != "")
+            sum += Integer.parseInt(num);
+        return sum;
+    }
+
     public static String inputByUser() {
         System.out.println("덧셈할 문자열을 입력해주세요");
         return readLine();
@@ -11,5 +29,6 @@ public class Application {
 
     public static void main(String[] args) {
         String user_input = inputByUser();
+        calculateValue(user_input);
     }
 }
