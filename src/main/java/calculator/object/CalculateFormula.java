bd3c3b1709ff267b76ddbfe93559bfce968e2ce3
JeongSu Park <bechalrang@gmail.com>
2024-10-16 16:45:22 +0900

    feat(CalculateFormula.java): 입력받은 `계산식`에 커스텀 구분자 분리
    - 입력받은 `계산식`에 커스텀 구분자가 있으면 분리하는 기능을 구현
    - 해당하는 구현 기능 목록을 지움
    
    CalculateFormula.java
    README.md

--- src/main/java/calculator/object/CalculateFormula.java
+++ src/main/java/calculator/object/CalculateFormula.java
@@ -11,4 +11,23 @@ public record CalculateFormula(
     private static final char SEPARATOR_COMMA = ',';
     private static final char SEPARATOR_COLON = ':';
 
+    public static CalculateFormula from(String inputFormula) {
+        String customSeparator = findCustomSeparator(inputFormula);
+        List<BigDecimal> numbers = findNumbers(inputFormula);
+        return new CalculateFormula(customSeparator, numbers);
+    }
+
+    private static String findCustomSeparator(String inputFormula) {
+        if (!inputFormula.contains("//") || inputFormula.indexOf("\n") <= 0) {
+            return "";
+        }
+
+        String customSeparator = inputFormula.substring(0, inputFormula.indexOf("\n"));
+        if (customSeparator.length() != 1) {
+            throw new IllegalArgumentException("잘못된 커스텀 구분자가 입력되었습니다.");
+        }
+
+        return customSeparator;
+    }
+
 }
