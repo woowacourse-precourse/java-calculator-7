3a03c349d3effbe23353d0586d65e58562774702
Byuntil <gugugu2014@jbnu.ac.kr>
2024-10-16 17:57:56 +0900

    feat: 문자열에서 구분자 추출 기능
    입력으로 들어온 문자에서 구분자를 추출 해옵니다. 커스텀 구분자의 경우 //과 \n 사이에 있는 구분자를 추출합니다.

---
+++ src/main/java/calculator/IntegersExtractor.java
@@ -0,0 +1,38 @@
+package calculator;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class IntegersExtractor {
+
+    public static final String NORMAL_DELIMITER = "[,:]";
+
+    public List<Integer> extractIntegers(String inputValue) {
+        String delimiter = getDelimiter(inputValue);
+        return Collections.emptyList();
+    }
+
+    private String getDelimiter(String inputValue) {
+        int firstCustomDelimiterIndex = inputValue.indexOf("//");
+        int secondCustomDelimiterIndex = inputValue.indexOf("\\n");
+
+        if (isNotCustomDelimiter(firstCustomDelimiterIndex)) {
+            return NORMAL_DELIMITER;
+        }
+
+        if (firstCustomDelimiterIndex >= secondCustomDelimiterIndex) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 커스텀 구분자는 //와 \\n 사이에 위치해야 합니다.");
+        }
+
+        return getCustomDelimiter(inputValue, firstCustomDelimiterIndex, secondCustomDelimiterIndex);
+    }
+
+    private boolean isNotCustomDelimiter(int firstCustomDelimiterIndex) {
+        return firstCustomDelimiterIndex == -1;
+    }
+
+    private String getCustomDelimiter(String inputValue, int firstCustomDelimiter, int secondCustomDelimiter) {
+        return Pattern.quote(inputValue.substring(firstCustomDelimiter + 2, secondCustomDelimiter));
+    }
+}
