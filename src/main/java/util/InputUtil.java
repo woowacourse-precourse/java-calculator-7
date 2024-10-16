216931be7a60f117a77a71c9d9e0b68ec285e592
dltmddus <tmddus4671@gmail.com>
2024-10-16 22:33:11 +0900

    feat: 출력 Message 유틸 구현

--- src/main/java/util/InputUtil.java
+++ src/main/java/util/InputUtil.java
@@ -1,3 +1,7 @@
+package util;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputUtil {
     public String userInput(){
         return Console.readLine();
---
+++ src/main/java/util/MessageUtil.java
@@ -0,0 +1,10 @@
+package util;
+
+public class MessageUtil {
+    public void printStringForSum() {
+        System.out.println("덧셈할 문자열을 입력해 주세요. ");
+    }
+    public void printResult() {
+        System.out.print("결과 : ");
+    }
+}
