4ad8d15f5e13e46cbb5ef555f7648aead6b68e15
jijongkwon <help3451@naver.com>
2024-10-16 21:33:01 +0900

    feat(service): InputService 클래스 기본 구조 추가
    - getValidInput: 사용자 입력 받기 및 검증
    - validateInput: 입력값 검증
    - validateCustomDelimiter: 커스텀 구분자 검증
    
    TODO: 각 메서드에 실제 입력 처리 및 검증 로직 구현

---
+++ src/main/java/calculator/service/InputService.java
@@ -0,0 +1,17 @@
+package calculator.service;
+
+public class InputService {
+
+    public String getValidInput() {
+        //TODO: 사용자 입력 받기 및 검증 로직 구현
+        return null;
+    }
+
+    private void validateInput(String input) {
+        //TODO: 입력값 검증 로직 구현
+    }
+
+    private void validateCustomDelimiter(String input) {
+        //TODO: 커스텀 구분자 검증 로직 구현
+    }
+}
