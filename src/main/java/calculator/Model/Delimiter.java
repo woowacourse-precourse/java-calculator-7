2d76c407b247b8aeeb4980157a972d5e1445910f
yush <yush2202@gmail.com>
2024-10-16 20:50:20 +0900

    feat($Delimiter): 구분자 예외 사항 추가
    구분자를 공백, 숫자, 비어있음을 불가능하게 하고 예외 메시지를 출력

--- src/main/java/calculator/Model/Delimiter.java
+++ src/main/java/calculator/Model/Delimiter.java
@@ -1,5 +1,9 @@
 package calculator.Model;
 
+import static calculator.Constants.ErrorMessages.INVALID_CUSTOM_SEPARATOR_BLANK_MESSAGE;
+import static calculator.Constants.ErrorMessages.INVALID_CUSTOM_SEPARATOR_DIGIT_MESSAGE;
+import static calculator.Constants.ErrorMessages.INVALID_CUSTOM_SEPARATOR_EMPTY_MESSAGE;
+
 public class Delimiter {
     private final String delimiter;
 
@@ -15,17 +19,24 @@ public class Delimiter {
     private void validateDelimiter(String delimiter) {
         isNull(delimiter);
         isDigit(delimiter);
+        isBlank(delimiter);
     }
 
     private void isNull(String delimiter) {
         if (delimiter.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_CUSTOM_SEPARATOR_EMPTY_MESSAGE.getErrorMessage());
+        }
+    }
+
+    private void isBlank(String delimiter) {
+        if (delimiter.isBlank()) {
+            throw new IllegalArgumentException(INVALID_CUSTOM_SEPARATOR_BLANK_MESSAGE.getErrorMessage());
         }
     }
 
     private void isDigit(String delimiter) {
         if (delimiter.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_CUSTOM_SEPARATOR_DIGIT_MESSAGE.getErrorMessage());
         }
     }
 }
