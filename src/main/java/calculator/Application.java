510b26c708dfaa7fff3cf9f07c18058152dd1024
param <spsummerdoc@gmail.com>
2024-10-16 12:19:23 +0900

    feat(application): main으로 Calculator 입력 및 출력을 할 수 있다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import calculator.io.InputView;
+import calculator.io.OutputView;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            String inputString = InputView.printInputAddString();
+            List<String> digits = Division.split(inputString);
+            result(digits);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
+
+    private static void result(List<String> digits) {
+        int sum = Calculator.sumPositiveNumbers(digits);
+        OutputView.printResult(sum);
     }
 }
---
+++ src/main/java/calculator/io/OutputView.java
@@ -0,0 +1,7 @@
+package calculator.io;
+
+public class OutputView {
+    public static void printResult(int sum) {
+        System.out.println("결과 : "+ sum );
+    }
+}
