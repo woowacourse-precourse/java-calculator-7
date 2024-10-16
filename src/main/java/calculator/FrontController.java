1a3475de140aa69e61d839ab5da63df63f5192f4
haebyun <bhk4392@gmail.com>
2024-10-16 22:58:11 +0900

    refactor(FrontController): CalculatorController를 생성자 인자로 받도록 변경
    - 직접 CalculatorController와 CalculatorService 인스턴스를 생성하는 대신 의존성 주입 방식으로 수정
    - config 패키지에서 의존성을 주입할 수 있도록 생성자와 클래스 선언부에 public 접근 제한자 추가

--- src/main/java/calculator/FrontController.java
+++ src/main/java/calculator/FrontController.java
@@ -1,16 +1,16 @@
 package calculator;
 
+import calculator.config.BeanConfig;
 import calculator.controller.CalculatorController;
 import calculator.dto.CalculationResultDTO;
-import calculator.service.CalculatorService;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
-final class FrontController {
+public final class FrontController {
     private final CalculatorController calculatorController;
 
-    FrontController() {
-        this.calculatorController = new CalculatorController(new CalculatorService());
+    public FrontController() {
+        this.calculatorController = BeanConfig.getBean(CalculatorController.class);
     }
 
     void run() {
