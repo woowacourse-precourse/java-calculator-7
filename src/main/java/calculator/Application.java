411416e28195a079d788fb913348f2a5ae6f7657
touhou09 <yuseungju@yuseungjuui-MacBookAir.local>
2024-10-16 18:07:32 +0900

    refactor($Application): 입력 기능 수정
    $Application 입력 수정:
    - Scanner 함수가 아닌 문제 조건대로 Console.readLine()을 사용하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,6 @@
 package calculator;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,14 +9,9 @@ public class Application {
         // 입력
         // 문자열 str 입력받음
         System.out.println("덧셈할 문자열을 입력해 주세요.\n");
-
-        Scanner scanner = new Scanner(System.in);
-
-        String str;
-        str = scanner.nextLine();
+        String str = Console.readLine();
 
         // 커스텀 구분자 찾기
-
 
         // 잘못된 문자열인지 오류 탐색 or 합 출력
     }
