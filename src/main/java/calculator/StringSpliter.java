0a80397b3d320a32ac331664c74627b9499d8129
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-10-16 17:31:33 +0900

    feat(StringSpliter): 구분자 ,, :를 인식하고 분리된 문자열을 반환해주는 로직 구현

--- src/main/java/calculator/StringSpliter.java
+++ src/main/java/calculator/StringSpliter.java
@@ -1,4 +1,12 @@
 package calculator;
 
 public class StringSpliter { //TODO: 구분자를 인식하고, 구분자로 분리된 문자열 반환
+    public static String[] split(String input) {
+        String spliter = "[,:]";
+        String[] splitedString = input.split(spliter);
+        for (String s : splitedString) {
+            System.out.print(s + " ");
+        }
+        return splitedString;
+    }
 }
