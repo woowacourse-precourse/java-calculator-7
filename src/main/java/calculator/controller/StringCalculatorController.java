bbf5d11fb425eb898985678db71ff0d73cc3e1fb
jijongkwon <help3451@naver.com>
2024-10-17 00:45:57 +0900

    feat(controller): StringCalculatorController 클래스에 전체 실행 로직 구현
    - 입력 서비스에서 유효한 입력 문자열 받기
    - 파싱 서비스에서 숫자 배열로 변환
    - 계산 서비스에서 숫자 배열 합 계산
    - 출력 서비스에서 계산 결과 출력

--- src/main/java/calculator/controller/StringCalculatorController.java
+++ src/main/java/calculator/controller/StringCalculatorController.java
@@ -1,8 +1,28 @@
 package calculator.controller;
 
+import calculator.service.CalculationService;
+import calculator.service.InputService;
+import calculator.service.ParsingService;
+import calculator.view.OutputView;
+
 public class StringCalculatorController {
+
+    private final InputService inputService;
+    private final CalculationService calculationService;
+    private final ParsingService parsingService;
+    private final OutputView outputView;
+
+    public StringCalculatorController(InputService inputService, CalculationService calculationService, ParsingService parsingService, OutputView outputView) {
+        this.inputService = inputService;
+        this.calculationService = calculationService;
+        this.parsingService = parsingService;
+        this.outputView = outputView;
+    }
 
     public void run() {
         //TODO: 전체 흐름 제어 로직 구현
+        String validInput = inputService.getValidInput();
+        int[] numbers = parsingService.parseNumbers(validInput);
+        outputView.printResult(calculationService.calculate(numbers));
     }
 }
