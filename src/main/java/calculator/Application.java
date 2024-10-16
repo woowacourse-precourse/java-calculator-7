0572d7943d090d8b198ecc7669c97b30c5be5563
joonwan <joonwan1024@naver.com>
2024-10-16 16:23:04 +0900

    feat: isCustom 메서드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,11 +6,16 @@ public class Application {
 
     public static void main(String[] args) {
         String inputData = getInputData();
+        boolean customStatus = isCustom(inputData);
     }
 
     private static String getInputData() {
         String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
         System.out.println(INPUT_MESSAGE);
         return Console.readLine();
+    }
+
+    private static boolean isCustom(String inputData) {
+        return inputData.startsWith("//");
     }
 }
