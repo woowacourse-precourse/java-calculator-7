6c202fdceec30b0c8d0b2e7d74da0ba76a915bcc
heejoo <heeju0203@hanyang.ac.kr>
2024-10-16 18:16:08 +0900

    feat(calculator): output handling and enhance user interaction
    -Add prompt for user input and final result output
    
    -Change error messages from English to Korean

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
             String str = Console.readLine();
 
             String[] parts = str.split("\\\\n");
@@ -25,11 +26,13 @@ public class Application {
             for (int i = 0; i < number.length; i++) {
                 int value = Integer.parseInt(number[i]);
 
-                if(value <= 0) throw new IllegalArgumentException("Invalid input value!");
+                if(value <= 0) throw new IllegalArgumentException("잘못된 입력값입니다!");
 
                 num[i] = value;
                 sum += num[i];
             }
+
+            System.out.println("결과 : " + sum);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
