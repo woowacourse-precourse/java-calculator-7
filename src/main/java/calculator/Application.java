4c7155856008b68631d9a68d67e438fc6dcf0b7b
mjk <mjk3220@ajou.ac.kr>
2024-10-16 11:25:27 +0900

    feat: make input method
    유저 인풋을 받기 위한 메소드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,7 +3,13 @@ package calculator;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
+
+    public static String inputByUser() {
+        System.out.println("덧셈할 문자열을 입력해주세요");
+        return readLine();
+    }
+
     public static void main(String[] args) {
-        
+        String user_input = inputByUser();
     }
 }
