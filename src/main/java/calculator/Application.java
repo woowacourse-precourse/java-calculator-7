3f2c6d32d9dea15437c6291cd922f7e8c35cf092
bk <naturalbb360@gmail.com>
2024-10-16 15:21:05 +0900

    feat(Application): 사용자 입력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,19 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
+
+    public static void getUserInput() {
+        String userInput;
+        Scanner scanner = new Scanner(System.in);
+        userInput = scanner.nextLine();
+        System.out.println(userInput);
+    }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해주세요");
-
+        getUserInput();
     }
 }
