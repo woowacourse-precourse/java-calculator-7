0e84ce17f60ddca60e68e8110295113a4d61963f
inswal843 <inswal843@gmail.com>
2024-10-16 19:06:42 +0900

    test : 기본적인 입력과 예외상황 테스트 코드 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,15 +1,39 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
     @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용_숫자_하나_입력() {
+        assertSimpleTest(() -> {
+            run("1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_숫자_하나_입력() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
@@ -17,10 +41,47 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 음수_입력_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("음수는 입력할 수 없습니다.")
+        );
+    }
+
+//    @Test
+//    void 빈_문자열_입력_테스트() {
+//        assertSimpleTest(() ->
+//                assertThatThrownBy(() -> runException(""))
+//                        .isInstanceOf(IllegalArgumentException.class)
+//                        .hasMessageContaining("빈 문자열은 입력할 수 없습니다.")
+//        );
+//    }
+
+    @Test
+    void 연속된_구분자_입력_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,,2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("구분자를 연속으로 입력할 수 없습니다.")
+        );
+    }
+
+    @Test
+    void 숫자가_아닌_값_입력_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,a,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("계산식에는 숫자만 입력할 수 있습니다.")
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_비어있음_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("커스텀 구분자는 비어 있을 수 없습니다.")
         );
     }
 
