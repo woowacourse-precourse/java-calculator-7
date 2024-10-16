536b4ad3112b5b4aecb9f039f22bd8ed011100f6
immyeong <wpfls6670@naver.com>
2024-10-16 18:40:07 +0900

    feat: 잘못된 입력값 예외 처리

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -8,6 +8,7 @@ public class Calculator {
     private final InputView inputView;
     private static final Integer PREFIX_DIVISION_INDEX = 2;
     private static final Integer NEXT_DIVISION_INDEX = 3;
+    private static final Integer START_NUMBER_INDEX = 5;
 
     public Calculator(InputView inputView) {
         this.inputView = inputView;
@@ -20,6 +21,7 @@ public class Calculator {
         if(input.equals("")) return 0;
 
         String customDivision = extractDivision(input);
+        checkInputString(input, customDivision);
 
         return num;
     }
@@ -29,5 +31,37 @@ public class Calculator {
             return input.substring(PREFIX_DIVISION_INDEX, NEXT_DIVISION_INDEX);
         }
         return "";
+    }
+
+    public void checkInputString(String input, String customDivision) {
+        if (customDivision.isEmpty()) {
+            if (checkOtherDivision(input, customDivision)) {
+                throw new IllegalArgumentException("기본 구분자 외 구분자가 포함되어있습니다.");
+            }
+        } else {
+            if (input.substring(PREFIX_DIVISION_INDEX, NEXT_DIVISION_INDEX + 1).equals("\\n")) {
+                throw new IllegalArgumentException("커스텀 구분자가 누락되었습니다.");
+            } else if (checkOtherDivision(input, customDivision)) {
+                throw new IllegalArgumentException("커스텀 구분자 외 구분자가 포함되어있습니다.");
+            }
+        }
+    }
+
+    public boolean checkOtherDivision(String input, String customDivision) {
+        if (customDivision.isEmpty()) {
+            for (char ch : input.toCharArray()) {
+                if (!Character.isDigit(ch) && ch != ';' && ch != ':') {
+                    return true;
+                }
+            }
+        } else {
+            input = input.substring(START_NUMBER_INDEX);
+            for (char ch : input.toCharArray()) {
+                if (!Character.isDigit(ch) && customDivision.charAt(0) != ch) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
