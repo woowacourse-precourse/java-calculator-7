ad7dcac138383288c1b1cd32923bbe9f8ea453f4
jijongkwon <help3451@naver.com>
2024-10-16 23:20:57 +0900

    test(service): InputService 클래스에 대한 단위 테스트 구현
    - 유효한 사용자 입력 반환 확인 (1,2:3)
    - 빈 문자열 입력 시 InvalidInputException 발생 확인
    - 유효한 커스텀 구분자 입력 처리 확인 (//;\\n1;2;3)
    - 유효하지 않은 커스텀 구분자 입력 시 InvalidInputException 발생 확인 (//\\n1,2,3)
    - 커스텀 구분자 뒤에 \\n이 없을 경우 InvalidInputException 발생 확인 (//;1;2;3)

---
+++ src/test/java/calculator/service/InputServiceTest.java
@@ -0,0 +1,76 @@
+package calculator.service;
+
+import calculator.exception.InvalidInputException;
+import calculator.view.InputView;
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class InputServiceTest {
+
+    private InputService inputService;
+
+    @BeforeEach
+    void setUp() {
+        inputService = new InputService(new InputView());
+    }
+
+    @AfterEach
+    void tearDown() {
+        Console.close();
+    }
+
+    private void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    @Test
+    void 유효한_입력값_반환() {
+        String input = "1,2:3";
+        provideInput(input);
+
+        String result = inputService.getValidInput();
+
+        assertEquals(input, result);
+    }
+
+    @Test
+    void 빈_문자열_입력시_예외_발생() {
+        provideInput("");
+
+        assertThrows(InvalidInputException.class, () -> inputService.getValidInput());
+    }
+
+    @Test
+    void 유효한_커스텀_구분자_입력_처리() {
+        String input = "//;\\n1;2;3";
+        provideInput(input);
+
+        String result = inputService.getValidInput();
+
+        assertEquals(input, result);
+    }
+
+    @Test
+    void 유효하지_않은_커스텀_구분자_예외_발생() {
+        String input = "//\\n1,2,3";
+        provideInput(input);
+
+        assertThrows(InvalidInputException.class, () -> inputService.getValidInput());
+    }
+
+    @Test
+    void 커스텀_구분자_뉴라인_누락시_예외_발생() {
+        String input = "//;1;2;3";
+        provideInput(input);
+
+        assertThrows(InvalidInputException.class, () -> inputService.getValidInput());
+    }
+}
