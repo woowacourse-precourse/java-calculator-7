a22efa8909ed2f3b7730fbe6652b1c717e020d56
praisebak <praisebak@naver.com>
2024-10-16 15:31:56 +0900

    fix : Scanner 종료 메서드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,13 @@
 package calculator;
 
 import calculator.calculator.AddNumCalculator;
+import camp.nextstep.edu.missionutils.Console;
+
 
 public class Application {
     public static void main(String[] args) {
         AddNumCalculator addNumCalculator = new AddNumCalculator();
         addNumCalculator.calculator();
+        Console.close();
     }
 }
