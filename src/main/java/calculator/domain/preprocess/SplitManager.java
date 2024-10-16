f7d4a00b5569f437da7e345ec376affd419db9d4
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-17 00:34:45 +0900

    feat(SplitManager): extractNumbers 기능 구현

--- src/main/java/calculator/domain/preprocess/SplitManager.java
+++ src/main/java/calculator/domain/preprocess/SplitManager.java
@@ -1,13 +1,22 @@
 package calculator.domain.preprocess;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SplitManager {
 
-    public List<String> discardBlankSpace(List<String> input){
+    public List<String> discardBlankSpace(List<String> input) {
         input.removeIf(String::isBlank);
         return input;
     }
 
+    public List<String> extractNumbers(List<String> delimeterList, String input){
+
+        String regularExpression = "["+String.join("",delimeterList)+"]";
+        String[] extractedNumbers = input.split(regularExpression);
+        List<String> extractedNumbersList = new ArrayList<>(Arrays.asList(extractedNumbers));
+        return  discardBlankSpace(extractedNumbersList);
+    }
 
 }
