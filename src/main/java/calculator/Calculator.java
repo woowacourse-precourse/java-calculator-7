b299a80b4713d0679bb769c662f06139b5ad3c52
ahpicl64 <ahpicl64@gmail.com>
2024-10-17 02:57:25 +0900

    feat: 사용자 입력창 구현 및 구분자 검증로직 일부 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Calculator {
+
+    public Calculator() {
+        System.out.println("숫자를 입력해 주세요.\nH : 도움말, X : 프로그램 종료, 기본 구분자 : ',' 또는 ':'");
+    }
+    
+    private void Scanner() {
+        Console.readLine();
+    }
+}
