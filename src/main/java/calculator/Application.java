35588ebd8b88fbd7da3533d79515f0413ebde3be
eunho <eunho1032@gmail.com>
2024-10-16 11:54:45 +0900

    feat(Application): add user input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,11 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("덧셈할 문자열을 입력해 주세요.");
+		String input = scanner.nextLine();
 	}
 }
