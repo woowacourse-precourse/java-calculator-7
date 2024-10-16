0a676eb647fb1c60d7a26f5d515e99671b89fd66
yxoni <psy337250@gmail.com>
2024-10-16 22:12:31 +0900

    feat: 커스텀 구분자 지정 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,6 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.StringTokenizer;
 
 public class Application {
@@ -10,7 +9,14 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        StringTokenizer st = new StringTokenizer(input, ",:");
+        String str = "";
+        if(input.charAt(0) == '/') {
+            str = String.valueOf(input.charAt(2));
+        }
+
+        StringTokenizer st;
+        st = new StringTokenizer(input.substring(5),",:"+str);
+        
         int sum = 0;
         while(st.hasMoreTokens()) {
             sum += Integer.parseInt(st.nextToken());
