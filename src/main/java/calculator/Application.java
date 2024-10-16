0f197d15b84adc5179196414d1233d8519a61b76
soeun2537 <soeun2537@gmail.com>
2024-10-17 03:46:24 +0900

    feat: 빈 입력 처리 로직 추가 (빈 문자열, 엔터, 탭, null)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        int sum = 0;
+
+        while (true) {
+            // 빈 문자열, 엔터, 탭, null 입력
+            if (input.isBlank() || input == null) {
+                System.out.println("결과 : " + sum);
+                break;
+            }
+        }
     }
 }
