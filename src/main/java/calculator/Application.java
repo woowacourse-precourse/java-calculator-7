a59d6ad7364571403f1deaa92f7435b9a25e2e61
조수아 <suacho0724@gmail.com>
2024-10-16 16:34:12 +0900

    feat: 구분자 클래스 생성 및 커스텀 구분자 추가 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,17 @@
 package calculator;
 
+import calculator.delimiter.Delimiter;
+import calculator.inputhandler.InputHandler;
+
 public class Application {
     public static void main(String[] args) {
+        InputHandler inputHandler = new InputHandler();
+        String input = inputHandler.getInput("덧셈할 문자열을 입력해주세요.\n");
+
+        Delimiter delimiter = new Delimiter();
+        if (input.startsWith("//")) {
+            input = delimiter.addCustomDelimiter(input);
+        }
     }
+
 }
---
+++ src/main/java/calculator/delimiter/Delimiter.java
@@ -0,0 +1,52 @@
+package calculator.delimiter;
+
+public class Delimiter {
+    private String del;
+    private static final int CUSTOM_DEL_INDEX = 2;
+    private static final String DEFAULT_DELIMITER = "[,:]";
+
+    public Delimiter() {
+        this.del = DEFAULT_DELIMITER;
+    }
+
+    public String getDel() {
+        return del;
+    }
+
+    public String addCustomDelimiter(String input) {
+        String customDelHeader = getCustomDelHeader(input);
+        validateInputHeader(customDelHeader);
+
+        char customDel = customDelHeader.charAt(CUSTOM_DEL_INDEX);
+        validateCustomDel(customDel);
+
+        this.del = this.del.substring(0, 3) + customDel + "]";
+        return input.length() == 5 ? "" : input.substring(5);
+    }
+
+    private String getCustomDelHeader(String input) {
+        if (input.length() < 5) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+        return input.substring(0, 5);
+    }
+
+    private void validateInputHeader(String inputHeader) {
+        if (!inputHeader.startsWith("//")) {
+            throw new IllegalArgumentException("커스텀 구분자 설정은 // 로 시작되어야합니다.");
+        }
+        if (!inputHeader.endsWith("\\n")) {
+            throw new IllegalArgumentException("커스텀 구분자 설정은 \\n 으로 끝나야합니다.");
+        }
+    }
+
+    private void validateCustomDel(char customDel) {
+        if (isDefaultDel(customDel)) {
+            throw new IllegalArgumentException("기본 구분자와 중복되는 커스텀 구분자입니다.");
+        }
+    }
+
+    private boolean isDefaultDel(char customDel) {
+        return customDel == ':' || customDel == ',';
+    }
+}
