ebc678823a3f73e726114ead816fde01ab7a7f78
Seonjeongg <sj030205@naver.com>
2024-10-16 09:40:25 +0900

    feat: 문자열 입력 받는 기능 구현

--- src/main/java/calculator/View/InputView.java
+++ src/main/java/calculator/View/InputView.java
@@ -1,4 +1,12 @@
 package calculator.View;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String ASK_SUM_STRING="덧셈할 문자열을 입력해 주세요.";
+
+    public static String ask_sum_string(){
+        System.out.print(ASK_SUM_STRING);
+        return Console.readLine();
+    }
 }
