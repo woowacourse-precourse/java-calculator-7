2c22c0a9a5d970ca8e545e958368a144bc368e31
qudcks8084 <qudcks8084@naver.com>
2024-10-16 13:08:14 +0900

    feat(model) : create model package and add Calculator Class
    - Add model package and Calculator class to calculate user's input
    - Implement two private parameter and Constructor

---
+++ src/main/java/calculator/model/Calculator.java
@@ -0,0 +1,11 @@
+package calculator.model;
+
+public class Calculator {
+    private String str;
+    private String delim;
+
+    public Calculator(String str, String delim) {
+        this.str = str;
+        this.delim = delim;
+    }
+}
