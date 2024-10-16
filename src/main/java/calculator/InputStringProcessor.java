2151633a996455d2b0abd2a4a95699b2558f4e5a
soeunnPark <carol0724@naver.com>
2024-10-16 22:16:41 +0900

    feat(removeFirstFiveCharacters): remove the first 5 characters from a string
    feature #2.1.
    2.1. 커스텀 구분자가 있는지 확인하고, 커스텀 구분자가 있는 첫 5글자를 검증한다.
    - [x] 길이가 5보다 큰 문자열의 첫 5글자를 구한다.
    
    Closes "issue: 길이가 5보다 큰 문자열의 첫 5글자를 구한다."

--- src/main/java/calculator/InputStringProcessor.java
+++ src/main/java/calculator/InputStringProcessor.java
@@ -2,9 +2,14 @@ package calculator;
 
 public class InputStringProcessor {
 
-	private static final int BEGIN_INDEX = 4;
+	private static final int BEGIN_INDEX = 0;
+	private static final int SEPARATOR_CRITERIA = 4;
 
-	public String removeFirstFiveCharacters(String strWithMinFiveChars) {
-		return strWithMinFiveChars.substring(BEGIN_INDEX);
+	public String removeFirstFiveCharacters(String strWithCustomSeparator) {
+		return strWithCustomSeparator.substring(SEPARATOR_CRITERIA);
+	}
+	
+	public String getFirstFiveCharacters(String strWithMinFiveChars) {
+		return strWithMinFiveChars.substring(BEGIN_INDEX, SEPARATOR_CRITERIA);
 	}
 }
