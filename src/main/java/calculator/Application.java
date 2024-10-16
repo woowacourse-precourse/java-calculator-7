67d01a8ac566a2b59e5859bdfe186754d27fe57b
5jun99 <oj041088@gmail.com>
2024-10-16 22:33:29 +0900

    feat(input): store numbers in array

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,11 @@
 package calculator;
 
+import calculator.io.ConsoleInputHandler;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator(new ConsoleInputHandler());
         calculator.run();
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,20 @@
 package calculator;
 
-import calculator.io.ConsoleInputHandler;
 import calculator.io.InputHandler;
 
 public class Calculator {
-    private final InputHandler inputHandler = new ConsoleInputHandler();
+    private final InputHandler inputHandler;
+
+    public Calculator(InputHandler inputHandler) {
+        this.inputHandler = inputHandler;
+    }
+
     public void run() {
         String userInput = inputHandler.getUserInput();
+        String[] inputNumbers = splitUserInput(userInput, "[,;]");
+    }
+
+    private String[] splitUserInput(String userInput, String delimiter) {
+        return userInput.split(delimiter);
     }
 }
