fcb9420ce12da13b4e220dd44f0b5c580d59e7af
qudcks8084 <qudcks8084@naver.com>
2024-10-16 13:21:09 +0900

    feat(Application) : add controller instance and add run method call
    - add CalcController instance to run main workflow

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.controller.CalcController;
+
 public class Application {
     public static void main(String[] args) {
+        CalcController calcController = new CalcController();
+        calcController.run();
         // TODO: 프로그램 구현
     }
 }
