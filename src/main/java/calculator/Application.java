99ea34468f5e75364c4534c7dd694dccd0e87d79
dawit0905 <dawit0905@gmail.com>
2024-10-16 18:40:27 +0900

    feat: 문자열을 입력받고, 빈 문자열이 들어온 경우 0으로 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = readLine();
+        if(!isEmpty(input)){
+
+        } else {
+            System.out.println("결과 : 0");
+        }
+    }
+
+    public static boolean isEmpty(String str){
+        return str.length() == 0;
     }
 }
