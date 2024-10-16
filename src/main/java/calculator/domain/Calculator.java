8e73c1331e7ed924d267862e0944e05dd382e3e3
immyeong <wpfls6670@naver.com>
2024-10-16 18:39:00 +0900

    feat: 커스텀 구분자 추출 함수 구현

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -6,6 +6,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class Calculator {
 
     private final InputView inputView;
+    private static final Integer PREFIX_DIVISION_INDEX = 2;
+    private static final Integer NEXT_DIVISION_INDEX = 3;
 
     public Calculator(InputView inputView) {
         this.inputView = inputView;
@@ -17,7 +19,15 @@ public class Calculator {
         String input = Console.readLine();
         if(input.equals("")) return 0;
 
+        String customDivision = extractDivision(input);
+
         return num;
     }
 
+    public String extractDivision(String input) {
+        if (input.startsWith("/")) {
+            return input.substring(PREFIX_DIVISION_INDEX, NEXT_DIVISION_INDEX);
+        }
+        return "";
+    }
 }
