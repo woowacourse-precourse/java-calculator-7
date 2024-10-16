ee03177f2f70587e63653f82e98044960ab57546
yunsseong <aster20@hanyang.ac.kr>
2024-10-16 22:46:54 +0900

    feat: 기분 구분자 형식 검증 기능 구현

--- src/main/java/calculator/service/DelimiterService.java
+++ src/main/java/calculator/service/DelimiterService.java
@@ -9,6 +9,7 @@ public class DelimiterService {
 
     private List<Delimiter> delimiters = new ArrayList<>();
     private final Pattern customDelimiterPattern = Pattern.compile("^//.\\n.*");
+    private final Pattern defaultDelimiterPattern = Pattern.compile("[0-9:,]*]");
 
     public void addDelimiter(String delimiter) {
         delimiters.add(new Delimiter(delimiter));
@@ -19,5 +20,9 @@ public class DelimiterService {
     }
     public boolean validateCustomDelimiterFormat(String string) {
         return customDelimiterPattern.matcher(string).matches();
+    }
+
+    public boolean validateDefaultDelimiterFormat(String string) {
+        return defaultDelimiterPattern.matcher(string).matches();
     }
 }
