49743d9e4b5946ccbca8856ba43e9ac777ff5d12
hyojun <hjlim45678@gmail.com>
2024-10-16 11:57:27 +0900

    feat: 기본 구분자를 통해 숫자를 구분하는 기능구현

--- src/main/java/calculator/MainController.java
+++ src/main/java/calculator/MainController.java
@@ -1,11 +1,22 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainController {
 
     public static void run() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputString = Console.readLine();
+
+        // 1. 기본 구분자로 분리
+        List<String> colonOrCommaSplited = Arrays.asList(inputString.split(",|:"));
+        //1-1. 출력해서 확인
+        for (String part : colonOrCommaSplited) {
+            System.out.println(part);
+        }
+
     }
 }
