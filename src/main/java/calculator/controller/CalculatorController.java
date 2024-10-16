511a8eedfb12289dd8980eb5b17a5e7722e41563
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-16 23:25:15 +0900

    feat(SeparatorParser): 구분자 파싱 기능 구현
    - 구분자에 기본 구분자를 추가한다.
    - 커스텀 구분자가 있다면, 파싱하여 추가한다.
    - 구분자와 계산해야될 문자열을 반환한다.

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,10 +1,14 @@
 package calculator.controller;
 
+import calculator.model.SeparatorParser;
 import calculator.view.InputView;
 
 public class CalculatorController {
     public void calculatorRun() {
         InputView inputView = new InputView();
         String inputString = inputView.readString();
+
+        SeparatorParser separatorParser = new SeparatorParser();
+        String separator = separatorParser.separatorParse(inputString);
      }
 }
---
+++ src/main/java/calculator/model/SeparatorParser.java
@@ -0,0 +1,34 @@
+package calculator.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SeparatorParser {
+    private final String SEPARATOR_FORMAT_STRING = "//|\\\\n";
+    private final String BASIC_SEPERATOR = "\\,|\\:";
+
+    public String separatorParse(String input) {
+        List<String> inputSeparatorList = Arrays.stream(input.split(SEPARATOR_FORMAT_STRING))
+                .map(String::trim)
+                .filter(s -> (!s.isEmpty()))
+                .toList();
+
+        String separator = "";
+        separator += BASIC_SEPERATOR;
+
+        // validator 과정
+        for (int inputSeparatorListIndex = 0; inputSeparatorListIndex < inputSeparatorList.size() - 1;
+             inputSeparatorListIndex++) {
+            String inputSeparator = inputSeparatorList.get(inputSeparatorListIndex);
+            int inputSeparatorASCIIIndex = (int) inputSeparator.charAt(0);
+            if ((inputSeparatorASCIIIndex >= 65 && inputSeparatorASCIIIndex <= 90) || (
+                    inputSeparatorASCIIIndex >= 97 && inputSeparatorASCIIIndex <= 122)) {
+                separator += ("|" + inputSeparator);
+            } else if ((inputSeparatorASCIIIndex >= 33 && inputSeparatorASCIIIndex <= 47)
+                    || inputSeparatorASCIIIndex >= 58) {
+                separator += ("|\\" + inputSeparator);
+            }
+        }
+        return separator;
+    }
+}
