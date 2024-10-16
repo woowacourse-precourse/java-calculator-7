1867582ebe99e5def979025243473db4c5224ac0
pakaheo <k70923@konkuk.ac.kr>
2024-10-16 12:10:52 +0900

    feat : create Constants final class and define global constants

--- src/main/java/calculator/constants/Constants.java
+++ src/main/java/calculator/constants/Constants.java
@@ -1,2 +1,16 @@
-package calculator.constants;public class Constants {
+package calculator.constants;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String COMMA = ",";
+    public static final String COLON = ":";
+    public static final int ZERO = 0;
+    public static final String WRONG_DELIMITER = "잘못된 구분자입니다";
+    public static final String NOT_EXIST_CUSTOM = "커스텀 구분자가 없습니다";
+    public static final String NOT_DEFINED_CUSTOM = "문자열에 정의되지 않은 커스텀 구분자가 있습니다";
+    public static final String NOT_CONTAINS_NUMBER = "숫자와 함께 입력해주세요";
+    public static final String CONTAINS_NEGATIVE = "문자열에 음수가 포함되어있습니다";
 }
