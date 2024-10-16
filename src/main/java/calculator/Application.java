72c569fe1bed572903236fe5269d9637e7716767
binn <anyubin0724@naver.com>
2024-10-16 10:10:37 +0900

    feat : isValidPositive 함수 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -13,4 +13,9 @@ public class Application {
         // TODO: 예외 처리 메소드 호출 후 에러 메세지 표시
         return readLine();
     }
+
+    private static boolean isValidPositive(String input) {
+        // TODO: 음수 or 빈 문자열 or null이면 false
+        return true;
+    }
 }
