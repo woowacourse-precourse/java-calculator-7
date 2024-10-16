c534f450d83950e8ce6c6dbbb66257d27d1e7042
Lys <redspinyant@gmail.com>
2024-10-16 09:42:48 +0900

    feat: 예외 처리
    - 음수나 잘못된 입력값이 들어왔을 때 `IllegalArgumentException`으로 처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,9 +9,12 @@ public class Calculator {
 	private static final String DEFAULT_SEPARATOR = "[,:]";
 	private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("//(.*)\n(.*)");
 
+	private static final String EMPTY_ERROR_MSG = "값을 입력해주세요.";
+	private static final String NEGATIVE_ERROR_MSG = "음수는 허용되지않습니다.";
+
 	public int sum(String input) {
 		if (input == null || input.isEmpty()) {
-			return 0;
+			throw new IllegalArgumentException(EMPTY_ERROR_MSG);
 		}
 
 		Matcher matcher = CUSTOM_SEPARATOR_PATTERN.matcher(input);
@@ -25,6 +28,15 @@ public class Calculator {
 	}
 
 	private int calculateSum(String numbers, String separator) {
-		return Arrays.stream(numbers.split(separator)).mapToInt(Integer::parseInt).sum();
+		return Arrays.stream(numbers.split(separator)).mapToInt(this::parseInt).sum();
 	}
+
+	private int parseInt(String input){
+		int number = Integer.parseInt(input.trim());
+		if (number < 0){
+			throw new IllegalArgumentException(NEGATIVE_ERROR_MSG);
+		}
+		return number;
+	}
+
 }
