0ba1a1b7f7f8ffec067ac2df9f0ce85e80c6b874
GoGradually <chkun3109@gmail.com>
2024-10-16 09:17:54 +0900

    feat(Checker): Checker 구현
    - regex 이용
    - 반복문 순화하여 구분자 여부 검사

--- src/main/java/calculator/function/Checker.java
+++ src/main/java/calculator/function/Checker.java
@@ -11,5 +11,14 @@ public class Checker {
         this.separator = separator;
     }
 
-    public void execute(){}
+    public void execute(){
+        for (String inputString : inputStrings) {
+            for (int i = 0; i < separator.length(); i++) {
+                char target = separator.charAt(i);
+                if(inputString.indexOf(target) != -1) throw new IllegalStateException();
+            }
+            if(inputString.length() > 1 && inputString.charAt(0) == '0') throw new IllegalStateException();
+            if(!inputString.matches("\\d*")) throw new IllegalStateException();
+        }
+    }
 }
