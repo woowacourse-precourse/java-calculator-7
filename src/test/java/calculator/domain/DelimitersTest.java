67967cc80b1cf120cf6147a87fec94e0ce8374a9
NaMinhyeok <nmh9097@gmail.com>
2024-10-16 17:41:38 +0900

    test: 구분자 테스트 작성

---
+++ src/test/java/calculator/domain/DelimitersTest.java
@@ -0,0 +1,80 @@
+package calculator.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DelimitersTest {
+
+    @DisplayName("사용자가 커스텀 구분자를 이용할 경우 커스텀 구분자를 가진다.")
+    @Test
+    void useCustomDelimiter() {
+        //given
+        String customDelimiterString = "//;\\n1;2;3";
+        //when
+        Delimiters delimiters = Delimiters.fromInput(customDelimiterString);
+        //then
+        assertThat(delimiters)
+            .extracting("delimiters").isEqualTo(Set.of(";"));
+    }
+
+    @DisplayName("사용자가 커스텀 구분자를 사용하지 않으면 기본 구분자인 :와 ,를 가진다")
+    @Test
+    void defaultDelimiter() {
+        //given
+        String defaultDelimiterString = "1,2:3";
+        //when
+        Delimiters delimiters = Delimiters.fromInput(defaultDelimiterString);
+        //then
+        assertThat(delimiters)
+            .extracting("delimiters").isEqualTo(Set.of(",", ":"));
+    }
+
+
+    @DisplayName("커스텀 구분자인지 확인 할 수 있다. ")
+    @Test
+    void isCustomDelimiter() {
+        //given
+        Delimiters delimiters = new Delimiters(";");
+        //when
+        //then
+        assertThat(delimiters.isCustomDelimiter()).isTrue();
+    }
+
+    @DisplayName("커스텀 구분자인지 확인한다.")
+    @Test
+    void isDefaultDelimiter() {
+        //given
+        Delimiters delimiters = new Delimiters();
+        //when
+        //then
+        assertThat(delimiters.isCustomDelimiter()).isFalse();
+    }
+
+    @DisplayName("구분자를 통해 기본 REGEX를 반환한다.")
+    @Test
+    void getDefaultRegex() {
+        //given
+        Delimiters delimiters = new Delimiters();
+        //when
+        String regex = delimiters.getRegex();
+        //then
+        assertThat(regex).contains(",", ":");
+    }
+
+    @DisplayName("구분자를 통해 커스텀 REGEX를 반환한다.")
+    @Test
+    void getCustomRegex() {
+        //given
+        Delimiters delimiters = new Delimiters(";");
+        //when
+        String regex = delimiters.getRegex();
+        //then
+        assertThat(regex).isEqualTo(";");
+    }
+
+
+}
