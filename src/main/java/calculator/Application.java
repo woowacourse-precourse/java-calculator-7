7f78b922eae12532c9327bc43f42f7fd9cceec87
ppzepi <hsunguk1999@gmail.com>
2024-10-16 20:36:16 +0900

    feat: feature readline feat: feature try-catch

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,13 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자를 입력하세요:");
         String input = Console.readLine(); //사용자입력
+        try {
+            int result = calculateSum(input);
+            System.out.println("결과: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
+
 }
