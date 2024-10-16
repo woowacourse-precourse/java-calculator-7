c268e2cba11f76ea213fb73ab4a90d5f5fc12cfe
hyojun <hjlim45678@gmail.com>
2024-10-16 17:48:19 +0900

    feat: 기본 구분자가 생략된 문자열인 경우 예외처리 구현

--- src/main/java/calculator/MainController.java
+++ src/main/java/calculator/MainController.java
@@ -1,7 +1,6 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,6 +9,7 @@ public class MainController {
     public static void run() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputString = Console.readLine();
+
         List<String> splittedValues ;
         int sumResult = 0;
 
@@ -22,12 +22,12 @@ public class MainController {
             System.out.printf("결과 : %d", sumResult);
             return;
         };
+
         splittedValues = SplitbyDefaultDelimiter(inputString);
         for (String part : splittedValues) {
             sumResult += Integer.parseInt(part);
         }
         System.out.printf("결과 : %d", sumResult);
-        return;
     }
 
     private static Boolean checkCustomDelimter(String inputString) {
@@ -37,8 +37,18 @@ public class MainController {
     }
 
     private static List<String> SplitbyDefaultDelimiter(String inputString) {
+        //inputString에 기본구분자가 없는경우 예외처리
+        containsDefaultDelimiter(inputString);
+
         List<String> defaultSplitValues = Arrays.asList(inputString.split(",|:"));
+
         return defaultSplitValues;
+    }
+
+    private static void containsDefaultDelimiter(String inputString) {
+        if (!inputString.contains(",") && !inputString.contains(":")) {
+            throw new IllegalArgumentException("기본 구분자가 포함되어 있지 않습니다.");
+        }
     }
 
     private static List<String> SplitbyCustomDelimiter(String inputString) {
