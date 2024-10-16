6fb9ab6a0657262b71e8738f1b79276393c84941
김동찬 <come753zz@gmail.com>
2024-10-16 16:44:38 +0900

    feat(ExtractNumber) 입력 문자열을 받으면 커스텀 유무 판단 후 숫자 추출
    ExtractNumber에 두가지 메소드를 추가했습니다:
    - 받은 문자열이 커스텀문자열이면 구분자를 뽑아내어 숫자를 문자열로 만들어 리턴
    - 받은 문자열이 기본 문자열이면 쉼표와 콜론으로 숫자만을 뽑아내어 문자열로 리턴
    
    두가지 메소드는 생성자에서 받은 input만을 사용해야하기 때문에 접근지시자를 private로 선언했습니다

--- src/main/java/calculator/ExtractNumber.java
+++ src/main/java/calculator/ExtractNumber.java
@@ -1,4 +1,29 @@
 package calculator;
 
 public class ExtractNumber {
+
+    public String input;
+
+    public ExtractNumber(String input) {
+        this.input = input;
+    }
+
+    public String[] split(){
+        if(input.startsWith("//")){
+            return splitByCustomExtracter();
+        }
+        else{
+            return splitByDefaultExtracter();
+        }
+    }
+
+    private String[] splitByCustomExtracter(){
+        String delimiter = input.substring(2,input.indexOf("\\n"));
+        String numberPart=input.substring(input.indexOf("\\n")+2);
+        return numberPart.split(delimiter);
+    }
+
+    private String[] splitByDefaultExtracter(){
+        return input.split(",|:");
+    }
 }
