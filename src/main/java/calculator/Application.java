511c40073e0a6ab694ac8925cbaa65e9fb169fc3
capDoYeonLee <ghtyu21@naver.com>
2024-10-17 00:48:23 +0900

    feat: 계산기 실행 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.service.CalculatorService;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputClass inputClass = new InputClass();
+        CalculatorService calculatorService = new CalculatorService();
+
+        String inputText = inputClass.input();
+        if (inputClass.existCustomSeparator(inputText)) {
+            calculatorService.calculateContainCustomSeparator(inputText);
+        }
+        calculatorService.calculateContainOriginalSeparator(inputText);
     }
 }
