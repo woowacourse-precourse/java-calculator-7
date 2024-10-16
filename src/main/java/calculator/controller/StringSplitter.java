ec335926d2bae62fab44abd91938f507585f4a7e
binn <anyubin0724@naver.com>
2024-10-16 22:43:51 +0900

    feat : StringSplitter.java 함수 구현
    기본 구분자, 커스텀 구분자를 기준으로 숫자들을 분리하는 splitter 함수 구현

--- src/main/java/calculator/controller/StringSplitter.java
+++ src/main/java/calculator/controller/StringSplitter.java
@@ -3,10 +3,13 @@ package calculator.controller;
 public class StringSplitter {
     public static String[] splitter(String input) {
         String delimiter = "[,:]";
+        input = input.trim();
+        input = input.replace("\\n", "\n");
 
         if (input.startsWith("//")) {
             int endDelimiter = input.indexOf("\n");
             delimiter = input.substring(2, endDelimiter);
+            System.out.println(delimiter);
             input = input.substring(endDelimiter + 1);
         }
 
