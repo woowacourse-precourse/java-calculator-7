52654d7618c5699513bb3bb0d4acf6e7d73b39ac
Fletacode <bassykd@gmail.com>
2024-10-16 21:33:56 +0900

    feat(tool1): 구분자 추출기능 구현
    구분자들을 추출하는 메서드 extractDelimiter 구현:
    - 구분자 문자열을 받아서 구분자들을 문자열 배열로 변환하여 리턴합니다.
     ex) "//,:\n1:2" -> {",",":"}
    - main에 구분자를 추출하는 메서드를 사용하고 구분자들을 delimiters 배열에 넣었습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,7 @@ package calculator;
 
 import calculator.tool1.StringChecker;
 import calculator.tool1.StringInput;
+import calculator.tool2.ExtractDelimiter;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.NoSuchElementException;
@@ -12,12 +13,18 @@ public class Application {
 
     try {
       //입력값 받기
-      String input = StringInput.inputString();
+      String[] input = StringInput.inputString();
+
+      String delimitersString = input[0];
+      String numsString = input[1];
 
       //문자열 유효성 검사
-      if (!StringChecker.checkString(input)) {
+      if (!StringChecker.checkString(delimitersString,numsString)) {
         throw new IllegalArgumentException("문자열이 유효하지 않습니다");
       }
+
+      String[] delimiters =  ExtractDelimiter.
+          extractDelimiter(delimitersString);
 
     } catch (NoSuchElementException |
              IllegalArgumentException noSuchElementException) {
