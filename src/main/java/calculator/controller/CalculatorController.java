7f83b3a3d4357c687a10da6cf967671f9279bbee
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-10-16 21:18:49 +0900

    feat: 문자열 입력 구현

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,11 @@
+package calculator.controller;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class CalculatorController {
+
+    public static void run() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = readLine();
+    }
+}
