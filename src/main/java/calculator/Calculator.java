fd361df3939aa33ae7b25daa1f90ceee9da66dd8
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-10-16 17:20:35 +0900

    feat: 구분자 기준으로 문자열 분리 함수 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,11 +2,21 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
+    /**
+     * 구분자 기준으로 문자열 분리
+     * 문자열 배열 생성
+     * @param separator 구분자
+     * @param str 입력받은 문자열
+     */
+    public String[] separate(String separator, String str){
+        return str.split(separator);
+    }
+
     // 시작 함수
     public void start(){
         // 문자열 입력
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
-        System.out.println(str);
+
     }
 }
