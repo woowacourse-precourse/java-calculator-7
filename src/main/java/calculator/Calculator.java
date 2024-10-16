52764e313d29c143369e784d28a04db8d53f904f
StoneCAU <seokwoo1999@naver.com>
2024-10-16 21:38:01 +0900

    feat: 커스텀 구분자 지정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -7,6 +7,9 @@ import static java.lang.String.format;
 public class Calculator {
     private static final String FIRST_DELIMITER = ",";
     private static final String SECOND_DELIMITER = ":";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+    private static String CUSTOM_DELIMITER = "";
 
     public static int calculate(final String input) {
         List<String> parsedNumbers = parseNumbers(input);
@@ -17,6 +20,26 @@ public class Calculator {
     }
 
     private static List<String> parseNumbers(final String input) {
+        if (hasCustomDelimiter(input)) {
+            setCustomDelimiter(input);
+
+            int subStringLength = CUSTOM_DELIMITER_PREFIX.length() + CUSTOM_DELIMITER.length() + CUSTOM_DELIMITER_SUFFIX.length();
+            String newInput = input.substring(subStringLength);
+
+            return List.of(newInput.split(format("[%s|%s|%s]", FIRST_DELIMITER, SECOND_DELIMITER, CUSTOM_DELIMITER)));
+        }
         return List.of(input.split(format("[%s|%s]", FIRST_DELIMITER, SECOND_DELIMITER)));
+    }
+
+    private static void setCustomDelimiter(final String input) {
+        int prefixIndex = input.indexOf(CUSTOM_DELIMITER_PREFIX);
+        int suffixIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+
+        CUSTOM_DELIMITER = input.substring(prefixIndex + CUSTOM_DELIMITER_PREFIX.length(), suffixIndex);
+    }
+
+    private static boolean hasCustomDelimiter(final String input) {
+        if(!input.startsWith(CUSTOM_DELIMITER_PREFIX)) return false;
+        return input.contains(CUSTOM_DELIMITER_SUFFIX);
     }
 }
