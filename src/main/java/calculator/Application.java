2b608cb95712a95681f10df307cf48928b6a82c1
seahwanKim <kimkimseasea@gmail.com>
2024-10-16 18:33:22 +0900

    feat: 입력한 문자열을 구분하여 합 계산 기능을 구현한다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println("결과 : " + calculator(Console.readLine()));
     }
+
+    private static int calculator(String input) throws IllegalArgumentException{
+
+        String[] numbers = input.split("[,;]");
+
+        int sum = 0;
+        for (String number : numbers) {
+             sum += Integer.parseInt(number);
+        }
+        return sum;
+    }
+
 }
