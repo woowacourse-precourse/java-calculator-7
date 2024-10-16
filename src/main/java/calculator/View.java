8d4a8a8ae346d1dc9a1099826263800961a3984b
speculatingwook <bwook9908@gmail.com>
2024-10-16 16:36:47 +0900

    feat: implement View class
    - prints instruction
    - prints result

---
+++ src/main/java/calculator/View.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class View {
+
+    public void printInstruction() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public void printResult(String text){
+        System.out.println(text);
+    }
+
+}
