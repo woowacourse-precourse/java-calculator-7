6966b27126c8db969d910f06ebb215bef59e8796
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-17 08:55:01 +0900

    feat: Add CalculatorMessage class for input and output messages

---
+++ src/main/java/calculator/message/CalculatorMessage.java
@@ -0,0 +1,8 @@
+package calculator.message;
+
+public class CalculatorMessage {
+    private static final String NEW_LINE = "\n";
+
+    public static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요." + NEW_LINE;
+    public static final String OUTPUT_MESSAGE = "결과 : %d" + NEW_LINE;
+}
