fd821d65f2356efe827617839dabbd95603f7729
MeangSung <aoddlsgh98@gmail.com>
2024-10-16 20:17:18 +0900

    Fix for SLP in Application.java
    해당 파일은 순수 실행을 위한 파일로만 사용하기 위해 Solution을 활용해
    요구사항을 수행하도록 수정
    
    Closes #3

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,16 +3,6 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        Input input = new Input();
-        Calculate calculate;
-
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String str = input.nextLine();
-        calculate = new Calculate(str);
-
-        int res = calculate.cal();
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("결과 : ").append(res);
+        new Solution().solve();
     }
 }
