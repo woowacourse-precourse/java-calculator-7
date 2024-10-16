975832c02ffceff6a84189084afeb641281aa3e6
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-16 23:26:41 +0900

    feat(InputManager): isInteger 기능 구현

---
+++ src/main/java/calculator/domain/preprocess/InputManager.java
@@ -0,0 +1,15 @@
+package calculator.domain.preprocess;
+
+import java.util.List;
+
+public class InputManager {
+
+    public boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
