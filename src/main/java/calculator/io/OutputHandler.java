a0249de0a2eedb300c8ff13e553b0d5503db9496
NaMinhyeok <nmh9097@gmail.com>
2024-10-16 14:06:27 +0900

    feat: 게임 출력 구현

---
+++ src/main/java/calculator/io/OutputHandler.java
@@ -0,0 +1,16 @@
+package calculator.io;
+
+public class OutputHandler {
+
+    public static final String INITIAL_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    public static final String RESULT_MESSAGE = "결과 : ";
+
+    public void showInitialMessage() {
+        System.out.println(INITIAL_MESSAGE);
+    }
+
+    public void showResultMessage(int result) {
+        System.out.println(RESULT_MESSAGE + result);
+    }
+
+}
