4d7fd42245794276e5a303819cc7b7c25f8005f2
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-16 23:11:01 +0900

    feat(StringParser): 커스텀 문자열 예외 기능 추가
    커스텀 문자열이 "\"문자를 포함하지 않을 경우, 예외를 발생시킨다.

--- src/main/java/calculator/domain/StringParser.java
+++ src/main/java/calculator/domain/StringParser.java
@@ -10,6 +10,7 @@ public class StringParser {
     private static final String FIRST_CUSTOM_STRING = "//";
     private static final String CUSTOM_NUMBER_PART_DELIMITER = "\\";
     private static final String EXCEPTION_EMPTY_STRING = "빈 값을 입력하셨습니다.";
+    private static final String EXCEPTION_CUSTOM_STRING = "올바른 형식으로 입력하세요.";
 
     private final List<String> numbers;
 
@@ -26,6 +27,7 @@ public class StringParser {
         if (!paresFirstString(inputStrings).equals(FIRST_CUSTOM_STRING)) {
             return parseStandardStrings(inputStrings);
         }
+        validateCustomStrings(inputStrings);
         return parseCustomStrings(inputStrings);
     }
 
@@ -51,6 +53,12 @@ public class StringParser {
     private void validateEmptyString(String inputStrings) {
         if (inputStrings.isEmpty()) {
             throw new IllegalArgumentException(EXCEPTION_EMPTY_STRING);
+        }
+    }
+
+    private void validateCustomStrings(String inputStrings) {
+        if (!inputStrings.contains(CUSTOM_NUMBER_PART_DELIMITER)) {
+            throw new IllegalArgumentException(EXCEPTION_CUSTOM_STRING);
         }
     }
 }
