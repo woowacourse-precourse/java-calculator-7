0f5764957428a3de66c9558a06ba989fe338e776
yunsseong <aster20@hanyang.ac.kr>
2024-10-16 22:44:18 +0900

    feat: 커스텀 구분자 형식 검증 기능 구현

--- src/main/java/calculator/service/DelimiterService.java
+++ src/main/java/calculator/service/DelimiterService.java
@@ -3,10 +3,12 @@ package calculator.service;
 import calculator.model.Delimiter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class DelimiterService {
 
     private List<Delimiter> delimiters = new ArrayList<>();
+    private final Pattern customDelimiterPattern = Pattern.compile("^//.\\n.*");
 
     public void addDelimiter(String delimiter) {
         delimiters.add(new Delimiter(delimiter));
@@ -14,5 +16,8 @@ public class DelimiterService {
 
     public void addDelimiters(List<String> delimiters) {
         delimiters.forEach(this::addDelimiter);
+    }
+    public boolean validateCustomDelimiterFormat(String string) {
+        return customDelimiterPattern.matcher(string).matches();
     }
 }
