301a7038e41e1702650e7a4b1bfb0a14e75f3742
SukJin Kim <rkathrwks@gmail.com>
2024-10-17 02:39:23 +0900

    feat(addition): implement addition with custom delimiters
     Implemented addition functionality that supports custom delimiters.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,17 +8,34 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String strToAdd = Console.readLine();
 
+        if (strToAdd.isEmpty()) {
+            System.out.println("결과 : " + 0);
+            return;
+        }
+
         if (!isValid(strToAdd)) {
             throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
         }
 
-        String[] numbers = strToAdd.split("[,:]");
+        String customDelimiter = null;
+        if (strToAdd.startsWith("//")) {
+            int newlineIndex = strToAdd.indexOf("\\n");
+            customDelimiter = Pattern.quote(strToAdd.substring(2, newlineIndex));
+            strToAdd = strToAdd.substring(newlineIndex + 2);
+        }
+
+        String delimiterRegex = "[,:]";
+
+        if (customDelimiter != null) {
+            delimiterRegex = String.format("[,:%s]", customDelimiter);
+        }
+
+        String[] numbers = strToAdd.split(delimiterRegex);
 
         int sum = 0;
-        if (numbers.length > 1) {
-            for (String number : numbers) {
-                sum += Integer.parseInt(number);
-            }
+
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
         }
 
         System.out.println("결과 : " + sum);
@@ -29,7 +46,24 @@ public class Application {
             return true;
         }
 
-        String regex = "^[1-9][0-9]*([,|:][1-9][0-9]*)*$";
+        String customDelimiter = null;
+        if (input.startsWith("//")) {
+            int newlineIndex = input.indexOf("\\n");
+            if (newlineIndex == -1) {
+                return false;
+            }
+
+            customDelimiter = Pattern.quote(input.substring(2, newlineIndex));
+            input = input.substring(newlineIndex + 2);
+        }
+
+        String delimiterRegex = "[,:]";
+
+        if (customDelimiter != null) {
+            delimiterRegex = String.format("[,:%s]", customDelimiter);
+        }
+
+        String regex = String.format("^[1-9][0-9]*(%s[1-9][0-9]*)*$", delimiterRegex);
 
         return Pattern.matches(regex, input);
     }
