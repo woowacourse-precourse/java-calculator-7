64118501fdfbfc041bd5c73884a4568ef504787f
hyunwon <khw73850@gmail.com>
2024-10-16 22:02:09 +0900

    chore: remove unused import statements

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,5 @@
 package calculator;
 
-import java.awt.image.AreaAveragingScaleFilter;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
