f49b1c375cdcd44c9f4fc7f2b69d2843ab2f17f0
jeonghyeon-ohk <ok6737@naver.com>
2024-10-16 21:32:03 +0900

    feat(view): OutputView 클래스에 결과값 출력 기능 구현

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,4 +1,9 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class OutputView{
+    public static void printResult(int result) {
+        System.out.print(result);
+    }
 }
