4b28ec4b2e23803999311a6fc088b0b67ce20fb7
SunHee <76764643+sunni546@users.noreply.github.com>
2024-10-16 22:17:51 +0900

    feat: 문자열 입력

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,9 +1,11 @@
 package calculator.controller;
 
+import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class CalculatorController {
     public void run() {
         OutputView.printInputStringMessage();
+        InputView.readInputString();
     }
 }
---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String readInputString() {
+        return Console.readLine();
+    }
+}
