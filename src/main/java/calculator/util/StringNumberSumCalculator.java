9a1467967038b0ee87dba0b5e465645949dfabdb
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-16 18:02:29 +0900

    chore: 패키지명을 model에서 util로 수정

--- src/main/java/calculator/util/StringNumberSumCalculator.java
+++ src/main/java/calculator/util/StringNumberSumCalculator.java
@@ -1,4 +1,4 @@
-package calculator.model;
+package calculator.util;
 
 import java.util.Arrays;
 
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,6 +1,6 @@
 package calculator;
 
-import calculator.model.StringNumberSumCalculator;
+import calculator.util.StringNumberSumCalculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
