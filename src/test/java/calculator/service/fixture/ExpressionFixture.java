a221d3cb4fad110c6fea582863ac99160e00588d
mete0rfish <sungwon326@naver.com>
2024-10-16 16:25:10 +0900

    refactor: ExpressionFixture의 클래스명 오타 수정
    ExpressinFixture에서 ExpressionFixture로 수정

--- src/test/java/calculator/service/fixture/ExpressionFixture.java
+++ src/test/java/calculator/service/fixture/ExpressionFixture.java
@@ -1,6 +1,6 @@
 package calculator.service.fixture;
 
-public class ExpressinFixture {
+public class ExpressionFixture {
 
     public final static String EXPRESSION_WITHOUT_CUSTOM_IDENTIFIER = "1:22.333";
     public final static String EXPRESSION_WITH_CUSTOM_IDENTIFIER = "//{\n1:22.333";
