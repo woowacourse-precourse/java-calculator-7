80f672c1edc5d1631e022a77c3761e31c13f4c1a
Lys <redspinyant@gmail.com>
2024-10-16 13:04:18 +0900

    feat: 메인 로직 연결
    - 프로그램 실행 시 작동 확인

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+
+		Calculator calculator = new Calculator();
+		CalculatorUI calculatorUI = new CalculatorUI(calculator);
+
+		calculatorUI.run();
+	}
 }
