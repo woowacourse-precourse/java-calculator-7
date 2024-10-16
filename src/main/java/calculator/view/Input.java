7f832ed149146e3e7f9398011883b8c664b69f65
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-16 23:45:33 +0900

    feat : 문자열 입력 문구 출력

--- src/main/java/calculator/view/Input.java
+++ src/main/java/calculator/view/Input.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     public static String inputStringToAdd(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 
