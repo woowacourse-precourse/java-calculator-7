b93d3f8bea8bbeecf51728f0d041a31815116f45
RAJUYEOB <pag1yun99@gmail.com>
2024-10-16 11:35:46 +0900

    feat: 기본 구분자(쉼표, 콜론) 처리 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,31 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> changedNumber = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please input the number.");
+        String input = sc.nextLine();
+
+        String[] number = input.split("[, :]");
+
+        for (String pureNumber : number) {
+            changedNumber.add(Integer.parseInt(pureNumber.trim()));
+        }
+
+        int sum = 0;
+        for (int i = 0; i < changedNumber.size(); i++) {
+            sum += changedNumber.get(i);
+        }
+
+        System.out.println(sum);
+
+        sc.close();
     }
 }
+
