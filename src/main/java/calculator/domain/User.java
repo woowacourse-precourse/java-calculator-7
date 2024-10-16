12df04e4f4cc32d3f6fbf19850e6a55e3a13a8ef
최창환 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-16 10:46:34 +0900

    feat: 사용자가 입력한 문자열 substring 하기

--- src/main/java/calculator/domain/User.java
+++ src/main/java/calculator/domain/User.java
@@ -1,4 +1,16 @@
 package calculator.domain;
 
+
 public class User {
+    public User(String userString) {
+        String[] userStringArray = substringUserStrings(userString);
+    }
+
+    private String[] substringUserStrings(String userString) {
+        String[] userStringArray = new String[userString.length()];
+        for (int i = 0; i < userString.length(); i++) {
+            userStringArray[i] = userString.substring(i, i + 1);
+        }
+        return userStringArray;
+    }
 }
