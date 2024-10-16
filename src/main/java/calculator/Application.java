84195b44495ee09a1659e050b08e4e0ac91a0d77
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-17 03:17:35 +0900

    feat(Application): Application 메인 함수 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.domain.numCalculator.NumCalculator;
+import calculator.domain.preprocess.DelimeterProcessor;
+import calculator.domain.preprocess.InputManager;
+import calculator.domain.preprocess.Preprocessor;
+import calculator.domain.preprocess.SplitManager;
+import calculator.view.ViewManager;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorController calculatorController = new CalculatorController(
+                new NumCalculator(),
+                new Preprocessor(
+                        new DelimeterProcessor(),
+                        new InputManager(),
+                        new SplitManager()),
+                new ViewManager());
+
+        calculatorController.runCalculator();
     }
 }
