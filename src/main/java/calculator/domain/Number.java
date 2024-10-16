cf40ac4a7349a87065ba1d7fa1e81ba66de81726
minzzun99 <skg06123@naver.com>
2024-10-16 23:00:37 +0900

    feat(domain): 숫자를 관리하고 더한 값을 반환하는 Number 클래스 구현
    - List를 사용해서 숫자를 저장
    - List 내부 요소를 더해서 값을 반환하는 getSum 메서드 구현
    - List에 요소를 추가하는 setNumberList 메서드 작성

---
+++ src/main/java/calculator/domain/Number.java
@@ -0,0 +1,25 @@
+package calculator.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Number {
+    private final List<Integer> numberList;
+
+    Number() {
+        this.numberList = new ArrayList<>();
+    }
+
+    public void setNumberList(int num) {
+        numberList.add(num);
+    }
+
+    public int getSum() {
+        int sum = 0;
+        for (int num : numberList) {
+            sum += num;
+        }
+
+        return sum;
+    }
+}
