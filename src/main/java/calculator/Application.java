2a2212b966742b170a9ea59df786415cfbe89a52
bogyeom0922 <pbk0398@gmail.com>
2024-10-16 19:30:25 +0900

    feat: 시작 메서드 생성
    - InputReader와 OutputWriter 인스턴스 생성
    - CalculatorController 인스턴스를 통해 덧셈 계산 시작

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.view.InputReader;
+import calculator.view.OutputWriter;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // InputReader, OutputWriter 인스턴스 생성
+        InputReader inputReader = new InputReader();
+        OutputWriter outputWriter = new OutputWriter();
+
+        // CalculatorController 인스턴스 생성
+        CalculatorController calculatorController = new CalculatorController(inputReader, outputWriter);
+
+        // 덧셈 계산 시작
+        calculatorController.run();
     }
 }
