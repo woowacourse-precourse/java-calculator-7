5cbafea2b576b5b7e81e0054643c540a1ac6deb1
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-16 22:29:47 +0900

    feat(DelimeterProccesor): validateCutomDelimeterRequest 기능 구현

--- src/main/java/calculator/domain/preprocess/DelimeterProcessor.java
+++ src/main/java/calculator/domain/preprocess/DelimeterProcessor.java
@@ -8,4 +8,11 @@ public class DelimeterProcessor {
         else
             return false;
     }
+
+    public void validateCutomDelimeterRequest(String input) {
+        if (input.indexOf("\n") == 2)
+            throw new IllegalArgumentException("구분자로 사용할 문자 미입력");
+        if (input.indexOf("\n") > 3)
+            throw new IllegalArgumentException("구분자로 사용할 문자가 두 글자 이상");
+    }
 }
