ea848646174300289a6b3d9863a87e540016b860
immyeong <wpfls6670@naver.com>
2024-10-16 18:52:58 +0900

    feat: main 함수 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import calculator.domain.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        Calculator calculator = new Calculator(inputView);
+        new OutputView(calculator.calculateNumber());
     }
 }
