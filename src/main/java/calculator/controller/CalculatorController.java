73dff0440eb408634703482ffd81c889d55bbe6f
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-16 23:00:40 +0900

    refactor(Application): MVC 패턴 적용
    - MVC 패턴 적용을 위해 컨트롤러를 생성하고 run 메서드를 작성
    - 메인 메서드에서 inputView가 아닌 컨트롤러를 생성하고 호출

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,10 @@
+package calculator.controller;
+
+import calculator.view.InputView;
+
+public class CalculatorController {
+    public void calculatorRun() {
+        InputView inputView = new InputView();
+        String inputString = inputView.readString();
+     }
+}
