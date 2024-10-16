68aca1a435b1f5a073b83cd22f51943922b8df7c
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-16 21:05:33 +0900

    feat(application): 계산기 동작 구현
    application 에서 계산기가 동작할 수 있도록 구현하였습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import calculator.controller.StringCalculatorController;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        StringCalculatorController controller = new StringCalculatorController(inputView, outputView);
+        controller.startCalculator();
     }
 }
