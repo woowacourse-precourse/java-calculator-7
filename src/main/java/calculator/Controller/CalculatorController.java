694042e2bc5fa2dc774d08b4d2162bb2ca129aa0
Seonjeongg <sj030205@naver.com>
2024-10-16 09:46:47 +0900

    feat: 빈 문자열 처리 기능

--- src/main/java/calculator/Controller/CalculatorController.java
+++ src/main/java/calculator/Controller/CalculatorController.java
@@ -1,4 +1,5 @@
 package calculator.Controller;
 
 public class CalculatorController {
+
 }
--- src/main/java/calculator/Service/CalculatorService.java
+++ src/main/java/calculator/Service/CalculatorService.java
@@ -1,4 +1,10 @@
 package calculator.Service;
 
 public class CalculatorService {
+    public static int add(String input){
+        if(input==null || input.isEmpty()){
+            return 0;
+        }
+    }
+
 }
