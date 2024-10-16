df45cf47d9704001e9c4e18d4b04d74e767a9e9e
Sinorang <sinholang@naver.com>
2024-10-16 19:17:23 +0900

    feat: 기본 구문자 기준 숫자 추출 및 합산 기능 구현
    - ,와 :을 구분자로 사용하는 문자열을 입력받아 숫자를 추출
    - 추출한 숫자를 합산

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -2,10 +2,18 @@ package calculator.domain;
 
 public class Calculator {
 
-    public int calculate(String input) {
+    public int splitAndSum(String input) {
         if (input == null || input.trim().isEmpty()) {
             return 0;
         }
-        return -1; // 임시 리턴값
+
+        String[] numbers = input.split(",|:");
+
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+
+        return sum;
     }
 }
