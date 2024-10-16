64d44a6cc51db380200cdca6193b0b254eaa7b83
Seonjeongg <sj030205@naver.com>
2024-10-16 09:57:34 +0900

    feat: 커스텀 구분자 추출 기능

--- src/main/java/calculator/Service/CalculatorService.java
+++ src/main/java/calculator/Service/CalculatorService.java
@@ -1,10 +1,21 @@
 package calculator.Service;
 
 public class CalculatorService {
+    private static String Separator = ",|:";
     public static int add(String input){
         if(input==null || input.isEmpty()){
             return 0;
         }
+        if(input.startsWith("//")) Custom_Separator(input);
+        String[] numbers = input.split(Separator);
+
+
+    }
+
+    private static String Custom_Separator(String input){
+        int separatorIndex = input.indexOf("\n");
+        Separator=input.substring(2,separatorIndex);
+        return input.substring(separatorIndex+1);
     }
 
 }
