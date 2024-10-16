ea5cb6f63c4bcbb4b51cd6e14b970b3aed4aec45
unh6unh6 <ruddbs3308@naver.com>
2024-10-16 19:28:57 +0900

    feat: CalculatorController 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,11 +1,22 @@
 package calculator.controller;
 
 
+import calculator.model.Calculator;
+import calculator.model.CustomDelimiterProcessor;
+import calculator.model.Delimiters;
+import calculator.model.NumberExtractor;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class CalculatorController {
     public void start() {
         InputView.printStartMessage();
         String inputString = InputView.getInput();
+        Delimiters delimiters = new Delimiters();
+        inputString = CustomDelimiterProcessor.addCustomDelimiters(inputString, delimiters);
+        Calculator calculator = new Calculator();
+        NumberExtractor.extractNumber(inputString, delimiters, calculator);
+        int result = calculator.calculate();
+        OutputView.printResultMessage(result);
     }
 }
--- src/main/java/calculator/model/NumberExtractor.java
+++ src/main/java/calculator/model/NumberExtractor.java
@@ -1,7 +1,7 @@
 package calculator.model;
 
 public class NumberExtractor {
-    public void extractNumber(String inputString, Delimiters delimiters, Calculator calculator) {
+    public static void extractNumber(String inputString, Delimiters delimiters, Calculator calculator) {
         if (!isStartingWithDigit(inputString)) {
             throw new IllegalArgumentException();
         }
@@ -13,18 +13,18 @@ public class NumberExtractor {
         }
     }
 
-    private boolean isStartingWithDigit(String inputString) {
+    private static boolean isStartingWithDigit(String inputString) {
         char firstChar = inputString.charAt(0);
         return Character.isDigit(firstChar);
     }
 
-    private String[] splitByDelimiters(Delimiters delimiters, String inputString) {
+    private static String[] splitByDelimiters(Delimiters delimiters, String inputString) {
         String delimiterRegex = createDelimiterRegex(delimiters);
         String[] splitedStringArray = inputString.split(delimiterRegex);
         return splitedStringArray;
     }
 
-    private String createDelimiterRegex(Delimiters delimiters) {
+    private static String createDelimiterRegex(Delimiters delimiters) {
         StringBuilder stringBuilder = new StringBuilder();
         for (char c : delimiters.getDelimiters()) {
             stringBuilder.append(c);
@@ -32,7 +32,7 @@ public class NumberExtractor {
         return stringBuilder.toString();
     }
 
-    private void validateNumberString(String inputString) {
+    private static void validateNumberString(String inputString) {
         for (char c : inputString.toCharArray()) {
             if (!Character.isDigit(c)) {
                 throw new IllegalArgumentException();
