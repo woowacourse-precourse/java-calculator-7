4c028c81795264c03e5623821bfa1331c67b93f0
Sujin Kim <su10jin11@khu.ac.kr>
2024-10-16 15:16:58 +0900

    feat : CalculatorController 구현

--- src/main/java/calculator/Controller/CalculatorController.java
+++ src/main/java/calculator/Controller/CalculatorController.java
@@ -1,10 +1,23 @@
 package calculator.Controller;
 
+import calculator.Model.CalculateModel;
+import calculator.View.Input;
+import calculator.View.InputImpl;
+import calculator.View.OutputImpl;
+
 public class CalculatorController {
     public void run() {
         //Input 호출
+        Input input = new InputImpl();
+        String inputString = input.getStringInput();
+
         //Model 호출
+        CalculateModel calculateModel = new CalculateModel();
+        int result = calculateModel.calculate(inputString);
+
         //Output 호출
+        OutputImpl output = new OutputImpl();
+        output.printResult(result);
 
     }
 }
