aa0d980cbbe56562fc51f5fc3ff6680ccc3ef191
Sujin Kim <su10jin11@khu.ac.kr>
2024-10-16 15:13:39 +0900

    feat : 숫자를 더하는 기능 구현

--- src/main/java/calculator/Model/CalculateModel.java
+++ src/main/java/calculator/Model/CalculateModel.java
@@ -27,7 +27,25 @@ public class CalculateModel {
         return sum(Numbers);
     }
 
-    //sum
+    //SUM
+    private int sum(String[] Numbers) {
+        int sum = 0;
+        for (String number : Numbers) {
+            sum += intParser(number);
+        }
+        return sum;
+    }
 
-    //잘못된 값이면 illegalArgumentException 던지고 종료
+    //문자열을 정수로 변환 + 예외처리
+    private int intParser(String number) {
+        try {
+            int intNumber = Integer.parseInt(number);
+            if (intNumber < 0) {
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+            }
+            return intNumber;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 숫자 형식입니다.");
+        }
+    }
 }
