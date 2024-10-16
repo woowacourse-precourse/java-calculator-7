b3c931644b1752adfc541ab1dc9159cd0b8a828c
jklee3409 <jklee3409@khu.ac.kr>
2024-10-16 12:19:19 +0900

    feat(delimiter) : 커스텀 구분자를 추출하고 유효성을 확인하는 getCustomDelimiter 메서드 구현

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -2,7 +2,26 @@ package calculator.domain;
 
 public class Delimiter {
 
+    // 커스텀 구분자가 있는지 확인하는 메서드
     private static boolean hasCustomDelimiter(String input) {
         return input.startsWith("//") && input.contains("\n");
+    }
+
+    // 커스텀 구분자를 추출하고 유효성 확인
+    public String getCustomDelimiter(String input) {
+        if (hasCustomDelimiter(input)) {
+            int delimiterIndex = input.indexOf("\n");
+            String delimiter = input.substring(2, delimiterIndex);
+
+            // 커스텀 구분자는 1개까지만 인정
+            if (delimiter.length() != 1) {
+                throw new IllegalArgumentException("커스텀 구분자는 한 글자여야 합니다.");
+            }
+
+            return delimiter;
+        }
+
+        // 커스텀 구분자가 없으면 null을 반환
+        return null;
     }
 }
