42796be7a5d36e73afad906df28b5701a1ab8980
ppzepi <hsunguk1999@gmail.com>
2024-10-16 18:37:47 +0900

    <feat>: feature readline

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자를 입력하세요:");
+        String input = Console.readLine(); //사용자입력
     }
 }
