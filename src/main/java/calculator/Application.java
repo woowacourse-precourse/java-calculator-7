b7e0ec7ccf6161a1c59853fcba1b6af398c6b855
JoonHyuk0331 <joonlife0901@gmail.com>
2024-10-16 20:05:37 +0900

    refactor(Application): calculator 클래스를 main application으로 이동
    calculator 로직을 Application 클래스의 main 메서드로 이동했습니다.
    이에는 입력 처리, 문자열 파싱 및 숫자의 합산이 포함됩니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("문자열을 입력해주세요");
+        input=sc.next();
+        String input2=input.replace(":",",");
+        String[] splitname=input2.split(",");
+        int sum=0;
+        for (String s : splitname) {
+            sum += Integer.parseInt(s);
+        }
+        System.out.println(sum);
     }
 }
