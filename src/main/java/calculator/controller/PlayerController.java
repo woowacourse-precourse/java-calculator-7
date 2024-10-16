6d61bff6210f7a685640033341a0cc21ca322774
bowook <mbosx316@naver.com>
2024-10-16 21:16:07 +0900

    refactor : PlayerController에 OutputView 인스턴스 생성

--- src/main/java/calculator/controller/PlayerController.java
+++ src/main/java/calculator/controller/PlayerController.java
@@ -3,13 +3,16 @@ package calculator.controller;
 import calculator.domain.Numbers;
 import calculator.domain.Separator;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class PlayerController {
     private Separator separator;
     private final InputView inputView;
+    private final OutputView outputView;
 
     public PlayerController() {
         this.inputView = new InputView();
+        this.outputView = new OutputView();
     }
     public void process() {
         input();
@@ -21,5 +24,6 @@ public class PlayerController {
     }
     private void calculate() {
         Numbers numbers = new Numbers(separator);
+        outputView.printResult(numbers.toString());
     }
 }
