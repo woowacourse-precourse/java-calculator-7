85b063594a1a93a541ec530ea386b870761f0524
wogus <ind07152@naver.com>
2024-10-16 12:06:53 +0900

    feat(MessageConstant): 계산기 결과 문구 상수화
    enum 타입으로 계산기 결과 문구를 상수화한다.

--- src/main/java/calculator/constant/MessageConstant.java
+++ src/main/java/calculator/constant/MessageConstant.java
@@ -1,7 +1,8 @@
 package calculator.constant;
 
 public enum MessageConstant {
-    CALCULATOR_START_MESSAGE("덧셈할 문자열을 입력해 주세요.");
+    CALCULATOR_START_MESSAGE("덧셈할 문자열을 입력해 주세요."),
+    RESULT_MESSAGE("결과 : ");
 
     private final String message;
 
