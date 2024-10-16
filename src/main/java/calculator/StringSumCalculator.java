00e6eb44a4711c969c3b5c8b85d51ee6225f8558
Byuntil <gugugu2014@jbnu.ac.kr>
2024-10-16 17:53:44 +0900

    feat: 숫자를 추출하여 덧셈할 문자열을 입력받는 기능

---
+++ src/main/java/calculator/StringSumCalculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import calculator.view.InputView;
+
+public class StringSumCalculator {
+
+    public void run() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputValue = InputView.getInputValue();
+
+    }
+}
---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String getInputValue() {
+        return Console.readLine();
+    }
+
+}
