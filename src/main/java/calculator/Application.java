95cc4665163140e17d5240c9cf87771316127dcf
inswal843 <inswal843@gmail.com>
2024-10-16 18:03:08 +0900

    feat : 빈 문자열을 입력한 경우의 예외처리 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = Console.readLine();
+        Validation.validateEmpty(userInput);
+
 //     계산을 수행하는 Calculator 생성
         Calculator calculator = new Calculator();
 
@@ -19,7 +21,7 @@ public class Application {
 
 //계산을 수행하는 클래스 분리
 class Calculator {
-    //  사용자의 입력을 구분자오 파싱하는 메서드
+    //  사용자의 입력을 구분자로 파싱하는 메서드
     public String[] splitInput(String userInput) {
         String delimiter = ",|:"; // 기본 구분자
         int customDelimiterIndex;
@@ -41,5 +43,14 @@ class Calculator {
             sum += Integer.parseInt(userInputNumber);
         }
         return sum;
+    }
+}
+
+class Validation {
+    //    입력이 비어있는 경우 예외 반환
+    public static void validateEmpty(String userInput) {
+        if (userInput.isEmpty()) {
+            throw new IllegalArgumentException("빈 문자열은 입력할 수 없습니다.");
+        }
     }
 }
