ea5d9eb1fbb16df62931e597af705e2589d4a357
choi <nippert@naver.com>
2024-10-17 00:05:43 +0900

    feat: 계산기 도메인 구성

---
+++ src/main/java/calculator/domain/StringAddCalculator.java
@@ -0,0 +1,13 @@
+package calculator.domain;
+
+import calculator.util.CommonConstant;
+
+public class StringAddCalculator {
+    private String formula;
+    private String delimeter;
+
+    public StringAddCalculator(String formula) {
+        this.formula = formula;
+        delimeter = CommonConstant.NORMAL_DELIMETER_PATTERN;
+    }
+}
---
+++ src/main/java/calculator/util/CommonConstant.java
@@ -0,0 +1,15 @@
+package calculator.util;
+
+public class CommonConstant {
+
+    private CommonConstant() {
+
+    }
+
+    /* 구분자 */
+    public static final String NORMAL_DELIMETER_PATTERN = ",|:";
+
+    /* 입력 오류 메시지 */
+    public static final String INVALID_FORMAT_MESSAGE = "잘못된 형식입니다.";
+    public static final String NEGATIVE_NUMBER_MESSAGE = "음수는 허용되지 않습니다.";
+}
