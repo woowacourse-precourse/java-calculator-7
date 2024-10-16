58c272fe1f017732e8dcfc0078de776dcd1b5f85
gyongcode <gyongcode@gmail.com>
2024-10-16 16:24:57 +0900

    Numbers 클래스 구현

--- src/main/java/calculator/model/Numbers.java
+++ src/main/java/calculator/model/Numbers.java
@@ -1,5 +1,19 @@
 package calculator.model;
 
-public class Numbers {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Numbers {
+    private ArrayList<Integer> numbers;
+
+    public Numbers(int[] numbers){
+        this.numbers = new ArrayList<>();
+        for (int i : numbers) {
+            this.numbers.add(i);
+        }
+    }
+
+    public ArrayList<Integer> getNumbers(){
+        return numbers;
+    }
 }
