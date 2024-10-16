106577aa775a3eba0836b7e004af144ea55270e7
minw0095 <kmu6898@naver.com>
2024-10-17 01:21:27 +0900

    feat: 사용자로부터 입력받기 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,9 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Calculation calculation = new Calculation();
+
+        calculation.start();
     }
 }
---
+++ src/main/java/calculator/Calculation.java
@@ -0,0 +1,11 @@
+package calculator;
+
+
+public class Calculation {
+
+    Input input = new Input();
+
+    public void start() {
+        
+    }
+}
