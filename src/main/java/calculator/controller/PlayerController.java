4d981ef1e70123bd4c3ad23fcfca2730037c521b
bowook <mbosx316@naver.com>
2024-10-16 21:01:33 +0900

    feat : Numbers 기능 추가

--- src/main/java/calculator/controller/PlayerController.java
+++ src/main/java/calculator/controller/PlayerController.java
@@ -1,9 +1,11 @@
 package calculator.controller;
 
+import calculator.domain.Numbers;
 import calculator.domain.Separator;
 import calculator.view.InputView;
 
 public class PlayerController {
+    private Separator separator;
     private final InputView inputView;
 
     public PlayerController() {
@@ -11,8 +13,13 @@ public class PlayerController {
     }
     public void process() {
         input();
+        calculate();
     }
     private void input() {
         String numbers = inputView.inputNumbers();
+        separator = new Separator(numbers);
+    }
+    private void calculate() {
+        Numbers numbers = new Numbers(separator);
     }
 }
--- src/main/java/calculator/domain/Numbers.java
+++ src/main/java/calculator/domain/Numbers.java
@@ -1,4 +1,21 @@
 package calculator.domain;
 
+import java.util.List;
+
 public class Numbers {
+    private final long sum;
+    public Numbers(Separator separator) {
+        this.sum = sum(separator.getExtractedNumbers());
+    }
+    private long sum(List<Integer> numbers) {
+        long sum = 0;
+        for(int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public long getSum() {
+        return sum;
+    }
 }
