4fc33d5678ee75bca3f60146721e7cfd891d8b90
woojin <zaza0804@uos.ac.kr>
2024-10-17 01:21:11 +0900

    feat($calculator): make Calculator class with Console.readLine

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.app.Calculator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Calculator calculator = new Calculator();
+        calculator.start();
     }
 }
---
+++ src/main/java/calculator/app/Calculator.java
@@ -0,0 +1,9 @@
+package calculator.app;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Calculator {
+    public void start() {
+        String input = Console.readLine();
+    }
+}
