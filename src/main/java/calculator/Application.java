f593b1aec24f420a59b536ab9363c157ead8f942
SeungminKimdev <ksm7250432@gmail.com>
2024-10-16 18:12:29 +0900

    feat: 문자열 입력 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,8 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputString inputString = new InputString();
+        String input = inputString.input();
+        System.out.println(input);
     }
 }
