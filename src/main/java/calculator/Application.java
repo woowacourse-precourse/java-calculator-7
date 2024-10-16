74a263cc59a147b00da7f277c2909fb8de01aaa2
DanMac <dan@DanMacui-MacBookPro.local>
2024-10-16 22:36:27 +0900

    feat: add a custom delimiter feature using // and \n

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,9 +6,21 @@ import java.util.*;
 public class Application {
 
     public static void main(String[] args) {
+        String delimiters = ",:";
+
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
-        String[] numbers = Console.readLine().split("[,:]+");
+        String input = Console.readLine();
+        Console.close();
+
+        if (input.startsWith("//")){
+            String[] input_separated = input.split("//", 2)[1].split("\\\\n", 2);
+
+            delimiters += input_separated[0];
+            input = input_separated[1];
+        }
+
+        String[] numbers = input.split("[" + delimiters + "]+");
 
         int sum = Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
 
