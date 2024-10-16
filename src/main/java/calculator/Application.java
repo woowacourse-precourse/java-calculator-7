216224b58bbf68be93bcbee24b69a53b0ac3e5de
dawit0905 <dawit0905@gmail.com>
2024-10-16 18:52:47 +0900

    style: 정규표현식 삭제, 문자 클래스로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -7,7 +10,8 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
         if(!isEmpty(input)){
-
+            ArrayList<String> str = splitToArrayList(input);
+            System.out.println(str);
         } else {
             System.out.println("결과 : 0");
         }
@@ -15,5 +19,10 @@ public class Application {
 
     public static boolean isEmpty(String str){
         return str.length() == 0;
+    }
+
+    public static ArrayList<String> splitToArrayList(String input) {
+        String[] splitValues = input.split("[,;]");
+        return new ArrayList<>(Arrays.asList(splitValues));
     }
 }
