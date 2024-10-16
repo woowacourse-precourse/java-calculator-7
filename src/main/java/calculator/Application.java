dd00c6e33ef4b5ff7df6bae112b6946c08d4aa0b
ajroot5685 <anjeong8577@naver.com>
2024-10-17 00:16:10 +0900

    feat(custom): 커스텀 구분자를 포함하는 입력 처리
    전역 상수를 적극 활용하여 로직을 이해하기 쉬운 의사코드로 구성한다.
    - 그러나 이게 과연 맞는 방법인지는 회의감이 든다.
    String의 다양한 메서드를 활용하여 커스텀 구분자가 있는지 확인한다.
    커스텀 구분자가 있는 경우 해당 문자열은 제거한다.
    커스텀 구분자를 추가하기 위해 동적으로 정규표현식을 구성한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,13 +7,41 @@ public class Application {
     private static final String INPUT_GUIDE = "덧셈할 문자열을 입력해 주세요.";
     private static final String OUTPUT_RESULT = "결과 : ";
 
+    private static final String BASIC_DELIMITER_1 = ",";
+    private static final String BASIC_DELIMITER_2 = ":";
+
+    private static final String CUSTOM_DELIMITER_START = "//";
+    private static final String CUSTOM_DELIMITER_END = "\\n";
+
+    private static final String LEFT_SQUARE_BRACKET = "[";
+    private static final String RIGHT_SQUARE_BRACKET = "]";
+    private static final String REGEX_OR_OPERATOR = "|";
+
     public static void main(String[] args) {
         // 입력
         System.out.println(INPUT_GUIDE);
         String input = Console.readLine();
 
+        // 구분자 정의
+        String[] delimiter = new String[3];
+        delimiter[0] = BASIC_DELIMITER_1;
+        delimiter[1] = BASIC_DELIMITER_2;
+
+        // 커스텀 구분자 확인
+        if (input.startsWith(CUSTOM_DELIMITER_START)) {
+            int customEndIndex = input.indexOf(CUSTOM_DELIMITER_END);
+            delimiter[2] = input.substring(2, customEndIndex);
+
+            // 커스텀 구분자 필드 제거
+            input = input.substring(customEndIndex + 2);
+        }
+
+        // 구분자 배열을 정규식 패턴으로 변환
+        // ex. {",", ":", ";"} -> [,|:|;]
+        String regex = LEFT_SQUARE_BRACKET + String.join(REGEX_OR_OPERATOR, delimiter) + RIGHT_SQUARE_BRACKET;
+
         // 숫자 추출
-        String[] split = input.split("[,:]");
+        String[] split = input.split(regex);
         int result = 0;
         for (String strNum : split) {
             result += Integer.parseInt(strNum);
