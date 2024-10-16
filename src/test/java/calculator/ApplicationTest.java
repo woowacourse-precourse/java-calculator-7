1eccf77ddfb02d2a1c2d126e8b29b69b02aaefc7
MinJun Choi <mj043000@naver.com>
2024-10-17 00:04:06 +0900

    test: 테스트 메소드 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -11,16 +11,40 @@ class ApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
         });
     }
 
     @Test
-    void 예외_테스트() {
+    void 길이가_긴_커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//&&\\n1&&2&&3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 네개이상_피연산자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3;4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 음수_예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 미등록_연산자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1;2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
