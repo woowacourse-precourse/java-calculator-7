4700c4d1b656b1340faf1886d25703be3c34c484
sc@t0a14 <gusdn6671@gmail.com>
2024-10-16 21:31:26 +0900

    feat(DelimiterResult) : DelimiterResult 클래스 생성

---
+++ src/main/java/calculator/DelimiterResult.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class DelimiterResult {
+    private String delimiterResult;
+    // 생성자에 구분자가 하나만들어가서 검증하는게 나은거같음 커스텀 구분자나 기본 구분자를 사용하게 되면 생성자가 2개의 파라미터를 가지게되는건 조금  보기안좋음
+    // 차라리 하나의 생성자로 만들어서 가져가자..
+
+
+
+}
