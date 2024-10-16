cbe64fe56ef386162dd199a530e7fb332f811054
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-10-16 22:43:26 +0900

    feat: add user input functionality

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,11 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String Input = Integer.parseInt(br.readLine());
+        System.out.println("덧셈할 문자열을 입력해주세요");
+
+
         // TODO: 프로그램 구현
     }
 }
