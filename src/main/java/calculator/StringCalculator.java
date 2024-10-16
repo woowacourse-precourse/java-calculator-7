9c2cc6752bbda8c001ab49469ed26ebfefe820d6
eb9862 <luclead622@gmail.com>
2024-10-16 16:47:25 +0900

    feat : 문자열 입력 받기

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,12 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class StringCalculator {
 
     void run() {
         printInputMsg();
+        String input = Console.readLine();
     }
 
     void printInputMsg() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
+
 }
