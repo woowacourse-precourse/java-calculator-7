154c2cc5278465f441114603a12e4db6ed6c1447
SongMinhyuk <dnfldpden32@gmail.com>
2024-10-16 22:05:42 +0900

    TEST(CalculatorTest.java): Calculator 클래스 테스트케이스 수정
    기존 테스트케이스 오류 수정

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,23 +1,23 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
     @BeforeEach
     void setUp() {
-        Calculator calculator = new Calculator();
+        calculator = new Calculator("1,2,3");
     }
 
 
     @Test
-    void 계산하기(){
-        String input="1,2,3"
-        calculator.setInput(input);
-        int result=calculator.add();
+    void 계산하기() {
+        int result = calculator.add();
         assertEquals(6, result);
     }
 }
