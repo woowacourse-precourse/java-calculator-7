444612fac29577b5343ea66278a8ca4a600883ed
eb9862 <luclead622@gmail.com>
2024-10-16 19:31:17 +0900

    feat : 커스텀 구분자 확인
    구분자 지정 여부 확인
    - 문자열이 "//"으로 시작하는지 확인
    - "//"으로 시작한다면 "\n"이 존재하는지 확인
    - 커스텀 구분자가 문자 1개인지 확인
    - 커스텀 구분자가 숫자가 아닌지 확인

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,10 +7,37 @@ public class StringCalculator {
     void run() {
         printInputMsg();
         String input = Console.readLine();
+        checkCustomSeparator(input);
     }
 
     void printInputMsg() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
+    void checkCustomSeparator(String input) {
+        int flag = 0;
+        if (beginTwoSlash(input)) {
+            if (!input.contains("\\n")) {
+                flag = 1;
+            } else {
+                if (input.indexOf("\\n") > 3) {
+                    flag = 2;
+                }
+                if (isSeparatorNumeric(input.charAt(2))) {
+                    flag = 3;
+                }
+            }
+        }
+        if (flag != 0) {
+            throw new IllegalArgumentException("커스텀 구분자 에러");
+        }
+    }
+
+    boolean beginTwoSlash(String input) {
+        return input.indexOf("//") == 0;
+    }
+
+    boolean isSeparatorNumeric(char separator) {
+        return Character.isDigit(separator);
+    }
 }
