32e8d65c802667c57a4e02469dbb6bcc4e3a18ed
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-16 23:13:37 +0900

    feat(OutputMessage): 결과 출력 시 사용될 접두사 상수화
    - 덧셈 결과 출력 때 사용될 문구를 enum 클래스에 저장하여 상수화

---
+++ src/main/java/calculator/global/constant/OutputMessage.java
@@ -0,0 +1,17 @@
+package calculator.global.constant;
+
+public enum OutputMessage {
+
+    OUTPUT_PREFIX("결과: ")
+    ;
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
