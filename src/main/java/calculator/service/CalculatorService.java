39e711674ccf8576bf13210f422cd2a82b55f409
haebyun <bhk4392@gmail.com>
2024-10-16 22:48:41 +0900

    refactor(CalculatorService): CalculatorService를 인터페이스로 변경하고, 구현체를 CalculatorServiceImpl로 분리
    - 기존 CalculatorService 클래스를 인터페이스로 변경
    - CalculatorServiceImpl에서 인터페이스 메서드 구현

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,15 +1,7 @@
 package calculator.service;
 
-import calculator.domain.model.Calculator;
-import calculator.domain.model.ExtractedInput;
 import calculator.dto.CalculationResultDTO;
-import calculator.service.util.DelimiterExtractor;
-import calculator.service.util.InputParser;
 
-public class CalculatorService {
-    public CalculationResultDTO calculateSum(String input) {
-        ExtractedInput extractedInput = DelimiterExtractor.extractDelimiters(input);
-        Calculator calculator = Calculator.of(InputParser.parseByDelimiters(extractedInput));
-        return new CalculationResultDTO(calculator.calculateSum());
-    }
+public interface CalculatorService {
+    CalculationResultDTO calculateSum(String input);
 }
---
+++ src/main/java/calculator/service/CalculatorServiceImpl.java
@@ -0,0 +1,15 @@
+package calculator.service;
+
+import calculator.domain.model.Calculator;
+import calculator.domain.model.ExtractedInput;
+import calculator.dto.CalculationResultDTO;
+import calculator.service.util.DelimiterExtractor;
+import calculator.service.util.InputParser;
+
+public class CalculatorServiceImpl implements CalculatorService {
+    public CalculationResultDTO calculateSum(String input) {
+        ExtractedInput extractedInput = DelimiterExtractor.extractDelimiters(input);
+        Calculator calculator = Calculator.of(InputParser.parseByDelimiters(extractedInput));
+        return new CalculationResultDTO(calculator.calculateSum());
+    }
+}
