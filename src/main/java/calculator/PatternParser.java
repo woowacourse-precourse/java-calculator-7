6f4df5471e10e87e77bb10f086aea16e919436e9
lchyeong <lchyeong@gmail.com>
2024-10-17 08:03:27 +0900

    feat(PatternParser): PatternParser 클래스에 패턴 확인 메서드 추가
    - CUSTOM 패턴을 확인하는 isCustomPattern 메서드 추가
    - RESERVED 패턴을 확인하는 isReservedPattern 메서드 추가

--- src/main/java/calculator/PatternParser.java
+++ src/main/java/calculator/PatternParser.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,5 +17,26 @@ public class PatternParser {
     private Matcher toMatcher(String regex, String input){
         return toPattern(regex).matcher(input);
     }
+
+    private boolean isCustomPattern(String input){
+        Matcher matcher = toMatcher(CUSTOM_PATTERN, input);
+        return matcher.find() && (matcher.start() == 0);
+    }
+
+    private boolean isReservePattern(String input){
+        Matcher matcher = toMatcher(RESERVE_PATTERN, input);
+
+        Set<String> delimiters = new HashSet<>();
+        while(matcher.find()){
+            delimiters.add(matcher.group());
+        }
+
+        if (delimiters.contains(",") || delimiters.contains(":")){
+            return true;
+        }
+        return false;
+    }
+
+
 
 }
