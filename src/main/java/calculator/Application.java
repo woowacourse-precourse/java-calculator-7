1661f6ec7d043d6e08d346a864d28ab5623326bb
bk <naturalbb360@gmail.com>
2024-10-16 14:19:50 +0900

    feat(Application): 시작 문구 출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해주세요");
+
     }
 }
