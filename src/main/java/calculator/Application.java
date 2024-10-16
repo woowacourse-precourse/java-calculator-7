d0de1abb53150e26807a740cf2e9255228ba10a6
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-16 13:41:17 +0900

    feat(sum): 숫자 하나 입력시 혹은 여러개 입력시 더하는 기능 추가 숫자 하나를 입력하거나 혹은 여러개를 delimeter를 기준으로 여러개를 입력했을 때 숫자를 더하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,22 +1,41 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String user_input = Console.readLine();
 
-        int answer = 0;
-
-        if (user_input.isEmpty()) {
-            answer = 0;
-        }
-
-
+        int answer = caculateSum(user_input);
 
         System.out.println("결과 : " + answer);
+
+    }
+
+    private static int caculateSum(String input) {
+        if (input == null || input.isEmpty() || input.isBlank()) {
+            return 0;
+        }
+
+        String delimiter = ",|:"; // 기본 구분자
+        String numbers = input;
+
+        String[] tokens = input.split(delimiter);
+
+        int sum = 0;
+        for (String token : tokens) {
+            if (token.isEmpty()) {
+                continue;
+            }
+
+            sum += Integer.parseInt(token);
+        }
+
+        return sum;
     }
 
 }
