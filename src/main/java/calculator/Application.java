a1cfc79d71dae0403b9376f62bcf570d8038e00c
hyunwon <khw73850@gmail.com>
2024-10-16 21:10:21 +0900

    feat: add input functionality

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+
     }
+
 }
