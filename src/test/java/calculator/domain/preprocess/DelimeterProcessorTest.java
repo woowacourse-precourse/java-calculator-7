88d62bcb99d92fe4c72749f57c4cc5bd1c860f72
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-16 23:07:32 +0900

    test(DelimeterProcessorTest): discardCustomDelimeterRequest 테스트 코드 작성

--- src/test/java/calculator/domain/preprocess/DelimeterProcessorTest.java
+++ src/test/java/calculator/domain/preprocess/DelimeterProcessorTest.java
@@ -18,31 +18,38 @@ class DelimeterProcessorTest {
 
     @Test
     @DisplayName("커스텀 구분자 미입력 예외 발생")
-    void no_custom_delimeter(){
-       IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
-               ()->delimeterProcessor.validateCutomDelimeterRequest("//\n2,3,4"));
-       assertEquals(e.getMessage(),"구분자로 사용할 문자 미입력");
+    void no_custom_delimeter() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> delimeterProcessor.validateCutomDelimeterRequest("//\n2,3,4"));
+        assertEquals(e.getMessage(), "구분자로 사용할 문자 미입력");
     }
 
     @Test
     @DisplayName("두 글자 이상의 구분자 지정시 예외 발생")
-    void long_custom_delimeter(){
+    void long_custom_delimeter() {
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
-                ()->delimeterProcessor.validateCutomDelimeterRequest("//;;;\n2,3,4"));
-        assertEquals(e.getMessage(),"구분자로 사용할 문자가 두 글자 이상");
+                () -> delimeterProcessor.validateCutomDelimeterRequest("//;;;\n2,3,4"));
+        assertEquals(e.getMessage(), "구분자로 사용할 문자가 두 글자 이상");
     }
 
     @Test
     @DisplayName("한 글자의 경우 예외 없음")
-    void right_custom_delimeter(){
+    void right_custom_delimeter() {
         assertDoesNotThrow(
-                ()->delimeterProcessor.validateCutomDelimeterRequest("//;\n2,3,4"));
+                () -> delimeterProcessor.validateCutomDelimeterRequest("//;\n2,3,4"));
     }
 
     @Test
     @DisplayName("구분자 추출 테스트")
-    void extract_custom_delimeter(){
+    void extract_custom_delimeter() {
         String delimeter = delimeterProcessor.extractCustomDelimeter("//;\n");
-        assertEquals(delimeter,";");
+        assertEquals(delimeter, ";");
+    }
+
+    @Test
+    @DisplayName("구분자 요청 제외 String 반환 테스트")
+    void discard_custom_delimeter_request() {
+        String newInput = delimeterProcessor.discardCustomDelimeterRequest("//;\n1,2,3");
+        assertEquals(newInput, "1,2,3");
     }
 }
