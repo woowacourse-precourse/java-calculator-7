fe23ff59e2dc87601611dc6d6305bc591719f138
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-10-16 19:19:08 +0900

    feat(core): add calculate function
    add calculate function
    - handle the case of valid input
    - support input of custom delimiters
    - return the sum of numbers separated by delimiters

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,29 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        String inputValue = scanner.nextLine();
+        calculate("1:2,34");
+
     }
+    // 올바른 입력형태 (ex. 11:22,33)
+    // 커스텀 구분자 입력 가능
+    private static int calculate(String input, String... customSep) {
+        String delimeters = ",|:";
+        if (customSep.length > 0) {
+            delimeters += "|" + customSep[0];
+        }
+        String[] arr = input.split(delimeters);
+        int sum = 0;
+        for (String s : arr){
+            sum += Integer.parseInt(s);
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
 }
