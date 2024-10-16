01c9325700ef533f25fd24a70cf16008cd4c6050
Minuring <mw9505@gmail.com>
2024-10-16 18:42:20 +0900

    feat(core): 커스텀 구분자 추출
    계산기 클래스의 생성자에서 커스텀 구분자 여부를 확인하고, 존재한다면
    - 정규 표현식에 커스텀 구분자 추가
    - 계산될 수식에서 커스텀 구분자 정의("//?\\n") 제거

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -10,5 +10,26 @@ public class Calculator {
     public Calculator(String expression) {
         this.regex = "[" + DEFAULT_SEPARATORS + "]";
         this.exp = expression;
+
+        if (hasCustomSeparator()) {
+            this.regex = "[" + DEFAULT_SEPARATORS + exp.charAt(2) + "]";
+            this.exp = exp.substring(5);
+        }
+    }
+
+    private boolean hasCustomSeparator() {
+        return exp.length() >= 5
+                && exp.startsWith("//")
+                && exp.indexOf("\\n") == 3
+                && !isNumeric(exp.charAt(2));
+    }
+
+    private boolean isNumeric(char c) {
+        try {
+            Double.parseDouble(String.valueOf(c));
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
