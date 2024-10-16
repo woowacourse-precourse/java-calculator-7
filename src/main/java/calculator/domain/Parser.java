384f8af6fe84bac6252aaf4f50347efb1b285050
jklee3409 <jklee3409@khu.ac.kr>
2024-10-16 18:18:15 +0900

    refactor(parser) : 구분자 생성 메서드를 분리
    spliInput의 구분자를 생성하는 기능을 makerDelimiter 메서드로 따로 구현함.

--- src/main/java/calculator/domain/Parser.java
+++ src/main/java/calculator/domain/Parser.java
@@ -1,26 +1,30 @@
 package calculator.domain;
 
+import static calculator.domain.Delimiter.getCustomDelimiter;
+
 import java.util.Arrays;
 import java.util.List;
 
-import static calculator.domain.Delimiter.getCustomDelimiter;
-
 public class Parser {
     public static List<String> splitInput(String input) {
-        StringBuilder delimiterBuilder = new StringBuilder("[:,");
-
-        if (getCustomDelimiter(input) != null) {
-            delimiterBuilder.append(getCustomDelimiter(input));
-        }
-
-        String delimiter = delimiterBuilder.append("]").toString();
+        String delimiter = makeDelimiter(input);
 
         String numbers = removeCustomDelimiterPart(input);
 
         return Arrays.asList(numbers.split(delimiter));
     }
 
-    public static String removeCustomDelimiterPart(String input) {
+    private static String makeDelimiter(String input) {
+        StringBuilder delimiterBuilder = new StringBuilder("[:,");
+
+        if (getCustomDelimiter(input) != null) {
+            delimiterBuilder.append(getCustomDelimiter(input));
+        }
+
+        return delimiterBuilder.append("]").toString();
+    }
+
+    private static String removeCustomDelimiterPart(String input) {
         if (Delimiter.getCustomDelimiter(input) != null) {
             int delimiterEndIndex = input.indexOf("\\n") + 1;
 
