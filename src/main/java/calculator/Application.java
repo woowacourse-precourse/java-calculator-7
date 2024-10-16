04bcba20970f60e0398e72161730cdb8f726fe03
LEEJaeHyeok97 <hazardous10@naver.com>
2024-10-16 15:04:56 +0900

    feat: 입력에 쉼표 또는 콜론이 포함 된 경우 합 반환 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 기본(쉼표, 콜론) 구분자로 각 숫자의 합을 반환하는 기능
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        int ans = 0;
+
+        String[] split = str.split("[,:]");
+
+        for (String s : split) {
+            ans += Integer.parseInt(s);
+        }
+
+        System.out.println("결과 : " + ans);
+
     }
 }
