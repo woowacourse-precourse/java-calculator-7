f479982e8cfbf39ca525f35cd84899065468770d
Hyeo_Ni__ <maehyong02@gmail.com>
2024-10-16 19:22:43 +0900

    feat: 빈 문자열 입력 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
+    private static String inputString;
+    private static int total;
     public static void main(String[] args) {
-        System.out.println("Test");
+        Scanner scanner = new Scanner(System.in);
+        inputString = scanner.nextLine();
+
+        if(inputString.isEmpty()) total = 0;
+
+        System.out.println(total);
     }
 }
+
