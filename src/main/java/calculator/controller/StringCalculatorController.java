5759e1001eff526e6ef6bf91397cc92d7090a41a
pakaheo <k70923@konkuk.ac.kr>
2024-10-16 12:13:24 +0900

    refactor : define string addition calculation flow

--- src/main/java/calculator/controller/StringCalculatorController.java
+++ src/main/java/calculator/controller/StringCalculatorController.java
@@ -1,9 +1,14 @@
 package calculator.controller;
 
+import calculator.domain.StringCalculator;
 import calculator.view.InputView;
+import calculator.view.ResultView;
+import camp.nextstep.edu.missionutils.Console;
 
 public class StringCalculatorController {
     public static void startStringCalculator() {
         InputView.promptForAdditionInput();
+        String input = Console.readLine();
+        ResultView.promptForAdditionResult(StringCalculator.calculate(input));
     }
 }
