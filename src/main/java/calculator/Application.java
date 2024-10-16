cd018867e4ec58ed0fef6465a543aae4854759d6
StoneCAU <seokwoo1999@naver.com>
2024-10-16 14:44:15 +0900

    feat: 문자열 덧셈 계산 및 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,11 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static final String ASK_INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    public static final String CALCULATE_RESULT_MESSAGE = "결과 : %d";
 
     public static void main(String[] args) {
         final String input;
+        final int result;
 
         System.out.println(ASK_INPUT_MESSAGE);
         input = Console.readLine();
+
+        result = Calculator.calculate(input);
+        System.out.printf(CALCULATE_RESULT_MESSAGE, result);
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,6 +8,14 @@ public class Calculator {
     private static final String FIRST_DELIMITER = ",";
     private static final String SECOND_DELIMITER = ":";
 
+    public static int calculate(final String input) {
+        List<String> parsedNumbers = parseNumbers(input);
+
+        return parsedNumbers.stream()
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
     private static List<String> parseNumbers(final String input) {
         return List.of(input.split(format("[%s|%s]", FIRST_DELIMITER, SECOND_DELIMITER)));
     }
