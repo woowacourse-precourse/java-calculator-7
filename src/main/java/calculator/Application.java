fb55781ebe4b0aa0b04407e70e20734026ba5540
heejoo <heeju0203@hanyang.ac.kr>
2024-10-16 17:30:55 +0900

    feat(calculator): implement custom delimiter addition
    Add the feature to support custom delimiter in the calculator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,18 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String str = Console.readLine();
-        String[] number = str.split("[,:]+");
+
+        String[] parts = str.split("\\\\n");
+        String delimiter = ",|:";
+        String numbers = parts[0];
+
+        if(numbers.startsWith("//")) {
+            delimiter = delimiter + "|" + numbers.charAt(2);
+            numbers = parts[1];
+        }
+
+        String[] number = numbers.split(delimiter);
+
+
     }
 }
