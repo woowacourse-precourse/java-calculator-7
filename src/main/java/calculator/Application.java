840c855c5f6e052a77a2c00c49fdb136eafd4c47
HanJun Lee <137124830+LeeHanJun00@users.noreply.github.com>
2024-10-16 20:56:10 +0900

    feat: 입력값 쉼표(,) 또는 콜론(:)을 기준으로 분리하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,13 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        String[] splitInputNumber = input.split(",|:");
     }
 }
