0a4d90b58e48c8dbec565df283d8d4e60840c592
양효빈 <hyobin3026@naver.com>
2024-10-16 20:33:41 +0900

    test: TDD 적용 및 테스트케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,13 +8,67 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+
+        assertSimpleTest(() -> {
+            run(",:");
+            assertThat(output()).contains("결과 : 0");
+        });
+
+    }
+
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
         });
+
+        assertSimpleTest(() -> {
+            run("//;\n1;2,3:4");
+            assertThat(output()).contains("결과 : 10");
+        });
+
+        assertSimpleTest(() -> {
+            run("//\n\n1\n23\n5");
+            assertThat(output()).contains("결과 : 29");
+        });
     }
+
+    @Test
+    void 입력값이_없을_때() {
+        assertSimpleTest(() -> {
+            run();
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자가_숫자() {
+        assertSimpleTest(() -> {
+            run("//22\n122322");
+            assertThat(output()).contains("결과 : 4");
+        });
+
+        assertSimpleTest(() -> {
+            run("//22\n222");
+            assertThat(output()).contains("결과 : 2");
+        });
+
+    }
+
 
     @Test
     void 예외_테스트() {
@@ -22,10 +76,57 @@ class ApplicationTest extends NsTest {
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2?3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("@//*\n1*33*1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
+
+//    @Test
+//    void checkType_null_입력_테스트(){
+//        //given
+//        Calculator calculator = new Calculator();
+//
+//        //when&then
+//        assertSimpleTest(() -> {
+//            calculator.checkType(null);
+//            assertThat(output()).contains("결과 : 0");
+//        });
+//
+//    }
+
+    
+//    @Test
+//    void isPositiveNum_예외_테스트(){
+//        //given
+//        Calculator calculator = new Calculator();
+//        String toBeChecked = "-1";
+//
+//        //when & then
+//        assertSimpleTest(() ->
+//                assertThatThrownBy(() -> calculator.isPositiveNum(toBeChecked))
+//                        .isInstanceOf(IllegalArgumentException.class));
+//    }
+
 
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
+
 }
+
+
+
+
