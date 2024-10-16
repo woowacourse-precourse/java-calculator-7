1993a945c3271d02ca53ed8767a04721e3a1d53c
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-16 12:42:12 +0900

    feat(StringParser): 커스텀 구분자 문자열인지, 쉼표(,) 또는 콜론(:) 구분자열인지 판단하여 문자 리스트 반환하는 기능 추가
    커스텀 구분자 문자열인지, 쉼표(,) 또는 콜론(:) 구분자열인지 판단하여 문자 리스트를 반환하는 기능 추가한다. 일급 컬렉션 형태로 변환한다.

--- src/main/java/calculator/domain/StringParser.java
+++ src/main/java/calculator/domain/StringParser.java
@@ -9,28 +9,40 @@ public class StringParser {
     private final static int SECOND_NUMBER = 2;
     private final static int THIRD_NUMBER = 3;
     private final static String STANDARD_DELIMITER = "[,|:]";
+    private static final String FIRST_CUSTOM_STRING = "/";
 
-    private final String inputStrings;
+    private final List<String> numbers;
 
-    public StringParser(final String inputStrings) {
-        this.inputStrings = inputStrings;
+    public StringParser(String inputStrings) {
+        this.numbers = parseStrings(inputStrings);
     }
 
-    private List<String> parseStandardStrings() {
+    public List<String> getNumbers() {
+        return numbers;
+    }
+
+    private List<String> parseStrings(String inputStrings) {
+        if (!paresFirstString(inputStrings).equals(FIRST_CUSTOM_STRING)) {
+            return parseStandardStrings(inputStrings);
+        }
+        return parseCustomStrings(inputStrings);
+    }
+
+    private List<String> parseStandardStrings(String inputStrings) {
         String[] standardStrings = inputStrings.split(STANDARD_DELIMITER);
         return List.of(standardStrings);
     }
 
-    private List<String> parseCustomStrings() {
-        String[] customStrings = inputStrings.split(extractCustomDelimiter());
+    private List<String> parseCustomStrings(String inputStrings) {
+        String[] customStrings = inputStrings.split(extractCustomDelimiter(inputStrings));
         return List.of(customStrings);
     }
 
-    private String extractCustomDelimiter() {
+    private String extractCustomDelimiter(String inputStrings) {
         return inputStrings.substring(SECOND_NUMBER, THIRD_NUMBER);
     }
 
-    private String paresFirstString() {
+    private String paresFirstString(String inputStrings) {
         return inputStrings.substring(INITIAL_NUMBER, FIRST_NUMBER);
     }
 }
