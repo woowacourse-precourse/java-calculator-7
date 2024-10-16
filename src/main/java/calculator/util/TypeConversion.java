5c3fd1fbcb517a7fc0e502b732051fbbccfad2be
tentenball <lth8905@gmail.com>
2024-10-17 02:49:40 +0900

    feat: 숫자 추출을 위한 타입변환 클래스 구현

--- src/main/java/calculator/util/TypeConversion.java
+++ src/main/java/calculator/util/TypeConversion.java
@@ -4,7 +4,7 @@ import calculator.exception.CalculatorException;
 import calculator.exception.message.ExceptionMessage;
 
 public class TypeConversion {
-	public static Long convertStirngToLong(String value) {
+	public static Long convertStringToLong(String value) {
 		try {
 			return Long.parseLong(value);
 		} catch(NumberFormatException e) {
