45435f1d13be427b034af1a8caa3b8d37f928df1
Etwashoeren <sungmin0609@nate.com>
2024-10-16 15:04:33 +0900

    refactor InputMethod -> UserInput으로 이름 리팩터링

--- src/main/java/calculator/UserInput.java
+++ src/main/java/calculator/UserInput.java
@@ -1,8 +1,10 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
-public class InputMethod {
+
+public class UserInput {
 
     public String input() {
+
         return Console.readLine();
     }
 }
