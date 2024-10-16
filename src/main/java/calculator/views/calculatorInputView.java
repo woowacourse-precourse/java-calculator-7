2472aa46a02a7377fc90c74e62178121c063f575
sangxxjin <sj_0368@naver.com>
2024-10-17 00:39:03 +0900

    fix: 라이브러리를 통한 readLine 사용
    본문:
    Scanner 사용 대신 요구 사항에 맞게 Console.readLine()을 사용하도록 수정

--- src/main/java/calculator/views/calculatorInputView.java
+++ src/main/java/calculator/views/calculatorInputView.java
@@ -1,15 +1,14 @@
 package calculator.views;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
+
 
 public class calculatorInputView {
 
-  private static final Scanner sc = new Scanner(System.in);
-
-  public static String readInput() {
-    System.out.println("덧셈할 문자열을 입력해 주세요.");
-    return sc.nextLine();
-  }
+    public static String readInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
 
   public static void displayResult(long result) {
     System.out.println("결과: " + result);
