872f33c6a94f3f837313fab8fa753b60ccfa5bad
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-17 02:51:27 +0900

    feat(ViewManager): printInputMessage 와 printResult 기능 구현

--- src/main/java/calculator/view/ViewManager.java
+++ src/main/java/calculator/view/ViewManager.java
@@ -1,5 +1,19 @@
 package calculator.view;
 
-public class ViewManager {
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class ViewManager {
+    public static final String INPUT_MESSAGE= "덧셈할 문자열을 입력해 주세요.";
+    public static final String RESULT_MESSAGE= "결과 : %d";
+
+    public String printInputMessage(){
+        System.out.println(INPUT_MESSAGE);
+        String input = readLine();
+        return input;
+    }
+
+    public void printResult(Integer sum){
+        System.out.println(String.format(RESULT_MESSAGE,sum));
+    }
 }
