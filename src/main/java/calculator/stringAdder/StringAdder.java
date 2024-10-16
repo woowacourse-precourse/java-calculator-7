fc725fc9eed74b045341e6a6d2df44f8266a17b5
jiffyin7 <jiffyin7@gmail.com>
2024-10-16 16:13:41 +0900

    feat(StringAdder): DelimiterParser 실행 로직 구현
    - DelimiterParser 클래스의 getDelimiters 메서드 호출
    - 예외 발생 시 에러 메시지 출력 하도록 변경
    
    이슈 : #6

--- src/main/java/calculator/stringAdder/StringAdder.java
+++ src/main/java/calculator/stringAdder/StringAdder.java
@@ -1,6 +1,8 @@
 package calculator.stringAdder;
 
 import calculator.stringAdder.inputvalidator.InputValidator;
+import calculator.stringAdder.delimiterParser.DelimiterParser;
+
 
 /**
  * @author : jiffyin7@gmail.com
@@ -10,14 +12,15 @@ public class StringAdder {
   public static void run() {
     try{
       InputValidator inputValidator = new InputValidator();
+      DelimiterParser delimiterParser = new DelimiterParser();
       while (true){
         String input = inputValidator.validateInput();
+        String[] delimeters = delimiterParser.getDelimiters(input);
       }
     } catch (IllegalArgumentException e){
-      System.out.println("잘못된 값을 입력 했습니다.");
+      System.err.println(e.getMessage());
     } finally{
       System.out.println("애플리케이션을 종료합니다.");
     }
-
   }
 }
---
+++ src/main/java/calculator/stringAdder/delimiterParser/DelimiterParser.java
@@ -0,0 +1,12 @@
+package calculator.stringAdder.delimiterParser;
+
+/**
+ * @author : jiffyin7@gmail.com
+ * @since : 24. 10. 16.
+ */
+public class DelimiterParser {
+  
+  public String[] getDelimiters(String input) {
+    return null;
+  }
+}
