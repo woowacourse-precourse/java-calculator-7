4379ff6d4a23516fc9f7cd06609b2d97bec3aae8
CheChe903 <cheche903@naver.com>
2024-10-16 21:32:18 +0900

    feat: 문자열 분리 로직 구현
    문자열을 분리합니다. 이때, 경우의 수는 두가지입니다.
    1. 주어진 구분자 사용
    - 기본적으로 정해진 구분자인 쉼표와 콜론을 사용합니다.
    
    2. 커스텀 구분자 사용
    - 만약 입력이 "//"으로 시작한다면, 커스텀 구분자를 사용합니다. 기능 요구 사항에는 "문자를"이라고 표기되어 있기 때문에, 문자 하나만 커스텀으로 사용합니다.
    - 다만, 예기치 못한 요구사항 변경을 위해 ArrayList에 구분자를 삽입하고, 해당 List의 Size로 예외처리합니다.
    
    이후 split 함수로 문자열을 분리합니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,47 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+
+        ArrayList<Character> sep = new ArrayList<>();
+        sep.add(':');
+        sep.add(',');
+
+        String input = Console.readLine();
+
+        if (input.startsWith("//")) {
+            sep.clear();
+
+            int index = 2;
+
+            while (!(input.charAt(index) == '\\' && input.charAt(index + 1) == 'n')) {
+                index++;
+            }
+
+            for (int i = 2; i < index; i++) {
+                sep.add(input.charAt(i));
+            }
+
+            if (sep.size() != 1) {
+                throw new IllegalArgumentException("커스텀 구분자는 하나여야 합니다.");
+            }
+
+            input = input.substring(index + 2);
+        }
+
+        StringBuilder regex = new StringBuilder();
+        for (char c : sep) {
+            if (!regex.isEmpty()) {
+                regex.append("|");
+            }
+            regex.append("\\").append(c);
+        }
+
+        String[] nums = input.split(regex.toString());
+
     }
 }
