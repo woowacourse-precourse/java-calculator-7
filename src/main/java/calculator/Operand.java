2cf4b6f280db259294360f45e367677bab84557d
Yeongjae Kim <sees77770@gmail.com>
2024-10-16 15:11:32 +0900

    feat: operand에 대한 유효성 검사 구현
    Operand가 생성될 때 문자가 아닌지, 양수인지를 검증하는 메서드를 구현한다.
    - Operand가 스스로 상태를 검증하고 생성하기 위함이다.

--- src/main/java/calculator/Operand.java
+++ src/main/java/calculator/Operand.java
@@ -1,10 +1,28 @@
 package calculator;
 
 public class Operand {
+    private final static String NOT_STRING_MESSAGE = "숫자가 아닌 문자열이 입력되었습니다.";
+    private final static String NOT_POSITIVE_MESSAGE = "입력된 숫자가 양수가 아닙니다.";
+
     private int operand;
 
     public Operand(String element) {
-        this.operand = Integer.parseInt(element);
+        this.operand = parseToInt(element);
+        validatePositive(operand);
+    }
+
+    private int parseToInt(String element) {
+        try {
+            return Integer.parseInt(element);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_STRING_MESSAGE);
+        }
+    }
+
+    private void validatePositive(int operand) {
+        if (operand <= 0) {
+            throw new IllegalArgumentException(NOT_POSITIVE_MESSAGE);
+        }
     }
 
     public int getOperand() {
