9a4b19667a46a58b9cc1d515ea07e2453a049318
gyuho <kyuhokim12@gmail.com>
2024-10-16 15:25:25 +0900

    refactor : Main에 집중 된 로직을 기능에 맞게 분리
    Main에 집중되어 있었던 로직을 Adder, Number, Seperator로 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,50 +1,23 @@
 package calculator;
 
-import java.util.ArrayList;
-import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        ArrayList<String> parser = new ArrayList<String>(List.of(",", ":"));
+        Separator separator = new Separator();
+        Number number = new Number();
+        Adder adder = new Adder();
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        if(input.equals("")){
+        if(input.isEmpty()){
             System.out.println("결과 : 0");
             return;
         }
 
-        if (input.startsWith("//")) {
-            int limitIndex = input.indexOf("\\n");
-            if (limitIndex != -1) {
-                String customParser = input.substring(2, limitIndex);
-                parser.add(customParser);
-                input = input.substring(limitIndex + 2);
-            } else {
-                throw new IllegalArgumentException();
-            }
-        }
-
-        String delimiters = String.join("|", parser);
-        String[] numbers = input.split(delimiters);
-
-        int sum = 0;
-
-        for (String number : numbers) {
-            if (!number.isEmpty()) {
-                try {
-                    int num = Integer.parseInt(number);
-                    if (num < 0) {
-                        throw new IllegalArgumentException();
-                    }
-                    sum += num;
-                } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException();
-                }
-            }
-        }
-        System.out.println("결과 : " + sum);
+        String processedString = separator.addCustomParser(input);
+        number.extractNumbers(processedString, separator.createDelimiterRegex());
+        adder.sumNumbers(number.getExtractedNumbers());
     }
 }
