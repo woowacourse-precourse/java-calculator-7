9aedc55e20919d7c7c0200e4789a758d367ae7a8
김민아 <alsdk9349@naver.com>
2024-10-16 20:48:23 +0900

    feat(Application) : add readLine
    "덧셈할 문자열을 입력해 주세요."라는 문자열 출력.
    Console.readLine();을 사용해서 입력받을 수 있는 코드를 작성.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,11 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String string = Console.readLine();
     }
 }
