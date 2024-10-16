415c4b9e265149051b4fe4808df367860a27f7d2
jijongkwon <help3451@naver.com>
2024-10-16 23:20:07 +0900

    feat(service): InputService 클래스에 사용자 입력 처리 및 검증 로직 구현
    - getValidInput() 메서드에서 InputView의 readInput() 호출하여 사용자 입력 받기
    - 입력값이 비어있는 경우 InvalidInputException 발생
    - validateCustomDelimiter() 메서드에서 입력값이 커스텀 구분자 형식(//구분자\n)인지 검증
     - 구분자가 존재하지 않거나 빈 문자열인 경우 InvalidInputException 발생

--- src/main/java/calculator/service/InputService.java
+++ src/main/java/calculator/service/InputService.java
@@ -1,17 +1,42 @@
 package calculator.service;
 
+import calculator.exception.InvalidInputException;
+import calculator.view.InputView;
+
 public class InputService {
+
+    private final InputView inputView;
+
+    public InputService(InputView inputView) {
+        this.inputView = inputView;
+    }
 
     public String getValidInput() {
         //TODO: 사용자 입력 받기 및 검증 로직 구현
-        return null;
-    }
-
-    private void validateInput(String input) {
-        //TODO: 입력값 검증 로직 구현
+        try{
+            String input = inputView.readInput();
+            validateCustomDelimiter(input);
+            return input;
+        }
+        catch (Exception e){
+            throw new InvalidInputException("입력값이 비어있습니다.");
+        }
     }
 
     private void validateCustomDelimiter(String input) {
         //TODO: 커스텀 구분자 검증 로직 구현
+        if(input.startsWith("//")){
+            int newlineIndex = input.indexOf("\\n");
+
+            if (newlineIndex == -1) {
+                throw new InvalidInputException("\\n 가 존재하지 않습니다.");
+            }
+
+            String delimiter = input.substring(2, newlineIndex);
+
+            if (delimiter.isEmpty()) {
+                throw new InvalidInputException("커스텀 구분자가 존재하지 않습니다.");
+            }
+        }
     }
 }
