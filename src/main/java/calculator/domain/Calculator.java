796cf9f597c91267f03c5c259f375a05aeee7efc
Sinorang <sinholang@naver.com>
2024-10-16 19:36:01 +0900

    feat: 커스텀 구분자 추출 기능 구현
    - 커스텀 구분자를 추출하고, 이를 사용하여 입력된 문자열을 분리하는 로직
      구현
    - 구분자를 이용한 합산 로직을 메서드로 분리

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -7,13 +7,20 @@ public class Calculator {
             return 0;
         }
 
-        String[] numbers = input.split(",|:");
+        if (input.startsWith("//")) {
+            String[] customSplit = input.split("\n", 2);
+            String customDelimiter = customSplit[0].substring(2);
+            return sumNumbers(customSplit[1].split(customDelimiter + "|,|:"));
+        }
 
+        return sumNumbers(input.split(",|:"));
+    }
+
+    private int sumNumbers(String[] numbers) {
         int sum = 0;
         for (String number : numbers) {
             sum += Integer.parseInt(number);
         }
-
         return sum;
     }
 }
