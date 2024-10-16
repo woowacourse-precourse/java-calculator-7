04936da3a8d0315bad1af2eca5bc30d09a070f2c
김동찬 <come753zz@gmail.com>
2024-10-16 16:30:40 +0900

    feat(main class) 사용자 입력 및 Extract클래스 생성
    사용자에게 입력을 받고
    받은 입력에서 숫자만 뽑아낼 Extract클래스를 생성
    Extract에서 가공한 데이터를 받을 문자열 배열 numbers생성
    
     사용자에게 입력을 받으면 Extract에서 숫자만 뽑아내고 이를 String[] 에 넣도록하자

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해주세요");
+        String input= Console.readLine();
+        String[] numbers;
     }
 }
