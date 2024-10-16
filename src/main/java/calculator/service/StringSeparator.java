21afeba733da6b29e4e3439136e3237b9f8a8997
slay <palgalow@gmail.com>
2024-10-16 16:10:16 +0900

    [refactor]: 구분자 조건문 수정

--- src/main/java/calculator/service/StringSeparator.java
+++ src/main/java/calculator/service/StringSeparator.java
@@ -30,7 +30,7 @@ public class StringSeparator {
                 }
                 number += str.charAt(i);
             } else {
-                if (calculator.getSeparators().contains(str.charAt(i))) {
+                if (calculator.getSeparators().contains(String.valueOf(str.charAt(i)))) {
                     if (!number.isEmpty()) {
                         calculator.addNumber(Integer.parseInt(number));
                     }
