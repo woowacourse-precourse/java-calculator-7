6780c00ac8eb3445abe0ae9225a511ac69896383
seahwanKim <kimkimseasea@gmail.com>
2024-10-16 18:36:18 +0900

    feat: 입력값 예외 추가한다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -15,7 +15,15 @@ public class Application {
 
         int sum = 0;
         for (String number : numbers) {
-             sum += Integer.parseInt(number);
+
+            try {
+                if (Integer.parseInt(number) < 0) {
+                    throw new IllegalArgumentException();
+                }
+                sum += Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException();
+            }
         }
         return sum;
     }
