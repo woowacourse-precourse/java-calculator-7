a001c33f05d75487a5fb8a37fc80ec1bb03965ea
woojin <zaza0804@uos.ac.kr>
2024-10-17 02:08:18 +0900

    test($stringChecker): make test about separator function
    - 커스텀 구분자 함수 기능 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,13 +1,53 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import calculator.app.StringChecker;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+    static StringChecker stringChecker = new StringChecker();
+
+    @Test
+    void 커스텀_구분자_식별함수_단위테스트() {
+        assertSimpleTest(() -> {
+            assertThat(stringChecker.checkCustomSeparator("//;\\n1").contains(";"));
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_식별함수_단위테스트_공백() {
+        assertSimpleTest(() -> {
+            StringChecker stringChecker = new StringChecker();
+            assertThat(stringChecker.checkCustomSeparator("// \\n1").contains(" "));
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_식별함수_단위테스트_숫자() {
+        assertSimpleTest(() -> {
+            assertThat(stringChecker.checkCustomSeparator("//5\\n1").contains("5"));
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_식별함수_단위테스트_기본() {
+        assertSimpleTest(() -> {
+            assertThat(stringChecker.checkCustomSeparator("//n1") == List.of(':', ','));
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_식별함수_단위테스트_추가() {
+        assertSimpleTest(() -> {
+            assertThat(stringChecker.checkCustomSeparator("//n\\n") == List.of(':', ',', 'n'));
+        });
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -19,8 +59,8 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
