c113ba32a86aafb0d7f6069f4c068be724b1e5a8
shin <79005737+Shinhisu@users.noreply.github.com>
2024-10-16 16:47:16 +0900

    test: 계산기 동작 test 구현
    개행 문자에 대한 입력 검증 후 정상 동작여부 파악 필요
    model.CalculatorLogic 메서드에 대한 동작 검증 test 작성
    
    Related to: #3

---
+++ src/test/java/calculator/model/CalculatorLogicTest.java
@@ -0,0 +1,50 @@
+package calculator.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorLogicTest {
+    CalculatorLogic calculatorLogic = new CalculatorLogic();
+
+    @Test
+    @DisplayName("기본 구분자 추출 테스트")
+    void testDefaultDelimiter() {
+        String input = "1,2:3";
+        String delimiter = calculatorLogic.extractDelimiter(input);
+        assertThat(delimiter).isEqualTo(",|:");
+    }
+
+    @Test
+    @DisplayName("사용자 정의 구분자 추출 테스트")
+    void testCustomDelimiter() {
+        String input = "//;\n1;2;3";
+        String delimiter = calculatorLogic.extractDelimiter(input);
+        assertThat(delimiter).isEqualTo(";");
+    }
+
+    @Test
+    @DisplayName("기본 구분자 통한 숫자 추출 테스트")
+    void extractDefaultNumbers() {
+        String input = "1,2:3";
+        String delimiter = calculatorLogic.extractDelimiter(input);
+        assertThat(calculatorLogic.extractNumbers(input, delimiter)).isEqualTo(List.of(1, 2, 3));
+    }
+
+    @Test
+    @DisplayName("사용자 정의 구분자 통한 숫자 추출 테스트")
+    void extractCustomNumbers() {
+        String input = "1;2;3";
+        String delimiter = ";";
+        assertThat(calculatorLogic.extractNumbers(input, delimiter)).isEqualTo(List.of(1, 2, 3));
+    }
+
+    @Test
+    @DisplayName("숫자들의 합 계산 테스트")
+    void calculate() {
+        List<Integer> numbers = List.of(1, 2, 3);
+        assertThat(calculatorLogic.calculate(numbers)).isEqualTo(6);
+    }
+}
