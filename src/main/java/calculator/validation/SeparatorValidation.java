ce5ae54679fafedd4100049e0a1fbad04caf17a6
jaeyeong <zzz26lk@gmail.com>
2024-10-16 23:09:24 +0900

    feat: 커스텀 구분자 검증 추가 커스텀 구분자 사용 시 커스텀 구분자와 기본 구분자 외의 특수문자를 사용하는 경우 해당 특수문자는 잘못된 입력으로 예외를 발생한다.

--- src/main/java/calculator/validation/SeparatorValidation.java
+++ src/main/java/calculator/validation/SeparatorValidation.java
@@ -10,6 +10,9 @@ public class SeparatorValidation {
             if (input.indexOf("//") != 0) {
                 throw new IllegalArgumentException("커스텀 구분자는 반드시 앞에 있어야합니다.");
             }
+            if (test(input)) {
+                throw new IllegalArgumentException("잘못된 구분자입니다.");
+            }
         }
     }
 
@@ -35,5 +38,23 @@ public class SeparatorValidation {
 
     private static boolean notContainSpecialCharacters(String input) {
         return !input.matches(".*[^a-zA-Z0-9가-힣,:\\- ].*");
+    }
+
+    private static boolean test(String input) {
+        StringBuilder customPatten = new StringBuilder();
+        String customSeparator = input.substring(input.indexOf("//") + 2, input.indexOf("\\n"));
+        String[] customSeparators = customSeparator.split("");
+        for (String separator : customSeparators) {
+            customPatten.append("\\").append(separator);
+        }
+        String patten = "[^\\w가-힣,:\\-" + customPatten + " ]";
+        input = input.substring(input.indexOf("\\n") + 2);
+        String[] inputStrings = input.split("");
+        for (String inputString : inputStrings) {
+            if (inputString.matches(patten)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
