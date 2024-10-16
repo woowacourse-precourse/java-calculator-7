60b589d53cc60240b5e0208192cec63c0a20fe1d
TroyKang <kdu3840@gmail.com>
2024-10-17 03:34:43 +0900

    test(calculator): revert test method names to Korean
    Reverted test method names back to Korean

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
-    void custom() {
+    void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
@@ -17,7 +17,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void exception() {
+    void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
