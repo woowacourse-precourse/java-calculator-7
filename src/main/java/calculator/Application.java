021aff9cd292298331ae06c67adb88fc1db60d55
hs98012 <hs98012@daum.net>
2024-10-16 16:41:18 +0900

    [feat] README.md 기능 목록 서술 및 기본 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,51 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String str = Console.readLine();
+
+    }
+
+    //숫자 추출함수
+    public static List<Integer> extractNumbers(String str, List<Character> separatorList) {
+        List<Integer> numberList = new ArrayList<>();
+
+        if (str.isEmpty()) {
+            numberList.add(0);
+        } else {
+            StringBuilder regexBuilder = new StringBuilder();
+            regexBuilder.append("[");
+
+            for (Character character : separatorList) {
+                regexBuilder.append(character);
+            }
+            regexBuilder.append("]");
+
+            String[] parts = str.split(regexBuilder.toString());
+            for (String part : parts) {
+                if (!part.isEmpty()) {
+                    try {
+                        int number = Integer.parseInt(part);
+                        numberList.add(number);
+                    } catch (NumberFormatException e) {
+                        System.out.println("잘못된 입력 : " + part);
+                    }
+                }
+            }
+        }
+        return numberList;
+    }
+
+    public static long sumNumbers(List<Integer> numberList) {
+        long sum = 0;
+        for (Integer i : numberList) {
+            sum += i;
+        }
+        return sum;
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,13 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,10 +19,31 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 숫자_추출_테스트() {
+        List<Character> separatorList = new ArrayList<>();
+        separatorList.add(':');
+        separatorList.add(',');
+
+        List<Integer> numbers = Application.extractNumbers("1,2:3", separatorList);
+        assertThat(numbers).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    void 숫자_합계_테스트() {
+        List<Integer> numberList = new ArrayList<>();
+        numberList.add(1);
+        numberList.add(2);
+        numberList.add(3);
+
+        long sum = Application.sumNumbers(numberList);
+        assertThat(sum).isEqualTo(6);
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
