2d8b9833206e2b97482859189e2d4d70b1d9d79f
OhYeHyun <ohyhohyl@naver.com>
2024-10-16 20:35:25 +0900

    feat: 숫자 더하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+    public static int sum(int[] numbers) {
+        return Arrays.stream(numbers).sum();
     }
 }
