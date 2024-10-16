2c477ec88971f28d7fb87a23e6f655f8ee032a07
bk <naturalbb360@gmail.com>
2024-10-16 21:45:48 +0900

    feat(Application): 커스텀 구분자 입력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,16 +4,27 @@ import java.util.Scanner;
 
 public class Application {
 
-    public static void getUserInput() {
+
+    public static String getCustomSeperator(String input) {
+        String customSeperator = "";
+        System.out.println("input:" + input);
+        if (input.contains("\\n") && input.contains("//")) {
+            customSeperator = input.substring(input.indexOf("//") + 2, input.indexOf("\\n"));
+        }
+        return customSeperator;
+    }
+
+    public static String getUserInput() {
         String userInput;
         Scanner scanner = new Scanner(System.in);
         userInput = scanner.nextLine();
         System.out.println(userInput);
+        return userInput;
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해주세요");
-        getUserInput();
+        getCustomSeperator(getUserInput());
     }
 }
