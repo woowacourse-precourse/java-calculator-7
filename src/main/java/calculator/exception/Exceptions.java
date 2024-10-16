31a61ad6d612711b8faea3d52eb419d5369186b5
Teddysir <sdzx0719@naver.com>
2024-10-16 15:45:04 +0900

    feat(exception): 음수 입력시 예외처리

--- src/main/java/calculator/exception/Exceptions.java
+++ src/main/java/calculator/exception/Exceptions.java
@@ -1,22 +1,33 @@
 package calculator.exception;
 
+import java.util.List;
+
 public class Exceptions {
 
     public void validateCommaDelimiter(String input) {
-        if(input.contains(",,")) {
+        if (input.contains(",,")) {
             throw new IllegalArgumentException("[ERROR] 연속된 콤마(,,)는 사용할 수 없습니다.");
         }
     }
 
     public void validateColonDelimiter(String input) {
-        if(input.contains("::")) {
+        if (input.contains("::")) {
             throw new IllegalArgumentException("[ERROR] 연속된 콜론(::)은 사용할 수 없습니다.");
         }
     }
 
     public void validateLeadingDelimiter(String input) {
-        if((input.charAt(0) == ',') || (input.charAt(0) == ':')) {
+        if ((input.charAt(0) == ',') || (input.charAt(0) == ':')) {
             throw new IllegalArgumentException("[ERROR] 문자열의 처음에 구분자가 올 수 없습니다.");
+        }
+    }
+
+    public void validateNegativeNumbers(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            Integer number = numbers.get(i);
+            if (number < 0) {
+                throw new IllegalArgumentException("[ERROR] 음수는 입력할 수 없습니다.");
+            }
         }
     }
 
