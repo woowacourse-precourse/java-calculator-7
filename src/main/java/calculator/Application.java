29dc05a852c19e485872ee9932558fa35f7839d1
JinGoo <zzgg3040@naver.com>
2024-10-16 13:25:33 +0900

    feat: main 에서 계산기 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,9 @@
 package calculator;
 
+import calculator.runner.CalculatorRunner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorRunner.run();
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,13 +19,69 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    @Test
+    void 정상입력_테스트_빈문자열() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 정상입력_테스트_커스텀_복잡() {
+        assertSimpleTest(() -> {
+            run("//|\\n2|5|10");
+            assertThat(output()).contains("결과 : 17");
+        });
+    }
+
+    @Test
+    void 정상입력_테스트_커스텀_스페이스바() {
+        assertSimpleTest(() -> {
+            run("// \\n10 20 30");
+            assertThat(output()).contains("결과 : 60");
+        });
+    }
+
+    @Test
+    void 예외_테스트_공백() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2: "))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_중간_공백() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//|\\n2| |5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_구분자_형식() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//[::]\\n1::2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_숫자_누락() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 }
