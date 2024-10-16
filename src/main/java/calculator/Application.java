51656dca62b309a70afa24ce0a66ec044e7a6750
Jong-Min <codejomo99@gmail.com>
2024-10-16 15:45:49 +0900

    feat: 빈 문자열 처리 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = Console.readLine();
+
+        // 입력이 비어있을 경우 0출력
+        if(input.isEmpty()){
+            System.out.println("결과:"+0);
+            return;
+        }
+
     }
 }
