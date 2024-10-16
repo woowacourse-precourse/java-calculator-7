9dcf9547ad1a6ec9f392e3d637192eccd0632e27
binn <anyubin0724@naver.com>
2024-10-16 10:00:17 +0900

    feat : InputString 함수 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static String inputString() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        // TODO: 예외 처리 메소드 호출 후 에러 메세지 표시
+        return readLine();
     }
 }
