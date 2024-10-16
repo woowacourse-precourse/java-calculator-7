9543c3bb9b7d0100ca2c7707148b5f42b73af564
sunwon <jsw5913@naver.com>
2024-10-16 20:32:09 +0900

    feat(OutputView): 사용자로부터 입력 받기 전 입력문구 출력 기능구현

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,10 @@
+package calculator.view;
+
+public class OutputView {
+
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.\n";
+
+    public void displayInputMessage() {
+        System.out.print(INPUT_MESSAGE);
+    }
+}
