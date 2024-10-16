fbd0ed0c6f1eda1ea115fe1f91205abbfdf7a78c
haeseon lee <haeseon4816@gmail.com>
2024-10-16 19:30:59 +0900

    feat: 커스텀 구분자 계산 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
         User user = new User();
         String userInput = user.inputString();
 
-        int[] basicNumberSeparator = getBasicSeparatorNumbers(userInput);
+        int[] basicNumberSeparator = getCustomSeparatorNumbers(userInput);
 
         for (int i : basicNumberSeparator) {
             result += i;
@@ -17,13 +17,25 @@ public class Calculator {
         System.out.println("결과 : " + result);
     }
 
-    private static int[] getBasicSeparatorNumbers(String userInput) {
-        String[] basicSeparator = userInput.split(",|:");
+    private static int[] getBasicSeparatorNumbers(String input) {
+        String[] basicSeparator = input.split(",|:");
 
         int[] basicNumberSeparator = new int[basicSeparator.length];
         for (int i = 0; i < basicSeparator.length; i++) {
             basicNumberSeparator[i] = Integer.valueOf(basicSeparator[i]);
         }
         return basicNumberSeparator;
+    }
+
+    private static int[] getCustomSeparatorNumbers(String input){
+        char custom = input.charAt(2);
+        String customString = input.substring(5);
+        String[] customSeparator = customString.split(String.valueOf(custom));
+
+        int[] customNumberSeparator = new int[customSeparator.length];
+        for (int i = 0; i < customSeparator.length; i++) { // 숫자는 양수!! 예외 처리
+            customNumberSeparator[i] = Integer.valueOf(customSeparator[i]);
+        }
+        return customNumberSeparator;
     }
 }
