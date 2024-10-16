4fa2c09b6bd16852ad299947fb4d5ba62d97946f
siyoung <wfs0502@gmail.com>
2024-10-17 01:06:14 +0900

    feat: 문자열 입력받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        runCalculator();
+    }
+
+    public static void runCalculator() {
+        String input = getInput();
+    }
+
+    public static String getInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
     }
 }
