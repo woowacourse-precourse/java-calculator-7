f51b728f3107aba0ed2419358eab4fd8bf48f668
Jong-Min <codejomo99@gmail.com>
2024-10-16 16:17:24 +0900

    style: 코드 포맷팅

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,17 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
 
     // 입력 문자열에서 구분자를 추출
-    public static String[] extractSeparators(String input){
+    public static String[] extractSeparators(String input) {
         String[] separators = new String[]{"."}; // . 으로 기본 구분자 설정
-        if(customSeparators(input)){
+        if (customSeparators(input)) {
             // 사용자 정의 구분자가 있는 경우 구분자 추가
-            separators = new String[]{".",input.substring(2,3)};
+            separators = new String[]{".", input.substring(2, 3)};
         }
         return separators;
     }
 
     // 입력 문자열이 사용자 정의 구분자 형식을 포함 하는지 확인하는 메서드
-    public static boolean customSeparators(String input){
+    public static boolean customSeparators(String input) {
         return input.startsWith("//") && input.contains("\\n");
     }
 
@@ -69,21 +69,19 @@ public class Application {
     }
 
 
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
 
         // 입력이 비어있을 경우 0출력
-        if(input.isEmpty()){
-            System.out.println("결과:"+0);
+        if (input.isEmpty()) {
+            System.out.println("결과:" + 0);
             return;
         }
 
         // 구분자 추출
         String[] separators = extractSeparators(input);
-
 
         // 사용자 정의 구분자가 있을 경우 문자열 수정
         if (customSeparators(input)) {
