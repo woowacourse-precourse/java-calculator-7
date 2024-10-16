fd7c51e1a24396fb4d507c26fea8eebc3aea1704
yjin-desktop <yjinlee99@gmail.com>
2024-10-16 20:40:48 +0900

    feat: 문자열 입력 받기

---
+++ src/main/java/calculator/MainProgram.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class MainProgram {
+
+
+    public void mainEntry() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = Console.readLine();
+    }
+}
