014be478f3aa283bb9e62001adf57d1a1e54a123
jijongkwon <help3451@naver.com>
2024-10-17 00:45:37 +0900

    feat(view): InputView 클래스의 안내 메시지 수정
    - readInput() 메서드에서 출력하는 안내 메시지를 "덧셈할 문자열을 입력해 주세요."로 변경

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String readInput() {
-        System.out.println("문자열을 입력해주세요:");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 }
