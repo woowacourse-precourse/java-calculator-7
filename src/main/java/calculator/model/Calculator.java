322c618ff697b06c15f2108bf1e4852c1217f475
qudcks8084 <qudcks8084@naver.com>
2024-10-16 13:29:06 +0900

    feat(model) : implement calcResult method ver1
    - implement calcResult method
    	- separate numbers by default delimiter(, & :)
    	- it doesn't matter about custom delimiter
    	- Ver 2 with custom delimiter is implement at 10.16(Thu)

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -1,7 +1,19 @@
 package calculator.model;
 
+import java.util.StringTokenizer;
+
 public class Calculator {
     public int calcResult(String str){
-        return 0;
+        int calcResult = 0;
+
+        // 10.16 Ver 1
+        // First It doesn't matter about custom delimiter
+        // Just Only , and :
+        StringTokenizer st = new StringTokenizer(str,",:");
+        while (st.hasMoreTokens()){
+            calcResult += Integer.parseInt(st.nextToken());
+        }
+
+        return calcResult;
     }
 }
