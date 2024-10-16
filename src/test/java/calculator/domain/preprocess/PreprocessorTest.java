8ace897e8c9513a25cedc50d708cb96e8cc39fbb
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-17 02:25:57 +0900

    test(PreprocessorTest): preprocessDelimeter 테스트 코드 작성

--- src/test/java/calculator/domain/preprocess/PreprocessorTest.java
+++ src/test/java/calculator/domain/preprocess/PreprocessorTest.java
@@ -2,6 +2,10 @@ package calculator.domain.preprocess;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +22,26 @@ class PreprocessorTest {
                 new DelimeterProcessor(),
                 new InputManager(),
                 new SplitManager());
+    }
+
+    @Test
+    @DisplayName("preprocessDelimeter 예외 발생 테스트")
+    void preprocessDelimeter_test_o(){
+
+        String given = "//3\n2,3,4";
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, ()->{
+            preprocessor.preprocessDelimeter(given);
+        });
+
+        assertEquals(e.getMessage(),"구분자로 숫자를 입력했습니다");
+    }
+
+    @Test
+    @DisplayName("preprocessDelimeter 기능 테스트")
+    void preprocessDelimeter_test_x(){
+
+        String given = "//;\n2,3,4";
+        assertEquals(preprocessor.preprocessDelimeter(given), Arrays.asList(",",":",";"));
     }
 
 }
