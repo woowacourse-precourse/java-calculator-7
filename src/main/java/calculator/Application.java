fc2f0cc781da6bb6e27def9c2ca8212b2d98ba3b
better.gyeom <better.gyeom@gmail.com>
2024-10-16 23:04:18 +0900

    feat: 문자열 입력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = scanner.nextLine();
+
+
+
+        System.out.println("결과: " + input);
+
+
     }
 }
