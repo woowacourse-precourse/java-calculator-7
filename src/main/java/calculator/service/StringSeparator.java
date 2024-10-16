27607b69f2c04cb40c85d17f9bdfc33891446426
slay <palgalow@gmail.com>
2024-10-16 15:43:46 +0900

    [rafactor]: 예외처리 로직 코드 시스템 종료하도록 리팩토링

--- src/main/java/calculator/service/StringSeparator.java
+++ src/main/java/calculator/service/StringSeparator.java
@@ -16,10 +16,15 @@ public class StringSeparator {
         String number = "";
         String divide = "";
         for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
             if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
                 if (!divide.isEmpty()) {
                     if (divide.charAt(0) != '/' || divide.charAt(1) != '/' || divide.charAt(divide.length() - 1) != 'n'|| divide.charAt(divide.length() - 2) != '\\') {
-                        IllegalArgumentException exception = new IllegalArgumentException("잘못된 입력입니다.");
+                        throw new IllegalArgumentException("잘못된 입력입니다.");
+                    } else {
+                        String separator = divide.substring(2, divide.length() - 2);
+                        calculator.addSeparator(separator);
+                        divide = "";
                     }
                 }
                 number += str.charAt(i);
