22936b6a19c4368ea4b8cf3453f685789840c3a9
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-16 12:30:23 +0900

    feat(StringParser): 커스텀 구분자를 추출하는 기능 추가.
    커스텀 구분자만 추출하는 메서드를 추가한다.

--- src/main/java/calculator/domain/StringParser.java
+++ src/main/java/calculator/domain/StringParser.java
@@ -6,6 +6,8 @@ public class StringParser {
 
     private final static int INITIAL_NUMBER = 0;
     private final static int FIRST_NUMBER = 1;
+    private final static int SECOND_NUMBER = 2;
+    private final static int THIRD_NUMBER = 3;
     private final static String STANDARD_DELIMITER = "[,|:]";
 
     private final String inputStrings;
@@ -14,9 +16,13 @@ public class StringParser {
         this.inputStrings = inputStrings;
     }
 
-    private List<String> parseStandardStrings(){
+    private List<String> parseStandardStrings() {
         String[] standardStrings = inputStrings.split(STANDARD_DELIMITER);
         return List.of(standardStrings);
+    }
+
+    private String extractCustomDelimiter(){
+        return inputStrings.substring(SECOND_NUMBER, THIRD_NUMBER);
     }
 
     private String paresFirstString() {
