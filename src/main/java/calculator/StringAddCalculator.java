200108ca3e68ccb7f13493c18725d4d02951e727
uniiii <126189239+fjqmqjrm@users.noreply.github.com>
2024-10-17 01:24:43 +0900

    feat: 빈 문자열이 들어올 경우 0 반환 및 양수 덧셈 계산

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,5 +1,21 @@
 package calculator;
 
-public class StringAddCalculator {
+import java.util.List;
 
+import static java.lang.Integer.sum;
+
+public class StringAddCalculator {
+    public static int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        List<Integer> numbers = InputParser.parse(input);
+
+        return sum(numbers);
+    }
+
+    private static int sum(List<Integer> numbers) {
+        return numbers.stream().mapToInt(Integer::intValue).sum();
+    }
 }
