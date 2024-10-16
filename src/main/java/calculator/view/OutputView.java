b426dd756acbeda9c93528ef347dae199a3eb204
ruudska6 <feels19@naver.com>
2024-10-16 11:43:23 +0900

    <feat>: 사용자에게 입력을 지시하는 메세지 출력 printInputMessage() 메서드

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+    private static final String INPUT_PROMPT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    // private static final String RESULT_PRINT_MESSAGE = "결과 : "
+    public static void printInputPromptMessage() {
+        System.out.println(INPUT_PROMPT_MESSAGE);
+    }
+}
