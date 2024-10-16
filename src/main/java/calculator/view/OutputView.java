67943f031523e43ebba00d789b87ac0666618191
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-10-16 11:08:48 +0900

    refactor: 결과를 Integer가 아닌 int로 받기

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -5,7 +5,7 @@ public class OutputView {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
-    public void printResult(Integer result) {
+    public void printResult(int result) {
         System.out.println("결과 : " + result);
     }
 }
