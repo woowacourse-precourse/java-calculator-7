dbf560d7b16373f344860cf77c02de1da40788c3
rjswjddn <rjswjddn@gmail.com>
2024-10-16 21:43:38 +0900

    feat: 구분자로 문자열에서 숫자 추출 getNumber 메서드
    String.split으ë¡와 구분자 리스트로 숫자 추출
    ì추출한 String숫 자를 ㅑInteger.parseInt로 int로 변환 이때 사용자가 잘못 입력해 에러가 발생하면 IllegalArgumentException throw
    parseInt한 ã숫자 모음인 List<Integer> ã반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,18 +1,21 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        List<String> separator = List.of(",", ":");
         String input;
 
         input = inputString();
+
         List<String> numberStringAndCustomSeparator = findCustomSeparator(input);
-        System.out.println(numberStringAndCustomSeparator);
+        List<String> separators = List.of(",", ":", numberStringAndCustomSeparator.get(1));
+        String numberString = numberStringAndCustomSeparator.get(0);
+
+        getNumber(numberString, separators);
     }
 
     // 문자열 입력
@@ -28,19 +31,47 @@ public class Application {
         int customSeparatorStart = input.indexOf("//");
         int customSeparatorEnd = input.indexOf("\\n");
 
-        if(customSeparatorStart == -1) {
+        if (customSeparatorStart == -1) {
             return List.of(input, "");
-        } else if(customSeparatorStart != 0 || customSeparatorEnd == -1) {
-            throw(new IllegalArgumentException());
+        } else if (customSeparatorStart != 0 || customSeparatorEnd == -1) {
+            throw (new IllegalArgumentException());
         }
 
         String customSeparator = input.substring(2, customSeparatorEnd);
-        String numberString = input.substring(customSeparatorEnd+2);
+        String numberString = input.substring(customSeparatorEnd + 2);
 
         return List.of(numberString, customSeparator);
     }
 
     // 구분자로 문자열에서 숫자 추출
+    public static List<Integer> getNumber(String numberString, List<String> separators) {
+        // numberString이 공백이면 0으로 간주
+        if (numberString.isBlank()) {
+            return List.of(0);
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        // split으로 구분자 기준으로 숫자만 추출
+        String[] numbers = numberString.split(separators.toString());
+
+        // parseInt를 사용해서 String 숫자를 int로 변환
+        // 숫자가 아닐경우 NumberFormatException이 발생함 -> IllegalArgumentException를 throw
+        for (String number : numbers) {
+            int numberInt;
+            try {
+                System.out.println(number);
+                numberInt = Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException();
+            }
+
+            result.add(numberInt);
+        }
+
+        return result;
+    }
+
     // 숫자 합 계산
     // 결과 반환
 }
