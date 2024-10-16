7cc2a6cffb1ac5f521af41e919562aee0946bd3d
DanMac <dan@DanMacui-MacBookPro.local>
2024-10-17 01:48:00 +0900

    feat: add IllegalArgumentException handlings

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -13,17 +13,26 @@ public class Application {
         String input = Console.readLine();
         Console.close();
 
-        if (input.startsWith("//")){
-            String[] input_separated = input.split("//", 2)[1].split("\\\\n", 2);
+        try {
+            if (input.startsWith("//")) {
+                String[] input_separated = input.split("//", 2)[1].split("\\\\n", 2);
 
-            delimiters += input_separated[0];
-            input = input_separated[1];
+                delimiters += input_separated[0];
+                input = input_separated[1];
+            }
+
+            String[] numbers = input.split("[" + delimiters + "]+");
+
+            for (String number:numbers){
+                if (Integer.parseInt(number) <= 0) {
+                    throw new IllegalArgumentException("모든 숫자는 양수여야 합니다.");
+                }
+            }
+
+            int sum = Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
+            System.out.println("결과 : " + sum);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e){
+            throw new IllegalArgumentException("잘못된 값입니다. 원래의 예외: " + e);
         }
-
-        String[] numbers = input.split("[" + delimiters + "]+");
-
-        int sum = Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
-
-        System.out.println("결과 : " + sum);
     }
 }
