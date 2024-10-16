dc41ee1f0f0d4e659ab9bd1ad653fb84934950e2
신관규 <soundbar91@gmail.com>
2024-10-16 22:48:05 +0900

    feat: 문자열 입력 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String calcFormula = readLine();
     }
 }
