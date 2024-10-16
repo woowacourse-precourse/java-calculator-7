0d3ac1a8a21959a5f5fd95e31013807adfc8b359
yush <yush2202@gmail.com>
2024-10-16 21:14:00 +0900

    feat($ErrorMessages): 에러 메시지 추가
    유효하지 않은 구분자 지시자에 따른 에러 메시지 상수 추가

--- src/main/java/calculator/Constants/ErrorMessages.java
+++ src/main/java/calculator/Constants/ErrorMessages.java
@@ -6,7 +6,7 @@ public enum ErrorMessages {
     INVALID_NUMBER_RANGE_MESSAGE("양수만 입력할 수 있습니다."),
     INVALID_NUMBER_MESSAGE("숫자가 아닙니다."),
     INVALID_NUMBERS_EMPTY_MESSAGE("숫자들은 비어있을 수 없습니다."),
-    INVALID_CUSTOM_INDICATOR_MESSAGE("// 및 \\n 이외의 커스텀 구분자 지시자를 사용할 수 없습니다."),
+    INVALID_CUSTOM_INDICATOR_MESSAGE("유효하지 않은 커스텀 구분자 지시자입니다."),
     INVALID_CUSTOM_SEPARATOR_BLANK_MESSAGE("잘못된 커스텀 구분자 입니다.(공백)"),
     INVALID_CUSTOM_SEPARATOR_EMPTY_MESSAGE("잘못된 커스텀 구분자 입니다.(비어있음)"),
     INVALID_CUSTOM_SEPARATOR_DIGIT_MESSAGE("잘못된 커스텀 구분자 입니다.(숫자)");
