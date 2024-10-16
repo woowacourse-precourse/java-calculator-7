4add2d4d0d7099b126833d79b4cd97e235831533
inswal843 <inswal843@gmail.com>
2024-10-16 17:39:20 +0900

    feat : 입력된 문자열을 기본 구분자로 파싱하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,21 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = Console.readLine();
-        System.out.println("결과 : " + userInput);
+//     계산을 수행하는 Calculator 생성
+        Calculator calculator = new Calculator();
+
+        String[] userInputNumbers = calculator.splitInput(userInput);
+
+        for (String userInputNumber : userInputNumbers) {
+            System.out.println(userInputNumber);
+        }
+    }
+}
+
+//계산을 수행하는 클래스 분리
+class Calculator {
+    public String[] splitInput(String userInput) {
+        String delimiter = ",|:"; // 기본 구분자
+        return userInput.split(delimiter);
     }
 }
