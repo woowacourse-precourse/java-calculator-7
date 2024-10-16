7d469ae11414dac379d2221458bde9f237cfeaae
jklee3409 <jklee3409@khu.ac.kr>
2024-10-16 17:55:03 +0900

    feat(calculator) : 빈 문자열 입력시 0을 반환하도록 수정

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,10 +1,15 @@
 package calculator.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Calculator {
     public static int performAddition(String input) {
         List<String> numbers = Parser.splitInput(input);
+
+        if (Objects.equals(input, "")) {
+            return 0;
+        }
 
         if (!isValidNumberString(numbers)) {
             throw new IllegalArgumentException();
@@ -13,9 +18,11 @@ public class Calculator {
         return numbers.stream()
                 .mapToInt(Integer::parseInt)
                 .sum();
+
     }
 
     private static boolean isValidNumberString(List<String> numbers) {
         return numbers.stream().allMatch(num -> num.matches("\\d+"));
     }
+
 }
