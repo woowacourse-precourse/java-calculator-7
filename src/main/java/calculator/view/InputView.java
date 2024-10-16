852f15de88f3a5bf7c8387ae0010299ed96a7f03
binn <anyubin0724@naver.com>
2024-10-16 17:28:42 +0900

    feat : InputView.java 수정
    다른 처리 없이 입력만 받도록 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -2,15 +2,9 @@ package calculator.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class InputView {
-    public static List<Integer> inputString() {
+    public static String inputString() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = readLine();
-        // TODO: 구분자 분리 메소드 구현 후 호출
-        List<Integer> list = new ArrayList<>();
-        return list;
+        return readLine();
     }
 }
