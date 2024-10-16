6c84e2d59a8ff64471d0ed91b861774dadb37a00
haebyun <bhk4392@gmail.com>
2024-10-16 17:42:51 +0900

    feat(Application): FrontController로 전체적인 애플리케이션을 실행할 수 있도록 코드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,7 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        FrontController frontController = new FrontController();
+        frontController.run();
     }
 }
