026baa37c8299a36e73261c4cf9dde4c736ff990
pakaheo <k70923@konkuk.ac.kr>
2024-10-16 12:16:53 +0900

    test : add test case(not exist custom in input & not contains number in input) and use Constants's error message

--- src/test/java/calculator/StringValidatorTest.java
+++ src/test/java/calculator/StringValidatorTest.java
@@ -1,31 +1,45 @@
 package calculator;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import calculator.constants.Constants;
 import calculator.domain.StringCalculator;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class StringValidatorTest {
 
     @Test
-    void 음수_포함_문자열() {
+    void 음수_포함() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> StringCalculator.calculate("-1,2,3"))
-                .withMessageMatching("문자열에 음수가 포함되어있습니다");
+                .withMessageMatching(Constants.CONTAINS_NEGATIVE);
     }
 
     @Test
-    void 기본_구분자_아닌_경우() {
+    void 기본_구분자_아님() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> StringCalculator.calculate("1.2.3"))
-                .withMessageMatching("잘못된 구분자입니다");
+                .withMessageMatching(Constants.WRONG_DELIMITER);
     }
 
     @Test
     void 정의되지_않은_커스텀_구분자() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> StringCalculator.calculate("//;\\n1;2.3"))
-                .withMessageMatching("정의되지 않은 커스텀 구분자가 있습니다");
+                .withMessageMatching(Constants.NOT_DEFINED_CUSTOM);
+    }
+
+    @Test
+    void 커스텀_구분자_없음() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> StringCalculator.calculate("//\\n1;2;3"))
+                .withMessageMatching(Constants.NOT_EXIST_CUSTOM);
+    }
+
+    @Test
+    void 구분자_하나() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> StringCalculator.calculate(";"))
+                .withMessageMatching(Constants.NOT_CONTAINS_NUMBER);
     }
 }
