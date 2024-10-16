52c41a1660118137714bcc5ecaa37cacf2a89316
yunsseong <aster20@hanyang.ac.kr>
2024-10-16 16:44:54 +0900

    feat: 기본 구분자 추가 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,12 +1,19 @@
 package calculator.controller;
 
+import calculator.service.DelimiterService;
 import calculator.view.InputView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CalculatorController {
 
     InputView inputView = new InputView();
+    DelimiterService delimiterService = new DelimiterService();
+    List<String> initialDelimiters = new ArrayList<>(Arrays.asList(",", ":"));
 
     public void run() {
         String userInput = inputView.input("덧셈할 문자열을 입력해 주세요.");
+        delimiterService.addDelimiters(initialDelimiters);
     }
 }
