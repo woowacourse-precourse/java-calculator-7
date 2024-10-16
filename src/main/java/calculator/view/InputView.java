2d910bfa0ae0691381263d8dc846ebdccc6af052
ghktndyd <hwasuyong99@naver.com>
2024-10-16 12:46:08 +0900

    feat(InputView): 입력받은 문자열에 커스텀 구분자가 포함되어 있는지를 검증하는 로직 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public class InputView {
 
+    private static final String COMMA = ",";
+    private static final String COLON = ":";
+
     public List<Integer> getAdditionNumbers() {
         String input = Console.readLine();
 
@@ -14,6 +17,39 @@ public class InputView {
             return Collections.emptyList();
         }
 
+        String separators = getSeparators(input);
+
         return null;
+    }
+
+    private String getSeparators(String input) {
+        if (hasCustomSeparator(input)) {
+            int separatorEndIndex = getSeparatorEndIndex(input);
+            validateCustomSeparator(separatorEndIndex);
+
+            String customSeparator = getCustomSeparator(input, separatorEndIndex);
+
+            return COMMA + "|" + COLON + "|" + customSeparator;
+        }
+
+        return COMMA + "|" + COLON;
+    }
+
+    private boolean hasCustomSeparator(String input) {
+        return input.startsWith("//");
+    }
+
+    private void validateCustomSeparator(int customSeparatorEndIndex) {
+        if (customSeparatorEndIndex == -1) {
+            throw new IllegalArgumentException("입력이 잘못되었습니다.");
+        }
+    }
+
+    private int getSeparatorEndIndex(String input) {
+        return input.indexOf("\n");
+    }
+
+    private String getCustomSeparator(String input, int separatorEndIndex) {
+        return input.substring(2, separatorEndIndex);
     }
 }
