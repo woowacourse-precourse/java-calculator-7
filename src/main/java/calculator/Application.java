9ce3ff72faa7ed6a1f1bbf0ebaf1907496b25aeb
jaeyeong <zzz26lk@gmail.com>
2024-10-16 19:07:26 +0900

    feat: 결과 값 출력 Application객체는 결과를 출력하는 OutputView에 넘겨준다 OutputView는 전달받은 결과를 콘솔에 출력한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,15 +2,16 @@ package calculator;
 
 import calculator.number.Number;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Application {
 
     private final static InputView input = new InputView();
-//    private final static OutputView output = new OutputView();
+    private final static OutputView output = new OutputView();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Number number = new Number(input.readString());
-//        output.printResult(number.getResult());
+        output.printResult(number.getResult());
     }
 }
