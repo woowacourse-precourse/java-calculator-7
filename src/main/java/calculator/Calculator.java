c178ece08ea8e8ccb1f87e65b6a15a6e50d53b2a
sung <salamander011017@gmail.com>
2024-10-16 18:19:45 +0900

    feat : 문자열 입력 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Calculator {
+
+    public static String inputString(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+}
