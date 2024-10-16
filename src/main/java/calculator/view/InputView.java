92715c29324e8e6d710c0e94dbc6f1b03372f5f2
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-16 17:07:37 +0900

    feat(InputView): 입력 값 반환하는 메서드에서 문구 출력
    입력 값을 반환하는 메서드에서 입력 요청 문구 출력한다.

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,6 +1,13 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private static final String CALCULATOR_START_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 
+    public String getInputString() {
+        System.out.println(CALCULATOR_START_MESSAGE);
+        
+        return Console.readLine();
+    }
 }
