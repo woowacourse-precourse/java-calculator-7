fb82ad4440797627e7bdb0e0b3162da4213938c6
Hong, SeokHyeon <tyuiop929@gmail.com>
2024-10-17 02:16:21 +0900

    feat: 입력 문자열을 받아서 합계를 계산.빈 문자열인 경우 0 반환.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,30 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public long add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0L;
+        }
+
+        String[] parts = parseInput(input);
+        return sumNumbers(parts);
+    }
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application calculator = new Application();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();  // 사용자로부터 입력을 받음
+
+        try {
+            long result = calculator.add(input);
+            System.out.println("결과 : " + result);
+        } finally {
+            Console.close();
+        }
     }
 }
