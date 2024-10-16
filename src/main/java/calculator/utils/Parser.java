3c5fa1e5fb23be921a9a9f1003006f794eda2e04
wogus <ind07152@naver.com>
2024-10-16 13:10:10 +0900

    feat(Parser): 커스텀 구분자 파싱 기능 추가
    입력 받은 문자열에서 커스텀 구분자를 getSeparators 메소드를 통해 파싱하여 반환한다.

--- src/main/java/calculator/utils/Parser.java
+++ src/main/java/calculator/utils/Parser.java
@@ -4,6 +4,9 @@ package calculator.utils;
 import static calculator.constant.StandardIndex.FIRST_STANDARD;
 import static calculator.constant.StandardIndex.SECOND_STANDARD;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parser {
 
     // 커스텀 구분자가 있는지 확인하는 메소드
@@ -16,6 +19,17 @@ public class Parser {
             return true;
         }
         throw new IllegalArgumentException();
+    }
+
+    // 구분자를 반환하는 메소드
+    private List<Character> getSeparators(String input) {
+        List<Character> separators = new ArrayList<>();
+        int firstIndex = input.indexOf(FIRST_STANDARD.getStandard());
+        int secondIndex = input.indexOf(SECOND_STANDARD.getStandard());
+        for (int i = firstIndex + 1; i < secondIndex; i++) {
+            separators.add(input.charAt(i));
+        }
+        return separators;
     }
 
 
