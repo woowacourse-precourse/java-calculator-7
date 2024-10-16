34f885ac98412c01813a63eac4927484034ec91e
bowook <mbosx316@naver.com>
2024-10-16 19:31:11 +0900

    feat : PlayerController 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.controller.PlayerController;
+
 public class Application {
     public static void main(String[] args) {
-
+        PlayerController playerController = new PlayerController();
+        playerController.process();
     }
 }
--- src/main/java/calculator/controller/PlayerController.java
+++ src/main/java/calculator/controller/PlayerController.java
@@ -1,4 +1,17 @@
 package calculator.controller;
 
+import calculator.domain.Separator;
+import calculator.view.InputView;
+
 public class PlayerController {
+    private final InputView inputView;
+
+    public PlayerController() {
+        this.inputView = new InputView();
+    }
+
+    public void process() {
+        String numbers = inputView.inputNumbers();
+        Separator separator = new Separator(numbers);
+    }
 }
