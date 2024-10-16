2ceda1c908e68223a9ad1c6030418f50986cb6b2
Minuring <mw9505@gmail.com>
2024-10-16 19:37:23 +0900

    style(application): 공백 제거
    구글 자바 스타일 가이드의 형식대로 공백 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,11 +2,9 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-
 public class Application {
 
     public static void main(String[] args) {
-
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
