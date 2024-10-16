877486d754f1640187d742bb7eac924976de6e68
JuSeong1130 <jsmoon1130@naver.com>
2024-10-16 20:58:25 +0900

    feat(Application) : 문자열 덧셈 기능 구현
    커스텀 구분자와 빈칸에 대한 입력 제외 ,와 :를 포함하여 입력했을때 계산한 값 구하는 기능 구현
    일단 작은 기능부터 만든 후 더 리팩토링 하는 것을 목표로함

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = readLine();
+        int answer = 0;
+        String[] strings = input.split(",|:");
+
+        for (String str : strings) {
+            answer += Integer.parseInt(str);
+        }
+        System.out.printf("결과 : %d", answer);
     }
 }
