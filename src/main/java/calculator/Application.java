635a5dc3c0465016d54f5a7685905b6485dfc565
jijongkwon <help3451@naver.com>
2024-10-17 00:46:09 +0900

    feat(main): Application 클래스에 프로그램 진입점 구현
    - InputView, CalculationService, ParsingService, OutputView 객체 생성
    - StringCalculatorController 객체 생성 및 run() 메서드 호출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import calculator.controller.StringCalculatorController;
+import calculator.service.CalculationService;
+import calculator.service.InputService;
+import calculator.service.ParsingService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        StringCalculatorController stringCalculatorController = new StringCalculatorController(
+                new InputService(new InputView()),
+                new CalculationService(),
+                new ParsingService(),
+                new OutputView()
+        );
+
+        stringCalculatorController.run();
     }
 }
