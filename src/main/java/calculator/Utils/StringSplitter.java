b56381b8f673b451dbe291225d3003c05f8974b6
yush <yush2202@gmail.com>
2024-10-16 18:00:54 +0900

    feat($StringSplitter): 커스텀 구분자 분리 메서드 구현
    커스텀 구분자가 있을 경우 사용할 분리 메서드 구현

--- src/main/java/calculator/Utils/StringSplitter.java
+++ src/main/java/calculator/Utils/StringSplitter.java
@@ -1,5 +1,8 @@
 package calculator.Utils;
 
+import static calculator.Constants.DelimiterConstants.CUSTOM_DELIMITER_END;
+import static calculator.Constants.DelimiterConstants.CUSTOM_DELIMITER_START;
+
 import calculator.Model.Delimiter;
 import java.util.Arrays;
 import java.util.List;
@@ -8,5 +11,9 @@ public class StringSplitter {
     public static List<String> splitByDelimiter(String userInput, Delimiter delimiter) {
         String[] splitedInput = userInput.split(delimiter.getDelimiter());
         return Arrays.stream(splitedInput).toList();
+    }
+
+    public static String getCustomDelimiter(String userInput) {
+        return userInput.substring(userInput.indexOf(CUSTOM_DELIMITER_START), userInput.indexOf(CUSTOM_DELIMITER_END));
     }
 }
