606eecb8341136a5a53360f872c1afea21e239e6
kim <hwiyul_g@daum.net>
2024-10-16 21:20:27 +0900

    feat(Separator): 커스텀 구분 기능 구현 추가
     `//구분자\\n` 형태로 된 것에서 구분자를 꺼내서 그것을 기반으로 split을 한다. 기본 구분 기능과 마찬가지로 ///구분자]]]n같은 잘못된 입력에 대해서 예외를 던진다.

--- src/main/java/calculator/service/Separator.java
+++ src/main/java/calculator/service/Separator.java
@@ -1,16 +1,28 @@
 package calculator.service;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Separator {
     private static final String BASIC_SEPARATOR_COMMA = ",";
     private static final String BASIC_SEPARATOR_COLON = ":";
-
+    private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("//(.+)\\\\n");
 
     public int[] basicSeparate(String content) {
         return Arrays.stream(
                 content.replaceAll(BASIC_SEPARATOR_COLON, BASIC_SEPARATOR_COMMA).split(BASIC_SEPARATOR_COMMA)
         ).mapToInt(this::convertToNumber).toArray();
+    }
+
+    public int[] customSeparate(String content) {
+        Matcher matcher = CUSTOM_SEPARATOR_PATTERN.matcher(content);
+        if (matcher.find()) {
+            String customSeparator = matcher.group(1);
+            return Arrays.stream(content.substring(matcher.end()).split(customSeparator))
+                    .mapToInt(this::convertToNumber).toArray();
+        }
+        throw new IllegalArgumentException("[ERROR] Separator: 타당하지 않은 형식의 커스텀 구분자");
     }
 
     private int convertToNumber(String input) {
