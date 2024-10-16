a17ccd5c38bd1126a2ec441348dbe7bd1d2b12de
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-17 02:20:13 +0900

    refactor: 사용자 입력의 이스케이프 처리 로직을 분리
    - ConsoleInputHandler에서 이스케이프 처리 로직을 분리
    - 이스케이프 처리 책임을 갖는 EscapeNewLineConverter 클래스 생성

--- src/main/java/calculator/io/input/ConsoleInputHandler.java
+++ src/main/java/calculator/io/input/ConsoleInputHandler.java
@@ -1,12 +1,20 @@
 package calculator.io.input;
 
+import calculator.converter.EscapeNewLineConverter;
 import camp.nextstep.edu.missionutils.Console;
 
 public class ConsoleInputHandler implements InputHandler {
 
+    private EscapeNewLineConverter escapeNewLineConverter = new EscapeNewLineConverter();
+
     @Override
     public String getUserInput() {
-        return Console.readLine();
+        String userInput = Console.readLine();
+        return convertEscapeNewLine(userInput);
+    }
+
+    private String convertEscapeNewLine(String input) {
+        return escapeNewLineConverter.convert(input);
     }
 
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,15 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import calculator.converter.EscapeNewLineConverter;
+import calculator.delimiter.DelimiterChecker;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.Scanner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,8 +23,8 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
@@ -28,4 +32,61 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
+    @Test
+    @DisplayName(",는 기본 구분자이다.")
+    void useGeneralDelimiter() {
+        // given
+        String userInput = "1,2,3";
+        userInput = getUserInput(userInput); // 콘솔 환경 적용
+
+        DelimiterChecker delimiterChecker = new DelimiterChecker();
+
+        // when
+        boolean isCustomDelimiter = delimiterChecker.isCustomDelimiter(userInput);
+
+        // then
+        assertThat(isCustomDelimiter).isFalse();
+    }
+
+    @Test
+    @DisplayName(",와 : 모두 기본 구분자이다.")
+    void useGeneralDelimiter2() {
+        // given
+        String userInput = "1,2:3";
+        userInput = getUserInput(userInput); // 콘솔 환경 적용
+
+        DelimiterChecker delimiterChecker = new DelimiterChecker();
+
+        // when
+        boolean isCustomDelimiter = delimiterChecker.isCustomDelimiter(userInput);
+
+        // then
+        assertThat(isCustomDelimiter).isFalse();
+    }
+
+    @Test
+    @DisplayName("//와 \n 사이에 커스텀 구분자를 지정할 수 있다.")
+    void useCustomDelimiter() {
+        // given
+        String userInput = "//t\\n1t2t3";
+        userInput = getUserInput(userInput); // 콘솔 환경 적용
+
+        DelimiterChecker delimiterChecker = new DelimiterChecker();
+
+        // when
+        boolean isCustomDelimiter = delimiterChecker.isCustomDelimiter(userInput);
+
+        // then
+        assertThat(isCustomDelimiter).isTrue();
+    }
+
+    private String getUserInput(String input) {
+        Scanner sc = new Scanner(input);
+        input = sc.nextLine();
+
+        EscapeNewLineConverter escapeNewLineConverter = new EscapeNewLineConverter();
+        return escapeNewLineConverter.convert(input);
+    }
+
 }
