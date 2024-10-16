d738ab2857d04c07f69ee0e9703af7cd0ef43a28
haeseon lee <haeseon4816@gmail.com>
2024-10-16 17:01:12 +0900

    refactor: 기본 구분자 계산 기능 메서드 분리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,17 +8,22 @@ public class Calculator {
         User user = new User();
         String userInput = user.inputString();
 
-        String[] basicSeparator = userInput.split(",|:");
-
-        int[] basicNumberSeparator = new int[basicSeparator.length];
-        for (int i = 0; i < basicSeparator.length; i++) {
-            basicNumberSeparator[i] = Integer.valueOf(basicSeparator[i]);
-        }
+        int[] basicNumberSeparator = getBasicSeparatorNumbers(userInput);
 
         for (int i : basicNumberSeparator) {
             result += i;
         }
 
         System.out.println("결과 : " + result);
+    }
+
+    private static int[] getBasicSeparatorNumbers(String userInput) {
+        String[] basicSeparator = userInput.split(",|:");
+
+        int[] basicNumberSeparator = new int[basicSeparator.length];
+        for (int i = 0; i < basicSeparator.length; i++) {
+            basicNumberSeparator[i] = Integer.valueOf(basicSeparator[i]);
+        }
+        return basicNumberSeparator;
     }
 }
