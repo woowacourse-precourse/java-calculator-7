26dec89e159577a32cc9dd92e4e11d0019c77e3c
haebyun <bhk4392@gmail.com>
2024-10-16 17:37:02 +0900

    feat(OutputView): 클래스 추가
    - CalculationResultDTO를 사용하여 계산 결과를 출력하는 기능 구현
    - ViewMessages의 결과 메시지 형식을 사용하여 결과 출력

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+import calculator.dto.CalculationResultDTO;
+
+public class OutputView {
+    public static void printResult(CalculationResultDTO resultDTO) {
+        System.out.printf(ViewMessages.RESULT_MESSAGE +  resultDTO.result());
+    }
+}
