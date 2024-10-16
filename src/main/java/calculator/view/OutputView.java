873a23e9a1e830f61053f23287f439477bd741ce
jijongkwon <help3451@naver.com>
2024-10-16 21:31:24 +0900

    feat(view): OutputView 클래스 기본 구조 추가
    TODO: printResult와 printErrorMessage 메서드에 실제 출력 로직 구현

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+public class OutputView {
+    public void printResult(int result) {
+        //TODO: 결과 출력 구현
+    }
+
+    public void printErrorMessage(String message) {
+        //TODO: 에러 메시지 출력 구현
+    }
+}
