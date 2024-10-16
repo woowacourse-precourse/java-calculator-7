bde583d684269fa834c1ede2347696fedf6cd897
imscow11253 <seaweed8759@naver.com>
2024-10-16 20:43:30 +0900

    fix : '덧셈할 문자열을 입력해 주세요.'을 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,7 @@ package calculator;
 import java.io.IOException;
 
 import static calculator.InputController.getInputString;
+import static calculator.PrintOutput.printDefaultMessage;
 import static calculator.PrintOutput.printOutput;
 
 public class Application {
@@ -10,6 +11,7 @@ public class Application {
     public static void main(String[] args) throws IOException {
         StringPlusCalculator calculator = new StringPlusCalculator();
 
+        printDefaultMessage();
         Integer result = calculator.plusCalculate(getInputString());
         printOutput(result);
     }
--- src/main/java/calculator/PrintOutput.java
+++ src/main/java/calculator/PrintOutput.java
@@ -5,4 +5,8 @@ public class PrintOutput {
     public static void printOutput(Integer result) {
         System.out.println("결과 : " + result);
     }
+
+    public static void printDefaultMessage(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
 }
