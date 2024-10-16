699662acbc17f723d51c9ebc44051079f2ca1667
jijongkwon <help3451@naver.com>
2024-10-17 00:09:38 +0900

    test(service): ParsingService 클래스에 대한 단위 테스트 구현
    normal
    - 기본 구분자(,|:)로 구분된 숫자 입력 파싱 확인
    - 커스텀 구분자(//;\\n)로 구분된 숫자 입력 파싱 확인
    - 기본 구분자와 커스텀 구분자 혼합 사용 입력 파싱 확인
    
    exception
    - 유효하지 않은 커스텀 구분자 형식 입력 시 InvalidInputException 발생 확인
    - 음수 입력 시 InvalidInputException 발생 확인
    - 유효하지 않은 숫자 형식 입력 시 InvalidInputException 발생 확인

---
+++ src/test/java/calculator/service/ParsingServiceTest.java
@@ -0,0 +1,58 @@
+package calculator.service;
+
+import calculator.exception.InvalidInputException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ParsingServiceTest {
+    private ParsingService parsingService;
+
+    @BeforeEach
+    void setUp(){
+        parsingService = new ParsingService();
+    }
+
+    //======================= normal =======================//
+    @Test
+    void 기본_구분자로_구분된_숫자_파싱() {
+        int[] result = parsingService.parseNumbers("1,2:3");
+        assertArrayEquals(new int[]{1, 2, 3}, result);
+    }
+
+    @Test
+    void 커스텀_구분자로_구분된_숫자_파싱() {
+        int[] result = parsingService.parseNumbers("//;\\n1;2;3");
+        assertArrayEquals(new int[]{1, 2, 3}, result);
+    }
+
+    @Test
+    void 기본_구분자와_커스텀_구분자_혼합_사용() {
+        int[] result = parsingService.parseNumbers("//;\\n1;2,3:4");
+        assertArrayEquals(new int[]{1, 2, 3, 4}, result);
+    }
+
+    //======================= Exception =======================//
+    @Test
+    void 커스텀_구분자_형식_오류시_예외_발생() {
+        assertThrows(InvalidInputException.class, () -> {
+            parsingService.parseNumbers("//\n1,2,3");
+        });
+    }
+
+    @Test
+    void 음수_입력시_예외_발생() {
+        assertThrows(InvalidInputException.class, () -> {
+            parsingService.parseNumbers("1,-2,3");
+        });
+    }
+
+    @Test
+    void 유효하지_않은_숫자_형식_예외_발생() {
+        assertThrows(InvalidInputException.class, () -> {
+            parsingService.parseNumbers("1,2,a");
+        });
+    }
+}
