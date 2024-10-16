8808cb67b7409eacdc7efeb8616f920a727cf21b
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-17 00:48:41 +0900

    feat : 커스텀 구분자가 null인 경우

--- src/main/java/calculator/domain/CustomDividerFinder.java
+++ src/main/java/calculator/domain/CustomDividerFinder.java
@@ -6,6 +6,7 @@ public class CustomDividerFinder {
     private final static Integer OPENER_INDEX = 0;
     private final static Integer CLOSER_INDEX = 3;
     private final static Integer CUSTOM_DIVIDER_INDEX = 2;
+    private final static Integer CALCULATED_VALUE_START_INDEX = 5;
 
     /**
      * 만약 커스텀 문자가 1개가 아닐 경우(null or 문자열) - 커스텀 양식의 길이를 측정
@@ -16,33 +17,49 @@ public class CustomDividerFinder {
 
         if (hasCustomDivider(inputValue)) {
             char charCustomDivider = inputValue.charAt(CUSTOM_DIVIDER_INDEX);
-            System.out.println("문자열" +charCustomDivider);
-            String newValue = inputValue.substring(5);
-            System.out.println("커스텀 양식을 뺀 계산식   " + newValue);
-            calculatedValue.setValue(newValue);
-            //todo 계산값 조정
-            divider.setCustomDivider(String.valueOf(charCustomDivider));
+            String onlyCalculatedValue = extractCalculatedValue(inputValue);
+
+            System.out.println("문자열   " +charCustomDivider);
+            System.out.println("커스텀 양식을 뺀 계산식    " + onlyCalculatedValue);
+
+            updateCalculatedValueAndDivider(calculatedValue, divider, onlyCalculatedValue, charCustomDivider);
             return;
         }
-        throw new IllegalArgumentException("커스텀 구분자 사용시 양식을 지켜주세요. //커스텀문자\n");
+        throw new IllegalArgumentException("커스텀 구분자 사용시 양식을 지켜주세요. //커스텀문자\\n");
+    }
+
+    private String extractCalculatedValue(String inputValue){
+        return inputValue.substring(CALCULATED_VALUE_START_INDEX);
+    }
+
+    private static void updateCalculatedValueAndDivider(CalculatedValue calculatedValue, Divider divider,
+                                                        String onlyCalculatedValue, char charCustomDivider) {
+        calculatedValue.setValue(onlyCalculatedValue);
+        divider.setCustomDivider(String.valueOf(charCustomDivider));
     }
 
     private boolean hasCustomDivider(String inputValue) {
         int openerIndex = inputValue.indexOf(OPENER);
         int closerIndex = inputValue.indexOf(CLOSER);
 
-        System.out.println("오픈 인덱스   " + openerIndex);
-        System.out.println("클로저 인덱스   " + closerIndex);
-        System.out.println();
+        System.out.println("오픈 인덱스   " + openerIndex); //note 0
+        System.out.println("클로저 인덱스   " + closerIndex); //note 3
 
         //todo validate
-        validateChar(closerIndex);
+        validateStringCustomDivider(closerIndex);
+        validateNullCustomDivider(closerIndex);
         return openerIndex == OPENER_INDEX && closerIndex == CLOSER_INDEX;
     }
 
-    private void validateChar(int closerIndex){
+    private void validateStringCustomDivider(int closerIndex){
         if(closerIndex > CLOSER_INDEX){
             throw new IllegalArgumentException("커스텀 구분자는 문자만 입력 가능합니다. 문자열은 불가능 합니다.");
+        }
+    }
+
+    private void validateNullCustomDivider(int closerIndex){
+        if(closerIndex < CLOSER_INDEX){
+            throw new IllegalArgumentException("커스텀 구분자를 문자로 입력해 주세요.");
         }
     }
 
