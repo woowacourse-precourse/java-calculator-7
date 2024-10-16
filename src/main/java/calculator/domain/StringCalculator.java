b35c6c6c82e7b9951caa6cbc84290ce7cb4be12f
pakaheo <k70923@konkuk.ac.kr>
2024-10-16 12:09:33 +0900

    refactor : manage constants, move the responsibility to handle the user input exception / move global constants to 'constants' package / add user input exception(not defined delimiter, not defined custom, only delimiter) and move responsibility handling user input exception to StringValidator

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -1,59 +1,61 @@
 package calculator.domain;
 
+import calculator.constants.Constants;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    private static final int ZERO = 0;
     private static final String DOUBLE_SLASH = "//";
     private static final String ESCAPE = "\\n";
-    private static final String BASIC_DELIMITER = ",|:";
+    private static final String BASIC_DELIMITER = Constants.COMMA + "|" + Constants.COLON;
     private static final String REGEX = "\\//(.)\\\\n(.*)";
     private static final Pattern PATTERN = Pattern.compile(REGEX);
+    private static final int FIRST_LETTER = 0;
     private static final int FIRST = 1;
     private static final int SECOND = 2;
 
     public static int calculate(String input) {
         if (isEmpty(input)) {
-            return ZERO;
+            return Constants.ZERO;
         }
         if (isContainsCustomDelimiter(input)) {
             return calculateSumByMatcher(input);
         }
-        return calculateSum(splitInput(input));
+        if (isContainsBasicDelimiter(input)) {
+            return calculateSum(splitInput(input));
+        }
+
+        StringValidator.wrongDelimiter(input);
+
+        return stringToInteger(input);
     }
 
     private static boolean isEmpty(String input) {
-        return input.isEmpty();
+        return input == null || input.isEmpty();
     }
 
     private static boolean isContainsCustomDelimiter(String input) {
         return input.contains(DOUBLE_SLASH) && input.contains(ESCAPE);
     }
 
+    private static boolean isContainsBasicDelimiter(String input) {
+        return input.contains(Constants.COMMA) || input.contains(Constants.COLON);
+    }
+
     private static int calculateSumByMatcher(String input) {
         Matcher matcher = getMatcher(input);
-        if (canFindRegex(matcher)) {
-            return calculateSum(splitInputByCustomDelimiter(matcher));
-        }
-        // TODO : 예외처리
-        return ZERO;
+
+        StringValidator.checkNotExistCustom(matcher);
+
+        return calculateSum(splitInputByCustomDelimiter(matcher));
     }
 
     private static Matcher getMatcher(String input) {
         return PATTERN.matcher(input);
     }
 
-    private static boolean canFindRegex(Matcher matcher) {
-        return matcher.find();
-    }
-
     private static String[] splitInput(String input) {
-        // TODO : 콤마, 콜론 상수화
-        if (!input.contains(",") && !input.contains(":")) {
-            throw new IllegalArgumentException("잘못된 구분자입니다");
-        }
         return input.split(BASIC_DELIMITER);
     }
 
@@ -65,8 +67,18 @@ public class StringCalculator {
 
     private static String findStringGroup(Matcher matcher, String delimiter) {
         String string = matcher.group(SECOND);
-        // TODO : 커스텀 구분자를 정의한 상태에서, 문자열에 커스텀 구분자가 아닌 다른 구분자가 들어가있다면 어떻게 해야 할까?
+        checkNotDefinedCustom(string, delimiter);
         return string;
+    }
+
+    private static void checkNotDefinedCustom(String string, String delimiter) {
+        char asciiOfDelimiter = delimiter.charAt(FIRST_LETTER);
+        char letter;
+
+        for (int i = 0; i < string.length(); i++) {
+            letter = string.charAt(i);
+            StringValidator.checkNotDefinedCustom(letter, asciiOfDelimiter);
+        }
     }
 
     private static String findDelimiterGroup(Matcher matcher) {
@@ -77,19 +89,14 @@ public class StringCalculator {
         int sum = 0;
         for (String str : strArr) {
             int number = stringToInteger(str);
-            checkNegative(number);
+            StringValidator.checkNumberNegative(number);
             sum += number;
         }
         return sum;
     }
 
     private static int stringToInteger(String string) {
+        StringValidator.checkNotContainsNumber(string);
         return Integer.parseInt(string);
-    }
-
-    private static void checkNegative(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException("문자열에 음수가 포함되어있습니다");
-        }
     }
 }
