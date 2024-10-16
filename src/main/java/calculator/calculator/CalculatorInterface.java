591e7395fc301733b2567d96041d7260342eb2b8
praisebak <praisebak@naver.com>
2024-10-16 14:15:47 +0900

    feat: 구분자 기반 계산기를 위한 인터페이스 추가

---
+++ src/main/java/calculator/calculator/CalculatorInterface.java
@@ -0,0 +1,9 @@
+package calculator.calculator;
+
+public interface CalculatorInterface {
+    public void calculator();
+
+    public void setCustomSplitter(String newSplitter);
+
+    public String getCustomSplitter();
+}
