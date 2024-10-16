4dd6a991757d71b1e7b430013f6904c112458ac6
sung <salamander011017@gmail.com>
2024-10-16 18:21:39 +0900

    feat : 숫자 추출
    - 커스텀 구분자가 없다면 바로 return
    - 있다면 커스텀 구분자로 구분 후 return

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,9 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
 
+    static final String DEFAULT_DELIMITERS = "[,:]"; //정규식 표현
+    static final String CUSTOM_DELIMITER_PREFIX = "//"; //커스텀 구분자를 구별해주는 앞부분
+    static final String CUSTOM_DELIMITER_SUFFIX = "\\n"; //커스텀 구분자를 구별해주는 뒷부분
+
     public static String inputString(){
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
+    }
+
+    public static String [] extractNumber(String input){
+        if(input.startsWith(CUSTOM_DELIMITER_PREFIX)){
+            int delimiterIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+            String customDelimiter = input.substring(CUSTOM_DELIMITER_PREFIX.length(),delimiterIndex);
+            String number = input.substring(CUSTOM_DELIMITER_PREFIX.length()+customDelimiter.length()
+                    +CUSTOM_DELIMITER_SUFFIX.length());
+            return number.split(customDelimiter);
+        }
+        return input.split(DEFAULT_DELIMITERS);
     }
 
 }
