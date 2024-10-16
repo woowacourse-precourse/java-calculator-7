9e223ecc5fb2dc9fc63e591787f2bccb93e4faf8
joshinhyeok <shtree07@naver.com>
2024-10-16 23:28:23 +0900

    test: Calculator에서 분리한 클래스들도 테스트 코드 작성

---
+++ src/test/java/refactor/DelimiterTest.java
@@ -0,0 +1,19 @@
+package refactor;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DelimiterTest {
+    @Test
+    void 기본_구분자를_사용하여_문자열을_분리한다() {
+        Delimiter delimiter = new Delimiter("1,2:3");
+        assertArrayEquals(new String[]{"1", "2", "3"}, delimiter.getDelimitedNumbers());
+    }
+
+    @Test
+    void 커스텀_구분자를_사용하여_문자열을_분리한다() {
+        Delimiter delimiter = new Delimiter("//;\\n1;2;3");
+        assertArrayEquals(new String[]{"1", "2", "3"}, delimiter.getDelimitedNumbers());
+    }
+}
---
+++ src/test/java/refactor/NumberValidatorTest.java
@@ -0,0 +1,24 @@
+package refactor;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class NumberValidatorTest {
+
+    @Test
+    void 음수_검증_테스트() {
+        NumberValidator validator = new NumberValidator();
+        assertThrows(IllegalArgumentException.class, () -> {
+            validator.NegativeOrZeroCheck(new String[]{"-1", "2", "3"});
+        });
+    }
+
+    @Test
+    void zero_검증_테스트() {
+        NumberValidator validator = new NumberValidator();
+        assertThrows(IllegalArgumentException.class, () -> {
+            validator.NegativeOrZeroCheck(new String[]{"0", "2", "3"});
+        });
+    }
+}
