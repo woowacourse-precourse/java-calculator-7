8967b1885db64b112a54b8acec6a64ab6ea3d8c7
yush <yush2202@gmail.com>
2024-10-16 20:49:22 +0900

    feat($ErrorMessages): 예외 메시지 추가
    구분자 예외 메시지를 추가함

--- src/main/java/calculator/Constants/ErrorMessages.java
+++ src/main/java/calculator/Constants/ErrorMessages.java
@@ -3,12 +3,13 @@ package calculator.Constants;
 public enum ErrorMessages {
     ERROR_PREFIX("[ERROR] "),
     INVALID_STRING_MESSAGE("문자열만 입력할 수 있습니다."),
-    INVALID_BLANK_MESSAGE("공백은 입력할 수 없습니다."),
     INVALID_NUMBER_RANGE_MESSAGE("양수만 입력할 수 있습니다."),
+    INVALID_NUMBER_MESSAGE("숫자가 아닙니다."),
     INVALID_NUMBERS_EMPTY_MESSAGE("숫자들은 비어있을 수 없습니다."),
-    INVALID_SEPARATOR_MESSAGE("잘못된 기본 구분자 입니다."),
     INVALID_CUSTOM_INDICATOR_MESSAGE("// 및 \\n 이외의 커스텀 구분자 지시자를 사용할 수 없습니다."),
-    INVALID_CUSTOM_SEPARATOR_MESSAGE("잘못된 커스텀 구분자 입니다.");
+    INVALID_CUSTOM_SEPARATOR_BLANK_MESSAGE("잘못된 커스텀 구분자 입니다.(공백)"),
+    INVALID_CUSTOM_SEPARATOR_EMPTY_MESSAGE("잘못된 커스텀 구분자 입니다.(비어있음)"),
+    INVALID_CUSTOM_SEPARATOR_DIGIT_MESSAGE("잘못된 커스텀 구분자 입니다.(숫자)");
 
     private String message;
 
