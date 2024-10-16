d5dbfc66d2b036025194a7c4acd283a63bff4502
jaeyeong <zzz26lk@gmail.com>
2024-10-17 00:39:33 +0900

    refactor: 숫자 검증 수정 기존에는 숫자를 검증할 때 한글과 영어를 따로 검사하였다. 그러나 불필요한 코드가 존재하고 해당 메서드를 두번 봐야하기 문제가 발생한다. 따라서 하나의 정규표현식으로 숫자 외의 모든 문자를 판별 가능하기에 변경한다.

--- src/main/java/calculator/validation/NumberValidation.java
+++ src/main/java/calculator/validation/NumberValidation.java
@@ -3,17 +3,13 @@ package calculator.validation;
 public class NumberValidation {
 
     public static void validation(String input) {
-        if (isKorean(input.trim()) || isAlphabet(input.trim()) || isNotPositiveNumber(input.trim())) {
+        if (isNotNumber(input.trim()) || isNotPositiveNumber(input.trim())) {
             throw new IllegalArgumentException("양수만 입력 가능합니다.");
         }
     }
 
-    private static boolean isKorean(String input) {
-        return input.matches("[가-힣]+");
-    }
-
-    private static boolean isAlphabet(String input) {
-        return input.matches("[a-zA-Z]+");
+    private static boolean isNotNumber(String input) {
+        return input.matches("[\\D]+");
     }
 
     private static boolean isNotPositiveNumber(String input) {
