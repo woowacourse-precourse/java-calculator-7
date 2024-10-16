9eeed0368d070e7d92d81d28aacaf16434e7406c
최창환 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-16 10:15:56 +0900

    feat: 시작 문구 출력 기능 추가

---
+++ src/main/java/calculator/handler/PrintHandler.java
@@ -0,0 +1,9 @@
+package calculator.handler;
+
+public class PrintHandler {
+    private static final String PRINT_START_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public void printStartMessage() {
+        System.out.println(PRINT_START_MESSAGE);
+    }
+}
