e04f9f60a205f30644d47e7bea6d4e100bad103d
sangxxjin <sj_0368@naver.com>
2024-10-16 23:59:12 +0900

    feat: CalculatorInputView 클래스와 readInput 메서드 구현
    - calculator 디렉토리 / views / calculatorInputView로 파일 경로 설정
    - Scanner를 static final로 선언하여 상수화함

---
+++ src/main/java/calculator/views/calculatorInputView.java
@@ -0,0 +1,14 @@
+package calculator.views;
+
+import java.util.Scanner;
+
+public class calculatorInputView {
+
+  private static final Scanner sc = new Scanner(System.in);
+
+  public static String readInput() {
+    System.out.println("덧셈할 문자열을 입력해 주세요.");
+    return sc.nextLine();
+  }
+
+}
