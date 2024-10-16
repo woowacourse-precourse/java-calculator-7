a18aa3eb3d6887b1fcf6f64318b2689d153388da
변우영 <likevangogh24@gmail.com>
2024-10-16 18:25:57 +0900

    feat(InputSplitter): 문자열을 커스텀 구분자를 기준으로 분리하는 기능 구현

--- src/main/java/calculator/domain/InputSplitter.java
+++ src/main/java/calculator/domain/InputSplitter.java
@@ -1,7 +1,30 @@
 package calculator.domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputSplitter {
+    private static final String REGEX = "^//(.)\\n";
+    private static final int CUSTOM_DELIMETER_START_INDEX = 2;
+    private static final int CUSTOM_DELIMETER_END_INDEX = 3;
+    private static final int START_INDEX_EXCEPT_REGEX = 4;
+
     public String[] splitByDelimeter(String input) {
+        if (containsCustomDelimeter(input)) {
+            String customDelimeter = getCustomDelimeter(input);
+            input = input.substring(START_INDEX_EXCEPT_REGEX);
+            return input.split(customDelimeter);
+        }
         return input.split("[,:]");
+    }
+
+    private boolean containsCustomDelimeter(String input) {
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
+
+    private String getCustomDelimeter(String input) {
+        return input.substring(CUSTOM_DELIMETER_START_INDEX, CUSTOM_DELIMETER_END_INDEX);
     }
 }
