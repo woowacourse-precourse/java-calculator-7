e15b98e868fb5ec069cc0da6d150b28ea8c6fd08
장혜민 <gpals5987@gmail.com>
2024-10-16 20:58:01 +0900

    [feat] CalculateController 클래스 구현
      1. 생성자에서 필요한 객체들 초기화
         - Separator, Numbers, CalculateService, SplitService 인스턴스 생성
      2. process 메서드 구현
         - 입력 문자열 길이 검증
         - 커스텀 구분자 형식 확인 및 처리
         - 숫자 문자열 분리 및 유효성 검사
         - 계산 수행 및 결과 반환

--- src/main/java/calculator/controller/CalculateController.java
+++ src/main/java/calculator/controller/CalculateController.java
@@ -1,40 +1,51 @@
 package calculator.controller;
 
+import calculator.constant.ValidConstants;
 import calculator.model.Numbers;
 import calculator.model.Separator;
+import calculator.service.CalculateService;
+import calculator.service.SplitService;
 import calculator.validator.InputValid;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class CalculateController {
     private Separator sep;
-    private Numbers numbers;
+    private Numbers num;
+    private CalculateService calculateService;
+    private SplitService splitService;
+
     public CalculateController() {
         this.sep = new Separator();
-        this.numbers = new Numbers();
+        this.num = new Numbers();
+        this.calculateService = new CalculateService();
+        this.splitService = new SplitService();
     }
 
-    public void input(String input){
+    public String process(String input){
         InputValid.checkStringLength(input);
 
-        if(InputValid.checkStartWithCustomSeparator(input)){
-            Matcher matcher = separateInput(input);
-            String separator = matcher.group(1);
-            InputValid.checkCustomSeparatorLength(separator);
+        String numbers = input;
+
+        if(InputValid.isValidCustomSeparatorFormat(input)){
+            String[] splitStr = splitService.extractCustomSeparatorAndNumbers(input);
+            numbers = splitStr[1];
+            String separator = splitStr[0];
+            sep.registerSeparator(separator);
         }
+
+        List<String> numbersList = splitService.splitNumber(sep, numbers);
+        InputValid.checkNumberCount(numbersList.size());
+        numbersList.forEach(num::addNumber);
+
+        return calculateService.calculate(num.getNumbers());
     }
 
-    public Matcher separateInput(String input){
-        String regex = "^//(.*?)\n(.*)";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
-        return matcher;
-    }
 
 
-    public String extractSeparator(String input){
-
-    }
 
 }
