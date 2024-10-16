96dd0a2fb5e6f427181032cfe182ff078a13e7de
googoo9918 <1013as@naver.com>
2024-10-16 15:32:54 +0900

    test(InputHandlerTest): InputHandler 테스트 클래스 구현
    - NsTest를 기반으로 InputHandler의 readInput 메서드 테스트
    - assertSimpleTest와 assertThat을 사용하여 입력값 테스트

---
+++ src/test/java/calculator/InputHandlerTest.java
@@ -0,0 +1,26 @@
+package calculator;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import calculator.controller.InputHandler;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+
+public class InputHandlerTest extends NsTest{
+
+  @Test
+  void 입력값_테스트(){
+    assertSimpleTest(() -> {
+      run("1,2:3");
+
+      InputHandler inputhandler = new InputHandler();
+      String result = inputhandler.readInput();
+
+      assertThat(result).isEqualTo("1,2:3");
+    });
+  }
+  @Override
+  public void runMain(){}
+}
