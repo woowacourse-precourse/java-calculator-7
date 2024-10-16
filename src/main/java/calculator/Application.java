3ad404210d64589312165cf071c90b9f5848b510
heejoo <heeju0203@hanyang.ac.kr>
2024-10-16 18:05:29 +0900

    feat(calculator): add exception handling for invalid input value
    Add functionality to handle exception for non-positive numbers

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,27 +4,34 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String str = Console.readLine();
+        try {
+            String str = Console.readLine();
 
-        String[] parts = str.split("\\\\n");
-        String delimiter = ",|:";
-        String numbers = parts[0];
+            String[] parts = str.split("\\\\n");
+            String delimiter = ",|:";
+            String numbers = parts[0];
 
-        if(numbers.startsWith("//")) {
-            delimiter = delimiter + "|" + numbers.charAt(2);
-            numbers = parts[1];
+            if (numbers.startsWith("//")) {
+                delimiter = delimiter + "|" + numbers.charAt(2);
+                numbers = parts[1];
+            }
+
+            String[] number = numbers.split(delimiter);
+
+            int[] num = new int[number.length];
+
+            int sum = 0;
+
+            for (int i = 0; i < number.length; i++) {
+                int value = Integer.parseInt(number[i]);
+
+                if(value <= 0) throw new IllegalArgumentException("Invalid input value!");
+
+                num[i] = value;
+                sum += num[i];
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
-
-        String[] number = numbers.split(delimiter);
-
-        int[] num = new int[number.length];
-
-        int sum = 0;
-
-        for(int i = 0; i < number.length; i++) {
-            num[i] = Integer.parseInt(number[i]);
-            sum += num[i];
-        }
-
     }
 }
