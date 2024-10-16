bbc669b962599a721fbb03e9239c3c8cd4241684
신관규 <soundbar91@gmail.com>
2024-10-16 23:54:53 +0900

    feat: 커스텀 구분자 추출 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,9 +2,28 @@ package calculator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String calcFormula = readLine();
+        String customSeparator = extractCustomSeparator(calcFormula);
+    }
+
+    public static String extractCustomSeparator(String calcFormula) {
+        String regex = "/{2}.\\\\n";
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(calcFormula);
+
+        StringBuilder customSeparator = new StringBuilder();
+
+        while (matcher.find()) {
+            customSeparator.append(matcher.group().charAt(2));
+        }
+
+        return customSeparator.toString();
     }
 }
