c90aba790f475065546b272f05b836e102912a42
조혜원 <johyewon@johyewon-ui-MacBookAir.local>
2024-10-16 15:07:29 +0900

    feat: main 메소드 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,34 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        String customSeperator = "";
+
+        if (isCustomSeperator(input)) customSeperator = extractCustomSeperator(input);
+
+        String[] number = splitBySeperator(input, customSeperator);
+
+        int result = sumCalculate(number);
+        System.out.println("결과 : " + result);
+    }
+
+    public static boolean isCustomSeperator(String input) {
+        return true;
+    }
+
+    public static String extractCustomSeperator(String input) {
+        return null;
+    }
+
+    public static String[] splitBySeperator(String input, String customSeperator) {
+        return null;
+    }
+
+    public static int sumCalculate(String[] number) {
+        return 0;
     }
 }
