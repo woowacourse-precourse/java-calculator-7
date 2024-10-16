d76a0bfa23ec1f71b90d6827e6ea678abf86b285
Yoon-Jemin <yjm1800@naver.com>
2024-10-16 15:19:44 +0900

    docs(readme): 기능 목록을 포함한 리드미 파일 수정
    기능 요구사항을 분석하고 리드미에 구현할 기능 목록을 정리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import java.util.HashMap;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
+
+    public static Set<String> seperators;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 1. "," ";" 구분자 추가
+        init();
+
+
+    }
+
+    private static void init() {
+        seperators = new HashSet<>();
+        seperators.add(",");
+        seperators.add(";");
     }
 }
