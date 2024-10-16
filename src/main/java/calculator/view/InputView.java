34120637441f03e3cdd3eb06843a0fb50afe64fa
star-candy <el0710@naver.com>
2024-10-16 21:44:57 +0900

    refactor(view): 사용자 입력 요구 문장 수정
    사용자 입력 요구 문장 개행 미포함 발견
    view.InputView 사용자 입력 요구 문장 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
     private static final String INPUT_MESSAGE_REQUEST = "덧셈할 문자열을 입력해 주세요.";
 
     public String inputMessage() {
-        System.out.print(INPUT_MESSAGE_REQUEST);
+        System.out.println(INPUT_MESSAGE_REQUEST);
         return Console.readLine();
     }
 }
