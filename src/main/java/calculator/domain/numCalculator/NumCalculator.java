13f054771d3966315140850787e8ba9dd5871a57
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-17 02:44:09 +0900

    feat(NumCalculator): calculate 기능 구현

--- src/main/java/calculator/domain/numCalculator/NumCalculator.java
+++ src/main/java/calculator/domain/numCalculator/NumCalculator.java
@@ -1,4 +1,14 @@
 package calculator.domain.numCalculator;
 
+import java.util.List;
+
 public class NumCalculator {
+
+    public Integer calculate(List<String> numbers){
+        Integer asnwer = 0;
+        for(String num:numbers){
+            asnwer+=Integer.parseInt(num);
+        }
+        return asnwer;
+    }
 }
