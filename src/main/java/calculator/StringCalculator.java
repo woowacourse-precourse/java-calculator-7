c6a4c42bfa4ec24cf80be7909b06c9020346255d
eb9862 <luclead622@gmail.com>
2024-10-16 18:31:58 +0900

    Revert "feat : 커스텀 구분자 확인"
    This reverts commit 5541484ad2b102206904cf8e0aecb5ac5c0b9c5d.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,39 +7,10 @@ public class StringCalculator {
     void run() {
         printInputMsg();
         String input = Console.readLine();
-        checkCustomSeparator(input);
-
     }
 
     void printInputMsg() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
-    void checkCustomSeparator(String input) {
-        int flag = 0;
-        if (beginTwoSlash(input)) {
-            if (!input.contains("\\n")) {
-                flag = 1;
-            } else {
-                if (input.indexOf("\\n") > 3) {
-                    flag = 2;
-                }
-                if (isSeparatorNumeric(input.charAt(2))) {
-                    flag = 3;
-                }
-            }
-        }
-        if (flag != 0) {
-            //System.out.println(flag);
-            throw new IllegalArgumentException("커스텀 구분자 에러");
-        }
-    }
-
-    boolean beginTwoSlash(String input) {
-        return input.indexOf("//") == 0;
-    }
-
-    boolean isSeparatorNumeric(char separator) {
-        return Character.isDigit(separator);
-    }
 }
