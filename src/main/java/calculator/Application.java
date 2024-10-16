addc728170d48ae25c2e540fcbb352dfead369a2
jeonghyeon-ohk <ok6737@naver.com>
2024-10-16 21:31:04 +0900

    feat(application): main 메서드에 프로그램 실행 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,20 @@
 package calculator;
 
 import calculator.controller.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        Calculator.run();
+
+        // 1. 사용자로부터 입력받은 값을 저장
+        String input = InputView.getInput();
+
+        // 2. 입력값에 대한 계산 실행
+        Calculator calculator = new Calculator();
+        int result = Calculator.calculate(input);
+
+        // 3. 계산 결과 출력
+        OutputView.printResult(result);
     }
 }
