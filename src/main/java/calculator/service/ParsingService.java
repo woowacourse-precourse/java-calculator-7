5ee21c0f76733c45a8922809c068e4bcfdb26644
jijongkwon <help3451@naver.com>
2024-10-16 21:34:13 +0900

    feat(service): ParsingService 클래스 기본 구조 추가
    - parseNumbers: 입력 문자열을 숫자 배열로 파싱
    - splitString: 문자열 분리
    - extractCustomDelimiter: 커스텀 구분자 추출
    
    TODO: 각 메서드에 실제 파싱 및 문자열 처리 로직 구현

---
+++ src/main/java/calculator/service/ParsingService.java
@@ -0,0 +1,18 @@
+package calculator.service;
+
+public class ParsingService {
+    public int[] parseNumbers(String input) {
+        //TODO: 입력 문자열 파싱 로직 구현
+        return null;
+    }
+
+    private String[] splitString(String input) {
+        //TODO: 문자열 분리 로직 구현
+        return null;
+    }
+
+    private String extractCustomDelimiter(String input) {
+        //TODO: 커스텀 구분자 추출 로직 구현
+        return null;
+    }
+}
