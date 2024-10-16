5b987af9ad131d26e7a81129dd9a71ebb37f293d
pakaheo <k70923@konkuk.ac.kr>
2024-10-16 12:12:34 +0900

    feat : create ResultView to print string addition result

--- src/main/java/calculator/view/ResultView.java
+++ src/main/java/calculator/view/ResultView.java
@@ -1,2 +1,7 @@
-package calculator.controller;public class ResultView {
+package calculator.view;
+
+public class ResultView {
+    public static void promptForAdditionResult(int result) {
+        System.out.print("결과 : " + result);
+    }
 }
