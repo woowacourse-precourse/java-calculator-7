98ebb0b42700785871546dc764be113edecb3a36
joonwan <joonwan1024@naver.com>
2024-10-16 16:57:44 +0900

    feat: checkValidCustomFormat 메서드 구현

--- src/main/java/calculator/DataParser.java
+++ src/main/java/calculator/DataParser.java
@@ -3,7 +3,16 @@ package calculator;
 import calculator.dto.Data;
 
 public class DataParser {
-    public static DataParser parseData(String inputData, boolean isCustom) {
+    public static DataParser parseData(String inputData, boolean isCustom) throws IllegalAccessException {
+        if (isCustom) {
+            checkValidCustomFormat(inputData);
+        }
         return new Data();
+    }
+
+    private static void checkValidCustomFormat(String inputData) throws IllegalAccessException {
+        if (!inputData.contains("\n")) {
+            throw new IllegalAccessException();
+        }
     }
 }
