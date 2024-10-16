7ceb490bb851aceb7b1b18257a877d9e60b537ab
jiffyin7 <jiffyin7@gmail.com>
2024-10-17 02:25:51 +0900

    feat(StringAdder): NumberParser 실행 로직 구현
    - NumberParser 클래스의 getNumbers 메서드 호출
    - Long 타입 범위를 넘어가는 숫자도 추출할 수 있도록 String 형태로 반환
    
    이슈 : #8

--- src/main/java/calculator/stringAdder/StringAdder.java
+++ src/main/java/calculator/stringAdder/StringAdder.java
@@ -2,6 +2,7 @@ package calculator.stringAdder;
 
 import calculator.stringAdder.inputvalidator.InputValidator;
 import calculator.stringAdder.delimiterParser.DelimiterParser;
+import calculator.stringAdder.numberParser.NumberParser;
 
 
 /**
@@ -13,9 +14,11 @@ public class StringAdder {
     try{
       InputValidator inputValidator = new InputValidator();
       DelimiterParser delimiterParser = new DelimiterParser();
+      NumberParser numberParser = new NumberParser();
       while (true){
         String input = inputValidator.validateInput();
         String[] delimeters = delimiterParser.getDelimiters(input);
+        String[] numbers = numberParser.getNumbers(input, delimeters);
       }
     } catch (IllegalArgumentException e){
       System.err.println(e.getMessage());
---
+++ src/main/java/calculator/stringAdder/numberParser/NumberParser.java
@@ -0,0 +1,12 @@
+package calculator.stringAdder.numberParser;
+
+/**
+ * @author : jiffyin7@gmail.com
+ * @since : 24. 10. 17.
+ */
+public class NumberParser {
+
+  public String[] getNumbers(String input, String[] delimeters) {
+    return null;
+  }
+}
