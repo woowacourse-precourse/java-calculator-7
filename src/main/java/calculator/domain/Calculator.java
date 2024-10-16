233eaad0c88fbdb10e94d65aee7a9ea9ef2b85d9
fbehddn <tom0611@naver.com>
2024-10-16 22:42:04 +0900

    feat: 커스텀 구분자의 존재를 확인하는 메서드 구현

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     private boolean isInputEmpty(String input) {
         return input == null || input.isEmpty();
     }
+
+    private boolean isCustomSeparator(String input) {
+        return input.startsWith("//");
+    }
 }
