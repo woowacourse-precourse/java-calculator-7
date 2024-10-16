a222ddbdd285548900288ea14d986c298ebcc4e0
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-17 00:13:27 +0900

    feat(SplitManager): discardBlankSpace 기능 구현

---
+++ src/main/java/calculator/domain/preprocess/SplitManager.java
@@ -0,0 +1,13 @@
+package calculator.domain.preprocess;
+
+import java.util.List;
+
+public class SplitManager {
+
+    public List<String> discardBlankSpace(List<String> input){
+        input.removeIf(String::isBlank);
+        return input
+    }
+
+
+}
