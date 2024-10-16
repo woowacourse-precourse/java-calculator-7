c3e719ff1f192628a24d7fbc5df21359f7a6f3d8
siyoung <wfs0502@gmail.com>
2024-10-17 02:43:04 +0900

    feat: 구분자 기준으로 문자열 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,10 +9,51 @@ public class Application {
 
     public static void runCalculator() {
         String input = getInput();
+        String[] strings = splitInput(input);
+        int[] numbers = parseToIntArray(strings);
     }
 
     public static String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
+    }
+
+    private static String[] splitInput(String input) {
+        if (isCustomDelimiter(input)) {
+            String customDelimiter = extractCustomDelimiter(input);
+            String numbersPart = input.substring(input.indexOf("\\n") + 2);
+            return numbersPart.split(customDelimiter);
+        } else {
+            return input.split("[,:]");
+        }
+    }
+
+    public static boolean isCustomDelimiter(String input) {
+        return input.startsWith("//") && input.contains("\\n");
+    }
+
+    public static String extractCustomDelimiter(String input) {
+        int delimiterEndIndex = input.indexOf("\\n");
+        String customDelimiter = input.substring(2, delimiterEndIndex);
+        return customDelimiter;
+    }
+
+    public static int[] parseToIntArray(String[] strings) {
+        int[] intArray = new int[strings.length];
+
+        for (int i = 0; i < strings.length; i++) {
+            intArray[i] = parseToInt(strings[i]);
+        }
+
+        return intArray;
+    }
+
+    public static int parseToInt(String string) {
+        if (string.isEmpty()) {
+            return 0;
+        } else {
+            int number = Integer.parseInt(string);
+            return number;
+        }
     }
 }
