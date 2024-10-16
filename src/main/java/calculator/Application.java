b68de08680d054e2c041d159de2bd90c7656456a
mingyeongbae <bmg1203@naver.com>
2024-10-16 17:51:16 +0900

    [refactor]: getCustomSeparator 메소드 구분자 추가 수정
    커스텀 구분자를 추출하여 추가시 | 문자를 추가하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,7 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Application {
-    static String sepeartor = ",:";
+    static String sepeartor = ",|:";
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -19,10 +19,12 @@ public class Application {
             isCustomSeparator = getCustomSeparator(str);
         }
 
+        // 숫자 추출
+        str = getNumbers(str);
+
         // 결과 출력
         System.out.println("결과 : " + str);
     }
-
 
     /**
      * 커스텀 구분자를 추출하는 함수 정규식을 활용함
@@ -39,9 +41,13 @@ public class Application {
         while (matcher.find()) { //일치하는 게 있다면
             found = true;
 
-            sepeartor += matcher.group(1);
+            sepeartor += "|" + matcher.group(1);
             //System.out.println("추출된 구분자: " + sepeartor);
         }
         return found;
+    }
+
+    private static String getNumbers(String str) {
+        return str;
     }
 }
