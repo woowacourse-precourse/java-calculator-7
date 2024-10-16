0ee4dd476fbf5130fa00180e6c2f8c7be2038311
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-16 18:59:33 +0900

    feat(Calculator): Add static factory method

--- src/test/java/calculator/domain/Calculator.java
+++ src/test/java/calculator/domain/Calculator.java
@@ -10,6 +10,12 @@ public class Calculator {
         this.numbers = numbers;
     }
 
+    // 외부에서 접근 가능한 정적 팩토리 메서드
+    public static Calculator of(List<Integer> numbers) {
+        return new Calculator(numbers);
+    }
+
+    // 리스트에 담긴 숫자들의 합을 구하는 메서드
     public int calculateSum() {
         return numbers.stream().mapToInt(Integer::intValue).sum();
     }
