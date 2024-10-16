bd67c989c58c3999bd5056e3bbb7504269e618a2
이창헌 <changbill@naver.com>
2024-10-16 16:17:35 +0900

    fix(format input): fix input split in CalculatorService.java

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -11,6 +11,7 @@ public class CalculatorService {
     private final char[] constantDelimiter = {',',':'};
     private Optional<char[]> customDelimiter;
     private Optional<String> input;
+    private String formatInput;
     private Optional<int[]> numbersToCalculate;
 
     public void input() {
@@ -21,28 +22,24 @@ public class CalculatorService {
     public void findCustomDelimiter() {
         String userInput = input.orElseThrow(() -> new IllegalArgumentException("입력값이 없습니다"));
         if(userInput.startsWith("//") && userInput.contains("\\n")) {
-            String[] seperateDelimiterAndString = userInput.split("\\n");
+            customDelimiter = Optional.of(userInput.substring(2, userInput.indexOf("\\n")).toCharArray());
+            formatInput = userInput.substring(userInput.indexOf("\\n") + 2);
+            System.out.println(formatInput);
 
-            if(seperateDelimiterAndString.length != 2) {
+            if(formatInput.contains("\\n")) {
                 throw new IllegalArgumentException("커스텀 구분자를 등록하는 도중 '\\n'이 2개 이상입니다");
             }
-
-            if(seperateDelimiterAndString[0].length() == 2) {
-                throw new IllegalArgumentException("등록할 커스텀 구분자가 없습니다");
-            }
-
-            customDelimiter = Optional.of(seperateDelimiterAndString[0].substring(2).toCharArray());
             return;
         } else if(userInput.contains("/") || userInput.contains("\\")) {
             throw new IllegalArgumentException("커스텀 구분자 등록 실패");
         }
+
+        formatInput = userInput;
         customDelimiter = Optional.empty();
     }
 
     public void deleteDelimiter() {
-        String userInput = input.orElseThrow(() -> new IllegalArgumentException("입력값이 없습니다"));
-        System.out.println(userInput);
-        String removeDelimiters = userInput.chars().filter(i -> {
+        String removeDelimiters = formatInput.chars().filter(i -> {
                     char currentChar = (char) i;
 
                     boolean isDelimiter = false;
@@ -76,7 +73,7 @@ public class CalculatorService {
     }
 
     public void output() {
-        int[] numbers = numbersToCalculate.orElseThrow(() -> new IllegalArgumentException("계산할 숫자가 없습니다"));
+        int[] numbers = numbersToCalculate.orElse(new int[]{});
         int result = Arrays.stream(numbers).sum();
         Output.outputMessage();
         Output.result(result);
