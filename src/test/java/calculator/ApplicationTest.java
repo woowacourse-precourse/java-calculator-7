511040ebfabbdc6dbfe8e6b7e70f65f06be7d6ff
byeongdoohan <quden04@gmail.com>
2024-10-16 22:41:33 +0900

    test:커스텀 구분자가 2글자 이상일 때 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -9,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
-    void 커스텀_구분자_사용() {
+    void useCustomSplit() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
@@ -17,11 +18,21 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    @DisplayName("음수가 포함됐을 때")
+    void exceptionTest() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자가 2글자 이상일 때")
+    void splitLengthTest() {
+        assertSimpleTest(() -> {
+            run("//;;;\\n1;;;2");
+            assertThat(output()).contains("결과 : 3");
+        });
     }
 
     @Override
