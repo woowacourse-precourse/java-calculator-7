e0311ec7b97285b7a9d4e02df0b52e095b093449
TalkingPotato90 <star901210@hanmail.net>
2024-10-17 00:49:47 +0900

    feat: 입력의 마지막이 숫자가 아니면 예외 처리하는 기능과 테스트 구현

--- src/main/java/calculator/model/InputData.java
+++ src/main/java/calculator/model/InputData.java
@@ -1,9 +1,21 @@
 package calculator.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputData {
     private final String input;
 
     public InputData(String input) {
         this.input = input;
+    }
+
+    public void validate(String input){
+        Pattern pattern = Pattern.compile(".*[0-9]$");
+        Matcher matcher = pattern.matcher(input);
+
+        if(!matcher.matches()){
+            throw new IllegalArgumentException();
+        }
     }
 }
---
+++ src/test/java/calculator/model/InputDataTest.java
@@ -0,0 +1,28 @@
+package calculator.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("사용자 입력 객체 테스트")
+class InputDataTest {
+    private InputData inputData;
+
+    @BeforeEach
+    void setUp() {
+        inputData = new InputData("test");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123!","0721가","2038a"})
+    @DisplayName("마지막 입력 값이 숫자가 아니면 예외가 발생한다")
+    void checkLastInput(String input){
+        assertThatThrownBy(()->inputData.validate(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
