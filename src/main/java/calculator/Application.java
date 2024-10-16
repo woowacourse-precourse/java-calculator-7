b179353a1d9d6f3df4391eee2f9ac08c32618db0
jiyun <tomatoziiilll@gmail.com>
2024-10-16 16:04:36 +0900

    fix: the output content was modified and the number conversion condition was modified

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,16 +8,17 @@ public class Application {
     static String cutomSeparatorStart = "//";
     static String cutomSeparatorEnd = "\\n";
     static String or = "|";
-    static String numberRegex = "-?\\d+"; // s.matches(numberRegex)
+    static String numberRegex = "\\d+"; // s.matches(numberRegex)
+    static String defaultSeparatorRegex = ",|:";
 
     public static void main(String[] args) {
-        StringBuilder separatorRegex = new StringBuilder(",|:");
+        StringBuilder separatorRegex = new StringBuilder(defaultSeparatorRegex);
         String input = Console.readLine();
 
         if (!input.isEmpty() && input.substring(0, 2).equals(cutomSeparatorStart)) {
             if (input.contains(cutomSeparatorEnd)) {
                 String customDelim = input.substring(2, input.indexOf(cutomSeparatorEnd));
-                separatorRegex.append(or + customDelim);
+                separatorRegex.append(or).append(customDelim);
             } else {
                 throw new IllegalArgumentException("\n지정 구분자는 //와 \\n 사이에 입력해주세요");
             }
@@ -31,14 +32,12 @@ public class Application {
         int sum = strs.stream()
                 .filter(s -> !s.isEmpty())
                 .map(s -> {
-                    try {
-                        return Integer.parseInt(s);
-                    } catch (Exception e) {
-                        throw new IllegalArgumentException(s + "는 허용되지 않는 문자입니다\n허용되는 문자: 숫자\n\t\t\t고정 구분자 , 와 :\n\t\t\t//와 \\n 사이에 지정 구분자");
-                    }
+                    if (!s.matches(numberRegex))
+                        throw new IllegalArgumentException(s + " 는 허용되지 않는 문자입니다");
+                    return Integer.parseInt(s);
                 })
                 .reduce(0, Integer::sum);
 
-        System.out.println(sum);
+        System.out.printf("결과 : %d\n", sum);
     }
 }
