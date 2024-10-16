73d50af759156de4e97b5ee87c1631d6f16d90fa
taek2222 <oot309@daum.net>
2024-10-17 01:41:49 +0900

    style(OutputView): 코드 가독성 향상을 위한 상수 분리
    하드 코딩인 부분을 가독성 향상을 위해, 상수로 분리해 정의하였습니다.

--- src/main/java/calculator/OutputView.java
+++ src/main/java/calculator/OutputView.java
@@ -2,11 +2,14 @@ package calculator;
 
 public class OutputView {
 
+    public static final String START_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    public static final String RESULT_MESSAGE = "결과 : ";
+
     public void printStartMessage() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println(START_MESSAGE);
     }
 
     public void printResultMessage(int result) {
-        System.out.println("결과 : " + result);
+        System.out.println(RESULT_MESSAGE + result);
     }
 }
