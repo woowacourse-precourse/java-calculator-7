00d610454a9d29adb8db2b2053fb09fe551b44a9
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-16 12:55:21 +0900

    feat(empty): 빈 문자열 입력시 빈 문자열 또는 null 값을 입력할 경우 0을 반환하는 기능을 구현함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String user_input = Console.readLine();
+
+        int answer = 0;
+
+        if (user_input.isEmpty()) {
+            answer = 0;
+        }
+
+
+
+        System.out.println("결과 : " + answer);
     }
+
 }
