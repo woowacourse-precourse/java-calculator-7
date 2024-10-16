b19a79a1ebe35c797c4bf80724c0d7e13f62161c
Hacanna42 <dtmdgus901@gmail.com>
2024-10-16 20:00:22 +0900

    feat: add hasCustomDelimiter() function

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,5 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String input = Console.readLine();
+        System.out.println(hasCustomDelimiter(input));
+    }
+
+    static boolean hasCustomDelimiter(String input) {
+        return input.startsWith("//");
     }
 }
