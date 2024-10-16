9f2d301b06bc47c238a81270c23eb9d7914544fd
songkyung <min.songkyung@gmail.com>
2024-10-16 15:45:38 +0900

    기본 구분자 분리 및 합산 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,57 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    enum Type {
+        DEFAULT, CUSTOM;
     }
+
+    public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = Console.readLine();
+
+//        char[] character = str.toCharArray();
+
+        Type strType = getType(str);
+
+        int sum = 0;
+
+        switch (strType) {
+            case DEFAULT:
+
+                // 쉼표(,) 또는 콜론(:)을 기준으로 문자열 분리
+                String[] parts = str.split("[, :]+");
+
+                for (int i=0; i<parts.length; i++) {
+                    sum += Integer.parseInt(parts[i]);
+                }
+
+                break;
+
+            case CUSTOM:
+                break;
+        }
+
+
+        System.out.println("결과 : " + sum);
+    }
+
+    private static Type getType(String str) {
+        if (str.startsWith("//")) {
+            return Type.CUSTOM;
+        } else {
+            return Type.DEFAULT;
+        }
+    }
+
+    // 숫자 추출 후 합산하는 함수
+//    private static int[] extractNumberSum(String str) {
+//
+//
+//
+//
+//    }
 }
