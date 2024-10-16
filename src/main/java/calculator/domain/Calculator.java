d82b62608ed986a631153c0cdb58b5ce38fd0abf
Yeongjae Kim <sees77770@gmail.com>
2024-10-16 22:26:35 +0900

    feat(Calculator): 커스텀 구분자 기능 구현
    계산기는 분리한 숫자(Operands)도 상태로 갖기 전, 커스텀 구분자가 입력될 시 계이를 기준으로 split 하기 위해 커스텀 구분자 상태를 갖는다.

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,14 +1,38 @@
 package calculator.domain;
 
 public class Calculator {
-    private static final String defaultSeparator = "[,;]";
+    private static final String defaultSeparator = "[,:]";
+    private String customSeparator = "";
 
     private Operands operands;
 
-    public Calculator(String input) {
-        String[] splitInput = input.split(defaultSeparator);
+    public Calculator(String inputValue) {
+        setCustomSeparator(inputValue);
+
+        String calcValue = getCalcValue(inputValue);
+        String[] splitInput = splitBySeparator(calcValue);
 
         this.operands = new Operands(splitInput);
+    }
+
+    private void setCustomSeparator(String inputValue) {
+        if (inputValue.startsWith("//")) {
+            this.customSeparator = String.valueOf(inputValue.charAt(2));
+        }
+    }
+
+    private String getCalcValue(String inputValue) {
+        if (inputValue.startsWith("//")) {
+            return inputValue.substring(5);
+        }
+        return inputValue;
+    }
+
+    private String[] splitBySeparator(String inputValue) {
+        if (customSeparator.isEmpty()) {
+            return inputValue.split(defaultSeparator);
+        }
+        return inputValue.split(customSeparator);
     }
 
     public int sum() {
