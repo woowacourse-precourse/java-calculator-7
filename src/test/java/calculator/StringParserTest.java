920055e36263aca8658a27f1fbc32843c0469c86
sunwon <jsw5913@naver.com>
2024-10-16 21:32:10 +0900

    refactor(StringParser): 테스트 변수명 개선
    - 공백_문자가_아닐시_예외를_던지지않는다()에서 필요없는 주석을 제거
    - 공백_문자가_아닐시_false를_반환하다()에서 변수 'blank'를 'notBlank'로 수정하여 실수를 정정
    - isBlank() 테스트 메소드들에서 '빈분자'를 '공백_문자'로 수정

--- src/test/java/calculator/StringParserTest.java
+++ src/test/java/calculator/StringParserTest.java
@@ -10,7 +10,7 @@ class StringParserTest {
     private StringParser stringParser = new StringParser();
 
     @Test
-    void 공백_문자일시_예외를_던진다() {
+    void 공백문자일시_예외를_던진다() {
         String blank = "";
 
         assertThrows(IllegalArgumentException.class, () -> {
@@ -19,28 +19,26 @@ class StringParserTest {
     }
 
     @Test
-    void 공백_문자가_아닐시_예외를_던지지않는다() {
-        // Given
-        String notBlank = "Not Blank";  // 빈 문자열이 아닌 유효한 문자열
+    void 공백문자가_아닐시_예외를_던지지않는다() {
+        String notBlank = "Not Blank";
 
-        // When & Then
         assertDoesNotThrow(() -> {
             stringParser.validateBlank(notBlank);
         });
     }
 
     @Test
-    void 빈문자일시_false를_반환하다() {
+    void 공백문자가_아닐시_false를_반환하다() {
         boolean expectedFalse = false;
-        String blank = "Not Blank";
+        String notBlank = "Not Blank";
 
-        boolean actual = stringParser.isBlank(blank);
+        boolean actual = stringParser.isBlank(notBlank);
 
         assertEquals(expectedFalse, actual);
     }
 
     @Test
-    void 빈문자일시_true를_반환하다() {
+    void 공백문자일시_true를_반환하다() {
         boolean expectedTrue = true;
         String blank = "";
 
