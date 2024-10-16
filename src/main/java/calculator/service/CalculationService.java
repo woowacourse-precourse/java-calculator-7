e823360dc3d7e53ee4fb6f9511aaac87d3bdf3bf
jijongkwon <help3451@naver.com>
2024-10-17 00:35:34 +0900

    feat(service): CalculationService 클래스에 숫자 배열 계산 로직 구현
    - calculate() 메서드에서 Arrays.stream(numbers).sum()을 이용하여 숫자 배열의 합 계산

--- src/main/java/calculator/service/CalculationService.java
+++ src/main/java/calculator/service/CalculationService.java
@@ -1,8 +1,11 @@
 package calculator.service;
 
+import java.util.Arrays;
+
 public class CalculationService {
+
     public int calculate(int[] numbers) {
         //TODO: 계산 로직 구현
-        return 0;
+        return Arrays.stream(numbers).sum();
     }
 }
