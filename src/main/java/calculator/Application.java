3599185077e3df248a055a84d0f1dc7ca46a7df7
MaiBoii <20183010@gs.cwnu.ac.kr>
2024-10-16 18:09:26 +0900

    feat: Customize Seperator key
    앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있다. 커스텀 구분자는 문자열 앞부분의 "//"와 "\n" 사이에 위치하는 문자를 커스텀 구분자로 사용한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,11 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 숫자 계산기 프로그램 구현
-        System.out.println("쉼표(,) 또는 콜론(:)으로 구분된 숫자를 입력하세요:");
+        System.out.println("문자열을 입력하시오(예: //;\\n1;2;3) : ");
+        String input = Console.readLine().trim();
 
-        String input = Console.readLine();
-
-        int result = Calculator.add(input);
-        System.out.println("합계: " + result);
+        try {
+            int result = Calculator.add(input);
+            System.out.println("결과: " + result);
+        } catch (IllegalArgumentException e) {
+            System.err.println("입력 오류: " + e.getMessage());
+            System.out.println(input);
+        } catch (Exception e) {
+            System.err.println("예외 발생: " + e.getMessage());
+        }
     }
 }
