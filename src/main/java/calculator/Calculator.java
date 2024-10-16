4c4ea0aa84e41398f3ec12b3641f8135f7328541
aaahyunseo <hs030608@naver.com>
2024-10-16 23:37:12 +0900

    feat(Calculator): 문자열 분리 함수 생성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -10,4 +10,21 @@ public class Calculator {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
+
+    /**
+     * 구분자에 따라 문자열 분리하는 함수
+     * **/
+    private static String[] operator(String str){
+        if(str.startsWith("//") && str.substring(3,5).equals("\\n")){
+            String op = str.substring(2,3);
+            str = str.substring(5);
+            // 백슬래시 구분자 처리
+            if (op.equals("\\")) {
+                op = "\\\\";
+            }
+            return str.split(op);
+        }else {
+            return str.split(",|:");
+        }
+    }
 }
