8eb8f2908b1b5f0e4c4e4cde082b56a8c985f864
cyunlee <siyunkr@gmail.com>
2024-10-17 01:35:02 +0900

    refactor(parsing): 구분자 추출하는 코드 구조 변경 및 메서드 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,31 +9,27 @@ public class Application {
         String input = Console.readLine();
         int sum = 0;
 
-        if(input.contains(",") || input.contains(":")) {
-            for(int i = 0; i < input.length(); i++){
-                if(String.valueOf(input.charAt(i)).equals(",")) {
-                    continue;
-                }else if(String.valueOf(input.charAt(i)).equals(":")) {
-                    continue;
-                }else {
-                    sum += Character.getNumericValue(input.charAt(i));
-                }
-            }
-        }
-
         if(input.contains("//") && input.contains("\\n")) {
             String custom = input.substring(input.indexOf("//")+2, input.indexOf("\\n"));
             String newInput = input.substring(input.indexOf("\\n")+2);
-            for(int i = 0; i < newInput.length(); i++) {
-                if(String.valueOf(newInput.charAt(i)).equals(custom)) {
-                    continue;
-                }else {
-                    sum += Character.getNumericValue(newInput.charAt(i));
+            String replacedInput = newInput.replace(custom, ",");
+
+            String[] strArrCustom = replacedInput.split("[,:]");
+
+            for(String str : strArrCustom) {
+                if(!str.isEmpty()) {
+                    sum += Integer.parseInt(str);
                 }
             }
-        }
+        }else if(input.contains(",") || input.contains(":")) {
+            String[] strArrBasic = input.split("[,:]");
 
-        if(input.isEmpty()) {
+            for(String str : strArrBasic) {
+                if(!str.isEmpty()) {
+                    sum += Integer.parseInt(str);
+                }
+            }
+        }else if(input.isEmpty()) {
             sum = 0;
         }
 
