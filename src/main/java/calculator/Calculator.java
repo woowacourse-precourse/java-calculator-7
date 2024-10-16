55226a42c19b0e324c3e9deef5dbcbbd1e226f63
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-10-16 17:01:47 +0900

    feat: 문자열 입력 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package calculator;
+import camp.nextstep.edu.missionutils.Console;
+
+public class Calculator {
+    // 시작 함수
+    public void start(){
+        // 문자열 입력
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = Console.readLine();
+        System.out.println(str);
+    }
+}
