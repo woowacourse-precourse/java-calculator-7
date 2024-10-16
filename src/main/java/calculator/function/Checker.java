e28f1c276cc3420df902ae1bd91d4c14e65f687b
GoGradually <chkun3109@gmail.com>
2024-10-16 09:03:48 +0900

    test(Checker): 테스트 코드 작성
    - 0으로 시작하는 정수에 대한 검사 -> 실패
    - 구분자를 포함하는 문자열에 대한 검사 -> 실패
    - 정수 형태가 아닌 문자가 포함되어있는지 수행하는 검사 -> 실패
    - 정상 검사 -> 통과
    - 빈 문자열 검사 -> 통과

--- src/main/java/calculator/function/Checker.java
+++ src/main/java/calculator/function/Checker.java
@@ -1,4 +1,15 @@
 package calculator.function;
 
+import java.util.List;
+
 public class Checker {
+    private List<String> inputStrings;
+    private String separator;
+
+    public Checker(List<String> inputStrings, String separator) {
+        this.inputStrings = inputStrings;
+        this.separator = separator;
+    }
+
+    public void execute(){}
 }
--- src/test/java/calculator/function/CheckerTest.java
+++ src/test/java/calculator/function/CheckerTest.java
@@ -1,7 +1,68 @@
 package calculator.function;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckerTest {
+    @Test
+    void 검사기_0으로_시작하는_정수(){
+        //given
+        List<String> inputString = new ArrayList<>();
+        inputString.add("00123");
+        String separator = "[,:]";
+        //when, then
+        Checker checker = new Checker(inputString, separator);
+        assertThrows(IllegalStateException.class, checker::execute);
 
+        
+    }
+    
+    @Test
+    void 검사기_문자열_구분자_포함(){
+        //given
+        List<String> inputString = new ArrayList<>();
+        inputString.add("1243");
+        String separator = "[,:4]";
+        //when, then
+        Checker checker = new Checker(inputString, separator);
+        assertThrows(IllegalStateException.class, checker::execute);
+        
+    }
+    
+    @Test
+    void 검사기_정수_형태_아닌_문자(){
+        //given
+        List<String> inputString = new ArrayList<>();
+        inputString.add("12^3");
+        String separator = "[,:]";
+        //when, then
+        Checker checker = new Checker(inputString, separator);
+        assertThrows(IllegalStateException.class, checker::execute);
+    }
+    
+    @Test
+    void 검사기_통과(){
+        //given
+        List<String> inputString = new ArrayList<>();
+        inputString.add("123");
+        String separator = "[,:]";
+        //when, then
+        Checker checker = new Checker(inputString, separator);
+        assertDoesNotThrow(checker::execute);
+        
+    }
+    @Test
+    void 검사기_빈_문자열(){
+        //given
+        List<String> inputString = new ArrayList<>();
+        inputString.add("");
+        String separator = "[,:]";
+        //when, then
+        Checker checker = new Checker(inputString, separator);
+        assertDoesNotThrow(checker::execute);
+    }
 }
