14d7e3fa2561753e111f13acf8a7dc4b241dd459
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-17 02:01:15 +0900

    feat(CalculatorContoller): 사용자의 입력을 파싱한 후 모델에 전달하여 계산 결과값을 받는 메서드 구현
    계산된 결과값은 결과를 출력하는 뷰에 전달

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,14 +1,31 @@
 package calculator.controller;
 
 import calculator.model.Calculator;
+import calculator.utils.Parser;
 import calculator.view.InputView;
+import calculator.view.OutputView;
+import java.util.List;
 
 public class CalculatorController {
     private final InputView inputView;
     private final Calculator calculator;
+    private final Parser parser;
+    private final OutputView outputView;
 
-    public CalculatorController(InputView inputView, Calculator calculator) {
+    public CalculatorController(InputView inputView, Calculator calculator, Parser parser, OutputView outputView) {
         this.inputView = inputView;
         this.calculator = calculator;
+        this.parser = parser;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        String userInput = inputView.getInputString();
+
+        List<Number> numbers = parser.parse(userInput);
+
+        Number result = calculator.add(numbers);
+
+        outputView.displayResult(result);
     }
 }
