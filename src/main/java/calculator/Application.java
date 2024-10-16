ee427ef8a623cf929f5fee16ea74830332b58e21
Yoon-Jemin <yjm1800@naver.com>
2024-10-16 17:41:43 +0900

    feat(main): main 파일에 계산기 기능 추가
    main 파일 수정된 내용:
    - Calculator 클래스의 필드 및 메서드 정의: 숫자를 저장하는  리스트와 결과를 저장하는  필드 추가
    -  메서드에서 입력된 문자열을 분리하여 숫자를 추가하는 로직 구현
    -  메서드를 통해 저장된 숫자의 합을 계산하는 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.HashSet;
@@ -12,8 +13,54 @@ public class Application {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        int result = 0;
-        System.out.println("결과 : " + result);
 
+        // 계산기 인스턴스 생성
+        Calculator calculator = new Calculator();
+
+        // 입력을 토대로 숫자 추가
+        calculator.addNumbers(input);
+
+        // 계산 진행
+        calculator.calculate();
+
+        System.out.println("결과 : " + calculator.result);
     }
+
+    // 계산기 클래스 정의
+    private static class Calculator {
+
+        private ArrayList<Integer> numbers;
+        private Set<String> seperators;
+        private int result;
+
+        private Calculator() {
+            numbers = new ArrayList<>();
+            seperators = new HashSet<>();
+            result = 0;
+            seperators.add(",");
+            seperators.add(":");
+        }
+
+        public void addNumbers(String input) {
+            int index = 0;
+            for(int i = 0; i < input.length(); i++) {
+                char ch = input.charAt(i);
+                if(seperators.contains(String.valueOf(ch))) {
+                    String number = input.substring(index, i);
+                    numbers.add(Integer.parseInt(number));
+                    index = i + 1;
+                }
+            }
+            String number = input.substring(index, input.length());
+            numbers.add(Integer.parseInt(number));
+        }
+
+        public void calculate() {
+            for(int number : numbers) {
+                result += number;
+            }
+        }
+    }
+
 }
+
