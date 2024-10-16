38b15ca2e8e1da4fc25bdd15fab95325ae1d8507
taek2222 <oot309@daum.net>
2024-10-17 02:25:44 +0900

    refactor(ExtractService): 상수 분리로 인한, 재사용성 향상
    해당 클래스에 공통적으로 사용하는 상수를 분리해 재사용성을 향상시켰습니다.

--- src/main/java/calculator/ExtractService.java
+++ src/main/java/calculator/ExtractService.java
@@ -2,21 +2,26 @@ package calculator;
 
 public class ExtractService {
 
-    public String extractCustomDelimiter(String readInput) {
-        if (readInput.contains("//") && readInput.contains("\\n")) {
-            int startPos = readInput.indexOf("//");
-            int endPos = readInput.indexOf("\\n");
+    public static final String CUSTOM_PREFIX = "//";
+    public static final String CUSTOM_SUFFIX = "\\n";
+    public static final String EMPTY = "";
+    public static final int CUSTOM_PREFIX_LENGTH = CUSTOM_PREFIX.length();
 
-            return readInput.substring(startPos + 2, endPos);
+    public String extractCustomDelimiter(String readInput) {
+        if (readInput.contains(CUSTOM_PREFIX) && readInput.contains(CUSTOM_SUFFIX)) {
+            int startPos = readInput.indexOf(CUSTOM_PREFIX);
+            int endPos = readInput.indexOf(CUSTOM_SUFFIX);
+
+            return readInput.substring(startPos + CUSTOM_PREFIX_LENGTH, endPos);
         }
 
-        return "";
+        return EMPTY;
     }
 
     public String extractPositiveString(String readInput, String customDelimiter) {
         if (!customDelimiter.isBlank()) {
-            String remove =  "//" + customDelimiter + "\\n";
-            return readInput.replace(remove, "");
+            String remove =  CUSTOM_PREFIX + customDelimiter + CUSTOM_SUFFIX;
+            return readInput.replace(remove, EMPTY);
         }
 
         return readInput;
