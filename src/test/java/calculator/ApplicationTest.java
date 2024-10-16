ec3c482427d92b9c333e3f5b9880a52269419ab9
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-17 03:15:00 +0900

    test(Parser): 빈 문자열 입력시 0을 출력하는 테스트
    사용자가 빈 문자열을 입력했을 때 Parser 는 0을 반환하여야 한다

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,13 +1,24 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import calculator.utils.Parser;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+    Parser parser = new Parser();
+
+    @Test
+    void 빈_문자열을_입력하면_0이_반환되어야한다() {
+        List<Number> result = parser.parse("");
+        Number expected = 0;
+        assertThat(result.getFirst()).isEqualTo(expected);
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -19,8 +30,8 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
