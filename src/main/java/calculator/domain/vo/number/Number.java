bd19d4834062c7c49645964e73a9250007552c77
Geonwoo Pack <packdev937@gmail.com>
2024-10-17 08:26:34 +0900

    feat : Value Object인 Number를 생성한다

---
+++ src/main/java/calculator/domain/vo/number/Number.java
@@ -0,0 +1,50 @@
+package calculator.domain.vo.number;
+
+import java.math.BigInteger;
+import java.util.Objects;
+
+public class Number {
+
+    private BigInteger value;
+
+    private Number(String value) {
+        validate(value);
+        this.value = new BigInteger(value);
+    }
+
+    protected void validate(String value) {
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            new BigInteger(value);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static Number from(String value) {
+        return new Number(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Number)) {
+            return false;
+        }
+        Number number = (Number) o;
+        return this.value.equals(number.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Number = %s", value);
+    }
+
+}
