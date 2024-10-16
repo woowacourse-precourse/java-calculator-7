1ff0e8155401b8fd8be1d261411acc8cf9d520db
world-dv <goodforx0147@naver.com>
2024-10-17 03:39:55 +0900

    feat: 덧셈 결과를 받아 출력갑니다.
    -덧셈 결과를 OutputView를 통해 출력합니다.
    -'결과 : ' 문구와 함께 덧셈 결과를 출력합니다.
    -기술 구현 목록을 갱신합니다.

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -3,12 +3,13 @@ package calculator.controller;
 import calculator.model.Calculate;
 import calculator.model.Numbers;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 import java.util.List;
 
 public class CalculatorController {
 
     public void callCalculator() {
-
+        endCalculate(doCalculate(startCalculate()));
     }
 
     public String startCalculate() {
@@ -19,5 +20,9 @@ public class CalculatorController {
     public Integer doCalculate(String userInput) {
         List<String> calculate = new Calculate(userInput).getSplitUserInput();
         return new Numbers(calculate).calculateNumbersSum();
+    }
+
+    public void endCalculate(Integer numbersSum) {
+        OutputView.printCalculateResult(numbersSum);
     }
 }
--- src/main/java/calculator/view/PrintMessage.java
+++ src/main/java/calculator/view/PrintMessage.java
@@ -2,7 +2,8 @@ package calculator.view;
 
 public enum PrintMessage {
 
-    USER_INPUT_MESSAGE("덧셈할 문자열을 입력해 주세요.");
+    USER_INPUT_MESSAGE("덧셈할 문자열을 입력해 주세요."),
+    USER_OUTPUT_MESSAGE("결과 : ");
 
     private final String message;
 
