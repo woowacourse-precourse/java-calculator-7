1d02ac84d40f3dc3784d043a0ad2df469e7680dd
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-10-16 15:26:09 +0900

    refactor: 빈 문자열을 판별할 시와 그 후 동작에 대한 메소드 수정
    - 빈 문자열 판별 시 사용하는 메소드를 isBlank()에서 isEmpty()로 수정
    
    - 빈 문자열 판별 후 종료하는 코드를 return에서 Console.close()로 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,9 +6,10 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
-        if (str.isBlank()) {
-            System.out.println("결과 : 0");
-            return;
+        
+        if (str.isEmpty()) {
+            System.out.print("결과 : 0");
+            Console.close();
         }
     }
 }
