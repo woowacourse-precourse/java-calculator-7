927460063401372a65386d21702de00f7eb65d13
tentenball <lth8905@gmail.com>
2024-10-17 02:47:59 +0900

    feat: 커스텀 구분자 생성을 위한 상수 설정

---
+++ src/main/java/calculator/constant/DrawConstant.java
@@ -0,0 +1,11 @@
+package calculator.constant;
+
+public class DrawConstant {
+	public static int EXTRACT_START_INDEX = 2;
+	public static String CUSTOM_DELIMITER_START_SIGN = "//";
+	public static String CUSTOM_DELIMITER_END_SIGN = "\\n";
+	public static int BACK_SLASH_INDEX = 1;
+	public static int N_INDEX = 2;
+	public static int CUSTOM_DELIMITER_INDEX = 0;
+	public static String CUSTOM_DELIMITER_REGEX = "[a-zA-Z]|[^a-zA-Z0-9]";
+}
