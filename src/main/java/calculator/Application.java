5049fba301a87f6d3de6f9e8fdc5a93cc4a5a79b
eb9862 <luclead622@gmail.com>
2024-10-16 16:32:14 +0900

    feat : 입력 메시지 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,9 @@
 package calculator;
 
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringCalculator sc = new StringCalculator();
+        sc.run();
     }
 }
---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class StringCalculator {
+
+    void run() {
+        printInputMsg();
+    }
+
+    void printInputMsg() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+}
