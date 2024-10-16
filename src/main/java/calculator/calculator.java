3167b0abb7753c592ec940349e38b70e319af2d7
JoonHyuk0331 <joonlife0901@gmail.com>
2024-10-16 17:13:54 +0900

    feat($calcualotor):문자열을 입출력하는 코드 작성

---
+++ src/main/java/calculator/calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        String input;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("문자열을 입력해주세요");
+        input=sc.next();
+        System.out.println(input);
+    }
+}
