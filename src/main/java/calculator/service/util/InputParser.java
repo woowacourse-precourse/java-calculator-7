947c6fe373450430bad67e9fea10b11c7deff9fd
haebyun <bhk4392@gmail.com>
2024-10-16 15:48:16 +0900

    feat(InputParser): parseToInt 라는 주어진 문자열에 대해 숫자로 변환하는 기능 구현
    - 만약 숫자가 아닌 문자가 들어올 경우 NumberFormatException이 발생하도록 구현

--- src/main/java/calculator/service/util/InputParser.java
+++ src/main/java/calculator/service/util/InputParser.java
@@ -1,7 +1,18 @@
 package calculator.service.util;
 
+import calculator.domain.exception.CalculatorException;
+import calculator.domain.exception.ErrorMessage;
+
 public final class InputParser {
     private static String[] splitByDelimiters(String input, String delimiters) {
         return input.split(delimiters);
+    }
+
+    private static int parseToInt(String numberString) {
+        try {
+            return Integer.parseInt(numberString.trim());
+        } catch (NumberFormatException e) {
+            throw CalculatorException.from(ErrorMessage.NOT_NUMBER_ERROR);
+        }
     }
 }
