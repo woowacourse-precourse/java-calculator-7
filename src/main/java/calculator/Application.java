e37fa6bd69c9dfadef81a530c049e99d9e4566cc
Minuring <mw9505@gmail.com>
2024-10-16 18:59:25 +0900

    feat(I/O): 결과 출력
    Calculator 객체의 계산 결과를 받아 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,5 +10,9 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
+        Calculator calculator = new Calculator(input);
+        int sum = calculator.calculate();
+
+        System.out.println("결과 : " + sum);
     }
 }
