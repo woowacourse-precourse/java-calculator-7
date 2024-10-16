aad3d05475fd9252e368e2b88b7e6ad8f74b6267
joonwan <joonwan1024@naver.com>
2024-10-16 15:20:48 +0900

    feat: getInputData 메서드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String inputData = getInputData();
+    }
+
+    private static String getInputData() {
+        System.out.println(INPUT_MESSAGE);
+        return Console.readLine();
     }
 }
