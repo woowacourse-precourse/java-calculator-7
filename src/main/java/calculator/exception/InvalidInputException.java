2c67caf307caa73cf76a2993356fd27369d1652a
jijongkwon <help3451@naver.com>
2024-10-16 23:19:24 +0900

    feat(exception): InvalidInputException 예외 클래스 구현
    - Constructor: [ERROR] + message

--- src/main/java/calculator/exception/InvalidInputException.java
+++ src/main/java/calculator/exception/InvalidInputException.java
@@ -2,4 +2,7 @@ package calculator.exception;
 
 public class InvalidInputException extends RuntimeException {
     //TODO: 예외 클래스 구현
+    public InvalidInputException(String message) {
+        super("[ERROR] " + message);
+    }
 }
