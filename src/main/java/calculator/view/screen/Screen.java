fa9e9b4d0810858b3edf28c5562443a4b33c8a4a
MinJun Choi <mj043000@naver.com>
2024-10-17 00:06:22 +0900

    fix: Screen.java 필요없는 코드 제거

--- src/main/java/calculator/view/screen/Screen.java
+++ src/main/java/calculator/view/screen/Screen.java
@@ -1,10 +1,8 @@
 package calculator.view.screen;
 
-import calculator.model.inputValue.InputValue;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Screen {
-    InputValue inputValue;
 
     public String getUserInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
