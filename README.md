2de222c51f8f335c077df862fe86a7c9e9ed5544
ZAKAR <saojung518@naver.com>
2024-10-16 17:26:56 +0900

    feat(calculator): add sum calculation for comma and colon seperators
    - Implemented logic to split input by comma and colon
    - Return the sum of the extracted numbers

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# java-calculator-precourse
+# 문자열 덧셈 계산기
+
+## 프로젝트 설명
+
+문자열 입력에서 숫자를 추출하고, 이를 합산하여 결과를 반환하는 계산기를 구현
+
+## 추가된 기능
+
+1. `camp.nextstep.edu.missionutils.Console.readLine()`를 사용하여 입력값을 받음
+
+2. 쉼표(,) 또는 콜론(:) 및 커스텀 구분자(//[구분자]\n)를 지정하여 숫자를 분리하여 합산
+
+- 예: `1,2,3` => 6
+- 예: `1,2:3` => 6
+
+3. 잘못된 값을 입력받을 시 `IllegalArgumentException` 발생시킨 후 프로그램 종료
+
+- 예: `2,-3:4` => `IllegalArgumentException`
+- 예: `2:ab,5` => `IllegalArgumentException`
--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,46 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        calculateNum(input);
+    }
+
+    private static int calculateNum(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String seperator = ",|:";
+
+        if (input.startsWith("//")) {
+            String[] parts = input.split("\n", 2);
+            seperator = parts[0].substring(2);
+            input = parts[1];
+        }
+
+        String[] numbers = input.split(seperator);
+
+        int sum = 0;
+        for (String number : numbers) {
+            sum += parseNumber(number);
+        }
+
+        return sum;
+    }
+
+    private static int parseNumber(String number) {
+        int result;
+        try {
+            result = Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다.");
+        }
+        if (result < 0) {
+            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+        }
+        return result;
     }
 }
