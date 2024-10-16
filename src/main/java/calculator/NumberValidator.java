102b6acf861c93678e4be5052386805100e39df8
corpi <lj845854@gmail.com>
2024-10-16 22:10:29 +0900

    feat(NumberValidator): 숫자가 아닐 경우 IllegalArgumentException 에러를 발생하는 기능 추가

---
+++ src/main/java/calculator/NumberValidator.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import java.util.List;
+import java.util.Objects;
+
+public class NumberValidator {
+    public void validate(List<String> splitNumber) {
+        for (String candidate : splitNumber) {
+            validParseNumber(candidate);
+        }
+    }
+
+    private void validParseNumber(String number) {
+        if (Objects.equals(number, "")) {
+            return;
+        }
+
+        try {
+            Double.parseDouble(number);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+    }
+}
