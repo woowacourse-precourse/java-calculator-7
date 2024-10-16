59f00003e3312733daaec5578a23c7bc1d8bd2ae
강승연 <syeon.dev@gmail.com>
2024-10-17 01:18:22 +0900

    feat: 잘못된 값에 대하여 IllegalArgumentException 예외처리
    잘못된 값의 정의
    * 커스텀 구분자 입력 형식을 지키지 않았다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,15 +2,47 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int result = 0;
+        ArrayList<Character> seperators = new ArrayList<>();
+        seperators.add(',');
+        seperators.add(':');
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        System.out.print("결과 : " + result);
+        try{
+            //구분자 리스트에 구분자 추가
+            if(input.charAt(0) == '/' && input.charAt(1) == '/'){
+                int index = 2;
+                while(!(input.charAt(index) == '\\' && input.charAt(index + 1) == 'n')){
+                    char c = input.charAt(index);
+                    if(Character.isDigit(c)){
+                        throw new IllegalArgumentException();
+                    }
+
+                    if(index + 2 >= input.length()){
+                        throw new IllegalArgumentException();
+                    }
+
+                    index++;
+                }
+            }
+
+            //커스텀 구분자가 존재하지 않음에도 시작 char이 문자열이 아님
+            if(!Character.isDigit(input.charAt(0))){
+                throw new IllegalArgumentException();
+            }
+
+            System.out.print("결과 : " + result);
+        }
+        catch(IllegalArgumentException illegalArgumentException){
+            throw new IllegalArgumentException();
+        }
     }
 }
