dcdee8fa38a1df661e2a7a80049c8e8dc02287d7
LJM\eueo8 <eueo8259@gmail.com>
2024-10-16 13:55:56 +0900

    feat: 예외처리 단계 1

--- src/test/java/calculator/CalculatorService.java
+++ src/test/java/calculator/CalculatorService.java
@@ -1,29 +1,36 @@
 package calculator;
 
 public class CalculatorService {
-    public int returnAnswer(String answer){
-        if (answer == ""){
+    public int returnAnswer(String answer) {
+        if (answer == "") {
             return 0;
         }
         return -1;
     }
 
-    public int [] splitByDefaultDelimiter(String answer){
-        String [] stringArray = answer.split(",|:");
-        int [] intArrays = new int[stringArray.length];
-        for (int i = 0; i < intArrays.length; i++){
+    public String getDelimiter(String input) {
+        String delimiter = ",|:";
+        if (input.startsWith("//") && input.contains("\n")) {
+            delimiter = input.substring(2, input.indexOf("\n"));
+            return delimiter;
+        }
+        return delimiter;
+    }
+
+    public int[] splitByDefaultDelimiter(String input) {
+        String[] stringArray = input.split(getDelimiter(input));
+        int[] intArrays = new int[stringArray.length];
+        for (int i = 0; i < intArrays.length; i++) {
             intArrays[i] = Integer.parseInt(stringArray[i]);
         }
         return intArrays;
     }
 
-    public int[] splitByCustomDelimiter(String answer) {
-        int customIndex = answer.indexOf("\n");
-        String customDelimiter = answer.substring(2, customIndex);
-        String exceptDelimiter = answer.substring(customIndex+1);
-        String [] stringArray = exceptDelimiter.split(customDelimiter);
-        int [] intArrays = new int [stringArray.length];
-        for (int i = 0; i < intArrays.length; i++){
+    public int[] splitByCustomDelimiter(String input) {
+        String exceptDelimiter = input.substring(input.indexOf("\n") + 1);
+        String[] stringArray = exceptDelimiter.split(getDelimiter(input));
+        int[] intArrays = new int[stringArray.length];
+        for (int i = 0; i < intArrays.length; i++) {
             intArrays[i] = Integer.parseInt(stringArray[i]);
         }
         return intArrays;
@@ -31,9 +38,26 @@ public class CalculatorService {
 
     public int plus(int[] input) {
         int sum = 0;
-        for (int i : input){
+        for (int i : input) {
             sum += i;
         }
         return sum;
     }
+
+    public void ensureValidInput(String input) {
+        //구분자가 존재하지 않는 경우
+        if (!input.contains(getDelimiter(input))) {
+            throw new IllegalArgumentException("구분자가 존재하지 않습니다.");
+        }
+        //커스텀 구분자 중 \n을 사용하지 않음
+        if (input.startsWith("//") && !input.contains("\n")) {
+            throw new IllegalArgumentException("커스텀 구분자 사용이 잘못되었습니다.");
+        }
+        //문자열 시작이 숫자 혹은 // 이 아닌 경우
+        if (Character.isDigit(input.charAt(0)) || input.startsWith("//")) {
+            throw new IllegalArgumentException("잘못된 문자열 입력입니다.");
+        }
+        //문자열 중 구분자와 다른 문자가 섞여있을 경우
+    }
+
 }
--- src/test/java/calculator/CalculatorServiceTest.java
+++ src/test/java/calculator/CalculatorServiceTest.java
@@ -1,51 +1,93 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorServiceTest {
     private int answer;
     private CalculatorService cal = new CalculatorService();
 
     @Test
-    void 빈_문자열_처리(){
+    void 빈_문자열_처리() {
         String input = "";
         answer = cal.returnAnswer(input);
         assertEquals(0, answer);
     }
 
     @Test
-    void 기본_구분자_처리(){
+    void 기본_구분자_처리() {
         String input = "1,2,3";
-        int [] expected = {1,2,3};
-        int [] actual = cal.splitByDefaultDelimiter(input);
+        int[] expected = {1, 2, 3};
+        int[] actual = cal.splitByDefaultDelimiter(input);
         assertArrayEquals(expected, actual);
 
         String input2 = "1:2,3";
-        int [] expected2 = {1,2,3};
-        int [] actual2 = cal.splitByDefaultDelimiter(input2);
+        int[] expected2 = {1, 2, 3};
+        int[] actual2 = cal.splitByDefaultDelimiter(input2);
         assertArrayEquals(expected2, actual2);
     }
 
     @Test
-    void 커스텁_구분자_처리(){
+    void 커스텁_구분자_처리() {
         String input = "//a\n1a2a3a";
-        int [] expected = {1,2,3};
-        int [] actual = cal.splitByCustomDelimiter(input);
+        int[] expected = {1, 2, 3};
+        int[] actual = cal.splitByCustomDelimiter(input);
         assertArrayEquals(expected, actual);
 
         String input2 = "//ab\n1ab2ab3ab4";
-        int [] expected2 = {1,2,3,4};
-        int [] actual2 = cal.splitByCustomDelimiter(input2);
+        int[] expected2 = {1, 2, 3, 4};
+        int[] actual2 = cal.splitByCustomDelimiter(input2);
         assertArrayEquals(expected2, actual2);
     }
 
     @Test
-    void 숫자_합산(){
-        int [] input = {1,2,3};
+    void 숫자_합산() {
+        int[] input = {1, 2, 3};
         int actual = cal.plus(input);
         assertEquals(6, actual);
+    }
+
+    @Test
+    void 구분자_존재_X() {
+        String input = "12345";
+        String input2 = "//a\n12345";
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    cal.ensureValidInput(input);
+                    cal.ensureValidInput(input2);
+                });
+    }
+
+    @Test
+    void 잘못된_커스텀_구분자_사용() {
+        String input = "//a//1a2a3a";
+        String input2 = "\na//1a2a3a";
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    cal.ensureValidInput(input);
+                    cal.ensureValidInput(input2);
+                });
+    }
+
+    @Test
+    void 첫시작이_잘못된_문자의_걍우() {
+        String input = ",1,2,3,4";
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    cal.ensureValidInput(input);
+                });
+    }
+
+    @Test
+    void 중간에_다른_구분자_사용() {
+        String input = "1,2.3:4";
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    cal.ensureValidInput(input);
+                });
     }
 
 }
