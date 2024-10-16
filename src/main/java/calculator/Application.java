fbfb2b9c3669edc31a6e416348c3abde9c9bf00c
min-0 <dhe08133@naver.com>
2024-10-16 23:36:50 +0900

    feat: 숫자 추출
    입력값 구분자를 뺀 숫자만 추출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,11 +2,23 @@ package calculator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
 
-        System.out.println(input);
+        String[] parts = input.split("\\D+");
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (String part : parts) {
+            if (!part.isEmpty()) { // 빈 문자열이 아닌 경우에만 처리
+                numbers.add(Integer.parseInt(part));
+            }
+        }
+
+        System.out.println(numbers);
     }
 }
