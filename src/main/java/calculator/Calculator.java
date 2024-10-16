b7e145ba17e49dc9375bacd920871a6b69bc52de
aaahyunseo <hs030608@naver.com>
2024-10-16 23:35:59 +0900

    feat(Calculator): 문자열 입력 함수 생성

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Calculator {
+    /**
+     * 문자열 입력받기
+     * **/
+    private static String input(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+}
