81a32dbe961370aa38436624f69d8bbcd90c6940
woojin <zaza0804@uos.ac.kr>
2024-10-17 01:47:07 +0900

    feat($stringChecker): make StringChecker with checking separator function
    - 커스텀 구분자 사용 여부 확인 기능 함수를 구현했습니다.

---
+++ src/main/java/calculator/app/StringChecker.java
@@ -0,0 +1,20 @@
+package calculator.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringChecker {
+
+    private static String LEFT_SEPARATOR = "//";
+    private static String RIGHT_SEPARATOR = "\\n";
+    private static List<Character> BASIC_SEPARATOR = List.of(',', ':');
+
+    List<Character> checkCustomSeparator(String expression) {
+        if (expression.substring(0, 2).equals(LEFT_SEPARATOR) && expression.substring(3, 5).equals(RIGHT_SEPARATOR)) {
+            List<Character> customSeparator = new ArrayList<>(BASIC_SEPARATOR);
+            customSeparator.add(expression.charAt(2));
+            return customSeparator;
+        }
+        return BASIC_SEPARATOR;
+    }
+}
