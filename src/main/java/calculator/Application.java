49f5a6c42668a352ed1e5a7bbb5e111609a2cda8
OhYeHyun <ohyhohyl@naver.com>
2024-10-16 21:31:49 +0900

    feat: 사용자 입력에서 숫자 추출하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,10 +3,19 @@ package calculator;
 import java.util.Arrays;
 
 public class Application {
+    public static String[] separators = {",", ":"};
+
     public static void main(String[] args) {
 
     }
+
     public static int sum(int[] numbers) {
         return Arrays.stream(numbers).sum();
+    }
+
+    public static int[] extractNumbers(String input, String[] separators) {
+        String separatorsRegex = String.join("|", separators);
+
+        return Arrays.stream(input.split(separatorsRegex)).mapToInt(Integer::parseInt).toArray();
     }
 }
