ac74297f99112e13572f0da28a7aeaf88c873de2
binn <anyubin0724@naver.com>
2024-10-16 17:06:34 +0900

    feat : Calculator.java 생성
    결과를 받는 get, set 함수 구현

---
+++ src/main/java/calculator/model/Calculator.java
@@ -0,0 +1,13 @@
+package calculator.model;
+
+public class Calculator {
+    private static int result;
+
+    private void setResult(int result) {
+        this.result = result;
+    }
+
+    public int getResult() {
+        return result;
+    }
+}
