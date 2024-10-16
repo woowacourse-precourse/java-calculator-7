e4ffe120cf6636a7fb537058e5f094ecc572b637
world-dv <goodforx0147@naver.com>
2024-10-17 03:20:36 +0900

    feat: 사용자 입력 문자열을 숫자 리스트로 구성합니다.
    -구분자를 기준으로 분리한 문자열 리스트를 정수 형태로 변환합니다.
    -문자열이 라면 숫자 0으로 치환합니다.
    -정수 변환 시 오류가 발생한다면 예외를 발생시킵니다.
    -변환된 정수를 리스트에 저장합니다.
    -기능 구현 목록을 갱신합니다.

---
+++ src/main/java/calculator/model/Numbers.java
@@ -0,0 +1,39 @@
+package calculator.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Numbers {
+
+    private static final Integer ZERO = 0;
+    private static final String ZERO_STRING = "";
+
+    private final List<Integer> numbers;
+
+    public Numbers(List<String> splitUserInput) {
+        this.numbers = new ArrayList<>();
+        makeNumbers(splitUserInput);
+    }
+
+    public void makeNumbers(List<String> splitUserInput) {
+        for (String splitInput : splitUserInput) {
+            numbers.add(isValidate(splitInput));
+        }
+    }
+
+    public Integer isValidate(String splitInput) {
+        if (compareSplitInputToZero(splitInput)) {
+            return ZERO;
+        }
+
+        try {
+            return Integer.parseInt(splitInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("에러 메시지-정수 변환");
+        }
+    }
+
+    public boolean compareSplitInputToZero(String splitInput) {
+        return splitInput.equals(ZERO_STRING);
+    }
+}
