9bdd218030960272c9aad24b0fe51bc79c9a6d17
jiffyin7 <jiffyin7@gmail.com>
2024-10-16 15:24:37 +0900

    feat(Application): StringAdder 클래스 추가
    - Application 클래스에 StringAdder.run() 호출 추가
    - StringAdder 클래스 생성 및 정적 run() 메서드 구현
    
    StringAdder의 상태를 유지할 필요가 없으므로, 객체 생성 없이 직접 호출.
    
    이슈: #2

---
+++ src/main/java/calculator/stringAdder/StringAdder.java
@@ -0,0 +1,15 @@
+package calculator.stringAdder;
+
+import calculator.stringAdder.inputvalidator.InputValidator;
+
+/**
+ * @author : jiffyin7@gmail.com
+ * @since : 24. 10. 16.
+ */
+public class StringAdder {
+  public static void run() {
+    while (true){
+      InputValidator inputValidator = new InputValidator();
+    }
+  }
+}
