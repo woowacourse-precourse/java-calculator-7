bfba69fd9b5bfe10b57538fe0406d0043e7f3253
김동찬 <come753zz@gmail.com>
2024-10-16 17:12:32 +0900

    feat(Application) 추출한 숫자를 sum해서 리턴합니다. 잘못된 입력있으면 예외발생
    - 숫자로 이루어진 String배열을 stream을 이용해 각각 Int형으로 parse하였습니다
    - 과정에서 음수가 들어오거나 잘못된 입력이 들어오면 예외를 발생시키게하였습니다
    - 각각의 숫자를 받아 더해서 반환하였습니다
    
    main에서 바로 적용하지 않고 sum메소드로 뽑아서 적용했습니다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,11 +2,33 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해주세요");
         String input= Console.readLine();
         String[] numbers;
+
+        ExtractNumber en= new ExtractNumber(input);
+        numbers=en.split();
+
+        System.out.println(sum(numbers));
+    }
+
+    private static int sum(String[] numbers){
+        try{
+            return Arrays.stream(numbers)
+                    .mapToInt(Integer::parseInt)
+                    .peek(num->{
+                        if(num<0){
+                            throw new IllegalArgumentException();
+                        }
+                    })
+                    .sum();
+        }catch(IllegalArgumentException e){
+            throw new IllegalArgumentException();
+        }
     }
 }
