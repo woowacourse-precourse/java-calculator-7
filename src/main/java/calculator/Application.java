7f30d75e410e827f267b65a383270f5246879271
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-16 23:21:43 +0900

    feat: 커스텀 구분자 검증을 위한 예외 처리 구현
    - 커스텀 구분자에 대한 검증 구현:
      - 입력이 "//"로 시작해야 함
      - 커스텀 구분자 앞에 문자 없어야 함
      - 커스텀 구분자에 이스케이프 문자는 없어야 함
      - 커스텀 구분자에 숫자는 없어야 함
    - 검증이 실패하면 IllegalArgumentException 발생.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -14,7 +14,15 @@ public class Application {
         String input = scanner.nextLine();
 
         if (input.contains("//") && input.contains("\\n")) {
+            if (!input.startsWith("//")) {
+                throw new IllegalArgumentException("잘못된 형식입니다.");
+            }
             String customDelimiter = input.split("\\\\n")[0].split("//")[1];
+            if (customDelimiter.contains("\\")) {
+                throw new IllegalArgumentException("이스케이프 문자가 포함되었습니다.");
+            } else if (customDelimiter.matches(".*\\d.*")) {
+                throw new IllegalArgumentException("커스텀 구분자엔 숫자가 포함될 수 없습니다.");
+            }
             delimiters.add(customDelimiter);
             input = input.split("\\\\n")[1];
         }
