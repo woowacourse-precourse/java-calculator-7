ea9001c23c880099fcd197b0224a201910d8ee6f
ghktndyd <hwasuyong99@naver.com>
2024-10-16 19:35:38 +0900

    feat(InputView): 입력받은 문자열을 구분자로 분리하는 기능 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,7 +1,7 @@
 package calculator.view;
 
 import camp.nextstep.edu.missionutils.Console;
-
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -18,8 +18,33 @@ public class InputView {
         }
 
         String separators = getSeparators(input);
+        String numbers = getNumbers(input);
 
-        return null;
+        return splitAndParseNumbers(separators, numbers);
+    }
+
+    private List<Integer> splitAndParseNumbers(String separators, String numbers) {
+        String[] inputTokens = numbers.split(separators);
+
+        return Arrays.stream(inputTokens)
+                .map(this::parseNumber)
+                .toList();
+    }
+
+    private int parseNumber(String token) {
+        try {
+            int number = Integer.parseInt(token);
+            validatePositiveNumber(number);
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    private void validatePositiveNumber(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("양수만 입력할 수 있습니다.");
+        }
     }
 
     private String getSeparators(String input) {
@@ -33,6 +58,15 @@ public class InputView {
         }
 
         return COMMA + "|" + COLON;
+    }
+
+    private String getNumbers(String input) {
+        if (hasCustomSeparator(input)) {
+            int separatorEndIndex = getSeparatorEndIndex(input);
+            return input.substring(separatorEndIndex + 1);
+        }
+
+        return input;
     }
 
     private boolean hasCustomSeparator(String input) {
