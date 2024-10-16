75049abcddf4ff5cda45f2ef8b7448a91ed8bb31
sunwon <jsw5913@naver.com>
2024-10-16 21:08:41 +0900

    test(StringParser): 공백 문자 검사 기능 테스트
    - 공백_문자일시_예외를_던진다()
    - 공백_문자가_아닐시_예외를_던지지않는다()

--- src/test/java/calculator/StringParserTest.java
+++ src/test/java/calculator/StringParserTest.java
@@ -3,11 +3,31 @@ package calculator;
 import calculator.model.StringParser;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class StringParserTest {
 
     private StringParser stringParser = new StringParser();
+
+    @Test
+    void 공백_문자일시_예외를_던진다() {
+        String blank = "";
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            stringParser.validateBlank(blank);
+        });
+    }
+
+    @Test
+    void 공백_문자가_아닐시_예외를_던지지않는다() {
+        // Given
+        String notBlank = "Not Blank";  // 빈 문자열이 아닌 유효한 문자열
+
+        // When & Then
+        assertDoesNotThrow(() -> {
+            stringParser.validateBlank(notBlank);
+        });
+    }
 
     @Test
     void 빈문자일시_false를_반환하다() {
