72901790d5db273e7e421aadef01c40c4ae93d3e
Hyeo_Ni__ <maehyong02@gmail.com>
2024-10-16 19:26:11 +0900

    feat: 기본 구분자(쉼표, 콜론)를 이용한 문자열 더하기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,13 +6,20 @@ import java.util.regex.Pattern;
 
 public class Application {
     private static String inputString;
+    private static String trimString;
+    private static String[] stringArray;
     private static int total;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         inputString = scanner.nextLine();
 
         if(inputString.isEmpty()) total = 0;
-
+        if(inputString.length() > 0) {
+            stringArray = inputString.split(",|:");
+            for (int i = 0; stringArray.length > i; i++) {
+                total += Integer.parseInt(stringArray[i]);
+            }
+        }
         System.out.println(total);
     }
 }
