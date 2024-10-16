bee2736e0e9570fc15707d9a8bf12f17fdfbe3f7
Hacanna42 <dtmdgus901@gmail.com>
2024-10-16 20:33:21 +0900

    docs: add comments to clarify code

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,18 +5,21 @@ import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
+        // 사용자로부터 문자열을 입력받음
         String input = Console.readLine();
 
+        // 구분자를 저장하기 위한 ArrayList
         ArrayList<String> delimiters = new ArrayList<>();
         delimiters.add(",");
         delimiters.add(":");
 
+        // 커스텀 구분자가 지정되어 있는지 확인 후, 지정되어 있다면 커스텀 구분자를 ArrayList에 추가
         if (hasCustomDelimiter(input)) {
             delimiters.add(getCustomDelimiter(input));
         }
     }
 
-    static boolean hasCustomDelimiter(String input) {
+    static boolean hasCustomDelimiter(String input) { // 커스텀 구분자가 지정되어 있는지 확인하는 함수
         return input.startsWith("//");
     }
 
