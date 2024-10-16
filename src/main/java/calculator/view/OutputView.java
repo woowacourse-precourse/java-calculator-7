02f13d1e26118058f0b3ba04bd1fa5765a1bd23a
woogym <kwj9294@gmail.com>
2024-10-16 23:24:30 +0900

    fix(view): delete Unnecessary line modulation

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -3,7 +3,6 @@ package calculator.view;
 public class OutputView {
 
     public void printCalculateResult(int result) {
-        System.out.println();
         System.out.println("결과 : " + result);
     }
 }
