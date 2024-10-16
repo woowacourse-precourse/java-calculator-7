e88fb834713a237bee6208f5aa719bd4b7c28cd8
sejinezy <psj8880@naver.com>
2024-10-17 01:32:09 +0900

    feat(input): add method to accept string input from console
    Add getInput method to prompt the user with a message and capture the input using Console.readLine().

---
+++ src/main/java/calculator/ConsoleIO.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleIO {
+
+    public String getInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+}
