5060cb617dd5037904425ca9e82f139064229493
jklee3409 <jklee3409@khu.ac.kr>
2024-10-16 12:49:01 +0900

    feat(calculator) : performAddition 메서드에서 문자열의 유효성 확인 기능 추가

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -6,6 +6,10 @@ public class Calculator {
     public static int performAddition(String input) {
         List<String> numbers = Parser.splitInput(input);
 
+        if (!isValidNumberString(numbers)) {
+            throw new IllegalArgumentException("숫자와 구분자 외의 문자가 포함되어 있습니다.");
+        }
+
         return numbers.stream()
                 .mapToInt(Integer::parseInt)
                 .sum();
