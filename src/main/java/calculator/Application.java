f54a6ba180a227ac9c70e30e732daff3b576fb1b
StoneCAU <seokwoo1999@naver.com>
2024-10-16 14:00:43 +0900

    feat: 사용자 문자열 입력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static final String ASK_INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final String input;
+
+        System.out.println(ASK_INPUT_MESSAGE);
+        input = Console.readLine();
     }
 }
