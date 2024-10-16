6a536c2b7566db78937404209c5cb3a8fe2c7a0b
ruudska6 <feels19@naver.com>
2024-10-16 11:45:58 +0900

    <feat>:  사용자가 문자열을 입력하는 기능 userInputStr() 메서드

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,8 @@
+package calculator.view;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String userInputStr() {
+        return Console.readLine();
+    }
+}
