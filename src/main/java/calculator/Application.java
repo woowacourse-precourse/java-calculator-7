7b940716ce626184697f6a808c1dc04cdbd92f2d
binn <anyubin0724@naver.com>
2024-10-16 21:26:12 +0900

    fix : Application.java 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,10 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
-    
-    private static boolean isValidPositive(String input) {
-        // TODO: 음수 or 빈 문자열 or null이면 false
-        return true;
+        CalculatorController.add();
     }
 }
