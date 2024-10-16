328d84bdf56ff61211c274a79074e9a4de3bcb1b
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-10-16 14:50:22 +0900

    테스트 코드 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,6 +1,10 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -23,6 +27,39 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 숫자_하나만_입력되었을_때() {
+        assertEquals(1, StringAddCalculator.add("1"));
+    }
+
+    @Test
+    void 빈_문자열_테스트() {
+        assertEquals(0, StringAddCalculator.add(""));
+    }
+
+    @Test
+    void 쉼표_구분자로_숫자_덧셈_테스트() {
+        assertEquals(6, StringAddCalculator.add("1,2,3"));
+    }
+
+    @Test 
+    void 콜론_구분자로_숫자_덧셈_테스트() {
+        assertEquals(6, StringAddCalculator.add("1:2:3"));
+    }
+
+    @Test
+    void 쉼표_콜론_둘다() {
+        assertEquals(6, StringAddCalculator.add("1:2:3"));
+    }
+
+    @Test
+    void 음수_입력_시_오류발생() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            StringAddCalculator.add("1,-2,3");
+        });
+        assertEquals("음수는 허용되지 않습니다: -2", exception.getMessage());
+    } 
 
     @Override
     public void runMain() {
