bb1155f46f26e2a936313a3186772951afe3c974
imjunghun <dlawndgns9@naver.com>
2024-10-16 17:21:07 +0900

    feat(CustomSeparator): 커스텀 구분자를 가지고 있는 CustomSeparator 클래스 구현

---
+++ src/main/java/calculator/domain/separator/CustomSeparator.java
@@ -0,0 +1,10 @@
+package calculator.domain.separator;
+
+public class CustomSeparator {
+
+    private final String separator;
+
+    public CustomSeparator(String separator) {
+        this.separator = separator;
+    }
+}
