3e93f52fa80fc3854ff4573ccad3d70aacacafb3
world-dv <goodforx0147@naver.com>
2024-10-16 20:00:44 +0900

    refactor: static를 추가하고 특수 문자를 사용하기 위한 처리를 합니다.
    - 객체 생성없이 함수를 사용하기 위해 static을 추가합니다.
    - 특수 문자 \n을 문자열로 사용하기 위해 \(역슬래시)를 추가해 인식 가능하도록 변경했습니다.
    - 문자열에서 \n의 인덱스를 +2하여 시작 위치가 아닌 끝 위치를 가질 수 있도록 수정했습니다.

--- src/main/java/calculator/service/CalculateCustom.java
+++ src/main/java/calculator/service/CalculateCustom.java
@@ -5,37 +5,37 @@ import java.util.List;
 public class CalculateCustom {
 
     private static final String CUSTOM_PRE = "//";
-    private static final String CUSTOM_POST = "\n";
+    private static final String CUSTOM_POST = "\\n";
 
-    public boolean checkContainCUSTOM_PRE(String userInput) {
+    public static boolean checkContainCUSTOM_PRE(String userInput) {
         return userInput.contains(CUSTOM_PRE);
     }
 
-    public boolean checkContainCUSTOM_POST(String userInput) {
+    public static boolean checkContainCUSTOM_POST(String userInput) {
         return userInput.contains(CUSTOM_POST);
     }
 
-    public boolean checkContainCUSTOM(String userInput) {
+    public static boolean checkContainCUSTOM(String userInput) {
         return checkContainCUSTOM_PRE(userInput) && checkContainCUSTOM_POST(userInput);
     }
 
-    public Integer getCustomPreIndex(String userInput) {
+    public static Integer getCustomPreIndex(String userInput) {
         return userInput.indexOf(CUSTOM_PRE);
     }
 
-    public Integer getCustomPostIndex(String userInput) {
-        return userInput.indexOf(CUSTOM_POST);
+    public static Integer getCustomPostIndex(String userInput) {
+        return userInput.indexOf(CUSTOM_POST) + 2;
     }
 
-    public String getCustomPreToPost(String userInput) {
+    public static String getCustomPreToPost(String userInput) {
         return userInput.substring(getCustomPreIndex(userInput), getCustomPostIndex(userInput));
     }
 
-    public String getCustomSeparator(String userInput) {
+    public static String getCustomSeparator(String userInput) {
         return getCustomPreToPost(userInput).replaceAll(CUSTOM_PRE+CUSTOM_POST, "");
     }
 
-    public List<String> splitWithCustom(String userInput) {
+    public static List<String> splitWithCustom(String userInput) {
         return List.of(userInput.split(getCustomSeparator(userInput)));
     }
 }
