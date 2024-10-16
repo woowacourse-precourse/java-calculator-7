64851c12b5a92393f8c2853ddc42e61f72146289
joonwan <joonwan1024@naver.com>
2024-10-16 15:26:31 +0900

    refactor: input message 를 class 멤버에서 지역 변수로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,13 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
 
-    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
-
     public static void main(String[] args) {
         String inputData = getInputData();
     }
 
     private static String getInputData() {
+        String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
         System.out.println(INPUT_MESSAGE);
         return Console.readLine();
     }
