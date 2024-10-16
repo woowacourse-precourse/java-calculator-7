3bbd7674f0e925c07a75d2a155bdc7b133c15e16
Taaate <an981022@naver.com>
2024-10-16 10:44:23 +0900

    feat: 커스텀 구분자 추출을 위한 구분자 예외 처리

--- src/main/java/calculator/service/DelimiterExtractor.java
+++ src/main/java/calculator/service/DelimiterExtractor.java
@@ -9,9 +9,23 @@ public class DelimiterExtractor {
     private static final String EMPTY = "";
     
     public List<String> extractDelimiter(String input) {
-        String[] split = input.split(SUFFIX);
-        String firstHalf = split[0].replaceAll(PREFIX, EMPTY);
+        String[] split = getSplitSuffix(input);
+        String firstHalf = getSplitPrefix(split[0]);
         String[] delimiters = firstHalf.split(EMPTY);
         return List.of(delimiters);
+    }
+
+    private String getSplitPrefix(String split) {
+        if (!split.startsWith(PREFIX)) {
+            throw new IllegalArgumentException("// 로 시작되어야 합니다.");
+        }
+        return split.replaceAll(PREFIX, EMPTY);
+    }
+
+    private String[] getSplitSuffix(String input) {
+        if (!input.contains(SUFFIX)) {
+            throw new IllegalArgumentException("\\n 으로 분리되어야 합니다.");
+        }
+        return input.split(SUFFIX);
     }
 }
