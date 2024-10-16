fee3360e5a00aa8ac209fad0029bc1ea67612de2
Hyungjung Kim <gudwnd1113@gmail.com>
2024-10-16 18:32:35 +0900

    feat(Machine): define Machine class
    Machine 클래스 정의
    Machine 클래스의 생성자 구현
    사용자의 입력을 받아 변수에 저장 후 반환하는 getString() 함수 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+}
+
+
+class Machine {
+    public Machine() {
+    }
+
+    private static String getString() {
+        String input;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        input = Console.readLine();
+        return input;
     }
 }
