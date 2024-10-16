f73b10c9e5a55fb875b9e463fe939342aaf29340
Hyungjung Kim <gudwnd1113@gmail.com>
2024-10-16 23:32:22 +0900

    feat(Machine): define setDelimiter(), escapeMetaChars() method
    정규식을 사용하여 커스텀 구분자를 추출하는 setDelimiter 함수 정의.
    정규식에서 특수한 의미를 가지는 메타문자가 커스텀 구분자로 입력될 경우,
    이에 대해 이스케이프 문자'\\'를 추가하는 escapeMetaChars 함수 정의.
    
    Machine 클래스에 대해 클래스 변수 metaChars, custom, string 선언.
    metaChars는 정규식에서이 메타문자가 나열된 문자열.
    custom은 Machine 인스턴스에서 커스텀 구분자를 저장하기 위한 String 변수.
    string은 Machine 인스턴스에서 사용자가 입력한 문자열을 저장하기 위한
    String 변수.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -10,6 +12,11 @@ public class Application {
 
 
 class Machine {
+    static final String metaChars = ".^$*+?()[]{}|\\";
+
+    String custom;
+    String string;
+
     public Machine() {
     }
 
@@ -21,7 +28,36 @@ class Machine {
         return input;
     }
 
-    public static void printResult(int data) {
+    private static void printResult(int data) {
         System.out.printf("결과 : %d", data);
+    }
+
+    private static String escapeMetaChars(String input) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            // 입력 문자가 정규식 메타 언어일 때
+            if (metaChars.indexOf(c) != -1) {
+                result.append('\\');
+            }
+            result.append(c);
+        }
+        return result.toString();
+    }
+
+    private void setDelimiter(String string) {
+        String regex = "//(.)\\\\n";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(string);
+
+        if (matcher.find()) {
+            // 커스텀 구분자 지정 문자열이 입력문자열의 앞이 아닐 때, 예외 처리
+            if (matcher.start() != 0) {
+                throw new IllegalArgumentException("커스텀 구분자가 문자열의 앞이 아닌 부분에서 정의됨");
+            }
+
+            this.custom = escapeMetaChars(matcher.group(1));
+            this.string = string.substring(matcher.end());
+        }
     }
 }
