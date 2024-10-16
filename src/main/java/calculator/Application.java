fa76fcce4cf25b8e6746089611679f7241f9258a
juhyeongKim <zxcvbm804@gmail.com>
2024-10-16 22:36:13 +0900

    feat: 구분자를 반환하는 기능 추가
    Calculator 클래스 내부에 구분자를 반환하는 getDelimiter 메소드를 추가
    커스텀 구분자가 있는지 확인하여 커스텀 구분자 또는 기본 구분자 2개를
    리스트에 추가한다. 리스트를 래핑하여 반환한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
 
@@ -13,13 +15,25 @@ public class Application {
 class Calculator {
 
     static Str str;
-    static char[] delimiterArr;
+    static Delimiters delimiters;
 
     Calculator() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         str = new Str(Console.readLine());
+        delimiters = getDelimiter();
     }
 
+    public static Delimiters getDelimiter() {
+        List<Character> delimiterList = new ArrayList<>();
+        if(str.checkingCustomDelimiter()){
+            delimiterList.add(str.getCustomDelimiter());
+            return new Delimiters(delimiterList);
+        }
+
+        delimiterList.add(',');
+        delimiterList.add(':');
+        return new Delimiters(delimiterList);
+    }
 }
 
 class Str {
@@ -34,5 +48,16 @@ class Str {
         return (string.substring(0, 2).equals("//") && string.charAt(3) == '\\'
             && string.charAt(4) == 'n');
 
+    }
+
+    Character getCustomDelimiter(){
+        return string.charAt(2);
+    }
+}
+
+class Delimiters{
+    private List<Character> delimiterList;
+    public Delimiters(List<Character> delimiterList){
+        this.delimiterList = delimiterList;
     }
 }
