5a694e967b24bd4bd9fa6ef69145c54ef8cce4ed
seahwanKim <kimkimseasea@gmail.com>
2024-10-16 16:50:22 +0900

    test: 일반 구분자 사용 테스트코드를 작성한다

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,46 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 일반_구분자_사용1() {
+        assertSimpleTest(() -> {
+            run("8,5,7");
+            assertThat(output()).contains("결과 : 20");
+        });
+    }
+
+    @Test
+    void 일반_구분자_사용2() {
+        assertSimpleTest(() -> {
+            run("3,15;3");
+            assertThat(output()).contains("결과 : 21");
+        });
+    }
+
+    @Test
+    void 일반_구분자_사용3() {
+        assertSimpleTest(() -> {
+            run("21;1,2");
+            assertThat(output()).contains("결과 : 24");
+        });
+    }
+
+    @Test
+    void 일반_구분자_사용4() {
+        assertSimpleTest(() -> {
+            run("11;12;13");
+            assertThat(output()).contains("결과 : 36");
+        });
+    }
+
+    @Test
+    void 일반_구분자_사용5() {
+        assertSimpleTest(() -> {
+            run("13,2,3;6,3;17;32;12,1;1");
+            assertThat(output()).contains("결과 : 90");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
