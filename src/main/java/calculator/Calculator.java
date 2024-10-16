8224198bb1380e33f8cfe1a7c370bb86e2c76e1d
hojinida <opqrs1103@gmail.com>
2024-10-17 00:37:09 +0900

    feat(Calculator): 입력 받는 기능 구현
    - Console의 readLine() 통해 사용자가 입력한 문자열을 받는다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Calculator {
     public void displayInputMessage() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public String getInput() {
+        return Console.readLine();
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,12 +3,14 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
+
     @Test
     @DisplayName("입력 메시지를 출력한다.")
     void 입력_메시지_출력() {
@@ -26,5 +28,23 @@ public class CalculatorTest {
 
         // Cleanup
         System.setOut(originalOut);
+    }
+
+    @Test
+    @DisplayName("입력 값을 반환한다.")
+    void 입력_값_반환() {
+        // Given
+        Calculator calculator = new Calculator();
+        String input = "입력값";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // When
+        String result = calculator.getInput();
+
+        // Then
+        assertThat(result).isEqualTo(input);
+
+        System.setIn(System.in);
     }
 }
