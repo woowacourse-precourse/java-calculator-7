58ca4aaef765d0a952dbbf5c65ce6ebabafba0fd
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-17 02:40:13 +0900

    style(PreprocessorTest): 코드 포맷팅

--- src/test/java/calculator/domain/preprocess/PreprocessorTest.java
+++ src/test/java/calculator/domain/preprocess/PreprocessorTest.java
@@ -19,7 +19,7 @@ class PreprocessorTest {
     private Preprocessor preprocessor;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         preprocessor = new Preprocessor(
                 new DelimeterProcessor(),
                 new InputManager(),
@@ -28,55 +28,55 @@ class PreprocessorTest {
 
     @Test
     @DisplayName("preprocessDelimeter 예외 발생 테스트")
-    void preprocessDelimeter_test_o(){
+    void preprocessDelimeter_test_o() {
 
         String given = "//3\n2,3,4";
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, ()->{
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
             preprocessor.preprocessDelimeter(given);
         });
 
-        assertEquals(e.getMessage(),"구분자로 숫자를 입력했습니다");
+        assertEquals(e.getMessage(), "구분자로 숫자를 입력했습니다");
     }
 
     @Test
     @DisplayName("preprocessDelimeter 기능 테스트")
-    void preprocessDelimeter_test_x(){
+    void preprocessDelimeter_test_x() {
 
         String given = "//;\n2,3,4";
-        assertEquals(preprocessor.preprocessDelimeter(given), Arrays.asList(",",":",";"));
+        assertEquals(preprocessor.preprocessDelimeter(given), Arrays.asList(",", ":", ";"));
     }
 
     @Test
     @DisplayName("preprocessString 커스텀 요청 있는 경우")
-    void preprocessString_with_customDelimeter(){
+    void preprocessString_with_customDelimeter() {
         String given = "//>\n2,3,4";
-        assertEquals(preprocessor.preprocessString(given),"2,3,4");
+        assertEquals(preprocessor.preprocessString(given), "2,3,4");
     }
 
     @Test
     @DisplayName("preprocessString 커스텀 요청 없는 경우")
-    void preprocessString_without_customDelimeter(){
+    void preprocessString_without_customDelimeter() {
         String given = "//2,3,4";
-        assertEquals(preprocessor.preprocessString(given),"//2,3,4");
+        assertEquals(preprocessor.preprocessString(given), "//2,3,4");
     }
 
 
     @Test
     @DisplayName("validateInputString 예외 발생 테스트")
-    void validateInputString_exception_o(){
+    void validateInputString_exception_o() {
         String input = "//2,3,4";
-        List<String> delimeterList = new ArrayList<>(Arrays.asList(",",":"));
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, ()->{
-            preprocessor.validateInputString(delimeterList,input);
+        List<String> delimeterList = new ArrayList<>(Arrays.asList(",", ":"));
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            preprocessor.validateInputString(delimeterList, input);
         });
-        assertEquals(e.getMessage(),"구분자 이외의 문자가 있습니다.");
+        assertEquals(e.getMessage(), "구분자 이외의 문자가 있습니다.");
     }
 
     @Test
     @DisplayName("validateInputString 예외 발생 x 테스트")
-    void validateInputString_exception_x(){
+    void validateInputString_exception_x() {
         String input = "2,3:4";
-        List<String> delimeterList = new ArrayList<>(Arrays.asList(",",":"));
-        assertDoesNotThrow(()->preprocessor.validateInputString(delimeterList,input));
+        List<String> delimeterList = new ArrayList<>(Arrays.asList(",", ":"));
+        assertDoesNotThrow(() -> preprocessor.validateInputString(delimeterList, input));
     }
 }
