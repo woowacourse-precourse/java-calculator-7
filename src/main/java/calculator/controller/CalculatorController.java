f2f3c0d54350249cec73a6fb027c1e7511aedb9b
Sinorang <sinholang@naver.com>
2024-10-17 03:39:17 +0900

    feat: CalculatorController 구현
    - run() 메서드를 추가해 입력, 계산, 출력 흐름 처리

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,16 @@
 package calculator.controller;
 
+import calculator.domain.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class CalculatorController {
+
+    private final Calculator calculator = new Calculator();
+
+    public void run() {
+        String input = InputView.getInput();
+        int result = calculator.splitAndSum(input);
+        OutputView.printResult(result);
+    }
 }
