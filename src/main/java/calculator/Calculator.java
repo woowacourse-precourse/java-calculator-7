d77e82ac04d134bdd549ac9ac2a25902f243e368
mohyerolo <garam2101@naver.com>
2024-10-16 16:58:55 +0900

    feat: 숫자 계산 메서드, 결과 출력 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -11,8 +11,19 @@ public class Calculator {
         try {
             Input input = new Input(Console.readLine());
             String[] splitString = splitManager.splitString(input);
+
+            int sum = !splitString[0].isEmpty() ? sum(splitString) : 0;
+            System.out.println("결과 : " + sum);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private int sum(String[] splitString) {
+        int sum = 0;
+        for (String str : splitString) {
+            sum += Integer.parseInt(str);
+        }
+        return sum;
     }
 }
