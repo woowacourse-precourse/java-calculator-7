5480b077a383f502617db116e7cbe91143aa6815
hyunwon <khw73850@gmail.com>
2024-10-16 21:58:47 +0900

    feat: validate input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -17,7 +17,7 @@ public class Application {
 
         input = parseCustomDelimiter(input);
 
-
+        if(validateInput(input)) throw new IllegalArgumentException("잘못된 입력입니다.");
     }
 
     private static String parseCustomDelimiter(String str){
@@ -28,4 +28,17 @@ public class Application {
         return str;
     }
 
+    private static boolean validateInput(String input){
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if(!isDigit(ch) && !delimContainer.contains(ch)) return true;
+        }
+
+        return false;
+    }
+
+    private static boolean isDigit(char ch){
+        int chValue = ch - '0';
+        return chValue >= 0 && chValue <= 9;
+    }
 }
