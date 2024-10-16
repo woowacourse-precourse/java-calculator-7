8fbff8b1bdedae710c12e70b9d172391b657b1a4
bogyeom0922 <pbk0398@gmail.com>
2024-10-16 21:58:06 +0900

    feat: 사용자로부터 입력 받는 메서드 생성
    Scanner 객체를 이용해 콘솔로부터 사용자 입력을 받고,
    입력값이 빈 문자열인 경우 IllegalArgumentException을 발생시킴

--- src/main/java/calculator/view/InputReader.java
+++ src/main/java/calculator/view/InputReader.java
@@ -1,4 +1,22 @@
 package calculator.view;
 
+import java.util.Scanner;
+
 public class InputReader {
+
+    private Scanner scanner;
+
+    public InputReader() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String readLine() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = scanner.nextLine();
+        if (input.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return input;
+    }
+
 }
