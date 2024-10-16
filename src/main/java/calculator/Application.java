47117a6290b5ccf9e3b8d73d438a3c36e4655d84
corpi <lj845854@gmail.com>
2024-10-16 23:18:41 +0900

    feat(Application): 문자열 덧셈 계산기 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringCalculator stringCalculator = new StringCalculator();
+        InputOutputService inputOutputService = new InputOutputService();
+
+        String input = inputOutputService.fetchUserInput();
+        List<String> splitString = stringCalculator.splitDelimiterAndNumbers(input);
+
+        String delimiterCandidates = splitString.get(0);
+        String customDelimiter = stringCalculator.extractDelimiter(delimiterCandidates);
+
+        String numbersCandidates = splitString.get(1);
+        List<Double> numberList = stringCalculator.splitNumbersByDelimiter(numbersCandidates, customDelimiter);
+
+        Double sum = stringCalculator.calculate(numberList);
+        inputOutputService.printResult(sum);
     }
 }
