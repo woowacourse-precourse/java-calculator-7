43193e960eddafc605640808081af399ca666d0d
hyojun <hjlim45678@gmail.com>
2024-10-16 10:58:18 +0900

    feat: 문자열을 입력받는 기능 구현

---
+++ src/main/java/calculator/MainController.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class MainController {
+
+    public static void run() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputString = Console.readLine();
+    }
+}
