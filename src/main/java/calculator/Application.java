0a6cc7c30d66eddeffa99febfaf19e700a3ecf86
jiyun <tomatoziiilll@gmail.com>
2024-10-16 13:45:33 +0900

    feat: extracted strings include custom separator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,13 +8,29 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Application {
+    static String cutomDelimStart = "//";
+    static String cutomDelimEnd = "\\n";
+    static String regexDelim = "|";
+
     public static void main(String[] args) {
+        StringBuilder regex = new StringBuilder(",|:");
         String input = Console.readLine();
-        List<Integer> numbers = Arrays.stream(input.split("[,:]"))
+
+        if (input.substring(0, 2).equals(cutomDelimStart)) {
+            if (input.contains(cutomDelimEnd)) {
+                String customDelim = input.substring(2, input.indexOf(cutomDelimEnd));
+
+                regex.append(regexDelim + customDelim);
+            } else {
+                // error
+            }
+            input = input.substring(input.indexOf(cutomDelimEnd) + cutomDelimEnd.length());
+        }
+
+        List<String> strs = Arrays.stream(input.split(regex.toString()))
                 .filter(s -> !s.isEmpty())
-                .map(Integer::parseInt)
                 .collect(Collectors.toCollection(ArrayList::new));
 
-        System.out.println(numbers);
+        System.out.println(strs);
     }
 }
