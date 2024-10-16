f1870174e72d65530310a2705fc8a1756c391f43
icebear0111 <wltjq0901@naver.com>
2024-10-16 21:56:39 +0900

    feat: Add feature for user output
    덧셈 결과를 출력하는 기능 생성

---
+++ src/main/java/calculator/view/UserOutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class UserOutputView {
+
+    public static void outputResult(int result) {
+
+        System.out.println("결과 : " + result); // 결과를 출력한다.
+    }
+}
