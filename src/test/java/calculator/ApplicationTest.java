ed598ea00535134c33acbab494b495e56435cc63
Mint <audwl03071@gmail.com>
2024-10-16 10:47:05 +0900

    test: 테스트 작성
    - TDD에 따라 테스트 먼저 작성한다.
    - 테스트는 구분자, 숫자, 계산기 로직으로 구분한다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,27 +1,118 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
-    @Test
-    void 커스텀_구분자_사용() {
-        assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
-        });
+
+    @Nested
+    class DelimiterTest {
+        @Test
+        void 성공_기본구분자() {
+            assertSimpleTest(() -> {
+                run("1,2:3");
+                assertThat(output()).contains("결과 : 6");
+            });
+        }
+
+        @Test
+        void 성공_커스텀구분자() {
+            assertSimpleTest(() -> {
+                run("//;\\n1");
+                assertThat(output()).contains("결과 : 1");
+            });
+        }
+
+        @Test
+        void 성공_커스텀구분자_여러개의구분자() {
+            assertSimpleTest(() -> {
+                run("//;\\n//!\\n1;2!3");
+                assertThat(output()).contains("결과 : 6");
+            });
+        }
+
+        @Test
+        void 성공_커스텀구분자_여러글자로이루어진구분자() {
+            assertSimpleTest(() -> {
+                run("//!5!\\n1!5!2");
+                assertThat(output()).contains("결과 : 3");
+            });
+        }
+
+        @Test
+        void 실패_커스텀구분자_숫자로만이루어진구분자() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("//5\\n15253"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        void 실패_커스텀구분자_기본구분자가포함된구분자() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("//,2\\n1,22,23"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
     }
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+    @Nested
+    class NumberTest {
+        @Test
+        void 실패_음수() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("-1,2,3"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        void 실패_소수() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("1.5,2,3"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        void 실패_범위를넘어가는수() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException((long) Math.pow(2, 32) + ",2,3"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        void 실패_숫자로변환되지않은문자() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("a,2,3"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+    }
+
+    @Nested
+    class LogicTest {
+        @Test
+        void 실패_결과값오버플로우() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException((int) Math.pow(2, 31) - 1 + ",2,3"))
+                            .isInstanceOf(IllegalStateException.class)
+            );
+        }
+
+        @Test
+        void 실패_결과값언더플로우() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException((int) Math.pow(2, -31) + ",-2,-3"))
+                            .isInstanceOf(IllegalStateException.class)
+            );
+        }
     }
 
     @Override
