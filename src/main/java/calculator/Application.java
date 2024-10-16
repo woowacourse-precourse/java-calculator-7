ff2f246f5f0ec8ef1bbdfedbcb83ec1d86f508be
zzzyoonnn <dwsrh03@gmail.com>
2024-10-16 14:23:52 +0900

    feat: 전역 변수 선언

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.*;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
+    static String input;
+    static String delimiter = ",|:";
+    static String regex = "^//(.+)\\\\n(.+)$";
+    static Pattern pattern;
+    static Matcher matcher;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        input = Console.readLine();
     }
 }
