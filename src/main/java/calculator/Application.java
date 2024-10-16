9588faf605550ad0e088f019ff86e31378a566ae
seaniiio <cnc4750@naver.com>
2024-10-17 01:12:53 +0900

    feat(Application): main 로직 수행을 위한 코드 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.controller.CalculatorOperation;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CalculatorOperation calculatorOperation = new CalculatorOperation();
+        calculatorOperation.operateCalculator();
     }
 }
