0839512fe8f4522a5fddac619edb858fbcdc6061
jijongkwon <help3451@naver.com>
2024-10-17 00:17:59 +0900

    feat(util): NumberValidator 클래스에 숫자 유효성 검증 로직 구현
    - isValidNumber() 메서드에서 문자열 입력 값이 숫자인지 검증
     - 숫자가 아닌 문자가 있는 경우 InvalidInputException 발생
    - 음수인 경우에도 InvalidInputException 발생

--- src/main/java/calculator/util/NumberValidator.java
+++ src/main/java/calculator/util/NumberValidator.java
@@ -1,8 +1,17 @@
 package calculator.util;
 
+import calculator.exception.InvalidInputException;
+
 public class NumberValidator {
-    public static boolean isValidNumber(String number) {
+    public static void isValidNumber(String numberString) {
         //TODO: 숫자 유효성 검증 유틸리티 메서드 구현
-        return false;
+        try {
+            int number = Integer.parseInt(numberString.trim());
+            if (number < 0) {
+                throw new InvalidInputException("음수는 허용되지 않습니다: " + number);
+            }
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException("유효하지 않은 숫자 형식입니다: " + numberString);
+        }
     }
 }
