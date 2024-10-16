46712a16abdf1d2d0a7d3091378bafb5cd082934
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-16 12:23:49 +0900

    feat(StringParser): 쉼표(,) 또는 콜론(:)에 따라 문자열을 구분하는 기능 추가
    쉼표(,) 또는 콜론(:)에 따라 문자열을 구분하는 기능 추가한다.

--- src/main/java/calculator/domain/StringParser.java
+++ src/main/java/calculator/domain/StringParser.java
@@ -1,14 +1,22 @@
 package calculator.domain;
 
+import java.util.List;
+
 public class StringParser {
 
     private final static int INITIAL_NUMBER = 0;
     private final static int FIRST_NUMBER = 1;
+    private final static String STANDARD_DELIMITER = "[,|:]";
 
     private final String inputStrings;
 
     public StringParser(final String inputStrings) {
         this.inputStrings = inputStrings;
+    }
+
+    private List<String> parseStandardStrings(){
+        String[] standardStrings = inputStrings.split(STANDARD_DELIMITER);
+        return List.of(standardStrings);
     }
 
     private String paresFirstString() {
