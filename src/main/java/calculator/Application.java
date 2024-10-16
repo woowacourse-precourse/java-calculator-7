618042184285767c9a99f3077d04ef217b51b30a
uniiii <126189239+fjqmqjrm@users.noreply.github.com>
2024-10-17 00:26:38 +0900

    feat: 사용자 입력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,12 @@
 package calculator;
 
+import calculator.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = InputView.getInput();
+
+
 
     }
 }
