15b0adf4d5a8e894e0dd5d1c8c5b9243afed6d47
yunsseong <aster20@hanyang.ac.kr>
2024-10-16 16:12:15 +0900

    feat: 구분자 객체 구현

---
+++ src/main/java/calculator/model/Delimiter.java
@@ -0,0 +1,10 @@
+package calculator.model;
+
+public class Delimiter {
+
+    private final String delimiter;
+
+    public Delimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+}
