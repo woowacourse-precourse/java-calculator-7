65cb17f8a79e022a53db908decd76b54fa1911b0
aaahyunseo <hs030608@naver.com>
2024-10-16 20:07:51 +0900

    feat(Application): 구분자에 따라 문자열 분리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,5 +7,16 @@ public class Application {
         // 문자열 입력받기
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
+
+        // 구분자에 따라 분류하기
+        String[] strArr;
+        int sum = 0;
+        if(str.startsWith("//") && str.contains("\\n")){
+            String op = str.substring(2,3);
+            str = str.substring(5);
+            strArr = str.split(op);
+        }else {
+                strArr = str.split(",|:");
+        }
     }
 }
