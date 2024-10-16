a7d211d4bb023a0edd956d1b63dd2fe1dad1dcec
capDoYeonLee <ghtyu21@naver.com>
2024-10-17 03:05:06 +0900

    fix: Scanner 객체 생성 방식 수정

--- src/main/java/calculator/InputClass.java
+++ src/main/java/calculator/InputClass.java
@@ -7,10 +7,10 @@ public class InputClass {
     private final String LEFT_CUSTOM_SEPARATOR = "//";
     private final String RIGHT_CUSTOM_SEPARATOR = "\\n";
     private static final String INPUT_SENTENCE = "덧셈할 문자열을 입력해 주세요.";
-    private static final Scanner scanner = new Scanner(System.in);
 
     public static String input() {
         System.out.println(INPUT_SENTENCE);
+        Scanner scanner = new Scanner(System.in);
         String inputText = scanner.nextLine();
         return inputText;
     }
