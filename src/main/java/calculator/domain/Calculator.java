50c99c8a536900a67a5ac7141187d5d7854e014a
tentenball <lth8905@gmail.com>
2024-10-17 01:46:32 +0900

    feat, docs: 구분자 Set자료구조로 변경, docs 구현 순서 변경

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,14 +1,21 @@
 package calculator.domain;
 
 import java.util.List;
+import java.util.Set;
 
 public class Calculator {
 	private final List<Long> calculationNumbers;
+	private final Set<String> delimiters;
 	private double sum;
 
-	public Calculator(List<Long> calculationNumbers) {
+	public Calculator(List<Long> calculationNumbers, Set<String> delimiters) {
 		this.calculationNumbers = calculationNumbers;
+		this.delimiters = delimiters;
 		this.sum = 0L;
+	}
+
+	public Set<String> getDelimiters() {
+		return delimiters;
 	}
 
 	public List<Long> getCalculationNumbers() {
