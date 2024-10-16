6457fc29a4e9ddfd4548c0551086fb1d139b9695
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-10-16 19:27:02 +0900

    반복 문구 수정

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,7 +2,6 @@ package calculator;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.ArrayList;
 import java.util.List;
