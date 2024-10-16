4b89350047a83df0089cf76d9e70c69e027ad0d5
kjyyjk <rlawnsdud920@gmail.com>
2024-10-17 00:10:16 +0900

    feat: 커스텀 구분자 추출 구현
    - 커스텀 구분자가 존재하지 않으면 customDelimiter는 null
    - 커스텀 구분자에 입력이 들어오지 않거나 2자 이상 들어오면, 즉 커스텀 구분자의 길이가 1이 아니면 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,5 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String input = Console.readLine();
+
+        Character customDelimiter = null;
+
+        if (input.startsWith("//") && input.contains("\\n")) { //커스텀 구분자 존재 시
+            String customDelimiterStr = input.substring(2, input.lastIndexOf("\\n"));
+
+            if (customDelimiterStr.length() != 1) { //커스텀 구분자의 길이가 1인지 확인
+                throw new IllegalArgumentException();
+            }
+
+            customDelimiter = customDelimiterStr.charAt(0);
+        }
     }
 }
