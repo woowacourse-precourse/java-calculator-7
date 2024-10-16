21d3b0523fe6c2f3f8eaedd55ad331e641a93a39
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-16 23:08:02 +0900

    feat: 구분자로 문자열 분할 기능 구현
    구현:
      - 기본 구분자(`,` 또는 `:`)로 입력 문자열을 분할하는 기능
      - 커스텀 구분자를 처리하는 기능
    - "\\\\n"을 사용하여 문자열 내의 줄바꿈 문자를 올바르게 처리.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,24 @@
 package calculator;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Application {
 
     public static void main(String[] args) {
 
+        Set<String> delimiters = new HashSet<>(Set.of(",", ":"));
         Scanner scanner = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = scanner.nextLine();
+
+        if (input.contains("//") && input.contains("\\n")) {
+            String customDelimiter = input.split("\\\\n")[0].split("//")[1];
+            delimiters.add(customDelimiter);
+            input = input.split("\\\\n")[1];
+        }
+
+        String[] numberTokens = input.split(String.join("|", delimiters));
     }
 }
