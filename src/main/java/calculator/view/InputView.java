51150fa89caa0eb567f3e2aebb13b662dbf24cc5
joungeun0825 <joungeun0825@gmail.com>
2024-10-16 14:43:40 +0900

    feat(inputview): 사용자로부터 문자열 받기 기능 구현

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,12 @@
+package calculator.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    private InputView(){}
+
+    public static String scanString(){
+        return readLine();
+    }
+}
