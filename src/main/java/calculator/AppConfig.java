48cb78a9c9b57c8c99fc450a26e7e94ec836fc4e
ouvir <kilhyunii@naver.com>
2024-10-16 20:26:40 +0900

    feat: AppConfig 에서 의존성 관리

--- src/main/java/calculator/AppConfig.java
+++ src/main/java/calculator/AppConfig.java
@@ -1,20 +1,28 @@
 package calculator;
 
+import binaryOperator.BinaryOperator;
 import detector.CustomSeparatorDetector;
 import detector.CustomSeparatorDetectorImpl;
+import binaryOperator.PlusOperator;
 import separator.Separator;
 import separator.SeparatorImpl;
+import separator.SeparatorParser;
+import separator.SeparatorParserImpl;
 
 public class AppConfig {
     private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
     private static final String OUTPUT_MESSAGE = "결과 : ";
 
-    private static Separator separator;
-    private static CustomSeparatorDetector customSeparatorDetector;
+    private final Separator separator;
+    private final CustomSeparatorDetector customSeparatorDetector;
+    private final SeparatorParser separatorParser;
+    private final BinaryOperator operator;
 
     public AppConfig() {
         separator = new SeparatorImpl();
         customSeparatorDetector = new CustomSeparatorDetectorImpl();
+        separatorParser = new SeparatorParserImpl();
+        operator = new PlusOperator();
     }
 
     public Separator separator() {
@@ -31,5 +39,13 @@ public class AppConfig {
 
     public String getOutputMessage() {
         return OUTPUT_MESSAGE;
+    }
+
+    public SeparatorParser separatorParser() {
+        return separatorParser;
+    }
+
+    public BinaryOperator operator() {
+        return operator;
     }
 }
