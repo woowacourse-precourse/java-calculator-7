d7caf3e1307044da8999f9d11a47b334d341d92f
choitaehoon <hugebigdream@gmail.com>
2024-10-16 20:24:29 +0900

    feat: find string separator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,10 +6,21 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = getInput();
-
+        if (isCustomSeparatorExist(input)) {
+            String customSeparator = getCustomSeparator(input);
+        }
     }
 
     public static String getInput() {
         return Console.readLine();
     }
+
+    public static boolean isCustomSeparatorExist(String given) {
+        return given.startsWith("//") && given.charAt(3) == '\n';
+    }
+
+    public static String getCustomSeparator(String given) {
+        return given.substring(2, 3);
+    }
+
 }
--- src/test/java/calculator/MethodTest.java
+++ src/test/java/calculator/MethodTest.java
@@ -16,8 +16,40 @@ class MethodTest {
         String expected = "1:2:3";
 
         System.setIn(new ByteArrayInputStream(given.getBytes()));
-
         String result = Application.getInput();
+
+        assertEquals(expected, result);
+    }
+
+    @DisplayName("시작에 커스텀 구분자(//\n)를 지정할경우 해당 커스텀 구분자를 존재여부 파악(존재할 경우)")
+    @Test
+    void test1() {
+        String given = "//;\n1;2;3";
+        boolean expected = true;
+
+        boolean result = Application.isCustomSeparatorExist(given);
+
+        assertEquals(expected, result);
+    }
+
+    @DisplayName("시작에 커스텀 구분자(//\n)를 지정할경우 해당 커스텀 구분자를 존재여부 파악(존재하지 않을 경우)")
+    @Test
+    void test2() {
+        String given = "1:2:3";
+        boolean expected = false;
+
+        boolean result = Application.isCustomSeparatorExist(given);
+
+        assertEquals(expected, result);
+    }
+
+    @DisplayName("시작에 커스텀 구분자(//\n)를 지정할경우 해당 커스텀 구분자를 리턴하기")
+    @Test
+    void test3(){
+        String given = "//ㅋ\n1;2;3";
+        String expected = "ㅋ";
+
+        String result = Application.getCustomSeparator(given);
 
         assertEquals(expected, result);
     }
