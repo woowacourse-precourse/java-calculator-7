74f9288233ec91e272d3327ef87708dab4fa22d8
Jaewon94 <wodnjs3580@gmail.com>
2024-10-16 17:05:27 +0900

    fix: 커스텀 구분자가 있을 경우에 대한 코드 업데이트
    - 커스텀 구분자가 있을 경우 커스텀 구분자 사용하여 계산
    - Pattern을 사용하여 구분자 추출
    - 커스텀 구분자 유무에 따라 반환값 설정
    - 공통 처리 가능한 코드 메서드로 추출

--- src/main/java/calculator/util/MyCalculator.java
+++ src/main/java/calculator/util/MyCalculator.java
@@ -1,5 +1,8 @@
 package calculator.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MyCalculator {
     
     public int calculate(String expression) {
@@ -7,16 +10,35 @@ public class MyCalculator {
             return 0;
         }
         
-        int result = 0;
+        // 넘어오는 \n값을 우리가 Pattern에서 설정한 \\과 맞추어 주기 위한 코드
+        // \n은 Java 문자열에서 이스케이프 처리되어야 하므로 \\n으로 표현
+        expression = expression.replace("\\n", "\n");
         
-        String replaceExpression = expression.replace(":", ",");
+        // 왼쪽부터 () 가 하나의 그룹으로 묶임
+        // (.)는 어떤 하나의 단일 문자이든지 매칭 -> 나중에 하나의 단일문자가 아니라 n개 이상의 문자도 들어올경우 .* 로 변경
+        // (.*)은 0개 이상의 모든 문자와 매칭
+        Pattern pattern = Pattern.compile("//(.)\\n(.*)");
+        Matcher matcher = pattern.matcher(expression);
         
-        String[] numbers = replaceExpression.split(",");
-        
-        for(String n : numbers) {
-            int num = Integer.parseInt(n);
+        if (matcher.matches()) {
+            String delimiter = matcher.group(1);
+            String numbers = matcher.group(2);
             
-            result += num;
+            // Pattern.quote(delimiter)를 사용하여 구분자를 리터럴 문자열로 처리
+            // 이는 구분자에 정규표현식 특수 문자가 포함되어도 의도대로 동작하게 함
+            return sum(numbers.split(Pattern.quote(delimiter)));
+        } else {
+            // 정규표현식에 의해서 [] 안에 나열된 문자들 중 하나와 매치됨을 의미
+            return sum(expression.split("[,:]"));
+        }
+    }
+    
+    private int sum(String[] numbers) {
+        int parseToInt;
+        int result = 0;
+        for (String number : numbers) {
+            parseToInt = Integer.parseInt(number.trim());
+            result += parseToInt;
         }
         return result;
     }
