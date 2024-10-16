aad99b618497d34f83e975db8c91fff92e10364b
seophohoho <seop0937@gmail.com>
2024-10-16 17:35:48 +0900

    style(Application): 자바 코드 컨벤션 적용

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
-    /** 구분자 목록 */
+    /**
+     * 구분자 목록
+     */
     public static List<String> separators = new ArrayList<>();
-    public static String input="";
+    public static String input = "";
     public static final String customSeparatorSymbol1 = "//";
     public static final String customSeparatorSymbol2 = "\\n";
 
@@ -22,53 +22,53 @@ public class Application {
         System.out.println(app.calc(app.splitString()));
     }
 
-    private void initSeparator(){
+    private void initSeparator() {
         separators.add(",");
         separators.add(":");
     }
 
-    private char getCustomSeparator(String input){
+    private char getCustomSeparator(String input) {
         int firstTokenIdx = input.indexOf(customSeparatorSymbol1);
         int lastTokenIdx = input.lastIndexOf(customSeparatorSymbol2);
 
-        if(firstTokenIdx == -1 || lastTokenIdx == -1){
+        if (firstTokenIdx == -1 || lastTokenIdx == -1) {
             throw new IllegalArgumentException();
         }
 
-        if(((firstTokenIdx+1) - (lastTokenIdx-1))>1){
+        if (((firstTokenIdx + 1) - (lastTokenIdx - 1)) > 1) {
             throw new IllegalArgumentException();
         }
 
-        char customSeparator = input.charAt(lastTokenIdx-1);
-        removeRangeString(firstTokenIdx,lastTokenIdx+2);
+        char customSeparator = input.charAt(lastTokenIdx - 1);
+        removeRangeString(firstTokenIdx, lastTokenIdx + 2);
         return customSeparator;
     }
 
-    private void removeRangeString(int start, int end){
+    private void removeRangeString(int start, int end) {
         StringBuilder sb = new StringBuilder(input);
         sb.delete(start, end);
         input = sb.toString();
     }
 
-    private void addSeparator(char separator){
+    private void addSeparator(char separator) {
         separators.add(String.valueOf(separator));
     }
 
-    private String[] splitString(){
+    private String[] splitString() {
         return input.split(String.join("|", separators));
     }
 
-    private int calc(String[] lst){
-        int result=0;
+    private int calc(String[] lst) {
+        int result = 0;
 
-        for(String separator : lst){
-            try{
+        for (String separator : lst) {
+            try {
                 int parseNumber = Integer.parseInt(separator);
-                if(parseNumber < 0){
+                if (parseNumber < 0) {
                     throw new IllegalArgumentException();
                 }
                 result += parseNumber;
-            }catch(NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 throw new NumberFormatException();
             }
         }
