e7d1aa3348b4a214344baabe87b0ed8f359e7bc5
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-16 22:14:30 +0900

    fix(DelimeterProcessor): checkCustomDelimeterRequest 로직 수정

--- src/main/java/calculator/domain/DelimeterProcessor.java
+++ src/main/java/calculator/domain/DelimeterProcessor.java
@@ -3,7 +3,7 @@ package calculator.domain;
 public class DelimeterProcessor {
 
     public boolean checkCustomDelimeterRequest(String input) {
-        if (input.startsWith("//") && input.indexOf("\n") == 3)
+        if (input.startsWith("//") && input.contains("\n"))
             return true;
         else
             return false;
