c68f4729231373e2bbcbf25078b4c513b88a78be
jijongkwon <help3451@naver.com>
2024-10-17 00:09:02 +0900

    feat(service): ParsingService 클래스에 입력 문자열 파싱 및 검증 로직 구현
    - parseNumbers() 메서드에서 입력 문자열 파싱 로직 구현 - 기본 구분자(,|:)와 커스텀 구분자 지원
      - 커스텀 구분자 추출, 입력 전처리 메서드 구현
    - splitString() 메서드에서 문자열을 구분자로 분리
    - parsePositiveNumber() 메서드에서 숫자 파싱 및 음수 예외 처리
    - 유효하지 않은 숫자 형식일 경우 InvalidInputException 발생

--- src/main/java/calculator/service/ParsingService.java
+++ src/main/java/calculator/service/ParsingService.java
@@ -1,18 +1,60 @@
 package calculator.service;
 
+import calculator.exception.InvalidInputException;
+
+import java.util.Arrays;
+
 public class ParsingService {
+
+    //====================== business ======================//
     public int[] parseNumbers(String input) {
         //TODO: 입력 문자열 파싱 로직 구현
-        return null;
+        String delimiter = ",|:";
+
+        if(input.startsWith("//")){
+            delimiter += extractCustomDelimiter(input);
+            input = preprocessingInput(input);
+        }
+
+        String[] numberStrings = splitString(input, delimiter);
+
+        return Arrays.stream(numberStrings)
+                .mapToInt(this :: parsePositiveNumber)
+                .toArray();
     }
 
-    private String[] splitString(String input) {
+    private String[] splitString(String input, String delimiter) {
         //TODO: 문자열 분리 로직 구현
-        return null;
+        return input.split(delimiter);
     }
+
 
     private String extractCustomDelimiter(String input) {
         //TODO: 커스텀 구분자 추출 로직 구현
-        return null;
+        int newlineIndex = input.indexOf("\\n");
+
+        if (newlineIndex == -1) {
+            throw new InvalidInputException("\\n 가 존재하지 않습니다.");
+        }
+
+        return "|" + input.substring(2, newlineIndex);
+    }
+
+    //====================== builder ======================//
+    private String preprocessingInput(String input){
+        int newlineIndex = input.indexOf("\\n");
+        return input.substring(newlineIndex + 2);
+    }
+
+    private int parsePositiveNumber(String numberString) {
+        try {
+            int number = Integer.parseInt(numberString.trim());
+            if (number < 0) {
+                throw new InvalidInputException("음수는 허용되지 않습니다: " + number);
+            }
+            return number;
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException("유효하지 않은 숫자 형식입니다: " + numberString);
+        }
     }
 }
