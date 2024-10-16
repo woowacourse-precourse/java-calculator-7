8e4ad341575df66edf119e04e9b3ede0dd8a36a1
ajroot5685 <anjeong8577@naver.com>
2024-10-16 23:26:52 +0900

    feat(IO): 입출력 처리
    입출력 요구 사항에 맞게 콘솔에 안내 문구가 보여지도록 추가했다.
    안내 문구는 불변이므로, 전역 상수로 선언하였다.
    주석 적극 활용하기라는 목표를 위해 주석을 추가하였다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,9 +3,19 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+    private static final String INPUT_GUIDE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String OUTPUT_RESULT = "결과 : ";
+
     public static void main(String[] args) {
+        // 입력
+        System.out.println(INPUT_GUIDE);
         String input = Console.readLine();
 
+        // 출력
+        System.out.println(OUTPUT_RESULT + input);
+
+        // scanner 닫기
         Console.close();
     }
 }
