0b5d2ec4fd37b7429ba181c72b956badc4a46833
JooIcksoo <dlrtn751@gmail.com>
2024-10-17 00:28:25 +0900

    refactor(Application): 입력 요청 메시지 출력 리팩토링
    - 클래스 추출(MessagePrinter)
      - 메시지 출력 책임 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,9 @@
 package calculator;
 
 public class Application {
+
     public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        MessagePrinter printer = new MessagePrinter();
+        printer.printInputRequest();
     }
 }
---
+++ src/main/java/calculator/MessagePrinter.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class MessagePrinter {
+
+    private static final String INPUT_REQUEST_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public void printInputRequest() {
+        System.out.println(INPUT_REQUEST_MESSAGE);
+    }
+}
