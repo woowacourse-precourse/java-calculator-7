4efb89294ef9269eef3b7e3f0b8bdcc2dd8afe2e
acupoframen <choe.hwan@gmail.com>
2024-10-16 23:31:17 +0900

    feat(Application): create input parsing
    Added new features in Application
    - set dividers
    - input receive through library make by woowa
    - set input pretty and clean, include custom divider
    - make input array

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+        //setting dividers, getting inputs
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String dividers = ",:";
+        String input = readLine();
+        if (input.startsWith("//")) {
+            int newlineidx = input.indexOf("\\n");
+            //proper divider not given
+            if (newlineidx == -1) {
+                throw new IllegalArgumentException("No Following \\n given in input");
+            }
+            //changing custom divider by ,
+            String customdivider = input.substring(2, newlineidx);
+            input=input.substring(newlineidx+2);
+            input=input.replace(customdivider,",");
+        }
+        // changing every divider to ,
+        for (char divider : dividers.toCharArray()) {
+            input=input.replace(divider,',');
+        }
+        int answer=0;
+
 }
