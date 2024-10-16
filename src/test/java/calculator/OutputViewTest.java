c77e4d1afcbe23f21298c4788a1f7e9340aa238e
sunwon <jsw5913@naver.com>
2024-10-16 20:32:33 +0900

    test(OutputView): 사용자로부터 입력 받기 전 입력문구 출력 기능테스트 구현

---
+++ src/test/java/calculator/OutputViewTest.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import calculator.view.OutputView;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+ class OutputViewTest {
+
+    private final OutputView outputView = new OutputView();
+
+    @Test
+    void 사용자로부터_입력받기_전_입력문구를_출력한다() {
+        String expectedOutput = "덧셈할 문자열을 입력해 주세요.\n";
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        outputView.displayInputMessage();
+
+        assertEquals(expectedOutput, outContent.toString());
+
+        System.setOut(System.out);
+    }
+}
