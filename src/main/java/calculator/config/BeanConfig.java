68c464c7983862b33cec4735a8d75fa900c4db74
haebyun <bhk4392@gmail.com>
2024-10-16 23:03:37 +0900

    feat(BeanConfig): 빈 등록 및 의존성 해결 로직 추가
    - registerBean 메서드를 통해 클래스의 빈을 등록하는 로직 구현
    - resolveDependencies 메서드를 통해 생성자의 파라미터 타입에 맞는 의존성을 해결하는 로직 구현
    - 빈 생성 중 오류가 발생 시 예외 처리 추가

--- src/main/java/calculator/config/BeanConfig.java
+++ src/main/java/calculator/config/BeanConfig.java
@@ -1,9 +1,14 @@
 package calculator.config;
 
 import calculator.FrontController;
+import calculator.config.exception.BeanCreationException;
+import calculator.config.exception.BeanErrorMessage;
 import calculator.controller.CalculatorController;
 import calculator.service.CalculatorService;
 import calculator.service.CalculatorServiceImpl;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -11,7 +16,7 @@ import java.util.Set;
 
 public class BeanConfig {
     // 컴파일 타임에 모든 의존성이 정리되며, 런타임에 의존성이 변경되는 일은 없다.
-    // -> 동시성을 고려하여 설계하지 않아도 된다.
+    // -> 동시성을 고려하여 설계하지 않아도 된다고 생각됨
     private static final Map<Class<?>, Class<?>> abstractToConcreteClassMap = new HashMap<>();
     private static final Map<Class<?>, Object> registeredBeans = new HashMap<>();
 
@@ -22,5 +27,55 @@ public class BeanConfig {
         abstractToConcreteClassMap.put(CalculatorController.class, CalculatorController.class);
         abstractToConcreteClassMap.put(CalculatorService.class, CalculatorServiceImpl.class);
         abstractToConcreteClassMap.put(FrontController.class, FrontController.class);
+
+        abstractToConcreteClassMap.keySet()
+                .forEach(abstractClass -> {
+                    try {
+                        registerBean(abstractClass);
+                    } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+                        throw BeanCreationException.from(BeanErrorMessage.BEAN_CREATION_ERROR, e);
+                    }
+                });
+    }
+
+    private static void registerBean(Class<?> clazz)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        // 이미 등록된 빈은 다시 등록하지 않음
+        if (registeredBeans.containsKey(clazz)) {
+            return;
+        }
+
+        beansInCreation.add(clazz); // 클래스 등록 과정 시작
+
+        // abstractToConcreteClassMap에서 주어진 clazz의 구체적인 구현 클래스를 가져오기
+        Class<?> specificClass = abstractToConcreteClassMap.get(clazz);
+        // specificClass의 모든 생성자를 배열로 반환한 후, 첫 번째 생성자([0])를 가져오기
+        // -> 사실 이 부분은 생성자가 여러 개일 수도 있는 상황에 대응하지 못하기 때문에 한계가 존재함
+        Constructor<?> constructor = specificClass.getConstructors()[0];
+
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+
+        Object[] dependencies = resolveDependencies(parameterTypes);
+
+        Object beanInstance = constructor.newInstance(dependencies);
+        registeredBeans.put(clazz, beanInstance);
+
+        beansInCreation.remove(clazz); // 클래스 등록 완료
+    }
+
+    // 선택한 생성자의 파라미터 타입들을 가져오기
+    private static Object[] resolveDependencies(Class<?>[] parameterTypes)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        return Arrays.stream(parameterTypes)
+                .map(dependency -> {
+                    if (!registeredBeans.containsKey(dependency)) {
+                        try {
+                            registerBean(dependency);
+                        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+                            throw BeanCreationException.from(BeanErrorMessage.BEAN_CREATION_ERROR, e);
+                        }
+                    }
+                    return registeredBeans.get(dependency);
+                }).toArray();
     }
 }
