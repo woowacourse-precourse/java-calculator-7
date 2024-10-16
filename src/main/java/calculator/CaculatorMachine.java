5e794f68da42e8bca5c01780a86fb95800eb8f21
WARDKEY <tjrmsduq@naver.com>
2024-10-16 21:22:36 +0900

    feat : 사용자 문자열 입력 기능 추가

--- src/main/java/calculator/CaculatorMachine.java
+++ src/main/java/calculator/CaculatorMachine.java
@@ -1,9 +1,14 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class CaculatorMachine {
 
     public void run() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String userInput = Console.readLine();
+
     }
 
 }
