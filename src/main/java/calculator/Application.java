901c38ed7376a7fb7a9e212ac2c053118631e542
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-10-16 14:12:21 +0900

    feat: Application 메인함수 큰틀 제작

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+        Calculator calculator = new Calculator();
+        InputHandler.DelimiterType delimiterType;
+
+        String userInput = inputHandler.getUserInput();
+        System.out.println("입력: "+ userInput);
+        System.out.println( "길이:"+ userInput.length());
+        delimiterType = inputHandler.isValidInput(userInput);
+        int[] numbers = inputHandler.stringToIntArray(userInput, delimiterType);
+        int result = calculator.addition(numbers);
+
+        System.out.println("결과 : " + Integer.toString(result));
+
     }
 }
