0f0ba8a6c0bc4bee558b6489cc791f98229e4811
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-16 21:47:21 +0900

    feat(DelimiterService): 커스텀 구분자를 사용한 숫자 분리 기능 추가

--- src/main/java/calculator/service/DelimiterService.java
+++ src/main/java/calculator/service/DelimiterService.java
@@ -1,11 +1,34 @@
 package calculator.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DelimiterService {
 
     private static final String DEFAULT_DELIMITERS = "[,:]";
+    private static final String CUSTOM_DELIMITERS = "//(.)\n(.*)";
 
     public String[] splitNumbers(String input) {
+        if (isCustomDelimiter(input)) {
+            return splitByCustomDelimiters(input);
+        }
+
         return splitByDefaultDelimiters(input);
+    }
+
+    private boolean isCustomDelimiter(String input) {
+        return input.startsWith("//") && !input.substring(2, input.indexOf("\n"))
+                .isEmpty();
+    }
+
+    private String[] splitByCustomDelimiters(String input) {
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITERS).matcher(input);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("매칭되는 구분자가 없습니다.");
+        }
+
+        return matcher.group(2).split(matcher.group(1));
     }
 
     private String[] splitByDefaultDelimiters(String input) {
