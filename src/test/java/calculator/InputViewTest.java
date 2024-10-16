e091b3835a1f94fc4c9fdd2a7a12c7eef6dc787e
joungeun0825 <joungeun0825@gmail.com>
2024-10-16 12:52:45 +0900

    test(inputview): InputView 접근 시 정적 메서드로 접근

--- src/test/java/calculator/InputViewTest.java
+++ src/test/java/calculator/InputViewTest.java
@@ -5,29 +5,26 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class InputTest {
+public class InputViewTest {
 
     @DisplayName("입력값을 잘 가져오는지 테스트")
     @Test
     void getInput(){
-        InputView inputView = new InputView();
-        String inputValue = inputView.scanString("1,2:3");
+        String inputValue = InputView.scanString("1,2:3");
         assertThat(inputValue).isEqualTo("1,2:3");
     }
 
     @DisplayName("올바른 입력 값일 때 유효한 값이라 판단하는지 테스트")
     @Test
     void getInput(){
-        InputView inputView = new InputView();
-        boolean inputValue = inputView.isValidString("//;\\n1");
+        boolean inputValue = InputView.isValidString("//;\\n1");
         assertThat(inputValue).isTrue();
     }
 
     @DisplayName("음수 입력 시 유효하지 않은 값이라 판단하는지 테스트")
     @Test
     void getInput(){
-        InputView inputView = new InputView();
-        boolean inputValue = inputView.isValidString("-1,2,3");
+        boolean inputValue = InputView.isValidString("-1,2,3");
         assertThat(inputValue).isFalse();
     }
 }
