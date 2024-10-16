efabb0a0c16182baaf0a917c91893c9a4dafa7ef
jijongkwon <help3451@naver.com>
2024-10-16 21:33:34 +0900

    feat(util): NumberValidator 유틸리티 클래스 기본 구조 추가
    TODO: isValidNumber 메서드에 실제 숫자 유효성 검증 로직 구현

---
+++ src/main/java/calculator/util/NumberValidator.java
@@ -0,0 +1,8 @@
+package calculator.util;
+
+public class NumberValidator {
+    public static boolean isValidNumber(String number) {
+        //TODO: 숫자 유효성 검증 유틸리티 메서드 구현
+        return false;
+    }
+}
