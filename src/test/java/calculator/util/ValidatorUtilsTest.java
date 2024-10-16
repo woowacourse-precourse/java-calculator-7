c4fc59c0df062b24a3a5254c28f902d99ec53de7
heehun <slrkqk2@naver.com>
2024-10-17 01:32:14 +0900

    test(ValidationUtilsTest): input 검증 로직 추가
    커스텀 구분자를 사용한 예외 케이스 테스트 추가\n커스텀 구분자를 제외한 문자열 예외 케이스 테스트 추가 예정

--- src/test/java/calculator/util/ValidatorUtilsTest.java
+++ src/test/java/calculator/util/ValidatorUtilsTest.java
@@ -2,30 +2,47 @@ package calculator.util;
 
 import static org.assertj.core.api.Assertions.*;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-public class ValidatorUtilsTest {
+public class ValidatorUtilsTest extends NsTest {
 
   @Test
   void 문자열_유효성() {
     String validInput  = "1,2";
-    String invalidInput  = "";
+    String EmptyValidInput  = "";
     String NotIncludeNumberInput = ",,,,";
-    String validInput_startWithChar = "//a\n//;\n1,2,3a4;5;6";
-    String invalidInput_startWithChar = "//a\n1b2,3,4";
+    String validInput_startWithChar = "//a\n1,2,3,4:5:6";
+    String invalidInput_startWithChar = "//ab\n1,2,3a4:5:6";
+    String invalidInput_startWithChar2 = "//a\n1,2,3a4:5b6";
 
+    String nullInput = null;
     assertThat(Validator.isValidate(validInput)).isEqualTo(true);
 
-    assertThatThrownBy(() -> Validator.isValidate(invalidInput))
-            .isInstanceOf(IllegalArgumentException.class);
+      assertThat(Validator.isValidate(EmptyValidInput)).isEqualTo(true);
+
 
     assertThatThrownBy(() -> Validator.isValidate(NotIncludeNumberInput))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("case2");
 
     assertThat(Validator.isValidate(validInput_startWithChar)).isEqualTo(true);
 
     assertThatThrownBy(() -> Validator.isValidate(invalidInput_startWithChar))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("커스텀 구분자 생성 오류");
+    
+    assertThatThrownBy(() -> Validator.isValidate(invalidInput_startWithChar2))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("잘못된 구분자 사용");
+    
+    assertThatThrownBy(() -> Validator.isValidate(nullInput))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("null");
   }
 
+  @Override
+  protected void runMain() {
+
+  }
 }
