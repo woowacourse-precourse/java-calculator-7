7fb3926146371784899eb05f7c7eb0b015e05bf9
JinGoo <zzgg3040@naver.com>
2024-10-16 20:12:10 +0900

    feat: 입력값 검증 후 input 객체를 반환하는 필터

---
+++ src/main/java/calculator/input/InputFilter.java
@@ -0,0 +1,45 @@
+package calculator.input;
+
+import calculator.Utils.NumUtil;
+
+public class InputFilter {
+
+    private static final String CUSTOM_PREFIX = "//";
+    private static final String CUSTOM_SUFFIX = "\\n";
+    private static final int CUSTOM_LENGTH = 5;
+
+    public static Input from(String input) {
+        if (isCustom(input)) {
+            return new CustomInput(getCleanedNums(input), getCustomSeparator(input));
+        }
+        return new Input(validateNums(input));
+    }
+
+    private static boolean isCustom(String input) {
+        if (input.length() < CUSTOM_LENGTH) {
+            return false;
+        }
+        return isCustomFormat(input);
+    }
+
+    private static boolean isCustomFormat(String input) {
+        String customPrefix = input.substring(0, CUSTOM_PREFIX.length());
+        String customSuffix = input.substring(CUSTOM_PREFIX.length() + 1, CUSTOM_LENGTH);
+        return customPrefix.equals(CUSTOM_PREFIX) && customSuffix.equals(CUSTOM_SUFFIX);
+    }
+
+    private static String getCleanedNums(String input) {
+        return validateNums(input.substring(CUSTOM_LENGTH));
+    }
+
+    private static String validateNums(String nums) {
+        if (!NumUtil.isValidFirstNum(nums)) {
+            throw new IllegalArgumentException("숫자 문자열이 숫자로 시작하지 않습니다.");
+        }
+        return nums;
+    }
+
+    private static char getCustomSeparator(String input) {
+        return input.charAt(CUSTOM_PREFIX.length());
+    }
+}
---
+++ src/test/java/calculator/input/InputFilterTest.java
@@ -0,0 +1,67 @@
+package calculator.input;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InputFilterTest {
+
+    @Test
+    void 커스텀_객체_변환() {
+        // given
+        String customInput = "//;\\n1;2;3";
+        // when & then
+        assertInstanceOf(CustomInput.class, InputFilter.from(customInput));
+    }
+
+    @Test
+    void 기본_객체_반환_확인() {
+        // given
+        String defaultInput = "1,2:3";
+        // when
+        Input defaultInputInstance = InputFilter.from(defaultInput);
+        // then
+        assertInstanceOf(Input.class, defaultInputInstance);
+        assertFalse(defaultInputInstance instanceof CustomInput);
+    }
+
+    @Test
+    void 커스텀_객체_변환_예외_숫자로_시작_안_함() {
+        // given
+        String customInput = "//;\\n" + ";2;3";
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> InputFilter.from(customInput));
+    }
+
+    @Test
+    void 커스텀_객체_변환_예외_구분자_없음() {
+        // given
+        String customInput = "//\\n" + "1;2;3";
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> InputFilter.from(customInput));
+    }
+
+    @Test
+    void 커스텀_객체_변환_예외_prefix_없음() {
+        // given
+        String customInput = ";\\" + "n1;2;3";
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> InputFilter.from(customInput));
+    }
+
+    @Test
+    void 커스텀_객체_변환_예외_suffix_없음() {
+        // given
+        String customInput = "//;" + "1;2;3";
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> InputFilter.from(customInput));
+    }
+
+    @Test
+    void 기본_객체_변환_예외_숫자로_시작_안_함() {
+        // given
+        String defaultInput = ";2;3";
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> InputFilter.from(defaultInput));
+    }
+}
