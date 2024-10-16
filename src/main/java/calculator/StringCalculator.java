ddcc19901a0cfdad1a4884ffd057fe1a4ead6431
kjyyjk <rlawnsdud920@gmail.com>
2024-10-17 00:46:06 +0900

    feat: 계산 문자열을 구분자로 분리
    - split 메서드의 인자로 기본 구분자와 커스텀 구분자를 이용한 정규표현식

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,6 +4,7 @@ public class StringCalculator {
     public int calculate(String input) {
         Character customDelimiter = null;
 
+        //커스텀 구분자 추출
         if (input.startsWith("//") && input.contains("\\n")) { //커스텀 구분자 존재 시
             String customDelimiterStr = input.substring(2, input.lastIndexOf("\\n"));
 
@@ -14,10 +15,19 @@ public class StringCalculator {
             customDelimiter = customDelimiterStr.charAt(0);
         }
 
+        //계산 문자열 구분
         String calculateStr = input;
         if (customDelimiter != null) {
             calculateStr = input.substring(input.lastIndexOf("\\n") + 2);
         }
+
+        //계산 문자열을 구분자로 분리
+        String splitRegex = ":|,";
+        if (customDelimiter != null) {
+            splitRegex = splitRegex + "|" + customDelimiter;
+        }
+
+        String[] parsedCalculateStr = calculateStr.split(splitRegex);
 
         return 0; //TODO
     }
