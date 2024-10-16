ee2df50cf4682a08fcfada82ebfad5f650cf1fb6
Yeongjae Kim <sees77770@gmail.com>
2024-10-17 00:07:45 +0900

    test: 다수의 테스트케이스를 추가
    다수의 입력 케이스를 테스트하기 위해 ParameterizedTest와 ValueSource를 이용한다.

--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -3,18 +3,19 @@ package calculator.domain;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
 
     @DisplayName("계산기 더하기 성공")
-    @Test
-    void sumTest() {
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2:3", "//;\\n1;2;3"})
+    void sumTest(String inputs) {
         // given
-        String inputValue = "1,2:3";
 
         // when
-        Calculator calc = new Calculator(inputValue);
+        Calculator calc = new Calculator(inputs);
 
         // then
         assertThat(calc.sum()).isEqualTo(6);
--- src/test/java/calculator/domain/OperandTest.java
+++ src/test/java/calculator/domain/OperandTest.java
@@ -3,30 +3,31 @@ package calculator.domain;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class OperandTest {
 
     @DisplayName("문자열 입력 실패 : 숫자가 아닌 문자열 입력")
-    @Test
-    void parseToIntTest() {
+    @ParameterizedTest
+    @ValueSource(strings = {"abc,2:3", "!@,2:3"})
+    void parseToIntTest(String inputs) {
         // given
-        String userInput = "a,2;3";
 
         // when, then
-        assertThatThrownBy(() -> new Calculator(userInput))
+        assertThatThrownBy(() -> new Calculator(inputs))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("숫자가 아닌 문자열이 입력되었습니다.");
     }
 
     @DisplayName("문자열 입력 실패 : 양수가 아닌 수 입력")
-    @Test
-    void validatePositiveTest() {
+    @ParameterizedTest
+    @ValueSource(strings = {"-1,2:3", "0,2:3"})
+    void validatePositiveTest(String inputs) {
         // given
-        String userInput = "-1,2;3";
 
         // when, then
-        assertThatThrownBy(() -> new Calculator(userInput))
+        assertThatThrownBy(() -> new Calculator(inputs))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("입력된 숫자가 양수가 아닙니다.");
     }
