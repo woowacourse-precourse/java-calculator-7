fed327e8009d4a0ea20271c50f909625953f449b
kim <hwiyul_g@daum.net>
2024-10-16 21:23:48 +0900

    feat(Separator): 기본 구분 기능과 커스텀 구분 기능을 구분과 이를 이용한 구분 기능 일원화

--- src/main/java/calculator/service/Separator.java
+++ src/main/java/calculator/service/Separator.java
@@ -9,13 +9,24 @@ public class Separator {
     private static final String BASIC_SEPARATOR_COLON = ":";
     private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("//(.+)\\\\n");
 
-    public int[] basicSeparate(String content) {
+    public int[] separate(String content) {
+        if (isCustomSeparator(content)) {
+            return customSeparate(content);
+        }
+        return basicSeparate(content);
+    }
+
+    private boolean isCustomSeparator(String content) {
+        return content.startsWith("//") && content.contains("\\n");
+    }
+
+    private int[] basicSeparate(String content) {
         return Arrays.stream(
                 content.replaceAll(BASIC_SEPARATOR_COLON, BASIC_SEPARATOR_COMMA).split(BASIC_SEPARATOR_COMMA)
         ).mapToInt(this::convertToNumber).toArray();
     }
 
-    public int[] customSeparate(String content) {
+    private int[] customSeparate(String content) {
         Matcher matcher = CUSTOM_SEPARATOR_PATTERN.matcher(content);
         if (matcher.find()) {
             String customSeparator = matcher.group(1);
