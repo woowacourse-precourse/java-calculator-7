4cd800060e431f78b2b2481f5471532249c78371
강승연 <syeon.dev@gmail.com>
2024-10-17 00:12:11 +0900

    feat: 입출력 형식 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int result = 0;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        System.out.print("결과 : " + result);
     }
 }
