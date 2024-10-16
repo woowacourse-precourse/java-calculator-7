c6e6d43cdcb30ec8e662a2eeda5011052e9d25fd
jklee3409 <jklee3409@khu.ac.kr>
2024-10-16 17:06:55 +0900

    refactor(delimiter) : Extracted custom delimiter validation into a separate method

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -2,21 +2,25 @@ package calculator.domain;
 
 public class Delimiter {
 
-    // 커스텀 구분자가 있는지 확인하는 메서드
+    // 문자열에서 커스텀 구분자를 지정했는지 확인하고 인덱스 반환
     private static boolean hasCustomDelimiter(String input) {
         return input.startsWith("//") && input.contains("\\n");
     }
 
-    // 커스텀 구분자를 추출하고 유효성 확인
+    // 커스텀 구분자의 유효성 확인
+    private static void isValidCustomDelimiter(String delimiter) {
+        if (delimiter.length() != 1) {
+            throw new IllegalArgumentException("커스텀 구분자는 한 글자여야 합니다.");
+        }
+    }
+
+    // 커스텀 구분자를 추출
     public static String getCustomDelimiter(String input) {
         if (hasCustomDelimiter(input)) {
             int delimiterIndex = input.indexOf("\\n");
             String delimiter = input.substring(2, delimiterIndex);
 
-            // 커스텀 구분자는 1개까지만 인정
-            if (delimiter.length() != 1) {
-                throw new IllegalArgumentException("커스텀 구분자는 한 글자여야 합니다.");
-            }
+            isValidCustomDelimiter(delimiter);
 
             return delimiter;
         }
