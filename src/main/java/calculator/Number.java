89df1ed0fee88b64a525eabc8dedbbe16125a308
sc@t0a14 <gusdn6671@gmail.com>
2024-10-17 00:45:46 +0900

    fix(Number) : ""으로 들어갈경우 0으로 처리 불가한 경우 fix

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -9,12 +9,18 @@ public class Number {
     }
 
     private static void validate(String number) {
+        if(number.equals("\"\"")){
+            number = "0";
+        }
+
+        if(!number.matches("\\d+")){
+            throw new IllegalArgumentException("숫자 외의 문자는 입력할수 없습니디");
+        }
+
         if (number.isBlank()) {
             throw new IllegalArgumentException("숫자에 빈값을 입력할수 없습니다");
         }
-        if (!number.matches("\\d+")) {
-            throw new IllegalArgumentException("숫자외에 다른 값을 입력할수 없습니다");
-        }
+
         if (Integer.parseInt(number) < 0) {
             throw new IllegalArgumentException("음수를 입력할수 없습니다");
         }
