49ea038012d3c2be68780477be0ccdcacd15911c
Byuntil <gugugu2014@jbnu.ac.kr>
2024-10-16 18:34:10 +0900

    feat: 추출한 숫자를 덧셈하는 기능

--- src/main/java/calculator/StringSumCalculator.java
+++ src/main/java/calculator/StringSumCalculator.java
@@ -2,11 +2,20 @@ package calculator;
 
 import calculator.view.InputView;
 
+import java.util.List;
+
 public class StringSumCalculator {
 
     public void run() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputValue = InputView.getInputValue();
+        IntegersExtractor integersExtractor = new IntegersExtractor();
+        List<Integer> integers = integersExtractor.extractIntegers(inputValue);
+        int sum = getSum(integers);
+        System.out.println("결과 : " + sum);
+    }
 
+    private int getSum(List<Integer> integers) {
+        return integers.stream().reduce(0, Integer::sum);
     }
 }
