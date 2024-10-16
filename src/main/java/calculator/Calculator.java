2d5afcc6ce5bd8ac60cfe8ed7ac8cf33665b6a27
Minuring <mw9505@gmail.com>
2024-10-16 18:55:13 +0900

    feat(core): 숫자 추출 및 계산
    수식으로부터 숫자를 추출해 더하는 기능을 구현하기 위해 아래 메소드를 구현
    - convertToNumbers(String[]) 구분자 사이의 문자열들로 이루어진 배열을 정수 리스트로 변환
    - calculate() : 정수 리스트의 합 계산
    
    convertToNumbers()는 구분자 사이의 문자열이 숫자가 아니거나 음수이면 예외를 던진다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,15 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
 
     private static final String DEFAULT_SEPARATORS = ",:";
 
     private String exp;
     private String regex;
+    private List<Integer> numbers;
 
     public Calculator(String expression) {
         this.regex = "[" + DEFAULT_SEPARATORS + "]";
@@ -15,6 +19,38 @@ public class Calculator {
             this.regex = "[" + DEFAULT_SEPARATORS + exp.charAt(2) + "]";
             this.exp = exp.substring(5);
         }
+
+        String[] parts = exp.split(regex);
+        this.numbers = convertToNumbers(parts);
+    }
+
+    public int calculate() {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private List<Integer> convertToNumbers(final String[] parts) {
+        List<Integer> list = new ArrayList<>();
+
+        for (String part : parts) {
+            if (part.isBlank()) {
+                list.add(0);
+                continue;
+            }
+
+            try {
+                int n = Integer.parseInt(part);
+                if (n < 0) {
+                    throw new IllegalArgumentException();
+                }
+
+                list.add(n);
+            } catch (NumberFormatException expected) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return list;
     }
 
     private boolean hasCustomSeparator() {
