1e98312545aa4e5bbe3f1cef49e2ffe2902677fa
sunwon <jsw5913@naver.com>
2024-10-16 20:34:29 +0900

    feat(OutputView): 계산 결과 출력 기능 구현

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -3,8 +3,13 @@ package calculator.view;
 public class OutputView {
 
     private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.\n";
+    private static final String RESULT_MESSAGE = "결과 : ";
 
     public void displayInputMessage() {
         System.out.print(INPUT_MESSAGE);
+    }
+
+    public void displayResultMessage(String number) {
+        System.out.print(RESULT_MESSAGE + number);
     }
 }
