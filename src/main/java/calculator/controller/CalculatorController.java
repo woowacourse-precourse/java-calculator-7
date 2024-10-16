0e446683963159b42463700d45a03203df5ab6ce
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-17 03:14:01 +0900

    feat(CalculatorController): runCalculator 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,28 @@
 package calculator.controller;
 
+import calculator.domain.numCalculator.NumCalculator;
+import calculator.domain.preprocess.Preprocessor;
+import calculator.view.ViewManager;
+
+import java.util.List;
+
 public class CalculatorController {
+    private final NumCalculator numCalculator;
+    private final Preprocessor preprocessor;
+    private final ViewManager viewManager
+
+    public CalculatorController(NumCalculator numCalculator, Preprocessor preprocessor, ViewManager viewManager) {
+        this.numCalculator = numCalculator;
+        this.preprocessor = preprocessor;
+        this.viewManager = viewManager;
+    }
+
+    public void runCalculator(){
+        String input = viewManager.printInputMessage();
+        List<String> delimeterList = preprocessor.preprocessDelimeter(input);
+        String preprocessedString = preprocessor.preprocessString(input);
+        preprocessor.validateInputString(delimeterList,preprocessedString);
+        Integer answer = numCalculator.calculate(preprocessor.extractSumNumbers(delimeterList,preprocessedString));
+        viewManager.printResult(answer);
+    }
 }
