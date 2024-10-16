53557367f6063bb64555087cd74666197fadf9b2
jklee3409 <jklee3409@khu.ac.kr>
2024-10-16 14:27:46 +0900

    feat(main) : 프로그램 반복 기능 제거
    한번의 문자열 입력에 대해서만 동작하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,4 +1,5 @@
 package calculator;
+
 import calculator.domain.Calculator;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -6,23 +7,19 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        while (true) {
-            try {
-                promptUserForInput();
-                String input = getInput();
+        try {
+            promptUserForInput();
+            String input = getInput();
 
-                if (isExitCommand(input)) {
-                    System.out.println("프로그램을 종료합니다.");
-                    break;
-                }
+            int result = Calculator.performAddition(input);
+            System.out.println("결과 : " + result);
 
-                int result = Calculator.performAddition(input);
-                System.out.println("결과 : " + result);
+            System.out.println();
 
-            } catch (IllegalArgumentException e) {
-                throw new IllegalArgumentException();
-            }
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
         }
+
     }
 
     private static void promptUserForInput() {
@@ -33,8 +30,4 @@ public class Application {
         return Console.readLine();
     }
 
-    private static boolean isExitCommand(String input) {
-        // 대소문자 구분없이, input == exit 비교
-        return "exit".equalsIgnoreCase(input);
-    }
 }
