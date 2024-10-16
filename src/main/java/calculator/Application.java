77c3ebecc23bc618ac8c42ac3a5b80695508e8be
mjk <mjk3220@ajou.ac.kr>
2024-10-16 10:59:54 +0900

    docs: create md
    구현할 기능 목록을 작성하기 위해 md 파일 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,9 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        
     }
 }
