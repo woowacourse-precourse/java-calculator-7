f4c28f48612835b255aed5c1a9552310f2b04d7c
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-16 20:53:14 +0900

    feat(StringCalculator): 숫자 값이 아닌 형태 에외처리 기능 추가 및 결과값 도출 기능 추가
    숫자 값이 아닌 형태 예외처리를 try-catch 문으로 구현하여, 결과값 도출 기능도 같이 연계되게 하였음

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -1,12 +1,39 @@
 package calculator.domain;
 
+import java.util.List;
+
 public class StringCalculator {
 
     private final static int INITIAL_NUMBER = 0;
-    private final static String EXCEPTION_MINUS_NUMBER = "잘못된 값을 입력하셨습니다.";
+    private final static String EXCEPTION_MINUS_NUMBER = "음수는 입력 불가합니다.";
+    private final static String EXCEPTION_STRING_NUMBER = "올바른 숫자 형식이 아닙니다.";
 
-    private void validateMinusNumber(int number){
-        if(number < INITIAL_NUMBER){
+    private final int result;
+
+    public StringCalculator(List<String> numbers) {
+        this.result = calculateStrings(numbers);
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    private int calculateStrings(List<String> numbers) {
+        try {
+            int sum = INITIAL_NUMBER;
+            for (String number : numbers) {
+                int realNumber = Integer.parseInt(number);
+                validateMinusNumber(realNumber);
+                sum += realNumber;
+            }
+            return sum;
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(EXCEPTION_STRING_NUMBER);
+        }
+    }
+
+    private void validateMinusNumber(int number) {
+        if (number < INITIAL_NUMBER) {
             throw new IllegalArgumentException(EXCEPTION_MINUS_NUMBER);
         }
     }
