0159fdf2980017e0735dee82d92acf4a6e717836
jiffyin7 <jiffyin7@gmail.com>
2024-10-17 03:37:57 +0900

    feat(NumberParser): 구분자를 이용해 문자열을 숫자 배열로 변환
    이슈 : #8

--- src/main/java/calculator/stringAdder/numberParser/NumberParser.java
+++ src/main/java/calculator/stringAdder/numberParser/NumberParser.java
@@ -1,12 +1,35 @@
 package calculator.stringAdder.numberParser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 /**
  * @author : jiffyin7@gmail.com
  * @since : 24. 10. 17.
  */
 public class NumberParser {
 
-  public String[] getNumbers(String input, String[] delimeters) {
-    return null;
+  public String[] getNumbers(String input, String[] delimiters) {
+    String numberString =
+        delimiters.length == 2?
+            input : input.substring(5);
+
+    String regex = Arrays.stream(delimiters)
+        .map(Pattern::quote)
+        .collect(Collectors.joining("|"));
+
+    return Arrays.stream(numberString.split(regex))
+        .filter(s -> !s.isEmpty())
+        .peek(this::validateNumber)
+        .toArray(String[]::new);
+  }
+
+  private void validateNumber(String number) {
+    if (number.startsWith("-")) {
+      throw new IllegalArgumentException(this.getClass().getSimpleName()+ ": 음수는 허용되지 않습니다: " + number);
+    }
   }
 }
