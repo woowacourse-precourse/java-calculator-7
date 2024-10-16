7e115ca1f72929c79cb79931c21eda1923bf3ba2
zzioozzioo <zzioo1006@naver.com>
2024-10-16 18:25:12 +0900

    test: 문자열에서 숫자를 추출할 때 잘못 입력된 기본 생성자가 있으면 예외를 던지는 테스트 코드 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,12 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import calculator.exception.IllegalInputException;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,9 +20,22 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+
+    @Test
+    void 문자열에서_숫자_추출하기_기본생성자_예외처리() {
+        //given
+        String input = "6;4,0";
+        Extractor extractor = new Extractor(input);
+
+        //when & then
+        assertThatThrownBy(extractor::extractNumber)
+                .isInstanceOf(IllegalInputException.class)
+                .hasMessageContaining("잘못된 구분자입니다");
     }
 
     @Override
