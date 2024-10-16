3f843c93f8128007908263727e007382f332eeaa
JinGoo <zzgg3040@naver.com>
2024-10-16 13:23:35 +0900

    feat: 입력 받기 및 결과 출력

---
+++ src/main/java/calculator/runner/CalculatorRunner.java
@@ -0,0 +1,16 @@
+package calculator.runner;
+
+import calculator.core.Calculator;
+import camp.nextstep.edu.missionutils.Console;
+
+public class CalculatorRunner {
+
+    public static void run() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        Console.close();
+
+        Calculator calculator = Calculator.of(input);
+        System.out.println("결과 : " + calculator.getResult());
+    }
+}
