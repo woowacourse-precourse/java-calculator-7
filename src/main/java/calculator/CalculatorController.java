71aff8f65c617ad929d0b85ecd0f9e82f626a965
DK <laancer4@gmail.com>
2024-10-16 16:10:35 +0900

    feat : 콘솔을 통해 사용자가 문자열을 입력하면, 구분자를 통해 숫자를 더해서 결과를 도출한다.

---
+++ src/main/java/calculator/CalculatorController.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CalculatorController {
+    private static final String INITIAL_COMMENT = "덧셈할 문자열을 입력해 주세요.";
+
+    private static final String RESULT = "결과 : ";
+
+    public void run() {
+        System.out.println(INITIAL_COMMENT);
+        Calculator calculator = new Calculator();
+        String input = Console.readLine();
+        int result = calculator.calculate(input);
+        System.out.println(RESULT + result);
+        Console.close();
+    }
+}
