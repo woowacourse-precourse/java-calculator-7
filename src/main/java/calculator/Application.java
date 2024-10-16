a2e107754c58cd652bbd805a6f67f454ca3b4cb9
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-17 02:12:06 +0900

    feat(Application): 계산 기능을 실행가능하도록 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.model.Calculator;
+import calculator.utils.Parser;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        Parser parser = new Parser();
+        Calculator calculator = new Calculator();
+        OutputView outputView = new OutputView();
+        CalculatorController calculatorController = new CalculatorController(inputView, calculator, parser, outputView);
+        calculatorController.run();
     }
 }
