2e882dcb5b0766cf6d4abcf29d65a72d0e9135db
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-10-16 14:11:39 +0900

    test: 빈문자열 처리 테스트코드

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,18 +1,49 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 빈문자열_처리() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용(){
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+
+            run("1:2:3");
+            assertThat(output()).contains("결과: 6");
+
+            run("1,3:2");
+            assertThat(output()).contains("결과: 6");
+        });
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
+
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과: 6");
+
         });
     }
 
