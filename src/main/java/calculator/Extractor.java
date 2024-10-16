bf7f118db25a5f0a1cc5e146c3b3b21ffba05e80
zzioozzioo <zzioo1006@naver.com>
2024-10-16 19:46:52 +0900

    feat(Extractor): 입력된 문자열이 양수로 이루어져 있는지 판별하는 기능 추가

--- src/main/java/calculator/Extractor.java
+++ src/main/java/calculator/Extractor.java
@@ -1,14 +1,15 @@
 package calculator;
 
-import calculator.exception.IllegalInputException;
 import java.util.Set;
 
 public class Extractor {
 
+    final String ERROR_MESSAGE = "잘못된 구분자입니다.";
+
     private final String input;
-    private Set<Integer> numbers;
     private String[] strArr;
     private String customSeparator;
+    private Set<Integer> numbers; // 계산할 숫자만이 저장된 Set
 
     public Extractor(String input) {
         this.input = input.trim();
@@ -20,6 +21,10 @@ public class Extractor {
      */
     public void extractNumber() {
         containCustomSeparator();
+
+        // TODO: 커스텀, 기본 구분자 동시에 존재할 때의 예외 처리
+//        customSeparatorRemovedString = input.substring(5);
+
         containBasicSeparator();
 
         for (String str : strArr) {
@@ -38,23 +43,24 @@ public class Extractor {
 
     private void saveCustomSeparator(int separatorStartIdx, int separatorEndIdx) {
         if (separatorEndIdx == -1) {
-            throw new IllegalInputException();
+            throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         customSeparator = input.substring(separatorStartIdx, separatorEndIdx);
 
         if (customSeparator.isEmpty()) {
-            throw new IllegalInputException();
+            throw new IllegalArgumentException(ERROR_MESSAGE);
         }
     }
 
     private void containBasicSeparator() {
         strArr = input.split(",|:");
 
-        // 문자열 배열의 각 문자열이 숫자로만 이루어져 있는지 판별
+        // 문자열 배열의 각 문자가 양수로만 이루어져 있는지 판별
         for (String s : strArr) {
             for (int i = 0; i < s.length(); i++) {
-                if (!Character.isDigit(s.charAt(i))) {
-                    throw new IllegalInputException();
+                char targetChar = s.charAt(i);
+                if (!Character.isDigit(targetChar) && Character.getNumericValue(targetChar) <= 0) {
+                    throw new IllegalArgumentException(ERROR_MESSAGE);
                 }
             }
         }
