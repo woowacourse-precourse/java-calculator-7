3fa3aff26a5d7ace73bdfd4184c3551d2fb9a574
juyeon320 <ju010320@gmail.com>
2024-10-17 00:42:56 +0900

    feat(calculator): add functionality to input a string for addition
    - Added functionality to receive a string input from the user using Scanner.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,12 @@
 package calculator;
-
+import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Scanner scan = new Scanner(System.in);
+        String str;
+        str = scan.nextLine();
     }
 }
 
