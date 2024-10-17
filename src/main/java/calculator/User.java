d413ae404ef8fd03044b5bb0d7bcedc7b40beec8
sc@t0a14 <gusdn6671@gmail.com>
2024-10-17 20:08:39 +0900

    feat(User) : 유저 클래스 생성

---
+++ src/main/java/calculator/User.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class User {
+    private String user;
+
+    public User(String user) {
+        this.user = user;
+    }
+
+    public String getUser() {
+        return user;
+    }
+}
