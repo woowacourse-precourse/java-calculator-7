0e141640366603a5931c8f6878485f8f0f09265c
LSBsity <dltmdqls51@naver.com>
2024-10-17 18:03:47 +0900

    feat: 정규 표현식 스플리터 베이스 코드

--- src/main/java/calculator/domain/factory/CalculatorFactory.java
+++ src/main/java/calculator/domain/factory/CalculatorFactory.java
@@ -2,6 +2,7 @@ package calculator.domain.factory;
 
 import calculator.domain.calc.Calculator;
 import calculator.domain.splitter.NormalSplitter;
+import calculator.domain.splitter.RegexSplitter;
 
 /**
  * Calculator Config
@@ -14,5 +15,9 @@ public class CalculatorFactory {
 
     private NormalSplitter normalSplitter() {
         return new NormalSplitter();
+    }
+
+    private RegexSplitter regexSplitter() {
+        return new RegexSplitter();
     }
 }
