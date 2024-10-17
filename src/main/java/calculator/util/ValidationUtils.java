6e66ed2e9890ec1b7339029b99b01dd777293af9
Sinorang <sinholang@naver.com>
2024-10-17 10:27:17 +0900

    feat: 커스텀 구분자 예외 처리 메서드 구현
    - null 혹은 빈 문자열인 커스텀 구분자 검증 로직 구현
    - 단일 문자 검증 로직 구현
    - 커스텀 구분자를 검증해야 할 로직이 많으므로 메서드로 분리하여 호출하는
      방식으로 구현

--- src/main/java/calculator/util/ValidationUtils.java
+++ src/main/java/calculator/util/ValidationUtils.java
@@ -5,7 +5,25 @@ import java.util.List;
 
 public class ValidationUtils {
 
-    public static void validateNegativeNumbers(List<Integer> numbers) {
+    public static void validateNumbers(List<Integer> numbers) {
+        validateNegativeNumbers(numbers);
+        validateZero(numbers);
+    }
+
+    public static void validateCustomDelimiter(String delimiter) {
+        validateNullOrEmpty(delimiter);
+        validateSingleCharacter(delimiter);
+    }
+
+    public static void validateNumber(String input) {
+        try {
+            Integer.parseInt(input.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("유효하지 않은 숫자 형식입니다. 문제가 되는 입력값: [" + input + "]");
+        }
+    }
+
+    private static void validateNegativeNumbers(List<Integer> numbers) {
         List<Integer> negativeNumbers = new ArrayList<>();
 
         for (int number : numbers) {
@@ -19,17 +37,21 @@ public class ValidationUtils {
         }
     }
 
-    public static void validateZero(List<Integer> numbers) {
+    private static void validateZero(List<Integer> numbers) {
         if (numbers.contains(0)) {
             throw new IllegalArgumentException("0은 허용되지 않습니다. 문제가 되는 입력값: [0]");
         }
     }
 
-    public static void validateNumber(String input) {
-        try {
-            Integer.parseInt(input.trim());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("유효하지 않은 숫자 형식입니다. 문제가 되는 입력값: [" + input + "]");
+    private static void validateNullOrEmpty(String delimiter) {
+        if (delimiter == null || delimiter.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 커스텀 구분자입니다. 문제가 되는 입력값: [" + delimiter + "]");
+        }
+    }
+
+    private static void validateSingleCharacter(String delimiter) {
+        if (delimiter.length() != 1) {
+            throw new IllegalArgumentException("유효하지 않은 커스텀 구분자입니다. 구분자는 단일 문자여야 합니다. 문제가 되는 입력값: [" + delimiter + "]");
         }
     }
 }
