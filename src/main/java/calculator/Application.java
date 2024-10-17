958b76b60e5d7b214f92b65e54ed78ce01199e0a
박성빈 <psb106305@gmail.com>
2024-10-17 13:51:20 +0900

    feat: 입력 & 커스텀 구분자 파싱

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String input = Console.readLine();
+        String customSeparator = "";
+        if (input.startsWith("//")) {
+            String[] split = input.split(Pattern.quote("\\n"));
+            customSeparator = split[0].substring(2);
+            input = split[1];
+        }
     }
 }
