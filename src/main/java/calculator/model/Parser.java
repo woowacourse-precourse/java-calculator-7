24f48cf91b08749309efbaffe51627c101a9599c
dradnats1012 <herjebi1012@gmail.com>
2024-10-17 21:17:11 +0900

    feat: 커스텀 구분자 분리 메서드 생성

--- src/main/java/calculator/model/Parser.java
+++ src/main/java/calculator/model/Parser.java
@@ -1,6 +1,8 @@
 package calculator.model;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
 
@@ -9,4 +11,22 @@ public class Parser {
 
         return splitString;
     }
+
+    private List<String> parseCustomSeparator(String input){
+        Pattern pattern = Pattern.compile("[//](.*?)[\\n]");
+        Matcher matcher = pattern.matcher(input);
+
+        String realInput = input.substring(input.lastIndexOf("\\n"));
+        String customSeparator = matcher.group();
+
+        return parseCustom(realInput, customSeparator);
+    }
+
+    public List<String> parseCustom(String input, String custom){
+        String separator = String.format(",|:|%s", custom);
+        List<String> splitString = List.of(input.split(separator));
+
+        return splitString;
+    }
+
 }
