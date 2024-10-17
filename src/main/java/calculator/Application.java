22f997b339b829cd874720713208a84f64b5fdcc
jkh28 <jkhkorea28@naver.com>
2024-10-17 14:55:01 +0900

    Feat: 문자열 덧셈 기능 구현
    문자열 덧셈 계산기 개발

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String str = Console.readLine();
+        String regex = "[,:]";
+
+        String[] array = str.split(regex);
+
+        int result = 0;
+        for (String s : array) {
+            result += Integer.parseInt(s);
+        }
+
+        System.out.println(result);
     }
 }
