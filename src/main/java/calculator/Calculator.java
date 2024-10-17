3eb54bd84ec14779c5c3ecb4cd8364d2860ce200
jonghwanlee <kavae550@gmail.com>
2024-10-17 23:47:38 +0900

    feat(Calculator): 숫자 합 계산 기능 구현
    Calculator 클래스에 숫자들의 합을 구하는 getSum() 메서드를 구현했습니다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,11 @@
 package calculator;
 
 public class Calculator {
+    public static int getSum(int[] numbers) {
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        return sum;
+    }
 }
