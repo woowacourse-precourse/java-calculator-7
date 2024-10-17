09a9bd4a1736aafe74646d47dad0dd86ef97a9e1
hootzz <wjqwjql3@gmail.com>
2024-10-17 22:41:03 +0900

    feat: 구분자(, :) 처리 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,6 +5,11 @@ public class StringCalculator {
         if (input == null || input.isEmpty()) {
             return 0;
         }
+
+        // 구분자 처리해 숫자 나누기
+        String delimiter = "[,;]";
+        String[] tokens = input.split(delimiter);
+
         return 0;
     }
 }
