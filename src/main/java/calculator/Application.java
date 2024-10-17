8c5e5a1c856f24354c18fc0183278144e5121ba0
sik9252 <sik9252@naver.com>
2024-10-17 20:47:12 +0900

    feat: "//"와 "\n" 안에 입력된 문자를 기준으로 문자열을 분리하는 커스텀 구분자 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,8 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
@@ -13,6 +15,17 @@ public class Application {
 
         if (input.isEmpty()) {
             System.out.println("결과: " + 0);
+        } else {
+            int escapeIdx = input.indexOf("\\n");
+            if(escapeIdx > 0) {
+                String delimiter = input.substring(0, escapeIdx);
+                if(delimiter.startsWith("//")){
+                    delimiter = delimiter.replace("//", "");
+                }
+                String leftInput = input.substring(escapeIdx + 2);
+                String splitInput = Arrays.toString(leftInput.split(delimiter));
+                System.out.println(splitInput);
+            }
         }
     }
 }
