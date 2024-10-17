8e875e6ec8c9b354b0271a094d19de8220c93c13
menzzi <minmin3927@naver.com>
2024-10-17 19:16:17 +0900

    refactor(model) : Delimiter - 변수 final 추가

--- src/main/java/calculator/model/Delimiter.java
+++ src/main/java/calculator/model/Delimiter.java
@@ -4,18 +4,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Delimiter {
-    private static Set<Character> delimiterSet = new HashSet<>();
+    private static final Set<Character> delimiterSet = new HashSet<>();
 
     static {
         delimiterSet.add(',');
         delimiterSet.add(':');
     }
 
-    public static void updateDelimiter(Character delimiter){
+    public static void updateDelimiter(Character delimiter) {
         delimiterSet.add(delimiter);
     }
 
-    public static boolean isDelimiter(String str){
+    public static boolean isDelimiter(String str) {
         char ch = str.charAt(0);
         return delimiterSet.contains(ch);
     }
