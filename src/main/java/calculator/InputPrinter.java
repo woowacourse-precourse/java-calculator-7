7f8724476d4ec81d7354377b888c0ced936211fe
Su-daa <subin.kk019@gmail.com>
2024-10-17 09:54:19 +0900

    Refactor: 입력 메시지 출력 기능 분리 및 클래스 이름 변경

--- src/main/java/calculator/InputPrinter.java
+++ src/main/java/calculator/InputPrinter.java
@@ -2,9 +2,12 @@ package calculator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-public class InputView {
-    public String input() {
+public class InputPrinter {
+    public void printInputMessage() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public String input() {
         return readLine();
     }
 }
