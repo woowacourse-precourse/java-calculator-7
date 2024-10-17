a5356ecfe93e88c4ac9ad6f07f07da649caa8d97
minzzun99 <skg06123@naver.com>
2024-10-17 16:24:53 +0900

    refactor(domain): Number 클래스 생성자 접근 지정자 수정
    - Number() 생성자 default -> public 으로 접근 지정자 변경

--- src/main/java/calculator/domain/Number.java
+++ src/main/java/calculator/domain/Number.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Number {
     private final List<Integer> numberList;
 
-    Number() {
+    public Number() {
         this.numberList = new ArrayList<>();
     }
 
