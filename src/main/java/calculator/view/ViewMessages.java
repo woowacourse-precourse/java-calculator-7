54eef25088d203f697753e160f61237804c924f8
choiyoung69 <choiyoung609@gmail.com>
2024-10-17 19:34:37 +0900

    refactor(ViewMessages): 결과를 출력하는 메세지 변수명 변경

--- src/main/java/calculator/view/ViewMessages.java
+++ src/main/java/calculator/view/ViewMessages.java
@@ -2,5 +2,5 @@ package calculator.view;
 
 public class ViewMessages {
     public static final String REQUIRE_OPERAND_STRING_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
-    public static final String PRINT_CALCULATION_RESULT_MESSAGE = "결과 : ";
+    public static final String CALCULATION_RESULT_MESSAGE = "결과 : ";
 }
