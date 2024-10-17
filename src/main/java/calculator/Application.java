77104dd6172f73c6921854617aadf22d2e28f0f1
KNU-K <knukang334@gmail.com>
2024-10-17 17:30:22 +0900

    feat(app): 계산기 애플리케이션 진입점 추가
    Application 클래스를 추가하여 계산기 애플리케이션의
    진입점을 구현합니다. 이 클래스는 사용자로부터 입력을 받고,
    계산 서비스에 해당 입력을 전달하여 계산 결과를 출력합니다.
    
    - Console을 사용하여 사용자 입력을 받고,
      결과를 출력하는 구조입니다.
    - CalculatorService 인터페이스를 통해
      계산 로직을 분리하여 코드의 가독성과 유지보수성을 향상시킵니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import calculator.service.CalculatorService;
+import calculator.service.CalculatorServiceImpl;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        Console.close();
+        CalculatorService calculatorService = new CalculatorServiceImpl();
+        Integer result = calculatorService.calculate(input);
+        System.out.println("결과 : " + result);
+
     }
 }
