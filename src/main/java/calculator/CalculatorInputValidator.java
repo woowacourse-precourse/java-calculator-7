2b37f1f36023823199da11fa800f5e856dc463ee
minSsan <msp1125@hanyang.ac.kr>
2024-10-18 00:35:34 +0900

    refactor: 객체 분리 및 구조 설계

---
+++ src/main/java/calculator/CalculatorInputValidator.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class CalculatorInputValidator {
+    public void validate(String string) {
+
+    }
+}
---
+++ src/main/java/calculator/CalculatorNumberList.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import java.util.List;
+
+public class CalculatorNumberList {
+    private List<Integer> numbers;
+
+    public int sum(String input) {
+        return 0;
+    }
+}
