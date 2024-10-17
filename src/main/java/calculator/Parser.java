e79269d8a4c19cbb507bb656d64d8d0a5cc5b9ea
wingyou <winwing.you@gmail.com>
2024-10-17 17:31:08 +0900

    feat(Parser): implement parse()

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -6,7 +6,11 @@ package calculator;
 public class Parser {
 
     public int[] parse(String input) {
-        // TODO: implement method
-        return null;
+        String[] segments = input.split("[,|:]");
+        int[] numbers = new int[segments.length];
+        for (int i = 0; i < segments.length; i++) {
+            numbers[i] = Integer.parseInt(segments[i]);
+        }
+        return numbers;
     }
 }
