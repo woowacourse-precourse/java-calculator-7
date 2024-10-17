74a92c2bd014f40de58f882b56d0c0f35cf9d697
menzzi <minmin3927@naver.com>
2024-10-17 19:11:59 +0900

    refactor(controller) : DelimiterSeparator 리팩토링

--- src/main/java/calculator/controller/DelimiterSeparator.java
+++ src/main/java/calculator/controller/DelimiterSeparator.java
@@ -4,34 +4,34 @@ import calculator.model.Delimiter;
 
 public class DelimiterSeparator {
 
-    public static int registerDelimiter(String userInput){
+    public static int registerDelimiter(String userInput) {
         int index = 0;
-        if(!NumberChecker.isNumber(userInput.substring(0,1))){
+        if (!NumberChecker.isNumber(userInput.substring(0, 1))) {
             index = specifyingDelimiter(userInput);
         }
         return index;
     }
 
-    public static int specifyingDelimiter(String userInput){
-        if(!userInput.startsWith("//")){
+    public static int specifyingDelimiter(String userInput) {
+        if (!userInput.startsWith("//")) {
             throw new IllegalArgumentException("잘못된 형식입니다.");
         }
         int index = 2;
-        if(userInput.substring(index).length() < 3 ){
+        if (userInput.substring(index).length() < 3) {
             throw new IllegalArgumentException("잘못된 형식입니다.");
         }
         Delimiter.updateDelimiter(userInput.charAt(index));
-        if(!userInput.substring(index+1,index+3).equals("\\n")){
+        if (!userInput.startsWith("\\n", index + 1)) {
             throw new IllegalArgumentException("잘못된 형식입니다.");
         }
         return index + 3;
     }
 
-    public static void validateDelimiter(String str){
-        if(NumberChecker.isNumber(str)){
+    public static void validateDelimiter(String str) {
+        if (NumberChecker.isNumber(str)) {
             throw new IllegalArgumentException("잘못된 형식입니다.");
         }
-        if(!Delimiter.isDelimiter(str)){
+        if (!Delimiter.isDelimiter(str)) {
             throw new IllegalArgumentException("잘못된 형식입니다.");
         }
     }
