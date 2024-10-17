b3b129b7638b551937e3db40c3003fb05d59b4e8
lsj0822 <lsj0822@khu.ac.kr>
2024-10-18 00:27:44 +0900

    fix : CalculatorMain 수정
    CalculatorMain을 객체지향적으로 수정하였습니다.

--- src/main/java/calculator/CalculatorMain.java
+++ src/main/java/calculator/CalculatorMain.java
@@ -2,15 +2,21 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 import inspector.ContentInspector;
+import inspector.DefaultInspector;
 import java.math.BigInteger;
+import parser.ContentParser;
 
 public class CalculatorMain {
-    private BigInteger calculateResult = new BigInteger("0");
+    private BigInteger calculateResult = BigInteger.ZERO;
     private String inputContent;
+    private String[] parsedContent;
 
     public void startCalculator() {
         setInputContent();
-        ContentInspector contentInspector = new ContentInspector(inputContent);
+        ContentInspector contentInspector = new DefaultInspector(inputContent);
+        ContentParser contentParser = new ContentParser(contentInspector.getDelimiterGroup(), inputContent);
+        parsedContent = contentParser.getParsedContent();
+        Calculation();
         getCalculateResult();
     }
 
@@ -21,5 +27,15 @@ public class CalculatorMain {
 
     private void getCalculateResult() {
         System.out.println("결과 : " + calculateResult);
+    }
+
+    private void Calculation() {
+        for (String content : parsedContent) {
+            BigInteger contentNumber = new BigInteger(content);
+            if (contentNumber.compareTo(BigInteger.ZERO) == -1) {
+                throw new IllegalArgumentException();
+            }
+            calculateResult = calculateResult.add(new BigInteger(content));
+        }
     }
 }
