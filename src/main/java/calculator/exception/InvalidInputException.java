294418a0ce0e35c16665cfa0f21c4fcf299530e5
minSsan <msp1125@hanyang.ac.kr>
2024-10-18 00:41:09 +0900

    refactor(InvalidInputException): 입력 형식 예외처리 문구 수정

--- src/main/java/calculator/exception/InvalidInputException.java
+++ src/main/java/calculator/exception/InvalidInputException.java
@@ -2,6 +2,6 @@ package calculator.exception;
 
 public class InvalidInputException extends IllegalArgumentException {
     public InvalidInputException() {
-        super("입력 값은 최소 한 글자 이상의 문자열 혹은 '// ... \\n ...' 패턴의 문자열이어야 합니다.");
+        super("입력 형식이 올바르지 않습니다.");
     }
 }
