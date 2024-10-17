3171fc734466cc162646fc4d8cd4c8400d19662b
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-10-18 00:01:27 +0900

    feat: 메인함수

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,15 +5,18 @@ import java.util.Arrays;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        InputHandler inputHandler = new InputHandler();
-        Calculator calculator = new Calculator();
-        InputHandler.DelimiterType delimiterType;
 
-        String userInput = inputHandler.getUserInput();
+        DelimiterType delimiterType;
+        InputReader inputReader = new InputReader();
+        InputValidator inputValidator = new InputValidator();
+        InputTransformer inputTransformer = new InputTransformer();
+        Calculator calculator = new Calculator();
+
+        String userInput = inputReader.getUserInput();
         System.out.println("입력: "+ userInput);
         System.out.println( "길이:"+ userInput.length());
-        delimiterType = inputHandler.isValidInput(userInput);
-        int[] numbers = inputHandler.stringToIntArray(userInput, delimiterType);
+        delimiterType = inputValidator.isValidInput(userInput);
+        int[] numbers = inputTransformer.stringToIntArray(userInput, delimiterType);
         int result = calculator.addition(numbers);
 
         System.out.println("결과 : " + Integer.toString(result));
