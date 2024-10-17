bcda96761be3542e617ae5ebac4cf0fc32c2f0ce
binn <anyubin0724@naver.com>
2024-10-17 17:10:49 +0900

    feat : StringSplitter.java 예외처리 추가
    커스텀 구분자에서 "\n"을 입력하지 않은 경우 예외처리

--- src/main/java/calculator/controller/StringSplitter.java
+++ src/main/java/calculator/controller/StringSplitter.java
@@ -8,10 +8,17 @@ public class StringSplitter {
 
         if (input.startsWith("//")) {
             int endDelimiter = input.indexOf("\n");
+            isValidCustom(endDelimiter);
             delimiter = input.substring(2, endDelimiter);
             input = input.substring(endDelimiter + 1);
         }
 
         return input.split(delimiter);
+    }
+
+    private static void isValidCustom(int index) {
+        if (index == -1) {
+            throw new IllegalArgumentException("커스텀 구분자 생성 규칙에 맞지 않습니다.");
+        }
     }
 }
