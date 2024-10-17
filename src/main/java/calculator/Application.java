4e342edf268e3b843b8353ea10f64a8187780b55
Jinu Yi <dionisos198@naver.com>
2024-10-17 17:06:15 +0900

    feat(Application): 사용자 입력 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import calculator.controller.CalculateController;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String userInput = Console.readLine();
+        CalculateController calculateController = new CalculateController();
+
+        calculateController.insertUserInput(userInput);
+
     }
 }
