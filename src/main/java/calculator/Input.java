d3afbc89fcb5132cd655fe878ee333ff4895ba79
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-10-17 15:24:44 +0900

    feat: Add input validation
    - Print 0 when the input is an empty string
    - Throw IllegalArgumentException for negative numbers
    - Throw IllegalArgumentException when no numbers are present

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -3,7 +3,25 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input  {
     public String getInput() {
         System.out.println("덧셈할 문자열을 입력해주세요.");
-        String Input = Console.readLine();
+        String input = Console.readLine();
+        validateInput(input);
+        return input;
+    }
+
+    public void validateInput(String input) {
+        // 빈 문자열 검사
+        if (input.trim().length() == 0) {
+            System.out.println("0");
+        }
+        // 음수 검사
+        if (input.contains("-")) {
+            throw new IllegalArgumentException("음수가 포함되어 있습니다.");
+        }
+        // 숫자 포함 검사
+        if (!input.matches(".*[0-9].*")) {
+            throw new IllegalArgumentException("숫자가 포함되지 않은 잘못된 입력입니다.");
+        }
+
     }
 
 
