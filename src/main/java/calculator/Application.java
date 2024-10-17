e57e7990f82195454dfdffa394ead816f2a75369
김민아 <alsdk9349@naver.com>
2024-10-17 20:42:57 +0900

    feat(Application) : basic feature setting
    가장 기본적인 형태의 기능은 구현
    추가로만들어야할 조건이 많음
    코드 자체도 잘 만들었는지 확인 필요

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,9 +3,35 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO: 프로그램 구현
+
+        // 입력을 요청하는 문자열 출력
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        // 입력 받아서 String에 저장
         String string = Console.readLine();
+
+        // 답을 저장할 int 변수 생성
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i=0;i<string.length();i++) {
+            char c= string.charAt(i);
+            int n = c-'0';
+            if (0<=n && n<=9) {
+                sb.append(c);
+            } else if (c==',' || c==':'){
+                if (!sb.isEmpty()) {
+                    answer += Integer.parseInt(sb.toString());
+                    sb = new StringBuilder();
+                }
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        if (!sb.isEmpty()) {
+            answer += Integer.parseInt(sb.toString());
+        }
+        System.out.println(answer);
     }
 }
