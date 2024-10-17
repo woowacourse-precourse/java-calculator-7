655ec2ebf15f318489170a598047dc923197222e
태규 <123963462+EOTAEGYU@users.noreply.github.com>
2024-10-17 17:28:40 +0900

    docs: README.md 수정
    기능 목록 추가 - 입력값 받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,9 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("//;\nhi");
-        // TODO:
+
     }
 }
