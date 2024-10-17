5d7cf6552e7f19a37118446f58ac56f75dacba8f
GO-TE <alsrudxo9810@naver.com>
2024-10-17 12:50:58 +0900

    chore: SeparatorManager 오타 수정

--- src/main/java/calculator/SeparatorManager.java
+++ src/main/java/calculator/SeparatorManager.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class SepartorMananger {
+public class SeparatorManager {
     List<String> separators;
 
-    public SepartorMananger() {
+    public SeparatorManager() {
         this.separators = new ArrayList<>(Arrays.asList(",", ":"));
     }
 
--- src/test/java/calculator/SepartorManangerTest.java
+++ src/test/java/calculator/SepartorManangerTest.java
@@ -4,25 +4,24 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
-class SepartorManangerTest {
-    SepartorMananger separtorMananger = new SepartorMananger();
+class SeparatorManangerTest {
+    SeparatorManager separatorManager = new SeparatorManager();
 
     @Test
     void 이미_있는_구분자라면_True_반환() {
-        assertTrue(separtorMananger.exists(":"));
+        assertTrue(separatorManager.exists(":"));
     }
 
     @Test
     void 없는_구분자라면_False_반환() {
-        assertFalse(separtorMananger.exists("!"));
+        assertFalse(separatorManager.exists("!"));
     }
 
     @Test
     void 받은_구분자_리스트의_수정_불가한지_확인() {
-        assertThatThrownBy(() -> separtorMananger.getSeparators().remove(0))
+        assertThatThrownBy(() -> separatorManager.getSeparators().remove(0))
                 .isInstanceOf(UnsupportedOperationException.class);
     }
 }
