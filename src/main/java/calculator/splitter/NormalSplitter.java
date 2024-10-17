f96906cd5600c41c503e7bdcc27309c8dfed4bb6
LSBsity <dltmdqls51@naver.com>
2024-10-17 17:06:06 +0900

    fix: Trim 추가

--- src/main/java/calculator/splitter/NormalSplitter.java
+++ src/main/java/calculator/splitter/NormalSplitter.java
@@ -9,19 +9,20 @@ public class NormalSplitter implements Splitter { //todo -> Make Regex splitter
     public String[] split(String input) {
         String delimiter = CalculatorConst.DEFAULT_DELIMITERS; // 기본 문자열 설정
 
-        if (input.startsWith("//")) {       // 커스텀 문자열 설정했는지
-            int idx = input.indexOf("\\n"); // '\n' 인덱스 찾기
+        String trimmed = input.trim();
+        if (trimmed.startsWith("//")) {       // 커스텀 문자열 설정했는지
+            int idx = trimmed.indexOf("\\n"); // '\n' 인덱스 찾기
             if (idx == -1) {                // 없으면 Exception
                 throw new IllegalArgumentException();
             }
 
-            String token = input.substring(2, idx);     // 커스텀 구분자 추출
+            String token = trimmed.substring(2, idx);     // 커스텀 구분자 추출
             delimiter += "|" + escapeRegex(token);      // 특수 문자를 Escape 처리 후 구분자에 추가
 
-            return input.substring(idx + 2)
+            return trimmed.substring(idx + 2)
                     .split(delimiter); //커스텀 구분자가 맞다면 '\n' 이후 문자열로부터 분리
         }
-        return input.split(delimiter); //커스텀 구분자가 아니면 기본 구분자를 통해 분리
+        return trimmed.split(delimiter); //커스텀 구분자가 아니면 기본 구분자를 통해 분리
     }
 
     private String escapeRegex(String input) {
