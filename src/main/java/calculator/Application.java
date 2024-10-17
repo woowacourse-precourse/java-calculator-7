de76e6896d18c1fa066efffd4aa522fc1ce5cb1d
Ga Dong Sik <ret0422@gmail.com>
2024-10-17 17:39:35 +0900

    style (Application.java): Application.java에 구현할 기능 TODO 작성 후 Class 생성
    - InputService.java 생성
    - CalculatorService.java 생성
    - OutputService.java 생성
    - Application.java에 Todo 작성
    
    구현을 위한 Todo 및 사용 클래스 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+
+import calculator.service.CalculatorService;
+import calculator.service.InputService;
+import calculator.service.OutputService;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 문자열 입력, 커스텀 구분자 추출 후 입력 문자열, 구분자 반환
+        InputService inputService = new InputService();
+        // TODO: 인자로 보내진 문자열과 구분자로 숫자 추출 후 더함. 더해진 값 반환
+        CalculatorService calculatorService = new CalculatorService();
+        // TODO: 반환된 결과값 출력
+        OutputService outputService = new OutputService();
     }
 }
