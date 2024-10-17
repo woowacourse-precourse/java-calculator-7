fae5bbdb4d8cb1103c8a369f365101918e814767
sim-mer <gyu2781@gmail.com>
2024-10-17 18:32:21 +0900

    refactor: 메서드 역할에 맞도록 수정

--- src/main/java/calculator/InputString.java
+++ src/main/java/calculator/InputString.java
@@ -7,21 +7,18 @@ public class InputString {
     private List<Integer> numbers;
 
     public InputString(String input) {
-        if (isInputEmpty(input)) {
-            return;
-        }
         this.numbers = inputToList(input);
     }
 
     private boolean isInputEmpty(String input) {
-        if(input == null || input.isEmpty()) {
-            this.numbers = List.of();
-            return true;
-        }
-        return false;
+        return input == null || input.isEmpty();
     }
 
     private List<Integer> inputToList(String input) {
+        if (isInputEmpty(input)) {
+            return List.of();
+        }
+
         return Arrays.stream(input.split("[,:]"))
                 .map(Integer::valueOf)
                 .toList();
