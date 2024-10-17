59fdbcfb4ae735fab9c6d52af2bc8c0c6a143bca
kaswhy <sxshx818@naver.com>
2024-10-17 17:16:51 +0900

    feat: 구분자 등록 - 커스텀 구분자 사용 여부 확인 함수 구현
    프로그램에서 필요한 단계별로 틀을 짜 놓았고, 그 중 입력과 구분자 사용 여부 함수를 구현하였습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,35 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    static String delimeter = ",|:";
+    static String inputExpression;
+
+    static int hasCustom(String str) {
+        int start = str.indexOf("//");
+        return start;
+    }
+
+    static void registerCustom(String str) {
+
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 입력
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        inputExpression = Console.readLine();
+
+        // 구분자 등록
+        if (hasCustom(inputExpression) != -1) {
+            registerCustom(inputExpression);
+        }
+
+        // 구분자로 문자열 자르기
+
+        // 추출된 숫자들의 합 구하기
+
+        // 결과 출력
     }
 }
+
