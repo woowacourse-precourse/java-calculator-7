d33be575a2677d09099bdd4e994d16ae72eeead5
Minseok <saltishDev@gmail.com>
2024-10-17 17:34:48 +0900

    feat: 기본 구분자를 기준으로, 입력받은 문자열을 분리하는 기능 구현
    1. 기본 구분자(쉼표, 콜론)를 기준으로, 입력받은 문자열을 분리
    2. 입력받은 문자열을 분리한 결과를 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,6 +7,12 @@ public class Application {
         System.out.print("문자열 입력 : ");
         String inputString = Console.readLine();
         System.out.printf("입력받은 문자열 : %s\n", inputString);
+
+        String[] splitStrings = splitString(inputString);
+        System.out.println("[입력받은 문자열을 분리한 결과]");
+        for (String splitString : splitStrings) {
+            System.out.println(splitString);
+        }
     }
 
     private static boolean isStringWithCustomDelimiter(String str) {
@@ -17,5 +23,13 @@ public class Application {
         String suffix = str.substring(3, 5);
 
         return prefix.equals("//") && suffix.equals("\\n");
+    }
+
+    private static String[] splitString(String str) {
+        if (isStringWithCustomDelimiter(str)) {
+            // FIXME. 기본 구분자와 커스텀 구분자 모두 고려하여, 입력받은 문자열을 분리하도록 수정
+            return new String[]{};
+        }
+        return str.split("[,:]");
     }
 }
