987c91cfe686bcffa413f33cc8fbc598d399ec21
GO-TE <alsrudxo9810@naver.com>
2024-10-17 17:08:07 +0900

    feat(Validator): 커스텀 구분자에 대한 예외처리 구현

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -8,4 +8,19 @@ public class Validator {
     public boolean isStartWithDigit(String input) {
         return Character.isDigit(input.charAt(0));
     }
+
+    public void validateSeparator(String separator) {
+        if (separator.length() != 1) {
+            throw new IllegalArgumentException("구분자의 길이는 1이어야 합니다.");
+        }
+        if (separator.equals(",") || separator.equals(":")) {
+            throw new IllegalArgumentException("이미 구분자로 등록 되어 있습니다.");
+        }
+        if (separator.equals(".")) {
+            throw new IllegalArgumentException(".은 구분자가 될 수 없습니다.");
+        }
+        if (isStartWithDigit(separator)) {
+            throw new IllegalArgumentException("숫자는 구분자가 될 수 없습니다.");
+        }
+    }
 }
