0dd9f2475246d53283035f995615a04aeae4ff1f
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-17 22:52:33 +0900

    feat(Prompt): 메시지를 출력하고 Input 받는 기능 추가
    "덧셈할 문자열을 입력해 주세요."을 출력하고 Console.readLine()을 통해 입력을 받는 기능을 추가했습니다.
    입력이 끝나면 Console.close()를 호출하고 입력 받는 String을 return 해주는 기능을 추가했습니다.

---
+++ src/main/java/calculator/Prompt.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Prompt {
+    public static String input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.\n");
+        String input = Console.readLine();
+        Console.close();
+        return input;
+    }
+}
