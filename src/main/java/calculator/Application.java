c09fba800d142ff0880786626af87f33c56df91d
reindeer002 <lsj000424@gmail.com>
2024-10-17 16:00:23 +0900

    feat(main): add application basic running process

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import calculator.service.CalculatorService;
+import calculator.service.impl.CalculatorServiceImpl;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorService calculator = new CalculatorServiceImpl();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String s = Console.readLine();
+        System.out.println("결과 : " + calculator.add(s));
+        Console.close();
     }
 }
