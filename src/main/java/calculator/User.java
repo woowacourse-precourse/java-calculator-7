2a56fe15d594c9eeaef4eaff9acfe9f4c84d3023
sc@t0a14 <gusdn6671@gmail.com>
2024-10-17 20:28:01 +0900

    Feat(User) : 사용자 입력값이 공백일 경우와 공백으로 시작할 경우 예외사항 구현

--- src/main/java/calculator/User.java
+++ src/main/java/calculator/User.java
@@ -1,9 +1,16 @@
 package calculator;
 
 public class User {
-    private String user;
+    private final String user;
 
     public User(String user) {
+        if(user.isBlank()){
+            throw new IllegalArgumentException("공백만 입력할수 없습니다");
+        }
+
+        if (Character.isWhitespace(user.charAt(0))) {
+            throw new IllegalArgumentException("입력값은 공백으로 시작할 수 없습니다");
+        }
         this.user = user;
     }
 
