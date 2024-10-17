416e46286e96628c17f5fa4c081c804ddafc8053
inhooo00 <chldlsgh0987@naver.com>
2024-10-17 23:42:27 +0900

    feat(model): Add CustomDelimiterPattern enum for custom separator validation
    Add CustomDelimiterPattern enum for custom separator validation:
    - "//"와 "\n"을 구분자 검증 패턴으로 등록.
    - 구분자 검증 패턴을 반환.

--- src/main/java/calculator/constant/CustomDelimiterPattern.java
+++ src/main/java/calculator/constant/CustomDelimiterPattern.java
@@ -1,2 +1,16 @@
-package calculator.constant;public enum CustomDelimiterPattern {
+package calculator.constant;
+
+public enum CustomDelimiterPattern {
+    PREFIX("//"),
+    SUFFIX("\n");
+
+    private final String symbol;
+
+    CustomDelimiterPattern(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
