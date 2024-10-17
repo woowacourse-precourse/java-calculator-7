b5d4f9dbcf9cf9ef045639d3630cba3e1d60b046
Hacanna42 <dtmdgus901@gmail.com>
2024-10-18 02:53:20 +0900

    feat: add validation logic to InputHandler

--- src/main/java/calculator/input/InputHandler.java
+++ src/main/java/calculator/input/InputHandler.java
@@ -1,18 +1,30 @@
 package calculator.input;
 
+import calculator.parse.StringParser;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
     private final Validator validator;
+    private final StringParser stringParser;
 
-    // 외부에서 Validator 를 주입받음. (의존성 주입)
-    public InputHandler(Validator validator) {
+    // 외부에서 validator, stringParser 주입받음. (의존성 주입)
+    public InputHandler(Validator validator, StringParser stringParser) {
         this.validator = validator;
+        this.stringParser = stringParser;
     }
 
     public String getInput() {
-        String input = Console.readLine();
-        validator.check(input); // Validator 에서 입력 값 검증 후 반환
+        String input = Console.readLine(); // 사용자에게 값을 입력 받음
+
+        // Validator 을 통해서 검증
+        if (validator.hasCustomDelimiter(input)) {
+            String customDelimiter = stringParser.getCustomDelimiter();
+            validator.check(input, customDelimiter);
+        } else {
+            validator.check(input);
+        }
+
+        // 올바른 입력 값 반환
         return input;
     }
 }
