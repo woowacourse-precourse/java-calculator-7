d51bb74b4a1bc0969c9e8734286eeef8fdc2c4d3
KSJ0128 <ksj01128@naver.com>
2024-10-17 12:38:33 +0900

    feat: FrontController 추가
    - 프로그램 실행 흐름 제어하는 FrontController 도입
    - 객체 초기화 및 주요 로직 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,11 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        FrontController frontController = initFrontController();
+        frontController.run();
+    }
+
+    public static FrontController initFrontController() {
+        return new FrontController();
     }
 }
