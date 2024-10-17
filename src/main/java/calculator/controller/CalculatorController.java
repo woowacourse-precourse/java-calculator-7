e311ee8ddd2391def24fd8a852ab917118233f46
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-10-17 17:29:38 +0900

    feat: 커스텀 구분자를 이용한 계산 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -11,12 +11,35 @@ public class CalculatorController {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = readLine();
 
-        print(defaultDelimiter(str));
+        if (isCustomDelimiter(str)) {
+            print(customDelimiter(str));
+        } else {
+            print(defaultDelimiter(str));
+        }
+    }
+
+    private static boolean isCustomDelimiter(String str) {
+        String firstStr = str.substring(0, 2);
+        String secondStr = str.substring(3, 5);
+
+        if (firstStr.equals("//") && secondStr.equals("\\n")) {
+            return true;
+        }
+        return false;
     }
 
     private static Integer defaultDelimiter(String str) {
         List<String> defaultValues = Arrays.asList(str.split(":|,"));
         return calculateSum(defaultValues);
+    }
+
+    private static Integer customDelimiter(String str) {
+        String delimiter = String.valueOf(str.charAt(2));
+        String realNumStr = str.substring(5);
+
+        List<String> customValues = Arrays.asList(realNumStr.split("\\Q" + delimiter + "\\E"));
+
+        return calculateSum(customValues);
     }
 
     private static int calculateSum(List<String> values) {
