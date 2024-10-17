d3e7a6dd1f27518c4c10347c20e0be2f2103b3f4
EHCTH <lees90896@gmail.com>
2024-10-17 18:10:01 +0900

    feat(Application): Application

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.controller.CalculatorControllerFactory;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CalculatorController calculatorController = CalculatorControllerFactory.create();
+        calculatorController.run();
     }
 }
