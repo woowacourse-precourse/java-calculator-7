d58ed02777243f81a8b337c8b1be164da6b0aaaf
EunSeop <doosan1112@naver.com>
2024-10-17 23:20:06 +0900

    feat: 구분자를 제외한 문자 입력시 예외처리 기능 구현

---
+++ src/main/java/calculator/validator/AnotherLetterValidator.java
@@ -0,0 +1,18 @@
+package calculator.validator;
+
+public class AnotherLetterValidator {
+    String input;
+    public AnotherLetterValidator(String input) {
+        this.input = input;
+        validateAnotherLetter();
+    }
+    private void validateAnotherLetter() {
+        if (input.chars().anyMatch(c -> !Character.isDigit(c) && c != ',')) {
+            throw new IllegalArgumentException("허용되지 않는 문자가 존재합니다.");
+        }
+        char Separator = input.charAt(2);
+        if (input.chars().anyMatch(c -> c != ',' && c != Separator && !Character.isDigit(c))) {
+            throw new IllegalArgumentException("허용되지 않는 문자가 존재합니다.");
+        }
+    }
+}
