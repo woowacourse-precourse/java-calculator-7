e0ab14e16303ec6e7d290475d6cd012a85d1d4b0
박근채 <pgc0419@gmail.com>
2024-10-17 17:04:29 +0900

    숫자와 구분자로 입력시 덧셈

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,17 @@ public class Application {
     public static void main(String[] args) {
         System.out.print("덧셈할 문자열을 입력해주세요." + "\n");
         String Input = Console.readLine();
+
+        int sum = 0;
+
+        String[] tokens = Input.split("[,:]");
+
+        for (String token : tokens) {
+            token = token.trim();
+            sum += Integer.parseInt(token);
+        }
+
+        System.out.println("결과 : " + sum);
     }
 }
 
