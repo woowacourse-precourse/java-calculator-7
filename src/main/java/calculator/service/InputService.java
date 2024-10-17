c9c0b95f0d09c9123a0b0fb61f494fd3efacabdb
jijongkwon <help3451@naver.com>
2024-10-17 23:27:29 +0900

    feat(service) : "" -> 0 처리 로직 변경

--- src/main/java/calculator/service/InputService.java
+++ src/main/java/calculator/service/InputService.java
@@ -1,6 +1,5 @@
 package calculator.service;
 
-import calculator.enums.ErrorMessages;
 import camp.nextstep.edu.missionutils.Console;
 
 import static calculator.util.Validator.validateCustomDelimiter;
@@ -8,12 +7,12 @@ import static calculator.util.Validator.validateCustomDelimiter;
 public class InputService {
     public String readInput() {
         //TODO: 사용자 입력 받기 및 검증 로직 구현
-        try {
-            String input = Console.readLine();
-            validateCustomDelimiter(input);
-            return input;
-        } catch (Exception e) {
-            throw new IllegalArgumentException(ErrorMessages.EMPTY_INPUT.getMessage());
-        }
+        String input = changeEmptyToZero(Console.readLine());
+        validateCustomDelimiter(input);
+        return input;
+    }
+
+    private String changeEmptyToZero(String input){
+        return input.isEmpty() ? "0" : input;
     }
 }
