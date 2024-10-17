95b4bf87e450b28e363b5ab721a23a912c147b2d
Yunsu4 <ysa020428@gmail.com>
2024-10-17 14:39:41 +0900

    test: add customized test cases
    add customized test cases based on feature list

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,9 +19,49 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 문자_입력_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자만_입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            run("910");
+            assertThat(output()).contains("결과 : 910");
+        });
+    }
+
+    @Test
+    void 빈문자_입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 구분자만_입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            run(":");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자가_여러개인_입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            run("4,5//%\\n6%7//,\\n8,9");
+            assertThat(output()).contains("결과 : 147");
+        });
     }
 
     @Override
