35ae8051fc782004b09c1338e1146772f9a5388f
최영우 <allnstar@naver.com>
2024-10-17 20:13:48 +0900

    feat: 덧셈을 실행한 결과를 출력하는 기능 구현

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,8 +2,13 @@ package calculator.view;
 
 public class OutputView {
     private static final String INITIAL_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String FORMAT_OF_RESULT = "결과 : %d\n";
 
     public static void printInitialMessage(){
         System.out.println(INITIAL_MESSAGE);
+    }
+
+    public static void printResult(int sum){
+        System.out.printf(FORMAT_OF_RESULT, sum);
     }
 }
