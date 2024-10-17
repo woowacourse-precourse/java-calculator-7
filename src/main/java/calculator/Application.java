a3fd7a7d580da8ca87c8cc76e38c96e5389ceaab
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-10-17 21:44:30 +0900

    feat: throw exception on invalid input
    Throw IllegalArgumentException on invalid input
    
    changed:
    - Implemented function to separate letters and numbers using comma(,) and colon(:) with contains method
    - Move the function to split with "//(char)\n" lower

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,17 +6,23 @@ public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String st = sc.next();
-        String[] arr = st.split("[:;]");
 
-        if(st.contains("//") && st.contains("\\n")) {
-            String[] arr1 = st.split("\\D");
-            for (String s : arr1) {
-                System.out.print(s);
-            }
-        } else {
+        if (st.contains(":") || st.contains(";")) {
+            String[] arr = st.split("\\D");
+
             for (String s : arr) {
                 System.out.print(s);
             }
+
+        } else if (st.contains("//") && st.contains("\\n")) {
+            String[] arr1 = st.split("\\D");
+
+            for (String s : arr1) {
+                System.out.print(s);
+            }
+
+        } else {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
         }
     }
 }
