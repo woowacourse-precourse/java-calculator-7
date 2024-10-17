44f0a96db31728ff170562d76fa12e89da56b62a
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2024-10-17 21:08:57 +0900

    feat: 기본 구분자와 커스텀 구분자로 문자열 나누기

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -3,6 +3,8 @@ package calculator.view;
 import calculator.validate.CalculatorValidate;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+
 public class InputView {
 
     private final CalculatorValidate calculatorValidate;
@@ -22,15 +24,21 @@ public class InputView {
     /**
      * 사용자로부터 문자열 입력 받음
      * 입력 받은 문자열에서 커스텀 구분자를 사용하는지 확인
+     * 커스텀 구분자를 사용 시 기존 구분자에 커스텀 구분자 추가
+     * 수정된 구분자로 문자열 분리
      */
     public void getUserInput()
     {
         String invalidateString = Console.readLine();
+        String separtors = ",:";
         if(calculatorValidate.isUsingCustomSeparator(invalidateString))
         {
-
+            separtors += invalidateString.charAt(2);
+            invalidateString = invalidateString.substring(5);
         }
+        String[] invalidateStringArray = invalidateString.split("[" + separtors + "]");
 
+        System.out.println(Arrays.toString(invalidateStringArray));
     }
 }
 
