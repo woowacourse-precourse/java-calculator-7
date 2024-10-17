af087cca779be5a62f006fe4747779080e388b19
kingjinyong <wlsdydwhswkf@gmail.com>
2024-10-17 11:14:02 +0900

    feat(Application): 커스텀 구분자 지정 판별 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,12 +2,22 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Scanner;
-
 public class Application {
+
+    public static Boolean CustomDiscrimination(String input){
+        if (input.startsWith("//") && input.contains("\\n"))
+            return true;
+        else if (!input.startsWith("//") && !input.contains("\\n"))
+            return false;
+        else
+            throw new IllegalArgumentException();
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        Console.readLine();
+        String str = Console.readLine();
+        Boolean isCustom = CustomDiscrimination(str);
     }
 }
+// //;\n1,2:3
