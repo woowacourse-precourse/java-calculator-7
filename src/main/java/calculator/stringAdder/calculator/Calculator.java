427d152b7e1cc01923a8911f23a4c11671e47699
jiffyin7 <jiffyin7@gmail.com>
2024-10-17 19:09:41 +0900

    feat(StringAdder): Calculator 실행 로직 구현
    - Calculator 클래스의 sum 메서드 호출
    - 반환된 결과를 출력
    
    이슈 : #10

---
+++ src/main/java/calculator/stringAdder/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator.stringAdder.calculator;
+
+/**
+ * @author : jiffyin7@gmail.com
+ * @since : 24. 10. 17.
+ */
+public class Calculator {
+
+
+  public String sum(String[] numbers) {
+    return null;
+  }
+}
