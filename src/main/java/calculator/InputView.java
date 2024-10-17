7c6d3aa96a1085c13e54d9a49275dcdc2c5ca679
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-17 22:08:57 +0900

    refactor(InputView): 상수 변수 추가, 사용자 입력 삭제
    재사용성을 높이기 위해 INPUT_MESSAGE 상수 변수 선언
    CalculatorController에서 사용자 입력을 받아 처리할 수 있도록 InputView에서 기능 삭제

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -1,12 +1,11 @@
 package calculator;
 
-import java.util.Scanner;
-
 public class InputView {
 
-    public String getUserInput() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public void printInput() {
+        System.out.println(INPUT_MESSAGE);
     }
+    
 }
