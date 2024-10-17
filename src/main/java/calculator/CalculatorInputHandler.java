7bac7d67907b3350fee03f67698b19915b3c42bf
Hanjjong <tim4974@naver.com>
2024-10-17 18:04:50 +0900

    feat(CalculatorInputHandler): 사용자 입력 받기 기능 추가

--- src/main/java/calculator/CalculatorInputHandler.java
+++ src/main/java/calculator/CalculatorInputHandler.java
@@ -1,15 +1,26 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class CalculatorInputHandler {
+    private final Scanner scanner;
 
     public CalculatorInputHandler() {
+        this.scanner = new Scanner(System.in);
     }
 
-    public void run() {
+    public String input() {
         printMessage();
+        return scanMessage();
     }
 
     private void printMessage() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    private String scanMessage() {
+        String input = scanner.nextLine();
+        scanner.close();
+        return input;
     }
 }
