e748e7330d2fa9abc3baacc705e8e03fad8acc6d
sangxxjin <sj_0368@naver.com>
2024-10-17 23:28:37 +0900

    refactor: 메시지 상수화
    본문:
    입력 및 출력 메시지를 상수화 했습니다.

--- src/main/java/calculator/views/calculatorInputView.java
+++ src/main/java/calculator/views/calculatorInputView.java
@@ -2,16 +2,17 @@ package calculator.views;
 
 import camp.nextstep.edu.missionutils.Console;
 
-
 public class calculatorInputView {
 
+    private static final String INPUT_PROMPT = "덧셈할 문자열을 입력해 주세요.";
+    private static final String RESULT_MESSAGE = "결과: ";
+
     public static String readInput() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println(INPUT_PROMPT);
         return Console.readLine();
     }
 
-  public static void displayResult(long result) {
-    System.out.println("결과: " + result);
-  }
-
+    public static void displayResult(long result) {
+        System.out.println(RESULT_MESSAGE + result);
+    }
 }
