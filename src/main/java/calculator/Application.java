94d6a0838ceea0ae618716c8617d1e0d4c544fb2
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-10-17 21:02:00 +0900

    feat: 문자열 입력 요청 메시지 출력 및 입력 문자열 단순 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputString = Console.readLine();
+
+        System.out.println("결과 : " + inputString);
+
+        // 사용이 끝나면 반드시 close() 메서드를 호출하여 자원 해제
+        Console.close();
+
     }
 }
