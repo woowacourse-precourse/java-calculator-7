5083782b0a55a18c3b78aab6a8ac4fbfb3c4fe4b
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-10-17 19:38:44 +0900

    feature : 문자열 입력 받기 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,12 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Calculator {
 
     public void start(){
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputStr = Console.readLine();
     }
 
 }
