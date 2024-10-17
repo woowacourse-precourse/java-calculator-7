d0eaabdd6529dcee66a0e96278b76eca29f6a032
dradnats1012 <herjebi1012@gmail.com>
2024-10-17 21:02:53 +0900

    feat: 입력받는 메서드 생성

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,4 +1,11 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public static String getInput(){
+
+        return Console.readLine();
+    }
 }
