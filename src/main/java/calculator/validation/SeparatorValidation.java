4b386daf5a5e7564861907dcf1d3c093005fa582
jaeyeong <zzz26lk@gmail.com>
2024-10-17 22:50:21 +0900

    refactor: 불리언 조건문 가독성 개선 boolean 조건문에 부정이 들어감으로써 가독성이 안 좋다. 조건문을 메서드로 만들어 has를 사용해 명확한 의미를 전달한다.

--- src/main/java/calculator/validation/SeparatorValidation.java
+++ src/main/java/calculator/validation/SeparatorValidation.java
@@ -4,7 +4,7 @@ import calculator.number.CustomPatternMaker;
 
 public class SeparatorValidation {
 
-    private static final String NOT_SPECIAL_CHARACTERS = "^\\wㄱ-ㅎ가-힣,:\\-";
+    private static final String SPECIAL_CHARACTERS = "^\\wㄱ-ㅎ가-힣,:\\-//\\n";
 
     public static void separator(String input) {
         if (isNotSeparator(input)) {
@@ -12,7 +12,7 @@ public class SeparatorValidation {
         }
         // 커스텀 구분자를 사용할 경우 검사
         if (input.contains("//") || input.contains("\\n")) {
-            if (!containCustomSeparator(input)) {
+            if (hasNotCustomSeparator(input)) {
                 throw new IllegalArgumentException("커스텀 구분자를 사용하려면 반드시 //과 \\n 모두 사용해야 합니다.");
             }
             if (input.indexOf("//") != 0) {
@@ -25,27 +25,26 @@ public class SeparatorValidation {
     }
 
     private static boolean isNotSeparator(String input) {
-        if (!input.contains(",") && !input.contains(":")) {
-            if (containCustomSeparator(input) || notContainSpecialCharacters(input)) {
-                return false;
-            }
-            return true;
-        }
-        if (!input.contains(",") || !input.contains(":")) {
-            if (notContainSpecialCharacters(input)) {
-                return false;
-            }
-            return true;
+        if (hasNotAllDefaultSeparator(input) || hasNotOneDefaultSeparator(input)) {
+            return hasSpecialCharacters(input);
         }
         return false;
     }
 
-    private static boolean containCustomSeparator(String input) {
-        return input.contains("//") && input.contains("\\n");
+    private static boolean hasNotAllDefaultSeparator(String input) {
+        return !input.contains(",") && !input.contains(":");
     }
 
-    private static boolean notContainSpecialCharacters(String input) {
-        return !input.matches("[" + NOT_SPECIAL_CHARACTERS + " ]+");
+    private static boolean hasNotOneDefaultSeparator(String input) {
+        return !input.contains(",") || !input.contains(":");
+    }
+
+    private static boolean hasSpecialCharacters(String input) {
+        return input.matches("[" + SPECIAL_CHARACTERS + " ]+");
+    }
+
+    private static boolean hasNotCustomSeparator(String input) {
+        return !(input.contains("//") && input.contains("\\n"));
     }
 
     private static boolean isNotCustomPattern(String input) {
@@ -53,7 +52,7 @@ public class SeparatorValidation {
         // 커스텀 구분자는 뒤에 | 이 붙으므로 제거해준다.
         customPattern = customPattern.replaceAll("\\|", "");
         // 커스텀 구분자와 특수문자 아닌 문자들을 정규표현식으로 묶어준다.
-        String pattern = "[" + NOT_SPECIAL_CHARACTERS + customPattern + " ]+";
+        String pattern = "[" + SPECIAL_CHARACTERS + customPattern + " ]+";
         // 커스텀 구분자를 제외 시켜준다.
         input = input.substring(input.indexOf("\\n") + 2);
         // 문자열 안에 커스텀 구분자와 기본 구분자 외의 특수문자가 있는지 검사한다.
