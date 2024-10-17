d819ad3d1084d458f5b1ec76a11d0049a31789e6
SongJaeHoonn <thdwogns121@naver.com>
2024-10-17 17:21:25 +0900

    feat: 문자열 입력받기 기능 구현

--- src/main/java/input/InputHandler.java
+++ src/main/java/input/InputHandler.java
@@ -1,4 +1,11 @@
 package input;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputHandler {
+
+    public String getInput() {
+        System.out.println("문자열을 입력해주세요");
+        return Console.readLine();
+    }
 }
