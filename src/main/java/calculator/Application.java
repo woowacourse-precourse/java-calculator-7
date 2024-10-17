984766d5d4fde4a1ba9e0208dac1834b8446b45d
onesay17 <onesay17@gmail.com>
2024-10-17 10:21:14 +0900

    feat: 애플리케이션 실행을 위한 메인 클래스 수정
    Application 클래스에서 CalculatorController, Calculator, DelimiterParser, InputValidator, CalculatorView 객체를 초기화하고, 프로그램 실행 흐름을 개발했습니다.
    
    Related to #6

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.model.Calculator;
+import calculator.model.DelimiterParser;
+import calculator.model.InputValidator;
+import calculator.view.CalculatorView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //TODO 의존관계 주입 더 나은 방법있는지 리팩터링 확인
+        InputValidator inputValidator = new InputValidator();
+        DelimiterParser delimiterParser = new DelimiterParser(inputValidator);
+        Calculator calculator = new Calculator(inputValidator, delimiterParser);
+        CalculatorView calculatorView = new CalculatorView();
+
+        CalculatorController calculatorController = new CalculatorController(calculator, calculatorView);
+        calculatorController.run();
+
     }
 }
