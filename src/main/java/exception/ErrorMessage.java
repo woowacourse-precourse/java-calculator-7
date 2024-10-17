1fd7d6ec5f3355007ebef3a21918964c4b00d943
aaahyunseo <hs030608@naver.com>
2024-10-17 22:59:25 +0900

    feat(ErrorMessage): 에러 메세지 추가

---
+++ src/main/java/exception/ErrorMessage.java
@@ -0,0 +1,16 @@
+package exception;
+
+public enum ErrorMessage {
+    LENGTH("입력된 숫자의 길이가 허용 범위를 초과하였습니다."),
+    INVALID_INPUT("숫자 외의 값은 입력할 수 없습니다."),
+    INVALID_NUMBER("0과 음수는 입력할 수 없습니다.");
+
+    private final String errorMessage;
+
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+    public String getErrorMessage(){
+        return this.errorMessage;
+    }
+}
