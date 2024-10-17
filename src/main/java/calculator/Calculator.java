def595557363545317083afd70d2cc1770ea8007
Jungwoo Chu <cjw980930@gmail.com>
2024-10-17 11:18:45 +0900

    feat(//확인): 문자열 앞부분 //인경우 커스텀 구분자 메소드 실행

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,10 +2,22 @@ package calculator;
 
 public class Calculator {
     public int add(String input) {
+
         //공백인 경우 0을 반환함
         if (input.isEmpty()) {
             return 0;
         }
+
+        if (input.startsWith("//")) {
+            String[] customResult = customDelimeter(input);
+            String newDelimiter = customResult[0];
+            input = customResult[1];
+        }
+
+
+    }
+
+    public String[] customDelimeter(String input) {
 
     }
 }
