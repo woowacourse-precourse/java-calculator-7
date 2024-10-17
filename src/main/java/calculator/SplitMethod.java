28e1ef8acada006e7f88b4f6682a18e1787d1ce2
Etwashoeren <sungmin0609@nate.com>
2024-10-18 03:21:12 +0900

    refactor SplitMethod 예외처리 수정

--- src/main/java/calculator/SplitMethod.java
+++ src/main/java/calculator/SplitMethod.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class SplitMethod {
     public int split(String input) {
 
@@ -7,15 +9,24 @@ public class SplitMethod {
 
         if (input.startsWith("//")) { // 커스텀 구분자
 
-            int endOfCustom = input.indexOf("\\");   // '\n'의 시작 위치
+            int endOfCustom = input.indexOf("\\n");   // '\n'의 시작 위치
+
+            if(endOfCustom == -1) {
+                throw new IllegalArgumentException("잘못된 입력겂입니다.");
+            }
 
             String customDelimiter = input.substring(2, endOfCustom); // 커스텀 구분자: '//'과 '\n'의 사이
 
+            String c = Pattern.quote(customDelimiter);
+
             String seperated = input.substring(endOfCustom+2); // '\n' 뒤에 위치하는 문자열
 
-            String[] tokens = seperated.split(customDelimiter);
+            String[] tokens = seperated.split(c);
 
             for (String token : tokens) {
+                if (Integer.parseInt(token) < 0) {
+                    throw new IllegalArgumentException("잘못된 입력값 입니다.");
+                }
                 sum += Integer.parseInt(token);
             }
         }
@@ -26,7 +37,7 @@ public class SplitMethod {
 
                 for (String token : tokens) {
                     if(Integer.parseInt(token) < 0){
-                        throw new IllegalArgumentException();
+                        throw new IllegalArgumentException("잘못된 입력값입니다.");
                     }
                     sum += Integer.parseInt(token);
                 }
