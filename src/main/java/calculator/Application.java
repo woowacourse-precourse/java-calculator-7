14f36b1601e7f2ed86ad31cc8e1224d3601efa0e
RAJUYEOB <pag1yun99@gmail.com>
2024-10-17 12:36:13 +0900

    feat: 사용자 입력 데이터 기준, 기본 구분자(, :)로 처리 후 덧셈 결과 출력 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,29 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> intNumber = new ArrayList<>();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        String cuttingMark = ",|:";
+
+        String[] numbers = input.split(cuttingMark);
+
+        for (String number : numbers) {
+            intNumber.add(Integer.parseInt(number));
+        }
+
+        int sum = 0;
+        for (int i = 0; i < intNumber.size(); i++) {
+            sum += intNumber.get(i);
+        }
+
+        System.out.println("결과 : " + sum);
     }
 }
