3a6d07007fefa65b35e89588d3f986e0348e925c
박근채 <pgc0419@gmail.com>
2024-10-17 17:00:53 +0900

    덧셈할 문자열을 입력해주세요. 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.print("덧셈할 문자열을 입력해주세요." + "\n");
     }
 }
+
