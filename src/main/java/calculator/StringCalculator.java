f20cec49ad04b64932422b5bc3c51fa57d416332
taehyun <xogus5070318@gmail.com>
2024-10-18 02:27:38 +0900

    feat: 문자열 입력받기 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 // 문자열 계산기 클래스
 public class StringCalculator {
 
@@ -8,7 +10,9 @@ public class StringCalculator {
 
     //사용자로부터 문자열을 입력받음
     public void input(){
-        //Todo: 문자열 입력받기
+
+        //문자열 입력받기
+        readString();
         //Todo: 잘못된 문자열을 입력받았을 경우, 예외처리
         //Todo: 커스텀 구분자가 있는 경우, 저장
     }
@@ -17,6 +21,13 @@ public class StringCalculator {
     public void outputAdd(){
         //Todo: Calculations 클래스에서 add 함수 가져오기
         //Todo: 결과 출력하기
+    }
+
+
+    //문자열 입력받기
+    private void readString(){
+        //문자열을 입력받음
+        inputString = readLine();
     }
 
 }
