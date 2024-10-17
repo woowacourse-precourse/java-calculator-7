15af977334bf0d0cb33066c7f86845be941b7fb0
rladmstn <oz33177351@gmail.com>
2024-10-17 18:10:06 +0900

    test(Parser) : 커스텀 구분자 존재 유무 판단 메서드 테스트 작성
    input 입력 시, 커스텀 구분자 존재 유무를 판단하는 메서드 테스트 작성

---
+++ src/test/java/calculator/domain/ParserTest.java
@@ -0,0 +1,29 @@
+package calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ParserTest {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,2:3 . false",
+            ",1:2 . false",
+            "//a\\n1a2a3 . true",
+            "/=\\n1=2=3 . false",
+            "// \\n1 2 3 . true",
+            " . false",
+            "//a\\3a2a1 . false",
+            "//1\\n112131 . false"},
+            delimiter = '.'
+    )
+    @DisplayName("커스텀 구분자 존재 유무 판단 테스트")
+    void hasCustomDelimiter(String delimiter, boolean expected){
+        boolean result = Parser.hasCustomDelimiter(delimiter);
+        assertThat(result).isEqualTo(expected);
+    }
+}
