02da92d02fd0782c9cc701845164e7f0caef8f0a
kangrae <kangrae0501@gmail.com>
2024-10-18 02:22:42 +0900

    feat: change delimiters type
    구분자를 관리하던 3개의 char 변수를 하나의 String으로 관리
    
    tokenize할때도 String형식이 필요하는 등,
    구분자들이 char 형식으로 각각 쓰이는 경우 없었던 것이 이유임

--- src/main/java/calculator/model/DelimiterHandler.java
+++ src/main/java/calculator/model/DelimiterHandler.java
@@ -2,21 +2,12 @@ package calculator.model;
 
 public class DelimiterHandler {
 
-    // final?? 써야한다는데 공부 필요 + static
+    // final 더 공부
     private String customStart ="//";
     private String customEnd ="\\n";
 
-    // 왠지 String으로 관리해도 좋을듯
-    private char delimiter1=',';
-    private char delimiter2=':';
+    private String delimiters=",:";
 
-    // custom 구분자 문제 해결 필요
-    // 디폴트구분자1과 2 랑 같은 값으로 지정해놓고 조건 만족하면 변경
-
-    // 실제로 사용자가 의도한 값이 ','이면 문제가 발생함
-    // 새로운 후보 : 0 (NULL 값)
-    // 또는 flag를 만든다? 이게 나은듯
-    private char customDelimiter = 0;
     private boolean customDelimiterFlag = false;
 
     private boolean checkDelimiter(String expression) {
@@ -26,20 +17,15 @@ public class DelimiterHandler {
         return false;
     }
 
-    public char getDelimiter(String expression) {
+    public void setDelimiter(String expression) {
             if (checkDelimiter(expression)) {
-                customDelimiter = expression.charAt(2);
+                delimiters += expression.charAt(2);
                 customDelimiterFlag = true;
             }
-        return customDelimiter;
     }
 
-    public char[] getAllDelimiters() {
-        if (customDelimiterFlag) {
-            return new char[]{delimiter1, delimiter2, customDelimiter}; // 직접 반환
-        } else {
-            return new char[]{delimiter1, delimiter2}; // 직접 반환
-        }
+    public String getAllDelimiters() {
+        return delimiters;
     }
 
     public boolean getCustomDelimiterFlag() {
