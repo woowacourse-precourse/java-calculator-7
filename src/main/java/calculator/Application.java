5ed841fa1b95e040972ed2e23d2c9ff1471cf35b
SongMinhyuk <dnfldpden32@gmail.com>
2024-10-17 12:51:22 +0900

    FEAT(Application.java): main함수 출력문 추가
    문제 요구사항에 있는 출력문 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         Calculator calculator = new Calculator(input);
         int result = calculator.add();
