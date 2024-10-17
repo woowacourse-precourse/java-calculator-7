fc31f00255cfb26fe0b9c6ea3d9103a4e0224597
onesay17 <onesay17@gmail.com>
2024-10-17 10:24:31 +0900

    feat: CalculatorView 클래스 수정
    사용자로부터 입력을 받고, 계산 결과를 출력하는 기능을 추가했습니다. 또한, 에러 발생 시 에러 메시지를 출력하는 기능도 포함됩니다.
    
    Closes #7

---
+++ src/main/java/calculator/view/CalculatorView.java
@@ -0,0 +1,19 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CalculatorView {
+
+    public String getInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine().replace("\\n", "\n");
+    }
+
+    public void showResult(int result) {
+        System.out.println("결과 : " + result);
+    }
+
+    public void showError(String message) {
+        System.out.println("에러 = " + message);
+    }
+}
