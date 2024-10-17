ebce124139ac5bf3a660265927a59704998e6c52
Hacanna42 <dtmdgus901@gmail.com>
2024-10-18 03:01:51 +0900

    refactor(constants): add DelimiterConstants class

--- src/main/java/calculator/constants/DelimiterConstants.java
+++ src/main/java/calculator/constants/DelimiterConstants.java
@@ -1,2 +1,8 @@
-package calculator.constants;public class DelimiterConstants {
+package calculator.constants;
+
+public class DelimiterConstants {
+    public static final char DELIMITER_COMMA = ',';
+    public static final char DELIMITER_COLON = ':';
+    public static final String CUSTOM_DELIMITER_START = "//";
+    public static final String CUSTOM_DELIMITER_END = "\\n";
 }
