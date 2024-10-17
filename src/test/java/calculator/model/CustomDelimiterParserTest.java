1fec6b645eff704d5d899ae9c81660ff82b7047c
hwangdaesun <hds650588@gmail.com>
2024-10-17 17:15:54 +0900

    test(CustomDelimiterParser.class) : orElseThrow 메서드로 parse 결과 처리

--- src/test/java/calculator/model/CustomDelimiterParserTest.java
+++ src/test/java/calculator/model/CustomDelimiterParserTest.java
@@ -9,7 +9,7 @@ class CustomDelimiterParserTest {
     void parse_1() {
         String input = "//;\\n1;2;3";
         CustomDelimiterParser customDelimiterParser = CustomDelimiterParser.getInstance();
-        String delimiter = customDelimiterParser.parse(input);
+        String delimiter = customDelimiterParser.parse(input).orElseThrow(NumberFormatException::new);
         Assertions.assertEquals(";", delimiter);
     }
 
@@ -17,7 +17,7 @@ class CustomDelimiterParserTest {
     void parse_2() {
         String input = "//;/\\n1;2;3";
         CustomDelimiterParser customDelimiterParser = CustomDelimiterParser.getInstance();
-        String delimiter = customDelimiterParser.parse(input);
+        String delimiter = customDelimiterParser.parse(input).orElseThrow(NumberFormatException::new);
         Assertions.assertEquals(";/", delimiter);
     }
 
@@ -25,7 +25,7 @@ class CustomDelimiterParserTest {
     void parse_3() {
         String input = "//;/\\\\n1;2;3";
         CustomDelimiterParser customDelimiterParser = CustomDelimiterParser.getInstance();
-        String delimiter = customDelimiterParser.parse(input);
+        String delimiter = customDelimiterParser.parse(input).orElseThrow(NumberFormatException::new);
         Assertions.assertEquals(";/\\", delimiter);
     }
 }
