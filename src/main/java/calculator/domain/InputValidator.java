08f92ec29aea80ce2f4d0e7cd0d4d26bfd6875fb
KangGeonyoung <kky6335@gmail.com>
2024-10-17 22:12:51 +0900

    feat(InputValidator): 커스텀 구분자를 추출하는 기능 InputValidator - getCustomSeparator: 문자열에서 커스텀 구분자 리스트를 반환하는 메서드

--- src/main/java/calculator/domain/InputValidator.java
+++ src/main/java/calculator/domain/InputValidator.java
@@ -1,11 +1,52 @@
 package calculator.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class InputValidator {
 
+    private static final String SEPARATOR_START = "//";
+    private static final String SEPARATOR_END = "\\n";
+
+    /**
+     * 커스텀 구분자가 존재하는지 확인
+     */
     public static boolean isExistCustomSeparator(String input) {
-        if (input.contains("//") && input.contains("\\n")) {
+        if (input.contains(SEPARATOR_START) && input.contains(SEPARATOR_END)) {
             return true;
         }
         return false;
+    }
+
+    /**
+     * 문자열에서 커스텀 구분자 리스트를 반환
+     * - 커스텀 구분자가 여러 개 존재할 수 있기 때문
+     */
+    public static List<String> getCustomSeparator(String input) {
+        boolean isSeparator = false;
+        List<String> separators = new ArrayList<>();
+
+        // 구분자 없이 "//\n"만 존재하는 경우
+        if (input.length() == 4) {
+            return separators;
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            // 커스텀 구분자 표시인 "//"일 경우
+            if (input.charAt(i) == SEPARATOR_START.charAt(0) && input.charAt(i+1) == SEPARATOR_START.charAt(1)) {
+                isSeparator = true;
+                i += 1;
+                continue;
+            }
+
+            if (isSeparator) {
+                separators.add(String.valueOf(input.charAt(i)));
+                if (input.charAt(i+1) == SEPARATOR_END.charAt(0) && input.charAt(i+2) == SEPARATOR_END.charAt(1)) {
+                    break;
+                }
+            }
+        }
+        return separators;
     }
 }
