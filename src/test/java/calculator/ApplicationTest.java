aa654dfb706975a49a9da8e64ab5761ba724d369
동영 김 <kimarang0@naver.com>
2024-10-17 22:50:34 +0900

    test(ApplicationTest): 테스트 추가 - 추후 테스트 더 추가할 예정

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,19 +8,84 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+    // 커스텀 구분자 사용
     @Test
-    void 커스텀_구분자_사용() {
+    void use_custom_divider() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("//;\\n1;2;4");
+            assertThat(output()).contains("결과 : 7");
         });
     }
 
+    // 한 숫자만 입력했을 때
     @Test
-    void 예외_테스트() {
+    void input_one_number() {
+        assertSimpleTest(() -> {
+            run("5");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+    // 여러 숫자를 입력했을 때
+    @Test
+    void input_many_numbers() {
+        assertSimpleTest(() -> {
+            run("5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4:5,4");
+            assertThat(output()).contains("결과 : 180");
+        });
+    }
+
+    // [오류] 구분자가 연속되어 입력되었을 때
+    @Test
+    void Error_input_continuous_dividers() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("1,,3::5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    // [오류] 구분자 사이에 공백만 있을 때
+    @Test
+    void Error_blank_between_divider() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1, :5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    // [오류] 음수가 입력되었을 때
+    @Test
+    void Error_negative_number() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    // [오류] 커스텀 구분자를 잘못 사용 - "\n" 미사용
+    @Test
+    void Error_wrong_custom_divider_1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    // [오류] 커스텀 구분자를 잘못 사용 - "//" 미사용
+    @Test
+    void Error_wrong_custom_divider_2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("/;\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    // [오류] 커스텀 구분자를 잘못 사용 - 커스텀 구분자가 없음
+    @Test
+    void Error_wrong_custom_divider_3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
