3faaa0ec8a80ecbfc3409e709f362e3f78435a40
ChaneHaDa <chane.ha.da@gmail.com>
2024-10-17 23:34:33 +0900

    feat: split input string based on basic delimiter

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        input1 = scanner.next();
+
+        String[] tokens = input1.split("[,:]");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (String token : tokens) {
+            try {
+                numbers.add(Integer.parseInt(token));
+            } catch (NumberFormatException e) {
+                System.out.println("숫자X " + token);
+            }
+        }
+
+        for (int i : numbers) {
+            System.out.println(i);
+        }
     }
 }
