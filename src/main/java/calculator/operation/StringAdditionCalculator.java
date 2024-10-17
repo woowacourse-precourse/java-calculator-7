278dd4a29c6f2ab377dad9d8de97f47cf5fc5b73
jenna <shinysuh1992@gmail.com>
2024-10-17 16:13:32 +0900

    feat: 커스텀 구분자 추출 기능 구현
    Closes #2

--- src/main/java/calculator/operation/StringAdditionCalculator.java
+++ src/main/java/calculator/operation/StringAdditionCalculator.java
@@ -1,6 +1,13 @@
 package calculator.operation;
 
+import java.util.List;
+
 public class StringAdditionCalculator {
+    private final String DELIMITER_START = "//";
+    private final String DELIMITER_END = "\\n";
+
+    private List<String> delimiters;
+
     private String numberString;
 
     public StringAdditionCalculator(String numberString) {
@@ -9,5 +16,27 @@ public class StringAdditionCalculator {
 
     public int calculateSum() {
         return 0;
+    }
+
+    /**
+     * 커스텀 구분자 추출
+     */
+    private void extractCustomDelimiter() {
+        if (!numberString.startsWith(DELIMITER_START)) {
+            return;
+        }
+
+        int startIdx = numberString.indexOf(DELIMITER_START);
+        int endIdx = numberString.indexOf(DELIMITER_END);
+
+        // 커스텀 구분자 추출
+        String customDelimiter = numberString.substring(startIdx + DELIMITER_START.length(), endIdx);
+        numberString = numberString.substring(endIdx + DELIMITER_END.length());
+
+        // 구분자 리스트에 추가
+        delimiters.add(customDelimiter);
+
+        // 추출되지 않은 커스텀 구분자가 남아있으면 추출 (recursive)
+        this.extractCustomDelimiter();
     }
 }
