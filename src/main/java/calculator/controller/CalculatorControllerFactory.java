ad625c8524b55bd335688306afcefa9e432e4a40
EHCTH <lees90896@gmail.com>
2024-10-17 18:09:13 +0900

    feat(CalculatorControllerFactory): 컨트롤러 팩토리 구현

--- src/main/java/calculator/controller/CalculatorControllerFactory.java
+++ src/main/java/calculator/controller/CalculatorControllerFactory.java
@@ -1,4 +1,28 @@
 package calculator.controller;
 
-public class CaculatorFactory {
+import calculator.number.service.PositiveNumberGenerator;
+import calculator.service.generator.CreateProcess;
+import calculator.service.generator.ExtractProcess;
+import calculator.service.calculator.CalculatorProcess;
+import calculator.separator.service.SeparatorGenerator;
+import calculator.view.input.handler.InputHandlerService;
+import calculator.view.input.handler.NumberInputHandler;
+import calculator.view.input.service.InputView;
+import calculator.view.output.OutputProcess;
+import calculator.view.output.ResultService;
+
+public class CalculatorControllerFactory {
+    public static CalculatorController create() {
+        CalculatorProcess calculatorProcess = new CalculatorProcess();
+        InputHandlerService inputHandlerService = new NumberInputHandler(new InputView(new OutputProcess()));
+        ResultService resultService = new OutputProcess();
+        SeparatorGenerator separatorGenerator = new SeparatorGenerator(new ExtractProcess(), new CreateProcess());
+        PositiveNumberGenerator numberGenerator = new PositiveNumberGenerator(new ExtractProcess(), new CreateProcess());
+        return new CalculatorController(
+                separatorGenerator,
+                numberGenerator,
+                calculatorProcess,
+                inputHandlerService,
+                resultService);
+    }
 }
