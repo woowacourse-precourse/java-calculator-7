448571e5cef5eeee66a7705971f2f857bc603965
garden14 <ci667@dgu.ac.kr>
2024-10-18 00:18:07 +0900

    feat: 예외 처리 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,15 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class Calculator {
     private static final String ENTER_MESSAGE = "덧셈할 문자열을 입력해주세요.";
 
-    public static void start() {
+    public static int start() {
         System.out.println(ENTER_MESSAGE);
         String userInput = Console.readLine();
 
         if (userInput == null || userInput.isEmpty()) {
-
+            return 0;
         }
         String[] tokens = Tokenizer.tokenization(userInput);
-
+        return add(Tokenizer.convertToNumber(tokens));
     }
 
     public static int add(int[] numbers) {
