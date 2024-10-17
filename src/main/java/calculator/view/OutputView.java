a1817e9b2fb409dd11d2d5ea60c93bc49d691716
최영우 <allnstar@naver.com>
2024-10-17 20:08:55 +0900

    feat: 입력 안내메세지를 출력하는 기능 구현

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+    private static final String INITIAL_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public static void printInitialMessage(){
+        System.out.println(INITIAL_MESSAGE);
+    }
+}
