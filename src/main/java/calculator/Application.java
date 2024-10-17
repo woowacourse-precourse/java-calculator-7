817002511826d3628cbb644898167214e736c6ea
Nahye Choi <skgp0628@naver.com>
2024-10-18 08:48:56 +0900

    feat: 문자열 입력 받기 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,34 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void gitmain(String[] args) {
         // TODO: 프로그램 구현
+        final String DELIMITER_1 = ",";
+        final String DELIMITER_2 = ":";
+
+        int[] splitNumbers = new int[]{};
+
+        // 문자열 입력
+        // - 안내문 출력
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        // - 입력 받기
+        String numbers = Console.readLine();
+        System.out.println(numbers);
+        // - 예외처리
+
+        // 숫자 추출
+        // - 구분자
+        
+        // - 커스텀 구분자
+
+        // 문자 -> 숫자 형변환
+
+        // 계산 기능 구현
+
+        // 결과 출력
     }
 }
