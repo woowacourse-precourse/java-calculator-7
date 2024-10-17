38cf3d9ecc7771279a764174b84e6fa43ae480ec
Dompoo <dlckdrms0517@gmail.com>
2024-10-17 23:03:34 +0900

    feat: InputParser 구현
    - 문자열을 받아서 커스텀구분자가 있는 부분과 숫자가 있는 부분을 분리
    - 커스텀구분자가 없는 경우에는 빈 문자열을 반환
    - 분리된 문자열들은 dto를 통해 전달

---
+++ src/main/java/calculator/io/InputParseResult.java
@@ -0,0 +1,7 @@
+package calculator.io;
+
+public record InputParseResult(
+		String customSeparatorPart,
+		String numberPart
+) {
+}
---
+++ src/test/java/calculator/io/InputParserTest.java
@@ -0,0 +1,99 @@
+package calculator.io;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class InputParserTest {
+	
+	@Test
+	void 커스텀구분자가_없는_입력을_잘_분리한다() {
+		//given
+		InputParser sut = new InputParser();
+		
+		//when
+		InputParseResult result = sut.parse("1:2:3^4;5");
+		
+		//then
+		Assertions.assertThat(result.customSeparatorPart()).isEqualTo("");
+		Assertions.assertThat(result.numberPart()).isEqualTo("1:2:3^4;5");
+	}
+	
+	@Test
+	void 커스텀구분자가_없는_두자리_이상의_숫자_입력을_잘_분리한다() {
+		//given
+		InputParser sut = new InputParser();
+		
+		//when
+		InputParseResult result = sut.parse("1:12:123^1234;12345");
+		
+		//then
+		Assertions.assertThat(result.customSeparatorPart()).isEqualTo("");
+		Assertions.assertThat(result.numberPart()).isEqualTo("1:12:123^1234;12345");
+	}
+	
+	@Test
+	void 커스텀구분자가_있는_입력을_잘_분리한다() {
+		//given
+		InputParser sut = new InputParser();
+		
+		//when
+		InputParseResult result = sut.parse("//^\\n1:2:3^4;5");
+		
+		//then
+		Assertions.assertThat(result.customSeparatorPart()).isEqualTo("^");
+		Assertions.assertThat(result.numberPart()).isEqualTo("1:2:3^4;5");
+	}
+	
+	@Test
+	void 여러개의_커스텀구분자가_있는_입력을_잘_분리한다() {
+		//given
+		InputParser sut = new InputParser();
+		
+		//when
+		InputParseResult result = sut.parse("//^&*\\n1:2:3^4;5");
+		
+		//then
+		Assertions.assertThat(result.customSeparatorPart()).isEqualTo("^&*");
+		Assertions.assertThat(result.numberPart()).isEqualTo("1:2:3^4;5");
+	}
+	
+	@Test
+	void 역슬래쉬_커스텀구분자가_있는_입력을_잘_분리한다() {
+		//given
+		InputParser sut = new InputParser();
+		
+		//when
+		InputParseResult result = sut.parse("//^&*\\\\n1:2:3^4;5");
+		
+		//then
+		Assertions.assertThat(result.customSeparatorPart()).isEqualTo("^&*\\");
+		Assertions.assertThat(result.numberPart()).isEqualTo("1:2:3^4;5");
+	}
+	
+	@Test
+	void 커스텀구분자가_있고_숫자가_두자리_이상인_입력을_잘_분리한다() {
+		//given
+		InputParser sut = new InputParser();
+		
+		//when
+		InputParseResult result = sut.parse("//^\\n1:12:123^4;5");
+		
+		//then
+		Assertions.assertThat(result.customSeparatorPart()).isEqualTo("^");
+		Assertions.assertThat(result.numberPart()).isEqualTo("1:12:123^4;5");
+	}
+	
+	@Test
+	void 여러개의_커스텀구분자가_있고_숫자가_여러개인_입력을_잘_분리한다() {
+		//given
+		InputParser sut = new InputParser();
+		
+		//when
+		InputParseResult result = sut.parse("//^&*\\n1:12:123^4;5");
+		
+		//then
+		Assertions.assertThat(result.customSeparatorPart()).isEqualTo("^&*");
+		Assertions.assertThat(result.numberPart()).isEqualTo("1:12:123^4;5");
+	}
+	
+}
