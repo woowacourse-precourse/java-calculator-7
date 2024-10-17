684e2a24ace47cbc3ed2c775d376b8604c18e822
dabeann <dabin4078@daum.net>
2024-10-17 12:44:12 +0900

    ✨  Feature : 기본 구분자(쉼표, 콜론)로 숫자 더하기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String input = Console.readLine();
+        int result = calculate(input);
+
+        System.out.println("결과 : " + result);
+    }
+
+    private static int calculate(String input) {
+        String[] tokens = input.split(",|:");
+        int sum = 0;
+
+        for (String token : tokens) {
+            int number = Integer.parseInt(token);
+            sum += number;
+        }
+
+        return sum;
     }
 }
