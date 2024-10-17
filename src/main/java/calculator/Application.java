b2b97464409ba41762442a87376c1ceb6fc06e73
OhYeHyun <ohyhohyl@naver.com>
2024-10-17 22:21:05 +0900

    feat: main 함수와 커스텀 구분자 처리하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,21 +1,58 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
-    public static String[] separators = {",", ":"};
+    public static List<String> separators = new ArrayList<>(Arrays.asList(",", ":"));
 
     public static void main(String[] args) {
+        String input = readLine();
+        if (isCustomSeparator(input)) {
+            try {
+                input = extractCustomSeparator(input, separators);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+        }
+        int[] numbers = extractNumbers(input, separators);
+        int sumValue = sum(numbers);
 
+        System.out.println(sumValue);
     }
 
     public static int sum(int[] numbers) {
         return Arrays.stream(numbers).sum();
     }
 
-    public static int[] extractNumbers(String input, String[] separators) {
+    public static int[] extractNumbers(String input, List<String> separators) {
         String separatorsRegex = String.join("|", separators);
-
         return Arrays.stream(input.split(separatorsRegex)).mapToInt(Integer::parseInt).toArray();
+    }
+
+    public static boolean isCustomSeparator(String input) {
+        return input.startsWith("//");
+    }
+
+    public static String extractCustomSeparator(String input, List<String> separators) {
+        Matcher matcher = Pattern.compile("//(.*?)\\\\n(.*)").matcher(input);
+
+        if (matcher.find()) {
+            String customSeparator = matcher.group(1);  // 커스텀 구분자
+            String extractNumber = matcher.group(2);  // 나머지 문자열
+
+            separators.add(customSeparator);
+
+            return extractNumber;
+        } else {
+            throw new IllegalArgumentException("커스텀 구분자를 찾을 수 없습니다.");
+        }
     }
 }
