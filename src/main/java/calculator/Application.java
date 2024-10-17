cdc1408cb3018694cb78239a8eb1bb9b4b2b50de
sonsuchang <son336208@naver.com>
2024-10-17 16:43:58 +0900

    feat : 문자열 입력 받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputString = camp.nextstep.edu.missionutils.Console.readLine();
     }
 }
