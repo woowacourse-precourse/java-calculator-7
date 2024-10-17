6d53efa51dbf6e8bdef2bae14aee500a09d47b96
rladmstn <oz33177351@gmail.com>
2024-10-17 20:49:59 +0900

    feat(constants) : 커스텀 구분자를 판단하기 위한 DelimiterConstants 추가
    커스텀 구분자 판단을 위한 접두사, 접미사를 constants로 추가
    CUSTOM_DELIMITER_PREFIX = "//";
    CUSTOM_DELIMITER_SUFFIX = "\n";

---
+++ src/main/java/calculator/constants/DelimiterConstants.java
@@ -0,0 +1,11 @@
+package calculator.constants;
+
+public class DelimiterConstants {
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+
+    private DelimiterConstants() {
+        throw new RuntimeException("Constants class cannot be instantiated");
+    }
+
+}
