88ab2de839bc1bc48759092e292cb6c9c769bebb
wingyou <winwing.you@gmail.com>
2024-10-17 20:26:08 +0900

    feat(Parser): handling invalid input in parse()

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -7,18 +7,27 @@ public class Parser {
 
     public int[] parse(String input) {
         String[] segments;
+        int[] numbers;
+        System.out.println(input);
 
-        if (input.startsWith("//")) {
-            char delimiter = input.charAt(2);
-            segments = input.split("\n")[1].split(String.valueOf(delimiter));
-        } else {
-            segments = input.split("[,|:]");
+        try {
+            if (input.startsWith("//")) {
+                char delimiter = input.charAt(2);
+                segments = input.split("\n")[1].split(String.valueOf(delimiter));
+            } else {
+                segments = input.split("[,|:]");
+            }
+
+            numbers = new int[segments.length];
+            for (int i = 0; i < segments.length; i++) {
+                System.out.println("    " + segments[i]);
+                numbers[i] = Integer.parseInt(segments[i]);
+                System.out.println("    -> " + numbers[i]);
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
         }
 
-        int[] numbers = new int[segments.length];
-        for (int i = 0; i < segments.length; i++) {
-            numbers[i] = Integer.parseInt(segments[i]);
-        }
         return numbers;
     }
 }
