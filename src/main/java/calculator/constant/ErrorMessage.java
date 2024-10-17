9cc4e4d093654fed6518e8e9a1f982e37a4ef43d
chyun <och2510@gmail.com>
2024-10-18 01:24:23 +0900

    feat(Delimiter): 구분자는 숫자가 될 수 없다

--- src/main/java/calculator/constant/ErrorMessage.java
+++ src/main/java/calculator/constant/ErrorMessage.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public enum ErrorMessage {
 	NUMBERS_POSITIVE("양수 값만 계산할 수 있습니다."),
-	DELIMITER_LENGTH("구분자의 길이는 1 이어야 합니다.");
+	DELIMITER_LENGTH("구분자의 길이는 1 이어야 합니다."),
+	DELIMITER_NOT_NUMBER("구분자는 숫자가 될 수 없습니다.");
 
 	private static final String SPACE = " ";
 	private static final String PREFIX = "[ERROR] ";
--- src/main/java/calculator/model/Delimiter.java
+++ src/main/java/calculator/model/Delimiter.java
@@ -1,24 +1,35 @@
 package calculator.model;
 
+import java.util.regex.Pattern;
+
 import calculator.constant.ErrorMessage;
 
 public class Delimiter {
 	private static final int STRICT_LENGTH = 1;
+	private static final Pattern SINGLE_NUMERIC_PATTERN = Pattern.compile("\\d");
 
 	private final String delimiter;
 
 	private Delimiter(String delimiter) {
 		validateLength(delimiter);
+		validateNotNumber(delimiter);
+
 		this.delimiter = delimiter;
 	}
 
 	public static Delimiter from(String delimiter) {
-		return new Delimiter(delimiter);
+		return new Delimiter(delimiter.trim());
 	}
 
 	private void validateLength(String delimiter) {
 		if (delimiter.isBlank() || delimiter.length() > STRICT_LENGTH) {
 			throw new IllegalArgumentException(ErrorMessage.DELIMITER_LENGTH.getMessage());
+		}
+	}
+
+	private void validateNotNumber(String delimiter) {
+		if (SINGLE_NUMERIC_PATTERN.matcher(delimiter).matches()) {
+			throw new IllegalArgumentException(ErrorMessage.DELIMITER_NOT_NUMBER.getMessage());
 		}
 	}
 }
