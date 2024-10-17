d5c8088749edea12e1b01946b806cff2276784a4
woojin <zaza0804@uos.ac.kr>
2024-10-17 16:29:55 +0900

    feat($calculator): inject StringChecker in Calculator class
    - 구현한 문자열 검증 클래스를 계산기에 주입하고 사용합니다.

--- src/main/java/calculator/app/Calculator.java
+++ src/main/java/calculator/app/Calculator.java
@@ -1,9 +1,23 @@
 package calculator.app;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class Calculator {
+    private static StringChecker stringChecker = new StringChecker();
+
     public void start() {
         String input = Console.readLine();
+        List<Character> separatorList = stringChecker.checkCustomSeparator(input);
+        List<Integer> integerList = List.of();
+        try {
+            if (separatorList.size() == 3) {
+                integerList = stringChecker.checkExpressionValidation(input.substring(5), separatorList);
+            } else if (separatorList.size() == 2) {
+                integerList = stringChecker.checkExpressionValidation(input, separatorList);
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
