603c5c7177cb003607812f3a8f4666afb9777186
woodongle <315ehdgur@gmail.com>
2024-10-17 14:47:42 +0900

    feat: 입력 검증(숫자 형식, 0보다 큰지), 출력 검증(비어있을 때 0출력)
    1. 사용자가 입력한 문자열이 숫자 형식인지, 0보다 큰지 확인합니다.
    2. 잘못된 값을 입력했다면 IllegalArgumentException을 발생시키고 "잘못된 값을 입력하였습니다." 라는 에러메시지 출력 후 애플리케이션이 종료됩니다.
    3. 아무것도 입력하지 않으면 결과는 0으로 출력하고 애플리케이션이 종료됩니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,35 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        // 덧셈할 문자열 입력
+        String input = Console.readLine();
+
+        // 아무것도 입력하지 않으면 결과는 0으로 출력
+        if (input.isEmpty()) {
+            System.out.println("결과: 0");
+            return;
+        }
+
+        try {
+            validateInput(input);
+            System.out.println("유효한 입력입니다: " + input);
+        } catch (IllegalArgumentException e) {
+            // 에러 메시지 출력
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    // 사용자 입력 검증 함수
+    public static void validateInput(String input) {
+        // 0보다 큰지 확인 그리고 숫자 형식인지 확인
+        if (!input.matches("^[1-9]\\d+$")) {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+        }
     }
 }
