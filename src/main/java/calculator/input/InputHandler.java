c4f91d53a69f3de81f5a2d23a35319a72741149f
Hacanna42 <dtmdgus901@gmail.com>
2024-10-18 00:53:20 +0900

    feat: implement InputHandler

--- src/main/java/calculator/input/InputHandler.java
+++ src/main/java/calculator/input/InputHandler.java
@@ -1,4 +1,18 @@
 package calculator.input;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputHandler {
+    private final Validator validator;
+
+    // 외부에서 Validator 를 주입받음. (의존성 주입)
+    public InputHandler(Validator validator) {
+        this.validator = validator;
+    }
+
+    public String getInput() {
+        String input = Console.readLine();
+        validator.check(input); // Validator 에서 입력 값 검증 후 반환
+        return input;
+    }
 }
