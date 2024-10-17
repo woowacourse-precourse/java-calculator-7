b71f243e4f4b95ce09ac74ebcadc95b94bb4a2e1
Chaeyoung714 <202102760@hufs.ac.kr>
2024-10-17 18:11:00 +0900

    feat : 1.1 사용자 값 입력 기능 구현
    controller/CalculatorController : InputView, OutputView 주입 & 생성자, run, postCalculator 메서드 추가
    view/InputView : inputValue 메서드 추가
    단순하고 콘솔 실행으로 테스트하는 것과 유사하여 테스트는 진행하지 않았음

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,27 @@
 package calculator.controller;
 
+import calculator.model.Calculator;
+import calculator.model.RegDelimiter;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class CalculatorController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CalculatorController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+        postCalculator();
+    }
+
+    private Calculator postCalculator() {
+        String inputValue = inputView.inputValue();
+        RegDelimiter regDelimiter = new RegDelimiter();
+        regDelimiter.findCustomDelimiter(inputValue);
+        return new Calculator(inputValue, regDelimiter);
+    }
 }
