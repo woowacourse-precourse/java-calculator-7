cc62cb5640bbb4cbb884205023b22aafd95d220e
SongMinhyuk <dnfldpden32@gmail.com>
2024-10-17 12:35:43 +0900

    FEAT(Application.java): 기본 main 로직 구현
    calculator을 이용한 로직

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+        Calculator calculator = new Calculator(input);
+        calculator.add();
+        System.out.println("결과 : " + calculator.getResult());
     }
 }
