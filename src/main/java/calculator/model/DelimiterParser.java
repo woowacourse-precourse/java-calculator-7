f9268831a874a7af9856abc9f2ab0b9839481611
onesay17 <onesay17@gmail.com>
2024-10-17 10:25:40 +0900

    feat: DelimiterParser 클래스에 구분자 처리 로직 추가
    문자열에서 커스텀 구분자와 기본 구분자를 처리하는 로직을 구현했습니다. "//"와 "\n" 사이의 구분자를 추출하고, 이를 기준으로 숫자들을 분리하는 기능이 포함됩니다.
    
    Closes #3

---
+++ src/main/java/calculator/model/DelimiterParser.java
@@ -0,0 +1,25 @@
+package calculator.model;
+
+import static calculator.util.Constant.DEFAULT_DELIMITERS;
+
+public class DelimiterParser {
+
+    private final InputValidator inputValidator;
+
+    public DelimiterParser(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
+    public String[] parse(String input) {
+        if (input.startsWith("//")) {
+            String[] parts = inputValidator.splitInput(input);
+            String customDelimiter = parts[0].substring(2);
+            inputValidator.validateCustomDelimiter(customDelimiter);
+            inputValidator.validateInputContent(parts[1], customDelimiter);
+            return parts[1].split(customDelimiter);
+        }
+
+        inputValidator.validateDefaultDelimiterContent(input);
+        return input.split(DEFAULT_DELIMITERS);
+    }
+}
