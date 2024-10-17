6191c51e4cd2f3dcf803629d197eaa1523243167
jonghwanlee <kavae550@gmail.com>
2024-10-18 00:15:24 +0900

    feat(InputHandler): 사용자 입력 받아 저장하는 기능 구현
    사용자에게 Console.readLine()을 통해 문자열을 받아 저장하는 setInputString() 메서드를 구현하였습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import input.InputHandler;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+        inputHandler.setInputString(); // 문자열 받는 메서드
+
+        
     }
 }
--- src/main/java/input/InputHandler.java
+++ src/main/java/input/InputHandler.java
@@ -1,4 +1,13 @@
 package input;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputHandler {
+    private String inputString;
+
+    public void setInputString() {
+        this.inputString = Console.readLine();
+    }
+
+
 }
