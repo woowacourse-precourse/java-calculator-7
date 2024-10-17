e85b0b0ffba2426643705a6e53f1b600ba9fafc3
joonwan <joonwan1024@naver.com>
2024-10-17 19:32:05 +0900

    refactor: 문자열 검증 메서드 삭제

--- src/main/java/calculator/DataParser.java
+++ src/main/java/calculator/DataParser.java
@@ -8,23 +8,13 @@ public class DataParser {
         boolean isCustom = isCustom(inputData);
 
         if (isCustom) {
-            checkValidCustomFormat(inputData);
+            Validator.checkValidCustomFormat(inputData);
         }
+
         char[] separators = createSeparators(inputData, isCustom);
         char[] contents = createContents(inputData, isCustom);
-        checkValidContents(separators, contents);
+        Validator.checkValidContents(separators, contents);
         return new Data(separators, contents);
-    }
-
-    private static void checkValidCustomFormat(String inputData) {
-        if (!inputData.contains("\\n")) {
-            throw new IllegalArgumentException();
-        }
-        String[] splitData = inputData.split("\\\\n");
-        int count = splitData.length;
-        if (!(count == 1 || count == 2)) {
-            throw new IllegalArgumentException();
-        }
     }
 
     private static char[] createSeparators(String inputData, boolean isCustom) {
@@ -57,32 +47,6 @@ public class DataParser {
         return new char[]{separator, ':', ','};
     }
 
-    private static void checkValidContents(char[] separators, char[] contents) {
-        int index = 0;
-        while (index < contents.length) {
-            char c = contents[index];
-            if (!(isSeparators(c, separators) || isNumber(c))) {
-                throw new IllegalArgumentException();
-            }
-            index++;
-        }
-    }
-
-    private static boolean isSeparators(char c, char[] separators) {
-        for (int i = 0; i < separators.length; i++) {
-            if (c == separators[i]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean isNumber(char c) {
-        if ('0' <= c && c <= '9') {
-            return true;
-        }
-        return false;
-    }
 
     private static boolean isCustom(String inputData) {
         return inputData.startsWith("//");
