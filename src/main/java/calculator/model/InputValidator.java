7330ea3fec3bef354bb2923e6148f7604878e564
onesay17 <onesay17@gmail.com>
2024-10-17 14:31:16 +0900

    refactor: InputValidator 입력값이 없을때 예외처리 기능 추가
    구분자로 구분할 입력값이 없을 때 예외처리 되도록 수정했습니다.
    
    related to #5

--- src/main/java/calculator/model/InputValidator.java
+++ src/main/java/calculator/model/InputValidator.java
@@ -53,7 +53,7 @@ public class InputValidator {
     public String[] splitByDelimiter(String input, String delimiter) {
 
         if (input == null || input.trim().isEmpty()) {
-            return new String[]{"0"};
+            throw new IllegalArgumentException("입력값이 없습니다. 빈 문자열 또는 null 입력은 허용되지 않습니다.");
         }
 
         if (!input.contains(delimiter) && input.contains(" ")) {
