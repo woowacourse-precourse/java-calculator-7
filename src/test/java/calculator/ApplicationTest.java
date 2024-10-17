dc1d21189d06bc04d7c6663f0184d9251c3a4465
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-17 18:26:10 +0900

    test(application): 정상적인 구분자 테스트를 기능 추가
    올바른 경우의 구분자 테스트 기능을 추가한다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -38,6 +39,26 @@ class ApplicationTest extends NsTest {
                             .isInstanceOf(IllegalArgumentException.class)
                             .hasMessageContaining("올바른 형식으로 입력하세요.")
             );
+        }
+    }
+
+    @Nested
+    class 정상적인_구분자_테스트를_진행한다 {
+
+        @Test
+        void 기본_구분자_테스트를_진행한다() {
+            assertSimpleTest(() -> {
+                run("1,2:3");
+                assertThat(output()).contains("결과 : 6");
+            });
+        }
+
+        @Test
+        void 커스텀_구분자_테스트를_진행한다() {
+            assertSimpleTest(() -> {
+                run("//;\\n1;2;3");
+                assertThat(output()).contains("결과 : 6");
+            });
         }
     }
 
