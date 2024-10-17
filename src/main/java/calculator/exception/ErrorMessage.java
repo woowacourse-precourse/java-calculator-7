a5615127b6cc465d0d3891d8dc4822cf93b85f54
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-17 19:20:07 +0900

    refactor(ErrorMessage): 상수명을 NOT_MATCH_FOUND_DELIMITER로 변경하여 의미 명확화

--- src/main/java/calculator/exception/ErrorMessage.java
+++ src/main/java/calculator/exception/ErrorMessage.java
@@ -2,7 +2,8 @@ package calculator.exception;
 
 public enum ErrorMessage {
 
-    NOT_MATCH_FOUND("매칭되는 구분자가 없습니다.");
+    NOT_MATCH_FOUND_DELIMITER("매칭되는 구분자가 없습니다."),
+    INVALID_INPUT("구분자와 숫자를 정확하게 입력해주세요.");
 
     private final String message;
 
