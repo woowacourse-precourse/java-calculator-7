83d875b8cdb7120d667269adf93777d210d349c8
world-dv <goodforx0147@naver.com>
2024-10-18 00:44:38 +0900

    docs: 불필요한 예외 처리 목록을 삭제합니다.
    -다른 예외 처리에서 해당 예외를 처리해줌으로 해당 예외 처리 문구와 메시지를 삭제합니다.
    -기능 구현 목록에서 해당 예외 처리를 삭제합니다.

--- src/main/java/calculator/exception/ExceptionMessage.java
+++ src/main/java/calculator/exception/ExceptionMessage.java
@@ -5,8 +5,7 @@ public enum ExceptionMessage {
     ERROR_MESSAGE("[ERROR] "),
     INPUT_IS_NOT_NUMBER_EXCEPTION("문자열은 구분자와 숫자만 입력되어야 합니다."),
     INPUT_IS_NOT_POSITIVE_EXCEPTION("문자열은 구분자와 양수만 입력되어야 합니다."),
-    INPUT_IS_WRONG_EXCEPTION("기본 구분자(콜론 ':', 쉼표 ',') 또는 커스텀 구분자('//구분자\\n')를 사용해야 합니다."),
-    CUSTOM_IS_WRONG_EXCEPTION("커스텀 구분자는 '//'와 '\\n'로 구성되어야 합니다.");
+    INPUT_IS_WRONG_EXCEPTION("기본 구분자(콜론 ':', 쉼표 ',') 또는 커스텀 구분자('//구분자\\n')를 사용해야 합니다.");
 
     private final String exceptionMessage;
 
