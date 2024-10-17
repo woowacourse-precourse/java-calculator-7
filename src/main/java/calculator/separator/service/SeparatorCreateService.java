c8e2e76a5255bf773598aa5792ca11c667c30449
EHCTH <lees90896@gmail.com>
2024-10-17 17:40:41 +0900

    feat(SeparatorCreateService): 구분자 생성 인터페이스 구현

--- src/main/java/calculator/separator/service/SeparatorCreateService.java
+++ src/main/java/calculator/separator/service/SeparatorCreateService.java
@@ -1,11 +1,10 @@
 package calculator.separator.service;
 
-import calculator.number.domain.Number;
-import calculator.separator.domain.Separator;
+
+import calculator.sentence.domain.Sentence;
 
 import java.util.function.Function;
-import java.util.function.Supplier;
 
-public interface CreateService {
-    <R> R create(String sentence, Function<String, String> transFunction, Function<String, R> constructor);
+public interface SeparatorCreateService {
+    <R> R createSeprator(Sentence sentence, Function<String, String> transFunction, Function<String, R> constructor);
 }
