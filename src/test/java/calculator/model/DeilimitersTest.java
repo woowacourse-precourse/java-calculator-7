655a289077299a85d331fa433c4a68dc347b8907
hwangdaesun <hds650588@gmail.com>
2024-10-17 22:16:21 +0900

    feat(Delimiters.class) : 커스텀 구분자가 포함된 Delimiters의 getAllDelimiters 메서드 테스트

--- src/test/java/calculator/model/DeilimitersTest.java
+++ src/test/java/calculator/model/DeilimitersTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DeilimitersTest {
-
     @Test
     void 구분자_조회() {
         Delimiters delimiters = Delimiters.create();
@@ -13,5 +12,16 @@ class DeilimitersTest {
         Assertions.assertEquals(Delimiter.values().length, allDelimiters.size());
         Assertions.assertTrue(allDelimiters.contains(Delimiter.COLON.getSymbol()));
         Assertions.assertTrue(allDelimiters.contains(Delimiter.COMMA.getSymbol()));
+    }
+
+    @Test
+    void 커스텀구분자_포함_구분자_조회() {
+        String customDelimiter = "&";
+        Delimiters delimiters = Delimiters.createWithCustomDelimiter(customDelimiter);
+        List<String> allDelimiters = delimiters.getAllDelimiters();
+        Assertions.assertEquals(Delimiter.values().length + 1, allDelimiters.size());
+        Assertions.assertTrue(allDelimiters.contains(Delimiter.COLON.getSymbol()));
+        Assertions.assertTrue(allDelimiters.contains(Delimiter.COMMA.getSymbol()));
+        Assertions.assertTrue(allDelimiters.contains(customDelimiter));
     }
 }
