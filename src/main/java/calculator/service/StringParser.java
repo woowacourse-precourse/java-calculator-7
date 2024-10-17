9b1cc6f970a9a4713233f3dd1fd3aab00022e22c
MCshin00 <sinminc7@gmail.com>
2024-10-17 21:43:22 +0900

    feat: 커스텀 구분자를 기준으로 숫자 추출 기능 구현

--- src/main/java/calculator/service/StringParser.java
+++ src/main/java/calculator/service/StringParser.java
@@ -4,10 +4,25 @@ import calculator.model.Numbers;
 import calculator.model.UserInput;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringParser {
+    private static final String DEFAULT_REGEX = ",|:";
+
     public Numbers parse(UserInput input) {
-        String[] strParts = input.getInputString().split("[,:]");
+        String regex = DEFAULT_REGEX;
+        String numString = input.getInputString();
+        Pattern pattern = Pattern.compile("//(.)\\\\n(.*)");
+        Matcher matcher = pattern.matcher(numString);
+
+        if (matcher.find()) {
+            String customRegex = Pattern.quote(matcher.group(1));
+            regex = regex + "|" + customRegex;
+            numString = matcher.group(2);
+        }
+
+        String[] strParts = numString.split(regex);
         List<Integer> numbers = Arrays.stream(strParts)
                 .map(Integer::parseInt)
                 .toList();
