f9b77e93ef3df681aa28f7a8ceb473785aecc19d
jenna <shinysuh1992@gmail.com>
2024-10-17 16:02:30 +0900

    feat: 문자열 덧셈 클래스 생성 및 호출부 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import calculator.operation.StringAdditionCalculator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String rawInputStr = Console.readLine();
+
+        StringAdditionCalculator calculator = new StringAdditionCalculator(rawInputStr);
+        System.out.println("결과 : " + calculator.calculateSum());
     }
 }
---
+++ src/main/java/calculator/operation/StringAdditionCalculator.java
@@ -0,0 +1,13 @@
+package calculator.operation;
+
+public class StringAdditionCalculator {
+    private String numberString;
+
+    public StringAdditionCalculator(String numberString) {
+        this.numberString = numberString;
+    }
+
+    public int calculateSum() {
+        return 0;
+    }
+}
