e5ac1f39ebf09b6fc59bf6a3561944bab9031482
IM-GYURI <kgulr0517@gmail.com>
2024-10-18 01:05:46 +0900

    feat : add DelimiterCalculator for calculate result
    calculate()
    - 사용자의 입력 값을 기반으로 계산
    - controller에서 호출할 함수
    - 입력 값이 null이거나 빈 값일 경우 예외 처리
    - splitInput()을 사용하여 입력 값을 String 배열로 변환
    - stream을 사용하여 String 배열의 각 요소를 int 값으로 변환한 후 전체 합산
    - 계산된 결과 값 반환
    
    parseNumber()
    - String 값을 int 값으로 변환
    - 형변환 도중 에러 발생 시 예외 처리
    - 변환된 int 값이 음수일 경우 예외 처리
    - 변환된 int 값 반환

--- src/main/java/calculator/model/DelimiterCalculator.java
+++ src/main/java/calculator/model/DelimiterCalculator.java
@@ -1,8 +1,34 @@
 package calculator.model;
 
+import static calculator.exception.ErrorMessage.INVALID_INPUT;
+import static calculator.exception.ErrorMessage.NEGATIVE_NUMBER;
+import static calculator.exception.ErrorMessage.NULL_OR_EMPTY_INPUT;
+
+import java.util.Arrays;
+
 public class DelimiterCalculator {
 
     private static final String DELIMITER_REGEX = "[,:]";
+
+    /**
+     * 사용자의 입력 값을 기반으로 계산
+     *
+     * @param input 사용자의 입력 값
+     * @return 계산된 결과 값
+     * @throws IllegalArgumentException : NULL_OR_EMPTY_INPUT
+     */
+    public int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(NULL_OR_EMPTY_INPUT);
+        }
+
+        String[] parts = splitInput(input);
+
+        return Arrays.stream(parts)
+                .mapToInt(this::parseNumber)
+                .sum();
+    }
+
 
     /**
      * 커스텀 구분자 존재 여부 판단 -> 구분자에 따라 split
@@ -19,4 +45,26 @@ public class DelimiterCalculator {
 
         return input.split(DELIMITER_REGEX);
     }
+
+    /**
+     * String -> int 변환
+     *
+     * @param part String 값
+     * @return 변환된 int 값
+     * @throws IllegalArgumentException : NEGATIVE_NUMBER, INVALID_INPUT
+     */
+    private int parseNumber(String part) {
+        try {
+            int num = Integer.parseInt(part);
+
+            if (num < 0) {
+                throw new IllegalArgumentException(NEGATIVE_NUMBER);
+            }
+
+            return num;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT);
+        }
+    }
+
 }
