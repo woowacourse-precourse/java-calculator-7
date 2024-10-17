d095d1d45b1d6e5d9137853b508d9b97b6654fab
RaZel713 <razel7130@gmail.com>
2024-10-18 01:20:04 +0900

    chore(Application.java): 예외처리 출력 메시지 삭제 및 종료 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -17,7 +17,7 @@ public class Application {
             int result = addNumbers(numbers);
             System.out.println("결과 : " + result);
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            throw e; // 프로그램 종료
         }
     }
 
@@ -34,16 +34,16 @@ public class Application {
         if (input.startsWith("//")) {
             // 3번 인덱스가 '\'인지, 4번 인덱스가 'n' 인지 확인
             if (input.charAt(3) != '\\' || input.charAt(4) != 'n') {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
+                throw new IllegalArgumentException();
             }
             // 2번 인덱스 위치에 숫자가 있는 경우 예외 처리
             if (Character.isDigit(input.charAt(2))) {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
+                throw new IllegalArgumentException();
             }
         } else { // 유효성 case 2: "//"로 시작하지 않는 경우
             for (char c : input.toCharArray()) {
                 if (!Character.isDigit(c) && c != ',' && c != ':') {
-                    throw new IllegalArgumentException("잘못된 입력입니다.");
+                    throw new IllegalArgumentException();
                 }
             }
         }
