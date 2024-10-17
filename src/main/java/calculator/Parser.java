00e5dc5a2a6f389b5fc3e4bd5f42ce35692811f0
kimchijinju <hanbimkim@gmail.com>
2024-10-17 17:47:22 +0900

    feat: 문자가 숫자로 이루어져있는지 확인하고, 숫자 배열에 저장

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -7,18 +7,24 @@ public class Parser {
     private final static String customDelimiterPrefix = "//";
     private final static String customDelimiterSuffix = "\\n";
 
-    private static List<Integer> numbers;
     private static List<Character> delimiters;
 
 
-    public static void parse(String input) {
+    public static List<Integer> parse(String input) {
         delimiters = new ArrayList<>(List.of(':', ','));
-        numbers = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>();
 
         input = parseCustomDelimiter(input);
-    }
 
-    public List<Integer> getNumbers() {
+        String[] stringNumbers = input.split(delimiters.toString());
+
+        for (String stringNumber : stringNumbers) {
+            checkNumeric(stringNumber);
+
+            int number = Integer.parseInt(stringNumber);
+            numbers.add(number);
+        }
+
         return numbers;
     }
 
@@ -41,5 +47,13 @@ public class Parser {
             return true;
         }
         throw new IllegalArgumentException();
+    }
+
+    private static void checkNumeric(String input) {
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
