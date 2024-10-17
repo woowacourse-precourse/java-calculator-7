093c1dfc4e72109920127b11af3c78b416f1b466
SongJaeHoonn <thdwogns121@naver.com>
2024-10-17 20:38:04 +0900

    feat: 숫자 덧셈 구현

--- src/main/java/calculation/Calculation.java
+++ src/main/java/calculation/Calculation.java
@@ -1,4 +1,21 @@
 package calculation;
 
 public class Calculation {
+
+    private static final Calculation INSTANCE = new Calculation();
+
+    private Calculation() {
+    }
+
+    public static Calculation getInstance() {
+        return INSTANCE;
+    }
+
+    public Integer calculate(String[] numbers) {
+        int sum = 0;
+        for(String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
+    }
 }
