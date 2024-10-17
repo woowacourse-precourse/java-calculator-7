bb83caa97f96d0e03626db3a19757ea047b28ea3
garden14 <ci667@dgu.ac.kr>
2024-10-18 00:26:32 +0900

    feat: 덧셈 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,22 +5,22 @@ import camp.nextstep.edu.missionutils.Console;
 public class Calculator {
     private static final String ENTER_MESSAGE = "덧셈할 문자열을 입력해주세요.";
 
-    public static int start() {
+    public static void start() {
         System.out.println(ENTER_MESSAGE);
         String userInput = Console.readLine();
 
         if (userInput == null || userInput.isEmpty()) {
-            return 0;
+            OutputView.printSum(0);
         }
         String[] tokens = Tokenizer.tokenization(userInput);
-        return add(Tokenizer.convertToNumber(tokens));
+        add(Tokenizer.convertToNumber(tokens));
     }
 
-    public static int add(int[] numbers) {
+    public static void add(int[] numbers) {
         int sum = 0;
         for (int number : numbers) {
             sum += number;
         }
-        return sum;
+        OutputView.printSum(sum);
     }
 }
