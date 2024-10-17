2304bc69a5b9d147c9820cbcc716bb87d85c0479
injae-348 <ant0593@naver.com>
2024-10-17 17:14:08 +0900

    feat: 사용자가 잘못된 값을 입력한 경우 IllegalArgumentException 발생 후 애플리케이션 종료 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,14 +7,37 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String expression = getExpression();
+
+        validateExpression(expression);
+
         String[] numList = parseNumbersFromExpression(expression);
         System.out.println("결과 : " + getResult(numList));
+    }
+
+    private static void validateExpression(String expression) {
+        if (expression.isEmpty() || expression == null) {
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+
+        if (expression.startsWith("//")) {
+            if (!expression.contains("\\n")) {
+                throw new IllegalArgumentException("커스텀 구분자가 잘못되었습니다.");
+            }
+        }
     }
 
     private static int getResult(String[] list) {
         int result = 0;
         for (String num : list) {
-            result += Integer.parseInt(num);
+            try {
+                int number = Integer.parseInt(num);
+                if (number < 0) {
+                    throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+                }
+                result += number;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자도 구분자도 아닌 값이 포함되어 있습니다.");
+            }
         }
         return result;
     }
