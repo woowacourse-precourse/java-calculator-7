fd732acb72bd3a430d2cfd84b76878083e14dfec
Changhyun <tabloni.dev@gmail.com>
2024-10-18 01:30:34 +0900

    feat: 사용자 입력을 StringBuilder에 저장하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        
+        // StringBuilder에 입력된 문자열 저장
+        StringBuilder sb = new StringBuilder(input);
+        
+        // 이후 로직 추가 예정
     }
 }
