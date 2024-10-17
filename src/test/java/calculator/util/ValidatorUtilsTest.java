55d6001b51168feaf7a3cf5c52db02243c4882ab
heehun <slrkqk2@naver.com>
2024-10-17 23:12:12 +0900

    test(ValidatorUtilsTest): 커스텀 구분자를 생성하는 부분을 제외한 문자열 검증 테스트 추가

--- src/test/java/calculator/util/ValidatorUtilsTest.java
+++ src/test/java/calculator/util/ValidatorUtilsTest.java
@@ -13,14 +13,16 @@ public class ValidatorUtilsTest extends NsTest {
     String EmptyValidInput  = "";
     String NotIncludeNumberInput = ",,,,";
     String validInput_startWithChar = "//a\n1,2,3,4:5:6";
-    String invalidInput_startWithChar = "//ab\n1,2,3a4:5:6";
+    String invalidInput_startWithChar = "//ab\n1,2,3ab4:5:6";
     String invalidInput_startWithChar2 = "//a\n1,2,3a4:5b6";
-
     String nullInput = null;
+    String invalidInput_startWithInt = "1,2:3a4";
+    String invalidInput_startWithInt2 = "1,2:3,4,";
+
+    // 숫자와 기본 구분자만 있는 경우 테스트
     assertThat(Validator.isValidate(validInput)).isEqualTo(true);
 
-      assertThat(Validator.isValidate(EmptyValidInput)).isEqualTo(true);
-
+    assertThat(Validator.isValidate(EmptyValidInput)).isEqualTo(true);
 
     assertThatThrownBy(() -> Validator.isValidate(NotIncludeNumberInput))
             .isInstanceOf(IllegalArgumentException.class)
@@ -39,6 +41,16 @@ public class ValidatorUtilsTest extends NsTest {
     assertThatThrownBy(() -> Validator.isValidate(nullInput))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("null");
+
+    // 커스텀 구분자가 없는데, 기본 구분자 외의 구분자를 사용한 경우 테스트
+    assertThatThrownBy(() -> Validator.isValidate(invalidInput_startWithInt))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("잘못된 구분자 사용");
+
+    // 커스텀 구분자가 없는데, 기본 구분자 외의 구분자를 사용한 경우 테스트
+    assertThatThrownBy(() -> Validator.isValidate(invalidInput_startWithInt2))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("잘못된 계산 문자열");
   }
 
   @Override
