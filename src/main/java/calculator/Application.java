665ab09c4b1d28a7883e5d148b0648375043c38d
hayeongKo <polarpheno@icloud.com>
2024-10-18 02:08:20 +0900

    feat: handle empty or null input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
+
+    public static int StringCalculator(String input) {
+        if(input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        return 0;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
