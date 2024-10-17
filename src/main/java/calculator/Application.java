99dc1a64b68e52a1d560c9ee312e7cee2f049eb0
JuSeong1130 <jsmoon1130@naver.com>
2024-10-17 18:39:12 +0900

    refactor(Application) : 커스텀 구분자 요구 사항 구현
    문자열 덧셈 기능에 커스텀 구분자를 입력할 수 있게 변경
    Pattern,Matcher와 정규식의 캡처링 기능을 이용하여 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,17 +2,74 @@ package calculator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
-        int answer = 0;
-        String[] strings = input.split(",|:");
+        input = input.isEmpty() ? "0" : input;
+        String delimiter = "";
 
-        for (String str : strings) {
-            answer += Integer.parseInt(str);
+
+        if(input.contains("\\n")) {
+            delimiter = getDelimiter(input.substring(0, input.indexOf("\\n") + 2) );
+            //System.out.println(input.indexOf("\\\\n"));
+            input = input.substring(input.indexOf("\\n") + 2);
+        } else {
+           delimiter = getDelimiter("");
         }
-        System.out.printf("결과 : %d", answer);
+
+        int answer = 0;
+        String[] nums = input.split(delimiter);
+
+        try {
+            for (String str : nums) {
+                //양수가 아니면 예외 처리
+                int i = Integer.parseInt(str);
+                if(i < 0) {
+                    throw new IllegalArgumentException();
+                }
+                answer += i;
+            }
+            System.out.printf("결과 : %d\n", answer);
+        } catch (Exception e) {
+            System.out.println("잘못된 값을 입력하셨습니다");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // 여기 matcher 오류 났던거 있었음 => match find안해서
+    public static String getDelimiter(String input) {
+
+        List<String> delimiters = new ArrayList<>(List.of(",", ":"));
+
+        Pattern pattern = Pattern.compile("//([^.$|()\\[\\]\\\\{^?*+]+)\\\\n");
+        //Pattern pattern = Pattern.compile("//(.+)\\n");
+
+        //Pattern pattern = Pattern.compile("//([^.$|()\\[\\]\\\\{^?*+]+)\\n", Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(input);
+        //System.out.println("Pattern: " + pattern.toString());
+        if (matcher.find()) {
+            //System.out.println(matcher.group(0)); // 매칭된 전체 문자열 출력
+            //System.out.println("매칭된 문자열 " + matcher.group(1)); // 캡처된 그룹(abc) 출력
+            String group = matcher.group(1);
+            delimiters.addAll(List.of(group.split("")));
+            //System.out.println(matcher.group(2)); // 캡처된 그룹(abc) 출력
+        }
+
+        //Pattern pattern = Pattern.compile("//([^.$|()\\[\\]\\\\{^?*+]+)\n");
+
+        /*Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            String group = matcher.group(1);
+            delimiters.addAll(List.of(group.split("")));
+        }*/
+        return String.join("|", delimiters);
     }
 }
