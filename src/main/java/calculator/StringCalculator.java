50f7ab3f119b5785ff8656edfea0530bfd9671fd
Dompoo <dlckdrms0517@gmail.com>
2024-10-17 22:56:31 +0900

    feat: StringCalculator 구현
    - 문자열을 통해 숫자리스트를 분리하는 StringParser와 숫자리스트를 통해 덧셈결과를 반환하는 Calculator의 컨트롤러 역할

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import calculator.customSeparatorExtractor.CustomSeparatorExtractor;
+import calculator.numberExtractor.NumberExtractor;
+
+import java.util.List;
+
+public class StringCalculator {
+	
+	private final StringParser stringParser;
+	private final Calculator calculator;
+	
+	public StringCalculator(
+			CustomSeparatorExtractor customSeparatorExtractor,
+			NumberExtractor numberExtractor,
+			Calculator calculator
+	) {
+		this.stringParser = new StringParser(customSeparatorExtractor, numberExtractor);
+		this.calculator = calculator;
+	}
+	
+	public int calculate(String customSeparatorPart, String numberPart) {
+		List<Integer> numbers = stringParser.parse(customSeparatorPart, numberPart);
+		
+		return calculator.add(numbers);
+	}
+}
