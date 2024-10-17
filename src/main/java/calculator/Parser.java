9572480351751ace17cd4b73bcdbfae4a356eff9
wingyou <winwing.you@gmail.com>
2024-10-17 18:00:29 +0900

    feat(Parser): custom delimiter feature of parse()

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -6,7 +6,15 @@ package calculator;
 public class Parser {
 
     public int[] parse(String input) {
-        String[] segments = input.split("[,|:]");
+        String[] segments;
+
+        if (input.startsWith("//")) {
+            char delimiter = input.charAt(2);
+            segments = input.split("\n")[1].split(String.valueOf(delimiter));
+        } else {
+            segments = input.split("[,|:]");
+        }
+
         int[] numbers = new int[segments.length];
         for (int i = 0; i < segments.length; i++) {
             numbers[i] = Integer.parseInt(segments[i]);
