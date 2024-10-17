86154cdf19aa834a134accdabf720d1413219e88
김건학 <kkh10002@naver.com>
2024-10-18 01:56:16 +0900

    refactor(split): getFrontString 반환값 수정
    getFrotString을 통해 얻은 문자열은 '구분자'로만 구성된다.
    앞 문자열의 시작 부분인 '//'를 제거하여,  '//'와 '\n'이 제거된 순수한 문자열을 반환받는다.

--- src/main/java/calculator/split/StringSplit.java
+++ src/main/java/calculator/split/StringSplit.java
@@ -3,7 +3,7 @@ package calculator.split;
 public class StringSplit {
     public static String getFrontString(String string) {
         String[] split = string.split("\n");
-        return split[0];
+        return split[0].substring(2);
     }
 
     public static String getBackString(String string) {
--- src/test/java/calculator/split/StringSplitTest.java
+++ src/test/java/calculator/split/StringSplitTest.java
@@ -11,7 +11,7 @@ class StringSplitTest {
     @Test
     @DisplayName("문자열에서 '\n' 앞 부분을 올바르게 추출했는지 검증")
     void extractFrontString() {
-        String string = "abc\n123";
+        String string = "//abc\n123";
 
         Assertions.assertEquals(getFrontString(string), "abc");
     }
@@ -19,7 +19,7 @@ class StringSplitTest {
     @Test
     @DisplayName("문자열에서 '\n' 뒷 부분을 올바르게 추출했는지 검증")
     void extractBackString() {
-        String string = "abc\n123";
+        String string = "//abc\n123";
 
         Assertions.assertEquals(getBackString(string), "123");
     }
@@ -27,7 +27,7 @@ class StringSplitTest {
     @Test
     @DisplayName("첫번째로 나오는 '\n'로 문자열을 자른다.")
     void splitAtFirstNewline() {
-        String string = "abc\n\n123";
+        String string = "//abc\n\n123";
 
         Assertions.assertEquals(getFrontString(string), "abc");
         Assertions.assertEquals(getBackString(string), "\n123");
@@ -36,7 +36,7 @@ class StringSplitTest {
     @Test
     @DisplayName("문자열에서 '\n' 뒷 부분이 빈 문자열인 경우")
     void backStringEmpty() {
-        String string = "abc\n";
+        String string = "//abc\n";
 
         Assertions.assertEquals(getBackString(string), "");
     }
