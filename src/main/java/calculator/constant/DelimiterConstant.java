6fd87224f0fffa3df8c03fa806c939c64c3167ac
tentenball <lth8905@gmail.com>
2024-10-18 02:35:20 +0900

    refactor, docs: 숫자 추출하기 비즈니스 로직 구현

--- src/main/java/calculator/constant/DelimiterConstant.java
+++ src/main/java/calculator/constant/DelimiterConstant.java
@@ -3,4 +3,9 @@ package calculator.constant;
 public class DelimiterConstant {
 	public static String COMMA = ",";
 	public static String COLON = ":";
+	public static int NO_CUSTOM_DELIMITER_LENGTH = 2;
+	public static String NO_CUSTOM_DELIMITER_SPLIT_REGEX = "[,:]";
+	public static String REGEX_START_SIGN = "[";
+	public static String REGEX_END_SIGN = "]";
+
 }
--- src/main/java/calculator/helper/ParseHelper.java
+++ src/main/java/calculator/helper/ParseHelper.java
@@ -1,13 +1,16 @@
 package calculator.helper;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
 
+import calculator.constant.DelimiterConstant;
 import calculator.constant.DrawConstant;
 import calculator.exception.CalculatorException;
 import calculator.exception.message.ExceptionMessage;
+import calculator.util.RegexGenerator;
+import calculator.util.TypeConversion;
 
 public class ParseHelper {
 	public String extractCustomDelimiter(String formula) {
@@ -16,19 +19,29 @@ public class ParseHelper {
 	}
 
 	public List<Long> extractNumbers(Set<String> delimiters, String formula) {
-		List<Long> numbers = new ArrayList<>();
-
+		if (delimiters.size() == DelimiterConstant.NO_CUSTOM_DELIMITER_LENGTH) {
+			return Arrays.stream(formula.split(DelimiterConstant.NO_CUSTOM_DELIMITER_SPLIT_REGEX))
+				.map(TypeConversion::convertStringToLong)
+				.toList();
+		}
+		String excludedCustomDelimiterFormula = formula.substring(
+			formula.indexOf(DrawConstant.CUSTOM_DELIMITER_END_SIGN) + DrawConstant.NEXT_INDEX);
+		return Arrays.stream(
+				excludedCustomDelimiterFormula.split(RegexGenerator.generateExtractNumberRegex(delimiters)))
+			.map(TypeConversion::convertStringToLong)
+			.toList();
 	}
 
 	private void validateCustomDelimiter(String formula) {
 		if (!formula.contains(DrawConstant.CUSTOM_DELIMITER_END_SIGN)) {
 			throw new CalculatorException(ExceptionMessage.NOT_FOUND_CUSTOM_DELIMITER_END_SIGN);
 		}
-		if (!(formula.charAt(DrawConstant.BACK_SLASH_INDEX) == '\\' && formula.charAt(DrawConstant.N_INDEX) == 'n')) {
+		if (!(formula.charAt(DrawConstant.BACK_SLASH_INDEX) == DrawConstant.BACK_SLASH
+			&& formula.charAt(DrawConstant.N_INDEX) == DrawConstant.N)) {
 			throw new CalculatorException(ExceptionMessage.INVALID_CUSTOM_PREFIX);
 		}
-		if (Pattern.matches(DrawConstant.CUSTOM_DELIMITER_REGEX,
-			String.valueOf(formula.charAt(DrawConstant.CUSTOM_DELIMITER_INDEX)))) {
+		if (!(Pattern.matches(DrawConstant.CUSTOM_DELIMITER_REGEX,
+			String.valueOf(formula.charAt(DrawConstant.CUSTOM_DELIMITER_INDEX))))) {
 			throw new CalculatorException(ExceptionMessage.INVALID_CUSTOM_PREFIX);
 		}
 	}
