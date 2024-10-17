a951212a7b0b1cf346dcb2e99c9ff88c74431f19
EHCTH <lees90896@gmail.com>
2024-10-17 18:02:42 +0900

    feat(ErrorMessage): Erromessage 상수 클래스 구현

--- src/main/java/calculator/constant/ErrorMessage.java
+++ src/main/java/calculator/constant/ErrorMessage.java
@@ -1,4 +1,8 @@
 package calculator.constant;
 
 public class ErrorMessage {
+    public static final String ERROR_MESSAGE = "[ERROR] 발생";
+    public static final String REGEX_ERROR = ERROR_MESSAGE + " 형식에 맞지 않는 입력값";
+    public static final String UNEXPECTED_ERROR = ERROR_MESSAGE + " 미확인 에러 발생";
+    public static final String MAX_VALUE_ERROR = ERROR_MESSAGE + " 9223372036854775807 보다 큰 수입니다";
 }
