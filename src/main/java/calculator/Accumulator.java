f986dd62da7e7a40d89cb7072be0289c0058f6a3
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-18 01:11:42 +0900

    feat(calculator): 합산 처리 클래스 Accumulator 추가
    Accumulator 클래스를 통해 문자열 배열을 정수로 변환하고 합산 처리.

--- src/main/java/calculator/Accumulator.java
+++ src/main/java/calculator/Accumulator.java
@@ -1,2 +1,14 @@
-package calculator;public class Accumulator {
+package calculator;
+
+public class Accumulator {
+
+    public int sum(String[] input) {
+        int sum = 0;
+
+        for (String s : input) {
+            sum += Integer.parseInt(s.trim());
+        }
+
+        return sum;
+    }
 }
