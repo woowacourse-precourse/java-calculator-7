a786e2d9a10d8a5c5eacdb77eb2ca20e6b0dc078
최창환 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-17 11:22:59 +0900

    refactor: inputString 지역 변수로 변경

--- src/main/java/calculator/handler/InputHandler.java
+++ src/main/java/calculator/handler/InputHandler.java
@@ -3,9 +3,8 @@ package calculator.handler;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
-    private String inputString;
-
     public String inputString() {
+        String inputString;
         return inputString = Console.readLine();
     }
 }
