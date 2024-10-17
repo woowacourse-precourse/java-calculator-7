503f8bdb08296112ff819a5a6be02119a8b8eda4
choi <nippert@naver.com>
2024-10-17 15:33:24 +0900

    refactor: 입력과 출력 방식을 변경

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,12 @@
+package calculator.controller;
+
+import calculator.domain.StringAddCalculator;
+import view.SystemView;
+
+public class CalculatorController {
+
+    public static void run() {
+        String formula = SystemView.input();
+
+    }
+}
--- src/main/java/view/SystemView.java
+++ src/main/java/view/SystemView.java
@@ -4,13 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class SystemView {
 
-    private static String input() {
+    public static String input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 
-    public static void run() {
-        String formula = input();
+    public static void printResult(int result) {
+        System.out.println("결과 : " + result);
         Console.close();
     }
 }
