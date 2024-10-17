45da56cc4cf716c9f631db05d31a90e4ed755358
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-17 22:12:38 +0900

    refactor(OutputView): 상수 변수 추가

--- src/main/java/calculator/OutputView.java
+++ src/main/java/calculator/OutputView.java
@@ -2,7 +2,9 @@ package calculator;
 
 public class OutputView {
 
+    private static final String OUTPUT_MESSAGE = "결과 : ";
+
     public void printSum(int sum) {
-        System.out.println("결과 : " + sum);
+        System.out.println(OUTPUT_MESSAGE + sum);
     }
 }
