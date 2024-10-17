48ef508c5709681f8b38883e33914d3096fdf5c1
dradnats1012 <herjebi1012@gmail.com>
2024-10-17 22:32:33 +0900

    feat: 입출력문 메서드 생성

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -4,7 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static String getInput(){
+    private static final String INPUT_STRING = "덧셈할 문자열을 입력해 주세요";
+
+    public static String getInput() {
+        System.out.println(INPUT_STRING);
 
         return Console.readLine();
     }
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,4 +1,10 @@
 package calculator.view;
 
 public class OutputView {
+
+    private static final String RESULT = "결과";
+
+    public static void printResult(int result){
+        System.out.println(RESULT + " : " + result);
+    }
 }
