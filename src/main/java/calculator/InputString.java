e3577278547aacecf4fb35bae6f50ca8e46f7bb3
sim-mer <gyu2781@gmail.com>
2024-10-17 18:43:40 +0900

    feat: 기능 구현 - 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있음 - 커스텀 구분자는 문자열 앞부분의 "//"와 "\n" 사이에 위치하는 문자를 커스텀 구분자로 사용

--- src/main/java/calculator/InputString.java
+++ src/main/java/calculator/InputString.java
@@ -19,6 +19,16 @@ public class InputString {
             return List.of();
         }
 
+        if(input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\n");
+            String delimiter = input.substring(2, delimiterIndex);
+            input = input.substring(delimiterIndex + 1);
+
+            return Arrays.stream(input.split(delimiter))
+                    .map(Integer::valueOf)
+                    .toList();
+        }
+
         return Arrays.stream(input.split("[,:]"))
                 .map(Integer::valueOf)
                 .toList();
