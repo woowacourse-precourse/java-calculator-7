c0b58fbc23659c230c97ae4e820acaddf62ef66e
태규 <123963462+EOTAEGYU@users.noreply.github.com>
2024-10-17 17:58:01 +0900

    feat: 문자열 계산기 기능 추가
    목표
    정상적인 값 더하는 계산기 먼저 구현
    입력값 구분자 기준 배열로 나누어 합 출력
    
    추가 해야 할 기능
    커스텀 구분자 추가, 잘못된 입력 값 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,11 +3,47 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+
+    // 구분자로 나누어 배열로 저장
+    private String[] splitNumbers(String input){
+        if(input == null || input.isEmpty()){
+            throw new IllegalArgumentException("입력 값이 비어있습니다.");
+        }
+
+        String delimiter = ",|:";
+        String numbers = input;
+
+
+
+        return numbers.split(delimiter);
+    }
+
+    // 나누어진 배열의 합 구하기
+    private int sumNumbers(String[] numbers){
+        int sum = 0;
+        for (String number: numbers){
+            int num = Integer.parseInt(number);
+            sum += num;
+        }
+        return sum;
+    }
+
+
     public static void main(String[] args) {
+        // 사용자 입력 받기
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        System.out.println(input);
+        Application application = new Application();
 
+        // 1. 사용자 입력 구분자 기준 배열로 나누기
+        String[] splitNumbers = application.splitNumbers(input);
+
+        // 2. 나누어진 배열 값 더하기
+        int result = application.sumNumbers(splitNumbers);
+
+
+        System.out.println("결과 : " + result);
     }
 }
