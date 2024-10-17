fdc8dc47bac36e1f5f2d92ef60b2b2085777ba0a
Mirim <marinrim23@gmail.com>
2024-10-17 15:45:46 +0900

    rename : Change 'Calculator' from test.java.calculator to main.java.calculator
    I made a mistake. Calculator was in wrong package.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,9 @@
 package calculator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요.\n");
         String str = readLine();
 
         String delimiter = ",|:";
