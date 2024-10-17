eb782e222457ed3d945ec0c3c2d341b0a9678840
hyuuuun <pam4522@gmail.com>
2024-10-17 18:02:22 +0900

    feat : Input 객체 생성

---
+++ src/main/java/calculator/Input.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    private String input;
+    public Input(){
+        input =  Console.readLine();
+    }
+
+
+}
