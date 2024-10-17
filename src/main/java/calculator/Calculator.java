f95b92bcb6523e286732a140d7b87f6892aa6fb7
bee9827 <ehfrhfo9494@naver.com>
2024-10-17 18:08:27 +0900

    feat: operator 관련기능
    기능 : 문자열 앞부분의 "//"와 "\n" 사이에 위치하는 문자를 커스텀 구분자로 사용한다.
    
    그뒤에 있는 숫자들을 operator로 구분하여 저장

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,12 +2,34 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
     private String input;
+    private String operator = ",:";
+    private String[] splitStr;
     public String readLine(){
         return input = Console.readLine();
     }
-    public String getInput(){
-        return input;
+
+    public String[] splitInput(){
+        Pattern pattern = Pattern.compile("//.\\\\n");
+        Matcher matcher = pattern.matcher(input);
+
+        while(matcher.find()){
+            operator += matcher.group(0).charAt(2);
+        }
+
+        String[] temp = input.split(pattern.pattern());
+        splitStr = temp[temp.length - 1].split("["+operator+"]");
+
+        return splitStr;
     }
+
+
+    public void isValid(){
+
+    }
+
 }
