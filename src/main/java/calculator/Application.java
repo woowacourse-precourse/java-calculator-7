faa5a8ab3b6c0366645da527959d93c866808d11
kimchijinju <hanbimkim@gmail.com>
2024-10-18 00:59:11 +0900

    feat: 결과물을 출력한다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         String input = InputHandler.readInput();
         int result = Calculator.calculate(Parser.parse(input));
+        OutputHandler.printOutput(result);
     }
 }
---
+++ src/main/java/calculator/OutputHandler.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class OutputHandler {
+    public static void printOutput(int result) {
+        System.out.println("결과 : " + result);
+    }
+}
