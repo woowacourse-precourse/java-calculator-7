1c515de5c4037bf827d19aa9d243ce9287af1b52
hwangdaesun <hds650588@gmail.com>
2024-10-17 13:41:40 +0900

    feat(CustomDelimiterParser.class) : 커스텀 구분자가 있으면 반환하는 CustomDelimiterParser 클래스 구현
    parse 메서드 내부 구현 미완료

---
+++ src/main/java/calculator/model/CustomDelimiterParser.java
@@ -0,0 +1,13 @@
+package calculator.model;
+
+public class CustomDelimiterParser {
+
+
+    public static CustomDelimiterParser getInstance() {
+        return new CustomDelimiterParser();
+    }
+
+    public String parse(String input) {
+        return "";
+    }
+}
