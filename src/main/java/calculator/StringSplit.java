00f038f130d7ab3398914bbd750ef6a950fe2bab
eunho <eunho1032@gmail.com>
2024-10-18 00:09:51 +0900

    fix(StringSplit): 복수 구분자를 처리하기 위한 표현 추가

--- src/main/java/calculator/StringSplit.java
+++ src/main/java/calculator/StringSplit.java
@@ -1,9 +1,14 @@
 package calculator;
 
 public class StringSplit {
-	public static String[] stringSplit(String input, String delimiter, int delmiiterEndIdx) {
-		String numberpart = input.substring(delmiiterEndIdx);
-		String[] d = numberpart.split(delimiter);
-		return d;
+	public static String[] splitString(String input, String delimiter, int beginIndex) {
+		String newString = input.substring(beginIndex);
+		String[] numbers = newString.split("[" + delimiter + "]"); // //\n 이후의 문자열을 구분자로 나눔
+		/*
+		 * 24.10.17
+		 * 사용자 입력이 //\\n22\23\44 처럼 들어오는 경우 위 ("[" + delimiter + "]") 표현에서 대괄호가 닫히지 않아 문제 발생
+		 * split 직접 구현 필요
+		 */
+		return numbers;
 	}
 }
