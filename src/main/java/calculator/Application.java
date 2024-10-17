66597bc27a30f6a1267d6cce054b01e1635c3853
sunwon <jsw5913@naver.com>
2024-10-17 23:58:59 +0900

    feat(Applcation): 계산기 프로그램 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import calculator.model.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
+
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        outputView.displayInputMessage();
+        String input = inputView.readInput();
+
+        Calculator calculator = new Calculator();
+        int sum = calculator.sumNumbersFromString(input);
+
+        outputView.displayResultMessage(String.valueOf(sum));
     }
 }
