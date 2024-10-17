80468969a22de73dcce654387ccd3d74980a98c5
myungkeun02 <audrms3568@gmail.com>
2024-10-18 00:43:18 +0900

    feat: 예외 처리 테스트 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -65,13 +65,47 @@ class ApplicationTest extends NsTest {
         });
     }
 
-//    @Test
-//    void 예외_테스트() {
-//        assertSimpleTest(() ->
-//            assertThatThrownBy(() -> runException("-1,2,3"))
-//                .isInstanceOf(IllegalArgumentException.class)
-//        );
-//    }
+    @Test
+    void 숫자가_아닌_문자_포함_예외_테스트() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("a,2:3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 연속된_구분자_사용_예외_테스트() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("1,,2:3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 잘못된_커스텀_구분자_형식_예외_테스트() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//;\1,,2:3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_숫자_사용_예외_테스트() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//1\n112:3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 음수_입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("-1,2:3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+
 
     @Override
     public void runMain() {
