148f35dccb333da7cb0265c652bd5a11543ec77b
hootzz <wjqwjql3@gmail.com>
2024-10-17 23:11:28 +0900

    feat: Application에 StringCalculator 실행 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        StringCalculator calculator = new StringCalculator();
+        try {
+            int result = calculator.add(input);
+            System.out.println("결과: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("입력한 값에 문제가 있습니다. " + e.getMessage());
+        }
+
     }
 }
