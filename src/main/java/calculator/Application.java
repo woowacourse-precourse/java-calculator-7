42decfa396c6c88d576ec48dcc4c66e859fadd57
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2024-10-17 16:31:16 +0900

    feat: 문자열 파싱 및 연산 기능 구현
    문자열 파싱
    - String 객체의 split() 메소드를 사용하여 다양한 구분자(디폴트: 쉼표, 콜론)들에 대해 문자열 내 숫자들만 따로 분리함.
    - 커스텀 구분자를 고려하기 위해 split() 메소드에 넣을 인자를 동적으로 처리함.
    - 연속으로 구분자가 입력되는 경우를 고려함.
    - 연속 구분자 입력, 커스텀 구분자 연속 입력, 2자리 이상의 숫자 입력에 대한 테스트 진행 완료.
    
    연산
    - Java Stream API를 사용하여 숫자 문자열 배열의 합을 연산.
    - 각각의 테스트 케이스에 대해 연산 결과가 정상적으로 나오는 것을 확인
      완료.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,45 @@
 package calculator;
 
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String delimiters = ",:";  // 커스텀 문자열까지 받기 위해 열어둔다.
+
+        Scanner sc = new Scanner(System.in);
+        String inputStr = sc.nextLine();
+        sc.close();
+
+        // 커스텀 문자열을 찾아서 포함시켜준다.
+        if (inputStr.substring(0, 2).equals("//")) {
+            String[] strings = findCustomDelimiter(inputStr);
+            delimiters = delimiters.concat(strings[0]);
+            inputStr = strings[1];
+        }
+
+        // 정규표현식 활용해서 연속으로 구분자가 들어오는 경우도 처리해준다.
+        String[] pureStr = inputStr.split("["+delimiters+"]+");
+
+        int result = Arrays.stream(pureStr).mapToInt(Integer::parseInt).sum();
+        System.out.println("결과 : "+result);
+    }
+
+    /**
+     * 커스텀 구분자를 찾는 메소드.
+     * @param inputStr
+     * @return
+     *     첫 번째 인자: 커스텀 구분자
+     *     두 번째 인자: 커스텀 구분자 입력 영역을 제외한 순수 문자열
+     */
+    public static String[] findCustomDelimiter(String inputStr) {
+
+        int index = inputStr.indexOf("\\n");
+        String[] tempStr = new String[2];
+        tempStr[0] = inputStr.substring(2, index);
+        tempStr[1] = inputStr.substring(index + 2);
+
+        return tempStr;  // 여러 개의 커스텀 문자열이 입력될 수 있다.
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,10 +17,34 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 연속된_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,,2::3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_연속된_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;!\\n1;;2!!3::4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 두_자리_이상_숫자() {
+        assertSimpleTest(() -> {
+            run("1,20,300:4000");
+            assertThat(output()).contains("결과 : 4321");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
