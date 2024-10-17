cd8830e76ab197b6e494b9af13c7a67536715862
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-10-17 15:10:12 +0900

    feat: add user input functionality

---
+++ src/main/java/calculator/Input.java
@@ -0,0 +1,15 @@
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input  {
+    public String getInput() {
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String Input = Console.readLine();
+    }
+
+
+
+
+
+
+    }
+}
