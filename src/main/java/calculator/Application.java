1d321ffa09fa158e3cae30f1060e72ff0c4059c9
5ms <oms010414@gmail.com>
2024-10-17 21:19:56 +0900

    구분자별로 숫자 분할

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,10 +2,7 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Application {
 
@@ -18,6 +15,8 @@ public class Application {
 
         validateInput(input);
 
+        ArrayList<Integer> tokens = inputParse(input);
+
     }
 
     public static String userInput(){
@@ -26,28 +25,76 @@ public class Application {
     }
 
     public static void validateInput(String str){
-        if(str.startsWith("//")){
+        if(str.length()>=4 && str.startsWith("//")){
             int idx = str.indexOf("\\n");
             if(idx == -1){
-                throw new IllegalStateException("커스텀 구분자가 올바른 형식으로 입력되지 않았습니다.");
+                throw new IllegalArgumentException();
             }
             String customSeparator = str.substring(2,idx);
 
             for(int i=0;i<customSeparator.length();i++){
                 separator.addSeparator(customSeparator.charAt(i));
-                System.out.println(customSeparator.charAt(i));
             }
             str = str.substring(idx+2);
         }
 
+        if(str.isEmpty()){
+            return;
+        }
+
         for(int i=0;i<str.length();i++){
             if(isDigit(str.charAt(i)) || separator.isSeparator(str.charAt(i))) continue;
-            throw new IllegalStateException("잘못된 입력입니다.");
+            throw new IllegalArgumentException();
         }
+
+        //연속적인 구분자
+        boolean flag = false;
+        for(int i=0;i<str.length();i++){
+            if(separator.isSeparator(str.charAt(i))){
+                if(!flag) {
+                    flag = true;
+                    continue;
+                }
+                throw new IllegalArgumentException();
+            }
+            flag = false;
+        }
+
+        //처음, 마지막 문자가 구분자라면 잘못된 입력
+        if(separator.isSeparator(str.charAt(0)) || separator.isSeparator(str.charAt(str.length()-1))){
+            throw new IllegalArgumentException();
+        }
+
     }
 
     public static boolean isDigit(Character c){
         return ('0'<=c && c<='9');
     }
+
+    public static ArrayList<Integer> inputParse(String str){
+        int idx = str.indexOf("\\n");
+        if(idx!=-1){
+            str = str.substring(idx+2);
+        }
+
+        ArrayList<Integer> tokens = new ArrayList<>();
+        StringBuilder tmp = new StringBuilder();
+        for(char c : str.toCharArray()){
+            if(separator.isSeparator(c)){
+                tokens.add(Integer.parseInt(tmp.toString()));
+                tmp.setLength(0);
+            }
+            else{
+                tmp.append(c);
+            }
+        }
+
+        if(!tmp.isEmpty()){
+            tokens.add(Integer.parseInt(tmp.toString()));
+        }
+
+        return tokens;
+    }
+
 
 }
