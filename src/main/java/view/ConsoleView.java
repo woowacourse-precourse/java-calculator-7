044d5adad79ff8342ee86edc4a47e2f6ba0e3d0e
jumdo12 <jumdo12@gmail.com>
2024-10-17 13:19:27 +0900

    style(view) ConsoleView 코드 누락
    - 누락된 코드 수정

--- src/main/java/view/ConsoleView.java
+++ src/main/java/view/ConsoleView.java
@@ -1,2 +1,14 @@
-package view;public class ConsoleView {
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleView {
+    public String getInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요:");
+        return Console.readLine();
+    }
+
+    public void displayResult(int result) {
+        System.out.println("결과 : " + result);
+    }
 }
