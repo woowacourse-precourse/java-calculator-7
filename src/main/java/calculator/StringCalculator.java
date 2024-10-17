b8307d0d0704a8f133d2bedc8063194731fd53a9
Changsu Park <gd12091@gmail.com>
2024-10-17 21:47:59 +0900

    feat: 기본 구분자 연산 구현

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,38 @@
+package calculator;
+
+import java.util.Arrays;
+
+public class StringCalculator {
+    private final String str;
+
+    public StringCalculator(String str) {
+        this.str = str;
+    }
+
+    // 기본 구분자
+    private String delimiter = ",:";
+
+    // 문자열을 정수로 변환하는 메서드
+    private int parseInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {   // 문자열을 정수로 전환할 수 없다면
+            throw new IllegalArgumentException("Cannot change string to integer");
+        }
+    }
+
+    // 문자열을 더하는 메서드
+    public int add() {
+        String numbers = str;
+
+        if (numbers == null || numbers.isEmpty()) { //입력값이 없다면 0을 리턴
+            return 0;
+        }
+
+        return Arrays.stream(numbers.split("[" + delimiter + "]", -1))
+                //limit:-1로 설정하면, 공백이어도 문자열의 끝까지 탐색해서 null로 저장한다.
+                .map(String::trim)
+                .mapToInt(this::parseInt)
+                .sum();
+    }
+}
