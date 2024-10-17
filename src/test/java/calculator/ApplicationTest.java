970b90a00ffdc18ab3f7a33fef9cee404b0596f5
yereumi <lovelyyeji01@gmail.com>
2024-10-17 20:13:40 +0900

    test(ApplicationTest): add new test cases

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,8 +19,64 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 십의_자리_수_인식() {
+        assertSimpleTest(() -> {
+            run("10,2;3");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
+    @Test
+    void 예외_테스트_공통1() { // 숫자로 혹은 '/'로 시작하지 않는 문자열
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a:2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_기본1() { // ',', ':' 외의 구분자
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1[2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_커스텀1() { // 커스텀 설정 시 오류1
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("/^1^2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_커스텀2() { // 커스텀 설정 시 오류2
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//^1^2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_커스텀3() { // 커스텀 설정 시 오류3
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//^\\1^2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_커스텀4() { // 기본/커스텀 외의 구분자
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//^\\n1^2+3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
