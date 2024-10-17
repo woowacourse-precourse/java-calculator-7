67b2a6d446b7f7c139694b5ffb14563fd11e73ab
onesay17 <onesay17@gmail.com>
2024-10-17 11:46:28 +0900

    refactor: CalculatorView 인터페이스 변경 및 CalculatorViewImpl 구현체 추가
    사용자로부터 입력을 받고 계산 결과를 출력하는 기능을 추상화한 CalculatorView 인터페이스를 추가하고, 이를 구현하는 CalculatorViewImpl 클래스를 추가했습니다. 이를 통해 UI 처리 방식에 대한 유연성을 제공합니다.
    
    Closes #7

--- src/main/java/calculator/view/CalculatorView.java
+++ src/main/java/calculator/view/CalculatorView.java
@@ -1,15 +1,7 @@
 package calculator.view;
 
-import camp.nextstep.edu.missionutils.Console;
+public interface CalculatorView {
+    String getInput();
 
-public class CalculatorView {
-
-    public String getInput() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine().replace("\\n", "\n");
-    }
-
-    public void showResult(int result) {
-        System.out.println("결과 : " + result);
-    }
+    void showResult(int result);
 }
