b5d7a01f7990a922133cef7ec5c21afc48c4f2af
seonwoonam <potatomoney716@gmail.com>
2024-10-18 02:20:36 +0900

    feat(splitString) : 구분자를 통한 문자열 분리 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,8 +1,13 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
     private final String inputString;
     private int sum = 0;
+    private String delimiter = "[,:]";
+    private List<Integer> numbers = new ArrayList<>();
 
     private Calculator() {
         this.inputString = Input.input();
@@ -10,5 +15,13 @@ public class Calculator {
 
     private void printResult() {
         System.out.println("결과 : " + this.sum);
+    }
+
+    private void splitString() {
+        String[] numbersStr = inputString.split(delimiter);
+        for (int i = 0; i < numbersStr.length; i++) {
+            int number = Integer.parseInt(numbersStr[i].trim());
+            numbers.add(number);
+        }
     }
 }
