d180571208ce2551bab445a1cf6336d6bcc6fd1f
dradnats1012 <herjebi1012@gmail.com>
2024-10-17 23:21:41 +0900

    refactor: VerificationNum 클래스 분리, stream 삭제

--- src/main/java/calculator/model/VerificationNum.java
+++ src/main/java/calculator/model/VerificationNum.java
@@ -1,25 +1,22 @@
 package calculator.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class Verification {
+public class VerificationNum {
 
     private final List<String> parsedList;
-    private final List<Integer> numList = new ArrayList<>();
 
-    public Verification(List<String> parsedList){
+    public VerificationNum(List<String> parsedList) {
         this.parsedList = parsedList;
     }
 
     public void verifyAndParseNums() {
-        parsedList.stream().forEach(num -> {
+        parsedList.forEach(num -> {
             try {
                 int parsedNum = Integer.parseInt(num);
                 if (parsedNum <= 0) {
                     throw new IllegalArgumentException("숫자는 양수여야 합니다!");
                 }
-                numList.add(parsedNum);
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("숫자가 아닌 문자가 들어가있습니다!");
             }
