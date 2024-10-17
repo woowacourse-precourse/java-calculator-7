077f08f56379907cddf331e41fc476fc1779ff51
junhyuk1248 <poa124@nate.com>
2024-10-17 18:17:06 +0900

    기본 구분자로 문자열 분리 및 숫자로 변환 후, 합산기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,14 +1,25 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> listNumber = new ArrayList<>();
+
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        String default_Delimiters = ",|;";
+        String default_Delimiters = ",|:";
+        String[] numbers = input.split(default_Delimiters);
 
-        
+        for (String number : numbers) {
+            listNumber.add(Integer.parseInt(number));
+        }
+
+        int sum = listNumber.stream().mapToInt(i -> i).sum();
+
+        System.out.println("sum = " + sum);
     }
 }
