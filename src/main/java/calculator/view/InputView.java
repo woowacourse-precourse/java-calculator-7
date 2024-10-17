5778e0dac5406bcffaf9f137ec2e2937a3b82431
DongYeop <s01054830455@gmail.com>
2024-10-17 15:15:29 +0900

    feat: 빈 공백 입력시 예외발생

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -3,11 +3,25 @@ package calculator.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String BLANK = " ";
 
     public String inputStrings() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
 
-        return Console.readLine();
+        validateBlank(input);
+
+        return input;
+    }
+
+    private void validateBlank(String input) {
+        if (isBlank(input)) {
+            throw new IllegalArgumentException("유효하지 않은 입력입니다.");
+        }
+    }
+
+    private boolean isBlank(String input) {
+        return input.contains(BLANK);
     }
 }
 
