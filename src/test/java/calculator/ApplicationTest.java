2917198dcc34357b20341c4ad9e169cced219ddf
immyeong <wpfls6670@naver.com>
2024-10-17 10:49:11 +0900

    test: 구현함수 검증 및 결과 검증

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import calculator.domain.Calculator;
+import calculator.view.InputView;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +24,98 @@ class ApplicationTest extends NsTest {
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 예외_테스트_커스텀_구분자_누락_테스트(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("커스텀 구분자가 누락되었습니다.")
+        );
+    }
+
+    @Test
+    void 예외_테스트_기본_구분자_외_구분자_입력_테스트(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2#3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("기본 구분자 외 구분자가 포함되어있습니다.")
+        );
+    }
+
+    @Test
+    void 예외_테스트_커스텀_구분자_외_구분자_입력_테스트(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//#\\n1#2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("커스텀 구분자 외 구분자가 포함되어있습니다.")
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_추출_테스트(){
+        InputView inputView = new InputView();
+        Calculator calculator = new Calculator(inputView);
+        assertSimpleTest(() -> {
+            String division = calculator.extractDivision("//;\\n1");
+            assertThat(division).isEqualTo(";");
+        });
+    }
+
+    @Test
+    void 메타_문자_확인_테스트(){
+        InputView inputView = new InputView();
+        Calculator calculator = new Calculator(inputView);
+        assertSimpleTest(() -> {
+            assertThat(calculator.checkMetacharacters("*")).isTrue();
+        });
+    }
+
+    @Test
+    void 일반_문자_확인_테스트(){
+        InputView inputView = new InputView();
+        Calculator calculator = new Calculator(inputView);
+        assertSimpleTest(() -> {
+            assertThat(calculator.checkMetacharacters("@")).isFalse();
+        });
+    }
+
+    @Test
+    void 문자열_숫자_추출_테스트(){
+        InputView inputView = new InputView();
+        Calculator calculator = new Calculator(inputView);
+        assertSimpleTest(() -> {
+            String[] numbers = calculator.splitString("//;\\n1;2;3;4;5", ";");
+            assertThat(numbers.length).isEqualTo(5);
+        });
+    }
+
+    @Test
+    void 문자열_숫자_파싱_덧셈_테스트(){
+        InputView inputView = new InputView();
+        Calculator calculator = new Calculator(inputView);
+        String[] numbers = new String[]{"1","2","3","4"};
+        assertSimpleTest(() -> {
+            int sum = calculator.parsingAndSum(numbers);
+            assertThat(sum).isEqualTo(10);
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_결과값_일치_테스트(){
+        assertSimpleTest(() -> {
+            run("//^\\n1^2^3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 기본_구분자_결과값_일치_테스트(){
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
     }
 
     @Override
