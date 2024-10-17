2373322e82c00b8c9abb21517f0d6e122fde591e
Jinu Yi <dionisos198@naver.com>
2024-10-17 17:14:23 +0900

    feat(CalculateController): 사용자 입력 구현

--- src/main/java/calculator/controller/CalculateController.java
+++ src/main/java/calculator/controller/CalculateController.java
@@ -1,5 +1,16 @@
 package calculator.controller;
 
+import calculator.service.CalculateService;
+
 //사용자의 Input 과 Output 을 담당하며, calculate service 를 이용할 수 있는 클래스
 public class CalculateController {
+
+    private final CalculateService calculateService = new CalculateService();
+
+    // userInput 을 받아 caculateService 에 넘김.
+    public void insertUserInput(final String userInput) {
+
+        Long calculateResult = calculateService.calculate(userInput);
+
+    }
 }
