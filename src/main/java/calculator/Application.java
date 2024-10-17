c03726cb4243be657062dfea1ebdb7de46b06287
HanJun Lee <137124830+LeeHanJun00@users.noreply.github.com>
2024-10-17 22:32:14 +0900

    refactor: 기능단위로 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,28 +8,54 @@ import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
+        // 숫자입력기능
+        String inputNumber = inputNumber();
+
+        // 구분자 분리 기능
+        String[] splitInputNumber = splitInputNumber(inputNumber);
+
+        // 구분된 문자열이 양수인 문자열인지 체크
+        validateSplitNumber(splitInputNumber);
+
+        // 결과 출력
+        printResult(splitInputNumber);
+
+    }
+
+    private static String inputNumber() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
+        return Console.readLine();
+    }
+
+    private static String[] splitInputNumber(String inputNumber) {
         String defaultDelimiter = ",|:";
         String[] splitInputNumber;
         Pattern pattern = Pattern.compile("^//(.)\\\\n");
-        Matcher matcher = pattern.matcher(input);
+        Matcher matcher = pattern.matcher(inputNumber);
 
         if (matcher.find()) {
+            // 커스텀 구분자가 있을때
             String inputDelimiter = matcher.group(1);
-            input = matcher.replaceFirst("");
+            inputNumber = matcher.replaceFirst("");
             String delimiter = defaultDelimiter + "|" + inputDelimiter;
-            splitInputNumber = input.split(delimiter);
+            splitInputNumber = inputNumber.split(delimiter);
         } else {
-            splitInputNumber = input.split(defaultDelimiter);
+            // 커스텀 구분자가 없을때
+            splitInputNumber = inputNumber.split(defaultDelimiter);
         }
-        
+        return splitInputNumber;
+    }
+
+    private static void validateSplitNumber(String[] splitInputNumber) {
+        System.out.println(Arrays.toString(splitInputNumber));
         for (String number : splitInputNumber) {
             if (!number.matches("\\d+")) {
                 throw new IllegalArgumentException();
             }
         }
+    }
 
+    private static void printResult(String[] splitInputNumber) {
         int result = 0;
         for (String number : splitInputNumber) {
             result += Integer.parseInt(number);
@@ -37,5 +63,4 @@ public class Application {
 
         System.out.println("결과 : " + result);
     }
-
 }
