41a21930c82b22a78f391c7b1bbe9e7c58b3f967
Park Yu Han <pbk2312@naver.com>
2024-10-17 18:36:53 +0900

    fix(view): 수정 및 구분자 오류 메시지 추가

--- src/main/java/calculator/view/ErrorMessage.java
+++ src/main/java/calculator/view/ErrorMessage.java
@@ -5,8 +5,7 @@ public enum ErrorMessage {
     NON_NUMERIC_INPUT_ERROR("숫자가 아닌 문자가 포함되었습니다."),
     NEGATIVE_NUMBER_ERROR("음수는 허용되지 않습니다."),
     INVALID_CUSTOM_DELIMITER_ERROR("잘못된 커스텀 구분자 형식입니다."),
-    INVALID_DELIMITER_FORMAT_ERROR("커스텀 구분자가 잘못되었습니다.");
-
+    MISSING_DELIMITER_ERROR("구분자가 존재하지 않습니다.");
     private final String message;
 
     ErrorMessage(String message) {
