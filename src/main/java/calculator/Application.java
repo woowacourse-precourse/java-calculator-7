f410a504e65efc022cf72cce80636ad7d4169146
yush <yush2202@gmail.com>
2024-10-17 20:16:54 +0900

    feat($Application): 메인 구현
    메인에서 계산기 애플리케이션 동작 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.Controller.CalculatorController;
+
 public class Application {
     public static void main(String[] args) {
-
+        CalculatorController calculator = new CalculatorController();
+        calculator.executeCalculator();
     }
 }
