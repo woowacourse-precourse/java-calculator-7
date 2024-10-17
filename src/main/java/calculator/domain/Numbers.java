748b4e8854d8cc0454b35aa07d6c94b12a554e85
hyosang <billy04100410@gmail.com>
2024-10-17 23:14:42 +0900

    feat(application): add function to convert String numbers to int.

--- src/main/java/calculator/domain/Numbers.java
+++ src/main/java/calculator/domain/Numbers.java
@@ -2,19 +2,33 @@ package calculator.domain;
 
 public class Numbers {
 
-    private String[] numbers;
+    private String[] parts;
 
-    public Numbers(String[] numbers) {
-        this.numbers = numbers;
+    private int[] numbers;
+
+    public Numbers(String[] parts) {
+        this.parts = parts;
+        this.numbers = new int[parts.length];
     }
 
     //구분한 결과의 배열안에 구분자 숫자 외에 문자가 있는지 검증한다.
     public void validateOnlyNumber() {
-        for (String number : numbers) {
-            try {
-                Integer.parseInt(number);
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("계산식의 형태가 올바르지 않습니다.");
+        parseNumbers();
+    }
+
+    // 구분한 문자열들을 int 타입으로 변환, 빈 문자열이면 0으로 변환한다.
+    private void parseNumbers() {
+        for (int i = 0; i < parts.length; i++) {
+            String number = parts[i];
+
+            if (number.isEmpty()) {
+                numbers[i] = 0;
+            } else {
+                try {
+                    numbers[i] = Integer.parseInt(number);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("계산식의 형태가 올바르지 않습니다.");
+                }
             }
         }
     }
