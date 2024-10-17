87723111805b53d121f9f70f0f011b5bc722ed08
김건학 <kkh10002@naver.com>
2024-10-17 21:40:27 +0900

    test(input): 입력한 문자열의 유효성 검증 테스트 코드 추가
    validateInput 테스트:
    - 유효한 입력에 대해 정상적으로 문자열이 반환되는지 검증
    - 유효하지 않은 입력에 대해 IllegalArgumentException을 던지는지 검증
      - null 입력인 경우
      - 빈 문자열인 경우
      - 문자열이 '//'로 시작하지 않는 경우
      - 문자열에 '\n'이 없는 경우

---
+++ src/test/java/calculator/input/stringInputTest.java
@@ -0,0 +1,53 @@
+package calculator.input;
+
+import static calculator.input.StringInput.validateInput;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StringInputTest {
+    @Test
+    @DisplayName("유효한 입력 테스트")
+    void validInput() {
+        String input1 = "//\n123";
+        String input2 = "//test\n12test";
+
+        String result1 = validateInput(input1);
+        String result2 = validateInput(input2);
+
+        assertEquals(input1, result1);
+        assertEquals(input2, result2);
+    }
+
+    @Test
+    @DisplayName("null 입력 테스트")
+    void inputWithNull() {
+        assertThrows(IllegalArgumentException.class, () -> validateInput(null));
+    }
+
+    @Test
+    @DisplayName("빈 입력 테스트")
+    void inputWithEmpty() {
+        String input = "";
+
+        assertThrows(IllegalArgumentException.class, () -> validateInput(input));
+    }
+
+    @Test
+    @DisplayName("'//'로 시작하지 않는 문자열 테스트")
+    void startWithoutSlash() {
+        String input = "test//";
+
+        assertThrows(IllegalArgumentException.class, () -> validateInput(input));
+    }
+
+    @Test
+    @DisplayName("'\\n'이 없는 문자열 테스트")
+    void inputWithoutEndLine() {
+        String input = "//test";
+
+        assertThrows(IllegalArgumentException.class, () -> validateInput(input));
+    }
+}
