fddbfff1f44b61c73bb7dfed87563399b90730f6
KNU-K <knukang334@gmail.com>
2024-10-17 17:36:06 +0900

    test(calculator): 커스텀 구분자 사용 및 예외 처리 테스트 추가
    - 커스텀 구분자를 사용하는 테스트 케이스를 구현하여 올바른 계산 결과를 보장합니다.
    - 음수에 대한 IllegalArgumentException이 발생하는지 확인하기 위한 예외 테스트 케이스를 추가했습니다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -8,7 +9,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
+    @DisplayName("커스텀 구분자 ';' 사용 - 입력값 '1'")
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
@@ -17,10 +20,47 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    @DisplayName("기본 구분자 ','와 ':' 사용 - 입력값 '1,2,3:4'")
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2,3:4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    @DisplayName("기본 구분자 ','와 ':' 사용 - 입력값 '1'")
+    void 기본_구분자_사용_2() {
+        assertSimpleTest(() -> {
+            run("1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    @DisplayName("음수 입력 예외 처리 - 입력값 '-1,2,3'")
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("잘못된 커스텀 구분자 예외 처리 - 입력값 '//;n-1,2,3'")
+    void 예외_테스트_2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;n-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("잘못된 커스텀 구분자 예외 처리 - 입력값 ';\\n-1,2,3'")
+    void 예외_테스트_3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(";\\n-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
