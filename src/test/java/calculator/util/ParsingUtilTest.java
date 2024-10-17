5bd7848702efe7c46e00dde4e546beb727b80e79
sanghee0820 <lsh277604@gmail.com>
2024-10-17 12:20:32 +0900

    test: input이 null일경우 오류 발생 테스트

---
+++ src/test/java/calculator/util/ParsingUtilTest.java
@@ -0,0 +1,22 @@
+package calculator.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import calculator.exception.CalculatorError;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ParsingUtilTest {
+
+    @Test
+    @DisplayName("입력 값이 null일 경우 IllegalArgumentException 발생")
+    void parseWithDefault_NullOrEmpty() {
+        // given
+        final String input = null;
+
+        // when & then
+        assertThatThrownBy(() -> ParsingUtil.parseWithDefault(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CalculatorError.INPUT_IS_NULL_OR_EMPTY.getMessage());
+    }
+}
