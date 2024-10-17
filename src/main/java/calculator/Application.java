cf91ddfeab874cf96c653f3061632bffd947227f
sieunp06 <psieun0606@gmail.com>
2024-10-17 12:22:26 +0900

    feat(Application.java): 문자열 덧셈 계산기 구현
    미션 요구 사항에 맞춰 문자열 덧셈 계산기를 구현함. 이후 리팩토링과 테스트 코드를 만들 예정.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,44 @@
 package calculator;
 
+import java.util.Arrays;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        var text = readLine();
+        var regex = ",|:";
+        String[] nums;
+        if (text.indexOf("//") != -1) {
+            if (text.indexOf("\\n") == -1) {
+                throw new IllegalArgumentException();
+            }
+            var custom = text.substring(2, text.indexOf("\\n"));
+            if (!custom.equals("")) {
+                regex += "|" + custom;
+            }
+            nums = text.substring(text.indexOf("\\n") + 2, text.length()).split(regex);
+        } else {
+            nums = text.split(regex);
+        }
+
+        System.out.println(Arrays.toString(nums));
+
+        var total = 0;
+        for (String num_text : nums) {
+            int num;
+            try {
+                num = Integer.parseInt(num_text);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException();
+            }
+            if (num < 0) {
+                throw new IllegalArgumentException();
+            }
+            total += num;
+        }
+        System.out.println("결과 : " + total);
     }
 }
