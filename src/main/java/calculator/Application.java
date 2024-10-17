3a062143b5bd80d8dfa3ae3e0cd8fa166ef98f03
hongkikii <hyangmeeh@icloud.com>
2024-10-17 18:05:23 +0900

    feat(Application): 덧셈 결과 출력 기능(5) 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,23 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.math.BigDecimal;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = Console.readLine();
+        InputParser inputParser = new InputParser(input);
+
+        Separator separator = new Separator();
+        separator.save(inputParser.getSeparatorCandidates());
+
+        OperandManager operandManager = new OperandManager();
+        operandManager.save(inputParser.getOperandCandidates(), separator.getSeparators());
+
+        Operator operator = new Operator();
+        BigDecimal result = operator.add(operandManager.getOperands());
+
+        System.out.println("결과 : " + result);
     }
 }
