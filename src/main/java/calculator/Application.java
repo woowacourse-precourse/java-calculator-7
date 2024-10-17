8b3225e225002c6004a75f69584c697ff1d8e6be
MinGwan Kim <fpem3309@gmail.com>
2024-10-17 22:34:35 +0900

    refactor: 구분자 사이의 값 검증 및 합산 로직 메서드 추출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,21 +10,26 @@ public class Application {
         int sum = 0;
 
         if (!userText.isEmpty() && !userText.isBlank()) {
-            String[] splitTexts = userText.split(",|:");
-
-            for (String text : splitTexts) {
-                try {
-                    int numberText = Integer.parseInt(text);    // 구분자 사이의 값이 숫자가 아니라면 예외 발생(NumberFormatException)
-                    if (numberText < 0) throw new IllegalArgumentException();   // 구분자 사이의 숫자가 0보다 작으면 예외 발생
-                    sum += numberText;
-                } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException();   // IllegalArgumentException 예외로 발생
-                }
-            }
+            sum = generateSum(userText, sum);
         }
 
         System.out.println("결과 : " + sum);
 
+    }
+
+    private static int generateSum(String userText, int sum) {
+        String[] splitTexts = userText.split(",|:");
+
+        for (String text : splitTexts) {
+            try {
+                int numberText = Integer.parseInt(text);    // 구분자 사이의 값이 숫자가 아니라면 예외 발생(NumberFormatException)
+                if (numberText < 0) throw new IllegalArgumentException();   // 구분자 사이의 숫자가 0보다 작으면 예외 발생
+                sum += numberText;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException();   // IllegalArgumentException 예외로 발생
+            }
+        }
+        return sum;
     }
 
 }
