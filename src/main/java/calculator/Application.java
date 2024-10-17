7fdd0688f2128e43493cdc6072761733dcd4a99f
seonmin <w.hand715@gmail.com>
2024-10-17 11:54:05 +0900

    feat: add Converter that convert input data
    Converter convert correct input data to String array that consist of only number like "19"

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,19 +2,22 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
         String data = Console.readLine();
-        Result result = Validator.validate(data);
+        Result validation = Validator.validate(data);
 
-        if (!result.isCorrect) {
-            raiseException(result.message, data);
+        if (!validation.isCorrect) {
+            raiseException(validation.message, data);
         }
 
-        System.out.println(data);
+        String[] result = Converter.convert(data);
+        System.out.println(Arrays.toString(result));
     }
 
 
