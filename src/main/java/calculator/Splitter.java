955cb2f9c6dd627b9aadf33cae42372bec12fd29
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-17 22:33:35 +0900

    feat(Spliter): 기본 구분자로 입력(문자열) 쪼개기

--- src/main/java/calculator/Splitter.java
+++ src/main/java/calculator/Splitter.java
@@ -1,4 +1,13 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Splitter {
+
+    public List<String> splitByBasicSeparator(String value) {
+        return Arrays.stream(value.split("[,:]")).toList();
+
+    }
+    
 }
