42078675dfb26a8e8974c550788294f936170401
eunho <eunho1032@gmail.com>
2024-10-17 21:58:07 +0900

    feat(CustomDelimiter): 커스텀 구분 메서드 보완

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,11 +7,13 @@ public class Application {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("덧셈할 문자열을 입력해 주세요.");
 		String input = scanner.nextLine();
-		String[] nums = input.split("[,;]");
 		int result = 0;
 
+		String customDelimiter = CustomDelimiter.findDelimiter(input);
+		int delmiiterEndIdx = CustomDelimiter.findDelimiterEndIndex(input);
+		String[] nums = StringSplit.stringSplit(input, customDelimiter, delmiiterEndIdx);
 		for (String num : nums)
-			result += Integer.parseInt(num);
+		result += Integer.parseInt(num); 
 		System.out.println("결과 : " + result);
 	}
 }
--- src/main/java/calculator/CustomDelimiter.java
+++ src/main/java/calculator/CustomDelimiter.java
@@ -3,14 +3,20 @@ package calculator;
 public class CustomDelimiter {
 	public static String findDelimiter(String input) {
 		String defaultDelimiter = ",:";
-		String delimiterPrefix = "//";
-		String delimiterSuffix = "\n";
+		int beginIndex = input.indexOf("//");
+		int endIndex = input.indexOf("\\n");
 
-		int beginIndex = input.indexOf(delimiterPrefix);
-		int endIndex = input.indexOf(delimiterSuffix);
-		if (beginIndex == -1 || endIndex == -1) {
+		if (beginIndex == -1) {
 			return defaultDelimiter;
 		}
+		if (endIndex == -1) {
+			throw new IllegalArgumentException("Invalid input");
+		}
 		return input.substring(beginIndex + 2, endIndex);
+	}
+
+	public static int findDelimiterEndIndex(String input) {
+		int endIndex = input.indexOf("\\n");
+		return endIndex + 2; // 커스텀 구분자 지정 문자를 뺀 끝 인덱스 반환
 	}
 }
