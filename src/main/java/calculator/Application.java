a5278b16ad0b7fcd96fcd5150b36011458181efb
softmoca <acyc0909@gmail.com>
2024-10-17 10:53:42 +0900

    feat: 기본 구분자(쉼표, 콜론)으로 문자열을 분리하여 덧셈 기능 구현 - 쉼표와 콜론을 기준으로 문자열을 분리한다. - 분리된 숫자들을 더한 결과를 반환한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,7 +6,30 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        System.out.println("입력값: " + input);
+
+        int result = calculateSum(input);
+        System.out.println("결과 : " + result);
+
 
     }
+
+    private static int calculateSum(String input) {
+
+        String[] tokens = input.split("[,:]");
+        int sum = 0;
+        for (String token : tokens) {
+            sum += Integer.parseInt(token);
+        }
+        return sum;
+    }
+
+
+
+
+
+
+
+
+
+
 }
