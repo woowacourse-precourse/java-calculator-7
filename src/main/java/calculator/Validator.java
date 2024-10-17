fd872245d6601fa00c2d989a12ea5b1f2df23f2f
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-17 16:30:19 +0900

    feat(Validator): 문자열 유효성 검사2 - 커스텀 구분자 선언 형식 검사

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -4,6 +4,7 @@ public class Validator {
 
     public static void validate(String value) {
         checkBeginAndEndOfValue(value);
+        checkCustomSeparatorFormat(value);
     }
 
     //문자열의 시작과 끝 확인
@@ -17,6 +18,24 @@ public class Validator {
         if (!value.endsWith("\n") && !isNum(value.charAt(value.length() - 1))) {
             throw new IllegalArgumentException("문자열의 끝은 커스텀 구분자 종료 선언 또는 양수여야 합니다.");
         }
+    }
+
+
+    //문자열에서 커스텀 구분자 형식 유효성 검사하기
+    private static void checkCustomSeparatorFormat(String value) {
+        if (!value.contains("//")) {
+            return;
+        }
+
+        int start = value.indexOf("//");
+        int end = value.indexOf("\n");
+
+        // 커스텀 구분자 선언이 //후에 \n인 형태인지,
+        if (start > end) {
+            throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다.");
+        }
+
+        checkCustomSeparatorFormat(value.substring(end + 2));
     }
 
 
