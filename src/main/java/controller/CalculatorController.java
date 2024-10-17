240c4d275ed0bd5a28f238f6a3b072f716b99419
aaahyunseo <hs030608@naver.com>
2024-10-17 23:31:15 +0900

    fix(CalculatorController): 문자열 계산기 실행 controller 로직 정리

---
+++ src/main/java/controller/CalculatorController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import model.Calculator;
+import model.Tokenization;
+import view.InputView;
+import view.OutputView;
+
+public class CalculatorController {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static final Tokenization tokenization = new Tokenization();
+    private static final Calculator calculator = new Calculator();
+
+
+    /**
+     * 문자열 계산기를 실행 함수
+     * **/
+    public static void run(){
+        String str = inputView.input();
+        int result = 0;
+        // 빈 문자열이면 "0"을 반환
+        if(!str.isEmpty()) {
+            result = calculator.add(tokenization.operator(str));
+        }
+        outputView.output(result);
+    }
+}
