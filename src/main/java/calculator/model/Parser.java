901bbb10863f6beac2961b9a7e56f3692d0bc5dc
dradnats1012 <herjebi1012@gmail.com>
2024-10-17 21:57:00 +0900

    fix: Parser 클래스 이스케이프 문자 수정

--- src/main/java/calculator/model/Parser.java
+++ src/main/java/calculator/model/Parser.java
@@ -6,31 +6,34 @@ import java.util.regex.Pattern;
 
 public class Parser {
 
-    public List<String> parseInput(String input){
-        if(input.startsWith("//")){
+    public List<String> parseInput(String input) {
+        if (input.startsWith("//")) {
             return parseCustomSeparator(input);
         }
 
         return parseBasicSeparator(input);
     }
 
-    private List<String> parseBasicSeparator(String input){
+    private List<String> parseBasicSeparator(String input) {
         List<String> splitString = List.of(input.split(",|:"));
 
         return splitString;
     }
 
-    private List<String> parseCustomSeparator(String input){
-        Pattern pattern = Pattern.compile("[//](.*?)[\\n]");
+    private List<String> parseCustomSeparator(String input) {
+        Pattern pattern = Pattern.compile("//(.*?)\\\\n");
         Matcher matcher = pattern.matcher(input);
 
-        String realInput = input.substring(input.lastIndexOf("\\n"));
-        String customSeparator = matcher.group();
+        String customSeparator = "";
+        if (matcher.find()) {
+            customSeparator = matcher.group(1);
+        }
+        String realInput = input.substring(input.lastIndexOf("\\n") + 2);
 
         return parseCustom(realInput, customSeparator);
     }
 
-    public List<String> parseCustom(String input, String custom){
+    public List<String> parseCustom(String input, String custom) {
         String separator = String.format(",|:|%s", custom);
         List<String> splitString = List.of(input.split(separator));
 
