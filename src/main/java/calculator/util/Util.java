2314189c87ee6e325785127a512545629e7c4ca8
mjw3723 <mjw3723@naver.com>
2024-10-18 06:26:50 +0900

    feat : 추출한 입력값이 숫자인지 검증 구현

--- src/main/java/calculator/util/Util.java
+++ src/main/java/calculator/util/Util.java
@@ -4,13 +4,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static calculator.util.Validator.validCustomSeparator;
+import static calculator.util.Validator.validExtractList;
+
 
 public class Util {
     private static final String BASIC_SEPARATOR = "[,:]";
     private static final String CUSTOM_SEPARATOR_REGEX = "//(.)\\\\n(.*)";
 
     public void extract(String text){
-        separatorExtract(text);
+        String[] extractList = separatorExtract(text);
+        validExtractList(extractList);
     }
     private String[] separatorExtract(String text){
         if(text.startsWith("//")){
--- src/main/java/calculator/util/Validator.java
+++ src/main/java/calculator/util/Validator.java
@@ -7,6 +7,20 @@ public class Validator {
 
     private static final String CUSTOM_SEPARATOR_REGEX = "//(.)\\\\n(.*)";
 
+    public static void validExtractList(String[] inputExtractText){
+        for(String text : inputExtractText){
+            validNumberFormat(text);
+        }
+    }
+
+    private static void validNumberFormat(String text){
+        try{
+            int number = Integer.parseInt(text);
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static void validCustomSeparator(String text){
         Matcher matcher = Pattern.compile(CUSTOM_SEPARATOR_REGEX).matcher(text);
         if(!matcher.matches()||matcher.group(1).isEmpty()){
