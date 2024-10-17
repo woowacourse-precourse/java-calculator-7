465e8483d024cf3faa82199c97005db4fc103dd5
tentenball <lth8905@gmail.com>
2024-10-18 02:35:35 +0900

    refactor: 중복 상수 정리

--- src/main/java/calculator/constant/DrawConstant.java
+++ src/main/java/calculator/constant/DrawConstant.java
@@ -1,11 +1,12 @@
 package calculator.constant;
 
 public class DrawConstant {
-	public static int EXTRACT_START_INDEX = 2;
+	public static int EXTRACT_START_INDEX, N_INDEX = 2;
 	public static String CUSTOM_DELIMITER_START_SIGN = "//";
 	public static String CUSTOM_DELIMITER_END_SIGN = "\\n";
-	public static int BACK_SLASH_INDEX = 1;
-	public static int N_INDEX = 2;
+	public static int BACK_SLASH_INDEX, NEXT_INDEX = 1;
 	public static int CUSTOM_DELIMITER_INDEX = 0;
-	public static String CUSTOM_DELIMITER_REGEX = "[a-zA-Z]|[^a-zA-Z0-9]";
+	public static String CUSTOM_DELIMITER_REGEX = "[^a-zA-Z0-9가-힣\\s]";
+	public static char BACK_SLASH = '\\';
+	public static char N = 'n';
 }
