4cf3f9aa0c1f7d9424f5f4af2da932ba90e39438
Sy P <yongsa0221@gmail.com>
2024-10-17 12:21:49 +0900

    test: 커스텀 구분자 존재시 반환하는 기능 테스트 추가

--- src/test/java/calculator/CustomSeparatorManagerTest.java
+++ src/test/java/calculator/CustomSeparatorManagerTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,12 +10,12 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class CustomSeparatorCheckerTest {
+class CustomSeparatorManagerTest {
 
     @ParameterizedTest
     @DisplayName("//로 시작하고 \\n으로 끝나는 문자열 안에 커스텀 구분자가 있는지 확인한다")
     @MethodSource("generateCustomSeparatorStringData")
-    void hasCustomSeparator(String inputString, boolean expected) {
+    void testHasCustomSeparator(String inputString, boolean expected) {
         assertThat(CustomSeparatorManager.hasCustomSeparator(inputString)).isEqualTo(expected);
     }
 
@@ -28,6 +29,21 @@ class CustomSeparatorCheckerTest {
                 Arguments.of("//customSeparator\\n", true),
                 Arguments.of("// \\n", false),
                 Arguments.of("a//d\\n", false)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("//와 \\n 사이의 커스텀 구분자를 반환하는지 확인")
+    @MethodSource("generateValidCustomSeparatorData")
+    void testGetCustomSeparator(String inputString, Separator expectedCustomSeparator) {
+        Separator actualSeparator = CustomSeparatorManager.getCustomSeparator(inputString);
+        assertThat(actualSeparator).isEqualTo(expectedCustomSeparator);
+    }
+
+    static Stream<Arguments> generateValidCustomSeparatorData() {
+        return Stream.of(
+                Arguments.of("//d\\n", new Separator("d")),
+                Arguments.of("//customSeparator\\n", new Separator("customSeparator"))
         );
     }
 }
