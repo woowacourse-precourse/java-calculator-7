e9162004115345d47d260a2665ffab078edcc775
jyh1108 <kobcom12@naver.com>
2024-10-17 10:37:49 +0900

    feat : 5. //;\n 커스텀구분자 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,11 +10,17 @@ public class Application {
         String input = Console.readLine(); //입력받을 문자열
 
         int sum = 0;   //합계를 저장할 변수
+        int startindex = 0; // 시작인덱스
+        char custom = 0; //커스텀을 저장할 인덱스
         StringBuilder number = new StringBuilder(); //임시로 숫자를 저장할 변수
 
-        for (int i = 0; i < input.length(); i++) { // input 문자열의 길이만큼 반복
+        if (input.startsWith("//") && input.charAt(3) == '\\' && input.charAt(4) == 'n') {
+            custom = input.charAt(2); //커스텀구분자를 지정
+            startindex = 5;  //커스텀구분자가 있을시 시작인덱스를 //;\n 을 스킵
+        }
+        for (int i = startindex; i < input.length(); i++) { // input 문자열의 길이만큼 반복
             char a = input.charAt(i);  // 현재 문자를 하나씩 가져온다
-            if (a == ',' || a == ':') { // 구분자를 만났을 때
+            if (a == ',' || a == ':' || a == custom) { // 구분자를 만났을 때
                 sum += Integer.parseInt(number.toString()); // 지금까지 저장한 숫자를 정수로 변환 후 합계에 더한다
                 number.setLength(0);   // 숫자를 저장하던 임시 변수를 초기화
             } else {
