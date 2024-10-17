b69811fcf5bbb314080b4100c7e9167a6d479ce0
lchyeong <lchyeong@gmail.com>
2024-10-17 17:17:10 +0900

    feat(NumberParser): 음수 체크 및 음수 검증 로직 추가
    - NEGATIVE_PATTERN로 체크 후 음수 판별
    - replaceNumber 메서드 내 음수 체크 함수 추가

--- src/main/java/calculator/NumberParser.java
+++ src/main/java/calculator/NumberParser.java
@@ -1,8 +1,19 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NumberParser {
+
+    public static final String NEGATIVE_PATTERN = "-\\d+";
+
     public String[] replaceNumber(String[] inputArr) {
         for (int i = 0; i < inputArr.length; i++) {
+
+            if (checkNegative(inputArr[i])) {
+                throw new IllegalArgumentException();
+            }
+
             inputArr[i] = inputArr[i].replaceAll("[^0-9]", "");
         }
         return inputArr;
@@ -16,5 +27,12 @@ public class NumberParser {
             }
         }
         return sum;
+    }
+
+    public boolean checkNegative(String input) {
+        Pattern pattern = Pattern.compile(NEGATIVE_PATTERN);
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.find();
     }
 }
