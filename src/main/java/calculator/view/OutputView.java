db0a0d642418231bf0c0b3c783b1e186eabf35a5
hmlssghm <kwonse326@gmail.com>
2024-10-17 23:30:17 +0900

    feat: OutputView 작성

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,4 +1,12 @@
 package calculator.view;
 
 public class OutputView {
+    public void askInput(){
+        System.out.print("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public void printAnswer(int answer){
+        System.out.print("결과 : " + answer);
+    }
+
 }
