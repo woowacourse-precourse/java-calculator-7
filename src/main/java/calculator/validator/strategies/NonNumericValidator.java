01b9476961265d80c9910d5139f2c318e41fd235
Park Yu Han <pbk2312@naver.com>
2024-10-17 18:31:11 +0900

    fix(validator): 빈 문자열 처리 로직 수정

--- src/main/java/calculator/validator/strategies/NonNumericValidator.java
+++ src/main/java/calculator/validator/strategies/NonNumericValidator.java
@@ -17,6 +17,7 @@ public class NonNumericValidator implements ValidationStrategy {
     // 입력이 숫자와 구분자로만 이루어져 있는지 확인
     private boolean containsOnlyValidCharacters(String input) {
         String[] numbers = DelimiterUtils.splitInput(input);
+
         // 숫자 배열을 확인하여 유효한지 판단
         for (String number : numbers) {
             if (!isValidNumber(number)) {
@@ -28,7 +29,11 @@ public class NonNumericValidator implements ValidationStrategy {
 
     // 숫자가 유효한지 확인하는 헬퍼 메서드
     private boolean isValidNumber(String number) {
-        return number.matches("[0-9]*"); // 숫자만 포함된 문자열인지 확인
+        // 빈 문자열일 경우도 유효한 것으로 간주
+        if (number.isEmpty()) {
+            return true;
+        }
+        return number.matches("[0-9]+"); // 숫자만 포함된 문자열인지 확인
     }
 
 }
