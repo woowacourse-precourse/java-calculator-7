ccaeb6b3f73f65dda4c21aadb4454b1f805f02a6
EHCTH <lees90896@gmail.com>
2024-10-17 18:08:04 +0900

    feat(CalculatorController): 컨트롤러 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,37 @@
 package calculator.controller;
 
+import calculator.number.domain.Number;
+import calculator.number.service.PositiveNumberGenerator;
+import calculator.sentence.domain.Sentence;
+import calculator.separator.domain.Separator;
+import calculator.service.calculator.CalculatorProcess;
+import calculator.separator.service.SeparatorGenerator;
+import calculator.view.input.handler.InputHandlerService;
+import calculator.view.output.ResultService;
+
 public class CalculatorController {
+    private final SeparatorGenerator separatorGenerator;
+    private final PositiveNumberGenerator positiveNumberGenerator;
+    private final CalculatorProcess calculatorProcess;
+    private final InputHandlerService inputHandlerService;
+    private final ResultService resultService;
+    public CalculatorController(
+            SeparatorGenerator separatorGenerator,
+            PositiveNumberGenerator positiveNumberGenerator,
+            CalculatorProcess calculatorProcess,
+                                InputHandlerService inputHandlerService,
+                                ResultService resultService) {
+        this.separatorGenerator = separatorGenerator;
+        this.positiveNumberGenerator = positiveNumberGenerator;
+        this.calculatorProcess = calculatorProcess;
+        this.inputHandlerService = inputHandlerService;
+        this.resultService = resultService;
+    }
+    public void run() {
+        Sentence sentence = inputHandlerService.receive(Sentence::new);
+        Separator separator = separatorGenerator.create(sentence);
+        Number positiveNumber =  positiveNumberGenerator.create(sentence, separator);
+        long result = calculatorProcess.sum(positiveNumber);
+        resultService.result(result);
+    }
 }
