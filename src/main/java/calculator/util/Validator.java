15bd5f58069613b45bb173876889ee3e611f11ba
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-17 23:50:56 +0900

    refactor(Validator): separate logic for custom delimiter validation and storage

--- src/main/java/calculator/util/Validator.java
+++ src/main/java/calculator/util/Validator.java
@@ -1,6 +1,7 @@
 package calculator.util;
 
-import calculator.message.Delimiter;
+import static calculator.message.Delimiter.CUSTOM_DELIMITER;
+
 import calculator.message.ErrorMessage;
 import java.util.List;
 
@@ -31,10 +32,26 @@ public class Validator {
     // 커스텀 구분자를 입력하는 부분은 문자열 검증 로직에서 제외
     public static String validateCustomDelimiter(String str) {
         if (str.length() >= 5 && str.startsWith("//") && str.charAt(3) == '\\' && str.charAt(4) == 'n') {
-            Delimiter.CUSTOM_DELIMITER = String.valueOf(str.charAt(2));
+            setCustomDelimiter(str);
+            validateCustomDelimiterIsNotNumber();
             return str.substring(5);
         }
 
         return str;
+    }
+
+    // 커스텀 구분자가 숫자라면 에러 반환
+    private static void validateCustomDelimiterIsNotNumber() {
+        try {
+            Integer.parseInt(CUSTOM_DELIMITER);
+            throw new IllegalArgumentException("커스텀 구분자는 숫자여서는 안 됩니다.");
+        } catch (NumberFormatException e) {
+
+        }
+    }
+
+    // 커스텀 구분자를 입력하는 형식이 감지되면 인덱스 2번에서 추출하여 저장
+    private static void setCustomDelimiter(String str) {
+        CUSTOM_DELIMITER = String.valueOf(str.charAt(2));
     }
 }
