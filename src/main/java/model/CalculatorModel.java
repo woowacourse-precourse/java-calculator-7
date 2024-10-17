cf3a3e204b53542aa2141284bebf372550e8b1b3
daxxn <dahyeonhan4529@gmail.com>
2024-10-17 15:22:28 +0900

    feat(model): add methods to handle custom delimiters
    - Implement checkCustomDelimiter to identify delimiter
    - Implement setDelimiter to update the delimiter list
    - Change delimiterList type from ArrayList<Character> to ArrayList<String>

--- src/main/java/model/CalculatorModel.java
+++ src/main/java/model/CalculatorModel.java
@@ -1,14 +1,35 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CalculatorModel {
-    private ArrayList<Character> delimiterList;
+    private ArrayList<String> delimiterList;
+    private final String customDelimiterRegex = "//(.)\n";
 
     public CalculatorModel() {
         delimiterList = new ArrayList<>();
-        delimiterList.add(',');
-        delimiterList.add(':');
+        delimiterList.add(",");
+        delimiterList.add(":");
     }
 
+    //초기 구분자 설정하기
+    public String checkCustomDelimiter(String input) {
+        Pattern pattern = Pattern.compile(customDelimiterRegex + "(.*)");
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.matches()) {
+            String delimiter = matcher.group(1);
+            setDelimiter(delimiter);
+            return matcher.group(2);
+        } else {
+            return input;
+        }
+    }
+
+    //구분자 추가하기
+    public void setDelimiter(String delimiter) {
+        delimiterList.add(delimiter);
+    }
 }
