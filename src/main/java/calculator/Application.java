d48e68f46bf352fb5f8d261bdd47607f03424526
Minseok <saltishDev@gmail.com>
2024-10-17 17:20:29 +0900

    feat: 입력받은 문자열의 종류를 판별하는 기능 구현
    입력받은 문자열이 커스텀 구분자를 고려해야 하는 문자열인지 판별

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,4 +8,14 @@ public class Application {
         String inputString = Console.readLine();
         System.out.printf("입력받은 문자열 : %s\n", inputString);
     }
+
+    private static boolean isStringWithCustomDelimiter(String str) {
+        if (str.length() < 5) {
+            return false;
+        }
+        String prefix = str.substring(0, 2);
+        String suffix = str.substring(3, 5);
+
+        return prefix.equals("//") && suffix.equals("\\n");
+    }
 }
