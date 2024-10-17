48de73b62609d8058900f32e152d6c0e6150b963
Chaeyoung714 <202102760@hufs.ac.kr>
2024-10-17 14:46:27 +0900

    feat : 2.1.3 커스텀 구분자 입력 종료 기능 구현
    model/RegDelimiter : findCustomDelimiter 완성, checkCustomDelimiterEnd, validateIfCustomDeliEnds 메서드 추가
    test/model/RegDelimiterTest : 커스텀 구분자 종료 문자 입력 관련 테스트 추가, 통과

--- src/main/java/calculator/model/RegDelimiter.java
+++ src/main/java/calculator/model/RegDelimiter.java
@@ -1,7 +1,5 @@
 package calculator.model;
 
-import java.util.List;
-import java.util.SimpleTimeZone;
 
 public class RegDelimiter {
     public boolean includesCustomDelimiter(String value) {
@@ -13,25 +11,48 @@ public class RegDelimiter {
         return false;
     }
 
-    public String findCustomDelimiter(String value) {
+    public int findCustomDelimiter(String value) {
         String customDeli = "";
         int valueLength = value.length();
+        int customDeliEndIdx = -1;
 
         for (int i=2; i<valueLength; i++) {
+            if (checkCustomDelimiterEnd(i, value, valueLength)) {
+                customDeliEndIdx = i + 1;
+                break;
+            }
             customDeli += value.substring(i, i+1);
         }
-        validateCustomDeli(customDeli);
-        return customDeli;
+
+        validateIfCustomDeliEnds(customDeliEndIdx);
+        validateCustomDelimiter(customDeli);
+
+        return customDeliEndIdx;
     }
 
-    public void validateCustomDeli(String customDeli) {
+    private boolean checkCustomDelimiterEnd(int idx, String value, int valueLength) {
+        if (idx > 2 && idx < valueLength - 1) {
+            if (value.charAt(idx) == '\\' && value.charAt(idx + 1) == 'n') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void validateIfCustomDeliEnds(int customDeliEndIdx) {
+        if (customDeliEndIdx == -1) {
+            throw new IllegalArgumentException("잘못된 입력값입니다. '\\n'을 입력해 커스텀 구분자 입력을 마쳐주세요.");
+        }
+    }
+
+    public void validateCustomDelimiter(String customDeli) {
         String[] reservedWords = new String[]{"//", "\\n", "-", ",", ":"};
         String numberRegex = ".*[0-9].*";
 
         for (String resWord : reservedWords) {
             if (customDeli.contains(resWord)
             || customDeli.matches(numberRegex)) {
-                throw new IllegalArgumentException("잘못된 구분자입니다. // \\n - , : 또는 숫자를 제외한 문자를 입력해주세요.");
+                throw new IllegalArgumentException("잘못된 구분자입니다. '//', '\\n', '-', ',', ':' 또는 숫자를 제외한 문자를 입력해주세요.");
             }
         }
     }
--- src/test/java/calculator/model/RegDelimiterTest.java
+++ src/test/java/calculator/model/RegDelimiterTest.java
@@ -8,10 +8,10 @@ import org.junit.jupiter.api.Test;
 public class RegDelimiterTest {
 
     @Test
-    void 커스텀_구분자가_있으면_true_반환() {
+    void 커스텀_구분자_시작_문자_true_반환() {
         //Given
         RegDelimiter regDelimiter = new RegDelimiter();
-        String testString1 = "//%\n1%2";
+        String testString1 = "//%\\n1%2";
         String testString2 = "//";
 
         //When
@@ -24,7 +24,7 @@ public class RegDelimiterTest {
     }
 
     @Test
-    void 커스텀_구분자가_없으면_false_반환() {
+    void 커스텀_구분자가_시작_문자_없으면_false_반환() {
         RegDelimiter regDelimiter = new RegDelimiter();
         String testString = "1,2";
 
@@ -33,15 +33,19 @@ public class RegDelimiterTest {
         assertThat(includesCustomDeli).isFalse();
     }
 
-    @Test
-    void 커스텀_구분자_입력() {
-        RegDelimiter regDelimiter = new RegDelimiter();
-        String testString = "//%%%";
-
-        String customDeli = regDelimiter.findCustomDelimiter(testString);
-
-        assertThat(customDeli).isEqualTo("%%%");
-    }
+    /**
+     * Deprecated
+     * 함수 구현 중간 단계에서 테스트한 메서드로, 함수의 최종 반환값과 일치하지 않아 주석 처리함.
+     */
+//    @Test
+//    void 커스텀_구분자_입력() {
+//        RegDelimiter regDelimiter = new RegDelimiter();
+//        String testString = "//%%%";
+//
+//        String customDeli = regDelimiter.findCustomDelimiter(testString);
+//
+//        assertThat(customDeli).isEqualTo("%%%");
+//    }
 
     @Test
     void 잘못된_커스텀_구분자_입력_시_예외_발생() {
@@ -52,6 +56,25 @@ public class RegDelimiterTest {
         assertThatThrownBy(() -> regDelimiter.findCustomDelimiter(testString1))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> regDelimiter.findCustomDelimiter(testString2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 커스텀_구분자_종료_문자_입력() {
+        RegDelimiter regDelimiter = new RegDelimiter();
+        String testString = "//%%\\n1%2,3";
+
+        int customDeliEndIdx = regDelimiter.findCustomDelimiter(testString);
+
+        assertThat(customDeliEndIdx).isEqualTo(5);
+    }
+
+    @Test
+    void 커스텀_구분자_종료_문자_없으면_예외_발생() {
+        RegDelimiter regDelimiter = new RegDelimiter();
+        String testString = "//%%1%2,3";
+
+        assertThatThrownBy(() -> regDelimiter.findCustomDelimiter(testString))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
