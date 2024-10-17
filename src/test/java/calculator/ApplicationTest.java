658017a2c808cd960e71493cefe91c5b5be4aa2f
bk <naturalbb360@gmail.com>
2024-10-17 13:48:06 +0900

    feat(ApplicationTest): 커스텀 구분자 위치, 개수 테스트 구현

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,13 +1,14 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -17,10 +18,26 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_위치_테스트() {
+        assertSimpleTest(() -> {
+            run("12;3//;\\n");
+            assertThat(output()).contains("커스텀 구분자는 문자열 처음에서만 추가할 수 있습니다.");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_개수_테스트() {
+        assertSimpleTest(() -> {
+            run("//!@;\\n");
+            assertThat(output()).contains("한개의 커스텀 구분자만 추가할 수 있습니다.");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
