ce1338b7a3b14188e73c246c265cb6957d89f178
kellyihyeon <kellykang13@gmail.com>
2024-10-17 17:32:55 +0900

    feat: 숫자 두 개를 더하는 기능 구현

--- src/main/java/calculator/AddCalculator.java
+++ src/main/java/calculator/AddCalculator.java
@@ -2,7 +2,17 @@ package calculator;
 
 public class AddCalculator {
 
+    private static Double sum;
+
     public static double add(double number) {
         return number;
+    }
+
+    public static void add(double a, double b) {
+        sum = a + b;
+    }
+
+    public static double sum() {
+        return sum;
     }
 }
--- src/test/java/calculator/AddCalculatorTest.java
+++ src/test/java/calculator/AddCalculatorTest.java
@@ -6,6 +6,16 @@ import org.junit.jupiter.api.Test;
 public class AddCalculatorTest {
 
     @Test
+    void 숫자_두_개를_더할_수_있다() {
+        double a = 1;
+        double b = 3;
+
+        AddCalculator.add(a, b);
+
+        assertEquals(4, AddCalculator.sum());
+    }
+
+    @Test
     void 숫자가_한_개면_해당_숫자를_결과_값으로_반환한다() {
         double number = 1;
         double result = AddCalculator.add(number);
