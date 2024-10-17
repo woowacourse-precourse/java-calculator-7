9ad64d6e24d41f7ccfa07ef5d8a672fc957cc161
rladmstn <oz33177351@gmail.com>
2024-10-17 15:46:31 +0900

    test(ApplicationTest) : input 예외 테스트 작성
    기능 설계에서 작성한 input 예외에 대한 테스트 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,7 +1,11 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.ValueSources;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    @DisplayName("커스텀 구분자 사용 : 성공")
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
@@ -16,10 +21,22 @@ class ApplicationTest extends NsTest {
         });
     }
 
-    @Test
-    void 예외_테스트() {
+    @ParameterizedTest
+    @ValueSource( strings = {
+            "1=3:2",
+            "A,2:1",
+            "+1,1:1",
+            "-1:1,1",
+            "/a\n1a3a2",
+            "//a\n10a3:2",
+            "//a\n10a3,2",
+            "//\n132",
+            "//&\n1|3&2"
+    })
+    @DisplayName("잘못된 input 입력 시, 예외 발생")
+    void inputException(String input) {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
+            assertThatThrownBy(() -> runException(input))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
