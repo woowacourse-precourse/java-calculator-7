55619c9323258457ebdafc5ea32d95828b93d4b5
박철현 <park82602@gmail.com>
2024-10-17 19:22:14 +0900

    style : DelimiterParser 클래스의 static 메서드를 변경함으로써 생기는 문제 해결
    - DelimiterParser 클래스의 static 메서드 변경으로 인한 문제점을 의존성 주입으로 해결

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,16 +3,16 @@ package calculator;
 import java.util.List;
 
 public class Calculator {
-
+    private DelimiterParser delimiterParser = new DelimiterParser();
     public int calculate(String input) {
         if (input.isEmpty()) {
             return 0;
         }
-        if (DelimiterParser.isDefaultDelimiter(input)) {
+        if (delimiterParser.isDefaultDelimiter(input)) {
             DelimiterParser delimiterParser = new DelimiterParser(":");
             String result = delimiterParser.splitByDelimiter(input);
 
-            List<Integer> inputNums = DelimiterParser.parseToIntList(result);
+            List<Integer> inputNums = delimiterParser.parseToIntList(result);
             Validator.validateIfInputNegative(inputNums);
 
             return inputNums.stream()
@@ -20,7 +20,7 @@ public class Calculator {
                     .sum();
         }
 
-        if (!DelimiterParser.isDefaultDelimiter(input)) {
+        if (!delimiterParser.isDefaultDelimiter(input)) {
             String delimiter1 = input.substring(0, 2);
             String delimiter2 = input.substring(3, 5);
             DelimiterParser delimiterParser = new DelimiterParser(delimiter1, delimiter2);
@@ -31,7 +31,7 @@ public class Calculator {
             String[] split = numberPart.split(customDelimiter);
 
             String splitString = String.join(",", split);
-            List<Integer> inputNums = DelimiterParser.parseToIntList(splitString); // -> 123 백이십삼
+            List<Integer> inputNums = delimiterParser.parseToIntList(splitString); // -> 123 백이십삼
             Validator.validateIfInputNegative(inputNums);
 
             return inputNums.stream()
