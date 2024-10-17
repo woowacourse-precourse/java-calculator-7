064428218afe77ed605c5d01c954cb969c0c8c51
SeungminKimdev <ksm7250432@gmail.com>
2024-10-17 16:20:16 +0900

    feat: 구분자로 숫자 분리기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,10 +4,15 @@ public class Application {
     public static void main(String[] args) {
         InputString inputString = new InputString();
         StringCheck stringCheck = new StringCheck();
+        SplitString splitString = new SplitString();
 
         String input = inputString.input();
         String separators = stringCheck.checkSeparator(input);
+        String[] splitNum = splitString.splitString(input, separators);
         System.out.println(input);
         System.out.println(separators);
+        for (String s : splitNum) {
+            System.out.println(s);
+        }
     }
 }
