14040bd5e9a5030ff1d4df35ac138004dcf5d130
kgy1008 <gayeon1008@g.skku.edu>
2024-10-18 01:57:06 +0900

    feat: print output result

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,10 @@
+package calculator.view;
+
+public class OutputView {
+    private static final String RESULT_MESSAGE = "결과 : ";
+
+    public static void printResult(final int result) {
+        System.out.printf(RESULT_MESSAGE + "%d", result);
+        System.out.println();
+    }
+}
