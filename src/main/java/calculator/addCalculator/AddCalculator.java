c789b8c53c1a1e43b032737856534dcd9a62388b
Hyso <hsk7953@gmail.com>
2024-10-17 17:35:08 +0900

    :sparkles: feat : calculateSum 메서드 예외 로직 추가 - 입력된 숫자가 음수 일 경우 예외 처리 및 메시지 추가

--- src/main/java/calculator/addCalculator/AddCalculator.java
+++ src/main/java/calculator/addCalculator/AddCalculator.java
@@ -1,12 +1,19 @@
 package calculator.addCalculator;
 
+import calculator.stringSplitter.StringSplitter;
+import camp.nextstep.edu.missionutils.Console;
+
 public class AddCalculator {
     public int calculateSum(String[] numbers){
         int sum = 0;
         for(String number : numbers){
-            sum += Integer.parseInt(number.trim());
-
+            int num = Integer.parseInt(number.trim());
+            if(num < 0){
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+            }
+            sum += num;
         }
         return sum;
     }
+
 }
