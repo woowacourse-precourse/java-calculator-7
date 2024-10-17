446fac6f74e47a134befce0193b7b8762b2f17a0
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-10-17 17:59:05 +0900

    feat: 음수, 유효한 구분자가 아닐 경우 예외처리 및 빈값 또는 하나의 숫자만 입력했을 경우 처리

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -11,6 +11,11 @@ public class CalculatorController {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = readLine();
 
+        if (str.isEmpty()) {
+            print(0);
+            return;
+        }
+
         if (isCustomDelimiter(str)) {
             print(customDelimiter(str));
         } else {
@@ -19,17 +24,19 @@ public class CalculatorController {
     }
 
     private static boolean isCustomDelimiter(String str) {
-        String firstStr = str.substring(0, 2);
-        String secondStr = str.substring(3, 5);
-
-        if (firstStr.equals("//") && secondStr.equals("\\n")) {
-            return true;
+        String firstStr = "";
+        String secondStr = "";
+        if (str.length() >= 5) {
+            firstStr = str.substring(0, 2);
+            secondStr = str.substring(3, 5);
         }
-        return false;
+
+        return firstStr.equals("//") && secondStr.equals("\\n");
     }
 
     private static Integer defaultDelimiter(String str) {
         List<String> defaultValues = Arrays.asList(str.split(":|,"));
+        validation(defaultValues);
         return calculateSum(defaultValues);
     }
 
@@ -38,7 +45,7 @@ public class CalculatorController {
         String realNumStr = str.substring(5);
 
         List<String> customValues = Arrays.asList(realNumStr.split("\\Q" + delimiter + "\\E"));
-
+        validation(customValues);
         return calculateSum(customValues);
     }
 
@@ -52,5 +59,33 @@ public class CalculatorController {
 
     private static void print(Integer sum) {
         System.out.println(sum);
+    }
+
+    private static void validation(List<String> values) {
+        if (values.isEmpty()) {
+            print(0);
+            return;
+        }
+        if (values.size() == 1) {
+            try {
+                int intValue = Integer.parseInt(values.get(0));
+                if (intValue < 0) {
+                    throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("허용되지 않은 문자가 포함되어 있습니다.");
+            }
+        }
+        for (String value : values) {
+            try {
+                int intValue = Integer.parseInt(value);
+                if (intValue < 0) {
+                    throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("문자가 포함되어 있습니다.");
+            }
+
+        }
     }
 }
