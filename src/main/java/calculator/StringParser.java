7c2e6430a668dc0baa4af1c43e98de8c5f435138
Su-daa <subin.kk019@gmail.com>
2024-10-17 21:30:08 +0900

    Fix: 구분자 여러문자일 경우와 \n 인식하도록 정규표현식 수정

--- src/main/java/calculator/StringParser.java
+++ src/main/java/calculator/StringParser.java
@@ -4,19 +4,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringParser {
-    private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
+    private static final String CUSTOM_DELIMITER_PATTERN = "//(.*?)\\\\n(.*)";
     private static final String DEFAULT_DELIMITER = "[,:]";
     public String[] parseString(String string) {
-        if (string.isEmpty()) {
-            return new String[]{"0"};
-        }
         Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(string);
+
         if (matcher.find()) {
             String delimiter = matcher.group(1);
             String input = matcher.group(2);
-            if (input.isEmpty()) {
-                return new String[]{"0"};
-            }
             return input.split(delimiter);
         }
         return string.split(DEFAULT_DELIMITER);
