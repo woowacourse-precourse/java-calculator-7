bf5606a98680d712fe32d165e22c5d3064052dec
Hyun0828 <spdlqj4818@naver.com>
2024-10-17 21:32:54 +0900

    feat: add feature1 in README.md

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,9 +5,16 @@ import calculator.service.InputService;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputService inputService = new InputService();
 
-        // 사용자가 덧셈할 문자열을 입력한다.
-        String userInput = InputService.enterInput();
+        // 기능 0 : 사용자가 덧셈할 문자열을 입력한다.
+//        inputService.enterInput();
 
+        // 기능 1 : 문자열 앞부분에 커스텀 구분자가 있는지 확인한다.
+        if (inputService.isCustomDelimiter()) {
+            System.out.println(inputService.getCustomDelimiter());
+        } else {
+            System.out.println("커스텀 구분자가 없습니다.");
+        }
     }
 }
--- src/main/java/calculator/service/InputService.java
+++ src/main/java/calculator/service/InputService.java
@@ -4,8 +4,39 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputService {
 
-    public static String enterInput() {
+    private String userInput; // 사용자의 입력
+    private char[] defaultDelimeter; // 기본 구분자인 쉼표와 콜론
+    private char customDelimiter; // 사용자가 입력한 커스텀 구분자
+
+    public InputService() {
+        this.userInput = "//;\n1;2;3";
+        this.defaultDelimeter = new char[]{',', ':'};
+    }
+
+    public void enterInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine();
+        this.userInput = Console.readLine();
+    }
+
+    public boolean isCustomDelimiter() {
+        if (userInput.length() >= 5) {
+            if (userInput.startsWith("//") && userInput.charAt(3) == '\n') {
+                this.customDelimiter = userInput.charAt(2);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getUserInput() {
+        return this.userInput;
+    }
+
+    public char[] getDefaultDelimeter() {
+        return this.defaultDelimeter;
+    }
+
+    public char getCustomDelimiter() {
+        return this.customDelimiter;
     }
 }
