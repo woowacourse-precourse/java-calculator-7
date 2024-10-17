a9540d685f22158887f1c1da291a97c014eb82fc
Aqoom01 <rlagusgh789@daum.net>
2024-10-17 19:05:02 +0900

    feat: Adder 클래스 구현

--- src/main/java/calculator/Adder.java
+++ src/main/java/calculator/Adder.java
@@ -1,4 +1,21 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Adder {
+    List<Integer> numberlist;
+    int result;
+
+    public Adder() {
+        numberlist = new ArrayList<Integer>();
+        result = 0;
+    }
+
+    public int add(List<Integer> numberlist) {
+        for(int i = 0; i < numberlist.size(); i++) {
+            result += numberlist.get(i);
+        }
+        return result;
+    }
 }
--- src/main/java/calculator/Tokenizer.java
+++ src/main/java/calculator/Tokenizer.java
@@ -1,4 +1,20 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tokenizer {
+    List<Integer> numberlist;
+
+    public Tokenizer() {
+        numberlist = new ArrayList<Integer>();
+    }
+
+    public List<Integer> tokenize(String input) {
+        numberlist.clear();
+        
+        //주요 로직 작성
+
+        return numberlist;
+    }
 }
