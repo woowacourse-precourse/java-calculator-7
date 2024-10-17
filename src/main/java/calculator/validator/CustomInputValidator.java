c8458260d4b1f12a77a52137dea1464ab547e1c0
googoo9918 <1013as@naver.com>
2024-10-17 23:23:01 +0900

    fix(CustomInputValidator): 기본 구분자에 대한 입력 검증 추가
    - 입력값이 //로 시작하지 않는 경우 기본 구분자 검증 수행
    - 기본 구분자인 ','와 ':', 그리고 숫자를 제외한 문자가 포함된 경우 예외가 발생

--- src/main/java/calculator/validator/CustomInputValidator.java
+++ src/main/java/calculator/validator/CustomInputValidator.java
@@ -10,6 +10,9 @@ public class CustomInputValidator implements InputValidator{
     if(input.startsWith("//")){
       validateCustomDelimiter(input);
     }
+    else{
+      validateDefaultDelimiter(input);
+    }
   }
 
   private void validateCustomDelimiter(String input) {
@@ -20,6 +23,14 @@ public class CustomInputValidator implements InputValidator{
     char delimiter = input.charAt(2);
     if(Character.isDigit(delimiter) || delimiter == '.' || delimiter == 'e' || delimiter == 'E'){
       throw  new IllegalArgumentException("커스텀 구분자 내용 오류: 숫자('0-9'), '.', 'e', 'E'는 사용할 수 없습니다.");
+    }
+  }
+
+  private void validateDefaultDelimiter(String input) {
+    for (char ch : input.toCharArray()) {
+      if(!Character.isDigit(ch) && ch != ',' && ch != ':'){
+        throw new IllegalArgumentException("허용되지 않는 문자: " + ch + "가 포함되어 있습니다.");
+      }
     }
   }
 }
