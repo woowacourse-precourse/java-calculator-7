178821bc837985b9cedb2659bf64cc375d72db3c
sihyun10 <hyhy200170@gmail.com>
2024-10-17 23:34:35 +0900

    feat(커스텀 구분자) : 지정한 커스텀 구분자로 문자 나누기 Matcher, Pattern 클래스들을 활용하여 커스텀 구분자를 추출 후, 그 커스텀 구분자를 사용하여 문자열을 split하는 기능이다.

--- src/main/java/calculator/DelimiterSplitter.java
+++ src/main/java/calculator/DelimiterSplitter.java
@@ -1,17 +1,33 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DelimiterSplitter {
     private String str;
+    private final String CUSTOM_REGEX = "//(.)\\n(.*)";
 
     public DelimiterSplitter(String str) {
         this.str = str;
     }
 
     public String[] split() {
+        if (Pattern.matches(CUSTOM_REGEX, str)) {
+            return customDelimiterSplit();
+        }
         return basicDelimiterSplit();
     }
 
     private String[] basicDelimiterSplit() {
         return str.split(",|:");
+    }
+
+    private String[] customDelimiterSplit() {
+        Matcher matcher = Pattern.compile(CUSTOM_REGEX).matcher(str);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+        return new String[0];
     }
 }
