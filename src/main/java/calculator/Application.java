72af5abada85efa9329513ceef027f3216c8d961
jinukee <jinukee333@gmail.com>
2024-10-17 13:52:27 +0900

    feat(calculator) : implement main method for string addition functionality

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
 
+        int result = sumNumbers(extractNumbers(input));
+        System.out.println("결과 : " + result);
     }
 
     public static String[] extractNumbers(String input){
