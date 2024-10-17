ef2f5cc04f285262ddfdfdceff23a48ed55dc6fa
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-10-17 21:44:05 +0900

    feat(DelimiterParser): 공백 제거기능, 예외처리기능 추가

--- src/main/java/calculator/DelimiterParser.java
+++ src/main/java/calculator/DelimiterParser.java
@@ -3,11 +3,15 @@ package calculator;
 public class DelimiterParser {
     public String[] parse(String input) {
         String delimiter = ",|:";
+        input=input.replace(" ","");
 
         if ((input.startsWith("//"))&&(input.contains("\n"))) {
             int newLineIndex = input.indexOf("\n");
             delimiter=input.substring(2,newLineIndex);
             input=input.substring(newLineIndex+1);
+        }
+        else{
+            throw new IllegalArgumentException("커스텀 구분자가 올바르지 않은 형식인 경우입니다.");
         }
         return input.split(delimiter);
     }
