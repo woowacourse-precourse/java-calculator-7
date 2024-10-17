2744608b32f15f00dbdaf9e0e6834ea3f55409ec
byeongdoohan <quden04@gmail.com>
2024-10-17 11:17:05 +0900

    test: 테스트 케이스 추가 1. 커스텀 구분자 추가 시 기본 구분자와 함께 사용 테스트 2. 커스텀 구분자 두개 이상 추가 시 사용 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -36,11 +36,47 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test()
+    @DisplayName("커스텀 구분자를 추가했을 때 기본 구분자와 함께 사용 테스트")
+    void basicSepWithCustomSepTest() {
+        assertSimpleTest(() -> {
+            run("//;;;\\n1;;;2,3:4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test()
+    @DisplayName("커스텀 구분자가 두개 이상일 때 테스트")
+    void manyCustomSepTest() {
+        assertSimpleTest(() -> {
+            run("//;;;,www\\n1;;;2,3:4www5");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
     @Test
     @DisplayName("입력값 마지막에 구분자가 있을 때")
     void lastCharIsSeperatorTest() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1,2,3-"))
+                assertThatThrownBy(() -> runException("1,2,3:"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("입력값 처음에 구분자가 있을 때")
+    void firstCharIsSeperatorTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(":1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("입력값 처음과 마지막에 구분자가 있을 때")
+    void firstAndLastCharIsSeperatorTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//abc\nabc1,2,3abc"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
@@ -58,8 +94,17 @@ class ApplicationTest extends NsTest {
     @DisplayName("long 범위를 넘어가는 수를 입력받았을 때")
     void beyondLongTest() {
         assertSimpleTest(() -> {
-            run("//;\\n9223372036854775807;3");
-            assertThat(output()).contains("결과 : 9223372036854775810");
+            run("//;\\n9223372036854775808;3");
+            assertThat(output()).contains("결과 : 9223372036854775811");
+        });
+    }
+
+    @Test
+    @DisplayName("long 범위를 넘어가는 수를 입력받았을 때")
+    void addCommaTest() {
+        assertSimpleTest(() -> {
+            run("//;\\n9223372036854775808;3");
+            assertThat(output()).contains("결과 : 9,223,372,036,854,775,811");
         });
     }
 
