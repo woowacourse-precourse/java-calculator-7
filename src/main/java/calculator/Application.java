6ec9c91d1cb9ad8ebbfd7b8b72c0484a5f2ef7a5
sik9252 <sik9252@naver.com>
2024-10-17 20:19:12 +0900

    feat: 빈 값이 입력 되었는지 검증하는 기능 구현
    - 입력 값이 null인 경우 IllegalArgumentException()를 반환한다.
    - 입력 값이 ""인 경우 결과: 0 을 반환한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,14 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String userInput = Console.readLine();
+        String input = Console.readLine();
+
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (input.isEmpty()) {
+            System.out.println("결과: " + 0);
+        }
     }
 }
