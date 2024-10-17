7499c407e21069581331fcaa109d4384fafbe5ba
fbehddn <tom0611@naver.com>
2024-10-17 22:30:43 +0900

    refactor: 기존 구분자 추출 로직 제거

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -24,20 +24,6 @@ public class Calculator {
         return input == null || input.isEmpty();
     }
 
-    private boolean isCustomSeparator(String input) {
-        return input.startsWith("//");
-    }
-
-    private String extractCustomSeparator(String input, String separator) {
-        int newlineIndex = input.indexOf("\\n");
-        String customSeparator = input.substring(2, newlineIndex);
-        return separator + customSeparator;
-    }
-
-    private String extractNumbersPart(String input) {
-        return input.substring(input.indexOf("\\n") + 2);
-    }
-
     private String[] splitNumbers(String input, String separator) {
         for (char delimiter : separator.toCharArray()) {
             input = input.replace(String.valueOf(delimiter), " ");
