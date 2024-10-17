eea443d01dff553cce25361ad9b7a156e0e1c526
rladmstn <oz33177351@gmail.com>
2024-10-17 23:17:41 +0900

    test(Parser) : 구분자 기준으로 숫자 리스트 추출 테스트 작성
    input에서 구분자 기준으로 숫자만 추출해 List<Integer>로 리턴하는 메서드 테스트 작성

--- src/test/java/calculator/domain/ParserTest.java
+++ src/test/java/calculator/domain/ParserTest.java
@@ -3,10 +3,15 @@ package calculator.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ParserTest {
     @ParameterizedTest
@@ -37,5 +42,32 @@ class ParserTest {
     void extractCustomDelimiter(String string, String expected){
         String result = Parser.extractCustomDelimiter(string);
         assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "inputAndDelimiterAndNumbers")
+    @DisplayName("구분자 기준으로 숫자 리스트 추출 테스트")
+    void parseNumbersFromInput(String input, List<String> delimiter, List<Integer> expected){
+        // when
+        List<Integer> result = Parser.parseNumbersFromInput(input, delimiter);
+        // then
+        assertThat(result.size()).isEqualTo(expected.size());
+        for(int i=0; i<expected.size(); i++)
+            assertThat(result.get(i)).isEqualTo(expected.get(i));
+    }
+
+    public static Stream<Arguments> inputAndDelimiterAndNumbers(){
+        return Stream.of(
+                Arguments.arguments("//a\\n1a2a3a1", List.of("a"), List.of(1,2,3,1)),
+                Arguments.arguments("//||\\n1||2||3||1", List.of("||"), List.of(1,2,3,1)),
+                Arguments.arguments("// \\n1 2 4 ", List.of(" "), List.of(1,2,4)),
+                Arguments.arguments("1:2,5", List.of(":",","), List.of(1,2,5)),
+                Arguments.arguments("1,2,4,6,8", List.of(":",","), List.of(1,2,4,6,8)),
+                Arguments.arguments(":,:", List.of(":",","), List.of()),
+                Arguments.arguments("", List.of(":",","), List.of()),
+                Arguments.arguments(",", List.of(":",","), List.of()),
+                Arguments.arguments("//-\\n", List.of("-"), List.of()),
+                Arguments.arguments("//-\\n3", List.of("-"), List.of(3))
+        );
     }
 }
