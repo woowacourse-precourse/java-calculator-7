a49211028c047858c8651310b2ca9e1a72d1adc7
Zrfv <youngoh07015@gmail.com>
2024-10-18 01:41:17 +0900

    feat(model): add 빈 문자열인지 확인하는 기능 in Number class
    문자열이 null 인지 확인하는 메서드 추가

--- src/main/java/calculator/model/Number.java
+++ src/main/java/calculator/model/Number.java
@@ -9,4 +9,11 @@ public class Number {
         this.numString = numString;
         this.sum = 0;
     }
+
+    private boolean isNullString() {
+        if (numString == null) {
+            return true;
+        }
+        return false;
+    }
 }
