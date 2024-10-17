fcd21c4113012cf678eecaf97ee1d87de54d6ea5
jeonghyeon-ohk <ok6737@naver.com>
2024-10-17 16:07:42 +0900

    refactor(validator): 공백 문자열 처리 로직을 StringValidator로 이동
    공백 문자열에 대한 유효성 검사 로직을 InputView에서 StringValidator로 옮겨 새로운 함수로 분리했습니다.

--- src/main/java/calculator/validator/StringValidator.java
+++ src/main/java/calculator/validator/StringValidator.java
@@ -1,4 +1,13 @@
 package calculator.validator;
 
 public class StringValidator{
+    public static String getValidatedInput(String input){
+        return isEmptyInput(input) ? "0" : input;
+    }
+
+    public static Boolean isEmptyInput(String input){
+        return input.isEmpty();
+    }
+
+
 }
--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,10 +1,14 @@
 package calculator.view;
+
 import camp.nextstep.edu.missionutils.Console;
+
+import static calculator.validator.StringValidator.getValidatedInput;
 
 public class InputView {
     public static String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        return input.isEmpty() ? "0" : input;
+
+        return getValidatedInput(input);
     }
 }
