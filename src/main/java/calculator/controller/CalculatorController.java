e827b95ce8b8348f255fa7d39b90f26d276fbd0e
dradnats1012 <herjebi1012@gmail.com>
2024-10-17 23:56:43 +0900

    feat: 컨트롤러 클래스 1차 완성

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,29 @@
 package calculator.controller;
 
+import java.util.List;
+
+import calculator.model.Calculator;
+import calculator.model.Converter;
+import calculator.model.Parser;
+import calculator.model.VerificationNum;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class CalculatorController {
+
+    private final Parser parser = new Parser();
+
+    public void calculate() {
+        String input = InputView.getInput();
+        List<String> parsedList = parser.parseInput(input);
+
+        VerificationNum verificationNum = new VerificationNum(parsedList);
+        verificationNum.verifyAndParseNums();
+
+        Converter converter = new Converter(parsedList);
+        Calculator calculator = new Calculator(converter.convertList());
+
+        int total = calculator.addNums();
+        OutputView.printResult(total);
+    }
 }
