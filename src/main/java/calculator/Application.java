5c47bcfe89c0a37361d33342d5978fbba6e6a1a3
태규 <123963462+EOTAEGYU@users.noreply.github.com>
2024-10-17 17:29:43 +0900

    feat: 입력값 받기 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        System.out.println(input);
 
     }
 }
