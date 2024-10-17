2faba763c5426864222747ad3da1af143da64532
choi <nippert@naver.com>
2024-10-17 15:45:48 +0900

    feat: 계산 결과를 출력

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,6 +1,8 @@
 package calculator.controller;
 
 import calculator.domain.StringAddCalculator;
+import calculator.service.CalculateService;
+import camp.nextstep.edu.missionutils.Console;
 import view.SystemView;
 
 public class CalculatorController {
@@ -8,5 +10,9 @@ public class CalculatorController {
     public static void run() {
         String formula = SystemView.input();
 
+        StringAddCalculator stringAddCalculator = new StringAddCalculator(formula);
+        CalculateService calculateService = new CalculateService(stringAddCalculator);
+        int calculate = calculateService.calculate();
+        SystemView.printResult(calculate);
     }
 }
