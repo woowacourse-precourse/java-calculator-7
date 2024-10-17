e2c8dba555f8fa7abde3543949c16fd99f7d9eda
SongJaeHoonn <thdwogns121@naver.com>
2024-10-17 17:33:36 +0900

    refactor: 조건문 로직 메서드 추출

--- src/main/java/parser/Delimiter.java
+++ src/main/java/parser/Delimiter.java
@@ -3,9 +3,17 @@ package parser;
 public class Delimiter {
 
     public String extractDelimiter(String input) {
-        if (input.startsWith("//") && input.charAt(3) == '\\' && input.charAt(4) == 'n') {
+        if (validateStartsWith(input) && validateEndsWith(input)) {
             return String.valueOf(input.charAt(2));
         }
         return null;
+    }
+
+    private boolean validateStartsWith(String input) {
+        return input.startsWith("//");
+    }
+
+    private boolean validateEndsWith(String input) {
+        return input.charAt(3) == '\\' && input.charAt(4) == 'n';
     }
 }
