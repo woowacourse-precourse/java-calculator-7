556a277bea2d9f622648fa48b64497831a4dba0c
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-17 22:54:21 +0900

    feat: 커스텀 구분자가 비어 있는 상황 예외 처리 추가
    - 커스텀 구분자는 값을 가져야 함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -18,7 +18,10 @@ public class Application {
             if (!input.startsWith("//")) {
                 throw new IllegalArgumentException("잘못된 형식입니다.");
             }
-            String customDelimiter = input.split("\\\\n")[0].split("//")[1];
+            String customDelimiter = input.split("\\\\n")[0].substring("//".length());
+            if (customDelimiter.isEmpty()) {
+                throw new IllegalArgumentException("커스텀 구분자가 비어있습니다.");
+            }
             if (customDelimiter.contains("\\")) {
                 throw new IllegalArgumentException("이스케이프 문자가 포함되었습니다.");
             } else if (customDelimiter.matches(".*\\d.*")) {
