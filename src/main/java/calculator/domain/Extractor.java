1009fc81193a07f9b9220f32f3d0e181992b68c3
zzioozzioo <zzioo1006@naver.com>
2024-10-17 20:05:00 +0900

    fix(Extractor): 정규표현식을 수정함으로써 커스텀 구분자 다음에 나오는 숫자를 인식하지 못하는 오류 해결

--- src/main/java/calculator/domain/Extractor.java
+++ src/main/java/calculator/domain/Extractor.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class Extractor {
 
     private static final String DEFAULT_DELIMITERS = ",|:";
-    private static final String CUSTOM_DELIMITERS = "//(.*?)(\\n|$)";
+    private static final String CUSTOM_DELIMITERS = "//(.*?)\\\\n";
 
     private String input;
     private final List<Integer> numbers = new ArrayList<>();
@@ -20,7 +20,6 @@ public class Extractor {
     public List<Integer> getNumbers() {
         return numbers;
     }
-    // TODO: 기본 구분자 사이에 어떤 값도 입력되지 않은 경우 → 예외처리하기
 
     /**
      * 문자열에서 숫자 추출
@@ -35,12 +34,10 @@ public class Extractor {
 
     private String getDelimiter() {
         Matcher matcher = Pattern.compile(CUSTOM_DELIMITERS).matcher(input);
+        
         if (matcher.find()) {
-            String customDelimiter = matcher.group(1);
 
-            if (customDelimiter.isEmpty()) {
-                throw new IllegalArgumentException("잘못된 구분자입니다.");
-            }
+            String customDelimiter = matcher.group();
 
             input = input.substring(matcher.end());
             return "[" + Pattern.quote(customDelimiter) + "]|" + DEFAULT_DELIMITERS;
@@ -60,7 +57,7 @@ public class Extractor {
                     Integer value = Integer.valueOf(data);
                     numbers.add(value);
                 } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("잘못된 구분자입니다.");
+                    throw new IllegalArgumentException("숫자만 계산할 수 있습니다.");
                 }
             }
         }
