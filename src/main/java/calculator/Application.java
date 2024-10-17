6a4173f679967bb62d0f3b504983ded35d119c1d
ChoiDongJoon <adong0808@naver.com>
2024-10-17 15:26:59 +0900

    feat(calculator): 커스텀 구분자 추출 추가
    feat(calculator): 커스텀 구분자 추출 추가
    
    커스텀 구분자 추출을 위한 extractCustomSeparation() 함수 정의

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
+
+    private static String extractCustomSeparation(String inputStr) {
+        if (inputStr.startsWith("//")) {
+            int endIndex = inputStr.indexOf("n");
+            if (endIndex != -1) {
+                return inputStr.substring(2, endIndex - 1);
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+//         TODO: 프로그램 구현
+        String inputStr = Console.readLine();
+//        String inputStr = "//;\n1;2;3";
+        ArrayList<Integer> listInt = new ArrayList<Integer>();
+
+        String customSeparation = extractCustomSeparation(inputStr);
+        if (customSeparation != null) {
+            System.out.println(customSeparation);
+        }
     }
 }
