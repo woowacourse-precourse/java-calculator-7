f9a2f2332f7bdbe254c31c21a52ef18b831fe93c
wogus <ind07152@naver.com>
2024-10-17 11:36:19 +0900

    feat(DelimiterFilter): 구분자 반환 기능 추가
    Parser의 구분자를 반환하는 역할을 DelimiterFilter 클래스로 분리한다.

--- src/main/java/calculator/utils/DelimiterFilter.java
+++ src/main/java/calculator/utils/DelimiterFilter.java
@@ -1,7 +1,12 @@
 package calculator.utils;
 
+import static calculator.constant.DefaultSeparator.COLON;
+import static calculator.constant.DefaultSeparator.COMMA;
 import static calculator.constant.StandardIndex.FIRST_STANDARD;
 import static calculator.constant.StandardIndex.SECOND_STANDARD;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DelimiterFilter {
 
@@ -9,5 +14,17 @@ public class DelimiterFilter {
         int firstIndex = input.indexOf(FIRST_STANDARD.getStandard());
         int secondIndex = input.indexOf(SECOND_STANDARD.getStandard());
         return firstIndex == 0 && secondIndex > 0;
+    }
+
+    public List<Character> getDelimiter(boolean hasCustomDelimiter, String input) {
+        if (hasCustomDelimiter) {
+            List<Character> separators = new ArrayList<>();
+            int secondIndex = input.indexOf(SECOND_STANDARD.getStandard());
+            for (int i = 2; i < secondIndex; i++) {
+                separators.add(input.charAt(i));
+            }
+            return separators;
+        }
+        return new ArrayList<>(List.of(COMMA.getSeparator(), COLON.getSeparator()));
     }
 }
