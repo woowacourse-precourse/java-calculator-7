ec318caf9c6adb6b113c19aa4082f08b755e1634
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-17 19:17:34 +0900

    refactor(StringNumberSumCalculator): 정수형 배열을 받아 합을 구하는 로직으로 변경

--- src/main/java/calculator/util/StringNumberSumCalculator.java
+++ src/main/java/calculator/util/StringNumberSumCalculator.java
@@ -4,9 +4,8 @@ import java.util.Arrays;
 
 public class StringNumberSumCalculator {
 
-    public static int sum(String[] strings) {
-        return Arrays.stream(strings)
-                .mapToInt(Integer::parseInt)
+    public static int sum(int[] numbers) {
+        return Arrays.stream(numbers)
                 .sum();
     }
 
