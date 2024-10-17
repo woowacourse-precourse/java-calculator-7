e3e245fe3d882c1fa6ce72ad822ec560fc5f8969
IM-GYURI <kgulr0517@gmail.com>
2024-10-18 00:26:24 +0900

    feat : add InputView for user input handling

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,13 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String INPUT_PROMPT = "덧셈할 문자열을 입력해 주세요.";
+
+    public static String requestInput() {
+        System.out.println(INPUT_PROMPT);
+        return Console.readLine();
+    }
+}
