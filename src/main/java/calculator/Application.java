d3cad29b25fe3855485819f0ac51fbbf669256db
junest66 <chlwndks333@gmail.com>
2024-10-18 02:15:07 +0900

    feat(Application): 계산기 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.delimiter.CustomDelimiterPattern;
+import calculator.delimiter.DefaultDelimiterPattern;
+import calculator.delimiter.DelimiterPattern;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<DelimiterPattern> patterns = List.of(new DefaultDelimiterPattern(), new CustomDelimiterPattern());
+        CalculatorService service = new CalculatorService(new InputView(), new OutputView(),
+                new InputValidator(patterns), new InputExtractor(), new SumCalculator());
+        service.start();
     }
 }
