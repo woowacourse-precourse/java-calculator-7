79349f9b654f264b145cbc7c58cfd89f77712783
이창헌 <changbill@naver.com>
2024-10-17 21:39:37 +0900

    refactor(constant): separate character constants

---
+++ src/main/java/calculator/constant/CharacterConstants.java
@@ -0,0 +1,14 @@
+package calculator.constant;
+
+public class CharacterConstants {
+
+    public static final char[] CONSTANT_DELIMITER = {',', ':'};
+    public static final String CUSTOM_DELIMITER_STARTS_WITH = "//";
+    public static final String CUSTOM_DELIMITER_ENDS_WITH = "\\n";
+    public static final String INVALID_CUSTOM_DELIMITER_START = "/";
+    public static final String INVALID_CUSTOM_DELIMITER_END = "\\";
+
+    private CharacterConstants() {
+        throw new UnsupportedOperationException("이 클래스는 인스턴스화 할 수 없습니다");
+    }
+}
--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,5 +1,10 @@
 package calculator.service;
 
+import static calculator.constant.CharacterConstants.CONSTANT_DELIMITER;
+import static calculator.constant.CharacterConstants.CUSTOM_DELIMITER_ENDS_WITH;
+import static calculator.constant.CharacterConstants.CUSTOM_DELIMITER_STARTS_WITH;
+import static calculator.constant.CharacterConstants.INVALID_CUSTOM_DELIMITER_END;
+import static calculator.constant.CharacterConstants.INVALID_CUSTOM_DELIMITER_START;
 import static calculator.constant.ErrorMessages.EMPTY_INPUT_MESSAGE;
 import static calculator.constant.ErrorMessages.INVAILD_CHAR_MESSAGE;
 import static calculator.constant.ErrorMessages.INVALID_DELIMITER_MESSAGE;
@@ -12,7 +17,6 @@ import java.util.Optional;
 
 public class CalculatorService {
 
-    private final char[] constantDelimiter = {',', ':'};
     private Optional<char[]> customDelimiter;
     private Optional<String> input;
     private String formatInput;
@@ -25,8 +29,8 @@ public class CalculatorService {
 
     public void findCustomDelimiter() {
         String userInput = input.orElseThrow(() -> new IllegalArgumentException(EMPTY_INPUT_MESSAGE));
-        if (userInput.startsWith("//") && userInput.contains("\\n")) {
-            int delimiterEndIndex = userInput.indexOf("\\n");
+        if (userInput.startsWith(CUSTOM_DELIMITER_STARTS_WITH) && userInput.contains(CUSTOM_DELIMITER_ENDS_WITH)) {
+            int delimiterEndIndex = userInput.indexOf(CUSTOM_DELIMITER_ENDS_WITH);
             customDelimiter = Optional.of(userInput.substring(2, delimiterEndIndex).toCharArray());
             char[] customDelimiterChars = customDelimiter.orElse(new char[0]);
             validCustomDelimiter(customDelimiterChars);
@@ -40,7 +44,8 @@ public class CalculatorService {
     }
 
     private void validFormatInput(String formatInput) {
-        if (formatInput.contains("/") || formatInput.contains("\\")) {
+        if (formatInput.contains(INVALID_CUSTOM_DELIMITER_START) || formatInput.contains(
+                INVALID_CUSTOM_DELIMITER_END)) {
             throw new IllegalArgumentException(INVAILD_CHAR_MESSAGE);
         }
     }
@@ -50,7 +55,8 @@ public class CalculatorService {
             if (Character.isDigit(c)) {
                 throw new IllegalArgumentException(NUMBER_ASSIGN_ERROR_MESSAGE);
             }
-            if (c == '\\' || c == '/') {
+            if (String.valueOf(c).equals(INVALID_CUSTOM_DELIMITER_START)
+                    || String.valueOf(c).equals(INVALID_CUSTOM_DELIMITER_END)) {
                 throw new IllegalArgumentException(INVAILD_CHAR_MESSAGE);
             }
         }
@@ -58,7 +64,7 @@ public class CalculatorService {
 
     public void deleteDelimiter() {
         String removeDelimiters = formatInput.chars()
-                .filter(i -> !isDelimiter((char) i))
+                .filter(i -> isDelimiter((char) i))
                 .mapToObj(c -> (char) c)
                 .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
                 .toString();
@@ -68,7 +74,7 @@ public class CalculatorService {
 
     private boolean isDelimiter(char currentChar) {
         boolean isDelimiter = false;
-        for (char c : constantDelimiter) {
+        for (char c : CONSTANT_DELIMITER) {
             if (currentChar == c) {
                 isDelimiter = true;
                 break;
@@ -85,7 +91,7 @@ public class CalculatorService {
 
     private void isNumeric(String removeDelimiters) {
         numbersToCalculate = Optional.of(removeDelimiters.chars().filter(i -> {
-            if (i < '0' || i > '9') {
+            if (!Character.isDigit(i)) {
                 throw new IllegalArgumentException(INVALID_DELIMITER_MESSAGE + (char) i);
             }
             return true;
