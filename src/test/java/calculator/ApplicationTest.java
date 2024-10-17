9b90120435daeb99de3d0c332bdf75f300eb53d3
GiSeok Kang <ime.rokz@gmail.com>
2024-10-18 01:40:18 +0900

    test: 구분자 및 여러 예외사항에 대한 테스트 코드 추가
    - 숫자는 구분자로 사용할 수 있으나, 구분자로 지정된 숫자가 오면 구분자가 연속으로 온 것으로 예외처리

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,13 +1,14 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -19,9 +20,122 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 공백_입력() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 기본구분자_정상_입력() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 기본구분자_음수_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("음수 입력 x")
+        );
+    }
+
+    @Test
+    void 기본구분자_사이_숫자_아닌_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1:d,4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본구분자_잘못된_사용() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1-3:4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본구분자_두개_연속_사용() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1::3:4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀구분자_정상_입력() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀구분자_끝_잘못_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\m1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀구분자_문자가_아닌_문자열_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;;\\n1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀구분자_숫자인_경우_입력() {
+        assertSimpleTest(() -> {
+            run("//9\\n19293");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀구분자_숫자인_경우_연속입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//9\\n19993"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀구분자_공백_입력() {
+        assertSimpleTest(() -> {
+            run("// \\n1 2 3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 기본_한자리수_이상_입력() {
+        assertSimpleTest(() -> {
+            run("10,20:33");
+            assertThat(output()).contains("결과 : 63");
+        });
+    }
+
+    @Test
+    void 커스텀_한자리수_이상_입력() {
+        assertSimpleTest(() -> {
+            run("//;\\n10;20;33");
+            assertThat(output()).contains("결과 : 63");
+        });
     }
 
     @Override
