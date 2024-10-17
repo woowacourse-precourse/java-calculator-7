25904ae8a003291dc9f6a4e43372c9ff142e5a43
양효빈 <hyobin3026@naver.com>
2024-10-17 15:20:22 +0900

    feat(Calculator): 생성자 생성

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,65 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.NoSuchElementException;
+
+public class Calculator {
+
+    private String input;
+    private int result;
+    private int inputLen;
+
+    // 생성자에서 입력 받기
+    public Calculator() {
+        try {
+            this.input = Console.readLine();
+            this.inputLen = input.length();
+            checkType(input);
+        } catch (NoSuchElementException e) {
+            isNull();
+        } finally {
+            Console.close();
+        }
+    }
+
+    // 테스트용 생성자
+    public Calculator(String input) {
+        this.input = input;
+        this.inputLen = input.length();
+    }
+
+    private void checkType(String input) {
+
+    }
+
+    private void isNull(){
+
+    }
+
+    public void isPositiveNum(String s, String delimiter){
+
+    }
+
+    public int printResult(){
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
