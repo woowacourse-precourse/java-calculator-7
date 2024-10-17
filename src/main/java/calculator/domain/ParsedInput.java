111a4f6d9934f697906978404d933f4d4ea7e332
dongjae <harold306122@gmail.com>
2024-10-17 22:19:32 +0900

    feat(domain): 파싱된 입력 데이터를 저장하는 ParsedInput 클래스 구현

--- src/main/java/calculator/domain/ParsedInput.java
+++ src/main/java/calculator/domain/ParsedInput.java
@@ -1,17 +1,15 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package calculator.domain;
 
 public class ParsedInput {
+    // 파싱된 숫자들을 저장할 배열
     private final String[] numbers;
 
+    // 생성자: 파싱된 숫자 배열을 받아서 초기화
     public ParsedInput(String[] numbers) {
         this.numbers = numbers;
     }
 
+    // 파싱된 숫자 배열을 반환하는 메서드
     public String[] getNumbers() {
         return this.numbers;
     }
