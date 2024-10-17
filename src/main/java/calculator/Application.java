624a4957347c43bdabd9af23ee36d8cf9a3013a7
jumdo12 <jumdo12@gmail.com>
2024-10-17 13:18:48 +0900

    style(calculator) Application 코드 누락
    - 누락된 코드 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import controller.StringCalculatorController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        StringCalculatorController controller = new StringCalculatorController();
+        controller.run();
     }
 }
