d92568a64492e6ffb139cc07ad9ef69e04d781bb
hwangdaesun <hds650588@gmail.com>
2024-10-17 14:04:03 +0900

    feat(CustomDelimiterParser.class) : CustomDelimiterParser의 parse 메서드 구현

--- src/main/java/calculator/model/CustomDelimiterParser.java
+++ src/main/java/calculator/model/CustomDelimiterParser.java
@@ -9,6 +9,21 @@ public class CustomDelimiterParser {
     }
 
     public String parse(String input) {
+        if (hasCustomDelimiter(input)) {
+            return input.substring(PREFIX.length(), getSuffixIndex(input));
+        }
         return "";
+    }
+
+    public boolean hasCustomDelimiter(String input) {
+        if (!input.startsWith(PREFIX)) {
+            return false;
+        }
+        int suffixIndex = getSuffixIndex(input);
+        return suffixIndex != -1;
+    }
+
+    private int getSuffixIndex(String input) {
+        return input.indexOf(SUFFIX);
     }
 }
