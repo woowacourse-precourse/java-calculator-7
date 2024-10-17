ec5991228c90f473bf14a2f7280b883f66725558
yedamhy <yeadalm04@gmail.com>
2024-10-18 02:05:23 +0900

    ♻️ refactor(domain): 구분자 처리 로직 추가
    DelimiterExtraction 클래스에서 구분자가 혼합되어 있는 경우 처리할 수 있도록 수정

--- src/main/java/domain/DelimiterExtraction.java
+++ src/main/java/domain/DelimiterExtraction.java
@@ -9,11 +9,13 @@ public class DelimiterExtraction {
     public String[] Extraction(String input){
         Matcher matcher = CUSTOM_DELIMITER.matcher(input);
 
-        // 커스텀 구분자일 때
+        // 커스텀 구분자가 있을 때
         if(matcher.find()){
-            String customDelimiter = matcher.group(1);
-            String numbers = matcher.group(2);
-            return numbers.split(Pattern.quote(customDelimiter)); // 커스텀 구분자로 분리
+            String customDelimiter = matcher.group(1); // 사용자 정의 구분자 추출
+            String numbers = matcher.group(2); // 숫자 부분 추출
+
+            String combinedDelimiter = Pattern.quote(customDelimiter) + "|" + DEFAULT_DELIMITER;
+            return numbers.split(combinedDelimiter); // 커스텀 구분자로 분리
         }
         // 구분자가 없는 경우 → 숫자만 존재할 때
         if(input.matches("[0-9]+")){
