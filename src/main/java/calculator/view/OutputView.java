642420220804af0697181b935b5d73c0dd964cd5
IM-GYURI <kgulr0517@gmail.com>
2024-10-18 01:53:16 +0900

    feat : add printRepeat() to display restart or exit prompt

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     private static final String INPUT_PROMPT = "덧셈할 문자열을 입력해 주세요.";
     private static final String OUTPUT_PROMPT = "결과 : ";
+    private static final String OUTPUT_REPEAT_PROMPT = "재시작을 원한다면 1, 종료를 원한다면 2를 입력하세요.";
 
     public static void printInput() {
         System.out.println(INPUT_PROMPT);
@@ -11,6 +12,10 @@ public class OutputView {
 
     public static void printResult(int result) {
         System.out.println(OUTPUT_PROMPT + result);
+    }
+
+    public static void printRepeat() {
+        System.out.println(OUTPUT_REPEAT_PROMPT);
     }
 
     public static void printError(String error) {
