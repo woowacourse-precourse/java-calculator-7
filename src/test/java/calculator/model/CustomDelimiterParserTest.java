6b3d9895ec9ee3ff444869b5bae84f3a7dc01c66
hwangdaesun <hds650588@gmail.com>
2024-10-17 22:54:49 +0900

    test(CustomDelimiterParser.class) : CustomDelimiterParser의 excludeFromPrefixToSuffixBy 메서드 테스트

--- src/test/java/calculator/model/CustomDelimiterParserTest.java
+++ src/test/java/calculator/model/CustomDelimiterParserTest.java
@@ -28,4 +28,20 @@ class CustomDelimiterParserTest {
         String delimiter = customDelimiterParser.parse(input).orElseThrow(NumberFormatException::new);
         Assertions.assertEquals(";/\\", delimiter);
     }
+
+    @Test
+    void 커스텀_구분자를_나타내기위한_부분_제거_1() {
+        String input = "//;\\n1;2;3";
+        CustomDelimiterParser customDelimiterParser = CustomDelimiterParser.getInstance();
+        String cutInput = customDelimiterParser.excludeFromPrefixToSuffixBy(input);
+        Assertions.assertEquals(cutInput, "1;2;3");
+    }
+
+    @Test
+    void 커스텀_구분자를_나타내기위한_부분_제거_2() {
+        String input = "//;\\\\n1;2;3";
+        CustomDelimiterParser customDelimiterParser = CustomDelimiterParser.getInstance();
+        String cutInput = customDelimiterParser.excludeFromPrefixToSuffixBy(input);
+        Assertions.assertEquals(cutInput, "1;2;3");
+    }
 }
