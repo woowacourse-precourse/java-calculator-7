8de8a30a28ebae3f03ad64edff093e0bc6bf2ddc
Firedrago95 <yonghwa95@naver.com>
2024-10-17 11:34:22 +0900

    test : 서비스 객체 커스텀 구분자 양수 검사 예외발생 테스트 코드 작성

--- src/test/java/calculator/model/CalculatorServiceTest.java
+++ src/test/java/calculator/model/CalculatorServiceTest.java
@@ -1,9 +1,9 @@
 package calculator.model;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,7 +19,7 @@ public class CalculatorServiceTest {
 
     @Test
     @DisplayName("빈 문자열 입력 테스트")
-    public void emptyInputTest() {
+    public void checkAndParseInputEmptyInputTest() {
         //given
         String empty = "";
 
@@ -28,6 +28,20 @@ public class CalculatorServiceTest {
 
         //then
         assertThat(list).isEqualTo(List.of(0, 0));
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 양수 검사 예외발생 테스트")
+    public void checkAndParseInputPositiveNumberExceptionTest() {
+        //given
+        String input1 = "//*\\n1*0*3";
+        String input2 = "//*\\n-1*2*3";
+
+        //then
+        assertThrows(IllegalArgumentException.class,
+            () -> calculatorService.checkAndParseInput(input1));
+        assertThrows(IllegalArgumentException.class,
+            () -> calculatorService.checkAndParseInput(input2));
     }
 
     @Test
