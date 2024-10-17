3ca23c79865e4c5cf0ff7f65cf2d49dab5d91ba2
5ms <oms010414@gmail.com>
2024-10-17 19:58:11 +0900

    ì사용자 입력 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,27 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
         Separator separator = new Separator(',',':');
+
+        String input = userInput();
 
 
     }
+
+    public static String userInput(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.\n");
+        return Console.readLine();
+    }
+
+
+
 }
