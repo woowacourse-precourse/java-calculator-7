15dc0dc0f2fe82bac7380d0a2d1c7b061c2f0d9a
Hanjjong <tim4974@naver.com>
2024-10-17 18:30:31 +0900

    feat(DelimiterParser): "//", "\n" 사이 커스텀 구분자가 존재하는 지 확인하는 기능 추가

--- src/main/java/calculator/DelimiterParser.java
+++ src/main/java/calculator/DelimiterParser.java
@@ -1,14 +1,21 @@
 package calculator;
 
 public class DelimiterParser {
+
+    private final String input;
+
     public DelimiterParser(String input) {
-        parseValidator(input);
+        this.input = input;
+        parseValidator();
     }
 
-    private void parseValidator(String input) {
+    private void parseValidator() {
         if (input.startsWith("//")) {
             int newlineIndex = input.indexOf('\n');
             if (newlineIndex == -1) {
+                throw new IllegalArgumentException("잘못된 입력 형식입니다.");
+            }
+            if (newlineIndex <= 2) {
                 throw new IllegalArgumentException("잘못된 입력 형식입니다.");
             }
         }
