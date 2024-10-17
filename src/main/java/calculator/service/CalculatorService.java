9d4854a67bd6e4425af584a78e278e5700c9216d
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-17 19:14:10 +0900

    feat(CalculatorService): calculateSum() 메서드에 양수인지 검증하는 로직 추가

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,20 +1,24 @@
 package calculator.service;
 
 import calculator.util.StringNumberSumCalculator;
+import calculator.validation.DelimiterNumberValidator;
 
 public class CalculatorService {
 
     private final DelimiterService delimiterService;
+    private final DelimiterNumberValidator delimiterNumberValidator;
 
-    public CalculatorService(DelimiterService delimiterService) {
+    public CalculatorService(DelimiterService delimiterService, DelimiterNumberValidator delimiterNumberValidator) {
         this.delimiterService = delimiterService;
+        this.delimiterNumberValidator = delimiterNumberValidator;
     }
 
     public int calculateSum(String input) {
-        String[] numbers = delimiterService.splitNumbers(input);
+        String[] stringNumbers = delimiterService.splitNumbers(input);
+        int[] numbers = delimiterNumberValidator.isValidNumber(stringNumbers);
 
         if (numbers.length == 1) {
-            return Integer.parseInt(numbers[0]);
+            return numbers[0];
         }
 
         return StringNumberSumCalculator.sum(numbers);
