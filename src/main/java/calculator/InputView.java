01f6461abc0458e7c069f1a910fc48df001f209d
seoys0910 <seoys0910@naver.com>
2024-10-17 17:51:46 +0900

    feat : 문자열을 입력받는 기능 구현

---
+++ src/main/java/calculator/InputView.java
@@ -0,0 +1,9 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String readInputString() {
+        return Console.readLine();
+    }
+}
