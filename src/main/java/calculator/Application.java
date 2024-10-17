a54cc3ddf95dc224199c107f1fc102f267af13a2
kang yeon joo <rkdduswndi@naver.com>
2024-10-17 21:27:51 +0900

    feat(exception): 커스텀 구분자 추출 시 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,8 +9,12 @@ public class Application {
 
         if(input_str.startsWith("//")){
             String[] split_str = input_str.split("\n");
-            String customDelimiter = split_str[0].substring(2);
-            input_str = split_str[1];
+            if(split_str.length ==2){
+                String customDelimiter = split_str[0].substring(2);
+                input_str = split_str[1];
+            }else {
+                throw new IllegalArgumentException("커스텀 구분자는 //와 \\n사이에 존재해야합니다. ");
+            }
         }
     }
 }
