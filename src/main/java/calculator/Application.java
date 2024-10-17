22fc00f2d73bd6d5482f8189b55b5a1dbe74f31b
seahwanKim <kimkimseasea@gmail.com>
2024-10-17 17:31:44 +0900

    refactor: 구현한 메서드를 Calculator 클래스를 생성해 리펙토링한다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,45 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
+import calculator.domain.Calculator;
 
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        System.out.println("결과 : " + calculator(Console.readLine()));
-    }
-
-    private static int calculator(String input) throws IllegalArgumentException{
-
-        String delimiter = "[,;]";
-
-        if (input.startsWith("//")) {
-
-            if (!input.contains("\\n")) {
-                throw new IllegalArgumentException();
-            }
-            delimiter = input.substring("//".length(), input.indexOf("\\n"));
-            input = input.substring(input.indexOf("\\n") + "\\n".length());
-        }
-
-        return sum(input, delimiter);
-    }
-
-    private static int sum(String input, String delimiter) {
-        String[] numbers = input.split(delimiter);
-
-        int sum = 0;
-        for (String number : numbers) {
-
-            try {
-                if (Integer.parseInt(number) < 0) {
-                    throw new IllegalArgumentException();
-                }
-                sum += Integer.parseInt(number);
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException();
-            }
-        }
-        return sum;
+        Calculator calculator = new Calculator();
+        calculator.run();
     }
 }
