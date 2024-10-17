770f1aa93cc55233e93a8b9918f4348efda96294
sunwon <jsw5913@naver.com>
2024-10-17 12:45:09 +0900

    feat(StringParser): 구분자 확인 메소드 구현

--- src/main/java/calculator/model/StringParser.java
+++ src/main/java/calculator/model/StringParser.java
@@ -1,10 +1,22 @@
 package calculator.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringParser {
 
     private static final String BLANK = "";
+    private static final String COMMA = ",";
+    private static final String COLON = ":";
+
+    private static List<String> separators = Arrays.asList(COMMA, COLON);
+
 
     public boolean isBlank(String input) {
         return input.equals(BLANK);
+    }
+
+    public boolean isSeparator(String potentialSeparator) {
+        return separators.contains(potentialSeparator);
     }
 }
