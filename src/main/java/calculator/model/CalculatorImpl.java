a870d88ce3af34d5f73242fc2f35c0654930ab9f
qudcks8084 <qudcks8084@naver.com>
2024-10-17 20:15:01 +0900

    feat(model) : add CalculatorImpl class
    Detail
    - Add a member parameter to save String Input
    - Add a Constructor to Set String Input parameter
    - Override calculate method to get result from non-Custom delemiter

--- src/main/java/calculator/model/CalculatorImpl.java
+++ src/main/java/calculator/model/CalculatorImpl.java
@@ -2,14 +2,22 @@ package calculator.model;
 
 import java.util.StringTokenizer;
 
-public class Calculator1 {
-    public int calcResult(String str){
+public class CalculatorImpl implements Calculator {
+
+    private final String string;
+
+    public CalculatorImpl(String string) {
+        this.string = string;
+    }
+
+    @Override
+    public int calculate() {
         int calcResult = 0;
 
         // 10.16 Ver 1
         // First It doesn't matter about custom delimiter
         // Just Only , and :
-        StringTokenizer st = new StringTokenizer(str,",:");
+        StringTokenizer st = new StringTokenizer(this.string,",:");
         while (st.hasMoreTokens()){
             calcResult += Integer.parseInt(st.nextToken());
         }
