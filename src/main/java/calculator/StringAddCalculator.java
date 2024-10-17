fb204a84fa98bc5bad215b47d1f29fb61565a6aa
dhwoo <wooda01@naver.com>
2024-10-17 21:29:59 +0900

    feat: 정상 입력일 때 문자열 덧셈 계산기 구현

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,48 @@
+package calculator;
+
+public class StringAddCalculator {
+    public static int calculate(String input) {
+        // 아무것도 입력되지 않은 경우 0 반환
+        if (input.isEmpty()) {
+            return 0;
+        }
+
+        // 커스텀 구분자가 있는지 확인하고 처리
+        if (input.contains("//")) {
+            return sumWithCustomSeparator(input);
+        }
+
+        // 기본 구분자로 계산
+        return sumWithBasicSeparator(input);
+
+    }
+
+    // 커스텀 구분자로 문자열 분리 후 sum
+    private static int sumWithCustomSeparator(String input) {
+        // "//" 와 "\n" 사이에 있는 커스텀 문자 추출
+        char custom_ch = input.charAt(2);
+
+        // //(커스텀문자)\n 이후를 input으로 다시 설정
+        input = input.substring(5);
+
+        // 커스텀 구분자로 문자열 분리
+        String[] splitString = input.split(String.valueOf(custom_ch));
+
+        return sum(splitString);
+    }
+
+    // 기본 구분자로 문자열 분리 후 sum
+    private static int sumWithBasicSeparator(String input) {
+        String[] splitString = input.split(",|:");
+        return sum(splitString);
+    }
+
+    // 숫자 합산 method
+    private static int sum(String[] splitString) {
+        int sum = 0;
+        for (int i = 0; i < splitString.length; i++) {
+            sum += Integer.parseInt(splitString[i]);
+        }
+        return sum;
+    }
+}
