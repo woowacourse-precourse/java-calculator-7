95006c7c8c72491af3feb817d4da35509354ffe9
hanwool <hanwe92@gmail.com>
2024-10-17 22:59:01 +0900

    feat: input processing when use custom delimiters

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -3,40 +3,79 @@ package calculator.controller;
 import calculator.controller.view.InputView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class CalculatorController {
+
     public void start() {
-        String input = InputView.getIdentifierAndNumbers();
-        String[] stringArray = input.split("");
-        List<Integer> numbers = new ArrayList<>();
-        List<String> identifiers = new ArrayList<>();
-        if (isNumber(stringArray[0])) {
-            List<String> numStrs = new ArrayList<>();
+        String input = InputView.getDelimiterAndNumbers();
+
+        Stream<Integer> numbers = Stream.empty();
+        List<String> delimiters = new ArrayList<>();
+        List<String> numStrs = new ArrayList<>();
+
+
+        if (startsWithNumber(input)) {
+            String[] stringArray = input
+                    .replaceAll("[,|:]", " ")
+                    .split(" ");
+
             for (int i = 0; i < stringArray.length; i++) {
                 if (i % 2 == 0) {
                     numStrs.add(stringArray[i]);
                 } else {
-                    identifiers.add(stringArray[i]);
+                    delimiters.add(stringArray[i]);
                 }
             }
 
-            if(identifiers.stream().allMatch(identifier ->
-                    identifier.equals(",") || identifier.equals(":"))) {
-                numbers = convertToInt(numStrs).toList();
+            boolean useProperDelimiter = delimiters.stream()
+                    .allMatch(delimiter -> delimiter.equals(",") || delimiter.equals(":"));
+            if(useProperDelimiter) {
+                numbers = convertToInt(numStrs);
+            } else {
+                throw new IllegalArgumentException("잘못된 형식의 입력입니다.");
             }
         } else {
-
+            // check the format of "//;\n1;2;3"
+            String newDelimiter = extractNewDelimiter(input);
+            numbers = Arrays.stream(
+                    input.substring(newDelimiter.length() + 4).split(newDelimiter)
+                    )
+                    .map(Integer::parseInt);
         }
-        int sum = numbers.stream().reduce(0, Integer::sum);
+        int sum = numbers.reduce(0, Integer::sum);
+        System.out.println("결과 : " + sum);
     }
 
-    private Boolean isNumber(String str) {
-        return str.matches("[0-9]+");
+    private String extractNewDelimiter(String str) {
+        checkInputFormat(str);
+        int delimiterIndex = str.indexOf("\\n");
+        return str.substring(2, delimiterIndex);
+    }
+
+    private void checkInputFormat(String str) {
+        if(!str.startsWith("//")) throw new IllegalArgumentException("잘못된 형식의 입력입니다.");
+        if(!str.contains("\\n")) throw new IllegalArgumentException("잘못된 형식의 입력입니다.");
+    }
+
+    private boolean startsWithNumber(String str) {
+        String first = String.valueOf(str.charAt(0));
+        return first.matches("[0-9]+");
     }
 
     private Stream<Integer> convertToInt(List<String> strs) {
         return strs.stream().map(Integer::parseInt);
     }
+
+    public static void main(String[] args) {
+        var version = "Java 23";
+        String str = "33,1:2";
+        String result = str.replaceAll("[,|:]", " ").replace(":", " ");;
+        System.out.println(result);
+
+    }
 }
+
+
--- src/main/java/calculator/controller/view/InputView.java
+++ src/main/java/calculator/controller/view/InputView.java
@@ -3,7 +3,7 @@ package calculator.controller.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
-    public static String getIdentifierAndNumbers() {
+    public static String getDelimiterAndNumbers() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return readLine();
     }
