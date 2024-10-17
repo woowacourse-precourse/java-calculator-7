d296e8d1f2358223c970940f16de340437e1f69d
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-18 01:17:01 +0900

    refactor: 입출력 전용 클래스를 사용하도록 변경
    - 입출력 로직을 중앙화하여 코드 가독성과 유지 보수성 개선.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,12 +3,12 @@ package calculator;
 import calculator.delimiter.CustomDelimiterService;
 import calculator.delimiter.Delimiter;
 import calculator.delimiter.Delimiters;
+import calculator.util.console.Console;
 import calculator.util.integer.IntegerUtils;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Scanner;
 
 public class Application {
 
@@ -20,9 +20,8 @@ public class Application {
         delimiterList.add(new Delimiter(","));
         delimiterList.add(new Delimiter(":"));
 
-        Scanner scanner = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = scanner.nextLine();
+        String input = Console.readLine();
 
         Optional<Delimiter> delimiter = customDelimiterService.extract(input);
         if (delimiter.isPresent()) {
@@ -37,7 +36,7 @@ public class Application {
         try {
             List<Integer> numbers = IntegerUtils.parsePositiveIntegers(numberTokens);
             int sum = IntegerUtils.sum(numbers);
-            System.out.print("결과 : " + sum);
+            Console.print("결과 : " + sum);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("구분자가 아닌 문자가 포함되어 있습니다.");
         }
