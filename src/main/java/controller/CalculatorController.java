afad7b120f3aae0d0a32d46d92ad437557f94b8b
daxxn <dahyeonhan4529@gmail.com>
2024-10-17 22:40:25 +0900

    feat(controller): add run method
    - Implement runController method for input and calculate processing

--- src/main/java/controller/CalculatorController.java
+++ src/main/java/controller/CalculatorController.java
@@ -11,4 +11,24 @@ public class CalculatorController {
         this.model = model;
         this.view = view;
     }
+
+    public void runController() {
+        //입력받기
+        String input = view.getInput();
+
+        //커스텀 구분자 설정 및 문자열 가공
+        String new_input = model.checkCustomDelimiter(input);
+
+        //빈 문자열인지 체크
+        if (new_input.isEmpty()) {
+            view.displayResult(0);
+        } else {
+            //숫자와 구분자 필터링
+            model.filterInput(new_input);
+            //숫자들의 합 계산
+            int result = model.addNumber();
+            //결과 출력하기
+            view.displayResult(result);
+        }
+    }
 }
