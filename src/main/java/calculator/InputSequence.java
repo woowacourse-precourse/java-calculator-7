88b833f8113b22484b05e4e658871a1cec6e6c4b
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-18 03:23:13 +0900

    feat(InputSequence): validateCustomDelimiter 추가
    커스텀 구분자가 있는지 확인하고 없으면 ,와 :를 구분자로 사용하는 로직을 구현했습니다.

--- src/main/java/calculator/InputSequence.java
+++ src/main/java/calculator/InputSequence.java
@@ -4,12 +4,24 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputSequence {
-
+    public static final int DELIMITER_START_INDEX = 2;
     private final List<Long> sequence;
 
     public InputSequence(String input) {
-        this.sequence = Arrays.stream(input.split("[,:]"))
+        String delimiter = validateCustomDelimiter(input);
+        this.sequence = Arrays.stream(input.split(delimiter))
                             .map(Long::parseLong)
                             .toList();
+    }
+
+    private String validateCustomDelimiter(String input) {
+        if (input.startsWith("//")) {
+            for (int i = DELIMITER_START_INDEX; i < input.length(); i++) {
+                if (input.startsWith("\\n", i)) {
+                    return input.substring(DELIMITER_START_INDEX, i);
+                }
+            }
+        }
+        return "[,:]";
     }
 }
