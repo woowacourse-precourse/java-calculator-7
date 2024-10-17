36f7aa39f1c5370f769f6b90868a87f243273870
jenna <shinysuh1992@gmail.com>
2024-10-17 16:41:23 +0900

    feat: 예외 처리 로직 구현
    문자열 빈값, 숫자 배열 내 빈값, 음수 확인
    숫자로 시작하지 않는 문자열, 커스텀 구분자 닫히지 않은 경우 확인
    parseInt 중 NumberFormatException 발생 시, IllegalArgumentException 발생시킴
    
    Closes #5

--- src/main/java/calculator/operation/StringAdditionCalculator.java
+++ src/main/java/calculator/operation/StringAdditionCalculator.java
@@ -19,6 +19,9 @@ public class StringAdditionCalculator {
     }
 
     public int calculateSum() {
+        // 빈값 체크
+        this.validateEmptyString();
+        // 합계 반환
         return this.getSum();
     }
 
@@ -28,8 +31,17 @@ public class StringAdditionCalculator {
     private int getSum() {
         // 최종 구분자
         String finalDelimiter = getFinalDelimiter();
-        // 합계 반환
-        return Arrays.stream(numberString.split(finalDelimiter)).mapToInt(Integer::parseInt).sum();
+        // 숫자 추출
+        List<String> numbers = Arrays.stream(numberString.split(finalDelimiter)).toList();
+
+        try {
+            // 구분 결과 값 체크 (빈값, 음수)
+            this.validateArrayValues(numbers);
+            // 합계 반환
+            return numbers.stream().mapToInt(Integer::parseInt).sum();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("유효하지 않은 숫자 형식", e);
+        }
     }
 
     /**
@@ -56,8 +68,13 @@ public class StringAdditionCalculator {
      */
     private void extractCustomDelimiter() {
         if (!numberString.startsWith(DELIMITER_START)) {
+            // 커스텀 구분자 추출 후 문자열 형태 체크
+            this.validateNumberString();
             return;
         }
+
+        // 커스텀 구분자 indicator 가 제대로 닫혀 있는지 체크
+        this.checkDelimiterEndIndicator();
 
         int startIdx = numberString.indexOf(DELIMITER_START);
         int endIdx = numberString.indexOf(DELIMITER_END);
@@ -71,5 +88,69 @@ public class StringAdditionCalculator {
 
         // 추출되지 않은 커스텀 구분자가 남아있으면 추출 (recursive)
         this.extractCustomDelimiter();
+    }
+
+    /**
+     * 커스텀 구분자 indicator 가 제대로 닫혀 있는지 체크
+     */
+    private void checkDelimiterEndIndicator() {
+        if (!numberString.contains(DELIMITER_END)) {
+            throw new IllegalArgumentException("커스텀 구분자 지정 마크가 제대로 닫혀 있지 않음");
+        }
+    }
+
+    /**
+     * 빈값 체크
+     */
+    private void validateEmptyString() {
+        if (numberString.trim().isEmpty()) {
+            throw new IllegalArgumentException("빈 문자열");
+        }
+    }
+
+    /**
+     * 숫자로 시작하는 문자열인지 체크
+     */
+    private void validateNumberString() {
+        // 빈값 체크
+        this.validateEmptyString();
+        // 문자열이 숫자로 시작하는지 않으면 exception
+        if (!Character.isDigit(numberString.charAt(0))) {
+            throw new IllegalArgumentException("계산 문자열이 숫자로 시작하지 않음");
+        }
+    }
+
+    /**
+     * 배열 내 빈값, 음수 체크
+     *
+     * @param numbers 추출된 숫자 배열
+     */
+    private void validateArrayValues(List<String> numbers) {
+        this.checkIfEmptyValueExists(numbers);
+        this.checkIfMinusValueExists(numbers);
+    }
+
+    /**
+     * 배열 내 빈값 체크
+     *
+     * @param numbers 추출된 숫자 배열
+     */
+    private void checkIfEmptyValueExists(List<String> numbers) {
+        // 빈값 체크
+        if (numbers.isEmpty() || numbers.contains("") || numbers.contains(null)) {
+            throw new IllegalArgumentException("구분자 사이에 빈 값 존재");
+        }
+    }
+
+    /**
+     * 배열 내 음수 체크
+     *
+     * @param numbers 추출된 숫자 배열
+     */
+    private void checkIfMinusValueExists(List<String> numbers) {
+        boolean hasMinus = numbers.stream().anyMatch(num -> Integer.parseInt(num) < 0);
+        if (hasMinus) {
+            throw new IllegalArgumentException("음수 사용 불가");
+        }
     }
 }
