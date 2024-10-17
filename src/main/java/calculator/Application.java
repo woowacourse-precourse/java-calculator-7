f30294285cc0da2dc03a711a2d660f87ddbf13cd
lsj0822 <lsj0822@khu.ac.kr>
2024-10-17 15:57:52 +0900

    feat : CalculatorMain 작성
    계산기가 돌아가는 주요 클래스인 CalculatorMain의 구조를 작성하였습니다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,6 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         CalculatorMain calculatorMain = new CalculatorMain();
-        calculatorMain.StartCalculator();
+        calculatorMain.startCalculator();
     }
 }
--- src/main/java/calculator/CalculatorMain.java
+++ src/main/java/calculator/CalculatorMain.java
@@ -1,18 +1,25 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import inspector.ContentInspector;
 import java.math.BigInteger;
 
 public class CalculatorMain {
     private BigInteger calculateResult = new BigInteger("0");
     private String inputContent;
 
-    public void StartCalculator() {
+    public void startCalculator() {
+        setInputContent();
+        ContentInspector contentInspector = new ContentInspector(inputContent);
+        getCalculateResult();
+    }
+
+    private void setInputContent() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         inputContent = Console.readLine();
     }
 
-    public void getCalculateResult() {
+    private void getCalculateResult() {
         System.out.println("결과 : " + calculateResult);
     }
 }
