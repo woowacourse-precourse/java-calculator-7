08836288c596960ab7048f40ee9c6305ac4cce15
EHCTH <lees90896@gmail.com>
2024-10-17 17:21:35 +0900

    feat(OutputProcess): 결과 및 정보 인터페이스 구현체 작성 * feat: info 기능 추가 * feat: result 기능 추가

--- src/main/java/calculator/view/output/OutputProcess.java
+++ src/main/java/calculator/view/output/OutputProcess.java
@@ -1,7 +1,14 @@
 package calculator.view.output;
 
-public class ResultOutput {
-    public void output(int resultValue) {
-        System.out.printf("결과 : %d", resultValue);
+public class OutputProcess implements InfoService, ResultService{
+
+    @Override
+    public void info() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    @Override
+    public void result(long value) {
+        System.out.printf("결과 : %d", value);
     }
 }
