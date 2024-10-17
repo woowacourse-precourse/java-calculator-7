00565aa4873c402b560ae52a671850b5c0082903
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-10-17 21:41:02 +0900

    feat(Application) : 2. 커스텀 구분자가 지정되었는지 확인하고, 커스텀 구분자를 추출
    - 입력 문자열의 포맷을 통해, 커스텀 구분자의 지정 여부 확인
    - 커스텀 구분자가 지정된 경우, 값을 추출하여 반환
    - 커스텀 구분자가 지정되지 않은 경우, null 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,5 +7,21 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputString = Console.readLine();
         System.out.println("입력된 문자열 : " + inputString);
+
+        String customDelimiter = getCustomDelimiter(inputString);
+        System.out.println("커스텀 구분자 : " + (customDelimiter == null ? "없음" : customDelimiter));
+    }
+
+    private static String getCustomDelimiter(String string) {
+        String customDelimiterOpener = "//";
+        String customDelimiterCloser = "\\n";
+
+        if (string.length() < 5) {
+            return null;
+        }
+        if (!string.substring(0, 2).equals(customDelimiterOpener) || !string.substring(3, 5).equals(customDelimiterCloser)) {
+            return null;
+        }
+        return string.substring(2, 3);
     }
 }
