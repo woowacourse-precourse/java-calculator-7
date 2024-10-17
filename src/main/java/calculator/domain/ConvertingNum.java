0acc59b516922cf2ea70d0b41aaa5964e6674a2a
yedamhy <yeadalm04@gmail.com>
2024-10-18 03:17:55 +0900

    ♻️ refactor: InvalidInputException에서 IllegalArgumentException으로 변경
    - 불필요한 사용자 정의 예외를 제거하고 표준 예외 사용

--- src/main/java/calculator/domain/ConvertingNum.java
+++ src/main/java/calculator/domain/ConvertingNum.java
@@ -1,7 +1,5 @@
 package calculator.domain;
 
-import calculator.exception.InvalidInputException;
-
 public class ConvertingNum {
     public int[] convertNums(String[] str){
         int[] nums = new int[str.length];
@@ -9,7 +7,7 @@ public class ConvertingNum {
         for(int i=0; i<str.length;i++){
             nums[i] = convertNum(str[i]);
             if(nums[i] < 0){
-                throw new InvalidInputException("음수를 제외하고 입력해주세요. ➣ " + nums[i]);
+                throw new IllegalArgumentException("음수를 제외하고 입력해주세요. ➣ " + nums[i]);
             }
         }
         return nums;
@@ -20,7 +18,7 @@ public class ConvertingNum {
         try{
             return Integer.parseInt(str);
         }catch(NumberFormatException e) {
-            throw new InvalidInputException("잘못된 숫자 형식입니다. ➣ " +str);
+            throw new IllegalArgumentException("잘못된 숫자 형식입니다. ➣ " +str);
         }
     }
 }
--- src/main/java/calculator/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package calculator.exception;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
