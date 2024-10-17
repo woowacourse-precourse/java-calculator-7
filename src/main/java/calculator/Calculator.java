0918e8a3b8c28388f49eec688bb6e2eb18620cd7
wingyou <winwing.you@gmail.com>
2024-10-17 17:17:46 +0900

    test(Calculator): add addition test

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+/**
+ * Calculator
+ */
+public class Calculator {
+
+    public int addNumbers(int[] numbers) {
+        // TODO: implement method
+        return 0;
+    }
+}
