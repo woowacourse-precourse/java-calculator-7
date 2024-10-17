1a99575c3f5fbc16ebee18e4f74808a165019808
5ms <oms010414@gmail.com>
2024-10-17 19:31:25 +0900

    Sepator 클래스 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,9 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Separator separator = new Separator(',',':');
+
+
     }
 }
--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -1,4 +1,22 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Separator {
+    final private HashSet<Character> separatorSet = new HashSet<>();
+
+
+    public Separator(Character... separators){
+        separatorSet.addAll(Arrays.asList(separators));
+    }
+
+    public void addSeparator(Character c){
+        separatorSet.add(c);
+    }
+
+    public boolean isSeparator(Character c){
+        return separatorSet.contains(c);
+    }
+
 }
