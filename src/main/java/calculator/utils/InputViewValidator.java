f188e7dcec5f130eab07692e56349144cf21690d
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-10-17 16:43:22 +0900

    feat(utils): InputViewValidator class develop validation
    - InputViewValidator 클래스의 검증및 예외 기능 구현

--- src/main/java/calculator/utils/InputViewValidator.java
+++ src/main/java/calculator/utils/InputViewValidator.java
@@ -2,22 +2,21 @@ package utils;
 
 public class InputViewValidator {
 
+    private static final String ERROR_PREFIX = "Error: ";
+    private static final String INPUT_NO_QUOTATION_PAIR = ERROR_PREFIX + "입력에 큰 따음표로 감싸져있지 않습니다.";
+    private static final String CUSTOM_NOT_CHAR = ERROR_PREFIX + "커스텀 값에 숫자는 허용하지 않습니다.";
 
-    public String validateQutoPair(String inputName) {
-
+    public void validateQuotationPair(String inputName) {
         if (inputName == null || !inputName.startsWith("\"") || !inputName.endsWith("\"")) {
-            throw new IllegalArgumentException("입력은 큰따옴표로 감싸져 있어야 합니다.");
+            throw new IllegalArgumentException(INPUT_NO_QUOTATION_PAIR);
         }
-        return inputName.substring(1, inputName.length() - 1);
     }
 
-    public Boolean validateCustom(String inputName) {
 
-        return false;
-    }
-
-    public void validateDuplicationCustom(String inputName) {
-
+    public void validateCustomNumeric(String customData) {
+        if (customData.matches("\\d+")) {
+            throw new IllegalArgumentException(CUSTOM_NOT_CHAR);
+        }
     }
 
 }
