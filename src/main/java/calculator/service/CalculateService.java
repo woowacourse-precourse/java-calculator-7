d30eb1f177fc80d18a84f942f6cdc2353f08b7b9
Jinu Yi <dionisos198@naver.com>
2024-10-17 17:14:34 +0900

    feat(CalculateService): 사용자 입력 구현

--- src/main/java/calculator/service/CalculateService.java
+++ src/main/java/calculator/service/CalculateService.java
@@ -2,4 +2,11 @@ package calculator.service;
 
 //계산기의 전반적인 기능을 담당하는 클래스
 public class CalculateService {
+
+
+    //임시 메서드
+    public Long calculate(final String userInput) {
+
+        return null;
+    }
 }
