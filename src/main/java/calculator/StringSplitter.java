62f6457ba1494be4524923679528a0090d2b76ba
SungJun <kmymy00@naver.com>
2024-10-17 17:48:03 +0900

    fix(StringSplitter): 커스텀 구분자 버그 수정
    Change:
    -split() 메서드 내 정규식 생성 과정에서 발생하는 버그 수정
    -getCustomDelimiter() 메서드 내 닫는 대괄호 이스케이프 문자 추가 버그 수정

--- src/main/java/calculator/StringSplitter.java
+++ src/main/java/calculator/StringSplitter.java
@@ -8,46 +8,31 @@ public class StringSplitter {
     public List<String> split(String str) {
         // 구분자를 정규식으로 치환
         StringBuilder regexBuilder = new StringBuilder();
-        regexBuilder.append("[,:");
+        regexBuilder.append("[,:]");
 
         // 커스텀 구분자 유무 확인 후, 정규식 추가
         if (str.startsWith("//") && str.contains("\\n")) {
             if (str.indexOf("\\n") - 2 > 0) {
-                regexBuilder.append(getCustomDelimiter(str));
+                String customDelimiter = getCustomDelimiter(str);
+                regexBuilder.insert(3, customDelimiter);
             }
             str = str.substring(str.indexOf("\\n") + 2);
         }
-
-        regexBuilder.append("]");
 
         String regex = regexBuilder.toString();
 
         return new ArrayList<>(Arrays.asList(str.split(regex)));
     }
 
-    private List<String> splitByDelimiter(List<String> stringList, String delimiter) {
-        List<String> splitStringList = new ArrayList<>();
-
-        for (String str : stringList) {
-            if (str.contains(delimiter)) {
-                String[] splitStringArr = str.split("[" + delimiter + "]");
-                splitStringList.addAll(Arrays.asList(splitStringArr));
-            } else {
-                splitStringList.add(str);
-            }
-        }
-
-        return splitStringList;
-    }
-
     private String getCustomDelimiter(String str) {
         StringBuilder customDelimiterBuilder = new StringBuilder(str.substring(2, str.indexOf("\\n")));
+
         if (customDelimiterBuilder.toString().contains("[")) {
             customDelimiterBuilder.insert(customDelimiterBuilder.indexOf("["), "\\");
         }
 
         if (customDelimiterBuilder.toString().contains("]")) {
-            customDelimiterBuilder.insert(customDelimiterBuilder.indexOf("["), "\\");
+            customDelimiterBuilder.insert(customDelimiterBuilder.indexOf("]"), "\\");
         }
 
         return customDelimiterBuilder.toString();
