34ca682cbdf6ddfe8f964937f65ef7f5ab9b08bd
LSBsity <dltmdqls51@naver.com>
2024-10-17 15:25:11 +0900

    feat: 메인 메소드 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import calculator.calc.Calculatable;
+import calculator.calc.Calculator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Calculatable calculator = new Calculator();
+
+        String input = getInput();
+
+        long result = calculator.sumOfString(input);
+        System.out.println("결과 : " + result);
+
+        Console.close();
+    }
+
+    private static String getInput() {
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = Console.readLine();
+        System.out.println("input = " + input);
+        return input;
     }
 }
