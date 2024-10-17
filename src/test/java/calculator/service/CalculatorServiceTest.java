fb489908bb4372686e4b003ae05e59ac40026e49
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-17 17:31:02 +0900

    test(CalculatorServiceTest): 구분자가 입력되지 않고, 문자가 입력되었을 때 예외가 발생하는 테스트 코드 작성

--- src/test/java/calculator/service/CalculatorServiceTest.java
+++ src/test/java/calculator/service/CalculatorServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CalculatorServiceTest {
 
@@ -54,6 +55,36 @@ class CalculatorServiceTest {
 
         // then
         assertThat(result).isEqualTo(123);
+    }
+    
+    @Test
+    @DisplayName("구분자가 입력되지 않고, 문자가 입력되었을 때 예외가 발생한다.")
+    void 구분자가_없고_문자가_입력된_경우_예외_발생() {
+        // given
+        String input = "a";
+
+        // when, then
+        assertThatThrownBy(() -> delimiterSplit(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구분자와 숫자를 정확하게 입력해주세요.");
+    }
+
+    private void delimiterSplit(String input) {
+        if (isNotDefaultDelimiter(input) && isNotCustomDelimiter(input)) {
+            try {
+                Integer.parseInt(input);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("구분자와 숫자를 정확하게 입력해주세요.");
+            }
+        }
+    }
+
+    private boolean isNotDefaultDelimiter(String input) {
+        return !(input.contains(",") || input.contains(":"));
+    }
+
+    private boolean isNotCustomDelimiter(String input) {
+        return !input.startsWith("//");
     }
 
 }
