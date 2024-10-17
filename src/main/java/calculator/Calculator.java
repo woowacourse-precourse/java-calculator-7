fdfd0602c2b150a01ab36a8b59bbc2e0888b8c5f
garden14 <ci667@dgu.ac.kr>
2024-10-17 22:26:08 +0900

    docs: README.md 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
     private static final String ENTER_MESSAGE = "덧셈할 문자열을 입력해주세요.";
+    private static final String COLON_SEPARATOR = ":";
+    private static final String COMMA_SEPARATOR = ",";
+    private static final String SLASH_SEPARATOR = "//";
+    private static final String LINE_BREAKING_SEPARATOR = "\n";
 
     public static void start() {
         System.out.println(ENTER_MESSAGE);
