c52b58c8f76f9749c924c7c9cb59c3e0aa33e30a
aaahyunseo <hs030608@naver.com>
2024-10-17 23:04:15 +0900

    feat(OutputView): 결과 출력 View 생성

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,12 @@
+package view;
+
+public class OutputView {
+    static final String OUTPUT_MESSAGE = "결과 :";
+
+    /**
+     * 결과 출력 함수
+     * **/
+    public static void output(int sum){
+        System.out.printf("%s %d", OUTPUT_MESSAGE, sum);
+    }
+}
