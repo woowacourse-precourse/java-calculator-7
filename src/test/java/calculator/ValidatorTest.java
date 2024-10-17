aab48c1a24ca1c6098f52e3a41a07289ef5579fe
박철현 <park82602@gmail.com>
2024-10-17 19:07:02 +0900

    style($validator) : ValidatorTest 클래스의 메서드명 변경으로 인한 삭제

--- src/test/java/calculator/ValidatorTest.java
+++ src/test/java/calculator/ValidatorTest.java
@@ -6,24 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 class ValidatorTest {
-    @Test
-    void validateIfNotNumberIfInputNegative() {
-        //given
-        String input = "1,-2:3";
-        List<Integer> numbersAfterParsing = DelimiterParser.getNumbersAfterParsing(input);
-        //when then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Validator.validateIfInputNegative(numbersAfterParsing);
-        });
-    }
-    @Test
-    void validateIfNotNumber() {
-        //given
-        String input = "1@2#3";
-        //when
-        //then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Validator.validateIfNotNumber(input);
-        });
-    }
+
+
 }
