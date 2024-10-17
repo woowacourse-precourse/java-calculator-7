ca42917b9d595a130aaa4f303b69153351fc11c2
dradnats1012 <herjebi1012@gmail.com>
2024-10-17 23:43:52 +0900

    feat: 계산기 클래스 생성

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -1,4 +1,22 @@
 package calculator.model;
 
+import java.util.List;
+
 public class Calculator {
+
+    List<Integer> numList;
+
+    public Calculator(List<Integer> numList) {
+        this.numList = numList;
+    }
+
+    public Integer addNums() {
+        int total = 0;
+
+        for (int num : numList) {
+            total += num;
+        }
+
+        return total;
+    }
 }
