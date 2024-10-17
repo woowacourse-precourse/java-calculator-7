42465725ee40ee9defe37ea2a61166b958c87b8e
patternplane <bss011202@naver.com>
2024-10-18 02:29:23 +0900

    feat(Parser): 커스텀 구분자 해석 기능 추가
    - 분리된 커스텀 구분자로부터 각 구분자들을 등록합니다.
    - 빠른 구분자 탐색을 위해 HashSet으로 구분자를 저장합니다.

--- src/main/java/calculator/NumbersParser.java
+++ src/main/java/calculator/NumbersParser.java
@@ -2,18 +2,23 @@ package calculator;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.HashSet;
 
 public class NumbersParser {
     
     private static final Pattern areaDividePattern = Pattern.compile("^(?://([^0-9]*)\\\\n)?(.*)");
     private String separatorArea = null;
     private String numberArea = null;
+
+    static final char[] defaultSeparators = new char[] { ',', ':' };
+    private HashSet<Character> separatorSet = null;
     
     public NumbersParser(String input) {
         if (input == null) {
             throw new IllegalArgumentException();
         }
         setAreaByPattern(input);
+        separatorSet = parseSeparator();
     }
     
     private void setAreaByPattern(String input) {
@@ -24,5 +29,18 @@ public class NumbersParser {
         
         separatorArea = (matchResult.group(1) == null ? "" : matchResult.group(1)); 
         numberArea = (matchResult.group(2) == null ? "" : matchResult.group(2));
+    }
+    
+    private HashSet<Character> parseSeparator() {
+        separatorSet = new HashSet<Character>(defaultSeparators.length + separatorArea.length());
+        
+        for (int i = 0; i < defaultSeparators.length; i++) {
+            separatorSet.add(defaultSeparators[i]);
+        }
+        for (int i = 0; i < separatorArea.length(); i++) {
+            separatorSet.add(separatorArea.charAt(i));
+        }
+        
+        return separatorSet;
     }
 }
