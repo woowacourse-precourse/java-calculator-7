451755e9d4f3c1a5ccc2b46266fee09db08c7ead
joonwan <joonwan1024@naver.com>
2024-10-17 19:33:27 +0900

    feat: DataParser 의 검증 메서드를 모아둔 Validator 클래스 작성

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -1,4 +1,42 @@
 package calculator;
 
 public class Validator {
+
+    public static void checkValidCustomFormat(String inputData) {
+        if (!inputData.contains("\\n")) {
+            throw new IllegalArgumentException();
+        }
+        String[] splitData = inputData.split("\\\\n");
+        int count = splitData.length;
+        if (!(count == 1 || count == 2)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkValidContents(char[] separators, char[] contents) {
+        int index = 0;
+        while (index < contents.length) {
+            char c = contents[index];
+            if (!(isSeparators(c, separators) || isNumber(c))) {
+                throw new IllegalArgumentException();
+            }
+            index++;
+        }
+    }
+
+    private static boolean isSeparators(char c, char[] separators) {
+        for (int i = 0; i < separators.length; i++) {
+            if (c == separators[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumber(char c) {
+        if ('0' <= c && c <= '9') {
+            return true;
+        }
+        return false;
+    }
 }
