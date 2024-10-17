adf6d2b2aef3d84a6cae9d17da2ae2ef8fd5553e
eusan <goodsan0330@gamil.com>
2024-10-17 11:03:21 +0900

    feat(input): implement input handling and validation based on delimiters

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,23 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
 
-        try {
-            System.out.println("덧셈할 문자열을 입력해 주세요.");
-            String userInput = Console.readLine();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String userInput = Console.readLine();
 
-            InputInfo inputInfo = new InputInfo();
-            inputInfo.validateInput(userInput);
+        InputInfo inputInfo = new InputInfo();
+        inputInfo.validateFormat(userInput);
 
-            /*
-                AddCalculator 객체에 inputInfo field 전달 후, format에 맞에 계산
-             */
-
-        } catch(IllegalArgumentException e) {
-            return;
-        }
-
-
-
+        /*
+            AddCalculator 객체에 inputInfo field 전달 후, format에 맞게 계산
+         */
 
     }
 }
--- src/main/java/calculator/InputInfo.java
+++ src/main/java/calculator/InputInfo.java
@@ -2,30 +2,53 @@ package calculator;
 
 public class InputInfo {
 
-    private char separator;
+    private String separator;
     private String format;
 
-    public char getSeparator() { return separator; }
+    public String getSeparator() { return separator; }
     public String getFormat() { return format; }
 
-    public void validateInput(String input) throws IllegalArgumentException {
+    public void validateFormat(String input) throws IllegalArgumentException {
 
-        if(input.length() == 0) return;
-        else if( '0' <= input.charAt(0) && input.charAt(0) <= '9') {
-            // basic format
-
-            return;
+        if(input.length() == 0) {
+            separator = "";
+            format = "basic";
         }
-        else if(input.length() >= 5
-                && input.substring(0,2).equals("//") && input.substring(3,5).equals("\\n")
-                && '0' <= input.charAt(2) && input.charAt(2) <= '9') {
-            // custom format
 
-
-            return;
+        if(input.length() > 5 && input.substring(0,2).equals("//") && input.substring(3,5).equals("\\n")) {
+            /* custom format */
+            separator = input.substring(2,3);
+            format = "custom";
+            validateNumbers(input.substring(5));
+        } else {
+            /* basic format */
+            separator = ",:";
+            format = "basic";
+            validateNumbers(input);
         }
-        else {
-            throw new IllegalArgumentException("입력에서 구분자 형식을 찾을 수 없습니다.");
+    }
+
+    private void validateNumbers(String str) throws IllegalArgumentException {
+        /*
+         * [예외 발생]
+         * 알맞은 구분자와 숫자 이외의 문자가 들어온 경우
+         * 양의 정수가 아닌 수가 들어오는 경우
+         */
+
+        if(!str.matches("[0-9" + separator + "]+")) {
+            throw new IllegalArgumentException("구분자와 숫자만 입력할 수 있습니다.");
+        }
+
+        String[] numbers = str.split("[" + separator + "]");
+
+        for(String number : numbers) {
+            System.out.println(number);
+            if (!number.isEmpty()) {
+                int num = Integer.parseInt(number);
+                if (num <= 0) throw new IllegalArgumentException("양의 정수만 가능합니다.");
+            } else {
+                throw new IllegalArgumentException("구분자의 위치가 적절하지 않습니다.");
+            }
         }
     }
 }
