8927b58ac012316028e0450e3f7ca99e35b70d8a
1876070677 <kdl5001@catholic.ac.kr>
2024-10-17 22:32:04 +0900

    feat(Application.java): 기본 구분자를 활용한 덧셈 기능 추가
    기본 구분자를 사용한 경우의 문자열 덧셈
    - 문자열을 기본 구분자 :와 ,를 통해 분리하고 분리된 숫자들의 덧셈 구현
    - 공백 문자열 입력시 0 출력
    - 음수나 문자가 입력되었을 시, 적절한 예외 발생

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,29 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int sum = 0;
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String msg = Console.readLine();
+        if (!msg.equals("")) {
+            String[] numbers = msg.split(":|,");
+            try {
+                System.out.println(numbers[0]);
+                for (String number : numbers) {
+                    int temp = Integer.parseInt(number);
+                    if (temp > 0) {
+                        sum += temp;
+                    } else {
+                        throw new Exception();
+                    }
+                }
+            } catch (Exception e) {
+                throw new IllegalArgumentException();
+            }
+        }
+        System.out.println("결과 : " + sum);
     }
 }
