a46931c08557f8a47404e117ecbdce0d6725cebc
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-17 22:51:31 +0900

    test(SeparatorExtractorTest): 커스텀 구분자 추출 테스트

--- src/test/java/calculator/SeparatorExtractorTest.java
+++ src/test/java/calculator/SeparatorExtractorTest.java
@@ -1,4 +1,28 @@
+package calculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class SeparatorExtractorTest {
-  
+    @Test
+    public void 커스텀_구분자_추출() {
+        //given
+        SeparatorExtractor extractor = new SeparatorExtractor();
+        List<String> customSeparator = new ArrayList<>();
+        String input = "//;\n4,5:6;//--\n1;2--//^^^\n1;2--";
+        //when
+        String stringAfterExtract = extractor.extractCustomSeparator(input, customSeparator);
+
+        //then
+        assertThat(customSeparator.size()).isEqualTo(3);
+        assertThat(customSeparator).containsExactly(";", "--", "^^^");
+        assertThat(stringAfterExtract).isEqualTo("4,5:6;1;2--1;2--");
+    }
+
 }
