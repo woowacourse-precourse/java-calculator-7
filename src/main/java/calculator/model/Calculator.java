7cde595e053f09070ca8f868980427b5bdaf484f
Chaeyoung714 <202102760@hufs.ac.kr>
2024-10-17 15:35:48 +0900

    feat : 2.2.1 입력값 타입 확인 기능 구현
    model/Calculator : 생성자, isNumber 메서드 추가
        입력값을 하나씩 숫자인지, 아닌지 확인하는 단계까지 구현
        입력값을 하나씩 순회하며 분기처리하는 것은 다음 단계부터 할 예정
    test/model/CalculatorTest : 입력값 타입 확인하는 테스트 추가, 통과

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -1,4 +1,16 @@
 package calculator.model;
 
 public class Calculator {
+    private String inputValue;
+    private int sum;
+
+    public Calculator(String inputValue) {
+        this.inputValue = inputValue;
+        this.sum = 0;
+    }
+
+    public boolean isNumber(String value) {
+        String numberIncludingMinusAndZeroRegex = "[0-9-]";
+        return value.matches(numberIncludingMinusAndZeroRegex);
+    }
 }
--- src/test/java/calculator/model/CalculatorTest.java
+++ src/test/java/calculator/model/CalculatorTest.java
@@ -1,4 +1,21 @@
 package calculator.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+
+    @Test
+    void 입력값_타입_확인() {
+        String testString = "1,-0 :";
+        Calculator calculator = new Calculator(testString);
+
+        assertThat(calculator.isNumber(testString.substring(0,1))).isTrue();
+        assertThat(calculator.isNumber(testString.substring(1,2))).isFalse();
+        assertThat(calculator.isNumber(testString.substring(2,3))).isTrue();
+        assertThat(calculator.isNumber(testString.substring(3,4))).isTrue();
+        assertThat(calculator.isNumber(testString.substring(4,5))).isFalse();
+        assertThat(calculator.isNumber(testString.substring(5,6))).isFalse();
+    }
 }
