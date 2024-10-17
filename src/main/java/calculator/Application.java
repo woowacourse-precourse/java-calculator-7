7703fb089c5d7149a397f2e84dcffc8f408b0de7
wingyou <winwing.you@gmail.com>
2024-10-17 20:51:59 +0900

    feat(Application): implement program

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private static Parser parser = new Parser();
+    private static Calculator calculator = new Calculator();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String line = Console.readLine();
+
+        int[] numbers = parser.parse(line);
+        int sum = calculator.addNumbers(numbers);
+
+        System.out.println("결과 : " + sum);
     }
 }
