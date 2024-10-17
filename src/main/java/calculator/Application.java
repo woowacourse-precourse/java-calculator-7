cc34a34a127d9c1fd5b90d20b9939637c7442d04
yedamhy <yeadalm04@gmail.com>
2024-10-18 03:19:05 +0900

    🚀 feat(calculator): 전체 애플리케이션 실행을 위한 Application 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import calculator.domain.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutView;
+
 public class Application {
+    private final InputView inputView = new InputView();
+    private final OutView outView = new OutView();
+    private final StringCalculator stringCalculator = new StringCalculator();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new Application().run();  // 애플리케이션 실행
     }
+
+    private void run() {
+
+        // 사용자 입력 받기
+        String input = inputView.getStringFromUser();
+
+        int result = stringCalculator.calculate(input);
+
+        //결과 출력
+        outView.printResult(result);
+
+    }
+
 }
