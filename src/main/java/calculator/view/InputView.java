e80406d0bf8c284f8d029eb83c2de84b40956428
BaSak0630 <babamba125@gmail.com>
2024-10-17 12:47:13 +0900

    fix : input string is empty check 빈 공백을 입력 받는 경우 예외 처리되게 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String readString() {
-        return Console.readLine();
+        String s = Console.readLine();
+        if(s.isEmpty()) throw new IllegalArgumentException("[ERROR]덧셈할 문자열을 입력해 주세요");
+        return s;
     }
 }
