d4b00541ecf7bd5fa1979b4ab83e5a75d7574b5b
imjunghun <dlawndgns9@naver.com>
2024-10-17 15:38:15 +0900

    feat(Numbers): Number를 List로 저장할 Numbers 클래스 생성

---
+++ src/main/java/calculator/domain/number/Numbers.java
@@ -0,0 +1,12 @@
+package calculator.domain.number;
+
+import java.util.List;
+
+public class Numbers {
+
+    private List<Number> numbers;
+
+    public Numbers(List<Number> numbers) {
+        this.numbers = numbers;
+    }
+}
