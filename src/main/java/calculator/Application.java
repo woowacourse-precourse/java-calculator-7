82e7d71fdb39f00930c84993361ce57443a782cf
sonsuchang <son336208@naver.com>
2024-10-17 23:40:23 +0900

    feat : 초기 구분자 리스트 생성 및 기능 목록 재구성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,5 +5,9 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputString = camp.nextstep.edu.missionutils.Console.readLine();
+        int len_inputString = inputString.length();
+        String[] separatorArray = new String[len_inputString];
+        separatorArray[0] = ",";
+        separatorArray[1] = ":";
     }
 }
