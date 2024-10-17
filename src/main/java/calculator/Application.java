c87151c5c394e60b9b78c20b25b2740a7c8b8cdf
Hanjjong <tim4974@naver.com>
2024-10-17 17:30:32 +0900

    feat(CalculatorInputHandler, main): 사용자 입력을 위한 안내문구 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,8 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorInputHandler calculatorInputHandler = new CalculatorInputHandler();
+
+        calculatorInputHandler.run();
     }
 }
---
+++ src/main/java/calculator/CalculatorInputHandler.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class CalculatorInputHandler {
+
+    public CalculatorInputHandler() {
+    }
+
+    public void run() {
+        printMessage();
+    }
+
+    private void printMessage() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+}
