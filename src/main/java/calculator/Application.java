393de8de3cb864708dd91f929f6dc4bb177bf3c7
eunho <eunho1032@gmail.com>
2024-10-17 15:28:12 +0900

    feat(Application): add simple code

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,5 +7,11 @@ public class Application {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("덧셈할 문자열을 입력해 주세요.");
 		String input = scanner.nextLine();
+		String[] nums = input.split("[,;]");
+		int result = 0;
+
+		for (String num : nums)
+			result += Integer.parseInt(num);
+		System.out.println("결과 : " + result);
 	}
 }
