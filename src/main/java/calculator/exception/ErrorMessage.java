5bd45fbc0e477dfa07d97b6abda4b305904ba865
IM-GYURI <kgulr0517@gmail.com>
2024-10-18 01:06:15 +0900

    feat : add ErrorMessage for Exception Handling
    - NULL_OR_EMPTY_INPUT
    - NEGATIVE_NUMBER
    - INVALID_INPUT

---
+++ src/main/java/calculator/exception/ErrorMessage.java
@@ -0,0 +1,7 @@
+package calculator.exception;
+
+public class ErrorMessage {
+    public static final String NULL_OR_EMPTY_INPUT = "입력 값은 null 또는 빈 값일 수 없습니다.";
+    public static final String NEGATIVE_NUMBER = "음수는 입력할 수 없습니다.";
+    public static final String INVALID_INPUT = "유효하지 않은 입력 값입니다.";
+}
