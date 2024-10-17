8a2d92ff1831826c7c16ddee3bee9e014e0ee97f
chyun <och2510@gmail.com>
2024-10-18 01:22:23 +0900

    refactor(DelimiterTest): 단일 배열을 지정해 단일 인수만 제공하도록 변경

--- src/test/java/calculator/model/DelimiterTest.java
+++ src/test/java/calculator/model/DelimiterTest.java
@@ -7,22 +7,17 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class DelimiterTest {
 
 	@DisplayName("구분자는 길이가 1이어야 한다")
 	@ParameterizedTest
-	@MethodSource("blankOrLengthOver1Delimiter")
+	@EmptySource
+	@ValueSource(strings = {"chihyun", "ch", "", " ", "\n", "\t"})
 	void validateLength(String invalidData) {
 		assertThrows(IllegalArgumentException.class, () -> Delimiter.from(invalidData));
-	}
-
-	static Stream<Arguments> blankOrLengthOver1Delimiter() {
-		return Stream.of(
-			Arguments.of(""),
-			Arguments.of(" "),
-			Arguments.of("gd")
-		);
 	}
 }
