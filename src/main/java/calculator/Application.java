1b9a69775820b9e378eed84d9fbda620159e7412
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-17 16:24:30 +0900

    refactor(Application, Validator): 문자열 유효성 검사 -> Validator 클래스 생성후 이동
    유효성 검사기능만을 따로 모아 관리할 수 있도록 checkBeginAndEndOfValue 메서드와 isNum 메서드 이동
    또한 메서드를 private 접근제한자로 캡슐화함
    캡슐화된 유효성 검사 기능들을 함께 실행시키기 위해 public 접근 제한자의 validate() 메서드 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,34 +10,10 @@ public class Application {
         // TODO: 프로그램 구현
         List<String> separators = new ArrayList<>();
 
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         Console.close();
 
-        checkBeginAndEndOfValue(input);
-
-        
     }
-
-    //문자열의 시작과 끝 확인
-    private static void checkBeginAndEndOfValue(String value) {
-        //문자열의 시작 확인
-        if (!value.startsWith("//") && !isNum(value.charAt(0))) {
-            throw new IllegalArgumentException("문자열의 시작은 커스텀 구분자 선언 또는 양수여야 합니다");
-        }
-
-        //문자열의 끝 확인
-        if (!value.endsWith("\n")|| !isNum(value.charAt(value.length() - 1))) {
-            throw new IllegalArgumentException("문자열의 끝은 커스텀 구분자 선언 종료 또는 양수여야 합니다.");
-        }
-    }
-
-
-    //char값이 1 ~ 9 사이의 양수인지 확인
-    private static boolean isNum(char c) {
-        return c >= 1 && c <= 9;
-    }
-
-
-
 
 }
--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -1,4 +1,27 @@
 package calculator;
 
 public class Validator {
+
+    public static void validate(String value) {
+        checkBeginAndEndOfValue(value);
+    }
+
+    //문자열의 시작과 끝 확인
+    private static void checkBeginAndEndOfValue(String value) {
+        //문자열의 시작 확인
+        if (!value.startsWith("//") && !isNum(value.charAt(0))) {
+            throw new IllegalArgumentException("문자열의 시작은 커스텀 구분자 선언 또는 양수여야 합니다");
+        }
+
+        //문자열의 끝 확인
+        if (!value.endsWith("\n") && !isNum(value.charAt(value.length() - 1))) {
+            throw new IllegalArgumentException("문자열의 끝은 커스텀 구분자 종료 선언 또는 양수여야 합니다.");
+        }
+    }
+
+
+    //char값이 1 ~ 9 사이의 양수인지 확
+    private static boolean isNum(char c) {
+        return c >= '1' && c <= '9';
+    }
 }
