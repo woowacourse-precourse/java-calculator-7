91afe882907d6bd0add914b7aded040219cabb48
kimgoat <kimgoatlove@gmail.com>
2024-10-17 16:57:04 +0900

    refactor: 커스텀 구분자 판별하는 로직 분리
    가독성을 위해서 커스텀 구분자를 판별하는 로직을 함수로 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -29,12 +29,15 @@ public class Application {
         userInput = input.replace("\\n", "\n");
     }
 
+    public boolean isCustomSeparator(){
+        return userInput.matches("//\\D+(\r?\n)[\\s\\S]+");
+    }
+
     /**
      * 구분자를 기준으로 숫자 추출하여 배열에 저장하는 함수
      */
     public void extractNumber(){
-        // 커스텀 구분자를 지정한 경우
-        if(userInput.matches("//\\D+(\r?\n)[\\s\\S]+")){
+        if(isCustomSeparator()){
             StringBuilder separator = new StringBuilder();
             int idx = 2;
             while(true){
@@ -45,7 +48,6 @@ public class Application {
                     replaceAll("([\\\\.\\[\\](){}*+?^$|])", "\\\\$1");
             numbers = userInput.substring(idx + 1).split(regexSeparator);
         }
-        // 커스텀 구분자를 지정하지 않은 경우 - 기본 구분자: 쉼표(,), 콜론(:)
         else{
             numbers = userInput.split("[,:]");
         }
