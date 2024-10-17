79860a695398c6d25d50d48c9faa200517f5a8ea
hanmh319 <hanmh319@naver.com>
2024-10-17 17:50:53 +0900

    feat: java-calculattor 1ì°기능 êµ구현

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # java-calculator-precourse
+[![Typing SVG](https://readme-typing-svg.demolab.com?font=Alkatra&weight=500&size=45&duration=3500&pause=3&color=6994CDEE&center=false&vCenter=false&multiline=true&repeat=true&width=1000&height=100&lines=hanmh319👋)](https://git.io/typing-svg)
+
+## 요구사항 목록
+<div style="display:flex; flex-direction:column; align-items:flex-start;">
+
+### 기능 요구사항
+1. 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환한다
+2. 앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있다. 커스텀 구분자는 문자열 앞부분의 "//"와 "\n" 사이에 위치하는 문자를 커스텀 구분자로 사용한다.
+3. 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,43 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String a = Console.readLine();
+
+        String separator1 = ","; // 구분자 설정
+        String separator2 = ":"; // 구분자 설정2
+        String separator3 = ""; // 커스텀 구분자 추가 설정, 공백으로 초기화
+        String[] sepArr;
+        int sum = 0;
+
+
+        Pattern pattern = Pattern.compile("//(.*?)\\\\n");
+        ;// "//", "\n" 사이의 문자 추출
+        Matcher matcher = pattern.matcher(a);
+
+        if (matcher.find()) {
+            separator3 = matcher.group(1); // 추출된 문자가 존재한다면 구분자로 지정
+            a = a.substring(matcher.end()).trim();
+        }
+        if (separator3.isEmpty()) {
+            sepArr = a.split(separator1 + "|" + separator2); // 구분자가 없을 때
+        } else {
+            sepArr = a.split(separator1 + "|" + separator2 + "|" + Pattern.quote(separator3)); // 모든 구분자로 나누기
+        }
+
+        System.out.println("나뉜 이후 문자열: " + a);
+
+        for (String numStr : sepArr) {
+            try {
+                sum += Integer.parseInt(numStr.trim()); // 공백 제거 후 파싱
+            } catch (NumberFormatException e) {
+                System.out.println("유효하지 않은 문자열이 포함되어 있습니다");
+            }
+        }
+        System.out.println("#" + " " + sum);
     }
 }
