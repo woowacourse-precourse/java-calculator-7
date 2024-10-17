abd33524e48abaaa51edf148a28e70b1f0f6809d
imjunghun <dlawndgns9@naver.com>
2024-10-17 21:43:10 +0900

    refactor(Number): 숫자인지 아닌지는 구분자에게 책임을 넘기고 숫자만 저장하는 역할에 충실하도록 리팩토링

--- src/main/java/calculator/domain/number/Number.java
+++ src/main/java/calculator/domain/number/Number.java
@@ -4,33 +4,11 @@ public class Number {
 
     private final int value;
 
-    public Number(String value) {
-        this.value = validate(value);
+    public Number(int value) {
+        this.value = value;
     }
 
     public int getValue() {
         return value;
-    }
-
-    private int validate(String value) {
-        if (isBlank(value)) {
-            return 0;
-        }
-        return validateNumber(value);
-    }
-
-    private int validateNumber(String value) {
-        if (isNotNumeric(value)) {
-            throw new IllegalArgumentException();
-        }
-        return Integer.parseInt(value);
-    }
-
-    private boolean isBlank(String value) {
-        return value.isBlank();
-    }
-
-    private boolean isNotNumeric(String value) {
-        return !value.matches("\\d+");
     }
 }
