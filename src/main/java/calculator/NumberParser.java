e3f51c113168db4ea6ce4ffd4079b3c3c1e3f2ca
lchyeong <lchyeong@gmail.com>
2024-10-17 10:16:28 +0900

    feat(NumberParser): 배열에서 숫자만 필터링하는 replaceNumber 메서드 추가
    - String 배열에서 숫자만 필터링하여 반환하는 메서드 추가

--- src/main/java/calculator/NumberParser.java
+++ src/main/java/calculator/NumberParser.java
@@ -1,6 +1,10 @@
 package calculator;
 
 public class NumberParser {
-
-
+    public String[] replaceNumber(String[] inputArr){
+        for(int i=0; i<inputArr.length; i++){
+            inputArr[i] = inputArr[i].replaceAll("[^0-9]","");
+        }
+        return inputArr;
+    }
 }
