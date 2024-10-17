042d4252fd0a07d6285462fa2c7e5eeb23dd3130
sese2204 <sese2204@naver.com>
2024-10-17 20:51:59 +0900

    refactor(StringParser): 메소드 분리로 가독성 개선

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,5 @@
 package calculator;
 
-import calculator.parser.ParseResult;
-import calculator.parser.StringParser;
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
--- src/main/java/calculator/parser/StringParser.java
+++ src/main/java/calculator/parser/StringParser.java
@@ -1,6 +1,5 @@
 package calculator.parser;
 
-import java.util.regex.Pattern;
 
 public class StringParser {
     private static final String DEFAULT_DELIMITERS = ",|:";
@@ -12,17 +11,33 @@ public class StringParser {
             return new ParseResult(null, null);
         }
 
-        String delimiters = DEFAULT_DELIMITERS;
-        String numberString = input;
-
         if (input.startsWith(CUSTOM_DELEMITER_PREFIX)) {
-            int delimiterEnd = input.indexOf(CUSTOM_DELEMITER_SUFFIX);
-            String customDelimiter = input.substring(CUSTOM_DELEMITER_PREFIX.length(), delimiterEnd);
-            delimiters += "|" + customDelimiter;
-            numberString = input.substring(CUSTOM_DELEMITER_SUFFIX.length() + delimiterEnd);
+            return parseWithCustomDelimiter(input);
+        }else{
+            return parseWithDefaultDelimiter(input);
         }
+    }
 
+    private static ParseResult parseWithDefaultDelimiter(String input) {
+        String[] numbers = input.split(DEFAULT_DELIMITERS);
+        return new ParseResult(numbers, DEFAULT_DELIMITERS);
+    }
+
+    private static ParseResult parseWithCustomDelimiter(String input) {
+        int delimiterEnd = input.indexOf(CUSTOM_DELEMITER_SUFFIX);
+        String customDelimiter = findCustomDelimiter(input, delimiterEnd);
+        String delimiters = DEFAULT_DELIMITERS + "|" + customDelimiter;
+        String numberString = findNumberString(input, delimiterEnd);
         String[] numbers = numberString.split(delimiters);
         return new ParseResult(numbers, delimiters);
     }
+
+    private static String findCustomDelimiter(String input, int delimiterEnd){
+        return input.substring(CUSTOM_DELEMITER_PREFIX.length(), delimiterEnd);
+    }
+
+    private static String findNumberString(String input, int delimiterEnd) {
+        return input.substring(CUSTOM_DELEMITER_SUFFIX.length() + delimiterEnd);
+    }
+
 }
