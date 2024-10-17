a765f85cdbb88409a1831cfb893f671767aff0fe
J0YERIM <chosarah70@gmail.com>
2024-10-17 14:40:17 +0900

    feat(Application): Application 클래스에 ApplicationContext를 통해 의존성 주입
    Application 클래스에서 ApplicationContext를 사용하여 StringAdditionController를 생성하고 의존성을 주입받도록 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,8 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ApplicationContext context = new ApplicationContext();
+        StringAdditionController stringAdditionController = context.getStringAdditionController();
+        stringAdditionController.run();
     }
 }
