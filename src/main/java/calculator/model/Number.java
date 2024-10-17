a3d6f0b767ce218ddd1469117786265c284e5e54
Zrfv <youngoh07015@gmail.com>
2024-10-18 01:45:18 +0900

    feat(model): add 추출한 숫자를 int 형으로 변환하는 기능 in Number class
    추출한 숫자를 int 형으로 변환하는 기능 추가

--- src/main/java/calculator/model/Number.java
+++ src/main/java/calculator/model/Number.java
@@ -16,4 +16,23 @@ public class Number {
         }
         return false;
     }
+
+    private void stringToInt() {
+        if (isNullString()) {
+            return;
+        }
+
+        numbers = new int[numString.length];
+        for (int i = 0; i < numString.length; ++i) {
+            try {
+                numbers[i] = Integer.parseInt(numString[i]);
+
+                if (numbers[i] < 1) {
+                    throw new IllegalArgumentException("양수만 입력 가능합니다.");
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("구분자 이외에 숫자만 입력가능합니다.");
+            }
+        }
+    }
 }
