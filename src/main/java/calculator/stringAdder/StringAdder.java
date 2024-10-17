82d884f334794156972a67dd01ca04018b300b4f
jiffyin7 <jiffyin7@gmail.com>
2024-10-17 19:09:41 +0900

    feat(StringAdder): Calculator 실행 로직 구현
    - Calculator 클래스의 sum 메서드 호출
    - 반환된 결과를 출력
    
    이슈 : #10

--- src/main/java/calculator/stringAdder/StringAdder.java
+++ src/main/java/calculator/stringAdder/StringAdder.java
@@ -1,9 +1,10 @@
 package calculator.stringAdder;
 
+
 import calculator.stringAdder.inputvalidator.InputValidator;
 import calculator.stringAdder.delimiterParser.DelimiterParser;
 import calculator.stringAdder.numberParser.NumberParser;
-
+import calculator.stringAdder.calculator.Calculator;
 
 /**
  * @author : jiffyin7@gmail.com
@@ -15,10 +16,13 @@ public class StringAdder {
       InputValidator inputValidator = new InputValidator();
       DelimiterParser delimiterParser = new DelimiterParser();
       NumberParser numberParser = new NumberParser();
+      Calculator calculator = new Calculator();
       while (true){
         String input = inputValidator.validateInput();
         String[] delimeters = delimiterParser.getDelimiters(input);
         String[] numbers = numberParser.getNumbers(input, delimeters);
+        String result = calculator.sum(numbers);
+        System.out.println("결과 : "+ result);
       }
     } catch (IllegalArgumentException e){
       System.err.println(e.getMessage());
