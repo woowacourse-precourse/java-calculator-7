d49d782864c17f569230a0cc354bbf961c496de3
jijongkwon <help3451@naver.com>
2024-10-17 22:57:27 +0900

    refactor(service) : 역할에 맞는 책임 이동
    - InputService에서 입력받도록 변경

--- src/main/java/calculator/service/InputService.java
+++ src/main/java/calculator/service/InputService.java
@@ -1,22 +1,15 @@
 package calculator.service;
 
 import calculator.enums.ErrorMessages;
-import calculator.view.InputView;
+import camp.nextstep.edu.missionutils.Console;
 
 import static calculator.util.Validator.validateCustomDelimiter;
 
 public class InputService {
-
-    private final InputView inputView;
-
-    public InputService(InputView inputView) {
-        this.inputView = inputView;
-    }
-
-    public String getValidInput() {
+    public String readInput() {
         //TODO: 사용자 입력 받기 및 검증 로직 구현
         try {
-            String input = inputView.readInput();
+            String input = Console.readLine();
             validateCustomDelimiter(input);
             return input;
         } catch (Exception e) {
