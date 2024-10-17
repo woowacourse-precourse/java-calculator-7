27d0c1ccd789a38a67fbc8c353381e80e1adca38
Dev-LHY <dev.leehy@gmail.com>
2024-10-18 01:04:59 +0900

    feat: 커스텀 구분자 확인 및 구분자 반환 기능 구현
    INPUT, TOKEN, REGEX는 final 키워드를 사용하여 불변성을 보장.
    정규식을 이용하여 커스텀 구분자를 확인하고 커스텀 구분자가 있을 경우 기본 구분자와 함께 해당 구분자를 반환. 커스텀 구분자가 없을 경우에는 기본 구분자만 반환.

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,25 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator {
+    private final String INPUT;
+    private final String TOKEN;
+    private final String REGEX = "//(.)\\\\n|//(.)(?=\\n)";
+
+    public Calculator(String input) {
+        INPUT = input;
+        TOKEN = checkCustomToken(INPUT);
+    }
+
+    private String checkCustomToken(String input) {
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            String token = matcher.group(1) != null ? matcher.group(1) : matcher.group(2);
+            return "[,:" + token + "]";
+        }
+        return "[,:]";
+    }
+}
