ac89750b885665c171cdc53cb0b45e7e7e6825cd
eunho <eunho1032@gmail.com>
2024-10-17 23:59:56 +0900

    feat(Application): Calculator 클래스 추가에 따른 표현 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,13 +7,11 @@ public class Application {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("덧셈할 문자열을 입력해 주세요.");
 		String input = scanner.nextLine();
-		int result = 0;
 
-		String customDelimiter = CustomDelimiter.findDelimiter(input);
-		int delmiiterEndIdx = CustomDelimiter.findDelimiterEndIndex(input);
-		String[] nums = StringSplit.stringSplit(input, customDelimiter, delmiiterEndIdx);
-		for (String num : nums)
-		result += Integer.parseInt(num); 
-		System.out.println("결과 : " + result);
+		try {
+			Calculator.calculator(input);
+		} catch (IllegalArgumentException e){
+			System.err.println(e.getMessage());
+		}
 	}
 }
