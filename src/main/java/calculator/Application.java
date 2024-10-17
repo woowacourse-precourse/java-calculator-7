ec2ddae8a60d5e37b1c7bea03d39b489cd6b0d5a
ppzepi <hsunguk1999@gmail.com>
2024-10-18 02:00:38 +0900

    feat: feature readLine split

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,15 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("숫자를 입력하세요:");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine(); //사용자입력
-        try {
-            int result = calculateSum(input);
-            System.out.println("결과: " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+
+        int sum = calculateSum(input);
+        System.out.println("합계"+sum);
     }
+
+    public static strsplit(String input){
+        String[] parts = input.split("[,:]");
+
+        return parts
+    }
+
+
 
 
 }
