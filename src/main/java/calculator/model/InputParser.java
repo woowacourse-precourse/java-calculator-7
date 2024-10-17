bdc73641993c869a71fd9b6f4705424dc0d52069
jae woo Jung <awfjol2008@gmail.com>
2024-10-17 18:00:01 +0900

    feat: 커스텀 구분자를 뺸 문자열을 입력값을 최종 저장하는 기능 구현

--- src/main/java/calculator/model/InputParser.java
+++ src/main/java/calculator/model/InputParser.java
@@ -13,6 +13,16 @@ public class InputParser {
 		return input.substring(delimiterStart, delimiterEnd);
 	}
 
+	public String removeCustomDelimiter(String input) {
+		if (hasCustomDelimiter(input)) {
+			int delimiterEnd = input.indexOf("\n");
+
+			return input.substring(delimiterEnd + 1); // \n 이후의 문자열 반환
+		}
+
+		return input; // 커스텀 구분자가 없을 경우 원본 문자열 반환
+	}
+
 	private boolean hasCustomDelimiter(String input) {
 		return input.startsWith("//");
 	}
