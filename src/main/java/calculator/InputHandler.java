1677115463f649e57b476f20c887d0c1476dba54
minseo <alstj778@gmail.com>
2024-10-18 01:00:11 +0900

    feat: 사용자로부터 문자열을 입력받을 수 있다

---
+++ src/main/java/calculator/InputHandler.java
@@ -0,0 +1,9 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+    public void inputString() {
+        String input = Console.readLine();
+    }
+}
--- src/main/java/calculator/StringSumCalculator.java
+++ src/main/java/calculator/StringSumCalculator.java
@@ -2,8 +2,14 @@ package calculator;
 
 public class StringSumCalculator {
     private static final String START_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static InputHandler inputHandler;
+
+    StringSumCalculator() {
+        inputHandler = new InputHandler();
+    }
 
     public void start() {
         System.out.println(START_MESSAGE);
+        inputHandler.inputString();
     }
 }
