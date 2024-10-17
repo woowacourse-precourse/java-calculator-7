42b09edbe9756679b1e048f63a1092603baae398
bee9827 <ehfrhfo9494@naver.com>
2024-10-17 19:12:39 +0900

    test: Add various exceptions
    추후 본코드 수정필요

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,13 +1,17 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import jdk.jfr.Name;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -17,10 +21,50 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 네자리숫자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;9999");
+            assertThat(output()).contains("결과 : 10000");
+        });
+    }
+
+    @Test
+    void 양수가_아닌_정수() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 공백() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_숫자구분자(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//1\n,1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_문자(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("///1\n,1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
