ac7a4415c625b19ef5f02a91bf9f5b58227a75dc
myungkeun02 <audrms3568@gmail.com>
2024-10-18 00:57:17 +0900

    feat: 음수 입력 예외 처리 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 public class Calculator {
     public int add(int[] numbers) {
-        return Arrays.stream(numbers).sum();
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < 0) {
+            throw new IllegalArgumentException();
+            }
+            sum += numbers[i];
+        }
+        return sum;
     }
 }
