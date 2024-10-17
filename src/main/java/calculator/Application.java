e2240799aaf0f3da0c68970ed3a64be02d20aede
softmoca <acyc0909@gmail.com>
2024-10-17 11:49:46 +0900

    feat: 커스텀 구분자 처리 기능 추가
    - '//' 구분자'\n' 형태로 커스텀 구분자가 주어진 경우 처리
    - '\n'를 처리과정 중 이스케이프 문자를 처리하기위해 replace 사용

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,6 +7,8 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
+
+
         int result = calculateSum(input);
         System.out.println("결과 : " + result);
 
@@ -14,22 +16,22 @@ public class Application {
     }
 
     private static int calculateSum(String input) {
+        input = input.replace("\\n", "\n");
 
-        String[] tokens = input.split("[,:]");
+        String delimiter = "[,:]";
+        if (input.startsWith("//")) {
+            String[] parts = input.split("\n", 2);
+
+            delimiter = "[" + parts[0].substring(2) + "]";
+            input = parts[1];
+        }
+
+        String[] tokens = input.split(delimiter);
+
         int sum = 0;
         for (String token : tokens) {
             sum += Integer.parseInt(token);
         }
         return sum;
     }
-
-
-
-
-
-
-
-
-
-
 }
