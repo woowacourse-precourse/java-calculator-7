4719e9fc349ff7bab724f23991874330bfc7faab
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-18 00:51:22 +0900

    refactor(InputView): 입력 뷰 내부에 입력 문구 상수 정의
    - InputView 내부에 입력 문구를 상수화

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,12 +1,13 @@
 package calculator.view;
 
-import calculator.global.constant.InputMessage;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
     public String inputStringToAdd() {
-        System.out.println(InputMessage.INPUT_MESSAGE.getMessage());
+        System.out.println(INPUT_MESSAGE);
         return Console.readLine();
     }
 }
