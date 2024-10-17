5acf2cdd07791b713a173997ec396d06a203cca2
KangGeonyoung <kky6335@gmail.com>
2024-10-17 23:29:41 +0900

    feat(CalculatorController): 덧셈 계산기 시작 기능 CalculatorController - start: 사용자로부터 문자열을 입력 받아 전처리를 진행하여 덧셈을 실행하는 메서드

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -13,8 +13,37 @@ import static calculator.domain.InputValidator.removeSeparatorInfo;
 import static calculator.domain.InputValidator.separate;
 
 public class CalculatorController {
+
     private static List<String> basicSeparators = new ArrayList<>();
     private static List<String> customSeparators = new ArrayList<>();
+
+    public void start() {
+        String input = InputView.readLine();
+
+        // 기본 구분자 초기화
+        initBasicSeparators();
+
+        // 커스텀 구분자가 존재하는 경우, 커스텀 구분자 추출
+        if (isExistCustomSeparator(input)) {
+            customSeparators = getCustomSeparator(input);
+            input = removeSeparatorInfo(input);  // 커스텀 구분자 정보 제거
+        }
+
+        // 구분자 합치기
+        List<String> separators = getTotalSeparator(basicSeparators, customSeparators);
+
+        // 구분자로 숫자 추출
+        List<Integer> numbers = separate(input, separators);
+
+        // 숫자에 대한 유효성 검사(양수 체크)
+        NumberValidator.isPositiveNumber(numbers);
+
+        // 계산기를 통해 모두 더하기
+        int result = Calculator.add(numbers);
+
+        // 결과 출력
+        OutputView.printResult(result);
+    }
 
     /**
      * 기본 구분자(쉼표, 콜론) 생성
