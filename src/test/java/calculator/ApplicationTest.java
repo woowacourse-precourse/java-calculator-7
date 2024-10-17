a9e1dca9cc13a8eaaba46e58206a3fabf98f2fef
JunHyuk Yu <ericyoo0107@gmail.com>
2024-10-17 13:37:18 +0900

    test: 기본에 주어진 구분자를 커스텀 구분자로 등록한 경우, 커스텀 문자열 없이 값을 추가하려 한 경우, 커스텀 문자열 포맷이 잘못된 경우 validation 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -61,6 +61,33 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 커스텀_문자열이_기존에_있는_구분자랑_같은경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//,\\n1,7"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("이미 추가한 구분자는 custom 으로 지정할 수 없습니다.")
+        );
+    }
+
+    @Test
+    void 커스텀_문자열_없이_임의로_값_추가한_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("10?7"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("잘못된 값이 입력 되었습니다.")
+        );
+    }
+
+    @Test
+    void 커스텀_문자열_생성_포맷이_잘못된_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//,1,7\""))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("커스텀 구분자를 추가 하기 위해선 //구분자\\n 형식 으로 입력 하세요.")
+        );
+    }
+
 
     @Override
     public void runMain() {
