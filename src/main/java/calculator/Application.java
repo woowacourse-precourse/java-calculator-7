cbbc6c68139eae28e40ea2964a435dc768ba3375
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-18 01:27:38 +0900

    feat: Accumulator와 StringSplitter를 사용하여 계산 로직 구현
    Application 클래스에서 Accumulator와 StringSplitter를 사용해 입력 문자열을 분리하고, 합산 처리 로직을 추가함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,21 +2,29 @@ package calculator;
 
 public class Application {
     private final InputHandler inputHandler;
+    private final StringSplitter stringSplitter;
+    private final Accumulator accumulator;
 
-    public Application(InputHandler inputHandler) {
+    public Application(InputHandler inputHandler, StringSplitter stringSplitter, Accumulator accumulator) {
         this.inputHandler = inputHandler;
+        this.stringSplitter = stringSplitter;
+        this.accumulator = accumulator;
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         InputHandler inputHandler = new InputHandler();
+        StringSplitter stringSplitter = new StringSplitter();
+        Accumulator accumulator = new Accumulator();
 
-        Application application = new Application(inputHandler);
+        Application application = new Application(inputHandler, stringSplitter, accumulator);
 
         application.run();
     }
 
     public void run() {
-        inputHandler.getInput();
+        String input = inputHandler.getInput();
+        int result = accumulator.sum(stringSplitter.split(input));
+        System.out.println("결과 : " + result);
     }
 }
