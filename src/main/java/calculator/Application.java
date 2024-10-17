65fd8e5960dd33acf993c4ea9d2e5736068b1fa1
gomin0 <gomin0981113@naver.com>
2024-10-17 22:52:45 +0900

    feat: 문자열 계산기 메인 애플리케이션 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringCalculator calculator = new StringCalculator();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        try {
+            int result = calculator.calculate(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 입력입니다.");
+        }
     }
 }
