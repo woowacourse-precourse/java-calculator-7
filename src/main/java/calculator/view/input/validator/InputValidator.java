eb328ee3d16fdd9ed8d11e81e2a04805ff7f60aa
EHCTH <lees90896@gmail.com>
2024-10-17 17:26:57 +0900

    feat(InputValidator): 입력값 예외검증 구현 * feat: 최댓값 검증 * feat: 입력 형식 검증

--- src/main/java/calculator/view/input/validator/InputValidator.java
+++ src/main/java/calculator/view/input/validator/InputValidator.java
@@ -1,8 +1,23 @@
-package calculator.view.inputView.validator;
+package calculator.view.input.validator;
+
+import calculator.constant.ErrorMessage;
+import calculator.separator.domain.RegexPattern;
 
 public class InputValidator {
     public static void validator(String input) {
-
+        isPrefixUseCustomSeparatorValidator(input);
+        isMaxValueValidator(input);
     }
 
+    private static void isPrefixUseCustomSeparatorValidator(String input) {
+        if (!RegexPattern.isMatch(input)) throw new IllegalArgumentException(ErrorMessage.REGEX_ERROR);
+    }
+    private static void isMaxValueValidator(String input) {
+        try {
+            if (input.matches("^[1-9]\\d*$")) Long.parseLong(input);
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.MAX_VALUE_ERROR);
+        }
+    }
 }
