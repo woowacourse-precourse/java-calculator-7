1087120d7f1139d7f24844654bea60d29ebf9508
EunSeop <doosan1112@naver.com>
2024-10-17 23:18:58 +0900

    feat: 숫자를 제외한 연속된 두 문자 예외처리 기능 구현

---
+++ src/main/java/calculator/validator/ConsecutiveLetterValidator.java
@@ -0,0 +1,28 @@
+package calculator.validator;
+
+public class ConsecutiveLetterValidator {
+    String input;
+    public ConsecutiveLetterValidator(String input) {
+        this.input = input;
+        validateConsecutiveLetters();
+    }
+    private void validateConsecutiveLetters() {
+        if (('0' <= input.charAt(0) || input.charAt(0) <= '9') && checkConsecutiveLetter(input)) {
+            new IllegalArgumentException("같은 문자가 두 번 연속 입력되었습니다.");
+        }
+        if(checkConsecutiveLetter(input.substring(5))){
+            new IllegalArgumentException("같은 문자가 두 번 연속 입력되었습니다.");
+        }
+    }
+    private boolean checkConsecutiveLetter(String input) {
+        char previousChar = input.charAt(0);
+        for (int i = 1; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if (currentChar == previousChar && !Character.isDigit(currentChar)) {
+                return true;
+            }
+            previousChar = currentChar;
+        }
+        return false;
+    }
+}
