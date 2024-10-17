51ccbded80dcaeacb1c2a6a4bda1f2041bd5dd0a
param <spsummerdoc@gmail.com>
2024-10-17 16:08:14 +0900

    refactor(errorMessage): 에러 메시지 명확한 구분을 위한 이름 변경

--- src/main/java/calculator/util/ErrorMessage.java
+++ src/main/java/calculator/util/ErrorMessage.java
@@ -1,9 +1,9 @@
 package calculator.util;
 
 public interface ErrorMessage {
-    String INVALID_POSITIVE_NUMBER = "양수만 입력해주세요";
-    String INVALID_DIGIT = "숫자만 입력해주세요";
-    String INVALID_CUSTOM_DELIMITER = "커스텀 구분자를 입력해주세요.";
-    String INVALID_FORMAT = "형식에 맞춰서 입력해주세요";
-    String DUPLICATE_DELIMITER = "이미 추가한 구분자는 custom으로 지정할 수 없습니다.";
+    String INVALID_POSITIVE_NUMBER = "[ERROR] 양수만 입력해주세요";
+    String INVALID_DIGIT = "[ERROR] 숫자만 입력해주세요";
+    String INVALID_CUSTOM_DELIMITER_FORMAT = "[ERROR] 커스텀 구분자형식에 맞춰서 입력해주세요.";
+    String INVALID_FORMAT = "[ERROR] 형식에 맞춰서 입력해주세요";
+    String DUPLICATE_DELIMITER = "[ERROR] 이미 추가한 구분자는 custom으로 지정할 수 없습니다.";
 }
