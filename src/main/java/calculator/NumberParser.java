feda6888c3b83a97a67fe011d36922659b97636e
lchyeong <lchyeong@gmail.com>
2024-10-17 14:18:19 +0900

    style: jave-wooteco-style 적용
    - 전체 클래스

--- src/main/java/calculator/NumberParser.java
+++ src/main/java/calculator/NumberParser.java
@@ -1,17 +1,19 @@
 package calculator;
 
 public class NumberParser {
-    public String[] replaceNumber(String[] inputArr){
-        for(int i=0; i<inputArr.length; i++){
-            inputArr[i] = inputArr[i].replaceAll("[^0-9]","");
+    public String[] replaceNumber(String[] inputArr) {
+        for (int i = 0; i < inputArr.length; i++) {
+            inputArr[i] = inputArr[i].replaceAll("[^0-9]", "");
         }
         return inputArr;
     }
 
-    public int sumNumber(String[] inputArr){
+    public int sumNumber(String[] inputArr) {
         int sum = 0;
-        for(int i=0; i<inputArr.length; i++){
-            sum += Integer.parseInt(inputArr[i]);
+        for (int i = 0; i < inputArr.length; i++) {
+            if (!inputArr[i].isEmpty()) {
+                sum += Integer.parseInt(inputArr[i]);
+            }
         }
         return sum;
     }
--- src/main/java/calculator/PatternParser.java
+++ src/main/java/calculator/PatternParser.java
@@ -7,52 +7,51 @@ import java.util.regex.Pattern;
 
 public class PatternParser {
 
-    public static final String CUSTOM_PATTERN = "^//(.*?)\n";
+    public static final String CUSTOM_PATTERN = "^//(.*?)\\\\n";
     public static final String RESERVE_PATTERN = "[,:]";
 
-    private Pattern toPattern(String regex){
+    private Pattern toPattern(String regex) {
         return Pattern.compile(regex);
     }
 
-    private Matcher toMatcher(String regex, String input){
+    private Matcher toMatcher(String regex, String input) {
         return toPattern(regex).matcher(input);
     }
 
-    public boolean isCustomPattern(String input){
+    public boolean isCustomPattern(String input) {
         Matcher matcher = toMatcher(CUSTOM_PATTERN, input);
         return matcher.find() && (matcher.start() == 0);
     }
 
-    public boolean isReservePattern(String input){
+    public boolean isReservePattern(String input) {
         Matcher matcher = toMatcher(RESERVE_PATTERN, input);
 
         Set<String> delimiters = new HashSet<>();
-        while(matcher.find()){
+        while (matcher.find()) {
             delimiters.add(matcher.group());
         }
 
         return delimiters.contains(",") || delimiters.contains(":");
     }
 
-    public String parseCustomPattern(String input){
+    public String parseCustomPattern(String input) {
         Matcher matcher = toMatcher(CUSTOM_PATTERN, input);
 
-        if(matcher.find()){
+        if (matcher.find()) {
             return matcher.group(1);
         }
 
         return null;
     }
 
-    public String[] splitPattern(String input){
+    public String[] splitPattern(String input) {
 
-        if(isCustomPattern(input)){
+        if (isCustomPattern(input)) {
             String delimiter = parseCustomPattern(input);
-            input = input.substring(input.indexOf("\n")+1);
             return input.split(delimiter);
         }
 
-        if (isReservePattern(input)){
+        if (isReservePattern(input)) {
             return input.split(RESERVE_PATTERN);
         }
 
