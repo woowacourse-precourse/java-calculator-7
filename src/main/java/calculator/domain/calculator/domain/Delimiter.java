abe3ccba7e73f32b0f462759f82cd15049c5f38f
ChiSaeHwang <cwh730909@gmail.com>
2024-10-17 16:34:40 +0900

    feat(): Delimiter 클래스 생성 및 Validate 추가

--- src/main/java/calculator/domain/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/calculator/domain/Delimiter.java
@@ -1,6 +1,31 @@
 package calculator.domain.calculator.domain;
 
 public class Delimiter {
+    private static final String DEFAULT_PATTERN = "[,:]";
+    private final String pattern;
 
+    private Delimiter(String pattern) {
+        this.pattern = validateDelimiter(pattern);
+    }
 
+    public static Delimiter of(
+            final String pattern
+    ) {
+        return new Delimiter(pattern);
+    }
+
+    private String validateDelimiter(String pattern) {
+        /*
+        TO-DO: 이 부분은 예외를 발생 시키고 시스템 종료 시켜도 상관없어 보이지만 체크
+         */
+        if (pattern.isEmpty()) {
+            return DEFAULT_PATTERN;
+        }
+
+        return pattern;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
 }
