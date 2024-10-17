6bdd7f45d5578110b9d52154d599e8e98b9bb7ca
babyfox225 <babyfox225@naver.com>
2024-10-17 22:29:40 +0900

    refactor: 문자열 입력 Calculator 외부로 이동

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.turnOn();
+        String userInput = getUserInput();
+    }
+
+    private static String getUserInput() {
+        String input = Console.readLine().trim();
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("입력값이 존재하지 않습니다.");
+        }
+        return input;
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,11 +4,4 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
 
-    public void turnOn() {
-        String input = Console.readLine().trim();
-        if (input.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-    }
-
 }
