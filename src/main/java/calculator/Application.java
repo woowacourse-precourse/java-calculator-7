ccd951fc91fa6541e5de182c0e4744199b9e7a3c
badgelatte <saucy5939@gmail.com>
2024-10-18 06:14:51 +0900

    문자열 덧셈 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,27 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try{
+            System.out.println("덧셈할 문자열을 입력해주세요.");
+            String r = Console.readLine();
+            String[] all = r.split("[,;]");
+            Integer result = 0;
+
+            for (String s : all) {
+                if(s.equals("")) {
+                    result += 0;
+                } else {
+                    result += Integer.parseInt(s);
+                }
+            }
+
+            System.out.println("결과 : " + result);
+        } catch (NumberFormatException e) {
+            System.out.println("잘못된 입력을 하셨습니다.");
+            System.out.println(e.getMessage());
+        }
     }
 }
