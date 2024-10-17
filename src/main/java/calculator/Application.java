142e33a063c9382c4cf7e9112df9acc7fa0e4577
YuyoungRhee <bbclara@naver.com>
2024-10-17 16:22:54 +0900

    feat(Application): 문자열 덧셈 계산기 로직 구현
    - 사용자 입력을 받아 덧셈을 수행하는 애플리케이션의 메인 로직 구현
    - 입력 문자열에서 구분자를 제거하고, 커스텀 및 기본 구분자를 찾아 숫자를 추출하는 과정 추가
    - 추출된 숫자들을 더하여 결과를 출력하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = Console.readLine();
+
+        StringPreprocessor preprocessor = new StringPreprocessor();
+        Delimiter delimiter = new Delimiter();
+        NumberExtractor numberExtractor = new NumberExtractor();
+        Calculator calculator = new Calculator();
+
+        String numbersPart = preprocessor.removeDelimiterPart(input);
+        String delimiters = delimiter.findDelimiter(input);
+
+        List<Integer> numbers = numberExtractor.extractNumbers(numbersPart, delimiters);
+        int result = calculator.calculate(numbers);
+
+        System.out.println("결과 : " + result);
     }
 }
