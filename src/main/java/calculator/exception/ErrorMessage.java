d26d3de50821b6a0adcaddd08c7ce72c5a3b4f43
IM-GYURI <kgulr0517@gmail.com>
2024-10-18 03:09:16 +0900

    fix : delete UNDEFINED_ERROR

--- src/main/java/calculator/exception/ErrorMessage.java
+++ src/main/java/calculator/exception/ErrorMessage.java
@@ -4,5 +4,4 @@ public class ErrorMessage {
     public static final String NULL_OR_EMPTY_INPUT = "입력 값은 null 또는 빈 값일 수 없습니다.";
     public static final String NEGATIVE_NUMBER = "음수는 입력할 수 없습니다.";
     public static final String INVALID_INPUT = "유효하지 않은 입력 값입니다.";
-    public static final String UNDEFINED_ERROR = "알 수 없는 오류가 발생했습니다. 다시 시도해 주세요.";
 }
