eecd5f56a7da1025bb6d165f72460b466864dccf
GiSeok Kang <ime.rokz@gmail.com>
2024-10-18 01:41:57 +0900

    fix: 공백 입력 테스트 시 No Line Found 에러 발생 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,12 +1,18 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class Calculator {
 
     public int process() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String userInput = Console.readLine();
+        String userInput;
+        try {
+            userInput = Console.readLine();
+        } catch (NoSuchElementException e) {
+            return 0;
+        }
 
         if (userInput.trim().isBlank()) {
             return 0;
