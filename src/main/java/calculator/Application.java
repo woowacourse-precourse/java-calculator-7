bc115e34115db37003adfecea32f3a974eb6f53e
Minuring <mw9505@gmail.com>
2024-10-17 22:49:03 +0900

    refactor(core): 커스텀 구분자를 외부에서 주입
    생성자에서 커스텀 구분자를 추출하던 기존 방식은 생성 시점에 너무 많은 일을 하고, main()의 입장에선 커스텀 구분자 추출이 이루어지리라 생각하는 것이 부자연스럽다고 판단함.
    계산기 클래스의 생성자에 커스텀 구분자를 주입하도록 변경함.
    계산기는 구분자에 대한 고려 없이 오직 계산에 대한 책임만 가질 수 있음.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,10 +7,33 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        Calculator calculator;
 
-        Calculator calculator = new Calculator(input);
+        if (hasCustomSeparator(input)) {
+            char customSeparator = input.charAt(2);
+            String expression = input.substring(5);
+            calculator = new Calculator(expression, customSeparator);
+        } else {
+            calculator = new Calculator(input);
+        }
+
         int sum = calculator.calculate();
-
         System.out.println("결과 : " + sum);
+    }
+
+    private static boolean hasCustomSeparator(String exp) {
+        return exp.length() >= 5
+                && exp.startsWith("//")
+                && exp.indexOf("\\n") == 3
+                && !isNumeric(exp.charAt(2));
+    }
+
+    private static boolean isNumeric(char c) {
+        try {
+            Double.parseDouble(String.valueOf(c));
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
