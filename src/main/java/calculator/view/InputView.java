809d7c98a6d0c82ceaeb27c6761949dc9df1691d
DongYeop <s01054830455@gmail.com>
2024-10-17 15:10:43 +0900

    feat: 문자열 입력 기능 추가

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,15 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputStrings() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        return Console.readLine();
+    }
+}
+
+
+
