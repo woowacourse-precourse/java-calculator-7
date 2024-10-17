79aedbcc71d98cb4dce8e198126d60d5f5b23a1e
dudxo <father8475@naver.com>
2024-10-18 00:57:01 +0900

    feat : 문자열 덧셈 계산기 프로그램 실행 코드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.calculate.CalculateSystem;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CalculateSystem system = new CalculateSystem();
+        system.process();
     }
 }
