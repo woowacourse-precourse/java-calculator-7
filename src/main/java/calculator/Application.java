9dc9516d6ae786e3b84b375c413daeb399fc39fc
ehgur <ehgur062300@gmail.com>
2024-10-18 04:27:54 +0900

    feat(getSeparator): 입력값의 구분자 추출 메서드 추가
    TODO: 커스텀 구분자 추출 메서드 만들어야함

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -17,6 +17,7 @@ import java.util.NoSuchElementException;
 
 public class Application {
 
+    private static final String BASIC_SEPARATOR = ":";
     private static final String CUSTOM_SEPARATOR_PREFIX = "//";
     private static final String NEWLINE_INDICATOR = "\\n";
 
@@ -26,7 +27,18 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
         String additionFormula = Console.readLine();
+        String separator = app.getSeparator(additionFormula);
 
+
+
+    }
+
+    // 구분자 추출
+    private String getSeparator(String value){
+        if (isCustom(value)) {
+            // 커스텀 구분자 추출 메서드
+        }
+        return BASIC_SEPARATOR;
     }
 
     // 구분자가 기본인지 커스텀인지 판단
