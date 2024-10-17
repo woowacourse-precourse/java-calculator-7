2d13fc6fbf45ca43524d3fd561034b463b7f804e
choiyoung69 <choiyoung609@gmail.com>
2024-10-17 19:37:04 +0900

    feat(OutputView): 연산 결과를 출력하는 기능 생성

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,9 +1,15 @@
 package calculator.view;
 
+import calculator.dto.ResultDTO;
+import static calculator.view.ViewMessages.CALCULATION_RESULT_MESSAGE;
 import static calculator.view.ViewMessages.REQUIRE_OPERAND_STRING_MESSAGE;
 
 public class OutputView {
     public static void printRequireOperandStringMessage(){
         System.out.println(REQUIRE_OPERAND_STRING_MESSAGE);
+    }
+
+    public static void printCalculationResultMessage(ResultDTO resultDto){
+        System.out.println(CALCULATION_RESULT_MESSAGE + resultDto.getCalculationResult());
     }
 }
