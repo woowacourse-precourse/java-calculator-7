78f126cba36e6221993d9d3f564583efcf6bb9d0
MinGwan Kim <fpem3309@gmail.com>
2024-10-17 20:43:08 +0900

    feat: 입력값이 빈 문자열인 경우 결과는 0으로 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,15 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userText = Console.readLine();
+
+        int sum = 0;
+
+        if (!userText.isEmpty() && !userText.isBlank()) {
+            // TODO: 입력값이 빈 문자열이 아닐 때
+        }
+
+        System.out.println("결과 : " + sum);
+
     }
+
 }
