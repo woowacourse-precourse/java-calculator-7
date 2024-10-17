4b79e0e323398f151d49f36b18fa7ea5e695a63e
sim-mer <gyu2781@gmail.com>
2024-10-17 12:54:19 +0900

    feat: 기능 구현 - 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환

--- src/main/java/calculator/InputString.java
+++ src/main/java/calculator/InputString.java
@@ -1,12 +1,19 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class InputString {
     private int sum;
 
     public InputString(String input) {
         if(input == null || input.isEmpty()) {
             sum = 0;
+            return;
         }
+        String[] numbers = input.split("[,:]");
+        sum = Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 
     public int getSum() {
