d0421ee61fb6dc766d9130d5e05d890f694e0ba6
wingyou <winwing.you@gmail.com>
2024-10-17 17:46:26 +0900

    test(Parser): add custom delimiter & invalid strings test

--- src/test/java/calculator/ParserTest.java
+++ src/test/java/calculator/ParserTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,4 +26,33 @@ public class ParserTest {
         assertThat(colonNumbers).contains(3, 4, 5);
         assertThat(compositeNumbers).contains(5, 6, 7);
     }
+
+    @Test
+    void parse_string_with_custom_delimiter() {
+        String custom = "//;\n1;2;3";
+
+        int[] customNumbers = parser.parse(custom);
+
+        assertThat(customNumbers).contains(1, 2, 3);
+    }
+
+    @Test
+    void throw_on_invalid_strings() {
+        String wrongDelimiter = "1;2;3";
+        String floatNumber = "1.2,3.5";
+        String invalidDelimiter = "//;1;2,3";
+
+        assertThatThrownBy(() -> {
+            parser.parse(wrongDelimiter);
+        }).hasCause(new IllegalArgumentException());
+
+        assertThatThrownBy(() -> {
+            parser.parse(floatNumber);
+        }).hasCause(new IllegalArgumentException());
+
+        assertThatThrownBy(() -> {
+            parser.parse(invalidDelimiter);
+        }).hasCause(new IllegalArgumentException());
+    }
+
 }
