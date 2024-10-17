dedb88f2e99317396b9169c40d1f6fa144b8a55f
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-18 05:18:29 +0900

    feat: 예외 처리를 위한 ExceptionType Enum 구현
    - 오류 메시지와 예외 클래스를 캡슐화하기 위한 ExceptionType 생성.
    - 예외 관리를 중앙화하여 유지보수성 향상.

---
+++ src/main/java/calculator/common/exception/ExceptionType.java
@@ -0,0 +1,26 @@
+package calculator.common.exception;
+
+public enum ExceptionType {
+    NEGATIVE_NUMBER("음수는 입력할 수 없습니다.", IllegalArgumentException.class),
+    NOT_DELIMITER("구분자가 아닌 값이 포함되어 있습니다.", IllegalArgumentException.class),
+    CUSTOM_DELIMITER_FORMAT_WRONG("커스텀 구분자의 형식이 올바르지 않습니다.", IllegalArgumentException.class),
+    CUSTOM_DELIMITER_EMPTY("커스텀 구분자가 비어있습니다.", IllegalArgumentException.class),
+    CUSTOM_DELIMITER_CONTAINS_NUMBER("커스텀 구분자에 숫자가 포함되어 있습니다.", IllegalArgumentException.class),
+    CUSTOM_DELIMITER_CONTAINS_ESCAPE("커스텀 구분자에 이스케이프 문자가 포함되어 있습니다.", IllegalArgumentException.class);
+
+    private final String message;
+    private final Class<? extends RuntimeException> exceptionClass;
+
+    ExceptionType(String message, Class<? extends RuntimeException> exceptionClass) {
+        this.message = message;
+        this.exceptionClass = exceptionClass;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Class<? extends RuntimeException> getExceptionClass() {
+        return exceptionClass;
+    }
+}
