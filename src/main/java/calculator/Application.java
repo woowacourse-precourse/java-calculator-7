d10ae6456aa720ac821b384eb2929bb521089fde
githublees <leeho_@naver.com>
2024-10-17 19:37:47 +0900

    test: add test examples for calculator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.calculator.Calculator;
+import calculator.input.Input;
+import calculator.output.Output;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Output.printInputMessage();
+
+        Calculator calculator = new Calculator();
+        int result = calculator.add(Input.getInputString());
+
+        Output.printResult(result);
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,12 +8,101 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void Null_또는_빈문자() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("", null))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 숫자가_하나인_경우() {
+        assertSimpleTest(() -> {
+            run("1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 쉼표_구분자만_사용() {
+        assertSimpleTest(() -> {
+            run("1,2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 콜론_구분자만_사용() {
+        assertSimpleTest(() -> {
+            run("1:2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 쉼표_콜론_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
         });
+    }
+
+    @Test
+    void 단일_커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자에_대괄호_사용() {
+        assertSimpleTest(() -> {
+            run("//[\\n1[2[3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자에_빈문자_사용() {
+        assertSimpleTest(() -> {
+            run("//\\n123");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자에_문자_사용() {
+        assertSimpleTest(() -> {
+            run("//abc\\n1abc2abc3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_잘못된_위치() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1;2//;\\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 정의되지_않은_커스텀_구분자_사용() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1;2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
