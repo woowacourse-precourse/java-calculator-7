b4a8647bdf547a13ccb806e2e12232b55cd283c0
MinseokGo <rhalstjr1999@naver.com>
2024-10-18 01:03:36 +0900

    feat(Application): 커스텀 구분자 등록 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -21,6 +21,13 @@ public class Application {
         // `쉼표(,)`와 `콜론(:)` 를 구분자로 등록한다.
         StringBuilder separatorBuilder = new StringBuilder();
         separatorBuilder.append("[,:");
+
+        // 사용자의 입력값에서 `커스텀 구분자` 를 찾는다.
+        if (input.contains("//") && input.contains("\\n")) {
+            // `커스텀 구분자` 가 있다면 구분자로 등록한다.
+            // 이때, `커스텀 구분자`는 문자열 앞부분의 `//`와 `\n` 사이에 위치하는 `특정 문자` 를 추출해서 등록한다.
+            separatorBuilder.append(input.charAt(2));
+        }
         separatorBuilder.append("]");
     }
 }
