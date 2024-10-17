c6ab8cfbae53f9e8306751802c18a02bf2040c32
dltmddus <tmddus4671@gmail.com>
2024-10-17 20:51:40 +0900

    feat: 전체적인 계산기 로직 구현

--- src/main/java/calculator/CalculatorRun.java
+++ src/main/java/calculator/CalculatorRun.java
@@ -1,2 +1,35 @@
-package calculator;public class CalculatorRun {
+package calculator;
+
+import java.util.List;
+import util.InputUtil;
+import util.MessageUtil;
+
+public class CalculatorRun {
+
+    private final InputUtil inputUtil = new InputUtil();
+    private final MessageUtil messageUtil = new MessageUtil();
+
+
+    public void run() {
+        messageUtil.printStringForSum();
+
+        List<Integer> integerList = getIntegerListInfo();
+        Integer result = computeResult(integerList);
+
+        messageUtil.printResult();
+    }
+
+    private List<Integer> getIntegerListInfo() {
+        String initialString = inputUtil.userInput();
+        List<Integer> integerList = splitInitialString();
+        return integerList;
+    }
+
+    private Integer computeResult(List<Integer> integerList) {
+        Integer result = 0;
+        for (Integer num : integerList) {
+            result += num;
+        }
+        return result;
+    }
 }
