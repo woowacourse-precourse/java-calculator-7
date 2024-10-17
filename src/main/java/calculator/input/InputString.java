e4531020002650177ba4cc549aadedf5d74d1213
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-10-17 15:48:53 +0900

    refactor: 사용자 입력값 받을 때 camp.nextstep.edu.missionutils.Console의 readLine() 사용하도록 수정

--- src/main/java/calculator/input/InputString.java
+++ src/main/java/calculator/input/InputString.java
@@ -1,14 +1,13 @@
 package calculator.input;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputString {
 
     // 사용자 입력 메서드
     public String input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-
-        Scanner sc = new Scanner(System.in);
-        return sc.nextLine(); //전체 줄 입력
+        return Console.readLine(); //전체 줄 입력
     }
+
 }
