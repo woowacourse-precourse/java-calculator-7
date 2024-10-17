e27f9f4ad563b93c49fc72cade1f3bb019c5aa26
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-17 14:53:13 +0900

    feat: Application 실행 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,15 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 사용자로부터 문자열 입력 받기
+        InputView inputView = new InputView();
+
+        // 입력 받은 문자열 내부 숫자들의 합 구하기
+        Calculator calculator = new Calculator(inputView.getUserInput());
+
+        // 계산 결과 출력
+        OutputView outputView = new OutputView();
+        outputView.printSum(calculator.getSum());
     }
 }
