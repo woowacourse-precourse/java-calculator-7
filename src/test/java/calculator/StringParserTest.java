22d6e9c96a2f4eadf42793b13d9b938bb408ebc8
sunwon <jsw5913@naver.com>
2024-10-17 22:44:03 +0900

    test(StringParser): 문자열에서 숫자 추출 기능 테스트

--- src/test/java/calculator/StringParserTest.java
+++ src/test/java/calculator/StringParserTest.java
@@ -3,11 +3,85 @@ package calculator;
 import calculator.model.StringParser;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringParserTest {
 
     private StringParser stringParser = new StringParser();
+
+
+    @Test
+    void 빈_입력시_0을_포함한_리스트_반환() {
+        List<Integer> result = stringParser.extractNumbers("");
+        assertEquals(Collections.singletonList(0), result);
+    }
+
+    @Test
+    void 정수_입력시_올바른_숫자_반환() {
+        List<Integer> result = stringParser.extractNumbers("1,2:3");
+        assertEquals(Arrays.asList(1, 2, 3), result);
+    }
+
+    @Test
+    void 커스텀_구분자_입력시_올바른_숫자_반환() {
+        List<Integer> result = stringParser.extractNumbers("//;\n1;2;3");
+        assertEquals(Arrays.asList(1, 2, 3), result);
+    }
+
+    @Test
+    void 혼합_구분자_입력시_올바른_숫자_반환() {
+        List<Integer> result = stringParser.extractNumbers("//;\n1,2:3;4");
+        assertEquals(Arrays.asList(1, 2, 3, 4), result);
+    }
+
+    @Test
+    void 유효하지_않은_숫자_입력시_예외_발생() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            stringParser.extractNumbers("1,2,a");
+        });
+    }
+
+    @Test
+    void 음수_입력시_올바른_숫자_반환() {
+        List<Integer> result = stringParser.extractNumbers("-1,2,-3");
+        assertEquals(Arrays.asList(-1, 2, -3), result);
+    }
+
+    @Test
+    void 큰_숫자_입력시_올바른_숫자_반환() {
+        List<Integer> result = stringParser.extractNumbers("1000,2000,3000");
+        assertEquals(Arrays.asList(1000, 2000, 3000), result);
+    }
+
+    @Test
+    void 여러_자리_커스텀_구분자_입력시_올바른_숫자_반환() {
+        List<Integer> result = stringParser.extractNumbers("//***\n1***2***3");
+        assertEquals(Arrays.asList(1, 2, 3), result);
+    }
+
+    @Test
+    void 유효하지_않은_여러_자리_커스텀_구분자_입력시_예외_발생() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            stringParser.extractNumbers("//*%*\n1***2***3");
+        });
+    }
+
+    @Test
+    void 중복된_구분자_입력시_올바른_숫자_반환() {
+        List<Integer> result = stringParser.extractNumbers("1,,2,:3");
+        assertEquals(Arrays.asList(1, 2, 3), result);
+    }
+
+    @Test
+    void 유효하지_않은_숫자_문자열_변환시_예외_발생() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            stringParser.extractNumbers("1,2,abc");
+        });
+    }
 
     @Test
     void 공백문자가_아닐시_false를_반환하다() {
