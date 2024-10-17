44aaa4ba2fe5b3325096456624257fdfdb4f7d8d
dd-jiyun <dlwldbs8015@gmail.com>
2024-10-18 00:23:43 +0900

    feat: 구분자 추출 기능

--- src/main/java/calculator/DelimiterExtractor.java
+++ src/main/java/calculator/DelimiterExtractor.java
@@ -1,7 +1,33 @@
 package calculator;
 
-// 구분자 추출
-public class DelimiterExtractor {
-    public void
-}
+import java.util.ArrayList;
+import java.util.List;
 
+public class DelimiterExtractor {
+
+    List<String> delimiters = new ArrayList<>();
+
+    public List<String> extract(String input) {
+        if (input.startsWith("//")) {
+            String customDelimiter = extractCustomDelimiter(input);
+            delimiters.add(customDelimiter);
+        }
+
+        extractBasicDelimiter(input);
+
+        return delimiters;
+    }
+
+    private String extractCustomDelimiter(String input) {
+        return input.substring(2, input.indexOf("\\n"));
+    }
+
+    private void extractBasicDelimiter(String input) {
+        Delimiter.getBasicDelimiters().forEach(delimiter -> {
+            if (input.contains(delimiter)) {
+                delimiters.add(delimiter);
+            }
+        });
+    }
+
+}
