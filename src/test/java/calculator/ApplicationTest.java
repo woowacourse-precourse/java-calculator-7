25f61de55963e9ffb2939539cd56f3f1977bcfc5
dhrgodms <dhrgodms123@gmail.com>
2024-10-17 13:51:19 +0900

    test: Add test_code to extract multi_delimiters

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -3,6 +3,8 @@ package calculator;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,6 +16,24 @@ class ApplicationTest extends NsTest {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
         });
+    }
+
+    @Test
+    void 다수의_커스텀_구분자_추출() {
+        // 비교군
+        Application.StringCalculator testCalculator = new Application.StringCalculator();
+        testCalculator.delimiterArr.add(";");
+        testCalculator.delimiterArr.add("m45i");
+
+        // given
+        String testDelimiterText = "//;\\n1//m45i\\n";
+
+        // when
+        Application.StringCalculator stringCalculator = new Application.StringCalculator();
+        ArrayList<String> result = stringCalculator.extractDelimiter(testDelimiterText);
+
+        // then
+            assertThat(result).isEqualTo(testCalculator.delimiterArr);
     }
 
     @Test
