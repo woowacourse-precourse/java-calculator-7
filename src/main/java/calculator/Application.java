e090a2aa75ba7716eaa93bc215ed2a0654c233d7
박근채 <pgc0419@gmail.com>
2024-10-17 17:01:40 +0900

    사용자 문자열 입력받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         System.out.print("덧셈할 문자열을 입력해주세요." + "\n");
+        String Input = Console.readLine();
     }
 }
 
