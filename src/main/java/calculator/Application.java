7786df701560ca624c0aec8ef71fc47026d5ec47
RAJUYEOB <pag1yun99@gmail.com>
2024-10-17 15:09:45 +0900

    feat: 커스텀 구분자 처리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,32 +2,52 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
-        List<Integer> intNumber = new ArrayList<>();
-
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         String cuttingMark = ",|:";
 
-        String[] numbers = input.split(cuttingMark);
+        int sum = 0;
 
         if (input.trim().isEmpty()) {
             System.out.println("결과 : 0");
+            return;
+        } else if (input.startsWith("//") && input.contains("\\n")) {
+            // 커스텀 구분자가 반드시 한 글자인 경우
+            String customCuttingMark = input.substring(2, 3);
+            String safeCustomCuttingMark = Pattern.quote(customCuttingMark);
+            String combinedCuttingMark = cuttingMark + "|" + safeCustomCuttingMark;
+            String cuttedNumbers = input.substring(5);
+            String[] numbers = cuttedNumbers.split(combinedCuttingMark);
+            sum = calculateSum(numbers);
+
+        } else if (input.startsWith("//")) {
+            // 커스텀 구분자가 여러 글자이고 개행문자 \n으로 커스텀 구분자를 입력 받을때.
+            int endIndex = input.indexOf("\n");
+            String customCuttingMark = input.substring(2, endIndex);
+            String safeCustomCuttingMark = Pattern.quote(customCuttingMark);
+            String combinedCuttingMark = cuttingMark + "|" + safeCustomCuttingMark;
+            String cuttedNumbers = input.substring(endIndex + 1);
+            String[] numbers = cuttedNumbers.split(combinedCuttingMark);
+            sum = calculateSum(numbers);
+
         } else {
-            for (String number : numbers) {
-                intNumber.add(Integer.parseInt(number));
-            }
-
-            int sum = 0;
-            for (int i = 0; i < intNumber.size(); i++) {
-                sum += intNumber.get(i);
-            }
-
-            System.out.println("결과 : " + sum);
+            // 기본 구분자 사용.
+            String[] numbers = input.split(cuttingMark);
+            sum = calculateSum(numbers);
         }
+
+        System.out.println("결과 : " + sum);
+    }
+
+    private static int calculateSum(String[] numbers) {
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
     }
 }
