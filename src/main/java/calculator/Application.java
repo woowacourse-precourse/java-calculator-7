949d04e33faa83ab1202990962badbc7e014caf7
Youngchan <wdc4512@naver.com>
2024-10-18 02:23:24 +0900

    feat: 입력 받기 기능
    - Console API를 통한 문자열 입력받기
    - 입력받은 값을 저장하고 출력하는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 사용자에게 입력 요청
+        System.out.print("문자열 입력: ");
+
+        // 콘솔에서 입력받은 값을 input에 저장
+        String input = Console.readLine();
+
+        // 입력받은 값을 출력
+        System.out.println("입력한 문자열: " + input);
     }
 }
