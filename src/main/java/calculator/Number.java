05a51ff82036f85dcd0606ce3949f3e568318458
sc@t0a14 <gusdn6671@gmail.com>
2024-10-18 01:06:01 +0900

    fix(Number) : 숫자 외의 문자는 입력할 수 없는 error fix

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -4,25 +4,24 @@ public class Number {
     private final String number;
 
     public Number(String number) {
+        if (number.equals("\"\"")) {
+            number = "0";
+        }
         validate(number);
         this.number = number;
     }
 
     private void validate(String number) {
-        if (number.equals("\"\"")) {
-            number = "0";
+        if (number.isBlank()) {
+            throw new IllegalArgumentException("숫자에 빈 값을 입력할 수 없습니다");
         }
 
         if (!number.matches("\\d+")) {
-            throw new IllegalArgumentException("숫자 외의 문자는 입력할수 없습니디");
-        }
-
-        if (number.isBlank()) {
-            throw new IllegalArgumentException("숫자에 빈값을 입력할수 없습니다");
+            throw new IllegalArgumentException("숫자 외의 문자는 입력할 수 없습니다");
         }
 
         if (Integer.parseInt(number) < 0) {
-            throw new IllegalArgumentException("음수를 입력할수 없습니다");
+            throw new IllegalArgumentException("음수를 입력할 수 없습니다");
         }
     }
 
