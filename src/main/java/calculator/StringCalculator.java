3a08f253f5617637ea68c49833ba2801a0f4a758
gomin0 <gomin0981113@naver.com>
2024-10-17 21:31:38 +0900

    feat: 구분된 숫자의 합을 계산하는 기능

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,4 +4,16 @@ public class StringCalculator {
     public String[] splitInput(String input) {
         return input.split("[,:]");
     }
+
+    public int calculate(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+        String[] numbers = splitInput(input);
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
+    }
 }
