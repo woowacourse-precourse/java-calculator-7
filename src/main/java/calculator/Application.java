b6d1e8592e03e64d504734eafafe15e123fb4363
SungJun <kmymy00@naver.com>
2024-10-17 17:18:38 +0900

    feat(Application): 값을 입력 받고 더하는 기능 추가
    Add:
    -Console을 이용해 값을 입력 받는 기능 추가
    -입력값들을 더하고 반환하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,29 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        Console.readLine();
+        String input = Console.readLine();
+
+        StringSplitter splitter = new StringSplitter();
+        List<String> splitStringList = splitter.split(input);
+
+        StringIntegerConverter converter = new StringIntegerConverter();
+
+        List<Integer> numberList;
+        try {
+            numberList = converter.convert(splitStringList);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+
+        int sum = numberList.stream().mapToInt(Integer::intValue).sum();
+
+        System.out.println("결과 : " + sum);
+        Console.close();
     }
 }
