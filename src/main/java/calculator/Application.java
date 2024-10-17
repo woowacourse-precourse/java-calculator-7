0a6cfe62037ea8e23cca292e2dfa91972da20451
sejinezy <psj8880@naver.com>
2024-10-17 17:27:52 +0900

    feat(Application): implement main method to initialize and execute string addition
    Added the main method in the Application class to initialize ConsoleIO,
    InputValidator, and StringAdder. It captures user input, processes the addition,
    and prints the result.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,13 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        ConsoleIO consoleIO = new ConsoleIO();
+        InputValidator validator = new InputValidator();
+        StringAdder stringAdder = new StringAdder(consoleIO, validator);
+
+        String input = consoleIO.getInput();
+        int result = stringAdder.add(input);
+        consoleIO.printResult(result);
     }
 }
