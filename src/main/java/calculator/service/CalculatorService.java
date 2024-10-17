79e81d92e07793cae86f153d8cb56a0881f28d0f
chae02yo <chae02kim@naver.com>
2024-10-17 14:46:42 +0900

    feat: add separation of numbers
    - separate numbers by comma and colon separator

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public class CalculatorService {
     String number = "";
-    int result;
+    int result = 0;
+    String printNumList = "";
 
     ArrayList<String> numList = new ArrayList<>();
 
@@ -12,6 +13,27 @@ public class CalculatorService {
         if (input.isEmpty()) { // 빈 입력값 처리
             return "0";
         }
-       return input;
+        else { // 쉼표와 콜론 구분자를 기준으로 숫자 분리
+            for (int i=0; i<input.length(); i++) {
+                char c = input.charAt(i);
+                switch (c) {
+                    case ',':
+                    case ';':
+                        numList.add(number);
+                        number = "";
+                        continue;
+                    default:
+                        number += c;
+                        break;
+                }
+            }
+            numList.add(number);
+
+            for (int i=0; i<numList.size(); i++) {
+                printNumList += numList.get(i) + " ";
+            }
+
+            return printNumList;
+        }
     }
 }
