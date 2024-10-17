b1cba873112117b74b055ecceaa56c3f81feb522
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-18 00:11:19 +0900

    refactor: 정수 유틸리티를 사용하도록 변경
    - 정수 연산의 책임을 IntegerUtils 에 위임.
    - 더 이상 사용하지 않는 Calculator 제거.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,7 @@ package calculator;
 
 import calculator.delimiter.Delimiter;
 import calculator.delimiter.Delimiters;
+import calculator.util.IntegerUtils;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -15,7 +16,6 @@ public class Application {
         delimiterList.add(new Delimiter(","));
         delimiterList.add(new Delimiter(":"));
 
-        Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = scanner.nextLine();
@@ -40,8 +40,12 @@ public class Application {
         Delimiters delimiters = new Delimiters(delimiterList);
 
         List<String> numberTokens = delimiters.split(input);
-        int sum = calculator.sum(numberTokens);
-
-        System.out.print("결과 : " + sum);
+        try {
+            List<Integer> numbers = IntegerUtils.parsePositiveIntegers(numberTokens);
+            int sum = IntegerUtils.sum(numbers);
+            System.out.print("결과 : " + sum);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("구분자가 아닌 문자가 포함되어 있습니다.");
+        }
     }
 }
