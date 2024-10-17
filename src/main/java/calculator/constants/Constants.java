29e5ede6b881a3bedb53eb00e5f3c9fbad5592c3
pakaheo <k70923@konkuk.ac.kr>
2024-10-17 21:51:21 +0900

    refactor : remove ZERO constant

--- src/main/java/calculator/constants/Constants.java
+++ src/main/java/calculator/constants/Constants.java
@@ -7,7 +7,6 @@ public final class Constants {
 
     public static final String COMMA = ",";
     public static final String COLON = ":";
-    public static final int ZERO = 0;
     public static final String WRONG_DELIMITER = "잘못된 구분자입니다";
     public static final String NOT_EXIST_CUSTOM = "커스텀 구분자가 없습니다";
     public static final String UNDEFINED_CUSTOM = "문자열에 정의되지 않은 커스텀 구분자가 있습니다";
