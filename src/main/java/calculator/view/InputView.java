bb07229d4c74599a9d6ae7c0002fe21049cf2a1e
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-10-17 16:36:59 +0900

    feat(view): develop InputView and OutputView classes
    - InputView 클래스에서 받아온 데이터 파싱 및 검증 로직 구현.
    - OutputView 클래스의 출력 기능 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,6 +1,7 @@
 package view;
 
 
+import utils.Custom;
 import utils.InputViewValidator;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -10,31 +11,50 @@ import java.util.ArrayList;
 public class InputView {
 
     private final InputViewValidator inputViewValidator = new InputViewValidator();
+    private final Custom custom;
 
     private String inputName;
+    private ArrayList<String> resultParsingInput;
 
-    public String readInputString() {
+    public InputView(Custom custom) {
+        this.custom = custom;
+    }
+
+
+    public void readInputString() {
         inputName = Console.readLine();
+        inputViewValidator.validateQuotationPair(inputName);
+        inputName = inputName.substring(1, inputName.length() - 1);
+        resultInput();
+    }
+
+    public Boolean isCustom() {
+        System.out.println("inputname: " + inputName);
+        /*조건문 분리 및 리팩토링 필요*/
+        if(inputName.length()>4) {
+            String prefix = inputName.substring(0, 2);
+            String suffix = inputName.substring(3, 5);
+            String customData = inputName.substring(2, 3);
+            if (prefix.equals("//") && suffix.equals("\\n")) {
+                inputViewValidator.validateCustomNumeric(customData);
+                custom.addCustomSet(customData); /*컨트롤러의 역할로 변경*/
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
+
+    private void resultInput() {
+        if (isCustom()) {
+            inputName = inputName.substring(5);
+        }
+    }
+
+    public String getInputString() {
         return inputName;
     }
-
-    public void validatorAllInput() {
-
-        inputName = inputViewValidator.validateQutoPair(inputName);
-
-        inputViewValidator.validateCustom(inputName);
-
-        inputViewValidator.validateDuplicationCustom(inputName);
-
-        // first custom messsage pasing validate
-    }
-    public void validatorCustomInput() {
-        inputViewValidator.validateCustom(inputName);
-    }
-
-    public ArrayList<String> resultParsingInput() {
-
-        return null;
-    }
-
 }
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,4 +1,18 @@
 package view;
 
 public class OutputView {
+    private static final String PRINT_READ_NUMBER_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String PRINT_CALCULATOR_RESULT_MESSAGE = "\n결과: ";
+
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printReadNumberMessage() {
+        printMessage(PRINT_READ_NUMBER_MESSAGE);
+    }
+
+    public void printCalculatorResultMessage(String result) {
+        printMessage(PRINT_CALCULATOR_RESULT_MESSAGE + result);
+    }
 }
