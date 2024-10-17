8db1d207cae96c8356c2c894fb130412091b8c02
Seungwan Yoo <ysw99@kakao.com>
2024-10-17 13:17:54 +0900

    feat: 기본 구분자 기준 문자열 분리 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -2,11 +2,20 @@ package calculator.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class InputView {
     private final static String INPUT_STRING_TO_ADD = "덧셈할 문자열을 입력해주세요.";
+    private final static String DEFAULT_REGEX = "[,:]";
 
-    public String getInputString() {
+    public List<String> getInputString() {
         System.out.println(INPUT_STRING_TO_ADD);
-        return Console.readLine();
+        String inputString = Console.readLine();
+        List<String> numbersList = splitByDefaultRegex(inputString);
+        return numbersList;
+    }
+
+    public List<String> splitByDefaultRegex(String inputString) {
+        return List.of(inputString.split(DEFAULT_REGEX));
     }
 }
