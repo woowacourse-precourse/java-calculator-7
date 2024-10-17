182f9b4d4a7c9e5621567b905da8a44d28351dd9
JooIcksoo <dlrtn751@gmail.com>
2024-10-17 14:41:48 +0900

    fix(ApplicationTest): 테스트 구문 수정
    - 불필요 테스트 구문 제거
    - 기존 테스트 구문 복구

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,19 +1,27 @@
 package calculator;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class ApplicationTest extends NsTest {
+    @Test
+    void 커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
 
     @Test
-    void 입력_요청_메시지_출력_테스트() {
-        assertSimpleTest(() -> {
-            run("");
-            assertThat(output()).contains("덧셈할 문자열을 입력해 주세요.");
-        });
+    void 예외_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("-1,2,3"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Override
