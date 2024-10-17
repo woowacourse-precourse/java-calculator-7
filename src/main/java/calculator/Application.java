215acb47d37402da8e0755af431ea27c4a7e9ac5
kang yeon joo <rkdduswndi@naver.com>
2024-10-17 21:21:52 +0900

    feat(parser): 커스텀 구분자 추출 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,11 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해주세요");
         String input_str = Console.readLine();
+
+        if(input_str.startsWith("//")){
+            String[] split_str = input_str.split("\n");
+            String customDelimiter = split_str[0].substring(2);
+            input_str = split_str[1];
+        }
     }
 }
