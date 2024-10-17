8715ba2d7d22681b31992c7f14f9fe678320e04b
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-10-17 23:53:50 +0900

    Feat: 커스텀 구분자 뽑아내는 함수 구현.
    1. 커스텀 구분자를 뽑아냄
    2. 커스텀 구분자를 기준으로 정수 분리
    TODO: 커스텀 구분자, 일반 구분자 동시 사용할 경우 처리 필요

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,17 +2,69 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // - 구분자를 제외한 숫자들의 ArrayList
+        List<Integer> numArrayList = new ArrayList<>();
+        // - 구분자들의 ArrayList
+        List<String> divider = new ArrayList<>();
+        // - 입력받는 문자열
+        String inputString = "";
+        // - 커스텀 구분자
+        String customDivider = "";
 
+
+        // divier에 기본 구분자 추가
+        divider.add(":"); divider.add(",");
+
+        // - 입력
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String inputString = Console.readLine();
+        inputString = Console.readLine();
 
+        // - custom 구분자를 찾아 추가
+        customDivider = findDivider(inputString);
+        if (customDivider != null) {
+            // \n 다음의 숫자 부분 추출
+            String tmpNum = inputString.substring(inputString.indexOf("\\n") + 2);
+
+            divider.add(customDivider);
+            // 구분자로 문자열 분리
+            // customDivider를 기준으로 문자열을 분리
+            // TODO: 현재 커스텀 구분자로만 문자열을 구분함. 커스텀, 기존 구분자 둘다 사용하여 구분할 수 있도록 수정 필요
+            String[] numbers = tmpNum.split(customDivider);
+
+            // 각 숫자를 정수로 변환하여 리스트에 추가
+            for (String number : numbers) {
+                numArrayList.add(Integer.parseInt(number.trim()));
+            }
+
+        }
+
+        // - 출력
         System.out.println("결과 : " + inputString);
+        System.out.println(divider);
+        System.out.println(numArrayList);
 
-        // 사용이 끝나면 반드시 close() 메서드를 호출하여 자원 해제
+        // Scanner 자원 해제
         Console.close();
+    }
 
+
+    // - 정수, 구분자를 구분하는 함수
+    public static String findDivider(String input) {
+        if (input.startsWith("//")) {
+            int lastLineIndex = input.indexOf("\\n");
+            System.out.println("lastLineIndex>>" + lastLineIndex);
+            // 커스텀 구분자가 char가 아닐수 있음(2자 이상)
+            if (lastLineIndex != -1 && lastLineIndex > 2) {
+
+                return input.substring(2, lastLineIndex);
+            }
+        }
+        return null;
     }
 }
