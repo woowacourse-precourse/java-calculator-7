2b330fc6717953fcfd91e4aadbabe4649d713c8d
joonwan <joonwan1024@naver.com>
2024-10-18 02:17:38 +0900

    test: Calculator 테스트 코드 작성

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest extends NsTest {
+
+    @Test
+    void 정상_입력_실행() {
+        assertSimpleTest(() -> {
+            run("//;\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Calculator.run();
+    }
+}
