6bf8fbabbfdf393a16475c0229a1a62469d3a9b3
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-10-17 18:18:48 +0900

    fix(core): fix extractCustomSepAndInputString function
    fix extractCustomSepAndInputString function
    - fix regex pattern for extract input string group
    - add "( )" in regex pattern for matcher.group(2)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,7 +2,6 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,13 +9,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = Console.readLine();
-
-        if (input.isEmpty()){
-            System.out.println("0");
-        }
-
-        //validateCustomSep(input);
-        //calculate();
+        // 함수 통합, 프로그램 구현
+        
 
     }
     // 올바른 입력형태 (ex. 11:22,33)
@@ -31,7 +25,6 @@ public class Application {
         for (String s : arr){
             sum += Integer.parseInt(s);
         }
-        System.out.println(sum);
         return sum;
     }
 
@@ -43,11 +36,23 @@ public class Application {
         return matcher.find();
     }
 
+    private static String[] extractCustomSepAndInputString(String input){
+        // 정규식으로 커스텀 구분자 사용을 탐지
+        Pattern pattern = Pattern.compile("//(.)\\\\n(.*)");
+        Matcher matcher = pattern.matcher(input);
+        String[] ret = new String[2];
+        if (matcher.find()){
+            ret[0] = matcher.group(1);
+            ret[1] = matcher.group(2);
+            return ret;
+        }
+        return null;
+    }
+
     private static boolean containsInvalidCharacters(String input, String... customSep) {
-        String regex = "0-9:.";
+        String regex = "0-9:,";
 
         if (customSep.length > 0) {
-
             String sep = customSep[0];
             if ("+*?[](){}|.^$".contains(sep)) {
                 sep = "\\\\" + sep;  // 자바에서 \\는 정규식에서 \로 인식된다. 정규식에서 \\를 만들어야되니까 자바에서 \\\\를 추가하는 것
@@ -55,10 +60,10 @@ public class Application {
             regex += sep;
         }
 
-        regex = ".*[^" + regex + "].*";
+        regex = "^[0-9" + regex + "]*$";
         return input.matches(regex);
     }
-    
+
     private static boolean startAndEndWithNumber(String input){
         return input.matches("^[0-9].*[0-9]$");
     }
