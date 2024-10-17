c3c612b4ef6051656e48449fab2c10d1e021009d
ruudska6 <feels19@naver.com>
2024-10-17 20:03:09 +0900

    <feat>: StringParsing 구분자를 통해 문자열을 파싱을 해주는 StringParsing 클래스의 Delimeter() 메서드

--- src/main/java/calculator/service/StringParsing.java
+++ src/main/java/calculator/service/StringParsing.java
@@ -1,7 +1,21 @@
 package calculator.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class StringParsing {
-    public static String[] userInputStrToArr(String strInput) {
-        return strInput.split("[,:]");
+
+    public static String[] Delimiter(String strInput) {
+        String delimiter = ",|:";
+        strInput = strInput.replace("\\n", "\n");
+        Matcher m = Pattern.compile("//(.*?)\\n(.*)").matcher(strInput);
+        if (m.find()) {
+            delimiter = delimiter + "|" + Pattern.quote(m.group(1));
+            strInput = m.group(2);
+        }
+        return strInput.split(delimiter);
     }
 }
+
+
