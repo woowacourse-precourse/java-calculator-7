6a2b743d099e726b708b32f1c66d789161d2c58a
IM-GYURI <kgulr0517@gmail.com>
2024-10-18 01:52:36 +0900

    refactor : move print input prompt to OutputView for better separation of responsibilities

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,7 +2,12 @@ package calculator.view;
 
 public class OutputView {
 
+    private static final String INPUT_PROMPT = "덧셈할 문자열을 입력해 주세요.";
     private static final String OUTPUT_PROMPT = "결과 : ";
+
+    public static void printInput() {
+        System.out.println(INPUT_PROMPT);
+    }
 
     public static void printResult(int result) {
         System.out.println(OUTPUT_PROMPT + result);
