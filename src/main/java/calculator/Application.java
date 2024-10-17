ab082710117c6067fbd9331f2ecf33186cad675b
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-17 23:51:46 +0900

    refactor : 코드 간략화

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,11 @@
 package calculator;
 
-import calculator.service.CalculatorService;
-import calculator.service.Service;
-import calculator.view.View;
+import calculator.controller.CalculatorController;
 
 public class Application {
 
     public static void main(String[] args) {
-        Service service = new CalculatorService(View.input());
-        service.run();
+        CalculatorController calculator = new CalculatorController();
+        calculator.run();
     }
 }
