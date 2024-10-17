4046eb1be20a222a563b68aa838c539f3f4cf8fc
박근채 <pgc0419@gmail.com>
2024-10-17 17:52:04 +0900

    feat(Application): String으로 문자열 입력받는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,23 +7,10 @@ public class Application {
         System.out.print("덧셈할 문자열을 입력해주세요." + "\n");
         String Input = Console.readLine();
 
-        try {
-            int sum = 0;
+        System.out.println("결과 : " + Input);
 
-            String[] tokens = Input.split("[,:]");
-
-            for (String token : tokens) {
-                if (token.isEmpty()) {
-                    token = "0";
-                }
-                sum += Integer.parseInt(token);
-            }
-            System.out.println("결과 : " + sum);
-        }
-        catch (IllegalArgumentException e) {
-            System.exit(1);
         }
 
     }
-}
+
 
