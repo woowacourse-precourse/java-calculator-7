cc20b7c46b449b44c820d2db6785dbf81beef959
cyunlee <siyunkr@gmail.com>
2024-10-17 17:28:51 +0900

    feat(exception): 문자열 입력 값 에러 처리 메서드 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,35 +1,45 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
 
 public class Application {
+
+    private static void checkIsValid(String str) {
+        for (char x : str.toCharArray()) {
+            if (!Character.isDigit(x)) {
+                throw new IllegalArgumentException("문자열에 음수 혹은 숫자가 아닌 것은 포함될 수 없습니다.");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         int sum = 0;
 
-        if(input.contains("//") && input.contains("\\n")) {
-            String custom = input.substring(input.indexOf("//")+2, input.indexOf("\\n"));
-            String newInput = input.substring(input.indexOf("\\n")+2);
+        if (input.contains("//") && input.contains("\\n")) {
+            String custom = input.substring(input.indexOf("//") + 2, input.indexOf("\\n"));
+            String newInput = input.substring(input.indexOf("\\n") + 2);
             String replacedInput = newInput.replace(custom, ",");
 
             String[] strArrCustom = replacedInput.split("[,:]");
 
-            for(String str : strArrCustom) {
-                if(!str.isEmpty()) {
+            for (String str : strArrCustom) {
+                if (!str.isEmpty()) {
+                    checkIsValid(str);
                     sum += Integer.parseInt(str);
                 }
             }
-        }else if(input.contains(",") || input.contains(":")) {
+        } else if (input.contains(",") || input.contains(":")) {
             String[] strArrBasic = input.split("[,:]");
 
-            for(String str : strArrBasic) {
-                if(!str.isEmpty()) {
+            for (String str : strArrBasic) {
+                if (!str.isEmpty()) {
+                    checkIsValid(str);
                     sum += Integer.parseInt(str);
                 }
             }
-        }else if(input.isEmpty()) {
+        } else if (input.isEmpty()) {
             sum = 0;
         }
 
