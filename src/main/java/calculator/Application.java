d369d9621eeadd58d727ed6c1af6c7619ebc5428
JeongSu Park <bechalrang@gmail.com>
2024-10-17 20:52:24 +0900

    feat(Application.java): 메인 실행 로직 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import calculator.object.CalculateFormula;
+import calculator.view.ConsoleView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConsoleView view = new ConsoleView();
+
+        String inputCalculationFormula = view.askCalculationFormula();
+
+        String result = CalculateFormula
+                .from(inputCalculationFormula)
+                .addAll();
+
+        view.showResult(result);
     }
+
 }
