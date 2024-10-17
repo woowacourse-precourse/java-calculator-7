dfd8e197ea0c674cf5814e70ab1f4e5d305fc773
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-17 15:18:48 +0900

    test(StringCalculatorTest): StringCalculator 가 예외를 발생시키는 테스트 기능 추가
    문자열이 음수를 포함할 경우와 문자를 포함할 경우, 연속되는 구분자의 경우, 아예 잘못된 형식이 리스트에 담길 경우 예외를 발생시키는 테스트 코드를 구현한다.

---
+++ src/test/java/calculator/domain/StringCalculatorTest.java
@@ -0,0 +1,72 @@
+package calculator.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+class StringCalculatorTest {
+
+    @Nested
+    class 예외_처리_테스트를_진행한다 {
+
+        @Test
+        void 음수_포함시_예외를_발생시킨다() {
+            //given
+            List<String> numbers = List.of("1", "-2", "3");
+            String expected = "음수는 입력 불가합니다.";
+
+            //when
+            IllegalArgumentException actual = assertThrows(IllegalArgumentException.class,
+                    () -> new StringCalculator(numbers));
+
+            //then
+            assertEquals(expected, actual.getMessage());
+        }
+
+        @Test
+        void 연속되는_기본_구분자의_경우_예외를_발생시킨다() {
+            //given
+            List<String> numbers = List.of("1", "", "2", "3"); //1,,2,3 입력시 StringParser 에서 다음과 같은 리스트를 반환한다.
+            String expected = "올바른 숫자 형식이 아닙니다.";
+
+            //when
+            IllegalArgumentException actual = assertThrows(IllegalArgumentException.class,
+                    () -> new StringCalculator(numbers));
+
+            //then
+            assertEquals(expected, actual.getMessage());
+        }
+
+        @Test
+        void 문자가_포함되는_경우_예외를_발생시킨다() {
+            //given
+            List<String> numbers = List.of("1", "a", "3");
+            String expected = "올바른 숫자 형식이 아닙니다.";
+
+            //when
+            IllegalArgumentException actual = assertThrows(IllegalArgumentException.class,
+                    () -> new StringCalculator(numbers));
+
+            //then
+            assertEquals(expected, actual.getMessage());
+        }
+
+        @Test
+        void 잘못된_형식이_리스트에_담길_경우_예외를_발생시킨다() {
+            //given
+            List<String> numbers = List.of("1?2?3"); //1?2?3? 입력시 StringParser 에서 다음과 같은 리스트를 반환한다.
+            String expected = "올바른 숫자 형식이 아닙니다.";
+
+            //when
+            IllegalArgumentException actual = assertThrows(IllegalArgumentException.class,
+                    () -> new StringCalculator(numbers));
+
+            //then
+            assertEquals(expected, actual.getMessage());
+        }
+    }
+}
