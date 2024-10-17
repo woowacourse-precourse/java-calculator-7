bb3fe5275fe57d518703eea3356a972b0f32c311
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-10-17 12:17:01 +0900

    feat: 사용자에게 문자열 입력을 요청하는 메시지를 출력하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private void printUserInputMessage() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 }
