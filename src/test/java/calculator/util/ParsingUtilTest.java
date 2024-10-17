103e7b4e782593ae753acf3c0a9405e3cc8e24f6
sanghee0820 <lsh277604@gmail.com>
2024-10-17 12:24:16 +0900

    test: default 구분자로 입력된 값 테스트 추가

--- src/test/java/calculator/util/ParsingUtilTest.java
+++ src/test/java/calculator/util/ParsingUtilTest.java
@@ -1,8 +1,10 @@
 package calculator.util;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import calculator.exception.CalculatorError;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -42,5 +44,19 @@ class ParsingUtilTest {
         assertThatThrownBy(() -> ParsingUtil.parseWithDefault(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(CalculatorError.INPUT_IS_NULL_OR_EMPTY.getMessage());
+    }
+
+
+    @Test
+    @DisplayName("Default 구분자로 입력 값이 기본 구분자로로 구분된 문자열을 List로 반환")
+    void parseWithDefault_CommaDelimiter() {
+        // given
+        final String input = "1,2:3";
+        final var expected = List.of("1", "2", "3");
+        // when
+        var result = ParsingUtil.parseWithDefault(input);
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }
