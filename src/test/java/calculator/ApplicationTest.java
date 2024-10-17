48e0be174a4f9abbfc53e8da8e104a4dacbdde7c
Ferrorist <ferrorist@naver.com>
2024-10-17 23:19:19 +0900

    test(custom delimiter exception): 커스텀 구분자 예외 테스트 코드 추가
    커스텀 구분자 관련 예외 테스트 코드를 추가했습니다.
    올바른 형식의 문자열이 아닌 경우 예외를 반환해야 합니다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -2,6 +2,9 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,6 +17,15 @@ class ApplicationTest extends NsTest {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
         });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"\\n1", "\\n//1", "//"})
+    void 커스텀_구분자_예외_테스트(String inputValue) {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException(inputValue))
+                    .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
