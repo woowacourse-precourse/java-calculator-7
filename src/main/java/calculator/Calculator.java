185a401a8ffb55cd5d795a43ee2797102ed507f3
633jinn <judyg00dy68@gmail.com>
2024-10-17 14:57:05 +0900

    feat: 구분자들을 기준으로 숫자의 합 출력

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,48 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Calculator {
+    static String input = "";
+    static List<String> separators = new ArrayList<>(Arrays.asList(",", ":"));
+
+    Calculator() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        input = Console.readLine();
+    }
+
+
+    public void calculate() {
+        findCustomSeparator();
+        System.out.println("결과 : " + sum());
+    }
+
+    /**
+     * findCustomSeparator(): 커스텀 구분자를 찾아 seperators 리스트에 추가한다.
+     */
+    private void findCustomSeparator() {
+        if (input.startsWith("//")) {
+            int separatorEndIndex = input.indexOf("\\n");
+            String customSeparator = input.substring(2, separatorEndIndex);
+
+            separators.add(customSeparator);
+            input = input.substring(separatorEndIndex + 2);
+        }
+    }
+
+    /**
+     * sum(): 구분자를 이용해 문자열 분리 후 숫자들 합계 리턴
+     *
+     * @return : 합계
+     */
+    private Integer sum() {
+        String[] numbers = input.split(separators.toString());
+
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+}
