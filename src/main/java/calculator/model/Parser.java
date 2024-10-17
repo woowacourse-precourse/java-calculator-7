f45cc7c6492d135d440178bedc3e0d5dfabff88e
dradnats1012 <herjebi1012@gmail.com>
2024-10-17 21:09:04 +0900

    feat: 기본 구분자 분리 메서드 생성

--- src/main/java/calculator/model/Parser.java
+++ src/main/java/calculator/model/Parser.java
@@ -1,4 +1,12 @@
 package calculator.model;
 
+import java.util.List;
+
 public class Parser {
+
+    private List<String> parseBasicSeparator(String input){
+        List<String> splitString = List.of(input.split(",|:"));
+
+        return splitString;
+    }
 }
