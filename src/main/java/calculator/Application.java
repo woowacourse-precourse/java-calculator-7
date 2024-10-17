ec83862761d6396e91b45e812f196df0a8b7e78c
동영 김 <kimarang0@naver.com>
2024-10-17 15:00:09 +0900

    feat(Application): 사용자에게 입력받기 및 결과 출력하기 기능 구현 및 기타 기반 코드 구현 - main 메서드에 입출력 코드 작성 - 커스텀 구분자를 찾아 적용하는 findCustomDivider 메서드 생성 - 합계를 계산하는 calculateSum 메서드 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해주세요.\n");
+        String input = Console.readLine();
+
+        String parsedInput = findCustomDivider(input);
+        int sum = calculateSum(parsedInput);
+
+        System.out.println("결과 : " + sum);
+
+        Console.close();
+    }
+
+    private static String findCustomDivider(String input) {
+        // TODO
+        return input;
+    }
+
+    private static int calculateSum(String parsedInput) {
+        // TODO
+        return 0;
     }
 }
