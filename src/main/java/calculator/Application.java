206a55f92f0ed4d3c63fb2a92e891ec4e856b948
minjun <ijmtylhtf@naver.com>
2024-10-17 20:46:33 +0900

    feat : 입력값 문자열에서 쉽표, 콜론 중 포함되어 있는 구분자 목록 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,24 @@
 package calculator;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private static final String[] originalSplitters = {",", ":"};
+    private static Set<String> inputSplitters = new HashSet<>(); 
+
     public static void main(String[] args) {
         String input = Console.readLine();
+        makeInputSplitters(input);
+    }
+
+    private static void makeInputSplitters(String input){
+        for (String originalSplitter : originalSplitters) {
+            if(input.contains(originalSplitter)){
+                inputSplitters.add(originalSplitter);
+            }
+        }
     }
 }
