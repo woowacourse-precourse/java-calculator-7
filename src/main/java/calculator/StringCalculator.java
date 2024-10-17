8a4752b8dbf85b06182ed5ccad8804f41a069ec1
gomin0 <gomin0981113@naver.com>
2024-10-17 21:36:11 +0900

    feat: 커스텀 구분자를 지정하는 기능

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,6 +2,12 @@ package calculator;
 
 public class StringCalculator {
     public String[] splitInput(String input) {
+        if (input.startsWith("//")) {
+            int customIdx = input.indexOf("\n");
+            String custom = input.substring(2, customIdx);
+            String numberString = input.substring(customIdx + 1);
+            return numberString.split(custom);
+        }
         return input.split("[,:]");
     }
 
