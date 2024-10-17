591d2694dd77634a1454a8bb9e839c53d27fb0b0
SongJaeHoonn <thdwogns121@naver.com>
2024-10-17 17:32:13 +0900

    feat: 커스텀 구분자 분리 메서드 구현

--- src/main/java/parser/Delimiter.java
+++ src/main/java/parser/Delimiter.java
@@ -1,4 +1,11 @@
 package parser;
 
 public class Delimiter {
+
+    public String extractDelimiter(String input) {
+        if (input.startsWith("//") && input.charAt(3) == '\\' && input.charAt(4) == 'n') {
+            return String.valueOf(input.charAt(2));
+        }
+        return null;
+    }
 }
