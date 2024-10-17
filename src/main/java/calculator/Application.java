be9ac15c2ab63ff115443abeb27dc42291d6f6f2
onesay17 <onesay17@gmail.com>
2024-10-17 11:42:53 +0900

    refactor: AppConfig를 통한 의존성 주입 구현
    Application 클래스에서 객체 생성을 제거하고, AppConfig를 통해 의존성을 주입하는 방식으로 변경했습니다. 이를 통해 객체 간 의존성을 낮추고, 의존성 관리를 중앙화하여 유지보수성을 향상시켰습니다.
    
    Related to #6, #7

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,21 +1,11 @@
 package calculator;
 
 import calculator.controller.CalculatorController;
-import calculator.model.Calculator;
-import calculator.model.DelimiterParser;
-import calculator.model.InputValidator;
-import calculator.view.CalculatorView;
 
 public class Application {
     public static void main(String[] args) {
-        //TODO 의존관계 주입 더 나은 방법있는지 리팩터링 확인
-        InputValidator inputValidator = new InputValidator();
-        DelimiterParser delimiterParser = new DelimiterParser(inputValidator);
-        Calculator calculator = new Calculator(inputValidator, delimiterParser);
-        CalculatorView calculatorView = new CalculatorView();
-
-        CalculatorController calculatorController = new CalculatorController(calculator, calculatorView);
+        AppConfig appConfig = new AppConfig();
+        CalculatorController calculatorController = appConfig.calculatorController();
         calculatorController.run();
-
     }
 }
