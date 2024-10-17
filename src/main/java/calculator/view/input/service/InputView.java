362928f784d22b85035eaf770f6f6d1fbe1d21b9
EHCTH <lees90896@gmail.com>
2024-10-17 17:24:12 +0900

    feat(InputView): InputService 인터페이스를 구현 받는 클래스 작성 * feat: input 기능 추가

--- src/main/java/calculator/view/input/service/InputView.java
+++ src/main/java/calculator/view/input/service/InputView.java
@@ -1,12 +1,21 @@
-package calculator.view.inputView.service;
+package calculator.view.input.service;
 
+import calculator.view.input.validator.InputValidator;
+import calculator.view.output.InfoService;
 import camp.nextstep.edu.missionutils.Console;
 
-public class InputView implements InputService{
 
+public class InputView implements InputService{
+    private final InfoService infoService;
+    public InputView(InfoService infoService) {
+        this.infoService = infoService;
+    }
     @Override
     public String input() {
+        infoService.info();
         String input = Console.readLine();
+        InputValidator.validator(input);
         return input;
     }
 }
+
