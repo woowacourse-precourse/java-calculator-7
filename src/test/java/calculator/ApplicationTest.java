80ef92321c41512559b7d3be137cd7edff421e2c
Devheun <dudwo5600@korea.ac.kr>
2024-10-17 17:56:01 +0900

    test: add missing test cases

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,11 +17,47 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 음수_예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 빈_문자열() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 구분자_혼합_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2:3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 구분자_패턴_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("/;\\n1;2"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 음수_구분자_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//-\\n1-2-3"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 숫자_자리_문자_예외() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1,*,3"))
+                .isInstanceOf(IllegalArgumentException.class));
     }
 
     @Override
