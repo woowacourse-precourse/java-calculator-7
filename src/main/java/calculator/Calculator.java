9f782690a44a061e6a9dee411fa7b6458b6ab73c
garden14 <ci667@dgu.ac.kr>
2024-10-18 00:06:54 +0900

    feat: 숫자 추출 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,14 +4,23 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
     private static final String ENTER_MESSAGE = "덧셈할 문자열을 입력해주세요.";
-    private static final String COLON_SEPARATOR = ":";
-    private static final String COMMA_SEPARATOR = ",";
-    private static final String SLASH_SEPARATOR = "//";
-    private static final String LINE_BREAKING_SEPARATOR = "\n";
 
     public static void start() {
         System.out.println(ENTER_MESSAGE);
         String userInput = Console.readLine();
+
+        if (userInput == null || userInput.isEmpty()) {
+
+        }
+        String[] tokens = Tokenizer.tokenization(userInput);
+
     }
 
+    public static int add(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
 }
---
+++ src/main/java/calculator/Tokenizer.java
@@ -0,0 +1,42 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Tokenizer {
+
+    public static String[] tokenization(String message) {
+        String Separator = ",|:";
+        String numbers = message;
+
+        if (message.startsWith("//")) {
+            Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(message);
+
+            if (!matcher.matches()) {
+                throw new IllegalArgumentException("잘못된 입력 형식입니다.");
+            }
+
+            Separator = Pattern.quote(matcher.group(1));
+            numbers = matcher.group(2);
+        }
+        return numbers.split(Separator);
+    }
+
+    public static int[] convertToNumber(String[] tokens) {
+        int[] numbers = new int[tokens.length];
+
+        for (int i = 0; i < tokens.length; i++) {
+            try {
+                numbers[i] = Integer.parseInt(tokens[i].trim());
+                if (numbers[i] < 0) {
+                    throw new IllegalArgumentException("잘못된 입력 형식입니다.");
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("잘못된 입력 형식입니다.");
+            }
+        }
+        return numbers;
+    }
+
+
+}
