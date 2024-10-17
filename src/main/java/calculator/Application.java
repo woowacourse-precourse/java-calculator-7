d41b5fca970e2f3f5156282cd000566f96877906
bk <naturalbb360@gmail.com>
2024-10-17 12:47:04 +0900

    feat(Application): 커스텀 구분자 입력 위치 검증 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,11 +10,13 @@ public class Application {
 
     public static void getCustomSeperator(String input) {
         String customSeperator;
-        System.out.println("input:" + input);
         if (input.contains("\\n") && input.contains("//")) {
             customSeperator = input.substring(input.indexOf("//") + 2, input.indexOf("\\n"));
             if (customSeperator.length() > 1) {
                 throw new IllegalArgumentException("한개의 커스텀 구분자만 추가할 수 있습니다.");
+            }
+            if (input.indexOf("//") != 0) {
+                throw new IllegalArgumentException("커스텀 구분자는 문자열 처음에서만 추가할 수 있습니다.");
             }
             seperatorList.add(customSeperator);
         }
@@ -29,7 +31,6 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해주세요");
         getCustomSeperator(getUserInput());
         System.out.println(seperatorList);
