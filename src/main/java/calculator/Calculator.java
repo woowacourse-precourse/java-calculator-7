9d1272472415f63481246d25c5ba09762360a576
chyo1 <hyowon3856@gmail.com>
2024-10-17 21:16:20 +0900

    FEAT : 커스텀 구분자 확인 및 지정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,12 +3,12 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
-	private String str;
+	private String inputStr;
 	private long totalSum;
 	private char[] separator = new char[3];
 
 	public Calculator() {
-		str = "";
+		inputStr = "";
 		totalSum = 0;
 		separator[0] = ',';
 		separator[1] = ':';
@@ -16,12 +16,13 @@ public class Calculator {
 
 	public void run() {
 		enter();
+		checkPersonalSeperator(inputStr);
 		print();
 	}
 
 	private void enter() {
 		System.out.println("덧셈할 문자열을 입력해 주세요.");
-		str = Console.readLine();
+		inputStr = Console.readLine();
 		Console.close();
 	}
 
@@ -31,5 +32,23 @@ public class Calculator {
 
 	private void calculateNums(long num) {
 		totalSum += num;
+	}
+
+	private void checkPersonalSeperator(String str) {
+		int startPos = str.indexOf("//");
+		int endPos = str.indexOf("\n");
+
+		// 커스텀 구분자가 처음에 위치하지 않거나, 커스텀 구분자로 1개 이상의 문자 입력 시
+		if (startPos != 0 || endPos != 3) {
+			throw new IllegalArgumentException();
+		}
+		// 커스텀 구분자로 숫자가 들어왔을 때
+		char newSeparator = str.charAt(2);
+		if ('0' <= newSeparator && newSeparator <= '9') {
+			throw new IllegalArgumentException();
+		}
+
+		// 커스텀 구분자 저장
+		separator[2] = newSeparator;
 	}
 }
