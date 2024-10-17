ac18756c08f5615c2ddbb7dd158c86d134ff6872
junhyuk1248 <poa124@nate.com>
2024-10-17 17:34:56 +0900

    구현 기능 목록 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,14 +2,13 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        String default_Delimiters = ",|;";
 
-        System.out.println("input = " + input);
+        
     }
 }
