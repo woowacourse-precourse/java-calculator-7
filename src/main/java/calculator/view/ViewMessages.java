3c5c4cf4e6a41a82c06eb0202fec85d8d5c8f549
choiyoung69 <choiyoung609@gmail.com>
2024-10-17 19:19:44 +0900

    feat(ViewMessages): 출력 메세지 정의

---
+++ src/main/java/calculator/view/ViewMessages.java
@@ -0,0 +1,6 @@
+package calculator.view;
+
+public class ViewMessages {
+    public static final String REQUIRE_OPERAND_STRING = "덧셈할 문자열을 입력해 주세요.";
+    public static final String PRINT_CALCULATION_RESULT = "결과 : ";
+}
