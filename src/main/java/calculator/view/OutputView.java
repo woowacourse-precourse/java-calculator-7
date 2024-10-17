c1b503a8f40b15e1463e6e64e797b1d1974e1150
choiyoung69 <choiyoung609@gmail.com>
2024-10-17 19:33:16 +0900

    feat(OutputView): 피연산자가 포함된 문자열을 요구하는 메세지 출력

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,1 +1,9 @@
+package calculator.view;
 
+import static calculator.view.ViewMessages.REQUIRE_OPERAND_STRING_MESSAGE;
+
+public class OutputView {
+    public static void printRequireOperandStringMessage(){
+        System.out.println(REQUIRE_OPERAND_STRING_MESSAGE);
+    }
+}
