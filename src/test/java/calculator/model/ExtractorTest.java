d52cfcd4c75051680b4cfbf86526f09fd77a08e3
woo <ldw28517@naver.com>
2024-10-17 23:05:14 +0900

    test: Extractor 테스트
    - 커스텀 구분자가 있는 경우, 없는 경우에 대해 숫자(후보)가 제대로 추출되는지 테스트
    - 커스텀 구분자 선언이 시작됐지만 끝나지 않은 경우 예외 발생되는지 테스트
    - 커스텀 구분자를 공백으로 선언한 경우 예외 발생되는지 테스트

---
+++ src/test/java/calculator/model/ExtractorTest.java
@@ -0,0 +1,81 @@
+package calculator.model;
+
+import calculator.domain.Candidate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ExtractorTest {
+    Storage storage;
+    Extractor extractor;
+
+    @BeforeEach
+    void init() {
+        storage = new Storage();
+        extractor = new Extractor();
+    }
+
+    @Test
+    void 커스텀구분자_X_숫자추출() {
+        //given
+        String input = "1,2:3";
+        List<String> expectedList = Arrays.asList("1", "2", "3");
+        List<String> resultList = new ArrayList<>();
+
+        //when
+        extractor.execute(input, storage);
+        for (Candidate c : storage.getNumberCandidates()) {
+            resultList.add(c.getCandidate());
+        }
+
+        //then
+        assertThat(resultList).containsAll(expectedList);
+    }
+
+    @Test
+    void 커스텀구분자_O_숫자추출() {
+        //given
+        String input = "//-\\n1,2:3-4";
+        List<String> expectedList = Arrays.asList("1", "2", "3", "4");
+        List<String> resultList = new ArrayList<>();
+
+        //when
+        extractor.execute(input, storage);
+        for (Candidate c : storage.getNumberCandidates()) {
+            resultList.add(c.getCandidate());
+        }
+
+        //then
+        assertThat(resultList).containsAll(expectedList);
+    }
+
+    @Test
+    void 커스텀구분자_O_구분자선언이끝나지않음() {
+        //given
+        String input = "//-1,2:3-4";
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> extractor.execute(input, storage))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 커스텀구분자_O_구분자가존재하지않음() {
+        //given
+        String input = "//\\n1,2:3-4";
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> extractor.execute(input, storage))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
+
