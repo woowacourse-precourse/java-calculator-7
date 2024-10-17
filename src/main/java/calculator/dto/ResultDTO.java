f91fedda36395a69e7b86014002887d7ad9050b1
choiyoung69 <choiyoung609@gmail.com>
2024-10-17 19:24:43 +0900

    feat(ResultDTO): 연산 결과를 저장하는 DTO 생성

---
+++ src/main/java/calculator/dto/ResultDTO.java
@@ -0,0 +1,13 @@
+package calculator.dto;
+
+public class ResultDTO {
+    private int calculationResult;
+
+    public int getCalculationResult() {
+        return calculationResult;
+    }
+
+    public void setCalculationResult(int calculationResult) {
+        this.calculationResult = calculationResult;
+    }
+}
