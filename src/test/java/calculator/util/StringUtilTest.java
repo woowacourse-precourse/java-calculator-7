eb62fcf19a64d405e84271a57f55a37505ec0d26
pakaheo <k70923@konkuk.ac.kr>
2024-10-17 22:48:13 +0900

    test : test of split string

--- src/test/java/calculator/util/StringUtilTest.java
+++ src/test/java/calculator/util/StringUtilTest.java
@@ -1,2 +1,13 @@
-package calculator.util;public class StringUtilTest {
+package calculator.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class StringUtilTest {
+
+    @Test
+    void split() {
+        assertThat(StringUtil.splitInput("1,2,3", ",")).containsExactly("1", "2", "3");
+    }
 }
