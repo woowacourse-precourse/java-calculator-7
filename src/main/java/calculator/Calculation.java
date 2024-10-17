ecfd51c95ff0dced5e96327fddf88b1a4bde3604
taehyun <xogus5070318@gmail.com>
2024-10-18 01:50:21 +0900

    feat: 연산 클래스 생성

---
+++ src/main/java/calculator/Calculation.java
@@ -0,0 +1,14 @@
+package calculator;
+
+//연산 클래스 생성
+public class Calculation {
+
+    //덧셈의 결과를 출력
+    public int Add(String str){
+
+
+        //Todo: 덧셈의 결과값을 반환하도록 기능 구현
+        return 1;
+    }
+
+}
