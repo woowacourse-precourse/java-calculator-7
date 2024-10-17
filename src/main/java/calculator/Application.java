54e949272df44fe406f6a52f51aca360fea980fc
ehgur <ehgur062300@gmail.com>
2024-10-18 04:21:13 +0900

    feat(Application): add string input functionality

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * packageName    : calculator
+ * fileName       : Application
+ * author         : ehgur062300
+ * date           : 2024-10-18
+ * description    : 가독성 및 유지보수성을 고려해서 구현
+ * ===========================================================
+ * DATE              AUTHOR                 NOTE
+ * -----------------------------------------------------------
+ * 2024-10-18      ehgur062300           version 1.0
+ */
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String additionFormula = Console.readLine();
+
     }
 }
