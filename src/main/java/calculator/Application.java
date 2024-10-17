d61f8bf2d2a4c9ee9f0f8cd17f92f96d74eb9d84
aaahyunseo <hs030608@naver.com>
2024-10-17 23:37:11 +0900

    fix(Application): 문자열 계산기 실행을 위한 main 함수 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,9 @@
 package calculator;
 
+import controller.CalculatorController;
+
 public class Application {
     public static void main(String[] args) {
-        Calculator.run();
+        CalculatorController.run();
     }
 }
