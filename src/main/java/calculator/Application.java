1602324aa27296e0020b889cbc5f7112707d7608
hayeongKo <polarpheno@icloud.com>
2024-10-18 02:18:46 +0900

    feat: implement for custom delimiter

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,9 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
 
     public static int StringCalculator(String input) {
@@ -10,6 +13,14 @@ public class Application {
         }
 
         String delimiter = ",;";
+
+        if (input.startsWith("//")) {
+            Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+            if (matcher.matches()) {
+                delimiter = Pattern.quote(matcher.group(1));
+                input = matcher.group(2);
+            }
+        }
 
         String[] tokens = input.split(delimiter);
         int sum = 0;
