ccc980268a9cbffb4af8d457c1f6f69251d1b936
MinJun Choi <mj043000@naver.com>
2024-10-18 00:02:45 +0900

    test: 예외 테스트 경우 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,6 +9,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void 기본_케이스() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1;2;3");
@@ -44,6 +52,22 @@ class ApplicationTest extends NsTest {
     void 미등록_연산자_예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1;2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 공백_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" "))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 알파벳_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("Hi! I'm Minjun"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
