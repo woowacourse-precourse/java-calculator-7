174854297b14dc70916da962de2fd33f190f2b5e
Jaewon94 <wodnjs3580@gmail.com>
2024-10-17 10:18:34 +0900

    fix: Console 라이브러리 사용으로 업데이트
    - 프로젝트 진행 요구사항인 Console 라이브러리를 사용하여 사용자의 입력으로 업데이트

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,14 +1,15 @@
 package calculator;
 
 import calculator.util.MyCalculator;
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Scanner scanner = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요. ");
-        String expression = scanner.nextLine();
+        
+        // 요구사항을 만족하기 위해 제시하는 라이브러리 사용
+        String expression = Console.readLine();
         
         MyCalculator calculator = new MyCalculator();
         
@@ -20,6 +21,9 @@ public class Application {
             // System.exit()을 하지 않고 종료하기위해
             // JVM으로 에러를 던지고, 프로그램이 예외 발생할 경우 종료되게 하기 위해 아래 코드 사용
             throw e;
+        } finally {
+            // try이든 catch이든 열어둔 Scanner를 꺼주기 위한 코드
+            Console.close();
         }
     }
 }
