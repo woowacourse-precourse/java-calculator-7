64962a3129ea41f81154078a761774eafd8c0f5e
joonwan <joonwan1024@naver.com>
2024-10-18 00:34:22 +0900

    fix: 커스텀 포멧 검증 로직 수정

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -2,12 +2,20 @@ package calculator;
 
 public class Validator {
 
-    private static final String target = "\\\\n";
-    private static final String end = "e";
-
     public static void checkValidCustomFormat(String inputData) {
-        String replaceData = inputData.replaceAll(target, end);
-        int count = getEndCount(replaceData);
+        char[] inputDataArray = inputData.toCharArray();
+        int count = 0;
+        int index = 0;
+
+        while (index < inputDataArray.length - 1) {
+            char c1 = inputDataArray[index];
+            char c2 = inputDataArray[index + 1];
+            if (c1 == '\\' && c2 == 'n') {
+                count++;
+            }
+            index++;
+        }
+        
         if (count != 1) {
             throw new IllegalArgumentException();
         }
@@ -38,17 +46,5 @@ public class Validator {
             return true;
         }
         return false;
-    }
-
-    private static int getEndCount(String replaceData) {
-        int count = 0;
-        char[] arr = replaceData.toCharArray();
-        char target = end.charAt(0);
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-                count++;
-            }
-        }
-        return count;
     }
 }
