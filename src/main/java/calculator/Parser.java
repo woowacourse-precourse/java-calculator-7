9b513503d878b05f939ac7c67f3a7e6b54c8f3bb
wingyou <winwing.you@gmail.com>
2024-10-17 20:51:40 +0900

    fix(Parser): custom delimiter & filtering negative number

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -8,21 +8,20 @@ public class Parser {
     public int[] parse(String input) {
         String[] segments;
         int[] numbers;
-        System.out.println(input);
 
         try {
             if (input.startsWith("//")) {
                 char delimiter = input.charAt(2);
-                segments = input.split("\n")[1].split(String.valueOf(delimiter));
+                segments = input.split("\\\\n")[1].split(String.valueOf(delimiter));
             } else {
                 segments = input.split("[,|:]");
             }
 
             numbers = new int[segments.length];
             for (int i = 0; i < segments.length; i++) {
-                System.out.println("    " + segments[i]);
                 numbers[i] = Integer.parseInt(segments[i]);
-                System.out.println("    -> " + numbers[i]);
+                if (numbers[i] < 0)
+                    throw new IllegalArgumentException();
             }
         } catch (Exception e) {
             throw new IllegalArgumentException();
