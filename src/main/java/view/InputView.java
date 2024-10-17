33247f053bc81d90a916206fb32fdc279a670af3
yedamhy <yeadalm04@gmail.com>
2024-10-17 22:52:03 +0900

    ✨ feat(view): 사용자 입력 처리 클래스 추가
    InputView 클래스에서 문자열 입력을 처리하도록 구현
    - Console.readLine()을 사용해 사용자로부터 입력 받음
    - 사용자 입력을 요청하기 위한 메세지 출력

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,10 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String getStringFromUser(){
+        System.out.println("문자열을 입력하세요 : ");
+        return Console.readLine();
+    }
+}
