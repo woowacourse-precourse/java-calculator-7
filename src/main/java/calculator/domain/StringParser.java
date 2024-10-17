1e0f7c28123b4df59f4f01718b30b477cd732c06
Kim Kyeongho <sgn07124@gmail.com>
2024-10-17 22:30:03 +0900

    feat(StringParser): 반환된 문자열 리스트를 정수 리스트로 변환하는 기능 추가
    - 반환된 문자열 리스트에서 정수형 리스트로 변환하는 기능 추가
    - 해당 기능에 대한 테스트 코드 추가

--- src/main/java/calculator/domain/StringParser.java
+++ src/main/java/calculator/domain/StringParser.java
@@ -1,5 +1,9 @@
 package calculator.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringParser {
 
     private static final String CUSTOM_FIRST_DELIMITER = "//";
@@ -9,6 +13,7 @@ public class StringParser {
 
     public static int calculateInput(String input) {
         String[] numbers = splitCustomInput(input);
+        List<Integer> intNumbers = convertIntList(numbers);
         int result = 0;
         return result;
     }
@@ -25,5 +30,9 @@ public class StringParser {
         } else {
             return input.split(DEFAULT_DELIMITERS);  // 기본 구분자를 사용하는 경우
         }
+    }
+
+    public static List<Integer> convertIntList(String[] numbers) {
+        return Arrays.stream(numbers).map(Integer::parseInt).collect(Collectors.toList());
     }
 }
--- src/test/java/calculator/domain/StringParserTest.java
+++ src/test/java/calculator/domain/StringParserTest.java
@@ -1,5 +1,7 @@
 package calculator.domain;
 
+import java.util.Arrays;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,6 +32,19 @@ class StringParserTest {
 
         //then
         Assertions.assertThat(numbers).isEqualTo(new String[]{"1", "2", "4"});
+    }
+
+    @DisplayName("문자열 리스트의 각 문자열을 정수형으로 변환하여 리스트로 반환")
+    @Test
+    public void convertIntListTest() {
+        //given
+        String[] numbers = {"1", "2", "3", "4"};
+
+        //when
+        List<Integer> convertNumbers = StringParser.convertIntList(numbers);
+
+        //then
+        Assertions.assertThat(convertNumbers).isEqualTo(Arrays.asList(1, 2, 3, 4));
     }
 
 }
