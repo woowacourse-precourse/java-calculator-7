68e80336e0db17b46bd692e8764b9ab97670ffad
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-10-17 15:45:02 +0900

    feat: 사용자 문자열 입력

--- src/main/java/calculator/input/InputString.java
+++ src/main/java/calculator/input/InputString.java
@@ -1,8 +1,14 @@
 package calculator.input;
 
+import java.util.Scanner;
+
 public class InputString {
+
+    // 사용자 입력 메서드
     public String input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return "";
+
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine(); //전체 줄 입력
     }
 }
