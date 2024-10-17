101d492d4e0dcfbadeb34152a7694c9e0c8a0511
mjw3723 <mjw3723@naver.com>
2024-10-18 04:28:01 +0900

    feat : 입력값에 구분자가 있는지 구현

--- src/main/java/calculator/util/Util.java
+++ src/main/java/calculator/util/Util.java
@@ -1,4 +1,8 @@
 package calculator.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Util {
     private final String BASIC_SEPARATOR = "[,:]";
 
@@ -6,11 +10,18 @@ public class Util {
         separatorExtract(text);
     }
     private String[] separatorExtract(String text){
+        if(text.startsWith("//")){
+            return customSeparatorExtract(text);
+        }
         return basicSeparatorExtract(text);
     }
 
     private String[] basicSeparatorExtract(String text){
         return text.split(BASIC_SEPARATOR);
+    }
+
+    private String[] customSeparatorExtract(String text){
+
     }
 
 }
