87bb4bb81b161ac7b50f8aba34f77e69835e19ca
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-17 22:15:34 +0900

    chore(Validator): 상수 변수 수정

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -2,19 +2,19 @@ package calculator;
 
 public class Validator {
 
-    private final String errorMessage = "IllegalArgumentException 발생";
+    private static final String ERROR_MESSAGE = "IllegalArgumentException 발생";
 
     public void isPositive() {
-        throw new IllegalStateException("isPositive() : " + errorMessage);
+        throw new IllegalStateException("isPositive() : " + ERROR_MESSAGE);
     }
 
     public void isValidDelimiter() {
-        throw new IllegalStateException("isValidDelimiter() : " + errorMessage);
+        throw new IllegalStateException("isValidDelimiter() : " + ERROR_MESSAGE);
 
     }
 
     public void isValidCustomDelimiter() {
-        throw new IllegalStateException("isValidCustomDelimiter() : " + errorMessage);
+        throw new IllegalStateException("isValidCustomDelimiter() : " + ERROR_MESSAGE);
     }
 
 }
