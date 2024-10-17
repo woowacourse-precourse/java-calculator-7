235b4047cb59c706829a37fa928917abfd9a2a27
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-18 00:52:47 +0900

    refactor(OutputView): 출력 뷰 내부에 결과 출력 문구 상수 정의
    - OutputView 내부에 결과 출력 문구를 상수화

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,10 +1,10 @@
 package calculator.view;
 
-import calculator.global.constant.OutputMessage;
-
 public class OutputView {
 
+    private static final String OUTPUT_PREFIX = "결과 : ";
+
     public void printResult(int result) {
-        System.out.println(OutputMessage.OUTPUT_PREFIX.getMessage() + result);
+        System.out.println(OUTPUT_PREFIX + result);
     }
 }
