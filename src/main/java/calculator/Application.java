9580fbe55897099d4a5bc584ac89b4d5a902e1c6
nodonghui <163218508+nodonghui@users.noreply.github.com>
2024-10-17 15:19:31 +0900

    feat(scanner): 문자열을 토큰으로 분리하는 기능 구현
    입력받은 문자열을 분리해 리스트에 저장하는 기능을 구현했습니다
    커스텀 구분자 인식과 예외 처리도 구현했습니다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,35 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Application {
-    public static void main(String[] args) {
+    static String input;
+    private static Scanner scanner;
+
+    static void Input() throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        input=br.readLine();
+
+
+    }
+    public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
-        System.out.println("git push test");
+
+        Input();
+        try {
+            scanner = new Scanner(input);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+
+
+        }
+        ArrayList<Integer> a=scanner.getOutput();
+
+        for(int i=0;i<a.size();i++) {
+            System.out.print(a.get(i) +" ");
+        }
     }
 }
--- src/main/java/calculator/Scanner.java
+++ src/main/java/calculator/Scanner.java
@@ -1,2 +1,81 @@
-package calculator;public class Scanner {
+package calculator;
+import java.util.ArrayList;
+public class Scanner {
+
+    private ArrayList<Integer> output;
+    private char customDelimiter=',';
+
+    enum State {
+        START,
+        NUMBER,
+        DELIMITER,
+        END
+    }
+    ArrayList<Integer> getOutput() {
+        return output;
+    }
+    public Scanner(String input)  {
+        State state= State.START;
+        output=new ArrayList<>();
+        StringBuilder cur_num=new StringBuilder();
+
+        //커스텀 구분자 처리
+        if(input.startsWith("//")) {
+            int delimiterEndIndex=input.indexOf("\\n");
+            if(delimiterEndIndex !=-1) {
+                customDelimiter=input.substring(2,delimiterEndIndex).charAt(0);
+                input=input.substring(delimiterEndIndex+2);
+            }
+        }
+        System.out.println("customDeliiter: " + customDelimiter);
+        System.out.println("input:" + input);
+
+
+        for(char ch: input.toCharArray()) {
+            switch(state) {
+                case START:
+                    if (Character.isDigit(ch)) {
+                        state=State.NUMBER;
+                        cur_num.append(ch);
+                    } else if(!isDelimiter(ch,customDelimiter)) {
+                        throw new IllegalArgumentException("Invaild input: " + ch);
+                    }
+                    break;
+                case NUMBER:
+                    if(Character.isDigit(ch)) {
+                        cur_num.append(ch);
+                    } else if(isDelimiter(ch,customDelimiter)) {
+                        state=State.DELIMITER;
+                        output.add(Integer.parseInt(cur_num.toString()));
+                        cur_num.setLength(0);
+                    } else {
+                        throw new IllegalArgumentException("Invaild input: " + ch);
+                    }
+                    break;
+                case DELIMITER:
+                    if(Character.isDigit(ch)) {
+                        state=State.NUMBER;
+                        cur_num.append(ch);
+                    } else if(!isDelimiter(ch,customDelimiter)) {
+                        throw new IllegalArgumentException("Invaild input: " + ch);
+                    }
+                    break;
+            }
+
+        }
+
+        if(cur_num.length()>0) {
+            output.add(Integer.parseInt(cur_num.toString()));
+        } else if(output.isEmpty()) {
+            output.add(0);
+        }
+    }
+
+    private static boolean isDelimiter(char ch,char custom_char) {
+
+        return ch == ';' || ch == ',' || ch==custom_char;
+    }
+
+
+
 }
