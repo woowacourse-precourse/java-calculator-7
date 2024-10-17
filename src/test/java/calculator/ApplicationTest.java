d3d8f986fa977a381863bc63fbfa6f84e5514dc4
dradnats1012 <herjebi1012@gmail.com>
2024-10-17 20:59:19 +0900

    test: 테스트코드 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,46 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 기본_구분자_테스트_1(){
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 기본_구분자_테스트_2(){
+        assertSimpleTest(() -> {
+            run("1:2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_숫자_예외_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//3\\n1,3,131"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_예외_테스트_1() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("////\\n1//3"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_예외_테스트2() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//\\n\\n1\\n3"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
