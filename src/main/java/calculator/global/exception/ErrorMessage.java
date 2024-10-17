db1e3bd3bdd7125669cd3ea8117f6f5cc58f39f6
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-18 01:12:47 +0900

    feat(ErrorMessage): 에러 메시지를 enum 클래스에서 상수화
    - 에러 메시지를 enum 클래스에서 정의하여, 에러 메시지의 변경과 확장에 대응

---
+++ src/main/java/calculator/global/exception/ErrorMessage.java
@@ -0,0 +1,17 @@
+package calculator.global.exception;
+
+public enum ErrorMessage {
+    INVALID_DELIMITER_ERROR("올바르지 않은 구분자 입력입니다."),
+    NOT_NUMBER_ERROR("구분자 사이에 숫자를 입력해주세요."),
+    BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
