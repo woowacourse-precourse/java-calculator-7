0af082f389f04f82dcbe5e119d489c152df2a718
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-18 01:15:14 +0900

    feat: 입출력 작업을 위한 Console 클래스 구현
    I/O 메서드 구현:
      - 입력을 위한 readLine 메서드
      - 콘솔 출력을 위한 print 메서드
    - 입출력 코드를 중앙 집중화하여 재사용성과 유지 관리성을 향상.

---
+++ src/main/java/calculator/util/console/Console.java
@@ -0,0 +1,12 @@
+package calculator.util.console;
+
+public class Console {
+
+    public static String readLine() {
+        return Console.readLine();
+    }
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
+}
