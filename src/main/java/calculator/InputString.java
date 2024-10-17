a3a424633ac1430fc4a6e3c6ccb39e977b245696
sim-mer <gyu2781@gmail.com>
2024-10-17 13:13:29 +0900

    refactor: 생성자의 기능들 메서드로 분리

--- src/main/java/calculator/InputString.java
+++ src/main/java/calculator/InputString.java
@@ -7,12 +7,22 @@ public class InputString {
     private List<Integer> numbers;
 
     public InputString(String input) {
-        if(input == null || input.isEmpty()) {
-            this.numbers = List.of();
+        if (isInputEmpty(input)) {
             return;
         }
-        String[] numbers = input.split("[,:]");
-        this.numbers = Arrays.stream(numbers)
+        this.numbers = inputToList(input);
+    }
+
+    private boolean isInputEmpty(String input) {
+        if(input == null || input.isEmpty()) {
+            this.numbers = List.of();
+            return true;
+        }
+        return false;
+    }
+
+    private List<Integer> inputToList(String input) {
+        return Arrays.stream(input.split("[,:]"))
                 .map(Integer::valueOf)
                 .toList();
     }
