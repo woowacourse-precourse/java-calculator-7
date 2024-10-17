5dea538e7d3840d15e59ec1dc22b889314c96a89
yush <yush2202@gmail.com>
2024-10-17 19:16:26 +0900

    feat($Delimiter): 구분자 . (dot) 검증 추가
    구분자에서 . 을 사용하지 못하게 검증 메서드 추가

--- src/main/java/calculator/Model/Delimiter.java
+++ src/main/java/calculator/Model/Delimiter.java
@@ -1,8 +1,12 @@
 package calculator.Model;
 
+import static calculator.Constants.DelimiterConstants.DOT;
 import static calculator.Constants.ErrorMessages.INVALID_CUSTOM_SEPARATOR_BLANK_MESSAGE;
 import static calculator.Constants.ErrorMessages.INVALID_CUSTOM_SEPARATOR_DIGIT_MESSAGE;
+import static calculator.Constants.ErrorMessages.INVALID_CUSTOM_SEPARATOR_DOT_MESSAGE;
 import static calculator.Constants.ErrorMessages.INVALID_CUSTOM_SEPARATOR_EMPTY_MESSAGE;
+
+import java.util.Objects;
 
 public class Delimiter {
     private final String delimiter;
@@ -20,6 +24,7 @@ public class Delimiter {
         isNull(delimiter);
         isDigit(delimiter);
         isBlank(delimiter);
+        isDot(delimiter);
     }
 
     private void isNull(String delimiter) {
@@ -37,6 +42,12 @@ public class Delimiter {
     private void isDigit(String delimiter) {
         if (delimiter.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException(INVALID_CUSTOM_SEPARATOR_DIGIT_MESSAGE.getErrorMessage());
+        }
+    }
+
+    private void isDot(String delimiter) {
+        if (Objects.equals(delimiter, DOT)) {
+            throw new IllegalArgumentException(INVALID_CUSTOM_SEPARATOR_DOT_MESSAGE.getErrorMessage());
         }
     }
 }
