17017f8f307a99de1292383c245b7257f4a5ba78
박성빈 <psb106305@gmail.com>
2024-10-17 13:55:29 +0900

    feat: 정규식으로 숫자 구분

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,24 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class Application {
+
+    public static String createSepRegex(String separators) {
+        String[] sep = separators.split("");
+        StringBuilder builder = new StringBuilder("[");
+
+        Arrays.stream(sep)
+                .map(Pattern::quote)
+                .forEach(builder::append);
+
+        builder.append("]");
+
+        return builder.toString();
+    }
+
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
@@ -14,5 +29,8 @@ public class Application {
             customSeparator = split[0].substring(2);
             input = split[1];
         }
+
+        String regex = createSepRegex(",:" + customSeparator);
+        String[] numbers = input.split(regex);
     }
 }
