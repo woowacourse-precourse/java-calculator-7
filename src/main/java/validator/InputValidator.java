4812db391e400ac972a1afa47fd9b3720f467de8
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-10-18 01:33:38 +0900

    feat: 음수가 포함된 문자열을 입력한 경우 예외 처리

--- src/main/java/validator/InputValidator.java
+++ src/main/java/validator/InputValidator.java
@@ -9,10 +9,12 @@ public class InputValidator {
 
     private static final String REGEX_CUSTOM_DELIMITER_START = "//";
     private static final String REGEX_CUSTOM_DELIMITER_END = "\\n";
+    private static final String MINUS_NUMBER = "-";
 
     public static void checkInput(final String input) {
         hasNumber(input);
         hasSeparator(input);
+        hasMinusNumber(input);
     }
 
     private static void hasNumber(final String input) {
@@ -53,5 +55,20 @@ public class InputValidator {
         }
 
         throw new IllegalArgumentException("올바른 구분자도 함께 입력해야 합니다.");
+    }
+
+    private static void hasMinusNumber(final String inputs) {
+        String[] inputArray = inputs.split("");
+        for(int i = 1; i < inputArray.length; i++){
+            checkMinusNumber(inputArray[i].charAt(0), inputArray[i-1].charAt(0));
+        }
+    }
+
+    private static void checkMinusNumber(final char firstValue, final char secondValue) {
+        if(Character.isDigit(firstValue)) {
+            if(MINUS_NUMBER.equals(String.valueOf(secondValue))) {
+                throw new IllegalArgumentException("양수를 입력해야 합니다.");
+            }
+        }
     }
 }
