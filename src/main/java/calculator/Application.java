ebc2fcc4e259f20241d42fb71d41f4c27562445d
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-17 22:11:51 +0900

    refactor(Application): CalculatorController 기능 사용
    CalculatorController 기능을 추가하여 View, Calculator 기능 객체 생성 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,14 +4,16 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        // 사용자로부터 문자열 입력 받기
-        InputView inputView = new InputView();
+        CalculatorController calculatorController = new CalculatorController();
 
-        // 입력 받은 문자열 내부 숫자들의 합 구하기
-        Calculator calculator = new Calculator(inputView.getUserInput());
+        // 입력 받기
+        calculatorController.displayInputView();
+        calculatorController.getUserInput();
 
-        // 계산 결과 출력
-        OutputView outputView = new OutputView();
-        outputView.printSum(calculator.getSum());
+        // 계산 하기
+        calculatorController.calculate();
+
+        // 출력하기
+        calculatorController.displayOutputView();
     }
 }
