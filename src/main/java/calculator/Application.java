bc347d9661bb6c3544a9e66cf09382edbbb874cb
JiHyeonSu <itip1@mju.ac.kr>
2024-10-18 17:32:33 +0900

    feat(Application): printMessage 메소드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,4 +4,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    private static void printMessage(String output) {
+        System.out.println(output);
+    }
 }
