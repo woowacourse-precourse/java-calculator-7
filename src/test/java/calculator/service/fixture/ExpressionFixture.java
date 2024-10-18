858e4de0d5d26354deb26a93d78680ffcb97917b
mete0rfish <sungwon326@naver.com>
2024-10-18 13:50:52 +0900

    test: 입력 식 Fixture 개행문자 수정
     입력 예시가 \n이기 때문에 개행문자 대신 \\n으로 처리하도록 수정

--- src/test/java/calculator/service/fixture/ExpressionFixture.java
+++ src/test/java/calculator/service/fixture/ExpressionFixture.java
@@ -2,20 +2,20 @@ package calculator.service.fixture;
 
 public class ExpressionFixture {
     public final static String EXPRESSION_WITHOUT_CUSTOM_IDENTIFIER = "1:22.333";
-    public final static String EXPRESSION_WITH_CUSTOM_IDENTIFIER = "//{\n1:22.333";
+    public final static String EXPRESSION_WITH_CUSTOM_IDENTIFIER = "//{\\n1:22.333";
 
     // 1.
-    public final static String EXPRESSION_STARTS_WITH_ONE_SLASH = "/{\n1:22.333";
-    public final static String EXPRESSION_STARTS_WITHOUT_NUMBER = "{\n1:22.333";
+    public final static String EXPRESSION_STARTS_WITH_ONE_SLASH = "/{\\n1:22.333";
+    public final static String EXPRESSION_STARTS_WITHOUT_NUMBER = "{\\n1:22.333";
 
     // 2.
-    public final static String EXPRESSION_DOUBLE_CUSTOM_IDENTIFIER = "//{{\n1:22.333";
+    public final static String EXPRESSION_DOUBLE_CUSTOM_IDENTIFIER = "//{{\\n1:22.333";
 
     // 3.
     public final static String EXPRESSION_CUSTOM_WITHOUT_SLASH_N = "//{1:22.333";
 
     // 4.
-    public final static String EXPRESSION_BLANK_CUSTOM_IDENTIFIER = "//\n1:22.333";
+    public final static String EXPRESSION_BLANK_CUSTOM_IDENTIFIER = "//\\n1:22.333";
 
     // 5.
     public final static String EXPRESSION_INVALID_IDENTIFIER = "1:22{333";
