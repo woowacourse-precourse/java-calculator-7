1e6f13dd473acb1a5433b4da65abf54cf04aea24
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-10-18 19:23:13 +0900

    feat: 입력값을 기본 구분자(쉼표,콜론)를 기준으로 숫자 구분하고 저장하기
    StringBuilder에 숫자들을 String 으로 저장한다.
    그리고 구분자를 만났을 때 저장되어 있는 String 값을 Integer로 바꾼다.
     그 후 해당 숫자를 arr List에 저장한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,6 +10,21 @@ public class Application {
         // TODO: 프로그램 구현
         String str = Console.readLine();
 
-        System.out.println(str);
+        int length = str.length();
+        List<Integer> arr = new ArrayList<>();
+        StringBuilder temp = new StringBuilder();
+        int sum = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                temp.append(str.charAt(i));
+            }
+
+            if (str.charAt(i) == ',' || str.charAt(i) == ':') {
+                arr.add(Integer.parseInt(temp.toString()));
+                temp = new StringBuilder();
+            }
+        }
+        arr.add(Integer.parseInt(temp.toString()));
     }
 }
