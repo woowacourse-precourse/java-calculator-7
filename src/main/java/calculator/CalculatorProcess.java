1eb3c8350d8db379cc6268e49836e6558467289e
taek2222 <oot309@daum.net>
2024-10-18 15:16:42 +0900

    refactor(CalculatorProcess): 검증 역할 분리 및 메서드 명, 스타일 리팩토링
    검증 처리를 분리해 Process 전체에서 처리하도록 변경했습니다.
    
    메서드 명을 유지보수 하기 쉽게 리팩토링 했습니다.
    
    Controller 네이밍에서 처리가 더 올바른 표현으로 생각해 변경했습니다.

--- src/main/java/calculator/CalculatorProcess.java
+++ src/main/java/calculator/CalculatorProcess.java
@@ -1,30 +1,34 @@
 package calculator;
 
-public class CalculatorController {
+public class CalculatorProcess {
     private final OutputView outputView;
     private final InputView inputView;
     private final CalculatorService calculatorService;
     private final ExtractService extractService;
+    private final NumberValidator numberValidator;
 
-    public CalculatorController() {
+    public CalculatorProcess() {
         this.outputView = new OutputView();
         this.inputView = new InputView();
         this.calculatorService = new CalculatorService();
         this.extractService = new ExtractService();
+        this.numberValidator = new NumberValidator();
     }
 
     public void run() {
         outputView.printStartMessage();
         String readInput = inputView.readInput();
 
-        String customDelimiter = extractService.getCustomDelimiter(readInput);
+        String customDelimiter = extractService.extractCustomDelimiter(readInput);
         String positiveString = extractService.extractPositiveString(readInput, customDelimiter);
 
-        String[] strings = calculatorService.splitByPositiveString(positiveString, customDelimiter);
-        int[] intArray = calculatorService.convertStringArrayToIntArray(strings);
+        char[] chars = calculatorService.removeDelimiterAndToCharArray(positiveString, customDelimiter);
+        numberValidator.validateNoDigits(chars); // 숫자 변환 가능한 문자 검사
+
+        int[] intArray = calculatorService.convertCharArrayToIntArray(chars);
+        numberValidator.validatorNoNegatives(intArray); // 양수 검사
 
         int result = calculatorService.calculatorNumbers(intArray);
-
         outputView.printResultMessage(result);
     }
 }
