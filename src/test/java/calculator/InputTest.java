a825025617f4076969782f6698198d80bc2fad99
kimdev <kim__dev@hufs.ac.kr>
2024-10-18 20:04:17 +0900

    test(Input): 입력 테스트 케이스 추가

--- src/test/java/calculator/InputTest.java
+++ src/test/java/calculator/InputTest.java
@@ -15,6 +15,66 @@ public class InputTest {
         input.getInputText(testText);
         assertTrue(input.hasText());
     }
+    @Test
+    @DisplayName("커스텀 구분자가 없이 올바르게 입력된다.")
+    public void 커스텀_구분자_없이_올바른_입력_값2() {
+        String testText = "1:2:3";
+        Input input = new Input();
+        input.getInputText(testText);
+        assertTrue(input.hasText());
+    }
+    @Test
+    @DisplayName("커스텀 구분자가 없이 올바르게 입력된다.")
+    public void 커스텀_구분자_없이_올바른_입력_값3() {
+        String testText = "1,2,3:4:5,6:7,8:9";
+        Input input = new Input();
+        input.getInputText(testText);
+        assertTrue(input.hasText());
+    }
+    @Test
+    @DisplayName("커스텀 구분자가 없이 올바르게 입력된다.")
+    public void 커스텀_구분자_없이_올바른_입력_값4() {
+        String testText = "-3,-4:-5,-6:-7";
+        Input input = new Input();
+        input.getInputText(testText);
+        assertTrue(input.hasText());
+    }
+    @Test
+    @DisplayName("커스텀 구분자가 없이 올바르게 입력된다.")
+    public void 커스텀_구분자_없이_올바른_입력_값5() {
+        String testText = "2147483647,0";
+        Input input = new Input();
+        input.getInputText(testText);
+        assertTrue(input.hasText());
+    }
+
+    @Test
+    @DisplayName("잘못된 입력으로, 예외가 발생한다.")
+    public void 커스텀_구분자_없이_잘못된_입력_값1() {
+        String testText = "1,2:a,4,b:6";
+        Input input = new Input();
+        assertThrows(IllegalArgumentException.class, () -> {
+            input.getInputText(testText);
+        });
+    }
+    @Test
+    @DisplayName("잘못된 입력으로, 예외가 발생한다.")
+    public void 커스텀_구분자_없이_잘못된_입력_값2() {
+        String testText = "2147483648,1:2,3:4";
+        Input input = new Input();
+        assertThrows(IllegalArgumentException.class, () -> {
+            input.getInputText(testText);
+        });
+    }
+    @Test
+    @DisplayName("잘못된 입력으로, 예외가 발생한다.")
+    public void 커스텀_구분자_없이_잘못된_입력_값3() {
+        String testText = "가,나:다,라:마";
+        Input input = new Input();
+        assertThrows(IllegalArgumentException.class, () -> {
+            input.getInputText(testText);
+        });
+    }
 
     @Test
     @DisplayName("커스텀 구분자와 함께 올바르게 입력된다.")
