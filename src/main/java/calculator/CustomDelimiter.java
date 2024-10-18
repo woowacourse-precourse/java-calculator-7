4a9859cf53a2e063c779ed231b9053edb82e41e0
eunho <eunho1032@gmail.com>
2024-10-18 16:27:30 +0900

    fix(CustomDelimiter): 끝 인덱스 반환값 1 줄임

--- src/main/java/calculator/CustomDelimiter.java
+++ src/main/java/calculator/CustomDelimiter.java
@@ -7,20 +7,20 @@ public class CustomDelimiter {
 		int endIndex = input.indexOf("\\n");
 		int errIndex = input.indexOf("///");
 
-		if (beginIndex == -1) {
+		if (beginIndex == -1 && endIndex == -1) {
 			return defaultDelimiter;
 		}
 		if (beginIndex != 0 || endIndex == -1 || errIndex == 0) { // 커스텀 구분자를 위한 포맷이 지켜지지 않았을 때
-			throw new IllegalArgumentException("틀린 커스텀 구분자 포맷");
+			throw new IllegalArgumentException("잘못된 커스텀 구분자");
 		}
 		if (beginIndex + 2 == endIndex){
-			throw new IllegalArgumentException("빈 커스텀 구분자");
+			throw new IllegalArgumentException("잘못된 커스텀 구분자");
 		}
 		return input.substring(beginIndex + 2, endIndex);
 	}
 
 	public static int findDelimiterEndIndex(String input) {
 		int endIndex = input.indexOf("\\n");
-		return endIndex + 2; // 커스텀 구분자 지정 문자를 뺀 끝 인덱스 반환
+		return endIndex + 1; // 커스텀 구분자 지정 문자를 뺀 끝 인덱스 반환
 	}
 }
