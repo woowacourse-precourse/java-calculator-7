0a2507e3db2f3245609a9eafbbeabef6921e6572
wlsdlf2 <wlsdlfzg@naver.com>
2024-10-18 21:04:49 +0900

    refactor: 커스텀 구분자 기능 오류 수정  - \n을 이스케이프 문자를 사용하여 \\n으로 변경  - 숫자부분의 문자열이 시작되는 인덱스를 나타내는 변수 추가

--- src/main/java/calculator/DelimiterParser.java
+++ src/main/java/calculator/DelimiterParser.java
@@ -11,7 +11,8 @@ public class DelimiterParser {
         String numbersPart = input; // 숫자 부분
 
         if (input.startsWith("//")) {
-            int delimiterEndIndex = numbersPart.indexOf("\n");
+            int delimiterEndIndex = numbersPart.indexOf("\\n");
+            int numbersStartIndex = delimiterEndIndex + 2;
 
             if (delimiterEndIndex == -1) {
                 // 잘못된 입력. 예외 발생.
@@ -21,7 +22,7 @@ public class DelimiterParser {
 
             // 기본 구분자와 커스텀 구분자를 합쳐서 정규표현식 생성
             delimiter += "|" + Pattern.quote(customDelimiter); // customDelimiter 이스케이프 처리
-            numbersPart = input.substring(delimiterEndIndex + 1);
+            numbersPart = input.substring(numbersStartIndex);
         }
 
         return numbersPart.split(delimiter);
