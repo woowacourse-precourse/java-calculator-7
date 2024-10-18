e1376ad38187b59b55fada67c609f96b13f0b6e3
rladmstn <oz33177351@gmail.com>
2024-10-18 17:21:29 +0900

    feat(validator) : input 관련 regex constants 추가
    input validation에 필요한 정규식 constants 클래스 추가

---
+++ src/main/java/calculator/constants/InputValidationConstants.java
@@ -0,0 +1,12 @@
+package calculator.constants;
+
+public class InputValidationConstants {
+    public static final String CONTAINS_NUMBERS_PATTERN = ".*[0-9].*";
+    public static final String CUSTOM_DELIMITER_PATTERN_PREFIX = "^([0-9]+";
+    public static final String CUSTOM_DELIMITER_PATTERN_SUFFIX = ")*[0-9]*$";
+    public static final String NORMAL_DELIMITER_INPUT_PATTERN = "^[:,0-9]*$";
+
+    private InputValidationConstants(){
+        throw new RuntimeException("Constants class cannot be instantiated");
+    }
+}
