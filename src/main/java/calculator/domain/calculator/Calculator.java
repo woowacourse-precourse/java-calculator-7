bfb25fb519ae713afcc2faa7c19693a6f2765ec7
woonie155 <woonie155@gmail.com>
2024-10-19 03:22:49 +0900

    style: 자바 코드 컨벤션 적용

--- src/main/java/calculator/domain/calculator/Calculator.java
+++ src/main/java/calculator/domain/calculator/Calculator.java
@@ -1,7 +1,6 @@
 package calculator.domain.calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 
 public class Calculator {
@@ -10,52 +9,52 @@ public class Calculator {
 
     private String inputStr;
     private ArrayList<Integer> numberList = new ArrayList<>();
-    private Integer result=0;
+    private Integer result = 0;
 
-    public Calculator(Delimiter delimiter){
+    public Calculator(Delimiter delimiter) {
         this.delimiter = delimiter;
     }
 
-    public void plusOperation(){
+    public void plusOperation() {
         this.getString();
         this.extractNumberList();
         this.plusCalculate();
         this.printResult();
     }
 
-    private void getString(){
+    private void getString() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String s = Console.readLine();
         Console.close();
 
-        if(s.length()>=5 && s.charAt(0)=='/' && s.charAt(1)=='/'
-                && s.charAt(3)=='\\' && s.charAt(4)=='n'){
-            if( Character.isDigit(s.charAt(2))){
+        if (s.length() >= 5 && s.charAt(0) == '/' && s.charAt(1) == '/'
+                && s.charAt(3) == '\\' && s.charAt(4) == 'n') {
+            if (Character.isDigit(s.charAt(2))) {
                 throw new IllegalArgumentException();
             }
             delimiter.addDelimiter(s.charAt(2));
             s = s.substring(5);
         }
 
-        for(int i = 0; i<s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if( !(Character.isDigit(ch) || delimiter.isDelimiter(ch))){
+            if (!(Character.isDigit(ch) || delimiter.isDelimiter(ch))) {
                 throw new IllegalArgumentException();
             }
         }
         this.inputStr = s;
     }
 
-    private void extractNumberList(){
+    private void extractNumberList() {
         int num = 0;
-        for(char ch: this.inputStr.toCharArray()){
-            if(delimiter.isDelimiter(ch)){
+        for (char ch : this.inputStr.toCharArray()) {
+            if (delimiter.isDelimiter(ch)) {
                 numberList.add(num);
                 num = 0;
-            }else{
+            } else {
                 num *= 10;
-                num += (ch-'0');
-                if(num >= CalculatorConstant.LIMIT_NUMBER){
+                num += (ch - '0');
+                if (num >= CalculatorConstant.LIMIT_NUMBER) {
                     throw new IllegalArgumentException();
                 }
             }
@@ -63,16 +62,16 @@ public class Calculator {
         numberList.add(num);
     }
 
-    private void plusCalculate(){
-        for(Integer n : numberList){
-            result+=n;
-            if(result>=CalculatorConstant.LIMIT_NUMBER){
+    private void plusCalculate() {
+        for (Integer number : numberList) {
+            result += number;
+            if (result >= CalculatorConstant.LIMIT_NUMBER) {
                 throw new IllegalArgumentException();
             }
         }
     }
 
-    private void printResult(){
-        System.out.println("결과 : "+result);
+    private void printResult() {
+        System.out.println("결과 : " + result);
     }
 }
