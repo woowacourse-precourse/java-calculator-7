6214b612330ccf11fe7a03a53c32e592a581f79e
mete0rfish <sungwon326@naver.com>
2024-10-18 13:49:09 +0900

    feat(#1): CalculatorOutput의 출력 구문 추가
    "입력 : "을 추가

--- src/main/java/calculator/view/CalculatorOutput.java
+++ src/main/java/calculator/view/CalculatorOutput.java
@@ -3,6 +3,6 @@ package calculator.view;
 public class CalculatorOutput {
 
     public static void outputResult(Long res) {
-        System.out.println(res);
+        System.out.println("결과 : " + res);
     }
 }
