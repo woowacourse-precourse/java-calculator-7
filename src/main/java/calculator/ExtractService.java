a839e838ee65006f7bec821cdeb2c948d1df9c85
taek2222 <oot309@daum.net>
2024-10-18 15:49:17 +0900

    style(ExtractService): 변수, 메서드 네이밍 변경
    더욱 직관적인 이름을 위해 메서드와 변수명을 변경했습니다.

--- src/main/java/calculator/ExtractService.java
+++ src/main/java/calculator/ExtractService.java
@@ -1,24 +1,22 @@
 package calculator;
 
 public class ExtractService {
-
     public static final String CUSTOM_PREFIX = "//";
     public static final String CUSTOM_SUFFIX = "\\n";
     public static final String EMPTY = "";
     public static final int CUSTOM_PREFIX_LENGTH = CUSTOM_PREFIX.length();
 
-    public String getCustomDelimiter(String readInput) {
+    public String extractCustomDelimiter(String readInput) {
         if (hasCustomDelimiter(readInput)) {
-            int[] position = findCustomDelimiterPosition(readInput);
-            return extractDelimiter(readInput, position);
+            int[] positions = findCustomDelimiterPosition(readInput);
+            return extractDelimiter(readInput, positions);
         }
         return EMPTY;
     }
 
     public String extractPositiveString(String readInput, String customDelimiter) {
         if (hasCustomDelimiter(readInput)) {
-            String remove = buildRemoveString(customDelimiter);
-            return removeCustomDelimiter(readInput, remove);
+            String remove = createRemoveString(customDelimiter);
             return readInput.replace(remove, EMPTY);
         }
         return readInput;
@@ -29,17 +27,16 @@ public class ExtractService {
     }
 
     private int[] findCustomDelimiterPosition(String readInput) {
-        int startPos = readInput.indexOf(CUSTOM_PREFIX);
-        int endPos = readInput.indexOf(CUSTOM_SUFFIX);
-
-        return new int[] {startPos, endPos};
+        int prefixPos = readInput.indexOf(CUSTOM_PREFIX) + CUSTOM_PREFIX_LENGTH; // "//" 문자열 크기 만큼 이후 탐색을 위해 합산
+        int suffixPos = readInput.indexOf(CUSTOM_SUFFIX);
+        return new int[] {prefixPos, suffixPos};
     }
 
     private String extractDelimiter(String readInput, int[] position) {
-        return readInput.substring(position[0] + CUSTOM_PREFIX_LENGTH, position[1]);
+        return readInput.substring(position[0], position[1]);
     }
 
-    private String buildRemoveString(String customDelimiter) {
+    private String createRemoveString(String customDelimiter) {
         return CUSTOM_PREFIX + customDelimiter + CUSTOM_SUFFIX;
     }
 }
