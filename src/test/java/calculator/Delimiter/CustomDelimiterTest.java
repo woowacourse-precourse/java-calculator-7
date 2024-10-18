f3e02bdd6058ba38f38ac36a4f5de9ab7d956086
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-19 05:28:31 +0900

    test: 커스텀 구분자 처리 예외 테스트 추가
    - 음수가 포함된 경우
    - 구분자 사이가 비어있을 경우
    - 커스텀 구분자 지정의 마무리를 \n으로 하지 않은 경우
    - 커스텀,기본 구분자 이외의 문자가 들어간 경우

--- src/test/java/calculator/Delimiter/CustomDelimiterTest.java
+++ src/test/java/calculator/Delimiter/CustomDelimiterTest.java
@@ -1,7 +1,9 @@
 package calculator.Delimiter;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import Delimiter.BasicDelimiter;
 import Delimiter.CustomDelimiter;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +20,39 @@ public class CustomDelimiterTest {
         assertThat(numbers).isEqualTo(result);
     }
 
+    @Test
+    void 커스텀_구분자_처리_예외_음수() {
+        //given
+        String example = "//;\\n1;2,-3;4";
+        //when, then
+        assertThatThrownBy(() -> CustomDelimiter.getNumber(example))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @Test
+    void 커스텀_구분자_처리_예외_연속된_구분자() {
+        //given
+        String example = "//;\\n1;;2,-3;4";
+        //when, then
+        assertThatThrownBy(() -> CustomDelimiter.getNumber(example))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 커스텀_구분자_처리_예외_커스텀_구분자_지정_마무리() {
+        //given
+        String example = "//;n:1,2;3";
+        //when, then
+        assertThatThrownBy(() -> CustomDelimiter.getNumber(example))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 커스텀_구분자_처리_예외_구분자_이외의_문자() {
+        //given
+        String example = "//;\\n:1,2;3^2";
+        //when, then
+        assertThatThrownBy(() -> BasicDelimiter.getNumber(example))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
