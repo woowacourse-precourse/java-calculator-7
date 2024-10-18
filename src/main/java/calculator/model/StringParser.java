4cd50a7549851284c2408b7690bc1283382ce587
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-19 04:09:47 +0900

    refactor(StringParser) : 제네릭 타입 이름 변경 및 일부 코드 수정 - 생성자의 파라미터를 ExpressionDTO에서 String으로 변경

--- src/main/java/calculator/model/StringParser.java
+++ src/main/java/calculator/model/StringParser.java
@@ -7,46 +7,46 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 
-public class StringParser<E> implements Parser<E> {
+public class StringParser<T> implements Parser<T> {
 
     private final ExpressionDTO expressionDTO;
 
-    public StringParser(ExpressionDTO expressionDto) {
-        this.expressionDTO = expressionDto;
+    public StringParser(String input) {
+        this.expressionDTO = new ExpressionDTO(input);
     }
 
     @Override
     public boolean checkIncorrectGrammar() {
-        return Pattern.compile(expressionDTO.getIncorrectPattern())
+        return Pattern.compile(expressionDTO.getIncorrectDelimiter())
                 .matcher(expressionDTO.getExpression())
                 .find();
     }
 
     @Override
-    public List<E> parse(Function<String, E> valueType) {
+    public List<T> parse(Function<String, T> typeToChange) {
         if (checkIncorrectGrammar()) {
-            throw new IllegalArgumentException(String.format("%s : 문법에 어긋나는 표현식입니다.", expressionDTO.getExpression()));
+            throw new IllegalArgumentException(expressionDTO.getExpression() + ": 문법에 어긋난 표현식입니다.");
         }
         return makeTokens().stream()
-                .map(s -> changeNumberTypeOrThrowException(s, valueType))
+                .map(s -> changeNumberTypeOrThrowException(s, typeToChange))
                 .toList();
     }
 
-    private List<String> makeTokens() {
+    public List<String> makeTokens() {
         String input = expressionDTO.getExpression();
         if (input.isEmpty()) {
             return Collections.emptyList();
         }
         return Arrays.stream(input.split(expressionDTO.getDelimiter()))
-                .filter(token -> !token.isEmpty())  // 구분자가 나란히 있는 경우에도 숫자 추출 가능
+                .filter(token -> !token.isEmpty())  // 구분자가 서로 붙어 있는 경우에도 숫자 추출 가능
                 .toList();
     }
 
-    private E changeNumberTypeOrThrowException(String token, Function<String, E> how) {
+    private T changeNumberTypeOrThrowException(String token, Function<String, T> typeToChange) {
         try {
-            return how.apply(token);
+            return typeToChange.apply(token);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(String.format("%s : 정수형이나 실수형으로 변환할 수 없는 문자열입니다.", token));
+            throw new IllegalArgumentException(String.format("%s : 숫자 자료형으로 변환할 수 없는 문자열입니다.", token));
         }
     }
 }
