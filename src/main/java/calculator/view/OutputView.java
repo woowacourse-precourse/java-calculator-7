80e0be8454591a51a0471fb0d5189a076f3bae91
변우영 <likevangogh24@gmail.com>
2024-10-18 17:44:43 +0900

    refactor(OutputView): OUTPUT_RESULT 에서 RESULT_PREFIX 로 이름 변경

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,9 +1,9 @@
 package calculator.view;
 
 public class OutputView {
-    private static final String OUTPUT_RESULT = "결과 : ";
+    private static final String RESULT_PREFIX = "결과 : ";
 
     public static void printResult(int sum) {
-        System.out.println(OUTPUT_RESULT + sum);
+        System.out.println(RESULT_PREFIX + sum);
     }
 }
