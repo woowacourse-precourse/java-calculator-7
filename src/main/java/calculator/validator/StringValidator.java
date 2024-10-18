d61a4eed3ac07ec2c6d060a29568e05e04cbd6d6
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-19 00:28:10 +0900

    feat(StringValidator): 커스텀 구분자에 숫자 입력 예외 처리
    - 숫자 구분 모호성을 고려해 커스텀 구분자에 숫자 입력 예외 처리

--- src/main/java/calculator/validator/StringValidator.java
+++ src/main/java/calculator/validator/StringValidator.java
@@ -9,4 +9,10 @@ public class StringValidator {
             throw new IllegalArgumentException(ErrorMessage.BLANK_INPUT_ERROR.getMessage());
         }
     }
+
+    public static void validateCustomDelimiterFormat(String str) {
+        if (str.matches("\\d")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DELIMITER_ERROR.getMessage());
+        }
+    }
 }
