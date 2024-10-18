85f28d34fce3a396467ec65e9f606a6ad52e520b
songsunkook <songsunkook@gmail.com>
2024-10-18 15:20:22 +0900

    test: 추가 테스트 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,12 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -24,8 +25,35 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 정상_케이스() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+            run("1::2::3");
+            assertThat(output()).contains("결과 : 6");
+            run("1,2,3:4:5");
+            assertThat(output()).contains("결과 : 15");
+            run("//1\\n21212");
+            assertThat(output()).contains("결과 : 6");
+            run(",,,,3");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 예외_케이스() {
+        assertSimpleTest(() -> {
+                assertThatThrownBy(() -> runException("//;;\\n1;2;3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+                assertThatThrownBy(() -> runException("//;\\n1,2:3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+            }
+        );
+    }
+
     @Override
     public void runMain() {
-        Application.main(new String[]{});
+        Application.main(new String[] {});
     }
 }
