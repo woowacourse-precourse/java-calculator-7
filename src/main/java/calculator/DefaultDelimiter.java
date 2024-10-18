8b5bddbbfa8480d750fcb8b955231ceb4d0e6435
sc@t0a14 <gusdn6671@gmail.com>
2024-10-18 09:06:12 +0900

    refactor(DefaultDelimiter) : 기본 구분자 생성 변경

--- src/main/java/calculator/DefaultDelimiter.java
+++ src/main/java/calculator/DefaultDelimiter.java
@@ -3,8 +3,8 @@ package calculator;
 public class DefaultDelimiter {
     private final String defaultDelimiter;
 
-    public DefaultDelimiter() {
-        this.defaultDelimiter = "[,:]";
+    public DefaultDelimiter(String defaultDelimiter) {
+        this.defaultDelimiter = defaultDelimiter ;
     }
 
     public String getDefaultDelimiter() {
