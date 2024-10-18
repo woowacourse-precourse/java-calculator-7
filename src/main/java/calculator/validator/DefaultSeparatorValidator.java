ae9e6ca5cdc16d8947d2b786010a62fdfe186af4
ljhee92 <weilainib7@gmail.com>
2024-10-18 20:16:57 +0900

    fix: 기본 구분자 . -> , 수정

--- src/main/java/calculator/validator/DefaultSeparatorValidator.java
+++ src/main/java/calculator/validator/DefaultSeparatorValidator.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class DefaultSeparatorValidator {
     public void validate(List<String> defaultSeparator, String input) {
         if (!Arrays.stream(input.split("")).toList().containsAll(defaultSeparator)) {
-            throw new IllegalArgumentException("[ERROR] 기본 구분자는 .과 :만 사용 가능합니다.");
+            throw new IllegalArgumentException("[ERROR] 기본 구분자는 ,과 :만 사용 가능합니다.");
         } // end if
     } // validate
 } // class
