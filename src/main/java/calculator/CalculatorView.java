51b2f1d1705d7138a35e946ef4937ada70299b20
wkqkel <wkqkel@naver.com>
2024-10-18 23:25:29 +0900

    refactor: 사용자입력 받기 Console Api로 변경

--- src/main/java/calculator/CalculatorView.java
+++ src/main/java/calculator/CalculatorView.java
@@ -1,12 +1,11 @@
 package calculator;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class CalculatorView {
 
     public String read() {
-        Scanner scanner = new Scanner(System.in);
-
-        return scanner.nextLine();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
     }
 }
