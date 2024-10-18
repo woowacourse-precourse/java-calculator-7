214da84409a858f32c4a5577ec59896bf70715a5
parksomii <somi020605@gmail.com>
2024-10-19 00:51:59 +0900

    feat: 커스텀 구분자 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,7 +4,12 @@ import java.util.Arrays;
 
 public class StringCalculator {
     public int add(String input) {
-        if (input.isEmpty()) return 0;
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\n");
+            String customDelimiter = input.substring(2, delimiterIndex);
+            input = input.substring(delimiterIndex + 1);
+            return Arrays.stream(input.split(customDelimiter)).mapToInt(Integer::parseInt).sum();
+        }
         return Arrays.stream(input.split("[,|:]")).mapToInt(Integer::parseInt).sum();
     }
 }
