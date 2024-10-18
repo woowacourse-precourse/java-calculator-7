f57480ff1578d9bee8466ba75900272dc3e054c4
jimmy0524 <10jmin04@naver.com>
2024-10-19 04:21:47 +0900

    test: 출력 테스트 작성

---
+++ src/test/java/calculator/view/CalculatorViewTest.java
@@ -0,0 +1,22 @@
+package calculator.view;
+
+import calculator.model.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CalculatorViewTest {
+    @Test
+    void 결과_출력_테스트() {
+        //given: 값 입력
+        Calculator calculator = new Calculator();
+        CalculatorView calculatorView = new CalculatorView();
+        calculator.setSum(10);
+
+        //when: 출력 처리
+        String output = calculatorView.displayOutput();
+
+        //then: "결과 : 10" 출력
+        assertThat(output).isEqualTo("결과 : 10");
+    }
+}
