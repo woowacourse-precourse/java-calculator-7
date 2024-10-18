2db770c9bf823a51dffd08be7924a7700585a993
whdmscks11 <whdmscks11@gmail.com>
2024-10-18 16:37:26 +0900

    feat: isolate numeric values
    User input is a combined mixture of numbers and delimiters. To calculate the sum of the given numeric values, the input must be split into individual components.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,14 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
+
+        String[] nums;
+        String delimiter = ",;";
+
+        if(str.matches("//.\\\\n.+")){
+            delimiter = String.valueOf(str.charAt(2));
+            str = str.substring(5);
+        }
+        nums = str.split(delimiter);
     }
 }
