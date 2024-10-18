e15192ecc232f0f65daeb09fd5bf612d99e05489
김연진 <love_homebody@naver.com>
2024-10-18 22:13:41 +0900

    chore: reformat code

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -7,6 +7,7 @@ public class OutputView {
     public void printInputGuide() {
         System.out.println(OutputMessage.INPUT_GUIDE.getMessage());
     }
+
     public void printResult(int result) {
         System.out.println(OutputMessage.RESULT.getMessage() + result);
     }
