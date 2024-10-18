7d9cee20b63c0c39048647a3edf22796436b0e4a
rladmstn <oz33177351@gmail.com>
2024-10-18 17:36:06 +0900

    feat(validator) : 유효한 input인지 검증하는 InputValidator 구현
    input 유효성 검증에 적용될 세 가지 조건을 검사하는 메서드 validate() 구현
    
    1. 커스텀 구분자를 사용할 경우, 커스텀 구분자와 숫자들로만 구성되어야 한다.
    2. 커스텀 구분자를 사용할 경우, 커스텀 구분자에 숫자가 포함되어선 안된다.
    3. 기본 구분자를 사용할 경우, 기본 구분자(`:` `,`)와 숫자들로만 구성되어야 한다.

---
+++ src/main/java/calculator/domain/InputValidator.java
@@ -0,0 +1,41 @@
+package calculator.domain;
+
+import static calculator.constants.InputValidationConstants.CUSTOM_DELIMITER_PATTERN_PREFIX;
+import static calculator.constants.InputValidationConstants.CUSTOM_DELIMITER_PATTERN_SUFFIX;
+import static calculator.constants.InputValidationConstants.CONTAINS_NUMBERS_PATTERN;
+import static calculator.constants.InputValidationConstants.INVALID_INPUT_EXCEPTION_MESSAGE;
+import static calculator.constants.InputValidationConstants.NORMAL_DELIMITER_INPUT_PATTERN;
+
+import java.util.regex.Pattern;
+
+public class InputValidator {
+    public static void validate(String input){
+        if(Parser.hasCustomDelimiter(input)){
+            String delimiter = Parser.extractCustomDelimiter(input);
+
+            validateCustomDelimiterForm(delimiter);
+            validateInputWithCustomDelimiter(input, delimiter);
+        }
+        else
+            validateInputWithNormalDelimiter(input);
+    }
+
+    private static void validateInputWithCustomDelimiter(String input, String delimiter) {
+        String escapedDelimiter = Pattern.quote(delimiter);
+        String pattern = CUSTOM_DELIMITER_PATTERN_PREFIX + escapedDelimiter + CUSTOM_DELIMITER_PATTERN_SUFFIX;
+        if (!Pattern.matches(pattern, input.substring(delimiter.length()+4)))
+            throw new IllegalArgumentException(INVALID_INPUT_EXCEPTION_MESSAGE);
+    }
+
+    private static void validateCustomDelimiterForm(String delimiter) {
+        if (delimiter.isEmpty() || Pattern.matches(CONTAINS_NUMBERS_PATTERN, delimiter))
+            throw new IllegalArgumentException(INVALID_INPUT_EXCEPTION_MESSAGE);
+    }
+
+    private static void validateInputWithNormalDelimiter(String input) {
+        String cleanedInput = input.replace(" ", "");
+        if(!Pattern.matches(NORMAL_DELIMITER_INPUT_PATTERN, cleanedInput)) {
+            throw new IllegalArgumentException(INVALID_INPUT_EXCEPTION_MESSAGE);
+        }
+    }
+}
