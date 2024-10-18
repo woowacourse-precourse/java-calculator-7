f4e9eb3f6035650d8a17335908c2283167743837
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-19 00:30:56 +0900

    refactor: 구분자 생성 팩토리 패턴 적용을 위한 클래스 추상화

--- src/main/java/calculator/delimiter/factory/DelimiterFactory.java
+++ src/main/java/calculator/delimiter/factory/DelimiterFactory.java
@@ -2,21 +2,17 @@ package calculator.delimiter.factory;
 
 import calculator.delimiter.domain.Delimiter;
 import calculator.delimiter.domain.Delimiters;
+import calculator.delimiter.validator.CustomDelimiterValidator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+public abstract class DelimiterFactory {
 
-public class DelimiterFactory {
+    protected final CustomDelimiterValidator customDelimiterValidator;
 
-    private static final List<Delimiter> DEFAULT_DELIMITERS = List.of(
-            new Delimiter(","),
-            new Delimiter(":")
-    );
-
-    public static Delimiters createDelimiters(Delimiter... additionalDelimiters) {
-        List<Delimiter> delimiters = new ArrayList<>(DEFAULT_DELIMITERS);
-        delimiters.addAll(Arrays.asList(additionalDelimiters));
-        return new Delimiters(delimiters);
+    public DelimiterFactory(CustomDelimiterValidator customDelimiterValidator) {
+        this.customDelimiterValidator = customDelimiterValidator;
     }
+
+    public abstract Delimiters createDelimiters(Delimiter... additionalDelimiters);
+
+    public abstract Delimiter createDelimiter(String value);
 }
