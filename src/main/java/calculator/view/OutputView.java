30b634d144abff6ba1994678308e7b2b3ee5ca08
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-10-19 01:57:20 +0900

    OutputView 클래스 구현

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,4 +1,9 @@
 package calculator.view;
 
 public class OutputView {
+    private static final String RESULT_MESSAGE = "결과 : %d";
+
+    public void showResultMessage(int result) {
+        System.out.printf(RESULT_MESSAGE, result);
+    }
 }
