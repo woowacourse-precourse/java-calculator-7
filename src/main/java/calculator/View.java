3525bca9efd3d1e983fea509cb45fde4e94e20a6
hyuuuun <pam4522@gmail.com>
2024-10-18 18:34:42 +0900

    feat : 계산 결과 출력

--- src/main/java/calculator/View.java
+++ src/main/java/calculator/View.java
@@ -6,6 +6,7 @@ public class View {
     Input input = new Input();
     String inputStr;
     String[] tokens;
+
     public void calView(){
         System.out.println("덧셈할 문자열을 입력해 주세요");
         inputStr = input.inputCal();
@@ -15,7 +16,7 @@ public class View {
         for(String token : tokens){
             System.out.println(token);
         }
-        System.out.println("결과 : ");
+        System.out.println("결과 : "+input.sumToken());
     }
 
 
