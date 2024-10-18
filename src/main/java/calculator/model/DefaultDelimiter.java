d0f46d86ac61817d3a042120687b73678b38eead
koseonje <kosunje1344@naver.com>
2024-10-19 01:30:46 +0900

    refactor : Enum 상수 이름 수정

--- src/main/java/calculator/model/DefaultDelimiter.java
+++ src/main/java/calculator/model/DefaultDelimiter.java
@@ -6,8 +6,8 @@ import java.util.stream.Collectors;
 
 public enum DefaultDelimiter {
 
-    DEFAULT_COMMA(","),
-    DEFAULT_COLON(":");
+    COMMA(","),
+    COLON(":");
 
     private final String delimiter;
 
