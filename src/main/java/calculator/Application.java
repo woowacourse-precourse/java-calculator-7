9cc7aead7ba2423db63b742913a36c61613678f4
jieunpark <jieun20626@gmail.com>
2024-10-19 01:24:29 +0900

    feat: 쉼표 또는 콜론으로 구분된 숫자들의 합을 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine();  // read input
+            int result;
+            result = calculate(input);
+            System.out.println("결과 : " + result);
+    }
+
+    public static int calculate(String input) {
+        String[] tokens = input.split("[,:]"); // basic separator : , :
+        int sum = 0;
+        for (String token : tokens) {
+            int number = Integer.parseInt(token); //string to int
+
+            sum += number;
+        }
+
+        return sum;
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,22 +8,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+//    @Test
+//    void 커스텀_구분자_사용() {
+//        assertSimpleTest(() -> {
+//            run("//;\\n1");
+//            assertThat(output()).contains("결과 : 1");
+//        });
+//    }
+//
+//    @Test
+//    void 예외_테스트() {
+//        assertSimpleTest(() ->
+//            assertThatThrownBy(() -> runException("-1,2,3"))
+//                .isInstanceOf(IllegalArgumentException.class)
+//        );
+//    }
+
+    // test 01 : comma
     @Test
-    void 커스텀_구분자_사용() {
+    void comma() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("1,2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+    // test 02 : colon
+    @Test
+    void colon() {
+        assertSimpleTest(() -> {
+            run("1:2");
+            assertThat(output()).contains("결과 : 3");
         });
     }
 
+    // test 03 : colon
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+    void comma_colon() {
+        assertSimpleTest(() -> {
+            run("1:2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
     }
-
     @Override
     public void runMain() {
         Application.main(new String[]{});
