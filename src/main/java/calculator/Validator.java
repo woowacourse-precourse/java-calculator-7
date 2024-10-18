096abcb75161df3693bfa57e1953fe756ebf7455
sinminseok12 <sinminseok12@naver.com>
2024-10-19 03:37:26 +0900

    feat : 숫자가 아닌 문자가 들어온 경우 예외 처리 구현

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.List;
+
 import static calculator.Extractor.extractDelimiter;
 import static calculator.Extractor.extractNumberPart;
 
@@ -7,6 +9,7 @@ public class Validator {
     private static final String ERROR_INVALID_DELIMITER = "커스텀 구분자로 숫자 또는 공백을 사용할 수 없습니다.";
     private static final String ERROR_INVALID_FORMAT = "커스텀 구분자 형식이 올바르지 않습니다.";
     private static final String ERROR_DELIMITER_TYPE = "커스텀 구분자는 영어,숫자,공백을 허용하지 않습니다.: ";
+    private static final String ERROR_NUMBER_TYPE = "잘못된 숫자 형식입니다: ";
     private static final String ERROR_DELIMITER_DUPLICATE = "중복된 구분자는 허용하지 않습니다.";
     private static final String INVALID_CUSTOM_DELIMITER_PATTERN = "//[\\d\\s]*((\\\\n)|(\\n)).*";
 
@@ -26,6 +29,22 @@ public class Validator {
         String numberPart = extractNumberPart(value, prefix, suffix);
         validateDelimiterType(numberPart, delimiter);
         validateDuplicateDelimiter(delimiter);
+        validateNumberPart(numberPart, delimiter);
+    }
+
+    private void validateNumberPart(String numberPart, String delimiter) {
+        List<String> numbers = List.of(numberPart.split(delimiter));
+        for (String number : numbers) {
+            validateNumberFormat(number);
+        }
+    }
+
+    private static void validateNumberFormat(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_NUMBER_TYPE + number);
+        }
     }
 
     private void validateCustomDelimiterFormat(String value){
