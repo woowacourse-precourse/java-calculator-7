0c71e37eb61b7b72755856f90b7b1dda2741efa9
eunho <eunho1032@gmail.com>
2024-10-18 13:02:24 +0900

    feat(StringSplit): 이스케이프 문자 예외처리

--- src/main/java/calculator/StringSplit.java
+++ src/main/java/calculator/StringSplit.java
@@ -2,13 +2,11 @@ package calculator;
 
 public class StringSplit {
 	public static String[] splitString(String input, String delimiter, int beginIndex) {
+		if (delimiter.indexOf("\\") != -1){
+			throw new IllegalArgumentException("틀린 커스텀 구분자 포맷");
+		}
 		String newString = input.substring(beginIndex);
 		String[] numbers = newString.split("[" + delimiter + "]"); // //\n 이후의 문자열을 구분자로 나눔
-		/*
-		 * 24.10.17
-		 * 사용자 입력이 //\\n22\23\44 처럼 들어오는 경우 위 ("[" + delimiter + "]") 표현에서 대괄호가 닫히지 않아 문제 발생
-		 * split 직접 구현 필요
-		 */
 		return numbers;
 	}
 }
