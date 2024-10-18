b79251d1adf55d8dae2cab000c378fef78c52e3c
Youngchan <wdc4512@naver.com>
2024-10-19 02:04:24 +0900

    refactor: 예외 처리 및 기타 코드 정리
    - 기본 문법 오류 수정 및 코드 정리
    - 정규식 특수문자 구분자 처리
    - IllegalArgumentException 예외 처리
    - 람다식을 메소드참조 형식으로 변환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,27 +1,27 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // 사용자에게 입력 요청
-        System.out.print("문자열 입력: ");
+        try {
+            // 사용자에게 입력 요청
+            System.out.println("덧셈할 문자열을 입력하세요.");
 
-        // 콘솔에서 입력받은 문자열을 input에 저장
-        String input = Console.readLine();
+            // 콘솔에서 입력받은 문자열을 input에 저장
+            String input = Console.readLine();
 
-        // 입력받은 문자열을 출력
-        System.out.println("입력한 문자열: " + input);
+            // 입력받은 문자열을 분리 후 숫자로 변환
+            List<Integer> numbers = StringCalculator.splitAndConvert(input);
 
-        // 입력받은 문자열을 분리 후 숫자로 변환
-        List<Integer> number = StringCalculator.splitAndConvert(input);
+            // 숫자로 변환된 리스트의 총합 구하기
+            int sum = StringCalculator.sumNumber(numbers);
 
-        // 숫자로 변환된 리스트의 총합 구하기
-        int sum = StringCalculator.sumNumber(number);
-
-        // 결과 출력
-        System.out.println("결과 : " + sum);
+            // 결과 출력
+            System.out.println("결과 : " + sum);
+        } catch (IllegalArgumentException e) {
+            System.out.println("오류: " + e.getMessage());
+        }
     }
 }
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,35 +2,47 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringCalculator {
     // 문자열을 쉼표와 콜론을 기준으로 분리 후, 숫자로 변환하는 메서드
     public static List<Integer> splitAndConvert(String input) {
-        // null 또는 빈 문자열 처리; 리스트에 0을 담아 반환
+        // null 또는 빈 문자열이 들어오면 IllegalArgumentException 발생
         if (input == null || input.isEmpty()) {
-            return Arrays.asList(0);
+            throw new IllegalArgumentException("입력 문자열이 없습니다.");
         }
 
         // 기본 구분자
-        String separator = "[,;]";
+        String separator = "[,:]";
 
         // "//"로 문자열이 시작하는 경우
         if (input.startsWith("//")) {
-            int separatorEndIndex = input.indexOf("\n"); // 커스텀 구분자가 끝나는 인덱스 파악
+            int separatorEndIndex = input.indexOf("\\n"); // 커스텀 구분자가 끝나는 인덱스 파악
+
+            if (separatorEndIndex == -1) {
+                throw new IllegalArgumentException("잘못된 커스텀 구분자 입력 형식입니다.");
+            }
+
             separator = input.substring(2, separatorEndIndex); // 2번인덱스부터 \n 전까지의 인덱스를 커스텀 구분자로 설정
-            input = input.substring(separatorEndIndex + 1); // 커스텀 문자열을 설정하는 문자열을 제거 -> 실제 계산부만 남김
+            input = input.substring(separatorEndIndex + 2); // 커스텀 문자열을 설정하는 문자열을 제거 -> 실제 계산부만 남김
+
+            // 커스텀 구분자가 정규식의 특수문자일 경우, 이스케이프 처리
+            separator = Pattern.quote(separator);
         }
 
-        List<Integer> num = Arrays.stream(input.split(separator)) // input을 쉼표, 콜론으로 분리 후 스트림으로 변환
-                .map(s -> s.trim()) // stream에 담긴 문자열 요소 각각에 대해 공백 제거
-                .map(s -> Integer.parseInt(s)) // 각 문자열을 정수로 변환
-                .collect(Collectors.toList()); // 스트림으로 처리한 데이터들을 리스트로 변환
-        return num;
+        try {
+            return Arrays.stream(input.split(separator))
+                         .map(String::trim) // 공백 제거
+                         .map(Integer::parseInt) // 문자열을 정수로 변환
+                         .collect(Collectors.toList()); // 스트림으로 처리한 데이터들을 리스트로 변환
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("수식이 잘못되었습니다.");
+        }
     }
 
     public static int sumNumber(List<Integer> number) {
-        return number.stream().mapToInt(s -> s.intValue()) // Integer -> int 변환
-                .sum(); // 스트림 내의 정수 합산 후 반환
+        return number.stream().mapToInt(Integer::intValue) // Integer -> int 변환
+                              .sum(); // 스트림 내의 정수 합산 후 반환
     }
 }
