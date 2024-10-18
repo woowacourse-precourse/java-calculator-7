ab3fa71c2d7a0acf0d8422ece0e25ebe3fb9d637
joshinhyeok <shtree07@naver.com>
2024-10-18 21:47:56 +0900

    refactor: 테스트 코드 리팩토링

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,39 +7,39 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+
     private Calculator calculator;
 
     @BeforeEach
     void setUp() {
-        calculator = new Calculator(); //테스트 메서드마다 실행 전 초기화
+        calculator = new Calculator();
     }
 
     @Test
-    void 빈_문자열_테스트() {
-        assertEquals(new BigInteger("0"), calculator.sumFromString(""));
-    }
-
-    @Test
-    void 기본_구분자_더하기_테스트() {
+    void 기본_구분자_계산() {
         assertEquals(new BigInteger("13"), calculator.sumFromString("1:2,10"));
     }
 
     @Test
-    void 커스텀_구분자_더하기_테스트() {
+    void 커스텀_구분자_계산() {
         assertEquals(new BigInteger("13"), calculator.sumFromString("//;\\n1;2,10"));
         assertEquals(new BigInteger("13"), calculator.sumFromString("//;.!\\n1;.!2;.!10"));
-        assertEquals(new BigInteger("1"), calculator.sumFromString("//;\\n1"));
     }
 
     @Test
-    void 단일_문자_테스트() {
+    void 빈_문자열_계산() {
+        assertEquals(new BigInteger("0"), calculator.sumFromString(""));
+    }
+
+    @Test
+    void 단일_숫자_계산() {
         assertEquals(new BigInteger("1"), calculator.sumFromString("1"));
     }
 
     @Test
-    void 아주_큰_숫자_테스트() {
-        assertEquals(new BigInteger("10000000000000000000000000000000000000"),
-                calculator.sumFromString(
-                        "5000000000000000000000000000000000000:5000000000000000000000000000000000000"));
+    void 큰_숫자_계산() {
+        assertEquals(
+                new BigInteger("10000000000000000000"),
+                calculator.sumFromString("9999999999999999999,1"));
     }
 }
--- src/test/java/calculator/NumberValidatorTest.java
+++ src/test/java/calculator/NumberValidatorTest.java
@@ -2,42 +2,39 @@ package calculator;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class NumberValidatorTest {
 
+    private NumberValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new NumberValidator();
+    }
+
     @Test
-    void 음수_검증_테스트() {
-        NumberValidator validator = new NumberValidator();
+    void 음수_예외처리() {
         assertThrows(IllegalArgumentException.class, () ->
                 validator.positiveNumberCheck(new String[]{"-1", "2", "3"}));
     }
 
     @Test
-    void zero_검증_테스트() {
-        NumberValidator validator = new NumberValidator();
+    void zero_예외처리() {
         assertThrows(IllegalArgumentException.class, () ->
                 validator.positiveNumberCheck(new String[]{"0", "2", "3"}));
     }
 
     @Test
-    void 알파벳_검증_테스트() {
-        NumberValidator validator = new NumberValidator();
+    void 문자_예외처리() {
         assertThrows(IllegalArgumentException.class, () ->
-                validator.positiveNumberCheck(new String[]{"a", "2", "3"}));
+                validator.positiveNumberCheck(new String[]{"a", "ㅁ", "a3"}));
     }
 
     @Test
-    void 공백_검증_테스트() {
-        NumberValidator validator = new NumberValidator();
+    void 공백_예외처리() {
         assertThrows(IllegalArgumentException.class, () ->
-                validator.positiveNumberCheck(new String[]{"1 ", "2", "3"}));
-    }
-
-    @Test
-    void 숫자_문자_혼합_테스트() {
-        NumberValidator validator = new NumberValidator();
-        assertThrows(IllegalArgumentException.class, () ->
-                validator.positiveNumberCheck(new String[]{"a1", "b2", "c3"}));
+                validator.positiveNumberCheck(new String[]{" 1 ", "2", "3"}));
     }
 }
