c614aac22aa45670cf1f31130a94110919c00fbf
CheChe903 <cheche903@naver.com>
2024-10-18 18:52:49 +0900

    refactor: Calculator 클래스 생성 및 Application 의 run 함수 해당 클래스로 리팩터링
    이제, Application은 코드들의 시작을 담당합니다.
    Calculator 클래스가 문자열 계산의 시작을 알리고, 각 클래스들의 메서드를 이용해 계산을 완료합니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,46 +1,10 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
-    private final Delimiters delimiters = new Delimiters();
-    private final Converter converter = new Converter();
 
     public static void main(String[] args) {
-        Application application = new Application();
-        application.run();
+        Calculator calculator = new Calculator();
+        calculator.startCalculate();
     }
-
-    public void run() {
-        System.out.println("덧셈할 문자열을 입력해주세요.");
-
-        String inputString = Console.readLine();
-
-        converter.validateInputString(inputString);
-
-        String numberString = delimiters.extractNumberString(inputString);
-
-        StringBuilder delimiterExpression = new StringBuilder();
-        for (char delimiter : delimiters.getDelimiters()) {
-            delimiters.appendDelimiter(delimiterExpression, delimiter);
-        }
-
-        String delimiterStr = delimiterExpression.toString();
-
-        String[] numList = numberString.split(delimiterStr);
-
-        int sum = 0;
-
-        for (String token : numList) {
-
-            String trimToken = token.trim();
-
-            sum += converter.convertToValidInteger(trimToken);
-
-        }
-
-        System.out.println("결과 : " + sum);
-    }
-
-
+    
 }
