0146392a138b5168feff19d2f0ef9bd77a5708f7
Avocado <ujubongbong@gmail.com>
2024-10-18 18:11:21 +0900

    :sparkles: feat: 문자열 입력받는 함수

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,7 +9,13 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해주세요.");
     }
 
+    // 문자열 입력받는 함수
+    public static void input() {
+       String input = Console.readLine();
+    }
+
     public static void main(String[] args) {
         init();
+        input();
     }
 }
