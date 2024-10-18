61c194cb8a5d065165a90cf0b77e871792e7a012
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-18 23:33:56 +0900

    feat(NumberRange): 숫자 범위 최소, 최대 정의

---
+++ src/main/java/calculator/constant/NumberRange.java
@@ -0,0 +1,13 @@
+package calculator.constant;
+
+import java.math.BigInteger;
+
+public class NumberRange {
+
+    public static final BigInteger MIN = BigInteger.valueOf(1);
+    public static final BigInteger MAX = BigInteger.valueOf(Integer.MAX_VALUE);
+
+    private NumberRange() {
+
+    }
+}
