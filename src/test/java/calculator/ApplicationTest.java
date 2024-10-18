792e692ef8f2aabcb7d21c9a540a9536b233692a
Goohong Chung <9rrong@uoslife.team>
2024-10-19 02:08:09 +0900

    test(ApplicationTest): 다양한 입력 상황 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -14,13 +14,66 @@ class ApplicationTest extends NsTest {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
         });
+
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3;4");
+            assertThat(output()).contains("결과 : 10");
+        });
     }
 
     @Test
-    void 예외_테스트() {
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 빈_문자열_입력() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 숫자_0_포함하여_입력() {
+        assertSimpleTest(() -> {
+            run("0,2:3");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+    @Test
+    void 여러_자리_숫자_입력() {
+        assertSimpleTest(() -> {
+            run("10,20:30");
+            assertThat(output()).contains("결과 : 60");
+        });
+    }
+
+    @Test
+    void 문자_예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("1,a,2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 음수_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;;\\n1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
