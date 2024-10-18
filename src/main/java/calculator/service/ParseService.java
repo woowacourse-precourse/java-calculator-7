a6193a2dc179d853af122bbbbd0a4d8741085bf9
moongua404 <hogun.soongit@gmail.com>
2024-10-19 00:56:07 +0900

    refactor(MVC): MVC 구조로 변환
    - View
        - 입력 및 출력
    - Model
        - 표현식과 연산자
    - Service
        - 전처리 서비스
        - 연산 서비스
    - Controller
        - View와 Service 중계

---
+++ src/main/java/calculator/service/ParseService.java
@@ -0,0 +1,48 @@
+package calculator.service;
+
+import calculator.constant.Message;
+import calculator.model.Expression;
+import calculator.model.Operators;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class calculatorService {
+
+    static public void parseExpression(String sentence, Operators operators, Expression expression) {
+        sentence = sentence.replace(" ", "").replace("\n", "");
+        sentence = getSeparator(sentence, operators);
+        formulate(sentence, expression);
+    }
+
+    static private String getSeparator(String sentence, Operators separators) {
+        Matcher matcher = Pattern.compile("^//(.)\\\\n(.*)").matcher(sentence); // "//(separator)\n"...
+        if (matcher.find()) {
+            char separator = matcher.group(1).charAt(0);
+            if (Character.isDigit(separator)) {
+                throw new IllegalArgumentException(Message.EXCEPTION_MESSAGE + "숫자 형태의 구분자");
+            }
+            separators.addOperators(separator);
+            return matcher.group(2);
+        }
+        return sentence;
+    }
+
+    static private void formulate(String sentence, Expression expression) {
+        StringBuilder operand = new StringBuilder();
+
+        for (char ch : sentence.toCharArray()) {
+            if (Character.isDigit(ch)/* || ch == '-'*/) { // 일단 음수는 불가능
+                operand.append(ch);
+            } else {
+                if (!operand.isEmpty()) {
+                    expression.addToken(Integer.parseInt(operand.toString()));
+                }
+                operand.delete(0, operand.length());
+                expression.addToken(ch);
+            }
+        }
+        if (!operand.isEmpty()) {
+            expression.addToken(Integer.parseInt(operand.toString()));
+        }
+    }
+}
