5222001c929994b0bc24a9f34d1c1cef3e62b751
yybmion <yyb400@naver.com>
2024-10-18 21:32:29 +0900

    feat: 숫자 유효성 검사와 계산 그리고 예외 추가
    구분자로 분리된 문자에 대해서 숫자로 변환할 수 있는지 유효성 검사 후
    각각의 숫자를 합산하여 결과를 응답한다.
    
    추가한 예외는 다음과 같다.
    1. 숫자를 파싱할때 음수는 허용되지 않는다.
    2. 파싱 도중 예외가 발생하면 NumberFormatException 예외를 던진다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,31 +2,30 @@ package calculator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import java.util.regex.Pattern;
+import java.util.Arrays;
 
 public class Application {
     static String DELIMITER = ",|:";
 
     public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputString = readLine();
 
         String newInputString = splitCustomDelimiter(inputString);
-        splitDelimiter(newInputString);
+        String[] splitDelimiterString = splitDelimiter(newInputString);
+        calculateNumbers(splitDelimiterString);
     }
 
     /**
      * @param inputString: 입력값
      * @return 커스텀 구분자 분리 이후 새로운 inputString 값 만약 입력값이 "//"로 시작되고 그 뒤에 "\n"가 존재한다면 커스텀 구분자가 존재한다 (커스텀 구분자 내 공백 제외).
-     * @Exception
-     * 1. '//'로 시작하지만 이후 '\n'가 존재하지 않을때
-     * 2. 커스텀 구분자가 정확히 하나의 유니코드 문자가 아닐때
-     * 3. 커스텀 구분자가 비어있을 때
+     * @Exception 1. '//'로 시작하지만 이후 '\n'가 존재하지 않을때 2. 커스텀 구분자가 정확히 하나의 유니코드 문자가 아닐때 3. 커스텀 구분자가 비어있을 때
      */
     public static String splitCustomDelimiter(String inputString) {
         if (inputString.startsWith("//")) {
-            int newLineIndex = inputString.indexOf("\n");
+            int newLineIndex = inputString.indexOf("\\n");
             if (newLineIndex == -1) {
-                throw new IllegalArgumentException("커스텀 구분자 지정 후 '\\\\n'이 없습니다.");
+                throw new IllegalArgumentException("커스텀 구분자 지정 후 " + "\\n" + "이 없습니다.");
             }
 
             String customDelimiter = inputString.substring(2, newLineIndex);
@@ -39,23 +38,43 @@ public class Application {
                 throw new IllegalArgumentException("커스텀 구분자가 비어있습니다.");
             }
 
-            DELIMITER += "|" + Pattern.quote(customDelimiter);
-            inputString = inputString.substring(newLineIndex + 1);
+            DELIMITER += "|" + customDelimiter;
+            inputString = inputString.substring(newLineIndex + 2);
         }
 
         return inputString;
     }
 
     /**
-     * @param newInputString: 커스텀 구분자 분리 후 새로운 문자열
-     * 새로운 문자열 내 계산해야할 숫자만 분리
+     * @param newInputString: 커스텀 구분자 분리 후 새로운 문자열 새로운 문자열 내 계산해야할 숫자만 분리
      */
-    private static void splitDelimiter(String newInputString) {
-        String[] numbers = newInputString.split(DELIMITER);
-
-        checkEachNumberAndCalculate(numbers);
+    private static String[] splitDelimiter(String newInputString) {
+        return newInputString.split(DELIMITER);
     }
 
-    private static void checkEachNumberAndCalculate(String[] numbers) {
+    /**
+     * @param numbers : 계산해야할 각각의 숫자
+     * @return 계산 결과
+     * @Explain 각각의 숫자에 대해 유효성 검사후 하나씩 더해준다.
+     */
+    private static int calculateNumbers(String[] numbers) {
+        return Arrays.stream(numbers).filter(s -> !s.isEmpty()).mapToInt(Application::parseNumber).sum();
+    }
+
+
+    /**
+     * @param number
+     * @return 유효성 검사를 마친 숫자
+     */
+    private static int parseNumber(String number) {
+        try {
+            int parsingNumber = Integer.parseInt(number.trim());
+            if (parsingNumber < 0) {
+                throw new IllegalArgumentException("음수는 허용되지 않습니다: " + parsingNumber);
+            }
+            return parsingNumber;
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("잘못된 숫자 형식: " + exception);
+        }
     }
 }
