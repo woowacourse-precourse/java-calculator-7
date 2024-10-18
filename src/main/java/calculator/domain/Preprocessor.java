68c455d3e443b4f368b1506ebd6b030f6c4df4ca
rookedsysc <rookedsysc36@gmail.com>
2024-10-18 23:55:20 +0900

    update: regex matcher 활용해서 delimiter, input 구분하도록 변경

--- src/main/java/calculator/domain/Preprocessor.java
+++ src/main/java/calculator/domain/Preprocessor.java
@@ -4,10 +4,16 @@ import calculator.domain.model.PreprocessedInput;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Preprocessor {
+    /**
+     * (.) : 임이의 한 문자 캡쳐
+     * (.*) : .: 임의의 *: 여러 문자
+     */
+    private static final String CUSTOM_REGEX_PATTERN = "//(.)\\\\n(.*)";
     private static final String DELIMITER_PREFIX = "//";
-    private static final String DELIMITER_SUFFIX = "\n";
     private static final ArrayList<Character> DELIMITERS = new ArrayList<>(Arrays.asList(',', ':'));
     // TODO: 여기가 final일 경우 코드가 좀 더 더러워 지는데, 여기가 final이 아니면 어떤 문제가 생길지 생각해보기
     private String input;
@@ -18,24 +24,20 @@ public class Preprocessor {
 
     public PreprocessedInput preprocess() {
         if (input.startsWith(DELIMITER_PREFIX)) {
-            addCustomDelimiter(input);
-            input = input.split(DELIMITER_SUFFIX)[1];
+            extractDelimiterWithMatcher();
         }
 
         return new PreprocessedInput(input, DELIMITERS);
     }
 
-    private void addCustomDelimiter(String input) {
-        int delimiterEnd = getDelimiterEndOrThrow(input);
-        String customDelimiter = input.substring(DELIMITER_PREFIX.length(), delimiterEnd);
-        DELIMITERS.add(customDelimiter.charAt(0));
-    }
+    private void extractDelimiterWithMatcher() {
+        Matcher matcher = Pattern.compile(CUSTOM_REGEX_PATTERN).matcher(input);
 
-    private int getDelimiterEndOrThrow(String input) {
-        int delimiterEnd = input.indexOf(DELIMITER_SUFFIX);
-        if (delimiterEnd == -1) { // suffix가 없으면 잘못된 입력
+        if (matcher.find()) {
+            DELIMITERS.add(matcher.group(1).charAt(0));
+            input = matcher.group(2);
+        } else {
             throw new IllegalArgumentException();
         }
-        return delimiterEnd;
     }
 }
