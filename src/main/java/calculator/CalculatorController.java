3810a3f6ea7ca2a0047a07d54fca2e392bef3564
Hyeongyu Son <shonenq00@gmail.com>
2024-10-18 22:54:50 +0900

    feat : Add start ment output 애플리케이션 실행 시 시작멘트 출력 기능구현

---
+++ src/main/java/calculator/CalculatorController.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import calculator.view.OutputView;
+
+public class CalculatorController {
+    private OutputView outputView;
+
+    public CalculatorController() {
+        this.outputView = new OutputView();
+    }
+
+    public void run(){
+        outputView.printStartMessage();
+    }
+}
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+    private final String START_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public void printStartMessage(){
+        System.out.println(START_MESSAGE);
+    }
+}
