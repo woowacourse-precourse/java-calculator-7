d32a4f91c015b14f74a7486f7831940b8dcb8eda
TalkingPotato90 <star901210@hanmail.net>
2024-10-18 17:48:09 +0900

    fix: 입력 데이터 테스트에서 인스턴스화 오류 수정 및 테스트 파라미터 수정

--- src/test/java/calculator/model/InputDataTest.java
+++ src/test/java/calculator/model/InputDataTest.java
@@ -10,43 +10,41 @@ import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("사용자 입력 객체 테스트")
 class InputDataTest {
-    private InputData inputData;
-
-    @BeforeEach
-    void setUp() {
-        inputData = new InputData("test");
-    }
 
     @ParameterizedTest
-    @ValueSource(strings = {"123!","0721가","2038a"})
+    @ValueSource(strings = {"//;\\n1;2;3;!","0,7,2,1,가","2,0,3,8,a"})
     @DisplayName("마지막 입력 값이 숫자가 아니면 예외가 발생한다")
     void checkLastInputIsNotDigit(String input){
+        InputData inputData = new InputData(input);
         assertThatThrownBy(()->inputData.validate(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[F0001]");
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"123!1","0721가2","2038a3"})
+    @ValueSource(strings = {"//;\\n1;2;3","0,7,2,1","2,0,3,8"})
     @DisplayName("마지막 입력 값이 숫자이면 예외가 발생하지 않는다")
     void checkLastInputIsDigit(String input){
+        InputData inputData = new InputData(input);
         assertThatCode(()->inputData.validate(input))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {",123","가0721","a2038"})
+    @ValueSource(strings = {",1,2,3","가,0,7,2,1","a,20,38"})
     @DisplayName("커스텀 구분자가 없고 입력 값의 처음이 숫자가 아니면 예외가 발생한다")
     void checkInputStartsWithoutCustomDelimiterIsInvalid(String input){
+        InputData inputData = new InputData(input);
         assertThatThrownBy(()->inputData.validate(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[F0002]");
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"1/,123","1가0721","2/!/a2038"})
+    @ValueSource(strings = {"1,1,23","1,7,21","2,20,38"})
     @DisplayName("커스텀 구분자가 없고 입력 값의 처음이 숫자라면 예외가 발생하지 않는다")
     void checkInputStartsWithoutCustomDelimiterIsValid(String input){
+        InputData inputData = new InputData(input);
         assertThatCode(()->inputData.validate(input))
                 .doesNotThrowAnyException();
     }
@@ -54,7 +52,7 @@ class InputDataTest {
     @Test
     @DisplayName("커스텀 구분자를 입력했을 때 True 값을 반환한다")
     void isDelimiterTrueWhenContainPattern(){
-        InputData inputData = new InputData("//;.!?\\n1;2.3!4?5");
+        InputData inputData = new InputData("//;.!\\n1;2.3!4");
         assertThat(inputData.isDelimiter()).isTrue();
     }
 
@@ -68,15 +66,15 @@ class InputDataTest {
     @Test
     @DisplayName("커스텀 구분자를 입력했을 때 구분자를 잘 분리한다")
     void splitDelimiterExactly(){
-        InputData inputData = new InputData("//;.!?\\n1;2.3!4?5");
-        assertThat(inputData.convertDelimiterPart()).isEqualTo(";.!?");
+        InputData inputData = new InputData("//;.!\\n1;2.3!4");
+        assertThat(inputData.convertDelimiterPart()).isEqualTo(";.!");
     }
 
     @Test
     @DisplayName("커스텀 구분자를 입력했을 때 연산부분을 잘 분리한다")
     void splitCalculatorExactly(){
-        InputData inputData = new InputData("//;.!?\\n1;2.3!4?5");
-        assertThat(inputData.convertCalculatorPart()).isEqualTo("1;2.3!4?5");
+        InputData inputData = new InputData("//;.!\\n1;2.3!4");
+        assertThat(inputData.convertCalculatorPart()).isEqualTo("1;2.3!4");
     }
 
 }
