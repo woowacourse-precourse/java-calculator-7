c1865886933c645845c705cbb8d5026ea644bf72
MinJae00 <exceedmin@gmail.com>
2024-10-18 16:43:18 +0900

    feat: 숫자 array에서 sum 값을 계산하는 Calculate 메소드 구현

--- src/main/java/calculator/Calculate.java
+++ src/main/java/calculator/Calculate.java
@@ -1,2 +1,13 @@
-package calculator;public class Calculate {
+package calculator;
+
+import java.util.ArrayList;
+
+public class Calculate {
+    int sum = 0;
+    int Makesum(ArrayList<Integer> numlist){
+        for(int i = 0; i < numlist.size(); i++){
+            sum += numlist.get(i);
+        }
+        return sum;
+    }
 }
