6ab1455fd5d8c3ea5ea1ca5138cd045a7694be15
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-10-18 15:47:49 +0900

    feat(Controller): develop Parsing logic Controller classes
    - CalculatorController: 파싱 함수 구현 및 로직 변경

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,45 +1,50 @@
 package controller;
 
+import model.Calculator;
 import model.Custom;
+import utils.AddEntryValidator;
 import view.InputView;
 import view.OutputView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class CalculatorController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final Calculator calculator;
+    private final Custom custom;
+    private final AddEntryValidator addEntryValidator = new AddEntryValidator();
 
     public CalculatorController() {
-        Custom custom = new Custom();
-        this.inputView = new InputView(custom);
+        this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.custom = new Custom();
+        this.calculator = new Calculator();
     }
 
     public void run() {
         outputView.printReadNumberMessage();
         inputView.readInputString();
-        String parsingInputString = inputView.getInputString();
-        parsingCalculatorInput(parsingInputString);
-        String result = calculatorAddFeature();
-        outputView.printCalculatorResultMessage(result);
+
+        custom.addCustomSet(inputView.getCustom());
+        parsingCalculatorInput(inputView.getInputString());
+        outputView.printCalculatorResultMessage(calculator.getResult());
     }
 
     private void parsingCalculatorInput(String parsingInputString) {
-        /**
-         *커스텀을 기준으로 split을 진행하고 split의 값이 숫자가 아닌 경우 예외처리를 진행해주고
-         *숫자인 경우 Array 동적 set에 담아주어야함"
-         *문자열 숫자의 길이를 생각해야함.
-         * 매우 큰 문자열 숫자의 경우도 확인할 수 있어야 함
-         */
-    }
 
-    private String calculatorAddFeature() {
-        /**
-         * 계산을 진행
-         * 매우 큰 수의 경우에도 더하기가 가능해야 하기 때문에
-         * String 더하기를 구현
-         */
-        return null;
-    }
+        String customDelimiters = String.join("", custom.getCustomSet());
+        StringTokenizer stringTokenizer = new StringTokenizer(parsingInputString, customDelimiters);
 
+        while (stringTokenizer.hasMoreTokens()) {
+            String addEntry = stringTokenizer.nextToken();
+            addEntryValidator.validateNumericEntry(addEntry);
+            calculator.setAddEntry(addEntry);
+        }
+
+    }
 
 }
