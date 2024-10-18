8fa554b098c14e7a91cda725dc956e144ef5f7c5
jimmy0524 <10jmin04@naver.com>
2024-10-19 03:44:22 +0900

    refactor: 커스텀 구분자 기능 리팩토링

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -8,10 +8,15 @@ public class CalculatorService {
 
         if (input.isEmpty()) {
             calculator.setNumbers(new int[]{0});
-        } else if(input.equals("//;\n1;2;3")) {
-            calculator.setNumbers(new int[]{1, 2, 3});
-        }else {
-            String[] splitInput = input.split("[,|:]");
+        } else {
+            String delimiters = "[,|:]";
+            if (input.startsWith("//")) {
+                String[] split = input.split("\n");
+                String customDelimiter = split[0].substring(2);
+                delimiters = "[" + delimiters.substring(1, 4) + "|" + customDelimiter + "]";
+                input = split[1];
+            }
+            String[] splitInput = input.split(delimiters);
             int[] splitNum = new int[splitInput.length];
 
             for (int i = 0; i < splitInput.length; i++) {
