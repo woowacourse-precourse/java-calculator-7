2ddd14592fcd6d16f5c6cae123abb881b89ad6e9
goochul <gooch123@naver.com>
2024-10-18 22:53:14 +0900

    feat(Application): 외부 인자 사용 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        String str;
+
+        if (args.length > 0) { // 주어지는 인자가 있을 경우
+            str = args[0];
+        } else { // 주어지는 인자가 없을 경우 직접 입력
+            str = Console.readLine();
+        }
+
+        Calculator calculator = new Calculator(str);
+        int calc = calculator.calc();
+
+        System.out.println("결과 : " + calc);
+
     }
 }
+
