b4977fec68ab17eabcedefcde53a1140e77670d7
thdudgus <gusthdud527@cau.ac.kr>
2024-10-19 04:54:56 +0900

    style : Console.readLine()으로 변경. scanner.nextLine() 대신 Console.readLine()으로 입력을 변경하였다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,6 @@ public class Application {
     public static void main(String[] args) {
         Input input = new Input();  // Input 객체 생성
         String userInput = input.getUserInput();  // 사용자 입력 받기
-        input.closeScanner();  // 스캐너 닫기
 
         //Select select = new Select(userInput);
     }
