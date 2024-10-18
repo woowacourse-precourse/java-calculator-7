5dc1c00fbe2ada342b4811e042cad9b99d9da388
giwoong <dev.choi0409@gmail.com>
2024-10-18 23:02:36 +0900

    refactor: 사용자가 입력한 값이 커스텀 구분자가 유효하지 않은 경우 예외 처리
    README.md 구현에 맞춰 최신화

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -49,6 +49,7 @@ public class Calculator {
             validateCustomDelimiterPresence(startIdx, endIdx);
 
             String customDelimiter = expression.substring(startIdx + 2, endIdx);
+            validateCustomDelimiterValidity(customDelimiter);
 
             String beforeCustomDelimiter = expression.substring(0, startIdx);
             String afterCustomDelimiter = expression.substring(endIdx + 2);
@@ -62,6 +63,18 @@ public class Calculator {
         if (startIdx == -1 || endIdx == -1) {
             throw new IllegalArgumentException("커스텀 구분자를 지정하지 못했습니다.");
         }
+    }
+
+    private void validateCustomDelimiterValidity(String customDelimiter) {
+        if (isInvalidCustomDelimiter(customDelimiter)) {
+            throw new IllegalArgumentException("커스텀 구분자가 유효하지 않습니다.");
+        }
+    }
+
+    private boolean isInvalidCustomDelimiter(String customDelimiter) {
+        return customDelimiter.isEmpty() ||
+                customDelimiter.equals(",") ||
+                customDelimiter.equals(":");
     }
 
 }
