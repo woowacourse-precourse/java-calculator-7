075a5e6e859a5ef65f2046d60a2874a8ceac3aaa
김도현 <hot99009@gmail.com>
2024-10-19 01:54:11 +0900

    feat(app): 사용자 입력 및 출력 처리를 위한 Application 클래스 추가
    프로그램의 진입점 역할을 하는 Application 클래스를 추가했습니다.
    사용자 입력을 받아 StringCalculator로 처리하고 결과를 콘솔에 출력하게 할 예정입니다.
    Issue: #1

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import java.util.Scanner;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 }
