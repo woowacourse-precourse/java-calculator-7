7b6f8e8df137800b56c3b41beb249f10b857728a
joonwan <joonwan1024@naver.com>
2024-10-19 03:04:49 +0900

    test: calculator test code 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,23 +9,120 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
-    void 커스텀_구분자_사용() {
+    void 기본_포멧_공백_테스트() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 기본_포멧_정상_입력_숫자만_존재() {
+        assertSimpleTest(() -> {
+            run("123");
+            assertThat(output()).contains("결과 : 123");
+        });
+    }
+
+    @Test
+    void 기본_포멧_정상_입력_숫자_커스텀_구분자_존재() {
+        assertSimpleTest(() -> {
+            run("1,2:3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 기본_포멧_정상_입력_구분자_먼저() {
+        assertSimpleTest(() -> {
+            run(",12,34");
+            assertThat(output()).contains("결과 : 46");
+        });
+    }
+
+    @Test
+    void 커스텀_포멧_공백_테스트() {
+        assertSimpleTest(() -> {
+            run("//;\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_포멧_정상_입력_숫자만_존재() {
+        assertSimpleTest(() -> {
+            run("//[\\n1");
             assertThat(output()).contains("결과 : 1");
         });
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_포멧_정상_입력_숫자_커스텀_구분자_존재() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;34,;");
+            assertThat(output()).contains("결과 : 37");
+        });
+    }
+
+    @Test
+    void 커스텀_포멧_정상_입력_숫자_커스텀_구분자_먼저() {
+        assertSimpleTest(() -> {
+            run("//;\\n;;;1;2;34,;");
+            assertThat(output()).contains("결과 : 37");
+        });
+    }
+
+
+    @Test
+    void 기본_포멧_음수_예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_포멧_구분자_숫자_아닌값_포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2;,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_포멧_구분자_오버플로우_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("2,3,1,2147483648"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_포멧_음수_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_포멧_구분자_숫자_아닌값_포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n1,2;[3]"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_포멧_구분자_오버플로우_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n2;3;1;2147483648"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
     @Override
-    public void runMain() {
-        Application.main(new String[]{});
+    protected void runMain() {
+        Calculator.run();
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -10,17 +10,114 @@ import org.junit.jupiter.api.Test;
 class CalculatorTest extends NsTest {
 
     @Test
-    void 정상_입력_실행() {
+    void 기본_포멧_공백_테스트() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 기본_포멧_정상_입력_숫자만_존재() {
+        assertSimpleTest(() -> {
+            run("123");
+            assertThat(output()).contains("결과 : 123");
+        });
+    }
+
+    @Test
+    void 기본_포멧_정상_입력_숫자_커스텀_구분자_존재() {
+        assertSimpleTest(() -> {
+            run("1,2:3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 기본_포멧_정상_입력_구분자_먼저() {
+        assertSimpleTest(() -> {
+            run(",12,34");
+            assertThat(output()).contains("결과 : 46");
+        });
+    }
+
+    @Test
+    void 커스텀_포멧_공백_테스트() {
+        assertSimpleTest(() -> {
+            run("//;\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_포멧_정상_입력_숫자만_존재() {
+        assertSimpleTest(() -> {
+            run("//[\\n1");
             assertThat(output()).contains("결과 : 1");
         });
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_포멧_정상_입력_숫자_커스텀_구분자_존재() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;34,;");
+            assertThat(output()).contains("결과 : 37");
+        });
+    }
+
+    @Test
+    void 커스텀_포멧_정상_입력_숫자_커스텀_구분자_먼저() {
+        assertSimpleTest(() -> {
+            run("//;\\n;;;1;2;34,;");
+            assertThat(output()).contains("결과 : 37");
+        });
+    }
+
+
+    @Test
+    void 기본_포멧_음수_예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_포멧_구분자_숫자_아닌값_포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2;,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_포멧_구분자_오버플로우_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("2,3,1,2147483648"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_포멧_음수_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_포멧_구분자_숫자_아닌값_포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n1,2;[3]"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_포멧_구분자_오버플로우_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n2;3;1;2147483648"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
