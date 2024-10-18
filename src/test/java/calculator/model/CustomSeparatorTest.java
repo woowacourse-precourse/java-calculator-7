27f59d5f357b709fd934029e5adf8e72433808f8
ljhee92 <weilainib7@gmail.com>
2024-10-18 21:01:21 +0900

    test: 커스텀 구분자 정상 작동 확인 테스트 추가

--- src/test/java/calculator/model/CustomSeparatorTest.java
+++ src/test/java/calculator/model/CustomSeparatorTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,6 +20,22 @@ public class CustomSeparatorTest {
     void containsCustomSeparator() {
         assertThat(new CalculatorController().containsCustomSeparator("//!@\\n1!2@3")).isTrue();
     } // containsCustomSeparator
+
+    @ParameterizedTest(name = "{index} : {2}")
+    @DisplayName("커스텀 구분자 정상 작동 확인")
+    @MethodSource("generateSuccessData")
+    void testCustomSeparator(String input, List<Integer> output, String message) {
+        assertThat(new CalculatorController().getNumbers(input)).isEqualTo(output);
+    } // testCustomSeparator
+
+    static Stream<Arguments> generateSuccessData() {
+        return Stream.of(
+                Arguments.of("//!!\\n1!23", List.of(1, 23), "중복 지정할 때"),
+                Arguments.of("//:\\n1:2:3", List.of(1, 2, 3), "기본 구분자를 지정했을 때"),
+                Arguments.of("//-\\n1--2", List.of(1, 0, 2), "커스텀 구분자를 연속 입력했을 때"),
+                Arguments.of("//;\\n;1;2;", List.of(0, 1, 2), "커스텀 구분자를 계산 부분 앞 뒤에 입력했을 때")
+        );
+    } // generateSuccessData
 
     @ParameterizedTest(name = "{index} : {1}")
     @DisplayName("커스텀 구분자 예외 확인")
