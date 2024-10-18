8d63b5c258bc2525b94b688836432df9e8070c3b
김연진 <love_homebody@naver.com>
2024-10-18 17:25:38 +0900

    feat(InputParser): "//"와 "\n"를 기준으로 문자열을 분리

--- src/main/java/calculator/view/InputParser.java
+++ src/main/java/calculator/view/InputParser.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 public class InputParser {
 
+    private static final int START_INDEX = 0;
+    private static final String CUSTOM_START = "//";
+    private static final String CUSTOM_END = "\\n";
+
     public List<Integer> parseInput(Delimeters delimiters, String input) {
         String delimiterRegex = String.join("|", delimiters.getElements());
         String[] tokens = input.split(delimiterRegex);
@@ -15,5 +19,10 @@ public class InputParser {
             numbers.add(Integer.parseInt(token.trim()));
         }
         return numbers;
+    }
+
+    public String parseCustomDelimiter(String input) {
+        return input.substring(START_INDEX, input.indexOf(CUSTOM_END))
+                .replace(CUSTOM_START, "");
     }
 }
