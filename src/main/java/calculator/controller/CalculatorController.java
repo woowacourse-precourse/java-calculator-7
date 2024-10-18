dd3182fc4b9bbb6e7d59063442fd03843ceb7634
skhnpark <skhnpark125@gmail.com>
2024-10-19 06:21:02 +0900

    feat: 구분자 관련 기능 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,12 +1,15 @@
 package calculator.controller;
 
+import calculator.model.Delimiter;
 import calculator.model.UserInput;
 import calculator.view.InputView;
 
 public class CalculatorController {
+    private final Delimiter delimiter;
     private final UserInput userInput;
 
     public CalculatorController() {
+        this.delimiter = new Delimiter();
         this.userInput = new UserInput();
     }
 
