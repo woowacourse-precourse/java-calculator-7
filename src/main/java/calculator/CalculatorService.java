fe668b0feb8eddfc1928dfc0743341673ea838bd
byeongdoohan <quden04@gmail.com>
2024-10-19 07:04:07 +0900

    feat: 에러 처리 관련 패턴 추가 1. 정상 패턴 - 커스텀 구분자 사이에 0을 추가해 구분자 여러개 추가 가능 2. 커스텀 구분자에 숫자가 들어 갔을 때 패턴 3. 처음 혹은 마지막에 0이 포함됐을 때 패턴 4. 커스텀 구분자에 값을 입력하지 않았을 때 패턴

--- src/main/java/calculator/CalculatorService.java
+++ src/main/java/calculator/CalculatorService.java
@@ -8,12 +8,16 @@ import java.util.regex.Pattern;
 
 public class CalculatorService {
     private final String startString = "덧셈할 문자열을 입력해 주세요.";
-    private final Pattern pattern = Pattern.compile("//(\\D)+\\\\n");
+    private final Pattern pattern = Pattern.compile("//\\D+(0?\\D)*\\\\n");
+    private final Pattern wrongPattern = Pattern.compile("^//[1-9]+.*|.*[1-9]+.*|[1-9]+\\\\n$");
+    private final Pattern zeroPattern = Pattern.compile("^//0+.*|.*0+\\\\n$");
+    private final Pattern nothingPattern = Pattern.compile("//\\\\n");
     private String inputString;
     private String transedString;
     private final String endString = "결과 : ";
     private BigInteger result;
     private String[] numList;
+    private String[] sepList;
     public void printStartString(){
         System.out.println(startString);
     }
@@ -38,8 +42,18 @@ public class CalculatorService {
     public String transCustomSeperator(Matcher matcher){
         String customMatcher = matcher.group();
         String customSeperator = customMatcher.substring(2, customMatcher.length()-2);
+        if(customSeperator.contains("0")) {
+            sepList = customSeperator.split("0");
+        }else {
+            sepList = new String[]{customSeperator};
+        }
         String expression = inputString.substring(customMatcher.length(), inputString.length());
-        transedString = expression.replaceAll(customSeperator,",").replaceAll(":", ",");
+        transedString = expression.replaceAll(":",",");
+        for(String i : sepList){
+            if(!i.equals("")) {
+                transedString = transedString.replaceAll(i, ",");
+            }
+        }
         return transedString;
     }
 
@@ -57,7 +71,9 @@ public class CalculatorService {
         for (String i : numList) {
             BigInteger n= new BigInteger(i);
             try{
-                if(n.compareTo(new BigInteger("0"))<0) throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+                if(n.compareTo(new BigInteger("0"))<0){
+                    throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+                }
                 result = result.add(n);
             }catch (NumberFormatException e){
                 throw new IllegalArgumentException("등록되지 않은 구분자가 포함되어 있습니다.");
@@ -78,8 +94,17 @@ public class CalculatorService {
 
     public void calculate(String inputString){
         Matcher matcher = pattern.matcher(inputString);
+        Matcher wrongMatcher = wrongPattern.matcher(inputString);
+        Matcher nothingMatcher = nothingPattern.matcher(inputString);
+        Matcher zeroMatcher = zeroPattern.matcher(inputString);
         if(matcher.find()){
             transedString = transCustomSeperator(matcher);
+        }else if(nothingMatcher.find()){
+            transedString = transCustomSeperator(nothingMatcher);
+        }else if(zeroMatcher.find()){
+            throw new IllegalArgumentException("0은 등록하고자 하는 구분자 사이에만 사용할 수 있습니다.");
+        }else if(wrongMatcher.find()){
+            throw new IllegalArgumentException("커스텀 구분자에 숫자를 입력할 수 없습니다.");
         }else{
             transedString = transBasicSeperator();
         }
