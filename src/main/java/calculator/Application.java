f83bc3d0bc2c47261d05b62cc09ecb4220722ff0
seonwoonam <potatomoney716@gmail.com>
2024-10-18 20:06:39 +0900

    fix(Exception) : main에서 예외처리 try catch문 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,12 +4,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Calculator calculator = new Calculator();
-
-        try {
-            calculator.run();
-        } catch (IllegalArgumentException e) {
-            System.out.println("오류 발생 : " + e.getMessage());
-        }
-
+        calculator.run();
     }
 }
