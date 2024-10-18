0c243216c80130e95935ec2be83c49ac3e3eaf0b
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-19 01:36:31 +0900

    feat: 기본 커스텀 구분자 패턴 매칭 처리기 구현
    - DefaultCustomDelimiterPatternMatcher 클래스를 구현하여 기본적인 커스텀 구분자 패턴 매칭을 처리.
    - 입력 값에서 구분자와 trim 된 그룹을 추출하는 메서드를 제공.
    - 정의된 커스텀 구분자 패턴에 대해 입력 문자열을 매칭하고 관련 그룹을 추출하는 메서드를 구현.

---
+++ src/main/java/calculator/delimiter/pattern/DefaultCustomDelimiterPatternMatcher.java
@@ -0,0 +1,29 @@
+package calculator.delimiter.pattern;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DefaultCustomDelimiterPatternMatcher implements CustomDelimiterPatternMatcher {
+
+    private final String DELIMITER_PREFIX = "//";
+    private final String DELIMITER_SUFFIX = "\\\\n";
+    private final Pattern DELIMITER_PATTERN = Pattern.compile(
+            "^" + DELIMITER_PREFIX + "(.*)" + DELIMITER_SUFFIX + "(.*)"
+    );
+
+    @Override
+    public Matcher match(String value) {
+        return DELIMITER_PATTERN.matcher(value);
+    }
+
+    @Override
+    public Optional<String> extractDelimiterGroup(String value) {
+        return matchAndFind(value).map(matcher -> matcher.group(1));
+    }
+
+    @Override
+    public Optional<String> extractTrimmedGroup(String value) {
+        return matchAndFind(value).map(matcher -> matcher.group(2));
+    }
+}
