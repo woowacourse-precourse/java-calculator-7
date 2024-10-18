6a387bbc0616c3ea536b532689d2a2572dc25916
김연진 <love_homebody@naver.com>
2024-10-18 20:58:44 +0900

    feat(Delimiters): 커스텀 구분자가 숫자일 경우 예외가 발생

--- src/main/java/calculator/constant/ExceptionMessage.java
+++ src/main/java/calculator/constant/ExceptionMessage.java
@@ -3,7 +3,8 @@ package calculator.constant;
 public enum ExceptionMessage {
 
     NOT_INTEGER("숫자를 입력해주세요."),
-    NOT_POSITIVE("양수만 입력할 수 있습니다.");
+    NOT_POSITIVE("양수만 입력할 수 있습니다."),
+    NUMBER_WITH_SEPARATOR("숫자는 구분자로 사용할 수 없습니다.");
 
     private final String message;
 
--- src/main/java/calculator/domain/Delimiters.java
+++ src/main/java/calculator/domain/Delimiters.java
@@ -1,7 +1,10 @@
 package calculator.domain;
 
+import calculator.constant.ExceptionMessage;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Delimiters {
 
@@ -18,6 +21,15 @@ public class Delimiters {
     }
 
     public void addDelimiter(String delimiter) {
+        validateDelimiter(delimiter);
         elements.add(delimiter);
+    }
+
+    private void validateDelimiter(String delimiter) {
+        String NUMBER_REGEXP = "^\\d*$";
+        if (Pattern.matches(NUMBER_REGEXP, delimiter)) {
+            ExceptionMessage exceptionMessage = ExceptionMessage.NUMBER_WITH_SEPARATOR;
+            throw new IllegalArgumentException(exceptionMessage.getMessage());
+        }
     }
 }
