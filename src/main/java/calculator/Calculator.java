9365f07e20513976b5fe280187699e69d78b2677
Huiyeongkim <huiyeong9619@naver.com>
2024-10-18 14:06:13 +0900

    feat: 커스텀 구분자 지정 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,23 +6,32 @@ import java.util.List;
 
 public class Calculator {
 
-    private static final String DEFAULT_DELIMITERS = ",|:";
-
+    private static String DEFAULT_DELIMITERS = ",|:";
 
     public List<Integer> splitString(String input) {
         if (input == null || input.isEmpty()) {
             return List.of(0);
         }
 
-        String delimiter = DEFAULT_DELIMITERS;
-
-        String[] tokens = input.split(delimiter);
+        String customNumber = customDelimiter(input);
+        String[] tokens = customNumber.split(DEFAULT_DELIMITERS);
         List<Integer> numbers = new ArrayList<>();
         for (String token : tokens) {
             numbers.add(Integer.parseInt(token));
         }
 
         return numbers;
+    }
+
+    public String customDelimiter(String input) {
+        if (input.startsWith("//")) {
+            int endIndex = input.indexOf("\\"+"n");
+
+            String customDelimiter = input.substring(2, endIndex);
+            DEFAULT_DELIMITERS = DEFAULT_DELIMITERS + "|" +customDelimiter;
+            return input.substring(endIndex + 2);
+        }
+        return input;
     }
 
     public Integer sumNumbers(List<Integer> numbers) {
