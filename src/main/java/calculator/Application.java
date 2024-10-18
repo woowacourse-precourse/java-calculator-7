7ba22e17dae1a340c275da12a1540ded84c2727c
차왕준 <ckdhdkwns@gmail.com>
2024-10-19 02:52:23 +0900

    feat(StringParser): 사용자 입력 처리 구현
    - 커스텀 구분자 분리
    - 입력값 검증

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Set<Character> defaultDelimiters = new HashSet<>(Arrays.asList(',', ':'));
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = readLine();
+        StringParser stringParser = new StringParser(input, defaultDelimiters);
+
+        String processedInput = stringParser.getProcessedInput();
+        Set<Character> delimiters = stringParser.getDelimiters();
+
+        System.out.println(processedInput);
+        System.out.println(delimiters);
     }
 }
