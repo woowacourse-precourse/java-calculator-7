81e32dd53e305a57a783f2ac5727748d60550e05
YoungGaLee <rkduddl1717@gmail.com>
2024-10-19 06:04:39 +0900

    feat: 기본 구분자로 분리, 덧셈
    쉼표 (,) 또는 콜론 (:)을 기본 구분자로 사용하여 입력된 문자열을 분리한 후, 각 숫자의 합을 반환한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,11 +6,17 @@ import static camp.nextstep.edu.missionutils.Console.close;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO:  문자열 입력받기
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String inputString = readLine();
 
         // TODO:  기본 구분자로 분리, 덧셈
+        String[] separatedInput = inputString.split(",|:");
+        int sumValue = 0;
+        for(int i=0; i<separatedInput.length;i++){
+            sumValue += Integer.parseInt(separatedInput[i]);
+        }
+        System.out.println(String.format("결과 : %d", sumValue));
+
         // TODO:  커스텀 구분자
         // TODO:  예외처리
     }
