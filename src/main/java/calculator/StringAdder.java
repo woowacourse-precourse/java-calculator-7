ddbb35145e1482166cb7358884ef453131c24f09
sejinezy <psj8880@naver.com>
2024-10-18 18:31:43 +0900

    feat(sumNumbers): 숫자 합산 기능 메서드 구현
    문자열 배열에 있는 숫자들을 정수로 변환한 후, 모두 더해 합계를 반환하는 기능 구현

--- src/main/java/calculator/StringAdder.java
+++ src/main/java/calculator/StringAdder.java
@@ -35,5 +35,13 @@ public class StringAdder {
         return input.split(Pattern.quote(delimiter));
     }
 
+    private int sumNumbers(String[] numbers) {
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
+    }
+
 
 }
