6a27ac910c84c3f0ed2e2e9aabf4c7cb43ff5399
DK <laancer4@gmail.com>
2024-10-18 21:08:48 +0900

    refactor : Factory 클래스의 로직을 stream 을 활용하도록 변경

--- src/main/java/calculator/delimiter/DelimiterFactory.java
+++ src/main/java/calculator/delimiter/DelimiterFactory.java
@@ -2,29 +2,23 @@ package calculator.delimiter;
 
 import calculator.calculator.Delimiter;
 import calculator.utils.ErrorMessage;
+import java.util.List;
 
 public class DelimiterFactory {
 
-    private final CustomDelimiter customDelimiter = new CustomDelimiter();
-
-    private final RegularDelimiter regularDelimiter = new RegularDelimiter();
-
-    private final FallbackDelimiter fallbackDelimiter = new FallbackDelimiter();
+    /**
+     * 반드시 Custom -> Regular -> Fallback 순서로 제공되어야 한다.
+     */
+    private final List<Delimiter> delimiters = List.of(
+            new CustomDelimiter(),
+            new RegularDelimiter(),
+            new FallbackDelimiter()
+    );
 
     public Delimiter getDelimiter(String rawInput) {
-        // 커스텀 먼저 반드시 확인되어야 한다.
-        if (customDelimiter.applicable(rawInput)) {
-            return customDelimiter;
-        }
-
-        if (regularDelimiter.applicable(rawInput)) {
-            return regularDelimiter;
-        }
-
-        if (fallbackDelimiter.applicable(rawInput)) {
-            return fallbackDelimiter;
-        }
-
-        throw new IllegalArgumentException(ErrorMessage.INVALID_DELIMITER.getMessage());
+        return delimiters.stream()
+                .filter(delimiter -> delimiter.applicable(rawInput))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_DELIMITER.getMessage()));
     }
 }
