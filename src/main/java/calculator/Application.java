16e89a2cc075051c4b74cf4564e1d5c33ec57d34
kingjinyong <wlsdydwhswkf@gmail.com>
2024-10-18 11:52:12 +0900

    style: 코드 포맷팅 적용

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,44 +1,46 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Application {
 
-    public static Boolean CustomDiscrimination(String input){
-        if (input.startsWith("//") && input.contains("\\n"))
+    public static Boolean CustomDiscrimination(String input) {
+        if (input.startsWith("//") && input.contains("\\n")) {
             return true;
-        else if (!input.startsWith("//") && !input.contains("\\n"))
+        } else if (!input.startsWith("//") && !input.contains("\\n")) {
             return false;
-        else
+        } else {
             throw new IllegalArgumentException();
+        }
     }
 
-    public static String CustomExtraction(String customInput){
+    public static String CustomExtraction(String customInput) {
         String extraction = customInput.substring(customInput.indexOf("//") + 2, customInput.indexOf("\\n"));
-        if (extraction.length() > 1 || extraction.matches("\\d+"))
+        if (extraction.length() > 1 || extraction.matches("\\d+")) {
             throw new IllegalArgumentException();
-        else if (extraction.isEmpty()) {
+        } else if (extraction.isEmpty()) {
             return null;
         }
         return extraction;
     }
 
-    public static String[] operationSetting(String operationInput, ArrayList<String> separate){
-            String separatorPattern = String.join("|", separate);
-            return operationInput.split(separatorPattern);
+    public static String[] operationSetting(String operationInput, ArrayList<String> separate) {
+        String separatorPattern = String.join("|", separate);
+        return operationInput.split(separatorPattern);
     }
 
-    public static int sumCalculator(String operationInput, ArrayList<String> separateList){
+    public static int sumCalculator(String operationInput, ArrayList<String> separateList) {
         int result = 0;
-        for(String number: operationSetting(operationInput, separateList)){
-            if (number.matches("[1-9]\\d*"))
+        for (String number : operationSetting(operationInput, separateList)) {
+            if (number.matches("[1-9]\\d*")) {
                 result += Integer.parseInt(number);
-            else if (number.isEmpty())
+            } else if (number.isEmpty()) {
                 result += 0;
-            else throw new IllegalArgumentException();
+            } else {
+                throw new IllegalArgumentException();
+            }
         }
         return result;
     }
@@ -48,11 +50,10 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
         ArrayList<String> separate = new ArrayList<>(Arrays.asList(",", ":"));
-        if (CustomDiscrimination(str)){
+        if (CustomDiscrimination(str)) {
             separate.add(CustomExtraction(str));
             System.out.println("결과 : " + sumCalculator(str.substring(str.indexOf("\\n") + 2), separate));
-        }
-        else{
+        } else {
             System.out.println("결과 : " + sumCalculator(str, separate));
         }
 
