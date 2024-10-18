bc9240efcc089f0eec8921b31739e31ab3eb4ead
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-10-18 23:40:29 +0900

    feat(Calculator): 분리된 문자열을 덧셈하고 반환하는 메소드 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,10 +1,22 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Calculator { //TODO: 입력된 숫자들을 모두 더해서 반환
     public static Integer calculate(String expression) {
         if (InputHandler.isNullOrEmpty(expression)) {
             return 0;
         }
-        return Integer.parseInt(expression);
+        int total = 0;
+        String[] numbers = StringSeparator.separator(expression);
+        total = getAdditionResult(numbers, total);
+        return total;
+    }
+
+    private static int getAdditionResult(String[] numbers, int total) {
+        for (String num : numbers) {
+            total += Integer.parseInt(num);
+        }
+        return total;
     }
 }
