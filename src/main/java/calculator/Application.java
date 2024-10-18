3c550a128e3545e424bff81ee4b17f4582e3c8f3
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-10-18 17:05:53 +0900

    feat: 문자열 입력받아 저장
    - 빈 문자열 입력 테스트도 가능하도록 hasNextLine() 여부 확인 구문 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import java.util.Scanner;
 public class Application {
+    private final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = "";
+        if (scanner.hasNextLine()) { // 입력 문자열이 null이 아니라면, 버퍼로부터 입력값을 가져온다.
+            input = scanner.nextLine();
+        }
     }
 }
