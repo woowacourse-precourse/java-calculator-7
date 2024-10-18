615fdbc4cde99e322553d5f7382e5b9e4351facd
HyeonsuLee <leehyeonsu4888@naver.com>
2024-10-18 16:49:20 +0900

    refactor: 매우 큰 숫자를 받을 수 있도록 BigInteger 사용

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -1,10 +1,15 @@
 package calculator.model;
 
+import java.math.BigInteger;
 import java.util.List;
 
 public class Calculator {
 
-    public int sum(List<Number> numbers) {
-        return numbers.stream().mapToInt(Number::number).sum();
+    public BigInteger sum(List<Number> numbers) {
+        BigInteger sum = BigInteger.valueOf(0L);
+        for (Number number: numbers) {
+            sum = sum.add(number.number());
+        }
+        return sum;
     }
 }
