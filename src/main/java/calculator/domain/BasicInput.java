3d7e22aa44fbcc327e7aad596ce46ab98a59dcd6
pakaheo <k70923@konkuk.ac.kr>
2024-10-18 14:18:58 +0900

    refactor : add static factory method and createCalculationInputs by Input

--- src/main/java/calculator/domain/BasicInput.java
+++ src/main/java/calculator/domain/BasicInput.java
@@ -1,17 +1,30 @@
 package calculator.domain;
 
+import calculator.constants.Constants;
+import java.util.Arrays;
 import java.util.Objects;
 
-public class BasicDelimiterInput extends Input {
+public class BasicInput extends Input {
 
     private final String text;
 
-    private BasicDelimiterInput(String text) {
+    private BasicInput(String text) {
         this.text = text;
     }
 
     public static Input from(String text) {
-        return new BasicDelimiterInput(text);
+        return new BasicInput(text);
+    }
+
+    @Override
+    public Long[] createCalculationInputs() {
+        return Arrays.stream(createStrings(text))
+                .map(Long::valueOf)
+                .toArray(Long[]::new);
+    }
+
+    private String[] createStrings(String text) {
+        return text.split(Constants.BASIC_DELIMITER);
     }
 
     @Override
@@ -19,7 +32,7 @@ public class BasicDelimiterInput extends Input {
         if (this == object) {
             return true;
         }
-        if (!(object instanceof BasicDelimiterInput that)) {
+        if (!(object instanceof BasicInput that)) {
             return false;
         }
         return Objects.equals(text, that.text);
@@ -29,4 +42,5 @@ public class BasicDelimiterInput extends Input {
     public int hashCode() {
         return Objects.hash(text);
     }
+
 }
