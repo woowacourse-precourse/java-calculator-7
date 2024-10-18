1007f6e9e05fbe9449116a997774b0f87aa1d1fa
donghyunn1 <dongdong0516@naver.com>
2024-10-19 01:47:22 +0900

    feat : 계산 합 출력 구현

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -6,6 +6,15 @@ public class Calculator {
 
     private static final String NEGATIVE_NUMBER_ERROR = "[ERROR] 양수만 입력 가능합니다.";
 
+    public int sum(List<Integer> numbers) {
+        int total = 0;
+        for (int number : numbers) {
+            validatePositive(number);
+            total += number;
+        }
+        return total;
+    }
+
     private void validatePositive(int number) {
         if (number < 0) {
             throw new IllegalArgumentException(NEGATIVE_NUMBER_ERROR);
