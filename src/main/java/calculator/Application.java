e86ba31bc9eb2755a7ae5387c3c07045dd3d9d87
jkh28 <jkhkorea28@naver.com>
2024-10-18 15:52:27 +0900

    feat: 커스텀 구분자 추가 및 계산 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,10 +3,9 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
-        String str = Console.readLine();
-        String regex = "[,:]";
 
+    public static int getSum(String str, String separator) {
+        String regex = "[" + separator + "]";
         String[] array = str.split(regex);
 
         int result = 0;
@@ -14,6 +13,24 @@ public class Application {
             result += Integer.parseInt(s);
         }
 
-        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String sep = ",:";
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = Console.readLine();
+
+        if (str.startsWith("//")) {
+            int idx = str.indexOf("\\n");
+            // 문자열 추출 후 새 구분자 문자열, 새 검사 문자열 선언
+            String newSeparator = sep + str.substring(2, idx);
+            String newStr = str.substring(idx + 2);
+
+            System.out.println("결과 : " + getSum(newStr, newSeparator));
+        } else {
+            System.out.println("결과 : " + getSum(str, sep));
+        }
+
     }
 }
