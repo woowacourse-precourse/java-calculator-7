9410bb1bc460e098caaa05130b7203aebac99edf
1999byh <1999byh@naver.com>
2024-10-18 17:51:37 +0900

    feat: 기본 구분자(, 또는 :)로 문자열을 구분한다

--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -1,4 +1,51 @@
 package calculator;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 public class Separator {
+
+    private final Validator validator;
+
+    public Separator(Validator validator) {
+        this.validator = validator;
+    }
+
+    public BigDecimal separate(String userInput) {
+        if (hasExistCustomSeparator(userInput)) {
+            return existCustomSeparator(userInput);
+        }
+        return notExistCustomSeparator(userInput);
+    }
+
+    public boolean hasExistCustomSeparator(String userInput) {
+        if (userInput == null) {
+            return false;
+        }
+        if (userInput.startsWith("//")) {
+            int index = userInput.indexOf("\n");
+            if (index > 2) {
+                return true;
+            }
+            throw new IllegalArgumentException("커스텀 구분자가 올바르지 않은 형식입니다.");
+        }
+        return false;
+    }
+
+    public BigDecimal notExistCustomSeparator(String userInput) {
+        String delimiter = ",|:";
+        List<String> numbers = Arrays.asList(userInput.split(delimiter));
+        BigDecimal sum = BigDecimal.ZERO;
+
+        for (String number : numbers) {
+            sum = sum.add(new BigDecimal(number));
+        }
+
+        return sum;
+    }
+
+    public BigDecimal existCustomSeparator(String userInput) {
+        return BigDecimal.ZERO;
+    }
 }
