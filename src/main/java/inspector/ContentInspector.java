cbc32c5e59a52b42a37259d858b077c74f59e8b9
lsj0822 <lsj0822@khu.ac.kr>
2024-10-18 14:49:18 +0900

    fix : ContentInspector 수정
    ContentInspector와 이를 상속 받는 DefaultInspector의 제어자를 수정하였습니다.
    잘못된 문자를 확인하는 inspecting 메서드를 부모 클래스로 이동하였습니다
    inspecting 메서드에서 호출하는 boolean 메서드인 characterCheck 메서드를 부모 클래스로 이동하였습니다.

--- src/main/java/inspector/ContentInspector.java
+++ src/main/java/inspector/ContentInspector.java
@@ -1,14 +1,28 @@
 package inspector;
 
 public abstract class ContentInspector {
-    protected String targetContent;
-    protected String delimiterGroup;
+    private String targetContent;
+    private String delimiterGroup;
 
     public ContentInspector(String targetContent) {
         this.targetContent = targetContent;
     }
 
-    protected abstract void inspecting();
 
     public abstract String getDelimiterGroup();
+
+    protected void inspecting() {
+        for (int i = 0; i < targetContent.length(); i++) {
+            if (!charcterCheck(targetContent.charAt(i))) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private boolean charcterCheck(char inputCharacter) {
+        if (Character.isDigit(inputCharacter)) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/inspector/DefaultInspector.java
+++ src/main/java/inspector/DefaultInspector.java
@@ -1,31 +1,15 @@
 package inspector;
 
 public class DefaultInspector extends ContentInspector {
-    protected String targetContent;
-    protected String delimiterGroup = ",|:";
+    private String targetContent;
+    private String delimiterGroup = ",|:";
 
     public DefaultInspector(String targetContent) {
         super(targetContent);
     }
 
     @Override
-    protected void inspecting() {
-        for (int i = 0; i < targetContent.length(); i++) {
-            if (!charcterCheck(targetContent.charAt(i))) {
-                throw new IllegalArgumentException();
-            }
-        }
-    }
-
-    @Override
     public String getDelimiterGroup() {
         return delimiterGroup;
-    }
-
-    private boolean charcterCheck(char inputCharacter) {
-        if (Character.isDigit(inputCharacter)) {
-            return true;
-        }
-        return false;
     }
 }
