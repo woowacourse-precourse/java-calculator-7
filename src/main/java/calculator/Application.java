75f63fa8b6076a41fb4e468f5067e14a64a395d1
kcs19 <changsung19@gmail.com>
2024-10-19 00:03:27 +0900

    feat: 문자열 입력 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    public static String inputLine(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static void main(String[] args){
+        String line= inputLine();
     }
 }
