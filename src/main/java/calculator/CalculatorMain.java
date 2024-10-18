6567153bb4864dc9b348dbbec71b8ad729ade4d0
lsj0822 <lsj0822@khu.ac.kr>
2024-10-18 22:37:04 +0900

    fix : calculation 리팩터링
    calculation 메서드에 있던 문자열을 검사하고 문자열을 BigInteger로 변환하는 메서드를 분리했습니다.

--- src/main/java/calculator/CalculatorMain.java
+++ src/main/java/calculator/CalculatorMain.java
@@ -9,35 +9,52 @@ import parser.ContentParser;
 public class CalculatorMain {
     private BigInteger calculateResult = BigInteger.ZERO;
     private String inputContent;
+    private String convertedContent;
+    private String parsingDelimiter;
     private String[] parsedContent;
 
     public void startCalculator() {
         setInputContent();
         ContentInspector contentInspector = InspectorSelector.selection(inputContent);
-        String parsingDelimiter = contentInspector.getDelimiterGroup();
-        String convertedContent = contentInspector.getContent();
+        parsingDelimiter = contentInspector.getParsingDelimiter();
+        convertedContent = contentInspector.getContent();
         ContentParser contentParser = new ContentParser(parsingDelimiter, convertedContent);
         parsedContent = contentParser.getParsedContent();
-        Calculation();
+        calculation();
         getCalculateResult();
     }
 
     private void setInputContent() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         inputContent = Console.readLine();
+        if (inputContent.isBlank()) {
+            getCalculateResult();
+        }
     }
 
     private void getCalculateResult() {
         System.out.println("결과 : " + calculateResult);
     }
 
-    private void Calculation() {
+    private void calculation() {
         for (String content : parsedContent) {
-            BigInteger contentNumber = new BigInteger(content);
-            if (contentNumber.compareTo(BigInteger.ZERO) == -1) {
-                throw new IllegalArgumentException();
-            }
-            calculateResult = calculateResult.add(new BigInteger(content));
+            BigInteger addedBigInteger = convertToBigInteger(content);
+            makeNegativeException(addedBigInteger);
+            calculateResult = calculateResult.add(addedBigInteger);
+        }
+    }
+
+    private BigInteger convertToBigInteger(String content) {
+        if (content.isEmpty()) {
+            return BigInteger.ZERO;
+        } else {
+            return new BigInteger(content);
+        }
+    }
+
+    private void makeNegativeException(BigInteger bigInteger) {
+        if (bigInteger.compareTo(BigInteger.ZERO) == -1) {
+            throw new IllegalArgumentException();
         }
     }
 }
