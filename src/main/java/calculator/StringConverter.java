6d30b49de75c87c7636cfd41b430446c62d5c3d4
sinyebin <dpqls0625@naver.com>
2024-10-19 01:20:21 +0900

    feat: 입력된 문자가 양수가 아닐 경우 에러

--- src/main/java/calculator/StringConverter.java
+++ src/main/java/calculator/StringConverter.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 
 public class StringConverter {
     private final static String NOT_VALIDATE_NUMBER = "유효하지 않은 숫자 형식입니다.";
+    private static final String NEGATIVE_NUMBER_ERROR = "음수는 허용되지 않습니다: ";
+
     public List<Integer> convertToNumbers(String[] inputs) {
         return Arrays.stream(inputs)
                 .map(this::convertToInt)
@@ -14,9 +16,17 @@ public class StringConverter {
 
     private int convertToInt(String input) {
         try {
-            return Integer.parseInt(input.trim());
+            int number = Integer.parseInt(input.trim());
+            validatePositiveNumber(number);
+            return number;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NOT_VALIDATE_NUMBER);
+        }
+    }
+
+    private void validatePositiveNumber(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER_ERROR);
         }
     }
 }
--- src/test/java/calculator/StringConverterTest.java
+++ src/test/java/calculator/StringConverterTest.java
@@ -13,9 +13,25 @@ import org.junit.jupiter.api.Test;
 class StringConverterTest {
     @DisplayName("문자를 숫자로 변환")
     @Test
-    public void 문자를_숫자로_변환() throws Exception {
+    void 문자를_숫자로_변환() {
         StringConverter stringConverter = new StringConverter();
-        List<Integer> numList = stringConverter.convertToNumbers(new String[]{"1","2","3"});
-        assertThat(numList).isEqualTo(Arrays.asList(1,2,3));
+        List<Integer> numList = stringConverter.convertToNumbers(new String[]{"1", "2", "3"});
+        assertThat(numList).isEqualTo(Arrays.asList(1, 2, 3));
+    }
+
+    @DisplayName("양수가 아닌 경우 에러(음수인 경우)")
+    @Test
+    void 양수가_아닌경우_에러1() {
+        StringConverter stringConverter = new StringConverter();
+        assertThatThrownBy(() -> stringConverter.convertToNumbers(new String[]{"-11", "2", "3"}))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("양수가 아닌 경우 에러(숫자가 아닌경우)")
+    @Test
+    void 양수가_아닌경우_에러2() {
+        StringConverter stringConverter = new StringConverter();
+        assertThatThrownBy(() -> stringConverter.convertToNumbers(new String[]{"a", "2", "3"}))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
