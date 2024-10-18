44c953ecf01ca94dcdaf6283e915af79b8314b43
seongeun308 <kse011010@gmail.com>
2024-10-19 02:49:44 +0900

    feat: String 배열을 int 배열로 변환하는 기능 구현
    String값이 빈값일 경우(0일 경우)와 양수일 경우에만 int 배열 요소로 저장
    그 외의 경우(입력 요구 사항에 위배되는 경우) 예외 발생

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -10,4 +10,23 @@ public class StringCalculator {
     private static String getCustomDelim(String data){
         return data.replaceAll(Delimiter.CUSTOM_DELIM.getRegex(), "");
     }
+
+    private static int[] convertStringArrayToIntArray(String[] split) {
+        int[] numbers = new int[split.length];
+        for(int i = 0; i< split.length; i++) {
+            // "" => 0
+            if(split[i].isEmpty())
+                numbers[i] = 0;
+            else{
+                try {
+                    int num = Integer.parseInt(split[i]);
+                    if (num >= 0) numbers[i] = num; // 양수일때만 베열에 저장
+                    else throw new IllegalArgumentException();
+                }catch (NumberFormatException e) {
+                    throw new IllegalArgumentException();   // 입력 요구 사항에 맞지 않는 경우
+                }
+            }
+        }
+        return numbers;
+    }
 }
