dabfaf6e2ee9b9ddb21285db21f01ce6de1b1a8e
chan <ckstj8027@naver.com>
2024-10-18 22:36:16 +0900

    refactor(dto): rename class NumberDTO to NumberDto

--- src/main/java/calculator/dto/NumberDto.java
+++ src/main/java/calculator/dto/NumberDto.java
@@ -2,13 +2,13 @@ package calculator.dto;
 
 import java.util.ArrayList;
 
-public class NumberDTO {
+public class NumberDto {
 
 
     private final ArrayList<Integer> numberRepository;
 
 
-    public NumberDTO(ArrayList<Integer> numberRepository) {
+    public NumberDto(ArrayList<Integer> numberRepository) {
         this.numberRepository = numberRepository;
     }
 
