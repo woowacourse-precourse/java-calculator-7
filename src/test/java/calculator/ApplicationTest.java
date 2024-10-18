e388db89648143694bb8e4536ee1ab47066ab513
gikhoon <ghikhoon@naver.com>
2024-10-18 17:05:45 +0900

    test(testing): 테스트 케이스 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -17,10 +18,42 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_구분자_사용X() {
+        assertSimpleTest(() -> {
+            run("1:2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 아무것도_없을_때_테스트() {
+        assertSimpleTest(() -> {
+            run("//\\\\n:,\\");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_백_슬래시() {
+        assertSimpleTest(() -> {
+            run("///\\\\n1\\2:3/4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 마이너스_예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 없는_구분자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n1/2;3:4"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
