bb3d342ccc8ef28b6edc80a2afebfccc4623496f
hongjungin <jungini0000@gmail.com>
2024-10-18 20:08:33 +0900

    feat: 커스텀 구분자 지정 기능

--- src/main/java/calculator/GetNumbers.java
+++ src/main/java/calculator/GetNumbers.java
@@ -4,9 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GetNumbers {
-    // 2. 기본 구분자(, :)를 기준으로 숫자 추출
     public List<Integer> extractNumbers(String input) {
-        String[] tokens = input.split("[,:]");
+        String defaultDelimiter = ",|:";
+        String customDelimiter = "";
+        String numbersPart = "";
+
+        // 커스텀 구분자인지 확인
+        if (input.startsWith("//")) {
+            int delimiterEndIndex = input.indexOf("\n");
+            customDelimiter = input.substring(2, delimiterEndIndex);
+            numbersPart = input.substring(delimiterEndIndex + 1);
+        }
+
+        // 구분자 설정
+        String delimiter = customDelimiter.isEmpty() ? defaultDelimiter : customDelimiter;
+        String[] tokens = numbersPart.split(delimiter);
         List<Integer> numbers = new ArrayList<>();
 
         for (String token : tokens) {
