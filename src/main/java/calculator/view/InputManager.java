a9533a3e128e588201caf29b48f618a1d8e0598b
Jang-GO <jang8195@naver.com>
2024-10-18 14:52:53 +0900

    feat(InputManager) : 문자열 입력받기 기능 구현
    - 문자열을 입력받아 String형으로 반환

---
+++ src/main/java/calculator/view/InputManager.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputManager {
+
+    public static String consoleInput(){
+        String str = Console.readLine();
+        return str;
+    }
+}
