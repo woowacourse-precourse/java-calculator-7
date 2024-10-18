04672bff6f1875ab386ad6b4380ccd299f6f6224
taehyun <xogus5070318@gmail.com>
2024-10-19 00:11:32 +0900

    feat(applicatioTest): 추가 테스트 함수 생성 및 설계

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -23,6 +24,39 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 예외_테스트_255자_이상() {
+        //Todo: 길이가 225이상인 문자열을 입력했을 경우 예외
+    }
+
+    @Test
+    void 예외_테스트_커스텀_구분자를_가진_문자열을_위한_최소길이_미충족() {
+        //Todo: "//"로 시작하는 문자열 중에서 길이가 5 이하인 문자열일 경우 예외처리
+
+    }
+
+    @Test
+    void 예외_테스트_커스텀_구분자를_가진_문자열_올바르지않은_형식(){
+        //Todo: "//"로 시작하는 문자열이 "//<구분자>\n" 형식이 아니면 예외처리
+    }
+
+    @Test
+    void 예외_테스트_커스텀_구분자가_숫자일_경우(){
+        //Todo: 커스텀 구분자가 숫자일 경우 예외처리
+    }
+
+    @Test
+    void 예외_테스트_덧셈_결과값이_10억_이상일_경우(){
+        //Todo: 덧셈 결과값이 10억 이상일 경우 예외처리
+    }
+
+    @Test
+    void 예외_테스트_입력한_숫자가_10억_이상일_경우(){
+        //Todo: 입력한 숫자가 10억 이상일 경우 예외처리
+    }
+
+
 
     @Override
     public void runMain() {
