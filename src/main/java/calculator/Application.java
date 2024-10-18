6a530740afadee9be0f8a98e0477a97e07b61997
UmbaDumba <dkdi3104@gmail.com>
2024-10-18 23:44:21 +0900

    parsing_string

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,14 @@
 package calculator;
 
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.platform.commons.util.StringUtils;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+
+    public static String sep = ",|:";
 
     public static int add_arrays(int[] nums){
 
@@ -12,13 +20,44 @@ public class Application {
         return result;
     }
 
+    public static boolean is_num(String str){
+        for(int i = 0; i<str.length(); i++){
+            int num = (int) str.charAt(i);
+            if(num < 48 || num > 57){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int[] parsing_string(String str){
+        // str은 숫자 구분자 숫자 구분자... 형식임을 가정한다
+
+        String[] spe_result = str.split(sep);
+
+        int[] result = new int[spe_result.length];
+
+        for(int i = 0; i<spe_result.length; i++){
+            if(is_num(spe_result[i])){
+                // 숫자 -> parsing 잘된것!
+                result[i] = Integer.parseInt(spe_result[i]);
+            }else{
+                throw new IllegalArgumentException("이상하오...");
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        // add_arrays test
-        int[] test_nums = {1,2,3,4,5};
 
-        System.out.println(add_arrays(test_nums));
+
+        String inputtext = readLine();
+        int[] nums = parsing_string(inputtext);
+
+        System.out.println(add_arrays(nums));
 
     }
 }
