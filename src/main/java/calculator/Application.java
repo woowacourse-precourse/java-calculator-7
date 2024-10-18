8e751409c674af68ae6e32898dd356c6b7fe7a43
kimdevspace <ejk5148@gmail.com>
2024-10-18 22:16:57 +0900

    feat : add function2

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
 
-    private static Function function = Function.getInstance();
+    private static final Function function = Function.getInstance();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -13,7 +13,16 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
 
-        int result = function.isSplit(str);
+        //기능 수행
+        int result = 0;
+
+        if (str.startsWith("//")) {
+            result = function.customSeparator(str);
+        } else  {
+            result = function.isSplit(str);
+        }
+
+
         System.out.println("결과 : " + result);
 
     }
--- src/main/java/calculator/Function.java
+++ src/main/java/calculator/Function.java
@@ -18,7 +18,30 @@ public class Function {
         StringTokenizer st = new StringTokenizer(str, ",:");
 
         while (st.hasMoreTokens()) {
-            sum += Integer.parseInt(st.nextToken());
+            String token = st.nextToken();
+            sum += Integer.parseInt(token);
+        }
+
+        return sum;
+    }
+
+    //기능 2 : 커스텀 구분자를 지정하여 숫자를 추출하는 기능
+    public int customSeparator(String str) {
+
+        int sum = 0;
+
+        str = str.replace("\\n", "\n"); // \n 입력을 공백으로 바꾸기
+
+        char separator = str.charAt(2); // 구분자는 "//" 뒤에오는 문자
+
+        int secondSeparatorIndex = str.indexOf("\n") + 1;
+
+        String subString = str.substring(secondSeparatorIndex); // 숫자가 시작되는 곳 추출
+
+        String[] numbers = subString.split(String.valueOf(separator)); //숫자들을 구분자로 분리하기
+
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
         }
 
         return sum;
