b1b56a8e287340826635bc225d7b5af5c5deff68
sinminseok12 <sinminseok12@naver.com>
2024-10-19 02:38:54 +0900

    feat : 덧셈 결과 출력 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Calculator {
     private static final String INPUT_STRING_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String OUTPUT_RESULT_MESSAGE = "결과 : ";
 
     Converter converter;
 
@@ -17,11 +18,17 @@ public class Calculator {
         String input = inputString();
         List<Integer> integers = converter.convertNumbersFromString(input);
         PositiveNumbers positiveNumbers = PositiveNumbers.from(integers);
+        printResult(positiveNumbers.calculateSum());
     }
 
     private String inputString() {
         System.out.println(INPUT_STRING_MESSAGE);
         return Console.readLine();
+    }
+
+
+    private void printResult(int result) {
+        System.out.println(OUTPUT_RESULT_MESSAGE + result);
     }
 
 }
