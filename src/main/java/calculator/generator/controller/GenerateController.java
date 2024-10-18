95beb35bfeb68c4d8aa66598ecce81bf66e3785e
EHCTH <lees90896@gmail.com>
2024-10-18 19:07:07 +0900

    refactor(GenratorController): Separator class 제거로 인한 구조 변경

--- src/main/java/calculator/generator/controller/GenerateController.java
+++ src/main/java/calculator/generator/controller/GenerateController.java
@@ -3,21 +3,13 @@ package calculator.generator.controller;
 import calculator.number.domain.Number;
 import calculator.generator.number.NumberGenerator;
 import calculator.sentence.domain.Sentence;
-import calculator.separator.domain.Separator;
-import calculator.generator.separator.SeparatorGenerator;
 
 public class GenerateController {
-    private final SeparatorGenerator separatorGenerator;
     private final NumberGenerator numberGenerator;
-    public GenerateController(SeparatorGenerator separatorGenerator,
-                              NumberGenerator numberGenerator) {
-        this.separatorGenerator = separatorGenerator;
+    public GenerateController(NumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
     }
-    public Separator createSeparator(Sentence sentence) {
-        return separatorGenerator.create(sentence);
-    }
-    public Number createNumber(Sentence sentence, Separator separator) {
-        return numberGenerator.create(sentence, separator);
+    public Number createNumber(Sentence sentence) {
+        return numberGenerator.create(sentence);
     }
 }
