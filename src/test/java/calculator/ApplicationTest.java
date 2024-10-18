7b15ad4261755a41596a6c231f6d761b5f956b01
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-18 20:33:17 +0900

    test(ApplicationTest): ApplicationTest 입력값에 대한 test 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -31,7 +31,29 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 입력값_공백() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
 
+    @Test
+    void 구분자로끝남() {
+        assertSimpleTest(() -> {
+            run("2,3");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+    @Test
+    void 구분자_없는_입력값() {
+        assertSimpleTest(() -> {
+            run("2345");
+            assertThat(output()).contains("결과 : 2345");
+        });
+    }
 
     @Override
     public void runMain() {
