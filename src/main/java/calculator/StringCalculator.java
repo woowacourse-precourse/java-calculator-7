d73e3a2a05b46d17be5bcc0ec6f9576de64945b9
sc@t0a14 <gusdn6671@gmail.com>
2024-10-18 09:58:46 +0900

    refactor(StringCalculator) : 커스텀 구분자를 사용 한 경우 리팩토링

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,8 +1,29 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class StringCalculator {
 
     public void run(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        UserInput userInput = new UserInput(input);
+        String defaultDelimiter = "[,:]";
+        DefaultDelimiter defaultDelimiters = new DefaultDelimiter(defaultDelimiter);
+        DelimiterResult delimiterResult = new DelimiterResult(defaultDelimiters.getDefaultDelimiter());
+
+        if(userInput.isCustom()){
+            int customDelimiterEndIndex = userInput.getUserInput().indexOf("\\n");
+            String customDelimiter = userInput.getUserInput().substring(2,customDelimiterEndIndex);
+            String updateDelimiter = delimiterResult.getDelimiterResult().replace("]", customDelimiter + "]");
+            delimiterResult = new DelimiterResult(updateDelimiter);
+
+        }
+
+
+
 
     }
+
 }
