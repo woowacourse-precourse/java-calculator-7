f81a9eb2a21e1f122639f03e7b28dad02ecf809e
FhRh <yiyop@naver.com>
2024-10-18 10:24:44 +0900

    feat(Calculator): 예외처리 구현
    - README.md 구현사항 반영

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -3,6 +3,7 @@ package calculator.model;
 import java.util.ArrayList;
 
 public class Calculator {
+
     private ArrayList<String> delimiters;
 
     public Calculator() {
@@ -15,15 +16,22 @@ public class Calculator {
         delimiters.add(customDelimeter);
     }
 
-    public int calculate(String expression) {
+    public int sum(String expression) {
         String regex = String.join("|", delimiters);
         String[] numbers = expression.split(regex);
 
         int sum = 0;
         for (String number : numbers) {
-            sum += Integer.parseInt(number); // 각 숫자를 더함
+            if (!isPositiveNumber(number)) {
+                throw new IllegalArgumentException();
+            }
+            sum += Integer.parseInt(number);
         }
 
         return sum;
+    }
+
+    private boolean isPositiveNumber(String number) {
+        return number.matches("[1-9]\\d*");
     }
 }
