d7223f1bab3c4e6e441c651395fa731093fd7275
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2024-10-18 20:18:02 +0900

    refactor(Application.java) : Refactor method name

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,7 +9,7 @@ public class Application {
         // TODO: 프로그램 구현
         Calculator calculator = new Calculator();
         // 입력받기
-        String input = Calculator.get_input();
+        String input = calculator.getInput();
         // 파싱하기
         List<Integer> numbers = calculator.parse(input);
         // 계산하기
