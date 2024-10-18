4918fc51d0f50e96df252ac1115e3ca01537dacd
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-10-18 16:28:40 +0900

    :necktie: feat: 계산기 기능을 시작하는 run() 메서드 추가
    - run() 메서드에서 사용자 입력을 받는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        run();
+    }
+
+    public static void run(){
+        String input = Console.readLine();
     }
 }
