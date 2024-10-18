836cdd334cdc70c326a2543be809ed9fd2059c07
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-10-18 18:43:32 +0900

    feat: implement sum of separated numbers
    Changed:
    - Updated to use camp.nextstep.edu.missionutils.Console.readLine for user input instead of external library

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,25 +1,44 @@
 package calculator;
 
-import java.util.Scanner;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String st = sc.next();
+    public static void main(String[] args){
+        String st = readLine();
+
+        int sum = 0;
 
         if (st.contains(":") || st.contains(";")) {
             String[] arr = st.split("\\D");
 
-            for (String s : arr) {
-                System.out.print(s);
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i].equals("")) {
+                    arr[i] = "0";
+                }
             }
+
+            for (String s : arr) {
+                int integer = Integer.parseInt(s);
+                sum += integer;
+            }
+
+            System.out.println("결과 : " + sum);
 
         } else if (st.contains("//") && st.contains("\\n")) {
             String[] arr1 = st.split("\\D");
 
-            for (String s : arr1) {
-                System.out.print(s);
+            for (int i = 0; i < arr1.length; i++) {
+                if(arr1[i].equals("")) {
+                    arr1[i] = "0";
+                }
             }
+
+            for (String s : arr1) {
+                int integer = Integer.parseInt(s);
+                sum += integer;
+            }
+
+            System.out.println("결과 : " + sum);
 
         } else {
             throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
