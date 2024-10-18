60140059edcbfcfc9b03e5b60e9383143bd85e2c
1999byh <1999byh@naver.com>
2024-10-18 11:22:15 +0900

    feat: 문자열을 입력받는다

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -1,4 +1,12 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
+
+    public String input() {
+        String userInput = Console.readLine();
+
+        return userInput;
+    }
 }
