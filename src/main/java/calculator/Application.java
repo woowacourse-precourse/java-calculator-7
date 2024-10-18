36550e5be89215db0b53ced3701b143df9c4f674
고희준 <kohuijun@gohuijun-ui-MacBookAir.local>
2024-10-18 16:41:24 +0900

    feat 기본 구별자기능, 문자열 숫자 변환 후 계산 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,41 @@
 package calculator;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+
+    public static String[] StringSplit(String input) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch == ':' || ch == ',') {
+                result.append("|");
+            } else {
+                result.append(ch);
+            }
+        }
+        return result.toString().split("\\|");
+    }
+
+    public static int calculator(String input) {
+        int sum = 0;
+        String[] number = StringSplit(input);
+
+        for (String numbers : number) {
+            sum += Integer.parseInt(numbers); // 문자열을 숫자로 변환하여 더하기
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String SeparatorWithNum = Console.readLine();
+
+        int sum = calculator(SeparatorWithNum);
+
+        System.out.println("결과 : " + sum);
     }
 }
+
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,6 +9,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
@@ -19,8 +27,8 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
