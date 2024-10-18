21cc8fc4b1cd56523700bb36060a9092399c670e
chungchung <chunghan@chungchung.local>
2024-10-18 16:27:10 +0900

    feat: 입력 문자열 커스텀 구분자 문자 추출
    (cherry picked from commit e089a69a60b5ee32dc93258dc256b71c83831cd3)

--- src/main/java/calculator/StringHandler.java
+++ src/main/java/calculator/StringHandler.java
@@ -2,6 +2,15 @@ package calculator;
 
 public class StringHandler {
     public String[] split(String text, String delimiter) {
+        if(text.startsWith("//")) {
+            int customDelimiterIndex = text.indexOf("\\n");
+            if(customDelimiterIndex == -1) {
+                throw new IllegalArgumentException("잘못된 구분자입니다.");
+            }
+            delimiter = text.substring(2, customDelimiterIndex);
+            text = text.substring(customDelimiterIndex + 2);
+        }
+
         return text.split(delimiter);
     }
 }
