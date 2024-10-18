7b3808a132b60bb6be1ded1465cdb96e95538ffe
donggundg <rlaehdrjs98@g.skku.edu>
2024-10-18 16:06:51 +0900

    feat(Application) : 입출력 구현
    기존 Scanner가 아닌 제공된 Console library를 이용하여 입력 받음.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //입력 받는 객체 scanner 생성
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = Console.readLine();
+
+        //출력 로직
+        String sum = "0";
+        System.out.println("결과 : " + sum);
     }
 }
