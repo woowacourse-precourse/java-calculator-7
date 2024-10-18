d0a3f856191cde09e0a38261e6548143909714d5
woonie155 <woonie155@gmail.com>
2024-10-19 04:47:07 +0900

    refactor: 입력 문자열 검증 메서드 분리
    문자열 입력 함수에서 검증 로직을 분리

--- src/main/java/calculator/domain/calculator/Calculator.java
+++ src/main/java/calculator/domain/calculator/Calculator.java
@@ -24,25 +24,28 @@ public class Calculator {
 
     private void getString() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String s = Console.readLine();
+        this.inputStr = Console.readLine();
         Console.close();
 
-        if (s.length() >= 5 && s.charAt(0) == '/' && s.charAt(1) == '/'
-                && s.charAt(3) == '\\' && s.charAt(4) == 'n') {
-            if (Character.isDigit(s.charAt(2))) {
+        this.validateInputData();
+    }
+
+    private void validateInputData() {
+        if (this.inputStr.length() >= 5 && this.inputStr.charAt(0) == '/' && this.inputStr.charAt(1) == '/'
+                && this.inputStr.charAt(3) == '\\' && this.inputStr.charAt(4) == 'n') {
+            if (Character.isDigit(this.inputStr.charAt(2))) {
                 throw new IllegalArgumentException();
             }
-            delimiter.addDelimiter(s.charAt(2));
-            s = s.substring(5);
+            delimiter.addDelimiter(this.inputStr.charAt(2));
+            this.inputStr = this.inputStr.substring(5);
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        for (int i = 0; i < this.inputStr.length(); i++) {
+            char ch = this.inputStr.charAt(i);
             if (!(Character.isDigit(ch) || delimiter.isDelimiter(ch))) {
                 throw new IllegalArgumentException();
             }
         }
-        this.inputStr = s;
     }
 
     private void extractNumberList() {
