333b1d9d5149e6e2274e91741992ef15541eb7dc
mhj1205 <lightnow1205@gmail.com>
2024-10-18 19:41:12 +0900

    feat: 커스텀 구분자 구별 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,11 +4,20 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         String input;
+        String sep = "";
+        String str = "";
+        int sum, result=0;
 
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+	System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         input = sc.nextLine();
         String[] input_split = input.split("");
+        String[] str_num = input.split(",|:");
 
+        if(input_split[0].equals("/")){
+            
+        }else{
+        
+        } 
     }
 }
