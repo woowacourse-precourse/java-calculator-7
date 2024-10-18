cf0c96b9c9927a5e5b1627992c857e9f0e05139a
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2024-10-18 19:05:23 +0900

    feat(Application.java) : Add whole functions

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Calculator calculator = new Calculator();
+        // 입력받기
+        String input = Calculator.get_input();
+        // 파싱하기
+        List<Integer> numbers = calculator.parse(input);
+        // 계산하기
+        int result = calculator.add(numbers);
+        // 출력하기
+        System.out.println("결과 : " + result);
     }
+
 }
