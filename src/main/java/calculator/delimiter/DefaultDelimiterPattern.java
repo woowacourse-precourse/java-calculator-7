38ed112105557589e8600a3e432d509da1fcdca7
junest66 <chlwndks333@gmail.com>
2024-10-19 02:59:28 +0900

    refactor(DefaultDelimiterPattern): 기본 정규식 패턴 변경
    - 숫자에서 0 제외

--- src/main/java/calculator/delimiter/DefaultDelimiterPattern.java
+++ src/main/java/calculator/delimiter/DefaultDelimiterPattern.java
@@ -3,7 +3,7 @@ package calculator.delimiter;
 import java.util.regex.Pattern;
 
 public class DefaultDelimiterPattern implements DelimiterPattern {
-    private static final String REGEX = "^\\d*([,:]\\d+)*$";
+    private static final String REGEX = "^([1-9]\\d*|$)([,:]([1-9]\\d*))*$";
 
     @Override
     public boolean validate(String input) {
