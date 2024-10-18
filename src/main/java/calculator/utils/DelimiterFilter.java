b2c59714f1027ed5c5ac77a122192eaef06e0b25
wogus <ind07152@naver.com>
2024-10-18 13:34:01 +0900

    refactor(DelimiterFilter): 하드 코딩을 상수화
    하드 코딩된 부분을 상수화하여 통일한다.

--- src/main/java/calculator/utils/DelimiterFilter.java
+++ src/main/java/calculator/utils/DelimiterFilter.java
@@ -10,17 +10,20 @@ import java.util.List;
 
 public class DelimiterFilter {
 
+    private final int STANDARD_INDEX_LENGTH = 2;
+    private final int INDEX_THRESHOLD = 0;
+
     public boolean hasCustomDelimiter(String input) {
         int firstIndex = input.indexOf(FIRST_STANDARD.getStandard());
         int secondIndex = input.indexOf(SECOND_STANDARD.getStandard());
-        return firstIndex == 0 && secondIndex > 0;
+        return firstIndex == INDEX_THRESHOLD && secondIndex > INDEX_THRESHOLD;
     }
 
     public List<Character> getDelimiter(boolean hasCustomDelimiter, String input) {
         if (hasCustomDelimiter) {
             List<Character> separators = new ArrayList<>();
             int secondIndex = input.indexOf(SECOND_STANDARD.getStandard());
-            for (int i = 2; i < secondIndex; i++) {
+            for (int i = STANDARD_INDEX_LENGTH; i < secondIndex; i++) {
                 separators.add(input.charAt(i));
             }
             return separators;
@@ -31,6 +34,6 @@ public class DelimiterFilter {
     public String getPureString(String input) {
         int secondIndex = input.indexOf(SECOND_STANDARD.getStandard());
 
-        return input.substring(secondIndex + 2);
+        return input.substring(secondIndex + STANDARD_INDEX_LENGTH);
     }
 }
