80108e89bdf50d76036bec2b0069853031cd7ab7
Park Yu Han <pbk2312@naver.com>
2024-10-18 16:24:14 +0900

    feat(view): EMPTY_NUMBER_ERROR, DUPLICATE_DELIMITER_ERROR 추가

--- src/main/java/calculator/view/ErrorMessage.java
+++ src/main/java/calculator/view/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
     NON_NUMERIC_INPUT_ERROR("숫자가 아닌 문자가 포함되었습니다."),
     NEGATIVE_NUMBER_ERROR("음수는 허용되지 않습니다."),
     INVALID_CUSTOM_DELIMITER_ERROR("잘못된 커스텀 구분자 형식입니다."),
-    MISSING_DELIMITER_ERROR("구분자가 존재하지 않습니다.");
+    EMPTY_NUMBER_ERROR("숫자가 없습니다."),
+    DUPLICATE_DELIMITER_ERROR("연속된 구분자 형식입니다.");
+
     private final String message;
 
     ErrorMessage(String message) {
@@ -15,5 +17,4 @@ public enum ErrorMessage {
     public String getMessage() {
         return message;
     }
-
 }
