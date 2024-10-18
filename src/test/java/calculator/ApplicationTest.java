42fafdded21883ed74348d684a609d3c5f3384df
yuntasha <jaljayo85@gmail.com>
2024-10-18 16:15:34 +0900

    test(ApplicationTest.java): add test
    add test list:
    - 형식에서 벗어남
    - 커스텀 구분자만 추가
    - 숫자 구분자 시작 예외
    - 기본 구분자만 사용
    - 구분자 한글자 초과 예외
    - 여러 구분자 사용
    - 숫자가 큰 경우

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,62 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 형식에서_벗어남_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("/a\n12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자만_추가() {
+        assertSimpleTest(() -> {
+            run("//;\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 숫자_구분자외_시작_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("k1,23"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_구분자만_사용() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 구분자_한글자_초과_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//ak\n1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 여러_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2:3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 숫자가_큰_경우() {
+        assertSimpleTest(() -> {
+            run("9223372036854775808");
+            assertThat(output()).contains("결과 : 9223372036854775808");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
