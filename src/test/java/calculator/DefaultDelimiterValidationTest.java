f2e4220c84c105a5337ec82221fa9b41c2530aa4
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-18 16:25:27 +0900

    test: 기본 구분자 실패 테스트 구현
    - 기본 구분자 실패하는 테스트 구현

--- src/test/java/calculator/DefaultDelimiterValidationTest.java
+++ src/test/java/calculator/DefaultDelimiterValidationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DefaultDelimiterValidationTest extends NsTest {
     @Test
@@ -17,7 +18,7 @@ class DefaultDelimiterValidationTest extends NsTest {
 
     @Test
     void 기본_구분자_성공_테스트2() {
-        assertSimpleTest(()-> {
+        assertSimpleTest(() -> {
             run("1:2,3");
             assertThat(output()).contains("결과 : 6");
         });
@@ -25,15 +26,54 @@ class DefaultDelimiterValidationTest extends NsTest {
 
     @Test
     void 기본_구분자_성공_테스트3() {
-        assertSimpleTest(()-> {
+        assertSimpleTest(() -> {
             run("\n");
             assertThat(output()).contains("결과 : 0");
         });
+    }
+
+    @Test
+    void 기본_구분자_실패_테스트1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0,1:2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_구분자_실패_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1-1:2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_구분자_실패_테스트3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> run("12-1:2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_구분자_실패_테스트4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("10,1:2,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_구분자_실패_테스트5() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("10,1:2:"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Override
     protected void runMain() {
         Application.main(new String[]{});
     }
-
 }
