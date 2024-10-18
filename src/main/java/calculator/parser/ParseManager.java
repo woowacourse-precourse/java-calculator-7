242e7c481c21c395710bfa5fc79042713f1d2158
joonwan <joonwan1024@naver.com>
2024-10-18 15:46:42 +0900

    feat: ParseManager 메서드 구현

--- src/main/java/calculator/parser/ParseManager.java
+++ src/main/java/calculator/parser/ParseManager.java
@@ -1,5 +1,6 @@
 package calculator.parser;
 
+import calculator.dto.Data;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,5 +8,25 @@ public class ParseManager {
 
     private final List<DataParser> parsers = new ArrayList<>();
 
+    public ParseManager() {
+        this.parsers.add(new DefaultDataParser());
+        this.parsers.add(new CustomDataParser());
+    }
 
+    public Data parseData(String inputData) {
+
+        DataParser targetParser = null;
+        for (DataParser dataParser : parsers) {
+            if (dataParser.isSupport(inputData)) {
+                targetParser = dataParser;
+                break;
+            }
+        }
+
+        if (targetParser == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return targetParser.parseData(inputData);
+    }
 }
