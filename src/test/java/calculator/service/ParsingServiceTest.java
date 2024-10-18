7807abe45b18d4003022fede64db931aaa0e22d0
mete0rfish <sungwon326@naver.com>
2024-10-18 13:50:13 +0900

    test: Parsing 관련 테스트 코드에서 필드로 Service 객체를 가지도록 수정
    반복되는 객체 생성을 해결하기 위해 공통적으로 사용되는 객체를 필드로 지정

--- src/test/java/calculator/service/ParsingServiceTest.java
+++ src/test/java/calculator/service/ParsingServiceTest.java
@@ -6,19 +6,22 @@ import calculator.service.fixture.ExpressionFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ParsingServiceTest {
 
+    private final Calculator calculator = CalculatorFixture.createCalculator();
+    private final ParsingService parsingService = new ParsingService();
+
     @Test
     @DisplayName("parsing Operands and Identifiers Without custom identifier")
     void parseOperandsAndIdentifiersWithoutCustomIdentifier() {
-        // given
-        ParsingService parsingService = new ParsingService(CalculatorFixture.createCalculator());
 
         //when
-        Calculator calculator = parsingService.parsing(ExpressionFixture.EXPRESSION_WITHOUT_CUSTOM_IDENTIFIER);
+        parsingService.parsing(calculator, ExpressionFixture.EXPRESSION_WITHOUT_CUSTOM_IDENTIFIER);
 
         // then
         assertThat(calculator.getOperands().size()).isEqualTo(3);
@@ -28,11 +31,9 @@ class ParsingServiceTest {
     @Test
     @DisplayName("parsing Operands and Identifiers With custom identifier")
     void parseOperandsAndIdentifiersWithCustomIdentifier() {
-        // given
-        ParsingService parsingService = new ParsingService(CalculatorFixture.createCalculator());
 
         // when
-        Calculator calculator = parsingService.parsing(ExpressionFixture.EXPRESSION_WITH_CUSTOM_IDENTIFIER);
+        parsingService.parsing(calculator, ExpressionFixture.EXPRESSION_WITH_CUSTOM_IDENTIFIER);
 
         // then
         assertThat(calculator.getOperands().size()).isEqualTo(3);
@@ -42,72 +43,54 @@ class ParsingServiceTest {
     @Test
     @DisplayName("Invalid input Stars with one slash")
     void invalidInputStarsWithOneSlash() {
-        // given
-        ParsingService parsingService = new ParsingService(CalculatorFixture.createCalculator());
-
         // when
         // then
-        assertThatThrownBy(() -> {parsingService.parsing(ExpressionFixture.EXPRESSION_STARTS_WITH_ONE_SLASH);})
+        assertThatThrownBy(() -> {parsingService.parsing(calculator, ExpressionFixture.EXPRESSION_STARTS_WITH_ONE_SLASH);})
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("Invalid input Stars without number")
     void invalidInputStarsWithoutNumber() {
-        // given
-        ParsingService parsingService = new ParsingService(CalculatorFixture.createCalculator());
-
         // when
         // then
-        assertThatThrownBy(() -> {parsingService.parsing(ExpressionFixture.EXPRESSION_STARTS_WITHOUT_NUMBER);})
+        assertThatThrownBy(() -> {parsingService.parsing(calculator, ExpressionFixture.EXPRESSION_STARTS_WITHOUT_NUMBER);})
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("Invalid input with double custom identifier")
     void invalidInputDoubleCustomIdentifier() {
-        // given
-        ParsingService parsingService = new ParsingService(CalculatorFixture.createCalculator());
-
         // when
         // then
-        assertThatThrownBy(() -> {parsingService.parsing(ExpressionFixture.EXPRESSION_DOUBLE_CUSTOM_IDENTIFIER);})
+        assertThatThrownBy(() -> {parsingService.parsing(calculator, ExpressionFixture.EXPRESSION_DOUBLE_CUSTOM_IDENTIFIER);})
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("Invalid input without \\n")
     void invalidInputCustomWithoutSlashN() {
-        // given
-        ParsingService parsingService = new ParsingService(CalculatorFixture.createCalculator());
-
         // when
         // then
-        assertThatThrownBy(() -> {parsingService.parsing(ExpressionFixture.EXPRESSION_CUSTOM_WITHOUT_SLASH_N);})
+        assertThatThrownBy(() -> {parsingService.parsing(calculator, ExpressionFixture.EXPRESSION_CUSTOM_WITHOUT_SLASH_N);})
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("Invalid input blank custom identifier")
     void invalidInputBlankCustomIdentifier() {
-        // given
-        ParsingService parsingService = new ParsingService(CalculatorFixture.createCalculator());
-
         // when
         // then
-        assertThatThrownBy(() -> {parsingService.parsing(ExpressionFixture.EXPRESSION_BLANK_CUSTOM_IDENTIFIER);})
+        assertThatThrownBy(() -> {parsingService.parsing(calculator, ExpressionFixture.EXPRESSION_BLANK_CUSTOM_IDENTIFIER);})
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("Invalid input invalid identifier")
     void invalidInputInvalidIdentifier() {
-        // given
-        ParsingService parsingService = new ParsingService(CalculatorFixture.createCalculator());
-
         // when
         // then
-        assertThatThrownBy(() -> {parsingService.parsing(ExpressionFixture.EXPRESSION_INVALID_IDENTIFIER);})
+        assertThatThrownBy(() -> {parsingService.parsing(calculator, ExpressionFixture.EXPRESSION_INVALID_IDENTIFIER);})
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
