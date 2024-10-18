e177d40192d47a904519ce4e6c353615ad723132
yoownny <kelosy00@naver.com>
2024-10-18 22:54:50 +0900

    feat(Application): 입력 문자열이 비었는지 확인
    - 입력 문자열이 비어 있는지 확인
    - 비어 있다면 IllegalArgumentException 발생
    
    Closes #3

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,5 +7,9 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
+        if(input.isEmpty()){
+            throw new IllegalArgumentException("올바르지 않은 입력값입니다. 프로그램을 종료합니다.");
+        }
+
     }
 }
