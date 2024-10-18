92c6d4fdf38c77fa8dd62e0f599aadd127694f93
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-10-18 16:38:12 +0900

    :necktie: feat: 입력값에 공백이 존재하는지 검사하는 기능 추가
    - inputValidationCheck(String) 함수 생성
    - checkBlankExist(String) 함수 생성 및 공백이 존재하는지 검사

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,5 +9,17 @@ public class Application {
 
     public static void run(){
         String input = Console.readLine();
+
+        inputValidationCheck(input);
+    }
+
+    public static void inputValidationCheck(String input){
+        checkBlankExist(input);
+    }
+
+    public static void checkBlankExist(String input){
+        if (input.contains(" ")){
+            throw new IllegalArgumentException("입력에 공백이 포함되어 있습니다.");
+        }
     }
 }
