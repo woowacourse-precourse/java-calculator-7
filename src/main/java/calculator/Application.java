55df6069561d652c3eca01739800b0dc53f0e644
zzzyoonnn <dwsrh03@gmail.com>
2024-10-18 16:29:09 +0900

    refactor: 구분자로 잘려진 문자형 배열인 stringArray을 splitStringArray로 변수명 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,9 +12,9 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String[] stringArray = inputString();
+        String[] splitStringArray = inputString();
 
-        int sum = calculateNumber(isNumeric(stringArray));
+        int sum = calculateNumber(isNumeric(splitStringArray));
 
         String answer = getAnswer(sum);
         System.out.println(answer);
