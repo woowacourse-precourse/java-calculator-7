6f6018deae2fa64c495e11aa1e54d948dbbd7616
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-18 15:57:39 +0900

    fix(SeparatorExtractorTest): 생성자에 test용 정규식인 //(.+?)\n을 받을 수 있도록 수정

--- src/test/java/calculator/SeparatorExtractorTest.java
+++ src/test/java/calculator/SeparatorExtractorTest.java
@@ -1,28 +1,27 @@
 package calculator;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class SeparatorExtractorTest {
+    private final SeparatorExtractor extractor = new SeparatorExtractor(Constant.EXTRACT_REGEX_TEST);
+
     @Test
     public void 커스텀_구분자_추출() {
         //given
-        SeparatorExtractor extractor = new SeparatorExtractor();
         List<String> customSeparator = new ArrayList<>();
-        String input = "//;\n4,5:6;//--\n1;2--//^^^\n1;2--";
+        String input = "//;\n4,5:6;//--\n1;2--//^^^\n1;2--3";
+
         //when
         String stringAfterExtract = extractor.extractCustomSeparator(input, customSeparator);
 
         //then
         assertThat(customSeparator.size()).isEqualTo(3);
         assertThat(customSeparator).containsExactly(";", "--", "^^^");
-        assertThat(stringAfterExtract).isEqualTo("4,5:6;1;2--1;2--");
+        assertThat(stringAfterExtract).isEqualTo("4,5:6;1;2--1;2--3");
     }
 
 }
