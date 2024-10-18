d69cc7911afc67a67dd3a90204ca22b96ac745e6
chul5 <och7164@gmail.com>
2024-10-19 02:43:39 +0900

    test: 공백 문자가 들어갔을 때 0이 나오는 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 예외_테스트2() {
+        assertSimpleTest(() -> {
+            run(" ");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
