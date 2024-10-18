555a5119514dcc7f82321d4657a983ba674b0f91
hmmini <pak4184@naver.com>
2024-10-18 16:12:05 +0900

    test: 테스트 케이스 추가
    엣지 테스트 케이스를 고려하여 추가하였습니다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,18 +9,56 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void 기본_구분자를_사용() {
+        assertSimpleTest(() -> {
+            run("10,20:30");
+            assertThat(output()).contains("결과 : 60");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
         });
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
     }
 
     @Test
-    void 예외_테스트() {
+    void 예외_테스트_음수_사용() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_문자열_앞뒤에_구분자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,3,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("!1,2,3@"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_커스텀_구분자_사용() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n1;2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
