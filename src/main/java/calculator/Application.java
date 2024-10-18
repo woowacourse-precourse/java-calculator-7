df17ad650a70840d045412e5d0127b3bfa2ad34e
UmbaDumba <dkdi3104@gmail.com>
2024-10-18 22:39:51 +0900

    add_arrays

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
 public class Application {
+
+    public static int add_arrays(int[] nums){
+
+        int result = 0;
+        for (int num : nums) {
+            result += num;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // add_arrays test
+        int[] test_nums = {1,2,3,4,5};
+
+        System.out.println(add_arrays(test_nums));
+
     }
 }
