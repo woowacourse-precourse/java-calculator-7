9603ec78a9d87ef418aa127e1177fa95e02739b3
rladmstn <oz33177351@gmail.com>
2024-10-18 19:31:27 +0900

    test(validator) : input 검증 시 새로운 테스트 케이스 추가

--- src/test/java/calculator/service/InputValidatorTest.java
+++ src/test/java/calculator/service/InputValidatorTest.java
@@ -22,7 +22,8 @@ class InputValidatorTest {
             "//ab\\n1ab2ab3ba",
             "//abc\\n1abc6bac",
             "//\\n132",
-            "//&\\n1|3&2"
+            "//&\\n1|3&2",
+            "//-a\\n1-2-a4-a"
     })
     @DisplayName("잘못된 input 입력 시, 예외 발생")
     void inputException(String input) {
@@ -42,6 +43,10 @@ class InputValidatorTest {
             "//abc\\n1abc3",
             "//a\\n1a2a3",
             "// \\n1 2 3",
+            "//a\\na10a20a30a",
+            "// \\n  1  3 2",
+            "//ab\\nab100ab10ab1ab",
+            "//a-\\na-100a-"
     })
     @DisplayName("옳은 입력 시, 메서드 정상 종료")
     void correctInput(String input) {
