cb34afe93f4b77ed2a04b53fc6d94c89ccf68c66
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-18 15:49:37 +0900

    documentation : InputManager 주석 추가

--- src/main/java/calculator/domain/preprocess/InputManager.java
+++ src/main/java/calculator/domain/preprocess/InputManager.java
@@ -20,6 +20,7 @@ public class InputManager {
         for(int i=0; i<input.length(); i++) {
             extractedCharacter = input.substring(i, i + 1);
             if (!isInteger(extractedCharacter) && !delimterList.contains(extractedCharacter))
+                //음수가 들어오는 예외를 따로 작업하지 않은 이유는 여기서 "-" 또한 걸러지기 떄문.
                 throw new IllegalArgumentException("구분자 이외의 문자가 있습니다.");
         }
     }
