40686d41fa7b9310f1df1cf415d34e558f85999c
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2024-10-18 14:28:27 +0900

    feat : Calculator.ja ì¹class ì

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Calculator {
+    public int calculate(String input) {
+        // 문자열 검사
+        validateInput(input);
+        // 구분자 선언
+    }
+
+    private void validateInput(String input) {
+
+    }
+}
