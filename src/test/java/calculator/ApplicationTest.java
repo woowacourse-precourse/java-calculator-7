e8032ca8a6a8504501cdec7eb021e75355425cc6
fbehddn <tom0611@naver.com>
2024-10-18 19:18:52 +0900

    test: 커스텀 구분자 관련 테스트케이스 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,5 +1,9 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import calculator.domain.Calculator;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Assertions;
@@ -35,6 +39,39 @@ class ApplicationTest extends NsTest {
             run("1,2:3");
             assertThat(output()).contains("결과 : 6");
         });
+    }
+
+    @Test
+    void 커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 커스텀구분자_처리_테스트() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add("//;\\n1,2;3");
+        Assertions.assertEquals(6, result);
+    }
+
+    @Test
+    void 커스텀_구분자가_맨앞에_존재하지않을때() {
+        Calculator calculator = new Calculator();
+
+        assertThatThrownBy(() -> calculator.add("1;2,3//;\\n"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("구분자가 아닌 값이 있습니다");
+    }
+
+    @Test
+    void 커스텀_구분자로_지정되지_않은_입력값_예외테스트() {
+        Calculator calculator = new Calculator();
+
+        assertThatThrownBy(() -> calculator.add("1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("구분자가 아닌 값이 있습니다");
     }
 
 
