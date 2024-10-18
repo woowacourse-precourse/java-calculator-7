30b20e19f94667741a0a8bf818eac05a9931176d
kgy1008 <gayeon1008@g.skku.edu>
2024-10-18 15:47:40 +0900

    feat: start calculator application

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.controller.Calculator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Calculator calculator = new Calculator();
+        calculator.add();
     }
 }
