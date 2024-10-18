6d9b07570b3819ce3eb1db1f7e4af9f3422a93f9
JongKyeong Kim <oo7bangjk@naver.com>
2024-10-19 00:37:59 +0900

    refact: 문자열 분리 로직 수정

--- src/main/java/calculator/SeparatorManager.java
+++ src/main/java/calculator/SeparatorManager.java
@@ -1,17 +1,13 @@
 package calculator;
 
-import java.util.Arrays;
-import java.util.regex.Pattern;
-import net.bytebuddy.implementation.bytecode.Throw;
-
 public class SeparatorManager {
-    public Separator getSeparator(String originString) {
-        if (originString.startsWith("//")) {
-            String[] parts = originString.split("\n", 2);
-            String delimiter = Pattern.quote(parts[0].substring(2));
+    private static final String customSeparatorStart = "//";
+    private static final String customSeparatorEnd = "\\\\n";
 
+    public Separator getSeparator(String originString) {
+        if (originString.startsWith(customSeparatorStart)) {
             Separator separator = new Separator();
-            separator.addSeparator(Arrays.toString(parts[1].split(delimiter)));
+            separator.addSeparator(extractDelimiter(originString));
 
             return separator;
         }
@@ -20,28 +16,25 @@ public class SeparatorManager {
         }
     }
 
-    public Number getNumber(Separator separator, String originString){
-        Number number = new Number();
-        originString = separateCustomSeparator(originString);
+    public static String extractDelimiter(String input) {
+        String[] parts = input.split(customSeparatorEnd, 2);
+        return parts[0].substring(2);
+    }
 
-        for(int i = 0; i < originString.length(); i = i + 2){
-            if(!Character.isDigit(originString.charAt(i))){
-                throw new IllegalArgumentException();
-            }
-            if(!separator.isValidSeparator(String.valueOf(originString.charAt(i+1)))){
-                throw new IllegalArgumentException();
-            }
-            number.addNumber(originString.charAt(i));
+    public Number getNumber(Separator separator, String originString){
+        if(originString.startsWith(customSeparatorStart)){
+            originString = separateCustomSeparator(originString);
         }
-        return number;
+
+        String regex = String.join("|", separator.getSeparators().stream()
+                .map(sep -> "\\Q" + sep + "\\E")
+                .toArray(String[]::new));
+
+        return new Number(originString.split(regex));
     }
 
     public String separateCustomSeparator(String originString){
-        String[] parts = originString.split("\n", 2);
-        if (parts.length > 1) {
-            return Pattern.quote(parts[1].substring(2));
-        } else {
-            return Pattern.quote(parts[0].substring(2));
-        }
+        String[] parts = originString.split(customSeparatorEnd);
+        return parts[1];
     }
 }
