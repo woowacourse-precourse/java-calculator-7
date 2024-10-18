72c2d2add2300e08baa46b8c8422bc589c55d00d
haeun <haeun9634@naver.com>
2024-10-18 19:13:34 +0900

    feat: 입력 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = getInput(); // 문자열 입력 받기
     }
+
+    private static String getInput() {
+        System.out.print("덧셈할 문자열을 입력해 주세요: ");
+        String input = Console.readLine();
+        return input;
+    }
+
 }
