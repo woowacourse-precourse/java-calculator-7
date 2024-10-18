0c4a8fd3fff046e073cc2dd0376c26f5d4146088
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-19 01:44:19 +0900

    feat(StringValidator): 커스텀 구분자 등록에 - 기호 등록 금지
    - 음수 표현에서 모호한 부분 제거

--- src/main/java/calculator/validator/StringValidator.java
+++ src/main/java/calculator/validator/StringValidator.java
@@ -5,7 +5,7 @@ import calculator.global.exception.ErrorMessage;
 public class StringValidator {
 
     public static void validateCustomDelimiterFormat(String str) {
-        if (str.matches("\\d")) {
+        if (str.matches("[\\d-]")) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_DELIMITER_ERROR.getMessage());
         }
     }
