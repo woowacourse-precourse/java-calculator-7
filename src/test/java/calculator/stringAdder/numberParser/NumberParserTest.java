403486ca7d0324e0798ec725bdf1ba4f50f6658f
jiffyin7 <jiffyin7@gmail.com>
2024-10-18 10:07:38 +0900

    test(NumberParser): 잘못된 입력에 대한 매개변수 테스트 추가
    - 테스트 커버리지 향상 및 코드 중복 감소

--- src/test/java/calculator/stringAdder/numberParser/NumberParserTest.java
+++ src/test/java/calculator/stringAdder/numberParser/NumberParserTest.java
@@ -2,8 +2,12 @@ package calculator.stringAdder.numberParser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /**
  * @author : jiffyin7@gmail.com
@@ -58,5 +62,21 @@ class NumberParserTest {
     String actualMessage = exception.getMessage();
 
     assertTrue(actualMessage.contains(expectedMessage));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideInvalidInputs")
+  void testGetNumbersWithInvalidInput(String input, String[] delimiters) {
+    assertThrows(IllegalArgumentException.class, () -> {
+      numberParser.getNumbers(input, delimiters);
+    });
+  }
+
+  private static Stream<Arguments> provideInvalidInputs() {
+    return Stream.of(
+        Arguments.of("1,2,-3,4", new String[]{",", ":"}),
+        Arguments.of("//;\n1;-2;3", new String[]{",", ":", ";"}),
+        Arguments.of("1:2:-3", new String[]{",", ":"})
+    );
   }
 }
