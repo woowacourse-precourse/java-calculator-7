81e4e1d7c33cfdc400e8860133f17babfb72bda0
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-10-18 20:04:34 +0900

    feat: add functionality to split numbers by default delimiter

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator {
+     public int add(String input) {
+         // 기본 구분자
+         String delimiter = ",|:";
+
+     }
+
+}
