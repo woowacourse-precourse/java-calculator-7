d83850548e2fe2c4bf9d358f0fc33c778b959e6e
sc@t0a14 <gusdn6671@gmail.com>
2024-10-18 10:10:53 +0900

    refactor(StringCalculator) : 커스텀 구분자 & 일반 구분자를 사용하여 문자열의 숫자 합 구하는 코드 리팩토링

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class StringCalculator {
 
-    public void run(){
+    public void run() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
@@ -13,16 +13,27 @@ public class StringCalculator {
         DefaultDelimiter defaultDelimiters = new DefaultDelimiter(defaultDelimiter);
         DelimiterResult delimiterResult = new DelimiterResult(defaultDelimiters.getDefaultDelimiter());
 
-        if(userInput.isCustom()){
+        if (userInput.isCustom()) {
             int customDelimiterEndIndex = userInput.getUserInput().indexOf("\\n");
-            String customDelimiter = userInput.getUserInput().substring(2,customDelimiterEndIndex);
+            String customDelimiter = userInput.getUserInput().substring(2, customDelimiterEndIndex);
             String updateDelimiter = delimiterResult.getDelimiterResult().replace("]", customDelimiter + "]");
             delimiterResult = new DelimiterResult(updateDelimiter);
-
+            String combinedInput = userInput.getUserInput().substring(customDelimiterEndIndex + 2);
+            input = combinedInput;
         }
 
+        DelimitedNumbers delimitedNumbers = new DelimitedNumbers(input, delimiterResult);
 
+        String[] separatedNumbers = delimitedNumbers.getStringNumbers().split(delimiterResult.getDelimiterResult());
+        Numbers numbers = new Numbers(separatedNumbers);
 
+        int sum = 0;
+        for (String stringNumbers : numbers.getNumbers()) {
+            Number number = new Number(stringNumbers);
+            sum += Integer.parseInt(number.getNumber());
+        }
+
+        System.out.println("결과 : " + sum);
 
     }
 
