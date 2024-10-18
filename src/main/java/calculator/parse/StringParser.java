928b44bb1b6c59fb1444f953d24e7cc2a69f04d4
Hacanna42 <dtmdgus901@gmail.com>
2024-10-18 17:12:39 +0900

    feat: implement StringParser

--- src/main/java/calculator/parse/StringParser.java
+++ src/main/java/calculator/parse/StringParser.java
@@ -1,4 +1,44 @@
 package calculator.parse;
 
+import static calculator.constants.DelimiterConstants.CUSTOM_DELIMITER_END;
+import static calculator.constants.DelimiterConstants.CUSTOM_DELIMITER_START;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringParser {
+    public String getCustomDelimiter(String input) {
+        int end_index = input.indexOf(CUSTOM_DELIMITER_END);
+        return input.substring(2, end_index);
+    }
+
+    public int[] convertStringToIntArray(String input) {
+        int start_index = 0;
+        if (input.startsWith(CUSTOM_DELIMITER_START)) { // 커스텀 구분자가 있는 경우
+            start_index = input.indexOf(CUSTOM_DELIMITER_END) + 2; // 그 이후부터 탐색하도록 인덱스 조정
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+        StringBuilder currentNumber = new StringBuilder(); // 현재 숫자를 임시로 저장하는 StringBuilder
+
+        for (int i = start_index; i < input.length(); i++) {
+            char currentChar = input.charAt(i); // 현재 인덱스의 문자
+
+            if (Character.isDigit(currentChar)) { // 현재 문자가 숫자일 경우, currentNumber(버퍼) 에 추가
+                currentNumber.append(currentChar);
+            } else { // 숫자가 아닌 문자가 등장했을 경우, 버퍼에 있던것을 리스트에 추가
+                int number = Integer.parseInt(currentNumber.toString());
+                numbers.add(number);
+                currentNumber.setLength(0); // StringBuilder 초기화
+            }
+        }
+
+        // 버퍼에 남아있는 것 추가
+        if (!currentNumber.isEmpty()) {
+            int number = Integer.parseInt(currentNumber.toString());
+            numbers.add(number);
+        }
+
+        return numbers.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
