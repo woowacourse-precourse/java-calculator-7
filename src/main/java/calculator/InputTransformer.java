057324163da3304c991c84dfc9ca4a767fafcdc7
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-10-18 14:59:45 +0900

    feat: 커스텀 구분자 숫자배열 변환 기능 추가

--- src/main/java/calculator/InputTransformer.java
+++ src/main/java/calculator/InputTransformer.java
@@ -1,22 +1,43 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputTransformer {
-    private int[] transDefaultDelimiter(String userInput){
+    private int[] transDefaultDelimiter(String userInput) {
         String[] stringArray = userInput.split("[:,]");
         int[] intArray = new int[stringArray.length];
 
-        for (int i = 0; i < stringArray.length; i++){
+        for (int i = 0; i < stringArray.length; i++) {
             intArray[i] = Integer.parseInt(stringArray[i]);
         }
         return intArray;
     }
 
-    private int[] transCustomDelimiter(String userInput){
-        return new int[0];
+    private int[] transCustomDelimiter(String userInput) {
+        String regex = "^//(.*?)\\\\n";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(userInput);
+        boolean a = matcher.find();
+
+        String delimiter = matcher.group(1);
+        int prefixLength = matcher.group().length();
+        String[] stringArray = userInput.substring(prefixLength).split(delimiter);
+        int[] intArray = new int[stringArray.length];
+
+        for (int i = 0; i < stringArray.length; i++) {
+            if (stringArray[i].isEmpty()) {
+                intArray[i] = 0;
+            } else {
+                intArray[i] = Integer.parseInt(stringArray[i]);
+            }
+        }
+
+        return intArray;
     }
 
-    public int[] stringToIntArray(String userInput, DelimiterType delimiterType){
-        if (delimiterType == DelimiterType.EMPTY) return new int[0];
+    public int[] stringToIntArray(String userInput, DelimiterType delimiterType) {
+        if (delimiterType == DelimiterType.EMPTY) return new int[]{};
         if (delimiterType == DelimiterType.DEFAULT) return transDefaultDelimiter(userInput);
         if (delimiterType == DelimiterType.CUSTOM) return transCustomDelimiter(userInput);
         return null;
