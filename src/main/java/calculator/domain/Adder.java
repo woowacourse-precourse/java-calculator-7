5d53eb02e718fcebde0277027875f5dd5d532ac9
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-10-18 21:42:35 +0900

    feat: 숫자들을 더하는 기능
    입력받은 숫자들을 모두 더해서 반환

--- src/main/java/calculator/domain/Adder.java
+++ src/main/java/calculator/domain/Adder.java
@@ -2,7 +2,20 @@ package calculator.domain;
 
 public class Adder {
 
+    //숫자 리스트의 합계를 계산하여 반환
     public int addAll(String numbers) {
-        return 0;
+        System.out.println(numbers);
+        //validateNumbers(numbers);  // 숫자 리스트 유효성 검사
+        int total = 0;
+
+        String[] numberArray = numbers.split(",");
+
+        for (String number : numberArray) {
+            if (!number.isEmpty()) {
+                total += Integer.parseInt(number.trim());
+            }
+        }
+
+        return total;
     }
 }
