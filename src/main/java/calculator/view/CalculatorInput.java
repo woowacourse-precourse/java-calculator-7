0ce032ee7c243862d5473cb02ce7bfdb59fcf6a3
mete0rfish <sungwon326@naver.com>
2024-10-18 10:26:27 +0900

    fix(#1): 입력 전, 출력 문구 추가

--- src/main/java/calculator/view/CalculatorInput.java
+++ src/main/java/calculator/view/CalculatorInput.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class CalculatorInput {
 
     public static String inputString() {
+        System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
         Console.close();
         return input;
