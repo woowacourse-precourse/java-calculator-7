82e77562d501b9d6f2bafbc992005852fdaf8494
김멍몽 <wjdahrrla@gmail.com>
2024-10-18 21:38:56 +0900

    refactor(view): ViewMessage를 만들어서 View에 필요한 코멘트 관리로 리팩토링
    ViewMessage를 만들어서 View에 필요한 코멘트를 관리한다.
    
    변경된 사항:
    - ViewMessage 생성
    - 위 변경에 맞게 InputView, OutputView 리팩토링

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,7 +1,10 @@
 package calculator.view;
 
+import static calculator.view.ViewMessage.OUTPUT_MESSAGE;
+
 public class OutputView {
     public static void printResult(int result) {
-        System.out.println("결과 : " + result);
+        StringBuilder sb = new StringBuilder();
+        System.out.println(sb.append(OUTPUT_MESSAGE).append(result).toString());
     }
 }
