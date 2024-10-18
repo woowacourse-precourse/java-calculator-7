94e01fd5f597dc13fa0bc0c99a23d40aaf951636
LEEJaeHyeok97 <hazardous10@naver.com>
2024-10-18 21:57:05 +0900

    feat: 커스텀 구분자에 따른 합 반환 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,24 +5,35 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         // 기본(쉼표, 콜론) 구분자로 각 숫자의 합을 반환하는 기능
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
 
+        // 빈 문자열 처리
         if (str.equals("")) {
             System.out.println("0");
             return;
         }
 
-        int ans = 0;
-
-        String[] split = str.split("[,:]");
-
-        for (String s : split) {
-            ans += Integer.parseInt(s);
+        // 커스텀 구분자 처리
+        String customDelimiter = ",|:";
+        if (str.startsWith("//")) {
+            int delimiterIndex = str.indexOf("\n");
+            customDelimiter = str.substring(2, delimiterIndex);
+            str = str.substring(delimiterIndex + 1);
         }
 
-        System.out.println("결과 : " + ans);
+        // 커스텀 문자 또는 기본 구분자로 분리
+        String[] numbers = str.split(customDelimiter);
+
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        System.out.println("결과 : " + sum);
+
 
     }
 }
