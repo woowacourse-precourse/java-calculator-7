94225cfddf92e3cd86cd68e2c5be4157ddd5ec07
soeun2537 <soeun2537@gmail.com>
2024-10-18 16:05:00 +0900

    docs: Adder 클래스 설명 주석 추가

--- src/main/java/calculator/Adder.java
+++ src/main/java/calculator/Adder.java
@@ -2,10 +2,13 @@ package calculator;
 
 import java.util.List;
 
+/**
+ * 주어진 숫자 리스트의 합계를 계산하는 클래스입니다.
+ */
 public class Adder {
 
     /**
-     * 숫자들의 합 계산
+     * 정수 리스트의 합 계산
      */
     public int sum(List<Integer> numbers) {
         int sum = 0;
