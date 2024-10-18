d2fa698ef90116a781a888844733e0f13bcf5bcc
skhnpark <skhnpark125@gmail.com>
2024-10-19 06:20:38 +0900

    feat: 사용자 입력 받는 기능 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,7 +1,16 @@
 package calculator.controller;
 
+import calculator.model.UserInput;
 import calculator.view.InputView;
 
 public class CalculatorController {
-    String userInput = InputView.inputUserString();
+    private final UserInput userInput;
+
+    public CalculatorController() {
+        this.userInput = new UserInput();
+    }
+
+    public void run() {
+        userInput.setUserInput(InputView.inputUserString());
+    }
 }
