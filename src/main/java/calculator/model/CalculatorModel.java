f651ea7f63a3f903225b675e5ca884b54c623fae
kim jihyeon <jihyeon546@gmail.com>
2024-10-18 19:58:39 +0900

    feat(model): 커스텀 구분자와 숫자 분리 기능 추가

--- src/main/java/calculator/model/CalculatorModel.java
+++ src/main/java/calculator/model/CalculatorModel.java
@@ -1,10 +1,20 @@
 package calculator.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CalculatorModel {
 
     public String[] StringSplitter(String input) {
 
         String delimiter = ",|:";
+        if (input.startsWith("//")) {
+            Matcher matcher = Pattern.compile("//(.+)\n(.+)").matcher(input);
+            if (matcher.find()) {
+                delimiter = Pattern.quote(matcher.group(1));
+                input = matcher.group(2);
+            }
+        }
         String[] numbers = input.split(delimiter);
 
         return numbers;
