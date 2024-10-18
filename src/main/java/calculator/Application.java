f2a6488179928baccb394bea52cb28888cf174c8
minjun <ijmtylhtf@naver.com>
2024-10-18 10:11:53 +0900

    refactor : 배열 원소 합 계산 함수 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,11 +9,16 @@ public class Application {
         String input = Console.readLine();
         String[] splitString = input.split(ORIGINAL_SPLITTER_REGEX);
         int sum = 0;
-        if(splitString.length > 1){
-            for (String s : splitString) {
-                sum += Integer.parseInt(s);
-            }
+        if(splitString.length >= 1) {
+            sum = calculateSum(splitString, sum);
         }
         System.out.println("결과 : " + sum);
+    }
+
+    private static int calculateSum(String[] splitString, int sum){
+        for (String s : splitString) {
+             sum += Integer.parseInt(s);
+        }
+        return sum;
     }
 }
