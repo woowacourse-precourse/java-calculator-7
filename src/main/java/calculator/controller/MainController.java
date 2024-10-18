3fa8e94bf4f6c3c4be01ddae119980f78e4cf7c5
DESKTOP-GRC68NA\foski <foskingson@naver.com>
2024-10-18 15:33:41 +0900

    style : 숫자 합 기능 함수명 수정

--- src/main/java/calculator/controller/MainController.java
+++ src/main/java/calculator/controller/MainController.java
@@ -8,8 +8,7 @@ import calculator.view.OutputView;
 public class MainController {
     public void run() {
 		String input = InputView.readInput("덧셈할 문자열을 입력해 주세요.");
-		String delimiter = InputValidator.extractDelimiter(input);
-        int result = StringCalculator.sumNumbers(input, delimiter); 
+        int result = StringCalculator.sum(input); 
         OutputView.showResult(result); 
 	}
 }
--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -1,8 +1,14 @@
 package calculator.model;
 
+import calculator.view.InputValidator;
+
 public class StringCalculator {
-    public static int sumNumbers(String inputValue, String delimiter) {
-        String[] numbers = inputValue.split(delimiter);
+    public static int sum(String input) {
+        if(input.isEmpty() || input.isBlank()){
+            return 0;
+        }
+        String delimiter = InputValidator.extractDelimiter(input);
+        String[] numbers = input.split(delimiter);
         int sum = 0;
         for (String numStr : numbers) {
             sum += Integer.parseInt(numStr.trim()); 
