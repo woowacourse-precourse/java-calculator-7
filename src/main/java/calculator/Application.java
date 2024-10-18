41a3906a41bca1898aeb340f9afb954f94098812
0tae <choiyt3465@naver.com>
2024-10-19 03:14:51 +0900

    feat: 애플리케이션 Main 코드 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.math.BigInteger;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ApplicationViewer.outputLine("덧셈할 문자열을 입력해 주세요.");
+        String givenInput = Console.readLine();
+        Calculator<BigInteger> calculator = new StringSummationCalculator();
+        BigInteger result = calculator.calculate(givenInput);
+
+        ApplicationViewer.outputLine("결과 : "+result);
     }
 }
