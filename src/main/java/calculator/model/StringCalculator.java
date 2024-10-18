71dda129eca3a896bfe80d8fc507ece282ecc19e
DESKTOP-GRC68NA\foski <foskingson@naver.com>
2024-10-18 15:54:55 +0900

    fix : 숫자 합 기능 커스텀 구분자인 경우도 가능하게 수정

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -2,13 +2,15 @@ package calculator.model;
 
 import calculator.view.InputValidator;
 
-public class StringCalculator {
+public class StringCalculator {    
     public static int sum(String input) {
+
         if(input.isEmpty() || input.isBlank()){
             return 0;
         }
         String delimiter = InputValidator.extractDelimiter(input);
-        String[] numbers = input.split(delimiter);
+        String[] numbers = InputValidator.validateNumbers(input,delimiter).split(delimiter);
+       
         int sum = 0;
         for (String numStr : numbers) {
             sum += Integer.parseInt(numStr.trim()); 
