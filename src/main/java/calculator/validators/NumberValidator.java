7a2dea3ce2dbf6f63d0c47c65cd3da1c5647edd7
wogus <ind07152@naver.com>
2024-10-18 13:33:32 +0900

    refactor(NumberValidator): 하드 코딩을 상수화
    하드 코딩된 부분을 상수화하여 통일한다.

--- src/main/java/calculator/validators/NumberValidator.java
+++ src/main/java/calculator/validators/NumberValidator.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 public final class NumberValidator {
 
+    private static int ZERO = 0;
+
     // 모든 값은 양수여야한다.
     public static void numberValidate(List<Integer> numbers) {
         for (Integer number : numbers) {
-            if (number < 0) {
+            if (number < ZERO) {
                 throw new IllegalArgumentException();
             }
         }
