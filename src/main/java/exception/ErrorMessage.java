6660a51a4b6fbd47df3b594bfb72618b9a949430
aaahyunseo <hs030608@naver.com>
2024-10-18 20:53:31 +0900

    refactor(ErrorMessage): 에러 변수명 변경

--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -2,7 +2,7 @@ package exception;
 
 public enum ErrorMessage {
     LENGTH("입력된 숫자의 길이가 허용 범위를 초과하였습니다."),
-    INVALID_INPUT("숫자 외의 값은 입력할 수 없습니다."),
+    INVALID_TYPE("숫자 외의 값은 입력할 수 없습니다."),
     INVALID_NUMBER("0과 음수는 입력할 수 없습니다.");
 
     private final String errorMessage;
