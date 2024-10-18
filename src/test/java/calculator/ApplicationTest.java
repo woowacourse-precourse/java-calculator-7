5ac8ed771983f061a73e28bf5ab88912f9ab6726
WhitePiano <startoflateness@gmail.com>
2024-10-18 21:20:45 +0900

    test(application): complement test (write more)

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,15 +1,15 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
     @Test
-    void 커스텀_구분자_사용() {
+    void 커스텀_구분자_테스트() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
@@ -17,11 +17,53 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 음수_입력_오류_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 빈_문자열_테스트() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 숫자_하나_테스트() {
+        assertSimpleTest(() -> {
+            run("1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 기본_구분자_테스트() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 중간_빈_숫자에_오류_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1,,3"))
+                .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 뒷_빈_숫자에_오류_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("//;\\n1;2;3;"))
+                .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 앞_빈_숫자에_오류_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("//;\\n;1;2;3"))
+                .isInstanceOf(IllegalArgumentException.class));
     }
 
     @Override
