c9cd38089f214fcea06bcc37b76dac49288cf70c
minzzun99 <skg06123@naver.com>
2024-10-18 18:59:33 +0900

    refactor(view): 클래스 이름 변경
    - 오타로 인한 클래스 이름 변경 Ouputview -> OutputView

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,6 +1,6 @@
 package calculator.view;
 
-public class OuputView {
+public class OutputView {
     private static final String RESULT_MESSAGE = "결과 : ";
 
     public static void printResult(int result) {
