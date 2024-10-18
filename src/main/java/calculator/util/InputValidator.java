0f4c50b0179569f9fab3b83987c42ddee85c8a6c
DongchannN <ldc991104@gmail.com>
2024-10-18 20:39:26 +0900

    fix(InputValidator): 유효한 계산식 문자 구성에 소수점 추가
    - 소수의 입력을 고려하여 '.'입력이 예외를 던지지 않도록 수정

--- src/main/java/calculator/util/InputValidator.java
+++ src/main/java/calculator/util/InputValidator.java
@@ -6,7 +6,7 @@ public class InputValidator {
 
     public static void validateExpression(String expression, Set<Character> delimiters) {
         for (Character c : expression.toCharArray()) {
-            if (!delimiters.contains(c) && !Character.isDigit(c)) {
+            if (!delimiters.contains(c) && !Character.isDigit(c) && c != '.') {
                 throw new IllegalArgumentException("유효하지 않은 계산식입니다.");
             }
         }
