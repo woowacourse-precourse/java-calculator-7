81676c0f63ba9dfeef1034573531d99cea52e81f
jjjjjoon <mjj7343@gmail.com>
2024-10-18 15:08:32 +0900

    feat(stringsplitter): 디폴트 구분자
    쉼표와 콜론을 구분자를 기준으로 분리한 숫자들 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,21 +3,24 @@ package calculator;
 public class Application {
     //의존성 주입
     private final InputHandler inputhandler;
+    private final StringSplitter stringsplitter;
 
     //의존성 주입된 inputhandler객체 생성
-    public  Application(InputHandler inputhandler) {
+    public  Application(InputHandler inputhandler, StringSplitter stringsplitter) {
         this.inputhandler = inputhandler;
+        this.stringsplitter = stringsplitter;
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Application application = new Application(new InputHandler());
-        String input = application.run();
+        Application application = new Application(new InputHandler(), new StringSplitter());
+        String[] input = application.run();
 
     }
 
     //사용자 입력 문자열 반환
-    public String run() {
-        return inputhandler.UserInput();
+    public String[] run() {
+        String[] result = stringsplitter.DefaultSplitter(inputhandler.UserInput());
+        return result;
     }
 }
---
+++ src/main/java/calculator/StringSplitter.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class StringSplitter {
+    public String[] DefaultSplitter(String input) {
+        String[] result = input.split("[,:]");
+        return result;
+    }
+
+//    public String CustomSplitter(String input) {
+//
+//    }
+}
