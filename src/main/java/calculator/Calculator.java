9f0fa8e49cc8ad76e0f579822c5947ee3b12215a
kwon <rnjsdndud00@naver.com>
2024-10-18 14:42:09 +0900

    feat(calculator): 구분자로 나눈 숫자 합계 구하기 기능 추가

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,42 @@
+package calculator;
+
+public class Calculator {
+
+    CustomSeparator customSeparator = new CustomSeparator();
+
+    public int calculate(String st) {
+        // 입력값이 없는 경우에 대한 예외 처리
+        if(st.isEmpty()) return 0;
+
+        try {
+            // 입력값이 1자리인 경우 그 숫자 그대로 반환
+            if (st.length() == 1) return Integer.parseInt(st);
+
+            // 입력값이 숫자가 아니라면 IllegalArgumentException 에러 발생
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+
+        // 구분자 생성
+        String separator = customSeparator.separator(st);
+
+        // 커스텀 구분자가 있을 경우 \n 이후의 문자열부터 슬라이싱
+        if(st.startsWith("/")){
+            st = st.substring(st.indexOf("n") + 1);
+        }
+
+        // 구분자를 사용해 각 문자를 슬라이싱한다.
+        String[] split = st.split(separator);
+
+        // 더해진 값 리턴
+        return intSum(split);
+    }
+
+    public int intSum(String[] st) {
+        int answer = 0;
+        for(String s : st) {
+            answer += Integer.parseInt(s);
+        }
+        return answer;
+    }
+}
--- src/main/java/calculator/CustomSeparator.java
+++ src/main/java/calculator/CustomSeparator.java
@@ -3,16 +3,17 @@ package calculator;
 public class CustomSeparator {
 
     public String separator(String st) {
+        // 기본 구분자
         String separator = "[,:";
+
+        // "//"와 "\n" 사이에 있는 값을 하나씩 구분자에 추가해주기 위해 배열로 변환
         String[] stSplit = st.split("");
-        for (int i = 0; i < stSplit.length - 1; i++) {
-            if (stSplit[i].equals("/") && stSplit[i + 1].equals("/")) {
-                for (int j = i + 2; j < stSplit.length; j++) {
-                    if (stSplit[j].equals("\\") && stSplit[j + 1].equals("n")) {
-                        break;
-                    }
-                    separator += stSplit[j];
-                }
+
+        // 만약 "/"로 시작하면 커스텀 구분자가 존재한다는 의미
+        if (st.startsWith("/")) {
+            // "\n" 문자가 나올 때까지 모든 문자를 separator에 추가
+            for (int i = 2; i < st.indexOf("\\"); i++) {
+                separator += stSplit[i];
             }
         }
         separator += "]";
