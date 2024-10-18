399891f6418c7aa711f482a001ea6cd5c11d4ce2
DESKTOP-GRC68NA\foski <foskingson@naver.com>
2024-10-18 14:33:02 +0900

    style : 입출력 스타일 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static String readInput(String message) {
-        System.out.print(message);
+        System.out.println(message);
         String input = Console.readLine(); 
         InputValidator.validateInput(input); 
         return input; 
