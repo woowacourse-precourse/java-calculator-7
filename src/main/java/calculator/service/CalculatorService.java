4a34cc9d56e7b711dac88746b3d770ea14026b67
chae02yo <chae02kim@naver.com>
2024-10-18 18:11:46 +0900

    feat: add separation of numbers
    - separate numbers by custom separator

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class CalculatorService {
     String number = "";
     int result = 0;
+    String customSeparator = "";
+    String checkSeparator = "";
     String printNumList = "";
 
     ArrayList<String> numList = new ArrayList<>();
@@ -13,26 +15,47 @@ public class CalculatorService {
         if (input.isEmpty()) { // 빈 입력값 처리
             return "0";
         }
-        else { // 쉼표와 콜론 구분자를 기준으로 숫자 분리
-            for (int i=0; i<input.length(); i++) {
+        else {
+            int i=0;
+            // 커스텀 구분자 지정
+            if (input.charAt(i) == '/' && input.charAt(i+1) == '/') {
+                i=2;
+                while(input.charAt(i) != '\\' && input.charAt(i+1) != 'n') {
+                    customSeparator += input.charAt(i);
+                    i++;
+                }
+                i += 2;
+            }
+
+            for (;i<input.length(); i++) {
                 char c = input.charAt(i);
                 switch (c) {
-                    case ',':
-                    case ';':
+                    case ',': // 쉼표와 콜론 구분자를 기준으로 숫자 분리
+                    case ':':
                         numList.add(number);
                         number = "";
                         continue;
                     default:
-                        number += c;
+                        if ((int)c >= 48 && (int)c <= 57) {
+                            if (checkSeparator.equals(customSeparator)) { // 커스텀 구분자를 기준으로 숫자 분리
+                                numList.add(number);
+                                number = "";
+                                checkSeparator = "";
+                            }
+                            number += c;
+                        }
+                        else {
+                            checkSeparator += c;
+                        }
                         break;
                 }
             }
             numList.add(number);
 
-            for (int i=0; i<numList.size(); i++) {
-                printNumList += numList.get(i) + " ";
+            System.out.println("커스텀 구분자: " + customSeparator + "\n");
+            for (int j=0; j<numList.size(); j++) {
+                printNumList += numList.get(j) + " ";
             }
-
             return printNumList;
         }
     }
