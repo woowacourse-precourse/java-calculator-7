624e5c862019a05790e029a731f8763fcf02414c
ljhee92 <weilainib7@gmail.com>
2024-10-18 21:43:00 +0900

    feat: 덧셈, 출력 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,10 +1,14 @@
 package calculator.controller;
 
+import calculator.model.Calculator;
 import calculator.model.CustomSeparator;
 import calculator.model.DefaultSeparator;
+import calculator.model.Numbers;
+import calculator.validator.InputValidator;
 import calculator.view.InputReader;
 import calculator.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CalculatorController {
@@ -12,20 +16,37 @@ public class CalculatorController {
         OutputView.displayStart();
         String input = InputReader.inputMessage();
 
+        Numbers numbers = new Numbers(getNumbers(input));
+
+        int result = new Calculator().calculateSum(numbers);
+        OutputView.displayResult(result);
+    } // run
+
+    public List<Integer> getNumbers(String input) {
+        List<Integer> numbers = new ArrayList<>();
         if (containsCustomSeparator(input)) {
-            new CustomSeparator().separate(input);
+            numbers = new CustomSeparator().separate(input);
         } // end if
 
         if (containsDefaultSeparator(input)) {
-            new DefaultSeparator().separate(input);
+            numbers = new DefaultSeparator().separate(input);
         } // end if
-    } // run
+
+        if (notContainsSeparator(input)) {
+            numbers = List.of(new InputValidator().validate(input));
+        } // end if
+        return numbers;
+    } // getNumbers
 
     public boolean containsCustomSeparator(String input) {
         return input.matches("^//(.*)\\\\n(.*)");
     } // containsCustomSeparator
 
     public boolean containsDefaultSeparator(String input) {
-        return input.contains(",") || input.contains(":");
+        return !containsCustomSeparator(input) && (input.contains(",") || input.contains(":"));
     } // containsDefaultSeparator
+
+    public boolean notContainsSeparator(String input) {
+        return !containsDefaultSeparator(input) && !containsCustomSeparator(input);
+    } // notContainsSeparator
 } // class
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void displayStart() {
         OutputWriter.displayMessage("덧셈할 문자열을 입력해 주세요.");
     } // displayStart
+
+    public static void displayResult(int result) {
+        OutputWriter.displayMessage("결과 : " + result);
+    } // displayResult
 } // class
