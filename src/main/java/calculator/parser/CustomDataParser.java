37e2a9e77e35a1b67010462e64a3ad362c082715
joonwan <joonwan1024@naver.com>
2024-10-18 16:30:43 +0900

    refactor: 기본 포멧과 커스텀 포멧 파서 분리

--- src/main/java/calculator/parser/CustomDataParser.java
+++ src/main/java/calculator/parser/CustomDataParser.java
@@ -1,15 +1,50 @@
 package calculator.parser;
 
+import calculator.Validator;
 import calculator.dto.Data;
 
 public class CustomDataParser implements DataParser {
+
+    private static final String REGEX = "\\\\n";
+    private static final String PREFIX = "//";
+
     @Override
     public boolean isSupport(String inputData) {
-        return false;
+        return inputData.startsWith(PREFIX);
     }
 
     @Override
     public Data parseData(String inputData) {
-        return null;
+        Validator.checkValidCustomFormat(inputData);
+
+        char[] separators = getCustomSeparator(inputData);
+        char[] contents = getContents(inputData);
+
+        Validator.checkValidContents(separators, contents);
+
+        String contentsString = getContentString(contents, separators);
+        String separatorsString = getSeparatorsString(separators);
+        return new Data(separatorsString, contentsString);
+
+    }
+
+    private char[] getCustomSeparator(String inputData) {
+        String[] split = inputData.split(REGEX);
+        String prefix = split[0];
+        if (prefix.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        char separator = prefix.charAt(2);
+        return new char[]{separator, ':', ','};
+    }
+
+    private char[] getContents(String inputData) {
+
+        String[] data = inputData.split(REGEX);
+        if (data.length == 1) {
+            return new char[]{'0'};
+        }
+        String datum = data[1];
+        return datum.toCharArray();
     }
 }
--- src/main/java/calculator/parser/DefaultDataParser.java
+++ src/main/java/calculator/parser/DefaultDataParser.java
@@ -1,15 +1,36 @@
 package calculator.parser;
 
+import calculator.Validator;
 import calculator.dto.Data;
 
 public class DefaultDataParser implements DataParser {
+
+    private static final String PREFIX = "//";
+
     @Override
     public boolean isSupport(String inputData) {
-        return false;
+        return !inputData.startsWith(PREFIX);
     }
 
     @Override
     public Data parseData(String inputData) {
-        return null;
+        char[] separators = getSeparator(inputData);
+        char[] contents = getContents(inputData);
+        Validator.checkValidContents(separators, contents);
+
+        String contentsString = getContentString(contents, separators);
+        String separatorsString = getSeparatorsString(separators);
+        return new Data(separatorsString, contentsString);
+    }
+
+    private static char[] getSeparator(String inputData) {
+        return new char[]{',', ':'};
+    }
+
+    private char[] getContents(String inputData) {
+        if (inputData.isEmpty()) {
+            return new char[]{'0'};
+        }
+        return inputData.toCharArray();
     }
 }
