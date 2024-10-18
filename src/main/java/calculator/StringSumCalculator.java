9e8d41e08b00f7cfa597f19e57877c557743b380
minseo <alstj778@gmail.com>
2024-10-18 15:14:21 +0900

    feat: 계산 결과를 형식에 맞게 출력한다

--- src/main/java/calculator/StringSumCalculator.java
+++ src/main/java/calculator/StringSumCalculator.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class StringSumCalculator {
     private static final String START_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String RESULT_MESSAGE_PREFIX = "결과 : ";
     private static InputHandler inputHandler;
 
     StringSumCalculator() {
@@ -16,9 +17,17 @@ public class StringSumCalculator {
         String inputString = inputHandler.inputString();
         List<Integer> numbers = inputHandler.extractNumbers(inputString);
         int result = sum(numbers);
+
+        printResult(result);
     }
 
     private int sum(List<Integer> numbers) {
-        return numbers.stream().mapToInt(Integer::intValue).sum();
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private void printResult(int result) {
+        System.out.println(RESULT_MESSAGE_PREFIX + result);
     }
 }
