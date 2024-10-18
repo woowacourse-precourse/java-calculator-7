6363e3045d41ee8f005363d026350be0088b8939
StoneCAU <seokwoo1999@naver.com>
2024-10-18 15:16:45 +0900

    refactor: 불필요한 예외처리 제거

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -12,21 +12,14 @@ public class Calculator {
     private static String CUSTOM_DELIMITER = "";
 
     public static int calculate(final String input) {
-        if (input.isEmpty()) throw new IllegalArgumentException("[ERROR] 입력이 비어 있습니다. 값을 입력해 주세요.");
         List<String> parsedNumbers = parseNumbers(input);
 
         return parsedNumbers.stream()
-                .mapToInt(num -> {
-                    if (isInvalidInput(num)) throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력입니다.");
-
-                    return Integer.parseInt(num);
-                })
+                .mapToInt(Integer::parseInt)
                 .sum();
     }
 
     private static List<String> parseNumbers(final String input) {
-        if (!isEndWithDigit(input)) throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력입니다.");
-
         if (hasCustomDelimiter(input)) {
             setCustomDelimiter(input);
 
@@ -50,29 +43,11 @@ public class Calculator {
 
         if(CUSTOM_DELIMITER.equals("\\")) CUSTOM_DELIMITER += "\\";
 
-        if(isDuplicatedDelimiter()) throw new IllegalArgumentException("[ERROR] 중복된 구분자 지정입니다.");
         if(isContainedDigit()) throw new IllegalArgumentException("[ERROR] 숫자를 포함한 구분자는 지정할 수 없습니다.");
-    }
-
-    private static boolean isEndWithDigit(final String input) {
-        char lastChar = input.charAt(input.length() - 1);
-
-        return Character.isDigit(lastChar);
-    }
-
-    private static boolean isInvalidInput(final String str) {
-        return !str.matches("(\\d)+");
     }
 
     private static boolean isContainedDigit() {
         return CUSTOM_DELIMITER.matches(".*\\d.*");
-    }
-
-    private static boolean isDuplicatedDelimiter() {
-        return (CUSTOM_DELIMITER.contains(FIRST_DELIMITER)
-            || CUSTOM_DELIMITER.contains(SECOND_DELIMITER)
-            || CUSTOM_DELIMITER.contains(CUSTOM_DELIMITER_PREFIX)
-            || CUSTOM_DELIMITER.contains(CUSTOM_DELIMITER_SUFFIX));
     }
 
     private static boolean hasCustomDelimiter(final String input) {
