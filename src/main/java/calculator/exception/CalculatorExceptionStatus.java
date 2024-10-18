a02bb81ed0ab493e41ff898c223af0ded82aa7ae
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-18 14:18:00 +0900

    [feat] : 계산기 예외 클래스 및 View 클래스 추가 - README에서 작성한 예외상황들 enum에 추가 - IllegalArgumentException을 상속한 CalculatorException 클래스 생성 - 입출력 View 클래스 생성

---
+++ src/main/java/calculator/exception/CalculatorExceptionStatus.java
@@ -0,0 +1,20 @@
+package calculator.exception;
+
+public enum CalculatorExceptionStatus {
+
+    INVALID_INPUT_STRING("입력 문자열이 올바르지 않습니다"),
+    INVALID_DEFAULT_DELIMITER_EXPRESSION("잘못된 입력 형식입니다"),
+    INVALID_CUSTOM_DELIMITER_EXPRESSION("수식에 올바르지 않은 커스텀 구분자가 들어갔습니다"),
+    INVALID_NEGATIVE_INTEGER("양수만 입력 가능합니다"),
+    INVALID_CUSTOM_DELIMITER_INTEGER("커스텀 구분자로 숫자를 사용할 수 없습니다.");
+
+    private final String errorMessage;
+
+    CalculatorExceptionStatus(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getMessage() {
+        return this.errorMessage;
+    }
+}
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+    private static final String OUTPUT_MESSAGE = "결과 : ";
+
+    public static void getOutputMessage(int result){
+        System.out.println(OUTPUT_MESSAGE + result);
+    }
+}
