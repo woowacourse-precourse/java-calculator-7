f5da9f0a5956749e498e3a9a2ff134b4eafb47c4
momoysm <90163578+momoysm@users.noreply.github.com>
2024-10-18 20:33:23 +0900

    feat: 사용자의 입력 구현 및 null, "", " " 검증

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import static calculator.StringCalculator.start;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.print("계산할 문자열을 입력하세요: ");
+        String input = Console.readLine();
+
+        int result = start(input);
+        System.out.println("결과 : " + result);
+
     }
 }
---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class StringCalculator {
+
+    public static int start(String input) {
+
+        // 빈 문자열 검증
+        if (input.isBlank()) {
+            return 0;
+        }
+
+        return 0;
+    }
+
+}
