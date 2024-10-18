54361e3254dcf437e8afbc6224a8507fc276379e
minw0095 <kmu6898@naver.com>
2024-10-18 15:27:40 +0900

    feat: 커스텀 구분자 사용시 구현

--- src/main/java/calculator/InputValidation.java
+++ src/main/java/calculator/InputValidation.java
@@ -19,6 +19,16 @@ public class InputValidation {
             numbers = userNumbers.split(separator);
         }
         
+        if (userNumbers.contains("/")) {
+            if ((userNumbers.substring(0, 2) + userNumbers.substring(3, 5)).equals("//\\n")) {
+                separator = "[" + normalSeparator + userNumbers.charAt(2) + "]";
+                numbers = userNumbers.substring(5).split(separator);
+            } else {
+                throw new IllegalArgumentException();
+            }
+
+        }
+
     }
 
 }
