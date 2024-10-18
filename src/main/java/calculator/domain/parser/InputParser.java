d0a49eb536237e316d04286fd88e67bca4aca5cc
gladhee <gladhee@icloud.com>
2024-10-18 21:48:45 +0900

    refactor(InputParser): 구분자 enum으로 변경에 따른 로직수정

--- src/main/java/calculator/domain/parser/InputParser.java
+++ src/main/java/calculator/domain/parser/InputParser.java
@@ -1,5 +1,10 @@
 package calculator.domain.parser;
 
+import static calculator.domain.CalculatorConstants.CUSTOM_DELIMITER_PREFIX;
+import static calculator.domain.CalculatorConstants.CUSTOM_DELIMITER_SUFFIX;
+import static calculator.domain.CalculatorConstants.DEFAULT_DELIMITER_COLON;
+import static calculator.domain.CalculatorConstants.DEFAULT_DELIMITER_COMMA;
+
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -7,21 +12,19 @@ import java.util.Arrays;
 
 public class InputParser {
 
-    private static final String[] DEFAULT_DELIMITERS = {",", ":"};
-    private static final String CUSTOM_DELIMITER_PREFIX = "//";
-    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
-    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
-
     public ParsedInput parse(String input) {
         if (input == null || input.isEmpty()) {
             return new ParsedInput(new String[]{"0"});
         }
 
         String numbers = input;
-        Set<String> delimiters = new HashSet<>(Arrays.asList(DEFAULT_DELIMITERS));
+        Set<String> delimiters = new HashSet<>();
 
-        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
-            int lastDelimiterIndex = input.lastIndexOf(CUSTOM_DELIMITER_SUFFIX);
+        delimiters.add(DEFAULT_DELIMITER_COLON.getValue());
+        delimiters.add(DEFAULT_DELIMITER_COMMA.getValue());
+
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX.getValue())) {
+            int lastDelimiterIndex = input.lastIndexOf(CUSTOM_DELIMITER_SUFFIX.getValue());
 
             if (lastDelimiterIndex == -1) {
                 throw new IllegalArgumentException("커스텀 구분자 지정이 잘못되었습니다.");
