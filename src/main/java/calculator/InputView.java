c09a24185d4f2a46926755533f5547419bd02a7f
MinJae00 <exceedmin@gmail.com>
2024-10-18 21:23:12 +0900

    refact: 입,출력 형식에 맞춰 문구 수정

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -1,9 +1,8 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
 public class InputView {
-
     public String getInput(){
-        System.out.println("문자열을 입력하세요:");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         return input;
     }
--- src/main/java/calculator/OutputView.java
+++ src/main/java/calculator/OutputView.java
@@ -1,2 +1,7 @@
-package calculator;public class OutputView {
+package calculator;
+
+public class OutputView {
+    void output(int sum){
+        System.out.println("결과 : "+sum);
+    }
 }
