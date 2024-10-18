3adeb74dd684f39d4f8c5a1bc319cd994c81bdbe
Hajuuu <hpt0127@naver.com>
2024-10-18 21:02:20 +0900

    feat: 정규식 수정 및 스트림 적용
    정규식이 제대로 동작하도록 수정하고 스트림을 적용해 splitString()에서 구분자 삭제 가능하도록 구현, isCustomSeparator() 삭제

--- src/main/java/calculator/Calculate.java
+++ src/main/java/calculator/Calculate.java
@@ -3,42 +3,44 @@ package calculator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Calculate {
 
+    private static String customSeparator = "";
 
     public int splitString(String input) {
-        String customSeparator = "";
-        Pattern pattern = Pattern.compile("//(.*?)\n");
+        customSeparator = "";
+        Pattern pattern = Pattern.compile("//(.*?)\\\\n");
         Matcher matcher = pattern.matcher(input);
-        while(matcher.find()) {
+        while (matcher.find()) {
             customSeparator = matcher.group(1);
         }
-        input = input.replaceAll("//(.*?)\n", "");
-        String numbers = input.replaceAll("[,:]", "");
-        return addition(customSeparator, numbers);
+        input = input.replaceAll("\\\\n|/", "").trim();
+        List<String> numbers = List.of(input.split(",|:|" + customSeparator)).stream().filter(n -> !"".equals(n))
+                .collect(Collectors.toList());
+        numbers.stream().filter(n -> n.equals(customSeparator) || (n == null && n.isEmpty()))
+                .collect(Collectors.toList());
+        return addition(numbers);
     }
 
-    private int addition(String customSeparator, String numbers) {
+    private int addition(List<String> numbers) {
         int sum = 0;
-        for(int i = 0; i < numbers.length(); i++) {
-            if(isCustomSeparator(customSeparator, numbers.charAt(i))) {
-                continue;
-            }
-            sum += isPositiveNumber(numbers.charAt(i));
+        for (String number : numbers) {
+            sum += isPositiveNumber(number);
         }
         return sum;
     }
 
-    private boolean isCustomSeparator(String customSeparator, char num) {
-        return num == customSeparator.charAt(0);
-    }
-
-    private int isPositiveNumber(char num) {
-        if(Character.isDigit(num) && num - '0' > 0) {
-            return num - '0';
+    private int isPositiveNumber(String number) {
+        try {
+            int num = Integer.parseInt(number);
+            if (num > 0) {
+                return num;
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
         }
-        throw new IllegalArgumentException();
+        return 0;
     }
 }
