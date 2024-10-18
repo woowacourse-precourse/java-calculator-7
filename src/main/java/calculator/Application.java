7f82f94fe8cc45c1c8b24afbd9c785748877bba0
suhyun9764 <suhyun9764@naver.com>
2024-10-19 00:09:57 +0900

    refactor(application): 메서드 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,103 +1,163 @@
 package calculator;
 
+import static calculator.Constants.CUSTOM_DELIMITER_PREFIX;
+import static calculator.Constants.CUSTOM_DELIMITER_SUFFIX;
+import static calculator.Constants.DEFAULT_DELIMITER;
+import static calculator.Constants.DEFAULT_DELIMITER_PATTERN;
+import static calculator.Constants.ERROR_MESSAGE;
+import static calculator.Constants.INPUT_MESSAGE;
+import static calculator.Constants.MAXIMUM_NUMBER;
+import static calculator.Constants.OUTPUT_MESSAGE;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Application {
-
-    // 1. 초기 세팅
-    public static final List<String> DEFAULT_DELIMITER = Arrays.asList(",", ":");
-    public static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
-    public static final String OUTPUT_MESSAGE = "결과 : ";
-    public static final String CUSTOM_DELIMITER_PREFIX = "//";
-    public static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
-    public static final Long MAXIMUM_NUMBER = Long.MAX_VALUE;
+    private static String inputString;
 
     public static void main(String[] args) {
+        // 문자열 입력 받기
+        inputString = getInputString();
+        // 커스텀 문자 추출
+        List<String> customDelimiters = extractCustomDelimiters();
+        // 기본 구분자, 커스텀 구분자 총합한 패턴 가져오기
+        String delimiterPattern = getDelimiterPattern(customDelimiters);
+        // 받은 문자열 분리하여 숫자로 저장
+        List<Long> values = getValues(delimiterPattern);
+        // 모든 수 더하고 값 출력
+        printResult(values);
+    }
 
-        // 2. 문자열 입력 받기
+    private static String getInputString() {
         System.out.println(INPUT_MESSAGE);
         String inputString = Console.readLine();
+        return inputString;
+    }
 
+    private static List<String> extractCustomDelimiters() {
         List<String> customDelimiters = new ArrayList<>();
-
-
-        String defaultDelimiterPattern = DEFAULT_DELIMITER.stream()
-                .map(Pattern::quote)
-                .collect(Collectors.joining("|"));
-
-        String customDelimiterPattern= null;
-        List<Long> values = new ArrayList<>();
-        Long result = 0L;
 
         // 3. 커스텀 구분자 저장
         while (inputString.startsWith(CUSTOM_DELIMITER_PREFIX)) { //  "//"로 시작하는지 확인 -> 무조건 뒤에 "\n"이 있어야함
-            int i = inputString.indexOf(CUSTOM_DELIMITER_SUFFIX); //    "\n"이 있는 위치를 찾고 그 사이를 분리
-            if (i == -1)
-                throw new IllegalArgumentException("입력이 잘못되었습니다");
+            int i = getIndexOfCustomDelimiterSuffix();
 
-            String subString = inputString.substring(CUSTOM_DELIMITER_PREFIX.length(), i);
-            inputString = inputString.substring(i + CUSTOM_DELIMITER_SUFFIX.length(), inputString.length());    //첫 번째 커스텀 구분자 지정 부분 삭제
+            String customDelimiterSection = extractCustomDelimiterSection(i);
+            removeCustomDelimiterFromInput(i);    //첫 번째 커스텀 구분자 지정 부분 삭제
 
-            String[] splitByDefaultDelimiter = subString.split(defaultDelimiterPattern);
-            for (String customDelimiter : splitByDefaultDelimiter) {
-                if (customDelimiter.length() > 1)
-                    throw new IllegalArgumentException();
-
-                if (customDelimiter.length() == 1) {
-                    if (Character.isLetterOrDigit(customDelimiter.charAt(0)))
-                        throw new IllegalArgumentException();
-
-                    if (!customDelimiters.contains(customDelimiter) && customDelimiter.charAt(0) != ',' && customDelimiter.charAt(0) != ':')
-                        customDelimiters.add(customDelimiter);
-                }
-            }
-
+            extractAndValidateCustomDelimiters(customDelimiterSection, customDelimiters);
         }
+        return customDelimiters;
+    }
 
-        if(!customDelimiters.isEmpty()){
+    private static int getIndexOfCustomDelimiterSuffix() {
+        int i = inputString.indexOf(CUSTOM_DELIMITER_SUFFIX); //    "\n"이 있는 위치를 찾고 그 사이를 분리
+        if (i == -1)
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        return i;
+    }
+
+    private static String extractCustomDelimiterSection(int i) {
+        String subString = inputString.substring(CUSTOM_DELIMITER_PREFIX.length(), i);
+        return subString;
+    }
+
+    private static void removeCustomDelimiterFromInput(int i) {
+        inputString = inputString.substring(i + CUSTOM_DELIMITER_SUFFIX.length(), inputString.length());
+    }
+
+    private static void extractAndValidateCustomDelimiters(String customDelimiterSection, List<String> customDelimiters) {
+        String[] splitByDefaultDelimiter = customDelimiterSection.split(DEFAULT_DELIMITER_PATTERN);
+        for (String customDelimiter : splitByDefaultDelimiter) {
+            validateRange(customDelimiter);
+            validateFormatAndAddCustomDelimiter(customDelimiters, customDelimiter);
+        }
+    }
+
+    private static void validateRange(String customDelimiter) {
+        if (customDelimiter.length() > 1)
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+    }
+
+    private static void validateFormatAndAddCustomDelimiter(List<String> customDelimiters, String customDelimiter) {
+        if (customDelimiter.length() == 1) {
+            if (Character.isLetterOrDigit(customDelimiter.charAt(0)))
+                throw new IllegalArgumentException(ERROR_MESSAGE);
+
+            if (!customDelimiters.contains(customDelimiter) &&
+                    customDelimiter.charAt(0) != DEFAULT_DELIMITER.get(0).charAt(0) &&
+                    customDelimiter.charAt(0) != DEFAULT_DELIMITER.get(1).charAt(0))
+                customDelimiters.add(customDelimiter);
+        }
+    }
+
+    private static String getDelimiterPattern(List<String> customDelimiters) {
+        String customDelimiterPattern = null;
+
+        customDelimiterPattern = getCustomDelimiterPattern(customDelimiters, customDelimiterPattern);
+
+        return getDelimiterPattern(customDelimiterPattern);
+    }
+
+    private static String getCustomDelimiterPattern(List<String> customDelimiters, String customDelimiterPattern) {
+        if (!customDelimiters.isEmpty()) {
             customDelimiterPattern = customDelimiters.stream()
                     .map(Pattern::quote)
                     .collect(Collectors.joining("|"));
         }
+        return customDelimiterPattern;
+    }
 
+    private static String getDelimiterPattern(String customDelimiterPattern) {
         String delimiterPattern = null;
 
-        if(customDelimiterPattern == null)
-            delimiterPattern = defaultDelimiterPattern;
-        else if(customDelimiterPattern != null  )
-            delimiterPattern = defaultDelimiterPattern + "|" + customDelimiterPattern;
+        if (customDelimiterPattern == null)
+            delimiterPattern = DEFAULT_DELIMITER_PATTERN;
+        else if (customDelimiterPattern != null)
+            delimiterPattern = DEFAULT_DELIMITER_PATTERN + "|" + customDelimiterPattern;
+        return delimiterPattern;
+    }
 
-        // 4. 받은 문자열 분리하여 숫자로 저장
+    private static List<Long> getValues(String delimiterPattern) {
+        List<Long> values = new ArrayList<>();
+
         String[] stringValues = inputString.split(delimiterPattern);
         for (String stringValue : stringValues) {
-            long longValue;
-            try {
-                longValue = Long.parseLong(stringValue);
-            }catch (NumberFormatException e) {
-                throw new IllegalArgumentException();
-            }
-            if(longValue > MAXIMUM_NUMBER)
-                throw new IllegalArgumentException();
-
-            if(longValue < 0)
-                throw new IllegalArgumentException();
+            long longValue = getLongValue(stringValue);
+            validateLongValue(longValue);
             values.add(longValue);
         }
+        return values;
+    }
 
+    private static void validateLongValue(long longValue) {
+        if (longValue > MAXIMUM_NUMBER)
+            throw new IllegalArgumentException(ERROR_MESSAGE);
 
-        for (Long value : values) {
-            result += value;
+        if (longValue < 0)
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+    }
+
+    private static long getLongValue(String stringValue) {
+        long longValue;
+        try {
+            longValue = Long.parseLong(stringValue);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
         }
-        if(result < 0)
-            throw new IllegalArgumentException();
+        return longValue;
+    }
+
+
+    private static void printResult(List<Long> values) {
+        Long result = values.stream().mapToLong(Long::longValue).sum();
+        if (result < 0)
+            throw new IllegalArgumentException(ERROR_MESSAGE);
 
         System.out.println(OUTPUT_MESSAGE + result);
-
-
     }
+
+
 }
