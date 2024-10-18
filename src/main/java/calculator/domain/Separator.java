2f4294e62fca3e93d04123ea5281d63305047016
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-10-18 21:37:19 +0900

    feat: 입력 문자열을 분리하여 숫자 추출해서 반환

--- src/main/java/calculator/domain/Separator.java
+++ src/main/java/calculator/domain/Separator.java
@@ -6,10 +6,21 @@ public class Separator {
 
     private DelimiterManager delimiterManager;
 
-    public String extractNumbers(String input) {
-        return "";
+    // 생성자를 통해 DelimiterManager 주입
+    public Separator(DelimiterManager delimiterManager) {
+        this.delimiterManager = delimiterManager;
     }
 
+    // 입력 문자열을 구분자를 기준으로 분리하여 숫자를 추출하는 메서드
+    public String extractNumbers(String input) {
+        // 사용할 구분자 리스트를 DelimiterManager에서 가져옴
+        List<String> delimiters = delimiterManager.getDelimiters(input);
+
+        // 구분자 리스트를 기반으로 입력 문자열을 분리
+        return splitByDelimiters(input, delimiters);
+    }
+
+    // 구분자를 사용하여 숫자를 분리하는 메서드
     private String splitByDelimiters(String input, List<String> delimiters) {
         return "";
     }
