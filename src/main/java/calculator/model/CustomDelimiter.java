d79632800b608de4d784300ffd34083842fdfc7b
이윤빈 <kanado20000@naver.com>
2024-10-18 22:31:00 +0900

    🛠️ fix(CustomDelimiter):커스텀 구분자 관련 함수 별도의 클래스로 분리

---
+++ src/main/java/calculator/model/CustomDelimiter.java
@@ -0,0 +1,36 @@
+package calculator.model;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class CustomDelimiter {
+    private String formula;
+
+    public CustomDelimiter(){
+        this.formula = formula;
+    }
+
+    public String getFormula(){
+        return this.formula;
+    }
+
+    public String getCustomDelimiter(){
+        Pattern pattern = Pattern.compile("//(.*?)\n");
+        Matcher matcher = pattern.matcher(formula);
+        return matcher.group(1);
+    }
+
+    public boolean checkCustomDelimiter(){
+        String[] splitedNumbers = formula.split("[,;]");
+        String engraftedNumbers = String.join("", splitedNumbers);  
+        if (!engraftedNumbers.matches("\\d+")){
+            return true;
+        }
+        return false;
+    }
+
+    public String SplitDelimiter(){
+        String[] splitedNumbers = formula.split("\n");
+        return splitedNumbers[1];
+    }
+}
