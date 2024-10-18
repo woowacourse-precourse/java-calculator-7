ca711092027853712965878737712654bffeaa8b
openmpy <suhwan@kakao.com>
2024-10-18 16:46:23 +0900

    refactor: DefaultDelimiter 에서 정규 표현식을 상수로 추출

--- src/main/java/calculator/strategy/DefaultDelimiter.java
+++ src/main/java/calculator/strategy/DefaultDelimiter.java
@@ -2,8 +2,10 @@ package calculator.strategy;
 
 public class DefaultDelimiter implements Delimiter {
 
+    private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+
     @Override
     public String[] split(String input) {
-        return input.split("[,:]");
+        return input.split(DEFAULT_DELIMITER_REGEX);
     }
 }
