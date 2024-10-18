f428b393c4c3e08fec7a490a4d6516ea658129c4
woo <ldw28517@naver.com>
2024-10-18 17:19:08 +0900

    test: RegexUtils 테스트 추가
    - Validator에서 사용하기 위한 정규 표현식 매치 메서드 테스트
    - 입력 문자열: 숫자로 시작하거나 //로 시작해야한다 or 문자열이 공백이다
    - 숫자 후보: 모든 문자가 포함되지 않은 양수이다

--- src/test/java/calculator/utils/RegexUtilsTest.java
+++ src/test/java/calculator/utils/RegexUtilsTest.java
@@ -1,16 +1,15 @@
 package calculator.utils;
 
+import calculator.domain.Candidate;
 import calculator.domain.delimiter.CustomDelimiter;
 import calculator.domain.delimiter.DefaultDelimiter;
 import calculator.domain.delimiter.Delimiter;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RegexUtilsTest {
     @Test
@@ -27,5 +26,89 @@ class RegexUtilsTest {
 
         //then
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void 입력_매치_성공_숫자() {
+        //given
+        String input = "1,2:3";
+
+        //when
+        boolean result = RegexUtils.isMatchedInput(input);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 입력_매치_성공_커스텀구분자() {
+        //given
+        String input = "//:\\n1,2:3";
+
+        //when
+        boolean result = RegexUtils.isMatchedInput(input);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 입력_매치_성공_공백() {
+        //given
+        String input = "";
+
+        //when
+        boolean result = RegexUtils.isMatchedInput(input);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 입력_매치_실패_문자로시작() {
+        //given
+        String input = "+2,3:1";
+
+        //when
+        boolean result = RegexUtils.isMatchedInput(input);
+
+        //then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void 숫자_매치_성공() {
+        //given
+        Candidate candidate = new Candidate("10");
+
+        //when
+        boolean result = RegexUtils.isMatchedNumber(candidate);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 숫자_매치_실패_음수() {
+        //given
+        Candidate candidate = new Candidate("-1");
+
+        //when
+        boolean result = RegexUtils.isMatchedNumber(candidate);
+
+        //then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void 숫자_매치_실패_숫자가아님() {
+        //given
+        Candidate candidate = new Candidate("=2");
+
+        //when
+        boolean result = RegexUtils.isMatchedNumber(candidate);
+
+        //then
+        assertThat(result).isFalse();
     }
 }
