c739dbcd54e737b799a3f3d353bff5bb8b9cc70f
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-19 02:05:21 +0900

    refactor(Application): 사용자 입력을 playCalculator 내부로 이동

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,12 +2,11 @@ package calculator;
 
 import calculator.config.AppConfig;
 import calculator.controller.StringAddCalculatorController;
-import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
         StringAddCalculatorController calculatorController = new StringAddCalculatorController(appConfig.calculatorService());
-        calculatorController.playCalculator(Console.readLine());
+        calculatorController.playCalculator();
     }
 }
