985875e38ef549fac4231b4ec01a119fdc0d82a5
GaBaljaintheroom <junsu1222@naver.com>
2024-10-18 23:31:05 +0900

    test(SumCalculatorTest): 숫자 리스트 합 최댓값 초과 예외 테스트 작성

--- src/test/java/calculator/SumCalculatorTest.java
+++ src/test/java/calculator/SumCalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -20,6 +21,17 @@ class SumCalculatorTest {
         //then
         int expect = 147;
         assertThat(actual).isEqualTo(expect);
+    }
+
+    @Test
+    void 숫자_리스트의_합이_최댓값을_초과하면_예외가_발생한다() {
+        //given
+        List<Integer> numbers = List.of(12, Integer.MAX_VALUE, 1);
+
+        //when & then
+        assertThatThrownBy(() -> calculator.sum(numbers))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("합계가 최대값을 초과했습니다.");
     }
 
 }
