717119cdd409493a60a46d98d175fc41c612f78f
Hyungjung Kim <gudwnd1113@gmail.com>
2024-10-18 20:38:46 +0900

    refactor(Machine): Separate the class into Machine and Utils classes
    Machine 클래스 내 getString과 printResult 함수를 Utils 클래스로 분리
    
    Application.java 내 정의된 Machine 클래스를 분리하여 Machine.java와
    Utils.java에 정의.

---
+++ src/main/java/calculator/Utils.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Utils {
+    public static String getString() {
+        String input;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        input = Console.readLine();
+        return input;
+    }
+
+    public static void printResult(int data) {
+        System.out.printf("결과 : %d", data);
+    }
+}
