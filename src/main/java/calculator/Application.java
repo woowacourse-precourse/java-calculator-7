850a40f6a70370cbc39f641090122ead9cf3f745
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-10-18 15:17:03 +0900

    feat(DelimiterParser): 공백일 경우 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,11 @@ public class    Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String string = Console.readLine();
+
+        Calculator calculator = new Calculator();
+        int result=0;
+        result=calculator.add(string);
+
+        System.out.println("결과: "+ result);
     }
 }
--- src/main/java/calculator/DelimiterParser.java
+++ src/main/java/calculator/DelimiterParser.java
@@ -5,14 +5,23 @@ public class DelimiterParser {
         String delimiter = ",|:";
         input=input.replace(" ","");
 
+        // 공백 제거 후 입력이 비어있는 경우
+        if (input.isEmpty()) {
+            return new String[]{"0"}; // 0을 포함한 배열 반환
+        }
+        String[] inputs = new String[100];
         if ((input.startsWith("//"))&&(input.contains("\n"))) {
             int newLineIndex = input.indexOf("\n");
             delimiter=input.substring(2,newLineIndex);
             input=input.substring(newLineIndex+1);
         }
+        else if((input.contains(","))||(input.contains(":"))){
+           inputs= input.split(delimiter);
+        }
         else{
             throw new IllegalArgumentException("커스텀 구분자가 올바르지 않은 형식인 경우입니다.");
         }
-        return input.split(delimiter);
+
+        return inputs;
     }
 }
