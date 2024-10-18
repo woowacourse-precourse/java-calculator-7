8d6eb998ca90ca441dbf1c0ee1bbaebdfc0c8da3
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-19 02:00:35 +0900

    feat(CalculatorView): 입・출력 메시지 로직 구현

---
+++ src/main/java/calculator/view/CalculatorView.java
@@ -0,0 +1,13 @@
+package calculator.view;
+
+public class CalculatorView {
+
+    public static void inputView() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public static void outputView(int result) {
+        System.out.println("결과 : " + result);
+    }
+
+}
