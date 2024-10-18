48bb46016baad2e71e59613bdda25d1b90b6a2a3
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-18 17:15:24 +0900

    feat(ErrorMessage): 음수를 입력할 때 발생하는 예외 메시지 작성

--- src/main/java/calculator/exception/ErrorMessage.java
+++ src/main/java/calculator/exception/ErrorMessage.java
@@ -3,7 +3,8 @@ package calculator.exception;
 public enum ErrorMessage {
 
     NOT_MATCH_FOUND_DELIMITER("매칭되는 구분자가 없습니다."),
-    INVALID_INPUT("구분자와 숫자를 정확하게 입력해주세요.");
+    INVALID_INPUT("구분자와 숫자를 정확하게 입력해주세요."),
+    NEGATIVE_NUMBER_NOT_ALLOWED("음수는 입력할 수 없습니다.");
 
     private final String message;
 
