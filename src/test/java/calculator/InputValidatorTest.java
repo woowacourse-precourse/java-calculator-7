36b178d41e4000d66cbd439b66c9b10a04f30d68
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-10-18 13:05:28 +0900

    test: 커스텀구분자 검증부분 테스트코드

--- src/test/java/calculator/InputValidatorTest.java
+++ src/test/java/calculator/InputValidatorTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class InputValidatorTest {
 
     @Test
-    void isDefaultDelimiterTest(){
+    void isDefaultDelimiterTest() {
         InputValidator inputValidator = new InputValidator();
         DelimiterType result;
 
@@ -21,4 +21,33 @@ public class InputValidatorTest {
         assertThat(result).isEqualTo(DelimiterType.DEFAULT);
 
     }
+
+    @Test
+    void isCustomDelimiter() {
+        InputValidator inputValidator = new InputValidator();
+        DelimiterType result;
+
+        result = inputValidator.isValidInput("//;\\n1");
+        assertThat(result).isEqualTo(DelimiterType.CUSTOM);
+
+        result = inputValidator.isValidInput("//;\\n1;2;3");
+        assertThat(result).isEqualTo(DelimiterType.CUSTOM);
+
+        result = inputValidator.isValidInput("//3\\n333");
+        assertThat(result).isEqualTo(DelimiterType.CUSTOM);
+
+        result = inputValidator.isValidInput("//3\\n43331");
+        assertThat(result).isEqualTo(DelimiterType.CUSTOM);
+
+        result = inputValidator.isValidInput("//o\\n1o2o3");
+        assertThat(result).isEqualTo(DelimiterType.CUSTOM);
+
+        result = inputValidator.isValidInput("//-3\\n2-33-32");
+        assertThat(result).isEqualTo(DelimiterType.CUSTOM);
+
+        result = inputValidator.isValidInput("// \\n2 3 4");
+        assertThat(result).isEqualTo(DelimiterType.CUSTOM);
+
+    }
+
 }
