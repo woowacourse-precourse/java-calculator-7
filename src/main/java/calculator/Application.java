1f5622fba4a2fe2824eeb06b6e6c5db6b845c046
jongsic17 <qkrwhdtlr178@naver.com>
2024-10-19 02:49:32 +0900

    feat: 커스텀 구분자 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,4 +1,7 @@
 package calculator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static camp.nextstep.edu.missionutils.Console.*;
 
 public class Application {
@@ -19,6 +22,11 @@ public class Application {
         if(str.isEmpty()){
             return 0;
         }
+        if(isPattern(str)){
+            s = CustomSeparator(str);
+            separator = s[0];
+            str = s[1];
+        }
 
         String[] x = str.split(separator);
         for (String string : x) {
@@ -27,4 +35,27 @@ public class Application {
         }
         return sum;
     }
+    public static String[] CustomSeparator(String str){
+        String[] s = {"",""};
+        Pattern pattern = Pattern.compile("//(.*?)(\\\\n)");
+        Matcher matcher = pattern.matcher(str);
+
+        if(matcher.find()){
+            s[0] = matcher.group(1);
+            s[1] = str.replace(matcher.group(0), "");
+            return s;
+        }else{
+            return new String[0];
+        }
+    }
+    public static boolean isPattern(String str){
+        boolean isPattern = false;
+        Pattern pattern = Pattern.compile("//(.*?)\\\\n");
+        Matcher matcher = pattern.matcher(str);
+        if(matcher.find()){
+            isPattern = true;
+        }
+        return isPattern;
+    }
+
 }
