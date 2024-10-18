39013a76208665bf07b399490b89299d183494a4
minSsan <msp1125@hanyang.ac.kr>
2024-10-18 19:15:00 +0900

    feat(CalculatorInputParser): 기본 구분자를 기준으로 파싱하는 기능

--- src/main/java/calculator/CalculatorInputParser.java
+++ src/main/java/calculator/CalculatorInputParser.java
@@ -1,10 +1,29 @@
 package calculator;
 
-import java.util.ArrayList;
+import calculator.exception.InvalidDelimiterException;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class CalculatorInputParser {
+    private final static String CUSTOM_DELIMITER_PREFIX = "//";
+    private final static String CUSTOM_DELIMITER_SUFFIX = "\\n";
+    private final static String DEFAULT_DELIMITERS = ",:";
+
+    private List<Integer> parseNumbers(String input, String regex) {
+        try {
+            return Arrays.stream(input.split(regex))
+                    .map(String::trim)
+                    .filter(s -> !s.isEmpty())
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw new InvalidDelimiterException();
+        }
+    }
+
     public List<Integer> parse(String input) {
-        return new ArrayList<>();
+        final String regex = "[" + DEFAULT_DELIMITERS + "]";
+        return parseNumbers(input, regex);
     }
 }
