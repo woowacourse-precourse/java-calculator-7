e8bb36cde2eba6b72968f92dbc56294f291a0eca
kimdev <kim__dev@hufs.ac.kr>
2024-10-18 19:46:24 +0900

    feat(Input): 숫자만 따로 분리해서 저장하는 기능 추가

--- src/main/java/calculator/Model/NumberList.java
+++ src/main/java/calculator/Model/NumberList.java
@@ -9,4 +9,30 @@ public class NumberList {
     public boolean isEmpty() {
         return !this.numberList.isEmpty();
     }
+
+    public boolean checkValidNumber(int number) {
+        return 0 <= number && number <= Integer.MAX_VALUE;
+    }
+
+    public int parseInt(String numberString) {
+        try {
+            return Integer.parseInt(numberString);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("입력이 올바르지 않습니다. 잘못된 구분자가 포함되어 있습니다.");
+        }
+    }
+
+    public void registerNumber(String numberString) {
+        int number = parseInt(numberString);
+        if (checkValidNumber(number)) {
+            numberList.add(number);
+        }
+    }
+
+    public void makeNumberList(String rawNumber, String customDelimiter) {
+        StringTokenizer inputNumber = new StringTokenizer(rawNumber, ",;"+customDelimiter);
+        while (inputNumber.hasMoreTokens()) {
+            registerNumber(inputNumber.nextToken());
+        }
+    }
 }
