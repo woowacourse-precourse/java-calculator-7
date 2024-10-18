a956d85e99d56c0f1bbc940d6ce3fb890d7e2e35
chungchung <chunghan@chungchung.local>
2024-10-18 16:22:34 +0900

    feat: 빈 문자열 처리 및 결과 출력
    (cherry picked from commit b80dd958587c84c63fbb67d331de57750b8dcc05)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
+
+        if (input == null || input.isEmpty()) {
+            System.out.println("결과 : " + 0);
+        }
     }
 }
