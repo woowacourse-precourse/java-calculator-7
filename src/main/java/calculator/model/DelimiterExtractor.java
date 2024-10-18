efc1db9a19d6c3a800e1586cee837054feac4f48
koseonje <kosunje1344@naver.com>
2024-10-19 02:45:51 +0900

    fix : 정규 표현식 오류 수정

--- src/main/java/calculator/model/DelimiterExtractor.java
+++ src/main/java/calculator/model/DelimiterExtractor.java
@@ -1,15 +1,13 @@
 package calculator.model;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DelimiterExtractor {
 
-    private static final String EXTRACTOR_REGEX = "//(.*?)\\n";
+    private static final String EXTRACTOR_REGEX = "//(.*?)\\\\n";
     private static final Pattern EXTRACT_PATTERN = Pattern.compile(EXTRACTOR_REGEX);
     private static final int REQUIRED_GROUP_NUMBER = 1;
 
