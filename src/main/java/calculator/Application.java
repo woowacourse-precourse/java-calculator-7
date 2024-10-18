49dcf814083815fe8b4502a3b3d6f260500d0f39
junhyuk1248 <poa124@nate.com>
2024-10-18 16:15:41 +0900

    feat(Application): 기본 구분자로 문자열을 입력 받아 계산기를 구현한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,43 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        int sum = 0;
+
+        String[] numbers = delimiter(input);
+        sum = calulator(numbers);
+
+
+        System.out.println("결과 : " + sum);
     }
+
+    private static String[] delimiter(String input) {
+        if (input.isEmpty()){
+            return null;
+        }
+        String default_Delimiters = ",|:";
+        return input.split(default_Delimiters);
+    }
+
+    private static int calulator(String[] numbers) {
+        List<Integer> listNumber = new ArrayList<>();
+        int sum = 0;
+        if (numbers == null){
+            return sum;
+        }
+        for (String number : numbers){
+            listNumber.add(Integer.parseInt(number));
+        }
+        sum = listNumber.stream().mapToInt(i -> i).sum();
+
+        return sum;
+    }
+
 }
