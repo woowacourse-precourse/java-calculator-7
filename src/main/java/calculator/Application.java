bf1e25159fe649e07a8b4fd77f02b9324e02211b
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-10-18 21:46:45 +0900

    feat(calculator) : 쉼표, 또는 콜론을 구분자로 하여 문자열을 분리하는 기능
    두 구분자를 이용하여 문자열을 구분하는 기능을 추가하였습니다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,12 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Carculator.sumNumbersFromLetters(Console.readLine());
 
     }
 }
--- src/main/java/calculator/Carculator.java
+++ src/main/java/calculator/Carculator.java
@@ -2,5 +2,16 @@ package calculator;
 
 public class Carculator {
 
-    
+
+    public static void sumNumbersFromLetters(String input){
+
+        String[] tmp;
+
+        String separator = "[,:;]";
+        tmp = input.split(separator);
+
+
+    }
+
+
 }
