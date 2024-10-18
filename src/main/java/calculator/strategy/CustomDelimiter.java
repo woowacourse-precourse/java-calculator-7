4cbc31fb939217c9672bd5958dbdf18da16e2c1d
openmpy <suhwan@kakao.com>
2024-10-18 15:35:48 +0900

    feat: 커스텀 구분자로 나누는 기능 추가

--- src/main/java/calculator/strategy/CustomDelimiter.java
+++ src/main/java/calculator/strategy/CustomDelimiter.java
@@ -2,8 +2,14 @@ package calculator.strategy;
 
 public class CustomDelimiter implements Delimiter {
 
+    private final String delimiter;
+
+    public CustomDelimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
     @Override
     public String[] split(String input) {
-        return new String[0];
+        return input.split(delimiter);
     }
 }
