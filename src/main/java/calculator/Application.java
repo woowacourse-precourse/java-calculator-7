cb7336110214582ba8a107d206f34cdb57947eb2
junhaa <2171326@hansung.ac.kr>
2024-10-18 19:33:53 +0900

    feat(Application): 전체 프로그램 구조 구현
    - 구현된 기능을 MVC 패턴으로 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import calculator.controller.StringCalculator;
+import calculator.domain.DelimiterExtractor;
+import calculator.domain.DelimiterTokenizer;
+import calculator.domain.ResultCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        DelimiterExtractor delimiterExtractor = new DelimiterExtractor();
+        DelimiterTokenizer delimiterTokenizer = new DelimiterTokenizer();
+        ResultCalculator resultCalculator = new ResultCalculator();
+        StringCalculator stringCalculator = new StringCalculator(inputView, outputView, delimiterExtractor, delimiterTokenizer, resultCalculator);
+
+        stringCalculator.run();
     }
 }
