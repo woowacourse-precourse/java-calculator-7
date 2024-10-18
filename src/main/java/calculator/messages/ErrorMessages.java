e263ebf543526921419083f3312c96b7bb4d5d9d
kymin <park48664866@gmail.com>
2024-10-18 11:50:33 +0900

    refactor($error): 에러 메시지 종류 세분화
    에러 메시지 종류 세분화:
    - 기존의 유효하지 않은 입력에 대한 에러 메시지 분리
    - 유효하지 않은 구분자와 유효하지 않은 숫자로 세분화

--- src/main/java/calculator/messages/ErrorMessages.java
+++ src/main/java/calculator/messages/ErrorMessages.java
@@ -2,7 +2,8 @@ package calculator.messages;
 
 public enum ErrorMessages {
     EXIST_SEPARTOR("이미 존재하는 구분자입니다."),
-    INVALID_INPUT("유효하지 않은 입력입니다.");
+    INVALID_SEPARTOR("유효하지 않은 구분자입니다."),
+    INVALID_NUMBER("유효하지 않은 숫자입니다.");
 
     private final String message;
 
