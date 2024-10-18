dfcebe2d9d53af8dbe92a4c75e830f465d02a9d6
seul-young <tmfud@naver.com>
2024-10-18 15:07:57 +0900

    feat: 쉼표 또는 콜론 구분자를 기준으로 분리한 각 숫자의 합 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,42 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	 System.out.println("덧셈할 문자열을 입력해 주세요.");
+    	 String in = Console.readLine();
+
+    	  try {
+          	int result = add(in);
+          	System.out.println("결과 : " + result);
+          } catch(IllegalArgumentException e) {
+          	System.out.println(e.getMessage());
+          }
+
     }
+    public static int add(String in) {
+
+    	if(in == null || in.isEmpty()) {
+    		return 0;
+    	}
+
+    	 String cus = ",|:";
+
+
+    	 String[] tokens = in.split(cus);
+   	     int sum = 0;
+
+   	     for(String token : tokens) {
+	    	try {
+	    		sum += Integer.parseInt(token);
+	    	} catch (NumberFormatException e) {
+	    		throw new IllegalArgumentException("IllegalArgumentException");
+	    	}
+	    }
+
+	    return sum;
+
+    }
+
 }
