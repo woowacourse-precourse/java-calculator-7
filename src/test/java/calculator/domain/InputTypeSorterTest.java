34fc0499f8915724ca4edf2deef13ba09128fa49
pakaheo <k70923@konkuk.ac.kr>
2024-10-18 09:42:44 +0900

    test : test input containg basic delimiter or custom delimiter or default

--- src/test/java/calculator/domain/InputTypeSorterTest.java
+++ src/test/java/calculator/domain/InputTypeSorterTest.java
@@ -1,2 +1,26 @@
-package calculator.domain;public class InputTypeSorterTest {
+package calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class InputTypeSorterTest {
+
+    @Test
+    void 기본_구분자_문자열_입력() {
+        String text = "1,2,3";
+        assertThat(InputTypeSorter.sort(text)).isEqualTo(new BasicDelimiterInput(text));
+    }
+
+    @Test
+    void 커스텀_구분자_문자열_입력() {
+        String text = "//;\\n1;2;3";
+        assertThat(InputTypeSorter.sort(text)).isEqualTo(new CustomDelimiterInput(text));
+    }
+
+    @Test
+    void 문자열_하나_입력() {
+        String text = "1";
+        assertThat(InputTypeSorter.sort(text)).isEqualTo(new DefaultInput(text));
+    }
 }
