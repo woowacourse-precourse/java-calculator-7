16b903be6f7cc176ec63f750796172e60066065a
seongeun308 <kse011010@gmail.com>
2024-10-19 02:43:53 +0900

    feat: 커스텀 구분자 반환하는 기능 구현
    정규표현식과 replaceAll()을 활용해 커스텀 구분자만을 남겨 이를 반환

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,4 +6,8 @@ public class StringCalculator {
     private static boolean isExistCustomDelim(String data){
         return data.matches(Delimiter.CUSTOM.getRegex());
     }
+
+    private static String getCustomDelim(String data){
+        return data.replaceAll(Delimiter.CUSTOM_DELIM.getRegex(), "");
+    }
 }
