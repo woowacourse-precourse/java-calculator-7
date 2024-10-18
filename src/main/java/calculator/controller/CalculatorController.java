a54497fcd46588e4043f0fe4fedfe3745a6d2907
chul5 <och7164@gmail.com>
2024-10-19 02:24:55 +0900

    feat: 적절한 메시지 출력하는 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,5 +1,6 @@
 package calculator.controller;
 
+import calculator.domain.Calculator;
 import calculator.service.CalculatorService;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -7,12 +8,15 @@ import java.util.List;
 
 public class CalculatorController {
 	private CalculatorService calculatorService;
+	private Calculator calculator;
 
 	public void run() {
 		System.out.println("덧셈할 문자열을 입력해 주세요.");
 		String line = Console.readLine();
 		calculatorService = new CalculatorService(line);
 		List<Integer> numbers = calculatorService.getNumbers();
+		calculator = new Calculator(numbers);
+		System.out.println("결과 : " + calculator.getSum());
 	}
 
 
