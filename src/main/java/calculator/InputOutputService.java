e0c57c39b01ce3ac2db44e699ec68f064dbd6341
corpi <lj845854@gmail.com>
2024-10-18 17:41:45 +0900

    refactor(InputOutputService): 입력 안내 문구를 변수로 관리하지 않고 inline으로 수정

--- src/main/java/calculator/InputOutputService.java
+++ src/main/java/calculator/InputOutputService.java
@@ -3,10 +3,8 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputOutputService {
-    private final String inputComment = "덧셈할 문자열을 입력해 주세요.";
-
     public String fetchUserInput() {
-        System.out.println(inputComment);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
 
         return Console.readLine();
     }
