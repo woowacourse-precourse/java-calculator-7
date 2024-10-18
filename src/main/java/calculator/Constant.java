954c3c8582fa67e5e63a27152bc0a8d7b7065782
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-18 15:51:40 +0900

    fix(Validator): 런타임에 \n가 개행문자로 인식되는 문제 해결
    커스텀 구분자 형식 검사에서 테스트 환경에서는 \n가 문자열로 인식되지만 런타임에는 개행 문자로 인식되어
    해당 문자를 찾을 수 없는 문제가 발생하여 Validator 클래스의 생성자에서 문자열을 받을 수 있도록 함

---
+++ src/main/java/calculator/Constant.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class Regex {
+    private static final String EXTRACT_REGEX_FOR_APP = "//(.+?)\\\\n";
+    private static final String EXTRACT_REGEX_FOR_TEST = "//(.+?)\n";
+    private static final String
+}
--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -2,33 +2,40 @@ package calculator;
 
 public class Validator {
 
-    public static void validate(String value) {
+    private final String customSeparatorEndString;
+
+    //테스트 환경에서는 \n가 일반문자로 인식되지만 실제 런타임에 개행문자로 인식되는 문제가 있어 상황에 맞게 문자열을 받도록 함
+    public Validator(String customSeparatorEndString) {
+        this.customSeparatorEndString = customSeparatorEndString;
+    }
+
+    public void validate(String value) {
         checkBeginAndEndOfValue(value);
         checkCustomSeparatorFormat(value);
     }
 
     //문자열의 시작과 끝 확인
-    private static void checkBeginAndEndOfValue(String value) {
+    private void checkBeginAndEndOfValue(String value) {
         //문자열의 시작 확인
-        if (!value.startsWith("//") && !isNum(value.charAt(0))) {
+        if (!value.startsWith("//") && !isPositiveNum(value.charAt(0))) {
             throw new IllegalArgumentException("문자열의 시작은 커스텀 구분자 선언 또는 양수여야 합니다");
         }
 
         //문자열의 끝 확인
-        if (!value.endsWith("\n") && !isNum(value.charAt(value.length() - 1))) {
+        if (!value.endsWith("\n") && !isPositiveNum(value.charAt(value.length() - 1))) {
             throw new IllegalArgumentException("문자열의 끝은 커스텀 구분자 종료 선언 또는 양수여야 합니다.");
         }
     }
 
 
     //문자열에서 커스텀 구분자 형식 유효성 검사하기
-    private static void checkCustomSeparatorFormat(String value) {
+    private void checkCustomSeparatorFormat(String value) {
         if (!value.contains("//")) {
             return;
         }
 
         int start = value.indexOf("//");
-        int end = value.indexOf("\n");
+        int end = value.indexOf(customSeparatorEndString);
 
         // 커스텀 구분자 선언이 //후에 \n인 형태인지,
         if (start > end) {
@@ -42,8 +49,8 @@ public class Validator {
     }
 
 
-    //char값이 1 ~ 9 사이의 양수인지 확
-    private static boolean isNum(char c) {
+    //char값이 1 ~ 9 사이의 양수인지 확인
+    private boolean isPositiveNum(char c) {
         return c >= '1' && c <= '9';
     }
 }
