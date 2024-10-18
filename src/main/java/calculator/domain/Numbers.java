f57646c4485560961551e10539e4e5a8cb28b580
giwoong <dev.choi0409@gmail.com>
2024-10-18 22:45:27 +0900

    refactor: 사용자가 음수와 0을 입력, 숫자가 아닌 값을 입력하는 경우 예외 처리
    README.md 구현에 맞춰 최신화

--- src/main/java/calculator/domain/Numbers.java
+++ src/main/java/calculator/domain/Numbers.java
@@ -6,10 +6,28 @@ public class Numbers {
         int[] intNumbers = new int[strNumbers.length];
 
         for (int i = 0; i < strNumbers.length; i++) {
-            intNumbers[i] = Integer.parseInt(strNumbers[i]);
+            intNumbers[i] = parseToInt(strNumbers[i]);
         }
 
         return intNumbers;
+    }
+
+    private int parseToInt(String number) {
+        try {
+            int parseInt = Integer.parseInt(number);
+
+            validatePositiveNumber(parseInt);
+
+            return parseInt;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닌 값을 입력할 수 없습니다.");
+        }
+    }
+
+    private void validatePositiveNumber(int parseInt) {
+        if (parseInt <= 0) {
+            throw new IllegalArgumentException("음수와 0은 입력할 수 없습니다.");
+        }
     }
 
 }
