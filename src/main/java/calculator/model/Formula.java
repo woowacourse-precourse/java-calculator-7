7416b51c23908b42e20d898943a96a1d51bbb8bd
yunsseong <aster20@hanyang.ac.kr>
2024-10-18 16:52:59 +0900

    feat: 수식 구분자 검증 기능 구현

--- src/main/java/calculator/model/Formula.java
+++ src/main/java/calculator/model/Formula.java
@@ -1,10 +1,37 @@
 package calculator.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Formula {
 
     private final String formula;
+    private final Pattern customPattern = Pattern.compile("^//.\\\\n.*");
+    private final Pattern defaultPattern = Pattern.compile("[0-9:,]*");
+    private String customDelimiter = "";
 
     public Formula(String formula) {
-        this.formula = formula;
+        this.formula = validate(formula);
+    }
+
+    public String validate(String formula) {
+        if (validateDelimiter(formula)) {
+            return formula;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public boolean validateDelimiter(String formula) {
+        if (!defaultPattern.matcher(formula).matches() && !customPattern.matcher(formula).matches()) {
+            return false;
+        }
+        customDelimiter = extractCustomDelimiter(formula);
+        String customValidationPattern = "^//" + customDelimiter + "\\\\n[0-9:," + customDelimiter + "]*";
+        return Pattern.matches(customValidationPattern, formula);
+    }
+
+    public String extractCustomDelimiter(String formula) {
+        return formula.substring(2, 4);
     }
 }
