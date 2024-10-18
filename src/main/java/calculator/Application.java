17695dbe426751795c28aefc93f06e525bd34686
huntedu <best00517@naver.com>
2024-10-19 00:05:02 +0900

    fix(Application): fix custom delimiters
    fixed event to Application:
     - 기본 구문자를 스트링 형식으로 변경
     - 커스텀 구문자를 기본 구문자에
    합치도록 변경
    
     - 기본 구문자와 커스텀 구문자를 둘 다 사용 가능하도록 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,12 +9,16 @@ public class Application {
         Scanner scanner = new Scanner(System.in);
 
         // 숫자의 합을 저장할 변수 선언
-        int answer = 0;
+        int result = 0;
 
         try {
-
+            if (!scanner.hasNextLine()) {
+                System.out.println("결과 : " + result);
+                return;
+            }
             String input = scanner.nextLine();
-            String[] delimiters = {",", ":"}; // 기본 구문자 설정
+
+            String delimiters = ",|:"; // 기본 구문자 설정
 
             // //로 문장이 시작하는 지 체크
             if (input.startsWith("//")) {
@@ -27,7 +31,12 @@ public class Application {
                     // // 뒤에서 \n 이전까지의 문자열을 구분자로 사용
                     String customDelimiters = input.substring(2, endpointIndex);
 
-                    delimiters = customDelimiters.split("");  // 개별 문자를 구분자로 사용
+                    // 커스텀 구분자 배열 생성
+                    String[] customDelimiterArray = customDelimiters.split("");
+
+                    for (String customDelimiter : customDelimiterArray) {
+                        delimiters += "|" + customDelimiter;
+                    }
 
                     // 입력 문자열에서 실제 값만 남기기 (구분자 정의 부분 제외)
                     input = input.substring(endpointIndex + 2);
@@ -43,19 +52,20 @@ public class Application {
 
             for (String numberStr : numbers) {
                 try {
-                    int number = Integer.parseInt(numberStr.trim());  // 숫자로 변환
-                    answer += number;
-                } catch (NumberFormatException e) {
+                    int number = Integer.parseInt(numberStr.trim()); // 숫자로 변환
+                    if (number < 0) {
+                        throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+                    }
+                    result += number;
+                } catch (IllegalArgumentException e) {
                     // 숫자가 아닌 부분은 무시하거나 오류 처리
-                    System.err.println("유효하지 않은 숫자: " + numberStr);
+                    System.err.println(e.getMessage());
                     System.exit(1);
                 }
             }
 
             // 정답 출력
-            System.out.println("결과 : " + answer);
-
-            scanner.close();
+            System.out.println("결과 : " + result);
         } catch (IllegalArgumentException e) {
             System.err.println("오류 : " + e.getMessage());
             System.exit(1);
