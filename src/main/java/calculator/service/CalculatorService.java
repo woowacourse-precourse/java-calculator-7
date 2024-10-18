c47f3b836ad38dcf9af1f7a4d2476e411fd8c2f4
chul5 <och7164@gmail.com>
2024-10-19 00:03:26 +0900

    feat: 커스텀 구분자 패턴 검증 기능 구현

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,7 +1,9 @@
 package calculator.service;
 
 public class CalculatorService {
+	private final String pattern = "//;\\n";
 	private String line;
+	private String delimiter = ":,";
 
 	public CalculatorService(String line) {
 		validate(line);
@@ -11,6 +13,19 @@ public class CalculatorService {
 	private void validate(String line) {
 		if (line == null)
 			throw new IllegalArgumentException("입력값이 null입니다.");
+		if (line.charAt(0) == '/')
+			line = findDelimiter(line);
+	}
+
+	private String findDelimiter(String line) {
+		for (int i = 0; i < pattern.length(); i++) {
+			if (i == 2)
+				continue;
+			if (pattern.charAt(i) != line.charAt(i))
+				throw new IllegalArgumentException("구분자 패턴이 아닙니다.");
+		}
+		delimiter += line.charAt(2);
+		return line.substring(pattern.length());
 	}
 
 }
