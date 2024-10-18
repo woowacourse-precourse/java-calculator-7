45cc0d33eda27c782436a889012722322ae55126
yyysolhhh <solbaram37@gmail.com>
2024-10-18 18:41:42 +0900

    feat: 입력, 예외 처리, 커스텀 구분자 기능 추가
    checkInput
    - 입력값 검사하는 함수
    - 커스텀 구분자 있을 경우 추가
    
    validateInput
    - 입력값에 들어올 수 있는 문자 검사하는 함수
    
    extractDelimiter
    - 커스텀 구분자 추출하는 함수

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,45 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String delimiter = ",|:";
+//        String delimiter = ",:";
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = readLine();
+        try {
+            delimiter = checkInput(input, delimiter);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        //splitInput();
+        //getSum();
+    }
+
+    private static String checkInput(String input, String delimiter) throws IllegalArgumentException {
+        if (input.substring(0, 2).equals("//") && input.contains("\\n")) {
+            String newDelimiter = extractDelimiter(input);
+            delimiter += "|" + newDelimiter;
+            input = input.substring(input.indexOf("\\n") + 2, input.length());
+        }
+        System.out.println("input = " + input);
+        for (char c : input.toCharArray()) {
+            if (!validateInput(c, delimiter)) {
+                throw new IllegalArgumentException("잘못된 입력");
+            }
+        }
+        return delimiter;
+    }
+
+    private static boolean validateInput(char c, String delimiter) {
+        return Character.isDigit(c) || delimiter.indexOf(c) != -1;
+    }
+
+    private static String extractDelimiter(String input) {
+//        String newDelimiter = input.split("//")[1].split("|\n|")[0];
+        return input.substring(2, input.indexOf("\\n"));
     }
 }
