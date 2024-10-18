c8b00bdf10cb0e6fa3f86ca001df0c4bc9b95037
quro <hs970216@naver.com>
2024-10-18 20:57:30 +0900

    feat: 커스텀 구분자 부분 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,39 +2,35 @@ package calculator;
 
 public class Calculator {
 
-    private static final char BASIC1 = ',';
-    private static final char BASIC2 = ':';
+    private static final char DEFAULT_DELIMITER1 = ',';
+    private static final char DEFAULT_DELIMITER2 = ':';
+    private static final String CUSTOM_PREFIX = "//";
+    private static final String CUSTOM_SUFFIX = "\\n";
 
     public int splitStr(String s) {
         if (s.isEmpty()) {
             return 0;
         }
 
-        int idx = s.indexOf("//");
-
         // 커스텀 구분자 있을 경우
-        if (idx != -1) {
-            if (s.charAt(idx + 3) == '\\' && s.charAt(idx + 4) == 'n') {
-                char custom = s.charAt(idx + 2);
+        if (s.startsWith(CUSTOM_PREFIX)) {
+            if (s.startsWith(CUSTOM_SUFFIX, 3)) {
+                char custom = s.charAt(2);
 
-                String sliceS = customDelimiterDelete(s, idx);
+                // 커스텀 구분자 부분 삭제
+                String sliceS = s.substring(5);
 
-                String[] customArr = sliceS.split(BASIC1 + "|" + BASIC2 + "|" + custom);
+                String[] customArr = sliceS.split(DEFAULT_DELIMITER1 + "|" + DEFAULT_DELIMITER2 + "|" + custom);
                 // TODO 반복문 돌며 양수 아닌값 있을 시 예외처리 후 종료
                 // TODO 양수만 있다면 합 반환
+            } else {
+                // TODO "\n" 없다면 예외처리 후 종료
             }
         }
         // 커스텀 구분자 없을 경우
-        String[] defaultArr = s.split("BASIC1|BASIC2");
+        String[] defaultArr = s.split(DEFAULT_DELIMITER1 + "|" + DEFAULT_DELIMITER2);
         // TODO 반복문 돌며 양수 아닌값 있을 시 예외처리 후 종료
         // TODO 양수만 있다면 합 반환
     }
 
-    // 커스텀 구분자 부분 삭제 함수
-    private String customDelimiterDelete(String s, int idx) {
-        String slice1 = s.substring(0, idx);
-        String slice2 = s.substring(idx+5);
-
-        return slice1 + slice2;
-    }
 }
