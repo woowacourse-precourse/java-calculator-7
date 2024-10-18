83b6fd582a783509fa22033e1c68f6ef3a984787
sejinezy <psj8880@naver.com>
2024-10-18 17:30:15 +0900

    feat(getInput): 문자열 출력 메서드 구현

--- src/main/java/calculator/ConsoleIO.java
+++ src/main/java/calculator/ConsoleIO.java
@@ -8,4 +8,8 @@ public class ConsoleIO {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
+
+    public void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
 }
