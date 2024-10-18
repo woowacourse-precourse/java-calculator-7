29d4d926fe39dfed50c784279a575540fbda58e6
Silbet <65993038+Silbet@users.noreply.github.com>
2024-10-18 20:18:24 +0900

    Feat: inPut() 기능 구현
    덧셈을 위한 값을 입력 받기 위한 메소드 구현.
    최종적으론 문자열을 입력 받지만 아직 문자열에서 숫자를 추출하는 기능을 구현하지 못했기 때문에 int형으로 구현함. 나중에 수정 필요.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int operandA = inPut();
+        int operandB = inPut();
+
+        System.out.printf("결과 : %d", operandA + operandB);
+    }
+
+    // 문자열에서 숫자 추출 기능 구현 시 리턴 타입과 구현 내용을 int -> String 으로 수정할 것
+    public static int inPut() {
+        System.out.println("덧셈할 정수를 입력해 주세요.");
+        Scanner operand = new Scanner(System.in);
+        return operand.nextInt();
     }
 }
