fd5dd3c0fff425b14bed47b92e35ad78a9979078
qudcks8084 <qudcks8084@naver.com>
2024-10-18 15:34:31 +0900

    feat(dto) : implement CalculatorDTO class
    Detail
    - add 3 parameter in Class
    - implement 3 getter method
    - implement constructor by Input String

--- src/main/java/calculator/dto/CalculatorDTO.java
+++ src/main/java/calculator/dto/CalculatorDTO.java
@@ -1,4 +1,34 @@
 package calculator.dto;
 
+import java.util.regex.Pattern;
+
 public class CalculatorDTO {
+    private final String str;
+    private final String delim;
+    private final boolean custom;
+
+    public CalculatorDTO(String str) {
+        if(str.startsWith("//")){
+            String[] firstSplit = str.split("\\\\n");
+            this.delim = firstSplit[0].split("//")[1].trim();
+            this.str = firstSplit[1];
+            this.custom = true;
+        }else{
+            this.str = str;
+            this.delim = ",:";
+            this.custom = false;
+        }
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public String getDelim() {
+        return delim;
+    }
+
+    public boolean hasCustomDelim(){
+        return custom;
+    }
 }
