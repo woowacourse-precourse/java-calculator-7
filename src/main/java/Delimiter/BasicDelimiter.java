915a2110d8d019869c463cf88a9162696234be24
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-19 03:50:14 +0900

    feat: 기본 구분자 처리 기능 구현

--- src/main/java/Delimiter/BasicDelimiter.java
+++ src/main/java/Delimiter/BasicDelimiter.java
@@ -1,5 +1,12 @@
 package Delimiter;
 
 public class BasicDelimiter {
-
+    public static int[] getNumber(String input) {
+        String[] tokens = input.split(",|:");
+        int[] numbers = new int[tokens.length];
+        for (int i = 0; i < tokens.length; i++) {
+            numbers[i] = Integer.parseInt(tokens[i]);
+        }
+        return numbers;
+    }
 }
