1e9e0e6fdd7b2b5264ba7af1f9f4a3a900c2d6bd
capDoYeonLee <ghtyu21@naver.com>
2024-10-18 23:36:57 +0900

    refactor: 초기 실행 로직 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,19 +1,25 @@
 package calculator;
 
+import calculator.domain.Calculator;
 import calculator.service.CalculatorService;
 
 public class Application {
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         InputClass inputClass = new InputClass();
-        CalculatorService calculatorService = new CalculatorService();
-
         String inputText = inputClass.input();
+        CalculatorStrategy calculatorStrategy;
+
         if (inputClass.existCustomSeparator(inputText)) {
-            calculatorService.calculateContainCustomSeparator(inputText);
+            calculatorStrategy = new CustomSeparatorStrategy();
+            calculatorStrategy.calculate(inputText);
         }
-        if (!inputClass.existCustomSeparator(inputText)){
-            calculatorService.calculateContainOriginalSeparator(inputText);
+
+        if (inputClass.existOriginalSeparator(inputText)) {
+            calculatorStrategy = new OriginalSeparatorStrategy();
+            calculatorStrategy.calculate(inputText);
         }
     }
 }
