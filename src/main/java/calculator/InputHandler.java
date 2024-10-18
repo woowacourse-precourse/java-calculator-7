0650cbd5ef5e14bb4bc2549d7147866e043bed6c
minseo <alstj778@gmail.com>
2024-10-18 17:16:37 +0900

    feat: 사용자가 커스텀 구분자를 지정할 수 있다

--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -3,10 +3,13 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputHandler {
     private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private static final String EXTRACT_DELIMITER_REGEX = "//(.*?)\\\\n";
 
     public String inputString() {
         return Console.readLine();
@@ -21,7 +24,25 @@ public class InputHandler {
     }
 
     private List<String> parseString(String str) {
+        String customDelimiter = extractCustomDelimiter(str);
+        if (customDelimiter != null) {
+            String inputString = str.replaceAll(EXTRACT_DELIMITER_REGEX, "");
+            String[] result = inputString.split(customDelimiter);
+            return Arrays.asList(result);
+        }
+
         String[] result = str.split(DEFAULT_DELIMITER_REGEX);
         return Arrays.asList(result);
+    }
+
+    private String extractCustomDelimiter(String str) {
+        Pattern pattern = Pattern.compile(EXTRACT_DELIMITER_REGEX);
+        Matcher matcher = pattern.matcher(str);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+
+        return null;
     }
 }
