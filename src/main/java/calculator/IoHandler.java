9c01436dac2b7eccce31f796bedf37c60af7d3d4
soohalee <suha630@naver.com>
2024-10-18 22:08:49 +0900

    feat : 입력 받는 기능 구현

---
+++ src/main/java/calculator/IoHandler.java
@@ -0,0 +1,10 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class IoHandler {
+
+    public static String getUserInput() {
+        return Console.readLine();
+    }
+}
