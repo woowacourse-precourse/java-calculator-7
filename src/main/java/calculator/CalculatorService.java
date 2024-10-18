7e040605b780a8b8dde66b5d3c1421dde2c9f10d
qyinm <qusseun@gmail.com>
2024-10-18 11:35:08 +0900

    feat: 커스텀 구분자 파싱 기능 작성

--- src/main/java/calculator/CalculatorService.java
+++ src/main/java/calculator/CalculatorService.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CalculatorService {
@@ -9,6 +10,20 @@ public class CalculatorService {
 
     public boolean isBlank(String input) {
         return input == null || input.trim().isEmpty();
+    }
+
+    /**
+     * 유저 입력에서 커스텀 구분자 있으면 파싱 후 반환 아니면 빈 문자열 반환
+     * @param input
+     * @return
+     */
+    public String parseCustomSeparator(String input) {
+        Pattern pattern = Pattern.compile("^//(.+?)\\\\n");
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return "";
     }
 
     public long logic(String input) {
