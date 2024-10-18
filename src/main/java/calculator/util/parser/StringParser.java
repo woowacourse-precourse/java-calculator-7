60956a53115a8d7d281f5c6ae337ac1fefdcbc0a
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-18 23:07:21 +0900

    feat(StringParser): StringParser 인터페이스 정의

---
+++ src/main/java/calculator/util/parser/StringParser.java
@@ -0,0 +1,10 @@
+package calculator.util.parser;
+
+public interface StringParser {
+
+    String extractCustomDelimiter(final String str);
+
+    String removeCustomDelimiterFormat(final String str);
+
+    String[] extractTokens(final String str);
+}
