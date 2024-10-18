0b20c6333f71639cc74c04975a3431fe0c1754b4
yyysolhhh <solbaram37@gmail.com>
2024-10-19 05:18:36 +0900

    refactor: Calculator 클래스 추가
    Application class에 있던 함수들 Calculator class로 옮김

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,49 +5,20 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String delimiter = ",|:";
-//        String delimiter = ",:";
-        int sum;
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
-        try {
-            delimiter = checkInput(input, delimiter);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return;
-        }
-        String[] numbers = splitInput(input, delimiter);
-        for (String number : numbers) {
-            System.out.print(number + ' ');
-        }
-//        sum = getSum();
-//        System.out.println("sum = " + sum);
-    }
+        Calculator calculator = new Calculator(input);
 
-    private static String[] splitInput(String input, String delimiter) {
-        return input.split(delimiter);
-    }
-
-    private static String checkInput(String input, String delimiter) throws IllegalArgumentException {
-        if (input.substring(0, 2).equals("//") && input.contains("\\n")) {
-            String newDelimiter = extractDelimiter(input);
-            delimiter += "|" + newDelimiter;
-            input = input.substring(input.indexOf("\\n") + 2, input.length());
-        }
-        for (char c : input.toCharArray()) {
-            if (!validateInput(c, delimiter)) {
-                throw new IllegalArgumentException("잘못된 입력");
-            }
-        }
-        return delimiter;
-    }
-
-    private static boolean validateInput(char c, String delimiter) {
-        return Character.isDigit(c) || delimiter.indexOf(c) != -1;
-    }
-
-    private static String extractDelimiter(String input) {
-//        String newDelimiter = input.split("//")[1].split("|\n|")[0];
-        return input.substring(2, input.indexOf("\\n"));
+//        try {
+//            stringCalculator.checkInput();
+//        } catch (IllegalArgumentException e) {
+//            throw new IllegalArgumentException("잘못된 입력");
+//            e.printStackTrace();
+//            System.exit(1);
+//        }
+        calculator.checkInput();
+        calculator.splitInput();
+        calculator.addNumbers();
+        System.out.println("결과 : " + calculator.getSum());
     }
 }
