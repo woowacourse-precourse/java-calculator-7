3d062e7bf3421493f13e651f9ce2dce974094d48
minSsan <msp1125@hanyang.ac.kr>
2024-10-18 20:24:52 +0900

    feat(CalculatorInputParser): 커스텀 구분자로 파싱하는 기능

--- src/main/java/calculator/CalculatorInputParser.java
+++ src/main/java/calculator/CalculatorInputParser.java
@@ -10,6 +10,25 @@ public class CalculatorInputParser {
     private final static String CUSTOM_DELIMITER_SUFFIX = "\\n";
     private final static String DEFAULT_DELIMITERS = ",:";
 
+    private int findCustomDelimiterBounds(String input) {
+        final int start = input.indexOf(CUSTOM_DELIMITER_PREFIX);
+        final int end = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+
+        if (start == -1 && end == -1) {
+            return -1;
+        }
+
+        return end;
+    }
+
+    private String extractCustomDelimiter(String input) {
+        final int end = findCustomDelimiterBounds(input);
+        if (end == -1) {
+            return "";
+        }
+        return input.substring(CUSTOM_DELIMITER_PREFIX.length(), end);
+    }
+
     private List<Integer> parseNumbers(String input, String regex) {
         try {
             return Arrays.stream(input.split(regex))
@@ -23,7 +42,15 @@ public class CalculatorInputParser {
     }
 
     public List<Integer> parse(String input) {
-        final String regex = "[" + DEFAULT_DELIMITERS + "]";
-        return parseNumbers(input, regex);
+        final String customDelimiter = extractCustomDelimiter(input);
+        final String regex = "[" + DEFAULT_DELIMITERS + customDelimiter + "]";
+
+        final StringBuffer numberStr = new StringBuffer(input);
+        if (!customDelimiter.isEmpty()) {
+            final int end = findCustomDelimiterBounds(input);
+            numberStr.delete(0, end + CUSTOM_DELIMITER_SUFFIX.length());
+        }
+
+        return parseNumbers(numberStr.toString(), regex);
     }
 }
