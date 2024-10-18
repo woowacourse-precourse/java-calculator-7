c4b08f9593861b831ddd1489e90f4949adc8059e
AYEOOON <k93853471@gmail.com>
2024-10-18 15:56:27 +0900

    feat: 사용자 입력 받기 기능 추가
    - 사용자로부터 계산할 문자열을 입력받는다.
    - null 또는 빈 문자열 입력에 대한 기본 처리.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+
+        // 전체 입력을 받기 위해 nextLine() 사용
+        String susik = sc.nextLine();
+
+        if (susik == null || susik.trim().isEmpty()) {
+            System.out.println("결과 : " + 0);
+        }
     }
 }
