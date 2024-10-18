7de026011b1963ebdcec31498870c09e1cb275e6
OSEUK <tpdnrzmzm3@naver.com>
2024-10-18 21:40:48 +0900

    feat(main): 입출력
    입력, 출력, 예외처리를 보여주는 메인 클래스

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,11 +2,22 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class Application {
+import java.util.regex.Pattern;
+
+public class Application{
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String s = Console.readLine();  // 문자열 입력
 
-        String[] strings = s.split("[,:]"); // 쉼표와 콜론으로 문자열 분리
+        Calculator calculator = new Calculator();
+        int sum = calculator.calculate(s);
+
+        if (sum == -1){
+            throw new IllegalArgumentException();
+        }
+
+        System.out.println("결과 : " + sum);
     }
 }
