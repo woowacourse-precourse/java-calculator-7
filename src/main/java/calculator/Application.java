5b204494fe67351a19cac3b4ba13e00361ee14a5
ppparkta <pppppark2@gmail.com>
2024-10-18 17:23:07 +0900

    feat: 스켈레톤 코드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,13 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 문자열 입력드받기
+
+        // TODO: 문자열 파싱
+        // TODO: 구분자 파싱 및 저장 -> 예외처리
+
+        // TODO: 연산 문자열 파싱 및 저장 -> 예외처리
+
+        // TODO: 결과 출력
     }
 }
