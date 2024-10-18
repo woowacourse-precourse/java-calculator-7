8ec12a661e8f3183000f5858017453aef9129b24
parksomii <somi020605@gmail.com>
2024-10-19 00:54:44 +0900

    feat: 잘못된 입력값에 대한 예외 처리 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,13 +4,17 @@ import java.util.Arrays;
 
 public class StringCalculator {
     public int add(String input) {
-        if (input.startsWith("//")) {
-            int delimiterIndex = input.indexOf("\n");
-            String customDelimiter = input.substring(2, delimiterIndex);
-            input = input.substring(delimiterIndex + 1);
-            return Arrays.stream(input.split(customDelimiter)).mapToInt(Integer::parseInt).sum();
+        try {
+            if (input.startsWith("//")) {
+                int delimiterIndex = input.indexOf("\n");
+                String customDelimiter = input.substring(2, delimiterIndex);
+                input = input.substring(delimiterIndex + 1);
+                return Arrays.stream(input.split(customDelimiter)).mapToInt(Integer::parseInt).sum();
+            }
+            return Arrays.stream(input.split("[,|:]")).mapToInt(Integer::parseInt).sum();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 입력 값입니다.");
         }
-        return Arrays.stream(input.split("[,|:]")).mapToInt(Integer::parseInt).sum();
     }
 }
 
