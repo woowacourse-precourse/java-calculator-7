45ec8d042696c17063ad170bfb25d1a4b9c4f8a0
Silbet <65993038+Silbet@users.noreply.github.com>
2024-10-18 20:41:46 +0900

    Feat: add 기능 구현
    인자 2개를 더해 return 하는 기능 구현.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,7 +7,8 @@ public class Application {
         int operandA = inPut();
         int operandB = inPut();
 
-        System.out.printf("결과 : %d", operandA + operandB);
+        int result = add(operandA, operandB);
+        System.out.println("결과 : " + result);
     }
 
     // 문자열에서 숫자 추출 기능 구현 시 리턴 타입과 구현 내용을 int -> String 으로 수정할 것
@@ -15,5 +16,9 @@ public class Application {
         System.out.println("덧셈할 정수를 입력해 주세요.");
         Scanner operand = new Scanner(System.in);
         return operand.nextInt();
+    }
+
+    public static int add(int operandA, int operandB) {
+        return operandA + operandB;
     }
 }
