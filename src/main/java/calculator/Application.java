52dc23506286b3d6d51e7a981b81e42c6b50786d
jimmy0524 <10jmin04@naver.com>
2024-10-19 04:53:43 +0900

    refactor: view 클래스에서 입력받도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,11 @@
 package calculator;
 
-import calculator.controller.CalculatorController;
-import camp.nextstep.edu.missionutils.Console;
+import calculator.view.CalculatorView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        // 입력 받기
-        String input = Console.readLine();
-
-        // controller 호출
-        CalculatorController controller = new CalculatorController();
+        CalculatorView calculatorView = new CalculatorView();
+        calculatorView.getInput();
     }
 }
--- src/main/java/calculator/view/CalculatorView.java
+++ src/main/java/calculator/view/CalculatorView.java
@@ -1,6 +1,17 @@
 package calculator.view;
 
+import calculator.controller.CalculatorController;
+import camp.nextstep.edu.missionutils.Console;
+
 public class CalculatorView {
+    public void getInput() {
+        CalculatorController controller = new CalculatorController();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        controller.processInput(input);
+    }
+
     public String displayOutput(int result) {
         String output = "결과 : " + result;
         System.out.println(output);
