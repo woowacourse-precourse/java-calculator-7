5a07a3d3ddf2a3aec5cde9981e0f818a4e41f1e0
sinminseok12 <sinminseok12@naver.com>
2024-10-19 00:47:38 +0900

    test : 커스텀 구분자 숫자 분리 기능 테스트

--- src/test/java/calculator/ConverterTest.java
+++ src/test/java/calculator/ConverterTest.java
@@ -8,11 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConverterTest {
 
-    Converter converter = new Converter("[,:]","//","\n");
+    Converter converter = new Converter("[,:]","//","\\n");
 
     @Test
-    void 문자열_숫자들로_변환(){
+    void 기본_구분자로_숫자_분리(){
         List<Integer> numbers = converter.convertNumbersFromString("1,2:3");
+        assertThat(numbers).isEqualTo(List.of(1,2,3));
+    }
+
+    @Test
+    void 커스텀_구분자로_숫자_분리(){
+        List<Integer> numbers = converter.convertNumbersFromString("//;\\n1,2;3");
         assertThat(numbers).isEqualTo(List.of(1,2,3));
     }
 }
