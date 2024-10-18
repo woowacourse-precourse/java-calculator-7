3fa2b3212936e7f1a57e68a88f0af688ca1cd4ab
이윤빈 <kanado20000@naver.com>
2024-10-18 23:08:42 +0900

    ✅feat(InputValidator): 입력받은 문자열의 커스텀 구분자 형식과 피연사자가 자연수인지 유효성 검사 함수 추가

---
+++ src/main/java/calculator/validation/InputValidator.java
@@ -0,0 +1,50 @@
+package calculator.validation;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+import calculator.model.CustomDelimiter;
+import calculator.model.Splitter;
+
+public class InputValidator {
+    private static final String NON_NATURAL_NUMBER = "자연수를 입력해주세요.";
+    private static final String INVALID_CUSTOM_DELIMITER = "커스텀 구분자가 정상적으로 입력되지 않았습니다.";
+
+    public void MainInputValidator(CustomDelimiter formula){
+        if (formula.checkCustomDelimiter()){
+            validateCustomDelimiter(formula.getFormula());
+        }
+        validateNatureNumber(formula);
+    }
+
+    private void validateCustomDelimiter(String numbers){
+        Pattern pattern = Pattern.compile("//(.*?)\n");
+        Matcher matcher = pattern.matcher(numbers);
+        if (!numbers.contains("//") || !numbers.contains("\n")){
+            throw new IllegalArgumentException(INVALID_CUSTOM_DELIMITER);
+        }
+        if (matcher.group(1).matches(".*\\d.*")){
+            throw new IllegalArgumentException(INVALID_CUSTOM_DELIMITER);
+        }
+        if (matcher.group(1) == "" || matcher.group(1) == " "){
+            throw new IllegalArgumentException(INVALID_CUSTOM_DELIMITER);
+        }
+    }
+
+    private void validateNatureNumber(CustomDelimiter formula){
+        String numbers = formula.getFormula();
+        numbers = formula.SplitDelimiter();
+        if(numbers.isEmpty()){
+            throw new IllegalArgumentException(NON_NATURAL_NUMBER);
+        }
+        Splitter splitter = new Splitter(formula);
+        String[] splitedNumbers = splitter.getSplittedNumbers();
+        for(String number : splitedNumbers){
+            if (!number.matches("\\d+")){
+                throw new IllegalArgumentException(NON_NATURAL_NUMBER);
+            }
+        }
+    }
+
+}
