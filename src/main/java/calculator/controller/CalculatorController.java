260c9b0ac96788345218ad48a44422b6079c411a
Goohong Chung <9rrong@uoslife.team>
2024-10-19 01:58:57 +0900

    feat(CalculatorController): `InputView`에 `문자열 입력`을 요청한다.

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,30 @@
+package calculator.controller;
+
+import calculator.model.CalculationString;
+import calculator.model.Calculator;
+import calculator.model.StringHandler;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class CalculatorController {
+    private final Calculator calculator;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final StringHandler stringHandler;
+
+    public CalculatorController(Calculator calculator, InputView inputView, OutputView outputView,
+                                StringHandler stringHandler) {
+        this.calculator = calculator;
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.stringHandler = stringHandler;
+    }
+
+    public void runCalculator() {
+        CalculationString calculationString = readInput();
+    }
+
+    private CalculationString readInput() {
+        return inputView.readInput();
+    }
+}
