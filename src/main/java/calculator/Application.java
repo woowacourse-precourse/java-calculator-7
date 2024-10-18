a49c59fa421fd39503f8b085b64e9901256499b9
fortune-man <wnguddl96@knou.ac.kr>
2024-10-18 20:08:42 +0900

    feat(Application) : 프로그램 시작점 정의

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String readLine = Console.readLine();
+        // 문자열을 입력하면 정수 연산 결과를 출력
+        System.out.println(calculating(readLine));
     }
 }
