09688cd8fd00f1b193ce40c9961ba995135e755f
hongjinsu <kaameo12@gmail.com>
2024-10-18 09:45:37 +0900

    feat: Add basic delimiters (,) and (:) to string calculator
    - Add function to split a string by comma (,) and colon (:) and return the sum of the numbers
    - Return 0 when the user input is an empty string

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String userInput = Console.readLine();
+        Console.close();
+        int[] integerArr = new int[512];
+        int sum = 0;
+        int idx = 0;
+        for (int i = 0; i < userInput.length(); i++) {
+            char inputChar = userInput.charAt(i);
+            if (Character.isDigit(inputChar)) {
+                integerArr[idx++] = inputChar - '0';
+            } else if (inputChar == ',' || inputChar == ':') {
+                continue;
+            }else {
+                throw new IllegalArgumentException();
+            }
+        }
+        for (int i : integerArr) {
+            sum += i;
+        }
+        System.out.println("결과 : " + sum);
     }
 }
