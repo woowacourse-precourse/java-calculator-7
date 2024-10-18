3de82d92c781a940be890d0f41afc6a67fc5bb30
JinGoo <zzgg3040@naver.com>
2024-10-18 21:01:57 +0900

    refactor: runner 메서드 분리 & 인스턴스 생성 방지

--- src/main/java/calculator/runner/CalculatorRunner.java
+++ src/main/java/calculator/runner/CalculatorRunner.java
@@ -5,12 +5,30 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class CalculatorRunner {
 
+    private CalculatorRunner() {}
+
     public static void run() {
+        turnOn();
+        Calculator calculator = getCalculator(readValue());
+        printResult(calculator.sum());
+    }
+
+    private static void turnOn() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    private static String readValue() {
         String value = Console.readLine();
         Console.close();
-
-        Calculator calculator = Calculator.enterValue(value);
-        System.out.println("결과 : " + calculator.sum());
+        return value;
     }
+
+    private static Calculator getCalculator(String value) {
+        return Calculator.enterValue(value);
+    }
+
+    private static void printResult(long result) {
+        System.out.println("결과 : " + result);
+    }
+
 }
