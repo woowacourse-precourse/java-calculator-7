2d62842cd82ba5810d8a1ef98d27f92c5b851a73
woogym <kwj9294@gmail.com>
2024-10-18 22:32:31 +0900

    fix(main): modify use controller method

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,20 @@
 package calculator;
 
 import calculator.controller.CalculatorController;
+import calculator.util.refine.DelimiterExtractor;
+import calculator.util.refine.SeparatePositiveNumber;
+import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        CalculatorController calculatorController = new CalculatorController();
+        DelimiterExtractor delimiterExtractor = new DelimiterExtractor();
+        SeparatePositiveNumber separatePositiveNumber = new SeparatePositiveNumber(delimiterExtractor);
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        CalculatorController calculatorController = new CalculatorController(separatePositiveNumber, inputView, outputView);
         calculatorController.startCalculate();
     }
 }
