9e8cbd8d982435ef41eaafff701e4ac4782e6366
Koo-EunSung <ku903@naver.com>
2024-10-18 18:36:02 +0900

    Feat: 입력 전 안내 문구 출력

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,6 +1,10 @@
 package calculator.view;
 
 public class OutputView {
+    public void inform() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
     public void result(Long sum) {
         System.out.println("결과 : " + sum);
     }
