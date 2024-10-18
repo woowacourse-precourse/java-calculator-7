184a7405c038c592837c07b3aa5edea5be6d4c95
pakaheo <k70923@konkuk.ac.kr>
2024-10-18 09:37:41 +0900

    feat : new class for sorting input type

--- src/main/java/calculator/domain/InputTypeSorter.java
+++ src/main/java/calculator/domain/InputTypeSorter.java
@@ -1,2 +1,26 @@
-package calculator.domain;public class InputTypeSorter {
+package calculator.domain;
+
+import calculator.constants.Constants;
+
+public class InputTypeSorter {
+    private static final String DOUBLE_SLASH = "//";
+    private static final String ESCAPE = "\\n";
+
+    public static Input sort(String text) {
+        if (containsBasicDelimiter(text)) {
+            return new BasicDelimiterInput(text);
+        }
+        if (containsCustomDelimiter(text)) {
+            return new CustomDelimiterInput(text);
+        }
+        return new DefaultInput(text);
+    }
+
+    public static boolean containsBasicDelimiter(String text) {
+        return text.contains(Constants.COMMA) || text.contains(Constants.COLON);
+    }
+
+    public static boolean containsCustomDelimiter(String text) {
+        return text.contains(DOUBLE_SLASH) && text.contains(ESCAPE);
+    }
 }
