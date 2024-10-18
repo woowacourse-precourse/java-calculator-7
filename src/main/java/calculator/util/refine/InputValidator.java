c5eeeae263adad1082b39b83659445f9fc21b4ea
woogym <kwj9294@gmail.com>
2024-10-18 22:17:56 +0900

    feat(util): implement inputvalidation logic

---
+++ src/main/java/calculator/util/refine/InputValidator.java
@@ -0,0 +1,30 @@
+package calculator.util.refine;
+
+import static calculator.util.exception.CalculatorValidationException.*;
+
+public class InputValidator {
+
+    static void validateDelimiterAfterNewLinePresence(int delimiterIndex) {
+        if (delimiterIndex == -1) {
+            throw new validateDelimiterAfterNewLinePresenceException();
+        }
+    }
+
+    static void validateNonNegativeNumber(int number) {
+        if (number < 0) {
+            throw new validateNonNegativeNumberException();
+        }
+    }
+
+    static void validateSingleCharacterDelimiter(String delimiter) {
+        if (delimiter.length() != 1) {
+            throw new validateSingleCharacterDelimiterException();
+        }
+    }
+
+    static void validateIsInteger(String token) {
+        if (!token.matches("-?\\d+")) {
+            throw new validateIsIntegerException();
+        }
+    }
+}
