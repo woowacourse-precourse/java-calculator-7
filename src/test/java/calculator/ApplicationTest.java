a008f15d84eba09ceceda97a9bc3783cac2cc2f6
changuii <rhljh7410@gmail.com>
2024-10-18 11:37:21 +0900

    test: 커스텀 구분자에 정규식의 특수 문자가 들어오는 경우 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -91,7 +91,39 @@ class ApplicationTest extends NsTest {
 			run(input);
 
 			//then
-			assertThat(output()).contains("결과 : " + 1);
+			assertThat(output()).contains("결과 : 1");
+		});
+	}
+
+	@Test
+	@DisplayName("커스텀 문자열에 특수 문자가 포함되는 입력 테스트")
+	public void hasSpecialCharTest(){
+		assertSimpleTest(()->{
+			// given
+			String input = "//\\\\n1\\2\\3";
+
+			//when
+			run(input);
+
+			// then
+			assertThat(output()).contains("결과 : 6");
+
+		});
+	}
+
+	@Test
+	@DisplayName("커스텀 문자열에 특수 문자 '[', ']'가 포함되며 수식에도 포함되는 입력 테스트")
+	public void hasSpecialCharsTest(){
+		assertSimpleTest(()->{
+			// given
+			String input = "//[\\n//]\\n1[2]3";
+
+			//when
+			run(input);
+
+			// then
+			assertThat(output()).contains("결과 : 6");
+
 		});
 	}
 
