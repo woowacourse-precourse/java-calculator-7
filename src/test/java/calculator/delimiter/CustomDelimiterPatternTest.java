8f3497ef3e63e1a0b31baa2731dbfd440ab51b0a
junest66 <chlwndks333@gmail.com>
2024-10-19 03:01:00 +0900

    test(CustomDelimiterPatternTest): 커스텀 정규식 패턴 테스트 추가

---
+++ src/test/java/calculator/delimiter/CustomDelimiterPatternTest.java
@@ -0,0 +1,58 @@
+package calculator.delimiter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CustomDelimiterPatternTest {
+
+    private static CustomDelimiterPattern pattern;
+
+    @BeforeAll
+    static void beforeAll() {
+        pattern = new CustomDelimiterPattern();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'//;\\n1;2;3'",
+            "'//!\\n'"
+    })
+    public void 정상적인_상황_성공(String input) {
+        assertThat(pattern.validate(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'//!\\n-1'",
+            "'//!\\n0!5'",
+            "'//!\\n1!-5'",
+    })
+    public void 구분자는_정상인데_숫자가_양수가_아닌경우(String input) {
+        assertThat(pattern.validate(input)).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'//5\\n'",
+            "'//2\\n2'",
+            "'//0\\n5'",
+    })
+    public void 구분자가_숫자일때(String input) {
+        assertThat(pattern.validate(input)).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'asd'",
+            "'aswe🤪'",
+            "'12:안녕하세요'",
+            "'1:안,녕:3'"
+    })
+    public void 아예_패턴에_맞지_않는_경우(String input) {
+        assertThat(pattern.validate(input)).isFalse();
+    }
+
+}
