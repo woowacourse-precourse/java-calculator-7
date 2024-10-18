becf859e46a4bf4f4f140e1748287a67eaf5b76c
unluckyboy214 <janggoon0214@naver.com>
2024-10-19 00:35:11 +0900

    1019 IllegalArgumentException apply

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,14 +6,28 @@ public class Application {
     public static void main(String[] args) {
         String input = Console.readLine();
 
-        String[] input_integer = input.split(",|:");
+        String delimiter = ",|:";
+
+        String[] input_integer = input.split(delimiter);
 
         int sum = 0;
 
         for(String integer : input_integer){
-            sum += Integer.parseInt(integer.trim());
+            if(integer.trim().isEmpty()) continue;
+
+            try{
+                int number = Integer.parseInt(integer.trim());
+                sum += number;
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println("invalid");
+                return;
+            }
+
         }
 
         System.out.println("결과 : " + sum);
     }
+
+
 }
