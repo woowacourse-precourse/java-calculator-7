6cff29cfb5f75d031c6f1788bf8584d9b443dc73
discphy <dinophy@nate.com>
2024-10-18 21:21:52 +0900

    refactor: 숫자 문자열로 인한 인자 변수명 변경

--- src/main/java/calculator/converter/StringToBigIntegerConverter.java
+++ src/main/java/calculator/converter/StringToBigIntegerConverter.java
@@ -4,9 +4,9 @@ import java.math.BigInteger;
 
 public class StringToBigIntegerConverter {
 
-    public BigInteger convert(String input) {
+    public BigInteger convert(String number) {
         try {
-            BigInteger result = new BigInteger(input);
+            BigInteger result = new BigInteger(number);
 
             if (result.compareTo(BigInteger.ZERO) <= 0) {
                 throw new IllegalArgumentException("입력 값이 양수가 아닙니다.");
