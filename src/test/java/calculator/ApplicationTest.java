987e3ceef6bb9e825a1f7868ce75d6fb705f622a
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-10-18 16:49:49 +0900

    :necktie: test: 공백 검사를 위한 테스트 코드 추가
    - 공백이 존재하는 경우 IllegalArgumentException 을 발생
    - 공백이 존재하지 않는 경우 정살 실행

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -6,8 +6,29 @@ import org.junit.jupiter.api.Test;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 공백이_존재하면_예외처리_공백이_존재하는_경우(){
+        // given
+        String inputWithBlank = "input test";
+
+        //when & then
+        assertThrows(IllegalArgumentException.class, () ->{
+            Application.checkBlankExist(inputWithBlank);
+        });
+    }
+
+    @Test
+    void 공백이_존재하면_예외처리_공백이_존재하지_않는_경우(){
+        // given
+        String inputWithBlank = "inputtest";
+        //when & then
+        Application.checkBlankExist(inputWithBlank);
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
