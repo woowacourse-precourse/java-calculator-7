ec766b3730829c5c7d342983a1492deaf9646745
junest66 <chlwndks333@gmail.com>
2024-10-19 02:59:51 +0900

    refactor(CustomDelimiterPattern): 커스텀 정규식 패턴 변경
    - 숫자에서 0 제외

--- src/main/java/calculator/delimiter/CustomDelimiterPattern.java
+++ src/main/java/calculator/delimiter/CustomDelimiterPattern.java
@@ -3,7 +3,7 @@ package calculator.delimiter;
 import java.util.regex.Pattern;
 
 public class CustomDelimiterPattern implements DelimiterPattern {
-    private static final String REGEX = "^//(\\D)\\\\n\\d*(\\1\\d+)*$";
+    private static final String REGEX = "^//(\\D)\\\\n([1-9]\\d*|$)(\\1[1-9]\\d*)*$";
 
     @Override
     public boolean validate(String input) {
