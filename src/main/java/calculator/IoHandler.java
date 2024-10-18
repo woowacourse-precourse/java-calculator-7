37230a7ec4090c41402833c84635fd5e32c81a0d
soohalee <suha630@naver.com>
2024-10-18 22:09:37 +0900

    feat : 출력하는 기능 구현

--- src/main/java/calculator/IoHandler.java
+++ src/main/java/calculator/IoHandler.java
@@ -7,4 +7,8 @@ public class IoHandler {
     public static String getUserInput() {
         return Console.readLine();
     }
+
+    public static <T> void print(T obj) {
+        System.out.print(String.valueOf(obj));
+    }
 }
