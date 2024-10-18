9637562758dff31fa2276122b1356736b47e49c1
gitubanana <ptyolo666@gmail.com>
2024-10-18 14:22:29 +0900

    style: fix typo (SUFFIX -> PREFIX)
    접두사를 접미사로 잘못 이름을 지어서 변경한다.

--- src/main/java/calculator/CalculatorOutput.java
+++ src/main/java/calculator/CalculatorOutput.java
@@ -1,12 +1,12 @@
 package calculator;
 
 public class CalculatorOutput {
-    private static final String PRINT_SUFFIX = "결과 : ";
+    private static final String PRINT_PREFIX = "결과 : ";
 
     private CalculatorOutput() {
     }
 
     public static void print(double result) {
-        System.out.println(PRINT_SUFFIX + result);
+        System.out.println(PRINT_PREFIX + result);
     }
 }
