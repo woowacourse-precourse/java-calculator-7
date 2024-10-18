d7aa23e93c488c4baeb0d7a277a7411ff3911bbc
kimdev <kim__dev@hufs.ac.kr>
2024-10-18 22:04:35 +0900

    test(Calculator): 수 합계 테스트 케이스 추가

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import calculator.Model.Input;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    @DisplayName("합계 출력")
+    public void 간단한_합계를_출력() {
+        String testText = "4:7";
+        Input input = new Input();
+        input.getInputText(testText);
+
+        Calculator calculator = new Calculator();
+        assertEquals(calculator.sumAllNumbers(input), 11);
+    }
+}
