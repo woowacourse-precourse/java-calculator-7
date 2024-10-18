7b1f5d883f6f624ab432524d9e9b20979bde6498
Zrfv <youngoh07015@gmail.com>
2024-10-18 11:46:13 +0900

    feat(CalculatorController): 잘못된 값 입력 시 예외 발생 기능 추가
    - CalculatorController 클래스에 예외 발생 기능 추가
    - 기능 구현에 따른 README 체크 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -12,14 +12,21 @@ public class CalculatorController {
     }
 
     public int processInput() {
-        String inputString = inputView.getInputString();
+        try {
+            String input = inputView.getInputString();
 
-        Delimiter delimiter = new Delimiter(inputString);
-        delimiter.checkCustomDelimiter();
-        String[] splitNumbers = delimiter.splitString();
+            Delimiter delimiter = new Delimiter(input);
+            delimiter.checkCustomDelimiter();
+            String[] splitNumbers = delimiter.splitString();
 
-        Number number = new Number(splitNumbers);
-        return number.addCalculator();
+            Number number = new Number(splitNumbers);
+            return number.addCalculator();
+
+        } catch (IllegalArgumentException e) {
+            // 예외 발생 시 메시지 출력 및 애플리케이션 종료
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
 
