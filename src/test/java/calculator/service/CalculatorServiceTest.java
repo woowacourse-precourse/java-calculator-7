5461d16c2b28f25db7ef8a52b2632f52fa49f47e
Gothax <scott990328@gmail.com>
2024-10-18 21:29:55 +0900

    test: 문자열 덧셈 기능 테스트
    case-커스텀 구분자인 경우, 기본 구분자인 경우

--- src/test/java/calculator/service/CalculatorServiceTest.java
+++ src/test/java/calculator/service/CalculatorServiceTest.java
@@ -1,5 +1,6 @@
 package calculator.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,12 +8,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorServiceTest {
     CalculatorService calculatorService = new CalculatorService();
 
-    @Test
+    @DisplayName("커스텀 구분자가 없는 경우")
+    @Test()
     void executeAdd() {
         // given
-
         // when
         Integer answer = calculatorService.executeAdd("1,2:3");
+        // then
+        assertEquals(6, answer);
+    }
+
+    @DisplayName("커스텀 구분자가 있는 경우")
+    @Test()
+    void executeAddWithCustomSeparator() {
+        // given
+        // when
+        Integer answer = calculatorService.executeAdd("//;\n1;2;3");
         // then
         assertEquals(6, answer);
     }
