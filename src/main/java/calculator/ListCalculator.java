b33fd09a1041655b360cbafa66595a9bea607dea
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-10-19 00:30:14 +0900

    refatcor(ListCalculator): list에서 sum을 구하는 로직을 reduce 함수로 변경

--- src/main/java/calculator/ListCalculator.java
+++ src/main/java/calculator/ListCalculator.java
@@ -5,8 +5,6 @@ import java.util.List;
 public class ListCalculator {
 
     public long calculate(List<Long> input) {
-        long sum = 0;
-        for(Long i : input) sum += i;
-        return sum;
+        return input.stream().reduce(0L, Long::sum);
     }
 }
