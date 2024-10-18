96a8ae11ecddbf638feb23081193d652d3870b6d
Devheun <dudwo5600@korea.ac.kr>
2024-10-18 14:58:27 +0900

    feat: add exception about number custom delimiter
    - check whether custom delimiter is number or not
    - remove redundancy code about custom delimiter

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -18,11 +18,12 @@ public class Application {
         String values = "";
 
         Pattern customDelimiterPattern = Pattern.compile("//(.)\\\\n(.*)");
-        if (Pattern.matches("//(.)\\\\n(.*)", input)) { // 입력값이랑 정규표현식이랑 일치하는지 확인
-            Matcher matcher = customDelimiterPattern.matcher(input);
-            if (matcher.matches() && !"-".equals(matcher.group(1))) { // 입력값이 해당 정규표현식과 완벽하게 일치하는지 확인, 음수기호 안되게
-                delimiter += "|" + matcher.group(1);
-                values += matcher.group(2);
+        Matcher matcher = customDelimiterPattern.matcher(input);
+        if (matcher.matches()) { // 입력값이 해당 정규표현식과 완벽하게 일치하는지 확인
+            String customDelimiter = matcher.group(1);
+            if (!"-".equals(customDelimiter) && !customDelimiter.matches("\\d")) { // 음수기호 및 숫자 제외
+                delimiter += "|" + customDelimiter;
+                values = matcher.group(2);
             }
         }
 
