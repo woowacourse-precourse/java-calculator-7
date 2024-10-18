953ad7d084094e1cb4b7bb1769425abde48a8f24
momoysm <90163578+momoysm@users.noreply.github.com>
2024-10-18 20:36:22 +0900

    feat: 기본 구분자 및 커스텀 구분자 검증

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,12 +1,28 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+
+    // 커스텀 구분자 정규식
+    private static final Pattern customDelimiterRegex = Pattern.compile("^//([\\D]+)\\\\n([1-9][0-9]*((\\1|,|:)[1-9][0-9]*)*)$");
 
     public static int start(String input) {
 
         // 빈 문자열 검증
         if (input.isBlank()) {
             return 0;
+        }
+
+        // 기본 구분자: 쉼표와 콜론
+        String delimiter = "[,:]";
+        // 커스텀 구분자 확인
+        Matcher matcher = customDelimiterRegex.matcher(input);
+
+        if (matcher.matches()) {
+            delimiter = Pattern.quote(matcher.group(1));  // 커스텀 구분자를 추출하여 패턴으로 변경
+            input = matcher.group(2);  // 숫자 부분 추출
         }
 
         return 0;
