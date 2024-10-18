7899883524bcf071b0337e2c8fe9f39628ade871
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-18 17:14:22 +0900

    feat: 계산기 내 입력부, 합계부 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,2 +1,31 @@
-package calculator;public class Calculator {
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class Calculator {
+
+    private static final String INPUT = "덧셈할 문자열을 입력해주세요.";
+
+    public static void Run() {
+        System.out.println(INPUT);
+        String input = Console.readLine();
+        int sum = Sum(input);
+        System.out.println("sum = " + sum);
+    }
+
+    private static int Sum(String input) {
+        String contents = Parser.getNumberString(input);
+        System.out.println("contents = " + contents);
+        List<String> separators = Parser.parse(input);
+        int[] numbers = Parser.getNumbers(contents, separators);
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public static void End() {
+    }
 }
