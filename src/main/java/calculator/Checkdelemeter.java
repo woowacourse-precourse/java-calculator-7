ce3a1a98ab5d708347ab7a4009997ce91943a2fe
MinJae00 <exceedmin@gmail.com>
2024-10-18 21:12:20 +0900

    refact: ArrayList형식 String으로 변경, 코드 단순화

--- src/main/java/calculator/Checkdelemeter.java
+++ src/main/java/calculator/Checkdelemeter.java
@@ -1,31 +1,37 @@
 package calculator;
 import calculator.GlobalConstant;
+
+import java.util.ArrayList;
+
 public class Checkdelemeter {
 
-    char[] delemeter = new char[3];
+    ArrayList<String> delemeter = new ArrayList<String>();
+    String slash = "//";
+    String line = "\\\\n";
+    String check_slash = "";
+    String check_line = "";
+    String adding_delemeter;
 
-    char[] AddDelemeter(String input){
-        String check_slash = "";
-        String check_line = "";
-        String slash = "//";
-        String line = "\\n";
-        check_slash += input.charAt(0);
-        check_slash += input.charAt(1);
-        check_line += input.charAt(3);
-        check_line += input.charAt(4);
-        char adding_delemeter;
+    ArrayList<String> AddDelemeter(String input){
+        //최소 길이가 5 이상이어야 delemeter확인 가능
+        //길이가 5 미만이라는 말은 구분자 가능성 없다는 말
+        if(input.length() < 6){
+            return delemeter;
+        }
+        check_slash = input.substring(0,2);
+        check_line = input.substring(3,6);
+
         //input의 사이즈 확인 예외처리
-        if(slash == check_slash && line == check_line){
-            adding_delemeter = input.charAt(2);
-            delemeter[2] = adding_delemeter;
-            GlobalConstant.StartIndex = 5;
+        if(slash.equals(check_slash) && line.equals(check_line)){
+            adding_delemeter = Character.toString(input.charAt(2));
+            delemeter.add(adding_delemeter);
+            GlobalConstant.StartIndex = 6;
             // delemeter가 유효한 char인지 예외처리
         }
         return delemeter;
     }
-
     Checkdelemeter(){
-        delemeter[0] = ',';
-        delemeter[1] = ':';
+        delemeter.add(",");
+        delemeter.add(":");
     }
 }
