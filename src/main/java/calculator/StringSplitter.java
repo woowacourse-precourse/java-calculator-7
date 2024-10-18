94f22fa09c628232415eb6988174241e64711634
sinyebin <dpqls0625@naver.com>
2024-10-19 00:31:49 +0900

    feat: 커스텀 구분자("//"와 "\n" 사이에 위치하는 문자)로 분리

--- src/main/java/calculator/StringSplitter.java
+++ src/main/java/calculator/StringSplitter.java
@@ -1,17 +1,53 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringSplitter {
     private static final String BASIC_DELIMITER = ",|:";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
     private static final String NO_DELIMITER_ERROR = "입력 문자열에 구분자가 없습니다.";
+    private static final int CUSTOM_DELIMITER_GROUP = 1;
+    private static final int NUMBER_GROUP = 2;
 
     public String[] split(String input) {
+        Matcher matcher = getCustomDelimiterMatcher(input);
+        if (matcher != null) {
+            return handleCustomDelimiter(matcher);
+        }
         validateBasicDelimiter(input);
-        return input.split(BASIC_DELIMITER);
+        return splitWithBasicDelimiter(input);
+    }
+
+    private Matcher getCustomDelimiterMatcher(String input) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        if (matcher.find()) {
+            return matcher;
+        }
+        return null;
+    }
+
+
+    private String[] handleCustomDelimiter(Matcher matcher) {
+        String customDelimiter = Pattern.quote(matcher.group(CUSTOM_DELIMITER_GROUP));
+        String numbers = matcher.group(NUMBER_GROUP);
+        return splitWithCustomDelimiter(numbers, customDelimiter);
+    }
+
+    private String[] splitWithCustomDelimiter(String input, String customDelimiter) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(NO_DELIMITER_ERROR);
+        }
+        return input.split(customDelimiter);
     }
 
     private void validateBasicDelimiter(String input) {
         if (!input.contains(",") && !input.contains(":")) {
             throw new IllegalArgumentException(NO_DELIMITER_ERROR);
         }
+    }
+
+    private String[] splitWithBasicDelimiter(String input) {
+        return input.split(BASIC_DELIMITER);
     }
 }
--- src/test/java/calculator/StringSplitterTest.java
+++ src/test/java/calculator/StringSplitterTest.java
@@ -23,4 +23,20 @@ class StringSplitterTest {
         assertThatThrownBy(() -> splitter.split("123"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("커스텀으로 구분자 분리")
+    @Test
+    void 커스텀구분자_분리() {
+        StringSplitter splitter = new StringSplitter();
+        String result[] = splitter.split("//;\n1;2;3");
+        assertThat(result).containsExactly("1", "2", "3");
+    }
+
+    @DisplayName("커스텀 구분자 존재 숫자가 없을 경우 예외 발생")
+    @Test
+    void 커스텀_구분자_숫자_없음_예외_테스트() {
+        StringSplitter splitter = new StringSplitter();
+        assertThatThrownBy(() -> splitter.split("//;\n"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
