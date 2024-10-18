d0c0160298ddb5a202d42fa184df83b55e4cd25d
CHISANW <keuye06380618@gmail.com>
2024-10-18 16:52:18 +0900

    feat(CalculatorService): 계산기 서비스 클래스 기본 구조 구현
    - 문자열을 구분자로 분리하는 paresInput() 메서드의 기본 틀 생성
    - 문자열 배열을 숫자로 변환하는 convertToNumbers() 메서드의 기본 틀 생성
    - 숫자 리스트의 합계를 계산하는 calculateSum() 메서드의 기본 틀 생성

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,6 +1,34 @@
 package calculator.service;
 
+import java.util.List;
+
 public class CalculatorService {
 
-  
+    /**
+     * 입력된 문자열이 기본 구분자([,:])를 사용하는지 또는 커스텀 구분자를 사용하는지 확인한 후, 해당 구분자를 기준으로 문자열을 분리하여 배열로 반환합니다.
+     *
+     * @param line 사용자가 입력한 문자열
+     * @return 분리된 숫자 리스트
+     */
+    public List<Integer> paresInput(String line) {
+        return List.of();
+    }
+
+    /**
+     * 파라미터로 전달된 문자열 배열에서 각 요소를 숫자로 변환할 수 있다면, values 리스트에 Int 타입으로 변환한 후 추가합니다. 만약 숫자로 변환할 수 없는 값이 있을 경우,
+     * NumberFormatException이 발생하며, 이를 IllegalArgumentException으로 변환해 예외를 던집니다.
+     *
+     * @param parts 구분자를 통해 분리된 문자열 배열(숫자)
+     * @return 숫자 리스트
+     */
+    private List<Integer> convertToNumbers(String[] parts) {
+        return List.of();
+    }
+
+    /**
+     * 숫자 리스트의 합계를 반환
+     */
+    public int calculateSum(List<Integer> values) {
+        return 0;
+    }
 }
