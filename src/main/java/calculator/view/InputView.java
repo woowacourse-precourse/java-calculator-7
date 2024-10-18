eae3424151437a31a37a43b3c36dc608a034ab30
jher235 <tim668666@gmail.com>
2024-10-18 22:47:27 +0900

    feat: 시작 메세지 출력 메서드 작성

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import static calculator.constant.Constant.START_MESSAGE;
+
+public class InputView {
+    public void showStartMessage(){
+        System.out.println(START_MESSAGE);
+    }
+}
