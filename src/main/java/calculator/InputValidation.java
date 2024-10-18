71489c4b1c9dd76b96e0455f66399045dcf1e8a9
minw0095 <kmu6898@naver.com>
2024-10-18 18:21:26 +0900

    refactor: 커스텀 구분자에 대한 리팩토링

--- src/main/java/calculator/InputValidation.java
+++ src/main/java/calculator/InputValidation.java
@@ -16,12 +16,12 @@ public class InputValidation {
 
         String separator;
 
-        if (!userNumbers.contains("/")) {
+        if (!userNumbers.startsWith("//")) {
             separator = "[" + normalSeparator + "]";
             numbers = userNumbers.split(separator);
         }
 
-        if (userNumbers.contains("/")) {
+        if (!userNumbers.startsWith("//")) {
             if ((userNumbers.substring(0, 2) + userNumbers.substring(3, 5)).equals("//\\n")) {
                 separator = "[" + normalSeparator + userNumbers.charAt(2) + "]";
                 numbers = userNumbers.substring(5).split(separator);
