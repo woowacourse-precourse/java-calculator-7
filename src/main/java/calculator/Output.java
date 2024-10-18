5efe9f2359a8db533dc75d7c345d47171218d733
GO-TE <alsrudxo9810@naver.com>
2024-10-19 00:32:11 +0900

    feat(Output): 결과값 출력 기능 구현

--- src/main/java/calculator/Output.java
+++ src/main/java/calculator/Output.java
@@ -4,4 +4,8 @@ public class Output {
     public void printStart() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
+
+    public void printResult(long result) {
+        System.out.println("결과 : " + result);
+    }
 }
