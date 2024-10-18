84ecbe845b37cf78b85a0d9779a88ccff546cdce
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-10-18 23:35:44 +0900

    feat(InputHandler): 요구사항 이외 잘못된 입력에 대해 IllegalArgumentException 발생하도록 구현

--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -10,4 +10,17 @@ public class InputHandler { //TODO: 사용자의 입력이 올바른지 판단
     public static boolean isNullOrEmpty(String expression) {
         return expression == null || expression.isEmpty();
     }
+
+    public static boolean isValidInput(String[] input) {
+        for (String s : input) {
+            try {
+                if (Integer.parseInt(s) < 0) {
+                    throw new IllegalArgumentException();
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return true;
+    }
 }
