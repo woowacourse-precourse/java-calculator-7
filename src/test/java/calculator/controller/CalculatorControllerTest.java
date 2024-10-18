8d91194af0ab1e570a1ac9bb119569d469b6bbf1
jimmy0524 <10jmin04@naver.com>
2024-10-19 02:21:47 +0900

    test: 쉼표,콜론 구분자 테스트 작성 (red)

--- src/test/java/calculator/controller/CalculatorControllerTest.java
+++ src/test/java/calculator/controller/CalculatorControllerTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CalculatorControllerTest  {
-
     @Test
     void 빈_문자열_입력_테스트() {
-        //Given: 빈 문자열 입력
+        //given: 빈 문자열 입력
         String input = "";
         CalculatorController controller = new CalculatorController();
 
@@ -21,7 +20,7 @@ public class CalculatorControllerTest  {
 
     @Test
     void 숫자_입력_테스트() {
-        //Given: 숫자 3 입력
+        //given: 숫자 3 입력
         String input = "3";
         CalculatorController controller = new CalculatorController();
 
@@ -32,4 +31,16 @@ public class CalculatorControllerTest  {
         assertThat(result).isEqualTo(3);
     }
 
+    @Test
+    void 쉼표_콜론_구분자_테스트() {
+        //given: "3;3,2"
+        String input = "3;3,2";
+        CalculatorController controller = new CalculatorController();
+
+        //when: 입력 처리
+        int result = controller.input(input);
+
+        //then: 결과는 8
+        assertThat(result).isEqualTo(8);
+    }
 }
