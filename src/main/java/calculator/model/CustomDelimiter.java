0e357f9dd961ca87a8ce4a2775249649c2c1e9f7
TalkingPotato90 <star901210@hanmail.net>
2024-10-19 01:54:17 +0900

    feat: 커스텀 구분자에 제한 문자를 사용하면 예외 발생 기능 및 테스트 추가

--- src/main/java/calculator/model/CustomDelimiter.java
+++ src/main/java/calculator/model/CustomDelimiter.java
@@ -1,5 +1,10 @@
 package calculator.model;
 
+import calculator.util.ErrorMessage;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CustomDelimiter extends Delimiter {
     public CustomDelimiter(String delimiter) {
         super(delimiter);
@@ -7,8 +12,19 @@ public class CustomDelimiter extends Delimiter {
 
     @Override
     public void validate() {
+        Matcher inValidDelimiter = createMatcher("[0-9\\-\\\\/]");
+
         if (delimiter.length() > 3) {
             throw new IllegalArgumentException("[ERROR][R0001] 커스텀 구분자는 최대 3개까지 등록 가능합니다.");
         }
+
+        if (inValidDelimiter.find()) {
+            throw new IllegalArgumentException(ErrorMessage.CUSTOM_DELIMITER_FORMAT.getError());
+        }
+    }
+
+    private Matcher createMatcher(String regex) {
+        Pattern customDelimiterPattern = Pattern.compile(regex);
+        return customDelimiterPattern.matcher(delimiter);
     }
 }
--- src/test/java/calculator/model/CustomDelimiterTest.java
+++ src/test/java/calculator/model/CustomDelimiterTest.java
@@ -1,7 +1,10 @@
 package calculator.model;
 
+import calculator.util.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -18,9 +21,26 @@ class CustomDelimiterTest {
     }
 
     @Test
-    @DisplayName("커스텀 구분자가 3개를 이하이면 예외가 발생하지 않는다.")
+    @DisplayName("커스텀 구분자가 3개 이하이면 예외가 발생하지 않는다.")
     void isCustomDelimiterCountBelowLimit() {
         assertThatCode(() -> new CustomDelimiter(";!?").validate())
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-","/","\\","0","9"})
+    @DisplayName("커스텀 구분자에 -,/,\\나 숫자를 입력하면 예외가 발생한다.")
+    void isCustomDelimiterFormatInvalid(String delimiter) {
+        assertThatThrownBy(() -> new CustomDelimiter(delimiter).validate())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.CUSTOM_DELIMITER_FORMAT.getError());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"@","!","a","가","+","=","^"})
+    @DisplayName("커스텀 구분자에 -,/,\\나 숫자가 없으면 예외가 발생하지 않는다.")
+    void isCustomDelimiterFormatValid(String delimiter) {
+        assertThatCode(() -> new CustomDelimiter(delimiter).validate())
                 .doesNotThrowAnyException();
     }
 
