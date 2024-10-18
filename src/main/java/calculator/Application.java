7d501b8d7885a6e6c57fb55a7b429b4fc2599d39
rlaehd62 <rlaehd62@gmail.com>
2024-10-19 00:37:12 +0900

    feat(Application): add StringCalculator in main method.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
-public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+import calculator.processor.Calculator;
+import calculator.processor.StringCalculator;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Optional;
+
+public class Application
+{
+    public static void main(String[] args)
+    {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        System.out.println(input);
+        Calculator cal = new StringCalculator(input);
+        Console.close();
+
+        Optional<Integer> result = cal.calculate();
+        result.orElseThrow(IllegalArgumentException::new);
+        System.out.println("결과 : " + result.get());
     }
 }
