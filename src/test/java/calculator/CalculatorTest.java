bec3513228fe6cbb835bfecf2f567d085abf7350
Su-daa <subin.kk019@gmail.com>
2024-10-18 15:21:07 +0900

    Test: 여러 개의 양수 문자열을 파싱한 경우 그 합계를 반환하는 테스트 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -30,4 +30,12 @@ class CalculatorTest {
         int result = calculator.calculate(input);
         assertThat(result).isEqualTo(1);
     }
+
+    @Test
+    @DisplayName("여러 개의 양수 문자열을 파싱한 경우 그 합계를 반환한다.")
+    void calculateWithMultipleValidNumbers() {
+        String[] input = {"1", "2", "3"};
+        int result = calculator.calculate(input);
+        assertThat(result).isEqualTo(6);
+    }
 }
