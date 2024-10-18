1b6b9c0e66b0b3b2bcf6238605410a1fe2dd9a9c
Nahye Choi <skgp0628@naver.com>
2024-10-18 16:54:51 +0900

    feat: 기본 구분자로 문자열 분리 기능 수정 및 숫자 더하기 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,14 +3,19 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
-    public static void gitmain(String[] args) {
+    public static void main(String[] args) {
         // TODO: 프로그램 구현
-        final String DELIMITER_1 = ",";
-        final String DELIMITER_2 = ":";
+        final String DELIMITER = ",|:";
+        String numRegEx = "[0-9]+"; //^[0-9]*$
 
-        int[] splitNumbers = new int[]{};
+
+        String[] splitNumbers = new String[]{};
+        int[] num = new int[]{};
+        int sum = 0;
 
         // 문자열 입력
         // - 안내문 출력
@@ -18,12 +23,27 @@ public class Application {
         // - 입력 받기
         String numbers = Console.readLine();
         // - 예외처리
+        // -- 숫자와 구분자만 포함하는지 판단
 
         // 숫자 추출
-        // - 구분자
-        if (numbers.contains(DELIMITER_1) || numbers.contains(DELIMITER_2)) {
-            System.out.println(Arrays.toString(numbers.split("DELIMITER_1|DELIMITER_2")));
+        // 정규 표현식 패턴 컴파일
+        Pattern pattern = Pattern.compile(DELIMITER);
+        Matcher matcher = pattern.matcher(numbers);
+
+        // - 지정 구분자로 분리
+        if (matcher.find()) {
+            Pattern patternNum = Pattern.compile(numRegEx);
+            Matcher matcherNum = patternNum.matcher(numbers);
+            if (matcherNum.find()) { // 문자 형태의 숫자를 검토해야하는 거였음
+                splitNumbers = numbers.split(DELIMITER);
+                num = Arrays.stream(splitNumbers).mapToInt(Integer::parseInt).toArray();
+                for (int i : num) {
+                    sum += i;
+                }
+            }
         }
+        System.out.println("결과 : " + sum);
+
         // - 커스텀 구분자
 
         // 문자 -> 숫자 형변환
