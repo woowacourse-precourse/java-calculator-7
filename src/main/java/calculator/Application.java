fdfea2b1d262f6d84975324b91c154272c79e453
jaehyeon <fanta4715@naver.com>
2024-10-19 00:42:41 +0900

    feat: Calculator와 Parser 생성 후 실행 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,9 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Parser parser = new Parser();
+        Calculator calculator = new Calculator(parser);
+
+        calculator.run();
     }
 }
