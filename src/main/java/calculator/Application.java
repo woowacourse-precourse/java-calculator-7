d4414f889ccd0a965d618984528fcbba7cf7db32
EHCTH <lees90896@gmail.com>
2024-10-18 20:33:19 +0900

    style(Application): 필요없는 문구 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,7 +6,6 @@ import calculator.controller.service.CalculatorControllerFactory;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        //TODO 예외 사항 테스트 돌려보고, 리팩토링해보자 그리고 해당 메서드가 해당 클래스에 적합한지 고민해보자
         CalculatorController calculatorController = CalculatorControllerFactory.create();
         calculatorController.run();
     }
