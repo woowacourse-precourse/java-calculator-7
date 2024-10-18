1d90233d97fb0afa4f769ac90673afa531d1378e
JiEung2 <181757@jnu.ac.kr>
2024-10-19 00:49:42 +0900

    feat(delimiterParser): 입력된 값을 구분자에 맞게 나누는 DelimiterParser 추가
    - 등록된 구분자에 맞게 나누기 위해 DelimiterManager 의존성 주입

---
+++ src/test/java/calculator/DelimiterParser.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class DelimiterParser {
+    private final DelimiterManager delimiterManager;
+
+    public DelimiterParser(DelimiterManager delimiterManager) {
+        this.delimiterManager = delimiterManager;
+    }
+}
