6e9ae551a67fe0e13eea1ade2049f71012719bfd
byunghyunkim0 <bb05qudgus@gamil.com>
2024-10-18 19:02:55 +0900

    feat: 입력 구분 기능 구현 - 유효성 검증   - 입력값이 null인 경우 예외 처리   - 입력값이 공백으로 시작하거나 끝나는 경우 예외 처리

--- src/main/java/calculator/util/InputUtils.java
+++ src/main/java/calculator/util/InputUtils.java
@@ -5,12 +5,26 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputUtils {
     private static String readInput() {
         String input = Console.readLine();
+        validateNullInput(input);
+        validateWhitespaceInput(input);
         if (input.startsWith("//")) {
             validateCustomDelimiterInput(input);
         } else {
             validateDefaultDelimiterInput(input);
         }
         return input;
+    }
+
+    private static void validateNullInput(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateWhitespaceInput(String input) {
+        if (input.startsWith(" ") || input.endsWith(" ")) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private static void validateCustomDelimiterInput(String input) {
