44dbd6f7110130564056e9daae088fe416086799
onesay17 <onesay17@gmail.com>
2024-10-18 15:50:59 +0900

    fix: 커스텀구분자 사용시 기본구분자 포함 검증 오류 수정
    커스텀구분자를 사용하는데 구분된값에 기본구분자가 있는데도 정상 실행되어서, 검증하는 로직을 추가했습니다.
    
    Related to #3

--- src/main/java/calculator/validator/ValueValidator.java
+++ src/main/java/calculator/validator/ValueValidator.java
@@ -1,11 +1,17 @@
 package calculator.validator;
 
+import static calculator.util.Constant.DEFAULT_DELIMITERS;
+
 public class ValueValidator implements Validator {
 
     @Override
     public void validate(String value) {
         if (value.contains(" ")) {
             throw new IllegalArgumentException("잘못된 입력: 값에 공백이 포함될 수 없습니다.");
+        }
+
+        if (value.matches(".*" + DEFAULT_DELIMITERS + ".*")) {
+            throw new IllegalArgumentException("잘못된 입력 형식: 기본 구분자와 커스텀 구분자가 혼용되었습니다.");
         }
     }
 }
