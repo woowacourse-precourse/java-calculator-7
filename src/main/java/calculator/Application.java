de158174dd9a9f2b9ee248388d64344193e792c0
GunsanHaribo <hth99131@gmail.com>
2024-10-18 18:57:44 +0900

    feat(Application.java): 구분자가 없는 경우 예외처리입니다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,9 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private static String COMMA = ",";
+    private static String SEMICOLON = ";";
+
     public static void main(String[] args) {
         String input = Console.readLine();
         int result = add(input);
@@ -10,8 +13,13 @@ public class Application {
     }
 
     private static int add(String input) {
+        boolean isNotContainingDelimiter = !input.contains(COMMA) && !input.contains(SEMICOLON);
+
         if (input.isEmpty()) {
             return 0;
+        }
+        if (isNotContainingDelimiter) {
+            throw new IllegalArgumentException("구분자가 없습니다");
         }
 
         return 1;
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
@@ -22,6 +23,22 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void validateNoDelimiter() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("1234")).isInstanceOf(IllegalArgumentException.class);
+        });
+        assertSimpleTest(() -> {
+            assertThatNoException().isThrownBy(() -> runException("123;123"));
+        });
+        assertSimpleTest(() -> {
+            assertThatNoException().isThrownBy(() -> runException("123,123"));
+        });
+        assertSimpleTest(() -> {
+            assertThatNoException().isThrownBy(() -> runException("123;12,3"));
+        });
+    }
+
 
     @Test
     void 커스텀_구분자_사용() {
@@ -33,10 +50,8 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("-1,2,3"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertSimpleTest(
+                () -> assertThatThrownBy(() -> runException("-1,2,3")).isInstanceOf(IllegalArgumentException.class));
     }
 
     @Override
