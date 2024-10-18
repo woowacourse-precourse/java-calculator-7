9b3ded78b08fdc846160d11284d0dc1183aaafed
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-19 05:48:55 +0900

    feat: 커스텀 구분자로 지정되지 않은 문자 예외처리 추가

--- src/main/java/calculator/delimiter/service/NumberDelimiterService.java
+++ src/main/java/calculator/delimiter/service/NumberDelimiterService.java
@@ -1,11 +1,15 @@
 package calculator.delimiter.service;
 
+import calculator.common.exception.ExceptionFactory;
 import calculator.delimiter.domain.Delimiter;
 import calculator.delimiter.domain.Delimiters;
 import calculator.delimiter.factory.DelimiterFactory;
+import calculator.util.pattern.PatternUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static calculator.common.exception.ExceptionType.NOT_DELIMITER;
 
 public class NumberDelimiterService extends DelimiterService {
 
@@ -21,7 +25,15 @@ public class NumberDelimiterService extends DelimiterService {
         Delimiters delimiters = getDelimiters(value);
         String strippedInput = customDelimiterService.stripCustomDelimiter(value);
 
+        validateStrippedInput(strippedInput, delimiters);
+
         return new ArrayList<>(List.of(strippedInput.split(delimiters.splitRegex())));
+    }
+
+    private void validateStrippedInput(String strippedInput, Delimiters delimiters) {
+        if (!PatternUtils.matchesWithDelimitersIgnoringNumbers(strippedInput, delimiters.pattern())) {
+            throw ExceptionFactory.createException(NOT_DELIMITER);
+        }
     }
 
     private Delimiters getDelimiters(String input) {
