258f15925f9f28e8bf3bf337389c04c621939282
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2024-10-18 19:04:04 +0900

    feat(ApplicationTest.java) : Add 기본_구분자_사용 & Modify 커스텀_구분자_사용, 예외_테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -5,21 +5,67 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ApplicationTest extends NsTest {
-    @Test
-    void 커스텀_구분자_사용() {
+
+    static Stream<Arguments> 기본_구분자_사용_TestCases() {
+        return Stream.of(
+                Arguments.of("1,2,3", "결과 : 6"),
+                Arguments.of("1,2:3", "결과 : 6"),
+                Arguments.of("1:2:3", "결과 : 6")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("기본_구분자_사용_TestCases")
+    void 기본_구분자_사용(String input, String expected) {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run(input);
+            assertThat(output()).contains(expected);
         });
     }
 
-    @Test
-    void 예외_테스트() {
+    static Stream<Arguments> 커스텀_구분자_사용_TestCases() {
+        return Stream.of(
+                Arguments.of("//;\\n1", "결과 : 1"),
+                Arguments.of("//@\\n1@2@3", "결과 : 6"),
+                Arguments.of("//@\\n1@2,3", "결과 : 6"),
+                Arguments.of("//@\\n1@2:3", "결과 : 6"),
+                Arguments.of("//@\\n//@\\n1@2,3", "결과 : 6"),
+                Arguments.of("//@\\n//;\\n1;2@3", "결과 : 6"),
+                Arguments.of("//@\\n1@2@3", "결과 : 6")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("커스텀_구분자_사용_TestCases")
+    void 커스텀_구분자_사용(String input, String expected) {
+        assertSimpleTest(() -> {
+            run(input);
+            assertThat(output()).contains(expected);
+        });
+    }
+
+    static Stream<Arguments> 예외_테스트_TestCases() {
+        return Stream.of(
+                Arguments.of("-1,2,3"),
+                Arguments.of("1,a,3"),
+                Arguments.of("asdf"),
+                Arguments.of("1@2@3"),
+                Arguments.of("//@1@2@3"),
+                Arguments.of("//@\\n//1,2,3")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("예외_테스트_TestCases")
+    void 예외_테스트(String input) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("-1,2,3"))
+                assertThatThrownBy(() -> runException(input))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
