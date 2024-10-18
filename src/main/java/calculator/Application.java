ed00b23d34d14dcc73a5b6b3de093e71bea357cd
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2024-10-18 22:45:36 +0900

    feat : Application

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputManager input = new InputManager();
+        NumberExtractor number = new NumberExtractor();
+        Calculator calculator = new Calculator();
+
+        String str = input.getString();
+
+        if(str.equals("Empty")){
+            return;
+        }
+
+        ArrayList<Integer> extractedNumbers = number.getArray(str);
+
+        calculator.getResult(extractedNumbers);
     }
 }
