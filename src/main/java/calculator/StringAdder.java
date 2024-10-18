4668339f307e2b358248dda668c61c25de3d4567
sejinezy <psj8880@naver.com>
2024-10-18 17:40:56 +0900

    feat(StringAdder): ConsoleIO와 InputValidator 의존성을 주입하는 생성자 추가
    StringAdder 클래스에 ConsoleIO와 InputValidator 의존성을 주입하는 생성자를 추가하여 입력과 유효성
    검사를 외부에서 의존성 주입 방식으로 처리할 수 있도록 구현

---
+++ src/main/java/calculator/StringAdder.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class StringAdder {
+    private final ConsoleIO consoleIO;
+    private final InputValidator validator;
+
+    public StringAdder(ConsoleIO consoleIO, InputValidator validator) {
+        this.consoleIO = consoleIO;
+        this.validator = validator;
+    }
+
+}
