b93df465fbd229b30aa29b0acf1b9e955620d7f0
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-10-18 22:56:07 +0900

    feat($calculator) : 구분자 제거 기능 변경
    변경 사항
    - 구분자 제거 기능 로직 변경
    - 내부 에러 처리 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -28,9 +28,45 @@ public class Calculator {
         this.sum = sum;
     }
 
-    public String addSeparator(String str) {
-        this.separator.add(str.substring(2, 3));
-        return str.substring(5);
+    public String addSeparator(String str) throws IllegalArgumentException {
+
+        while (str.length() > 0 && str.charAt(0) == '/') {
+            if (str.length() >= 5 && str.charAt(1) == '/') { // 문자 길이로 애초에 양식 안맞고 길이 안맞는거 걸러서 indexing 오류 제거
+                if (str.substring(2, 3).matches("[^a-zA-Z0-9\\\\\\\\s]")) { //특수 문자인지 확인
+                    String sep = str.substring(2, 3);
+                    str = str.substring(3);
+                    if (str.startsWith("\\n")) {
+                        str = str.substring(2);
+                        this.separator.add(sep);
+                    } else {
+                        throw new IllegalArgumentException("잘못된 입력입니다. \\n이 없습니다.");
+                    }
+                } else {
+                    throw new IllegalArgumentException("잘못된 입력입니다. 구분자가 없습니다.");
+                }
+                System.out.println(str);
+            } else {
+                throw new IllegalArgumentException("잘못된 입력입니다. // 양식 혹은 길이가 맞지 않습니다.");
+            }
+        }
+
+        System.out.println(str);
+        return str;
+    }
+
+    public void manageAdd(String input) throws IllegalArgumentException {
+        this.setInput(input);
+        String stack = new String("");
+
+        if (this.input.isEmpty()) {
+            return;
+        }
+        this.addSeparator(this.input);
+//        while (!this.input.isEmpty()) {
+//            stack = stack.concat(this.input.substring(0, 1));
+//            this.input = this.input.substring(1);
+//            if (stack)
+//        }
     }
 
 
