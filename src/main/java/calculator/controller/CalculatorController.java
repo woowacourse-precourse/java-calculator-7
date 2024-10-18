a133f8dc1f8d68032392c582fdd24419cbba761e
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-19 07:24:36 +0900

    feat: 계산기 Controller 추상 클래스로 변경
    - 문자, 숫자 등 다양한 계산을 수행하는 점 강조.
    - 확장성 증가.

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -2,41 +2,18 @@ package calculator.controller;
 
 import calculator.common.config.NumberCalculatorDependencyRegistry;
 import calculator.model.delimiter.service.DelimiterService;
-import calculator.util.integer.IntegerUtils;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
-import java.util.List;
+public abstract class CalculatorController implements Controller {
 
-public class CalculatorController {
-
-    private final DelimiterService delimiterService;
-    private final InputView inputView;
-    private final OutputView outputView;
+    protected final DelimiterService delimiterService;
+    protected final InputView inputView;
+    protected final OutputView outputView;
 
     public CalculatorController(NumberCalculatorDependencyRegistry dependencyRegistry) {
         this.delimiterService = dependencyRegistry.getDelimiterService();
         this.inputView = dependencyRegistry.getInputView();
         this.outputView = dependencyRegistry.getOutputView();
-    }
-
-    public void run() {
-        String input = inputView.readUserInput();
-        List<String> numberStrings = splitInputByDelimiters(input);
-        List<Integer> numbers = parseNumberStringsToIntegers(numberStrings);
-        int sum = calculateSum(numbers);
-        outputView.printSumResult(sum);
-    }
-
-    private List<String> splitInputByDelimiters(String input) {
-        return delimiterService.splitByDelimiters(input);
-    }
-
-    private List<Integer> parseNumberStringsToIntegers(List<String> numberStrings) {
-        return IntegerUtils.parseIntegers(numberStrings);
-    }
-
-    private int calculateSum(List<Integer> numbers) {
-        return IntegerUtils.sum(numbers);
     }
 }
