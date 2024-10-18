e499ed7d3919b75445d37c37dbc6bd008026dd89
AYEOOON <k93853471@gmail.com>
2024-10-18 19:38:51 +0900

    refactor: 메서드명 제일 앞 글자 소문자로 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,33 +7,33 @@ import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
-        String input = Input();
-        int result = Add_Process(input);
+        String input = input();
+        int result = add_Process(input);
         System.out.println("결과 : " + result);
     }
 
     // 사용자의 입력을 받는 메서드
-    private static String Input() {
+    private static String input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         return sc.nextLine().replace("\\n", "\n"); // 문자열에서 "\n"을 실제 줄바꿈 문자로 변환
     }
 
     // 입력된 문자열을 처리하는 메서드
-    private static int Add_Process(String input) {
+    private static int add_Process(String input) {
         if (input == null || input.trim().isEmpty()) {
             return 0; // 입력이 비어있거나 null이면 0을 반환
         }
-        
-        String[] nums = Refine_Input(input); // 입력을 정제하여 숫자 배열로 반환
+
+        String[] nums = refine_Input(input); // 입력을 정제하여 숫자 배열로 반환
 
         System.out.println(Arrays.toString(nums));
 
-        return Sum(nums);
+        return sum(nums);
     }
 
     // 입력 문자열에서 구문자를 처리하고 숫자 배열로 반환하는 메서드
-    private static String[] Refine_Input(String input) {
+    private static String[] refine_Input(String input) {
         // 정규표현식 패턴
         String regex = "//(.+)\\n";  // 중간에 여러 문자가 있을 수도 있으므로 (.+) 사용
 
@@ -51,7 +51,7 @@ public class Application {
     }
 
     // 숫자 배열을 합산하는 메서드
-    private static int Sum(String[] nums) {
+    private static int sum(String[] nums) {
         int result = 0;
 
         for (String word : nums) {
