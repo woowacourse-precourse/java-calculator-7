82bf63e770c5b940d9ff44ab007c97f08c1e437f
Park Yu Han <pbk2312@naver.com>
2024-10-19 00:00:00 +0900

    refactor: 메인 클래스(Application) 수정 및 객체 생성 로직 리팩토링

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,37 @@
 package calculator;
 
+import calculator.controller.StringCalculatorController;
+import calculator.model.StringCalculator;
+import calculator.model.StringParser;
+import calculator.service.CalculatorService;
+import calculator.validator.ValidatorFactory;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            StringCalculatorController calculatorController = createController();
+            calculatorController.run();  // 프로그램 실행
+        } catch (IllegalArgumentException e) {
+            // 예외 발생 시 메시지 출력 후 프로그램 종료
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
+
+    // Controller 및 필요한 객체들을 생성하는 메소드
+    private static StringCalculatorController createController() {
+        StringParser stringParser = new StringParser();
+        StringCalculator stringCalculator = new StringCalculator();
+        var inputValidator = ValidatorFactory.createInputValidator();
+
+        CalculatorService calculatorService = new CalculatorService(stringParser, stringCalculator, inputValidator);
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        return new StringCalculatorController(inputView, outputView, calculatorService);
+    }
+
 }
