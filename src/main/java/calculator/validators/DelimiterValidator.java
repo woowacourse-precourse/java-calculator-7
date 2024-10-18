9a6a8052aa9fd825aef80ead86380038ae2e78f0
wogus <ind07152@naver.com>
2024-10-18 12:52:11 +0900

    fix(DelimiterValidator): 구분자 관련 예외 버그 수정
    커스텀 구분자를 나누는 기준이 하나밖에 없을 떄도 예외가 발생해야한다.

--- src/main/java/calculator/validators/DelimiterValidator.java
+++ src/main/java/calculator/validators/DelimiterValidator.java
@@ -1,13 +1,20 @@
 package calculator.validators;
 
 import static calculator.constant.StandardIndex.FIRST_STANDARD;
+import static calculator.constant.StandardIndex.SECOND_STANDARD;
 
 public final class DelimiterValidator {
 
-    // "//"의 인덱스 위치는 커스텀 구분자가 있으면 0, 아니면 -1 이여야한다.
     public static void validateDelimiterFormat(String input) {
         int firstIndex = input.indexOf(FIRST_STANDARD.getStandard());
+        int secondIndex = input.indexOf(SECOND_STANDARD.getStandard());
         if (firstIndex > 0) {
+            throw new IllegalArgumentException();
+        }
+        if (firstIndex < 0 && secondIndex >= 0) {
+            throw new IllegalArgumentException();
+        }
+        if (firstIndex == 0 && secondIndex < 0) {
             throw new IllegalArgumentException();
         }
     }
