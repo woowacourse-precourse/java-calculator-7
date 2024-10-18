23d96236fc12a650503e850949df30c3f0cd5a47
hmlssghm <kwonse326@gmail.com>
2024-10-18 15:44:03 +0900

    fix: 출력 첫줄 하고 한 줄 다음으로 입력 받게 수정

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,7 +2,7 @@ package calculator.view;
 
 public class OutputView {
     public void askInput(){
-        System.out.print("덧셈할 문자열을 입력해 주세요.");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
     public void printAnswer(int answer){
