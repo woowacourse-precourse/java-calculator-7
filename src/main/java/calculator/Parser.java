ccd83cdffad644c29876f1edc23fc09901254cef
SeriousBug98 <martin1156@konkuk.ac.kr>
2024-10-18 22:06:54 +0900

    refactor: 지역변수를 의도한 뜻에 맞춰서 필드명 변경

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -5,7 +5,7 @@ public class Parser {
     private static final String DEFAULT_DELIMITERS = ",|:";
     private static final String CUSTOM_DELIMITER_START = "//";
     private static final String CUSTOM_DELIMITER_END = "\\n";
-    private static final int CUSTOM_STRING_LENGTH= 2;
+    private static final int CUSTOM_DELIMITER_LENGTH= 2;
 
     // 구분자를 기준으로 문자열 파싱하기
     public String[] parse(String input) {
@@ -25,8 +25,8 @@ public class Parser {
         String delimiter;
         if (isCustomDelimiter(input)) {
             int delimiterIndex = input.indexOf(CUSTOM_DELIMITER_END);
-            delimiter = input.substring(CUSTOM_STRING_LENGTH, delimiterIndex);
-            input = input.substring(delimiterIndex + CUSTOM_STRING_LENGTH);
+            delimiter = input.substring(CUSTOM_DELIMITER_LENGTH, delimiterIndex);
+            input = input.substring(delimiterIndex + CUSTOM_DELIMITER_LENGTH);
         } else {
             delimiter = DEFAULT_DELIMITERS;
         }
