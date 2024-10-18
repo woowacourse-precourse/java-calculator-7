641cec4e51fc24ddfa20ac6e7d7e9d0c7f2e755a
chahyunsoo <chahs1012@naver.com>
2024-10-18 20:07:48 +0900

    feat(ErrorMessage) : ErrorMessage 상수 클래스 구현
    * 에러 메시지를 상수로 관리하기 위해 ErrorMessage 클래스를 생성했습니다.
    
    * 잘못된 입력, 음수 입력, 잘못된 숫자 형식 관련 에러 메시지를 상수로 정의했습니다.

---
+++ src/main/java/calculator/constant/ErrorMessage.java
@@ -0,0 +1,7 @@
+package calculator.constant;
+
+public class ErrorMessage {
+    public static final String INVALID_INPUT = "[ERROR] 잘못된 입력입니다.";
+    public static final String NEGATIVE_NUMBER = "[ERROR] 음수는 입력할 수 없습니다.";
+    public static final String WRONG_FORMAT = "[ERROR] 숫자 형식이 아닌 입력값이 포함되어 있습니다.";
+}
