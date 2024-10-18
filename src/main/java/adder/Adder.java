f4d70cb59e06fc175189bdb9768cafc6ef9effb8
parkseongyeol <ert2154@naver.com>
2024-10-18 19:48:03 +0900

    feat($Adder): 구분자 문자열 판별 기능 제작

--- src/main/java/adder/Adder.java
+++ src/main/java/adder/Adder.java
@@ -1,17 +1,32 @@
 package adder;
 
+import separator.Separator;
+
 import java.util.ArrayList;
 
 public class Adder {
 
     public Integer addNumbers(ArrayList<Integer> numbers) {
+
         int result = 0;
 
-        for(int number: numbers){
+        for (int number : numbers) {
             result += number;
         }
 
         return result;
     }
+
+    private boolean isSeparator(String e) {
+
+        for (char separator : Separator.separators) {
+            if (e.equals(String.valueOf(separator))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 
 }
