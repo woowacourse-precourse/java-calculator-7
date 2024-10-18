59813ec37664e2c5ff5c16ff6646239a06f3e6c9
dongwoo <woomacho@gmail.com>
2024-10-18 20:07:11 +0900

    feat: 계산기 기능구현 완료 - System.out 사용

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import calculator.input_processor.OperandConverter;
+import calculator.input_processor.SimpleCalculator;
+import calculator.input_processor.OperandExtractor;
+import calculator.input_processor.OperandProcessor;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OperandExtractor operandExtractor = new OperandExtractor();
+        OperandConverter operandConverter = new OperandConverter();
+        OperandProcessor operandProcessor = new OperandProcessor();
+        SimpleCalculator simpleCalculator = new SimpleCalculator(operandExtractor, operandConverter, operandProcessor);
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        int result = simpleCalculator.run(Console.readLine());
+        System.out.printf("결과 : %d:", result);
     }
 }
