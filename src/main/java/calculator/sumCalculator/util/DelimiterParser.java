9f72fbd3cd4a6500289af8515a39b16d553c836f
daeuun <leeda51925@gmail.com>
2024-10-19 00:51:25 +0900

    feat: 기본 구분자 유효성 검사
    연산식에 기본 구분자의 유효성을 검사하는 기능을 추가한다.
    콤마, 세미콜론을 제외하고 사용할 수 없다.

--- src/main/java/calculator/sumCalculator/util/DelimiterParser.java
+++ src/main/java/calculator/sumCalculator/util/DelimiterParser.java
@@ -7,17 +7,29 @@ import java.util.regex.Pattern;
 
 public class DelimiterParser {
 
-    private static final String REGEXP_CUSTOM_DELIMITER_PATTERN = "//([^\s]+)\\n";
-    private static final String INVALID_REGEXP_CUSTOM_DELIMITER_PATTERN = "//.*[\\s]+.*\\n";
+    private static final String REGEX_CUSTOM_DELIMITER_PATTERN = "//([^\s]+)\\n";
+    private static final String INVALID_REGEX_CUSTOM_DELIMITER_PATTERN = "//.*[\\s]+.*\\n";
+    private static final String REGEX_DEFAULT_DELIMITER_PATTERN = "^[0-9,;]+$";
 
+    /**
+     * 기본 구분자 유효성 검사한다.
+     * 콤마, 세미콜론 제외한 문자 사용시 예외 처리한다.
+     * @param input 사용자 입력 문자열
+     */
+    public void validateDefaultDelimiter(String input) {
+        boolean isValid = input.matches(REGEX_DEFAULT_DELIMITER_PATTERN);
+        if (!isValid) {
+            throw new IllegalArgumentException(ExceptionCode.INVALID_DEFAULT_DELIMITER_USED.getMessage());
+        }
+    }
     /**
      * 커스텀 구분자를 추출한다.
      * @param input 사용자 입력 문자열
      * @return 추출된 커스텀 구분자
      */
     public String extractCustomDelimiter(String input) {
-        validate(input);
-        Pattern pattern = Pattern.compile(REGEXP_CUSTOM_DELIMITER_PATTERN);
+        validateCustomDelimiter(input);
+        Pattern pattern = Pattern.compile(REGEX_CUSTOM_DELIMITER_PATTERN);
         Matcher matcher = pattern.matcher(input);
         if (matcher.find()) {
             return matcher.group(1);
@@ -31,8 +43,8 @@ public class DelimiterParser {
      * 예외 패턴(공백) 사용 시 예외 처리한다.
      * @param input 사용자 입력 문자열
      */
-    private void validate(String input) {
-        Pattern invalidPattern = Pattern.compile(INVALID_REGEXP_CUSTOM_DELIMITER_PATTERN);
+    private void validateCustomDelimiter(String input) {
+        Pattern invalidPattern = Pattern.compile(INVALID_REGEX_CUSTOM_DELIMITER_PATTERN);
         Matcher invalidMatcher = invalidPattern.matcher(input);
         if (invalidMatcher.find()) {
             throw new IllegalArgumentException(ExceptionCode.INVALID_CUSTOM_DELIMITER_PATTERN.getMessage());
