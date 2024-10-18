af9595f69ac4a8229ae2fcadf7a1a33cdab66da5
yybmion <yyb400@naver.com>
2024-10-18 18:57:57 +0900

    [feat]: 커스텀 구분자를 기존 구분자에 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,18 +2,23 @@ package calculator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.regex.Pattern;
+
 public class Application {
+    static String DELIMITER = ",|:";
+
     public static void main(String[] args) {
         String inputString = readLine();
 
-        splitString(inputString);
+        String newInputString = splitCustomDelimiter(inputString);
+        splitDelimiter(newInputString, DELIMITER);
     }
 
     /**
      * @param inputString: 입력값
-     * @return 계산 결과 만약 입력값이 "//"로 시작되고 그 뒤에 "\n"가 존재한다면 커스텀 구분자가 존재한다 (커스텀 구분자 내 공백 제외).
+     * @return 커스텀 구분자 분리 이후 새로운 inputString 값 만약 입력값이 "//"로 시작되고 그 뒤에 "\n"가 존재한다면 커스텀 구분자가 존재한다 (커스텀 구분자 내 공백 제외).
      */
-    static public int splitString(String inputString) {
+    static public String splitCustomDelimiter(String inputString) {
         if (inputString.startsWith("//")) {
             int newLineIndex = inputString.indexOf("\n");
             if (newLineIndex == -1) {
@@ -25,6 +30,15 @@ public class Application {
             if (customDelimiter.isEmpty()) {
                 throw new IllegalArgumentException("커스텀 구분자가 비어있습니다.")
             }
+
+            DELIMITER += "|" + Pattern.quote(customDelimiter);
+            inputString = inputString.substring(newLineIndex + 1);
         }
+
+        return inputString;
+    }
+
+    static private void splitDelimiter(String newInputString, String delimiter) {
+
     }
 }
