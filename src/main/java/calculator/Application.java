2eb5b6f81e897a6f6e9254dadeaea98f79aa4da3
donggundg <rlaehdrjs98@g.skku.edu>
2024-10-18 16:16:18 +0900

    feat(Application) : 구분자 추가하기
    기존 ,와 :를 포함한 op string을 만듦.문자열 앞에 //와\n 사이에 커스텀 구분자가 존재하면 op에 커스텀 구분자를 새로 등록. 이후 앞 5자리는 input string에서 제외시킴.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,11 +2,31 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         //입력 받는 객체 scanner 생성
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
+
+        //구분자 추가하기
+        ArrayList<String> seperatorList = new ArrayList<String>();
+        seperatorList.add(",");
+        seperatorList.add(":");
+        String op = ",|:";
+        //새로 등록할 구분자 있나 확인
+        if (input.length() >= 5) {
+            String front = input.substring(0, 2);
+            String back = input.substring(3, 5);
+            //제대로된 형식 사이에 구분자가 끼어있는지 verify
+            if (front.equals("//") && back.equals("\\n")) {
+                seperatorList.add(Character.toString(input.charAt(2)));
+                op += "|";
+                op += Character.toString(input.charAt(2));
+                input = input.substring(5);
+            }
+        }
 
         //출력 로직
         String sum = "0";
