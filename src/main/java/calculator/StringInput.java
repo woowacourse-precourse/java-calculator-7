f5c96d0b87dc7877a103b1c9b69d7ae80ca6c420
sinyebin <dpqls0625@naver.com>
2024-10-18 22:09:20 +0900

    feat:  문자가 null 또는 빈문자일 경우 에러처리

---
+++ src/main/java/calculator/StringInput.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class StringInput {
+    private static final String BLINK_INPUT_ERROR = "입력된 값이 없습니다.";
+    private final String input;
+
+    public StringInput(String input) {
+        validateString(input);
+        this.input = input;
+    }
+
+    private static void validateString(String input) {
+        if (isBlank(input)) {
+            throw new IllegalArgumentException(BLINK_INPUT_ERROR);
+        }
+    }
+
+    private static boolean isBlank(String input) {
+        return (input == null || input.trim().isEmpty());
+    }
+}
