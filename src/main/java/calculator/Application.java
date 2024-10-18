236c633121d68cd8a2c718e6d46ea346359c0aa5
chul5 <och7164@gmail.com>
2024-10-19 02:58:23 +0900

    style: 코드 정렬

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,9 +3,9 @@ package calculator;
 import calculator.controller.CalculatorController;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        CalculatorController calculatorController = new CalculatorController();
-        calculatorController.run();
-    }
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+		CalculatorController calculatorController = new CalculatorController();
+		calculatorController.run();
+	}
 }
