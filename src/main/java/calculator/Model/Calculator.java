b8f8e479b2516e83ef8292bf8816e34a726389dc
kimdev <kim__dev@hufs.ac.kr>
2024-10-18 22:54:23 +0900

    feat(Calculator): 합계 시 오버플로우 문제 처리

--- src/main/java/calculator/Model/Calculator.java
+++ src/main/java/calculator/Model/Calculator.java
@@ -3,10 +3,21 @@ package calculator.Model;
 public class Calculator {
     private int sum = 0;
 
+    public void checkOverflow(int nowNumber) {
+        if (nowNumber > 0 && sum > Integer.MAX_VALUE-nowNumber) {
+            throw new IllegalArgumentException("표현할 수 있는 최대치를 초과하였습니다.");
+        }
+        if (nowNumber < 0 && sum < Integer.MIN_VALUE - nowNumber) {
+            throw new IllegalArgumentException("표현할 수 있는 최소치를 초과하였습니다.");
+        }
+    }
+
     public int calcSum(Input input) {
         int numberSize = input.size();
         for (int i=0; i<numberSize; i++) {
-            sum += input.get(i);
+            int nowNumber = input.get(i);
+            checkOverflow(nowNumber);
+            sum += nowNumber;
         }
 
         return sum;
