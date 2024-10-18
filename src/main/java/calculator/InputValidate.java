029f7e76f900853710f404327bb049d186db6ea6
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-18 22:29:18 +0900

    refactor: 커스텀 구분자 확인 로직 분리

--- src/main/java/calculator/InputValidate.java
+++ src/main/java/calculator/InputValidate.java
@@ -3,13 +3,15 @@ package calculator;
 public class InputValidate {
     private static String delimiter = ",|:";
     private static final String customStart = "//";
-    private static String[] UserInputArr;
 
-    public static void CheckCustom(String UserInput) {
-        if (!(UserInput.startsWith(customStart) && UserInput.indexOf("\\n", 2) == 3)) {
-            throw new IllegalArgumentException("입력 값이 올바르지 않습니다.");
+    public static String checkCustom(String UserInput) {
+        if (UserInput.startsWith(customStart)) {
+            if (UserInput.indexOf("\\n", 2) != 3) {
+                throw new IllegalArgumentException("입력 값이 올바르지 않습니다.");
+            }
+            CustomValidateCheck(UserInput.charAt(2));
         }
-        CustomValidateCheck(UserInput.charAt(2));
+        return delimiter;
     }
 
     public static void CustomValidateCheck(char CustomVal) {
@@ -19,38 +21,14 @@ public class InputValidate {
         addDelimiter(CustomVal);
     }
 
-    public static void addDelimiter(char addDelimiter) {
-        delimiter += "|" + addDelimiter;
-    }
+    public static void addDelimiter(char addOneDelimiter) {
+        //delimiter += "|" + addOneDelimiter;
+        String specialCharacters = ".^$|()[]{}*+?\\";
 
-    public static void StringValidateCheck(String UserInput, String delimiter) {
-        UserInputArr = UserInput.split(delimiter);
-        //System.out.println("배열 안에 값들" + Arrays.toString(UserInputArr));//디버깅코드
-    }
-
-    public static int calculateSum(String UserInput) {
-        if (UserInput.startsWith(customStart)) {
-            CheckCustom(UserInput);
-            UserInput = UserInput.substring(UserInput.indexOf("\\n") + 2);
+        if (specialCharacters.indexOf(addOneDelimiter) != -1) {
+            delimiter += "|\\" + addOneDelimiter;
+        } else {
+            delimiter += "|" + addOneDelimiter;
         }
-        StringValidateCheck(UserInput, delimiter);
-        return UserInputArrIsNum(UserInputArr);
-    }
-
-    public static int UserInputArrIsNum(String[] UserInputArr) {
-        int sum = 0;
-
-        for (String s : UserInputArr) {
-            if (s.isEmpty()) {
-                continue;
-            }
-            try {
-                int num = Integer.parseInt(s);
-                sum += num;
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다");
-            }
-        }
-        return sum;
     }
 }
