be047bb002233effe46e8e5405f97dc540fdc8c8
sinminseok12 <sinminseok12@naver.com>
2024-10-19 03:25:58 +0900

    feat : 커스텀 구분자 타입 예외처리 구현

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -1,8 +1,12 @@
 package calculator;
 
+import static calculator.Extractor.extractDelimiter;
+import static calculator.Extractor.extractNumberPart;
+
 public class Validator {
     private static final String ERROR_INVALID_DELIMITER = "커스텀 구분자로 숫자 또는 공백을 사용할 수 없습니다.";
     private static final String ERROR_INVALID_FORMAT = "커스텀 구분자 형식이 올바르지 않습니다.";
+    private static final String ERROR_DELIMITER_TYPE = "커스텀 구분자는 영어,숫자,공백을 허용하지 않습니다.: ";
     private static final String INVALID_CUSTOM_DELIMITER_PATTERN = "//[\\d\\s]*((\\\\n)|(\\n)).*";
 
     private final String defaultDelimiter;
@@ -17,6 +21,9 @@ public class Validator {
 
     public void validateDelimiter(String value){
         validateCustomDelimiterFormat(value);
+        String delimiter = extractDelimiter(value, defaultDelimiter, prefix, suffix);
+        String numberPart = extractNumberPart(value, prefix, suffix);
+        validateDelimiterType(numberPart, delimiter);
     }
 
     private void validateCustomDelimiterFormat(String value){
@@ -28,6 +35,12 @@ public class Validator {
         }
         if (value.matches(INVALID_CUSTOM_DELIMITER_PATTERN)) {
             throw new IllegalArgumentException(ERROR_INVALID_DELIMITER);
+        }
+    }
+
+    private void validateDelimiterType(String value, String delimiter){
+        if (!value.trim().isEmpty() && delimiter.matches("[a-zA-Z0-9]+")) {
+            throw new IllegalArgumentException(ERROR_DELIMITER_TYPE + value);
         }
     }
 }
