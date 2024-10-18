533ae047a9219b50e3acf4e64ab97fdf44bcd29f
hwangdaesun <hds650588@gmail.com>
2024-10-18 10:13:42 +0900

    test(InputParser.class) : 구분자를 기준으로 문자열을 구분하는 기능 테스트

--- src/test/java/calculator/model/InputParserTest.java
+++ src/test/java/calculator/model/InputParserTest.java
@@ -1,17 +1,29 @@
 package calculator.model;
 
+import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class InputParserTest {
-    private InputParser inputParser = InputParser.getInstance();
+    
+    @Test
+    void 구분자를_기준으로_문자열_구분_1() {
+        List<String> delimiters = List.of(",", ":");
+        InputParser inputParser = InputParser.getInstance();
+        String[] values = inputParser.parseByDelimiter("1,2:3", delimiters);
+        Assertions.assertTrue(Arrays.stream(values).toList().contains("1"));
+        Assertions.assertTrue(Arrays.stream(values).toList().contains("2"));
+        Assertions.assertTrue(Arrays.stream(values).toList().contains("3"));
+    }
 
     @Test
-    void parseByDelimiter() {
+    void 구분자를_기준으로_문자열_구분_2() {
         List<String> delimiters = List.of(",", ":");
-        String[] values = inputParser.parseByDelimiter("1,2:3", delimiters);
-        for (String value : values) {
-            System.out.println(value);
-        }
+        InputParser inputParser = InputParser.getInstance();
+        String[] values = inputParser.parseByDelimiter("1,2:3;4", delimiters);
+        Assertions.assertTrue(Arrays.stream(values).toList().contains("1"));
+        Assertions.assertTrue(Arrays.stream(values).toList().contains("2"));
+        Assertions.assertTrue(Arrays.stream(values).toList().contains("3;4"));
     }
 }
