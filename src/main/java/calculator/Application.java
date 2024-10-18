32a08c93975b4adf992fd740702d734b31fd86a2
Jin409 <sualng123@gmail.com>
2024-10-19 00:55:15 +0900

    feat(Processor, Application): 구분자를 기준으로 문자열을 자르는 기능 구현
    processor 에 구분자를 기준으로 문자열을 자르는 기능 구현:
    - 구분자들을 List 형태로 받아 저장
    - 정규식을 이용해 문자열을 자르도록 함
    - Processor 의 execute 메서드만 public 으로 선언해 진행 과정을 캡슐화

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,6 +8,7 @@ public class Application {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        processor.extractCustomDelimiter(input);
+
+        processor.execute(input);
     }
 }
--- src/main/java/calculator/Processor.java
+++ src/main/java/calculator/Processor.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Processor {
 
     public Processor() {
@@ -7,13 +10,28 @@ public class Processor {
 
     private static final String DELIMITER_DECLARATION_SECTION = "\\";
     private static final String DELIMITER_AND_NUMBERS_SEPARATOR = "\n";
+    private List<String> delimiters = new ArrayList<>(List.of(",", ":"));
+
+    public void execute(String input) {
+        if (input.startsWith(DELIMITER_DECLARATION_SECTION)) {
+            String customDelimiter = extractCustomDelimiter(input);
+            delimiters.add(customDelimiter);
+        }
+
+        String[] splittedStrings = splitByDelimiters(input);
+    }
 
     public String extractCustomDelimiter(String input) {
-        if (input.startsWith(DELIMITER_DECLARATION_SECTION)) {
-            int delimiterStart = DELIMITER_DECLARATION_SECTION.length();
-            int delimiterEnd = input.indexOf(DELIMITER_AND_NUMBERS_SEPARATOR);
-            return input.substring(delimiterStart, delimiterEnd);
-        }
-        return "";
+        int delimiterStart = DELIMITER_DECLARATION_SECTION.length();
+        int delimiterEnd = input.indexOf(DELIMITER_AND_NUMBERS_SEPARATOR);
+        return input.substring(delimiterStart, delimiterEnd);
+
+    }
+
+    private String[] splitByDelimiters(String input) {
+        String regex = String.join("|", delimiters.stream()
+                .map(delimiter -> "\\" + delimiter)
+                .toArray(String[]::new));
+        return input.split(regex);
     }
 }
