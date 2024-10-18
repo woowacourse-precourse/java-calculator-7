5a47878fdc6ccd62bd7efe649b50236654cfe9b8
CHISANW <keuye06380618@gmail.com>
2024-10-18 17:40:54 +0900

    test(CalculatorServiceTest): convertToNumbers() 메서드 단위 테스트 추가
    - 올바른 문자열을 숫자로 변환하여 리스트로 반환하는지 검증
    - 빈 문자열 입력 시 0으로 변환되는지 확인
    - 올바르지 않은 문자열 입력 시 IllegalArgumentException 발생 테스트 추가
    - 음수가 포함된 문자열 입력 시 IllegalArgumentException 발생 테스트 추가

--- src/test/java/calculator/service/CalculatorServiceTest.java
+++ src/test/java/calculator/service/CalculatorServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 
 class CalculatorServiceTest {
 
-
     @DisplayName("주어진 숫자 리스트의 총합을 계산")
     @Test
     void calculateSum() {
@@ -20,4 +19,50 @@ class CalculatorServiceTest {
         Assertions.assertThat(calculateSum).isNotEqualTo(20);
     }
 
+    @DisplayName("올바른 문자열을 숫자로 변환하여 숫자 리스트로 반환")
+    @Test
+    void convertToNumbers() {
+        String[] parts = {"1", "2", "4", "10"};
+
+        CalculatorService service = new CalculatorService();
+        List<Integer> values = service.convertToNumbers(parts);
+
+        Assertions.assertThat(values).containsAll(List.of(1, 2, 4, 10));
+        Assertions.assertThat(values).doesNotContainSequence(List.of(15, 42, 4, 10));
+    }
+
+    @DisplayName("빈 문자열 입력 시 0으로 변환")
+    @Test
+    void convertToNumbersContainsEmpty() {
+        String[] parts = {"", "2", "", "10"};
+        CalculatorService service = new CalculatorService();
+        List<Integer> values = service.convertToNumbers(parts);
+
+        Assertions.assertThat(values).containsAll(List.of(0, 2, 0, 10));
+        Assertions.assertThat(values).doesNotContainSequence(List.of(2, 10));
+    }
+
+    @DisplayName("올바르지 않은 문자열 입력 시 IllegalArgumentException 발생")
+    @Test
+    void convertToNumbersContainsInvalidStringThrowByIllegalArgumentException() {
+        String[] parts = {"1a", "2b", "4", "10"};
+
+        CalculatorService service = new CalculatorService();
+
+        Assertions.assertThatThrownBy(() -> service.convertToNumbers(parts))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 값이 입력되었습니다.");
+    }
+
+    @DisplayName("음수가 포함된 문자열 입력 시 IllegalArgumentException 발생")
+    @Test
+    void convertToNumbersContainsNegativeValueThrowByIllegalArgumentException() {
+        String[] parts = {"-1", "20", "4", "10"};
+
+        CalculatorService service = new CalculatorService();
+
+        Assertions.assertThatThrownBy(() -> service.convertToNumbers(parts))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 값이 입력되었습니다.");
+    }
 }
