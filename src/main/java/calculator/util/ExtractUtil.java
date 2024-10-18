5968b618f1319355e3c8536e391eef443de8df16
taek2222 <oot309@daum.net>
2024-10-18 22:49:43 +0900

    style(ExtractUtil): 변수 네이밍 간소화
    장황하게 작성한 네이밍을 간소화했습니다.

--- src/main/java/calculator/util/ExtractUtil.java
+++ src/main/java/calculator/util/ExtractUtil.java
@@ -6,31 +6,31 @@ public class ExtractUtil {
     public static final String EMPTY = "";
     public static final int CUSTOM_PREFIX_LENGTH = CUSTOM_PREFIX.length();
 
-    public String extractCustomDelimiter(String readInput) {
-        if (hasCustomDelimiter(readInput)) {
-            int[] positions = findCustomDelimiterPosition(readInput);
-            return extractDelimiter(readInput, positions);
+    public String extractCustomDelimiter(String input) {
+        if (hasCustomDelimiter(input)) {
+            int[] positions = findCustomDelimiterPosition(input);
+            return extractDelimiter(input, positions);
         }
         return EMPTY;
     }
 
-    private boolean hasCustomDelimiter(String readInput) {
-        return readInput.contains(CUSTOM_PREFIX) && readInput.contains(CUSTOM_SUFFIX);
+    private boolean hasCustomDelimiter(String input) {
+        return input.contains(CUSTOM_PREFIX) && input.contains(CUSTOM_SUFFIX);
     }
 
-    private int[] findCustomDelimiterPosition(String readInput) {
-        int prefixPos = readInput.indexOf(CUSTOM_PREFIX) + CUSTOM_PREFIX_LENGTH; // "//" 문자열 크기 만큼 이후 탐색을 위해 합산
-        int suffixPos = readInput.indexOf(CUSTOM_SUFFIX);
+    private int[] findCustomDelimiterPosition(String input) {
+        int prefixPos = input.indexOf(CUSTOM_PREFIX) + CUSTOM_PREFIX_LENGTH; // "//" 문자열 크기 만큼 이후 탐색을 위해 합산
+        int suffixPos = input.indexOf(CUSTOM_SUFFIX);
         return new int[] {prefixPos, suffixPos};
     }
 
-    private String extractDelimiter(String readInput, int[] position) {
-        return readInput.substring(position[0], position[1]);
+    private String extractDelimiter(String input, int[] position) {
+        return input.substring(position[0], position[1]);
     }
 
-    public String extractPositiveString(String readInput, String customDelimiter) {
+    public String extractPositiveString(String input, String customDelimiter) {
         String remove = createRemoveString(customDelimiter);
-        return readInput.replace(remove, EMPTY);
+        return input.replace(remove, EMPTY);
     }
 
     private String createRemoveString(String customDelimiter) {
