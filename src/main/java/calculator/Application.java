43c659adb84ff0efc7737aecbc32d2141bfc6bd9
Youngchan <wdc4512@naver.com>
2024-10-18 23:27:31 +0900

    feat: 덧셈 수행 및 결과 출력
    - 숫자 리스트의 합을 계산하는 sumNumber 메서드
    - 결과를 출력하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,15 +2,26 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // 사용자에게 입력 요청
         System.out.print("문자열 입력: ");
 
-        // 콘솔에서 입력받은 값을 input에 저장
+        // 콘솔에서 입력받은 문자열을 input에 저장
         String input = Console.readLine();
 
-        // 입력받은 값을 출력
+        // 입력받은 문자열을 출력
         System.out.println("입력한 문자열: " + input);
+
+        // 입력받은 문자열을 분리 후 숫자로 변환
+        List<Integer> number = StringCalculator.splitAndConvert(input);
+
+        // 숫자로 변환된 리스트의 총합 구하기
+        int sum = StringCalculator.sumNumber(number);
+
+        // 결과 출력
+        System.out.println("결과 : " + sum);
     }
 }
