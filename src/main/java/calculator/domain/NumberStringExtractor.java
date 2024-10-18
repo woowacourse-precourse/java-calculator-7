ba47dad1ba977ba7a72d2272f917ba163c2d3007
Jiihyun <cwlgusc@gmail.com>
2024-10-18 21:54:39 +0900

    fix: 구분자 제외한 숫자 배열얻을 때 공백 들어가는 부분 수정

--- src/main/java/calculator/domain/NumberStringExtractor.java
+++ src/main/java/calculator/domain/NumberStringExtractor.java
@@ -20,7 +20,9 @@ public class NumberStringExtractor {
             customDelimiter = customDelimiterExtractor.getCustomDelimiter(stringToAdd);
             stringToAdd = changeStringWithoutFormat(stringToAdd);
         }
-        return stringToAdd.split("[" + customDelimiter + COLON + COMMA + "]");
+        //  | 기호는 정규 표현식에서 "또는"의 의미를 가지고 있습니다.
+        //  즉, 아래 코드는 customDelimiter, COLON, 또는 COMMA 로 문자열을 나누겠다는 뜻입니다.
+        return stringToAdd.split(customDelimiter + "|" + COLON + "|" + COMMA);
     }
 
     private String changeStringWithoutFormat(String stringWithDelimiter) {
