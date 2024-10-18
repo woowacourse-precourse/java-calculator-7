fdfe5d2092c6245719d6fa252be22640c91c159e
SEOB <svj7531@naver.com>
2024-10-19 04:33:05 +0900

    feat: 커스텀 구분자 덧셈 기능 구현

--- src/main/java/calculator/Delimiter.java
+++ src/main/java/calculator/Delimiter.java
@@ -4,6 +4,9 @@ public class Delimiter {
 
     public static String[] getDelimiter(String word) {
 
+        if (word.startsWith("//")) {
+            return customDelimiter(word);
+        }
         return defaultDelimiter(word);
 
     }
@@ -11,6 +14,16 @@ public class Delimiter {
     private static String[] defaultDelimiter(String words) {
         String[] numbers = words.split("[,:]");
         return numbers;
+    }
+
+    private static String[] customDelimiter(String words) {
+
+        String regex = words.substring(2, 3);
+        String numbersPart = words.substring(5);
+
+        String[] numbers = numbersPart.split(regex);
+        return numbers;
+
     }
 
 }
