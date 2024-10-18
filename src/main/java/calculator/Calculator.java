009f25cb4d3b2f8c98c13fc171028faa100430a7
Leejunho <junho767@naver.com>
2024-10-18 19:37:45 +0900

    feat : 구분자와 숫자 분리 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -16,10 +16,9 @@ public class Calculator {
             return;
         }
 
-        if(splitByDelimiters(str)){ // 커스텀 구분자 유무 검사
-            System.out.println("true");
-        } else {
-            System.out.println("false");
+        List<String> numbers = splitByDelimiters(str);  // 구분자로 숫자 분리
+        for(String i : numbers){ // 숫자 분리 확인
+            System.out.println(i);
         }
     }
 
@@ -29,7 +28,27 @@ public class Calculator {
     }
 
     // 입력된 문자열에서 구분자로 숫자 분리
-    public static boolean splitByDelimiters(String str) {
-        return str.startsWith("//");
+    public static List<String> splitByDelimiters(String str) {
+        if (str.startsWith("//")) {
+            return splitByCustomDelimiter(str);  // 커스텀 구분자 처리
+        } else {
+            return splitByDefaultDelimiters(str); // 기본 구분자 처리
+        }
+    }
+
+    // 기본 구분자로 문자열을 분리
+    private static List<String> splitByDefaultDelimiters(String str) {
+        return Arrays.asList(str.split("[,:]"));
+    }
+
+    // 커스텀 구분자로 문자열을 분리
+    private static List<String> splitByCustomDelimiter(String str) {
+        int startIndex = 2; // 커스텀 구분자의 시작 인덱스
+        int endIndex = str.indexOf("\\n"); // 구분자와 숫자 부분을 분리
+        String customDelimiters = str.substring(startIndex, endIndex); // 커스텀 구분자 추출
+
+        String numberPart = str.substring(endIndex + 2); // 숫자 부분 추출
+        String delimiters = "[" + customDelimiters + ",:]"; // 커스텀 구분자와 기본 구분자를 모두 포함
+        return Arrays.asList(numberPart.split(delimiters)); // 최종 구분자로 문자열을 분리
     }
 }
