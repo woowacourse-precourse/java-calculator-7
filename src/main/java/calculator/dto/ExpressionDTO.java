444054262ac9605643e28a4e36f1d12c67d303f3
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-19 04:05:58 +0900

    refactor(ExpressionDTO) : 코드 가독성 개선

--- src/main/java/calculator/dto/ExpressionDTO.java
+++ src/main/java/calculator/dto/ExpressionDTO.java
@@ -6,32 +6,37 @@ import java.util.regex.Pattern;
 public class ExpressionDTO {
 
     private static final String CUSTOM_DELIMITER_FORMAT = "//([^0-9]+)\\\\n";
+    private static final String DEFAULT_DELIMITER_FORMAT = ":,";
 
-    private String incorrectPattern = "[^:,.0-9]";
-    private String delimiter = "[:,]";
+    private String incorrectDelimiter = "[^" + DEFAULT_DELIMITER_FORMAT + ".0-9]";
+    private String delimiter = "[" + DEFAULT_DELIMITER_FORMAT + "]";
     private String expression;
 
-    public ExpressionDTO(String expression) {
-        this.expression = expression;
-        setCustomDelimiter();
+    public ExpressionDTO(String input) {
+        this.expression = input;
+        initialize(input);
     }
 
-    private void setCustomDelimiter() {
+    private void initialize(String expression) {
         Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_FORMAT).matcher(expression);
 
         if (matcher.find() && expression.startsWith(matcher.group())) {
-            // 정규표현식의 예약 문자에 해당되는 특수문자를 구분자로 사용하기 위한 밑작업
-            String newDelimiter = matcher.group(1)
-                    .replace("\\", "\\\\")
-                    .replace("[", "\\[");
+            String newDelimiter = convertMetaLetterToRegular(matcher.group(1));
             this.expression = expression.replace(matcher.group(), "").trim();
-            this.delimiter = delimiter.replace(":,", String.join("\\\\", newDelimiter));
-            this.incorrectPattern = incorrectPattern.replace(":,", newDelimiter);
+            this.delimiter = delimiter.replace(":,", newDelimiter);
+            this.incorrectDelimiter = incorrectDelimiter.replace(":,", newDelimiter);
         }
     }
+    // 정규표현식의 예약 문자도 커스텀 구분자로 사용하기 위한 밑작업
+    private String convertMetaLetterToRegular(String delimiter) {
+        return delimiter.replace("\\", "\\\\")
+                .replace("[", "\\[")
+                .replace("^", "\\^")
+                .replace("]", "\\]");
+    }
 
-    public String getIncorrectPattern() {
-        return incorrectPattern;
+    public String getIncorrectDelimiter() {
+        return incorrectDelimiter;
     }
 
     public String getDelimiter() {
