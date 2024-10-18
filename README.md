2b148b1d156f77f4b6b134408d5f357931e63973
김정욱 <yeoback07@icloud.com>
2024-10-19 01:03:57 +0900

    feat(Application.java, README.md) : add
    1. camp.nextstep.edu.missionutils.Console 클래스를 임포트해서 입력값을 받아옴.
    2. 커스텀구분자가 있을 것을 생각해 기본 구분자를 변수에 할당.
    3. 조건문을 이용해서 "//"(이하 더블슬래쉬)로 시작하는 입력문은 조건문 내부로 진행.
    4. 조건문 내부에서 더블슬래쉬와 "\n"(이하 역슬래쉬n)의 인덱스 값을 받아옴.
    5. substring() 함수를 이용해서 커스텀구분자만 추출.
    6. 기존 입력값도 커스텀구분자 지정문을 뺀 입력값으로 변경.
    7. 본문으로 돌아와서, 문자열 배열으로 split()함수를 사용하여 추출한 커스텀구분자와 기본 구분자를 병합.
    8. try catch 문을 사용해 split의 배열요소 값 만큼 반복해서 result 변수에 더하며 할당.
    9. 잘못된 값을 입력하였을 때, IllegalArgumentException을 반환하며, 에러메세지를 표시함.
    10. 합산한 값 표시
    
    역슬래쉬n을 추출하는 과정에서 어려움을 겪음. "\n"의 인덱스 값을 구하려고 하니, -1이란 값이 나옴. 값이 존재하지 않음. 하지만 이것을 "\\n"으로 변경하여 문제를 해결함. 다른 방법도 시도해보았지만 효율적이지 않음.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # java-calculator-precourse
+
+* * *
+
+### 구현할 기능
+
+* `camp.nextstep.edu.missionutills`에서 제공하는 `Console` 클래스 파일 확인하기
+* `try`,`catch`를 사용하여 잘못된 값을 입력할 경우에 `IllegalArgumentException`을 처리 할 수 있도록 하기
+* 예상치 못한 문제 : `Console` 명령어는 `Scanner`를 이용해 값을 입력받는데, `\n`을 할 경우, 입력값이 줄바꿈되며 사라진다. 따라서 인덱스 값을 찾을 수 없다. 다른 방법을 찾아야겠다.
--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,37 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine(); // camp.nextstep.edu.missionutils.Console에 readLine 함수 호출. nextLine() 고정.
+//        System.out.println(input);
+        
+        String customDelimiter = "[,:]";
+        if (input.startsWith("//")) {
+            int indexOfDoubleSlash = input.indexOf("//");
+            int indexOfReveredSlashN = input.indexOf("\\n");
+//            System.out.println("\\n의 인덱스 : " + indexOfReveredSlashN);
+            customDelimiter = input.substring(indexOfDoubleSlash + 2,
+                    indexOfReveredSlashN);
+//            System.out.println(customDelimiter);
+            input = input.substring(indexOfReveredSlashN + 2);
+//            System.out.println("3 : " + input);
+        }
+
+//        System.out.println(customDelimiter);
+        String[] split = input.split(customDelimiter + "|[,:]");
+        int result = 0;
+        try {
+            for (String s : split) {
+                result += Integer.parseInt(s);
+            }
+        } catch (Exception e) {
+//            System.err.println(e.getMessage());
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다 : " + e.getMessage());
+        }
+        System.out.println("결과 : " + result);
     }
 }
