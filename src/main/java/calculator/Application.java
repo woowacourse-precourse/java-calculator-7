e79cf565b6ef878ed8efe145fa4cf4c8ccbc1987
samueljang <legoddsl@gmail.com>
2024-10-18 17:49:13 +0900

    feat($Application): Add String Input and Sum Output feature
    문자열을 입력 받아 기본 구분자인 ,와:를 기준으로 나눠 숫자라면 더하여 출력합니다. 숫자가 아닐 경우 잘못된 입력으로 에러처리를 합니다. 아직 커스텀 구분자 설정 기능을 추가해야합니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,30 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.\n");
+        String input = scanner.nextLine();
+
+        //일단은 ,와: 기준으로만 문자열 분리
+        // 커스텀 구분자 추가해야함!!!
+        String[] parts = input.split("[,:]");
+
+        int sum = 0;
+        for(String part : parts) {
+            try {
+                //숫자라면 더하기
+                sum += Integer.parseInt(part);
+            } catch (NumberFormatException e) {
+                //숫자가 아니라면 에러 처리
+                throw new IllegalArgumentException("입력값 에러 발생 : " + part );
+
+            }
+        }
+        System.out.println("결과 : " + sum);
     }
 }
