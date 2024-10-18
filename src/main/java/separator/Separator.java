6dc047c61bbcf4deab470919fd6809752199a339
parkseongyeol <ert2154@naver.com>
2024-10-18 19:45:01 +0900

    feat($Separator): 구분자 목록 저장소 생성
    기본 구분자와 커스텀 구분자를 저장할 separators를 static으로 선언

---
+++ src/main/java/separator/Separator.java
@@ -0,0 +1,12 @@
+package separator;
+
+import java.util.ArrayList;
+
+public class Separator {
+    public static ArrayList<Character> separators = new ArrayList<>();
+
+    public Separator() {
+        separators.add(',');
+        separators.add(':');
+    }
+}
