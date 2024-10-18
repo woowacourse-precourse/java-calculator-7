4329d33bdad9cc5b54df9ddb5c06f0d081de2116
Jinu Yi <dionisos198@naver.com>
2024-10-18 15:46:00 +0900

    feat(Application): 기본 출력문 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,10 +6,13 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = Console.readLine();
+
         CalculateController calculateController = new CalculateController();
 
-        calculateController.insertUserInput(userInput);
+        System.out.println("결과 : " + calculateController.insertUserInput(userInput));
+
 
     }
 }
