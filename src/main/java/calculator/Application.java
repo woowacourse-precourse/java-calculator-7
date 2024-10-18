5ddd89e7de6b56b3d57b32eb53f125ceb5f7661a
parkseongyeol <ert2154@naver.com>
2024-10-18 22:24:26 +0900

    feat($Application): 문자열 덧셈 계산기 제작 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,42 @@
 package calculator;
 
+import adder.Adder;
+import camp.nextstep.edu.missionutils.Console;
+import separator.Separator;
+
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Adder adder = new Adder();
+        Separator separator = new Separator();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        String calculatorFormula = input;
+        // 구분자 추출
+        if(separator.validateCustomSeparatorForParsing(input)){
+            Character customSeparator = separator.parseCustomSeparator(input);
+            separator.addNewSeparators(customSeparator);
+            calculatorFormula = separator.parseString(input);
+        }
+
+        // 계산 시작
+        ArrayList<String> parsedCalculatorFormula = adder.parseStringWithSeparators(calculatorFormula);
+
+        if(!adder.isValidated(parsedCalculatorFormula)){
+            throw new IllegalArgumentException();
+        }
+
+        ArrayList<Integer> numbers = adder.parseOnlyNumbers(parsedCalculatorFormula);
+        if(!adder.isAllPositiveInteger(numbers)){
+            throw new IllegalArgumentException();
+        }
+
+        int result = adder.addNumbers(numbers);
+
+        System.out.println("결과 : " + result);
+
     }
 }
