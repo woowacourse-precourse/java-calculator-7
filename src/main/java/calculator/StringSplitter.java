97d1bf0fbfe75273042b1a2b793d7f481a450a3c
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-18 23:14:37 +0900

    feat(splitter): 커스텀 구분자 추출 함수 구현
    - 구분자 관련 상수를 final로 선언
    
    - input이 CUSTOM_DELIMITER_PREFIX로 시작할 경우 커스텀 구분자로 split 후 기본 구분자로 split 처리

--- src/main/java/calculator/StringSplitter.java
+++ src/main/java/calculator/StringSplitter.java
@@ -3,12 +3,27 @@ package calculator;
 public class StringSplitter {
 
     private static final String DEFAULT_DELIMITERS = "[,:]";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String DELIMITER_END = "\\n";
 
     public String[] split(String input) {
         if (input == null || input.isEmpty()) {
             return new String[0];
         }
 
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            String customDelimiter = extractCustomDelimiter(input);
+            int contentStartIndex = input.indexOf(DELIMITER_END) + DELIMITER_END.length();
+            return input.substring(contentStartIndex)
+                    .split(customDelimiter);
+        }
+
         return input.split(DEFAULT_DELIMITERS);
+    }
+
+    private String extractCustomDelimiter(String input) {
+        int startIndex = CUSTOM_DELIMITER_PREFIX.length();
+        int endIndex = input.indexOf(DELIMITER_END);
+        return input.substring(startIndex, endIndex);
     }
 }
