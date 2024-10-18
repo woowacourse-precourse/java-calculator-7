7898a7ecfb06a2554f8b9b3b97cb93c4886b4d43
yybmion <yyb400@naver.com>
2024-10-18 18:43:15 +0900

    [feat]: 커스텀 구분자 존재 여부 확인 및 커스텀 구분자 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,5 +5,26 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         String inputString = readLine();
+
+        splitString(inputString);
+    }
+
+    /**
+     * @param inputString: 입력값
+     * @return 계산 결과 만약 입력값이 "//"로 시작되고 그 뒤에 "\n"가 존재한다면 커스텀 구분자가 존재한다 (커스텀 구분자 내 공백 제외).
+     */
+    static public int splitString(String inputString) {
+        if (inputString.startsWith("//")) {
+            int newLineIndex = inputString.indexOf("\n");
+            if (newLineIndex == -1) {
+                throw new IllegalArgumentException("커스텀 구분자 지정 후 '\\\\n'이 없습니다.");
+            }
+
+            String customDelimiter = inputString.substring(2, newLineIndex);
+
+            if (customDelimiter.isEmpty()) {
+                throw new IllegalArgumentException("커스텀 구분자가 비어있습니다.")
+            }
+        }
     }
 }
