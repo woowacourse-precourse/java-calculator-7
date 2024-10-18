b9d9023c6dee3113919cbd46a67e1472542a2e8d
Avocado <ujubongbong@gmail.com>
2024-10-18 18:11:02 +0900

    :sparkles: feat: 문자열 덧셈 계산기 시작 함수

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    // 문자열 덧셈 계산기 시작 함수
+    public static void init() {
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        init();
     }
 }
