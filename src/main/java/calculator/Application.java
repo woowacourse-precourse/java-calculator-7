321123aa1a5bc077632504ffed45c00d8135346f
배석현 <bjh4966@naver.com>
2024-10-18 12:22:57 +0900

    1. 입력과 입력에 대한 오류 체크

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application app = new Application();
+        System.out.print("덧셈할 문자열을 입력해 주세요.\n");
+
+        String input = null;
+        try {
+            input = Console.readLine();
+        } catch (NoSuchElementException e) {
+            System.err.println("입력이 없습니다. 프로그램을 종료합니다.");
+            return;
+        }
     }
 }
