df7afe330fe833b8c6899c43928953e78de3c6e5
김영록 <praoo800@konkuk.ac.kr>
2024-10-19 01:48:04 +0900

    <feat>(<Application.java>): implement a sum of characters, including commas or colons
    add the following features:
    - receive string input using Console api
    - replace all with spaces if the string contains a colon or comma
    - 0 output if the string length of the result of replacement is 0
    - split the string into spaces, change the type to numbers, and add them all

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String str = Console.readLine();
+        ArrayList<String> seperator = new ArrayList<>();
+        seperator.add(",");
+        seperator.add(":");
+        for (String s : seperator) {
+            str = str.replaceAll(s, " ");
+        }
+        int sum = 0;
+        if(!str.isEmpty()){
+            String[] number = str.split(" ");
+            for(String s : number){
+                sum += Integer.parseInt(s);
+            }
+        }
+        System.out.println(sum);
     }
 }
