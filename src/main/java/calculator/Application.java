583338706df0be46cad01f3375595aa5b34fc425
sik9252 <sik9252@naver.com>
2024-10-18 22:36:37 +0900

    refactor: DataVerification을 ValidateUtils로 명칭 변경하고, Utils Class로 사용하도록 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,21 +3,18 @@ package calculator;
 import calculator.utils.InputUtils;
 import calculator.utils.PrintUtils;
 import calculator.utils.SplitUtils;
+import calculator.utils.ValidateUtils;
 
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        DataVerification dataVerification = new DataVerification();
 
         String userInput = InputUtils.getUserInput();
-        dataVerification.checkIsInputEmpty(userInput);
-
         List<String> splitInput = SplitUtils.getSplitInput(userInput);
 
-        dataVerification.checkIsPositiveNumber(splitInput);
-        dataVerification.checkHasNumberFormat(splitInput);
+        ValidateUtils.checkIsNumericAndPositive(splitInput);
 
         int total = calculator.getTotal(splitInput);
         PrintUtils.printResult(total);
---
+++ src/main/java/calculator/utils/ValidateUtils.java
@@ -0,0 +1,23 @@
+package calculator.utils;
+
+import calculator.constant.ErrorMessage;
+
+import java.util.List;
+
+public class ValidateUtils {
+    public static void checkIsNumericAndPositive(List<String> splitInput) {
+        for (String text : splitInput) {
+            if (isNumeric(text)) {
+                if (Integer.parseInt(text) < 0) {
+                    throw new IllegalArgumentException(ErrorMessage.NEGATIVE_NUMBER_INPUT.getMessage());
+                }
+            } else {
+                throw new IllegalArgumentException(ErrorMessage.NOT_A_NUMBER.getMessage());
+            }
+        }
+    }
+
+    private static  boolean isNumeric(String str) {
+        return str != null && str.matches("-?\\d+");
+    }
+}
