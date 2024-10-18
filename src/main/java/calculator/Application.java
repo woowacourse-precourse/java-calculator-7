c218b9e2c1397ce50be073323aed8440e22b5f17
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-10-18 19:16:38 +0900

    feat: string 입력받기
    camp.nextstep.edu.missionutils.Console 를 사용하여 입력값 받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String str = Console.readLine();
+
+        System.out.println(str);
     }
 }
