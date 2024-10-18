2298a25643faaf70d09ab579ea57d1b9f4d777ca
junhaa <2171326@hansung.ac.kr>
2024-10-18 19:02:29 +0900

    feat(StringCalculator): 메인 로직에 구현한 기능 추가
    - 메인 로직에 DelimiterExtractor, DelimiterTokenizer 기능 추가

--- src/main/java/calculator/controller/StringCalculator.java
+++ src/main/java/calculator/controller/StringCalculator.java
@@ -2,10 +2,12 @@ package calculator.controller;
 
 import calculator.domain.DelimiterExtractor;
 import calculator.domain.DelimiterTokenizer;
+import calculator.domain.ExtractResult;
 import calculator.domain.ResultCalculator;
 import calculator.global.OutputMessage;
 import calculator.view.InputView;
 import calculator.view.OutputView;
+import java.util.List;
 
 public class StringCalculator {
     private InputView inputView;
@@ -29,6 +31,13 @@ public class StringCalculator {
 
         // 문자열 입력
         String input = inputView.getInput();
+
+        // 구분자 추출, 기존 문자열의 구분자 부분 제거
+        ExtractResult extractResult = delimiterExtractor.getDelimiters(input);
+
+        // 구분자로 문자열 토큰 단위 후 Integer 리스트로 추출
+        List<Integer> numbers = delimiterTokenizer.getNumbers(extractResult);
+
 
 
     }
