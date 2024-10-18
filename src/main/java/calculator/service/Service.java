c70c57fe87e73a799a6cb2a19f2668ca675126e4
chan <ckstj8027@naver.com>
2024-10-18 17:44:34 +0900

    feat(service): 커스텀과 기본구분자 타입구분 기능과 덧셈기능 추가

--- src/main/java/calculator/service/Service.java
+++ src/main/java/calculator/service/Service.java
@@ -1,5 +1,6 @@
 package calculator.service;
 
+import calculator.calculator.Calculator;
 import calculator.delimiterExtractor.DelimiterExtractor;
 import calculator.numberExtractor.NumberExtractor;
 import java.util.ArrayList;
@@ -18,6 +19,28 @@ public class Service {
 
         return numberExtractor.extractNumbers(input, delimiter);
 
+    }
+
+
+    public String checkTypeOfInput(String input) {
+        char[] inputToCharArray = input.toCharArray();
+        String firstString = String.valueOf(inputToCharArray[0]);
+        String secondString = String.valueOf(inputToCharArray[1]);
+
+        if (firstString.equals("/") && secondString.equals("/")) {
+            return "CustomInput";
+        }
+
+        if (input.contains(",") || input.contains(":")) {
+            return "DefaultInput";
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+
+    public int addAll(ArrayList<Integer> extractNumbers) {
+        return Calculator.allSum(extractNumbers);
     }
 
 
