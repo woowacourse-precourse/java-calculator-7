732ac18ff583aeed5980d59073a0ce007c7cc26f
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-10-18 23:52:07 +0900

    feat($calculator) : 덧셈 수행 및 통합 진행 기능 개발
    추가사항:
    - 전체 진행 과정 주관하는 manageAdd 개발
    - 덧셈 수행하는 parseAndAdd 개발

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -28,9 +28,9 @@ public class Calculator {
         this.sum = sum;
     }
 
-    public String addSeparator(String str) throws IllegalArgumentException {
+    private String addSeparator(String str) throws IllegalArgumentException {
 
-        while (str.length() > 0 && str.charAt(0) == '/') {
+        while (!str.isEmpty() && str.charAt(0) == '/') {
             if (str.length() >= 5 && str.charAt(1) == '/') { // 문자 길이로 애초에 양식 안맞고 길이 안맞는거 걸러서 indexing 오류 제거
                 if (str.substring(2, 3).matches("[^a-zA-Z0-9\\\\\\\\s]")) { //특수 문자인지 확인
                     String sep = str.substring(2, 3);
@@ -44,30 +44,60 @@ public class Calculator {
                 } else {
                     throw new IllegalArgumentException("잘못된 입력입니다. 구분자가 없습니다.");
                 }
-                System.out.println(str);
             } else {
                 throw new IllegalArgumentException("잘못된 입력입니다. // 양식 혹은 길이가 맞지 않습니다.");
             }
         }
-
-        System.out.println(str);
         return str;
     }
 
     public void manageAdd(String input) throws IllegalArgumentException {
         this.setInput(input);
-        String stack = new String("");
-
+        StringBuilder stack = new StringBuilder();
+        boolean isSeparator = true;
         if (this.input.isEmpty()) {
             return;
         }
-        this.addSeparator(this.input);
-//        while (!this.input.isEmpty()) {
-//            stack = stack.concat(this.input.substring(0, 1));
-//            this.input = this.input.substring(1);
-//            if (stack)
-//        }
+        String remained = this.addSeparator(this.input);
+
+        for (int i = 0; i < remained.length(); i++) {
+            char current = remained.charAt(i);
+            // separater인 경우
+            if (this.separator.contains(String.valueOf(current))) {
+                if (isSeparator) { //구분자가 2번 연속 나옴
+                    throw new IllegalArgumentException("잘못된 구분자가 연속으로 나왔습니다.");
+                }
+
+                this.sum += parseAndAdd(stack.toString()); // 스택에 있는 숫자를 합산하고 초기화
+                System.out.println("separator에서 sum 더한 이후" + this.sum);
+                stack.setLength(0);
+                isSeparator = true; // 다음 문자가 구분자임을 표시
+            } else { // 숫자 쌓기
+                if (!Character.isDigit(current)) {
+                    throw new IllegalArgumentException("유효하지 않은 문자가 입력되었습니다.");
+                }
+                stack.append(current);
+                System.out.println("stack에 숫자 쌓은 이후" + stack);
+                isSeparator = false; // 이전 문자가 구분자가 아님
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            sum += parseAndAdd(stack.toString());
+        }
+
+        if (isSeparator) {
+            throw new IllegalArgumentException("구분자 뒤에 숫자가 나오지 않았습니다");
+        }
+
     }
 
+    private int parseAndAdd(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) { //Int보다 큰 범위 탐지
+            throw new IllegalArgumentException("숫자 형식이 잘못되었습니다.");
+        }
+    }
 
 }
