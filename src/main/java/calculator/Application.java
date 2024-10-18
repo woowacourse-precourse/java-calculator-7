721ef08ce5a08a7570c18076d121f2426077849b
sanghun <vitash1215@gmail.com>
2024-10-19 03:19:32 +0900

    feat(empty) : 빈문자 처리
    사용자가 빈문자를 입력하면 결과값을 0으로 출력한다.
    
    빈문자 입력처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String string = Console.readLine();
+        if (string.isEmpty()) {
+            System.out.println("결과 : 0");
+            Console.close();
+            return;
+        }
         System.out.println("결과 : " + string);
         Console.close();
     }
