0d929090c79b737d114c44d7678436d74c059609
joon <khjoon372@naver.com>
2024-10-18 20:26:56 +0900

    feat: 문자열에서 숫자 추출하는 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,8 +1,8 @@
 package calculator;
 
 public class Calculator {
-
     String inputString;
+
 
     public Calculator(String inputString){
         this.inputString = inputString;
@@ -15,6 +15,41 @@ public class Calculator {
      */
     private boolean isEmptyString(String inputString) {
         return inputString == null || inputString.isEmpty();
+    }
+
+    /**
+     * 숫자를 추출하는 메서드
+     * @param inputString
+     * @return 추출된 숫자 배열
+     */
+    private String[] extractNumbers(String inputString){
+
+        String delimiters = ",|:";
+
+        if(inputString.indexOf("//") != 0){
+            int start = inputString.indexOf("//");
+            int end = inputString.indexOf("\n");
+
+            String newDelimiter = extractDelimiter(inputString,start,end);
+
+            delimiters += "|" + newDelimiter;
+
+            inputString = inputString.substring(end+1);
+        }
+
+        return inputString.split(delimiters);
+    }
+
+    /**
+     * 사용자 지정 구분자 추출 함수
+     * @param inputString
+     * @param start
+     * @param end
+     * @return 추출된 구분자
+     */
+    private String extractDelimiter(String inputString, int start, int end){
+
+        return inputString.substring(start,end);
     }
 
 
