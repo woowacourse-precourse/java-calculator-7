05e76e044a2e0f40dead5d9c25cd0e578dd10185
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2024-10-18 18:41:21 +0900

    fix(Calculator.java) : Fix get_input() method to use Console.readLine() method
    Fix get_input() method to use Console.readLine() method from Scanner

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,15 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Calculator {
 
     public static String get_input() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return sc.nextLine();
+        String input = Console.readLine();
+        Console.close();
+        return input;
     }
 
     public static List<Integer> parse(String input) {
