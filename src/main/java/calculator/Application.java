47f140dfa5697bc76ae0b908838c19cc11472981
GunsanHaribo <hth99131@gmail.com>
2024-10-18 18:26:35 +0900

    feat(Application.java): 빈문자열이 들어왔을떄 0을 반환하는 기능입니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        int result = add(input);
+        System.out.println("결과 : " + result);
+    }
+
+    private static int add(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+
+        return 1;
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,6 +8,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void validateEmptyInput() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+
+        assertSimpleTest(() -> {
+            run("100");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -19,8 +34,8 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
