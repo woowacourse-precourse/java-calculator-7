4310299cc600c6cce2a11fd3366e77da1b4c3809
goochul <gooch123@naver.com>
2024-10-18 20:44:07 +0900

    feat(CustomDelimiter): 커스텀 구분자 기능 추가
    정규표현식을 통해 // 와 \n 사이의 문자열을 가져와 분리 후 문자열 배열로 반환

---
+++ src/main/java/calculator/feature/CustomDelimiter.java
@@ -0,0 +1,21 @@
+package calculator.feature;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomDelimiter {
+
+    String str;
+
+    public CustomDelimiter(String str) {
+        this.str = str;
+    }
+
+    public String[] getDelimiter() {
+        Pattern pattern = Pattern.compile("//(.*?)\\n"); // '//' 와 '\n' 사이 패턴
+        Matcher matcher = pattern.matcher(str);
+
+        return matcher.group(1).split("");
+    }
+
+}
