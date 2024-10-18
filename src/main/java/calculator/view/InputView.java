a8cf32db047755f3267c048d463232528964f7a5
dbdb456 <adad568@naver.com>
2024-10-18 22:34:51 +0900

    feat(InputView): 사용자 입력을 통해 덧셈할 문자열을 받는 기능 추가

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,4 +1,16 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public String startCalculatorWithInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        try {
+            return Console.readLine();
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
