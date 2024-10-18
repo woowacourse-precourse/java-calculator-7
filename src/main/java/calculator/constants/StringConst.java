34bfdb2362fde99c6f5132b3a7b6c0ceafcc5963
chan <ckstj8027@naver.com>
2024-10-19 00:57:10 +0900

    refactor(StringConst): String상수 추가 및 일부 이름 변경

--- src/main/java/calculator/constants/StringConst.java
+++ src/main/java/calculator/constants/StringConst.java
@@ -2,8 +2,15 @@ package calculator.constants;
 
 public class StringConst {
 
-    public static final String LINE_SEPARATOR = "\\\\n";
+    public static final String LINE_SEPARATOR_REGEX = "\\\\n";
 
-    public static final String DEFAULT_DELiMITER = ",|:";
+    public static final String LINE_SEPARATOR_STRING = "\\n";
+
+    public static final String DEFAULT_DELiMITER_REGEX = ",|:";
+
+    public static final String CUSTOM_START_STRING = "//";
+
+    public static final String COMMA = ",";
+    public static final String COLON = ":";
 
 }
