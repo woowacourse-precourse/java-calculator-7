2fbe348e3ffd7c90bb04d0233683bff7f2b5c410
yesjuhee <juhee0924@g.skku.edu>
2024-10-18 13:20:38 +0900

    feat: 문자열에서 커스텀 구분자를 추출하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,8 +6,15 @@ public class Application {
     public static void main(String[] args) {
 
         // 문자열을 입력 받아 저장한다.
-        String inputStr = Console.readLine();
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String inputString = Console.readLine();
         Console.close();
-        Calculator calculator = new Calculator(inputStr);
+
+        // 문자열에서 커스텀 구분자를 추출할 수 있다.
+        DelimiterParser delimiterParser = new DelimiterParser();
+        delimiterParser.parseDelimiters(inputString);
+
+        System.out.println("Delimiters: " + delimiterParser.getDelimiters());
+        System.out.println("ValueString: " + delimiterParser.getValueString());
     }
 }
---
+++ src/main/java/calculator/DelimiterParser.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DelimiterParser {
+    private final List<Character> delimiters = new ArrayList<>();
+    private String valueString;
+
+    public void parseDelimiters(String inputString) {
+        int currentIndex = 0;
+        while (inputString.startsWith("//", currentIndex)) {
+            int delimiterStartIndex = currentIndex + 2;
+            int delimiterEndIndex = inputString.indexOf("\\n", delimiterStartIndex);
+            delimiters.add(inputString.charAt(delimiterStartIndex));
+            currentIndex = delimiterEndIndex + 2;
+        }
+        valueString = inputString.substring(currentIndex);
+    }
+
+    public List<Character> getDelimiters() {
+        return delimiters;
+    }
+
+    public String getValueString() {
+        return valueString;
+    }
+}
