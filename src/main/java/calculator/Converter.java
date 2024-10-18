d578a9b97cc14f3b38b4624a5b79f866f1ab5730
soeun2537 <soeun2537@gmail.com>
2024-10-18 16:05:08 +0900

    docs: Converter 클래스 설명 주석 추가

--- src/main/java/calculator/Converter.java
+++ src/main/java/calculator/Converter.java
@@ -3,10 +3,13 @@ package calculator;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 문자열 배열을 정수 리스트로 변환하는 클래스입니다.
+ */
 public class Converter {
 
     /**
-     * 문자열 배열을 정수 리스트로 변환
+     * 문자열을 정수 리스트로 변환
      */
     public List<Integer> convertToNumbers(String[] tokens) {
         List<Integer> numbers = new ArrayList<>();
