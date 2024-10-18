d36b740aaae9a2647097516dc1dbe3e2ed626492
Dohun Kim <dohun8758@naver.com>
2024-10-19 03:32:51 +0900

    feat : 덧셈 기능
    - 파싱된 문자열 배열을 숫자로 변환하여 더하는 메서드 구현

--- src/main/java/calculator/service/StringAddCalculatorService.java
+++ src/main/java/calculator/service/StringAddCalculatorService.java
@@ -1,10 +1,28 @@
 package calculator.service;
 
+import calculator.domain.Calculator;
 import calculator.util.CustomStringParser;
 import calculator.util.StandardStringParser;
 
 public class StringAddCalculatorService {
 
+    public String add(String input) {
+        String[] splitStringArr = stringParse(input);
+
+        Calculator calculator = new Calculator();
+
+        return stringAdd(splitStringArr, calculator);
+    }
+
+    private String stringAdd(String[] splitStringArr, Calculator calculator) {
+        for (String s : splitStringArr) {
+            long num = Long.parseLong(s);
+
+            calculator.add(num);
+
+        }
+        return calculator.getResult();
+    }
 
     private String[] stringParse(String input) {
         if (CustomStringParser.hasCustomSeparator(input)) {
