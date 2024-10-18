48661f01c86e1d1093a92c28a5f72543ef143f2f
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-10-18 23:52:55 +0900

    feat($calculator) : 에러 처리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,13 +5,22 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         while (true) {
-            Calculator calculator = new Calculator();
+            try {
+                Calculator calculator = new Calculator();
 
-            System.out.println("덧셈할 문자열을 입력해 주세요.");
+                System.out.println("덧셈할 문자열을 입력해 주세요.");
 
-            String input = Console.readLine();
+                String input = Console.readLine();
 
-            System.out.println("결과 : " + calculator.getSum());
+                calculator.manageAdd(input);
+
+                System.out.println("결과 : " + calculator.getSum());
+
+            } catch (IllegalArgumentException e) {
+                System.err.println("예외 발생: " + e.getMessage());
+                break;
+
+            }
         }
     }
 }
