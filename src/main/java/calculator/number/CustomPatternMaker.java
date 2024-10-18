1e59cbaa6bed63195a7f30fb09b5211e0aef5cb1
jaeyeong <zzz26lk@gmail.com>
2024-10-18 14:44:51 +0900

    fix: 커스텀 구분자가 특수문자가 아니면 다른 예외가 발생 커스텀 구분자로 특수문자가 아닌 문자가 들어오면 정규표현식에서 예외를 발생한다. 모든 잘못된 입력은 IllegalArgumentException으로 처리해야된다. 커스텀 구분자가 특수문자인지 검증하도록 하여 아닌 경우 IllegalArgumentException으로 처리한다.

--- src/main/java/calculator/number/CustomPatternMaker.java
+++ src/main/java/calculator/number/CustomPatternMaker.java
@@ -10,10 +10,18 @@ public class CustomPatternMaker {
         String customSeparator = input.substring(input.indexOf("//") + 2, input.indexOf("\\n"));
         // 커스텀 구분자가 문자로 되어있고 여러 개 사용할 수 있으므로 분리시켜준다.
         String[] customPatterns = customSeparator.split("");
+        // 특수문자로 인식하기 위해 이스케이프를 추가해준다.
         List<String> customPatternList = new ArrayList<>();
         for (String customPattern : customPatterns) {
+            isSpecialCharacter(customPattern);
             customPatternList.add("\\" + customPattern);
         }
         return customPatternList;
+    }
+
+    private static void isSpecialCharacter(String pattern) {
+        if (pattern.matches("[\\w]")) {
+            throw new IllegalArgumentException("커스텀 구분자는 특수문자만 가능합니다.");
+        }
     }
 }
