33c3977a527ee0ee12ec49c3608dd30264c12ce6
joonwan <joonwan1024@naver.com>
2024-10-18 15:21:33 +0900

    feat: ParseManager 생성

---
+++ src/main/java/calculator/parser/CustomDataParser.java
@@ -0,0 +1,4 @@
+package calculator.parser;
+
+public class CustomDataParser im{
+}
---
+++ src/main/java/calculator/parser/ParseManager.java
@@ -0,0 +1,11 @@
+package calculator.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParseManager {
+
+    private final List<DataParser> parsers = new ArrayList<>();
+
+
+}
