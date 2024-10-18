9eeafb9b327dba59916a27ca34f6b7a4c0215d41
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-10-18 11:14:26 +0900

    chore: 전반적인 프로그램 구조 설계
    - 프로그램을 구현하기 전에 기본적으로 필요한 전체적인 구조를 설계했습니다.

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    private List<String> division = new ArrayList<>();
+    private List<Integer> numbers = new ArrayList<>();
+
+    public List<String> getDivision(/*입력 받은 문자열*/) {
+        return null; //구분자 리스트 반환
+    }
+
+    public List<Integer> getNumbers(/*입력 받은 문자열*/) {
+        return null; //숫자 리스트 반환
+    }
+
+    public int calculator() {
+        return 0; // 계산 결과값 반환
+    }
+}
---
+++ src/main/java/calculator/CalculatorIO.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class CalculatorIO {
+    public String input() {
+        return null; // 입력 받은 문자열 반환
+    }
+
+    public String output() {
+        return null; // 계산한 결과값 출력
+    }
+}
