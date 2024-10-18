d3e372d26823405af239a5de06b03b1c2147079f
DongHoon Lee <dhl1924@naver.com>
2024-10-18 17:07:34 +0900

    style(OutputView): 중복되는 개행 삭제

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,7 +2,7 @@ package calculator.view;
 
 public class OutputView {
 
-    private static final String ADDITION_PROMPT = "덧셈할 문자열을 입력해 주세요.\n";
+    private static final String ADDITION_PROMPT = "덧셈할 문자열을 입력해 주세요.";
 
     private OutputView() {
     }
