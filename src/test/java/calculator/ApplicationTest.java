18ba8d3d8153fafd37009a01e8c65879549d405d
rladmstn <oz33177351@gmail.com>
2024-10-18 21:04:18 +0900

    test(application) : input 케이스 별 통합 테스트 작성
    - 기본 구분자 사용하는 input에 대해 성공 케이스 테스트 작성
    - 커스텀 구분자 사용하는 input에 대해 성공 케이스 테스트 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -2,42 +2,49 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    @Test
-    @DisplayName("커스텀 구분자 사용 : 성공")
-    void 커스텀_구분자_사용() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1:2,3 | 결과 : 6",
+            "::: | 결과 : 0",
+            "1   : 2 ,   3 | 결과 : 6",
+            " '' | 결과 : 0",
+            ",,: | 결과 : 0",
+            "100,100: | 결과 : 200",
+            "1,3,2 | 결과 : 6"},
+            delimiter = '|'
+    )
+    @DisplayName("기본 구분자 사용 성공")
+    void normalDelimiterCase(String input, String expected) {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run(input);
+            assertThat(output()).contains(expected);
         });
     }
-
     @ParameterizedTest
-    @ValueSource( strings = {
-            "1=3:2",
-            "A,2:1",
-            "+1,1:1",
-            "-1:1,1",
-            "/a\n1a3a2",
-            "//a\n10a3:2",
-            "//a\n10a3,2",
-            "//\n132",
-            "//&\n1|3&2"
-    })
-    @DisplayName("잘못된 input 입력 시, 예외 발생")
-    void inputException(String input) {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException(input))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+    @CsvSource(value = {
+            "//a\\n10a3a2 | 결과 : 15",
+            "//abc\\n1abc3 | 결과 : 4",
+            "//a\\n1a2a3 | 결과 : 6",
+            "// \\n1 2 3 | 결과 : 6",
+            "//a\\na10a20a30a | 결과 : 60",
+            "// \\n  1  3 2 | 결과 : 6",
+            "//ab\\nab100ab10ab1ab | 결과 : 111",
+            "//a-\\na-100a- | 결과 : 100"},
+            delimiter = '|'
+    )
+    @DisplayName("커스텀 구분자 사용 성공")
+    void customDelimiterCase(String input, String expected) {
+        assertSimpleTest(() -> {
+            run(input);
+            assertThat(output()).contains(expected);
+        });
     }
 
     @Override
