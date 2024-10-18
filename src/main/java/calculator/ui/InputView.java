13f38d9a8a4c7fd214f151dd1633ff681ff2b874
Kim5eongkwang <ksg6736@jbnu.ac.kr>
2024-10-19 02:43:30 +0900

    feat: 덧셈할 문자열을 입력

---
+++ src/main/java/calculator/ui/InputView.java
@@ -0,0 +1,10 @@
+package calculator.ui;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    public String readLetters() {
+        return readLine();
+    }
+}
