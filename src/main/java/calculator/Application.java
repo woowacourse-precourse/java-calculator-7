a20df956176ccd4290badf63e80c6ce339510b59
junhyuk1248 <poa124@nate.com>
2024-10-18 17:52:03 +0900

    feat(Application): 예외 처리 기능 추가
    -커스텀 구분자가 숫자 일 경우, 구분자와 숫자를 제외한 문자가 포함된 경우

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -19,14 +19,21 @@ public class Application {
     }
 
     private static String[] delimiter(String input) {
-        if (input.isEmpty()){
+        String default_Delimiters = ",|:";
+        if (input.isEmpty()) {
             return null;
         }
-        String default_Delimiters = ",|:";
-        if (input.startsWith("//") && input.contains("\\n")){
+        if (input.startsWith("//") && input.contains("\\n")) {
             int endIndex = input.indexOf("\\n");
             String number = input.substring(endIndex + 2);
             String custom_Delimiters = input.substring(2, endIndex);
+
+            if ( custom_Delimiters.matches("[+-]?\\d*(\\.\\d+)?")) {
+                throw new IllegalArgumentException();
+            }
+            if (number.isEmpty()) {
+                return null;
+            }
             return number.split(custom_Delimiters);
         }
         return input.split(default_Delimiters);
@@ -35,15 +42,22 @@ public class Application {
     private static int calulator(String[] numbers) {
         List<Integer> listNumber = new ArrayList<>();
         int sum = 0;
-        if (numbers == null){
+        if ( numbers == null ){
             return sum;
         }
         for (String number : numbers){
-            listNumber.add(Integer.parseInt(number));
+            listNumber.add(parseInt(number));
         }
         sum = listNumber.stream().mapToInt(i -> i).sum();
 
         return sum;
     }
 
+    private static int parseInt(String numbers){
+        try {
+            return Integer.parseInt(numbers);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
 }
