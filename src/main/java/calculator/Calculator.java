6b2777d5c3e6b373d645745fe4abd00c1331ed2c
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-18 16:03:30 +0900

    feat(Calculator): 결과값 계산 기능

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,21 @@
 package calculator;
 
+import java.util.List;
+
 public class Calculator {
+
+    public int calculate(List<String> values) {
+        int sum = 0;
+
+        for (String value : values) {
+            int num = Integer.parseInt(value);
+            if (num <= 0) {
+                throw new IllegalArgumentException("0 또는 음수는 입력값에 포함될 수 없습니다.");
+            }
+            sum += Integer.parseInt(value);
+        }
+
+        return sum;
+    }
+
 }
