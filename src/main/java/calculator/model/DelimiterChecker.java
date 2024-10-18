1c396bef4fc8f8b75e71d57ddb26823c1fe15ad0
koseonje <kosunje1344@naver.com>
2024-10-19 01:29:53 +0900

    refactor : 매직넘버 상수 처리

--- src/main/java/calculator/model/DelimiterChecker.java
+++ src/main/java/calculator/model/DelimiterChecker.java
@@ -4,11 +4,12 @@ public class DelimiterChecker {
 
     private static final String DELIMITER_CREATOR_FRONT = "//";
     private static final String DELIMITER_CREATOR_BACK = "\\n";
+    private static final int CUSTOM_DELIMITER_NOT_FOUND = -1;
 
     public boolean existCustomDelimiter(String value) {
         int frontIndex = value.indexOf(DELIMITER_CREATOR_FRONT);
         int backIndex = value.indexOf(DELIMITER_CREATOR_BACK);
 
-        return frontIndex != -1 && backIndex != -1 && frontIndex < backIndex;
+        return frontIndex != CUSTOM_DELIMITER_NOT_FOUND && backIndex != CUSTOM_DELIMITER_NOT_FOUND && frontIndex < backIndex;
     }
 }
