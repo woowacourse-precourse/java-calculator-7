c4f746785d01e59fef003dbd6f52538510f9f935
YoungGaLee <rkduddl1717@gmail.com>
2024-10-19 06:01:01 +0900

    feat: 문자열 입력
    '덧셈할 문자열을 입력해주세요.'라는 문구와 함께 문자열 입력을 받는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Console.close;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO:  문자열 입력받기
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String inputString = readLine();
+
+        // TODO:  기본 구분자로 분리, 덧셈
+        // TODO:  커스텀 구분자
+        // TODO:  예외처리
     }
 }
