1ad941a10bea359d5e4ab835f6e0eb0db8efc6e3
Jungwoo Chu <cjw980930@gmail.com>
2024-10-18 16:18:06 +0900

    test(추가): 추가로 고려해야 할 커스텀 구분자 테스트 작성

---
+++ src/test/java/calculator/ApplicationTest2.java
@@ -0,0 +1,47 @@
+package calculator;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest2 extends NsTest {
+    @Test
+    void 문자_1개일_경우() {
+        assertSimpleTest(() -> {
+            run("//;\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 문자_여러개일_경우() {
+        assertSimpleTest(() -> {
+            run("//;#\\n1;#1,2");
+            assertThat(output()).contains("결과 : 4");
+        });
+    }
+
+    @Test
+    void 같은_문자_여러개일_경우() {
+        assertSimpleTest(() -> {
+            run("//;;\\n1;;2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 개행문자_일_경우() {
+        assertSimpleTest(() -> {
+            run("//\\n\\n1\\n2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
