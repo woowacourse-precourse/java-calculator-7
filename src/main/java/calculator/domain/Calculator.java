adda759547f1a9605755be44880c9446807e8fc1
donghyunn1 <dongdong0516@naver.com>
2024-10-19 01:27:59 +0900

    feat : 음수가 들어오면 예외처리

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,4 +1,14 @@
 package calculator.domain;
 
+import java.util.List;
+
 public class Calculator {
+
+    private static final String NEGATIVE_NUMBER_ERROR = "[ERROR] 양수만 입력 가능합니다.";
+
+    private void validatePositive(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER_ERROR);
+        }
+    }
 }
