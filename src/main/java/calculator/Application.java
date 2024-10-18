4535a0d71c58d4411618d17c853f316d8353ba18
seongeun308 <kse011010@gmail.com>
2024-10-19 02:59:47 +0900

    feat: 출력 기능 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        int addResult = StringCalculator.add(input);
+        System.out.println("결과 : " + addResult);
     }
 }
