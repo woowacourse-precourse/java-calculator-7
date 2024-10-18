4109710117ca63a31344c3568ef4364ea69f5bfa
CHISANW <keuye06380618@gmail.com>
2024-10-18 16:25:41 +0900

    feat(CalculatorView): 계산기 입력 및 결과 출력 기능 구현
    - 덧셈할 문자열을 입력받는 input() 메서드 구현
    - 계산 결과를 출력하는 resultOutput() 메서드 구현

--- src/main/java/calculator/viiew/CalculatorView.java
+++ src/main/java/calculator/viiew/CalculatorView.java
@@ -1,6 +1,23 @@
 package calculator.viiew;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class CalculatorView {
 
-   
+    /**
+     * 계산할 문자열을 작성합니다.
+     *
+     * @return 작성한 문자열
+     */
+    public String input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    /**
+     * 계산기의 입력한 숫자를 더한 최종 값을 출력합니다.
+     */
+    public void resultOutput(int result) {
+        System.out.println("결과 : " + result);
+    }
 }
