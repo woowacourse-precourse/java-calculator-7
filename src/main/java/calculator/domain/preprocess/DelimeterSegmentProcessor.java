43ce73a0cc0b4b9a2f9399b8e4ab4b31508198b5
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-18 17:29:23 +0900

    style(DelimeterSegmentProcessor): validateCustomDelimeterRequest 로직에 대한 설명 주석 추가

--- src/main/java/calculator/domain/preprocess/DelimeterSegmentProcessor.java
+++ src/main/java/calculator/domain/preprocess/DelimeterSegmentProcessor.java
@@ -16,12 +16,13 @@ public class DelimeterSegmentProcessor {
             return false;
     }
 
+    //구분자가 두 글자 이상일 경우 가령 ",," 와 같은 것을 구분자로 받는 경우 기존의 구분자와 충돌이 발생할 수 있다고 판단하여 제한.
     private void validateCustomDelimeterRequest(String input) {
         if (input.indexOf("\n") == 2)
             throw new IllegalArgumentException("구분자로 사용할 문자 미입력");
         if (input.indexOf("\n") > 3)
             throw new IllegalArgumentException("구분자로 사용할 문자가 두 글자 이상");
-        if(isInteger(Character.toString(input.charAt(2))))
+        if (isInteger(Character.toString(input.charAt(2))))
             throw new IllegalArgumentException("구분자로 숫자를 사용할 수 없습니다");
     }
 
