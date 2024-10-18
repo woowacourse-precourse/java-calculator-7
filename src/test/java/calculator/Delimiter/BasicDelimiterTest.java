cfcb2a036ca6c31484fcf0376745d2443fe241c2
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-19 04:00:46 +0900

    test: 기본 구분자 처리 예외 테스트 구현

--- src/test/java/calculator/Delimiter/BasicDelimiterTest.java
+++ src/test/java/calculator/Delimiter/BasicDelimiterTest.java
@@ -1,7 +1,9 @@
 package calculator.Delimiter;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import Delimiter.BasicDelimiter;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BasicDelimiterTest {
@@ -14,7 +16,24 @@ public class BasicDelimiterTest {
         //when
         int[] numbers = BasicDelimiter.getNumber(example);
         //then
-        Assertions.assertThat(numbers).isEqualTo(result);
+        assertThat(numbers).isEqualTo(result);
     }
-    
+
+    @Test
+    void 기본_구분자_처리_예외_음수() {
+        //given
+        String example = "1,2,-3:4";
+        //when, then
+        assertThatThrownBy(() -> BasicDelimiter.getNumber(example))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 기본_구분자_처리_예외_연속된_구분자() {
+        //given
+        String example = "1,2,,3:4";
+        //when, then
+        assertThatThrownBy(() -> BasicDelimiter.getNumber(example))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
