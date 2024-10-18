fedfa812f9ddeb44023c223ad44ca3ca88aa12ec
EHCTH <lees90896@gmail.com>
2024-10-18 15:40:36 +0900

    refactor(CalculatorControllerFactory): SRP 원칙에 따른 기능 분리 * refactor: Number, Separator create extract 기능 분리

--- src/main/java/calculator/controller/service/CalculatorControllerFactory.java
+++ src/main/java/calculator/controller/service/CalculatorControllerFactory.java
@@ -1,11 +1,13 @@
 package calculator.controller.service;
 
 import calculator.controller.CalculatorController;
-import calculator.number.service.PositiveNumberGenerator;
-import calculator.service.generator.CreateProcess;
-import calculator.service.generator.ExtractProcess;
-import calculator.service.calculator.CalculatorProcess;
-import calculator.separator.service.SeparatorGenerator;
+import calculator.generator.controller.GenerateController;
+import calculator.generator.number.NumberGenerator;
+import calculator.generator.number.impl.NumberCreateProcess;
+import calculator.generator.number.impl.NumberExtractProcess;
+import calculator.generator.separator.impl.SeparatorCreateProcess;
+import calculator.generator.separator.impl.SeparatorExtractProcess;
+import calculator.generator.separator.SeparatorGenerator;
 import calculator.view.input.handler.InputHandlerService;
 import calculator.view.input.handler.NumberInputHandler;
 import calculator.view.input.service.InputView;
@@ -16,11 +18,11 @@ public class CalculatorControllerFactory {
     public static CalculatorController create() {
         InputHandlerService inputHandlerService = new NumberInputHandler(new InputView(new OutputProcess()));
         ResultService resultService = new OutputProcess();
-        SeparatorGenerator separatorGenerator = new SeparatorGenerator(new ExtractProcess(), new CreateProcess());
-        PositiveNumberGenerator numberGenerator = new PositiveNumberGenerator(new ExtractProcess(), new CreateProcess());
+        SeparatorGenerator separatorGenerator = new SeparatorGenerator(new SeparatorExtractProcess(), new SeparatorCreateProcess());
+        NumberGenerator numberGenerator = new NumberGenerator(new NumberExtractProcess(), new NumberCreateProcess());
+        GenerateController generateController = new GenerateController(separatorGenerator, numberGenerator);
         return new CalculatorController(
-                separatorGenerator,
-                numberGenerator,
+                generateController,
                 inputHandlerService,
                 resultService);
     }
