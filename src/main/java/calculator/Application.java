99e99d03b415cb93faf53c262deff46cc1519b5d
Sooyong Yun <sooyongyun@Sooyongui-MacBookAir.local>
2024-10-18 15:50:09 +0900

    feat: 기능1 쉼표 콜론을 구분자로 분리한 숫자의 합

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    // 기능1. 쉼표 또는 콜론으로 분리한 각 숫자의 합을 반환
+    public static int basic_add(String string_to_calc){
+        int answer = 0;
+
+        String[] numbers = string_to_calc.split("[: ,]");
+        for (String number : numbers){
+            answer += Integer.parseInt(number);
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String string_to_calc = Console.readLine();
+        System.out.println(basic_add(string_to_calc));
     }
 }
