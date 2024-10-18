197c167534d7f155cb11a5d1cb1d12e855813386
minjun <ijmtylhtf@naver.com>
2024-10-18 09:59:11 +0900

    feat : 쉼표나 콜론 구분자가 존재할 경우 합 계산 및 결과 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,5 +8,12 @@ public class Application {
     public static void main(String[] args) {
         String input = Console.readLine();
         String[] splitString = input.split(ORIGINAL_SPLITTER_REGEX);
+        int sum = 0;
+        if(splitString.length > 1){
+            for (String s : splitString) {
+                sum += Integer.parseInt(s);
+            }
+        }
+        System.out.println("결과 : " + sum);
     }
 }
