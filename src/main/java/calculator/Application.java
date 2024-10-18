8fadc7c3ec04d3c02beebadb8d64e80ab300f80b
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-18 22:57:25 +0900

    refactor: calculator클래스의 메서드를 활용하여 호출문을 짧게 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,9 +3,6 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String UserInput = CalStart.InputMessage();
-        int sum = InputValidate.calculateSum(UserInput);
-
-        System.out.println("결과: " + sum);
+        Calculator.run();
     }
 }
