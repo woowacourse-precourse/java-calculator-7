4f39d9947121a2feffd36aab2dc95feeef0b257b
Jiihyun <cwlgusc@gmail.com>
2024-10-18 22:00:25 +0900

    test: 문자열에서 숫자를 추출하여 각 숫자의 합 반환하는 Calculator 테스트 작성

---
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -0,0 +1,76 @@
+package calculator.domain;
+
+import calculator.validator.CalculatorValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorTest {
+
+    @DisplayName("사용자가 입력한 문자열이 비어있으면 0을 반환한다.")
+    @Test
+    void returnZeroWhenUserPutEmptyValue() {
+        //given
+        String stringToAdd = "";
+        Calculator calculator = createCalculator();
+        //when
+        int sum = calculator.getSum(stringToAdd);
+        //then
+        assertThat(sum).isEqualTo(0);
+    }
+
+    @DisplayName("사용자가 입력한 커스텀구분자와 기존 구분자들로 숫자 합을 구할 수 있다.")
+    @Test
+    void getSumWithCustomAndOriginalDelimiters() {
+        //given
+        String stringToAdd = "//;\\n1;2:3,4,5";
+        Calculator calculator = createCalculator();
+        //when
+        int sum = calculator.getSum(stringToAdd);
+        //then
+        assertThat(sum).isEqualTo(15);
+    }
+
+    @DisplayName("기존 구분자들로 사용자가 입력한 문자열의 숫자 합을 구할 수 있다.")
+    @Test
+    void getSumWithOriginalDelimiters() {
+        //given
+        String stringToAdd = "10,2:8";
+        Calculator calculator = createCalculator();
+        //when
+        int sum = calculator.getSum(stringToAdd);
+        //then
+        assertThat(sum).isEqualTo(20);
+    }
+
+    @DisplayName("사용자가 입력한 커스텀구분자로 숫자 합을 구할 수 있다.")
+    @Test
+    void getSumWithCustomDelimiter() {
+        //given
+        String stringToAdd = "//d\\n1d2d3d4d";
+        Calculator calculator = createCalculator();
+        //when
+        int sum = calculator.getSum(stringToAdd);
+        //then
+        assertThat(sum).isEqualTo(10);
+    }
+
+    @DisplayName("사용자가 입력한 커스텀구분자로 숫자 합을 구할 수 있다.")
+    @Test
+    void getSumWithCustomDelimiter2() {
+        //given
+        String stringToAdd = "//구분\\n1구분2구분3구분4";
+        Calculator calculator = createCalculator();
+        //when
+        int sum = calculator.getSum(stringToAdd);
+        //then
+        assertThat(sum).isEqualTo(10);
+    }
+
+    private Calculator createCalculator() {
+        CalculatorValidator calculatorValidator = new CalculatorValidator();
+        NumberStringExtractor numberStringExtractor = new NumberStringExtractor(new CustomDelimiterExtractor());
+        return new Calculator(calculatorValidator, numberStringExtractor);
+    }
+}
