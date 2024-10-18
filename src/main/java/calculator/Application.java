8cb59ff20dcb486a75330bd069b172fa057c7de4
jwh946 <jwh946@gmail.com>
2024-10-18 14:46:56 +0900

    기본 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,34 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+        int sum = 0;
+        Set<Character> set = new HashSet<>();
+        set.add(':');
+        set.add(',');
+        StringBuilder tmp = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isDigit(c)) {
+                tmp.append(c);
+            }
+            else if (set.contains(c)) {
+                if (tmp.length() > 0) {
+                    sum += Integer.parseInt(tmp.toString());
+                    tmp.setLength(0);
+                }
+            }
+        }
+        if (tmp.length() > 0) {
+            sum += Integer.parseInt(tmp.toString());
+        }
+        System.out.println("결과 : " + sum);
     }
 }
