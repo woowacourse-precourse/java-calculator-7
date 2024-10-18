18584aab5a93aa45946e51e94f678f9a635f493d
taek2222 <oot309@daum.net>
2024-10-18 15:13:07 +0900

    refactor(CalculatorService): 검증 역할 분리 작업 진행
    메서드의 중간 마다 검증 로직의 불필요하다고 생각해, 이를 전체적으로 처리하는 Process 로 옮겼습니다.

--- src/main/java/calculator/CalculatorService.java
+++ src/main/java/calculator/CalculatorService.java
@@ -3,25 +3,13 @@ package calculator;
 import java.util.Arrays;
 
 public class CalculatorService {
-    private final NumberValidator numberValidator;
-
-    public CalculatorService() {
-        this.numberValidator = new NumberValidator();
-    }
 
     public char[] removeDelimiterAndToCharArray(String positiveString, String customDelimiter) {
         String positives = removeDelimiter(positiveString, customDelimiter);
         return positives.toCharArray();
     }
 
-    private String removeDelimiter(String positiveString, String customDelimiter) {
-        positiveString = positiveString.replaceAll("[:|,]", "");
-        return positiveString.replace(customDelimiter, "");
-    }
-
     public int[] convertCharArrayToIntArray(char[] positiveCharArray) {
-        numberValidator.validateNoDigits(positiveCharArray);
-
         return new String(positiveCharArray)
                 .chars()
                 .map(Character::getNumericValue)
@@ -29,9 +17,12 @@ public class CalculatorService {
     }
 
     public int calculatorNumbers(int[] numbers) {
-        numberValidator.validatorNoNegatives(numbers);
-
         return Arrays.stream(numbers)
                 .sum();
+    }
+
+    private String removeDelimiter(String positiveString, String customDelimiter) {
+        positiveString = positiveString.replaceAll("[:|,]", "");
+        return positiveString.replace(customDelimiter, "");
     }
 }
