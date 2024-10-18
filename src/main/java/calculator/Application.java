1a9d3f41e9c41c65ab9032e83eccfac65b17a0ad
CheChe903 <cheche903@naver.com>
2024-10-18 18:31:29 +0900

    refactor: 문자 -> 숫자 변환 로직 클래스로 리팩터링 진행
    구분자와 마찬가지로, 문자를 숫자로 바꾸어주는 Converter를 객체로 만듭니다.
    해당과 관련된 메서드를 모두 리팩터링 진행합니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     private final Delimiters delimiters = new Delimiters();
+    private final Converter converter = new Converter();
 
     public static void main(String[] args) {
         Application application = new Application();
@@ -15,7 +16,7 @@ public class Application {
 
         String inputString = Console.readLine();
 
-        validateInputString(inputString);
+        converter.validateInputString(inputString);
 
         String numberString = delimiters.extractNumberString(inputString);
 
@@ -34,36 +35,11 @@ public class Application {
 
             String trimToken = token.trim();
 
-            sum += convertToValidInteger(trimToken);
+            sum += converter.convertToValidInteger(trimToken);
 
         }
 
         System.out.println("결과 : " + sum);
-    }
-
-    private void validateInputString(String inputString) {
-        if (inputString == null || inputString.trim().isEmpty()) {
-            throw new IllegalArgumentException("입력값이 공백이거나 빈 문자열입니다.");
-        }
-    }
-
-    public int convertToValidInteger(String token) {
-        try {
-            String trimToken = (token.trim());
-            int num = Integer.parseInt(trimToken);
-
-            validateNonNegative(num);
-            return num;
-
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 입력되지 않았습니다.");
-        }
-    }
-
-    private void validateNonNegative(int num) {
-        if (num < 0) {
-            throw new IllegalArgumentException("음수는 허용되지 않습니다.");
-        }
     }
 
 
