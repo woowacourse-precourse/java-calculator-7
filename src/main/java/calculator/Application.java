e1902f806349655b612b656b68fdb30abf295e1a
yunhacandy <yunhacandy@gmail.com>
2024-10-18 18:19:49 +0900

    feat (Application): 기본 구분자와 숫자로만 이루어진 문자열에 대한 계산기 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,30 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        int result = add(input);
+        System.out.println("결과 : " + result);
+    }
+
+    public static int add(String input) {
+
+        String seperator = ",|:"; // 기본 구분자: 쉼표와 콜론
+
+        // 구분자를 기준으로 숫자를 분리
+        String[] numbers = input.split(seperator);
+
+        // 각 숫자에 대해 검증 및 덧셈 처리
+        int sum = 0;
+        for (String number : numbers) {
+            if (!number.trim().isEmpty()) {  // 빈 문자열은 무시
+                sum += Integer.parseInt(number.trim());  // 숫자 변환 후 덧셈
+            }
+        }
+
+        return sum;
     }
 }
