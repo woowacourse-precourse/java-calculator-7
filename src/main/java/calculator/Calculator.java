6f69785e7b5c2e69b8bffbf1b5b4e94f3b9ef760
sanghun <vitash1215@gmail.com>
2024-10-19 05:21:02 +0900

    feat(split) : custom 구분자 처리
    문자열 앞에 //과 \n 사이에 문자를 넣으면 해당 문자를 커스텀 구분자로 사용하여 ,와 :처럼 사용할 수 있게 함

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,13 +1,26 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
+    private static final String DEFAULT_DELIMITER = ",|:";
+
     public static int add(String text) {
+
         if (text == null || text.isEmpty()) {
             return 0;
         }
+        return sum(split(text));
+    }
 
-        String[] values = text.split(",|:");
-        return sum(values);
+    private static String[] split(String text) {
+        Matcher matcher = Pattern.compile("//(.)\\\\n(.*)").matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(DEFAULT_DELIMITER + "|" + customDelimiter);
+        }
+        return text.split(DEFAULT_DELIMITER);
     }
 
     private static int sum(String[] values) {
