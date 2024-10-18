96bfee4878506bd372f0943958fe6c6394698b45
kcs19 <changsung19@gmail.com>
2024-10-19 01:19:53 +0900

    feat: 문자열 분리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,7 +9,20 @@ public class Application {
         return Console.readLine();
     }
 
+    public static String[] separateString(String line){
+        if(line.startsWith("//")){
+            String[] parts = line.split("\\\\n"); //\n으로 구분
+
+            String separator = parts[0].replace("//","");
+            separator += ",:"; //기본 문자열 추가
+
+            return new String[]{separator, parts[1]};
+        }
+        return new String[]{",:", line};
+    }
+
     public static void main(String[] args){
         String line= inputLine();
+        String[] separatorAndOperation = separateString(line);
     }
 }
