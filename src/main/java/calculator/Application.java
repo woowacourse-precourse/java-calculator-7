bcc357d644f8061ae3608a47ff2768cb19a12479
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-19 07:31:41 +0900

    refactor: 숫자 계산기 컨트롤러를 사용하도록 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,16 +2,15 @@ package calculator;
 
 import calculator.common.config.IntegerCalculatorDependencyRegistry;
 import calculator.common.config.NumberCalculatorDependencyRegistry;
-import calculator.controller.CalculatorController;
+import calculator.controller.NumberCalculatorController;
 
 public class Application {
-
 
     public static void main(String[] args) {
         NumberCalculatorDependencyRegistry numberCalculatorDependencyRegistry =
                 IntegerCalculatorDependencyRegistry.getInstance();
 
-        CalculatorController calculatorController = new CalculatorController(numberCalculatorDependencyRegistry);
-        calculatorController.run();
+        NumberCalculatorController numberCalculatorController = new NumberCalculatorController(numberCalculatorDependencyRegistry);
+        numberCalculatorController.run();
     }
 }
