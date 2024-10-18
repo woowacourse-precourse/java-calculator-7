b48444920c245140e140ffe559b8ce8388b346a4
wlsdlf2 <wlsdlfzg@naver.com>
2024-10-18 21:08:54 +0900

    feat: 덧셈 결과를 출력하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,8 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        StringCalculator calculator = new StringCalculator();
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        int result = calculator.calculate(input);
+        System.out.println("결과 : " + result);
     }
 }
