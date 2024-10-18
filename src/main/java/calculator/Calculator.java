8049b489e5417e00a502c39eb189d47e7f0e7f86
Leejunho <junho767@naver.com>
2024-10-18 19:15:06 +0900

    feat : 커스텀 구분자 유무 검사 기능

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -15,10 +15,21 @@ public class Calculator {
             System.out.println("결과 : 0");
             return;
         }
+
+        if(splitByDelimiters(str)){ // 커스텀 구분자 유무 검사
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 
     // 입력한 문자열이 비었는지 확인 -> 비었으면 0 출력
     public static boolean checkEmpty(String str) {
         return str == null || str.isEmpty();
+    }
+
+    // 입력된 문자열에서 구분자로 숫자 분리
+    public static boolean splitByDelimiters(String str) {
+        return str.startsWith("//");
     }
 }
