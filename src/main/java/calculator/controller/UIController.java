ce256edaa722dd451599c3a13a29832eedd046dd
imjunghun <dlawndgns9@naver.com>
2024-10-18 17:47:31 +0900

    feat(UIController): 더하기 결과 출력 기능 구현

--- src/main/java/calculator/controller/UIController.java
+++ src/main/java/calculator/controller/UIController.java
@@ -5,9 +5,18 @@ import calculator.view.OutputView;
 
 public class UIController {
     private static final String SUM_STRING_INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String RESULT_FORMAT = "결과 : ";
 
     public static String receive() {
         OutputView.print(SUM_STRING_INPUT_MESSAGE);
         return InputView.readValue();
+    }
+
+    public static void printSumResult(int number) {
+        OutputView.print(fitFormat(number));
+    }
+
+    private static String fitFormat(int number) {
+        return RESULT_FORMAT + number;
     }
 }
