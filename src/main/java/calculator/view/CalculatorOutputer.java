655069efce1505fc38849c21199b8bc68ce31a18
FhRh <yiyop@naver.com>
2024-10-18 10:31:41 +0900

    feat(CalculatorOutputer): 계산된 결과를 출력합니다.

--- src/main/java/calculator/view/CalculatorOutputer.java
+++ src/main/java/calculator/view/CalculatorOutputer.java
@@ -5,4 +5,8 @@ public class CalculatorOutputer {
     public void printIntro() {
         System.out.println("덧셈할 문자열을 입력해주세요.");
     }
+
+    public void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
 }
