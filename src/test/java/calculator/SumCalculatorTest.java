c44fd95ee14e7e433d25bef36e6dea5c9629b2b4
GaBaljaintheroom <junsu1222@naver.com>
2024-10-18 23:22:47 +0900

    test(SumCalculatorTest): 숫자 리스트의 합을 구하는 테스트 작성

---
+++ src/test/java/calculator/SumCalculatorTest.java
@@ -0,0 +1,25 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class SumCalculatorTest {
+
+    private final Calculator calculator = new SumCalculator();
+
+    @Test
+    void 숫자_리스트의_합을_구한다() {
+        //given
+        List<Integer> numbers = List.of(123, 23, 1);
+
+        //when
+        long actual = calculator.sum(numbers);
+
+        //then
+        long expect = 147;
+        assertThat(actual).isEqualTo(expect);
+    }
+
+}
