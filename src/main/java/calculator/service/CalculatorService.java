b932ebd40f567639ee5e58f2a9f10720f5f06085
DEV_WOONG <ywcsuwon@gmail.com>
2024-10-18 14:46:55 +0900

    feat(calculator): add error-handling for negative numbers and non-numeric input
    - Throws IllegalArgumentException when negative numbers are encountered.
    - Throws IllegalArgumentException for any non-numeric input strings.

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -27,11 +27,26 @@ public class CalculatorService {
             }
         }
 
-        String[] numbers = input.split(delimiter);
+        String[] numbers = input.split(delimiter); //입력값을 구분자로 나누어 String형 배열에 저장
         return sum(numbers);
     }
 
 
     private int sum(String[] numbers) {
+        int total = 0;
+        for (String number : numbers) {
+            number = number.trim(); //양쪽 공백제거
+            try {
+                int num = Integer.parseInt(number);
+                if (num < 0) { //숫자가 음수라면
+                    throw new IllegalArgumentException();
+                }
+                total += num;
+            } catch (NumberFormatException nfe) { //숫자가 아닌 문자열을 정수형으로 바꾸면 예외가 발생한다.
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return total;
     }
 }
