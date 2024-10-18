ade1dc0eb71ba3d25ba5d22f7e7a991abf476315
seoys0910 <seoys0910@naver.com>
2024-10-18 23:00:41 +0900

    feat : 덧셈된 결과를 출력하는 기능 구현

--- src/main/java/calculator/OutputView.java
+++ src/main/java/calculator/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void printStartMessage() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
+
+    public static void printSumResult(int sum) {
+        System.out.println("결과 : " + sum);
+    }
 }
