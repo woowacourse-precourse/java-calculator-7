22043a2b60ca86e3c30441da4bea3f34e47018db
hyeondo <eun041212@gmail.com>
2024-10-18 17:46:56 +0900

    refactor(DelimiterExtractor) : 추출한 구분자를 기본 구분자와 함께 사용하도록 변경

---
+++ src/main/java/calculator/model/DelimiterExtractor.java
@@ -0,0 +1,31 @@
+package calculator.model;
+
+public class DelimiterExtractor {
+
+    private String delimiter = "[,:]";
+    private int contentStartIndex;
+    private static final int DELIMITER_START = 2;
+
+    public void addCustomDelimiter(String input) {
+        String customDelimiter = extractCustomDelimiter(input);
+        delimiter += "|" + customDelimiter;
+    }
+
+    public String extractCustomDelimiter(String input) {
+        int delimiterEnd = input.indexOf("\\n");
+        setContentStartIndex(delimiterEnd);
+        return input.substring(DELIMITER_START, delimiterEnd);
+    }
+
+    private void setContentStartIndex(int delimiterEnd) {
+        contentStartIndex = delimiterEnd + 2;
+    }
+
+    public String getDelimiter() {
+        return delimiter;
+    }
+
+    public int getContentStartIndex() {
+        return contentStartIndex;
+    }
+}
