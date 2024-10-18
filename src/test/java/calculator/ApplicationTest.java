33234662d29fd4da3ebf2124b6fe7bb1c7223771
acupoframen <choe.hwan@gmail.com>
2024-10-18 14:45:14 +0900

    test(Application): more than two custom dividers
    create custom divider when given more than two characters
    vague because is without insturction

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,10 +9,42 @@ import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
+    void 기본테스트_1() {
+        assertSimpleTest(() -> {
+            run("1,2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 기본테스트_2() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 일반_구분자_혼용_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_일반_구분자_혼용_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2,3:4");
+            assertThat(output()).contains("결과 : 10");
         });
     }
 
@@ -53,6 +85,38 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() -> {
             run("");
             assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_마이너스_입력() {
+        assertSimpleTest(() -> {
+            run("//-\\n1-1");
+            assertThat(output()).contains("결과 : 2");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_마이너스_입력_예외_케이스() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//-\\n1--1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자_끝_위치_예외_케이스() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1:"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 두_문자_이상_커스텀_구분자_설정() {
+        assertSimpleTest(() -> {
+            run("//-;\\n1-2;3:4");
+            assertThat(output()).contains("결과 : 10");
         });
     }
 
