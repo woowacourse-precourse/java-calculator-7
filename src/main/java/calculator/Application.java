55818ffad3e4c46a74a214f422f037846fe9c4f1
JiHyeonSu <itip1@mju.ac.kr>
2024-10-18 21:34:21 +0900

    feat(Application): getInput 메소드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static String getInput() {
+        printMessage("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
     }
 
     private static void printMessage(String output) {
@@ -16,5 +23,4 @@ public class Application {
     private static void printResult(int result) {
         System.out.println("결과 : " + result);
     }
-
 }
