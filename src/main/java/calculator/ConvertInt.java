ffd4bfbd8c2ace3a86bb07ccde41c41e6df8429e
eunho <eunho1032@gmail.com>
2024-10-18 17:04:55 +0900

    fix(ConvertInt): parseInt 직접 구현

--- src/main/java/calculator/ConvertInt.java
+++ src/main/java/calculator/ConvertInt.java
@@ -1,15 +1,29 @@
 package calculator;
+
 public class ConvertInt {
 	public static int convertInt(String num)
 	{
-		try {
-			return Integer.parseInt(num); // 문자열을 int로 변환
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException("잘못된 입력");
+		long	result = 0;
+		int		digit = 0;
+		char	charNum = 0;
+		
+		if (num == null){
+			throw new IllegalArgumentException("잘못된 숫자 입력");
 		}
-		/*
-		 * 24.10.18
-		 * MAX_INT 를 넘는 값에서 오버플로우 발생 확인 수정 또는 직접 구현 필요해보임
-		 */
+		if (num.charAt(0) == '-' || num.charAt(0) == '-'){
+			throw new IllegalArgumentException("부호 입력");
+		}
+		for (int i = 0; i < num.length(); i++) {
+			charNum = num.charAt(i);
+			if (!Character.isDigit(charNum)){
+				throw new IllegalArgumentException("잘못된 숫자 입력 포함");
+			}
+			digit = charNum - '0';
+			if (result * 10 > Calculator.MAX_INT){
+				throw new IllegalArgumentException("INT 초과 값");
+			}
+			result = result * 10 + (digit);
+		}
+		return (int)result;
 	}
 }
