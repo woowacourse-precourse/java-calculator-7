4967c4f2ad836e3df20deed6136ab4b244b4ab65
badgelatte <saucy5939@gmail.com>
2024-10-19 06:39:19 +0900

    fix: 체크리스트 작성하며 다시 진행하기 위해 리셋
    체크리스트와 함께 진행하기 위해 리셋

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,24 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        try{
-            System.out.println("덧셈할 문자열을 입력해주세요.");
-            String input = Console.readLine();
-            String[] splitInputs = input.split("[,;]");
-            Integer result = 0;
 
-            for (String s : splitInputs) {
-                if(s.equals("")) {
-                    result += 0;
-                } else {
-                    result += Integer.parseInt(s);
-                }
-            }
-
-            System.out.println("결과 : " + result);
-        } catch (NumberFormatException e) {
-            System.out.println("잘못된 입력을 하셨습니다.");
-            System.out.println(e.getMessage());
-        }
     }
 }
