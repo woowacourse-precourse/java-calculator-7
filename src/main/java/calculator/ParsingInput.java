d19e2865751380b08c8b096d516d707c8ac2d127
Dev-LHY <dev.leehy@gmail.com>
2024-10-18 22:48:40 +0900

    feat: 문자열 배열을 정수형 배열로 변환 및 변환 값 검증 기능 구현
    문자열을 정수로 파싱하고 입력 값이 양수인지 검증. 양수일 경우 정수형 배열에 담음.

--- src/main/java/calculator/ParsingInput.java
+++ src/main/java/calculator/ParsingInput.java
@@ -9,4 +9,16 @@ public class ParsingInput {
         return input.substring(input.indexOf("\\n")+2).split(token);
     }
 
+    private static int[] parseIntArray(String[] inputArray) {
+        int[] parsedValueArray = new int[inputArray.length];
+
+        for (int i = 0; i < inputArray.length; i++) {
+            int parsedValue = Integer.parseInt(inputArray[i]);
+            if (parsedValue <= 0) {
+                throw new IllegalArgumentException("잘못된 입력입니다. 애플리케이션을 종료합니다.");
+            }
+            parsedValueArray[i] = parsedValue;
+        }
+        return parsedValueArray;
+    }
 }
