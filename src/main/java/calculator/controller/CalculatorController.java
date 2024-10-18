f66577a479c844301ae4bc8f53d8f2e8d6fee1cf
Zrfv <youngoh07015@gmail.com>
2024-10-18 11:41:40 +0900

    feat(CalculatorController): 컨트롤러에서 처리하는 기능 추가
    - CalculatorController 클래스 생성
    - README 구현한 기능 체크 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,26 @@
 package calculator.controller;
 
+import calculator.model.Delimiter;
+import calculator.model.Number;
+import calculator.view.InputView;
+
 public class CalculatorController {
+    private InputView inputView;
+
+    public CalculatorController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public int processInput() {
+        String inputString = inputView.getInputString();
+
+        Delimiter delimiter = new Delimiter(inputString);
+        delimiter.checkCustomDelimiter();
+        String[] splitNumbers = delimiter.splitString();
+
+        Number number = new Number(splitNumbers);
+        return number.addCalculator();
+    }
 }
+
+
