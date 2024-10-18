b4d5869a17ce73366264ae9b44dafa63d1c8a502
gikhoon <ghikhoon@naver.com>
2024-10-18 17:31:26 +0900

    refactor(calculator): 구분 문자 리스트를 정규화시키는 로직 변경
    -기존 "|" 로 구분 -> List.toString으로 [] 형태로 변경

--- src/main/java/calculator/CalculatorService.java
+++ src/main/java/calculator/CalculatorService.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class CalculatorService {
     private static final List<String> DEFAULT_SEPARATOR = List.of(":", ",");
-    private static final String SEPARATOR_DELIMITER = "|";
     private static final String DOUBLE_BACKSLASH_DELIMITER = "\\\\";
     private static final String BACKSLASH_DELIMITER = "\\";
 
@@ -17,13 +16,16 @@ public class CalculatorService {
 
     public void addSeparator(String newSeparator) {
         if(!separators.contains(newSeparator)) {
-            separators.add(newSeparator);
+            if (newSeparator.equals(BACKSLASH_DELIMITER)) {
+                separators.add(DOUBLE_BACKSLASH_DELIMITER);
+            } else {
+                separators.add(newSeparator);
+            }
         }
     }
 
     public int calculate(String expression) {
-        String[] splitExpression = expression.split(convertSeparatorToRegex(separators));
-
+        String[] splitExpression = expression.split(separators.toString());
         int result = 0;
         for (String number : splitExpression) {
             int numberInt = Integer.parseInt(number);
@@ -31,30 +33,6 @@ public class CalculatorService {
             result += numberInt;
         }
         return result;
-    }
-
-    //List로 되어 있는 구분 문자들을 정규식에 맞게 하나의 문자열로 변환
-    private String convertSeparatorToRegex(List<String> separators) {
-        StringBuilder regexBuilder = new StringBuilder();
-
-        for (String separator : separators) {
-            // "\"(실제론 "\\")가 구분 문자라면 문자열 \\(실제로는 "\\\\")를 넣는다
-            if (separator.equals(BACKSLASH_DELIMITER)) {
-                regexBuilder.append(DOUBLE_BACKSLASH_DELIMITER);
-            } else {
-                regexBuilder.append(separator);
-            }
-
-            regexBuilder.append(SEPARATOR_DELIMITER);
-        }
-
-        String regex = regexBuilder.toString();
-        //마지막 "|" 는 제거한다.
-        if (!regex.isEmpty()) {
-            regex = regex.substring(0, regex.length() - 1);
-        }
-
-        return regex;
     }
 
     private void checkNumberIsPositive(int numberInt) {
