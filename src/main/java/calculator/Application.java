04e0483aeab185dadd80a3e8e702b2294709e9c9
soonhong99 <nanayah99@naver.com>
2024-10-18 22:09:18 +0900

    feat(Application): 문자열 입력 기능 추가
    - bufferedReader를 이용한 사용자 입력값 추출
    - 잘못된 입력값일 경우 오류를 리턴하도록 설정
    
    Signed-off-by: soonhong99 <nanayah99@naver.com>

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 1. 문자열을 입력하는 기능
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String userInput = null;
+        try {
+            userInput = br.readLine();
+        } catch (IOException e) {
+            System.err.println("입력 오류: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("예기치 못한 오류: " + e.getMessage());
+        }
     }
 }
