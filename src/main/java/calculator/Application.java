86a536cc0bb3895f06f3c7eafda9aa0fb2c7ebe2
chino <tnehd83@naver.com>
2024-10-18 19:15:36 +0900

    feat(Application.java): 사용자 문자열 입력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String UserInput = ValueInput();
+        
+    }
+
+
+    static String ValueInput() {
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            return "";
+        }
     }
 }
