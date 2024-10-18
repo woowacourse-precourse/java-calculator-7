5c73c6d322baf4209d7c43a92b27d504c10a8a0b
yush <yush2202@gmail.com>
2024-10-18 19:46:50 +0900

    refactor($StringSplitter): 상수 제거
    상수 제거
    before:
    - userInput.replaceAll(CUSTOM_INDICATOR_REGEX, "");
    after:
    - userInput.replaceAll(CUSTOM_INDICATOR_REGEX, BLANK);

--- src/main/java/calculator/Utils/StringSplitter.java
+++ src/main/java/calculator/Utils/StringSplitter.java
@@ -9,6 +9,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StringSplitter {
+    public final static String BLANK = "";
+
     public static List<String> splitByDelimiter(String userInput, Delimiter delimiter) {
         String[] splittedInput = userInput.split(delimiter.getDelimiter());
         return Arrays.stream(splittedInput).toList();
@@ -19,6 +21,6 @@ public class StringSplitter {
     }
 
     public static String deleteIndicator(String userInput) {
-        return userInput.replaceAll(CUSTOM_INDICATOR_REGEX, "");
+        return userInput.replaceAll(CUSTOM_INDICATOR_REGEX, BLANK);
     }
 }
