8f3004fc388932d92b8e81d756ed5323b4836071
정소민 <yjk70702002@gmail.com>
2024-10-18 21:18:10 +0900

    feat: 문자열 입력받는 기능

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import calculator.io.InputHandler;
+
+import java.util.List;
+
+public class Calculator {
+    public void run() {
+        InputHandler inputHandler = new InputHandler();
+        String inputString = inputHandler.getInputString();
+    }
+}
