76a7a7fece0806d78861d3d8dbfa30a7e6a4e6d9
mhj1205 <lightnow1205@gmail.com>
2024-10-18 19:37:31 +0900

    feat: 문자열 입력 받는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        input = sc.nextLine();
+        String[] input_split = input.split("");
+
     }
 }
