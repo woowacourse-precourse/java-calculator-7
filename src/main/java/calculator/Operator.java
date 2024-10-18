40b616aa5b097fc2a5c59993e444329f6438ff49
hongkikii <hyangmeeh@icloud.com>
2024-10-18 20:37:59 +0900

    fix(Operator): 덧셈 오류 수정

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -6,7 +6,9 @@ import java.util.List;
 public class Operator {
     public BigDecimal add(List<BigDecimal> operands) {
         BigDecimal result = BigDecimal.ZERO;
-        operands.forEach(result::add);
+        for (BigDecimal operand : operands) {
+            result = result.add(operand);
+        }
         return result;
     }
 }
