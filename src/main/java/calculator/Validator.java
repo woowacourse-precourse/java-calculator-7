af7272aaa0e3a637b2a455b0ea0d8d118eb207e8
sinminseok12 <sinminseok12@naver.com>
2024-10-19 02:53:08 +0900

    feat : 커스텀 구분자 공백, 숫자 예외 처리 구현

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -1,7 +1,9 @@
 package calculator;
 
 public class Validator {
+    private static final String ERROR_INVALID_DELIMITER = "커스텀 구분자로 숫자 또는 공백을 사용할 수 없습니다.";
     private static final String ERROR_INVALID_FORMAT = "커스텀 구분자 형식이 올바르지 않습니다.";
+    private static final String INVALID_CUSTOM_DELIMITER_PATTERN = "//[\\d\\s]*((\\\\n)|(\\n)).*";
 
     private final String defaultDelimiter;
     private final String prefix;
@@ -23,6 +25,9 @@ public class Validator {
         }
         if (!value.startsWith(prefix) || !value.contains(suffix)) {
             throw new IllegalArgumentException(ERROR_INVALID_FORMAT);
+        }
+        if (value.matches(INVALID_CUSTOM_DELIMITER_PATTERN)) {
+            throw new IllegalArgumentException(ERROR_INVALID_DELIMITER);
         }
     }
 }
