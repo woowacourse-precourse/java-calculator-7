e88eae26bd174e59bb27962a187da5df3fc8d6a2
pakaheo <k70923@konkuk.ac.kr>
2024-10-18 14:19:10 +0900

    refactor : add static factory method and createCalculationInputs by Input

--- src/main/java/calculator/domain/CustomInput.java
+++ src/main/java/calculator/domain/CustomInput.java
@@ -1,17 +1,52 @@
 package calculator.domain;
 
+import calculator.util.InputValidator;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class CustomDelimiterInput extends Input {
+public class CustomInput extends Input {
+
+    private static final String REGEX = "\\//(.)\\\\n(.*)";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
+    private static final int FIRST = 1;
+    private static final int SECOND = 2;
 
     private final String text;
 
-    private CustomDelimiterInput(String text) {
+    private CustomInput(String text) {
         this.text = text;
     }
 
     public static Input from(String text) {
-        return new CustomDelimiterInput(text);
+        return new CustomInput(text);
+    }
+
+    @Override
+    public Long[] createCalculationInputs() {
+        Matcher matcher = getMatcher();
+
+        InputValidator.validateCustomFormat(matcher);
+
+        String[] strings = findString(matcher)
+                .split(findCustom(matcher));
+
+        return Arrays.stream(strings)
+                .map(Long::valueOf)
+                .toArray(Long[]::new);
+    }
+
+    private Matcher getMatcher() {
+        return PATTERN.matcher(text);
+    }
+
+    private String findString(Matcher matcher) {
+        return matcher.group(SECOND);
+    }
+
+    private String findCustom(Matcher matcher) {
+        return matcher.group(FIRST);
     }
 
     @Override
@@ -19,7 +54,7 @@ public class CustomDelimiterInput extends Input {
         if (this == object) {
             return true;
         }
-        if (!(object instanceof CustomDelimiterInput that)) {
+        if (!(object instanceof CustomInput that)) {
             return false;
         }
         return Objects.equals(text, that.text);
