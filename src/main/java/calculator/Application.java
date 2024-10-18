95663aa43106f0c58fb3090ad142afa4ab75552a
minitiated <minitiated@gmail.com>
2024-10-19 03:12:28 +0900

    fix(calculator): started from the beginning again
    when it becomes a tangled spaghetti code,
    
    eat it all and do it again with the empty dish from the beginning...

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,22 +1,8 @@
 package calculator;
 
-import calculator.domain.UserInput;
-import calculator.service.InputReceiverService;
-import calculator.service.ParserService;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        //값을 입력받아 UserInput 형태로 돌려주는 InputReceiverService 초기화
-        InputReceiverService inputReceiver = new InputReceiverService();
-        ParserService parserService = new ParserService();
 
-        try {
-            UserInput userInput = inputReceiver.getUserInput();
-            parserService.checkUserInputEmpty(userInput);
-
-        } catch (IllegalArgumentException e) {
-            System.out.println("입력하신 값에 오류가 있습니다. 다시 입력해 주세요.");
-        }
     }
 }
