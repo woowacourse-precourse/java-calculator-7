e3ce568ea44c9265539a5ed03e61a90d784dcccd
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-10-18 17:33:03 +0900

    fix : 커스터 구분자 형식이 잘못 되었을 경우 예외 처리 추가

--- src/main/java/calculator/model/DelimiterParser.java
+++ src/main/java/calculator/model/DelimiterParser.java
@@ -6,6 +6,11 @@ public class DelimiterParser {
     public String parseDelimiter(String input){
         if(input.startsWith("//")){
             int index = input.indexOf("\\n");
+
+            if(index == -1){
+                throw new IllegalArgumentException("커스텀 구분자 지정 형식이 잘못 되었습니다.");
+            }
+
             String customDelimiter = input.substring(2, index);
             return DELIMITERS + "|" + customDelimiter;
         }
