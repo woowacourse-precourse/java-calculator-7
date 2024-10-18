8ff0b56cf6ec4b3a1490bf79c1630cacd6b72967
haeun <haeun9634@naver.com>
2024-10-18 19:21:43 +0900

    feat: 커스틈 구분자 확인 및 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String input = getInput(); // 문자열 입력 받기
+        String delimiter = getCustomDelimiter(input);
     }
 
     private static String getInput() {
@@ -18,6 +19,15 @@ public class Application {
             throw new IllegalArgumentException("문자열이 비어있습니다.");
         }
     }
+    private static String getCustomDelimiter(String input){
+        if(input.startsWith("//")){
+            int Nindex = input.indexOf("\\n");
+            return input.substring(2,Nindex);//커스텀 구분자
+        }
+        return "[,;]";//기본 구분자
+    }
+
+
 
 
 
