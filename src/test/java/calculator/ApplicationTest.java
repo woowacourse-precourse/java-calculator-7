d07b9c9db87960ef7db9d302c0a78d02ed0a4583
alpin87 <qortmdals94@naver.com>
2024-10-18 22:57:34 +0900

    test: 테스트 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,31 +1,88 @@
 package calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+class CalculatorTest extends NsTest {
 
-class ApplicationTest extends NsTest {
-    @Test
-    void 커스텀_구분자_사용() {
-        assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
-        });
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
     }
 
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+    void 빈문자열_테스트() {
+        assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    void 단일숫자_테스트() {
+        assertEquals(1, calculator.add("1"));
+    }
+
+    @Test
+    void 두개의숫자_테스트() {
+        assertEquals(3, calculator.add("1,2"));
+    }
+
+    @Test
+    void 여러개의숫자_테스트() {
+        assertEquals(6, calculator.add("1,2,3"));
+    }
+
+    @Test
+    void 새줄구분자_사용시_예외발생_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.add("1\n2,3"));
+    }
+
+    @Test
+    void 사용자정의구분자_테스트() {
+        assertEquals(6, calculator.add("//;\n1;2;3"));
+    }
+
+    @Test
+    void 음수_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.add("-1,2"));
+    }
+
+    @Test
+    void 유효하지않은숫자_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.add("1,a"));
+    }
+
+    @Test
+    void 유효하지않은사용자정의구분자_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.add("//\n1;2"));
+    }
+
+    @Test
+    void 사용자정의구분자_덧셈결과_테스트() {
+        assertEquals(3, calculator.add("//;\n1;2"));
+    }
+
+    @Test
+    void 사용자정의구분자_여러숫자_테스트() {
+        assertEquals(6, calculator.add("//;\n1;2;3"));
+    }
+
+    @Test
+    void 사용자정의구분자_여러문자_테스트() {
+        assertEquals(6, calculator.add("//;;\n1;;2;;3"));
+    }
+
+    @Test
+    void 사용자정의구분자_특수문자_테스트() {
+        assertEquals(6, calculator.add("//***\n1***2***3"));
     }
 
     @Override
-    public void runMain() {
-        Application.main(new String[]{});
+    protected void runMain() {
+        Application.main(new String[0]);
     }
 }
