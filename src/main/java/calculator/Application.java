65de951f27f5fad50d3b61b5f0bb4226a1cab7ed
na0th <105775683+na0th@users.noreply.github.com>
2024-10-18 12:08:24 +0900

    feat : 화면 출력할 view 추가
    추가한 내용 :
    Input 화면과 Output 화면을 분리하여 구성합니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView.printInputView();
+        String output = "";
+        OutputView.printOutputView(output);
+
+
+
     }
 }
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,8 @@
+package calculator.view;
+
+public class OutputView {
+
+    public static void printOutputView(String output){
+        System.out.println("결과 : "+ output);
+    }
+}
