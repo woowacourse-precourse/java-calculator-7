e92be86193cb0c942141a2f7b09561ddd17141e6
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-19 04:24:02 +0900

    feat: 기본 구분자 처리 예외처리 기능 구현
    checkIsEmpty: 기본 구분자 사이가 비었는지 체크
    isPositive: 추출한 숫자가 음수인지 체크

--- src/main/java/Delimiter/BasicDelimiter.java
+++ src/main/java/Delimiter/BasicDelimiter.java
@@ -1,12 +1,36 @@
 package Delimiter;
 
 public class BasicDelimiter {
+
+    public static final String ERROR_INPUT_IS_NUMBER = "[ERROR] 구분자 외 입력은 오직 숫자만 가능합니다.";
+    public static final String ERROR_INPUT_IS_POSITIVE = "[ERROR] 숫자는 양수만 입력 가능합니다.";
+    public static final String ERROR_INPUT_IS_EMPTY = "[ERROR] 구분자 사이에 값을 입력해야 합니다.";
+
+
     public static int[] getNumber(String input) {
         String[] tokens = input.split(",|:");
         int[] numbers = new int[tokens.length];
-        for (int i = 0; i < tokens.length; i++) {
-            numbers[i] = Integer.parseInt(tokens[i]);
+        try {
+            for (int i = 0; i < tokens.length; i++) {
+                checkIsEmpty(tokens[i]);
+                numbers[i] = isPositive(Integer.parseInt(tokens[i]));
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ERROR_INPUT_IS_NUMBER);
         }
         return numbers;
+    }
+
+    private static void checkIsEmpty(String token) {
+        if (token.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_INPUT_IS_EMPTY);
+        }
+    }
+
+    private static int isPositive(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(ERROR_INPUT_IS_POSITIVE);
+        }
+        return number;
     }
 }
