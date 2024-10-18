756be030633c6ccc8830fdc93968ae989daf38fd
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-19 06:25:51 +0900

    feat: 입력 뷰 구현
    - 사용자의 입력을 받기 위한 View.

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+import calculator.util.io.IOConsole;
+
+public class InputView {
+
+    public String readUserInput() {
+        IOConsole.print("덧셈할 문자열을 입력해 주세요.");
+        return IOConsole.readLine();
+    }
+}
