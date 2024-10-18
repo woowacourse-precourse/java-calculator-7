4b4e23429ef1364f4624f7e7b3d61895e01d6ca1
minSsan <msp1125@hanyang.ac.kr>
2024-10-18 21:09:00 +0900

    feat(CalculatorNumberList): 파싱된 숫자에서 음수를 검증하는 기능

--- src/main/java/calculator/CalculatorNumberList.java
+++ src/main/java/calculator/CalculatorNumberList.java
@@ -1,12 +1,21 @@
 package calculator;
 
+import calculator.exception.InvalidNumberException;
+
 import java.util.List;
 
 public class CalculatorNumberList {
     private final List<Integer> numbers;
 
     public CalculatorNumberList(List<Integer> input) {
+        validateNegative(input);
         numbers = input;
+    }
+
+    private void validateNegative(List<Integer> input) {
+        if (input.stream().anyMatch(i->i < 0)) {
+            throw new InvalidNumberException();
+        }
     }
 
     public int sum() {
