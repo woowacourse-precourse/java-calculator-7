62c4cef5a42914440f651cab580f40b83b79dcdf
박철현 <park82602@gmail.com>
2024-10-18 16:51:44 +0900

    fix : 정규 표현식에 사용되는 문자가 커스텀 구분자로 사용될 경우 구분자로 인식 못하는 문제 해결 -> Pattern.quote 사용

--- src/main/java/calculator/DelimiterParser.java
+++ src/main/java/calculator/DelimiterParser.java
@@ -3,6 +3,7 @@ package calculator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class DelimiterParser {
     private static final String COMMON_DELIMITER = ",";
@@ -16,12 +17,12 @@ public class DelimiterParser {
     public String replace(String input) {
         String result = input;
         for (String delimiter : delimiterList) {
-            if (delimiter.equals(input.substring(0,2))) {
-                result = result.replace(input.substring(0,2), "");
+            if (delimiter.equals(input.substring(0, 2))) {
+                result = result.replace(input.substring(0, 2), "");
                 continue;
             }
-            if (delimiter.equals(input.substring(3,5))) {
-                result = result.replace(input.substring(3,5), "");
+            if (delimiter.equals(input.substring(3, 5))) {
+                result = result.replace(input.substring(3, 5), "");
                 continue;
             }
             result = result.replace(delimiter, COMMON_DELIMITER);
@@ -30,28 +31,30 @@ public class DelimiterParser {
     }
 
 
-
     public static boolean isDefaultDelimiter(String input) {
         return input.contains(":") || input.contains(",");
     }
+
     public List<Integer> parseInputToIntList(String input) {
-        // input = 1,2:3, 1:2:3, 1,2,3, -1,2:3:, 1,-2,3, 1:2:-3;
-        if (isDefaultDelimiter(input)) {
-            String replaceAllCommas = replaceAllByDefaultDelimiter(input);
-            return parseToIntList(replaceAllCommas);
-        }
-        return parseToIntList(input);
+        String replaceAllCommas = replaceAllCommas(input);
+        return parseToIntList(replaceAllCommas);
     }
 
-    private String replaceAllByDefaultDelimiter(String input) {
-        return input.replaceAll("[,:]", ",");
+    private String replaceAllCommas(String input) {
+        if (isDefaultDelimiter(input)) {
+            return input.replaceAll("[,:]", COMMON_DELIMITER);
+        }
+        String delimiter = input.substring(2, input.indexOf("\\n"));
+        String numbersPart = input.substring(input.indexOf("\\n") + 2);
+        return numbersPart.replaceAll(Pattern.quote(delimiter), COMMON_DELIMITER);
     }
-    private String[] splitCommonDelimiter(String input) {
-        return input.split(COMMON_DELIMITER);
+
+    private List<String> splitCommonDelimiter(String input) {
+        return Arrays.stream(input.split(COMMON_DELIMITER)).toList();
     }
 
     private List<Integer> parseToIntList(String input) {
-        return Arrays.stream(splitCommonDelimiter(input))
+        return splitCommonDelimiter(input).stream()
                 .map(Validator::validateIfNotNumber)
                 .map(Validator::validateIfInputNegative)
                 .toList();
