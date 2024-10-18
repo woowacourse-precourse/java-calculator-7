90326d38ac012205dfb2d3f0d7c28b55216b7428
Jeongyounghyeon <snorlax1106@naver.com>
2024-10-18 18:49:55 +0900

    feat(output): 출력 기능 개발

--- src/main/java/calculator/view/CalculatorView.java
+++ src/main/java/calculator/view/CalculatorView.java
@@ -1,11 +1,16 @@
 package calculator.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.math.BigInteger;
 
 public class CalculatorView {
 
     public static String sumInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
+    }
+
+    public static void result(BigInteger result) {
+        System.out.println("결과 : " + result);
     }
 }
