78b5e35e216a2dc398f478aa68445d274794b9f7
Dohun Kim <dohun8758@naver.com>
2024-10-19 02:36:30 +0900

    feat : 문자열 출력 기능
    - Output 객체 생성 및 StandardOutputView 클래스의 인스턴스 할당

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,10 +2,13 @@ package calculator;
 
 import calculator.view.InputView;
 import calculator.view.ConsoleInputView;
+import calculator.view.OutputView;
+import calculator.view.StandardOutputView;
 
 public class Application {
 
     public static void main(String[] args) {
         InputView inputView = new ConsoleInputView();
+        OutputView outputView = new StandardOutputView();
     }
 }
