7dda6292ad9a08ba7820d19fbac76c955402745d
1999byh <1999byh@naver.com>
2024-10-18 11:21:31 +0900

    feat: "덧셈할 문자열을 입력해 주세요."를 출력하다

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,10 @@
 package calculator;
 
 public class Calculator {
+
+    private final Output output;
+
+    public Calculator(Output output) {
+        this.output = output;
+    }
 }
--- src/main/java/calculator/Output.java
+++ src/main/java/calculator/Output.java
@@ -1,4 +1,9 @@
 package calculator;
 
 public class Output {
+
+    public Output() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
 }
