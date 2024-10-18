02c18a37e7de4502e9f32869665352920d59762c
qyinm <qusseun@gmail.com>
2024-10-18 11:00:29 +0900

    feat: 유저 입력, 비즈니스 레이어 분리 작성

---
+++ src/main/java/calculator/CalculatorService.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import java.util.regex.Pattern;
+
+public class CalculatorService {
+
+    public CalculatorService() {
+    }
+
+    public long logic(String input) {
+        return 1;
+    }
+}
