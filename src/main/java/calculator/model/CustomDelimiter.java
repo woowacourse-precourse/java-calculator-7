0e17fe75efe6a3d5222414682f1cdc8ea28acd4a
TalkingPotato90 <star901210@hanmail.net>
2024-10-19 01:10:38 +0900

    feat: 커스텀 구분자 개수 제한 기능 및 테스트 구현

--- src/main/java/calculator/model/CustomDelimiter.java
+++ src/main/java/calculator/model/CustomDelimiter.java
@@ -7,6 +7,8 @@ public class CustomDelimiter extends Delimiter {
 
     @Override
     public void validate() {
-
+        if (delimiter.length() > 3) {
+            throw new IllegalArgumentException("[ERROR][R0001] 커스텀 구분자는 최대 3개까지 등록 가능합니다.");
+        }
     }
 }
--- src/test/java/calculator/model/CustomDelimiterTest.java
+++ src/test/java/calculator/model/CustomDelimiterTest.java
@@ -1,10 +1,27 @@
 package calculator.model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("커스텀 구분자 테스트")
 class CustomDelimiterTest {
+
+    @Test
+    @DisplayName("커스텀 구분자가 3개를 초과하면 예외가 발생한다.")
+    void isCustomDelimiterCountExceedLimit() {
+        assertThatThrownBy(() -> new CustomDelimiter(";!?#").validate())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[R0001]");
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자가 3개를 이하이면 예외가 발생하지 않는다.")
+    void isCustomDelimiterCountBelowLimit() {
+        assertThatCode(() -> new CustomDelimiter(";!?").validate())
+                .doesNotThrowAnyException();
+    }
 
 }
