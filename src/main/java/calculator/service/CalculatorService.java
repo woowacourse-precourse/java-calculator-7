96c8fc57651c3978c5298680c78643306a49964a
skhnpark <skhnpark125@gmail.com>
2024-10-19 05:56:51 +0900

    feat: 커스텀 구분자가 있는지 확인하고 처리하는 기능 구현

---
+++ src/main/java/calculator/service/CalculatorService.java
@@ -0,0 +1,29 @@
+package calculator.service;
+
+public class CalculatorService {
+    private static String customDelimiter;
+
+    public boolean hasCustomDelimiter(String userInput) {
+        return userInput.startsWith("//");
+    }
+
+    public void handleCustomDelimiter(String userInput) {
+        String[] userInputs = userInput.split("\\\\n");
+        if (userInputs.length != 2) {
+            throw new IllegalArgumentException("커스텀 구분자는 한 개만 입력해 주세요.");
+        }
+        setCustomDelimiter(userInputs);
+    }
+
+    public void setCustomDelimiter(String[] userInputs) {
+        String delimiter = userInputs[0].substring(2);
+        if (delimiter.matches("[0-9]")) {
+            throw new IllegalArgumentException("커스텀 구분자가 숫자입니다.");
+        }
+        customDelimiter = delimiter;
+    }
+
+    public String getCustomDelimiter() {
+        return customDelimiter;
+    }
+}
