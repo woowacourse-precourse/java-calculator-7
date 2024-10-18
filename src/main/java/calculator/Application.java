8b4ddcd4f8e4895dcdb34ef60916ee928ec86a77
cm <chanmi125128@gmail.com>
2024-10-18 10:35:53 +0900

    feat(input): handle input string and return 0 for empty input
    - Read a string input from the user using camp.nextstep.edu.missionutils.Console's readLine() method.
    - If the input string is empty, the program will print 0.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 1) 문자열 입력 및 처리
+        String input = Console.readLine();
+
+        // 1.1) 입력 문자열이 비어있을 경우 0을 출력
+        if (input.isEmpty()) {
+            System.out.println("결과 : 0");
+        }
     }
 }
