d02af0651161c798742db1ccc0e6b20d8c29ed61
thsghdud13 <thsgdud25@naver.com>
2024-10-19 04:29:37 +0900

    feat(exception): 사용자의 입력에 대한 예외처리
    * 기존에 사용자의 입력을 예외처리 하지 않았는데 알맞은 형식만 들어오도록 예외처리
    * 정규표현식을 사용해 잘못된 값이 입력되면 IllegalArgumentException 발생
    * 커스텀 구분자 추출 부분도 정규표현식을 사용해 구분자를 추출하도록 변경해 예외처리
    * 사용자가 입력하지 않는다면 결과가 0이 출력되도록 예외처리

--- src/main/java/calculator/model/CalculatorServiceImpl.java
+++ src/main/java/calculator/model/CalculatorServiceImpl.java
@@ -1,23 +1,43 @@
 package calculator.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CalculatorServiceImpl implements CalculatorService {
-
-    private String delimiter = ",|:";
-
     @Override
     public Integer calculate(String input) {
+        String delimiter = ",|:";
         boolean isCustom = input.startsWith("//");
         if (isCustom) {
-            delimiter = delimiter.concat("|" + input.charAt(2));
-            input = input.substring(5);
-            System.out.println(delimiter);
+            Matcher matcher = Pattern.compile("//(.+)\\\\n(.*)").matcher(input);
+            if (matcher.find()) {
+                String customDelimiter = matcher.group(1);
+                String customDelimiterQuote = Pattern.quote(customDelimiter);
+                delimiter = delimiter + "|" + customDelimiterQuote;
+                input = matcher.group(2);
+            } else {
+                throw new IllegalArgumentException("알맞은 입력 형식이 아닙니다.");
+            }
         }
+        Matcher inputMatcher = Pattern.compile("^\\d+((,|:|\\|;)(\\d+))*").matcher(input);
+
+        if (input.isEmpty()) {
+            return 0;
+        }
+        if (!inputMatcher.find()) {
+            throw new IllegalArgumentException("알맞은 입력 형식이 아닙니다.");
+        }
+
         String[] numberList = input.split(delimiter);
 
         int result = 0;
 
         for (String number : numberList) {
-            result += Integer.parseInt(number);
+            try {
+                result += Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("알맞은 입력 형식이 아닙니다.");
+            }
         }
         return result;
     }
