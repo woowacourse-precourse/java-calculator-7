58e6dd5c657ded46ae4857f6008ba6f5ad2e28b6
chul5 <och7164@gmail.com>
2024-10-19 02:17:14 +0900

    feat: 구분자로 구분된 문자가 숫자인지 검증하는 기능

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,31 +1,48 @@
 package calculator.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalculatorService {
-	private final String pattern = "//;\\n";
+	private final String PATTERN = "//;\\n";
+	private final int DELIMITER_INDEX = 2;
 	private String line;
 	private String delimiter = ":,";
 
 	public CalculatorService(String line) {
-		validate(line);
-		this.line = line;
+		this.line = validate(line);
 	}
 
-	private void validate(String line) {
+	private String validate(String line) {
 		if (line == null)
 			throw new IllegalArgumentException("입력값이 null입니다.");
-		if (line.charAt(0) == pattern.charAt(0))
+		if (line.charAt(0) == PATTERN.charAt(0))
 			line = findDelimiter(line);
+		return line;
 	}
 
 	private String findDelimiter(String line) {
-		for (int i = 0; i < pattern.length(); i++) {
-			if (i == 2)
+		for (int i = 0; i < PATTERN.length(); i++) {
+			if (i == DELIMITER_INDEX)
 				continue;
-			if (pattern.charAt(i) != line.charAt(i))
+			if (PATTERN.charAt(i) != line.charAt(i))
 				throw new IllegalArgumentException("구분자 패턴이 아닙니다.");
 		}
-		delimiter += line.charAt(2);
-		return line.substring(pattern.length());
+		delimiter += line.charAt(DELIMITER_INDEX);
+		return line.substring(PATTERN.length());
 	}
 
+	private List<Integer> getNumbers() {
+		List<Integer> numbers = new ArrayList<>();
+		for (String number : line.split("[" + delimiter + "]")) {
+			int item = 0;
+			try {
+				item = Integer.parseInt(number);
+			} catch (Exception e) {
+				throw new IllegalArgumentException("구분된 문자열이 숫자가 아닙니다.");
+			}
+			numbers.add(item);
+		}
+		return numbers;
+	}
 }
