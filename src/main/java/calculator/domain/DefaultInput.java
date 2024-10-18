0a587572987feed5ef7efb1f2b5eeb2c8e8352ca
pakaheo <k70923@konkuk.ac.kr>
2024-10-18 14:19:20 +0900

    refactor : add static factory method and createCalculationInputs by Input

--- src/main/java/calculator/domain/DefaultInput.java
+++ src/main/java/calculator/domain/DefaultInput.java
@@ -1,17 +1,29 @@
 package calculator.domain;
 
+import calculator.util.InputValidator;
 import java.util.Objects;
 
-public class DefaultDelimiterInput extends Input {
+public class DefaultInput extends Input {
 
     private final String text;
 
-    private DefaultDelimiterInput(String text) {
+    private DefaultInput(String text) {
         this.text = text;
     }
 
     public static Input from(String text) {
-        return new DefaultDelimiterInput(text);
+        return new DefaultInput(text);
+    }
+
+    @Override
+    public Long[] createCalculationInputs() {
+        InputValidator.validateHasNumber(text);
+
+        return new Long[]{toLong(text)};
+    }
+
+    private Long toLong(String text) {
+        return Long.valueOf(text);
     }
 
     @Override
@@ -19,7 +31,7 @@ public class DefaultDelimiterInput extends Input {
         if (this == object) {
             return true;
         }
-        if (!(object instanceof DefaultDelimiterInput that)) {
+        if (!(object instanceof DefaultInput that)) {
             return false;
         }
         return Objects.equals(text, that.text);
