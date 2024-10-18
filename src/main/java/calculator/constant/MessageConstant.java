b648289649d5e79acae536ff771ced405cc9d516
kkmdevel <kkm.devel@gmail.com>
2024-10-18 14:59:48 +0900

    feat: MessageConstant Class 생성
    정해진 메세지를 저장하는 객체 MessageConstant Class 생성

---
+++ src/main/java/calculator/constant/MessageConstant.java
@@ -0,0 +1,17 @@
+package calculator.constant;
+
+/**
+ * 메시지 관련 상수 관리 클래스
+ */
+public final class MessageConstant {
+
+    public static final String START_CALCULATOR_INPUT_MSG = "덧셈할 문자열을 입력해 주세요.";
+    public static final String END_CALCULATOR_OUTPUT_MSG = "결과 : ";
+
+    public static final String INVALID_CUSTOM_SEPARATOR_MSG = "잘못된 커스텀 구분자 문자열 입력입니다.";
+    public static final String INVALID_NUMBER_MSG = "잘못된 숫자 문자열 입력입니다.";
+
+    private void MessageConst() {
+        
+    }
+}
