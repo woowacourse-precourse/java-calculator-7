075f40c17bd079c1b62713f5aa80f05882ed80c8
minSsan <msp1125@hanyang.ac.kr>
2024-10-18 21:15:18 +0900

    feat(Calculator): 각 개체의 기능 통합

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,22 @@
 package calculator;
 
+import java.util.List;
+
 public class Calculator {
+    private final CalculatorInput calculatorInput;
+    private final CalculatorOutput calculatorOutput;
+    private final CalculatorInputParser calculatorInputParser;
+
+    public Calculator() {
+        calculatorInput = new CalculatorInput();
+        calculatorOutput = new CalculatorOutput();
+        calculatorInputParser = new CalculatorInputParser();
+    }
+
+    public void run() {
+        String input = calculatorInput.read();
+        List<Integer> parsed = calculatorInputParser.parse(input);
+        CalculatorNumberList numberList = new CalculatorNumberList(parsed);
+        calculatorOutput.print(numberList.sum());
+    }
 }
