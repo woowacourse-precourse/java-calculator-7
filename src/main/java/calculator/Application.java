e87c76132b034e8aa192dfbece51518bdcbb09b6
Jang-GO <jang8195@naver.com>
2024-10-18 14:29:40 +0900

    fix: 프로그램 처리흐름을 ConsoleManager에 위임
    - main 메서드에서 프로그램 처리 흐름을 ConsoleManager에서 담당하도록 지정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
+import calculator.view.ConsoleManager;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String s = Console.readLine();
-
+        ConsoleManager consoleManager = new ConsoleManager();
+        consoleManager.run();
     }
 }
