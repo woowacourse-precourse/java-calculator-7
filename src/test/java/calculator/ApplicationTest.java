80de7cffbe12e5b211cacf197450c443593ba6bd
minjun <ijmtylhtf@naver.com>
2024-10-18 09:58:15 +0900

    test : 쉼표 구분자로만 이루어진 입력값 테스트 코드 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 쉼표_구분자만_존재() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
