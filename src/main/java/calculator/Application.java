ca4a73f7dadb980f5a600d8e2d90fc43bc500872
Park Yu Han <pbk2312@naver.com>
2024-10-19 00:21:51 +0900

    refactor : Application 클래스에서 객체 생성 로직을 Factory로 리팩토링

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,37 +1,19 @@
 package calculator;
 
 import calculator.controller.StringCalculatorController;
-import calculator.model.StringCalculator;
-import calculator.model.StringParser;
-import calculator.service.CalculatorService;
-import calculator.validator.ValidatorFactory;
-import calculator.view.InputView;
-import calculator.view.OutputView;
+import calculator.factory.CalculatorFactory;
 
 public class Application {
 
     public static void main(String[] args) {
         try {
-            StringCalculatorController calculatorController = createController();
+            StringCalculatorController calculatorController = CalculatorFactory.createController();  // Factory를 사용하여 Controller 생성
             calculatorController.run();  // 프로그램 실행
         } catch (IllegalArgumentException e) {
             // 예외 발생 시 메시지 출력 후 프로그램 종료
-            System.out.println(e.getMessage());
-            throw e;
+            System.out.println("오류 발생: " + e.getMessage());
+            throw e;  // 예외를 다시 던져 프로그램을 종료
         }
-    }
-
-    // Controller 및 필요한 객체들을 생성하는 메소드
-    private static StringCalculatorController createController() {
-        StringParser stringParser = new StringParser();
-        StringCalculator stringCalculator = new StringCalculator();
-        var inputValidator = ValidatorFactory.createInputValidator();
-
-        CalculatorService calculatorService = new CalculatorService(stringParser, stringCalculator, inputValidator);
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-
-        return new StringCalculatorController(inputView, outputView, calculatorService);
     }
 
 }
