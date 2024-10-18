e08ace6b0c9473151e99fbb6dc8a8424b87c804d
wkqkel <wkqkel@naver.com>
2024-10-18 23:25:02 +0900

    feat: 사용자입력 받기 구현

---
+++ src/main/java/calculator/CalculatorView.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class CalculatorView {
+
+    public String read() {
+        Scanner scanner = new Scanner(System.in);
+
+        return scanner.nextLine();
+    }
+}
