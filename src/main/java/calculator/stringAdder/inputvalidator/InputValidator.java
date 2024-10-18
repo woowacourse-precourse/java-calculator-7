647f54c216c0f5613ae77c6939e938548e1d7084
jiffyin7 <jiffyin7@gmail.com>
2024-10-18 16:55:28 +0900

    hotfix(NumberParser): NoSuchElementException 처리 추가
    - Console.readLine() 호출을 try-catch 블록으로 감싸 예외 처리
    - NoSuchElementException 발생 시 IllegalArgumentException으로 변환
    - 테스트 환경에서의 입력 시뮬레이션을 위한 예외 처리 개선

--- src/main/java/calculator/stringAdder/inputvalidator/InputValidator.java
+++ src/main/java/calculator/stringAdder/inputvalidator/InputValidator.java
@@ -1,6 +1,7 @@
 package calculator.stringAdder.inputvalidator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 /**
  * @author : jiffyin7@gmail.com
@@ -10,10 +11,15 @@ public class InputValidator {
 
   public String validateInput() {
     System.out.println("덧셈할 문자열을 입력해주세요.");
-    String input = Console.readLine();
-    if (input == null || input.isEmpty()){
-      throw new IllegalArgumentException(this.getClass().getSimpleName()+ ": 입력한 문자열이 비었습니다.");
+    try{
+      String input = Console.readLine();
+      if (input == null || input.isEmpty()){
+        throw new IllegalArgumentException(this.getClass().getSimpleName()+ ": 입력한 문자열이 비었습니다.");
+      }
+      return input;
+    } catch (NoSuchElementException e){ //테스트용
+      throw new IllegalArgumentException(this.getClass().getSimpleName()+ ": NoSuchElementException, 입력한 문자열이 비었습니다.");
     }
-    return input;
+
   }
 }
