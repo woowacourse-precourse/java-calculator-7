34f60651ddd4b4ffc7d907b48870d2525bc09f42
SeungminKimdev <ksm7250432@gmail.com>
2024-10-18 22:12:14 +0900

    feat: 음수 처리 기능 추가

--- src/main/java/calculator/AddNum.java
+++ src/main/java/calculator/AddNum.java
@@ -4,7 +4,11 @@ public class AddNum {
     int add(String[] numList) {
         int answer = 0;
         for (String num : numList) {
-            answer += Integer.parseInt(num);
+            int addNum = Integer.parseInt(num);
+            if (addNum < 0) {
+                throw new IllegalArgumentException();
+            }
+            answer += addNum;
         }
         return answer;
     }
