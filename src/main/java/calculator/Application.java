24d866e279a9b3c2b19ab97fbebc9ba9098a5340
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-10-18 21:51:22 +0900

    feat: 애플리케이션 실행 기능
    애플리케이션 실행 시 사용자가 입력한 문자열을 받아 결과를 출력하는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import calculator.console.CalculatorConsole;
+import calculator.domain.Adder;
+import calculator.domain.DelimiterManager;
+import calculator.domain.Separator;
+import calculator.exception.ExceptionHandler;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 객체들 생성
+        DelimiterManager delimiterManager = new DelimiterManager();
+        Separator separator = new Separator(delimiterManager);
+        Adder adder = new Adder();
+        ExceptionHandler exceptionHandler = new ExceptionHandler(delimiterManager);
+
+        // CalculatorConsole 객체를 생성하여 계산기를 실행
+        CalculatorConsole calculatorConsole = new CalculatorConsole(separator, adder,
+            exceptionHandler);
+
+        // 계산기 실행
+        calculatorConsole.run();
     }
 }
