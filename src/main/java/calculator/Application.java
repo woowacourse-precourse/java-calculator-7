3db5a48ac164714d102019ce1dd03f1b51dda62c
kwon <rnjsdndud00@naver.com>
2024-10-18 14:53:06 +0900

    feat(main): 메인에서 계산기 실행
    ./gradlew.bat clean test 통과,
    입력값: //;\n1;2;3 테스트 통과

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String st = new Scanner(System.in).nextLine();
+        int value = new Calculator().calculate(st);
+        System.out.println("결과 : " + value);
     }
 }
