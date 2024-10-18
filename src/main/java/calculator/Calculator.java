e17c756931b8f34aa3a153b80fe3d16b6f18843e
5jun99 <oj041088@gmail.com>
2024-10-18 10:23:22 +0000

    feat(calculator): add function to sum parsed numbers

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import calculator.io.InputHandler;
+import java.util.Arrays;
 
 public class Calculator {
     private final InputHandler inputHandler;
@@ -11,7 +12,20 @@ public class Calculator {
 
     public void run() {
         String userInput = inputHandler.getUserInput();
-        String[] inputNumbers = splitUserInput(userInput, "[,;]");
+
+        String[] inputStringNumbers = splitUserInput(userInput, "[,;]");
+        int[] inputIntegerNumbers = changeStringArrayToIntegerArray(inputStringNumbers);
+        int result = sumAllNumbers(inputIntegerNumbers);
+    }
+
+    private int sumAllNumbers(int[] inputIntegerNumbers) {
+        return Arrays.stream(inputIntegerNumbers).sum();
+    }
+
+    private int[] changeStringArrayToIntegerArray(String[] StringNumbers) {
+        return Arrays.stream(StringNumbers)
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 
     private String[] splitUserInput(String userInput, String delimiter) {
