10fc38fabc1822893c12ec1177901d97ba8453a8
JooYoung <beachu23@naver.com>
2024-10-18 20:58:55 +0900

    feat(Calculator): 커스텀 구분자와 숫자 분리 기능 개발
    미션에서 주어진 규칙에 따라 사용자 입력에서 커스텀 구분자와 숫자를 분리한다.
    분리 기준이 되는 문자 "//", "\n"를 기본으로 사용하되, 확장성을 고려해 외부에서 생성자를 통해 주입할 수 있도록 설계한다.
    
    BREAKING CHANGE: customDelimiter, customDelimiterStart, customDelimiterEnd 필드를 추가한다.
    	separateCustomDelimiter 메서드를 생성한다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,6 +5,23 @@ import camp.nextstep.edu.missionutils.Console;
 public class Calculator {
 
     private String equation;
+    private String customDelimiter;
+    private final String customDelimiterStart;
+    private final String customDelimiterEnd;
+
+    Calculator() {
+        this.equation = "";
+        this.customDelimiter = "";
+        this.customDelimiterStart = "//";
+        this.customDelimiterEnd = "\\n";
+    }
+
+    Calculator(String customDelimiterStart, String customDelimiterEnd) {
+        this.equation = "";
+        this.customDelimiter = "";
+        this.customDelimiterStart = customDelimiterStart;
+        this.customDelimiterEnd = customDelimiterEnd;
+    }
 
     private String readFromConsole() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
@@ -15,7 +32,18 @@ public class Calculator {
         return input;
     }
 
+    private void separateCustomDelimiter() {
+        if (equation.startsWith(customDelimiterStart)) {
+            int delimEndIndex = equation.lastIndexOf(customDelimiterEnd);
+            if (delimEndIndex != -1) {
+                customDelimiter = equation.substring(customDelimiterStart.length(), delimEndIndex);
+                equation = equation.substring(delimEndIndex + customDelimiterEnd.length());
+            }
+        }
+    }
+
     public void operate() {
         equation = readFromConsole();
+        separateCustomDelimiter();
     }
 }
