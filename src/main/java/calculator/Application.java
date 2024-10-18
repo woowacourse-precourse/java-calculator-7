205958c5d60b62eab1e7547e1f030bff3100c22a
nahyunKoo <angelcindy09@gmail.com>
2024-10-18 13:38:45 +0900

    feat : Application main
    Application main 함수에서 컨트롤러 구동

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import controller.CalculatorController;
+
 public class Application {
     public static void main(String[] args) {
-
+       final CalculatorController calculatorController = new CalculatorController();
+       calculatorController.run();
     }
 }
