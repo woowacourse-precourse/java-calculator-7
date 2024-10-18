8811fd16dd86206e65d38d62dedcdc08654e8189
Hanjjong <tim4974@naver.com>
2024-10-18 20:12:56 +0900

    feat(DelimiterParser): "//" 과 "\n"사이의 문자열 저장 구현

--- src/main/java/calculator/DelimiterParser.java
+++ src/main/java/calculator/DelimiterParser.java
@@ -3,13 +3,11 @@ package calculator;
 public class DelimiterParser {
 
     private final String input;
+    private String numbersPart;
+    private DelimiterStrategy delimiterStrategy;
 
     public DelimiterParser(String input) {
         this.input = input;
-        parseValidator();
-    }
-
-    private void parseValidator() {
         if (input.startsWith("//")) {
             int newlineIndex = input.indexOf('\n');
             if (newlineIndex == -1) {
@@ -18,6 +16,22 @@ public class DelimiterParser {
             if (newlineIndex <= 2) {
                 throw new IllegalArgumentException("잘못된 입력 형식입니다.");
             }
+            if (input.startsWith("//")) {
+                String customDelimiter = input.substring(2, newlineIndex);
+                numbersPart = input.substring(newlineIndex + 1);
+                delimiterStrategy = new CustomDelimiterStrategy(customDelimiter);
+            }
+        } else {
+            numbersPart = input;
+            delimiterStrategy = new DefaultDelimiterStrategy();
         }
+    }
+
+    public String getNumbersPart() {
+        return numbersPart;
+    }
+
+    public DelimiterStrategy getDelimiterStrategy() {
+        return delimiterStrategy;
     }
 }
