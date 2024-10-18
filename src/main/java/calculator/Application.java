78f284210413e9d952791535ab4a20b68f622381
urim96 <qusdbfla1@gmail.com>
2024-10-18 15:36:50 +0900

    2 기능 구현 하며 입력값이 숫자가 아닌 경우 IllegalArgumentException 발생시키기위해 메인메소드에 try catch 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,8 +5,23 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
 
-    	System.out.println("덧셈할 문자열을 입력해 주세요.");
-    	String str = Console.readLine();
-	
+    	try {
+    		System.out.println("덧셈할 문자열을 입력해 주세요.");
+    		String str = Console.readLine();
+    		int checkNum = Application.checkStr(str);
+    		System.out.println("결과 : " + checkNum);
+    	}catch(Exception e) {
+    		throw new IllegalArgumentException();
+    	}
+    }
+    
+    public static int checkStr(String str) {
+    	if(str.length() == 0 || str == null) {
+    		return 0;
+    	}
+    	if(str.length() == 1) {
+    		return Integer.parseInt(str);
+    	}
+		return 0;
     }
 }
