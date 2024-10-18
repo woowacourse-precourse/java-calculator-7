60fa33c7c09fd1493dc1394136ca44f72efd3545
zi-eunn <jieunkim1117@gmail.com>
2024-10-18 15:15:08 +0900

    1번 기본 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String[] numbers = input.split(",|;");
+        int sum = 0;
+
+        try {
+            for (String number : numbers) {
+                sum += Integer.parseInt(number.trim());
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("not number");
+            return 0;
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int result = add(input);
+        System.out.println("Sum: " + result);
     }
 }
