268047570fdd87efea631164054b413d7d059f36
tmdcheol <leesc08392@gmail.com>
2024-10-18 22:12:59 +0900

    feat(calculator): 시작 가이드 메세지 출력
    - Calculator 객체 생성
    - run() 메소드를 통해 가이드 메세지 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Calculator calculator = new Calculator();
+        calculator.run();
     }
 }
---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+
+    private static final String GUIDE_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public void run() {
+        System.out.println(GUIDE_MESSAGE);
+    }
+
+}
