370e5dd1d525871bfb97421d84cd92ceec4d0750
jseom <2ujs4311@gmail.com>
2024-10-18 10:25:20 +0900

    STYLE: 주석 처리해놨던 예전 코드들 삭제
    현재 기능을 구현하기 전에 처음에 구현했었던 기능들을 삭제
    
    Close #5

--- src/main/java/calculator/MyCalculator.java
+++ src/main/java/calculator/MyCalculator.java
@@ -8,7 +8,7 @@ public class MyCalculator {
         if (str.matches("^//[\\D]*\\\\n.*$")) {
             newStr += str.substring(str.indexOf("n") + 1);
         }
-        return newStr.equals("") ? str : newStr;
+        return newStr.isEmpty() ? str : newStr;
     }
 
     private static String[] splitString(String str) {
@@ -31,53 +31,4 @@ public class MyCalculator {
 
         return sum;
     }
-
-//    public String findDelimiter(String str) {
-//        String delimiter = "";
-//        for (int i = 0; i < delimiters.length; i++) {
-//            if (str.matches("//(s)\n(.*)")) {
-//                throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
-//            }
-//            delimiter = delimiters[i];
-//        }
-//        return delimiter;
-//    }
-
-//    public MyCalculator(String numString) {
-//        this.numString = numString;
-//    }
-
-//    private String customSeparator(String str) {
-//        if (str.startsWith("//") && str.contains("\n")) {
-//            return Character.toString(str.charAt(2));
-//        }
-//        return " ";
-//    }
-//
-//    private String[] makeArray() {
-//        if (numString.contains(customSeparator(numString))) {
-//            numString = numString.substring(4);
-//            numString = numString.replace(customSeparator(numString), " ");
-//        }
-//        numString = numString.replace(",", " ");
-//        numString = numString.replace(":", " ");
-//
-//        String[] numArray = numString.split(" ");
-//
-//        return numArray;
-//    }
-
-//    public int getSum() {
-//        if (numString.equals("")) {
-//            return 0;
-//        }
-//        String[] array = makeArray();
-//        int sum = 0;
-//
-//        for (int i = 0; i < array.length; i++) {
-//            sum += Integer.parseInt(array[i]);
-//        }
-//
-//        return sum;
-//    }
 }
