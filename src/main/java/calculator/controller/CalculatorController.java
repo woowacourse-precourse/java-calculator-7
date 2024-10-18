959d5d9a75cc0ffd158529869dcb3b6a3f795043
kim jihyeon <jihyeon546@gmail.com>
2024-10-18 21:59:38 +0900

    feat: 계산기 시작 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,19 +1,27 @@
 package calculator.controller;
 
 import calculator.model.CalculatorModel;
+import calculator.view.CalculatorView;
 
 public class CalculatorController {
 
     private final CalculatorModel calculatorModel;
+    private final CalculatorView calculatorView;
 
     public CalculatorController() {
         this.calculatorModel = new CalculatorModel();
+        this.calculatorView = new CalculatorView();
     }
 
     public int calculate(String input) {
         String[] numbers = calculatorModel.StringSplitter(input);
         int result = calculatorModel.add(numbers);
         return result;
+    }
+
+    public void startCalculator() {
+        String input = calculatorView.getInput();
+        int result = this.calculate(input);
     }
 
 }
