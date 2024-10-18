40b4bf233987ac096583c2d33001637b09c2b870
minseojeong <jgoldstone7@gmail.com>
2024-10-19 01:17:50 +0900

    feat(custom-split): 문자열 앞부분에 // 이나 \n 이 있을 경우, 그 사이에 위치하는 문자를 커스텀 구분자로 사용

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,6 +10,20 @@ public class Application {
 
         if (input == null || input.isEmpty()) {
             System.out.println(0);
+        } else if (input.startsWith("//")) {
+            int targetIndex = input.indexOf('\n');
+            if (targetIndex == -1) {
+                throw new IllegalArgumentException("잘못된 형식입니다.");
+            } else {
+                String custom = input.substring(2, targetIndex);
+                String numbers = input.substring(targetIndex + 1);
+                String[] splitedNumber = numbers.split(custom);
+                int customSum = 0;
+                for (String s : splitedNumber) {
+                    customSum += Integer.parseInt(s);
+                }
+                System.out.println(customSum);
+            }
         } else {
             String[] splited = input.split("[,|:]");
             int sum = 0;
