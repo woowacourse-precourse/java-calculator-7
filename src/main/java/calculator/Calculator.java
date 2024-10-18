9a9c1944717678fe2662ad9b4a259de2c0a95a24
joon <khjoon372@naver.com>
2024-10-18 18:38:27 +0900

    feat: 빈 문자열 처리 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -7,4 +7,15 @@ public class Calculator {
     public Calculator(String inputString){
         this.inputString = inputString;
     }
+
+    /**
+     * 빈 문자열을 처리하는 메서드
+     * @param inputString
+     * @return 빈 문자열일 경우 true
+     */
+    private boolean isEmptyString(String inputString) {
+        return inputString == null || inputString.isEmpty();
+    }
+
+
 }
