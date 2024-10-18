2077314e93565c833bf4facb9572027b03379ae1
MinJae00 <exceedmin@gmail.com>
2024-10-18 20:13:01 +0900

    feat: 예외처리 담당 기능

--- src/main/java/calculator/Exception.java
+++ src/main/java/calculator/Exception.java
@@ -2,13 +2,20 @@ package calculator;
 
 public class Exception {
 
-    int IsNum(String s){
+    int IsPositiveNum(String s){
         try{
-            Integer.parseInt(s);
-            return 1;
+            if(s == ""){
+                throw new NumberFormatException();
+            }
+            int num = Integer.parseInt(s);
+            if(num < 0){
+                throw new NumberFormatException();
+            }
+            return num;
         }
         catch(NumberFormatException e){
-            return 0;
+            throw new IllegalArgumentException();
         }
     }
+
 }
