d5615b75107c0698a49b243187921088befa38fb
YoungGaLee <rkduddl1717@gmail.com>
2024-10-19 06:12:13 +0900

    feat: 커스텀 구분자 지정
    문자열 앞부분에 //와 \n 사이에 구분자로 사용할 문자를 위치시킴으로써 커스텀 구분자를 지정할 수 있다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,21 +3,27 @@ package calculator;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static camp.nextstep.edu.missionutils.Console.close;
 
-
 public class Application {
+
     public static void main(String[] args) {
+        String delimiter = ",|:";
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String inputString = readLine();
 
-        // TODO:  기본 구분자로 분리, 덧셈
-        String[] separatedInput = inputString.split(",|:");
+        // TODO:  커스텀 구분자
+        if (inputString.startsWith("//")){
+            String[] findDelim = inputString.split("\\\\n");
+            delimiter = findDelim[0].substring(2);
+            inputString = inputString.substring(4+delimiter.length());
+        }
+
+        String[] separatedInput = inputString.split(delimiter);
         int sumValue = 0;
         for(int i=0; i<separatedInput.length;i++){
             sumValue += Integer.parseInt(separatedInput[i]);
         }
         System.out.println(String.format("결과 : %d", sumValue));
 
-        // TODO:  커스텀 구분자
         // TODO:  예외처리
     }
 }
