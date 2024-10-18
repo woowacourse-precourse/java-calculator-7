244a708ae17f923e47b1071b1c03458ce0d632f5
eunho <eunho1032@gmail.com>
2024-10-18 17:04:15 +0900

    feat(Calculator): INT_MAX 및 검사 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,8 @@
 package calculator;
 
 public class Calculator {
+	public static final int MAX_INT = 2147483647;
+
 	public static void calculator(String input){
 		if (input == null || input == ""){
 			throw new IllegalArgumentException("빈 사용자 입력"); 
@@ -11,6 +13,9 @@ public class Calculator {
 		long result = 0;
 
 		for (String num : numbers){
+			if (result + ConvertInt.convertInt(num) > MAX_INT){
+				throw new IllegalArgumentException("INT 초과 합");
+			}
 			result += ConvertInt.convertInt(num);
 		}
 		System.out.println("결과 : " + result);
