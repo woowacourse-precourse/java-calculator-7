c5aa7a0b6855f04d6a9897728aaaf4bdf7f0c806
Dohun Kim <dohun8758@naver.com>
2024-10-19 02:35:12 +0900

    feat : 문자열 입력 기능
    - InputView 객체 생성 및 ConsoleInputView 클래스의 인스턴스 할당

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.view.InputView;
+import calculator.view.ConsoleInputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new ConsoleInputView();
     }
 }
