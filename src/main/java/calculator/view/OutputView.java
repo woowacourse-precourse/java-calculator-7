848b421db2fa74f6f721414d02f710e681acb99f
DESKTOP-GRC68NA\foski <foskingson@naver.com>
2024-10-18 15:51:56 +0900

    fix : 출력결과 요구조건에 맞춰 수정

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,6 +2,6 @@ package calculator.view;
 
 public class OutputView {
     public static void showResult(int result) {
-        System.out.println("결과: " + result);
+        System.out.println("결과 : " + result);
     }
 }
