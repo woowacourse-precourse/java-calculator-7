2099fdfd2292ae9357cc47ccf13a8f8d73d1485e
hootzz <wjqwjql3@gmail.com>
2024-10-18 15:33:41 +0900

    fix: escape 문자 \\n 처리 시 구분자 추출 문제 해결

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -11,12 +11,16 @@ public class StringCalculator {
 
         // 커스텀 구분자 처리
         if (input.startsWith("//")) {
+            // \\n라고 적어야 `\`, `n` 검색 가능
+            // \\는 escape 문자로, `\\`를 `\` 한 글자로 취급함
             int delimiterIndex = input.indexOf("\\n");
             // 커스텀 구분자 입력이 정상적으로 되었는지 확인
             if (delimiterIndex != -1) {
                 // 커스텀 구분자가 2글자 이상일 경우를 가정 -> 정규 표현식 사용
                 delimiter = "[" + input.substring(2, delimiterIndex) + "]";
-                input = input.substring(delimiterIndex + 1);
+                // 기존 방식: \n = 1글자 => Index + 1
+                // 현재 방식: \\n = 2글자 => Index + 2 (escape 문자 고려)
+                input = input.substring(delimiterIndex + 2);
             } else {
                 throw new IllegalArgumentException("유효하지 않은 커스텀 구분자 형식입니다.");
             }
