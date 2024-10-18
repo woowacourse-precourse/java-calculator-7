d2f4eb0050d6ffbb8df7589c8cb2fae4011fda50
btongtong <leeym2615@naver.com>
2024-10-19 01:18:31 +0900

    feat(application): add input message for user

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+    public static final String inputMessage = "덧셈할 문자열을 입력해 주세요.";
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(inputMessage);
+        String input = readLine();
     }
 }
