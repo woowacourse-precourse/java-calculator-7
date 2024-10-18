b835777e2b054e4dff05d56597784e33742c5553
parkseongyeol <ert2154@naver.com>
2024-10-18 22:35:23 +0900

    test($ApplicationTest): 커스텀 구분자 사용 테스트 케이스 작성
    커스텀 문자열 지정 문자열의 요소들을 커스텀 구분자로 사용하는 경우 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,6 +17,31 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 역슬레쉬_커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//\\\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 슬레쉬_커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("///\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void n_커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//n\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
