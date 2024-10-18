8ec55e71aa9e5eaf693d758fac24c3a5236c16b1
SeungminKimdev <ksm7250432@gmail.com>
2024-10-18 22:09:00 +0900

    feat: 입력 에러 처리 기능 추가

--- src/main/java/calculator/InputString.java
+++ src/main/java/calculator/InputString.java
@@ -4,7 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputString {
     String input() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine();
+        try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            return Console.readLine();
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
