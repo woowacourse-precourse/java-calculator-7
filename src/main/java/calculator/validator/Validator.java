75f12821835b79bf4a8399424676a69685bbfc91
chan <ckstj8027@naver.com>
2024-10-18 19:15:59 +0900

    feat(validator): \n 다음 내용이있는지 검증

--- src/main/java/calculator/validator/Validator.java
+++ src/main/java/calculator/validator/Validator.java
@@ -6,8 +6,19 @@ public class Validator {
     public static void validate(int number) {
 
         if (number <= 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[NumberError] 양수를 입력해주세요");
         }
+
+    }
+
+
+    public static boolean isHaveNextToken(String input) {
+
+        if (input.split("\\\\n").length == 1) {
+            throw new IllegalArgumentException("[FORMAT_ERROR] NextToken의 입력값이 없습니다");
+        }
+
+        return true;
 
     }
 
