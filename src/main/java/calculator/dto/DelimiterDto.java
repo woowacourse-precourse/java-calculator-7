8aae51a880e398662aa4c60265b7e242dfc0a297
chan <ckstj8027@naver.com>
2024-10-19 00:13:26 +0900

    refactor: Delimiter 객체와 DelimiterDto 도입

---
+++ src/main/java/calculator/dto/DelimiterDto.java
@@ -0,0 +1,13 @@
+package calculator.dto;
+
+public class DelimiterDto {
+    private String delimiter;
+
+    public DelimiterDto(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public String getDelimiter() {
+        return delimiter;
+    }
+}
