1409d17b933b2a30e27b0312ff1ddf729b98e5c4
Su-daa <subin.kk019@gmail.com>
2024-10-18 15:12:37 +0900

    Test: 유효한 숫자 하나를 파싱한 경우 숫자 반환하는 테스트 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -16,10 +16,18 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("빈 문자열 배열을 입력할 경우 0을 반환해야 한다.")
+    @DisplayName("빈 문자열 배열을 파싱한 경우 0을 반환한다.")
     void calculateWithEmptyArray() {
         String[] input = {};
         int result = calculator.calculate(input);
         assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("유효한 숫자 하나를 파싱한 경우 해당 숫자를 반환한다.")
+    void calculateWithSingleValidNumber() {
+        String[] input = {"1"};
+        int result = calculator.calculate(input);
+        assertThat(result).isEqualTo(1);
     }
 }
