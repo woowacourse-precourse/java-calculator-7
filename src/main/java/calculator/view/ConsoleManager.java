e608a7235abf194c77c62452c86a1c50b17de40d
Jang-GO <jang8195@naver.com>
2024-10-18 15:11:23 +0900

    test(ParserSelectorTest) : 적절한 parser를 리턴하는지 테스트
    - 기본, 커스텀 parser 리턴 테스트

--- src/main/java/calculator/view/ConsoleManager.java
+++ src/main/java/calculator/view/ConsoleManager.java
@@ -3,6 +3,8 @@ package calculator.view;
 import calculator.parser.ParserSelector;
 import calculator.parser.StringParser;
 
+import java.util.List;
+
 public class ConsoleManager {
     private final static String WELCOME_MESSAGE = "덧셈할 문자열을 입력해주세요.";
     private final static String RESULT_MESSAGE = "결과 : ";
@@ -13,14 +15,14 @@ public class ConsoleManager {
 
         StringParser parser = ParserSelector.select(str);
         List<String> list = parser.parse(str);
-        int result = Calculator.sum(list);
-
-        printResultMessage(result);
+//        int result = Calculator.sum(list);
+//
+//        printResultMessage(result);
     }
     public void printWelcomeMessage(){
         System.out.println(WELCOME_MESSAGE);
     }
-    public String printResultMessage(int result){
+    public void printResultMessage(int result){
         System.out.println(RESULT_MESSAGE + result);
     }
 }
