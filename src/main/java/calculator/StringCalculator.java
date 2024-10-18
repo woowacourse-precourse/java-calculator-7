a13505b25261e298fffb792ab463e0b7251e1e1c
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-10-18 20:52:25 +0900

    Refactoring method

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,19 +4,33 @@ public class StringCalculator {
 
     public int add(String input) {
         // input null or 0
-        if (input == null || input.isEmpty()) {
+        if (isBlank(input)) {
             return 0;
         }
         // 구분자 "," 입력
-
-            String[] values =  input.split(",");
-            int sum = 0;
-            for(String value : values){
-                sum += Integer.parseInt(value);
-            }
-            return sum;
-
+       return sum(toInts(split(input)));
+    }
+    private boolean isBlank(String input) {
+        return input == null || input.isEmpty();
+    }
+    private String[] split(String input) {
+        String[] values =  input.split(",");
+        return values;
     }
 
+    private int[] toInts(String[] values){
+        int[] numbers = new int[values.length];
+        for(int i =0; i < values.length; i++){
+            numbers[i] = Integer.parseInt(values[i]);
+        }
+        return numbers;
+    }
 
+    private int sum(int[] numbers) {
+        int sum = 0;
+        for(int number : numbers){
+            sum += number;
+        }
+        return sum;
+    }
 }
