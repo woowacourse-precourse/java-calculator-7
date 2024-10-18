278f6d114b2011874c6b5a5a464a2c012072decd
hongkikii <hyangmeeh@icloud.com>
2024-10-18 22:14:06 +0900

    feat(Application): 문자열 입력 기능(1) 구현
    문자열 입력 시점의 알림 문구 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
 
         InputManager inputManager = new InputManager(input);
