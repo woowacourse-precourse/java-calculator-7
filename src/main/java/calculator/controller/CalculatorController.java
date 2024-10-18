35dec0df606454976627ac80d6f3b9e9e930ec24
jae woo Jung <awfjol2008@gmail.com>
2024-10-18 15:42:49 +0900

    feat: CalculatorController 클래스 필드 변수 및 생성자 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,17 @@
 package calculator.controller;
 
+import calculator.model.InputValidator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class CalculatorController {
+	private final InputView inputView;
+	private final InputValidator inputValidator;
+	private final OutputView outputView;
+
+	public CalculatorController() {
+		this.inputView = new InputView();
+		this.outputView = new OutputView();
+		this.inputValidator = new InputValidator();
+	}
 }
