1d5ba37879caaf2a2edb963a4b5b44d8ba9b61d0
Donghee <hongricky73@naver.com>
2024-10-18 17:47:20 +0900

    feat: return 0 if you enter an empty string or null value

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,48 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        //문자열 입력받기
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
-
-        try {
-            int result = add(input);
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 입력입니다: " + e.getMessage());
-        }
+        StringCalculator calculator = new StringCalculator();
+        System.out.println("덧셈할 문자열을 입력하세요:");
+        int result = calculator.calculate();
+        System.out.println("결과 : " + result);
     }
+}
 
-    public static int add(String input) {
-        //입력 된 값이 없을 경우
-        if (input == null || input.isEmpty()) {
+class StringCalculator {
+
+    public int calculate() {
+        String input = Console.readLine(); // 사용자 입력 받기
+
+        // null 혹은 빈 문자열을 입력할 경우 0을 반환
+        if (input == null || input.isBlank()) {
             return 0;
         }
 
-        String[] num;
-        String delimiter = ",|:";
-        if (input.startsWith("//")) {
-            // 커스텀 구분자 추출
-            int delimiterIndex = input.indexOf("\n");
-            delimiter = input.substring(2, delimiterIndex);
-            input = input.substring(delimiterIndex + 1);
-        }
-
-        num = input.split(delimiter);
-        int sum = 0;
-        for (String number : num) {
-            int num1 = parseNumber(number);
-            sum += num1;
-        }
-
-        return sum;
-    }
-
-    private static int parseNumber(String number) {
-        try {
-            return Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
-        }
+        // 기본 동작 추가 예정
+        return -1;  // 임시값
     }
 }
