9e2ed46f4b969eaf8ffa7614337ca289180e2ab3
TaegeunYou <dbxorms2310@naver.com>
2024-10-18 16:15:19 +0900

    feat(InputView): 시작 문구를 출력하는 기능 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -4,7 +4,10 @@ import calculator.view.console.ConsoleReader;
 import calculator.view.console.ConsoleWriter;
 
 public class InputView {
+    private static final String START_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
     public String enterString() {
+        ConsoleWriter.printlnMessage(START_MESSAGE);
         return ConsoleReader.enterMessage();
     }
 }
---
+++ src/main/java/calculator/view/console/ConsoleWriter.java
@@ -0,0 +1,7 @@
+package calculator.view.console;
+
+public final class ConsoleWriter {
+    public static void printlnMessage(String message) {
+        System.out.println(message);
+    }
+}
