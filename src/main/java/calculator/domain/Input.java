d49dc01d3450f150b7741e2404e2d8769aee8fe5
pakaheo <k70923@konkuk.ac.kr>
2024-10-18 09:40:39 +0900

    feat : new abstract class for user input

--- src/main/java/calculator/domain/Input.java
+++ src/main/java/calculator/domain/Input.java
@@ -1,2 +1,28 @@
-package calculator.domain;public class Input {
+package calculator.domain;
+
+import java.util.Objects;
+
+public abstract class Input {
+
+    private final String text;
+
+    public Input(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Input input)) {
+            return false;
+        }
+        return Objects.equals(text, input.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(text);
+    }
 }
