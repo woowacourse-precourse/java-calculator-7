711a035c1940cd91fb4c7c1bd1cdd80b6d12a14a
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-18 15:56:35 +0900

    fix(SeparatorExtractor): 런타임에 정규식 //(.+?)을 매칭시킬 수 없는 문제 해결
    커스텀 구분자 추출 메서드에서 테스트 환경에서는 정규식 //(.+?)\n을 매칭시키지만
    
    런타임에서는 매칭시키지 못하는 문제가 발생하여 SeparatorExtractor 클래스 생성시 정규식을 입력받도록 수정

--- src/main/java/calculator/SeparatorExtractor.java
+++ src/main/java/calculator/SeparatorExtractor.java
@@ -6,27 +6,37 @@ import java.util.regex.Pattern;
 
 public class SeparatorExtractor {
 
+    private final String regex;
+
+
+    //테스트환경에서는 \n가 문자열로 인식되지만 실제 런타입에서는 개행문자로 인식되는 문제가 있어 정규식을 상황에 맞게 받도록함
+    public SeparatorExtractor(String regex) {
+        this.regex = regex;
+    }
+
     public String extractCustomSeparator(String value, List<String> customSeparators) {
         if (!value.contains("//")) {
             return value;
         }
 
-        String regex = "//(.+?)\n";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(value);
 
         while (matcher.find()) {
+            //커스텀 구분자 추출
             String separator = matcher.group(1);
 
+            //커스텀 구분자에 양수가 포함되면 예외 발생
             boolean isNumContains = separator.chars().anyMatch(c -> c >= '1' && c <= '9');
             if (isNumContains) {
                 throw new IllegalArgumentException("커스텀 구분자에는 양수가 포함될 수 없습니다.");
             }
 
+            //구분자 리스트에 추출한 커스텀 구분자 추가
             customSeparators.add(separator);
         }
 
-        // 커스텀 구분자 선언이 추출되어 삭제된 문자열 반환
+        // 커스텀 구분자 선언이 추출되고 남은 문자열 반환
         return matcher.replaceAll("").trim();
     }
 }
