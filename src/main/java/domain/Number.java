1fb23c2813acc00e0e7df2f8bee5440a5aaf0785
nahyunKoo <angelcindy09@gmail.com>
2024-10-18 13:07:57 +0900

    feat : validateNumber (domain)
    유효성 검사 함수 생성

--- src/main/java/domain/Number.java
+++ src/main/java/domain/Number.java
@@ -4,6 +4,15 @@ public class Number {
     int[] numbers;
     public Number() {}
     public Number(int[] numbers){
+        validateNumber(numbers);
         this.numbers = numbers;
+    }
+
+    private void validateNumber(int[] numbers) {
+        for(int number : numbers){
+            if(number <=0){
+                throw new IllegalArgumentException("모든 숫자는 양수여야 합니다.");
+            }
+        }
     }
 }
