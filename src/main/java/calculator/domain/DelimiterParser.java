18cd8c9fbdf439be1d0eaf8814516ac5767c6cc4
Lanvizu <ghzm888@gmail.com>
2024-10-18 15:24:39 +0900

    feat: 계산, 구분자 구별, 문자열 분할 하는 클래스 생성

---
+++ src/main/java/calculator/domain/DelimiterParser.java
@@ -0,0 +1,9 @@
+package calculator.domain;
+
+public class DelimiterParser {
+    private static final String DEFAULT_DELIMITER = ",|:";
+
+//    public String parse(String input) {
+//        // 커스텀 구분자 파싱 로직 구현
+//    }
+}
---
+++ src/main/java/calculator/domain/StringSplitter.java
@@ -0,0 +1,7 @@
+package calculator.domain;
+
+public class StringSplitter {
+//    public String[] split(String input, String delimiter) {
+//        // 문자열 분할 로직 구현
+//    }
+}
