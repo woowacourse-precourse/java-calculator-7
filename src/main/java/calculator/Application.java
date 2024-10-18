b10e0c647609dc3927ed2d499222482a508cb019
JEONGHUNJEON <jjhoon06@gmail.com>
2024-10-18 17:53:54 +0900

    feat: 커스텀 구분자 확인 메서드 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,30 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static String[] checkInput (String input, StringBuilder delimiter) {
+        String validatedCustomDelimiterInput = checkCustomDelimiter(input, delimiter);
+//        if (!checkArguments(validatedCustomDelimiterInput, delimiter))
+//            throw new IllegalArgumentException("wrong arguments");
+//        return checkDuplicateDelimiter(validatedCustomDelimiterInput.split(delimiter.toString()));
+        return null;
+    }
+
+    public static String checkCustomDelimiter (String input, StringBuilder delimiter) {
+        String customInput = input;
+        if (input.startsWith("//")) {
+            int delimiterEndIndex = input.indexOf("\\n");
+            if (delimiterEndIndex == -1)
+                throw new IllegalArgumentException("custom delimiter error");
+            delimiter.append(input, 2, delimiterEndIndex);
+            customInput = input.substring(delimiterEndIndex + 2);
+        }
+        delimiter.append("]");
+        return customInput;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ;
     }
 }
