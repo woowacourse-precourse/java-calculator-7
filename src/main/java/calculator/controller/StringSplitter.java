d68cedb54a9596bf0465a5981fdada82f0e3601c
binn <anyubin0724@naver.com>
2024-10-18 10:47:58 +0900

    feat : StringSplitter.java 예외처리 추가
    - 커스텀 구분자, 기본 구분자가 아닌 경우 예외처리
    - 입력 중간에 공백이 들어간 경우 예외처리

--- src/main/java/calculator/controller/StringSplitter.java
+++ src/main/java/calculator/controller/StringSplitter.java
@@ -4,13 +4,15 @@ public class StringSplitter {
     public static String[] splitter(String input) {
         String delimiter = "[,:]";
         input = input.trim();
-        input = input.replace("\\n", "\n");
+        input = input.replace("\\n", "\n").replace(" ", "");
 
         if (input.startsWith("//")) {
             int endDelimiter = input.indexOf("\n");
             isValidCustom(endDelimiter);
             delimiter = input.substring(2, endDelimiter);
             input = input.substring(endDelimiter + 1);
+        } else {
+            isValidDefault(input);
         }
 
         return input.split(delimiter);
@@ -19,6 +21,12 @@ public class StringSplitter {
     private static void isValidCustom(int index) {
         if (index == -1) {
             throw new IllegalArgumentException("커스텀 구분자 생성 규칙에 맞지 않습니다.");
+        }
+    }
+
+    private static void isValidDefault(String input) {
+        if (!input.matches("[0-9,:\\s]*")) {
+            throw new IllegalArgumentException("기본 구분자 규칙에 맞지 않습니다.");
         }
     }
 }
