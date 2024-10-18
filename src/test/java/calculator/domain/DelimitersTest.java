f71233a6f08e5bcf51e1c01b7fa941389a917149
김연진 <love_homebody@naver.com>
2024-10-18 23:13:40 +0900

    test(Delimiters): 구분자 생성 및 추가 테스트

--- src/test/java/calculator/domain/DelimitersTest.java
+++ src/test/java/calculator/domain/DelimitersTest.java
@@ -1,11 +1,37 @@
 package calculator.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class DelimitersTest {
 
     @Test
+    @DisplayName("구분자를 추가한다.")
     void addDelimiter() {
+        // given
+        Delimiters delimiters = new Delimiters();
+        String customDelimiter = ";";
+
+        // when
+        delimiters.addDelimiter(customDelimiter);
+
+        // then
+        assertThat(delimiters.getElements()).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("구분자에 숫자가 추가될 경우 예외가 발생한다.")
+    void throwExceptionWhenNumberInput() {
+        // given
+        Delimiters delimiters = new Delimiters();
+        String customDelimiter = "1";
+
+        // when, then
+        assertThatThrownBy(() -> delimiters.addDelimiter(customDelimiter))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
