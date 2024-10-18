bc037e7261e44b38753a98c3e94b8a0a8341294f
yybmion <yyb400@naver.com>
2024-10-18 20:13:02 +0900

    feat : splitDelimiter 메서드 추가
    splitDelimiter 메서드를 통해 이전에 커스텀 구분자를 분리 후에 새로운
    문자열을 가져왔다.
    해당 새로운 문자열을 구분자 DELIMITER를 이용해 계산해야할 숫자만 따로
    분리해준다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,14 +11,18 @@ public class Application {
         String inputString = readLine();
 
         String newInputString = splitCustomDelimiter(inputString);
-        splitDelimiter(newInputString, DELIMITER);
+        splitDelimiter(newInputString);
     }
 
     /**
      * @param inputString: 입력값
      * @return 커스텀 구분자 분리 이후 새로운 inputString 값 만약 입력값이 "//"로 시작되고 그 뒤에 "\n"가 존재한다면 커스텀 구분자가 존재한다 (커스텀 구분자 내 공백 제외).
+     * @Exception
+     * 1. '//'로 시작하지만 이후 '\n'가 존재하지 않을때
+     * 2. 커스텀 구분자가 정확히 하나의 유니코드 문자가 아닐때
+     * 3. 커스텀 구분자가 비어있을 때
      */
-    static public String splitCustomDelimiter(String inputString) {
+    public static String splitCustomDelimiter(String inputString) {
         if (inputString.startsWith("//")) {
             int newLineIndex = inputString.indexOf("\n");
             if (newLineIndex == -1) {
@@ -27,8 +31,12 @@ public class Application {
 
             String customDelimiter = inputString.substring(2, newLineIndex);
 
+            if (customDelimiter.codePoints().count() != 1) {
+                throw new IllegalArgumentException("커스텀 구분자는 정확히 하나의 유니코드 문자여야 합니다.");
+            }
+
             if (customDelimiter.isEmpty()) {
-                throw new IllegalArgumentException("커스텀 구분자가 비어있습니다.")
+                throw new IllegalArgumentException("커스텀 구분자가 비어있습니다.");
             }
 
             DELIMITER += "|" + Pattern.quote(customDelimiter);
@@ -38,7 +46,16 @@ public class Application {
         return inputString;
     }
 
-    static private void splitDelimiter(String newInputString, String delimiter) {
+    /**
+     * @param newInputString: 커스텀 구분자 분리 후 새로운 문자열
+     * 새로운 문자열 내 계산해야할 숫자만 분리
+     */
+    private static void splitDelimiter(String newInputString) {
+        String[] numbers = newInputString.split(DELIMITER);
 
+        checkEachNumberAndCalculate(numbers);
+    }
+
+    private static void checkEachNumberAndCalculate(String[] numbers) {
     }
 }
