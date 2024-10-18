360896b22d736f4679af74f178e8c922573145ae
oseungtaek <tmdxor2580@naver.com>
2024-10-18 15:24:36 +0900

    feat(Application): implement main method to get input and print the sum result

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,8 +5,12 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        int result = add(input);
-        System.out.println("결과 : " + result);
+        try {
+            int result = add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 입력입니다: " + e.getMessage());
+        }
     }
 
     public static int add(String input) {
