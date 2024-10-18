a9943c9464ae711795f79f956ae263d75a013b4c
capDoYeonLee <ghtyu21@naver.com>
2024-10-18 23:37:49 +0900

    feat: 입력 내부 일반 구분자 판별 기능

--- src/main/java/calculator/InputClass.java
+++ src/main/java/calculator/InputClass.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import calculator.service.CalculatorService;
+
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class InputClass {
@@ -7,12 +10,22 @@ public class InputClass {
     private final String LEFT_CUSTOM_SEPARATOR = "//";
     private final String RIGHT_CUSTOM_SEPARATOR = "\\n";
     private static final String INPUT_SENTENCE = "덧셈할 문자열을 입력해 주세요.";
+    private CalculatorService calculatorService;
 
-    public static String input() {
-        System.out.println(INPUT_SENTENCE);
-        Scanner scanner = new Scanner(System.in);
-        String inputText = scanner.nextLine();
-        return inputText;
+    /*public InputClass(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }*/
+
+
+    public String input() {
+        try {
+            System.out.println(INPUT_SENTENCE);
+            Scanner scanner = new Scanner(System.in);
+            String inputText = scanner.nextLine();
+            return inputText;
+        } catch (NoSuchElementException e) {
+        }
+        return "";
     }
 
     public Boolean existCustomSeparator(String inputText) {
@@ -20,5 +33,12 @@ public class InputClass {
             return true;
         }
         return false;
+    }
+
+    public Boolean existOriginalSeparator(String inputText) {
+        if ((inputText.contains(LEFT_CUSTOM_SEPARATOR)) && (inputText.contains(RIGHT_CUSTOM_SEPARATOR))) {
+            return false;
+        }
+        return true;
     }
 }
