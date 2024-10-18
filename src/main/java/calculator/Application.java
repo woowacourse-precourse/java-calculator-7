a177e71aeeeb23a70df029c11513d1401ad48a3c
hongjinsu <kaameo12@gmail.com>
2024-10-18 13:53:14 +0900

    refactor(Application): Refactor code and better readability
    - Replaca 2D Array with ArrayList for better readability
    - Improve code reusability by utilizing methods
    
    test: Add test codes for exception handling and basic delimiters
    
    - Add test for string calculation using basic delimiters (,) and (:)
    - Add test for string calculation using non-basic delimiters
    - Add test to verify IllegalArgumentException is thrown for negative input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,27 +2,50 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         // TODO: 프로그램 구현
         String userInput = Console.readLine();
         Console.close();
-        int[] integerArr = new int[512];
-        int sum = 0;
-        int idx = 0;
-        for (int i = 0; i < userInput.length(); i++) {
-            char inputChar = userInput.charAt(i);
-            if (Character.isDigit(inputChar)) {
-                integerArr[idx++] = inputChar - '0';
-            } else if (inputChar == ',' || inputChar == ':') {
-                continue;
-            }else {
-                throw new IllegalArgumentException();
+
+        List<Integer> nums = new ArrayList<>();
+        if (userInput.startsWith("//") && userInput.startsWith("\\n", 3)) {
+//            nums = commandDelim(userInput);
+        } else {
+            nums = basicDelim(userInput);
+        }
+
+
+        int sum = nums.stream().mapToInt(i -> i).sum();
+        System.out.println("결과 : " + sum);
+    }
+
+    private static List<Integer> basicDelim(String userInput) {
+        List<String> strings = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(userInput, ",");
+        while (st.hasMoreTokens()) {
+            strings.add(st.nextToken());
+        }
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < strings.size(); i++) {
+            st = new StringTokenizer(strings.get(i), ":");
+            while (st.hasMoreTokens()) {
+                int num;
+                try {
+                    num = Integer.parseInt(st.nextToken());
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException();
+                }
+                if (num <= 0) {
+                    throw new IllegalArgumentException();
+                }
+                nums.add(num);
             }
         }
-        for (int i : integerArr) {
-            sum += i;
-        }
-        System.out.println("결과 : " + sum);
+        return nums;
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,6 +9,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3,4,5:6:7");
+            assertThat(output()).contains("결과 : 28");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
@@ -17,10 +25,18 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 예외_테스트1() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
