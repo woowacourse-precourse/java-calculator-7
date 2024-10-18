e8ddab47d7ecf895a8c37918bdbb0fa8cec36531
ChiSaeHwang <cwh730909@gmail.com>
2024-10-18 16:03:13 +0900

    refactor(StringCalculator): 커스텀 구분자를 제외한 input값 수정

--- src/main/java/calculator/domain/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/calculator/domain/StringCalculator.java
@@ -5,7 +5,8 @@ public class StringCalculator {
     private final Delimiter delimiter;
 
     private StringCalculator(String input) {
-        this.input = validateStringCalculator(input);
+        String stringCalculator = validateStringCalculator(input);
+        this.input = extractCalculationInput(stringCalculator);
         this.delimiter = Delimiter.of(input);
     }
 
@@ -22,6 +23,16 @@ public class StringCalculator {
 
         if (input == null) {
             throw new IllegalArgumentException("아무값도 입력하지 않을 순 없습니다.");
+        }
+
+        return input;
+    }
+
+    private String extractCalculationInput(String input) {
+        int lastIndex = input.indexOf("\\n");
+        if (lastIndex != -1 && lastIndex < input.length() - 1) {
+            String result = input.substring(lastIndex + 2);
+            return result;
         }
 
         return input;
