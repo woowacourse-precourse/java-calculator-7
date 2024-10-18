a10cac9ef015e16f4a574bb9daffe0b485897766
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-10-19 03:49:19 +0900

    feat: add core calculate logic

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String REGEX = "[,:]";
+        System.out.println("덧셈할 문자열을 입력해주세요");
+        Scanner sc = new Scanner(System.in);
+
+        String user_input = sc.nextLine();
+
+        sc.close();
+
+        float result = 0;
+        String[] a_arr = user_input.split(REGEX);
+        for (String s : a_arr) {
+            float number = Float.parseFloat(s);
+            result += Float.parseFloat(s);
+        }
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.printf("결과 : %s", df.format(result));
     }
 }
