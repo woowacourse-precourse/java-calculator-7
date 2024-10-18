cdb8894494580c92a6d75a2d2903a375279dac9d
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-10-18 15:17:28 +0900

    feat(utils) develop AddEntryValidator class
    - 계산을 위한 숫자를 검증 구현

--- src/main/java/calculator/utils/AddEntryValidator.java
+++ src/main/java/calculator/utils/AddEntryValidator.java
@@ -1,4 +1,14 @@
 package utils;
 
 public class AddEntryValidator {
+
+    private static final String ERROR_PREFIX = "Error: ";
+    private static final String ADDENTRY_NOT_NUMERIC = ERROR_PREFIX + "계산을 위한 입력값이 숫자가 아닙니다.";
+
+    public void validateNumericEntry(String entry) {
+        if (!entry.matches("\\d+")) {
+            throw new IllegalArgumentException(ADDENTRY_NOT_NUMERIC);
+        }
+    }
+
 }
