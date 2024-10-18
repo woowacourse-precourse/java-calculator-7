f60603584fa62d975c75e2d711e232a9ebb4c027
Hacanna42 <dtmdgus901@gmail.com>
2024-10-18 17:30:04 +0900

    feat: implement Calculator

--- src/main/java/calculator/calculate/Calculator.java
+++ src/main/java/calculator/calculate/Calculator.java
@@ -1,4 +1,12 @@
 package calculator.calculate;
 
 public class Calculator {
+    public int calculateNumbers(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        return sum;
+    }
 }
