8b094ee556032652bc23d6ffd78befb2e7bf4ef9
woo <ldw28517@naver.com>
2024-10-18 17:21:50 +0900

    test: Validator 테스트
    - input 문자열 검증: 숫자로 시작하거나 //로 시작, 검증 실패 시 예외 발생
    - 숫자 후보 검증: 오로지 양수인 숫자만 가능, 검증 실패 시 예외 발생

---
+++ src/test/java/calculator/model/ValidatorTest.java
@@ -0,0 +1,108 @@
+package calculator.model;
+
+import calculator.domain.Candidate;
+import calculator.utils.RegexUtils;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorTest {
+
+    Validator validator;
+    Storage storage;
+
+    @BeforeEach
+    void init() {
+        validator = new Validator();
+        storage = new Storage();
+    }
+
+    @Test
+    void 입력_예외_발생_X_숫자() {
+        //given
+        String input = "1,2:3";
+        List<Candidate> candidates = storage.getNumberCandidates();
+
+        //when
+
+        //then
+        assertThatCode(() -> validator.execute(input, candidates))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 입력_예외_발생_X_커스텀구분자() {
+        //given
+        String input = "//=\\n1,2:3";
+        List<Candidate> candidates = storage.getNumberCandidates();
+
+        //when
+
+        //then
+        assertThatCode(() -> validator.execute(input, candidates))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 입력_예외_발생_O_문자로시작() {
+        //given
+        String input = "=1,2:3";
+        List<Candidate> candidates = storage.getNumberCandidates();
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> validator.execute(input, candidates))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자_예외_발생_X() {
+        //given
+        String input = "1,2";
+        List<Candidate> candidates = storage.getNumberCandidates();
+        candidates.add(new Candidate("1"));
+        candidates.add(new Candidate("2"));
+
+        //when
+
+        //then
+        assertThatCode(() -> validator.execute(input, candidates))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 숫자_예외_발생_O_음수포함() {
+        //given
+        String input = "1,2";
+        List<Candidate> candidates = storage.getNumberCandidates();
+        candidates.add(new Candidate("-1"));
+        candidates.add(new Candidate("2"));
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> validator.execute(input, candidates))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자_예외_발생_O_문자포함() {
+        //given
+        String input = "1,2";
+        List<Candidate> candidates = storage.getNumberCandidates();
+        candidates.add(new Candidate("=1+"));
+        candidates.add(new Candidate("2"));
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> validator.execute(input, candidates))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
