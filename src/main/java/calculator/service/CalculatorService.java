a7b5a917745e13534957d41c3b04e891b12686e4
taek2222 <oot309@daum.net>
2024-10-18 20:32:21 +0900

    refactor(CalculatorService): 비즈니스 로직 옮김 작업
    Controller 역할에 집중된 비즈니스를 Service 로 옮겼습니다.
    
    내부에서만 사용하는 계산 로직을 private 제어자로 변경했습니다.

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,10 +1,35 @@
 package calculator.service;
 
+import calculator.util.ArrayConverter;
+import calculator.util.ExtractUtil;
+import calculator.util.NumberValidator;
 import java.util.Arrays;
 
 public class CalculatorService {
+    private final ExtractUtil extractUtil;
+    private final ArrayConverter arrayConverter;
+    private final NumberValidator numberValidator;
 
-    public int calculatorNumbers(int[] numbers) {
+    public CalculatorService() {
+        this.extractUtil = new ExtractUtil();
+        this.arrayConverter = new ArrayConverter();
+        this.numberValidator = new NumberValidator();
+    }
+
+    public int processCalculator(String readInput) {
+        String customDelimiter = extractUtil.extractCustomDelimiter(readInput);
+        String positiveString = extractUtil.extractPositiveString(readInput, customDelimiter);
+
+        char[] positiveCharacters = arrayConverter.toCharArrayWithoutDelimiter(positiveString, customDelimiter);
+        numberValidator.validateNoDigits(positiveCharacters); // 숫자 변환 가능한 문자 검사
+
+        int[] positives = arrayConverter.convertCharArrayToIntArray(positiveCharacters);
+        numberValidator.validatorNoNegatives(positives); // 양수 검사
+
+        return calculatorNumbers(positives);
+    }
+
+    private int calculatorNumbers(int[] numbers) {
         return Arrays.stream(numbers)
                 .sum();
     }
