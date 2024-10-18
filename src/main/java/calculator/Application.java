391ec8e06ead001c6042ad9e950953847f03335a
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-10-18 11:12:41 +0900

    feat: 기본 구분자 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,5 +4,29 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        String question = "";
+        int answer = 0;
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        question = Console.readLine();
+        Console.close();
+        if(question.equals("")){
+            System.out.println("결과 : " + 0);
+            return;
+        }
+        if(!question.contains("//") || !question.contains("\\n")){
+            answer = noCustom(question);
+        }
+
+        System.out.println("결과 : " + answer);
     }
+    public static int noCustom(String question){
+        String[] str = question.split("[,:]");
+        int num = 0;
+        for(int i = 0; i<str.length; i++){
+            num += Integer.parseInt(str[i]);
+        }
+        return num;
+    }
+
+
 }
