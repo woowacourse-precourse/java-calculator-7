1ae86e5ec24f88012d7d43339262a9b84b415420
JooYoung <beachu23@naver.com>
2024-10-18 20:51:04 +0900

    feat(Calculator): 사용자 입력 기능 개발
    콘솔 입력을 통해 사용자 입력을 받을 수 있도록 기능을 개발한다.
    비정상적인 입력으로 인한 NullPointerException을 방지하기 위한 null 체크를 수행한다.
    null이 전달되더라도 예외를 발생시키지 않고, 빈 문자열 ""로 해석한다.
    
    BREAKING CHANGE: 프로그램 실행 시 사용자의 입력을 위한 콘솔이 열린다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,8 +1,21 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Calculator {
 
+    private String equation;
+
+    private String readFromConsole() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        if (input == null) {
+            input = "";
+        }
+        return input;
+    }
+
     public void operate() {
-        
+        equation = readFromConsole();
     }
 }
