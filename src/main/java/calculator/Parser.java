287d99196fec129dad4f5ec3df442dc6d320e912
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-19 03:54:04 +0900

    fix: list형으로 추가하는 커스텀 구분자를 문자열로 반환하도록 로직 수정

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -9,23 +9,61 @@ public class Parser {
     private static final String CUSTOM = "custom";
     private static final String DEFAULT = "default";
 
-    public static List<String> parse(String input) {
+    public static List<Integer> parse(String input) {
+        List<Integer> list = new ArrayList<>();
+        String type = checkType(input);
+        if (type.equals(CUSTOM)) {
+            Validation.checkCustomSeparator(input);
+        }
+        String separators = getSeparator(input);
+        String contents = getNumberString(input);
+
+        StringTokenizer st = new StringTokenizer(contents, separators);
+        try {
+            while (st.hasMoreTokens()) {
+                int num = Integer.parseInt(st.nextToken());
+                list.add(num);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        return list;
+    }
+
+    private static String checkType(String input) {
+        if (input.startsWith("//")) {
+            return CUSTOM;
+        }
+        return DEFAULT;
+    }
+
+    public static String getSeparator(String input) {
         List<String> separators = new ArrayList<>();
         separators.add(",");
         separators.add(":");
 
-        String type = checkInput(input);
+        String type = checkInputV2(input);
         if (type.equals(DEFAULT)) {
             if (!(isCorrectInput(input))) {
                 throw new IllegalArgumentException("잘못된 입력입니다.");
             }
-            return separators;
+            return listToString(separators);
         }
-        if (!validateCustomSeparatorForm(input)) {
+        if (!Validation.isCorrectCustomSeparatorForm(input)) {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
         addCustomSeparators(separators, input);
-        return separators;
+
+        return listToString(separators);
+
+    }
+
+    private static String listToString(List<String> list) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : list) {
+            sb.append(s);
+        }
+        return sb.toString();
     }
 
     private static boolean isCorrectInput(String input) {
@@ -52,13 +90,7 @@ public class Parser {
         System.out.println("separators = " + separators);
     }
 
-    private static String checkInput(String input) {
-
-//        if (hasCustomSeparator(input)) {
-//            return CUSTOM;
-//        } else {
-//            return DEFAULT;
-//        }
+    public static String checkInputV2(String input) {
         if (input.startsWith("//")) {
             return CUSTOM;
         }
@@ -68,7 +100,7 @@ public class Parser {
     private static boolean hasCustomSeparator(String input) {
         if (input.startsWith("//")) {
             String separator = getSeparatorString(input);
-            if (validateCustomSeparatorForm(separator)) {
+            if (Validation.isCorrectCustomSeparatorForm(separator)) {
                 return true;
             } else {
                 throw new IllegalArgumentException("잘못된 입력입니다.");
@@ -114,7 +146,7 @@ public class Parser {
         return numbers;
     }
 
-    private static String getSeparatorString(String input) {
+    public static String getSeparatorString(String input) {
         int separatorIdx = getLastSeparatorIdx(input);
         char[] customSeparatorArr = new char[separatorIdx];
         for (int i = 0; i < separatorIdx; i++) {
@@ -124,7 +156,7 @@ public class Parser {
         return str;
     }
 
-    private static int getLastSeparatorIdx(String input) {
+    public static int getLastSeparatorIdx(String input) {
         int idx = 1;
         for (int i = 1; i < input.length(); i++) {
             if (input.charAt(i) >= '0' && input.charAt(i) <= '9') {
@@ -137,47 +169,5 @@ public class Parser {
             }
         }
         return idx;
-    }
-
-    private static boolean validateCustomSeparatorForm(String input) {
-        return isCorrectPair(input);
-    }
-
-    private static boolean isCorrectPair(String input) {
-        int startPair = 0;
-        int lastPair = 0;
-        int flag = 0;
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == '/' && input.charAt(i + 1) == '/') {
-                if (i + 4 >= input.length()) {
-                    return false;
-                }
-                if (!isCorrectLastElement(input, i + 3)) {
-                    return false;
-                }
-                startPair++;
-                flag = 1;
-                i++;
-            }
-            if (input.charAt(i) == '\\' && input.charAt(i + 1) == 'n') {
-                if (flag == 0) {
-                    return false;
-                }
-                lastPair++;
-                flag = 0;
-                i++;
-            }
-        }
-        if ((startPair == lastPair) && startPair != 0) {
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean isCorrectLastElement(String input, int idx) {
-        if (input.charAt(idx) == '\\' && input.charAt(idx + 1) == 'n') {
-            return true;
-        }
-        return false;
     }
 }
