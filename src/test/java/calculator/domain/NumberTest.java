da9a844cdc20e8a763cde60ab84d4a60bafb7956
taehyeon <taehyeon@taehyeonui-MacBookPro.local>
2024-10-18 23:26:51 +0900

    test(calculator): 숫자가 제대로 들어온 경우

--- src/test/java/calculator/domain/NumberTest.java
+++ src/test/java/calculator/domain/NumberTest.java
@@ -1,7 +1,9 @@
 package calculator.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.in;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -16,5 +18,19 @@ class NumberTest {
     @Test
     void 숫자가_양수가_아닌_경우_예외처리() {
         assertThatThrownBy(() -> new Number("-1")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"+1234, 1234", "0, 0", "1234, 1234", "01234, 1234"})
+    void 숫자가_들어온_경우(String input, String excepted) {
+        //given
+        //테스트 입력값이 @CsvSource를 통해 제공됨
+
+        //when
+        Number number = new Number(input);
+
+        //then
+        String result = number.getValue().toString();
+        Assertions.assertThat(result).isEqualTo(excepted);
     }
 }
