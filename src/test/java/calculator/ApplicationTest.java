7f40b870bf15f1910f579fe75e684ca564b7b95a
mingyeongbae <bmg1203@naver.com>
2024-10-18 15:00:08 +0900

    [test]: 연속 커스텀 구분자 예외 테스트 추가
    커스텀 구분자 내에 문자가 아닌 문자들이 들어가 있으면 예외를 발생시키도록 함

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -40,6 +40,14 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 연속_커스텀_구분자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;]\\n1;2]3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
