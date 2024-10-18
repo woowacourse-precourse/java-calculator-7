3749d94321fc178a9c4b16b52d3c05c1dbc8e348
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-18 16:46:09 +0900

    feat(Application): 문자열 덧셈 프로그램 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,6 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,10 +9,40 @@ public class Application {
         // TODO: 프로그램 구현
         List<String> separators = new ArrayList<>();
 
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Validator validator = new Validator(Constant.CUSTOM_SEPARATOR_END_STR_APP);
+
+        Splitter splitter = new Splitter();
+
+        SeparatorExtractor separatorExtractor = new SeparatorExtractor(Constant.EXTRACT_REGEX_APP);
+
+        Calculator calculator = new Calculator();
+
+        //시작
+        System.out.println(Constant.START_APP);
         String input = Console.readLine();
         Console.close();
 
+        //문자열 유효성 검사
+        validator.validate(input);
+
+        //기본 구분자로 분리된 문자열 리스트
+        List<String> stringListSplitByBasicSeparators = splitter.splitByBasicSeparator(input);
+
+        //커스텀 구분자를 추출하고 분리된 양수 문자열을 담을 리스트
+        List<String> stringNumbers = new ArrayList<>();
+
+        for (String str : stringListSplitByBasicSeparators) {
+            //커스텀 구분자를 추출하고 남은 문자열
+            String extractedString = separatorExtractor.extractCustomSeparator(str, separators);
+
+            //커스텀 구분자로 쪼갠 문자열 리스트
+            List<String> stringNumberPart = splitter.splitByCustomSeparator(extractedString, separators);
+            stringNumbers.addAll(stringNumberPart);
+        }
+
+        int sum = calculator.calculate(stringNumbers);
+
+        System.out.println(Constant.RESULT + sum);
     }
 
 }
