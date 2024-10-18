da029f21e7d2e2b61509e120e737e73eab573861
btongtong <leeym2615@naver.com>
2024-10-19 01:32:43 +0900

    style(application): add blank line

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,8 +4,10 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static final String inputMessage = "덧셈할 문자열을 입력해 주세요.";
+
     public static void main(String[] args) {
         System.out.println(inputMessage);
+
         String input = readLine();
     }
 }
