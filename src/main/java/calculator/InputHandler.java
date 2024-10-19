da4add5856d9843a3caaf2e500481fb9e9629732
lol-chang <doclecsj@naver.com>
2024-10-20 03:32:43 +0900

    feat($InputHandler): [1] 입력값 Null 예외처리

--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -7,6 +7,14 @@ public class InputHandler {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
+        checkNullInput(input);
+
         return input;
+    }
+
+    private void checkNullInput(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("입력 값이 비어 있습니다.");
+        }
     }
 }
