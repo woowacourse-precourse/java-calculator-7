e2d8fba6709e167d25a7f26c9b6d099a3c0e8abf
etd93 <etd93@naver.com>
2024-10-19 22:51:40 +0900

    [feat] 입력 받은 문자열에서 각 숫자를 String으로 뽑아내는 메서드 구현

--- src/main/java/calculator/service/ExtractService.java
+++ src/main/java/calculator/service/ExtractService.java
@@ -2,6 +2,8 @@ package calculator.service;
 
 import calculator.domain.Delimiters;
 
+import java.util.LinkedList;
+
 public class ExtractService {
     public void extractDelimeter(String inputString, Delimiters delimiters){
         int startIndex = inputString.indexOf("//");
@@ -10,5 +12,24 @@ public class ExtractService {
         if(customDelimeter.length()==1){
             delimiters.getDelimeterCollections().add(customDelimeter.charAt(0));
         }
+    }
+
+    public LinkedList<String> extractNumbers(String inputString) {
+        LinkedList<String> numbers = new LinkedList<>();
+        String number = "";
+        for (int i = 0; i < inputString.length(); i++) {
+            if (Character.isDigit(inputString.charAt(i))) {
+                number += inputString.charAt(i);
+            } else {
+                if (!number.isEmpty()) {
+                    numbers.add(number);
+                }
+                number = "";
+            }
+        }
+        if (!number.isEmpty()) {
+            numbers.add(number);
+        }
+        return numbers;
     }
 }
