5d18024893ad1134b42b617fde127c0e56665562
sc@t0a14 <gusdn6671@gmail.com>
2024-10-19 19:22:43 +0900

    refactor(Number): 최소 허용 숫자 0을 MIN_ALLOWED_NUMBER 로 상수화

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -1,10 +1,14 @@
 package calculator;
 
 public class Number {
+    public static final String ONLY_DIGITS = "\\d+";
+    public static final String ZERO_STRING = "\"\"";
+    public static final int MIN_ALLOWED_NUMBER = 0;
+
     private final String number;
 
     public Number(String number) {
-        if (number.equals("\"\"")) {
+        if (number.equals(ZERO_STRING)) {
             number = "0";
         }
         validate(number);
@@ -12,15 +16,16 @@ public class Number {
     }
 
     private void validate(String number) {
+
         if (number.isBlank()) {
             throw new IllegalArgumentException("숫자에 빈 값을 입력할 수 없습니다");
         }
 
-        if (!number.matches("\\d+")) {
+        if (!number.matches(ONLY_DIGITS)) {
             throw new IllegalArgumentException("숫자 외의 문자는 입력할 수 없습니다");
         }
 
-        if (Integer.parseInt(number) < 0) {
+        if (Integer.parseInt(number) < MIN_ALLOWED_NUMBER) {
             throw new IllegalArgumentException("음수를 입력할 수 없습니다");
         }
     }
