1057919343e92fe82b86527e79d32a7a91fb5341
JJY <ddglackrp@gmail.com>
2024-10-20 02:24:36 +0900

    feat "output view 상수 도입"

---
+++ src/main/java/calculator/constant/ViewConstant.java
@@ -0,0 +1,8 @@
+package calculator.constant;
+
+public class ViewConstant {
+
+    public static final String START_OUT_VIEW = "덧셈할 문자열을 입력해 주세요.";
+    public static final String RESULT_OUT_VIEW = "결과 : ";
+
+}
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,13 +1,15 @@
 package calculator.view;
 
+import static calculator.constant.ViewConstant.*;
+
 public class OutputView {
 
     public static void startCalculator(){
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println(START_OUT_VIEW);
     }
 
     public static void printResult(Number calculatorResult){
-        String calculatorOutPut = "결과 : " + calculatorResult;
+        String calculatorOutPut = RESULT_OUT_VIEW + calculatorResult;
         System.out.println(calculatorOutPut);
     }
 }
