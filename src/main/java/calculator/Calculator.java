903a17ab5791776f29c66a466e02bd0b6e1ed4eb
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-10-19 14:57:37 +0900

    feat : 계산기 실행 메소드 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,9 +9,29 @@ import calculator.separator.DefaultSeparator;
 import calculator.separator.SeparatorStorage;
 
 public class Calculator {
+
+    private final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
+    private final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
+    private final DefaultSeparator defaultSeparator = new DefaultSeparator();
+    private final CalculatorAdder calculatorAdder = new CalculatorAdder();
+    private final CalculateComposition calculateComposition = new CalculateComposition(calculatorAdder);
     private static final SeparatorStorage separatorStorage = new SeparatorStorage();
+
     public void initialize(){
+
         separatorStorage.addSeparator(":");
         separatorStorage.addSeparator(",");
+
+    }
+    public void run(){
+        consoleOutputHandler.showInputComments();
+        String userInput = consoleInputHandler.getUserInput();
+        String parsedInput = AddSeparator.addCustomSeparator(userInput);
+
+        int[] toSumArrays = defaultSeparator.separateStringToSum(parsedInput,separatorStorage);
+
+        int sum = calculateComposition.makeSum(toSumArrays);
+
+        consoleOutputHandler.showFinalOutput(sum);
     }
 }
