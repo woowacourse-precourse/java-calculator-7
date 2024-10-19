fc93261f60b79be660f4e7bc6d63fdda464ed40e
seophohoho <seop0937@gmail.com>
2024-10-20 03:29:39 +0900

    test(Validator): 테스트 코드 추가

---
+++ src/test/java/calculator/ValidatorTest.java
@@ -0,0 +1,54 @@
+package calculator;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+public class ValidatorTest extends NsTest {
+    @Test
+    void testIsInputEmpty() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> {
+                Validator obj = new Validator();
+                obj.validate("");
+            }).isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void testIsInputContainNegative() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> {
+                Validator obj = new Validator();
+                obj.validate("-1:2,-3");
+            }).isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void testCustomSeparatorIsCorrectConvention() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> {
+                Validator obj = new Validator();
+                obj.validate("//;1;2;3");
+            }).isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void testCustomSeparatorIsOneSize() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> {
+                Validator obj = new Validator();
+                obj.validate("//!@\\n;1;2;3");
+            }).isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Override
+    protected void runMain() {
+
+    }
+}
