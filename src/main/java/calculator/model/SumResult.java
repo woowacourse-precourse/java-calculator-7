74fdf0babda1c96d880f3a1a78c0129dfa131468
chahyunsoo <chahs1012@naver.com>
2024-10-19 19:59:57 +0900

    feat(SumResult) : 계산된 합을 담는 SumResult 클래스 구현
    * 결과 값을 반환하는 getResult() 메서드를 추가하였습니다.

---
+++ src/main/java/calculator/model/SumResult.java
@@ -0,0 +1,13 @@
+package calculator.model;
+
+public class SumResult {
+    private final int result;
+
+    public SumResult(int result) {
+        this.result = result;
+    }
+
+    public int getResult() {
+        return result;
+    }
+}
