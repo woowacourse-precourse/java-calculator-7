aed11f26159e85bf8ed7b4c44143aa716456b8a9
chaerish <ccyy8432@naver.com>
2024-10-19 17:22:59 +0900

    refactor(Application): main 함수 리팩토링한 클래스 토대로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         CalculatorController calculatorController = new CalculatorController();
-        Long result = calculatorController.startCalculate();
-        System.out.println("결과 : " + result);
+        System.out.println("결과 : " + calculatorController.startCalculate());
     }
 }
