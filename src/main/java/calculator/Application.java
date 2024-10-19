2cf3d0376600b8ff7c0552f5cabe901c790eb661
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-10-20 02:12:06 +0900

    Feat: 입력받은 문자열이 Null이거나 빈칸인 경우를 식별하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
 public class Application {
+
+    public static boolean isBlankOrNull(String sentence){
+        return sentence.isBlank() || sentence == null;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String str = Console.readLine();
+        int result = 0;
+
     }
 }
