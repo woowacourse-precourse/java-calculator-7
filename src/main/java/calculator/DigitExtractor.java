170651e021b694c382eaa9cb9088d6f4e910086d
ssggii <haan7525@gmail.com>
2024-10-19 21:51:11 +0900

    feat: 커스텀 구분자를 이용한 연산 기능 추가

--- src/main/java/calculator/DigitExtractor.java
+++ src/main/java/calculator/DigitExtractor.java
@@ -2,7 +2,6 @@ package calculator;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,8 +12,9 @@ public class DigitExtractor {
     /**
      * 문자열에서 숫자 추출
      */
-    public List<Integer> extractDigits(String input) {
-        if (!isValidateInput(input)) {
+    public List<Integer> extractDigits(String input, String regex) {
+        parseTokens(input, regex);
+        if (!validateInput(input)) {
             throw new IllegalArgumentException("Invalid input: " + input);
         }
         List<Integer> digits = new ArrayList<>(List.of(0)); // 입력이 빈 문자열("")인 경우 0 반환
@@ -25,21 +25,16 @@ public class DigitExtractor {
     }
 
     /**
+     * 토큰 분리
+     */
+    private void parseTokens(String input, String regex) {
+        tokens = Stream.of(input.split(regex)).map(String::trim).collect(Collectors.toList());
+    }
+
+    /**
      * 문자열의 유효성 검증
      */
-    public boolean isValidateInput(String input) {
-        if (input.startsWith("//")) { // TODO 커스텀 구분자 사용
-            char customDelimiter = input.charAt(3);
-            if (input.charAt(4) != '\n' || customDelimiter == ' '
-                    || Character.getNumericValue(customDelimiter) != -1) {
-                return false;
-            }
-            String regex = "[,:]" + "|" + Pattern.quote(Character.toString(customDelimiter));
-            tokens = List.of(input.split(regex));
-        } else { // 기본 구분자만 사용
-            tokens = Stream.of(input.split("[,:]")).map(String::trim).collect(Collectors.toList());
-        }
-
+    private boolean validateInput(String input) {
         return tokens.stream().noneMatch(token -> token.trim().matches(".*[^0-9].*"));
     }
 
