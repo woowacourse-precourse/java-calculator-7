a13c9bbc3e5dacaed887795de5e80672cceaa1f5
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-10-19 21:56:57 +0900

    fix: static 전역 변수 사용으로 인한 테스트 충돌 문제 해결
    Scanner 객체를 전역 static 필드로 관리하여 공유 자원인 System.in이 한 번 사용된 후 닫히는 문제를 발견하였다.
    따라서 Scanner 객체를 main() 내부로 이동하여 테스트 간 간섭을 방지하였다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,9 +2,9 @@ package calculator;
 
 import java.util.Scanner;
 public class Application {
-    private final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = "";
         if (scanner.hasNextLine()) { // 입력 문자열이 null이 아니라면, 버퍼로부터 입력값을 가져온다.
