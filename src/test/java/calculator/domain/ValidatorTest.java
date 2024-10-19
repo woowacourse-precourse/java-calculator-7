6599a577071c4df9333db95190e775c080ab0150
dnwldla <leeemjw@naver.com>
2024-10-19 21:05:01 +0900

    Test: Validator 테스트 추가

---
+++ src/test/java/calculator/domain/ValidatorTest.java
@@ -0,0 +1,31 @@
+package calculator.domain;
+
+import calculator.exception.InvalidInputException;
+import calculator.validation.MessageType;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ValidatorTest {
+
+    @Test
+    void 구분자가_숫자이면_예외_발생(){
+        String seperator="3";
+
+        assertThatThrownBy(()->Validator.validateSeperator(seperator))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessageContaining(MessageType.NUMBER_SEPERATOR.getMessage());
+    }
+
+    @Test
+    void 구분자가_빼기면_예외_발생(){
+        String seperator="-";
+
+        assertThatThrownBy(()->Validator.validateSeperator(seperator))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessageContaining(MessageType.MINUS_SEPERATOR.getMessage());
+
+    }
+
+}
