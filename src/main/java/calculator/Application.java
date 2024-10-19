fd7d539961464b591f13117fe6b96a9bece62227
Jeong yong hwan <baechoo0905@naver.com>
2024-10-19 23:44:28 +0900

    feat: Application 클래스에 전체 프로그램 흐름 추가 및 결과 출력 구현
    - Application 클래스에서 main 메서드를 통해 프로그램의 시작점을 구현하였습니다.
    - 사용자로부터 입력 문자열을 받아 계산 과정을 진행합니다.
    - DelimiterParser, NumberParser, Calculator 클래스를 활용하여 입력 값을 처리하고 합계를 계산합니다.
    - 계산 결과를 콘솔에 출력합니다.
    - Scanner 객체를 사용한 후 close() 메서드를 호출하여 자원을 해제합니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,9 +10,19 @@ public class Application {
 
     private void start() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("덧셈할 문자열을 입력해 주세요: ");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = scanner.nextLine();
+        System.out.println(input);
 
+        DelimiterParser delimiterParser = new DelimiterParser();
+        NumberParser numberParser = new NumberParser();
+        Calculator calculator = new Calculator();
+
+        String delimiter = delimiterParser.getDelimiter(input);
+        String s = delimiterParser.extractNumbers(input);
+        int[] numbers = numberParser.parse(s, delimiter);
+        int result = calculator.sum(numbers);
+        System.out.println("결과 : " + result);
         scanner.close();
     }
 }
