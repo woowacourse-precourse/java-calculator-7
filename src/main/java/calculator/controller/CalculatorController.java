f473d5b6f0fa5eaeec45705bb9cf18c9aa8e94fc
DongHoon Lee <dhl1924@naver.com>
2024-10-19 17:37:56 +0900

    feat(CalculatorController): 사용자 입력 처리 로직 추가
    - AdditionInput을 통한 입력 유효성 검사
    - 입력이 비어 있을 경우 기본 결과 출력 후 재실행

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,10 +1,20 @@
 package calculator.controller;
 
+import calculator.dto.AdditionInput;
+import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class CalculatorController {
 
     public void run() {
         OutputView.printAdditionInputPrompt();
+        AdditionInput additionInput = InputView.readAdditionInput();
+        String validatedInput = additionInput.input();
+
+        if (validatedInput.isEmpty()) {
+            OutputView.printDefaultResult();
+            run();
+        }
+
     }
 }
