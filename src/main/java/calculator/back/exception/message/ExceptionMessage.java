1895460683798682b31e408ce01f20a17d357efb
whatsgoodg <rlwjddl1596@naver.com>
2024-10-19 20:38:56 +0900

    feat(ExceptionMessage): ExceptionMessage Enum 추가
    Exception 발생시 사용할 메세지를 관리하기 위한 Enum 추가

---
+++ src/main/java/calculator/back/exception/message/ExceptionMessage.java
@@ -0,0 +1,17 @@
+package calculator.back.exception.message;
+
+public enum ExceptionMessage {
+    INVALID_INPUT_EXCEPTION("입력 형식에 적합하지 않거나 구분자가 존재하지 않습니다.\n"
+            + "다음과 같은 형식으로 입력해야합니다.\n"
+            + "Regex: ^(//<Custom Delimiter>\\n)*" + "([1-9]\\d*)*([<Delimiters>][1-9]\\d*)*$");
+
+    ExceptionMessage(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    private final String exceptionMessage;
+
+    public String getMessage() {
+        return this.exceptionMessage;
+    }
+}
