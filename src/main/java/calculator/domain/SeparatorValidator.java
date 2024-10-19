10e6af0cffc8011b4ac6f55fd864d1cc3bb3ecaa
JoonKyo <joonkyolee@gmail.com>
2024-10-20 03:52:24 +0900

    feat(SeparatorValidator): 구분자를 이용해서 문자열을 분리하는 메서드 구현

--- src/main/java/calculator/domain/SeparatorValidator.java
+++ src/main/java/calculator/domain/SeparatorValidator.java
@@ -1,10 +1,14 @@
 package calculator.domain;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class SeparatorValidator {
     private static final String CUSTOM_START_LETTERS = "//";
     private static final String CUSTOM_END_LETTERS = "\\n";
+    private static final String MAKE_SPLIT_FORMAT_LETTER = "|";
+    private static final Pattern VALID_NUMBER_PATTERN = Pattern.compile("[-\\d,:]+");
 
     public SeparatorValidator() {
     }
@@ -17,6 +21,8 @@ public class SeparatorValidator {
     }
 
     public static List<String> removeSeparator(List<String> separators, String inputData) {
+        String str = removeCustomSeparatorFormat(inputData);
+        List<String> separatedString = Arrays.asList(separateString(separators, inputData));
     }
 
     private static boolean containsCustomSeparator(String inputData) {
@@ -25,5 +31,16 @@ public class SeparatorValidator {
 
     private static String removeCustomSeparatorFormat(String inputData) {
         return inputData.substring(inputData.indexOf(CUSTOM_END_LETTERS) + CUSTOM_END_LETTERS.length());
+    }
+
+    private static String[] separateString(List<String> separators, String inputData) {
+        String splitFormat = "";
+        for (int i = 0; i < separators.size(); i++) {
+            splitFormat += separators.get(i);
+            if (i < separators.size() - 1) {
+                splitFormat += MAKE_SPLIT_FORMAT_LETTER;
+            }
+        }
+        return inputData.split(splitFormat);
     }
 }
