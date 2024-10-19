92268eb5caa7d22f9f49cb271e326af49b3a8f71
jedo5000 <jsg998877@gmail.com>
2024-10-20 02:49:46 +0900

    refactor: update splitByDelimiter(String input)
    - change to static method

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -4,11 +4,11 @@ import calculator.model.InputFormatter;
 import calculator.model.StringFormatter;
 
 public class CalculatorService {
-    public Long calculateSumFromString(String input) {
+    public int calculateSumFromString(String input) {
         // 1. 입력값을 구분자로 구분하여 문자열 배열로 만듬.
-        InputFormatter inputFormatter = new InputFormatter();
-        String[] strInput = inputFormatter.splitByDelimiter(input);
+        String[] strInput = InputFormatter.splitByDelimiter(input);
         int[] intArr = StringFormatter.toIntArr(strInput);
-        return null;
+
+        return 0;
     }
 }
