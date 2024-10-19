45921629c12f2b1b98b374ac2ec1b0838c494e06
Lucerna00 <pjs3859811@naver.com>
2024-10-20 00:22:02 +0900

    feat(Calculator.java): add logic to calculate numbers

---
+++ src/main/java/calculator/controller/Calculator.java
@@ -0,0 +1,58 @@
+package calculator.controller;
+
+import calculator.model.Numbers;
+import calculator.view.Output;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+
+public class Calculator {
+
+    private static final int ZERO = 0;
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String OR = "|";
+    private static final int CUSTOM_DELIMITER_INDEX = 2;
+    private static final int CUSTOM_DELIMITER_PREFIX = 5;
+
+    private Output output = new Output();
+    private Numbers numbers = new Numbers();
+
+    public void run() {
+        output.printInputPrompt();
+        String input = Console.readLine();
+
+        if (input.isEmpty()) {
+            output.printResultPrompt(ZERO);
+            return;
+        }
+
+        String delimiter = getDelimiter(input);
+        numbers.setNumbers(getNumbers(input, delimiter));
+
+        output.printResultPrompt(numbers.sum());
+        Console.close();
+    }
+
+    private String getDelimiter(String input) {
+        String delimiter = DEFAULT_DELIMITER;
+        if (Validation.hasCustomDelimiter(input)) {
+            delimiter += (OR + input.charAt(CUSTOM_DELIMITER_INDEX));
+        }
+        return delimiter;
+    }
+
+    private int[] getNumbers(String input, String delimiter) {
+        if (!Validation.isDefaultDelimiter(delimiter)) {
+            input = input.substring(CUSTOM_DELIMITER_PREFIX);
+        }
+        String[] tokens = input.split(delimiter);
+        if (!Validation.isAllNumbers(tokens)) {
+            throw new IllegalArgumentException();
+        }
+        int[] numbers = Arrays.stream(tokens)
+            .mapToInt(Integer::parseInt).toArray();
+        if (!Validation.isAllPositive(numbers)) {
+            throw new IllegalArgumentException();
+        }
+        return numbers;
+    }
+}
