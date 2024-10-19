c0116136c635751851464f83c10a8963594c34d4
exena <exena01@gmail.com>
2024-10-19 18:10:32 +0900

    feat: main function merges custom separators and default separators

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,14 +2,22 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String user_input = Console.readLine();
-        char[] separators = {',',':'};
+        Character[] default_separators = {',',':'};
+        List<Character> separators = new ArrayList<> (Arrays.asList(default_separators));
+        char[] customSeparators =  Tokenizer.getCustomSeparators(user_input);
+        for (char c : customSeparators) {
+            separators.add(c);
+        }
+        user_input = Tokenizer.removeHeader(user_input);
         String[] tokens = Tokenizer.tokenize(separators,user_input);
         int[] nums = Tokenizer.tokensToNums(tokens);
         System.out.println("결과 : " + IntStream.of(nums).sum());
