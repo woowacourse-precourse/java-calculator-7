905c5fdb79180c62b02aa27b2e2e04233f67acac
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-19 20:34:31 +0900

    feat: 계산기 추가

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,4 +1,20 @@
 package calculator.domain;
 
+import calculator.global.util.DelimiterExtractor;
+
+import java.util.Arrays;
+
 public class Calculator {
+
+    private final DelimiterExtractor delimiterExtractor;
+
+    public Calculator(DelimiterExtractor delimiterExtractor) {
+        this.delimiterExtractor = delimiterExtractor;
+    }
+
+    public int calculate(String input) {
+        return Arrays.stream(delimiterExtractor.extractNumber(input))
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
 }
