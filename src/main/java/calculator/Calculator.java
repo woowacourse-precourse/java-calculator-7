6a763786ada3ae964ef1587da6d4d202bd9478ac
ziyoungkwon <gy011003@naver.com>
2024-10-20 01:03:56 +0900

    feat: customCalculate 기능 구현
    구분자가 "//"와 "\n" 사이에 있는 커스텀 구분자인 경우의 계산 기능을 구현했다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,12 +3,18 @@ package calculator;
 public class Calculator {
     public int calculate(String input) {
 
+        int sum = 0;
+
         if (input == null || input.isEmpty()) {
-            return 0;
+            return sum;
+        }
+
+        if (input.startsWith("/")){
+            sum = customCalculate(input);
+            return sum;
         }
 
         String[] tokens = input.split("[,:]"); //쉼표나 콜론을 구분자로 분리
-        int sum = 0;
 
         for (String token : tokens) {
 
@@ -19,6 +25,27 @@ public class Calculator {
             sum += number;
         }
 
+        return sum;
+    }
+
+    private int customCalculate(String input) {
+
+        String delimiter = String.valueOf(input.charAt(2));
+        String realInput = input.substring(5);
+
+        String[] tokens = realInput.split(delimiter);
+
+        int sum = 0;
+        int number = 0;
+
+        for (String token : tokens) {
+
+            number = Integer.parseInt(token.trim());
+            if(number < 0) {
+                throw new IllegalArgumentException("음수는 허용되지 않습니다: " + number);
+            }
+            sum += number;
+        }
         return sum;
     }
 }
