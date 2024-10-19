0c0839311ad8afc4936b466d0ccc2a2cce96bca8
JiEung2 <181757@jnu.ac.kr>
2024-10-19 22:17:22 +0900

    test(delimiterParserTest): 커스텀 구분자를 파싱해서 반환하는지 테스트 추가

--- src/test/java/calculator/DelimiterParserTest.java
+++ src/test/java/calculator/DelimiterParserTest.java
@@ -10,8 +10,14 @@ class DelimiterParserTest {
     private final DelimiterParser delimiterParser = new DelimiterParser(new DelimiterManager());
 
     @Test
-    void 등록된_구분자를_기준으로_숫자_분리_테스트(){
+    void 등록된_구분자를_기준으로_숫자_분리_테스트() {
         String input = "1:2,3";
         assertThat(delimiterParser.parse(input)).isEqualTo(List.of(1, 2, 3));
+    }
+
+    @Test
+    void 커스텀_구분자만_파싱해서_반환하는지_테스트() {
+        String input = "//;\n1;2;3";
+        assertThat(delimiterParser.parseCustomDelimiter(input)).isEqualTo(";");
     }
 }
