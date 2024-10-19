7aecc9739d973fda547bfa8590b4b06e80baa9f6
sinminseok12 <sinminseok12@naver.com>
2024-10-20 01:55:27 +0900

    test: 테스트 상황 추가 및 중복 코드 제거

--- src/main/java/calculator/validator/Validator.java
+++ src/main/java/calculator/validator/Validator.java
@@ -2,14 +2,9 @@ package calculator.validator;
 
 import static calculator.converter.Extractor.extractDelimiter;
 import static calculator.converter.Extractor.extractNumberPart;
+import static calculator.utils.ErrorConstants.*;
 
 public class Validator {
-    private static final String ERROR_INVALID_FORMAT = "커스텀 구분자 형식이 올바르지 않습니다.";
-    private static final String ERROR_INVALID_DELIMITER = "커스텀 구분자로 '-',영어,숫자,공백을 사용할 수 없습니다.";
-    private static final String ERROR_NUMBER_EMPTY = "숫자는 공백을 허용하지 않습니다.";
-    private static final String ERROR_DELIMITER_NOT_EXIST = "구분자가 아닌 문자를 허용하지 않습니다.";
-    private static final String ERROR_NUMBER_TYPE = "영어를 허용하지 않습니다.";
-    private static final String ERROR_DELIMITER_DUPLICATE = "중복된 구분자는 허용하지 않습니다.";
     private static final String INVALID_CUSTOM_DELIMITER_PATTERN = "//[a-zA-Z0-9\\s-]+\\\\n.*";
 
     private final String defaultDelimiter;
@@ -69,10 +64,10 @@ public class Validator {
             return;
         }
         if (Character.isLetter(character)) {
-            throw new IllegalArgumentException(ERROR_NUMBER_TYPE + character);
+            throw new IllegalArgumentException(ERROR_NUMBER_TYPE);
         }
         if (!charAsString.matches(delimiter)) {
-            throw new IllegalArgumentException(ERROR_DELIMITER_NOT_EXIST + character);
+            throw new IllegalArgumentException(ERROR_DELIMITER_NOT_EXIST);
         }
     }
 }
--- src/test/java/calculator/validator/ValidatorTest.java
+++ src/test/java/calculator/validator/ValidatorTest.java
@@ -1,39 +1,64 @@
 package calculator.validator;
 
-import calculator.validator.Validator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static calculator.utils.CalculatorConstants.*;
+import static calculator.utils.ErrorConstants.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ValidatorTest {
 
-    Validator validator = new Validator("[,:]", "//", "\\n");
+    Validator validator = new Validator(DELIMITER_REGEX, CUSTOM_DELIMITER_PREFIX, CUSTOM_DELIMITER_SUFFIX);
 
-    @Test
-    void 커스텀_구분자_형식에_벗어날때_예외처리(){
-        String value = "//\n1,2,3";
+    @ParameterizedTest
+    @ValueSource(strings = {"//\1", "//\n", "/\n"})
+    void 커스텀_구분자_형식이_올바르지_않으면_예외를_던진다(String value) {
         assertThatThrownBy(() -> validator.validateDelimiter(value))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_INVALID_FORMAT);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//1\\n", "//a\\n", "//-\\n", "// \\n"})
+    void 커스텀_구분자가_올바르지_않으면_예외를_던진다(String value) {
+        assertThatThrownBy(() -> validator.validateDelimiter(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_INVALID_DELIMITER);
     }
 
     @Test
-    void 커스텀_구분자가_숫자일때_예외처리(){
-        String value = "//1\n1,2,3";
+    void 숫자가_공백일때_예외를_던진다() {
+        String value = "1, ,3";
         assertThatThrownBy(() -> validator.validateDelimiter(value))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_NUMBER_EMPTY);
     }
 
-    @Test
-    void 커스텀_구분자가_공백일때_예외처리(){
-        String value = "// \n1,2,3";
+    @ParameterizedTest
+    @ValueSource(strings = {"1+2", "//+\\n1_"})
+    void 구분자가_아닌_문자가_있을경우_예외를_던진다(String value) {
         assertThatThrownBy(() -> validator.validateDelimiter(value))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_DELIMITER_NOT_EXIST);
     }
 
-    @Test
-    void 구분자_중복될때_예외처리(){
-        String value = "//,\n1,2,3";
+    @ParameterizedTest
+    @ValueSource(strings = {"1,a,3", "//;\\na"})
+    void 숫자_입력란예_영어가_있을경우_예외를_던진다(String value) {
         assertThatThrownBy(() -> validator.validateDelimiter(value))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_NUMBER_TYPE);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//,\\n", "//:\\n"})
+    void 커스텀_구분자가_중복이_있을경우_예외를_던진다(String value) {
+        assertThatThrownBy(() -> validator.validateDelimiter(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_DELIMITER_DUPLICATE);
+    }
+
+
 }
