9a329d3c0a44f03a0b60e7b27c494d266fd82a78
jongseo <jongseo0703@naver.com>
2024-10-19 20:06:47 +0900

    feat: 문자열에서 숫자 추출 기능 구현

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringCalculator {
+
+    // 문자열에서 숫자를 추출하는 함수 extractNumToString
+    public List<Integer> extractNumToString(String input) {
+
+        List<Integer> numbers = new ArrayList<>();
+        StringBuilder currentNum = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i); // 입력한 문자열에서 해당 인덱스 문자를 가져옴
+            if (Character.isDigit(c)) {
+                currentNum.append(c); // 숫자면 currentNum에 추가
+            }
+            else if (currentNum.length() > 0) {
+                numbers.add(Integer.parseInt(currentNum.toString())); // 현재 숫자 리스트에 추가
+                currentNum.setLength(0); // currentNum 초기화
+            }
+        }
+
+        if (currentNum.length() > 0) { // 문자열 끝에 숫자가 남아있으면 추가
+            numbers.add(Integer.parseInt(currentNum.toString()));
+        }
+
+        return numbers; // 추출한 숫자 리스트를 반환함.
+    }
+
+}
