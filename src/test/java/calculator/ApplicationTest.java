1917ec45ba48c45170a6596d5951a5dc9b7e3f04
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-19 16:36:17 +0900

    Test: 기본 구분자를 사용하고 입력값이 콤마로 시작하는 경우 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -145,4 +145,13 @@ class ApplicationTest extends NsTest {
                 numbers
         );
     }
+
+    @Test
+    void 기본_구분자를_사용하고_입력값이_콤마로_시작하는_경우() {
+        final String input = ",1,2:3";
+        SeparationException exception = assertThrows(SeparationException.class, () -> {
+            basicSeparationService.getNumbers(input, BASIC_SEPARATOR_COMMA, BASIC_SEPARATOR_COLON);
+        });
+        assertEquals(STARTS_WITH_SEPARATOR.message(), exception.getMessage());
+    }
 }
