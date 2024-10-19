092ed038ba78d0482809fe5dca97f31db1f68bde
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-20 02:20:39 +0900

    feat(InputReader): 문자열 입력 받는 기능 구현
    주어진 라이브러리를 통해서, 구분자와 양수로 이루어진 문자열 입력받고 DTO로 감사서 반환

--- src/main/java/calculator/Service/InputReader.java
+++ src/main/java/calculator/Service/InputReader.java
@@ -1,4 +1,13 @@
 package calculator.Service;
+import calculator.DTO.StringCalculatorDTO;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputReader {
+
+    // 입력을 받아 DTO로 반환하는 메서드
+    public StringCalculatorDTO read() {
+        System.out.println("구분자와 양수로 구성되어있는 문자열을 입력하세요: ");
+        String input = Console.readLine();      // 사용자로부터 입력 받기
+        return new StringCalculatorDTO(input);  // 입력값을 DTO로 감싸서 반환
+    }
 }
