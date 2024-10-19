96a6faf123df87989bcf2fdc44a248c9ac41d7fa
Jeong yong hwan <baechoo0905@naver.com>
2024-10-19 13:08:20 +0900

    feat: 사용자 입력 기능 추가
    - Scanner를 사용하여 사용자로부터 문자열을 입력받는 기능을 구현
    - 입력된 문자열은 이후 계산 로직에서 사용될 예정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application app = new Application();
+        app.start();
+    }
+
+    private void start() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("덧셈할 문자열을 입력해 주세요: ");
+        String input = scanner.nextLine();
+
+        scanner.close();
     }
 }
