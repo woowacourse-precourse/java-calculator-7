c48c52121e499e4d00fada25fd4dcc844701400b
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2024-10-19 15:57:41 +0900

    fix: 커스텀 구분자 기능 - 이스케이프 문자 관련 오류 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,12 +3,17 @@ package calculator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Application {
 
-    static String[] setDelimiter( String input ) {
-        StringBuffer sb = new StringBuffer( input );
-        String[] delimiterList = { input.split( "\n", 2 )[0].substring(2), ",", ":" };
+    static String[] setDelimiter( String input ){
+        StringTokenizer tokenizer = new StringTokenizer( input, "\\n" );
+        if( tokenizer.countTokens() > 1 ){
+            String[] delimiterList = { tokenizer.nextToken().substring(2), ",", ":" };
+            return delimiterList;
+        }
+        String[] delimiterList = { ",", ":" };
         return delimiterList;
     }
 
@@ -16,10 +21,12 @@ public class Application {
         int result = 0;
         try{
             BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
+            System.out.println( "덧셈할 문자열을 입력해 주세요." );
             String input = bufferedReader.readLine();
-            String[] dellimiterList = setDelimiter( input );
+            String[] delimiterList = setDelimiter( input );
 
-            System.out.println( result );
+
+            System.out.println( "결과: " + result );
             bufferedReader.close();
         } catch (IOException e ){
             e.printStackTrace();
