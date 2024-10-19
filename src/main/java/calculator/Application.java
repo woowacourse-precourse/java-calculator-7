67f068fdafce5f644adf23dbcaa5bca077191d89
HwangBBang <h1009218@hufs.ac.kr>
2024-10-20 00:44:07 +0900

    feat: implement I/O functionality for feature 2

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,29 +4,39 @@ import calculator.string.RegexUtil;
 import calculator.string.StringCalculator;
 import camp.nextstep.edu.missionutils.Console;
 
+
 public class Application {
 
     private static final String DEFAULT_REGEX_ELEMENT1 = ",";
     private static final String DEFAULT_REGEX_ELEMENT2 = ":";
 
     public static void main(String[] args) {
-        /* 기본 구분자에 대한 기능 구현
+        /* 기본 구분자 + 커스텀 구분자 까지 기능 구현
         * */
 
         StringCalculator stringCalculator = new StringCalculator();
         RegexUtil regexUtil = new RegexUtil();
 
-        //todo: 커스텀 추가시 입력 이후로 이동
-        String regex = regexUtil.createRegex(DEFAULT_REGEX_ELEMENT1, DEFAULT_REGEX_ELEMENT2);
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputStr = Console.readLine();
+        String customDelim = regexUtil.parseCustomDelimiter(inputStr);
+
+        String regex;
+        if (customDelim == null) {
+            regex = regexUtil.createRegex(DEFAULT_REGEX_ELEMENT1, DEFAULT_REGEX_ELEMENT2);
+        } else {
+            regex = regexUtil.createRegex(DEFAULT_REGEX_ELEMENT1, DEFAULT_REGEX_ELEMENT2, customDelim);
+            inputStr = regexUtil.removeCustomDelimiter(inputStr);
+        }
 
         try {
             int result = stringCalculator.calculate(inputStr, regex);
             System.out.println("결과 : " + result);
+
         } catch (IllegalArgumentException ex) {
             System.err.println(ex.getMessage());
+
         }
 
     }
--- src/main/java/calculator/string/RegexUtil.java
+++ src/main/java/calculator/string/RegexUtil.java
@@ -1,10 +1,13 @@
 package calculator.string;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class RegexUtil {
+
+
 
     // 확장 가능한 메서드로 설계
     /** 요소들 통해서 정규식을 만들어주는 메서드
@@ -19,4 +22,34 @@ public class RegexUtil {
         return regex;
     }
 
+    /** 문자열에서 커스텀 구분자를 추출
+     * @param str 커스텀 구분자를 추출할 문자열
+     * @return 파싱된 구분자, 없으면 Null
+     */
+    public String parseCustomDelimiter(String str) {
+        Pattern pattern = Pattern.compile("//(.*)\\\\n(.*)");
+        Matcher matcher = pattern.matcher(str);
+
+        if (matcher.find()) {
+            String quote = matcher.group(1);// 구분자를 찾으면 반환
+            return quote;
+        }
+        return null;
+    }
+
+    /** 문자열에서 커스텀 구분자 제거 후 문자열 반환
+     * @param str 커스텀 구분자를 제거 할 문자열
+     * @return 커스텀 구분자 제거된 문자열
+     */
+    public String removeCustomDelimiter(String str) {
+
+        Pattern pattern = Pattern.compile("//(.*)\\\\n(.*)");
+        Matcher matcher = pattern.matcher(str);
+
+        if (matcher.find()) {
+            return matcher.group(2);
+        }
+        return str;
+    }
 }
+
