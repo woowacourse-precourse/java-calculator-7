f220be3381162985be8fb73fc0eb655fa960419e
JoonKyo <joonkyolee@gmail.com>
2024-10-20 00:17:17 +0900

    feat(SeparatorValidator): 커스텀 구분자를 추출하는 메서드 구현

--- src/main/java/calculator/domain/SeparatorValidator.java
+++ src/main/java/calculator/domain/SeparatorValidator.java
@@ -1,16 +1,22 @@
 package calculator.domain;
 
+import java.util.List;
+
 public class SeparatorValidator {
     private static final String CUSTOM_START_LETTERS = "//";
-    private static final String CUSTOM_END_LETTERS = "\\n";
+    private static final String CUSTOM_END_LETTERS = "\\n";출
 
     public SeparatorValidator() {
     }
 
-    public static String validateSeparator(String inputData) {
+    public static List<String> extractCustomSeparator(List<String> separator, String inputData) {
+        if (containsCustomSeparator(inputData)) {
+            separator.add(inputData.substring(CUSTOM_START_LETTERS.length(), inputData.indexOf(CUSTOM_END_LETTERS)));
+        }
+        return separator;
     }
 
-    private boolean containsCustomSeparator(String inputData) {
+    private static boolean containsCustomSeparator(String inputData) {
         return inputData.startsWith(CUSTOM_START_LETTERS) && inputData.contains(CUSTOM_END_LETTERS);
     }
 }
