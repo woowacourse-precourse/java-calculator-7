0a8b1c0a241a23bf5842eb5b4723f08b9622750a
alswnsp411 <alswnsp411@naver.com>
2024-10-20 01:04:43 +0900

    feat: 입력 문자열에서 구분자를 기준으로 숫자를 추출한다.

--- src/main/java/calculator/model/InputString.java
+++ src/main/java/calculator/model/InputString.java
@@ -1,5 +1,7 @@
 package calculator.model;
 
+import java.util.Arrays;
+
 /**
  * 사용자로부터 입력받은 문자열 관리 model
  */
@@ -25,6 +27,8 @@ public class InputString {
 
         String delimiters = this.customDelimiter == null ? ",|:" : customDelimiter + "|,|:";
 
+        long[] numberList = splitStringByDelimiters(delimiters);
+
         return this.resultSum;
     }
 
@@ -36,6 +40,20 @@ public class InputString {
             this.customDelimiter = inputString.substring(2, 3);
             this.inputString = inputString.substring(5);
         }
+    }
+
+    /**
+     * 문자열에서 숫자 추출
+     *
+     * @param delimiters 구분자(쉼표 (,)또는 콜론(:)또는 커스텀 구분자)
+     * @return 문자열에서 추출한 숫자 리스트
+     */
+    private long[] splitStringByDelimiters(String delimiters) {
+        String[] numbersInString = this.inputString.split(delimiters);
+
+        return Arrays.stream(numbersInString)
+                .mapToLong(Long::parseLong)
+                .toArray();
     }
 
 }
