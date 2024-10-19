1ddc533f965cee9584e6e91d761f8a2436cc7b2f
ppparkta <pppppark2@gmail.com>
2024-10-19 22:54:29 +0900

    feat(calculatorView): Console classs 내의 scanner 닫아줌

--- src/main/java/calculator/view/CalculatorView.java
+++ src/main/java/calculator/view/CalculatorView.java
@@ -5,7 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class CalculatorView {
     public String getUserInput() {
         System.out.println("덧셈할 문자열을 입력해주세요.");
-        return Console.readLine();
+        String inputString = Console.readLine();
+        Console.close();
+        return inputString;
     }
 
     public void printResult(int result) {
