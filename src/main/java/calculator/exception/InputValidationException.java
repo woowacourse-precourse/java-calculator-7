c8a6f11334a4f0e1f2304a00d4679d199cda49d2
Dev-LHY <dev.leehy@gmail.com>
2024-10-19 23:32:33 +0900

    refactor: void에서 Stirng으로 반환 형태 변경

--- src/main/java/calculator/exception/InputValidationException.java
+++ src/main/java/calculator/exception/InputValidationException.java
@@ -5,10 +5,11 @@ public class InputValidationException extends IllegalArgumentException{
         super(message);
     }
 
-    public static void isEmpty(String input) {
+    public static String isEmpty(String input) {
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException("아무것도 입력하지 않았습니다. 애플리케이션을 종료합니다.");
         }
+        return input;
     }
 
     public static void isPositiveNumber(int value) {
