7c56ca023a91865432e89cfc75ddb9d6f4de81ef
siu <siu31415@gmail.com>
2024-10-19 17:03:46 +0900

    feat: Application 출력 기능 추가, 예외 처리 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,5 +5,13 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String str = Console.readLine();
+
+        try{
+            int sum = StringCalculator.calculate(str);
+            System.out.println("결과 : " + sum);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
