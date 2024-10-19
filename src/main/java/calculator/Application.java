671843075a465d622db5e6872ed98718338278d5
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2024-10-20 06:02:01 +0900

    fix : main 문에 controller 와 service 등록하고 값을 입력받는 기능 구현
    controller 와 service 를 등록하고 매개변수로 입력을 받아서 수행할 수 있도록 구현.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,12 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculateService calculateService = new CalculateService(new NormalCalculatorImpl(), new SpecialCalculatorImpl());
+        CalculatorController controller = new CalculatorController(calculateService);
+
+        //입력을 받아서 수행할 수 있는 부분을 추가
+        if (args.length > 0) {
+            String input = args[0];
+            controller.processCalculation(input);
     }
 }
