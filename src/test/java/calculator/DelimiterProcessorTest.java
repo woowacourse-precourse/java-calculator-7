08839c1ab96bda0aa9d22b63495d59ad9e1fad63
JooIcksoo <dlrtn751@gmail.com>
2024-10-20 03:01:20 +0900

    test(DelimiterProcessorTest): 구분자 처리기 테스트 추가
    - 다중 커스텀 구분자 관련 테스트
    - 구분자가 없는 경우 관련 테스트

--- src/test/java/calculator/DelimiterProcessorTest.java
+++ src/test/java/calculator/DelimiterProcessorTest.java
@@ -20,7 +20,7 @@ class DelimiterProcessorTest {
     }
 
     @Test
-    void 다중_문자_구분자는_예외를_발생시킨다() {
+    void 다중_구분자는_예외를_발생시킨다() {
         // given
         String input = "//&^\n1;2;3";
 
@@ -30,7 +30,7 @@ class DelimiterProcessorTest {
     }
 
     @Test
-    void 커스텀_구분자가_없으면_구분자는_두_개다() {
+    void 커스텀_구분자가_없으면_기본_구분자는_두_개다() {
         // given
         String input = "1,2:3";
         DelimiterProcessor delimiterProcessor = new DelimiterProcessor(input);
@@ -42,4 +42,33 @@ class DelimiterProcessorTest {
         assertThat(delimiterProcessor.getDelimiterCount()).isEqualTo(2);
     }
 
+    @Test
+    void 다중_커스텀_구분자를_처리할_수_있다() {
+        // given
+        String input = "//;\n1;2;3//&\n4&5&6";
+
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> new DelimiterProcessor(input));
+    }
+
+    @Test
+    void 구분자가_없는_입력은_예외를_발생시킨다() {
+        // given
+        String input = "123";
+
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> new DelimiterProcessor(input));
+    }
+
+    @Test
+    void 기본_구분자만_있는_입력을_처리할_수_있다() {
+        // given
+        String input = "1,2:3";
+
+        // when
+        DelimiterProcessor delimiterProcessor = new DelimiterProcessor(input);
+
+        // then
+        assertThat(delimiterProcessor.getDelimiterCount()).isEqualTo(2); // 기본 구분자만 존재
+    }
 }
