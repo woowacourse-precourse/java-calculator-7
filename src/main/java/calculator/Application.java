88ed1e0e3c27b3db0fcec56712985029937af46a
sgy0917 <sgy0917@naver.com>
2024-10-20 01:28:45 +0900

    feat<Application>문자열을 변환해서 숫자를 더하는 로직을 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,14 +1,24 @@
 package calculator;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String userInput = Console.readLine();
+        String [] input = userInput.split("[,/]");
 
+        int num = 0;
+        for(int i =0; i<input.length; i++){
+            System.out.println(input[i]);
 
+            //input[i] 공백 처리;
+            if(!input[i].isEmpty()){
+                num += Integer.parseInt(input[i]);
 
-
-
-
-
+            }
+        }
+        System.out.println(num);
     }
 }
