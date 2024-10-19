48a8788d9da12f15303464370c423462513d4f93
lvalentine6 <ysr1227@naver.com>
2024-10-19 15:42:16 +0900

    refactor: change to constructor dependency injection

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,10 @@
 package calculator;
 
 public class Application {
-    public static void main(String[] args) {
-        CalculatorController calculatorController = new CalculatorController();
-        calculatorController.run();
-    }
+	public static void main(String[] args) {
+		CalculatorController calculatorController = CalculatorController.of(Calculator.create(),
+			InputHandler.from(ValidatingParser.create()), OutputHandler.create());
+
+		calculatorController.run();
+	}
 }
