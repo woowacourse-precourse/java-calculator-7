c889f43c4e5155239a60aac35a4d59f4134a033d
Seongmo Ahn <djgood780@gmail.com>
2024-10-19 23:03:37 +0900

    feat(Calculation): 입력 타입에 따른 문자열 슬라이싱 기능 추가

--- src/main/java/calculator/Calculation.java
+++ src/main/java/calculator/Calculation.java
@@ -2,7 +2,27 @@ package calculator;
 
 public class Calculation {
     public static int stringCalculation(InputData input){
-
+        switch(input.getInputType()) {
+            case InputType.EMPTY:
+                return 0;
+            case InputType.GENERAL:
+                sliceGeneralString(input);
+                break;
+            case InputType.CUSTOM:
+                sliceCustomString(input);
+                break;
+        }
         return -1;
     }
+
+    public static void sliceGeneralString(InputData input){
+
+
+    }
+
+    public static void sliceCustomString(InputData input){
+
+
+    }
+
 }
