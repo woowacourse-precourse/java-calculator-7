54ff9f0dfe1ec16eb37e36d880be7bb601d3fd28
himinqq <alsrud0966@naver.com>
2024-10-19 20:43:33 +0900

    refactor: 메서드 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,36 +8,15 @@ public class Application {
     public static void main(String[] args) {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-
         String input = Console.readLine();
 
         if(input.startsWith("//")){
-
-            String[] split = input.split("\\\\n");
-
-            String delimiter = split[0].replace("//", "");
-            String numbers = split[1];
-
-            input = numbers.replace(delimiter, ",");
+            input = normalizeDelimiter(input);
         }
 
         char[] charArray = input.toCharArray();
-        for(int i = 0; i< charArray.length; i=i+2){
-            if(!Character.isDigit(charArray[i])){
-                throw new IllegalArgumentException("숫자를 입력하세요.");
-            }
-
-            if(Character.getNumericValue(charArray[i]) < 0 ){
-                throw new IllegalArgumentException("양수를 입력하세요.");
-            }
-        }
-
-        for(int i =1; i<charArray.length; i=i+2){
-            if(charArray[i] != '|' || charArray[i] != ','){
-                throw new IllegalArgumentException("구분자 입력이 올바르지 않습니다.");
-            }
-        }
-
+        isPositiveNumber(charArray);
+        isDelimiter(charArray);
 
         String[] split1 = input.split(",|:");
 
@@ -47,4 +26,37 @@ public class Application {
 
         Console.close();
     }
+
+    private static String normalizeDelimiter(String input) {
+        String[] split = input.split("\\\\n");
+
+        String delimiter = split[0].replace("//", "");
+        String numbers = split[1];
+
+        input = numbers.replace(delimiter, ",");
+        return input;
+    }
+
+    private static void isDelimiter(char[] charArray) {
+        for(int i = 1; i< charArray.length; i=i+2){
+            char delimiter = charArray[i];
+            if(delimiter != '|' || delimiter != ','){
+                throw new IllegalArgumentException("구분자 입력이 올바르지 않습니다.");
+            }
+        }
+    }
+
+    private static void isPositiveNumber(char[] charArray) {
+        for(int i = 0; i< charArray.length; i=i+2){
+            char number = charArray[i];
+            if(!Character.isDigit(number)){
+                throw new IllegalArgumentException("숫자를 입력하세요.");
+            }
+
+            if(Character.getNumericValue(number) < 0 ){
+                throw new IllegalArgumentException("양수를 입력하세요.");
+            }
+        }
+    }
+
 }
