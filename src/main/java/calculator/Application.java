9a0b4f6cbbfa03b9e541505a1909ebe6d8efe6af
DongchannN <ldc991104@gmail.com>
2024-10-19 15:58:37 +0900

    feat: 연산 결과 출력하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,14 +3,17 @@ package calculator;
 import calculator.controller.CalculatorController;
 import calculator.service.CalculatorService;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         final InputView inputViewBean = new InputView();
+        final OutputView outputViewBean = new OutputView();
         final CalculatorService calculatorService = new CalculatorService();
 
         final CalculatorController calculatorControllerBean = new CalculatorController(
                 inputViewBean,
+                outputViewBean,
                 calculatorService
         );
 
--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -2,18 +2,24 @@ package calculator.controller;
 
 import calculator.service.CalculatorService;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class CalculatorController {
     private final InputView inputView;
+    private final OutputView outputView;
     private final CalculatorService calculatorService;
 
-    public CalculatorController(InputView inputView, CalculatorService calculatorService) {
+    public CalculatorController(InputView inputView,
+                                OutputView outputView,
+                                CalculatorService calculatorService) {
         this.inputView = inputView;
+        this.outputView = outputView;
         this.calculatorService = calculatorService;
     }
 
     public void run() {
         String rawInput = inputView.getCalculatorInput();
         String result = calculatorService.calculate(rawInput);
+        outputView.printCalculatorResult(result);
     }
 }
