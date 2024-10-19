5893bbcb6b745d461be924821daa8a655e6a9640
RIWON <121172065+awrion3@users.noreply.github.com>
2024-10-19 18:30:04 +0900

    refactor: 메서드 파라미터 변경 및 줄바꿈 추가

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -3,9 +3,10 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-    public String requestInput(StringData stringData) {
+    public String requestInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputString = Console.readLine();
+        
         return inputString;
     }
 }
