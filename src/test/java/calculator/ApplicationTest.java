7e3a1fe28fbddc6a6b8527d32916169100f31fba
sun <50648835+kimseongyu@users.noreply.github.com>
2024-10-19 16:07:18 +0900

    test: add test for various cases

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -14,13 +14,62 @@ class ApplicationTest extends NsTest {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
         });
+        assertSimpleTest(() -> {
+            run("//;\\n1,2:3;4");
+            assertThat(output()).contains("결과 : 10");
+        });
     }
 
     @Test
-    void 예외_테스트() {
+    void 빈_문자열_입력() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 다양한_숫자_입력() {
+        assertSimpleTest(() -> {
+            run("1,12,3");
+            assertThat(output()).contains("결과 : 16");
+        });
+        assertSimpleTest(() -> {
+            run("01,12,03");
+            assertThat(output()).contains("결과 : 16");
+        });
+    }
+
+
+    @Test
+    void 음수_예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 문자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2:3;4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;;\\n1,2:3;4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("/;\\n1,2:3;4"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
