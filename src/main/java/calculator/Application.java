314d1bceb9bdeae7a5fc1d881c5d4c3cbceddfe0
Lkk7380 <dlrlrhkd@naver.com>
2024-10-20 06:25:33 +0900

    feat : 구분자 처리(커스텀 포함)  로직 개선 및 split() 활용

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,18 +1,30 @@
 package calculator;
 
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public int stringAdd(String text) {
         if (text == null || text.isBlank()) {
             return 0;
         }
-        if (text.contains(",") || text.contains(":")) {
-            String[] numbers = text.split("[,:]");
-            int sum = 0;
-            for (String number : numbers) {
-                sum += Integer.parseInt(number);
-            }
-            return sum;
+        String[] numbers = text.split(",|:");
+        Pattern pattern = Pattern.compile("//(.)\n(.*)");
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            String customDelimiter = Pattern.quote(matcher.group(1));
+            numbers = matcher.group(2).split(customDelimiter);
         }
-        return Integer.parseInt(text);
+        return calculateSum(numbers);
+    }
+
+    private int calculateSum(String[] numbers) {
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
     }
 }
