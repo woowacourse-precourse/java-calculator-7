1b53aedbe01ee88ef830c43da6dcf6d7e67aeb33
siu <siu31415@gmail.com>
2024-10-19 16:56:14 +0900

    feat: StringCalculator.calculate() 수정, 기능 요구 사항 2 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -8,9 +8,12 @@ public class StringCalculator {
     public static int calculate(String str) {
         //정규식 이용
         Pattern p1 = Pattern.compile("^\\d*([,:]\\d+)*$");
-        Matcher m1 = p1.matcher(str);
+        Pattern p2 = Pattern.compile("^[/]{2}([^,:])\\\\n\\d*(\\1\\d+)*$");
 
+        Matcher m1 = p1.matcher(str);
+        Matcher m2 = p2.matcher(str);
         int ans = 0;
+
         if (m1.matches()) {// 1번 케이스
             String[] arr = str.split("[,:]");
             for (String s : arr) {
@@ -18,8 +21,20 @@ public class StringCalculator {
                     ans += Integer.parseInt(s);
                 }
             }
+
         }
-        else{}
+        else if (m2.matches()) {
+            String separator = str.substring(2, 3);
+            String[] arr = str.substring(5).split(separator);
+
+            for (String s : arr) {
+                if(s.matches("\\d+")) {
+                    ans += Integer.parseInt(s);
+                }
+            }
+        }
+        else {
+        }
 
         return ans;
     }
