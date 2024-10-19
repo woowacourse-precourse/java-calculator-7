907c63c02748f9bcfd3ad857c74f11e798e61c9d
김예서 <callikys@naver.com>
2024-10-20 01:51:49 +0900

    feat: 프로그램 실행시 동작 시킬 함수 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import static calculator.View.*;
+import static calculator.SumController.*;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO: 프로그램 구현
+
+        outputValue(checkValue(inputValue()));
     }
 }
