e8a4f20b098ffa326bd77cd91e3a9b4af3af1bd2
lejuho <zedd1324@gmail.com>
2024-10-19 15:33:21 +0900

    feat(parseSeparators) : add separator-extracting function
    add function, which returns set containing separators located between
    "//" and "\n", to help finding separators in string after "\n"

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,11 +9,35 @@ public class Application {
 
     public static void main(String[] args) {
         String s = readIn();
+        Set<String> separatorSet = parseSeparator(s);
+        System.out.println("Separators: " + separatorSet);
     }
 
     public static String readIn() {
         String new_in = Console.readLine();
+        if (new_in == null || new_in.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         return new_in;
     }
 
+    public static Set<String> parseSeparator(String input) {
+        Set<String> separatorSet = new HashSet<>();
+
+        if (input.startsWith("//")) {
+            for (int i = 2; i < input.length(); i++) {
+                if (input.charAt(i) == '\\') {
+                    if (i < input.length() - 1) {
+                        if(input.charAt(i+1) == 'n') break;
+                        separatorSet.add(input.substring(i, i + 1));
+                    }
+                }
+                else separatorSet.add(input.substring(i, i + 1));
+            }
+        }
+        separatorSet.add(",");
+        separatorSet.add(":");
+        return separatorSet;
+    }
 }
+
