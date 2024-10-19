0976640b9ab067de35fb3246d8c96efc4ce03eed
wvssm <neoramiregi@gmail.com>
2024-10-19 22:57:58 +0900

    refactor(CalculatorParser): 하나의 함수가 하나의 기능을 하도록 분리

--- src/main/java/calculator/domain/CalculatorParser.java
+++ src/main/java/calculator/domain/CalculatorParser.java
@@ -8,20 +8,19 @@ public class CalculatorParser {
     private final List<Character> delimiters = new ArrayList<>(Arrays.asList(':',','));
 
     public List<Double> parse(String sentence){
-        List<Double> operands;
-
-        if(sentence.startsWith("//")){
+        if(isDelimiterCustom(sentence)){
             processCustomDelimiter(sentence);
             sentence = removeCustomDelimiterDefinition(sentence);
         }
+        return parseAndValidateOperands(sentence);
+    }
 
-        operands = parseAndValidOperands(sentence);
-        return operands;
+    private boolean isDelimiterCustom(String sentence){
+        return sentence.startsWith("//");
     }
 
     private void processCustomDelimiter(String sentence) {
         String[] result = sentence.split("\n");
-
         if (result[0].length() != 3) {
             throw new IllegalArgumentException("커스텀 구분자가 1글자를 초과했습니다.");
         }
@@ -32,10 +31,10 @@ public class CalculatorParser {
         return sentence.substring(4);
     }
 
-    private List<Double> parseAndValidOperands(String sentence){
+    private List<Double> parseAndValidateOperands(String sentence){
         List<Double> operands = new ArrayList<>();
         double operand;
-        for(String word : sentence.split(getDelimiterRegex())){
+        for(String word : sentence.split(generateDelimiterRegex())){
             operand = parseOperand(word);
             validateOperand(operand);
             operands.add(operand);
@@ -43,9 +42,8 @@ public class CalculatorParser {
         return operands;
     }
 
-    private String getDelimiterRegex(){
+    private String generateDelimiterRegex(){
         StringBuilder delimiterRegex = new StringBuilder();
-
         for(int i=0; i<delimiters.size(); i++){
             delimiterRegex.append(delimiters.get(i));
             if(i == delimiters.size()-1){
@@ -69,6 +67,5 @@ public class CalculatorParser {
             throw new IllegalArgumentException("피연산자가 양수가 아닙니다.");
         }
     }
-
 }
 
