da448556d2afdf3ce8b65aef8368cbd6096f1eff
junhyunlim73 <limjh0703@gmail.com>
2024-10-19 20:13:30 +0900

    refactor: Calculator 클래스를 생성하여 Application 클래스와 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,66 +1,10 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
-        try {
-            // 입력받은 문자열로 덧셈 계산
-            int result = StringCalculate(input);
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println("예외 발생: " + e.getMessage());
-        }
-    }
-
-    private static int StringCalculate(String input) {
-        if(input.equals("") || input == null) {
-            return 0;
-        }
-
-        int sum = 0;
-
-        String delimiter = ",|:";
-
-        if (input.startsWith("//")) {
-            delimiter = parseDelimiter(input);  // 커스텀 구분자 추출
-            input = input.substring(input.indexOf("\\n") + 2);
-        }
-
-        String[] temps = input.split(delimiter);
-
-        for(String num : temps) {
-            int parsedNum = parseNumber(num);
-            sum += parsedNum;
-        }
-
-        return sum;
-    }
-
-    private static String parseDelimiter(String input) {
-        int startIndex = input.indexOf("//") + 2;
-        int endIndex = input.indexOf("\\n");
-
-        if (endIndex == -1) {
-            throw new IllegalArgumentException("구분자가 잘못되었습니다.");
-        }
-
-        return input.substring(startIndex, endIndex); // "//"와 "\n" 사이의 구분자 추출
-    }
-
-    private static int parseNumber(String num) {
-        try {
-            int parsedNum = Integer.parseInt(num);
-            if (parsedNum < 0) {
-                throw new IllegalArgumentException();
-            }
-            return parsedNum;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
-        }
+        Calculator calculator = new Calculator();
+        calculator.run();
     }
 
 }
