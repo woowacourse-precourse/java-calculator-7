884d586057de6db67d4c1ffbda3f7d4a51b45e8c
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-10-20 01:43:58 +0900

    feat(Input): 입력된 문자열 유효성 검사 메소드 추가
    입력된 문자열이 요구사항에 맞는 형식인지 유효성을 검사하기 위해 isValid 메소드 추가
    입력된 문자열의 처리와 관련되어 있는 메소드이기에 Input 클래스에 추가

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class Input {
     private final String input;
 
@@ -11,7 +13,33 @@ public class Input {
         return camp.nextstep.edu.missionutils.Console.readLine();
     }
 
+
     public String getInput() {
         return input;
+    }
+
+    public boolean isValid() {
+        if (input.isEmpty() || input.matches("\\d+")) {
+            return true;
+        } else {
+            if (input.matches("\\d+([,:]\\d+)*")) {
+                return true;
+            } else if (input.charAt(0) == '/') {
+                ArrayList<String> customDelimiters = Delimiter.getCustomDelimiters(input);
+                if (customDelimiters.isEmpty()) {
+                    return false;
+                } else {
+                    StringBuilder delimiterPattern = new StringBuilder(",:");
+                    for (String delimiter : customDelimiters) {
+                        delimiterPattern.append(delimiter);
+                    }
+                    String regex = "(//(.)\\\\n)+\\d+([" + delimiterPattern + "]\\d+)*";
+                    return input.matches(regex);
+                }
+
+            } else {
+                return false;
+            }
+        }
     }
 }
