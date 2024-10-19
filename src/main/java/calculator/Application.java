73ef849254872e3e20304493ee287ba9a4ba27e1
dhrgodms <dhrgodms123@gmail.com>
2024-10-19 16:05:16 +0900

    feat: JCAL_INPUT_01 - 문자열 입력받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,43 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        StringCalculator stringCalculator = new StringCalculator();
+        stringCalculator.run();
+    }
+
+    public static class StringCalculator {
+        // 구분자를 담을 문자 배열 (구분자가 문자열일 수 있는 경우의 수 고려)
+        ArrayList<String> delimiterArr;
+        ArrayList<Integer> extractedNumArr;
+
+        // 생성자
+        public StringCalculator() {
+            this.delimiterArr = new ArrayList<>();
+            this.extractedNumArr = new ArrayList<>();
+
+            // 기본 구분자
+            this.delimiterArr.add(":");
+            this.delimiterArr.add(",");
+        }
+
+        public void run() {
+            String input = getInput();
+        }
+
+        // 입력
+        public String getInput(){
+            // 입력 받기
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            return Console.readLine();
+        }
+
+
     }
 }
