126e841fdc471c1b5dc638f6774f61a7f5bf6147
Grayson1999 <bbt1250912@gmail.com>
2024-10-19 23:07:31 +0900

    feat: implement separator function for commas and colons
    입력 문자열에서 쉼표(,)와 콜론(:)을 기준으로 숫자를 분리하는 기능을 구현
    사용자로부터 입력 문자열을 받아는다.
    빈 문자열일 경우 빈 배열을 반환하고 아니라면 입력 문자열을 쉼표(,) 또는 콜론(:)으로 분리하고, 공백이 포함된 경우에도 숫자만 추출한다.
    추출한 숫자는 리스트나 배열로 반환한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,39 @@
 package calculator;
 
+import java.util.Arrays;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int[] arrayUserInput;
+        String userInput;
+        String numberLine;
+
+        userInput = readLine(); // userInput 입력
+
+        numberLine = userInput;
+        arrayUserInput = splitDelimiters(numberLine);
+        System.out.println(Arrays.toString(arrayUserInput));//쉼표 또는 콜론 구분자로 분리 된 리스트
+
+    }
+
+
+    public static int[] splitDelimiters(String numberLine) {
+        //빈 문자열이 입력 되었을 경우
+        if (numberLine.isEmpty()) {
+            return new int[]{};
+        }
+
+        String[] stringNumbers = numberLine.split("[,:]");
+        int[] numbers = new int[stringNumbers.length];
+
+        for (int i = 0; i < stringNumbers.length; i++) {
+            numbers[i] = Integer.parseInt(stringNumbers[i].trim());
+        }
+        return numbers;
     }
 }
+
+
+
