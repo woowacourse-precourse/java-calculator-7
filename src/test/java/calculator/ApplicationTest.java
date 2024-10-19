af121af6f01839ac4d07476ad4c010850bfafb55
Yehyeok Bang <qkddpgur318@gmail.com>
2024-10-19 23:22:22 +0900

    test: 커스텀 구분자 규칙 관련 테스트 작성
    커스텀 구분자 규칙을 어긴 입력에 대한 예외 처리 테스트를 추가합니다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,8 +19,56 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_1자_이상_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//ab\n1;2ab3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_2개_이상_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\n//@\\n1;2@3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 허용되지_않는_커스텀_구분자_사용_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//+\n1:2+3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자를_커스텀_구분자_사용_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//2\\n121"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 지정하지_않은_커스텀_구분자_사용_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_구분자를_커스텀_구분자로_사용_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//:\n1:2:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
