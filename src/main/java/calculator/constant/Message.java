2a24576c814db361718649878672718fea9fa3cc
ljhee92 <weilainib7@gmail.com>
2024-10-20 02:21:23 +0900

    feat: 상수 추가

--- src/main/java/calculator/constant/Message.java
+++ src/main/java/calculator/constant/Message.java
@@ -5,6 +5,7 @@ public class Message {
     public static final String WHITE_SPACE = " ";
     public static final String DEFAULT_VALUE_STR = "0";
     public static final String CUSTOM_SEPARATE_REGEX = "^//(.*)\\\\n(.*)";
+    public static final String SLASH = "/";
     public static final String FIRST_DEFAULT_SEPARATOR = ",";
     public static final String SECOND_DEFAULT_SEPARATOR = ":";
 } // class
