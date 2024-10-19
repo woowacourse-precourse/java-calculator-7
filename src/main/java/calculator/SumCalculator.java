cb549137c221a1c7d39bff96f7a5156c058eea5b
howareyoo <newworld121@naver.com>
2024-10-20 04:35:22 +0900

    feat : 숫자 덧셈
    문자열 List Integer로 변환

--- src/main/java/calculator/SumCalculator.java
+++ src/main/java/calculator/SumCalculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import static calculator.StringValidator.getSumString;
+import static calculator.StringValidator.userInputValidator;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,6 +10,19 @@ import java.util.stream.Collectors;
 public class SumCalculator {
 
     private SumCalculator() {
+    }
+
+    public static Integer getFinalResult(String userInput) {
+        List<String> delimiters;
+        delimiters = userInputValidator(userInput);
+
+        String sumString = getSumString(userInput, delimiters);
+
+        List<Integer> numbers = (getNumbers(sumString, delimiters)).stream().map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Integer> integerList = vaildateNumber(numbers);
+        return getSumInteger(integerList);
     }
 
     public static List<String> getNumbers(String sumString, List<String> delimiters) {
@@ -25,6 +41,14 @@ public class SumCalculator {
             }
         }
         return numbers;
+    }
+
+    public static Integer getSumInteger(List<Integer> numbers) {
+        Integer sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 
 
