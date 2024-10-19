a7262981bf8e46ed60f3ddb90b038d1ced44e5d9
jongseo <jongseo0703@naver.com>
2024-10-19 19:25:29 +0900

    feat: 입력기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.print("덧셈할 문자열을 입력해주세요 : ");
+        String input = Console.readLine(); // 사용자의 값을 입력받음
+
     }
 }
