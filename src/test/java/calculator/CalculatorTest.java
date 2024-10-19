13080e765906805915d59a56442cd3f3776d024c
sobaman <sobaman522@gmail.com>
2024-10-19 21:20:30 +0900

    test(calculator): add unit tests customConvert 의 단위 테스트 진행
    - 커스텀한 구분자를 하나만 설정하고 사용 시 합이 정상적으로 출력
    - 커스탐한 구분자를 여러 개 설정하고 합이 정상적으로 출력
    - 커스텀 구분자 양식을 지키지 않았을 때 에러 발생
    - 커스텀 구분자 설정 후 다른 구분자를 사용하였을 때 에러 발생

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -31,4 +31,54 @@ class CalculatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("잘못된 값을 입력하였습니다, 구분자로 \",\" 나 \":\" 를 사용하세요");
     }
+
+    @Test
+    @DisplayName("커스텀한 구분자를 하나만 설정하고 사용 시 합이 정상적으로 출력")
+    void customConvert() {
+        // given
+        Calculator calculator = new Calculator();
+        // when
+        int result = calculator.convert("//;\n3;3;3");
+        // then
+        Assertions.assertThat(result).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("커스텀한 구분자를 여러 개 설정하고 사용 시 합이 정상적으로 출력")
+    void multiCustomConvert() {
+        // given
+        Calculator calculator = new Calculator();
+        // when
+        int result = calculator.convert("//;j\n3;3j3");
+        // then
+        Assertions.assertThat(result).isEqualTo(9);
+    }
+
+
+    @Test
+    @DisplayName("커스텀 구분자 양식을 지키지 않았을 때 에러 발생")
+    void customConvertError() {
+        // given
+        Calculator calculator = new Calculator();
+        // when
+
+        // then
+        Assertions.assertThatThrownBy(() -> calculator.convert("//s\\3,3;3"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 형식입니다, 커스텀 구분자를 \"\\\" \"\n\" 사이에 넣으세요");
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 설정 후 다른 구분자를 사용하였을 때 에러 발생")
+    void customConvertError2() {
+        // given
+        Calculator calculator = new Calculator();
+        // when
+
+        // then
+        Assertions.assertThatThrownBy(() -> calculator.convert("//s\n3,3;3"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 값을 입력하였습니다, 설정한 구분자를 사용하세요");
+    }
+
 }
