43a6dc4a5b1ffd8f8de030a35da424a4591dc015
Ahnsukjoo <bos98@naver.com>
2024-10-20 01:23:41 +0900

    feat: Identifier 기능 구현
    - 문자열에서 커스텀 구분자를 식별해주는 기능 구현

--- src/main/java/calculator/domain/identifier/Identifier.java
+++ src/main/java/calculator/domain/identifier/Identifier.java
@@ -6,6 +6,12 @@ public class Identifier {
     private final String customKey2 = "\\n";
 
     public String findDelimiter(String string) {
-
-
+        if (string.startsWith(customKey1)) {
+            int customKeyEndIndex = string.indexOf(customKey2);
+            if (customKeyEndIndex == 3) {
+                return string.substring(customKey1.length(), customKeyEndIndex);
+            }
+        }
+        return null;
+    }
 }
