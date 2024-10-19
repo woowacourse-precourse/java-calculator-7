13d79b871a280cf377e8679a56ac74cdceb00012
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-20 03:03:03 +0900

    feat(StringProcessorController): 기본 구분자를 기준으로 분리하는 로직 호출부 추가
    해당 로직이 존재하는 DelimiterExtractor을 호출하기 위해서 의존성 주입
    입력받은 DTO를 넘겨준 뒤, 구분자 관련 로직 구현 예정

--- src/main/java/calculator/Controller/StringProcessorController.java
+++ src/main/java/calculator/Controller/StringProcessorController.java
@@ -1,4 +1,30 @@
 package calculator.Controller;
 
+import calculator.DTO.StringCalculatorDTO;
+import calculator.Service.DelimiterExtractor;
+import calculator.Service.InputReader;
+import java.util.List;
+
 public class StringProcessorController {
+    // 의존성 주입 이후, 불변성 보장
+    private final InputReader inputReader;
+    private final DelimiterExtractor delimiterExtractor;
+
+    // 생성자에서 외부로부터의 의존성 주입
+    public StringProcessorController(InputReader inputReader, DelimiterExtractor delimiterExtractor) {
+
+        this.inputReader = inputReader;
+        this.delimiterExtractor = delimiterExtractor;
+    }
+
+    // 전체 프로세스 조율
+    public void process() {
+        // 입력을 받아서 DTO 생성
+        StringCalculatorDTO inputDTO = inputReader.read();
+
+        // 문자열을 기본 구분자를 기준으로 분리하는 로직 호출
+        List<String> afterDefaultExtractor = delimiterExtractor.extractDelimiter(inputDTO);
+    }
+
+
 }
