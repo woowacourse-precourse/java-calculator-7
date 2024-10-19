3e84030affe17575adcd42ae0959ed584852e6ef
LSHGrandvoyage <2718sh0517@gm.gist.ac.kr>
2024-10-19 18:27:05 +0900

    First Code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # java-calculator-precourse
+
+## Check the function of calculator and what I need to handle
+
+### - 커스텀 구분자 존재 확인
+####    - 커스텀 구분자가 존재하지만 맨 앞에 위치하지 않는 경우
+####    - 커스텀 구분자의 length가 2 이상일 경우 
+####    - 커스텀 구분자가 존재할 때 기본 구분자 또한 사용 가능하게
+
+### - 구분자를 기준으로 split the input
+####    - Length가 0일 경우 바로 결과 출력
+
+### - input을 split한 결과 array의 elements add
+####    - 음수의 경우
+####    - 구분자에 포함되지 않은 문자가 존재하는 경우
--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,54 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int num;
+        int endex;
+        int sum = 0;
+        String sep = ",|:";
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        //String input = Console.readLine();
+        String input = "//!!!\n3,4!!!5:8";
+
+        if (input.startsWith("//")) {
+            endex = input.indexOf("\n");
+            if (endex == -1) {
+                throw new IllegalArgumentException("Unavailable format. Check your input.");
+            }
+            String c_sep = input.substring(2, endex);
+            if("(){}[]\\*+?^$|".contains(c_sep)){
+                c_sep = "\\" + c_sep;
+            }
+            sep = sep + "|" + c_sep;
+            input = input.substring(endex + 1);
+        }
+        else if(input.contains("//")){
+            throw new IllegalArgumentException("Custom Separator Grammar must be placed at the first.");
+        }
+
+        String[] value = input.split(sep);
+        if (value.length == 0) {
+            System.out.println("결과 : 0");
+            return;
+        } else {
+            for (String v : value) {
+                try {
+                    num = Integer.parseInt(v);
+                    if (num < 0) {
+                        throw new IllegalArgumentException("The number should be positive. Error number = " + num);
+                    }
+                    sum = sum + num;
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("Not allowed separator is used.");
+                }
+            }
+        }
+        System.out.println("결과 : " + sum);
+
     }
 }
