b5a6490cf21ad48b9e0f98cb669cec241a5a8229
whatsgoodg <rlwjddl1596@naver.com>
2024-10-19 21:08:50 +0900

    test(ArgumentResolverTest): ArgumentResolverTest 단위 테스트 수행
    수행한 테스트
    - TotalDelimitedInput 객체를 RequestDTO로 변환하고 각 필드가 일치한지 확인

---
+++ src/test/java/calculator/back/resolver/ArgumentResolverTest.java
@@ -0,0 +1,27 @@
+package calculator.back.resolver;
+
+import calculator.back.dto.RequestDTO;
+import calculator.front.input.CustomDelimiterParsedInput;
+import calculator.front.input.TotalDelimitedInput;
+import calculator.front.parser.CustomDelimiterParser;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ArgumentResolverTest {
+
+    @Test
+    @DisplayName("TotalDelimitedInput을 ReqeustDTO 객체로 변환")
+    void 객체_변환_테스트(){
+        //given
+        String rawInput = "//;\\n1:2:3";
+        ArgumentResolver argumentResolver = new ArgumentResolver();
+        //when
+        CustomDelimiterParsedInput customDelimiterParsedInput = CustomDelimiterParser.parse(rawInput);
+        TotalDelimitedInput totalDelimitedInput = TotalDelimitedInput.fromCustomDelimiterParseInput(customDelimiterParsedInput);
+        RequestDTO requestDTO = argumentResolver.resolve(totalDelimitedInput);
+        //then
+        assertThat(requestDTO.delimiters()).isEqualTo(totalDelimitedInput.getDelimiters());
+        assertThat(requestDTO.inputWithDelimiters()).isEqualTo(totalDelimitedInput.getInputWithDelimiters());
+    }
+}
