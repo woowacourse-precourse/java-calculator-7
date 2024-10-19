3337a0112c26fe0e9d32a56be6bf1e6fd4054304
neoskyclad <neoskyclad@naver.com>
2024-10-20 02:05:46 +0900

    feat(Application): add function that split user input by seperator and store as integer array
    Added new function convertStringToIntArray() to Application:
    - get string input and split it by the seperator
    - once splited, stores it to integer array
    - returns the integer array

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,11 +6,41 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input;
+        char[] seperatorArray = {',', ':'};
+        int[] numberArray;
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
         input = Console.readLine();    // 사용자로부터 문자열 입력 받기
+        numberArray = convertStringToIntArray(input, seperatorArray);
 
-        System.out.println(input);    //Debug
+        for (int i : numberArray) {
+            if (i == -1)
+                break;
+            System.out.println(i);
+        }
+    }
+
+    public static int[] convertStringToIntArray(String input, char[] seperator) {
+        int[] temp = new int[input.length()];
+
+        int lastIndex = 0;
+        int inputIndex = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            temp[i] = -1;
+
+            for (char c : seperator) {
+                if (input.charAt(i) == c) {
+                    temp[inputIndex++] = Integer.parseInt(input.substring(lastIndex, i));
+                    lastIndex = i + 1;
+                }
+            }
+            if (i == input.length() - 1) {
+                temp[inputIndex] = Integer.parseInt(input.substring(lastIndex));
+            }
+        }
+
+        return temp;
     }
 }
