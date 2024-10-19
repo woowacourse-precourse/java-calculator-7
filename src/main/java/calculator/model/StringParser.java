5eac798ace0b484ab80c8eee0f0e1d479def2aeb
yseo14 <lmslys14@naver.com>
2024-10-19 14:39:25 +0900

    feat: 커스텀 구분자 추출 기능 구현

---
+++ src/main/java/calculator/model/StringParser.java
@@ -0,0 +1,34 @@
+package calculator.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringParser {
+    private static final String INITIAL_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+    private static final String CUSTOM_DELIMITER_REGEX = "//([^0-9])\\\\n";
+    private static final String[] SPECIAL_CHARACTER = {"\\", "^", "$", ".", "|", "?", "*", "+", "(", ")", "{", "}", "[", "]"};
+    private static final String ESCAPE_STRING = "\\";
+
+    public String extractCustomDelimiter(String input) {    //  문자열에서 커스텀 구분자를 추출하는 메서드
+        Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            if (isSpecialChar(customDelimiter)) {
+                return ESCAPE_STRING + customDelimiter;
+            }
+            return customDelimiter;
+        } else {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+        }
+    }
+
+    public boolean isSpecialChar(String customDelimiter) {  //  escape 처리가 필요한 특수 문자인지 확인하는 메서드
+        return Arrays.asList(SPECIAL_CHARACTER).contains(customDelimiter);
+    }
+}
