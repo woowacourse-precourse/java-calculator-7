5fc8a82b1f895accd617374891eccc7a9eec532e
EHCTH <lees90896@gmail.com>
2024-10-20 00:56:47 +0900

    fix: 0.0이 안되게 수정

--- src/main/java/calculator/regex/constant/RegexConstant.java
+++ src/main/java/calculator/regex/constant/RegexConstant.java
@@ -11,7 +11,7 @@ public class RegexConstant {
     public static final String DOT = ".";
     public static final String DOT_REGEX = "\\.";
     public static final Pattern ONLY_NUMBER_SEPARATOR_REGEX = Pattern.compile("[1-9]\\d*([,:][1-9]\\d*)*");
-    public static final Pattern ONLY_NUMBER_SEPARATOR_DOUBLE_REGEX = Pattern.compile("^0\\.\\d*[1-9]([,:]\\d+(\\.\\d+)?)*(?![,:])$|^\\d*[1-9]\\d*(\\.\\d+)?([,:]\\d+(\\.\\d+)?)*(?![,:])$");
+    public static final Pattern ONLY_NUMBER_SEPARATOR_DOUBLE_REGEX = Pattern.compile("^(?:0\\.(?!0+([,:]|$))\\d+|[1-9]\\d*(?:\\.\\d+)?)(?:[,:](?:0\\.(?!0+([,:]|$))\\d+|[1-9]\\d*(?:\\.\\d+)?))*$");
     public static final Pattern CUSTOM_SEPARATOR_REGEX = Pattern.compile("^//([^\\d\\s])\\\\n[1-9]\\d*(\\1[1-9]\\d*)*$");
     public static final Pattern CUSTOM_SEPARATOR_DOUBLE_REGEX = Pattern.compile("^//([^\\d\\s.])\\\\n(0\\.(?!0+($|\\D))\\d+|[1-9]\\d*(\\.\\d+)?)(\\1(0\\.(?!0+($|\\D))\\d+|[1-9]\\d*(\\.\\d+)?))*$");
     public static final Pattern BLANK_REGEX = Pattern.compile("^$");
