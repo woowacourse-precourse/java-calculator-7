9e2d954b139824f5276121d4b87193a9c476c5c3
HSK021843 <celestielk@gmail.com>
2024-10-19 20:37:15 +0900

    feat(calculator): add string handler and update README
    - Update README.md to include function lists.
    - Implement string handler function using readLine() from the library.
    - The function returns 0 if the input string is empty, in other case, it
      returns 1.

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+    public static int calculate(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+
+        return 1;
+    }
+}
