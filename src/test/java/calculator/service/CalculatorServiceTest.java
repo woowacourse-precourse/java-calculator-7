f27bf79c15cc835e6bb83b8f37361accc67b4bac
Jewan1120 <hawaii1468@gmail.com>
2024-10-19 22:41:26 +0900

    test(service): 구분자를 이용해서 문자열 분리하는 테스트 작성
    입력된 문자열에서 기존 구분자와 커스텀 구분자로 문자열을 구분하는 로직에 대한 테스트를 작성했습니다.

---
+++ src/test/java/calculator/service/CalculatorServiceTest.java
@@ -0,0 +1,137 @@
+package calculator.service;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorServiceTest {
+
+    CalculatorService calculatorService = new CalculatorService();
+
+    @Test
+    void 기본_구분자로_분리() {
+        String inputString = "1,2:3";
+        String delimiter = ",:";
+
+        List<String> expected = List.of("1", "2", "3");
+
+        Assertions.assertEquals(calculatorService.splitInputWithDelimiter(inputString, delimiter), expected);
+    }
+
+    @Test
+    void 잘못된_구분자로_분리() {
+        String inputString = "1,2;3";
+        String delimiter = ",:";
+
+        List<String> expected = List.of("1", "2;3");
+
+        Assertions.assertEquals(calculatorService.splitInputWithDelimiter(inputString, delimiter), expected);
+    }
+
+    @Test
+    void 문자열에서_커스텀_구분자_제외() {
+        String inputString = "//;\\n1,2;3:4";
+        String delimiter = ",:";
+        if (calculatorService.haveCustomDelimiter(inputString)) {
+            delimiter = calculatorService.addCustomDelimiter(inputString, delimiter);
+            inputString = calculatorService.extractInput(inputString, delimiter);
+        }
+
+        String expected = "1,2;3:4";
+
+        Assertions.assertEquals(inputString, expected);
+    }
+
+    @Test
+    void 커스텀_기본_구분자로_분리() {
+        String inputString = "//;\\n1,2;3:4";
+        String delimiter = ",:";
+        if (calculatorService.haveCustomDelimiter(inputString)) {
+            delimiter = calculatorService.addCustomDelimiter(inputString, delimiter);
+            inputString = calculatorService.extractInput(inputString, delimiter);
+        }
+
+        List<String> expected = List.of("1", "2", "3", "4");
+
+        Assertions.assertEquals(calculatorService.splitInputWithDelimiter(inputString, delimiter), expected);
+    }
+
+    @Test
+    void 공백_커스텀_구분자로_분리() {
+        String inputString = "// \\n1 2 3 4";
+        String delimiter = ",:";
+        if (calculatorService.haveCustomDelimiter(inputString)) {
+            delimiter = calculatorService.addCustomDelimiter(inputString, delimiter);
+            inputString = calculatorService.extractInput(inputString, delimiter);
+        }
+
+        List<String> expected = List.of("1", "2", "3", "4");
+
+        Assertions.assertEquals(calculatorService.splitInputWithDelimiter(inputString, delimiter), expected);
+    }
+
+    @Test
+    void 커스텀_구분자_중복_입력() {
+        String inputString = "//;\\n1//'\\n,2;3:4";
+        String delimiter = ",:";
+        if (calculatorService.haveCustomDelimiter(inputString)) {
+            delimiter = calculatorService.addCustomDelimiter(inputString, delimiter);
+            inputString = calculatorService.extractInput(inputString, delimiter);
+        }
+
+        List<String> expected = List.of("1//'\\n", "2", "3", "4");
+
+        Assertions.assertEquals(calculatorService.splitInputWithDelimiter(inputString, delimiter), expected);
+    }
+
+    @Test
+    void 빈_문자열_구분() {
+        String inputString = "";
+        String delimiter = ",:";
+
+        List<String> expected = List.of();
+
+        Assertions.assertEquals(calculatorService.splitInputWithDelimiter(inputString, delimiter), expected);
+    }
+
+    @Test
+    void 중간_빈_문자열_구분() {
+        String inputString = "1,,2:3";
+        String delimiter = ",:";
+
+        List<String> expected = List.of("1", "2", "3");
+
+        Assertions.assertEquals(calculatorService.splitInputWithDelimiter(inputString, delimiter), expected);
+    }
+
+    @Test
+    void 공백_문자열_구분() {
+        String inputString = " ";
+        String delimiter = ",:";
+
+        List<String> expected = List.of(" ");
+
+        Assertions.assertEquals(calculatorService.splitInputWithDelimiter(inputString, delimiter), expected);
+    }
+
+    @Test
+    void 중간_공백_문자열_구분1() {
+        String inputString = "1, ,2:3";
+        String delimiter = ",:";
+
+        List<String> expected = List.of("1", " ", "2", "3");
+
+        Assertions.assertEquals(calculatorService.splitInputWithDelimiter(inputString, delimiter), expected);
+    }
+
+    @Test
+    void 중간_공백_문자열_구분2() {
+        String inputString = "1, 2:3";
+        String delimiter = ",:";
+
+        List<String> expected = List.of("1", " 2", "3");
+
+        Assertions.assertEquals(calculatorService.splitInputWithDelimiter(inputString, delimiter), expected);
+    }
+
+}
