f5cbe1a3a3b6b7b23593c29a0abecf6a66dbcaee
imjunghun <dlawndgns9@naver.com>
2024-10-19 18:56:49 +0900

    refactor(SeparatorService): service가 인터페이스에만 의존할 수 있도록 리팩토링

--- src/main/java/calculator/service/separator/SeparatorService.java
+++ src/main/java/calculator/service/separator/SeparatorService.java
@@ -1,56 +1,11 @@
 package calculator.service.separator;
 
 import calculator.domain.number.Numbers;
-import calculator.domain.separator.BasicSeparator;
-import calculator.domain.separator.CustomSeparator;
 import calculator.domain.separator.Separator;
-import java.util.Arrays;
-import java.util.Optional;
 
 public class SeparatorService {
 
-    private static final String PREFIX_CUSTOM_SEPARATOR = "//";
-    private static final String SUFFIX_CUSTOM_SEPARATOR = "\\\\n";
-
-    public Numbers separate(String input) {
-        return generate(input).separate(input);
-    }
-
-    private Separator generate(String input) {
-        if (hasCustomSeparator(input)) {
-            return new CustomSeparator(extractCustomSeparatorIn(input));
-        }
-
-        return new BasicSeparator();
-    }
-
-    private String extractCustomSeparatorIn(String input) {
-        return extractSeparator(removePrefix(splitBySuffix(input)));
-    }
-
-    private String extractSeparator(Optional<String> input) {
-        if (input.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-        return input.get();
-    }
-
-    private Optional<String> removePrefix(String[] inputs) {
-        return Arrays.stream(inputs)
-                .filter(this::isPrefixIn)
-                .map(value -> value.substring(PREFIX_CUSTOM_SEPARATOR.length()))
-                .findFirst();
-    }
-
-    private String[] splitBySuffix(String input) {
-        return input.split(SUFFIX_CUSTOM_SEPARATOR);
-    }
-
-    private boolean isPrefixIn(String input) {
-        return input.contains(PREFIX_CUSTOM_SEPARATOR);
-    }
-
-    private boolean hasCustomSeparator(String value) {
-        return value.startsWith(PREFIX_CUSTOM_SEPARATOR);
+    public Numbers separate(Separator separator, String input) {
+        return separator.separate(input);
     }
 }
--- src/test/java/calculator/service/separator/SeparatorServiceTest.java
+++ src/test/java/calculator/service/separator/SeparatorServiceTest.java
@@ -1,9 +1,11 @@
 package calculator.service.separator;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import calculator.domain.number.Number;
 import calculator.domain.number.Numbers;
+import calculator.domain.separator.BasicSeparator;
+import calculator.domain.separator.CustomSeparator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -18,17 +20,20 @@ class SeparatorServiceTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "\\n;//1;2;3")
-    void 커스텀구분자예외(String input) {
-        assertThatThrownBy(() -> service.separate(input))
-                .isInstanceOf(IllegalArgumentException.class);
+    @ValueSource(strings = {"1,2:3", "1,2,3", "1:2:3"})
+    void 기본구분자_정상작동(String input) {
+        Numbers numbers = service.separate(new BasicSeparator(), input);
+
+        assertThat(numbers.getNumbers().size()).isEqualTo(3);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"1,2:3", "//;\\n1;2;3", "1,2,3", "1:2:3"})
-    void 정상작동(String input) {
-        Numbers numbers = service.separate(input);
+    @ValueSource(strings = "")
+    void 커스텀구분자_정상작동(String input) {
+        Numbers numbers = service.separate(new CustomSeparator(";"), input);
+        Number number = numbers.getNumbers().getFirst();
 
-        assertThat(numbers.getNumbers().size()).isEqualTo(3);
+        assertThat(numbers.getNumbers().size()).isEqualTo(1);
+        assertThat(number.getValue()).isEqualTo(0);
     }
 }
