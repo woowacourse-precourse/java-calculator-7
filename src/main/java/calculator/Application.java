040703c8c774f4a5081fe7568ca1ec4e83d44388
soonhong99 <nanayah99@naver.com>
2024-10-19 16:55:22 +0900

    refactor(input): BufferedReader에서 제공된 Console API로 입력
    - camp.nextstep.edu.missionutils.Console API 도입
    - Console.readLine() 메소드를 사용하여 사용자 입력 처리
    - 기존 BufferedReader 관련 코드 제거
    
    Signed-off-by: soonhong99 <nanayah99@naver.com>

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,21 +1,15 @@
 package calculator;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String userInput = null;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String userInput = Console.readLine();
+        StringCalculator sc = new StringCalculator(userInput);
 
         try {
-            userInput = br.readLine();
-            StringCalculator sc = new StringCalculator(userInput);
             sc.calculate();
-        } catch (IOException e) {
-            System.err.println("입력 오류: " + e.getMessage());
         } catch (Exception e) {
             System.err.println("예기치 못한 오류: " + e.getMessage());
         }
