cccd5cb0dc585c5bf046da3d2645a353e3121ee7
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-20 04:04:23 +0900

    refactor: 사용한 자원 반환하는 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        Calculator.Run();
+        try {
+            Reader.printInputMessage();
+            String input = Console.readLine();
+            List<Integer> list = Parser.parse(input);
+            int sum = Calculator.sum(list);
+            Printer.print(sum);
+        } finally {
+            Console.close();
+        }
     }
 }
