513522993ae608555b3e5c7743300eefd7304cb9
wodus4145@naver.com <wodus4145@naver.com>
2024-10-19 14:29:46 +0900

    refactor: Change the name of methods

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,14 +11,14 @@ public class Application {
         return  sum;
     }
 
-    public int Separate(String str) {
+    public int separate(String str) {
         int answer = 0;
         String[] strArr = str.split(",|:");
         answer = addition(strArr);
         return  answer;
     }
 
-    public int Custom(String str) {
+    public int custom(String str) {
         int answer = 0;
         String delimiter = str.substring(2,str.indexOf("\n"));
         String str2 = str.substring(str.indexOf("\n")+1);
@@ -32,11 +32,11 @@ public class Application {
         String str = Console.readLine();
         try {
             if(str.startsWith("//")) {
-                System.out.println(app.Custom(str));
+                System.out.println(app.custom(str));
             } else if(str!=null && str.isEmpty()) {
                 System.out.println("0");
             } else {
-                System.out.println(app.Separate(str));
+                System.out.println(app.separate(str));
             }
         } catch (IllegalArgumentException e) {
             return;
