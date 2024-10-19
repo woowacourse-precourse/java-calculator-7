add7d220eccbc2eed0e00a8a3465e64237ab5731
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-19 16:51:49 +0900

    fix(Splitter): splitByCustomSeparator 메서드 - 커스텀 구분자가 없으면 바로 종료하도록 변경

--- src/main/java/calculator/Splitter.java
+++ src/main/java/calculator/Splitter.java
@@ -2,6 +2,7 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Splitter {
 
@@ -11,8 +12,15 @@ public class Splitter {
     }
 
     public List<String> splitByCustomSeparator(String value, List<String> customSeparators) {
+        if (customSeparators.isEmpty()) {
+            return List.of(value);
+        }
+
         StringBuilder sb = new StringBuilder();
-        customSeparators.forEach(s -> sb.append(s).append("|"));
+
+        customSeparators.forEach(s -> sb.append(Pattern.quote(s)).append("|"));
+
+        // 마지막 구분자의 '|' 제거
         sb.deleteCharAt(sb.length() - 1);
 
         return Arrays.stream(value.split(sb.toString())).toList();
