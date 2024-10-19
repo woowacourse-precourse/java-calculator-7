702a23a1883d877d12685a118eca2d339de5939b
HongSeongJo <164273757+HongSeongJo@users.noreply.github.com>
2024-10-20 02:14:40 +0900

    feat(parser): 커스텀 구분자와 숫자를 분리한다

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -1,11 +1,31 @@
 package calculator;
 
-import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
+    private static final String DEFAULT_DELIMITERS = "[,:]";
     public String[] split(String input) {
-        String delimiter = "[,:]";
+        StringBuilder delimiter = new StringBuilder(DEFAULT_DELIMITERS);
 
-        return input.split(delimiter);
+        Pattern pattern = Pattern.compile("//(.)\n");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+
+            if (customDelimiter.isEmpty()) {
+                throw new IllegalArgumentException("커스텀 구분자가 입력되지 않았습니다.");
+            }
+            if (customDelimiter.length() != 1) {
+                throw new IllegalArgumentException("커스텀 구분자는 하나의 문자여야 합니다.");
+            }
+
+            delimiter.append("|").append(Pattern.quote(customDelimiter));
+
+            input = input.substring(matcher.end());
+        }
+
+        return input.split(delimiter.toString());
     }
 }
