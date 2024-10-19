829e179888fb5477779bf3245de9f65b8d4cfbf5
lvalentine6 <ysr1227@naver.com>
2024-10-19 16:31:25 +0900

    fix: edit constant name

--- src/main/java/calculator/service/Calculator.java
+++ src/main/java/calculator/service/Calculator.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Calculator {
 	private static final String ERROR_MESSAGE_NEGATIVE_NUMBER = "음수는 계산할 수 없습니다.";
-	private static final String ERROR_OVERFLOW_SUM = "계산 범위를 초과 하였습니다.";
+	private static final String ERROR_MESSAGE_OVERFLOW_SUM = "계산 범위를 초과 하였습니다.";
 
 	private Calculator() {
 	}
@@ -31,7 +31,7 @@ public class Calculator {
 
 	private void validateNoOverflow(int sum, int number) {
 		if (sum > Integer.MAX_VALUE - number) {
-			throw new IllegalArgumentException(ERROR_OVERFLOW_SUM);
+			throw new IllegalArgumentException(ERROR_MESSAGE_OVERFLOW_SUM);
 		}
 	}
 }
