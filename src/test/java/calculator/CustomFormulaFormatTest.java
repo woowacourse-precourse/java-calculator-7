5ca33cfb1a390ddef06e5ea8bc53bd8d2d72522d
GaBaljaintheroom <junsu1222@naver.com>
2024-10-19 15:49:27 +0900

    style(CustomFormulaFormatTest): import static 위치 수정

--- src/test/java/calculator/CustomFormulaFormatTest.java
+++ src/test/java/calculator/CustomFormulaFormatTest.java
@@ -1,7 +1,8 @@
 package calculator;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CustomFormulaFormatTest {
 
