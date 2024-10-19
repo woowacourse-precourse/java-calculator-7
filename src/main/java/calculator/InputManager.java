16b97f9d68f02fbbbaf0c7ff815798c57d1ee0b5
hongkikii <hyangmeeh@icloud.com>
2024-10-19 19:31:37 +0900

    style(InputManger): 메서드 정의 순서 변경
    get 메서드를 생성자 아래 위치로 설정

--- src/main/java/calculator/InputManager.java
+++ src/main/java/calculator/InputManager.java
@@ -9,19 +9,6 @@ public class InputManager {
         this.input = input;
     }
 
-    public void validate() {
-        if(input.matches("[1-9,:]+")) {
-            return;
-        }
-        if(!(input.startsWith("//") && input.contains("\\n"))) {
-            throw new IllegalArgumentException("입력 형식이 유효하지 않습니다.");
-        }
-    }
-
-    public boolean isCustomSeparatorCandidateExisted() {
-        return this.input.startsWith("//");
-    }
-
     public String getSeparatorCandidate() {
         if(separatorCandidate == null) {
             int startIndex = input.indexOf("//") + 2;
@@ -40,5 +27,18 @@ public class InputManager {
             operandCandidates = input.substring(startIndex);
         }
         return this.operandCandidates;
+    }
+
+    public void validate() {
+        if(input.matches("[1-9,:]+")) {
+            return;
+        }
+        if(!(input.startsWith("//") && input.contains("\\n"))) {
+            throw new IllegalArgumentException("입력 형식이 유효하지 않습니다.");
+        }
+    }
+
+    public boolean isCustomSeparatorCandidateExisted() {
+        return this.input.startsWith("//");
     }
 }
