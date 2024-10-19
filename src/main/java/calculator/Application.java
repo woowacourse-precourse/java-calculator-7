877d7a8ba84feb91446d73e414c8d6dc2f86687e
LimJaeEun0930 <je9513666@gmail.com>
2024-10-19 23:05:30 +0900

    test(Calculator): add Test

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,7 +8,7 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
         int sum = Calculator.add(input);
-        System.out.println(sum);
+        System.out.println("결과 : " + sum);
         close();
     }
 }
