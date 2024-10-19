3eee3b6f70451bee711036b19232ddd4a6573ac7
ppparkta <pppppark2@gmail.com>
2024-10-19 22:37:56 +0900

    refactor(CalculatorController): run 메서드 controller로 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,14 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.model.CalculatorModel;
+import calculator.view.CalculatorView;
+
 public class Application {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.run();
+        CalculatorModel model = new CalculatorModel();
+        CalculatorView view = new CalculatorView();
+        CalculatorController controller = new CalculatorController(model, view);
+        controller.run();
     }
 }
--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,25 @@
 package calculator.controller;
 
+import calculator.model.CalculatorModel;
+import calculator.utils.Parser;
+import calculator.view.CalculatorView;
+
 public class CalculatorController {
+    private final CalculatorModel model;
+    private final CalculatorView view;
+    private final Parser parser;
+
+    public CalculatorController(CalculatorModel model, CalculatorView view) {
+        this.model = model;
+        this.view = view;
+        this.parser = new Parser();
+    }
+
+    public void run() {
+        String inputString = view.getUserInput();
+        parseCustomSeparator();
+        addBasicSeparator();
+        parseOperand();
+        view.printResult();
+    }
 }
