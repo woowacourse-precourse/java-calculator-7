df83af129e96673d5b29c3f2c2a7d74c0505988e
ruudska6 <feels19@naver.com>
2024-10-19 12:34:17 +0900

    <refactor>: StringArrToIntegerArr 사용자로부터 받은 입력값의 인자가 공백이라면 0으로 대체

--- src/main/java/calculator/service/StringArrToIntegerArr.java
+++ src/main/java/calculator/service/StringArrToIntegerArr.java
@@ -5,7 +5,11 @@ public class StringArrToIntegerArr {
     public static int[] toNumbers(String[] strArr) {
         int[] numbers = new int[strArr.length];
         for (int i = 0; i < strArr.length; i++) {
-            numbers[i] = Integer.parseInt(strArr[i]);
+            if (strArr[i].isEmpty()) {
+                numbers[i] = 0;
+            } else if (!strArr[i].isEmpty()) {
+                numbers[i] = Integer.parseInt(strArr[i]);
+            }
         }
         return numbers;
     }
