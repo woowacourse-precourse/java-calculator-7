ff143457ccd315ddcef05074985b58be5b35be81
junhaa <2171326@hansung.ac.kr>
2024-10-19 23:57:59 +0900

    style(DelimiterTokenizer): 주석 추가, 코드 포맷팅
    - 클래스, 메서드, 변수에 대한 주석 추가

--- src/main/java/calculator/domain/DelimiterTokenizer.java
+++ src/main/java/calculator/domain/DelimiterTokenizer.java
@@ -4,16 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * 구분자를 기준으로 문자열을 자른 후, 정수 값으로 변환하는 클래스
+ */
 public class DelimiterTokenizer {
 
+    /**
+     * 구분자를 기준으로 문자열을 자르고,
+     * 잘린 문자들을 정수로 변환한 후,
+     * List에 담아서 전달
+     *
+     * @param delimiterResult 구분자 추출 후 결과 값
+     * @return 변환된 정수 형태의 List
+     */
     public List<Integer> getNumbers(DelimiterResult delimiterResult) {
-        List<Integer> numbers = new ArrayList<>();
-        String numberInput = delimiterResult.getNumberString();
-        Set<Character> delimiters = delimiterResult.getDelimiters();
-        String buffer = "";
+        List<Integer> numbers = new ArrayList<>(); // 반환할 정수 List
+        String numberInput = delimiterResult.getNumberString(); // 구분자를 기준으로 자를 문자열
+        Set<Character> delimiters = delimiterResult.getDelimiters(); // 구분자 목록
+        String buffer = ""; // 문자열 형태의 정수 buffer
 
         for (int i = 0; i < numberInput.length(); i++) {
-            Character curChar = numberInput.charAt(i);
+            Character curChar = numberInput.charAt(i); // 현재 index의 문자
+
             // 현재 문자가 숫자인 경우
             if (Character.isDigit(curChar)) {
                 buffer += curChar;
@@ -27,11 +39,14 @@ public class DelimiterTokenizer {
                 buffer = "";
             }
         }
-        // 남은 buffer check
+
+        // 남은 buffer check, 문자열의 마지막이 숫자인 경우 버퍼에 남게된다.
         if (buffer.isBlank()) {
             throw new IllegalArgumentException("입력이 올바르지 않습니다.");
         }
+
         numbers.add(Integer.parseInt(buffer));
+
         return numbers;
     }
 }
