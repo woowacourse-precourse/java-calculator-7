abb130a89311d9566fe2aff25cc6bdc1b8598aa7
ruudska6 <feels19@naver.com>
2024-10-19 13:17:56 +0900

    <refactor>: StringParsing 긴 커스텀 구분자 사용 가능 하도록 변경

--- src/main/java/calculator/service/StringParsing.java
+++ src/main/java/calculator/service/StringParsing.java
@@ -3,16 +3,17 @@ package calculator.service;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class StringParsing {
-
     public static String[] Delimiter(String strInput) {
-        String delimiter = ",|:";
+        String delimiter = ",|:"; // 기본 구분자
         strInput = strInput.replace("\\n", "\n");
-        Matcher m = Pattern.compile("//(.*?)\\n(.*)").matcher(strInput);
+
+        Matcher m = Pattern.compile("//(.*?)\\n(.*)", Pattern.DOTALL).matcher(strInput);
         if (m.find()) {
-            delimiter = delimiter + "|" + Pattern.quote(m.group(1));
-            strInput = m.group(2);
+            String delimPart = m.group(1); // 커스텀 구분자 부분
+            strInput = m.group(2);         // 숫자 부분
+
+            delimiter += "|" + Pattern.quote(delimPart);
         }
         return strInput.split(delimiter);
     }
