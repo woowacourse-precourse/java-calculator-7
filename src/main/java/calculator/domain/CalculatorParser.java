b3b366af94c38502fb5d01a70e53767619491274
wvssm <neoramiregi@gmail.com>
2024-10-20 00:04:35 +0900

    refactor(CalculatorParser): Pattern, Matcher 클래스를 이용하여 커스텀 구분자 및 문자열 추출하도록 수정

--- src/main/java/calculator/domain/CalculatorParser.java
+++ src/main/java/calculator/domain/CalculatorParser.java
@@ -3,39 +3,43 @@ package calculator.domain;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CalculatorParser {
     private final List<Character> delimiters = new ArrayList<>(Arrays.asList(':',','));
 
-    public List<Double> parse(String sentence){
-        if(isDelimiterCustom(sentence)){
-            parseAndValidCustomDelimiter(sentence);
-            sentence = removeCustomDelimiterDefinition(sentence);
+    public List<Double> parse(String input){
+        if(isDelimiterCustom(input)){
+            input = parseAndValidCustomDelimiter(input);
         }
-        return parseAndValidateOperands(sentence);
+        return parseAndValidateOperands(input);
     }
 
     private boolean isDelimiterCustom(String input){
         return input.startsWith("//");
     }
 
-    private void parseAndValidCustomDelimiter(String input) {
-        String regex = "^//(.)\\n.*";
+    private String parseAndValidCustomDelimiter(String input) {
+        String regex = "^//(.)\\\\n(.*)";
 
-        if(input.matches(regex)){
-            char delimiter = input.charAt(2);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        if(matcher.find()){
+            char delimiter = matcher.group(1).charAt(0);
+            String pureInput = matcher.group(2);
+
             if(Character.isDigit(delimiter)){
                 throw new IllegalArgumentException("숫자는 구분자로 사용할 수 없습니다.");
             }
             delimiters.add(delimiter);
+            return pureInput;
         } else{
             throw new IllegalArgumentException("구분자는 길이가 1인 문자여야 합니다.");
         }
     }
 
-    private String removeCustomDelimiterDefinition(String sentence){
-        return sentence.substring(4);
-    }
 
     private List<Double> parseAndValidateOperands(String sentence){
         List<Double> operands = new ArrayList<>();
@@ -62,6 +66,7 @@ public class CalculatorParser {
 
     private double parseOperand(String word){
         try{
+            System.out.println(word);
             return Double.parseDouble(word);
         } catch (NumberFormatException e){
             throw new IllegalArgumentException("피연산자가 숫자가 아닙니다.");
