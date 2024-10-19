4d32a397f6c31d5c0dd9b1ddf93cb08784898052
suyeong jang <jangsuyeong923@naver.com>
2024-10-20 05:13:44 +0900

    feat(model): 추출된 리스트 합 로직 구현

--- src/main/java/calculator/model/CalculatorOperation.java
+++ src/main/java/calculator/model/CalculatorOperation.java
@@ -2,7 +2,7 @@ package calculator.model;
 
 import java.util.List;
 
-public class CalculatorOperation {
+public class CalculatorOperation { //추후 확장도 가능하도록 연산 기능은 분리
 
     public int calculateSum(List<Integer> numbers) {
         return numbers.stream()
