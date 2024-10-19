c57c400a50ac84a66638427bd379980794773a7f
Layla Oh <crispylemon7120@gmail.com>
2024-10-19 20:06:21 +0900

    feat(parser): 구분자를 인식하는 기능
    커스텀 구분자가 있다면 구분자 목록에 추가하고, 없다면 기본 구분자로 목록을 구성한다.
    만약 형식이 잘못되었다면 에러로 처리한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
+import java.lang.System;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 구분자 목록
+        String[] custom_seperators;
+
+        // 1. 구분자를 인식하는 기능 - 커스텀 구분자가 있다면 구분자 목록에 추가
+        String input_string = args[0];
+        String[] splited_string = input_string.split("\\\\n");
+        if (splited_string.length > 1 && splited_string[0].startsWith("//")){
+            String seperator = splited_string[0].substring(2);
+            custom_seperators = new String[]{",", ":", seperator};
+        } else if (splited_string.length == 1){
+            custom_seperators = new String[]{",", ":"};
+        } else {
+            // '\n'은 등장했으나 '//' 은 등장하지 않은 상황으로 에러처리
+            throw new IllegalArgumentException("값이 잘못 되었습니다.");
+        }
+//        System.out.println(Arrays.toString(splited_string));
+//        System.out.println(Arrays.toString(custom_seperators));
+//        System.out.println(input_string);
     }
 }
