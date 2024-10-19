27ce4112e758eee1174fca944470b09d7ce7adbe
승엽 <tmdduqflfl@naver.com>
2024-10-19 16:20:53 +0900

    refactor (OutputView) : add newLine refactor
    클린코드를 위한 개행추가
    
    Close

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -3,6 +3,7 @@ package calculator.view;
 public class OutputView {
 
     public void outputResult(int sum){
-        System.out.println("결과 :" + sum);
+        System.out.println("결과 : " + sum);
     }
+
 }
