8811d21e22219054875ce2edce377f1e03713d3a
jihwan <jihwan1535@nate.com>
2024-10-19 23:50:23 +0900

    refactor(custom): 기본 구분자와 커스텀 구분자를 분리

--- src/main/java/calculator/domain/prompt/Delimiter.java
+++ src/main/java/calculator/domain/prompt/Delimiter.java
@@ -5,26 +5,25 @@ import java.util.Set;
 
 public class Delimiter {
 
-    private static final char DEFAULT_DELIMITER_COMMA = ',';
-    private static final char DEFAULT_DELIMITER_COLON = ':';
-    private static final int MAX_DELIMITER_COUNT = 3;
+    private static final Set<Character> DEFAULT_DELIMITERS = Set.of(',', ':');
+    private static final int CUSTOM_DELIMITER_COUNT_LIMIT = 1;
 
-    private final Set<Character> delimiters;
+    private final Set<Character> customDelimiters;
 
     public Delimiter() {
-        this.delimiters = new HashSet<>(Set.of(DEFAULT_DELIMITER_COMMA, DEFAULT_DELIMITER_COLON));
+        this.customDelimiters = new HashSet<>();
     }
 
     public void add(char delimiter) {
-        if (this.delimiters.size() >= MAX_DELIMITER_COUNT) {
+        if (this.customDelimiters.size() >= CUSTOM_DELIMITER_COUNT_LIMIT) {
             throw new IllegalArgumentException();
         }
 
-        this.delimiters.add(delimiter);
+        this.customDelimiters.add(delimiter);
     }
 
     public boolean isIn(char delimiter) {
-        return this.delimiters.contains(delimiter);
+        return this.customDelimiters.contains(delimiter) || DEFAULT_DELIMITERS.contains(delimiter);
     }
 
 }
