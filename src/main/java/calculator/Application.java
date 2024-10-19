260e586e465f9ca18573fd8db1cb62cc08b2658a
zi-eunn <jieunkim1117@gmail.com>
2024-10-20 00:17:51 +0900

    feat(calculator): change Add custom delimiter

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,15 +8,39 @@ public class Application {
             return 0;
         }
 
-        String[] nums = numbers.split("[,:]");
+        String delimiter = "[,:]";
+        String numberString = numbers;
+
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\\n");
+            if (delimiterIndex != -1) {
+                delimiter = escapeSpecialRegexChars(numbers.substring(2, delimiterIndex));
+                numberString = numbers.substring(delimiterIndex + 2); // +2 to skip "\\n"
+            } else {
+                System.out.println("error0");
+                return 0;
+            }
+        }
+
+        String[] nums = numberString.split(delimiter);
         int sum = 0;
 
         for (String num : nums) {
-            sum += Integer.parseInt(num.trim());
+            try {
+                sum += Integer.parseInt(num.trim());
+            } catch (NumberFormatException e) {
+                System.out.println("error" + num);
+                return 0;
+            }
         }
 
         return sum;
     }
+
+    public static String escapeSpecialRegexChars(String delimiter) {
+        return delimiter.replaceAll("([\\\\.*+?^${}()|\\[\\]])", "\\\\$1");
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
