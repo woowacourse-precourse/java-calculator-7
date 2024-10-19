8e2ca6086d7eba3791c4cd049f7ee49ee0b51edc
wgrgwg <kyoeng14697@naver.com>
2024-10-19 19:13:02 +0900

    feat(model): add parseDefaultString method

--- src/main/java/calculator/model/CalculatorModel.java
+++ src/main/java/calculator/model/CalculatorModel.java
@@ -1,7 +1,23 @@
 package calculator.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public final class CalculatorModel {
+
     public boolean isCustom(String input) {
         return input.startsWith("//") && input.contains("\\n");
+    }
+
+    public List<String> parseUserInput(String input) {
+        if (isCustom(input)) {
+
+        }
+
+        return parseDefaultString(input);
+    }
+
+    public List<String> parseDefaultString(String input) {
+        return Arrays.asList(input.split("[,;]"));
     }
 }
