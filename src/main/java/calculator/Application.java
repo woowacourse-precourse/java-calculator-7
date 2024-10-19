6807fa7c8765cdd9f89124f595a9b605f69c1c7b
Lkk7380 <dlrlrhkd@naver.com>
2024-10-19 23:52:10 +0900

    feat : null과 빈 문자열 처리, 그리고 문자열을 정수로 변환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,10 @@
 package calculator;
 
 public class Application {
-
-
+    public int stringAdd(String text) {
+        if (text == null || text.isBlank()) {
+            return 0;
+        }
+        return Integer.parseInt(text);
+    }
 }
