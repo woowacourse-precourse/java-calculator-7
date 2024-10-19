5bdbbcf21438fb10d89cc1e951f96d4ec6257812
윤혜진 <lold2424@gmail.com>
2024-10-19 17:41:45 +0900

    feat : 입력 처리 및 구분자 검증 기능 클래스

---
+++ src/main/java/calculator/Controller/InputController.java
@@ -0,0 +1,17 @@
+package calculator.Controller;
+
+import calculator.vaildator.InputValidator;
+import calculator.vaildator.SeparatorHandler;
+
+public class InputController {
+
+    private final SeparatorHandler separatorHandler = new SeparatorHandler();
+    private final InputValidator inputValidator = new InputValidator();
+
+    public String[] processInput(String input) {
+        char separator = separatorHandler.getCustomSeparator(input);
+        String[] inputArr = separatorHandler.splitInput(input, separator);
+        inputValidator.validateInputArray(inputArr);
+        return inputArr;
+    }
+}
