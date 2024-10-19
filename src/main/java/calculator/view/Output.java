f2da6b4c811fc4caba58802d7f6dbc167a4a1e2d
ahma0 <dsd932@naver.com>
2024-10-19 17:17:10 +0900

    feat: 계산기의 실행 결과 문구를 출력하는 기능

--- src/main/java/calculator/view/Output.java
+++ src/main/java/calculator/view/Output.java
@@ -1,9 +1,23 @@
 package calculator.view;
 
+import java.math.BigInteger;
+
 public class Output {
 
     public static void printBeginningPhrase() {
         System.out.println("덧셈할 문자열을 입력해주세요.");
+    }
+
+    public static void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
+
+    public static void printResult(long result) {
+        System.out.println("결과 : " + result);
+    }
+
+    public static void printResult(BigInteger result) {
+        System.out.println("결과 : " + result);
     }
 
 }
