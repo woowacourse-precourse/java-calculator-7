ff790c21dd5efd5b867f8909c3d5a741f9979d71
윤진완 <yunjin-wan@yunjin-wan-ui-MacBookAir.local>
2024-10-19 14:44:03 +0900

    feat: 기본 구분자 구분

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        String delimiter = ",|:";
+        String[] split = input.split(delimiter);
+        int sum = 0;
+        for (String token : split) {
+            if(!token.isEmpty()){
+                try {
+                    int num = Integer.parseInt(token);
+                    if (num < 0) {
+                        throw new IllegalArgumentException("음수");
+                    }
+                    sum += num;
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("숫자가 아닌 문자");
+                }
+            }
+        }
+
+
+
+
+        System.out.println("결과 : " + sum);
     }
 }
