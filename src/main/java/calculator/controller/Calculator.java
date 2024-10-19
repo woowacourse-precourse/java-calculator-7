40eff0de13257ad1fafeb78fa0d4b20ae71fdd06
jeonghyeon-ohk <ok6737@naver.com>
2024-10-19 17:15:52 +0900

    feat(controller): 계산 기능을 처리하는 calculate 매서드 구현
    입력된 문자열을 기반으로 계산을 처리하는 calculate 매서드를 구현했습니다.
    - 기분 구분자 (',' , ':')와 커스텀 구분자을 처리
    - 입력된 값이 모두 양의 정수인지 검증
    - 계산 결과를 반환하는 로직 추가

--- src/main/java/calculator/controller/Calculator.java
+++ src/main/java/calculator/controller/Calculator.java
@@ -1,16 +1,31 @@
 package calculator.controller;
 
-import calculator.view.InputView;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static calculator.domain.CustomDelimiter.*;
+import static calculator.domain.NumberExtractor.getIntegerArray;
+import static calculator.domain.Result.getResult;
+import static calculator.validator.NumberValidator.areAllPositiveIntegers;
+import static calculator.validator.StringValidator.hasCustomDelimiter;
+
 public class Calculator{
-    List<String> delimilter = new ArrayList<>(); // 구분자를 저장할 리스트
-    static String input; // 사용자가 입력한 값을 저장할 변수
+    ArrayList<String> delimiter; // 구분자를 저장할 리스트
+    String input;
 
-    public static void run(){
-        input = InputView.getInput();
+    public Calculator(String input) {
+        this.input = input;
+        delimiter = new ArrayList<>(Arrays.asList(",", ":")); // 기본 구분자 (,)와 (:)을 저장
+    }
+    public int calculate() {
+        // 커스텀문자열이 존재할 경우
+        if (hasCustomDelimiter(input)) {
+            delimiter.add(getCustomDelimiter(input)); // 커스텀 구분자를 delimiter 리스트에 저장
+            input = removeCustomDelimiter(input); // 구분자 양식을 제거하여 input에 다시 저장
+        }
+        areAllPositiveIntegers(input, delimiter); // 모든 값이 양의 정수인지 판단
 
+        return getResult(getIntegerArray(input, delimiter));
     }
 }
