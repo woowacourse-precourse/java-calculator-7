96fa6e3f223c4a845437117081589178335dd7fa
navangs <43688529+insertend23@users.noreply.github.com>
2024-10-20 02:49:57 +0900

    fix(SeparatorManager.java): 추가한 테스트 코드들을 기반으로 실패한 케이스들에 대하여 오류 수정

--- src/main/java/calculator/SeparatorManager.java
+++ src/main/java/calculator/SeparatorManager.java
@@ -8,12 +8,12 @@ public class SeparatorManager {
     private final List<Character> separatorList;
 
     public SeparatorManager(Character separator) {
-        if (separator != null) {
+        if (separator == null || defaultSeparatorList.contains(separator)) {
+            separatorList = defaultSeparatorList;
+        } else {
             List<Character> tempSeparatorList = new ArrayList<>(defaultSeparatorList);
             tempSeparatorList.add(separator);
             separatorList = List.copyOf(tempSeparatorList);
-        } else {
-            separatorList = defaultSeparatorList;
         }
     }
 
@@ -24,14 +24,23 @@ public class SeparatorManager {
     public List<String> getSeparatedStringNumber(String input) {
         List<String> separatedStringList = new ArrayList<>();
         int offset = 0;
+
         for (int i = 0; i < input.length(); i++) {
-            if (!separatorList.contains(input.charAt(i)) && !Character.isDigit(input.charAt(i))) {
-                throw new IllegalArgumentException();
-            } else {
-                separatedStringList.add(input.substring(offset, i));
+            if(separatorList.contains(input.charAt(i))) {
+                if(offset < i){
+                    separatedStringList.add(input.substring(offset, i));
+                }
                 offset = i + 1;
+            } else if (Character.isDigit(input.charAt(i))) {
+            } else {
+                throw new IllegalArgumentException("잘못된 입력값입니다.");
             }
         }
+
+        if(offset < input.length()) {
+            separatedStringList.add(input.substring(offset));
+        }
+
         return separatedStringList;
     }
 }
