be0d7d2dc4e9e019819f1bbbf485babca116f70a
etd93 <etd93@naver.com>
2024-10-19 22:39:34 +0900

    [feat] 구분자(쉼표, 콜론, 커스텀 구분자)가 숫자와 숫자 사이에 있는지 판단하는 메서드 구현

--- src/main/java/calculator/exception/InputStringException.java
+++ src/main/java/calculator/exception/InputStringException.java
@@ -4,14 +4,16 @@ import calculator.domain.Delimiters;
 import calculator.domain.Numbers;
 import calculator.service.ExtractService;
 
+import java.util.LinkedList;
+
 public class InputStringException {
     private static final ExtractService extractService = new ExtractService();
     public Numbers validateInputPattern(String inputString, Numbers numbers, Delimiters delimiters){
         if(inputString.contains("//") || inputString.contains("\\n")) {
             isCustomDelimiterDeclaredInStandardFormat(inputString);
             extractService.extractDelimeter(inputString, delimiters);
-
         }
+        validateInputInStandardFormat(delimiters, inputString);
 
         return new Numbers();
     }
@@ -25,6 +27,25 @@ public class InputStringException {
         String checkInputString = inputString.substring(startIdx+2, endIdx);
         if(!(startIdx ==0 && endIdx == 3 && checkInputString.length()==1 && !Character.isDigit(checkInputString.charAt(0)))){
             throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateInputInStandardFormat(Delimiters delimiters, String inputString){
+        if(inputString.contains("//") && inputString.contains("\\n")){
+            inputString = inputString.substring(5);
+        }
+        LinkedList<Character> delimitersList = delimiters.getDelimeterCollections();
+        for(int i=0;i<inputString.length();i++){
+            if(Character.isDigit(inputString.charAt(i))) continue;
+            if(!delimitersList.contains(inputString.charAt(i))){
+                throw new IllegalArgumentException("");
+            }
+            if(!(i>0 && i<inputString.length()-1)){
+                throw new IllegalArgumentException("");
+            }
+            if(!(Character.isDigit(inputString.charAt(i-1)) && Character.isDigit(inputString.charAt(i+1)))){
+                throw new IllegalArgumentException("");
+            }
         }
     }
 }
