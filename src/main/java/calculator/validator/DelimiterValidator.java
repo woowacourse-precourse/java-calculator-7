2ded09ed239c7ec9d99a1796f7f140ef6994d690
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-19 15:13:18 +0900

    refactor: 재사용성을 위해 상수 추출

--- src/main/java/calculator/validator/DelimiterValidator.java
+++ src/main/java/calculator/validator/DelimiterValidator.java
@@ -4,32 +4,27 @@ import calculator.global.exception.ErrorMessage;
 
 import static calculator.global.constant.DelimiterConstant.PREFIX;
 import static calculator.global.constant.DelimiterConstant.SUFFIX;
+import static calculator.global.constant.DelimiterConstant.DELIMITER_LIMIT_LENGTH;
+import static calculator.global.constant.DelimiterConstant.VALID_NUMBER_REGEX;
 
 public class DelimiterValidator {
 
     public static boolean hasCustomDelimiter(String str) {
-        if (str.length() >= 5) {
-            str = str.substring(str.indexOf(PREFIX), str.lastIndexOf(SUFFIX)+SUFFIX.length());
+        if (str.length() >= DELIMITER_LIMIT_LENGTH) {
+            str = str.substring(0, str.lastIndexOf(SUFFIX)+SUFFIX.length());
             return str.startsWith(PREFIX) && str.endsWith(SUFFIX);
         }
         return false;
     }
 
-    public static String validateRequireEscapeCharacter(String customDelimiter) {
-        if (customDelimiter.equals("|") || customDelimiter.equals(".")) {
-            customDelimiter = "\\" + customDelimiter;
-        }
-        return customDelimiter;
-    }
-
     public static void validateCustomDelimiterLength(String customDelimiter) {
-        if (customDelimiter.length() > 5) {
+        if (customDelimiter.length() > DELIMITER_LIMIT_LENGTH) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_DELIMITER_ERROR.getMessage());
         }
     }
 
     public static void validateIncludeDelimiter(String str) {
-        if (!str.matches("-?\\d+")) {
+        if (!str.matches(VALID_NUMBER_REGEX)) {
             throw new IllegalArgumentException(ErrorMessage.NOT_ENROLLED_DELIMITER.getMessage());
         }
     }
