cb3cbda14bd16fbde1c3843187c6ae426a00a3f0
swlee8783 <siwon8783@gmail.com>
2024-10-19 12:53:26 +0900

    docs(Application): add comments to explain logic and functionality

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -17,17 +17,17 @@ public class Application {
     }
 
     public static class StringAddCalculator {
-        // 빈 문자열 혹은 null 체크
         public static int add(String input) {
+            // 빈 문자열 혹은 null 체크 후 이에 해당될 경우 0 반환
             if (input == null || input.isBlank()) {
                 return 0;
             }
 
-            // 쉼표 또는 콜론을 기준으로 문자열 분리
+            // 쉼표 또는 콜론을 문자열 분리 디폴트로 설정
             String delimiter = "[,:]";
             String numbers = input;
 
-            // 커스텀 구분자 입력 확인
+            // 커스텀 구분자 입력 확인 및 처리
             if (input.startsWith("//")) {
                 int endIndexDelimiter = input.indexOf("\n");
                 delimiter = input.substring(2, endIndexDelimiter);
@@ -36,9 +36,10 @@ public class Application {
                 delimiter = escapeSpecialCharacters(delimiter);
             }
 
+            // 구분자 기준으로 문자열 분리
             String[] tokens = numbers.split(delimiter);
 
-            // 각 숫자의 합을 계산
+            // 각 숫자의 합 계산
             int sum = 0;
             for (String token : tokens) {
                 sum += Integer.parseInt(token.trim()); // 문자열을 정수로 변환하여 합산
@@ -47,6 +48,7 @@ public class Application {
             return sum;
         }
 
+        // 정규식 특수문자 처리용 메소드
         private static String escapeSpecialCharacters(String delimiter) {
             return delimiter.replaceAll("([\\[\\]{}()*+?^$\\\\|.])", "\\\\$1");
         }
