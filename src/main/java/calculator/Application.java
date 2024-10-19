33afdce9d46cbd5a5f7ff20645c12713dcf82839
bill291104 <bill29123@gmail.com>
2024-10-19 18:43:09 +0900

    feat(DefaultCal): add calculator class, requirement no.1
    -Implement cal() method to split input by delimiter and calculate sum
    -Support delimiter: comma (,) and colon (:)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("입력하세요");
+        String input = readLine();
+
+        DefaultCal calculator = new DefaultCal();
+        int result = calculator.cal(input);
+
+        System.out.println("결과 : " + result);
     }
 }
