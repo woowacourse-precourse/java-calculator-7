797a1b85a7188656d64f9e8f09d08549d48f45fd
GaBaljaintheroom <junsu1222@naver.com>
2024-10-19 15:49:42 +0900

    style(SumCalculatorTest): import static 위치 수정

--- src/test/java/calculator/SumCalculatorTest.java
+++ src/test/java/calculator/SumCalculatorTest.java
@@ -1,10 +1,10 @@
 package calculator;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import java.util.List;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SumCalculatorTest {
 
