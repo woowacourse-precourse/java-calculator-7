2a23e1302d3ea630b5b7cad808eebce793232986
LSH-kw0315 <tmdgjsdl1234@naver.com>
2024-10-20 03:53:28 +0900

    <feat> 비정상 입력에 대한 예외 처리 구현
    비정상적인 입력이 들어왔을 때 예외를 발생시키도록 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,10 +11,15 @@ public class Application {
 
         String str=Console.readLine();
 
+        if(str==null){
+            throw new IllegalArgumentException();
+        }
+
         if(str.isEmpty()){
             System.out.println("결과 : 0");
             return;
         }
+
 
         String customSeparator=null;
         if(str.startsWith("//")){
@@ -22,6 +27,11 @@ public class Application {
             customSeparator=str.substring(0,end);
 
             str=str.substring(end+2);
+        }
+
+        String regex=customSeparator!=null?String.format("[^0-9,:%s]",customSeparator):"[^0-9,:]";
+        if(str.matches(regex)){
+            throw new IllegalArgumentException();
         }
 
         String[] separator=customSeparator!=null?new String[]{",",":",customSeparator}:new String[]{",",";"};
@@ -35,6 +45,9 @@ public class Application {
         int result=0;
         for(String s : integers){
             int i=Integer.parseInt(s);
+            if(i<0){
+                throw new IllegalArgumentException();
+            }
             result+=i;
         }
 
