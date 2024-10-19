949bcc0691d6e72663b01333215b609f87b3bfff
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-19 15:59:03 +0900

    Fix: 입력값이 없을 경우 예외가 발생하는 것이 아니라 0 을 반환하도록 검증 로직 제거

--- src/main/java/calculator/service/validation/SeparationValidator.java
+++ src/main/java/calculator/service/validation/SeparationValidator.java
@@ -3,7 +3,6 @@ package calculator.service.validation;
 import static calculator.service.exception.SeparationExceptionType.ENDS_WITH_SEPARATOR;
 import static calculator.service.exception.SeparationExceptionType.INVALID_SEPARATOR;
 import static calculator.service.exception.SeparationExceptionType.NOT_A_POSITIVE_NUMBER;
-import static calculator.service.exception.SeparationExceptionType.NO_INPUT;
 import static calculator.service.exception.SeparationExceptionType.STARTS_WITH_SEPARATOR;
 
 import calculator.service.exception.SeparationException;
@@ -12,18 +11,10 @@ import java.util.Arrays;
 public class SeparationValidator {
 
     public static void validate(String input, String... separators) {
-        validateExistInput(input);
         validateStartsWithSeparator(input, separators);
         validateEndWithSeparator(input, separators);
         validateIsDigit(input, separators);
         validateIsPositive(input, separators);
-    }
-
-    // 입력값이 존재하는지 검증합니다.
-    private static void validateExistInput(String input) {
-        if (input.isBlank()) {
-            throw new SeparationException(NO_INPUT);
-        }
     }
 
     // 입력값이 구분자로 시작하는지 검증합니다.
@@ -43,7 +34,7 @@ public class SeparationValidator {
     // 구분자로 나뉘어진 모든 요소가 숫자인지 검증합니다.
     private static void validateIsDigit(String input, String... separators) {
         String regex = String.join("|", separators);
-        if (!Arrays.stream(input.split(regex)).allMatch(str -> isDigit(str))) {
+        if (!input.isBlank() && !Arrays.stream(input.split(regex)).allMatch(str -> isDigit(str))) {
             throw new SeparationException(INVALID_SEPARATOR);
         }
     }
@@ -60,7 +51,7 @@ public class SeparationValidator {
     // 구분자로 나뉘어진 모든 요소가 양수인지 검증합니다.
     private static void validateIsPositive(String input, String... separators) {
         String regex = String.join("|", separators);
-        if (!Arrays.stream(input.split(regex)).allMatch(str -> isPositive(str))) {
+        if (!input.isBlank() && !Arrays.stream(input.split(regex)).allMatch(str -> isPositive(str))) {
             throw new SeparationException(NOT_A_POSITIVE_NUMBER);
         }
     }
