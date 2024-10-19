485c52f77c04fa42bfc171bc3537a18342247ad7
Seongmo Ahn <djgood780@gmail.com>
2024-10-19 22:09:07 +0900

    feat(Application): 프로그램 종료 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Calculator calculator = new Calculator();
-        calculator.run();
-        calculator.end();
+        calculator.run(); //프로그램 실행
+        calculator.end(); //프로그램 종료
     }
 }
