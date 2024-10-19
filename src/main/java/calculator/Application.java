ada72fae697b9d1a9e056b5338175ee7bf251147
whatsgoodg <rlwjddl1596@naver.com>
2024-10-19 23:59:00 +0900

    feat(Application): Application 클래스 main 매서드에서 FrontController 실행
    추가한 것
    - FrontController 객체 생성
    - FrontController의 run() 매서드 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import calculator.back.controller.CalculatorController;
+import calculator.back.frontcontroller.FrontController;
+import calculator.back.resolver.ArgumentResolver;
+import calculator.back.service.impl.CalculatorServiceImpl;
+import calculator.front.view.InputView;
+import calculator.front.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        FrontController controller = new FrontController(new InputView(), new OutputView(), new ArgumentResolver(),
+                new CalculatorController(new CalculatorServiceImpl()));
+        controller.run();
     }
 }
