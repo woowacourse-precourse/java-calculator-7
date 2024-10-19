9d624569824dcad54495e03dbcd7f1606e1c42a6
wkqkel <wkqkel@naver.com>
2024-10-19 22:19:42 +0900

    feat: 커스텀구분자 및 정수 예외처리

--- src/main/java/calculator/CalculatorParser.java
+++ src/main/java/calculator/CalculatorParser.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import calculator.exception.CalculatorParsedException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -7,21 +8,52 @@ import java.util.regex.Pattern;
 
 public class CalculatorParser {
     public static final String DEFAULT_DELIMITER = ",:";
-    public static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.*)\n(.*)");
+    public static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.*)(\n|\\\\n)(.*)");
 
     public static List<Integer> parse(String text) {
+        Result result = getResult(text);
+        // 숫자로 변환
+        String regex = "[" + result.delimiter() + "]";
+
+        return Arrays.stream(result.text().split(regex))
+                .map(CalculatorParser::toInteger)
+                .toList();
+    }
+
+    private static Result getResult(String text) {
         String delimiter = DEFAULT_DELIMITER;
         Matcher matcher = CUSTOM_PATTERN.matcher(text);
 
-        if (matcher.find()) {
-            delimiter += matcher.group(1);
-            text = matcher.group(2);
+        if (!matcher.find()) {
+            return new Result(delimiter, text);
         }
 
-        String regex = "[" + delimiter + "]";
+        String customDelimiter = matcher.group(1);
+        String parsedText = matcher.group(3);
 
-        return Arrays.stream(text.split(regex))
-                .map(Integer::valueOf)
-                .toList();
+        if (customDelimiter.isEmpty()) {
+            throw new CalculatorParsedException("커스텀 구분자로 빈 값이 올 수 없습니다.");
+        }
+        if (customDelimiter.length() >= 2) {
+            throw new CalculatorParsedException("커스텀 구분자로 2자 이상이 올 수 없습니다.");
+        }
+        if (Character.isDigit(customDelimiter.charAt(0))) {
+            throw new CalculatorParsedException("커스텀 구분자로 숫자가 올 수 없습니다.");
+        }
+
+        return new Result(delimiter + customDelimiter, parsedText);
+    }
+
+    private record Result(String delimiter, String text) {
+    }
+
+    private static int toInteger(String str) {
+        int i = Integer.parseInt(str);
+
+        if (i < 0) {
+            throw new CalculatorParsedException("정수만 입력할 수 있습니다.");
+        }
+
+        return i;
     }
 }
--- src/test/java/calculator/CalculatorParserTest.java
+++ src/test/java/calculator/CalculatorParserTest.java
@@ -1,9 +1,12 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import calculator.exception.CalculatorParsedException;
 import java.util.List;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -24,12 +27,12 @@ class CalculatorParserTest {
         return Stream.of(
                 Arguments.of("1:2:3", List.of(1, 2, 3)),
                 Arguments.of("1,2,3", List.of(1, 2, 3)),
-                Arguments.of("1,2:3", List.of(1, 2, 3))
+                Arguments.of("1,2:33", List.of(1, 2, 33))
         );
     }
 
     @ParameterizedTest
-    @MethodSource("provide3")
+    @MethodSource("provide2")
     void 커스텀_구분자를_기준으로_파싱할_수_있다(String input, List<Integer> guess) {
         // when
         List<Integer> ret = CalculatorParser.parse(input);
@@ -38,10 +41,65 @@ class CalculatorParserTest {
         assertThat(ret).isEqualTo(guess);
     }
 
-    private static Stream<Arguments> provide3() {
+    private static Stream<Arguments> provide2() {
         return Stream.of(
                 Arguments.of("//A\n1A2A3", List.of(1, 2, 3)),
-                Arguments.of("//?\n1?2:3", List.of(1, 2, 3))
+                Arguments.of("//?\n1?2:3", List.of(1, 2, 3)),
+                Arguments.of("//;\\n1", List.of(1))
         );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provide3")
+    void 커스텀_구분자로_2자_이상이_값이_올_수_없다(String input) {
+        // when
+        Throwable exception = assertThrows(CalculatorParsedException.class, () -> {
+            CalculatorParser.parse(input);
+        });
+
+        // then
+        assertThat(exception.getMessage()).isEqualTo("커스텀 구분자로 2자 이상이 올 수 없습니다.");
+    }
+
+    private static Stream<Arguments> provide3() {
+        return Stream.of(
+
+                Arguments.of("//AAA\n1AAA2AAA3"),
+                Arguments.of("////\n1//2//3")
+//                Arguments.of("//\n\n1\n2\n3")
+        );
+    }
+
+    @Test
+    void 커스텀_구분자로_빈_값이_올_수_없다() {
+        // when
+        Throwable exception = assertThrows(CalculatorParsedException.class, () -> {
+            CalculatorParser.parse("//\n13:A:2");
+        });
+
+        // then
+        assertThat(exception.getMessage()).isEqualTo("커스텀 구분자로 빈 값이 올 수 없습니다.");
+    }
+
+    @Test
+    void 정수만_입력_할_수_있습니다() {
+        // when
+        Throwable exception = assertThrows(CalculatorParsedException.class, () -> {
+            CalculatorParser.parse("-1:2:-3");
+        });
+
+        // then
+        assertThat(exception.getMessage()).isEqualTo("정수만 입력할 수 있습니다.");
+    }
+
+    @Test
+    void 커스텀_구분자로_숫자가_올_수_없다() {
+        // when
+        Throwable exception = assertThrows(CalculatorParsedException.class, () -> {
+            CalculatorParser.parse("//3\n13:A:2");
+        });
+
+        // then
+        assertThat(exception.getMessage()).isEqualTo("커스텀 구분자로 숫자가 올 수 없습니다.");
     }
 }
