02fa3ac34aaccfcf5c590043f17a3a6121b00e23
AppleMint98 <hong721816@konkuk.ac.kr>
2024-10-19 22:28:49 +0900

    feat : 기능 목록 3, 잘못된 값 입력시 예외처리
    잘못된 값 입력 시 IllegalArgumentException

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -15,17 +15,25 @@ public class Application {
                 for (String num : nums) {
                     String[] splits = num.split(":");
                     for (String split : splits) {
+                        if (Long.parseLong(split) < 0) {
+                            throw new IllegalArgumentException("구분자와 양수로 구성된 문자열을 입력해 주세요.");
+                        }
                         sum += Long.parseLong(split);
                     }
                 }
             }
         } else if (userInput.contains("//") && userInput.contains("\\n")) {
-            String separator = userInput.substring(userInput.indexOf("//") + 2  ,
+            String separator = userInput.substring(userInput.indexOf("//") + 2,
                 userInput.indexOf("\\n"));
             String[] nums = userInput.substring(userInput.indexOf("\\n") + 2).split(separator);
             for (String num : nums) {
+                if (Long.parseLong(num) < 0) {
+                    throw new IllegalArgumentException("구분자와 양수로 구성된 문자열을 입력해 주세요.");
+                }
                 sum += Long.parseLong(num);
             }
+        } else {
+            throw new IllegalArgumentException("구분자와 양수로 구성된 문자열을 입력해 주세요.");
         }
 
         System.out.println("결과 : " + sum);
