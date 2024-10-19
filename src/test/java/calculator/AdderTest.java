2f8adbe910115019c67c143cfcc9b6fb48e7231a
moonbinchoi <whsahd12@gmail.com>
2024-10-19 18:33:31 +0900

    test(Adder): Adder 클래스 수정에 따른 수정

--- src/test/java/calculator/AdderTest.java
+++ src/test/java/calculator/AdderTest.java
@@ -2,81 +2,26 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-
-import java.util.Set;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class AdderTest {
 
     private Adder adder;
 
-    @Nested
-    class Success {
+    @ParameterizedTest
+    @EnumSource(ExpressionTestCase.class)
+    void sum(ExpressionTestCase testCase) {
+        //given
+        final Adder adder = new Adder(testCase.delimiters, testCase.operands);
 
-        /*
-         * Used expression on success cases
-         * "123:234,345,456" - Using only default delimiters
-         * "123:234?345w456" - Using default and custom delimiters
-         * "123:  234 ? 345  w456" - Using default and custom delimiters until expression include spaces
-         */
+        //when
+        final int sum = adder.sum();
 
-        protected final int exprResult = 123 + 234 + 345 + 456;
-
-        @Test
-        void With_no_custom_delimiters() {
-            //given
-            adder = new Adder("123:234,345,456");
-
-            //when
-            final int result = adder.run();
-
-            //then
-            assertThat(result).isEqualTo(exprResult);
-        }
-
-        @Test
-        void With_custom_delimiters() {
-            //given
-            final Set<String> delimiterSet = Set.of("?", "w");
-            adder = new Adder("123:234?345w456", delimiterSet);
-
-            //when
-            final int result = adder.run();
-
-            //then
-            assertThat(result).isEqualTo(exprResult);
-        }
-
-        @Test
-        void With_spaces() {
-            //given
-            final Set<String> delimiterSet = Set.of("?", "w");
-            adder = new Adder("123:  234 ? 345  w456", delimiterSet);
-
-            //when
-            final int result = adder.run();
-
-            //then
-            assertThat(result).isEqualTo(exprResult);
-        }
-    }
-
-    @Nested
-    class Fails_throw_IllegalArgumentException {
-
-        @Test
-        void When_delimiter_not_found() {
-            //given
-            adder = new Adder("123:456?789");
-
-            //when, then
-            assertThrows(IllegalArgumentException.class,
-                    () -> adder.run());
-        }
+        //then
+        assertThat(sum).isEqualTo(testCase.sum);
     }
 }
