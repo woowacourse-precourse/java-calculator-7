d8b0cf2c2f56d5bad661c7d5bcc7abe64c5ac960
Chanwon-Seo <scwonn60@gmail.com>
2024-10-19 12:23:03 +0900

    test: 클래스명 변경

--- src/test/java/calculator/domain/validation/InputParserValidatorTest.java
+++ src/test/java/calculator/domain/validation/InputParserValidatorTest.java
@@ -7,28 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class InputValidatorTest {
-    private final InputValidator validator = new InputValidator();
+class InputParserValidatorTest {
 
     @Test
     @DisplayName("입력이 null일 때 IllegalArgumentException 발생")
     void testCheckForNullInputThrowsIllegalArgumentException() {
         assertThrows(IllegalArgumentException.class, () -> {
-            validator.checkForEmptyInput(null);
+            InputParserValidator.checkForEmptyInput(null);
         });
     }
 
     @Test
     @DisplayName("입력이 빈문자열일 때 예외가 발생하지 않아야 함")
     void testCheckForEmptyInputDoesNotThrowException() {
-        assertDoesNotThrow(() -> validator.checkForEmptyInput(""));
+        assertDoesNotThrow(() -> InputParserValidator.checkForEmptyInput(""));
     }
 
     @Test
     @DisplayName("공백 문자열 입력 시 공백 제거 결과 확인")
     void testRemoveLeadingAndTrailingSpaces_EmptyString() {
         String input = " ";
-        String result = validator.removeLeadingAndTrailingSpaces(input);
+        String result = InputParserValidator.removeLeadingAndTrailingSpaces(input);
 
         assertEquals("", result);
     }
@@ -37,7 +36,7 @@ class InputValidatorTest {
     @DisplayName("양쪽 공백 제거된 문자열 확인")
     void testRemoveLeadingAndTrailingSpaces_SpacesAround() {
         String input = " 1:2:3 ";
-        String result = validator.removeLeadingAndTrailingSpaces(input);
+        String result = InputParserValidator.removeLeadingAndTrailingSpaces(input);
 
         assertEquals("1:2:3", result);
     }
@@ -46,7 +45,7 @@ class InputValidatorTest {
     @DisplayName("양쪽 공백 제거된 문자열 확인")
     void testRemoveLeadingAndTrailingSpaces_ComplexString() {
         String input = " //;\\n1;2;3 ";
-        String result = validator.removeLeadingAndTrailingSpaces(input);
+        String result = InputParserValidator.removeLeadingAndTrailingSpaces(input);
 
         assertEquals("//;\\n1;2;3", result);
     }
