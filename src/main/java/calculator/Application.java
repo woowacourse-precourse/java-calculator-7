1b5c4a9f6b556e69bd30525e18c00fff70cc00cf
chxghee <francis001021@gmail.com>
2024-10-19 23:50:35 +0900

    Feat: 입력 유효성 검사 기능 구현
    커스텀 구분자를 포함한 여러 입력에 대한 유효성 검사와 예외처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputValidator inputValidator = new InputValidator();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        try{
+            if (!inputValidator.isValid(input)){
+                throw new IllegalArgumentException("잘못된 입력입니다. " + input);
+            }
+            System.out.println("유효한 입력입니다: " + input);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
+
     }
 }
