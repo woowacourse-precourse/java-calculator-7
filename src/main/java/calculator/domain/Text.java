f62694f81783e20fa492433e4f21d6746d1a9fee
hooniverse <sung010326@gmail.com>
2024-10-20 01:23:42 +0900

    feat: 문자열 관련 정보 클래스 생성

---
+++ src/main/java/calculator/domain/Text.java
@@ -0,0 +1,17 @@
+package calculator.domain;
+
+public class Text {
+
+    private String text;
+
+    public Text() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
