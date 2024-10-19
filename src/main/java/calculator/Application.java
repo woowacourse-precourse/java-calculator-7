4e9891dff29ce38c48e8a3d75f9c02400d450e9b
haechangchoi <buraldongja@naver.com>
2024-10-19 20:37:32 +0900

    feat: 문자열을 구분자로 분리하여 숫자를 합산하는 sum 메서드 추가
    입력된 문자열을 지정된 구분자로 분리하고, 분리된 숫자들을 합산하는 sum 메서드를 구현했습니다.
    
    - 구분자로 문자열을 분리한 후, 각 숫자를 변환하여 합산하는 로직을 구현.
    - 음수나 유효하지 않은 값에 대한 예외 처리는 향후 처리 예정.
    밋되지 않도록 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,4 +8,15 @@ public class Application {
         // 입력 받기
         String input = Console.readLine();
     }
+
+    // 주어진 구분자를 사용해 문자열을 숫자로 분리하고 합을 구하는 메서드
+    private int sum(String input, String delimiter) {
+        String[] tokens = input.split(delimiter); // 구분자로 숫자 분리
+        int result = 0;
+        for (String token : tokens) {
+            int number = toPositiveInt(token);
+            result += number;
+        }
+        return result;
+    }
 }
