2cc4cee4b3f5b9c62cf442f8be2180cc3af223f2
jungsu <vwx1@naver.com>
2024-10-19 16:50:31 +0900

    refactor(Application) : Change the logic of the processInput

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,8 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
 
@@ -22,34 +24,27 @@ public class Application {
             return 0;
         }
 
-        String delimiter = ",|:";  // 기본 구분자 쉼표(,)와 콜론(:)
-        String numbers = input;
+        int sum;
+        int[] numbers;
+        String delimiter = ",|:";
 
         // 커스텀 구분자가 있는지 확인하고 처리
         if (checkCustomDelimiter(input)) {
-            delimiter = addCustomDelimiter(input);  // 커스텀 구분자 추가
-            numbers = extractNumbers(input);  // 숫자 부분 추출
+            delimiter = addCustomDelimiter(input);
         }
 
-        // 문자열을 구분자를 기준으로 분리하고 합을 구함
-        return getSum(splitString(numbers, delimiter));
+        numbers = extractNumbers(input, delimiter);
+        sum = Arrays.stream(numbers).sum();
+        return sum;
     }
 
-    private static int getSum(Object o) {
-    }
-
-    private static Object splitString(String numbers, String delimiter) {
-    }
-
-    private static String extractNumbers(String input) {
-    }
-
-    private static String addCustomDelimiter(String input) {
-    }
 
     private static boolean checkCustomDelimiter(String input) {
     }
-
+    private static String addCustomDelimiter(String input) {
+    }
+    private static int[] extractNumbers(String input) {
+    }
 
     private static void printOutput(int result) {
     }
