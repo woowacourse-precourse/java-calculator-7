8a149abada38fb128abcda810f590949b8839e14
howareyoo <newworld121@naver.com>
2024-10-20 04:38:40 +0900

    feat : 결과 출력

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -1,14 +1,17 @@
 package calculator;
 
+import static calculator.SumCalculator.getFinalResult;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class CalculatorController {
 
-    public void inputString() {
+    public void runCalculator() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = Console.readLine();
 
-        System.out.println(userInput);
+        Integer finalResult = getFinalResult(userInput);
+        System.out.println("결과 : " + finalResult);
     }
 
 }
