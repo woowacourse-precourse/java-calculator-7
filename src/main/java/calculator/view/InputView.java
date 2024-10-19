44b1b82398519ecfccbb71b5218f2adce5df8635
Jiihyun <cwlgusc@gmail.com>
2024-10-20 00:46:39 +0900

    style: 필요없는 주석 제거

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -6,8 +6,6 @@ public class InputView {
 
     private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 
-    //TODO: input이 null인 경우 예외처리
-
     public String readStringToAdd() {
         System.out.println(INPUT_MESSAGE);
         return Console.readLine().trim();
