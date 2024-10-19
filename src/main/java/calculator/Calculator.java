33732f31e231840b69e61b53f6575a95dfd044b1
Bae-JunSeok <qownstjr77@gmail.com>
2024-10-20 04:53:30 +0900

    feat: 사용자 입력 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,11 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Calculator {
+
     public static void run() {
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = Console.readLine();
     }
 }
