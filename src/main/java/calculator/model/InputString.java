6f17903e7219dbdc75c3b3e0d62a37a76b7a2e64
alswnsp411 <alswnsp411@naver.com>
2024-10-20 00:57:54 +0900

    feat: 문자열에서 커스텀 구분자를 추출하는 기능 추가 - Character[] customSeperator -> String customDelimiter 수정  : 커스텀 구분자로는 문자 하나를 받는다. - 커스텀 구분자를 입력받은 경우 추출하여 InputString의 customDelimiter에 저장, inputString에는 덧셈을 수행할 문자열만을 저장한다.

--- src/main/java/calculator/model/InputString.java
+++ src/main/java/calculator/model/InputString.java
@@ -7,10 +7,35 @@ public class InputString {
 
     private String inputString;             // 문자열
     private long resultSum;                 // 문자열에서 추출한 숫자들을 더한 결과
-    private Character[] customSeparator;    // 커스텀 구분자
+    private String customDelimiter;         // 커스텀 구분자
 
     public InputString(String inputString) {
         this.inputString = inputString;
+    }
+
+    //== 비즈니스 로직 ==//
+
+    /**
+     * 문자열에서 숫자를 추출해서 덧셈한다.
+     *
+     * @return resultSum 덧셈 계산 결과값
+     */
+    public long calculate() {
+        getCustomDelimiter();
+
+        String delimiters = this.customDelimiter == null ? ",|:" : customDelimiter + "|,|:";
+
+        return this.resultSum;
+    }
+
+    /**
+     * 문자열에서 커스텀 구분자 추출
+     */
+    private void getCustomDelimiter() {
+        if (inputString.startsWith("//") && inputString.substring(3, 5).equals("\\n")) {
+            this.customDelimiter = inputString.substring(2, 3);
+            this.inputString = inputString.substring(5);
+        }
     }
 
 }
