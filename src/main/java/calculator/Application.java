ba9fd9d5441758402b050e5d401c8de8e9697b83
kjyyjk <rlawnsdud920@gmail.com>
2024-10-20 01:01:18 +0900

    feat: 입력 요구사항 출력 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         InputString inputString = new InputString(Console.readLine());
 
         StringCalculator stringCalculator = new StringCalculator();
