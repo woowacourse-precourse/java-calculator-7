09a853452aa3b164c95a009fabac26fd4c5be5ee
taek2222 <oot309@daum.net>
2024-10-19 18:02:43 +0900

    refactor(NumberValidator): 오류 메시지 정의 및 접근 제어자 변경
    상수가 public 접근 제어자로 설정되어 있었다. 이는 해당 클래스에만 사용하는 상수로 private 로 변경했다.
    
    사용자에게 잘못된 부분을 알려주기 위해 에러 메시지를 추가했다.

--- src/main/java/calculator/util/NumberValidator.java
+++ src/main/java/calculator/util/NumberValidator.java
@@ -2,19 +2,21 @@ package calculator.util;
 
 public class NumberValidator {
 
-    public static final int ZERO = 0;
+    private static final String NO_DIGIT_MESSAGE = "문자가 아닌, 숫자로만 더하기 계산을 할 수 있어요.";
+    private static final String NO_NEGATIVE_MESSAGE = "음수가 아닌, 양수로만 계산을 진행할 수 있어요.";
+    private static final int ZERO = 0;
 
     public void validateNoDigits(char[] chars) {
         for (char ch : chars) {
             if (!Character.isDigit(ch))
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(NO_DIGIT_MESSAGE);
         }
     }
 
-    public void validatorNoNegatives(int[] numbers) {
+    public void validateNoNegatives(int[] numbers) {
         for (int number : numbers) {
             if (isNegative(number))
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(NO_NEGATIVE_MESSAGE);
         }
     }
 
