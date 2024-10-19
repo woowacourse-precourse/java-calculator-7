492c0e68eadec6a2d2d4933070e2c4b966ef924b
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-10-20 03:02:34 +0900

    feat: 계산기 서비스 구현
    커스텀 구분자 유무에 따라 구분자 목록과 숫자 목록을 얻은 후 덧셈을 하는 기능 구현

---
+++ src/main/java/calculator/service/CalculatorService.java
@@ -0,0 +1,53 @@
+package calculator.service;
+
+import java.util.List;
+
+import calculator.domain.Calculator;
+import calculator.domain.DelimiterMaker;
+import calculator.domain.InputParser;
+import calculator.domain.NumberExtractor;
+
+public class CalculatorService {
+
+	private final Calculator calculator;
+	private final DelimiterMaker delimiterMaker;
+	private final NumberExtractor numberExtractor;
+	private final InputParser inputParser;
+
+	public CalculatorService(Calculator calculator, DelimiterMaker delimiterMaker,
+		NumberExtractor numberExtractor, InputParser inputParser) {
+		this.calculator = calculator;
+		this.delimiterMaker = delimiterMaker;
+		this.numberExtractor = numberExtractor;
+		this.inputParser = inputParser;
+	}
+
+	public long calculate(String input) {
+		List<String> delimiters = getDelimiters(input);
+		List<Long> numbers = getNumbers(input, delimiters);
+		return calculator.calculate(numbers);
+	}
+
+	private List<String> getDelimiters(String useInput) {
+		if (delimiterMaker.hasCustomSeparator(useInput)) {
+			String delimiterSection = inputParser.getDelimiterSectionFrom(useInput);
+			return delimiterMaker.getDelimitersFrom(delimiterSection);
+		}
+
+		return delimiterMaker.getDefaultDelimiters();
+	}
+
+	private List<Long> getNumbers(String input, List<String> delimiters) {
+		String numberSection = getNumberSection(input);
+		return numberExtractor.getNumbers(numberSection, delimiters);
+	}
+
+	private String getNumberSection(String userInput) {
+		if (delimiterMaker.hasCustomSeparator(userInput)) {
+			return inputParser.getNumberSectionFrom(userInput);
+		}
+
+		return userInput;
+	}
+
+}
