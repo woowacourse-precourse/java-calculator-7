95fed2ecdb2a386f3537d3aebb3216a30e675390
yyysolhhh <solbaram37@gmail.com>
2024-10-20 01:54:33 +0900

    test: 테스트코드 추가, 중복된 테스트끼리 합침
    - 같은 경우의 테스트는 하나의 함수에서 배열로 모아서 테스트
    - 정상 케이스, 에러 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,12 +8,6 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
-//    @Test
-//    void 항상_통과() {
-//        assertSimpleTest(() -> {
-//            assertThat(true).isTrue();
-//        });
-//    }
 
     @Test
     void 커스텀_구분자_사용() {
@@ -31,65 +25,92 @@ class ApplicationTest extends NsTest {
         );
     }
 
-    // 주어진 케이스
+    // 정상 케이스
     @Test
     void 빈문자열() {
         assertSimpleTest(() -> {
-            run("");
+            run("\n"); // TODO 이렇게 안하면 java.util.NoSuchElementException: No line found 이렇게 되는데...
             assertThat(output()).contains("결과 : 0");
         });
     }
 
     @Test
-    void 기본_구분자_쉼표_한번() {
-        assertSimpleTest(() -> {
-            run("1,2");
-            assertThat(output()).contains("결과 : 3");
-        });
+    void 기본_구분자_한번() {
+        String[] inputStrings = {"1,2", "1:2"};
+        for (String inputString : inputStrings) {
+            assertSimpleTest(() -> {
+                run(inputString);
+                assertThat(output()).contains("결과 : 3");
+            });
+        }
     }
 
     @Test
-    void 기본_구분자_쉼표_두번() {
-        assertSimpleTest(() -> {
-            run("1,2,3");
-            assertThat(output()).contains("결과 : 6");
-        });
-    }
+    void 기본_구분자_두번() {
+        String[] inputStrings = {"1,2,3", "1:2:3", "1,2:3", "1:2,3"};
+        for (String inputString : inputStrings) {
+            assertSimpleTest(() -> {
+                run(inputString);
+                assertThat(output()).contains("결과 : 6");
+            });
 
-    @Test
-    void 기본_구분자_쉼표_한번_콜론_한번() {
-        assertSimpleTest(() -> {
-            run("1,2:3");
-            assertThat(output()).contains("결과 : 6");
-        });
+        }
     }
 
     @Test
     void 커스텀_구분자_한개_3번_사용() {
         assertSimpleTest(() -> {
-            run("//;\n1;2;3");
+            run("//;\\n1;2;3");
             assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_여러글자_1번_사용() {
+        assertSimpleTest(() -> {
+            run("//;@\\n1;@2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_기본_구분자_모두_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2:3,4");
+            assertThat(output()).contains("결과 : 10");
         });
     }
 
     // 에러 케이스
     @Test
-    void 음수기호_맨앞() {
-        assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("-1,2"))
-                    .isInstanceOf(IllegalArgumentException.class);
-        });
+    void 음수기호_포함() {
+        String[] inputStrings = {"-1,2", "1,-2", "-1:2", "1:-2"};
+        for (String inputString : inputStrings) {
+            assertSimpleTest(() -> {
+                assertThatThrownBy(() -> runException(inputString))
+                        .isInstanceOf(IllegalArgumentException.class);
+            });
+        }
     }
 
     @Test
-    void 음수기호_중간() {
+    void 잘못된_커스텀구분자() {
+        String[] inputStrings = {"/;\\n1;2", "//;\\1;2", "\\n;//1;2"};
+        for (String inputString : inputStrings) {
+            assertSimpleTest(() -> {
+                assertThatThrownBy(() -> runException(inputString))
+                        .isInstanceOf(IllegalArgumentException.class);
+            });
+        }
+    }
+
+    @Test
+    void 커스텀_구분자_여러글자_일부_사용() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("1,-2"))
+            assertThatThrownBy(() -> runException("//;@\\n1;2"))
                     .isInstanceOf(IllegalArgumentException.class);
         });
     }
-
-    // 정상 케이스
 
     @Override
     public void runMain() {
