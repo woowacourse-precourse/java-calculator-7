3e1813ebe52c783e9ae254f038f29295682c0742
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-20 05:00:38 +0900

    feat(Application): add 메인 클래스 및 프로그램 실행 흐름
    프로그램 실행을 위한 Application 클래스
    - IOService, DelimiterService, NumberService의 인스턴스를 생성하고, Coordinator에 주입하여 실행
    - 입력 -> 구분자 처리 -> 숫자 변환 및 계산 -> 결과 출력 흐름을 담당

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,19 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 입력과 출력 담당
+        IOService ioService = new IOService();
+
+        // 구분자 처리 담당
+        DelimiterService delimiterService = new DelimiterService();
+
+        // 숫자 처리 및 계산 담당
+        NumberService numberService = new NumberService();
+
+        // Coordinator에 필요한 의존성 주입
+        Coordinator coordinator = new Coordinator(ioService, delimiterService, numberService);
+
+        // 프로그램 실행
+        coordinator.execute();
     }
 }
