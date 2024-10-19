e7c9017e2fee7903693e796f06e983a3d78c4a21
JEN <ekffuclxk@gmail.com>
2024-10-19 12:35:47 +0900

    feat: 공백 문자열 입력 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,11 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        int result = 10;
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        System.out.println("Input : " + input);
+        // 엔터, 공백 입력
+        if (input.isBlank()) {
+            result = 0;
+        }
+
+        System.out.println("Input : " + result);
 
     }
 }
