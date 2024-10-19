c22227298eafdd7af16c2284d6bef13c7859e896
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-20 05:36:10 +0900

    feat: 숫자 Service 구현
    - 숫자에 대한 작업을 책임지는 클래스.

---
+++ src/main/java/calculator/service/NumberService.java
@@ -0,0 +1,16 @@
+package calculator.service;
+
+import calculator.util.number.NumberUtils;
+
+import java.util.List;
+
+public class NumberService {
+
+    public List<? extends Number> convertFrom(List<String> value, Class<? extends Number> numberClass) {
+        return NumberUtils.parseNumbers(value, numberClass);
+    }
+
+    public Number sum(List<? extends Number> numbers, Class<? extends Number> numberClass) {
+        return NumberUtils.sum(numbers, numberClass);
+    }
+}
