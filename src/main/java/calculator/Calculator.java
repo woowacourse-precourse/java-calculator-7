04569af639acd33b6ad22b62519afb6d00bdf016
Bae-JunSeok <qownstjr77@gmail.com>
2024-10-20 04:56:46 +0900

    feat: 커스텀 구분자를 사용한 문자열 분리 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
     private static final String DEFAULT_DELIMITERS = ",|:";
+    private static final String PREFIX_CUSTOM = "//";
+    private static final String POSTFIX_CUSTOM = "\\n";
+    private static final int CUSTOM_DELIMITER_LENGTH = 2;
 
     public static void run() {
         System.out.println("덧셈할 문자열을 입력해주세요.");
@@ -17,6 +20,29 @@ public class Calculator {
     }
 
     public static String[] splitString(String input) {
-        return input.split(DEFAULT_DELIMITERS);
+        String delimiters = extractDelimiters(input);
+        String numbersPart = extractNumbers(input);
+        return numbersPart.split(delimiters);
+    }
+
+    private static String extractDelimiters(String input) {
+        String delimiters = DEFAULT_DELIMITERS;
+        if (hasCustomDelimiter(input)) {
+            int customDelimiterEndIdx = input.indexOf(POSTFIX_CUSTOM);
+            delimiters = input.substring(CUSTOM_DELIMITER_LENGTH, customDelimiterEndIdx);
+        }
+        return delimiters;
+    }
+
+    private static String extractNumbers(String input) {
+        if (hasCustomDelimiter(input)) {
+            int customDelimiterEndIdx = input.indexOf(POSTFIX_CUSTOM);
+            return input.substring(customDelimiterEndIdx + 2);
+        }
+        return input;
+    }
+
+    private static boolean hasCustomDelimiter(String input) {
+        return input.startsWith(PREFIX_CUSTOM) && input.contains(POSTFIX_CUSTOM);
     }
 }
