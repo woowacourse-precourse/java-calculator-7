c9485c7ccbf57de0834035d07f0f544c7932bc5a
2jin2.1031 <2jin2.1031@gmail.com>
2024-10-19 14:35:11 +0900

    feat: 0 입력 시 예외 처리

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -36,8 +36,8 @@ public class Input {
         }
 
         for (int number : numbers) {
-            if (number < 0) {
-                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+            if (number <= 0) {
+                throw new IllegalArgumentException("0 또는 음수는 입력할 수 없습니다.");
             }
         }
         return numbers;
