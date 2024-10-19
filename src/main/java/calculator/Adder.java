eb795b657bdac2c1d2db3487845e7f1fb932872b
moonbinchoi <whsahd12@gmail.com>
2024-10-19 15:36:47 +0900

    refactor(Adder): 가산 기능만 하도록 수정
    피연산자 추출 및 검증 기능을 `Expression` 클래스로 이동함
    
    BREAKING CHANGES: `Adder.run()`에서 `Adder.sum()` 으로 변경

--- src/main/java/calculator/Adder.java
+++ src/main/java/calculator/Adder.java
@@ -1,54 +1,25 @@
 package calculator;
 
 import java.util.*;
-import java.util.regex.Pattern;
 
-public class Adder {
+public class Adder { //TODO: 싱글톤을 고려해볼 것
 
     private final Set<String> delimiters;
-    private final List<String> operands = new ArrayList<>();
-    private final String expr;
+    private final List<String> operands;
 
 
-    public Adder(String expr) {
-        this(expr, Set.of());
+    public Adder(List<String> operands) {
+        this(Set.of(), operands);
     }
 
-    public Adder(String expr, Collection<String> delimiters) {
-        this.expr = expr;
+    public Adder(Set<String> delimiters, List<String> operands) {
         this.delimiters = new HashSet<>(Set.of(",", ":"));
         this.delimiters.addAll(delimiters);
+
+        this.operands = new ArrayList<>(operands);
     }
 
-    public int run() {
-        extractOperands();
-        validate();
-
-        return getTotalSum();
-    }
-
-    private void extractOperands() {
-        operands.addAll(
-                Arrays.stream(expr.split(buildDelimiterRegex()))
-                        .map(String::trim)
-                        .toList());
-    }
-
-    private void validate() {
-        for (String delimiter : delimiters) {
-            if (isNumeric(delimiter)) {
-                throw new IllegalArgumentException("커스텀 구분자는 숫자로만 이루어져서는 안됩니다: " + delimiter);
-            }
-        }
-
-        for (String operand : operands) {
-            if (!isNumeric(operand)) {
-                throw new IllegalArgumentException("올바르지 않은 피연산자입니다: " + operand);
-            }
-        }
-    }
-
-    private int getTotalSum() {
+    public int sum() {
         int sum = 0;
 
         for (String operand : operands) {
@@ -58,18 +29,4 @@ public class Adder {
     }
 
 
-    private String buildDelimiterRegex() {
-        final StringBuilder sb = new StringBuilder();
-        for (String delimiter : delimiters) {
-            sb.append(Pattern.quote(delimiter));
-            sb.append("|");
-        }
-        sb.delete(sb.length() - 1, sb.length());
-
-        return sb.toString();
-    }
-
-    private boolean isNumeric(String str) {
-        return str.chars().allMatch(Character::isDigit);
-    }
 }
