401cf8826e64ce8e61e99eff6b2fe567add3ea2b
Hanjjong <tim4974@naver.com>
2024-10-20 01:32:11 +0900

    feat(CalculatorOutputHandler): 결과 출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,10 +3,12 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         CalculatorInputHandler calculatorInputHandler = new CalculatorInputHandler();
+        CalculatorOutputHandler calculatorOutputHandler = new CalculatorOutputHandler();
 
         String input = calculatorInputHandler.input();
         Parser parser = new DefaultParser(input);
         Alu AddUnit = new AddUnit(parser.parse());
         Calculator calculator = new Calculator(AddUnit);
+        calculatorOutputHandler.output(calculator.calculate());
     }
 }
---
+++ src/main/java/calculator/CalculatorOutputHandler.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class CalculatorOutputHandler {
+    public CalculatorOutputHandler() {
+    }
+
+    public void output(Long number) {
+        System.out.println("결과 : " + number);
+
+    }
+}
