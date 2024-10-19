3cdb66c02cc86d3b90c79d5aaec0de9f8977ad86
ssggii <haan7525@gmail.com>
2024-10-19 17:59:33 +0900

    feat: 덧셈 수행 및 결과 출력

--- src/main/java/calculator/OperationProcessor.java
+++ src/main/java/calculator/OperationProcessor.java
@@ -4,18 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OperationProcessor {
-    char operator;
     List<Integer> operands; // 피연산자(숫자)
 
     public OperationProcessor() {
-        operator = '+';
         operands = new ArrayList<>();
     }
 
     /**
-     * 연산 수행
+     * 덧셈 연산 수행
      */
     public int calculate(List<Integer> operands) {
-        return 0;
+        int result = 0;
+        for (int operand : operands) {
+            result += operand;
+        }
+        return result;
     }
 }
