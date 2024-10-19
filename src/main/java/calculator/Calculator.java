437a5cd68bda5c39d1bf996978f40ccd32ad0d64
jinwook <wjswlsdnr333@gmail.com>
2024-10-19 20:32:46 +0900

    fix: 입력는경우 출력 함수 추가 및 변경

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,6 @@
 package calculator;
+import org.junit.jupiter.api.DisplayName;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,30 +13,18 @@ public class Calculator {
     private List<String> parts;
 
     public Calculator(String input) {
-        if(input.isEmpty() || !input.matches(".*\\d.*")) {
-            System.out.println("결과 : 0");
-        }
-
-        if(input.matches(".*-\\d.*")){
-            throw new IllegalArgumentException("음수값이 포함되면 안됩니다.");
-        }
-
         this.No_Space_Input = input.replaceAll("\\s+", "");
     }
-    /*
-    public void print(){
-        Custom_Deli(); // 커스텀 구분자 추가
-        split_input(); // 커스텀 구분자 구문 삭제된 문자열에서 구분자를 이용해 문자열 구분 후 parts 리스트 저장
-        double_check(parts); // 구분자로 나뉜 문자열 실수형 체크
-        System.out.println(parts);
-        //System.out.println(Delimiter);
-        //System.out.println(del_deli_input);
-        //System.out.println(No_Space_Input);
-    }
-     */
 
     public void add(){
         double sum = 0;
+        if(isEmpty()){
+            System.out.println("결과 : 0");
+            return;
+        }
+        if(isNegative()){
+            throw new IllegalArgumentException("음수값이 포함되면 안됩니다.");
+        }
         Custom_Deli(); // 커스텀 구분자 추가
         split_input(); // 커스텀 구분자 구문 삭제된 문자열에서 구분자를 이용해 문자열 구분 후 parts 리스트 저장
         double_check(parts); // 구분자로 나뉜 문자열 실수형 체크
@@ -42,6 +32,14 @@ public class Calculator {
             sum += Double.parseDouble(num);
         }
         System.out.println("결과 : " + sum);
+    }
+
+    public boolean isEmpty(){
+        return (No_Space_Input.isEmpty() || !No_Space_Input.matches(".*\\d.*"));
+    }
+
+    public boolean isNegative(){
+        return No_Space_Input.matches(".*-\\d.*");
     }
 
     public void Custom_Deli(){
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -32,95 +32,14 @@ class ApplicationTest extends NsTest {
         });
     }
 
-    /*
     @Test
-    void Input_음수판단(){
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("-15:523"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-    @Test
-    void 구분된_숫자_실수판별_성공(){
-        assertSimpleTest(() ->{
-            run("//*\\n37*38.7:34.5");
-            assertThat(output()).contains("[37, 38.7, 34.5]");
-        });
-    }
-    @Test
-    void 구분된_숫자_실수판별_실패1(){
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("//*\\n37*38..7:34.5"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 구분된_숫자_실수판별_실패2(){
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("//*\\n37*.38.7:34.5"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 구분된_숫자_실수판별_실패3(){
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("//*\\n37*.38.7:34.5."))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-
-    @Test
-    void Input_구분자_구분(){
-        assertSimpleTest(() ->{
-            run("//#\\n39:24#72");
-            assertThat(output()).contains("[39, 24, 72]");
+    void 입력_x() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
         });
     }
 
-    @Test
-    void Input_구분자_구분_특수문자(){
-        assertSimpleTest(() ->{
-            run("//+\\n21+23+52");
-            assertThat(output()).contains("[21, 23, 52]");
-        });
-    }
-    @Test
-    void Input_커스텀구분자_중간(){
-        assertSimpleTest(() ->{
-            run("14$23$//$\\n72");
-            assertThat(output()).contains("[14, 23, 72]");
-        });
-    }
-
-    @Test
-    void Input_구분_문자포함(){
-        assertSimpleTest(() ->
-        assertThatThrownBy(() -> runException("1sf4$2@#3$//$\\n72"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void Input_커스텀구문자_추가1(){
-        assertSimpleTest(() ->{
-            run("//#\\n3924");
-            assertThat(output()).contains("[,, :, #]");
-            assertThat(output()).contains("3924");
-        });
-    }
-    @Test
-    void Input_커스텀구문자_추가2(){
-        assertSimpleTest(() ->{
-            run("72//#\\n28");
-            assertThat(output()).contains("[,, :, #]");
-            assertThat(output()).contains("7228");
-        });
-    }
-
-     */
 
     @Override
     public void runMain() {
