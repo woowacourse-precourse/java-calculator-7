2c1592daa4a5d63bf609b6d06cfe37401bceb72f
koomchang <leahpar0401@gmail.com>
2024-10-20 04:23:17 +0900

    test: 기본 구분자가 컴마로만 이루어지면 테스트를 통과한다.

---
+++ src/test/java/calculator/validator/InputValidatorTest.java
@@ -0,0 +1,29 @@
+package calculator.validator;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class InputValidatorTest {
+    private InputValidator inputValidator;
+
+    @BeforeEach
+    void setUp() {
+        inputValidator = new InputValidator();
+    }
+
+    @Nested
+    @DisplayName("기본 구분자 입력값 테스트")
+    class DefaultInputTest {
+
+        @Test
+        void 기본_구분자가_컴마_로만_이루어지면_테스트를_통과한다() {
+            assertDoesNotThrow(() -> inputValidator.validate("1,2,3"));
+        }
+
+    }
+}
