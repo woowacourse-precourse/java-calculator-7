dd50e3f0e848a8749703637726ce59c8887b40d1
mingking2 <mingee641@gmail.com>
2024-10-19 11:06:15 +0900

    feat: 문자열 계산기 1차 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,25 +3,56 @@ package calculator;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        int result = 0;
-        String input = readLine();
-        System.out.println("input = " + input);
 
-        int len = input.length();
-        for (int i = 0; i < len; i++) {
-            if (isNumber(input.charAt(i))) {
-                System.out.println(" number = " + input.charAt(i));
-            } else if (isBasicSeparator(input.charAt(i))) {
-                System.out.println(" basic = " + input.charAt(i));
-            } else if (isSpecialSeparator(input.charAt(i), input)) {
-                System.out.println(" special = " + input.charAt(i));
+    private static final String CUSTOM_PREFIX = "//";
+    private static final String CUSTOM_SUFFIX = "\\n";
+    private static final String DEFAULT = ",|:";
+
+
+    public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = readLine();
+
+        int result = 0;
+        try {
+            checkDigit(input);
+            char custom = hasSpecialSeparator(input);
+            if (custom != 0) {
+                input = input.substring(5);
+                String[] input_array = input.split(String.valueOf(custom));
+                checkPositiveNumber(input_array);
+                for (String s : input_array) {
+                    //System.out.println(s);
+                    result += Integer.parseInt(s);
+                }
+            } else {
+                String[] input_array = input.split(DEFAULT);
+                checkPositiveNumber(input_array);
+                for (String s : input_array) {
+                    //System.out.println(s);
+                    result += Integer.parseInt(s);
+                }
             }
+
+            System.out.println("결과 : " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public static boolean isNumber(char c) {
+    private static void checkDigit(String input) {
+        int tmp = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (isNumber(input.charAt(i))) {
+                tmp = 1;
+            }
+        }
+        if (tmp == 0) {
+            throw new IllegalArgumentException("숫자가 존재하지 않습니다.");
+        }
+    }
+
+    private static boolean isNumber(char c) {
         int tmp = (int) c;
         if (c >= '0' && c <= '9') {
             return true;
@@ -29,19 +60,19 @@ public class Application {
         return false;
     }
 
-    public static boolean isBasicSeparator(char c) {
-        if (c == ';' || c == ',') {
-            return true;
+
+    private static char hasSpecialSeparator(String input) {
+        if (input.startsWith(CUSTOM_PREFIX) && input.substring(3, 5).equals(CUSTOM_SUFFIX)) {
+            return input.charAt(2);
         }
-        return false;
+        return 0;
     }
 
-    public static boolean isSpecialSeparator(char c, String input) {
-        if (input.charAt(0) == '/' && input.charAt(1) == '/' && input.charAt(3) == '\\' && input.charAt(4) == 'n') {
-            if (c == input.charAt(2)) {
-                return true;
+    private static void checkPositiveNumber(String[] array) {
+        for (String s : array) {
+            if (Integer.parseInt(s) < 0) {
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
             }
         }
-        return false;
     }
 }
