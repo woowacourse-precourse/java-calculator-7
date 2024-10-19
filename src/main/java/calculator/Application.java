1ca86387bd6f6e29e275bd53373580cae2bffe8c
Yongjin <hi653@naver.com>
2024-10-20 02:53:18 +0900

    feat: 문자열 입력받기 기능 구현
    Application class에 대한 주석과 사용자로부터 문자열을 입력받기 위한 코드 작성.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+/*
+ * 입력한 문자열에서 숫자를 추출하여 더하는 계산기를 구현한다.
+ */
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 문자열 입력받기 기능 구현.
+        System.out.println("문자열을 입력하세요.");
+        String input = Console.readLine();
+
+
+
+
+
     }
 }
