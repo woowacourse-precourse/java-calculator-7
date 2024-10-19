b04c36829ba50f41bc13142b149cb345fe47a288
dnwldla <leeemjw@naver.com>
2024-10-19 22:01:39 +0900

    Refactor: 특수기호를 상수로 변경

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -11,6 +11,8 @@ public class Calculator {
     private static final Pattern CUSTOM_SEPERATOR_PATTERN=Pattern.compile("^//(.)\\\\n{1}(.*)");
 
     private static String seperatorRegex=",|:";
+    private static final String SPLITOR="|";
+    private static final String ESCAPE_CHAR="\\";
 
     public static int inputCalculate(String input){
         //커스텀 구분자 패턴이 존재하는지 확인한다
@@ -50,12 +52,12 @@ public class Calculator {
     private static void addRegex(String customSeperator) {
         StringBuilder regexBuilder=new StringBuilder();
         //커스텀 구분자가 | 이라면 이스케이프문자를 추가해햐 한다
-        if (customSeperator.equals("|")){
+        if (customSeperator.equals(SPLITOR)){
             customSeperator=addEscapeChar(customSeperator);
         }
 
         regexBuilder.append(seperatorRegex);
-        regexBuilder.append("|");
+        regexBuilder.append(SPLITOR);
         regexBuilder.append(customSeperator);
 
        seperatorRegex=regexBuilder.toString();
@@ -64,7 +66,7 @@ public class Calculator {
     private static String addEscapeChar(String customSeperator){
         StringBuilder seperatorBuilder=new StringBuilder();
 
-        seperatorBuilder.append("\\");
+        seperatorBuilder.append(ESCAPE_CHAR);
         seperatorBuilder.append(customSeperator);
 
         return customSeperator.toString();
