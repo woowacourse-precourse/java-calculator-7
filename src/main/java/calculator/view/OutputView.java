513be31da1ecdfc29b602f240568d8aa837a16c0
WonJun Park <tngkrt2004@gmail.com>
2024-10-19 16:33:46 +0900

    refactoring(view): 입력 구문 OutputView로 이동

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,8 +1,11 @@
 package calculator.view;
 
 public class OutputView {
+    public void printInput(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
     public void printResult(int result) {
         System.out.println("결과 : " + result);
     }
-
 }
