ea41e551b7650d384a0a126a00b99945d6a684bd
minjae k <rkdalswoals@gmail.com>
2024-10-20 00:26:22 +0900

    chore: remove comments from InputHandler and Parser

--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -3,7 +3,6 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
-    //사용자 입력을 받아오는 메서드
     public String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
