e2deedbc4ca33adeee3955269cc07d6fca1f5194
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-19 21:07:35 +0900

    feat(StringCalculatorApp): 문자열 입력받기 기능 작성
    - Console을 활용하여 문자열을 입력받는 readInput() 함수 작성

--- src/main/java/calculator/app/StringCalculatorApp.java
+++ src/main/java/calculator/app/StringCalculatorApp.java
@@ -1,11 +1,23 @@
 package calculator.app;
 
 import calculator.calculator.Calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class StringCalculatorApp {
     private Calculator stringCalculator;
 
     public StringCalculatorApp(Calculator stringCalculator) {
         this.stringCalculator = stringCalculator;
+    }
+
+    public void run() {
+        String inputData = readInput();
+
+    }
+
+    // 1. 문자열 입력받기
+    public String readInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
     }
 }
