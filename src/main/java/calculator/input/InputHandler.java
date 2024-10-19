72bd283431bdba6d78329cfec7cb7b263bc86cb9
SongJaeHoonn <thdwogns121@naver.com>
2024-10-19 23:23:25 +0900

    refactor: InputHandler 싱글톤 패턴으로 변경

--- src/main/java/calculator/input/InputHandler.java
+++ src/main/java/calculator/input/InputHandler.java
@@ -4,6 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
 
+    private static final InputHandler INSTANCE = new InputHandler();
+
+    private InputHandler() {}
+
+    public static InputHandler getInstance() {
+        return INSTANCE;
+    }
+
     public String getInput() {
         System.out.println("문자열을 입력해주세요");
         return Console.readLine();
