c776cbcbe0ff6a895825b7c02f9baa81f841e11d
JooIcksoo <dlrtn751@gmail.com>
2024-10-20 03:21:27 +0900

    feat(StringAddCalculator): 입력 문자열 파싱 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,12 +1,20 @@
 package calculator;
 
+import java.util.List;
+
 public class StringAddCalculator {
     InputProcessor inputProcessor = new InputProcessor();
     MessagePrinter messagePrinter = new MessagePrinter();
 
+
     public void run() {
         messagePrinter.printInputRequest();
-
         inputProcessor.readInput();
+
+        StringParser stringParser = new StringParser(inputProcessor.getInput());
+        List<Integer> numbers = stringParser.parse();
+
+
+
     }
 }
