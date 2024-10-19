047db6ba80bbfaff1b3c1ad445752170858f3261
RIWON <121172065+awrion3@users.noreply.github.com>
2024-10-19 18:49:16 +0900

    refactor: 메서드 세분화 및 변수명 가독성 개선

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -1,61 +1,118 @@
 package calculator;
 
 public class Controller {
+    private boolean isSuccess;
+    private StringData stringData;
+    private SumData sumData;
+
     public void controlFlow() throws IllegalArgumentException {
         try {
-            StringData stringData = new StringData();
-            Input input = new Input();
-            String inputString = input.requestInput(stringData);
-            stringData.setInputString(inputString);
+            stringData = new StringData();
+            sumData = new SumData();
 
-            SumData sumData = new SumData();
-            Output output = new Output(sumData);
+            controlInput();
+
+            Output output = new Output();
             Checker checker = new Checker(stringData);
-            if (checker.checkIsNotString()) {
-                output.formatOutput();
+
+            isSuccess = controlStringChecker(checker);
+            if (isSuccess) {
+                controlOutput(output);
                 return;
             }
 
             Calculator calculator = new Calculator(stringData, sumData);
-            if (checker.checkIsSingleNumber()) {
-                Double sumResult = calculator.calculateSingleValue();
-                sumData.setSumData(sumResult);
-                output.formatOutput();
+
+            isSuccess = controlNumberChecker(checker, calculator);
+            if (isSuccess) {
+                controlOutput(output);
                 return;
             }
 
-            String separator = checker.checkCustomSeparator();
-            stringData.setSeparator(separator);
-            if (separator != null) {
-                stringData.setHasCustomSeparator(true);
-            }
-            if (separator == null) {
-                separator = checker.checkDefaultSeparator();
-                stringData.setSeparator(separator);
-            }
+            controlSeparatorChecker(checker);
+            controlCleaner();
 
-            boolean hasCustomSeparator = stringData.getHasCustomSeparator();
-            boolean isEmptyString = false;
-            if (hasCustomSeparator) {
-                Cleaner cleaner = new Cleaner(stringData);
-
-                String refinedString = cleaner.refineString();
-                stringData.setInputString(refinedString);
-                isEmptyString = refinedString.isEmpty();
-
-                String refinedSeparator = cleaner.refineSeparator();
-                stringData.setSeparator(refinedSeparator);
-            }
-            if (isEmptyString) {
-                output.formatOutput();
+            isSuccess = controlStringChecker(checker);
+            if (isSuccess) {
+                controlOutput(output);
                 return;
             }
 
-            Double sumResult = calculator.calculateString();
-            sumData.setSumData(sumResult);
-            output.formatOutput();
+            isSuccess = controlCalculator(calculator);
+            controlOutput(output);
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public void controlInput() {
+        Input input = new Input();
+        String inputString = input.requestInput();
+
+        stringData.setInputString(inputString);
+    }
+
+    public void controlOutput(Output output) {
+        if (isSuccess) {
+            output.formatOutput(sumData);
+        }
+    }
+
+    public boolean controlStringChecker(Checker checker) {
+        isSuccess = false;
+
+        if (checker.checkIsNotString()) {
+            isSuccess = true;
+        }
+        return isSuccess;
+    }
+
+    public boolean controlNumberChecker(Checker checker, Calculator calculator) {
+        isSuccess = false;
+
+        if (checker.checkIsSingleNumber()) {
+            Double sumResult = calculator.calculateSingleValue();
+
+            sumData.setSumData(sumResult);
+            isSuccess = true;
+        }
+        return isSuccess;
+    }
+
+    public void controlSeparatorChecker(Checker checker) throws IllegalArgumentException {
+        String separator = checker.checkCustomSeparator();
+
+        stringData.setSeparator(separator);
+        if (separator != null) {
+            stringData.setHasCustomSeparator(true);
+        }
+
+        if (separator == null) {
+            separator = checker.checkDefaultSeparator();
+            stringData.setSeparator(separator);
+        }
+    }
+
+    public void controlCleaner() {
+        boolean hasCustomSeparator = stringData.getHasCustomSeparator();
+
+        if (hasCustomSeparator) {
+            Cleaner cleaner = new Cleaner(stringData);
+
+            String refinedString = cleaner.refineString();
+            stringData.setInputString(refinedString);
+
+            String refinedSeparator = cleaner.refineSeparator();
+            stringData.setSeparator(refinedSeparator);
+        }
+    }
+
+    public boolean controlCalculator(Calculator calculator) throws NumberFormatException {
+        Double sumResult = calculator.calculateString();
+
+        sumData.setSumData(sumResult);
+        isSuccess = true;
+
+        return isSuccess;
     }
 }
