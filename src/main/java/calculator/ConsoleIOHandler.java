bb6ab1e583da8bec7d8b0b11f586cc7e94a97223
moonbinchoi <whsahd12@gmail.com>
2024-10-19 15:39:34 +0900

    refactor(ConsoleIOHandler): 클래스명 변경, 입출력값 제어 기능만 하도록 수정
    커스텀 구분자 추출 및 검증 기능을 `Expression` 클래스로 이동함
    
    BREAKING CHANGES: `ExpressionInputReader`에서 `ConsoleIOHandler`로 클래스명 변경
    - try-with-resources 문을 사용하도록 수정
    - `print()`기능 추가

---
+++ src/main/java/calculator/ConsoleIOHandler.java
@@ -0,0 +1,26 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleIOHandler implements AutoCloseable {
+
+    public String read() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String input = Console.readLine();
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("빈 문자열은 입력할 수 없습니다.");
+        }
+
+        return input;
+    }
+
+    public void print(Object obj) {
+        System.out.println("결과 : " + obj);
+    }
+
+    @Override
+    public void close() throws Exception {
+        Console.close();
+    }
+}
