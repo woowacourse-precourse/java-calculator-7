c60ea305daccb1b7dc043fe70210ddfa246a75a4
Park Yu Han <pbk2312@naver.com>
2024-10-19 23:55:47 +0900

    chore: 불필요한 주석 제거

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -5,17 +5,15 @@ import java.util.List;
 // 문자열 배열의 숫자를 합산하는 계산기
 public class StringCalculator {
 
-    // List로 전달된 숫자들을 합산하는 메소드
     public int sumNumbers(List<String> numbers) {
         return calculateSum(numbers);
     }
 
-    // Stream을 사용하여 List의 숫자들을 합산하는 메소드
     private int calculateSum(List<String> numbers) {
         return numbers.stream()
-                .map(String::trim)  // 문자열 앞뒤 공백 제거
-                .mapToInt(Integer::parseInt)  // 숫자로 변환
-                .sum();  // 합산
+                .map(String::trim)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 
 }
--- src/main/java/calculator/model/StringParser.java
+++ src/main/java/calculator/model/StringParser.java
@@ -7,7 +7,6 @@ import java.util.List;
 // 입력 문자열을 파싱하는 클래스
 public class StringParser {
 
-    // 입력 문자열을 파싱하여 숫자 리스트를 반환하는 메서드
     public List<String> parseInput(String input) {
         String[] numbersArray = DelimiterUtils.splitByDelimiter(input);
         return Arrays.asList(numbersArray);
