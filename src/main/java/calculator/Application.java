21a3a00db1dbad75f1ef11b5ba83753d6b607b00
seonseon1515 <seonseon1515@gmail.com>
2024-10-19 17:29:54 +0900

    feat: 입력한 문자열에서 기본 구분자, 커스텀 구분자 찾아내기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,5 +11,18 @@ public class Application {
     // 문자열 입력 받기
     System.out.println("덧셈할 문자열을 입력해 주세요.");
     String inputStr = Console.readLine();
+
+    // 입력한 문자열에서 기본 구분자, 커스텀 구분자 찾아내기
+    String customPolymerPattern = "//[^\\w\\s]";
+    String basicPolymerList = "[,:]";
+    String customPolymer = "";
+
+    Pattern pattern = Pattern.compile(customPolymerPattern);
+    Matcher matcher = pattern.matcher(inputStr);
+
+    if(matcher.find() && inputStr.substring(3,5).equals("\\n")){
+        customPolymer = inputStr.substring(2,3);
+    }
+
     }
 }
