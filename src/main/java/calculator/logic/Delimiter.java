7a79a9a8fce78153e3350a9d339f476d66bc421a
장시영 <sysmile77@gmail.com>
2024-10-19 20:17:34 +0900

    fear(Delimiter): 일반문자열 분리, 커스텀 문자열 분리

--- src/main/java/calculator/logic/Delimiter.java
+++ src/main/java/calculator/logic/Delimiter.java
@@ -1,6 +1,26 @@
 package calculator.logic;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class Delimiter {
-    public static void execution() {
+    public static String[] execution(String str, boolean n) {
+
+        if(n) {
+            return doCustomDelimiter(str);
+        } else {
+            return  doNormalDelimiter(str);
+        }
+    }
+
+    private static String[] doNormalDelimiter(String str) {
+        String delimiter = ",|:";
+        return str.split(delimiter);
+    }
+
+    private static String[] doCustomDelimiter(String str) {
+        String delimiter = CustomChecker.getCustom(str);
+        String adjustedStr = str.substring(delimiter.length()+4);
+        return adjustedStr.split(delimiter);
     }
 }
