d42243f701332bb165004f907bbcdfb59a5dd689
koomchang <leahpar0401@gmail.com>
2024-10-20 01:41:37 +0900

    feat: 입력을 담당하는 InputView 생성

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,7 +1,7 @@
 package calculator.view;
 
 public class OutputView {
-    public void askForString() {
+    public void askForStringToCalculate() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 }
