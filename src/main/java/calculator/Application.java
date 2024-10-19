e4701f5a89c801ce7bc0d00313e78bc772c6df07
Jinho622 <aceho21@gmail.com>
2024-10-19 21:10:17 +0900

    feat(): 쉼표(,)와 콜론(:)으로 구분되어 있는 숫자의 합 반환
    calculate, sum 함수 추가
    쉼표와 콜론 그리고 숫자만 입력값으로 들어왔을때 값을 반환해준다.
    커스텀 구분자와 예외상항은 아직 처리해주지 않았다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,41 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static java.lang.Character.isDigit;
+
 public class Application {
+
+    private static String delimiters = "[,:]";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine(); // 사용자 입력을 받는 메소드
+
+        int RESULT = calculate(input);
+        System.out.println("결과 : " + RESULT);
+
+    }
+
+    public static int calculate (String input){
+        if(input.equals("")){ // 빈 값 처리해주기
+            return 0;
+        }
+        return sum(input);
+    }
+
+    private static int sum (String numbers) {
+        int SUM = 0;
+        String[] splitedNumbers = numbers.split(delimiters);
+        for (String number : splitedNumbers) {
+                int NUM = Integer.parseInt(number);
+            if (NUM < 0) {
+                throw new IllegalArgumentException("음수는 허용되지 않습니다: " + NUM);
+            }
+            SUM += NUM;
+        }
+        return SUM;
     }
 }
