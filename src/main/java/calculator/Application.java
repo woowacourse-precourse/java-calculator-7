1f101e222c818b6d2a642efc6f1285796db2ae40
jarvis0720 <gktkddms9631@gmail.com>
2024-10-19 23:49:16 +0900

    feat(Application): 기본 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String str = readLine();
+        if (str.isEmpty()) {
+            System.out.println("결과 : 0");
+            return;
+        }
+        if (str.contains("//")){
+
+        }
+        String regex = "[:,]";
+        String[] numbers = str.split(regex);
+        int sum = 0;
+
+        int[] intArray = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) intArray[i] = Integer.parseInt(numbers[i].trim());
+
+        for (int num : intArray) sum += num;
+        System.out.println("결과 : " + sum);
+
     }
 }
