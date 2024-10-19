0786732cef838250b70fd33e09601245957dfb2e
jongseo <jongseo0703@naver.com>
2024-10-19 20:23:02 +0900

    feat: 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException발생 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -10,20 +10,44 @@ public class StringCalculator {
 
         List<Integer> numbers = new ArrayList<>();
         StringBuilder currentNum = new StringBuilder();
+        boolean isNegative = false; // 음수를 감지하는 플래그
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i); // 입력한 문자열에서 해당 인덱스 문자를 가져옴
-            if (Character.isDigit(c)) {
+
+            if (c == '-') { // '-' 기호가 있으면 음수임
+                isNegative = true;
+            }
+
+            else if (Character.isDigit(c)) {
                 currentNum.append(c); // 숫자면 currentNum에 추가
             }
             else if (currentNum.length() > 0) {
+                int num = Integer.parseInt(currentNum.toString());
+                if (isNegative) {
+                    num = -num;
+                }
+
+                if (num <= 0) {
+                    throw new IllegalArgumentException();
+                }
                 numbers.add(Integer.parseInt(currentNum.toString())); // 현재 숫자 리스트에 추가
                 currentNum.setLength(0); // currentNum 초기화
+                isNegative = false; // 플래그 초기화
             }
         }
 
         if (currentNum.length() > 0) { // 문자열 끝에 숫자가 남아있으면 추가
-            numbers.add(Integer.parseInt(currentNum.toString()));
+            int num = Integer.parseInt(currentNum.toString());
+            if (isNegative) {
+                num = -num;
+            }
+            if (num <= 0) {
+                throw new IllegalArgumentException();
+            }
+
+            numbers.add(num);
+
         }
 
         return numbers; // 추출한 숫자 리스트를 반환함.
