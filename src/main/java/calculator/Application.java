5d031104d6d1da95cedc1a3f124aa652d50bcfd6
Lee Jeong Woo <sysliar@gmail.com>
2024-10-20 04:22:57 +0900

    feat: 커스텀 구분자 사용 여부 확인

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,11 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+    public static boolean hasCustomDelimiter(String input) {
+        return input.startsWith("//") && input.contains("\\n");
+    }
+
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
@@ -10,5 +15,6 @@ public class Application {
             throw new IllegalArgumentException("문자열 입력하지 않음");
         }
 
+        boolean hasCustomDelimiter = hasCustomDelimiter(input);
     }
 }
