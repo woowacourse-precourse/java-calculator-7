f4c92bf9b57fab32d519aecf4c716f11f9828a85
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-19 15:33:35 +0900

    Feat: 공통 로직에 진입한 이후 구분자로 나눈 요소가 숫자가 아닌 경우를 위한 열거형 상수 구현

--- src/main/java/calculator/service/exception/SeparationExceptionType.java
+++ src/main/java/calculator/service/exception/SeparationExceptionType.java
@@ -6,11 +6,12 @@ public enum SeparationExceptionType implements ExceptionType {
 
     // SeparationService
     NO_INPUT("값을 입력하지 않았습니다."),
-    // CustomSeparationService
-    NOT_EXIST_CUSTOM_SEPARATOR("커스텀 구분자가 존재하지 않습니다."),
     STARTS_WITH_SEPARATOR("입력값이 구분자로 시작합니다."),
     ENDS_WITH_SEPARATOR("입력값이 구분자로 끝납니다."),
     INVALID_SEPARATOR("유효한 구분자가 아닙니다."),
+    NOT_A_POSITIVE_NUMBER("양수가 아닙니다."),
+    // CustomSeparationService
+    NOT_EXIST_CUSTOM_SEPARATOR("커스텀 구분자가 존재하지 않습니다."),
     ;
 
     private final String message;
