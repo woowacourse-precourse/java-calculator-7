82d2b2ed614072ecd75aa1c0c3b1edb25653d489
howareyoo <newworld121@naver.com>
2024-10-19 16:54:46 +0900

    feat : 입력 안내 메세지 출력

---
+++ src/main/java/calculator/CalculatorController.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CalculatorController {
+
+    public void inputString(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+}
