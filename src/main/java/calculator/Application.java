bb644d261a316dd6b327b1c1ef50d0b222934b64
zoooooz2 <92962527+zoooooz2@users.noreply.github.com>
2024-10-19 23:17:31 +0900

    docs(README): README.md 파일 수정
    - 프로젝트에 불필요한 부분 삭제
    - 디자인 수정
    
    feat(Application): 덧셈 기능, 결과 출력 로직 추가
    
    - 기본 구분자(',' ':')를 기준으로 문자 분리-> 공백 제거-> 덧셈 로직 구현
    - NumberFormatException으로 잘못된 문자입력 시 예외처리 후 종료 로직 추가
    - 결과 출력 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,14 +7,32 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.\n");
         String numInput = Console.readLine();
 
-        try {
-            int num = Integer.parseInt(numInput);
-            if(num < 0) {
-                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+        // 기본 구분자 ',' ':'로 문자열 분리
+        String[] numbers = numInput.split("[,:]", -1); // -1은 빼고 될 듯. 일단 나중에 생각
+
+        int sum = 0; // 덧셈 결과 저장할 변수
+
+        for (String number : numbers) {
+            number = number.trim(); // 숫자 앞 뒤 공백 제거
+
+            if (number.isEmpty()) { // 빈 문자열 체크
+                //sum += 0;
+                continue;
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return;
+            try {
+                int num = Integer.parseInt(number);
+                if (num < 0) {
+                    throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+                }
+                sum += num; // 덧셈
+            } catch (NumberFormatException e) {
+                System.out.println("기본 구분자(',' ':')와 숫자를 제외한 문자를 입력하였습니다.");
+                return;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
         }
+        System.out.println("결과 : " + sum);
     }
 }
