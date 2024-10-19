dcc5e2e820d995de83793b2c7c5813e82fa25653
sik9252 <sik9252@naver.com>
2024-10-19 11:34:30 +0900

    refactor: main 메서드에서는 Calculator.run()만 수행하도록 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,22 +1,7 @@
 package calculator;
 
-import calculator.utils.InputUtils;
-import calculator.utils.PrintUtils;
-import calculator.utils.SplitUtils;
-import calculator.utils.ValidateUtils;
-
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-
-        String userInput = InputUtils.getUserInput();
-        List<String> splitInput = SplitUtils.getSplitInput(userInput);
-
-        ValidateUtils.checkIsNumericAndPositive(splitInput);
-
-        int total = calculator.getTotal(splitInput);
-        PrintUtils.printResult(total);
+        Calculator.run();
     }
 }
