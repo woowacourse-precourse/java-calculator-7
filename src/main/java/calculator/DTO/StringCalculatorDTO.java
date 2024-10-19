34a36c1eef69e454096c2abedfa4b92b032f0173
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-20 03:18:25 +0900

    feat(StringCalculatorDTO): 필드 접근을 위한 Getter 구현
    - input 필드를 통해 입력된 문자열을 저장
    - 생성자를 통해 필드 초기화
    - getInput() 메서드로 문자열에 접근 가능

--- src/main/java/calculator/DTO/StringCalculatorDTO.java
+++ src/main/java/calculator/DTO/StringCalculatorDTO.java
@@ -1,4 +1,13 @@
 package calculator.DTO;
 
 public class StringCalculatorDTO {
+    String input;
+
+    public StringCalculatorDTO(String input) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return input;
+    }
 }
