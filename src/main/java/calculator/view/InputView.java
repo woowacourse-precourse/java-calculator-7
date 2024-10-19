16bbbd85cec3ab0ecec2f6ba22a9637679bdbff5
jaepyo99 <jaepyo99@naver.com>
2024-10-19 15:32:22 +0900

    refactory: 입력시 예상치못한 예외처리를 위한 try~catch 적용

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -4,10 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String startInputValue() {
-        String input = Console.readLine();
-        if (input.isBlank()) {
-            return "0";
+        try {
+            String input = Console.readLine();
+            if (input.isBlank()) {
+                return "0";
+            }
+            return input;
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
         }
-        return input;
     }
 }
