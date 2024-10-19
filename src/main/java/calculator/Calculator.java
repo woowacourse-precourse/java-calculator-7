b37d0313cd7f01686650f68390b71e389fcb47fe
“PororoAndFriends” <gbham0815@pukyong.ac.kr>
2024-10-19 22:16:55 +0900

    <feat>(Calculator) : update feature implementation list 1 - input

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Calculator {
+
+    public int run() {
+        // 입력값 받기
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        System.out.println(input);
+        return 0;
+    }
+
+}
