1ac5b7aa75605bd32bd30812952d4a4f6941b889
hekim52 <acrossnightsky@gmail.com>
2024-10-19 20:07:19 +0900

    feat: 쉼표 또는 콜론 구분자
    쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,64 @@
 package calculator;
 
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String str = readLine();
+
+            ArrayList<Integer> tempList = new ArrayList<>();
+            ArrayList<Integer> wholeList = new ArrayList<>();
+
+            for(int i = 0; i < str.length(); i++) {
+
+                char tempChar = str.charAt(i);
+
+                if(Character.isDigit(tempChar)) {
+                    int tempInt = tempChar - '0';
+                    tempList.add(tempInt);
+                }
+                else {
+                    if(tempChar == ',' || tempChar == ':') {
+                        int tempSize = tempList.size();
+                        int powCount = tempSize - 1;
+                        int tempInt2 = 0;
+                        for(int j = 0; j < tempSize; j++) {
+                            tempInt2 += tempList.get(j) * Math.pow(10, powCount);
+                            powCount--;
+                        }
+                        wholeList.add(tempInt2);
+                        tempList.clear();
+                    }
+                    else {
+                        throw new IllegalArgumentException("잘못된 값이 입력되었습니다.");
+                    }
+                }
+            }
+
+            if (tempList.size() != 0) {
+                int tempSize2 = tempList.size();
+                int powCount2 = tempSize2 - 1;
+                int tempInt2 = 0;
+                for(int j = 0; j < tempSize2; j++) {
+                    tempInt2 += tempList.get(j) * Math.pow(10, powCount2);
+                    powCount2--;
+                }
+                wholeList.add(tempInt2);
+                tempList.clear();
+            }
+
+            int finalSum = 0;
+            for(int i = 0; i < wholeList.size(); i++) {
+                finalSum += wholeList.get(i);
+            }
+            System.out.println("결과 : " + finalSum);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
