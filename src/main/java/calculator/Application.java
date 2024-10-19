7e5916b3eb2ffc30927bb15ad4bd1d57fe012d2f
kimshinwoo <tlsdn67@naver.com>
2024-10-19 18:55:39 +0900

    feat: 사용자의 입출력 받는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // TODO: 프로그램 구현
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(input);
     }
 }
