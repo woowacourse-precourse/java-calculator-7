69c9ee8c52c5abe80b83cfe3a6b2604bc7b7dc5e
Hong, SeokHyeon <tyuiop929@gmail.com>
2024-10-19 18:42:26 +0900

    refactor: 예외 수정

--- src/main/java/calculator/extractor/DelimiterExtractor.java
+++ src/main/java/calculator/extractor/DelimiterExtractor.java
@@ -19,12 +19,19 @@ public class DelimiterExtractor {
         int delimiterEndIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
         validateCustomDelimiter(input, delimiterEndIndex);
         String customDelimiter = input.substring(2, delimiterEndIndex);
+        validateNotDefaultDelimiter(customDelimiter);
         return Pattern.quote(customDelimiter) + "|" + DEFAULT_DELIMITERS;
     }
 
     private void validateCustomDelimiter(String input, int delimiterEndIndex) {
         if (delimiterEndIndex == -1 || delimiterEndIndex == 2) {
             throw new IllegalArgumentException("잘못된 입력 형식: 커스텀 구분자가 누락되었거나 잘못된 형식입니다.");
+        }
+    }
+
+    private void validateNotDefaultDelimiter(String customDelimiter) {
+        if (",".equals(customDelimiter) || ":".equals(customDelimiter)) {
+            throw new IllegalArgumentException("쉼표(,)나 콜론(:)은 기본 구분자입니다.");
         }
     }
 
