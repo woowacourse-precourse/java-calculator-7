b2e7127c25bd0f1f381e5700f1ca5a2548c8e12d
mingking2 <mingee641@gmail.com>
2024-10-19 15:09:04 +0900

    refactor: 배열 검증 추가

--- src/main/java/calculator/util/InputUtil.java
+++ src/main/java/calculator/util/InputUtil.java
@@ -1,8 +1,8 @@
-package calculator;
+package calculator.util;
 
 public class InputUtil {
 
-    public static void checkDigit(String input) {
+    public static void checkInputDigit(String input) {
         int tmp = 0;
         for (int i = 0; i < input.length(); i++) {
             if (Character.isDigit(input.charAt(i))) {
@@ -14,11 +14,21 @@ public class InputUtil {
         }
     }
 
-    public static void checkPositiveNumber(String[] array) {
+    public static void checkArrayNumber(String[] array) {
         for (String s : array) {
+            checkArrayDigit(s);
             if (Integer.parseInt(s) < 0) {
                 throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
             }
         }
     }
+
+    public static void checkArrayDigit(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                throw new IllegalArgumentException("잘못된 입력값입니다.");
+            }
+        }
+    }
+
 }
