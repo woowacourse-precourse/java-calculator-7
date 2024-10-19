f4d5146de8cd029ce5b8175869d727402df5d143
JEN <ekffuclxk@gmail.com>
2024-10-19 19:41:23 +0900

    feat: 커스텀 구분자 추출 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -22,7 +22,18 @@ public class Application {
             }
         }
 
+        //커스텀 구분자 처리           ;\n1;2;3
+        if (input.startsWith("//")) {
+            int idx = input.indexOf("\\n");
+            String customDelimiter = input.substring(2, idx); // 구분자 추출
+            String str = input.substring(idx + 2); // \n 뒷부분 추출
+            String[] numbers = str.split(customDelimiter); // 커스텀 구분자로 분리
+            for (String number : numbers) {
+                System.out.println(number);
+            }
+        }
         System.out.println("Input : " + result);
 
     }
+
 }
