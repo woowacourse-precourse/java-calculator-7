5a19b5a9f144e7447397a6ed0f6270f86b9171ac
Ahnsukjoo <bos98@naver.com>
2024-10-20 03:02:37 +0900

    feat: 빈 문자열 입력시 0으로 반환하는 예외 기능 구현
    - 빈 문자열 입력시 0으로 반환되도록 구현

--- src/main/java/calculator/domain/computer/Adder.java
+++ src/main/java/calculator/domain/computer/Adder.java
@@ -1,10 +1,15 @@
 package calculator.domain.computer;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import calculator.domain.separator.Separator;
+import java.util.NoSuchElementException;
 import java.util.Queue;
 
 public class Adder {
 
+    private String inputValue;
+    private String outputValue;
     private int sum = 0;
     private final Separator separator;
 
@@ -12,8 +17,19 @@ public class Adder {
         this.separator = new Separator();
     }
 
-    public int inPut(String string) {
-        return addAll(separator.separateString(string));
+    public void inPut() {
+        try {
+            inputValue = readLine();
+            if (inputValue.isEmpty()) {
+                System.out.println("결과 : 0");
+            } else {
+                outputValue = addAll(separator.separateString(inputValue)) + "";
+                System.out.println("결과 : " + outputValue);
+            }
+        } catch (NoSuchElementException e) {
+            System.out.println("결과 : 0");
+        }
+
     }
 
     public int addAll(Queue<String> string) {
