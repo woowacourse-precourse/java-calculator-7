9ee40d95abba5ce68a9ed16445ac06e176ea8a25
ilwoo <53050413+Oilwoo@users.noreply.github.com>
2024-10-20 02:57:50 +0900

    feat: 커스텀 구분자를 처리할 수 있는 로직, 올바른 값이 입력되었는지 정규식을 통해 검사

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     public int calculate(String input) {
         // 입력 값이 없을 경우
@@ -7,9 +10,22 @@ public class StringAddCalculator {
             return 0;
         }
 
-        String delimiter = ",|:";
+        String delimiters = ",|:";
+        String numbers = input;
 
-        String[] result = input.split(delimiter);
+        // 커스텀 구분자 처리
+        if (input.startsWith("//")) {
+            //group 메소드 확인하고 공부 하기
+            Pattern customDelimiterPattern = Pattern.compile("^//([^0-9])\\\\n(.*)");
+            Matcher matcher = customDelimiterPattern.matcher(input);
+            if (!matcher.matches()) {
+                throw new IllegalArgumentException("커스텀 구분자를 올바르게 지정해주세요.");
+            }
+            delimiters += "|" + matcher.group(1);
+            numbers = matcher.group(2);
+        }
+
+        String[] result = numbers.split(delimiters);
 
         int sum = 0;
         for (String token : result) {
