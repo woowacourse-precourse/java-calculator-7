3bd21df57d8e1f65fcfeeb83a01fc90123b61895
chaerish <ccyy8432@naver.com>
2024-10-19 17:17:34 +0900

    feat(ApplicationTest): 커스텀/기본 구분자 성공 실패 테스트 구현

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
-    void 커스텀_구분자_사용() {
+    void 커스텀_구분자_성공() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
@@ -17,10 +17,44 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 음수_입력시_오류_예외() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("-1,2,3"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void Suffix_커스텀_구분자_오류_예외() {
+        assertSimpleTest(
+                () -> assertThatThrownBy(() -> runException("//#1#2#3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void Prefix_커스텀_구분자_오류_예외() {
+        assertSimpleTest(
+                () -> assertThatThrownBy(() -> runException("/^\\n1^2^3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_다른거_썼을때_예외() {
+        assertSimpleTest(
+                () -> assertThatThrownBy(() -> runException("//^\\n1?2^3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 기본_구분자_썼을때_성공() {
+        assertSimpleTest(
+                () -> {
+                    run("1,2,3");
+                    assertThat(output()).contains("결과 : 1");
+                }
         );
     }
 
