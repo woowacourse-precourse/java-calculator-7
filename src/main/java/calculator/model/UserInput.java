7b3445e77e0edda4f34e920ace86ff8a681033c6
Etwashoeren <sungmin0609@nate.com>
2024-10-19 20:38:53 +0900

    refactor.메소드 수정

--- src/main/java/calculator/model/UserInput.java
+++ src/main/java/calculator/model/UserInput.java
@@ -3,7 +3,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
 
-    public String input() {
+    public String getInput() {
+
         return Console.readLine();
     }
+
 }
