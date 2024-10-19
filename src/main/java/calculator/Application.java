1daa7ba79296bbd8bff8cfe70b1a3c566c28e3a4
Hyso <hsk7953@gmail.com>
2024-10-19 20:25:56 +0900

    :sparkles: feat : 계산기 애플리케이션 메인 로직 구현 - StringSplitter,AddCalculator, MainCalculator 인스턴스 생성 - MainCalculator의 run 메서드 호출하여 프로그램 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import calculator.addCalculator.AddCalculator;
+import calculator.main.MainCalculator;
+import calculator.stringSplitter.StringSplitter;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        StringSplitter splitter = new StringSplitter();
+        AddCalculator addCalculator = new AddCalculator();
+        MainCalculator mainCalculator = new MainCalculator(addCalculator,splitter);
+        mainCalculator.run();
     }
 }
