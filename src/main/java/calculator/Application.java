67caa0547fc47e2f3575e12cd8cac38f72cb13b2
yumi <yumkoori@gmail.com>
2024-10-19 22:19:26 +0900

    refactor: 계산기를 이용하는 사용자의 입력을 받고 결과를 보여주는 Application 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Calculator calculator = new CalculatorImpl();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = Console.readLine();
+
+        int result = calculator.plus(str);
+
+        System.out.println("결과 : " + result);
     }
 }
