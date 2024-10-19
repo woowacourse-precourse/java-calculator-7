3cac9c992df38f32f6ed9f6fb3a1c562adc756ee
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-19 15:42:23 +0900

    Feat: 모든 열거형 상수를 순회하며 컨테이너에 등록할 수 있도록 컨테이너 클래스 구현

---
+++ src/main/java/calculator/service/container/SeparationServiceContainer.java
@@ -0,0 +1,24 @@
+package calculator.service.container;
+
+import calculator.service.separation.SeparationService;
+import java.util.EnumMap;
+import java.util.stream.Stream;
+
+public class SeparationServiceContainer {
+
+    private final EnumMap<SeparationServiceType, SeparationService> separationServices
+            = new EnumMap<>(SeparationServiceType.class);
+
+    public SeparationServiceContainer() {
+        Stream.of(SeparationServiceType.values())
+                .forEach(type -> separationServices.put(type, type.construct()));
+    }
+
+    public SeparationService getSeparationService(SeparationServiceType type) {
+        return separationServices.get(type);
+    }
+
+    public EnumMap<SeparationServiceType, SeparationService> getSeparationServices() {
+        return separationServices;
+    }
+}
