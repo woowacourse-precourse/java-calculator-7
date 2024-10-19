3e1bd4f1f763128b5dbf28f3188192035475efb8
jeonghyeon-ohk <ok6737@naver.com>
2024-10-19 17:06:26 +0900

    feat(domain): 정수 배열의 합을 반환하는 로직 구현

--- src/main/java/calculator/domain/Result.java
+++ src/main/java/calculator/domain/Result.java
@@ -1,4 +1,9 @@
 package calculator.domain;
 
+import java.util.Arrays;
+
 public class Result{
+    public static int getResult(int[] input){
+        return Arrays.stream(input).reduce(0, Integer::sum);
+    }
 }
