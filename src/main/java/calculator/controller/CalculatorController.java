3258551972bc5fdd18505ac4191816ad628ff23d
etd93 <etd93@naver.com>
2024-10-19 22:23:59 +0900

    [feat] 사용자 입력 받는 메서드 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,7 +1,12 @@
 package calculator.controller;
 
-public class CalculatorController {
-    public void startCalculating(){
+import calculator.util.OutputUtil;
+import camp.nextstep.edu.missionutils.Console;
 
+public class CalculatorController {
+    private final OutputUtil outputUtil = new OutputUtil();
+    public void startCalculating(){
+        outputUtil.printProgramStart();
+        String inputString = Console.readLine();
     }
 }
---
+++ src/main/java/calculator/util/OutputUtil.java
@@ -0,0 +1,16 @@
+package calculator.util;
+
+public class OutputUtil {
+    public void printProgramStart(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public void printResultPhrase(){
+        System.out.print("결과 : ");
+    }
+
+    public void printResult(int result){
+        System.out.print(result);
+    }
+
+}
