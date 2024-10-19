94cbe19aafcdad8269dce5e9205ac3f42c7330bc
jklee3409 <jklee3409@khu.ac.kr>
2024-10-19 11:41:55 +0900

    feat(delimiter) : hasCustomDelimiter 주석 수정

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -2,7 +2,7 @@ package calculator.domain;
 
 public class Delimiter {
 
-    // 문자열에서 커스텀 구분자를 지정했는지 확인하고 인덱스 반환
+    // 문자열에서 커스텀 구분자를 지정했는지 확인
     private static boolean hasCustomDelimiter(String input) {
         return input.startsWith("//") && input.contains("\\n");
     }
