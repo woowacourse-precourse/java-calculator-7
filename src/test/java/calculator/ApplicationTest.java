2fc6c3846d906c5389d40f4bcd5975b20d015457
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-10-20 01:44:30 +0900

    test(ApplicationTest): 테스트 코드 작성
    여러 자리 숫자를 사용했을 때를 테스트합니다.
    구분자를 잘못 입력했을 때를 테스트합니다.
    커스텀 구분자로 공백을 사용했을 때를 테스트합니다.
    아무런 입력이 없을 경우를 테스트합니다.
    구분자 없이 숫자만 입력된 경우를 테스트합니다.
    숫자 없이 구분자만 입력된 경우를 테스트합니다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,8 +19,85 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 여러_자리수_사용() {
+        assertSimpleTest(() -> {
+            run("10,100:1000");
+            assertThat(output()).contains("결과 : 1110");
+        });
+    }
+
+    @Test
+    void 기본_구분자_테스트_1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",1:2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+
+    @Test
+    void 기본_구분자_테스트_2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1:2:"))
+                        .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+
+    @Test
+    void 다른_구분자_사용_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("3.4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+
+    @Test
+    void 공백_커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("// \\n1 3");
+            assertThat(output()).contains("결과 : 4");
+        });
+    }
+
+    @Test
+    void 다른_커스텀_구분자_사용() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\\\n1,2;3?4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+
+        );
+    }
+
+    @Test
+    void 아무_입력_없음() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 구분자_입력_없음() {
+        assertSimpleTest(() -> {
+            run("123");
+            assertThat(output()).contains("결과 : 123");
+        });
+    }
+
+    @Test
+    void 숫자_입력_없음() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(":"))
+                        .isInstanceOf(IllegalArgumentException.class)
+
         );
     }
 
