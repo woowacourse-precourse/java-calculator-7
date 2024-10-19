d9c98a078ac6d2ffbf9a2a7d6b04e8a34ae6dcd3
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-19 15:59:36 +0900

    Fix: 입력값이 없을 경우 예외가 발생하는 것이 아니라 0 을 반환하도록 디폴트 메서드 수정

--- src/main/java/calculator/service/separation/SeparationService.java
+++ src/main/java/calculator/service/separation/SeparationService.java
@@ -4,11 +4,16 @@ import calculator.domain.Numbers;
 
 public interface SeparationService {
 
+    String[] EMPTY_ARRAY = {"0"};
+
     boolean hasCustomSeparator(String input);
 
     Numbers getNumbers(String input, String... separators);
 
     default String[] split(String input, String separatorOrRegex) {
+        if (input.isBlank()) {
+            return EMPTY_ARRAY;
+        }
         return input.split(separatorOrRegex);
     }
 }
