6d88c85166d6efc8c28c74328790b5e88ea8f2f6
sinyebin <dpqls0625@naver.com>
2024-10-20 01:38:27 +0900

    Revert "docs: 요구사항 정리"
    This reverts commit 7265c5c6a10ec6bd5154924f95394068dfca06b7.

--- src/main/java/calculator/ResultView.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-
-public class ResultView {
-    private final static String PRINT_INPUT_STRING = "덧셈할 문자열을 입력해 주세요.";
-
-    public static void printInputString(){
-        System.out.println(PRINT_INPUT_STRING);
-    }
-}
