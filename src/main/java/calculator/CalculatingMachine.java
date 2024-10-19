9f8c12907b3a826227057baebe0e66813f2bb6c4
chaerish <ccyy8432@naver.com>
2024-10-19 17:22:37 +0900

    refactor(CalculatingMachine): 계산기 머신의 지역변수 제거

--- src/main/java/calculator/CalculatingMachine.java
+++ src/main/java/calculator/CalculatingMachine.java
@@ -3,19 +3,9 @@ package calculator;
 import java.util.List;
 
 public class CalculatingMachine {
-    private List<Long> nums;
-
-    CalculatingMachine(List<Long> nums) {
-        this.nums = nums;
-    }
-
-    /*
-  계산하는 거 정도는 가능.
-  모델에 어느정도까지 역할 분배를 시킬까?
-   */
-    public Long calculate() {
+    public Long calculate(List<Long> nums) {
         Long result = 0L;
-        for (Long num : this.nums) {
+        for (Long num : nums) {
             result += num;
         }
         return result;
