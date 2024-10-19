a2e065d9c43c347d2f872c6ffeae7a5c537c5895
sc@t0a14 <gusdn6671@gmail.com>
2024-10-19 23:14:43 +0900

    feat(CustomDelimiter): 커스텀 구분자에 기본 구분자를 입력했을 경우 예외처리

--- src/main/java/calculator/CustomDelimiter.java
+++ src/main/java/calculator/CustomDelimiter.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class CustomDelimiter {
     public static final String ONLY_DIGITS = "\\d+";
-    
+
     private final String customDelimiter;
 
     public CustomDelimiter(String customDelimiter) {
@@ -19,10 +19,13 @@ public class CustomDelimiter {
             throw new IllegalArgumentException("커스텀 구분자는 숫자를 사용할 수 없습니다.");
         }
 
+        if (customDelimiter.equals(",") || customDelimiter.equals(":")) {
+            throw new IllegalArgumentException("커스텀 구분자에 기본 구분자를 입력할수 없습니다");
+        }
+
     }
 
     public String getCustomDelimiter() {
         return customDelimiter;
     }
-
 }
