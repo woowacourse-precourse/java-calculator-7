4bc865c197d778d4e17dbe40d5262848b35c4e6d
wgrgwg <kyoeng14697@naver.com>
2024-10-19 23:02:00 +0900

    fix(controller): fix readUserInput
    Fix readUserInput so that it trims input string from the user.

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -22,7 +22,7 @@ public final class CalculatorController {
     public String readUserInput() {
         String userInput;
 
-        userInput = calculatorView.getUserInput();
+        userInput = calculatorView.getUserInput().trim();
 
         return userInput;
     }
