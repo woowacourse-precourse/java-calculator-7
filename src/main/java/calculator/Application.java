040605924354a5a2a58c9b290e1cc769ed852a80
JJY0 <92962527+JJY0@users.noreply.github.com>
2024-10-19 17:19:04 +0900

    feat(Application): 사용자 입력 처리 및 음수 예외 처리 구현
    - 사용자로부터 문자열을 입력받는 로직 구현
    - 음수 입력 시 IllegaLArgumentException을 발생하여 처리하는 로직 추가
    - 예외 발생 시 프로그램을 종료하도록 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.\n");
+        String numInput = Console.readLine();
+
+        try {
+            int num = Integer.parseInt(numInput);
+            if(num < 0) {
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
     }
 }
