e1ad34ea8a701c57a2ada7552fefc91f6da6e90f
suyeong jang <jangsuyeong923@naver.com>
2024-10-20 05:00:23 +0900

    feat(model): 구분자에 따른 숫자 추출 로직 구현

--- src/main/java/calculator/model/CalculatorService.java
+++ src/main/java/calculator/model/CalculatorService.java
@@ -1,12 +1,27 @@
 package calculator.model;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class CalculatorService {
+
+    private static final String CUSTOM_DELIMITER_PREFIX = "\\n"; // \n를 그대로 사용하기 위함.
 
     private final ExtractLogic extractLogic = new ExtractLogic();
 
     private String getDelimiter(Scanner scanner) {
         return extractLogic.extractDelimiter(scanner.nextLine());
+    }
+
+    private List<Integer> getNumbers(String input, String delimiter, Scanner scanner) {
+        String numbersInput;
+        if (scanner.hasNextLine()) {
+            numbersInput = scanner.nextLine();
+        } else if (input.contains(CUSTOM_DELIMITER_PREFIX)) {
+            numbersInput = input.substring(input.indexOf(CUSTOM_DELIMITER_PREFIX) + 2);
+        } else {
+            numbersInput = input;
+        }
+        return extractLogic.extractNumber(numbersInput, delimiter);
     }
 }
