a0ab4ef578a996911dc65fd9c25673ee81a2eb62
Lkk7380 <dlrlrhkd@naver.com>
2024-10-20 00:35:37 +0900

    feat: 쉼표로 구분된 숫자 문자열의 덧셈 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,14 @@ public class Application {
         if (text == null || text.isBlank()) {
             return 0;
         }
+        if (text.contains(",")) {
+            String[] numbers = text.split(",");
+            int sum = 0;
+            for (String number : numbers) {
+                sum += Integer.parseInt(number);
+            }
+            return sum;
+        }
         return Integer.parseInt(text);
     }
 }
