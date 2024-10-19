44efa7b584907f8df1bc779b3ee454e46e938418
seophohoho <seop0937@gmail.com>
2024-10-20 04:24:57 +0900

    refactor(Separator): 로컬 final 값을 글로벌 final 값으로 변경

--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -2,8 +2,6 @@ package calculator;
 
 public class Separator {
     private String SEPARATOR = ",|:";
-    private final String CUSTOM_SEPARATOR_PREFIX = "//";
-    private final String CUSTOM_SEPARATOR_SUFFIX = "\\n";
 
     public String[] separate(String input) {
         return separate(input, SEPARATOR);
@@ -12,9 +10,9 @@ public class Separator {
     public String[] separate(String input, String separator) {
         String regex = separator;
 
-        if (input.startsWith(CUSTOM_SEPARATOR_PREFIX) && input.contains(CUSTOM_SEPARATOR_SUFFIX)) {
-            int prefixIdx = input.indexOf(CUSTOM_SEPARATOR_PREFIX);
-            int suffixIdx = input.lastIndexOf(CUSTOM_SEPARATOR_SUFFIX);
+        if (input.startsWith(Constants.CUSTOM_SEPARATOR_PREFIX) && input.contains(Constants.CUSTOM_SEPARATOR_SUFFIX)) {
+            int prefixIdx = input.indexOf(Constants.CUSTOM_SEPARATOR_PREFIX);
+            int suffixIdx = input.lastIndexOf(Constants.CUSTOM_SEPARATOR_SUFFIX);
 
             String customSeparator = String.valueOf(input.charAt(suffixIdx - 1));
             StringBuilder sb = new StringBuilder(input);
