1d875d14f7b2204a9f7d09dd9b1ad85972b492a8
lvalentine6 <ysr1227@naver.com>
2024-10-19 15:41:26 +0900

    refactor: apply static factory method

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -1,14 +1,27 @@
 package calculator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CalculatorController {
+	private final Calculator calculator;
+	private final InputHandler inputHandler;
+	private final OutputHandler outputHandler;
+
+	private CalculatorController(Calculator calculator, InputHandler inputHandler, OutputHandler outputHandler) {
+		this.calculator = calculator;
+		this.inputHandler = inputHandler;
+		this.outputHandler = outputHandler;
+	}
+
+	public static CalculatorController of(Calculator calculator, InputHandler inputHandler,
+		OutputHandler outputHandler) {
+		return new CalculatorController(calculator, inputHandler, outputHandler);
+	}
+
 	public void run() {
-		Calculator calculator = new Calculator();
-		List<Integer> numbers = new ArrayList<>();
-		ValidatingParser validatingParser = new ValidatingParser();
-		InputHandler inputHandler = InputHandler.of(validatingParser);
-		inputHandler.getValidatedUserInput();
+		outputHandler.showPromptMessage();
+		List<Integer> numbers = inputHandler.getValidatedNumbersFromUserInput();
+		int sum = calculator.sumNumbers(numbers);
+		outputHandler.showCalculateResult(sum);
 	}
 }
