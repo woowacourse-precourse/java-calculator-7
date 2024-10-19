1cca49b96396120537dc1c99af9a63d7bd4395f4
lol-chang <doclecsj@naver.com>
2024-10-20 04:02:05 +0900

    feat($Apllication): 인스턴스 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,10 +2,16 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
+        InputHandler inputHandler = new InputHandler();
+        CalculatorController controller = new CalculatorController();
+        OutputHandler outputHandler = new OutputHandler();
 
+        String input = inputHandler.getInput();
 
+        // TODO : CalculatorController 함수 호출
+        int result=0;
 
+        outputHandler.printResult(result);
     }
 }
