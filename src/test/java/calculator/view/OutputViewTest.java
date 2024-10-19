8da5a046074fdc6928cfebd606d1cea021c4d112
Zrfv <youngoh07015@gmail.com>
2024-10-19 18:31:03 +0900

    test(OutputViewTest): 예외 상황에 맞는 출력 메세지 검증

---
+++ src/test/java/calculator/view/OutputViewTest.java
@@ -0,0 +1,57 @@
+package calculator.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import calculator.Application;
+import calculator.model.Delimiter;
+import calculator.model.Number;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class OutputViewTest extends NsTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private OutputView outputView;
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        outputView = new OutputView();
+    }
+
+    @Test
+    void 구분자_예외_메시지_출력_검증() {
+        String invalidInput = "/?\\n1,2";
+
+        try {
+            Delimiter delimiter = new Delimiter(invalidInput);
+            delimiter.checkCustomDelimiter();
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            assertThat(outputStreamCaptor.toString().trim())
+                    .isEqualTo("잘못된 입력입니다: 올바른 형식의 커스텀 구분자가 아닙니다.");
+        }
+    }
+
+    @Test
+    void 숫자_예외_메시지_출력_검증() {
+        String invalidInput[] = {"1", "-5", "3"};
+
+        try {
+            Number number = new Number(invalidInput);
+            number.stringToInt();
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            assertThat(outputStreamCaptor.toString().trim())
+                    .isEqualTo("잘못된 입력입니다: 양수만 입력 가능합니다.");
+        }
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
