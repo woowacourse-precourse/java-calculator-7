ac01f8fbe54d62a3f4a27c5d7d1b704f212bd3a6
dgh06175 <dgh06175@gmail.com>
2024-10-19 21:27:07 +0900

    feat: 기본 구조 작성

---
+++ src/main/java/calculator/CalculatorController.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class CalculatorController {
+    private final CalculatorView calculatorView;
+    private final CalculatorModel calculatorModel;
+
+    public CalculatorController(CalculatorView calculatorView, CalculatorModel calculatorModel) {
+        this.calculatorView = calculatorView;
+        this.calculatorModel = calculatorModel;
+    }
+
+    public void run() {
+        // 입력 받기
+
+        // 문자열 구분하기
+
+        // 문자열 계산하기
+
+        // 결과 출력하기
+    }
+}
---
+++ src/main/java/calculator/CalculatorModel.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import java.util.List;
+
+public class CalculatorModel {
+    private List<String> dividers = List.of(",", ":");
+
+    public int calculateSum(String input) {
+        return 0;
+    }
+}
