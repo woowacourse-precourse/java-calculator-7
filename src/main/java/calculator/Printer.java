062046d80c3d3f053177771794db6cd9e2e9bf69
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-20 04:03:05 +0900

    refactor: 출력부 class로 분리

--- src/main/java/calculator/Printer.java
+++ src/main/java/calculator/Printer.java
@@ -1,2 +1,9 @@
-package calculator;public class Printer {
+package calculator;
+
+public class Printer {
+    private static final String OUTPUT_MESSAGE = "결과 : ";
+
+    public static void print(int number) {
+        System.out.println(OUTPUT_MESSAGE + number);
+    }
 }
