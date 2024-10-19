a8af53343b9f5c46a093e9b7d4be4256a0ab8805
neoskyclad <neoskyclad@naver.com>
2024-10-20 02:55:55 +0900

    feat(Application): add custom seperator in input string
    Implemented functionality to allow the use of a custom seperator
    for splitting the user input string.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,17 +2,31 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input;
-        char[] seperatorArray = {',', ':'};
+        List<Character> seperatorArray = new ArrayList<>(Arrays.asList(',', ':'));
+        char customSeperator;
         int[] numberArray;
         int result = 0;
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
         input = Console.readLine();    // 사용자로부터 문자열 입력 받기
+
+        customSeperator = getCustomSeperator(input);
+
+        if(customSeperator != ' ')
+        {
+            seperatorArray.add(customSeperator);
+            input = input.substring(5);
+        }
+
         numberArray = convertStringToIntArray(input, seperatorArray);
 
         for (int i : numberArray) {
@@ -25,7 +39,24 @@ public class Application {
         System.out.println(result);
     }
 
-    public static int[] convertStringToIntArray(String input, char[] seperator) {
+    public static char getCustomSeperator(String input) {
+        char temp = ' ';
+
+        if(input.length() < 5)
+            return temp;
+
+        if(input.charAt(0) == '/' && input.charAt(1) == '/')
+        {
+            if(input.charAt(3) == '\\' && input.charAt(4) == 'n')
+            {
+                temp = input.charAt(2);
+            }
+        }
+
+        return temp;
+    }
+
+    public static int[] convertStringToIntArray(String input, List<Character> seperator) {
         int[] temp = new int[input.length()];
 
         int lastIndex = 0;
@@ -35,6 +66,9 @@ public class Application {
             temp[i] = -1;
 
             for (char c : seperator) {
+                if (c == ' ')
+                    continue;
+
                 if (input.charAt(i) == c) {
                     temp[inputIndex++] = Integer.parseInt(input.substring(lastIndex, i));
                     lastIndex = i + 1;
