40d6d5d2aac44cb7892980f9a7bdde75df5a8e67
leeedohyun <ldh19738@gmail.com>
2024-10-19 15:01:31 +0900

    feat: 커스텀 구분자 예외 처리

--- src/main/java/calculator/Delimiter.java
+++ src/main/java/calculator/Delimiter.java
@@ -32,8 +32,13 @@ public class Delimiter {
 
     private static String getCustomDelimiter(String input) {
         if (!input.startsWith("//") || !input.contains("\n")) {
+            throw new IllegalArgumentException("[ERROR] 커스텀 구분자가 잘못된 형식입니다. 형식은 '//'와 구분자, 그리고 '\n'으로 시작해야 합니다.");
+        }
+
+        String customDelimiter = input.substring(2, input.indexOf("\n"));
+        if (customDelimiter.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 커스텀 구분자가 존재하지 않습니다.");
         }
-        return input.substring(2, input.indexOf("\n"));
+        return customDelimiter;
     }
 }
--- src/test/java/calculator/DelimiterTest.java
+++ src/test/java/calculator/DelimiterTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,20 @@ class DelimiterTest {
         
         // then
         assertThat(delimiter).isEqualTo(new Delimiter(";"));
+    }
+
+    @Test
+    void 커스텀_구분자를_잘못_입력한_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> Delimiter.createCustomDelimiter("/\n1;2;3"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 커스텀 구분자가 잘못된 형식입니다. 형식은 '//'와 구분자, 그리고 '\n'으로 시작해야 합니다.");
+    }
+
+    @Test
+    void 커스텀_구분자가_없는_경우_예외가_발생한다() {
+
+        assertThatThrownBy(() -> Delimiter.createCustomDelimiter("//\n1;2;3"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 커스텀 구분자가 존재하지 않습니다.");
     }
 }
