c2209b199534d4370c144ad0b26e93f1ba3da378
HwangSgHn <dolphin33@jbnu.ac.kr>
2024-10-19 18:01:07 +0900

    feat: 구분자를 기준으로 숫자 분리 구현
    문자열이 숫자와 구분자로 구성된 것을 확인 후, 구분자로 숫자를 분리하여  저장하는 함수 구현.

--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -1,14 +1,17 @@
 package calculator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Separator {
     private String string;
+    private final List<Integer> numbers;
     private final List<Character> separators;
 
     public Separator(String string) {
         this.string = string;
+        numbers = new ArrayList<>();
         separators = new ArrayList<>();
         separators.add(',');
         separators.add(':');
@@ -44,5 +47,28 @@ public class Separator {
         }
         regex.append("][0-9]+)*)?$");
         return regex.toString();
+    }
+
+    public void separate() {
+        while (!string.matches("([0-9]+)?")) {
+            int index = getFirstSeparatorIndex();
+            numbers.add(Integer.parseInt(string.substring(0, index)));
+            string = string.substring(index + 1);
+        }
+        if (!string.isEmpty()) {
+            numbers.add(Integer.parseInt(string));
+        }
+    }
+
+    private int getFirstSeparatorIndex() {
+        List<Integer> index = new ArrayList<>();
+        for (Character separator : separators) {
+            int idx = string.indexOf(separator);
+            if (idx != -1) {
+                index.add(idx);
+            }
+        }
+        Collections.sort(index);
+        return index.getFirst();
     }
 }
