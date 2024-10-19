b93075d4abe0b0b3d40e6466dcba4fd6550b0f98
TalkingPotato90 <star901210@hanmail.net>
2024-10-20 03:01:23 +0900

    feat: 주요 기능 구현 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,7 @@ import calculator.controller.StringCalculatorController;
 import calculator.model.Arithmetic;
 import calculator.model.Delimiter;
 import calculator.model.InputData;
+import calculator.model.SumCalculator;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,7 +13,9 @@ public class Application {
         StringCalculatorController stringCalculatorController = new StringCalculatorController();
         DelimiterController delimiterController = new DelimiterController();
         InputData inputData = stringCalculatorController.handoverInput();
-        Delimiter delimiter =  delimiterController.createDelimiterPart(inputData);
+        Delimiter delimiter = delimiterController.createDelimiterPart(inputData);
         Arithmetic arithmetic = new Arithmetic(delimiter, inputData);
+        SumCalculator sumCalculator = new SumCalculator();
+        System.out.println("결과 : " + sumCalculator.calculate(arithmetic));
     }
 }
