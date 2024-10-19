140b0a62bd76872aaed70fd7f605248e4df8b6b9
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-10-20 02:44:37 +0900

    refactor: 숫자 문자열 영역에서 빈 문자열이 들어오면 0만 들어있는 list를 반환하는 기능 추가

--- src/main/java/calculator/domain/NumberExtractor.java
+++ src/main/java/calculator/domain/NumberExtractor.java
@@ -1,5 +1,6 @@
 package calculator.domain;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,8 +9,13 @@ import calculator.util.NumberChecker;
 public class NumberExtractor {
 
 	private static final String OR_SIGN = "|";
+	private static final long ZERO = 0L;
 
 	public List<Long> getNumbers(String numberSection, List<String> delimiters) {
+
+		if(numberSection.isBlank()) {
+			return new ArrayList<>(List.of(ZERO));
+		}
 
 		String delimiter = String.join(OR_SIGN, delimiters);
 		String[] split = numberSection.split(delimiter);
