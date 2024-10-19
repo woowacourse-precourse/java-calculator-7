778f1e839ab5bca57ef77252bc94a1a7cb39c006
lee-haeseung <lhser123@cau.ac.kr>
2024-10-19 22:46:40 +0900

    test: test case를 method별로 분리
    하나의 test 안에 여러 경우에 대한 test를 진행할 경우,
    output 내부의 내용이 초기화 되지 않아 각 test를 method별로 분리

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -30,24 +30,34 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 기본_구분자_테스트() {
+    void 숫자_한_개_테스트() {
         // 기본
         assertSimpleTest(() -> {
             run("1");
             assertThat(output()).contains("결과 : 1");
         });
+    }
 
+    @Test
+    void 빈_문자열_테스트() {
         // 빈문자열
         assertSimpleTest(() -> {
             run("\n");
             assertThat(output()).contains("결과 : 0");
         });
+    }
 
+    @Test
+    void 숫자_여러_개_테스트1() {
         // 정상값 입력
         assertSimpleTest(() -> {
             run("1:3,2");
             assertThat(output()).contains("결과 : 6");
         });
+    }
+
+    @Test
+    void 숫자_여러_개_테스트2() {
         assertSimpleTest(() -> {
             run("1:3,2,40,800,29");
             assertThat(output()).contains("결과 : 875");
