96ad5dc3d47bdd2c7afb00f6cb1e0b562fa6fce5
chyun <och2510@gmail.com>
2024-10-19 13:55:21 +0900

    feat(OutputView): 덧셈한 결과를 출력한다

---
+++ src/main/java/calculator/constant/SystemMessage.java
@@ -0,0 +1,7 @@
+package calculator.constant;
+
+public class SystemMessage {
+	public static final String ENTER = "%n";
+	public static final String INPUT_EQUATION = "덧셈할 문자열을 입력해 주세요.";
+	public static final String RESULT_FORMAT = "결과 : %d";
+}
