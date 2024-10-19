f999d68dc08bf7944406c38bf49946a307aac7dd
tentenball <lth8905@gmail.com>
2024-10-20 00:46:02 +0900

    refactor: 상수 final 적용

--- src/main/java/calculator/constant/DelimiterConstant.java
+++ src/main/java/calculator/constant/DelimiterConstant.java
@@ -1,11 +1,11 @@
 package calculator.constant;
 
 public class DelimiterConstant {
-	public static String COMMA = ",";
-	public static String COLON = ":";
-	public static int NO_CUSTOM_DELIMITER_LENGTH = 2;
-	public static String NO_CUSTOM_DELIMITER_SPLIT_REGEX = "[,:]";
-	public static String REGEX_START_SIGN = "[";
-	public static String REGEX_END_SIGN = "]";
+	public static final String COMMA = ",";
+	public static final String COLON = ":";
+	public static final int NO_CUSTOM_DELIMITER_LENGTH = 2;
+	public static final String NO_CUSTOM_DELIMITER_SPLIT_REGEX = "[,:]";
+	public static final String REGEX_START_SIGN = "[";
+	public static final String REGEX_END_SIGN = "]";
 
 }
--- src/main/java/calculator/constant/DrawConstant.java
+++ src/main/java/calculator/constant/DrawConstant.java
@@ -1,9 +1,9 @@
 package calculator.constant;
 
 public class DrawConstant {
-	public static String CUSTOM_DELIMITER_START_SIGN = "//";
-	public static int CUSTOM_DELIMITER_FORMULA_NUMBER_START_INDEX = 5;
-	public static int CUSTOM_DELIMITER_INDEX = 2;
-	public static String CUSTOM_DELIMITER_FORMULA_REGEX = "//(.)\\\\n(\\d+)(.*?)";
-	public static String INEVITABLE_CONTAINS_HYPHEN_FORMAT = "--";
+	public static final String CUSTOM_DELIMITER_START_SIGN = "//";
+	public static final int CUSTOM_DELIMITER_FORMULA_NUMBER_START_INDEX = 5;
+	public static final int CUSTOM_DELIMITER_INDEX = 2;
+	public static final String CUSTOM_DELIMITER_FORMULA_REGEX = "//(.)\\\\n(\\d+)(.*?)";
+	public static final String INEVITABLE_CONTAINS_HYPHEN_FORMAT = "--";
 }
