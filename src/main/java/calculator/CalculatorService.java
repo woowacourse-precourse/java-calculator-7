fc14346cbb65082b35662c1f26d222fdb874a52e
lchyeong <lchyeong@gmail.com>
2024-10-19 22:02:50 +0900

    refactor(CalculatorService): 서비스 메서드를 calculate로 통합
    - 서비스 테스트 클래스도 맞춰 수정

--- src/main/java/calculator/CalculatorService.java
+++ src/main/java/calculator/CalculatorService.java
@@ -2,18 +2,15 @@ package calculator;
 
 public class CalculatorService {
 
-    NumberParser numberParser = new NumberParser();
-    PatternParser patternParser = new PatternParser();
+    final NumberParser numberParser = new NumberParser();
+    final PatternParser patternParser = new PatternParser();
 
-    public String[] splitInput(String input) {
-        return patternParser.splitPattern(input);
-    }
 
-    public String[] replaceNumber(String[] splitInput) {
-        return numberParser.replaceNumber(splitInput);
-    }
+    public int calculate(String input) {
+        String[] splitInput = patternParser.splitPattern(input);
+        String[] splitInputReplaceNumber = numberParser.replaceNumber(splitInput);
+        int result = numberParser.sumNumber(splitInputReplaceNumber);
 
-    public int sumNumber(String[] splitNumberInput) {
-        return numberParser.sumNumber(splitNumberInput);
+        return result;
     }
 }
