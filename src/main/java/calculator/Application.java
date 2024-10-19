1e42a3fda59484cdae5d385b8d519028de1ebc8f
yaongmeow <minnie_00@naver.com>
2024-10-19 17:47:15 +0900

    feat(logic): 커스텀 구분자 사용 여부 확인
    - '//' 시작 여부를 통해 판단

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,6 +8,13 @@ public class Application {
         System.out.print("덧셈할 문자열을 입력해 주세요.\n");
         String input = scanner.nextLine();
         Integer sum = 0;
+
+        if (input.startsWith("//")) {
+            // 커스텀 문자열 사용
+        }
+        else {
+            // 커스텀 문자열 미사용
+        }
         System.out.println("결과 : " + sum);
     }
 }
