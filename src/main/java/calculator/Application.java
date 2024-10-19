06f5badfd9df206d0e0b5c1b7e0de3412712ae0d
yngbao97 <yukyj0418@gmail.com>
2024-10-19 11:13:21 +0900

    feat: 커스텀 구분자 여부 확인

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,11 +2,36 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
+
+    static Character customDelemiter;
     public static void main(String[] args) {
 
         String input = Console.readLine();
+        String prefix = input.split("\n")[0];
+
+        if (prefix != null && !prefix.isEmpty()) {
+            customDelemiter = getCustomDelimiter(prefix);
+        }
+
 
         Console.close();
     }
+
+    private static Character getCustomDelimiter(String input) {
+
+        Pattern pattern = Pattern.compile("//(.*?)");
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            String delimiter = matcher.group(1);    // //와 \n 사이의 내용 추출
+            if (delimiter.length() > 1) return null;    // 예외처리 필요
+            return delimiter.charAt(0);
+        }
+        return null;
+    }
+
 }
