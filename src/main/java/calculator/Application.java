7d7ca161b668a62deb962ca201aa27839dde26f1
hjg1473 <hjg1473@naver.com>
2024-10-19 23:02:12 +0900

    feat: add separator decision function
    Add a function that takes a string as input and returns a separator if there is one within that string.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,7 +7,27 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        System.out.println(input);
+        System.out.println(DecideSeparatorInString(input));
         Console.close();
+    }
+
+    private static String DecideSeparatorInString(String string) {
+        String value = "";
+        int indexDoubleSlash = string.indexOf("//");
+        int indexNewline = string.indexOf("\\n");
+        int isComma = string.indexOf(",");
+        int isColon = string.indexOf(":");
+        System.out.println("DoubleSlash"+indexDoubleSlash);
+        System.out.println("NewLine"+indexNewline);
+        if(indexDoubleSlash != -1 && indexNewline != -1) {
+            value += string.substring(indexDoubleSlash+2, indexNewline);
+        }
+        if(isComma != -1) {
+            value += ",";
+        }
+        if(isColon != -1) {
+            value += ":";
+        }
+        return value;
     }
 }
