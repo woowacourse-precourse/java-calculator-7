76a9518b64ab63c5ed06f538253bcf874a7345da
yu-k-m <karen0806@naver.com>
2024-10-19 21:14:20 +0900

    feat: create ArrayList to receive input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String value = readLine();
+        List<String> valuesToCalculate = new ArrayList<>();
+        valuesToCalculate.add(value);
     }
 }
