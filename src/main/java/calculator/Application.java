514db7c244c8b61510a187316ab31a9f823ce3c5
wowseok <wowseok2774@gmail.com>
2024-10-19 23:45:29 +0900

    refactor: 구체적인 객체 생성을 CalculatorService 클래스에 위임. Application은 진입점만 담당하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,30 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        OutputHandler outputHandler = new OutputHandler();
-        outputHandler.guideComment();
-
-        InputHandler inputHandler = new InputHandler();
-        String userInput = inputHandler.getUserInput();
-
-        ExceptionHandler exceptionHandler = new ExceptionHandler(userInput);
-        // 빈 문자열이면 0을 출력 후 종료
-        if (!exceptionHandler.validateInput()) {
-            outputHandler.printSum(0);
-            return;
-        }
-        Calculator cal = new Calculator(userInput);
-
-        //exceptionHandler.validateCommonInput();
-        String delimiter = cal.extractDelimiter();
-
-        String[] numbers = cal.extractNumbers(delimiter);
-
-        int sum = cal.sumStringArray(numbers);
-
-        outputHandler.printExtractedValues(delimiter, numbers);
-
-        outputHandler.printSum(sum);
-
+        CalculatorService calculatorService = new CalculatorService();
+        calculatorService.run();  // 프로그램 실행
     }
 }
