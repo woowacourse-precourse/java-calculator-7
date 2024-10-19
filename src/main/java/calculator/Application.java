bb30fa0220c1ac5142df9fd28a3336ec13c04741
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-19 22:53:31 +0900

    feat(Application): 프로그램 진입점 구성
    진입점인 main 함수에서 Controller 객체 생성.
    - 컨트롤러에서 프로그램의 제어권을 넘김.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
+import calculator.Controller.StringProcessorController;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        StringProcessorController stringProcessorController = new StringProcessorController();
     }
 }
