6f881a7ae095b7c32485717c98b45a334fca7dca
JiEung2 <181757@jnu.ac.kr>
2024-10-20 00:08:17 +0900

    feat(inputValidator): 입력값이 패턴에 맞지 않을 경우의 유효성 검사 추가
    - 기본 구분자만을 사용하는 패턴
    - 커스텀 구분자 등록을 함께 사용하는 패턴

--- src/main/java/calculator/InputValidator.java
+++ src/main/java/calculator/InputValidator.java
@@ -1,11 +1,19 @@
 package calculator;
 
 public class InputValidator {
+    private static final String DEFAULT_DELIMITER_PATTERN = "^(\\d+([,:]\\d+)*)?$";
+    private static final String CUSTOM_DELIMITER_PATTERN = "^//(.+)\\n\\d+([,:]\\d+|\\\\1\\d+)*$";
 
     public boolean isEmptyInput(String input) {
         if (input.isEmpty()) {
             return true;
         }
         return false;
+    }
+
+    public void validateInputPattern(String input) {
+        if (!input.matches(DEFAULT_DELIMITER_PATTERN) && !input.matches(CUSTOM_DELIMITER_PATTERN)) {
+            throw new IllegalArgumentException("잘못된 형식의 입력입니다.");
+        }
     }
 }
