44c194a04112e286951ae150046d75d2eebb8234
junsoo0719 <junsoo0719@gmail.com>
2024-10-19 18:35:52 +0900

    feat: 빈 문자열 입력 시 0 반환 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+class StringAddCalculator {
+    public int add(String input) {
+        if(input == null)
+            return 0;
+
+        return 0;
+    }
+}
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringAddCalculator stringAddCalculator = new StringAddCalculator();
+
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = Console.readLine();
+
+        int result = stringAddCalculator.add(input);
+        System.out.println("결과: " + result);
     }
 }
