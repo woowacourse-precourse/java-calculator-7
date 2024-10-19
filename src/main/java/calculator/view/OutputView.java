f1c13a43da8fa9722022456b6dc9d786035fbfa0
ruudska6 <feels19@naver.com>
2024-10-19 12:27:50 +0900

    <feat>: OutputView 결과 출력 메서드 printResult() 추가

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -4,6 +4,12 @@ public class OutputView {
     private static final String INPUT_PROMPT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
     // private static final String RESULT_PRINT_MESSAGE = "결과 : "
     public static void printInputPromptMessage() {
+        System.out.println();
         System.out.println(INPUT_PROMPT_MESSAGE);
     }
+
+    public static void printResult(int sum) {
+        System.out.println("결과 : " + sum);
+    }
+
 }
