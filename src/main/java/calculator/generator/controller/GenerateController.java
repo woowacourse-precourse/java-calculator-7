8d5a0218bb3a466a232c241f13c8fa621eb29915
EHCTH <lees90896@gmail.com>
2024-10-20 00:23:13 +0900

    refactor: 다형성 이용

--- src/main/java/calculator/generator/controller/GenerateController.java
+++ src/main/java/calculator/generator/controller/GenerateController.java
@@ -1,15 +1,25 @@
 package calculator.generator.controller;
 
-import calculator.number.domain.Number;
+import calculator.number.domain.NumberService;
 import calculator.generator.number.NumberGenerator;
+import calculator.number.util.NumberConvertorService;
 import calculator.sentence.domain.Sentence;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class GenerateController {
     private final NumberGenerator numberGenerator;
+
     public GenerateController(NumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
     }
-    public Number createNumber(Sentence sentence) {
-        return numberGenerator.create(sentence);
+
+    public NumberService createNumber(
+            Sentence sentence,
+            Supplier<NumberConvertorService> convertorServiceSupplier,
+            Function<List<Number>, NumberService> function) {
+        return numberGenerator.create(sentence, convertorServiceSupplier, function);
     }
 }
