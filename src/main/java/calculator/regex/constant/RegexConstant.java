5a12370538420dcfc61dd6644a6c556b72953b3e
EHCTH <lees90896@gmail.com>
2024-10-20 02:36:58 +0900

    style: package 변경으로 인한 공백 제거

--- src/main/java/calculator/regex/constant/RegexConstant.java
+++ src/main/java/calculator/regex/constant/RegexConstant.java
@@ -3,18 +3,9 @@ package calculator.regex.constant;
 import java.util.regex.Pattern;
 
 public class RegexConstant {
-    public static final int EXTRACT_START_WITH_ADD_INDEX = 2;
-    public static final int EXTRACT_SLASH_NEXT_LINE_ADD_INDEX = 1;
-    public static final String SINGLE_BACK_SLASH = "\\";
-    public static final String DOUBLE_BACK_SLASH = "\\\\";
-    public static final String FORWARD_SLASH = "/";
-    public static final String DOT = ".";
-    public static final String DOT_REGEX = "\\.";
     public static final Pattern ONLY_NUMBER_SEPARATOR_REGEX = Pattern.compile("[1-9]\\d*([,:][1-9]\\d*)*");
     public static final Pattern ONLY_NUMBER_SEPARATOR_DOUBLE_REGEX = Pattern.compile("^(?:0\\.(?!0+([,:]|$))\\d+|[1-9]\\d*(?:\\.\\d+)?)(?:[,:](?:0\\.(?!0+([,:]|$))\\d+|[1-9]\\d*(?:\\.\\d+)?))*$");
     public static final Pattern CUSTOM_SEPARATOR_REGEX = Pattern.compile("^//([^\\d\\s])\\\\n[1-9]\\d*(\\1[1-9]\\d*)*$");
     public static final Pattern CUSTOM_SEPARATOR_DOUBLE_REGEX = Pattern.compile("^//([^\\d\\s.])\\\\n(0\\.(?!0+($|\\D))\\d+|[1-9]\\d*(\\.\\d+)?)(\\1(0\\.(?!0+($|\\D))\\d+|[1-9]\\d*(\\.\\d+)?))*$");
     public static final Pattern BLANK_REGEX = Pattern.compile("^$");
-
-
 }
