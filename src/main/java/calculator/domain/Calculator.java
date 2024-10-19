bef81454039b750b5ff9b494d3e233d477c8e86e
dbdb456 <adad568@naver.com>
2024-10-19 20:26:30 +0900

    feat(Calculator): 구분자 리스트 변수 추가 및 초기화 메서드 구현

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,4 +1,15 @@
 package calculator.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
+
+    public List<Character> delimiters = new ArrayList<>();
+
+
+    public void init() {
+        this.delimiters.add(':');
+        this.delimiters.add(',');
+    }
 }
