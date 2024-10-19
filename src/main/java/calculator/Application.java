f188d3190b40cc2679b68179aa97c224475914bc
neoskyclad <neoskyclad@naver.com>
2024-10-20 01:28:24 +0900

    feat(Application): add console message to wait for user input
    Added a console message that informs the user to input data.
    The application waits for user input and prints it back to the console.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        input = Console.readLine();    // 사용자로부터 문자열 입력 받기
+
+        System.out.println(input);    //Debug
     }
 }
