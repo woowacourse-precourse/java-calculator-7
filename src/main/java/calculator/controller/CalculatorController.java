48eaf2d3e6a510b98dcfaf92491826bc62adface
whrjsgml0000 <whrjsgml4515@naver.com>
2024-10-19 12:29:26 +0900

    refactor:input 메서드와 output 메서드로 기능 분리

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -13,6 +13,9 @@ public class CalculatorController {
     public void input(){
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String s = Console.readLine();
+        output(s);
+    }
+    private void output(String s){
         int sum = calculatorService.sum(s);
         System.out.println("결과 : "+sum);
     }
