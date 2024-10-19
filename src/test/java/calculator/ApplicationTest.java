b8ed05edbe8b7b595c6b84a142609114d2077074
Bae-JunSeok <qownstjr77@gmail.com>
2024-10-20 05:03:16 +0900

    test: 테스트 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -2,7 +2,6 @@ package calculator;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
@@ -18,10 +17,58 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("-1,2,3"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertSimpleTest(() -> {
+            run("-1,2,3");
+            assertThat(output()).contains("음수는 입력할 수 없습니다: -1");
+        });
+    }
+
+    @Test
+    void 음수_입력_예외() {
+        assertSimpleTest(() -> {
+            run("-1,2,3");
+            assertThat(output()).contains("음수는 입력할 수 없습니다: -1");
+        });
+    }
+
+    @Test
+    void 일반_커스텀_구분자_함께_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:4");
+            assertThat(output()).contains("결과 : 7");
+        });
+    }
+
+    @Test
+    void 문자_입력_예외() {
+        assertSimpleTest(() -> {
+            run("1,a,3");
+            assertThat(output()).contains("유효하지 않은 숫자가 포함되어 있습니다: a");
+        });
+    }
+
+    @Test
+    void 입력_없음() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 숫자만_입력() {
+        assertSimpleTest(() -> {
+            run("1234");
+            assertThat(output()).contains("결과 : 1234");
+        });
+    }
+
+    @Test
+    void 긴_숫자_입력() {
+        assertSimpleTest(() -> {
+            run("1,999999999999999999999999");
+            assertThat(output()).contains("유효하지 않은 숫자가 포함되어 있습니다: 999999999999999999999999");
+        });
     }
 
     @Override
