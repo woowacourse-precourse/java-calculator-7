ae2c491c6ff1fb7473f761f4a9015d099bc01425
seophohoho <seop0937@gmail.com>
2024-10-20 03:48:06 +0900

    feat(Validator): 음수 입력 시, 에러 처리 추가

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -1,2 +1,9 @@
-package calculator;public class Validator {
+package calculator;
+
+public class Validator {
+    public void validate(String input) {
+        if (input.contains("-")) {
+            throw new IllegalArgumentException("ㅁㅊ음? 음수를 넣는다고?");
+        }
+    }
 }
