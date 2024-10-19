22038676ae93b7312addd958f94deb6bc87a11af
pakaheo <k70923@konkuk.ac.kr>
2024-10-19 17:06:14 +0900

    refactor : leave necessary verification and renamed verification method

--- src/main/java/calculator/util/InputValidator.java
+++ src/main/java/calculator/util/InputValidator.java
@@ -1,34 +1,21 @@
 package calculator.util;
 
-import calculator.constants.Constants;
+import calculator.constants.InvalidInputConstants;
 import java.util.regex.Matcher;
 
 public class InputValidator {
 
-    private static final int NUMBER_UNDER_BOUND = 47;
-    private static final int NUMBER_OVER_BOUND = 58;
-
-    public static void validateDelimiter(String input) {
-        if (!containsComma(input) && !containsColon(input) && isLongerThanOne(input)) {
-            throw new IllegalArgumentException(Constants.WRONG_DELIMITER);
+    public static Long validateOnlyPlainNumber(String text) {
+        try {
+            return Long.valueOf(text);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(InvalidInputConstants.WRONG_DELIMITER);
         }
-    }
-
-    private static boolean containsComma(String input) {
-        return input.contains(Constants.COMMA);
-    }
-
-    private static boolean containsColon(String input) {
-        return input.contains(Constants.COMMA);
-    }
-
-    private static boolean isLongerThanOne(String input) {
-        return input.length() > 1;
     }
 
     public static void validateCustomFormat(Matcher matcher) {
         if (!canFindRegex(matcher)) {
-            throw new IllegalArgumentException(Constants.INVALID_CUSTOM_FORMAT);
+            throw new IllegalArgumentException(InvalidInputConstants.INVALID_CUSTOM_FORMAT);
         }
     }
 
@@ -36,31 +23,10 @@ public class InputValidator {
         return matcher.find();
     }
 
-    public static void validateDefinedCustom(char letter, char asciiOfDelimiter) {
-        if (!canBeNumber(letter) && isDifferentCharacter(letter, asciiOfDelimiter)) {
-            throw new IllegalArgumentException(Constants.UNDEFINED_CUSTOM);
+    public static void validatePositive(Long number) {
+        if (number < 0L) {
+            throw new IllegalArgumentException(InvalidInputConstants.CONTAINS_NEGATIVE);
         }
     }
 
-    private static boolean canBeNumber(char letter) {
-        return letter > NUMBER_UNDER_BOUND && letter < NUMBER_OVER_BOUND;
-    }
-
-    private static boolean isDifferentCharacter(char letter, char asciiOfDelimiter) {
-        return letter != asciiOfDelimiter;
-    }
-
-    public static void validateHasNumber(String string) {
-        try {
-            Long.valueOf(string);
-        } catch (NumberFormatException numberFormatException) {
-            throw new IllegalArgumentException(Constants.NOT_CONTAINS_NUMBER);
-        }
-    }
-
-    public static void validatePositive(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException(Constants.CONTAINS_NEGATIVE);
-        }
-    }
 }
