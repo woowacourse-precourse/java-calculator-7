82c2ed8f0589b48aeb54dddf4c37a66ea4cd43bf
pakaheo <k70923@konkuk.ac.kr>
2024-10-20 08:36:26 +0900

    refactor : renamed parameter inputs -> calculationInputs

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Calculator {
 
-    public static Long addition(Long[] inputs) {
-        return Arrays.stream(inputs)
+    public static Long addition(Long[] calculationInputs) {
+        return Arrays.stream(calculationInputs)
                 .reduce(0L, Long::sum);
     }
 
