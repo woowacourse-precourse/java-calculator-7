ee9f4a1eb6ec2d4d40173a2094c44c5bb580c541
yaongmeow <minnie_00@naver.com>
2024-10-19 18:52:43 +0900

    feat(logic): 메타문자/이스케이프 문자 처리
    - 커스텀 문자로 메타문자/이스케이프 문자 제공 가능하도록 구현
    - Pattern.quote 사용
    - 커스텀 문자가 \n인 경우 분리하여 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,29 +1,37 @@
 package calculator;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("덧셈할 문자열을 입력해 주세요.\n");
         String input = scanner.nextLine();
-        Integer sum = 0;
+        int sum = 0;
 
         if (input.startsWith("//")) {
             // 커스텀 문자열 사용
             String [] customInfo = input.split("\\\\n", 2);
-
-            String customString = customInfo[0].substring(2);
-            String numbersString = customInfo[1];
-
-            String[] parsedInput = numbersString.split(String.format("%s|,|:", customString));
-            for (String number : parsedInput) {
-                sum += Integer.parseInt(number);
+            if (customInfo[0].equals("//") && customInfo[1].startsWith("\\n")){
+                String numbersString = customInfo[1].substring(2);
+                String[] parsedInput = numbersString.split(",|:|\\\\n");
+                for (String number : parsedInput) {
+                    sum += Integer.parseInt(number);
+                }
+            }
+            else{
+                String customString = Pattern.quote(customInfo[0].substring(2));
+                String numbersString = customInfo[1];
+                String[] parsedInput = numbersString.split(String.format("%s|,|:", customString));
+                for (String number : parsedInput) {
+                    sum += Integer.parseInt(number);
+                }
             }
         }
         else {
             // 커스텀 문자열 미사용
-            String[] parsedInput = input.split(",|:");
+            String[] parsedInput = input.split("[,:]");
 
             for (String s : parsedInput) {
                 sum += Integer.parseInt(s);
