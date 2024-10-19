72bad9f8a905e760eee2111a0a977ef25adb5dcb
Chaeruin <cms3604@naver.com>
2024-10-19 14:14:12 +0900

    fix(calc): improve error handling for invalid input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,75 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 1. 입력
+        String str = Console.readLine();
+        String seperator;
+        boolean isBasic;
+
+        // 2. 커스텀 구분자
+        // 커스텀 구분자 하면 기존 구분자는 무시하기로 하였음
+        String regex = "//(.*?)\\\\n";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+
+        if (matcher.find()) {
+            seperator = matcher.group(1);
+            str = str.substring(matcher.end());
+            isBasic = false;
+        }
+        // 3. 기본 구분자
+        else {
+            seperator = "[,:]";
+            isBasic = true;
+        }
+
+        // 4. 문자열 처리 - 숫자 분리
+        String[] result = str.split(seperator);
+
+        int[] resultInt = forStringToIntArray(result);
+        int sum = 0;
+
+        if (isPositive(resultInt)) {
+            for (int i : resultInt) {
+                sum += i;
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+        System.out.println("결과 : " + sum);
     }
+
+    // 문자열 To 숫자열
+    public static int[] forStringToIntArray(String[] strArr) {
+        int[] result = new int[strArr.length];
+        try {
+            for (int i = 0; i < strArr.length; i++) {
+                result[i] = Integer.parseInt(strArr[i]);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        
+        return result;
+    }
+
+    // 양수 검증
+    public static boolean isPositive(int[] intArr) {
+        for (int i : intArr) {
+            if (i <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+//    public static
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,15 +1,40 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+
     @Test
-    void 커스텀_구분자_사용() {
+    void 기본_구분자_사용_1() {
+        assertSimpleTest(() -> {
+            run("1,2,4");
+            assertThat(output()).contains("결과 : 7");
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용_2() {
+        assertSimpleTest(() -> {
+            run("1,2:4");
+            assertThat(output()).contains("결과 : 7");
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용_3() {
+        assertSimpleTest(() -> {
+            run("1:2:4");
+            assertThat(output()).contains("결과 : 7");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_1() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
@@ -17,10 +42,50 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_구분자_사용_2() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;3;2");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 예외_테스트_1() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//|\\n-1|2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2 3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_5() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//|\\n-1|2 3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
