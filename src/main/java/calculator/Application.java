ed25401b7547497f072b4ad1faa2c122914436f7
ComNewbie <iforking3@naver.com>
2024-10-20 00:29:46 +0900

    feat(ProgramOutput): 사용자 문자열 입력 요청 구현 void requestString() 메서드를 통해 - "덧셈할 문자열을 입력해 주세요." 문구 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import calculator.io.ProgramOutput;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ProgramOutput output = new ProgramOutput();
+
+        output.requestString();
     }
 }
---
+++ src/main/java/calculator/io/ProgramOutput.java
@@ -0,0 +1,9 @@
+package calculator.io;
+
+public class ProgramOutput {
+    private static final String REQUEST_STRING_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public void requestString() {
+        System.out.println(REQUEST_STRING_MESSAGE);
+    }
+}
