f825aff98109f491b7c6a6b6010bb450ef6722ac
Chaeruin <cms3604@naver.com>
2024-10-19 17:01:36 +0900

    chore(calc): clean up unused variables(isBasic) in addition logic

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,51 +1,43 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         // 1. 입력
         String str = Console.readLine();
-        String seperator;
-        boolean isBasic;
+        String[] seperator = new String[1];
 
-        // 2. 커스텀 구분자
-        // 커스텀 구분자 하면 기존 구분자는 무시하기로 하였음
-        String regex = "//(.*?)\\\\n";
-
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(str);
-
-        if (matcher.find()) {
-            seperator = matcher.group(1);
-            str = str.substring(matcher.end());
-            isBasic = false;
-        }
-        // 3. 기본 구분자
-        else {
-            seperator = "[,:]";
-            isBasic = true;
-        }
-
-        // 4. 문자열 처리 - 숫자 분리
-        String[] result = str.split(seperator);
+        // 문자열 처리 - 숫자 분리
+        String[] result = str.split(seperator[0]);
+//        String[] result = getSepAndStringArr(str, seperator);
 
         int[] resultInt = forStringToIntArray(result);
-        int sum = 0;
-
-        if (isPositive(resultInt)) {
-            for (int i : resultInt) {
-                sum += i;
-            }
-        } else {
-            throw new IllegalArgumentException();
-        }
+        int sum = sumIntArr(resultInt);
 
         System.out.println("결과 : " + sum);
     }
+
+    // Input Processing
+//    public static String[] getSepAndStringArr(String input, String[] seperator) {
+//        // 2. 커스텀 구분자
+//        // 커스텀 구분자 하면 기존 구분자는 무시하기로 하였음
+//        String regex = "//(.*?)\\\\n";
+//
+//        Pattern pattern = Pattern.compile(regex);
+//        Matcher matcher = pattern.matcher(input);
+//
+//        if (matcher.find()) {
+//            seperator[0] = matcher.group(1);
+//            return input.substring(matcher.end()).split(seperator[0]);
+//        }
+//        // 3. 기본 구분자
+//        else {
+//            seperator[0] = "[,:]";
+//            return input.split(seperator[0]);
+//        }
+//    }
 
     // 문자열 To 숫자열
     public static int[] forStringToIntArray(String[] strArr) {
@@ -57,7 +49,7 @@ public class Application {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }
-        
+
         return result;
     }
 
@@ -65,11 +57,20 @@ public class Application {
     public static boolean isPositive(int[] intArr) {
         for (int i : intArr) {
             if (i <= 0) {
-                return false;
+                throw new IllegalArgumentException();
             }
         }
         return true;
     }
 
-//    public static
+    // Sum
+    public static int sumIntArr(int[] intArr) {
+        int sum = 0;
+        if (isPositive(intArr)) {
+            for (int i : intArr) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
 }
