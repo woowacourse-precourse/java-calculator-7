6cbccb4a6f46982e21ecf3f020ca3bb010110524
Cheong Ye Jun <113380447+LooanCheong@users.noreply.github.com>
2024-10-20 00:52:23 +0900

    fix: UserInput 버그 수정
    UserInput의 separators를 초기화 하는 로직 수정:
    - 기존 코드를 수정하며 separators의 기본 구분자 처리가 빠진 부분을 복구

--- src/main/java/calculator/UserInput.java
+++ src/main/java/calculator/UserInput.java
@@ -2,11 +2,12 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.List;
 
 public class UserInput {
 
     private String input;
-    private ArrayList<Character> separators;
+    private ArrayList<Character> separators = new ArrayList<>(List.of(',', ':'));
 
     public void input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
