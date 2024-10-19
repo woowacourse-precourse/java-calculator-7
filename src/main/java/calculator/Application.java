1c776db4ef63080427fa99eedbac8b6da1c9a0c8
Jinho622 <aceho21@gmail.com>
2024-10-19 21:57:58 +0900

    feat(): 커스텀 구분자 추출 및 활용, 관련 예외처리
    '//'가 문자열 맨앞에 있는 경우 handleCustomDelimiter함수에서 커스텀 구분자 추출
    '//'와 '\n'사이에 커스텀 구분자가 하나 있을 때를 제외한 경우는 예외 처리
    예외 처리 후 sum함수를 통해 나머지 문자열의 숫자 합 계산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -23,7 +23,24 @@ public class Application {
         if(input.equals("")){ // 빈 값 처리해주기
             return 0;
         }
+        if (input.startsWith("//")) {
+            return handleCustomDelimiter(input);
+        }
         return sum(input);
+    }
+
+    private static int handleCustomDelimiter (String input){
+        String[] parts;
+        if(input.charAt(3) == '\\' && input.charAt(4) == 'n'){
+            delimiters = delimiters.substring(0, 3) +  input.substring(2, 3) + "]";
+//            delimiters += "|" + input.substring(2, 3);
+            parts = input.split("n");
+        }
+        else{ // \n가 없거나 커스텀 구분자가 여러개라면 예외처리
+            throw new IllegalArgumentException("커스텀 구분자 정의형식이 잘못되었습니다.");
+        }
+        String numbers = parts[1];
+        return sum(numbers);
     }
 
     private static int sum (String numbers) {
