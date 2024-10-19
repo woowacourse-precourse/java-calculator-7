45dfa7d968885881c9b25cebe589a1d3f6ad22f4
wowseok <wowseok2774@gmail.com>
2024-10-19 23:46:28 +0900

    feat: 프로그램 실행 로직 담당

---
+++ src/main/java/calculator/CalculatorService.java
@@ -0,0 +1,26 @@
+package calculator;
+
+public class CalculatorService {
+    private final OutputHandler outputHandler;
+    private final InputHandler inputHandler;
+
+
+    // 생성자에서 의존성 주입
+    public CalculatorService() {
+        this.outputHandler = new OutputHandler();
+        this.inputHandler = new InputHandler();
+    }
+
+
+    public void run() {
+        try {
+            outputHandler.guideComment();
+            String userInput = inputHandler.getUserInput();
+            Calculator calculator = new Calculator(userInput);
+            int result = calculator.calculate();  // 계산 수행
+            outputHandler.printSum(result); // 결과 출력
+        } catch (IllegalArgumentException e) {
+            System.err.println("잘못된 입력: " + e.getMessage());
+        }
+    }
+}
