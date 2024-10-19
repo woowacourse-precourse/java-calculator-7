78790ed1ce97a42f343b78644037920cc1a47c02
Jun2-Lee <ljh19980823@gmail.com>
2024-10-20 00:48:19 +0900

    feat : 커스텀 구분자 존재 시, 구분자 목록에 추가하기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -24,6 +24,13 @@ public class Application {
         return matcher.find();
     }
 
+    // 커스텀 구분자 존재 시, 커스텀 구분자 부분 분리하여 구분자에 추가하기
+    private static String getCustomDelimiter(String str) {
+        String regex = "//(.*?)\n";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.group(1);
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         StringBuilder delimiter = new StringBuilder();
@@ -31,7 +38,7 @@ public class Application {
         init_print();
         String str = input();
         if(checkCustomDelimiter(str)) {
-
+            delimiter.append("|").append(getCustomDelimiter(str));
         }
     }
 }
