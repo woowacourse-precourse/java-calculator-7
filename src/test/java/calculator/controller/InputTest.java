62387c5ac2ec9246420fdea33bb017d03f65aeb4
kdongsu5509 <kdongsu5509@gmail.com>
2024-10-19 18:01:01 +0900

    fix(input): change getUserInputTestTwo codes
    Change getUserInputTestTwo codes to same Input with when using termanl.
    Before fix, input value at test is not equal to terminal input

--- src/test/java/calculator/controller/InputTest.java
+++ src/test/java/calculator/controller/InputTest.java
@@ -1,11 +1,15 @@
 package calculator.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.Test;
 
 class InputTest {
+    /**
+     * 특수 문자 없는 입력값을 받는지 확인한다.
+     */
     @Test
     void getUserInputTest() {
         // Given
@@ -18,5 +22,22 @@ class InputTest {
 
         // Then
         assertThat(userInput).contains(sampleInput);
+    }
+
+    /**
+     * 특수 문자 \n 을 정상적으로 처리하는지 확인.
+     */
+    @Test
+    void getUserInputTestTwo() {
+        // Given
+        String sampleString = "//;\\n1;2;3";
+        ByteArrayInputStream sampleStringByteArray = new ByteArrayInputStream(sampleString.getBytes());
+        System.setIn(sampleStringByteArray);
+
+        // When
+        String userInput = new Input().getUserInput();
+
+        // Then
+        assertEquals(userInput, sampleString);
     }
 }
