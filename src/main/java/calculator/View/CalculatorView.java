1da7a4427b59b8c0108ddc6bfea9b508c91c2270
윤혜진 <lold2424@gmail.com>
2024-10-19 17:54:04 +0900

    refactor : 추가된 입, 출력 메시지 enum을 적용하게 수정

--- src/main/java/calculator/View/CalculatorView.java
+++ src/main/java/calculator/View/CalculatorView.java
@@ -6,13 +6,13 @@ import camp.nextstep.edu.missionutils.Console;
 public class CalculatorView {
 
     public String getInput() { // 사용자 입력을 받음
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println(IOMessage.INPUT_MESSAGE.getMessage());
         String input = Console.readLine();
         return input;
     }
 
     public void showResult(int sum) { // 결과를 출력
-        System.out.printf("결과 : %d", sum);
+        System.out.printf(IOMessage.OUTPUT_MESSAGE.getMessage() + sum);
     }
 
     public void showError(String message) { // 오류 메시지를 출력
