dcdcb052b91ca7f6d6a92242c7efc1091870763a
wodus4145@naver.com <wodus4145@naver.com>
2024-10-19 17:31:16 +0900

    refactor: Modify the addition method
    Modify the code to handle exceptions when the user inputs negative numbers.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,7 +7,11 @@ public class Application {
         int sum = 0;
         for(String number : strArr) {
             try {
-                sum += Integer.parseInt(number);
+                if(Integer.parseInt(number)<0) {
+                    throw new IllegalArgumentException();
+                } else {
+                    sum += Integer.parseInt(number);
+                }
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException();
             }
