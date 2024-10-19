7265c5c6a10ec6bd5154924f95394068dfca06b7
sinyebin <dpqls0625@naver.com>
2024-10-20 01:38:13 +0900

    docs: 요구사항 정리

---
+++ src/main/java/calculator/InputView.java
@@ -0,0 +1,10 @@
+package calculator;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+    public static String inputString(Runnable messagePrinter){
+        messagePrinter.run();
+        return readLine();
+    }
+}
