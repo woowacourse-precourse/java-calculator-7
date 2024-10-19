a4d27c270f620c819d73ff659b9fa01f0b772d6c
KJungW <sagakkb0707@naver.com>
2024-10-19 13:24:43 +0900

    feat(calculator): implement a calculator that calculates sum based on ',' and ':'
    Implement a calculator that proceeds through the following process:
    1. receive calculation input f​rom the user.
    2. separate operands from calculation input ​​using ',', and ':' as delimiters.
    3. add up all operands.
    4. print the calculation result.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String calculationInput = getCalculationInput();
+        List<Integer> operands = decodeCalculationInput(calculationInput);
+        System.out.println("결과 : " + addAll(operands));
     }
+
+    private static String getCalculationInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    private static List<Integer> decodeCalculationInput(String calculationInput) {
+        List<String> operands = List.of(calculationInput.split("[,:]"));
+        return operands.stream().map(Integer::valueOf).toList();
+    }
+
+    private static Integer addAll(List<Integer> operands) {
+        return operands.stream().mapToInt(operand -> operand).sum();
+    }
+
 }
