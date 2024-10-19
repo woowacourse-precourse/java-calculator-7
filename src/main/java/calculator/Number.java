fbee408d881626bb6b415a43f6abb416d511055e
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-10-20 02:08:17 +0900

    feat(number): add method to return value of Number instance
    Add method to return value of Number instance from biginteger
    - return number value using toString()

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -32,6 +32,10 @@ public class Number {
         return this;
     }
 
+    public String value() {
+        return value.toString();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
