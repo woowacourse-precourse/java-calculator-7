50d8d5e803162e7fac6062228a7678890f449393
hooniverse <sung010326@gmail.com>
2024-10-20 01:45:37 +0900

    feat: Application 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,37 @@
 package calculator;
 
+import calculator.domain.TextInfo;
+import calculator.io.Input;
+import calculator.io.Output;
+import calculator.service.NumberService;
+import calculator.service.SplitService;
+import calculator.service.TextService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Input input = new Input();
+        String userInput = input.inputText();
+
+        TextInfo textInfo = new TextInfo(userInput);
+
+        SplitService splitService = new SplitService();
+        TextService textService = new TextService();
+
+        if (splitService.checkCustomSplit(textInfo)) {
+            splitService.addSplit(textInfo);
+            textService.changeText(textInfo);
+        }
+        NumberService numberService = new NumberService();
+
+        String finalSplitString = splitService.mergeSplits(textInfo.split);
+        splitService.splitText(textInfo, finalSplitString);
+
+        numberService.setNumbers(textInfo);
+        numberService.addNumbers(textInfo);
+
+        Output output = new Output();
+        output.outputResult(textInfo);
     }
+
 }
