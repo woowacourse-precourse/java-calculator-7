b0c60ae8ef9d9e652f76272c1c2f9779297f3069
Jeon Young Sang <iwtl0303@naver.com>
2024-10-19 22:47:58 +0900

    feat(): 문자열 구분 후 덧셈 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,7 +9,12 @@ public class Application {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String line = "";
+        String newLine = "";
         String custom = "";
+        String[] ansLine1 = {};
+        String[] ansLine2 = {};
+        boolean isCus = false;
+        int ans = 0;
         try {
             bw.write("덧셈할 문자열을 입력해 주세요.\n");
             bw.flush();
@@ -19,24 +24,44 @@ public class Application {
         }
         String[] lines = line.split("");
         int x = 2;
-        if (lines[0].equals("/") && lines[1].equals("/")) {
-            while(true) {
+        if (lines[0].equals("/") && lines[1].equals("/")) { // 커스텀 구분자 추가
+            while (x < lines.length) {
                 if (lines[x].equals("\\") && lines[x+1].equals("n")) {
+                    isCus = true;
                     break;
                 }
                 custom += lines[x++];
             }
+
         }
+        if (isCus) {
+            for (int i = x+2; i < lines.length; i++) {
+                newLine += lines[i];
+            }
+        } else {
+            newLine = line;
+        }
+        ansLine1 = newLine.split("[,:]");
+        for (int i = 0; i < ansLine1.length; i++) {
+            if (isCus) {
+                ansLine2 = ansLine1[i].split(custom);
+                for (int j = 0; j < ansLine2.length; j++) {
+                    ans += Integer.parseInt(ansLine2[j]);
+                }
+            } else {
+                ans += Integer.parseInt(ansLine1[i]);
+            }
+        }
+
         try {
-            bw.write("결과 : " + custom);
+            bw.write("결과 : " + ans + "\n");
+            for (int i = 0; i < ansLine1.length; i++) {
+                bw.write(ansLine1[i] + "\n");
+            }
             bw.flush();
             bw.close();
         } catch (IOException e) {
-
+            e.printStackTrace();
         }
-
-
-
-
     }
 }
