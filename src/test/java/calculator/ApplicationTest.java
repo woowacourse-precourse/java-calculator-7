2019c479ab1ac7ef6a7390111371224ad8338714
kgy1008 <gayeon1008@g.skku.edu>
2024-10-19 16:29:26 +0900

    test: add test for custom delimiter longer than one character

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,6 +17,14 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 길이가_1이상인_커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;?\\n1;?2;?3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 구분자_없을_경우_테스트() {
         assertSimpleTest(() -> {
             run("3");
