2003f0b5fa8fd89e850f1a9baeea4e459a231488
Niceinga <Niceinga>
2024-10-19 21:33:34 +0900

    문자열을 입력받는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+
+        String str = camp.nextstep.edu.missionutils.Console.readLine();
+
+        int ans = 0;
+        ans = summarize(str);
+        System.out.println("결과 : " + ans);
     }
 }
