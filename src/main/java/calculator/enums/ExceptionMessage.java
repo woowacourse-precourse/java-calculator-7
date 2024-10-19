935042b6146ff173a34ebb620e6d438907ee8998
codrin2 <hssang127@gmail.com>
2024-10-19 18:28:28 +0900

    feat(InputModel): 빈 문자열이나 null 입력 시 예외처리하는 검증 로직 추가

---
+++ src/main/java/calculator/enums/ExceptionMessage.java
@@ -0,0 +1,5 @@
+package calculator.enums;
+
+public class ExceptionMessage {
+	public static final String INPUT_EMPTY = "입력값이 비어있습니다.";
+}
--- src/main/java/calculator/model/InputModel.java
+++ src/main/java/calculator/model/InputModel.java
@@ -1,13 +1,22 @@
 package calculator.model;
 
+import calculator.enums.ExceptionMessage;
+
 public class InputModel {
 	private final String input;
 
 	public InputModel(String input) {
+		validateInput(input);
 		this.input = input;
 	}
 
 	public String getInput() {
 		return input;
+	}
+
+	private void validateInput(String input) {
+		if (input == null || input.trim().isEmpty()) {
+			throw new IllegalArgumentException(ExceptionMessage.INPUT_EMPTY);
+		}
 	}
 }
