387c178a0a3fe1d0b4bed77d066909fa25e47058
woojin <zaza0804@uos.ac.kr>
2024-10-20 02:15:05 +0900

    refactor($Application): inject Calculator instance from main
    - Calculator의 의존성을 main에서 주입합니다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,16 @@
 package calculator;
 
-import calculator.app.Calculator;
+import calculator.controller.Calculator;
+import calculator.model.Separator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Separator separator = new Separator();
+        Calculator calculator = new Calculator(inputView, outputView, separator);
         calculator.start();
     }
 }
