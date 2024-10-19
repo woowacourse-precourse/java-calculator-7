043016537069ec71972c395c5e9dcf2b1cbc88cc
장시영 <sysmile77@gmail.com>
2024-10-20 02:03:58 +0900

    feat(Validation): 유효성검증 및 예외상황처리

--- src/main/java/calculator/logic/Validation.java
+++ src/main/java/calculator/logic/Validation.java
@@ -1,29 +1,62 @@
 package calculator.logic;
 
+import static calculator.logic.CustomChecker.isCustom;
+
 public class Validation {
 
     public static void execution(String str) {
-
-    isNotNull(str);
-    isContainCustom(str);
-
+        if(isCustom(str)) {
+            checkCustomVal(str);
+        } else {
+            checkNormalVal(str);
+        }
     }
 
+    private static void checkNormalVal(String str) {
 
-    public static boolean isNotNull(String str) {
+        String[] numbers = Delimiter.doNormalDelimiter(str);
 
-        return false;
+        if(!(checkInteger(numbers[0]))) {
+            throw new IllegalArgumentException("일반 입력의 경우에는 첫번째 수는 숫자이어야합니다");
+        }
+
+        for (String number : numbers) {
+            try {
+                int num = Integer.parseInt(number); // 문자열을 정수로 변환, getInt랑 다ㅇ
+                if (num < 0) {
+                    throw new IllegalArgumentException("입력값 중 음수가 있습니다");
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("유효하지 않은 숫자 형식입니다");
+            }
+        }
     }
 
-    public static boolean isContainCustom(String str) {
+    // 커스텀 문자로 숫자가 들어온 경우
+    private static void checkCustomVal(String str) {
+      String[] numbers = Delimiter.doCustomDelimiter(str);
 
-        return false;
+        if((checkInteger(CustomChecker.getCustom(str)))){
+            throw new IllegalArgumentException("커스텀 값으로 숫자는 입력할 수 없습니다.");
+        }
+
+      for(String number : numbers) {
+
+          if(Integer.parseInt(number) < 0) {
+              throw new IllegalArgumentException("커스텀 입력 값 중 음수가 있습니다.");
+
+                 }
+            }
+        }
+
+    private  static boolean checkInteger(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (NumberFormatException e){
+            return false;
+        }
     }
 
-
-
-    // 입력 유효성 판별
-    // 비어있는 문자열인가? -> 요구사항에서 "" 의 경우에는 ... 0의 값을 리턴하라고 했는데..
-    // ( 커스텀 체크를 하고 ) -> 유효한 문자열로 구분되어 있는가?
 
 }
