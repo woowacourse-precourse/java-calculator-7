deb3559698043aea3ecf01ae1d634bf3b07c0e1f
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-19 14:56:30 +0900

    refactor(StringValidator): 커스텀 구분자 포맷 정규표현식 수정
    - 커스텀 구분자로 여러 자리의 숫자도 등록하지 못하게 변경

--- src/main/java/calculator/validator/StringValidator.java
+++ src/main/java/calculator/validator/StringValidator.java
@@ -4,8 +4,10 @@ import calculator.global.exception.ErrorMessage;
 
 public class StringValidator {
 
+    private static final String DELIMITER_FORMAT_REGEX = "^[0-9-]*$";
+
     public static void validateCustomDelimiterFormat(String str) {
-        if (str.matches("[\\d-]")) {
+        if (str.matches(DELIMITER_FORMAT_REGEX)) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_DELIMITER_ERROR.getMessage());
         }
     }
