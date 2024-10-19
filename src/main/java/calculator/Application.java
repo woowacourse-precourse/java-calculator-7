b4143b064dd85d700eb281e2ca92fa048ffa9600
badgelatte <saucy5939@gmail.com>
2024-10-19 23:52:43 +0900

    feat: 입력된 문자열 쉼표 혹은 세미콜론으로 분리, 예외 발생 처리
    쉼표 혹은 세미콜론으로 문자열 분리. 예외 발생 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,7 +7,16 @@ public class Application {
         try {
             System.out.println("덧셈할 문자열을 입력해 주세요.");
             String input = Console.readLine();
-        } catch (Exception e) {
+            String delimiter = "[,;]";
+            String[] inputSplits = input.split(delimiter);
+            int result = 0;
+
+            for (String s : inputSplits) {
+                result += Integer.parseInt(s);
+            }
+
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
