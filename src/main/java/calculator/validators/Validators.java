f40d3a42c69eb5948f92da35e066b83beb082860
dbdb456 <adad568@naver.com>
2024-10-19 20:10:37 +0900

    feat(Validators): 구분자 선언 검증 메서드 추가

--- src/main/java/calculator/validators/Validators.java
+++ src/main/java/calculator/validators/Validators.java
@@ -1,4 +1,14 @@
 package calculator.validators;
 
 public class Validators {
+
+    public static boolean validateDelimiterDeclaration(String s) {
+        if (s.startsWith("//")) {
+            if (!s.contains("\\n")) {
+                throw new IllegalArgumentException();
+            }
+            return true;
+        }
+        return false;
+    }
 }
