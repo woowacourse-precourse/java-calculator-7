2113c4171899b5ceb34d1556b5e5529d2a34d2b7
Sooyong Yun <sooyongyun@Sooyongui-MacBookAir.local>
2024-10-20 01:04:46 +0900

    feat: 기능3 잘못된 값을 입력할 경우 예외 처리 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -14,8 +14,14 @@ public class Application {
         int answer = 0;
 
         String[] numbers = string_to_calc.split("[" +separator + ": ,]");
-        for (String number : numbers){
-            answer += Integer.parseInt(number);
+        for (String number : numbers) {
+            if (!number.isEmpty()) {
+                try {
+                    answer += Integer.parseInt(number);
+                } catch (IllegalArgumentException e) { // 기능3. 잘못된 값 입력할 경우 예외 발생
+                    System.exit(0);
+                }
+            }
         }
 
         return answer;
@@ -40,12 +46,11 @@ public class Application {
     public static void main(String[] args) {
         String string_to_calc = Console.readLine();
         //\n을 인식하도록 입력 문자열 처리
-        string_to_calc = string_to_calc.replace("\\n", "\n");
+        //string_to_calc = string_to_calc.replace("\\n", "\n");
         char separator = find_separator(string_to_calc);
-        if (separator == ','){
+        if (separator == ',') {
             System.out.println(basic_add(string_to_calc));
-        }
-        else {
+        } else {
             //커스텀 구분자가 있으면 \n 뒤 문자열에서 계산
             int newlineIndex = string_to_calc.indexOf("\n");
             if (newlineIndex != -1) {
