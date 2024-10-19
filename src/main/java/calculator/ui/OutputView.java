80bcc88a1602160b8cf10b266311c748f1734468
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-10-20 02:31:06 +0900

    refactor: 추후 변경 가능성을 감안해 ui 부분을 interface로 정의

--- src/main/java/calculator/ui/OutputView.java
+++ src/main/java/calculator/ui/OutputView.java
@@ -1,12 +1,13 @@
 package calculator.ui;
 
-public class OutputView {
+public class OutputView implements OutputHandler {
 
     private static final String OUTPUT_MESSAGE = "결과 : ";
 
     public OutputView() {
     }
 
+    @Override
     public void output(int result) {
         System.out.println(OUTPUT_MESSAGE + result);
     }
