021104d72369e351f864f65933abdbd070d3af09
Chanwon-Seo <scwonn60@gmail.com>
2024-10-19 12:12:13 +0900

    refactor: CalculatorController 구조 간소화
    - InputHandler와 OutputView에 대한 직접적인 의존성을 제거하고 InputView와 OutputResponse를 사용하도록 변경
    - run 메소드에서 입력 및 출력 처리를 간소화
    - 유지보수성을 높이기 위해 책임 분리 개선

--- src/main/java/calculator/domain/CalculatorController.java
+++ src/main/java/calculator/domain/CalculatorController.java
@@ -1,22 +1,20 @@
 package calculator.domain;
 
-import calculator.domain.handler.InputHandler;
+import calculator.domain.dto.InputRequest;
+import calculator.domain.dto.OutputResponse;
+import calculator.domain.view.InputView;
 import calculator.domain.view.OutputView;
 
 public class CalculatorController {
-    private final InputHandler inputHandler;
-    private final OutputView outputView;
     private final CalculatorService calculatorService;
 
     public CalculatorController() {
-        this.inputHandler = new InputHandler();
-        this.outputView = new OutputView();
         this.calculatorService = new CalculatorService();
     }
 
     public void run() {
-        String input = inputHandler.getInput();
-        int result = calculatorService.calculate(input);
-        outputView.outputMessage(result);
+        InputRequest input = InputView.getInputRequest();
+        OutputResponse result = calculatorService.calculate(input);
+        OutputView.outputMessage(result);
     }
 }
