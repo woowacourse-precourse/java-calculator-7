9ddc44d11e96a7ba74c420e88b19384d2320c6a1
reindeer002 <lsj000424@gmail.com>
2024-10-20 01:54:11 +0900

    refactor(constant): add constants custom delimiter prefix/suffix and default delimiter

---
+++ src/main/java/calculator/constant/DelimiterConstant.java
@@ -0,0 +1,7 @@
+package calculator.constant;
+
+public class DelimiterConstant {
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+    public static final String DEFAULT_DELIMITERS = ",|:";
+}
