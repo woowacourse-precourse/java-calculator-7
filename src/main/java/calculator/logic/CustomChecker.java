5f9842c6c04de66585363a0b2b64382e510b2d0d
장시영 <sysmile77@gmail.com>
2024-10-19 20:18:49 +0900

    feat(CustomChecker): 커스텀 문자열 추출

--- src/main/java/calculator/logic/CustomChecker.java
+++ src/main/java/calculator/logic/CustomChecker.java
@@ -1,12 +1,29 @@
 package calculator.logic;
 
+
 public class CustomChecker {
 
     public static char[] customArray  = new char[10];
+    static StringBuilder sb = new StringBuilder();
 
-    /* 커스텀 문자가 있는지 확인하고, 만약 커스텀문자열이라면 커스텀문자열을 배열로 저장*/
+    public static String getCustom(String str) {
+
+        if(str.charAt(0) == '/' && str.charAt(1)=='/'){
+            for(int i=2; i<str.length(); i++) {
+                if(str.charAt(i-1) == '\\' && str.charAt(i)=='n') {
+                    for(int j=0; j<i-3; j++) {
+                        //  System.out.println("j:"+j+" i:"+(i-2)+" "+str.charAt(j));
+                        sb.append(str.charAt(j + 2));
+                    }
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+
+
     public static boolean isCustom(String str) {
-
         if(str.charAt(0) == '/' && str.charAt(1)=='/'){
             for(int i=2; i<str.length(); i++) {
                 if(str.charAt(i-1) == '\\' && str.charAt(i)=='n') {
@@ -18,7 +35,6 @@ public class CustomChecker {
                 }
             }
         }
-
         return false;
     }
 }
