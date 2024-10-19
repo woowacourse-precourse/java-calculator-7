471c6f994037a8eab5ef116e8dba8b4bccc959d0
howareyoo <newworld121@naver.com>
2024-10-19 23:13:41 +0900

    feat : 커스텀 구분자 찾기

--- src/main/java/calculator/StringValidator.java
+++ src/main/java/calculator/StringValidator.java
@@ -1,10 +1,21 @@
 package calculator;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringValidator {
 
-    public static String customDelimiterValidator(String userInput) throws IllegalArgumentException {
+    public static String findCustomDelimiter(String userInput) {
+        Pattern customPatter = Pattern.compile("//(.)\\n");
+        Matcher matcher = customPatter.matcher(userInput);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return null;
+    }
+
+    public static String delimiterValidator(String userInput) throws IllegalArgumentException {
         for (Delimiter delimiter : Delimiter.values()) {
             if (userInput.contains(delimiter.getDelimiter())) {
                 return delimiter.getDelimiter();
