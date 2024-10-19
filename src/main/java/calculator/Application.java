e40cf7aa25bf756f53ed115891791cb6f9528024
thoi2 <kinggun1505@naver.com>
2024-10-19 15:40:45 +0900

    README 5 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -14,6 +14,16 @@ package calculator;
         }
         System.out.println(delimiter);
         String[] tokens = input.split(delimiter);
+        int sum = 0;
+        for (String token : tokens) {
+            try {
+                sum += Integer.parseInt(token);
 
+        }  catch(NumberFormatException e){
+            throw new IllegalArgumentException("잘못된 입력입니다. :" + token);
+        }
     }
+        return  sum;
+    }
+}
 
