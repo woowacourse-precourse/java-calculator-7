86e9da0e9dde8d808568194cd3ab42b980e3f642
reindeer002 <lsj000424@gmail.com>
2024-10-20 02:00:07 +0900

    chore(main): update main function to use the refactored code structure

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,23 @@
 package calculator;
 
+import calculator.model.Operand;
 import calculator.service.CalculatorService;
+import calculator.service.ExtractorService;
 import calculator.service.impl.CalculatorServiceImpl;
+import calculator.service.impl.ExtractorServiceImpl;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        ExtractorService extractor = new ExtractorServiceImpl();
         CalculatorService calculator = new CalculatorServiceImpl();
+
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String s = Console.readLine();
-        System.out.println("결과 : " + calculator.add(s));
+        String input = Console.readLine();
         Console.close();
+
+        Operand operand = extractor.extract(input);
+
+        System.out.println("결과 : " + calculator.calculate(operand));
     }
 }
