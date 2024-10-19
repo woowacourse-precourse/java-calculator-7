0a0ae44dd9e2cafcf8ca3b0661683442b020ae46
exena <exena01@gmail.com>
2024-10-19 18:03:00 +0900

    feat: added a function to get custom separators from a user input

--- src/main/java/calculator/Tokenizer.java
+++ src/main/java/calculator/Tokenizer.java
@@ -21,6 +21,15 @@ public class Tokenizer {
         return nums;
     }
 
+    public static char[] getCustomSeparators(String user_input) {
+        Pattern pattern = Pattern.compile("^(//)(.*)\\\\n");
+        Matcher matcher = pattern.matcher(user_input);
+        if (matcher.find()){
+            return matcher.group(2).trim().toCharArray();
+        }
+        return new char[] {};
+    }
+
     public static String removeHeader(String user_input) {
         return user_input.replaceAll("^(//).*\\\\n", "");
     }
