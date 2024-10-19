2ba6fe844a112187afd0c043db0e4c31eff2c389
zjhj0814 <zjhj0814@gmail.com>
2024-10-20 00:28:21 +0900

    feat(Application): 계산기 시작 안내 문자열 상수화
    '덧셈할 문자열을 입력해 주세요.'를 상수화한다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,8 @@
 package calculator;
 
 public class Application {
+    private static final String inputPhrase = "덧셈할 문자열을 입력해 주세요.";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
