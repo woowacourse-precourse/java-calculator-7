7929837c8bf6b617d2b4b30730a8242c8a8a3bb4
HongSeongJo <164273757+HongSeongJo@users.noreply.github.com>
2024-10-20 01:00:11 +0900

    feat(input): 사용자에게 입력을 받는다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final String EXIT_COMMAND = "N";
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        while (true) {
+            System.out.println("문자열 덧셈 계산기");
+            System.out.print("계산하고 싶은 문자열을 입력하시오(종료를 원할시 'N'을 입력하세요): ");
+            String input = Console.readLine();
+
+            if(input.isEmpty()) {
+                throw new IllegalArgumentException("문자열이 입력되지 않았습니다.");
+            }
+
+            if(input.equals(EXIT_COMMAND))
+                break;
+        }
+        System.out.println("지금까지 문자열 덧셈 계산기였습니다.");
     }
 }
