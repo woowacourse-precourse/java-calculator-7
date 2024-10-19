8964d3995316eeb054fcab7fd071a2b8b5f3b495
Seungwan Yoo <ysw99@kakao.com>
2024-10-19 13:26:03 +0900

    feat: 커스텀 구분자를 통한 문자열 분리 후 계산 구현

--- src/main/java/calculator/model/CalcModel.java
+++ src/main/java/calculator/model/CalcModel.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 public class CalcModel {
     private final static String DEFAULT_DELIMITER_PREFIX = "[,:]";
+    private final static String CUSTOM_DELIMITER_START_PREFIX = "//";
 
     public int calcResult(String inputString) {
         if (inputString == null || inputString.isBlank())
             return 0;
+
+        if (inputString.startsWith(CUSTOM_DELIMITER_START_PREFIX))
+            return sumWithCustomDelimiter(inputString);
 
         return sumNumbers(parseByDelimiter(inputString, DEFAULT_DELIMITER_PREFIX));
     }
@@ -16,7 +20,17 @@ public class CalcModel {
         return numbersList.stream().mapToInt(Integer::parseInt).sum();
     }
 
+    public int sumWithCustomDelimiter(String inputString) {
+        String customDelimiter = "[" + inputString.charAt(2) + "]";
+        List<String> numbersList = parseByDelimiter(inputString, customDelimiter);
+        return sumNumbers(numbersList);
+    }
+
     public List<String> parseByDelimiter(String inputString, String delimiter) {
-        return List.of(inputString.split(delimiter));
+        if (inputString.startsWith(CUSTOM_DELIMITER_START_PREFIX)) {
+            return List.of(inputString.substring(5).split(delimiter));
+        } else {
+            return List.of(inputString.split(delimiter));
+        }
     }
 }
