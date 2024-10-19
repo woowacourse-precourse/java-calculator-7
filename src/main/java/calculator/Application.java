04ea8f5ac3324841bfad78b196edbf9cda16b51a
tibetyo <zjhj0814@gamil.com>
2024-10-20 02:01:06 +0900

    feat(Application): 커스텀 구분자 사용여부 확인 기능 추가
    커스텀 구분자 패턴을 상수화하고 커스텀 구분자 사용 여부를 확인한다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,9 +5,16 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     private static final String inputPhrase = "덧셈할 문자열을 입력해 주세요.";
     private static final String outputPhrase = "결과 : ";
+    private static final String startCustomPattern = "//";
+    private static final String endCustomPattern = "\n";
 
     public static void main(String[] args) {
-        System.out.println("inputPhrase = " + inputPhrase);
+        System.out.println(inputPhrase);
         String userInput = readLine();
     }
+
+    private static boolean isCustomDelimiter(String userInput) {
+        return userInput.startsWith(startCustomPattern) && userInput.contains(endCustomPattern);
+    }
+
 }
