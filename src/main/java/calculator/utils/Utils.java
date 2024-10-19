7aac69f7611c35163b987368fcec4ff78e11d2fa
dbdb456 <adad568@naver.com>
2024-10-19 21:00:45 +0900

    feat(Utils): 커스텀 구분자를 리스트에 추가하는 메서드 추가

--- src/main/java/calculator/utils/Utils.java
+++ src/main/java/calculator/utils/Utils.java
@@ -1,4 +1,21 @@
 package calculator.utils;
 
+import calculator.domain.Calculator;
+
 public class Utils {
+
+    public static void addCustomDelimiters(Calculator list, String s) {
+        if (s.startsWith("//")) {
+            char[] ss = extractDelimitersFromHeader(s);
+            for (char c : ss) {
+                list.delimiters.add(c);
+            }
+        }
+    }
+
+    private static char[] extractDelimitersFromHeader(String s) {
+        String ss = s.substring(2);
+        String[] split = ss.split("\\\\n");
+        return split[0].toCharArray();
+    }
 }
