5943249d4428bfe98b029805cc6dccede3448587
고경우 <dfdfg11@gmail.com>
2024-10-19 12:42:56 +0900

    feat: 빈 문자열 또는 null 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,14 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+
+        // 빈 문자열 또는 null 처리
+        if (input == null || input.trim().isEmpty()) {
+            System.out.println("결과 : 0");
+            return;
+        }
+
 
     }
 }
