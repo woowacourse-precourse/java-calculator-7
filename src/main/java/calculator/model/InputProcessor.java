566de244b7b8ed965baa7e6838499cb527efe61b
kimsehw <seunghwan9703@gmail.com>
2024-10-20 07:04:31 +0900

    feat(InputProcessor): 인풋의 구분자 case 구별 메서드 추가

--- src/main/java/calculator/model/InputProcessor.java
+++ src/main/java/calculator/model/InputProcessor.java
@@ -1,4 +1,20 @@
 package calculator.model;
 
 public class InputProcessor {
+
+    private String input;
+    //public static final D
+
+    public InputProcessor(String input) {
+        this.input = input;
+    }
+
+    public boolean isDefaultCase() {
+        return Character.isDigit(input.charAt(0));
+    }
+
+    public boolean isCustomCase() {
+        return input.startsWith("//");
+    }
+
 }
