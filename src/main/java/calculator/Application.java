5ff2515a0098dc2cf0e28105988d93cd664475bf
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-10-19 23:33:37 +0900

    feat(Application): 입력 안내문 출력 및 결과 반환 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,8 +3,9 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해주세요.");
         String expression = InputHandler.getInput();
         Integer calculated = Calculator.calculate(expression);
-        System.out.println(calculated);
+        System.out.println("결과 : " + calculated);
     }
 }
