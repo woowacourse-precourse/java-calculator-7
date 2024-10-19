4e2cc72fc7dfaffec814cfc4d8a91bf19d95ea0a
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-10-19 23:30:14 +0900

    feat(CalculatorController): checkCustomDelimiter 생성
    커스텀 구분자가 제대로 있는지 확인합니다.
    있다면 커스텀 구분자를 구분자 리스트에 추가합니다.
    커스텀 구분자 구문을 뺀 나머지 문자열을 반환합니다.

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class CalculatorController {
     private static final CalculatorView calculatorView = new CalculatorView();
     private static List<Integer> nums = new ArrayList<>();
-    private static List<Character> delimiters = new ArrayList<>(List.of(',', ':'));
+    private static final List<Character> delimiters = new ArrayList<>(List.of(',', ':'));
 
     public static void run() {
         startCalculator();
@@ -27,6 +27,19 @@ public class CalculatorController {
                 delimiters.stream().anyMatch(d -> inputString.endsWith(String.valueOf(d)))) {
             throw new IllegalArgumentException("문자열을 잘못 입력하였습니다.");
         }
+    }
+
+    private static String checkCustomDelimiter(String inputString) {
+        String prefix = inputString.substring(0, 2);
+        String suffix = inputString.substring(3, 5);
+        int len = inputString.length();
+
+        if (prefix.equals("//") && suffix.equals("\\n")) {
+            delimiters.add(inputString.charAt(2));
+            return inputString.substring(5);
+        }
+
+        throw new IllegalArgumentException("커스텀 문자열을 잘못 입력하였습니다.");
     }
 
 }
