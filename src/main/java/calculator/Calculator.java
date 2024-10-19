7e2e50205c7457190a353a01b20149ff79a8cd8a
ziyoungkwon <gy011003@naver.com>
2024-10-20 01:34:51 +0900

    refactor: customCalculate 함수 삭제
    customCalculate 함수를 삭제하고 calculate 함수에 커스텀 구분자인 경우 구분자 수정하는 것으로 바꾸었다. 성능 개선을 위한 리팩토링이기도 하다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,15 +6,17 @@ public class Calculator {
         int sum = 0;
 
         if (input == null || input.isEmpty()) {
-            return sum;
+            return 0;
         }
 
-        if (input.startsWith("/")){
-            sum = customCalculate(input);
-            return sum;
-        }
+        String delimiter = "[,:]";
 
-        String[] tokens = input.split("[,:]"); //쉼표나 콜론을 구분자로 분리
+        if(input.startsWith("//")){
+
+            delimiter = String.valueOf(input.charAt(2));
+            input = input.substring(5);
+        }
+        String[] tokens = input.split(delimiter); //쉼표나 콜론을 구분자로 분리
 
         for (String token : tokens) {
 
@@ -25,27 +27,6 @@ public class Calculator {
             sum += number;
         }
 
-        return sum;
-    }
-
-    private int customCalculate(String input) {
-
-        String delimiter = String.valueOf(input.charAt(2));
-        String realInput = input.substring(5);
-
-        String[] tokens = realInput.split(delimiter);
-
-        int sum = 0;
-        int number = 0;
-
-        for (String token : tokens) {
-
-            number = Integer.parseInt(token.trim());
-            if(number < 0) {
-                throw new IllegalArgumentException("음수는 허용되지 않습니다: " + number);
-            }
-            sum += number;
-        }
         return sum;
     }
 }
