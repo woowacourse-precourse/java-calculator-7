dfda7876fc60179db545b4b5531f1cd8e03508a8
Seongmo Ahn <djgood780@gmail.com>
2024-10-19 22:03:50 +0900

    feat(Output): 덧셈 결과 출력

--- src/main/java/calculator/Output.java
+++ src/main/java/calculator/Output.java
@@ -6,4 +6,8 @@ public class Output {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
+    public static void printResult(int result){
+        System.out.print("결과 : " + result);
+    }
+
 }
