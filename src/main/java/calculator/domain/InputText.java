d0ccbcdcfeac6e767a29d1a5b50146130951a3bc
USER <cji513@naver.com>
2024-10-19 14:34:57 +0900

    feat(InputText): divideTextAndSeparateList 구현
    - 커스텀 구분자가 어떠한 형태로 넘어오더라도 인식할 수 있도록 구현

--- src/main/java/calculator/domain/InputText.java
+++ src/main/java/calculator/domain/InputText.java
@@ -5,10 +5,29 @@ import java.util.List;
 
 public class InputText {
 
-    private String input;
-    private List<String> separatorList = new ArrayList<>(List.of(",",":"));
+    private final String text;
+    private final List<String> separatorList = new ArrayList<>(List.of(",",":"));
 
     public InputText (String input){
-        this.input = input.trim();
+        text = divideTextAndSeparateList(input.trim());
     }
+
+    private String divideTextAndSeparateList(String input){
+        if (input.startsWith("//")){
+            int lastIndex = input.lastIndexOf("\\n");
+            String separator = input.substring(2,lastIndex);
+            separatorList.add(separator);
+            return input.substring(lastIndex+2);
+        }
+        return input;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public List<String> getSeparatorList() {
+        return separatorList;
+    }
+
 }
