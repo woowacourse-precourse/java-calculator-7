f00e79d18272a6970688ea08fd45980d1a5d1ebe
choign3123 <choign3123@naver.com>
2024-10-19 15:44:44 +0900

    Refactor(CalculatorDisplay): ICalculatorDisplay 인터페이스를 구현하도록 수정
    기존 메소드인 show()와 getPromptUserInput()는 삭제 예정

--- src/main/java/calculator/CalculatorDisplay.java
+++ src/main/java/calculator/CalculatorDisplay.java
@@ -3,11 +3,11 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 
-public class CalculatorDisplay {
+public class CalculatorDisplay implements ICalculatorDisplay {
 
     private static final String inputMessage = "덧셈할 문자열을 입력해 주세요.";
     private static final String outputMessage = "결과 : %d";
-    
+
     public void show() {
         // 사용자 입력 받기
         System.out.println(inputMessage);
@@ -26,5 +26,20 @@ public class CalculatorDisplay {
         Console.close();
 
         return input;
+    }
+
+    @Override
+    public String getPromptInput() {
+        System.out.println(inputMessage);
+
+        String input = Console.readLine();
+        Console.close();
+
+        return input;
+    }
+
+    @Override
+    public void printResult(int result) {
+        System.out.printf(outputMessage, result);
     }
 }
