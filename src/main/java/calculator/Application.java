3de4122d000dbcf63eefa441a7b8eeac72ea0d8f
minju26 <mjalswn26@gmail.com>
2024-10-19 23:05:09 +0900

    feat(Application): 커스텀 구분자를 포함한 문자열에서 숫자 추출 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,28 +2,55 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
 
     private static final String INPUT_GUIDE_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
-    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private static final String CUSTOM_DELIMITER_INPUT_REGEX = "^//(.*?)\\\\n(.*)";
 
     public static void main(String[] args) {
 
-        String inputString = getInputString();
-        int[] extractedNumbers = extractNumbers(inputString, DEFAULT_DELIMITER);
+        String[] inputStringAndDelimiters = getInputString();
+        String delim = inputStringAndDelimiters[0];
+        String inputString = inputStringAndDelimiters[1];
+
+        int[] extractedNumbers = extractNumbers(delim, inputString);
         System.out.println(Arrays.toString(extractedNumbers));
     }
 
-    private static String getInputString() {
+    private static String[] getInputString() {
+
+        String[] inputStringAndDelimiters = new String[2];
 
         System.out.println(INPUT_GUIDE_MESSAGE);
-        return Console.readLine();
+        String userInput = Console.readLine();
+
+        Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_INPUT_REGEX);
+        Matcher matcher = pattern.matcher(userInput);
+
+        if (matcher.matches()) {
+            inputStringAndDelimiters[0] = makeDelimiterRegex(matcher.group(1));
+            inputStringAndDelimiters[1] = matcher.group(2);
+
+            return inputStringAndDelimiters;
+        }
+
+        inputStringAndDelimiters[0] = DEFAULT_DELIMITER_REGEX;
+        inputStringAndDelimiters[1] = userInput;
+
+        return inputStringAndDelimiters;
     }
 
-    private static int[] extractNumbers(String inputString, String delim) {
+    private static int[] extractNumbers(String delim, String inputString) {
 
         String[] extractedStringNumbers = inputString.split(delim);
         return Arrays.stream(extractedStringNumbers).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static String makeDelimiterRegex(String DelimString) {
+        return "[" + DelimString + "]";
     }
 }
