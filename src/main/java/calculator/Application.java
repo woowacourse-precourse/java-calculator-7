a0e7706fbaa32f39786f47d867c16dbee4ba6ffe
imjunghun <dlawndgns9@naver.com>
2024-10-19 18:58:25 +0900

    fix: 설계 변경으로 인한 코드 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,13 +2,14 @@ package calculator;
 
 import calculator.controller.Calculator;
 import calculator.service.calculator.CalculatorService;
+import calculator.service.separator.SeparatorHandler;
 import calculator.service.separator.SeparatorService;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Calculator calculator = new Calculator(
-                new SeparatorService(),
+                new SeparatorHandler(new SeparatorService()),
                 new CalculatorService()
         );
 
--- src/main/java/calculator/controller/Calculator.java
+++ src/main/java/calculator/controller/Calculator.java
@@ -2,24 +2,24 @@ package calculator.controller;
 
 import calculator.domain.Operator;
 import calculator.domain.number.Number;
+import calculator.domain.number.Numbers;
 import calculator.service.calculator.CalculatorService;
-import calculator.service.separator.SeparatorService;
+import calculator.service.separator.SeparatorHandler;
 
 public class Calculator {
 
-    SeparatorService separatorService;
+    SeparatorHandler separatorHandler;
     CalculatorService calculatorService;
 
-    public Calculator(SeparatorService separatorService,
+    public Calculator(SeparatorHandler separatorHandler,
                       CalculatorService calculatorService) {
-        this.separatorService = separatorService;
+        this.separatorHandler = separatorHandler;
         this.calculatorService = calculatorService;
     }
 
     public void run() {
-        Number result = calculatorService.operate(
-                Operator.PLUS, separatorService.separate(UIController.receive())
-        );
+        Numbers numbers = separatorHandler.process(UIController.receive());
+        Number result = calculatorService.operate(Operator.PLUS, numbers);
 
         UIController.printSumResult(result.getValue());
     }
