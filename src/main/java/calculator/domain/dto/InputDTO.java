c4d34dec4444bee6426e8b45577004baf28a46d5
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2024-10-20 04:41:46 +0900

    feat : InputDTO.java 생성
    InputDTO.java 를 생성함.

---
+++ src/main/java/calculator/domain/dto/InputDTO.java
@@ -0,0 +1,18 @@
+package calculator.domain.dto;
+
+public class InputDTO {
+    private  String input;
+
+    public InputDTO(String input) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+}
