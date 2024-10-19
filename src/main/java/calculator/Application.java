05c25cc1794b39d7bdb557cf3cb4e09328b6c023
jungsu <vwx1@naver.com>
2024-10-19 16:28:47 +0900

    feat(Application) : add getInput function

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
 
@@ -11,7 +13,8 @@ public class Application {
     }
 
     private static String getInput() {
-        return "";
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
     }
 
     private static int processInput(String input) {
