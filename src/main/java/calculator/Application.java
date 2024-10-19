dd49183717c94a63109de671e3594a18ff8ec7b0
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-20 02:16:42 +0900

    feat(Application): 의존성 주입과 프로그램 진입점 구현
    진입점인 main 함수에서 InputReader 객체 생성
    - InputReader 객체를 StringProcessorController에 주입
    - 의존성 주입을 통해 StringProcessorController가 InputReader와 느슨하게 연결시킴
    진입점인 main 함수에서 StringProcessorController 객체 생성
    - main 함수에서 프로그램 실행을 위한 진입점을 제공

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,18 @@
 package calculator;
 import calculator.Controller.StringProcessorController;
+import calculator.Service.InputReader;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        StringProcessorController stringProcessorController = new StringProcessorController();
+        // InputReader 객체 생성
+        InputReader inputReader = new InputReader();
+
+        // StringProcessorController에 의존성 주입
+        StringProcessorController stringProcessorController = new StringProcessorController(inputReader);
+
+        // 프로세스 시작
+        stringProcessorController.process();
     }
 }
