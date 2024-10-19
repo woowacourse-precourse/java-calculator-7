deb47e6910539edd3151e946a85560e40739a49e
kkonii <119216472+kkonii@users.noreply.github.com>
2024-10-20 03:50:52 +0900

    feat : 커스텀 구분자가 숫자 타입인지 검증 구현

--- src/main/java/calculator/domain/CustomDelimiter.java
+++ src/main/java/calculator/domain/CustomDelimiter.java
@@ -3,17 +3,25 @@ package calculator.domain;
 public class CustomDelimiter {
 
     private static final int FIX_SIZE = 1;
+    private static final String NUMBER_TYPE = "\\d";
 
     private final String delimiter;
 
     public CustomDelimiter(String delimiter) {
         validateLength(delimiter);
+        validateType(delimiter);
         this.delimiter = delimiter;
     }
 
     private void validateLength(String delimiter) {
         if (delimiter.length() != FIX_SIZE) {
             throw new IllegalArgumentException("커스텀 구분자는 " + FIX_SIZE + "글자만 입력해야 합니다.");
+        }
+    }
+
+    private void validateType(String delimiter) {
+        if (delimiter.matches(NUMBER_TYPE)) {
+            throw new IllegalArgumentException("커스텀 구분자에 숫자는 입력할 수 없습니다.");
         }
     }
 }
