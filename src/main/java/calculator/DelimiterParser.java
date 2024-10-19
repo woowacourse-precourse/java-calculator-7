4241f196bd43ee03a8aa67bed81c4fda0ec6b5d4
Jeong yong hwan <baechoo0905@naver.com>
2024-10-19 20:48:00 +0900

    feat: DelimiterParser 클래스 추가하여 구분자 추출 기능 구현
    - 입력 문자열에서 기본 및 커스텀 구분자를 추출하는 DelimiterParser 클래스를 추가하였습니다.
    - getDelimiter 메서드를 통해 입력에 따른 적절한 구분자를 반환합니다.
    - extractCustomDelimiter 메서드는 커스텀 구분자를 추출하며, 잘못된 형식의 입력에 대한 예외 처리를 포함합니다.

---
+++ src/main/java/calculator/DelimiterParser.java
@@ -0,0 +1,27 @@
+package calculator;
+
+public class DelimiterParser {
+
+    private static final String DEFAULT_DELIMITER = ",|:";
+
+    public String getDelimiter(String input) {
+        if (input.startsWith("//")) {
+            return extractCustomDelimiter(input);
+        }
+        return DEFAULT_DELIMITER;
+    }
+
+
+    private String extractCustomDelimiter(String input) {
+
+        // 커스텀 구분자 처리
+        int delimiterEnd = input.indexOf("\\n");
+        if (delimiterEnd != -1) {
+            // 커스텀 구분자 추출
+            return input.substring(2, delimiterEnd);
+        } else {
+            // '\n'이 없을 경우 예외 처리
+            throw new IllegalArgumentException("잘못된 형식의 입력입니다. 커스텀 구분자는 //와 \\n 사이에 위치해야 합니다.");
+        }
+    }
+}
