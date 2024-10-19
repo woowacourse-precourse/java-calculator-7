bcb19308c380e3f6edaa193fe890746730f0e489
ppparkta <pppppark2@gmail.com>
2024-10-19 22:33:35 +0900

    refactor(CalculatorModel): 덧셈 연산 model로 분리

--- src/main/java/calculator/model/CalculatorModel.java
+++ src/main/java/calculator/model/CalculatorModel.java
@@ -1,5 +1,21 @@
 package calculator.model;
 
-public class CalculatorModel {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CalculatorModel {
+    private List<Integer> operand = new ArrayList<>();
+
+    public CalculatorModel(List<Integer> operands) {
+        this.operand = operands;
+    }
+
+    public int calculateSum() {
+        int sum = 0;
+
+        for (int i = 0; i < operand.size(); i++) {
+            sum += operand.get(i);
+        }
+        return sum;
+    }
 }
