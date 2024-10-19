cebcba36fad724b9d21fae8023ad57e83b020924
moon-akdong <problemy0913@gmail.com>
2024-10-20 05:30:45 +0900

    checkSpecialDelimiter 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        //testg
+        String strs = "//: \n1;2;3,4,5:6";
+        String strs1 = "1,2,3,4:5";
+        //ArrayList‹String> delimiters = getDelimiter(strs);
+        for (String item : delimiters) {
+            System.out.printun("구분자 =" + item);
+        }
+    }
+        public static String checkSpecialDelimiter (String inputString) {
+            // 정규표현식
+            // 특수 구분자가 하나의 char이 아닐수 있음
+            // //n으로 주는지 /n으로 주어지는지 모르겠다.
+            String regex = "^//.*\\n";
+            Pattern pattern =Pattern.compile(regex);
+            Matcher matcher1 = pattern.matcher (inputString);
+            if (matcher1.find()) {
+                String delimiter = matcher1.group();
+                return delimiter;
+            } else {
+                return null;
+            }
     }
 }
