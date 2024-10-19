913f3a832e3deb21d2b03759f6d30e807dd15c36
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-20 06:18:27 +0900

    feat: 패턴 매칭 시 특수 문자 이스케이핑 함수 추가
    - 정규식 패턴 매칭 시 특수 문자를 이스케이프하여 정상적으로 동작하도록 개선.
    - 특수 문자가 이스케이프되지 않아 발생하는 매칭 오류 문제 해결.

--- src/main/java/calculator/util/pattern/PatternUtils.java
+++ src/main/java/calculator/util/pattern/PatternUtils.java
@@ -7,6 +7,9 @@ public class PatternUtils {
 
     public final static Pattern NUMBER_PATTERNS = Pattern.compile("[0-9]");
     public final static String SPLIT_DELIMITER = "|";
+    private final static List<String> SPECIAL_CHARACTERS = List.of(
+            ".", "*", "+", "?", "^", "$", "[", "]", "{", "}", "(", ")", "|"
+    );
 
     public static String join(String delimiter, List<String> values) {
         return String.join(delimiter, values);
@@ -15,5 +18,12 @@ public class PatternUtils {
     public static boolean matchesWithDelimitersIgnoringNumbers(String value, Pattern delimiterPattern) {
         String combinedPattern = String.format("[%s]", NUMBER_PATTERNS + delimiterPattern.pattern());
         return value.matches(combinedPattern + "*");
+    }
+
+    public static String escapeSpecialCharacters(String value) {
+        for (String specialCharacter : SPECIAL_CHARACTERS) {
+            value = value.replace(specialCharacter, "\\" + specialCharacter);
+        }
+        return value;
     }
 }
