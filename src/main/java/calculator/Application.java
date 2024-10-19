8af0eb0d4de02be9c8f8eddd1358268abe7be027
LeeJU <thirdspirit@naver.com>
2024-10-19 21:40:14 +0900

    feat(application): implementation features
    Implement split by ':,'

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,8 +7,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        ArrayList<String> numbers = new ArrayList<>();
-
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
@@ -22,12 +20,14 @@ public class Application {
                 if (input.startsWith("//", i)) {
                     isCustom = true;
                     tempCustomString = new StringBuilder("//");
+                    i++;
                 }
             } else {
                 if (input.startsWith("\\n", i)) {
                     isCustom = false;
                     tempCustomString.append(input, i, i + 2);
                     rest = rest.replace(tempCustomString, ":");
+                    i++;
                 } else {
                     tempCustomString.append(input.charAt(i));
                 }
@@ -36,6 +36,19 @@ public class Application {
 
         if (isCustom) {
             throw new IllegalArgumentException("커스텀 구분자가 닫혀 있지 않습니다.");
+        }
+
+        String[] numbers = rest.split("[:,]");
+
+        for (String number : numbers) {
+            if (number.length() == 1 && number.charAt(0) == '0') {
+                throw new IllegalArgumentException("0값은 올 수 없습니다.");
+            }
+            for (int i = 0; i < number.length(); i++) {
+                if (!(number.charAt(i) >= '0' && number.charAt(i) <= '9')) {
+                    throw new IllegalArgumentException("양수가 아니거나, 올바른 숫자가 아닙니다.");
+                }
+            }
         }
     }
 }
