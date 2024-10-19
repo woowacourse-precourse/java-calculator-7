0e8a5715345bc4d1fc5987ae3689bbfd575eed47
JS <zks145@naver.com>
2024-10-20 03:44:51 +0900

    test : 산정한 예외 상황 테스트 코드 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,11 +17,96 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_구분자_미입력() {
+        assertSimpleTest(() -> {
+            run("//\\n1,2,3");
+            assertThat(output().contains("결과 : 6"));
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_복수() {
+        assertSimpleTest(() -> {
+            run("//[]{}\\n1[7}9");
+            assertThat(output()).contains("결과 : 17");
+        });
+    }
+    @Test
+    void 커스텀_구분자_알파벳() {
+        assertSimpleTest(() -> {
+            run("//AB\\n1A2B3");
+            assertThat(output().contains("결과 : 6"));
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_특수문자1() {
+        assertSimpleTest(() -> {
+            run("//`\\n1`2`3");
+            assertThat(output().contains("결과 : 6"));
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_특수문자2() {
+        assertSimpleTest(() -> {
+            run("//~\\n1~2~3");
+            assertThat(output().contains("결과 : 6"));
+        });
+    }
+
+
+    @Test
+    void 기본입력() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output().contains("결과 : 6"));
+        });
+    }
+
+    @Test
+    void 연속정규식입력() {
+        assertSimpleTest(() -> {
+            run("//;\\\\n1;2;3");
+            assertThat(output().contains("결과 : 6"));
+        });
+    }
+
+    @Test
+    void 예외_테스트1() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 에외_테스트2() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//;\\n1,2,abc"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+    @Test
+    void 에외_테스트3() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//;.\\n1,2;@3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+    @Test
+    void 에외_테스트4() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//;\\n1, 2, 3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+    @Test
+    void 에외_테스트5() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//;\\n1;2;3,|"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
     }
 
     @Override
