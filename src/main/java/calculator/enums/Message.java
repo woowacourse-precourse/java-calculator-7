61223fba6e81348998fd5d1d339c817e742a5b80
codrin2 <hssang127@gmail.com>
2024-10-19 18:00:58 +0900

    feat(OutputView): 계산기 작동 시작을 보여주는 기능

---
+++ src/main/java/calculator/enums/Message.java
@@ -0,0 +1,6 @@
+package calculator.enums;
+
+public class Message {
+	public static final String START_MESSAGE = "계산기가 시작되었습니다.\n사용자는 덧셈할 문자열을 입력해 주세요.";
+
+}
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,10 @@
+package calculator.view;
+
+import calculator.enums.Message;
+
+public class OutputView {
+	public void printStartMessage() {
+		System.out.println(Message.START_MESSAGE);
+	}
+
+}
