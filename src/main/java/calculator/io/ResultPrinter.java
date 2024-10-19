3cdedf3dffb5cb8bd48388b821c98919fa32c994
J0YERIM <chosarah70@gmail.com>
2024-10-20 05:42:34 +0900

    refactor: 결과 출력 시 formatted 메서드 사용하도록 수정

--- src/main/java/calculator/io/ResultPrinter.java
+++ src/main/java/calculator/io/ResultPrinter.java
@@ -2,9 +2,9 @@ package calculator.io;
 
 public class ResultPrinter {
 
-    private static final String RESULT_MESSAGE_PREFIX = "결과 : ";
+    private static final String RESULT_MESSAGE_FORMAT = "결과 : %d";
 
     public void printResult(int result) {
-        System.out.println(RESULT_MESSAGE_PREFIX + result);
+        System.out.println(RESULT_MESSAGE_FORMAT.formatted(result));
     }
 }
