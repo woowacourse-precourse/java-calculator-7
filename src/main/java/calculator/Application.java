7721ee99d2a4c1b89ffe17443deff0974a8ca190
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2024-10-19 11:54:30 +0900

    feat: 커스텀 구분자 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,11 +6,18 @@ import java.io.InputStreamReader;
 
 public class Application {
 
+    static String[] setDelimiter( String input ) {
+        StringBuffer sb = new StringBuffer( input );
+        String[] delimiterList = { input.split( "\n", 2 )[0].substring(2), ",", ":" };
+        return delimiterList;
+    }
+
     public static void main(String[] args) {
         int result = 0;
         try{
             BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
             String input = bufferedReader.readLine();
+            String[] dellimiterList = setDelimiter( input );
 
             System.out.println( result );
             bufferedReader.close();
