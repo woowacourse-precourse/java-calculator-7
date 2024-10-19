736f3f49f1db996827e30b176ade531d0b3826d7
Yewon Yoo <yooyy0106@naver.com>
2024-10-20 02:21:02 +0900

    feat: 사용자 입력값 받기 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static int stringCalc(String str) {
+        if (str == null || str.isEmpty()) {
+            return 0;
+        }
+
+        String[] numbers = str.split("[,:]");
+
+        int result = 0;
+        for (String number : numbers) {
+            result += Integer.parseInt(number);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열 : ");
+        String str = Console.readLine();
+
+        int answer = stringCalc(str);
+        System.out.println("결과 : " + answer);
     }
 }
