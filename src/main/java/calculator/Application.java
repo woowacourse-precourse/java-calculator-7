99b01291e9a32249b344f3ca2a2861b8b5f1f936
junghun00 <ji8657447@gmail.com>
2024-10-19 19:49:01 +0900

    feat: I/O 처리 객체 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,15 +2,16 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
+        InputOutputManager printManager = new InputOutputManager();
 
-        String input = InputOutputManager.inputMessage();
+        String input = printManager.inputMessage();
+
         String delimiters = CustomDelimiter.parseDelimiters(input);
         String parseString = ParsingString.parsingString(input, delimiters);
 
         Calculator calculator = new Calculator();
-        InputOutputManager.outputMessage(calculator.add(parseString, delimiters));
+        printManager.outputMessage(calculator.add(parseString, delimiters));
     }
 }
 
-// Main 문 리펙토링
 // 이스케이프가 필요한 구분자 추가 구현
--- src/main/java/calculator/InputOutputManager.java
+++ src/main/java/calculator/InputOutputManager.java
@@ -6,12 +6,12 @@ public class InputOutputManager {
     private static final String GUIDE_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
     private static final String RESULT_MESSAGE = "결과 : ";
 
-    public static String inputMessage() {
+    public String inputMessage() {
         System.out.println(GUIDE_MESSAGE);
         return Console.readLine();
     }
 
-    public static void outputMessage(int message) {
+    public void outputMessage(int message) {
         System.out.print(RESULT_MESSAGE);
         System.out.println(message);
     }
