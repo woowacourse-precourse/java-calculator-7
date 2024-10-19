48d279e28eae021a7a7245e19b120562897e17cf
99hyeon <hyeon99@ajou.ac.kr>
2024-10-19 13:31:47 +0900

    feat: 기능2, 2-1, 2-2 커스텀 관련 기능 코드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,6 +12,15 @@ public class Application {
         if(!checkEmpty(inputString)){
             return;
         }
+
+        char separator;
+        if(inputString.charAt(0) == '/'){
+            separator = checkCustom(inputString);
+
+            if(separator == '0'){
+                throw new IllegalArgumentException("IllegalArgumentException 발생");
+            }
+        }
     }
 
     static boolean checkEmpty(String string){
@@ -27,7 +36,13 @@ public class Application {
         return true;
     }
 
-    static void checkCustom(String string){ // 구분자 표시 : //(구분자)\n
+    static char checkCustom(String string){ // 구분자 표시 : //(구분자)\n
 
+        if(string.charAt(1) == '/' && string.charAt(3) == '\\' && string.charAt(4) == 'n'){
+            if((string.charAt(2) >= 33 && string.charAt(2) <= 47) || (string.charAt(2) >= 58 && string.charAt(2) <= 64) || (string.charAt(2) >= 91 && string.charAt(2) <= 95) || (string.charAt(2) >= 123 && string.charAt(2) <= 126))
+                return string.charAt(2);
+        }
+
+        return '0';
     }
 }
