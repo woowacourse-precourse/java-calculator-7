f5b09bbc81a2eeb1182ae3d575ad56d37be60d9a
[supernovaMK] <[minki5336@naver.com]>
2024-10-20 01:42:34 +0900

    test(CalculateTest): 파싱된 숫자를 더하는 기능 테스트 코드 작성
    - 간단한 숫자의 덧셈을 하는 테스트 작성
    - 오버플로우가 났을때의 상황 테스트 작성

---
+++ src/test/java/calculator/CalculateTest.java
@@ -0,0 +1,30 @@
+package calculator;
+
+import calculator.model.Calculate;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculateTest {
+
+    @Test
+    void sum_간단한_숫자_덧셈() {
+        //given
+        List<Long> numberList = List.of(1L, 2L, 3L);
+        //when
+        Calculate calculate = new Calculate(numberList);
+        long result = calculate.sum();
+        //then
+        Assertions.assertEquals(result, 6);
+    }
+
+    @Test
+    void sum_오버플로우_수_연산_예외처리() {
+        //given
+        List<Long> numberList = List.of(Long.MAX_VALUE, 1L);
+        //when
+        Calculate calculate = new Calculate(numberList);
+        //then
+        Assertions.assertThrows(IllegalArgumentException.class, calculate::sum);
+    }
+}
