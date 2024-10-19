6653b80096fef2813fbd9862f79c0a4d32541974
Layla Oh <crispylemon7120@gmail.com>
2024-10-19 20:58:21 +0900

    feat(calculator): 덧셈을 하는 기능 추가
    각각의 값이 양수인지 확인하고, 만약 다른 값이 들어 왔을 경우 에러 처리한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,6 @@
 package calculator;
 import java.lang.System;
-import java.util.Arrays;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Application {
@@ -10,9 +10,13 @@ public class Application {
         // 구분자로 나눠진 문자 배열
         String[] splited_by_seperators;
 
+        // 사용자 input 받는 부분
+        Scanner scan_input  = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
         // 1. 구분자를 인식하는 기능 - 커스텀 구분자가 있다면 구분자 목록에 추가
         // 2. 구분자 목록을 기반으로 입력된 문자열을 나누는 기능
-        String input_string = args[0];
+        String input_string = scan_input.nextLine();
         String[] splited_string = input_string.split("\\\\n");
         // 커스텀 구분자가 있는 경우
         if (splited_string.length > 1 && splited_string[0].startsWith("//")){
@@ -28,8 +32,21 @@ public class Application {
             // '\n'은 등장했으나 '//' 은 등장하지 않은 상황으로 에러처리
             throw new IllegalArgumentException("값이 잘못되었습니다");
         }
-//        System.out.println(Arrays.toString(splited_string));
-//        System.out.println(Arrays.toString(custom_seperators));
-        System.out.println(Arrays.toString(splited_by_seperators));
+
+        // 3. 덧셈을 하는 기능
+        int sum = 0;
+        if (splited_by_seperators.length == 1 && splited_by_seperators[0].equals("")){
+            System.out.println("결과 : " + sum);
+        } else {
+            for (int i = 0; i < splited_by_seperators.length; i++) {
+                if (Pattern.matches("^[0-9]*$", splited_by_seperators[i])){
+                    sum += Integer.parseInt(splited_by_seperators[i]);
+                } else {
+                    // 양수를 제외한 다른 것이 나왔을 때 에러처리
+                    throw new IllegalArgumentException("덧셈은 양수만 가능합니다");
+                }
+            }
+            System.out.println("결과 : " + sum);
+        }
     }
 }
