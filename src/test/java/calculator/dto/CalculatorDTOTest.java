0dc48f7433c281f0fffa7cb1ddd1572b7df27041
qudcks8084 <qudcks8084@naver.com>
2024-10-19 16:18:45 +0900

    test(DTO) : add DTO constuctor error test
    Detail
    - add 3 case
    	1. when there is empty input
    	2. there is no custom delim between "//" and "\n"
    	3. there are no calculate target like "//*\n"

--- src/test/java/calculator/dto/CalculatorDTOTest.java
+++ src/test/java/calculator/dto/CalculatorDTOTest.java
@@ -31,4 +31,28 @@ class CalculatorDTOTest {
         assertThat(calculatorDTO.hasCustomDelim()).isEqualTo(true);
     }
 
+    @Test
+    @DisplayName("빈 문자열이 입력되는 경우")
+    void constructorTest3(){
+        String str = "";
+        assertThrows(IllegalArgumentException.class,
+                ()-> new CalculatorDTO(str));
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자가 없는 경우")
+    void constructorTest4(){
+        String str = "//\n1:2:3";
+        assertThrows(IllegalArgumentException.class,
+                ()-> new CalculatorDTO(str));
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자는 있지만 계산 대상이 없는 경우")
+    void constructorTest5(){
+        String str = "//?\n";
+        assertThrows(IllegalArgumentException.class,
+                ()-> new CalculatorDTO(str));
+    }
+
 }
