c11491c486659a4122b170581ffc5253eba5d9b4
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-10-20 01:49:14 +0900

    test(ConvertTest): String 배열을 Integer 배열로 변환하는 테스트
    - 성공하는 경우
    - null 입력받았을 때, 0 반환해야 하는 경우
    - 문자가 포함된 경우 예외를 던져야 한다
    - 음수가 포함된 경우 예외를 던져야 한다

--- src/test/java/calculator/ConverterTest.java
+++ src/test/java/calculator/ConverterTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class ConverterTest {
 
     @Test
@@ -28,17 +31,51 @@ public class ConverterTest {
 
         // when
         List<Integer> result = Converter.convert(input);
-        List<Integer> expected = Arrays.asList(0);
+        List<Integer> expected = List.of(0);
 
         // then
         Assertions.assertThat(result).isEqualTo(expected);
     }
 
-    // 문자가 포함된 경우
+    @Test
+    public void 문자가_포함된_경우() {
+        // given
+        String[] input = new String[]{"21,", "5_34", "62", "36", "53"};
+
+        // when
+        // then
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> Converter.convert(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
     // 음수가 포함된 경우
+    @Test
+    public void 음수가_포함된_경우() {
+        // given
+        String[] input = new String[]{"-21","-6", "36", "53"};
+
+        // when
+        // then
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> Converter.convert(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
     // 0만 있는 경우
+    @Test
+    public void _0만_있는_경우() {
+        // given
+        String[] input = new String[]{"0"};
 
+        // when
+        List<Integer> result = Converter.convert(input);
+        List<Integer> expected = List.of(0);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
 
 }
