2cbb187c0dd3aa515db9b63df5a0fef385ca010f
rjswjddn <rjswjddn@gmail.com>
2024-10-19 17:20:59 +0900

    test(ApplicationTest): 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,9 +19,65 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 커스텀_구분자_여러개_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n//?\\n1;2?3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 예외_구분자_사용() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("////\\n1//2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자_구분자_사용() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//123\\n41235"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 맨_앞_구분자() {
+        assertSimpleTest(() -> {
+            run(",1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 맨_뒤_구분자() {
+        assertSimpleTest(() -> {
+            run("1,2,3,");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 공백_커스텀_구분자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 연속_구분자() {
+        assertSimpleTest(() -> {
+            run("//,,\\n1,,2,,3");
+            assertThat(output()).contains("결과 : 6");
+        });
     }
 
     @Override
