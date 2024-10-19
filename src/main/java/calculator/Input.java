b286b0aedd4c9a82c5a8f10e7597e55ff1188c70
2jin2.1031 <2jin2.1031@gmail.com>
2024-10-19 15:00:33 +0900

    refactor: convertToIntArray 메서드 분리

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -26,16 +26,20 @@ public class Input {
             return new int[0];
         }
 
-        int[] numbers = new int[splitStr.length];
-        try {
-            for (int i = 0; i < splitStr.length; i++) {
-                numbers[i] = Integer.parseInt(splitStr[i]);
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
-        }
-
+        int[] numbers = parseToIntArray(splitStr);
         validPositiveArray(numbers);
+        return numbers;
+    }
+
+    private static int[] parseToIntArray(String[] splitStr) {
+        int[] numbers = new int[splitStr.length];
+        for (int i = 0; i < splitStr.length; i++) {
+            try {
+                numbers[i] = Integer.parseInt(splitStr[i]);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
+            }
+        }
         return numbers;
     }
 
