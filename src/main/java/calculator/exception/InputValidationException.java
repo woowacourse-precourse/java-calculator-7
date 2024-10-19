78412139fe74f6503b8cb7594cf76cad3d71291d
Dev-LHY <dev.leehy@gmail.com>
2024-10-19 17:04:27 +0900

    feat: 음수 입력시 예외처리 기능 구현

--- src/main/java/calculator/exception/InputValidationException.java
+++ src/main/java/calculator/exception/InputValidationException.java
@@ -10,4 +10,10 @@ public class InputValidationException extends IllegalArgumentException{
             throw new IllegalArgumentException("아무것도 입력하지 않았습니다. 애플리케이션을 종료합니다.");
         }
     }
+
+    public static void isPositiveNumber(int value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 애플리케이션을 종료합니다.");
+        }
+    }
 }
