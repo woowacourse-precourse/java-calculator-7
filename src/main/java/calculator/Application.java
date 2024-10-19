038be9838ad9778e705aaa10410143074a19e676
yeong-hwan <jghff700@naver.com>
2024-10-20 05:29:14 +0900

    refactor(view): change to readline in inputview from console injection

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        CalculatorService calculatorService = new CalculatorService();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        CalculatorController controller = new CalculatorController(calculatorService, inputView, outputView);
+        controller.run();
     }
 }
--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -4,14 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private final Console console;
-
-    public InputView(Console console) {
-        this.console = console;
-    }
-
     public String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return console.readLine();
+        return Console.readLine();
     }
 }
