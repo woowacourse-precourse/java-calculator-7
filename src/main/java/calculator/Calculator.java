21ac11ae3fa5814be04be18b783638af2596897e
inha0319@naver.com <inha031913@gmail.com>
2024-10-19 12:48:50 +0900

    [feat] 기본& 커스텀 구분자 파싱 로직

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,11 +2,44 @@ package calculator;
 
 public class Calculator {
 
-    public static int stringSum(String input){
+    public static int stringSum(String input) {
 
-        if(input == null || input.isEmpty()){
+        if (input == null || input.isEmpty()) {
             return 0;
         }
+
+        String delimiters = checkDelimiter(input);
+
         return -1;
     }
+
+    private static String checkDelimiter(String input) {
+        
+        StringBuilder delimiters = new StringBuilder(); 
+        
+        //기본 구분자
+        delimiters.append(",").append(":");
+
+        // 커스텀 구분자
+        if (input.startsWith("//")) {
+            int start = 2;
+            int end = input.indexOf('\n', start);
+
+            while (end != -1) {
+                String customDelimiter = input.substring(start, end);
+                delimiters.append(customDelimiter);
+                start = end + 1;
+
+                if (input.startsWith("//", start)) {
+                    start += 2;
+                    end = input.indexOf('\n', start);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return delimiters.toString();
+    }
+
 }
