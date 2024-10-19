aef53964297d6a8833d0c129e37641a7c31d3ba5
DEV_WOONG <ywcsuwon@gmail.com>
2024-10-19 13:01:43 +0900

    test(ApplicationTest): add test code about exception

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -5,48 +5,61 @@ import java.util.regex.Pattern;
 
 public class CalculatorService {
 
-
     /**
-     * caculate 메소드 입력값의 유효성을 검증하고 구분자를 통해 문자열에서 숫자를 추출한다.
+     * 문자열 입력값을 확인후 합산하여 반환하는 메서드
      *
      * @param input
      * @return
      */
     public int caculate(String input) {
-        if (input == null || input.isEmpty()) { //입력값이 null 혹은 비어있다면
+        if (input == null || input.isEmpty()) { // 입력값이 null 혹은 비어있다면
             return 0;   // 0을 반환한다.
         }
 
-        String delimiter = ",|:"; //기본 구분자
-        //커스텀 구분자
-        if (input.startsWith("//") && input.contains("\n")) {
-            Matcher matcher = Pattern.compile("//(.)\\n(.*)").matcher(input); //와 \n 사이에 있는 구분자로 입력 문자열을 나눈다.
+        String delimiter = ",|:"; // 기본 구분자
+        // 커스텀 구분자
+        if (input.startsWith("//")) {
+            Matcher matcher = Pattern.compile("//(.)\\\\n(.*)").matcher(input); //와 \n 사이에 있는 구분자로 입력 문자열을 나눈다.
             if (matcher.find()) {
                 delimiter = Pattern.quote(matcher.group(1));
                 input = matcher.group(2);
             }
         }
 
-        String[] numbers = input.split(delimiter); //입력값을 구분자로 나누어 String형 배열에 저장
-        return sum(numbers);
-    }
+        String[] numbers = input.split(delimiter); // 입력값을 구분자로 나누어 String형 배열에 저장
+        return calculateSum(numbers); // 합산 메서드 호출
+    }//calculate
 
-
-    private int sum(String[] numbers) {
+    /**
+     * 문자열 배열 안의 숫자를 정수형 변환후 합산하는 메서드
+     *
+     * @param numbers
+     * @return
+     */
+    private int calculateSum(String[] numbers) {
         int total = 0;
         for (String number : numbers) {
-            number = number.trim(); //양쪽 공백제거
-            try {
-                int num = Integer.parseInt(number);
-                if (num < 0) { //숫자가 음수라면
-                    throw new IllegalArgumentException();
-                }
-                total += num;
-            } catch (NumberFormatException nfe) { //숫자가 아닌 문자열을 정수형으로 바꾸면 예외가 발생한다.
+            number = number.trim(); // 양쪽 공백 제거
+            total += validateNumber(number); // 숫자 유효성 검사를 통해 합산
+        }
+        return total; // 최종 합계 반환
+    }//calculateSum
+
+    /**
+     * 문자열 유효성 검사 메서드 success : 정수형 숫자를 return fail : IllegalArgumentException()을 발생한다.
+     *
+     * @param number
+     * @return
+     */
+    private int validateNumber(String number) {
+        try {
+            int num = Integer.parseInt(number);
+            if (num < 0) { // 숫자가 음수라면
                 throw new IllegalArgumentException();
             }
+            return num;
+        } catch (NumberFormatException nfe) { // 숫자가 아닌 문자열을 정수형으로 바꾸면 예외가 발생한다.
+            throw new IllegalArgumentException();
         }
-
-        return total;
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,10 +17,26 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
