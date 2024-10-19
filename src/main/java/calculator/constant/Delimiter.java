57ead14d760d72389bc3e158a365582d27df1474
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-19 23:03:32 +0900

    refactor(Delimiter): 커스텀 구분자 형식 수정

--- src/main/java/calculator/constant/Delimiter.java
+++ src/main/java/calculator/constant/Delimiter.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class Delimiter {
 
-    public static final String CUSTOM_DELIMITER_FORMAT = "^//(.)\\\\n";
+    public static final String CUSTOM_DELIMITER_FORMAT = "^//(.+)\\\\n";
     public static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER_FORMAT);
     public static final String COMMA = ",";
     public static final String COLON = ":";
