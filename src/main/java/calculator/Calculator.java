c30a33933a30c96f4727f32fed8b5111ea5878fe
Bae-JunSeok <qownstjr77@gmail.com>
2024-10-20 04:55:40 +0900

    feat: 기본 구분자를 사용한 문자열 분리 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,9 +3,20 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
+    private static final String DEFAULT_DELIMITERS = ",|:";
 
     public static void run() {
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
+
+        String[] numbers = splitString(input);
+        System.out.println("분리된 숫자: ");
+        for (String number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+    public static String[] splitString(String input) {
+        return input.split(DEFAULT_DELIMITERS);
     }
 }
