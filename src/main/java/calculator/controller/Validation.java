fe639c75604f754c7767a5da265616fe0b86ae51
Lucerna00 <pjs3859811@naver.com>
2024-10-19 17:31:11 +0900

    refactor(Validation.java): declare custom delimiter regex as constant

--- src/main/java/calculator/controller/Validation.java
+++ src/main/java/calculator/controller/Validation.java
@@ -2,8 +2,10 @@ package calculator.controller;
 
 public class Validation {
 
+    private static final String CUSTOM_DELIMITER_REGEX = "^(//.?\\n).*$";
+
     public static boolean hasCustomDelimiter(String input) {
-        if (input.matches("^(//.?\\n).*$")) {
+        if (input.matches(CUSTOM_DELIMITER_REGEX)) {
             return true;
         }
         return false;
