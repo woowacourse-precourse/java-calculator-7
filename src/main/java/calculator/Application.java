8b7b862fcafe2c0d6dfef90ea390ddbd1cacf7dd
yoownny <kelosy00@naver.com>
2024-10-19 13:43:07 +0900

    fix(Application): 빈 문자열 입력 처리 방식 수정
    - 빈 문자열 입력 시 0을 출력하고 프로그램 종료
    
    Closes #6

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,15 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            System.out.println("덧셈할 문자열을 입력해 주세요.");
-            String input = Console.readLine();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
 
-            if (input.isEmpty()) {
-                throw new IllegalArgumentException("입력값이 없습니다. 프로그램을 종료합니다.");
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        if (input.isEmpty()) {
+            System.out.println(0);
+            return;
         }
 
     }
