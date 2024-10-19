0dbfcf92b480744790645d8568841128634225f8
minzzun99 <skg06123@naver.com>
2024-10-19 13:57:34 +0900

    feat(domain): 구분자 검증 기능 추가
    - 커스텀 구분자 존재 여부 확인
    - 커스텀 구분자가 1개 이상의 문자일 경우 예외 발생
    - 커스텀 구분자가 숫자일 때 예외 발생
    - 구분자 추가하는 로직 추가

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -15,7 +15,14 @@ public class Delimiter {
 
     // 구분자를 추가하는 메서드
     public void addDelimiter(char delimiter) {
+        // 커스텀 구분자 입력 시에 숫자가 입력될 경우 예외 발생 코드 추가
         delimiterSet.add(delimiter);
+    }
+
+    // 구분자를 확인하고 구분자와 숫자로 된 문자열을 반환하는 메서드
+    public String[] changeInputLine(String input) {
+        String numberLine = validateFormat(input);
+        return splitToNumbersArray(numberLine);
     }
 
     // 구분자 집합을 사용해서 split 메서드 사용을 위한 regex를 생성하는 메서드
@@ -32,5 +39,34 @@ public class Delimiter {
     // 입력받은 문자열을 split 메서드를 사용해서 구분자를 기준으로 나누는 메서드
     public String[] splitToNumbersArray(String input) {
         return input.split(setRegex());
+    }
+
+    private String validateFormat(String input) {
+        if (input.startsWith("//")) {
+            // 구분자의 입력 format을 검증하고 예외 발생
+            int index = input.indexOf("\\n");
+            if (index == -1) {
+                throw new IllegalArgumentException("커스텀 구분자는 '//'와 '\\n' 사이의 문자로 입력 가능합니다.");
+            }
+
+            // 커스텀 구분자가 1개 이상의 문자라면 예외 발생
+            String customLine = input.substring(2, index);
+            if (customLine.length() != 1) {
+                throw new IllegalArgumentException("커스텀 구분자는 반드시 하나의 문자로 구성되어야 합니다.");
+            }
+
+            // 커스텀 구분자가 숫자라면 예외 발생
+            char customDelimiter = customLine.charAt(0);
+            if (customDelimiter >= '0' && customDelimiter <= '9') {
+                throw new IllegalArgumentException("커스텀 구분자는 반드시 하나의 문자로 구성되어야 합니다.");
+            }
+
+            // 예외가 발생하지 않으면 구분자로 추가
+            addDelimiter(customDelimiter);
+            return input.substring(index + 2);
+        }
+
+        // 커스텀 구분자거 없을 경우
+        return input.substring(2);
     }
 }
