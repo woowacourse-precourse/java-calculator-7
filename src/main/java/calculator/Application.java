41bc1b77d5f3085ceffa4211491a7c9ffc890280
un-known0 <nam0221@naver.com>
2024-10-19 16:17:29 +0900

    feat(getCustomSeparator): 커스텀 구분자 추출 함수 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,8 +2,24 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class Application {
+
+    public static String getCustomSeparator(String input){
+        if(!input.startsWith("//")){
+            return "[,:]";
+        }
+        if(!input.contains("\\n") || input.indexOf("\\n")==2){
+            throw new IllegalArgumentException();
+        }
+        return "([,:]|" + Pattern.quote(input.substring(2,input.indexOf("\\n"))) + ")";
+    }
+
     public static void main(String[] args) {
         String input = Console.readLine();
+        String separator = getCustomSeparator(input);
+        
     }
 }
