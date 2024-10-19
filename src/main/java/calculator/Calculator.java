89432f270287e0fd26dbcd8f790306f7a55dff83
JooYoung <beachu23@naver.com>
2024-10-19 17:20:25 +0900

    chore(Calculator): Consolse close 처리 로직 추가
    의도하지 않은 입출력으로 인한 런타임 에러를 방지하기 위해 사용자 입력 이후 Console.close를 호출한다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -36,6 +36,7 @@ public class Calculator {
     private String readFromConsole() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        Console.close();
         if (input == null) {
             input = "";
         }
