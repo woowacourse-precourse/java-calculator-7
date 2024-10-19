a54fdc43ca8cb8e8976ef63f563cc485311fe14c
mipangg <morganjeong119@gmail.com>
2024-10-19 14:57:12 +0900

    <feat>(Calculator): implement input handling for user strings

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,17 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+    Scanner scanner;
+    String input;
+
+    Calculator() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void run() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        input = scanner.next();
+    }
 }
