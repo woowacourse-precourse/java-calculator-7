2cdc2a01385f7d20e9b3fb71ca7e1b42b93be3d5
SongJaeHoonn <thdwogns121@naver.com>
2024-10-19 23:30:02 +0900

    refactor: Calculation 싱글톤 패턴으로 변경

--- src/main/java/calculator/calculation/Calculation.java
+++ src/main/java/calculator/calculation/Calculation.java
@@ -4,8 +4,7 @@ public class Calculation {
 
     private static final Calculation INSTANCE = new Calculation();
 
-    private Calculation() {
-    }
+    private Calculation() {}
 
     public static Calculation getInstance() {
         return INSTANCE;
