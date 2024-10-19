a376896afb1903e99714054728f3d7883db77ad7
hwan2-99 <thh1233123@gmail.com>
2024-10-19 11:16:18 +0900

    refactor: 계산기 클래스내에서 문자열 분리하는 로직을 새로운 클래스에 할당

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,14 @@
 package calculator;
 
+import calculator.domain.Splitter;
 import calculator.domain.StringAddCalculator;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        final StringAddCalculator stringAddCalculator = new StringAddCalculator();
+        final Splitter splitter = new Splitter();
+        final StringAddCalculator stringAddCalculator = new StringAddCalculator(splitter);
         OutputView.printCalculationResult(stringAddCalculator.splitAndSum(InputView.inputString()));
     }
 }
--- src/main/java/calculator/domain/StringAddCalculator.java
+++ src/main/java/calculator/domain/StringAddCalculator.java
@@ -1,66 +1,37 @@
 package calculator.domain;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class StringAddCalculator {
-    private static final String DELIMITER = ",|:";
-    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\\\\n(.*)");
-    private static final String NUMBER_FORMAT_ERROR = "숫자값만 입력가능합니다.";
     private static final String MIN_VALUE_ERROR = "양수만 입력가능합니다.";
     private static final int DEFAULT_VALUE = 0;
-    private static final int CUSTOM_DELIMITER_INDEX = 1;
-    private static final int TARGET_STRING_INDEX = 2;
+
+    private final Splitter splitter;
 
     public StringAddCalculator() {
+        this(new Splitter());
+    }
+
+    public StringAddCalculator(Splitter splitter) {
+        this.splitter = splitter;
     }
 
     public int splitAndSum(final String inputValue) {
         if (isBlank(inputValue)) {
             return DEFAULT_VALUE;
         }
-        return sum(inputValue);
+        return sum(splitter.convertToInt(inputValue));
     }
 
     private static boolean isBlank(final String inputValue) {
         return inputValue == null || inputValue.isEmpty();
     }
 
-    private String[] splitStringByRegx(final String inputValue) {
-        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(inputValue);
-        if (matcher.find()) {
-            String customDelimiter = matcher.group(CUSTOM_DELIMITER_INDEX);
-            return matcher.group(TARGET_STRING_INDEX).split(customDelimiter);
-        }
-        return inputValue.split(DELIMITER);
-    }
-
-    private int sum(final String inputValue) {
-        int[] values = convertToInt(inputValue);
+    private int sum(final int[] values) {
         int sum = 0;
         for (int value : values) {
             validateMinValue(value);
             sum += value;
         }
         return sum;
-    }
-
-    private int[] convertToInt(final String inputValue) {
-        String[] values = splitStringByRegx(inputValue);
-        int[] result = new int[values.length];
-        for (int i = 0; i < values.length; i++) {
-            validateNumberValue(values[i]);
-            result[i] = Integer.parseInt(values[i]);
-        }
-        return result;
-    }
-
-    private void validateNumberValue(final String value) {
-        try {
-            Integer.parseInt(value);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR);
-        }
     }
 
     private void validateMinValue(final int value) {
