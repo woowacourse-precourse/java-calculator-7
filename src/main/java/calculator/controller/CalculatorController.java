7749cde3cf1fe375ada723ac477d072d854a8794
Jewan1120 <hawaii1468@gmail.com>
2024-10-19 19:21:20 +0900

    refactor(calculator): 커스텀 구분자를 기존 구분자에 더해주는 로직을 컨트롤러에서 분리
    검출된 커스텀 구분자를 기존 구분자에 더해주는 로직을 컨트롤러에서 서비스 계층에서 옮겼습니다.

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -3,13 +3,13 @@ package calculator.controller;
 import calculator.service.CalculatorService;
 import calculator.view.InputView;
 import calculator.view.OutputView;
+import java.util.List;
 
 public class CalculatorController {
 
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
     private final CalculatorService calculatorService = new CalculatorService();
-    private final int CUSTOM_DELIMITER_INDEX = 2;
     private String delimiter = ",:";
 
     public void start() {
@@ -26,15 +26,11 @@ public class CalculatorController {
     public void proceed() {
         String inputString = inputView.promptString();
         if (calculatorService.haveCustomDelimiter(inputString)) {
-            addCustomDelimiter(inputString);
+            delimiter = calculatorService.addCustomDelimiter(delimiter, inputString);
         }
     }
 
     public void end() {
         outputView.printEnd();
-    }
-
-    private void addCustomDelimiter(String inputString) {
-        delimiter += inputString.charAt(CUSTOM_DELIMITER_INDEX);
     }
 }
--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,12 +1,21 @@
 package calculator.service;
 
 import calculator.validator.CalculatorValidator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class CalculatorService {
 
     private final CalculatorValidator calculatorValidator = new CalculatorValidator();
+    private final int CUSTOM_DELIMITER_INDEX = 2;
 
     public boolean haveCustomDelimiter(String inputString) {
         return calculatorValidator.validateCustomDelimiterInput(inputString);
+    }
+
+    public String addCustomDelimiter(String delimiter, String inputString) {
+        delimiter += inputString.charAt(CUSTOM_DELIMITER_INDEX);
+        return delimiter;
     }
 }
