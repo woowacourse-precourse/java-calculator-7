69f2f7dcf4530545081164870bf76be29edbe0ac
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-20 04:03:20 +0900

    feat(StringProcessorController): 커스텀 구분자를 기준으로 분리하는 로직 호출부 추가
    - 해당 로직이 존재하는 CustomDelimiterExtractor의 메서드를 호출하기 위해서 의존성 주입
    - 기본 구분자를 기준으로 분리된 DTO를 넘겨준 뒤, 커스텀 구분자 관련 로직 구현 예정

--- src/main/java/calculator/Controller/StringProcessorController.java
+++ src/main/java/calculator/Controller/StringProcessorController.java
@@ -1,6 +1,7 @@
 package calculator.Controller;
 
 import calculator.DTO.StringCalculatorDTO;
+import calculator.Service.CustomDelimiterExtractor;
 import calculator.Service.DelimiterExtractor;
 import calculator.Service.InputReader;
 import java.util.List;
@@ -9,12 +10,15 @@ public class StringProcessorController {
     // 의존성 주입 이후, 불변성 보장
     private final InputReader inputReader;
     private final DelimiterExtractor delimiterExtractor;
+    private final CustomDelimiterExtractor customDelimiterExtractor;
 
     // 생성자에서 외부로부터의 의존성 주입
-    public StringProcessorController(InputReader inputReader, DelimiterExtractor delimiterExtractor) {
+    public StringProcessorController(InputReader inputReader, DelimiterExtractor delimiterExtractor,
+                                     CustomDelimiterExtractor customDelimiterExtractor) {
 
         this.inputReader = inputReader;
         this.delimiterExtractor = delimiterExtractor;
+        this.customDelimiterExtractor = customDelimiterExtractor;
     }
 
     // 전체 프로세스 조율
@@ -27,7 +31,7 @@ public class StringProcessorController {
         inputDTO.setDetachedInput(afterDefaultExtractor);
 
         // 기본 구분자로 분리된 문자열을 커스텀 구분자를 기준으로 또 다시 분리하는 로직 호출
-        List<String> afterCustomExtractor = CustomDelimiterExtractor.extractDelimiter(inputDTO);
+        List<String> afterCustomExtractor = customDelimiterExtractor.extractDelimiter(inputDTO);
     }
 
 
