e780a72fe7c3edabf1da4853ebcab3b01009c9b2
eunwoo <lkjin0110@gmail.com>
2024-10-19 14:08:28 +0900

    feat<Application>: receive input value
    Add classes and features
    - Printer: print input message to prompt
    - Reader: receive input value

---
+++ src/main/java/calculator/Printer.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class Printer {
+    public static void printInputMessage() {
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+    }
+}
---
+++ src/main/java/calculator/Reader.java
@@ -0,0 +1,10 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Reader {
+    public static String readString() {
+        String input = Console.readLine();
+        return input;
+    }
+}
