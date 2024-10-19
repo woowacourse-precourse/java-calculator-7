0f59bb78abc7daee68a50c39768831d940d2eba0
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-19 16:36:41 +0900

    feat(StringAddCalculatorController): 사용자 입력값을 검증하는 로직 추가

--- src/main/java/calculator/controller/StringAddCalculatorController.java
+++ src/main/java/calculator/controller/StringAddCalculatorController.java
@@ -1,20 +1,35 @@
 package calculator.controller;
 
 import calculator.service.CalculatorService;
+import calculator.validation.InputValidator;
 import calculator.view.CalculatorView;
 import camp.nextstep.edu.missionutils.Console;
 
 public class StringAddCalculatorController {
 
     private final CalculatorService calculatorService;
+    private final InputValidator inputValidator;
 
-    public StringAddCalculatorController(CalculatorService calculatorService) {
+    private static final int INVALID_RESULT = 0;
+
+    public StringAddCalculatorController(CalculatorService calculatorService, InputValidator inputValidator) {
         this.calculatorService = calculatorService;
+        this.inputValidator = inputValidator;
     }
 
     public void playCalculator() {
+        String input = getInput();
+
+        if (inputValidator.isInvalidInput(input)) {
+            CalculatorView.outputView(INVALID_RESULT);
+            return;
+        }
+        CalculatorView.outputView(calculatorService.calculateSum(input));
+    }
+
+    private String getInput() {
         CalculatorView.inputView();
-        CalculatorView.outputView(calculatorService.calculateSum(Console.readLine()));
+        return Console.readLine();
     }
 
 }
