c63b039ed521452e6bd9c02d7e9e10a7ddd7449a
yummygyudon <bang2brew@gmail.com>
2024-10-20 03:30:37 +0900

    feat(`application.util.io.read.InputReader`) : Console Close 추가 및 NoSuchElementException 예외 처리 추가
    - 본 함수의 Console.readLine()을 통해 입력을 받은 후에는 scanner를 종료하는 `close()`메서드를 호출하도록 했습니다.
    - Scanner의 입력 값이 비정상적으로 어떤 한 값도 들어오지 않은 경우(NoSuchElement)까지 처리했습니다.

--- src/main/java/calculator/application/util/io/read/InputReader.java
+++ src/main/java/calculator/application/util/io/read/InputReader.java
@@ -3,15 +3,23 @@ package calculator.application.util.io.read;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.NoSuchElementException;
+
 public final class InputReader {
 
     private InputReader() {}
 
     public static String read() {
-        String input = Console.readLine();
-        if (input == null || input.isEmpty()) {
+        try {
+            String input = Console.readLine();
+            if (input == null || input.trim().isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            Console.close();
+            return input.trim();
+        } catch (NoSuchElementException exception) {
             throw new IllegalArgumentException();
         }
-        return input.trim();
     }
+
 }
