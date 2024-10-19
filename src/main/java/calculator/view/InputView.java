e25be0f160721e807d0eb8f49f3a9b9199f7c388
kimsehw <seunghwan9703@gmail.com>
2024-10-20 06:11:07 +0900

    feat(InputView): 인풋(입력 문자열)을 전달 받는 기능 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,4 +1,14 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String getString() {
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        return input;
+    }
+
 }
