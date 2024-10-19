34ef69f8dae01bf7555539908a9dcdc4a017fced
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2024-10-19 11:40:07 +0900

    feat: I/O 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int result = 0;
+        try{
+            BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
+            String input = bufferedReader.readLine();
+
+            System.out.println( result );
+            bufferedReader.close();
+        } catch (IOException e ){
+            e.printStackTrace();
+        }
     }
 }
