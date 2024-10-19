e96168179fd2f719bffdc6308d7147232d0773bc
yush <yush2202@gmail.com>
2024-10-19 20:48:51 +0900

    refactor($NumberUtils): 숫자 검증 메서드 수정
    유연한 사용을 위해 숫자 검증 메서드를 boolean 으로 변경
    before:
    - public static void isNumeric ...
    
    after:
    - public static boolean isNumeric ...

--- src/main/java/calculator/Utils/NumberUtils.java
+++ src/main/java/calculator/Utils/NumberUtils.java
@@ -1,18 +1,17 @@
 package calculator.Utils;
 
-import static calculator.Constants.ErrorMessages.INVALID_NUMBER_MESSAGE;
-
 public class NumberUtils {
 
     NumberUtils() {
 
     }
 
-    public static void isDouble(String number) {
+    public static boolean isNumeric(String number) {
         try {
             Double.parseDouble(number);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(INVALID_NUMBER_MESSAGE.getErrorMessage());
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 
