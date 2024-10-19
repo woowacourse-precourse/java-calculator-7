b0c2a4a4715e556d2f18d79c7b2718f389add3ff
RawBrain <95132660+RawBrain@users.noreply.github.com>
2024-10-20 00:11:35 +0900

    feat: add get input function

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,31 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+    static List<String> separator = Arrays.asList(",", ":");
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            String userInput = Console.readLine();
+            addSeparator(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
+
+    public static void addSeparator(String userInput) {
+        if (userInput.startsWith("//.*\n")) {
+            String nsep = userInput.substring(2, userInput.indexOf(10));
+            if (nsep.matches(".*[0-9].*")) {
+                throw new IllegalArgumentException("오류: 구분자에 숫자가 포함되어 있습니다.");
+            }
+            separator.add(nsep);
+        }
+    }
+
+
 }
