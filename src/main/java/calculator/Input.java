789536832c03971e67857afb68ec534ab69b9551
hyuuuun <pam4522@gmail.com>
2024-10-19 15:35:02 +0900

    feat : 입력 문자가 숫자가 아닐 경우 예외처리

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -11,14 +11,30 @@ public class Input {
     }
     public String[] inputSplit(){
         tokens = input.split("[,:]");
+        isValidNumber();
         return tokens;
     }
     public int sumToken(){
         int sum = 0;
         for(String token : tokens){
-            sum += Integer.parseInt(token);
+            sum += stringToInt(token);
         }
         return sum;
     }
+    private void isValidNumber(){
+        try {
+            for (String token : tokens) {
+                Integer.parseInt(token);
+            }
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("토큰이 숫자가 아닙니다");
+        }
+
+        //다른 특수문자 사용
+    }
+    private int stringToInt(String str){
+        return Integer.parseInt(str);
+    }
+
 
 }
