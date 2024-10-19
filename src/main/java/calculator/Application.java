8c3ab7f06bbd99440b65f05adb587ee5521627aa
Min <binine2@gmail.com>
2024-10-20 03:16:26 +0900

    feat: 입력 파트 분리 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,11 +2,34 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
+    }
+
+    // 입력을 구분자파트와 문자열파트로 분리하는 메서드
+    public static List<String> inputSeparate(String input){
+        int start = input.indexOf("//");
+        int end = input.indexOf("\\n");
+
+        // 구분자파트가 없는 경우
+        if (start == -1 && end == -1){
+            return Arrays.asList("", input);
+        }
+        // 구분자 파트가 잘 들어온 경우
+        else if (start == 0 && end == 3) {
+            String delPart = String.valueOf(input.charAt(2));
+            String strPart = input.substring(end + 2);
+
+            return Arrays.asList(delPart, strPart);
+        } else {
+            throw new IllegalArgumentException("올바른 입력값을 넣어주세요.");
+        }
     }
 }
