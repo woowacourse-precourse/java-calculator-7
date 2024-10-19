8aef9d3ecf1451d363cfd9bb766add961beeaf27
JaeGyun-Yoo <dbworbs15@gmail.com>
2024-10-20 02:24:46 +0900

    feat(Application): main함수 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        input = input.replace("\\n", "\n");  // 사용자가 입력한 "\\n"을 실제 개행 문자로 변환
+        try {
+            ParsingStrategy strategy = ParsingStrategyFactory.getStrategy(input);
+            int result = strategy.parse(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
