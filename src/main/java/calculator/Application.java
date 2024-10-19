84cdba5713b42aec0716122b53b3e77073cd75ab
tibetyo <zjhj0814@gamil.com>
2024-10-20 00:30:36 +0900

    feat(Application): 계산기 시작 안내 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,6 @@ public class Application {
     private static final String inputPhrase = "덧셈할 문자열을 입력해 주세요.";
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("inputPhrase = " + inputPhrase);
     }
 }
