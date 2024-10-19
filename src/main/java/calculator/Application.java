a9529c50d955ee9a547232b5c01545b5ed0abf40
승일 <seoung1515@naver.com>
2024-10-19 21:06:15 +0900

    feat: 빈 문자열은 0을 반환하고, 음수 값은 IllegalArgumentException을 던지는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            StringCalculate calculator = new StringCalculate();
+            System.out.println("덧셈할 문자열을 입력해 주세요. ");
+            String input = Console.readLine();
+            int result = calculator.calculate(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+            throw e;
+        }
     }
 }
