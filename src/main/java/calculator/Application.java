f5d79df69a3108c9a29b9e951510a961f8abb327
sleepyhoon <pokjm99@naver.com>
2024-10-19 21:20:07 +0900

    feat: 커스텀 구분자 입력 구현 및 테스트 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,19 +1,35 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        if (input == null || input.isEmpty()) {
+        if (input.isEmpty()) {
             System.out.println("결과 : 0");
         } else {
             String delimiter = "[,:]";
             // 커스텀 문자열 추가 시작
-
+            String patternString = "(//(.*)\\\\n)?(.*)";
+            Pattern pattern = Pattern.compile(patternString);
+            Matcher matcher = pattern.matcher(input);
+            if (!matcher.matches()) {
+                throw new IllegalArgumentException("입력 형식이 올바르지 않습니다");
+            }
+            String customDelimiter = matcher.group(2);
+            String numberAndDelimiter = matcher.group(3);
+            if (customDelimiter != null) {
+                // "\" 을 문자열 자체로 취급하려면 "\\"으로 작성해야 한다.
+                customDelimiter = customDelimiter.replace("\\", "\\\\");
+                delimiter = "[,:" + customDelimiter + "]";
+            }
             // 커스텀 문자열 추가 종료
-            String[] tokens = input.split(delimiter);
+
+            // 2번째 인수로 limit==-1 로 선언하면 마지막까지 공백으로 받을 수 있음.
+            String[] tokens = numberAndDelimiter.split(delimiter, -1);
             int sum = 0;
             for (String token : tokens) {
                 int number = parseString(token);
@@ -21,8 +37,9 @@ public class Application {
                     throw new IllegalArgumentException("음수는 입력 불가합니다");
                 }
                 sum += number;
-                System.out.println("결과 : " + sum);
             }
+            System.out.println("결과 : " + sum);
+            Console.close();
         }
 
     }
@@ -31,7 +48,7 @@ public class Application {
         try {
             return Integer.parseInt(token);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(e.getMessage());
+            throw new IllegalArgumentException("입력 형식이 올바르지 않습니다");
         }
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,15 +1,24 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+
     @Test
-    void 커스텀_구분자_사용() {
+    void 구분자_없이_숫자() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_1() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
@@ -17,10 +26,66 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_구분자_사용_2() {
+        assertSimpleTest(() -> {
+            run("//*\\n32,1:2*9");
+            assertThat(output()).contains("결과 : 44");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_3() {
+        assertSimpleTest(() -> {
+            run("///*-\\n32,1:2*9/56-7");
+            assertThat(output()).contains("결과 : 107");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_4() {
+        assertSimpleTest(() -> {
+            run("///\\*46-\\n32,1:2*9/565-7\\30");
+            assertThat(output()).contains("결과 : 91");
+        });
+    }
+
+    @Test
+    void 예외_테스트_음수입력() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_잘못된_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_구분자_입력_오류_1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_구분자_입력_오류_2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("/;\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_잘못된_구분자_사용_3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\n1$2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
