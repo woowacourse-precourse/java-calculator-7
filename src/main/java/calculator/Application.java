fbf0ed2d226414aa644fc252876fcd9bdd8aadce
yunju <juile0827@naver.com>
2024-10-19 17:20:02 +0900

    feat(Application): 구분자 정의 기능 추가
    Application 클래스 내 getSeparator 메서드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    // 기능 1: 커스텀 구분자 확인 및 전체 구분자 정의
+    private static List<Character> getSeparator(String inputString){
+
+        // 기본 구분자
+        List<Character> separatorList = new ArrayList<>();
+        separatorList.add(',');
+        separatorList.add(':');
+
+        // 커스텀 구분자 찾기
+        // 커스텀 구분자는 문자열 처음에 한번만 지정되거나 없다고 가정
+        String s1 = inputString.substring(0,2);
+        String s2 = inputString.substring(3,5);
+
+        if (s1.equals("//") && s2.equals("\\n")){
+            char customSeparator = inputString.charAt(2);
+            separatorList.add(customSeparator);
+        }
+
+        return separatorList;
+    }
+
 }
