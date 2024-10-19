7e9e1938e5d4faad5e547f27768a5ad6b85fad8c
J0YERIM <chosarah70@gmail.com>
2024-10-20 05:32:29 +0900

    feat: ErrorMessage 클래스 추가하여 에러 메시지 상수 관리

---
+++ src/main/java/calculator/common/ErrorMessage.java
@@ -0,0 +1,7 @@
+package calculator.common;
+
+public final class ErrorMessage {
+
+    public static final String INVALID_INPUT_ERROR = "잘못된 입력 형식입니다.";
+    public static final String NON_POSITIVE_NUMBER_ERROR = "양수가 아닌 값이 있습니다.";
+}
