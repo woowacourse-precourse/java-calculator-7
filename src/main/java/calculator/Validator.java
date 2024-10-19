66c0621a45b0f0a20a7534a75556440a16f32264
Imseonghun <daltjdgns123@gmail.com>
2024-10-20 00:41:06 +0900

    feat(Validator) : 기본 구분자 구별

---
+++ src/main/java/calculator/Validator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+  public static boolean checkDefaultDelimterFormat(String inputString) {
+    String rex = "^[0-9]+([,:]\\d+)*$";
+    Pattern pattern = Pattern.compile(rex);
+    Matcher matcher = pattern.matcher(inputString);
+
+    return matcher.find();
+  }
+}
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,6 +8,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
