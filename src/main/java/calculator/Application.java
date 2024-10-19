b8a3b81b64a3a23338a34258198dce3b537d7b48
htjworld <116538001+htjworld@users.noreply.github.com>
2024-10-19 15:41:21 +0900

    두개의 구분자로 구분 후 덧셈 기능 구현(Todo.커스텀 구분자 에러 해결,예외 오류처리)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,43 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        float result = 0;
+
+        String input = Console.readLine();
+        ArrayList <String> delimiters = new ArrayList<>(Arrays.asList(",",":"));
+
+        Pattern pattern = Pattern.compile("//(.*?)\\n");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            String delimiter = matcher.group(1);
+            System.out.println(("구분자 추가"+delimiter));
+            delimiters.add(delimiter);
+        }
+
+        System.out.println("최종구분자"+delimiters);
+
+        for (String d : delimiters){
+            input = input.replaceAll(Pattern.quote("//"+d+"q"),",");
+        }
+        System.out.println(input);
+
+        String regex = String.join("|",delimiters);
+
+        String arr[] = input.split(regex);
+        System.out.println("arr:"+arr);
+        for (String a : arr){
+            result+= Float.parseFloat(a);
+        }
+        System.out.println(result);
     }
 }
