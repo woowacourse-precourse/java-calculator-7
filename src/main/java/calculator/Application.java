6e8de548fbe06ba029306e76f769d869cec0053c
Nahye Choi <skgp0628@naver.com>
2024-10-19 21:35:33 +0900

    feat: 커스텀 구분자로 문자열 구분 및 계산 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,47 +9,75 @@ import java.util.regex.Pattern;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        final String DELIMITER = ",|:";
-        String numRegEx = "[0-9]+"; //^[0-9]*$
-
-
-        String[] splitNumbers = new String[]{};
-        int[] num = new int[]{};
         int sum = 0;
 
-        // 문자열 입력
-        // - 안내문 출력
+         // 문자열 입력
+         // - 안내문 출력
         System.out.println("덧셈할 문자열을 입력해주세요.");
         // - 입력 받기
         String numbers = Console.readLine();
-        // - 예외처리
-        // -- 숫자와 구분자만 포함하는지 판단
 
-        // 숫자 추출
+        // - 예외처리
+        if (numbers == null || numbers.isEmpty()) {
+            System.out.println("결과: " + 0);
+        }else {
+            if (numbers.startsWith("//")){
+                sum = customDelimiterSum(numbers);
+            } else{
+                sum = delimiterSum(numbers);
+            }
+        }
+        System.out.println("결과: " + sum);
+    }
+
+    // 기본 구분자
+    public static int delimiterSum(String numbers) {
+        int sum = 0;
+        final String DELIMITER = ",|:";
+
         // 정규 표현식 패턴 컴파일
         Pattern pattern = Pattern.compile(DELIMITER);
         Matcher matcher = pattern.matcher(numbers);
-
+        // -- 숫자와 구분자만 포함하는지 판단
         // - 지정 구분자로 분리
         if (matcher.find()) {
-            Pattern patternNum = Pattern.compile(numRegEx);
-            Matcher matcherNum = patternNum.matcher(numbers);
-            if (matcherNum.find()) { // 문자 형태의 숫자를 검토해야하는 거였음
-                splitNumbers = numbers.split(DELIMITER);
-                num = Arrays.stream(splitNumbers).mapToInt(Integer::parseInt).toArray();
-                for (int i : num) {
+            if (isValidNumber(numbers)) {
+                String[] splitNumbers = numbers.split(DELIMITER);
+                int[] num = Arrays.stream(splitNumbers).mapToInt(Integer::parseInt).toArray(); //형변환
+                for (int i : num) { // 계산 기능 구현
                     sum += i;
                 }
             }
         }
-        System.out.println("결과 : " + sum);
+        return sum;
+    }
 
-        // - 커스텀 구분자
+    // 커스텀
+    public static int customDelimiterSum(String numbers){
+        int sum = 0;
+        final String CUSTOM_DELIMITER = "//(.)\n(.*)";
 
-        // 문자 -> 숫자 형변환
+        Matcher customMatcher = Pattern.compile(CUSTOM_DELIMITER).matcher(numbers);
+        if (customMatcher.find()) {
+            String customDelimiter = customMatcher.group(1);
+            String[] splitCustom = customMatcher.group(2).split(customDelimiter);
 
-        // 계산 기능 구현
+            if (isValidNumber(numbers)) {
+                int[] numberSum = Arrays.stream(splitCustom).mapToInt(Integer::parseInt).toArray();
+                for (int i : numberSum) {
+                    sum += i;
+                }
+            }
+        }
+        return sum;
+    }
 
-        // 결과 출력
+    //숫자인지 확인
+    public static boolean isValidNumber(String numbers) {
+        String numRegEx = "[0-9]+"; // 문자 형태의 숫자를 검토해야하는 거였음
+
+        Pattern patternNum = Pattern.compile(numRegEx);
+        Matcher matcherNum = patternNum.matcher(numbers);
+        return matcherNum.find();
     }
 }
