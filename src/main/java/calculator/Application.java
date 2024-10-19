944873cd132f0a2a26720b9acd98e9063f50abff
윤진완 <yunjin-wan@yunjin-wan-ui-MacBookAir.local>
2024-10-19 15:22:18 +0900

    feat: 특수 구분자 구분

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,15 +2,36 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         String delimiter = ",|:";
-        String[] split = input.split(delimiter);
+        String afterInput = input;
+        Pattern pattern = Pattern.compile("//(.)\\\\n(.*)");
+        if (input.startsWith("//")) {
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.matches()) {
+                String customDelimiter = matcher.group(1);
+
+                if (customDelimiter.matches("[a-zA-Z0-9]")) {
+                    throw new IllegalArgumentException("특수 문자 아닌 문자");
+                }
+                delimiter = delimiter + "|" + Pattern.quote(customDelimiter);
+                afterInput = matcher.group(2);
+            }
+            else
+                throw new IllegalArgumentException("특수 구분자 형식이 아닙니다");
+        }
+
+
+        String[] tokens = afterInput.split(delimiter);
         int sum = 0;
-        for (String token : split) {
+        for (String token : tokens) {
             if(!token.isEmpty()){
                 try {
                     int num = Integer.parseInt(token);
@@ -23,10 +44,7 @@ public class Application {
                 }
             }
         }
-
-
-
-
+        
         System.out.println("결과 : " + sum);
     }
 }
