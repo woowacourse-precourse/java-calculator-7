358957cdc8c0dd30725468bac41e6ea3c2ab9d17
whrjsgml0000 <whrjsgml4515@naver.com>
2024-10-19 12:48:37 +0900

    comment:주석 한글화함.

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -5,7 +5,7 @@ public class CalculatorService {
     private String plusSeparator;
 
     /**
-     * Run all logic step by step.
+     * 덧셈 로직을 순차적으로 진행한다.
      */
     public int sum(String s) {
         this.s = s;
@@ -16,9 +16,9 @@ public class CalculatorService {
     }
 
     /**
-     * Check existence of another separator.
+     * 커스텀 구분자가 있는지 확인한다.
      *
-     * @return if exist another separator, return true. Or return false.
+     * @return 만약 커스텀 구분자가 있다면 true, 없다면 false 를 반환한다.
      */
     private boolean checkPlusSeparator() {
         if (s.length() >= 5 && s.startsWith("//") && s.startsWith("\\n", 3)) {
@@ -30,15 +30,15 @@ public class CalculatorService {
     }
 
     /**
-     * Separate input string. if escape string(\) added separator, need action.
+     * 받은 문자열을 구분자를 기준으로 나눈다. 만약 커스텀 구분자에 역슬래시가 있다면 문제가 생기지 않도록 한다.
      *
-     * @param hasPlusSeparator if another separator exist, set true. Or false.
-     * @return separated string.
+     * @param hasPlusSeparator 커스텀 구분자가 있다면 true, 없다면 false 를 입력한다.
+     * @return 분리된 문자열을 반환한다. 이 문자열에는 반드시 숫자만 있는 것은 아니다.
      */
     private String[] separate(boolean hasPlusSeparator) {
         if (!hasPlusSeparator) {
             return s.split("[:,]");
-        } else {
+        } else { //else 문 삭제해도 됨. ####################################################
             if (plusSeparator.equals("\\")) {
                 return s.split("[:," + plusSeparator.repeat(2) + "]");
             }
@@ -47,9 +47,9 @@ public class CalculatorService {
     }
 
     /**
-     * Check input string array contains not a number. if it contains NaN, throw IllegalArgumentException.
+     * 숫자가 아닌 문자열이 있는지 확인한다. 만약 있다면 IllegalArgumentException 으로 처리한다.
      *
-     * @param separatedStringArr String separated by separator.
+     * @param separatedStringArr 숫자 외의 문자가 있는지 확인하고 싶은 문자열을 입력한다.
      */
     private void hasNaN(String[] separatedStringArr) {
         for (String s : separatedStringArr) {
@@ -62,10 +62,10 @@ public class CalculatorService {
     }
 
     /**
-     * Sum all of them
+     * 문자열을 int 형으로 바꾼 뒤 합한다.
      *
-     * @param separatedStringArr clean integer string array.
-     * @return Sum
+     * @param separatedStringArr 숫자로만 이루어진 문자열 배열을 입력한다.
+     * @return 합한다.
      */
     private int sumSeparatedStringArr(String[] separatedStringArr) {
         int sum = 0;
