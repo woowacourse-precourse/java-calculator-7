e2e3b685272b525b1db94e63b47343c8a623954e
likerhythm <wjdalsdl0412@gmail.com>
2024-10-19 17:08:21 +0900

    feat(Application): 프로그램 시작점 역할

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,8 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CalculatorApp calculatorApp = new CalculatorApp();
+
+        calculatorApp.run();
     }
 }
