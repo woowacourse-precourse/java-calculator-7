8e7a5643b7ac3df934d333b5ccf0c01320fe70ee
YeonjiIsGonji <165933276+YeonjiIsGonji@users.noreply.github.com>
2024-10-20 01:23:04 +0900

    feat(findDelimiter): 입력 및  커스텀 구분자 확인 기능 구현
    문자열을 입력받는 기능과 구분자를 추출하는 기능을 구현.
    '//;\' 형태의 구분자 형식이 아닌 경우 예외 처리 추가.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,38 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 입력
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = Console.readLine();
+        String[] splitStr = str.split("");
+
+        // 구분자 확인
+        if (splitStr[0].equals("/")) {
+            String delimiter = findDelimiter(splitStr);
+            // 문자열 분리
+        } else {
+            // 문자열 분리
+        }
+
+        // 출력
+
+    }
+
+    public static String findDelimiter (String[] splitStr) {
+        if (splitStr[1].equals("/") && splitStr[3].equals("\\") && splitStr[4].equals("n")) {
+            String delimiter = splitStr[2];
+            return delimiter;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
