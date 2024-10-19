6aab87b2592b1cd8c6ddf43b6e010b6e521dc6b9
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-10-19 14:13:12 +0900

    feat : 사용자 입력 받기

---
+++ src/main/java/calculator/io/ConsoleInputHandler.java
@@ -0,0 +1,13 @@
+package calculator.io;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleInputHandler {
+
+    public String getUserInput(){
+        String userInput = Console.readLine();
+
+        return userInput;
+    }
+
+}
