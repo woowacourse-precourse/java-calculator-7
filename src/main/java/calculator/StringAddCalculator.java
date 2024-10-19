a1cc3fbb62038d548ec6f8c759373f8d5a3f2535
yanghyen <sk0310803@naver.com>
2024-10-19 22:30:16 +0900

    feat(calculator): 사용자 입력 받는 기능 추가

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class StringAddCalculator {
+
+  public static void main(String[] args) {
+    // 사용자 입력 받기
+    System.out.println("덧셈할 문자열을 입력해 주세요.");
+    String userInput = Console.readLine();
+  }
+}
