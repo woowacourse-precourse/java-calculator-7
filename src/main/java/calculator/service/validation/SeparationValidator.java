b031832e083eba1b50c2cbf7e40f175434f19f4d
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-19 15:33:54 +0900

    Feat: 공통 로직에 진입한 이후 구분자로 나눈 요소가 숫자가 아닌 경우 예외 처리

--- src/main/java/calculator/service/validation/SeparationValidator.java
+++ src/main/java/calculator/service/validation/SeparationValidator.java
@@ -2,6 +2,7 @@ package calculator.service.validation;
 
 import static calculator.service.exception.SeparationExceptionType.ENDS_WITH_SEPARATOR;
 import static calculator.service.exception.SeparationExceptionType.INVALID_SEPARATOR;
+import static calculator.service.exception.SeparationExceptionType.NOT_A_POSITIVE_NUMBER;
 import static calculator.service.exception.SeparationExceptionType.NO_INPUT;
 import static calculator.service.exception.SeparationExceptionType.STARTS_WITH_SEPARATOR;
 
@@ -15,6 +16,7 @@ public class SeparationValidator {
         validateStartsWithSeparator(input, separators);
         validateEndWithSeparator(input, separators);
         validateIsDigit(input, separators);
+        validateIsPositive(input, separators);
     }
 
     // 입력값이 존재하는지 검증합니다.
@@ -53,5 +55,20 @@ public class SeparationValidator {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    // 구분자로 나뉘어진 모든 요소가 양수인지 검증합니다.
+    private static void validateIsPositive(String input, String... separators) {
+        String regex = String.join("|", separators);
+        if (!Arrays.stream(input.split(regex)).allMatch(str -> isPositive(str))) {
+            throw new SeparationException(NOT_A_POSITIVE_NUMBER);
+        }
+    }
+
+    private static boolean isPositive(String str) {
+        if (Integer.parseInt(str) > 0) {
+            return true;
+        }
+        return false;
     }
 }
