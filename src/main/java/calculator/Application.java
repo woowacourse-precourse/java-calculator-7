9fb454185630f1a31d5d212400bc1b7f43c09ac1
yummygyudon <bang2brew@gmail.com>
2024-10-20 03:56:05 +0900

    feat(`Application`) : 어플리케이션 실행 구현
    - Calculator 객체를 생성한 후, 유일한 공개 메서드인 `execute()`를 실행하도록 했습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.application.Calculator;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Calculator calculator = new Calculator();
+        calculator.execute();
     }
 }
