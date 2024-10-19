eef9af4d03703aceb3fd551e1dd75110dd46775d
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-19 20:39:34 +0900

    fix: OutputView 매개변수 타입수정

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -4,7 +4,7 @@ public class OutputView {
 
     private static final String OUTPUT_MESSAGE = "결과 : ";
 
-    public static void printResult(String result) {
+    public static void printResult(int result) {
         System.out.println(OUTPUT_MESSAGE + result);
     }
 }
