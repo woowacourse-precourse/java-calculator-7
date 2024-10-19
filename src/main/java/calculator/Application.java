8dbb600954725eb642f11be99ddec184f99af09c
haneul0509 <haneul0509@naver.com>
2024-10-19 17:20:24 +0900

    feat: 1.문자열 입력 받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        //1. camp.nextstep.edu.missionutils에서 제공하는 Console API를 사용하여 문자열 입력 받기
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        System.out.println(input);
     }
 }
