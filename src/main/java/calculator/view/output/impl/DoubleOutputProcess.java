02c71f45379e2691542263e5503bb366bbfbffaf
EHCTH <lees90896@gmail.com>
2024-10-20 00:14:04 +0900

    refactor: 인터페이스를 사용하여 double, long을 출력 * feat: 다형성

--- src/main/java/calculator/view/output/impl/DoubleOutputProcess.java
+++ src/main/java/calculator/view/output/impl/DoubleOutputProcess.java
@@ -3,15 +3,15 @@ package calculator.view.output.impl;
 import calculator.view.output.InfoService;
 import calculator.view.output.ResultService;
 
-public class LongOutputProcess implements InfoService, ResultService {
-
+public class DoubleOutputProcess implements InfoService, ResultService {
     @Override
     public void info() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+
     }
 
     @Override
     public void result(Number number) {
-        System.out.printf("결과 : %,d", number.longValue());
+        System.out.printf("결과 : %.2f", number.doubleValue());
     }
 }
