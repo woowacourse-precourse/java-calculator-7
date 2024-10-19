48db66a826454feb3c2d96624fe4deea9bd576e3
김성민 <klg1425@gmail.com>
2024-10-20 00:42:55 +0900

    test : 첫번째 test (테스트2개 추가)
    기본 제공 테스트 이외에 테스트 2개를 추가했습니다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,7 +17,23 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_구분자_사용2() {
+        assertSimpleTest(() -> {
+            run("//!\\n1!2!3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자와_기본_구분자_함께_사용() {
+        assertSimpleTest(() -> {
+            run("//!\\n1!2,3,4:");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 음수_예외처리_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
