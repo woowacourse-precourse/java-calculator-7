836593800ea8f35e5d7d9d7da323bbbeaa403b28
dnwldla <leeemjw@naver.com>
2024-10-19 22:19:57 +0900

    feat: 계산결과를 출력하는 메소드 추가

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,9 +2,13 @@ package calculator.view;
 
 public class OutputView {
     private static final String START_COMMAND="덧셈할 문자열을 입력해 주세요.";
-
-    //게임 시작을 출력한다
+    private static final String RESULT_COMMAND="결과 : %d";
+    //시작을 출력한다
     public static void printCommand(){
         System.out.println(START_COMMAND);
+    }
+
+    public static void printResult(int result) {
+        System.out.printf(RESULT_COMMAND,result);
     }
 }
