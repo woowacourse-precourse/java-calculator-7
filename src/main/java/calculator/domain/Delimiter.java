c5f4eb1a3383cbfdfae25c47ef82df8eb802c62e
ykmxxi <ykmxxi97@gmail.com>
2024-10-19 13:44:28 +0900

    feat: 입력을 구분자로 분리한다

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -1,11 +1,15 @@
 package calculator.domain;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Delimiter {
 
     private static final Pattern DEFAULT = Pattern.compile("[\\d*,|:?]*");
     private static final Pattern CUSTOM = Pattern.compile("//(.)+\n(.)*");
+    private static final String DEFAULT_DELIMITER = "[,|:]";
 
     public boolean isDefault(final String input) {
         return DEFAULT.matcher(input)
@@ -15,6 +19,46 @@ public class Delimiter {
     public boolean isCustom(final String input) {
         return CUSTOM.matcher(input)
                 .matches();
+    }
+
+    public List<String> split(final String input) {
+        if (isCustom(input)) {
+            return splitCustom(input);
+        }
+        if (isDefault(input)) {
+            return splitDefault(input);
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private List<String> splitDefault(final String input) {
+        return Arrays.stream(input.split(DEFAULT_DELIMITER))
+                .toList();
+    }
+
+    private List<String> splitCustom(final String input) {
+        List<String> delimiterAndNumbers = splitByLineFeed(input);
+        if (isDefaultNumbers(delimiterAndNumbers)) {
+            return Collections.emptyList();
+        }
+        String customDelimiter = getCustomDelimiter(delimiterAndNumbers);
+        return Arrays.stream(delimiterAndNumbers.getLast()
+                .split(customDelimiter)
+        ).toList();
+    }
+
+    private boolean isDefaultNumbers(final List<String> delimiterAndNumbers) {
+        return delimiterAndNumbers.size() == 1;
+    }
+
+    private List<String> splitByLineFeed(final String input) {
+        return input.lines()
+                .toList();
+    }
+
+    private String getCustomDelimiter(final List<String> delimiterAndNumbers) {
+        return delimiterAndNumbers.getFirst()
+                .replaceAll("//", "");
     }
 
 }
--- src/test/java/calculator/domain/DelimiterTest.java
+++ src/test/java/calculator/domain/DelimiterTest.java
@@ -1,11 +1,18 @@
 package calculator.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class DelimiterTest {
@@ -42,6 +49,50 @@ class DelimiterTest {
     @DisplayName("커스텀 구분자 불일치: 빈 문자열은 커스텀 구분자가 아님")
     void 커스텀_구분자_불일치() {
         assertThat(delimiter.isCustom("//\n")).isFalse();
+    }
+
+    @MethodSource(value = "provideDefaultInputAndNumbers")
+    @ParameterizedTest(name = "{0}의 분리 결과는 {1}")
+    @DisplayName("기본 구분자 입력 분리")
+    void 기본_구분자_분리(String input, List<String> expectedNumbers) {
+        assertThat(delimiter.split(input)).isEqualTo(expectedNumbers);
+    }
+
+    @MethodSource(value = "provideCustomInputAndNumbers")
+    @ParameterizedTest(name = "{0}의 분리 결과는 {1}")
+    @DisplayName("커스텀 구분자 입력 분리")
+    void 커스텀_구분자_분리(String input, List<String> expectedNumbers) {
+        assertThat(delimiter.split(input)).isEqualTo(expectedNumbers);
+    }
+
+    @ValueSource(strings = {"1.", "!1", "1,2:3;4", ",|;", "//\n"})
+    @ParameterizedTest(name = "{0}은 분리할 수 없습니다")
+    @DisplayName("입력 분리 실패: 기본/커스텀 구분자에 속하지 않음")
+    void 입력_분리_실패(String input) {
+        assertThatThrownBy(() -> delimiter.split(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> provideDefaultInputAndNumbers() {
+        return Stream.of(
+                Arguments.of("", List.of("")),
+                Arguments.of("1", List.of("1")),
+                Arguments.of("1,2", List.of("1", "2")),
+                Arguments.of("1,2:3", List.of("1", "2", "3")),
+                Arguments.of(",2:3", List.of("", "2", "3")),
+                Arguments.of(",2:", List.of("", "2")),
+                Arguments.of(",:", Collections.emptyList())
+        );
+    }
+
+    static Stream<Arguments> provideCustomInputAndNumbers() {
+        return Stream.of(
+                Arguments.of("//;\n1", List.of("1")),
+                Arguments.of("//;\n1;2", List.of("1", "2")),
+                Arguments.of("// \n1 2 3", List.of("1", "2", "3")),
+                Arguments.of("// \n", Collections.emptyList()),
+                Arguments.of("// \n 1", List.of("", "1"))
+        );
     }
 
 }
