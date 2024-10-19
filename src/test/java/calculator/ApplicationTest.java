6ab0dfb3bcbb447bc16180e7868b03de752de597
(minjae8563) <(minjae8563@naver.com)>
2024-10-19 16:34:25 +0900

    Test: 테스트 케이스 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -15,15 +16,62 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains("결과 : 1");
         });
     }
-
+    @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+    @Test
+    void 기본_구분자_혼합_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+    @Test
+    void 공백은_0으로_인식(){
+        assertSimpleTest(() -> {
+            run("1:::::2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+    @Test
+    void 커스텀_구분자_기본_구분자_혼합_사용(){
+        assertSimpleTest(() -> {
+            run("//-\\n1:2-3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+    @Test
+    void 예외_테스트_숫자는_양수만() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1:0:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_문자열은_양수와_구분자만() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("////;\n\n1//;2//;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_구분자_외_문자_존재() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("////;\n\n1//;\n2//;\n4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
-
     @Override
     public void runMain() {
         Application.main(new String[]{});
