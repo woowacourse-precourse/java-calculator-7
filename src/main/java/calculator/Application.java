df1aa22c32879bdb11b001bb02e6c30c239cab9b
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-10-19 11:53:07 +0900

    Feat: Application 메인문 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import calculator.controller.Controller;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Controller controller = new Controller(inputView, outputView);
+
+        controller.run();
     }
 }
