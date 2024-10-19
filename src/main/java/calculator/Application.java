8338c43fdaa91e2032a16859ed69b5d441dd0162
gabean13 <choegabin171@gmail.com>
2024-10-20 01:39:32 +0900

    feat(Application) : exception 처리 및 프로그램 종료 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,16 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.run();
+        try {
+            calculator.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println("올바른 입력이 아닙니다.");
+        } catch (ArithmeticException e) {
+            System.out.println("연산 가능한 범위를 초과했습니다.");
+        } catch (Exception e) {
+            System.out.println("에러가 발생했습니다.");
+        } finally {
+            System.exit(0);
+        }
     }
 }
