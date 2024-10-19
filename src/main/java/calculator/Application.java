e7940f294b4153587d0777cb3f703ab6d27e1300
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-10-20 03:18:34 +0900

    feat: 계산기 컨트롤러 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.domain.AdditionCalculator;
+import calculator.domain.DelimiterMaker;
+import calculator.domain.InputParser;
+import calculator.domain.NumberExtractor;
+import calculator.io.input.InputHandler;
+import calculator.io.output.OutputHandler;
+import calculator.service.CalculatorService;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+
+		OutputHandler outputHandler = new OutputHandler();
+		InputHandler inputHandler = new InputHandler();
+		CalculatorService calculatorService =
+			new CalculatorService(new AdditionCalculator(), new DelimiterMaker(), new NumberExtractor(), new InputParser());
+
+		CalculatorController calculatorController =
+			new CalculatorController(outputHandler, inputHandler, calculatorService);
+		calculatorController.run();
+	}
 }
