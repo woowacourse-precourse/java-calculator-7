51d74ff16981dd167b0bf054c245d2a4191ffd8f
USER <cji513@naver.com>
2024-10-19 19:29:30 +0900

    feat(OutputView): lastCommand method 추가

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -6,4 +6,8 @@ public class OutputView {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
+    public void lastCommand(int result){
+        System.out.println("결과 : " + result);
+    }
+
 }
