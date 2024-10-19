7dbfeab2be3079d32e688eadfbaa4beb432cd82c
KF97 <aysul0@naver.com>
2024-10-19 15:04:27 +0900

    test: 여러개의_커스텀_구분자_사용 테스트 코드 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -36,6 +36,14 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 여러개의_커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;+\\n1+2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("-1,2,3"))
