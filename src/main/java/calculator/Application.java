776e5a3a11f5877125471fe9544b5a4497ce8d54
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-10-19 11:40:57 +0900

    feat: 문자열에서 숫자 추출 기능 구현
    - 입력 받은 문자열에서 커스텀 구분자와 기본 구분자로 숫자를 추출하는 기능을 구현했습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,7 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Calculator calculator = new Calculator();
+        calculator.getNumbers("1,2:3-9");
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,18 +1,27 @@
 package calculator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Calculator {
-    private final List<String> division = new ArrayList<>();
+    private String division = "-";
     private final List<Integer> numbers = new ArrayList<>();
 
-    public void getDivision(/*입력 받은 문자열*/) {
+    public void getDivision(String inputString) {
 
     }
 
-    public void getNumbers(/*입력 받은 문자열*/) {
+    public void getNumbers(String inputString) {
+        String[] customStrings = inputString.split(division);
+        List<String> strings;
 
+        for (String customString : customStrings) {
+            strings =  Arrays.asList(customString.split(",|:"));
+            for (String string : strings) {
+                numbers.add(Integer.parseInt(string));
+            }
+        }
     }
 
     public int calculator() {
