278783a396b27a6d46b962fedb9a0dfdd9b4189a
DongHoon Lee <dhl1924@naver.com>
2024-10-19 17:38:39 +0900

    feat(OutputView): 기본 결과 출력 메서드 추가

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -3,11 +3,16 @@ package calculator.view;
 public class OutputView {
 
     private static final String ADDITION_PROMPT = "덧셈할 문자열을 입력해 주세요.";
+    private static final String DEFAULT_RESULT = "결과 : 0";
 
     private OutputView() {
     }
 
     public static void printAdditionInputPrompt() {
         System.out.println(ADDITION_PROMPT);
+    }
+
+    public static void printDefaultResult() {
+        System.out.println(DEFAULT_RESULT);
     }
 }
