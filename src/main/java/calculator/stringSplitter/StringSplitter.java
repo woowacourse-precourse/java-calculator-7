2029ed43050a37412311c0439245e6d5f6b83ddc
Hyso <hsk7953@gmail.com>
2024-10-20 02:40:50 +0900

    :recycle: refactor : StringSplitter 클래스의 splitsString 메서드의 로직을 별도 메서드들로 분리 각 검증 로직과 진행 과정을 우선적으로 독립적인 메서드로 분리 - 리팩토링 과정에서 도움을 주기 위한 가독성 증진

--- src/main/java/calculator/stringSplitter/StringSplitter.java
+++ src/main/java/calculator/stringSplitter/StringSplitter.java
@@ -1,75 +1,81 @@
 package calculator.stringSplitter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringSplitter {
     public String[] splitsString(String input){
-        List<String>  separator = new ArrayList<>();
-        separator.add(",");
-        separator.add(":");
+        List<String> separator = getSeparator();
         String number = input;
-        if(!Character.isDigit(input.charAt(input.length()-1))){
-            throw new IllegalArgumentException("마지막은 숫자로 입력해주세요");
-        }
+        checkLastCharIsDigit(input);
         if(input.startsWith("//")){
+            ensureCustomDelimiterExists(input);
             int endPoint = input.lastIndexOf("\\n");
-            if(endPoint == -1){
-                throw new IllegalArgumentException("\\n가 없다면 커스텀 구분자를 사용할 수 없습니다."); // 예외발생
-            }
-
             String customSeparators = input.substring(2,endPoint);
-            for(char c : customSeparators.toCharArray()){
-                if(Character.isWhitespace(c)){
-                    throw new IllegalArgumentException("구분자에는 빈칸이 불가능합니다.");
-                }else if(Character.isDigit(c)){
-                    throw new IllegalArgumentException("구분자에는 숫자가 들어갈 수 없습니다.");
-                }
-            }
-            if(customSeparators.length() > 1){
-                String[] splited = customSeparators.split("");
-                for(String str : splited){
-                    separator.add(str);
-                }
-            }else{
-                separator.add(customSeparators);
-            }
+            validateSeparators(customSeparators);
+            addSeparators(separator, customSeparators);
             number = input.substring(endPoint+2);
         }
+        validateInput(separator, number);
+        String calculateSeparator = getCalculateSeparator(separator);
+
+        return number.split(calculateSeparator);
+    }
+
+    private static void addSeparators(List<String> separator, String customSeparators) {
+        if(customSeparators.length() > 1){
+            String[] splited = customSeparators.split("");
+            for(String str : splited){
+                separator.add(str);
+            }
+        }else{
+            separator.add(customSeparators);
+        }
+    }
+
+    private static String getCalculateSeparator(List<String> separator) {
+        return separator.stream().map(Pattern::quote)
+                .collect(Collectors.joining("|"));
+    }
+
+    private static void validateInput(List<String> separator, String number) {
         for (char c : number.toCharArray()) {
             if(!Character.isDigit(c) && !separator.contains(String.valueOf(c))){
                 throw new IllegalArgumentException("허용되지 않은 입력입니다.");
             }
         }
-        String calculateSeparator = separator.stream().map(Pattern::quote)
-                .collect(Collectors.joining("|"));
-
-        return number.split(calculateSeparator);
     }
 
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add(",");
-        list.add(":");
-        list.add(Pattern.quote("+"));
-        list.add(Pattern.quote("@"));
-        String regex = list.stream().collect(Collectors.joining("|"));
-        String tmp ="1+3@4:5,6"; // true
-        String tmp2 = "1-3@4:5"; // false
-        String tmp3 = "1+3!4,5@6;"; // false
-
-
-    }
-    private static void check(String tmp, Pattern pattern){
-        Matcher matcher = pattern.matcher(tmp);
-        if (matcher.matches()) {
-            System.out.println("매칭됨: \"" + tmp + "\"");
-        } else {
-            System.out.println("매칭되지 않음: \"" + tmp + "\"");
+    private static void validateSeparators(String customSeparators) {
+        for(char c : customSeparators.toCharArray()){
+            if(Character.isWhitespace(c)){
+                throw new IllegalArgumentException("구분자에는 빈칸이 불가능합니다.");
+            }else if(Character.isDigit(c)){
+                throw new IllegalArgumentException("구분자에는 숫자가 들어갈 수 없습니다.");
+            }
         }
-
     }
+
+    private static void ensureCustomDelimiterExists(String input) {
+        if(!input.contains("\\n")){
+            throw new IllegalArgumentException("\\n가 없다면 커스텀 구분자를 사용할 수 없습니다."); // 예외발생
+        }
+    }
+
+    private static void checkLastCharIsDigit(String input) {
+        if(!Character.isDigit(input.charAt(input.length()-1))){
+            throw new IllegalArgumentException("마지막은 숫자로 입력해주세요");
+        }
+    }
+
+    private static List<String> getSeparator() {
+        List<String>  separator = new ArrayList<>();
+        separator.add(",");
+        separator.add(":");
+        return separator;
+    }
+
 }
