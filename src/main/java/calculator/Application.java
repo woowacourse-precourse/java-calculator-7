851f77707e51a7448a6b132a763b2231a955c465
Jun2-Lee <ljh19980823@gmail.com>
2024-10-19 23:04:20 +0900

    feat : 사용자에게 초기에 보여줄 메시지 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
 public class Application {
+    // "덧셈할 문자열을 입력해주세요." 구문 출력
+    private static void init_print() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        init_print();
+
     }
 }
