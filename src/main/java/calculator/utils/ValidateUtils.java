1c6e4e10eff96b901499aaf0d12be17d59012830
sik9252 <sik9252@naver.com>
2024-10-19 11:33:26 +0900

    refactor: 메서드명 변경, 파라미터로 전달받는 인수 타입 변경
    - checkIsNumericAndPositive() => checkNumberIsValid()
    - List<String> splitInput => String number

--- src/main/java/calculator/utils/ValidateUtils.java
+++ src/main/java/calculator/utils/ValidateUtils.java
@@ -2,18 +2,14 @@ package calculator.utils;
 
 import calculator.constant.ErrorMessage;
 
-import java.util.List;
-
 public class ValidateUtils {
-    public static void checkIsNumericAndPositive(List<String> splitInput) {
-        for (String text : splitInput) {
-            if (isNumeric(text)) {
-                if (Integer.parseInt(text) < 0) {
-                    throw new IllegalArgumentException(ErrorMessage.NEGATIVE_NUMBER_INPUT.getMessage());
-                }
-            } else {
-                throw new IllegalArgumentException(ErrorMessage.NOT_A_NUMBER.getMessage());
+    public static void checkNumberIsValid(String number) {
+        if (isNumeric(number)) {
+            if (Integer.parseInt(number) < 0) {
+                throw new IllegalArgumentException(ErrorMessage.NEGATIVE_NUMBER_INPUT.getMessage());
             }
+        } else {
+            throw new IllegalArgumentException(ErrorMessage.NOT_A_NUMBER.getMessage());
         }
     }
 
