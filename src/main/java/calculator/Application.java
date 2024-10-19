278d3dd37d0a83abd9acbdc424900242b9d5c1f9
고경우 <dfdfg11@gmail.com>
2024-10-19 13:22:15 +0900

    feat: 기본 구분자를 통해 덧셈구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 사용자 입력 받기
         String input = Console.readLine();
 
         // 빈 문자열 또는 null 처리
@@ -13,7 +13,17 @@ public class Application {
             return;
         }
 
+        String delimiter = ",|:"; // 기본 구분자 (쉼표와 콜론)
 
+
+        // 구분자를 사용해 문자열을 분리
+        String[] input_integer = input.split(delimiter);
+        int sum = 0;
+        for (String integer : input_integer) {
+            sum += Integer.parseInt(integer.trim()); // 각 문자열을 정수로 변환해 합산
+        }
+
+        // 결과 출력
+        System.out.println("결과 : " + sum);
     }
 }
-
