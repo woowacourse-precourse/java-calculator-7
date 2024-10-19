f70143e42e2f18d9a9b4765ad935a22346246690
starMinK <rbals040329@gmail.com>
2024-10-19 16:36:49 +0900

    feat(input): 사용자 입력 처리 기능 추가
    특정 기호로 구분된 숫자 문자열을 입력받아 리스트로 변환하는 기능을 추가했습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        input();
+    }
+
+    private static void input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        readLine();
     }
 }
