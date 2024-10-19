d77110f72449298557a952823ea8986806a43207
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-20 02:44:01 +0900

    feat: 자원 반환 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,27 +5,30 @@ import java.util.List;
 
 public class Calculator {
 
-    private static final String INPUT = "덧셈할 문자열을 입력해주세요.";
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해주세요.";
+    private static final String OUTPUT_MESSAGE = "결과 : ";
 
-    public static void Run() {
-        System.out.println(INPUT);
-        String input = Console.readLine();
-        int sum = Sum(input);
-        System.out.println("sum = " + sum);
+    public static void start() {
+        try {
+            System.out.println(INPUT_MESSAGE);
+            String input = Console.readLine();
+            List<Integer> list = Parser.parse(input);// parser
+            int sum = sum(list);
+            end(sum);
+        } finally {
+            Console.close();
+        }
     }
 
-    private static int Sum(String input) {
-        String contents = Parser.getNumberString(input);
-        System.out.println("contents = " + contents);
-        List<String> separators = Parser.parse(input);
-        int[] numbers = Parser.getNumbers(contents, separators);
+    public static int sum(List<Integer> input) {
         int sum = 0;
-        for (int number : numbers) {
+        for (int number : input) {
             sum += number;
         }
         return sum;
     }
 
-    public static void End() {
+    public static void end(int sum) {
+        System.out.println(OUTPUT_MESSAGE + sum);
     }
 }
