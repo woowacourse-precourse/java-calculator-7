f131b30841299fb04fb03e2c650b66bcf366633d
kingjinyong <wlsdydwhswkf@gmail.com>
2024-10-19 12:58:30 +0900

    test(CalculatorModelTest): 커스텀 구분자 지정 시 지정 문자 올바른 사용 테스트 코드 작성
    커스텀 구분자를 지정 하려고 할 때, 올바른 지정 문자를 사용했는지 확인하는 테스트 코드를 작성했습니다.

---
+++ src/test/java/calculator/model/CalculatorModelTest.java
@@ -0,0 +1,18 @@
+package calculator.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CalculatorModelTest {
+
+    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\\n1,2;3", "//\\n1,2,3,4"})
+    void 커스텀_구분자_지정_문자_올바른_사용(String input) {
+        CalculatorModel cm = new CalculatorModel();
+        Assertions.assertThat(cm.CustomDiscrimination(input)).isEqualTo(true);
+    }
+
+}
