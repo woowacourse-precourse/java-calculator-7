335a2c4219e3727b82f4221a38fdaf70e0a90505
dgh06175 <dgh06175@gmail.com>
2024-10-19 21:51:54 +0900

    feat: 컨트롤러 구현

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.List;
+
 public class CalculatorController {
     private final CalculatorView calculatorView;
     private final CalculatorModel calculatorModel;
@@ -11,11 +13,16 @@ public class CalculatorController {
 
     public void run() {
         // 입력 받기
+        calculatorView.printInputMessage();
+        String input = calculatorView.readInput();
 
         // 문자열 구분하기
+        List<Integer> values = calculatorModel.splitString(input);
 
         // 문자열 계산하기
+        int result = calculatorModel.calculateSum(values);
 
         // 결과 출력하기
+        calculatorView.printOutputMessage(result);
     }
 }
--- src/main/java/calculator/CalculatorModel.java
+++ src/main/java/calculator/CalculatorModel.java
@@ -1,11 +1,18 @@
 package calculator;
 
+import java.util.Collections;
 import java.util.List;
 
 public class CalculatorModel {
     private List<String> dividers = List.of(",", ":");
 
-    public int calculateSum(String input) {
+    public List<Integer> splitString(String input) {
+        // TODO: 문자열 구분하기
+        return Collections.emptyList();
+    }
+
+    public int calculateSum(List<Integer> values) {
+        // TODO: 값 더해서 반환하기
         return 0;
     }
 }
