dae909183cd21a12d7b0dca95692ed52779f4233
capDoYeonLee <ghtyu21@naver.com>
2024-10-19 20:22:31 +0900

    refactor: calculate 내부 로그 확인 기능 제거

--- src/main/java/calculator/CustomSeparatorStrategy.java
+++ src/main/java/calculator/CustomSeparatorStrategy.java
@@ -8,7 +8,6 @@ public class CustomSeparatorStrategy implements CalculatorStrategy {
 
     public void calculate(String inputText) {
         String customSeparator = calculator.findCustomSeparator(inputText);
-        System.out.println("커스텀 구분자 : " + customSeparator);
         String text = calculator.removeCustomReserveWord(inputText);
         String arr[] = calculator.extractNumber(customSeparator, text);
         calculator.sum(arr);
