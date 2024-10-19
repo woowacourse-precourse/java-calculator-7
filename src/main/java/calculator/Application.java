9d0b8fe3d98bb3137d0660d87a006981a76bf1cb
wgrgwg <kyoeng14697@naver.com>
2024-10-20 00:44:17 +0900

    feat: implement main method for execution
    Implement main method in Application class to instantiate
    calculatorModel, calculatorView, and calculatorController objects, facilitating the calculator startup process.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.model.CalculatorModel;
+import calculator.view.CalculatorView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorModel calculatorModel = new CalculatorModel();
+        CalculatorView calculatorView = new CalculatorView();
+        CalculatorController calculatorController = new CalculatorController(calculatorModel, calculatorView);
+
+        calculatorController.calculate();
     }
 }
