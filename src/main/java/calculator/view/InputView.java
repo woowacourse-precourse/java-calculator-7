caa599a78e9b28b6133bb396bea944318e37f989
w0dnj5 <jw991211@icloud.com>
2024-10-19 16:41:20 +0900

    feat(InputView): 입력값 공백 제거 기능 추가
    - 입력값의 앞뒤 공백을 제거해주기 위해 trim() 함수 추가

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -6,6 +6,6 @@ public class InputView {
 
     public String readInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine();
+        return Console.readLine().trim();
     }
 }
