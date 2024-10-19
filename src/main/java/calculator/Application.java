a9840a0a74aafb5dbb52d8bde9d120313dd99777
sjsk3232 <sjsk3232@gmail.com>
2024-10-19 21:40:45 +0900

    refactor : 계산할 문자열 입력 받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
     }
 }
