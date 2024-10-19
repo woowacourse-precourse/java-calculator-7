f5ff35148aed529da1ac0468a7f739bdf57360b4
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-19 20:40:01 +0900

    feat: 계산기 실행로직추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.global.util.CustomDelimiterExtractor;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorController calculatorController = new CalculatorController();
+        calculatorController.run();
     }
 }
