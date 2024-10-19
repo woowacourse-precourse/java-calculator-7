485d6dfde9f8077dd935b888d07fb78055ba9231
Gyulguma <ybg653924@gmail.com>
2024-10-20 00:53:36 +0900

    feat(Application): 문자열 입력 기능 추가
    문자열 입력 기능만 추가되었기 때문에,
    결과에 사용자가 입력한 내용이 출력된다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,27 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public Application() {
+    }
+
+    public void run() {
+        String input = getInput();
+        System.out.println("결과 : " + input);
+    }
+
+    private String getInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        Console.close();
+        return input;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application application = new Application();
+        application.run();
     }
 }
