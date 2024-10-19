71da9bf1f2ab96fd09e338e604c8451065d3e4a7
zjhj0814 <zjhj0814@gmail.com>
2024-10-20 00:06:44 +0900

    feat(calculate): 계산 인터페이스와 구현체 생성
    CalculateTest 컴파일을 위해 Calculate 인터페이스와 IntegerCalculate 구현체를 생성한다

---
+++ src/main/java/calculateStrategy/integerCalculate.java
@@ -0,0 +1,13 @@
+package calculateStrategy;
+
+import java.util.List;
+
+public class integerCalculate implements Calculate {
+    public integerCalculate() {
+    }
+
+    @Override
+    public Integer add(List<Integer> operands) {
+        return null;
+    }
+}
