1f6603dacff29f670b128ac6aa310d48457d70c5
ykmxxi <ykmxxi97@gmail.com>
2024-10-19 15:49:41 +0900

    feat: 사용자 입력을 검증한다

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -7,11 +7,23 @@ public class InputView {
     public String read() {
         String input;
         try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
             input = Console.readLine();
+            validateWhiteSpaces(input);
         } finally {
             Console.close();
         }
         return input;
+    }
+
+    private void validateWhiteSpaces(final String input) {
+        if (isWhitespaces(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isWhitespaces(final String input) {
+        return !input.isEmpty() && input.isBlank();
     }
 
 }
--- src/test/java/calculator/view/InputViewTest.java
+++ src/test/java/calculator/view/InputViewTest.java
@@ -1,11 +1,13 @@
 package calculator.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -13,17 +15,37 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class InputViewTest {
 
+    private InputView inputView;
+
+    @BeforeEach
+    void setUp() {
+        inputView = new InputView();
+    }
+
     @MethodSource(value = "provideInputAndExpected")
     @ParameterizedTest(name = "입력값: \"{0}\", 기대값: \"{1}\"")
     @DisplayName("정상 입력을 받으면 해당 줄을 읽어온다")
     void 정상_입력(String input, String expected) {
-        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
-        System.setIn(inputStream);
-        InputView inputView = new InputView();
+        systemIn(input);
 
         String read = inputView.read();
 
         assertThat(read).isEqualTo(expected);
+    }
+
+    @MethodSource(value = "provideWrongInput")
+    @ParameterizedTest
+    @DisplayName("잘못된 입력은 공백으로 이루어진 입력이다")
+    void 잘못된_입력(String input) {
+        systemIn(input);
+
+        assertThatThrownBy(() -> inputView.read())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void systemIn(final String input) {
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
     }
 
     private static Stream<Arguments> provideInputAndExpected() {
@@ -31,6 +53,13 @@ class InputViewTest {
                 Arguments.of(createInputWithLineSeparator(""), ""),
                 Arguments.of(createInputWithLineSeparator("1,2"), "1,2"),
                 Arguments.of(createInputWithLineSeparator("1,2;3"), "1,2;3")
+        );
+    }
+
+    private static Stream<Arguments> provideWrongInput() {
+        return Stream.of(
+                Arguments.of(createInputWithLineSeparator(" ")),
+                Arguments.of(createInputWithLineSeparator("   "))
         );
     }
 
