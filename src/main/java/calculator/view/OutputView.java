a177a6a58721f722efde139380041dbff6e1506f
jeonghyeon-ohk <ok6737@naver.com>
2024-10-19 16:59:34 +0900

    style(view): 출력 결과에 "결과 :" 문자열 추가

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,9 +1,8 @@
 package calculator.view;
-
 import camp.nextstep.edu.missionutils.Console;
 
 public class OutputView{
     public static void printResult(int result) {
-        System.out.print(result);
+        System.out.print("결과 : "+result);
     }
 }
