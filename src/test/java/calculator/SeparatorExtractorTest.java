c9748f91aa6aedf7aa7a5dcaa071e6fc7a2c3665
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-19 18:45:50 +0900

    test(SeparatorExtractor): 커스텀 구분자 선언전 사용 테스트

--- src/test/java/calculator/SeparatorExtractorTest.java
+++ src/test/java/calculator/SeparatorExtractorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,7 @@ class SeparatorExtractorTest {
     private final SeparatorExtractor extractor = new SeparatorExtractor(Constant.EXTRACT_REGEX_TEST);
 
     @Test
-    public void 커스텀_구분자_추출() {
+    void 커스텀_구분자_추출() {
         //given
         List<String> customSeparator = new ArrayList<>();
         String input = "//;\n4,5:6;//--\n1;2--//^^^\n1;2--3";
@@ -22,6 +23,18 @@ class SeparatorExtractorTest {
         assertThat(customSeparator.size()).isEqualTo(3);
         assertThat(customSeparator).containsExactly(";", "--", "^^^");
         assertThat(stringAfterExtract).isEqualTo("4,5:6;1;2--1;2--3");
+    }
+
+    @Test
+    void 커스텀_구분자_추출_실패_커스텀_구분자_선언전_사용() {
+        //given
+        List<String> customSeparator = new ArrayList<>();
+        String input = "1,2:3;4;5//;\n:6";
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> extractor.extractCustomSeparator(input, customSeparator),
+                "커스텀 구분자 선언이 사용보다 선행되어야합니다.");
+
     }
 
 }
