886ec9aec1fb9fae1edc936300b069b1540984bd
ykmxxi <ykmxxi97@gmail.com>
2024-10-19 13:04:23 +0900

    feat: 커스텀 구분자인지 판단한다

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -5,9 +5,15 @@ import java.util.regex.Pattern;
 public class Delimiter {
 
     private static final Pattern DEFAULT = Pattern.compile("[\\d*,|:?]*");
+    private static final Pattern CUSTOM = Pattern.compile("//(.)+\n(.)*");
 
     public boolean isDefault(final String input) {
         return DEFAULT.matcher(input)
+                .matches();
+    }
+
+    public boolean isCustom(final String input) {
+        return CUSTOM.matcher(input)
                 .matches();
     }
 
--- src/test/java/calculator/domain/DelimiterTest.java
+++ src/test/java/calculator/domain/DelimiterTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -28,6 +29,19 @@ class DelimiterTest {
     @DisplayName("기본 구분자 불일치: 쉼표(,) 또는 콜론(:)외 포함")
     void 기본_구분자_불일치(String input) {
         assertThat(delimiter.isDefault(input)).isFalse();
+    }
+
+    @ValueSource(strings = {"//;\n1;2;3", "// \n1 2 3", "//;\n1", "// \n"})
+    @ParameterizedTest(name = "\"{0}\"은 커스텀 구분자")
+    @DisplayName("커스텀 구분자 일치: //와 \n 사이에 1개 이상의 문자가 존재")
+    void 커스텀_구분자_일치(String input) {
+        assertThat(delimiter.isCustom(input)).isTrue();
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 불일치: 빈 문자열은 커스텀 구분자가 아님")
+    void 커스텀_구분자_불일치() {
+        assertThat(delimiter.isCustom("//\n")).isFalse();
     }
 
 }
