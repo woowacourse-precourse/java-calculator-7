d8e1408a8a477121f2c8cd49146448646981a939
dongri <l0o0lv@github.com>
2024-10-20 03:04:58 +0900

    refactor(ErrorMessages): ERROR_PARSE_INPUT 변수 삭제, ERROR_INPUT 값 변경

--- src/main/java/calculator/util/ErrorMessages.java
+++ src/main/java/calculator/util/ErrorMessages.java
@@ -5,7 +5,4 @@ public class ErrorMessages {
 
     public static final String ERROR_NUMBER_TYPE = "음수가 입력되었습니다.";
     public static final String ERROR_INPUT = "잘못된 입력입니다.";
-    public static final String ERROR_PARSE_INPUT = "기본 구분자는 ,(쉼표)와 :(콜론) 두 개만 사용 가능합니다.";
-
-
 }
