e7cec709bfb5a4dff5a6f0b9f41c8e941c530761
seonseon1515 <seonseon1515@gmail.com>
2024-10-19 19:59:44 +0900

    feat: 음수를 입력했을 경우의 예외 처리하기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,54 +6,60 @@ import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
-    // TODO: 프로그램 구현
+        // TODO: 프로그램 구현
+        // 문자열 입력 받기
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputStr = Console.readLine();
 
-    // 문자열 입력 받기
-    System.out.println("덧셈할 문자열을 입력해 주세요.");
-    String inputStr = Console.readLine();
+        // 입력한 문자열에서 기본 구분자, 커스텀 구분자 찾아내기
+        String customPolymerPattern = "//[^\\w\\s]";
+        String basicPolymerList = "[,:]";
+        String customPolymer = "";
 
-    // 입력한 문자열에서 기본 구분자, 커스텀 구분자 찾아내기
-    String customPolymerPattern = "//[^\\w\\s]";
-    String basicPolymerList = "[,:]";
-    String customPolymer = "";
+        Pattern pattern = Pattern.compile(customPolymerPattern);
+        Matcher matcher = pattern.matcher(inputStr);
 
-    Pattern pattern = Pattern.compile(customPolymerPattern);
-    Matcher matcher = pattern.matcher(inputStr);
+        if(matcher.find() && inputStr.substring(3,5).equals("\\n")){
+            customPolymer = inputStr.substring(2,3);
+        }
 
-    if(matcher.find() && inputStr.substring(3,5).equals("\\n")){
-        customPolymer = inputStr.substring(2,3);
-    }
+        // 기본 구분자 리스트에 커스텀 구분자 추가하기
+        String customPolymerList = "";
+        if(customPolymer.length() > 0){
+            customPolymerList = basicPolymerList.substring(0,basicPolymerList.length()-1) + customPolymer + "]";
+        }
 
-    // 기본 구분자 리스트에 커스텀 구분자 추가하기
-    String customPolymerList = "";
-    if(customPolymer.length() > 0){
-        customPolymerList = basicPolymerList.substring(0,basicPolymerList.length()-1) + customPolymer + "]";
-    }
+        // 입력한 문자열에서 구분자를 기준으로 숫자 문자열 분리시키기
+        String[] inputStrList;
+        if(customPolymer.length() == 1){
+            inputStrList = inputStr.substring(5,inputStr.length()).split(customPolymerList);
+        } else {
+            inputStrList = inputStr.split(basicPolymerList);
+        }
 
-    // 입력한 문자열에서 구분자를 기준으로 숫자 문자열 분리시키기
-    String[] inputStrList;
-    if(customPolymer.length() == 1){
-        inputStrList = inputStr.substring(5,inputStr.length()).split(customPolymerList);
-    } else {
-        inputStrList = inputStr.split(basicPolymerList);
-    }
+        // 분리된 문자열 숫자로 변환하기
+        int[] strToIntList = new int[inputStrList.length];
+        boolean isMinus = false;
+        int index = 0;
+        for (String str : inputStrList) {
+            strToIntList[index] = Integer.parseInt(str);
+            if (strToIntList[index] < 0){
+                isMinus = true;
+            }
+            index++;
+        }
 
-    // 분리된 문자열 숫자로 변환하기
-    int[] strToIntList = new int[inputStrList.length];
-    int index = 0;
-    for (String str : inputStrList) {
-        strToIntList[index] = Integer.parseInt(str);
-        index++;
-    }
+        if(isMinus){
+            throw new IllegalArgumentException("음수 입력 불가");
+        }
 
+        // 변환된 숫자를 더하기
+        int sum = 0;
+        for (int number : strToIntList) {
+            sum += number;
+        }
 
-    // 변환된 숫자를 더하기
-    int sum = 0;
-    for (int number : strToIntList) {
-        sum += number;
-    }
-
-    // 덧셈 결과 출력하기
-    System.out.println("결과 : " + sum);
+        // 덧셈 결과 출력하기
+        System.out.println("결과 : " + sum);
     }
 }
