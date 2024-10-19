09d2fd1bbb9cb814561686e449447aee7ea2d968
wodus4145@naver.com <wodus4145@naver.com>
2024-10-19 14:16:06 +0900

    feat: Creat Separate method
    Split the String using the delimiter(,:)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,17 +11,10 @@ public class Application {
         return  sum;
     }
 
-    public int solution(String str) throws IllegalArgumentException {
+    public int Separate(String str) {
         int answer = 0;
-        if(str.startsWith("//")) {
-            String delimiter = str.substring(2,str.indexOf("\n"));
-            String str2 = str.substring(str.indexOf("\n")+1);
-            String[] strArr = str2.split(delimiter);
-            answer = addition(strArr);
-        } else {
-            String[] strArr = str.split(",|:");
-            answer = addition(strArr);
-        }
+        String[] strArr = str.split(",|:");
+        answer = addition(strArr);
         return  answer;
     }
 
