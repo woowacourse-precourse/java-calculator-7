ee2ddb8cb4fca8aaf7895da4704e29e8cca4b8a2
ssggii <haan7525@gmail.com>
2024-10-19 17:29:20 +0900

    refactor: 커스텀 구분자 추출 메서드 삭제

--- src/main/java/calculator/DigitExtractor.java
+++ src/main/java/calculator/DigitExtractor.java
@@ -1,35 +1,33 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DigitExtractor {
 
-    private char[] delimiters = new char[3]; // 구분자
-
-    public DigitExtractor() {
-        delimiters[0] = ',';
-        delimiters[1] = ':';
-    }
+    private List<String> tokens = new ArrayList<>(); // 구분자 기준으로 분리한 문자열 토큰
 
     /**
-     * 문자열에서 숫자 추출
+     * TODO 문자열에서 숫자 추출
      */
     public List<Integer> extractDigits(String input) {
         return null;
     }
 
     /**
-     * 문자열의 유효성 검증
+     * TODO 문자열의 유효성 검증
      */
-    private boolean isValidateInput(String input) {
-        return false;
-    }
+    public boolean isValidateInput(String input) {
+        if (input.startsWith("//")) { // 커스텀 구분자 사용
 
-    /**
-     * 커스텀 구분자 설정
-     */
-    private void setCustomDelimiter(char customDelimiter) {
-        delimiters[3] = customDelimiter;
+        } else { // 기본 구분자만 사용
+            tokens = Stream.of(input.split("[,:]")).map(String::trim).collect(Collectors.toList());
+        }
+
+        return tokens.stream().noneMatch(token -> token.trim().matches(".*[^0-9].*"));
     }
 
 }
