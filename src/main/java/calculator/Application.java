158c75de77e4415ef92dd1e0a8b89c32ff539bf2
ehgk4245 <ehgkrk14@naver.com>
2024-10-20 01:26:21 +0900

    feat(extractDelimiter): 커스텀 구분자 추출기능추가
    "//(.)\n(.*)"패턴에서 구분자를 추출하여 반환한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,8 +2,22 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
+
+    public static final String DELIMITER_PATTERN = "//(.)\\\\n(.*)";
     public static void main(String[] args) {
         String input = Console.readLine(); //문자열 입력받기.
+    }
+
+    public static String extractDelimiter(String input){ // "//(.)\\\\n(.*)" 패턴에서 구분자 추출하여 반환.
+        Pattern pattern = Pattern.compile(DELIMITER_PATTERN);
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.find()){
+            return matcher.group(1);
+        }
+        return null;
     }
 }
