d7084b6427283b9df49b77f39e1255f67a77037c
suyeong jang <jangsuyeong923@naver.com>
2024-10-20 05:19:09 +0900

    feat(controller): 계산된 값 출력 로직 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,18 +1,24 @@
 package calculator.controller;
 
 
+import calculator.model.CalculatorService;
 import calculator.model.ValidationUtils;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 
 public class CalculatorController {
     private final InputView inputview = new InputView();
     private final ValidationUtils validationUtils = new ValidationUtils();
+    private final CalculatorService calculatorService = new CalculatorService();
+    private final OutputView outputView = new OutputView();
 
     public void run() {
         try {
             String input = inputview.input();
             validationUtils.validateInput(input);
+            int result = calculatorService.calculate(input);
+            outputView.output(result);
 
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(e.getMessage());
