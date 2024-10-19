881b078d3a990fe3000765ca164fb726d097509a
jeyong <118044367+joon6093@users.noreply.github.com>
2024-10-19 21:58:16 +0900

    feat: 필요한 기능을 외부에서 주입받도록 하여 변화에 쉽게 대응할 수 있도록 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.controller.converter.ExpressionToPositiveIntegerOperandConverter;
+import calculator.view.input.ConsoleInputView;
+import calculator.view.output.ConsoleOutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final ConsoleInputView inputView = new ConsoleInputView();
+        final ConsoleOutputView outputView = new ConsoleOutputView();
+        final ExpressionToPositiveIntegerOperandConverter converter = new ExpressionToPositiveIntegerOperandConverter();
+        final CalculatorController controller = new CalculatorController(inputView, outputView, converter);
+
+        controller.calculate();
     }
 }
