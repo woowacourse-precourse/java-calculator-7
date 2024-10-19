f8a07930eed7e3b5114cbb032079a913108cd741
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-10-19 14:54:50 +0900

    feat : 구분자를 통해 문자열에서 양수 추출시키기

---
+++ src/main/java/calculator/separator/DefaultSeparator.java
@@ -0,0 +1,26 @@
+package calculator.separator;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class DefaultSeparator {
+
+    public int[] separateStringToSum(String UserInput, SeparatorStorage separatorStorage){
+
+        String [] parsedStrings = UserInput.split(getRegex(separatorStorage));
+
+        int[] numbers = Arrays.stream(parsedStrings)
+                .mapToInt(Integer::parseUnsignedInt)
+                .toArray();
+
+        return numbers;
+    }
+
+    private static String getRegex(SeparatorStorage separatorStorage) {
+        return separatorStorage.getSeparators()
+                .stream()
+                .map(Pattern::quote)
+                .collect(Collectors.joining("|"));
+    }
+}
