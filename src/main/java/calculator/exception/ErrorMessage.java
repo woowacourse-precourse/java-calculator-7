970f454ebd3d6054160374a505a8d3d8a8b748f8
IM-GYURI <kgulr0517@gmail.com>
2024-10-19 11:32:31 +0900

    feat : add ErrorMessage
    - CONSECUTIVE_DELIMITERS

--- src/main/java/calculator/exception/ErrorMessage.java
+++ src/main/java/calculator/exception/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
     public static final String NULL_OR_EMPTY_INPUT = "입력 값은 null 또는 빈 값일 수 없습니다.";
     public static final String NEGATIVE_NUMBER = "음수는 입력할 수 없습니다.";
     public static final String INVALID_INPUT = "유효하지 않은 입력 값입니다.";
+    public static final String CONSECUTIVE_DELIMITERS = "연속된 구분자는 허용되지 않습니다.";
 }
