a92f742130d2259f8bf35433dadc928c84ec1270
junsoo0719 <junsoo0719@gmail.com>
2024-10-20 00:46:54 +0900

    fix: 예외 처리 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -44,7 +44,12 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
 
-        int result = stringAddCalculator.add(input);
-        System.out.println("결과 : " + result);
+        try {
+            int result = stringAddCalculator.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
