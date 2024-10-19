be42376cef95b46cb01cf02d600212bc3c15d0b2
reindeer002 <lsj000424@gmail.com>
2024-10-20 01:51:06 +0900

    refactor: due to srp and test code writing issues
    details:
    - many responsibilities, making it difficult to perform tests for extraction, validation, and calculation
    - ambiguous to identify the points that need to be modified when requirements change

--- src/main/java/calculator/service/impl/CalculatorServiceImpl.java
+++ src/main/java/calculator/service/impl/CalculatorServiceImpl.java
@@ -1,86 +1,16 @@
 package calculator.service.impl;
 
+import calculator.model.Operand;
 import calculator.service.CalculatorService;
-import java.util.regex.Pattern;
 
 public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
-    public int add(String input) throws IllegalArgumentException {
-        if (input == null || input.isEmpty()) {
-            return 0;
-        }
-
-        String delimiter = ",|:";
-
-        if (input.startsWith("//")) {
-            delimiter = extractCustomDelimiter(input);
-            input = input.substring(input.lastIndexOf("\\n") + 2);
-        }
-
-        validateCalculationString(input, delimiter);
-
-        String[] numbers = input.split(delimiter);
-        return sumNumbers(numbers);
-    }
-
-    private String extractCustomDelimiter(String input) {
-        int delimiterEndIndex = input.lastIndexOf("\\n");
-        if (delimiterEndIndex == -1) {
-            throw new IllegalArgumentException("Invalid input format");
-        }
-
-        String customDelimiter = input.substring(2, delimiterEndIndex);
-        if (customDelimiter.isEmpty()) {
-            return ",|:";
-        }
-
-        String[] delimitersArray = customDelimiter.split("");
-
-        StringBuilder delimiterPattern = new StringBuilder();
-        for (String delimiter : delimitersArray) {
-            if (isNumeric(delimiter)) {
-                throw new IllegalArgumentException("Custom delimiter cannot be a number: " + delimiter);
-            }
-            delimiterPattern.append(Pattern.quote(delimiter)).append("|");
-        }
-
-        delimiterPattern.append(",|:");
-
-        return delimiterPattern.toString();
-    }
-
-    private void validateCalculationString(String input, String delimiter) {
-        if (input.matches("^([" + delimiter + "]+).*") || input.matches(".*([" + delimiter + "]+)$")) {
-            throw new IllegalArgumentException("Input cannot start or end with a delimiter.");
-        }
-
-        if (input.matches(".*([" + delimiter + "]{2,}).*")) {
-            throw new IllegalArgumentException("Consecutive delimiters are not allowed.");
-        }
-
-        if (!delimiter.contains("-") && input.contains("-")) {
-            throw new IllegalArgumentException("Negative numbers are not allowed");
-        }
-    }
-
-    private int sumNumbers(String[] numbers) {
+    public int calculate(Operand operand) {
         int sum = 0;
-
-        for (String number : numbers) {
-            int num = Integer.parseInt(number);
+        for (int num : operand.getNumbers()) {
             sum += num;
         }
-
         return sum;
-    }
-
-    private boolean isNumeric(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
     }
 }
