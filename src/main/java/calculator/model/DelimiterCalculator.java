409a9cc5be2bbe150f2e91c2a5a344257de41c57
IM-GYURI <kgulr0517@gmail.com>
2024-10-19 11:32:16 +0900

    feat : add features
    calculate()
    - 입력 값 검증 추가
    
    isUsingDefaultDelimiter()
    - 기본 구분자 사용 여부 판단
    - (커스텀 구분자를 사용하지 않는지)
    
    validateDefaultInput()
    - 기본 구분자를 사용하는 입력 값을 검증하는 로직
    - 숫자와 기본 구분자로만 이루어진 문자열인지 판단
    - 구분자를 연속으로 사용하지는 않는지 판단 (e.g. 1,,3)

--- src/main/java/calculator/model/DelimiterCalculator.java
+++ src/main/java/calculator/model/DelimiterCalculator.java
@@ -1,14 +1,18 @@
 package calculator.model;
 
+import static calculator.exception.ErrorMessage.CONSECUTIVE_DELIMITERS;
 import static calculator.exception.ErrorMessage.INVALID_INPUT;
 import static calculator.exception.ErrorMessage.NEGATIVE_NUMBER;
 import static calculator.exception.ErrorMessage.NULL_OR_EMPTY_INPUT;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class DelimiterCalculator {
 
     private static final String DELIMITER_REGEX = "[,:]";
+    private static final Pattern INVALID_DEFAULT_INPUT_PATTERN = Pattern.compile("[^0-9,:]");
+    private static final Pattern CONSECUTIVE_DELIMITER_PATTERN = Pattern.compile("(,|:){2,}");
 
     /**
      * 사용자의 입력 값을 기반으로 계산
@@ -24,6 +28,10 @@ public class DelimiterCalculator {
 
         input = input.replace(" ", "");
 
+        if (isUsingDefaultDelimiter(input)) {
+            validateDefaultInput(input);
+        }
+
         String[] parts = splitInput(input);
 
         return Arrays.stream(parts)
@@ -31,6 +39,31 @@ public class DelimiterCalculator {
                 .sum();
     }
 
+    /**
+     * 기본 구분자 사용 여부 판단
+     *
+     * @param input 사용자의 입력 값
+     * @return 기본 구분자 사용 여부
+     */
+    private boolean isUsingDefaultDelimiter(String input) {
+        return !input.startsWith("//");
+    }
+
+    /**
+     * 기본 구분자를 사용하는 입력 값 검증 : 숫자와 기본 구분자로만 이루어진 문자열인지, 구분자를 연속으로 사용하지 않는지 판단
+     *
+     * @param input 사용자의 입력 값
+     * @throws IllegalArgumentException : INVALID_INPUT, CONSECUTIVE_DELIMITERS
+     */
+    private void validateDefaultInput(String input) {
+        if (INVALID_DEFAULT_INPUT_PATTERN.matcher(input).find()) {
+            throw new IllegalArgumentException(INVALID_INPUT);
+        }
+
+        if (CONSECUTIVE_DELIMITER_PATTERN.matcher(input).find()) {
+            throw new IllegalArgumentException(CONSECUTIVE_DELIMITERS);
+        }
+    }
 
     /**
      * 커스텀 구분자 존재 여부 판단 -> 구분자에 따라 split
