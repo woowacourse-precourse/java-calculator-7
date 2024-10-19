71d4d66735ce54f11e11601dd607b1aadf2d3669
gyuseon <mn5592@naver.com>
2024-10-19 18:55:35 +0900

    feat: 사용자가 입력한 문자열 읽는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+
+    private List<Character> delimiters = new ArrayList<>(Arrays.asList(',', ':'));
+    private boolean isCustomDelimiter = false;
+
+
+    public void startApplication() {
+        List<Long> numbers = new ArrayList<>();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+       long sum = 0;
+
+        System.out.println("결과 : " + sum);
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application app = new Application();
+        app.startApplication();
     }
+
 }
