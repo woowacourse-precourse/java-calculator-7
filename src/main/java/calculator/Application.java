caf8e9baa9199ac26baafdaae9dd6c3781752ef2
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-10-19 16:10:35 +0900

    :sparkles: feat($Application) add determine handling and input logic
    - readLine() 함수 사용하여 입력
    - "//" 와 "\n" 사이에 커스텀 구분자 처리
    - 커스텀 구분자가 없거나 잘못된 구분자가 있을 경우 IllegalArgumentException 발생
    - 커스텀 구분자가 없을 경우 기본 구분자로 계산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,30 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        String determine = "[,:]";
+
+        if (input.startsWith("//")) { // 커스텀 구분자 선언이 있을 경우
+            int endIndex = input.indexOf("\\n");
+            if (endIndex != -1) { // \n 이 있다면 커스텀 구분자로 업데이트
+                determine = input.substring(2, endIndex); // 커스텀 구분자 업데이트
+                input = input.substring(endIndex + 2);
+            } else { // \n 이 없을 경우 커스텀 선언이 안됨
+                throw new IllegalArgumentException();
+            }
+        }else if(!input.contains(determine)){
+            throw new IllegalArgumentException();
+        }
+
+        Calc c = new Calc();
+        int sum = c.calculate(input, determine);
+
+        System.out.println("결과 : " + sum);
     }
 }
