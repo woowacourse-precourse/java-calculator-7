5de9c71c60989393d9634a0d78c7692753d81ed6
heo5620 <tnqls1035@naver.com>
2024-10-19 12:34:38 +0900

    feat : 문자열에서 숫자 추출하기

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,12 +3,44 @@ package calculator;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class StringCalculator {
+
+    public static final String DEFAULT_DELIMITER = ",|:";
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\\\\n";
+
     public void run() {
         String input = askForStringInput();
+        String[] extractedNumbers = extractNumbersFrom(input);
+
+
     }
 
     private String askForStringInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return readLine();
     }
+
+    private String[] extractNumbersFrom(String input) {
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            return splitByCustomDelimiter(input);
+        }
+        return splitByDefaultDelimiter(input);
+    }
+
+    private static String[] splitByDefaultDelimiter(String input) {
+        return input.split(DEFAULT_DELIMITER);
+    }
+
+    private String[] splitByCustomDelimiter(String input) {
+        String[] splitedInput = input.split(CUSTOM_DELIMITER_SUFFIX);
+        String customDelimiter = extractCustomDelimiter(splitedInput[0]);
+        return splitedInput[1].split(customDelimiter);
+    }
+
+    private String extractCustomDelimiter(String input) {
+        int customDelimiterIndex = input.length() - 1;
+        char customDelimiter = input.charAt(customDelimiterIndex);
+        return String.valueOf(customDelimiter);
+    }
+
 }
