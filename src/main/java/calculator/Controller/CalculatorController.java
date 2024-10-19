57c978711f832b48795a9cc0fdf463e695068376
윤혜진 <lold2424@gmail.com>
2024-10-19 18:04:49 +0900

    refactor : 문제 조건인 IllegalArgumentException를 발생하도록 수정

--- src/main/java/calculator/Controller/CalculatorController.java
+++ src/main/java/calculator/Controller/CalculatorController.java
@@ -16,22 +16,19 @@ public class CalculatorController {
     }
 
     public void run() {
-        try {
-            String input = view.getInput();
-            if (input == null || input.isEmpty()) {
-                view.showResult(0); // 입력이 없을 때 0 출력
-                return;
-            }
 
-            // 구분자 처리 및 입력 검증
-            String[] inputArr = validator.processInput(input);
-
-            // 숫자 배열로 변환 후 모델에 전달하여 계산 수행
-            int[] inputIntArr = model.convertToIntArray(inputArr);
-            int sum = model.sum(inputIntArr);
-            view.showResult(sum);
-        } catch (IllegalArgumentException e) {
-            view.showError(e.getMessage()); // 예외 발생 시 오류 메시지 출력
+        String input = view.getInput();
+        if (input == null || input.isEmpty()) {
+            view.showResult(0); // 입력이 없을 때 0 출력
+            return;
         }
+
+        // 구분자 처리 및 입력 검증
+        String[] inputArr = validator.processInput(input);
+
+        // 숫자 배열로 변환 후 모델에 전달하여 계산 수행
+        int[] inputIntArr = model.convertToIntArray(inputArr);
+        int sum = model.sum(inputIntArr);
+        view.showResult(sum);
     }
 }
