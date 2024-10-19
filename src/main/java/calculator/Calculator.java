be923157ad8efaad7d0700383026e577d81a4fec
정소민 <yjk70702002@gmail.com>
2024-10-19 16:32:42 +0900

    feat: 덧셈 결과 출력하는 기능

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,12 +1,15 @@
 package calculator;
 
 import calculator.io.InputHandler;
+import calculator.io.OutputHandler;
 import calculator.splitor.Splitor;
 import calculator.util.SplitorExtractor;
 
 public class Calculator {
+    private final InputHandler inputHandler = new InputHandler();
+    private final OutputHandler outputHandler = new OutputHandler();
+    private final SplitorExtractor splitorExtractor = new SplitorExtractor();
     public void run() {
-        InputHandler inputHandler = new InputHandler();
         InputString inputString = inputHandler.getInputString();
 
         if (inputString == null || inputString.isBlank()) {
@@ -14,12 +17,10 @@ public class Calculator {
             return;
         }
 
-        SplitorExtractor splitorExtractor = new SplitorExtractor();
-
         Splitor splitor = splitorExtractor.extractSplitor(inputString);
         SplittedResult splittedResult = splitor.split();
         int sum = splittedResult.sum();
 
-
+        outputHandler.outputResult(sum);
     }
 }
---
+++ src/main/java/calculator/io/OutputHandler.java
@@ -0,0 +1,8 @@
+package calculator.io;
+
+public class OutputHandler {
+    public void outputResult(int sum) {
+        StringBuilder result = new StringBuilder();
+        System.out.println(result.append("결과 : ").append(sum));
+    }
+}
