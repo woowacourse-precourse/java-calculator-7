553d3e055a7452f08c1ddf6b0614a5d6e84570a2
minitiated <minitiated@gmail.com>
2024-10-19 22:28:21 +0900

    feat(application): add user input receiving function
    show console the first instruction message
    
    and redeveloped user input receiving function in application.main()

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        // 콘솔에서 사용자에게 덧셈할 문자열을 요청하는 메시지를 띄우고, 입력 받는 기능
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String userInput = Console.readLine();
+        System.out.println(userInput);
     }
 }
