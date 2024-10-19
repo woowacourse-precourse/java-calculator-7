7c410e81e3f3d79b9314548b08abb8879ee47dad
hgh1472 <hgh1472@naver.com>
2024-10-19 12:49:08 +0900

    refactor : 문자열 입력 요청 메세지 수정

--- src/main/java/calculator/constant/message/OutputMessage.java
+++ src/main/java/calculator/constant/message/OutputMessage.java
@@ -1,7 +1,7 @@
 package calculator.constant.message;
 
 public enum OutputMessage {
-    INPUT_STRING("덧셈할 문자열을 입력해주세요"),
+    INPUT_STRING("덧셈할 문자열을 입력해주세요."),
     RESULT("결과 : ");
 
     private final String message;
