3843c0d6ceb5e16228723f455e2922029e2853bf
ljg0610 <jggy0627@naver.com>
2024-10-19 23:29:58 +0900

    feat: 빈 문자열입력 처리

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Calculator {
+    public int calcString(String input){
+        if(input.isEmpty()){
+            return 0;
+        }
+
+        int result = 0;
+
+        return result;
+    }
+}
