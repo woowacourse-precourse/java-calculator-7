030a00f4202469e032e490f5b2c7f11b05099f99
jongseo <jongseo0703@naver.com>
2024-10-19 20:07:45 +0900

    문자열에서 숫자 추출 기능 구현 후 테스트 성공

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,5 +8,11 @@ public class Application {
         System.out.print("덧셈할 문자열을 입력해주세요 : ");
         String input = Console.readLine(); // 사용자의 값을 입력받음
 
+        StringCalculator calculator = new StringCalculator();
+
+        // 숫자 추출 테스트
+        System.out.println("입력된 문자열에서 추출된 숫자: " + calculator.extractNumToString(input));
+
+
     }
 }
