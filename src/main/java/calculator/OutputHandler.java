cfd9d80dc52eebf1b8b61386f8103c6b41772296
lvalentine6 <ysr1227@naver.com>
2024-10-19 15:39:39 +0900

    feat: OutputHandler impl

--- src/main/java/calculator/OutputHandler.java
+++ src/main/java/calculator/OutputHandler.java
@@ -1,4 +1,21 @@
 package calculator;
 
 public class OutputHandler {
+	private static final String PROMPT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+	private static final String RESULT_PREFIX = "결과 : ";
+
+	private OutputHandler() {
+	}
+
+	public static OutputHandler create() {
+		return new OutputHandler();
+	}
+
+	public void showPromptMessage() {
+		System.out.println(PROMPT_MESSAGE);
+	}
+
+	public void showCalculateResult(int sum) {
+		System.out.println(RESULT_PREFIX + sum);
+	}
 }
