11b326e0f532752a6fbd5a2bfb8897c1afdf6e62
howareyoo <newworld121@naver.com>
2024-10-20 01:25:54 +0900

    refactor : 문자열 검사 클래스 메소드 수정
    단일 반환 값 List 변환
    findCustomDelimiter 메서드 수정

--- src/main/java/calculator/StringValidator.java
+++ src/main/java/calculator/StringValidator.java
@@ -1,27 +1,63 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringValidator {
 
+    private StringValidator() {
+    }
+
+    public static List<String> userInputValidator(String userInput) {
+        List<Delimiter> delimiter = delimiterValidator(userInput);
+        return setDelimiter(userInput, delimiter);
+    }
+
+    public static List<String> setDelimiter(String userInput, List<Delimiter> delimiters) {
+        List<String> curDelimiterList = new ArrayList<>();
+        for (Delimiter delimiter : delimiters) {
+            if (isCustom(delimiter)) {
+                curDelimiterList.add(findCustomDelimiter(userInput));
+            } else {
+                stringFiter(userInput);
+                curDelimiterList.add(delimiter.getDelimiter());
+            }
+        }
+        return curDelimiterList;
+    }
+
+    public static boolean isCustom(Delimiter delimiter) {
+        return delimiter == Delimiter.CUSTOM_DOUBLE_SLASH;
+    }
+
     public static String findCustomDelimiter(String userInput) {
-        Pattern customPatter = Pattern.compile("//(.)\\n");
-        Matcher matcher = customPatter.matcher(userInput);
+        Pattern customPattern = Pattern.compile("//(.)\\\\n|//(.)\n");
+        Matcher matcher = customPattern.matcher(userInput);
 
         if (matcher.find()) {
-            return matcher.group(1);
+            String delimiter = matcher.group(1);
+            if (delimiter == null) {
+                delimiter = matcher.group(2);
+            }
+            if (delimiter != null) {
+                return delimiter;
+            } else {
+                throw new IllegalArgumentException("유효하지 않은 구분자입니다.");
+            }
         }
         return null;
     }
 
-    public static String delimiterValidator(String userInput) throws IllegalArgumentException {
+    public static List<Delimiter> delimiterValidator(String userInput) throws IllegalArgumentException {
+        List<Delimiter> delimiters = new ArrayList<>();
         for (Delimiter delimiter : Delimiter.values()) {
             if (userInput.contains(delimiter.getDelimiter())) {
-                return delimiter.getDelimiter();
+                delimiters.add(delimiter);
             }
         }
-        throw new IllegalArgumentException();
+        return delimiters;
     }
 
     public static boolean stringFiter(String userInput) throws IllegalArgumentException {
