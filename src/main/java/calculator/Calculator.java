762a07df8dd2adc344525674196fd84822738d86
sobaman <sobaman522@gmail.com>
2024-10-19 21:15:00 +0900

    feat(calculator): add new 커스텀 구분자를 설정하여 작동하는 customConvert 기능 구현
    - 문자열 맨 처음 "//" 와 "\n" 사이에 위치한 문자를 구분자로 사용 가능
    - "//" "\n" 사이에 위치하라는 형식을 지키지 않았을 경우 IllegalArgumentException 발생
    - 설정한 구분자를 사용하지 않았을 경우 IllegalArgumentException 발생

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,8 +3,7 @@ public class Calculator {
 
     public int convert(String str){
         if(str.startsWith("//")){
-            //todo customConvert
-            return 0;
+            return customConvert(str);
         } else {
             return defaultConvert(str);
         }
@@ -18,6 +17,37 @@ public class Calculator {
                 result += Integer.parseInt(s);
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("잘못된 값을 입력하였습니다, 구분자로 \",\" 나 \":\" 를 사용하세요");
+            }
+        }
+        return result;
+    }
+
+    private int customConvert(String str) {
+        String custom;
+        int result = 0;
+        int idx = str.indexOf("\n");
+
+        if(idx == -1) {
+            throw new IllegalArgumentException("잘못된 형식입니다, 커스텀 구분자를 \"\\\" \"\n\" 사이에 넣으세요");
+        }
+        // 커스텀이 하나일경우
+        custom = str.substring(2, idx);
+        // 커스텀이 여러개일경우
+        if(custom.length() > 1){
+            String[] tmp = custom.split("");
+            custom = tmp[0];
+            for(int i=1; i<tmp.length; i++){
+                custom += "|" + tmp[i];
+            }
+        }
+        String substring = str.substring(str.lastIndexOf("\n") + 1);
+        String[] split = substring.split(custom);
+
+        for (String s : split) {
+            try {
+                result += Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("잘못된 값을 입력하였습니다, 설정한 구분자를 사용하세요");
             }
         }
         return result;
