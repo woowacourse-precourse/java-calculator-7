142fe4f48b454549f2120d87883eab42d50c6ec9
SunHee <76764643+sunni546@users.noreply.github.com>
2024-10-19 19:18:52 +0900

    feat: 기본 구분자 사용 예외 처리

--- src/main/java/calculator/validator/InputValidator.java
+++ src/main/java/calculator/validator/InputValidator.java
@@ -9,5 +9,23 @@ public class InputValidator {
         if (!input.startsWith("//") && !Character.isDigit(input.charAt(0))) {
             throw new IllegalArgumentException();
         }
+
+        if (Character.isDigit(input.charAt(0))) {
+            validateBasicSeparator(input);
+        }
+    }
+
+    private static void validateBasicSeparator(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (!Character.isDigit(c) && c != ',' && c != ':') {
+                throw new IllegalArgumentException();
+            }
+
+            if (i == input.length() - 1 && !Character.isDigit(c)) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
