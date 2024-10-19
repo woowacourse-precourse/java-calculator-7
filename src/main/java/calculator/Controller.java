f8fb64f9773c0e21729f79bb19ab2603f7bee77d
Imseonghun <daltjdgns123@gmail.com>
2024-10-20 00:30:53 +0900

    refactor(Controller) : 입력 기능 리팩토링

---
+++ src/main/java/calculator/Controller.java
@@ -0,0 +1,12 @@
+package calculator;
+import camp.nextstep.edu.missionutils.Console;
+
+public class Controller {
+  public static void run() {
+    String input = Console.readLine();
+
+    int result = 0;
+
+    System.out.println(result);
+  }
+}
