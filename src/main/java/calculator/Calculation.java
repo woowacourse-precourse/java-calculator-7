962bf5db005d65934e61d1d8abbcea0179d15af3
홍성문 <2020041412@hanyang.ac.kr>
2024-10-19 11:04:22 +0900

    feat: V. class made

---
+++ src/main/java/calculator/Calculation.java
@@ -0,0 +1,16 @@
+package calculator;
+
+import java.util.ArrayList;
+
+public class Calculation {
+    private ArrayList<Integer> valList;
+    private ArrayList<String> separatorList;
+
+    public Calculation()
+    {
+        this.valList = new ArrayList<>();
+        this.separatorList = new ArrayList<>();
+    }
+
+
+}
---
+++ src/main/java/calculator/IOManager.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class IOManager {
+
+    public String inputCalculationString()
+    {
+        return null;
+    }
+
+    public void printCalculation()
+    {
+
+    }
+}
