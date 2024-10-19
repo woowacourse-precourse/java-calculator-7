af1a7a0d24d431173d79c69d3329bcdc6d2c5086
Mirim <marinrim23@gmail.com>
2024-10-19 11:08:24 +0900

    chore : Rearrange functions of the DelimiterProcess to improve a legibility
    just change the position of functions.

--- src/main/java/calculator/DelimiterProcess.java
+++ src/main/java/calculator/DelimiterProcess.java
@@ -15,19 +15,6 @@ public class DelimiterProcess {
         delimitersInit.add(":");
     }
 
-    //addDelimiter(): 문자열에서 커스텀 구분자를 분리 -> 구분자 리스트에 추가, userInput 잘라서 초기화
-    private void addDelimiter(){
-        int endDelimIndex = userInput.indexOf("\\n");
-
-         if (endDelimIndex == -1) {
-             throw new IllegalArgumentException("잘못된 구분자 형식입니다. \\n이 누락되었습니다.");
-         }
-
-         String customDelimiter = userInput.substring(2, endDelimIndex);
-         delimitersInit.add(customDelimiter);
-         userInput = userInput.substring(endDelimIndex + 2);
-    }
-
     //getDelimiter(): 커스텀 구별자가 없을 때까지 반복 -> 커스텀 구별자를 ArrayList에 넣기
     public List<String> getDelimiters() {
         isNumberStarting();
@@ -40,6 +27,30 @@ public class DelimiterProcess {
         return delimitersInit;
     }
 
+    //isNumberString(): true일 때 커스텀 구별자가 모두 끝난 것
+    private void isNumberStarting(){
+        if(userInput.startsWith("//")){
+            delimStarting = true;
+        } else {
+            delimStarting = false;
+        }
+    }
+
+    //addDelimiter(): 문자열에서 커스텀 구분자를 분리 -> 구분자 리스트에 추가, userInput 잘라서 초기화
+    private void addDelimiter(){
+        int endDelimIndex = userInput.indexOf("\\n");
+
+        //"\\n"을 찾지 못했을 때 예외 처리
+         if (endDelimIndex == -1) {
+             throw new IllegalArgumentException("잘못된 구분자 형식입니다. \\n이 누락되었습니다.");
+         }
+
+         String customDelimiter = userInput.substring(2, endDelimIndex);
+         delimitersInit.add(customDelimiter);
+         userInput = userInput.substring(endDelimIndex + 2);
+    }
+
+
     //getUserInput(): 구분자를 모두 처리한 문자열만을 반환
     public String getUserInput() {
         userInput = userInput.trim();
@@ -49,12 +60,4 @@ public class DelimiterProcess {
         return userInput;
     }
 
-    //isNumberString(): true일 때 커스텀 구별자가 모두 끝난 것
-    private void isNumberStarting(){
-        if(userInput.startsWith("//")){
-            delimStarting = true;
-        } else {
-            delimStarting = false;
-        }
-    }
 }
