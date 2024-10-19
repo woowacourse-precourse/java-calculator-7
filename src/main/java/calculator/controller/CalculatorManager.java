c1c1f3ae72f4e85712d0c48ca94d5db250182369
TaegeunYou <dbxorms2310@naver.com>
2024-10-19 11:08:01 +0900

    feat(CalculatorManager): 숫자 추출해서 총합을 구하는 메인 컨트롤러 구현

--- src/main/java/calculator/controller/CalculatorManager.java
+++ src/main/java/calculator/controller/CalculatorManager.java
@@ -1,21 +1,31 @@
 package calculator.controller;
 
+import calculator.domain.Numbers;
 import calculator.dto.SeparatorsResult;
 import calculator.extractor.CustomSeparatorExtractor;
+import calculator.extractor.NumbersExtractor;
 import calculator.view.InputView;
+import calculator.view.OutputView;
+import java.math.BigDecimal;
 
 public class CalculatorManager {
     private final InputView inputView;
+    private final OutputView outputView;
     private final CustomSeparatorExtractor customSeparatorExtractor;
+    private final NumbersExtractor numbersExtractor;
 
     public CalculatorManager() {
         this.inputView = new InputView();
+        this.outputView = new OutputView();
         this.customSeparatorExtractor = new CustomSeparatorExtractor();
+        this.numbersExtractor = new NumbersExtractor();
     }
 
     public void run() {
         String input = inputView.enterString();
 
         SeparatorsResult separatorsResult = customSeparatorExtractor.getSeparatorsResult(input);
+        Numbers numbers = numbersExtractor.extractNumbers(separatorsResult);
+        BigDecimal sum = numbers.sum();
     }
 }
