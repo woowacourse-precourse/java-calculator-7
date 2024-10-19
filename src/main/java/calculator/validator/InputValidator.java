bf2fbbddc176f033b6810e399c768723fa9331db
SunHee <76764643+sunni546@users.noreply.github.com>
2024-10-20 01:15:14 +0900

    feat: 커스텀 구분자 사용 예외 처리

--- src/main/java/calculator/validator/InputValidator.java
+++ src/main/java/calculator/validator/InputValidator.java
@@ -12,6 +12,8 @@ public class InputValidator {
 
         if (Character.isDigit(input.charAt(0))) {
             validateBasicSeparator(input);
+        } else if (input.startsWith("//")) {
+            validateCustomSeparator(input);
         }
     }
 
@@ -24,6 +26,36 @@ public class InputValidator {
             }
 
             if (i == input.length() - 1 && !Character.isDigit(c)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private static void validateCustomSeparator(String input) {
+        int index = input.indexOf("\\n");
+
+        if (index < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (!Character.isDigit(input.charAt(input.length() - 1))) {
+            throw new IllegalArgumentException();
+        }
+
+        String separator = input.substring(2, index);
+
+        if (separator.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else if (separator.length() > 1) {
+            throw new IllegalArgumentException();
+        } else if (Character.isDigit(separator.charAt(0))) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = index + 2; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (!Character.isDigit(c) && c != separator.charAt(0)) {
                 throw new IllegalArgumentException();
             }
         }
