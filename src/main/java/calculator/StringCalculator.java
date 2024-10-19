935bbddb1677ba43bda104c7cc14b39bf3a0acdb
woongjee <jeewoong7753@naver.com>
2024-10-19 09:31:59 +0900

    feat(StringCalculator):입력 문자열을 더하는 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -11,5 +11,21 @@ public class StringCalculator {
         this.delimiter = new CustomDelimiter(customDelimiter);
     }
 
+    public int add(String input) {
+        if(input == null||input.isEmpty()) {
+            return 0;
+        }
+        if(input.startsWith("//"){
+            int delimiterIndex = input.indexOf("\n");
+            if(delimiterIndex == -1) {
+                throw new IllegalArgumentException("잘못된 입력입니다");
+            }
+            String CustomDelimiter = input.substring(2,delimiterIndex);
+            this.delimiter = new CustomDelimiter(CustomDelimiter);
+            input = input.substring(delimiterIndex+1);
+        }
+        String[] tokens = delimiter.split(input);
+        return sumNumbers(tokens);
+    }
 
 }
