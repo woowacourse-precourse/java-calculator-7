c95eb1df6e79c3974602f83395b2d8635a7d6026
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-10-19 22:12:30 +0900

    feat: 커스텀 구분자 지정 기능 구현
    - 커스텀 구분자 지정 기능에 대한 테스트를 추가하였다.
    - Assertions.assertAll, ParameterizedTest, 등을 이용하여 다양한 방식의 테스트를 진행하였다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,9 +2,12 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class Application {
+    private final static String OR = "|";
     private final static String DEFAULT_SEPARATOR = ",|:";
 
     public static void main(String[] args) {
@@ -17,7 +20,30 @@ public class Application {
             System.out.println("결과 : 0");
             return;
         }
+
+        // 커스텀 구분자를 감싸는 정규식
+        String customKeywordRegex = "^//(.*)\\\\n";
+        Matcher customKeywordMatcher = getMatcher(customKeywordRegex, input);
+
         String separator = DEFAULT_SEPARATOR;
+        if (customKeywordMatcher.find()) {
+            separator = customKeywordMatcher.group(1);
+            // 숫자인지 판별
+            Matcher numberMatcher = getMatcher("[0-9]", separator);
+            if(numberMatcher.find()) {
+                throw new IllegalArgumentException("숫자는 커스텀 구분자로 사용할 수 없습니다.");
+            }
+            if(separator.length() != 1) {
+                throw new IllegalArgumentException("커스텀 구분자는 1개의 1Byte 문자만 지정할 수 있습니다. (추출한 구분자:"+separator+")");
+            }
+            // 입력문자열 앞부분의 커스텀 지정 부분 제거
+            input = input.replaceFirst(customKeywordRegex, "");
+            // 커스텀 구분자 추가 지정
+            separator = DEFAULT_SEPARATOR + OR + separator;
+        }
+
+        if (!input.matches("[0-9" + separator + "]*")) {
+            throw new IllegalArgumentException("문자열에 지정된 구분자 이외의 문자가 포함되어 있습니다.");
         }
 
         String[] str = input.split(separator);
@@ -32,5 +58,19 @@ public class Application {
                 });
 
         System.out.println("결과 : "+ numbers.sum());
+
+    }
+
+    /**
+     * 주어진 정규 표현식과 입력 문자열에 대해 Matcher 객체를 반환한다.<br/>
+     * 제공된 정규 표현식을 Pattern으로 컴파일하고, 그 Pattern을 기반으로 입력 문자열에 대한 Matcher를 생성한다.
+     * @param regex 컴파일할 정규 표현식
+     * @param input 정규 표현식에 대해 매칭할 입력 문자열
+     * @return 입력 문자열에 대한 매칭 작업을 수행할 수 있는 Matcher 객체
+     */
+    private static Matcher getMatcher(String regex, String input) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher;
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,17 +1,100 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    @Test
-    void 커스텀_구분자_사용() {
+
+    @Nested
+    @DisplayName("커스텀 구분자 테스트")
+    class CustomSeparatorTest{
+
+        // ==== Assertions.assertAll 적용 ====
+        @Test
+        void 커스텀_구분자_사용() {
+            Assertions.assertAll(
+                    () -> {
+                        run("//;\\n1");
+                        assertThat(output()).contains("결과 : 1");
+                    },
+                    () -> assertThatThrownBy(() -> runException("//😀\\n1😀2😀3"))
+                            .isInstanceOf(IllegalArgumentException.class),
+                    () -> assertThatThrownBy(() -> runException("//ㄱㄱ\\n1ㄱㄱ2ㄱㄱ3"))
+                            .isInstanceOf(IllegalArgumentException.class),
+                    () -> assertThatThrownBy(() -> runException("//2\\n12325"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+
+        }
+
+        // ==== ValueSource 적용해보기 ====
+        @ParameterizedTest
+        @ValueSource(strings = { "//;\\n1;2;3", "//ㅎ\\n1ㅎ2ㅎ3", "//ㄱ\\n1ㄱ2:3", "//n\\n1n2:2,1", "//-\\n1-2-2,1"})
+        void 가능한_커스텀_구분자_사용(String input) {
+            assertSimpleTest(() -> {
+                run(input);
+                assertThat(output()).contains("결과 : 6");
+            });
+        }
+
+        // ==== MethodSource 적용해보기 ====
+        static Stream<Arguments> customInputs() {
+            return Stream.of(
+                    Arguments.of("지정되지 않은 커스텀 문자 사용", "//;\\n1;2ㄱ3"),
+                    Arguments.of("이모티콘 구분자 사용", "//😀\\n1😀2😀3"),
+                    Arguments.of("두글자 구분자 사용", "//ㄱㄱ\\n1ㄱㄱ2:3"),
+                    Arguments.of("숫자 구분자 사용", "//2\\n12325")
+            );
+        }
+        @ParameterizedTest(name = "{index} - {0}")
+        @MethodSource("customInputs")
+        void 불가능한_커스텀_구분자_사용(String description, String input) {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(input))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+
+        // ==== CsvSource 적용해보기 ====
+        /*
+        @ParameterizedTest(name = "{index} - {0}")
+        @CsvSource({
+                "'지정되지 않은 커스텀 문자 사용', '//;\\n1;2ㄱ3'",
+                "'이모티콘 구분자 사용', '//😀\\n1😀2😀3'",
+                "'중복 문자 구분자 사용', '//ㄱㄱ\\n1ㄱㄱ2:3'",
+                "'숫자 구분자 사용', '//2\\n12325'"
+        })
+        void 불가능한_커스텀_구분자_사용_테스트3(String description, String input) {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(input))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+         */
+
+        @Test
+        void 커스텀_구분자를_음수기호로_지정하고_음수를_입력한_경우(){
+            // [1, 2, null, 3] 으로 분리하게 되므로, 숫자가 아니라는 오류가 발생해야 한다.
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("//-\\n1,2,-3"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+    }
+
+
     @ParameterizedTest(name = "{index} - {0}")
     @CsvSource({
             "'기본 구분자 혼합(: ,) 사용', '1:2,3'",
