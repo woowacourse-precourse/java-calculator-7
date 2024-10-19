f95d9ca5b3e4043bbb5350737b16324e715fb9ee
Kyuwon-Choi <kyuwon30@naver.com>
2024-10-19 21:35:39 +0900

    feat: 커스텀 문자열 기능 구현

--- src/main/java/calculator/util/StringParser.java
+++ src/main/java/calculator/util/StringParser.java
@@ -2,11 +2,26 @@ package calculator.util;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringParser {
 
-    public static List<String> parseInput(final String input) {
+    private static final String DEFAULT_DELIMITER = ",|:";
 
-        return Arrays.asList(input.split(",|:"));
+    public static List<String> parseInput(final String input) {
+        String delimiter = DEFAULT_DELIMITER;
+
+        if (input.startsWith("//") && input.contains("\\n")) {
+            delimiter += extractCustomDelimiters(input);
+        }
+
+        return Arrays.asList(input.substring(input.indexOf("\\n") + 2).split(delimiter));
+    }
+
+    private static String extractCustomDelimiters(String input) {
+        String customDelimiters = input.substring(2, input.indexOf("\\n"));
+        return Arrays.stream(customDelimiters.split(""))
+                .map(c -> "\\" + c)
+                .collect(Collectors.joining("|"));
     }
 }
