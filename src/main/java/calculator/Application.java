58a53fd324827ce14282ee4ad8d6c09b8aff8de5
99hyeon <hyeon99@ajou.ac.kr>
2024-10-19 14:13:00 +0900

    feat: 기능3, 4 구현 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,16 @@
 package calculator;
 
 
+import java.util.LinkedList;
+import java.util.regex.Pattern;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
 
     public static void main(String[] args) {
 
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputString = readLine();
 
         if(!checkEmpty(inputString)){
@@ -14,13 +18,26 @@ public class Application {
         }
 
         char separator;
+        String[] numArray = null;
         if(inputString.charAt(0) == '/'){
             separator = checkCustom(inputString);
 
             if(separator == '0'){
                 throw new IllegalArgumentException("IllegalArgumentException 발생");
+            }else{
+                inputString = inputString.substring(5);
+                numArray = inputString.split(",|:|" + Pattern.quote(Character.toString(separator)));
             }
+        }else{
+            numArray = inputString.split(",|:");
         }
+
+        LinkedList<Integer> numbers = checkNumber(numArray);
+        int sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
+        }
+        System.out.println("결과 : " + sum);
     }
 
     static boolean checkEmpty(String string){
@@ -44,5 +61,19 @@ public class Application {
         }
 
         return '0';
+    }
+
+    static LinkedList<Integer> checkNumber(String[] numArray){
+        LinkedList<Integer> numbers = new LinkedList<>();
+        for (String s : numArray) {
+            try{
+                int tmp = Integer.parseInt(s);
+                numbers.add(tmp);
+            }catch (NumberFormatException e){
+                throw new IllegalArgumentException("문자열 정수로 변환 불가");
+            }
+        }
+
+        return numbers;
     }
 }
