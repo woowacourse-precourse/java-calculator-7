b2bab5980a9d7158396f7fb5fff3cc0e1023ee76
kingrangE <yuvw123@gmail.com>
2024-10-20 03:27:56 +0900

    fix(model): update Delimeter for finding custom delimeter
    개행문자로 인식하여 커스텀 구분자를 찾지 못하여 해당 부분을 수정하였습니다.

--- src/main/java/calculator/model/Delimeter.java
+++ src/main/java/calculator/model/Delimeter.java
@@ -4,16 +4,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Delimeter {
-    public ArrayList<String> values = new ArrayList<String>(Arrays.asList(",", ":"));
+    private ArrayList<String> values = new ArrayList<String>(Arrays.asList(",", ":"));
 
     public Delimeter(){}
 
     public String setDelimeter(String input){
-        if(input.startsWith("//") && input.indexOf("\n")==3){
+
+        if(input.startsWith("//")&& input.indexOf("\\n")==3){
             // //로 시작하고 문자 하나 오고, \n로 닫히는 경우 -> 사이에 있는 것은 Custom Delimeter
             String customDelimeter = Character.toString(input.charAt(2));
             values.add(customDelimeter);
-            return input.replace("//"+customDelimeter+"\n", "");
+            return input.replace("//"+customDelimeter+"\\n", "");
         }
         return input;
     }
