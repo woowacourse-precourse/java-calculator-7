06e5cc31f1870b81a1ad0f9096ca492a37855a91
whatsgoodg <rlwjddl1596@naver.com>
2024-10-19 20:11:17 +0900

    test(TotalDelimitedInputTest): TotalDelimitedInputTest 단위 테스트 수행
    TotalDelimitedInput 객체의 fromCustomDelimiterParseInput 매서드를 테스트
    - CustomDelimiterParser의 parse 수행
    - fromCustomDelimiterParseInput 매서드를 통해 TotalDelimitedInput 객체
      생성
    - 커스텀 구분자와 기본 구분자를 모두 포함하는지 확인

---
+++ src/test/java/calculator/front/input/TotalDelimitedInputTest.java
@@ -0,0 +1,24 @@
+package calculator.front.input;
+
+import calculator.front.parser.CustomDelimiterParser;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class TotalDelimitedInputTest {
+
+    @Test
+    @DisplayName("CustomDelimiterParser 객체가 TotalDelimitedInput으로 반환되어야 함.")
+    void 완전_구분된_입력값_테스트() {
+        //given
+        String rawInput = "//;\\n1:2:3";
+        //when
+        CustomDelimiterParsedInput customDelimiterParsedInput = CustomDelimiterParser.parse(rawInput);
+        TotalDelimitedInput totalDelimitedInput = TotalDelimitedInput.fromCustomDelimiterParseInput(customDelimiterParsedInput);
+        Character[] customDelimiters = customDelimiterParsedInput.delimiters().toArray(new Character[0]);
+        //then
+        assertThat(totalDelimitedInput.getDelimiters()).containsAnyOf(customDelimiters);
+        assertThat(totalDelimitedInput.getDelimiters()).containsExactly(':', ',', ';');
+        assertThat(totalDelimitedInput.getInputWithDelimiters()).isEqualTo(customDelimiterParsedInput.inputWithDelimiter());
+    }
+}
