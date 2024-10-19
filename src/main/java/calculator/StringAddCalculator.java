02bc489c1fa5eaec49b5820908d7e1beb9a294db
yanghyen <sk0310803@naver.com>
2024-10-19 22:43:26 +0900

    feat(calculator): 커스텀 구분자 형식 유효성 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -9,4 +9,15 @@ public class StringAddCalculator {
     System.out.println("덧셈할 문자열을 입력해 주세요.");
     String userInput = Console.readLine();
   }
+
+  // 커스텀 구분자 형식 유효성 검사
+  private static void validateInput(String userInput) throws IllegalAccessException {
+    if (userInput.startsWith("//")) {
+      int delimiterIndex = userInput.indexOf('\n');
+      String numbersPart = userInput.substring(delimiterIndex + 1);
+      if (numbersPart.isEmpty()) {
+        throw new IllegalAccessException("잘못된 구분자 형식입니다.");
+      }
+    }
+  }
 }
