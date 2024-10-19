c579bf70908a28028ee352f160d02d0b03983b7a
Park Yu Han <pbk2312@naver.com>
2024-10-19 17:59:10 +0900

    refactor(model): String[] 대신 List<String>을 반환하도록 수정

--- src/main/java/calculator/model/StringParser.java
+++ src/main/java/calculator/model/StringParser.java
@@ -1,13 +1,16 @@
 package calculator.model;
 
 import calculator.util.DelimiterUtils;
+import java.util.Arrays;
+import java.util.List;
 
 // 입력 문자열을 파싱하는 클래스
 public class StringParser {
 
-    // 입력 문자열을 파싱하여 숫자 배열을 반환하는 메서드
-    public String[] parseInput(String input) {
-        return DelimiterUtils.splitByDelimiter(input); // DelimiterUtils의 메서드를 호출하여 구분 처리
+    // 입력 문자열을 파싱하여 숫자 리스트를 반환하는 메서드
+    public List<String> parseInput(String input) {
+        String[] numbersArray = DelimiterUtils.splitByDelimiter(input); // 배열을 반환하는 유틸리티 메서드 호출
+        return Arrays.asList(numbersArray);  // 배열을 List로 변환하여 반환
     }
 
 }
