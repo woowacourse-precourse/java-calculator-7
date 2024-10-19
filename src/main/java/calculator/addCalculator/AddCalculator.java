3676161ea57c8c187484fd442d1b2b12f3e0c7c4
Hyso <hsk7953@gmail.com>
2024-10-20 00:07:10 +0900

    :sparkles: fix : calculateSum 메서드 빈 문자열 처리 로직 추가 - 빈 문자열을 왔을 경우 무시하고 처리 - 전부 빈 문자열일 경우는 0을 반환

--- src/main/java/calculator/addCalculator/AddCalculator.java
+++ src/main/java/calculator/addCalculator/AddCalculator.java
@@ -1,13 +1,15 @@
 package calculator.addCalculator;
 
-import calculator.stringSplitter.StringSplitter;
-import camp.nextstep.edu.missionutils.Console;
-
 public class AddCalculator {
     public int calculateSum(String[] numbers){
         int sum = 0;
+
         for(String number : numbers){
-            int num = Integer.parseInt(number.trim());
+            String trimmedNumber = number.trim();
+            if(trimmedNumber.isEmpty()){
+                continue;
+            }
+            int num = Integer.parseInt(trimmedNumber);
             if(num < 0){
                 throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
             }
