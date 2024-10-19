2c64e497a06d1e087859809f99aa7905a916ffe3
ljg0610 <jggy0627@naver.com>
2024-10-20 00:21:21 +0900

    feat: 커스텀 구분자 처리 후 계산값 반환

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,23 +5,32 @@ public class Calculator {
     private static final String COLON = ":";
 
     public int calcString(String input){
-        if(input.isEmpty()){
+        if(input.isEmpty()) {
             return 0;
         }
 
-        if(input.startsWith("//")){
-            return 0;
+        if(input.startsWith("//")) {
+            return calcWithCustom(input);
         }else {
             return calcWithDefault(input);
         }
     }
 
-    private int calcWithDefault(String input){
+    private int calcWithDefault(String input) {
         String[] numbers = input.split(COMMA + "|" + COLON);
         return calcSum(numbers);
     }
 
-    private int calcSum(String[] numbers){
+    private int calcWithCustom(String input) {
+        int delimiterEnd = input.indexOf("\\n");
+        String delimiter = input.substring(2,delimiterEnd);
+        String numbersString = input.substring(delimiterEnd + 2);
+
+        String[] numbers = numbersString.split(delimiter);
+        return calcSum(numbers);
+    }
+
+    private int calcSum(String[] numbers) {
         int result = 0;
 
         for(String num : numbers){
