c2e6bdf42f38a81c62911a1f97cf21e7ec8e8d31
홍성문 <2020041412@hanyang.ac.kr>
2024-10-19 14:03:36 +0900

    refactor: check print() deletion

--- src/main/java/calculator/Calculation.java
+++ src/main/java/calculator/Calculation.java
@@ -27,12 +27,7 @@ public class Calculation {
             input = checkCustomSperator(input);
         }
 
-        // 중간점검
-        System.out.println(input);
-        System.out.println(separatorList);
-
         // 구분자로 숫자 분리
-        //String[] tokens = input.split();
         String tmp = "";
         for (int i = 0; i < input.length(); i++) {
             char currentChar = input.charAt(i); // 현재 문자를 가져옴
@@ -52,8 +47,6 @@ public class Calculation {
             }
         }
         valList.add(Integer.parseInt(tmp)); // 마지막 value 추가
-        System.out.println(valList);
-
     }
 
     // 커스텀 구분자가 있는 경우 해당 구분자를 추가
