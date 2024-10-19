938e3f7668d50d9eb2760cee9b0819b17ab94a4c
윤진완 <yunjin-wan@yunjin-wan-ui-MacBookAir.local>
2024-10-19 17:33:07 +0900

    refactor: Parser클래스 기능

---
+++ src/main/java/calculator/Parser.java
@@ -0,0 +1,37 @@
+package calculator;
+
+import java.util.regex.Pattern;
+
+public class Parser {
+    private String delimiter = ",|:";
+
+    public int calculateSentence(String input) {
+        String[] tokens = input.split(delimiter);
+        int sum = 0;
+        for (String token : tokens) {
+            if(!token.isEmpty()) {
+                try {
+                    int num = parseNumber(token);
+                    sum += num;
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("숫자가 아닙니다.");
+                }
+            }
+        }
+        return sum;
+
+    }
+    public int parseNumber(String token) {
+        int num = Integer.parseInt(token);
+        if(num <0) {
+            throw new IllegalArgumentException("양수만 가능합니다.");
+        }
+        return num;
+    }
+
+    public void updateDelimiter(String customDelimiter){
+        delimiter = delimiter + "|" + Pattern.quote(customDelimiter);
+    }
+
+}
+
