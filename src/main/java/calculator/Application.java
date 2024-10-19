64c5467ad576d788a41d30dbb6463f52805549d9
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-10-19 16:05:41 +0900

    feat: 기본 구문주와 커스텀 구문자 처리 기능 함수

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,17 +6,48 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Application {
-    public static void main(String[] args) {
-        String question = "";
-        int answer = 0;
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        question = Console.readLine();
+    public static String insert(){
+        String question = Console.readLine();
         Console.close();
-        if(question.equals("")){
+        return question;
+    }
+    public static void checkMinus(int i1){
+        if(i1 < 0)  {
+            throw new IllegalArgumentException("잘못 입력하셨습니다.");
+        }
+    }
+    public static int calculate(String str){
+        System.out.println("입력된 문자열: " + str); // 입력 문자열 출력
+
+        Matcher matcher = Pattern.compile("//(.*?)\\\\n(.*)").matcher(str);
+        String[] word = null;
+
+        if (matcher.find()){
+            System.out.println("여기 들어옴");
+            String delimeter = matcher.group(1);
+            word = matcher.group(2).split(Pattern.quote(delimeter));
+        }
+
+        if (word == null) {
+            System.out.println("word==null에 들어옴");
+            word = str.split(",|:");
+        }
+        int sum = 0;
+        for (int i = 0; i < word.length; i++) {
+            int i1 = Integer.parseInt(word[i]);
+            checkMinus(i1);
+            sum += i1;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String question = insert();
+        if(question.isEmpty() || question == null){
             System.out.println("결과 : " + 0);
             return;
         }
+        int answer = calculate(question);
         System.out.println("결과 : " + answer);
     }
-
 }
