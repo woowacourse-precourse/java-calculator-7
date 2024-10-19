1ad46d864856b7588181ba56e37648c1c2bdcd59
Imseonghun <daltjdgns123@gmail.com>
2024-10-20 02:07:57 +0900

    fix(Controller) : 입력 유효성 검사

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -6,6 +6,11 @@ public class Controller {
   public static void run() {
     String input = Console.readLine();
 
+    Validator.checkEmptyInput(input);
+    if (!Validator.checkDefaultDelimterFormat(input) && !Validator.checkCustomDelimiterFormat(input)){
+      throw new IllegalArgumentException("(기본 구분자를 활용한 입력 포맷) 또는 (커스텀 구분자 지정 포맷)이 잘못되었습니다.");
+    }
+
     int result = PlusCalculator.calculateSum(input);
 
     OutputView.printResult(result);
