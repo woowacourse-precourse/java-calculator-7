711157c81e2ff602e7ff62187fff4ab2500f2a5f
JJY <ddglackrp@gmail.com>
2024-10-20 02:19:55 +0900

    teat "구분자 regex 테스트 추가"

--- src/test/java/calculator/utils/ParsingUtilsTest.java
+++ src/test/java/calculator/utils/ParsingUtilsTest.java
@@ -4,10 +4,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import static calculator.utils.ParsingUtils.parsingUserInput;
-import static org.junit.jupiter.api.Assertions.*;
+import static calculator.utils.ParsingUtils.buildMultiDelimiterFromSet;
+import static calculator.utils.ParsingUtils.splitUserInputTowPart;
 
 class ParsingUtilsTest {
 
@@ -16,7 +19,7 @@ class ParsingUtilsTest {
     void 사용자_입력_분리_커스텀_X_테스트(){
         String userInput = "1,2:3";
 
-        List<String> parsedUserInput = parsingUserInput(userInput);
+        List<String> parsedUserInput = splitUserInputTowPart(userInput);
 
         Assertions.assertThat(parsedUserInput.size()).isEqualTo(1);
         Assertions.assertThat(parsedUserInput.getFirst()).isEqualTo(userInput);
@@ -27,11 +30,21 @@ class ParsingUtilsTest {
     void 사용자_입력_분리_커스텀_O_테스트(){
         String userInput = "//[]\\n1,2:3";
 
-        List<String> parsedUserInput = parsingUserInput(userInput);
+        List<String> parsedUserInput = splitUserInputTowPart(userInput);
 
         Assertions.assertThat(parsedUserInput.size()).isEqualTo(2);
         Assertions.assertThat(parsedUserInput.getFirst()).isEqualTo("[]");
         Assertions.assertThat(parsedUserInput.getLast()).isEqualTo("1,2:3");
+    }
+
+    @Test
+    @DisplayName("Split regex builder 테스트")
+    void SPLIT_REGEX_BUILDER_TEST(){
+        Set<String> delimiters = new HashSet<>(Arrays.asList(":", ",", ";"));
+
+        String delimiterRegex = buildMultiDelimiterFromSet(delimiters);
+
+        Assertions.assertThat(delimiterRegex).isEqualTo("[\\:\\;\\,]");
     }
 
 }
