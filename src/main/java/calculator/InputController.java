950b87488955172c500a672146faa82ef3092cbd
imscow11253 <seaweed8759@naver.com>
2024-10-19 22:43:57 +0900

    refactor : Console 클래스의 static method close() 호출

--- src/main/java/calculator/InputController.java
+++ src/main/java/calculator/InputController.java
@@ -1,11 +1,13 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.close;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputController {
 
     public static InputString getInputString(){
         String inputString = readLine();
+        close();
         return new InputString(inputString);
     }
 }
