98da06b8abdcb3d6eb574ae37c22dcc17c5083e8
Haneul Shin <shn7980@gmail.com>
2024-10-19 22:29:25 +0900

    feat: 입출력 이벤트 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringAddCalculator calculator = new StringAddCalculator();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String expression = Console.readLine();
+        double result = calculator.splitAndSum(expression);
+        System.out.println("결과 : " + result);
     }
 }
