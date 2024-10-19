633f92883edc74db678d21797f0daf5500d8a9e8
openmpy <suhwan@kakao.com>
2024-10-19 16:02:44 +0900

    test: Application 테스트 코드 추가
    - 여러 커스텀 구분자 사용
    - 기본 구분자 사용
    - 예외 테스트 커스텀 구분자
    - 예외 테스트 기본 구분자

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,13 +1,17 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class ApplicationTest extends NsTest {
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -16,11 +20,74 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @ParameterizedTest(name = "입력값: {0}, 기대값: {1}")
+    @CsvSource({
+            "'//;\\n', 0",
+            "'//@\\n1', 1",
+            "'//#\\n1#2', 3",
+            "'//aa\\n1aa2aa3', 6",
+            "'// \\n1 2 3', 6",
+            "'//ㅁ\\n1ㅁ2ㅁ3', 6",
+    })
+    void 여러_커스텀_구분자_사용(String input, long expected) {
+        assertSimpleTest(() -> {
+            run(input);
+            assertThat(output()).contains("결과 : " + expected);
+        });
+    }
+
+    @ParameterizedTest(name = "입력값: {0}, 기대값: {1}")
+    @CsvSource({
+            "'\n', 0",
+            "'1', 1",
+            "'1,', 1",
+            "'1,2', 3",
+            "'1:2,', 3",
+            "'1,2:3', 6",
+            "'1,2:3:', 6",
+    })
+    void 기본_구분자_사용(String input, long expected) {
+        assertSimpleTest(() -> {
+            run(input);
+            assertThat(output()).contains("결과 : " + expected);
+        });
+    }
+
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @ParameterizedTest(name = "입력값: {0}")
+    @ValueSource(strings = {
+            "//;\\na",
+            "//;\\n1;2;a",
+            "//;\\n1.2;2;3",
+            "//;\\n-1;2;3",
+            "//;\\n1; ;3",
+    })
+    void 예외_테스트_커스텀_구분자(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @ParameterizedTest(name = "입력값: {0}")
+    @ValueSource(strings = {
+            "a",
+            "1,2,a",
+            "1.2,2,3",
+            "-1,2:3",
+            "1, :3",
+    })
+    void 예외_테스트_기본_구분자(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
