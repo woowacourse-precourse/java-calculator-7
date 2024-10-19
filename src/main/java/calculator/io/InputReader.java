cb673027cb31a44ffc09679f32775400c4728c07
JINSU <bluesea9519@gmail.com>
2024-10-20 01:13:01 +0900

    feat: input string

---
+++ src/main/java/calculator/io/InputReader.java
@@ -0,0 +1,11 @@
+package calculator.io;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputReader {
+
+    public String readInput() {
+        return Console.readLine();
+    }
+
+}
