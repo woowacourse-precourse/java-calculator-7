5f3c86890010044d4b95133a51b3939b03cea9a7
Cori1304-Seong <andyjm7739@gmail.com>
2024-10-20 04:15:53 +0900

    feat(InputService): add changeStrNlToNl()
    "\\n" 문자를 "\n"으로 변환 시키는 기능 추가

--- src/main/java/calculator/InputService.java
+++ src/main/java/calculator/InputService.java
@@ -7,12 +7,32 @@ public class InputService {
     public static String readLine() {
         System.out.println("숫자가 포함된 문자열을 입력하세요: ");
         String input = changeEmptyToZero(Console.readLine());
+        String result = changeStrNlToNl(input);
 
-        return input;
+        return result;
     }
 
     private static String changeEmptyToZero(String input) {
         return input.isEmpty() ? "0" : input;
+    }
+
+    private static String changeStrNlToNl(String input) {
+        if (input.startsWith("//")) {
+            int newlineIndex = input.indexOf("\\n");
+
+            if (newlineIndex == -1) {
+                throw new IllegalArgumentException();
+            }
+
+            String delimiter = input.substring(2, newlineIndex);
+
+            if (delimiter.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+
+        }
+        // '\\n' new line 으로 인식하기 위해 변환;
+        return input.replace("\\n", "\n");
     }
 
 }
