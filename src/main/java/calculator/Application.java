4d08f94dec888b1f25b499d29871f98f49242171
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-10-20 02:01:27 +0900

    feat(Application): 요구사항의 출력 형식에 맞도록 구현
    요구사항에 명시된 출력 형식에 맞도록 main 함수 수정 및 내용 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,7 +3,12 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = camp.nextstep.edu.missionutils.Console.readLine();
-        System.out.println(input);
+        Input input = new Input();
+        if (input.isValid()) {
+            int result = Calculator.sum(input.getNumbersFromInput());
+            System.out.println("결과 : " + result);
+        } else {
+            throw new IllegalArgumentException("입력이 유효하지 않습니다.");
+        }
     }
 }
