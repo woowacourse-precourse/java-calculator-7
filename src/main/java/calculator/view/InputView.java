24d09588a30507cb2111d7cc040013c5700f51f2
이윤빈 <kanado20000@naver.com>
2024-10-19 09:10:40 +0900

    🛠️ feat(InputView): 입력이 null이거나 빈문자열일 떄 예외처리 추가

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -4,10 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String USER_NUMBERS = "덧셈할 문자열을 입력해 주세요.";
+    private static final String INVALID_INPUT = "입력이 잘못되었습니다.";
 
     public String readUserNumbers() {
         System.out.print(USER_NUMBERS);
-        return Console.readLine();
+        System.out.print("\n");
+        String input = Console.readLine();
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(INVALID_INPUT);
+        }
+        return input;
     }
 
 }
