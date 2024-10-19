94f3817c08594b711c4e31f595eb871195b3f61a
suyeong jang <jangsuyeong923@naver.com>
2024-10-20 03:56:17 +0900

    feat(model): 입력값에 따른 예외 처리 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,17 +1,19 @@
 package calculator.controller;
 
 
+import calculator.model.ValidationUtils;
 import calculator.view.InputView;
 
 
 public class CalculatorController {
     private final InputView inputview = new InputView();
+    private final ValidationUtils validationUtils = new ValidationUtils();
 
     public void run() {
         try {
             String input = inputview.input();
+            validationUtils.validateInput(input);
 
-            
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(e.getMessage());
         }
--- src/main/java/calculator/model/ValidationUtils.java
+++ src/main/java/calculator/model/ValidationUtils.java
@@ -2,5 +2,13 @@ package calculator.model;
 
 
 public class ValidationUtils {
+    public void validateInput(String input) {
+        if (isEmpty(input)) {
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+    }
 
+    private boolean isEmpty(String input) {
+        return input.isEmpty();
+    }
 }
