a10e9ced59313290d16b536c9d06929e45c73e36
KF97 <aysul0@naver.com>
2024-10-19 14:56:41 +0900

    test: 기본_구분자 테스트 코드 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,6 +17,17 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 기본_구분자() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
