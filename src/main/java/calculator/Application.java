dae0c0c5d4fed7a9d9f8bd8e7eea7d14613a9eee
LSH-kw0315 <tmdgjsdl1234@naver.com>
2024-10-20 03:46:27 +0900

    <feat> 기본 구분자를 이용한 정상 입력 작동 구현
    기본 구분자 ,와 ;를 사용한 정상 입력에 대하여 정상적으로 작동하도록 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,36 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String str=Console.readLine();
+
+        if(str.isEmpty()){
+            System.out.println("결과 : 0");
+            return;
+        }
+
+        String[] separator=new String[]{",",";"};
+
+        for(String s:separator){
+            str=str.replace(s," ");
+        }
+
+        String[] integers=str.split(" ");
+
+        int result=0;
+        for(String s : integers){
+            int i=Integer.parseInt(s);
+            result+=i;
+        }
+
+        System.out.println("결과 : "+result);
+
     }
 }
