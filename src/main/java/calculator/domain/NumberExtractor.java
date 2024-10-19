bac4e774dc8ca221e184063e623ac96f1d961aab
jeonghyeon-ohk <ok6737@naver.com>
2024-10-19 16:34:50 +0900

    feat(domain): 입력값 배열을 정수 배열로 변환하는 로직 추가
    NumberExtractor 클래스에 문자열 배열을 정수 배열로 변환하는 매서드를 구현했습니다.

--- src/main/java/calculator/domain/NumberExtractor.java
+++ src/main/java/calculator/domain/NumberExtractor.java
@@ -1,4 +1,13 @@
 package calculator.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import static calculator.domain.ValueExtractor.getDelimitedValue;
+
 public class NumberExtractor{
+    public static int[] getIntegerArray(String input, ArrayList<String> delimiter){
+        String[] delimitedValue = getDelimitedValue(input, delimiter);
+
+        return Arrays.stream(delimitedValue).mapToInt(Integer::parseInt).toArray();
+    }
 }
