38ff0f585106549576330590222914ea24495236
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-19 16:29:10 +0900

    refactor(Application): AppConfig에서 컨트롤러를 호출하는 방식으로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,10 @@
 package calculator;
 
 import calculator.config.AppConfig;
-import calculator.controller.StringAddCalculatorController;
 
 public class Application {
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
-        StringAddCalculatorController calculatorController = new StringAddCalculatorController(appConfig.calculatorService());
-        calculatorController.playCalculator();
+        appConfig.stringAddCalculatorController().playCalculator();
     }
 }
