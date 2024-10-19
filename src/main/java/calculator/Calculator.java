93ec32d0fd44a3c3f7e340f6ed1b07dc612f8b2e
inha0319@naver.com <inha031913@gmail.com>
2024-10-19 10:33:28 +0900

    [feat] 빈문자열 입력 처리 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,12 @@
 package calculator;
 
 public class Calculator {
+
+    public static int stringSum(String input){
+
+        if(input == null || input.isEmpty()){
+            return 0;
+        }
+        return -1;
+    }
 }
