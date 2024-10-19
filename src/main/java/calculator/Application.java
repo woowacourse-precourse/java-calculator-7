13f1bb77624111f5340d04f5bfd772c822979602
jinmlee <127607365+jinmlee@users.noreply.github.com>
2024-10-19 22:48:24 +0900

    feat: 결과 출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,12 +3,14 @@ package calculator;
 import calculator.controller.CalculatorController;
 import calculator.dto.DelimiterInputDto;
 import calculator.view.CalculatorRequest;
+import calculator.view.CalculatorResponse;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         CalculatorRequest calculatorRequest = new CalculatorRequest();
+        CalculatorResponse calculatorResponse = new CalculatorResponse();
         CalculatorController calculatorController = new CalculatorController();
 
         calculatorRequest.getStartMessage();
@@ -16,6 +18,12 @@ public class Application {
         DelimiterInputDto delimiterInputDto =
                 new DelimiterInputDto(calculatorRequest.inputString(), new ArrayList<>(List.of(",", ":")));
 
-        calculatorController.calculateSum(delimiterInputDto);
+        try {
+            int result = calculatorController.calculateSum(delimiterInputDto);
+            calculatorResponse.printResult(result);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
---
+++ src/main/java/calculator/view/CalculatorResponse.java
@@ -0,0 +1,8 @@
+package calculator.view;
+
+public class CalculatorResponse {
+
+    public void printResult(int number){
+        System.out.println("결과 : " + number);
+    }
+}
