b5a073692051ec6aed0949ed21aded9926809065
sik9252 <sik9252@naver.com>
2024-10-19 11:34:07 +0900

    feat: Calculator Class 내부에 run(), calculating(), checkEachInputIsValid() 메서드 생성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,15 +1,39 @@
 package calculator;
 
+import calculator.utils.InputUtils;
+import calculator.utils.PrintUtils;
+import calculator.utils.SplitUtils;
+import calculator.utils.ValidateUtils;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    public int getTotal(List<String> splitInput) {
-        int total = 0;
+    private static String userInput;
+    static List<String> numberList = new ArrayList<>();
+    static int total = 0;
 
-        for (String number : splitInput) {
+    public static void run() {
+        userInput = InputUtils.getUserInput();
+        calculating();
+    }
+
+    private static void calculating() {
+        numberList = SplitUtils.getCalculationSection(userInput);
+        checkEachInputIsValid();
+        getTotal();
+        PrintUtils.printResult(total);
+    }
+
+    private static void checkEachInputIsValid() {
+        for (String number : numberList) {
+            ValidateUtils.checkNumberIsValid(number);
+        }
+    }
+
+    private static void getTotal() {
+        for (String number : numberList) {
             total += Integer.parseInt(number);
         }
-
-        return total;
     }
 }
