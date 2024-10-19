67a94df4698df635ea07e7a60af9f526962d01bd
iceHood <hanyo3477@gmail.com>
2024-10-19 19:30:15 +0900

    feat: 입력받는 함수 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = readLine();
+
+    }
+
+    // Helper
+    private static String readLine() {
+        return Console.readLine();
     }
 }
