a05fe15a94b8c9baa70ee1c8ff2434edb8b8b5ee
etd93 <etd93@naver.com>
2024-10-19 22:33:45 +0900

    [feat] 커스텀 구분자 지정이 제대로 되어 있는지 검증하는 메서드 구현

--- src/main/java/calculator/exception/InputStringException.java
+++ src/main/java/calculator/exception/InputStringException.java
@@ -5,6 +5,22 @@ import calculator.domain.Numbers;
 
 public class InputStringException {
     public Numbers validateInputPattern(String inputString, Numbers numbers, Delimiters delimiters){
+        if(inputString.contains("//") || inputString.contains("\\n")) {
+            isCustomDelimiterDeclaredInStandardFormat(inputString);
+        }
+
         return new Numbers();
+    }
+
+    public static void isCustomDelimiterDeclaredInStandardFormat(String inputString){
+        int startIdx = inputString.indexOf("//");
+        int endIdx = inputString.lastIndexOf("\\n");
+        if(startIdx == -1 || endIdx == -1){
+            throw new IllegalArgumentException();
+        }
+        String checkInputString = inputString.substring(startIdx+2, endIdx);
+        if(!(startIdx ==0 && endIdx == 3 && checkInputString.length()==1 && !Character.isDigit(checkInputString.charAt(0)))){
+            throw new IllegalArgumentException();
+        }
     }
 }
