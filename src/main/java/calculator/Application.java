73e22de5e2b0dc753b4246e1ab5c9db2443f612e
lejuho <zedd1324@gmail.com>
2024-10-19 11:01:16 +0900

    feat(readIn) : add input-reading function
    add function, which return string which "Console.readLine()" read in, to get input.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String s = readIn();
     }
+
+    public static String readIn() {
+        String new_in = Console.readLine();
+        return new_in;
+    }
+
 }
