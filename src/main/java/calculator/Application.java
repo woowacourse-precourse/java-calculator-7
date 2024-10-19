c39232da6af18a4e4c1ea673049209f07253d1a4
yeonsu <jack0810@kookmin.ac.kr>
2024-10-20 00:37:51 +0900

    FEATURE: Add calculator start

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.view.CalculatorView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorView calculatorView = new CalculatorView();
+        CalculatorController calculatorController = new CalculatorController(calculatorView);
+
+        calculatorController.start();
     }
 }
--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -4,4 +4,13 @@ import calculator.view.CalculatorView;
 
 public class CalculatorController {
     private CalculatorView calculatorView;
+
+    public CalculatorController(CalculatorView view) {
+        calculatorView = view;
+    }
+
+    public void start() {
+        calculatorView = new CalculatorView();
+        String input = calculatorView.getInput();
+    }
 }
