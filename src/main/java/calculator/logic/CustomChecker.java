21ab477cfd18b4ca36b9fc25a61fc7027036484c
장시영 <sysmile77@gmail.com>
2024-10-19 23:45:26 +0900

    fix(CustomChecker): 단위테스트는 성공하지만, 전체테스트는 실패하는 문제

--- src/main/java/calculator/logic/CustomChecker.java
+++ src/main/java/calculator/logic/CustomChecker.java
@@ -4,9 +4,10 @@ package calculator.logic;
 public class CustomChecker {
 
     public static char[] customArray  = new char[10];
-    static StringBuilder sb = new StringBuilder();
+
 
     public static String getCustom(String str) {
+        StringBuilder sb = new StringBuilder();
 
         if(str.charAt(0) == '/' && str.charAt(1)=='/'){
             for(int i=2; i<str.length(); i++) {
@@ -24,17 +25,6 @@ public class CustomChecker {
 
 
     public static boolean isCustom(String str) {
-        if(str.charAt(0) == '/' && str.charAt(1)=='/'){
-            for(int i=2; i<str.length(); i++) {
-                if(str.charAt(i-1) == '\\' && str.charAt(i)=='n') {
-                    for(int j=0; j<i-3; j++) {
-                        //  System.out.println("j:"+j+" i:"+(i-2)+" "+str.charAt(j));
-                        customArray[j] = str.charAt(j+2);
-                    }
-                    return true;
-                }
-            }
-        }
-        return false;
+        return str.startsWith("//");
     }
 }
