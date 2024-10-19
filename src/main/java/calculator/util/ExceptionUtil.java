700742c2ca13ff5d04577269f985397b121d511c
qudcks8084 <qudcks8084@naver.com>
2024-10-19 15:47:58 +0900

    feat(util) : implement checkParamsIsNotEmpty()
    Detail
    - Parameter has been changed
    	- String str, String delim, boolean custom -> String str
    - If there are no Sentence when It has Custom delim It Occur IndexOutOfBound Err
    	-> So Before Constructor Occur IndexOutOfBound, It can check the Exception

--- src/main/java/calculator/util/ExceptionUtil.java
+++ src/main/java/calculator/util/ExceptionUtil.java
@@ -4,6 +4,7 @@ import calculator.dto.CalculatorDTO;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
@@ -42,5 +43,27 @@ public class ExceptionUtil {
             throw new IllegalArgumentException("ERR : 잘못된 인자값 입니다.");
         }
 
+    }
+
+    public static void checkParamsIsNotEmpty(String str) throws IllegalArgumentException {
+        if(str.startsWith("//")){ // 커스텀 구분자를 가지는 경우
+            String[] strList = str.split("\\\\n");
+
+            // 문자열이 비어있는지를 검사
+            if(strList.length != 2 ){
+                throw new IllegalArgumentException("ERR : 계산을 위한 대상이 존재하지 않습니다. ");
+            }
+
+            // 구분자가 비어있는지를 검사
+            if(Objects.equals(strList[0], "//")){
+                throw new IllegalArgumentException("ERR : 커스텀 구분자가 존재하지 않습니다. ");
+            }
+
+
+        }else{ // 커스텀 구분자를 가지지 않는 경우
+            if(str.isEmpty()){ // 문자열이 비어있는지를 검사
+                throw new IllegalArgumentException("ERR : 계산을 위한 대상이 존재하지 않습니다. ");
+            }
+        }
     }
 }
