b24dfd99485b7f5536d519b38f631030fc9df02d
dnwldla <leeemjw@naver.com>
2024-10-19 22:19:26 +0900

    Style: 빈 줄 제거

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,26 +1,13 @@
 package calculator.view;
 
-
-import calculator.domain.Seperator;
-import calculator.validation.Validator;
 import camp.nextstep.edu.missionutils.Console;
 
 
 public class InputView {
 
-
     public static String enterInput(){
         String input= Console.readLine();
 
         return input;
-
     }
-
-
-
-
-
-
-
-
 }
