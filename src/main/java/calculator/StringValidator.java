69c81ae8f8ae67febfabf2e96e06535980468448
howareyoo <newworld121@naver.com>
2024-10-19 20:08:45 +0900

    feat : 다른 문자 검사
    숫자, 특수문자 , n 제외한 나머지 문자 입력시 예외

--- src/main/java/calculator/StringValidator.java
+++ src/main/java/calculator/StringValidator.java
@@ -1,7 +1,9 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class StringValidator {
-    
+
     public static String customDelimiterValidator(String userInput) throws IllegalArgumentException {
         for (Delimiter delimiter : Delimiter.values()) {
             if (userInput.contains(delimiter.getDelimiter())) {
@@ -9,6 +11,10 @@ public class StringValidator {
             }
         }
         throw new IllegalArgumentException();
+    }
+
+    public static boolean stringFiter(String userInput) throws IllegalArgumentException {
+        return Pattern.matches("^[0-9\\p{Punct}n]*", userInput);
     }
 
 }
