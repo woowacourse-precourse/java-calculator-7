62f506ea442374665774df5b0b77ee5fe98d8208
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-19 15:15:41 +0900

    Feat: 공통 로직에 진입한 이후 입력값이 구분자로 시작할 경우 예외 처리

--- src/main/java/calculator/service/validation/SeparationValidator.java
+++ src/main/java/calculator/service/validation/SeparationValidator.java
@@ -1,8 +1,10 @@
 package calculator.service.validation;
 
 import static calculator.service.exception.SeparationExceptionType.NO_INPUT;
+import static calculator.service.exception.SeparationExceptionType.STARTS_WITH_SEPARATOR;
 
 import calculator.service.exception.SeparationException;
+import java.util.Arrays;
 
 public class SeparationValidator {
 
@@ -17,4 +19,10 @@ public class SeparationValidator {
         }
     }
 
+    // 입력값이 구분자로 시작하는지 검증합니다.
+    private static void validateStartsWithSeparator(String input, String... separators) {
+        if (Arrays.stream(separators).anyMatch(input::startsWith)) {
+            throw new SeparationException(STARTS_WITH_SEPARATOR);
+        }
+    }
 }
