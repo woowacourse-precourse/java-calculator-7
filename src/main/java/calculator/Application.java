93a554321ee553003f464f4c625204393f87ccb3
minju26 <mjalswn26@gmail.com>
2024-10-19 21:35:21 +0900

    feat(Application): 기본 구분자를 포함한 문자열에서 숫자 추출 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,36 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Application {
+
+    private static final String INPUT_GUIDE_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String DEFAULT_DELIMITER = ",:";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        String inputString = getInputString();
+        ArrayList<Integer> extractedNumbers = extractNumbers(inputString, DEFAULT_DELIMITER);
+        System.out.println(extractedNumbers);
+    }
+
+    private static String getInputString() {
+
+        System.out.println(INPUT_GUIDE_MESSAGE);
+        return Console.readLine();
+    }
+
+    private static ArrayList<Integer> extractNumbers(String inputString, String delim) {
+
+        ArrayList<Integer> numbersToSum = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(inputString, delim);
+
+        while (tokenizer.hasMoreTokens()) {
+            numbersToSum.add(Integer.valueOf(tokenizer.nextToken()));
+        }
+
+        return numbersToSum;
     }
 }
