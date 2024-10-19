601652acbb172874da91b8c6eec289ecc6b531e9
etd93 <etd93@naver.com>
2024-10-19 22:59:01 +0900

    [feat] 결과값 출력하는 메서드 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -3,12 +3,14 @@ package calculator.controller;
 import calculator.domain.Delimiters;
 import calculator.domain.Numbers;
 import calculator.exception.InputStringException;
+import calculator.service.CalculateService;
 import calculator.util.OutputUtil;
 import camp.nextstep.edu.missionutils.Console;
 
 public class CalculatorController {
     private final OutputUtil outputUtil = new OutputUtil();
     private final InputStringException inputStringException = new InputStringException();
+    private final CalculateService calculateService = new CalculateService();
     public void startCalculating(){
         Numbers numbers = new Numbers();
         Delimiters delimiters = new Delimiters();
@@ -17,5 +19,8 @@ public class CalculatorController {
         String inputString = Console.readLine();
 
         numbers = inputStringException.validateInputPattern(inputString, numbers, delimiters);
+        outputUtil.printResultPhrase();
+        outputUtil.printResult(calculateService.calculateSum(numbers));
+
     }
 }
