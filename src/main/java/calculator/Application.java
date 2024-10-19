e76db51539afa77d56269a4157f2f3db750995d2
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2024-10-19 16:16:11 +0900

    feat: 숫자들을 추출하는 기능 추가 및 효율성을 위한 구조 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,11 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 public class Application {
 
-    static String[] setDelimiter( String input ){
-        StringTokenizer tokenizer = new StringTokenizer( input, "\\n" );
+    static String[] setDelimiter( StringTokenizer tokenizer ){
         if( tokenizer.countTokens() > 1 ){
             String[] delimiterList = { tokenizer.nextToken().substring(2), ",", ":" };
             return delimiterList;
@@ -17,15 +17,29 @@ public class Application {
         return delimiterList;
     }
 
+    static int[] getNumbers( StringTokenizer tokenizer, String[] delimiterList ){
+        if( tokenizer.countTokens() > 1 ){ tokenizer.nextToken(); }
+        String numString = tokenizer.nextToken();
+        String regex = String.join( "|", delimiterList );
+        Pattern pattern = Pattern.compile( regex );
+        String[] nums = pattern.split( numString );
+        int[] result = new int[nums.length];
+        for( int i = 0; i < nums.length; i++ ){
+            result[i] = Integer.parseInt( nums[i] );
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int result = 0;
         try{
             BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
             System.out.println( "덧셈할 문자열을 입력해 주세요." );
             String input = bufferedReader.readLine();
-            String[] delimiterList = setDelimiter( input );
-
-
+            StringTokenizer tokenizer = new StringTokenizer( input, "\\n" );
+            String[] delimiterList = setDelimiter( tokenizer );
+            int[] numbers = getNumbers( tokenizer, delimiterList );
+            
             System.out.println( "결과: " + result );
             bufferedReader.close();
         } catch (IOException e ){
