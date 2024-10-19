418e77f642bb7f45fbe92c752b824eade3b28dc2
seonseon1515 <seonseon1515@gmail.com>
2024-10-19 16:46:19 +0900

    feat: 문자열 입력 받기 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    // TODO: 프로그램 구현
+
+    // 문자열 입력 받기
+    System.out.println("덧셈할 문자열을 입력해 주세요.");
+    String inputStr = Console.readLine();
     }
 }
