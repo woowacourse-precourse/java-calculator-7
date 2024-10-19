250482f3c597d8cc4d4cb48349b259966568f75f
ljg0610 <jggy0627@naver.com>
2024-10-19 23:33:56 +0900

    feat: 입력값 구분자 처리 후 계산값 반환

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,12 +1,20 @@
 package calculator;
 
 public class Calculator {
+    private static final String COMMA = ",";
+    private static final String COLON = ":";
+
     public int calcString(String input){
         if(input.isEmpty()){
             return 0;
         }
 
+        String[] numbers = input.split(COMMA + "|" + COLON);
         int result = 0;
+
+        for(String num : numbers){
+            result += Integer.parseInt(num);
+        }
 
         return result;
     }
