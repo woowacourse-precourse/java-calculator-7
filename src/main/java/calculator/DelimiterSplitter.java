fdacfdc2c1e840196b5320ecda2998f4c0d3c6dd
sihyun10 <hyhy200170@gmail.com>
2024-10-19 23:24:14 +0900

    style(상수) : 기본,커스텀 구분자 상수로 변경 기본 구분자는 쉼표와 콜론을 가지며, 커스텀 구분자는 //와 \n 사이에 위치하는 문자이다. 일일히 작성해주는것보단 상수로 만들어서 필요할때 가져다 쓸 수 있게끔 수정하였습니다.

--- src/main/java/calculator/DelimiterSplitter.java
+++ src/main/java/calculator/DelimiterSplitter.java
@@ -5,25 +5,26 @@ import java.util.regex.Pattern;
 
 public class DelimiterSplitter {
     private String str;
-    private final String CUSTOM_REGEX = "//(.)\\n(.*)";
+    private static final String BASIC_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
 
     public DelimiterSplitter(String str) {
         this.str = str;
     }
 
     public String[] split() {
-        if (Pattern.matches(CUSTOM_REGEX, str)) {
+        if (Pattern.matches(CUSTOM_DELIMITER, str)) {
             return customDelimiterSplit();
         }
         return basicDelimiterSplit();
     }
 
     private String[] basicDelimiterSplit() {
-        return str.split(",|:");
+        return str.split(BASIC_DELIMITER);
     }
 
     private String[] customDelimiterSplit() {
-        Matcher matcher = Pattern.compile(CUSTOM_REGEX).matcher(str);
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(str);
         if (matcher.find()) {
             String customDelimiter = matcher.group(1);
             return matcher.group(2).split(customDelimiter);
