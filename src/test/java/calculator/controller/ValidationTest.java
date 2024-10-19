394ea570a0550b2a14bffc055c80abc4351f75f9
Lucerna00 <pjs3859811@naver.com>
2024-10-19 19:34:13 +0900

    test(ValidationTest.java): add tests to isAllPositive method

--- src/test/java/calculator/controller/ValidationTest.java
+++ src/test/java/calculator/controller/ValidationTest.java
@@ -32,4 +32,43 @@ public class ValidationTest {
         // then
         assertThat(actual).isTrue();
     }
+
+    @Test
+    @DisplayName("모든 숫자가 양수인 입력")
+    void isAllPositive_withPositiveNumbers() {
+        // given
+        int[] numbers = {1, 2, 3};
+
+        // when
+        boolean actual = Validation.isAllPositive(numbers);
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    @DisplayName("0이 포함된 입력")
+    void isAllPositive_withZeroNumber() {
+        // given
+        int[] numbers = {0, 1, 2, 3};
+
+        // when
+        boolean actual = Validation.isAllPositive(numbers);
+
+        // then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    @DisplayName("음수가 포함된 입력")
+    void isAllPositive_withNegativeNumber() {
+        // given
+        int[] numbers = {-1, 1, 2, 3};
+
+        // when
+        boolean actual = Validation.isAllPositive(numbers);
+
+        // then
+        assertThat(actual).isFalse();
+    }
 }
