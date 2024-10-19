5684a8376029451380c27e65da852cf41fe80a5f
yuntasha <jaljayo85@gmail.com>
2024-10-19 21:39:42 +0900

    feat(Application.java): isCommand method
    add new method:
    - add is Command method
    - make part of main method

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,40 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
 public class Application {
-    public static void main(String[] args) {
+
+    static HashSet<Integer> delimiters = new HashSet<Integer>();
+
+    public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
+        var bf = new BufferedReader(new InputStreamReader(System.in));
+
+        delimiters.add((int)':');
+        delimiters.add((int)',');
+
+        var result = 0;
+
+        var now = bf.readLine();
+        while (isCommand(now)) {
+            break;
+        }
+    }
+
+    static private boolean isCommand(String s) throws IllegalArgumentException {
+        if (s.charAt(0)=='/' && s.charAt(1)=='/') {
+            return false;
+        } else if (isNum(s.charAt(0)) || delimiters.contains((int)s.charAt(0))) {
+            return true;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    static private boolean isNum(char c) {
+        return '0'<=c && c<='9';
     }
 }
