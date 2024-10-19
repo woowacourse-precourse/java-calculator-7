34d728b8a323a6426c4f7edc5261470c24262db2
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-19 15:53:28 +0900

    feat: 덧셈 기능 구현

--- src/main/java/util/Adder.java
+++ src/main/java/util/Adder.java
@@ -1,4 +1,11 @@
 package util;
 
 public class Adder {
+    public static int getSum(int[] numbers) {
+        int sum = 0;
+        for (int i : numbers) {
+            sum += i;
+        }
+        return sum;
+    }
 }
