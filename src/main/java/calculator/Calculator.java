d0e25b54ea54fd7008e0f73da1100c759118a383
xeimun <hinote444@naver.com>
2024-10-20 02:33:51 +0900

    feat: 구분자를 설정하는 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,10 +1,22 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class Calculator {
 
     private static final String CUSTOM_DELIMITER_PREFIX = "//";
     private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
     private static final int CUSTOM_DELIMITER_START_INDEX = 2;
+    private static final String DEFAULT_DELIMITER = "[,|:]";
+
+    // 커스텀 구분자가 있는 경우 기본 구분자와 커스텀 구분자 결합
+    public String determineDelimiter(String customDelimiter) {
+        if (!customDelimiter.isEmpty()) {
+            String CustomAndDefaultDelimiter = Pattern.quote(customDelimiter) + "|" + DEFAULT_DELIMITER;
+            return CustomAndDefaultDelimiter;
+        }
+        return DEFAULT_DELIMITER;
+    }
 
     // 커스텀 구분자가 있는 경우 커스텀 구분자 선언 부분을 제외한 숫자 부분 추출
     public String extractNumbersPart(String userInput, String customDelimiter) {
