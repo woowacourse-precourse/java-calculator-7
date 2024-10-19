aa20e4606dcc94831eba022e555a569a6c34451a
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-19 16:32:06 +0900

    feat(application): 계산기 실행 구현
    절차
    1. 사용자 입력
    2. 빈 문자열 검증
    3. 구분자 유형 분류
    4. 분류된 유형에 따른 결과 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,26 @@
 package calculator;
 
+import Delimiter.BasicDelimiter;
+import Delimiter.CustomDelimiter;
+import Delimiter.DelimiterHandler;
 import camp.nextstep.edu.missionutils.Console;
+import util.Adder;
+import util.Validator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
+        Validator.isEmpty(input);
+        String delimiterType = DelimiterHandler.detectDelimiterType(input);
+        if (delimiterType.equals("Basic")) {
+            int result = Adder.getSum(BasicDelimiter.getNumber(input));
+            System.out.printf("결과 : %d", result);
+            return;  // Basic 구분자가 처리되면 일찍 종료
+        }
+
+        // Custom 구분자 처리
+        int result = Adder.getSum(CustomDelimiter.getNumber(input));
+        System.out.printf("결과 : %d", result);
     }
 }
