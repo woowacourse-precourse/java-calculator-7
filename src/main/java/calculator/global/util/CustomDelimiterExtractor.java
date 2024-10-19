7ce9eb032047c88012a017cadc6dd376a88e00c6
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-19 14:02:06 +0900

    feat: 커스텀 구분자 추출기 생성

--- src/main/java/calculator/global/util/CustomDelimiterExtractor.java
+++ src/main/java/calculator/global/util/CustomDelimiterExtractor.java
@@ -1,4 +1,19 @@
 package calculator.global.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CustomDelimiterExtractor {
+
+    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n";
+
+    private static final Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+
+    public String extractDelimiter(String input) {
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return "";
+    }
 }
