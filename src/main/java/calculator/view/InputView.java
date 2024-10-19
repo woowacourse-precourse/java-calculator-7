fdc9b62d3f6cd69e7034ad0464394f503a242774
scc9811 <wjsrudtjr98@naver.com>
2024-10-19 18:54:20 +0900

    fix(InputView) : default 구분자 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class InputView {
     private final static String ADDITION_INPUT_PROMPT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
-    private final static String DEFAULT_DELIMITERS = ",:";
+    private final static String DEFAULT_DELIMITERS = ",|:";
 
     public String getAdditionInput() {
         System.out.println(ADDITION_INPUT_PROMPT_MESSAGE);
