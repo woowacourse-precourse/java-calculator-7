d13e1c1cb89f39038591971f198785be14c42c45
tibetyo <zjhj0814@gamil.com>
2024-10-20 00:37:52 +0900

    feat(Application): 사용자 결과 안내 문자열 상수화
    '결과 : '를 상수화한다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     private static final String inputPhrase = "덧셈할 문자열을 입력해 주세요.";
+    private static final String outputPhrase = "결과 : ";
 
     public static void main(String[] args) {
         System.out.println("inputPhrase = " + inputPhrase);
