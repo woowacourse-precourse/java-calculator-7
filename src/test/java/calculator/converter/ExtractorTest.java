2bfbb3b4d3b372dd2c13c22128f2709c01b1f950
sinminseok12 <sinminseok12@naver.com>
2024-10-20 02:09:39 +0900

    test: 구분자,숫자 추출 기능 테스트

---
+++ src/test/java/calculator/converter/ExtractorTest.java
@@ -0,0 +1,37 @@
+package calculator.converter;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static calculator.utils.CalculatorConstants.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ExtractorTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "'//+\\n1,2,3', '[,:+]'",
+            "'//_\\n1_2,3', '[,:_]'",
+            "'//;\\n1;2;3', '[,:;]'"
+    })
+    void 구분자_추출_테스트(String value, String expectedDelimiter) {
+        String actualDelimiter = Extractor.extractDelimiter(value, DELIMITER_REGEX, CUSTOM_DELIMITER_PREFIX, CUSTOM_DELIMITER_SUFFIX);
+
+        assertThat(actualDelimiter).isEqualTo(expectedDelimiter);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'//+\\n1,2,3', '1,2,3'",
+            "'//_\\n1', '1'",
+            "'//;\\n1;2;3', '1;2;3'"
+    })
+    void 숫자_추출_테스트(String value, String expectedNumberPart) {
+        String actualDelimiter = Extractor.extractNumberPart(value, CUSTOM_DELIMITER_PREFIX, CUSTOM_DELIMITER_SUFFIX);
+
+        assertThat(actualDelimiter).isEqualTo(expectedNumberPart);
+    }
+
+
+}
