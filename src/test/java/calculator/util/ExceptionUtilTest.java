bdd1e25c1452eb48d2eb805fa0a9cdda0c148482
qudcks8084 <qudcks8084@naver.com>
2024-10-19 15:57:37 +0900

    test(util) : test code checkNextTargetIsLiteral()
    Detail
    - add 3 test case
    	- That is literal but it is lower than 0
    	- That is not literal
    	- That is literal and it is bigger than 0

--- src/test/java/calculator/util/ExceptionUtilTest.java
+++ src/test/java/calculator/util/ExceptionUtilTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static calculator.util.ExceptionUtil.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExceptionUtilTest {
@@ -41,5 +42,49 @@ class ExceptionUtilTest {
         Assertions.assertThrows(IllegalArgumentException.class,
                 //when
                 () -> checkParamsIsNotEmpty(str));
+    }
+
+    @Test
+    @DisplayName("계산되는 다음 값이 0보다 작은 숫자인 경우")
+    void nextTargetIsLowThanZero(){
+        //given
+        String str = "-1";
+        //then
+        Assertions.assertThrows(IllegalArgumentException.class,
+                //when
+                () -> checkNextTargetIsLiteral(str));
+    }
+
+    @Test
+    @DisplayName("계산되는 다음 값이 숫자가 아닌 경우")
+    void nextTargetIsNotLiteral1(){
+        //given
+        String str = " ";
+        //then
+        Assertions.assertThrows(IllegalArgumentException.class,
+                //when
+                () -> checkNextTargetIsLiteral(str));
+    }
+
+    @Test
+    @DisplayName("계산되는 다음 값이 숫자가 아닌 경우")
+    void nextTargetIsNotLiteral2(){
+        //given
+        String str = "?";
+        //then
+        Assertions.assertThrows(IllegalArgumentException.class,
+                //when
+                () -> checkNextTargetIsLiteral(str));
+    }
+
+    @Test
+    @DisplayName("계산되는 다음 값이 정상적인 경우")
+    void nextTargetIsBiggerThanZero(){
+        //given
+        String str = "20";
+        //when
+        int num = checkNextTargetIsLiteral(str);
+        //then
+        assertThat(num).isEqualTo(20);
     }
 }
