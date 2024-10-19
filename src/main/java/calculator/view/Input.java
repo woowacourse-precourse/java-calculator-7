09662750702880f816f93d96389146d3294eb888
ahma0 <dsd932@naver.com>
2024-10-19 17:38:42 +0900

    feat: `IllegalArgumentException`을 일으키는 기능

--- src/main/java/calculator/view/Input.java
+++ src/main/java/calculator/view/Input.java
@@ -1,14 +1,19 @@
 package calculator.view;
 
+import calculator.exception.CalculatorException;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
     public String getInput() {
         String input = Console.readLine();
+        validateInput(input);
+        return input;
+    }
 
+    private String validateInput(String input) {
         if (input == null || isInputBlank(input)) {
-            throw new IllegalArgumentException("띄어쓰기를 포함시킬 수 없습니다.");
+            CalculatorException.causeException("띄어쓰기를 포함시킬 수 없습니다.");
         }
 
         return input;
