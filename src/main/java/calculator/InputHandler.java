64725c87926a3f6d72edc9daf214723efd7c5bb0
JiEung2 <181757@jnu.ac.kr>
2024-10-19 22:37:53 +0900

    feat(inputHandler): 입력을 다루는 클래스 생성 및 사용자 입력을 받는 기능 추가

--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -3,8 +3,13 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
+    private final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 
     public String getInput() {
         return Console.readLine();
+    }
+
+    public void printInputMessage() {
+        System.out.println(INPUT_MESSAGE);
     }
 }
