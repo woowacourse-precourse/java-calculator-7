b00b899123b952b4533a9a1ecb67b1b47e9b07dc
CHISANW <keuye06380618@gmail.com>
2024-10-20 03:39:09 +0900

    test(CalculatorControllerTest): 계산 기능에 대한 테스트 추가
    - 다양한 입력값을 통해 구현한 기능이 정상적으로 결과를 도출하는지 검증하는 테스트 작성

---
+++ src/test/java/calculator/controller/CalculatorControllerTest.java
@@ -0,0 +1,97 @@
+package calculator.controller;
+
+import calculator.service.CalculatorService;
+import calculator.viiew.CalculatorView;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorControllerTest {
+
+
+    private CalculatorController calculatorController;
+    private CalculatorService calculatorService;
+    private CalculatorView calculatorView;
+
+    @BeforeEach
+    void init() {
+        calculatorService = new CalculatorService();
+    }
+
+    @DisplayName("기본 문자열 입력 시 계산")
+    @Test
+    void calculateDefault() {
+        setupController("1,2:3", 6);
+        calculatorController.calculate();
+    }
+
+    @DisplayName("빈값이 포함된 문자열일 경우 계산")
+    @Test
+    void calculateContainsEmpty() {
+        setupController("1,,2", 3);
+        calculatorController.calculate();
+    }
+
+    @DisplayName("빈 문자열일 경우 계산")
+    @Test
+    void calculateOnlyEmpty() {
+        setupController("", 0);
+        calculatorController.calculate();
+    }
+
+    @DisplayName("커스텀 구분자가 있는 문자열일 경우 계산")
+    @Test
+    void calculatorCustom() {
+        setupController("//:\\n1:2:3", 6);
+        calculatorController.calculate();
+    }
+
+    @DisplayName("음수 사용시 예외 발생")
+    @Test
+    void calculatorNegativeNumberException() {
+        setupControllerWithException("-1,2,3");
+    }
+
+    @DisplayName("기본 구분자 중 잘못된 입력일 경우 예외 발생")
+    @Test
+    void calculatorDefaultException() {
+        setupControllerWithException("1,2:3a");
+    }
+
+    @DisplayName("커스텀 구분자 중 잘못된 입력일 경우 예외 발생")
+    @Test
+    void calculatorCustomException() {
+        setupControllerWithException("//:\\n1:2a:3");
+    }
+    
+    private void setupController(String input, int expectedResult) {
+        calculatorView = new CalculatorView() {
+            @Override
+            public String input() {
+                return input;
+            }
+
+            @Override
+            public void resultOutput(int result) {
+                Assertions.assertThat(result).isEqualTo(expectedResult);
+            }
+        };
+        calculatorController = new CalculatorController(calculatorService, calculatorView);
+    }
+
+    private void setupControllerWithException(String input) {
+        calculatorView = new CalculatorView() {
+            @Override
+            public String input() {
+                return input;
+            }
+        };
+
+        calculatorController = new CalculatorController(calculatorService, calculatorView);
+        Assertions.assertThatThrownBy(() -> calculatorController.calculate())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 값이 입력되었습니다.");
+    }
+
+}
