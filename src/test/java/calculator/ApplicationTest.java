2aad99188c189d33fc70d61e998ed22de919316c
dd-jiyun <dlwldbs8015@gmail.com>
2024-10-20 01:12:22 +0900

    test: Application 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -8,8 +9,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
-    void 커스텀_구분자_사용() {
+    @DisplayName("입력 값이 공백일 경우 예외가 발생한다.")
+    void inputBlankString() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException(" "))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    @DisplayName("기본 구분자를 사용했을 때 기대한 결과값이 나온다.")
+    void usingBasicDelimiter() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    @DisplayName("여러 기본 구분자를 사용했을 때 기대한 결과값이 나온다.")
+    void usingMultipleBasicDelimiter() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자를 사용했을 때 기대한 결과값이 나온다.")
+    void usingCustomDelimiter() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
@@ -17,10 +47,29 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    @DisplayName("커스텀 구분자와 기본 구분자를 혼합 사용할 때 기대한 결과값이 나온다.")
+    void usingCustomAndBasicDelimiter() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    @DisplayName("입력값에 음수가 존재할 때 예외가 발생한다.")
+    void inputNegativeNumberException1() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("입력값에 음수가 존재할 때 예외가 발생한다.")
+    void inputNegativeNumberException2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\n1;-2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
@@ -28,4 +77,5 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
 }
