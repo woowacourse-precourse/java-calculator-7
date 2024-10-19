815178163e5fcd8d13656c293e219ea85c869ce3
seophohoho <seop0937@gmail.com>
2024-10-19 23:52:46 +0900

    feat(Plus): 누적 합 기능 구현

--- src/main/java/calculator/Plus.java
+++ src/main/java/calculator/Plus.java
@@ -4,4 +4,14 @@ public class Plus {
     public int convertToInteger(String str) {
         return Integer.parseInt(str);
     }
+
+    public int plus(String[] lst) {
+        int result = 0;
+
+        for (int i = 0; i < lst.length; i++) {
+            result += convertToInteger(lst[i]);
+        }
+
+        return result;
+    }
 }
