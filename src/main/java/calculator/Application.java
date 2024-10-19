a2ae24cc3f35fe19b3e9d6087392b8a7cb4ccff7
Seol-Munhyeok <abc1234568@naver.com>
2024-10-19 22:59:38 +0900

    feat(입력 처리) : 간단한 입력에 대한 예외를 처리하였다.
    입력이 공백이면 0 리턴
    
    입력이 구분자 없이 숫자 하나면 해당 숫자 리턴
    
    isDigit() 함수 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input;
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        input = Console.readLine();
+        if (input.isEmpty()) {
+            System.out.println("결과 : 0");
+        }
+        else if (input.length() == 1 && isDigit((input.charAt(0)))) {
+            System.out.println("결과 : " + input);
+        }
+
     }
 }
