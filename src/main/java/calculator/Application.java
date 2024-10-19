acbf1210d96099cc618cae61f2c12da7ca6749d0
Park Yu Han <pbk2312@naver.com>
2024-10-19 17:45:53 +0900

    refactor : ControllerFactory 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,13 @@
 package calculator;
 
 import calculator.controller.StringCalculatorController;
-import calculator.factory.CalculatorFactory;
+import calculator.factory.ControllerFactory;
 
 public class Application {
 
     public static void main(String[] args) {
         try {
-            StringCalculatorController calculatorController = CalculatorFactory.createController();  // Factory를 사용하여 Controller 생성
+            StringCalculatorController calculatorController = ControllerFactory.createController();  // ControllerFactory를 사용하여 Controller 생성
             calculatorController.run();  // 프로그램 실행
         } catch (IllegalArgumentException e) {
             // 예외 발생 시 메시지 출력 후 프로그램 종료
