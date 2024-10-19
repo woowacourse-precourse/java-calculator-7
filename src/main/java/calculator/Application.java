342f670a7705ba2124dd392afa6be629ff502b1a
yaongmeow <minnie_00@naver.com>
2024-10-19 17:39:27 +0900

    feat(I/O): 입/출력 활성화
    - 과제 포맷과 동일한 입/출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("덧셈할 문자열을 입력해 주세요.\n");
+        String input = scanner.nextLine();
+        Integer sum = 0;
+        System.out.println("결과 : " + sum);
     }
 }
