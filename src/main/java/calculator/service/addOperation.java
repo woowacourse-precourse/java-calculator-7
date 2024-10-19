76435df5ea814802bb39a946fe10678ea9cf7eb9
gongPyeon <pyeonkang@naver.com>
2024-10-19 19:46:15 +0900

    fix(addOperation): 입력된 문자열 형식 확인하기

--- src/main/java/calculator/service/addOperation.java
+++ src/main/java/calculator/service/addOperation.java
@@ -5,6 +5,12 @@ import calculator.domain.Number;
 public class addOperation {
     String input;
 
+    public static boolean isValidFormat(String str, String delimiter) {
+        // 정규 표현식 수정: 구분자 사이에 숫자가 올 수 있도록
+        String regex = "(\\d+(" + delimiter + "\\d+)*)+";
+        return str.matches(regex);  // 형식 확인
+    }
+
     public int add(String input) {
         if (input == null || input.isEmpty()) { // 빈문자열일 경우 (공백이 포함되어있을 경우)
             return 0;
@@ -14,15 +20,18 @@ public class addOperation {
         if (input.startsWith("//")) {
             int startIdx = input.indexOf("//");
             int endIdx = input.indexOf("\\n");
-            System.out.println(startIdx + " " + endIdx);
+
             if(startIdx + 2 == endIdx) throw new IllegalArgumentException("구분자가 정의되지 않았습니다");
 
             if (endIdx == -1) {
                 throw new IllegalArgumentException("커스텀 구분자가 올바르지 않습니다.");
             }
             delimiter = input.substring(2, endIdx);
+
             input = input.substring(endIdx + 2); // 구분자를 제외한 값
         }
+
+        if(!isValidFormat(input, delimiter)) throw new IllegalArgumentException("문자열 형태가 올바르지 않습니다 (숫자 구분자 숫자)");
 
         String[] tokens = input.split(delimiter); // 구분자 기준으로 숫자 저장하기
 
