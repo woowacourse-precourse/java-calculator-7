f5bd06f4e2a42037b7d8902497c10eef2432566e
장시영 <sysmile77@gmail.com>
2024-10-19 20:42:45 +0900

    feat(Sum):  수열의 합산

--- src/main/java/calculator/logic/Sum.java
+++ src/main/java/calculator/logic/Sum.java
@@ -1,6 +1,20 @@
 package calculator.logic;
 
+import java.util.Arrays;
+
 public class Sum {
-    public static void execution() {
+
+
+
+    public static int doSummation(String[] str){
+
+        int sum = 0;
+
+        for(int i=0; i<str.length ; i++) {
+            sum += Integer.parseInt(str[i]);
+        }
+
+        return sum;
     }
+
 }
