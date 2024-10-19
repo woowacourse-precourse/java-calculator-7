ab7c330f96df77de604d1bc7b66387448cb5dc61
ehgk4245 <ehgkrk14@naver.com>
2024-10-20 02:48:03 +0900

    feat(splitNumbers): 구분자로 숫자 추출 기능 추가
    커스텀 구분자가 있다면 기본 구분자에 더하여 split()으로 숫자 추출
    추출한 숫자 문자열 배열로 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,6 +10,7 @@ public class Application {
     public static final String DELIMITER_PATTERN = "//(.)\\\\n(.*)";
     public static void main(String[] args) {
         String input = Console.readLine(); //문자열 입력받기.
+        String[] numbers = splitNumbers(input); //구분자로 구분한 후 추출한 숫자
     }
 
     public static String extractDelimiter(String input){ // "//(.)\\\\n(.*)" 패턴에서 구분자 추출하여 반환.
@@ -19,5 +20,15 @@ public class Application {
             return matcher.group(1);
         }
         return null;
+    }
+
+    public static String[] splitNumbers(String input){ // 구분자를 기준을 문자로된 숫자 추출 후 반환.
+        String delimiters = ",:"; // 기본 구분자
+        String customDelimiter = extractDelimiter(input); // 커스텀 구분자 추출
+        if(customDelimiter != null){ // 커스텀 구분자 존재시 기본 구분자에 추가
+            delimiters += customDelimiter;
+            input = input.substring(5); //커스텀 구분자 패턴 삭제
+        }
+        return input.split("[" + delimiters + "]");
     }
 }
