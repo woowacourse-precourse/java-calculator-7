7bcfc53b5f5a5614e8689ebb8d9a4e0d37980201
JoonKyo <joonkyolee@gmail.com>
2024-10-20 00:55:08 +0900

    feat(SeparatorValidator): 입력한 문자열에서 커스텀 구분자 포맷을 제거하는 메서드 구현

--- src/main/java/calculator/domain/SeparatorValidator.java
+++ src/main/java/calculator/domain/SeparatorValidator.java
@@ -4,19 +4,26 @@ import java.util.List;
 
 public class SeparatorValidator {
     private static final String CUSTOM_START_LETTERS = "//";
-    private static final String CUSTOM_END_LETTERS = "\\n";출
+    private static final String CUSTOM_END_LETTERS = "\\n";
 
     public SeparatorValidator() {
     }
 
-    public static List<String> extractCustomSeparator(List<String> separator, String inputData) {
+    public static List<String> extractCustomSeparator(List<String> separators, String inputData) {
         if (containsCustomSeparator(inputData)) {
-            separator.add(inputData.substring(CUSTOM_START_LETTERS.length(), inputData.indexOf(CUSTOM_END_LETTERS)));
+            separators.add(inputData.substring(CUSTOM_START_LETTERS.length(), inputData.indexOf(CUSTOM_END_LETTERS)));
         }
-        return separator;
+        return separators;
+    }
+
+    public static List<String> removeSeparator(List<String> separators, String inputData) {
     }
 
     private static boolean containsCustomSeparator(String inputData) {
         return inputData.startsWith(CUSTOM_START_LETTERS) && inputData.contains(CUSTOM_END_LETTERS);
+    }
+
+    private static String removeCustomSeparatorFormat(String inputData) {
+        return inputData.substring(inputData.indexOf(CUSTOM_END_LETTERS) + CUSTOM_END_LETTERS.length());
     }
 }
