7b235a3656a886585be7674d40e897fb15f2e5c8
chyo1 <hyowon3856@gmail.com>
2024-10-20 03:30:31 +0900

    REFACTOR : 프로그램 실행 부분 변경
    기존 Calculator를 실행
    -> executor에 필요한 객체 생성 후 전달하여 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,8 +3,12 @@ package calculator;
 public class Application {
 	public static void main(String[] args) {
 
+		IOController ioController = new IOController();
+		Parser parser = new Parser();
 		Calculator calculator = new Calculator();
 
-		calculator.run();
+		Executor executor = new Executor(ioController, parser, calculator);
+
+		executor.executeApplication();
 	}
 }
