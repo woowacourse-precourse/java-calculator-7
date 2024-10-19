b1f89de5b368984fa6f8e55a8e860a669ad06aa2
LeeJU <thirdspirit@naver.com>
2024-10-19 20:36:31 +0900

    feat(application): implementation features
    Implement data input.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        ArrayList<String> numbers = new ArrayList<>();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -4,8 +4,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,11 +16,47 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3,4:5");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
+    @Test
+    void 구분자_에러_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 숫자_하나_테스트() {
+        assertSimpleTest(() -> {
+            run("1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_에러_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//;1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException(";\\n1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void Long_범위_초과_숫자_연산_테스트() {
+        assertSimpleTest(() -> {
+            run("123456789123456789123456789123456789123456789:123456789123456789123456789123456789123456789");
+            assertThat(output()).contains("결과 : 246913578246913578246913578246913578246913578");
+        });
     }
 
     @Override
