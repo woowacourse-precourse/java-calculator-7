15cc7ffa1f500e378b220d699b0df05aeeda5cf8
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-10-19 18:53:17 +0900

    refactor(calculator): extract 로직을 단순하게 만들어라

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -4,14 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator implements StringCalculator {
     private static final int DEFAULT_VALUE = 0;
     private static final String DEFAULT_DELIMITER = ",|:";
-    private static final int CUSTOM_DELIMITER_ORDER = 1;
-    private static final int ARITHMETIC_EXPRESSION_ORDER = 2;
+    private static final int CUSTOM_DELIMITER_ORDER = 0;
+    private static final int ARITHMETIC_EXPRESSION_ORDER = 1;
     public static final Set<Character> ALLOWED_DELIMITERS = Set.of(';', ',');
 
     @Override
@@ -38,15 +37,10 @@ public class StringAddCalculator implements StringCalculator {
     }
 
     private DelimiterAndNumber extractCustomDelimiterAndNumbers(String input) {
-        String delimiters = DEFAULT_DELIMITER;
-        String numbersString = input;
-        Matcher matcher = CustomRegexMatcher.getMatcher(input);
-        if (matcher.find()) {
-            String customDelimiter = matcher.group(CUSTOM_DELIMITER_ORDER);
-            numbersString = matcher.group(ARITHMETIC_EXPRESSION_ORDER);
-            // 주의!!! "|"는 기본 구분자에 커스텀 구분자를 추가하려면 반드시 더해주어야 함.
-            delimiters += "|" + Pattern.quote(customDelimiter);
-        }
+        String[] parts = input.split("\\\\n", 2);
+        String customDelimiter = parts[CUSTOM_DELIMITER_ORDER];
+        String numbersString = parts[ARITHMETIC_EXPRESSION_ORDER];
+        String delimiters = DEFAULT_DELIMITER + "|" + Pattern.quote(customDelimiter);
         return new DelimiterAndNumber(delimiters, numbersString);
     }
 
