c14f90b1df5727602bedb386657a1476af4dbb47
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-20 04:02:23 +0900

    refactor: 입력부 class로 분리

---
+++ src/main/java/calculator/Reader.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class Reader {
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해주세요.";
+    
+    public static void printInputMessage() {
+        System.out.println(INPUT_MESSAGE);
+    }
+}
