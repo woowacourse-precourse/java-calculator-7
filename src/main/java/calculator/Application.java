7fdb922f0aa0640f48b7ea0797b37221640e0c0e
kingrangE <yuvw123@gmail.com>
2024-10-20 14:27:19 +0900

    fix(Application): Add Business Logic Start Code
    비즈니스 로직을 시작시키는 코드를 추가하였습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.controller.SplitController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        SplitController splitController = new SplitController();
+        splitController.process();
     }
 }
