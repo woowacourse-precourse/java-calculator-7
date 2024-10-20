0de22f16d1b7092873536565ed4ccc53763688a5
JoonKyo <joonkyolee@gmail.com>
2024-10-20 21:22:27 +0900

    feat(InputData): 사용자가 비어있는 문자열을 입력했을 때 처리하는 과정 구현

--- src/main/java/calculator/domain/InputData.java
+++ src/main/java/calculator/domain/InputData.java
@@ -4,15 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputData {
+    private static final String EMPTY_STRING = "";
     private static final String SEPARATOR_COMMA = ",";
     private static final String SEPARATOR_COLON = ":";
-    private final List<String> separators;
     private final String firstInputData;
+    private List<String> separators;
+    private String[] extractedStrings;
+    private List<Integer> numbersForSum;
 
     public InputData(String firstInputData) {
         this.firstInputData = firstInputData;
         separators = new ArrayList<>();
         separators.add(SEPARATOR_COLON);
         separators.add(SEPARATOR_COMMA);
+        extractedStrings = new String[];
+        numbersForSum = new ArrayList<>();
+    }
+
+    private List<Integer> handleInputString() {
+        if (hasEmptyString()) {
+            numbersForSum.add(0);
+            return numbersForSum;
+        }
+    }
+
+    private boolean hasEmptyString() {
+        return firstInputData.equals(EMPTY_STRING);
     }
 }
