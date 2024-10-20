02e983299d28477f3ddfb69982fd82ee57ff20fc
yunjeooong <yunjeongssu@gmail.com>
2024-10-20 23:12:58 +0900

    feat: 사용자 입력 처리 기능 추가
    -InputView 클래스 생성
    -Console을 사용하여 사용자 입력 받기 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,15 @@
 package calculator;
 
+import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-    }
 
+        String input = inputView.getInput();
+
+    }
 }
