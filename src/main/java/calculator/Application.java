af99ec793d7d94097efa08dd98aa6abf51a3eaa9
song hyeongyu <lit3207@ajou.ac.kr>
2024-10-20 12:46:38 +0900

    refactor: remove unused import

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,16 +1,12 @@
 package calculator;
 
 import calculator.controller.Process;
-import calculator.exception.SeparatorException;
 
-public class Application {
+public class Application{
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try {
-            Process process = new Process();
-            process.run();
-        } catch (SeparatorException e) {
-            System.out.println(e.getMessage());
-        }
+        Process process = new Process();
+        process.run();
+
     }
 }
