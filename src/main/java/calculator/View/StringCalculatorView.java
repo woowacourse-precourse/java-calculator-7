9e224506a677aeaf74ae4b1c51460e00d6ef3b01
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-21 04:05:38 +0900

    feat(StringCalculatorView): 계산된 합계를 출력하는 기능 구현
    - 넘겨받은 DTO에서 getSum() 메서드를 이용해서 합계 출력

--- src/main/java/calculator/View/StringCalculatorView.java
+++ src/main/java/calculator/View/StringCalculatorView.java
@@ -1,4 +1,11 @@
 package calculator.View;
 
+import calculator.DTO.StringCalculatorDTO;
+
 public class StringCalculatorView {
+    // 결과를 출력하는 메서드
+    public void displayResult(StringCalculatorDTO inputDTO) {
+        // DTO에서 합계를 가져와 출력
+        System.out.println("결과 : " + inputDTO.getSum());
+    }
 }
