477ef05fd7c4d930c480478a0a7472932d69f7ab
pil-go <pilbongx@naver.com>
2024-10-21 07:55:37 +0900

    feat(DelimiterValidator): 커스텀 구분자 개수 초과 예외처리 추가

--- src/main/java/calculator/DelimiterValidator.java
+++ src/main/java/calculator/DelimiterValidator.java
@@ -1,13 +1,23 @@
 package calculator;
 
 public class DelimiterValidator {
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+    private static final int CUSTOM_DELIMITER_LIMIT = 1;
+
     public String getCustomDelimiter(String input) {
         //커스텀 구분자가 있다면 반환, 없다면 "" 반환
-        String result = "";
-        if (input.matches("//.\\\\n.*")) {
-            result = input.substring(2, 3);
+        String delimiter = "";
+        int start = input.indexOf(CUSTOM_DELIMITER_PREFIX);
+        int end = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+
+        if (start != -1 && end != -1) {
+            delimiter = input.substring(start + CUSTOM_DELIMITER_PREFIX.length(), end);
+        }
+        if (delimiter.length() > CUSTOM_DELIMITER_LIMIT) {
+            throw new RuntimeException("커스텀 구분자는 문자 " + CUSTOM_DELIMITER_LIMIT + "개까지만 지정 가능합니다. 입력한 구분자: " + delimiter);
         }
 
-        return result;
+        return delimiter;
     }
 }
