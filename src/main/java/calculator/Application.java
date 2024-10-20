e075ddea07ef343451e1d9db8de6ee941045d5a3
HEX <123macanic@naver.com>
2024-10-20 12:00:23 +0900

    feat: Input 클래스에 문자열 입력 메서드 구현
    추가된 기능
    - 문자열을 입력하고 해당 문자열을 반환하는 메서드

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Input.inputString();
+    }
+}
+
+class Input {
+    public static String inputString() {
+        System.out.println("Please enter the strings to add.");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
     }
 }
