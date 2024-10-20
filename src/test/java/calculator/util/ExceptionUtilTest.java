cc875a099e701b04fbfeece90747eea16f64fc17
qudcks8084 <qudcks8084@naver.com>
2024-10-20 19:12:10 +0900

    test(util) : add 2 test case in Util
    Detail
    - Add 2 test case to test new method
    	- 1. When normal case
    	- 2. When Custom delim

--- src/test/java/calculator/util/ExceptionUtilTest.java
+++ src/test/java/calculator/util/ExceptionUtilTest.java
@@ -13,22 +13,22 @@ class ExceptionUtilTest {
 
     @Test
     @DisplayName("커스텀 구분 문자열 X 내용이 비어있는 경우")
-    void checkNoStr(){
+    void checkNoStr() {
         //given
         String str = "";
         //then
-        Assertions.assertThrows(IllegalArgumentException.class,
+        assertThrows(IllegalArgumentException.class,
                 //when
                 () -> checkParamsIsNotEmpty(str));
     }
 
     @Test
     @DisplayName("커스텀 구분자가 비어있는 경우")
-    void checkNoCustomDelim(){
+    void checkNoCustomDelim() {
         //given
         String str = "//\n1?2?3";
         //then
-        Assertions.assertThrows(IllegalArgumentException.class,
+        assertThrows(IllegalArgumentException.class,
                 //when
                 () -> checkParamsIsNotEmpty(str));
     }
@@ -39,52 +39,74 @@ class ExceptionUtilTest {
         //given
         String str = "//?\n";
         //then
-        Assertions.assertThrows(IllegalArgumentException.class,
+        assertThrows(IllegalArgumentException.class,
                 //when
                 () -> checkParamsIsNotEmpty(str));
     }
 
     @Test
     @DisplayName("계산되는 다음 값이 0보다 작은 숫자인 경우")
-    void nextTargetIsLowThanZero(){
+    void nextTargetIsLowThanZero() {
         //given
         String str = "-1";
         //then
-        Assertions.assertThrows(IllegalArgumentException.class,
+        assertThrows(IllegalArgumentException.class,
                 //when
                 () -> checkNextTargetIsLiteral(str));
     }
 
     @Test
     @DisplayName("계산되는 다음 값이 숫자가 아닌 경우")
-    void nextTargetIsNotLiteral1(){
+    void nextTargetIsNotLiteral1() {
         //given
         String str = " ";
         //then
-        Assertions.assertThrows(IllegalArgumentException.class,
+        assertThrows(IllegalArgumentException.class,
                 //when
                 () -> checkNextTargetIsLiteral(str));
     }
 
     @Test
     @DisplayName("계산되는 다음 값이 숫자가 아닌 경우")
-    void nextTargetIsNotLiteral2(){
+    void nextTargetIsNotLiteral2() {
         //given
         String str = "?";
         //then
-        Assertions.assertThrows(IllegalArgumentException.class,
+        assertThrows(IllegalArgumentException.class,
                 //when
                 () -> checkNextTargetIsLiteral(str));
     }
 
     @Test
     @DisplayName("계산되는 다음 값이 정상적인 경우")
-    void nextTargetIsBiggerThanZero(){
+    void nextTargetIsBiggerThanZero() {
         //given
         String str = "20";
         //when
         int num = checkNextTargetIsLiteral(str);
         //then
         assertThat(num).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자와 계산 대상의 개수를 확인")
+    void checkNumOfDelimAndTarget1(){
+        //given
+        CalculatorDTO calculatorDTO = new CalculatorDTO("//?\\n1?2?");
+
+        //when
+        assertThrows(IllegalArgumentException.class,
+                () -> checkStringHasRightNumOfTarget(calculatorDTO));
+    }
+
+    @Test
+    @DisplayName("일반 구분자와 계산 대상의 개수를 확인")
+    void checkNumOfDelimAndTarget2(){
+        //given
+        CalculatorDTO calculatorDTO = new CalculatorDTO(",2,3");
+
+        //when
+        assertThrows(IllegalArgumentException.class,
+                () -> checkStringHasRightNumOfTarget(calculatorDTO));
     }
 }
