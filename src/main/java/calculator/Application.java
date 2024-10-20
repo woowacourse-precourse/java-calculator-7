4a0c661c3173839ad51778b4d28ee9514c98b867
gorapaduckoo <jioung93@naver.com>
2024-10-21 02:04:11 +0900

    refactor: Prompt의 입출력 핸들링 로직을 Calculator로 이동
    - Prompt의 입출력 핸들링 로직을 Calculator로 이동
    - 이동한 로직은 run()이라는 별도 메서드로 분리
    - Prompt 클래스 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,9 +2,10 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        Prompt prompt = new Prompt();
+        InputHandler inputHandler = new InputHandler();
+        OutputHandler outputHandler = new OutputHandler();
         Calculator calculator = new Calculator();
 
-        prompt.getCommand(calculator);
+        calculator.run(inputHandler, outputHandler);
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,6 +2,15 @@ package calculator;
 
 public class Calculator {
 
+    public void run(InputHandler inputHandler, OutputHandler outputHandler) {
+        outputHandler.printInitMessage();
+
+        int[] numbers = inputHandler.getInput();
+        int result = calculate(numbers);
+
+        outputHandler.printResult(result);
+    }
+
     public int calculate(int[] numbers) {
         int result = 0;
         for (int number : numbers) {
