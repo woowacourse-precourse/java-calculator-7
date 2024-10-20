aa5aa7637420ab4c4ac11be404e261108f6368e2
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-20 16:35:39 +0900

    style: 예외처리 문구 상수화

--- src/main/java/calculator/StringParsing.java
+++ src/main/java/calculator/StringParsing.java
@@ -4,11 +4,12 @@ public class StringParsing {
 
     private static String[] userInputArr;
     private static final String IS_NOT_NUMBER = "숫자가 아닌 값이 포함되어 있습니다";
+    private static final String IS_NOT_POSITIVE = "양수가 아닙니다.";
 
     public static String[] splitNpushString(String userInput, String delimiter) {
         for (int i = 0; i < userInput.length(); i++) {
             if (!Character.isDigit(userInput.charAt(i))) {
-                throw new IllegalArgumentException("양수가 아닙니다");
+                throw new IllegalArgumentException(IS_NOT_POSITIVE);
             }
         }
         userInputArr = userInput.split(delimiter);
