1dc61748796ef2380747008773191fcefce85396
song hyeongyu <lit3207@ajou.ac.kr>
2024-10-21 02:53:49 +0900

    refactor: revise calculateSum logic

--- src/main/java/calculator/runner/CalculatorRunner.java
+++ src/main/java/calculator/runner/CalculatorRunner.java
@@ -29,41 +29,31 @@ public class CalculatorRunner {
             temp = addStringToCharSet(newSeparator);
         }
         String number = splitString(userInput);
-        if (temp.contains(number.charAt(0))) {
-            throw new IllegalArgumentException("입력은 구분자로 시작할 수 없습니다.");
-        }
         IOHelper.printResult(calculateSum(number,temp));
     }
 
     public int calculateSum(String inputString,Set<Character> newSeparator) {
         StringBuilder number = new StringBuilder();
-        int sum = 0;
-        boolean lastCharWasSeparator = false;
-
+        int num = 0;
         for (char ch : inputString.toCharArray()) {
             if (Character.isDigit(ch)) {
                 number.append(ch);
-                lastCharWasSeparator = false;
+                continue;
             } else if (newSeparator.contains(ch)) {
-                if (lastCharWasSeparator) {
+                if (number.isEmpty()) {
                     throw new IllegalArgumentException("구분자가 연속으로 들어왔습니다.");
                 }
-
-                if (number.length() > 0) {
-                    sum += Integer.parseInt(number.toString());
-                    number.setLength(0);
-                }
-                lastCharWasSeparator = true;
-            } else {
-                // 잘못된 구분자가 있는 경우 예외 발생
-                throw new IllegalArgumentException("잘못된 구분자입니다.");
+                num += Integer.parseInt(number.toString());
+                number = new StringBuilder();
+                continue;
             }
+            throw new IllegalArgumentException("잘못된 구분자.");
         }
 
-        if (number.length() > 0) {
-            sum += Integer.parseInt(number.toString());
+        if (!number.isEmpty()) {
+            num += Integer.parseInt(String.valueOf(number));
         }
 
-        return sum;
+        return num;
     }
 }
