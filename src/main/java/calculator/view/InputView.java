2f621d41902fc4f910b82b34908531d0b687f6fc
rootTiket <l20078829@gmail.com>
2024-10-20 18:52:47 +0900

    feat: 문자열 입력 view 클래스 추가

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,12 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputString() {
+        String input = Console.readLine();
+
+        return input;
+    }
+}
