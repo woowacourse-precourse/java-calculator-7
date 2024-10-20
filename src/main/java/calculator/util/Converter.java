d3077aea4dca83072614259be283a2fd35e93e2e
chanwon <clw8679@naver.com>
2024-10-20 22:58:54 +0900

    refactor: 문자열 분리 기능 리팩토링

--- src/main/java/calculator/util/Converter.java
+++ src/main/java/calculator/util/Converter.java
@@ -1,30 +1,25 @@
 package calculator.util;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Converter {
-    public static List<Integer> stringToNumbers(String string) {
-        String expression = string;
-        String customSeparator = "";
+    public static List<Integer> stringToNumbers(String input) {
+        String expression = input;
+        String delimiter = ",|:";
 
         if (expression.startsWith("//")) {
             int separatorIndex = expression.indexOf("\n");
-            customSeparator = expression.substring(2, separatorIndex);
+            String customSeparator = expression.substring(2, separatorIndex);
             expression = expression.substring(separatorIndex + 1);
-        }
-
-        String delimiter = ",|:";
-        if (!customSeparator.isEmpty()) {
             delimiter += "|" + customSeparator;
         }
 
         String[] tokens = expression.split(delimiter);
-        List<Integer> numbers = new ArrayList<>();
-        for (String token : tokens) {
-            numbers.add(Integer.parseInt(token.trim()));
-        }
 
-        return numbers;
+        return Arrays.stream(tokens)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
