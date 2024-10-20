b72bcf44d30e7f2e09744c8ef8a66b809b2b0597
gomin0 <gomin0981113@naver.com>
2024-10-20 16:04:03 +0900

    fix: Application 예외를 잡지 말고 그대로 던지도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,11 +9,7 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        try {
-            int result = calculator.calculate(input);
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 입력입니다.");
-        }
+        int result = calculator.calculate(input);
+        System.out.println("결과 : " + result);
     }
 }
