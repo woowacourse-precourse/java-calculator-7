0c73589bef81b19ce48962e78f19e114590cdaf1
ZZKIM <rkgml424@naver.com>
2024-10-20 18:01:28 +0900

    add: Calculator Class

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
+
+    private static int Calculator(String st){
+        int result = 0;
+
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = sc.nextLine();
+
+        int answer = Calculator(input);
+        System.out.printf("결과 : %d", answer);
     }
 }
