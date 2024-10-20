cb9e2b262aaa6a5ee75f5c11f54b6180cdcf0d53
jongsic17 <qkrwhdtlr178@naver.com>
2024-10-21 08:22:28 +0900

    refactor: 변수명 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,63 +2,59 @@ package calculator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static camp.nextstep.edu.missionutils.Console.*;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String a = readLine();
-        System.out.println("결과 : " + Calculator(a));
-
+        System.out.println("결과 : " + calculate(a));
     }
 
-
-    public static int Calculator(String str){
+    public static int calculate(String text){
         int sum = 0;
-        String[] s = {"",""};
+        String[] separatorAndText = {"",""};
         String separator = "[,:]";
-        if(str.isEmpty()){
+        if(text.isEmpty()){
             return 0;
         }
-        if(isPattern(str)){
-            s = CustomSeparator(str);
-            separator = s[0];
-            str = s[1];
+        if(isPattern(text)){
+            separatorAndText = CustomSeparator(text);
+            separator = separatorAndText[0];
+            text = separatorAndText[1];
         }
 
-        String[] x = str.split(separator);
-        for (String string : x) {
-            int z = Integer.parseInt(string);
-            if(z < 0){
+        String[] suspectedNumbers = text.split(separator);
+
+        for (String suspectedNumber : suspectedNumbers) {
+            int parsedNumber = Integer.parseInt(suspectedNumber);
+            if(parsedNumber < 0){
                 throw new IllegalArgumentException("음수가 입력되었습니다.");
             }
-            sum += z;
+
+            sum += parsedNumber;
         }
         return sum;
     }
-    public static String[] CustomSeparator(String str){
-        String[] s = {"",""};
+
+    public static String[] CustomSeparator(String text){
+        String[] separatorAndText = {"",""};
         Pattern pattern = Pattern.compile("//(.*?)(\\\\n)");
-        Matcher matcher = pattern.matcher(str);
+        Matcher matcher = pattern.matcher(text);
 
         if(matcher.find()){
-            s[0] = matcher.group(1);
-            s[1] = str.replace(matcher.group(0), "");
-            return s;
+            separatorAndText[0] = matcher.group(1);
+            separatorAndText[1] = text.replace(matcher.group(0), "");
+            return separatorAndText;
         }else{
             return new String[0];
         }
     }
-    public static boolean isPattern(String str){
-        boolean isPattern = false;
+    public static boolean isPattern(String text){
         Pattern pattern = Pattern.compile("//(.*?)\\\\n");
-        Matcher matcher = pattern.matcher(str);
-        if(matcher.find()){
-            isPattern = true;
-        }
-        return isPattern;
-    }
+        Matcher matcher = pattern.matcher(text);
 
+        return matcher.find();
+    }
 }
