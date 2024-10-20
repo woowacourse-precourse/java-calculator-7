34f458b8b3f34292273c7a4f160de5edec8f20a6
longrunpc <pcjmo4050@gmail.com>
2024-10-21 04:09:52 +0900

    feat(output): IOHandler에 output 메서드 추가
    IOHandler 클래스에 계산 결과를 출력하는 output 메서드를 추가

--- src/main/java/calculator/handler/IOHandler.java
+++ src/main/java/calculator/handler/IOHandler.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class IOHandler {
     // 입력 및 출력 메시지를 상수로 캡슐화
     private static final String INPUT_PROMPT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String OUTPUT_RESULT_MESSAGE = "결과 : ";
     private static final String ERROR_MESSAGE_EMPTY_INPUT = "입력값이 존재하지 않습니다.";
 
     /**
@@ -13,12 +14,21 @@ public class IOHandler {
      *
      * @return 사용자 입력 문자열
      */
-    public String input() {
+    public static String input() {
         System.out.println(INPUT_PROMPT_MESSAGE);
         String input = Console.readLine();
         if (input.isEmpty()) {
             throw new IllegalArgumentException(ERROR_MESSAGE_EMPTY_INPUT);
         }
         return input;
+    }
+
+    /**
+     * 계산 결과를 출력하는 메서드.
+     *
+     * @param result 출력할 결과 값
+     */
+    public static void output(int result) {
+        System.out.println(OUTPUT_RESULT_MESSAGE + result);
     }
 }
