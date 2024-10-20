87751c54c7639688ccac8775414525b516ec4f70
zi-eunn <jieunkim1117@gmail.com>
2024-10-20 21:25:14 +0900

    feat(Application): 커스텀 구분자 기능 추가 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,24 +4,41 @@ import java.util.Scanner;
 
 public class Application {
     public static int add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
+        String delimiter = "[,:]";
+        String numberString = numbers;
+
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\\n");
+            if (delimiterIndex != -1) {
+                delimiter = escapeSpecialRegexChars(numbers.substring(2, delimiterIndex));
+                numberString = numbers.substring(delimiterIndex + 2);
+            } else {
+                System.out.println("error0");
+                return 0;
+            }
         }
 
-        String[] nums = numbers.split("[,:]");
+        String[] nums = numberString.split(delimiter);
         int sum = 0;
 
         for (String num : nums) {
-            sum += Integer.parseInt(num.trim());
+            try {
+                sum += Integer.parseInt(num.trim());
+            } catch (NumberFormatException e) {
+                System.out.println("error" + num);
+                return 0;
+            }
         }
 
         return sum;
     }
 
+    public static String escapeSpecialRegexChars(String delimiter) {
+        return delimiter.replaceAll("([\\\\.*+?^${}()|\\[\\]])", "\\\\$1");
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         Scanner sc = new Scanner(System.in);
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = sc.nextLine();
         int result = add(input);
         System.out.println("결과 : " + result);
