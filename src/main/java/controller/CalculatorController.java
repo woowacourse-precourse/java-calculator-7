21ff43c8d4eee105fafcb3e7ed44e8e717c23a3e
sinji <tobeabestns@naver.com>
2024-10-20 21:49:45 +0900

    feat: 덧셈 계산을 하는 컨트롤러 구현

--- src/main/java/controller/CalculatorController.java
+++ src/main/java/controller/CalculatorController.java
@@ -1,4 +1,26 @@
 package controller;
 
+import domain.Calculator;
+import domain.Separator;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
 public class CalculatorController {
+    private final Calculator calculator;
+    private final Separator separator;
+
+    public CalculatorController(Separator separator) {
+        this.separator = separator;
+        this.calculator = new Calculator(separator);
+    }
+
+    public void run() {
+        String input = InputView.inputString();
+        separator.findAndAddSeparator(input);
+        List<Integer> numbers = separator.extractionNumbers(input);
+        calculator.addNumber(numbers);
+        OutputView.printResult(calculator);
+    }
 }
