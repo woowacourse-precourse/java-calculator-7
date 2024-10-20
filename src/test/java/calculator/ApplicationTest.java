24d17939320656cb38ee8b3941ed948f2e90c1e2
llcodingll <ll.o.llwhkr@gmail.com>
2024-10-20 20:50:24 +0900

    test: 숫자 0 포함인 경우, 덧셈 작동 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -58,6 +58,13 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 숫자_0_처리_테스트() {
+        assertSimpleTest(() -> {
+            run("0,1,2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
 
     @Override
     public void runMain() {
