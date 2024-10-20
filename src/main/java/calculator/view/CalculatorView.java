1249f8a3d7f886d6ebf496abb3c27aa6e56092b5
kim jihyeon <jihyeon546@gmail.com>
2024-10-20 18:36:35 +0900

    feat: 문자열 공백 제거 기능 구현

--- src/main/java/calculator/view/CalculatorView.java
+++ src/main/java/calculator/view/CalculatorView.java
@@ -6,7 +6,7 @@ public class CalculatorView {
 
     public String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine();
+        return Console.readLine().trim();
     }
 
     public void showOutput(int result) {
