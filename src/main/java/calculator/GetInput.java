e0ce1b3b98dfe132a55957b71c996bb0282cbf8b
AYEOOON <k93853471@gmail.com>
2024-10-20 15:23:35 +0900

    feat: 사용자 입력 방식을 Console로 변경
    - Scanner 대신 camp.nextstep.edu.missionutils.Console의 readLine() 메서드를 사용하여 사용자 입력을 처리하도록 수정

--- src/main/java/calculator/GetInput.java
+++ src/main/java/calculator/GetInput.java
@@ -1,13 +1,12 @@
 package calculator;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class GetInput {
 
     // 사용자의 입력을 받는 메서드
     static String input_String() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        Scanner sc = new Scanner(System.in);
-        return sc.nextLine().replace("\\n", "\n"); // 문자열에서 "\n"을 실제 줄바꿈 문자로 변환
+        return Console.readLine().replace("\\n", "\n"); // 문자열에서 "\n"을 실제 줄바꿈 문자로 변환
     }
 }
