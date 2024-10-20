622c1f0ccbc0e604ed984abd7faec550c0ec3403
Gyulguma <ybg653924@gmail.com>
2024-10-20 22:00:05 +0900

    feat(CustomParser): 커스텀 구분자로 숫자 추출 기능 추가
    사용자 입력 값에서 숫자를 추출하는 CustomParser 클래스에
    커스텀 구분자로 추출하는 로직을 구현 및 예외처리 추가

--- src/main/java/calculator/CustomParser.java
+++ src/main/java/calculator/CustomParser.java
@@ -6,28 +6,37 @@ public class CustomParser {
     private final String BASIC_SEPARATOR;
 
     public CustomParser() {
-        this.BASIC_SEPARATOR = "[,:]";
+        this.BASIC_SEPARATOR = ",|:";
     }
 
     public int[] parse(String input) {
-        if (input.isEmpty()) { // 빈 문자열일 경우 0반환
+        String regx = "([0-9][,:])+[0-9]";
+        if (input.matches(regx)) { // 기본 구분자를 사용할 경우
+            return parse(input, this.BASIC_SEPARATOR);
+        }
+        if (input.matches("//.\\\\n.*")) { // 커스텀 구분자를 사용할 경우
+            String separator = input.charAt(2) + "";
+            String temp = input.substring(5);
+            return parse(temp, this.BASIC_SEPARATOR + "|" + separator);
+        }
+        throw new IllegalArgumentException("입력 형식을 확인해주세요.");
+    }
+
+    private int[] parse(String input, String separator) {
+        if (input.isEmpty()) { // 빈 문자열일 경우
             return new int[]{0};
         }
-
-        int[] result = null;
-
+        int[] result;
         try {
-            result = Arrays.stream(input.split(this.BASIC_SEPARATOR)).mapToInt(Integer::parseInt).toArray();
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("입력 형식을 확인해주세요(숫자와 기본 구분자만 허용)");
+            result = Arrays.stream(input.split(separator)).mapToInt(Integer::parseInt).toArray();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("입력 형식을 확인해주세요. (문자 x, 구분자는 숫자 사이에만 존재할 수 있습니다)");
         }
-
         Arrays.stream(result).forEach(i -> {
             if (i < 0) {
                 throw new IllegalArgumentException("음수는 허용하지 않습니다.");
             }
         });
-
         return result;
     }
 
