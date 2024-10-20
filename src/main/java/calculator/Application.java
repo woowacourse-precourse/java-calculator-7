b02c4d73dd7443b6e1926bd15cb8d9eca8637d6a
seungheejang <justinholla908@gmail.com>
2024-10-20 20:26:52 +0900

    feat: 사용자 입력값 받기를 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         /** 기능 1 구현 */
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        /** 기능 2 구현 */
+        String inputData = Console.readLine();
     }
 }
