270905fc844a5400cacca6716c7cd54f074f86c3
JiEung2 <181757@jnu.ac.kr>
2024-10-20 20:37:08 +0900

    test(inputValidatorTest): 등록되지 않은 구분자 검증 테스트 추가

--- src/test/java/calculator/InputValidatorTest.java
+++ src/test/java/calculator/InputValidatorTest.java
@@ -1,6 +1,8 @@
 package calculator;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class InputValidatorTest {
@@ -10,7 +12,16 @@ class InputValidatorTest {
     @Test
     void 빈_문자열_입력_처리_테스트() {
         String input = "";
-        Assertions.assertThat(inputValidator.isEmptyInput(input)).isTrue();
+        assertThat(inputValidator.isEmptyInput(input)).isTrue();
+    }
+
+    @Test
+    void 등록되지_않은_구분자_검증_테스트() {
+        List<String> delimiters = List.of(";",":",",");
+
+        assertThatThrownBy(() -> inputValidator.validateUnregisteredDelimiter(delimiters))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("등록되지 않은 구분자가 포함되어 있습니다.");
     }
 
 }
