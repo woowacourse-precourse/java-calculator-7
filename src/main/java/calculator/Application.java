ea322c707679f1d5773e769958be66ac034815d7
Jinyshin <iwbm312@gmail.com>
2024-10-20 21:29:19 +0900

    feat(input): implement input handler
    separated responsibility by creating InputHandler class to manage user input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,9 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+        Calculator calculator = new Calculator();
+
+        String inputStr = inputHandler.getInput();
     }
 }
---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    private final ArrayList<Integer> operands;
+
+    public Calculator() {
+        operands = new ArrayList<>();
+    }
+
+    public void parseAndAddNumbers(String input) {
+
+    }
+
+    public void sum() {
+
+    }
+}
