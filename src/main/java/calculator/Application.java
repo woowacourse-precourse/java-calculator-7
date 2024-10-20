df2222fa8a07a0371d487a4ecc2f41e444d518ae
Hyoseop Song <crad_on25@naver.com>
2024-10-21 03:27:07 +0900

    feat(Applcation): 계산기 프로그램 실행기점 설정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,8 @@
 package calculator;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		UserConsoleView userConsoleView = new UserConsoleView();
+		userConsoleView.run();
+	}
 }
