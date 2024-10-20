5dae01ede33ebf611db4633f262deedeb1af599b
한관희 <noop103@naver.com>
2024-10-20 14:53:48 +0900

    feat: Main 메소드에서 문자열 기반 계산기가 작동하도록 코드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,37 @@
 package calculator;
 
+import calculator.splitter.DefaultStringSplitter;
+import calculator.validator.DefaultValidator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        try {
+            String customDelimiter = extractCustomDelimiter(input);
+
+            StringCalculator calculator = StringCalculator.create(
+                    DefaultStringSplitter.create(),
+                    DefaultValidator.create(customDelimiter)
+            );
+
+            String result = calculator.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
+
+    private static String extractCustomDelimiter(String input) {
+        if (input.startsWith("//") && input.contains("\n")) {
+            int start = input.indexOf("//") + 2;
+            int end = input.indexOf("\n");
+            return input.substring(start, end);
+        }
+        return "";
     }
 }
