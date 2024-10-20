56bff61a38cc2e025f7474cc373f514506630fe5
euni <kongeuni1004@gmail.com>
2024-10-21 03:40:41 +0900

    feat: 문자열 입력받는 메서드 추가
    - 사용자가 문자열을 입력할 수 있도록 하는 메서드를 작성했습니다.
    - 콘솔에서 입력을 받아 반환하는 방식으로 구현되었습니다.
    - 기본적인 입력 검증 로직은 포함하지 않았으며, 추후 검증 로직을 추가할 예정입니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public String getInputString(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return input;
     }
 }
