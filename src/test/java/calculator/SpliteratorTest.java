ee299abc82ee517f7ba73ca7b869d6749e532dca
kwongio <rldh11111@naver.com>
2024-10-20 17:58:05 +0900

    style: google java format 적용

--- src/test/java/calculator/SpliteratorTest.java
+++ src/test/java/calculator/SpliteratorTest.java
@@ -5,31 +5,31 @@ import org.junit.jupiter.api.Test;
 
 class SpliteratorTest {
 
-    @Test
-    void split() {
-        String[] result = split("[,:]", "1,2,3");
-        Assertions.assertThat(result).isEqualTo((new String[]{"1", "2", "3"}));
-    }
+  @Test
+  void split() {
+    String[] result = split("[,:]", "1,2,3");
+    Assertions.assertThat(result).isEqualTo((new String[] {"1", "2", "3"}));
+  }
 
-    @Test
-    void customSplit() {
-        String[] result = split("[,:]", "1;2;3:4:5", ";");
-        Assertions.assertThat(result).isEqualTo(new String[]{"1", "2", "3", "4", "5"});
-    }
+  @Test
+  void customSplit() {
+    String[] result = split("[,:]", "1;2;3:4:5", ";");
+    Assertions.assertThat(result).isEqualTo(new String[] {"1", "2", "3", "4", "5"});
+  }
 
-    @Test
-    void customSplit1() {
-        String[] result = split("[,:]", "1d2d3d4", "d");
-        Assertions.assertThat(result).isEqualTo(new String[]{"1", "2", "3", "4"});
-    }
+  @Test
+  void customSplit1() {
+    String[] result = split("[,:]", "1d2d3d4", "d");
+    Assertions.assertThat(result).isEqualTo(new String[] {"1", "2", "3", "4"});
+  }
 
-    private static String[] split(String regex, String input) {
-        Spliterator spliterator = new Spliterator(regex);
-        return spliterator.split(input);
-    }
+  private static String[] split(String regex, String input) {
+    Spliterator spliterator = new Spliterator(regex);
+    return spliterator.split(input);
+  }
 
-    private static String[] split(String regex, String input, String extraRegex) {
-        Spliterator spliterator = new Spliterator(regex);
-        return spliterator.split(input, extraRegex);
-    }
+  private static String[] split(String regex, String input, String extraRegex) {
+    Spliterator spliterator = new Spliterator(regex);
+    return spliterator.split(input, extraRegex);
+  }
 }
