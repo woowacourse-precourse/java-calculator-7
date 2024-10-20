4c30df5ffeddeee9c66435e88660d4a9f4ff1356
RedJunKi <bing8821@naver.com>
2024-10-20 17:53:05 +0900

    feat: InputView 클래스 기능 수정
    수정된 기능:
     - Input message 상수로 지정
     - InputView.getInput 메서드 호출시 Input message 보이도록 수정

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -4,7 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
     public String getInput() {
+        System.out.println(INPUT_MESSAGE);
         return Console.readLine();
     }
 }
