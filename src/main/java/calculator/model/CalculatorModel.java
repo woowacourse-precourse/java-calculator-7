1ff3879418b3f291b4e9162a3f061f2ca8ac6bb9
JunBe <wnsqowid0901@gmail.com>
2024-10-20 18:16:40 +0900

    fix: Integer 범위를 넘는 정수 예외 처리 추가

--- src/main/java/calculator/model/CalculatorModel.java
+++ src/main/java/calculator/model/CalculatorModel.java
@@ -1,5 +1,7 @@
 package calculator.model;
 
+import java.math.BigInteger;
+
 public class CalculatorModel {
     public int getResult(String[] digits) {
         int result = 0;
@@ -17,11 +19,21 @@ public class CalculatorModel {
 
     public int exception(String val) {
         int intVal;
+
         try {
             intVal = Integer.parseInt(val);
         } catch (NumberFormatException e) {
+            try {
+                BigInteger bigVal = new BigInteger(val);
+                if (bigVal.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) > 0) {
+                    throw new IllegalArgumentException("너무 큰 수를 입력했습니다.");
+                }
+            } catch (NumberFormatException bigE) {
+                throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+            }
             throw new IllegalArgumentException("숫자만 입력 가능합니다.");
         }
+
 
         if (Integer.parseInt(val) <= 0) {
             throw new IllegalArgumentException("음수, 0 사용 불가");
