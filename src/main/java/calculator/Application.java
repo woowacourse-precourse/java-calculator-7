52b1a89a9fb57d30900d0392fc9581b2b2cb8b74
zerocat36 <Seo.james2020@gmail.com>
2024-10-20 22:26:18 +0900

    feat($Application): add hasCustomDelimiter function
    Added new function to $Application
    - check whether input string has custom delimiter set pattern

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
 public class Application {
+    private static boolean hasCustomDelimiter(String input) {
+        return input.startsWith("//");
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
 }
