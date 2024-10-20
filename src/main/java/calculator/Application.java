135180c331bbcdac84cc1952f218955a4bf110bb
jarvis0720 <gktkddms9631@gmail.com>
2024-10-21 01:59:01 +0900

    feat(Application): 커스텀 구분자 기능추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,25 +2,34 @@ package calculator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         String str = readLine();
+        int result = 0;
+        String[] numbers;
         if (str.isEmpty()) {
             System.out.println("결과 : 0");
             return;
         }
-        if (str.contains("//")){
-
+        StringBuilder seperator = new StringBuilder("[:,]");
+        if (str.contains("//") && str.contains("\\n")) {
+            String[] divstr = str.split("\\\\n");
+            String customSeparator = divstr[0].substring(2);
+            seperator.insert(1, customSeparator);
+            numbers = divstr[1].split(seperator.toString());
         }
-        String regex = "[:,]";
-        String[] numbers = str.split(regex);
-        int sum = 0;
+        else{
+            numbers = str.split(seperator.toString());
+        }
 
         int[] intArray = new int[numbers.length];
         for (int i = 0; i < numbers.length; i++) intArray[i] = Integer.parseInt(numbers[i].trim());
 
-        for (int num : intArray) sum += num;
-        System.out.println("결과 : " + sum);
+       for (int num : intArray) result += num;
+       System.out.println("결과 : " + result);
 
     }
 }
