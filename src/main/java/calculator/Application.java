d38d2bdd5bfeba37e96632c5fa8afb4e731a1093
capDoYeonLee <ghtyu21@naver.com>
2024-10-20 19:18:01 +0900

    feat: 입력 공백일 경우, 구분자가 없을 경우 분기점 기능 개발

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -14,9 +14,21 @@ public class Application {
             calculatorStrategy.calculate(inputText);
         }
 
+
         if (inputClass.existOriginalSeparator(inputText)) {
             calculatorStrategy = new OriginalSeparatorStrategy();
             calculatorStrategy.calculate(inputText);
+        }
+
+
+        //입력값이 아무값도 들어오지 않은 경우
+        if (inputClass.isInputBlank(inputText)) {
+            System.out.println("결과 : 0");
+        }
+
+        //입력값이 숫자 또는 문자 또는 문자열이 들어왔을 때 분기점
+        if (inputClass.checkOnlyNumber(inputText)) {
+            System.out.println("결과 : " + inputText);
         }
     }
 }
