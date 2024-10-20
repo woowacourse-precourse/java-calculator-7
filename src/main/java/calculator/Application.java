4489af2d14fd010b8d4212d4ad4220d711693309
Lee Chae Rin <chaerin0916@gmail.com>
2024-10-20 21:43:28 +0900

    커스텀 구분자 readLine 1번 받는 것으로 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,23 +8,17 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-
         int result = 0;
         // "//"로 시작하면 Custom 구분자
         if(input.startsWith("//")){
-            String deli = input.substring(2); // "//" 제거해서 구분자만 받기
-            if(deli.isEmpty()){
-                throw new IllegalArgumentException("구분자가 공백입니다.");
-            }
-            String input_nums = Console.readLine();
+
             CustomInput customInput = new CustomInput();
-            result = customInput.customParseSum(deli, input_nums);
+            result = customInput.customParseSum(input);
         }
         else{
             DefaultInput defaultInput = new DefaultInput();
             result = defaultInput.defaultParseSum(input);
         }
-
         System.out.println("결과 : " + result);
     }
 }
