235a8f5f8aa1400a9acb416853cd755731b2f396
JoonKyo <joonkyolee@gmail.com>
2024-10-20 17:37:53 +0900

    feat(NumberValidator): 분리한 문자열을 숫자로 변환하는 메서드 구현

--- src/main/java/calculator/domain/NumberValidator.java
+++ src/main/java/calculator/domain/NumberValidator.java
@@ -1,4 +1,16 @@
 package calculator.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberValidator {
+
+    public static List<Integer> convertNumbers(String[] separatedString) {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String number : separatedString) {
+            numbers.add(Integer.parseInt(number));
+        }
+        return numbers;
+    }
 }
