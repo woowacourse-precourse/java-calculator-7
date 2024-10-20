0853152c051eaaed1094e7de4b775688930abe63
JINSU <bluesea9519@gmail.com>
2024-10-20 22:01:30 +0900

    feat: print result

--- src/main/java/calculator/io/OutputWriter.java
+++ src/main/java/calculator/io/OutputWriter.java
@@ -3,6 +3,6 @@ package calculator.io;
 public class OutputWriter {
 
     public void writeOutput(Object output) {
-        System.out.println(output);
+        System.out.println("결과 : " + output);
     }
 }
