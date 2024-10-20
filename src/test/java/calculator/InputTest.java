fed50a707a17aaf965781a9f456b9064e7fb39e8
KSJ0128 <ksj01128@naver.com>
2024-10-20 15:55:08 +0900

    test(InputTest): 아무 것도 입력되지 않는 케이스
    - 입력 : 빈 문자열을 주면 No Line Found 에러가 발생해 줄바꿈 문자를 입력으로 넣음.
    
    - 결과 : 아무 입력도 없는 상태로 인식돼 결과 : 0이 출력됨

---
+++ src/test/java/calculator/InputTest.java
@@ -0,0 +1,26 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static calculator.Message.Message.OUTPUT_MESSAGE;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputTest extends NsTest {
+
+    @Test
+    @DisplayName("아무 것도 입력되지 않는 케이스")
+    void blankFormula() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains(OUTPUT_MESSAGE + "0");
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
