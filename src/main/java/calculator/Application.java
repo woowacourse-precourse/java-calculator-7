25f447d7563185ced129970be91d151afe5ad0cd
dfb5361@gmail.com <dfb5361@gmail.com>
2024-10-21 00:13:03 +0900

    feat(Application): add input handling for string addition calculator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,14 +1,26 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         ArrayList<Character> delimiters = new ArrayList<>();
         ArrayList<Integer> numbers = new ArrayList<>();
 
         delimiters.add(',');
         delimiters.add(':');
+
+        try{
+            System.out.print("덧셈할 문자열을 입력해 주세요.");
+            String input = br.readLine();
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
