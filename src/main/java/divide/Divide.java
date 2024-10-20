57a46cc089a9bd11e74cffa9cf6b1a12031e07c5
agi1004 <agi1009@naver.com>
2024-10-20 18:19:49 +0900

    feat: 문자열의 숫자를 구분자로 나누는 인터페이스 생성
    해당 인터페이스의 구현체 목록
    - 기본 구분자로 문자열의 숫자를 구분하는 구현체
    - 커스텀 구분자로 문자열의 숫자를 구분하는 구현체
    문자열 배열을 숫자 배열로 변경하는 디폴트 메서드 선언

--- src/main/java/divide/Divide.java
+++ src/main/java/divide/Divide.java
@@ -1,5 +1,14 @@
 package divide;
 
+import java.util.Arrays;
+
 public interface Divide {
-	void divide(String str);
+	int[] divideNumbers(final String str);
+	
+	// 문자열 배열을 숫자 배열로 변경하는 디폴트 메서드
+	default int[] toIntArr(final String[] strArr) {
+		return Arrays.stream(strArr)
+				.mapToInt(Integer::parseInt)
+				.toArray();
+	}
 }
