2f52712d7aed0661e1df2a7bb896e01188e45f98
gorapaduckoo <jioung93@naver.com>
2024-10-21 01:57:19 +0900

    refactor: Prompt의 츨력 관련 로직을 OutputHandler 클래스로 분리

---
+++ src/main/java/calculator/OutputHandler.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class OutputHandler {
+    void printInitMessage() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
+}
--- src/main/java/calculator/Prompt.java
+++ src/main/java/calculator/Prompt.java
@@ -3,12 +3,13 @@ package calculator;
 public class Prompt {
 
     public void getCommand(Calculator calculator) {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-
         InputHandler inputHandler = new InputHandler();
+        OutputHandler outputHandler = new OutputHandler();
+
+        outputHandler.printInitMessage();
         int[] numbers = inputHandler.getInput();
         int result = calculator.calculate(numbers);
 
-        System.out.println("결과 : " + result);
+        outputHandler.printResult(result);
     }
 }
