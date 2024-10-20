c95ad71ee7e6a42064e2668708c2125dc6a16213
dnwldla <leeemjw@naver.com>
2024-10-21 06:51:32 +0900

    refactor: addRegex 함수가 String을 리턴하게 변경 String 상태를 UserInput에서 변경한다

--- src/main/java/calculator/domain/SeperatorFactory.java
+++ src/main/java/calculator/domain/SeperatorFactory.java
@@ -3,10 +3,9 @@ package calculator.domain;
 public class SeperatorFactory {
     private static final String SPLITOR="|";
     private static final String ESCAPE_CHAR="\\";
-    private static final String DEFAULT_SEPERATOR_REGEX = ",|:";
-    private static String seperatorRegex=",|:";
+    private static String seperatorRegex=",|;";
 
-    public static void addRegex(String customSeperator) {
+    public static String addRegex(String customSeperator) {
         StringBuilder regexBuilder=new StringBuilder();
         //커스텀 구분자가 | 이라면 이스케이프문자를 추가해햐 한다
         if (customSeperator.equals(SPLITOR)){
@@ -17,7 +16,7 @@ public class SeperatorFactory {
                 .append(SPLITOR)
                 .append(customSeperator);
 
-        seperatorRegex=regexBuilder.toString();
+        return regexBuilder.toString();
     }
 
     private static String addEscapeChar(String customSeperator){
@@ -27,13 +26,5 @@ public class SeperatorFactory {
                 .append(customSeperator);
 
         return customSeperator.toString();
-    }
-
-    public static String getSeperatorRegex(){
-        return seperatorRegex;
-    }
-
-    public static void resetSeperatorRegex() {
-        seperatorRegex=DEFAULT_SEPERATOR_REGEX;
     }
 }
