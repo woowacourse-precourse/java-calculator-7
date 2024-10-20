bf5c88ec73f675d7d41f0635cfcd97694f8ff32c
AwesomeMinsik <beaut3579@gmail.com>
2024-10-20 14:05:27 +0900

    refactor: if 구문 내부 로직에 {} 괄호 추가
     - Java Style guide 에 따르는 코드 작성방식에 따라 if 구문 내부의 로직이 한줄 인 경우에도 중괄호로 감싸도록 통일하여 코드 일관성 유지.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,8 +6,9 @@ public class  Calculator {
     private final StringFilter stringFilter = new StringFilter();
 
     public int calculate(String input) {
-        if (input.isEmpty())
+        if (input.isEmpty()){
             return 0;
+        }
         return Arrays.stream(stringFilter.convertToIntArray(input)).sum();
     }
 }
