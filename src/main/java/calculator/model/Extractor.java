6332cccae9ef0a3fad7dca30a4d3e33f12b039a5
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-20 17:45:48 +0900

    feat: 생성자에 기본 구분자 추가 로직

--- src/main/java/calculator/model/Extractor.java
+++ src/main/java/calculator/model/Extractor.java
@@ -5,5 +5,9 @@ import java.util.List;
 public class Extractor {
 
     private List<String> delimiters;
+    // 기본 구분자 및 커스텀 구분자 추가
+    public Extractor(String input) {
+        this.delimiters = new ArrayList<>(List.of(",", ":"));
+    }
 
 }
