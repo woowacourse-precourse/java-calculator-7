2dffb5bc9eb2d06afa2cf11449883e90dd7368e2
Seol-Munhyeok <abc1234568@naver.com>
2024-10-20 16:36:22 +0900

    feat(숫자 추출 및 계산) : 구분자로 숫자를 추출하고 합을 계산하는 것 구현
    - README.md 파일 수정
    - ArrayList 활용하는 것으로 변경 (기본 라이브러리)
    
    - 커스텀 구분자 추출 로직 변경
    - hasCustomDelimiter() 메서드 제거
    - indexOf() 메서드 활용하는 방식으로 구현
    - 그 외 다양한 예외 처리하는 부분 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,30 +2,44 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import calculator.StringUtil.*;
-
+import java.util.ArrayList;
 
 public class Application {
-
     public static void main(String[] args) {
         String input;
-        char customDelimiter;
-        int[] numbers = {};
+        String delimiter = ",:";
+        String customDelimiter;
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         input = Console.readLine();
-        // 간단한 입력 예외 확인
+
+        // 빈 문자열 입력 예외 처리
         if (input.isEmpty()) {
             System.out.println("결과 : 0");
+            return;
         }
-        else if (input.length() == 1 && StringUtil.isDigit((input.charAt(0)))) {
-            System.out.println("결과 : " + input);
+        // 커스텀 구분자 추출
+        if (input.startsWith("//")) {
+            int delimiterEndIndex = input.indexOf("\\n");
+            //System.out.println(delimiterEndIndex);
+            if (delimiterEndIndex >= 0) {
+                customDelimiter = StringUtil.getCustomDelimiter(input, delimiterEndIndex);
+                delimiter = delimiter + customDelimiter;
+                //System.out.println(delimiter);
+                input = input.substring(delimiterEndIndex + 2);  // 커스텀 구분자 지정 부분 제거
+            }
         }
 
-        if (StringUtil.hasCustomDelimiter(input)) {
-            customDelimiter = StringUtil.getCustomDelimiter(input);
+        // 숫자 분리
+        ArrayList<Integer> numbers;
+        numbers = StringUtil.split(input, delimiter);
+
+        // 합 계산
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number;
         }
-
-
+        System.out.println("결과 : " + sum);
     }
 }
