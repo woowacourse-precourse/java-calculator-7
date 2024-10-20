9006aa46ab7428810fec433661b5ef6760d4fe90
jhseob <jhseob06@gmail.com>
2024-10-20 22:55:49 +0900

    refactor: 콘솔 출력 방식 System.out으로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,19 +4,15 @@ import calculator.cal.Calculator;
 import calculator.cal.CalculatorImpl;
 import calculator.cal.numberadder.string.StringNumberAdder;
 import camp.nextstep.edu.missionutils.Console;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.NoSuchElementException;
 
 public class Application {
     public static void main(String[] args) throws IOException {
         Calculator calculator = new CalculatorImpl(new StringNumberAdder());
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         //문자열 받기
-        writer.write("덧셈할 문자열을 입력해 주세요.\n");
-        writer.flush();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputString;
         try {
             inputString = Console.readLine();
@@ -26,7 +22,6 @@ public class Application {
 
         //결과 처리
         int result = calculator.add(inputString);
-        writer.write("결과 : " + result);
-        writer.flush();
+        System.out.println("결과 : " + result);
     }
 }
