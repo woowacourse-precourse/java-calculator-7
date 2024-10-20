f94a3e7210c9dcadfc6e8a9ac89956be7064fb70
sese2204 <sese2204@naver.com>
2024-10-20 21:15:34 +0900

    feat(application): Application 클래스 구현
    - Application 클래스에 프로그램 진입점 구현
    - CalculatorController, UserInterface 객체 생성 및 조립

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.ui.ConsoleUI;
+import calculator.ui.UserInterface;
+
 
 public class Application {
     public static void main(String[] args) {
+        UserInterface ui = new ConsoleUI();
+        CalculatorController calculatorController = new CalculatorController(ui);
+        calculatorController.run();
     }
 }
