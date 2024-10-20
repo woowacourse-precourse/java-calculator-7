2cce72ac9b31bf28bedd1798168b6a947b811ef9
Chan531 <chan657312@gmail.com>
2024-10-20 17:38:27 +0900

    feat(CustomDelimiterValidator): 커스텀 구분자 추출

--- src/main/java/calculator/model/CustomDelimiterValidator.java
+++ src/main/java/calculator/model/CustomDelimiterValidator.java
@@ -6,9 +6,15 @@ public class CustomDelimiterValidator {
 	private static final int MIN_LENGTH_FOR_CUSTOM_DELIMITER = 5;
 	private static final String CUSTOM_DELIMITER_PREFIX = "//";
 	private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+	private static final int CUSTOM_DELIMITER_INDEX = 2;
+	private static final int CUSTOM_DELIMITER_LENGTH = 1;
 
 	public boolean hasCustomDelimiter(String input) {
 		return input.length() >= MIN_LENGTH_FOR_CUSTOM_DELIMITER && input.startsWith(CUSTOM_DELIMITER_PREFIX)
 		        && input.startsWith(CUSTOM_DELIMITER_SUFFIX, CUSTOM_DELIMITER_SUFFIX_START_INDEX);
+	}
+
+	public String getCustomDelimiter(String input) {
+		return input.substring(CUSTOM_DELIMITER_INDEX, CUSTOM_DELIMITER_INDEX + CUSTOM_DELIMITER_LENGTH);
 	}
 }
