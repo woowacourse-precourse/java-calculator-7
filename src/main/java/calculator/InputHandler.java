6bb219f08ec500a02026967f802748721ffadce9
ssun <56533266+blaire-pi@users.noreply.github.com>
2024-10-21 01:19:28 +0900

    feat: 문자열 입력받기

---
+++ src/main/java/calculator/InputHandler.java
@@ -0,0 +1,10 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+    public static void main(String[] args) {
+        System.out.print("식을 입력하세요: ");
+        String userInput = Console.readLine();
+    }
+}
