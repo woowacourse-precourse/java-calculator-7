1409b620cafa81339840de41fcd5a381e7379cdc
김재형 <kjhstar1124@naver.com>
2024-10-20 12:50:05 +0900

    refactor: 구분자 기준 합 반환 함수 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,46 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // 1. 입력 받은 문자열이 "커스텀 문자열"로 시작하는지 확인
         String inputText = Console.readLine();
-        String separator = "";
-        int totalSum = 0;
+        int totalSum;
+        // 입력 받은 문자열이 "커스텀 문자열"로 시작하는지 확인
         if (isCustomSeparator(inputText)) {
-            separator = inputText.substring(2, 3);
-            String[] splitText = inputText.substring(5).split(separator);
-            for (String num : splitText) {
-                try {
-                    int inputNum = Integer.parseInt(num);
-                    if (inputNum <= 0) {
-                        System.out.println("입력 오류: 양수를 입력해주세요");
-                        throw new IllegalArgumentException();
-                    }
-                    totalSum += inputNum;
-                } catch (NumberFormatException e) {
-                    System.out.println("입력 오류: 숫자를 입력해주세요");
-                    throw new IllegalArgumentException();
-                }
-            }
+            totalSum = getTotalSum(inputText, true);
         } else {
-            // 2. 커스텀 문자열로 시작하지 않는다면, 기본 구분자를 포함하는지 확인
+            // 커스텀 문자열로 시작하지 않는다면, 기본 구분자를 포함하는지 확인
             if (inputText.contains(",") || inputText.contains(":")) {
-                // 기본 구분자를 포함한다면, 이를 기준으로 분리
-                String[] splitText = inputText.split("[,:]");
-                for (String num : splitText) {
-                    try {
-                        int inputNum = Integer.parseInt(num);
-                        if (inputNum <= 0) {
-                            System.out.println("입력 오류: 양수를 입력해주세요");
-                            throw new IllegalArgumentException();
-                        }
-                        totalSum += inputNum;
-                    } catch (NumberFormatException e) {
-                        System.out.println("입력 오류: 숫자를 입력해주세요");
-                        throw new IllegalArgumentException();
-                    }
-                }
+                // 기본 구분자를 포함한다면, 이를 기준으로 분리;
+                totalSum = getTotalSum(inputText, false);
             } else {
-                // 3. 기본 구분자도 포함하지 않는다면, 잘못된 입력
+                // 기본 구분자도 포함하지 않는다면, 잘못된 입력
                 System.out.println("입력 오류: 올바른 구분자를 입력해주세요");
                 throw new IllegalArgumentException();
             }
@@ -72,5 +44,30 @@ public class Application {
             }
         }
         return false;
+    }
+
+    private static int getTotalSum(String inputText, boolean isCustomSeparator) {
+        String[] splitText;
+        int totalSum = 0;
+        if (isCustomSeparator) {
+            String separator = inputText.substring(2, 3);
+            splitText = inputText.substring(5).split(separator);
+        } else {
+            splitText = inputText.split("[,:]");
+        }
+        for (String num : splitText) {
+            try {
+                int inputNum = Integer.parseInt(num);
+                if (inputNum <= 0) {
+                    System.out.println("입력 오류: 양수를 입력해주세요");
+                    throw new IllegalArgumentException();
+                }
+                totalSum += inputNum;
+            } catch (NumberFormatException e) {
+                System.out.println("입력 오류: 숫자를 입력해주세요");
+                throw new IllegalArgumentException();
+            }
+        }
+        return totalSum;
     }
 }
