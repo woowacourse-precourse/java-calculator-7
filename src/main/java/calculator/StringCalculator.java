020030b7024bd12fd912bdbb5550cc4d06a928ff
이희수 <hslee9234@naver.com>
2024-10-20 19:17:07 +0900

    feat: stringCalculator 클래스 정의
    stringCalculator는 문자열 List를 받아 문자열 간 덧셈을 진행한 후 최종 결과 값을 반환하는 클래스임

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import java.util.List;
+
+public class StringCalculator {
+
+    // 문자열 List를 받아 문자열 간 덧셈을 진행
+    public String add(List<String> numbers) {
+
+    }
+
+    // 두 문자열 간 덧셈
+    private String addString(String num1, String num2) {
+
+    }
+
+}
