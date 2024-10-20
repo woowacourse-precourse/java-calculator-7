12f7c153de4a83435ce564a9db9145a56ddcc952
Chan531 <chan657312@gmail.com>
2024-10-20 18:51:16 +0900

    chore(CustomDelimiterValidator): 커스텀 구분자 지정 문자열 길이를 나타내는 상수명 변경

--- src/main/java/calculator/model/CustomDelimiterValidator.java
+++ src/main/java/calculator/model/CustomDelimiterValidator.java
@@ -3,14 +3,14 @@ package calculator.model;
 public class CustomDelimiterValidator {
 
 	private static final int CUSTOM_DELIMITER_SUFFIX_START_INDEX = 3;
-	private static final int MIN_LENGTH_FOR_CUSTOM_DELIMITER = 5;
+	private static final int SPECIFIED_CUSTOM_DELIMITER_LENGTH = 5;
 	private static final String CUSTOM_DELIMITER_PREFIX = "//";
 	private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
 	private static final int CUSTOM_DELIMITER_INDEX = 2;
 	private static final int CUSTOM_DELIMITER_LENGTH = 1;
 
 	public boolean hasCustomDelimiter(String input) {
-		return input.length() >= MIN_LENGTH_FOR_CUSTOM_DELIMITER && input.startsWith(CUSTOM_DELIMITER_PREFIX)
+		return input.length() >= SPECIFIED_CUSTOM_DELIMITER_LENGTH && input.startsWith(CUSTOM_DELIMITER_PREFIX)
 		        && input.startsWith(CUSTOM_DELIMITER_SUFFIX, CUSTOM_DELIMITER_SUFFIX_START_INDEX);
 	}
 
