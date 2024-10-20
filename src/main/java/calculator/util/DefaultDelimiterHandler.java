9d2fd0f633d0bcc5e5326a390cc21acdc912f732
jjun522 <tjwowns522@>
2024-10-20 17:15:18 +0900

    chore: 기존 유틸리티 파일 및 서비스 파일 삭제
    기존의 단일 책임 원칙을 따르지 않은 서비스 및 유틸리티 클래스들을 삭제하였습니다.
    MVC 패턴에 맞게 새로운 구조로 재작성하기 위함입니다.

--- src/main/java/calculator/util/DefaultDelimiterHandler.java
+++
@@ -1,8 +0,0 @@
-package calculator.util;
-
-public class DefaultDelimiterHandler {
-    // 기본 구분자 반환 (쉼표 또는 콜론)
-    public static String getDefaultDelimiter() {
-        return ",|:";
-    }
-}
--- src/main/java/calculator/util/ErrorHandler.java
+++
@@ -1,7 +0,0 @@
-package calculator.util;
-
-public class ErrorHandler {
-    public static void handleError(Exception e) {
-        System.out.println("An error occurred: " + e.getMessage());
-    }
-}
