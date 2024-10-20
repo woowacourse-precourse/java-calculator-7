7094592136e716ddc01b7d1950258fe61033bcd3
조윤성 <yunsung1206@gmail.com>
2024-10-20 23:51:24 +0900

    feat: 커스텀 구분자를 기준으로 숫자를 분리하는 기능 추가

--- src/main/java/calculator/NumberParser.java
+++ src/main/java/calculator/NumberParser.java
@@ -5,13 +5,36 @@ import java.util.Collections;
 import java.util.List;
 
 public class NumberParser {
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final String CUSTOM_PREFIX = "//";
+    private static final String CUSTOM_SUFFIX = "\\n";
+    private static final int CUSTOM_PREFIX_LENGTH = CUSTOM_PREFIX.length();
+    private static final int CUSTOM_SUFFIX_LENGTH = CUSTOM_SUFFIX.length();
+
     public List<Integer> parse(String input) {
         if (input.isEmpty()) {
             return Collections.emptyList();
         }
-        String delimiters = "[,:]";
-        String[] numbers = input.split(delimiters);
+        String[] numbers = split(input);
         return convertToIntegerList(numbers);
+    }
+
+    private String[] split(String input) {
+        if (hasCustomDelimiter(input)) {
+            return splitCustomDelimiter(input);
+        }
+        return input.split(DEFAULT_DELIMITER);
+    }
+
+    private boolean hasCustomDelimiter(String input) {
+        return input.startsWith(CUSTOM_PREFIX);
+    }
+
+    private String[] splitCustomDelimiter(String input) {
+        int index = input.indexOf(CUSTOM_SUFFIX);
+        String customDelimiter = input.substring(CUSTOM_PREFIX_LENGTH, index);
+        String result = input.substring(index + CUSTOM_SUFFIX_LENGTH);
+        return result.split(customDelimiter);
     }
 
     private List<Integer> convertToIntegerList(String[] numbers) {
