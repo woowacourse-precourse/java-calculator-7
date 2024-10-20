887cf6a1fbb4ec5cbd0df08f1bb08289c3c74c26
DongHoon Lee <dhl1924@naver.com>
2024-10-20 21:28:47 +0900

    feat(DelimiterConstants): 공통 상수 관리를 위한 final 클래스 생성

---
+++ src/main/java/calculator/global/constants/DelimiterConstants.java
@@ -0,0 +1,9 @@
+package calculator.global.constants;
+
+public final class DelimiterConstants {
+    public static final char COMMA_DELIMITER = ',';
+    public static final char COLON_DELIMITER = ':';
+
+    private DelimiterConstants() {
+    }
+}
