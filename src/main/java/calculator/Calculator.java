a3e911739857c29839d23634a83cc5c60ddda288
parkseongyeol <ert2154@naver.com>
2024-10-21 02:14:42 +0900

    feat($calculator): 문자열 덧셈 계산기 기능 추가 및 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,30 @@
+package calculator;
+
+import adder.Adder;
+import io.Input;
+import io.Output;
+import parser.Parser;
+import separator.Separator;
+
+public class Calculator {
+
+    public void calculator() {
+        Adder adder = new Adder();
+        Separator separator = new Separator();
+        Parser parser = new Parser();
+        Input i = new Input();
+        Output o = new Output();
+
+        String input = i.input();
+
+        separator.saveSeparator(input);
+
+        String stringToCalculate = parser.parseStringToCalculate(input);
+
+        int result = adder.add(stringToCalculate);
+
+        o.output(result);
+    }
+
+
+}
