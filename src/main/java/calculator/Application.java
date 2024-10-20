0a891ea3d6e5dfcd4fbe97d0080c0f8f6af198f1
ZZKIM <rkgml424@naver.com>
2024-10-20 22:57:54 +0900

    add: CustomDelimiter가 있는지 확인하고 계산하는 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,14 +3,53 @@ package calculator;
 import java.util.Scanner;
 
 public class Application {
-
-    private static int Calculator(String st){
+    private static int normalDeliCal(String st){
         int result = 0;
 
+        for(int i = 0; i < st.length(); i++){
+            if(48 <= st.charAt(i) && st.charAt(i) < 58){
+                result += st.charAt(i) - 48;
+            }
+            else if( ! (st.charAt(i) == ',' || st.charAt(i) == ':')){
+                throw new IllegalArgumentException();
+            }
+        }
 
         return result;
     }
+    private static int customDeliCal(char deli, String st){
+        int result = 0;
 
+        for(int i = 0; i < st.length(); i++){
+            if(48 <= st.charAt(i) && st.charAt(i) < 58){
+                result += st.charAt(i) - 48;
+            }
+            else if(st.charAt(i) != deli){
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return result;
+    }
+    private static boolean checkCustomDeli(String st){
+
+        if(st.substring(0, 2).equals("//") && st.substring(3,5).equals("\\n")){
+            return true;
+        }
+        return false;
+    }
+
+    private static int Calculator(String st){
+
+        boolean isCustom = checkCustomDeli(st);
+
+        if(!isCustom){
+            return normalDeliCal(st);
+        }
+        else{
+            return customDeliCal(st.charAt(2), st.substring(5));
+        }
+    }
     public static void main(String[] args) {
         // 프로그램 구현
         Scanner sc = new Scanner(System.in);
