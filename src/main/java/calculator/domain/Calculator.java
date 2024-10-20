90253543e5bbebb8af320447c29762213c066582
dnwldla <leeemjw@naver.com>
2024-10-20 22:21:24 +0900

    delete: Validator 클래스 제거 Calculator에게 역할 위임

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,15 +1,19 @@
 package calculator.domain;
 
+import calculator.exception.InvalidInputException;
+
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
     private static final Pattern CUSTOM_SEPERATOR_PATTERN=Pattern.compile("^//(.)\\\\n{1}(.*)");
+    private static final String NUMBER_REGEX="[0-9]{0,}";
 
     private static final String OVERFLOW_RESULT="덧셈의 결과가 int 범위를 넘어갔습니다";
     private static final String OVERFLOW_NUMBER="특정 수가 int 범위를 넘어갔습니다";
     private static final String INVALID_SEPERATOR="커스텀 구분자가 아닌 기호를 입력했습니다";
+
     private static final String BLANK="";
 
     public static int inputCalculate(String input){
@@ -36,12 +40,27 @@ public class Calculator {
     private static int calculate(String input,String seperatorRegex) {
         int result= Arrays.stream(input.split(seperatorRegex))
                 .filter(num->!isBlank(num))
-                .peek(num->Validator.validateCustomSeperator(num))
-                .peek(num->Validator.validateInteger(num))
+                .peek(num->validateCustomSeperator(num))
+                .peek(num->validateInteger(num))
                 .mapToInt(Integer::parseInt)
                 .reduce(0,(a,b)->addTwoNum(a,b));
 
         return result;
+    }
+
+    //존재하지 않는 커스텀 구분자 예외발생
+    private static void validateCustomSeperator(String num){
+        if (!(num.matches(NUMBER_REGEX))){
+            throw new InvalidInputException(MessageType.INVALID_SEPERATOR);
+        }
+    }
+
+    private static void validateInteger(String num){
+        try{
+            Integer.parseInt(num);
+        }catch(IllegalArgumentException e){
+            throw new IllegalArgumentException(MessageType.OVERFLOW_NUMBER.getMessage());
+        }
     }
 
     private static int addTwoNum(int a,int b) {
