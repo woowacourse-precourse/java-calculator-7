5b9549ff8967e0f196a2850d13dccc9a0e02f0e8
chanwon <clw8679@naver.com>
2024-10-21 02:06:27 +0900

    fix: 문자열 분리 기능 오류 수정

--- src/main/java/calculator/util/Converter.java
+++ src/main/java/calculator/util/Converter.java
@@ -7,27 +7,45 @@ import java.util.stream.Collectors;
 public class Converter {
     private static final String ERROR_MESSAGE = "유효하지 않은 문자열입니다.";
 
-    public static List<Integer> stringToNumbers(String input) {
-        String expression = input;
-        String customDelimiter = "";
-
-        if (expression.startsWith("//")) {
-            if (!expression.contains("\n")) {
-                throw new IllegalArgumentException(ERROR_MESSAGE);
-            }
-            int separatorIndex = expression.indexOf("\n");
-            customDelimiter = "|" + expression.substring(2, expression.indexOf("\n"));
-            expression = expression.substring(separatorIndex + 1);
+    public static List<Integer> stringToNumbers(String str) {
+        if (!Character.isDigit(str.charAt(str.length() - 1))) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
         }
 
-        String[] tokens = expression.split(",|:" + customDelimiter);
+        Expression expression = getExpression(str);
+        String delimiters = ",|:" + expression.customDelimiter();
+        String[] tokens = expression.str.split(delimiters);
 
         return Arrays.stream(tokens).map(token -> {
-            if (!isNumeric(token)) {
+                    if (!isNumeric(token)) {
+                        throw new IllegalArgumentException(ERROR_MESSAGE);
+                    }
+                    return Integer.parseInt(token);
+                }
+        ).collect(Collectors.toList());
+    }
+
+    private static Expression getExpression(String str) {
+        if (str.startsWith("//")) {
+            if (!str.contains("\\n")) {
                 throw new IllegalArgumentException(ERROR_MESSAGE);
             }
-            return Integer.parseInt(token);
-        }).collect(Collectors.toList());
+
+            int separatorIndex = str.indexOf("\\n");
+            String customDelimiter = "|" + str.substring(2, separatorIndex);
+
+            if (customDelimiter.equals("|")) {
+                customDelimiter = "";
+            }
+
+            if (customDelimiter.equals("|\\")) {
+                customDelimiter = "|\\\\";
+            }
+
+            String expression = str.substring(separatorIndex + 2);
+            return new Expression(expression, customDelimiter);
+        }
+        return new Expression(str, "");
     }
 
     private static boolean isNumeric(String str) {
@@ -40,7 +58,9 @@ public class Converter {
                 return false;
             }
         }
-
         return true;
+    }
+
+    private record Expression(String str, String customDelimiter) {
     }
 }
