7aa65c73436935d3c3bd39fcba9969af3a592788
CoconutGithub <khkoh5932@gmail.com>
2024-10-21 02:24:10 +0900

    feat : "//","\n" 사이에 지정된 커스텀 구분자가 있다면 추가하여 구분자로 사용

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,13 +4,23 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         int sum = 0;
+        String[] nums;
+        String separator = ",|:";
 
         String string = Console.readLine();
-        String[] nums = string.split(",|:");
+        if (string.startsWith("//")) {
+            nums = string.split("\\\\n", 2);
+            String newSep=nums[0].replace("//", "");
+            separator += "|" + newSep;
+            string = nums[1];
+        }
+
+        nums = string.split(separator);
         for (String num : nums) {
             sum += Integer.parseInt(num);
         }
 
         System.out.println("sum = " + sum);
     }
+
 }
