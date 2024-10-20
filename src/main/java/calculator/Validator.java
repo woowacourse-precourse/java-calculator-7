030ff3f540ea57eca1318037ded41c5ee63f5e6a
seophohoho <seop0937@gmail.com>
2024-10-20 15:39:37 +0900

    feat(Validator): 커스텀 구분자가 올바른 값이 아닐 시, 에러 처리 추가

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -13,5 +13,14 @@ public class Validator {
         if (!input.startsWith(Constants.CUSTOM_SEPARATOR_PREFIX) && input.contains(Constants.CUSTOM_SEPARATOR_SUFFIX)) {
             throw new IllegalArgumentException("커스텀 구분자 지정 컨벤션이 올바르지 않아요");
         }
+
+        if (input.startsWith(Constants.CUSTOM_SEPARATOR_PREFIX) && input.contains(Constants.CUSTOM_SEPARATOR_SUFFIX)) {
+            int prefixIdx = input.indexOf(Constants.CUSTOM_SEPARATOR_PREFIX);
+            int suffixIdx = input.lastIndexOf(Constants.CUSTOM_SEPARATOR_SUFFIX);
+            
+            if (((suffixIdx - 1) - (prefixIdx + 1)) > 1) {
+                throw new IllegalArgumentException("커스텀 구분자는 길이가 '1' 이여야 한다.");
+            }
+        }
     }
 }
