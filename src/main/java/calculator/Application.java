5ff0e92bbb96648d76180c1979fda36d1727e07c
Seongmo Ahn <djgood780@gmail.com>
2024-10-20 18:30:43 +0900

    test(ApplicationTest): 사용자 입력 검증 테스트 13개 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,19 +1,5 @@
 package calculator;
 
-/*
-테스트 입력
-"1,2,3"
-"1,2,3,"
-"1,2:3@4"
-"1,;2:3"
-"1, 2;3"
-"//;\n1;2;3"
-"//;\n1;2; 3"
-"//;\n1,2;3:4"
-"//;\n1:2@3;4"
-"//7\n172,3"
-*/
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -7,6 +7,23 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+/*
+테스트 입력
+"1,2,3"
+"1,2:3"
+"-1,2,-3"
+"1,2,3,"
+"1,2:3@4"
+"1,;2:3"
+"1, 2;3"
+"//;\n1;2;2"
+"//;\n1,2;3:4"
+"//;\n1;2; 3"
+"//;\n1:2@3;4"
+"//7\n172,3"
+"//\n1,2:2"
+*/
+
 class ApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_사용() {
@@ -17,10 +34,114 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_사용1() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;2");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용2() {
+        assertSimpleTest(() -> {
+            run("//;\\n1,2;3:4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용3() {
+        assertSimpleTest(() -> {
+            run("//a\\n1,2a3:1");
+            assertThat(output()).contains("결과 : 7");
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용1() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용2() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,3,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2:3@4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,;2:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1, 2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트5() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n1;2; 3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트6() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n1:2@3;4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트7() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//7\\n172,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트8() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n1,2:2"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
