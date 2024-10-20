5d775d157f19b9d0d09b5724f8cb101d3696d5de
Dev-LHY <dev.leehy@gmail.com>
2024-10-20 15:36:01 +0900

    chore: 클래스명 변경 CalculatorTest to ExceptionTest

--- src/test/java/calculator/ExceptionTest.java
+++ src/test/java/calculator/ExceptionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class CalculatorTest {
+class ExceptionTest {
     @Test
     void 공백_예외_테스트() {
         assertThatThrownBy(() -> {
