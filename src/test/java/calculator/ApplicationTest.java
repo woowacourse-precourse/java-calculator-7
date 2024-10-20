227617c473ec95a96bfac3fb8950ffd0b161f50c
longrunpc <pcjmo4050@gmail.com>
2024-10-21 06:14:14 +0900

    test(application): 다양한 구분자 및 예외 처리에 대한 테스트 코드 추가
    - 기본 구분자(쉼표, 콜론)를 사용한 덧셈 기능 테스트 추가
    - 커스텀 구분자 및 특수 문자를 포함한 구분자 테스트 추가
    - 음수 및 0 입력 시 예외 발생 여부 테스트 추가
    - 문자 입력 시 예외 발생 여부 테스트 추가
    - 유효하지 않은 커스텀 구분자 입력 시 예외 처리 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -11,16 +13,54 @@ class ApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("//;\\n1;2,1:4");
+            assertThat(output()).contains("결과 : 8");
+        });
+    }
+    @Test
+    void 커스텀_구분자_특수문자_사용() {
+        assertSimpleTest(() -> {
+            run("//&\\n1&2,1:4");
+            assertThat(output()).contains("결과 : 8");
         });
     }
 
     @Test
-    void 예외_테스트() {
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 예외_테스트_음수값_입력() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_0_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_문자_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(";,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_커스텀_구분자_두개_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//q]\n1:2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
