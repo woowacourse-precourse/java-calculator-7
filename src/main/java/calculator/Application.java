31c5037bdcc30982dceaf58c59755231a873de6c
minyeongg <1030pmy@gmail.com>
2024-10-20 23:06:53 +0900

    fix : 입력 유효성 검사 로직 수정
    String.split()메소드에 |로 구분한 여러 구분자를 인자로 넣어서 호출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,7 +3,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static int add(String input) {
-        String delimiter = "";
+        String delimiter = ",|:";
         String[] numbers;
         int sum = 0;
         if (input == null || input.isEmpty() || input.isBlank()) {
@@ -13,49 +13,19 @@ public class Application {
             if(newlineIndex == -1) {
                 throw new IllegalArgumentException("커스텀 구분자를 지정할 수 없습니다.");
             }
-            delimiter = input.substring(2, newlineIndex);
+            String customDelimiter = input.substring(2, newlineIndex);
+            delimiter = customDelimiter + "|" + delimiter;
             input = input.substring(newlineIndex + 2);
-            if(input == null || input.isEmpty() || input.isBlank()) {
-                return 0;
-            }
-            String first = "" + input.charAt(0);
-            String firstDlm = input.substring(0, delimiter.length());
-            String last = "" + input.charAt(input.length()-1);
-            String lastDlm = input.substring(input.length() - delimiter.length(), input.length());
-            if(first.equals(":") || first.equals(",") || firstDlm.equals(delimiter)) {
-                throw new IllegalArgumentException("구분자로 시작할 수 없습니다.");
-            } else if(last.equals(":") || last.equals(",") || lastDlm.equals(delimiter)) {
-                throw new IllegalArgumentException("구분자로 끝날 수 없습니다.");
-            } else {
-                numbers = split(input, delimiter);
-                for(String number : numbers) {
-                    int num = parseNumber(number);
-                    if(num < 0) {
-                        throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
-                    }
-                    sum += num;
+            numbers = input.split(delimiter);
+            for(String number : numbers) {
+                int num = parseNumber(number);
+                if (num < 0) {
+                    throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
                 }
-                return sum;
-            }
-        } else {
-            String first = "" + input.charAt(0);
-            String last = "" + input.charAt(input.length()-1);
-            if(first.equals(":") || first.equals(",")) {
-                throw new IllegalArgumentException("구분자로 시작할 수 없습니다.");
-            } else if(last.equals(":") || last.equals(",")) {
-                throw new IllegalArgumentException("구분자로 끝날 수 없습니다.");
-            } else {
-                numbers = split(input);
-                for (String number : numbers) {
-                    int num = parseNumber(number);
-                    if (num < 0) {
-                        throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
-                    }
-                    sum += num;
-                }
-                return sum;
+                sum += num;
             }
         }
+        return sum;
     }
 
     private static int parseNumber(String number) {
@@ -63,27 +33,8 @@ public class Application {
             return Integer.parseInt(number);
         } catch(NumberFormatException e) {
             throw new IllegalArgumentException(
-                    "구분자나 숫자를 연속해서 입력하거나 " +
-                            "구분자로 지정하지 않은 문자를 입력할 수 없습니다.");
+                    "숫자 형식이 아닙니다.");
         }
-    }
-
-    private static String[] split(String input) {
-        String temp = input.replace(":", ",");
-        if(!temp.contains(",")) {
-            throw new IllegalArgumentException(
-                    "숫자만 입력하거나 구분자로 지정하지 않은 문자를 입력할 수 없습니다.");
-        }
-        return temp.split(",");
-    }
-
-    private static String[] split(String input, String delimiter) {
-        String temp = input.replace(delimiter, ",");
-        temp = temp.replace(":", ",");
-        if(!temp.contains(",")) {
-            throw new IllegalArgumentException("숫자만 입력할 수 없습니다.");
-        }
-        return temp.split(",");
     }
 
     public static void main(String[] args) {
