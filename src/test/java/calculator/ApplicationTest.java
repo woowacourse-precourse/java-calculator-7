537ca8ac810aa0b44a6aa2d36fa9113478f05f38
hwangdaesun <hds650588@gmail.com>
2024-10-20 20:39:54 +0900

    test(InputView.class) : 빈문자열 또는 공백문자만 입력이 올 경우 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -32,6 +32,24 @@ class ApplicationTest extends NsTest {
         });
     }
 
+
+    @Test
+    void 공백문자만_사용() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 빈문자열만_사용() {
+        assertSimpleTest(() -> {
+            run("  ");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
