338bda40ce2e163ce31f85dd1719efd1a74abea4
softmoca <acyc0909@gmail.com>
2024-10-20 21:45:57 +0900

    test: 빈 문자열 입력 시 엔터(\n)를 추가하여 NoSuchElementException 해결
    빈 문자열 입력 시 Scanner의 nextLine()에서 발생하는 NoSuchElementException 문제를 해결하기 위해,
    빈 문자열 대신 엔터(\n)를 입력하도록 테스트 코드를 수정함.
    이를 통해 빈 입력에 대한 처리가 정상적으로 이루어져 테스트가 성공하도록 변경.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -3,13 +3,16 @@ package calculator;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
-    void 커스텀_구분자_사용() {
+    void 커스텀_구분자_숫자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
@@ -19,10 +22,127 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+
+    @Test
+    void 빈_문자열일_경우() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+
+    @Test
+    void 기본_구분자_쉼표_콜론_사용() {
+        assertSimpleTest(() -> {
+            run("1,2");
+            assertThat(output()).contains("결과 : 3");
+
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+
+            run("//;\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_구분자만_존재_빈문자열_반환() {
+        assertSimpleTest(() -> {
+            run("//;\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 연속된_구분자_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,,2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 음수_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n2;21;-2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자만_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",,::"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자로_숫자_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//1\\n1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void long_범위_초과() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("9223372036854775807,1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_구문_오류() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자_없이_줄바꿈() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_길이_초과() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//ab\\n1ab2ab3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자_외의_값_포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,abc"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
 
     @Override
     public void runMain() {
