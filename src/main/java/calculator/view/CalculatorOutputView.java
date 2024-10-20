0db6a387471a23731a82ad972f8167b2c482c20f
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 02:54:27 +0900

    feat(CalculatorOutputView): 결괏값 출력 메서드 추가

--- src/main/java/calculator/view/CalculatorOutputView.java
+++ src/main/java/calculator/view/CalculatorOutputView.java
@@ -5,4 +5,8 @@ public class CalculatorOutputView {
     public void printInstruction() {
         System.out.println(Message.INSTRUCTION_MESSAGE.getMessage());
     }
+
+    public void printResult(String result) {
+        System.out.println(Message.RESULT_MESSAGE.getMessage() + result);
+    }
 }
