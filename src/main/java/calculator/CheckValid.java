60658af5166b6562c5b2b8a7304d5b760b41eca4
AYEOOON <k93853471@gmail.com>
2024-10-20 13:51:48 +0900

    fix: CheckValid 예외 처리 로직 수정
    - checkContinuousDelimiters 에서 구분자가 두 개 이상 연속으로 사용된 경우에 대한 예외 처리 로직 수정

--- src/main/java/calculator/CheckValid.java
+++ src/main/java/calculator/CheckValid.java
@@ -8,10 +8,10 @@ public class CheckValid {
         try {
             int num = Integer.parseInt(word);
             if (num < 0) {
-                throw new IllegalArgumentException("음수는 입력할 수 없습니다: " + word);
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
             }
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("유효한 숫자가 아닙니다: " + word);
+            throw new IllegalArgumentException("유효한 숫자가 아닙니다.");
         }
     }
 
@@ -23,25 +23,16 @@ public class CheckValid {
 
         // 숫자 또는 구분자 앞뒤에 공백이 있으면 예외 발생
         if (word.startsWith(" ") || word.endsWith(" ")) {
-            throw new IllegalArgumentException("숫자나 구분자 앞뒤에 공백이 포함되어 있습니다: '" + word + "'");
+            throw new IllegalArgumentException("숫자나 구분자 앞뒤에 공백이 포함되어 있습니다.");
         }
     }
 
     // 구분자가 연속으로 있는지 확인하는 메서드
-    static void checkContinuousDelimiters(String input) {
-        int nonNumericCount = 0; // 숫자가 아닌 문자의 개수
-        char[] chars = input.toCharArray(); // 문자열을 문자 배열로 변환
-        System.out.println(chars);
-        for (char c : chars) {
-            // 숫자가 아닌 경우
-            if (!Character.isDigit(c) && c != ' ' && c != '-' && !Character.isAlphabetic(c)) {
-                nonNumericCount++;
-                // 연속적으로 2개 이상 발견한 경우
-                if (nonNumericCount >= 2) {
-                    throw new IllegalArgumentException("구분자가 연속으로 사용되었습니다.");
-                }
-            } else {
-                nonNumericCount = 0; // 숫자인 경우 카운트 초기화
+    static void checkContinuousDelimiters(String[] input) {
+        // 각 구분자 사이에 비어있는 문자열이 있으면 구분자가 연속으로 사용된 것
+        for (int i = 0; i < input.length - 1; i++) {
+            if (input[i].isEmpty()) {
+                throw new IllegalArgumentException("구분자가 두 개 이상 연속으로 사용되었습니다.");
             }
         }
     }
