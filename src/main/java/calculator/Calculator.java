1c9dd33c9535f0e1b0dda406de47ed1a335aa35b
vvalvvizal <vvalvvizal@gmail.com>
2024-10-20 11:50:11 +0900

    feat(calculator) : implement processing and calculate methods to sum numbers separated by commas or colons
    Added functionality to handle strings containing numbers separated by commas (,) or colons (:). The `processing` method splits the input string by these delimiters, and the `calculate` method sums the resulting numbers.
    - Example cases:
      - "1,2" => 3
      - "1,2,3" => 6
      - "1,2:3" => 6

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,7 +6,8 @@ public class Calculator {
         System.out.println(result);
     }
     String[] processing(String input){
-        return input.split(",");
+        String[] splitInput = input.trim().split("[,:]");
+       return splitInput;
     }
     void calculate(String[] input){
         for(int i=0;i<input.length;i++){
