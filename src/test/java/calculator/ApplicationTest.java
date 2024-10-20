fde5bb9216826f0e2ae02fb1ccbdf6ef3dd964a3
openmpy <suhwan@kakao.com>
2024-10-20 17:47:46 +0900

    test: ApplicationTest 계산기 합계 결과 값 타입 변환 (long -> Number)

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -5,9 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ApplicationTest extends NsTest {
@@ -21,14 +23,8 @@ class ApplicationTest extends NsTest {
     }
 
     @ParameterizedTest(name = "입력값: {0}, 기대값: {1}")
-    @CsvSource({
-            "'//@\\n1', 1",
-            "'//#\\n1#2', 3",
-            "'//aa\\n1aa2aa3', 6",
-            "'// \\n1 2 3', 6",
-            "'//ㅁ\\n1ㅁ2ㅁ3', 6",
-    })
-    void 여러_커스텀_구분자_사용(String input, long expected) {
+    @MethodSource("provideNumbersWithCustomDelimiter")
+    void 여러_커스텀_구분자_사용(String input, Number expected) {
         assertSimpleTest(() -> {
             run(input);
             assertThat(output()).contains("결과 : " + expected);
@@ -36,15 +32,8 @@ class ApplicationTest extends NsTest {
     }
 
     @ParameterizedTest(name = "입력값: {0}, 기대값: {1}")
-    @CsvSource({
-            "'\n', 0",
-            "'1', 1",
-            "'1,2', 3",
-            "'1:2', 3",
-            "'1,2:3', 6",
-            "'1:2,3', 6",
-    })
-    void 기본_구분자_사용(String input, long expected) {
+    @MethodSource("provideNumbersWithDefaultDelimiter")
+    void 기본_구분자_사용(String input, Number expected) {
         assertSimpleTest(() -> {
             run(input);
             assertThat(output()).contains("결과 : " + expected);
@@ -64,7 +53,6 @@ class ApplicationTest extends NsTest {
             "//;\\n",
             "//;\\na",
             "//;\\n1;2;a",
-            "//;\\n1.2;2;3",
             "//;\\n-1;2;3",
             "//;\\n1; ;3",
             "//;\\n1;2,3",
@@ -85,7 +73,6 @@ class ApplicationTest extends NsTest {
     @ValueSource(strings = {
             "a",
             "1,2,a",
-            "1.2,2,3",
             "-1,2:3",
             "1, :3",
             "1,",
@@ -103,5 +90,34 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    static Stream<Arguments> provideNumbersWithCustomDelimiter() {
+        return Stream.of(
+                Arguments.of("//;\\n1", 1L),
+                Arguments.of("//;\\n1.0", 1L),
+                Arguments.of("//!\\n1!2", 3L),
+                Arguments.of("//@\\n1@2@3", 6L),
+                Arguments.of("//asd\\n1asd2asd3", 6L),
+                Arguments.of("//0\\n10203", 6L),
+                Arguments.of("// \\n1 2 3", 6L),
+                Arguments.of("//\b\\n1\b2\b3", 6L),
+                Arguments.of("//,\\n1,2,3", 6L),
+                Arguments.of("//;\\n1.2;2.3;3.4", 6.9),
+                Arguments.of("//.\\n1.2.3", 6L),
+                Arguments.of("//..\\n1..2..3", 6L)
+        );
+    }
+
+    static Stream<Arguments> provideNumbersWithDefaultDelimiter() {
+        return Stream.of(
+                Arguments.of("\n", 0),
+                Arguments.of("1", 1),
+                Arguments.of("1,2", 3),
+                Arguments.of("1:2", 3),
+                Arguments.of("1,2:3", 6),
+                Arguments.of("1:2,3", 6),
+                Arguments.of("1.2:2.3,3.4", 6.9)
+        );
     }
 }
