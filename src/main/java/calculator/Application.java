e6c78bd992574bc9e7577683a4814334611d4977
J-1ac <tkdwns26@naver.com>
2024-10-20 22:24:32 +0900

    feat: 구분자 추출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,27 @@
 package calculator;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        System.out.println(input);
+
+        String[] result = extractCustomDelimiter(input);
+    }
+
+    public static String[] extractCustomDelimiter(String input) {
+        String delimiter = ",|:";
+        String numberString = input;
+
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\n");
+            if (delimiterIndex != -1) {
+                delimiter = input.substring(2, delimiterIndex);
+                numberString = input.substring(delimiterIndex + 1);
+            }
+        }
+
+        return new String[]{delimiter, numberString};
     }
 }
