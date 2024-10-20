a4110471f6abda5d0726ebc8053811788fa55685
xeulbn <gsbtiger0215@gmail.com>
2024-10-20 23:58:42 +0900

    결과값 0 나오도록 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import net.bytebuddy.pool.TypePool;
+
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 
@@ -8,35 +10,37 @@ public class Application {
 
 
     public static void main(String[] args) {
-
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String inputStr =null;
+
         try{
-            String inputStr= Console.readLine();
-
-            Method method= new Method();
-
-            String[] splitStr;
-
-            if(inputStr.startsWith("//")){
-                inputStr=method.changeString(inputStr);
-            }
-
-            splitStr=method.changeToArray(inputStr);
-
-            int answer =0;
-            try{
-                answer= Arrays.stream(splitStr)
-                        .mapToInt(Integer::parseInt)
-                        .sum();
-            } catch (NumberFormatException e) {
-                System.out.println("잘못된 숫자 형식이 포함되어 있습니다.");
-            }
-
-            System.out.println("결과 : "+answer);
-        }catch(NoSuchElementException e){
+            inputStr= Console.readLine();
+        }catch (NoSuchElementException e){
             System.out.println("결과 : 0");
-            return;
         }
+
+        Method method= new Method();
+
+        String[] splitStr;
+
+        if(inputStr.startsWith("//")&&inputStr.substring(3,5).equals("\n")){
+            inputStr=method.changeString(inputStr);
+        }
+
+        splitStr=method.changeToArray(inputStr);
+
+        int answer =0;
+        try{
+            answer= Arrays.stream(splitStr)
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+        } catch (NumberFormatException e) {
+            System.out.println("잘못된 숫자 형식이 포함되어 있습니다.");
+        }
+
+        System.out.println("결과 : "+answer);
+
     }
 
 }
--- src/main/java/calculator/Method.java
+++ src/main/java/calculator/Method.java
@@ -13,11 +13,14 @@ public class Method {
     public String[] changeToArray(String changeInput){
         String [] toArray= changeInput.split(",|;");
 
+        for(int i=0;i<toArray.length;i++){
+            if(toArray[i].contains("-")){
+                throw new IllegalArgumentException("음수가 포함되어 있습니다.");
+            }
+        }
+
         for (int i = 0; i < toArray.length; i++) {
             toArray[i] = toArray[i].replaceAll("[^0-9]", "");
-            if(toArray[i].isEmpty()){
-                throw new IllegalArgumentException();
-            }
         }
 
         return toArray;
