bdf44e1e6a1aa2b6b15d0ebdcd85966caae84bf9
JunwooPark <wnsdn1994@gmail.com>
2024-10-21 00:04:02 +0900

    feat(calculator): 빈 문자열 또는 null 입력 시 0 반환 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,4 +4,11 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        return -1;  // TODO: 나머지 로직 추가
+    }
 }
