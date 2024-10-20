fd3d6664269c162a9386d4d0c61a01fe6d11900a
dbdb456 <adad568@naver.com>
2024-10-20 14:30:16 +0900

    feat(Controller): Validators와 Utils 의존성 추가

--- src/main/java/calculator/controller/Controller.java
+++ src/main/java/calculator/controller/Controller.java
@@ -1,6 +1,8 @@
 package calculator.controller;
 
 import calculator.domain.Calculator;
+import calculator.utils.Utils;
+import calculator.validators.Validators;
 import calculator.view.InputView;
 import calculator.view.OutPutView;
 
@@ -8,11 +10,16 @@ public class Controller {
     private final InputView inputView;
     private final OutPutView outputView;
     private final Calculator calculator;
+    private final Validators validators;
+    private final Utils utils;
 
-    public Controller(InputView inputView, OutPutView outputView, Calculator calculator) {
+    public Controller(InputView inputView, OutPutView outputView, Calculator calculator, Validators validators,
+                      Utils utils) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.calculator = calculator;
+        this.validators = validators;
+        this.utils = utils;
     }
 
     public void startWithDelimiterInitialization() {
