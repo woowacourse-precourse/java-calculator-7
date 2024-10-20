a1b2b8426f1c0e581b0769b99af270f833df41cc
hyunjilee1132 <hyunji.lee1132@gmail.com>
2024-10-20 20:49:47 +0900

    feat: 구분자로부터 수 추출 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,6 +10,28 @@ public class Application {
 
         // custom 판별
         boolean custom = isCustom(line);
+
+        // 구분자로 파싱
+        String[] line_result = {};
+        if (custom) {
+            boolean found = false;
+            char[] exceptional_chars = {'^','$','.','|','*','+','?','\\','(',')','{','}','[',']'};
+            char delimiter = line.charAt(2);
+            String substring = line.substring(5);
+
+            for (char exceptional_char: exceptional_chars) {
+                if (delimiter == exceptional_char) {
+                    line_result = substring.split("\\" + String.valueOf(delimiter));
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                line_result = substring.split(String.valueOf(delimiter));
+        }
+        else {
+            line_result = line.split(",|:");
+        }
     }
 
     // 기본 구분자 및 커스텀 구분자 판별
