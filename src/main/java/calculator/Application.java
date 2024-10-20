8b0ab24e5af73197c860bdf9a087e1624eac32e7
Parkjyun <jaeyun0507@naver.com>
2024-10-20 16:47:36 +0900

    feat(Application): 계산기를 시작한다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.model.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorController calculatorController = new CalculatorController(new InputView(), new OutputView(),
+                new Calculator());
+        calculatorController.run();
     }
 }
