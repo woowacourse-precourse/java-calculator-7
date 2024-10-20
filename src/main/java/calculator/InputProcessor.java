0eb91bd8ede72f31b49d63016776c70ef2bac23e
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-20 21:47:40 +0900

    feat(InputProcessor.java): 커스텀 구분자 인식 기능 구현
    - 사용자가 입력한 문자열에서 커스텀 구분자가 존재 여부를 확인하는 isInputHasCustomDelim 메서드를 구현하였다.
    - 사용자의 입력에서 커스텀 구분자를 꺼내오는 getCustomDelim 메서드를 구현하였다.
    - 사용자의 입력이 올바른지 검증하는 validateIsInputIncorrect 메서드를 구현하였다.

--- src/main/java/calculator/InputProcessor.java
+++ src/main/java/calculator/InputProcessor.java
@@ -15,7 +15,51 @@ public class InputProcessor {
         return instance;
     }
 
+    ExceptionHandler exceptionHandler = ExceptionHandler.getInstance();
+
     public void getInputFromUser() {
         String input = Console.readLine();
+        String delimiters = ",:";
+        char customDelim = '0';
+        boolean customDelimFlag = isInputHasCustomDelim(input);
+
+        if (customDelimFlag) {
+            delimiters = delimiters.concat(getCustomDelim(input));
+            input = input.substring(5);
+            customDelim = delimiters.charAt(2);
+        }
+
+        validateIsInputIncorrect(input, customDelimFlag, customDelim);
+    }
+
+    private boolean isInputHasCustomDelim(String input) {
+        if (input.charAt(0) == '/') {
+            for (int i = 0; i < 2; i++) {
+                exceptionHandler.checkIncorrectDelimGenerateInput(input.charAt(i));
+            }
+            for (int i = 3; i < 5; i++) {
+                exceptionHandler.checkIncorrectDelimGenerateInput(input.charAt(i));
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private String getCustomDelim(String input) {
+        char delim = input.charAt(2);
+        exceptionHandler.checkIncorrectDelim(delim);
+
+        return String.valueOf(delim);
+    }
+
+    private void validateIsInputIncorrect(String input, boolean customDelimFlag, char customDelim) {
+        if (customDelimFlag) {
+            exceptionHandler.checkIncorrectInputWithCustomDelim(input.toCharArray(), customDelim);
+            return;
+        }
+
+        exceptionHandler.checkIncorrectInputWithoutCustomDelim(input.toCharArray());
     }
 }
