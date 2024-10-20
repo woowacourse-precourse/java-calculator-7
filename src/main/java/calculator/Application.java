39199ad37a9839b22a2e9f597d8562cbb5f7feff
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 02:53:12 +0900

    feat(Application): 문자열 입력 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,15 @@
 package calculator;
 
 import calculator.model.DelimiterManagerImpl;
+import calculator.view.CalculatorInputView;
 
 public class Application {
     public static void main(String[] args) {
 
         DelimiterManagerImpl delimiterManagerImpl = new DelimiterManagerImpl();
         delimiterManagerImpl.init();
+
+        CalculatorInputView calculatorInputView = new CalculatorInputView();
+        String inputStr = calculatorInputView.inputExpression();
     }
 }
