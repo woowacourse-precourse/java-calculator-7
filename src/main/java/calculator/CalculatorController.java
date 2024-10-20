111e3296634721e67a3bedb2a1bf61efbbe4c7de
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-10-21 06:07:29 +0900

    refactor(StringSplitter): 문자열을 분리하는 메서드 분리
    문자열을 구분자로 구분하는 기능을 분리하는 메서드를 StringSplitter로 분리합니다.

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -1,20 +1,19 @@
 package calculator;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CalculatorController {
     private static final CalculatorView calculatorView = new CalculatorView();
-    private static final List<Integer> nums = new ArrayList<>();
     private final Delimiters delimiters;
     private final StringChecker stringChecker;
+    private final StringSplitter stringSplitter;
     private String inputString;
 
     public CalculatorController(String inputString) {
         this.inputString = inputString;
         this.delimiters = new Delimiters();
         this.stringChecker = new StringChecker(inputString, delimiters);
+        this.stringSplitter = new StringSplitter(inputString, delimiters);
         startCalculator();
     }
 
@@ -26,28 +25,13 @@ public class CalculatorController {
 
         stringChecker.checkBoundary();
         inputString = stringChecker.checkCustomDelimiter();
-        splitString(inputString);
-        calculate();
+        List<Integer> numbers = stringSplitter.splitString();
+        calculate(numbers);
     }
 
-    private void splitString(String inputString) {
-        String regex = "[" + delimiters.getDelimiters().stream()
-                .map(String::valueOf)
-                .collect(Collectors.joining()) + "]";
-
-        String[] splitString = inputString.split(regex);
-
-        for (String part : splitString) {
-            if (!part.chars().allMatch(Character::isDigit)) {
-                throw new IllegalArgumentException("구분자를 잘못 입력하였습니다.");
-            }
-            nums.add(Integer.parseInt(part));
-        }
-    }
-
-    private void calculate() {
+    private void calculate(List<Integer> numbers) {
         int sumResult = 0;
-        for (int i : nums) {
+        for (int i : numbers) {
             sumResult += i;
         }
 
---
+++ src/main/java/calculator/StringSplitter.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringSplitter {
+    private final String inputString;
+    private final Delimiters delimiters;
+
+    public StringSplitter(String inputString, Delimiters delimiters) {
+        this.inputString = inputString;
+        this.delimiters = delimiters;
+    }
+
+    public List<Integer> splitString() {
+        String regex = "[" + delimiters.getDelimiters().stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining()) + "]";
+
+        String[] splitString = inputString.split(regex);
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String part : splitString) {
+            if (!part.chars().allMatch(Character::isDigit)) {
+                throw new IllegalArgumentException("구분자를 잘못 입력하였습니다.");
+            }
+            numbers.add(Integer.parseInt(part));
+        }
+
+        return numbers;
+    }
+}
