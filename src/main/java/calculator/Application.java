cc7fe0ac760d8b201a86872d96bfcb695611424e
MelGob <mipo5204@naver.com>
2024-10-20 23:39:15 +0900

    구분자 1차 병합

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,7 +6,13 @@ public class Application {
         // 문자열 입력받기
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputStr;
+        String convertedStr;
         inputStr = camp.nextstep.edu.missionutils.Console.readLine();
         System.out.println(inputStr);
+
+        // 구분자 1차 병합 (":" -> ",")
+        convertedStr = inputStr.replace(":",",");
+        StringBuilder entireStr = new StringBuilder(convertedStr);
+
     }
 }
