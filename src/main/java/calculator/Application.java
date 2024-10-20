7b6fe21233ab9cb2eec9d819f468eb101370014a
iamdonghyuk <iamdonghyuk@gmail.com>
2024-10-20 21:32:12 +0900

    [프리코스 1주차] 문자열 덧셈 계산기 구현 - 주석삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,7 +5,6 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        //Scanner sc = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
 
