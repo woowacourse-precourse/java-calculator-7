08f77203dd0ced8104d66a274b296180bbf7e430
Fletacode <bassykd@gmail.com>
2024-10-21 00:46:57 +0900

    refactor(tool2): Extracts 리팩토링
    nums 문자열과 구분자문자열 regex로 split 구현:

--- src/main/java/calculator/tool2/Extracts.java
+++ src/main/java/calculator/tool2/Extracts.java
@@ -1,45 +1,35 @@
 package calculator.tool2;
 
+import calculator.tool1.StringChecker;
 import java.util.Arrays;
 
 public class Extracts {
 
+  private int[] nums;
+
   private String numString;
-  private String delimiterString;
-  private String[] delimiters;
-  private long[] nums;
 
+  private String delimiterRegex;
 
-  public long[] getNums() {
+  public int[] getNums() {
     return this.nums;
   }
 
-  public String[] extractDelimiter() {
-    return this.delimiters =
-        delimiterString.substring(2).split("");
-  }
-
-  public long[] extractNum() {
-    //구분자들로 regex를 만듭니다. ex) [:,]
-    String regex = convertToRegex(delimiters);
-
-    //regex로 숫자들을 추출하고 long 형 배열로 숫자들을 리턴합니다.
-    return this.nums = Arrays.stream(numString.split(regex))
-        .mapToLong(Long::parseLong)
-        .toArray();
-  }
-
-  private String convertToRegex(String[] delimiters) {
-    String regex = "[:,";
-    for (String delimiter : delimiters) {
-      regex += delimiter;
-    }
-    return regex + "]";
-  }
-
-  public Extracts(String numString, String delimiterString) {
+  public Extracts(String numString, String delimiterRegex) {
     this.numString = numString;
-    this.delimiterString = delimiterString;
+    this.delimiterRegex = delimiterRegex;
+  }
+
+  public void splitNums(){
+    String[] strNums = numString.split(delimiterRegex);
+
+    if (!StringChecker.checkNumberic(strNums)){
+      throw new IllegalArgumentException("숫자가 아니거나 음수입니다.");
+    }
+
+    this.nums = Arrays.stream(strNums)
+        .mapToInt(Integer::parseInt)
+        .toArray();
   }
 
 }
