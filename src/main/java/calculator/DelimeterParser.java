07778ccd379cfc2bf422295a4a7a81cdb9b21c32
sung-silver <jinna0319@gmail.com>
2024-10-21 00:13:34 +0900

    fix: DelimeterParse에서 이스케이프 문자열 처리 수정

--- src/main/java/calculator/DelimeterParser.java
+++ src/main/java/calculator/DelimeterParser.java
@@ -1,32 +1,33 @@
 package calculator;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
-
 public class DelimeterParser {
 
     private static final String DEFAULT_DELIMITERS = "[,|:]";
+    private static final String CUSTOM_DELIMITER_START = "//";
+    private static final String CUSTOM_DELIMITER_END = "\\n";
     private static final String CUSTOM_DELIMITER_PREFIX = "//.*\n";
 
     public static String[] parse(String input) {
         String delimiters = getDelimiter(input);
         String numbers = removeCustomDelimiterPrefix(input);
+        System.out.println("delimiters = " + delimiters);
         return numbers.split(delimiters);
     }
 
     private static String getDelimiter(String input) {
-        boolean hasCustomDelimiter = input.startsWith("//");
+        boolean hasCustomDelimiter = input.startsWith(CUSTOM_DELIMITER_START);
 
         if (hasCustomDelimiter) {
-            int delimiterEndIndex = input.indexOf("\n");
-            return Pattern.quote(input.substring(2, delimiterEndIndex));
+            int delimiterStartIndex = CUSTOM_DELIMITER_START.length();
+            int delimiterEndIndex = input.indexOf(CUSTOM_DELIMITER_END);
+            return input.substring(delimiterStartIndex, delimiterEndIndex);
         }
 
         return DEFAULT_DELIMITERS;
     }
 
     private static String removeCustomDelimiterPrefix(String input) {
+        input = input.replace("\\n", "\n"); // to convert escape character to new line
         return input.replaceFirst(CUSTOM_DELIMITER_PREFIX, "");
     }
 }
