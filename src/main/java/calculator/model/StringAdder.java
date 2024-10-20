8368d68f922e785d24d0af38b3fc1ebdcb67d1f2
mirolis6636 <mirolis6636@gmail.com>
2024-10-21 06:46:17 +0900

    refactor: remove redundant local variable and expression
    Before
      number.isEmpty()==false
      int result = a sort of stream method, and it returns result
    After
      !number.isEmpty()
      return stream result directly

--- src/main/java/calculator/model/StringAdder.java
+++ src/main/java/calculator/model/StringAdder.java
@@ -5,10 +5,9 @@ import java.util.Arrays;
 public class StringAdder {
     public int addString(String input, String delimiter){
         String[] numbers = input.split(delimiter);
-        int result = Arrays.stream(numbers)
-                .filter(number->number.isEmpty()==false)
+        return Arrays.stream(numbers)
+                .filter(number->!number.isEmpty())
                 .mapToInt(Integer::parseInt)
                 .sum();
-        return result;
     }
 }
