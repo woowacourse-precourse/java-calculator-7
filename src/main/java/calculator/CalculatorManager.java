58d49c293b8e9f3080a926ff3b3ef0309f066046
theplayer1003 <theplayer1003@gmail.com>
2024-10-20 22:05:06 +0900

    feat(CalculatorManager): implement calculatormanager
    implement:
    - call other functions
    
    issue:
    - DI issue

--- src/main/java/calculator/CalculatorManager.java
+++ src/main/java/calculator/CalculatorManager.java
@@ -3,4 +3,32 @@ package calculator;
 import java.util.List;
 
 public class CalculatorManager {
+    private String userInput;
+    private String parsedUserInput;
+    private List<Character> delimiter;
+    private int sum;
+
+    public void makeUserInput(InputHandler inputHandler){
+        this.userInput = inputHandler.getUserIntput();
+    }
+
+    public void makeDelimiter(DelimiterParser delimiterParser){
+        this.delimiter = delimiterParser.parseDelimiter(userInput);
+    }
+
+    public void parseMarker(DelimiterParser delimiterParser){
+        this.parsedUserInput = delimiterParser.parseMarker(userInput);
+    }
+
+    public void checkCompatibility(CheckCompatibility checkCompatibility){
+        checkCompatibility.checkInvalidInput(parsedUserInput, delimiter);
+    }
+
+    public void calculateSum(Calculator calculator){
+        this.sum = calculator.getSum(parsedUserInput,delimiter);
+    }
+
+    public void printAnswer(OutputHandler outputHandler){
+        outputHandler.printAnswer(sum);
+    }
 }
