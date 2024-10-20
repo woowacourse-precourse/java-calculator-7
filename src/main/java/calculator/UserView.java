f4c6885158a08fe2410e68880ca7ec11787f2ed7
33jyu33 <jy04169@gmail.com>
2024-10-21 05:52:04 +0900

    fix(input): exclude strip function from readline
    Spaces should also be included in validation because they are input values

--- src/main/java/calculator/UserView.java
+++ src/main/java/calculator/UserView.java
@@ -3,16 +3,15 @@ package calculator;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class UserView {
-    public String readInputString() {
-        return readLine().strip();
+    public static String readInputString() {
+        return readLine();
     }
 
-    public void printStartCalculator(){
+    public static void printStartCalculator(){
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
-    public void printResult(int result){
+    public static void printResult(int result){
         System.out.printf("결과 : %d", result);
     }
-
 }
