894550faae1ade0862133ab9c3749ea1e76bf28b
JeongHwan Kim <silverice1973@naver.com>
2024-10-21 08:58:41 +0900

    feat: 애플리케이션 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import calculator.domain.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView input = new InputView();
+        Calculator calculator = new Calculator();
+        new OutputView(calculator.calculate(input.getUserInput()));
     }
 }
