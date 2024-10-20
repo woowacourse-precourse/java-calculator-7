f13be5193de6e58c117bca3bb929fcc0457b16d2
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-20 21:04:36 +0900

    refactor: 출력 dto 레코드 타입으로 변경

--- src/main/java/calculator/dto/OutputResponse.java
+++ src/main/java/calculator/dto/OutputResponse.java
@@ -1,15 +1,6 @@
 package calculator.dto;
 
-public class OutputResponse {
-    private final int result;
-
-    public OutputResponse(int result) {
-        this.result = result;
-    }
-
-    public int getResult() {
-        return result;
-    }
+public record OutputResponse(int result) {
 
     public static OutputResponse of(int result) {
         return new OutputResponse(result);
