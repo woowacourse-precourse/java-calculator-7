25b55047af8111cd91d4f5b0bbab16376449eb23
mohyerolo <garam2101@naver.com>
2024-10-20 23:23:41 +0900

    fix: Input 책임 줄이고 커스텀 구분자 사용 시 여러개일 경우 추가

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -1,66 +1,57 @@
 package calculator;
 
 public class Input {
+    private static final String DEFAULT = ",|:";
+
     private final String str;
     private final String splitter;
 
     public Input(String input) {
-        validateInput(input);
         this.splitter = findSplitter(input);
         this.str = getInputString(input);
-        validateCharacter(str);
-    }
-
-    private void validateInput(String input) {
-        if (input.startsWith("//")) {
-            validateUsingCustomSplitter(input);
-        } else {
-            validateDefault(input);
-        }
-    }
-
-    private void validateUsingCustomSplitter(String input) {
-        if (input.length() < 5 || !input.startsWith("\\n", 3)) {
-            throw new IllegalArgumentException("잘못된 입력으로 프로그램이 종료됩니다. \"//\"와 \"\\n\" 사이에 위치하는 문자만 커스텀 구분사로 사용할 수 있습니다.");
-        }
-    }
-
-    private void validateDefault(String input) {
-        if (!input.isEmpty() && !Character.isDigit(input.charAt(0))) {
-            throw new IllegalArgumentException("잘못된 입력으로 프로그램이 종료됩니다. 입력이 없거나 숫자 또는 //로 시작해야합니다");
-        }
     }
 
     private String findSplitter(String input) {
-        if (!input.startsWith("//")) return "[,:]";
-        return String.valueOf(input.charAt(2));
+        if (input.startsWith("//")) {
+            return findCustomSplitter(input);
+        }
+        return DEFAULT;
+    }
+
+    private String findCustomSplitter(String input) {
+        StringBuilder customSplitter = new StringBuilder(",|:");
+        while (input.startsWith("//")) {
+            int endIndex = input.indexOf("\\n");
+            if (endIndex == -1) {
+                throw new IllegalArgumentException("잘못된 입력으로 프로그램이 종료됩니다. 커스텀 구분자 종료 \\n가 존재하지 않습니다.");
+            }
+
+            String custom = checkMetaCharacters(input.substring(2, endIndex));
+            customSplitter.append("|").append(custom);
+
+            input = input.substring(endIndex + 2);
+        }
+
+        return customSplitter.toString();
+    }
+
+    private String checkMetaCharacters(String splitter) {
+        return splitter.replaceAll("([\\^$.?*+()\\[\\]{}|\\\\-])", "\\\\$1");
     }
 
     private String getInputString(String input) {
-        if (splitter.equals("[,:]")) {
+        if (splitter.equals(DEFAULT)) {
             return input;
         }
-        return input.substring(5);
+        return getInputWithoutCustomSplitter(input);
     }
 
-    private void validateCharacter(String str) {
-        boolean defaultSplitter = splitter.equals("[,:]");
-        char chSplitter = splitter.charAt(0);
-        for (char ch : str.toCharArray()) {
-            if (!isValidCharacter(ch, defaultSplitter, chSplitter)) {
-                throw new IllegalArgumentException("잘못된 입력으로 프로그램이 종료됩니다. 문자열은 비어있거나 숫자, 구분자만 입력받습니다.");
-            }
+    private String getInputWithoutCustomSplitter(String input) {
+        while (input.startsWith("//")) {
+            int endIndex = input.indexOf("\\n");
+            input = input.substring(endIndex + 2);
         }
-    }
-
-    private boolean isValidCharacter(char ch, boolean defaultSplitter, char customSplitter) {
-        if (Character.isDigit(ch)) {
-            return true;
-        }
-        if (defaultSplitter && (ch == ',' || ch == ':')) {
-            return true;
-        }
-        return ch == customSplitter;
+        return input;
     }
 
     public String getStr() {
