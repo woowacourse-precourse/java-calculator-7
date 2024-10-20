c94120068de40b18abdb7fcb7cf5193dd7c82f69
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:45:25 +0900

    :sparkles: feat(CalculatorController): 입력된 문자열을 처리하여 계산 결과를 반환

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,22 @@
 package calculator.controller;
 
+import calculator.model.StringCalculator;
+import calculator.view.CalculatorView;
+
 public class CalculatorController {
+
+    private final StringCalculator stringCalculator;
+    private final CalculatorView calculatorView;
+
+    public CalculatorController(StringCalculator stringCalculator, CalculatorView calculatorView) {
+        this.stringCalculator = stringCalculator;
+        this.calculatorView = calculatorView;
+    }
+
+    public void run() {
+        String input = calculatorView.getInput();
+
+        int result = stringCalculator.add(input);
+        calculatorView.displayResult(result);
+    }
 }
