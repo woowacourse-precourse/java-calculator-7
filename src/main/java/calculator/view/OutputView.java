ceac95c63d2008f7543927df6bb8b1c41a2e3139
wvssm <neoramiregi@gmail.com>
2024-10-20 11:11:20 +0900

    feat(OutputView): 연산 결과과 정수인 경우 정수로 출력하는 기능 구현

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -4,6 +4,20 @@ public class OutputView {
     private static final String RESULT = "결과 : ";
 
     public static void printResult(double number){
-        System.out.println(RESULT + number);
+        String formattedNumber = formatNumber(number);
+        System.out.println(RESULT + formattedNumber);
+    }
+
+
+    private static String formatNumber(double number) {
+        if (isInteger(number)) {
+            return String.valueOf((int) number);
+        } else {
+            return String.valueOf(number);
+        }
+    }
+
+    public static boolean isInteger(double value) {
+        return value % 1 == 0;
     }
 }
