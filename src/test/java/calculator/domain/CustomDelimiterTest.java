d07dc467330d4be00de42fd15331161bfc442b41
kjyyjk <rlawnsdud920@gmail.com>
2024-10-20 22:42:25 +0900

    refactor: import문 와일드카드 * 제거

--- src/test/java/calculator/domain/CustomDelimiterTest.java
+++ src/test/java/calculator/domain/CustomDelimiterTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CustomDelimiterTest {
 
--- src/test/java/calculator/domain/DelimitersTest.java
+++ src/test/java/calculator/domain/DelimitersTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DelimitersTest {
 
