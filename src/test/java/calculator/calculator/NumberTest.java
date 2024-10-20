2ee04e3e0450877f513774b64a3e2870575f3bb0
hunko <rhwlgns4386@naver.com>
2024-10-20 18:34:27 +0900

    test : 문자가 아닌 숫자 생성시 예외 발생 테스트 추가

--- src/test/java/calculator/calculator/NumberTest.java
+++ src/test/java/calculator/calculator/NumberTest.java
@@ -1,8 +1,10 @@
 package calculator.calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import calculator.Number;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,5 +14,10 @@ public class NumberTest {
     @ValueSource(strings = {" ", ""})
     public void 빈문자는_0으로취급(String value) {
         assertThat(Number.from(value)).isEqualTo(new Number(0));
+    }
+
+    @Test
+    public void 숫자가_아닌_문자로_생성() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Number.from("a"));
     }
 }
