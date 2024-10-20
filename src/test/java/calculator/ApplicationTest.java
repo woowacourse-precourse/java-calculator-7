c4048b61e65b5bd384366c9458de120b2eb53532
Jinyshin <iwbm312@gmail.com>
2024-10-21 01:19:06 +0900

    test(calculator): add tests for blank string inputs

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -8,6 +9,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    @DisplayName("빈 문자열 입력시 결과가 0으로 출력된다")
+    void 빈_문자열_입력_테스트() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과: 0");
+        });
+    }
+
+    @Test
+    @DisplayName("공백 문자열 입력시 결과가 0으로 출력된다")
+    void 공백_문자열_입력_테스트() {
+        assertSimpleTest(() -> {
+            run("    ");
+            assertThat(output()).contains("결과: 0");
+        });
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
