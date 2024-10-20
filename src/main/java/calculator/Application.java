4e5b493d8211358a35ab835df1fb84631e3b6c80
htjworld <116538001+htjworld@users.noreply.github.com>
2024-10-20 19:24:53 +0900

    덧셈 계산 및 커스텀 구분자 로직 추가 (예외 처리 예정)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -14,27 +14,39 @@ public class Application {
 
         String input = Console.readLine();
         ArrayList <String> delimiters = new ArrayList<>(Arrays.asList(",",":"));
+        // 재사용성을 위해 delimiters 안에 값을 몇개고 추가할 수 있게 해놓음
 
-        Pattern pattern = Pattern.compile("//(.*?)\\n");
-        Matcher matcher = pattern.matcher(input);
+        if (input.charAt(0) =='/' & input.charAt(1) == '/'){
+            System.out.println("와우");
 
-        while (matcher.find()) {
-            String delimiter = matcher.group(1);
-            System.out.println(("구분자 추가"+delimiter));
-            delimiters.add(delimiter);
+            int index = input.indexOf("\\n");
+            String new_delimiter = input.substring(2,index);
+            System.out.println(new_delimiter);
+            input = input.substring(index+2,input.length());
+            delimiters.add(new_delimiter);
+        }
+
+
+        try{
+            if(delimiters.size()>3){
+                throw new IllegalArgumentException("커스텀 구분자가 2개 이상 추가되었습니다. 커스텀 구분자는 1개까지만 허용됩니다.");
+            }
+
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            System.exit(1); //애플리케이션 종료
         }
 
         System.out.println("최종구분자"+delimiters);
 
-        for (String d : delimiters){
-            input = input.replaceAll(Pattern.quote("//"+d+"q"),",");
-        }
-        System.out.println(input);
 
+        System.out.println("input은:"+input);
         String regex = String.join("|",delimiters);
 
+
         String arr[] = input.split(regex);
-        System.out.println("arr:"+arr);
+        System.out.println("에러냐");
+        System.out.println(arr);
         for (String a : arr){
             result+= Float.parseFloat(a);
         }
