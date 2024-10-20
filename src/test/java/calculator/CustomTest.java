dbbdfc97109ba9bfa50d83d6114d6c1fdcdce4c8
making-a-scene <4936joo@naver.com>
2024-10-20 21:10:46 +0900

    test: 정규식을 통한 커스텀 구분자 사용 여부 확인 기능 테스트

---
+++ src/test/java/calculator/CustomTest.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CustomTest {
+
+    @Test
+    void 커스텀_구분자_사용() {
+        String CUSTOM_DELIMITER_FORMAT = "//.\\n.*";
+        assertThat(Pattern.matches(CUSTOM_DELIMITER_FORMAT, "//?\n2:3,2")).isTrue();
+    }
+
+    @Test
+    void 커스텀_구분자_사용하지_않음() {
+        String CUSTOM_DELIMITER_FORMAT = "//.\\n.*";
+        assertThat(Pattern.matches(CUSTOM_DELIMITER_FORMAT, "2:3,2")).isFalse();
+    }
+}
