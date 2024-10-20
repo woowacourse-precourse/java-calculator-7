31411722a61636b235d590138a81e71ed00fd014
ruka-sh <ori8591y@gmail.com>
2024-10-20 12:39:37 +0900

    feat: 커스텀 구분자를 포함한 문자열의 합 계산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,16 +6,34 @@ import java.util.StringTokenizer;
 
 
 public class Application {
+
+    /**
+     * 구분자와 양수로 구성된 문자열에서 숫자의 합을 계산합니다.
+     *
+     * @param s 합을 계산할 문자열
+     * @return 합계
+     */
     static int calculate(String s) {
         //System.out.println(s);
 
+        String refinedString = s;
+        String listOfDelimiters = ",|:";
         int sumResult = 0;
 
         if (s.equals("")) {
             return 0;
         }
 
-        StringTokenizer st = new StringTokenizer(s, ",|:");
+        // 커스텀 구분자 처리
+        if (s.startsWith("//")) {
+            int endOfDelimeter = s.indexOf("\\n");
+            listOfDelimiters += "|" + s.substring(2, endOfDelimeter);
+
+            int startOfNumbers = endOfDelimeter + 2;
+            refinedString = s.substring(startOfNumbers);
+        }
+
+        StringTokenizer st = new StringTokenizer(refinedString, listOfDelimiters);
 
         while (st.hasMoreElements()) {
             String token = String.valueOf(st.nextElement());
@@ -24,6 +42,7 @@ public class Application {
         }
 
         return sumResult;
+
     }
 
     public static void main(String[] args) {
@@ -33,7 +52,6 @@ public class Application {
         String inputString = Console.readLine();
 
         calculate(inputString);
-        // System.out.println("결과 : " + calculate(inputString));
-
+        //System.out.println("결과 : " + calculate(inputString));
     }
 }
