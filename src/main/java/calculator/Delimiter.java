a482f553e3158b24525ba716bdf1728a273e3ba0
YuyoungRhee <bbclara@naver.com>
2024-10-20 17:14:38 +0900

    refactor(Delimiter): 커스텀 구분자 처리 로직 리팩토링 및 상수 분리
    - 기본 구분자와 커스텀 구분자에 대한 상수를 도입해 가독성 향상
    - 커스텀 구분자를 추출하는 로직을 분리하여 메서드의 단일 책임 원칙을 준수

--- src/main/java/calculator/Delimiter.java
+++ src/main/java/calculator/Delimiter.java
@@ -2,16 +2,25 @@ package calculator;
 
 public class Delimiter {
 
+    private static final String DEFAULT_DELIMITERS = ",|:";
+    private static final String CUSTOM_DELIMITER_START = "//";
+    private static final String CUSTOM_DELIMITER_END = "\\n";
+
     public String findDelimiter(String input) {
-        String delimiters = ",|:";
-
-        if (input.contains("//") && input.contains("\\n")) {
-            int startIndex = input.indexOf("//") + 1;
-            int endIndex = input.indexOf("\\n");
-            String customDelimiters = input.substring(startIndex + 1, endIndex);
-            delimiters += "|" + customDelimiters;
+        String customDelimiter = extractCustomDelimiter(input);
+        if (customDelimiter.isEmpty()) {
+            return DEFAULT_DELIMITERS;
         }
+        return DEFAULT_DELIMITERS + "|" + customDelimiter;
+    }
 
-        return delimiters;
+    public String extractCustomDelimiter(String input) {
+        if (input.contains(CUSTOM_DELIMITER_START) && input.contains(CUSTOM_DELIMITER_END)) {
+            int startIndex = input.indexOf(CUSTOM_DELIMITER_START) + CUSTOM_DELIMITER_START.length();
+            int endIndex = input.indexOf(CUSTOM_DELIMITER_END);
+
+            return input.substring(startIndex, endIndex);
+        }
+        return "";
     }
 }
