a6502377f24cb9867954fa0f546a2ef6c5fd2f95
woojin <zaza0804@uos.ac.kr>
2024-10-20 17:37:13 +0900

    test(Application): add Application Test
    - NsTest를 이용해 에플리케이션의 통합테스트를 추가했습니다

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -12,9 +12,33 @@ class ApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("//;\\n1;5");
+            assertThat(output()).contains("결과 : 6");
         });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_혼합() {
+        assertSimpleTest(() -> {
+            run("//;\\n12,3,;5");
+            assertThat(output()).contains("결과 : 20");
+        });
+    }
+
+    @Test
+    void 구분자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//nn\\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_0포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
