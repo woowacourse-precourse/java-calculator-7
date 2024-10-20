c2d0213af24480e1635d3322f3402a596dde05a2
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-10-20 16:15:36 +0900

    :bug: fix($Application) add exception case
    - 구분자가 문자가 아닌 문자열 일 경우 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -15,7 +15,10 @@ public class Application {
 
             if (endIndex != -1) { // \n 이 있다면 커스텀 구분자로 업데이트
                 String customDetermine = input.substring(2, endIndex); // 커스텀 구분자 업데이트
-                determine = "[,:" + customDetermine+ "]";
+                if(customDetermine.length() > 1){
+                    throw new IllegalArgumentException();
+                }
+                determine = "[,:" + customDetermine + "]";
                 input = input.substring(endIndex + 2);
 
                 if(!input.contains(customDetermine)){ // 커스텀 구분자로 구분되지 않을 경우
