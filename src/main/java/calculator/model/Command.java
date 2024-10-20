d292a8d2ba56570e5b3c1018b283237be86b5419
HyeonsuLee <leehyeonsu4888@naver.com>
2024-10-20 15:09:16 +0900

    refactor: 커스텀 구분자가 숫자인 경우 예외 반환

--- src/main/java/calculator/model/Command.java
+++ src/main/java/calculator/model/Command.java
@@ -9,7 +9,7 @@ public record Command(
     String header,
     String content
 ) {
-    private static final Pattern customPattern = Pattern.compile("//(.+?)\\\\n");
+    private static final Pattern customPattern = Pattern.compile("^//(.+?)\\\\n");
 
     public static Command from(String command) {
         Matcher matcher = customPattern.matcher(command);
@@ -19,11 +19,18 @@ public record Command(
             header = matcher.group(1);
             content = matcher.replaceAll("");
         }
+        if (containsDigit(header)) {
+            throw new IllegalArgumentException("커스텀 구분자에 숫자를 포함할 수 없습니다.");
+        }
         return new Command(header, content);
     }
 
     public List<Number> extractNumbers() {
         String splitter = String.format("[%s,:]", header);
         return Arrays.stream(content.split(splitter)).map(Number::from).toList();
+    }
+
+    private static boolean containsDigit(String input) {
+        return input.matches(".*\\d.*");
     }
 }
