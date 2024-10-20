c8d0af0a6b0a1920cb516b9b2541932362c4f130
skadjs <skadjs11@gmail.com>
2024-10-20 22:11:19 +0900

    feat: 입력값 공백 제거 기능 추가
    사용자 입력값의 공백을 제거하기 위해 removeBlank() 메서드를 추가함.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,10 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class StringCalculator {
 
     String userInput;
+    String noBlankInput;
 
     public void getUserInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         this.userInput = Console.readLine();
         System.out.println("사용자 입력값 : " + this.userInput); //메서드 기능 점검용 코드 - 추후 삭제
+    }
+
+    public void removeBlank() {
+        this.noBlankInput = this.userInput.replaceAll(" ", "");
+        System.out.println("공백 제거된 사용자 입력값: " + this.noBlankInput); //메서드 기능 점검용 코드
     }
 }
