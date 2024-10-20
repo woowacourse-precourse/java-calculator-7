3ca0332cc845122550e3dabed28c52c5ac39cd93
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-20 21:09:11 +0900

    refactor: 출력 View dto 방식으로 변경

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,7 +1,12 @@
 package calculator.view;
 
+import calculator.dto.OutputResponse;
+
+import static calculator.message.OutputMessage.RESPONSE_MESSAGE;
+
 public class OutputView {
-    public void print(long result){
-        System.out.println("결과 : " + result);
+    public static void outputMessage(OutputResponse response) {
+        System.out.print(RESPONSE_MESSAGE.getMessage() + response.result());
     }
 }
+
