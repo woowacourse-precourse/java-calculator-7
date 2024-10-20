e8a1b0e8a3a6de528cd1880f4acc168d669e554d
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-20 18:56:30 +0900

    refactor : utils.Validator 에러처리 커스텀화 구분 로직으로 변경

--- src/main/java/calculator/utils/Validator.java
+++ src/main/java/calculator/utils/Validator.java
@@ -1,5 +1,7 @@
 package calculator.utils;
 
+import calculator.exception.InvalidDelimiterException;
+import calculator.exception.InvalidNumberException;
 import java.util.List;
 
 public class Validator {
@@ -7,14 +9,15 @@ public class Validator {
     public static void validateNumberList(List<Double> numbers) {
         for (Double number : numbers) {
             if (number <= 0) {
-                throw new IllegalArgumentException("숫자는 양수만 가능합니다");
+                throw new InvalidNumberException("숫자는 양수만 가능합니다");
             }
         }
     }
 
     public static void validateCustomDelimiter(String customDelimiter) {
         if (customDelimiter.length() != 1 || Character.isDigit(customDelimiter.charAt(0))) {
-            throw new IllegalArgumentException("커스텀 구분자는 1자리만 가능합니다");
+            throw new InvalidDelimiterException("커스텀 구분자는 1자리만 가능합니다");
         }
     }
 }
+
