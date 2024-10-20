63eb890cd4790cf69f564e0fffd5b2e2ef8732c8
023-dev <0_2_3@naver.com>
2024-10-20 15:45:27 +0900

    feat(Calculator.class): 문자열이 null인 경우 예외 처리 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -13,12 +13,28 @@ public class Calculator {
     }
 
     /**
-     * 빈 문자열일 경우 0을 반환합니다.
+     * 입력 문자열이 null인지 확인합니다.
+     *
+     * @param input 사용자가 입력한 문자열
+     * @throws IllegalArgumentException null일 경우 IllegalArgumentException을 발생시킵니다.
+     */
+    private void isNull(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("입력값이 null입니다.");
+        }
+    }
+
+    /**
+     * <ul>
+     *     <li>빈 문자열일 경우 0을 반환합니다.</li>
+     *     <li>null일 경우 IllegalArgumentException을 발생시킵니다.</li>
+     * </ul>
      *
      * @param input 사용자가 입력한 문자열입니다.
      * @return 문자열에서 추출한 숫자의 합산 값을 반환합니다.
      */
     public int add(String input) {
+        isNull(input);
         if (isEmpty(input)) {
             return 0;
         }
