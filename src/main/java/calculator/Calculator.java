6d94380a3680522a1b66909b9617b5a72bf5af06
fortune-man <wnguddl96@knou.ac.kr>
2024-10-20 22:35:18 +0900

    refactor(Calculator) : && 오타를 !=로 수정, 커스텀 구분자 메서드 분리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import static java.lang.Character.*;
+import static java.lang.Integer.parseInt;
 import static java.lang.String.valueOf;
 
 public class Calculator {
@@ -11,6 +12,14 @@ public class Calculator {
 
     // [] 입력 문자열 탐색중
     for (int i = 0; i < readLine.length(); i++) {
+      // 문자열이 커스텀 구분자로 시작하면
+      if (readLine.startsWith("//;\\n")) {
+        // 비정상 입력을 체크하고
+
+        // 정상인 경우 결과를 합산한다
+        String numeric = valueOf(readLine.charAt(i));
+        addNumbers(convertToNumber(numeric));
+      }
       char index = readLine.charAt(i);
       // 예외 체크
       checkException(index);
@@ -20,6 +29,10 @@ public class Calculator {
     // 전달
     return target;
   }
+  // 문자열을 숫자로 변환
+  private static char convertToNumber(String numeric) {
+    return (char) parseInt(numeric);
+  }
 
   private void validProcess(char index) {
     //  인덱스가 기본 구분자 ‘,’ , ‘:’ 인 경우
@@ -27,20 +40,26 @@ public class Calculator {
       // 건너뛴다
       return;
     }
+    addNumbers(index);
+  }
+
+  private void addNumbers(char index) {
     // [] 입력 문자열 탐색 중 인덱스가 정수 변환 가능한 경우
     if (isDigit(index)) {
       // target에 합산한다
-      target += Integer.parseInt(valueOf(index));
+      target += parseInt(valueOf(index));
     }
   }
 
-  // 문자열 유효 체크
+  // 문자열의 요소가 유효하지 않으면 예외 처리한다
   private void checkException(char index) {
     // [] 디버깅 :  first type:  boolean,  second type: char
-    if (index != ',' && index && ':' && !isDigit(index)) {
+    // 특정 구분자가 아닌 경우
+    if (index != ',' && index != ':' && isDigit(index)) {
       // [] 비정상 입력 예외 처리
       throw new IllegalArgumentException("입력 가능한 문자열인지 확인해주세요. ',', ':'");
     }
+
 
 
 
