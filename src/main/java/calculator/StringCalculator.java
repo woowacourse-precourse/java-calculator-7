9e223738d77e2f97aff96cc6d41f85e2499046c7
mirinae700 <mirinae219@gmail.com>
2024-10-20 22:51:22 +0900

    feat: 문자열 필터링 메서드
    커스텀 구분자 지정 여부 확인 후 문자열에서 필터링

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,15 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private final String BASIC_SEPARATOR = ",|:";
+    private final String CUSTOM_SEPARATOR_PATTERN = "//(.)\\\\n(.*)";
+    private String customSeparator = "";
+
+
     public String input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
@@ -22,5 +28,21 @@ public class StringCalculator {
         Pattern pattern = Pattern.compile("[^0-9" + regString + "]");
 
         return pattern.matcher(userInput).find();
+    }
+
+    public String filterCustomSeparatorPattern(String userInput) {
+        // 1. Pattern 객체를 사용하여 문자열을 패턴 객체로 변환
+        Pattern pattern = Pattern.compile(CUSTOM_SEPARATOR_PATTERN);
+
+        // 2. 패턴 객체를 사용해 사용자 입력값을 검사하고 필터링된 결과를 Matcher 객체로 반환
+        Matcher matcher = pattern.matcher(userInput);
+
+        // 3. 매칭된 결과가 있는지 확인
+        if (matcher.find()) {
+            customSeparator = matcher.group(1);
+            userInput = matcher.group(2);
+        }
+
+        return userInput;
     }
 }
