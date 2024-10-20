a80b2017633ddfa567dce65ec5c68e3497f085b3
HiImSUN <kshk08@hanyang.ac.kr>
2024-10-20 20:31:53 +0900

    feat: 커스텀 구분자 사용 여부 판별 기능 추가. 이후 사용 여부에 맞게 핸들링 메소드 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,6 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.*;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -21,5 +20,58 @@ public class Application {
         for (char c : input.toCharArray()) {
             inputList.add(c);
         }
+
+        if (inputList.getFirst() == '/' && inputList.size() > 4) {
+            sum = handleCustomDelimiter(inputList);
+        } else if (Character.isDigit(inputList.getFirst())) {
+            sum = handleDefaultDelimiters(inputList);
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+        System.out.println("결과 : " + sum);
+    }
+
+    private static int handleCustomDelimiter(List<Character> inputList) {
+        List<Character> dividerList = new ArrayList<>();
+        List<Integer> tmpNum = new ArrayList<>();
+        int countOfDivider = 0;
+        int sum = 0;
+
+        if (inputList.get(1) == '/' && inputList.get(3) == '\\' && inputList.get(4) == 'n') {
+            if (Character.isDigit(inputList.get(2))
+                    || inputList.get(2).equals(':')
+                    || inputList.get(2).equals(',')) {
+                System.out.println("구분자에 콤마, 콜론, 숫자를 사용한 경우");
+                throw new IllegalArgumentException();
+            } else if (inputList.size() == 5) {
+                System.out.println("//[커스텀 구분자]\\n만 입력한 경우");
+                return sum;
+            }
+
+            dividerList.add(inputList.get(2));
+            System.out.println("커스텀 구분자 : "+inputList.get(2));
+            inputList = inputList.subList(5, inputList.size());
+
+            sum = processNumbers(inputList, dividerList, tmpNum, countOfDivider);
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+        return sum;
+    }
+    private static int handleDefaultDelimiters(List<Character> inputList) {
+        List<Character> dividerList = Arrays.asList(':', ',');
+        List<Integer> tmpNum = new ArrayList<>();
+        int countOfDivider = 0;
+
+        return processNumbers(inputList, dividerList, tmpNum, countOfDivider);
+    }
+
+    private static int processNumbers(List<Character> inputList, List<Character> dividerList,
+                                      List<Integer> tmpNum, int countOfDivider) {
+        // TODO
+        System.out.println("아직 processNumbers 기능이 구현되지 않았습니다. 임시결과로 999가 출력됩니다.");
+        return 999;
     }
 }
