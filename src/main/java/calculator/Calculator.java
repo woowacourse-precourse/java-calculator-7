767880a2105d396f339a10ed2341755baf0fe0b8
songjunhyen <songjunhyen@gmail.com>
2024-10-20 15:36:22 +0900

    Feat : 문자열 분리 Fix : 커스텀 구분자 이후 문자열 index 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,7 +5,9 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Calculator {
     public void calculate() {
         String input = readLine();
-        String delimiter = InputHandler.parse(input);
-        System.out.println("구분자 : " + delimiter);
+        String[] parsedInput = InputHandler.parse(input);
+        for (String result : parsedInput) {
+            System.out.println("결과 : " + result);
+        }
     }
 }
--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -5,11 +5,11 @@ public class InputHandler {
     private static final String CUSTOM_DELIMITER_PREFIX = "//"; // 커스텀 구분자 접두사
     private static final String CUSTOM_DELIMITER_END = "\\n"; // 커스텀 구분자 종료 문자
 
-    public static String parse(String input) {
+    public static String[] parse(String input) {
         return parseinput(input);
     }
 
-    private static String parseinput(String input) {
+    private static String[] parseinput(String input) {
         String delimiter = DEFAULT_DELIMITERS;
         if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
             int delimiterIndex = input.indexOf(CUSTOM_DELIMITER_END); // 실제 줄바꿈 인식
@@ -17,16 +17,14 @@ public class InputHandler {
                 String customDelimiter = input.substring(2, delimiterIndex).trim();
                 customDelimiter = escapeMeta(customDelimiter);
                 delimiter = customDelimiter + "|" + delimiter;
-
-                String numbers = input.substring(delimiterIndex + 1).trim();
-                System.out.println("숫자 부분: " + numbers);
+                String numbers = input.substring(delimiterIndex + 2).trim();
+                return splitinput(numbers, delimiter);
             } else {
                 throw new IllegalArgumentException("커스텀 구분자 지정 종료가 존재하지 않습니다: " + input);
             }
         }
-        return delimiter;
+        return input.split(delimiter);
     }
-
 
     private static String escapeMeta(String delimiter) {
         StringBuilder escaped = new StringBuilder();
@@ -39,4 +37,10 @@ public class InputHandler {
         }
         return escaped.toString();
     }
+
+    private static String[] splitinput(String input, String delimiter) {
+        String[] tokens = input.split(delimiter);
+        return tokens;
+    }
+
 }
