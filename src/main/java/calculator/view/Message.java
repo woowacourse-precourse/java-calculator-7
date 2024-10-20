1f9fa3031c17c5add2d2901c06475bfc7aa521b7
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 02:30:51 +0900

    feat(Message): null 입력 예외 메시지 추가

--- src/main/java/calculator/view/Message.java
+++ src/main/java/calculator/view/Message.java
@@ -2,7 +2,8 @@ package calculator.view;
 
 public enum Message {
 
-    INSTRUCTION_MESSAGE("덧셈할 문자열을 입력해 주세요.");
+    INSTRUCTION_MESSAGE("덧셈할 문자열을 입력해 주세요."),
+    NO_INPUT("입력이 없습니다.");
 
     private final String message;
 
