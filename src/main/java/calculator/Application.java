b4f43c601eca6eb3a2f349dfa2d87fffc477fb6f
Choi Min Young <codingmy@naver.com>
2024-10-20 22:27:32 +0900

    feat: 문자열 입력 기능 구현
    BufferedReader를 사용한 입력 구현
    IOException 발생처리를 if catch로 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import java.io.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String inputString = getInputString();
+    }
+
+    private static String getInputString() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String input = br.readLine();
+            return input;
+        } catch (IOException e) {
+            throw new IllegalArgumentException("잘못된 입력값이 입력되었습니다.");
+        }
     }
 }
