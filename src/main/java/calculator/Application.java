5426dad974b93477fe880351adf3d900d377f2dc
ham8962 <zzing199842@gmial.com>
2024-10-20 20:12:51 +0900

    1-1. 입력 시에 \n 을 하나의 문자로 인식되게끔 하는 법 확인

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,11 +7,23 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        /*
         System.out.println("입력값 : " + input);
         if (input.contains("\n")) {
             System.out.println("개행문자가 포함되어 있습니다.");
         } else {
             System.out.println("개행문자가 포함되어 있지 않습니다.");
+        }*/
+
+        input = input.replace("\\n", "\n");
+        if (input.contains("\n")) {
+            System.out.println("개행문자가 포함되어 있습니다.");
+            System.out.println("결과 : " + input);
+            System.out.println(input.indexOf("\n"));
+        } else {
+            System.out.println("개행문자가 포함되어 있지 않습니다.");
         }
+
+
     }
 }
