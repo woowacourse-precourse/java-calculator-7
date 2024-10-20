c314c07b4478ce27de7f4749af56e2914f30f876
yooo <ghddbtl@gmail.com>
2024-10-20 23:41:33 +0900

    <test> 메서드
    수정된 메서드 테스트 실행

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,27 +1,37 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculatorTest extends RegexCheckTest {
+public class CalculatorTest {
+  Calculator calculator = new Calculator("//`\\n1`2`3");
 
+  @BeforeEach
+  public void setUp() {
+    calculator.inputValidator();
+  }
 
   @Test
   public void allPositiveNumbers() {
+    assertInstanceOf(Calculator.class, calculator.allPositiveNumber());
+
+    assertArrayEquals(new int[]{1,2,3},calculator.allPositiveNumber().getNumbers());
+
     assertThrows(IllegalArgumentException.class, () -> {
       calculator = new Calculator("//`\\n-1`2`3");
-      calculator.matchesAndInsert();
+      calculator.inputValidator();
       calculator.allPositiveNumber();
     });
+
   }
 
 
   @Test
   public void addTest() {
-    int result = calculator.add(new int[]{1, 2, 3});
-    assertEquals(6, result);
-
+    assertEquals(6, Calculator.add(new int[]{1, 2, 3}));
   }
 
 }
