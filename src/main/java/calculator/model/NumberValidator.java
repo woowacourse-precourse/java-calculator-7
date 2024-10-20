a4d869ebb4f061496237d186342de504860770b5
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:36:21 +0900

    :sparkles: feat(NumberValidator): 잘못된 입력인 경우, 예외를 발생시킨다

--- src/main/java/calculator/model/NumberValidator.java
+++ src/main/java/calculator/model/NumberValidator.java
@@ -5,10 +5,14 @@ import calculator.exception.InvalidInputException;
 public class NumberValidator {
 
     public int validateAndParse(String number) {
-        int result = Integer.parseInt(number);
-        if (result < 0) {
-            throw new InvalidInputException("음수는 허용되지 않습니다: " + number);
+        try {
+            int result = Integer.parseInt(number);
+            if (result < 0) {
+                throw new InvalidInputException("음수는 허용되지 않습니다: " + number);
+            }
+            return result;
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException("유효한 숫자가 아닙니다: " + number, e);
         }
-        return result;
     }
 }
