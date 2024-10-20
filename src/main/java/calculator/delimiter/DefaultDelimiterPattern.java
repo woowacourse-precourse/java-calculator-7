f7ecefa87ac480b7ac2a99be1b4213e2fe2ce7b1
junest66 <chlwndks333@gmail.com>
2024-10-20 15:38:01 +0900

    refactor(DefaultDelimiterPattern): 기본구분자 패턴 정규식 수정
    - 숫자 갯수 제한 추가
    - 숫자 최대 범위 1000까지 제한 추가

--- src/main/java/calculator/delimiter/DefaultDelimiterPattern.java
+++ src/main/java/calculator/delimiter/DefaultDelimiterPattern.java
@@ -3,7 +3,7 @@ package calculator.delimiter;
 import java.util.regex.Pattern;
 
 public class DefaultDelimiterPattern implements DelimiterPattern {
-    private static final String REGEX = "^([1-9]\\d*|$)([,:]([1-9]\\d*))*$";
+    private static final String REGEX = "^(([1-9]\\d{0,2}|1000)|$)([,:]([1-9]\\d{0,2}|1000)){0,49}$";
 
     @Override
     public boolean validate(String input) {
