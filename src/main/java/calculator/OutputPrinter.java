52025317d1b115105fafee4cc897356a4eef943c
문고은 <>
2024-10-20 23:35:31 +0900

    fix(output) : 결과 출력 메시지 띄어쓰기 수정

--- src/main/java/calculator/OutputPrinter.java
+++ src/main/java/calculator/OutputPrinter.java
@@ -6,6 +6,6 @@ public class OutputPrinter {
     }
 
     public void printResult(int result) {
-        System.out.println("결과: " + result);
+        System.out.println("결과 : " + result);
     }
 }
