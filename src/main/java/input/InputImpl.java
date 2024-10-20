18206b544cb57299c31391d9e331d069d901bd71
agi1004 <agi1009@naver.com>
2024-10-20 17:49:21 +0900

    feat: 문자열을 입력받고 결과를 전달하는 인터페이스의 구현체 생성
    클라이언트와 직접적으로 맞닿아있는 인터페이스의 구현체
    의존하는 인터페이스: Validate, Choose
    해당 구현체가 의존하는 인터페이스들의 의존성을 InputFactory 클래스의 정적 팩터리 메서드에서 해당 구현체의 객체를 생성할 때 생성자의 매개변수로 전달하여 주입받음
    1. 문자열을 입력받고 Validate 인터페이스로 유효성을 검사
    2. Choose 인터페이스에서 기본 구분자와 커스텀 구분자 중에 어떤 것을 사용할 지 전략(Divide 인터페이스)을 고른 다음, 전략을 리턴받음
    3. 리턴받은 Divide 인터페이스로 숫자들을 분리하여 숫자 배열을 리턴받음
    4. 숫자 배열의 합을 클라이언트 코드로 리턴

--- src/main/java/input/InputImpl.java
+++ src/main/java/input/InputImpl.java
@@ -1,42 +1,39 @@
 package input;
 
+import java.util.Arrays;
+
 import camp.nextstep.edu.missionutils.Console;
 import choose.Choose;
-import choose.ChooseImpl;
 import divide.Divide;
+import validate.Validate;
 
 public class InputImpl implements Input {
-	private Choose choose = new ChooseImpl();
-	private Divide divide;
+	private final Validate validate;
+	private final Choose choose;
+	
+	// 의존성 주입 (DI)
+	public InputImpl(Validate validate, Choose choose) {
+		this.validate = validate;
+		this.choose = choose;
+	}
 
 	@Override
-	public int input() {
+	public int inputStr() {
+		// 문자열 입력 받기
 		String str = Console.readLine();
 		
-		if (!checkValidation(str)) {
+		// 문자열 유효성 검사
+		if (!validate.isValid(str)) {
 			throw new IllegalArgumentException("올바른 값을 입력해주세요.");
 		}
 		
-		divide = choose.choose(str);
-		divide.divide(str);
+		// 기본 구분자와 커스텀 구분자 중 무엇을 사용하는지 구별
+		Divide divide = choose.chooseStrategy(str);
 		
-		return 1;	// 답 리턴
-	}
-	
-	private boolean checkValidation(String str) {
-		if (str.equals("")) {
-			return true;
-		}
+		// 위에서 구별한 기준으로 숫자들을 분리하여 리턴
+		int[] numbers = divide.divideNumbers(str);
 		
-		if (str.contains("-")) {
-			return false;
-		}
-		
-		if (!str.contains(",") && !str.contains(":") 
-			&& !str.contains("//") && !str.contains("\n")) {
-			return false;
-		}
-		
-		return true;
+		// 모든 숫자들의 합 리턴
+		return Arrays.stream(numbers).sum();
 	}
 }
