8e70da6c9ae8e96182f261b2a02dbec7d4b0b811
kjyyjk <rlawnsdud920@gmail.com>
2024-10-20 17:21:45 +0900

    test: CustomDelimiterTest에 테스트 케이스 추가
    - ParameterizedTest 사용
    - 생성 성공 테스트 시 영문자, 공백문자, 숫자문자, 특수문자 테스트
    - 생성 실패 테스트 시 빈문자, 연속 공백문자, 연속 숫자문자, 연속 특수문자 테스트

--- src/test/java/calculator/domain/CustomDelimiterTest.java
+++ src/test/java/calculator/domain/CustomDelimiterTest.java
@@ -1,31 +1,34 @@
 package calculator.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 class CustomDelimiterTest {
 
-    @Test
+    @ParameterizedTest
     @DisplayName("CustomDelimiter는_길이_1의_문자열을_입력받아_생성될_수_있다")
-    public void create() {
+    @ValueSource(strings = {"t", " ", "1", "-"})
+    public void create(String input) {
         //given
         //when
-        CustomDelimiter result = new CustomDelimiter("t");
+        CustomDelimiter result = new CustomDelimiter(input);
 
         //then
         assertThat(result.getValue())
-                .isEqualTo("t");
+                .isEqualTo(input);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("CustomDelimiter는_길이_1이_아닌_문자열을_입력받아_생성되면_예외를_던진다")
-    public void LengthException() {
+    @ValueSource(strings = {"", "  ", "test", "22", "--"})
+    public void LengthException(String input) {
         //given
         //when
         //then
-        assertThatThrownBy(() -> new CustomDelimiter("test"))
+        assertThatThrownBy(() -> new CustomDelimiter(input))
                 .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("구분자는 길이 1의 문자여야합니다");
     }
