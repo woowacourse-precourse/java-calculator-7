d11c448509798ef435a7ab142d9c065a0298a81c
bogyeom0922 <pbk0398@gmail.com>
2024-10-20 17:05:06 +0900

    feat: 숫자 외 값 포함 시 검증 로직 추가 및 빈 문자열 처리
    - 숫자 외 값이 포함된 경우 유효하지 않은 숫자 예외 발생

--- src/main/java/calculator/domain/Validator.java
+++ src/main/java/calculator/domain/Validator.java
@@ -2,11 +2,21 @@ package calculator.domain;
 
 public class Validator {
     public int[] validateAndConvert(String[] numbers) {
+        if(numbers.length == 0) {
+            throw new IllegalArgumentException("입력된 숫자가 존재하지 않습니다.");
+        }
+
         int[] numbersArray = new int[numbers.length];
+
         for (int i = 0; i < numbers.length; i++) {
-            numbersArray[i] = Integer.parseInt(numbers[i]);
-            if (numbersArray[i] < 0) {
-                throw new IllegalArgumentException(numbersArray[i] + "는 음수 값입니다.");
+            try {
+                numbersArray[i] = Integer.parseInt(numbers[i]);
+
+                if (numbersArray[i] < 0) {
+                    throw new IllegalArgumentException(numbersArray[i] + "는 음수 값입니다.");
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(numbers[i]+ "는 유효한 숫자가 아닙니다.");
             }
         }
         return numbersArray;
