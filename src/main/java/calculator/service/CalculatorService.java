037eb2e5f715b763a80b37d83d27d507f774ad08
Kyuwon-Choi <kyuwon30@naver.com>
2024-10-20 17:56:36 +0900

    feat: 예외 처리 추가

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,7 +1,6 @@
 package calculator.service;
 
 import calculator.model.Operand;
-import calculator.util.OperandConverter;
 import calculator.util.StringParser;
 
 import java.util.List;
@@ -11,8 +10,7 @@ public class CalculatorService {
     public int calculateSum(String input) {
 
         List<String> parsedStrings = StringParser.parseInput(input);
-        List<Operand> operands = OperandConverter.convertToOperands(parsedStrings);
-
+        List<Operand> operands = Operand.fromStringList(parsedStrings);
         return operands.stream().mapToInt(Operand::number).sum();
 
     }
