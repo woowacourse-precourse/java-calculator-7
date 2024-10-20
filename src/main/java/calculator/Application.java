1390ea39ef63484fdd360376f026fe82b5b79173
Mini <mini@MiniMac.local>
2024-10-20 18:40:26 +0900

    feat: return 0 when input is empty or null

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,6 +10,12 @@ public class Application {
         System.out.println("구분자와 양수 구성된 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
+        //빈 문자열 또는 null이 입력된 경우 0을 반환
+        if(input == null || input.equals("")) {
+            System.out.println("0");
+            return;
+        }
+
 
     }
 }
