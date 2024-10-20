06c663685570d14bf93b2b43bddc67d9af92cd0f
usingnameing <cnrrn0312@gmail.com>
2024-10-20 22:32:55 +0900

    style : change file structure

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,9 @@
 package calculator;
 
-import calculator.api.Calculation;
-import calculator.api.Validation;
-
-import javax.swing.plaf.SeparatorUI;
-
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
+    private static String separator= "[,:]";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String inputString = readLine();
@@ -20,16 +16,57 @@ public class Application {
                 throw new IllegalArgumentException();
             }
         }
-        try{
-            Validation validation = new Validation();
-            String separator = validation.validateString(inputString);
 
-            Calculation calculation = new Calculation();
-            Double result = calculation.calculationString(separator,inputString);
+        try{
+            separator = validateString(inputString);
+
+            Double result = calculationString(separator,inputString);
 
             System.out.println("결과 : " + result);
         }catch (IllegalStateException err){
             return;
         }
+    }
+
+    public static String validateString(String input){
+        try{
+            checkCustom(input);
+            return separator;
+        }catch(IllegalArgumentException e){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkCustom(String input){
+        if(input.substring(0,2).equals("//") && input.contains("\\n")){
+            int i = input.indexOf("\\");
+            separator = "[";
+            separator = separator + input.substring(2, i) + "]";
+        }else if(!input.substring(0,2).equals("//") && !input.contains("\\n")){
+            return;
+        }else{
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static Double calculationString(String separator, String input){
+
+        Double sum=0.0;
+
+        if(!separator.equals("[,:]")){
+            int i = input.indexOf("\\");
+            input = input.substring(i+2);
+        }
+
+        String[] split = input.split(separator);
+
+        try{
+            for(String temp : split){
+                sum+=Double.parseDouble(temp);
+            }
+        } catch(NumberFormatException err){
+            throw new IllegalArgumentException();
+        }
+        return sum;
     }
 }
