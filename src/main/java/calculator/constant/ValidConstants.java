8d70133605533b2b86369f071fe7cf20eff30a83
장혜민 <gpals5987@gmail.com>
2024-10-20 22:15:44 +0900

    [fix] customSeparator범위 수정

--- src/main/java/calculator/constant/ValidConstants.java
+++ src/main/java/calculator/constant/ValidConstants.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class ValidConstants {
     public static final int MAX_TOTAL_LENGTH = 100;
-    public static final int MAX_SEPARATOR_LENGTH = 5;
+    public static final int MAX_SEPARATOR_LENGTH = 2;
     public static final int MAX_NUMBER_COUNT = 100;
 
     public static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("^//(.*)\\\\n(.*)");
