8864e97b09138ec0752e1d5caad7b541faa0c960
dfb5361@gmail.com <dfb5361@gmail.com>
2024-10-21 00:06:26 +0900

    feat(Application): add delimiter and number arrays for string addition calculator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ArrayList<Character> delimiters = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        delimiters.add(',');
+        delimiters.add(':');
     }
 }
