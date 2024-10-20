7c079da0003a8276ea7cc24fcb1795d0524843a4
mirolis6636 <mirolis6636@gmail.com>
2024-10-21 02:00:07 +0900

    feat: add readUserInput method

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class Calculator {
+    private String userInput;
+
+    public void readUserInput(){
+        userInput = camp.nextstep.edu.missionutils.Console.readLine();
+    }
+}
