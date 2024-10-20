c2587a0e4cb8c9222fb576d7b1e297ccad22aa4f
seoys0910 <seoys0910@naver.com>
2024-10-20 16:45:33 +0900

    feat : 입력값에 대한 검증 로직 추가
    사용자에게 빈 문자열을 입력받거나, 문자열을 숫자로 변환할 때 숫자가 아닌 값이나 음수가 있는지 검증하는 로직을 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,9 +3,25 @@ package calculator;
 import java.util.List;
 
 public class Calculator {
-    private int convertAndSum(List<String> userInput) {
+    public static int numberSum(List<String> userInput) {
         return userInput.stream()
+                .peek(Calculator::validateNumber)
                 .mapToInt(Integer::parseInt)
                 .sum();
+    }
+
+    private static void validateNumber(String num) {
+        try {
+            int number = Integer.parseInt(num);
+            validateNegative(number);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("잘못된 값이 입력되었습니다.");
+        }
+    }
+
+    private static void validateNegative(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("양수를 입력해주세요.");
+        }
     }
 }
--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -4,6 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static String readInputString() {
-        return Console.readLine();
+        String userInput = Console.readLine();
+        return validateEmpty(userInput);
+    }
+
+    private static String validateEmpty(String userInput) {
+        if (userInput.isEmpty()) {
+            return "0";
+        }
+        return userInput;
     }
 }
+
+
