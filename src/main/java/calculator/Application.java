dae38c285a8f50b56506b5d05cf8b3d7306814be
Bell-isHere <quf417@gmail.com>
2024-10-20 19:20:40 +0900

    feat(application): 커스텀 구분자 처리 기능 구현
    - 입력 문자열에서 커스텀 구분자 확인 및 추출
    - 커스텀 구분자로 숫자 분리
    - 커스텀 구분자와 기본 구분자 , 와 : 동시 사용
    - 커스텀 구분자가 비어있거나 덜 완성된 경우 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,20 +1,44 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
         try {
-        System.out.println("덧셈할 문자열을 입력해 주세요.:");
-        String inputString = Console.readLine();
-        String[] tokens = inputString.split("[,:]");
+            System.out.println("덧셈할 문자열을 입력해 주세요.:");
+            String inputString = Console.readLine();
 
-        System.out.println("분리된 값들:");
-        for (String token : tokens) {
-            System.out.println(token);
-        }
+            String delimiter = "[,|:]";
+            if (inputString.startsWith("//")) {
+                // 개행 문자 \n을 \\n로 찾을 수 있도록 함
+                int delimiterEndIndex = inputString.indexOf("\\n");
+                System.out.println(delimiterEndIndex);
+                if (delimiterEndIndex == -1) {
+                    throw new IllegalArgumentException("커스텀 구분자가 미완성되었습니다.");
+                }
+                String customDelimiter = inputString.substring(2, delimiterEndIndex);
+                System.out.println(customDelimiter);
+                if (customDelimiter.isEmpty()) {
+                    throw new IllegalArgumentException("커스텀 구분자를 입력해주세요.");
+                }
+                // 커스텀 구분자와 기존 구분자를 함께 사용하도록 함
+                delimiter = Pattern.quote(customDelimiter) + "|,|:";
+                inputString = inputString.substring(delimiterEndIndex + 2);
+
+                // 디버깅용 출력
+                System.out.println("커스텀 구분자: " + customDelimiter);
+            }
+
+            String[] tokens = inputString.split(delimiter);
+
+            System.out.println("분리된 값들:");
+            for (String token : tokens) {
+                System.out.println(token);
+            }
 
             List<Integer> numbers = convertToIntegers(tokens);
 
