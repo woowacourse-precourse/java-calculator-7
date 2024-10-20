7977c7a76fe2f84a42cc300f712e2e7c0b71543a
goochul <gooch123@naver.com>
2024-10-20 17:21:11 +0900

    fix(SplitNumber): 예외 상황 추가
    음수가 들어오면 예외 발생

--- src/main/java/calculator/feature/SplitNumber.java
+++ src/main/java/calculator/feature/SplitNumber.java
@@ -30,7 +30,11 @@ public class SplitNumber {
 
         try {
             for (int i = 0; i < split.length; i++) {
-                result[i] = Integer.parseInt(split[i]); // 분리한 문자열을 정수 배열에 추가
+                int num = Integer.parseInt(split[i]); // 분리한 문자열을 정수 배열에 추가
+                if (num < 0)
+                    throw new NumberFormatException();
+                else
+                    result[i] = num;
             }
         } catch (NumberFormatException e) { // 숫자와 구분자 외에 다른 문자를 입력했을 경우 예외 발생
             throw new IllegalArgumentException();
