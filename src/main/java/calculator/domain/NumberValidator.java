d4385ba9b34bac2137d50f868a55778985d06381
JoonKyo <joonkyolee@gmail.com>
2024-10-21 02:26:09 +0900

    refactor(NumberValidator): 숫자로 변환하는 메서드 삭제

--- src/main/java/calculator/domain/NumberValidator.java
+++ src/main/java/calculator/domain/NumberValidator.java
@@ -1,23 +1,12 @@
 package calculator.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class NumberValidator {
     private static final String ERROR_MESSAGE_FOR_NEGATIVE_NUMBER = "음수를 입력하셨습니다. 프로그램을 종료합니다.";
     private static final String ERROR_MESSAGE_FOR_ZERO = "0을 입력하셨습니다. 프로그램을 종료합니다.";
 
-    public static List<Integer> convertNumbers(String[] separatedString) {
-        List<Integer> numbers = new ArrayList<>();
-
-        for (String number : separatedString) {
-            numbers.add(Integer.parseInt(number));
-        }
-        validateNumber(numbers);
-        return numbers;
-    }
-
-    private static void validateNumber(List<Integer> numbers) {
+    public static void validateNumber(List<Integer> numbers) {
         if (hasNegativeNumber(numbers)) {
             throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NEGATIVE_NUMBER);
         }
