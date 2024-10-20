a08180911475736f6a8541a5adf56ca72085752e
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-20 16:01:11 +0900

    feat(StringValidator): 커스텀 구분자로 이스케이프 문자 등록 제한

--- src/main/java/calculator/validator/StringValidator.java
+++ src/main/java/calculator/validator/StringValidator.java
@@ -4,7 +4,7 @@ import calculator.global.exception.ErrorMessage;
 
 public class StringValidator {
 
-    private static final String DELIMITER_FORMAT_REGEX = "^[0-9-]*$";
+    private static final String DELIMITER_FORMAT_REGEX = "^[0-9-\\\\]*$";
 
     public static void validateCustomDelimiterFormat(String str) {
         if (str.matches(DELIMITER_FORMAT_REGEX)) {
