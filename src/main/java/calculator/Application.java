1027e6087031ee70609dde887d40ec1d88dbde18
hyeblee <hyeb1224@naver.com>
2024-10-21 08:28:28 +0900

    문자열 더하는 메서드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,8 +12,13 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-
+        StringCalculator calculator = new StringCalculator();
         int result = 0;
-        System.out.println("결과 : " + result);
+        try{
+            result = calculator.stringCalculate(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("error: "+e.getMessage());
+        }
     }
 }
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,16 +1,56 @@
 package calculator;
 
 public class StringCalculator {
+
     public String[] splitString(String input, String delimiter) {
         return input.split(delimiter);
     }
 
-    public int stringtToInt(String number){
+    public int stringToInt(String number){
+        if(number.isEmpty()) return 0;
         try{
             return Integer.parseInt(number);
         } catch (NumberFormatException e){
-            throw new NumberFormatException("올바르지 않은 숫자 형식입니다.");
+            throw new IllegalArgumentException("숫자가 아닌 문자가 있습니다.");
         }
+    }
+
+    public int addString(String[] numbers){
+        int sum=0;
+        for(String number:numbers){
+
+            int num = stringToInt(number);
+            if(num < 0)
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+            sum+=num;
+        }
+        return sum;
+    }
+
+    public int stringCalculate(String input){
+        if(input.isEmpty())
+            return 0;
+
+        String delimiter = ",|:";
+        String specialChars = ".^$*+?()[]{}\\|/";
+
+        if(input.startsWith("//")){
+            if(input.charAt(3)=='\\'&&input.charAt(4)=='n'){
+                String customDelimiter = String.valueOf(input.charAt(2));
+                if(specialChars.indexOf(customDelimiter) != -1)
+                    delimiter+="|\\"+customDelimiter;
+                else
+                    delimiter+="|"+customDelimiter;
+                input = input.substring(5);
+            } else{
+                throw new IllegalArgumentException("커스텀 구분자 형식이 유효하지 않습니다.");
+            }
+        }
+        System.out.println("delimiter: "+delimiter);
+        String[] numbers = splitString(input, delimiter);
+        int sum = addString(numbers);
+
+        return sum;
     }
 
 }
