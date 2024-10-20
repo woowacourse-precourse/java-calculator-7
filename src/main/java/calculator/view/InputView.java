9c3155e59f1478bf04e6cdee145809bd9fe6618c
dongyun <ehddbs452100@naver.com>
2024-10-21 00:18:22 +0900

    feat(InputView): validate input and throw error if input were wrong
    - 사용자의 입력값을 숫자만 입력했는지, 숫자만 입력했더라고 양수만 입력했는지 검증하는 로직
    - 정규식을 바탕으로 input을 parsing을 함
    - 1단계에선 CustomSeperator클래스에서 명시한 커스텀 구분자를 input에서 삭제를 함
    - 2단계에선 ",",":"을 이용하여 input에서 parsing을 하고 List로 변환하여 문자가 있는지 확인하고 있으면 오류
    - 3단계에선 숫자들이 양수인지 확인하고 아니면 오류
    - 최종적으로 String타입의 숫자들을 Double타입으로 변경하여 전달

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,15 +1,90 @@
 package calculator.view;
 
 import static calculator.global.Message.INPUT_MESSAGE;
+import static calculator.global.Seperator.COLON;
+import static calculator.global.Seperator.COMMA;
+import static calculator.global.Seperator.CUSTOM;
+import static calculator.global.error.ErrorCode.NOT_POSITIVE_ERROR_MESSAGE;
+import static calculator.global.error.ErrorCode.NUMERIC_ERROR_MESSAGE;
 
+import calculator.global.error.NumericError;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
+
+    private static final String DOUBLE_SLASH = "\\Q//";
+    private static final String NEW_LINE = "\\n";
+    private static final String DOUBLE_REGEX = "^-?\\d+(\\.\\d+)?$";
+    private static final String REGEX_QUOTE_START = "\\Q";
+    private static final String REGEX_QUOTE_END = "\\E";
 
     public static String receiveInput() {
         System.out.println(INPUT_MESSAGE.getMessage());
         return Console.readLine();
     }
 
-}
+    public static List<Double> getNumber(String input) {
 
+        List<String> numbers = splitInput(removeUnUsedSentence(input));
+        validate(numbers);
+        return typeConvert(numbers);
+    }
+
+    public static List<String> splitInput(List<String> input) {
+
+        return input.stream()
+                .flatMap(str -> Arrays.stream(str.split(getRegex())))
+                .filter(s -> !s.isEmpty())
+                .collect(Collectors.toList());
+    }
+
+    private static String getRegex() {
+        return COMMA.getSeperator() + "|"
+                + COLON.getSeperator() + "|"
+                + REGEX_QUOTE_START
+                + CUSTOM.getSeperator()
+                + REGEX_QUOTE_END;
+    }
+
+    private static void validate(List<String> numbers) {
+        
+        validateNumber(numbers);
+        validatePositiveNumber(numbers);
+    }
+
+    public static void validatePositiveNumber(List<String> numbers) {
+
+        boolean validate = numbers.stream().allMatch(number -> (Double.parseDouble(number) > 0));
+
+        if (!validate) {
+            throw new NumericError(NOT_POSITIVE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateNumber(List<String> numbers) {
+
+        boolean validate = numbers.stream().allMatch(number -> number.matches(DOUBLE_REGEX));
+
+        if (!validate) {
+            throw new NumericError(NUMERIC_ERROR_MESSAGE);
+        }
+    }
+
+    private static List<Double> typeConvert(List<String> numbers) {
+        return numbers.stream()
+                .filter(number -> !number.isEmpty())
+                .map(number -> Double.parseDouble(number))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> removeUnUsedSentence(String input) {
+
+        return Arrays.asList(input.split(DOUBLE_SLASH + CUSTOM.getSeperator() + NEW_LINE))
+                .stream()
+                .filter(sentence -> !sentence.isEmpty())
+                .collect(Collectors.toList());
+    }
+}
