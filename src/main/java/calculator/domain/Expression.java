ea6c6d555acb83f44d6b9d1d6e7ee76842bee6e6
young-in <hardworkingini@gmail.com>
2024-10-21 00:59:39 +0900

    refactor : input으로 받은 문자열을 위한 domain 작성

---
+++ src/main/java/calculator/domain/Expression.java
@@ -0,0 +1,13 @@
+package calculator.domain;
+
+public class Expression {
+    private String expression;
+
+    public Expression(String expression) {
+        this.expression = expression;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+}
