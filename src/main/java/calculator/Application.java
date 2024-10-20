7a163bb7fdddcbc1ac0129aad72e2dcc5d7cc16c
bronzehyeon <bronzehyeon@gmail.com>
2024-10-20 18:01:27 +0900

    doc : 구현주석 및 문서주석 추가
    구현 주석 및 문서주석 추가
    
    BREAKING CHANGE:

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,7 +2,14 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+/**
+ * java-calculator 메인 클래스입니다.
+ */
 public class Application {
+    /**
+     * 문자열을 입력받아 계산하여 정답을 출력합니다.
+     * @param args
+     */
     public static void main(String[] args) {
 
         System.out.println("덧셈할 문자열을 입력하세요.");
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,13 +3,26 @@ package calculator;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 문자열 덧셈 클래스입니다.
+ */
 public class Calculator {
 
+    /**
+     * 문자열에서 숫자를 추출하여 더하여 반환합니다.
+     * 문자열에서 구분자를 기준으로 숫자를 추출하여 더합니다.
+     * 기본 구문자로는 ',', ':'가 있니다.
+     * 문자열 맨 앞에 //와 \n 사이에 문자를 위치시켜 커스텀 구분자를 지정할 수 있습니다.
+     * @param str
+     * @return 숫자의 합
+     */
     public static int calculate(String str){
+        // 구분자 집합 생성 및 기본 구분자 추가
         Set<Character> delimiters = new HashSet<>();
         delimiters.add(',');
         delimiters.add(':');
 
+        // 문자열이 비어있는 경우 0 반환
         if (str.isEmpty()) {
             return 0;
         }
@@ -20,6 +33,7 @@ public class Calculator {
 
         int i = 0;
 
+        // 커스텀 구분자를 추출하고 커스텀 구분자 지정 구문이 잘못됐다면 오류를 발생시킵니다.
         if(str.startsWith("//")) {
             String custom = str.substring(0,5);
             if(custom.endsWith("\\n")) {
@@ -36,6 +50,7 @@ public class Calculator {
 
         int answer = 0;
 
+        // 구분자를 기준으로 숫자를 추출하여 더하고, 구분자와 양수 이외의 문자가 있다면 오류를 반환합니다.
         for (; i < str.length(); i++) {
             char c = str.charAt(i);
             if(Character.isDigit(c)) {
