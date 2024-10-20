b16b7a7934b5235a2e43f8e46c4f5ec90a26052b
jedo5000 <jsg998877@gmail.com>
2024-10-20 13:21:29 +0900

    feat: 계산의 시작과 끝이 숫자인지 판별하는 기능 구현
    - 문자열의 시작, 끝 원소가 숫자인지 확인하고, 아니면 에러

--- src/main/java/calculator/exception/ErrorCode.java
+++ src/main/java/calculator/exception/ErrorCode.java
@@ -3,8 +3,8 @@ package calculator.exception;
 public enum ErrorCode {
     // Invalid Argument Error
     INVALID_NUMBER("양수만 입력할 수 있습니다."),
-    INVALID_VALUE("숫자가 아닌 값이 포함되었습니다.");
-
+    INVALID_VALUE("숫자가 아닌 값이 포함되었습니다."),
+    INVALID_SEQUENCE("계산의 시작과 끝은 숫자여야 합니다.");
     private final String message;
 
     ErrorCode(String message) {
