4da7ee05c70432bb7e932e83ee223041f1855463
yunjeooong <yunjeongssu@gmail.com>
2024-10-20 23:33:20 +0900

    feat: 문자열 계산기 애플리케이션 완성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,26 @@
 package calculator;
 
+import calculator.service.StringCalculatorService;
+import calculator.validator.InputValidator;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputValidator inputValidator = new InputValidator();
+
+        StringCalculatorService calculatorService = new StringCalculatorService(inputValidator);
+
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
 
         String input = inputView.getInput();
+
+        int result = calculatorService.calculate(input);
+
+        outputView.printResult(result);
+
 
     }
 }
