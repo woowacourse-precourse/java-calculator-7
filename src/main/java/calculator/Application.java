116ebff6238e1592fa78c31a5ef79330e4cacf41
BusanGukbap <dong2857210@gmail.com>
2024-10-20 22:40:42 +0900

    feat(Application) : 입력 받기 추가
    사용자로부터 덧셈할 문자열을 입력받는 기능을 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,13 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 1. 문자열 입력 받기
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = scanner.nextLine();
+
+
+
     }
 }
