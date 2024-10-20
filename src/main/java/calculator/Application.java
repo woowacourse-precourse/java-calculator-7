516bed6d0914652c58e44a103449e15e2a2517d2
polybear <ldy092000@gmail.com>
2024-10-20 21:43:23 +0900

    feat(calculate) : update Application.java
    Application.java 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import calculator.domain.StringCalculator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringCalculator calculator = new StringCalculator();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        long result = calculator.add(input);
+        System.out.println("결과 : " + result);
+
+
     }
 }
