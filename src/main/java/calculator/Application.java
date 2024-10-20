712e6dfd59e8fb4fba75bec074236e4e15cbcd46
rdyjun <rdyjun00@gmail.com>
2024-10-20 23:18:18 +0900

    refactor: IOException 예외처리 방식 개선
    - 메소드 시그니처 -> try-catch로 개선

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,11 +11,11 @@ public class Application {
     private static final String SEPARATOR_PATTERN = "//(.+)\\\\n";
     private static final String DEFAULT_SEPARATOR = "[,:]";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String input = bufferedReader.readLine();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));;
+        String input = readInput(bufferedReader);
 
         boolean customSeparatorDefined = Pattern.compile(SEPARATOR_PATTERN)
                 .matcher(input)
@@ -27,7 +27,24 @@ public class Application {
         int result = Arrays.stream(numbers)
                 .sum();
 
-        System.out.println("결과 : " + result);
+        end(bufferedReader, result);
+    }
+
+    private static String readInput(BufferedReader bufferedReader) {
+        try {
+            return bufferedReader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException("입력을 받는 중 오류가 발생했습니다.", e);
+        }
+    }
+
+    private static void end(BufferedReader bufferedReader, int result) {
+        try {
+            System.out.println("결과 : " + result);
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException("BufferedReader를 종료하던 도중 오류가 발생했습니다.", e);
+        }
     }
 
     private static int[] getNumbers(String input, String separator) {
