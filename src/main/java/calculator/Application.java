6e8e04cf4f8027537274a67241413eea4e83e898
minyeongg <1030pmy@gmail.com>
2024-10-20 17:17:00 +0900

    feat : 커스텀 구분자 형식 판별하는 기능
    커스텀 구분자 형식이 올바르지 않은 경우
    IllegalArgumentException을 발생시킨 후 애플리케이션은 종료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,11 +8,17 @@ public class Application {
         String delimiter = "";
         String[] numbers;
         int sum = 0;
-        if(input == null || input.isEmpty() || input.isBlank()) {
+        if (input == null || input.isEmpty() || input.isBlank()) {
             return 0;
-        } else {
-            return sum;
+        } else if (input.startsWith("//")) {
+            String[] s = input.split("");
+            if((s[3].equals("\\")) && (s[4].equals("n"))) {
+
+            } else {
+                throw new IllegalArgumentException("잘못된 구분자 형식입니다.");
+            }
         }
+        return sum;
     }
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
