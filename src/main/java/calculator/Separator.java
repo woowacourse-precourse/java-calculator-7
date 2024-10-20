5cf039fb84ad8f6b21678267ba7806a1de17661a
seophohoho <seop0937@gmail.com>
2024-10-20 19:36:40 +0900

    feat(Separator): 입력 값에서 커스텀 구분자 관련 삭제 메소드 추가 및 커스텀 구분자 존재 유무 메소드 추가

--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -12,7 +12,25 @@ public class Separator {
 
     public String[] separate(String input, String separator) {
         String regex = separator;
+
+        if (isContainsCustomSeparatorSymbol(input)) {
+            input = deleteCustomSeparatorConvention(input);
+        }
+
         return input.split(regex);
+    }
+
+    public boolean isContainsCustomSeparatorSymbol(String input) {
+        return input.startsWith(Constants.CUSTOM_SEPARATOR_PREFIX) && input.contains(Constants.CUSTOM_SEPARATOR_SUFFIX);
+    }
+
+    public String deleteCustomSeparatorConvention(String input) {
+        StringBuilder sb = new StringBuilder(input);
+        int prefixIdx = input.indexOf(Constants.CUSTOM_SEPARATOR_PREFIX);
+        int suffixIdx = input.lastIndexOf(Constants.CUSTOM_SEPARATOR_SUFFIX);
+
+        sb.delete(prefixIdx, suffixIdx + 2);
+        return sb.toString();
     }
 
     public String addCustomSeparator(String regex, String customSeparator) {
@@ -21,13 +39,15 @@ public class Separator {
 
     public String getSeparator(String input) {
         String regex = SEPARATOR;
-        Matcher match = Pattern.compile(Constants.REGEX_CUSTOM_SEPARATOR_PATTERN).matcher(input);
 
-        while (match.find()) {
-            String customSeparator = match.group(1);
-            regex = addCustomSeparator(regex, customSeparator);
+        if (isContainsCustomSeparatorSymbol(input)) {
+            Matcher match = Pattern.compile(Constants.REGEX_CUSTOM_SEPARATOR_PATTERN).matcher(input);
+
+            while (match.find()) {
+                String customSeparator = match.group(1);
+                regex = addCustomSeparator(regex, customSeparator);
+            }
         }
-
         return regex;
     }
 }
