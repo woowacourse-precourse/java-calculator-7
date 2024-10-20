75d552ed2377d1353b54f0a3cd79fe8fa62c033e
btongtong <leeym2615@naver.com>
2024-10-20 15:35:16 +0900

    feat(signanddivider): add SignAndDivider class
    Add startSign and endSign for custom divider creation
    Add default divider

---
+++ src/main/java/calculator/SignAndDivider.java
@@ -0,0 +1,8 @@
+package calculator;
+
+class SignAndDivider {
+    public static final String startSign = "//";
+    public static final String endSign = "\n";
+    public static final String defaultDivider1 = ":";
+    public static final String defaultDivider2 = ";";
+}
