2b4d75ed6582dcd7a3b3702544269b7e9d49f2ee
parkseongyeol <ert2154@naver.com>
2024-10-20 15:08:22 +0900

    test($ApplicationTest): 정상 계산 테스트 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,6 +9,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void 정상_계산_1() {
+        assertSimpleTest(() -> {
+            run("1,1,1");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 정상_계산_2() {
+        assertSimpleTest(() -> {
+            run("1:1:1");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
