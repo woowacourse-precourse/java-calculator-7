496e1218d4a01c62f9e197f7cee6ad62b6b30e0d
lkdcode <dlrlejr1@naver.com>
2024-10-20 20:31:26 +0900

    Feat($App) main applciation

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import calculator.adapter.Console;
+import calculator.adapter.Convertor;
+import calculator.adapter.MessageInputAdapter;
+import calculator.adapter.MessageOutputAdapter;
+import calculator.application.DelimiterParser;
+import calculator.application.DelimiterValidator;
+import calculator.domain.Plus;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final var console = new Console(new MessageInputAdapter(), new MessageOutputAdapter());
+
+        final var convertor = new Convertor();
+        final var delimiterValidator = new DelimiterValidator();
+        final var delimiterParser = new DelimiterParser(delimiterValidator);
+
+        final var plus = new Plus();
+
+        var inputString = console.readString();
+
+        final var delimiterList = delimiterParser.parseToDelimiterList(inputString);
+        final var parsed = delimiterParser.getParsed();
+        final var numberList = convertor.convertToNumberList(parsed, delimiterList);
+
+        final var result = plus.execute(numberList);
+
+        console.printResult(result);
     }
 }
