9f591a588c1cb18de435f8cffdf50e9e14d67517
Revi1337 <david122123@gmail.com>
2024-10-20 17:03:33 +0900

    style: 불필요한 개행 제거

--- src/main/java/calculator/error/ErrorMessage.java
+++ src/main/java/calculator/error/ErrorMessage.java
@@ -1,7 +1,6 @@
 package calculator.error;
 
 public enum ErrorMessage {
-
     UNSUPPORTED_CUSTOM_DELIMITER_NUMBER("커스텀 구분자는 숫자일 수 없습니다."),
     UNSUPPORTED_NUMBER_EXCLUDE_POSITIVE("구분자를 제외한 문자에서 음수, 0 은 허용되지 않습니다."),
     UNSUPPORTED_CHAR_EXCLUDE_DELIMITER("구분자와 동일하지 않은 문자는 허용되지 않습니다."),
--- src/main/java/calculator/model/Tokens.java
+++ src/main/java/calculator/model/Tokens.java
@@ -18,5 +18,4 @@ public class Tokens {
                 .mapToInt(Token::getCost)
                 .sum();
     }
-
 }
