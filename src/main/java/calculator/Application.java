8254e5f12ceacc4b91c42a87a155c10a09f4bf10
yoona <103414935+yoonali@users.noreply.github.com>
2024-10-21 00:32:58 +0900

    feat : Add calculateNumber()
    문자열 덧셈 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+}
+
+class Calculator {
+    private static int calculateNumber(String input, String separator) {
+        int sum = 0;
+
+        String [] numbers = input.split(separator);
+
+        for (String number : numbers) {
+            int value = Integer.parseInt(number.trim());
+
+            sum += value;
+        }
+
+        return sum;
     }
 }
