c5c0cd9d66712bbd2359d16f69b16f01ecfc408e
Kim-dong-kyun <kdk942@gmail.com>
2024-10-20 16:29:45 +0900

    feat: 빈 문자열 여부 확인

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력하세요.");
+        String input = Console.readLine();
     }
+
+    public static boolean isBlank(String str) {
+        return str == null || str.trim().isBlank();
+    }
+
 }
