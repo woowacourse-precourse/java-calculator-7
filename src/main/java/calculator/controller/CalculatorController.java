fe9a73c9ef6b995f147a42b3f43c09cfa7029840
이희수 <hslee9234@naver.com>
2024-10-21 00:07:19 +0900

    refactor: CalculatorController
    기존 CalculatorController가 StringParser,StringValidator,CalculatorService를 모두 의존하는 형태에서 ParsingService,CalculatorService 만 의존하게 변경

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -2,36 +2,27 @@ package calculator.controller;
 
 import calculator.exception.CalculatorException;
 import calculator.service.CalculatorService;
-import calculator.util.StringParser;
-import calculator.util.StringValidator;
+import calculator.service.ParsingService;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class CalculatorController {
 
-    private final StringParser parser;
-    private final StringValidator validator;
+    private final ParsingService parser;
     private final CalculatorService calculator;
 
     public CalculatorController() {
-        this.parser = new StringParser();
-        this.validator = new StringValidator();
+        this.parser = new ParsingService();
         this.calculator = new CalculatorService();
     }
 
     public void run() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
-
         try {
-            // 입력 유효성 검사
-            validator.validate(input);
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine();
 
             // 문자열 파싱
             List<String> numbers = parser.parse(input);
-
-            // 파싱된 숫자 유효성 검사
-            validator.validateNumbers(numbers);
 
             // 덧셈 결과 계산
             String result = calculator.add(numbers);
