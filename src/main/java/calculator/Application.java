56fe2ab328011e3b398017b046e0d4136a81f267
Bobae Kim <rlaqhqo0418@naver.com>
2024-10-20 16:02:52 +0900

    refactor: 클래스 책임 분리(분리기와 계산기)
    - 클래스의 책임을 분리하기 위해 Calculator 클래스를 Separator와 Calculator 클래스로 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,9 +4,12 @@ public class Application {
     public static void main(String[] args) {
         ConsoleReader consoleReader = new ConsoleReader();
         ConsoleWriter consoleWriter = new ConsoleWriter();
+        Separator separator = new Separator();
         Calculator calculator = new Calculator();
+
         String input = consoleReader.readCalcLine();
-        int result = calculator.calc(input);
+        int[] nums = separator.separateInput(input);
+        int result = calculator.calcSum(nums);
         consoleWriter.writeCalcResult(result);
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,49 +1,8 @@
 package calculator;
 
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Calculator {
-    public int calc(String input) {
-        String separators = ",|:";
-        String pattern = "^(//.+\\\\n)*([0-9]+(.+[0-9]+)*|)$";
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(input);
-
-        if (!m.find()) {
-            throw new IllegalArgumentException();
-        }
-
-        if (!(m.group(1) == null)) {
-            separators = buildSeparators(m.group(1));
-        }
-        int[] nums = separateNums(m.group(2), separators);
-
-        return sumNums(nums);
-    }
-
-    private int[] separateNums(String input, String separators) {
-        String[] stringNums = input.split(separators);
-        return Arrays.stream(stringNums).mapToInt(this::stringToInt).toArray();
-    }
-
-    private String buildSeparators(String customSeparators) {
-        String basicSeparators = ",|:";
-        customSeparators = customSeparators.replace("\\n", "");
-        customSeparators = customSeparators.replace("//", "|");
-        return basicSeparators + customSeparators;
-    }
-
-    private int stringToInt(String stringNum) {
-        if (stringNum.isBlank()) {
-            return 0;
-        }
-        try {
-            return Integer.parseInt(stringNum);
-        } catch (Exception e) {
-            throw new IllegalArgumentException();
-        }
+    public int calcSum(int[] input) {
+        return sumNums(input);
     }
 
     private int sumNums(int[] nums) {
