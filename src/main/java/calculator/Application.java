b90f8d84feabe5610bd19060c59e9d6de20ff4bb
Mini <mini@MiniMac.local>
2024-10-20 19:15:56 +0900

    feat: add custom delimiter

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -19,6 +19,16 @@ public class Application {
         String delimiter = ",|:";
         String[] numbers = input.split(delimiter);
 
+        //커스텀 구분자 처리
+        if (input.length() > 2 && input.charAt(0) == '/' && input.charAt(1) == '/') {
+            int nindex = input.indexOf("\n");
+            delimiter = delimiter + "|" + input.substring(2, nindex);
+            numbers = (input.substring(nindex+1)).split(delimiter);
+
+
+
+        }
+
         int sum = 0;
         for (String number : numbers) {
             sum += Integer.parseInt(number.trim());
