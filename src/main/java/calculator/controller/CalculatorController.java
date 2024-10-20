9a844698b352957c3416097620939e4ffc233e26
byegu <rkdqudrn07@naver.com>
2024-10-20 16:50:40 +0900

    파싱한 문자열 덧셈 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -9,14 +9,22 @@ public class CalculatorController {
     InputString inputString = new InputString();
     InputView inputView = new InputView();
     StringValidator stringValidator = new StringValidator();
+    Integer result = 0;
 
     public void executeCalculator() {
         saveInputString();
+        calculate();
     }
 
     private void saveInputString() {
         inputString.setUserInput(inputView.enterInputString());
         List<Integer> userOperand = stringValidator.convertStringToIntegerList(inputString.getUserInput());
         inputString.setUserOperand(userOperand);
+    }
+
+    private void calculate() {
+        result = inputString.getUserOperand().stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
