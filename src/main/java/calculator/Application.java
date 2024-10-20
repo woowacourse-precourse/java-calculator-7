0edbb0a969f33ae175dd7c97e0ee7042a056cc60
jonghwanlee <kavae550@gmail.com>
2024-10-20 17:50:03 +0900

    refactor: Controller.java 세분화
    Controller.java 내의 start() 메서드를 start(), calculate(), resulting() 세 개로 세분화

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,9 @@ import calculator.controller.Controller;
 public class Application {
     public static void main(String[] args) {
         Controller controller = new Controller();
+        
         controller.start();
+        controller.calculate();
+        controller.resulting();
     }
 }
--- src/main/java/calculator/controller/Controller.java
+++ src/main/java/calculator/controller/Controller.java
@@ -4,15 +4,20 @@ import calculator.model.InputHandler;
 import calculator.view.View;
 
 public class Controller {
-    public void start() {
-        InputHandler inputHandler = new InputHandler();
-        View view = new View();
+    InputHandler inputHandler = new InputHandler();
+    View view = new View();
 
+    public void start() {
         view.displayInputPrompt();
         inputHandler.setInputString(); // 문자열 받는 메서드
+    }
 
+    public void calculate() {
         inputHandler.ensureCustomDelimiter();// 커스텀 구분자가 있는 지 확인
         inputHandler.extractNumbers();
+    }
+
+    public void resulting() {
         inputHandler.getResult();
         view.displayResult();
     }
