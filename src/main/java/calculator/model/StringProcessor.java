862fe73d7470c7d26e1a9b8b1e6197858cced5be
kim jihyeon <jihyeon546@gmail.com>
2024-10-20 18:39:17 +0900

    refactor: 커스텀 구분자 파싱 로직 분리

--- src/main/java/calculator/model/StringProcessor.java
+++ src/main/java/calculator/model/StringProcessor.java
@@ -1,9 +1,24 @@
 package calculator.model;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringProcessor {
+    private String parseCustomDelimiter(String input) {
+        if (input.startsWith("//")) {
+            int delimiterEndIndex = input.indexOf("\\n");
+            if (delimiterEndIndex != -1) {
+                String customDelimiter = input.substring(2, delimiterEndIndex);
+                if (customDelimiter.isEmpty()) {
+                    throw new IllegalArgumentException("커스텀 구분자를 입력해 주세요.");
+                }
+                return Pattern.quote(customDelimiter);
+            } else {
+                throw new IllegalArgumentException("잘못된 구분자 형식입니다.");
+            }
+        }
+        return null;
+    }
+
     public String[] StringSplitter(String input) {
 
         if (input == null || input.isEmpty()) {
@@ -11,28 +26,25 @@ public class StringProcessor {
         }
 
         String delimiter = ",|:";
-        if (input.startsWith("//")) {
-            Matcher matcher = Pattern.compile("//(.+)\n(.+)").matcher(input);
-            if (matcher.find()) {
-                String customDelimiter = matcher.group(1);
-                if (customDelimiter.isEmpty()) {
-                    throw new IllegalArgumentException("커스텀 구분자를 입력해 주세요.");
-                }
-                delimiter = Pattern.quote(customDelimiter);
-                input = matcher.group(2);
-            }
+        String numbers = input;
+
+        String customDelimiter = parseCustomDelimiter(input);
+        if (customDelimiter != null) {
+            delimiter = customDelimiter;
+            int delimiterEndIndex = input.indexOf("\\n");
+            numbers = input.substring(delimiterEndIndex + 2);
         }
 
-        if (!input.matches("[\\d" + delimiter + "]+")) {
+        if (!numbers.matches("[-\\d" + delimiter + "]+")) {
             throw new IllegalArgumentException("올바르지 않은 구분자입니다.");
         }
 
-        String[] numbers = input.split(delimiter);
+        String[] splitNumbers = numbers.split(delimiter);
 
-        if (numbers.length == 0 || input.matches(delimiter + "+")) {
+        if (splitNumbers.length == 0 || numbers.matches(delimiter + "+")) {
             throw new IllegalArgumentException("구분자를 쓸 때는 숫자를 입력해 주세요.");
         }
 
-        return numbers;
+        return splitNumbers;
     }
 }
