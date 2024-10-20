31835f3d965943645145777deda7b902cbb078d6
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:34:23 +0900

    :sparkles: feat(NumberValidator): 숫자 문자열을 정수로 변환

--- src/main/java/calculator/model/NumberValidator.java
+++ src/main/java/calculator/model/NumberValidator.java
@@ -1,4 +1,9 @@
 package calculator.model;
 
 public class NumberValidator {
+
+    public int validateAndParse(String number) {
+        int result = Integer.parseInt(number);
+        return result;
+    }
 }
