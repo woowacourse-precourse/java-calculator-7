7864f616fcf9ac96756563927ac98ec63c15327a
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-20 21:28:11 +0900

    refactor: 계산기 클래스 추상클래스로 변경

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -2,17 +2,6 @@ package calculator.model;
 
 import java.util.List;
 
-public class Calculator {
-
-    private final List<Long> numbers;
-
-    public Calculator(List<Long> numbers) {
-        this.numbers = numbers;
-    }
-
-    public long sum() {
-        return numbers.stream()
-                .mapToLong(Long::longValue)
-                .sum();
-    }
+public abstract class Calculator {
+    public abstract int calculate(List<Integer> numbers);
 }
