5c0e9502ff448bc35c9c844f0044fac348a6c84b
openmpy <suhwan@kakao.com>
2024-10-20 17:21:41 +0900

    refactor: 커스텀 구분자 정규 표현식 Escape 처리

--- src/main/java/calculator/strategy/CustomDelimiter.java
+++ src/main/java/calculator/strategy/CustomDelimiter.java
@@ -5,11 +5,15 @@ public class CustomDelimiter implements Delimiter {
     private final String delimiter;
 
     public CustomDelimiter(String delimiter) {
-        this.delimiter = delimiter;
+        this.delimiter = escapeCustomDelimiter(delimiter);
     }
 
     @Override
     public String[] split(String input) {
         return input.split(delimiter, -1);
+    }
+
+    private String escapeCustomDelimiter(String delimiter) {
+        return delimiter.replaceAll("([.*+?^$()\\[\\]{}|\\\\])", "\\\\$1");
     }
 }
