a484d0ef26d8a90f740bd9e6bc2f94486be401ae
kjyyjk <rlawnsdud920@gmail.com>
2024-10-20 22:35:17 +0900

    test: StringCalculatorTest 테스트케이스 추가
    - 커스텀 구분자가 없는경우
    - 커스텀 구분자가 있는 경우
    - 아무 숫자도 입력되지 않은 경우 -> 0

--- src/test/java/calculator/domain/StringCalculatorTest.java
+++ src/test/java/calculator/domain/StringCalculatorTest.java
@@ -2,22 +2,25 @@ package calculator.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StringCalculatorTest {
 
-    @Test
+    @ParameterizedTest
     @DisplayName("StringCalculator는_입력_문자열_내_숫자의_합을_반환할_수_있다")
-    public void calculate() {
+    @CsvSource(value = {"123 : 123", "//;\\n1,2:3;4 : 10", ",: : 0"}, delimiterString = " : ")
+    public void calculate(String input, int expected) {
         //given
         StringCalculator calculator = new StringCalculator();
-        InputString inputString = new InputString("//;\\n3:4,4;19");
+        InputString inputString = new InputString(input);
 
         //when
         int result = calculator.calculate(inputString);
 
         //then
-        assertThat(result).isEqualTo(30);
+        assertThat(result).isEqualTo(expected);
     }
 }
