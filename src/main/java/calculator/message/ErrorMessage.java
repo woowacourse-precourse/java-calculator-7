39cbc087885bbf0c403b517a57040253531e655c
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-20 21:02:49 +0900

    feat: 에러 메세지 추가

--- src/main/java/calculator/message/ErrorMessage.java
+++ src/main/java/calculator/message/ErrorMessage.java
@@ -1,17 +1,19 @@
-package calculator.error;
+package calculator.message;
 
-public enum ErrorCode {
+public enum ErrorMessage implements MessageProvider{
 
-    NOT_ALLOWED_NEGATIVE_NUMBER("음수는 허용되지 않습니다."),
-    INVALID_NUMBER_FORMAT("잘못된 숫자 형식이 포함되어 있습니다."),
     EMPTY_INPUT("입력값이 비어 있습니다."),
+    INVALID_INPUT("유효하지 않은 입력입니다."),
     INVALID_WHITESPACE("공백은 허용되지 않습니다."),
+    INVALID_NUMBER_FORMAT("잘못된 숫자 형식입니다."),
     INVALID_CHARACTER("구분자 외의 문자가 포함되어 있습니다."),
+    NOT_ALLOWED_NEGATIVE_NUMBER("음수는 허용되지 않습니다."),
+    INTEGER_OUT_OF_RANGE("표현 가능 범위를 초과하였습니다."),
     MISSING_CUSTOM_DELIMITER_END("잘못된 커스텀 문자입니다.");
 
     private final String message;
 
-    ErrorCode(String message) {
+    ErrorMessage(String message) {
         this.message = message;
     }
 
