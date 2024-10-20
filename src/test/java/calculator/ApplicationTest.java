9d05ebc9347c9aed5b08cc983e1368743a7b2bd1
Hanjjong <tim4974@naver.com>
2024-10-20 17:05:41 +0900

    test: 커스텀 구분자로 공백 입력 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -72,6 +72,13 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 커스텀_구분자_공백입력_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//   \\n1:2:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
     @Override
     public void runMain() {
