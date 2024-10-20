1f3812625d19a24262cd88a7ba1136c8482f73d8
Parkjyun <jaeyun0507@naver.com>
2024-10-20 16:13:19 +0900

    feat(Parser): 기본 구분자로 ,와 ;를 사용한다

---
+++ src/main/java/calculator/model/Parser.java
@@ -0,0 +1,8 @@
+package calculator.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+    private List<String> delimiters = new ArrayList<>(List.of(",", ":"));
+}
