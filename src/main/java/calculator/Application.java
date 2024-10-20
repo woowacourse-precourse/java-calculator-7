caf0b0fa26da0a8e2e5cfab3fba33c9ebac5ceac
urim96 <qusdbfla1@gmail.com>
2024-10-20 22:53:04 +0900

    명확한 에러메세지를 위해 catch문에 NumberFormatException 추가, 커스텀 구분자 사용 시 정확성을 위해 조건문 변경, split에 사용할 수 없는 메타문자 작성 시 에러 발생 시키도록 조건문 추가, 입력한 문자열 사이 빈 문자열은 0으로 더하도록 조건문 추가, 필요없는 코드 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,26 +7,28 @@ public class Application {
 
     	try {
     		System.out.println("덧셈할 문자열을 입력해 주세요.");
+    		System.out.println(". $ | ( ) [ { ^ ? * + \\를 커스텀 구분자로 지정할 시 문자 앞에 \\를 기술해주세요.");
+    		System.out.println("예) " + "//\\.\\n1.2.3");
     		String str = Console.readLine();
     		int checkNum = Application.checkStr(str);
     		System.out.println("결과 : " + checkNum);
-    	}catch(Exception e) {
+    	}catch(NumberFormatException ne) {
+    		throw new IllegalArgumentException("올바른 숫자와 구분자를 입력해주세요.");
+    	}
+    	catch(Exception e) {
     		throw new IllegalArgumentException();
     	}
     }
     
     public static int checkStr(String str) {
-    	if(str.length() == 0 || str == null) {
+    	if(str == null || str.length() == 0 ) {
     		return 0;
     	}
     	if(str.length() == 1) {
     		return Integer.parseInt(str);
     	}
-    	if(str.length() > 1) {
-    		String[] arr = Application.checkFirstNum(str);
-    		return Application.checkArrNum(arr);
-    	}
-		return 0;
+    	String[] arr = Application.checkFirstNum(str);
+    	return Application.checkArrNum(arr);
     }
     
     public static String[] checkFirstNum(String str) {
@@ -34,9 +36,13 @@ public class Application {
     	if(Character.isDigit(str.charAt(0))) {
     		arr = str.split(",|:");
     	}
-    	else if(str.charAt(0) == '/') {
+    	else if(str.startsWith("//")) {
     		String custom = str.substring(2, str.indexOf("\\n"));
     		str = str.substring(str.indexOf("\\n")+2,str.length());
+            if (".$|()[{^?*+".contains(custom.substring(0, 1))) {
+            	System.err.println("커스텀 구분자 사용 시 형식을 지켜주세요.");
+            	throw new IllegalArgumentException();
+            }
     		arr = str.split(custom);
     	}
     	else {
@@ -48,7 +54,9 @@ public class Application {
     public static int checkArrNum(String[] arr) {
     	int sum = 0;
     	for(int i = 0; i < arr.length; i++) {
-    		if(Integer.parseInt(arr[i]) >= 0) {
+    		if(arr[i] == null || arr[i].isEmpty()) {
+    			sum += 0;
+    		}else if(Integer.parseInt(arr[i]) >= 0) {
     			sum += Integer.parseInt(arr[i]);
     		}else {
     			throw new IllegalArgumentException();
