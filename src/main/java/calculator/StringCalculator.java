e0d9a6245b3e337c5478adb8ce1e5b348ddcbb9c
true <truee030@gmail.com>
2024-10-20 22:26:13 +0900

    feat: 숫자 배열 합산 메서드

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,6 +5,16 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
+    // 숫자 배열을 합하여 반환하는 메서드
+    private int calculateSum(String[] numbers) {
+        int sum = 0;
+        for (String number : numbers) {
+            int num = parsePositiveNumber(number);
+            sum += num;
+        }
+        return sum;
+    }
+
     // 음수가 입력되면 예외를 던지는 숫자 파싱 메서드
     private int parsePositiveNumber(String number) {
         int parsedNumber = Integer.parseInt(number.trim());
