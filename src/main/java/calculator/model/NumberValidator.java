5ba57a2e42c7b10daf7e288e1147e3c2669cb6df
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:35:15 +0900

    :sparkles: feat(NumberValidator): 숫자가 음수인 경우, 예외를 발생시킨다

--- src/main/java/calculator/model/NumberValidator.java
+++ src/main/java/calculator/model/NumberValidator.java
@@ -1,9 +1,14 @@
 package calculator.model;
 
+import calculator.exception.InvalidInputException;
+
 public class NumberValidator {
 
     public int validateAndParse(String number) {
         int result = Integer.parseInt(number);
+        if (result < 0) {
+            throw new InvalidInputException("음수는 허용되지 않습니다: " + number);
+        }
         return result;
     }
 }
