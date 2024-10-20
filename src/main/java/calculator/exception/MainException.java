bb69f97571b8997315bb61aa4b2953c15ddf2357
jjd3109 <jongdoo2001@naver.com>
2024-10-20 17:04:12 +0900

    feat : - 각 구분자를 제외한 곳에는 양수가 들어오지 않게, - 잘못된 값을 입력할 경우 예외 발생 메시지 처리

---
+++ src/main/java/calculator/exception/MainException.java
@@ -0,0 +1,7 @@
+package calculator.exception;
+
+public class MainException {
+
+    public static final String putError = "잘못된 값을 입력하였습니다.";
+    public static final String containsNegative = "구분자를 제외한 곳에는 양수 외에 들어오면 안됩니다.";
+}
