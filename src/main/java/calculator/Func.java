0d6ff0fe9474ca93a9d6fd9f5213e2e969658377
JamogFrinds <ghrb6769@gmail.com>
2024-10-20 16:48:16 +0900

    fix: Func.java  매서드 파라미터 수정

--- src/main/java/calculator/Func.java
+++ src/main/java/calculator/Func.java
@@ -4,18 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Func{
-    public List<String> split(Input input){
+    Input input = new Input();
+
+    public List<String> split(){
         ArrayList<String> splitNum = new ArrayList<>();
         String[] splitInput = input.userInput().split(",");
         for(int i=0; i<splitInput.length; i++)
             splitNum.add(splitInput[i]);
         return splitNum;
     }
-    public ArrayList<Integer> strToInt(Input input) {
+    public ArrayList<Integer> strToInt() {
         ArrayList<Integer> num = new ArrayList<>();
-        ArrayList<String> splitNum = new ArrayList<>(split(input));
+        ArrayList<String> splitNum = new ArrayList<>(split());
 
-        for(int i=0; i<split(input).size(); i++) {
+        for(int i=0; i<split().size(); i++) {
             num.add(Integer.parseInt(splitNum.get(i)));
         }
         return num;
