6441ebb5cd0461f8599a28d6e31d78834ecfee16
zjhj0814 <zjhj0814@gmail.com>
2024-10-20 13:33:18 +0900

    feat(DelimiterStrategy): 구분자 설정 인터페이스와 구현체 생성
    DelimiterStrategyTest 컴파일을 위해 DelimiterStrategy 인터페이스와 커스텀 구분자 구현체, 디폴트 구분자 구현체를 생성한다

---
+++ src/main/java/calculator/delimiterStrategy/DefaultDelimiterStrategy.java
@@ -0,0 +1,12 @@
+package calculator.delimiterStrategy;
+
+import java.util.List;
+
+public class DefaultDelimiterStrategy implements DelimiterStrategy {
+    public static final List<String> defaultDelimiters = List.of(",", ":");
+
+    @Override
+    public List<String> decideDelimiter(String userInput) {
+        return null;
+    }
+}
---
+++ src/main/java/calculator/delimiterStrategy/DelimiterStrategy.java
@@ -0,0 +1,7 @@
+package calculator.delimiterStrategy;
+
+import java.util.List;
+
+public interface DelimiterStrategy {
+    public List<String> decideDelimiter(String userInput);
+}
