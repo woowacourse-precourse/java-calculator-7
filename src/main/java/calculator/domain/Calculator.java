d005ec99e2b802e695b9c22d70b6253d090e91d2
inhor <inhoru126@gmail.com>
2024-10-20 23:45:37 +0900

    feat : 구분자 파싱 객체 생성

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -9,10 +9,12 @@ import static calculator.util.validator.InputValidator.validate;
 public class Calculator {
 
     private final String input;
+    private final NumberParser numberParser;
 
     public Calculator(String input) {
         validate(input);
         this.input = input;
+        this.numberParser = new NumberParser();
     }
 
 }
