65fb4719b8dcc2d8c70707212ca0f79409010f7a
SeongJunChun <csj991210@naver.com>
2024-10-20 20:22:30 +0900

    style: 코드 컨벤션 적용

--- src/test/java/calculator/ConverterTest.java
+++ src/test/java/calculator/ConverterTest.java
@@ -14,23 +14,24 @@ class ConverterTest {
     @DisplayName("문자열을 숫자로 변환")
     @ParameterizedTest(name = "{displayName}: {0}")
     @MethodSource("numbers")
-    void convertToInteger(List<String> input,List<Integer> expected) {
-       Converter converter = new Converter();
+    void convertToInteger(List<String> input, List<Integer> expected) {
+        Converter converter = new Converter();
         List<Integer> numbers = converter.convert(input);
         assertEquals(expected, numbers);
     }
 
     static Stream<Arguments> numbers() {
         return Stream.of(
-                Arguments.of(List.of("1","2","3"), List.of(1,2,3)),
+                Arguments.of(List.of("1", "2", "3"), List.of(1, 2, 3)),
                 Arguments.of(List.of("123"), List.of(123)),
-                Arguments.of(List.of("15","26","37"), List.of(15,26,37))
+                Arguments.of(List.of("15", "26", "37"), List.of(15, 26, 37))
         );
     }
+
     @DisplayName("문자열을 정수로 반환 할수 없을 시 예외")
     @Test
     void checkInvalidNumbers() {
         Converter converter = new Converter();
-        assertThrows(IllegalArgumentException.class, () -> converter.convert(List.of("1a","2b","[3c")));
+        assertThrows(IllegalArgumentException.class, () -> converter.convert(List.of("1a", "2b", "[3c")));
     }
 }
