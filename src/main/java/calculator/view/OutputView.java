49e689ccb704a9b58c1c0a151b72022ab54834e9
hyosang <billy04100410@gmail.com>
2024-10-20 22:59:15 +0900

    feat(application): add Console.close

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,8 +1,11 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class OutputView {
 
     public void showResult(int result) {
+        Console.close();
         System.out.println("결과 : " + result);
     }
 }
