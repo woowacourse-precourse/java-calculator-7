bef1dc98ba7a93c5bb185c08f87293134f992791
dpfls0922 <dpfls0922@naver.com>
2024-10-20 16:23:02 +0900

    feat: 사용자 정의 구분자 처리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,22 @@
 package calculator;
 
 public class Application {
+
+    public static String findCustomDelimiter(String input) {
+        int startIndex;
+        int endIndex;
+
+        if (input.contains("//") && input.contains("\\n")) {
+            startIndex = input.indexOf("//") + 2;
+            endIndex = input.indexOf("\\n");
+            if (startIndex == endIndex) {
+                throw new IllegalArgumentException("구분자를 입력해주세요.");
+            }
+            return input.substring(startIndex, endIndex);
+        }
+        return "";
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
