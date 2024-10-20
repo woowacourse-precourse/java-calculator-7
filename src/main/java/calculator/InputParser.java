ad693b75fe15c5eb8790d00f732c18b39afa4315
EunSeop <doosan1112@naver.com>
2024-10-20 21:09:20 +0900

    fix : 입력값이 없을 때 오류 수정

--- src/main/java/calculator/InputParser.java
+++ src/main/java/calculator/InputParser.java
@@ -2,13 +2,13 @@ package calculator;
 
 public class InputParser {
     private String parsedinput;
-
     public InputParser(String input) {
         parsedinput = parseInput(input);
     }
-
     private String parseInput(String input) {
-        return input.substring(5);
+        input = input.substring(5);
+        if(input.isEmpty()) return "0";
+        return input;
     }
 
     public String getParsedInput() {
