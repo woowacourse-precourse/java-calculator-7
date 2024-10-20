ebf4822a2e27cdc54eba80fc02436b89d6590d0a
배성연 <bsy309@ajou.ac.kr>
2024-10-20 14:13:52 +0900

    refactor(ListParser): 메소드 분리

--- src/main/java/calculator/parsing/ListParser.java
+++ src/main/java/calculator/parsing/ListParser.java
@@ -10,18 +10,15 @@ public class ListParser {
 
     public static List<Integer> parseList(String input, List<String> delimiter) {
 
-        String delimiterRegex = String.join("|", delimiter);
+        String delimiterRegex = getDelimiterRegex(delimiter);
 
-        String[] parsedNumbers = input.split(delimiterRegex);
+        String[] parsedNumbers = getParsedNumbers(input, delimiterRegex);
 
         List<Integer> numbersList = new ArrayList<>();
 
         for (String numStr : parsedNumbers) {
             try {
-                int number = Integer.parseInt(numStr);
-                if (number < 0) {
-                    throw new IllegalArgumentException("양의 정수를 입력해야 합니다.");
-                }
+                int number = parseNumbers(numStr);
                 numbersList.add(number);
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("올바르지 않은 형식입니다.");
@@ -29,5 +26,21 @@ public class ListParser {
         }
 
         return numbersList;
+    }
+
+    private static String getDelimiterRegex(List<String> delimiter) {
+        return String.join("|", delimiter);
+    }
+
+    private static String[] getParsedNumbers(String input, String delimiterRegex) {
+        return input.split(delimiterRegex);
+    }
+
+    private static int parseNumbers(String numStr) {
+        int number = Integer.parseInt(numStr);
+        if (number < 1) {
+            throw new IllegalArgumentException("양의 정수를 입력해야 합니다.");
+        }
+        return number;
     }
 }
