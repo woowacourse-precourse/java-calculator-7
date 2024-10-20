125ea1d37f6ed7cb3cec307dcbcb22c1819e5c0d
Jang <sdsd090811@gmail.com>
2024-10-20 13:42:39 +0900

    feat: 메인 함수에 애플리케이션 동작 코드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,39 @@
 package calculator;
 
+import calculator.command.CommandReader;
+import calculator.command.DelimiterLine;
+import calculator.command.NumbersLine;
+import calculator.number.NumbersReader;
+import calculator.number.rule.PositiveNumbersRule;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        print("덧셈할 문자열을 입력해 주세요.");
+        String userInput = Console.readLine();
+        Console.close();
+        CommandReader commandReader = createCommandReader();
+        DelimiterLine delimiterLine = commandReader.readDelimiter(userInput);
+        NumbersLine numbersLine = commandReader.readNumbers(userInput);
+
+        MyCalculator calculator = createPositiveNumbersCalculator();
+        int result = calculator.calculate(delimiterLine, numbersLine);
+        print("결과 : ".concat(String.valueOf(result)));
+    }
+
+    private static void print(String string) {
+        System.out.println(string);
+    }
+
+    private static CommandReader createCommandReader() {
+        return new CommandReader();
+    }
+
+    private static MyCalculator createPositiveNumbersCalculator() {
+        return new MyCalculator(createPositiveNumberReader());
+    }
+
+    private static NumbersReader createPositiveNumberReader() {
+        return new NumbersReader(new PositiveNumbersRule());
     }
 }
