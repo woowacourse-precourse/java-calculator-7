96699d18029dab9fb88308076a5f7e1fb0bd4adc
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 03:59:31 +0900

    feat: 구분자 목록에 따라 문자열을 분리하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import calculator.controller.CalculatorController;
 import calculator.model.CustomStringParser;
 import calculator.model.CustomStringParserImpl;
 import calculator.model.DelimiterManagerImpl;
+import calculator.model.NumberStringSplitterImpl;
 import calculator.view.CalculatorInputView;
 import calculator.view.CalculatorOutputView;
 
@@ -15,7 +16,7 @@ public class Application {
 
         CustomStringParser customStringParser = new CustomStringParserImpl(delimiterManagerImpl);
         CalculatorController calculatorController = new CalculatorController(customStringParser,
-                new CalculatorOutputView());
+                new NumberStringSplitterImpl(delimiterManagerImpl), new CalculatorOutputView());
         CalculatorInputView calculatorInputView = new CalculatorInputView();
 
         String inputStr = calculatorInputView.inputExpression();
--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,23 +1,30 @@
 package calculator.controller;
 
 import calculator.model.CustomStringParser;
+import calculator.model.NumberStringSplitter;
 import calculator.view.CalculatorOutputView;
 
 public class CalculatorController {
 
     private final CustomStringParser customStringParser;
+    private final NumberStringSplitter numberStringSplitter;
     private final CalculatorOutputView calculatorOutputView;
 
-    public CalculatorController(CustomStringParser customStringParser, CalculatorOutputView calculatorOutputView) {
+    public CalculatorController(CustomStringParser customStringParser,
+                                NumberStringSplitter numberStringSplitter,
+                                CalculatorOutputView calculatorOutputView) {
         this.customStringParser = customStringParser;
+        this.numberStringSplitter = numberStringSplitter;
         this.calculatorOutputView = calculatorOutputView;
     }
 
     public void processInput(String inputStr) {
         if (inputStr.isEmpty()) {
             calculatorOutputView.printResult("0");
+            return;
         }
 
         inputStr = customStringParser.parse(inputStr);
+        String[] inputStrArr = numberStringSplitter.split(inputStr);
     }
 }
