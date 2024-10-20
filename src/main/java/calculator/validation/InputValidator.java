32efd806f0adfa30a5cfde92bd12ef0e2788cd51
jae woo Jung <awfjol2008@gmail.com>
2024-10-20 17:19:59 +0900

    refactor: 필요없는 검증 메서드 제거

--- src/main/java/calculator/validation/InputValidator.java
+++ src/main/java/calculator/validation/InputValidator.java
@@ -8,17 +8,5 @@ public class InputValidator {
 
 	public void validateInput(String input) {
 		delimiters = Arrays.asList(",", ":");
-		validateHasNumber(input);
-	}
-
-	private void validateHasNumber(String input) {
-		if (!hasNumber(input)) {
-			throw new IllegalArgumentException("입력값에 숫자가 포함되어 있지 않습니다.");
-		}
-	}
-
-	private boolean hasNumber(String input) {
-		return input.chars()
-			.anyMatch(Character::isDigit);
 	}
 }
