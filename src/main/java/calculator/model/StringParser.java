5d81d718c9c6b0481c6bf006c172104b53de6a92
eunseobb <seobseob0810@naver.com>
2024-10-21 01:44:10 +0900

    refactor(model): 커스텀 구분자가 특수문자인 경우 처리
    "StringParser" 클래스의 "escapeSpecialCharacter" 메소드를 통해 특수문자가 커스텀 구분자일 때
    escape할 수 있도록 함
    
    Closes #767

--- src/main/java/calculator/model/StringParser.java
+++ src/main/java/calculator/model/StringParser.java
@@ -5,24 +5,44 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StringParser {
-    public List<Integer> extractNumbers(String str){
-        List<Integer> numbers;
-        String delimeter = findDelimeter(str);
+    public List<Integer> extractNumbers(String str) {
+        Validation inputValidation = new Validation();
+        List<String> stringList;
+        String delimiter = findDelimiter(str);
 
-        numbers = Arrays.stream(str.split(delimeter))
-                .map(s -> s.isEmpty() ? "0" : s)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        str = trimInputString(str);
+
+        stringList = Arrays.stream(str.split(delimiter)).map(s -> s.isEmpty() ? "0" : s).collect(Collectors.toList());
+
+        if (!inputValidation.isValidInput(stringList)) {
+            throw new IllegalArgumentException("입력된 문자열에 유효하지 않은 값이 포함되어 있습니다.");
+        }
+
+        List<Integer> numbers = stringList.stream().map(Integer::parseInt).collect(Collectors.toList());
 
         return numbers;
     }
 
-    public String findDelimeter(String str){
-        if (str.startsWith("//")&&str.contains("\n")){
-            return str.substring(2, str.indexOf("\n"));
+    //커스텀 구분자 찾기
+    private String findDelimiter(String str) {
+        if (str.startsWith("//") && str.contains("\\n")) {
+            String delimiter = str.substring(2, str.indexOf("\\n"));
+            return escapeSpecialCharacters(delimiter);
         }
         return ",|:";
     }
 
+    //커스텀 구분자 이후 문자열 반환
+    private String trimInputString(String str) {
+        if (str.startsWith("//") && str.contains("\\n")) {
+            return str.substring(str.indexOf("\\n") + 2);
+        }
+        return str;
+    }
+
+    //특수문자 예외처리
+    private String escapeSpecialCharacters(String delimiter) {
+        return delimiter.replaceAll("([\\[\\](){}.*+?^$|\\\\])", "\\\\$1");
+    }
 
 }
