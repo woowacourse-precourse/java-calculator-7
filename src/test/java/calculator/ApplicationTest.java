99408bfee9269584d4e40cf1c372484c289c60b8
egaeng <egaeng09@gmail.com>
2024-10-20 22:34:47 +0900

    test(ApplicationTest): 입력 경우의 수에 따른 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,10 +17,74 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_혼합() {
+        assertSimpleTest(() -> {
+            run("//#\\n1#2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 임의_구분자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2*3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 빈_문자열_테스트() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_숫자_제외() {
+        assertSimpleTest(() -> {
+            run("//&\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 구분자_없이_숫자만() {
+        assertSimpleTest(() -> {
+            run("123");
+            assertThat(output()).contains("결과 : 123");
+        });
+    }
+
+    @Test
+    void 숫자와_문자_혼합() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,b"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 문자만_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a,b,c"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자와_임의_구분자_혼합() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//#\\n1#2,3&4"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
