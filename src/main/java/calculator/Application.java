35e19ae55e16afdec926eb665aae1a76d5c1d58a
hyuntae99 <hyuntae9912@naver.com>
2024-10-20 15:54:40 +0900

    fix : 입력이 아예 없는 경우를 처리할 수 있도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,25 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
+        try {
+            // 입력 받기
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine();
 
-        String[] numbers = Service.separate(input);
+            // 숫자 추출
+            String[] numbers = Service.separate(input);
 
-        int result = Service.add(numbers);
-        System.out.println("결과 : " + result);
+            // 숫자 배열의 합
+            int result = Service.add(numbers);
+
+            // 출력
+            System.out.println("결과 : " + result);
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("입력이 없습니다.");
+        }
     }
 }
