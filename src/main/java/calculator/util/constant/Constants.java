91b503cf3c38c0d3ad0f3315025ee70c00251306
inhor <inhoru126@gmail.com>
2024-10-20 23:39:20 +0900

    feat: 상수값 설정

---
+++ src/main/java/calculator/util/constant/Constants.java
@@ -0,0 +1,14 @@
+package calculator.util.constant;
+
+public class Constants {
+
+    public static final String DELIMITER_COMMA = ",";
+    public static final String DELIMITER_COLON = ":";
+    public static final String ERROR_MESSAGE = "[ERROR] ";
+
+    public static final String DEFAULT_DELIMITER_PATTERN = ",|:";
+    public static final String CUSTOM_DELIMITER_PATTERN = "//(.*)\\\\n(.*)";
+    public static final String SINGLE_NUMBER_PATTERN = "\\d+";
+    public static final String RESULT = "결과 : ";
+
+}
