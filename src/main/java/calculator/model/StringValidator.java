efaee35b2b76968e234b66de1c533f9eb1e96175
Chan531 <chan657312@gmail.com>
2024-10-20 19:55:20 +0900

    feat(StringValidator): 시작 상태 체크

--- src/main/java/calculator/model/StringValidator.java
+++ src/main/java/calculator/model/StringValidator.java
@@ -8,10 +8,21 @@ public class StringValidator {
 
 	private static final String COMMA_DELIMITER = ",";
 	private static final String COLON_DELIMITER = ",";
+	private static final int DIGIT = 0;
+	private static final int DELIMITER = 1;
+	private static final int INPUT_START_INDEX = 0;
 
 	private List<String> delimiters = new ArrayList<>(Arrays.asList(COMMA_DELIMITER, COLON_DELIMITER));
 
 	public void addCustomDelimiter(String customDelimiter) {
 		delimiters.add(customDelimiter);
+	}
+
+	private int checkStartStatus(String input) {
+		char start = input.charAt(INPUT_START_INDEX);
+		if (Character.isDigit(start)) {
+			return DIGIT;
+		}
+		return DELIMITER;
 	}
 }
