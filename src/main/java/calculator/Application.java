55f7803727d26a562f7a708309e3d83829826b3e
GwonDooHyeon <secure3141@gmail.com>
2024-10-20 23:33:55 +0900

    feat: 입력 문자열이 빈 문자열로 들어오는 경우 0을 반환한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int result = 0;
+        
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        
+        String input = Console.readLine();
+        if (input.isEmpty()) {
+            return;
+        }
+        
+        System.out.println("결과 : " + result);
     }
+    
 }
