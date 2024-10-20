12dec5374fd2da9da51aeca2f09a63b694ea720c
윤민수 <koelysa5696@naver.com>
2024-10-21 00:27:27 +0900

    0출력을 결과 : 0(정수) 으로 변경

--- src/main/java/controller/runCalculator.java
+++ src/main/java/controller/runCalculator.java
@@ -9,12 +9,11 @@ public class RunCalculator {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine(); // 문자를 입력받고 앞뒤 공백을 제거.
 
-        if (input.isEmpty()) { // 빈 입력이면 0 반환
-            System.out.print("결과 : 0");
+        if (input.isEmpty()) { // 빈 입력이면 0을 반환
+            System.out.print("결과 : " + 0);
             return;
         }
         int sumResult = delimiterService.DelimiterProcess(input);
         System.out.print("결과 : " + sumResult);
-
     }
 }
