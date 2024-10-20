e323ccc7ed177bf0821cf054b542cf9c93d40f3a
kjyyjk <rlawnsdud920@gmail.com>
2024-10-20 23:47:21 +0900

    feat: 커스텀 구분자로 정규표현식 예약어가 들어올 경우 이스케이프 문자 추가 구현

--- src/main/java/calculator/domain/Delimiters.java
+++ src/main/java/calculator/domain/Delimiters.java
@@ -1,9 +1,11 @@
 package calculator.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Delimiters {
+    private final String REGEX_EXPRESSION = ".^$*+?()[]{}|\\";
     private List<String> delimiters = new ArrayList<>();
 
     public Delimiters(CustomDelimiter customDelimiter) {
@@ -11,12 +13,26 @@ public class Delimiters {
         delimiters.add(",");
 
         if (customDelimiter.isValueExist()) {
-            String value = customDelimiter.getValue();
+            String value = getCustomDelimiterValue(customDelimiter.getValue());
             delimiters.add(value);
         }
     }
 
     public String convertToRegex() {
         return String.join("|", delimiters);
+    }
+
+    private String getCustomDelimiterValue(String value) {
+        if (isValueHasRegexExpression(value)) {
+            return "\\" + value;
+        }
+
+        return value;
+    }
+
+    private boolean isValueHasRegexExpression(String value) {
+        String[] exps = REGEX_EXPRESSION.split("");
+        return Arrays.stream(exps)
+                .anyMatch(exp -> value.contains(exp));
     }
 }
