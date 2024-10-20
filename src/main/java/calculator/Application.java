9acc6ad753ab62c5ff884c609950b59d37079b42
settle54 <settle54@kangwon.ac.kr>
2024-10-21 02:25:21 +0900

    docs: 요구사항 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,10 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int i = 1;
+        for (int j = 0; j < 10; j++) {
+            i += 1;
+        }
+        System.out.println(i);
     }
 }
