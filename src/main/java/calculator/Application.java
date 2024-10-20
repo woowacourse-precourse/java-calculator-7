b2218dfd3f68efd7f53e73f27899d2b84a7bde53
Avocado <ujubongbong@gmail.com>
2024-10-20 23:18:11 +0900

    :sparkles: feat: 커스텀 구분자 지정하는 함수

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,8 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private static String[] basicSeparators = {",", "\\n"};
+    private static char[] customSeparators;
 
     // 문자열 덧셈 계산기 시작 함수
     public static void init() {
@@ -12,6 +14,16 @@ public class Application {
     // 문자열 입력받는 함수
     public static void input() {
        String input = Console.readLine();
+    }
+
+    // 커스텀 구분자 지정하는 함수
+    public static void customSeparator(String input) {
+        // "//"와 "\n" 사이에 문자가 존재하면 커스텀 구분자로 지정
+        if(input.startsWith("//")) {
+            if(input.substring(3, 5).equals("\\n")) {
+                customSeparators = new char[]{input.charAt(2)};
+            }
+        }
     }
 
     public static void main(String[] args) {
