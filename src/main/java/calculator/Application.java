3459d5f60636dc7e1dee8b50edf34594ea3b5174
NamJihyun99 <namjihyun9936@gmail.com>
2024-10-21 00:02:25 +0900

    feat:> 입출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Calculator calculator = new Calculator(Console.readLine());
+        String result = "결과 : " + calculator.calculate();
+        System.out.println(result);
     }
 }
