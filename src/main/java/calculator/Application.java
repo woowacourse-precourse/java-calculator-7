5c454f0aba9b5314688a035551de6d415dd4fdab
parkseongyeol <ert2154@naver.com>
2024-10-21 02:17:14 +0900

    feat($Application): Application 구조 단순화
    기존 로직 calculator로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,25 +1,9 @@
 package calculator;
 
-import adder.Adder;
-import camp.nextstep.edu.missionutils.Console;
-import parser.Parser;
-import separator.Separator;
-
 public class Application {
     public static void main(String[] args) {
 
-        Adder adder = new Adder();
-        Separator separator = new Separator();
-        Parser parser = new Parser();
-
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
-
-        separator.saveSeparator(input);
-        String stringToCalculate = parser.parseStringToCalculate(input);
-        int result = adder.add(stringToCalculate);
-
-        System.out.println("결과 : " + result);
-
+        Calculator calculator = new Calculator();
+        calculator.calculator();
     }
 }
