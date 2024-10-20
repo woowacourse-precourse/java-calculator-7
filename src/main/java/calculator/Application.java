0421776ade939658265d27e95db89169c0735c88
princebyun <princebyun@gmail.com>
2024-10-20 20:09:32 +0900

    주석작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,9 +9,9 @@ public class Application {
         String userInputText = readLine(); //구현 요구 사항중 해당 라이브러리만을 사용하여 구현해야 하는 항목 충족
         try {
             InputValidation.validateInput(userInputText);
-            String[] textValues = SplitText.textSplit(userInputText);
-            int[] sumValues = ValueCheck.checkValue(textValues);
-            int result = ValueSum.sumValue(sumValues);
+            String[] textValues = SplitText.textSplit(userInputText);//문자열 나누는 클래스
+            int[] sumValues = ValueCheck.checkValue(textValues);//나눠진 문자열을 벨리데이션 진행하는 클래스
+            int result = ValueSum.sumValue(sumValues);//숫자로 형변환 된 배열을 계산하는 클래스
             System.out.println("결과 : " + result);
         } catch (IllegalArgumentException e) {
             System.out.println("오류: " + e.getMessage());
