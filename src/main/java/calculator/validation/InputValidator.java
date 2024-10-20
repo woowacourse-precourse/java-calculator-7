dd96717b2904bd6bcb858c64f3c6a04fc8e468f9
jae woo Jung <awfjol2008@gmail.com>
2024-10-20 17:39:45 +0900

    refactor: 기존에 InputParser 클래스에 혼재됐던 검증 로직을 InputValidator 클래스로 이동

--- src/main/java/calculator/validation/InputValidator.java
+++ src/main/java/calculator/validation/InputValidator.java
@@ -9,4 +9,27 @@ public class InputValidator {
 	public void validateInput(String input) {
 		delimiters = Arrays.asList(",", ":");
 	}
+
+	public void validateInvalidDelimiter(String input) {
+		String regex = "[\\d" + String.join("", delimiters) + "]*";
+		String filteredInput = input.replaceAll(regex, "");
+
+		if (!input.matches(regex)) {
+			throw new IllegalArgumentException("허용되지 않은 구분자가 포함되어 있습니다: " + filteredInput);
+		}
+	}
+
+	public void validateDigit(String s) {
+		try {
+			Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("구분자 사이에 정수가 아닌 값이 포함되어 있습니다.");
+		}
+	}
+
+	public void validateNumberPositive(String s) {
+		if (Integer.parseInt(s) <= 0) {
+			throw new IllegalArgumentException("0 또는 음수가 포함되어 있습니다.");
+		}
+	}
 }
