2e368a95841f18c913c29bbc211cdaad580e9ba3
soeunnPark <carol0724@naver.com>
2024-10-20 15:56:28 +0900

    test(inputStringProcessor): update test method in InputStringProcessor
    Modified a test method for feature #2
    2.1. 커스텀 구분자가 있는지 확인한다.
    커스텀 구분자가 있는지 확인하는 hasCustomSeparator boolean 변수를 추가했습니다.
    
    Added a test case for feature #2.2.
    2.2. 구분자 리스트를 만든다.
    
    Added a test case for feature #2.3.
    2.3. 문자열에서 커스텀 구분자 형식을 제거한다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,12 +1,16 @@
 package calculator;
 
-import calculator.exception.SeparatorNotExistException;
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -20,41 +24,74 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
-    @Test
-    void 커스텀_구분자가_있는_경우_이를_제거하여_반환하는_테스트() {
-       String inputStr = "//+\n12,3+4:5";
-       InputStringProcessor inputStringProcessor = new InputStringProcessor();
-       String slicedInputStr = inputStringProcessor.removeFirstFiveCharacters(inputStr);
-       assertThat(slicedInputStr).isEqualTo("12,3+4:5");
+    @ParameterizedTest
+    @MethodSource("provideInputStrAndIfContainsCustomSeparator")
+    void 문자열에_커스텀_구분자가_존재하는지_확인하는_테스트(String inputStr, boolean hasCustomSeparator) {
+        InputStringProcessor inputStringProcessor = new InputStringProcessor();
+        assertThat(inputStringProcessor.checkIfInputStringContainsSeparator(inputStr)).isEqualTo(hasCustomSeparator);
     }
 
-    @Test
-    void 문자열의_첫5글자를_반환하는_테스트() {
-        String inputStr = "//+\n12,3+4:5";
-        InputStringProcessor inputStringProcessor = new InputStringProcessor();
-        String slicedInputStr = inputStringProcessor.getFirstFiveCharacters(inputStr);
-        assertThat(slicedInputStr).isEqualTo("//+\n");
+    private static Stream<Arguments> provideInputStrAndIfContainsCustomSeparator() {
+        return Stream.of(
+                Arguments.of("12345", false),
+                Arguments.of("123", false),
+                Arguments.of("//12345", false),
+                Arguments.of("123\n:,5", false),
+                Arguments.of("//+\1234:,", false),
+                Arguments.of("//,\n12,3+4:5", true),
+                Arguments.of("//:\n12,3+4:5", true),
+                Arguments.of("//+\n12,3+4:5", true),
+                Arguments.of("//-\n12,3+4:5", true)
+        );
     }
 
-    @Test
-    void 자른_5글자가_유효한_커스텀_구분자를_포함하는지_확인하고_커스텀_구분자를_반환하는데_성공하는_테스트() {
-        String firstFiveCharacters = "//+\n";
+    @ParameterizedTest
+    @MethodSource("provideInputStrAndExpectedSeparators")
+    void 구분자_리스트를_만드는_테스트(String inputStr, boolean hasCustomSeparator, Set<Character> expectedSeparators) {
         InputStringProcessor inputStringProcessor = new InputStringProcessor();
-        Character separator = inputStringProcessor.getSeparator(firstFiveCharacters);
-        assertThat(separator).isEqualTo('+');
+        assertThat(inputStringProcessor.getSeparatorList(hasCustomSeparator, inputStr)).isEqualTo(expectedSeparators);
     }
 
-    @Test
-    void 자른_5글자가_유효한_커스텀_구분자를_포함하는지_확인하고_형식이_맞지않아_예외가_발생하는_테스트() {
-        String firstFiveCharacters = "/*8/n";
+    private static Stream<Arguments> provideInputStrAndExpectedSeparators() {
+        Set<Character> defaultSeparators = Set.of(':', ',');
+        return Stream.of(
+                Arguments.of("12345", false, defaultSeparators),
+                Arguments.of("123", false, defaultSeparators),
+                Arguments.of("//12345", false, defaultSeparators),
+                Arguments.of("123\n:,5", false, defaultSeparators),
+                Arguments.of("//+\1234:,", false, defaultSeparators),
+                Arguments.of("//:\n12,3+4:5", true, defaultSeparators),
+                Arguments.of("//,\n12,3+4:5", true, defaultSeparators),
+                Arguments.of("//+\n12,3+4:5", true, Set.of(':', ',', '+')),
+                Arguments.of("//-\n12,3+4:5", true, Set.of(':', ',', '-'))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInputStrAndExpectedSlicedStr")
+    void 커스텀_문자열을_포함하는_경우_이를_제거하고_그렇지_않다면_문자열을_그대로_반환하는_테스트(String inputStr, boolean hasCustomSeparator,
+                                                            String slicedStr) {
         InputStringProcessor inputStringProcessor = new InputStringProcessor();
-        assertThatThrownBy(() -> inputStringProcessor.getSeparator(firstFiveCharacters))
-            .isInstanceOf(SeparatorNotExistException.class);
+        assertThat(inputStringProcessor.removeSeparatorForm(hasCustomSeparator, inputStr)).isEqualTo(slicedStr);
+    }
+
+    private static Stream<Arguments> provideInputStrAndExpectedSlicedStr() {
+        return Stream.of(
+                Arguments.of("12345", false, "12345"),
+                Arguments.of("123", false, "123"),
+                Arguments.of("//12345", false, "//12345"),
+                Arguments.of("123\n:,5", false, "123\n:,5"),
+                Arguments.of("//+\1234:,", false, "//+\1234:,"),
+                Arguments.of("//:\n12,3+4:5", true, "12,3+4:5"),
+                Arguments.of("//,\n12,3+4:5", true, "12,3+4:5"),
+                Arguments.of("//+\n12,3+4:5", true, "12,3+4:5"),
+                Arguments.of("//-\n12,3+4:5", true, "12,3+4:5")
+        );
     }
 
 
