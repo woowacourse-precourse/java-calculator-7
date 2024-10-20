9da81ab09551bc2e28d5b1c4f92ec13a7d016f3c
esfuerzo <dkdldrha@naver.com>
2024-10-21 08:14:21 +0900

    feat(calculator): 덧셈 계산기 만들기
    methods: 정규식처리 : 구분자들을 정규식 처리
             숫자 확인 : 양수인지 확인
             숫자 합 : 정규식 처리된 문자들을 합처리
    
    ref) 전반적인 리팩토링 필요

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,76 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringBuilder sb = new StringBuilder(); // 정규식 넣을 stringBuilder;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String s = Console.readLine();  // 문자열 입력받기
+        if (s.startsWith("//")) { // "//"로 시작했을 때
+            int start = 2; // "//" 이후 시작 위치
+            int end = s.indexOf("\\n"); // 첫 번째 "\n"을 찾고 '\'의 위치 반환
+            if (end == -1) { // "\n"가 없다면
+                throw new IllegalArgumentException("Invalid input."); //예외상황
+            }
+
+            String customSeparator = s.substring(start, end); // //와 \n사이 커스텀문자열
+            String tail = s.substring(end + 2); // \n뒤에 문자열
+            if (tail.isEmpty()) {
+                System.out.print("결과 : 0");
+                return;
+            }
+            expressionProcessing(sb, customSeparator);
+            sb.deleteCharAt(sb.length() - 1); // 마지막 | 제거
+            String[] split = tail.split(sb.toString());
+            System.out.print("결과 : " + sumNumber(split));
+        } else {
+            if (s.isEmpty()) {
+                System.out.print("결과 : 0");
+                return;
+            }
+            expressionProcessing(sb, ":");
+            expressionProcessing(sb, ",");
+            sb.deleteCharAt(sb.length() - 1); // 마지막 | 제거
+            String[] split = s.split(sb.toString());
+            System.out.print("결과 : " + sumNumber(split));
+        }
+    }
+
+    /*
+     숫자 합 method : string[]에 있는 것들을 int로 바꾸어서 합산
+     ref) integer가 아닌게 있으면 예외상황
+     */
+    public static int sumNumber(String[] strings) throws IllegalArgumentException {
+        return Arrays.stream(strings)
+                .filter(str -> !str.isEmpty()) // 빈 문자열 필터링 -> 빈 문자열은 sum이 0(조건)
+                .mapToInt(str -> {
+                    if (!isNumeric(str)) {
+                        throw new IllegalArgumentException("Invalid input.");
+                    }
+                    return Integer.parseInt(str);
+                })
+                .sum();
+    }
+
+    /*
+     숫자 확인 method : 양수인지 확인
+     */
+    public static boolean isNumeric(String str) {
+        return str.matches("\\d+");
+    }
+
+    /*
+     정규식처리 method : 구분자 정규식 처리
+     */
+    public static void expressionProcessing(StringBuilder sb, String s) {
+        String quote = Pattern.quote(s);
+        sb.append(quote).append("|");
     }
 }
+
+
+
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,26 +1,93 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
     @Test
-    void 커스텀_구분자_사용() {
+    void 커스텀_구분자_사용_X_양수_기본구분자_번갈아가면서_input() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
         });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_X_연속된_기본구분자_input() {
+        assertSimpleTest(() -> {
+            run("1,,:2:3,0");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_X_빈문자열_input() {
+
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_X_음수_input() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_사용_X_구분자외_문자_input() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,;2b,$3naa"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_사용_양수_기본구분자_번갈아가면서_input() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_빈문자열_input() {
+
+        assertSimpleTest(() -> {
+            run("//;\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_역슬레쉬n이_없는경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;1,2:3;4"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+    @Test
+    void 커스텀_구분자_사용_구분자외_input() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n1,2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("/wa\\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
@@ -28,4 +95,6 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
+
 }
