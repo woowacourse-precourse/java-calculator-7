338f72abe7ac4af5d322dbc47de8ff3bea0aef35
MinjuKwak01 <mkwak1125@gmail.com>
2024-10-20 16:16:19 +0900

    feat : 배열에 담긴 값들을 모두 더하는 기능

--- src/main/java/calculator/model/Delimiter.java
+++ src/main/java/calculator/model/Delimiter.java
@@ -26,6 +26,15 @@ public class Delimiter {
                 .collect(Collectors.toList());
     }
 
-
+    public int sum(List<Integer> numberArray) {
+        int sum = 0;
+        for (Integer number : numberArray) {
+            if (number < 0) {
+                throw new IllegalArgumentException("[Error] 음수는 입력할 수 없습니다.");
+            }
+            sum += number;
+        }
+        return sum;
+    }
 }
 
