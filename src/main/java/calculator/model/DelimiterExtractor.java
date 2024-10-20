fbad0fd16a16bb812d8a8e91792b361fc8571b0d
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:21:57 +0900

    :sparkles: feat(DelimiterExtractor): 커스텀 구분자가 있는 경우 기본 구분자(쉼표, 콜론)와 같이 반환

--- src/main/java/calculator/model/DelimiterExtractor.java
+++ src/main/java/calculator/model/DelimiterExtractor.java
@@ -1,9 +1,19 @@
 package calculator.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DelimiterExtractor {
     private static final String DEFAULT_DELIMITERS = ",|:";
 
     public String extractDelimiters(String input) {
+        if (input.startsWith("//")) {
+            Matcher matcher = Pattern.compile("//(.)\\\\n").matcher(input);
+            if (matcher.find()) {
+                String customDelimiter = matcher.group(1);
+                return Pattern.quote(customDelimiter) + "|" + DEFAULT_DELIMITERS;
+            }
+        }
         return DEFAULT_DELIMITERS;
     }
 }
