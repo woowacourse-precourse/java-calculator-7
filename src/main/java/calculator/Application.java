4074f21e891608287b716015f027aebefa39fb07
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-10-20 19:26:23 +0900

    :necktie: feat: add checkCustomSeparatorExist method

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,18 +3,29 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private static final String CUSTOM_SEPARATOR_START = "//";
+    private static final String CUSTOM_SEPARATOR_END = "\\n";
+
     public static void main(String[] args) {
         run();
     }
 
     public static void run(){
         String input = Console.readLine();
-
         inputValidationCheck(input);
     }
 
     public static void inputValidationCheck(String input){
         checkBlankExist(input);
+        if (checkCustomSeparatorExist(input)){
+            System.out.println("맞았습니다.");
+        }
+
+    }
+
+    public static boolean checkCustomSeparatorExist(String input) {
+        String customSeparatorEnd = input.substring(3,5);
+        return input.startsWith(CUSTOM_SEPARATOR_START) && customSeparatorEnd.equals(CUSTOM_SEPARATOR_END);
     }
 
     public static void checkBlankExist(String input){
