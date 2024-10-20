9b221c752a41590110c7d851934335d661cf6ecb
YWSIK <yuusik818@gmail.com>
2024-10-21 04:07:58 +0900

    feat(Application) : 음수처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -16,37 +16,55 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+
         // 1. 입력 받기
         try {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine();
 
-        if (input.isEmpty()) {
-            System.out.println("결과 : 0");
-            return;
-        }
+            if (input.isEmpty() || input == null) {
+                System.out.println("결과 : 0");
+                return;
+            }
 
-        // 2. 문자열 구분
-        String delimiter = ",|:";
+            // 2. 문자열 구분
+            String delimiter = ",|:";
 
-        System.out.println("결과 : " + input + delimiter);
+            // 2-2. 커스텀 구분자
+            if (input.startsWith("//")) {
+                int delimiterIndex = input.indexOf("\n");
 
-        // 3. 숫자 처리
-        String[] numbers = input.split(delimiter);
+                if (delimiterIndex == -1) {
+                    throw new IllegalArgumentException("잘못된 형식입니다.");
+                }
 
-        // 4. 덧셈
-        int sum = 0;
+                // 커스텀 구분자 추출
+                String customDelimiter = input.substring(2, delimiterIndex);
+                delimiter = customDelimiter;
+                input = input.substring(delimiterIndex + 1);
+            }
 
-        for (int i = 0; i < numbers.length; i++) {
-            sum += Integer.parseInt(numbers[i].trim());
-        } // 1,2:3
+            // 3. 숫자 처리
+            String[] numbers = input.split(delimiter);
 
-        // 5. 결과 출력
+            // 4. 덧셈
+            int sum = 0;
+            for (String number : numbers) {
+                sum += parsePositiveNumber(number);
+            } // 1,2:3   //;\n1;2;3
 
+            // 5. 결과 출력
             System.out.println("결과 : " + sum);
         } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 입력입니다.");
+            System.out.println(e.getMessage());
         }
     }
-}
 
+    private static int parsePositiveNumber(String number) {
+        int num = Integer.parseInt(number);
+        if (num < 0) {
+            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+        }
+        return num;
+    }
+}
