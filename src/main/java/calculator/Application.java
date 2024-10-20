9ab3064d1cd491d39b62c540de0052afc46b5044
zi-eunn <jieunkim1117@gmail.com>
2024-10-20 21:21:01 +0900

    feat(Application): 기본 문자열 계산기 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,29 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
+    public static int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        String[] nums = numbers.split("[,:]");
+        int sum = 0;
+
+        for (String num : nums) {
+            sum += Integer.parseInt(num.trim());
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = sc.nextLine();
+        int result = add(input);
+        System.out.println("결과 : " + result);
     }
 }
