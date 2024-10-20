92d9f180cdeb6157549564adf7ed6d9a7e71d8ff
dy102 <ekagnlek2@gmail.com>
2024-10-20 19:50:44 +0900

    feat: 숫자들의 합을 구하는 기능

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Calculator {
+    private int number = 0;
+
+    public void add(int number) {
+        this.number += number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
