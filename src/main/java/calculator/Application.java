9b6049a3be7b825fb24c8e0678273bae3faa7157
seonseon1515 <seonseon1515@gmail.com>
2024-10-20 15:50:24 +0900

    style: 공백 라인 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         // 문자열 입력 받기
         String inputStr = getInput();
 
@@ -16,7 +15,6 @@ public class Application {
         // 기본 구분자 리스트에 커스텀 구분자 추가하기
         String basicPolymerList = "[,:]";
         String customPolymerList = addPolymerList(customPolymer, basicPolymerList);
-
 
         // 입력한 문자열에서 구분자를 기준으로 숫자 문자열 분리시키기
         String[] inputStrList = separateStr(inputStr, customPolymer, basicPolymerList, customPolymerList);
@@ -41,68 +39,65 @@ public class Application {
     }
 
     // 문자열 입력 받기
-    private static String getInput(){
+    private static String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 
     // 입력한 문자열에서 기본 구분자, 커스텀 구분자 찾아내기
-    private static String findCustomPolymer(String inputStr){
+    private static String findCustomPolymer(String inputStr) {
         String customPolymerPattern = "//[^\\w\\s]";
-
         String customPolymer = "";
 
         Pattern pattern = Pattern.compile(customPolymerPattern);
         Matcher matcher = pattern.matcher(inputStr);
 
-        if(matcher.find() && inputStr.substring(3,5).equals("\\n")){
-            customPolymer = inputStr.substring(2,3);
+        if (matcher.find() && inputStr.substring(3, 5).equals("\\n")) {
+            customPolymer = inputStr.substring(2, 3);
         }
 
         return customPolymer;
     }
 
-    private static String addPolymerList(String customPolymer, String basicPolymerList){
-        // 기본 구분자 리스트에 커스텀 구분자 추가하기
+    // 기본 구분자 리스트에 커스텀 구분자 추가하기
+    private static String addPolymerList(String customPolymer, String basicPolymerList) {
         String customPolymerList = "";
-        if(customPolymer.length() > 0){
-            customPolymerList = basicPolymerList.substring(0,basicPolymerList.length()-1) + customPolymer + "]";
+        if (customPolymer.length() > 0) {
+            customPolymerList = basicPolymerList.substring(0, basicPolymerList.length() - 1) + customPolymer + "]";
         }
         return customPolymerList;
     }
 
     // 입력한 문자열에서 구분자를 기준으로 숫자 문자열 분리시키기
     private static String[] separateStr(String inputStr, String customPolymer,
-                                        String basicPolymerList, String customPolymerList){
+                                        String basicPolymerList, String customPolymerList) {
         String[] inputStrList;
-
-        if(customPolymer.length() == 1){
-            inputStrList = inputStr.substring(5,inputStr.length()).split(customPolymerList);
+        if (customPolymer.length() == 1) {
+            inputStrList = inputStr.substring(5).split(customPolymerList);
         } else {
             inputStrList = inputStr.split(basicPolymerList);
         }
-
         return inputStrList;
     }
 
     // 예외처리 -1: 문자열 숫자가 아닌 경우
-    private static void isStrNum(boolean isStringNumber, String[] inputStrList){
+    private static void isStrNum(boolean isStringNumber, String[] inputStrList) {
         for (String s : inputStrList) {
-            if(!s.matches("\\d+")){
+            if (!s.matches("\\d+")) {
                 isStringNumber = false;
             }
         }
-        if(!isStringNumber){
+        if (!isStringNumber) {
             throw new IllegalArgumentException("숫자 제외 입력 금지");
         }
     }
 
     // 분리된 문자열 숫자로 변환하기
-    private static void strToInt(int[] strToIntList, String[] inputStrList, boolean isMinus){
+    private static void strToInt(int[] strToIntList, String[] inputStrList, boolean isMinus) {
         int index = 0;
         for (String str : inputStrList) {
             strToIntList[index] = Integer.parseInt(str);
-            if (strToIntList[index] < 0){
+            if (strToIntList[index] < 0) {
                 isMinus = true;
             }
             index++;
@@ -110,14 +105,14 @@ public class Application {
     }
 
     // 예외 처리 -2 : 음수인 경우
-    private static void checkMinusNum(boolean isMinus){
-        if(isMinus){
+    private static void checkMinusNum(boolean isMinus) {
+        if (isMinus) {
             throw new IllegalArgumentException("음수 입력 불가");
         }
     }
 
     // 변환된 숫자를 더하기
-    private static int sumNumList(int[] strToIntList){
+    private static int sumNumList(int[] strToIntList) {
         int sum = 0;
         for (int number : strToIntList) {
             sum += number;
