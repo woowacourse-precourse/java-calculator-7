5784f5d38c5b1b456cafcbeb4c5e89dc66f2c402
ljhee92 <weilainib7@gmail.com>
2024-10-20 23:15:37 +0900

    test: 커스텀구분자 백슬래시 허용, SeparatorsService의 구분자부분 분리 메서드 활용

--- src/test/java/calculator/model/CustomSeparatorsTest.java
+++ src/test/java/calculator/model/CustomSeparatorsTest.java
@@ -1,5 +1,7 @@
 package calculator.model;
 
+import calculator.service.SeparatorsService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,18 +13,24 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static calculator.constant.Message.EMPTY;
-import static calculator.constant.Message.SLASH;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("CustomSeparators 객체 테스트")
 public class CustomSeparatorsTest {
+    private SeparatorsService separatorsService;
+
+    @BeforeEach
+    void setUp() {
+        separatorsService = new SeparatorsService();
+    } // setUp
+
     @ParameterizedTest(name = "{index} : {3}")
     @DisplayName("CustomSeparators 객체가 숫자를 잘 구분하는지 확인")
     @MethodSource("generateSuccessData")
     void testCorrectSeparated(String separatePart, String calculatePart, List<Integer> output, String message) {
-        List<String> inputCustomSeparators = Arrays.stream(separatePart.replaceAll(SLASH, EMPTY)
-                .replaceAll("\\\\n", EMPTY).split(EMPTY)).toList();
+         List<String> inputCustomSeparators = Arrays.stream(separatorsService.getSeparatePart(separatePart)
+                                                                            .split(EMPTY)).toList();
         CustomSeparators customSeparators = new CustomSeparators(inputCustomSeparators);
 
         assertThat(customSeparators.separateNumbers(calculatePart)).isEqualTo(output);
@@ -41,8 +49,8 @@ public class CustomSeparatorsTest {
     @DisplayName("CustomSeparators 객체 생성 예외 확인")
     @MethodSource("generateExceptionData")
     void testCreateCustomSeparatorsException(String separatePart, String message) {
-        List<String> inputCustomSeparators = Arrays.stream(separatePart.split(EMPTY)).toList();
-
+        List<String> inputCustomSeparators = Arrays.stream(separatorsService.getSeparatePart(separatePart)
+                                                                            .split(EMPTY)).toList();
         assertThatThrownBy(() -> new CustomSeparators(inputCustomSeparators))
                 .isInstanceOf(IllegalArgumentException.class);
     } // testCreateCustomSeparatorsException
@@ -50,7 +58,6 @@ public class CustomSeparatorsTest {
     static Stream<Arguments> generateExceptionData() {
         return Stream.of(
                 Arguments.of("//\\n", "공백일 때"),
-                Arguments.of("//\\\\n", "\\일 때"),
                 Arguments.of("//0\\n", "숫자일 때"),
                 Arguments.of("//!@#\\n", "커스텀 구분자의 개수가 2개 초과일 때")
                 );
@@ -59,8 +66,8 @@ public class CustomSeparatorsTest {
     @Test
     @DisplayName("지정한 커스텀 구분자 이외의 문자가 포함되어 있을 때 예외 확인")
     void testContainsOtherCharacters() {
-        List<String> inputCustomSeparators = Arrays.stream("//!\\n".replaceAll(SLASH, EMPTY)
-                .replaceAll("\\\\n", EMPTY).split(EMPTY)).toList();
+        List<String> inputCustomSeparators = Arrays.stream(separatorsService.getSeparatePart("//!\\n")
+                                                                            .split(EMPTY)).toList();
         CustomSeparators customSeparators = new CustomSeparators(inputCustomSeparators);
 
         assertThatThrownBy(() -> customSeparators.separateNumbers("1,2!3"))
