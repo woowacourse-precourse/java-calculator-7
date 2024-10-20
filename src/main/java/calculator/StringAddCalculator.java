7e434855fa3b2078210fc1fa40705ba8d98a8776
SeuneKeun <eiziong1@gmail.com>
2024-10-20 21:25:46 +0900

    feat: 커스텀 구분자로 문자열 분리 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,19 +1,32 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    private static final String PATTERN = ",|:";
+    private static final String PATTERN = "[,:]";
+    private static final String CUSTOM_PATTERN = "//(.)\\\\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_PATTERN);
+    private static final int PATTERN_IN_GROUP = 1;
+    private static final int NUMBERS_IN_GROUP = 2;
 
     public int calculate(String input) {
         return splitAndSumNumber(input);
     }
 
-    public int splitAndSumNumber(String input) {
+    private int splitAndSumNumber(String input) {
         return Arrays.stream(splitString(input)).mapToInt(Integer::parseInt).sum();
     }
 
-    public String[] splitString(String input) {
-        return input.split(PATTERN);
+    private String[] splitString(String input) {
+        String pattern = PATTERN;
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+
+        if(matcher.find()) {
+            pattern = matcher.group(PATTERN_IN_GROUP);
+            input = matcher.group(NUMBERS_IN_GROUP);
+        }
+        return input.split(pattern);
     }
 }
