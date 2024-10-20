87e80d9a0852788fd7b882ac3101e7a5d9734b0b
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-20 10:59:14 +0900

    test : ResultDTO 추가에 따른 테스트 코드 수정

--- src/test/java/calculator/controller/CalculatorControllerTests.java
+++ src/test/java/calculator/controller/CalculatorControllerTests.java
@@ -2,6 +2,7 @@ package calculator.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import calculator.dto.ResultDTO;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,21 +15,17 @@ class CalculatorControllerTests {
     @DisplayName("입력한 문자열의 덧셈 결과가 정수")
     @CsvSource(value = {"1:2:3", "1.0:2.0:3.0"})
     public void testIfResultInteger(String input) {
-        String result1 = CalculatorController.sum(BigDecimal.ZERO, input);
-        String result2 = CalculatorController.sum(0, input);
+        ResultDTO<BigDecimal> result = CalculatorController.sum(BigDecimal.ZERO, input);
 
-        assertThat(String.format("결과: %s", result1)).contains("결과: 6");
-        assertThat(String.format("결과: %s", result2)).contains("결과: 6");
+        assertThat(String.format("결과: %s", result.getTotal())).contains("결과: 6");
     }
 
     @Test
     @DisplayName("입력한 문자열의 덧셈 결과가 소수점 이하 자릿수가 존재하는 실수")
     public void testIfResultDecimal() {
         String input = "1.1:2.2:3.3";
-        String result1 = CalculatorController.sum(BigDecimal.ZERO, input);
-        String result2 = CalculatorController.sum(0, input);
+        ResultDTO<BigDecimal> result = CalculatorController.sum(BigDecimal.ZERO, input);
 
-        assertThat(String.format("결과: %s", result1)).contains("결과: 6.6");
-        assertThat(String.format("결과: %s", result2)).contains("결과: 6.6");
+        assertThat(String.format("결과: %s", result.getTotal())).contains("결과: 6.6");
     }
 }
