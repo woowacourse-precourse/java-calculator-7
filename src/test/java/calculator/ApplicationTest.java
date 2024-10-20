321d9fe9411ef5a1aeb2086a4740c0c91189a71e
JJadeYoon <yess9904@naver.com>
2024-10-21 04:14:50 +0900

    feat(ApplicationTest.java): 테스트 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,49 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 빈_문자열_입력() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException(" "))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_입력_예외() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//;1"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 음수값_입력() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("-1,2,3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+
+            assertThatThrownBy(() -> runException("//;\n1;-2;3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 커스텀_구분자에_이스케이프__문자() {
+        assertSimpleTest(() -> {
+            run("//\\\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
