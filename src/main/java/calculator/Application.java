62daa858dc229ad5161da2c11e244d244bc4c7f2
MinJae00 <exceedmin@gmail.com>
2024-10-21 02:00:29 +0900

    refact: main 함수에서 인스턴스 호출부분 다 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,26 +1,11 @@
 package calculator;
-
-import calculator.calculate.Calculate;
-import calculator.calculate.ParsingString;
-import calculator.exception.Exception;
 import calculator.global.GlobalConstant;
 import calculator.view.InputView;
-import calculator.view.OutputView;
-
-import java.util.ArrayList;
-
 public class Application {
     public static void main(String[] args) {
         GlobalConstant.StartIndex = 0;
-        ArrayList<Integer> numlist;
         InputView input_view = new InputView();
-        OutputView output_view = new OutputView();
-        String input = input_view.getInput();
-        Exception.IsEmptyInput(input);
-        ParsingString check_delemeter = new ParsingString(input);
-        check_delemeter.AddDelemeter();
-        numlist = check_delemeter.MakeNumList();
-        int result = Calculate.Makesum(numlist);
-        output_view.output(result);
     }
 }
+
+
