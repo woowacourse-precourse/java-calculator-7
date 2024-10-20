4201129e7ba86d1f7cc07ac7d0a22e9b09e5d534
HEX <123macanic@naver.com>
2024-10-20 12:38:12 +0900

    feat: 입력 유효성 검사 메서드 구현
    추가된 기능
    - 입력한 문자열에 대해서 입력 요구사항을 잘 지켰는지 검사하고, 요구사항과 다를 경우 IllegalArgumentException 예외를 발생시키는 메서드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = Input.inputString();
+        String numberPart = Input.validateInput(input);
     }
 }
 
@@ -13,5 +14,23 @@ class Input {
     public static String inputString() {
         System.out.println("Please enter the strings to add.");
         return Console.readLine();
+    }
+
+    public static String validateInput(String input) {
+        if(input.isBlank())
+            throw new IllegalArgumentException("Blank InputString Error");
+
+        if(input.startsWith("//") && input.contains("\\n")) {
+            String delimiterSection = input.substring(2, input.indexOf("\\n"));
+            if(delimiterSection.length() == 1)
+                return input.substring(input.indexOf("\\n") + 2);
+            else
+                throw new IllegalArgumentException("Separator format is invalid");
+        }
+        else if (input.charAt(0) >= '0' && input.charAt(0) <= '9') {
+            return input;
+        }
+        else
+            throw new IllegalArgumentException("Invalid InputString Error");
     }
 }
