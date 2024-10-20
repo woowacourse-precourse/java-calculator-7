72e18a4a81e6ea0d3f7f3ac1fd3b3d04e2bacf5f
kwongwangjae <kgj9906@naver.com>
2024-10-20 14:24:52 +0900

    feat(메인 화면): 시작, 결과 문구 출력 화면에 보이는 입력, 결과 문구 기능 구현

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,13 @@
+package calculator.controller;
+
+import calculator.view.View;
+
+public class CalculatorController {
+	public CalculatorController() {
+	}
+
+	public void run() {
+		View.InputMessage();
+		View.OutputMessage();
+	}
+}
---
+++ src/main/java/calculator/view/View.java
@@ -0,0 +1,18 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class View {
+	private static final String Input = "덧셈할 문자열을 입력해 주세요.";
+	private static final String Output= "결과 : ";
+
+	public static void InputMessage() {
+		System.out.println(Input);
+	}
+
+	public static void OutputMessage() {
+		System.out.println(Output);
+	}
+
+
+}
