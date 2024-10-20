6084b0ea498c2ea34f4e96981d95097b85e658c3
jieun <ju3391@naver.com>
2024-10-20 20:13:31 +0900

    refactor(InputValidator.java): 상수 도입으로 코드 개선
    - 기본 구분자와 커스텀 구분자, 개행 문자를 상수로 정의
    - 상수 사용을 통해 코드의 가독성과 유지보수성을 향상

--- src/main/java/calculator/model/InputValidator.java
+++ src/main/java/calculator/model/InputValidator.java
@@ -5,13 +5,18 @@ import java.util.regex.Pattern;
 // 입력 검증 클래스
 public class InputValidator {
 
+    // 상수 정의 (기본 구분자, 커스텀 구분자, 개행 문자)
+    private static final String DEFAULT_DELIMITER_REGEX = ",|:";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String NEWLINE = "\\n";
+
     // 입력을 받아 검증하고 숫자 배열을 반환하는 메서드
     public String[] validate(String input) {
         if(input == null || input.isEmpty()) {
             return new String[] {"0"}; // 빈 입력일 경우 기본값 반환
         }
 
-        if (input.startsWith("//")) {
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
             return validateCustomDelimiter(input); // 커스텀 구분자 처리
         } else {
             return validateDefaultDelimiter(input); // 기본 구분자(쉼표, 콜론) 처리
@@ -20,14 +25,14 @@ public class InputValidator {
 
     // 기본 구분자(쉼표, 콜론)로 입력을 처리하는 메서드
     private String[] validateDefaultDelimiter(String input) {
-        String[] numbers = input.split(",|:");
+        String[] numbers = input.split(DEFAULT_DELIMITER_REGEX);
         checkNegativeNumbers(numbers); // 음수 검증 수행
         return numbers;
     }
 
     // 커스텀 구분자 처리 및 검증하는 메서드
     private String[] validateCustomDelimiter(String input) {
-        int delimiterIndex = input.indexOf("\\n");
+        int delimiterIndex = input.indexOf(NEWLINE);
 
         // 구분자 포맷이 잘못된 경우 예외 발생
         if (delimiterIndex == -1 ) {
