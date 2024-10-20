827ace72c7967f9c9f6c8f82e3f9b60963f2e01a
GiSeok Kang <ime.rokz@gmail.com>
2024-10-20 15:02:00 +0900

    test: test 코드 @ParameterizedTest로 변경

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ApplicationTest extends NsTest {
 
@@ -33,82 +35,83 @@ class ApplicationTest extends NsTest {
         });
     }
 
-    @Test
-    void 기본구분자_정상_입력() {
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2:3", "1:2:7", "12,3,2", "13:43,54"})
+    void 기본구분자_정상_입력(String input) {
         assertSimpleTest(() -> {
-            run("1,2:3");
-            assertThat(output()).contains("결과 : 6");
+            run(input);
+            assertThat(output()).contains("결과 : ");
         });
     }
 
-    @Test
-    void 기본구분자_음수_입력() {
+    @ParameterizedTest
+    @ValueSource(strings = {"-1,2,3", "1:-3,4", "1,-4,-2"})
+    void 기본구분자_음수_입력(String input) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("-1,2,3"))
+                assertThatThrownBy(() -> runException(input))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
-    @Test
-    void 기본구분자_사이_숫자_아닌_경우() {
+    @ParameterizedTest
+    @ValueSource(strings = {"1:d,4", "f,3,s", "d,2:3"})
+    void 기본구분자_사이_숫자_아닌_경우(String input) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1:d,4"))
+                assertThatThrownBy(() -> runException(input))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
-    @Test
-    void 기본구분자_잘못된_사용() {
+    @ParameterizedTest
+    @ValueSource(strings = {"1-3:4", "1;6;8", "1:4;3"})
+    void 기본구분자_잘못된_사용(String input) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1-3:4"))
+                assertThatThrownBy(() -> runException(input))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
-    @Test
-    void 기본구분자_두개_연속_사용() {
+    @ParameterizedTest
+    @ValueSource(strings = {"1::3:4", "2,,4::5", "2,,,,,,3:4", "1:2:3,,,,:::5"})
+    void 기본구분자_두개_연속_사용(String input) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1::3:4"))
+                assertThatThrownBy(() -> runException(input))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
-    @Test
-    void 커스텀구분자_정상_입력() {
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\\n1;2;3", "//d\\n1d2d3", "//9\\n19293", "//2\\n1125523"})
+    void 커스텀구분자_정상_입력(String input) {
         assertSimpleTest(() -> {
-            run("//;\\n1;2;3");
-            assertThat(output()).contains("결과 : 6");
+            run(input);
+            assertThat(output()).contains("결과 : ");
         });
     }
 
-    @Test
-    void 커스텀구분자_끝_잘못_입력() {
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\\m1;2;3", "//c\\2,3:4", "//9\n19293"})
+    void 커스텀구분자_끝_잘못_입력(String input) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("//;\\m1;2;3"))
+                assertThatThrownBy(() -> runException(input))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
-    @Test
-    void 커스텀구분자_문자가_아닌_문자열_입력() {
+    @ParameterizedTest
+    @ValueSource(strings = {"//;;\\n1;2;3", "//asd\\n1asd2asd3"})
+    void 커스텀구분자_문자가_아닌_문자열_입력(String input) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("//;;\\n1;2;3"))
+                assertThatThrownBy(() -> runException(input))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
-    @Test
-    void 커스텀구분자_숫자인_경우_입력() {
-        assertSimpleTest(() -> {
-            run("//9\\n19293");
-            assertThat(output()).contains("결과 : 6");
-        });
-    }
-
-    @Test
-    void 커스텀구분자_숫자인_경우_연속입력() {
+    @ParameterizedTest
+    @ValueSource(strings = {"//9\\n19993", "//2\\n1229,3"})
+    void 커스텀구분자_숫자인_경우_연속입력(String input) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("//9\\n19993"))
+                assertThatThrownBy(() -> runException(input))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
@@ -121,19 +124,21 @@ class ApplicationTest extends NsTest {
         });
     }
 
-    @Test
-    void 기본_한자리수_이상_입력() {
+    @ParameterizedTest
+    @ValueSource(strings = {"146,312,123", "31235:3122,123"})
+    void 기본_한자리수_이상_입력(String input) {
         assertSimpleTest(() -> {
-            run("10,20:33");
-            assertThat(output()).contains("결과 : 63");
+            run(input);
+            assertThat(output()).contains("결과 : ");
         });
     }
 
-    @Test
-    void 커스텀_한자리수_이상_입력() {
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\\n10;20;33", "//7\\n14723746"})
+    void 커스텀_한자리수_이상_입력(String input) {
         assertSimpleTest(() -> {
-            run("//;\\n10;20;33");
-            assertThat(output()).contains("결과 : 63");
+            run(input);
+            assertThat(output()).contains("결과 : ");
         });
     }
 
