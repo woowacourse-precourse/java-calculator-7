ea1e45459540519e126728e950e935f696dd9a37
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-10-20 13:39:50 +0900

    refactor : 테스트 이름 변경

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -26,13 +26,13 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 음수_입력_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
-
+    
     @Override
     public void runMain() {
         Application.main(new String[]{});
