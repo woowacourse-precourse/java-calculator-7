d47191c1e3dca5f115e80e1980c52e0b7c0e8c5d
jjjjjoon <mjj7343@gmail.com>
2024-10-20 20:37:21 +0900

    refactor(Application): InputHandler클래스 삭제
    의존성 주입되어있던 InputHandler클래스 삭제 후 run()메소드에 직접 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,26 +2,26 @@ package calculator;
 
 import calculator.util.StringSplitter;
 import calculator.util.StringValidator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    private final InputHandler inputhandler;
     private final ArrayCalculator arraycalculator;
     private static final StringValidator validator = new StringValidator();
     private static final StringSplitter splitter = new StringSplitter(validator);
 
-    public Application(InputHandler inputhandler, ArraySumCalculator arraysumcalculator) {
-        this.inputhandler = inputhandler;
+    public Application(ArraySumCalculator arraysumcalculator) {
         this.arraycalculator = arraysumcalculator;
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Application application = new Application(new InputHandler(), new ArraySumCalculator(splitter));
+        Application application = new Application(new ArraySumCalculator(splitter));
         application.run(application);
     }
 
     public void run(Application application) {
-        String input = inputhandler.userInput();
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = Console.readLine();
         int result = arraycalculator.sumArrayElements(input);
         System.out.println("결과 : " + result);
     }
