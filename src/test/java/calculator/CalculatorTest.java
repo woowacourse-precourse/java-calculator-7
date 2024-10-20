c4d78fc72db2cde012b48fa58a0872de0aac8d2f
Park, Sojeong <sojeong2101@gmail.com>
2024-10-20 17:37:33 +0900

    test($CalculatorTest): 커스텀 구분자 여러 개 사용 테스트 추가

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest extends NsTest {
+    @Test
+    void 커스텀_구분자_여러_개_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1,2;3,4,//-\\n4-2");
+            assertThat(output()).contains("결과 : 16");
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
