94f50663889d2d34dfe83682df8786116ef907c1
jhseob <jhseob06@gmail.com>
2024-10-21 08:00:35 +0900

    refactor: 입력 방식 Console.readLine()으로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,22 +3,26 @@ package calculator;
 import calculator.cal.Calculator;
 import calculator.cal.CalculatorImpl;
 import calculator.cal.numberadder.string.StringNumberAdder;
-import java.io.BufferedReader;
+import camp.nextstep.edu.missionutils.Console;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.NoSuchElementException;
 
 public class Application {
     public static void main(String[] args) throws IOException {
         Calculator calculator = new CalculatorImpl(new StringNumberAdder());
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         //문자열 받기
         writer.write("덧셈할 문자열을 입력해 주세요.\n");
         writer.flush();
-        String inputString = reader.readLine();
+        String inputString;
+        try {
+            inputString = Console.readLine();
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("문자열을 입력하지 않았습니다.");
+        }
 
         //결과 처리
         int result = calculator.add(inputString);
