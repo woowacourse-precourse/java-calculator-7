5205753a6dfc8c0d57c32b1199bf835d12b843ce
theplayer1003 <theplayer1003@gmail.com>
2024-10-20 21:12:41 +0900

    feat(Calculator): implement calculator for sum
    implement:
    - getSum
    - makeSplitNumbers

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,33 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Calculator {
+    private String[] splitNumbers;
+    private int sum;
+
+    public int getSum(String parsedUserInput, List<Character> delimiter){
+        makeSplitNumbers(parsedUserInput, delimiter);
+//        for(String number : splitNumbers){
+//            sum += Integer.parseInt(number);
+//        }
+
+        return sum;
+    }
+
+    private void makeSplitNumbers(String parsedUserInput, List<Character> delimiter){
+        StringBuilder sb = new StringBuilder();
+        this.sum = 0;
+
+        for(int i=0;i<parsedUserInput.length();i++){
+            if(!delimiter.contains(parsedUserInput.charAt(i))){
+                sb.append(parsedUserInput.charAt(i));
+            }else{
+                if(sb.length()==0) continue;
+                sum += Integer.parseInt(sb.toString());
+                sb.delete(0, sb.length());
+            }
+        }
+    }
 }
