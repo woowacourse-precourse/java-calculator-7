0eca3755d95f9dd9e12e1dc9252f637690dccfcb
songkyung <min.songkyung@gmail.com>
2024-10-20 20:53:53 +0900

    잘못된 값 입력 시 예외처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,9 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -12,6 +15,17 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
+
+        // 사용자가 잘못된 값을 입력했을 경우, 음수를 입력하거나 문자를 입력했을 경우
+        try {
+            int answer = calculate(str);
+            System.out.println("결과 : " + answer);
+        } catch (IllegalArgumentException e) {
+            // 잘못된 값을 입력하는 예외가 발생한 경우, 애플리케이션 종료시킴
+            System.out.println("잘못된 입력 : " + e.getMessage());
+        }
+    }
+    public static int calculate(String str) {
 
         DelimiterType strType = getType(str);
 
@@ -39,10 +53,21 @@ public class Application {
                 break;
         }
 
-        for (int i=0; i<parts.length; i++) {
-            sum += Integer.parseInt(parts[i]);
+        for (String part: parts) {
+            if (!part.isEmpty()) {
+                try {
+                    int positive_value = Integer.parseInt(part);
+                    if (positive_value < 0) {
+                        // 입력값이 양수가 아닌 경우의 예외 처리
+                        throw new IllegalArgumentException("양수가 아닌 값이 입력되었습니다. " + positive_value);
+                    }
+                    sum += positive_value;
+                } catch (NumberFormatException e){
+                    throw new IllegalArgumentException("잘못된 값이 입력되었습니다. " + part);
+                }
+            }
         }
-        System.out.println("결과 : " + sum);
+        return sum;
     }
 
     private static DelimiterType getType(String str) {
