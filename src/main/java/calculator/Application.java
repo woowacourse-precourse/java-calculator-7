cc0cd08cd80d2e6e98b4baa6f9821859ae39ebbf
yel106 <yel_@hanmail.net>
2024-10-20 15:54:35 +0900

    Custom구분자 추출 : 기본구분자 외에 '//'로 시작하는 커스텀구분자가 있는 경우, 분리 후 separator List에 넣음

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,6 +15,21 @@ public class Application {
             throw new IllegalArgumentException("입력하신 값이 없습니다.");
         }
 
+        List<String> separator = new ArrayList<>();
+        separator.add(",");
+        separator.add(":");
+
+        extractCustomSep(input, separator);
 
     }
+
+    // 커스텀 구분자(Separator, Sep)를 추출
+    private static void extractCustomSep(String input, List<String> separator) {
+        if (input.startsWith("//")) {
+            int sepEndIndex = input.indexOf("\\n");
+            String customSep = input.substring(2, sepEndIndex - 1);
+            separator.add(customSep);
+        }
+    }
+
 }
