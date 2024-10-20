7a3f2a855bddbbd17b390b79dc6c7603eafd998f
kellyihyeon <kellykang13@gmail.com>
2024-10-20 17:09:16 +0900

    feat: 구분자 문자열에서 숫자를 추출하는 기능 구현

---
+++ src/main/java/calculator/NumberExtractor.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NumberExtractor {
+
+    private String text;
+
+    public NumberExtractor(String input) {
+        this.text = input;
+    }
+
+    public List<Double> extract(String regex) {
+        String numberLeft = this.text.replaceAll(regex, " ");
+
+        return Arrays.stream(numberLeft.split(" "))
+                .mapToDouble(Double::valueOf)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/calculator/NumberExtractorTest.java
@@ -0,0 +1,38 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class NumberExtractorTest {
+
+    @Test
+    void 커스텀_구분자_문자열에서_숫자를_추출한다() {
+        String customDelimiterRegex = ";";
+        String input = "//;\n4;5;6";
+
+        input = input.split("\n")[1];
+        NumberExtractor numberExtractor = new NumberExtractor(input);
+        List<Double> numberLeft = numberExtractor.extract(customDelimiterRegex);
+
+        List<Double> expected = Arrays.asList(4.0, 5.0, 6.0);
+        boolean maybeTrue = Arrays.equals(expected.toArray(), numberLeft.toArray());
+        assertTrue(maybeTrue);
+
+    }
+
+    @Test
+    void 기본_구분자_문자열에서_숫자를_추출한다() {
+        String basicDelimiterRegex = ",|:";
+        String input = "1,2:3";
+
+        NumberExtractor numberExtractor = new NumberExtractor(input);
+        List<Double> numberLeft = numberExtractor.extract(basicDelimiterRegex);
+
+        List<Double> expected = Arrays.asList(1.0, 2.0, 3.0);
+        boolean maybeTrue = Arrays.equals(expected.toArray(), numberLeft.toArray());
+        assertTrue(maybeTrue);
+    }
+}
