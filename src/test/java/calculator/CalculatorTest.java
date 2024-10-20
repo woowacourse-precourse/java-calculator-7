150e929428fe56a925cf129dc0cac171d7b6d949
Dev-LHY <dev.leehy@gmail.com>
2024-10-20 15:24:40 +0900

    test: null에 대한 예외처리 단위 테스트 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -10,7 +10,16 @@ class CalculatorTest {
     @Test
     void 공백_예외_테스트() {
         assertThatThrownBy(() -> {
-            InputValidationException.isEmpty(" ");
+            InputValidationException.isEmpty("");
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("아무것도 입력하지 않았습니다. 애플리케이션을 종료합니다.");
+    }
+
+    @Test
+    void null_예외_테스트() {
+        assertThatThrownBy(() -> {
+            InputValidationException.isEmpty(null);
         })
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("아무것도 입력하지 않았습니다. 애플리케이션을 종료합니다.");
