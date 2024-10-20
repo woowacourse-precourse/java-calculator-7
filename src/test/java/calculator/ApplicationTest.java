435644e19470445fd35944c39d2222e0ca981f85
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-10-20 23:18:30 +0900

    test: 종류별 커스텀 구분자와 양수 하나를 쓰는 테스트 코드 구현

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -2,6 +2,8 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,13 +21,27 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "//'\\n1", "//\"\\n1", "//,\\n1", "//;\\n1", "//:\\n1", "//-\\n1", "//.\\n1", "//*\\n1","//_\\n1",
+            "//+\\n1", "//?\\n1", "//[\\n1", "//]\\n1", "//{\\n1", "//}\\n1"
+            ,"//(\\n1","//)\\n1","//|\\n1","//^\\n1","//$\\n1","//!\\n1","//@\\n1","//#\\n1", "//&\\n1","//`\\n1",
+            "//~\\n1","//<\\n1","//>\\n1","///\\n1","//\\\\n1"
+    })
+    void 커스텀_구분자_단독(String input) throws Exception {
+        assertSimpleTest(() -> {
+            run(input);
+            assertThat(output()).contains("결과 : 1");
+        });
     }
 }
