2dc5b35339e804dfcaec37c571ed118903eb1be4
seophohoho <seop0937@gmail.com>
2024-10-20 15:29:12 +0900

    feat(Validator): 올바른 커스텀 구분자 생성 컨벤션이 아닐 시, 에러 처리 추가

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -5,5 +5,13 @@ public class Validator {
         if (input.contains("-")) {
             throw new IllegalArgumentException("ㅁㅊ음? 음수를 넣는다고?");
         }
+
+        if (input.startsWith(Constants.CUSTOM_SEPARATOR_PREFIX) && !input.contains(Constants.CUSTOM_SEPARATOR_SUFFIX)) {
+            throw new IllegalArgumentException("커스텀 구분자 지정 컨벤션이 올바르지 않아요");
+        }
+
+        if (!input.startsWith(Constants.CUSTOM_SEPARATOR_PREFIX) && input.contains(Constants.CUSTOM_SEPARATOR_SUFFIX)) {
+            throw new IllegalArgumentException("커스텀 구분자 지정 컨벤션이 올바르지 않아요");
+        }
     }
 }
