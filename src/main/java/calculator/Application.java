aec144c24fcf8c1782a80d73a4bac215b84a9b8a
yuntasha <jaljayo85@gmail.com>
2024-10-20 15:53:41 +0900

    feat(Application.java): add extract method
    add new method:
    - add extract method

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,7 +3,9 @@ package calculator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class Application {
 
@@ -19,12 +21,13 @@ public class Application {
         var result = 0;
 
         var now = bf.readLine();
+
         while (isCommand(now)) {
             break;
         }
     }
 
-    static private boolean isCommand(String s) throws IllegalArgumentException {
+    static private boolean isCommand(String s) {
         if (s.charAt(0)=='/' && s.charAt(1)=='/') {
             return false;
         } else if (isNum(s.charAt(0)) || delimiters.contains((int)s.charAt(0))) {
@@ -50,5 +53,27 @@ public class Application {
         }
 
         delimiters.add(n);
+    }
+
+    static List<String> extract(String s) {
+        List<String> result = new ArrayList<>();
+
+        int start = 0;
+
+        for (int i=0; i<s.length(); i++) {
+            int now = s.charAt(i);
+            if (delimiters.contains(now)) {
+                result.add(s.substring(start, i));
+                start = i+1;
+            } else if (!isNum(s.charAt(i))) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        if (start != s.length()) {
+            result.add(s.substring(start));
+        }
+
+        return result;
     }
 }
