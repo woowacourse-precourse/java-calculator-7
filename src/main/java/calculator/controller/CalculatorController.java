b90281521bec0611e13d14bbd816ad0bf74562fe
dnwldla <leeemjw@naver.com>
2024-10-20 15:13:25 +0900

    refactor: controller 내부 실행 메소드 수정

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,6 +1,7 @@
 package calculator.controller;
 
 import calculator.domain.Calculator;
+import calculator.domain.UserInput;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
@@ -9,7 +10,8 @@ public class CalculatorController {
     public static void run(){
         OutputView.printCommand();
         String input=InputView.enterInput();
-        int result= Calculator.inputCalculate(input);
-        OutputView.printResult(result);
+        UserInput userInput=new UserInput(input);
+        int result=userInput.startCalculate();
+       OutputView.printResult(result);
     }
 }
