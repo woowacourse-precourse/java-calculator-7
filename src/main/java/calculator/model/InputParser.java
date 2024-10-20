6b1eb5a7b09cdb363aa6b99bf096eed7dcc2d152
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:31:20 +0900

    :sparkles: feat(InputParser): 구분자 추출기로 문자열에서 구분자를 추출

--- src/main/java/calculator/model/InputParser.java
+++ src/main/java/calculator/model/InputParser.java
@@ -1,4 +1,13 @@
 package calculator.model;
 
 public class InputParser {
+    private final DelimiterExtractor delimiterExtractor;
+
+    public InputParser() {
+        this.delimiterExtractor = new DelimiterExtractor();
+    }
+
+    public String[] parse(String input) {
+        String delimiters = delimiterExtractor.extractDelimiters(input);
+    }
 }
