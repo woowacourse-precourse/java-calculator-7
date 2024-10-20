2711a575b5386cb2a1d3662c22c8a5c37a7b65b3
coolman0123 <socool4m@naver.com>
2024-10-21 04:53:36 +0900

    feat(OutputPrinter): 결과문 출력 기능
    - 계산 결과를 출력하는 printResult 메소드를 구현했습니다.
    
    Closes #8

--- src/main/java/calculator/view/OutputPrinter.java
+++ src/main/java/calculator/view/OutputPrinter.java
@@ -6,4 +6,8 @@ public class OutputPrinter {
         System.out.println("덧셈할 문자열을 입력해 주세요");
     }
 
+    public static void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
+
 }
