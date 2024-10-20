89dba61a4ff896a4d3145a646e2418c1ca733988
minju <minju9711@naver.com>
2024-10-20 21:50:15 +0900

    modify : custom 숫자 찾은 후 해당 문자와 인덱스 값 반환하도록 로직 변경

--- src/main/java/calculator/model/validator/InputValidator.java
+++ src/main/java/calculator/model/validator/InputValidator.java
@@ -3,5 +3,5 @@ package calculator.model.validator;
 public interface InputValidator {
     boolean validate(String input);
     void validateNumbers(String[] splitStrings);
-    String findCustomDelimiter(String input);
+    Object[] findCustomDelimiter(String input);
 }
