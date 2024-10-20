067cb892533fda3ebc354c1d23b9ced0b9bcdbeb
Nahye Choi <skgp0628@naver.com>
2024-10-20 21:08:35 +0900

    refactor: 문자열 계산 및 검사 기능 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,63 +7,63 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Application {
+    static final String CUSTOM_DELIMITER = "//(.)\\\\n(.*)";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int sum = 0;
 
-         // 문자열 입력
-         // - 안내문 출력
         System.out.println("덧셈할 문자열을 입력해주세요.");
-        // - 입력 받기
-        String numbers = Console.readLine();
+        String numbersStr = Console.readLine();
 
-        // - 예외처리
-        if (numbers == null || numbers.isEmpty()) {
-            System.out.println("결과: " + 0);
+        if (numbersStr == null || numbersStr.isEmpty()) {
+            System.out.println("결과 : " + 0);
         }else {
-            if (numbers.startsWith("//")){
-                sum = customDelimiterSum(numbers);
+            if (numbersStr.matches(CUSTOM_DELIMITER)) {
+                sum = customDelimiterSum(numbersStr);
             } else{
-                sum = delimiterSum(numbers);
+                sum = delimiterSum(numbersStr);
             }
+            System.out.println("결과 : " + sum);
         }
-        System.out.println("결과: " + sum);
     }
 
     // 기본 구분자
-    public static int delimiterSum(String numbers) {
+    public static int delimiterSum(String numbersStr) {
         int sum = 0;
         final String DELIMITER = ",|:";
 
         // 정규 표현식 패턴 컴파일
         Pattern pattern = Pattern.compile(DELIMITER);
-        Matcher matcher = pattern.matcher(numbers);
-        // -- 숫자와 구분자만 포함하는지 판단
+        Matcher matcher = pattern.matcher(numbersStr);
+
         // - 지정 구분자로 분리
         if (matcher.find()) {
-            if (isValidNumber(numbers)) {
-                String[] splitNumbers = numbers.split(DELIMITER);
-                int[] num = Arrays.stream(splitNumbers).mapToInt(Integer::parseInt).toArray(); //형변환
-                for (int i : num) { // 계산 기능 구현
+            String[] numbersArr = numbersStr.split(DELIMITER);
+            if (validNumber(numbersArr)) {
+                int[] num = Arrays.stream(numbersArr).mapToInt(Integer::parseInt).toArray();
+                for (int i : num) {
                     sum += i;
                 }
             }
+        } else {
+            isValidNumber(numbersStr);
+            sum = Integer.parseInt(numbersStr);
         }
-        return sum;
+        return sum; //
     }
 
-    // 커스텀
-    public static int customDelimiterSum(String numbers){
+    // 커스텀 구분자
+    public static int customDelimiterSum(String numbersStr){
         int sum = 0;
-        final String CUSTOM_DELIMITER = "//(.)\n(.*)";
 
-        Matcher customMatcher = Pattern.compile(CUSTOM_DELIMITER).matcher(numbers);
+        Matcher customMatcher = Pattern.compile(CUSTOM_DELIMITER).matcher(numbersStr);
         if (customMatcher.find()) {
             String customDelimiter = customMatcher.group(1);
-            String[] splitCustom = customMatcher.group(2).split(customDelimiter);
+            String[] numbersArr = customMatcher.group(2).split(customDelimiter);
 
-            if (isValidNumber(numbers)) {
-                int[] numberSum = Arrays.stream(splitCustom).mapToInt(Integer::parseInt).toArray();
+            if (validNumber(numbersArr)) {
+                int[] numberSum = Arrays.stream(numbersArr).mapToInt(Integer::parseInt).toArray();
                 for (int i : numberSum) {
                     sum += i;
                 }
@@ -72,12 +72,36 @@ public class Application {
         return sum;
     }
 
-    //숫자인지 확인
-    public static boolean isValidNumber(String numbers) {
-        String numRegEx = "[0-9]+"; // 문자 형태의 숫자를 검토해야하는 거였음
+    private static boolean isValidNumber(String numbersStr) {
+        String numRegEx = "^[0-9]+$";
 
         Pattern patternNum = Pattern.compile(numRegEx);
-        Matcher matcherNum = patternNum.matcher(numbers);
-        return matcherNum.find();
+        Matcher matcherNum = patternNum.matcher(numbersStr);
+
+        if (!matcherNum.matches()){
+            throw new IllegalArgumentException();
+        }
+        if (Integer.parseInt(numbersStr) < 0){
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
+    public static boolean validNumber(String[] numbersArr) {
+        String numRegEx = "^[0-9]+$";
+
+        Pattern patternNum = Pattern.compile(numRegEx);
+
+        for (String number : numbersArr){
+            Matcher matcherNum = patternNum.matcher(number.trim());
+
+            if (!matcherNum.matches()){
+                throw new IllegalArgumentException();
+            }
+            if (Integer.parseInt(number.trim()) < 0){
+                throw new IllegalArgumentException();
+            }
+        }
+        return true;
     }
 }
