86b7533246b163d77375bd92b9f842de9e37fd0d
navangs <43688529+insertend23@users.noreply.github.com>
2024-10-20 17:32:16 +0900

    refactor: NumberString 클래스를 이용하여 메인 클래스의 로직 간소화

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.math.BigInteger;
 import java.util.List;
 
 public class Application {
@@ -8,13 +9,9 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        Character customSeparator = SeparatorManager.getCustomSeparator(input);
-        if(customSeparator != null) {
-            input = input.substring(5);
-        }
+        List<String> numbers = NumberString.parse(input);
+        BigInteger result = StringCalculator.calculate(numbers);
 
-        List<String> numbers = new SeparatorManager(customSeparator).getSeparatedStringNumber(input);
-
-        System.out.println("결과 : " + StringCalculator.calculate(numbers));
+        System.out.println("결과 : " + result);
     }
 }
