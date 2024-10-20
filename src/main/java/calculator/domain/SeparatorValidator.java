c653f76313fbfba604f39f72b0657d1aaf08e25b
JoonKyo <joonkyolee@gmail.com>
2024-10-20 12:58:37 +0900

    feat(SeparatorValidator): 기본 구분자와 커스텀 구분자 외에 다른 구분자를 입력했을 때 처리하는 메서드 구현

--- src/main/java/calculator/domain/SeparatorValidator.java
+++ src/main/java/calculator/domain/SeparatorValidator.java
@@ -7,7 +7,7 @@ public class SeparatorValidator {
     private static final String CUSTOM_START_LETTERS = "//";
     private static final String CUSTOM_END_LETTERS = "\\n";
     private static final String MAKE_SPLIT_FORMAT_LETTER = "|";
-    private static final String ERROR_MESSAGE_FOR_CONSECUTIVE_SEPARATOR = "구분자를 연속해서 입력하셨습니다. 프로그램을 종료합니다."
+    private static final String ERROR_MESSAGE_FOR_SEPARATOR = "구분자를 잘못 입력하셨습니다. 프로그램을 종료합니다.";
     private static final Pattern VALID_NUMBER_PATTERN = Pattern.compile("[-\\d,:]+");
 
     public SeparatorValidator() {
@@ -25,10 +25,11 @@ public class SeparatorValidator {
         String[] separatedString = separateString(separators, str);
 
         for (String string : separatedString) {
-            if (hasConsecutiveSeparator(string)) {
-                throw new IllegalArgumentException(ERROR_MESSAGE_FOR_CONSECUTIVE_SEPARATOR);
+            if (hasInvalidSeparatorFormat(string)) {
+                throw new IllegalArgumentException(ERROR_MESSAGE_FOR_SEPARATOR);
             }
         }
+        return separatedString;
     }
 
     private static boolean containsCustomSeparator(String inputData) {
@@ -40,17 +41,25 @@ public class SeparatorValidator {
     }
 
     private static String[] separateString(List<String> separators, String inputData) {
-        String splitFormat = "";
+        StringBuilder splitFormat = new StringBuilder();
         for (int i = 0; i < separators.size(); i++) {
-            splitFormat += separators.get(i);
+            splitFormat.append(separators.get(i));
             if (i < separators.size() - 1) {
-                splitFormat += MAKE_SPLIT_FORMAT_LETTER;
+                splitFormat.append(MAKE_SPLIT_FORMAT_LETTER);
             }
         }
-        return inputData.split(splitFormat);
+        return inputData.split(splitFormat.toString());
+    }
+
+    private static boolean hasInvalidSeparatorFormat(String str) {
+        return hasConsecutiveSeparator(str) || hasInvalidSeparator(str);
     }
 
     private static boolean hasConsecutiveSeparator(String str) {
         return str.isEmpty();
+    }
+
+    private static boolean hasInvalidSeparator(String str) {
+        return !VALID_NUMBER_PATTERN.matcher(str).matches();
     }
 }
