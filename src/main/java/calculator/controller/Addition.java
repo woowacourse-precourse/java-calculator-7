442a4bc5adca3b3c90be020cd216e315bc6f80a6
hmlssghm <kwonse326@gmail.com>
2024-10-20 18:29:58 +0900

    refactor: Addition 템플릿 메서드 패턴으로 개선

--- src/main/java/calculator/controller/Addition.java
+++ src/main/java/calculator/controller/Addition.java
@@ -3,16 +3,24 @@ package calculator.controller;
 import java.util.List;
 
 public class Addition {
-    public int pickNumbers(List<String> separated) {
+    public int calculate(List<String> separated) {
+        // 숫자만 배열에 담아 반환
+        int[] numbers = pickNumbers(separated);
+
+        // 더한 값 반환
+        int answer = sum(numbers);
+
+        return answer;
+    }
+
+    private int[] pickNumbers(List<String> separated) {
         int[] numbers = new int[separated.size() / 2 + 1];
         for (int i = 0; i < separated.size(); i++) {
             if (i % 2 == 0) { // 숫자
                 numbers[i / 2] = Integer.parseInt(separated.get(i));
             }
         }
-
-        int answer = sum(numbers);
-        return answer;
+        return numbers;
     }
 
     private int sum(int[] numbers) {
