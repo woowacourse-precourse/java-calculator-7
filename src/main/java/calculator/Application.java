c5d041e9422b57d20fa168bb763176b25d0afbd9
HyeonJeong <wgwjh05169@gmail.com>
2024-10-21 00:11:34 +0900

    feat: 변수 초기화

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Splitter splitter = new Splitter();
+
     }
 }
