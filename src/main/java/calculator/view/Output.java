bb74a1335d3a3f2bc6bae5f1f3fee78ed4a0f61d
Chan531 <chan657312@gmail.com>
2024-10-20 20:48:56 +0900

    feat(Output): 결과 메세지 출력

--- src/main/java/calculator/view/Output.java
+++ src/main/java/calculator/view/Output.java
@@ -5,4 +5,8 @@ public class Output {
 	public void printInitialMessage() {
 		System.out.println("덧셈할 문자열을 입력해 주세요.");
 	}
+
+	public void printAnswerMessage(int answer) {
+		System.out.println("결과 : " + answer);
+	}
 }
