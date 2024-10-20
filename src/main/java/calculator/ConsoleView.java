e5e3e9064923b7399b52b138f22b58d207547dc3
gwanrok <145204556+moonditation@users.noreply.github.com>
2024-10-20 21:42:44 +0900

    feat: Implement ConsoleView for user input/output handling

---
+++ src/main/java/calculator/ConsoleView.java
@@ -0,0 +1,17 @@
+package calculator;
+
+public class ConsoleView {
+
+    public String getInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return camp.nextstep.edu.missionutils.Console.readLine(); // 입력 받기
+    }
+
+    public void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
+
+    public void printError(String errorMessage) {
+        System.out.println("Error: " + errorMessage);
+    }
+}
