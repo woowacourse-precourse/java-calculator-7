2bd7fa164fcb698cf25c158156fc6e147315a9f1
codrin2 <hssang127@gmail.com>
2024-10-20 15:09:53 +0900

    feat(CalculatorService): 추가된 예외 처리 로직 추가

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -2,35 +2,62 @@ package calculator.service;
 
 import calculator.constants.ExceptionMessage;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CalculatorService {
-	private static final String DEFAULT_DELIMITERS = ",|:";
-	private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.*)\n(.*)");
+	private static final String DEFAULT_DELIMITERS = ",:";
 
 	public int add(String text) {
-		Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+		validateInput(text);
+		text = text.trim();
+		text = text.replace("\\n", "\n");
+
 		String delimiters = DEFAULT_DELIMITERS;
-		if (matcher.matches()) {
-			delimiters += "|" + Pattern.quote(matcher.group(1));
-			text = matcher.group(2);
+
+		if (text.startsWith("//")) {
+			int delimiterEndIndex = text.indexOf("\n");
+
+			if (delimiterEndIndex == -1) {
+				throw new IllegalArgumentException(ExceptionMessage.INVALID_DELIMITER_FORMAT);
+			}
+			String customDelimiter = text.substring(2, delimiterEndIndex);
+			validateCustomDelimiter(customDelimiter);
+			delimiters += "|" + Pattern.quote(customDelimiter);
+			text = text.substring(delimiterEndIndex + 1).trim();
 		}
+
 		return calculateSum(split(text, delimiters));
 	}
 
+	private void validateInput(String text) {
+		if (text == null || text.trim().isEmpty()) {
+			throw new IllegalArgumentException(ExceptionMessage.INPUT_EMPTY);
+		}
+	}
+
+	private void validateCustomDelimiter(String customDelimiter) {
+		if (customDelimiter.matches("[0-9]+")) {
+			throw new IllegalArgumentException(ExceptionMessage.INVALID_CUSTOM_DELIMITER_NUMBER);
+		}
+
+		if (customDelimiter.length() > 1) {
+			throw new IllegalArgumentException(ExceptionMessage.MULTIPLE_CUSTOM_DELIMITERS);
+		}
+	}
+
 	private String[] split(String text, String delimiters) {
-		if (text.contains(",,") || text.contains("::") ) {
+		if (text.contains(",,") || text.contains("::")) {
 			throw new IllegalArgumentException(ExceptionMessage.INVALID_DELIMITER_FORMAT);
 		}
+
 		return text.split(delimiters);
 	}
 
 	private int calculateSum(String[] numbers) {
 		int sum = 0;
 		for (String number : numbers) {
-			if (!number.isEmpty()) {
-				int num = parsePositiveNumber(number);
+			if (!number.trim().isEmpty()) {
+				int num = parsePositiveNumber(number.trim());
 				sum += num;
 			}
 		}
@@ -40,7 +67,7 @@ public class CalculatorService {
 	private int parsePositiveNumber(String number) {
 		try {
 			int num = Integer.parseInt(number);
-			if (num < 0) {
+			if (num <= 0) {
 				throw new IllegalArgumentException(ExceptionMessage.NEGATIVE_NUMBER);
 			}
 			return num;
