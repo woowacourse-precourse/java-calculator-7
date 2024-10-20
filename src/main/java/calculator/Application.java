177f47b8c64f861739eab10a366ddd15d227bc3f
yejin923 <yejin923@naver.com>
2024-10-20 13:25:31 +0900

    feat: add separate with ':',','

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        int total = 0;
+
+        System.out.println("덧셈할 문자열을 입력해주세요. ");
+        String line = Console.readLine();
+
+        String[] str = line.split(":|,");
+
+
+        for (int i = 0; i < str.length; i++) {
+            try {
+                total += Integer.parseInt(str[i]);
+            } catch (NumberFormatException e){
+                total += 0;
+            }
+        }
+
+        System.out.println("결과 : " + total);
+
+
     }
 }
