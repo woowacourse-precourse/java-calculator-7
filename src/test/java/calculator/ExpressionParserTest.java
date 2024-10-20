751f78240a4582d5d4f73a60749a1d4fbd9399ab
09954 <177149797+09954@users.noreply.github.com>
2024-10-20 22:06:16 +0900

    feat: 구분자를 기준으로 문자를 나누고 수로 변환

---
+++ src/test/java/calculator/ExpressionParserTest.java
@@ -0,0 +1,34 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class ExpressionParserTest {
+
+    @Test
+    void 구분자의_길이가_1이_아닌_경우_예외를_발생한다() {
+        List<String> delimiters = List.of("::");
+        assertThatThrownBy(() -> new ExpressionParser(delimiters))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구분자는 1 글자여야 합니다.");
+    }
+
+    @Test
+    void 구분자를_기준으로_나누어_문자열을_변환한다() {
+        List<String> delimiters = List.of(",", ":");
+        ExpressionParser expressionParser = new ExpressionParser(delimiters);
+        List<Integer> numbers = expressionParser.parse("1,2:3");
+        assertThat(numbers).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    void 숫자가_아닌_값이_포함된_경우_예외를_발생한다() {
+        ExpressionParser expressionParser = new ExpressionParser(List.of(",", ":"));
+        assertThatThrownBy(() -> expressionParser.parse("1,2:a:b"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자가 아닌 값이 포함되어 있습니다");
+    }
+}
