fb177e061761626c2f1e5ae5e4d55aa331757fb2
조윤성 <yunsung1206@gmail.com>
2024-10-20 18:46:21 +0900

    feat: 숫자가 아닌 문자가 있는지 확인하는 기능 추가

--- src/main/java/calculator/NumberValidator.java
+++ src/main/java/calculator/NumberValidator.java
@@ -3,14 +3,22 @@ package calculator;
 public class NumberValidator {
     private static final String NEGATIVE_SIGN = "-";
     private static final String NEGATIVE_ERROR_MESSAGE = "문자열에 음수가 포함되어 있습니다.";
+    private static final String NOT_NUMBER_ERROR_MESSAGE = "문자열에 숫자가 아닌 문자가 포함되어 있습니다.";
 
     public static void validate(String number) {
         if (NumberValidator.isNegative(number)) {
             throw new IllegalArgumentException(NEGATIVE_ERROR_MESSAGE);
         }
+        if (!NumberValidator.isNumber(number)) {
+            throw new IllegalArgumentException(NOT_NUMBER_ERROR_MESSAGE);
+        }
     }
 
     private static boolean isNegative(String number) {
         return number.startsWith(NEGATIVE_SIGN);
+    }
+
+    private static boolean isNumber(String number) {
+        return number.matches("[0-9]+");
     }
 }
