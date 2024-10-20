359e47e3cd68becf415111e0f8bd180aba007fad
cobinding <cntpqls0218@gmail.com>
2024-10-20 23:11:04 +0900

    feat: Consol API를 통한 입력 받기 및 메소드 정의

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,41 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String word = Console.readLine();
+
+        try {
+            int checkInput = checkInput(word);
+            if (checkInput == 0) {
+                System.out.println("결과 : " + 0);
+            } else {
+                String delimiter = getDelimiter(word);
+                String[] numbers = extractNumbers(word, delimiter);
+                int resultSum = sumNumbers(numbers);
+                System.out.println("결과 : " + resultSum);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static int checkInput(String word) {
+        return -1;
+    }
+
+    private static String getDelimiter(String word) {
+        return "[,:]";
+    }
+
+    private static String[] extractNumbers(String word, String delimiter) {
+        return word.split(delimiter);
+    }
+
+    private static int sumNumbers(String[] numbers) {
+        int sum = 0;
+        return sum;
     }
 }
