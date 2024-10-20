1a0a724fd0f4ad9fb168b2c5876d85cea7daa0bf
minju <minju9711@naver.com>
2024-10-20 21:50:58 +0900

    modify : 숫자형 타입 long으로 변경

--- src/main/java/calculator/model/calculator/SumCalculator.java
+++ src/main/java/calculator/model/calculator/SumCalculator.java
@@ -3,8 +3,8 @@ package calculator.model.calculator;
 public class SumCalculator implements Calculator {
 
     @Override
-    public int calculate(int [] numbers) {
-        int answer = 0;
+    public long calculate(long [] numbers) {
+        long answer = 0;
         for(int i=0; i<numbers.length; i++) {
             answer+=numbers[i];
         }
