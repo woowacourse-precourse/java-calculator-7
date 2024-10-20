9942066f37f3941337b333328a4b49c2da7c3195
theplayer1003 <theplayer1003@gmail.com>
2024-10-20 22:48:31 +0900

    feat(controller): implement controller with apllication start

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,8 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
+        Controller controller = new Controller();
 
+        controller.startProcess();
     }
 }
--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -1,4 +1,18 @@
 package calculator;
 
 public class Controller {
+    CalculatorManager calculatorManager = new CalculatorManager();
+    InputHandler inputHandler = new InputHandler();
+    DelimiterParser delimiterParser = new DelimiterParser();
+    CheckCompatibility checkCompatibility = new CheckCompatibility();
+    OutputHandler outputHandler = new OutputHandler();
+    Calculator calculator = new Calculator();
+
+    public void startProcess(){
+        calculatorManager.makeUserInput(inputHandler);
+        calculatorManager.makeDelimiter(delimiterParser);
+        calculatorManager.checkCompatibility(checkCompatibility);
+        calculatorManager.calculateSum(calculator);
+        calculatorManager.printAnswer(outputHandler);
+    }
 }
