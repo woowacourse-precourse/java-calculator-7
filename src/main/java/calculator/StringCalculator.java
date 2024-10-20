74396b67611bfdc1741f61715a08d5585b8521eb
조윤성 <yunsung1206@gmail.com>
2024-10-21 07:55:44 +0900

    refactor: 숫자의 합을 구할 때 stream을 활용하도록 수정

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -15,10 +15,6 @@ public class StringCalculator {
     }
 
     private int sum(List<Integer> numbers) {
-        int sum = 0;
-        for (Integer number : numbers) {
-            sum += number;
-        }
-        return sum;
+        return numbers.stream().reduce(0, Integer::sum);
     }
 }
