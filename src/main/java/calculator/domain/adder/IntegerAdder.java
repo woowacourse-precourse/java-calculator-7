eeca71b4ce17b06cba4a43e26168a27465906507
Mint <audwl03071@gmail.com>
2024-10-20 23:19:02 +0900

    fix: 음수 오버플로우 검사 추가
    - 기존에는 양수 오버플로우만 검사했지만, 음수 오버플로우 검사도 추가하여 덧셈 로직을 개선한다.

--- src/main/java/calculator/domain/adder/IntegerAdder.java
+++ src/main/java/calculator/domain/adder/IntegerAdder.java
@@ -1,27 +1,28 @@
-package calculator.adder;
+package calculator.domain.adder;
 
 import java.util.List;
 
 public class IntegerAdder implements Addable<Integer> {
 
     private static final Integer MAX_VALUE = Integer.MAX_VALUE;
+    private static final Integer MIN_VALUE = Integer.MIN_VALUE;
 
     @Override
     public Integer addNumbers(final List<Integer> numbers) {
         int sum = 0;
-        for (Number number : numbers) {
-            sum = addSafely(sum, number.intValue());
+        for (Integer number : numbers) {
+            sum = addSafely(sum, number);
         }
 
         return sum;
     }
 
     private int addSafely(final int number1, final int number2) {
-        if (number1 > 0 && number2 > MAX_VALUE - number1) {
+        if (number2 > 0 && number1 > MAX_VALUE - number2) {
             throw new IllegalStateException("오버플로우가 발생했습니다.");
         }
 
-        if (number2 > 0 && number1 > MAX_VALUE - number2) {
+        if (number2 < 0 && number1 < MIN_VALUE - number2) {
             throw new IllegalStateException("오버플로우가 발생했습니다.");
         }
 
