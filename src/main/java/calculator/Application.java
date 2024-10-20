9aae7bc5931b1ff3f421c0ed601511b6839e78af
Avocado <ujubongbong@gmail.com>
2024-10-21 01:58:27 +0900

    :sparkles: feat: 구분자(쉼표 or 콜론)을 올바르게 사용했는지 확인하는 함수

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,8 +3,9 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    private static String[] basicSeparators = {",", "\\n"};
-    private static char[] customSeparators;
+    private static char[] basicSeparators = {',', ':'};
+    private static char[] customSeparators = {'\0'};
+    private static String userInput;
 
     // 문자열 덧셈 계산기 시작 함수
     public static void init() {
@@ -13,7 +14,7 @@ public class Application {
 
     // 문자열 입력받는 함수
     public static void input() {
-       String input = Console.readLine();
+        userInput = Console.readLine();
     }
 
     // 커스텀 구분자 지정하는 함수
@@ -21,21 +22,52 @@ public class Application {
         // "//"와 "\n" 사이에 문자가 존재하면 커스텀 구분자로 지정
         if(input.startsWith("//")) {
             if(input.substring(3, 5).equals("\\n")) {
+                userInput = input.substring(5);
                 customSeparators = new char[]{input.charAt(2)};
             }
         }
     }
 
     // 빈 문자열인지 확인하는 함수
-    public static int validateInput(String input) {
-        if(input == null || input.isEmpty()) {
-            return 0;
+    public static boolean validateInput(String input) {
+        return input == null || input.isEmpty();
+    }
+
+    // 구분자(쉼표 or 콜론)을 올바르게 사용했는지 확인하는 함수
+    public static void validateBasicSeparator(String input) {
+        // 처음에 숫자가 나왔는지 확인
+        if(!Character.isDigit(input.charAt(0))) {
+            throw new IllegalArgumentException("숫자부터 입력해야 합니다.");
         }
-        return 0;
+
+        // 기본 구분자인지 확인
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                char separator = input.charAt(i);
+
+                if (!(separator == basicSeparators[0] || separator == basicSeparators[1])) {
+                    throw new IllegalArgumentException("올바르지 않은 구분자를 사용하였습니다.");
+                }
+            }
+        }
+
+        // 숫자 사이에 하나의 구분자만 있는지 확인
+        String[] st = input.split("[" + basicSeparators[0] + basicSeparators[1] + customSeparators[0] + "]");
+        for(String s : st) {
+            if(s == null || s.isEmpty()) {
+                throw new IllegalArgumentException("구분자가 연속이거나 숫자 사이에 존재하지 않습니다.");
+            }
+        }
     }
 
     public static void main(String[] args) {
         init();
         input();
+        customSeparator(userInput);
+        if(validateInput(userInput)) {
+            System.out.println("결과 : " + 0);
+            return;
+        };
+        validateBasicSeparator(userInput);
     }
 }
