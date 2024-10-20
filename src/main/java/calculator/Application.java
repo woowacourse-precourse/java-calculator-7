723f2e59767e1a22c4552b56381d907ff5fe4633
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-10-20 15:29:36 +0900

    feat:  커스텀 구분자를 기준으로 문자열을 나누는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
 	private static final String INPUT_REQUEST_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 	private static final String DEFAULT_DELIMETERS = ",|:";
+	private static final String CUSTOM_DELIMETERS_PREFIX = "//";
+	private static final String CUSTOM_DELIMETERS_SUFFIX = "\\n";
 	
     public static void main(String[] args) {
         System.out.println(INPUT_REQUEST_MESSAGE);
@@ -15,6 +17,20 @@ public class Application {
         	return;
         }
         
-        String[] tokens = input.split(DEFAULT_DELIMETERS);
+        String delimiters = DEFAULT_DELIMETERS;
+        String defaultInput = input;
+        
+        if(input.startsWith(CUSTOM_DELIMETERS_PREFIX)) {
+        	int delimiterStartIndex = CUSTOM_DELIMETERS_PREFIX.length();
+        	int delimiterEndIndex = input.indexOf(CUSTOM_DELIMETERS_SUFFIX, delimiterStartIndex);   
+        	delimiters = input.substring(delimiterStartIndex, delimiterEndIndex);
+        	defaultInput = input.substring(delimiterEndIndex + 2);
+        } 
+        
+        String[] tokens = defaultInput.split(delimiters);
+        
+        for (int i = 0; i < tokens.length; i++) {
+            System.out.println(tokens[i]);
+        }    
     }
 }
