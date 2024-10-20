47ac571206ba4ca2e7f782fab0c999bf0c85b56e
geonu <kgo000926@gmail.com>
2024-10-20 22:15:05 +0900

    feat: 구분자를 바탕으로 문자열에서 피연산자 추출

---
+++ src/main/java/calculator/Operand.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class Operand {
+    private final int value;
+
+    public Operand(String value) {
+        this.value = Integer.parseInt(value);
+    }
+}
--- src/main/java/calculator/Splitter.java
+++ src/main/java/calculator/Splitter.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,11 +9,24 @@ import java.util.regex.Pattern;
 public class Splitter {
     private static final String PATTERN = "^//(.*?)\\n";
 
-    private List<String> extractDelimiters(String text) {
+    public List<Operand> split(String text) {
+        String delimiters = extractDelimiters(text);
+        String expression = removeCustomDelimiter(text);
+
+        return Arrays.stream(expression.split("[" + delimiters + "]"))
+                .map(Operand::new)
+                .toList();
+    }
+
+    private String removeCustomDelimiter(String text) {
+        return text.replaceFirst(PATTERN, "");
+    }
+
+    private String extractDelimiters(String text) {
         List<String> delimiters = new ArrayList<>(List.of(",", ":"));
         addCustomDelimiter(text, delimiters);
 
-        return delimiters;
+        return String.join("", delimiters);
     }
 
     private void addCustomDelimiter(String text, List<String> delimiters) {
