325a8e2e12a40b9cb8309804d3f0fefc484f9ef9
Kwon <rnjsdndud00@naver.com>
2024-10-20 17:44:35 +0900

    refactor(separator): 커스텀 구분자 추가 로직 간단하게 변경
    반복문 제거하고 substring과 indexOf 함수 활용

--- src/main/java/calculator/CustomSeparator.java
+++ src/main/java/calculator/CustomSeparator.java
@@ -6,16 +6,11 @@ public class CustomSeparator {
         // 기본 구분자
         String separator = "[,:";
 
-        // "//"와 "\n" 사이에 있는 값을 하나씩 구분자에 추가해주기 위해 배열로 변환
-        String[] stSplit = st.split("");
-
         // 만약 "/"로 시작하면 커스텀 구분자가 존재한다는 의미
         if (st.startsWith("/")) {
-            // "\n" 문자가 나올 때까지 모든 문자를 separator에 추가
-            for (int i = 2; i < st.indexOf("\\"); i++) {
-                separator += stSplit[i];
-            }
+            separator += st.substring(2, st.indexOf("\\"));
         }
+
         separator += "]";
         return separator;
     }
