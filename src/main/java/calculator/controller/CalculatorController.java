c4bb251c2e079088e23aad103ecc1bc90b93bf3f
byegu <rkdqudrn07@naver.com>
2024-10-20 16:52:45 +0900

    문자열 덧셈 결과 출력 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -2,18 +2,21 @@ package calculator.controller;
 
 import calculator.model.InputString;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 import java.util.List;
 
 public class CalculatorController {
     InputString inputString = new InputString();
     InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
     StringValidator stringValidator = new StringValidator();
     Integer result = 0;
 
     public void executeCalculator() {
         saveInputString();
         calculate();
+        sendResultMessage();
     }
 
     private void saveInputString() {
@@ -26,5 +29,9 @@ public class CalculatorController {
         result = inputString.getUserOperand().stream()
                 .mapToInt(Integer::intValue)
                 .sum();
+    }
+
+    private void sendResultMessage() {
+        outputView.printResult(result);
     }
 }
