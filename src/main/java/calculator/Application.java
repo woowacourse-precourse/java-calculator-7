2c7465fce12f699ffe32837a119f17ec8add392d
Korigin99 <rldnjs3413@gmail.com>
2024-10-20 15:03:22 +0900

    feat: 문자열 덧셈 계산기 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,16 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = camp.nextstep.edu.missionutils.Console.readLine();
+        StringCalculator calculator = new StringCalculator();
+
+        try {
+            int result = calculator.calculate(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
