65884ab34e37f6be6bbf8dbe58aac7906c3f778e
hanmh319 <hanmh319@naver.com>
2024-10-20 18:45:51 +0900

    refactor: splitNumberãer, IlException 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,13 +8,18 @@ public class Application {
     private static final String CUSTOM_SEPARATOR_REGEX = "//(.*?)\\\\n";
 
     public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
-        String[] splitArr = splitInput(input);
-        for (int i = 0; i < splitArr.length; i++) {
-            System.out.print(splitArr[i] + " ");
+        try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine();
+            String[] sepArr = splitInput(input); // 커스텀 구분자 지정 및 구분자 기준으로 나누는 메서드 호출, 배열에 저장
+            int sum = 0;
+            for (String numStr : sepArr) {
+                sum += splitNumber(numStr);
+            }
+            System.out.print(sum);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage()); // 예외 발생 따로 처리
         }
-
     }
 
     private static String[] splitInput(String input) {
@@ -37,4 +42,23 @@ public class Application {
         }
 
     }
+
+    private static int splitNumber(String numStr) {
+        int sum = 0;
+        numStr = numStr.trim(); // 문자열의 앞뒤 공백 제거
+        if (numStr.isEmpty()) {
+            throw new IllegalArgumentException("공백입니다.");
+        }
+        try {
+            int number = Integer.parseInt(numStr); // 형변환
+            if (number < 0) {
+                throw new IllegalArgumentException("음수는 허용되지 않습니다: " + number); // 음수 입력 시 예외처리
+            }
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(
+                    "유효하지 않은 문자열이 포함되어 있습니다: " + numStr.trim()); // 유효하지 않은 문자열 포함 시 예외처리
+        }
+    }
+
 }
