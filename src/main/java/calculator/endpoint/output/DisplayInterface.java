206f466d108387660a33593569a2e558bb3db9f8
YeaChan05 <qkenrdl05@gmail.com>
2024-10-20 18:14:21 +0900

    feat(DisplayInterface): 출력기 구현
    시스템 메세지와 결과 출력을 위한 출력기 구현

---
+++ src/main/java/calculator/endpoint/output/DisplayInterface.java
@@ -0,0 +1,11 @@
+package calculator.endpoint.output;
+
+public class DisplayInterface {
+    public static void displaySystemMessage() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public static void displayAnswer(int answer) {
+        System.out.println("결과 :" + answer);
+    }
+}
