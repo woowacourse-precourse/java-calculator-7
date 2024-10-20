8610ce5230abe90ca2d0d4baa4975c9c98fb4ae0
heejjinkim <hjhj12@dgu.ac.kr>
2024-10-21 00:47:25 +0900

    feat: 커스텀 구분자 추출
    커스텀 구분자가 지정되지 않은 경우, 기본 구분자 사용

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,13 +4,27 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        String delimiters = ":,";
 
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
+
+        if (isCustomDelimiterDefined(input)) {
+            delimiters = getCustomDelimiter(input);
+        }
     }
 
     private static boolean isCustomDelimiterDefined(String input) {
         return input.startsWith("//") && (input.indexOf("\n") == input.indexOf("//") + 3);
     }
 
+    private static String getCustomDelimiter(String input) {
+
+        int startIndex = input.indexOf("//") + 2;
+        int endIndex = input.indexOf("\n");
+
+        String customDelimiter = input.substring(startIndex, endIndex);
+
+        return customDelimiter;
+    }
 }
