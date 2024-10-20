ee73ed38137f5cb7f11d970763ae6e83ce25a082
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-21 01:21:50 +0900

    feat : 문자열 입력 받기 및 출력 동작 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,13 @@
 package calculator.controller;
 
+import calculator.service.CalculatorService;
+import calculator.view.CalculatorView;
+
 public class CalculatorController {
+    CalculatorService calculatorService = new CalculatorService();
+
+    public void start() {
+        String input= CalculatorView.InputString();
+        calculatorService.mainController(input);
+    }
 }
