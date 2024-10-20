aa4c9af6adb911d14f5c04d231c5f78b01326d56
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-10-20 22:14:59 +0900

    feat: 커스텀 구분자를 식별하고 구분자 문자열에 넣는 기능 구현
    - substring을 통해서 커스텀 구분자 패턴을 식별 후 커스텀 구분자를 구분자 목록에 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,40 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
-        
+
         if (str.isEmpty()) {
             System.out.print("결과 : 0");
             Console.close();
+        }
+
+        ArrayList<String> delimeters = new ArrayList<>(List.of(",", ":"));
+
+        if (str.substring(0, 2).equals("//")) {
+            int delimeterIndex = -1;
+            delimeterIndex = str.indexOf("\\n");
+            if (delimeterIndex != -1) {
+                String customDelimeter = str.substring(2, delimeterIndex);
+                delimeters.add(customDelimeter);
+                str = str.substring(delimeterIndex + 2, str.length());
+            } else {
+                throw new IllegalStateException();
+            }
+        }
+
+        String splitDelimeters = "";
+
+        for (int i = 0; i < delimeters.size(); i++) {
+            if (i == delimeters.size() - 1) {
+                splitDelimeters += delimeters.get(i);
+            }
+            splitDelimeters += delimeters.get(i) + "|";
         }
     }
 }
