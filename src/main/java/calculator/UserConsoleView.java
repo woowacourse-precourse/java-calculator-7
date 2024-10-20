00ed8ed22787b6ae5b0461064a243fb7fc9b12b3
Hyoseop Song <crad_on25@naver.com>
2024-10-21 03:26:32 +0900

    feat(UserConsoleView): 사용자 입력으로 계산 결과를 출력하는 계산기 실행 기능 추가

---
+++ src/main/java/calculator/UserConsoleView.java
@@ -0,0 +1,44 @@
+package calculator;
+
+import calculator.console.InputView;
+import calculator.console.OutputView;
+import calculator.exception.InvalidInputException;
+import calculator.exception.SystemException;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class UserConsoleView {
+	private final InputView inputView;
+	private final OutputView outputView;
+	private final DelimiterParser delimiterParser;
+	private final Calculator calculator;
+
+	public UserConsoleView() {
+		inputView = new InputView();
+		outputView = new OutputView();
+		delimiterParser = new DelimiterParser();
+		calculator = new Calculator();
+	}
+
+	public void run() {
+		String expression = getExpression();
+		List<Long> terms = delimiterParser.extractTerms(expression);
+		Long result = calculator.sum(terms);
+		outputView.printOperationResult(result);
+	}
+
+	public String getExpression() {
+		return getUserInput(() -> {
+			outputView.printExpressionRequestMessage();
+			return inputView.readConsole();
+		});
+	}
+
+	public <T> T getUserInput(Supplier<T> inputReader) {
+		try {
+			return inputReader.get();
+		} catch (SystemException | IllegalArgumentException e) {
+			throw new InvalidInputException(e.getMessage());
+		}
+	}
+}
---
+++ src/main/java/calculator/exception/SystemException.java
@@ -0,0 +1,8 @@
+package calculator.exception;
+
+public class SystemException extends RuntimeException {
+
+	public SystemException(final String message) {
+		super(message);
+	}
+}
