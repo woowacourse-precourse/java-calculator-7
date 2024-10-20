bca5a63c520d4527499d6968a77691ebca0369da
eunseobb <seobseob0810@naver.com>
2024-10-21 01:53:15 +0900

    feat(controller): 사용자 입력 처리 및 덧셈 계산 기능 추가
    사용자로부터 입력을 받아 문자열을 처리한 후, 덧셈 계산을 수행하고 결과를 출력하도록 구현.
    
    Closes #767

--- src/main/java/calculator/controller/CalculateController.java
+++ src/main/java/calculator/controller/CalculateController.java
@@ -1,10 +1,21 @@
 package calculator.controller;
 
+import calculator.model.CalculateService;
+import calculator.model.StringParser;
 import calculator.view.InputView;
+import calculator.view.OutputView;
+import java.util.List;
 
 public class CalculateController {
-    public void calculator(){
+    public void calculator() {
+        //input
         String userInput = new InputView().getUserInput();
-        System.out.println("input: " + userInput);
+        List<Integer> numbers = new StringParser().extractNumbers(userInput);
+
+        //calculate
+        Integer result = new CalculateService().calculateNumbers(numbers);
+
+        //output
+        new OutputView().printResult(result);
     }
 }
