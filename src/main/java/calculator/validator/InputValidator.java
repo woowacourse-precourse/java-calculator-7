2451260cc2c5a3f9e32a4c2ce447a43ecdcee8c9
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-21 02:24:57 +0900

    feat(InputValidator): 기본 문자열 확인 기능 구현
    - 입력값을 regex를 통해 숫자 또는 구분자가 아닌 단순 문자로 시작되는 단순 문자열인지 확인합니다.
    - 단순 문자열이라면 true를 반환합니다.
    - format과 조건이 겹쳐, isValidFormat에서 실행하였습니다.

--- src/main/java/calculator/validator/InputValidator.java
+++ src/main/java/calculator/validator/InputValidator.java
@@ -6,9 +6,25 @@ public class InputValidator {
         return input.matches("-\\d+");
     }
 
+    private boolean isValidFormat(String input) {
+        if (input.matches("^//.*")) {
+            return input.contains("\\n");
+        }
+        if (!isSimpleString(input)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isSimpleString(String input) {
+        if (input.matches("^\\d.*")) {
+            return false;
+        } else if (input.matches("^[,:].*|^//.*")) {
+            return false;
+        }
+        return true;
+    }
+
     // TODO: 잘못된 입력이었을 경우에는 예외를 발생시킨다. 입력이 유효한지 검증한다.
 
-    // TODO: 커스텀 구분자를 형식에 맞게 입력했는지 확인한다.
-
-    // TODO: 숫자 또는 기본 구분자가 아닌 단순 문자로 시작되는지 확인한다.
 }
