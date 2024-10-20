7cf163576b33b6ff230c915c20e6c90e102d9aca
davemins <davemins4125@naver.com>
2024-10-21 08:57:44 +0900

    feat(View): 문자열을 입력하는 공통 함수 생성

--- src/main/java/calculator/view/View.java
+++ src/main/java/calculator/view/View.java
@@ -1,6 +1,6 @@
 package calculator.view;
 
-
+import camp.nextstep.edu.missionutils.Console;
 import calculator.global.constants.MessageType;
 import static calculator.global.constants.MessageType.STRING_REQUEST_MESSAGE;
 
@@ -8,10 +8,16 @@ import static calculator.global.constants.MessageType.STRING_REQUEST_MESSAGE;
 public final class View {
     public static String requestString() {
         printlnMessage(STRING_REQUEST_MESSAGE);
+        return enterMessage();
     }
 
     /* Output View */
     private static void printlnMessage(MessageType messageType) {
         System.out.println(messageType.getMessage());
+    }
+
+    /* Input View */
+    private static String enterMessage() {
+        return Console.readLine();
     }
 }
