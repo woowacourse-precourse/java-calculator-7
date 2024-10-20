c76163ce696a42eda5fd0092f310c9c873b2fa88
softmoca <acyc0909@gmail.com>
2024-10-20 17:31:33 +0900

    feat: 커스텀 구분자 추출 및 예외 처리 기능 구현
    - "//"와 "\n" 사이의 문자를 추출하여 커스텀 구분자로 사용하는 로직 추가.
    - 커스텀 구분자가 숫자인 경우 IllegalArgumentException 발생 처리.
    - 커스텀 구분자가 올바르게 추출되지 않을 때 예외 발생하도록 구현.
    - 정규식을 사용하여 커스텀 구분자 추출 로직을 구성하고, Matcher 객체로 패턴을 매칭.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,9 +10,13 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        String delimiter;
 
         if (isCustomDelimiterPattern(input)) {
-           System.out.println("Yes");
+            delimiter = getCustomDelimiter(input);
+
+
+           System.out.println("Yes"+delimiter);
         }
 
     }
@@ -20,6 +24,21 @@ public class Application {
         Pattern pattern = Pattern.compile("//(.)\\\\n.*");
         Matcher matcher = pattern.matcher(input);
         return matcher.matches();
+    }
+
+
+    private static String getCustomDelimiter(String input) {
+        Pattern pattern = Pattern.compile("//(.)\\\\n.*");
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            String delimiter = matcher.group(1);
+            if (delimiter.matches("[0-9]")) {
+                throw new IllegalArgumentException("숫자는 구분자로 사용할 수 없습니다.");
+            }
+            return delimiter;
+        }
+
+        throw new IllegalArgumentException("올바른 커스텀 구분자가 없습니다.");
     }
 
 
