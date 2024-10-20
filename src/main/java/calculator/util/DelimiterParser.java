90b5d3b486fd8df046175325f491a4e408f0845c
wjdtjdgns <person3275@gmail.com>
2024-10-20 21:21:50 +0900

    fix(parser): 커스텀 구분자 스플릿 방식 변경

--- src/main/java/calculator/util/DelimiterParser.java
+++ src/main/java/calculator/util/DelimiterParser.java
@@ -3,24 +3,18 @@ package calculator.util;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class DelimiterParser {
     private static final List<String> DELIMITERS = Arrays.asList(",", ":");
 
     public List<String> parseDelimiter(String input) {
-        input = input.replace("\\n", "\n");
-
         List<String> delimiters = new ArrayList<>(DELIMITERS);
 
         if (input.startsWith("//")) {
-            Matcher matcher = Pattern.compile("//(.*?)\n(.*)", Pattern.DOTALL).matcher(input);
-            if (matcher.matches()) {
-                String customDelimiter = matcher.group(1);
-                delimiters.add(Pattern.quote(customDelimiter));
-            } else {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
+            int delimiterEndIndex = input.indexOf("\\n");
+            if (delimiterEndIndex != -1) {
+                String customDelimiter = input.substring(2, delimiterEndIndex);
+                delimiters.add(customDelimiter);
             }
         }
 
@@ -28,15 +22,9 @@ public class DelimiterParser {
     }
 
     public String[] split(String input, List<String> delimiters) {
-        input = input.replace("\\n", "\n");
-
         if (input.startsWith("//")) {
-            int index = input.indexOf('\n');
-            if (index != -1) {
-                input = input.substring(index + 1);
-            } else {
-                throw new IllegalArgumentException("잘못된 입력입니다.");
-            }
+            int index = input.indexOf("\\n");
+            input = input.substring(index + 2);
         }
 
         return input.split(String.join("|", delimiters));
