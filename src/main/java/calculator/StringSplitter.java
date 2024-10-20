6ad9d6ff3cd7edbec31fcafcba1902ac9b186ce7
inti0 <142914095+inti0@users.noreply.github.com>
2024-10-21 02:16:36 +0900

    refactor: INVALID_DELIMITER +-.에서 -.로 변경

--- src/main/java/calculator/StringSplitter.java
+++ src/main/java/calculator/StringSplitter.java
@@ -5,6 +5,6 @@ import java.util.List;
 public interface StringSplitter {
 
     static final String DEFAULT_DELIMITER = ",:";
-    static final String INVALID_DELIMITER = "-+.";
+    static final String INVALID_DELIMITER = "-.";
     List<String> splitString(String str);
 }
