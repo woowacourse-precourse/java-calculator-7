ab9fb951ae3ee9418d3c359faf68d49ffc1e7906
wooqqq <sge0225@naver.com>
2024-10-20 17:28:32 +0900

    Refactor: 숫자 유효성 검사 분리

--- src/main/java/calculator/service/Validator.java
+++ src/main/java/calculator/service/Validator.java
@@ -4,9 +4,13 @@ public class Validator {
 
     public void validate(String[] numbers) {
         for (String num : numbers) {
-            if (!num.matches("\\d+")) {
-                throw new IllegalArgumentException("올바르지 않은 숫자 형식입니다: " + num);
-            }
+            isNumber(num);
+        }
+    }
+
+    private void isNumber(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException("올바르지 않은 숫자 형식입니다.");
         }
     }
 }
