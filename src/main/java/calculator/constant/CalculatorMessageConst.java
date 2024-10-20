4cee11c3820e57034b8a0e0aeac442eb5994d016
oznchex <114240463+oznchex@users.noreply.github.com>
2024-10-20 20:42:47 +0900

    feat(CalculatorMessageConst): 계산기 입출력 메시지에 사용되는 상수 추가
    - 덧셈용 계산기에서 사용자 입력 가이드용 메시지 START_ADDITION_CALCULATOR_GUIDE_MESSAGE 추가
    - 계산기에서 공통적으로 출력을 나타내는 가이드 메시지 OUTPUT_GUIDE_MESSAGE 추가
    - 잘못된 입력에 대해 공통으로 잘못된 입력임을 출력하는 INPUT_ERROR_MESSAGE 추가

---
+++ src/main/java/calculator/constant/CalculatorMessageConst.java
@@ -0,0 +1,7 @@
+package calculator.constant;
+
+public class CalculatorMessageConst {
+    public static final String START_ADDITION_CALCULATOR_GUIDE_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    public static final String OUTPUT_GUIDE_MESSAGE = "결과 : ";
+    public static final String INPUT_ERROR_MESSAGE = "잘못된 입력입니다.";
+}
