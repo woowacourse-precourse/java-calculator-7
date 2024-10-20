ff84952fd097ebda4ae0398d6595d2b2752a1646
KF97 <aysul0@naver.com>
2024-10-20 22:03:41 +0900

    feat: 빈 문자열 처리 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,6 +8,10 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         try {
             String str = Console.readLine();
+            
+            if (str.equals("")) {
+                System.out.println("결과 : 0");
+            }
 
         } catch (IllegalArgumentException e) {
             return;
