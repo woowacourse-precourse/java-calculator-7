a97059e453394d3495aef726bd5b1ffe276c1cf0
sihyun10 <hyhy200170@gmail.com>
2024-10-21 02:51:07 +0900

    refactor(성능 개선) : 계속 객체 생성되는 문제 String.matches()를 사용할때면 Pattern 객체가 생성된다. 사용이 끝나면 GC 대상이기에 너무 많이 사용하면 좋지 않다고 한다. 이 문제를 Pattern 객체를 캐싱해둔 다음 다시 객체가 생성되지 않게 하였다. 또한, 코드 구조를 좀 더 가독성이 좋도록 변경하였다.

--- src/main/java/calculator/DelimiterSplitter.java
+++ src/main/java/calculator/DelimiterSplitter.java
@@ -7,41 +7,50 @@ public class DelimiterSplitter {
     private final String str;
     private static final String BASIC_DELIMITER = ",|:";
     private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+    private static final Pattern NEGATIVE_NUMBER_PATTERN = Pattern.compile(".*-\\d+.*");
+    private static final Pattern NOT_NUMBER_PATTERN = Pattern.compile(".*[a-zA-Zㄱ-ㅎ가-힣].*");
 
     public DelimiterSplitter(String str) {
         this.str = str;
     }
 
     public String[] split() {
-        validateUserInput(str);
-
         if (str.isBlank()) {
             return new String[0];
         }
-        if (Pattern.matches(CUSTOM_DELIMITER, str)) {
-            return customDelimiterSplit();
+
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(str);
+        if (matcher.find()) {
+            return customDelimiterSplit(matcher);
         }
+
         return basicDelimiterSplit();
     }
 
     private String[] basicDelimiterSplit() {
+        validateUserInput(str);
         return str.split(BASIC_DELIMITER);
     }
 
-    private String[] customDelimiterSplit() {
-        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(str);
-        if (matcher.find()) {
-            String customDelimiter = matcher.group(1);
-            String numbers = matcher.group(2);
-            return numbers.isBlank() ? new String[0] : numbers.split(customDelimiter);
-        }
-        return new String[0];
+    private String[] customDelimiterSplit(Matcher matcher) {
+        String customDelimiter = matcher.group(1);
+        String numbers = matcher.group(2);
+        validateUserInput(numbers);
+        return numbers.isBlank() ? new String[0] : numbers.split(customDelimiter);
     }
 
     private static void validateUserInput(String str) {
-        if (str.matches(".*-\\d+.*") || str.matches("//(.)\n(.*-\\d+.*)")) {
+        validateNegativeNum(str);
+        validateNotNum(str);
+    }
+
+    private static void validateNegativeNum(String str) {
+        if (NEGATIVE_NUMBER_PATTERN.matcher(str).matches()) {
             throw new UserInputException("[계산 실패] 음수는 입력할 수 없습니다.");
-        } else if (str.matches(".*[a-zA-Zㄱ-ㅎ가-힣].*") || str.matches("//(.)\n(.*[a-zA-Zㄱ-ㅎ가-힣].*)")) {
+        }
+    }
+    private static void validateNotNum(String str) {
+        if (NOT_NUMBER_PATTERN.matcher(str).matches()) {
             throw new UserInputException("[계산 실패] 숫자 외에 영어나 한글은 입력할 수 없습니다.");
         }
     }
