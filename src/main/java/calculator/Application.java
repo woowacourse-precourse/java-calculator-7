7063c1f84c6aca08ed104640761f50fed42112bf
GeonHui Lee <spongging@gmail.com>
2024-10-20 23:30:50 +0900

    예외사항 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,14 +8,10 @@ import java.util.regex.Pattern;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try {
             System.out.println("덧셈할 문자열을 입력해 주세요.");
             String input = Console.readLine();
             int result = StringCalculator.calculate(input);
             System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-        }
     }
 
 
