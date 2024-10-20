a56ab3d80cb9c0f8fc62b4f3a474bf9ea27b061e
JamogFrinds <ghrb6769@gmail.com>
2024-10-20 22:51:45 +0900

    fix: 문자열을 실수로 변환할때 구분자로 인해 발생하는 오류 수정

--- src/main/java/calculator/Model/StringManagement.java
+++ src/main/java/calculator/Model/StringManagement.java
@@ -1,42 +1,33 @@
 package calculator.Model;
 
 import java.util.ArrayList;
-import java.util.List;
 
-class StringManagement {
-    String Delimiter = ",:";
+public class StringManagement {
+    String delimiter = ",:";
 
-    public void customDelimiter(String input){
+    public String customDelimiter(String input){
         int start = input.indexOf("//");
-        int last = input.indexOf("\n");
-
+        int last = input.indexOf("\\n");
         if(start != -1 && last != -1){
-            Delimiter += input.substring(start+1,last);
+            delimiter += input.substring(2,3);
+            input = input.substring(5);
         }
         else if(start != -1 || last != -1){
-            System.out.println("구분자를 분리할 수 없습니다.");
+            throw new IllegalArgumentException("구분자를 분리할 수 없습니다.");
         }
+        return input;
     }
 
-    public List<String> splitString(String input){
-        ArrayList<String> splitNum = new ArrayList<>();
-        customDelimiter(input);
-        String[] splitInput = input.split(Delimiter);
-        for(int i=0; i<splitInput.length; i++)
-            splitNum.add(splitInput[i]);
-        return splitNum;
-    }
-
-    public ArrayList<Double> strToInt(String input) {
-        ArrayList<Double> num = new ArrayList<>();
-        ArrayList<String> splitNum = new ArrayList<>(splitString(input));
-
-        for(int i=0; i<splitString(input).size(); i++) {
-            num.add(Double.parseDouble(splitNum.get(i)));
-            if (num.get(i) < 0){
-                System.out.println("음수는 입력할 수 없습니다.");
+    public ArrayList<Double> strToDouble(String input) {
+        ArrayList<Double> splitNum = new ArrayList<>();
+        String[] splitInput = input.split("["+delimiter+"]");
+        for(int i=0; i<splitInput.length; i++) {
+            if (splitInput[i].indexOf("-") != -1){
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
             }
+            double num = Double.parseDouble(splitInput[i]);
+            splitNum.add(num);
         }
-        return num;
+        return splitNum;
     }
 }
