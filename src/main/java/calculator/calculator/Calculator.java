3a29616ac1d86b5934935816a5754e5c40db3693
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-10-21 01:11:09 +0900

    feat: 덧셈 계산 후 결과 반환 로직 구현

--- src/main/java/calculator/calculator/Calculator.java
+++ src/main/java/calculator/calculator/Calculator.java
@@ -2,6 +2,7 @@ package calculator.calculator;
 
 import calculator.exception.ValidateInput;
 import calculator.io.InputString;
+import calculator.io.OutputResult;
 
 import java.util.List;
 
@@ -10,11 +11,13 @@ public class Calculator {
     private final InputString inputString;
     private final ValidateInput validateInput;
     private final StringParsing stringParsing;
+    private final OutputResult outputResult;
 
     public Calculator() {
         this.inputString = new InputString();
         this.validateInput = new ValidateInput();
         this.stringParsing = new StringParsing();
+        this.outputResult = new OutputResult();
     }
 
     //계산기 로직
@@ -30,5 +33,17 @@ public class Calculator {
 
         //숫자 추출
         List<Integer> integers = stringParsing.parseString(input);
+
+        //덧셈 계산 후 결과 반환
+        outputResult.printResult(calculateResult(integers));
+    }
+
+    //덧셈 계산
+    private int calculateResult(List<Integer> integers) {
+        int sum = 0;
+        for (int number : integers) {
+            sum += number;
+        }
+        return sum;
     }
 }
