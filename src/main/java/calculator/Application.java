552bc7b1d00ff27fea43a83b4dad742491196e0f
etd93 <etd93@naver.com>
2024-10-20 18:09:52 +0900

    [fix] Application의 try-catch문 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,11 +5,13 @@ import calculator.controller.CalculatorController;
 public class Application {
 
     public static void main(String[] args) {
-        try{
-            CalculatorController calculatorController = new CalculatorController();
-            calculatorController.startCalculating();
-        }catch(IllegalArgumentException e){
-
-        }
+//        try{
+//            CalculatorController calculatorController = new CalculatorController();
+//            calculatorController.startCalculating();
+//        }catch(IllegalArgumentException e){
+//            throw e;
+//        }
+        CalculatorController calculatorController = new CalculatorController();
+        calculatorController.startCalculating();
     }
 }
