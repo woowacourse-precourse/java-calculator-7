a752ecc2c28d48917ed071017b685c30be1ca01d
smyeongL <sangmyeonglee123@gmail.com>
2024-10-20 13:47:45 +1100

    feat(application): declare context class, Calculator, with Add strategy

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // Declare Strategy Context Class, Calculator
+        Calculator addCalculator = new Calculator(new Add());
     }
+
 }
