5dc7c129568f04cfd397010e7e9030ca747fd272
18hailey <hailey9459@gmail.com>
2024-10-20 15:26:17 +0900

    test(ApplicationTest): add basic tests and reset delim during test execution
    - tokenize using a custom delimiter
    - handle a case where default and custom delimiters are mixed

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,13 +1,21 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+
+    @BeforeEach
+    void setUp() {
+        Application.delim = new StringBuilder(",:");
+    }
+
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -19,9 +27,25 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 커스텀_구분자로_분리() {
+        assertSimpleTest(() -> {
+            run("1,2//;\\n3;4;5");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
+    @Test
+    void 기본_구분자와_커스텀_구분자_혼재() {
+        assertSimpleTest(() -> {
+            run("1,2:3//;\\n4;5:6,7//$\\n8:9;10,11$12");
+            assertThat(output()).contains("결과 : 78");
+        });
     }
 
     @Override
