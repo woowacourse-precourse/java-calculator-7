69fdd23e9448197f23f6994e135aa3b96af362fd
yunjeooong <yunjeongssu@gmail.com>
2024-10-20 23:09:01 +0900

    feat: 계산 결과 출력 기능 추가
    - OutputView 클래스 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
     }
+
 }
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,7 @@
+package calculator.view;
+
+public class OutputView {
+    public void printResult(int result) {
+        System.out.println("결과 : " + result);  // 콜론 뒤에 공백 추가
+    }
+}
