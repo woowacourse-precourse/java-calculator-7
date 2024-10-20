63b36ab30b74502a3fdbdd26df9165bdbca9a2e0
chaerish <ccyy8432@naver.com>
2024-10-21 04:09:47 +0900

    refactor(CustomSeparator): 커스텀 구분자를 정규 표현식으로 수정

--- src/main/java/calculator/separator/CustomSeparator.java
+++ src/main/java/calculator/separator/CustomSeparator.java
@@ -2,22 +2,25 @@ package calculator.separator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomSeparator implements Separator {
     private String separator = "";
     private final String customPrefix = "//";
     private final String customSuffix = "\\n";
+    private final String regex = "//([^0-9])\\\\n((\\d+\\1)*\\d+)";
 
     @Override
     public List<Long> separate(String userInput) {
-        int pIndex = userInput.indexOf(customPrefix);
-        int sIndex = userInput.indexOf(customSuffix);
-        if (sIndex == -1 || pIndex == -1) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(userInput);
+        if (!matcher.matches()) {
             throw new IllegalArgumentException();
         }
-        this.separator = userInput.substring(pIndex + customPrefix.length(), sIndex);
+        this.separator = matcher.group(1);
 
-        return separateNumbers(userInput.substring(sIndex + customSuffix.length()));
+        return separateNumbers(matcher.group(2));
     }
 
     @Override
