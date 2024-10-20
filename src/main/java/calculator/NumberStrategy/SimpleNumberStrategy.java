e0d25d9fc54147ac0840cad0a5316f64e0d35fe3
tibetyo <zjhj0814@gamil.com>
2024-10-20 13:46:06 +0900

    feat(NumberStrategy): 숫자 추출 인터페이스와 구현체 생성
    NumberStrategyTest 컴파일을 위해 숫자 추출 인터페이스와 구현체를 생성한다

---
+++ src/main/java/calculator/NumberStrategy/SimpleNumberStrategy.java
@@ -0,0 +1,13 @@
+package calculator.NumberStrategy;
+
+import java.util.List;
+
+public class SimpleNumberStrategy implements NumberStrategy {
+    public SimpleNumberStrategy() {
+    }
+
+    @Override
+    public List<Integer> extractNumber(String userInput, List<String> delimiter) {
+        return null;
+    }
+}
