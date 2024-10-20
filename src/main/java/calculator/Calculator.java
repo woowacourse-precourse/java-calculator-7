26916cbcb48f1af088d0abee59b22cba62ab17c7
sseen2 <kse10023@naver.com>
2024-10-20 15:24:00 +0900

    feat: IllegalArgumentException 예외 처리 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,13 +3,14 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
-    public void run() {
+    public void run() throws IllegalArgumentException {
         String input = Console.readLine();
         int sum = 0;
         String[] inputSplit;
 
         if (input.startsWith("//")) {
             int start = input.indexOf("\\n");
+            if (start == -1) throw new IllegalArgumentException("잘못된 입력입니다: " + input);
             String delimiter = input.substring(2, start);
             String num = input.substring(start + 2);
             inputSplit = num.split(delimiter);
@@ -17,8 +18,11 @@ public class Calculator {
         else inputSplit = input.split(",|:");
 
         if (!input.isEmpty())
-            for (int i = 0; i < inputSplit.length; i++)
-                sum += Integer.parseInt(inputSplit[i]);
+            for (int i = 0; i < inputSplit.length; i++) {
+                int num = Integer.parseInt(inputSplit[i]);
+                if (num < 0) throw new IllegalArgumentException("잘못된 입력입니다: " + num);
+                sum += num;
+            }
 
         System.out.println("결과 : " + sum);
     }
