f4988aed92def918f24ef7e69beb8103ca80b1a1
dragonwaterr <yongsu626@naver.com>
2024-10-20 18:39:28 +0900

    feat : 커스텀 구분자를 찾아서 반환하는 기능 추가

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,14 +1,31 @@
 package calculator.view;
 
 
+import calculator.validation.*;
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputView {
     private InputView() {};
 
     public static String inputString() {
+        String customSeperator = "";
         System.out.print(ViewMessage.INPUT_STRING);
         String input = Console.readLine();
+        customSeperator = getCustomSeperator(input);
         return input;
+    }
+    private static String getCustomSeperator(String input) {
+        String seperator = "";
+        Pattern pattern = Pattern.compile("//(.+)\\\\n");
+        Matcher matcher = pattern.matcher(input);
+
+        if(matcher.find()) {
+            seperator = matcher.group(1);
+        }
+
+        return seperator;
     }
 }
