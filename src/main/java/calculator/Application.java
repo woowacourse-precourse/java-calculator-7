14958524e0595e5089d75669f7a2d9a279c0f227
RedSunSmile <naver-or@hanmail.net>
2024-10-20 18:34:28 +0900

    main에서 사용자입력 정의 및 작동호출로직

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import java.util.*;
+
 public class Application {
+    private String insert;
+    private int sum;
+
+    public void changingNumber() {
+        System.out.println("뎃셈할 문자열을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        insert = sc.nextLine();
+        TextNumber plus = new TextNumber();
+        plus.calculatingNumberPlus(insert);
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application change = new Application();
+        change.changingNumber();
     }
 }
