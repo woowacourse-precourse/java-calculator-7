c09aa410885f6a8f4fcf282f0f89247c3debada1
wjdtjdgns <person3275@gmail.com>
2024-10-20 21:49:39 +0900

    fix(parser): 커스텀 구분자 이스케이프 추가

--- src/main/java/calculator/util/DelimiterParser.java
+++ src/main/java/calculator/util/DelimiterParser.java
@@ -3,6 +3,7 @@ package calculator.util;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class DelimiterParser {
     private static final List<String> DELIMITERS = Arrays.asList(",", ":");
@@ -14,7 +15,7 @@ public class DelimiterParser {
             int delimiterEndIndex = input.indexOf("\\n");
             if (delimiterEndIndex != -1) {
                 String customDelimiter = input.substring(2, delimiterEndIndex);
-                delimiters.add(customDelimiter);
+                delimiters.add(Pattern.quote(customDelimiter));
             }
         }
 
