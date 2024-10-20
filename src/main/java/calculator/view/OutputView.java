a66ca0c59e30dea373b2e77c5af929a629c7aad9
yunsseong <aster20@hanyang.ac.kr>
2024-10-20 19:09:59 +0900

    feat: 메세지 프린트 기능 구현

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public void printMessage(String message) {
         System.out.println(message);
     }
+
+    public void printMessageWithNumber(String message, int number) {
+        System.out.print(message + number);
+    }
 }
