6135d88ac7694dc39e40bcefb9432cad7ef685f2
jhan0121 <jhan0121@gmail.com>
2024-10-20 20:27:15 +0900

    fix(Splitter): 커스텀 구분자 사용 여부 로직 및 대상 구분자 문자열 내 정규식 메타 문자 처리 로직 추가
    커스텀 구분자와 기본 구분자를 모두 사용하는 splitter로 구성하는 로직으로 수정
    String.split() 함수내 파라미터로 구분자를 제공하기 이전에 정규식 메타 문자를 처리하는 로직 추가

--- src/main/java/calculator/service/Splitter.java
+++ src/main/java/calculator/service/Splitter.java
@@ -1,12 +1,19 @@
 package calculator.service;
 
 import calculator.model.Global;
+import java.util.List;
 
 public class Splitter {
 
     private static final String CUSTOM_SPLIT_START = "//";
     private static final String CUSTOM_SPLIT_END = "\\n";
     private static final String BASIC_SPLITTER = ",|;";
+    private static final List<String> META_CHARACTER = List.of(
+            "^", "$", "[", "]", "{",
+            "}", "(", ")", ".", "*",
+            "+", "?", "|", "/", "\\"
+    );
+
     private final Global global;
 
     public Splitter() {
@@ -18,11 +25,23 @@ public class Splitter {
     public void findSplitter() {
         String input = global.getInput();
         if (input.startsWith(CUSTOM_SPLIT_START)) {
-            String splitter = input.substring(CUSTOM_SPLIT_START.length(),
-                    input.indexOf(CUSTOM_SPLIT_END));
+            String splitter = changeMetaChar(input);
             global.setSplitStr(splitter);
             removeCustomSplitterFromInput();
         }
+    }
+
+    private String changeMetaChar(String input) {
+        StringBuilder splitter = new StringBuilder(global.getSplitStr() + "|");
+        String[] customSplitter = input.substring(CUSTOM_SPLIT_START.length(), input.indexOf(CUSTOM_SPLIT_END))
+                .split("");
+        for (String i : customSplitter) {
+            if (META_CHARACTER.stream().anyMatch(str -> str.equals(i))) {
+                splitter.append("\\");
+            }
+            splitter.append(i);
+        }
+        return splitter.toString();
     }
 
     // 대상 문자열 내 커스텀 구분자 판정 파트 제거
