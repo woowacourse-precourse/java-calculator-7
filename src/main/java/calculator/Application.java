18f14c1d955207de6aa24029388902f2c5ed7e67
Huijun Seo <gjshj366@gmail.com>
2024-10-20 16:22:22 +0900

    feat(application/input): add input handling for string addition

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO : input, process, output
     }
 }
+
+
+class Input {
+    private static final String INPUT_INSTRUCTION = "덧셈할 문자열을 입력해 주세요.";
+
+    public static String read(){
+        System.out.println(INPUT_INSTRUCTION);
+        return Console.readLine();
+    }
+}
+
