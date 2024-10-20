4ea21eb8652fc4550eef13a18db466b44e603acf
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-10-20 18:41:17 +0900

    feat: 문자열에서 구분자를 기준으로 숫자들을 추출하고 합을 구하는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,7 +6,7 @@ public class Application {
         StringCalculator stringCalculator = new StringCalculator();
         stringCalculator.setInput();
         stringCalculator.addDelimiter();
-
+        stringCalculator.sumDelimitedNumbers();
     }
 }
 
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,7 @@
 package calculator;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Queue;
 
 /**
  *  1. 문자열을 입력받는다.
@@ -20,7 +18,6 @@ public class StringCalculator {
     // 숫자 구분자
     private String delimiter;
     // 추출한 숫자 리스트
-    private String[] numList;
 
     public StringCalculator(){
         this.sum = 0;
@@ -36,20 +33,31 @@ public class StringCalculator {
     // 커스텀 구분자를 구하는 기능
     public void addDelimiter(){
         // 커스텀 구분자가 추가될려면 문자열 길이 5 이상
-        if(input.length() < 5){
-            return;
-        }
-        for(int i=0; i<input.length()-5; i+=5){
-            if(input.startsWith("//", i) && input.startsWith("\\n", i+3)){
-                delimiter += input.charAt(i+2);
-                // 원본 문자열에서 커스텀문자열 추가 부분 제거
-                this.input = input.substring(0,i) + input.substring(i+5);
-                System.out.println(input.charAt(i+2));
+        if(input.length() >= 5) {
+            for (int i = 0; i < input.length() - 5; i += 5) {
+                if (input.startsWith("//", i) && input.startsWith("\\n", i + 3)) {
+                    delimiter += input.charAt(i + 2);
+                    // 원본 문자열에서 커스텀문자열 추가 부분 제거
+                    this.input = input.substring(0, i) + input.substring(i + 5);
+                    System.out.println(input.charAt(i + 2));
+                }
             }
         }
         delimiter+="]";
-        System.out.println(delimiter);
+        System.out.println("delimiter : " + delimiter);
     }
 
-
+    // 문자열에서 구분자를 기준으로 숫자들을 추출하고 합을 구하는 기능
+    public void sumDelimitedNumbers(){
+        String[] numList = input.split(delimiter);
+        System.out.println("numList : "+Arrays.toString(numList));
+        for(String c : numList){
+            // 구분자를 제외한 문자가 있을경우 예외처리
+            if(!c.matches("^[0-9]*$")){
+                throw new IllegalArgumentException("잘못된 값을 입력하였습니다");
+            }
+            this.sum += Integer.parseInt(c);
+        }
+        System.out.println("sum : "+sum);
+    }
 }
