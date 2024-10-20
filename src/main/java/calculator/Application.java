a1d673dcb191ec496c5e1b84b5a15e7f3ba6821b
eunseongu <rndmstjs75@gmail.com>
2024-10-20 16:21:42 +0900

    feat: 커스텀 구분자 추출 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,27 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
+    public static String[] getCustomDelimiterAndContent(String input) {
+        if (input.startsWith("//")) {
+            int delimiterEndIndex = input.indexOf("\\n");
+            if (delimiterEndIndex != -1) {
+                String customDelimiter = input.substring(2, delimiterEndIndex);
+                String content = input.substring(delimiterEndIndex + 1);
+                return new String[]{customDelimiter, content};
+            }
+        }
+        return new String[]{"", input};
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your input");
+        String input = scanner.nextLine();
+
+        String[] delimiterAndContent = getCustomDelimiterAndContent(input);
+        System.out.println("Custom Delimiter: " + delimiterAndContent[0]);
+        System.out.println("Content: " + delimiterAndContent[1]);
     }
 }
