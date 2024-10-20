2e724d05a361e1e9405d141493e1dfd0cd78b929
Yoo SeungHyeon <effort-result@naver.com>
2024-10-20 15:00:25 +0900

    feat(Logic): add new Logic - create "Calculator.java"

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the numeric string to add");
+        String input = scanner.nextLine();
+
+        Calculator sentence = new Calculator(input);
+        sentence.calculate();
     }
 }
