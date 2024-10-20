73ef2ec2ee48846992a8fe574da6927d904f715f
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:39:29 +0900

    :sparkles: feat(SumCalculator): 숫자의 유효성을 검사하고 파싱

--- src/main/java/calculator/model/SumCalculator.java
+++ src/main/java/calculator/model/SumCalculator.java
@@ -1,4 +1,17 @@
 package calculator.model;
 
 public class SumCalculator {
+    private final NumberValidator numberValidator;
+
+    public SumCalculator() {
+        this.numberValidator = new NumberValidator();
+    }
+
+    public int calculateSum(String[] numbers) {
+        int total = 0;
+        for (String number : numbers) {
+            total += numberValidator.validateAndParse(number);
+        }
+        return total;
+    }
 }
