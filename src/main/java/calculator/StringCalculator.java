4f13726f6e262e6f9e28f2552f776358dee8d66a
cba700 <dlackddls5@naver.com>
2024-10-20 22:02:32 +0900

    feat : 문자열 매치 메서드 matchPattern 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,5 +1,24 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
-    // 나중에 메서드 추가 예정
+    public static int calculate(String input) {
+        if (isDefaultPattern(input)) {
+            // 합해주는 메소드 구현 예정
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    private static boolean isDefaultPattern(String input) {
+        return matchPattern(input, "^\\d*([,:]\\d+)*$");
+    }
+
+    private static boolean matchPattern(String input, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
 }
