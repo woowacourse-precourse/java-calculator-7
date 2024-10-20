8fc484402c29a663911e5dc0844a91729f45fb28
MinJae00 <exceedmin@gmail.com>
2024-10-20 22:49:18 +0900

    refact: 컨트롤러 업데이트

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,27 @@
 package calculator;
-import camp.nextstep.edu.missionutils.Console;
+
+import calculator.calculate.Calculate;
+import calculator.calculate.ParsingString;
+import calculator.exception.Exception;
+import calculator.global.GlobalConstant;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+import java.util.ArrayList;
+
 public class Application {
 
     public static void main(String[] args) {
+        GlobalConstant.StartIndex = 0;
+        ArrayList<Integer> numlist;
         InputView input_view = new InputView();
-        Checkdelemeter check_delemeter = new Checkdelemeter();
+        OutputView output_view = new OutputView();
         String input = input_view.getInput();
-        char[] delemeter = new char[3];
-        delemeter = check_delemeter.AddDelemeter(input);
-        Parsing parsing = new Parsing(delemeter);
-        parsing.MakeNumList(input);
+        Exception.IsEmptyInput(input);
+        ParsingString check_delemeter = new ParsingString(input);
+        check_delemeter.AddDelemeter();
+        numlist = check_delemeter.MakeNumList();
+        int result = Calculate.Makesum(numlist);
+        output_view.output(result);
     }
-
 }
