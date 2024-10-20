6cd021da885781e97d457b7d480eb5feb214551d
dack3 <joyunjeong75@gmail.com>
2024-10-20 18:17:50 +0900

    feat:  ,와 ;을 구분자로 배열에 저장하고 배열의 합을 구한다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String str = "1,2;34";
+        String[] num = str.split("[,;]");
+
+        int sum = 0;
+        for (String cal : num){
+            sum +=Integer.parseInt(cal);
+        }
+        System.out.println(sum);
     }
 }
