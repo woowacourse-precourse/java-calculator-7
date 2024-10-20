c16880931afc4a5d674514a296bcac68ea976001
momoysm <90163578+momoysm@users.noreply.github.com>
2024-10-20 17:20:51 +0900

    refactor: Application파일 try catch 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,11 +4,6 @@ import calculator.controller.CalculatorController;
 
 public class Application {
     public static void main(String[] args) {
-        try{
-            new CalculatorController().run();
-        } catch(IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-        }
-
+        new CalculatorController().run();
     }
 }
