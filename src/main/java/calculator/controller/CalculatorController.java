f07196d16e86590a20d81f2bd9f1fbe1c1cfe90e
dnwldla <leeemjw@naver.com>
2024-10-21 07:14:02 +0900

    refactor: 컨트롤러 수정

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,6 +1,5 @@
 package calculator.controller;
 
-import calculator.domain.Calculator;
 import calculator.domain.UserInput;
 import calculator.view.InputView;
 import calculator.view.OutputView;
@@ -10,8 +9,12 @@ public class CalculatorController {
     public static void run(){
         OutputView.printCommand();
         String input=InputView.enterInput();
+
         UserInput userInput=new UserInput(input);
+        userInput.checkExtractor();
+
         int result=userInput.startCalculate();
-       OutputView.printResult(result);
+
+        OutputView.printResult(result);
     }
 }
