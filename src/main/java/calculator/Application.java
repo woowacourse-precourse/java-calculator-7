45f9cdcf53a960e3831d15c0b2c0250ce3ddbca6
김재형 <kjhstar1124@naver.com>
2024-10-20 13:13:35 +0900

    refactor: 기본 구분자 확인여부 함수 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,22 +6,17 @@ public class Application {
     public static void main(String[] args) {
         String inputText = Console.readLine();
         int totalSum;
-        // 입력 받은 문자열이 "커스텀 문자열"로 시작하는지 확인
+
         if (isCustomSeparator(inputText)) {
             totalSum = getTotalSum(inputText, true);
+        } else if (isDefaultSeparator(inputText)) {
+            totalSum = getTotalSum(inputText, false);
         } else {
-            // 커스텀 문자열로 시작하지 않는다면, 기본 구분자를 포함하는지 확인
-            if (inputText.contains(",") || inputText.contains(":")) {
-                // 기본 구분자를 포함한다면, 이를 기준으로 분리;
-                totalSum = getTotalSum(inputText, false);
-            } else {
-                // 기본 구분자도 포함하지 않는다면, 잘못된 입력
                 System.out.println("입력 오류: 올바른 구분자를 입력해주세요");
                 throw new IllegalArgumentException();
-            }
         }
         System.out.printf("결과 : %d\n", totalSum);
-    }
+        }
 
     private static boolean isCustomSeparator(String inputText) {
         if (inputText.startsWith("//")) {
@@ -29,15 +24,14 @@ public class Application {
                 // "//문자\n" 형태라면 올바른 입력
                 if (inputText.startsWith("\\n", 3)) {
                     return true;
-                } else {
-                    // 에러 식별
-                    if (inputText.contains("\\n")) {
-                        System.out.println("입력 오류: 커스텀 구분자가 두 글자 이상입니다.");
-                    } else {
-                        System.out.println("입력 오류: 올바른 커스텀 구분자 양식이 아닙니다.");
-                    }
-                    throw new IllegalArgumentException();
                 }
+                // 에러 식별
+                if (inputText.contains("\\n")) {
+                    System.out.println("입력 오류: 커스텀 구분자가 두 글자 이상입니다.");
+                } else {
+                    System.out.println("입력 오류: 올바른 커스텀 구분자 양식이 아닙니다.");
+                }
+                throw new IllegalArgumentException();
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("입력 오류: 커스텀 구분자 양식을 확인해주세요");
                 throw new IllegalArgumentException();
@@ -46,15 +40,17 @@ public class Application {
         return false;
     }
 
+    private static boolean isDefaultSeparator(String inputText) {
+        return inputText.contains(",") || inputText.contains(":");
+    }
+
     private static int getTotalSum(String inputText, boolean isCustomSeparator) {
         String[] splitText;
         int totalSum = 0;
-        if (isCustomSeparator) {
-            String separator = inputText.substring(2, 3);
-            splitText = inputText.substring(5).split(separator);
-        } else {
-            splitText = inputText.split("[,:]");
-        }
+
+        String separator = isCustomSeparator ? inputText.substring(2, 3) : "[,:]";
+        splitText = isCustomSeparator ? inputText.substring(5).split(separator) : inputText.split(separator);
+
         for (String num : splitText) {
             try {
                 int inputNum = Integer.parseInt(num);
