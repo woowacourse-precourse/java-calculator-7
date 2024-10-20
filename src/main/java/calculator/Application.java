a321a7063a9b0697aeff702b91691ca2268e7889
AYEOOON <k93853471@gmail.com>
2024-10-20 15:34:04 +0900

    refactor: 계산 로직을 CalculateLogic 클래스로 분리
    - 계산 관련 기능을 CalculateLogic 클래스로 이동
    - Application 클래스에서는 CalculateLogic 클래스를 호출하여 계산 수행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,17 +2,7 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        String input = GetInput.input_String();
-
-        // 구분자 추출
-        String delimiter = ExtractDelimiter.extract_delimiter(input);
-
-        // 문자열 전처리
-        String refine_string = SplitInput.refine(input);
-
-        // 문자열 분리
-        String[] nums = SplitInput.split(refine_string, delimiter);
-
-        System.out.println("결과 : " + AddingNum.sum(nums));
+        CalculateLogic calculator = new CalculateLogic();
+        calculator.calculate();
     }
 }
---
+++ src/main/java/calculator/CalculateLogic.java
@@ -0,0 +1,18 @@
+package calculator;
+
+public class CalculateLogic {
+    public void calculate() {
+        String input = GetInput.input_String();
+
+        // 구분자 추출
+        String delimiter = ExtractDelimiter.extract_delimiter(input);
+
+        // 문자열 전처리
+        String refine_string = SplitInput.refine(input);
+
+        // 문자열 분리
+        String[] nums = SplitInput.split(refine_string, delimiter);
+
+        System.out.println("결과 : " + AddingNum.sum(nums));
+    }
+}
