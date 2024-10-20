c8c7be199a35d8b63a3217419a66edf18249fa37
Dohun Kim <dohun8758@naver.com>
2024-10-20 16:07:21 +0900

    fix : 문자열 파싱 기능
    - 커스텀 구분자 파싱기능 오류 수정

--- src/main/java/calculator/util/StringParser.java
+++ src/main/java/calculator/util/StringParser.java
@@ -7,13 +7,16 @@ public class StringParser {
     public static final char[] STANDARD_SEPARATOR = {',', ':'};
 
     public static String[] split(String input) {
-        String separator = Arrays.toString(STANDARD_SEPARATOR);
+        StringBuilder separator = new StringBuilder();
+        for (char c : STANDARD_SEPARATOR) {
+            separator.append(c);
+        }
 
         if (CustomSeparatorParser.hasCustomSeparator(input)) {
-            separator += CustomSeparatorParser.getCustomSeparator(input);
+            separator.append(CustomSeparatorParser.getCustomSeparator(input));
             input = input.substring(5);
         }
 
-        return input.split(Arrays.toString(STANDARD_SEPARATOR));
+        return input.split("[" + separator.toString() + "]");
     }
 }
