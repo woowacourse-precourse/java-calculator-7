69bc8f80dcee46ecacd4028904eb7dfd5683ea68
1998ray <1998ray98@gmail.com>
2024-10-21 08:06:15 +0900

    feat: 커스텀 구분자 지정 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -13,24 +13,36 @@ public class Application {
         System.out.println("결과 : "+ result);
     }
 
-        // 1. 빈 문자열 또는 null 입력 시 0을 반환
     public static int add(String input) {
+        // 1. 빈 문자열 또는 null 입력 시 0을 반환
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        
-        // 2. 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열의 숫자 합을 반환
-        String[] numbers = input.split("[,|:]");
-        int sum = 0;
-        for (String number : numbers) {
-            sum += Integer.parseInt(number);
-        }
-        return sum;
 
+        // 커스텀 구분자가 있는 경우
         // 3. 커스텀 구분자를 지정하여 숫자의 합을 반환
+        if (input.startsWith("//")) {
+            int Index = input.indexOf("\n"); // 커스텀 구분자 끝
+            String delimiter = input.substring(2, Index); // 커스텀 구분자 추출
+            input = input.substring(Index + 1); // 커스텀 구분자 다음부터 입력으로 설정
+
+            String[] numbers = input.split(delimiter);
+            int sum = 0;
+            for (String number : numbers) {
+            sum += Integer.parseInt(number);
+            }
+            return sum;
+        } 
+        else {
+        // 2. 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열의 숫자 합을 반환
+            String[] numbers = input.split("[,|:]");
+            int sum = 0;
+            for (String number : numbers) {
+            sum += Integer.parseInt(number);
+            }
+            return sum;
+        }
         // 4. 잘못된 입력 시 IllegalArgumentException을 발생
     }
     
-
-
 }
