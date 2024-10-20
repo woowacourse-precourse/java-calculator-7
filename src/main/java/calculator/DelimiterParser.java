2114d295c28c6951a9c02fa7ccf627bd24bfd9ec
SeoYoung <seoyoung7623@naver.com>
2024-10-21 01:25:14 +0900

    feat(parser): 커스텀 구분자 처리 기능 추가
    문자열에서 //와 \n 사이에 커스텀 구분자가 있을 경우 해당 구분자를 처리하도록 구현

--- src/main/java/calculator/DelimiterParser.java
+++ src/main/java/calculator/DelimiterParser.java
@@ -2,13 +2,37 @@ package calculator;
 
 public class DelimiterParser {
     private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
 
-    public String[] parse(String input) {
-        String[] split = split(input, DEFAULT_DELIMITER);
-        return split;
+    public String parse(String input) {
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            int delimiterIndex = findDelimiterIndex(input);
+            String customDelimiter = input.substring(CUSTOM_DELIMITER_PREFIX.length(), delimiterIndex);
+
+            return join(customDelimiter);
+        }
+        return DEFAULT_DELIMITER;
     }
 
-    private String[] split(String input, String delimiter) {
-        return input.split(delimiter);
+    public String extractNumbers(String input) {
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            int delimiterIndex = findDelimiterIndex(input);
+            String substring = input.substring(delimiterIndex + CUSTOM_DELIMITER_SUFFIX.length());
+            return substring;
+        }
+        return input;
+    }
+
+    private int findDelimiterIndex(String input) {
+        int delimiterIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+        if (delimiterIndex == -1) {
+            throw new IllegalArgumentException("\"\\n\" 문자가 존재하지 않습니다.");
+        }
+        return delimiterIndex;
+    }
+
+    private String join(String customDelimiter) {
+        return DEFAULT_DELIMITER + "|" + String.join("|", customDelimiter.split(""));
     }
 }
