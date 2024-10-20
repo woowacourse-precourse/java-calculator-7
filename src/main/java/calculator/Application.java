3d60bc744efa8a0e9f5dd9f50203519a07d06564
Now <khj990322@gmail.com>
2024-10-21 07:21:53 +0900

    fix: Add space to result sout

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,7 +12,7 @@ public class Application {
             System.out.println("덧셈할 문자열을 입력해 주세요.");
             String input = Console.readLine();
             int res = calSum(input);
-            System.out.println("결과: " + res);
+            System.out.println("결과 : " + res);
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(e);
         }
