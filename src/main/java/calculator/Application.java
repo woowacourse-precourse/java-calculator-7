3f12b89c9171d3512e0cddd901ec6ac54887da77
yoona <103414935+yoonali@users.noreply.github.com>
2024-10-21 00:39:31 +0900

    feat: Update Main method

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,7 +8,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Calculator.init();
     }
 }
 
