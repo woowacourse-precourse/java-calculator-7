81808a5a9ba73968e39006b738a240557bd6d5f8
Jang-GO <jang8195@naver.com>
2024-10-20 20:11:40 +0900

    feat(CustomStringParser) : 커스텀 구분자 parser 구현
    - 기본(콤마, 콜론)구분자 및 커스텀 구분자 기준으로 파싱하여 숫자만 담긴 List<String>를 반환

--- src/main/java/calculator/parser/CustomStringParser.java
+++ src/main/java/calculator/parser/CustomStringParser.java
@@ -1,10 +1,29 @@
 package calculator.parser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class CustomStringParser implements StringParser{
+    private static final String BASIC_DELIMITER_REGEX= "[,|:]"; // 기본 구분자 ,와 :
+    private String customDelimiter;
+    public void parseDelimiter(String input){
+        if (input.startsWith("//")) {
+            int delimiterEndIndex = input.indexOf("\n");
+            customDelimiter = input.substring(2, delimiterEndIndex);
+        }
+    }
+
     @Override
-    public List<String> parse(String str) {
-        return null;
+    public List<String> parse(String input) {
+        parseDelimiter(input);
+        String str = input.startsWith("//") ? input.split("\n", 2)[1] : input;
+
+        String customRegex = customDelimiter != null
+                ? BASIC_DELIMITER_REGEX + "|" + Pattern.quote(customDelimiter)
+                : BASIC_DELIMITER_REGEX;
+
+        return Arrays.stream(str.split(customRegex)).toList();
     }
 }
