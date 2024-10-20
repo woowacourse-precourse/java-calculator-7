31d3849a25473d8e8f602dce6d64ceeeeadbf663
Seung Won Lee <devsolfe@gmail.com>
2024-10-20 20:57:01 +0900

    feat: change Applcation logic
    Now Application use Tokenizer & Calculator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,12 +3,17 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
+        Calculator calculator = new Adder();
         String input;
         int result = 0;
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         input = Console.readLine();
+
+        int[] tokens = new Tokenizer(input).toArray();
+        result = calculator.calculate(tokens);
+
         System.out.println("결과 : " + result);
     }
 }
