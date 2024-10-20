8b7899713b681792d61a2a49502891a0895cfd82
kimshinwoo <tlsdn67@naver.com>
2024-10-20 20:24:58 +0900

    feat: 기본 구분자로 문자열 분류

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,14 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args){
         // TODO: 프로그램 구현
         String input = Console.readLine();
-        System.out.println(input);
+        String[] result = input.split("[:,]");
+        System.out.println(Arrays.toString(result));
+
     }
 }
