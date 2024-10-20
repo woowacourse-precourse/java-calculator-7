07fccbcdc73052baa5bd22488a64fb181a3677cb
cksgud0403 <cksgud0403@naver.com>
2024-10-20 19:57:43 +0900

    feat: add input handling functionality

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+
+        if(input.isEmpty()) {
+            System.out.println("결과 : 0");
+            return;
+        }
     }
 }
