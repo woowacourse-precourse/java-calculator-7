6a2661ba4bbd159a924ed670927febb17fa299b0
princebyun <princebyun@gmail.com>
2024-10-20 17:09:07 +0900

    사용자가 입력할떄 입력값에 대한 빈값 체크 유무 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,19 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-
-        
-
-
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String userInputText = readLine();
+        try {
+            if (userInputText == null || userInputText.trim().isEmpty()) {
+                throw new IllegalArgumentException("입력한 값이 없습니다.다시 확인 후 다시 입력해주세요");
+            }
+            System.out.println("결과 : " + userInputText);
+        } catch (IllegalArgumentException e) {
+            System.out.println("오류: " + e.getMessage());
+        }
     }
 }
