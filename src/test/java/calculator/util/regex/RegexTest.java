5caaab4cefc4bc5efca6e0092dc090144e332c67
Mint <audwl03071@gmail.com>
2024-10-21 03:46:50 +0900

    test: regex 테스트 작성

--- src/test/java/calculator/util/regex/RegexTest.java
+++ src/test/java/calculator/util/regex/RegexTest.java
@@ -9,13 +9,27 @@ import org.junit.jupiter.api.Test;
 class RegexTest {
 
     @Test
-    @DisplayName("정규식에 메타문자가 아닌 문자를 더한다")
-    void 성공_정규식_메타문자X() {
+    @DisplayName("정규식에 문자를 더한다")
+    void 성공_정규식_문자() {
         // Given
         Regex regex = new Regex("a");
 
         // When
         regex.add("b");
+
+        // Then
+        Regex expectedRegex = new Regex("a\\Qb\\E");
+        assertThat(regex).isEqualTo(expectedRegex);
+    }
+
+    @Test
+    @DisplayName("정규식에 문자를 이어서 더한다")
+    void 성공_정규식_이어진문자() {
+        // Given
+        Regex regex = new Regex("a");
+
+        // When
+        regex.addContinuously("b");
 
         // Then
         Regex expectedRegex = new Regex("a|\\Qb\\E");
