8baaa34d5fb8a16ff3fbbf4b2909bcd622a4af1e
BHC-Chicken <tij05078@gmail.com>
2024-10-21 05:37:44 +0900

    feat(calculation): 커스텀 구분자를 이용한 합산 기능 추가
    사용자가 지정한 문자열을 구분자로 이용해 문자열을 구분하고 합산하는 기능입니다.

--- src/main/java/calculator/CalculatorModel.java
+++ src/main/java/calculator/CalculatorModel.java
@@ -3,6 +3,9 @@ package calculator;
 import calculator.dto.CalculatorResultResponse;
 import calculator.dto.NumberRequest;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CalculatorModel {
 
     public CalculatorResultResponse calculateSum(NumberRequest request) {
@@ -10,24 +13,58 @@ public class CalculatorModel {
             throw new IllegalArgumentException("올바른 문자를 입력하세요.");
         }
 
+        if (request.word().isBlank()) {
+            return new CalculatorResultResponse(0);
+        }
+
+        String[] numbers;
         String word = request.word();
-        String[] numbers = word.split("[,:]");
+
+        if (word.startsWith("//")) {
+            numbers = validateRegularExpression(word);
+        } else {
+            numbers = validateNormalRegularExpression(word);
+        }
 
         int sum = 0;
 
-        try {
-            for (String number : numbers) {
-                sum += parseAndValidateNumber(Integer.parseInt(number));
-            }
-        } catch (Exception e) {
-            throw new IllegalArgumentException("올바른 구분자를 입력해주세요.");
+        for (String number : numbers) {
+            sum += parseAndValidateNumber(Integer.parseInt(number));
         }
 
         return new CalculatorResultResponse(sum);
     }
 
+    private String[] validateRegularExpression(String word) {
+        Pattern pattern = Pattern.compile("^//(.)\\\\n(.*)$");
+        Matcher matcher = pattern.matcher(word);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("커스텀 구분자에는 \\n 또는 숫자는 사용 불가능합니다.");
+        }
+
+        String customDelimiter = matcher.group(1);
+        String numbers = matcher.group(2);
+
+        for (int i = 0; i < customDelimiter.length(); i++) {
+            if (Character.isDigit(customDelimiter.charAt(i))) {
+                throw new IllegalArgumentException("커스텀 구분자에는 \\n 또는 숫자는 사용 불가능합니다.");
+            }
+        }
+
+        return numbers.split(customDelimiter);
+    }
+
+    private String[] validateNormalRegularExpression(String word) {
+        try {
+            return word.split("[,:]");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("올바른 구분자를 사용해 주세요.");
+        }
+    }
+
     private boolean validateNumberRequest(NumberRequest numberRequest) {
-        return numberRequest != null && numberRequest.word() != null && !numberRequest.word().isBlank();
+        return numberRequest != null && numberRequest.word() != null;
     }
 
     private int parseAndValidateNumber(int parsedNumber) {
