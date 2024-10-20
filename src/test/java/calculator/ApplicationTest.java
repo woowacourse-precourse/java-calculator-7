927e252bda9e53de2bb4bf79ad74f170588e586a
ziyoungkwon <gy011003@naver.com>
2024-10-20 16:28:38 +0900

    test: test 코드 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,6 +9,54 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void 빈문자() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 한개의_숫자() {
+        assertSimpleTest(() -> {
+            run("5");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+    @Test
+    void 구분자_쉼표_사용() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output().contains("결과 : 6"));
+        });
+    }
+
+    @Test
+    void 구분자_콜론_사용() {
+        assertSimpleTest(() -> {
+            run("3:4:5");
+            assertThat(output()).contains("결과 : 12");
+        });
+    }
+
+    @Test
+    void 구분자_둘다_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용2() {
+        assertSimpleTest(() -> {
+            run("//-\\n1-2-3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
