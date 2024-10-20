abc6fb129703025081f149b0a039f358085076de
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2024-10-20 15:42:04 +0900

    fix : import 문 오류 수정
    import 문이 잘못되어서 발생하는 오류 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,14 @@
 package calculator;
 
-import calculator.domain.service.CalculateService;
+import calculator.domain.controller.CalculatorController;
+import calculator.domain.service.CalculatorService;
 import calculator.domain.service.NormalCalculatorImpl;
 import calculator.domain.service.SpecialCalculatorImpl;
 
 public class Application {
     public static void main(String[] args) {
-        CalculateService calculateService = new CalculateService(new NormalCalculatorImpl(), new SpecialCalculatorImpl());
-        domain.controller.controller.CalculatorController controller = new domain.controller.controller.CalculatorController(calculateService);
+        CalculatorService calculatorService = new CalculatorService(new NormalCalculatorImpl(), new SpecialCalculatorImpl());
+        CalculatorController controller = new CalculatorController(calculatorService);
 
         controller.processCalculation();
 
