ff9f96c62215d0b05146d950b18cc98d4053cbda
heychi <heychi0728@gmail.com>
2024-10-20 16:04:58 +0900

    fix: calculator project 2nd commit

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -37,12 +37,12 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("계산할 문자열을 입력하세요:");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
         try {
             int result = add(input);
-            System.out.println("결과: " + result);
+            System.out.println("결과 : " + result);
         } catch (IllegalArgumentException e) {
             return;
         }
