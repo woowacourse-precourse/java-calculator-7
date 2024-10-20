a606b6ea7eef92f3e142b9080b624003e8f8b92b
신동현 <donghyeun02@gmail.com>
2024-10-20 17:55:50 +0900

    feat : 구분자 형식이 잘못되었을 경우 예외 처리

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -4,14 +4,18 @@ import calculator.utils.SplitString;
 
 public class StringCalculator {
     public int calculateSum(String input) {
-        String[] numbers = SplitString.splitInput(input);
-        int sum = 0;
+        try {
+            String[] numbers = SplitString.splitInput(input);
+            int sum = 0;
 
-        for (String number : numbers) {
-            int parsedNumber = ParseNumber.parseNumber(number);
-            sum += parsedNumber;
+            for (String number : numbers) {
+                int parsedNumber = ParseNumber.parseNumber(number);
+                sum += parsedNumber;
+            }
+
+            return sum;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
         }
-
-        return sum;
     }
 }
--- src/main/java/calculator/utils/SplitString.java
+++ src/main/java/calculator/utils/SplitString.java
@@ -8,12 +8,21 @@ public class SplitString {
 
         if (input.startsWith("//")) {
             int delimiterIndex = input.indexOf("\\n");
+
+            if (delimiterIndex == -1 || delimiterIndex == 2) {
+                throw new IllegalArgumentException("커스텀 구분자 형식이 잘못되었습니다.");
+            }
+
             String customDelimiter = input.substring(2, delimiterIndex);
             delimiter = customDelimiter;
 
             input = input.substring(delimiterIndex + 2);
         }
 
-        return input.split(delimiter);
+        try {
+            return input.split(delimiter);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 }
