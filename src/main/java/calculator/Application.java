bab24bd5aea41892131c49b2b41a62e5155022ed
KOO612 <ksm99007@gmail.com>
2024-10-20 17:04:05 +0900

    feat : 구분자를 쉼표(,)와 콜론(:) 으로 지정 후 덧셈 후 출력한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,9 +4,35 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
         String data = Console.readLine();
+        int result = calculate(data);
+        System.out.println("결과 : " + result);
+    }
+
+
+    public static int calculate(String data) {
+        if (data.isEmpty()) {
+            return 0;
+        }
+
+        String[] numbers;
+
+        if (data.contains(",")){
+            numbers = data.split(",");
+        } else if (data.contains(":")) {
+            numbers = data.split(":");
+        } else {
+            throw new IllegalArgumentException("구분자 존재하지 않습니다.");
+        }
+
+        int sum = 0;
+
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
+
     }
 }
