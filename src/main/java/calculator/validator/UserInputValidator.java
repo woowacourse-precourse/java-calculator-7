7d87dd11a77d00f45244e12eb2dae6f0901f5d8e
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2024-10-20 21:07:17 +0900

    feat : 정수가 양수가 아닌 경우 예외발생

--- src/main/java/calculator/validator/UserInputValidator.java
+++ src/main/java/calculator/validator/UserInputValidator.java
@@ -2,6 +2,7 @@ package calculator.validator;
 
 public class UserInputValidator {
     private static final String NON_NUMBER_FORMAT_EXCEPTION_MESSAGE = "정수를 입력해주세요.";
+    private static final String NEGATIVE_NUMBER_MESSAGE = "양수만 입력해주세요";
 
     private UserInputValidator() {
         throw new IllegalStateException("Validate Class");
@@ -9,6 +10,7 @@ public class UserInputValidator {
 
     public static void validateUserInput(String[] splitString) {
         validateIntegerInput(splitString);
+        validateNegativeInteger(splitString);
     }
 
     private static void validateIntegerInput(String[] splitString) {
@@ -17,6 +19,15 @@ public class UserInputValidator {
                 Integer.parseInt(str);
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException(NON_NUMBER_FORMAT_EXCEPTION_MESSAGE);
+            }
+        }
+    }
+
+    private static void validateNegativeInteger(String[] splitString) {
+        for (String str : splitString) {
+            int i = Integer.parseInt(str);
+            if (i < 0) {
+                throw new IllegalArgumentException(NEGATIVE_NUMBER_MESSAGE);
             }
         }
     }
