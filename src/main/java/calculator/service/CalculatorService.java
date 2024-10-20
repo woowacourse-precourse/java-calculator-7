89c0a57204ebc4d34a8b3a661478bf8e17e7590e
young-in <hardworkingini@gmail.com>
2024-10-20 20:58:52 +0900

    feat : 빈 문자열의 경우, 0을 반환하도록 구현

---
+++ src/main/java/calculator/service/CalculatorService.java
@@ -0,0 +1,10 @@
+package calculator.service;
+
+public class CalculatorService {
+    public int add(String input){
+        if (input == null || input.isEmpty()){
+            return 0;
+        }
+        return 0;
+    }
+}
