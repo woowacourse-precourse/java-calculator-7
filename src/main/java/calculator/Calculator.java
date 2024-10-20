6c7bcac2dc907463fe93d595ca720d2bb84f6e98
Cmk1031 <maple1031@naver.com>
2024-10-21 01:12:02 +0900

    feat: 커스텀 문자열 확인

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,10 +8,30 @@ public class Calculator {
     public void run() {
         //1. 문자열 입력받기
         String str = getString();
+
+        //구분자 문자열
+        String separator = "";
+
+        if (Custom(str)) { //2. 커스텀 구분자
+
+        } else { //커스텀 구분자가 아닌 문자열들
+
+        }
+
     }
 
     private static String getString() {
         System.out.println(inputMessage);
         return Console.readLine();
     }
+
+    //2. 커스텀 문자열 확인
+    private boolean Custom(String str) {
+        //문자열 길이가 1보다 작은 경우 false를 반환하는 조건이 있는 이유는 substring(0,2)에서 오류가 발생하기 때문에 미리 처리
+        if (str.length() <= 1) {
+            return false;
+        }
+        return str.startsWith("//") && str.startsWith("\\n", 3);
+    }
+
 }
