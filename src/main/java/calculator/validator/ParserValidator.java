56551fb5cf6901e6a0cd743f5485de25189cbdcc
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-21 02:36:28 +0900

    feat(ParserValidator): 구분자 형식 검증 기능 구현
    - 커스텀 구분자 형식이 중복 입력되었는지 검증합니다.
    - 커스텀 구분자 형식을 통해 split한 문자열의 개수를 기준으로 검증을 진행합니다.

--- src/main/java/calculator/validator/ParserValidator.java
+++ src/main/java/calculator/validator/ParserValidator.java
@@ -1,7 +1,14 @@
 package calculator.validator;
 
 public class ParserValidator {
-    // TODO: 커스텀 구분자 형식이 중복되었는지 확인한다.
+    public boolean isCustomSeparator(int inputSeparatorArraySize) {
+        if (inputSeparatorArraySize < 2) {
+            return false;
+        } else if (inputSeparatorArraySize > 2) {
+            throw new IllegalArgumentException("구분자 형식이 중복 입력되었습니다.");
+        }
+        return true;
+    }
 
     // TODO: 오직 구분자만 입력되었는지 확인한다.
 
