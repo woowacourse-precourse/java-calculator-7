838fcd69b6c239b653b4d404b0128d8d0f389d08
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2024-10-20 23:05:15 +0900

    refactor : 커스텀 구분자를 받기 위한 정규표현식 상수화

--- src/main/java/calculator/util/StringSplitter.java
+++ src/main/java/calculator/util/StringSplitter.java
@@ -7,13 +7,16 @@ import java.util.regex.Pattern;
 
 public class StringSplitter {
     private static final String DEFAULT_SPLITTER = "[,:]";
+    private static final String CUSTOM_SPLITTER_SETTER_PREFIX = "//";
+    private static final String CUSTOM_SPLITTER_SETTER_SUFFIX = "\n";
+    private static final String CUSTOM_SPLITTER_PATTERN = CUSTOM_SPLITTER_SETTER_PREFIX + "(.*?)" + CUSTOM_SPLITTER_SETTER_SUFFIX + "(.*)";
 
     private StringSplitter() {
         throw new IllegalStateException(UTILITY_CLASS_MESSAGE);
     }
 
     public static String[] splitUserInput(String userInput) {
-        Pattern pattern = Pattern.compile("//(.*?)\\n(.*)");
+        Pattern pattern = Pattern.compile(CUSTOM_SPLITTER_PATTERN);
         Matcher matcher = pattern.matcher(userInput);
         if (matcher.matches()) {
             String customDelimiter = matcher.group(1);
