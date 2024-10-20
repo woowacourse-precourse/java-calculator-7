3d2d7adff2aaa8a9d7561106840b3ce38a1ae6a6
LeeHaeJun <haejun0410@gmail.com>
2024-10-21 01:08:12 +0900

    feat: Splitter 구현
    - split : 구분자를 기준으로 숫자를 분리하여 리스트로 반환

---
+++ src/main/java/calculator/split/Splitter.java
@@ -0,0 +1,36 @@
+package calculator.split;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Splitter implements SplitterInterface {
+    private Seperator seperator;
+
+    public Splitter() {
+        this.seperator = new Seperator();
+    }
+
+    @Override
+    public List<Integer> split(String text){
+        String pattern = String.join("|", seperator.getSeperators()
+                .stream()
+                .map(Pattern::quote)
+                .toArray(String[]::new));
+
+        String[] numberString = text.split(pattern);
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String number : numberString) {
+            number = number.trim();
+            if (!number.isEmpty()) {
+                try{
+                    numbers.add(Integer.parseInt(number));
+                } catch (IllegalArgumentException e){
+
+                }
+            }
+        }
+        return numbers;
+    }
+}
