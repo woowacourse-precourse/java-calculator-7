b8ad130f1cef4b54ffb36c9d34acf79aff48fe29
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-20 18:35:57 +0900

    feat(custom pivot calculator) : calculator with custom pivot

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,19 +9,28 @@ public class Application {
         String inputStr = Console.readLine();
 
         int result = 0;
-        // 문자열이 아무것도 입력이 안되었다면 0으로 설정
 
-        // 문자열 입력시 , : 로 구분하기
-        String[] split = inputStr.split(":|,");
-        for (String s : split) {
-            int s1 = Integer.parseInt(s);
-            result +=s1;
+        if (inputStr.charAt(0)=='/' && inputStr.charAt(1)=='/'){
+            String pivot = inputStr.substring(2, inputStr.indexOf('\\'));
+            String target = inputStr.substring((inputStr.indexOf("n")+1));
+
+            String[] split = target.split(pivot);
+
+            for (String s : split) {
+                int num = Integer.parseInt(s);
+                result += num;
+            }
         }
+
+        else {
+            String[] split = inputStr.split(":|,");
+            for (String s : split) {
+                int s1 = Integer.parseInt(s);
+                result +=s1;
+            }
+        }
+
         System.out.println("결과 : "+ result);
-
-
-
-
 
     }
 
