0ebd322df281d1c2cf412e66da36cd0182dc1fbd
pil-go <pilbongx@naver.com>
2024-10-21 05:35:52 +0900

    feat(StringSplitter): 문자열 분리 커스텀구분자 적용

--- src/main/java/calculator/StringSplitter.java
+++ src/main/java/calculator/StringSplitter.java
@@ -1,9 +1,19 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class StringSplitter {
     private static final String DEFAULT_DELIMITERS = ",:";
+    private final DelimiterValidator delimiterValidator;
+
+    StringSplitter(DelimiterValidator delimiterValidator) {
+        this.delimiterValidator = delimiterValidator;
+    }
 
     public String[] split(String input) {
-        return input.split("[" + DEFAULT_DELIMITERS + "]",-1);
+        String customDelimiter = delimiterValidator.getCustomDelimiter(input);
+        customDelimiter = Pattern.quote(customDelimiter);
+
+        return input.split("[" + DEFAULT_DELIMITERS + customDelimiter + "]", -1);
     }
 }
