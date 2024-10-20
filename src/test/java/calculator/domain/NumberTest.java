234f405281306377e002c5e1463114c79418fbac
leejh7 <777joonho@kookmin.ac.kr>
2024-10-20 20:46:13 +0900

    test: Number 클래스의 메서드 테스트 코드 작성

---
+++ src/test/java/calculator/domain/NumberTest.java
@@ -0,0 +1,79 @@
+package calculator.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class NumberTest {
+
+    @DisplayName("정수가 아닌 문자열 예외 테스트")
+    @Test
+    void givenNonNumber_whenMakeNumberClass_thenThrowException() throws Exception
+    {
+        // given
+        String input = "A";
+
+        // when & then
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Number(input);
+        });
+        assertEquals(String.format("%s 값이 유효한 정수가 아닙니다.", input), exception.getMessage());
+    }
+
+    @DisplayName("null 예외 테스트")
+    @Test
+    void givenNull_whenMakeNumberClass_thenThrowException() throws Exception
+    {
+        // given
+        String input = null;
+
+        // when & then
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Number(input);
+        });
+        assertEquals(String.format("%s 값이 유효한 정수가 아닙니다.", input), exception.getMessage());
+    }
+
+    @DisplayName("정수가 아닌 공백 문자열 예외 테스트")
+    @Test
+    void givenBlank_whenMakeNumberClass_thenThrowException() throws Exception
+    {
+        // given
+        String input = "";
+
+        // when & then
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Number(input);
+        });
+        assertEquals(String.format("%s 값이 유효한 정수가 아닙니다.", input), exception.getMessage());
+    }
+
+    @DisplayName("음의 정수 문자열 예외 테스트")
+    @Test
+    void givenNegativeNumber_whenMakeNumberClass_thenThrowException() throws Exception
+    {
+        // given
+        String input = "-1";
+
+        // when & then
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Number(input);
+        });
+        assertEquals(String.format("%s 값이 0이상의 정수가 아닙니다.", input), exception.getMessage());
+    }
+
+    @DisplayName("올바른 정수 문자열 테스트")
+    @Test
+    void givenCorrectNumber_whenMakeNumberClass_thenDone() throws Exception
+    {
+        // given
+        String input = "1";
+        int result = 1;
+
+        // when
+        Number number = new Number(input);
+
+        // then
+        assertEquals(number.getValue(), result);
+    }
+}
