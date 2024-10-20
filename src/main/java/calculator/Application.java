5c95117bbf791b458f3492382735266b96e46888
heychi <heychi0728@gmail.com>
2024-10-20 15:49:41 +0900

    feat: calculator project 1st commit

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,50 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String sep = ",|:";
+        String numbers = input;
+
+        if (input.startsWith("//")) {
+            int sepIndex = input.indexOf("\n");
+            if (sepIndex == -1) {
+                throw new IllegalArgumentException();
+            }
+            sep = input.substring(2, sepIndex);
+            numbers = input.substring(sepIndex + 1);
+        }
+
+        String[] units = numbers.split(sep);
+        int sum = 0;
+
+        try {
+            for (String unit : units) {
+                if (!unit.isEmpty()) {
+                    sum += Integer.parseInt(unit);
+                }
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("계산할 문자열을 입력하세요:");
+        String input = Console.readLine();
+
+        try {
+            int result = add(input);
+            System.out.println("결과: " + result);
+        } catch (IllegalArgumentException e) {
+            return;
+        }
     }
 }
