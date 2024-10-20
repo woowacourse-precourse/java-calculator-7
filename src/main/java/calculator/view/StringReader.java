9d2cd3cbfe5d779eacc7b5f5d57ae86d91d8eecd
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-10-20 11:55:28 +0900

    refactor(StringReader): read 호출 시, 입력 메시지 출력하도록 수정
    문제의 요구사항에 맞게 입력 값을 요구할 때, 메시지를 출력합니다.

--- src/main/java/calculator/view/StringReader.java
+++ src/main/java/calculator/view/StringReader.java
@@ -4,7 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class StringReader {
 
+    private final String MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
     public String read() {
+        System.out.println(MESSAGE);
         return Console.readLine();
     }
 }
