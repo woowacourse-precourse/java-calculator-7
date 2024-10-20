e2d4d481447e91696c728909c8ffeaf25e3352c5
alswnsp411 <alswnsp411@naver.com>
2024-10-20 17:39:19 +0900

    fix: 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨다. - CalculatorController에서 사용자 입력값에 대해 유효성을 검증한다.

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -24,11 +24,23 @@ public class CalculatorController {
      */
     private void createInputString() {
         String input = inputView.getInputString();  // 덧셈할 문자열을 입력해주세요.
+
+        validateInputString(input);
+
         InputString inputString = new InputString(input);
 
         long resultSum = inputString.calculate();  // 문자열 계산
 
         outputView.getOutputResult(resultSum);
+    }
+
+    /**
+     * 입력받은 문자열에 대해 유효성 검증
+     */
+    private void validateInputString(String input) {
+        if (input.startsWith("//") && !input.substring(3, 5).equals("\\n")) {
+            throw new IllegalArgumentException("올바른 커스텀 구분자 입력 형식이 아닙니다.");
+        }
     }
 
 }
