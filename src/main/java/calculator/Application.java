6f25ec15d2b34006ea9945cc0ba60ad8f32bee15
jongseo <jongseo0703@naver.com>
2024-10-20 15:37:55 +0900

    체크리스트 업데이트

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,14 +10,10 @@ public class Application {
 
         StringCalculator calculator = new StringCalculator();
 
-        try {
-            int result = calculator.add(input); // 사용자가 입력한 문자열로 덧셈 계산
+        int result = calculator.add(input); // 사용자가 입력한 문자열로 덧셈 계산
 
-            System.out.println("결과 : " + result); // 결과 출력
-        }
-        catch (IllegalArgumentException e) { // 잘못된 입력 예외 처리
-            System.out.println("예외가 발생 하였습니다. 예외 내용 : " + e.getMessage());
-        }
+        System.out.println("결과 : " + result); // 결과 출력
+
 
     }
 }
