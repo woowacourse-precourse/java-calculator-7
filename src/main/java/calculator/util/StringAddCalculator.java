1dbcb572a4dfd1b05eedb210690ccc3d2e86c65d
DongYeop <s01054830455@gmail.com>
2024-10-20 15:46:21 +0900

    refactor: private 메서드 배치 순서를 논리적인 순서를 따르도록 변경
    - public 메서드에서 호출한 private 메서드의 실행 순서대로 메서드를 배치하였음

--- src/main/java/calculator/util/StringAddCalculator.java
+++ src/main/java/calculator/util/StringAddCalculator.java
@@ -19,27 +19,18 @@ public class StringAddCalculator {
         return isNull(input) || isEmpty(input);
     }
 
-    private static void validateContainBlank(String input) {
-        if (isContainBlank(input)) {
-            throw new IllegalArgumentException("공백을 포함할 수 없습니다.");
-        }
-    }
-
-    private static int calculateSum(String[] splitInput) {
-        int sum = ZERO_VALUE;
-        for (String splitNumber : splitInput) {
-            PositiveNumber positiveNumber = PositiveNumber.create(toInt(splitNumber));
-            sum = positiveNumber.addNumber(sum);
-        }
-        return sum;
-    }
-
     private static boolean isNull(String input) {
         return input == null;
     }
 
     private static boolean isEmpty(String input) {
         return input.isEmpty();
+    }
+
+    private static void validateContainBlank(String input) {
+        if (isContainBlank(input)) {
+            throw new IllegalArgumentException("공백을 포함할 수 없습니다.");
+        }
     }
 
     private static boolean isContainBlank(String input) {
@@ -60,5 +51,14 @@ public class StringAddCalculator {
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException("구분자가 잘못 되었습니다. 커스텀 구분자, 기본 구분자를 확인 후, 다시 입력하세요");
         }
+    }
+
+    private static int calculateSum(String[] splitInput) {
+        int sum = ZERO_VALUE;
+        for (String splitNumber : splitInput) {
+            PositiveNumber positiveNumber = PositiveNumber.create(toInt(splitNumber));
+            sum = positiveNumber.addNumber(sum);
+        }
+        return sum;
     }
 }
