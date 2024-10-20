e2479316f6c260304fa9b24169c91289db3b65e7
이윤빈 <kanado20000@naver.com>
2024-10-20 19:40:14 +0900

    :art: refactor: 두 print문을 하나로 합치기

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -6,8 +6,7 @@ public class InputView {
     private static final String USER_NUMBERS = "덧셈할 문자열을 입력해 주세요.";
 
     public static String readUserNumbers() {
-        System.out.print(USER_NUMBERS);
-        System.out.print("\n");
+        System.out.print(USER_NUMBERS + "\n");
         String input = Console.readLine();
         return input;
     }
