7f62ec018d58bb22f51f4a4f2a79035e6ed87ee0
jaewon07 <jowonjae07@gmail.com>
2024-10-21 03:49:01 +0900

    feat(StringSplitter): StringSplitter 클래스 생성
    커스텀 구분자가 있을 때와 없을 때 문자열을 배열에 넣는 두 개의 메소드 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,18 +6,30 @@ public class Application {
 
     public static void main(String[] args) {
 
-        Application app = new Application();
 
+        StringSplitter stringSplitter = new StringSplitter();
         CusTomDelimiterParser customdelimiterParser = new CusTomDelimiterParser();
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
         String inputString = Console.readLine();  // 사용자로부터 입력
 
-            // 사용자 정의 구분자 추출
-            String customDelimiter = customdelimiterParser.delimiter(inputString);
+        // 사용자 정의 구분자 추출
+        String customDelimiter = customdelimiterParser.delimiter(inputString);
 
+        // 사용자 정의 구분자가 있을 시, 사용자 정의 구분자를 포함한 문자열 분리
+        if (!customDelimiter.isEmpty()) {
+            String numberList[] = stringSplitter.splitWithCustomDelimiter(customDelimiter, inputString);
 
+        }
 
+        // 사용자 정의 구분자가 없을 시, 사용자 정의 구분자를 포함하지 않은 문자열 분리
+        if (customDelimiter.isEmpty()) {
+
+            String numberList[] = stringSplitter.splitWithDefaultDelimiter(inputString);
+
+        }
     }
+
 }
+
