b40db9176c69f9fcd6c392711109a39f7cbe3b03
dev-joeysos <joeysos@naver.com>
2024-10-21 02:04:10 +0900

    feat: add base caculation feature

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input;
+
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        input = Console.readLine();
+        String[] numbers = input.split("[,:]");
+
+        int sum = 0;
+        for(String number : numbers) {
+            if (!number.trim().isEmpty()) {
+                try {
+                    sum += Integer.parseInt(number.trim());
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("잘못된 입력입니다.");
+                }
+            }
+        }
+        System.out.println("결과: " + sum);
     }
 }
