6e8868466432aac6e38e1b96a0ba83ac4112bcec
jinho7 <69239293+jinho7@users.noreply.github.com>
2024-10-21 01:56:04 +0900

    feat(Application): 결과 출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,9 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        StringCalculator calculator = new StringCalculator();
+
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        // TODO : 이후 처리 로직
+        try {
+            int result = calculator.calculate(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -44,5 +44,4 @@ public class StringCalculator {
 
         return sum;
     }
-
 }
