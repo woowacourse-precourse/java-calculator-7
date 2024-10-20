2ef909069db927fe9e590c38c377c16eed59cac6
Park, minsu <cmwmss59@gmail.com>
2024-10-20 13:44:58 +0900

    feat: 사용자로 부터 입력을 받는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+
+    }
+
+    //1. 입력 받기
+    private static String readInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        //커스텀 구분자 사용 시 다음줄 입력받기.
+        if (input.startsWith("//") && input.contains("\n")) {
+            String nextLine=Console.readLine();
+            input +="\n"+nextLine;
+        }
+        return input;
     }
 }
