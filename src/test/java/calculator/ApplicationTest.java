04f480a96e5e1da3a1dff40e216e48447f87a856
shlim <jfknfj920@naver.com>
2024-10-20 15:50:41 +0900

    feat: 유닛 테스트 코드 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,7 +9,47 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
-    void 커스텀_구분자_사용() {
+    void 공백_입력() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용_1() {
+        assertSimpleTest(() -> {
+            run("1,2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용_2() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 기본_구분자_혼합_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_1() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_2() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
@@ -17,10 +57,50 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 기본_구분자와_커스텀_구분자_혼합_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2,4");
+            assertThat(output()).contains("결과 : 7");
+        });
+    }
+
+    @Test
+    void 실수_입력() {
+        assertSimpleTest(() -> {
+            run("1,2.2");
+            assertThat(output()).contains("결과 : 3.2");
+        });
+    }
+
+    @Test
+    void 예외_테스트_음수() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_음수_및_기본_구분자와_커스텀_구분자_혼합_사용() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//;\\n1;2,-300"))
+                    .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_구분자_사용이_없음() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1 2 3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_수가_하나만_들어옴() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
