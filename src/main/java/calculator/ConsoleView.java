f5cb360fe6053eff13cd4242ba6a19eb5b627cca
gwanrok <145204556+moonditation@users.noreply.github.com>
2024-10-20 21:56:46 +0900

    style(ConsoleView): improve console output format

--- src/main/java/calculator/ConsoleView.java
+++ src/main/java/calculator/ConsoleView.java
@@ -4,11 +4,11 @@ public class ConsoleView {
 
     public String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return camp.nextstep.edu.missionutils.Console.readLine(); // 입력 받기
+        return camp.nextstep.edu.missionutils.Console.readLine();
     }
 
     public void printResult(int result) {
-        System.out.println("결과 : " + result);
+        System.out.println("결과: " + result);
     }
 
     public void printError(String errorMessage) {
