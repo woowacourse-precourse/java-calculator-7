a5e122ba56ec05d47d04f3374f1533f86bd2b840
HARDY0070 <hwv1219@gmail.com>
2024-10-21 00:25:13 +0900

    feat(Application): 기능 추가 및 README.md 업데이트
    추가된 기능들:
    - 사용자 입력을 받는 기능 추가
    - 문자열에서 사용자 커스텀 구분자를 추출하는 기능 추가
    
    README.md 기능 구현 목록 추가 및 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int result = 0;
+
+        System.out.print("덧셈할 문자열을 입력해 주세요.");
+        String userInputStr = Console.readLine();
+
+        String delimiter = userInputStr.substring(2, userInputStr.indexOf("\\n"));
+        System.out.println("delimiter = " + delimiter);
     }
 }
