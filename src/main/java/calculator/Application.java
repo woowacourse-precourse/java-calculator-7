fd1dc7e0fc6a61600bb72fb0e2ee879d9c3ea972
moon <0917jong@gmail.com>
2024-10-20 19:15:02 +0900

    feat: 커스텀 구분자 추출 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -14,6 +14,9 @@ public class Application {
     
     // 주어진 문자열에서 커스텀 구분자를 추출
     public static String parseDelimiter(String sentence) {
+
+        // 커스텀 구분자 업데이트를 위한 StringBuilder
+        StringBuilder stringBuilder = new StringBuilder(delimiters);
         
         // 커스텀 구분자가 계속 존재한다면 탐색
         while (sentence.contains("//") && sentence.contains("\\n")) {
@@ -23,11 +26,14 @@ public class Application {
             
             // 커스텀 구분자 목록 업데이트
             String customDelimiter = sentence.substring(startIdx + 2, endIdx);
-            delimiters = delimiters + "|" + customDelimiter;
+            stringBuilder.append("|").append(customDelimiter);
             
             // 커스텀 구분자를 제외한 나머지 문자열
             sentence = sentence.substring(0, startIdx) + sentence.substring(endIdx + 2);
         }
+
+        // stringBuilder를 사용해 최종 업데이트 된 구분자 반영
+        delimiters = stringBuilder.toString();
         
         return sentence;
     }
@@ -35,9 +41,6 @@ public class Application {
     public static int calculator(String sentence) {
         // 주어진 문자열에서 커스텀 구분자 추가하고 제외
         sentence = parseDelimiter(sentence);
-
-        System.out.println(delimiters);
-        System.out.println(sentence);
 
         return 0;
     }
