b99f12ef9b0d8fd9ca0ade9d9d0d18b09aabf059
PSW99 <steamdayz@naver.com>
2024-10-21 01:36:49 +0900

    refactor(view): 출력형식 수정

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,7 +2,7 @@ package calculator.view;
 
 public class OutputView {
     public void printResult(int result) {
-        System.out.println("결과 :" + result);
+        System.out.println("결과 : " + result);
     }
 
 }
