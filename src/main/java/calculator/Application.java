0f64f547f8777cd08e3edbd037c32a690a813628
jjun522 <tjwowns522@>
2024-10-20 15:43:07 +0900

    feat: 메인 클래스 추가
    프로그램 실행의 시작점인 Application 클래스 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,27 @@
 package calculator;
 
+import calculator.service.StringSumCalculator;
+import calculator.util.InputHandler;
+import calculator.util.ErrorHandler;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+
+        // 사용자에게 문자열 입력 요청
+        String input = InputHandler.getInput(scanner);
+
+        StringSumCalculator calculator = new StringSumCalculator();
+        try {
+            // 입력된 문자열의 합 계산
+            int result = calculator.calculateSum(input);
+            System.out.println("결과: " + result);
+        } catch (IllegalArgumentException e) {
+            // 잘못된 입력이 있을 경우 예외 처리
+            ErrorHandler.handleError(e);
+        } finally {
+            scanner.close();
+        }
     }
 }
