3d743324232e02a467945e4d7f499dddc48f9fa8
조윤성 <yunsung1206@gmail.com>
2024-10-20 17:03:54 +0900

    feat: 빈 문자열에 대한 조건문 추가

--- src/main/java/calculator/NumberParser.java
+++ src/main/java/calculator/NumberParser.java
@@ -1,10 +1,14 @@
 package calculator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class NumberParser {
     public List<Integer> parse(String input) {
+        if (input.isEmpty()) {
+            return Collections.emptyList();
+        }
         String delimiters = "[,:]";
         String[] numbers = input.split(delimiters);
         return convertToIntegerList(numbers);
