6f507a54919c870ef1e858c31bd95db75597b32a
Kim5eongkwang <ksg6736@jbnu.ac.kr>
2024-10-21 01:57:21 +0900

    refactor: Calculator 클래스가 sum 값을 유지하지 않도록 변경

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -3,19 +3,11 @@ package calculator.domain;
 import calculator.domain.number.Numbers;
 
 public class Calculator {
-    private int sum;
 
-    public Calculator(Numbers numbers) {
-        sumNumbers(numbers);
-    }
-
-    private void sumNumbers(Numbers numbers) {
-        this.sum = numbers.toInteger().stream()
+    public int sumNumbers(Numbers numbers) {
+        return numbers.toInteger().stream()
                 .mapToInt(Integer::intValue)
                 .sum();
     }
 
-    public int getSum() {
-        return sum;
-    }
 }
--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -1,24 +1,26 @@
 package calculator.domain;
 
-
 import calculator.domain.number.Numbers;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
+
     @Test
     void 추출한_숫자열의_숫자를_더함() {
         //given
         Calculator calculator;
+        calculator = new Calculator();
         Numbers numbers = new Numbers();
         numbers.addNumber("1");
         numbers.addNumber("2");
 
         //when
-        calculator = new Calculator(numbers);
+        int sum = calculator.sumNumbers(numbers);
 
         //then
-        Assertions.assertThat(calculator.getSum()).isEqualTo(3);
+        Assertions.assertThat(sum).isEqualTo(3);
     }
+
 
 }
