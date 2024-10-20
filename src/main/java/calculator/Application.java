3dead3033a6624d485cc20d0f9079ea86722768b
zi-eunn <jieunkim1117@gmail.com>
2024-10-20 20:18:23 +0900

    Update Application.java

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -41,6 +41,7 @@ public class Application {
 
 
     public static void main(String[] args) {
+        // TODO: 프로그램 구현
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
