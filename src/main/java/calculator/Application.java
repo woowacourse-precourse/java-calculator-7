141d1f1c85ab0953f80549008b60228a06f8f10d
swhyeon98 <smreo75@gmail.com>
2024-10-20 15:00:05 +0900

    feat: 문자열에서 숫자를 추출하여 합산하는 계산기 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
+    private static final String delimiter = "[,:]";
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String numbers = Console.readLine();
+        if (numbers.isEmpty()) {
+            System.out.println("결과: 0");
+            return;
+        }
+        List<String> split = List.of(numbers.split(delimiter));
+        System.out.println("결과: " + split.stream().mapToInt(Integer::parseInt).sum());
     }
 }
