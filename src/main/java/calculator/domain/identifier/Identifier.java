58537a5705323f3a528795ac448e9471cb53ee51
Ahnsukjoo <bos98@naver.com>
2024-10-20 16:59:49 +0900

    feat: 커스텀 구분자에 정규식 메타문자 입력시 예외 기능 구현
    -커스텀 구분자에 정규식 메타문자 입력시 IllegalArgumentException 을 반환

--- src/main/java/calculator/domain/identifier/Identifier.java
+++ src/main/java/calculator/domain/identifier/Identifier.java
@@ -4,14 +4,26 @@ public class Identifier {
 
     private final String customKey1 = "//";
     private final String customKey2 = "\\n";
+    private String costomSting;
 
     public String findDelimiter(String string) {
         if (string.startsWith(customKey1)) {
             int customKeyEndIndex = string.indexOf(customKey2);
             if (customKeyEndIndex == 3) {
-                return string.substring(customKey1.length(), customKeyEndIndex);
+                costomSting = string.substring(customKey1.length(), customKeyEndIndex);
+                validateDelimiter(costomSting);
+                return costomSting;
             }
         }
         return null;
+    }
+
+    public void validateDelimiter(String delimiter) {
+        String regexMetaChars = ".*+?^${}()|[]\\";
+        for (char c : delimiter.toCharArray()) {
+            if (regexMetaChars.indexOf(c) != -1) {
+                throw new IllegalArgumentException("정규식 메타 문자는 커스텀 구분자로 사용할 수 없습니다: " + delimiter);
+            }
+        }
     }
 }
