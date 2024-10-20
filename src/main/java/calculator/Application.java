3d0dac4ae71f4802fa79ec6f1f09020e485be134
jintakkim <mine407603@gmail.com>
2024-10-20 13:34:16 +0900

    feat(runCalculator): 계산기가 Main메서드에서 작동되도록 구현
    RunCalculator
    -각 매니저를 초기화
    -outputManager 통해 출력 메서드 호출
    -inputManager 를 통해 입력 메서드 호출
    -calculationManager를 통해 계산 메서드 호출
    
    Application
    -main메서드에서 RunCalculator의 run메서드를 호출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,7 @@
 package calculator;
 
-import calculator.repository.number.NumberRepository;
-import calculator.repository.number.NumberRepositoryImpl;
-import calculator.repository.separator.SeparatorRepository;
-import calculator.repository.separator.SeparatorRepositoryImpl;
-
 public class Application {
     public static void main(String[] args) {
-        SeparatorRepository separatorRepository = SeparatorRepositoryImpl.getInstance();
-        NumberRepository numberRepository = NumberRepositoryImpl.getInstance(separatorRepository);
+        RunCalculator.run();
     }
 }
