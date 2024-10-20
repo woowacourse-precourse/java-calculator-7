4346ee342f74129ce4f9b9e005766515db3c3011
seophohoho <seop0937@gmail.com>
2024-10-21 00:53:13 +0900

    refactor(Validator): 기존 예외 처리를 정규식 처리 방식으로 개선

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -1,26 +1,15 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
     public void validate(String input) {
-        if (input.contains("-")) {
-            throw new IllegalArgumentException("ㅁㅊ음? 음수를 넣는다고?");
-        }
+        Pattern pattern = Pattern.compile(Constants.REGEX_INPUT_PATTERN);
+        Matcher matcher = pattern.matcher(input);
 
-        if (input.startsWith(Constants.CUSTOM_SEPARATOR_PREFIX) && !input.contains(Constants.CUSTOM_SEPARATOR_SUFFIX)) {
-            throw new IllegalArgumentException("커스텀 구분자 지정 컨벤션이 올바르지 않아요");
-        }
-
-        if (!input.startsWith(Constants.CUSTOM_SEPARATOR_PREFIX) && input.contains(Constants.CUSTOM_SEPARATOR_SUFFIX)) {
-            throw new IllegalArgumentException("커스텀 구분자 지정 컨벤션이 올바르지 않아요");
-        }
-
-        if (input.startsWith(Constants.CUSTOM_SEPARATOR_PREFIX) && input.contains(Constants.CUSTOM_SEPARATOR_SUFFIX)) {
-            int prefixIdx = input.indexOf(Constants.CUSTOM_SEPARATOR_PREFIX);
-            int suffixIdx = input.lastIndexOf(Constants.CUSTOM_SEPARATOR_SUFFIX);
-            
-            if (((suffixIdx - 1) - (prefixIdx + 1)) > 1) {
-                throw new IllegalArgumentException("커스텀 구분자는 길이가 '1' 이여야 한다.");
-            }
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("잘못된 입력 :)");
         }
     }
 }
