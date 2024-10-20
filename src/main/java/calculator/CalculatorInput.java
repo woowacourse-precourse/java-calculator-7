8b34373353e3856fce58fcb5f2de77b200726ed6
minSsan <msp1125@hanyang.ac.kr>
2024-10-21 02:14:39 +0900

    refactor: 입력 문구 출력 책임을 CalculatorOuput으로 변경

--- src/main/java/calculator/CalculatorInput.java
+++ src/main/java/calculator/CalculatorInput.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class CalculatorInput {
     public String read() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 }
--- src/main/java/calculator/CalculatorOutput.java
+++ src/main/java/calculator/CalculatorOutput.java
@@ -1,7 +1,10 @@
 package calculator;
 
 public class CalculatorOutput {
-    public void print(int result) {
+    public void printInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+    public void printResult(int result) {
         System.out.println("결과 : " + result);
     }
 }
