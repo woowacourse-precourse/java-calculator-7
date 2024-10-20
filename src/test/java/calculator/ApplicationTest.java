31d3fee60a96fe336265be0d42b6030782d45f46
Gothax <scott990328@gmail.com>
2024-10-20 15:30:50 +0900

    test: 테스트 코드 추가
    구분자를 띄어쓰기로 제공하는 경우를 허용, 여러글자로 제공하는 경우를 허용, 여러개의 커스텀 구분자 불허용

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,96 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 공백_입력() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+
+    @Test
+    void 커스텀_구분자() {
+        assertSimpleTest(()->{
+            run("//^\\n1^3^3");
+            assertThat(output()).contains("결과 : 7");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자2() {
+        assertSimpleTest(()->{
+            run("//&\\n1&2&3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_띄어쓰기() {
+        assertSimpleTest(()->{
+            run("// \\n1 2 3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_2글자() {
+        assertSimpleTest(() -> {
+            run("//;;\\n1;;2;;3");
+            assertThat(output()).contains("결과 : 6");
+                         }
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_5글자(){
+        assertSimpleTest(()->{
+            run("//;;;;;\\n1;;;;;2;;;;;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 알려주지_않은_커스텀_구분자() {
+        assertSimpleTest(() ->
+             assertThatThrownBy(() -> runException("1;4,2:3"))
+             .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_숫자대신문자() {
+        assertSimpleTest(() ->
+             assertThatThrownBy(() -> runException("a,1,2"))
+             .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자없이(){
+        assertSimpleTest(()->{
+            run("123");
+            assertThat(output()).contains("결과 : 123");
+        });
+    }
+
+    @Test
+    void 예외테스트_커스텀구분자_여러번(){
+        assertSimpleTest(()->{
+            assertThatThrownBy(()->runException("//;\\n//#\\n1;2;3;4"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 예외테스트_커스텀구분자가_기본구분자일때(){
+        assertSimpleTest(()->{
+            assertThatThrownBy(()->runException("//,\\n1,2,3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
--- src/test/java/calculator/service/CalculatorServiceTest.java
+++ src/test/java/calculator/service/CalculatorServiceTest.java
@@ -3,7 +3,10 @@ package calculator.service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class CalculatorServiceTest {
     CalculatorService calculatorService = new CalculatorService();
@@ -27,4 +30,46 @@ class CalculatorServiceTest {
         // then
         assertEquals(6, answer);
     }
+
+    @DisplayName("커스텀 구분자가 있는 경우2")
+    @Test()
+    void executeAddWithCustomSeparator2() {
+        // given
+        // when
+        Integer answer = calculatorService.executeAdd("//^\\n1^2^3");
+        // then
+        assertEquals(6, answer);
+    }
+
+    @DisplayName("커스텀 구분자가 띄어쓰기인 경우")
+    @Test()
+    void executeAddWithCustomSeparatorWithBlank() {
+        // given
+        // when
+        Integer answer = calculatorService.executeAdd("// \\n1 2 3");
+        // then
+        assertEquals(6, answer);
+    }
+
+    @DisplayName("커스텀 구분자가 공백인 경우")
+    @Test()
+    void executeAddWithCustomSeparatorWithBlank2() {
+        // given
+        // when
+        assertThatThrownBy(() -> calculatorService.executeAdd("//\\n123"))
+                .isInstanceOf(IllegalArgumentException.class);
+        // then
+    }
+
+    @DisplayName("커스텀 구분자가 없는 경우")
+    @Test()
+    void executeAddWithCustomSeparatorWithBlank3() {
+        // given
+        // when
+        assertThatThrownBy(() -> calculatorService.executeAdd("//\\n1,2,3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        // then
+    }
+
+
 }
