01283dbd1774d13c1397ed1a9c67dca8a81ec5e9
JunHakLee03 <junhak111701@gmail.com>
2024-10-20 16:23:11 +0900

    feat: 입력 받은 문자열 검사 로직 추가
    - 커스텀 구분자 여부 확인 로직 구현
    - 실입력값의 양 끝이 숫자인지 확인하는 유효성 검사 추가
    - 유효하지 않은 입력에 대해 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,4 +7,31 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = Console.readLine();
+
+        long result = calculate(userInput);
+        System.out.println("결과 : " + result);
     }
+
+    // 계산 로직
+    public static long calculate(String userInput) {
+        String value = userInput;
+        char customDelimiter = ',';
+
+        // 커스텀 구분자 사용 여부 확인
+        if (userInput.startsWith("//") && userInput.indexOf("\\n") == 3) {
+            customDelimiter = userInput.charAt(2);
+            if (Character.isDigit(customDelimiter)) {
+                throw new IllegalArgumentException("숫자는 커스텀 문자로 사용 할 수 없습니다.");
+            }
+            value = userInput.substring(userInput.indexOf("\\n") + 2);
+        }
+
+        // 유효성 검사
+        if (!Character.isDigit(value.charAt(0)) && Character.isDigit(value.charAt(value.length() - 1))) {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
+
+        return result;
+    }
+
+}
