bfe7198ff37dface68e2fc642837e854af737157
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-10-20 13:00:58 +0900

    feat : 문자열에 내 구분자 목록을 제외한 구분자가 들어있으면 예외처리 추가

--- src/main/java/calculator/separator/DefaultSeparator.java
+++ src/main/java/calculator/separator/DefaultSeparator.java
@@ -7,15 +7,38 @@ import java.util.stream.Collectors;
 
 public class DefaultSeparator {
 
-    public BigInteger[] separateStringToSum(String userInput, SeparatorStorage separatorStorage){
+    public BigInteger[] separateStringToSum(String userInput, SeparatorStorage separatorStorage) throws IllegalArgumentException{
+
+        if (containsInvalidSeparator(userInput, separatorStorage)) {
+            throw new IllegalArgumentException("유효한 구분자가 입력에 포함되지 않았습니다");
+        }
 
         String [] parsedStrings = userInput.split(getRegex(separatorStorage));
+
+        if(Arrays.stream(parsedStrings)
+                .anyMatch(String::isEmpty)){
+            throw new IllegalArgumentException("문자열에 숫자가 포함되지 않은 부분이 있습니다");
+        }
 
         BigInteger[] numbers = Arrays.stream(parsedStrings)
                 .map(BigInteger::new)
                 .toArray(BigInteger[]::new);
 
         return numbers;
+    }
+
+    private static boolean containsInvalidSeparator(String input, SeparatorStorage separatorStorage) {
+
+        String allowedSeparators = separatorStorage.getSeparators()
+                .stream()
+                .map(Pattern::quote)
+                .collect(Collectors.joining("|"));
+
+        String regex = "^[\\d" + allowedSeparators + "]+$";
+
+        System.out.println(regex);
+
+        return !Pattern.matches(regex, input);
     }
 
     private static String getRegex(SeparatorStorage separatorStorage) {
