643501d90f493ddf47c90116c4da1cca93343aff
rdyjun <rdyjun00@gmail.com>
2024-10-20 20:57:38 +0900

    feat: 사용자 입력 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String input = bufferedReader.readLine();
     }
 }
