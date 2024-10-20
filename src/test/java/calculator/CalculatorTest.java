e6bba8c09b4c3de126e18a4635aa739bbf2350bb
YuyoungRhee <bbclara@naver.com>
2024-10-20 20:06:20 +0900

    test(Calculator): 빈 리스트 입력 시 0을 반환하는 테스트 추가
    - 빈 리스트가 입력되었을 때, 계산 결과가 0이 되는지 확인하는 테스트 케이스 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,13 +1,11 @@
 package calculator;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
@@ -23,5 +21,19 @@ class CalculatorTest {
 
         // then
         assertThat(result).isEqualTo(6);
+    }
+
+    @DisplayName("빈 리스트가 입력되면 0을 반환한다.")
+    @Test
+    void calculate_emptyList() {
+        // given
+        Calculator calculator = new Calculator();
+        List<Integer> numbers = List.of();
+
+        // when
+        int result = calculator.calculate(numbers);
+
+        // then
+        assertThat(result).isEqualTo(0);
     }
 }
