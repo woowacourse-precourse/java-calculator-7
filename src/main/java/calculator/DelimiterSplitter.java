70eb367f05aaf48e8ad5e27d0d5c762a9249a841
siwol025 <cyk99115695@gmail.com>
2024-10-21 08:04:49 +0900

    feat: 구분자를 통해 문자열을 구분하는 기능 구현

--- src/main/java/calculator/DelimiterSplitter.java
+++ src/main/java/calculator/DelimiterSplitter.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 public class DelimiterSplitter {
     private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\\n(.*)";
+    private static final String DEFAULT_DELIMITER_PATTERN = ",|;";
 
     public boolean isCustomDelimiter(String input) {
         char firstChar = input.charAt(0);
@@ -20,4 +21,14 @@ public class DelimiterSplitter {
         throw new IllegalArgumentException("커스텀 구분자를 찾을 수 없습니다. 올바른 형식은 //구분자\\n입니다.");
     }
 
+    public String[] splitByDefaultDelimiter(String input) {
+        return input.split(DEFAULT_DELIMITER_PATTERN);
+    }
+
+    public String[] split(String input) {
+        if (isCustomDelimiter(input)) {
+            return splitByCustomDelimiter(input);
+        }
+        return splitByDefaultDelimiter(input);
+    }
 }
