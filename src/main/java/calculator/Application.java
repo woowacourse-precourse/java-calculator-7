100e46a80bfa4e2c6b3dd5edde21094088b7a1a6
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-21 04:30:26 +0900

    feat(Application.java): 메인코드 구현
    지금까지 구현한 클래스와 메서드를 바탕으로 동작을 수행하는 메인코드를 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputProcessor inputProcessor = InputProcessor.getInstance();
+        List<Integer> nums = inputProcessor.getInputFromUser();
+
+        Calculator calculator = new Calculator();
+        int result = calculator.calculateResult(nums);
+
+        OutputProcessor outputProcessor = new OutputProcessor();
+        outputProcessor.printResult(result);
     }
 }
