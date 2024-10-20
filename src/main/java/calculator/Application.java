eb34e75a05eef8f7750d8457d045ab7b24e956e5
euni <kongeuni1004@gmail.com>
2024-10-21 04:10:12 +0900

    feat: main 메서드에서 문자열 입력, 슬라이싱 및 합계 계산 로직 추가
    - 문자열을 입력받고 슬라이싱한 후, 합계를 계산하는 로직 구현
    - 각 메서드를 호출하여 결과를 출력하는 테스트 코드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application app = new Application();
+
+        String input = app.getInputString();
+        int[] numList = app.sliceString(input);
+        int sum = app.sumList(numList);
+        System.out.println(sum);
     }
 
     public String getInputString(){
