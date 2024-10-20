3b27b7575cadfb95af43007d937f70f60a616f66
kingjinyong <wlsdydwhswkf@gmail.com>
2024-10-20 10:12:57 +0900

    fix(CalculatorModelTest): 커스텀 구분자 지정 문자 올바른 사용 에러 수정
    해당 테스트 코드 작성할 때, Test 어노테이션과 ParameterizedTest어노테이션을 혼용을 하지 않아도 된다는 것을 알게 된 후 수정하게 되었습니다.

--- src/test/java/calculator/model/CalculatorModelTest.java
+++ src/test/java/calculator/model/CalculatorModelTest.java
@@ -1,13 +1,11 @@
 package calculator.model;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CalculatorModelTest {
 
-    @Test
     @ParameterizedTest
     @ValueSource(strings = {"//;\\n1,2;3", "//\\n1,2,3,4"})
     void 커스텀_구분자_지정_문자_올바른_사용(String input) {
