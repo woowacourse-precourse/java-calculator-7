6e6f6b2f0211c5ddea5a978c52a3850df8e0d9c0
skadjs <skadjs11@gmail.com>
2024-10-20 22:19:42 +0900

    feat: 커스텀 구분자 설정 기능 추가
    커스텀 구분자를 설정하기 위한 addCustomDivider() 메서드 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,11 +1,22 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
     String userInput;
     String noBlankInput;
+    Matcher userInputForCheck;
+    List<String> defaultDividerList = List.of(",", ":");
+    String customDivider;
+    List<String> dividerList;
 
     public void getUserInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
@@ -16,5 +27,23 @@ public class StringCalculator {
     public void removeBlank() {
         this.noBlankInput = this.userInput.replaceAll(" ", "");
         System.out.println("공백 제거된 사용자 입력값: " + this.noBlankInput); //메서드 기능 점검용 코드
+    }
+
+    public void addCustomDivider() {
+        List<String> customDividerList = new ArrayList<>();
+        Set<String> duplicationFilter = new LinkedHashSet<>(defaultDividerList);
+
+        this.userInputForCheck = Pattern.compile("//(.+)\\\\n(.*)").matcher(this.noBlankInput);
+
+        if (userInputForCheck.find()) {
+            customDivider = userInputForCheck.group(1);
+            customDividerList = Arrays.asList(customDivider.split(""));
+        }
+        System.out.println("커스텀 구분자 리스트: " + customDividerList); //메서드 기능 점검용 코드
+
+        duplicationFilter.addAll(customDividerList);
+        dividerList = new ArrayList<>(duplicationFilter);
+
+        System.out.println("최종 구분자 리스트: " + dividerList); //메서드 기능 점검용 코드
     }
 }
