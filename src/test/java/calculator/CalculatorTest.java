83095185ce94760477579666674de59557626c0c
Dev-LHY <dev.leehy@gmail.com>
2024-10-20 15:27:53 +0900

    test: 음수 입력에 대한 예외처리 단위 테스트 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -24,4 +24,16 @@ class CalculatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("아무것도 입력하지 않았습니다. 애플리케이션을 종료합니다.");
     }
+
+    @Test
+    void 음수_예외_테스트() {
+        assertThatThrownBy(() -> {
+            int[] testNumber = {1, -2, 3};
+            for (int i : testNumber) {
+                InputValidationException.isPositiveNumber(i);
+            }
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 입력입니다. 애플리케이션을 종료합니다.");
+    }
 }
