cc637824c6627e334d202d1af87f4aad122763a3
alpin87 <qortmdals94@naver.com>
2024-10-21 01:12:51 +0900

    feat: try-catch를 통한 예외출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,21 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Application {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-
-        String input = Console.readLine();
-
-        int result = calculator.add(input);
-        System.out.println("결과: " + result);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            Calculator calculator = new Calculator();
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = reader.readLine();
+            int result = calculator.add(input);
+            System.out.println("결과: " + result);
+        } catch (IOException e) {
+            System.out.println("입력을 읽는 중 오류가 발생했습니다: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
