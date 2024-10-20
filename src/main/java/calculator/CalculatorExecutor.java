79f80c597f43cf42ef4c2b3367708043bef59d97
JiEung2 <181757@jnu.ac.kr>
2024-10-20 22:11:47 +0900

    feat(calculatorExecutor): run 메서드 및 계산 로직 추가
    - run 메서드 추가하여 문자열 덧셈 계산 로직 구현
    - 커스텀 구분자 처리 및 기본 구분자 처리 로직 추가
    - 구분자 추출 및 숫자 합산을 위한 메서드 추가
    - 빈 문자열 입력 처리
    - 하나의 숫자 입력 처리

--- src/main/java/calculator/CalculatorExecutor.java
+++ src/main/java/calculator/CalculatorExecutor.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.List;
+
 public class CalculatorExecutor {
 
     private final Calculator calculator;
@@ -20,4 +22,50 @@ public class CalculatorExecutor {
         this.delimiterManager = delimiterManager;
     }
 
+    public void run() {
+        int result = 0;
+        inputHandler.printInputMessage();
+        String input = inputHandler.getInput();
+
+        if (inputValidator.isEmptyInput(input)) {
+            resultPrinter.printResult(result);
+            return;
+        }
+
+        if (inputValidator.isSingleNumberInput(input)) {
+            result = Integer.parseInt(input);
+            resultPrinter.printResult(result);
+            return;
+        }
+
+        inputValidator.validateInput(input);
+
+        if (input.startsWith("//")) {
+            result = processCustomDelimiter(input);
+        } else {
+            result = processDefaultDelimiter(input);
+        }
+
+        resultPrinter.printResult(result);
+    }
+
+    private int processCustomDelimiter(String input) {
+        String delimiter = delimiterParser.parseCustomDelimiter(input);
+        delimiterManager.registerCustomDelimiter(delimiter);
+
+        String removedCustomDelimiterInput = delimiterParser.removeCustomDelimiter(input);
+        return calculateSum(removedCustomDelimiterInput);
+    }
+
+    private int processDefaultDelimiter(String input) {
+        return calculateSum(input);
+    }
+
+    private int calculateSum(String input) {
+        List<String> delimiters = delimiterParser.extractDelimiters(input);
+        inputValidator.validateUnregisteredDelimiter(delimiters);
+
+        List<Integer> numbers = delimiterParser.parse(input);
+        return calculator.sumNumbers(numbers);
+    }
 }
