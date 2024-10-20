17b4b9e1369c759297ed7361c5a115ced35a7814
Lee Chae Rin <chaerin0916@gmail.com>
2024-10-20 18:59:20 +0900

    기본 구분자, 커스텀 구분자 main에서 구분하는 조건문 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,15 +2,26 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 import calculator.input.DefaultInput;
+import calculator.input.CustomInput;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        DefaultInput defaultInput = new DefaultInput();
+        int result = 0;
+        // "//"로 시작하면 Custom 구분자
+        if(input.startsWith("//")){
+            String deli = input.substring(2); // "//" 제거해서 구분자만 받기
+            String input_nums = Console.readLine();
+            CustomInput customInput = new CustomInput();
+            result = customInput.customParseSum(deli, input_nums);
+        }
+        else{
+            DefaultInput defaultInput = new DefaultInput();
+            result = defaultInput.defaultParseSum(input);
+        }
 
-        int result = defaultInput.defaultParseSum(input);
         System.out.println("결과 : " + result);
     }
 }
