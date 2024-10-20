28d6626291e47cd2983f3857c0ec64a86e317932
이윤서 <ddal0524@inu.ac.kr>
2024-10-20 16:34:57 +0900

    feat: 빈 문자열 처리 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,6 +2,10 @@ package calculator;
 
 public class StringCalculator {
     public int sumTokens(String[] tokens) {
+        if (tokens.length == 1 && tokens[0].isEmpty()) {
+            return 0; // 빈 문자열일 경우 0 반환
+        }
+
         int sum = 0;
         for (String token : tokens) {
             sum += Integer.parseInt(token); // 숫자로 변환 후 합산
