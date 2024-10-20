1298c3d14bf435d2f4771716fe2637cd913335c4
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-21 02:05:26 +0900

    refactor(InputView): Validator 생성 및 의존성 생성
    - validator 구현을 위해 view에서 의존성을 생성했습니다.
    - validator에 TODO List를 작성했습니다.

---
+++ src/main/java/calculator/validator/InputValidator.java
@@ -0,0 +1,11 @@
+package calculator.validator;
+
+public class InputValidator {
+    // TODO: 잘못된 입력이었을 경우에는 예외를 발생시킨다. 입력이 유효한지 검증한다.
+
+    // TODO: 숫자가 음수인지 확인한다.
+
+    // TODO: 커스텀 구분자를 형식에 맞게 입력했는지 확인한다.
+
+    // TODO: 숫자 또는 기본 구분자가 아닌 단순 문자로 시작되는지 확인한다.
+}
--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,13 +1,15 @@
 package calculator.view;
 
+import calculator.validator.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private final InputValidator inputValidator = new InputValidator();
     private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 
     public String readString() {
         System.out.println(INPUT_MESSAGE);
-        String InputString = Console.readLine();
-        return InputString;
+        String inputString = Console.readLine();
+        return inputString;
     }
 }
