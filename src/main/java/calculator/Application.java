250642337404e73934a70de381ccaaddb9e9b7f8
ehgk4245 <ehgkrk14@naver.com>
2024-10-21 01:55:23 +0900

    refactor(processCalculation): 계산 처리를 총괄하는 메서드로 구조 개선
    -새 메서드 processCalculation을 추가하여 입력 처리 및 계산을 총괄
    -기존 메서드 splitNumbers와 hasCustomDelimiter를 수정하여 메서드간의 관계 개선 및 코드 가독성 향상

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,33 +7,41 @@ import java.util.regex.Pattern;
 
 public class Application {
 
-    public static final String DELIMITER_PATTERN = "//(.)\\\\n(.*)";
+    public static final String CUSTOM_DELIMITER_PATTERN = "^//(.)\\\\n*";
     public static void main(String[] args) {
         String input = Console.readLine(); //문자열 입력받기.
-        if (input == null || input.trim().isEmpty()) { // 사용자 입력이 null이거나 ""일 경우
-            System.out.println(0);
-        } else {
-            String[] numbers = splitNumbers(input); // 구분자로 구분한 후 추출한 숫자
-            System.out.println(calculateSum(numbers));
-        }
+        System.out.println(processCalculation(input));
     }
 
-    public static String extractDelimiter(String input){ // "//(.)\\\\n(.*)" 패턴에서 구분자 추출하여 반환.
-        Pattern pattern = Pattern.compile(DELIMITER_PATTERN);
+    public static int processCalculation(String input){ //주어진 입력문자열 처리하여 숫자의 합 반환.
+        Matcher matcher = hasCustomDelimiter(input);
+        String[] numbers;
+        if(matcher != null){ //커스텀 구분자가 있을 경우
+            numbers = splitNumbers(input, matcher);
+        } else{ // 커스텀 구분자가 없을 경우
+            numbers = splitNumbers(input);
+        }
+        return calculateSum(numbers);
+    }
+
+    public static Matcher hasCustomDelimiter(String input){ // "^//(.)\\\\n(.*)" 패턴이 있을 시 Matcher반환.
+        Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_PATTERN);
         Matcher matcher = pattern.matcher(input);
         if(matcher.find()){
-            return matcher.group(1);
+            return matcher;
         }
         return null;
     }
 
-    public static String[] splitNumbers(String input){ // 구분자를 기준을 문자로된 숫자 추출 후 반환.
+    public static String[] splitNumbers(String input){ // 구분자를 기준으로 숫자로된 문자열 배열 반환.
         String delimiters = ",:"; // 기본 구분자
-        String customDelimiter = extractDelimiter(input); // 커스텀 구분자 추출
-        if(customDelimiter != null){ // 커스텀 구분자 존재시 기본 구분자에 추가
-            delimiters += customDelimiter;
-            input = input.substring(5); //커스텀 구분자 패턴 삭제
-        }
+        return input.split("[" + delimiters + "]");
+    }
+
+    public static String[] splitNumbers(String input, Matcher matcher){ // 구분자 + 커스텀 구분자를 기준으로 숫자로된 문자열 배열 반환.
+        String delimiters = ",:"; // 기본 구분자
+        delimiters += matcher.group(1); // 커스텀 구분자 추가
+        input = input.substring(5); // 커스텀 구분자 패턴 제거
         return input.split("[" + delimiters + "]");
     }
 
@@ -44,4 +52,5 @@ public class Application {
         }
         return sum;
     }
+
 }
