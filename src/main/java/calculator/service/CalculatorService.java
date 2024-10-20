b88e4780d0fa4b807b9d20c98f8e8daca1a422c1
young-in <hardworkingini@gmail.com>
2024-10-20 21:12:04 +0900

    feat : 커스텀 구분자 유무 확인 후, 구분자로 숫자 분리 구현

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,5 +1,8 @@
 package calculator.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CalculatorService {
 
     private static final String DEFAULT_DELIMITERS = ",|:";
@@ -10,6 +13,16 @@ public class CalculatorService {
 
         String delimiter = DEFAULT_DELIMITERS;
         String numbers = input;
+
+        if(input.startsWith("//")){
+            Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+            if(matcher.matches()){
+                delimiter = matcher.group(1);
+                numbers = matcher.group(2);
+            }else{
+                throw new IllegalArgumentException("잘못된 구분자 형식입니다.");
+            }
+        }
 
         String[] tokens = numbers.split(delimiter);
         return 0;
