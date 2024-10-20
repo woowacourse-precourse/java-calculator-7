fedf08ddfa314f62f3aa6c30c0ab8b540e0037a3
Junad-Park <qkrwnsgk1624@icloud.com>
2024-10-21 08:17:53 +0900

    fix(NumberExtractor): 정규식 메타 문자를 안전하게 처리하도록 Pattern.quote() 적용

--- src/main/java/calculator/util/NumberExtractor.java
+++ src/main/java/calculator/util/NumberExtractor.java
@@ -3,10 +3,10 @@ package calculator.util;
 import calculator.common.Delimiters;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class NumberExtractor {
-    private static final String REGEX_ESCAPE = "\\";
     private static final String REGEX_OR = "|";
 
     private NumberExtractor() {
@@ -14,7 +14,7 @@ public class NumberExtractor {
 
     public static List<Long> parseNumbers(String cleanedInput, Delimiters delimiters) {
         String delimiterPattern = delimiters.getDelimiters().stream()
-                .map(delimiter -> REGEX_ESCAPE + delimiter)
+                .map(Pattern::quote)
                 .collect(Collectors.joining(REGEX_OR));
 
         return Arrays.stream(cleanedInput.split(delimiterPattern))
