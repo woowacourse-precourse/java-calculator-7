7e7394fae25b3fd2337c2455d48cb1b47053b24d
Cori1304-Seong <andyjm7739@gmail.com>
2024-10-20 16:58:51 +0900

    feat(ApplicationTest): 테스트 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,13 +1,37 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 빈_문자열_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(""))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자_구분자_사용1() {
+        assertSimpleTest(() -> {
+            run("1,1,1,1");
+            assertThat(output()).contains("결과 : 4");
+        });
+    }
+    @Test
+    void 숫자_구분자_사용2() {
+        assertSimpleTest(() -> {
+            run("1,2,3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -17,10 +41,27 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_사용2() {
+        assertSimpleTest(() -> {
+            run(",,//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 영어_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a,b,c"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
