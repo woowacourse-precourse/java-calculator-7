a1f4aeb38b5c31a216bb0b5b9825d6b486ec1bc8
jihyun <jihyun3066@naver.com>
2024-10-20 23:17:43 +0900

    feat : main 구현 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        StringHandler stringHandler = new StringHandler();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = Console.readLine();
+        int value = stringHandler.checkString(str);
+
+        System.out.println("결과 : " + value);
+
     }
 }
