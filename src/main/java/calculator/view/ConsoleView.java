a06d2c8eca6b655dd134b86ad847ca1329c35096
jieun <ju3391@naver.com>
2024-10-20 19:24:43 +0900

    refactor(view): 예외 처리 메서드 삭제 및 주석 수정
    - 사용되지 않는 printErrorMessage() 메서드 삭제
    - 콘솔 입력 및 결과 출력 관련 주석 수정

--- src/main/java/calculator/view/ConsoleView.java
+++ src/main/java/calculator/view/ConsoleView.java
@@ -2,25 +2,18 @@ package calculator.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+// 콘솔에서 사용자 입력을 처리하는 클래스
 public class ConsoleView {
 
-    //사용자로부터 문자열 입력 받기
+    // 사용자로부터 문자열 입력을 받는 메서드
     public String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 
-    //계산 결과를 콘솔에 출력: 예) '결과 : 6'
+    // 계산 결과를 콘솔에 출력하는 메서드
     public void printResult(int result) {
         System.out.println("결과 : " +  result);
     }
-
-    //오류 발생시 오류 메시지 출력
-    public void printErrorMessage(String message) {
-        System.out.println("오류 발생 : " + message);
-    }
-
-
-
 
 }
