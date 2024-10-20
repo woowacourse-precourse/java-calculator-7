db420bd560e196a14dfd8540a2a23a0217a48fb8
wjdtjdgns <person3275@gmail.com>
2024-10-20 19:20:11 +0900

    feat(calculate): 빈 문자열, null 값일 경우 0반환

---
+++ src/main/java/calculator/model/StringCalculator.java
@@ -0,0 +1,12 @@
+package calculator.model;
+
+public class StringCalculator {
+
+    public int calculate(String input) {
+        if (input == null || input.isBlank()) {
+            return 0;
+        }
+
+        return 0;
+    }
+}
