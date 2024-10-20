929e1f56d95823131ff50087afe1c3be9fbd3710
GitDohyun <GitDohyun@gmail.com>
2024-10-21 03:18:06 +0900

    feat(Application): main 구현 최종 테스트를 위한 Application의 main()구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,11 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringInputReader inputReader = new StringInputReader();
+        StringValidator validator = new StringValidator();
+
+        String input = inputReader.readStringInput();
+        String result = validator.validateAndProcess(input);
+        System.out.println(result);
     }
 }
