3d167fe6e21bfe0d7899fca4e7bacda539582f6d
btongtong <leeym2615@naver.com>
2024-10-20 23:39:10 +0900

    fix(signanddivider): modify endSign to "\\\\n"
    Change the endSign from "\n" to "\\\\n" for literal string interpretation

--- src/main/java/calculator/SignAndDivider.java
+++ src/main/java/calculator/SignAndDivider.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class SignAndDivider {
     public static final String startSign = "//";
-    public static final String endSign = "\n";
+    public static final String endSign = "\\\\n";
     public static final String defaultDivider1 = ":";
     public static final String defaultDivider2 = ";";
 }
