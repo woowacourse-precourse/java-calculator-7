4904f9572b0f7c4508e41858c0e8972b0489aeb8
dbdb456 <adad568@naver.com>
2024-10-20 14:23:20 +0900

    feat(Controller): InputView, OutputView, Calculator 의존성 주입을 위한 생성자 추가

--- src/main/java/calculator/controller/Controller.java
+++ src/main/java/calculator/controller/Controller.java
@@ -1,4 +1,17 @@
 package calculator.controller;
 
+import calculator.domain.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutPutView;
+
 public class Controller {
+    private final InputView inputView;
+    private final OutPutView outputView;
+    private final Calculator calculator;
+
+    public Controller(InputView inputView, OutPutView outputView, Calculator calculator) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.calculator = calculator;
+    }
 }
