8e89b60b0e4fc5ead35c1a034c6fd88728ff57c5
JoonKyo <joonkyolee@gmail.com>
2024-10-20 18:58:08 +0900

    feat(InputData): 사용자에게 입력받은 문자열 및 기본 구분자를 처리하는 생성자 구현

--- src/main/java/calculator/domain/InputData.java
+++ src/main/java/calculator/domain/InputData.java
@@ -1,6 +1,18 @@
 package calculator.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputData {
     private static final String SEPARATOR_COMMA = ",";
     private static final String SEPARATOR_COLON = ":";
+    private final List<String> separators;
+    private final String firstInputData;
+
+    public InputData(String firstInputData) {
+        this.firstInputData = firstInputData;
+        separators = new ArrayList<>();
+        separators.add(SEPARATOR_COLON);
+        separators.add(SEPARATOR_COMMA);
+    }
 }
