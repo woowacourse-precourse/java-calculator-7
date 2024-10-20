9a0f886884a3854a58139c8b2f19569bef7e2af1
ruka-sh <ori8591y@gmail.com>
2024-10-20 11:44:12 +0900

    feat: 쉼표 또는 콜론을 구분자로 갖는 문자열의 합 계산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,11 +2,38 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.StringTokenizer;
+
+
 public class Application {
+    static int calculate(String s) {
+        //System.out.println(s);
+
+        int sumResult = 0;
+
+        if (s.equals("")) {
+            return 0;
+        }
+
+        StringTokenizer st = new StringTokenizer(s, ",|:");
+
+        while (st.hasMoreElements()) {
+            String token = String.valueOf(st.nextElement());
+
+            sumResult += Integer.parseInt(token);
+        }
+
+        return sumResult;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
-        Console.readLine();
+        String inputString = Console.readLine();
+
+        calculate(inputString);
+        // System.out.println("결과 : " + calculate(inputString));
+
     }
 }
