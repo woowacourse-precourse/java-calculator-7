e01ce89bc5b85943713bb32fbfa7bb56738b9539
Seung Won Lee <devsolfe@gmail.com>
2024-10-20 20:30:50 +0900

    test: add redcase test in TokenizerTest

--- src/test/java/calculator/TokenizerTest.java
+++ src/test/java/calculator/TokenizerTest.java
@@ -3,10 +3,11 @@ package calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TokenizerTest {
     @Test
-    void 기본_구분자_테스트() {
+    void 기본_구분자_성공() {
         // given
         String s = "1,2:3";
         Tokenizer t = new Tokenizer(s);
@@ -19,7 +20,7 @@ class TokenizerTest {
     }
 
     @Test
-    void 커스텀_구분자_테스트() {
+    void 커스텀_구분자_성공() {
         // given
         String s = "//;\\n1;2:3";
         Tokenizer t = new Tokenizer(s);
@@ -29,5 +30,14 @@ class TokenizerTest {
 
         // then
         assertArrayEquals(result, new int[]{1, 2, 3});
+    }
+
+    @Test
+    void 잘못된_입력() {
+        // given
+        String s = "1?2:3";
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> new Tokenizer(s));
     }
 }
