ea2b17eff77bf45675784cf6bbb7e1ab2bf19438
coolman0123 <socool4m@naver.com>
2024-10-20 23:38:51 +0900

    feat(Separator): 커스텀 구분자를 추출하는 기능
     `handleCustomSeparator()`는 커스텀 구분자가 있다면 정규 표현식을
      통해 커스텀 구분자를 추출하여 `activeSeparator`에 기본 구분자와 함께
      추가합니다.
    
      Closes #4

--- src/main/java/calculator/model/Separator.java
+++ src/main/java/calculator/model/Separator.java
@@ -5,15 +5,30 @@ import java.util.regex.Pattern;
 
 public class Separator {
     private static final String DEFAULT_SEPARATOR = ",|:";
+    static String activeSeparator;
+    String customSeparator;
 
 
     public static String checkCustomSeparator(String input) {
         if (input.startsWith("//")) {
             System.out.println("커스텀 있음");
-            return input;
+            return handleCustomSeparator(input);
         }
         System.out.println("커스텀 없음");
         return DEFAULT_SEPARATOR;
+    }
+
+    private static String handleCustomSeparator(String input) {
+        Matcher matcher = Pattern.compile("//(.)\\\\n(.*)").matcher(input);
+        if (matcher.find()) {
+            String customSeparator = Pattern.quote(matcher.group(1));
+            System.out.println("커스텀 구분자 : " + customSeparator );
+            activeSeparator = DEFAULT_SEPARATOR + "|" + customSeparator;
+            System.out.println(activeSeparator);
+            return activeSeparator;
+        }
+        System.out.println("커스텀 구분자 없음");
+        return input;
     }
 
 }
