0f97ce5decf08817c60ff5ffe8f4ba3adf7a0f85
KNU-K <knukang334@gmail.com>
2024-10-20 14:45:32 +0900

    refactor(app): 입력과 출력 처리 메소드 분리
    main 내부에서의 불필요한 형식의 내용을 줄이고 최대한 간결하게 나타내기 위해서 입력과 출력 멘트를 묶어서 따로 분리함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,12 +5,21 @@ import calculator.service.CalculatorServiceImpl;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    public static String input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static void printResult(Integer result) {
+        System.out.println("결과 : " + result);
+    }
+
     public static void main(String[] args) {
         CalculatorService calculatorService = new CalculatorServiceImpl();
         try {
-            String input = Console.readLine();
-            Integer result = calculatorService.calculate(input);
-            System.out.println("결과 : " + result);
+            String command = input();
+            Integer result = calculatorService.calculate(command);
+            printResult(result);
         } finally {
             Console.close();
         }
