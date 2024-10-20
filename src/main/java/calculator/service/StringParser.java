9c7c0cfb95db2ae99452662db5f5eeeef8dbe91a
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-10-20 20:31:41 +0900

    feat : // 커스텀 구분자를 추출하고 숫자 부분과 커스텀 구분자를 반환

--- src/main/java/calculator/service/StringParser.java
+++ src/main/java/calculator/service/StringParser.java
@@ -19,6 +19,7 @@ public class StringParser {
         List<String> result = new ArrayList<>();
         System.out.println(matcher.matches());
 
+        // 추출한 커스텀 구분자와 숫자부분을 리스트에 담음
         if (matcher.matches()){
 
             result.add(matcher.group(1));
@@ -30,6 +31,32 @@ public class StringParser {
         return result;
     }
 
+    //문자 나누기기
+    public List<String> splitNumber(String input){
+        String delimiter = DEFAULT_DELIMITER;
+        //커스텀 구분자가 존재하면  커스텀 구분자 파싱 실행
+        if (input.startsWith("//")){
+
+            List<String> customDelimiterParse = customDelimiterParse(input);
+            delimiter += "|";
+            delimiter += customDelimiterParse.get(0);
+            input = customDelimiterParse.get(1);
+        }
+
+        String[] tokens = input.split(delimiter);
+
+        List <String> result =new ArrayList<>();
+
+        for (String token : tokens) {
+            if (!token.isEmpty()) {
+                result.add(token);
+            }
+        }
+
+        return result;
+
+
+    }
 
 
 
