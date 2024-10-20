611b968f6c933d23e946e2d69d3193ed7360faa1
ariimo <159447466+ariimo@users.noreply.github.com>
2024-10-21 03:00:21 +0900

    feat: add custom separator logic
    입력이 //으로 시작하는 경우 커스텀 구분자 추가 로직 시작
    - \n이 없는 경우 예외처리
    - 커스텀 문자의 길이가 1이 아닐 경우 예외처리
    - 커스텀 문자가 숫자일 경우 예외처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,10 +3,17 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
     public static void main(String[] args) {
 
+        Separator separator = new Separator();
 //        입력받기.
         String input = getInput();
+
+//        //으로 시작하는 경우 커스텀 구분자 추출하기.
+        if (input.startsWith("//")) {
+            input = separator.extractCustomSeparatorFrom(input);
+        }
     }
 
     public static String getInput() {
--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -2,5 +2,33 @@ package calculator;
 
 public class Separator {
 
+    static final int START_OF_CUSTOM_SEPARATOR = 2;
     static final String[] DEFAULT_SEPARATOR = {",", ":"};
+    String customSeparator = "";
+
+    public String extractCustomSeparatorFrom(String input) {
+        int endOfCustomSeparator = input.indexOf("\\n");
+
+//        \\n이 없는 경우 예외 처리
+        if (endOfCustomSeparator == -1) {
+            throw new IllegalArgumentException("구분자는 반드시 //와 \\n 사이에 있어야 합니다.");
+        }
+
+//         커스텀 문자의 길이가 1이 아닐 경우 예외처리
+        String extractedSeparator = input.substring(START_OF_CUSTOM_SEPARATOR,
+            endOfCustomSeparator);
+        if (extractedSeparator.length() != 1) {
+            throw new IllegalArgumentException("커스텀 구분자는 하나의 문자만 가능합니다.");
+        }
+
+//        커스텀 구분자가 숫자인 경우 예외 처리
+        if (Character.isDigit(extractedSeparator.charAt(0))) {
+            throw new IllegalArgumentException("커스텀 구분자는 숫자가 될 수 없습니다.");
+        }
+
+        this.customSeparator = extractedSeparator;
+
+//         커스텀 구분자 정의를 제외하고 반환
+        return input.substring(endOfCustomSeparator + 2);
+    }
 }
