40d0783a0c911dc9bc8097b1819b12e4b2a1303c
longrunpc <pcjmo4050@gmail.com>
2024-10-21 02:08:40 +0900

    feat(calculate): CalculatorServiceImpl 클래스에 기본 구조 구현
    CalculatorService 인터페이스의 구현체인  CalculatorServiceImpl 클래스를 생성한다.
    calculateSum, parseIntArray 메서드의 기본 구조를 추가한다.

---
+++ src/main/java/calculator/calculator/CalculatorServiceImpl.java
@@ -0,0 +1,17 @@
+package calculator.calculator;
+
+/**
+ * CalculatorService 인터페이스의 구현체.
+ * 숫자 배열의 합계를 계산하고, 문자열 배열을 정수 배열로 변환하는 기능을 제공합니다.
+ */
+public class CalculatorServiceImpl implements CalculatorService {
+    @Override
+    public int calculateSum(int[] numbers) {
+        return 0;
+    }
+
+    @Override
+    public int[] parseIntArray(String[] inputStrings) {
+        return new int[0];
+    }
+}
