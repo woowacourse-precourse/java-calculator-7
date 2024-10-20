21f26a2dd459f3b21399e547f58a4b91039fc972
hongkikii <hyangmeeh@icloud.com>
2024-10-20 16:25:02 +0900

    refactor(Application): main 메서드 책임 수정
    프로그램 실행 진입점으로써의 기능에 충실하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,23 +1,10 @@
 package calculator;
 
-import java.math.BigInteger;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        UserInteractionManager userInteractionManager = new UserInteractionManager();
-        InputManager inputManager = new InputManager();
-        SeparatorManager separatorManager = new SeparatorManager();
-        OperandManager operandManager = new OperandManager(separatorManager);
-        Operator operator = new Operator();
-
-        String input = userInteractionManager.readInput();
-        inputManager.save(input);
-        if(inputManager.isCustomSeparatorCandidateExisted()) {
-            separatorManager.save(inputManager.getSeparatorCandidate());
-        }
-        operandManager.save(inputManager.getOperandCandidates());
-        BigInteger result = operator.add(operandManager.getOperands());
-        userInteractionManager.printOutput(result);
+        ApplicationContext applicationContext = new ApplicationContext();
+        ApplicationRunner applicationRunner = new ApplicationRunner(applicationContext);
+        applicationRunner.run();
     }
 }
