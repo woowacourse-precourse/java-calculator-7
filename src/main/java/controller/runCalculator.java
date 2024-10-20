e364fa1453eb8aceb303e6f9b89ba8bb448f2565
윤민수 <koelysa5696@naver.com>
2024-10-21 00:07:40 +0900

    줄바꿈 삭제

--- src/main/java/controller/runCalculator.java
+++ src/main/java/controller/runCalculator.java
@@ -6,15 +6,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class RunCalculator {
     public void run() {
         DelimiterService delimiterService = new DelimiterService();
-        System.out.print("덧셈할 문자열을 입력해 주세요.");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine(); // 문자를 입력받고 앞뒤 공백을 제거.
 
         if (input.isEmpty()) { // 빈 입력이면 0을 반환
-            System.out.println(0);
+            System.out.print(0);
             return;
         }
         int sumResult = delimiterService.DelimiterProcess(input);
-        System.out.println("결과 : " + sumResult);
+        System.out.print("결과 : " + sumResult);
 
     }
 }
