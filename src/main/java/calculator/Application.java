827246dd56607606e2398d965f424efdaf59283e
sgy0917 <sgy0917@naver.com>
2024-10-20 23:07:37 +0900

    refacto<application> 클래스 구문 정리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,59 +1,48 @@
 package calculator;
 
-
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        String userInput = Console.readLine();
-        int start = 0;
-        int end = 0;
-        if(userInput.startsWith("//")) {
-            start = userInput.indexOf("//");
-            end = userInput.indexOf("\\n");
-            String custom = null;
-            if (end != -1) {
-                // "//" 의 문자열 길이 만큼 subString 첫번째 인자값 설정 , "\n"의 시작지점 까지 두번째 인자값
-                // "//" custom "\n"  커스텀 구분자의 인덱스 값을 찾아내 문자열로 변환한다.
-                custom = userInput.substring(start + 2, end);
-                //커스텀구분자 지정 값들을 배열에서 없앤다. end의 원 스트링의 길이는 3이기에 2를 더해준다.
-                String customInput = userInput.substring(end + 2);
-                //커스텀 문자열 삭제
-                String []input = customInput.split(custom);
-                int num =0;
-                for(String s : input){
-                    if(!s.isEmpty()){
-                        num += Integer.parseInt(s);
-                    }else{
-                        throw new IllegalArgumentException("숫자가 존재하지않습니다."+ num);
-                    }
-                }
-
-                System.out.println(num);
-            }else{
-                throw new IllegalArgumentException("범위를 지정할 수 없습니다"+ end);
-            }
+        try {
+            String userInput = Console.readLine();
+            System.out.println("결과 : " + calculate(userInput));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
         }
+    }
 
+    private static int calculate(String input) {
+        if (input.startsWith("//")) {
+            int end = input.indexOf("\n");
+            if (end == -1) {
+                throw new IllegalArgumentException("구분자 형식이아님.");
+            }
+            //"//"의 문자열 기후 부터 end 의 범위까지
+            String customDelimiter = input.substring(2, end);
+            return splitAndSum(input.substring(end + 1), customDelimiter);
+        } else {
+            return splitAndSum(input, ",|:");
+        }
+    }
 
+    private static int splitAndSum(String numbers, String delimiter) {
+        if (numbers.isEmpty())
+            return 0;
+        String[] tokens = numbers.split(delimiter);
+        int sum = 0;
+        for (String token : tokens) {
+            sum += parse(token);
+        }
+        return sum;
+    }
 
-
-
-
-
-//        String [] input = userInput.split("[,/]");
-//
-//        int num = 0;
-//        for(int i =0; i<input.length; i++){
-//            System.out.println(input[i]);
-//
-//            //input[i] 공백 처리;
-//            if(!input[i].isEmpty()){
-//                num += Integer.parseInt(input[i]);
-//
-//            }
-//        }
-//        System.out.println(num);
+    private static int parse(String number) {
+        int num = Integer.parseInt(number);
+        if (num < 0) {
+            throw new IllegalArgumentException("음수는 허용안함.");
+        }
+        return num;
     }
 }
