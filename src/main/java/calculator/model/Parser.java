9ff3b841af0ed459c2a77be30cae479971205c70
Parkjyun <jaeyun0507@naver.com>
2024-10-20 16:17:01 +0900

    feat(Parser): 문자열의 커스텀 구분자 사용 여부를 판단한다

--- src/main/java/calculator/model/Parser.java
+++ src/main/java/calculator/model/Parser.java
@@ -4,5 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
     private List<String> delimiters = new ArrayList<>(List.of(",", ":"));
+    private String input;
+
+    public Parser(String input) {
+        this.input = input;
+    }
+
+    private boolean usesCustomValidator() {
+        return input.startsWith(CUSTOM_DELIMITER_PREFIX);
+    }
 }
