1c8505b50b82fd68e1d298556bd91963c7ad100d
belight2 <chlwnsgur090@gmail.com>
2024-10-20 11:56:52 +0900

    feat: Application Constructor와 initialize(), calculate() method 작성
    문자열을 인자로 받는 생성자와 문자열 검사, 파싱을 진행할 initialize()와 리스트에 숫자들을 계산하는 calculate()로 나누었다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,27 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private String inputString;
+
+    Application(String inputString) {
+        this.inputString = inputString;
+        initialize();
+    }
+
+    public Integer calculate() {
+        return 0;
+    }
+
+    private void initialize() {
+
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Application calculator = new Application(Console.readLine());
+        System.out.println(calculator.calculate());
     }
 }
