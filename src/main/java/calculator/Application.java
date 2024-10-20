77836ada8238797ba8f1cecc1b4301de0ef40b8b
RawBrain <95132660+RawBrain@users.noreply.github.com>
2024-10-20 10:38:44 +0900

    feat: add separate number function

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Application {
     static List<String> separator = Arrays.asList(",", ":");
@@ -11,6 +12,8 @@ public class Application {
         try {
             String userInput = Console.readLine();
             addSeparator(userInput);
+            int[] numbers = separatorNum(userInput);
+
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
@@ -27,5 +30,30 @@ public class Application {
         }
     }
 
+    public static int[] separatorNum(String userInput) {
+        StringBuilder separator_sb = new StringBuilder();
+        for (String str : separator) {
+            if (!separator_sb.isEmpty()) {
+                separator_sb.append("|");
+            }
+            separator_sb.append(Pattern.quote(str));
+        }
+        String[] strarr =  userInput.split(separator_sb.toString(), -1);
+        int[] intarr = new int[strarr.length];
+
+
+        for(int i = 0; i < intarr.length; i++) {
+            try {
+                intarr[i] = Integer.parseInt(strarr[i]);
+                if (intarr[i] < 0) {
+                    throw new IllegalArgumentException("오류: 음수는 허용되지 않습니다.");
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("오류: 허용하지 않는 구분자를 사용하였습니다: " + strarr[i]);
+            }
+        }
+
+        return intarr;
+    }
 
 }
