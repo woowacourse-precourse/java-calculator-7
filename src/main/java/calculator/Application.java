c887ff412126f15a7c6f8973ff3c7ea998218a22
cba700 <dlackddls5@naver.com>
2024-10-20 22:09:27 +0900

    feat : 입출력 제대로 나오도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,12 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String str = Console.readLine();
+        String input = Console.readLine();
 
-        System.out.println(str);
-
-;    }
+        int result = StringCalculator.calculate(input);
+        System.out.println("결과 : " + result);
+    }
 }
