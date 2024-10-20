0479ebdb18ef4a50f54db1b433a12b7eedd7de11
sso9594 <sso9594@naver.com>
2024-10-21 01:10:21 +0900

    feat(Application): 문자열 계산기 프로그램 초기화 및 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.service.CalculatorService;
+import calculator.view.CalculatorInputView;
+import calculator.view.CalculatorOutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CalculatorInputView inputView = new CalculatorInputView();
+        CalculatorService calculatorService = new CalculatorService();
+        CalculatorOutputView outputView = new CalculatorOutputView();
+
+        CalculatorController calculatorController = new CalculatorController(inputView, calculatorService, outputView);
+        calculatorController.run();
     }
 }
