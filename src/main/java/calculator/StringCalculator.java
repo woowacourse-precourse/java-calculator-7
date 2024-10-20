c8fafc13feb401d89abe90da2be6c4305bca0765
skadjs <skadjs11@gmail.com>
2024-10-21 05:14:14 +0900

    fix: 메서드 기능 점검용 코드 삭제
    함수가 기능을 잘하는지 알아보기 위해 적었던 print() 함수를 제거함

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -24,12 +24,10 @@ public class StringCalculator {
     public void getUserInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         this.userInput = Console.readLine();
-        System.out.println("사용자 입력값 : " + this.userInput); //메서드 기능 점검용 코드 - 추후 삭제
     }
 
     public void removeBlank() {
         this.noBlankInput = this.userInput.replaceAll(" ", "");
-        System.out.println("공백 제거된 사용자 입력값: " + this.noBlankInput); //메서드 기능 점검용 코드
     }
 
     public void addCustomDivider() {
@@ -41,22 +39,17 @@ public class StringCalculator {
             customDivider = userInputForCheck.group(1);
             customDividerList = Arrays.asList(customDivider.split(""));
         }
-        System.out.println("커스텀 구분자 리스트: " + customDividerList); //메서드 기능 점검용 코드
 
         duplicationFilter.addAll(customDividerList);
         dividerList = new ArrayList<>(duplicationFilter);
-
-        System.out.println("최종 구분자 리스트: " + dividerList); //메서드 기능 점검용 코드
     }
 
     public void removeDividerSettings() {
         this.noBlankInput = this.noBlankInput.replaceAll("//(.+)\\\\n", "");
-        System.out.println("구분자 지정 문구 제거된 문자열 : " + noBlankInput); //메서드 기능 점검용 코드
     }
 
     public void makeStringForSplit() {
         regexString = String.join("|", dividerList);
-        System.out.println("구분자 정규식 문자열 : " + regexString); //메서드 기능 점검용 코드
     }
 
     public void extractNumbers() {
