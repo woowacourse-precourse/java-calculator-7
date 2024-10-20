fc2a2e7af72737d915ce46d9a5d11742e8c53f29
Yunsu4 <ysa020428@gmail.com>
2024-10-20 15:17:34 +0900

    chore: modify variable name concisely
    because the class name contains regex, modify the regex contained in the variable name to be omitted

--- src/main/java/calculator/staticValue/RegexPatterns.java
+++ src/main/java/calculator/staticValue/RegexPatterns.java
@@ -1,20 +1,20 @@
 package calculator.staticValue;
 
-public enum StaticValue {
+public enum RegexPatterns {
 
 
     CUSTOM_SEPARATOR_PREFIX("//"),
     CUSTOM_SEPARATOR_SUFFIX("\\\\n"),
 
-    CUSTOM_SEPARATOR_REGEX("//(.*?)\\\\n"),
+    CUSTOM_SEPARATOR("//(.*?)\\\\n"),
 
-    BASIC_SEPARATORS_REGEX(",|:"),
-    INVALID_CHAR_REGEX("([a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣])"),
-    POSITIVE_NUMBER_REGEX("([1-9]+)");
+    BASIC_SEPARATORS(",|:"),
+    INVALID_CHAR("([a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣])"),
+    POSITIVE_NUMBER("([1-9]+)");
 
     final String stringValue;
 
-    StaticValue(String stringValue) {
+    RegexPatterns(String stringValue) {
         this.stringValue = stringValue;
     }
 
