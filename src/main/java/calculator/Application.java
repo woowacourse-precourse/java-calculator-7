7351000954321c5ae2aadf8078ba20d0a148233f
Now <khj990322@gmail.com>
2024-10-21 06:27:55 +0900

    feat: Add calSum method

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,12 +2,37 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
             String input = Console.readLine();
+            int res = calSum(input);
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(e);
         }
+    }
+
+    private static int calSum(String input) {
+        if(input.isEmpty()){
+            return 0;
+        }
+        String separator = ",|;";
+        String numbers = input;
+
+        if(input.startsWith("//")){
+            Matcher matcher = Pattern.compile("//(.)\\n(.*)").matcher(input);
+            if(matcher.find()){
+                separator = Pattern.quote(matcher.group(1));
+                numbers = matcher.group(2);
+            }
+        }
+        return Arrays.stream(numbers.split(separator))
+                .mapToInt(Application::parseNumbers)
+                .sum();
     }
 }
