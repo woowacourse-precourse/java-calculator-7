d27c6ad662cb8e3e2433d9cdc288cd214e72953a
ssggii <haan7525@gmail.com>
2024-10-20 21:40:55 +0900

    fix: 커스텀 구분자 사용하는 경우 문자열 전처리 오류 수정

--- src/main/java/calculator/InputProcessor.java
+++ src/main/java/calculator/InputProcessor.java
@@ -15,8 +15,8 @@ public class InputProcessor {
     }
 
     /**
-     * 구분자 맞춰서 연산 대상이 되는 문자열 결정
-     * 기본 구분자이면 첫번째 입력값을 반환하고, 커스텀 구분자이면 추가로 입력 받은 두번째 문자열을 반환한다.
+     * 구분자에 따라 입력받은 문자열 전처리
+     * 커스텀 구분자이면 'n'까지는 버리고 그 뒤의 문자열만 반환하고 기본 구분자는 입력 받은 문자열 그대로 반환
      */
     public String getOperationInput() {
         System.out.println("덧셈할 문자열을 입력해주세요.");
@@ -24,7 +24,7 @@ public class InputProcessor {
         if (input.startsWith("//")){ // 커스텀 구분자 사용
             char customDelimiter = extractCustomDelimiter(input);
             delimiterRegex = delimiterRegex + "|" + Pattern.quote(Character.toString(customDelimiter));
-            input = getUserInput();
+            input = input.substring(input.indexOf('n') + 1);
         }
         return input;
     }
@@ -40,8 +40,8 @@ public class InputProcessor {
      * 커스텀 구분자 추출
      */
     private char extractCustomDelimiter(String customInput) {
-        if (customInput.length() > 3 || customInput.length() == 2 ||
-                Character.getNumericValue(customInput.charAt(2)) != -1) {
+        if (Character.getNumericValue(customInput.charAt(2)) != -1 ||
+                !(customInput.charAt(3) == '\\' && customInput.charAt(4) == 'n')) {
             handleInvalidInput(customInput);
         }
         return customInput.charAt(2);
