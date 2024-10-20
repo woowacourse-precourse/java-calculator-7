e98d66610694f73581d2e4e34fd6246b9189609e
신동현 <donghyeun02@gmail.com>
2024-10-20 17:08:37 +0900

    feat : 숫자 덧셈 기능

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -1,4 +1,17 @@
 package calculator.domain;
 
+import calculator.utils.SplitString;
+
 public class StringCalculator {
+    public int calculateSum(String input) {
+        String[] numbers = SplitString.splitInput(input);
+        int sum = 0;
+
+        for (String number : numbers) {
+            int parsedNumber = ParseNumber.parseNumber(number);
+            sum += parsedNumber;
+        }
+
+        return sum;
+    }
 }
