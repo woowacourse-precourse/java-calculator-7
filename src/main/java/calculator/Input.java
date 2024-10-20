fcd57867633a0a2b30ce5de9d3bf5dbc118b5c5b
Park, Sojeong <sojeong2101@gmail.com>
2024-10-20 18:11:10 +0900

    feat($Input): 안내 메시지 출력 구현

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
+    public Input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
     public String getNumberWithSeparator() {
         return Console.readLine();
     }
