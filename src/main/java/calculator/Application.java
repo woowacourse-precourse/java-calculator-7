9fdffdf7ba054771b6e666dbe8312edda909868e
k1mjunyoung <me@kimjunyoung.com>
2024-10-20 18:36:42 +0900

    fix: 커스텀 구분자 사용 시 기본 구분자 대체하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,30 +1,38 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import org.junit.platform.commons.util.StringUtils;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = Console.readLine();
         int result = 0;
-        List<String> seperator = new ArrayList<>();
-        seperator.add(",");
-        seperator.add(";");
+        String customSeperator = new String();
 
         if (input.startsWith("//") && input.charAt(3) == 92 && input.charAt(4) == 'n') {
             char customSeperatorChar = input.charAt(2);
-            String cumtomSeperator = Character.toString(customSeperatorChar);
-            seperator.add(cumtomSeperator);
+
+            customSeperator = Character.toString(customSeperatorChar);
         }
 
-        String[] splitedInput = input.split(",|;");
-        for (String inputNumber : splitedInput) {
-            int number = Integer.parseInt(inputNumber);
-            result += number;
+        if (StringUtils.isBlank(customSeperator)) {
+            String[] splitedInput = input.split(",|;");
+            for (String inputNumber : splitedInput) {
+                int number = Integer.parseInt(inputNumber);
+                result += number;
+            }
         }
+
+        if (!StringUtils.isBlank(customSeperator)) {
+            String subInput = input.substring(5);
+            String[] splitedInput = subInput.split(customSeperator);
+            for (String inputNumber : splitedInput) {
+                int number = Integer.parseInt(inputNumber);
+                result += number;
+            }
+        }
+
 
         System.out.println("결과 : " + result);
     }
