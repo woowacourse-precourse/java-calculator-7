f4b02e1bcb91ac9ab74facb20ee39d64880cc9ec
jieun <ju3391@naver.com>
2024-10-20 18:16:41 +0900

    refactor(InputValidator.java): 입력 검증 로직 및 구분자 처리 개선
    - 입력 값에 대한 검증과 음수 처리 수정
    - 기본 및 커스텀 구분자 처리 수정

--- src/main/java/calculator/model/InputValidator.java
+++ src/main/java/calculator/model/InputValidator.java
@@ -5,39 +5,36 @@ import java.util.regex.Pattern;
 //입력 검증
 public class InputValidator {
 
-    //기본 구분자 입력값 검증: 쉼표 또는 콜론 외에는 모두 양수인지 검증
-    public void validate(String input) {
+    //입력을 받아 검증하고 숫자 배열 반환
+    public String[] validate(String input) {
         if(input == null || input.isEmpty()) {
-            return;
+            return new String[] {"0"};
         }
+
         if (input.startsWith("//")) {
             // 커스텀 구분자가 있을 때 처리
-            validateCustomDelimiter(input);
+            return validateCustomDelimiter(input);
         } else {
             // 기본 구분자(쉼표, 콜론) 처리
-            validateDefaultDelimiter(input);
+            return validateDefaultDelimiter(input);
         }
     }
 
-    // 기본 구분자 입력값 검증: 쉼표(,) 또는 콜론(:)
-    private void validateDefaultDelimiter(String input) {
+    // 기본 구분자 쉼표(,) 또는 콜론(:) 처리
+    private String[] validateDefaultDelimiter(String input) {
         String[] numbers = input.split(",|:");
         checkNegativeNumbers(numbers);
+        return numbers;
     }
 
-    //커스텀 구분자 입력값 검증
+    //커스텀 구분자 입력값 처리 및 검증
     //`"//"`와 `"\n"` 사이에 커스텀 구분자 지정 가능
     //커스텀 구분자가 지정되면 기본 구분자 사용 불가
     //구분자와 구분자 설정 외에는 모두 양수인지 검증
-    private void validateCustomDelimiter(String input) {
-
-        // "//"로 시작하는지 확인
-        if (!input.startsWith("//")) {
-            throw new IllegalArgumentException("커스텀 구분자는 //로 시작해야 합니다.");
-        }
+    private String[] validateCustomDelimiter(String input) {
+        int delimiterIndex = input.indexOf("\\n");
 
         // '\n' 위치 파악 (찾지 못한 경우 예외 처리)
-        int delimiterIndex = input.indexOf("\\n");
         if (delimiterIndex == -1 ) { //|| delimiterIndex <= 2
             throw new IllegalArgumentException("입력 형식이 잘못되었습니다. 커스텀 구분자는 //로 시작하고 \\n으로 끝나야 합니다.");
         }
@@ -61,11 +58,12 @@ public class InputValidator {
         String[] numbers = numbersPart.split(Pattern.quote(delimiter));
         //음수 예외 처리
         checkNegativeNumbers(numbers);
+        return numbers;
     }
 
-    //예외 처리(음수)
+    //음수 검증
     //사용자 잘못된 값 입력 시 `IllegalArgumentException` 발생하여 프로그램 종료
-    private void checkNegativeNumbers(String[] numbers) {
+    public void checkNegativeNumbers(String[] numbers) {
         for (String number : numbers) {
             if (!number.isEmpty()) { // 빈 문자열이 아닌 경우만 처리
                 int num = Integer.parseInt(number);
