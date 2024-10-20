ec858d31a8efede1d4250e3567c53e87e5b012ee
btongtong <leeym2615@naver.com>
2024-10-20 16:18:21 +0900

    fix(signanddivider): add public access modifier

--- src/main/java/calculator/SignAndDivider.java
+++ src/main/java/calculator/SignAndDivider.java
@@ -1,6 +1,6 @@
 package calculator;
 
-class SignAndDivider {
+public class SignAndDivider {
     public static final String startSign = "//";
     public static final String endSign = "\n";
     public static final String defaultDivider1 = ":";
