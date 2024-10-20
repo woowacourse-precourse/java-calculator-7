ac8e7c02d4579d65aab02d46dc03af1105d310a6
jjun522 <tjwowns522@>
2024-10-20 15:42:38 +0900

    feat: 커스텀 구분자 덧셈 기능 추가
    커스텀 구분자를 지정하여 문자열을 분리하고 덧셈을 수행하는 기능 추가

---
+++ src/main/java/calculator/util/CustomDelimiterHandler.java
@@ -0,0 +1,21 @@
+package calculator.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomDelimiterHandler {
+    // 커스텀 구분자가 있는지 확인하는 매처 반환
+    public static Matcher getCustomDelimiterMatcher(String str) {
+        return Pattern.compile("//(.)\\\\n(.*)").matcher(str);
+    }
+
+    // 커스텀 구분자 추출
+    public static String getCustomDelimiter(Matcher matcher) {
+        return matcher.group(1);
+    }
+
+    // 커스텀 구분자를 사용하여 숫자 부분 추출
+    public static String getNumbersWithCustomDelimiter(Matcher matcher) {
+        return matcher.group(2);
+    }
+}
