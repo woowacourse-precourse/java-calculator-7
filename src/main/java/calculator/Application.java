1e7a4ad55f305552c9c469991c2d60ecdf58126a
HiImSUN <kshk08@hanyang.ac.kr>
2024-10-20 19:43:14 +0900

    feat: 기본 입력 처리 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.*;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Character> inputList = new ArrayList<>();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = readLine();
+        int sum = 0;
+
+        if (input.isEmpty()) {
+            System.out.println("결과 : " + sum);
+            Console.close();
+            return;
+        }
+        for (char c : input.toCharArray()) {
+            inputList.add(c);
+        }
     }
 }
