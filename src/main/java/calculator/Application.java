8cd935b1f71e374c7f8f2b6a299468875d10b4ab
woohyuk <7dngur7@naver.com>
2024-10-20 15:47:03 +0900

    feat(Application): 커스텀 구분자 처리 로직 추가
    기본 구분자(‘:’, ‘,’) 외에 커스텀 구분자를 입력받아 처리할 수 있는 로직을 추가:
    - "//"로 시작하는 커스텀 구분자 확인 기능 구현
    - 구분자를 추출하고 새로운 구분자를 HashSet에 저장하는 기능 추가
    - 입력 문자열에서 커스텀 구분자와 기본 구분자를 함께 처리하는 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,48 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        String s = Console.readLine();
+
+        // 기본 구분자 넣어줌
+        HashSet<String> sepLetterHash = new HashSet<>();
+        sepLetterHash.add(":");
+        sepLetterHash.add(",");
+
+        int letterIndex = 0;
+
+        // 커스텀 구분자 있는지 확인
+        while (s.length() > 2 && letterIndex + 1 < s.length() && s.charAt(letterIndex) == '/' && s.charAt(letterIndex + 1) == '/') {
+            letterIndex += 2;  // "//" 스킵
+            StringBuilder sepLetter = new StringBuilder();
+            boolean endFlagChk = false;
+
+            // 새로운 구분자 추출
+            while (letterIndex + 1 < s.length() && (s.charAt(letterIndex) != '\\' || s.charAt(letterIndex + 1) != 'n')) {
+                if (s.charAt(letterIndex) == '\\' && s.charAt(letterIndex + 1) == 'n') {
+                    endFlagChk = true;
+                    break;
+                }
+                sepLetter.append(s.charAt(letterIndex));
+                letterIndex++;
+            }
+
+            System.out.println("cur 구분자: " + sepLetter);
+            if (endFlagChk = true) {
+                sepLetterHash.add(sepLetter.toString());
+                letterIndex += 2;  // '\n'을 스킵
+            }
+        }
+
+        // 디버깅: 구분자 확인
+        System.out.println("sep: ");
+        for (String sep : sepLetterHash) {
+            System.out.println(sep);
+        }
     }
 }
