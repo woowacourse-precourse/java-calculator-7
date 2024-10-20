52a17d28167a69cc7f5d0cda7e3b1fcf27173953
MinJae00 <exceedmin@gmail.com>
2024-10-20 23:17:49 +0900

    refact: AddDelemeter 메소드 캡슐화

--- src/main/java/calculator/calculate/ParsingString.java
+++ src/main/java/calculator/calculate/ParsingString.java
@@ -6,62 +6,72 @@ import calculator.exception.Exception;
 import java.util.ArrayList;
 
 public class ParsingString {
-
     private ArrayList<Character> delemeter;
-    String check_slash = "";
-    String check_line = "";
-    char adding_delemeter;
-    ArrayList<Integer> numlist = new ArrayList<Integer>();
-    String input;
-    String parsingInput;
+    private String check_slash = "";
+    private String check_line = "";
+    private char adding_delemeter;
+    private ArrayList<Integer> numlist = new ArrayList<Integer>();
+    private String input;
+    private String parsingInput;
 
-    public void AddDelemeter(){
-        if(LenCheck()) return ;
+    public void AddDelemeter() {
+        if (LenCheck()) return;
         ParsingPattern();
-        if(check_slash.equals("//") && check_line.equals("\\n")){
+        if (check_slash.equals("//") && check_line.equals("\\n")) {
             Exception.IsDelemeterChar(input.charAt(2));
             adding_delemeter = input.charAt(2);
             delemeter.add(adding_delemeter);
             GlobalConstant.StartIndex = 5;
         }
     }
+
     public void ParsingPattern(){
         check_slash = input.substring(0,2);
         check_line = input.substring(3,5);
     }
+
     public boolean LenCheck(){
         if(input.length() < 5) return true;
         else return false;
     }
-    public ArrayList<Integer> MakeNumList(){
-        parsingInput = input.substring(GlobalConstant.StartIndex,input.length());
-        String tmp = ""; // 임시로 숫자를 저장할 문자열
+
+    public ArrayList<Integer> MakeNumList() {
+        parsingInput = input.substring(GlobalConstant.StartIndex);
+        String tmp = "";
         for (int i = 0; i < parsingInput.length(); i++) {
             char currentChar = parsingInput.charAt(i);
-            // 구분자 배열을 확인하여 현재 문자가 구분자인지 체크
-            boolean isDelimiter = false;
-            for (char delimeter : delemeter) {
-                if (currentChar == delimeter) {
-                    isDelimiter = true;
-                    break;
-                }
-            }
-            if (isDelimiter) {
-                int num = Exception.IsPositiveNum(tmp);
-                numlist.add(num);
+
+            if (isDelimiter(currentChar)) {
+                addNumToList(tmp);
                 tmp = "";
             } else {
                 tmp += currentChar;
             }
         }
-        int num = Exception.IsPositiveNum(tmp);
+        addNumToList(tmp);
         Exception.IsArrayNotEmpty(numlist);
-        numlist.add(num);
         return numlist;
+    }
+
+    private boolean isDelimiter(char currentChar) {
+        for (char delimiter : delemeter) {
+            if (currentChar == delimiter) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void addNumToList(String tmp) {
+        if (!tmp.isEmpty()) {
+            int num = Exception.IsPositiveNum(tmp);
+            numlist.add(num);
+        }
     }
 
     public ParsingString(String input){
         this.input = input;
+        this.delemeter = new ArrayList<>();
         delemeter.add(',');
         delemeter.add(':');
     }
