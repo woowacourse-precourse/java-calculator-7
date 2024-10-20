da744a40e090e71a0807c4456f4b5d8b84e02c17
Chan Mi <rkdcksal02@gmail.com>
2024-10-20 23:40:13 +0900

    fix(controller): 입력 검증 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,6 +1,7 @@
 package calculator.controller;
 
 import calculator.model.Calculator;
+import calculator.model.InputValidator;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
@@ -17,6 +18,8 @@ public class CalculatorController {
     public void start() {
         String input = inputView.getInput();
         try {
+            InputValidator validator = new InputValidator();
+            validator.validate(input);
             int result = calculator.add(input);
             outputView.displayResult(result);
         } catch (Exception e) {
