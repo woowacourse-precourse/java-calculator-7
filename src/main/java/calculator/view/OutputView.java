31d29af944a0995b3f5176998344b2e874281d29
byegu <rkdqudrn07@naver.com>
2024-10-20 20:17:37 +0900

    feat : 문자열 덧셈 및 결과 출력 기능 구현

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+    private static final String CALCULATE_RESULT_MESSAGE = "결과 : ";
+
+    public void printResult(Integer result) {
+        System.out.println(CALCULATE_RESULT_MESSAGE + result);
+    }
+}
