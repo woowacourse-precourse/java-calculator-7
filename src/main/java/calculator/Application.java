e78c1b1be33300b7ad6c3a6bcd82706f6f90970b
soonhong99 <nanayah99@naver.com>
2024-10-20 13:18:41 +0900

    refactor(Application): 요구사항에 맞는 에러 throw
    - 기존 println(e.getMessage())에서 error를 직접 throw하도록 코드 개선
    
    Signed-off-by: soonhong99 <nanayah99@naver.com>

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,11 +8,6 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = Console.readLine();
         StringCalculator sc = new StringCalculator(userInput);
-
-        try {
-            sc.calculate();
-        } catch (Exception e) {
-            System.err.println("예기치 못한 오류: " + e.getMessage());
-        }
+        sc.calculate();
     }
 }
