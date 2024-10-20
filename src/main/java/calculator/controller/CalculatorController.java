4363f5ff3c542770cc371496d737a86a25b605a6
EgPwl <egpwl07@gmail.com>
2024-10-20 13:53:39 +0900

    feat: 문자열 입력

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,11 @@
+package calculator.controller;
+
+import calculator.view.InputView;
+
+public class CalculatorController {
+
+    public void run() {
+        String input = InputView.getString();
+    }
+
+}
