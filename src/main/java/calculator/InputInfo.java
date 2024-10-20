e43db38f6d3158cf6f36fe54734067379f15a82b
eusan <goodsan0330@gamil.com>
2024-10-20 14:48:05 +0900

    fix: throw exception when input string ends with a delimiter

--- src/main/java/calculator/InputInfo.java
+++ src/main/java/calculator/InputInfo.java
@@ -31,11 +31,17 @@ public class InputInfo {
     private void validateNumbers(String str) throws IllegalArgumentException {
         /*
          * [예외 발생]
+         * 0인 경우는 예외가 아닌 0을 출력하도록 반환 (예외의 예외)
+         * 구분자로 문자열이 끝나는 경우
          * 알맞은 구분자와 숫자 이외의 문자가 들어온 경우
          * 양의 정수가 아닌 수가 들어오는 경우
          */
 
         if(str.length() == 0) return;
+
+        if(str.charAt(str.length()-1) < '0' || '9' < str.charAt(str.length()-1)) {
+            throw new IllegalArgumentException("구분자로 끝날 수 없습니다.");
+        }
 
         if(!str.matches("[0-9" + separator + "]+")) {
             throw new IllegalArgumentException("구분자와 숫자만 입력할 수 있습니다.");
@@ -44,7 +50,7 @@ public class InputInfo {
         String[] numbers = str.split("[" + separator + "]");
 
         for(String number : numbers) {
-            if (!number.isEmpty()) {
+            if (!number.equals("")) {
                 int num = Integer.parseInt(number);
                 if (num <= 0) throw new IllegalArgumentException("양의 정수만 가능합니다.");
             } else {
