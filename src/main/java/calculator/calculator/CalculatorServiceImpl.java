0b00bd1f1f227b77c543f9cb2c2cb1ad49fb436e
longrunpc <pcjmo4050@gmail.com>
2024-10-21 02:13:46 +0900

    feat(calculate): calculateSum 메서드 오버라이딩 및 구현
    CalculatorService 인터페이스의 calculateSum 메서드를 오버라이딩하여 구현한다.
    주어진 정수 배열의 합을 계산하는 로직이다.

--- src/main/java/calculator/calculator/CalculatorServiceImpl.java
+++ src/main/java/calculator/calculator/CalculatorServiceImpl.java
@@ -5,9 +5,20 @@ package calculator.calculator;
  * 숫자 배열의 합계를 계산하고, 문자열 배열을 정수 배열로 변환하는 기능을 제공합니다.
  */
 public class CalculatorServiceImpl implements CalculatorService {
+
+    /**
+     * 주어진 숫자 배열의 합을 계산합니다.
+     *
+     * @param numbers 정수 배열
+     * @return 배열의 모든 숫자의 합
+     */
     @Override
     public int calculateSum(int[] numbers) {
-        return 0;
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 
     @Override
