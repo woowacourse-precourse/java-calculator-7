3acf9abff3a48dc3e3cf9de8987f79a08d347180
yyysolhhh <solbaram37@gmail.com>
2024-10-21 02:38:56 +0900

    refactor(Validator): 기본 구분자 검사도 정규표현식으로 수정
    validateDefaultInput
    - 반복문을 한문자씩 검사하던 방식에서 정규표현식으로 바꿈

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -4,16 +4,20 @@ public class Validator {
     private static final String ERROR_MESSAGE = "잘못된 문자 포함됨";
 
     public void validateDefaultInput(String input) {
-        for (char c : input.toCharArray()) {
-            if (isInvalidCharacter(c)) {
-                throw new IllegalArgumentException(ERROR_MESSAGE);
-            }
+//        for (char c : input.toCharArray()) {
+//            if (isInvalidCharacter(c)) {
+//                throw new IllegalArgumentException(ERROR_MESSAGE);
+//            }
+//        }
+        String regex = "^[0-9:,]*$";
+        if (!input.matches(regex)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
         }
     }
 
-    private boolean isInvalidCharacter(char c) {
-        return !Character.isDigit(c) && c != ':' && c != ','; // TODO
-    }
+//    private boolean isInvalidCharacter(char c) {
+//        return !Character.isDigit(c) && c != ':' && c != ','; // TODO
+//    }
 
     public void validateCustomInput(String input, String customDelimiter) {
         String regex = "^([0-9:,]+|(" + customDelimiter + "))*$";
