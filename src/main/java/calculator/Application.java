3f23eb0029280da6e1faef9d2a6c38af28082ed4
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 04:00:56 +0900

    refactor(Application): CalculatorController 생성을 위한 메서드 분리 및 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import calculator.controller.CalculatorController;
 import calculator.model.CustomStringParser;
 import calculator.model.CustomStringParserImpl;
 import calculator.model.DelimiterManagerImpl;
+import calculator.model.NumberStringSplitter;
 import calculator.model.NumberStringSplitterImpl;
 import calculator.view.CalculatorInputView;
 import calculator.view.CalculatorOutputView;
@@ -15,11 +16,20 @@ public class Application {
         delimiterManagerImpl.init();
 
         CustomStringParser customStringParser = new CustomStringParserImpl(delimiterManagerImpl);
-        CalculatorController calculatorController = new CalculatorController(customStringParser,
-                new NumberStringSplitterImpl(delimiterManagerImpl), new CalculatorOutputView());
+        CalculatorController calculatorController = getCalculatorController(delimiterManagerImpl, customStringParser);
         CalculatorInputView calculatorInputView = new CalculatorInputView();
 
         String inputStr = calculatorInputView.inputExpression();
         calculatorController.processInput(inputStr);
+    }
+
+    private static CalculatorController getCalculatorController(DelimiterManagerImpl delimiterManagerImpl,
+                                                                CustomStringParser customStringParser) {
+        NumberStringSplitter numberStringSplitter = new NumberStringSplitterImpl(delimiterManagerImpl);
+        CalculatorOutputView calculatorOutputView = new CalculatorOutputView();
+
+        return new CalculatorController(
+                customStringParser, numberStringSplitter, calculatorOutputView
+        );
     }
 }
