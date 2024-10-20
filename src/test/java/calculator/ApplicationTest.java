d64ae381d5e8cfa0d3766054ec5296e51e433e01
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-10-20 13:48:55 +0900

    test : 구분자 마이너스 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -26,13 +26,30 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 구분자_마이너스_사용() {
+        assertSimpleTest(() -> {
+            run("//-\\n1-2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 구분자_마이너스_사용2() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//-\\n1--2"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+    @Test
     void 음수_입력_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
-    
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
