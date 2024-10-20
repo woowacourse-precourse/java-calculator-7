e24db1e0e265831505cb7d241631f00d8d6cadae
ppparkta <pppppark2@gmail.com>
2024-10-20 17:36:02 +0900

    test(ApplicationTest): 테스트케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,10 +17,42 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 여러개_커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//hi\\n//ho\\n1hi2ho3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 양의_정수가_아닌_피연산자_사용_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0.1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀구분자_안의_커스텀구분자_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//hi//ho\\n\\n1hi2ho3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자가_아닌_값_사용_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
