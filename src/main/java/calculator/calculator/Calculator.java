7f0be47ac2b082a3ee6c9731db4cd420cfe443a0
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-10-21 00:55:07 +0900

    feat: 사용자 입력값을 Input 클래스로 받고 입력값 검사 로직 구현

--- src/main/java/calculator/calculator/Calculator.java
+++ src/main/java/calculator/calculator/Calculator.java
@@ -1,16 +1,27 @@
 package calculator.calculator;
 
+import calculator.exception.ValidateInput;
 import calculator.io.InputString;
 
 
 public class Calculator {
-    private InputString inputString;
+    private final InputString inputString;
+    private final ValidateInput validateInput;
 
     public Calculator() {
         this.inputString = new InputString();
+        this.validateInput = new ValidateInput();
     }
 
+    //계산기 로직
     public void playCalculator() {
-        String input = inputString.input();
+        String inputStr = inputString.input();
+        Input input = new Input(inputStr);
+
+        if (inputStr.startsWith("//") || inputStr.startsWith("\\n")) { //커스텀 구분자 있을 경우 - "//" 또는 "\n"로 시작
+            validateInput.validateCustomDelimiter(input);
+        } else { //커스텀 구분자 없을 경우
+            validateInput.validateDefaultDelimiter(input);
+        }
     }
 }
