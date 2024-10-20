97ed4b8e5c5820a185db859b5302cad28c8859c6
HyeonJeong <wgwjh05169@gmail.com>
2024-10-21 00:15:22 +0900

    feat: 애플리케이션에서 기능 호출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,9 +3,22 @@ package calculator;
 import java.util.List;
 
 public class Application {
+
+    private static final String guideMessage = "계산할 값을 입력하세요.";
+
     public static void main(String[] args) {
 
         Splitter splitter = new Splitter();
 
+        Io.print(guideMessage);
+        String input = Io.input();
+
+        String target = splitter.splitCustomSeparators(input);
+        List<String> tokens = splitter.split(target);
+        List<Integer> numbers = Converter.convertStringToInteger(tokens);
+
+        long result = Calculator.sum(numbers);
+
+        Io.print(Long.toString(result));
     }
 }
