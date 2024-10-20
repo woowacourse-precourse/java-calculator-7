92fa916b94dd094cf2805e28ca7957dd43717d23
023-dev <0_2_3@naver.com>
2024-10-21 08:35:52 +0900

    test(ApplicationTest.class): 기본 구분자와 커스텀 구분자 기능 및 예외 처리 테스트 코드 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,22 +8,6 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
-    
-    @Test
-    void 커스텀_구분자_사용() {
-        assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
-        });
-    }
-
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("-1,2,3"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
 
     @Test
     void 빈_문자열을_입력하면_0을_반환() {
@@ -35,6 +19,93 @@ class ApplicationTest extends NsTest {
             //then
             assertThat(output()).contains("결과 : 0");
         });
+    }
+
+    @Test
+    void 기본_구분자_콜론_사용() {
+        assertSimpleTest(() -> {
+            //given
+            String input = "1:2:3";
+            //when
+            run(input);
+            //then
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 기본_구분자_쉼표_사용() {
+        assertSimpleTest(() -> {
+            //given
+            String input = "1,2,3";
+            //when
+            run(input);
+            //then
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            //given
+            String input = "//;\\n1;2;3";
+            //when
+            run(input);
+            //then
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_기본_구분자_결합_사용() {
+        assertSimpleTest(() -> {
+            //given
+            String input = "//;\\n1;2,3:4";
+            //when
+            run(input);
+            //then
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 잘못된_구분자_입력시_예외_처리() {
+        assertThatThrownBy(() -> {
+            //given
+            Calculator calculator = new Calculator();
+            String input = "//;\n1;2,a";
+            //when
+            calculator.add(input);
+            //then
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 커스텀 구분자 형식입니다");
+    }
+
+    @Test
+    void 문자_입력_예외_처리() {
+        assertThatThrownBy(() -> {
+            //given
+            Calculator calculator = new Calculator();
+            String input = "//;\\n1;2,a";
+            //when
+            calculator.add(input);
+            //then
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자가 아닌 값이 포함되어 있습니다");
+    }
+
+    @Test
+    void 음수_입력_예외_처리() {
+        assertThatThrownBy(() -> {
+            //given
+            Calculator calculator = new Calculator();
+            String input = "//;\\n1;2,-3";
+            //when
+            calculator.add(input);
+            //then
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력값이 음수입니다");
     }
 
     @Override
