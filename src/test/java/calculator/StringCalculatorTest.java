1b55085c96e46925d0fabd7ce26cb742c39109ea
이희수 <hslee9234@naver.com>
2024-10-20 21:16:23 +0900

    test: 문자열 덧셈 테스트 코드 구현

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,83 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+    private final StringCalculator calculator = new StringCalculator();
+
+    @Test
+    void 문자열_덧셈_기본() {
+        List<String> numbers = Arrays.asList("1", "2", "3");
+
+        // 예상 결과
+        String expected = "6";
+
+        // 실제 결과
+        String result = calculator.add(numbers);
+
+        // 예상 결과와 실제 결과 비교
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void 문자열_덧셈_큰_숫자() {
+        List<String> numbers = Arrays.asList("123456789876543212345678987654321", "123456789876543212345678987654321",
+                "123456789876543212345678987654321");
+
+        // 예상 결과
+        String expected = "370370369629629637037036962962963";
+
+        // 실제 결과
+        String result = calculator.add(numbers);
+
+        // 예상 결과와 실제 결과 비교
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void 문자열_덧셈_올림수() {
+        List<String> numbers = Arrays.asList("1", "999");
+
+        // 예상 결과
+        String expected = "1000";
+
+        // 실제 결과
+        String result = calculator.add(numbers);
+
+        // 예상 결과와 실제 결과 비교
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void 문자열_덧셈_빈_문자열() {
+        List<String> numbers = Arrays.asList("", "1");
+
+        // 예상 결과
+        String expected = "1";
+
+        // 실제 결과
+        String result = calculator.add(numbers);
+
+        // 예상 결과와 실제 결과 비교
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void 문자열_덧셈_0() {
+        List<String> numbers = Arrays.asList("0", "1", "0");
+
+        // 예상 결과
+        String expected = "1";
+
+        // 실제 결과
+        String result = calculator.add(numbers);
+
+        // 예상 결과와 실제 결과 비교
+        assertEquals(expected, result);
+    }
+}
