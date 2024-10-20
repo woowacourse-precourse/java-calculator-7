f6bcd483877962ecf28047a94c0781be1e29ae80
gyongcode <gyongcode@gmail.com>
2024-10-20 21:55:05 +0900

    컨트롤러, 서비스 리팩토링

--- src/main/java/calculator/service/Service.java
+++ src/main/java/calculator/service/Service.java
@@ -2,37 +2,25 @@ package calculator.service;
 
 import calculator.model.Numbers;
 import calculator.model.Separator;
+import calculator.validation.Validator;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Service {
-    Numbers numbers;
-    Separator separator;
-    public void saveNumbersSeparator(String input){
-        separator = new Separator(",:");
-        numbers = new Numbers();
 
-        if(input.startsWith("//")){
-            int end = input.indexOf("\\n");
-            String newSeparator = input.substring(2, end);
-            separator.addSeparator(newSeparator);
-            input = input.substring(end+2);
-            System.out.println(input);
-        }
-
-        String[] s = input.split(separator.getSeparators());
-        for (String i : s){
-            int n = Integer.parseInt(i);
-            numbers.add(n);
-        }
+    public Separator setSeparator(String input){
+        return new Separator(input);
     }
 
-    public int getSum(){
-        int sum = 0;
-        ArrayList<Integer> nums = numbers.getNumbers();
+    public void addSeparator(Separator separator, String input){
+        separator.addSeparator(input);
+    }
+
+    public Numbers setNumbers(ArrayList<Integer> nums){
+        Numbers numbers = new Numbers();
         for (int i : nums){
-            sum+=i;
+            numbers.add(i);
         }
-        return sum;
+        return numbers;
     }
 }
--- src/main/java/calculator/validation/Validator.java
+++ src/main/java/calculator/validation/Validator.java
@@ -1,19 +1,20 @@
 package calculator.validation;
 
 public class Validator {
-    private final static String EXCEOPTION_MESSAGE = "잘못된 입력입니다.";
+    private final static String EXCEPTION_MESSAGE = "잘못된 입력입니다.";
+
 
     public static void validateSeparator(String separator){
         for (int i = 0; i < separator.length(); i++) {
             if(Character.isDigit(separator.charAt(i)))
-                throw new IllegalArgumentException(EXCEOPTION_MESSAGE);
+                throw new IllegalArgumentException(EXCEPTION_MESSAGE);
         }
     }
 
-    public static void validateNumbers(String numbers){
+    public static void validateDigit(String numbers){
         for (int i = 0; i < numbers.length(); i++) {
             if(!Character.isDigit(numbers.charAt(i)))
-                throw new IllegalArgumentException(EXCEOPTION_MESSAGE);
+                throw new IllegalArgumentException(EXCEPTION_MESSAGE);
         }
     }
 }
