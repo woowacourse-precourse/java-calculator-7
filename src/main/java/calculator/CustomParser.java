0753cb771a0f845e52e06e7b83238dd4fe68c299
Gyulguma <ybg653924@gmail.com>
2024-10-21 00:39:09 +0900

    fix(CustomParser): 특수문자 사용 시 버그 수정
    커스텀 구분자로 특수문자를 사용할 시, 구분자를 인식하지 못하고
    0을 반환하는 버그를 processSeparator 메소드를 통해 수정

--- src/main/java/calculator/CustomParser.java
+++ src/main/java/calculator/CustomParser.java
@@ -10,13 +10,13 @@ public class CustomParser {
     }
 
     public int[] parse(String input) {
-        String regx = "([0-9][,:])+[0-9]";
+        String regx = "([0-9]+[" + this.BASIC_SEPARATOR + "])*[0-9]*";
         if (input.matches(regx)) { // 기본 구분자를 사용할 경우
             return parse(input, this.BASIC_SEPARATOR);
         }
 
         if (input.matches("//.\\\\n.*")) { // 커스텀 구분자를 사용할 경우
-            String separator = input.charAt(2) + "";
+            String separator = processSeparator(input.charAt(2) + "");
             String temp = input.substring(5);
             return parse(temp, this.BASIC_SEPARATOR + "|" + separator);
         }
@@ -32,7 +32,7 @@ public class CustomParser {
         int[] result;
         try {
             result = Arrays.stream(input.split(separator)).mapToInt(Integer::parseInt).toArray();
-        } catch (Exception e) {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력 형식을 확인해주세요. (문자 x, 구분자는 숫자 사이에만 존재할 수 있습니다)");
         }
 
@@ -45,4 +45,13 @@ public class CustomParser {
         return result;
     }
 
+    // 특수문자를 커스텀 구분자로 사용하는 경우, 이스케이프 처리를 해줘야 함
+    // 검사 대상 특수문자들: . * + ? ^ $ { } [ ] ( ) | \ /
+    private String processSeparator(String separator) {
+        String symbol = ".*+?^${}[]()|\\/";
+        if (symbol.contains(separator)) {
+            return "\\" + separator;
+        }
+        return separator;
+    }
 }
