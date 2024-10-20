595b2338f7fd8a6dcaa8d33ef1be7f86d88990f3
heejjinkim <hjhj12@dgu.ac.kr>
2024-10-21 01:30:44 +0900

    feat: 입력값 유효성 검사
    정규식 사용

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,10 +8,32 @@ public class Application {
 
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
+        boolean isCustomDelimiter = false;
 
         if (isCustomDelimiterDefined(input)) {
             delimiters = getCustomDelimiter(input);
+            isCustomDelimiter = true;
         }
+
+        if (!isValidInput(input, delimiters, isCustomDelimiter)) {
+            throw new IllegalArgumentException("입력값이 유효하지 않습니다.");
+        }
+    }
+
+    private static boolean isValidInput(String input, String delimiters, boolean isCustomDelimiter) {
+        if (input.isBlank()) {
+            return true;
+        }
+
+        String validTarget = input;
+
+        if (isCustomDelimiter) {
+            validTarget = input.substring(input.indexOf("\n") + 1);
+        }
+
+        String regex = "^\\d+(" + "[" + delimiters + "]" + "\\d+)*$";
+
+        return validTarget.matches(regex);
     }
 
     private static boolean isCustomDelimiterDefined(String input) {
