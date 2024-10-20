0bf6781d5ce7bca902c7ffea687c9d5c34ed29c1
princebyun <princebyun@gmail.com>
2024-10-20 19:34:46 +0900

    줄정리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,8 +8,8 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInputText = readLine(); //구현 요구 사항중 해당 라이브러리만을 사용하여 구현해야 하는 항목 충족
         try {
-           InputValidation.validateInput(userInputText);
-            String[] textValues =  SplitText.textSplit(userInputText);
+            InputValidation.validateInput(userInputText);
+            String[] textValues = SplitText.textSplit(userInputText);
             int[] sumValues = ValueCheck.checkValue(textValues);
             int result = ValueSum.sumValue(sumValues);
             System.out.println("결과 : " + result);
