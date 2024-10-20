84c99ecdfcedd07d448d77a887b60f72dc7fd7f9
yumi <yumkoori@gmail.com>
2024-10-20 21:22:30 +0900

    refactor: CalculatorFactory의 가독성을 높이기 위해서 로직 분리
    가독성을 높이기 위한splitStringToNumbers메서드 수정
    - 이 메서드에서는 문자열에 포함된 식별자만 구분해서 분기에 따라 적절한 메서드만 호출하도록 로직 간소화
    - 기존 로직에서 splitWithCustomSegment및 splitWithDefaultSegment 메서드를 생성해 분리
    
    수정 되기전 splitStirngToNumber메서드에서는 구분자 식별의 역할 뿐만 아니라, 처리의 책임도 가지고 있어서 가독성이 떨어졌는데,
    로직 분리로 인해 가독성이 높아졌고, 재사용성과 중복코드도 제거 효과

--- src/main/java/calculator/CalculatorFactory.java
+++ src/main/java/calculator/CalculatorFactory.java
@@ -5,24 +5,30 @@ public class CalculatorFactory {
     private static final String CUSTOM_FORMAT_END = "\\n";
 
     public int[] splitStringToNumbers(String str) {
-        //입력된 문자열이 커스텀 지정 구분자일경우
         if (str.startsWith(CUSTOM_FORMAT_START)) {
-            int customEndIndex = str.indexOf(CUSTOM_FORMAT_END);    //커스텀 지정 형식이 끝나는 인덱스 추출
-            String customSegment = str.substring(2, customEndIndex);  //지정 커스텀 문자 추출
-            String removedCustomFormat = str.substring(customEndIndex + 2);    //커스텀 형식을 제외한 문자열 추출
-            String[] splitStr = removedCustomFormat.split(customSegment);  //커스텀 문자를 기준으로 문자열 분리
-            return mapStringToNumbers(splitStr); //분리된 문자열 배열을 숫자 배열로 매핑
+            return splitWithCustomSegment(str);
 
-        } else if (str.contains(",") || str.contains(":")) {     //입력된 문자열에 기본 구분자가 포함될 경우
-            String[] defaultSplit = str.split(",|:");   //기본 구분자로 문자 분리
-            return mapStringToNumbers(defaultSplit);
+        } else if (str.contains(",") || str.contains(":")) {
+            return splitWithDefaultSegment(str);
 
-        } else { //구분자가 포함되어 있지 않을 경우
+        } else {
             if (str.isEmpty()) {
                 return new int[0];
             }
             return null;
         }
+    }
+
+    private int[] splitWithCustomSegment(String str) {
+        int customEndIndex = str.indexOf(CUSTOM_FORMAT_END);    //커스텀 지정 형식이 끝나는 인덱스 추출
+        String customSegment = str.substring(2, customEndIndex);  //지정 커스텀 문자 추출
+        String[] splitStr = str.substring(customEndIndex + 2).split(customSegment);    //커스텀 형식을 제외한 문자열 분리
+        return mapStringToNumbers(splitStr);
+    }
+
+    private int[] splitWithDefaultSegment(String str) {
+        String[] defaultSplit = str.split(",|:");   //기본 구분자로 문자 분리
+        return mapStringToNumbers(defaultSplit);
     }
 
     //구분자를 기준으로 추출된 문자열 배열을 숫자 배열로 매핑 해주는 함수
