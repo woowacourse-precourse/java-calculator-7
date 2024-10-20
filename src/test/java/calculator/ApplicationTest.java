61154a28e80a63443d67fbd91587bab41e6791e7
wgrgwg <kyoeng14697@naver.com>
2024-10-20 15:50:28 +0900

    test: add test to ensure validation works properly

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,10 +17,26 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 구분자만_입력() {
+        assertSimpleTest(() -> {
+            run("; ;;,");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_잘못된_형식_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//@1@2@3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
