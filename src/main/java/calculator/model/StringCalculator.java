638e291bdd1c200c4ec93e2b737895f109b99eda
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:43:41 +0900

    :sparkles: feat(StringCalculator): 문자열을 파싱하고 숫자 리스트를 생성

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -1,10 +1,16 @@
 package calculator.model;
 
 public class StringCalculator {
+    private final InputParser inputParser;
+
+    public StringCalculator() {
+        this.inputParser = new InputParser();
+    }
 
     public int add(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
+        String[] numbers = inputParser.parse(input);
     }
 }
