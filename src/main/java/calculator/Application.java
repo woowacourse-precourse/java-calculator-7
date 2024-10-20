e309434730b5f6edf9f462682b74b0c2c4aefa5b
Eclieee <100896@naver.com>
2024-10-21 00:36:19 +0900

    feat(input) : 사용자로부터 입력받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Input.readLine();
+        
     }
 }
---
+++ src/main/java/calculator/Input.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Input {
+
+    public static String readLine() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+}
