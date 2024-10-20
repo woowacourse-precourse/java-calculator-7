0f38aebd45105bf86887aa3079d0b256a74f8307
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-20 20:46:15 +0900

    test: 숫자 유틸리티 테스트 작성
    - 문자열 리스트를 숫자로 변환(parseNumbers 메서드).
    - 숫자 리스트의 합계를 계산(sum 메서드).

---
+++ src/test/java/calculator/util/number/NumberUtilsTest.java
@@ -0,0 +1,58 @@
+package calculator.util.number;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("NumberUtils 클래스")
+public class NumberUtilsTest {
+
+    static Stream<Arguments> provideIntegerConvertInput() {
+        return Stream.of(
+                Arguments.of(List.of("-1", "2", "3"), List.of(-1, 2, 3), Integer.class),
+                Arguments.of(List.of("1", "2", "-3", "4"), List.of(1, 2, -3, 4), Integer.class),
+                Arguments.of(List.of("1", "2", "3", "4", "5"), List.of(1, 2, 3, 4, 5), Integer.class)
+        );
+    }
+
+    static Stream<Arguments> provideIntegerSumInput() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3), 6, Integer.class),
+                Arguments.of(List.of(1, 2, 3, 4), 10, Integer.class),
+                Arguments.of(List.of(1, 2, 3, 4, 5), 15, Integer.class),
+                Arguments.of(List.of(1, 2, 3, 4, 5, -6), 9, Integer.class)
+        );
+    }
+
+    @ParameterizedTest(name = "문자열 리스트: {0}, 기대값: {1}")
+    @MethodSource({
+            "provideIntegerConvertInput",
+    })
+    void 문자열_리스트를_숫자로_변환한다(List<String> input, List<Number> expected, Class<? extends Number> numberClass) {
+
+        // when
+        List<? extends Number> actual = NumberUtils.parseNumbers(input, numberClass);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest(name = "리스트: {0}, 기대값: {1}")
+    @MethodSource({
+            "provideIntegerSumInput"
+    })
+    void 숫자_리스트의_합계를_계산한다(List<Number> values, int expected, Class<? extends Number> numberClass) {
+
+        // when
+        Number actual = NumberUtils.sum(values, numberClass);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
