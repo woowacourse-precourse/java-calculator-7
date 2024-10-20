7ca7dbc781b27e3c0fccbf3c62184e7a524aa293
kjyyjk <rlawnsdud920@gmail.com>
2024-10-20 17:37:08 +0900

    test: DelimitersTest 테스트케이스 추가
    - ParameterizedTest 사용
    - 정규표현식 변환 테스트 시 커스텀문자로 영문자, 공백문자, 숫자문자, 특수문자 테스트

--- src/test/java/calculator/domain/DelimitersTest.java
+++ src/test/java/calculator/domain/DelimitersTest.java
@@ -2,21 +2,37 @@ package calculator.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 class DelimitersTest {
 
-    @Test
-    @DisplayName("Delimiters는_기본_구분자와_커스텀_구분자를_모두_고려하여_정규표현식을_반환한다")
-    public void convertToRegex() {
+    @ParameterizedTest
+    @DisplayName("Delimiters는_커스텀_구분자를_고려하여_정규표현식을_반환할_수_있다")
+    @CsvSource(value = {"t/:|,|t", "1/:|,|1", ";/:|,|;"}, delimiter = '/')
+    public void convertToRegex(String input, String expected) {
         //given
-        CustomDelimiter customDelimiter = new CustomDelimiter("t");
+        CustomDelimiter customDelimiter = new CustomDelimiter(input);
 
         //when
         String result = new Delimiters(customDelimiter).convertToRegex();
 
         //then
-        assertThat(result).isEqualTo(":|,|t");
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Delimiters는_공백문자인_커스텀_구분자를_고려하여_정규표현식을_반환할_수_있다")
+    public void convertToRegex_CanConsiderBlankDelimiter() {
+        //given
+        CustomDelimiter customDelimiter = new CustomDelimiter(" ");
+
+        //when
+        String result = new Delimiters(customDelimiter).convertToRegex();
+
+        //then
+        assertThat(result).isEqualTo(":|,| ");
     }
 }
