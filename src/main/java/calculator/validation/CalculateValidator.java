457603f21c5ef4294cdee196087197a0804bd340
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-20 21:24:54 +0900

    refactor: 계산 유효성 검증 로직 분리

--- src/main/java/calculator/validation/CalculateValidator.java
+++ src/main/java/calculator/validation/CalculateValidator.java
@@ -10,16 +10,16 @@ public class CalculateValidator {
     }
 
     // 문자열 정수로 변환하고, 양수인지 검증
-    public int parseToInt(String number) {
+    public static int parseToInt(String number) {
         return Optional.of(number)
                 .map(String::trim)
-                .map(this::validateAndParse)
-                .filter(this::isPositive)
+                .map(CalculateValidator::validateAndParse)
+                .filter(CalculateValidator::isPositive)
                 .orElseThrow(() -> CustomException.from(ErrorMessage.NOT_ALLOWED_NEGATIVE_NUMBER)); // 0 이하일 경우 예외 발생
     }
 
     // 숫자 형식인지 검증하고 정수로 변환
-    private int validateAndParse(String s) {
+    private static int validateAndParse(String s) {
         return Optional.of(s)
                 .filter(str -> ValidationPattern.INTEGER_PATTERN.getPattern().matcher(str).matches()) // 정규식을 사용하여 숫자 형식 검증
                 .map(Integer::parseInt)
@@ -27,7 +27,7 @@ public class CalculateValidator {
     }
 
     // 양수인지 검증
-    private boolean isPositive(int num) {
+    private static boolean isPositive(int num) {
         return num > 0;
     }
 }
