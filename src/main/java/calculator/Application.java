70cb3a142add39385fa954f001e7616b1d69be65
seungheejang <justinholla908@gmail.com>
2024-10-20 20:25:12 +0900

    feat: 실행시 출력문 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,7 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        /** 기능 1 구현 */
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 }
