547a3692543ffb2e141947a5a822bee0f25992ff
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-21 00:46:08 +0900

    refactor: 자원 반납하는 부분을 Reader에 넣을 수 있도록 수정

--- src/main/java/calculator/Reader.java
+++ src/main/java/calculator/Reader.java
@@ -1,9 +1,20 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Reader {
     private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해주세요.";
-    
-    public static void printInputMessage() {
+
+    public static String getInput() {
+        printInputMessage();
+        return Console.readLine();
+    }
+
+    public static void close() {
+        Console.close();
+    }
+
+    private static void printInputMessage() {
         System.out.println(INPUT_MESSAGE);
     }
 }
