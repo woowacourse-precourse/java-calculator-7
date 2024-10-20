ca6d3643a23944f465fb3269c0cc82241fe278db
eb9862 <luclead622@gmail.com>
2024-10-20 18:10:18 +0900

    refactor : 커스텀 구분자에 대한 예외 메시지 상수화

--- src/main/java/calculator/CustomPartManager.java
+++ src/main/java/calculator/CustomPartManager.java
@@ -2,9 +2,12 @@ package calculator;
 
 public class CustomPartManager {
 
+    private static final String ERROR_CUSTOM_LENGTH = "커스텀 구분자의 길이가 1이 아닙니다";
+    private static final String ERROR_CUSTOM_IS_NUMBER = "커스텀 구분자가 숫자입니다";
+
     static boolean validateCustomSeparator(String input) {
         if (input.indexOf("\\n") != 3) {
-            throw new IllegalArgumentException("커스텀 구분자의 길이가 1이 아닙니다");
+            throw new IllegalArgumentException(ERROR_CUSTOM_LENGTH);
         }
         return !isSeparatorNumeric(input.charAt(2));
     }
@@ -16,7 +19,7 @@ public class CustomPartManager {
     static boolean validateCustomPart(String input) {
         if (hasCustomPart(input)) {
             if (!validateCustomSeparator(input)) {
-                throw new IllegalArgumentException("커스텀 구분자가 숫자입니다");
+                throw new IllegalArgumentException(ERROR_CUSTOM_IS_NUMBER);
             }
             return true;
         }
