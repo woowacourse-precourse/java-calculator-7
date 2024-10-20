33aaa43092ded458b1ffdf450d5ab71dcf79b1a2
agi1004 <agi1009@naver.com>
2024-10-20 18:25:53 +0900

    feat: 문자열의 숫자를 구분자로 나누는 인터페이스의 구현체2: 커스텀 구분자 사용
    커스텀 구분자로 숫자들을 분리하여 문자열 배열에 추가
    이후 문자열 배열을 숫자 배열로 변경하여 Input 인터페이스의 구현체로 리턴

--- src/main/java/divide/CustomDivide.java
+++ src/main/java/divide/CustomDivide.java
@@ -1,8 +1,26 @@
 package divide;
 
-public class CustomDivide implements Divide {
+import static separate.Separator.CUSTOM;
+
+public class CustomDivide implements Divide {	// 커스텀 구분자로 분리
+	private static final int START_NUMBER_INDEX = 1;
+	
 	@Override
-	public void divide(String str) {
+	public int[] divideNumbers(final String str) {
+		// 커스텀 구분자를 구별하는 문자 제거 ("//", "\\n")
+		String[] separators = CUSTOM.getSeparators();
+		String cleanedStr = str.replace(separators[0], "").replace(separators[1], "");
 		
+		// 커스텀 구분자 추출
+		String customSeparator = "\\" + cleanedStr.substring(0, START_NUMBER_INDEX);
+		
+		// 숫자 문자열 추출
+		String numberStr = cleanedStr.substring(START_NUMBER_INDEX);
+		
+		// 커스텀 구분자로 숫자 분리
+		String[] strArr = numberStr.split(customSeparator);
+		
+		// 숫자 배열로 변환하여 리턴
+		return toIntArr(strArr);
 	}
 }
