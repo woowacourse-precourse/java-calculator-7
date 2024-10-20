0f79b3f3ce8938918fc4ca26b64bf03906e8170a
NaMinhyeok <nmh9097@gmail.com>
2024-10-20 22:45:03 +0900

    refactor: OutputHandler 출력 형식 변경

--- src/main/java/calculator/io/OutputHandler.java
+++ src/main/java/calculator/io/OutputHandler.java
@@ -3,14 +3,14 @@ package calculator.io;
 public class OutputHandler {
 
     public static final String INITIAL_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
-    public static final String RESULT_MESSAGE = "결과 : ";
+    public static final String RESULT_MESSAGE = "결과 : %d";
 
     public void showInitialMessage() {
         System.out.println(INITIAL_MESSAGE);
     }
 
     public void showResultMessageWith(int result) {
-        System.out.println(RESULT_MESSAGE + result);
+        System.out.printf(RESULT_MESSAGE, result);
     }
 
 }
