e5c175116b8c7590d777f922bb07d762809a7688
yereumi <lovelyyeji01@gmail.com>
2024-10-20 15:15:56 +0900

    feat(Application): add main application

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,11 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        IO io = new IO();
+        Logic logic = new Logic();
+
+        String str = io.input();
+        io.output(logic.calculate(str));
     }
 }
