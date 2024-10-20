cac74f3171ff65c35d5a8e7c01b0701d1a695a6c
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-10-20 23:46:19 +0900

    feat: Implement addition feature using default delimiters

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,36 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> delimiters = new ArrayList<>();
+        delimiters.add(","); // 기본 구분자
+        delimiters.add(":"); // 기본 구분자
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = sc.nextLine();
+
+
+        StringBuilder regexBuilder = new StringBuilder();
+        for (String delimiter : delimiters) {
+            regexBuilder.append(Pattern.quote(delimiter)).append("|"); // 각 구분자를 이스케이프
+        }
+        String delimiterRegex = regexBuilder.substring(0, regexBuilder.length() - 1);
+
+        String[] splitNums = input.split(delimiterRegex);
+
+        int sum = 0;
+        for (String num : splitNums) {
+            int number = Integer.parseInt(num);
+            sum += number;
+        }
+        System.out.printf("결과 : %d%n", sum);
     }
 }
