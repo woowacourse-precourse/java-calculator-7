e13c8eeea662ef717507790810493e465fdea587
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-20 20:37:38 +0900

    test: 애플리케이션 테스트 작성
    - 커스텀 구분자를 사용하는 경우.
    - 커스텀 구분자를 사용하지 않는 경우.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,27 +1,52 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    @Test
-    void 커스텀_구분자_사용() {
+
+    static Stream<Arguments> provideWithCustomDelimiterInput() {
+        return Stream.of(
+                Arguments.of("//;\\n1;2;3", 6),
+                Arguments.of("//.\\n1.2:3", 6),
+                Arguments.of("//;\\n1;2;3;4;5", 15),
+                Arguments.of("//.\\n1.2:3:4:5", 15),
+                Arguments.of("//a\\n", 0)
+        );
+    }
+
+    static Stream<Arguments> provideWithoutCustomDelimiterInput() {
+        return Stream.of(
+                Arguments.of("1,2,3", 6),
+                Arguments.of("1,2:3,4", 10),
+                Arguments.of("1,2:3:4,5", 15),
+                Arguments.of("\n", 0)
+        );
+    }
+
+    @ParameterizedTest(name = "입력: {0}, 기대값: {1}")
+    @MethodSource("provideWithCustomDelimiterInput")
+    void 커스텀_구분자_사용(String input, int expected) {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run(input);
+            assertThat(output()).contains("결과 : " + expected);
         });
     }
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+    @ParameterizedTest(name = "입력: {0}, 기대값: {1}")
+    @MethodSource("provideWithoutCustomDelimiterInput")
+    void 커스텀_구분자_미사용(String input, int expected) {
+        assertSimpleTest(() -> {
+            run(input);
+            assertThat(output()).contains("결과 : " + expected);
+        });
     }
 
     @Override
