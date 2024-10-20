dac4f41d8a85f0dc84a53a2147fd8d14bf659dbd
usingnameing <cnrrn0312@gmail.com>
2024-10-20 17:21:06 +0900

    feat(Validation) : add input string vadlidation

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import calculator.api.Calculation;
+import calculator.api.Validation;
+
+import javax.swing.plaf.SeparatorUI;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String inputString = readLine();
+        try{
+            Validation validation = new Validation();
+            String separator = validation.validateString(inputString);
+
+            Calculation calculation = new Calculation();
+            Double result = calculation.calculationString(separator,inputString);
+            System.out.println("결과 = " + result);
+        }catch (IllegalStateException err){
+            return;
+        }
     }
 }
---
+++ src/main/java/calculator/api/Calculation.java
@@ -0,0 +1,2 @@
+package calculator.api;public class Calculation {
+}
