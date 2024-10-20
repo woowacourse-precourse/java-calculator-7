29e7e24371cb2e0b03dfefa602ec286269a19ffe
moonbinchoi <whsahd12@gmail.com>
2024-10-21 01:35:34 +0900

    refactor(Adder): Delimiter 클래스 의존성 삭제

--- src/main/java/calculator/Adder.java
+++ src/main/java/calculator/Adder.java
@@ -5,24 +5,17 @@ import java.util.List;
 
 public class Adder {
 
-    private final Delimiters delimiters;
     private final List<String> operands;
 
 
     public Adder(List<String> operands) {
-        this(Delimiters.of(), operands);
-    }
-
-    public Adder(Delimiters delimiters, List<String> operands) {
-        Delimiters.check(delimiters);
         Expression.checkOperandsConstraints(operands);
 
-        this.delimiters = delimiters;
         this.operands = new ArrayList<>(operands);
     }
 
     public Adder(Expression expression) {
-        this(expression.getDelimiters(), expression.getOperands());
+        this(expression.getOperands());
     }
 
     public int sum() {
