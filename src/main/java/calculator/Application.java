e595b99b32b0e1bdcf2f61247d105b57bebc0bb2
jhpark1227 <junhyeok0246@naver.com>
2024-10-20 12:18:26 +0900

    feat: 문자열 입력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = readInput();
+    }
+
+    private static String readInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {  //입력이 없는 경우
+            return "";
+        }
     }
 }
