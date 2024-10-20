1a2072857c2350dc65abfda9c77e959f77a3645f
leejongyoon1209 <bidakee11@naver.com>
2024-10-21 00:40:08 +0900

    계산기

--- src/main/java/calculator/Calculate.java
+++ src/main/java/calculator/Calculate.java
@@ -3,7 +3,7 @@ public class Calculate {
 
     public static int sum(String input) {
         if (input == null || input.isEmpty()) {
-            return 0;  // 빈 문자열일 경우 0 반환
+            return 0;
         }
 
         if (input.startsWith("//")) {
