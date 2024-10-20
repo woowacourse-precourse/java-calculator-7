445d00cffd6769f9b23429499bdd9565af1d549f
ratatuwee <pkqwaszx@gmail.com>
2024-10-20 18:31:44 +0900

    test(application): 전체 동작 테스트 및 예외 테스트 추가
    입력값만을 가지고 전체 동작이 정상적으로 이뤄지는지 확인하는 테스트들을 추가하였습니다
    숫자, 소수, 구분자 상황에 따라 테스트를 나누어 추가하였습니다

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -15,12 +15,62 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains("결과 : 1");
         });
     }
+    @Test
+    void 커스텀_구분자_숫자사용() {
+        assertSimpleTest(() -> {
+            run("//7\\n7172");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_미사용() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+    @Test
+    void 커스텀_구분자_미사용_소수계산() {
+        assertSimpleTest(() -> {
+            run("1.1,2.1,3.1");
+            assertThat(output()).contains("결과 : 6.3");
+        });
+    }
+    @Test
+    void 커스텀_구분자_사용_소수계산() {
+        assertSimpleTest(() -> {
+            run("//;\\n1.1;2.1,3.1");
+            assertThat(output()).contains("결과 : 6.3");
+        });
+    }
+    @Test
+    void 커스텀_구분자_숫자사용_소수계산() {
+        assertSimpleTest(() -> {
+            run("//7\\n1.172.1,3.1");
+            assertThat(output()).contains("결과 : 6.3");
+        });
+    }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_소수사용_정수누락시() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(".12,0.1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_소수사용_소수누락시() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("2.,0.1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
