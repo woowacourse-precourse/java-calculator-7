13d180c7e5bbdb8a4d846f96c6c5fa3641f3e4bd
jungjoo <wjddis000507@gmail.com>
2024-10-20 23:38:25 +0900

    feat : 기본/커스텀 구분자 사용 시 합 계산하는 로직 구현

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -3,9 +3,31 @@ package calculator.service;
 public class CalculatorService {
 
     /**
-     * 사용자로부얻어온 String을 계산하는 로직
+     * 사용자로부터 얻어온 String을 계산하는 로직
      */
-    public void calculate(String inputString){
+    public int calculate(String inputString){
+
+        int sum =0;
+
+        String[] cal;
+        String delim = "[,:]";
+
+        //커스텀 구분자 포함시
+        if(inputString.startsWith("//")){
+            int idx = inputString.indexOf("\\n"); // \n인덱스
+            delim = inputString.substring(2, idx); //구분자
+            inputString = inputString.substring(idx + 2); // \n 이후 문자열
+        }
+
+        cal = inputString.split(delim);
+
+        for(String num : cal){
+            sum += Integer.parseInt(num);
+        }
+
+        System.out.println(sum);
+
+        return sum;
 
     }
 }
