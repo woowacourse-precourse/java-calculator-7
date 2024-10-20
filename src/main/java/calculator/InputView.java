da07170604a0733cf0fe11ade5a4c0f09ed155e8
likerhythm <wjdalsdl0412@gmail.com>
2024-10-20 14:39:42 +0900

    fix(InputView): 프로그래밍 요구사항 missionutils.Console의 readLine 메서드로 변경

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -1,14 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.Scanner;
 
 public class InputView {
 
     public String getUserInput() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = scanner.nextLine();
-        scanner.close();
+        String input = Console.readLine();
+        Console.close();
         return input;
     }
 }
