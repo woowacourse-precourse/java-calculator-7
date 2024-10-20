a54659bfc5b178bb5f732caca020d49f47dbd678
Parkjyun <jaeyun0507@naver.com>
2024-10-20 21:40:16 +0900

    refactor(DelimitersTest): Delimiters의 역할 축소에 따른 테스트 리팩토링

--- src/test/java/calculator/model/DelimitersTest.java
+++ src/test/java/calculator/model/DelimitersTest.java
@@ -6,14 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class DelimitersTest {
     @Test
-    @DisplayName("커스텀 구분자를 갖고 있는지 판단할 수 있다.")
-    void hasCustomDelimiter() {
-        String input = "//a\\n1a2a3";
-        Delimiters delimiters = new Delimiters();
-        Assertions.assertThat(delimiters.hasCustomDelimiter(input)).isTrue();
-    }
-
-    @Test
     @DisplayName("커스텀 구분자를 추출할 수 있다.")
     void addCustomDelimiter() {
         String input = "//a\\n1a2a3";
