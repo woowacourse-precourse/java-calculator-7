92455e1b98b1b545660662777178cb546aaa4024
JoonKyo <joonkyolee@gmail.com>
2024-10-20 17:49:49 +0900

    feat(NumberValidator): 음수가 들어있는지 판별하는 메서드 구현

--- src/main/java/calculator/domain/NumberValidator.java
+++ src/main/java/calculator/domain/NumberValidator.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NumberValidator {
+    private static final String ERROR_MESSAGE_FOR_NEGATIVE_NUMBER = "음수를 입력하셨습니다. 프로그램을 종료합니다.";
+    private static final String ERROR_MESSAGE_FOR_ZERO = "0을 입력하셨습니다. 프로그램을 종료합니다.";
 
     public static List<Integer> convertNumbers(String[] separatedString) {
         List<Integer> numbers = new ArrayList<>();
@@ -11,6 +13,18 @@ public class NumberValidator {
         for (String number : separatedString) {
             numbers.add(Integer.parseInt(number));
         }
+        validateNumber(numbers);
         return numbers;
+    }
+
+    private static void validateNumber(List<Integer> numbers) {
+        if (hasNegativeNumber(numbers)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NEGATIVE_NUMBER);
+        }
+    }
+
+    private static boolean hasNegativeNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number < 0);
     }
 }
