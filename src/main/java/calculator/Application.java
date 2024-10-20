e0d971c796aa43808949cb767bbeb6b0263db489
조혜원 <johyewon@johyewon-ui-MacBookAir.local>
2024-10-20 22:33:45 +0900

    refactor: 매직 넘버 상수로 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 
 public class Application {
+    private static final String DEFAULT_SEPERATOR = ",|:";
+    private static final String CUSTOM_SEPERATE_START = "//";
+    private static final String CUSTOM_SEPERATE_END = "\\n";
+
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
@@ -21,23 +25,23 @@ public class Application {
     }
 
     public static boolean isCustomSeperator(String input) {
-        return input.contains("//") && input.contains("\\n");
+        return input.contains(CUSTOM_SEPERATE_START) && input.contains(CUSTOM_SEPERATE_END);
     }
 
     public static String extractCustomSeperator(String input) {
-        int start = input.indexOf("//") + 2; // 커스텀 문자 인덱스 시작
-        int end = input.indexOf("\\n"); // 커스텀 문자 인덱스 끝
+        int start = input.indexOf(CUSTOM_SEPERATE_START) + 2; // 커스텀 문자 인덱스 시작
+        int end = input.indexOf(CUSTOM_SEPERATE_END); // 커스텀 문자 인덱스 끝
 
         return input.substring(start, end);
     }
 
     public static String[] splitBySeperator(String input, String customSeperator) {
-        String seperator = ",|:";
+        String seperator = DEFAULT_SEPERATOR;
         if (input.equals("")) return new String[]{"0"};
 
         if (!customSeperator.equals("")) {
-            input = input.replace("//" + customSeperator + "\\n", "");
-            seperator = ",|:|" + customSeperator;
+            input = input.replace(CUSTOM_SEPERATE_START + customSeperator + CUSTOM_SEPERATE_END, "");
+            seperator = DEFAULT_SEPERATOR + "|" + customSeperator;
         }
         String[] number = input.split(seperator);
 
