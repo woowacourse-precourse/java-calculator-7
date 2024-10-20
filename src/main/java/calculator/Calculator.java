7328f8fdb5c12de9b36f1604b6d19539362b6c99
mirolis6636 <mirolis6636@gmail.com>
2024-10-21 02:09:54 +0900

    feat: add detectDelimiter method

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,26 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class Calculator {
     private String userInput;
+    private String delimiter;
+    private static final Pattern defaultPattern = Pattern.compile("([1-9][0-9]*([,:][1-9][0-9]*)*)?");
+    private static final Pattern customPattern = Pattern.compile("//(.)\\\\n([1-9][0-9]*(\\1[1-9][0-9]*)*)?");
 
     public void readUserInput(){
         userInput = camp.nextstep.edu.missionutils.Console.readLine();
     }
+
+    public void detectDelimiter() throws IllegalArgumentException{
+        boolean isDefaultDelimiter = defaultPattern.matcher(userInput).matches();
+        boolean isCustomDelimiter = customPattern.matcher(userInput).matches();
+        if(isDefaultDelimiter)
+            delimiter = "[,|:]";
+        else if(isCustomDelimiter)
+            delimiter = ""+userInput.charAt(2);
+        else
+            throw new IllegalArgumentException("ERROR: invalid input");
+    }
+
 }
