34bb2eca264ea10284c4c65de5125116007141c1
Gothax <scott990328@gmail.com>
2024-10-20 16:33:29 +0900

    refactor: service 예외처리 코드 분리
    가독성을 위해 함수로 분리

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,6 +1,7 @@
 package calculator.service;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
@@ -15,20 +16,7 @@ public class CalculatorService {
         String[] splittedString = detectSeparator(inputString);
         String separator = splittedString[0];
         return Stream.of(splittedString[1].split(separator))
-                .map(number->{
-                    try {
-                        int parsedInt = Integer.parseInt(number);
-                        if (parsedInt <= 0) {
-                            // 음수인 경우
-                            throw new IllegalArgumentException("입력은 구분자와 양수로 구성된 문자열이어야 합니다.");
-                        }
-                        return parsedInt;
-
-                    } catch (IllegalArgumentException e) {
-                        // 숫자가 아닌 경우
-                        throw new IllegalArgumentException("입력은 구분자와 양수로 구성된 문자열이어야 합니다.");
-                    }
-                })
+                .map(parseAndValidate())
                 .toList();
     }
 
@@ -37,27 +25,50 @@ public class CalculatorService {
     private String[] detectSeparator(String inputString) {
         // 커스텀 구분자가 있는 경우
         if (inputString.startsWith("//")) {
-            if (!inputString.contains("\\n")) {
-                throw new IllegalArgumentException("커스텀 구분자를 사용하려면 \\n을 포함해야 합니다.");
-            }
-            String[] split = inputString.replace("\\n", "\n").split("\n");
-            String sub = split[0].substring(2);
-            if (sub.isEmpty()) {
-                throw new IllegalArgumentException("커스텀 구분자는 공백일수 없습니다.");
-            } else if (sub.equals(",") || sub.equals(":")) {
-                throw new IllegalArgumentException("커스텀 구분자는 , 또는 : 이외의 문자여야 합니다.");
-            }
-            split[0] = Pattern.quote(sub);
-            return split;
+            return getStringAndValidate(inputString);
         }
         // 커스텀 구분자가 없는 경우
         return new String[]{",|:", inputString};
     }
-
 
     private Integer add(List<Integer> numbers) {
         return numbers.stream()
                 .mapToInt(Integer::intValue)
                 .sum();
     }
+
+    private Function<String, Integer> parseAndValidate() {
+        return number -> {
+            try {
+                int parsedInt = Integer.parseInt(number);
+                if (parsedInt <= 0) {
+                    // 음수인 경우
+                    throw new IllegalArgumentException("입력은 구분자와 양수로 구성된 문자열이어야 합니다.");
+                }
+                return parsedInt;
+            } catch (IllegalArgumentException e) {
+                // 숫자가 아닌 경우
+                throw new IllegalArgumentException("입력은 구분자와 양수로 구성된 문자열이어야 합니다.");
+            }
+        };
+    }
+
+    private String[] getStringAndValidate(String inputString) {
+        if (!inputString.contains("\\n")) {
+            throw new IllegalArgumentException("커스텀 구분자를 사용하려면 \\n을 포함해야 합니다.");
+        }
+        String[] split = inputString.replace("\\n", "\n").split("\n");
+        validateSeparator(split[0]);
+        split[0] = Pattern.quote(split[0].substring(2));
+        return split;
+    }
+
+    private void validateSeparator(String separator) {
+        if (separator.substring(2).isEmpty()) {
+            throw new IllegalArgumentException("커스텀 구분자는 공백일수 없습니다."); // 띄어쓰기는 허용
+        } else if (separator.substring(2).equals(",") || separator.substring(2).equals(":")) {
+            throw new IllegalArgumentException("커스텀 구분자는 , 또는 : 이외의 문자여야 합니다.");
+        }
+    }
+
 }
