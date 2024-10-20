55b0437453ecb3cc012e2fe05cf782d9573e32a4
seophohoho <seop0937@gmail.com>
2024-10-21 06:47:56 +0900

    feat(Application): 입력 메시지와 출력 메시지 인터페이스 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,12 +5,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-
         try {
             int result = calculator.calc(input);
-            System.out.println(result);
+            System.out.println("결과 : " + result);
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
         }
