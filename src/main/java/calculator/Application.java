810d77e3f4262260a19f743cb65fc72c2e3d6907
joon <junkyu83@gmail.com>
2024-10-20 21:50:47 +0900

    feat: 문자열 입력, 문자열에서 구분자로 분리한 Array 리턴

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,34 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String userInput = Console.readLine();
+
+        // 입력한 문자열로부터 구분자로 분리한 Array을 구한다.
+        String[] stringArray = getStringArray(userInput);
+
+    }
+
+    private static String[] getStringArray(String userInput) {
+        // 커스텀 구분자를 사용한 경우
+        if (userInput.startsWith("//") && userInput.contains("\\n")) {
+            // 커스텀 구분자를 추출한다.
+            int startIndex = userInput.indexOf("//") + 2;
+            int endIndex = userInput.indexOf("\\n");
+            String customSeparator = userInput.substring(startIndex, endIndex);
+            // 커스텀 구분영역을 뺀 나머지 입력 값을 확인한다.
+            int inputWithSeparatorIndex = userInput.indexOf("\\n") + 2;
+            String inputWithSeparator = userInput.substring(inputWithSeparatorIndex);
+            // 구분자로 입력 값을 분리하여 Array로 만들고 리턴한다.
+            return inputWithSeparator.split(customSeparator);
+        }
+        // 기본 구분자를 사용한 경우
+        else {
+            // 기본 구분자 "," 또는 ":" 로 입력 값을 분리하여 Array 만들고 리턴한다.
+            return userInput.split("[,:]");
+        }
     }
 }
