4434e5dc75aaf0c217b4b6a624c36b9434c27bb9
byegu <rkdqudrn07@naver.com>
2024-10-20 16:54:34 +0900

    커스텀 구분자 기능 구현

--- src/main/java/calculator/model/InputString.java
+++ src/main/java/calculator/model/InputString.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 public class InputString {
     private String userInput;
+    private char customSeparator;
     private List<Integer> userOperand;
 
     public void setUserInput(String userInput) {
-        this.userInput = userInput.replaceAll("[,:]", " ");
+        if(userInput.contains("//") && userInput.contains("\\n")) {
+            customSeparator = userInput.replaceAll("[//\n]", "").charAt(0);
+            String regex = String.format("[,:%s]", customSeparator);
+            this.userInput = userInput.substring(userInput.indexOf("n")+1).replaceAll(regex, " ");
+        } else {
+            this.userInput = userInput.replaceAll("[,:]", " ");
+        }
     }
 
     public String getUserInput() {
