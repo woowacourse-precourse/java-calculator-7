a05d89239105d2c79f3653f9cd20e0d34ad15280
min429 <lsm990429@gmail.com>
2024-10-20 10:49:48 +0900

    feat: 콘솔 입출력 기능을 추가한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	private static String input;
+	private static long result;
+
+	public static void main(String[] args) {
+		System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+		input = readLine();
+
+		solution();
+
+		System.out.println("결과 : " + result);
+	}
 }
