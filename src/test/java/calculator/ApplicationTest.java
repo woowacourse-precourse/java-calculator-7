7c2c87f5566c3f40df4ae2e50b3137ce5f40b596
song hyeongyu <lit3207@ajou.ac.kr>
2024-10-20 12:31:11 +0900

    test add testcases

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,12 +17,41 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 구분자_단독_존재() {
+        assertSimpleTest(() -> {
+            run(",:");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 음수_구분자_존재(){
+        assertSimpleTest(() -> {
+            run("//-\\n-1-2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 구분자_연속_존재() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+
+
 
     @Override
     public void runMain() {
