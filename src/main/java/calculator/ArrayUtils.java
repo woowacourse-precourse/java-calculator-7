75a7230780d9e88aea1ef7544579a3d4a6a029e9
siwol025 <cyk99115695@gmail.com>
2024-10-21 08:51:26 +0900

    feat: 구분된 문자열을 숫자로 변환하는 기능 구현

--- src/main/java/calculator/ArrayUtils.java
+++ src/main/java/calculator/ArrayUtils.java
@@ -6,7 +6,30 @@ public class ArrayUtils {
 
     private void validateNotEmpty(String str) {
         if (str == null || str.isEmpty()) {
-            throw new IllegalArgumentException("더할값은 null이거나 비어 있을 수 없습니다.");
+            throw new IllegalArgumentException("더할 값은 null이거나 비어 있을 수 없습니다.");
         }
+    }
+
+    private void validatePositive(int num) {
+        if (num <= 0) {
+            throw new IllegalArgumentException("더할 값은 양수여야 합니다.");
+        }
+    }
+
+    private int parseStringToInt(String str) {
+        try {
+            validateNotEmpty(str);
+            int num = Integer.parseInt(str);
+            validatePositive(num);
+            return num;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("더할 값은 숫자여야 합니다.");
+        }
+    }
+
+    public int[] convert(String[] inputs) {
+        return Arrays.stream(inputs)
+                .mapToInt(this::parseStringToInt)
+                .toArray();
     }
 }
--- src/main/java/calculator/DelimiterSplitter.java
+++ src/main/java/calculator/DelimiterSplitter.java
@@ -7,12 +7,12 @@ public class DelimiterSplitter {
     private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\\n(.*)";
     private static final String DEFAULT_DELIMITER_PATTERN = ",|;";
 
-    public boolean isCustomDelimiter(String input) {
+    private boolean isCustomDelimiter(String input) {
         char firstChar = input.charAt(0);
         return !Character.isDigit(firstChar);
     }
 
-    public String[] splitByCustomDelimiter(String input) {
+    private String[] splitByCustomDelimiter(String input) {
         Matcher m = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(input);
         if (m.find()) {
             String customDelimiter = m.group(1);
@@ -21,7 +21,7 @@ public class DelimiterSplitter {
         throw new IllegalArgumentException("커스텀 구분자를 찾을 수 없습니다. 올바른 형식은 //구분자\\n입니다.");
     }
 
-    public String[] splitByDefaultDelimiter(String input) {
+    private String[] splitByDefaultDelimiter(String input) {
         return input.split(DEFAULT_DELIMITER_PATTERN);
     }
 
