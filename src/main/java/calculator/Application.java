2064deb265c5ac2c90cb084546cfd86f1a22938d
leejongyoon1209 <bidakee11@naver.com>
2024-10-21 00:40:14 +0900

    메인

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,13 +2,12 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현f
 
         try {
             System.out.println("write number. (. or :)");
             String input = camp.nextstep.edu.missionutils.Console.readLine();
             int result = Calculate.sum(input);
-            System.out.println("result: " + result);
+            System.out.println("결과 : " + result);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
