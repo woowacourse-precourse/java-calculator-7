e68b1b71497db609c9710e46c346458f681747f4
AwesomeMinsik <beaut3579@gmail.com>
2024-10-20 19:03:25 +0900

    style: 필요없는 주석 제거
    - 문자열 쪼개기 및 구분자 추출을 담당하는 StringFilter 클래스의 메서드를 설명하는 주석 제거.

--- src/main/java/calculator/StringFilter.java
+++ src/main/java/calculator/StringFilter.java
@@ -7,7 +7,6 @@ public class StringFilter {
     private final String DEFAULT_DELIMITER = "[,:]";
     private final StringValidator validator = new StringValidator();
 
-    //입력값을 정수 배열로 변환
     public int[] convertToIntArray(String input){
         String[] splitInput = checkDelimiterType(input);
         validator.validateString(splitInput);
@@ -18,7 +17,6 @@ public class StringFilter {
                 .toArray();
     }
     
-    //구분자 타입 확인
     public String[] checkDelimiterType(String input) {
         if (input.contains("//") && input.contains("\\n")) {
             return findCustomDelimiter(input);
@@ -26,19 +24,16 @@ public class StringFilter {
         return splitDefaultDelimiter(input);
     }
     
-    //기본구분자로 나누기
     private String[] splitDefaultDelimiter(String input) {
         return input.split(DEFAULT_DELIMITER);
     }
 
-    //커스텀 구분자 추출하기
     private static String[] findCustomDelimiter(String Input) {
         String replaceString = Input.replace("//", "").replace("\\n", "");
         String customDelimiter = replaceString.substring(0, 1);
         return splitCustomDelimiter(replaceString, customDelimiter);
     }
     
-    //커스텀 구분자로 나누기
     private static String[] splitCustomDelimiter(String replaceString, String customDelimiter) {
         return replaceString.substring(1).split(Pattern.quote(customDelimiter));
     }
