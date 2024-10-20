b47bba0d2fd1afb71004f497df787a5438c24bad
areuming <djarys92@gmail.com>
2024-10-20 14:34:00 +0900

    우아한 테크코스 과제1

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	// 사용자에게 문자열 입력을 받음
+    	System.out.println("문자열을 입력하세요.");
+    	
+    	// Console 라이브러리를 사용하여 입력을 받음
+    	String input = Console.readLine();
+    	
+    	try {
+    		int result = StringAddCalculator.add(input);
+    		System.out.println("계산 결과: " + result);
+    	} catch (IllegalArgumentException e) {
+    		System.out.println(e.getMessage());
+    	}
     }
 }
