138da288a276bda78d47944f991c2aa41473c674
hjg1473 <hjg1473@naver.com>
2024-10-20 10:52:39 +0900

    feat: add all the numbers
    Add all the transformations of the string into a number.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -25,6 +25,7 @@ public class Application {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
+            sum += Integer.parseInt(arr[i]);
         }
         Console.close();
     }
