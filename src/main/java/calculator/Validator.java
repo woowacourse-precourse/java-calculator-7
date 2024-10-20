4d3f968beaa642fbe16168eb6468a1edfe81e4bc
yyysolhhh <solbaram37@gmail.com>
2024-10-21 02:35:13 +0900

    fix(Validator): isEmptyString함수에서 null 체크 추가
    isEmptyString
    - 먼저 input이 null인지 확인한뒤 빈 문자열인지 체크

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -1,11 +1,12 @@
 package calculator;
 
 public class Validator {
+    private static final String ERROR_MESSAGE = "잘못된 문자 포함됨";
 
     public void validateDefaultInput(String input) {
         for (char c : input.toCharArray()) {
             if (isInvalidCharacter(c)) {
-                throw new IllegalArgumentException("잘못된 문자 포함됨");
+                throw new IllegalArgumentException(ERROR_MESSAGE);
             }
         }
     }
@@ -17,46 +18,11 @@ public class Validator {
     public void validateCustomInput(String input, String customDelimiter) {
         String regex = "^([0-9:,]+|(" + customDelimiter + "))*$";
         if (!input.matches(regex)) {
-            throw new IllegalArgumentException("잘못된 문자 포함됨");
+            throw new IllegalArgumentException(ERROR_MESSAGE);
         }
     }
 
     public static boolean isEmptyString(String input) {
-        return input.isEmpty();
+        return input == null || input.isEmpty();
     }
-
-//    public void validateInput(String input) {
-////        if (isEmptyString(input)) {
-////            return;
-////        }
-//        if (isNegativeNumber(input)) {
-//            throw new IllegalArgumentException("음수는 입력할 수 없음");
-//        }
-//        if (isInvalidCharacter(input)) {
-//            throw new IllegalArgumentException("잘못된 문자 포함됨");
-//        }
-//    }
-
-//    private boolean isNegativeNumber(String input) {
-//        return !InputParser.isDelimiter("-") && input.contains("-");
-//    }
-
-//    private boolean isInvalidCharacter(String input) {
-//        for (char c : input.toCharArray()) {
-//            if (!Character.isDigit(c) && !isDelimiter(c)) {
-//                return true;
-//            }
-//        }
-//        // TODO 정규표현식으로 바꾸기
-////        String pattern = "^[0-9]+([:,][0-9]+)*$";
-//        String pattern = "^(\\\\d+([,:" + InputParser.getCustomDelimiter() + "]\\\\d+)*)?$";
-//        return !input.matches(pattern);
-//
-////        return false;
-//    }
-
-//    private boolean isDelimiter(char c) {
-//        return InputParser.isDelimiter(String.valueOf(c));
-//    }
 }
-
