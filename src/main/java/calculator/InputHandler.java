395686cd4d5c78b22a328af5b6195671cb3cb057
문고은 <>
2024-10-21 00:45:52 +0900

    refactor(separator) : 커스텀 구분자 enum으로 변경

--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -2,18 +2,21 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static calculator.enums.CustomDelimiter.CUSTOM_END;
+import static calculator.enums.CustomDelimiter.CUSTOM_START;
+
 public class InputHandler {
     public String readInput() {
         return Console.readLine();
     }
 
     public boolean isCustomDelimiterPresent(String userInput) {
-        return userInput.contains("//") && userInput.contains("\\n");
+        return userInput.contains(CUSTOM_START.getCustomDelimiter()) && userInput.contains(CUSTOM_END.getCustomDelimiter());
     }
 
     public char extractCustomDelimiter(String userInput) {
-        int customDelimiterStartIndex = userInput.indexOf("//");
-        int customDelimiterEndIndex = userInput.indexOf("\\n");
+        int customDelimiterStartIndex = userInput.indexOf(CUSTOM_START.getCustomDelimiter());
+        int customDelimiterEndIndex = userInput.indexOf(CUSTOM_END.getCustomDelimiter());
         validateCustomDelimiter(customDelimiterStartIndex, customDelimiterEndIndex);
 
         return userInput.charAt(customDelimiterStartIndex+2);
