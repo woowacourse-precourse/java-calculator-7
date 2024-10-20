75a76e82982a53794c143ebe025b0edbdbfe24a8
Cmk1031 <maple1031@naver.com>
2024-10-21 01:13:38 +0900

    feat: 구분자 세가지 경우 조정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -13,13 +13,19 @@ public class Calculator {
         String separator = "";
 
         if (Custom(str)) { //2. 커스텀 구분자
-
+            //3. 커스텀 구분자 중 [,\,] 세가지인 경우 구분자 조정
+            String a = ConvertSeparator(String.valueOf(str.charAt(2)));
+            //구분자를 separator에 추가
+            separator = "[,:".concat(a).concat("]");
+            //계산에 필요한 문자열만 잘라줌
+            str = str.substring(5);
         } else { //커스텀 구분자가 아닌 문자열들
-
+            separator = "[,:]"; //구분자 아니면 구분자[,:]로!
         }
 
     }
 
+    //1. 사용자의 입력을 받는 메서드
     private static String getString() {
         System.out.println(inputMessage);
         return Console.readLine();
@@ -32,6 +38,16 @@ public class Calculator {
             return false;
         }
         return str.startsWith("//") && str.startsWith("\\n", 3);
+    }
+
+    // 3. 구분자가 [, \, ] 인 경우 구분자 조정
+    private static String ConvertSeparator(String a) {
+        switch (a) {
+            case "\\" -> a = "\\\\";
+            case "[" -> a = "\\[";
+            case "]" -> a = "\\]";
+        }
+        return a;
     }
 
 }
