e7c5a68c9b7955d5aaed6dd53f8187b6d2ea73bc
wjdtjdgns <person3275@gmail.com>
2024-10-20 21:17:05 +0900

    fix(parser): 매칭 패턴 수정

--- src/main/java/calculator/util/DelimiterParser.java
+++ src/main/java/calculator/util/DelimiterParser.java
@@ -10,13 +10,17 @@ public class DelimiterParser {
     private static final List<String> DELIMITERS = Arrays.asList(",", ":");
 
     public List<String> parseDelimiter(String input) {
+        input = input.replace("\\n", "\n");
+
         List<String> delimiters = new ArrayList<>(DELIMITERS);
 
         if (input.startsWith("//")) {
-            Matcher matcher = Pattern.compile("//(.*?)\\\\n(.*)", Pattern.DOTALL).matcher(input);
+            Matcher matcher = Pattern.compile("//(.*?)\n(.*)", Pattern.DOTALL).matcher(input);
             if (matcher.matches()) {
                 String customDelimiter = matcher.group(1);
                 delimiters.add(Pattern.quote(customDelimiter));
+            } else {
+                throw new IllegalArgumentException("잘못된 입력입니다.");
             }
         }
 
@@ -24,9 +28,15 @@ public class DelimiterParser {
     }
 
     public String[] split(String input, List<String> delimiters) {
+        input = input.replace("\\n", "\n");
+
         if (input.startsWith("//")) {
-            int index = input.indexOf("\\n");
-            input = input.substring(index + 2);
+            int index = input.indexOf('\n');
+            if (index != -1) {
+                input = input.substring(index + 1);
+            } else {
+                throw new IllegalArgumentException("잘못된 입력입니다.");
+            }
         }
 
         return input.split(String.join("|", delimiters));
