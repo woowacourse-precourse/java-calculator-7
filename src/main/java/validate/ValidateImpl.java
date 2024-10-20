75de7321e88f5d8781795e386699cb3420dea832
agi1004 <agi1009@naver.com>
2024-10-20 21:19:52 +0900

    refactor: 유효하지 않은 문자를 걸러내는 로직 추가
    음수 사용하면 false, 빈 문자열이면 true, 커스텀 구분자가 존재하면 true를 우선 리턴 후,
    모든 문자열의 문자를 검사하여 숫자나 기본 구분자가 아닌 문자가 있다면 false 리턴

--- src/main/java/validate/ValidateImpl.java
+++ src/main/java/validate/ValidateImpl.java
@@ -1,7 +1,6 @@
 package validate;
 
-import static separate.Separator.BASIC;
-import static separate.Separator.CUSTOM;
+import static separate.Separator.*;
 
 public class ValidateImpl implements Validate {
 	public boolean isValid(final String str) {	// 입력받은 문자열이 유효한지 검사
@@ -20,16 +19,11 @@ public class ValidateImpl implements Validate {
 			return true;
 		}
 		
-		// 기본 구분자 사용 가능
-		if (BASIC.hasOneSeparators(str)) {
-			return true;
-		}
+		String[] separators = BASIC.getSeparators();	// 기본 구분자 2개
 		
-		// 문자열이 숫자로만 이루어져 있으면 가능
-		if (str.matches("\\d+")) {
-			return true;
-		}
-		
-		return false;	// 이 외에는 모두 유효성에 어긋남
+		// 숫자나 기본 구분자가 아닌 올바르지 않은 구분자가 포함되어있다면 불가
+		return str.chars()
+				.filter(ch -> ch < '0' || ch > '9')	// 숫자가 아닌 문자만 필터링
+				.allMatch(ch -> ch == separators[0].charAt(0) || ch == separators[1].charAt(0));	// 기본 구분자와 일치 여부 확인
 	}
 }
