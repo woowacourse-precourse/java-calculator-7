e44b987e4c616c966ed1484715d3f6172662bfe2
EHCTH <lees90896@gmail.com>
2024-10-20 13:47:56 +0900

    test: 테스트 추가 * feat: 기본 구분자 * feat: 커스텀 구분자 * feat: 더블 기본 구분자 * feat: 더블 커스텀 구분자 * feat: 공백

--- src/test/java/calculator/regex/domain/RegexPatternTest.java
+++ src/test/java/calculator/regex/domain/RegexPatternTest.java
@@ -1,5 +1,6 @@
-package calculator.separator.domain;
-import calculator.regex.domain.RegexPattern;
+package calculator.regex.domain;
+import calculator.constant.ErrorMessage;
+import calculator.sentence.dto.Sentence;
 import org.junit.jupiter.api.Test;
 
 
@@ -16,5 +17,54 @@ public class RegexPatternTest {
         assertThat(RegexPattern.extractNumberProcess(s)).isEqualTo(excepted);
         assertThat(RegexPattern.extractNumberProcess(s2)).isEqualTo(excepted);
         assertThat(RegexPattern.extractNumberProcess(s3)).isEqualTo(List.of(""));
+    }
+    @Test
+    void customSeparatorDouble() {
+        Sentence sentence = new Sentence("//s\\n7.0s0.1231s0.21312s0.2134");
+        assertThat(RegexPattern.getRegexPattern(sentence)).isEqualTo(RegexPattern.CUSTOM_SEPARATOR_DOUBLE);
+
+        Sentence exceptionSentence = new Sentence("//;\\n0;0");
+        assertThatThrownBy(() -> RegexPattern.getRegexPattern(exceptionSentence))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.UNEXPECTED_ERROR);
+
+    }
+    @Test
+    void customSeparator() {
+        Sentence sentence = new Sentence("//.\\n10.12310.213120.2134");
+        assertThat(RegexPattern.getRegexPattern(sentence)).isEqualTo(RegexPattern.CUSTOM_SEPARATOR);
+
+        Sentence exceptionSentence = new Sentence("//s\\n0s0");
+        assertThatThrownBy(() -> RegexPattern.getRegexPattern(exceptionSentence))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.UNEXPECTED_ERROR);
+
+    }
+    @Test
+    void defaultSeparator() {
+        Sentence sentence = new Sentence("1:2,3");
+        assertThat(RegexPattern.getRegexPattern(sentence)).isEqualTo(RegexPattern.DEFAULT_SEPARATOR);
+
+        Sentence exceptionSentence = new Sentence("0,1:2");
+        assertThatThrownBy(() -> RegexPattern.getRegexPattern(exceptionSentence))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.UNEXPECTED_ERROR);
+    }
+    @Test
+    void defaultSeparatorDouble() {
+        Sentence sentence = new Sentence("7.123,0.12321:312.3123");
+        assertThat(RegexPattern.getRegexPattern(sentence)).isEqualTo(RegexPattern.DEFAULT_DOUBLE_SEPARATOR);
+
+        Sentence exceptionSentence = new Sentence("0.0");
+        assertThatThrownBy(() -> RegexPattern.getRegexPattern(exceptionSentence))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.UNEXPECTED_ERROR);
+
+    }
+    @Test
+    void blank() {
+        Sentence sentence = new Sentence("");
+        assertThat(RegexPattern.getRegexPattern(sentence)).isEqualTo(RegexPattern.BLANK);
+
     }
 }
