9ed4a3403e73f25fa19f9812d88f70d306e3e7bc
unh6unh6 <ruddbs3308@naver.com>
2024-10-20 19:10:41 +0900

    test: 추출하는 문자열이 숫자가 아닌 문자로 시작 시 예외 테스트
    - 클래스 전체에서 쓰는 객체 전역으로 변경

--- src/test/java/calculator/model/NumberExtractorTest.java
+++ src/test/java/calculator/model/NumberExtractorTest.java
@@ -1,19 +1,29 @@
 package calculator.model;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class NumberExtractorTest {
+
+    NumberExtractor numberExtractor = new NumberExtractor();
+    Delimiters delimiters = new Delimiters();
+
     @Test
     void 구분자_기준_숫자_추출() {
-        NumberExtractor numberExtractor = new NumberExtractor();
-        Delimiters delimiters = new Delimiters();
         delimiters.addCustomDelimiter('&');
         delimiters.addCustomDelimiter('+');
         Numbers numbers = numberExtractor.extractNumber("2+13,9:4&4", delimiters);
         assertThat(numbers.getNumbers())
                 .isEqualTo(Arrays.asList(new Long[]{2L, 13L, 9L, 4L, 4L}));
+    }
+
+    @Test
+    void 숫자_아닌_문자_시작시_예외() {
+        assertThatThrownBy(() ->
+                numberExtractor.extractNumber("+5,3", delimiters))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
