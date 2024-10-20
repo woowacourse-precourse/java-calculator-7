527a23d575b3329959128f0022e5d8279850425e
SeongJunChun <csj991210@naver.com>
2024-10-20 20:48:15 +0900

    feat: 숫자 리스트 덧셈 기능 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+import java.util.List;
+
+public class Calculator {
+    private Integer accumulator = 0;
+
+    public Integer addAll(List<Integer> numbers) {
+        numbers.forEach(x -> {
+            checkPositiveNumber(x);
+            accumulator += x;
+        });
+        return accumulator;
+    }
+
+    private void checkPositiveNumber(Integer number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
