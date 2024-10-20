9441c574f67688699c2365e54f78ee1b71b3dd6d
yseo14 <lmslys14@naver.com>
2024-10-21 00:39:11 +0900

    feat: 에러 메시지 출력 기능 구현

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -4,4 +4,7 @@ public class OutputView {
     public void printResult(int result) {
         System.out.println("결과 : " + result);
     }
+    public void printError(String errorMessage){
+        System.out.println(errorMessage);
+    }
 }
