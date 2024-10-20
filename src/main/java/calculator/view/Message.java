4c9afac955031587eab7eba5e6dd3a69a00f5bcc
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 02:54:00 +0900

    feat(Message): 결괏값 출력 메시지 추가

--- src/main/java/calculator/view/Message.java
+++ src/main/java/calculator/view/Message.java
@@ -3,7 +3,8 @@ package calculator.view;
 public enum Message {
 
     INSTRUCTION_MESSAGE("덧셈할 문자열을 입력해 주세요."),
-    NO_INPUT("입력이 없습니다.");
+    NO_INPUT("입력이 없습니다."),
+    RESULT_MESSAGE("결과 : ");
 
     private final String message;
 
