a69252dfe95df1c2e425f381a48cfadb541768fb
minyeongg <1030pmy@gmail.com>
2024-10-20 17:42:13 +0900

    feat : 커스텀 구분자 없는 입력에서 계산 결과 반환
    1) 입력의 처음 혹은 마지막 문자가 구분자 중에 하나인 경우
       IllegalArgumentException을 발생시킨 후 애플리케이션은 종료
    2) 1)에 해당하지 않는 경우
        i. 입력에서 구분자를 기준으로 숫자를 분리해 배열을 만들어 전달하는 메소드 호출
        ii. 숫자 배열의 각 숫자에 대해서 유효성 검사
          a. 문자열에서 숫자 추출하는 메소드 호출
          b. 추출한 숫자가 음수인 경우
             IllegalArgumentException을 발생시킨 후 애플리케이션은 종료
          c. a,b에 해당하지 않는 경우
              숫자의 총합을 반환
    
    split 메소드 오버로딩

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -41,8 +41,25 @@ public class Application {
             } else {
                 throw new IllegalArgumentException("잘못된 구분자 형식입니다.");
             }
+        } else {
+            String first = "" + input.charAt(0);
+            String last = "" + input.charAt(input.length()-1);
+            if(first.equals(":") || first.equals(",") || first.equals(delimiter)) {
+                throw new IllegalArgumentException("구분자로 시작할 수 없습니다.");
+            } else if(last.equals(":") || last.equals(",") || last.equals(delimiter)) {
+                throw new IllegalArgumentException("구분자로 끝날 수 없습니다.");
+            } else {
+                numbers = split(input);
+                for (String number : numbers) {
+                    int num = parseNumber(number);
+                    if (num < 0) {
+                        throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+                    }
+                    sum += num;
+                }
+                return sum;
+            }
         }
-        return sum;
     }
 
     private static int parseNumber(String number) {
@@ -53,6 +70,15 @@ public class Application {
                     "구분자나 숫자를 연속해서 입력하거나 " +
                             "구분자로 지정하지 않은 문자를 입력할 수 없습니다.");
         }
+    }
+
+    private static String[] split(String input) {
+        String temp = input.replace(":", ",");
+        if(!temp.contains(",")) {
+            throw new IllegalArgumentException(
+                    "숫자만 입력하거나 구분자로 지정하지 않은 문자를 입력할 수 없습니다.");
+        }
+        return temp.split(",");
     }
 
     private static String[] split(String input, String delimiter) {
