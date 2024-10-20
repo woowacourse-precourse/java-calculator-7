0c1942dc61968ed6e9d2183a543022c1f16fd0fc
jungjoo <wjddis000507@gmail.com>
2024-10-20 22:26:41 +0900

    feat: 사용자가 문자열 입력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
+        CalculatorController calculatorController = new CalculatorController(new CalculatorService(), new InputView());
+
+        calculatorController.start();
     }
 }
+
