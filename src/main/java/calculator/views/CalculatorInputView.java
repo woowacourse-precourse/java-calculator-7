03ad4b65a993ee7782c6e452639525ec85e134b3
sangxxjin <sj_0368@naver.com>
2024-10-21 00:48:04 +0900

    refactor: 메세지 출력 양식에맞춰 변경 본문:

--- src/main/java/calculator/views/CalculatorInputView.java
+++ src/main/java/calculator/views/CalculatorInputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class CalculatorInputView {
 
     private static final String INPUT_PROMPT = "덧셈할 문자열을 입력해 주세요.";
-    private static final String RESULT_MESSAGE = "결과: ";
+    private static final String RESULT_MESSAGE = "결과 : ";
 
     public static String readInput() {
         return Console.readLine();
