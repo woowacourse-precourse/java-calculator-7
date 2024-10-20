b0223079cd4115897dced0925a7f1b72b9589801
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:17:44 +0900

    :sparkles: feat(CalculatorView): 계산 결과를 출력한다

--- src/main/java/calculator/view/CalculatorView.java
+++ src/main/java/calculator/view/CalculatorView.java
@@ -8,5 +8,9 @@ public class CalculatorView {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
+
+    public void displayResult(int result) {
+        System.out.println("결과 : " + result);
+    }
 }
 
