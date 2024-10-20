5daff8a4813f56ea75690f27c5173051c9568718
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-20 18:47:46 +0900

    feat : Application 구현 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.controller.CalculController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculController controller = new CalculController();
+        controller.start();
     }
 }
