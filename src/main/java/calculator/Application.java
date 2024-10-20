584cdfc41e33d242dbe66cb1dc8308684547142d
peanut990 <peanut1998@naver.com>
2024-10-20 19:35:25 +0900

    feat : add input message shown when starting the app
    added message is "덧셈할 문자열을 입력해 주세요."

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputString = readLine();
         Parser parser = new Parser();
         parser.parse(inputString);
