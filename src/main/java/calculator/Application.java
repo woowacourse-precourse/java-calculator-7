562d3c0515e1739cda28185d20a509119174a945
making-a-scene <4936joo@naver.com>
2024-10-20 18:30:59 +0900

    refactor: 일부 상수 값 final 키워드로 선언해 사용
    값의 임의 변경을 막기 위해 결과 출력 형식, 기본 구분자 사용을 위한 정규식, 커스텀 구분자 사용 형식을 static final로 선언함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,42 +3,46 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private static final String RESULT_STATEMENT = "결과 : ";
+    private static final String DEFAULT_REGEX = ",|:";
+    private static final String CUSTOM_DELIMITER_FORMAT = "//#\\n";
     public static void main(String[] args) {
         String input = Console.readLine();
 
         if (input.isBlank()) {
-            System.out.println("결과 : " + 0);
+            System.out.println(RESULT_STATEMENT + 0);
             return;
         }
         run(input);
     }
 
     private static void run(String input) {
-        String regex = ",|:";
+        String regex = DEFAULT_REGEX;
         if (checkWhetherInputUsingCustomDelimiter(input)) {
             regex = regex + '|' + getCustomDelimiter(input);
-            input = input.substring(5, input.length() - 1);
+            input = input.substring(5);
         }
         String[] splited = input.split(regex);
-        System.out.println("결과 : " + calculate(splited));
+        System.out.println(RESULT_STATEMENT + calculate(splited));
     }
 
     private static boolean checkWhetherInputUsingCustomDelimiter(String input) {
         if (input.charAt(0) >= '0' && input.charAt(0) <= '9') {
             return false;
-        } else if (input.charAt(0) == '\\') {
+        } else if (input.charAt(0) == CUSTOM_DELIMITER_FORMAT.charAt(0)) {
             return true;
         }
         throw new IllegalArgumentException();
     }
 
     private static char getCustomDelimiter(String input) {
+
         for (int i = 1; i <= 4; i++) {
-            if (i == 1 && input.charAt(i) != '/') {
+            if (i == 1 && input.charAt(i) != CUSTOM_DELIMITER_FORMAT.charAt(1)) {
                 throw new IllegalArgumentException();
-            } else if (i == 3 && input.charAt(i) != '\\') {
+            } else if (i == 3 && input.charAt(i) != CUSTOM_DELIMITER_FORMAT.charAt(3)) {
                 throw new IllegalArgumentException();
-            } else if (i == 4 && input.charAt(i) != 'n') {
+            } else if (i == 4 && input.charAt(i) != CUSTOM_DELIMITER_FORMAT.charAt(4)) {
                 throw new IllegalArgumentException();
             }
         }
