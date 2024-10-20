f769ed8b25ee82858b216de7b1eff1b4da3669ac
Lee-KwanBeom <dmtmqhdkk@naver.com>
2024-10-20 21:30:20 +0900

    Add Initial Test

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,10 +9,55 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void null_공백_테스트() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+
+            run(" ");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 단일_숫자() {
+        assertSimpleTest(() -> {
+            run("7");
+            assertThat(output()).contains("결과 : 7");
+        });
+    }
+
+    @Test
+    void 쉼표_숫자() {
+        assertSimpleTest(() -> {
+            run("1,2,10");
+            assertThat(output()).contains("결과 : 13");
+        });
+    }
+
+    @Test
+    void 기본_구분자_숫자() {
+        assertSimpleTest(() -> {
+            run("1,2:10:42");
+            assertThat(output()).contains("결과 : 55");
+        });
+    }
+
+    @Test
+    void 기본_구분자_큰단위_숫자() {
+        assertSimpleTest(() -> {
+            run("1,100:200:1000");
+            assertThat(output()).contains("결과 : 1301");
+        });
+    }
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
+
+            run("//;\\n1;2;7");
+            assertThat(output()).contains("결과 : 10");
         });
     }
 
