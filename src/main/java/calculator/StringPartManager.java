403e97a74e004b5c997a0ebf81522d4287856244
eb9862 <luclead622@gmail.com>
2024-10-20 18:09:37 +0900

    refactor : 문자열에 대한 예외 메시지 상수화

--- src/main/java/calculator/StringPartManager.java
+++ src/main/java/calculator/StringPartManager.java
@@ -2,11 +2,13 @@ package calculator;
 
 public class StringPartManager {
 
+    private static final String ERROR_INVALID_STRING = "커스텀 구분자 지정을 제외한 문자열은 양수와 구분자로 이루어져야 합니다";
+
     static void validateString(String str, String separators) {
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             if (!isSeparator(c, separators) && !Character.isDigit(c)) {
-                throw new IllegalArgumentException("커스텀 구분자 지정을 제외한 문자열은 양수와 구분자로 이루어져야 합니다");
+                throw new IllegalArgumentException(ERROR_INVALID_STRING);
             }
         }
     }
