6b6ae39c22974a42d49595696a5a0f55f21595fe
harvey <alstjr971@naver.com>
2024-10-20 19:11:17 +0900

    feat : 구분자로 문자열을 나눈 후 배열에 담는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,6 +8,16 @@ public class Application {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();  // 입력 데이터 읽기
-        System.out.println("input : " + input); // 입력 데이터 확인
+//        System.out.println("input : " + input); // 입력 데이터 확인
+
+
+        String[] tokens;
+        String delimiter = ",|:";  // 기본 구분자
+
+        tokens = input.split(delimiter);   // 입력 문자열을 구분자를 기준으로 분리
+
+        for (String token : tokens) {   // 배열에 담긴 값 확인
+            System.out.println(token);
+        }
     }
 }
