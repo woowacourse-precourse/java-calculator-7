6d8f2209a885ea1dfb8ccee22081e3a210a892e0
JaeHyunPyun <pyunjaehyun@gmail.com>
2024-10-21 03:13:39 +0900

    feat(split): 커스텀 구분자를 통해 피연산자를 분리하여 배열로 반환하도록 구현 >> - 사용자 입력에서 커스텀 구분자를 분리한뒤, 해당 구분자로 입력된 피연산자들을 구분하여 long타입 배열로 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
         CalculatorView calculatorView = new CalculatorView();
         CalculatorModel calculatorModel = new CalculatorModel();
         CalculatorController calculatorController = new CalculatorController(calculatorView, calculatorModel);
--- src/main/java/calculator/CalculatorModel.java
+++ src/main/java/calculator/CalculatorModel.java
@@ -4,9 +4,6 @@ public class CalculatorModel {
     private final Util util = new Util();
 
     public long[] changeStringtoLongOperandArray(String userInput) {
-        int lastIndex = getLastIndexofCustomDelimiter(userInput);
-        System.out.println(lastIndex);
-        
         if (!util.checkforInvalidPrefix(userInput)) {
             throw new IllegalArgumentException("잘못된 입력값입니다: " + userInput);
         }
@@ -14,20 +11,32 @@ public class CalculatorModel {
             // case1: 문자열이 비어있는 경우
             long[] operandArrayinLong = {0};
             return operandArrayinLong;
-        }
-//        else if (userInput.startsWith("//")) {
-//            //case2: 커스텀 구분자가 있는 경우
-//
-//        }
-        else {
-            //case3: 커스텀 구분자가 없는 경우
-            String[] operandArrayinString = userInput.split(",|:");
-            long[] operandArrayinLong = changeStringArrtoLongArr(operandArrayinString);
+        } else if (userInput.startsWith("//")) {
+            //case2: 커스텀 구분자가 있는 경우
+            int lastDeliiterIndex = getLastCustomDelimiterIndex(userInput);
+            System.out.println(lastDeliiterIndex);
+            String delimiter = getCustomDelimiter(userInput, lastDeliiterIndex);
+            System.out.println(delimiter);
 
+            String operandSubString = userInput.substring(lastDeliiterIndex + 2, userInput.length());
+
+            long[] operandArrayinLong = getLongArrayFromStringArray(operandSubString, delimiter);
+            return operandArrayinLong;
+
+        } else {
+            //case3: 커스텀 구분자가 없는 경우
+            long[] operandArrayinLong = getLongArrayFromStringArray(userInput, ",|:");
             return operandArrayinLong;
         }
-
     }
+
+    public long[] getLongArrayFromStringArray(String userInput, String delimiter) {
+        String[] operandArrayinString = userInput.split(delimiter);
+        long[] operandArrayinLong = changeStringArrtoLongArr(operandArrayinString);
+
+        return operandArrayinLong;
+    }
+
 
     public long[] changeStringArrtoLongArr(String[] stringArr) {
         long[] longArr = new long[stringArr.length];
@@ -42,30 +51,33 @@ public class CalculatorModel {
         return longArr;
     }
 
-    public int getLastIndexofCustomDelimiter(String userInput) {
+    public int getLastCustomDelimiterIndex(String userInput) {
 
         // 예외처리: //만 입력후 끝난 경우
         if (userInput.length() < 3) {
             throw new IllegalArgumentException("커스텀 구분자를 입력하지 않으셨습니다.");
         }
 
-        int delimiter = userInput.lastIndexOf("\\n");
-        //System.out.println(delimiter);
+        int delimiterIndex = userInput.lastIndexOf("\\n");
 
         // 예외처리: \n이 끝까지 안나오는 경우
-        if (delimiter == -1) {
+        if (delimiterIndex == -1) {
             throw new IllegalArgumentException("커스텀 구분자를 입력하지 않으셨습니다.");
         }
 
         // 예외처리: // 과 마지막 \n에 사이에 아무것도 없는 경우:
-        if (delimiter == 2) {
+        if (delimiterIndex == 2) {
             throw new IllegalArgumentException("커스텀 구분자를 입력하지 않으셨습니다.");
         }
 
-        // System.out.println(delimiter);
-
-        return delimiter;
+        return delimiterIndex;
     }
 
-
+    public String getCustomDelimiter(String userInput, int delimiterIndex) {
+        // \n 다음에 아무런 문자열이 오지 않는 경우:
+        if (delimiterIndex + 2 >= userInput.length()) {
+            throw new IllegalArgumentException("피연산자를 입력하지 않으셨습니다.");
+        }
+        return userInput.substring(2, delimiterIndex);
+    }
 }
