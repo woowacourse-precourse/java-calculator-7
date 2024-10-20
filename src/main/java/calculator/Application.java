2bd5d04bebb6b9c6c4328a197a7acf74bf40054f
MinjuKwak01 <mkwak1125@gmail.com>
2024-10-20 16:21:31 +0900

    feat : Application 클래스 main 함수 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import calculator.controller.StringAdderController;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        StringAdderController controller = new StringAdderController(inputView, outputView);
+        controller.run();
     }
+
 }
