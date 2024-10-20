238309424d93cced538065b82d7fe948b85e1efd
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 03:34:55 +0900

    feat(Message): 커스텀 구분자 관련 예외 메시지 추가
    커스텀 구분자가 없을 때
    커스텀 구분자가 2개 이상일 때
    커스텀 구분자가 숫자일 때

--- src/main/java/calculator/view/Message.java
+++ src/main/java/calculator/view/Message.java
@@ -2,10 +2,13 @@ package calculator.view;
 
 public enum Message {
 
+    INCLUDE_NOT_NUMBER("숫자가 아닌 값이 포함되어 있습니다."),
     INSTRUCTION_MESSAGE("덧셈할 문자열을 입력해 주세요."),
     INVALID_DELIMITER_ORDER("'//'는 '\\n'보다 앞에 위치해야 합니다."),
     INVALID_DELIMITER_POSITION("'//'와 '\\n'이 모두 존재해야 합니다."),
+    INVALID_NUMERIC_CUSTOM_DELIMITER("커스텀 구분자는 숫자가 될 수 없습니다."),
     NO_INPUT("입력이 없습니다."),
+    ONLY_ONE_CUSTOM_DELIMITER("커스텀 구분자는 한 개 이어야 합니다."),
     RESULT_MESSAGE("결과 : ");
 
     private final String message;
