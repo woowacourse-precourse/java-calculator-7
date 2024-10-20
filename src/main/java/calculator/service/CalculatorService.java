75d6795bdd629e5229ec8c50c72f99d9e42ca501
JS <zks145@naver.com>
2024-10-20 13:22:15 +0900

    feat (service) : 입력값에 커스텀 구분자가 포함되었는지 판단하는 기능 추가

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,4 +1,31 @@
 package calculator.service;
 
+import calculator.domain.DelimiterAndNumber;
+import calculator.exception.CalculatorException;
+import calculator.validation.DelimiterValidation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CalculatorService {
+
+    private static final List<String> DEFAULT_DELIMITERS = Arrays.asList(",", ":");
+
+
+    // 기본 구분자, 커스텀 구분자 타입 체크
+    private DelimiterAndNumber isDelimiterCustomOrDefault(String input, DelimiterValidation validation) {
+        DelimiterAndNumber delimiterAndNumberInput;
+
+        if (validation.isCustomDelimiter(input)) {
+            delimiterAndNumberInput = getCustomInputParse(input);
+        } else {
+            delimiterAndNumberInput = new DelimiterAndNumber(DEFAULT_DELIMITERS, input);
+        }
+
+        return delimiterAndNumberInput;
+    }
+
 }
