b5a2190aa4b3b942b1fadddd001669cf0a2442b8
zerocat36 <Seo.james2020@gmail.com>
2024-10-20 22:35:17 +0900

    feat($Application): add extractCustomDelimiter function
    Added new function to $Application
    - check input string has correct formats to set custom delimiter with regular expression
    - extract custom delimiter and string of numbers and delimiter separately
    - if input string is not correct format then evokes IllegalArgumentException
    - return string array of custom delimiter and numbers

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,27 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     private static boolean hasCustomDelimiter(String input) {
         return input.startsWith("//");
+    }
+
+    private static String[] extractCustomDelimiterNumbers(String input) {
+        String[] custom_delimiter_numbers = new String[2];
+
+        Pattern pattern = Pattern.compile("//(.)\\\\n(.*)");
+        Matcher matcher = pattern.matcher(input);
+
+        if(matcher.find()) {
+            custom_delimiter_numbers[0] = Pattern.quote(matcher.group(1));
+            custom_delimiter_numbers[1] = matcher.group(2);
+        }
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException();
+        }
+        return custom_delimiter_numbers;
     }
 
     public static void main(String[] args) {
