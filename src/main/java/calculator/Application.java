0babe9c6cedef13bb0d296af40fdd5ddee912dea
jwh946 <jwh946@gmail.com>
2024-10-20 20:12:33 +0900

    출력 형식 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,6 +8,7 @@ import java.util.Set;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         int sum = 0;
         Set<Character> set = new HashSet<>();
