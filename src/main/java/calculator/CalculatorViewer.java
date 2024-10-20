287e0112d4309e3c5d408ed0ef1f3b9d6e11c0b5
lchyeong <lchyeong@gmail.com>
2024-10-20 16:25:34 +0900

    refactor(CalculatorViewer): 코드 가독성을 위한 클래스 및 메서드 이름 수정

--- src/main/java/calculator/CalculatorViewer.java
+++ src/main/java/calculator/CalculatorViewer.java
@@ -1,12 +1,12 @@
 package calculator;
 
-public class CalculatorView {
+public class CalculatorViewer {
 
-    public void inputView() {
+    public void showInputPrompt() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
-    public void outputView(int result) {
+    public void displayResult(int result) {
         System.out.println("결과 : " + result);
     }
 }
--- src/test/java/calculator/CalculatorViewerTest.java
+++ src/test/java/calculator/CalculatorViewerTest.java
@@ -6,33 +6,33 @@ import java.io.PrintStream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CalculatorViewTest {
+public class CalculatorViewerTest {
 
-    private final CalculatorView calculatorview = new CalculatorView();
+    private final CalculatorViewer calculatorviewer = new CalculatorViewer();
 
     @Test
-    public void testInputView() throws Exception {
+    public void testShowInputPrompt() throws Exception {
         //given
         String testInput = "덧셈할 문자열을 입력해 주세요.\n";
         OutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
 
         //when
-        calculatorview.inputView();
+        calculatorviewer.showInputPrompt();
 
         //then
         Assertions.assertEquals(testInput, outputStream.toString());
     }
 
     @Test
-    public void testOutputView() throws Exception {
+    public void testDisplayResult() throws Exception {
         //given
         String testOutput = "결과 : 3\n";
         OutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
 
         //when
-        calculatorview.outputView(3);
+        calculatorviewer.displayResult(3);
 
         //then
         Assertions.assertEquals(testOutput, outputStream.toString());
