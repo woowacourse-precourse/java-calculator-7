5cc16add0e07255daf8de4f231006f6fdc4b127f
eb9862 <luclead622@gmail.com>
2024-10-20 18:18:43 +0900

    fix : 커스텀 문자로 메타 문자 들어올 경우
    SeparatorManager 클래스의 initSeparator() 메서드 수정
    - separator 반환 전 Pattern.quote 사용

--- src/main/java/calculator/SeparatorManager.java
+++ src/main/java/calculator/SeparatorManager.java
@@ -1,12 +1,14 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class SeparatorManager {
 
     static String initSeparator(String input, boolean hasCustomSeparator) {
         String basicSeparators = ",:";
         if (hasCustomSeparator) {
             char customSeparator = input.charAt(2);
-            return basicSeparators + customSeparator;
+            return Pattern.quote(basicSeparators + customSeparator);
         }
         return basicSeparators;
     }
