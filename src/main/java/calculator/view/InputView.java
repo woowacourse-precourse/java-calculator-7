a03e193941e4e743e32e36ff56e1e7b674b2718b
heehun <slrkqk2@naver.com>
2024-10-20 14:42:06 +0900

    refactor(InputView): validator를 inputview 내부에서 실행

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,12 +1,16 @@
 package calculator.view;
 
+import calculator.util.Validator;
 import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private final static String INPUT_MESSAGE= "덧셈할 문자열을 입력해 주세요";
 
     public static String getExpression() {
         System.out.println(INPUT_MESSAGE);
-        return Console.readLine();
+        String expression = Console.readLine();
+        if (Validator.isValidate(expression))
+            return Console.readLine();
+        throw new IllegalArgumentException("Input Invalid");
     }
 
 }
