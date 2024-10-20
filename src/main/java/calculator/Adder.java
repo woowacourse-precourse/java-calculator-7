d9a4ed4b8dcea46aca1da7b9264a91605f01d781
j4821_s <sonhm48021@gmail.com>
2024-10-20 21:43:03 +0900

    feat: Adder 클래스 작성
    추출된 모든 숫자의 합계 출력

--- src/main/java/calculator/Adder.java
+++ src/main/java/calculator/Adder.java
@@ -1,4 +1,20 @@
 package calculator;
 
+import java.util.List;
+
 public class Adder {
+    private final List<String> numList;
+
+    public Adder(List<String> numList) {
+        this.numList = numList;
+    }
+
+    public int addAll() {
+        int sum = 0;
+        for(String number : numList) {
+            sum += Integer.parseInt(number);
+        }
+
+        return sum;
+    }
 }
