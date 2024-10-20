ef3851dd8054018c2636480570066a478c084e18
Bell-isHere <quf417@gmail.com>
2024-10-21 03:19:42 +0900

    fix(application): 테스트 코드 수정
    - 지원 사이트 테스트를 위한 코드 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,39 +7,34 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class Application {
-    public static void main(String[] args) {
-        try {
-            System.out.println("덧셈할 문자열을 입력해 주세요.:");
-            String inputString = Console.readLine();
-            inputString = inputString.replace("\\n", "\n");
+    public static void main(String[] args) throws IllegalArgumentException {
+        System.out.println("덧셈할 문자열을 입력해 주세요 (커스텀 구분자 사용 시 '//구분자\\n숫자' 형식):");
 
-            String delimiter = "[,:]";
+        String firstInput = Console.readLine();
+        String delimiter = "[,:]";
+        String numbersInput;
 
-            if (inputString.startsWith("//")) {
-                int delimiterEndIndex = inputString.indexOf("\n");
-                if (delimiterEndIndex == -1) {
-                    throw new IllegalArgumentException("커스텀 구분자가 미완성되었습니다.");
-                }
-                String customDelimiter = inputString.substring(2, delimiterEndIndex);
-                if (customDelimiter.isEmpty()) {
-                    throw new IllegalArgumentException("커스텀 구분자를 입력해주세요.");
-                }
-                // 커스텀 구분자와 기존 구분자를 함께 사용하도록 함
-                delimiter = Pattern.quote(customDelimiter) + "|,|:";
-                inputString = inputString.substring(delimiterEndIndex + 1);
+        if (firstInput.startsWith("//")) {
+            int delimiterEndIndex = firstInput.indexOf("\\n");
+            if (delimiterEndIndex == -1) {
+                throw new IllegalArgumentException("커스텀 구분자가 미완성되었습니다. 형식: '//구분자\\n숫자'");
             }
-
-            String[] tokens = inputString.split(delimiter);
-
-            List<Integer> numbers = convertToIntegers(tokens);
-
-            int sum = calculateSumWithForLoop(numbers);
-
-            System.out.println("합계: " + sum);
-
-        } catch (IllegalArgumentException e) {
-            System.err.println("입력 값이 잘못되었습니다: " + e.getMessage());
+            String customDelimiter = firstInput.substring(2, delimiterEndIndex);
+            if (customDelimiter.isEmpty()) {
+                throw new IllegalArgumentException("커스텀 구분자를 입력해주세요.");
+            }
+            delimiter = Pattern.quote(customDelimiter) + "|,|:";
+            numbersInput = firstInput.substring(delimiterEndIndex + 2);
+        } else {
+            numbersInput = firstInput;
         }
+
+        String[] tokens = numbersInput.split(delimiter);
+        List<Integer> numbers = convertToIntegers(tokens);
+
+        int sum = calculateSumWithForLoop(numbers);
+        System.out.println("결과 : " + sum);
+
     }
 
     private static List<Integer> convertToIntegers(String[] tokens) {
