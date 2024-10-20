2af689b3bad2aa4d6c190318da1893acc6210cd1
k1mh4kj00n <k1mh4kj00n@gmail.com>
2024-10-21 01:49:39 +0900

    test4

--- src/main/java/calculator/Exceptions.java
+++ src/main/java/calculator/Exceptions.java
@@ -5,15 +5,6 @@ public class Exceptions implements ErrorCheck{
 
     public void ExceptionCheck(String str){
 
-//
-//        if(!Character.isDigit(str.charAt(0))) {
-//
-//
-//            throw new IllegalArgumentException("Empty Error");
-//        }
-//
-//
-
 
         }
 
