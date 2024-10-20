76b074f132e9e9fcdfbfd93b0cb0a40ebe551e88
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-21 00:44:43 +0900

    refactor: input가져오는 역할을 Reader 클래스로 이동

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,18 +1,16 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         try {
-            Reader.printInputMessage();
-            String input = Console.readLine();
+            String input = Reader.getInput();
             List<Integer> list = Parser.parse(input);
             int sum = Calculator.sum(list);
             Printer.print(sum);
         } finally {
-            Console.close();
+            Reader.close();
         }
     }
 }
