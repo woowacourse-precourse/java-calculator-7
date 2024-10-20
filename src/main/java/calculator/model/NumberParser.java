d97cc1cae1a926556a612a34d38c427ea105d3f7
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-21 02:11:48 +0900

    refactor(NumberParser): Validator 생성 및 의존성 생성, 메서드명 변경
    - validator 구현을 위해 Parser에서 의존성을 생성했습니다.
    - validator에 TODO List를 작성했습니다.
    - 메서드 명을 동사형으로 바꿔 명확하게 표현했습니다.
    - SeparatorParser에서 수행하던 separatorRegex 정의를 NumberParser에서 수행하게 했습니다.

--- src/main/java/calculator/model/NumberParser.java
+++ src/main/java/calculator/model/NumberParser.java
@@ -1,16 +1,39 @@
 package calculator.model;
 
+import calculator.validator.NumberValidator;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class NumberParser {
-    public ArrayList<String> inputNumberParse(String[] separatoredString) {
-        String separator = separatoredString[0];
-        ArrayList<String> operandList = new ArrayList<>();
-        operandList.addAll(Arrays.stream(separatoredString[1].split(separator))
-                .map(String::trim)
-                .filter(s -> s.matches("\\d+"))
-                .toList());
-        return operandList;
+    private final NumberValidator numberValidator = new NumberValidator();
+
+    public ArrayList<String> parseNumberString(HashMap<String, String> separatorAndNumbersMap) {
+
+        String numberString = separatorAndNumbersMap.get("numberString");
+        String separatorRegex = String.valueOf(separatorAndNumbersMap.get("separator").charAt(0));
+
+        for (int separatorIndex = 1; separatorIndex < separatorAndNumbersMap.get("separator").length();
+             separatorIndex++) {
+            String separator = String.valueOf(separatorAndNumbersMap.get("separator").charAt(separatorIndex));
+
+            int separatorAsciiValue = (int) separator.charAt(0);
+            if ((separatorAsciiValue >= 65 && separatorAsciiValue <= 90) || (separatorAsciiValue >= 97
+                    && separatorAsciiValue <= 122)) {
+                separatorRegex += ("|" + separator);
+            } else if ((separatorAsciiValue >= 33 && separatorAsciiValue <= 47) || separatorAsciiValue >= 58) {
+                separatorRegex += ("|\\" + separatorAsciiValue);
+            }
+        }
+
+        ArrayList<String> parsedNumberList = new ArrayList<>();
+
+        String[] numbers = numberString.split(separatorRegex);
+
+        for (String number : numbers) {
+            String trimmedNumber = number.trim();
+            parsedNumberList.add(trimmedNumber);
+        }
+
+        return parsedNumberList;
     }
 }
---
+++ src/main/java/calculator/validator/NumberValidator.java
@@ -0,0 +1,5 @@
+package calculator.validator;
+
+public class NumberValidator {
+    // TODO: 숫자 문자열에 숫자 또는 구분자가 아닌 문자가 포함되었는지 확인한다.
+}
