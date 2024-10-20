12d2b1dd332bdad97e7ae758093f76c883684bfc
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2024-10-20 15:25:46 +0900

    fix : try-catch 문 오류 수정
    NormalCalculatorImpl.java 과 SpecialCalculatorImpl.java 에서
    try - catch 문 안에 제대로 수행할 구문이 제대로 들어가 있지 않아서 발생하는 오류 해결

--- src/main/java/calculator/domain/service/NormalCalculatorImpl.java
+++ src/main/java/calculator/domain/service/NormalCalculatorImpl.java
@@ -1,6 +1,7 @@
 package calculator.domain.service;
 
-import dto.InputDTO;
+
+import calculator.domain.dto.InputDTO;
 
 public class NormalCalculatorImpl implements NormalCalculator {
 
@@ -9,6 +10,7 @@ public class NormalCalculatorImpl implements NormalCalculator {
         String input = inputDTO.getInput();
         String[] tokens = input.split(",|:");
         int sum = 0;
+
         for (String token : tokens) {
             //입력값에 공백이 포함된 경우에 대한 예외처리
             if (token.contains(" ")) {
@@ -16,15 +18,16 @@ public class NormalCalculatorImpl implements NormalCalculator {
             }
             //token으로 잘린 값에 문자열같이 Int형으로 변환될 수 없는 것에 대한 예외처리
             try {
-                int num = Integer.parseInt(tokens);
+                int num = Integer.parseInt(token);
+                //조건에서 양수만 들어온다고 하였기에 음수가 들어온 경우에 대한 예외처리
+                if (num < 0) {
+                    throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+                }
+                sum += num;
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("정수를 입력해야 합니다.");
             }
-            //조건에서 양수만 들어온다고 하였기에 음수가 들어온 경우에 대한 예외처리
-            if (num < 0) {
-                throw new IllegalArgumentException("음수는 허용되지 않습니다.");
-            }
-            sum += num;
+
         }
         return sum;
     }
--- src/main/java/calculator/domain/service/SpecialCalculatorImpl.java
+++ src/main/java/calculator/domain/service/SpecialCalculatorImpl.java
@@ -1,9 +1,12 @@
 package calculator.domain.service;
 
+import calculator.domain.dto.InputDTO;
+
 public class SpecialCalculatorImpl implements SpecialCalculator {
+
     @Override
-    public int calculate(String input, String delimiter) {
-        String[] tokens = input.split(delimiter);
+    public int calculate(InputDTO inputDTO, String delimiter) {
+        String[] tokens = inputDTO.getInput().split(delimiter);
         int sum = 0;
         for (String token : tokens) {
             //입력값에 공백이 포함된 경우에 대한 예외처리
@@ -12,15 +15,15 @@ public class SpecialCalculatorImpl implements SpecialCalculator {
             }
             //token으로 잘린 값에 문자열같이 Int형으로 변환될 수 없는 것에 대한 예외처리
             try {
-                int num = Integer.parseInt(tokens);
+                int num = Integer.parseInt(token);
+                //조건에서 양수만 들어온다고 하였기에 음수가 들어온 경우에 대한 예외처리
+                if (num < 0) {
+                    throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+                }
+                sum += num;
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("정수를 입력해야 합니다.");
             }
-            //조건에서 양수만 들어온다고 하였기에 음수가 들어온 경우에 대한 예외처리
-            if (num < 0) {
-                throw new IllegalArgumentException("음수는 허용되지 않습니다.");
-            }
-            sum += num;
         }
         return sum;
     }
