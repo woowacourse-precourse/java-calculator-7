e00bf79d6cc42fbb9abe612901084e129d88cece
kimsehw <seunghwan9703@gmail.com>
2024-10-20 21:00:48 +0900

    feat(Controller): 계산기를 사용해 입력받은 문자열을 계산하는 기능 구현

--- src/main/java/calculator/controller/Controller.java
+++ src/main/java/calculator/controller/Controller.java
@@ -1,5 +1,11 @@
 package calculator.controller;
 
+import calculator.model.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Controller {
-    
+    public static void run() {
+        OutputView.showOutput(Calculator.calculate(InputView.getString()));
+    }
 }
