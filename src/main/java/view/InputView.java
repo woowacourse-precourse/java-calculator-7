02be68d4397ec5d853a06e153ed8e4ad98fc3610
sinji <tobeabestns@naver.com>
2024-10-20 21:47:52 +0900

    feat: 실행 시 입력하라는 문구 출력

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,13 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String inputString() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        final String inputString = scanner.nextLine();
+        return inputString;
+    }
 }
