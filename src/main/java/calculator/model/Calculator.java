5f13b4964b6652467f9368a4051cf96db109fdd1
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-21 03:04:50 +0900

    feat(Calculator): 자료형에 따른 덧셈 기능 구현
    - 구분자를 통해 구분한 숫자 문자열을 자료형에 따라 다르게 계산한다.
    - 숫자의 크기에 따라 long과 BigInteger로 나눈다.
    - BigInteger가 성능이 더 느리기 때문에 try-catch문을 통해서 숫자 크기에 맞게 계산하도록 구현했습니다.

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -1,15 +1,24 @@
 package calculator.model;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class Calculator {
     public String numberCalculator(ArrayList<String> numbers) {
         String result = "";
-        long sum = 0;
-        for (String number : numbers) {
-            sum += Long.parseLong(number);
+        try {
+            long sum = 0;
+            for (String number : numbers) {
+                sum += Long.parseLong(number);
+            }
+            result = String.valueOf(sum);
+        } catch (NumberFormatException e) {
+            BigInteger sum = BigInteger.ZERO;
+            for (String number : numbers) {
+                sum = sum.add(new BigInteger(number));
+            }
+            result = sum.toString();
         }
-        result = String.valueOf(sum);
         return result;
     }
 }
