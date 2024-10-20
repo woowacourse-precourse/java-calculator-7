47a51194c8f486eabf88420891232c288e921413
minSsan <msp1125@hanyang.ac.kr>
2024-10-21 02:15:29 +0900

    refactor: Calculator 내부 CalculatorManager 호출하여 로직 단순화

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,22 +1,20 @@
 package calculator;
 
-import java.util.List;
-
 public class Calculator {
     private final CalculatorInput calculatorInput;
     private final CalculatorOutput calculatorOutput;
-    private final CalculatorInputParser calculatorInputParser;
 
     public Calculator() {
         calculatorInput = new CalculatorInput();
         calculatorOutput = new CalculatorOutput();
-        calculatorInputParser = new CalculatorInputParser();
     }
 
     public void run() {
-        String input = calculatorInput.read();
-        List<Integer> parsed = calculatorInputParser.parse(input);
-        CalculatorNumberList numberList = new CalculatorNumberList(parsed);
-        calculatorOutput.print(numberList.sum());
+        calculatorOutput.printInput();
+        final String input = calculatorInput.read();
+
+        final CalculatorManager calculatorManager = new CalculatorManager(input);
+
+        calculatorOutput.printResult(calculatorManager.calculateSum());
     }
 }
