c2c41c0f46254dc9e56a47bf0de3b962484e9e86
TalkingPotato90 <star901210@hanmail.net>
2024-10-21 02:25:47 +0900

    refactor: 테스트 코드 중복 제거

--- src/test/java/calculator/controller/NumericCalculatorControllerTest.java
+++ src/test/java/calculator/controller/NumericCalculatorControllerTest.java
@@ -1,5 +1,7 @@
 package calculator.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import calculator.model.Arithmetic;
 import calculator.model.CustomDelimiter;
 import calculator.model.DefaultDelimiter;
@@ -8,13 +10,12 @@ import calculator.model.InputData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("연산자 작동 테스트")
 class NumericCalculatorControllerTest {
@@ -25,30 +26,22 @@ class NumericCalculatorControllerTest {
         numericCalculatorController = new NumericCalculatorController();
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"1,2,3,4", "1:2:3"})
-    @DisplayName("기본 구분자를 사용한 데이터 객체를 받아서 연산부분 객체를 생성한다")
-    void createCalculatorWithDefaultDelimiter(String input) {
-        InputData inputData = new InputData(input);
-        Delimiter delimiter = new DefaultDelimiter();
-        assertThat(numericCalculatorController.createCalculatorPart(delimiter, inputData))
-                .isInstanceOf(Arithmetic.class);
-    }
-
-    private static Stream<List<String>> customDelimiterData() {
+    private static Stream<Arguments> delimiterData() {
         return Stream.of(
-                List.of("//;\\n1;2;3", ";"),
-                List.of("//!\\n1!2!3", "!"),
-                List.of("//a\\n1a2a3", "a")
+                Arguments.of(new InputData("//;\\n1;2;3"), new CustomDelimiter(";"), "[;] 커스텀 구분자"),
+                Arguments.of(new InputData("//!\\n1!2!3"), new CustomDelimiter("!"), "[!] 커스텀 구분자"),
+                Arguments.of(new InputData("//a\\n1a2a3"), new CustomDelimiter("a"), "[a] 커스텀 구분자"),
+                Arguments.of(new InputData("//;a\\n1;2a3"), new CustomDelimiter(";a"), "[;a] 커스텀 구분자"),
+                Arguments.of(new InputData("1,2,3,4"), new DefaultDelimiter(), "[,] 기본 구분자"),
+                Arguments.of(new InputData("1:2:3"), new DefaultDelimiter(), "[:] 기본 구분자"),
+                Arguments.of(new InputData("1:2:3,4,5"), new DefaultDelimiter(), "[,:] 기본 구분자")
         );
     }
 
-    @ParameterizedTest
-    @MethodSource("customDelimiterData")
-    @DisplayName("커스텀 구분자를 사용한 데이터 객체를 받아서 연산부분 객체를 생성한다")
-    void createCalculatorWithCustomDelimiter(List<String> input) {
-        InputData inputData = new InputData(input.get(0));
-        Delimiter delimiter = new CustomDelimiter(input.get(1));
+    @ParameterizedTest(name = "{2}")
+    @MethodSource("delimiterData")
+    @DisplayName("데이터 객체를 받아서 연산부분 객체를 생성한다")
+    void createArithmeticWithDelimiter(InputData inputData,Delimiter delimiter, String testCase) {
         assertThat(numericCalculatorController.createCalculatorPart(delimiter, inputData))
                 .isInstanceOf(Arithmetic.class);
     }
