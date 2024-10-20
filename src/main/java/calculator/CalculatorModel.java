62d7c262160430a58c38fbfb4a3e68133b4efeba
JaeHyunPyun <pyunjaehyun@gmail.com>
2024-10-20 23:54:23 +0900

    커스텀 구분자가 없는 경우 사용자 입력을 배열로 변환한 String Array를 Long타입 Array로 변환하는 메서드 구현

--- src/main/java/calculator/CalculatorModel.java
+++ src/main/java/calculator/CalculatorModel.java
@@ -4,12 +4,26 @@ public class CalculatorModel {
     private final Util util = new Util();
 
     public Long[] changeStringtoLongOperandArray(String userInput) {
-        Long[] operandArray = new Long[0];
+
         if (!util.checkforInvalidPrefix(userInput)) {
             throw new IllegalArgumentException("잘못된 입력값입니다: " + userInput);
         }
 
-        return operandArray;
+        //case1: 커스텀 구분자가 있는 경우
+
+        //case2: 커스텀 구분자가 없는 경우
+        String[] operandArrayinString = userInput.split(",|:");
+        Long[] operandArrayinLong = changeStringArrtoLongArr(operandArrayinString);
+
+        return operandArrayinLong;
+    }
+
+    public Long[] changeStringArrtoLongArr(String[] stringArr) {
+        Long[] longArr = new Long[stringArr.length];
+        for (int i = 0; i < stringArr.length; i++) {
+            longArr[i] = Long.parseLong(stringArr[i]);
+        }
+        return longArr;
     }
 
 
