2f074d3bd7689927f2363bc3cce64189f878beef
Lee Chae Rin <chaerin0916@gmail.com>
2024-10-20 23:37:35 +0900

    feat: 기본 구분자를 통한 입력 예외 처리
    - 구분자로 나누었을 때 숫자가 아닌 문자가 포함되어 있는 경우 예외 처리
    - 양수가 아닌 값이 포함된 경우 예외 처리

--- src/main/java/calculator/input/DefaultInput.java
+++ src/main/java/calculator/input/DefaultInput.java
@@ -10,8 +10,18 @@ public class DefaultInput {
         while(tokenizer.hasMoreTokens()){
             String token = tokenizer.nextToken();
 
+            // 구분자를 통해 분리한 토큰이 숫자가 아닌 경우 예외 처리
+            if (!token.matches("\\d+")) {
+                throw new IllegalArgumentException("숫자가 아닌 문자가 포함되어 있습니다");
+            }
+
             // 숫자로 변환
             int tokenToInt = Integer.parseInt(token);
+
+            // 숫자가 양수가 아닌 경우 예외 처리
+            if (tokenToInt <= 0) {
+                throw new IllegalArgumentException("양수가 아닌 값이 포함되어 있습니다");
+            }
 
             // 양수일 경우 리스트에 추가
             tokens.add(tokenToInt);
