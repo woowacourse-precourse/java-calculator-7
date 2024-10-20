18f21ccdff267ef07f61cabe81f4abad6c1a5b7b
TalkingPotato90 <star901210@hanmail.net>
2024-10-20 21:00:55 +0900

    test: 덧셈 계산 테스트 케이스 추가

--- src/test/java/calculator/model/SumCalculatorTest.java
+++ src/test/java/calculator/model/SumCalculatorTest.java
@@ -3,6 +3,9 @@ package calculator.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.in;
@@ -16,19 +19,21 @@ class SumCalculatorTest {
         sumCalculator = new SumCalculator();
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"'1,2:3:4',10", "'',0", "'500',500"})
     @DisplayName("기본 구분자를 사용할 때 덧셈 연산을 정확하게 수행한다")
-    void calculateSumExactlyWithDefaultDelimiter(){
-        InputData inputData = new InputData("1,2,3,4");
+    void calculateSumExactlyWithDefaultDelimiter(String input, int expected){
+        InputData inputData = new InputData(input);
         assertThat(sumCalculator.calculate(new Arithmetic(new DefaultDelimiter(),inputData)))
-                .isEqualTo(10);
+                .isEqualTo(expected);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"'//;.\\n1;2,3,4',10", "'//!\\n',0", "'//!\\n500',500"})
     @DisplayName("커스텀 구분자를 사용할 때 덧셈 연산을 정확하게 수행한다")
-    void calculateSumExactlyWithCustomDelimiter(){
-        InputData inputData = new InputData("//;.\\n1;2,3,4");
+    void calculateSumExactlyWithCustomDelimiter(String input, int expected){
+        InputData inputData = new InputData(input);
         assertThat(sumCalculator.calculate(new Arithmetic(new CustomDelimiter(inputData.convertDelimiterPart()),inputData)))
-                .isEqualTo(10);
+                .isEqualTo(expected);
     }
 }
