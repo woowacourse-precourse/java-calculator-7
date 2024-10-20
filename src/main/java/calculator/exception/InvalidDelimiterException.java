18ccb2168bd286e7cfdb90f9c763f4304ed2bc06
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-20 18:55:17 +0900

    feat : exception.InvaildDelimiterException 생성

---
+++ src/main/java/calculator/exception/InvalidDelimiterException.java
@@ -0,0 +1,7 @@
+package calculator.exception;
+
+public class InvalidDelimiterException extends RuntimeException {
+    public InvalidDelimiterException(String message) {
+        super(message);
+    }
+}
