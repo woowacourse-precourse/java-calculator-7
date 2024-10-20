146a764e05a2e390cdcc02e9044acee70c14e699
ziyoungkwon <gy011003@naver.com>
2024-10-20 16:56:03 +0900

    refactor: Application의 run 함수 수정
    빈문자를 테스트 했을 경우 noSuchElementException이 뜨면서 오류가 발생해서 readLine단계에서 해당 오류를 잡았다. inputdl 빈문자 일경우 결과는 0을 반환하도록 했다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,8 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.NoSuchElementException;
+
 public class Application {
 
     private final Calculator calculator;
@@ -21,8 +23,18 @@ public class Application {
     public void run() {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
+
+        String input = null;
+        try{
+            input = Console.readLine();
+        }catch(NoSuchElementException e){
+            System.out.println("결과 : 0");
+            return;
+        }
+
+
         int output = calculator.calculate(input);
         System.out.println("결과 : " + output);
+
     }
 }
