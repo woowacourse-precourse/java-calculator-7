520a4f63b2bc29522334a6b2fe434bcfefa3e58c
htjworld <116538001+htjworld@users.noreply.github.com>
2024-10-21 00:37:40 +0900

    (feat) 커스텀 구분자를 input에서 쉼표로 대체하고, 쉼표와 콜론 구분자로만 처리하도록 로직 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,9 +2,6 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,11 +9,10 @@ public class Application {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        ArrayList <String> delimiters = new ArrayList<>(Arrays.asList(",",":"));
-        // 재사용성을 위해 값을 유동적으로 추가할 수 있는 리스트로 설정
+
 
         // 공백이 입력된 경우(입력값이 없는 경우)
-        if (input.isEmpty()){
+        if (input.isBlank()){
             System.out.println("결과 : "+result);
             return;
         }
@@ -28,25 +24,15 @@ public class Application {
         }
 
         // 커스텀 구분자 확인 및 저장
-        if (input.length() >=2 && input.charAt(0) =='/' && input.charAt(1) == '/'){
-            try{
-                if (!input.contains("\\n")){
-                    throw new IllegalArgumentException("입력 문자열에 \\n문자가 포함돼있지 않습니다");
-                }
-                int index = input.indexOf("\\n");
-                String new_delimiter = input.substring(2,index);
-                input = input.substring(index+2);
-                delimiters.add(new_delimiter);
-            }catch (IllegalArgumentException e){
-                System.out.println(e.getMessage());
-                System.exit(1); // 애플리케이션 종료
+        if (input.startsWith("//")) {
+            int index = input.indexOf("\\n");
+            if (index == -1) {
+                throw new IllegalArgumentException("입력 문자열에 \\n 문자가 포함돼있지 않습니다.");
             }
-
+            String new_delimiter = input.substring(2, index);
+            input = input.substring(index + 2).replace(new_delimiter, ",");
         }
-
-        String splitPattern = String.join("|",delimiters.stream().map(Pattern::quote).toArray(String[]::new));
-        //구분자를 문자 그대로 처리하도록 이스케이프 처리(구분자에 메타 문자가 있을 경우 오류 방지)
-        String[] arr = input.split(splitPattern); // splitPattern 안에 구분자들을 기준으로 분리함
+        String[] arr = input.split("[,:]"); // splitPattern 안에 구분자들을 기준으로 분리함
 
         try {
             for (String a : arr) {
