1ff2aa21c31f1f4515eec68e7b71b28dd5f248e1
HARDY0070 <hwv1219@gmail.com>
2024-10-21 03:36:31 +0900

    feat(Application): 기능 및 예외처리 추가와 README.md 수정
    추가된 기능:
    - 커스텀 구분자를 제외한 문자열을 반환하는 기능
    - 문자열에서 숫자만 추출하는 기능
    - 추출한 숫자를 더하는 기능
    
    추가된 예외처리:
    - 구분자를 연속적으로 사용하는 경우
    - 음수를 입력하는 경우
    - 숫자 외의 문자를 입력하는 경우
    - 공백문자를 입력했을 때 0을 반환
    
    README.md 구현 기능 목록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,12 +5,43 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int result = 0;
-
-        System.out.print("덧셈할 문자열을 입력해 주세요.");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInputStr = Console.readLine();
 
-        String delimiter = userInputStr.substring(2, userInputStr.indexOf("\\n"));
-        System.out.println("delimiter = " + delimiter);
+        if (userInputStr == "") {
+            System.out.print("결과 : " + 0);
+            return;
+        }
+
+        String[] splitNumberArr;
+
+        if (userInputStr.indexOf("//") == 0) {
+            String delimiter = userInputStr.substring(2, userInputStr.indexOf("\\n"));
+
+            String userString = userInputStr.substring(userInputStr.indexOf("\\n") + 2);
+
+            splitNumberArr = userString.split(",|:|delimiter");
+
+        } else {
+            splitNumberArr = userInputStr.split(",|:");
+        }
+
+        int result = 0;
+
+        for (String splitNumber : splitNumberArr) {
+            if (splitNumber.equals("")) {
+                throw new IllegalArgumentException("구분자는 연속적으로 사용할 수 없습니다.");
+
+            } else if (splitNumber.matches("[^0-9]")) {
+                throw new IllegalArgumentException("숫자 외의 문자는 입력할 수 없습니다.");
+
+            } else if (Integer.valueOf(splitNumber) < 0) {
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+            }
+
+            result += Integer.valueOf(splitNumber);
+        }
+
+        System.out.println("결과 : " + result);
     }
 }
