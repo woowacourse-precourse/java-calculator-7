1c061bd9b2c17867fa66861566737a5898ffc6e3
yushinc <cys74351@gmail.com>
2024-10-20 22:39:47 +0900

    test: 피연산자_파싱_테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -33,7 +33,7 @@ class ApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_파싱_테스트() {
         // given
-        String inputString = "//;\\n2///!2//*\\n";
+        String inputString = "//;\\n2///!/n//*\\n";
         Parser parser = new Parser();
 
         // when
@@ -42,6 +42,22 @@ class ApplicationTest extends NsTest {
         // then
         assertThat(parser.getSeparators())
                 .containsExactly(';', '*');  // ;와 *가 제대로 파싱되었는지 확인
+    }
+
+    @Test
+    void 피연산자_파싱_테스트() {
+        // given
+        String inputString = "//;\\n2;32,15";
+        Parser parser = new Parser();
+
+        // when
+        parser.addBasicSeparators(); // 기본 구분자 추가
+        parser.parsingCustomSeparators(inputString); // 커스텀 구분자 파싱
+        parser.parsingOperands(inputString); // 피연산자 파싱
+
+        // then
+        assertThat(parser.getOperands())
+                .containsExactly(2, 32, 15);
     }
 
 }
