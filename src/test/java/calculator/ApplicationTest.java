11249ba2d9f09b50209c5a7b94f5cc36bf6d775e
soohalee <suha630@naver.com>
2024-10-20 17:39:07 +0900

    test: 여러 커스텀 구분자 사용

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,6 +17,14 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 여러_커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;hefa\\n1;2h3e4f3a3");
+            assertThat(output()).contains("결과 : 16");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("-1,2,3"))
