bf0538137d231e872f5860d1f36e1f10a16da18f
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2024-10-20 17:26:57 +0900

    fix : 커스텀 delimeter를 추가해주는 로직 추가
    SpecialCalculator를 사용하기 위해서 필요한 문자열을 delimeter 기준으로 자르고 delimeter에 추가하는 해주는 로직을 추가함

--- src/main/java/calculator/domain/service/CalculatorService.java
+++ src/main/java/calculator/domain/service/CalculatorService.java
@@ -17,12 +17,24 @@ public class CalculatorService  {
     public ResultDTO calculate(InputDTO inputDTO) {
         String input = inputDTO.getInput();
         if (input.startsWith("//")) {
-            int delimiterIndex = input.indexOf("\n");
-            String delimiter = input.substring(2, delimiterIndex);
-            String numbers = input.substring(delimiterIndex + 1);
-            int result = specialCalculator.calculate(new InputDTO(numbers), delimiter);
-            return new ResultDTO(result);
+            //"//"의 뒤에 나오게 되는 새로운 delimeter의 경우는 "\n"으로 끝나야 하기 때문에 인덱스 3이 "\"이고 인덱스 4가 "n"인지 확인한다.
+            char checkChar =input.charAt(3);
+            char checkChar2 =input.charAt(4);
+            if (checkChar == '\\' && checkChar2 == 'n') {
+                char delimiter = input.charAt(2);
+                String delimiters = ",|:|" + delimiter;
+                // "\n" 이후의 값을 input으로 넣어줘야 한다.
+                int delimeterIndex = input.indexOf('n');
+
+                String numbersArray = input.substring( delimeterIndex+ 1);
+                int result = specialCalculator.calculate(new InputDTO(numbersArray), delimiters);
+                return new ResultDTO(result);
+            }else{
+                throw new IllegalArgumentException("잘못된 구분자 입력입니다.");
+            }
+
         } else {
+            // "//"으로 시직하지 않는 경우는 일반적인 계산기를 사용한다.
             int result = normalCalculator.calculate(inputDTO);
             return new ResultDTO(result);
         }
