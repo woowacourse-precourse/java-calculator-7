80eb49e086f2cb660723b6efc0cc85305113941b
JoonKyo <joonkyolee@gmail.com>
2024-10-21 00:04:07 +0900

    refactor(Data, SeparatorValidator): 구분자 관리를 Data쪽으로 이동시키고 커스텀 구분자를 추출하는 동시에 문자열에서 커스텀 구분자 포맷 제거

--- src/main/java/calculator/domain/Data.java
+++ src/main/java/calculator/domain/Data.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Data {
+    private static final String CUSTOM_START_LETTERS = "//";
+    private static final String CUSTOM_END_LETTERS = "\\n";
+    private static final String MAKE_SPLIT_FORMAT_LETTER = "|";
     private static final String EMPTY_STRING = "";
     private static final String SEPARATOR_COMMA = ",";
     private static final String SEPARATOR_COLON = ":";
@@ -14,8 +17,6 @@ public class Data {
     public Data(String firstInputData) {
         this.firstInputData = firstInputData;
         separators = new ArrayList<>();
-        separators.add(SEPARATOR_COLON);
-        separators.add(SEPARATOR_COMMA);
         numbersForSum = handleInputString();
     }
 
@@ -32,9 +33,57 @@ public class Data {
             numbersForSum.add(0);
             return numbersForSum;
         }
-        separators = SeparatorValidator.extractCustomSeparator(separators, firstInputData);
-        String[] extractedStrings = SeparatorValidator.removeSeparator(separators, firstInputData);
+        separators = handleSeparator();
+        String[] extractedStrings = removeSeparator(separators, firstInputData);
         return NumberValidator.convertNumbers(extractedStrings);
+    }
+
+    private List<String> handleSeparator() {
+        separators.add(SEPARATOR_COLON);
+        separators.add(SEPARATOR_COMMA);
+        separators.addAll(extractCustomSeparator());
+        return separators;
+    }
+
+    public List<String> extractCustomSeparator() {
+        List<String> customSeparator = new ArrayList<>();
+        if (containsCustomSeparator(firstInputData)) {
+            customSeparator.add(firstInputData.substring(CUSTOM_START_LETTERS.length(),
+                    firstInputData.indexOf(CUSTOM_END_LETTERS)));
+            removeCustomSeparatorFormat();
+        }
+        return customSeparator;
+    }
+
+    public String[] removeSeparator() {
+        String str = inputData;
+        String[] separatedString = separateString(separators, str);
+
+        for (String string : separatedString) {
+            if (hasInvalidSeparatorFormat(string)) {
+                throw new IllegalArgumentException(ERROR_MESSAGE_FOR_SEPARATOR);
+            }
+        }
+        return separatedString;
+    }
+
+    private boolean containsCustomSeparator(String inputData) {
+        return inputData.startsWith(CUSTOM_START_LETTERS) && inputData.contains(CUSTOM_END_LETTERS);
+    }
+
+    private String removeCustomSeparatorFormat() {
+        return firstInputData.substring(firstInputData.indexOf(CUSTOM_END_LETTERS) + CUSTOM_END_LETTERS.length());
+    }
+
+    private String[] separateString(List<String> separators, String inputData) {
+        StringBuilder splitFormat = new StringBuilder();
+        for (int i = 0; i < separators.size(); i++) {
+            splitFormat.append(separators.get(i));
+            if (i < separators.size() - 1) {
+                splitFormat.append(MAKE_SPLIT_FORMAT_LETTER);
+            }
+        }
+        return inputData.split(splitFormat.toString());
     }
 
     private boolean hasEmptyString() {
--- src/main/java/calculator/domain/SeparatorValidator.java
+++ src/main/java/calculator/domain/SeparatorValidator.java
@@ -1,58 +1,10 @@
 package calculator.domain;
 
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class SeparatorValidator {
-    private static final String CUSTOM_START_LETTERS = "//";
-    private static final String CUSTOM_END_LETTERS = "\\n";
-    private static final String MAKE_SPLIT_FORMAT_LETTER = "|";
     private static final String ERROR_MESSAGE_FOR_SEPARATOR = "구분자를 잘못 입력하셨습니다. 프로그램을 종료합니다.";
     private static final Pattern VALID_NUMBER_PATTERN = Pattern.compile("[-\\d,:]+");
-
-    public SeparatorValidator() {
-    }
-
-    public static List<String> extractCustomSeparator(List<String> separators, String inputData) {
-        if (containsCustomSeparator(inputData)) {
-            separators.add(inputData.substring(CUSTOM_START_LETTERS.length(), inputData.indexOf(CUSTOM_END_LETTERS)));
-        }
-        return separators;
-    }
-
-    public static String[] removeSeparator(List<String> separators, String inputData) {
-        String str = inputData;
-        if (containsCustomSeparator(inputData)) {
-            str = removeCustomSeparatorFormat(inputData);
-        }
-        String[] separatedString = separateString(separators, str);
-
-        for (String string : separatedString) {
-            if (hasInvalidSeparatorFormat(string)) {
-                throw new IllegalArgumentException(ERROR_MESSAGE_FOR_SEPARATOR);
-            }
-        }
-        return separatedString;
-    }
-
-    private static boolean containsCustomSeparator(String inputData) {
-        return inputData.startsWith(CUSTOM_START_LETTERS) && inputData.contains(CUSTOM_END_LETTERS);
-    }
-
-    private static String removeCustomSeparatorFormat(String inputData) {
-        return inputData.substring(inputData.indexOf(CUSTOM_END_LETTERS) + CUSTOM_END_LETTERS.length());
-    }
-
-    private static String[] separateString(List<String> separators, String inputData) {
-        StringBuilder splitFormat = new StringBuilder();
-        for (int i = 0; i < separators.size(); i++) {
-            splitFormat.append(separators.get(i));
-            if (i < separators.size() - 1) {
-                splitFormat.append(MAKE_SPLIT_FORMAT_LETTER);
-            }
-        }
-        return inputData.split(splitFormat.toString());
-    }
 
     private static boolean hasInvalidSeparatorFormat(String str) {
         return hasConsecutiveSeparator(str) || hasInvalidSeparator(str);
