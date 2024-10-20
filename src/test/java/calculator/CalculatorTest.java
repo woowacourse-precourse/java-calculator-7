24d9245aeca622ee6ce6ba69e5da8decced5124f
Dev-LHY <dev.leehy@gmail.com>
2024-10-20 15:24:09 +0900

    test: 공백에 대한 예외처리 단위 테스트 추가

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import calculator.exception.InputValidationException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CalculatorTest {
+    @Test
+    void 공백_예외_테스트() {
+        assertThatThrownBy(() -> {
+            InputValidationException.isEmpty(" ");
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("아무것도 입력하지 않았습니다. 애플리케이션을 종료합니다.");
+    }
+}
