f2707ffe2c4f273d6fee5e5f9bbddada622146f9
jhk223 <wjdgkrud1@naver.com>
2024-10-21 02:27:33 +0900

    기본 구분자(쉼표, 콜론)를 기준으로 숫자 분리 및 합 계산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,31 @@
 package calculator;
+
+import java.util.Arrays;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-    	System.out.println("덧셈할 문자열을 입력해 주세요");
-    	String input = Console.readLine();
+       System.out.println("문자열을 입력해 주세요");
+        String input = Console.readLine();
+            int result = plus(input);
+            System.out.printf("result: %d%n", result);
+    }
+
+    public static int plus(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        int sum = 0;
+        String separator = "[,:]";  
+         String[] answer = input.split(separator);
+         for (int i = 0; i < answer.length; i++) {
+             String number = answer[i];
+                 if (Integer.parseInt(number) > 0) {
+                      sum += Integer.parseInt(number);
+                  } 
+         }
+         return sum;
+
     }
 }
