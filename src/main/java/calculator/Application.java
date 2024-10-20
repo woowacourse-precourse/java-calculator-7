9a08a6d83d1a5577013f36b569e522988a9e5c61
wngktjd13 <seonguk3553@kookmin.ac.kr>
2024-10-20 20:33:41 +0900

    feat: 예외 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,17 @@
 package calculator;
 
+import calculator.exception.InvalidInputException;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("문자열을 입력하세요.");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
         try {
+            // 입력값이 빈 문자열인 경우 예외 발생
             if (input == null || input.isEmpty()) {
-                throw new IllegalArgumentException("입력값이 없습니다.");
+                throw new InvalidInputException("입력값이 없습니다.");
             }
 
             StringCalculator calculator = new StringCalculator();
@@ -17,8 +19,8 @@ public class Application {
 
             System.out.println("결과 : " + result);
 
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        } catch (InvalidInputException e) {
+            System.out.println(e.getMessage());  // 예외 메시지 출력
         }
     }
 }
