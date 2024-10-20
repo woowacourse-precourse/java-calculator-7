47f0d155e7e54df668930e9bf8347b83c9427095
Yongjin <hi653@naver.com>
2024-10-20 12:08:55 +0900

    feat: 입력받은 문자열에 대해 유효성 검증 및 커스텀 구분자 인식
    기본 및 커스텀 구분자를 위한 변수 선언, 입력과정에서 발생할 수 있는 예외들에 대한 처리를 하였음.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,15 +6,46 @@ import camp.nextstep.edu.missionutils.Console;
  * 입력한 문자열에서 숫자를 추출하여 더하는 계산기를 구현한다.
  */
 public class Application {
+
+    public static final char BASIC_SEP_ONE = ':';
+    public static final char BASIC_SEP_TWO = ',';
+    public static char customSep  = '1';
+
+    // BASIC_SEP 혹은 숫자인지 검사
+    public static boolean checkBasic(char c){
+        return (c == BASIC_SEP_ONE || c == BASIC_SEP_TWO || Character.isDigit(c));
+    }
+
+    // 입력값 검증
+    public static void validateName(String input){
+        if(input.charAt(0) == '/' && input.length() < 5) {
+            throw new IllegalArgumentException("Input is not valid.");
+        }
+        if(input.charAt(0) == '/' && input.charAt(1) == '/'
+            && input.charAt(3) == '\\' && input.charAt(4) == 'n' && !Character.isDigit(input.charAt(2))){
+                customSep = input.charAt(2);
+        }
+        for (int i = 0; i < input.length(); i++){
+            if(customSep != '1' && i < 5) continue;
+            if(!checkBasic(input.charAt(i)) && input.charAt(i) != customSep){
+                throw new IllegalArgumentException(input.charAt(i) + " is inserted." );
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        // 문자열 입력받기 기능 구현.
-        System.out.println("문자열을 입력하세요.");
-        String input = Console.readLine();
-
-
-
+        // 입력받기 및 입력값 검증
+        try{
+            // 문자열 입력받기 기능 구현.
+            System.out.println("Please type string.");
+            String input = Console.readLine();
+            validateName(input);
+        }catch (IllegalArgumentException e){
+            System.out.println("Wrong Input : " + e.getMessage());
+            System.exit(0);
+        }
 
 
     }
