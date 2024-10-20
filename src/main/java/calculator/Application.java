ab69399df13a2bab61a91c892fb32437517f16f6
minyeongg <1030pmy@gmail.com>
2024-10-20 23:14:17 +0900

    fix : 커스텀 구분자 없는 입력 처리 로직 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,20 +10,20 @@ public class Application {
             return 0;
         } else if (input.startsWith("//")) {
             int newlineIndex = input.indexOf("\\n");
-            if(newlineIndex == -1) {
+            if (newlineIndex == -1) {
                 throw new IllegalArgumentException("커스텀 구분자를 지정할 수 없습니다.");
             }
             String customDelimiter = input.substring(2, newlineIndex);
             delimiter = customDelimiter + "|" + delimiter;
             input = input.substring(newlineIndex + 2);
-            numbers = input.split(delimiter);
-            for(String number : numbers) {
-                int num = parseNumber(number);
-                if (num < 0) {
-                    throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
-                }
-                sum += num;
+        }
+        numbers = input.split(delimiter);
+        for(String number : numbers) {
+            int num = parseNumber(number);
+            if (num < 0) {
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
             }
+            sum += num;
         }
         return sum;
     }
