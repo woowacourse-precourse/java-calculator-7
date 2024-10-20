a9625ac7133bc37cdf467e986a83d66ad77a14a9
Seongho Kim <kksshh0612@gmail.com>
2024-10-20 23:24:27 +0900

    refactor: 커스텀 구분자 로직 매서드 분리

--- src/main/java/calculator/controller/CalculationController.java
+++ src/main/java/calculator/controller/CalculationController.java
@@ -1,18 +1,24 @@
 package calculator.controller;
 
+import calculator.model.DelimiterGroup;
+import calculator.model.Operand;
+import calculator.model.Result;
 import calculator.service.CalculationService;
+import calculator.service.Converter;
 import calculator.service.TokenizationService;
 import calculator.view.CalculationView;
 public class CalculationController {
 
-    private final CalculationService calculationService;
     private final TokenizationService tokenizationService;
+    private final CalculationService calculationService;
+    private final Converter converter;
     private final CalculationView calculationView;
 
-    public CalculationController(CalculationService calculationService, TokenizationService tokenizationService,
-                                 CalculationView calculationView) {
-        this.calculationService = calculationService;
+    public CalculationController(TokenizationService tokenizationService, CalculationService calculationService,
+                                 Converter converter, CalculationView calculationView) {
         this.tokenizationService = tokenizationService;
+        this.calculationService = calculationService;
+        this.converter = converter;
         this.calculationView = calculationView;
     }
 
@@ -20,10 +26,17 @@ public class CalculationController {
 
         String input = calculationView.getInput();
 
-        long[] arr = tokenizationService.tokenize(input);
+        DelimiterGroup delimiterGroup = tokenizationService.makeDelimiterGroup(input);
 
-        long result = calculationService.sum(arr);
+        String[] operandStrings = tokenizationService.tokenize(input, delimiterGroup);
 
-        calculationView.displayResult(result);
+        Operand[] operands = new Operand[operandStrings.length];
+        for (int i = 0; i < operandStrings.length; i++) {
+            operands[i] = converter.convertToOperand(operandStrings[i]);
+        }
+
+        Result sum = calculationService.sum(operands);
+
+        calculationView.displayResult(sum);
     }
 }
--- src/main/java/calculator/service/TokenizationService.java
+++ src/main/java/calculator/service/TokenizationService.java
@@ -1,37 +1,65 @@
 package calculator.service;
 
+import calculator.model.DelimiterGroup;
+import calculator.model.Delimiters;
+import java.util.List;
+
 public class TokenizationService {
 
     private final CustomDelimiterChecker customDelimiterChecker;
-    private final Converter converter;
 
-    public TokenizationService(CustomDelimiterChecker customDelimiterChecker, Converter converter) {
+    private final static int CALCULATION_START_INDEX = 5;
+
+    public TokenizationService(CustomDelimiterChecker customDelimiterChecker) {
         this.customDelimiterChecker = customDelimiterChecker;
-        this.converter = converter;
     }
 
-    public long[] tokenize(String input){
-
-        StringBuilder delimiterBuilder = new StringBuilder(",|:");
-
-        if(input.startsWith("//")){
-            customDelimiterChecker.checkCustomDelimiterRule(input);
-            customDelimiterChecker.checkCustomDelimiter(input);
-
-            delimiterBuilder.append("|").append(input.charAt(2));
-
-            input = input.substring(5);
-        }
-
-        String[] nums = input.split(delimiterBuilder.toString());
-
-        long[] numbers = new long[nums.length];
-        int idx = 0;
-        for(String numStr : nums){
-            numbers[idx++] = converter.convertToOperand(numStr);
-        }
-
-        return numbers;
-
+    private boolean isCustomDelimiterExist(String input) {
+        return input.startsWith("//");
     }
+
+    private String getCustomDelimiter(String input) {
+
+        // 커스텀 구분자 지정 형식에 맞는 입력인지 확인
+        customDelimiterChecker.checkCustomDelimiterRule(input);
+        customDelimiterChecker.checkCustomDelimiter(input);
+
+        String delimiter = input.substring(2, 3);
+
+        return delimiter;
+    }
+
+    public DelimiterGroup makeDelimiterGroup(String input) {
+
+        DelimiterGroup delimiterGroup = new DelimiterGroup(new Delimiters());
+        Delimiters delimiters = delimiterGroup.getDelimiters();
+
+        delimiters.addAllDelimiter(List.of(",", ":"));              // 기본 구분자 추가
+
+        if (isCustomDelimiterExist(input)) {
+            String customDelimiter = getCustomDelimiter(input);
+            delimiters.addDelimiter(customDelimiter);
+        }
+
+        return delimiterGroup;
+    }
+
+    public String[] tokenize(String input, DelimiterGroup delimiterGroup) {
+
+        Delimiters delimiters = delimiterGroup.getDelimiters();
+
+        StringBuilder delimiterRegex = new StringBuilder();
+        for (String delimiter : delimiters.getDelimiterList()) {
+            delimiterRegex.append(delimiter).append("|");
+        }
+        delimiterRegex.deleteCharAt(delimiterRegex.length() - 1);
+
+        if (isCustomDelimiterExist(input)) {
+            input = input.substring(CALCULATION_START_INDEX);
+        }
+
+        return input.split(delimiterRegex.toString());
+    }
+
+
 }
