ec7a54635ad760ef139567f74bac7514d8206a27
JMJ <ghtntkdnsk@naver.com>
2024-10-21 02:23:59 +0900

    feat: add basic structure for input and output

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String userInput = Console.readLine();
+
+        String[] parsedNumbers = parseInput(userInput);
+        int sumResult = sumParsedNumbers(parsedNumbers);
+
+        System.out.println("결과 : " + sumResult);
     }
 }
