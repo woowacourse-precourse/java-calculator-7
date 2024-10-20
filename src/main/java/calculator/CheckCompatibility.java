23d11abc3ebc92d66742dfa6674fd9f41bb67607
theplayer1003 <theplayer1003@gmail.com>
2024-10-20 18:59:39 +0900

    feat(checkcompatibility): implement invalidinput check function
    implement:
    - checkInvalidInput

--- src/main/java/calculator/CheckCompatibility.java
+++ src/main/java/calculator/CheckCompatibility.java
@@ -1,4 +1,18 @@
 package calculator;
 
+import java.util.List;
+
 public class CheckCompatibility {
+
+    public void checkInvalidInput(String parsedUserInput, List<Character> delimiter) {
+        for (Character checkChar : parsedUserInput.toCharArray()) {
+            if (!Character.isDigit(checkChar)) {
+                isdelimiter(delimiter, checkChar);
+            }
+        }
+    }
+
+    private void isdelimiter(List<Character> delimiter, Character checkChar) {
+        if(!delimiter.contains(checkChar)) throw new IllegalArgumentException();
+    }
 }
