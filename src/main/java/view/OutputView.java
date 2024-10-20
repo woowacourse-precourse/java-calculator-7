16b951dc80c80f6a2e8dc9b987596af7c62a06b8
sinji <tobeabestns@naver.com>
2024-10-20 21:47:02 +0900

    feat: 결과 양식 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,9 @@
 package view;
 
+import domain.Calculator;
+
 public class OutputView {
+    public static void printResult(Calculator calculator) {
+        System.out.printf("결과 : %d", calculator.getCalculator());
+    }
 }
