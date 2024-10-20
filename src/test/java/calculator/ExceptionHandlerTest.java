60be7dbcf30dbf23597f27009b1b724abded3fba
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-20 16:50:03 +0900

    test(ExceptionHandlerTest.java): 세번째 예외 처리 기능에 대한 테스트 코드 작성
    구분자로 분리된 숫자들 중 0 또는 음수가 존재하는 것을 감지하는 checkZeroAndNegativeDigit 메서드에 대한 테스트 코드를 작성 및 검증했다.

--- src/test/java/calculator/ExceptionHandlerTest.java
+++ src/test/java/calculator/ExceptionHandlerTest.java
@@ -2,9 +2,13 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ExceptionHandlerTest {
@@ -75,5 +79,40 @@ class ExceptionHandlerTest {
     void checkIncorrectDelimNotThrowException(char delim) {
         assertThatCode(()->exceptionHandler.checkIncorrectDelim(delim))
                 .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @DisplayName("구분자로 분리된 숫자들 중 0 또는 음수가 존재할 경우 예외가 발생한다.")
+    @MethodSource("generateWrongNumberList")
+    void checkZeroAndNegativeDigitThrowException(List<Integer> input) {
+        assertThatThrownBy(() -> exceptionHandler.checkZeroAndNegativeDigit(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("구분자로 분리된 숫자들 중 0 또는 음수가 존재할 경우 예외가 발생한다.")
+    @MethodSource("generateRightNumberList")
+    void checkZeroAndNegativeDigitNotThrowException(List<Integer> input) {
+        assertThatCode(() -> exceptionHandler.checkZeroAndNegativeDigit(input))
+                .doesNotThrowAnyException();
+    }
+
+    static Stream<Arguments> generateWrongNumberList() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 0, 3)),
+                Arguments.of(Arrays.asList(0, 5, 9)),
+                Arguments.of(Arrays.asList(7, 20, 0)),
+                Arguments.of(Arrays.asList(-10, 20, 5)),
+                Arguments.of(Arrays.asList(10, -20, 5)),
+                Arguments.of(Arrays.asList(10, 20, -5))
+        );
+    }
+
+    static Stream<Arguments> generateRightNumberList() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3)),
+                Arguments.of(Arrays.asList(10, 5, 9)),
+                Arguments.of(Arrays.asList(50, 37, 60))
+        );
     }
 }
