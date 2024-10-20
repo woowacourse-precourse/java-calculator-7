ae2f46b0d0c71171bc27d025366f21e3b03c531a
dgh06175 <dgh06175@gmail.com>
2024-10-20 16:33:52 +0900

    docs: 비즈니스 로직 메소드 주석 작성

--- src/main/java/calculator/CalculatorModel.java
+++ src/main/java/calculator/CalculatorModel.java
@@ -11,12 +11,18 @@ public class CalculatorModel {
     public static final String[] DEFAULT_DIVIDERS = {",", ":"};
     private List<String> dividers = new ArrayList<>(Arrays.asList(DEFAULT_DIVIDERS));
 
+    /**
+     * input 을 구분자 콤마(,) 와 콜론(:)으로 나누어 List<Integer> 로 반환한다.
+     * 커스텀 구분자를 사용 할 수 있다. 커스텀 구분자는 문자열 앞부분의 "//" 와 "\n" 사이에 위치하고, 한 글자여야 한다.
+     *
+     * @param input 구분할 문자열입니다.
+     * @return 구분한 값들의 정수 리스트입니다.
+     */
     public List<Integer> splitString(String input) {
         if (input == null || input.isBlank()) {
             return List.of(0);
         }
 
-        // 커스텀 구분자 추출 및 삭제
         input = extractCustomDividerFromInput(input, dividers);
         validateInput(input, dividers);
 
@@ -25,6 +31,7 @@ public class CalculatorModel {
                 .toList();
     }
 
+    // 커스텀 구분자 추출 및 input 에서 커스텀 구분자 부분 삭제
     private String extractCustomDividerFromInput(String input, List<String> dividers) {
         if (input.startsWith("/")) {
             validateCustomDividerInput(input);
@@ -34,12 +41,14 @@ public class CalculatorModel {
         return input;
     }
 
+    // 커스텀 구분자 입력 형식 검증 (커스텀 구분자는 "//[구분자]\n" 형식이어야 함)
     private void validateCustomDividerInput(String customDividerInput) {
         if (!customDividerInput.startsWith("//") || !customDividerInput.startsWith("\\n", 3)) {
             throw new InvalidInputException(customDividerInput, ErrorMessage.CUSTOM_INPUT_ERROR);
         }
     }
 
+    // 입력값 검증 (숫자와 구분자로만 이루어져야 함)
     private void validateInput(String defaultDividerInput, List<String> dividers) {
         String dividersString = String.join("", dividers);
         String regex = "^(\\d+[,:" + dividersString + "])*\\d+$";
@@ -48,10 +57,12 @@ public class CalculatorModel {
         }
     }
 
+    // 구분자들로 문자열을 나누어 List 로 반환
     private List<String> splitStringWithDividers(String input, List<String> dividers) {
         return List.of(input.split(String.join("|", dividers)));
     }
 
+    // 문자열을 정수로 변환, NumberFormatException 예외를 InvalidInputException 으로 변환
     private int parseToInt(String value) {
         try {
             return Integer.parseInt(value.trim());
@@ -60,6 +71,12 @@ public class CalculatorModel {
         }
     }
 
+    /**
+     * values 의 합을 반환한다.
+     *
+     * @param values 합을 계산할 정수 리스트입니다.
+     * @return 합계 값입니다.
+     */
     public int calculateSum(List<Integer> values) {
         return values.stream()
                 .mapToInt(Integer::intValue)
