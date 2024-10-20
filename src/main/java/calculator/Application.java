d9bd565d077ccd1302aa88245ce23e138672b071
htjworld <116538001+htjworld@users.noreply.github.com>
2024-10-21 00:00:20 +0900

    구분자가 메타문자일 경우 이스케이프 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -21,7 +22,7 @@ public class Application {
         }
 
         // 단일 숫자가 입력된 경우
-        if (input.matches("[-+]?[0-9]+")){
+        if (input.matches("[0-9]+")){
             System.out.println("결과 : "+Integer.parseInt(input));
             return;
         }
@@ -43,12 +44,13 @@ public class Application {
 
         }
 
-        String splitPattern = String.join("|",delimiters); // 예시 : ",|:|;"
+        String splitPattern = String.join("|",delimiters.stream().map(Pattern::quote).toArray(String[]::new));
+        //구분자를 문자 그대로 처리하도록 이스케이프 처리(구분자에 메타 문자가 있을 경우 오류 방지)
         String[] arr = input.split(splitPattern); // splitPattern 안에 구분자들을 기준으로 분리함
 
         try {
             for (String a : arr) {
-                if (!a.matches("[-+]?[0-9]+")) {
+                if (!a.matches("[0-9]+")) {
                     throw new IllegalArgumentException("잘못된 숫자 형식:" + a); // 분리된 arr 안에 값들이 정수인지 확인
                 }
                 result += Integer.parseInt(a);
