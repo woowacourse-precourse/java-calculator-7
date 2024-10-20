383928f46bcac40881a9993a78e087aabf8079a9
chan <ckstj8027@naver.com>
2024-10-20 22:09:20 +0900

    refactor(constants): ErrorConst 추가

---
+++ src/main/java/calculator/constants/ErrorConst.java
@@ -0,0 +1,8 @@
+package calculator.constants;
+
+public class ErrorConst {
+
+    public static final String NUMBER_ERROR = "[ERROR]: 숫자는 양수이어야 합니다.";
+    public static final String NOT_HAVE_NEXT_TOKEN_ERROR = "[ERROR]: nextToken 의값을 입력해주세요.";
+    public static final String CAN_NOT_PARSE_NUMBER_ERROR = "[ERROR]: 숫자를 입력해주세요";
+}
