1ac345cc28c4abcd25b5dba585370e5e82a36371
yejin923 <yejin923@naver.com>
2024-10-20 15:29:54 +0900

    fix: change to regex

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,39 +1,42 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
         int total = 0;
         String separator = null;
-        String[] str = null;
+        String num = null;
+        String[] str;
+
 
         System.out.println("덧셈할 문자열을 입력해주세요. ");
         String line = Console.readLine();
-        String sep1 = line.substring(0,2);
-        String sep2 = line.substring(3,5);
 
-        if(sep1.equals("//") && sep2.equals("\\n")){
-            separator = line.substring(2,3);
-            line = line.substring(5);
-            str = line.split(",|:|"+separator);
-        }else{
-            str = line.split(",|:");
+        Pattern pattern = Pattern.compile("//(.)\\\\n(.*)");
+        Matcher sep = pattern.matcher(line);
+
+        if (sep.find()) {
+            separator = sep.group(1);
+            num = sep.group(2);
+            str = num.split(Pattern.quote(separator) + "|,|:");
+
+        } else {
+            str = line.split(":|,");
         }
+
 
         for (int i = 0; i < str.length; i++) {
             try {
                 total += Integer.parseInt(str[i]);
-            } catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 total += 0;
             }
         }
 
         System.out.println("결과 : " + total);
-
-
     }
 }
