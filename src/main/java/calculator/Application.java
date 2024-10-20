c2e4c22616e7c4adc9f431144c898f188bec1260
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-20 21:02:14 +0900

    feat(exception control) : throw exception and end application when number is negative(음수) or input is null

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,36 +1,40 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-
 public class Application {
     public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String inputStr = Console.readLine();
 
         int result = 0;
 
-        if (inputStr.charAt(0)=='/' && inputStr.charAt(1)=='/'){
-            String pivot = inputStr.substring(2, inputStr.indexOf('\\'));
-            String target = inputStr.substring((inputStr.indexOf("n")+1));
+        try {
+            String inputStr = Console.readLine();
 
-            String[] split = target.split(pivot);
+            if (inputStr.charAt(0) == '/' && inputStr.charAt(1) == '/') {
+                String pivot = inputStr.substring(2, inputStr.indexOf('\\'));
+                String target = inputStr.substring((inputStr.indexOf("n") + 1));
 
-            for (String s : split) {
-                int num = Integer.parseInt(s);
-                result += num;
+                String[] split = target.split(pivot);
+
+                for (String s : split) {
+                    int num = Integer.parseInt(s);
+                    result += num;
+                }
+            } else {
+                String[] split = inputStr.split(":|,");
+                for (String s : split) {
+                    int s1 = Integer.parseInt(s);
+                    result += s1;
+                }
             }
+
+            if (inputStr.contains("-")){
+                throw new IllegalArgumentException("잘못된 입력입니다. 자연수만 압력해주세요.");
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            result = 0;
         }
 
-        else {
-            String[] split = inputStr.split(":|,");
-            for (String s : split) {
-                int s1 = Integer.parseInt(s);
-                result +=s1;
-            }
-        }
-
-        System.out.println("결과 : "+ result);
+        System.out.print("결과 : "+ result);
 
     }
 
