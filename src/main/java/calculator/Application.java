110caa8ff4d992448f21c282def257bd1407f971
yongsik <wkdrndydtlr@naver.com>
2024-10-20 15:55:17 +0900

    feat(add)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,62 @@
 package calculator;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args){
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String input = readLine();
+        String splitString = ",|:";
+
+        if(input == null || input.isEmpty()){
+            System.out.println("결과: "+0);
+            return;
+        }
+
+        if(isCustom(input)){
+            splitString += getCustom(input);
+        }
+
+        String[] tmp = input.split("\\\\n");
+        input = tmp[tmp.length-1];
+        int sum=0;
+
+        try{
+            String[] numArr = input.split(splitString);
+            sum = calSum(numArr);
+        }catch(IllegalArgumentException e){
+            return;
+        }finally{
+            System.out.println("결과: "+sum);
+        }
+
     }
+
+    public static int calSum(String[] numArr){
+        int sum=0;
+            for (String s : numArr) {
+                if (!s.matches("[+-]?\\d*(\\.\\d+)?")) throw new IllegalArgumentException();
+                if(Integer.parseInt(s)<=0){
+                    System.out.println("minus");
+                    throw new IllegalArgumentException();
+                }
+                sum += Integer.parseInt(s);
+            }
+        return sum;
+    }
+
+    public static boolean isCustom(String input){
+        return input.startsWith("//");
+    }
+
+    public static String getCustom(String input){
+        String customCommand="";
+        String[] tmpStr = input.split("\\\\n");
+
+        for(String s: tmpStr) if(isCustom(s)) customCommand += "|"+s.split("//")[1];
+
+        return customCommand;
+    }
+
 }
