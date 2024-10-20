3f6f191a8828868aff22cab1d4e12c130a005730
Son JunHyung <kingjh1125@gmail.com>
2024-10-20 16:30:05 +0900

    test($controller): add integration test
    통합 테스트 추가:
    - 무자열 덧셈 실패 테스트 추가

--- src/test/java/calculator/controller/calculatorControllerTest.java
+++ src/test/java/calculator/controller/calculatorControllerTest.java
@@ -2,6 +2,7 @@ package calculator.controller;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import calculator.Application;
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -11,12 +12,22 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class calculatorControllerTest extends NsTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"///\\n1/2/3", "//\\\\n1\\2\\3", "//n\\n1n2n3", "// \\n1 2 3"})
+    @ValueSource(strings = {"///\\n1/2/3", "//\\\\n1\\2\\3", "//n\\n1n2n3", "// \\n1 2 3", "1,2:3", "//^\\n1^2^3"})
     void 문자열_덧셈_성공_테스트(String input) {
         assertSimpleTest(() -> {
             run(input);
             assertThat(output()).contains("결과 : 6");
         });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//]\\n1[2[3", "//1\\n1,2,3", "HelloWorld", "/\\n1", "//]]\\n1", "1,2,3,", ",1,2,3",
+            "1,2,a", "-1,2,3", "1,,2"})
+    void 문자열_덧셈_실패_테스트(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Override
