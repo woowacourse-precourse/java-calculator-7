a4780c0e17dc2d64d12bac1c98627fff8bd870e8
seongjae6751 <seongjae6751@naver.com>
2024-10-20 18:35:47 +0900

    chore: 필요없는 코드 삭제

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -11,7 +11,6 @@ public class StringCalculator {
     }
 
     public int add(String input) {
-        System.out.println("입력된 값: '" + input + "'");
         if (input == null || input.trim().isEmpty()) {
             return 0;
         }
