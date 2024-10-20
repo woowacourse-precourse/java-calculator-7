a4a4e3f83a9dee040071c7e3efe981b1ef86230b
Chan531 <chan657312@gmail.com>
2024-10-20 19:01:01 +0900

    fix(StringValidator): 커스텀 구분자가 추가되지않는 에러 수정

--- src/main/java/calculator/model/StringValidator.java
+++ src/main/java/calculator/model/StringValidator.java
@@ -1,5 +1,7 @@
 package calculator.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StringValidator {
@@ -7,7 +9,7 @@ public class StringValidator {
 	private static final String COMMA_DELIMITER = ",";
 	private static final String COLON_DELIMITER = ",";
 
-	private List<String> delimiters = List.of(COMMA_DELIMITER, COLON_DELIMITER);
+	private List<String> delimiters = new ArrayList<>(Arrays.asList(COMMA_DELIMITER, COLON_DELIMITER));
 
 	public void addCustomDelimiter(String customDelimiter) {
 		delimiters.add(customDelimiter);
