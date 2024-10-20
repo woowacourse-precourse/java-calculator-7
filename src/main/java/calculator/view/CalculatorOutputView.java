0d44bb623b7b1fc11593331a9a27a84e907a75dc
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 02:11:38 +0900

    feat(CalculatorOutputView): CalculatorOutputView 생성 및 입력 안내 메시지 출력 메서드 구현

---
+++ src/main/java/calculator/view/CalculatorOutputView.java
@@ -0,0 +1,8 @@
+package calculator.view;
+
+public class CalculatorOutputView {
+
+    public void printInstruction() {
+        System.out.println(Message.INSTRUCTION_MESSAGE.getMessage());
+    }
+}
