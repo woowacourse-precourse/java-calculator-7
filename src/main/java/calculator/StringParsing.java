d7a036f028eb2abdaaf5dcc8a29f8db6ceac3e91
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-20 15:31:50 +0900

    refactor: 문자열 파싱 클래스의 메서드 수정
    -splitNpushString() : 구분자를 기준으로 split을 하여 String배열에 넣음
    -stringValidateCheck() : 문자열에 숫자 이외의 값이 들어가는지 확인

--- src/main/java/calculator/StringParsing.java
+++ src/main/java/calculator/StringParsing.java
@@ -3,9 +3,21 @@ package calculator;
 public class StringParsing {
     private static String[] userInputArr;
 
-    public static String[] stringValidateCheck(String userInput, String delimiter) {
+    public static String[] splitNpushString(String userInput, String delimiter) {
         userInputArr = userInput.split(delimiter);
-        //System.out.println("배열 안에 값들" + Arrays.toString(UserInputArr));//디버깅코드
         return userInputArr;
+    }
+
+    public static void stringValidateCheck(String[] userInputArr) {
+        for (String s : userInputArr) {
+            if (s.isEmpty()) {
+                continue;
+            }
+            try {
+                Long.parseLong(s);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다");
+            }
+        }
     }
 }
