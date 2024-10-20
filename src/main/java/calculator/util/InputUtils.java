bd1881ccbad0010818ee6c043a4e0e80790db991
byunghyunkim0 <bb05qudgus@gamil.com>
2024-10-20 11:42:33 +0900

    feat: 문자열 검증 기능 구현 - 유효성 검증   - 구분자로 구분된 값들이 양수가 아닐경우 예외 처리

--- src/main/java/calculator/util/InputUtils.java
+++ src/main/java/calculator/util/InputUtils.java
@@ -3,7 +3,7 @@ package calculator.util;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputUtils {
-    private static String readInput() {
+    public static String readInput() {
         String input = Console.readLine();
         validateNullInput(input);
         validateWhitespaceInput(input);
@@ -28,10 +28,36 @@ public class InputUtils {
     }
 
     private static void validateCustomDelimiterInput(String input) {
-        // todo 커스텀 구분자 검증 로직
+        String delimiter = input.substring(2, input.indexOf("\\n"));
+        String expression = input.substring(input.indexOf("\\n") + 2);
+        String[] tokens = expression.split(delimiter);
+        for (String token : tokens) {
+            if (token.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            validateNumericInput(token);
+        }
     }
 
     private static void validateDefaultDelimiterInput(String input) {
-        // todo 기본 구분자 검증 로직
+        String[] tokens = input.split("[,:]");
+        for (String token : tokens) {
+            if (token.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            validateNumericInput(token);
+        }
+    }
+
+    // 문자열이 양수인지 검증하는 메서드
+    private static void validateNumericInput(String token) {
+        try {
+            int number = Integer.parseInt(token); // 숫자로 변환 시도
+            if (number < 0) {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
