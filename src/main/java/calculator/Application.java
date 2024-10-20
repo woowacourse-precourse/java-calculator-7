8b1337904bc84209f05fa1bd20e66d773cebf58b
JINSU <bluesea9519@gmail.com>
2024-10-20 22:34:15 +0900

    refactor: finalize main method inplementation

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import calculator.domain.StringCalculator;
+import calculator.io.InputReader;
+import calculator.io.OutputWriter;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputReader inputReader = new InputReader();
+        OutputWriter outputWriter = new OutputWriter();
+        StringCalculator stringCalculator = new StringCalculator();
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = inputReader.readInput();
+//        try {
+//            int total = stringCalculator.calculate(input);
+//            outputWriter.writeOutput(total);
+//        } catch (IllegalArgumentException e) {
+//            outputWriter.writeOutput(e.getMessage());
+        int total = stringCalculator.calculate(input);
+        outputWriter.writeOutput(total);
     }
 }
+
