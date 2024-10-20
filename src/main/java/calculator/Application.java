0f20e884965b95261701cfd43a0468d644d4cce7
sumin <grandcias123@gmail.com>
2024-10-20 15:38:06 +0900

    feat: 입출력, 문자열 처리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import calculator.controller.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        StringCalculator calculator = new StringCalculator();
+
+        String input = inputView.getInput();
+        int sum = calculator.calculateInput(input);
+        outputView.printResult(sum);
     }
 }
