ba63be54f2237a4742fc11a12e9e4362082dab03
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-20 15:48:20 +0900

    refactor : 출력부 클래스 분리

---
+++ src/main/java/calculator/CalFinish.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class CalFinish {
+    private static final String FINISH_CAL_MESSAGE = "결과 : ";
+
+    public static void print(int sum) {
+        System.out.println(FINISH_CAL_MESSAGE + sum);
+    }
+}
