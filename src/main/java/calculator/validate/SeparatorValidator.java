48061992208c8e3e725b0f3cb2ecdfc4f9dcccb2
hyosang <billy04100410@gmail.com>
2024-10-20 21:44:45 +0900

    feat(application): add validation to check for presence of separators

--- src/main/java/calculator/validate/SeparatorValidator.java
+++ src/main/java/calculator/validate/SeparatorValidator.java
@@ -12,9 +12,19 @@ public class SeparatorValidator {
         }
     }
 
-    public void validateDuplicateSeparator(String input, List<String> separatorList) {
-        if (separatorList.contains(input)) {
-            throw new IllegalArgumentException("커스텀 구분자가 중복되었습니다.");
+    public void validateHasSeparators(String input, List<String> separatorList) {
+        boolean hasSeparator = false;
+
+        for (String separator : separatorList) {
+            if (input.contains(separator)) {
+                hasSeparator = true;
+                break;
+            }
+        }
+
+        if (!hasSeparator) {
+            throw new IllegalArgumentException("입력에 유효한 구분자가 없습니다.");
         }
     }
+    
 }
