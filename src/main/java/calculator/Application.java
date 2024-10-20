4efa4845d7e345601a1c4ccc45346f48e21361f3
haeun <bhaeun1105@gmail.com>
2024-10-21 01:10:17 +0900

    feat: 문자열 덧셈 계산기 구현
    - 입력값이 비어있는지 확인
    - 입력값이 숫자로 시작하는지, 커스텀 구분자로 시작하는지, 그 외의 문자로 시작하는지 확인
    - 숫자로 시작한다면 기본 구분자(,;)로 문자열을 자른다.
    - 커스텀 구분자 구문(//.\n) 으로 시작한다면 커스텀 구분자를 찾아 문자열을 자른다.
    - 문자열 배열을 숫자로 변경하고 합산한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,35 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.stream.Stream;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+
+        // 시작 문자가 숫자다 => 기본 구분자 사용
+        // 시작 문자가 숫자가 아니다 => 커스텀 구분자 사용
+        if (!input.isBlank()) {
+            char start = input.charAt(0);
+            if ('0' <= start && start <= '9') {
+                String[] nums = input.split("[,:]");
+                printNumberSum(nums);
+            } else if (input.matches("^//.\\\\n.*")) {
+                String spliter = String.valueOf(input.charAt(2));
+                input = input.substring(5);
+                String[] nums = input.split(spliter);
+                printNumberSum(nums);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
+
+    private static void printNumberSum(String[] nums) {
+        int result = Stream.of(nums).map(Integer::parseInt).reduce(0, Integer::sum);
+        System.out.println("결과 : " + result);
+    }
+
 }
