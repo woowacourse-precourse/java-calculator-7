5d0eeb296ee1849feabad09a91d23771ba5d05d5
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 03:54:44 +0900

    feat(NumberStringSplitterImpl): '-'구분자와 음수 문자열이 동시에 오는 예외 처리

--- src/main/java/calculator/model/NumberStringSplitterImpl.java
+++ src/main/java/calculator/model/NumberStringSplitterImpl.java
@@ -16,6 +16,14 @@ public class NumberStringSplitterImpl implements NumberStringSplitter {
         List<String> delimiters = delimiterManager.getDelimiters();
         String delimiterPattern = "[" + String.join("", delimiters).replaceAll("([\\W])", "\\\\$1") + "]";
 
+        if (delimiterPattern.contains("-")) {
+            inputStr = inputStr.replaceAll("--", "-00 ");
+
+            return Arrays.stream(inputStr.split(delimiterPattern))
+                    .map(part -> part.contains("00 ") ? part.replace("00 ", "-") : part)
+                    .toArray(String[]::new);
+        }
+
         return Arrays.stream(inputStr.split(delimiterPattern))
                 .map(String::trim)
                 .toArray(String[]::new);
