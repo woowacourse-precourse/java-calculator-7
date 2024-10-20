b0a3fc9f0c9d4d932c0069969060d2e061dacad5
LeeHaeJun <haejun0410@gmail.com>
2024-10-21 00:59:09 +0900

    refactor: 클래스 이름 변경
    - SplitInterface -> SpliterInterface

--- src/main/java/calculator/split/SplitInterface.java
+++
@@ -1,7 +0,0 @@
-package calculator.split;
-
-import java.util.List;
-
-public interface SplitInterface {
-    List<String> split(String text);
-}
---
+++ src/main/java/calculator/split/SpliterInterface.java
@@ -0,0 +1,7 @@
+package calculator.split;
+
+import java.util.List;
+
+public interface SpliterInterface {
+    List<Integer> split(String text);
+}
