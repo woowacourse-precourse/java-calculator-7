62936888e23bcf76ef6f31bebe359d9b3bc23151
qudcks8084 <qudcks8084@naver.com>
2024-10-20 19:11:05 +0900

    feat(util) : add 1 exception check method
    Detail
    - add checkStringHasRightNumOfTarget to check Wrong Target String

--- src/main/java/calculator/util/ExceptionUtil.java
+++ src/main/java/calculator/util/ExceptionUtil.java
@@ -6,44 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.util.regex.Pattern.*;
+
 public class ExceptionUtil {
-
-    public static void checkParameterIsLiteral(CalculatorDTO calculatorDTO) throws IllegalArgumentException{
-        String str = calculatorDTO.getStr();
-        String delim = calculatorDTO.getDelim();
-        String[] checkList;
-
-        if(calculatorDTO.hasCustomDelim()){
-            String escapeCustomDelimiter = Pattern.quote(delim);
-            checkList = str.split(escapeCustomDelimiter);
-        }else{
-            // Default Delimiter 처리 (StringTokenizer 사용)
-            List<String> tokens = new ArrayList<>();
-            StringTokenizer st = new StringTokenizer(str, delim);
-
-            // 각 토큰을 리스트에 저장
-            while (st.hasMoreTokens()) {
-                tokens.add(st.nextToken());
-            }
-
-            // 리스트를 배열로 변환
-            checkList = tokens.toArray(new String[0]);
-        }
-
-        try{
-            for (String s : checkList) {
-                int num = Integer.parseInt(s);
-                if(num < 0){
-                    throw new IllegalArgumentException("ERR : 잘못된 인자값 입니다.");
-                }
-            }
-        }catch (NumberFormatException e) {
-            throw new IllegalArgumentException("ERR : 잘못된 인자값 입니다.");
-        }
-
-    }
 
     public static void checkParamsIsNotEmpty(String str) throws IllegalArgumentException {
         if(str.startsWith("//")){ // 커스텀 구분자를 가지는 경우
@@ -77,6 +45,52 @@ public class ExceptionUtil {
             }
         }catch (NumberFormatException e) { // 정수가 아닌 경우  Integer.parseInt에서 발생하는 에러
             throw new IllegalArgumentException("ERR : 계산할수 없는 대상이 포함되어 있습니다.");
+        }
+    }
+
+    public static void checkStringHasRightNumOfTarget(CalculatorDTO calculatorDTO) throws IllegalArgumentException{
+        String str = calculatorDTO.getStr();
+        String delim = calculatorDTO.getDelim();
+
+        if(calculatorDTO.hasCustomDelim()){
+            int numOfDelim = 0;
+
+            Pattern p = compile(quote(delim));
+            Matcher m = p.matcher(str);
+
+            while (m.find()) {
+                numOfDelim++;
+            }
+
+            int numOfTarget = str.split(quote(delim)).length;
+
+            if(numOfTarget - numOfDelim != 1 )
+                throw new IllegalArgumentException("ERR : 계산 대상의 개수가 이상합니다.");
+
+        }else{
+            int numOfDelim = 0;
+
+            Pattern p = compile(quote(","));
+            Matcher m = p.matcher(str);
+            while (m.find()) {
+                numOfDelim++;
+            }
+
+            p = compile(quote(":"));
+            m = p.matcher(str);
+            while (m.find()) {
+                numOfDelim++;
+            }
+
+            StringTokenizer st = new StringTokenizer(str, delim);
+            int numOfTarget = 0;
+            while (st.hasMoreTokens()){
+                st.nextToken();
+                numOfTarget++;
+            }
+
+            if(numOfTarget - numOfDelim != 1 )
+                throw new IllegalArgumentException("ERR : 계산 대상의 개수가 이상합니다.");
         }
     }
 }
