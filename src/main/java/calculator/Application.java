56e308b9dc05ff4c3552078f1d32fca5919ae21a
minyeongg <1030pmy@gmail.com>
2024-10-20 17:02:52 +0900

    feat: 입력으로부터 계산 결과 반환하는 기능
    빈 문자열인 경우 0을 반환한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,17 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+    public static int add(String input) {
+        String delimiter = "";
+        String[] numbers;
+        int sum = 0;
+        if(input == null || input.isEmpty() || input.isBlank()) {
+            return 0;
+        } else {
+            return sum;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
