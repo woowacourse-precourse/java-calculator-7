f552bec2fd70233dbcabc7527b9f218c8e588299
minSsan <msp1125@hanyang.ac.kr>
2024-10-21 02:13:50 +0900

    feat(CalculatorManager): 입력 문자열 파싱 및 연산을 수행하는 클래스 추가

---
+++ src/main/java/calculator/CalculatorManager.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import java.util.List;
+
+public class CalculatorManager {
+    private final CalculatorNumberList numberList;
+
+    public CalculatorManager(String input) {
+        final Delimiter delimiter = new CalculatorDelimiter(input);
+        final String regex = delimiter.getDelimiterRegex();
+        final String content = delimiter.getContentWithoutDelimiter();
+        final List<Integer> parsed = CalculatorParser.parseInteger(regex, content);
+        numberList = new CalculatorNumberList(parsed);
+    }
+
+    public int calculateSum() {
+        return numberList.sum();
+    }
+}
