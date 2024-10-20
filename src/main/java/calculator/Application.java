aea3be887165189407e19828695a388bc76f8145
Choi Min Young <codingmy@naver.com>
2024-10-20 23:52:20 +0900

    feat: 신규 구분자 추출 기능 구현
    1개 메서드에 1개 책임 부여 목적으로 커스텀 구분자 지정 여부 확인 메서드와 커스텀 구분자 추출 메서드 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,21 +5,28 @@ import java.io.*;
 public class Application {
     public static void main(String[] args) {
         String inputString = getInputString();
-        String newDelimiter = getNewDelimiter(inputString);
+        String newDelimiter = isContainNewDelimiter(inputString) ? getNewDelimiter(inputString) : null;
         //입력 값 숫자로 처리
         //연산
         //결과 출력
         //System.out.println(output);
     }
 
-    private static String getNewDelimiter(String inputString) {
-        String newDelimiter = null;
-        if(inputString.startsWith("//")){
-            if(!inputString.contains("\\n"))
-                throw new IllegalArgumentException("신규 구분자 지정 문법에 맞지 않습니다.");
-            else{
-                 newDelimiter = inputString.substring(2, inputString.indexOf("\\n"));
+    private static boolean isContainNewDelimiter(String inputString) {
+        if (inputString.startsWith("//")) {
+            if (inputString.contains("\\n")) {
+                return true;
             }
+            throw new IllegalArgumentException("신규 구분자 지정 문법에 맞지 않습니다.");
+        }
+        return false;
+    }
+
+    private static String getNewDelimiter(String inputString) {
+        String newDelimiter;
+        newDelimiter = inputString.substring(2, inputString.indexOf("\\n"));
+        if (newDelimiter.matches(".*[0-9].*")) {
+            throw new IllegalArgumentException("신규 구분자에 숫자를 포함할 수 없습니다.");
         }
         return newDelimiter;
     }
