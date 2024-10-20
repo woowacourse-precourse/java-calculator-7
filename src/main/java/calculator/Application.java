a84364d983d5857cccc45ea7fde016174a4a8a23
KNU-K <knukang334@gmail.com>
2024-10-20 14:41:08 +0900

    refactor(app): Console.close()를 finally 영역에서 호출하도록 변경
    기존의 형식에서는 예기치않은 에러가 발생하면 자원을 닫지않고 끝내게 되었다. 현재 finally를 통해서 이 형식을 잡을 수 있도록 변경함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,11 +6,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String input = Console.readLine();
-        Console.close();
         CalculatorService calculatorService = new CalculatorServiceImpl();
-        Integer result = calculatorService.calculate(input);
-        System.out.println("결과 : " + result);
-
+        try {
+            String input = Console.readLine();
+            Integer result = calculatorService.calculate(input);
+            System.out.println("결과 : " + result);
+        } finally {
+            Console.close();
+        }
     }
 }
