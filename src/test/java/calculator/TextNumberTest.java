11628f3e6767c264b3aeb2f1421abcffe3f77050
RedSunSmile <naver-or@hanmail.net>
2024-10-20 18:36:58 +0900

    입력값테스트와 예외테스트로직

--- src/test/java/calculator/TextNumberTest.java
+++ src/test/java/calculator/TextNumberTest.java
@@ -1,27 +1,64 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class ApplicationTest extends NsTest {
-    @Test
-    void 커스텀_구분자_사용() {
-        assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
-        });
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.*;
+
+class TextNumberTest extends NsTest {
+    TextNumber number;
+
+    public TextNumberTest(){
+
     }
+
+    @BeforeEach
+    public void setNumber() {
+        this.number=new TextNumber();
+
+    }
+    @Test
+    @DisplayName("입력값을 숫자로 바꾸고 더하기")
+    public void calculatingNumberPlus() {
+//        String inputText=Console.readLine();
+          String inputText="123abc";
+          assertNotNull(inputText);
+
+          AtomicInteger sum=new AtomicInteger(0);
+
+          assertSimpleTest(() -> {
+              for (char reverseNum : inputText.toCharArray()) {
+                    if(Character.isDigit(reverseNum))
+                        sum.addAndGet(Character.getNumericValue(reverseNum));
+                }
+                run("//;\\n1");
+                assertThat(calculatingNumberPlus(sum.get())).contains("결과 : " + sum.get());
+            });
+        }
 
     @Test
     void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+//        assertSimpleTest(() -> {
+//            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+//        });
+        assertThatThrownBy(() -> {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다."); // 예외 발생
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 값을 입력하였습니다."); // 메시지 확인
+    }
+
+
+    public String calculatingNumberPlus(int sum){
+        return "결과 : "+sum;
     }
 
     @Override
