cbd019a5c9cc94919d1b3b3b17865daadbf8377a
delena0702 <delena0702@gmail.com>
2024-10-20 15:34:02 +0900

    feat(StringCalculator): 커스텀 구분자를 분리하는 함수 작성
    StringCaculator.getCustomDelimiter():
    커스텀 구분자가 있는 부분 `//[커스텀 구분자]\n`를 읽어 커스텀 구분자를 반환하는 함수를 구현.
    커스텀 구분자가 숫자인 경우, `IllegalArgumentException` 발생.
    
    커스텀 구분자가 2글자 이상이거나, `//[커스텀 구분자]\n` 형태가 2번 이상 반복되는 경우는 이후 값을 계산하는 과정에서 예외처리되어야 함.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,14 +2,47 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     private String userInput;
+    private List<String> delimiters;
+
+    public StringCalculator() {
+        delimiters = new ArrayList<String>(Arrays.asList(",", ":"));
+    }
 
     public void run() {
         userInput = getUserInput();
+        String customDelimiter = getCustomDelimiter(userInput);
+        addCustomDelimiter(customDelimiter);
     }
 
     private String getUserInput() {
         return Console.readLine();
+    }
+
+    private void addCustomDelimiter(String delimiter) {
+        delimiters.add(delimiter);
+    }
+
+    private String getCustomDelimiter(String userInput) {
+        String customDelimiterPattern = "^//(.)\\\\n";
+
+        Pattern pattern = Pattern.compile(customDelimiterPattern);
+        Matcher matcher = pattern.matcher(userInput);
+
+        if (!matcher.find())
+            return null;
+
+        String result = matcher.group(1);
+        if (Pattern.matches("[0-9]", result))
+            throw new IllegalArgumentException("Cannot specify a number with a custom delimiter.");
+
+        return result;
     }
 }
