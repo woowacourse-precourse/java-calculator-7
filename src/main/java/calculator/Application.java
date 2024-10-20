471dfb31fd255001d45a59c8dfb60fc33626a802
leejongyoon1209 <bidakee11@naver.com>
2024-10-21 01:08:05 +0900

    계산기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,11 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("write number. (. or :)");
+        String input = camp.nextstep.edu.missionutils.Console.readLine();
+        int result = Calculate.sum(input);
+        System.out.println("결과 : " + result);
+        /*
         try {
             System.out.println("write number. (. or :)");
             String input = camp.nextstep.edu.missionutils.Console.readLine();
@@ -10,5 +15,7 @@ public class Application {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+
+         */
     }
 }
