39b00b9431e0a7ea736b4a91de0b55c7e6165b2e
chyo1 <hyowon3856@gmail.com>
2024-10-20 13:48:23 +0900

    TEST : addNum 테스트 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,8 +7,21 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
+
 	@Test
 	public void 숫자_덧셈() {
+		//given
+		Calculator calculator = new Calculator();
+		int testNum = 2;
+		//when
+		calculator.addNum(testNum);
+
+		//then
+		assertThat(calculator.getTotalSum()).isEqualTo(2);
+	}
+
+	@Test
+	public void 리스트의_모든_숫자_덧셈() {
 		//given
 		Calculator calculator = new Calculator();
 		List<Integer> nums = new ArrayList<>(List.of(1, 2, 3, 4));
