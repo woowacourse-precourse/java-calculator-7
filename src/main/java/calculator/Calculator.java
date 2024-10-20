b3a95e41c05d168b17f2f9cc8993b9c00f4ea501
jjr7181 <jjr7181@gmail.com>
2024-10-20 22:54:43 +0900

    feat: substitue blank into 0 when splitting numbers

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,6 +6,12 @@ public class Calculator {
             return 0;
         }
 
+        String[] numbers = splitNumbers(input); // 문자열 숫자로 나누기
+        validateNumbers(numbers);  // 유효한 문자인지 검사
+        return sumNumbers(numbers);
+    }
+
+    private String[] splitNumbers(String input) {
         String customDelimiter = extractCustomDelimiter(input); // 커스텀 구분자 추출
         String numbersPart = extractNumbersPart(input); // 커스텀 구분자 지정 문자 제외한 부분 추출
 
@@ -15,13 +21,11 @@ public class Calculator {
         } else {
             regex = "[,:]";
         }
-        String[] numbers = numbersPart.split(regex);
 
-        validateNumbers(numbers);  // 유효한 문자인지 검사
-        return sumNumbers(numbers);
+        return numbersPart.split(regex);
     }
 
-    public String extractCustomDelimiter(String input) {
+    private String extractCustomDelimiter(String input) {
         // 첫 2글자가 "//"로 시작하는지 확인
         if (input.startsWith("//")) {
             int delimiterEndIndex = input.indexOf("\\n");
@@ -34,7 +38,7 @@ public class Calculator {
         return null;
     }
 
-    public String extractNumbersPart(String input) {
+    private String extractNumbersPart(String input) {
         if (input.startsWith("//")) {
             int delimiterEndIndex = input.indexOf("\\n");
             if (delimiterEndIndex != -1 && delimiterEndIndex > 2) {
@@ -44,17 +48,22 @@ public class Calculator {
         return input;
     }
 
-    public void validateNumbers(String[] numbers) {
-        for (String number : numbers) {
-            number = number.trim();
-            // 숫자 && 양수 검사
-            if (!number.matches("\\d+")) {
+    private void validateNumbers(String[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            String number = numbers[i].trim();
+
+            // 빈 문자열일 경우 0으로 치환
+            if (number.isEmpty()) {
+                numbers[i] = "0";
+            }
+            // 숫자 && 0 이상인지 검사
+            if (!numbers[i].matches("\\d+")) {
                 throw new IllegalArgumentException("유효한 숫자가 아닙니다: " + number);
             }
         }
     }
 
-    public int sumNumbers(String[] numbers) {
+    private int sumNumbers(String[] numbers) {
         int sum = 0;
         for (String number : numbers) {
             sum += Integer.parseInt(number.trim());
