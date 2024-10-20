b59af2f1a29b693ba3b5778d99076266014fca71
seongsiyeong <ssy2681q@naver.com>
2024-10-21 05:14:26 +0900

    fix : 요구사항에 맞게 아무 입력을 하지 않을 경우 0을 출력하도록 검증 메서드 제거

--- src/main/java/calculator/UserInputReader.java
+++ src/main/java/calculator/UserInputReader.java
@@ -1,20 +1,11 @@
 package calculator;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import camp.nextstep.edu.missionutils.Console;
 
 public class UserInputReader {
 
     public static String readInput() {
-
-        String input = readLine();
-        validateInput(input);
-
-        return input;
+        return Console.readLine();
     }
 
-    private static void validateInput(String input) {
-        if (input == null || input.isEmpty() || input.isBlank()) {
-            throw new IllegalArgumentException("사용자의 입력이 없거나 공백입니다.");
-        }
-    }
 }
