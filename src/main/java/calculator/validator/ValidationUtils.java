c2b637ba48bc825b77f9428773c5d47d38e0badf
chungjeongsu <wjdwlgh2000@gmail.com>
2024-10-21 00:25:40 +0900

    docs: 문서 PROBLEM1 Validator 수정
    - 설정하지 않은 구분자가 있을 경우 parseLong으로 자동으로 Exception발생

--- src/main/java/calculator/validator/ValidationUtils.java
+++ src/main/java/calculator/validator/ValidationUtils.java
@@ -1,7 +1,5 @@
 package calculator.validator;
 
-import java.util.regex.Matcher;
-
 public class ValidationUtils {
     public static void validateStartWithDoubleSlash(String inputString) {
         if (!inputString.startsWith("//") && !Character.isDigit(inputString.charAt(0))) {
@@ -9,15 +7,7 @@ public class ValidationUtils {
         }
     }
 
-    public static void validateCustomLineFormat(Matcher delimiterStringMatcher) {
-        if (!delimiterStringMatcher.find()) {
-            throw new IllegalArgumentException("커스텀 구분자 입력 형식이 잘못되었습니다.");
-        }
-        validateDuplicatedCharacter(delimiterStringMatcher.group(1));
-        validateIllegalNumericDelimiter(delimiterStringMatcher.group(1));
-    }
-
-    private static void validateDuplicatedCharacter(String delimiterString) {
+    public static void validateDuplicatedCharacter(String delimiterString) {
         StringBuilder checkedDelimiter = new StringBuilder();
         for (char delimiter : delimiterString.toCharArray()) {
             if (!checkedDelimiter.toString().contains(String.valueOf(delimiter))) {
@@ -28,17 +18,11 @@ public class ValidationUtils {
         }
     }
 
-    private static void validateIllegalNumericDelimiter(String delimiterString) {
+    public static void validateIllegalNumericDelimiter(String delimiterString) {
         for (char delimiter : delimiterString.toCharArray()) {
             if (Character.isDigit(delimiter)) {
-                throw new IllegalArgumentException("숫자는 입력되지 않습니다.");
+                throw new IllegalArgumentException("숫자는 커스텀 구분자로 입력되지 않습니다.");
             }
-        }
-    }
-
-    public static void validateNumberLineExist(Matcher numberLineMatcher) {
-        if (!numberLineMatcher.find()) {
-            throw new IllegalArgumentException("숫자 라인이 입력되지 않았습니다.");
         }
     }
 }
