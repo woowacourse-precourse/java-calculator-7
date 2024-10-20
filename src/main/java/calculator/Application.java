550094b3c5f4282a2269578cbfb61c5302badd35
ehgk4245 <ehgkrk14@naver.com>
2024-10-21 03:29:14 +0900

    feat(validateInput): 사용자 입력 유효성 검사 및 전처리 기능, 예외 처리 추가
    -validatePattern메서드로 유효성 검사(양의 정수,패턴) 추가
    -inputIsEmpty메서드로 사용자 입력 공백("")시 처리 추가
    -validateAndPrepareInput메서드로 사용자 입력 전처리 및 유효성 검사 추가
    -processCalculation메서드 전처리 데이터 활용과 예외 발생시 대처하도록 코드 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -13,14 +13,20 @@ public class Application {
         System.out.println(processCalculation(input));
     }
 
-    public static int processCalculation(String input){ //주어진 입력문자열 처리하여 숫자의 합 반환.
-        Matcher matcher = hasCustomDelimiter(input);
-        String[] numbers;
-        if(matcher != null){ //커스텀 구분자가 있을 경우
-            numbers = splitNumbers(input, matcher);
-        } else{ // 커스텀 구분자가 없을 경우
-            numbers = splitNumbers(input);
+    public static int processCalculation(String input){ // 주어진 입력문자열 처리하여 숫자의 합 반환.
+        if(inputIsEmpty(input)){ // 입력이 "" 이면 0반환.
+            return 0;
         }
+        String[] validatedInput;
+        try{
+            validatedInput = validateAndPrepareInput(input); // 사용자 입력 유효성 검사 및 전처리
+        } catch (IllegalArgumentException e){
+            System.err.println(e.getMessage());
+            return 0;
+        }
+        String preparedInput = validatedInput[0]; // 유효성 검사 및 전처리 완료된 input
+        String delimiters = validatedInput[1]; // 전처리 완료된 구분자
+        String[] numbers = splitNumbers(preparedInput, delimiters);
         return calculateSum(numbers);
     }
 
@@ -33,15 +39,8 @@ public class Application {
         return null;
     }
 
-    public static String[] splitNumbers(String input){ // 구분자를 기준으로 숫자로된 문자열 배열 반환.
-        String delimiters = ",:"; // 기본 구분자
-        return input.split("[" + delimiters + "]");
-    }
 
-    public static String[] splitNumbers(String input, Matcher matcher){ // 구분자 + 커스텀 구분자를 기준으로 숫자로된 문자열 배열 반환.
-        String delimiters = ",:"; // 기본 구분자
-        delimiters += matcher.group(1); // 커스텀 구분자 추가
-        input = input.substring(5); // 커스텀 구분자 패턴 제거
+    public static String[] splitNumbers(String input, String delimiters){ // 구분자 + 커스텀 구분자를 기준으로 숫자로된 문자열 배열 반환.
         return input.split("[" + delimiters + "]");
     }
 
@@ -53,4 +52,27 @@ public class Application {
         return sum;
     }
 
+    public static String[] validateAndPrepareInput(String input){ // 사용자 입력 유효성 검사 및 전처리
+        Matcher matcher = hasCustomDelimiter(input);
+        String delimiter = ",|:"; // 기본 구분자
+        if(matcher != null){ //커스텀 구분자가 있을시
+            delimiter += "|" + matcher.group(1); // 커스텀 구분자 추가
+            input = input.substring(5); // 커스텀 구분자 패턴 제거
+            validatePattern(input, delimiter); // 유효성 검사
+            return new String[]{input,delimiter.replace("|","")};
+        }
+        validatePattern(input, delimiter); // 유효성 검사
+        return new String[]{input,delimiter.replace("|","")};
+    }
+
+    public static void validatePattern(String input, String delimiter){ // 입력 패턴 유효성 검사
+        String pattern = "^[0-9]+([" + delimiter + "][0-9]+)*$";
+        if(!input.matches(pattern)){ // 유효하지 않을시 예외 발생
+            throw new IllegalArgumentException("IllegalArgumentException");
+        }
+    }
+
+    public static boolean inputIsEmpty(String input){ // 사용자 입력이 "" 인지 판단
+        return input == null || input.trim().isEmpty();
+    }
 }
