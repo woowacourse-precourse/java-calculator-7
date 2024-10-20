2b5e20073ebda0b6996a40a86be88b30580ac692
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:19:49 +0900

    :sparkles: feat(DelimiterExtractor): 기본 구분자(쉼표, 콜론)를 구분자로 반환

--- src/main/java/calculator/model/DelimiterExtractor.java
+++ src/main/java/calculator/model/DelimiterExtractor.java
@@ -1,4 +1,9 @@
 package calculator.model;
 
 public class DelimiterExtractor {
+    private static final String DEFAULT_DELIMITERS = ",|:";
+
+    public String extractDelimiters(String input) {
+        return DEFAULT_DELIMITERS;
+    }
 }
