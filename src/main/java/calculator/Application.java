8d650ff3c83ae1651078cef1ceff0bc3a48078d1
mskim <msung98@naver.com>
2024-10-20 23:54:03 +0900

    feat: complete the second feature implementation

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,16 +1,29 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();  // 사용자 입력을 받음
         System.out.println(input);
+
         int sum = 0;
+        String spliter = "[:|,]";
+
+        if(input.startsWith("//")){
+            input = input.replace("\\n", "\n");
+            int ender = input.indexOf("\n");
+            if(ender != -1){
+                String newSpliter = input.substring(2, ender);
+                spliter = spliter.replace("]", "") + "|" +  newSpliter + "]";
+                input = input.substring(ender + 1);
+            }
+        }
+
         if(!(input.isEmpty())){
-            String[] nums = input.split("[:,,]");
+            String[] nums = input.split(spliter
+            );
             for (String num : nums){
                 sum += Integer.parseInt(num);
             }
