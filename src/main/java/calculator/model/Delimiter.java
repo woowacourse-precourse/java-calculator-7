6a427a6fc4365d46081dc70e2bcaf94087d444f6
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-20 14:56:13 +0900

    refactor : 메서드 분리

--- src/main/java/calculator/model/Delimiter.java
+++ src/main/java/calculator/model/Delimiter.java
@@ -11,10 +11,6 @@ public class Delimiter {
     private static final Pattern pattern = Pattern.compile("//(.)\n(.*)");
     private static Set<String> delimiter;
 
-    public static String[] splitWithDelimiter(String expression) {
-        return expression.split(",|:");
-    }
-
     public Delimiter() {
         delimiter = new HashSet<>();
         delimiter.add(",");
@@ -24,19 +20,27 @@ public class Delimiter {
     public static String[] splitWithCustomDelimiter(String expression) {
         Matcher m = pattern.matcher(expression.replace("\\n", "\n"));
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            delimiter.add(customDelimiter);
-            String delimiterToUse = customAndBasicDelimiter();
-            String[] tokens = m.group(2).split(delimiterToUse);
-            return tokens;
+            String[] splitedExpression = splitExpression(m);
+            return splitedExpression;
         }
         return splitWithDelimiter(expression);
+    }
+
+    private static String[] splitExpression(Matcher m) {
+        String customDelimiter = m.group(1);
+        delimiter.add(customDelimiter);
+        String delimiterToUse = customAndBasicDelimiter();
+        String[] splitedExpression = m.group(2).split(delimiterToUse);
+        return splitedExpression;
+    }
+
+    public static String[] splitWithDelimiter(String expression) {
+        return expression.split(",|:");
     }
 
     private static String customAndBasicDelimiter() {
         return delimiter.stream()
                 .collect(Collectors.joining("|"));
     }
-
 
 }
