e0d36dfadd3b40644c426d4b903c81016bf023dd
hojooo <ghwn5833@gmail.com>
2024-10-21 03:31:17 +0900

    feat: add emptyStringCheck, numberCheck, delimiterCheck, lastCharCheck, customDelimiterNumCheck in validChecker

--- src/main/java/calculator/StringSeparator.java
+++ src/main/java/calculator/StringSeparator.java
@@ -1,32 +1,54 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringSeparator {
+    private final ValidChecker validChecker = new ValidChecker();
+
     private static final String DEFAULT_DELIMITER = "[,:]";
-    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\\\\n(.*)");
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\\\\n(.*)");      // 정규 표현식을 통해 커스텀 구분자의 길이가 1을 넘을 수 없음
 
     public String[] parseNumbers(String input) {
+        validChecker.emptyStringCheck(input);
+
         if (input.startsWith("//")) {
             return parseWithCustomDelimiter(input);
         }
 
-        return input.split(DEFAULT_DELIMITER);
+        String[] splitStrings = input.split(DEFAULT_DELIMITER);
+        validChecker.numberCheck(splitStrings);     // 숫자가 아닌 문자 확인
+        validChecker.delimiterCheck(splitStrings);  // 맨 끝에 구분자 혹은 구분자가 연속해서 두 개인 경우 (비어있는 문자열이 분리된 문자열에 포함되어 있음)
+        validChecker.lastCharCheck(input, DEFAULT_DELIMITER);
+
+        return Arrays.stream(splitStrings)
+                .map(String::trim)
+                .toArray(String[]::new);
     }
 
     private String[] parseWithCustomDelimiter(String input) {
         Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
 
-        if (!matcher.find()) {
+        if (!matcher.find()) {      // 잘못된 커스텀 구분자 형식 처리
             throw new IllegalArgumentException();
         }
 
         String delimiter = matcher.group(1);
+        validChecker.customDelimiterNumCheck(delimiter);
+
         String numbers = matcher.group(2);
 
         String allDelimiter = String.format("%s|%s", DEFAULT_DELIMITER, delimiter);
+        System.out.println(allDelimiter);
+        if ("-".equals(delimiter) && numbers.contains("-")) {    // 커스텀 구분자가 '-'이면 음수 불가능
+            throw new IllegalArgumentException();
+        }
 
-        return numbers.split(allDelimiter);
+        String[] splitStrings = numbers.split(allDelimiter);
+        validChecker.numberCheck(splitStrings);     // 숫자가 아닌 문자 확인
+        validChecker.delimiterCheck(splitStrings);  // 맨 끝에 구분자 혹은 구분자가 연속해서 두 개인 경우 (비어있는 문자열이 분리된 문자열에 포함되어 있음)
+        validChecker.lastCharCheck(input, allDelimiter);
+        return splitStrings;
     }
 }
---
+++ src/main/java/calculator/ValidChecker.java
@@ -0,0 +1,47 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+public class ValidChecker {
+    public void emptyStringCheck(String input) {      // 빈 문자열 처리
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void numberCheck(String[] input) {     // 숫자가 아닌 문자 확인
+        Optional<String> findChar = Arrays.stream(input)
+                .filter(s -> !s.matches("-?\\d+"))
+                .findFirst();
+
+        if (findChar.isPresent()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void delimiterCheck(String[] input) {   // 구분자가 연속해서 두 개인 경우 (비어있는 문자열이 분리된 문자열에 포함되어 있음)
+        Optional<String> findEmpty = Arrays.stream(input)
+                .filter(String::isEmpty)
+                .findFirst();
+
+        if (findEmpty.isPresent()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void lastCharCheck(String input, String delimiter) {   // 맨 끝 문자가 구분자인 경우
+        String pattern = delimiter + "$";
+        System.out.println(pattern);
+        if (input.matches(".*" + pattern)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void customDelimiterNumCheck(String delimiter) {   // 커스텀 구분자가 숫자인 경우
+        if (delimiter.matches("-?\\d+")) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
