e0c1b15acd4d83463974223a834923f0dea2f482
wjdtjdgns <person3275@gmail.com>
2024-10-20 21:54:55 +0900

    fix(view): 출력 양식 변경

--- src/main/java/calculator/view/ResultView.java
+++ src/main/java/calculator/view/ResultView.java
@@ -1,7 +1,7 @@
 package calculator.view;
 
 public class ResultView {
-    private static final String RESULT_MESSAGE = "결과: ";
+    private static final String RESULT_MESSAGE = "결과 : ";
 
     public void printResult(int result) {
         System.out.printf("%s%d%n", RESULT_MESSAGE, result);
