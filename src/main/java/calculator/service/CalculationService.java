6f7972d4bc8750e1baaaa6014d1f91a412a6d787
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-20 18:43:39 +0900

    feat : service.CalculationService 생성

--- src/main/java/calculator/service/CalculationService.java
+++ src/main/java/calculator/service/CalculationService.java
@@ -1,5 +1,17 @@
 package calculator.service;
 
-public class CalculationService {
+import calculator.domain.InputHandler;
+import calculator.domain.Calculator;
+import calculator.domain.SimpleCalculator;
 
+import java.util.List;
+
+public class CalculationService {
+    private final Calculator calculator = new SimpleCalculator();
+
+    public Number calculate(String input) {
+        InputHandler inputHandler = new InputHandler(input);
+        List<Double> numbers = inputHandler.getParsedNumbers();
+        return calculator.calculateSum(numbers);
+    }
 }
