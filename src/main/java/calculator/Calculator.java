72544657a2682b757d9f23d530d5347812139cda
mins-git <m2insapp@gmail.com>
2024-10-20 17:15:04 +0900

    feat(Calculator.java): 커스텀 구분자 중간에 있는경우 로직 추가 # 2
    - parseInput()메서드가 커스텀 구분자를 구분해서 basicDelimiter에 넣음
    - splitAndSum() 메서드를 통해 구분자를 나누고 sum함.
    - commit을 쓰며 아래 문제 발생 할수도있음 예상됨.
    1. 커스텀 구분자가 여러개인 경우,
    2. 커스텀 구분자 사이에 ","와 ";"도 있을경우
    3. 커스텀 구분자가 여러개면서 ","와 ";"도 섞일경우
    4. 커스텀 구분자와 기본 구분자가 전혀 없을수도있음.
    5. 커스텀 구분자만 있을수도있음.
    
    예상되는 위의 엣지 케이스 필터해야됨.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,6 +3,7 @@ package calculator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
@@ -13,17 +14,33 @@ public class Calculator {
     }
 
 
-
     private void parseInput(String input){
-        // 쉼표와 콜론 제거해서 list에 추가하기.
-        String cleanedInput = input.replace(",", "").replace(";", "");
 
+        List<String> delimiters = new ArrayList<>();
+        String basicDelimiter = ",|;"; // 기본 구분자 2가지
+        String onlyNumbers = input; // 숫자 구분
 
+        int customDelimiterStart = input.indexOf("//");
+        int customDlimiterEnd = input.indexOf("\n");
 
-        for (char c :cleanedInput.toCharArray()) {
-            result += Character.getNumericValue(c);
+        if (customDelimiterStart != -1 && customDlimiterEnd != -1 && customDlimiterEnd > customDelimiterStart){
+            basicDelimiter = input.substring(customDelimiterStart + 2, customDlimiterEnd);
+            onlyNumbers = input.substring(customDlimiterEnd+1);
+        }
+
+        splitAndSum(basicDelimiter, onlyNumbers);
+    }
+
+    private void splitAndSum(String basicDelimiter, String onlyNumbers){
+        String[] splitNumbers = onlyNumbers.split(Pattern.quote(basicDelimiter));
+
+        for (String num : splitNumbers){
+            if (!num.isEmpty()){
+                result += Integer.parseInt(num);
+            }
         }
     }
+
 
     public int getresult() {
         return result;
