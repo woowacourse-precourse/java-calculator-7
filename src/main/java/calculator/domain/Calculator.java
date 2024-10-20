738f9b6c5a452d879d892323077ffd87dc3dd860
jayjae97 <skyroof97@gmail.com>
2024-10-20 12:41:58 +0900

    [FEAT] custom delimiter Exception 처리, input값 음수 Exception 처리

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -6,6 +6,9 @@ public class Calculator {
     private String expression;
 
     public Calculator(String expression) {
+        if(expression == null || expression.isEmpty()){
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
         this.expression = expression;
     }
 
@@ -31,7 +34,11 @@ public class Calculator {
             int startIdx = expression.indexOf("//");
             int endIdx = expression.indexOf("\\n");
 
+            validateCustomDelimiter();
+
             String customDelimiter = expression.substring(startIdx + 2, endIdx);
+
+            validateCustomDelimiterValue(customDelimiter);
 
             String beforeCustomDelimiter = expression.substring(0, startIdx);
             String afterCustomDelimiter = expression.substring(endIdx + 2);
@@ -41,11 +48,38 @@ public class Calculator {
         }
     }
 
+    // 커스텀 구분자가 잘못되었는지 확인
+    private void validateCustomDelimiter() {
+        if (expression.startsWith("//")) {
+            int startIdx = expression.indexOf("//");
+            int endIdx = expression.indexOf("\\n");
+
+            if (startIdx == -1 || endIdx == -1) {
+                throw new IllegalArgumentException("커스텀 구분자가 잘못되었습니다.");
+            }
+        }
+    }
+
+    // 커스텀 구분자를 추출했을 때, 그 구분자에 숫자가 포함되어있거나, 비어있는 경우 예외처리
+    private void validateCustomDelimiterValue(String customDelimiter) {
+        if (customDelimiter.isEmpty()) {
+            throw new IllegalArgumentException("커스텀 구분자가 비어있습니다.");
+        }
+        if (customDelimiter.matches(".*\\d.*")) {
+            throw new IllegalArgumentException("커스텀 구분자에 숫자가 포함되어 있습니다.");
+        }
+    }
+
     public int calculate() {
         String[] strNumbers = getStrNumbers();
         int sum = 0;
+        // 음수가 들어올 경우 예외처리
         for (String strNumber : strNumbers) {
-            sum += Integer.parseInt(strNumber);
+            int number = Integer.parseInt(strNumber);
+            if (number < 0) {
+                throw new IllegalArgumentException("음수가 입력되었습니다.");
+            }
+            sum += number;
         }
         return sum;
     }
