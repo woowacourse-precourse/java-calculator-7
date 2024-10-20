2de316c591a78f13709dcb337ebf1afae3067fab
sinji <tobeabestns@naver.com>
2024-10-20 21:46:13 +0900

    feat: 고정 구분자 지정 및 커스텀 구분자 추가 기능, 구분자로 구분한 숫자를 모아주는 기능

--- src/main/java/domain/Separator.java
+++ src/main/java/domain/Separator.java
@@ -1,4 +1,68 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Separator {
+    private final List<String> separatorCollection;
+
+    public Separator(List<String> separatorCollection) {
+        this.separatorCollection = new ArrayList<>(separatorCollection);
+        defaultSeparator();
+    }
+
+    private void defaultSeparator() {
+        if (!separatorCollection.contains(",")) {
+            separatorCollection.add(",");
+        }
+        if (!separatorCollection.contains(":")) {
+            separatorCollection.add(":");
+        }
+    }
+
+    public void findAndAddSeparator(String inputString) {
+        Pattern pattern = Pattern.compile("/\\/(.*?)\\n");
+        Matcher matcher = pattern.matcher(inputString);
+        while (matcher.find()) {
+            String separator = matcher.group(1);
+            if (!separatorCollection.contains(separator)) {
+                separatorCollection.add(separator);
+            }
+        }
+        defaultSeparator();
+    }
+
+    public List<Integer> extractionNumbers(String inputString) {
+        List<Integer> numberCollection = new ArrayList<>();
+
+        StringBuilder currentNumber = new StringBuilder();
+        boolean isSeparator;
+
+        for (int i = 0; i < inputString.length(); i++) {
+            char currentChar = inputString.charAt(i);
+            isSeparator = false;
+            for (String separator : separatorCollection) {
+                isSeparator = isSeparator || separator.indexOf(currentChar) != -1;
+            }
+            if (isSeparator) {
+                if (!currentNumber.isEmpty()) {
+                    numberCollection.add(Integer.parseInt(currentNumber.toString()));
+                    currentNumber.setLength(0);
+                }
+            }
+            if (!isSeparator) {
+                currentNumber.append(currentChar);
+            }
+        }
+        if (!currentNumber.isEmpty()) {
+            numberCollection.add(Integer.parseInt(currentNumber.toString()));
+        }
+        return numberCollection;
+    }
+
+    public List<String> getSeparatorCollection() {
+        return separatorCollection;
+    }
 }
