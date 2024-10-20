ee3e67effc8c6c94a5ab935adc5f3e64cfcd407e
Chan Mi <rkdcksal02@gmail.com>
2024-10-21 00:14:58 +0900

    feat(utils): StringParser에 유효성 검사 추가

--- src/main/java/calculator/utils/StringParser.java
+++ src/main/java/calculator/utils/StringParser.java
@@ -1,7 +1,15 @@
 package calculator.utils;
 
+import calculator.model.InputValidator;
+
 public class StringParser {
+    private final InputValidator validator;
+
+    public StringParser() {
+        this.validator = InputValidator.getInstance();
+    }
     public int[] parse(String input) {
+        validator.validate(input);
         String[] parts = input.split(",");
         int[] numbers = new int[parts.length];
         for (int i = 0; i < parts.length; i++) {
