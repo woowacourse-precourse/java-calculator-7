ef2606d1f39dc948deb6979022ab8b830db2f61c
gahyeon <1000ji1@naver.com>
2024-10-20 21:21:26 +0900

    feat: 문자열 앞부분에 커스텀 구분자를 지정했는지 확인

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,16 +1,39 @@
 package calculator.service;
 
 import calculator.utils.Validator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CalculatorService {
+    private static final Pattern CUSTOM_SEPARATOR_REGEX = Pattern.compile("//(.*)\\\\n(.*)");
     private String[] tokens;
 
     public void separate(String[] separators, String expression) {
         String cleanedExpression = expression.replaceAll("\\s+", "");
-        this.tokens = cleanedExpression.split(String.join("|", separators));
-        validateIsNumber(tokens);
-        validateIsPositiveNumber(tokens);
+        customSeparate(cleanedExpression);
+
+//        this.tokens = cleanedExpression.split(String.join("|", separators));
+//        validateIsNumber(tokens);
+//        validateIsPositiveNumber(tokens);
     }
+
+    private void customSeparate(String expression) {
+        Matcher matcher = CUSTOM_SEPARATOR_REGEX.matcher(expression);
+        if (hasCustomSeparator(expression, matcher)) {
+
+        }
+
+    }
+
+
+    private boolean hasCustomSeparator(String expression, Matcher matcher) {
+        if (expression.startsWith("//") && matcher.find()) {
+            return true;
+        }
+
+        return false;
+    }
+
 
     private void validateIsNumber(String[] tokens) {
         Validator.isNumber(tokens);
