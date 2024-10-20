9f0baaaaf8aa3294b443eef17c12bbec07b71015
cyunlee <siyunkr@gmail.com>
2024-10-21 03:02:22 +0900

    refactor(methods): 하나의 메서드가 하나의 역할을 하도록 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,7 +8,6 @@ public class Application {
     private static final String CUSTOM_DELIMITER_BACK = "\\n";
     private static final String BASIC_DELIMITER = "[,:]";
 
-
     private static void checkInputValid(String str) {
         for (char x : str.toCharArray()) {
             if (!Character.isDigit(x)) {
@@ -33,36 +32,59 @@ public class Application {
         }
     }
 
+    private static boolean isCustomInput(String str) {
+        return str.contains(CUSTOM_DELIMITER_FRONT) && str.contains(CUSTOM_DELIMITER_BACK);
+    }
+
+    private static boolean isBasicInput(String str) {
+        return str.contains(",") || str.contains(":");
+    }
+
+    private static String parseCustomDelimiter(String str) {
+        return str.substring(str.indexOf(CUSTOM_DELIMITER_FRONT) + 2, str.indexOf(CUSTOM_DELIMITER_BACK));
+    }
+
+    private static String deleteCustomDelimiter(String str) {
+        return str.substring(str.indexOf(CUSTOM_DELIMITER_BACK) + 2);
+    }
+
+    private static String replaceCustomToBasic(String delimiter, String str) {
+        return str.replace(delimiter, ",");
+    }
+
+    private static int handleCustomInput(String str) {
+        checkCustomStyle(str);
+        String custom = parseCustomDelimiter(str);
+        checkCustomEmpty(custom);
+        String newInput = deleteCustomDelimiter(str);
+        String replacedInput = replaceCustomToBasic(custom, newInput);
+
+        return calculateSum(replacedInput);
+    }
+
+    private static int calculateSum(String str) {
+        String[] numbers = str.split(BASIC_DELIMITER);
+        int sum = 0;
+
+        for (String number : numbers) {
+            if (!number.isEmpty()) {
+                checkInputValid(number);
+                sum += Integer.parseInt(number);
+            }
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         int sum = 0;
 
-        if (input.contains(CUSTOM_DELIMITER_FRONT) && input.contains(CUSTOM_DELIMITER_BACK)) {
-            checkCustomStyle(input);
-            String custom = input.substring(input.indexOf(CUSTOM_DELIMITER_FRONT) + 2,
-                    input.indexOf(CUSTOM_DELIMITER_BACK));
-            checkCustomEmpty(custom);
-            String newInput = input.substring(input.indexOf(CUSTOM_DELIMITER_BACK) + 2);
-            String replacedInput = newInput.replace(custom, ",");
-
-            String[] strArrCustom = replacedInput.split(BASIC_DELIMITER);
-
-            for (String str : strArrCustom) {
-                if (!str.isEmpty()) {
-                    checkInputValid(str);
-                    sum += Integer.parseInt(str);
-                }
-            }
-        } else if (input.contains(",") || input.contains(":")) {
-            String[] strArrBasic = input.split(BASIC_DELIMITER);
-
-            for (String str : strArrBasic) {
-                if (!str.isEmpty()) {
-                    checkInputValid(str);
-                    sum += Integer.parseInt(str);
-                }
-            }
+        if (isCustomInput(input)) {
+            sum = handleCustomInput(input);
+        } else if (isBasicInput(input)) {
+            sum = calculateSum(input);
         } else if (input.isEmpty()) {
             sum = 0;
         } else if (input.matches("[0-9]+")) {
