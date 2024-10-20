7f161043a4fcf7506a398d9165160c3053f318e9
je488 <pje488@naver.com>
2024-10-21 00:23:20 +0900

    feat: 문자열을 분리하기 위해 구분자들을 모아놓은 문자열을 생성하는 메서드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,19 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.StringTokenizer;
 
 public class Application {
+    public static String makeSeperators(String str) {
+        String seperators = ",:";
+        // 커스텀 구분자가 있는 경우, 커스텀 구분자를 찾아 문자열을 분리할 구분자들(seperators)에 추가
+        if (str.contains("//") && str.contains("\\n")) {
+            int idx = str.indexOf("\\n");
+            seperators += str.substring(2, idx);
+            str = str.substring(idx + 2);
+        }
+        return seperators;
+    }
+
     //기본 구분자와 커스텀 구분자를 제외한 다른 구분자를 포함하고 있는지 검사
     public static boolean isInvalidSeperator(String[] nums) {
         for (String num : nums) {
@@ -48,23 +58,6 @@ public class Application {
             throw new IllegalArgumentException("숫자를 포함하여 입력해 주세요.");
         }
 
-        // 기본 구분자가 있는 경우 기본 구분자를 기준으로 문자열 분리하기
-        if (str.contains(",") || str.contains(":")) {
-            String[] nums = str.split(",|:");
-        }
-
-        // 커스텀 구분자가 있는 경우 커스텀 구분자를 기준으로 문자열 분리하기
-        if (str.contains("//") && str.contains("\\n")) {
-            int idx = str.indexOf("\\n");
-            String custom = str.substring(2, idx);
-            str = str.substring(idx + 2);
-            StringTokenizer st = new StringTokenizer(str, custom);
-            String[] nums = new String[st.countTokens()];
-            int i = 0;
-            while (st.hasMoreTokens()) {
-                nums[i] = st.nextToken();
-                i++;
-            }
-        }
+        String seperators = makeSeperators(str);
     }
 }
