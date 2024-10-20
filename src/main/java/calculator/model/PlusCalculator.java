2eb03ac084486948c817e40f363ae980f7e83016
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-20 21:31:38 +0900

    refactor: Operator 인터페이스 생성자 주입

--- src/main/java/calculator/model/PlusCalculator.java
+++ src/main/java/calculator/model/PlusCalculator.java
@@ -3,6 +3,13 @@ package calculator.model;
 import java.util.List;
 
 public class PlusCalculator extends Calculator {
+
+    private final Operator operator;
+
+    public PlusCalculator(Operator operator) {
+        this.operator = operator;
+    }
+
     @Override
     public int calculate(List<Integer> numbers) {
         return numbers.stream()
