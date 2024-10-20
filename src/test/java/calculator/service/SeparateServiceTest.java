bd6bc6b97966ba57a3a844e47159911590b96d3a
digitpic <jkw5033@gmail.com>
2024-10-20 15:11:22 +0900

    test(separate-service-test): separate service 에 대한 테스트 작성

---
+++ src/test/java/calculator/service/SeparateServiceTest.java
@@ -0,0 +1,30 @@
+package calculator.service;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SeparateServiceTest {
+    private static SeparateService separateService;
+
+    @BeforeAll
+    static void setUp() {
+        separateService = new SeparateService();
+    }
+
+    @Test
+    @DisplayName("구분자를 기준으로 토큰을 분리하는 데 성공한다.")
+    void separate() {
+        // given
+        String input = "1,2,3";
+        String[] expected = input.split(",");
+
+        // when
+        String[] real = separateService.separate(input);
+
+        // then
+        assertThat(real).isEqualTo(expected);
+    }
+}
