2a1249e4d48f690306d8f9dbdfc15cbf26340885
sanikani <sanikani@naver.com>
2024-10-20 20:29:18 +0900

    refactor: 입력 문자열 처리 기능 분리
    - Calculator 클래스에서 진행한 입력 문자열을 처리하는 로직을 InputStringParser 클래스를 정의하여 기능을 분리
    - 이제 Calculator는 입력을 받아 처리된 값을 더하는 역할만 수행

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,42 +1,21 @@
 package calculator;
 
 import java.util.Arrays;
-import java.util.regex.Pattern;
 
 public class Calculator {
-    private String delimiterPattern = "[,:]";
+    private final InputStringParser inputStringParser;
 
-    private static final Calculator instance = new Calculator();
-
-    private Calculator() {
+    public Calculator() {
+        inputStringParser = new InputStringParser();
     }
 
-    public static Calculator getInstance() {
-        return instance;
-    }
-
-    public int processString(String input) {
-        int sum = 0;
-        input = input.trim();
+    public int calculate(String input) {
+        input = inputStringParser.processInputString(input);
         if (input.isEmpty()) {
-            return sum;
+            return 0;
         }
-        if (input.startsWith("//")) {
-            input = processCustomDelimiter(input);
-        }
-        String[] numbers = input.split(delimiterPattern);
-        InputValidator.validateNumbers(numbers);
-        return Arrays.stream(numbers)
+        return Arrays.stream(inputStringParser.parseAndSplit(input))
                 .mapToInt(Integer::parseInt)
                 .sum();
-    }
-
-    public String processCustomDelimiter(String input) {
-        InputValidator.validateCustomDelimiterFormat(input);
-        int delimiterIndex = input.indexOf("\\n");
-        String customDelimiter = input.substring(2, delimiterIndex);
-        InputValidator.validateCustomDelimiter(customDelimiter);
-        delimiterPattern = Pattern.quote(customDelimiter) + "|" + delimiterPattern;
-        return input.substring(delimiterIndex + 2);
     }
 }
---
+++ src/main/java/calculator/InputStringParser.java
@@ -0,0 +1,44 @@
+package calculator;
+
+import java.util.regex.Pattern;
+
+public class InputStringParser {
+
+    public static final String CUSTOM_DELIMITER_END = "\\n";
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final int CUSTOM_DELIMITER_START_INDEX = 2;
+    private static final int CUSTOM_DELIMITER_END_INDEX = 5;
+
+    private String delimiterPattern = DEFAULT_DELIMITER;
+
+    public String processInputString(String input) {
+        input = input.trim();
+        if (input.isEmpty()) {
+            return input;
+        }
+        if (input.startsWith("//")) {
+            input = processCustomDelimiter(input);
+        }
+        return input;
+    }
+
+    public String processCustomDelimiter(String input) {
+        InputValidator.validateCustomDelimiterFormat(input);
+        int delimiterIndex = input.indexOf(CUSTOM_DELIMITER_END);
+        String customDelimiter = input.substring(CUSTOM_DELIMITER_START_INDEX, delimiterIndex);
+        InputValidator.validateCustomDelimiter(customDelimiter);
+        delimiterPattern = Pattern.quote(customDelimiter) + "|" + delimiterPattern;
+        return input.substring(CUSTOM_DELIMITER_END_INDEX);
+    }
+
+    public String[] split(String input) {
+        String[] numbers = input.split(delimiterPattern);
+        InputValidator.validateNumbers(numbers);
+        return numbers;
+    }
+
+    public String[] parseAndSplit(String input) {
+        input = processInputString(input);
+        return split(input);
+    }
+}
