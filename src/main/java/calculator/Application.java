0cf4ef8de14e9310746a16368b93fad63d7dc926
dfb5361@gmail.com <dfb5361@gmail.com>
2024-10-21 01:24:51 +0900

    feat(Application): implement custom delimiter support for string addition calculator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,6 +9,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
 
         ArrayList<Character> delimiters = new ArrayList<>();
         ArrayList<Integer> numbers = new ArrayList<>();
@@ -17,8 +18,37 @@ public class Application {
         delimiters.add(':');
 
         try{
-            System.out.print("덧셈할 문자열을 입력해 주세요.");
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
             String input = br.readLine();
+
+            if (input.isEmpty()){
+                System.out.println(result);
+                return;
+            }
+
+            boolean prev = true;
+            int number = 0;
+            int pos = 0;
+            boolean flag = false;
+
+            if (input.charAt(0) == '/'){
+                flag = true;
+                if (input.charAt(1) == '/'){
+                    if (input.charAt(3) == '\\') {
+                        if (input.charAt(4) == 'n') {
+                            delimiters.add(input.charAt(2));
+                            pos = 5;
+                            flag = false;
+                        }
+                    }
+                }
+            }
+
+            if (flag){
+                throw new IllegalArgumentException();
+            }
+
+
         } catch (IOException e) {
             throw new IllegalArgumentException();
         }
