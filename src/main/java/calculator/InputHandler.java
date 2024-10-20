28712dbc41697dc27d674971db372b524fde0c65
2jin2.1031 <2jin2.1031@gmail.com>
2024-10-20 21:15:12 +0900

    style: 전체 코드 스타일 포맷 일관성

--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -5,6 +5,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import calculator.view.InputPrint;
 
 public class InputHandler {
+
     public String getUserInput() {
         InputPrint.printInput();
         return readLine();
--- src/main/java/calculator/OutputHandler.java
+++ src/main/java/calculator/OutputHandler.java
@@ -3,6 +3,7 @@ package calculator;
 import calculator.view.OutputPrint;
 
 public class OutputHandler {
+
     public void printResult(int result) {
         OutputPrint.printResult(result);
     }
