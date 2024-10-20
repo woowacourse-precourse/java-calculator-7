f6463c40b4220a1a343b2389ee06af60112b2f37
kyunghoon01 <sports0124@naver.com>
2024-10-20 18:30:47 +0900

    feat: implement main() method
    main() 메소드를 통해 사용자 입력을 받고, 커스텀 구분자 처리를 포함한
    문자열 덧셈 계산 기능 구현.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,34 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    static StringBuilder sb;
+    static String str;
+    static String regex;
+    static String[] numbers;
+    static long result;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        sb = new StringBuilder();
+        regex = "[,:]";
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        str = Console.readLine();
+        sb.append("결과 : ");
+
+        if (checkCustom(str)) {
+            addCustom(regex, str);
+        }
+
+        if (str.equals("")) {
+            sb.append(0);
+        } else {
+            numbers = str.split(regex);
+            result = addSum(numbers);
+            sb.append(result);
+        }
+        System.out.print(sb);
     }
 }
