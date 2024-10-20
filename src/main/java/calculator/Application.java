7e9b9a7a55e37ae2c902a888e540b5af1e99d3d9
spotydol7 <spotydol7@gmail.com>
2024-10-21 01:18:54 +0900

    feat: 프로그램 시작점 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,9 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-
+        String input = InputView.getInput();
+        Calculator calculator = new Calculator();
+        int result = calculator.add(input);
+        OutputView.printResult(result);
     }
 }
