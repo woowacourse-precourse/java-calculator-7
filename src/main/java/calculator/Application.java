4ad9034264623314d63d7485d8541808726fe063
hojooo <ghwn5833@gmail.com>
2024-10-21 04:33:57 +0900

    feat: add number processing feature

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,10 +2,17 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-
 public class Application {
+    private static final StringSeparator separator = new StringSeparator();
+    private static final Calculator calculator = new Calculator();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        String[] numbers = separator.parseNumbers(input);
+
+        System.out.println("결과 : " + calculator.calculate(numbers));
     }
 }
