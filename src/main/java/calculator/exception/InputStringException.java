41d85d2c9bb15cd3d2c249f4a898e2b3b875be26
etd93 <etd93@naver.com>
2024-10-20 18:17:53 +0900

    [refactor] InputStringException 클래스, 예외 처리 문구 추가 및 메서드 분리

--- src/main/java/calculator/exception/InputStringException.java
+++ src/main/java/calculator/exception/InputStringException.java
@@ -14,7 +14,6 @@ public class InputStringException {
             extractService.extractDelimeter(inputString, delimiters);
         }
         validateInputInStandardFormat(delimiters, inputString);
-
         return checkIfNumeric(inputString, numbers);
     }
 
@@ -22,11 +21,11 @@ public class InputStringException {
         int startIdx = inputString.indexOf("//");
         int endIdx = inputString.lastIndexOf("\\n");
         if(startIdx == -1 || endIdx == -1){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("커스텀 지정방식이 잘못됐습니다.");
         }
         String checkInputString = inputString.substring(startIdx+2, endIdx);
         if(!(startIdx ==0 && endIdx == 3 && checkInputString.length()==1 && !Character.isDigit(checkInputString.charAt(0)))){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("커스텀 지정방식이 잘못됐습니다.");
         }
     }
 
@@ -37,33 +36,47 @@ public class InputStringException {
         LinkedList<Character> delimitersList = delimiters.getDelimeterCollections();
         for(int i=0;i<inputString.length();i++){
             if(Character.isDigit(inputString.charAt(i))) continue;
-            if(!delimitersList.contains(inputString.charAt(i))){
-                throw new IllegalArgumentException("");
-            }
-            if(!(i>0 && i<inputString.length()-1)){
-                throw new IllegalArgumentException("");
-            }
-            if(!(Character.isDigit(inputString.charAt(i-1)) && Character.isDigit(inputString.charAt(i+1)))){
-                throw new IllegalArgumentException("");
-            }
+            validateIfCharIncludedInCollections(delimitersList, inputString, i);
+            validateCharInBetweenStartAndEnd(inputString, i);
+            validateIfDelimitersInBetweenNumbers(inputString, i);
+        }
+    }
+    public static void validateIfCharIncludedInCollections(LinkedList<Character> delimitersList, String inputString, int index){
+        if(!delimitersList.contains(inputString.charAt(index))){
+            throw new IllegalArgumentException("구분자와 숫자 이외의 문자가 있습니다.");
+        }
+    }
+    public static void validateCharInBetweenStartAndEnd(String inputString, int index){
+        if(!(index>0 && index<inputString.length()-1)){
+            throw new IllegalArgumentException("구분자는 맨 앞과 맨 끝에 존재할 수 없습니다.");
+        }
+    }
+    public static void validateIfDelimitersInBetweenNumbers(String inputString, int index){
+        if(!(Character.isDigit(inputString.charAt(index-1)) && Character.isDigit(inputString.charAt(index+1)))){
+            throw new IllegalArgumentException("구분자가 숫자와 숫자 사이에 존재하지 않습니다.");
         }
     }
 
+
     public static Numbers checkIfNumeric(String inputString, Numbers numbers){
-        LinkedList<String> stringNumber = extractService.extractNumbers(inputString);
-        for(String s : stringNumber){
+        LinkedList<String> stringNumbers = extractService.extractStringNumbers(inputString);
+        for(String s : stringNumbers){
             if(s.isEmpty()) continue;
-            if(s.length() !=1 && s.charAt(0)=='0'){
-                throw new IllegalArgumentException();
-            }
-            if(Integer.parseInt(s) <=0){
-                throw new IllegalArgumentException();
-            }
+            checkNumberFormat(s);
+            checkIfPositiveNumber(s);
         }
-        for(int i=0;i<stringNumber.size();i++){
-            if(stringNumber.get(i).isEmpty()) continue;
-            numbers.getNumbersList().add(Integer.parseInt(stringNumber.get(i)));
+        return extractService.addStringNumberToNumbers(stringNumbers, numbers);
+    }
+
+    public static void checkNumberFormat(String stringNumber){
+        if(stringNumber.length() !=1 && stringNumber.charAt(0)=='0'){
+            throw new IllegalArgumentException("숫자의 형태가 아닙니다.");
         }
-        return numbers;
+    }
+
+    public static void checkIfPositiveNumber(String stringNumber){
+        if(Integer.parseInt(stringNumber) <=0){
+                throw new IllegalArgumentException("숫자가 양수가 아닙니다.");
+        }
     }
 }
