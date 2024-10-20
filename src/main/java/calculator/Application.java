4122e860bbc0927d7212ac26f1d0e987f896f259
kimshinwoo <tlsdn67@naver.com>
2024-10-20 20:26:31 +0900

    feat: 분류한 문자열 정수 변환 및 합 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,9 +6,19 @@ import java.util.Arrays;
 public class Application {
     public static void main(String[] args){
         // TODO: 프로그램 구현
+        int[] result2 = new int[result.length];
+        int dap = 0;
+
         String input = Console.readLine();
         String[] result = input.split("[:,]");
-        System.out.println(Arrays.toString(result));
 
+        for(int i=0;i< result.length;i++){
+            result2[i] = Integer.parseInt(result[i]);
+        }
+
+        for (int j : result2) {
+            dap += j;
+        }
+        System.out.println(dap);
     }
 }
