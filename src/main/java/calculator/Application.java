19a82a30bea2f9125ceeab52e2824fa02a120aed
EP45 <5510816+EP45@users.noreply.github.com>
2024-10-21 02:24:58 +0900

    feat(Application): add user delimiters string extractor
    Added feature that extracts user delimiter from input string
    - Include default delimiters
    - Extract user-defined delimiter(s) given with literal '\n'
    - Extract user-defined delimiter(s) given with Enter key

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,32 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class Application {
+
+    public static String delimiterRegex = ",|:";
+
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = Console.readLine();
-        // TODO: handle delimiters
+
+        if (userInput.startsWith("//")) {
+            String userDelimiterInput = "";
+            // delimiter(s) given with Enter key
+            if (!userInput.contains("\\n")) {
+                userDelimiterInput = userInput.substring("//".length());
+                userInput = Console.readLine();
+            }
+            // delimiter(s) given with literal '\n'
+            if (userInput.contains("\\n")) {
+                userDelimiterInput = userInput.substring("//".length(),
+                        userInput.indexOf("\\n"));
+                userInput = userInput.substring(userInput.indexOf("\\n") + "\\n".length());
+            }
+
+            // TODO: loop thorough userDelimiterInput by char
+        }
 
         int sumResult = 0;
         // TODO: parse user formula
