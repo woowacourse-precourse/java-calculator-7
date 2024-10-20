7837512ad59ffe5e00088ceaa651f5e0a9a0224d
JS <zks145@naver.com>
2024-10-20 14:28:36 +0900

    feat (application) : 계산기 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+
 public class Application {
     public static void main(String[] args) {
-
+        CalculatorController controller = new CalculatorController();
+        controller.run();
     }
 }
