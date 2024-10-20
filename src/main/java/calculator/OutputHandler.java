a7e795419f2f5bf3e45b215792b83f489460861e
sung-silver <jinna0319@gmail.com>
2024-10-21 00:14:19 +0900

    fix: 출력 형식에 공백 수정

--- src/main/java/calculator/OutputHandler.java
+++ src/main/java/calculator/OutputHandler.java
@@ -3,9 +3,9 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class OutputHandler {
-    private static final String RESULT_MESSAGE = "결과: %d";
+    private static final String RESULT_MESSAGE = "결과 : %d";
 
-    public static void printResult(int result) {
+    public static void printResult(long result) {
         System.out.printf(RESULT_MESSAGE, result);
         Console.close();
     }
