d80bffa112b5f8a2654e3e98468cb3d2d35593e1
unh6unh6 <ruddbs3308@naver.com>
2024-10-20 19:29:58 +0900

    test: 계산기 계산 테스트

---
+++ src/test/java/calculator/model/CalculatorTest.java
@@ -0,0 +1,19 @@
+package calculator.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+    Numbers numbers = new Numbers();
+
+    @Test
+    void 계산기_계산() {
+        numbers.saveNumber(2);
+        numbers.saveNumber(13);
+        assertThat(calculator.calculate(numbers))
+                .isEqualTo(15);
+    }
+}
