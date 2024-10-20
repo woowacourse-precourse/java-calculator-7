09a31246d8d4e87e79cab1b03e255403f8e8a04e
samueljang <legoddsl@gmail.com>
2024-10-20 10:48:52 +0900

    feat($Application): Add CustomDelimeiter feature
    문자열을 입력 받아 기본 구분자인 ,와: 뿐만아니라 "//" 와 "\n"사이에 특수 문자를 넣어 입력하면 해당 특수 문자를 커스텀 구분자로 사용할 수 있는 기능을 추가 그리고 음수를 입력할 경우 에러 처리하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,21 +10,37 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.\n");
         String input = scanner.nextLine();
 
-        //일단은 ,와: 기준으로만 문자열 분리
-        // 커스텀 구분자 추가해야함!!!
-        String[] parts = input.split("[,:]");
+        // 커스텀 구분자 추출
+        String delimiter = "[,:]";  // 기본 구분자
+        if (input.startsWith("//")) {
+            String[] splitInput = input.split("\\\\n");  // "\n"을 기준으로 두 부분으로 분리
 
+            if (splitInput.length < 2) {
+                throw new IllegalArgumentException("입력값 에러 발생: \\n이 누락되었습니다.");
+            }
+
+            String customDelimiter = splitInput[0].substring(2);  // "//" 이후 구분자 추출
+            delimiter = "[" + customDelimiter + ",:]";  // 구분자 설정
+            input = splitInput[1];  // 본문 추출
+        }
+
+        String[] parts = input.split(delimiter);
         int sum = 0;
+
         for(String part : parts) {
             try {
                 //숫자라면 더하기
-                sum += Integer.parseInt(part);
-            } catch (NumberFormatException e) {
+                if(Integer.parseInt(part) < 0) {
+                    throw new IllegalArgumentException("입력값 에러 발생 : 양수만 입력 가능합니다.");
+                }else {
+                    sum += Integer.parseInt(part);
+                }
+            } catch (NumberFormatException e ) {
                 //숫자가 아니라면 에러 처리
-                throw new IllegalArgumentException("입력값 에러 발생 : " + part );
-
+                throw new IllegalArgumentException("입력값 에러 발생 : " + part + "는 숫자가 아닙니다.");
             }
         }
         System.out.println("결과 : " + sum);
     }
+
 }
