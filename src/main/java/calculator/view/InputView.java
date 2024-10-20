81b4f79a5397e83fa583df9b8efe82cd1d13c1b5
yunsseong <aster20@hanyang.ac.kr>
2024-10-20 19:09:45 +0900

    feat: 개행 문자 처리 기능 추가

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -2,6 +2,8 @@ package calculator.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import calculator.util.Util;
+
 public class InputView {
 
     private OutputView outputView = new OutputView();
@@ -13,5 +15,10 @@ public class InputView {
     public String input(String message) {
         outputView.printMessage(message);
         return input();
+    }
+
+    public String newlineInput(String message) {
+        outputView.printMessage(message);
+        return Util.newlineConverter(input());
     }
 }
