0b5fda73940777534bb6aeaf6c3569062f32deee
chan <ckstj8027@naver.com>
2024-10-21 00:31:05 +0900

    feat: Application.java 실행 코드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.service.Service;
+import calculator.service.ServiceV2;
+import calculator.validator.Validator;
+import calculator.validator.ValidatorImpl;
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        //;\n1;2;3
+        //;as\\\\\n1;as\\\\2;as\\\\37
+        //;+{\n1;+{2;+{37
+        //;-\n1;-2;-3
+        //;-\n1;--2;-3
+
+        //Service service = new ServiceV1();
+        Validator validator = new ValidatorImpl();
+        Service service = new ServiceV2();
+        CalculatorController calculatorController = new CalculatorController(service, validator);
+        calculatorController.execute();
     }
 }
