ca8c3b9ecc6db0051984a8a7f5b790897a399376
longrunpc <pcjmo4050@gmail.com>
2024-10-21 04:20:58 +0900

    feat(application): Application 클래스에 main 메서드 수정
    Application 클래스에 main 메서드를 수정하여 CalculateController 인스턴스를 생성하고 run 메서드를 호출해
    전체 계산 흐름을 실행하도록 구현.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.controller.CalculateController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // CalculateController 인스턴스 생성 및 실행
+        CalculateController controller = new CalculateController();
+        controller.run();
     }
 }
