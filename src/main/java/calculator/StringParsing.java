d7d444b7c139677d01d7c1028691f5114b2dd12d
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-20 16:33:46 +0900

    fix: split하기 전 음수가 들어올 경우 예외처리하도록 수정

--- src/main/java/calculator/StringParsing.java
+++ src/main/java/calculator/StringParsing.java
@@ -1,9 +1,16 @@
 package calculator;
 
 public class StringParsing {
+
     private static String[] userInputArr;
+    private static final String IS_NOT_NUMBER = "숫자가 아닌 값이 포함되어 있습니다";
 
     public static String[] splitNpushString(String userInput, String delimiter) {
+        for (int i = 0; i < userInput.length(); i++) {
+            if (!Character.isDigit(userInput.charAt(i))) {
+                throw new IllegalArgumentException("양수가 아닙니다");
+            }
+        }
         userInputArr = userInput.split(delimiter);
         return userInputArr;
     }
@@ -16,7 +23,7 @@ public class StringParsing {
             try {
                 Long.parseLong(s);
             } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다");
+                throw new IllegalArgumentException(IS_NOT_NUMBER);
             }
         }
     }
