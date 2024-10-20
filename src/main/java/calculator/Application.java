f70bdaceafc516c728e7a59ed36f00aeb4b859c9
k1mh4kj00n <k1mh4kj00n@gmail.com>
2024-10-21 01:45:35 +0900

    test3

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,5 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-
-
-import static camp.nextstep.edu.missionutils.Console.*;
-
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Scanner;
-import static camp.nextstep.edu.missionutils.Console.*;
-
 public class Application {
 
     public static void main(String[] args) {
