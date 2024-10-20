9d22d4b01aa71fc10c05a355873bdff09840ef50
kimsehw <seunghwan9703@gmail.com>
2024-10-20 16:34:37 +0900

    feat(InputProcessor): 커스텀 구분자를 추출하는 기능 추가
    커스텀 구분자 시작,끝 을 상수로 추가합니다.
    구분자들의 리스트를 필드로 추가합니다.

--- src/main/java/calculator/model/InputProcessor.java
+++ src/main/java/calculator/model/InputProcessor.java
@@ -1,12 +1,20 @@
 package calculator.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputProcessor {
 
     private String input;
-    //public static final D
+    private List<String> delimiterList = new ArrayList<>();
+
+    private static final String CUSTOM_DELIM_START = "//";
+    private static final String CUSTOM_DELIM_END = "\n";
 
     public InputProcessor(String input) {
         this.input = input;
+        delimiterList.add(",");
+        delimiterList.add(":");
     }
 
     public boolean isDefaultCase() {
@@ -14,7 +22,17 @@ public class InputProcessor {
     }
 
     public boolean isCustomCase() {
-        return input.startsWith("//");
+        return input.startsWith(CUSTOM_DELIM_START);
+    }
+
+    public void getCustomDelimiter() {
+        while (input.indexOf(CUSTOM_DELIM_START) == 0) {
+            String customDelimiter = input.substring((input.indexOf(CUSTOM_DELIM_START) + CUSTOM_DELIM_START.length()),
+                    (input.indexOf(CUSTOM_DELIM_END) - CUSTOM_DELIM_END.length() + 1));
+            delimiterList.add(customDelimiter);
+
+            input = input.substring(input.indexOf(CUSTOM_DELIM_END) + CUSTOM_DELIM_END.length());
+        }
     }
 
 }
