6af90839ffe039dd3e1e927d0e0d079450865cc3
jollywitch <jollywitch75@gmail.com>
2024-10-21 00:33:38 +0900

    프로그램 main 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.controller.Controller;
+import calculator.view.View;
+
 public class Application {
     public static void main(String[] args) {
-
+        View view = new View();
+        new Controller(view);
     }
 }
