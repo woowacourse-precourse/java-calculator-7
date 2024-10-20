a5d09e096454278bb8026810ee3b86375c9222e7
smyeongL <sangmyeonglee123@gmail.com>
2024-10-20 21:27:33 +1100

    feat(application): add calculator in Application

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,6 +9,15 @@ public class Application {
     public static void main(String[] args) {
         // Declare Strategy Context Class, Calculator
         Calculator addCalculator = new Calculator(new Add());
+        InputHandler inputHandler = new InputHandler();
+        inputHandler.getUserInput();
+        // inputHandler.setUserInput("//m\n1,2:3m2");
+        inputHandler.setCustomSeparators();
+        inputHandler.removeCustomSeparators();
+        inputHandler.parseInput();
+        addCalculator.setNumbers(inputHandler.getNumbers());
+        System.out.println(addCalculator.calculate());
+
     }
 
 }
