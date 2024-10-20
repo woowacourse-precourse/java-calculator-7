0df207b6af987eb905e537f1c0f0953cd17d0a7b
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-20 17:51:59 +0900

    feat (basic calculator) : add calculator with comma(,) and colon(:) delimiter

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputStr = Console.readLine();
+
+        int result = 0;
+        // 문자열이 아무것도 입력이 안되었다면 0으로 설정
+
+        // 문자열 입력시 , : 로 구분하기
+        String[] split = inputStr.split(":|,");
+        for (String s : split) {
+            int s1 = Integer.parseInt(s);
+            result +=s1;
+        }
+        System.out.println("결과 : "+ result);
+
+
+
+
+
     }
+
 }
