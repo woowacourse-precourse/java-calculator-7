ca7bde92ab03251dacd12d8258b3e7208304e107
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-20 18:56:16 +0900

    refactor(ViewMessage): 뷰 출력 메시지 가독성 개선
    ViewMessage를 생성하여 inputView, outputView 메시지 가독성 개선

--- src/main/java/calculator/view/CalculatorView.java
+++ src/main/java/calculator/view/CalculatorView.java
@@ -3,11 +3,11 @@ package calculator.view;
 public class CalculatorView {
 
     public static void inputView() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println(ViewMessage.START_MESSAGE.getMessage());
     }
 
     public static void outputView(int result) {
-        System.out.println("결과 : " + result);
+        System.out.println(ViewMessage.END_MESSAGE.getMessageWithResult(result));
     }
 
 }
---
+++ src/main/java/calculator/view/ViewMessage.java
@@ -0,0 +1,22 @@
+package calculator.view;
+
+public enum ViewMessage {
+
+    START_MESSAGE("덧셈할 문자열을 입력해 주세요."),
+    END_MESSAGE("결과 : ");
+
+    private final String message;
+
+    ViewMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessageWithResult(int result) {
+        return String.format("%s%d", message, result);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
