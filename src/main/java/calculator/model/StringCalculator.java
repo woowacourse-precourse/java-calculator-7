883d7cd9f48b1b4aa37bb9c1908bbd661180b8dc
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:42:51 +0900

    :sparkles: feat(StringCalculator): 빈 문자열인 경우 0을 반환

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -1,4 +1,10 @@
 package calculator.model;
 
 public class StringCalculator {
+
+    public int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+    }
 }
