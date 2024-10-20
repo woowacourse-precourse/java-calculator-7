8190bfb0272ad95ecb9bf8a9ac31a8b271555629
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-21 00:06:41 +0900

    feat: print 메서드 추가
    - 줄바꿈 없는 출력을 위한 메서드.

--- src/main/java/calculator/util/io/IOConsole.java
+++ src/main/java/calculator/util/io/IOConsole.java
@@ -11,4 +11,8 @@ public class IOConsole {
     public static void println(String message) {
         System.out.println(message);
     }
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
 }
