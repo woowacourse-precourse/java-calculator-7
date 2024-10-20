bcd6c8c0ea2435762fb36b1e6e51fb75a874adef
kaswhy <sxshx818@naver.com>
2024-10-20 15:23:30 +0900

    feat(CalculatorController): 컨트롤러에서 프로그램 전체 흐름 구현
    View를 통해 입력받고, 이를 ExpressionModel에 전달하여 계산 후 다시 View를 통해 계산 결과 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -128,68 +128,19 @@ class CalculatorController {
 
     public void calculate(int cnt) {
         String expression = view.getUserInput();
+
         // 데이터 처리
+        ExpressionModel expressionModel = new ExpressionModel(expression);
+        int result = model.calculate(expressionModel);
 
         // 출력
+        view.displayResult(result);
     }
 }
 
 public class Application {
-    static String delimiter = ",|:";
-    static String inputExpression;
-
-    static int hasCustom(String str) {
-        int start = str.indexOf("//");
-        return start;
-    }
-
-    static void registerCustom(String str) {
-        int customStart = hasCustom(str);
-        for (int i = customStart + 2; i < str.length() - 2; i++) {
-            // 여기 안의 과정을 하나로 묶어야 함 (idx 필요)
-            if (str.startsWith("\\n", i)) {
-                String tmp = str.substring(customStart + 2, i);
-                for (int j = 0; j < tmp.length(); j++) {
-                    delimiter += "|\\" + tmp.charAt(j);
-                }
-                inputExpression = inputExpression.substring(i + 2);
-                break;
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        // 입력
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        inputExpression = Console.readLine();
 
-        // 구분자 등록
-        if (hasCustom(inputExpression) != -1) {
-            registerCustom(inputExpression);
-        }
-
-        // 구분자로 문자열 자르기
-        String[] numbers = inputExpression.split(delimiter);
-
-        // 추출된 숫자들의 합 구하기
-        int cnt = 0;
-        for (String nowStr : numbers) {
-            String REGEXP_ONLY_NUM = "^[\\d]*$";
-            if (!Pattern.matches(REGEXP_ONLY_NUM, nowStr)) {
-                throw new IllegalArgumentException();
-            }
-            if (nowStr.equals("")) {
-                continue;
-            }
-            int nowInt = Integer.parseInt(nowStr);
-            if (nowInt <= 0) {
-                throw new IllegalArgumentException();
-            }
-            cnt += nowInt;
-        }
-
-        // 결과 출력
-        System.out.println("결과 : " + cnt);
     }
 }
 
