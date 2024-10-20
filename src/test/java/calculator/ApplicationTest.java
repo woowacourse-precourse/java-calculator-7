c02a764579bd8a377d84839ed59bf41dc4ba7962
RaZel713 <razel7130@gmail.com>
2024-10-20 19:19:30 +0900

    test: 문자열 분리기능 테스트 코드 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -20,6 +20,48 @@ class ApplicationTest extends NsTest {
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    // 추가 test 사항
+
+    @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 구분자_연속_입력() {
+        assertSimpleTest(() -> {
+            run("1::2,,3,::4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 단일_양수_입력() {
+        assertSimpleTest(() -> {
+            run("5");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+    @Test
+    void 한_자리_이상의_숫자_입력() {
+        assertSimpleTest(() -> {
+            run("10,5:201");
+            assertThat(output()).contains("결과 : 216");
+        });
+    }
+
+    @Test
+    void 정해진_구분자_이외의_문자_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2a3"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
