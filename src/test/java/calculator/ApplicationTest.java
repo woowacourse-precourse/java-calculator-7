22c201ceede537a5cb0eab840bcf9cfa249b8290
yunju <juile0827@naver.com>
2024-10-20 18:37:12 +0900

    test(ApplicationTest): 예외 종류별 테스트 추가
    ApplicationTest 클래스 내 테스트 메서드 추가
    - 예외_테스트_숫자_커스텀()
    - 예외_테스트_문자열_검증()
    - 예외_테스트_문자열_입력없음()

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,33 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 예외_테스트_숫자_커스텀() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//1\\n1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("커스텀 구분자는 숫자로 지정할 수 없습니다.")
+        );
+    }
+
+    @Test
+    void 예외_테스트_문자열_검증() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n1;2*3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("구분자 이외의 문자가 포함되어 있습니다:")
+        );
+    }
+
+    @Test
+    void 예외_테스트_문자열_입력없음() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("덧셈을 진행할 문자열을 입력하세요.")
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
