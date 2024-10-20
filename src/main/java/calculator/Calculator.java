7b8d26996d25fdb10eafe94db59b46f6cb6443dc
jjr7181 <jjr7181@gmail.com>
2024-10-20 22:24:38 +0900

    chore: modify comments

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -15,18 +15,17 @@ public class Calculator {
         } else {
             regex = "[,:]";
         }
-//        String regex = "[," + customDelimiter + ":]";
         String[] numbers = numbersPart.split(regex);
 
-        validateNumbers(numbers);
+        validateNumbers(numbers);  // 유효한 문자인지 검사
         return sumNumbers(numbers);
     }
 
     public String extractCustomDelimiter(String input) {
         // 첫 2글자가 "//"로 시작하는지 확인
         if (input.startsWith("//")) {
-            // \n 이전의 부분에서 커스텀 구분자를 추출
             int delimiterEndIndex = input.indexOf("\\n");
+            //커스텀 구분자가  존재한다면 추출
             if (delimiterEndIndex != -1 && delimiterEndIndex > 2) {
                 return input.substring(2, delimiterEndIndex);
             }
@@ -45,28 +44,21 @@ public class Calculator {
         return input;
     }
 
-    // 문자열 유효성 검사
     public void validateNumbers(String[] numbers) {
         for (String number : numbers) {
-            // 공백 제거
             number = number.trim();
-            // 숫자인지 검사
+            // 숫자 && 양수 검사
             if (!number.matches("\\d+")) {
                 throw new IllegalArgumentException("유효한 숫자가 아닙니다: " + number);
             }
         }
     }
 
-    public boolean isPositive(String number) {
-        int parsedNumber = Integer.parseInt(number);
-        return parsedNumber >= 0;
-    }
-
     public int sumNumbers(String[] numbers) {
         int sum = 0;
         for (String number : numbers) {
-            sum += Integer.parseInt(number.trim()); // 합산
+            sum += Integer.parseInt(number.trim());
         }
-        return sum; // 합계 반환
+        return sum;
     }
 }
