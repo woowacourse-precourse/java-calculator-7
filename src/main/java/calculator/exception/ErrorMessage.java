15422d7f37219c570ccdbe6847090d7866873174
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-20 17:33:44 +0900

    feat(ErrorMessage): 커스텀 구분자에 숫자를 사용할 때 발생하는 예외 메시지 작성

--- src/main/java/calculator/exception/ErrorMessage.java
+++ src/main/java/calculator/exception/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     NOT_MATCH_FOUND_DELIMITER("매칭되는 구분자가 없습니다."),
     INVALID_INPUT("구분자와 숫자를 정확하게 입력해주세요."),
-    NEGATIVE_NUMBER_NOT_ALLOWED("음수는 입력할 수 없습니다.");
+    NEGATIVE_NUMBER_NOT_ALLOWED("음수는 입력할 수 없습니다."),
+    CUSTOM_DELIMITER_CANNOT_BE_NUMBER("커스텀 문자에 숫자를 입력할 수 없습니다.");
 
     private final String message;
 
