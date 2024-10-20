70795915e2ed30efba6ccae24ded65bdf15ddb9c
Gothax <scott990328@gmail.com>
2024-10-20 15:51:56 +0900

    test: test case 추가
    \\n으로 끝나지 않는 커스텀 구분자 사용 케이스를 포함하지 못해서 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -114,6 +114,14 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void n_구분자_테스트(){
+        assertSimpleTest(()->{
+            assertThatThrownBy(()->runException("//^\\1^2^3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
