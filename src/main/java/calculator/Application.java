0bf9ff53f2da01f2bb8c3ed4c119c5d1a38593db
mskim <msung98@naver.com>
2024-10-20 22:23:24 +0900

    첫번째 기능요구사항 완성
    feat: complete the first feature implementation

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();  // 사용자 입력을 받음
+        System.out.println(input);
+        int sum = 0;
+        if(!(input.isEmpty())){
+            String[] nums = input.split("[:,,]");
+            for (String num : nums){
+                sum += Integer.parseInt(num);
+            }
+        }
+        System.out.println(sum);
     }
 }
