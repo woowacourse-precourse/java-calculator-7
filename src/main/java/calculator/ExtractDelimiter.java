24b52673d7c8382126399a8639775e4f0faf8827
AYEOOON <k93853471@gmail.com>
2024-10-20 13:42:40 +0900

    fix: 구분자 추출 로직 수정
    - 기존 정규표현식 방식은 연속된 구분자 예외 처리가 어려움
    - 구분자 추출 로직을 수정
       - "//"로 시작하지 않으면 기본구분자로 처리
       - "//"로 시작하면 \n 사이의 문자를 구분자로 설정

--- src/main/java/calculator/ExtractDelimiter.java
+++ src/main/java/calculator/ExtractDelimiter.java
@@ -1,27 +1,15 @@
 package calculator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-
 // 입력 문자열에서 구문자를 추출하는 클래스
 public class ExtractDelimiter {
     // 구분자 추출
-    static String extract_basic_delimiter(String input) {
-        String regex = "//(.+)\\n";
-
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
-
-        if (matcher.find()) {
-            return extract_custom_delimiter(matcher);
+    static String extract_delimiter(String input) {
+        if (input.startsWith("//")) {
+            // 커스텀 구분자 추출
+            int delimiterEnd = input.indexOf("\n");
+            return input.substring(2, delimiterEnd);
         }
+        // 기본 구분자 ',' 또는 ':'
         return "[,:]";
-    }
-
-    // 커스텀 구분자 추출
-    static String extract_custom_delimiter(Matcher matcher) {
-        String delimiter = matcher.group(1); // 커스텀 구분자 추출
-        return Pattern.quote(delimiter); // 커스텀 구분자에 대해 이스케이프 처리
     }
 }
