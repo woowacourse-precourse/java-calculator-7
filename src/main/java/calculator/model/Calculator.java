981159f9e68c86430f473f1b2a1ecae999ce119b
choitaehoon <hugebigdream@gmail.com>
2024-10-20 18:04:13 +0900

    refactor: add rules to the calculator class

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -5,6 +5,20 @@ import java.util.Arrays;
 public class Calculator {
 
     public int sum(String[] numberArr) {
-        return Arrays.stream(numberArr).mapToInt(Integer::parseInt).sum();
+        return Arrays.stream(numberArr)
+                .mapToInt(this::parseNumber)
+                .sum();
+    }
+
+    public int parseNumber(String number) {
+        int num = Integer.parseInt(number);
+        validateNumber(num);
+        return num;
+    }
+
+    private void validateNumber(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("음수는 불가능합니다.");
+        }
     }
 }
