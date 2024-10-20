9f767a1b53614c258ca2a3fe976a49bbbec838f5
song hyeongyu <lit3207@ajou.ac.kr>
2024-10-20 12:37:31 +0900

    feat: add splitString()

--- src/main/java/calculator/controller/Process.java
+++ src/main/java/calculator/controller/Process.java
@@ -1,6 +1,8 @@
 package calculator.controller;
 
+import calculator.exception.SeparatorException;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,36 +10,45 @@ import java.util.List;
 
 public class Process {
 
-    private final InputView inputView =  new InputView();
+    private final InputView inputView = new InputView();
     private String userInput;
     private List<Character> basicSeparator = new ArrayList<>(Arrays.asList(':', ','));
+    OutputView outputView = new OutputView();
+
+    public void run() throws SeparatorException {
+
+        userInput = inputView.getInput();
+        if (isSeparator()) {
+            String newSeparator = findSeparator();
+            addStringToCharList(newSeparator);
+        }
+        String number = splitString();
+        outputView.printResult(calculateSum(number));
+    }
 
     public boolean isSeparator() {
-        if (userInput.startsWith("//") && userInput.contains("\\n")) {
-            return true;
-        }
-        return false;
+        return userInput.startsWith("//") && userInput.contains("\\n");
     }
 
     public String findSeparator() {
         int startIndex = 2;
         int endIndex = userInput.lastIndexOf("\\n");
-
         return userInput.substring(startIndex, endIndex);
     }
 
     public void addStringToCharList(String separator) {
-        for(int i = 0; i < separator.length(); i++) {
+        for (int i = 0; i < separator.length(); i++) {
             basicSeparator.add(separator.charAt(i));
         }
     }
 
+    public String splitString() {
+        if (userInput.startsWith("//") && userInput.contains("\\n")) {
+            int index = userInput.lastIndexOf("\\n");
+            return userInput.substring(index + 2);
+        }
+        return userInput;
+    }
+
+
 }
-
-
-
-
-
-
-
-
