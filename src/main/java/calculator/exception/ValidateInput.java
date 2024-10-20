c9cce269a6a888101203f54e4f6abb9629b0a5d7
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-10-21 00:43:12 +0900

    refactor: validateDefaultDelimiter 메서드의 매개변수 타입을 Input 클래스로 변경

--- src/main/java/calculator/exception/ValidateInput.java
+++ src/main/java/calculator/exception/ValidateInput.java
@@ -1,22 +1,18 @@
 package calculator.exception;
 
-import java.util.Arrays;
-import java.util.List;
+import calculator.calculator.Input;
 
 public class ValidateInput {
 
     //사용자 입력값 검사 - 커스텀 구분자 없는 경우
-    public List<String> validateDefaultDelimiter(String input) {
-        List<String> delimiters = Arrays.asList(",", ":");
-
+    public void validateDefaultDelimiter(Input input) {
         //기본 구분자(",", ":")와 양수만 허용하는 정규식 (빈 문자열도 허용함)
         String regex = "^[0-9]*([,:][0-9]*)*$";
 
         //정규식과 일치하지 않을 때 IllegalArgumentException
-        if (!input.matches(regex)) {
+        if (!input.getInputString().matches(regex)) {
             throw new IllegalArgumentException("잘못된 입력값입니다. 기본 구분자는 쉼표와 콜론만 가능하고, 숫자는 양수만 입력할 수 있습니다.");
         }
 
-        return delimiters; //구분자 리스트 반환
     }
 }
