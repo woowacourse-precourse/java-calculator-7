4455810cf777a86d40299436b990ba12f8e892cf
hanmh319 <hanmh319@naver.com>
2024-10-20 18:43:58 +0900

    refactor: splitInput êㄱê구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,43 +1,40 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
-    public static void main(String[] args) {
-        String a = Console.readLine();
+    private static final String CUSTOM_SEPARATOR_REGEX = "//(.*?)\\\\n";
 
+    public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        String[] splitArr = splitInput(input);
+        for (int i = 0; i < splitArr.length; i++) {
+            System.out.print(splitArr[i] + " ");
+        }
+
+    }
+
+    private static String[] splitInput(String input) {
         String separator1 = ","; // 구분자 설정
         String separator2 = ":"; // 구분자 설정2
         String separator3 = ""; // 커스텀 구분자 추가 설정, 공백으로 초기화
-        String[] sepArr;
-        int sum = 0;
 
-
-        Pattern pattern = Pattern.compile("//(.*?)\\\\n");
-        ;// "//", "\n" 사이의 문자 추출
-        Matcher matcher = pattern.matcher(a);
+        Pattern pattern = Pattern.compile(CUSTOM_SEPARATOR_REGEX); // "//", "\n" 사이의 문자 추출
+        Matcher matcher = pattern.matcher(input);
 
         if (matcher.find()) {
             separator3 = matcher.group(1); // 추출된 문자가 존재한다면 구분자로 지정
-            a = a.substring(matcher.end()).trim();
+            input = input.substring(matcher.end()).trim(); // 구분자 지정하는 구간 삭제
         }
-        if (separator3.isEmpty()) {
-            sepArr = a.split(separator1 + "|" + separator2); // 구분자가 없을 때
+
+        if (separator3.isEmpty()) { // 커스텀 구분자가 있는 경우, 없는 경우 나눠서 return
+            return input.split(separator1 + "|" + separator2); // 구분자가 없을 때
         } else {
-            sepArr = a.split(separator1 + "|" + separator2 + "|" + Pattern.quote(separator3)); // 모든 구분자로 나누기
+            return input.split(separator1 + "|" + separator2 + "|" + Pattern.quote(separator3)); // 모든 구분자로 나누기
         }
 
-        System.out.println("나뉜 이후 문자열: " + a);
-
-        for (String numStr : sepArr) {
-            try {
-                sum += Integer.parseInt(numStr.trim()); // 공백 제거 후 파싱
-            } catch (NumberFormatException e) {
-                System.out.println("유효하지 않은 문자열이 포함되어 있습니다");
-            }
-        }
-        System.out.println("#" + " " + sum);
     }
 }
