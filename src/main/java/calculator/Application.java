d7e7ccbed5ac5653ebd7bdf353f3b3030b2ea99e
CoconutGithub <khkoh5932@gmail.com>
2024-10-21 01:57:46 +0900

    feat : 문자열에서 구분자와 숫자로 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int sum = 0;
+
+        String string = Console.readLine();
+        String[] nums = string.split(",|:");
+        for (String num : nums) {
+            sum += Integer.parseInt(num);
+        }
+
     }
 }
