33f8a9a472812e51fb5387b08824b30ee0a9ea42
sinji <tobeabestns@naver.com>
2024-10-20 21:48:49 +0900

    feat: 구분자로 구별한 숫자들의 총합 구하는 기능

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -1,4 +1,28 @@
 package domain;
 
+import java.util.List;
+
 public class Calculator {
+    private int calculationResult;
+    private final Separator separator;
+
+    public Calculator(Separator separator) {
+        this.separator = separator;
+    }
+
+    public int addNumber(List<Integer> numberCollection) {
+        int sum = 0;
+
+        List<String> separators = separator.getSeparatorCollection();
+
+        for (Integer number : numberCollection) {
+            sum += Integer.parseInt(String.valueOf(number));
+        }
+        calculationResult = sum;
+        return sum;
+    }
+
+    public int getCalculator() {
+        return calculationResult;
+    }
 }
