a69d6682d57e676c383117dc13d522e3c64f1330
yu-k-m <karen0806@naver.com>
2024-10-20 19:58:17 +0900

    feat: add only numeric into List
    BREAKING CHANGE: change generic type from List<String> List<Integer>

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,8 +7,22 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String value = readLine();
-        List<String> valuesToCalculate = new ArrayList<>();
-        valuesToCalculate.add(value);
+        List<Integer> valuesToCalculate = new ArrayList<>();  //양수및커스텀구분자와특정구분자만 여기에 접수시킬것
+
+        if (value.isEmpty()) {
+            valuesToCalculate.add(0);
+        }
+
+        for (int i = 0; i < value.length(); i++) {
+            char c = value.charAt(i);
+            if (Character.isDigit(c)) {
+                valuesToCalculate.add(Character.getNumericValue(c));
+            }
+
+        }
+        System.out.println(valuesToCalculate);
     }
+
 }
