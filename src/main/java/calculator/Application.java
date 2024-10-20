f3571962a47110032d7e4f4b7e8b29251b4b9d23
hyeblee <hyeb1224@naver.com>
2024-10-20 21:15:08 +0900

    커스텀 구분자 찾는 메서드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,36 @@
 package calculator;
-import camp.nextstep.edu.missionutils.Console;
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
 
-        
+import camp.nextstep.edu.missionutils.Console;
+
+
+
+public class Application {
+
+    // 유효한 커스텀 구분자 갖고있는지 확인하는 메서드
+    public static boolean validCustomDelimiter(String input) {
+        if (input.length() < 5) {
+            return false;
+        }
+        if (!input.startsWith("//")) {
+            return false;
+        }
+        if (!(input.charAt(3) == '\\' && input.charAt(4) == 'n')) {
+            return false;
+        }
+        // 구분자가 숫자면 에러
+        if(input.charAt(2)>='0'&&input.charAt(2)<='9')
+            return false;
+        return true;
+    }
+
+
+    public static String[] splitString(String input, String delimiter){
+        return input.split(delimiter);
+    }
+
+
+    public static void main(String[] args) {
+        String input = Console.readLine();
 
     }
 }
