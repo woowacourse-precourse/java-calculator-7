c7723cbd7b036ae7bb2261b927bb20dcf5578fcb
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-10-20 20:33:46 +0900

    :necktie: feat: add validation check for numbers between // and \n

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,23 +5,29 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     private static final String CUSTOM_SEPARATOR_START = "//";
     private static final String CUSTOM_SEPARATOR_END = "\\n";
+    private static String userInput;
+    private static String baseSeparator = ",|:";
 
     public static void main(String[] args) {
         run();
     }
 
     public static void run(){
-        String input = Console.readLine();
-        inputValidationCheck(input);
+        userInput = Console.readLine();
+        inputValidationCheck(userInput);
     }
 
     public static void inputValidationCheck(String input){
         checkBlankExist(input);
         checkMinusExist(input);
         if (checkCustomSeparatorExist(input)){
-
+            char customSeparator = input.charAt(2);
+            if (Character.isDigit(customSeparator)){
+                throw new IllegalArgumentException("숫자를 구분자로 지정할 수 없습니다.");
+            }
+            baseSeparator += "|" + customSeparator;
+            userInput = input.substring(5);
         }
-
     }
 
     public static void checkMinusExist(String input) {
