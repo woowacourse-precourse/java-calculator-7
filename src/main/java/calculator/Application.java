774130c29faf692ddd103e9db8bfb451d9a4b890
YeaChan05 <qkenrdl05@gmail.com>
2024-10-20 18:30:35 +0900

    feat(Application): 전체 프로세스 조합
    구현된 클래스들을 기반으로 요구사항에 맞게 조합

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.endpoint.input.InputInterface;
+import calculator.endpoint.output.DisplayInterface;
+import calculator.service.Calculator;
+import calculator.service.CalculatorFactory;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Calculator calculator = CalculatorFactory.createCalculator();
+
+        DisplayInterface.displaySystemMessage();
+        String input = InputInterface.input();
+        int result = calculator.calculate(input);
+        DisplayInterface.displayAnswer(result);
     }
 }
