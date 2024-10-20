b49db5cb4177f6f0ea93a75f249677a1ed02a9c3
rootTiket <l20078829@gmail.com>
2024-10-20 21:00:33 +0900

    feat: 입력값이 비어있을때 예외처리

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,12 +1,20 @@
 package calculator.view;
 
+import calculator.global.exception.CalculatorException;
+import calculator.view.constant.errorMessage.InputErrorMessage;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
     public String inputString() {
         String input = Console.readLine();
-
+        validateInput(input);
         return input;
+    }
+
+    private void validateInput(String input) {
+        if (input.isEmpty()) {
+            throw new CalculatorException(InputErrorMessage.NULL_INPUT_ERROR);
+        }
     }
 }
