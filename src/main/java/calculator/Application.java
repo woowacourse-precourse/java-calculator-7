40b3e15da6ea070d77cfcd21101c451ab8a8a799
angrychimpanzee <136212007+angrychimpanzee@users.noreply.github.com>
2024-10-21 02:09:21 +0900

    feat(StringCalculator) : Implement basic function to split input string by commas and colons, then sum the numbers.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,6 +7,23 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        System.out.println("입력값: " + input);
+        // System.out.println("입력값: " + input);
+        int result = StringCalculator.add(input);
+        System.out.println("결과 : " + result);
+    }
+}
+
+class StringCalculator{
+    public static int add(String input){
+        String[] numbers = input.split(",|:");
+        return sumNumbers(numbers);
+    }
+
+    private static int sumNumbers(String[] numbers){
+        int sum = 0;
+        for (String number : numbers){
+            sum += Integer.parseInt(number.trim());
+        }
+        return sum;
     }
 }
