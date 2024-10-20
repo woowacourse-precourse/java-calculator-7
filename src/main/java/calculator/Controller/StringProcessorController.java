9f16cae21aae9b04e4dd8e54df36744e0f60611f
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-21 03:00:33 +0900

    feat(StringProcessorController): 분리된 문자열들에게서 합을 구하는 로직 호출부 추가
    - 해당 로직이 존재하는 SumCalculator의 메서드를 호출하기 위해서 의존성 주입
    - 기본과 커스텀 구분자로 분리된 문자열 리스트를 DTO에 설정한 후 DTO를 통해 sumCalculator.sum() 호출
    - 문자열 리스트에서 숫자를 추출한 뒤, 합을 계산하는 로직 구현 예정

--- src/main/java/calculator/Controller/StringProcessorController.java
+++ src/main/java/calculator/Controller/StringProcessorController.java
@@ -4,6 +4,7 @@ import calculator.DTO.StringCalculatorDTO;
 import calculator.Service.CustomDelimiterExtractor;
 import calculator.Service.DelimiterExtractor;
 import calculator.Service.InputReader;
+import calculator.Service.SumCalculator;
 import java.util.List;
 
 public class StringProcessorController {
@@ -11,14 +12,16 @@ public class StringProcessorController {
     private final InputReader inputReader;
     private final DelimiterExtractor delimiterExtractor;
     private final CustomDelimiterExtractor customDelimiterExtractor;
+    private final SumCalculator sumCalculator;
 
     // 생성자에서 외부로부터의 의존성 주입
     public StringProcessorController(InputReader inputReader, DelimiterExtractor delimiterExtractor,
-                                     CustomDelimiterExtractor customDelimiterExtractor) {
+                                     CustomDelimiterExtractor customDelimiterExtractor, SumCalculator sumCalculator) {
 
         this.inputReader = inputReader;
         this.delimiterExtractor = delimiterExtractor;
         this.customDelimiterExtractor = customDelimiterExtractor;
+        this.sumCalculator = sumCalculator;
     }
 
     // 전체 프로세스 조율
@@ -32,6 +35,10 @@ public class StringProcessorController {
 
         // 기본 구분자로 분리된 문자열을 커스텀 구분자를 기준으로 또 다시 분리하는 로직 호출
         List<String> afterCustomExtractor = customDelimiterExtractor.extractDelimiter(inputDTO);
+        inputDTO.setDetachedInput(afterCustomExtractor);
+
+        // 구분자들로 분리된 문자열들을 계산해주는 로직 호출
+        sumCalculator.sum(inputDTO);
     }
 
 
