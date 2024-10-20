aef359413ea468863299a2269da68dfb17d78482
Park, Sojeong <sojeong2101@gmail.com>
2024-10-20 17:07:17 +0900

    refactor($Input): 불필요한 checkNull 메서드 삭제

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -5,13 +5,6 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     public String getNumberWithSeparator() {
         String numberWithSeparator = Console.readLine();
-        checkNull(numberWithSeparator);
         return numberWithSeparator;
-    }
-
-    private void checkNull(String numberWithSeparator) {
-        if (numberWithSeparator == null) {
-            throw new IllegalArgumentException("문자열을 입력해야 합니다.");
-        }
     }
 }
