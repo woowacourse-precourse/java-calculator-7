4b711e5bff725d73f1296a979cfc75567c9b3b21
Jeon Young Sang <iwtl0303@naver.com>
2024-10-20 22:36:35 +0900

    feat(): 예외 처리 추가
    잘못된 커스텀 구분자 추가 형식 예외 추가
    커스텀 구분자 및 기본 구분자 외 문자 입력 시 예외 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,39 +1,85 @@
 package calculator;
 
 import java.io.*;
-import java.util.*;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String line = "";
-        String newLine = "";
-        String custom = "";
-        String[] ansLine1 = {};
-        String[] ansLine2 = {};
-        boolean isCus = false;
-        int ans = 0;
-        try {
-            bw.write("덧셈할 문자열을 입력해 주세요.\n");
-            bw.flush();
-            line = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String[] lines = line.split("");
-        int x = 2;
-        if (lines[0].equals("/") && lines[1].equals("/")) { // 커스텀 구분자 추가
-            while (x < lines.length) {
+    static String line;
+    static String[] lines;
+    static String[] ansLine1;
+    static String[] ansLine2;
+    static String newLine;
+    static String custom;
+    static boolean isCus;
+    static int ans;
+    static int x;
+
+    static void init() {
+        line = "";
+        lines = new String[] {};
+        ansLine1 = new String[] {};
+        ansLine2 = new String[] {};
+        newLine = "";
+        custom = "";
+        isCus = false;
+        ans = 0;
+        x = 2;
+    }
+    static void chkCus() { // 커스텀 구분자 추가
+        if (lines[0].equals("/") && lines[1].equals("/")) {
+            while (x < lines.length-1) {
                 if (lines[x].equals("\\") && lines[x+1].equals("n")) {
                     isCus = true;
                     break;
                 }
                 custom += lines[x++];
             }
-
+            if (!isCus) {
+                throw new IllegalArgumentException("커스텀 구분자 입력형식이 잘못되었습니다.");
+            }
         }
+    }
+
+    static void divLine() { // 구분자로 나누기
+        int negative = 0;
+        for (int i = 0; i < ansLine1.length; i++) {
+            if (isCus) {
+                ansLine2 = ansLine1[i].split(custom);
+                for (int j = 0; j < ansLine2.length; j++) {
+                    negative = Integer.parseInt(ansLine2[j]);
+                    if (negative < 0) {
+                        throw new IllegalArgumentException("구분자 입력형식이 잘못되었습니다.");
+                    }
+                    ans += negative;
+                }
+            } else {
+                negative = Integer.parseInt(ansLine1[i]);
+                if (negative < 0) {
+                    throw new IllegalArgumentException("구분자 입력형식이 잘못되었습니다.");
+                }
+                ans += negative;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        init();
+        // TODO: 프로그램 구현
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            bw.write("덧셈할 문자열을 입력해 주세요.\n");
+            bw.flush();
+            line = Console.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        lines = line.split("");
+        chkCus();
+//        try {
+//            chkCus();
+//        } catch (IllegalArgumentException e) {
+//            System.out.println(e.getMessage());
+//            return;
+//        }
         if (isCus) {
             for (int i = x+2; i < lines.length; i++) {
                 newLine += lines[i];
@@ -42,22 +88,16 @@ public class Application {
             newLine = line;
         }
         ansLine1 = newLine.split("[,:]");
-        for (int i = 0; i < ansLine1.length; i++) {
-            if (isCus) {
-                ansLine2 = ansLine1[i].split(custom);
-                for (int j = 0; j < ansLine2.length; j++) {
-                    ans += Integer.parseInt(ansLine2[j]);
-                }
-            } else {
-                ans += Integer.parseInt(ansLine1[i]);
-            }
-        }
+        divLine();
+//        try {
+//            divLine();
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("구분자 입력형식이 잘못되었습니다.");
+//            return;
+//        }
 
         try {
             bw.write("결과 : " + ans + "\n");
-            for (int i = 0; i < ansLine1.length; i++) {
-                bw.write(ansLine1[i] + "\n");
-            }
             bw.flush();
             bw.close();
         } catch (IOException e) {
