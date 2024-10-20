8735619db63d6adc6c18d5ea794631e44d39321a
MinJae00 <exceedmin@gmail.com>
2024-10-21 01:59:39 +0900

    refact: InputView에서 Calculate함수 바로 호출

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,9 +1,13 @@
 package calculator.view;
+import calculator.calculate.Calculate;
+import calculator.exception.Exception;
 import camp.nextstep.edu.missionutils.Console;
 public class InputView {
-     public String getInput(){
+    Calculate calculate = new Calculate();
+     public InputView(){
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        return input;
+        Exception.IsEmptyInput(input);
+        calculate.CalStringNum(input);
     }
 }
