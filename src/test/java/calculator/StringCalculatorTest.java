eea2ee363f75c6c08915b26e9dc7b1b731225d59
delena0702 <delena0702@gmail.com>
2024-10-20 17:04:55 +0900

    test(StringCaculator): 입력 문자열에서 헤더를 제외하는 기능 테스트 구현
    removeHeaderFromInput의 테스트 구현

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -22,4 +22,17 @@ class StringCalculatorTest {
         assertThat(calculator.getCustomDelimiter("//\\n")).isEqualTo(null);
         assertThat(calculator.getCustomDelimiter("1,2,3,4")).isEqualTo(null);
     }
+
+    @Test
+    void removeHeaderFromInput() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertThat(calculator.removeHeaderFromInput("//;\\n")).isEqualTo("");
+        assertThat(calculator.removeHeaderFromInput("//;\\n1,2,3:4;5")).isEqualTo("1,2,3:4;5");
+        assertThat(calculator.removeHeaderFromInput("//;\\n//;\\n123")).isEqualTo("//;\\n123");
+        assertThat(calculator.removeHeaderFromInput("1:2,3")).isEqualTo("1:2,3");
+        assertThat(calculator.removeHeaderFromInput("//;\\")).isEqualTo("//;\\");
+        assertThat(calculator.removeHeaderFromInput("")).isEqualTo("");
+        assertThat(calculator.removeHeaderFromInput("1//;\\n")).isEqualTo("1//;\\n");
+    }
 }
