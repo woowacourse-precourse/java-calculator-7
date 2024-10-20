621f4d35e71198ec089402e7c8ef821d8ef5d45f
KNU-K <knukang334@gmail.com>
2024-10-20 14:34:01 +0900

    refactor(validate): 메소드 이름 변경 및 정규식 타입 변경
    - 기존 List<String> 에서 List<Pattern>으로 변경
    - 가독성을 올리기 위해 메소드 이름 변경

--- src/main/java/calculator/validate/CalculatorValidator.java
+++ src/main/java/calculator/validate/CalculatorValidator.java
@@ -5,14 +5,13 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class CalculatorValidator {
-
-    private static final List<String> REGEX_LIST = List.of(
-            "^//([^0-9])\\\\n([0-9]+(\\1[0-9]+)*)*$",    // Custom delimiter expression
-            "^([0-9]+([,:][0-9]+)*)*$"         // Default expression with multiple digits
+    private static final List<Pattern> REGEX_PATTERNS = List.of(
+            Pattern.compile("^//([^0-9])\\\\n([0-9]+(\\1[0-9]+)*)*$"),   // Custom delimiter expression
+            Pattern.compile("^[0-9]+([,:][0-9]+)*$")                  // Default expression with multiple digits
     );
 
     public static void validateExpression(String expression) {
-        if (!isValidExpression(expression) || !isValidExpressionWithRegex(expression)) {
+        if (!isNonEmptyExpression(expression) || !matchesAnyPattern(expression)) {
             throw new InvalidFormatException();
         }
     }
@@ -21,13 +20,11 @@ public class CalculatorValidator {
         return expression.startsWith("//") && expression.contains("\\n");
     }
 
-    public static boolean isValidExpressionWithRegex(String expression) {
-        return REGEX_LIST.stream().anyMatch(regex -> Pattern.matches(regex, expression));
+    private static boolean matchesAnyPattern(String expression) {
+        return REGEX_PATTERNS.stream().anyMatch(pattern -> pattern.matcher(expression).matches());
     }
 
-    private static boolean isValidExpression(String expression) {
+    private static boolean isNonEmptyExpression(String expression) {
         return expression != null && !expression.trim().isEmpty();
     }
-
 }
-
