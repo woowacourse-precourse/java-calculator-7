35f30812da5ec5da3fd059b1e994f64741994d63
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-21 01:15:40 +0900

    feat : 빈 문자열 처리 구현

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,4 +1,16 @@
 package calculator.service;
 
+import calculator.view.CalculatorView;
+
 public class CalculatorService {
+
+    public Boolean checkZero(String input) {
+        if (input == null || input.isEmpty()) {
+            CalculatorView.printResult(0);
+            return true;
+        }
+        return false;
+    }
+
+
 }
