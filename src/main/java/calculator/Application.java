1450bb9ef8c267aff5f236d3dd1345e89a08a47e
vvalvvizal <vvalvvizal@gmail.com>
2024-10-20 21:54:39 +0900

    feat(application): add exception handling for invalid input
    Throw IllegalArgumentException to terminate application on invalid input.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,15 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-        String input = Console.readLine();
-        Calculator calculator = new Calculator();
-        String[] processedInput = calculator.processing(input);
-        calculator.calculate(processedInput);
-        calculator.print();
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine();
+            Calculator calculator = new Calculator();
+            String[] processedInput = calculator.processing(input);
+            calculator.calculate(processedInput);
+            calculator.print();
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,34 +1,42 @@
 package calculator;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class Calculator {
-    int result = 0;
-    void print(){
-        System.out.println(result);
+    private int result = 0;
+    public void print(){
+        System.out.println("결과 : " + result);
     }
-    String[] processing(String input) {
+    public String[] processing(String input)  {
+
+        if(input==null){
+            throw new IllegalArgumentException("입력값이 null입니다.");
+        }
         String regex = "//(.*)\\\\n";
         Pattern pattern = Pattern.compile(regex);
 
         Matcher matcher = pattern.matcher(input.trim());
-        System.out.println("Input: " + input);
         if (matcher.find()) {
             String customSplitter = matcher.group(1);
-            System.out.println("Custom Splitter: " + customSplitter);
             String restOfInput = input.substring(matcher.end());
 
             return restOfInput.split(customSplitter);
         } else {
-            System.out.println("No match found.");
-            return input.split("[,;]");
+            return input.split("[,:]");
         }
     }
-    void calculate(String[] input){
-        for(int i=0;i<input.length;i++){
-            String s = input[i];
-            this.result += Integer.valueOf(s);
+    public void calculate(String[] input){
+        for (String s : input){
+           if(!s.matches("-?\\d+"))
+           {
+               throw new IllegalArgumentException("숫자가 아닌 값입니다.");
+           }
+           int number = Integer.valueOf(s);
+           if(number<0){
+               throw new IllegalArgumentException("입력한 수가 음수입니다.");
+           }
+            this.result += number;
         }
     }
 }
