d6defcf34c23a0bab608556fbe7d1ff61c05a7af
young-in <hardworkingini@gmail.com>
2024-10-20 21:28:27 +0900

    feat : 음수가 입력 되는 경우 에외 처리 완료

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -45,6 +45,13 @@ public class CalculatorService {
         }catch(NumberFormatException e){
             throw new IllegalArgumentException("숫자가 아닙니다" + token);
         }
+        return checkPositiveNumber(number);
+    }
+
+    private int checkPositiveNumber(int number){
+        if (number < 0){
+            throw new IllegalArgumentException("음수는 허용되지 않습니다." + number);
+        }
         return number;
     }
 }
