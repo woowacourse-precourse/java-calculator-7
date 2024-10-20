2ef146615b680c8ed4c90047d36591649399d14a
GwonDooHyeon <secure3141@gmail.com>
2024-10-20 23:52:56 +0900

    refactor: 쉼표 또는 콜론을 구분으로 가지는 경우 구분자의 합 구하는 로직 리팩토링

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,13 +6,9 @@ import java.util.Arrays;
 
 public class Application {
     
-    private static final String COMMA = ",";
-    private static final String COLON = ":";
     private static final String DELIMITER = "[,:]";
     
     public static void main(String[] args) {
-        int result = 0;
-        
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         
         String input = Console.readLine();
@@ -20,40 +16,20 @@ public class Application {
             return;
         }
         
-        if (input.contains(COMMA) && input.contains(COLON)) {
-            String[] numbers = input.split(DELIMITER);
-            
-            try {
-                result = Arrays.stream(numbers)
-                    .mapToInt(Integer::parseInt)
-                    .sum();
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
-            }
-        } else if (input.contains(COMMA)) {
-            String[] numbers = input.split(COMMA);
-            
-            try {
-                result = Arrays.stream(numbers)
-                    .mapToInt(Integer::parseInt)
-                    .sum();
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
-            }
-        } else if (input.contains(COLON)) {
-            String[] numbers = input.split(COLON);
-            
-            try {
-                result = Arrays.stream(numbers)
-                    .mapToInt(Integer::parseInt)
-                    .sum();
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
-            }
-        }
-        
-        
+        int result = calculateSum(input);
         System.out.println("결과 : " + result);
+    }
+    
+    private static int calculateSum(String input) {
+        String[] numbers = input.split(DELIMITER);
+        
+        try {
+            return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
+        }
     }
     
 }
