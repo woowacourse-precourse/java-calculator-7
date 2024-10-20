862ee678ca73e0d001084f0765923343433a18b8
hanwool <hanwe92@gmail.com>
2024-10-20 19:14:48 +0900

    fix: input view function name

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,9 +1,9 @@
-package calculator.controller.view;
+package calculator.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
-    public static String getDelimiterAndNumbers() {
+    public static String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return readLine();
     }
