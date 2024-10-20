67d68e619546a544756396d03e6abd0ae05da58d
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 04:07:24 +0900

    feat(Message): 숫자 문자열 변환 관련 예외 메시지 추가
    음수 문자열일 때
    Long이 표현할 수 없는 숫자일 때
    숫자가 아닌 문자열 일 때

--- src/main/java/calculator/view/Message.java
+++ src/main/java/calculator/view/Message.java
@@ -2,6 +2,7 @@ package calculator.view;
 
 public enum Message {
 
+    INCLUDE_NEGATIVE_NUMBER("음수가 포함되어 있습니다."),
     INCLUDE_NOT_NUMBER("숫자가 아닌 값이 포함되어 있습니다."),
     INSTRUCTION_MESSAGE("덧셈할 문자열을 입력해 주세요."),
     INVALID_DELIMITER_ORDER("'//'는 '\\n'보다 앞에 위치해야 합니다."),
@@ -9,7 +10,8 @@ public enum Message {
     INVALID_NUMERIC_CUSTOM_DELIMITER("커스텀 구분자는 숫자가 될 수 없습니다."),
     NO_INPUT("입력이 없습니다."),
     ONLY_ONE_CUSTOM_DELIMITER("커스텀 구분자는 한 개 이어야 합니다."),
-    RESULT_MESSAGE("결과 : ");
+    RESULT_MESSAGE("결과 : "),
+    TOO_BIG_NUMBER("숫자가 너무 큽니다.");
 
     private final String message;
 
