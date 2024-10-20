825633bca03423abac8697cdfbba908ae92cc6d3
MelGob <mipo5204@naver.com>
2024-10-20 23:38:45 +0900

    문자열 입력받기-2

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,9 +3,10 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 문자열 입력받기
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String s;
-        s = camp.nextstep.edu.missionutils.Console.readLine();
-
+        String inputStr;
+        inputStr = camp.nextstep.edu.missionutils.Console.readLine();
+        System.out.println(inputStr);
     }
 }
