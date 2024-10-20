40b348910e8ed2ec43b97a4f3fe1356812234193
whrjsgml0000 <whrjsgml4515@naver.com>
2024-10-20 12:38:27 +0900

    refactor:view를 따로 관리하도록 분리함

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,24 +1,29 @@
 package calculator.controller;
 
 import calculator.service.CalculatorService;
-import camp.nextstep.edu.missionutils.Console;
+import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class CalculatorController {
     private final CalculatorService calculatorService;
+    private final InputView inputView;
+    private final OutputView outputView;
 
     public CalculatorController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
         this.calculatorService = new CalculatorService();
     }
 
     public void input() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String s = Console.readLine();
-        output(s);
+        String s = inputView.inputString();
+        inputView.close();
+        int addedResult = calculatorService.add(s);
+        output(addedResult);
     }
 
-    private void output(String s) {
-        int result = calculatorService.add(s);
-        System.out.println("결과 : " + result);
+    private void output(int i) {
+        outputView.outputInteger(i);
     }
 
 }
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,7 @@
+package calculator.view;
+
+public class OutputView {
+    public void outputInteger(int i) {
+        System.out.println("결과 : " + i);
+    }
+}
