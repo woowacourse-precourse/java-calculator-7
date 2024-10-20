9cf31dde3c7d21ec2ab99346d243116628710517
jinno Yun <jinno321@naver.com>
2024-10-21 00:11:46 +0900

    feat: Num 추출 메서드 작성

--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -15,4 +15,35 @@ public class Separator {
         nums = new ArrayList<>();
     }
 
+    public List<Integer> extractNumbers(String input) {
+        List<Integer> numbers = new ArrayList<>();
+        StringBuilder currentNumber = new StringBuilder();
+
+        for (char ch : input.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                currentNumber.append(ch);
+            }
+            else{
+                if (!currentNumber.isEmpty()) {
+                    numbers.add(Integer.parseInt(String.valueOf(currentNumber)));
+                    currentNumber.setLength(0);
+                }
+            }
+        }
+
+        if (!currentNumber.isEmpty()) {
+            numbers.add(Integer.parseInt(String.valueOf(currentNumber)));
+        }
+
+        return numbers;
+    }
+
+    public List<Integer> getNums() {
+        return nums;
+    }
+
+    private void saveNums(List<Integer> inputs){
+        nums.addAll(inputs);
+    }
+
 }
