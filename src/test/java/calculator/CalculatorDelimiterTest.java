1fd02a07d9b1f5ddafaac1be8c7ea96fcb817e3a
minSsan <msp1125@hanyang.ac.kr>
2024-10-21 02:11:32 +0900

    test(CalculatorDelimiterTest): Delimiter 테스트 분리

---
+++ src/test/java/calculator/CalculatorDelimiterTest.java
@@ -0,0 +1,82 @@
+package calculator;
+
+import calculator.exception.InvalidInputException;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CalculatorDelimiterTest {
+    @Test
+    void 커스텀_구분자는_맨_앞에_위치한다() {
+        assertSimpleTest(() -> {
+            // given
+            final String input = "1;2;3//;\\n";
+
+            assertThatThrownBy(() -> {
+                // when
+                new CalculatorDelimiter(input);
+                // then
+            }).isInstanceOf(InvalidInputException.class);
+        });
+    }
+
+    @Test
+    void 커스텀_구분자의_개행_미포함() {
+        assertSimpleTest(() -> {
+            // given
+            final String input = "//;1;2;3";
+
+            assertThatThrownBy(() -> {
+                // when
+                new CalculatorDelimiter(input);
+                // then
+            }).isInstanceOf(InvalidInputException.class);
+        });
+    }
+
+    @Test
+    void 커스텀_구분자의_슬래시_미포함() {
+        assertSimpleTest(() -> {
+            // given
+            final String input = ";\\n1;2;3";
+
+            assertThatThrownBy(() -> {
+                // when
+                new CalculatorDelimiter(input);
+                // then
+            }).isInstanceOf(InvalidInputException.class);
+        });
+    }
+
+    @Test
+    void 올바른_커스텀_구분자() {
+        assertSimpleTest(() -> {
+            // given
+            final String input = "//;\\n1;2;3";
+
+            // when
+            final CalculatorDelimiter calculatorDelimiter = new CalculatorDelimiter(input);
+            final String regex = calculatorDelimiter.getDelimiterRegex();
+
+            // then
+            assertThat(regex).contains(";");
+        });
+    }
+
+    @Test
+    void test() {
+        List<String> tmp = new ArrayList<>(){{
+            add("");
+            add("1");
+            add("2");
+        }};
+
+        String str = String.join("|", tmp);
+        System.out.println(str);
+    }
+}
