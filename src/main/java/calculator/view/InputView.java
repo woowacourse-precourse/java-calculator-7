f5c494bb81d7079e8df5c2aaf751d3d72ac4e1c0
gladhee <gladhee@icloud.com>
2024-10-20 22:09:25 +0900

    feat(InputView, OutputView, ViewMessages): 입출력 로직 작성

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,17 @@
+package calculator.view;
+
+import calculator.dto.CalculatorRequestDto;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static CalculatorRequestDto beginInput() {
+        System.out.println(ViewMessages.INPUT_MESSAGE);
+
+        String input = Console.readLine();
+        Console.close();
+
+        return new CalculatorRequestDto(input);
+    }
+
+}
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+
+    public void printResult(String result) {
+        System.out.println(ViewMessages.RESULT_MESSAGE + result);
+    }
+
+}
