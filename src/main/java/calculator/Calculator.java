edf134b87bf8cb4470d7204c5cab3fde1f3f497e
JJadeYoon <yess9904@naver.com>
2024-10-21 02:58:49 +0900

    feat(Calculator.java): 커스텀 구분자 추출 함수 구현
    입력 받은 문자열에 커스텀 구분자 사용 조건이 적용되었는지 확인하고, 있다면 추출하여 separator에 추가한다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,6 +9,9 @@ class Calculator {
     private final List<Integer> numbers;
     private int result;
 
+    final static int CUSTOM_SEPARATOR_INDEX = 2;
+    final static int CUSTOM_SEPARATOR_END_INDEX = 5;
+
     public Calculator(String input) {
         this.input = input;
         this.result = 0;
@@ -20,9 +23,17 @@ class Calculator {
 
 
     /***
-     * 입력 받은 문자열의 커스텀 문자열이 있는지 확인
+     * 입력 받은 문자열에 커스텀 문자가 있다면 추출한다.
      */
     private void checkCustomSeparator() {
+        // 커스텀 문자열이 있는지 확인
+        if (input.startsWith("//")) {
+            if (!input.contains("\\n")) {
+                throw new IllegalArgumentException("커스텀 구분자 형식이 잘못되었습니다.");
+            }
+            separators.add(input.charAt(CUSTOM_SEPARATOR_INDEX) + "");
+            this.input = input.substring(CUSTOM_SEPARATOR_END_INDEX);
+        }
     }
 
 
@@ -37,6 +48,8 @@ class Calculator {
      * @return 정수 결과값
      */
     public int calculate() {
+        checkCustomSeparator();
+        // TODO: 입력값이 숫자와 구분자로만 이루어졌는지 확인
         return this.result;
     }
 }
