4d565943e11a2fef3131c2c8cc542fe82142a8a3
Fletacode <bassykd@gmail.com>
2024-10-21 00:49:04 +0900

    fix(Application): Application 로직 수정
    리팩토링 된 클래스들에 맞추어 메인 로직 수정:

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,35 +12,18 @@ public class Application {
 
   public static void main(String[] args) {
 
-    try {
-      //입력값 받기
-      StringInput input = new StringInput();
+    //입력값 받기
+    StringInput input = new StringInput();
 
-      //문자열 유효성 검사
-      if (input.getIsCustomDelimiter() &&
-          !StringChecker.checkString
-              (input.getDelimiters(), input.getNums())) {
-        throw new IllegalArgumentException("문자열이 유효하지 않습니다");
-      }
+    Extracts extracts = new Extracts(input.getNums(), input.getDelimiterRegex());
 
-      Extracts extracts =
-          new Extracts(input.getNums(), input.getDelimiters());
+    extracts.splitNums();
 
-      extracts.extractDelimiter();
-      extracts.extractNum();
+    Answer answer = new Answer(extracts.getNums());
 
-      Answer answer = new Answer(extracts.getNums());
-
-      answer.calSum();
-      answer.printAns();
-
-    } catch (NoSuchElementException |
-             IllegalArgumentException noSuchElementException) {
-      throw new IllegalArgumentException("문자열이 유효하지 않습니다");
-    } finally {
-      Console.close();
-    }
-
+    answer.calSum();
+    answer.printAns();
 
   }
+
 }
