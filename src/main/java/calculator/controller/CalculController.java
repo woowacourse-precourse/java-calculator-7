3b3887e13ddedd42a0f48196cbc3320cefed63aa
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-20 18:45:13 +0900

    feat : controller.CalculController 구현 완료

--- src/main/java/calculator/controller/CalculController.java
+++ src/main/java/calculator/controller/CalculController.java
@@ -1,9 +1,17 @@
 package calculator.controller;
 
+import calculator.service.CalculationService;
+import static calculator.view.Input.getUserInput;
+import static calculator.view.Output.displayResult;
+import static calculator.view.Output.startCalculate;
 
 public class CalculController {
+    private final CalculationService calculationService = new CalculationService();
 
     public void start() {
-
+        startCalculate();
+        String input = getUserInput();
+        Number result = calculationService.calculate(input);
+        displayResult(result);
     }
 }
