c68081146655a69a6af9478408937c6ca49ca8d3
Mini <mini@MiniMac.local>
2024-10-20 17:50:39 +0900

    feat: add user input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 사용자 입력 받기
+        System.out.println("구분자와 양수 구성된 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+
     }
 }
