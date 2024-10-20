9dd185fee11e35b624c56b6d84f3e7b3348b85f4
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:44:17 +0900

    :sparkles: feat(StringCalculator): 파싱된 숫자 리스트의 합을 반환

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -2,9 +2,11 @@ package calculator.model;
 
 public class StringCalculator {
     private final InputParser inputParser;
+    private final SumCalculator sumCalculator;
 
     public StringCalculator() {
         this.inputParser = new InputParser();
+        this.sumCalculator = new SumCalculator();
     }
 
     public int add(String input) {
@@ -12,5 +14,6 @@ public class StringCalculator {
             return 0;
         }
         String[] numbers = inputParser.parse(input);
+        return sumCalculator.calculateSum(numbers);
     }
 }
