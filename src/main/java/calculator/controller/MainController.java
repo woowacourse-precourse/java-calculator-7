23a92ef2e39450e3bffafe5e5f2d583e97d28678
nns503 <73779581+nns503@users.noreply.github.com>
2024-10-20 19:45:10 +0900

    feat: 계산된 결과값을 출력하는 기능 구현

--- src/main/java/calculator/controller/MainController.java
+++ src/main/java/calculator/controller/MainController.java
@@ -1,12 +1,17 @@
 package calculator.controller;
 
+import calculator.dto.AddCalculatorRequest;
+import calculator.dto.AddCalculatorResponse;
 import calculator.view.UserInputView;
 import calculator.view.UserOutputView;
 
 public class MainController {
 
+    CalculatorController calculatorController = new CalculatorController();
     public void start(){
         UserOutputView.printAddStartMessage();
         String userInput = UserInputView.readUserInput();
+        AddCalculatorResponse response = calculatorController.addCalculator(new AddCalculatorRequest(userInput));
+        UserOutputView.printAddResultMessage(response);
     }
 }
--- src/main/java/calculator/view/UserOutputView.java
+++ src/main/java/calculator/view/UserOutputView.java
@@ -1,7 +1,13 @@
 package calculator.view;
 
+import calculator.dto.AddCalculatorResponse;
+
 public class UserOutputView {
     public static void printAddStartMessage(){
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public static void printAddResultMessage(AddCalculatorResponse response){
+        System.out.printf("결과 : %d", response.sum());
     }
 }
