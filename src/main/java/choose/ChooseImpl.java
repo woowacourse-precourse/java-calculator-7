ef54865f92533cf3efb543f02bc108234f826ef8
agi1004 <agi1009@naver.com>
2024-10-20 18:15:36 +0900

    feat: 문자열의 숫자를 나누는 전략을 선별 후 리턴하는 메서드를 가지는 인터페이스의 구현체 생성
    전략: Divide 인터페이스
    해당 문자열이 커스텀 구분자를 가지고 있다면, 커스텀 구분자로 문자열을 나누는 전략 리턴
    그렇지 않다면, 기본 구분자로 문자열을 나누는 전략 리턴

--- src/main/java/choose/ChooseImpl.java
+++ src/main/java/choose/ChooseImpl.java
@@ -1,16 +1,18 @@
 package choose;
 
+import static separate.Separator.*;
+
 import divide.BasicDivide;
 import divide.CustomDivide;
 import divide.Divide;
 
 public class ChooseImpl implements Choose {
 	@Override
-	public Divide choose(final String str) {
-		if (str.equals("") || str.contains(",") || str.contains(":")) {
-			return new BasicDivide();
-		} else {
+	public Divide chooseStrategy(final String str) {
+		if (CUSTOM.hasAllSeparators(str)) {	// 커스텀 구분자 사용
 			return new CustomDivide();
+		} else {	// 기본 구분자 사용
+			return new BasicDivide();
 		}
 	}
 }
