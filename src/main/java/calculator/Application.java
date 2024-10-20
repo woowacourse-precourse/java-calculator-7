1776fc26434624632562a875737a736850082cfd
joon <junkyu83@gmail.com>
2024-10-20 21:55:54 +0900

    feat: 배열 요소 중 잘못된 입력 값 검증

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,8 +7,11 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = Console.readLine();
 
-        // 입력한 문자열로부터 구분자로 분리한 Array을 구한다.
+        // 입력한 문자열로부터 구분자로 분리한 배열을 구한다.
         String[] stringArray = getStringArray(userInput);
+
+        // 배열 요소 중 잘못된 값이 들어있는지 확인한다.
+        checkValidation(stringArray);
 
     }
 
@@ -22,13 +25,22 @@ public class Application {
             // 커스텀 구분영역을 뺀 나머지 입력 값을 확인한다.
             int inputWithSeparatorIndex = userInput.indexOf("\\n") + 2;
             String inputWithSeparator = userInput.substring(inputWithSeparatorIndex);
-            // 구분자로 입력 값을 분리하여 Array로 만들고 리턴한다.
+            // 구분자로 입력 값을 분리하여 배열로 만들고 리턴한다.
             return inputWithSeparator.split(customSeparator);
         }
         // 기본 구분자를 사용한 경우
         else {
-            // 기본 구분자 "," 또는 ":" 로 입력 값을 분리하여 Array 만들고 리턴한다.
+            // 기본 구분자 "," 또는 ":" 로 입력 값을 분리하여 배열로 만들고 리턴한다.
             return userInput.split("[,:]");
         }
     }
+
+    private static void checkValidation(String[] arr) {
+        for (String text: arr) {
+            if (!text.matches("[1-9]\\d*") ) {
+                throw new IllegalArgumentException("wrong text");
+            }
+        }
+    }
+
 }
