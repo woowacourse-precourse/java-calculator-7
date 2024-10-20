acedcc0a8c96ad9baa5cb160019e279e70fc6416
johyewon <johyewon4601@gmail.com>
2024-10-21 05:01:50 +0900

    refactor: 변수명 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,44 +3,44 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    private static final String DEFAULT_SEPERATOR = ",|:";
-    private static final String CUSTOM_SEPERATE_START = "//";
-    private static final String CUSTOM_SEPERATE_END = "\\n";
+    private static final String DEFAULT_SEPARATOR = ",|:";
+    private static final String CUSTOM_SEPARATE_START = "//";
+    private static final String CUSTOM_SEPARATE_END = "\\n";
     private static final String EXCEPTION_MESSAGE = "잘못된 값";
 
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        String customSeperator = "";
+        String customSeparator = "";
 
-        if (isCustomSeperator(input)) customSeperator = extractCustomSeperator(input);
-        String[] number = splitBySeperator(input, customSeperator);
+        if (isCustomSeparator(input)) customSeparator = extractCustomSeparator(input);
+        String[] number = splitBySeparator(input, customSeparator);
 
         System.out.println("결과 : " + sumCalculate(number));
     }
 
-    public static boolean isCustomSeperator(String input) {
-        return input.contains(CUSTOM_SEPERATE_START) && input.contains(CUSTOM_SEPERATE_END);
+    public static boolean isCustomSeparator(String input) {
+        return input.contains(CUSTOM_SEPARATE_START) && input.contains(CUSTOM_SEPARATE_END);
     }
 
-    public static String extractCustomSeperator(String input) {
-        int start = input.indexOf(CUSTOM_SEPERATE_START) + 2; // 커스텀 문자 인덱스 시작
-        int end = input.indexOf(CUSTOM_SEPERATE_END); // 커스텀 문자 인덱스 끝
+    public static String extractCustomSeparator(String input) {
+        int start = input.indexOf(CUSTOM_SEPARATE_START) + 2; // 커스텀 문자 인덱스 시작
+        int end = input.indexOf(CUSTOM_SEPARATE_END); // 커스텀 문자 인덱스 끝
 
         if (end == -1 || start == end) throw new IllegalArgumentException(EXCEPTION_MESSAGE);
         return input.substring(start, end);
     }
 
-    public static String[] splitBySeperator(String input, String customSeperator) {
-        String seperator = DEFAULT_SEPERATOR;
+    public static String[] splitBySeparator(String input, String customSeparator) {
+        String separator = DEFAULT_SEPARATOR;
         if (input.equals("")) return new String[]{"0"};
 
-        if (!customSeperator.equals("")) {
-            input = input.replace(CUSTOM_SEPERATE_START + customSeperator + CUSTOM_SEPERATE_END, "");
-            seperator = DEFAULT_SEPERATOR + "|" + customSeperator;
+        if (!customSeparator.equals("")) {
+            input = input.replace(CUSTOM_SEPARATE_START + customSeparator + CUSTOM_SEPARATE_END, "");
+            separator = DEFAULT_SEPARATOR + "|" + customSeparator;
         }
 
-        return input.split(seperator);
+        return input.split(separator);
     }
 
     public static int sumCalculate(String[] number) {
