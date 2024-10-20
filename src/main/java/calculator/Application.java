e1e9ed71567e7e83c5bbd165839d3e51ac548ba1
yoownny <kelosy00@naver.com>
2024-10-20 18:43:24 +0900

    refactor(Application): try-catch 블록 제거
    - 스택 트레이스를 보여주지 않기 위해 try-catch 블록을 사용했으나
      테스트를 통과하지 못하여 에러 발생
    - try-catch 블록을 제거하여 에외를 직접 전파하도록 수정
    
    Closes #9

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,12 +9,7 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        try {
-            int result = calculator.calculate(input);
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
+        int result = calculator.calculate(input);
+        System.out.println("결과 : " + result);
     }
 }
