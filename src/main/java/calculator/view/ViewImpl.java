6a6e8d49fcc2b552a3da685b2bb7a1b6ea7c5d05
bbangHo <sonsins1@naver.com>
2024-10-20 18:35:44 +0900

    refactor: 뷰 클래스명 수정

--- src/main/java/calculator/view/ViewImpl.java
+++ src/main/java/calculator/view/ViewImpl.java
@@ -1,6 +1,6 @@
 package calculator.view;
 
-public class CalculatorView implements View {
+public class ViewImpl implements View {
     @Override
     public void print(int sumResult) {
         System.out.println("결과 : " + sumResult);
