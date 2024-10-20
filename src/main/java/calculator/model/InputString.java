902dc96e1a0186bc1c7e19605e24cf06c3352b18
byegu <rkdqudrn07@naver.com>
2024-10-20 20:25:15 +0900

    docs : 주석 추가

--- src/main/java/calculator/model/InputString.java
+++ src/main/java/calculator/model/InputString.java
@@ -2,11 +2,19 @@ package calculator.model;
 
 import java.util.List;
 
+/**
+ * 입력받은 문자열 저장을 위한 InputString
+ */
 public class InputString {
     private String userInput;
     private char customSeparator;
     private List<Integer> userOperand;
 
+    /**
+     * 커스텀 구분자를 구분하여 대체 후 input을 저장하기 위한 메소드
+     *
+     * @param userInput
+     */
     public void setUserInput(String userInput) {
         if(userInput.contains("//") && userInput.contains("\\n")) {
             customSeparator = userInput.replaceAll("[//\n]", "").charAt(0);
