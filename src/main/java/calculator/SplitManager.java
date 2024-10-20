a6e3ac113eb478a731a928579383ea69c4fb6753
mohyerolo <garam2101@naver.com>
2024-10-20 23:25:49 +0900

    fix: SplitManager에서 문자 하나씩 검사, 에러 처리 경우 추가

--- src/main/java/calculator/SplitManager.java
+++ src/main/java/calculator/SplitManager.java
@@ -1,12 +1,40 @@
 package calculator;
 
-import java.util.regex.Pattern;
-
 public class SplitManager {
     public String[] splitString(Input input) {
-        String str = input.getStr().trim();
+        String str = input.getStr();
         String splitter = input.getSplitter();
 
-        return splitter.equals("[,:]") ? str.split("[,:]") : str.split(Pattern.quote(splitter));
+        return splitInputBySplitter(str, splitter);
+    }
+
+    private String[] splitInputBySplitter(String str, String splitter) {
+        String[] split = str.split(splitter);
+        if (split.length < 2 && split[0].isEmpty()) return split;
+        checkValidString(split);
+        return split;
+    }
+
+    private void checkValidString(String[] split) {
+        for (String str : split) {
+            if (!isValidCharacter(str)) {
+                throw new IllegalArgumentException("잘못된 입력으로 프로그램이 종료됩니다. 구분자 외의 문자열은 양수만 가능합니다.");
+            }
+        }
+    }
+
+    private boolean isValidCharacter(String ch) {
+        if (ch.isEmpty()) {
+            return true;
+        }
+        if (ch.equals("0") || ch.contains(" ")) {
+            return false;
+        }
+        try {
+            int num = Integer.parseInt(ch);
+            return num > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
