1525c3188cd3153f9dd55d6b1d40752040727972
true <truee030@gmail.com>
2024-10-20 22:27:20 +0900

    feat: 구분자로 문자열을 분리하는 메서드

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,6 +5,14 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
+    private static final String DEFAULT_DELIMITERS = ",|:";  // 기본 구분자: 쉼표 또는 콜론
+
+
+    // 기본 구분자(쉼표 또는 콜론)로 문자열을 분리하는 메서드
+    private String[] splitWithDefaultDelimiters(String input) {
+        return input.split(DEFAULT_DELIMITERS);
+    }
+
     // 숫자 배열을 합하여 반환하는 메서드
     private int calculateSum(String[] numbers) {
         int sum = 0;
