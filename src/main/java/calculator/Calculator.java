80e1cc025eff205909b3c5f91e0772295f1788ab
chyo1 <hyowon3856@gmail.com>
2024-10-20 13:47:52 +0900

    FEAT : addNum 추가 및 addNums 내부 addNum으로 치환

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,9 +5,23 @@ import java.util.List;
 public class Calculator {
 	private long totalSum;
 
+	/**
+	 * 숫자 하나 더하기
+	 *
+	 * @param num
+	 */
+	public void addNum(Integer num) {
+		totalSum += num;
+	}
+
+	/**
+	 * list의 모든 숫자들 더하기
+	 *
+	 * @param inputNums
+	 */
 	public void addNums(List<Integer> inputNums) {
 		for (Integer n : inputNums) {
-			totalSum += n;
+			addNum(n);
 		}
 	}
 
