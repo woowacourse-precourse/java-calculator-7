3e22c1264016a3619176b6ff984be460b36f52c4
Yong U Cho <yongu2000@naver.com>
2024-10-20 20:34:17 +0900

    feat: 검사가 끝난 문자열 계산하기 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,18 +1,18 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Application {
 
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 입력
         DataRepository dataRepository = new DataRepository();
         new InputHandler(dataRepository);
+
+        // 기능
         LogicHandler logicHandler = new LogicHandler();
 
         List<String> stringList = logicHandler.splitInputString(dataRepository.getCustomSplitter(),
@@ -20,8 +20,7 @@ public class Application {
 
         logicHandler.removeEmpty(stringList);
         logicHandler.checkInputString(stringList);
-        System.out.println(stringList.toString());
-
+        logicHandler.printResult(stringList);
     }
 
 
--- src/main/java/calculator/LogicHandler.java
+++ src/main/java/calculator/LogicHandler.java
@@ -10,6 +10,7 @@ public class LogicHandler {
     public LogicHandler() {
     }
 
+    // 입력받은 문자열 구분하기
     public List<String> splitInputString(String customSplitter, String inputString) {
         StringBuilder sb = new StringBuilder();
         sb.append(customSplitter);
@@ -18,10 +19,12 @@ public class LogicHandler {
         return new ArrayList<>(Arrays.asList(inputString.split(sb.toString())));
     }
 
+    // 빈 칸이 있다면 0으로 간주하여 제외하기
     public void removeEmpty(List<String> inputString) {
         inputString.removeIf(str -> str == null || str.isEmpty());
     }
 
+    // 정수 이외 다른 문자 (음수, 0, 특수문자) 가 있다면 예외 발생
     public void checkInputString(List<String> inputString) {
         for (String i : inputString) {
             if (!i.matches("\\d+")) {
@@ -33,4 +36,12 @@ public class LogicHandler {
         }
     }
 
+    // 검사가 끝난 문자열 계산 및 출력
+    public void printResult(List<String> inputString) {
+        List<Integer> result = new ArrayList<>();
+        for (String i : inputString) {
+            result.add(Integer.parseInt(i));
+        }
+        System.out.println("결과 : " + result.stream().mapToInt(Integer::intValue).sum());
+    }
 }
