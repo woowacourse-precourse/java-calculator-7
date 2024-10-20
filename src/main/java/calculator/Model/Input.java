c103ea0a78f543b63b4301f994a726254937dcff
kimdev <kim__dev@hufs.ac.kr>
2024-10-20 23:01:19 +0900

    refactor(Input): 마지막 문자가 숫자가 아니어도 되도록 수정

--- src/main/java/calculator/Model/Input.java
+++ src/main/java/calculator/Model/Input.java
@@ -3,12 +3,6 @@ package calculator.Model;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-    public static void validInputText(char endChar) {
-        if (!('0' < endChar && endChar <= '9')) {
-            throw new IllegalArgumentException("입력이 올바르지 않습니다. 허용되지 않는 문자가 포함되어 있습니다.");
-        }
-    }
-
     public static String consoleInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String rawText = Console.readLine();
@@ -16,7 +10,6 @@ public class Input {
         if (rawText.length() == 0) {
             throw new IllegalArgumentException("입력이 올바르지 않습니다. 공백은 허용되지 않습니다.");
         }
-        validInputText(rawText.charAt(rawText.length()-1));
 
         return rawText;
     }
