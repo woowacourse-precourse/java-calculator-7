f8934e279ccb1f96467fdd7329275c6f6823c72c
minyeongg <1030pmy@gmail.com>
2024-10-20 21:18:47 +0900

    feat : 결과 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -82,6 +82,12 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        try {
+            int result = add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
 
