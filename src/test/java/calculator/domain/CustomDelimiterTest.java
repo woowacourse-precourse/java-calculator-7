660c60fde181497d9602912bf8c7fa163667e590
kjyyjk <rlawnsdud920@gmail.com>
2024-10-20 21:58:45 +0900

    test: CustomDelimiterTest null값 테스트 추가
    - null로 null을 value로 가지는 CustomDelimiter를 생성할 수 있다
    - 커스텀 구분자의 존재 여부를 반환하는 isValueExist 테스트

--- src/test/java/calculator/domain/CustomDelimiterTest.java
+++ src/test/java/calculator/domain/CustomDelimiterTest.java
@@ -2,6 +2,9 @@ package calculator.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
@@ -9,7 +12,8 @@ import static org.assertj.core.api.Assertions.*;
 class CustomDelimiterTest {
 
     @ParameterizedTest
-    @DisplayName("CustomDelimiter는_길이_1의_문자열을_입력받아_생성될_수_있다")
+    @DisplayName("CustomDelimiter는_null_또는_길이_1의_문자로_생성될_수_있다")
+    @NullSource
     @ValueSource(strings = {"t", " ", "1", "-"})
     public void create(String input) {
         //given
@@ -22,14 +26,29 @@ class CustomDelimiterTest {
     }
 
     @ParameterizedTest
+    @CsvSource({"d,true", ",false"})
+    @DisplayName("CustomDelimiter는_값의_존재_여부를_반환할_수_있다")
+    public void isValueExist(String input, boolean expected) {
+        //given
+        //when
+        CustomDelimiter result = new CustomDelimiter(input);
+
+        //then
+        assertThat(result.isValueExist())
+                .isEqualTo(expected);
+    }
+
+    @ParameterizedTest
     @DisplayName("CustomDelimiter는_길이_1이_아닌_문자열을_입력받아_생성되면_예외를_던진다")
-    @ValueSource(strings = {"", "  ", "test", "22", "--"})
+    @EmptySource
+    @ValueSource(strings = {"  ", "test", "22", "--"})
     public void LengthException(String input) {
         //given
         //when
         //then
         assertThatThrownBy(() -> new CustomDelimiter(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage("구분자는 길이 1의 문자여야합니다");
+                        .hasMessage("커스텀 구분자는 한 문자여야합니다");
     }
+
 }
