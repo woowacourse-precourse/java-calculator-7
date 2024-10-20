ee7bd9ccd2d08b8bf84dada1c098eb5641273f85
이희수 <hslee9234@naver.com>
2024-10-20 23:03:33 +0900

    feat: 입출력 및 통합 실행 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,27 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        StringParser parser = new StringParser();
+        StringValidator validator = new StringValidator();
+        StringCalculator calculator = new StringCalculator();
+
+        try {
+            validator.validate(input);
+            List<String> numbers = parser.parse(input);
+            validator.validateNumbers(numbers);
+            String result = calculator.add(numbers);
+            System.out.println("결과 : " + result);
+        } catch (CalculatorException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
