c9f22475127c1ebf0e8d0ea585a1b7d3577a86eb
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-20 19:42:04 +0900

    refactor: 불필요한 메서드 삭제

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -10,13 +10,9 @@ public class Validator {
     private static final String WRONG_CUSTOM_SEPARATOR_FORM_MESSAGE = "custom 구분자의 형식이 잘못되었습니다.";
     private static final String IS_BEYOND_COMPUTE_MESSAGE = "계산할 수 없는 범위입니다.";
 
-    public static void checkCustomSeparator(String input) {
-        isCorrectCustomSeparatorForm(input);
-    }
-
-    public static void isCorrectCustomSeparatorForm(String input) {
-        input = Parser.getSeparatorString(input);
-        isCorrectPair(input);
+    public static void validateCustomSeparator(String input) {
+        String separatorString = Parser.getSeparatorString(input);
+        isCorrectPair(separatorString);
     }
 
     public static void isCorrectDefaultInput(String input) {
@@ -61,7 +57,7 @@ public class Validator {
     }
 
     private static boolean isCorrectLastElement(String input, int idx) {
-        if (input.charAt(idx) == '\\' && input.charAt(idx + 1) == 'n') {
+        if (input.startsWith(PREFIX_CUSTOM_LAST, idx)) {
             return true;
         }
         return false;
