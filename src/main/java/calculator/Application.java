da0141afd5ae96c2c6586f773bd4e566bfc39448
YoungGaLee <rkduddl1717@gmail.com>
2024-10-20 19:32:30 +0900

    fix: Update 기본 구분자 동작
    커스텀 구분자를 지정하더라도 '기본' 구분자는 항상 동작해야한다고 판단되어 코드를 수정하였다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,20 +2,20 @@ package calculator;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
-    public static void main(String[] args) {
-        String delimiter = ",|:";
+    public void calculator() {
+        String splitor = ",|:";
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String inputString = readLine();
         if (inputString.startsWith("//")){
             String[] findDelim = inputString.split("\\\\n");
-            delimiter = findDelim[0].substring(2);
+            String delimiter = findDelim[0].substring(2);
             inputString = inputString.substring(4+delimiter.length());
+            splitor += "|" + delimiter;
         }
 
-        String[] separatedInput = inputString.split(delimiter);
+        String[] separatedInput = inputString.split(splitor);
         int sumValue = 0;
         for(int i=0; i<separatedInput.length;i++){
-            // TODO:  예외처리
             try{
                 sumValue += Integer.parseInt(separatedInput[i]);
             }catch(Exception e){
@@ -23,5 +23,8 @@ public class Application {
             }
         }
         System.out.println(String.format("결과 : %d", sumValue));
+    }
+    public static void main(String[] args) {
+        new Application().calculator();
     }
 }
