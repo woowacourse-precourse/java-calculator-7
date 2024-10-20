8f83fb80cefd3641ff3ce6d654a9eba014629e8e
JamogFrinds <ghrb6769@gmail.com>
2024-10-21 00:39:40 +0900

    feat: 0을 입력할 경우 추가

--- src/main/java/calculator/View/Input.java
+++ src/main/java/calculator/View/Input.java
@@ -5,6 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     public String userInput(){
         System.out.println("덧샘할 문자열을 입력해 주세요.");
-        return Console.readLine();
+        String input = Console.readLine();
+        if(input == "")
+            input = "0";
+        return input;
     }
 }
