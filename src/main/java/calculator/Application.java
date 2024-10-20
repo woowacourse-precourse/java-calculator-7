965c1523b52f613ade8c481026ce16bd899a9a23
xeulbn <gsbtiger0215@gmail.com>
2024-10-20 22:37:17 +0900

    오류 기능 추가 및 메서드 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,40 +4,32 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 
 public class Application {
+
+
     public static void main(String[] args) {
+
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputStr= Console.readLine();
-        String[] splitStr;
 
-        if(inputStr.startsWith("//")){
-            inputStr=changeString(inputStr);
+        try{
+            Method method= new Method();
+
+            String[] splitStr;
+
+            if(inputStr.startsWith("//")){
+                inputStr=method.changeString(inputStr);
+            }
+
+            splitStr=method.changeToArray(inputStr);
+
+            int answer= Arrays.stream(splitStr)
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+
+            System.out.println("결과 : "+answer);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
         }
-
-        splitStr=changeToArray(inputStr);
-
-        int answer= Arrays.stream(splitStr)
-                        .mapToInt(Integer::parseInt)
-                                .sum();
-
-        System.out.println("결과 : "+answer);
-    }
-
-    public static String changeString(String input){
-        input= input.substring(4);
-        String index= input.substring(2,3);
-        input=input.replace(index,",");
-
-        return input;
-    }
-
-    public static String[] changeToArray(String changeInput){
-        String [] toArray= changeInput.split(",|;");
-
-        for (int i = 0; i < toArray.length; i++) {
-            toArray[i] = toArray[i].replaceAll("[^0-9]", "");
-        }
-
-        return toArray;
     }
 
 }
---
+++ src/main/java/calculator/Method.java
@@ -0,0 +1,23 @@
+package calculator;
+
+public class Method {
+
+    public String changeString(String input){
+        input= input.substring(4);
+        String index= input.substring(2,3);
+        input=input.replace(index,",");
+
+        return input;
+    }
+
+    public String[] changeToArray(String changeInput){
+        String [] toArray= changeInput.split(",|;");
+
+        for (int i = 0; i < toArray.length; i++) {
+            toArray[i] = toArray[i].replaceAll("[^0-9]", "");
+        }
+
+        return toArray;
+    }
+
+}
