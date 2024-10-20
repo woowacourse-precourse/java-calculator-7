ad4e81ab93242c1a9f146a9965315903877cabdb
junseo Lee <juns0720@naver.com>
2024-10-20 16:34:50 +0900

    feat: "숫자 추출 기능 구현"

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,17 +1,24 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 
 public class Application {
 
     static String result;
+    static List<Integer> numbers;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
         System.out.println(Constants.REQUEST);
         String rawString = Console.readLine();
 
-        StringProcessing.processString(rawString);
+        //문자열 가공
+        numbers = StringProcessing.processString(rawString);
+        System.out.println(numbers);
+
 
 
 
--- src/main/java/calculator/StringProcessing.java
+++ src/main/java/calculator/StringProcessing.java
@@ -6,14 +6,19 @@ import java.util.Arrays;
 public class StringProcessing {
     private static ArrayList<String> seperators = new ArrayList<>(Arrays.asList(",",":"));
     private static ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(0));
-    static String processString(String rawString){
 
+    static ArrayList<Integer> processString(String rawString){
+        int startIdx = 0;
         // 커스텀 구분자 추가
-        if (rawString.startsWith("//"))
+        if (rawString.startsWith("//")) {
+            startIdx = 5;
             findCustomSeperator(rawString);
+        }
 
-        System.out.println(seperators);
-        return "";
+        //숫자 추출
+        extractNumber(rawString.substring(startIdx));
+
+        return numbers;
     }
 
 
@@ -28,8 +33,31 @@ public class StringProcessing {
             tempSeperator.append(rawString.charAt(i));
         }
         if (!isFind)
-            throw new IllegalArgumentException("커스텀 구분자는 \\n으로 끝나야 합니다.");
+            throw new IllegalArgumentException("커스텀 구분자는 //와 \\n사이에 위치해야 합니다.");
         seperators.add(tempSeperator.toString());
+
+    }
+
+    private static void extractNumber(String rawString) {
+        String key = seperators.getFirst();
+        //구분자 통일
+        for (String seperator : seperators)
+            rawString = rawString.replaceAll(seperator, key);
+
+        //숫자 추출
+        StringBuilder tempNumber = new StringBuilder();
+        for(int i = 0; i < rawString.length(); i++){
+            if (Character.toString(rawString.charAt(i)).equals(key)) {
+                try {
+                    numbers.add(Integer.parseInt(tempNumber.toString()));
+                    tempNumber.setLength(0);
+                } catch (NumberFormatException e) {
+                    throw new NumberFormatException("구분자 사이에는 숫자만 입력해야 합니다.");
+                }
+            }
+            else
+                tempNumber.append(rawString.charAt(i));
+        }
 
     }
 }
