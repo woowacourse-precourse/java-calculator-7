1059fc83a753857667e346ebda6acb531a03541a
unh6unh6 <ruddbs3308@naver.com>
2024-10-20 19:00:53 +0900

    test: 커스텀 구분자 포맷 예외 1
    - 커스텀 구분자를 지정하는 앞 쪽 포맷 테스트
    - 클래스 전체에서 쓰는 객체 전역으로 생성

--- src/test/java/calculator/model/CustomDelimiterProcessorTest.java
+++ src/test/java/calculator/model/CustomDelimiterProcessorTest.java
@@ -1,18 +1,27 @@
 package calculator.model;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class CustomDelimiterProcessorTest {
 
+    CustomDelimiterProcessor customDelimiterProcessor = new CustomDelimiterProcessor();
+    Delimiters delimiters = new Delimiters();
+
     @Test
     void 커스텀_구분자_추가() {
-        CustomDelimiterProcessor customDelimiterProcessor = new CustomDelimiterProcessor();
-        Delimiters delimiters = new Delimiters();
         customDelimiterProcessor.addCustomDelimiters("//%\\n//#\\n1%2", delimiters);
         assertThat(delimiters.getDelimiters()).
                 isEqualTo(Arrays.asList(new Character[]{',', ':', '%', '#'}));
+    }
+
+    @Test
+    void 커스텀_구분자_포맷_예외_1() {
+        assertThatThrownBy(() ->
+                customDelimiterProcessor.addCustomDelimiters("/%$\\n5,3", delimiters))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
