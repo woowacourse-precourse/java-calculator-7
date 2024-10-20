c3cc2b8422d4b361ae7a4b76fe26b17f0b909232
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-10-20 22:00:58 +0900

    feat(StringSumCalculator.java): 단일 책임 원칙에 의해서 새로운 클래스 생성
    - 문자열에서 숫자를 찾는 기능 추가
    - 찾은 숫자를 sum 변수를 이용해서 총합을 구하는 기능 추가

--- src/main/java/calculator/StringSumCalculator.java
+++ src/main/java/calculator/StringSumCalculator.java
@@ -1,4 +1,45 @@
 package calculator;
 
 public class StringSumCalculator {
+    public int sumInString(String input) {
+        // 숫자 추출 및 합산 로직을 수행
+        // 숫자와 부호를 추출하기 위한 StringBuilder
+        ExceptionHandler exceptionHandler = new ExceptionHandler();
+        StringBuilder currentNumber = new StringBuilder();
+        int sum = 0;
+
+        // 문자열 순회
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            // 음수 검사
+            exceptionHandler.checkForNegativeNumbers(c, i, input);
+
+            sum = numFindInString(c, sum, currentNumber);
+        }
+
+        sum = findTheTotal(sum, currentNumber);
+
+        return sum;
+    }
+
+    public int numFindInString(char c, int sum, StringBuilder currentNumber) {
+
+        if (Character.isDigit(c)) {
+            currentNumber.append(c);
+        } else {
+            sum = findTheTotal(sum, currentNumber);
+        }
+
+        return sum;
+    }
+
+    public int findTheTotal(int sum, StringBuilder currentNumber) {
+        if (currentNumber.length() > 0) {
+            sum += Integer.parseInt(currentNumber.toString());
+            currentNumber.setLength(0);  // StringBuilder 초기화
+        }
+
+        return sum;
+    }
 }
