05baa30afb97b9e0ec8372fe9cc3ffa29c54e15d
FhRh <yiyop@naver.com>
2024-10-20 20:05:25 +0900

    fix(Calculator): 정규표현식 사용을 -> 구분자를 :로 변경
    - :이외의 구분자를 :로 바꾸어 계산하도록 로직을 변경한다.
    - 메서드를 추출하여 보다 간결하게 작성합니다.

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -1,6 +1,9 @@
 package calculator.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Calculator {
 
@@ -8,8 +11,8 @@ public class Calculator {
 
     public Calculator() {
         this.delimiters = new ArrayList<>();
-        this.delimiters.add(",");
         this.delimiters.add(":");
+        this.delimiters.add(",");
     }
 
     public void patchDelimeter(String customDelimiter) {
@@ -19,8 +22,10 @@ public class Calculator {
     }
 
     public int sum(String expression) {
-        String regex = String.join("|", delimiters);
-        String[] numbers = expression.split(regex);
+        //delimiters를 거꾸로 돌면서 구분자로 정의된 문자를 : 으로 변경해야 한다.
+        String changedExpression = replaceDelimitersToColon(expression);
+
+        String[] numbers = changedExpression.split(":");
 
         int sum = 0;
         for (String number : numbers) {
@@ -31,6 +36,16 @@ public class Calculator {
         }
 
         return sum;
+    }
+
+    private String replaceDelimitersToColon(String expression) {
+        List<String> subDelimiters = delimiters.subList(1, delimiters.size());
+        Collections.reverse(subDelimiters);
+
+        for(String delimiter : subDelimiters){
+            expression = expression.replace(delimiter,":");
+        }
+        return expression;
     }
 
     private boolean isPositiveNumber(String number) {
