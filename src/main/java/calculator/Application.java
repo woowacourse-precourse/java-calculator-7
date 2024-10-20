0e0289e2e9bcc86801b0cd82aaaac9c695738cef
JamogFrinds <ghrb6769@gmail.com>
2024-10-20 23:57:51 +0900

    feat: 계산기 시작 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.Controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller();
+
+        controller.calculator();
     }
 }
