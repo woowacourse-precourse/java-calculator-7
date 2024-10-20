3a73dc1b43ec2d1db6840ca3c285e58593cabfe0
AYEOOON <k93853471@gmail.com>
2024-10-20 13:53:46 +0900

    refactor: Application 클래스 수정
    - 입력 처리 및 결과 출력 로직 정리
    - 메서드 분리 및 코드 가독성 개선

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         String input = getInput();
-        System.out.println("결과 : " + process_to_calculate(input));
+
+        // 구분자 추출
+        String delimiter = ExtractDelimiter.extract_delimiter(input);
+
+        // 문자열 전처리
+        String refine_string = SplitInput.refine(input);
+
+        // 문자열 분리
+        String[] nums = SplitInput.split(refine_string, delimiter);
+
+        System.out.println("결과 : " + AddingNum.sum(nums));
     }
 
     // 사용자의 입력을 받는 메서드
@@ -14,19 +24,4 @@ public class Application {
         Scanner sc = new Scanner(System.in);
         return sc.nextLine().replace("\\n", "\n"); // 문자열에서 "\n"을 실제 줄바꿈 문자로 변환
     }
-
-    private static int process_to_calculate(String input) {
-        // 구분자 추출
-        String delimiter = ExtractDelimiter.extract_basic_delimiter(input);
-
-        // 문자열 전처리
-        String refine_string = SplitInput.refine(input);
-
-        // 문자열 분리
-        String[] nums = SplitInput.split(refine_string, delimiter);
-
-        // 숫자를 더하고 결과 반환
-        return AddingNum.sum(nums);
-    }
-
 }
