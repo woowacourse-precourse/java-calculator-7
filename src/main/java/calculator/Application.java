8a8d9649cb04bf213a7f45dc30a615ae999b913f
chaerish <ccyy8432@naver.com>
2024-10-20 23:50:26 +0900

    refactor(Application): main에 controller만 남기기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         CalculatorController calculatorController = new CalculatorController();
-        System.out.println("결과 : " + calculatorController.startCalculate());
+        calculatorController.startCalculate();
     }
 }
