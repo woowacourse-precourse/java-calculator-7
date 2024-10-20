c54e088b54b11f8b2cb3370350221f8e2bd20301
hyunjilee1132 <hyunji.lee1132@gmail.com>
2024-10-20 20:45:41 +0900

    feat: 기본 구분자 및 커스텀 구분자 판별 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,5 +7,21 @@ public class Application {
         // 입력받기
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String line = readLine();
+
+        // custom 판별
+        boolean custom = isCustom(line);
+    }
+
+    // 기본 구분자 및 커스텀 구분자 판별
+    public static boolean isCustom(String line) {
+        //*\n
+        if (line.length() >= 5) {
+            if (line.charAt(0) == '/'
+                    && line.charAt(1) == '/'
+                    && line.charAt(3) == '\\'
+                    && line.charAt(4) == 'n') {
+                return true;
+            } else return false;
+        } else return false;
     }
 }
