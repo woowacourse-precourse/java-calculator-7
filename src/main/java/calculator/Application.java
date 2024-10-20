54d7a1e66f0ec87abbb4ceb07b0c9652eabc4727
koojun99 <koojun99@naver.com>
2024-10-20 17:25:19 +0900

    feat(Application): handle empty input
    입력 값이 빈 문자열인 경우(null 또는 "") 0을 반환하도록 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 사용자 입력 받기
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        // 계산 결과 출력
+        int result = calculate(input);
+        System.out.println("결과 : " + result);
+    }
+
+    private static int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        // TODO: 계산 로직 구현
+        return 1;
     }
 }
