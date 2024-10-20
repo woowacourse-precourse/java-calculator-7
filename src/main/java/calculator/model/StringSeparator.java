e252eb6b74136d5b87f965f1288d0bc042294bcd
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-10-20 19:51:32 +0900

    feat : 커스텀 구분자 판별 로직 추가

--- src/main/java/calculator/model/StringSeparator.java
+++ src/main/java/calculator/model/StringSeparator.java
@@ -13,6 +13,15 @@ public class StringSeparator {
     }
 
     private String[] separateNumbers(String inputString) {
+        if (inputString.substring(0, 2).equals("//") && inputString.substring(3, 5).equals("\\n")) {
+            return separateNumbersByCustom(inputString);
+        }
         return inputString.split(",|;");
+    }
+
+    private String[] separateNumbersByCustom(String input) {
+        char customSeparator = input.charAt(2);
+        String numbers = input.substring(5);
+        return numbers.split(",|;|" + customSeparator);
     }
 }
