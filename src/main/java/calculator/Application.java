87bff28b6874bf00c1d22c1fdfd04f6e61e97398
Hyejeong <thswjd0609@gmail.com>
2024-10-20 19:09:59 +0900

    feat: 커스텀 구분자 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String line = Console.readLine();
+
+        String[] nums;
+        int sum = 0;
+        String delimiter = ",|:";
+
+        // 커스텀 구분자
+        if (line.startsWith("//")) {
+            nums = line.split("\\\\n", 2);
+            if (nums.length == 1) {
+                throw new IllegalArgumentException("Invalid custom delimiter format");
+            } else {
+                delimiter += "|" + nums[0].substring(2);
+                line = nums[1];
+            }
+        }
     }
 }
