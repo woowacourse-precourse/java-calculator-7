481274e5076f53afd1d495f8680c3b061437d6d8
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2024-10-21 00:52:56 +0900

    feat: split 함수 사용 시, 구두점과 알파벳 format 처리

--- src/main/java/calculator/Spliter.java
+++ src/main/java/calculator/Spliter.java
@@ -22,9 +22,13 @@ public class Spliter {
         if(isNum(inputString.charAt(0))) { // 문자열 시작이 숫자이면
             result = inputString.split(":|,");
         } else { // 문자열 시작이 문자이면
-            // 커스텀 구분자를 추출하여 문자열 Split
             generateCustomSeparator();
-            result = inputString.split(":|,|" + "\\" + customSeparator1);
+
+            if(customSeparator1 >= 65 && customSeparator1 <= 90 || customSeparator1 >= 97 && customSeparator1 <= 122) {
+                result = inputString.split(":|,|" + customSeparator1);
+            } else {
+                result = inputString.split(":|,|" + "\\" + customSeparator1);
+            }
         }
 
         // result 배열 내 잘못된 변수 존재 시 프로그램 종료
@@ -46,23 +50,24 @@ public class Spliter {
         }
     }
 
+    // 작성 필요
     public void generateCustomSeparator() {
         if(inputString.length() >= 5) {
             char[] inputArray = inputString.toCharArray();
 
             // '//?\n' 형태로 시작하면 커스텀 구분자 등록 후 문자열 시작위치 변경
-
             customSeparator1 = inputArray[2];
             this.inputString = inputString.substring(5);
         }
     }
 
+    // 작성 필요
     public boolean inspectionValue(String[] result) {
         return true;
     }
 
     public void sum(String[] result) {
-        System.out.println("커스텀 구분자 = " + customSeparator1 + ", result = " + Arrays.toString(result));
+        //System.out.println("커스텀 구분자 = " + customSeparator1 + ", result = " + Arrays.toString(result));
         for(int i=0; i<result.length; i++) {
             this.resultValue += Integer.parseInt(result[i]);
         }
