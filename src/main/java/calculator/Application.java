56bbc82f65ad10ac3180d32149b257a3e9332959
1998ray <1998ray98@gmail.com>
2024-10-21 07:35:15 +0900

    feat : 쉼표와 콜론으로 구분된 숫자의 덧셈 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -13,16 +13,24 @@ public class Application {
         System.out.println("결과 : "+ result);
     }
 
-    // 1. 빈 문자열 또는 null 입력 시 0을 반환
+        // 1. 빈 문자열 또는 null 입력 시 0을 반환
     public static int add(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        return -1;
-    }
+        
+        // 2. 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열의 숫자 합을 반환
+        String[] numbers = input.split("[,|:]");
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
 
-    // 2. 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열의 숫자 합을 반환
-    // 3. 커스텀 구분자를 지정하여 숫자의 합을 반환
-    // 4. 잘못된 입력 시 IllegalArgumentException을 발생
+        // 3. 커스텀 구분자를 지정하여 숫자의 합을 반환
+        // 4. 잘못된 입력 시 IllegalArgumentException을 발생
+    }
+    
+
 
 }
