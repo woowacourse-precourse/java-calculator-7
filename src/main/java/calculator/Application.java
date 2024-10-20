6e575ee455c5228ddb4957b8ba72e68cd4df6cfb
Mini <mini@MiniMac.local>
2024-10-20 23:22:18 +0900

    feat: add exception handling for negative numbers and invalid input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,7 +12,7 @@ public class Application {
 
         //빈 문자열 또는 null이 입력된 경우 0을 반환
         if(input == null || input.equals("")) {
-            System.out.println("0");
+            System.out.println("결과: 0");
             return;
         }
         //쉼표 or 콜론을 구분자로 숫자 추출 후 합산
@@ -22,20 +22,40 @@ public class Application {
         //커스텀 구분자 처리
         if (input.length() > 2 && input.charAt(0) == '/' && input.charAt(1) == '/') {
             int nindex = input.indexOf("\n");
+            if (nindex == -1) {
+                throw new IllegalArgumentException("커스텀 구분자 형식이 잘못되었습니다.");
+            }
             delimiter = delimiter + "|" + input.substring(2, nindex);
-            numbers = (input.substring(nindex+1)).split(delimiter);
+            numbers = (input.substring(nindex + 1)).split(delimiter);
 
 
 
         }
 
         int sum = 0;
-        for (String number : numbers) {
-            sum += Integer.parseInt(number.trim());
+
+        // 음수이거나 숫자가 아닌경우 예외처리
+        try {
+            for (String number : numbers) {
+                int parseNumber = Integer.parseInt(number.trim());
+                if (parseNumber < 0) {
+                    throw new IllegalArgumentException("음수는 입력할 수 없습니다");
+                }
+                sum += parseNumber;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("잘못된 입력값이 있습니다.");
+            return;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
+
+
+
         }
 
         //테스트용 결과 출력
-        System.out.println(sum);
+        System.out.println("결과: " + sum);
 
 
 
@@ -45,3 +65,4 @@ public class Application {
 
     }
 }
+
