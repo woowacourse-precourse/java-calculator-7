a2ddf5baceb13cc54af4663e61e33ad82c3698a6
googoo9918 <1013as@naver.com>
2024-10-21 01:44:37 +0900

    feat(controller): 출력 결과를 처리하는 OutputHandler 추가
    - 결과 값을 출력하는 printResult 메서드 구현

--- src/main/java/calculator/controller/OutputHandler.java
+++ src/main/java/calculator/controller/OutputHandler.java
@@ -2,6 +2,6 @@ package calculator.controller;
 
 public class OutputHandler {
   public void printResult(String result){
-    System.out.println("결과: " + result);
+    System.out.println("결과 : " + result);
   }
 }
