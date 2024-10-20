27cbb3e5de4f7fda4615ef900c975c451147ac56
zjhj0814 <zjhj0814@gmail.com>
2024-10-20 22:27:26 +0900

    refactor(StringCalculator): 세터 주입에서 생성자 주입으로 변경
    프로그램의 유지보수를 위해 생성자 주입으로 변경한다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,13 +6,13 @@ import calculator.calculateStrategy.EmptyStringCalculateStrategy;
 import calculator.calculateStrategy.IntegerCalculateStrategy;
 import calculator.delimiterStrategy.CustomDelimiterStrategy;
 import calculator.delimiterStrategy.DefaultDelimiterStrategy;
+import calculator.delimiterStrategy.DelimiterStrategy;
 import calculator.numberStrategy.SimpleNumberStrategy;
 import java.util.NoSuchElementException;
 
 public class Application {
     private static final String inputPhrase = "덧셈할 문자열을 입력해 주세요.";
     private static final String outputPhrase = "결과 : ";
-    private static StringCalculator stringCalculator;
 
     public static void main(String[] args) {
         System.out.println(inputPhrase);
@@ -21,30 +21,28 @@ public class Application {
             userInput = readLine();
             userInput = userInput.replace("\\n", "\n");
 
-            stringCalculator = new StringCalculator(userInput);
-            stringCalculator.setNumberStrategy(new SimpleNumberStrategy());
-            stringCalculator.setCalculateStrategy(new IntegerCalculateStrategy());
-            configureDelimiterStrategy(userInput);
+            StringCalculator stringCalculator = new StringCalculator(userInput, configureDelimiterStrategy(userInput),
+                    new SimpleNumberStrategy(), new IntegerCalculateStrategy());
 
             System.out.println(outputPhrase + stringCalculator.operate());
         } catch (NoSuchElementException e) {
-            userInput = "";
-            stringCalculator = new StringCalculator(userInput);
-            stringCalculator.setCalculateStrategy(new EmptyStringCalculateStrategy());
+            StringCalculator stringCalculator = new StringCalculator("", new CustomDelimiterStrategy(),
+                    new SimpleNumberStrategy(),
+                    new EmptyStringCalculateStrategy());
             System.out.println(outputPhrase + stringCalculator.emptyStringOperate());
         }
     }
 
     private static boolean isCustomDelimiter(String userInput) {
-        return userInput.startsWith(stringCalculator.getStartCustomPattern())
-                && userInput.contains(stringCalculator.getEndCustomPattern());
+        return userInput.startsWith(StringCalculator.getStartCustomPattern())
+                && userInput.contains(StringCalculator.getEndCustomPattern());
     }
 
-    private static void configureDelimiterStrategy(String userInput) {
+    private static DelimiterStrategy configureDelimiterStrategy(String userInput) {
         if (isCustomDelimiter(userInput)) {
-            stringCalculator.setDelimiterStrategy(new CustomDelimiterStrategy());
+            return new CustomDelimiterStrategy();
         } else {
-            stringCalculator.setDelimiterStrategy(new DefaultDelimiterStrategy());
+            return new DefaultDelimiterStrategy();
         }
     }
 }
