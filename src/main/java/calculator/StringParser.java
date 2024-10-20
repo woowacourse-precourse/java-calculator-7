2ac116234be519f54935b65c8034091b0da74c53
이희수 <hslee9234@naver.com>
2024-10-20 19:24:38 +0900

    feat: StringParser 클래스 정의
    StringParser는 입력받은 문자열을 구분자와 양수로 구성된 문자열 List로 분리함

---
+++ src/main/java/calculator/StringParser.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import java.util.List;
+
+public class StringParser {
+    // 입력받은 문자열을 구분자와 양수로 구성된 문자열 List 로 분리함
+    public List<String> parse(String input) {
+
+    }
+    
+}
