bf0084ea4db94f034ab87ee2497599fc370852e9
JangSeHoone <sehoon.jang@vicsnc.com>
2024-10-20 16:29:19 +0900

    feat : calculateController 추가
    - 문자열 내 숫자를 인식해서 계산하는 프로그램

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.controller.CalculateStringNumberController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculateStringNumberController calculateStringNumberController = new CalculateStringNumberController();
+
+        calculateStringNumberController.getResult();
     }
 }
