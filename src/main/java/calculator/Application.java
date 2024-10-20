d9867c6a2e853a8fd28a7c353aada1823d3a3211
Lee Ye Jin <jene0028@gmail.com>
2024-10-20 19:20:10 +0900

    feat: 커스텀 구분자 문자열 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,8 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해주세요.");
-        String input = Console.readLine(); // Input 입력 받기
+        String input = Console.readLine();
         Calculator calculator = new Calculator(input);
-        // Calculator의 결과를 받아서 출력
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,13 +3,29 @@ package calculator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
-
+    private static final String DEFAULT_DELIMITERS = ",|:";
+    private static final String CUSTOM_DELIMITER_PATTERN = "//(.*?)\\\\n(.*)";
     private String input;
     private List<String> delimiters = new ArrayList<>(Arrays.asList(",", ":"));
 
     public Calculator(String input) {
         this.input = input != null ? input : "";
     }
+
+    public void addCustomDelimiter() {
+        Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_PATTERN);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            input = matcher.group(2);
+
+            delimiters.add(Pattern.quote(customDelimiter));
+        }
+    }
+
 }
