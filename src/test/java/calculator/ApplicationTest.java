ea53e643a69bc1c7d38355a1c0ec1e7f6d54e0d9
Chanwon-Seo <scwonn60@gmail.com>
2024-10-20 17:41:10 +0900

    test: 다양한 구분자 및 예외 처리에 대한 테스트 추가
    - 커스텀 구분자를 사용한 계산 결과 테스트 추가
    - 예외 상황 발생 시 IllegalArgumentException을 확인하는 테스트 추가
    - 다양한 구분자를 통한 값 합계 테스트 추가
    - 잘못된 입력 값으로 예외가 발생하는 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,16 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,8 +24,66 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    @DisplayName("다양한 구분자를 사용한 값 합계 테스트")
+    public void testCalculateSum(String input, int expectedSumResult) {
+        assertSimpleTest(() -> {
+            run(input);
+            assertThat(output()).contains("결과 : " + expectedSumResult);
+        });
+    }
+
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(
+                Arguments.of("\n", 0), // 빈 문자열
+                Arguments.of("1", 1), // 기본 구분자
+                Arguments.of("1,2", 3),
+                Arguments.of("1,2:3", 6),
+                Arguments.of("//;\\n1;2", 3), // 커스텀 구분자 - ;
+                Arguments.of("// \\n1 2", 3), // 커스텀 구분자 - {공백}
+                Arguments.of("//?\\n1?2", 3), // 커스텀 구분자 - ?
+                Arguments.of("//;;\\n1;;2;;3", 6), // 커스텀 구분자 - ;;
+                Arguments.of("//;;;;;\\n1;;;;;2;;;;;3", 6), // 커스텀 구분자 - ;;;;;(5자)
+                Arguments.of("//;\\n1,2:3;4", 10), // 커스텀 구분자
+                Arguments.of("//-\\n1-2", 3) // 커스텀 구분자 - -
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("exceptionInputProvider")
+    @DisplayName("잘못된 입력 값으로 예외 발생 테스트")
+    public void testInvalidInputThrowsException(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    static Stream<Arguments> exceptionInputProvider() {
+        return Stream.of(
+                Arguments.of(";1;2"), // 첫 시작이 구분자
+                Arguments.of("1::::"), // 무분별한 구분자
+                Arguments.of("test"), // 양수가 아닌 문자
+                Arguments.of("1:-2:-2"), // 양수가 아닌 문자
+                Arguments.of("-1,2,3"),
+                Arguments.of("/1,2,3"), // 커스텀 구분자 조건 미충족
+                Arguments.of("!//!\\n1!1"),
+                Arguments.of("/;\\n1"),
+                Arguments.of("//;;1;;2;;3"), // \n 누락
+                Arguments.of("/n;\\n1"),
+                Arguments.of("//;n1"),
+                Arguments.of("//1\\n112"), // 구분자가 될 수 없는 정수형
+                Arguments.of("//!1!\\n11!1!2!1!"),
+                Arguments.of("//!!!!!!\\n1!!!!!!2"), // 1 ~ 5자를 초과한 구분자
+                Arguments.of("//\\n12"), // 커스텀 구분자 미입력
+                Arguments.of("2147483647:2"), // 정수형 표햔 범위를 벗어난 경우
+                Arguments.of("                    ") //무분별한 공백
         );
     }
 
