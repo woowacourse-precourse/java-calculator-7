edfebce8bc649a4d640e4be9845e50cc054b9692
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-10-20 21:16:28 +0900

    feat: 덧셈 결과의 범위 검증 로직 추가

--- src/main/java/calculator/domain/AdditionCalculator.java
+++ src/main/java/calculator/domain/AdditionCalculator.java
@@ -2,12 +2,24 @@ package calculator.domain;
 
 import java.util.List;
 
+import calculator.constant.ErrorMessage;
+
 public class AdditionCalculator implements Calculator {
 
 	public long calculate(List<Long> numbers) {
-		return numbers.stream()
-			.mapToLong(Long::longValue)
-			.sum();
+
+		long sum = 0;
+		for (Long number : numbers) {
+			validateSum(sum, number);
+			sum += number;
+		}
+		return sum;
+	}
+
+	private void validateSum(long sum, long nextNumber) {
+		if (nextNumber > Long.MAX_VALUE - sum) {
+			throw new IllegalArgumentException(ErrorMessage.OVER_FLOW_SUM.getMessage());
+		}
 	}
 
 }
