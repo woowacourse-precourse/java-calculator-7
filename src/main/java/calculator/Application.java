d4ab50aba27dcf6e51eac7a51f687edb8626497c
Lee Chae Rin <chaerin0916@gmail.com>
2024-10-20 23:32:32 +0900

    feat: 기본 구분자를 처리하는 부분 추가
    DefaultInput 클래스 생성해서 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import calculator.input.DefaultInput;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        // 덧셈 결과 저장할 변수
+        int result = 0;
+
+        DefaultInput defaultInput = new DefaultInput();
+        result = defaultInput.defaultParseSum(input);
+
+        System.out.println("결과 : " + result);
     }
 }
