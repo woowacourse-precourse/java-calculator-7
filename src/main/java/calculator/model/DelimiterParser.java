9311daa61b34e0be5618d6abf36e6b17cea7f926
kim jihyeon <jihyeon546@gmail.com>
2024-10-20 20:45:03 +0900

    refactor(model): if문 중첩 제거

--- src/main/java/calculator/model/DelimiterParser.java
+++ src/main/java/calculator/model/DelimiterParser.java
@@ -6,18 +6,20 @@ import java.util.regex.Pattern;
 public class DelimiterParser {
 
     public String parseCustomDelimiter(String input) {
-        if (input.startsWith("//")) {
-            int delimiterEndIndex = input.indexOf("\\n");
-            if (delimiterEndIndex != -1) {
-                String customDelimiter = input.substring(2, delimiterEndIndex);
-                if (customDelimiter.isEmpty()) {
-                    throw new IllegalArgumentException(ExceptionMessages.CUSTOM_DELIMITER_ERROR);
-                }
-                return Pattern.quote(customDelimiter);
-            } else {
-                throw new IllegalArgumentException(ExceptionMessages.INVALID_DELIMITER_FORMAT);
-            }
+        if (!input.startsWith("//")) {
+            return null;
         }
-        return null;
+
+        int delimiterEndIndex = input.indexOf("\\n");
+        if (delimiterEndIndex == -1) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_DELIMITER_FORMAT);
+        }
+
+        String customDelimiter = input.substring(2, delimiterEndIndex);
+        if (customDelimiter.isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessages.CUSTOM_DELIMITER_ERROR);
+        }
+
+        return Pattern.quote(customDelimiter);
     }
 }
