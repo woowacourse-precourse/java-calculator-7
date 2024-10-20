7853b1ee236e116b9224937e8ddbaa0f3a7ed4de
Junad-Park <qkrwnsgk1624@icloud.com>
2024-10-21 00:20:53 +0900

    test(DelimiterExtractorTest): "//"에 대응하는 "\n"의 존재 여부에 따른 예외 처리 테스트 추가

---
+++ src/test/java/calculator/util/DelimiterExtractorTest.java
@@ -0,0 +1,37 @@
+package calculator.util;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import calculator.enums.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class DelimiterExtractorTest {
+
+    @Nested
+    @DisplayName("parse 메서드는")
+    class ParseMethod {
+
+        @Test
+        void 대응하는_구분자_접미사를_찾을_수_없는_경우_예외를_발생시킨다() {
+            // given
+            String input = "//;1,2,3";
+
+            // when & then
+            assertThatThrownBy(() -> DelimiterExtractor.parse(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.DELIMITER_SUFFIX_NOT_FOUND.getMessage());
+        }
+
+        @Test
+        void 대응하는_구분자_접미사가_있는_경우_예외를_발생시키지_않는다() {
+            // given
+            String input = "//;\\n1;2;3";
+
+            // when & then
+            assertThatNoException().isThrownBy(() -> DelimiterExtractor.parse(input));
+        }
+    }
+}
