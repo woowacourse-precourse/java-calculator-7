9317e6f477c627d694290dccd591dfd9ec5df63e
geonu <kgo000926@gmail.com>
2024-10-20 22:48:50 +0900

    feat: 결과 메시지 출력

--- src/main/java/calculator/Output.java
+++ src/main/java/calculator/Output.java
@@ -3,8 +3,13 @@ package calculator;
 public class Output {
 
     private static final String START_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String RESULT_MESSAGE = "결과 : ";
 
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
+    }
+
+    public void printResult(int result) {
+        System.out.println(RESULT_MESSAGE + result);
     }
 }
