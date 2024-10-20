3c436bd2a631d327739b738608008c91a2e53b61
Jang-GO <jang8195@naver.com>
2024-10-20 20:11:04 +0900

    feat(BasicStringParser) : 기본 구분자 parser 구현
    - 기본(콤마, 콜론)구분자 기준으로 파싱하여 숫자만 담긴 List<String>를 반환

--- src/main/java/calculator/parser/BasicStringParser.java
+++ src/main/java/calculator/parser/BasicStringParser.java
@@ -1,11 +1,17 @@
 package calculator.parser;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class BasicStringParser implements StringParser{
 
+    private static final String BASIC_DELIMITER_REGEX = "[,|:]";
+
     @Override
-    public List<String> parse(String str) {
-        return null;
+    public List<String> parse(String input) {
+        if (input.isEmpty()) {
+            return List.of("0");
+        }
+        return Arrays.stream(input.split(BASIC_DELIMITER_REGEX)).toList();
     }
 }
