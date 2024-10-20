674aa5c508bffba429cf5f8b04b1381a7734ab06
chaerish <ccyy8432@naver.com>
2024-10-20 21:48:39 +0900

    refactor(CustomSeparator): interface 상속받게 수정

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -6,7 +6,7 @@ public class CalculatorController {
     private String userInput;
     private final String startComment = "덧셈할 문자열을 입력해 주세요.";
     private final String customPrefix = "//";
-    private AbstractSeparator abstractSeparator;
+    private Separator separator;
     private CalculatingMachine calculatingMachine;
 
     public CalculatorController() {
@@ -18,10 +18,10 @@ public class CalculatorController {
         System.out.println(startComment);
         this.userInput = Console.readLine();
         if (userInput.startsWith(customPrefix)) {
-            this.abstractSeparator = new CustomSeparator();
-            return calculatingMachine.calculate(abstractSeparator.separate(userInput));
+            this.separator = new CustomSeparator();
+            return calculatingMachine.calculate(separator.separate(userInput));
         }
-        this.abstractSeparator = new DefaultSeparator();
-        return calculatingMachine.calculate(abstractSeparator.separate(userInput));
+        this.separator = new DefaultSeparator();
+        return calculatingMachine.calculate(separator.separate(userInput));
     }
 }
