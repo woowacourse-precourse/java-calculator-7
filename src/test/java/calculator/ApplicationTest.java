c5000248178841e7a9b76246918b69556b750e37
RaZel713 <razel7130@gmail.com>
2024-10-20 20:42:18 +0900

    test: 커스텀 구분자가 공백일때

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -114,6 +114,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 커스텀_구분자가_공백() {
+        assertSimpleTest(() -> {
+            run("// \\n1 2 3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
