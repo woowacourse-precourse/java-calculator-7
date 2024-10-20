a8ee7c83fbce6c14731682cd2c528ec615d73dcd
softmoca <acyc0909@gmail.com>
2024-10-20 17:16:38 +0900

    refactor: 기존 문자열 계산기 로직 제거 및 새로운 로직 작성을 위한 초기화
    - 기존 계산 로직을 삭제하여 새로운 요구 사항에 맞는 로직을 작성할 준비 완료.
    - 새로운 기능 구현 목록에 따른 리팩토링을 위한 코드 초기화.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,45 +1,12 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
 
-
-
-        int result = calculateSum(input);
-        System.out.println("결과 : " + result);
 
 
     }
 
-    private static int calculateSum(String input) {
-        if (input.isEmpty()) {
-            return 0;
-        }
 
-        input = input.replace("\\n", "\n");
-
-        String delimiter = "[,:]";
-        if (input.startsWith("//")) {
-            String[] parts = input.split("\n", 2);
-
-            delimiter = "[" + parts[0].substring(2) + "]";
-            input = parts[1];
-        }
-
-        String[] tokens = input.split(delimiter);
-
-        int sum = 0;
-        for (String token : tokens) {
-            int number = Integer.parseInt(token);
-            if (number < 0) {
-                throw new IllegalArgumentException("음수는 허용되지 않습니다.");
-            }
-            sum += number;
-        }
-        return sum;
-    }
 }
