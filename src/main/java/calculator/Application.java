b7a63a3e8f963211cfadc0a4ce2e5146ae8af2d0
chino <tnehd83@naver.com>
2024-10-20 17:11:46 +0900

    refactor(Constant.java): 불변값에 대한 관리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,32 +7,32 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        int StartIndex = Constant.ZERO;
         int Result = Constant.ZERO;
+        int StartIndex;
+
+        boolean PatternEnable;
+
+        String AllSeparator;
+        String UserInput;
 
         System.out.println(Constant.INPUT_MESSAGE);
-        String UserInput = valueInput();
-        String AllSeparator;
+        UserInput = valueInput();
 
         if (UserInput.isEmpty()) {
             resultPrint(Constant.ZERO);
             return;
         }
 
-        if (hasPattern(UserInput)) {
-            AllSeparator = ",|:|" + UserInput.substring(Constant.CUSTOM_PATTERN_INDEX);
-            StartIndex = Constant.CUSTOM_PATTERN_STARTINDEX;
-        } else {
-            AllSeparator = ",|:";
-        }
+        PatternEnable = hasPattern(UserInput);
+        AllSeparator = SeparatorSetting(UserInput, PatternEnable);
+        StartIndex = StartIndexSetting(PatternEnable);
 
-        String[] NumberList =
-                UserInput.substring(StartIndex).split(AllSeparator);
+        String[] NumberList = UserInput.substring(StartIndex).split(AllSeparator);
 
         for (String s : NumberList) {
             if (s.matches(Constant.MATCHER_CHECK)) {
                 Result += Integer.parseInt(s);
-            } else if (s.matches("")) {
+            } else if (s.matches(Constant.NONE_INPUT)) {
                 Result += Constant.ZERO;
             } else {
                 throw new IllegalArgumentException();
@@ -42,25 +42,41 @@ public class Application {
         resultPrint(Result);
     }
 
+    private static int StartIndexSetting(boolean patternEnable) {
+        if (patternEnable) {
+            return Constant.CUSTOM_PATTERN_STARTINDEX;
+        } else {
+            return Constant.ZERO;
+        }
+    }
+
+    private static String SeparatorSetting(String UserInput, boolean patternEnable) {
+        if (patternEnable) {
+            return Constant.PATTERN_ENABLE + UserInput.substring(Constant.CUSTOM_PATTERN_INDEX);
+        } else {
+            return Constant.PATTERN_DISABLE;
+        }
+    }
 
     static String valueInput() {
         try {
             return Console.readLine();
         } catch (NoSuchElementException e) {
-            return "";
+            return Constant.NONE_INPUT;
         }
     }
 
     static boolean hasPattern(String input) {
         try {
-            String checkMobile = input.substring(0, 5);
-            return checkMobile.contains("//") & checkMobile.contains("\\n");
+            String checkMobile = input.substring(Constant.ZERO, Constant.PATTERN_CHECK_LAST_POSITION);
+            return checkMobile.contains(Constant.PATTERN_CHECK_FIRST_TEXT)
+                    & checkMobile.contains(Constant.PATTERN_CHECK_LAST_TEXT);
         } catch (StringIndexOutOfBoundsException e) {
             return false;
         }
     }
 
     static void resultPrint(int AllCombinedValues) {
-        System.out.println("결과 : " + AllCombinedValues);
+        System.out.println(Constant.END_MESSAGE + AllCombinedValues);
     }
 }
--- src/main/java/calculator/Constant.java
+++ src/main/java/calculator/Constant.java
@@ -2,6 +2,11 @@ package calculator;
 
 public class Constant {
     public static final int ZERO = 0;
+    public static final String NONE_INPUT = "";
+
+    public static final int PATTERN_CHECK_LAST_POSITION = 5;
+    public static final String PATTERN_CHECK_FIRST_TEXT = "//";
+    public static final String PATTERN_CHECK_LAST_TEXT = "\\n";
 
     public static final int CUSTOM_PATTERN_INDEX = 2;
     public static final int CUSTOM_PATTERN_STARTINDEX = 5;
@@ -9,4 +14,9 @@ public class Constant {
     public static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 
     public static final String MATCHER_CHECK = "[0-9]+";
+
+    public static final String PATTERN_ENABLE = ",|:|";
+    public static final String PATTERN_DISABLE = ",|:";
+
+    public static final String END_MESSAGE = "결과 : ";
 }
