6036627bbea10892af72fcc4e86e86b33c6c9eb0
Kim-dong-kyun <kdk942@gmail.com>
2024-10-20 15:03:56 +0900

    테스트

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # java-calculator-precourse
+
+# feat: empty
+
+## 입력값이 비어있으면 합이 0이므로 0 반환
+----------------------------------------------
+
+# feat: negative number
+
+## 음수의 경우 IllegalArgumentException 반환
+----------------------------------------------
+
+# feat: there is no separator
+
+## 문자열 숫자 정수로 반환
+----------------------------------------------
+
+# feat: there is no custom separator
+
+## 기본 구분자로 분리한 각 숫자의 합 반환
+
+## 이때 숫자가 아닌 다른 문자는 IllegalArgumentException 반환
+----------------------------------------------
+
+# feat: there is coustom separator
+
+## 커스텀 구분자가 있으면 해당 커스텀 구분자도 추가
+
+## 구분자 배열을 만들어주는 방법으로 기능 구현
--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,71 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = Console.readLine();
+
+        if (isBlank(str)) {
+            System.out.println(0);
+        } else {
+            // string to int 반환
+            if (isCustom(str)) {
+                int result = useCustomSeparator(str);
+                System.out.println(result);
+            }
+            // string to int 반환
+            else {
+                int result = basicSeparator(str);
+                System.out.println(result);
+            }
+        }
+
+    }
+
+    public static boolean isBlank(String str) {
+        return (str == null || str.trim().isEmpty());
+    }
+
+    public static boolean isCustom(String str) {
+        return str.startsWith("//") && str.contains("\n");
+    }
+
+    //IllegalArgumentException 생각
+    public static int useCustomSeparator(String str) {
+        int separatorIndex = str.indexOf("\n");
+        String customSeparator = str.substring(2, separatorIndex);
+        String numbersTmp = str.substring(separatorIndex + 1);
+        String[] numbers = numbersTmp.split(customSeparator);
+        System.out.println("구분자 추출");
+        return numberSum(numbers);
+    }
+
+
+    public static int basicSeparator(String str) {
+        String[] numbers = str.split("[,:]");
+        return numberSum(numbers);
+    }
+
+    public static int numberSum(String[] numbers) {
+        int sum = 0;
+        for (String number : numbers) {
+            int num = makeInt(number);
+            if (num < 0) {
+                throw new IllegalArgumentException();
+            }
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static int makeInt(String number) {
+        try {
+            return Integer.parseInt(number.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
