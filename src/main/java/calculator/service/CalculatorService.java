f45f556ce1e9a33224a188dec20cd21e3a008121
young-in <hardworkingini@gmail.com>
2024-10-20 21:04:01 +0900

    feat : 기본 구분자 삭제 완료
    기본 구분자를 지우고, 숫자들로만 구성하도록 하도록 하는 코드 작성 완료

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,10 +1,17 @@
 package calculator.service;
 
 public class CalculatorService {
+
+    private static final String DEFAULT_DELIMITERS = ",|:";
     public int add(String input){
         if (input == null || input.isEmpty()){
             return 0;
         }
+
+        String delimiter = DEFAULT_DELIMITERS;
+        String numbers = input;
+
+        String[] tokens = numbers.split(delimiter);
         return 0;
     }
 }
