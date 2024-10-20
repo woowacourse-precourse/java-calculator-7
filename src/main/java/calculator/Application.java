a2a838cdae974112c4c14783ef22fe69cbba0a4c
htjworld <116538001+htjworld@users.noreply.github.com>
2024-10-20 21:37:46 +0900

    StringIndexOutOfBoundsException 해결 및 코드 정리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -14,7 +14,7 @@ public class Application {
         // 재사용성을 위해 값을 유동적으로 추가할 수 있는 리스트로 설정
 
         // 공백이 입력된 경우(입력값이 없는 경우)
-        if (input.length() == 0){
+        if (input.isEmpty()){
             System.out.println(result);
             return;
         }
@@ -26,15 +26,14 @@ public class Application {
         }
 
         // 커스텀 구분자 확인 및 저장
-        if (input.charAt(0) =='/' & input.charAt(1) == '/'){
+        if (input.length() >=2 && input.charAt(0) =='/' && input.charAt(1) == '/'){
             try{
                 if (!input.contains("\\n")){
                     throw new IllegalArgumentException("입력 문자열에 \\n문자가 포함돼있지 않습니다");
                 }
                 int index = input.indexOf("\\n");
                 String new_delimiter = input.substring(2,index);
-                System.out.println(new_delimiter);
-                input = input.substring(index+2,input.length());
+                input = input.substring(index+2);
                 delimiters.add(new_delimiter);
             }catch (IllegalArgumentException e){
                 System.out.println(e.getMessage());
@@ -43,18 +42,8 @@ public class Application {
 
         }
 
-        // 구분자 최대 3개로 고정
-        try{
-            if(delimiters.size()>3){
-                throw new IllegalArgumentException("커스텀 구분자가 2개 이상 추가되었습니다. 커스텀 구분자는 1개까지만 허용됩니다.");
-            }
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            System.exit(1);
-        }
-
         String splitPattern = String.join("|",delimiters); // 예시 : ",|:|;"
-        String arr[] = input.split(splitPattern); // splitPattern 안에 구분자들을 기준으로 분리함
+        String[] arr = input.split(splitPattern); // splitPattern 안에 구분자들을 기준으로 분리함
 
         try {
             for (String a : arr) {
