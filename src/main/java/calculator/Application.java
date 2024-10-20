a973515e0d6204ea430336b4861b7c6b6053d614
etd93 <etd93@naver.com>
2024-10-20 19:53:48 +0900

    [refactor] 불필요한 주석 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,12 +5,6 @@ import calculator.controller.CalculatorController;
 public class Application {
 
     public static void main(String[] args) {
-//        try{
-//            CalculatorController calculatorController = new CalculatorController();
-//            calculatorController.startCalculating();
-//        }catch(IllegalArgumentException e){
-//            throw e;
-//        }
         CalculatorController calculatorController = new CalculatorController();
         calculatorController.startCalculating();
     }
