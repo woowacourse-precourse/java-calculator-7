30aada0e84504118ece1dfe35a1fe00674bdad4d
jinho7 <69239293+jinho7@users.noreply.github.com>
2024-10-21 01:54:44 +0900

    feat(StringCalculator): 문자열 파싱 기능 구현

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    public String[] splitInput(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("입력값이 null입니다.");
+        }
+
+        if (input.startsWith("//")) {
+            int customIdx = input.indexOf("\n");
+            if (customIdx == -1) {
+                throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다.");
+            }
+
+            String custom = input.substring(2, customIdx);
+            String numberString = input.substring(customIdx + 1);
+            return numberString.split(Pattern.quote(custom));
+        }
+
+        return input.split("[,:]");
+    }
+}
