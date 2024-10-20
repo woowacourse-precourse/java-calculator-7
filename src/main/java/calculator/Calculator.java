07313b99a308335e72a5b0d8860f1127602282d0
seophohoho <seop0937@gmail.com>
2024-10-20 15:48:00 +0900

    feat(Calculator): 빈 입력값 시, 결과값 0으로 처리 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,2 +1,9 @@
-package calculator;public class Calculator {
+package calculator;
+
+public class Calculator {
+    public int calc(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+    }
 }
