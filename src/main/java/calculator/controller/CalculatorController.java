7c1babbc095d896acc906ea32f4460f1cd6bdd7b
Chan Mi <rkdcksal02@gmail.com>
2024-10-21 00:13:53 +0900

    fix(controller): CalculatorController에서 중복된 유효성 검사 제거

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,7 +1,6 @@
 package calculator.controller;
 
 import calculator.model.Calculator;
-import calculator.model.InputValidator;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
@@ -9,18 +8,15 @@ public class CalculatorController {
     private final InputView inputView;
     private final OutputView outputView;
     private final Calculator calculator;
-    private final InputValidator validator;
 
     public CalculatorController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
         this.calculator = new Calculator();
-        this.validator = InputValidator.getInstance();
     }
     public void start() {
         String input = inputView.getInput();
         try {
-            validator.validate(input);
             int result = calculator.add(input);
             outputView.displayResult(result);
         } catch (Exception e) {
