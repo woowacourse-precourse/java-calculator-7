3885aad19b9c2c3ea3d1fcdaab934757c2db35f0
minyeongg <1030pmy@gmail.com>
2024-10-20 17:35:20 +0900

    feat : 숫자 총합 반환하는 기능
    문자열을 int형으로 변환
    변환이 되지 않는 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료
    추출한 숫자가 음수인 경우
    IllegalArgumentException을 발생시킨 후 애플리케이션은 종료
    숫자의 총합을 반환
    
    parseNumber 메소드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -27,6 +27,16 @@ public class Application {
                     throw new IllegalArgumentException("구분자로 시작할 수 없습니다.");
                 } else if(last.equals(":") || last.equals(",") || last.equals(delimiter)) {
                     throw new IllegalArgumentException("구분자로 끝날 수 없습니다.");
+                } else {
+                    numbers = split(input, delimiter);
+                    for(String number : numbers) {
+                        int num = parseNumber(number);
+                        if(num < 0) {
+                            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+                        }
+                        sum += num;
+                    }
+                    return sum;
                 }
             } else {
                 throw new IllegalArgumentException("잘못된 구분자 형식입니다.");
@@ -34,6 +44,26 @@ public class Application {
         }
         return sum;
     }
+
+    private static int parseNumber(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException(
+                    "구분자나 숫자를 연속해서 입력하거나 " +
+                            "구분자로 지정하지 않은 문자를 입력할 수 없습니다.");
+        }
+    }
+
+    private static String[] split(String input, String delimiter) {
+        String temp = input.replace(delimiter, ",");
+        temp = temp.replace(":", ",");
+        if(!temp.contains(",")) {
+            throw new IllegalArgumentException("숫자만 입력할 수 없습니다.");
+        }
+        return temp.split(",");
+    }
+
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
