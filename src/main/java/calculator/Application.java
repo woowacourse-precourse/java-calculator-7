aad7a9bd8ba81e5d6d0c847684e69de6616fcff1
Jeongho <fgh2585@gmail.com>
2024-10-21 02:40:23 +0900

    feat: 출력
    출력 결과 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import calculator.controller.Controller;
+import calculator.model.AddedString;
+import calculator.validation.InputValidator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        InputValidator inputValidator = new InputValidator();
+        OutputView outputView = new OutputView();
+        AddedString model = new AddedString();
+
+        Controller controller = new Controller(inputView, inputValidator, outputView, model);
+
+        controller.run();
     }
 }
