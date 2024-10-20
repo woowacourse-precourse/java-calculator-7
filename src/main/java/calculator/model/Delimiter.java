4aa8f8967ba21c42e6528e39cb35dfd0efd21dd2
neu99 <neu990112@gmail.com>
2024-10-21 08:24:42 +0900

    feat: 구분자 클래스 구현
    - 메시지에 커스텀 구분자가 존재하지 않으면 기본 구분자를 반환한다.
    - 메시지에 커스텀 구분자가 존재하면 커스텀 구분자를 추가하여 반환한다.
    - 커스텀 구분자가 1개 이상의 문자로 이루어져 있을 때를 고려하여 숫자가 시작하는 인덱스를 반환하는 메서드를 구현.

---
+++ src/main/java/calculator/model/Delimiter.java
@@ -0,0 +1,49 @@
+package calculator.model;
+
+import java.util.regex.Pattern;
+
+public class Delimiter {
+    private static final String DEFAULT_DELIMITERS = ",|:";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+
+    private String delimiters;
+
+    public Delimiter(String message) {
+        this.delimiters = DEFAULT_DELIMITERS;
+        parseCustomDelimiter(message);
+    }
+
+    private void parseCustomDelimiter(String message) {
+        if (hasCustomDelimiter(message)) {
+            String customDelimiter = extractCustomDelimiter(message);
+            addCustomDelimiter(customDelimiter);
+        }
+    }
+
+    private boolean hasCustomDelimiter(String message) {
+        return message.startsWith(CUSTOM_DELIMITER_PREFIX);
+    }
+
+    private String extractCustomDelimiter(String message) {
+        int startIndex = CUSTOM_DELIMITER_PREFIX.length();
+        int endIndex = message.indexOf(CUSTOM_DELIMITER_SUFFIX);
+        return message.substring(startIndex, endIndex);
+    }
+
+    private void addCustomDelimiter(String newDelimiter) {
+        String escapedDelimiter = Pattern.quote(newDelimiter);
+        this.delimiters += "|" + escapedDelimiter;
+    }
+
+    public String getRegexString() {
+        return this.delimiters;
+    }
+
+    public int getNumbersStartIndex(String message) {
+        if (hasCustomDelimiter(message)) {
+            return message.indexOf(CUSTOM_DELIMITER_SUFFIX) + CUSTOM_DELIMITER_SUFFIX.length();
+        }
+        return 0;
+    }
+}
