6390bc80e2b6906b26217e0e065f8f6e2e016fd0
LeeHaeJun <haejun0410@gmail.com>
2024-10-20 21:46:02 +0900

    feat: 커스텀 구분자 기능 구현
    커스텀 구분자 찾기 기능 구현

--- src/main/java/calculator/delimiter/CustomDelimiterDetector.java
+++ src/main/java/calculator/delimiter/CustomDelimiterDetector.java
@@ -1,5 +1,16 @@
 package calculator.delimiter;
 
 public class CustomDelimiterDetector implements CustomDelimiterDetectorInterface {
+    private static final String DELIMITER_PREFIX = "//";
+    private static final String DELIMITER_SUFFIX = "\n";
 
+    @Override
+    public String findCustomDelimiter(String input) {
+        int prefixIndex = input.indexOf(DELIMITER_PREFIX);
+        int suffixIndex = input.indexOf(DELIMITER_SUFFIX);
+        if (prefixIndex < suffixIndex && prefixIndex != -1) {
+            return input.substring(prefixIndex + DELIMITER_PREFIX.length(), suffixIndex);
+        }
+        return "";
+    }
 }
