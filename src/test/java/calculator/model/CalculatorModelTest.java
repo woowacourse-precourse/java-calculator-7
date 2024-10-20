6c78e258347372c21814d96af0b99f8ea5fc4fd6
kingjinyong <wlsdydwhswkf@gmail.com>
2024-10-20 10:20:10 +0900

    test(CalculatorModelTest): 커스텀 구분자 지정 문자 사용 예외 테스트 코드 작성

--- src/test/java/calculator/model/CalculatorModelTest.java
+++ src/test/java/calculator/model/CalculatorModelTest.java
@@ -1,5 +1,7 @@
 package calculator.model;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,6 +13,15 @@ class CalculatorModelTest {
     void 커스텀_구분자_지정_문자_올바른_사용(String input) {
         CalculatorModel cm = new CalculatorModel();
         Assertions.assertThat(cm.CustomDiscrimination(input)).isEqualTo(true);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;1,2:3", ";\\n1,2,3;4"})
+    void 커스텀_구분자_지정_문자_사용_예외(String input) {
+        CalculatorModel cm = new CalculatorModel();
+        assertThatThrownBy(() -> cm.CustomDiscrimination(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
