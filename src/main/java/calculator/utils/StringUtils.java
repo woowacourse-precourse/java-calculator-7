b85fa715125cd8e4f79c7859b28c6635f4abb895
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-20 20:11:49 +0900

    feat : utils.StringUtils 생성

---
+++ src/main/java/calculator/utils/StringUtils.java
@@ -0,0 +1,22 @@
+package calculator.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringUtils {
+    private static final String DEFAULT_DELIMITERS = ",|:";
+
+    public static boolean isEmpty(String input) {
+        return input == null || input.trim().isEmpty();
+    }
+
+    public static String[] extractNumbers(String input) {
+        String delimiters = DEFAULT_DELIMITERS;
+        Matcher matcher = Pattern.compile("^//(.)\\n(.*)").matcher(input);
+        if (matcher.find()) {
+            delimiters = matcher.group(1);
+            input = matcher.group(2);
+        }
+        return input.split(delimiters);
+    }
+}
