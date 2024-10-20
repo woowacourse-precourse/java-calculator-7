afdfe4d9361ab2113aa2c1f68f62a5dfdfcbf448
softmoca <acyc0909@gmail.com>
2024-10-20 17:26:06 +0900

    feat: 커스텀 구분자 패턴 인식 기능 추가
    - 문자열에서 "//"와 "\n" 사이의 구분자를 인식하는 기능 추가.
    - isCustomDelimiterPattern() 메서드를 통해 정규식을 사용하여 "//(.)\\n.*" 패턴을 매칭.
    - Matcher 객체를 사용해 패턴이 문자열과 일치하는지 확인하고, 이를 통해 커스텀 구분자 사용 여부를 판단.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,12 +3,23 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
+        if (isCustomDelimiterPattern(input)) {
+           System.out.println("Yes");
+        }
 
+    }
+    private static boolean isCustomDelimiterPattern(String input) {
+        Pattern pattern = Pattern.compile("//(.)\\\\n.*");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
     }
 
 
