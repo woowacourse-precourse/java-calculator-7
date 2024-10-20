cc8a348e9ed28cdad43ae01cd4fbc28592810f27
minseo <alstj778@gmail.com>
2024-10-20 14:33:44 +0900

    fix: 결과값 출력시 실수, 정수 포맷팅 분기처리

--- src/main/java/calculator/OutputHandler.java
+++ src/main/java/calculator/OutputHandler.java
@@ -4,6 +4,18 @@ public class OutputHandler {
     private static final String RESULT_MESSAGE_PREFIX = "결과 : ";
 
     public static void printResult(double result) {
-        System.out.println(RESULT_MESSAGE_PREFIX + result);
+        System.out.println(RESULT_MESSAGE_PREFIX + formatResult(result));
+    }
+
+    private static String formatResult(double result) {
+        if (isInteger(result)) {
+            return String.valueOf((int) result);
+        }
+
+        return String.valueOf(result);
+    }
+
+    private static boolean isInteger(double number) {
+        return number == (int) number;
     }
 }
