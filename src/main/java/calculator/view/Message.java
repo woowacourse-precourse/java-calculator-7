1a8537caa1d1119220ef9c18e00f3994888daa03
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 03:27:51 +0900

    feat(Message): 잘못된 위치의 '//' 및 '\n' 처리 예외 메시지 추가

--- src/main/java/calculator/view/Message.java
+++ src/main/java/calculator/view/Message.java
@@ -3,6 +3,8 @@ package calculator.view;
 public enum Message {
 
     INSTRUCTION_MESSAGE("덧셈할 문자열을 입력해 주세요."),
+    INVALID_DELIMITER_ORDER("'//'는 '\\n'보다 앞에 위치해야 합니다."),
+    INVALID_DELIMITER_POSITION("'//'와 '\\n'이 모두 존재해야 합니다."),
     NO_INPUT("입력이 없습니다."),
     RESULT_MESSAGE("결과 : ");
 
