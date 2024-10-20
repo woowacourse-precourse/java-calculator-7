85d683f9bcb07fbcd1943091484edc648e2b7b14
조현석 <hsjo0227@gmail.com>
2024-10-20 13:27:37 +0900

    feat: 구분자 추출 기능 구현
    주어진 입력에서 구분자를 추출하여 모든 구분자를 반환하는 메서드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,45 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    /**
+     * 정규식으로 //와 \n 사이의 구분자를 추출하여 모든 구분자를 반환한다.
+     *
+     * @param input 구분자를 추출할 문자열
+     * @return |로 구분한 기본 구분자와 커스텀 구분자
+     * @throws IllegalArgumentException 입력이 //로 시작하였지만 구분자를 찾지 못한 경우
+     */
+    private static String getDelimiter(String input) throws IllegalArgumentException {
+        String delimiters = ",|:";
+
+        // 커스텀 구분자가 없는 경우
+        if (!input.startsWith("//")) {
+            return delimiters;
+        }
+
+        Pattern pattern = Pattern.compile("//(.)\\\\n");
+        Matcher matcher = pattern.matcher(input);
+
+        // //로 시작하였지만 구분자를 찾지 못함
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("커스텀 구분자를 찾을 수 없습니다.");
+        }
+
+        String customDelimiter = matcher.group(1);
+
+        // '|'가 구분자로 들어온 경우 이스케이프 처리
+        if (customDelimiter.equals("|")) {
+            customDelimiter = "\\|";
+        }
+
+        delimiters = delimiters + "|" + customDelimiter;
+
+        return delimiters;
     }
 }
