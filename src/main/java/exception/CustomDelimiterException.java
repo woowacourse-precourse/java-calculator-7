b8201d2c2e838334f721b1d2e484fb3d94fb2727
윤민수 <koelysa5696@naver.com>
2024-10-20 22:37:09 +0900

    예외 catch 구문 삭제3

--- src/main/java/exception/CustomDelimiterException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class CustomDelimiterException extends IllegalArgumentException {
-
-    public CustomDelimiterException(String message) {
-        super(message);
-    }
-
-}
