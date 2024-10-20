073701d70fd8b7574bd1c5a7990e7aa3aa35479b
wngktjd13 <seonguk3553@kookmin.ac.kr>
2024-10-21 05:20:30 +0900

    refactor: Console.readLine() 사용

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,17 +1,16 @@
 package calculator;
 
 import calculator.exception.InvalidInputException;
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("문자열을 입력하세요.");
+        String input = Console.readLine();  // Scanner 대신 Console 사용
 
-        String input = scanner.nextLine();
         try {
-            String result = StringCalculator.calculate(input);
-            System.out.println(result);
+            int result = Integer.parseInt(StringCalculator.calculate(input));
+            System.out.println("결과 : " + result);
         } catch (InvalidInputException e) {
             System.err.println(e.getMessage());
         }
