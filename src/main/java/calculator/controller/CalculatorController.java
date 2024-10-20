82930be91673ebfc05db46002f776283d80599f7
JS <zks145@naver.com>
2024-10-20 14:27:30 +0900

    feat (controller) : 계산 결과 조회 기능 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,17 @@
 package calculator.controller;
 
+import calculator.service.CalculatorService;
+import calculator.view.CalculatorInput;
+import calculator.view.CalculatorOutput;
+
 public class CalculatorController {
+
+    private final CalculatorService service = new CalculatorService();
+
+    public void run() {
+        String input = CalculatorInput.getInput();
+        int result = service.initCalculator(input);
+        CalculatorOutput.printResult(result);
+    }
+
 }
