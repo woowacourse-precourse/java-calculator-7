2d39d689f2a3e48159804a3e7c5c7af488be3501
jinno Yun <jinno321@naver.com>
2024-10-21 00:17:52 +0900

    feat: 메인 함수 및 입출력 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        final Separator separator = new Separator();
+        final Calculator calculator = new Calculator();
 
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        separator.makeNumsAndParams(input);
+
+        int result = calculator.Calculate(separator.getNums());
+        System.out.println("결과 : "+result);
+        Console.close();
     }
 }
