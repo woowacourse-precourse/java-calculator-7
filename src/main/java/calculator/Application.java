e5d2f092686e1caee7b30ae90f7e7e4df63abb80
ruka-sh <ori8591y@gmail.com>
2024-10-20 20:45:07 +0900

    refactor: 기능을 메서드로 분리하여 코드 구조 개선

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,8 +3,22 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.regex.Pattern;
 
-
 public class Application {
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+            String inputString = Console.readLine();
+            double result = processInput(inputString);
+
+            System.out.println("결과 : " + result);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+
+    }
 
     /**
      * 구분자와 양수로 구성된 문자열에서 숫자의 합을 계산합니다.
@@ -12,32 +26,78 @@ public class Application {
      * @param s 합을 계산할 문자열
      * @return 합계
      */
-    static double calculate(String s) {
+    static double processInput(String s) {
         if (s == null || s.isEmpty()) {
             return 0;
         }
 
+        String customDelimiter = getCustomDelimiter(s);
+        String delimiters = getDelimiters(customDelimiter);
+        String refinedString = refineInput(s);
 
-        String refinedString = s;
-        String listOfDelimiters = ",|:";
+        String[] tokens = refinedString.split(delimiters);
+        return addNumbers(tokens);
+    }
 
-        // 커스텀 구분자 처리
+    /**
+     * 구분자와 양수로 구성된 문자열에서 커스텀 구분자를 추출합니다.
+     *
+     * @param s 구분자와 양수로 구성된 문자열
+     * @return 커스텀 구분자 또는 null
+     */
+    static String getCustomDelimiter(String s) {
         if (s.startsWith("//")) {
             int endOfDelimiter = s.indexOf("\\n");
 
-            String customDelimiter = s.substring(2, endOfDelimiter);
-            listOfDelimiters += "|" + Pattern.quote(customDelimiter);
-
-            int startOfNumbers = endOfDelimiter + 2;
-            refinedString = s.substring(startOfNumbers);
+            return s.substring(2, endOfDelimiter);
         }
 
+        return null;
+    }
 
-        String[] tokens = refinedString.split(listOfDelimiters);
+    /**
+     * 기본 구분자와 커스텀 구분자를 포함한 모든 구분자들을 반환합니다.
+     *
+     * @param customDelimiter 커스텀 구분자
+     * @return 구분자들
+     */
+    static String getDelimiters(String customDelimiter) {
+        String defaultDelimiters = ",|:";
+
+        if (customDelimiter != null) {
+            return defaultDelimiters + "|" + Pattern.quote(customDelimiter);
+        }
+
+        return defaultDelimiters;
+    }
+
+    /**
+     * 커스텀 구분자를 제외한 나머지 문자열을 정제합니다.
+     *
+     * @param s 커스텀 구분자
+     * @return 정제된 문자열
+     */
+    static String refineInput(String s) {
+        if (s.startsWith("//")) {
+            int startOfNumbers = s.indexOf("\\n") + 2;
+            return s.substring(startOfNumbers);
+        }
+
+        return s;
+    }
+
+    /**
+     * 문자열 배열에 담긴 숫자들을 더합니다.
+     *
+     * @param strArr 문자열 배열
+     * @return 합계
+     */
+    static double addNumbers(String[] strArr) {
+
         double sumResult = 0;
 
-        for (String stringToken : tokens) {
-            double doubleToken = Double.parseDouble(stringToken);
+        for (String strToken : strArr) {
+            double doubleToken = Double.parseDouble(strToken);
 
             if (doubleToken <= 0) {
                 throw new IllegalArgumentException("양수가 아닙니다.");
@@ -50,14 +110,4 @@ public class Application {
 
     }
 
-    public static void main(String[] args) {
-
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-
-        String inputString = Console.readLine();
-
-        double result = calculate(inputString);
-        System.out.println("결과 : " + result);
-
-    }
 }
