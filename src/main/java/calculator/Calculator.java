d135d2db011b206d2533c7a242e315e919f265ab
younyoung <00younyoung@naver.com>
2024-10-20 16:56:42 +0900

    Feat: 커스텀 구분자의 경우 토큰화 Calculator 클래스의 tokenize 메소드에서 커스텀 구분자(“//“와 “\n” 사이에 위치한 문자)가 지정되는 경우, 해당 구분자를 기준으로 문자열을 토큰화하여 문자열 배열 형태로 반환하도록 함

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -18,6 +18,13 @@ public class Calculator {
     static String[] tokenize(String input) {
         String delimiter = ",|:"; // 기본 구분자
 
+        // 커스텀 구분자가 있는 경우
+        if (input.startsWith("//")) {
+            int delimiterEndIndex = input.indexOf("\\n");
+            delimiter = Pattern.quote(input.substring(2, delimiterEndIndex)); // 커스텀 구분자 추출
+            input = input.substring(delimiterEndIndex + 2); // 숫자 부분만 추출
+        }
+
         return input.split(delimiter); // 구분자로 문자열 분리
     }
 
