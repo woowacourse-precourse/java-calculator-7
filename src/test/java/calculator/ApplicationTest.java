01cb9e7b59ceef3091d507064792b2cc34b2d359
byunghyunkim0 <bb05qudgus@gamil.com>
2024-10-20 18:21:52 +0900

    test: 기본 커스텀 입력 테스트 - 수정할 테스트 내용   - 와 같이 결과가 0인 경우   - //

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,8 +1,12 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -16,6 +20,27 @@ class ApplicationTest extends NsTest {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
         });
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateDefaultData")
+    void 기본_커스텀_구분자_사용_테스트(String element, String expected) {
+        run(element);
+        assertThat(output()).contains(expected);
+    }
+
+    static Stream<Arguments> generateDefaultData() {
+        return Stream.of(
+                Arguments.of("1:2", "결과 : 3"),
+                Arguments.of("1:100", "결과 : 101"), // 한자리수 + 여러자리수
+                Arguments.of("1:10,2", "결과 : 13"), // 기본 구분자 여러개 사용
+                Arguments.of("", "결과 : 0"), // 아무것도 입력안하는 경우
+                Arguments.of("//;\\n1;3", "결과 : 4"), // 커스텀 구분자 사용
+                Arguments.of("//;\\n", "결과 : 0"), // 커스텀 구분자 사용 & 아무것도 입력안하는 경우
+                Arguments.of("//-\\n1-3", "결과 : 4"), // 사칙연산 구분자인 경우
+                Arguments.of("//;;\\n1;;3", "결과 : 4"), // 구분자의 길이가 1이아닌경우
+                Arguments.of("100000000000000000:100000000000000000", "결과 : 200000000000000000") // Long보다 큰 매우 큰수
+        );
     }
 
     @Test
