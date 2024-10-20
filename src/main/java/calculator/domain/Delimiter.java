370495e8bf40c7fb1f4afa090fa44313c9d3430b
Zrfv <youngoh07015@gmail.com>
2024-10-21 03:33:08 +0900

    refactor: 구분자 클래스 상수 분리
    - 구분자 클래스 상수 분리하여 상수 클래스 생성

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -1,5 +1,10 @@
 package calculator.domain;
 
+import static calculator.domain.constant.CustomDelimiter.CUSTOM_DELIMITER_PREFIX;
+import static calculator.domain.constant.CustomDelimiter.CUSTOM_DELIMITER_SUFFIX;
+import static calculator.domain.constant.CustomDelimiter.ESCAPE_CHAR;
+import static calculator.domain.constant.CustomDelimiter.SPECIAL_REGEX_CHARS;
+
 import calculator.domain.constant.DefaultDelimiter;
 import calculator.exception.DelimiterException;
 import calculator.validator.EmptyStringValidator;
@@ -11,11 +16,6 @@ public class Delimiter {
     private List<String> delimiterList = new ArrayList<>();
     private String customDelimiter;
 
-    private final String CUSTOM_DELIMITER_START = "//";
-    private final String CUSTOM_DELIMITER_END = "\\n";
-    private final String SPECIAL_REGEX_CHARS = "([\\[\\]\\\\.^$|?*+(){}])";
-    private final String ESCAPE_CHAR = "\\\\$1";
-
     public Delimiter(String inputString) {
         this.inputString = inputString;
         initDefaultDelimiter();
@@ -26,19 +26,25 @@ public class Delimiter {
     }
 
     private void setCustomDelimiter() {
-        if (inputString.startsWith(CUSTOM_DELIMITER_START)) {
+        if (inputString.startsWith(CUSTOM_DELIMITER_PREFIX)) {
             DelimiterException.validateCustomDelimiterFormat(findDelimiterEndIndex());
-
-            customDelimiter = inputString.substring(2, findDelimiterEndIndex());
-            DelimiterException.validateCustomDelimiter(customDelimiter);
-
-            delimiterList.add(escapeSpecialRegexChars(customDelimiter));
-            inputString = inputString.substring(findDelimiterEndIndex() + 2);
+            extractCustomDelimiter();
+            updateInputString();
         }
     }
 
+    private void extractCustomDelimiter() {
+        customDelimiter = inputString.substring(2, findDelimiterEndIndex());
+        DelimiterException.validateCustomDelimiter(customDelimiter);
+        delimiterList.add(escapeSpecialRegexChars(customDelimiter));
+    }
+
+    private void updateInputString() {
+        inputString = inputString.substring(findDelimiterEndIndex() + 2);
+    }
+
     private int findDelimiterEndIndex() {
-        return inputString.indexOf(CUSTOM_DELIMITER_END);
+        return inputString.indexOf(CUSTOM_DELIMITER_SUFFIX);
     }
 
     public String[] getSplitString() {
@@ -46,7 +52,7 @@ public class Delimiter {
         if (EmptyStringValidator.isEmptyString(inputString)) {
             return null;
         }
-        String delimiterRegex = String.join("|", delimiterList); // ",|:|customDeli"
+        String delimiterRegex = String.join("|", delimiterList);
         return inputString.split(delimiterRegex);
     }
 
@@ -57,6 +63,4 @@ public class Delimiter {
     public String getCustomDelimiter() {
         return customDelimiter;
     }
-
-
 }
---
+++ src/main/java/calculator/domain/constant/CustomDelimiter.java
@@ -0,0 +1,8 @@
+package calculator.domain.constant;
+
+public class CustomDelimiter {
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+    public static final String SPECIAL_REGEX_CHARS = "([\\[\\]\\\\.^$|?*+(){}])";
+    public static final String ESCAPE_CHAR = "\\\\$1";
+}
