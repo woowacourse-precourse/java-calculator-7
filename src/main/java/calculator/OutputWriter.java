35b113d332df6515e18fed8420babaf3893b0001
seongsiyeong <ssy2681q@naver.com>
2024-10-21 04:30:22 +0900

    refactor : 구글자바컨벤션에 따라 상수를 CONSTANT_CASE 스타일로 수정

--- src/main/java/calculator/OutputWriter.java
+++ src/main/java/calculator/OutputWriter.java
@@ -2,9 +2,9 @@ package calculator;
 
 public class OutputWriter {
 
-    private static final String outputFormat = "결과 : ";
+    private static final String OUTPUT_FORMAT = "결과 : ";
 
     public static void writeOutput(Long sum) {
-        System.out.println(outputFormat + sum);
+        System.out.println(OUTPUT_FORMAT + sum);
     }
 }
