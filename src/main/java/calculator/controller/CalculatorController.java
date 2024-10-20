31d6269347420c4a9d815aa72c0166ed76a9b4d4
hig1002 <hig7463@naver.com>
2024-10-20 15:22:54 +0900

    feat : 결과 출력하기

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -5,9 +5,16 @@ import calculator.view.CalculatorView;
 
 public class CalculatorController {
 
+    private final Calculator calculator;
+
+    public CalculatorController(Calculator calculator) {
+        this.calculator = calculator;
+    }
+
     public void run(){
         try{
             String input = CalculatorView.getInput();
+            int result = calculator.add(input);
         }
         catch (IllegalArgumentException e){
             CalculatorView.printError(e.getMessage());
