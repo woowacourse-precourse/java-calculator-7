2175299ce60a96d3f0972df274a1ece55fb3046a
haennni <ggowater33@kangnam.ac.kr>
2024-10-20 17:47:34 +0900

    feat(application): add main logic for string addition
    - 사용자로부터 입력을 받아 문자열 덧셈 계산을 수행하는 메인 로직 구현
    - 입력된 숫자들의 합을 계산하여 결과를 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,18 +1,45 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.stream.BaseStream;
-
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private final InputParser inputParser;
+    private final Validator validator;
+
+    public Application(InputParser inputParser, Validator validator) {
+        this.inputParser = inputParser;
+        this.validator = validator;
+    }
+
+    public int add(String input) {
+        int sum = 0;
+        /*1. 빈 문자열 입력 시 0 반환*/
+        if (validator.isNull(input)) {
+            return sum;
+        }
+        /*2. 구분자 기준으로 파싱*/
+        String[] parse = inputParser.parse(input);
+        /*3. 문자열에 음수 혹은 문자열이 포함되어있는지 유효성 검사*/
+        validator.validation(parse);
+
+        for (String num : parse) {
+            sum += Integer.parseInt(num);
+        }
+        return sum;
+    }
+
+
     public static void main(String[] args){
         // TODO: 프로그램 구현
+        InputParser inputParser = new InputParser();
+        Validator validator = new Validator();
+
+        Application application = new Application(inputParser, validator);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        int result = application.add(Console.readLine());
+        System.out.println("결과 : " + result);
+
     }
 }
+
