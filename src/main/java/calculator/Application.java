43db2f3ce68561b0f1be34ff28c8724985bdba25
yeonnhuu <yeonnhuu@gmail.com>
2024-10-20 14:47:45 +0900

    :sparkles: feat(Application): Model, View, Controller 연결 및 실행 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.model.StringCalculator;
+import calculator.view.CalculatorView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringCalculator stringCalculator = new StringCalculator();
+        CalculatorView CalculatorView = new CalculatorView();
+        CalculatorController controller = new CalculatorController(stringCalculator, CalculatorView);
+
+        controller.run();
     }
 }
