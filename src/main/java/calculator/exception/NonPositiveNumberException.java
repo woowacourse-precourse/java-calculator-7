1a6e978ed3b5b0456f3d64903e20aff00ee113da
변우영 <likevangogh24@gmail.com>
2024-10-20 10:54:53 +0900

    refactor(NonPositiveNumberException): 예외 메시지 수정

--- src/main/java/calculator/exception/NonPositiveNumberException.java
+++ src/main/java/calculator/exception/NonPositiveNumberException.java
@@ -2,6 +2,6 @@ package calculator.exception;
 
 public class NonPositiveNumberException extends IllegalArgumentException {
     public NonPositiveNumberException() {
-        super("입력값 중 숫자는 0보다 커야합니다.");
+        super("입력값 중 숫자는 양수여야 합니다.");
     }
 }
