9f3afd35de54607bc6d522177541590d6e141993
hseungho <hsh47607@gmail.com>
2024-10-20 22:06:29 +0900

    feat: Application 계산 로직 조합 추가
    Signed-off-by: hseungho <hsh47607@gmail.com>

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,30 @@
 package calculator;
 
+import calculator.io.page.Page;
+import calculator.io.page.ReaderPage;
+import calculator.io.page.SimpleTextPage;
+import calculator.io.page.SimpleTextReaderPage;
+import calculator.math.Calculator;
+import calculator.math.IntegerCalculator;
+import calculator.parser.Parser;
+import calculator.parser.StringToIntegerListParser;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 1. 사용자 입력
+        ReaderPage<String, String> index = new SimpleTextReaderPage("덧셈할 문자열을 입력해 주세요.\n");
+        index.render();
+        String input = index.getOutput();
+
+        // 2. 입력값 파싱
+        Parser<String, List<Integer>> parser = new StringToIntegerListParser();
+        List<Integer> data = parser.parse(input);
+
+        // 3. 입력값 총합 계산 및 출력
+        Calculator<Integer> calculator = new IntegerCalculator();
+        Integer sum = calculator.sum(data);
+        Page<String> result = new SimpleTextPage("결과 : " + sum);
+        result.render();
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -28,4 +28,56 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
+    @Test
+    void 빈_값_입력_테스트() {
+        assertSimpleTest(() -> {
+            run ("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 여러_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n//_\\n1;2_3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 여러_커스텀_구분자_그룹_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n//_$\\n//^\\n1,2:3;4_5$6^7");
+            assertThat(output()).contains("결과 : 28");
+        });
+    }
+
+    @Test
+    void 예외_테스트_사용할_수_없는_구분자_사용() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//-\\n//+\\n//*/\\n1-3+4"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("사용할 수 없는 구분자입니다.");
+        });
+    }
+
+    @Test
+    void 예외_테스트_올바르지_않은_입력값_입력() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("1:2,a:5"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("올바르지 않은 입력값입니다.");
+        });
+    }
+
+    @Test
+    void 예외_테스트_양수가_아닌_숫자_입력() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("-1,2,3"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("숫자는 1 이상의 양수만 입력 가능합니다.");
+        });
+    }
+
 }
