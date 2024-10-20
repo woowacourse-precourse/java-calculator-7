ccf02290dbd3a0063ff80dffdb2ccf1ea11c5f60
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-21 04:34:43 +0900

    refactor(ExceptionHandlerTest.java): 패키지 이동
    누락된 테스트 코드의 패키지를 이동

--- src/test/java/calculator/utils/ExceptionHandlerTest.java
+++ src/test/java/calculator/utils/ExceptionHandlerTest.java
@@ -1,8 +1,7 @@
-package calculator;
+package calculator.utils;
 
 import static org.assertj.core.api.Assertions.*;
 
-import calculator.utils.ExceptionHandler;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
