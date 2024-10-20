c7eafdbd832553c389bd23ea52ab602f3306bd81
yejin923 <yejin923@naver.com>
2024-10-20 16:04:57 +0900

    feat: add exception function

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,36 +7,42 @@ import java.util.regex.Pattern;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int total = 0;
-        String separator = null;
-        String num = null;
-        String[] str;
+        try {
+            int total = 0;
+            String separator = null;
+            String num = null;
+            String[] str;
 
+            System.out.println("덧셈할 문자열을 입력해주세요. ");
+            String line = Console.readLine();
 
-        System.out.println("덧셈할 문자열을 입력해주세요. ");
-        String line = Console.readLine();
+            if(line.matches(".*[가-힣]+.*"))
+                throw new IllegalArgumentException("한글은 입력할 수 없습니다.");
 
-        Pattern pattern = Pattern.compile("//(.)\\\\n(.*)");
-        Matcher sep = pattern.matcher(line);
+            Pattern pattern = Pattern.compile("//(.)\\\\n(.*)");
+            Matcher sep = pattern.matcher(line);
 
-        if (sep.find()) {
-            separator = sep.group(1);
-            num = sep.group(2);
-            str = num.split(Pattern.quote(separator) + "|,|:");
+            if (sep.find()) {
+                separator = sep.group(1);
+                num = sep.group(2);
+                str = num.split(Pattern.quote(separator) + "|,|:");
 
-        } else {
-            str = line.split(":|,");
-        }
-
-
-        for (int i = 0; i < str.length; i++) {
-            try {
-                total += Integer.parseInt(str[i]);
-            } catch (NumberFormatException e) {
-                total += 0;
+            } else {
+                str = line.split(":|,");
             }
-        }
 
-        System.out.println("결과 : " + total);
+
+            for (int i = 0; i < str.length; i++) {
+                try {
+                    total += Integer.parseInt(str[i]);
+                } catch (NumberFormatException e) {
+                    total += 0;
+                }
+            }
+
+            System.out.println("결과 : " + total);
+        } catch(IllegalArgumentException e){
+
+        }
     }
 }
