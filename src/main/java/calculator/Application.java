72dd78f65fc49cff83dd678272b0931f3c38fc12
Yongjin <hi653@naver.com>
2024-10-20 15:25:13 +0900

    feat: custom 구분자에 숫자 및 - 가 허용되도록 설정
    magic number 사용을 지양하고, customSep에 - 및 숫자가 허용되도록 코드를 개선했다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.logging.Logger;
 
 /*
  * 입력한 문자열에서 숫자를 추출하여 더하는 계산기를 구현한다.
@@ -9,7 +10,9 @@ public class Application {
 
     public static final char BASIC_SEP_ONE = ':';
     public static final char BASIC_SEP_TWO = ',';
-    public static char customSep  = '1';
+    private static final char NO_CUSTOM_SEP = '1';
+    public static char customSep  = NO_CUSTOM_SEP;
+    private static final Logger logger = Logger.getLogger(Application.class.getName());
 
     // BASIC_SEP, CustomSep, 숫자인지 유효성 검사
     public static boolean isValidChar(char c){
@@ -21,16 +24,17 @@ public class Application {
         if(input.isEmpty()){
             return;
         }
-        if(input.contains("-")) throw new IllegalArgumentException();
+        if(input.contains("-") && !input.startsWith("//")) {
+            throw new IllegalArgumentException("NEGATIVE_NUMBERS_ARE_NOT_ALLOWED");
+        }
         if((input.charAt(0) == '/' && input.length() < 5)) {
             throw new IllegalArgumentException("Input is not valid.");
         }
-        if(input.startsWith("//") && input.charAt(3) == '\\'
-                && input.charAt(4) == 'n' && !Character.isDigit(input.charAt(2))){
+        if(input.startsWith("//") && input.charAt(3) == '\\' && input.charAt(4) == 'n'){
                 customSep = input.charAt(2);
         }
         for (int i = 0; i < input.length(); i++){
-            if(customSep != '1' && i < 5) continue;
+            if(customSep != NO_CUSTOM_SEP && i < 5) continue;
             if(!isValidChar(input.charAt(i))){
                 throw new IllegalArgumentException(input.charAt(i) + " is inserted." );
             }
@@ -40,15 +44,15 @@ public class Application {
     // 문자열에서 숫자 덧셈 계산
     public static int calculate(String input){
         int sum = 0;
-        int i = (customSep == '1' ? 0 : 5);
+        int i = (customSep == NO_CUSTOM_SEP ? 0 : 5);
 
         while(i < input.length()){
-            if(!Character.isDigit(input.charAt(i))){
+            if(!Character.isDigit(input.charAt(i)) || input.charAt(i) == customSep){
                 i++;
                 continue;
             }
             int num = 0;
-            while(i < input.length() && Character.isDigit(input.charAt(i))){
+            while(i < input.length() && Character.isDigit(input.charAt(i)) && input.charAt(i) != customSep){
                 num = num * 10 + input.charAt(i) - '0';
                 i++;
             }
@@ -59,11 +63,9 @@ public class Application {
 
     public static void main(String[] args) {
         // 입력받기 및 입력값 검증
-                System.out.println("PLEASE_ENTER_THE_STRING_TO_BE_ADDED");
-                String input = Console.readLine();
-                isValid(input);
-
-        System.out.println("결과 : " + calculate(input));
-
+        System.out.println("PLEASE_ENTER_THE_STRING_TO_BE_ADDED");
+        String input = Console.readLine();
+        isValid(input);
+        logger.info("결과 : " + calculate(input));
     }
 }
