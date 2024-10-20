cc392d7c6f33788f1bcab46ff9eeb16910afd12d
alswnsp411 <alswnsp411@naver.com>
2024-10-20 23:19:07 +0900

    fix: ""를 입력했을 때 발생하는 NoSuchElementException 예외 처리

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,6 +1,7 @@
 package calculator.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 /**
  * 사용자로부터 문자열을 입력받는 view
@@ -10,7 +11,11 @@ public class InputView {
     public String getInputString() {
         System.out.println("덧셈할 문자열을 입력해주세요.");
 
-        return Console.readLine();
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            return "";
+        }
     }
 
     public String getInputStringFromNextLine(String input) {
