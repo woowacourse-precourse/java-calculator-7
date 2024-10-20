a1f2d15cd8376eafabec74072456fb60189b6542
jjun522 <tjwowns522@>
2024-10-20 17:15:29 +0900

    refactor: 애플리케이션 진입점 수정
    기존 로직에서 컨트롤러를 사용하여 MVC 구조로 프로그램의 흐름을 재구성하였습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,22 +1,10 @@
 package calculator;
 
-import calculator.service.StringSumCalculator;
-import calculator.util.InputHandler;
-import calculator.util.ErrorHandler;
-import java.util.Scanner;
+import calculator.controller.CalculatorController;
 
 public class Application {
     public static void main(String[] args) {
-        String input = InputHandler.getInput();
-
-        StringSumCalculator calculator = new StringSumCalculator();
-        try {
-            // 입력된 문자열의 합 계산
-            int result = calculator.calculateSum(input);
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            // 잘못된 입력이 있을 경우 예외 처리
-            throw e;
-        }
+        CalculatorController controller = new CalculatorController();
+        controller.run();
     }
 }
