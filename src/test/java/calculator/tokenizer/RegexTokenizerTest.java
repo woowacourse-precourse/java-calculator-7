d906aaec3171ab1e4335e7b7a93ae9cd517f70b6
sangcci <pshsh910@gmail.com>
2024-10-21 01:57:31 +0900

    refactor: 입력 값 검증 옮김으로 인한 테스트 삭제

--- src/test/java/calculator/tokenizer/RegexTokenizerTest.java
+++ src/test/java/calculator/tokenizer/RegexTokenizerTest.java
@@ -1,7 +1,6 @@
 package calculator.tokenizer;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -30,7 +29,7 @@ public class RegexTokenizerTest {
     @Test
     void 입력_문자열에_커스텀_구분자를_추가하는_구문이_있다면_커스텀_구분자를_추가한다() {
         // given
-        String input = "//!\\n1:2,3";
+        String input = "!";
         Delimiters delimiters = new Delimiters();
         Tokenizer tokenizer = new RegexTokenizer(delimiters);
 
@@ -39,31 +38,5 @@ public class RegexTokenizerTest {
 
         // then
         assertThat(delimiters.getDelimiters()).contains("!");
-    }
-
-    @Test
-    void 커스텀_구분자를_추가할_때_개행_문자가_없다면_추가하지_않고_통과한다() {
-        // given
-        String input = "//!1:2,3";
-        Delimiters delimiters = new Delimiters();
-        Tokenizer tokenizer = new RegexTokenizer(delimiters);
-
-        // when
-        tokenizer.setCustomDelimiter(input);
-
-        // then
-        assertThat(delimiters.getDelimiters()).doesNotContain("!");
-    }
-
-    @Test
-    void 커스텀_구분자를_추가할_때_등록_문자가_없다면_예외를_발생시킨다() {
-        // given
-        String input = "!\\n1:2,3";
-        Delimiters delimiters = new Delimiters();
-        Tokenizer tokenizer = new RegexTokenizer(delimiters);
-
-        // when & then
-        assertThatThrownBy(() -> tokenizer.setCustomDelimiter(input))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 }
