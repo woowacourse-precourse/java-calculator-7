c1ffc252b8a8da572ebbb9672b98a9a287f9c9dd
gladhee <gladhee@icloud.com>
2024-10-20 21:53:29 +0900

    refactor(Calculator): 성능비교상 math라이브러리의 bigInteger가 우위이며 코드 단순화이점으로 인해 리펙토링

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -5,31 +5,14 @@ import java.math.BigInteger;
 
 public class Calculator {
 
-    public static String calculate(ParsedInput parsedInput) {
-        String sum = "0";
+    public String calculate(ParsedInput parsedInput) {
+        BigInteger result = BigInteger.ZERO;
         for (String numStr : parsedInput.numbers()) {
-            sum = addStrings(sum, numStr);
+            BigInteger num = new BigInteger(numStr);
+            result = result.add(num);
         }
 
-        return sum;
-    }
-
-    private static String addStrings(String num1, String num2) {
-        StringBuilder result = new StringBuilder();
-
-        int carry = 0;
-        int p1 = num1.length() - 1;
-        int p2 = num2.length() - 1;
-
-        while (p1 >= 0 || p2 >= 0 || carry != 0) {
-            int x = (p1 >= 0) ? num1.charAt(p1--) - '0' : 0;
-            int y = (p2 >= 0) ? num2.charAt(p2--) - '0' : 0;
-            int sum = x + y + carry;
-            result.append(sum % 10);
-            carry = sum / 10;
-        }
-
-        return result.reverse().toString();
+        return result.toString();
     }
 
 }
