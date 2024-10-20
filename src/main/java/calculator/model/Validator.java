5a139da41cff4906124456f4bb1ffde99980f0e8
MinChangJeong <wjdalsckd777@naver.com>
2024-10-20 13:39:32 +0900

    test: 테스트 코드 [정규표현식 메타데이터를 커스텀 구분자로 사용하는 켱우] (2차 검증)

--- src/main/java/calculator/model/Validator.java
+++ src/main/java/calculator/model/Validator.java
@@ -1,5 +1,7 @@
 package calculator.model;
 
+import java.util.regex.Pattern;
+
 public class Validator {
 
     private final Parser parser;
@@ -13,7 +15,7 @@ public class Validator {
             return new int[0];
         }
 
-        expression = expression.trim().replaceAll("\\\\n", "\n");
+        expression = expression.trim().replace("\\n", "\n");
 
         String delimiter = ",|:";
         String numbers = expression;
@@ -23,7 +25,16 @@ public class Validator {
             if (delimiterEndIdx == -1) {
                 throw new IllegalArgumentException("잘못된 형식입니다: " + expression);
             }
-            delimiter = expression.substring(2, delimiterEndIdx);
+
+            String customDelimiter = expression.substring(2, delimiterEndIdx);
+            if (customDelimiter.matches("\\d+")) {
+                throw new IllegalArgumentException("구분자는 숫자가 될 수 없습니다: " + customDelimiter);
+            }
+
+            // 정규 표현식에서 메타문자들을 이스케이프 처리하기 위해 Pattern.quote 사용
+            customDelimiter = Pattern.quote(customDelimiter);
+
+            delimiter = delimiter + "|" + customDelimiter;
             numbers = expression.substring(delimiterEndIdx + 1);
         }
 
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -2,11 +2,13 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -21,12 +23,19 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트_음수_입력() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("-1,2,3"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining("음수는 허용되지 않습니다")
-        );
+    void 커스텀_구분자_사용_정규표현식_메타데이터_이스케이프처리() {
+        assertSimpleTest(() -> {
+            run("//+\\n1+2+3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_기본_구분자_혼합사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1,2:3;4,5;6:7,8,9;10");
+            assertThat(output()).contains("결과 : 55");
+        });
     }
 
     @Test
@@ -46,19 +55,50 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 큰_수_입력_테스트() {
-        assertThatThrownBy(() -> runException("2147483648"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("입력 값이 정수의 범위를 초과합니다");
-
+    void 정수자료형_최대값_계산() {
         assertSimpleTest(() -> {
             run("1000000000,1000000000,147483647");
             assertThat(output()).contains("결과 : 2147483647");
         });
+    }
 
+    @Test
+    void 입력값이_정수형_자료범위_초과() {
+        assertThatThrownBy(() -> runException("2147483648"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력 값이 정수의 범위를 초과합니다");
+    }
+
+    @Test
+    void 계산결과가_정수형_자료범위_초과() {
         assertThatThrownBy(() -> run("2000000000,2000000000"))
                 .isInstanceOf(ArithmeticException.class)
                 .hasMessageContaining("integer overflow");
+    }
+
+    @Test
+    void 예외_테스트_음수_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("음수는 허용되지 않습니다")
+        );
+    }
+
+    @Test
+    void 예외_테스트_커스텀_구분자_숫자미입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//test\\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_커스텀_구분자_숫자입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//3\\n132333"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Override
