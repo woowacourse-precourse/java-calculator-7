1e5acb25238a8749ccf10f82c501bf3d7ee8f50b
문고은 <>
2024-10-20 23:28:02 +0900

    feat(output) : 결과 출력 메시지 추가

--- src/main/java/calculator/OutputPrinter.java
+++ src/main/java/calculator/OutputPrinter.java
@@ -4,4 +4,8 @@ public class OutputPrinter {
     public void promptForInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
+
+    public void printResult(int result) {
+        System.out.println("결과: " + result);
+    }
 }
