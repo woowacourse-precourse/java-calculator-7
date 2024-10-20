2b7050fe168fe39a569816009f0a56aef7e4cd03
JooIcksoo <dlrtn751@gmail.com>
2024-10-20 22:18:36 +0900

    feat(StringAddCalculator): 문자열 덧셈 계산기
    - 출력 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -5,16 +5,17 @@ import java.util.List;
 public class StringAddCalculator {
     InputProcessor inputProcessor = new InputProcessor();
     MessagePrinter messagePrinter = new MessagePrinter();
-
+    Adder adder = new Adder();
 
     public void run() {
         messagePrinter.printInputRequest();
-        inputProcessor.readInput();
+        String input = inputProcessor.readInput();
 
-        StringParser stringParser = new StringParser(inputProcessor.getInput());
+        StringParser stringParser = new StringParser(input);
         List<Integer> numbers = stringParser.parse();
 
+        int result = adder.add(numbers);
 
-
+        messagePrinter.printResult(result);
     }
 }
