aa4b0e8cb12f1233be976942b503010125a7029e
sseongyunn <sseongyunn@korea.ac.kr>
2024-10-21 01:40:28 +0900

    feat: getUserInput 메서드 구현
    '덧셈할 문자열을 입력해주세요.' 콘솔 출력
    BufferedReader와 readLine() 이용하여 콘솔입력 받기
    BufferedReader 이용을 위한 import문 추가
    아무 것도 입력되지 않았을 경우에 대한 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,10 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -7,8 +12,25 @@ public class Application {
 }
 
 class StringParser {
+    String user_input;
 
     public StringParser() {
     }
 
+    //문자열 입력 받는 메서드(getUserInput)
+    public String getUserInput() throws IllegalArgumentException {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+
+        try {
+            user_input = br.readLine();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("입력값이 없습니다. 다시 입력해주세요.");
+        }
+
+        return user_input;
+    }
 }
