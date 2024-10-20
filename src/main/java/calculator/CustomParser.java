f438e7dc6ee3fc6715f73509302b14510e541ca3
Gyulguma <ybg653924@gmail.com>
2024-10-20 23:40:31 +0900

    refactor(CustomParser): forEach문을 for문으로 변경
    음수 판별하는 로직에 작성했던 forEach문을 for문으로 변경

--- src/main/java/calculator/CustomParser.java
+++ src/main/java/calculator/CustomParser.java
@@ -14,11 +14,13 @@ public class CustomParser {
         if (input.matches(regx)) { // 기본 구분자를 사용할 경우
             return parse(input, this.BASIC_SEPARATOR);
         }
+
         if (input.matches("//.\\\\n.*")) { // 커스텀 구분자를 사용할 경우
             String separator = input.charAt(2) + "";
             String temp = input.substring(5);
             return parse(temp, this.BASIC_SEPARATOR + "|" + separator);
         }
+
         throw new IllegalArgumentException("입력 형식을 확인해주세요.");
     }
 
@@ -26,17 +28,20 @@ public class CustomParser {
         if (input.isEmpty()) { // 빈 문자열일 경우
             return new int[]{0};
         }
+
         int[] result;
         try {
             result = Arrays.stream(input.split(separator)).mapToInt(Integer::parseInt).toArray();
         } catch (Exception e) {
             throw new IllegalArgumentException("입력 형식을 확인해주세요. (문자 x, 구분자는 숫자 사이에만 존재할 수 있습니다)");
         }
-        Arrays.stream(result).forEach(i -> {
-            if (i < 0) {
+
+        for (int number : result) {
+            if (number < 0) {
                 throw new IllegalArgumentException("음수는 허용하지 않습니다.");
             }
-        });
+        }
+
         return result;
     }
 
