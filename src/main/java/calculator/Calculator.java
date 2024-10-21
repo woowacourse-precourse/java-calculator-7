da4c41b817ef009ffdd20d4b841c13c7c59727e1
AppleMint98 <hong721816@konkuk.ac.kr>
2024-10-21 18:26:23 +0900

    style : indent 설정 변경 2 -> 4

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,41 +4,43 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
 
-  private static final IOHandler ioHandler = new IOHandler();
+    private static final IOHandler ioHandler = new IOHandler();
 
-  public static void run() {
+    public static void run() {
 
-    String userInput = ioHandler.getUserInput();
-    System.out.println("userInput : " + userInput);
-    long sum = 0;
-    if (userInput == "")
-      sum = 0;
-    else if (userInput.contains(",") || userInput.contains(":")) {
-      if (userInput.contains(",")) {
-        String[] nums = userInput.trim().split("[,:]");
-        for (String num : nums) {
-          if (Long.parseLong(num) < 0) {
+        String userInput = ioHandler.getUserInput();
+        System.out.println("userInput : " + userInput);
+
+        long sum = 0;
+
+        if (userInput == "") {
+            sum = 0;
+        } else if (userInput.contains(",") || userInput.contains(":")) {
+            if (userInput.contains(",")) {
+                String[] nums = userInput.trim().split("[,:]");
+                for (String num : nums) {
+                    if (Long.parseLong(num) < 0) {
+                        throw new IllegalArgumentException("양수로 구성된 문자열을 입력해 주세요.");
+                    }
+                    sum += Long.parseLong(num);
+                }
+            }
+        } else if (userInput.contains("//") && userInput.contains("\\n")) {
+            String separator = userInput.substring(userInput.indexOf("//") + 2,
+                    userInput.indexOf("\\n"));
+            String[] nums = userInput.substring(userInput.indexOf("\\n") + 2).split(separator);
+            for (String num : nums) {
+                if (Long.parseLong(num) < 0) {
+                    throw new IllegalArgumentException("양수로 구성된 문자열을 입력해 주세요.");
+                }
+                sum += Long.parseLong(num);
+            }
+        } else {
             throw new IllegalArgumentException("구분자와 양수로 구성된 문자열을 입력해 주세요.");
-          }
-          sum += Long.parseLong(num);
         }
-      }
-    } else if (userInput.contains("//") && userInput.contains("\\n")) {
-      String separator = userInput.substring(userInput.indexOf("//") + 2,
-          userInput.indexOf("\\n"));
-      String[] nums = userInput.substring(userInput.indexOf("\\n") + 2).split(separator);
-      for (String num : nums) {
-        if (Long.parseLong(num) < 0) {
-          throw new IllegalArgumentException("구분자와 양수로 구성된 문자열을 입력해 주세요.");
-        }
-        sum += Long.parseLong(num);
-      }
-    } else {
-      throw new IllegalArgumentException("구분자와 양수로 구성된 문자열을 입력해 주세요.");
+
+        ioHandler.printCalculatedOutput(sum);
+        Console.close();
+
     }
-
-    ioHandler.printCalculatedOutput(sum);
-    Console.close();
-
-  }
 }
--- src/main/java/calculator/IOHandler.java
+++ src/main/java/calculator/IOHandler.java
@@ -4,12 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class IOHandler {
 
-  public String getUserInput() {
-    System.out.println("덧셈할 문자열을 입력해 주세요.");
-    return Console.readLine();
-  }
+    public String getUserInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
 
-  public void printCalculatedOutput(long sum) {
-    System.out.println("결과 : " + sum);
-  }
+    public void printCalculatedOutput(long sum) {
+        System.out.println("결과 : " + sum);
+    }
 }
