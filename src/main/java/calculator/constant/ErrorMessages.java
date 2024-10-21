f8de30752d30585986c2ca041d69619befe0ce2e
kimchanho97 <nh0903@pusan.ac.kr>
2024-10-21 20:08:35 +0900

    feat(DefaultValidator): 기본 구분자로 입력 문자열 검증

---
+++ src/main/java/calculator/constant/ErrorMessages.java
@@ -0,0 +1,8 @@
+package calculator.constant;
+
+public class ErrorMessages {
+
+    public static final String NEGATIVE_NUMBER_NOT_ALLOWED = "음수는 허용되지 않습니다.";
+    public static final String INVALID_NUMBER_FORMAT = "유효하지 않은 숫자입니다.";
+
+}
--- src/main/java/calculator/model/DefaultValidator.java
+++ src/main/java/calculator/model/DefaultValidator.java
@@ -1,10 +1,33 @@
 package calculator.model;
 
+import static calculator.constant.DelimiterConstants.DEFAULT_DELIMITER_REGEX;
+import static calculator.constant.ErrorMessages.INVALID_NUMBER_FORMAT;
+import static calculator.constant.ErrorMessages.NEGATIVE_NUMBER_NOT_ALLOWED;
+
 public class DefaultValidator implements Validator{
 
     @Override
     public ParsedInput validate(String input) {
+        String[] numbers = input.split(DEFAULT_DELIMITER_REGEX);
+        validateNumbers(numbers);
 
-        return null;
+        return new ParsedInput(DEFAULT_DELIMITER_REGEX, input);
+    }
+
+    private static void validateNumbers(String[] numbers) {
+        for (String number : numbers) {
+            if (number.isEmpty()) {
+                continue;
+            }
+
+            try {
+                int num = Integer.parseInt(number);
+                if (num < 0) {
+                    throw new IllegalArgumentException(NEGATIVE_NUMBER_NOT_ALLOWED);
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(INVALID_NUMBER_FORMAT);
+            }
+        }
     }
 }
