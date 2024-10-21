07fa070b22747e5c0ad74647346e10bc72b75af5
CBAAabced <nmrkmj0108@kyonggi.ac.kr>
2024-10-21 10:25:59 +0900

    feat: implement custom check

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,11 +7,21 @@ public class Application {
         // TODO: 프로그램 구현
         int sum = 0;
         String inputString = getUserInput();
-        System.out.println("입력된 문자열: " + inputString);  //확인용 출력
+        if (isCustom(inputString)){
+            //확인용 출력
+            System.out.println("커스텀 구분자");
+        }
+        else {
+            //확인용 출력
+            System.out.println("쉼표 또는 콜론");
+        }
     }
     public static String getUserInput(){
         System.out.print("덧셈할 문자열을 입력하세요: ");
         String inputString = Console.readLine();
         return inputString;
+    }
+    public static boolean isCustom(String inputString){
+        return inputString.charAt(0) == '/';
     }
 }
