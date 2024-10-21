ccef85d2692c75e40d3f0fb3bf70d60743c2f84f
chaeshee0908 <chaeshee0908@naver.com>
2024-10-21 22:51:28 +0900

    feat(InputView): 문자열 입력받기

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,10 +1,17 @@
 package calculator.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
 
     private static final String startMessage = "덧셈할 문자열을 입력해 주세요.";
 
     public static void printStartMessage() {
         System.out.println(startMessage);
+    }
+
+    public static String readInputString() {
+        String input = readLine();
+        return input;
     }
 }
