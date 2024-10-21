27dfe20d0c9e51d0059404b2cd1a9ce097e2634d
Sujin Kim <su10jin11@khu.ac.kr>
2024-10-21 20:24:48 +0900

    feat : constant 클래스에서 에러 메시지 따로 추출

--- src/main/java/calculator/util/NumberParser.java
+++ src/main/java/calculator/util/NumberParser.java
@@ -5,16 +5,16 @@ public class NumberParser {
     public static int parseAndValidateNumber(String number) {
         number = number.trim();
         if (number.isEmpty()) {
-            throw new IllegalArgumentException(Constants.ERROR_EMPTY_INPUT);
+            throw new IllegalArgumentException(CustomErrorMessage.ERROR_EMPTY_INPUT);
         }
         try {
             int intNumber = Integer.parseInt(number);
             if (intNumber < 0) {
-                throw new IllegalArgumentException(Constants.ERROR_NEGATIVE_NUMBER);
+                throw new IllegalArgumentException(CustomErrorMessage.ERROR_NEGATIVE_NUMBER);
             }
             return intNumber;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(Constants.ERROR_INVALID_NUMBER);
+            throw new IllegalArgumentException(CustomErrorMessage.ERROR_INVALID_NUMBER);
         }
     }
 }
