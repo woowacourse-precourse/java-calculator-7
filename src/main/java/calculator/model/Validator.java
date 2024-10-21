e379e2d3eacc9e7bd75dbe432d954e577f9352d4
LeeDongWoo99 <dleh684@gmail.com>
2024-10-21 23:44:02 +0900

    refactor(validator): inputValidator 로직 내 불 필요한 코드 수정 및 validateMatcher 로직 조건문 수정

--- src/main/java/calculator/model/Validator.java
+++ src/main/java/calculator/model/Validator.java
@@ -7,16 +7,13 @@ public class Validator {
     private static final String REGEX = "^(//([a-zA-Z!@#$%^&*?._=+\\-;~`|/\\\\])\\\\n)?(\\d+(?:(,|:|\\2)\\d+)*)?$|^\"\"$";
 
     public static boolean inputValidator(String input) {
-        if (input.equals("\"\"")) { // 공백 문자열인 경우 true 반환
-            return true;
-        }
         Pattern inputPattern = Pattern.compile(REGEX);
         Matcher matcher = inputPattern.matcher(input);
         return matcher.find();
     }
 
     public static void validateMatcher(Matcher matcher) {
-        if (!matcher.find()) {
+        if (!matcher.matches()) {
             throw new IllegalArgumentException("잘못된 입력 형식입니다.");
         }
     }
