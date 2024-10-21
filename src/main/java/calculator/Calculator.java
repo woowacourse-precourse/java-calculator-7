93cbc808decfffbac5951d7d9eb5421e64e27d98
Seongmo Ahn <djgood780@gmail.com>
2024-10-21 21:21:33 +0900

    refactor(Output): 메서드 명 변경 (printUserInput -> printInputRequest)

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
     public Calculator() {}
 
     public void run(){
-        Output.printUserInput();
+        Output.printInputRequest();
         String inputData = Input.getUserInput();
         this.result = Calculation.getCalculationResult(inputData);
     }
--- src/main/java/calculator/Output.java
+++ src/main/java/calculator/Output.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class Output {
 
-    public static void printUserInput(){
+    public static void printInputRequest(){
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
