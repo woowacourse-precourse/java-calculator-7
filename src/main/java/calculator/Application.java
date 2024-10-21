c481ea656da5673aecd637db3da2928ba5c8258f
Gahyeon <best11gh@naeileun.dev>
2024-10-21 18:49:42 +0900

    feat: 입력 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import calculator.controller.CaculatorController;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+  public static void main(String[] args) {
+    CaculatorController caculatorController = new CaculatorController();
+
+    System.out.println("덧셈할 문자열을 입력해 주세요.");
+    String input = Console.readLine();
+    caculatorController.input(input);
+  }
 }
