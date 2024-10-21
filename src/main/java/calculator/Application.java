34890b132dbe57e8489d2f9fc9992d6a1bdbdc99
Jeong yong hwan <baechoo0905@naver.com>
2024-10-21 16:46:36 +0900

    refactor: Application 클래스에서 사용하지 않는 import문 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,5 @@
 package calculator;
 
-import java.util.Scanner;
-
 public class Application {
     public static void main(String[] args) {
         ApplicationController controller = new ApplicationController();
