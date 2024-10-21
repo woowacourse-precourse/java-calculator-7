fa8fceec9523a7fd9e4ddce86b3e6ea2c54c19fa
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-21 16:49:26 +0900

    refactor: 계산기 실행 코드 Calculator로 이동
    run()함수를 통해 application에서 실행가능하게끔 리팩토링

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,26 +1,10 @@
 package calculator;
 
-import Delimiter.BasicDelimiter;
-import Delimiter.CustomDelimiter;
-import Delimiter.DelimiterHandler;
 import util.Calculator;
-import util.Validator;
-import view.InputView;
-import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        String input = InputView.readLine();
-        Validator.isEmpty(input);
-        String delimiterType = DelimiterHandler.detectDelimiterType(input);
-        if (delimiterType.equals("Basic")) {
-            int result = Calculator.getSum(BasicDelimiter.getNumber(input));
-            OutputView.printResult(result);
-            return;  // Basic 구분자가 처리되면 일찍 종료
-        }
-
-        // Custom 구분자 처리
-        int result = Calculator.getSum(CustomDelimiter.getNumber(input));
-        OutputView.printResult(result);
+        Calculator calculator = new Calculator();
+        calculator.run();
     }
 }
--- src/main/java/util/Calculator.java
+++ src/main/java/util/Calculator.java
@@ -1,6 +1,27 @@
 package util;
 
+import Delimiter.BasicDelimiter;
+import Delimiter.CustomDelimiter;
+import Delimiter.DelimiterHandler;
+import view.InputView;
+import view.OutputView;
+
 public class Calculator {
+
+    public void run() {
+        String input = InputView.readLine();
+        Validator.isEmpty(input);
+        String delimiterType = DelimiterHandler.detectDelimiterType(input);
+        if (delimiterType.equals("Basic")) {
+            int result = Calculator.getSum(BasicDelimiter.getNumber(input));
+            OutputView.printResult(result);
+            return;  // Basic 구분자가 처리되면 일찍 종료
+        }
+        // Custom 구분자 처리
+        int result = Calculator.getSum(CustomDelimiter.getNumber(input));
+        OutputView.printResult(result);
+    }
+
     public static int getSum(int[] numbers) {
         int sum = 0;
         for (int i : numbers) {
