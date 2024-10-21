43985a1314b1dacb6d12b0ba424d97bd34bccee2
pjy2163 <pjy2163@naver.com>
2024-10-21 16:41:24 +0900

    refactor: main에서 input, output 함수를 InOutput.java로 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,9 @@
 package calculator;
-import camp.nextstep.edu.missionutils.*;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
+        String input = InOutput.getInput();
         List<String> numbers = CalculatorParser.parser(input);
 
         if (numbers.isEmpty()){
@@ -22,6 +20,6 @@ public class Application {
                 sum += num; 
             }
         }
-        System.out.println("결과 : " + sum);   
+        InOutput.printResult(sum);
     }
 }
