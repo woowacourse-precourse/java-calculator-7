aecd3feba322c1507fcaa30b4595566df14303a6
sonshn <sonshn@naver.com>
2024-10-21 20:19:24 +0900

    fix(ApplicationTest): 새로 추가한 예외 처리 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,6 +9,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1;2;3");
@@ -17,9 +25,73 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 예외_테스트_구분자가_없는_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("123"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_커스텀_구분자가_공백인_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_커스텀_구분자가_숫자인_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//1\\n2:3:"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_피연산자가_없는_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",:"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_피연산자가_0인_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_피연산자에_음수가_포함된_경우() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_피연산자에_구분자가_아닌_문자가_포함된_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2;4,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_피연산자가_정수_범위를_벗어난_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("2147483648:1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_연산_결과가_정수_범위를_벗어난_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("2147483647:1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
