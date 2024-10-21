978b18be219872ede2b70ef12094685956a305a6
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-21 19:33:06 +0900

    feat(DelimiterStringSplitter): extractDelimiterAndTargetString 메서드 구현

---
+++ src/main/java/calculator/DelimiterStringSplitter.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public record DelimiterStringSplitter(
+        String targetString,
+        String[] delimiters
+) {
+    public static DelimiterStringSplitter extractDelimiterAndTargetString(String input) {
+
+        Pattern defaultPattern = Pattern.compile("^(\\d*(,\\d+)*(:\\d+)*)*");
+        Matcher defaultMatcher = defaultPattern.matcher(input);
+
+        Pattern customPattern = Pattern.compile("^/{2}(\\D)\\\\n(.*)");
+        Matcher customMatcher = customPattern.matcher(input);
+
+        String[] delimiter;
+        String targetString;
+
+        if (defaultMatcher.matches()) {
+            delimiter = new String[]{",", ":"};
+            targetString = input;
+        } else if (customMatcher.matches()) {
+            delimiter = new String[]{customMatcher.group(1)};
+            targetString = customMatcher.group(2);
+        } else {
+            throw new IllegalArgumentException("입력하신 문자열이 프로그램에서 제공하는 구분자가 아닙니다.");
+        }
+
+        return new DelimiterStringSplitter(targetString, delimiter);
+    }
+}
