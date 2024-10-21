c9ea76e8671a79ac2a1c47544f058187079f56c5
sytTest <syt1128@naver.com>
2024-10-21 21:12:40 +0900

    :sparkle: (InputController) add filterVacantInput()
    입력을 받고 빈 입력이라면 "0"을 리턴하는 filterVacantInput() 추가

--- src/main/java/calculator/controller/InputController.java
+++ src/main/java/calculator/controller/InputController.java
@@ -1,4 +1,11 @@
 package calculator.controller;
 
+import calculator.view.Users;
+
 public class InputController {
+
+    private String filterVacantInput() {
+        String input = Users.input();
+        return input.isEmpty() ? "0" : input;
+    }
 }
