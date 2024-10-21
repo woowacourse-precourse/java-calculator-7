08267321de3e5560bec4784a99ef2507159ce9f2
PSangEon <qkr99102@naver.com>
2024-10-21 21:14:52 +0900

    Feat: feat string
    커스텀 식별자 구현
    
    #3

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,8 +8,16 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        String[] numbers = input.split(",|:");
-        int result = sumNumbers(numbers);
+        int result = 0;
+
+        if (input.startsWith("//")) {
+            String sep = input.substring(2, input.indexOf("\\n"));
+            String[] numbers = input.substring(input.indexOf("\\n") + 2).split("[,:" + sep + "]");
+            result = sumNumbers(numbers);
+        } else {
+            String[] numbers = input.split(",|:");
+            result = sumNumbers(numbers);
+        }
 
         System.out.println("결과 : " + result);
     }
