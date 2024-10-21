228e2273604a1d5d7aac0e40e9c278bd4fec5b50
yunho_yun <yunho_yun@naver.com>
2024-10-21 23:03:01 +0900

    test: 테스트케이스 추가
    구분자없는 경우, 오버플로

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -49,9 +49,25 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 예외_테스트_음수() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_구분자없음() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("2::33"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_오버플로() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("2147483648:1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
