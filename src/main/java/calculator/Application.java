dcc85e2f65117b1ab2364e27e735ef47eb088d53
박채린 <bewithbomi@gmail.com>
2024-10-21 23:48:43 +0900

    feat(app) : 최종 수정 완료
    - main 메소드 수정 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,16 +1,16 @@
 package calculator;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         try {
             String input = getInput();
-            int result = calculatorSum(input);
+            int result = calculateSum(input);
             printResult(result);
         } catch (IllegalArgumentException e) {
-            System.out.println("오류 : " + e.getMessage());
+            System.out.println(e.getMessage());
         }
     }
 
@@ -19,42 +19,46 @@ public class Application {
         return Console.readLine();
     }
 
+    private static int calculateSum(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+
+        String[] tokens = parseInput(input);
+        int sum = 0;
+
+        for (String token : tokens) {
+            sum += parseNumber(token);
+        }
+
+        return sum;
+    }
+
     private static String[] parseInput(String input) {
         String delimiter = ",|:";
-        String number = input;
+        String numbers = input;
 
         if (input.startsWith("//")) {
-            int delimiterIdx = input.indexOf("\n");
-            if (delimiterIdx != -1) {
-                delimiter = Pattern.quote(input.substring(2, delimiterIdx));
-                number = input.substring(delimiterIdx + 1);
+            int delimiterIndex = input.indexOf("\n");
+            if (delimiterIndex != -1) {
+                delimiter = Pattern.quote(input.substring(2, delimiterIndex));
+                numbers = input.substring(delimiterIndex + 1);
             }
         }
-        return number.split(delimiter);
+
+        return numbers.split(delimiter);
     }
 
     private static int parseNumber(String token) {
         try {
             int number = Integer.parseInt(token.trim());
-            if(number < 0) {
-                throw new IllegalArgumentException(number + "은 음수이므로 허용되지 않습니다");
+            if (number < 0) {
+                throw new IllegalArgumentException(number + "은 음수이므로 허용되지 않습니다: " + number);
             }
             return number;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(token + "은 유효하지 않은 숫자입니다");
+            throw new IllegalArgumentException(token + "은 유효하지 않은 숫자입니다.");
         }
-    }
-
-    private static int calculatorSum(String input) {
-        if(input.isEmpty()) {
-            return 0;
-        }
-        String[] tokens = parseInput(input);
-        int sum = 0;
-        for(String token : tokens) {
-            sum += parseNumber(token);
-        }
-        return sum;
     }
 
     private static void printResult(int result) {
