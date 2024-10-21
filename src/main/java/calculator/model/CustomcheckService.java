701c297336abe092cf6637124ed76e9e79a2133b
Hun0914 <bkh010914@naver.com>
2024-10-21 14:26:05 +0900

    feat: 커스텀 구분자 처리 기능 추가 및 입력 검증 개선
    CustomcheckService 클래스에 입력된 문자열을 커스텀 구분자로 처리하는 customsum 메서드를 추가했습니다.

--- src/main/java/calculator/model/CustomcheckService.java
+++ src/main/java/calculator/model/CustomcheckService.java
@@ -1,5 +1,6 @@
 package calculator.model;
 
+import calculator.view.CalculatorView;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +8,76 @@ public class CustomcheckService {
 
     public List<Integer> nonCustom(String input) {
 
+
+        private static List<Integer> customsum(String input ) {
+
+            String word="";
+            char delimiter = input.charAt(2);
+            List<Integer> list = new ArrayList<>();
+
+            for (int i = 5; i < input.length(); i++) {
+
+                char currentChar = input.charAt(i);
+
+                if (!Character.isDigit(input.charAt(i)) && input.charAt(i) != delimiter&&input.charAt(i) != '-') {
+                    throw new IllegalArgumentException("문자 오류");
+                }
+
+
+                if (Character.isDigit(currentChar)) {
+                    word += currentChar;
+                }
+
+                if (input.charAt(i) == delimiter) {
+
+                    if (!word.isEmpty()) {
+                        list.add(Integer.parseInt(word));
+                        word = "";
+                    }
+                }
+            }
+
+            if (!word.isEmpty()) {
+                list.add(Integer.parseInt(word));
+            }
+
+            return list;
+        }
+
+        private static List<Integer> customsum(String input ) {
+
+            String word="";
+            char delimiter = input.charAt(2);
+            List<Integer> list = new ArrayList<>();
+
+            for (int i = 5; i < input.length(); i++) {
+
+                char currentChar = input.charAt(i);
+
+                if (!Character.isDigit(input.charAt(i)) && input.charAt(i) != delimiter&&input.charAt(i) != '-') {
+                    throw new IllegalArgumentException("문자 오류");
+                }
+
+
+                if (Character.isDigit(currentChar)) {
+                    word += currentChar;
+                }
+
+                if (input.charAt(i) == delimiter) {
+
+                    if (!word.isEmpty()) {
+                        list.add(Integer.parseInt(word));
+                        word = "";
+                    }
+                }
+            }
+
+            if (!word.isEmpty()) {
+                list.add(Integer.parseInt(word));
+            }
+
+            return list;
+        }
 
         if(!(input.charAt(0) ==',') && !(input.charAt(0) ==':') && !Character.isDigit(input.charAt(0))) {
             throw new IllegalArgumentException("유효하지 않은 입력값입니다");
@@ -36,4 +107,7 @@ public class CustomcheckService {
 
         return list;
 
-}
+
+
+
+    }
