d7aacd2d88bce25006c3f8ff798d9f88164e1c72
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-21 17:09:38 +0900

    [chore]<extractIntegerFromExpression> : 커스텀 구분자 추가 로직
    split 메서드가 인식하는 특수문자를 일반 문자열로 처리하기 위해
    Pattern의 quote 메서드 활용

--- src/main/java/calculator/utils/IntegerExtractor.java
+++ src/main/java/calculator/utils/IntegerExtractor.java
@@ -1,12 +1,13 @@
 package calculator.utils;
 
 import calculator.model.CalculatorValues;
+import java.util.regex.Pattern;
 
 public class IntegerExtractor {
 
     public static void extractIntegerFromExpression(CalculatorValues calculatorValues) {
         String[] tokens = calculatorValues.getExpression()
-                .split(calculatorValues.getDelimiter());
+                .split(Pattern.quote(calculatorValues.getDelimiter()));
         for(String token : tokens){
             calculatorValues.addNumbersFromInputByDelimiter(token);
         }
