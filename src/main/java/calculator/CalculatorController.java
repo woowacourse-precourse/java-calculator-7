22cb6fb15585d935aebb8182e71a388ec4bab839
thdudgus <gusthdud527@cau.ac.kr>
2024-10-21 20:24:04 +0900

    feat : calculator controller로 input, output, calculator 제어

---
+++ src/main/java/calculator/CalculatorController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import calculator.Calculator;
+import view.InputView;
+import view.OutputView;
+
+public class CalculatorController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Calculator calculator;
+
+    public CalculatorController() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        calculator = new Calculator();
+    }
+
+    public void run() {
+        try {
+            String input = inputView.getInput();
+            int result = calculator.add(input);
+            outputView.printResult(result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
