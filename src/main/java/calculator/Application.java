72f3c0b4508148946ffb823fffbe4e93da0bfbae
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2024-10-21 20:12:51 +0900

    feat(Application): run
    calculator 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,16 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input.print();
+        String input = Input.input();
+        Parser parser = new Parser(input);
+        parser.replace();
+        String[] splitInput = parser.split();
+        Validate.validate(splitInput);
+        Char2Num char2Num = new Char2Num();
+        char2Num.convert(splitInput);
+        Summation summation = new Summation(char2Num.numbers);
+        summation.sum();
+        Output.print(summation.sum);
     }
 }
