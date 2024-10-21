e0444ec8c0fd8ebec4940edf339a56836efc4ba3
rnrnr <rkdqudtjs6@naver.com>
2024-10-21 19:48:44 +0900

    feat: 애플리케이션 기본 구조 및 Console 입력 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine();
+            System.out.println("결과 : " + 0);
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 입력입니다. " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("알 수 없는 오류가 발생했습니다. " + e.getMessage());
+        }
     }
 }
