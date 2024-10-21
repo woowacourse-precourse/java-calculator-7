8d58af53bda8a5c00e4f834e030439fb3a07c294
rootTiket <l20078829@gmail.com>
2024-10-21 18:52:14 +0900

    test: 구분자가 올바른 위치에 있지 않을 경우 예외 발생

--- src/test/java/calculator/domain/validator/InputValidatorTest.java
+++ src/test/java/calculator/domain/validator/InputValidatorTest.java
@@ -1,5 +1,9 @@
 package calculator.domain.validator;
 
+import static calculator.domain.constant.errorMessage.ParseError.INCORRECT_POSITION;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,4 +17,15 @@ class InputValidatorTest {
         String input = "//;\\n1;2;3";
         validator.validateFormat(input);
     }
+
+    @Test
+    @DisplayName("구분자의 시작과 끝이 잘못된 위치에 있을 때 예외가 발생한다")
+    void throwsExceptionForIncorrectDelimiterPosition() {
+        String input = "1,2//;\n3";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> validator.validateFormat(input));
+
+        assertEquals(INCORRECT_POSITION.getMessage(), exception.getMessage());
+    }
+
 }
