d3d0370794ad1bf2631febedd4faf1ffec679ebe
YeaChan05 <qkenrdl05@gmail.com>
2024-10-21 17:17:10 +0900

    fix(HeaderUtils): 빈 구분자 예외처리
    구분자가 빈 문자인 경우 예외 처리 추가

--- src/main/java/calculator/service/HeaderUtils.java
+++ src/main/java/calculator/service/HeaderUtils.java
@@ -1,24 +1,33 @@
 package calculator.service;
 
 public class HeaderUtils {
+
+    private static final String END_STR = "\\n";
+    private static final String START_STR = "//";
+
     public static boolean hasCustomDelimiter(String input) {
-        return input != null && input.startsWith("//");
+        return input != null && input.startsWith(START_STR);
     }
 
     public static String extractHeader(String input) {
-        String endStr = "\\n";
-        int endIndex = input.indexOf(endStr);
+        int endIndex = input.indexOf(END_STR);
         if (endIndex == -1) {
             throw new IllegalArgumentException("invalid header: missing newline after header");
         }
-        String header = input.substring(0, endIndex+2);//"//"와 "\n" 포함
-        validate(header);
-        return header;
+
+        String delimiter = input.substring(START_STR.length(), endIndex);
+        validate(delimiter);
+
+        return input.substring(0, endIndex + END_STR.length());
     }
 
-    private static void validate(String header) {
-        if(header.length()!=5){
+    private static void validate(String delimiter) {
+        if (delimiter.isBlank()) {
+            throw new IllegalArgumentException("invalid header: delimiter cannot be blank");
+        }
+        if (delimiter.length() != 1) {
             throw new IllegalArgumentException("invalid header: invalid length");
         }
+
     }
 }
