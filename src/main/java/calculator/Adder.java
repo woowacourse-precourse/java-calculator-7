e21504e594472e17519e0d0f86ed5ac82bcd5c2e
zionhann <hanzion1108@gmail.com>
2024-10-21 22:21:52 +0900

    refactor: 덧셈 함수 로직 메서드 분리

--- src/main/java/calculator/Adder.java
+++ src/main/java/calculator/Adder.java
@@ -1,36 +1,43 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Adder {
 
     private static final String DEFAULT_DELIM = "[,:]";
     private static final String CUSTOM_DELIM_PREFIX = "//";
     private static final String CUSTOM_DELIM_SUFFIX = "\n";
-    private static final int ALLOW_TAILING_EMPTY_STRING = -1;
 
     public int run(String input) {
-        if (input.startsWith(CUSTOM_DELIM_PREFIX) && input.contains(CUSTOM_DELIM_SUFFIX)) {
-            String[] split = input.split(CUSTOM_DELIM_SUFFIX, ALLOW_TAILING_EMPTY_STRING);
-            String customDelimiter = split[0].substring(CUSTOM_DELIM_PREFIX.length());
-
-            return run(split[1], customDelimiter);
+        if (hasCustomDelim(input)) {
+            String customDelim = input.substring(
+                    input.indexOf(CUSTOM_DELIM_PREFIX) + CUSTOM_DELIM_PREFIX.length(),
+                    input.indexOf(CUSTOM_DELIM_SUFFIX)
+            );
+            String numberString = input.substring(input.indexOf(CUSTOM_DELIM_SUFFIX) + 1);
+            return run(numberString, customDelim);
         }
         return run(input, DEFAULT_DELIM);
     }
 
-    private int run(String input, String delim) {
-        if (input.isEmpty()) {
+    private boolean hasCustomDelim(String input) {
+        return input.startsWith(CUSTOM_DELIM_PREFIX) && input.contains(CUSTOM_DELIM_SUFFIX);
+    }
+
+    private int run(String numberString, String delim) {
+        if (numberString.isEmpty()) {
             return 0;
         }
-        String[] stringNumbers = input.split(delim);
-        int sum = 0;
+        return Arrays.stream(numberString.split(delim))
+                .mapToInt(Integer::parseInt)
+                .map(this::validateNumber)
+                .sum();
+    }
 
-        for (String stringNumber : stringNumbers) {
-            int number = Integer.parseInt(stringNumber);
-            if (number <= 0) {
-                throw new IllegalArgumentException("전달된 숫자는 0보다 작거나 같을 수 없습니다.");
-            }
-            sum += number;
+    private int validateNumber(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("입력된 수가 음수이거나 0입니다.");
         }
-        return sum;
+        return number;
     }
 }
