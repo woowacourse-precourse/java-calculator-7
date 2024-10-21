135e75ece2dd386921632672157dc8a9221709e3
sytTest <syt1128@naver.com>
2024-10-21 21:34:05 +0900

    :sparkle: (Calc) add sumNumbers()
    입력받은 Long 타입 리스트를 전부 더한 후 필드에 저장하는 sumNumbers() 추가

--- src/main/java/calculator/domain/Calc.java
+++ src/main/java/calculator/domain/Calc.java
@@ -1,8 +1,14 @@
 package calculator.domain;
 
+import java.util.List;
+
 public class Calc {
 
     private Long value = 0L;
+
+    public void sumAllNumbers (List<Long> numbers) {
+        value = numbers.stream().mapToLong(Long::longValue).sum();
+    }
 
     public Long getValue() {
         return value;
