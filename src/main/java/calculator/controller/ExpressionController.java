c8c868ffa3f1cb571ea6478a664f516249b71aeb
sytTest <syt1128@naver.com>
2024-10-21 21:18:14 +0900

    :sparkle: (ExpressionController) add createExpression()
    표현식 객체를 구분자 저장소를 생성한 후 필드에 지정하는  createExpression() 추가

--- src/main/java/calculator/controller/ExpressionController.java
+++ src/main/java/calculator/controller/ExpressionController.java
@@ -1,4 +1,15 @@
 package calculator.controller;
 
+import calculator.domain.UserExpression;
+import calculator.repository.DelimiterRepository;
+
 public class ExpressionController {
+
+    private UserExpression userExpression;
+    private DelimiterRepository delimiterRepository;
+
+    private void createExpression(UserExpression userExpression) {
+        this.userExpression = userExpression;
+        delimiterRepository = new DelimiterRepository();
+    }
 }
