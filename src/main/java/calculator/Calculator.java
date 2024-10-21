f70f8c48c7cbe407a5ed7ea22f31754f9b9476b5
Namkyung Koo <united94@naver.com>
2024-10-21 17:01:51 +0900

    feat: add comments about Parser-inner class

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -105,11 +105,18 @@ public class Calculator {
         System.out.println(String.format("결과 : %.1f", sum));
     }
 
+    /** 입력한 문자열의 파싱을 담당하는 내부 클래스 */
     private class Parser {
 
         public Parser() {
         }
 
+        /**
+         * 입력된 문자열이 유효한 문자열인지 검사하고 문자열을 재조립합니다.
+         *
+         * @param originalInput 사용자 입력 문자열의 원본
+         * @return 구분자 섹션이 있으면, 구분자 섹션을 제외한 사용자 입력 문자열을 구분자 섹션이 없으면 원본 문자열을 반환
+         */
         private String parseString(String originalInput) {
             String strippedInput = originalInput.strip();
 
@@ -126,6 +133,11 @@ public class Calculator {
             return originalInput;
         }
 
+        /**
+         * 앞,뒤로 White space를 제거한 사용자 입력 문자열에 구분자 섹션이 있는지 확인합니다.
+         *
+         * @param strippedInput 앞,뒤로 White space를 제거한 사용자 입력 문자열
+         */
         private void hasDelimiterSection(String strippedInput) {
             if (Character.isDigit(strippedInput.charAt(0))) {
                 return;
@@ -135,12 +147,23 @@ public class Calculator {
                     strippedInput.charAt(delimiterManager.getLenOfPrefix()));
         }
 
+        /**
+         * 유효한 형식을 갖춘 문자열인지 확인합니다.
+         *
+         * @param targetString 검증받을 문자열
+         */
         private void checkValidInput(String targetString) {
             if (targetString.length() <= delimiterManager.getLenOfPrefix() + delimiterManager.CUSTOM_DELIMITER_LEN) {
                 throw new IllegalArgumentException("The input formats invalidly.");
             }
         }
 
+        /**
+         * 커스텀 구분자를 기준으로 문자열을 쪼개 커스텀 구분자를 포함하지 않은 뒤 문자열을 반환합니다.
+         *
+         * @param strippedInput 쪼개질 대상이 될 문자열
+         * @return 커스텀 구분자를 포함하지 않은 뒤 문자열
+         */
         private String splitByDelimiterChar(String strippedInput) {
             String customDelimiter = Character.toString(delimiterManager.getCustomDelimiter());
 
@@ -151,6 +174,12 @@ public class Calculator {
             return splittedString[1];
         }
 
+        /**
+         * 해당 문자열이 구분자 섹션 접미사와 일치하는지 확인합니다.
+         *
+         * @param stringWithSuffix 커스텀 구분자 뒤부터 시작하는 문자열
+         * @return 구분자 섹션을 제외한 문자열
+         */
         private String compareToSuffix(String stringWithSuffix) {
             if (stringWithSuffix.length() < delimiterManager.getLenOfSuffix()) {
                 throw new IllegalArgumentException("The input formats invalidly. especially at suffix.");
