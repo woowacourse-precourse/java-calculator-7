4537c448bfd4abec1c6ffb0d86d9efdef7b7fdc4
cherry031 <20203093@kookmin.ac.kr>
2024-10-21 19:13:29 +0900

    fix: edit custom delimiter condition

--- src/main/java/calculator/service/StringParser.java
+++ src/main/java/calculator/service/StringParser.java
@@ -7,14 +7,14 @@ public class StringParser {
         if (input.startsWith("//")) {
             String customDelimiter = findCustomDelimiter(input);
             String delimiters = DEFAULT_DELIMITERS + "|" + customDelimiter;
-            return splitString(input.substring(input.indexOf("\n") + 1), delimiters);
+            return splitString(input.substring(input.indexOf("\\n") + 2), delimiters);
         }
 
         return splitString(input, DEFAULT_DELIMITERS);
     }
 
     private String findCustomDelimiter(String input) {
-        int endIndex = input.indexOf("\n");
+        int endIndex = input.indexOf("\\n");
         return input.substring(2, endIndex);
     }
 
