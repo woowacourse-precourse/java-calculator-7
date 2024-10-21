4198f0b9eec245fceeb0f27625b16dd1baf24c23
leestana01 <leestana01@naver.com>
2024-10-21 09:06:26 +0900

    feat(Application): 기본 구분자로 문제 해결
    문제 상황 해결 로직 추가
    - 사용자 입력 받기
    - 기본 구분자 정의
    - 입력과 구분자 기반으로 결과 값 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        String delimiter = "[,:]";
+
+        System.out.println("결과 : " +
+            Arrays.stream(input.split(delimiter))
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum));
     }
 }
