a3a042f14febbc95a711c1c712aa67945734dba4
Nestory <xofks777@naver.com>
2024-10-21 20:30:54 +0900

    feature(Display): 덧셈할 문자열을 입력받는 메서드 구현

---
+++ src/main/java/calculator/Display.java
@@ -0,0 +1,10 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Display {
+    public static String displayInput() {
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        return Console.readLine();
+    }
+}
