f25081691c9c4eada83575efeb84e9e776a2804b
Park - Jeonghwan <990706leo@gmail.com>
2024-10-21 16:38:37 +0900

    feat(CalculatorController): 계산을 하고 결과를 출력한다

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,5 +1,7 @@
 package calculator.controller;
 
+import calculator.model.Calculator;
+import calculator.model.vo.Number;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
@@ -12,5 +14,9 @@ public class CalculatorController {
         outputView.printStart();
         String expression = inputView.inputNumbers();
 
+        Calculator calculator = new Calculator();
+        Number sum = calculator.calculate(expression);
+
+        outputView.printResult(sum);
     }
 }
