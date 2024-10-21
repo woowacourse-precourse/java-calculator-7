2d6dfc97d2fa3fc5cdc5e973faa2f4a107f8a6b2
김세희 <sehee0954@gmail.com>
2024-10-21 12:23:39 +0900

    feat(): 문자 추출 후 덧셈 기능 추가
    에 문자 추출 후 덧셈 기능을 추가했습니다:
    - delimiters 로 기본 구분자를 선언 후 문자열을 기본 구분자로 분리하여 추출하였습니다.
    - 추출된 숫자들을  sum에 더했습니다.
    - 만약 num이 음수시, 숫자 이외의 문자 입력시에 대한 예외처리도 하였습니다.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,7 +2,26 @@ package calculator;
 
 public class StringCalculator {
     public static int add(String input){
+        String delimiters = ",|:";
 
+        // 구분자를 기준으로 문자열 분리
+        String[] numbers = input.split(delimiters);
+        int sum = 0;
+
+        try {
+            for (String number : numbers) {
+                if (!number.isEmpty()) {
+                    int num = Integer.parseInt(number);
+                    if (num < 0) {// 음수 값 입력시
+                        throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+                    }
+                    sum += num;
+                }
+            }
+        } catch (NumberFormatException e) { //숫자가 아닌 값 입력시,
+            throw new IllegalArgumentException("잘못된 입력 값입니다. 숫자만 포함되어야 합니다.");
+        }
+
+        return sum;
     }
-
 }
