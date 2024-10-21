3a7aa8ca68b0d4e65b82e51558766231103f9e27
DESKTOP-V78KGF4\LOVE <wkdskdud05@naver.com>
2024-10-21 22:08:53 +0900

    feat(return 0): return 0 for empty or null input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.print("숫자를 입력하세요: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        try {
+            System.out.println("결과 : " + add(input));
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 값을 입력하였습니다.: " + e.getMessage());
+        }
+
+        scanner.close();  // Scanner 닫기
+    }
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
     }
 }
