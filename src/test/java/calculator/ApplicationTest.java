293008f702b6b108eb022f37b68f3388358d5444
V4N1LLA <bubble1549@gmail.com>
2024-10-21 17:21:45 +0900

    test: 테스트 코드 추가(구분자 검증, 공백 검증)

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -5,24 +5,59 @@ import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
-    void 커스텀_구분자_사용() {
+    void testBasicDelimiters() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+
+        assertSimpleTest(() -> {
+            run("4:5:6");
+            assertThat(output()).contains("결과 : 15");
+        });
+
+        assertSimpleTest(() -> {
+            run("1,2:3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void testCustomDelimiter() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
         });
     }
 
+
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+    void testException() {
+        assertSimpleTest(() -> {
+            run("-1,2,3");
+            assertThat(output()).contains("음수는 허용되지 않습니다: -1");
+        });
     }
+
+    @Test
+    void testSpace() {
+        assertSimpleTest(() -> {
+            run(" ");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    /*@Test
+    void testNull() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }*/
 
     @Override
     public void runMain() {
