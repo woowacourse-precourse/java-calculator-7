9529a1c7539024b309b2fa51ef8712d4fe65e794
ssun <56533266+blaire-pi@users.noreply.github.com>
2024-10-21 19:51:13 +0900

    feat: 입력값이 빈 문자열일 때, 조건 처리

--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -3,8 +3,13 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
-    public static void main(String[] args) {
-        System.out.print("식을 입력하세요: ");
-        String userInput = Console.readLine();
+    public String getInput() {
+        System.out.print("문자열을 입력하세요: ");
+        String input = Console.readLine();
+
+        if (input == null || input.trim().isEmpty()) {
+            return "0";
+        }
+        return input;
     }
 }
