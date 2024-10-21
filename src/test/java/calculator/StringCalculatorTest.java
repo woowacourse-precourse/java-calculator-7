ec21552e4e816c1d37d78c5abe31f3600b4a4c29
wlsdlf2 <wlsdlfzg@naver.com>
2024-10-21 17:14:55 +0900

    test: @ParameterizedTest로 테스트 코드 수정
    - @MethodSource사용하여 테스트 통합
    - @EmptySource로 빈 문자열 처리

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -2,57 +2,43 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class StringCalculatorTest {
     StringCalculator calculator = new StringCalculator();
 
-    @Test
-    void 빈_문자열_처리() {
-        //given
-        String input = "";
-
-        //when
-        int result = calculator.calculate(input);
-
-        //then
-        assertThat(result).isEqualTo(0);
+    static Stream<Arguments> successfulInputs() {
+        return Stream.of(
+                Arguments.of("1,2:3", 6),
+                Arguments.of("//;\\n1;2;3", 6),
+                Arguments.of("//;\\n1;2,3:4;5", 15)
+        );
     }
 
-    @Test
-    void 기본_구분자_덧셈() {
-        //given
-        String input = "1,2:3";
-
+    @ParameterizedTest
+    @MethodSource("successfulInputs")
+    @DisplayName("기본 구분자, 커스텀 구분자, 혼합 구분자를 사용하여 계산에 성공한다.")
+    void 계산_성공_기본_및_커스텀_구분자(String input, int expected) {
         //when
         int result = calculator.calculate(input);
 
         //then
-        assertThat(result).isEqualTo(6);
+        assertThat(result).isEqualTo(expected);
     }
 
-    @Test
-    void 커스텀_구분자_덧셈() {
-        //given
-        String input = "//;\\n1;2;3";
-
+    @ParameterizedTest
+    @EmptySource
+    void 빈_문자열_처리(String input) {
         //when
         int result = calculator.calculate(input);
 
         //then
-        assertThat(result).isEqualTo(6);
-    }
-
-    @Test
-    void 혼합_구분자_덧셈() {
-        //given
-        String input = "//;\\n1;2,3:4";
-
-        //when
-        int result = calculator.calculate(input);
-
-        //then
-        assertThat(result).isEqualTo(10);
+        assertThat(result).isZero();
     }
 
 }
