f1626c3cbe594cf053b2c07406460e75f4ff471e
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-21 18:05:09 +0900

    chore: [기능 구현 4번] InputViewImpl 불필요 코드 제거

--- src/main/java/calculator/view/input/InputViewImpl.java
+++ src/main/java/calculator/view/input/InputViewImpl.java
@@ -5,14 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputViewImpl implements InputView {
 
-    private static final String INPUT_MESSAGE = "아래 예시와 같이 문자를 입력해주세요.\n";
-    private static final String INPUT_MESSAGE_EXAMPLE = "\"\" => 0, \"1,2\" => 3, \"1,2,3\" => 6, \"1,2:3\" => 6 (구분자: 쉼표(,) 또는 콜론(:))";
     private static final String INPUT_STRING = "덧셈할 문자열을 입력해 주세요.";
 
     @Override
     public String readInputMessage() {
-        System.out.println(INPUT_MESSAGE);
-        System.out.println(INPUT_MESSAGE_EXAMPLE);
         System.out.println(INPUT_STRING);
 
         String inputData = Console.readLine();
