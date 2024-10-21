6b7ac1074900bccf536c6446407537b1b01154e4
eoehd1ek <eoehd2ek@gmail.com>
2024-10-21 13:57:11 +0900

    feat(Application): 문자열 값 입력 기능 추가
    입력한 문자열에서 숫자를 추출하여 더하는 계산기를 구현하기 위해 문자열을 입력받는다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,7 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 문자열 값 입력
+        String input = camp.nextstep.edu.missionutils.Console.readLine();
     }
 }
