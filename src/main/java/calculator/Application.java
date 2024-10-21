609a646d8551a38ed6ace3e19e075b2db959c2b7
Trillion_Impulse <tkddu2897@naver.com>
2024-10-21 22:19:35 +0900

    fix: correct exception for customSep

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,7 +11,8 @@ public class Application {
 
         // 문자열 입력 받기
         String input = Console.readLine();
-        
+            //System.out.println("입력값 : " + input);
+
 
         // 구분자 변수에 기본 구분자 넣기
         String Sep = ",|:";
@@ -20,18 +21,25 @@ public class Application {
         
         
         // 커스텀 구분자
-        // 문자열이 "//"로 시작되면 커스텀 구분자 설정으로 인지
-        if (input.startsWith("//")){
-            String[] division = input.split("\n",2); // \n을 기준으로 문자열을 2개로 분할
-                // 분할된 input 테스트
-                //System.out.println("분할0번지 : " + division[0]);
-                //System.out.println("분할1번지 : " + division[1]);
-            String customSep = division[0].substring(2); // 나눈 문자열의 0번지 배열의 문자열에서 "//"제거 후 커스텀 구분자로 지정
-            Sep += "|";
-            Sep += customSep; // 구분자에 커스텀 구분자 포함시키기
-                // 구분자에 커스텀 구분자가 포함 되었는지 테스트
-                //System.out.println("구분자 : " + Sep);
-            input = division[1]; // 나눈 문자열의 1번지 배열의 문자열을 input으로 지정
+        if (input.startsWith("//")){ // 문자열이 "//"로 시작되면 커스텀 구분자 설정으로 인지
+
+            int end = input.indexOf("\\n"); //문자열에서 \n의 위치 찾기, 인덱스번호를 end에 삽입
+                //end 확인
+                //System.out.println("\\n의 위치 : " + end);
+
+            if (end == -1) { //문자열에 \n 없으면 예외처리
+                throw new IllegalArgumentException("구분자와 양수, 커스텀 구분자 설정 형식만 입력 가능합니다."); // 예외처리 output_message
+            } else if (end == 2) {
+                throw new IllegalArgumentException("커스텀 구분자가 입력되지 않았습니다."); // 예외처리 output_message
+            }
+            Sep += "|" + input.substring(2,end); // 구분자에 커스텀 구분자 포함시키기
+                //구분자에 커스텀 구분자가 포함 되었는지 테스트
+                //System.out.println("커스텀된 구분자 : " + Sep);
+
+            input = input.substring(end + 2); // 나눈 문자열의 1번지 배열의 문자열을 input으로 지정
+                //커스텀 구분자 처리 이후 input    
+                //System.out.println("커스텀구분자처리 이후 입력값 : " + input);
+            
         }
 
 
@@ -56,11 +64,11 @@ public class Application {
                 int num = Integer.parseInt(number); // 타입을 문자에서 숫자로 변환
 
                 if (num < 0){ // 음수의 경우
-                    throw new IllegalArgumentException("구분자와 양수만 입력 가능합니다."); // 음수의 경우 예외처리
+                    throw new IllegalArgumentException("구분자와 양수, 커스텀 구분자 설정 형식만 입력 가능합니다."); // 예외처리 output_message
                 }
                 sum += num; // 덧셈
             } catch (NumberFormatException e) { // 숫자가 아닐 경우
-                throw new IllegalArgumentException("구분자와 양수만 입력 가능합니다."); // 이외 예외처리
+                throw new IllegalArgumentException("구분자와 양수, 커스텀 구분자 설정 형식만 입력 가능합니다."); // 예외처리 output_message
             }
         }
 
