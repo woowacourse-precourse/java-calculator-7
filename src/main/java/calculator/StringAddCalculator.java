11391f549f487bc0cd14f33704cbc5a87f2018c7
wobrorry <128110758+wobrorry@users.noreply.github.com>
2024-10-21 23:03:26 +0900

    refactor(StringAddCalculator): 코드 분리 중첩된 if 문 혹은 for 문을 메소드로 분리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -17,11 +17,11 @@ public class StringAddCalculator {
     private static String[] splitString(String input){
         String delimiter = ",|:";
 
+        //커스텀 구분자 추가 과정
         if (input.startsWith("//")) {
             int delimiterIndex = input.indexOf("\\n");
-            if (delimiterIndex == -1) {
-                throw new IllegalArgumentException("구분자 지정 형식이 올바르지 않습니다. '\\n'이 필요합니다.");
-            }
+            wrongInputFormat(delimiterIndex); // 구분자 지정 형식이 올바르지 않을 경우
+
             delimiter = input.substring(2,delimiterIndex);
             String anotherDelimiter = "|".concat(input.substring(2, delimiterIndex));
             input = input.substring(delimiterIndex + 2);
@@ -31,30 +31,42 @@ public class StringAddCalculator {
         return input.split(delimiter);
     }
 
+    private static void wrongInputFormat(int delimiterIndex){
+        if (delimiterIndex == -1) {
+            throw new IllegalArgumentException("구분자 지정 형식이 올바르지 않습니다. '\\n'이 필요합니다.");
+        }
+    }
+
     private static int sumNumbers(String[] numbers){
         int sum = 0;
         for(String number : numbers){
-            int num;
-            if(isNullOrEmpty(number)){
-                num = 0;
-            } else {
-                num = checkNumber(number);
-            }
+            int num = arrayEmptyStringCheck(number);
             sum += num;
         }
         return sum;
+    }
+
+    private static int arrayEmptyStringCheck(String number){
+        if (isNullOrEmpty(number)) {
+            return 0;
+        }
+        return checkNumber(number);
     }
 
     private static int checkNumber(String number) {
         int num;
         try{
             num = Integer.parseInt(number);
-            if(num < 0){
-                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
-            }
+            negativeNumberCheck(num);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자 혹은 구분자만 입력해주세요.");
         }
         return num;
+    }
+
+    private static void negativeNumberCheck(int num){
+        if(num < 0){
+            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+        }
     }
 }
