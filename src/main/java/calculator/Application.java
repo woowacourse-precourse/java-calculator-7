b1dba41b72eb9c3a0e0b0e65aa5d8631ee174974
이규빈 <kyubin.dev@gmail.com>
2024-10-21 22:00:40 +0900

    feat: 기본 구분자를 기준으로 분리한 각 숫자 합 구하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String input = Console.readLine();
+        String[] sArr = input.split(",|:");
+
+        long answer = 0;
+        for (String s : sArr) {
+            answer += Integer.parseInt(s);
+        }
+
+        System.out.println("결과 : " + answer);
     }
 }
