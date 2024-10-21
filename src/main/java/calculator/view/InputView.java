74394aa6db7e909f36574eb15ed8b892b82c47be
youjae <youjae0803@gmail.com>
2024-10-21 17:25:53 +0900

    feat: InputView 는 입력을 커스텀 구분자와 덧셈 문자열로 구분한다.

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -9,6 +9,33 @@ public class InputView {
     public static List<String> getInput(){
         List<String> ret = new ArrayList<String>();
         String input = Console.readLine();
+
+        String customSeparator = extractCustomSeparator(input);
+        String customFormula = extractFormula(input);
+
+        ret.add(customSeparator);
+        ret.add(customFormula);
+
         return ret;
+    }
+
+    private static String extractCustomSeparator(String input) {
+        String subStr = input.substring(0, 5);
+
+        if (subStr.startsWith("//")) {
+            if(subStr.endsWith("\\n"))
+                return "" + subStr.charAt(2);
+        }
+
+        return null;
+    }
+
+    private static String extractFormula(String input){
+        String formula = input;
+
+        if (input.startsWith("//"))
+            formula = input.substring(5);
+
+        return formula;
     }
 }
