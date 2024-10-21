ace8a3de2e97af01ef74c6348df7ae050b01d31e
장영재 <urinaner@naver.com>
2024-10-21 12:46:03 +0900

    feat(Calculator): 문자열 계산 구현

---
+++ src/main/java/calculator/Caculator.java
@@ -0,0 +1,20 @@
+package calculator;
+
+public class Caculator {
+    public int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        int[] numbers = DataCleaner.cleanData(input);
+        return sum(numbers);
+    }
+
+    private int sum(int[] numbers) {
+        int result = 0;
+        for (int num : numbers) {
+            result += num;
+        }
+        return result;
+    }
+}
