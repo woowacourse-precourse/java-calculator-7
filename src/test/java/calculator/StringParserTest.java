be53311d1e6ed0780374311a5e6d4c3bb3b4c25a
Seyoung Goo <0917sy@gmail.com>
2024-10-21 18:20:20 +0900

    test: 기본 구분자로 문자열 구분 기능 테스트
    - 기본 구분자(쉼표, 클론)으로 문자열 구분
    - 추출된 각 요소가 숫자인지 확인하고 숫자로 변환
    - 잘못된 값이 포함된 경우 예외 발생(음수, 정의되지 않은 구분자, 숫자 이외의 값)

---
+++ src/test/java/calculator/StringParserTest.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import calculator.model.StringParser;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class StringParserTest {
+    StringParser stringParser = new StringParser();
+
+    private static Stream<Arguments> provideDefaultDelimiterInputSuccess() {
+        return Stream.of(
+                Arguments.of("1,2,10", List.of(BigDecimal.ONE, BigDecimal.TWO, BigDecimal.TEN)),
+                Arguments.of("1:2:10", List.of(BigDecimal.ONE, BigDecimal.TWO, BigDecimal.TEN)),
+                Arguments.of("1,2:10", List.of(BigDecimal.ONE, BigDecimal.TWO, BigDecimal.TEN)),
+                Arguments.of("1,2,10000000000", List.of(BigDecimal.ONE, BigDecimal.TWO, BigDecimal.valueOf(
+                        10000000000L)))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDefaultDelimiterInputSuccess")
+    public void 기본_구분자로_문자열_분리(String input, List<BigDecimal> expected) {
+        List<BigDecimal> numbers = stringParser.parse(input);
+        assertEquals(expected, numbers);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,-2,10", "1/2/10", "1,2,a,10"})
+    public void 입력에_잘못된_값이_포함된_경우_에러_발생(String input) {
+        assertThrows(IllegalArgumentException.class, ()
+                -> stringParser.parse(input));
+    }
+}
