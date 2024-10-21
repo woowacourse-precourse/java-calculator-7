8235c34aecedd7bb6cf9d7e5ba68bce34f9383c1
zeus <jj99526@naver.com>
2024-10-21 22:32:47 +0900

    feat : 모든 값 더하기 및 에러처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,7 +9,6 @@ public class Application {
         Scanner scanner=new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = scanner.nextLine();
-
         return input;
     }
 
@@ -24,22 +23,39 @@ public class Application {
         }
         return separator;
     }
+    public int sumAll(String input,Set<String> separator){
+        String delimiterPattern = String.join("|", separator); // Set에 있는 구분자를 패턴으로 생성
 
+        // "//"로 시작하는 경우 입력에서 구분자와 숫자를 분리
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\\n");
+            input = input.substring(delimiterIndex + 2); // 구분자 부분 제거
+        }
 
+        // 입력값이 없는 경우 0 반환
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("숫자를 입력하지 않으셨습니다.");
+        }
 
+        // 구분자를 기준으로 문자열 분리
+        String[] numbers = input.split(delimiterPattern);
+        // 숫자 합 계산 및 에러 처리 (숫자가 아닌 값이 있을 때)
+
+        int sum = 0;
+        for (String number : numbers) {
+            try {
+                sum += Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("입력된 문자열에 숫자가 아닌 값이 포함되어 있습니다.");
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args) {
-
         Application application=new Application();
         String input= application.getInput();
         Set<String>separator=application.separatorCheck(input);
-//        int result=application.sum(input,separator);
-
-
-
-
-
-
-
-        // TODO: 프로그램 구현
+        int result=application.sumAll(input,separator);
+        System.out.println("결과 : "+result);
     }
 }
