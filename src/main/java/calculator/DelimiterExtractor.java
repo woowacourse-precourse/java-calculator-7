88ac315fa64de738272f4fe59f53493ace91eca4
Eclieee <100896@naver.com>
2024-10-21 22:08:39 +0900

    feat(delimiter): 커스텀 구분자가 존재할 경우 사용하도록 구현

--- src/main/java/calculator/DelimiterExtractor.java
+++ src/main/java/calculator/DelimiterExtractor.java
@@ -1,10 +1,38 @@
 package calculator;
 
-public class DelimiterExtractor {
-    private final String input;
+import static calculator.LiteralString.INVALID_CUSTOM_DELIMITER;
 
-    public DelimiterExtractor(String input){
+class DelimiterExtractor {
+    private static final String DEFAULT_DELIMITERS = ",|:";
+    private final String input;
+    private String delimiter;
+    private String numbers;
+
+    public DelimiterExtractor(String input) {
         this.input = input;
+        extractDelimiterAndNumbers();
     }
 
+    public String getDelimiter() {
+        return delimiter;
+    }
+
+    public String getNumbers() {
+        return numbers;
+    }
+
+    private void extractDelimiterAndNumbers() {
+        if (input.startsWith("\\")) {
+            int customDelimiterEndIndex = input.indexOf("\n");
+            if (customDelimiterEndIndex == -1) {
+                throw new IllegalArgumentException(INVALID_CUSTOM_DELIMITER);
+            }
+            int customDelimiterStartIndex = input.indexOf("\\");
+            this.delimiter = input.substring(customDelimiterStartIndex, customDelimiterEndIndex).trim();
+            this.numbers = input.substring(customDelimiterEndIndex + 1);
+        } else {
+            this.delimiter = DEFAULT_DELIMITERS;
+            this.numbers = input;
+        }
+    }
 }
---
+++ src/main/java/calculator/LiteralString.java
@@ -0,0 +1,5 @@
+package calculator;
+
+public class LiteralString {
+    public static final String INVALID_CUSTOM_DELIMITER = "It seems to be invalid custom delimiter format, Missing newline after custom delimiter.";
+}
