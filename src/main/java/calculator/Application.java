8894023fed89930e11576ac8fed5c49a5d101c2e
soobin <a01056239778@gmail.com>
2024-10-21 15:41:51 +0900

    feat: "Add calculate based on default separator (comma or colon)"
    기본 구분자(쉼표 또는 콜론)을 기준으로 분리한 후 각 숫자의 합을 반환 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.test.NsTest;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application application = new Application();
+        String input = Console.readLine();
+        int result = application.calculate(input);
+        System.out.println("결과 : " + result);
     }
+
+    public int calculate(String text) {
+        String defaultSeparator = ":";
+        String[] textArray = replaceSeparator(text, defaultSeparator).split(defaultSeparator);
+        int sum = 0;
+        for (String number : textArray) {
+            try {
+                sum += Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return sum;
+    }
+
+    private String replaceSeparator(String text, String separator) {
+        return text.replaceAll(",", separator);
+    }
+
 }
