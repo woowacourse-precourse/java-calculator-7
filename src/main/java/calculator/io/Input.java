57fac79d60d307a7651ccc7674308fea768f8940
jeonjuho <wjswngh23@daum.net>
2024-10-21 16:00:27 +0900

    feat(Input): 문자열 입력 추가
    - 입력된 문자열을 읽어오는 방법을 알지 않아도 되도록 Input.readLine()을 통해 래핑

---
+++ src/main/java/calculator/io/Input.java
@@ -0,0 +1,11 @@
+package calculator.io;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public static String readLine() {
+        return Console.readLine();
+    }
+
+}
