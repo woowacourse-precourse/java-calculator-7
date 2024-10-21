97b256601926809a970244b29fb942d9baf88efc
daxxn <dahyeonhan4529@gmail.com>
2024-10-21 17:18:50 +0900

    test(view): add unit test and update view logic
    - Add CalculatorViewTest to validate user input and output
    - Modified input handling in CalculatorView to address edge case

--- src/main/java/view/CalculatorView.java
+++ src/main/java/view/CalculatorView.java
@@ -1,6 +1,7 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class CalculatorView {
 
@@ -9,7 +10,11 @@ public class CalculatorView {
 
     public String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine();
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("Wrong input.");
+        }
     }
 
     public void displayResult(int result) {
