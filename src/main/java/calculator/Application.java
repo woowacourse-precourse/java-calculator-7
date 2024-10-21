7a99215a8eecdcb43505dc8cd9edf68a121c084e
pjy2163 <pjy2163@naver.com>
2024-10-21 15:04:45 +0900

    feat: 빈 문자열 체크 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,22 +5,31 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        String[] numbers = input.split("[,:]");
+        String[] numbers;
 
         if (input.startsWith("//")) {
-            String delimiter = input.substring(2, input.indexOf("\n"));
-            input = input.substring(input.indexOf("\n") + 1);
-            numbers = input.split(delimiter);
+            int newLineIndex = input.indexOf("\n");
+            if (newLineIndex == -1) {
+                throw new IllegalArgumentException("구분자가 잘못되었습니다.");
+            }
+            String delimiter = input.substring(2, newLineIndex);
+            input = input.substring(newLineIndex + 1);
+            numbers = input.split("[" + delimiter + ",:]");
+        }
+        else {
+            numbers = input.split("[,:]");
         }
     
         int sum = 0;
         for (String number : numbers) {
-            int num = Integer.parseInt(number.trim()); 
-            if (num < 0) {
-                throw new IllegalArgumentException("음수는 허용되지 않습니다.");
-            }
-            sum += num; 
-        }  
+            if (!number.trim().isEmpty()) {
+                int num = Integer.parseInt(number.trim()); 
+                if (num < 0) {
+                    throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+                }
+                sum += num; 
+            }  
+        }
         System.out.println("결과 : " + sum);
-    }
+        
 }
