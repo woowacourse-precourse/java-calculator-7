a8e1408291535b4c58dd95bde70c560d13ef0c23
YJ9985 <lucy1486@naver.com>
2024-10-21 13:22:14 +0900

    feat: 빈 문자열 입력 처리 기능 추가
    빈 문자열 입력 시 0을 반환하도록 구현
    - StringCalculator 클래스에 빈 문자열 체크 로직 추가
    - 빈 문자열인 경우 즉시 0 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        StringCalculator calculator = new StringCalculator();
+
+        System.out.println("덧셈할 문자열을 입력해주세요: ");
+        String input = sc.nextLine();
+
+        try{
+            int result = calculator.calculate(input);
+            System.out.println("결과: " + result);
+        } catch (IllegalArgumentException e){
+            System.out.println("오류: " + e.getMessage());
+        }
+
+        sc.close();
+    }
+
+    class StringCalculator {
+        public int calculate(String input) {
+            return 0;
+        }
     }
 }
