76c5129b80531f0ca1cccd5e826fb39ca26353f3
LouiIII3 <jikdsunghuilee@naver.com>
2024-10-21 19:40:07 +0900

    feat(calculator): 입력 및 출력 처리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import java.util.Scanner;
+import java.util.stream.Stream;
+import java.util.regex.Pattern;
+
 public class Application {
+    private int answer = 0;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(new Application().calculator());
+    }
+
+    public String calculator() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = scanner.nextLine();
+
+        scanner.close();
+        return "결과 : " + answer;
     }
 }
