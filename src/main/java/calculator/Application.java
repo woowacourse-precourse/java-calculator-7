53e0a16e2b4a276cdca4b3c59299475a3db9f108
MINSEO PARK <miinseo0324@gmail.com>
2024-10-21 19:22:30 +0900

    feat: 빈문자열 처리 기능 추가
    빈 문자열 or 잘못된 문자열을 입력했다면,
    0을 반환하도록 함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,31 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    public static int emptyInput(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0; // 빈 문자열을 입력했다면,
+        }
+        return -1; // 정상적인 값을 넣었다면,
+    }
+
+    public static int add(String input) {
+        int result = emptyInput(input);
+        if (result != -1) {
+            return result; // 빈 문자열이면 0 반환
+        }
+        return 1;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        try {
+            int result = add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            throw e;
+        }
     }
 }
