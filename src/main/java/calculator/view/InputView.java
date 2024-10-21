892cbb92b6203a90817595ae16f48d3e1813cf40
davemins <davemins4125@naver.com>
2024-10-21 17:46:58 +0900

    refactor(View): MessageType에서 메시지를 관리하도록 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,22 +1,18 @@
 package calculator.view;
 
-import calculator.global.constants.MessageType;
 import camp.nextstep.edu.missionutils.Console;
+import calculator.global.constants.MessageType;
 import calculator.global.validator.Validator;
 
 public class InputView {
     public static String requestString() {
-        printlnMessage(MessageType.STRING_REQUEST_MESSAGE);
+        OutputView.printMessage(MessageType.STRING_REQUEST_MESSAGE);
         return enterMessage();
-    }
-
-    private static void printlnMessage(MessageType messageType) {
-        System.out.println(messageType.getMessage());
     }
 
     private static String enterMessage() {
         String message = Console.readLine();
-        Validator.validateNotEmpty(message);
+        Validator.validateNotEmpty(message); // 입력 검증
         return message;
     }
 }
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -4,10 +4,10 @@ import calculator.global.constants.MessageType;
 
 public class OutputView {
     public static void printResult(int result) {
-        System.out.println("결과 : " + result);
+        System.out.print(MessageType.RESULT_MESSAGE.getMessage() + result);
     }
 
-    public static void printlnMessage(MessageType messageType) {
-        System.out.println(messageType.getMessage());
+    public static void printMessage(MessageType messageType) {
+        messageType.printMessage();
     }
 }
