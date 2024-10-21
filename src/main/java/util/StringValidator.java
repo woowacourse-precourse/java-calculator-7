b0b4307b04d3ba2e8fee906e93c5f8d98b425d4e
sseopsseop <ponde119@outlook.com>
2024-10-21 15:26:29 +0900

    fix: add StringValidator 클래스 문자열을 검증하는 클래스 문자열이 null 이거나 조건에 맞지 않는 문자열 입력이 들어온다면 IllegalArgumentException을 발생

--- src/main/java/util/StringValidator.java
+++ src/main/java/util/StringValidator.java
@@ -1,4 +1,51 @@
 package util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringValidator {
+    // 문자열 검증 메서드
+    public static void stringValidate(String inputString) throws IllegalArgumentException{
+       checkNull(inputString);
+       checkNegativeValue(inputString);
+       if(inputString.contains("//")){
+           validateCustomDelimiter(inputString);
+       }else validateDelimiters(inputString, Constants.DEFAULT_DELIMITERS);
+    }
+
+    // 입력 문자열이 null인 경우
+    public static void checkNull(String inputString){
+        if(inputString == null){
+            throw new IllegalArgumentException("입력한 문자열이 null 입니다.");
+        }
+    }
+
+    // 문자열 앞부분의 "//"와 "\n" 사이에 커스텀 구분자를 지정하지 않은 경우
+    // "//"와 "\n" 사이에 2개 이상의 문자가 포함되어 있는 경우
+    public static void validateCustomDelimiter(String inputString){
+        Pattern customDelimiterPattern = Pattern.compile("//(.)\n(.*)");
+        Matcher matcher = customDelimiterPattern.matcher(inputString);
+        if(matcher.matches()){
+            String customDelimiter = matcher.group(1);
+            String otherText = matcher.group(2);
+
+            String newDelimiter = Constants.DEFAULT_DELIMITERS + "|"+customDelimiter;
+            validateDelimiters(otherText, newDelimiter);
+
+        }else throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다.");
+    }
+
+    // 기본 및 지정된 커스텀 구분자와 숫자 이외의 문자가 포함된 경우
+    public static void validateDelimiters(String inputString, String delimiters){
+        String[] splitTexts = delimiters.split(delimiters);
+
+        for(String text : splitTexts){
+            if(!text.matches("\\d+")) throw new IllegalArgumentException("지정하지 않은 구분자가 포함되어 있습니다.");
+        }
+    }
+    // 음수가 입력된 경우
+    public static void checkNegativeValue(String inputString){
+        if(inputString.matches(".*-\\d+.*"))
+            throw new IllegalArgumentException("입력 값에 음수가 포함되어 있습니다.");
+    }
 }
