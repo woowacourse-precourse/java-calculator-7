14075aac6caf082664d735ab5659bc8d7925e5d4
kkmdevel <kkm.devel@gmail.com>
2024-10-21 12:50:02 +0900

    refactor: SeparatorValidationUtil 입력 문자열 줄바꿈 문자 치환
    "//?\\n"의 입력을 기본으로 설정

--- src/main/java/calculator/util/SeparatorValidationUtil.java
+++ src/main/java/calculator/util/SeparatorValidationUtil.java
@@ -5,16 +5,19 @@ import calculator.constant.MessageConst;
 public class SeparatorValidationUtil {
 
     /**
-     * 구분자 문자열이 //로 시작하고 \n으로 끝나는지, 그리고 사이에 정확히 한 개의 문자가 있고, 그 문자가 숫자가 아닌지 검증.
+     * 구분자 문자열이 //로 시작하고 \\n으로 끝나는지, 그리고 사이에 한 개의 문자가 있고, 그 문자가 숫자가 아닌지 검증.
      *
      * @param separatorString 입력 문자열
      */
     public static void validateSeparatorStringFormat(String separatorString) {
-        if (!separatorString.startsWith("//") || !separatorString.endsWith("\n")) {
+        separatorString = separatorString.replace("\n", "\\n");
+
+        if (!separatorString.startsWith("//") || !separatorString.endsWith("\\n")) {
             throw new IllegalArgumentException(MessageConst.INVALID_SEPARATOR_MSG);
         }
 
-        String betweenSeparators = separatorString.substring(2, separatorString.length() - 1);
+        String betweenSeparators = separatorString.substring(2, separatorString.length() - 2);
+        System.out.println(betweenSeparators);
         if (betweenSeparators.length() != 1 || Character.isDigit(betweenSeparators.charAt(0))) {
             throw new IllegalArgumentException(MessageConst.INVALID_SEPARATOR_MSG);
         }
