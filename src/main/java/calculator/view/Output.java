a52780f76a4a1369b6cf87c0800903fea9c40e11
mikekks <thdalsrb79@naver.com>
2024-10-21 19:58:08 +0900

    chore(Output): default 스펠링 수정

--- src/main/java/calculator/view/Output.java
+++ src/main/java/calculator/view/Output.java
@@ -3,7 +3,7 @@ package calculator.view;
 public class Output {
 
 	private static final String RESULT_MESSAGE = "결과 : ";
-	private static final String DEFALUT_VALUE = "0";
+	private static final String DEFAULT_VALUE = "0";
 	private static final String INPUT_PROMPT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 
 	public static void printResult(int result){
@@ -11,7 +11,7 @@ public class Output {
 	}
 
 	public static void printDefaultResult(){
-		System.out.println(RESULT_MESSAGE + DEFALUT_VALUE);
+		System.out.println(RESULT_MESSAGE + DEFAULT_VALUE);
 	}
 
 	public static void printInputPrompt(){
