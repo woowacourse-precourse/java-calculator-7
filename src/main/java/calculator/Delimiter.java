0a2d13cb233541ad3f0230dbbd9cf4e0358b94a7
SECHANG1412 <binaryblitz1412@gmail.com>
2024-10-21 10:17:01 +0900

    git add . git commit -m "fix: Delimiter 클래스에서 커스텀 구분자 없는 경우 예외 처리 추가" git push origin main

--- src/main/java/calculator/Delimiter.java
+++ src/main/java/calculator/Delimiter.java
@@ -4,9 +4,9 @@ public class Delimiter {
     private String delimiter;
 
     public Delimiter(String input) {
-        if (input.startsWith("//")) {
+        if (input.startsWith("//") && input.contains("\n")) {
             int delimiterIndex = input.indexOf("\n");
-            this.delimiter = input.substring(2, delimiterIndex);
+            this.delimiter = input.substring(2, delimiterIndex);  // 커스텀 구분자
         } else {
             this.delimiter = "[,|:]";  // 기본 구분자 쉼표와 콜론
         }
