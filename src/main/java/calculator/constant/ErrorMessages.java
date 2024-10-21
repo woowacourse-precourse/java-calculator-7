df8875bc58feb4bead94ac885a32f6869d72bb9e
kimchanho97 <nh0903@pusan.ac.kr>
2024-10-21 20:13:50 +0900

    feat(CustomValidator): 커스텀 구분자로 입력 문자열 검증

--- src/main/java/calculator/constant/ErrorMessages.java
+++ src/main/java/calculator/constant/ErrorMessages.java
@@ -4,5 +4,5 @@ public class ErrorMessages {
 
     public static final String NEGATIVE_NUMBER_NOT_ALLOWED = "음수는 허용되지 않습니다.";
     public static final String INVALID_NUMBER_FORMAT = "유효하지 않은 숫자입니다.";
-
+    public static final String CUSTOM_DELIMITER_NOT_FOUND = "커스텀 구분자가 없습니다.";
 }
--- src/main/java/calculator/model/CustomValidator.java
+++ src/main/java/calculator/model/CustomValidator.java
@@ -2,6 +2,9 @@ package calculator.model;
 
 import java.util.regex.Matcher;
 
+import static calculator.constant.DelimiterConstants.DEFAULT_DELIMITER_REGEX;
+import static calculator.constant.ErrorMessages.*;
+
 public class CustomValidator implements Validator{
 
     private final Matcher matcher;
@@ -12,7 +15,44 @@ public class CustomValidator implements Validator{
 
     @Override
     public ParsedInput validate(String input) {
+        String delimiter = extractDelimiter();
+        String numbersWithDelimiter = matcher.group(2);
 
-        return null;
+        String delimiterRegex = buildDelimiterRegex(delimiter);
+        String[] numbers = numbersWithDelimiter.split(delimiterRegex);
+
+        validateNumbers(numbers);
+
+        return new ParsedInput(delimiterRegex, numbersWithDelimiter);
+    }
+
+    private static void validateNumbers(String[] numbers) {
+        for (String number : numbers) {
+            if (number.isEmpty()) {
+                continue;
+            }
+
+            try {
+                int num = Integer.parseInt(number);
+                if (num < 0) {
+                    throw new IllegalArgumentException(NEGATIVE_NUMBER_NOT_ALLOWED);
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(INVALID_NUMBER_FORMAT);
+            }
+        }
+    }
+
+    private String buildDelimiterRegex(String delimiter) {
+        return DEFAULT_DELIMITER_REGEX.substring(0, DEFAULT_DELIMITER_REGEX.length() - 1)
+                + delimiter + "]";
+    }
+
+    private String extractDelimiter() {
+        String delimiter = matcher.group(1);
+        if (delimiter.isEmpty()) {
+            throw new IllegalArgumentException(CUSTOM_DELIMITER_NOT_FOUND);
+        }
+        return delimiter;
     }
 }
