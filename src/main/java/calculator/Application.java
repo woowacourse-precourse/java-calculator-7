c6f95208035c7607d72c90aba9de5362a955a37b
hakSick <koukai@kakao.com>
2024-10-21 12:39:30 +0900

    fix : 입력문과 출력문 수정
    입력문과 출력문 영문에서 한글로 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,14 +5,14 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
     	
-    	System.out.println("put the number: ");
+    	System.out.println("덧셈할 문자열을 입력해 주세요: ");
 
         String input = Console.readLine();
 
         StringCalculator calculator = new StringCalculator();
 
         int result = calculator.add(input);
-        System.out.println("answer : " + result);
+        System.out.println("결과 : " + result);
     }
 }
 
