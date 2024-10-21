9cdafac814053cbc7b229a60d3a56d6da887936b
Cori1304-Seong <andyjm7739@gmail.com>
2024-10-21 11:55:02 +0900

    feat(Application): update OperationService 사용 결과 출력
    - 출력 String 테스트 코드 결과 같은 형식 설정
    
    에) "결과 : 1"

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,12 +4,8 @@ public class Application {
     public static void main(String[] args) {
 
       String input = InputService.readLine();
+      int result = OperationService.sumStringNum(input);
 
-      // TODO: int로 바뀐 숫자 sum 기능 구현, class나 함수로 분리
-        int number = Integer.parseInt(input);
-        System.out.println("number의 타입; : " + ((Object) number).getClass().getName());
-        number += 10;
-        System.out.println("number + 10 = " + (number));
-        System.out.println("number의 타입; : " + ((Object) number).getClass().getName());
+      System.out.println("결과 : " + result);
     }
 }
