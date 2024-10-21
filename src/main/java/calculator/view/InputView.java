b30eae87984fbec6a20aeb90fdbe6fe997cc3479
COBI-98 <tkdgus968@naver.com>
2024-10-21 23:46:28 +0900

    refactor: 출력 뷰 메서드 명 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_STRING_TO_ADD = "덧셈할 문자열을 입력해 주세요.";
 
-    private static String inputString(){
+    public static String createString() {
         System.out.println(INPUT_STRING_TO_ADD);
         return Console.readLine();
     }
