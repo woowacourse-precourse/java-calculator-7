b90cf16005f8a412051ddc0c0125c06e5731c523
Jhsysng <ysng1118@naver.com>
2024-10-21 21:56:12 +0900

    feat: trime 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,7 +8,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         System.out.print("덧셈할 문자열을 입력해주세요.");
-        String input = readLine();
+        String input = readLine().trim();
 
         try{
             input = input.replace("\\n", "\n");
