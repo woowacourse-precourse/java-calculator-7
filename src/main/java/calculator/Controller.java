ac2c8a00582de20625321d4752aa8fc56efc5b24
gracepec <gracepec0621@gmail.com>
2024-10-21 18:35:13 +0900

    feat(customSeparator): 커스텀 구분자 확인 및 구분자 통일
    - SeparatorHandler 클래스 추가
    - 여러 커스텀 구분자 설정이 가능하도록 구현
    - 기본 구분자 및 커스텀 구분자를 ','로 통일

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -1,15 +1,20 @@
 package calculator;
 
 import calculator.service.InputHandler;
+import calculator.service.SeparatorHandler;
 
 public class Controller {
     private final InputHandler inputHandler;
+    private final SeparatorHandler separatorHandler;
 
     public Controller() {
         this.inputHandler = new InputHandler();
+        this.separatorHandler = new SeparatorHandler();
     }
 
     public void run(){
         String input = inputHandler.getUserInput();
+
+        input = separatorHandler.checkCustomSeparator(input);
     }
 }
