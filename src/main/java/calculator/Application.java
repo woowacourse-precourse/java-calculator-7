253bd4b4cbb41417d7c3f422cad3254f8249f015
kmc198989 <kmc198989@naver.com>
2024-10-21 19:09:35 +0900

    feat(calculator): null 또는 빈 문자열 입력 시 0을 반환하는 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        System.out.println("결과 : " + add(input));
+    }
+
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        // 이후 단계에서 추가 코드 작성
+        return 0;
     }
 }
