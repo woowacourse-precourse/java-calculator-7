ac73e8395e05cf3f782f3ffffe7d0ec04d24f16f
706com <ehdgusjdbswl@gmail.com>
2024-10-21 23:50:17 +0900

    [Fix] 콘솔 출력 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,7 +11,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = InputConsole.inputConsole();
-        System.out.println(input);
 
         String[] numbers = calculatorService.splitInputString(input);
 
