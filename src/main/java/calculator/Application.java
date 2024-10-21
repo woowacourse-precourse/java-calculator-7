417ac03044dc4d84709d614c9dfd6f74c12cff5c
smk412 <whu1477@naver.com>
2024-10-21 16:11:26 +0900

    feat : 문자열 입력 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+
+        System.out.println(str);
+
+
     }
 }
