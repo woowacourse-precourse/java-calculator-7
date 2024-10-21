dd915f0515f01c1b9f853e6909d0b77f38273dfb
gyeong <gyeong011212@gmail.com>
2024-10-21 17:23:04 +0900

    feat: 문자열을 쉼표 및 콜론으로 분리하여 합계를 계산하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,33 @@
 package calculator;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+
+        // 문자열 입력
+        System.out.print("덧셈할 문자열을 입력해주세요.");
+        String input = scanner.nextLine();
+
+        // 결과 출력
+        int result = StringCalculator.add(input);
+        System.out.println("결과 : " + result);
+
+        scanner.close();
+    }
+}
+
+class StringCalculator {
+    public static int add(String input) {
+
+        // 쉼표(,)와 콜론(:)을 기준으로 문자열을 분리
+        String[] numbers = input.split("[,:]");
+
+        // 각 숫자를 합산
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number.trim());
+        }
+        return sum;
     }
 }
