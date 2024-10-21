c0e3d57c9bd2db480bd549424b09ec1d6aea0b6e
Chan531 <chan657312@gmail.com>
2024-10-21 18:08:11 +0900

    test(CustomDelimiterValidatorTest): 기본 문자열 테스트

---
+++ src/test/java/calculator/model/CustomDelimiterValidatorTest.java
@@ -0,0 +1,31 @@
+package calculator.model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CustomDelimiterValidatorTest {
+
+	@Test
+	@DisplayName("커스텀 구분자가 포함되지않는 문자열이 입력되었을 때 기본 생성자로 생성된 StringValidator를 반환한다.")
+	void 커스텀_구분자가_포함된_문자열이_입력되지_않았을_때_이를_반영한_문자열_유효성_검사_객체를_반환한다() {
+		// given
+		CustomDelimiterValidator customDelimiterValidator = new CustomDelimiterValidator();
+		String input = "1,2:3";
+		String inputExpectedResult = "1,2:3";
+		List<Delimiter> delimitersExpectedResult = Stream.of(",", ":")
+			.map(Delimiter::new)
+			.toList();
+
+		// when
+		StringValidator stringValidator = customDelimiterValidator.validCustomDelimiter(input);
+
+		// then
+		assertThat(stringValidator.getInput()).isEqualTo(inputExpectedResult);
+		assertThat(stringValidator.getDelimiters()).isEqualTo(delimitersExpectedResult);
+	}
+}
