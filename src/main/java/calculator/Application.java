6ce7570c9db4c644db327880318b6e1cb6e915d1
Hyso <hsk7953@gmail.com>
2024-10-21 21:24:52 +0900

    :recycle: refactor : CalculatorFactory 도입으로 인한 코드 개선

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,15 +1,11 @@
 package calculator;
 
-import calculator.addCalculator.AddCalculator;
 import calculator.model.MainCalculator;
-import calculator.stringSplitter.StringController;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        StringController controller = new StringController();
-        AddCalculator addCalculator = new AddCalculator();
-        MainCalculator mainCalculator = new MainCalculator(addCalculator,controller);
+        MainCalculator mainCalculator = CalculatorFactory.createCalculator();
         mainCalculator.run();
     }
 }
