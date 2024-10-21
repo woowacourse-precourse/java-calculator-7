d534ec8d9c6a02171902b023fa670615fdca9c81
lrive <dlwldms9803@gmail.com>
2024-10-21 21:15:38 +0900

    feat: 입력받은 문자열에서 숫자 추출 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 수를 입력해주세요.");
+        String input = Console.readLine();
+        try {
+            int result = Calculator.calculate(input);
+            //System.out.println("결과: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println();
+        }
     }
+
+
 }
---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator {
+
+    public static int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        String delimiter = "[,:]";
+        if (input.startsWith("//")) {
+            Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+            if (matcher.find()) {
+                delimiter = Pattern.quote(matcher.group(1)); // 커스텀 구분자를 사용
+                input = matcher.group(2);
+            }
+        }
+        String[] tokens = input.split(delimiter);
+        return sum(tokens);
+    }
+}
