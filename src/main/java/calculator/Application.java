63376f091ad33804cb1569bd4486126c6658643e
dudujh0106 <rkdwlgh01@naver.com>
2024-10-21 21:13:28 +0900

    refactor: extract custom delimiter exception handling to separate method

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -26,6 +26,7 @@ class StringCalculator {
                 throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다.");
             }
             delimiter = input.substring(2, endIdx);
+            checkCustomDelimiter(delimiter);
             input = input.substring(endIdx + 2);
             if(input.isEmpty()) return 0;
         }
@@ -51,16 +52,18 @@ class StringCalculator {
         }
     }
 
+    private static void checkCustomDelimiter(String delimiter) {
+        if(delimiter.isEmpty()) {
+            throw new IllegalArgumentException("커스텀 구분자가 입력되지 않았습니다.");
+        }
+        if(delimiter.matches(".*\\d.*")) {
+            throw new IllegalArgumentException("숫자는 커스텀 구분자가 될 수 없습니다.: " + delimiter);
+        }
+    }
+
     private static String[] splitString(String input, String delimiter) {
         try {
-            if(delimiter.isEmpty()) {
-                throw new IllegalArgumentException("커스텀 구분자가 입력되지 않았습니다.");
-            }
-
             String delimiters = String.join("|", delimiter.split(""));
-            if(delimiter.matches(".*\\d.*")) {
-                throw new IllegalArgumentException("숫자는 커스텀 구분자가 될 수 없습니다.: " + delimiter);
-            }
             return input.split(escapeMetaCharacter(delimiters));
         } catch (PatternSyntaxException e) {
             throw new IllegalArgumentException("커스텀 구분자의 입력이나 처리가 올바르지 않습니다.: " + delimiter);
