029b12f814206a99ab1124fa8ee70627f0415dbf
seoyeon2001 <syeon1250@naver.com>
2024-10-21 15:54:55 +0900

    Chore: 필요 없는 파일 및 주석 삭제

--- src/main/java/calculator/service/SumCalculator.java
+++ src/main/java/calculator/service/SumCalculator.java
@@ -3,8 +3,6 @@ package calculator.service;
 public class SumCalculator {
 
     public int sum(String[] inputs) {
-//        System.out.println(Arrays.toString(inputs));
-//        System.out.println(inputs[0].getClass().getName()); // java.lang.String
         int result = 0;
 
         for (String input : inputs) {
@@ -14,9 +12,8 @@ public class SumCalculator {
                     throw new IllegalArgumentException("음수는 입력할 수 없습니다. 입력된 값: " + input);
                 }
                 result += number;
-            } catch (NumberFormatException ex) { // 숫자가 아닌 문자가 들어온 경우 처리
-//                ex.printStackTrace();
-//                throw new IllegalArgumentException(ex.getMessage(), ex); // 에러 => 종료
+            } catch (NumberFormatException ex) {
+                // 숫자가 아닌 문자가 들어온 경우 처리
                 throw new IllegalArgumentException("구분자 외의 문자는 입력할 수 없습니다." + input, ex);
             } catch (IllegalArgumentException ex) {
                 // 음수가 들어온 경우 처리
