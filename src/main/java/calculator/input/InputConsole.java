94baa5fbfcb6bb5ab37dfa32145f045e9bb19a28
706com <ehdgusjdbswl@gmail.com>
2024-10-21 22:54:21 +0900

    [Feat] Console Input 요소 메서드 구현

---
+++ src/main/java/calculator/input/InputConsole.java
@@ -0,0 +1,9 @@
+package calculator.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputConsole {
+    public static String inputConsole(){
+        return Console.readLine();
+    }
+}
