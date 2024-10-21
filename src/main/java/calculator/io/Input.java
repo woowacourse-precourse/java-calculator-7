3d9c9f543686766b4d4cdfa50769fb4490780fcd
jeonjuho <wjswngh23@daum.net>
2024-10-21 20:55:19 +0900

    fix(Input): 입력 값이 없을 경우 빈 문자열 반환하도록 수정
    - 입력 값이 없을 때를 예외로 처리하지 않고 결과 값을 0으로 출력하도록 수정

--- src/main/java/calculator/io/Input.java
+++ src/main/java/calculator/io/Input.java
@@ -1,11 +1,17 @@
 package calculator.io;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class Input {
 
     public static String readLine() {
-        return Console.readLine();
+        String inputString = "";
+        try {
+            inputString = Console.readLine();
+        } catch (NoSuchElementException ignored) {
+        }
+        return inputString;
     }
 
 }
