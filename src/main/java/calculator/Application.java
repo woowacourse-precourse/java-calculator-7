23740bba0c18c5692b293427e1cdebe75fcfeb08
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-10-21 21:21:17 +0900

    feat(Application): 기본 구분자를 통해 숫자를 추출한다
    기본 구분자를 통해 문자열을 분리하고 그 값들을 배열에 저장하여 더할 수 있도록 한다

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    public static void calculate(String input) {
+        String numbers;
+            numbers = input;
+            // 쉼표와 콜론을 기준으로 분리
+            String[] numberArray = numbers.split("[,;]");
+        return;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            System.out.println("숫자를 입력하세요");
+            String input = Console.readLine();
+            calculate(input);
+        }
+        finally {
+            Console.close();
+        }
     }
 }
