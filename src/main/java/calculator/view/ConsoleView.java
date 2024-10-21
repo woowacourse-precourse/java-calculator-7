45bce0d0fccf4a357d0002e0efbb56547602b88d
kkmdevel <kkm.devel@gmail.com>
2024-10-21 16:23:23 +0900

    refactor: ConsoleView 사용자 입력받기 기능
    입력시 빈 문자열이여도 오류 출력하지 않고 받음

--- src/main/java/calculator/view/ConsoleView.java
+++ src/main/java/calculator/view/ConsoleView.java
@@ -3,19 +3,24 @@ package calculator.view;
 import calculator.constant.MessageConst;
 import camp.nextstep.edu.missionutils.Console;
 import java.math.BigInteger;
+import java.util.NoSuchElementException;
 
 public class ConsoleView {
 
     public String getUserInput() {
         System.out.println(MessageConst.INPUT_PROMPT);
-        return Console.readLine();
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            return "";
+        }
     }
 
     public void printResult(BigInteger result) {
-        System.out.print("\n" + MessageConst.RESULT_MSG + result);
+        System.out.print(MessageConst.RESULT_MSG + result);
     }
 
     public void printErrorMessage(String errorMsg) {
-        System.out.print("\n" + MessageConst.INVALID_INPUT_MSG + errorMsg);
+        System.out.print(MessageConst.INVALID_INPUT_MSG + errorMsg);
     }
 }
