e8e9f4e511ea6bafa973087414746e83b849f0a3
jaehwannnnnn <seanjung0915@gmail.com>
2024-10-21 17:24:23 +0900

    feat: 기본 구분자 및 커스텀 구분자로 문자열 분리하기 구현

---
+++ src/main/java/calculator/InputParser.java
@@ -0,0 +1,20 @@
+package calculator;
+
+public class InputParser {
+    public static String[] stringParse(String str){
+        if(str.startsWith("//")){
+            return customParser(str);
+        }else{
+            return str.split(":|;");
+        }
+    }
+    public static String[] customParser(String str){
+        int idx = str.indexOf("\n");
+        if(idx == -1){
+            throw new IllegalArgumentException("잘못된 구분자 형식입니다.");
+        }
+        String custom = str.substring(2, idx);
+
+        return str.substring(idx + 1).split("custom");
+    }
+}
