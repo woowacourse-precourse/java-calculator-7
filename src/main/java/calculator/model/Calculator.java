b2257b196c0ec9761a442e9c224ee12221575666
JinHyung-Park <trupjh@gmail.com>
2024-10-21 22:57:03 +0900

    feature : 구분자와 숫자 파싱 기능 구현

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -4,11 +4,42 @@ import java.util.Arrays;
 
 public class Calculator {
     public int calc(String input) throws IllegalArgumentException {
-        return 1;
+            int[] arr = validateInput(input);
+            return sum(arr);
     }
     public int sum(int[] arr) {
         return Arrays.stream(arr).sum();
     }
+    private int[] validateInput(String input) throws IllegalArgumentException{
+        String delimiter = null;
 
+        if(input.contains("//") && input.contains("\\n")){
+            delimiter = extractCustomDelimiter(input);
+            input = input.replace("//" + delimiter + "\\n", "");
+        }else if(input.contains(",")){
+            delimiter = ",";
+        }else if(input.contains(".")){
+            delimiter = ".";
+        }
+
+        return parseNumbers(input, delimiter);
+    }
+
+    private String extractCustomDelimiter(String input) {
+        int startIndex = input.indexOf("//") + 2;
+        int endIndex = input.indexOf("\\n", startIndex);
+        return input.substring(startIndex, endIndex);
+    }
+
+    private int[] parseNumbers(String input, String delimiter) throws IllegalArgumentException {
+        String[] stringNumbers = input.split(delimiter);
+        return convertToIntArray(stringNumbers);
+    }
+
+    private int[] convertToIntArray(String[] stringNumbers) {
+        return Arrays.stream(stringNumbers)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
 
 }
