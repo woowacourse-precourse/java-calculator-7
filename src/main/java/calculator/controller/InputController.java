3083b87b33d069824e0346b49d8409d8253a53e3
sytTest <syt1128@naver.com>
2024-10-21 21:14:19 +0900

    :sparkle: (InputController) add comesNewInput()
    빈 입력이 필터링 된 입력 값을 userExpression 객체 생성한 후 담는 comesNewInput() 추가

--- src/main/java/calculator/controller/InputController.java
+++ src/main/java/calculator/controller/InputController.java
@@ -1,8 +1,16 @@
 package calculator.controller;
 
+import calculator.domain.UserExpression;
 import calculator.view.Users;
 
 public class InputController {
+
+    private UserExpression userExpression;
+
+    public void comesNewInput() {
+        userExpression = new UserExpression(filterVacantInput());
+    }
+
 
     private String filterVacantInput() {
         String input = Users.input();
