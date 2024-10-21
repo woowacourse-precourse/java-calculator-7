d22e442c60754931b110d59a4e2c4bc1688bb098
pil-go <pilbongx@naver.com>
2024-10-21 09:26:09 +0900

    refactor(DelimiterValidator): 커스텀 구분자 처리 메소드 역할 분리

--- src/main/java/calculator/DelimiterValidator.java
+++ src/main/java/calculator/DelimiterValidator.java
@@ -5,24 +5,46 @@ public class DelimiterValidator {
     private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
     private static final int CUSTOM_DELIMITER_LIMIT = 1;
 
+    public boolean hasCustomDelimiter(String input) {
+        int start = input.indexOf(CUSTOM_DELIMITER_PREFIX);
+        int end = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+
+        if (end != -1 && start > end) {
+            throw new IllegalArgumentException("커스텀 구분자 지정 명령이 잘못되었습니다. 올바른 명령:" + CUSTOM_DELIMITER_PREFIX + "[구분자]" + CUSTOM_DELIMITER_SUFFIX);
+        }
+        if (start != -1 && end != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public void validateDelimiter(String customDelimiter) {
+        if (customDelimiter.length() > CUSTOM_DELIMITER_LIMIT) {
+            throw new IllegalArgumentException("커스텀 구분자는 문자 " + CUSTOM_DELIMITER_LIMIT + "개까지만 지정 가능합니다. 입력한 구분자: " + customDelimiter);
+        }
+        for (char c : customDelimiter.toCharArray()) {
+            if (Character.isDigit(c)) {
+                throw new IllegalArgumentException("커스텀 구분자는 숫자로 지정할 수 없습니다. 입력한 구분자: " + customDelimiter);
+            }
+        }
+    }
+
     public String getCustomDelimiter(String input) {
         //커스텀 구분자가 있다면 반환, 없다면 "" 반환
         String delimiter = "";
+
+        if (!hasCustomDelimiter(input)) {
+            return delimiter;
+        }
+
         int start = input.indexOf(CUSTOM_DELIMITER_PREFIX);
         int end = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
 
         if (start != -1 && end != -1) {
             delimiter = input.substring(start + CUSTOM_DELIMITER_PREFIX.length(), end);
         }
-        if (delimiter.length() > CUSTOM_DELIMITER_LIMIT) {
-            throw new IllegalArgumentException("커스텀 구분자는 문자 " + CUSTOM_DELIMITER_LIMIT + "개까지만 지정 가능합니다. 입력한 구분자: " + delimiter);
-        }
-        for (char c : delimiter.toCharArray()) {
-            if (Character.isDigit(c)) {
-                throw new IllegalArgumentException("커스텀 구분자는 숫자로 지정할 수 없습니다. 입력한 구분자: " + delimiter);
-            }
-        }
 
+        validateDelimiter(delimiter);
         return delimiter;
     }
 }
