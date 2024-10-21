a85dded0c8b268411c4d532f892ac139bef2f2e0
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-10-21 23:36:57 +0900

    feat: Add string input and output results in Application.java

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 안내 메시지 출력
+        System.out.println("덧셈할 숫자를 구분자와 함께 입력해주세요\n"
+                + "기본 구분자는 : 와 , 이며 커스텀 구분자를 설정하고 싶으시다면 문장 맨 앞에 // 문자와 \\n 문자 사이에 1개의 문자를 설정해주시면 됩니다.");
+
+        // 2. 문자열 입력 받기
+        String input = Console.readLine();
+
+        // 입력 값을 처리하여 결과를 계산하고 출력
+        int result = StringCalculator.add(input);
+        System.out.println("결과 : " + result);
     }
 }
