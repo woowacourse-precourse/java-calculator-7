79e632bc099ea012018a075e2230de0b54cd2300
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-10-21 21:36:17 +0900

    feat(Application): 커스텀 구문자를 통해 값들을 추출하여 저장한다
    "//"를 통해 커스텀 구문자 처리로 분기처리한다. "\n"을 통해 커스텀 구분자를 추출한다.
    구분자를 활용하여 문자열을 분리하고 추출한다. 만약에, "\n"이 존재하지 않으면 예외처리한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,9 +4,23 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void calculate(String input) {
         String numbers;
+        if (input.length() >= 2 && input.substring(0, 2).equals("//")) {
+            int slashIndex = input.indexOf("\\n");
+            // 앞의 구분자를 제외하고 numbers에 다시 저장한다
+            numbers = input.substring(slashIndex + 2);
+            if (slashIndex != -1) {
+                String customDivider = input.substring(2, slashIndex);
+                String[] numberArray = numbers.split(String.valueOf(customDivider));
+            }
+            else{
+                throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다.");
+            }
+         }
+        else {
             numbers = input;
             // 쉼표와 콜론을 기준으로 분리
             String[] numberArray = numbers.split("[,;]");
+        }
         return;
     }
     public static void main(String[] args) {
