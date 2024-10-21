67fed0a6de382bbd16da60da0b5a3064ee9c490c
YujinJuhn <holsya@naver.com>
2024-10-21 19:42:20 +0900

    feat: Add exception handling for negative and invalid inputs

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -13,7 +13,21 @@ public class StringAddCalculator {
         return sumTokens(tokens);
     }
 
-    private static int sumTokens(String[] tokens){
-        return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
+    private static int sumTokens(String[] tokens) {
+        try {
+            int sum = 0;
+            for(String token : tokens){
+                int number = Integer.parseInt(token);
+                if(number < 0){
+                    throw new IllegalArgumentException("음수가 포함되어 있습니다.");
+                }
+                sum += number;
+            }
+            return sum;
+        } catch (NumberFormatException e) {
+            System.out.println(Arrays.toString(tokens));
+            throw new IllegalArgumentException("잘못된 입력 값이 포함되어 있습니다.");
+        }
     }
+
 }
