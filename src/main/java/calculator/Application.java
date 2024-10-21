656d9e11f8518421d776d354bf1005f6b08b622d
baesh <baesh6778@gmail.com>
2024-10-21 21:07:23 +0900

    feat: 문자로 된 숫자 더해서 결과 출력하기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,12 +7,29 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-
+        int result = 0;
+        
         if (input.startsWith("//")) {
             String sep = input.substring(2, input.indexOf("\\n"));
             String[] numbers = input.substring(input.indexOf("\\n") + 2).split(",|:|" + sep);
+            result = sumNum(numbers);
         } else {
             String[] numbers = input.split("[,:]");
+            result = sumNum(numbers);
         }
+
+        System.out.println("결과 : " + result);
+    }
+
+    private static int sumNum(String[] numbers) {
+        int result = 0;
+        for (String num : numbers) {
+            int number = Integer.parseInt(num.trim());
+            if (number < 0) {
+                throw new IllegalArgumentException("잘못된 값입니다: " + number);
+            }
+            result += number;
+        }
+        return result;
     }
 }
