dd62234e36ddff37787d944ce0f19cd9a6ee975f
sik9252 <sik9252@naver.com>
2024-10-21 19:36:06 +0900

    refactor: Calculator에 생성자 주입하는 형태로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,7 +5,9 @@ import calculator.utils.Splitter;
 public class Application {
     public static void main(String[] args) {
         Splitter splitter = new Splitter();
-        Calculator calculator = new Calculator(splitter);
+        Validator validator = new Validator();
+        StringSumCalculator stringSumCalculator = new StringSumCalculator();
+        Calculator calculator = new Calculator(splitter, validator, stringSumCalculator);
 
         calculator.run();
     }
