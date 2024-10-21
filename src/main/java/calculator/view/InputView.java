8d9bdbc17b2fda5530924aacd1f48ec0703eb903
youjae <youjae0803@gmail.com>
2024-10-21 16:59:18 +0900

    feat: InputView 사용자로부터 입력 받는 기능

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,14 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputView {
+    public static List<String> getInput(){
+        List<String> ret = new ArrayList<String>();
+        String input = Console.readLine();
+        return ret;
+    }
+}
