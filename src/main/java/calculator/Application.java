b1d41f2e8f3bc1e9ba8ba9983b86bd0c9e710a24
gladhee <gladhee@icloud.com>
2024-10-21 17:21:16 +0900

    fix(Application): 안 쓰는 import 문 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,6 @@
 package calculator;
 
 import calculator.controller.CalculatorController;
-import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
