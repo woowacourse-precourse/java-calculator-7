ea32a641955f1067420bedaac76a707c1d7eca05
sytTest <syt1128@naver.com>
2024-10-21 20:16:22 +0900

    :sparkle: create CustomDelim
    커스텀 구분자를 담는 CustomDelim 클래스 생성

---
+++ src/main/java/calculator/domain/CustomDelim.java
@@ -0,0 +1,12 @@
+package calculator.domain;
+
+public class CustomDelim {
+    private final String delim = null;
+
+    public CustomDelim(String input) {
+    }
+
+    public String getDelim() {
+        return delim;
+    }
+}
