ac28219b0a2cab051a409fa0ee4005b44e76376a
김세희 <sehee0954@gmail.com>
2024-10-21 14:51:22 +0900

    feat(): 커스텀 구분자 처리
    에 커스텀 구분자 처리 하였습니다:
    - pattern을 사용하여 구분 기준을 잡고 matcher로 커스텀 구분자를 추출하였습니다.
    - 커스텀 구분자를 구분자 변수에 추가하고 뒤의 문자열은 앞의 커스텀 부분을 제거 후 재선언 하였습니다.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,8 +1,24 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public static int add(String input){
         String delimiters = ",|:";
+
+        // 커스텀 구분자 처리
+        if (input.startsWith("//")) {
+            Pattern pattern = Pattern.compile("//(.)\\\\n");
+            Matcher matcher = pattern.matcher(input);
+            if(matcher.find()){
+                String customDelimiter = matcher.group(1);
+                input = input.replaceFirst(pattern.pattern(),"");
+                delimiters += "|" + Pattern.quote(customDelimiter);
+            } else {
+                throw new IllegalArgumentException("잘못된 입력 형식입니다.");
+            }
+        }
 
         // 구분자를 기준으로 문자열 분리
         String[] numbers = input.split(delimiters);
