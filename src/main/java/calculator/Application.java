b5fc14607960b7b2e9c729d1bb75d095efb7968a
smk412 <whu1477@naver.com>
2024-10-21 17:58:38 +0900

    feat : 커스텀  구분자로 문자열 분리 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,13 +6,25 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
+        String sep="";
         String str = sc.nextLine();
-        String newSep = str;
+        String [] newSep = str.split("");
+        String[] newStr = new String[]{""};
 
-        String[] sep = str.split("[,:]");
-        for( int i = 0; i < sep.length; i++ ) {
-            System.out.println(sep[i]);
+        for (int i=2; i < newSep.length;i++){
+            if (newSep[i].equals("\\") && newSep[i + 1].equals("n")) {
+                break;
+            }
+            sep += newSep[i];
         }
-
+        if(!sep.isEmpty()){
+            newSep = str.split("[,:n"+sep+"]");
+            newStr = newSep;
+        }else{
+            newStr = str.split("[,:]");
+        }
+        for (String s : newStr) {
+            System.out.println(s);
+        }
     }
 }
