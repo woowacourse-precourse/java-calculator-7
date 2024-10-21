5e7ee2c0da4fc76779820a1a0cfa19b86af3c273
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-10-21 18:17:27 +0900

    feat: 커스텀 구분자 파트와 수식 파트로 분리 기능 추가
    - 입력값의 유효성 검증

--- src/main/java/calculator/domain/Separator.java
+++ src/main/java/calculator/domain/Separator.java
@@ -1,13 +1,33 @@
 package calculator.domain;
 
+import calculator.constant.Constant;
 import calculator.util.RegexGenerator;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Separator {
 
     public List<String> separateByDelimiter(String input, String delimiter) {
         String delimiterRegex = RegexGenerator.generateRegex(delimiter);
         return Arrays.asList(input.split(delimiterRegex));
+    }
+
+    public String separateToFormulaPart(String input) {
+        if (input.startsWith(Constant.CUSTOM_DELIMITER_PREFIX)) {
+            Matcher matcher = Pattern.compile(Constant.VALIDATION_REGEX).matcher(input);
+            validateFormat(matcher);
+
+            return input.split(Constant.CUSTOM_DELIMITER_SUFFIX)[1];
+        }
+
+        return input;
+    }
+
+    private void validateFormat(Matcher matcher) {
+        if (matcher.results().count() != 1) {
+            throw new IllegalArgumentException("잘못된 형식의 입력입니다.");
+        }
     }
 }
