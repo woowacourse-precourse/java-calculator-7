8eab293cdab61c6d269ea4310fcd2e8e94e7423d
chan <ckstj8027@naver.com>
2024-10-21 20:38:22 +0900

    refactor(delimiterExtractor): 검증기에 의존하게하여 구분자추출에만 집중가능

--- src/main/java/calculator/delimiterExtractor/CustomDelimiterExtractor.java
+++ src/main/java/calculator/delimiterExtractor/CustomDelimiterExtractor.java
@@ -3,24 +3,34 @@ package calculator.delimiterExtractor;
 import calculator.constants.StringConst;
 import calculator.delimiter.Delimiter;
 import calculator.dto.DelimiterDto;
-import calculator.utils.ExceptionUtils;
+import calculator.validator.Validator;
 import java.util.regex.Pattern;
 
 public class CustomDelimiterExtractor implements DelimiterExtractor {
+    private Validator validator;
 
+    public CustomDelimiterExtractor(Validator validator) {
+        this.validator = validator;
+    }
 
     public DelimiterDto extractDelimiter(String input) {
 
         String[] firstSecondInput = input.split(StringConst.LINE_SEPARATOR_REGEX);
         String firstInput = firstSecondInput[0];
         String delimiterString = firstInput.substring(2);
-        isNotNumeric(delimiterString);
+        validateCustomDelimiter(delimiterString);
         delimiterString = escapeDelimiter(delimiterString);
         Delimiter delimiter = new Delimiter(delimiterString);
 
         return delimiter.toDto();
     }
 
+    private void validateCustomDelimiter(String delimiterString) {
+        validator.isDelimiterNullOrBlank(delimiterString);
+        validator.isNotNumeric(delimiterString);
+        validator.isDelimiterLengthOne(delimiterString);
+        validator.isNotCustomEqualDefault(delimiterString);
+    }
 
     private String escapeDelimiter(String escapeDelimiter) {
 
@@ -28,15 +38,5 @@ public class CustomDelimiterExtractor implements DelimiterExtractor {
 
     }
 
-    private boolean isNotNumeric(String escapeDelimiter) {
 
-        try {
-            Long.parseLong(escapeDelimiter);
-            ExceptionUtils.throwDelimiterCanNotBeNumberException();
-        } catch (NumberFormatException e) {
-
-        }
-        return true;
-
-    }
 }
