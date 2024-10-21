104f9683d93821472cafaca4c849fd9f81acee87
caboodles <sanghyup2000@gmail.com>
2024-10-21 21:18:54 +0900

    feat(calculator): update Application class to fit output requirements

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,12 +3,21 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         double result = StringAddCalculator.calculate(input);
-        System.out.println(result);
-        
+        if (isInteger(result)) {
+            System.out.printf("결과 : %.0f",result);
+        } else {
+            System.out.println("결과 : " + result);
+        }
     }
+    
+    private static boolean isInteger(double num) {
+        return num % 1 == 0.0;
+    }
+
 }
