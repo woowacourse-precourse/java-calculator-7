c4782b0ae2d74e622bfbcea225879c8e0f2723ce
mikekks <thdalsrb79@naver.com>
2024-10-21 11:53:25 +0900

    feat(Output): 기본 출력값 출력하는 기능 추가

--- src/main/java/calculator/Output.java
+++ src/main/java/calculator/Output.java
@@ -3,10 +3,15 @@ package calculator;
 public class Output {
 
 	private static final String RESULT_MESSAGE = "결과 : ";
+	private static final String DEFALUT_VALUE = "0";
 	private static final String INPUT_PROMPT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 
 	public static void printResult(int result){
 		System.out.println(RESULT_MESSAGE + result);
+	}
+
+	public static void printDefaultResult(){
+		System.out.println(RESULT_MESSAGE + DEFALUT_VALUE);
 	}
 
 	public static void printInputPrompt(){
