95edb0b7efb2b4bd07332639d1ce11faefe67156
20160295 <dorem96@icloud.com>
2024-10-21 14:49:35 +0900

    feat:문자열 분해(커스텀 구분자 구분)
    DelimiterExtractor
    -문자열에서 커스텀 구분자를 구분
    -커스텀 구분자 설정 : 시작("//"), 끝("\n")

--- src/main/java/calculator/model/DelimiterExtractor.java
+++ src/main/java/calculator/model/DelimiterExtractor.java
@@ -2,10 +2,19 @@ package calculator.model;
 
 public class DelimiterExtractor {
     public static final String DEFAULT_DELIMITER = ",|:";
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String NEWLINE = "\\n";
 
     public static DelimiterExtractionResult extractDelimiterAndNumbers(String input) {
         String delimiter = DEFAULT_DELIMITER;
         String numbers = input;
+
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            int delimiterIndex = input.indexOf(NEWLINE);
+            String customDelimiter = input.substring(2, delimiterIndex);
+            delimiter = DEFAULT_DELIMITER + "|" + customDelimiter;
+            numbers = input.substring(delimiterIndex + NEWLINE.length());
+        }
 
         return new DelimiterExtractionResult(delimiter, numbers);
     }
