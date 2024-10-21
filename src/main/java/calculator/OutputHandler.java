eda912bf24139b238dc45f020545fb8ede338e9c
halfmoon-mind <5338095@gmail.com>
2024-10-21 23:10:41 +0900

    feat: 출력 부분 구현

--- src/main/java/calculator/OutputHandler.java
+++ src/main/java/calculator/OutputHandler.java
@@ -1,2 +1,11 @@
-package calculator;public class OutputHandler {
+package calculator;
+
+public class OutputHandler {
+    public static void printInputRequest() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public static void printResult(int value) {
+        System.out.println(String.format("결과 : %d", value));
+    }
 }
