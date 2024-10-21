5e768129d2cc1c9e034ddf5213a339945d027a8c
da1hwang <GDMxKyio@gmail.com>
2024-10-21 22:43:48 +0900

    refactor(calculator): 공백 문자열 처리 개선을 위해 trim() 추가
    - 빈 문자열과 공백 문자열을 동일하게 0으로 계산하도록 개선

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,8 +6,8 @@ public class Calculator {
 
     // 쉼표(,) 또는 콜론(:)을 구분자로 숫자를 추출하는 메서드
     public String[] extractNumbers(String input) {
-        // 빈 문자열을 받았다면 0이 담긴 배열을 반환
-        if (input.isEmpty()) {
+        // 빈 문자열, 공백 문자열을 받았다면 0이 담긴 배열을 반환
+        if (input.trim().isEmpty()) {
             return new String[]{"0"};
         }
 
