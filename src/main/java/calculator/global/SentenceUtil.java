f22c27939fd6fb9f4c2df95e8432099b45f3919e
dk7648 <dk7648@korea.ac.kr>
2024-10-21 21:39:42 +0900

    feat: 예외처리 범위 추가

--- src/main/java/calculator/global/SentenceUtil.java
+++ src/main/java/calculator/global/SentenceUtil.java
@@ -1,19 +1,32 @@
 package calculator.global;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SentenceUtil {
     public static String extractSentence(String input) {
+        String result = input;
         if (input.contains("\\n")) {
-            return input.substring(input.indexOf("\\n") + 2);
+            result = input.substring(input.indexOf("\\n") + 2);
         }
-        return input;
+        validSentence(result);
+        return result;
     }
 
+    public static void validSentence(String input) {
+        List<String> numbers = new ArrayList<>();
+        int firstIndex = 0;
+        int lastIndex = input.length()-1;
+        String firstValue = String.valueOf(input.charAt(firstIndex));
+        String lastValue = String.valueOf(input.charAt(lastIndex));
+        numbers.add(firstValue);
+        numbers.add(lastValue);
+        validNumbers(numbers);
+    }
     public static void validNumbers(List<String> numbers) {
         for (String number : numbers) {
             if (!number.matches("[0-9]+")) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("잘못된 입력 값 입니다.");
             }
         }
     }
