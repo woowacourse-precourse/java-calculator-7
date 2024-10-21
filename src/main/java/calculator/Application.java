c706a5461708c508bded61bd0523d8f42f202a5c
YooSejeong <nived3@naver.com>
2024-10-21 20:02:37 +0900

    fix(Application, InputValidator): 음수 검사 추가, try-catch문 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,21 +5,23 @@ import validator.InputValidator;
 
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+
         // 사용자에게 문자열 입력 받기
         System.out.println("덧셈할 문자열을 입력해 주세요. ");
         String input = Console.readLine();
-        try {
-            // 올바른 값인지 입력값 검증
-            InputValidator.validateInput(input);
-            // 덧셈 수행
-            int result = SumCalculator.add(input);
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 입력입니다: " + e.getMessage());
-        }
+
+        // 올바른 값인지 입력값 검증
+        InputValidator.validateInput(input);
+
+        // 덧셈 수행
+        int result = SumCalculator.add(input);
+        System.out.println("결과 : " + result);
 
 
     }
+
+
+
 }
 
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -19,13 +19,16 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+
 
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
 }
+
