11bc4715b296f219669b795a4a9c3b6a9d5b9ef6
spiky-haired-Pyramid-head <qns0147@gmail.com>
2024-10-21 15:24:50 +0900

    feat: 구분자로 숫자를 분리 및 커스텀 구분자를 추출하는 기능 구현

---
+++ src/main/java/calculator/service/PlusCalculator.java
@@ -0,0 +1,61 @@
+package calculator.service;
+
+import calculator.validation.CalculatorValidation;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PlusCalculator implements CalculatorService {
+
+    private static CalculatorService instance;
+
+    private PlusCalculator() {
+    }
+
+    public static CalculatorService getInstance() {
+        if (instance == null) {
+            instance = new PlusCalculator();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public int operation(List<String> numList) {
+        return 0;
+    }
+
+    @Override
+    public List<String> splitDigits(String numStr) {
+        String[] arrs = numStr.split("\\\\n");
+
+        String customArr = "";
+        String digitsArr = "";
+
+        if (arrs.length == 1) digitsArr = numStr;
+        if (arrs.length == 2) {
+            customArr = arrs[0];
+            digitsArr = arrs[1];
+        }
+
+        return Arrays.stream(digitsArr.split(findCustomRex(customArr)))
+                .filter(nums -> !nums.isEmpty())
+                .toList();
+    }
+
+    public String findCustomRex(String customArr) {
+        String rex = "[,:";
+        String customRex = "";
+
+        if (customArr.startsWith("//") && customArr.length() > 2) {
+            customRex = customArr.substring(2);
+        }
+
+        if (!customRex.isEmpty()) rex += customRex;
+
+        rex += "]";
+
+        return rex;
+    }
+
+}
