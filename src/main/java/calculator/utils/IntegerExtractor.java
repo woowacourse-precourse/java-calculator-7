578a9ab53cd43fb8f39ff56f5945c81eebd14feb
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-21 18:43:53 +0900

    [chore]<extractIntegerFromExpression> : 소수 점 비정상 입력 시 예외처리
    1.2.3과 같은 수식이 입력될 시 예외처리

--- src/main/java/calculator/utils/IntegerExtractor.java
+++ src/main/java/calculator/utils/IntegerExtractor.java
@@ -1,23 +1,28 @@
 package calculator.utils;
 
+import calculator.exception.CalculatorException;
+import calculator.exception.CalculatorExceptionStatus;
 import calculator.model.CalculatorValues;
 import java.util.regex.Pattern;
 
 public class IntegerExtractor {
 
     public static void extractIntegerFromExpression(CalculatorValues calculatorValues) {
-        String[] tokens;
 
-        if(calculatorValues.getDelimiter().equals("[,:]")){
-            tokens = calculatorValues.getExpression()
-                    .split(calculatorValues.getDelimiter());
+        String delimiter = Pattern.quote(calculatorValues.getDelimiter());
+
+        if (calculatorValues.getDelimiter().equals("[,:]")) {
+            delimiter = calculatorValues.getDelimiter();  // 정규식 구분자는 그대로 사용
         }
-        else{
-            tokens = calculatorValues.getExpression()
-                    .split(Pattern.quote(calculatorValues.getDelimiter()));
-        }
-        for(String token : tokens){
-            calculatorValues.addNumbersFromInputByDelimiter(token);
+
+        String[] tokens = calculatorValues.getExpression().split(delimiter);
+
+        for (String token : tokens) {
+            try{
+                calculatorValues.addNumbersFromInputByDelimiter(token);
+            }catch (IllegalArgumentException e){
+                throw new CalculatorException(CalculatorExceptionStatus.INVALID_DEFAULT_DELIMITER_EXPRESSION);
+            }
         }
     }
 }
