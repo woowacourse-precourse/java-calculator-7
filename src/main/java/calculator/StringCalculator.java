e4690c3fb6d5079a9b68faafd06cff4f399f2488
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-10-21 23:45:49 +0900

    feat: Add get separator and numbers from input in StringCalculator.java

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class StringCalculator {
     private static final String DEFAULT_DELIMITERS = ",|:";
     private static final String REGEX = "//(.)\\s*(.*)";
@@ -16,6 +19,20 @@ class StringCalculator {
             return 0;
         }
 
+        // 4. 구분자 확인 및 설정
+        // 기본 구분자로 구분자 설정
+        String delimiters = DEFAULT_DELIMITERS;
+        // 숫자 부분 설정
+        String numbers = input;
+
+        // 커스텀 구분자 존재 여부 확인 및 추출
+        if (input.startsWith("//")) {
+            // 커스텀 구분자 추출
+            delimiters = extractCustomDelimiter(input);
+            // 커스텀 구분자 이후의 숫자 부분 추출
+            numbers = extractNumbersAfterDelimiter(input);
+        }
+
         return 0;
     }
 
@@ -27,6 +44,46 @@ class StringCalculator {
      */
     private static boolean isEmpty(String input) {
         return input == null || input.trim().isEmpty();
+    }
+
+    /**
+     * 문자열에서 커스텀 구분자를 추출
+     *
+     * @param input 입력 문자열
+     * @return 커스텀 구분자
+     */
+    private static String extractCustomDelimiter(String input) {
+        // // 와 \n 사이의 임의의 한 문자 추출
+        Matcher matcher = Pattern.compile(REGEX).matcher(input);
+
+        if (matcher.find()) {
+            // 찾은 첫번째 구분자와 기본 구분자 함께 반환
+            String customDelimiter = Pattern.quote(matcher.group(1));
+            return customDelimiter + "|" + DEFAULT_DELIMITERS;
+        }
+
+        // 그 외의 경우 커스텀 구분자 형식이 잘못됨을 알림(ex //;'\n )
+        throw new IllegalArgumentException("커스텀 구분자 형식이 잘못되었습니다.");
+    }
+
+    /**
+     * 문자열에서 커스텀 구분자 이후의 숫자 부분을 추출
+     *
+     * @param input 입력 문자열
+     * @return 숫자 부분 문자열
+     */
+    private static String extractNumbersAfterDelimiter(String input) {
+        // //{임의의 한 문자}\n 이후의 모든 문자열 추출
+        Matcher matcher = Pattern.compile(REGEX).matcher(input);
+        if (matcher.find()) {
+            // 찾은 첫번째 문자열 줄바꿈 제외하고 반환
+            String numbers = matcher.group(2);
+            numbers = numbers.replace("\\n", "");
+            return numbers;
+        }
+
+        // 그 외의 경우 커스텀 구분자 이후에 숫자가 올바르게 입력되지 않음을 알림
+        throw new IllegalArgumentException("커스텀 구분자 이후에 숫자가 올바르게 입력되지 않았습니다.");
     }
 
 }
