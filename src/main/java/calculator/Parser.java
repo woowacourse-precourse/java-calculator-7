f690e0457efb5732554620cfaab1c5c80eb6fdac
JooYoung <beachu23@naver.com>
2024-10-21 20:48:41 +0900

    chore: 예외 메시지 오타 수정
    "잘못 된 입력입니다."로 작성한 예외 메시지를 "잘못된 입력입니다."로 수정한다.
    
    Closes

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -59,7 +59,7 @@ public class Parser {
             if (number.isEmpty()) {
                 continue;
             } else if (!operands.validate(number)) {
-                throw new IllegalArgumentException("잘못 된 입력입니다.");
+                throw new IllegalArgumentException("잘못된 입력입니다.");
             } else {
                 operands.addOperand(number);
             }
