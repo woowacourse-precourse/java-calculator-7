a7e6488ff5aabb61e5aaa3c6abdbed8d5644ba73
kimshinwoo <tlsdn67@naver.com>
2024-10-21 14:52:14 +0900

    feat: 추가 구분자 추가 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,23 +2,43 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int[] result2 = new int[result.length];
-        int dap = 0;
-
+//        String newSplit = "";
+        String splits = ":,";
+        String patterString = "(?<=//).*"; //사이만 추출가능
+        String patterString1 = "//.*?\\n"; //새 구분자 부분 지우기위한 키워드 추출
         String input = Console.readLine();
-        String[] result = input.split("[:,]");
+        if (input.startsWith("//")){
+            Pattern pattern = Pattern.compile(patterString);
+            Matcher matcher = pattern.matcher(input);
+            if(matcher.find()){
+                String newSplit = matcher.group();
+                splits+= newSplit;
+            }
+            else{
+                System.out.println("there is no matched String");
+            }
+            input = Console.readLine(); //새 input받기
+        }
+        String splitFormmat = String.format("[%s]",splits); //정규식으로 전환
+        String[] strArr = input.split(splitFormmat);
 
-        for(int i=0;i< result.length;i++){
-            result2[i] = Integer.parseInt(result[i]);
+        int[] intArr = new int[strArr.length];
+        int sum = 0;
+        for (int i = 0; i < strArr.length; i++) {
+            intArr[i] = Integer.parseInt(strArr[i]);
         }
 
-        for (int j : result2) {
-            dap += j;
+        for (int j : intArr) {
+            sum += j;
         }
-        System.out.println(dap);
+        System.out.println(sum);
     }
 }
