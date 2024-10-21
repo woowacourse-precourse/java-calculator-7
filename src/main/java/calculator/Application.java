264c6c6f5d3b3674405418d1473a8225ab24a612
PSangEon <qkr99102@naver.com>
2024-10-21 21:14:14 +0900

    Feat: feat sum
    덧셈 기능 구현
    
    #3

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,31 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] numbers = input.split(",|:");
+        int result = sumNumbers(numbers);
+
+        System.out.println("결과 : " + result);
+    }
+
+    private static int sumNumbers(String[] numbers) {
+        int result = 0;
+        for (String num : numbers) {
+            int number = Integer.parseInt(num.trim());
+            if (number < 0) {
+                throw new IllegalArgumentException("잘못된 값입니다: " + number);
+            }
+            result += number;
+        }
+        return result;
     }
 }
+
+
+
