6bf895695a29a499de47a6b624c2aab0c7acecaf
awaykim <wonj010101@gmail.com>
2024-10-21 21:40:55 +0900

    chore: 각주 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,11 +6,6 @@ import calculator.model.Calculator;
 import calculator.model.Validator;
 import calculator.view.ConsoleView;
 
-/*
--fix 사항: 오류 사유를 명확히 출력할 수 있도록 Validation 수정하기
--test: customTest로 edge case 확인하기 
--refac
- */
 public class Application {
     public static void main(String[] args) {
         ConsoleView view = new ConsoleView();
