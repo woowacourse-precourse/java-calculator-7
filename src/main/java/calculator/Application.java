0b45111248d253a8694987f255221142e2976ff8
young219257 <young219257@gmail.com>
2024-10-21 22:13:33 +0900

    feat : 메인 메서드 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine(); // 사용자 입력 받기
+
+            int result = add(input); // 결과 계산
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
 
     }
 
