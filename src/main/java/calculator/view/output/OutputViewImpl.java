b309e5bd579587fc64ff6ec651b377cba045dfe0
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-21 14:34:05 +0900

    feat: [기능 구현 2번] OutputViewImpl 결과 출력 메서드 구현

--- src/main/java/calculator/view/output/OutputViewImpl.java
+++ src/main/java/calculator/view/output/OutputViewImpl.java
@@ -1,4 +1,13 @@
 package calculator.view.output;
 
+import calculator.model.CalculationResult;
+
 public class OutputViewImpl implements OutputView {
+
+  private static final String RESULT_PREFIX = "결과 : ";
+
+  @Override
+  public void printCalculateResult(CalculationResult calculationResult) {
+    System.out.println(RESULT_PREFIX + calculationResult.getCalculatedNumber());
+  }
 }
