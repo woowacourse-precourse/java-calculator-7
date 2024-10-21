29d7ab0de7b62fda2511f17c0ac2b5736f0c038a
bokyeong <wntjdals0412@gmail.com>
2024-10-21 21:08:13 +0900

    test : 테스트 코드

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,9 +19,49 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 한글자_이상의_커스텀_구분자_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//l;\n1;3;2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자_커스텀_구분자_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//7\n2723"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_예외처리() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\n\n27\n23"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_기본_구분자_모두_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;5;2,1:4");
+            assertThat(output()).contains("결과 : 13");
+        });
+    }
+
+    @Test
+    void 구분자_대시_사용() {
+        assertSimpleTest(() -> {
+            run("//-\\n2-7-2-3");
+            assertThat(output()).contains("결과 : 14");
+        });
     }
 
     @Override
