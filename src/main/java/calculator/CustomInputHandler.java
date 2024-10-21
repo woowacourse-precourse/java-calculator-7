875862a74c4d8c1cdb5aa0b2e1ccda44210bc335
halfmoon-mind <5338095@gmail.com>
2024-10-21 23:10:28 +0900

    feat: custom input 관리 부분 추가

--- src/main/java/calculator/CustomInputHandler.java
+++ src/main/java/calculator/CustomInputHandler.java
@@ -1,2 +1,37 @@
-package calculator;public class CustomInputHandler {
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomInputHandler {
+    static List<Character> evaluateCustomInput(String input) {
+        String regex = "^//(.*?)\\\\n";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            String capturedContent = matcher.group(1).trim();
+            List<Character> charList = new ArrayList<>();
+            for (char ch : capturedContent.toCharArray()) {
+                if (!charList.contains(ch)) { // 중복된 구분자를 방지
+                    charList.add(ch);
+                }
+            }
+            return charList;
+        }
+        return new ArrayList<>();
+
+    }
+
+    static String removeCustomInput(String input) {
+        String regex = "^//.*?\\\\n";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            return matcher.replaceFirst("");
+        }
+        return input;
+    }
 }
