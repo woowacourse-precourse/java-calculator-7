bf0942097efd1e8dbefcc5c8ff242ac5b5c75ef6
sall <keinpark1@naver.com>
2024-10-21 17:11:51 +0900

    feat(calculator) : 입력한 문자열에서 숫자를 추출하여 더하는 계산기를 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,50 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String delimiter = ",|:";
+        String numbers = input;
+
+        // 커스텀 구분자 처리
+        if (input.startsWith("//")) {
+            input = input.replace("\\n", "\n");
+            String[] splitStr = input.split("\n",2);
+            delimiter = splitStr[0].substring(2); // 커스텀 구분자 추출
+            numbers = splitStr[1];
+        }
+
+        // 숫자를 구분자를 기준으로 분리
+        String[] tokens = numbers.split(delimiter);
+
+        int sum = 0;
+        for (String token : tokens) {
+            int number = Integer.parseInt(token);
+            if (number < 0) {
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+            }
+            sum += number;
+        }
+
+        return sum;
+    }
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        try {
+            int result = add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            throw e;
+        }
     }
 }
