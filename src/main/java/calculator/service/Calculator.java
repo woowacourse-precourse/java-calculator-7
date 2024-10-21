35d55a7bdb00e8531ae0ae411592fe0eb30cd2a9
gracepec <gracepec0621@gmail.com>
2024-10-21 20:38:25 +0900

    fix(Calculator): 입력한 각 수와 합의 범위 int로 제한
    - README 파일에 입력 제한 요구사항 추가
    - Calculator 해당 fix 사항 변경 및 추가

--- src/main/java/calculator/service/Calculator.java
+++ src/main/java/calculator/service/Calculator.java
@@ -2,24 +2,35 @@ package calculator.service;
 
 public class Calculator {
     public int calculate(String input) {
-        int sum = 0;
+        long sum = 0;
 
         if (input.contains(",")) {
             String[] nums_str = input.split(",");
 
             for (String s : nums_str) {
+                if (s.length() > 10 || (s.length() == 10 && s.compareTo("2147483647") > 0)) {
+                    throw new IllegalArgumentException("입력 값이 int 범위를 초과합니다.");
+                }
+
                 try {
                     sum = sum + Integer.parseInt(s);
                 } catch (NumberFormatException e) {
                     throw new IllegalArgumentException("구분자 사이 입력이 숫자가 아닙니다.");
                 }
+
+                if (sum > Integer.MAX_VALUE) {
+                    throw new IllegalArgumentException("입력 값의 합이 int 범위를 초과합니다.");
+                }
             }
         }
 
         if (!input.contains(",")) {
-            sum = Integer.parseInt(input);
+            sum = Long.parseLong(input);
+            if (sum > Integer.MAX_VALUE) {
+                throw new IllegalArgumentException("입력 값의 합이 int 범위를 초과합니다.");
+            }
         }
 
-        return sum;
+        return (int) sum;
     }
 }
