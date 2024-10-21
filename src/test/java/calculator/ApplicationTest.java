23aa4e1eba8afa33128efac406607979c084f847
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-10-21 21:48:52 +0900

    TEST(ApplicationTest): 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,19 +8,60 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
-    void 커스텀_구분자_사용() {
+    void 커스텀_구분자_없이_계산() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+    
+    @Test
+    void 커스텀_구분자_1개_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2");
+            assertThat(output()).contains("결과 : 3");
         });
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_구분자_복수개_사용() {
+        assertSimpleTest(() -> {
+            run("//#$%\\n1#2$3%4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_입력을_종료하지_않는_경우() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("//*1,2:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자에_해당하지_않는_문자가_문자열에_포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1*2:3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 문자열에_숫자가_포함X() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("::"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 문자열에_포함된_숫자가_연산하기에_너무_큰_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,99999999999999999999"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
