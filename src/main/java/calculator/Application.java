b711b0dfedd3572100c6a3062afc9c76e51eb5c9
minjeeki <minjeeki@student.42seoul.kr>
2024-10-21 23:11:50 +0900

    feat(app) : set up project structure and docs(readme) : update README.md

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 기능 0. 입력 받기
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
     }
 }
+
