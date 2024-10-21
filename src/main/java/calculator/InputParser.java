7981ac78ed6cd966cc625fdbbd98eb3a06071fab
heejjinkim <hjhj12@dgu.ac.kr>
2024-10-21 23:52:22 +0900

    refactor: 상수 정의
    하드 문자열보다는 이해하기 쉽게 상수로 정의

--- src/main/java/calculator/InputParser.java
+++ src/main/java/calculator/InputParser.java
@@ -2,11 +2,17 @@ package calculator;
 
 public class InputParser {
 
+    private static final String CUSTOM_DEF_START = "//";
+    private static final String CUSTOM_DEF_END = "\\n";
+    private static final String ESCAPE_STRING = "\\";
+    private static final String DEFAULT_DELIMITERS = ":,";
+    private static final String SPECIAL_CHARS_IN_REG = "|*+()[]{}.^$\\&";
+
     private String delimiters;
     private String numbersSection;
 
     public InputParser(String input) {
-        delimiters = ":,";
+        delimiters = DEFAULT_DELIMITERS;
         numbersSection = input;
         parseDelimiterAndNumbersSection(input);
     }
@@ -32,12 +38,13 @@ public class InputParser {
     }
 
     private boolean hasCustomDelimiter(String input) {
-        return input.startsWith("//") && (input.indexOf("\\n") == input.indexOf("//") + 3);
+        return input.startsWith(CUSTOM_DEF_START) && (input.indexOf(CUSTOM_DEF_END)
+                == input.indexOf(CUSTOM_DEF_START) + 3);
     }
 
     private String extractCustomDelimiter(String input) {
-        int startIndex = input.indexOf("//") + 2;
-        int endIndex = input.indexOf("\\n");
+        int startIndex = input.indexOf(CUSTOM_DEF_START) + 2;
+        int endIndex = input.indexOf(CUSTOM_DEF_END);
 
         String delimiter = input.substring(startIndex, endIndex);
 
@@ -45,15 +52,15 @@ public class InputParser {
     }
 
     private String extractNumbersSection(String input) {
-        return input.substring(input.indexOf("\\n") + 2);
+        return input.substring(input.indexOf(CUSTOM_DEF_END) + 2);
     }
 
     private String escapeSpecialCharacters(String delimiter) {
         StringBuilder escapedDelimiter = new StringBuilder();
 
         for (char ch : delimiter.toCharArray()) {
-            if ("|*+()[]{}.^$\\&".indexOf(ch) != -1) {
-                escapedDelimiter.append("\\"); // 이스케이프 문자 추가
+            if (SPECIAL_CHARS_IN_REG.indexOf(ch) != -1) {
+                escapedDelimiter.append(ESCAPE_STRING); // 이스케이프 문자 추가
             }
             escapedDelimiter.append(ch);
         }
