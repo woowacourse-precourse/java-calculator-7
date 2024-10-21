d166227bd736397378f367879f593b0a5ba66219
davemins <davemins4125@naver.com>
2024-10-21 10:15:07 +0900

    feat(Controller): 숫자를 추출하는 함수 호출

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,19 +1,23 @@
 package calculator.controller;
 
 import calculator.domain.DelimiterExtractor;
+import calculator.domain.NumberParser;
 import calculator.view.View;
 
 public class CalculatorController {
     private final DelimiterExtractor delimiterExtractor;
+    private final NumberParser numberParser;
 
 
     public CalculatorController() {
         this.delimiterExtractor = new DelimiterExtractor();
+        this.numberParser = new NumberParser();
     }
 
     public void run() {
         String input = View.requestString();
 
         String delimiters = delimiterExtractor.extract(input);
+        String[] numbers = numberParser.parse(input, delimiters);
     }
 }
