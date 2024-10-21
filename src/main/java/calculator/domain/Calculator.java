5afd9cb8879f268a3dd39b92abd78105a5f0967f
yunho_yun <yunho_yun@naver.com>
2024-10-21 22:30:50 +0900

    feat: 문자열 파싱에 따른 덧셈 연산 구현

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -7,15 +7,29 @@ import static calculator.util.InputValidator.validate;
 
 public class Calculator {
     private final String input;
+    private final InputParser inputParser;
 
-    public Calculator(String input) {
+    public Calculator(String input, InputParser inputParser) {
         validate(input);
         this.input = input;
+        this.inputParser = inputParser;
     }
 
-    public int add(ArrayList<Integer> numbers) {
+    public int add(String input) {
+        return sum(inputParser.parse(input));
+    }
+
+    public int sum(String[] numbers) {
         int result = 0;
-        for (int n : numbers) {
+        for (String number : numbers) {
+            int n;
+            try {
+                n = Integer.parseInt(number);
+                if (n < 0) throw new IllegalArgumentException("입력은 음수를 포함할 수 없습니다.");
+
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("입력이 잘못되었습니다.");
+            }
             result += n;
         }
         return result;
