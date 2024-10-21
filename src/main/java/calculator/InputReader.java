4b74f639460144f8d0699fbde68e201d1483ac8e
KIM MINSEOK <mskim.glenn@gmail.com>
2024-10-21 14:25:35 +0900

    feat(InputReader): Scanner 대신 Console로 교체
    입력 처리 방식으로 Scanner 대신 camp.nextstep.edu.missionutils.Console을 사용하여 사용자 입력을 받음.

--- src/main/java/calculator/InputReader.java
+++ src/main/java/calculator/InputReader.java
@@ -1,16 +1,14 @@
 package calculator;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputReader {
 
     private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 
     public String promptForInput() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println(INPUT_MESSAGE);
-            // 모든 공백을 빈 문자열로 대체하여 제거
-            return scanner.nextLine().replaceAll("\\s+", "");
-        }
+        System.out.println(INPUT_MESSAGE);
+        // 모든 공백을 빈 문자열로 대체하여 제거
+        return Console.readLine().replaceAll("\\s+", "");
     }
 }
