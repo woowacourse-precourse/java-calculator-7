833774d4d40ac2da58197b092c410ee964965b8e
dayaelee <dayae.dev@gmail.com>
2024-10-21 11:28:45 +0900

    feat: 사용자에게서 입력값 받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 1. 사용자에게서 입력값 받기
+        // Scanner 클래스의 인스턴스 생성하기
+        Scanner scanner = new Scanner(System.in);
+        // nextLine() 메서드를 통해 입력 값 변수에 저장하기
+        String inputValue = scanner.nextLine();
+
     }
 }
