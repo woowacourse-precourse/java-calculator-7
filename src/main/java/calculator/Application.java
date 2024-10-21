ad350f2b83e9a14cf23bfca484a6376ce20628c8
sete3683 <sete3683@gmail.com>
2024-10-21 22:22:24 +0900

    feat(구분자) : 커스텀 구분자 식별 기능 추가
    커스텀 구분자의 도출 및 잘못된 형식의 예외 처리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,46 @@
 package calculator;
 
 public class Application {
+	private static final char DELIMITER_COMMA = ',';
+	private static final char DELIMITER_COLON = ':';
+	
+	//커스텀 구분자를 도출하는 함수
+	//커스텀 구분자가 없을 경우, 0을 반환
+	//입력 포맷이 잘못되었을 경우, 예외 처리
+	private static char getCustomDelimiter(String inputString) {
+		//1.
+		//커스텀 구분자를 가질 수 없는 경우
+		if (inputString.length() < 4)
+			return 0;
+		
+		//2.
+		//커스텀 구분자를 가지는 경우
+		char customDelimiter = inputString.charAt(2);
+		String inputFormat = inputString.substring(0, 4);
+		String correctFormat = String.format("//%c\n", customDelimiter);
+		if (inputFormat.equals(correctFormat))
+			return customDelimiter;
+
+		//3.
+		//입력이 잘못된 경우
+		char inputCharacter;
+		for (int i = 0; i < 4; i++) {
+			inputCharacter = inputFormat.charAt(i);
+			if (('0' <= inputCharacter && inputCharacter <= '9') ||
+					inputCharacter == DELIMITER_COMMA ||
+					inputCharacter == DELIMITER_COLON) {
+				continue;
+			} else {
+				throw new IllegalArgumentException();
+			}
+		}
+		
+		//4.
+		//커스텀 구분자가 없는 경우
+		return 0;
+	}
+	
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(getCustomDelimiter("//a\n001230,0456:08792"));
     }
 }
