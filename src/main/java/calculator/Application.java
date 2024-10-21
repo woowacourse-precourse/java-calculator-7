e64045847420843c36a75f9d3b2e38ebba9e5b64
706com <ehdgusjdbswl@gmail.com>
2024-10-21 23:42:37 +0900

    [Feat] 계산기 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import calculator.console.InputConsole;
+import calculator.console.OutputConsole;
+import calculator.service.CalculatorService;
+
 public class Application {
+
+    private static CalculatorService calculatorService = new CalculatorService();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = InputConsole.inputConsole();
+        System.out.println(input);
+
+        String[] numbers = calculatorService.splitInputString(input);
+
+        int result = calculatorService.calculateSum(numbers);
+
+        OutputConsole.outputConsole(result);
+
     }
 }
