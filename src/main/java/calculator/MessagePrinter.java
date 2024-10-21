4cf20e118f19de8a2f5e6f033b120358db330ff4
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-10-21 23:58:45 +0900

    done

--- src/main/java/calculator/MessagePrinter.java
+++ src/main/java/calculator/MessagePrinter.java
@@ -2,6 +2,6 @@ package calculator;
 
 public class MessagePrinter {
     public void printResult(int sum) {
-        System.out.println("결과 : " + sum); // 출력 -> 결과 : 43
+        System.out.println("결과 : " + sum);
     }
 }
