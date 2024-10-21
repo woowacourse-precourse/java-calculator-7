cb2d6044097d9393e087bc5f1464d8471a767cb9
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-10-21 21:06:42 +0900

    feat: 문자를 입력받는 기능

---
+++ src/main/java/calculator/view/Input.java
@@ -0,0 +1,12 @@
+package calculator.view;
+
+import calculator.entity.Validator;
+
+public class Input {
+    private final Validator validator;
+
+    public Input() {
+        this.validator = new Validator();
+    }
+
+}
