6c82fe8ca63d5f763cb1ea85036e7eb1e927cb08
daheePark <qkrekgml7414@gmail.com>
2024-10-21 19:28:19 +0900

    feat: 문제 설명 출력 기능 구현

--- src/main/java/calculator/view/CalculatorOutputView.java
+++ src/main/java/calculator/view/CalculatorOutputView.java
@@ -1,10 +1,12 @@
 package calculator.view;
 
+import static calculator.view.Output.INPUT_STRING;
+
 public class CalculatorOutputView implements OutputView {
 
     @Override
     public void inputString() {
-
+        System.out.println(INPUT_STRING.getMessage());
     }
 
     @Override
