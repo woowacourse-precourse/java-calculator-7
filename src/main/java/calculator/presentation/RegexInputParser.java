5b27a15954b492d0d93ea38eb068d0480780e63e
sangcci <pshsh910@gmail.com>
2024-10-21 09:28:50 +0900

    style: 주석 제거

--- src/main/java/calculator/presentation/RegexInputParser.java
+++ src/main/java/calculator/presentation/RegexInputParser.java
@@ -5,7 +5,6 @@ public class RegexInputParser implements InputParser {
     private static final String PREFIX_REGEX = "//";
     private static final String SUFFIX_REGEX = "\\\\n";
 
-    // json도 파싱하듯이, 우리만의 파싱 역할이 따로 있음
     public CalculatorRequest parseToRequest(String input) {
         String[] splitInput = input.split(SUFFIX_REGEX);
 
