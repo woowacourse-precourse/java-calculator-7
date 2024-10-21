70462d7921858004d757af859ce998477df34fed
Shrimptempura <ongtzxowns98@gmail.com>
2024-10-21 21:43:53 +0900

    feat: 사용자에게 문자열을 입력받고 결과 값을 받음

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        int result = add(input);
+        System.out.println("결과 : " + result);
     }
-}
 
     // 구분자 확인
     public static int add(String input) {
