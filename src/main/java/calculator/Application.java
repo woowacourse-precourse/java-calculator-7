a2aa16e6b78b3019f4784d4a5778a466bafdb02d
hyeblee <hyeb1224@naver.com>
2024-10-21 09:14:43 +0900

    에러 테스트

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,17 +3,22 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 
+
+
 public class Application {
+
 
 
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         StringCalculator calculator = new StringCalculator();
-        long result;
-
-        result = calculator.stringCalculate(input);
-        System.out.println("결과 : " + result);
-
+        long result = 0;
+        try{
+            result = calculator.stringCalculate(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("error: "+e.getMessage());
+        }
     }
 }
