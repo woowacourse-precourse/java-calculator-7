b34334908394b5ceb7f2bb972179d7c7a8478648
GunsanHaribo <hth99131@gmail.com>
2024-10-21 16:36:37 +0900

    refactor: 상수 접근제어자 default에서 public으로 전환입니다
    상수이기때문에 다른 클래스에서도 사용할 수 있을 수 있다고 생각해 default에서 public으로 수정했습니다. 대상은 BasicDelimiter, ErrorMessage 입니다.

--- src/main/java/calculator/BasicDelimiter.java
+++ src/main/java/calculator/BasicDelimiter.java
@@ -1,9 +1,9 @@
 package calculator;
 
 public class BasicDelimiter {
-    static final String COMMA = ",";
-    static final String SEMICOLON = ";";
-    static final String COMMA_OR_SEMICOLON = COMMA + "|" + SEMICOLON;
+    public static final String COMMA = ",";
+    public static final String SEMICOLON = ";";
+    public static final String COMMA_OR_SEMICOLON = COMMA + "|" + SEMICOLON;
     private BasicDelimiter() {
     }
 }
--- src/main/java/calculator/ErrorMessages.java
+++ src/main/java/calculator/ErrorMessages.java
@@ -1,16 +1,15 @@
 package calculator;
 
 public class ErrorMessages {
-    static final String ERROR_NO_EXPRESSION_CUSTOM_DELIMITER = "커스텀 구분자 이후로 수식이 없습니다";
-    static final String ERROR_CUSTOM_DELIMITER_OVER_TWO = "커스텀 구분자가 2개이상 있습니다";
-    static final String ERROR_CUSTOM_DELIMITER_NOT_START_WITH_FORMAT = "커스텀 구분자의 형식인 //로 시작하지 않습니다";
-    static final String ERROR_CUSTOM_DELIMITER_CONTAIN_NUMBER = "구분자에 숫자가 들어있습니다";
-    static final String ERROR_NOT_CONTAIN_DELIMITER = "구분자가 없습니다";
-    static final String ERROR_BASIC_DELIMITER_ONLY_DELIMITER_SUPPORT = " 밖에 없습니다";
-    static final String ERROR_OPERANDS_CONTAIN_OTHER_CHAR = "피연산자에 숫자가 아닌 다른 문자가 있습니다";
-    static final String ERROR_OPERANDS_CONTAIN_ZERO = "피연산자들 중 0이 있습니다";
-    static final String ERROR_OPERANDS_CONTAIN_MINUS = "피연산자가 중 음수가 있습니다";
-
+    public static final String ERROR_NO_EXPRESSION_CUSTOM_DELIMITER = "커스텀 구분자 이후로 수식이 없습니다";
+    public static final String ERROR_CUSTOM_DELIMITER_OVER_TWO = "커스텀 구분자가 2개이상 있습니다";
+    public static final String ERROR_CUSTOM_DELIMITER_NOT_START_WITH_FORMAT = "커스텀 구분자의 형식인 //로 시작하지 않습니다";
+    public static final String ERROR_CUSTOM_DELIMITER_CONTAIN_NUMBER = "구분자에 숫자가 들어있습니다";
+    public static final String ERROR_NOT_CONTAIN_DELIMITER = "구분자가 없습니다";
+    public static final String ERROR_BASIC_DELIMITER_ONLY_DELIMITER_SUPPORT = " 밖에 없습니다";
+    public static final String ERROR_OPERANDS_CONTAIN_OTHER_CHAR = "피연산자에 숫자가 아닌 다른 문자가 있습니다";
+    public static final String ERROR_OPERANDS_CONTAIN_ZERO = "피연산자들 중 0이 있습니다";
+    public static final String ERROR_OPERANDS_CONTAIN_MINUS = "피연산자가 중 음수가 있습니다";
     private ErrorMessages() {
     }
 }
