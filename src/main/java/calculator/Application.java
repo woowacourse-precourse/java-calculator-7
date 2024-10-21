64c2b8203acab7923b47c7da4391a92a7b7fa9e3
woong2e <tlswodnd12345@naver.com>
2024-10-21 22:13:22 +0900

    feat: 계산기 프로그램 시작 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import calculator.input.InputReader;
+import calculator.output.OutputWriter;
+import calculator.stringcalculator.StringCalculator;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+        InputReader inputReader = new InputReader();
+        OutputWriter outputWriter = new OutputWriter();
+
+        StringCalculator stringCalculator = appConfig.stringCalculator();
+        String input = inputReader.readLine();
+        String result = stringCalculator.operate(input);
+        outputWriter.writer(result);
+
+        inputReader.close();
     }
 }
