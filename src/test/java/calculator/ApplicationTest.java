413904cd374d2578aa1508d92130fdbe51f48750
이보연 <btothey99@ewhain.net>
2024-10-21 23:19:26 +0900

    test: 빈 문자열 처리 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -7,6 +7,8 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.io.ByteArrayInputStream;
+
 class ApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_사용() {
@@ -24,8 +26,17 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 빈_문자열_처리() {
+        assertSimpleTest(() -> {
+            run("");  // NsTest의 run 메서드 사용
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
+
 }
