b59650831075f7d6b4d7a42de671a2d4b26bd105
yongsik <wkdrndydtlr@naver.com>
2024-10-21 16:20:19 +0900

    fix: exception

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,25 +10,21 @@ public class Application {
         String splitString = ",|:";
 
         if(input == null || input.isEmpty()){
-            System.out.println("결과: "+0);
+            System.out.println("결과 : "+0);
             return;
         }
         int sum=0;
-
-        try{
-            if(isCustom(input)){
-                splitString += getCustom(input);
-            }
-            String[] tmp = input.split("\\\\n");
-            input = tmp[tmp.length-1];
-
-
-            String[] numArr = input.split(splitString);
-            sum = calSum(numArr);
-        }catch(IllegalArgumentException e){
-            return ;
+        if(isCustom(input)){
+            splitString += getCustom(input);
         }
-        System.out.println("결과: "+sum);
+        String[] tmp = input.split("\\\\n");
+        input = tmp[tmp.length-1];
+
+
+        String[] numArr = input.split(splitString);
+        sum = calSum(numArr);
+
+        System.out.println("결과 : "+sum);
 
     }
 
@@ -37,7 +33,6 @@ public class Application {
             for (String s : numArr) {
                 if (!s.matches("[+-]?\\d*(\\.\\d+)?")) throw new IllegalArgumentException();
                 if(Integer.parseInt(s)<=0){
-                    System.out.println("minus");
                     throw new IllegalArgumentException();
                 }
                 sum += Integer.parseInt(s);
