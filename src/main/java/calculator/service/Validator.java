07b26f968f508911528a3ce2c77b555d8d56f41e
iceprins <junghankyul@gmail.com>
2024-10-21 22:09:19 +0900

    feat: Validator, ManageDelimiter 구현 입력받은 문자열 검증하고 구분자 리스트에 추가하는 기능 구현

--- src/main/java/calculator/service/Validator.java
+++ src/main/java/calculator/service/Validator.java
@@ -1,56 +1,60 @@
 package calculator.service;
 
+import calculator.service.util.ManageDelimiters;
 import calculator.view.Constant;
 
+import java.util.List;
+
 public class Validator {
+    static ManageDelimiters manageDelimiters = new ManageDelimiters();
 
-    private static boolean isCustomDelimiter(String s) {
-        if (s.startsWith("//")) {
-            return s.charAt(3) == '\n';
-        }
-        return false;
+    private static boolean hasCustomDelimiter(String s) {
+        return s.startsWith("//") && (s.contains("\\n"));
     }
 
-    private static boolean isRightDelimiter(String s) {
-        String[] numbers = s.split("[:|,]");
-        for (String number : numbers) {
-            if (!number.matches("^[0-9]*")) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static boolean isIncludeZero(String s) {
-        String[] numbers = s.split("[:|,]");
-        for (String number : numbers) {
-            if (Integer.parseInt(number) <= 0) {
-                return false;
-            }
-        }
-        return true;
+    public static void validateCustomDelimiter(String s) {
+        if (s.indexOf("\\n") == 2)
+            throw new IllegalArgumentException(Constant.NOT_ENTERED_DELIMITER);
+        if (s.indexOf("\\n") > 3)
+            throw new IllegalArgumentException(Constant.NOT_ONE_DELIMITER);
+        if (s.indexOf("\\n") == 3)
+            manageDelimiters.addDelimiter(getCustomDelimiter(s));
     }
 
     private static String getCustomDelimiter(String s) {
         return String.valueOf(s.charAt(2));
     }
 
-    private static void isRightInput(String s) {
-        if (!isCustomDelimiter(s)) {
-            if (!isRightDelimiter(s)) {
+    private static void validateDelimiter(String s) {
+        String[] numbers = s.split(manageDelimiters.getDelimiters());
+        for (String number : numbers) {
+            if (!number.matches("^[0-9]*"))
                 throw new IllegalArgumentException(Constant.NOT_ALLOWED_DELIMITER);
-            }
-            if (isIncludeZero(s)) {
+        }
+    }
+
+    private static void validateNumbers(String s) {
+        String[] numbers = s.split(manageDelimiters.getDelimiters());
+        for (String number : numbers) {
+            if (Integer.parseInt(number) <= 0)
                 throw new IllegalArgumentException(Constant.INCLUDE_ZERO);
-            }
         }
     }
 
     public void validateInput(String s) {
-        try {
-            isRightInput(s);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        if (hasCustomDelimiter(s)) {
+            validateCustomDelimiter(s);
+            s = s.substring(5);
         }
+        validateDelimiter(s);
+        validateNumbers(s);
+    }
+
+    public List<Integer> getConvertedInput(String s) {
+        Converter converter = new Converter();
+        if (hasCustomDelimiter(s)) {
+            s = s.substring(5);
+        }
+        return converter.convertToNumbers(s, manageDelimiters.getDelimiters());
     }
 }
