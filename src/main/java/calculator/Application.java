e41ae75a10b4aacde3c49d6324d7405377dace19
dia <dia19dev@gmail.com>
2024-10-21 15:36:56 +0900

    feat(Application): add delimiter separation functionality
    Implemented a feature to separate input based on delimiters.
    
    Closes

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,7 +2,9 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
+        String separator = "[,;]";
         String str;
+        String[] operand;
 
         //문자열 입력받기
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -11,6 +13,9 @@ public class Application {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        //구분자 분리
+        operand = str.split(separator);
 
     }
 }
