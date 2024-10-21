3b97bf4f9c0f0ac03bfaca7d41cac983416255d4
rootTiket <l20078829@gmail.com>
2024-10-21 16:07:55 +0900

    fix: //와 \n의 위치가 바뀌어도 문자열을 substring하도록 변경

--- src/main/java/calculator/domain/InputSanitizer.java
+++ src/main/java/calculator/domain/InputSanitizer.java
@@ -3,12 +3,39 @@ package calculator.domain;
 public class InputSanitizer {
 
     public String sanitizeInput(String input, String customDelimiterStart, String customDelimiterEnd) {
-        if (input.startsWith(customDelimiterStart)) {
-            int newlineIndex = input.indexOf(customDelimiterEnd);
-            if (newlineIndex != -1) {
-                return input.substring(newlineIndex + customDelimiterEnd.length());
+        int startDelimiterIndex = input.indexOf(customDelimiterStart);
+        int endDelimiterIndex = input.indexOf(customDelimiterEnd);
+
+        if (hasDelimiters(startDelimiterIndex, endDelimiterIndex)) {
+            if (isStartDelimiterFirst(startDelimiterIndex, endDelimiterIndex)) {
+                return removeAfterEndDelimiter(input, endDelimiterIndex, customDelimiterEnd);
+            }
+
+            if (isEndDelimiterFirst(endDelimiterIndex, startDelimiterIndex)) {
+                return removeAfterStartDelimiter(input, startDelimiterIndex, customDelimiterStart);
             }
         }
+
         return input;
+    }
+
+    private boolean hasDelimiters(int startDelimiterIndex, int endDelimiterIndex) {
+        return startDelimiterIndex != -1 || endDelimiterIndex != -1;
+    }
+
+    private boolean isStartDelimiterFirst(int startDelimiterIndex, int endDelimiterIndex) {
+        return startDelimiterIndex == 0 && endDelimiterIndex != -1 && endDelimiterIndex > startDelimiterIndex;
+    }
+
+    private boolean isEndDelimiterFirst(int endDelimiterIndex, int startDelimiterIndex) {
+        return endDelimiterIndex == 0 && startDelimiterIndex != -1 && startDelimiterIndex > endDelimiterIndex;
+    }
+
+    private String removeAfterEndDelimiter(String input, int endDelimiterIndex, String customDelimiterEnd) {
+        return input.substring(endDelimiterIndex + customDelimiterEnd.length());
+    }
+
+    private String removeAfterStartDelimiter(String input, int startDelimiterIndex, String customDelimiterStart) {
+        return input.substring(startDelimiterIndex + customDelimiterStart.length());
     }
 }
