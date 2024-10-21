737d01d7fd145df4a643ee55bad530829df35758
유윤지 <ll.o.llwhkr@gmail.com>
2024-10-21 20:34:32 +0900

    refactor: 입출력에서 sout 사용 줄이기 위해 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,19 +1,17 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
+        UserInputOutput userIO = new UserInputOutput();
+        String input = userIO.input();
 
         InputValidator validator = new InputValidator();
-
         StringSumCalculator calculator = new StringSumCalculator(validator);
+
         int result = calculator.add(input);
 
-        System.out.println("결과 : " + result);
+        userIO.displayResult(result);
     }
 }
