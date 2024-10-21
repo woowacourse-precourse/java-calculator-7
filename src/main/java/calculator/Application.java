1877699efe44ca0ec1dc5c190af68ab653c5c18b
Jaewon Lee <jaewonlee.pro@gmail.com>
2024-10-21 20:10:33 +0900

    feat(application): 계산기 프로그램 main에 사용자 입출력과 게산기를 생성하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import static calculator.presentation.DelimiterConstants.CUSTOM_DELIMITER_PREFIX;
+import static calculator.presentation.DelimiterConstants.CUSTOM_DELIMITER_SUFFIX;
+import static calculator.presentation.DelimiterConstants.DEFAULT_DELIMITER;
+import static calculator.presentation.Message.CALCULATOR_INPUT_MESSAGE;
+import static calculator.presentation.Message.RESULT_PREFIX;
+
+import calculator.domain.Calculator;
+import calculator.presentation.InputView;
+import calculator.presentation.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView(CALCULATOR_INPUT_MESSAGE, DEFAULT_DELIMITER, CUSTOM_DELIMITER_PREFIX,
+                CUSTOM_DELIMITER_SUFFIX);
+        OutputView outputView = new OutputView(RESULT_PREFIX);
+
+        Calculator calculator = new Calculator();
+
+        String line = inputView.readInput();
+        String[] numbers = inputView.parseInput(line);
+
+        int result = calculator.add(numbers);
+        outputView.printResult(result);
     }
 }
