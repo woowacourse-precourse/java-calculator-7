372b5f1db01d3fda0b83382d49355ba2dc833044
Hyemin Ji <hyeminji.minerva@gmail.com>
2024-10-21 23:29:46 +0900

    feat: 계산 결과를 출력하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,17 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = "1,2:3"; // 예시 입력, 나중에 사용자의 입력으로 변경할 수 있습니다.
+
+        int result = add(input);
+        System.out.println("결과 : " + result);
+
+
+    }
+    public static int add(String input) {
+        // 빈 문자열 입력 시 0을 반환
+        if (input.isEmpty()) {
+            return 0;
+        }
     }
 }
