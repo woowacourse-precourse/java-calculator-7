57ad6f71c8f8b94448c4a88f2994ed958a112d97
DESKTOP-V78KGF4\LOVE <wkdskdud05@naver.com>
2024-10-21 23:14:49 +0900

    feat(calculator): add custom delimiter code

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -17,20 +18,31 @@ public class Application {
 
         scanner.close();  // Scanner 닫기
     }
+
     public static int add(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
+
         String delimiter = ",|:"; // 기본 구분자: 쉼표와 콜론
 
-        String[] numbers = input.split(delimiter);
+        // 커스텀 구분자 처리
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\\n");
+            if (delimiterIndex == -1) {
+                throw new IllegalArgumentException("커스텀 구분자 지정 오류");
+            }
+            delimiter = input.substring(2, delimiterIndex);
+            input = input.substring(delimiterIndex + 2);
+     }
+
+        String[] numbers = input.split(delimiter); // 구분자를 기준으로 숫자 분리
 
         int sum = 0;
         for (String number : numbers) {
-            sum += Integer.parseInt(number);
+            sum += Integer.parseInt(number); // 각 숫자를 정수로 변환하여 더함
         }
 
         return sum;
-
     }
 }
