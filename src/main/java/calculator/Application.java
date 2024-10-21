ee77b1c11a8606f4933ebf4be09da3718ea52883
b2etr <102886338+betterb2etr@users.noreply.github.com>
2024-10-21 21:32:43 +0900

    fix: 입력 시 개행문자 처리 추가
    - 사용자가 입력한 개행문자를 올바르게 인식하도록 개선

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        //\n입력시 주의
         String input = Console.readLine().replace("\\n", "\n");
 
             StringCalculator calculator = new StringCalculator();
