d6037ba55e73f29c848a5c662cc83b8f5730c07a
Trillion_Impulse <tkddu2897@naver.com>
2024-10-21 16:14:08 +0900

    chore: add comments for previous work

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,9 +6,11 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        // 입력 문구
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        // 문자열 입력 받기
         String input = Console.readLine();
-        
         
 
     }
