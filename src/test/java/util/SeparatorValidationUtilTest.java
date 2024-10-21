24f01fd2d4632d089731b5d1ef062c293fed9672
kkmdevel <kkm.devel@gmail.com>
2024-10-21 12:44:21 +0900

    test: SeparatorValidationUtilTest 테스트 케이스 변경

--- src/test/java/util/SeparatorValidationUtilTest.java
+++ src/test/java/util/SeparatorValidationUtilTest.java
@@ -9,31 +9,27 @@ import org.junit.jupiter.api.Test;
 public class SeparatorValidationUtilTest {
 
     @Test
-    @DisplayName("틀린 커스텀 구분자 1.형식이 틀림 2.없음 3.한개가 아님 4.숫자가 들어감 ")
-    public void testInvalidSeparatorFormat() {
+    @DisplayName("틀린 커스텀 구분자 1.형식이 틀림 2.없음 3.숫자가 들어감 ")
+    public void testInvalidSeparatorStringFormat() {
         assertThrows(IllegalArgumentException.class, () ->
-                SeparatorValidationUtil.validateSeparatorFormat("/;\n")
+                SeparatorValidationUtil.validateSeparatorStringFormat("/;\\n")
         );
 
         assertThrows(IllegalArgumentException.class, () ->
-                SeparatorValidationUtil.validateSeparatorFormat("//\n")
+                SeparatorValidationUtil.validateSeparatorStringFormat("//\\n")
         );
 
         assertThrows(IllegalArgumentException.class, () ->
-                SeparatorValidationUtil.validateSeparatorFormat("//abc\n")
-        );
-
-        assertThrows(IllegalArgumentException.class, () ->
-                SeparatorValidationUtil.validateSeparatorFormat("//1\n")
+                SeparatorValidationUtil.validateSeparatorStringFormat("//1\\n")
         );
 
     }
 
     @Test
     @DisplayName("올바른 커스텀 구분자 띄어쓰기도 가능 ")
-    public void testValidSeparatorFormat() {
-        SeparatorValidationUtil.validateSeparatorFormat("//;\n");
-
-        SeparatorValidationUtil.validateSeparatorFormat("// \n");
+    public void testValidSeparatorStringFormat() {
+        SeparatorValidationUtil.validateSeparatorStringFormat("//\\\n");
+        SeparatorValidationUtil.validateSeparatorStringFormat("//;\\n");
+        SeparatorValidationUtil.validateSeparatorStringFormat("// \\n");
     }
 }
