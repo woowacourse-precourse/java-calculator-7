ff20951eb3f03daedeaf072d66e83aa313b3f648
woorimj <forestwr06@gmail.com>
2024-10-21 16:52:17 +0900

    Fix: 예외처리 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,19 +3,14 @@ package calculator;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException{
         Scanner sc = new Scanner(System.in);
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = sc.nextLine();
 
         // 예외처리
-        try{
-            System.out.println("결과 : " + sum(input));
-
-        }catch (IllegalArgumentException e){
-            return;
-        }
+        System.out.println("결과 : " + sum(input));
     }
 
     public static int sum(String input) {
@@ -28,7 +23,7 @@ public class Application {
             int customLast = input.indexOf("\\n");
             // 구분자가 없는 경우
             if (customLast == -1) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("구분자가 없습니다.");
             }
 //            System.out.println(customLast);
             base = String.valueOf(input.charAt(customStart + 2));
@@ -45,7 +40,7 @@ public class Application {
                 int num = Integer.parseInt(n);
                 // 숫자가 음수인경우
                 if (num < 0) {
-                    throw new IllegalArgumentException();
+                    throw new IllegalArgumentException("음수는 허용되지 않습니다.");
                 }
                 total += num;
             }
