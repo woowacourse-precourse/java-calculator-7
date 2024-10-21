7c9555f195e6269ec8c6643a1cc5aaa081a27ddb
Lucerna00 <pjs3859811@naver.com>
2024-10-21 23:03:19 +0900

    feat(Calculator.java): add method to get string without delimiter

--- src/main/java/calculator/controller/Calculator.java
+++ src/main/java/calculator/controller/Calculator.java
@@ -5,6 +5,8 @@ import calculator.model.Numbers;
 import calculator.view.Output;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
@@ -12,7 +14,8 @@ public class Calculator {
     private static final String DEFAULT_DELIMITER = ",|:";
     private static final String OR = "|";
     private static final int CUSTOM_DELIMITER_INDEX = 2;
-    private static final int CUSTOM_DELIMITER_PREFIX = 5;
+    private static final String CUSTOM_DELIMITER_REGEX = "^(//.?\\\\n)(.*$)";
+    private static final int CONTENT = 2;
 
     private Output output = new Output();
     private Numbers numbers = new Numbers();
@@ -43,7 +46,7 @@ public class Calculator {
 
     private int[] getNumbers(String input, String delimiter) {
         if (!Validation.isDefaultDelimiter(delimiter)) {
-            input = input.substring(CUSTOM_DELIMITER_PREFIX);
+            input = getContent(input);
         }
         String[] tokens = input.split(delimiter);
         if (!Validation.isAllNumbers(tokens)) {
@@ -56,5 +59,12 @@ public class Calculator {
             throw new IllegalArgumentException(ErrorType.NOT_ALLOWED_NEGATIVE.getMessage());
         }
         return numbers;
+    }
+
+    private String getContent(String input) {
+        Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        matcher.matches();
+        return matcher.group(CONTENT);
     }
 }
