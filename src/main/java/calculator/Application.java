9092974f2fa05d457835d88a3d18b403c418a76f
cloky0000 <lyjin2002@gmail.com>
2024-10-21 23:34:46 +0900

    MVC 형태 틀 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,12 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         // 깃 테스트용
-
+        ApplicationController controller = new ApplicationController();
+        controller.run();
     }
 }
