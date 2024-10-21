43aecadd6a169760767b3b976c0853a84446112e
V4N1LLA <bubble1549@gmail.com>
2024-10-21 18:05:47 +0900

    test: ApplicationCustomTest 클래스 추가

---
+++ src/test/java/calculator/ApplicationCustomTest.java
@@ -0,0 +1,77 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ApplicationCustomTest extends NsTest {
+
+    @Test
+    void testBasicDelimiters() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+
+        assertSimpleTest(() -> {
+            run("4:5:6");
+            assertThat(output()).contains("결과 : 15");
+        });
+
+        assertSimpleTest(() -> {
+            run("1,2:3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void testCustomDelimiter() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void testCustomDelimiterNegative() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> run("//;\\n-1;2;-3"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("0과 음수는 허용되지 않습니다: [-1, -3]");
+        });
+    }
+
+    @Test
+    void testNegative() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> run("-1,-2,-3"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("0과 음수는 허용되지 않습니다: [-1, -2, -3]");
+        });
+    }
+
+
+    @Test
+    void testSpace() {
+        assertSimpleTest(() -> {
+            run(" ");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    /*@Test
+    void testNull() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }*/
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
