7f173f84a7a98b1d18e946eced77baa9a4c45970
Hannana <hancrystal@sookmyung.ac.kr>
2024-10-21 23:19:17 +0900

    fix: splitString()함수 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,5 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
-
-import javax.sound.midi.Soundbank;
-import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class Application {
@@ -10,25 +7,18 @@ public class Application {
         //사용자로부터 문자열 입력 받기
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputStr = Console.readLine();
-        int answer = 0;
-        String[] atoms;
         try {
-            if (isNull(inputStr)) {//빈 문자열이 입력될 경우
-                displayResult(answer);
-            } else {
-                if (inputStr.startsWith("//")) { // 사용자 입력 문자열 분리하기 with 커스텀 구분자
-                    atoms = splitCustom(inputStr);
-                    answer = sumNum(atoms);
-                } else { // 사용자 입력 문자열 분리하기 with 기본 구분자
-                    atoms = splitBasic(inputStr);
-                    answer = sumNum(atoms);
-                }
-                displayResult(answer);
-            }
+            int answer = isNull(inputStr) ? 0 : processInput(inputStr);
+            displayResult(answer);
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage()); // 예외 메시지 출력
+            System.out.println(e.getMessage());
+        } finally {
+            Console.close();
         }
-        Console.close();
+    }
+    private static int processInput(String inputStr) {
+        String[] atoms = inputStr.startsWith("//") ? splitCustom(inputStr) : splitBasic(inputStr);
+        return sumNum(atoms);
     }
     private static int sumNum(String[] numbers) {//분리 된 숫자 합산하기
         return Arrays.stream(numbers).mapToInt(num -> {
@@ -40,15 +30,22 @@ public class Application {
         }).sum();
     }
     private static String[] splitCustom(String inputStr) {
-        int endDelimeter = inputStr.indexOf("\\n");//이스케이프문 \ 포함
-        String delimiter = inputStr.substring(2, endDelimeter);//\n전까지 커스텀 구분자 지정
-        inputStr = inputStr.substring(endDelimeter + 2);//구분자 적용 대상 문자열로 분리
-        return Arrays.stream(inputStr.split(delimiter))
-                .map(s -> isNull(s) ? "0" : s) //예외: null 또는 빈 문자열이면 "0"으로 변환
-                .toArray(String[]::new);
+        int endDelimiter = inputStr.indexOf("\\n");
+        if (endDelimiter == -1) {
+            throw new IllegalArgumentException("잘못된 형식입니다. \\n이 없습니다.");
+        }
+
+        String delimiter = inputStr.substring(2, endDelimiter); // \n 전까지 커스텀 구분자 지정
+        inputStr = inputStr.substring(endDelimiter + 2); // 구분자 적용 대상 문자열로 분리
+
+        return splitString(inputStr, delimiter);
     }
     private static String[] splitBasic(String inputStr) {
-        return Arrays.stream(inputStr.split(",|:"))
+        return splitString(inputStr, ",|:"); // 기본 구분자 콤마(,) 또는 콜론(:)
+    }
+    // 문자열을 구분자로 분리하고 빈 문자열을 0으로 처리
+    private static String[] splitString(String inputStr, String delimiter) {
+        return Arrays.stream(inputStr.split(delimiter))
                 .map(s -> isNull(s) ? "0" : s) // null 또는 빈 문자열이면 "0"으로 변환
                 .toArray(String[]::new);
     }
