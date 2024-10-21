0397c519d65680b2dbd72ab5eaba7cc89e13369a
spaghetticutter <frost396@naver.com>
2024-10-21 19:44:56 +0900

    feat: 기본 덧셈 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,33 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static int woowaWeek1(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        // 기본 구분자(쉼표(,) 또는 콜론(:))를 이용한 문자열 계산기 구현
+        String delimiter = "[,:]";
+        String[] numbers = input.split(delimiter);
+        int sum = 0;
+
+        // 합계 계산
+        for (String num : numbers) {
+            if (!num.isEmpty()) {
+                int number = Integer.parseInt(num.trim());
+                sum += number;
+            }
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        int result = woowaWeek1(input);
+        System.out.println("결과 : " + result);
     }
 }
