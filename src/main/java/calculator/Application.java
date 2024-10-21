27daa1a818c6ff58bc9bdf02dbdcd6324af444fd
문준일 <answnsdlfwkd@naver.com>
2024-10-21 23:13:02 +0900

    fix: 쉼표 및 콜론을 구분자로 사용하여 문자열을 분리하고 합 계산 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,5 +10,21 @@ public class Application {
         if (str == null || str.isEmpty()) {
             throw new IllegalArgumentException("입력 문자열이 비어 있습니다.");
         }
+
+        String delimiter = "[,:]";  // 쉼표 또는 콜론
+
+        String[] strArr = str.split(delimiter);
+        int sum = 0;
+        
+        for (String token : strArr) {
+            try {
+                int number = Integer.parseInt(token);
+                sum += number;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다.");
+            }
+        }
+
+        System.out.println("결과 : " + sum);
     }
 }
