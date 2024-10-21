ddc2ae3db5b5f51526c9f8d53c81e38f52d086c6
iceprins <junghankyul@gmail.com>
2024-10-21 22:19:47 +0900

    refactor: Application 수정 불필요한 부분 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,5 @@ public class Application {
     public static void main(String[] args) {
         CalculateController controller = new CalculateController();
         controller.start();
-        //;\n1;2;0
     }
 }
