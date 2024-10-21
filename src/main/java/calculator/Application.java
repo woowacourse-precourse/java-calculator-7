3ce57983b9c11104328fd650c63797ba30a1ec03
aprilnineteen <dencoding@naver.com>
2024-10-21 15:30:39 +0900

    feat: 구분자 분리된 값을 더하는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,7 +6,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
         String[] numbers = splitNumbers(input);
+        int sum = calculateSum(numbers);
     }
 
     private static String[] splitNumbers(String input) {
@@ -17,5 +19,14 @@ public class Application {
             return input.split(customDelimiter);
         }
         return input.split("[,;]");
+    }
+
+    private static int calculateSum(String[] numbers) {
+        int sum = 0;
+        for (String number : numbers) {
+            int num = Integer.parseInt(number);
+            sum += num;
+        }
+        return sum;
     }
 }
