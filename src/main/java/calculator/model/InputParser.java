bc8425ea7a4155d3747eef8b2537a47b41f52636
ryan-dia <cjfdnjs158@naver.com>
2024-10-21 20:14:38 +0900

    style: 더 명확한 이름으로 변경
    - tokens -> parsedNumbers

--- src/main/java/calculator/model/InputParser.java
+++ src/main/java/calculator/model/InputParser.java
@@ -17,15 +17,15 @@ public class InputParser {
             return List.of();
         }
 
-        String[] tokens = numbers.split(delimiterRegex, -1);
-        validateTokens(tokens);
+        String[] parsedNumbers = numbers.split(delimiterRegex, -1);
+        validateTokens(parsedNumbers);
 
-        return List.of(tokens);
+        return List.of(parsedNumbers);
     }
 
-    private static void validateTokens(String[] tokens) {
-        Arrays.stream(tokens).forEach(token -> {
-            if (token.isEmpty()) {
+    private static void validateTokens(String[] parsedNumbers) {
+        Arrays.stream(parsedNumbers).forEach(parsedNumber -> {
+            if (parsedNumber.isEmpty()) {
                 throw new IllegalArgumentException("연속된 구분자는 허용되지 않습니다.");
             }
         });
