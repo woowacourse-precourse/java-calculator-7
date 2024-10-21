9b54d71e25f544428dd128f3606fabd102176a68
yebrong <officeguider@naver.com>
2024-10-21 22:59:55 +0900

    feat : 구분자 처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -15,10 +15,29 @@ public class Calculator {
             throw new IllegalArgumentException();
         }else{
             this.inputData=inputData;
-//            getSeparator();
+            getSeparator();
 
         }
     }
+
+    // 커스텀 구분자 처리
+    private void getSeparator(){
+        // "//"로 시작하면 커스텀, 그렇지 않으면 기본 구분자
+        if(inputData.startsWith("//")){
+            isDefault=false;
+            String[] parts = inputData.split("\\\\n", 2);
+            if (parts.length < 2) {
+                throw new IllegalArgumentException("Invalid input format: missing number part.");
+            }
+            separator = parts[0].substring(2); // "//" 이후의 구분자만 추출
+            inputData = parts[1]; // 구분자 뒤의 숫자들
+        }  else {
+            isDefault = true;
+            inputData = inputData.replaceAll(":",",");
+            separator = ",";
+        }
+    }
+
 
 
 
