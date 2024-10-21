5de2492891ed678ce4c67620e1d43ed033e4be3e
GrO0vy <lmc2819@naver.com>
2024-10-21 13:41:36 +0900

    feat: 계산식 계산 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -16,6 +16,11 @@ public class Application {
 
         // 4. 계산식이 올바른 형식의 계산식인지 확인한다.
         isValidExpression(expression);
+
+        // 5. 계산식에서 구분자를 기준으로 나눈 각각의 숫자들을 더한 합을 구한다.
+        long total = sum(expression);
+
+        System.out.printf("결과 : %d", total);
     }
 
     // 문자열을 입력 받아 입력 받은 문자열을 반환하는 메소드
@@ -56,5 +61,27 @@ public class Application {
         // 숫자로 시작해서 숫자로 끝나지 않거나, 구분자를 연속으로 사용 할 경우 입력 오류
         if(!expression.matches("^[0-9]$|^[0-9].*[0-9]$") || expression.contains(",,"))
             throw new IllegalArgumentException("입력 양식을 지켜주세요. ex) 1,2:3, //*\\n1:2*3");
+    }
+
+    // 구분자로 분리한 숫자들의 합을 구해서 반환하는 메소드
+    private static long sum(String expression){
+        String[] numbers = expression.split(","); // 계산식의 모든 구분자를 쉼표로 치환했기 때문에 쉼표로 숫자 구분
+        long total = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            try {
+                long number = Long.parseLong(numbers[i]);
+
+                // number + total 이 Long 범위를 넘으면 예외 발생
+                if (number > Long.MAX_VALUE - total) throw new RuntimeException();
+
+                total += number;
+
+            } catch (Exception e) {
+                // 계산식에 들어있는 수가 Long 범위를 벗어나거나, 총 합이 Long 범위를 벗어나면 예외 발생
+                throw new IllegalArgumentException(String.format("절대값이 %d를 넘는 값은 계산할 수 없습니다.", Long.MAX_VALUE));
+            }
+        }
+
+        return total;
     }
 }
