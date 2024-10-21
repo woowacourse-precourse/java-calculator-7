d1962132b5110f936877eb96bc6fe9c2e1070209
jjw0071 <jjw0071@naver.com>
2024-10-21 16:31:44 +0900

    feat: 커스텀 구분자 추출 및 숫자 부분 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,12 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        String delimiter1 = ",";
-        String delimiter2 = ":";
+        String delimiter = ",|:";
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        String[] s = input.split("\\\\n");
+        String customDelimiter = "";
+        if(s[0].startsWith("//")){
+            customDelimiter = s[0].substring(2);
+        }else{
+            throw new IllegalArgumentException("커스텀 구분자를 사용하려면 //로 시작해야 합니다.");
+        }
+        String totalDelimiter = delimiter+"|"+customDelimiter;
+        String[] inputNumbers = s[1].split(totalDelimiter);
+        
 
 
 
