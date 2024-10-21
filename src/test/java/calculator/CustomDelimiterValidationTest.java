7e0942cff26503f94f68282a373a21c3fc4c9709
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-21 17:07:27 +0900

    test: 커스텀 구분자 2개 이상 테스트 추가
    - // \n 커스텀 구분자 2개 이상의 문자 테스트 추가

--- src/test/java/calculator/CustomDelimiterValidationTest.java
+++ src/test/java/calculator/CustomDelimiterValidationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class CustomDelimiterValidationTest extends NsTest {
     @Test
@@ -41,9 +41,17 @@ class CustomDelimiterValidationTest extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_성공_테스트5() {
+        assertSimpleTest(() -> {
+            run("//-*\\n123-*13");
+            assertThat(output()).contains("결과 : 136");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_실패_테스트1() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("//qw\\n123qw23"))
+                assertThatThrownBy(() -> runException("//qw\\n123q23"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
