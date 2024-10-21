7e9079379261579329d56fe77c0fd0d62172fb43
githublees <leeho_@naver.com>
2024-10-21 18:40:07 +0900

    refactor(parser): apply named capture to regex

--- src/main/java/calculator/calculator/Parser.java
+++ src/main/java/calculator/calculator/Parser.java
@@ -7,14 +7,15 @@ import java.util.regex.Pattern;
 
 public class Parser {
 
-    private static final Pattern customPattern = Pattern.compile("//(.*)\\\\n(.*)");
+    private static final Pattern customPattern = Pattern.compile("//(?<delimiter>.*)\\\\n(?<content>.*)");
 
     public String[] split(String input) {
         Matcher matcher = customPattern.matcher(input);
         if (matcher.find()) {
-            String delimiter = matcher.group(1);
+            String delimiter = matcher.group("delimiter");
+            String content = matcher.group("content");
             InputValidator.validateDelimiterBlank(delimiter);
-            return matcher.group(2).split(Pattern.quote(delimiter));
+            return content.split(Pattern.quote(delimiter));
         }
 
         return input.split(Constant.ORIGIN_DELIMITER_REGEX);
