5c000c3b598aedc381c124ab870d30cee8b4305c
JeongYeonSeung <asas4654@daum.net>
2024-10-21 16:02:58 +0900

    feat: add custom delimiter detection for AddCalculator
    - create AddCalculator class
    - implement static regex pattern for custom delimiter syntax
    - add method to check for presence of custom delimiter

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,17 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class AddCalculator {
+    static Pattern customDelimiterRegex = Pattern.compile("//(.)\\\\n(.*)");
+
+    boolean isCustomDelimiterExisted(String input) {
+        Matcher matcher = customDelimiterRegex.matcher(input);
+        return matcher.matches();
+    }
+}
 
 public class Application {
     public static void main(String[] args) {
@@ -8,6 +19,8 @@ public class Application {
         int result = 0;
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Console.readLine();
+
+        AddCalculator addCalculator = new AddCalculator();
 
         System.out.println("결과 : " + result);
     }
