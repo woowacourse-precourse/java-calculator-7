6ceffad44d50970fbeffd3a01c348b10fa0d2361
SinnoLn <sintmtn69@gmail.com>
2024-10-21 12:36:43 +0900

    feat: null 예외처리 추가

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -2,6 +2,10 @@ package calculator;
 
 public class StringCalculator {
     public int calculate(int[] numbers) {
+        if (numbers == null) {
+            throw new IllegalArgumentException("입력 값이 null 일 수 없습니다");
+        }
+
         int sum = 0;
         for (int num : numbers) {
             sum += num;
