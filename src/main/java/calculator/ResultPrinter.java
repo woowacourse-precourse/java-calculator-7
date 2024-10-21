cf95dab0224f7142ee9f74242cce9bbaaaa06f0e
KIM TAE HYEONG <thk12378905@gmail.com>
2024-10-21 23:13:39 +0900

    docs: 결과 출력 기능 주석 넘버링 수정

--- src/main/java/calculator/ResultPrinter.java
+++ src/main/java/calculator/ResultPrinter.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class ResultPrinter {
 
-    // 6. 결과 출력 기능
+    // 7. 결과 출력 기능
     public void printResult(int result) {
         System.out.println("결과 : " + result);
     }
