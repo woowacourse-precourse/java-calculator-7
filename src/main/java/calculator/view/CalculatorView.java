79299ad28b0cb7ba313b73a3029d9f97957f0eed
gyuoo <yeomky98@naver.com>
2024-10-21 22:49:01 +0900

    feat: add CalculatorView class

---
+++ src/main/java/calculator/view/CalculatorView.java
@@ -0,0 +1,13 @@
+package calculator.view;
+
+import calculator.service.InputService;
+
+public class CalculatorView {
+
+	private final InputService inputService;
+
+	public CalculatorView(InputService inputService) {
+		this.inputService = inputService;
+	}
+
+}
