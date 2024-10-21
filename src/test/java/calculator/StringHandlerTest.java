9aad1c318aa77a108e65d8167dc611965e1f8791
GO-TE <alsrudxo9810@naver.com>
2024-10-21 10:55:09 +0900

    style: import 와일드 카드 제거

--- src/test/java/calculator/StringHandlerTest.java
+++ src/test/java/calculator/StringHandlerTest.java
@@ -1,6 +1,6 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import calculator.util.SeparatorManager;
 import calculator.util.StringHandler;
