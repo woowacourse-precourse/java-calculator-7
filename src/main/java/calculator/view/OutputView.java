570d1d808ab1a287f556dc1f4f313d3b06d42fb9
rootTiket <l20078829@gmail.com>
2024-10-21 16:25:38 +0900

    refactor: 메시지를 enum으로 관리하도록 변경

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,10 +1,11 @@
 package calculator.view;
 
+import static calculator.view.constant.outputMessage.OutputMessage.RESULT_MESSAGE;
 import static calculator.view.constant.outputMessage.OutputMessage.START_MESSEAGE;
 
 public class OutputView {
     public void print(int value) {
-        System.out.println("결과: " + value);
+        System.out.println(RESULT_MESSAGE.getMessage() + value);
     }
 
 
