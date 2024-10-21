7c0cb6d9f772de9ecd84218fab23c274c9450fe3
rootTiket <l20078829@gmail.com>
2024-10-21 17:27:15 +0900

    fix: 결과가 정수 범위를 넘어가면 예외처리

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,12 +1,25 @@
 package calculator.domain;
 
+import static calculator.domain.constant.errorMessage.ValueError.OUT_OF_RANGE_RESULT;
+
+import calculator.global.exception.CalculatorException;
 import java.util.List;
 
 public class Calculator {
 
     public int sum(List<Integer> numbers) {
-        return numbers.stream()
-                .mapToInt(Integer::intValue)
+        long sum = numbers.stream()
+                .mapToLong(Integer::longValue)
                 .sum();
+
+        checkOutOfRange(sum);
+
+        return (int) sum;
+    }
+
+    private void checkOutOfRange(long sum) {
+        if (sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE) {
+            throw new CalculatorException(OUT_OF_RANGE_RESULT);
+        }
     }
 }
--- src/main/java/calculator/domain/constant/errorMessage/ValueError.java
+++ src/main/java/calculator/domain/constant/errorMessage/ValueError.java
@@ -5,7 +5,9 @@ import calculator.global.globalEnum.Message;
 public enum ValueError implements Message {
     MINUS_VALUE("음수가 올 수 없습니다."),
     INVALID_VALUE_FORMAT("올바르지 않은 형식입니다."),
-    OUT_OF_RANGE_VALUE("Int 범위 외로는 입력할 수 없습니다.");
+    OUT_OF_RANGE_VALUE("Int 범위 외로는 입력할 수 없습니다."),
+    OUT_OF_RANGE_RESULT("결과가 범위를 초과합니다.");
+
 
     private final String message;
 
