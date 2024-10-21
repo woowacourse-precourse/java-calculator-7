5d811a144cd29db167b93df5d70ef621519295ab
chan <ckstj8027@naver.com>
2024-10-21 23:11:07 +0900

    style(Application): 불필요한 주석제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,11 +10,6 @@ import calculator.validator.ValidatorImpl;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        //;\n1;2;3
-        //;as\\\\\n1;as\\\\2;as\\\\37
-        //;+{\n1;+{2;+{37
-        //;-\n1;-2;-3
-        //;-\n1;--2;-3
 
         //Service service = new ServiceV1();
         Validator validator = new ValidatorImpl();
