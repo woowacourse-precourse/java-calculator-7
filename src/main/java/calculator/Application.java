3821a15076104af9d59705b24033d07a243f0980
masee1994 <masee1994@gmail.com>
2024-10-21 23:32:33 +0900

    feat: IllegalArgumentException 처리 추가
    - 빈 문자열 입력 시 처리 로직 추가
    - 잘못된 입력값의 유효성을 검사하는 ValidChk 메소드 추가
    
    fix: 버그 수정
    - `input = input.substring(end + 1)`를 `input = input.substring(end + 2)`로 수정
    - 이스케이프 시퀀스(`\n`)가 아닌 문자열(`\\n`) 처리를 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,17 +6,51 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = Console.readLine();
-        String split = ",|:";
+        int ans = 0;
+
+        if(input.equals("")) {
+        }
+        else{
+            if (!ValidChk(input)) {
+                throw new IllegalArgumentException();
+            }
+            String split = ",|:";
+            if (input.startsWith("//")) {
+                int end = input.indexOf("\\n");
+                split += ("|" + input.substring(2, end));
+                input = input.substring(end + 2);
+            }
+
+            String num[] = input.split(split);
+            int sum = 0;
+
+            for (String number : num) {
+                int n = Integer.parseInt(number);
+                if (n < 0) {
+                    throw new IllegalArgumentException();
+                }
+                sum += n;
+            }
+
+            ans = sum;
+        }
+
+        System.out.println("결과 : " + ans);
+
+    }
+
+    private static boolean ValidChk(String input) {
+        if(input.trim().isEmpty()) {
+            return false;
+        }
+
         if(input.startsWith("//")) {
-            int end = input.indexOf("\n");
-            split+=("|"+input.substring(2,end));
-            input = input.substring(end+1);
+            int end = input.indexOf("\\n");
+
+            if(end == -1 || end == 2) {
+                return false;
+            }
         }
-        String num[] = input.split(split);
-        int sum = 0;
-        for (String number : num) {
-            sum += Integer.parseInt(number);
-        }
-        System.out.println("결과 : "+sum);
+        return true;
     }
 }
