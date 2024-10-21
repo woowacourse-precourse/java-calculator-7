2d005c623d1825671b1f24df3c9d694431f8fb33
kckc0608 <kckc0608@naver.com>
2024-10-21 17:02:58 +0900

    feat: 기본 문자열 파싱

--- src/main/java/calculator/stringCalculator/parser/Parser.java
+++ src/main/java/calculator/stringCalculator/parser/Parser.java
@@ -5,8 +5,33 @@ import java.util.List;
 
 public class Parser {
 
+    private final List<Character> splitters = List.of(',', ':');
+
     public List<Long> parse(String input) {
         List<Long> result = new ArrayList<>();
+        // tryParseUserDefinedSplitter(input);
+
+        StringBuilder builder = new StringBuilder();
+        for (char character : input.toCharArray()) {
+            if (splitters.contains(character)) {
+                result.add(Long.parseLong(builder.toString()));
+                builder = new StringBuilder();
+                continue;
+            }
+
+            builder.append(character);
+        }
+
+        if (!builder.isEmpty()) {
+            result.add(Long.parseLong(builder.toString()));
+        }
+
+        System.out.println(result);
+
         return result;
     }
+
+//    public void tryParseUserDefinedSplitter(String input) {
+//
+//    }
 }
