41721d35c74129e45a1d16a7aa2f8a51ec607150
jae0000 <jyyp3395@gmail.com>
2024-10-21 21:41:48 +0900

    계산기 기능 구현 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,50 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Vector;
+
+import static java.lang.Character.isDigit;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String word = Console.readLine();
+
+        int sum=0;
+
+        boolean check =false;
+
+        if(word.charAt(0)=='/'){
+            if(word.charAt(1)=='/'&&word.charAt(3)=='\\'&&word.charAt(4)=='n'){
+                check = true;
+            }
+        }
+
+
+        if(check){
+            Cal cal = new Cal(5,word.charAt(2),word,word.length());
+            sum = cal.getSum();
+        }
+
+
+        else{
+            if(!isDigit(word.charAt(0))){
+                throw new IllegalArgumentException();
+            }
+            else{
+                Cal cal = new Cal(0,'`',word,word.length());
+                sum = cal.getSum();
+            }
+
+        }
+
+        System.out.println("결과 : "+ sum);
+
+
     }
 }
