9c7a12a0b54a8b70b5503a9aad40e042bc2e646f
Jaewon Lee <jaewonlee.pro@gmail.com>
2024-10-21 20:09:51 +0900

    feat(message): 입출력 상수 정의

---
+++ src/main/java/calculator/presentation/DelimiterConstants.java
@@ -0,0 +1,7 @@
+package calculator.presentation;
+
+public class DelimiterConstants {
+    public static final String DEFAULT_DELIMITER = ",|:";
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+}
