e75d4e4362668468b3fcdc0f3a9b45e422addf07
zelatndnjs <zelatndnjs@naver.com>
2024-10-21 23:25:03 +0900

    feature (getCustomSeparator 및 생성자 작성)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,6 +10,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
+        Calculator c = new Calculator(input);
         close();
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,5 +6,28 @@ public class Calculator {
     String numberString; // 구분자와 숫자만 들어있는 문자열
     ArrayList<Character> separatorList; // 구분자 종류
 
+    // Calulator 인스턴스를 생성하며 Custom구분자를 추가
+    public Calculator(String input) {
+        separatorList = new ArrayList<>();
+        separatorList.add(',');
+        separatorList.add(':');
+        getCustomSeparator(input);
+    }
+
+    // input을 통해 구분자를 list에 저장하고, numberString을 추출해낸다.
+    public void getCustomSeparator(String input) throws IllegalArgumentException {
+        if(input.length()<6)
+            numberString = input;
+        else if (input.substring(0, 2).equals("//") && input.substring(3,5).equals("\\n")) {
+            char customSeparator = input.charAt(2);
+            if (customSeparator > '0' && customSeparator < '9')
+                throw new IllegalArgumentException();
+            separatorList.add(customSeparator);
+            numberString = input.substring(5, input.length());
+        }
+        else
+            numberString=input;
+    }
+
 
 }
