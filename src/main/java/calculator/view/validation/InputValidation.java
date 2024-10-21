6177aa720fb5a8a68b813e6652261885fe0b486b
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-21 17:41:06 +0900

    feat: [기능 구현 3번] InputValidation 커스텀 구분자 검사 메서드 추가 및 일부 로직 수정

--- src/main/java/calculator/view/validation/InputValidation.java
+++ src/main/java/calculator/view/validation/InputValidation.java
@@ -1,11 +1,12 @@
 package calculator.view.validation;
 
+import static calculator.utils.CalculationUtils.*;
+
 import java.util.Arrays;
 
 public class InputValidation {
-
-  private static final String VALID_INPUT_REGEX = "[0-9,\\s:]*";
   private static final String INVALID_INPUT_DATE_ERROR = "[ERROR] 유효하지 않은 입력값입니다. 다시 입력해 주세요.";
+  private static String separator = ",|;";
 
   public static void validateInputString(String inputData) {
 
@@ -15,16 +16,39 @@ public class InputValidation {
       return;
     }
 
-    if (!inputData.matches(VALID_INPUT_REGEX)) {
-      throw new IllegalArgumentException(INVALID_INPUT_DATE_ERROR);
+    if (inputData.startsWith("//")) {
+      inputData = checkCustomSeparator(inputData);
     }
 
     try {
-      Arrays.stream(inputData.split("[,:]"))
-          .forEach(Integer::parseInt);
+
+      Arrays.stream(inputData.split(separator))
+          .map(Integer::parseInt)
+          .forEach(value -> {
+            if (value < 1) {
+              throw new IllegalArgumentException(INVALID_INPUT_DATE_ERROR);
+            }
+          });
 
     } catch (Exception e) {
       throw new IllegalArgumentException(INVALID_INPUT_DATE_ERROR);
     }
+  }
+
+  private static String checkCustomSeparator(String inputData) {
+
+    int separatorEndIndex = inputData.indexOf("\\n");
+
+    if (separatorEndIndex == -1) {
+      throw new IllegalArgumentException(INVALID_INPUT_DATE_ERROR);
+    }
+
+    String customSeparator = inputData.substring(2, separatorEndIndex);
+
+
+    separator = escapeSpecialCharacters(customSeparator) + "|" + separator;
+    inputData = inputData.substring(separatorEndIndex + 2);
+
+    return inputData;
   }
 }
