56a8f2e84bc7d8b2e2ddc5e4af573d2a0848d246
choiyoung69 <choiyoung609@gmail.com>
2024-10-21 22:19:57 +0900

    feat(CalculateService): 덧셈 과정에서 발생하는 에러 처리

--- src/main/java/calculator/service/CalculateService.java
+++ src/main/java/calculator/service/CalculateService.java
@@ -1,13 +1,16 @@
 package calculator.service;
 
+import static calculator.validators.InvalidCalculationException.checkSumOverFlow;
+
 import calculator.domain.Operands;
 import calculator.dto.ResultDTO;
 
 public class CalculateService {
-    public static ResultDTO addOperand(Operands operandDTO){
+    public static ResultDTO addOperand(Operands operandDTO) {
         int sum = 0;
 
-        for(int x : operandDTO.getOperandList()){
+        for (int x : operandDTO.getOperandList()) {
+            checkSumOverFlow(sum, x);
             sum += x;
         }
 
