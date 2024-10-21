4b3154f45f8188b7c7dc809ee8295a6d6e40144a
hyunseon <dbsgustjs730@naver.com>
2024-10-21 20:22:39 +0900

    1st freecos

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,27 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int sum = 0;
+
+        for(int i=0; i<str.length(); i++){
+            if(isNumber(str.charAt(i))){
+                sum += Integer.parseInt(String.valueOf(str.charAt(i)));
+            }
+        }
+        System.out.println(sum);
+    }
+    public static boolean isNumber(char str){
+        try{
+            int num = Integer.parseInt(String.valueOf(str));
+            return true;
+        }catch(NumberFormatException e) {
+            return false;
+        }
     }
 }
