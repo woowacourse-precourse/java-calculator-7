0d72cbf2a0477fd4a96018cdeb9f83aa8b3008c1
zelatndnjs <zelatndnjs@naver.com>
2024-10-21 23:21:38 +0900

    feature (스켈로톤 코드 작성)

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    String numberString; // 구분자와 숫자만 들어있는 문자열
+    ArrayList<Character> separatorList; // 구분자 종류
+
+
+}
