9cbd21036d5240d2e1a5e11b7d799d5e890db8ec
김예서 <callikys@naver.com>
2024-10-21 19:49:30 +0900

    fix: console.close 추가

--- src/main/java/calculator/View.java
+++ src/main/java/calculator/View.java
@@ -1,11 +1,14 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.close;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class View {
     public static String inputValue() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return readLine();
+        String text = readLine();
+        close();
+        return text;
     }
 
     public static void outputValue(int result) {
