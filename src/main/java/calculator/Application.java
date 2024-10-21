befd5c0f92d338269fc2cacd6af77619a819abec
JGM <cnxw4570123@gmail.com>
2024-10-21 19:27:14 +0900

    feat : 계산 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import java.util.List;
+
+import calculator.validator.InputValidator;
+import calculator.view.View;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+		View view = new View();
+		InputValidator inputValidator = new InputValidator();
+		String input = view.getInput();
+		String[] splited = inputValidator.validateInputSplittable(input);
+		List<Integer> integers = inputValidator.validatePositiveNumbers(splited);
+
+		view.printOutput(getSum(integers));
+	}
+
+	private static long getSum(List<Integer> integers) {
+		return integers.stream()
+			.reduce(Integer::sum)
+			.orElseThrow(IllegalArgumentException::new);
+	}
 }
