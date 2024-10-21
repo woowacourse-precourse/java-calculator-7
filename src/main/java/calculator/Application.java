b8ac713d687743a8356427622714dfe5d8bdb0f5
jaewonwi <jww5555@naver.com>
2024-10-21 17:24:28 +0900

    feat: Add Calculator 입력 받은 문자 기준 계산 기능 추가 커스텀 구분자 구분 기능 추가 예외처리 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,34 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("덧셈할 문자열을 입력해 주세요.\n");
-//        String input = scanner.scan()
+    public static void main(String[] args) throws IllegalAccessException {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String divider = ".:";
+
+        String input = Console.readLine();
+
+        if (input.contains("//") && input.contains("\\n")){     // 커스텀 구분자 추가
+            int endIdx = input.indexOf("\\n");
+            String customDivider = input.substring(2, endIdx);
+            divider += customDivider;
+
+            input = input.substring(endIdx+2);
+        }
+
+        if (input.matches(".*[^0-9"+divider+"].*"))
+            throw new IllegalAccessException("잘못된 입력입니다.");
+
+
+        int result = 0;
+        String[] numbers = input.split("["+divider+"]");
+
+        for (String number : numbers){
+            if (!number.isEmpty())
+                result += Integer.valueOf(number);
+        }
+
+        System.out.println("결과: " + result);
     }
 }
