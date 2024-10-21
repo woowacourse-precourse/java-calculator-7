92817ab74b144fd3f7a630fe5e0e0a04b9f7adf2
gimogibumo <wolfbone0304@naver.com>
2024-10-21 19:58:13 +0900

    feat: Application 클래스에서 사용자 입력과 결과 출력 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
+/**
+ * Application 클래스는 StringCalculator 프로그램을 실행하는 메인 클래스로,
+ * 사용자 입력을 받아 처리하고 결과를 출력합니다.
+ */
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        try {
+            int result = StringCalculator.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 입력입니다: " + e.getMessage());
+        }
     }
 }
