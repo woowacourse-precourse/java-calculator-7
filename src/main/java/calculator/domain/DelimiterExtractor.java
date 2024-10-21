d7761255b6e09a868a06ac0eab6f7a567967bddb
davemins <davemins4125@naver.com>
2024-10-21 17:26:00 +0900

    refactor(Domain): 커스텀 구분자일 때 사용하는 메서드 분리

--- src/main/java/calculator/domain/DelimiterExtractor.java
+++ src/main/java/calculator/domain/DelimiterExtractor.java
@@ -1,19 +1,23 @@
 package calculator.domain;
 
+import java.util.regex.Pattern;
+
 public class DelimiterExtractor {
+    private static final String DEFAULT_DELIMITERS = ",|:";
+
     public String[] extract(String input) {
-        String defaultDelimiters = ",|:";
-
         if (input.startsWith("//")) {
-            String[] parts = input.split("\\\\n", 2);
-            String customDelimiter = parts[0].substring(2);
-            String numbers = parts.length > 1 ? parts[1] : "";
-
-            String combinedDelimiters = defaultDelimiters + "|" + customDelimiter;
-
-            return new String[] {combinedDelimiters, numbers};
+            return extractCustomDelimiter(input);
         }
+        return new String[] {DEFAULT_DELIMITERS, input};
+    }
 
-        return new String[] {defaultDelimiters, input};
+    private String[] extractCustomDelimiter(String input) {
+        String[] parts = input.split("\\\\n", 2);
+        String customDelimiter = parts[0].substring(2);
+        String numbers = parts.length > 1 ? parts[1] : "";
+
+        String combinedDelimiters = DEFAULT_DELIMITERS + "|" + Pattern.quote(customDelimiter);
+        return new String[] {combinedDelimiters, numbers};
     }
 }
