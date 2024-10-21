b5d093c9eb9c3526f2ac5d623ca9115dcc13c776
sonshn <sonshn@naver.com>
2024-10-21 19:43:47 +0900

    feat(Application): 프로그램 실행 함수 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        String inputString = inputView.getInput();
+
+        CalculatorService calculatorService = new CalculatorService();
+        int result = calculatorService.calculate(inputString);
+
+        OutputView outputView = new OutputView();
+        outputView.getOutput(result);
     }
 }
+
