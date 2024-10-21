92fb7f67f0209a8a505ac4565ea45513465f0aa0
Seyoung Goo <0917sy@gmail.com>
2024-10-21 20:21:06 +0900

    test: 커스텀 구분자 처리
    - 커스텀 구분자로 문자열 분리 테스트
    - 커스텀 구분자와 기본 구분자가 혼합된 경우 분리 테스트
    - 잘못된 커스텀 구분자 형식일 경우 에러 발생

--- src/test/java/calculator/StringParserTest.java
+++ src/test/java/calculator/StringParserTest.java
@@ -7,13 +7,20 @@ import calculator.model.StringParser;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringParserTest {
-    StringParser stringParser = new StringParser();
+    StringParser stringParser;
+
+    @BeforeEach
+    public void setUp() {
+        stringParser = new StringParser();
+    }
 
     private static Stream<Arguments> provideDefaultDelimiterInputSuccess() {
         return Stream.of(
@@ -27,15 +34,62 @@ public class StringParserTest {
 
     @ParameterizedTest
     @MethodSource("provideDefaultDelimiterInputSuccess")
-    public void 기본_구분자로_문자열_분리(String input, List<BigDecimal> expected) {
+    void 기본_구분자로_문자열을_분리한다(String input, List<BigDecimal> expected) {
         List<BigDecimal> numbers = stringParser.parse(input);
         assertEquals(expected, numbers);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"1,-2,10", "1/2/10", "1,2,a,10", "1,,2,10"})
-    public void 입력에_잘못된_값이_포함된_경우_에러_발생(String input) {
+    void 잘못된_값이_포함된_경우_IllegalArgumentException_발생(String input) {
         assertThrows(IllegalArgumentException.class, ()
                 -> stringParser.parse(input));
     }
+
+
+    private static Stream<Arguments> provideCustomDelimiterInput() {
+        return Stream.of(
+                Arguments.of("//;\\n1;2;3", List.of(BigDecimal.ONE, BigDecimal.TWO, BigDecimal.valueOf(3))),
+                Arguments.of("//.\\n1.2.3", List.of(BigDecimal.ONE, BigDecimal.TWO, BigDecimal.valueOf(3))),
+                Arguments.of("//a\\n1a2a3", List.of(BigDecimal.ONE, BigDecimal.TWO, BigDecimal.valueOf(3))),
+                Arguments.of("//**\\n1**2**3", List.of(BigDecimal.ONE, BigDecimal.TWO, BigDecimal.valueOf(3))),
+                Arguments.of("//[**]\\n1[**]2[**]3", List.of(BigDecimal.ONE, BigDecimal.TWO, BigDecimal.valueOf(3)))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCustomDelimiterInput")
+    void 커스텀_구분자로_문자열을_분리한다(String input, List<BigDecimal> expected) {
+        List<BigDecimal> numbers = stringParser.parse(input);
+        assertEquals(expected, numbers);
+    }
+
+    @Test
+    void 커스텀_구분자와_기본_구분자가_혼합된_경우에도_문자열을_분리한다() {
+        String input = "//;\\n1;2:3,4";
+        List<BigDecimal> expected = List.of(BigDecimal.ONE, BigDecimal.TWO, BigDecimal.valueOf(3),
+                BigDecimal.valueOf(4));
+
+        List<BigDecimal> numbers = stringParser.parse(input);
+        assertEquals(expected, numbers);
+    }
+
+    private static Stream<Arguments> provideWrongCustomDelimiterInput() {
+        return Stream.of(
+                Arguments.of("//;1;2;3;4",
+                        List.of(BigDecimal.ONE, BigDecimal.valueOf(2), BigDecimal.valueOf(3), BigDecimal.valueOf(4))),
+                Arguments.of("//|\\n1|2,3:4",
+                        List.of(BigDecimal.ONE, BigDecimal.valueOf(2), BigDecimal.valueOf(3), BigDecimal.valueOf(4)))
+        );
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;1;2;3;4", "//|\\n1;2;3;4", "//;\\n1;2,,3"})
+    void 잘못된_커스텀_구분자_형식일_경우_IllegalArgumentException_발생(String input) {
+        assertThrows(IllegalArgumentException.class, ()
+                -> stringParser.parse(input));
+    }
+
+
 }
