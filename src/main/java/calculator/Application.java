5f1ce9cebceaf9a11e7eccdc1f82a31915d23600
young-in <hardworkingini@gmail.com>
2024-10-21 21:26:45 +0900

    refactor : error 처리 방식 변화
    정상 종료를 위해 error를 처리하였는데, 오류가 발생해서 강제 종료 시키는 것으로 다시 코드를 돌렸다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,13 +4,7 @@ import calculator.controller.CalculatorController;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            CalculatorController controller = new CalculatorController();
-            controller.run();
-        } catch (IllegalArgumentException e) {
-            //System.out.println("[error] " + e.getMessage());
-            return;
-        }
-        System.out.println();
+        CalculatorController controller = new CalculatorController();
+        controller.run();
     }
 }
