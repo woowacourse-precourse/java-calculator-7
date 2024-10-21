b0538c8b851903589d1c59004a09728efe444a73
zeus <jj99526@naver.com>
2024-10-21 17:11:53 +0900

    feat : 메인 만들기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
+
     public static void main(String[] args) {
+        Set<String> separator=new HashSet<>();
+        separator.add(",");
+        separator.add(":");
+
+
         // TODO: 프로그램 구현
     }
 }
