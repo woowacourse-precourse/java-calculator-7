b671e3710a4a87090a3237bf1f7600f5431bec19
shortboy7 <thdwldnd7@naver.com>
2024-10-21 13:38:40 +0900

    feat: 덧셈 함수 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
-public class Application {
-    public static void main(String[] args) {
+import java.util.List;
 
+public class Application {
+    static int sum(List<Integer> numbers) {
+        return numbers
+            .stream()
+            .reduce(Integer::sum)
+            .orElse(0);
+    }
+
+    public static void main(String[] args) {
     }
 }
