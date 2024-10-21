d231e79fb5e5579bdaaf05f8aff3433e76370905
min-0 <dhe08133@naver.com>
2024-10-21 20:45:13 +0900

    refactor: 불필요 코드 변경
    변수 초기화, For문 구조 변경 등

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,29 +1,69 @@
 package calculator;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Application {
+    static ArrayList<String> list = new ArrayList<>(Arrays.asList(",", ":"));
+
     public static void main(String[] args) {
+        String[] delimiters;
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = readLine();
 
-        String[] parts = input.split("\\D+");
+        String input = Console.readLine();
+        int answer;
 
-        ArrayList<Integer> numbers = new ArrayList<>();
+        if (input.length() == 0) {
+            answer = 0;
+        } else {
+            delimiters = splitDelimiter(input);
+            answer = sumNumbers(delimiters);
+        }
 
-        for (String part : parts) {
-            if (!part.isEmpty()) { // 빈 문자열이 아닌 경우에만 처리
-                numbers.add(Integer.parseInt(part));
+        System.out.println("결과 : " + answer);
+    }
+
+    static public String[] splitDelimiter(String input) {
+        int start = 2, end;
+
+        if (input.startsWith("//")) {
+            end = input.indexOf("\\n");
+            String delimiter = input.substring(start, end);
+            list.add(delimiter);
+            input = input.substring(end + 2);
+        } else if (input.startsWith("-")) {
+            throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+        } else if (!Character.isDigit(input.charAt(0))) {
+            throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다.");
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (!Character.isDigit(c) && !list.contains(String.valueOf(c))) {
+                throw new IllegalArgumentException("올바른 커스텀 구분자 형식을 사용하세요.");
             }
         }
 
-        int sum = 0;
-        for (int num : numbers) {
-            sum += num;
+        String det = "[";
+        for (String s : list) {
+            det += s;
         }
+        det += "]";
 
-        System.out.println("결과 : " + sum);
+        return input.split(det);
+    }
+
+    static int sumNumbers(String[] numbers) {
+        return Arrays.stream(numbers)
+                .mapToInt(number -> {
+                    int parsedNumber = Integer.parseInt(number.trim());
+                    if (parsedNumber < 0) {
+                        throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+                    }
+                    return parsedNumber;
+                })
+                .sum();
     }
 }
