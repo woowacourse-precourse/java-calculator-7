07fceebe048047473d04ce6cdade5107f1ca8414
Hyemin Ji <hyeminji.minerva@gmail.com>
2024-10-21 23:36:32 +0900

    feat: 빈 문자열 입력 시 0을 반환하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,17 +3,15 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String input = "1,2:3"; // 예시 입력, 나중에 사용자의 입력으로 변경할 수 있습니다.
-
-        int result = add(input);
-        System.out.println("결과 : " + result);
-
-
+        String input = "";  // 빈 문자열 입력
+        System.out.println(sum(input));  // 결과 출력
     }
-    public static int add(String input) {
-        // 빈 문자열 입력 시 0을 반환
+
+    public static int sum(String input) {
         if (input.isEmpty()) {
-            return 0;
+            return 0;  // 빈 문자열이면 0 반환
         }
+        return 0;  // 기본 반환 값
+
     }
 }
