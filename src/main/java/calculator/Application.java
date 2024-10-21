0d69e6ecc2729a1fe621d70d089ed85237011e92
son <hh001204@naver.com>
2024-10-21 21:57:59 +0900

    feat: 입력 값이 공백인 경우와 예외 처리 추가 - 입력 값이 공백인 경우 "결과 : 0"을 반환하도록 함 - 문자열이 잘못 입력된 경우 IllegalArgumentException로 예외 처리를 진행함

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,30 +12,46 @@ public class Application {
         // 2. 연산 과정
         String separator = ",|:";
 
-        // 2-1. 커스텀 구분자가 있을 경우
-        if (inputText.startsWith("//")) {
-            int separatorIndex = inputText.indexOf("\\n");
-            if (separatorIndex == -1) { // "\n"이 없는 경우 [오류]
-                System.out.println("잘못된 입력 값 입니다.");
+        try {
+            // 2-1 공백일 경우
+            if (inputText.isEmpty()) {
+                System.out.println("결과 : 0");
+                return;
             }
 
-            separator = inputText.substring(2, separatorIndex);
-            inputText = inputText.substring(separatorIndex + 2);
-        }
+            // 2-2. 커스텀 구분자가 있을 경우
+            if (inputText.startsWith("//")) {
+                int separatorIndex = inputText.indexOf("\\n");
+                if (separatorIndex == -1) { // "\n"이 없는 경우 [오류]
+                    throw new IllegalArgumentException("잘못된 입력 값 입니다.");
+                }
 
-        String[] tokens = inputText.split(separator);
-
-        int sum = 0;
-
-        for (String token : tokens) {
-            int number = Integer.parseInt(token);
-            if (number < 0) {
-                System.out.println("양수만 입력해 주세요.");
+                separator = inputText.substring(2, separatorIndex);
+                inputText = inputText.substring(separatorIndex + 2);
             }
-            sum += number;
-        }
 
-        // 3. 출력
-        System.out.println("결과 : " + sum);
+            String[] tokens = inputText.split(separator);
+
+            int sum = 0;
+
+
+
+            for (String token : tokens) {
+                try {
+                    int number = Integer.parseInt(token);
+                    if (number < 0) {
+                        throw new IllegalArgumentException("양수만 입력해 주세요.");
+                    }
+                    sum += number;
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("잘못된 입력 값 입니다.");
+                }
+            }
+
+            // 3. 출력
+            System.out.println("결과 : " + sum);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
