150d34a447fe5188d3e07063ca906d00c4acdddd
chaeshee0908 <chaeshee0908@naver.com>
2024-10-21 22:50:27 +0900

    feat(InputView): 시작 메시지 출력

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,10 @@
+package calculator.view;
+
+public class InputView {
+
+    private static final String startMessage = "덧셈할 문자열을 입력해 주세요.";
+
+    public static void printStartMessage() {
+        System.out.println(startMessage);
+    }
+}
