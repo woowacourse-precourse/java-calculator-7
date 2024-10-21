56afa4b951b8b267a14f5c8602435ea8b5f850fb
kellyihyeon <kellykang13@gmail.com>
2024-10-21 14:16:01 +0900

    feat: 문자열에서 연산식을 추출하는 기능 구현

--- src/main/java/calculator/StringParser.java
+++ src/main/java/calculator/StringParser.java
@@ -5,4 +5,14 @@ public class StringParser {
     public static String parseBr(String input) {
         return input.replace("\\n", "\n");
     }
+
+    public static String parseOperationFormula(String input) {
+        final int OPERATION_FORMULA_INDEX = 1;
+
+        if (DelimiterType.CUSTOM.equals(new DelimiterExtractor(input).type())) {
+            return input.split("\n")[OPERATION_FORMULA_INDEX];
+        }
+
+        return input;
+    }
 }
--- src/test/java/calculator/StringParserTest.java
+++ src/test/java/calculator/StringParserTest.java
@@ -7,6 +7,18 @@ import org.junit.jupiter.api.Test;
 public class StringParserTest {
 
     @Test
+    void 문자열에서_연산식을_추출한다() {
+        String customInput = "//;\n1";
+        String basicInput = "1,2:3";
+
+        String operationFormulaInCustom = StringParser.parseOperationFormula(customInput);
+        String operationFormulaInBasic = StringParser.parseOperationFormula(basicInput);
+
+        assertEquals("1", operationFormulaInCustom);
+        assertEquals("1,2:3", operationFormulaInBasic);
+    }
+
+    @Test
     void 입력값의_개행문자를_줄바꿈으로_치환한다() {
         String input = "//;\\n1";
         String parsedInput = StringParser.parseBr(input);
