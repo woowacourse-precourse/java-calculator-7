a0c4c920892b88f98ba49a5ea3c75cd7b4a0c99d
1998ray <1998ray98@gmail.com>
2024-10-21 09:44:22 +0900

    fix: \n 이후 숫자 부분 추출 방법 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,16 +1,16 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         // 사용자 입력
-        String input = Console.readLine();
+        String input = readLine();
         // 덧셈 결과
         int result = add(input);
         // 결과 출력
-        System.out.println("결과 : "+ result);
+        System.out.println("결과 : "+result);
     }
 
     public static int add(String input) {
@@ -22,10 +22,10 @@ public class Application {
         // 커스텀 구분자가 있는 경우
         // 3. 커스텀 구분자를 지정하여 숫자의 합을 반환
         if (input.startsWith("//")) {
-            int Index = input.indexOf("\n"); // 커스텀 구분자 끝
-            String delimiter = input.substring(2, Index); // 커스텀 구분자 추출
-            input = input.substring(Index + 1); // 커스텀 구분자 다음부터 입력으로 설정
+            String delimiter = input.substring(2, 3);  // 커스텀 구분자 추출
+            input = input.substring(5);  // 숫자 부분 추출 (//구분자\n 이후부터)
 
+            // 커스텀 구분자로 숫자 분리
             String[] numbers = input.split(delimiter);
             int sum = 0;
             for (String number : numbers) {
