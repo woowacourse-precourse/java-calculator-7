1857bd2461564e3826cbaac8ae4f92a350936f13
yokxim <yooksi53@gmail.com>
2024-10-21 21:56:55 +0900

    [chore] 문자열 구분자를 담을 ArrayList<String> 생성과 초기화

--- src/main/java/calculator/tool/JavaCalculator.java
+++ src/main/java/calculator/tool/JavaCalculator.java
@@ -1,6 +1,18 @@
 package calculator.tool;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JavaCalculator {
+
+    public List<String> partitions;
+
+    public JavaCalculator() {
+        partitions = new ArrayList<>();
+        partitions.add(",");
+        partitions.add(":");
+    }
+
     public int solution(String input) {
         return 0;
     }
