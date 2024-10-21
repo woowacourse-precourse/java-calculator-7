db267004be9bac097bba316b39c3167a0fefcd7c
JJhwan7099 <ghks7099@naver.com>
2024-10-21 21:55:55 +0900

    feat(Application.java): 커스텀 구분자가 있는지 확인 및 추출기능 추가
    커스텀 구분자가 있는지 확인하는 함수를 추가하고, 확인 후 커스텀 구분자를
    추출 및 "\n"을 기준으로 분리하는 함수도 작성했습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,11 +9,18 @@ public class Application {
     }
     // 커스텀 구분자를 추가하는지 확인하는 함수
     private static boolean exist_custon_delimiter(String input) {
-
+        if(input.startsWith("//")) {
+            return true;
+        }
+        return false;
     }
     // "//"과  "\n" 사이의 커스텀 구분자를 추출하고 "\n" 뒤의 문자열을 분리하는 함수
     private static String[] get_custom_delimiter(String input) {
-
+        String delimeter = ",:";
+        String str_for_add = "";
+        delimeter += input.strip().split("\\\\n")[0].split("//")[1];
+        str_for_add = input.strip().split("\\\\n")[1];
+        return new String[] {delimeter, str_for_add};
     }
     // 문자열 검증하는 함수
     private static void validate_str(String delimiter, String str) {
@@ -21,6 +28,18 @@ public class Application {
     }
     // 덧셈 연산을 하는 함수
     private static void add(String input) {
-
+        // 초기 구분자 세팅
+        String delimiter = "";
+        String str_for_add ="";
+        if(exist_custon_delimiter(input)) {
+            String[] tokens;
+            tokens = get_custom_delimiter(input);
+            delimiter = tokens[0];
+            str_for_add = tokens[1];
+        }
+        else {
+            delimiter = ",:";
+            str_for_add = input;
+        }
     }
 }
