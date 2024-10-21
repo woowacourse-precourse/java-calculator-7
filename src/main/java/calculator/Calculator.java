eb78ae687842de527bf481d8f2f18e7d27304dec
EoN <jaegyun1011@naver.com>
2024-10-21 17:51:50 +0900

    refactor : 예외 처리 메서드 위치 변경

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,30 +3,25 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
-    String expr;
-
     public void run() {
-        boolean valid = input();
-        if(valid) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        try {
+            input();
             int result = add();
             System.out.println("결과 : " + result);
-        }
-        else {
+        } catch(IllegalArgumentException e) {
             System.out.println("올바르지 않은 입력입니다.");
         }
     }
 
-    public boolean input() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        try {
-            expr = Console.readLine();
-            return true;
-        } catch(IllegalArgumentException e) {
-            return false;
-        }
+    public void input() {
+        String expr = Console.readLine();
+
     }
 
     public int add() {
-        return 0;
+        int sum = 0;
+
+        return sum;
     }
 }
