e8e885355f588f1610356742c3a08eab33b742b5
kingjinyong <wlsdydwhswkf@gmail.com>
2024-10-21 12:38:40 +0900

    refactor(CalculatorModel): 각 클래스별 기능 설명 주석 처리

--- src/main/java/calculator/model/CalculatorModel.java
+++ src/main/java/calculator/model/CalculatorModel.java
@@ -5,7 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CalculatorModel {
-
+    // 커스텀 구분자 지정 판별 기능
     public Boolean customDiscrimination(String input) {
         if (input.startsWith("//") && input.contains("\\n")) {
             return true;
@@ -16,6 +16,7 @@ public class CalculatorModel {
         }
     }
 
+    // 커스텀 구분자 추출 기능
     public String customExtraction(String customInput) {
         Pattern compiledPattern = Pattern.compile("//" + "(.*?)" + "\\\\n");
         Matcher matcher = compiledPattern.matcher(customInput);
@@ -30,11 +31,13 @@ public class CalculatorModel {
         }
     }
 
+    // 구분자들을 기준으로 연산 할 문자열 분리 해주는 기능
     public String[] operationSetting(String operationInput, ArrayList<String> separate) {
         String separatorPattern = String.join("|", separate);
         return operationInput.split(separatorPattern);
     }
 
+    // 분리 된 문자열들 더해주고 출력 해주는 기능
     public int sumCalculator(String operationInput, ArrayList<String> separateList) {
         int result = 0;
         for (String number : operationSetting(operationInput, separateList)) {
