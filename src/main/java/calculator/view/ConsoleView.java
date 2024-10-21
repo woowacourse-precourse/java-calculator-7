a78bc90a422447933d8a3e2d0bbc85f61c0146b1
Jo Myeong-geun <happy63590@gmail.com>
2024-10-21 20:47:01 +0900

    fix: 콘솔에 출력할 문구 변경

--- src/main/java/calculator/view/ConsoleView.java
+++ src/main/java/calculator/view/ConsoleView.java
@@ -5,11 +5,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class ConsoleView {
 
     public String getInput() {
-        System.out.println("덧셈할 문자열을 입력해 주세요:");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 
-    public void displayResult(int result) {
-        System.out.println("결과 : " + result);
+    public void displayResult(long result) {
+        System.out.print("결과 : " + result);
     }
 }
