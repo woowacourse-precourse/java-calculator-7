21de28b5de57276f587108ee690e610c06a10d17
unknown <saree98@naver.com>
2024-10-21 15:31:27 +0900

    feat(InputSplitter): 입력 문자열 분리기 구현
    InputSplitter에 검증 완료된 문자열을 파싱하는 기능 추가
    - 커스텀 구분자가 없으면 문자열을 기본 구분자로 분리
    - 커스텀 구분자가 있으면 구분자를 찾고 분리

--- src/main/java/calculator/InputSplitter.java
+++ src/main/java/calculator/InputSplitter.java
@@ -1,2 +1,89 @@
-package calculator;public class InputSplitter {
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputSplitter {
+    private static final String REGEX = "//.*\\\\n";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(REGEX);
+
+    public static int[] split(String s){
+
+        if(s.startsWith("/"))
+            return splitIntoOperandsWithCustomDelimiter(s);
+
+        return splitIntoOperands(s);
+    }
+
+    private static int[] splitIntoOperands(String input){
+
+        String[] segments = input.split("[,|;]");
+
+        if(doesNotHaveAnyNumber(segments))
+            return new int[]{0};
+
+        return convertOperandsIntoNumber(segments);
+    }
+
+    private static int[] splitIntoOperandsWithCustomDelimiter(String input){
+
+        String matchingPart = findMatchingPart(input);
+
+        String delimiter = extractDelimiterFrom(matchingPart);
+
+        int startIndex = matchingPart.length();
+        String numberString = detachDelimiterFrom(input, startIndex);
+
+        String[] segments = numberString.split(delimiter);
+
+        if(doesNotHaveAnyNumber(segments))
+            return new int[]{0};
+
+        return convertOperandsIntoNumber(segments);
+    }
+
+    private static int[] convertOperandsIntoNumber(String[] segments) {
+
+        List<Integer> numberList = new ArrayList<>();
+
+        for(int i = 0; i < segments.length; i++) {
+
+            if(segments[i].isEmpty())
+                continue;
+
+            try {
+                numberList.add(Integer.parseInt(segments[i]));
+            }catch (Exception e){
+                throw new IllegalArgumentException();
+            }
+
+        }
+
+        int[] result = new int[numberList.size()];
+
+        for(int i = 0; i < result.length; i++)
+            result[i] = numberList.get(i);
+
+        return result;
+    }
+
+    private static String extractDelimiterFrom(String matchingPart) {
+        return matchingPart.substring(2, matchingPart.length() - 2);
+    }
+
+    private static String detachDelimiterFrom(String s, int startIndex){
+        return s.substring(startIndex);
+    }
+
+    private static String findMatchingPart(String input) {
+        Matcher matchingResult = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        matchingResult.find();
+        return matchingResult.group();
+    }
+
+    private static boolean doesNotHaveAnyNumber(String[] operands) {
+        return operands.length == 1 && "".equals(operands[0]);
+    }
 }
