357447aced3e246b73fc4d2fbc74de3690e02e40
halfmoon-mind <5338095@gmail.com>
2024-10-21 23:11:16 +0900

    feat: main application 조합 및 주석 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputHandler.printInputRequest();
+        // 입력
+        String input = InputHandler.readInput();
+
+        // custom input 처리
+        List<Character> customInputList = CustomInputHandler.evaluateCustomInput(input);
+        input = CustomInputHandler.removeCustomInput(input);
+
+        // 구분점에 custom input에서 처리된 부분 추가
+        List<Character> divisionCharacter = new ArrayList<>(List.of(':', ','));
+        if(!customInputList.isEmpty()) {
+            divisionCharacter.addAll(customInputList);
+        }
+
+        // 계산
+        int result = Calculater.calculateInput(input, divisionCharacter);
+        OutputHandler.printResult(result);
     }
 }
