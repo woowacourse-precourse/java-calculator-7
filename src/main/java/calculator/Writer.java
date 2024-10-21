2de362226ef4a4e597a19019c216046d4016832b
최승연 <csy03178@naver.com>
2024-10-21 22:05:36 +0900

    Make Writer

---
+++ src/main/java/calculator/Writer.java
@@ -0,0 +1,10 @@
+package calculator;
+
+import java.io.IOException;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+public class Writer {
+    public static String write(){
+        return readLine();
+    }
+}
