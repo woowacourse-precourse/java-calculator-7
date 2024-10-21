6b1455d0715cb3e8601d2655bad36f91c2e8c523
JOONHOYOON <joonho9892@gmail.com>
2024-10-21 21:11:56 +0900

    feat(addition) : 기본 덧셈 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,16 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 숫자 덧셈 예시
+        int result = add(5, 7, 8, 20);
+        System.out.println("합: " + result);
+    }
+
+    public static int add(int... numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 }
