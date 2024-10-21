8535f306d9da655e8741235dda57c341d9af355b
coolman0123 <socool4m@naver.com>
2024-10-21 12:07:16 +0900

    refactor: 불필요한 확인용 출력문 삭제

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,25 +1,26 @@
 package calculator.controller;
 
-import calculator.model.NumberConverter;
 import calculator.model.Separator;
+import calculator.model.SeparatorValidator;
 import calculator.view.InputReader;
 import calculator.view.OutputPrinter;
-
-import java.util.Arrays;
 
 import static calculator.model.Separator.*;
 
 public class CalculatorController {
     public static void run(){
+
         OutputPrinter.requestInput();
         String input = InputReader.readInput();
-        System.out.println(input); //확인
 
         Calculator calculator = new Calculator();
         String token = Separator.checkCustomSeparator(input);
-        System.out.println("결말; 토큰 상태: " + token + " 활성 구분자: " + activeSeparator);
+
+        SeparatorValidator.validateInputEndsWithNumber(token, activeSeparator);
+        SeparatorValidator.validateNoConsecutiveDelimiters(token,activeSeparator);
+
         String[] tokens = splitInput(token);
-        System.out.println(Arrays.toString(tokens));
+
         int result = calculator.sum(tokens);
         OutputPrinter.printResult(result);
 
