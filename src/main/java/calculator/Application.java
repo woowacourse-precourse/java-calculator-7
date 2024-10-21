8d1d73d4fb6933faf56611743813109a6e3487d0
pang0916 <qrv22@naver.com>
2024-10-21 19:35:37 +0900

    feat(Application): add integer parsing and print sum result from String[] operands
    Added validation logic to the inputParser:
    - checks for empty inputs
    - ensures that inputs only contain digits
    - handles '+' and '-' prefixes appropriately
    
    Breaks README.md empty inputs should be allowed

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,26 +4,38 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.nio.charset.StandardCharsets;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
         System.setOut(new PrintStream(new PrintStream(System.out, true, StandardCharsets.UTF_8)));
 
-        int result;
+        int result = 0;
         String[] operands;
 
         // read input and split
-        // System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine().strip();
         operands = inputSplitter(input);
 
-        System.out.println(String.join(" ", operands));
+        // parse to int and print sum
+        for (String operand : operands) {
+            if (!operand.isEmpty()) {
+                if (Character.isDigit(operand.charAt(0))) {
+                    result += Integer.parseInt(operand);
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
 
-        result = 1;
         System.out.println("결과 : " + result);
     }
 
-    public static String[] inputSplitter(String input) throws NumberFormatException {
+    public static String[] inputSplitter(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
         String splitPattern = "[,:]";
 
         return input.split(splitPattern);
