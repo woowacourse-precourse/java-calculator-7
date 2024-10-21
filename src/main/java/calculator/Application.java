0f55a909991a135374aab45273aa131765a734ac
HeungY <rkrkrkrk02@naver.com>
2024-10-21 17:31:33 +0900

    chore: TODO 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,6 @@ import calculator.presentation.CalculatorController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         new CalculatorController();
     }
 }
