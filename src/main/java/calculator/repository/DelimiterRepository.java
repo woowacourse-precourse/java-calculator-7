d16916f3dd15a460e9e1ad1b3f2f78a928babb4e
sytTest <syt1128@naver.com>
2024-10-21 20:22:40 +0900

    :sparkle: create DelimiterRepository
    구분자들을 저장할 DelimiterRepository 생성

---
+++ src/main/java/calculator/repository/DelimiterRepository.java
@@ -0,0 +1,18 @@
+package calculator.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DelimiterRepository {
+    private final List<String> delimiters;
+
+    public DelimiterRepository() {
+        delimiters = new ArrayList<>();
+        delimiters.add(",");
+        delimiters.add(":");
+    }
+
+    public List<String> getDelimiters() {
+        return delimiters;
+    }
+}
