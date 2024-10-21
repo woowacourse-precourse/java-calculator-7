d8bfb9ef65efca20926a58cf92756a3b82c369dc
ffineapple <imffineapple@gmail.com>
2024-10-21 12:07:30 +0900

    feat(CalculatorController): 사용자가 문자열 입력하는 함수 구현

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class CalculatorController {
     private void printStartText() {
         System.out.println(Constants.MESSAGE_START_INPUT);
+    }
+
+    private String inputData() {
+        return Console.readLine();
     }
 }
