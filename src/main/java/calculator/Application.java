ffacf722035e2544a76ff5285090b1f72e0839c0
seophohoho <seop0937@gmail.com>
2024-10-21 19:06:55 +0900

    chore(Application): try-catch문 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,14 +5,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-
-        try {
-            int result = calculator.calc(input);
-            System.out.print("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
+        
+        int result = calculator.calc(input);
+        System.out.print("결과 : " + result);
     }
 }
