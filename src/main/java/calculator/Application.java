3400b374beb889dd41c08bda4393e8c13937fae1
yunho_yun <yunho_yun@naver.com>
2024-10-21 20:03:40 +0900

    feat: 문자열 입력 기능 모듈화
    view로 인터페이스 설계

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,8 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String command = Console.readLine();
-        Console.close();
+
     }
 }
