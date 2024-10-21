c0d46ccd7a4b09d9a0bd96335e68cec53a4d2593
Jhsysng <ysng1118@naver.com>
2024-10-21 21:09:20 +0900

    feat: 빈문자열 0 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = readLine();
+
+        try{
+            int result = add(input);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static int add(String input) {
+        if(input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        return 0;
     }
 }
