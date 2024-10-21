35f48a40d6d1e2607236bdcd8f4f534210b77f0e
KF97 <aysul0@naver.com>
2024-10-21 18:42:48 +0900

    refactor: 메서드 추출 리팩토링

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,45 +7,70 @@ public class Application {
      * 커스텀 구분자 처리를 위한 이스케이프 문자와 특수문자를 담은 문자열
      */
     static final String special_letters = "+-*/%=><'\"\\";
+    static String identifier = ",|:"; // 구분자를 모아놓은 문자열
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int result = 0;
-        String identifier = ",|:"; // 구분자를 모아놓은 문자열
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-
         String str = Console.readLine();
-        if (str.equals("")) { // 빈 문자열인 경우
-            System.out.println("결과 : 0");
-        } else {
-            if (str.startsWith("//")) { // 커스텀 구분자인 경우
-                int lastIndex = str.indexOf("\\n");
+        int result = process(str);
+        System.out.println("결과 : " + result);
+    }
 
-                if (lastIndex == -1) {
-                    throw new IllegalArgumentException();
-                }
+    public static int process(String str) {
+        int result = 0;
 
-                String custom = str.substring(2, lastIndex);
-
-                for (char c : custom.toCharArray()) {
-                    if (special_letters.contains(c + "")) {
-                        identifier += "|\\" + c;
-                    } else {
-                        identifier += "|" + c;
-                    }
-                }
-                str = str.substring(lastIndex + 2);
-            }
-            String[] numbers = str.split(identifier);
-            for (String s : numbers) {
-                int num = Integer.parseInt(s);
-                if (num < 0) {
-                    throw new IllegalArgumentException();
-                }
-                result += Integer.parseInt(s);
-            }
-            System.out.println("결과 : " + result);
+        if (str.isEmpty()) {
+            return result;
         }
 
+        str = customIdentifierProcess(str); // 커스텀 구분자 처리
+        return extractNumbers(str); // 계산된 값을 반환
+    }
+
+    public static String customIdentifierProcess(String str) {
+        if (str.startsWith("//")) {
+            int lastIndex = str.indexOf("\\n");
+            invalidCustomIdentifierCheck(lastIndex);
+            addCustomIdentifier(str.substring(2, lastIndex));
+            str = str.substring(lastIndex + 2); // 커스텀 구분자 문자열을 삭제
+        }
+        return str;
+    }
+
+    public static void invalidCustomIdentifierCheck(int lastIndex) {
+        if (lastIndex == -1) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void addCustomIdentifier(String custom) {
+        for (char c : custom.toCharArray()) {
+            if (special_letters.contains(c + "")) {
+                identifier += "|\\" + c; // 특수문자는 이스케이프 처리
+            } else {
+                identifier += "|" + c; // 일반문자는 그대로 추가
+            }
+        }
+    }
+
+    public static int extractNumbers(String str) {
+        return getResult(str.split(identifier));
+    }
+
+    public static int getResult(String[] numbers) {
+        int result = 0;
+        for (String s : numbers) {
+            int num = Integer.parseInt(s);
+            negativeCheck(num);
+            result += num;
+        }
+        return result;
+    }
+
+    public static void negativeCheck(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }
