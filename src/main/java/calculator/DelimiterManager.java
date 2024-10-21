b9cfaf9ead8a2c4aaeb596a581ab18c53273e9b3
Namkyung Koo <united94@naver.com>
2024-10-21 17:26:42 +0900

    feat: add comments about DelimiterManager-class

--- src/main/java/calculator/DelimiterManager.java
+++ src/main/java/calculator/DelimiterManager.java
@@ -1,28 +1,63 @@
 package calculator;
 
-/** 구분자를 관리하는 클래스. 커스텀 구분자 지정 시, 예외 처리와 커스텀 구분자 저장을 담당합니다. */
+/** 구분자와 관련된 속성, 기능을 관리하는 클래스 */
 public class DelimiterManager {
 
     /**
-     * MAX_CUSTOM_DELIMITERS: 커스텀 구분자를 포함한 구분자 문자열의 최대 길이
-     * delimiters: 구분자 문자열
-     * count: 현재 구분자 문자열의 길이
+     * DELIMITER_PREFIX : 구분자 섹션의 접두사
+     * DELIMITER_SUFFIX : 구분자 섹션의 접미사
+     * MAX_LEN_DELIMITERS : 구분자들의 최대 갯수
+     * CUSTOM_DELIMITER_LEN : 사용자 정의 구분자 길이
+     * lenOfPrefix : 구분자 섹션 접두사의 길이
+     * lenOfSuffix : 구분자 섹션 접미사의 길이
+     * customDelimiter : 사용자 정의 구분자
+     * delimiters : 구분자들
+     * count : 구분자들의 개수
      */
-    private static final int MAX_CUSTOM_DELIMITERS = 3;
+    static final String DELIMITER_PREFIX = "//";
+    static final String DELIMITER_SUFFIX = "\\n";
+    private static final int MAX_LEN_DELIMITERS = 3;
+    public static final int CUSTOM_DELIMITER_LEN = 1;
+    private int lenOfPrefix;
+    private int lenOfSuffix;
+    private char customDelimiter;
     private String delimiters;
     private int count;
 
     public DelimiterManager() {
+        lenOfPrefix = DELIMITER_PREFIX.length();
+        lenOfSuffix = DELIMITER_SUFFIX.length();
         delimiters = ",:";
         count = 2;
     }
 
-    /**
-     * 기존의 구분자 문자열에 커스텀 구분자를 더하고 구분자 개수를 1 증가 시킵니다.
-     *
-     * @param delimiter 기존의 구분자 문자열에 추가할 새로운 커스텀 구분자
-     */
-    public void addDelimiter(String delimiter) {
+    /** 멤버 변수 customdelimiter의 getter */
+    public char getCustomDelimiter() {
+        return customDelimiter;
+    }
+
+    /** 멤버 변수 lenOfPrefix의 getter */
+    public int getLenOfPrefix() {
+        return lenOfPrefix;
+    }
+
+    /** 멤버 변수 lenOfSuffix의 getter */
+    public int getLenOfSuffix() {
+        return lenOfSuffix;
+    }
+
+    /** 멤버 변수 delimiters의 getter */
+    public String getDelimiters() {
+        return delimiters;
+    }
+
+    /** 멤버 변수 customdelimiter의 setter */
+    public void setCustomDelimiter(char customDelimiter) {
+        this.customDelimiter = customDelimiter;
+        addDelimiter(Character.toString(customDelimiter));
+    }
+
+    private void addDelimiter(String delimiter) {
 
         isDelimiterLimitExceeded();
         isDelimiterExists(delimiter);
@@ -31,41 +66,15 @@ public class DelimiterManager {
         count++;
     }
 
-    /**
-     * 구분자의 최대 개수를 초과하는지 확인합니다.
-     *
-     * @throws IllegalArgumentException 커스텀 구분자의 개수는 1개를 초과할 수 없습니다.
-     */
     private void isDelimiterLimitExceeded() {
-        if (count >= MAX_CUSTOM_DELIMITERS) {
+        if (count >= MAX_LEN_DELIMITERS) {
             throw new IllegalArgumentException("Only one custom delimiter is allowed.");
         }
     }
 
-    /**
-     * 매개변수로 전달받은 구분자가 이미 존재하는 구분자와 같은지 확인합니다.
-     *
-     * @param customDelimiter 입력받은 커스텀 구분자
-     * @throws IllegalArgumentException 기존의 구분자(",", ":")와 동일한 구분자는 커스텀 구분자가 될 수 없습니다.
-     */
     private void isDelimiterExists(String customDelimiter) {
         if (delimiters.contains(customDelimiter)) {
             throw new IllegalArgumentException("Custom delimiter cannot be a default delimiter (',' or ':').");
         }
-    }
-
-    /**
-     * 해당 문자가 구분자인지 확인합니다.
-     *
-     * @param c 구분자인지 확인할 문자
-     * @return 구분자인지(true) 아닌지(false) 여부
-     */
-    public boolean isDelimiter(char c) {
-        int indexOfDelimiter = delimiters.indexOf(c);
-
-        if (indexOfDelimiter == -1) {
-            return false;
-        }
-        return true;
     }
 }
