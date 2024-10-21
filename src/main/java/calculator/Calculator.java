0c870678bbeab2e27f1b13b536c5d627d2a5e4a0
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-10-21 15:24:29 +0900

    feat : 2.ë빈 문자열 입력 처리

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Calculator {
+    public int run(String input) {
+        return cal(input);
+    }
+
+    public int cal(String str) {
+        // 2. 빈 문자열 입력 처리
+        if (str.isEmpty()) return 0;
+    }
+
+}
