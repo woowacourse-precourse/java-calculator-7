5730ee360dbca5c1a2c02ca51343ba447f139240
KF97 <aysul0@naver.com>
2024-10-21 14:04:16 +0900

    feat: 음수의 값 처리 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,21 +8,21 @@ public class Application {
         int result = 0;
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        try {
-            String str = Console.readLine();
 
-            if (str.equals("")) {
-                System.out.println("결과 : 0");
-            } else {
-                String[] numbers = str.split(",|:");
-                for (String s : numbers) {
-                    result += Integer.parseInt(s);
+        String str = Console.readLine();
+        if (str.equals("")) {
+            System.out.println("결과 : 0");
+        } else {
+            String[] numbers = str.split(",|:");
+            for (String s : numbers) {
+                int num = Integer.parseInt(s);
+                if (num < 0) {
+                    throw new IllegalArgumentException();
                 }
-                System.out.println("결과 : " + result);
+                result += Integer.parseInt(s);
             }
-
-        } catch (IllegalArgumentException e) {
-            return;
+            System.out.println("결과 : " + result);
         }
+
     }
 }
