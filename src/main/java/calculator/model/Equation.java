8589a14bdf1081e05341709ea2378703821d9f1a
jollywitch <jollywitch75@gmail.com>
2024-10-21 09:43:40 +0900

    주석 추가

--- src/main/java/calculator/model/Equation.java
+++ src/main/java/calculator/model/Equation.java
@@ -7,25 +7,28 @@ public class Equation {
         parse(expression);
     }
 
+    // 입력받은 표현식을 구분자와 수식으로 분리하는 메소드
     public void parse(String expression) {
         Separator separator = new Separator();
-        var raw = separator.consumeCustomSeparator(expression);
+        var raw = separator.consumeCustomSeparator(expression); // 입력받은 문자열 중 수식을 저장
         if(!isValid(raw, separator)) {
             throw new IllegalArgumentException();
         }
-        String[] strNums = raw.split("[" + separator.raw +"]");
+        String[] strNums = raw.split("[" + separator.raw +"]"); // 수식을 구분자로 분리해 나온 숫자들을 배열로 저장
 
         nums = new Integer[strNums.length];
         for(int i = 0; i < strNums.length; i++) {
-            nums[i] = Integer.parseInt(strNums[i]);
+            nums[i] = Integer.parseInt(strNums[i]); // 숫자가 문자열 자료형이므로 정수형으로 변환
         }
     }
 
+    // 수식에 유효한 문자열이 있는지 검사하는 메소드
     public boolean isValid(String expression, Separator separator) {
-        var a = expression.replaceAll("[\\d" + separator.raw + "]", "");
-        return a.isEmpty();
+        var a = expression.replaceAll("[\\d" + separator.raw + "]", ""); // 문자열에서 유효한 문자를 모두 제거
+        return a.isEmpty(); // 문자열이 비어있는지 결과를 반환
     }
 
+    // 수식에서 분리한 숫자들을 더하는 메소드
     public Integer sum() {
         Integer sum = 0;
 
--- src/main/java/calculator/model/Separator.java
+++ src/main/java/calculator/model/Separator.java
@@ -1,24 +1,27 @@
 package calculator.model;
 
 public class Separator {
-    public String raw = ",:";
+    public String raw = ",:"; // 기본 구분자
 
+    // 구분자 문자열에 커스텀 구분자를 추가하는 메소드
     private void append(String customSeparator) {
         raw += customSeparator;
     }
 
+    // 커스텀 구분자 문자열을 분리하고 수식을 반환하는 메소드
     public String consumeCustomSeparator(String expression) {
         if(hasCustomSeparator(expression)) {
             int newlineIndex = expression.indexOf("\n");
-            String customSeparator = expression.substring(2, newlineIndex);
-            append(customSeparator);
+            String customSeparator = expression.substring(2, newlineIndex); // //와 \n 사이의 문자열을 추출
+            append(customSeparator); // 커스텀 구분자인 추출한 문자열을 구분자에 추가
 
-            return expression.substring(newlineIndex + 1);
+            return expression.substring(newlineIndex + 1); // 커스텀 구분자를 제외한 수식 부분을 반환
         }
 
-        return expression;
+        return expression; // 커스텀 구분자가 없으니 수식을 그대로 반환
     }
 
+    // 커스텀 구분자가 있는지 검사하는 메소드
     private boolean hasCustomSeparator(String expression) {
         boolean startsWithDoubleSlash = expression.startsWith("//");
         boolean containsNewline = expression.contains("\n");
