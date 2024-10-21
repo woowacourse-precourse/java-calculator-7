3fa7a463486d1ceae947fe553bd6b9e02d2a1dfc
허재원 <jaewonheo666@gmail.com>
2024-10-21 15:29:16 +0900

    fix(Calculator): "\\n" 입력을 "\n"으로 처리하여 관련 에러 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,21 +4,21 @@ public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        System.out.println("1,2,3 = " + calculator.addNumber("1,2,3"));
-        System.out.println("1:2:3 = " + calculator.addNumber("1:2:3"));
-        System.out.println("1,2:3 = " + calculator.addNumber("1,2:3"));
+        System.out.println("1,2,3 = " + calculator.addNumbers("1,2,3"));
+        System.out.println("1:2:3 = " + calculator.addNumbers("1:2:3"));
+        System.out.println("1,2:3 = " + calculator.addNumbers("1,2:3"));
 
 
-        System.out.println("//;\n1;2;3 = " + calculator.addNumber("//;\n1;2;3"));
-        System.out.println("//@\n2@3@4 = " + calculator.addNumber("//@\n2@3@4"));
+        System.out.println("//;\n1;2;3 = " + calculator.addNumbers("//;\n1;2;3"));
+        System.out.println("//@\n2@3@4 = " + calculator.addNumbers("//@\n2@3@4"));
 
-
+        System.out.println("//@\n2@3@4 = " + calculator.addNumbers("//;\\n1"));
 //        System.out.println("'' = " + calculator.addNumber(""));
 //        System.out.println("' ' = " + calculator.addNumber(" "));
 
 
-        System.out.println("//.\n1.2.3 = " + calculator.addNumber("//.\n1.2.3"));
-        System.out.println("//*\n1*2*3 = " + calculator.addNumber("//*\n1*2*3"));
+        System.out.println("//.\n1.2.3 = " + calculator.addNumbers("//.\n1.2.3"));
+        System.out.println("//*\n1*2*3 = " + calculator.addNumbers("//*\n1*2*3"));
 
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,31 @@
     package calculator;
-    import java.util.regex.Matcher;
     import java.util.regex.Pattern;
 
     public class Calculator {
+        private static final String DEFAULT_REGEX = ",|:";
+
+        public int addNumbers(String input){
+            String regEx = DEFAULT_REGEX;
+            String numbers = input;
+            if(input.startsWith("//")){
+                input = input.replace("\\n", "\n");
+                int regExIndex = input.indexOf("\n");
+                if (regExIndex == -1) {
+                    throw new IllegalArgumentException("커스텀 구분자 선언 후 개행이 필요 합니다.");
+                } else{
+                    regEx = input.substring(2, regExIndex);
+                    numbers = input.substring(regExIndex + 1);
+                }
+            }
+            return returnSum(splitNumbers(numbers, regEx));
+        }
         private String[] splitNumbers(String numbers, String regEx){
-            if(regEx.equals(",|:")){
+            if(regEx.equals(DEFAULT_REGEX)){
                 return numbers.split("[,:]");
             }
             return numbers.split(Pattern.quote(regEx));
         }
+
         public int returnSum(String[] numbers){
             int sum = 0;
             for (String num: numbers){
@@ -16,18 +33,5 @@
                 sum += n;
             }
             return sum;
-        }
-
-        public int addNumber(String input){
-            String regEx = ",|:";
-            String numbers = input;
-            if(input.startsWith("//")){
-                int regExIndex = input.indexOf("\n");
-                if (regExIndex != -1) {
-                    regEx = input.substring(2, regExIndex);
-                    numbers = input.substring(regExIndex + 1);
-                }
-            }
-            return returnSum(splitNumbers(numbers, regEx));
         }
     }
