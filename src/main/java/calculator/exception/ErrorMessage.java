4ea06f9bc03dd6b7876be66f04b9a049f77937e3
jae woo Jung <awfjol2008@gmail.com>
2024-10-21 18:59:04 +0900

    refactor: 에러 메시지를 가독성있게 분류

--- src/main/java/calculator/exception/ErrorMessage.java
+++ src/main/java/calculator/exception/ErrorMessage.java
@@ -2,12 +2,17 @@ package calculator.exception;
 
 public enum ErrorMessage {
 
+	// prefix
 	ERROR_PREFIX("[ERROR] "),
+
+	// delimiter
 	INVALID_DELIMITER("허용되지 않은 구분자가 포함되어 있습니다."),
-	NON_INTEGER_VALUE("구분자 사이에 정수가 아닌 값이 포함되어 있습니다."),
 	INVALID_CUSTOM_DELIMITER("커스텀 구분자는 정수일 수 없습니다."),
-	NON_POSITIVE_NUMBER("계산기에 입력될 숫자는 양수이어야 합니다."),
-	EMPTY_CUSTOM_DELIMITER("커스텀 구분자는 빈 문자열일 수 없습니다.");
+	EMPTY_CUSTOM_DELIMITER("커스텀 구분자는 빈 문자열일 수 없습니다."),
+
+	// calculator
+	NON_INTEGER_VALUE("계산기에 입력될 숫자는 정수이어야 합니다."),
+	NON_POSITIVE_NUMBER("계산기에 입력될 숫자는 양수이어야 합니다.");
 
 	private final String message;
 
