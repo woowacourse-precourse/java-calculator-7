bfc7ef8dad9d9fd38826334612b159abf65330ed
Seongmo Ahn <djgood780@gmail.com>
2024-10-21 22:35:16 +0900

    refactor(Calculation): 구글 자바 가이드에 따른 코드 수정
    - else 예약어 제거
    - stringCalculation(-> calculateString) 메서드 명 및 내부 변수 명 변경

--- src/main/java/calculator/Calculation.java
+++ src/main/java/calculator/Calculation.java
@@ -1,37 +1,36 @@
 package calculator;
 
 public class Calculation {
-    public static int getCalculationResult(String input){
-        if(input.isEmpty()){
+    public static int getCalculationResult(String input) {
+        if (input.isEmpty()) {
             return 0;
-        }else{
-            return stringCalculation(input);
         }
+        return calculateString(input);
     }
 
-    public static int stringCalculation(String input){
+    public static int calculateString(String input) {
         String result = "";
         int sum = 0;
         for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if (Character.isDigit(c)) {
-                result += input.charAt(i);
-                if (i != input.length() - 1 && Character.isDigit(input.charAt(i + 1))) {
+            char inputChar = input.charAt(i);
+            if (Character.isDigit(inputChar)) {
+                result += inputChar;
+                if (i != (input.length() - 1) && Character.isDigit(input.charAt(i + 1))) {
                     result += input.charAt(i + 1);
                     i++;
-                    if(i != input.length() - 1){
+                    if (i != (input.length() - 1)) {
                         continue;
                     }
                 }
                 sum += Integer.parseInt(result);
                 result = "";
-            }else{
-                if(result.isEmpty()){
-                    result = "0";
-                }
-                sum += Integer.parseInt(result);
-                result = "";
+                continue;
             }
+            if (result.isEmpty()) {
+                result = "0";
+            }
+            sum += Integer.parseInt(result);
+            result = "";
         }
         return sum;
     }
