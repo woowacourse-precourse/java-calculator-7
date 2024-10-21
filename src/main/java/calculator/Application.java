77f67e68a9b96430ac7c1cb3535584a6f4cda056
Ek, Kyungbin <eukyungbin0@gaml.com>
2024-10-21 16:12:10 +0900

    feat: 커스텀 구분자 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,39 @@
 package calculator;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
+    // TODO: 프로그램 구현
+    // 입력받은 문자열로 덧셈을 수행하는 메소드
+    public static int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String delimiter = ",|;"; // 구분자
+        String numbers = input; // 아직은 input이 문자열 타입이다.
+
+        // 커스텀 구분자
+        Matcher customDelimiter = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (customDelimiter.matches()) {
+            delimiter = customDelimiter.group(1); //커스텀 구분자를 추출한다. 
+            numbers = customDelimiter.group(2); // 커스텀 구분자 이후의 숫자들을 추출한다.
+        }
+
+
+        String[] tokens = numbers.split(delimiter); // 구분자를 기준으로 numbers를 나누고tokens에 저장
+        int sum = 0;
+
+        for (String token : tokens) {
+            int number = Integer.parseInt(token); // 양수로 변환해주기
+
+            sum += number;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Scanner sc = new Scanner(System.in);
@@ -17,24 +48,5 @@ public class Application {
             System.out.println("잘못된 입력입니다." + e.getMessage());
         }
     }
-    // 입력받은 문자열로 덧셈을 수행하는 메소드
-    public static int calculate(String input) {
-        if (input == null || input.isEmpty()) {
-            return 0;
-        }
 
-        String delimiter = ",|;"; // 구분자
-        String numbers = input; // 아직은 input이 문자열 타입이다.
-
-
-        String[] tokens = numbers.split(delimiter);
-        int sum = 0;
-
-        for (String token : tokens) {
-            int number = Integer.parseInt(token); // 양수로 변환해주기
-
-            sum += number;
-        }
-        return sum;
-    }
 }
