bdd4eaf5bc598829e4f0358fdfa74a79dda32595
KimJongMin <whdals011203@naver.com>
2024-10-21 12:30:37 +0900

    feat(Application.java): Application 클래스 구현
    Application 클래스 구현
    애플리케이션이 동작하기 위한 Parser, Calculator, Context 선언 및 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import calculator.parser.ExpressionParser;
+import calculator.parser.Parser;
+
+/**
+ * think: 이 애플리케이션에 필요한 것
+ * 1. Parser : 입력한 문자열 파싱
+ * 2.Calculator : 파싱된 문자열 계산
+ * 3. Application Context
+ */
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        
+        Parser parser = new ExpressionParser();
+        Calculator calculator = new Calculator();
+
+        Context context = new Context(parser, calculator);
+        context.start();
+
     }
 }
