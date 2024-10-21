f48dcdd3148780d78cd04161867e6b23106bbddf
Trillion_Impulse <tkddu2897@naver.com>
2024-10-21 16:53:00 +0900

    feat: add sum and output_message

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,14 +12,27 @@ public class Application {
         // 문자열 입력 받기
         String input = Console.readLine();
         
+        // 숫자 추출
         // 기본 구분자와 숫자 구분하여 숫자만 문자열 배열에 넣기
         String[] numbers = input.split(",|:");
             // 테스트 위해 구분된 숫자 출력
             /*
-            for(String number : numbers){
-                System.out.println(number);
+            for(String test : numbers){
+                System.out.println(test);
             }
             */
-        
+
+        // 더하기
+        int sum = 0; // 더한 값 저장할 변수
+
+        for (String number : numbers){
+            int num = Integer.parseInt(number); // 타입을 문자에서 숫자로 변환
+            sum += num; // 덧셈
+        }
+
+        //결과 출력
+        System.out.println("결과 : " + sum);
+
+
     }
 }
