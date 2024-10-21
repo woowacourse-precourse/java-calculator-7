d7e25fc0a8d5093ac90afae43fe29a86e5a6cb72
JJhwan7099 <ghks7099@naver.com>
2024-10-21 21:52:41 +0900

    refactor(Application.java): 전체 메서드 구성하기
    기능 목록에 해당하는 기능들을 개별 함수로 미리 분리해두었습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 입력받기
+        String input = Console.readLine();
+    }
+    // 커스텀 구분자를 추가하는지 확인하는 함수
+    private static boolean exist_custon_delimiter(String input) {
+
+    }
+    // "//"과  "\n" 사이의 커스텀 구분자를 추출하고 "\n" 뒤의 문자열을 분리하는 함수
+    private static String[] get_custom_delimiter(String input) {
+
+    }
+    // 문자열 검증하는 함수
+    private static void validate_str(String delimiter, String str) {
+
+    }
+    // 덧셈 연산을 하는 함수
+    private static void add(String input) {
+
     }
 }
