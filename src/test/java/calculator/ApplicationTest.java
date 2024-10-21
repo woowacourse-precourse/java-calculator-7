109af357f97e9dd155f6fb27f94902c75b6aa45e
gobchang7 <anchovyzzang@gmail.com>
2024-10-21 20:45:41 +0900

    docs README.md 수정

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -11,17 +11,16 @@ class ApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("//;\\n1;4;7");
+            assertThat(output()).contains("결과 : 12");
         });
     }
 
     @Test
     void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
+            assertThatThrownBy(() -> Calculator.add("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
-        );
+                    .hasMessage("음수가 포함되었습니다.");
     }
 
     @Override
