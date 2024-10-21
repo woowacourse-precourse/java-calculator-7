468f9207f4967331610a517d0ed4be406a7187d5
Park - Jeonghwan <990706leo@gmail.com>
2024-10-21 15:15:02 +0900

    feat(InputView): 입력에는 숫자가 포함되어야 한다

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,11 +1,21 @@
 package calculator.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class InputView {
 
+    private static final Pattern INPUT_NUMBER_PATTERN = Pattern.compile("^[^\\d\\s]+$");
+
     public String inputNumbers() {
         String input = Console.readLine();
+        validate(input);
         return input;
+    }
+
+    private void validate(String input) {
+        if (INPUT_NUMBER_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException("숫자를 포함해야 합니다.");
+        }
     }
 }
