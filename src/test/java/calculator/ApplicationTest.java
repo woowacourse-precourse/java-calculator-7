e42b5215acc6b6ad3dc93fb23ebcac27d3f155f1
jinhyeon <tiemo0708@naver.com>
2024-10-21 21:02:45 +0900

    test: 쉼표_콜론 동시사용, 빈문자열 입력, 하나의 숫자 입력, 기본 구분자로 여러개 숫자입력, 커스텀 구분자로 여러개 숫자 입력, 잘못된 구분자 형식 입력, 잘못된 문자 입력에 관한 테스트 코드 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -23,7 +23,63 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+    @Test
+    void 기본_구분자_쉼표_콜론_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
 
+    @Test
+    void 빈_문자열_입력() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 하나의_숫자_입력() {
+        assertSimpleTest(() -> {
+            run("5");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+    @Test
+    void 여러개의_숫자_입력_기본_구분자() {
+        assertSimpleTest(() -> {
+            run("1,2,3,4,5");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_여러개_입력() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3;4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 잘못된_구분자_형식_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("구분자 형식이 올바르지 않습니다")
+        );
+    }
+
+    @Test
+    void 잘못된_문자_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,a,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("유효하지 않은 값이 포함되어 있습니다")
+        );
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
