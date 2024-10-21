e48d1988d17b926a639ee2469a18987f0faca2c7
leestana01 <leestana01@naver.com>
2024-10-21 10:06:52 +0900

    refactor(Application): 함수 분리 진행
    테스트 코드 작성을 위한 기능 단위 함수 분리 진행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,26 +8,39 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        new Application().run();
+    }
+
+    public void run() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        Matcher matcher = Pattern.compile("//(.*)\\\\n(.*)").matcher(input);
+        String[] splitInput = splitInput(input);
+        System.out.println("결과 : " + getSum(splitInput));
+    }
 
+    public String[] splitInput(String input) {
+        Matcher matcher = Pattern.compile("//(.*)\\\\n(.*)").matcher(input);
         boolean isMatched = matcher.find();
         String delimiter = "[,:" + (isMatched ? matcher.group(1) : "") + "]";
         String numbers = isMatched ? matcher.group(2) : input;
-
-        System.out.println("결과 : " +
-            Arrays.stream(numbers.split(delimiter))
-                .map(num -> {
-                    try {
-                        int parsedNum = Integer.parseInt(num);
-                        if (parsedNum <= 0) throw new IllegalArgumentException();
-                        return parsedNum;
-                    } catch (NumberFormatException e) {
-                        throw new IllegalArgumentException();
-                    }
-                })
-                .reduce(0, Integer::sum));
-
+        return new String[]{delimiter, numbers};
     }
+
+    public Integer getSum(String[] splitInput) {
+        String delimiter = splitInput[0];
+        String numbers = splitInput[1];
+        return Arrays.stream(numbers.split(delimiter))
+            .map(this::convertToNum)
+            .reduce(0, Integer::sum);
+    }
+
+    public int convertToNum(String num) {
+        try {
+            int parsedNum = Integer.parseInt(num);
+            if (parsedNum <= 0) throw new IllegalArgumentException();
+            return parsedNum;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+	}
 }
