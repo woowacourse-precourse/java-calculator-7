3599ffd80a32babce6c17afead84658482277be7
yoonjin <skyj217@naver.com>
2024-10-21 21:56:05 +0900

    feat: 입출력, 예외 상황 기능 추가 feat: 빈 문자열 입력 시 0 반환 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,14 +5,18 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
 
     public static void main(String[] args) {
-       try{ // 입력
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
+       try { // 입력
+           System.out.println("덧셈할 문자열을 입력해 주세요.");
+           String input = Console.readLine();
 
-        // 출력
-        System.out.println("결과 : " + input);
-    }
-        catch (IllegalArgumentException e) {
+           // 빈 문자열 처리
+           if (input == null || input.isEmpty()) {
+               System.out.println("결과 : 0");
+               return;
+           }
+           // 출력
+           System.out.println("결과 : " + input);
+       } catch (IllegalArgumentException e) {
             // 예외 발생 시 프로그램 종료
             System.err.println(e);
         }
