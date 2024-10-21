efb0229f16352c378af0e056fa38e04907be549f
Park-Min-Jun <gomdole3963@gmail.com>
2024-10-21 23:51:12 +0900

    feat: 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         String input;
         String sep = "";
         String str = "";
-        int result = 0;
-        int sum = 0;
+        int sum, result = 0;
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
@@ -19,35 +17,42 @@ public class Application {
         String[] str_num = input.split(",|:");
 
         if (input_split[0].equals("/")) {
-            for (int i = 2; i < input_split.length; i++) {
-                if (input_split[0].equals("/") && input_split[1].equals("/")) {
-                    if (input_split[i].equals("\\") && input_split[i + 1].equals("n")) {
-                        break;
+            try {
+                for (int i = 2; i < input_split.length; i++) {
+                    if (input_split[0].equals("/") && input_split[1].equals("/")) {
+                        if (input_split[i].equals("\\") && input_split[i + 1].equals("n")) {
+                            break;
+                        }
+                        sep += input_split[i];
                     }
-                    sep += input_split[i];
                 }
-            }
 
-            for (int i = 4 + sep.length(); i < input.length(); i++) {
-                str = str + input_split[i];
-            }
+                for (int i = 4 + sep.length(); i < input.length(); i++) {
+                    str = str + input_split[i];
+                }
 
-            String[] input_split2 = str.split(sep);
-            for (int i = 0; i < input_split2.length; i++) {
-                result += Integer.parseInt(input_split2[i]);
+                String[] input_split2 = str.split(sep);
+                for (int i = 0; i < input_split2.length; i++) {
+                    result += Integer.parseInt(input_split2[i]);
+                }
+                System.out.println("결과 : " + result);
+            } catch (Exception e) {
+                throw new IllegalArgumentException(e);
             }
-            System.out.println("결과 : " + result);
-
         } else {
-            for (int i = 0; i < str_num.length; i++) {
-                if (Integer.parseInt(str_num[i]) >= 0) {
-                    sum = Integer.parseInt(str_num[i]);
-                    result += sum;
-                } else {
-                    throw new IllegalArgumentException("잘못된 값입니다.");
+            try {
+                for (int i = 0; i < str_num.length; i++) {
+                    if (Integer.parseInt(str_num[i]) >= 0) {
+                        sum = Integer.parseInt(str_num[i]);
+                        result += sum;
+                    } else {
+                        throw new IllegalArgumentException("잘못된 값입니다.");
+                    }
                 }
+                System.out.println("결과 : " + result);
+            } catch (Exception e) {
+                throw new IllegalArgumentException(e);
             }
-            System.out.println("결과 : " + result);
         }
     }
 }
