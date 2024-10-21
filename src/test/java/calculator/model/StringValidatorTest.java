c8c2128b8af81e572c6232fd15aab3fba6c86a17
Chan531 <chan657312@gmail.com>
2024-10-21 18:26:33 +0900

    test(StringValidatorTest): 커스텀 구분자가 포함된 문자열 테스트

--- src/test/java/calculator/model/StringValidatorTest.java
+++ src/test/java/calculator/model/StringValidatorTest.java
@@ -18,4 +18,20 @@ class StringValidatorTest {
 		// when, then
 		assertThatCode(stringValidator::validate).doesNotThrowAnyException();
 	}
+
+	@ParameterizedTest
+	@DisplayName("커스텀 구분자가 포함된 문자열이 입력되었을 때 에러 없이 이를 검증한다.")
+	@ValueSource(strings = {"//#\\n1,2:3#4", "//?\\n1?2?3", "//\\\n1\\2\\3"})
+	void 커스텀_구분자가_포함된_문자열이_입력되었을_때_검증한다(String input) {
+		// given
+		int customDelimiterIndex = 2;
+		int customDelimiterLength = 1;
+		int specifiedCustomDelimiterLength = 5;
+		String customDelimiter = input.substring(customDelimiterIndex, customDelimiterIndex + customDelimiterLength);
+		String customDelimiterRemovedInput = input.substring(specifiedCustomDelimiterLength);
+		StringValidator stringValidator = new StringValidator(customDelimiterRemovedInput, customDelimiter);
+
+		// when, then
+		assertThatCode(stringValidator::validate).doesNotThrowAnyException();
+	}
 }
