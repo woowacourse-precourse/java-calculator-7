7e594616d827c9dc779c71f633652ffe512b43ea
= <hyeonhc328@gmail.com>
2024-10-21 17:03:42 +0900

    refactor(parser): 파싱 로직에서 숫자 추출과 합산에  캡슐화를 적용해 분리함
    Parser.java 의 parse이 숫자 추출과 합산을 모두 책임지고 있어 이를 분리함
    - 숫자 추출 : parseNumber
    - 숫자 합산 : sumNenbers

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -4,18 +4,26 @@ import java.util.ArrayList;
 
 public class Parser {
     public String parse(String input, Delimiter delimiter) {
+
         int result = 0;
+        ArrayList<Integer> numbers = parseNumbers(input, delimiter);
+        result = sumNumbers(numbers, result);
+        return String.valueOf(result);
+    }
+
+    private static ArrayList<Integer> parseNumbers(String input, Delimiter delimiter) {
         String delimiters = delimiter.getDelimiters();
         ArrayList<Integer> numbers = new ArrayList<>();
-
         for (String number : input.split(delimiters)) {
             numbers.add(Integer.parseInt(number));
         }
+        return numbers;
+    }
 
+    private static int sumNumbers(ArrayList<Integer> numbers, int result) {
         for (int number : numbers) {
             result += number;
         }
-
-        return String.valueOf(result);
+        return result;
     }
 }
