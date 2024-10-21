485a54c402ab117d9244574c1cd559c68e6b38f9
sanghee0820 <lsh277604@gmail.com>
2024-10-21 21:50:55 +0900

    feat: 구분자가 숫자일 경우 발생 오류 추가

--- src/main/java/calculator/exception/CalculatorError.java
+++ src/main/java/calculator/exception/CalculatorError.java
@@ -1,7 +1,8 @@
 package calculator.exception;
 
 public enum CalculatorError {
-    INPUT_IS_NULL_OR_EMPTY("[Error] 입력 값이 존재하지 않습니다.");
+    INPUT_IS_NULL_OR_EMPTY("[Error] 입력 값이 존재하지 않습니다."),
+    DELIMITER_IS_NUMBER("[Error] 구분자는 숫자가 될 수 없습니다.");
 
     private final String message;
 
