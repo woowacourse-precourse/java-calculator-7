b2d23b978f4c61dea445c3da6cce3f5e4a8c5753
youngseo3 <wys4475@gmail.com>
2024-10-21 22:01:58 +0900

    refactor: 커스텀 구분자 예외처리 수정 문자열 앞에 5글자가 유효한지 검증한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,19 +11,17 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        /*
-        * 커스텀 구분자가 있는지 확인한다. (5문자) //와 \n 사이에 문자
-        * 커스텀 구분자가 있으면
-        * 문자열 앞의 5글자가 "//" 와 "\\"로 이루어져 있는지 확인하고 사이에 문자가 있는지 확인한다.
-        * 커스텀 구분자가 없으면
-        * 기본 구분자를 사용한다.
-        */
         if (input.startsWith("//")) { // 커스텀 구분자로 시작하는지 확인
-            // "\n"으로 끝나는지 확인
-            if (input.substring(3, 5).equals("\\n")) {
+            if (input.substring(3, 5).equals("\\n") && input.length() > 5) { // "\n"으로 끝나는지 확인
                 delimiter = input.substring(2, 3);
                 input = input.substring(5);
-                // TODO: 비어있는지 체크
+            }
+            else if (input.substring(3, 5).equals("\\n") && input.length() == 5) { // 커스텀 구분자로 지정만 한 경우
+                System.out.println("결과 : " + 0);
+                return;
+            }
+            else if (input.length() < 5) {
+                throw new IllegalArgumentException("문자열의 길이가 5보다 작습니다.");
             }
             else {
                 throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다.");
