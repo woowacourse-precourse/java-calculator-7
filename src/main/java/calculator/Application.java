77935995ced943242c31d0d39c011cbc8aca4388
sumin <grandcias123@gmail.com>
2024-10-21 21:15:30 +0900

    refactor: input 에 따라 로직 분기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,16 +3,26 @@ package calculator;
 import calculator.controller.StringCalculator;
 import calculator.view.InputView;
 import calculator.view.OutputView;
+import java.io.PrintStream;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         StringCalculator calculator = new StringCalculator();
 
         String input = inputView.getInput();
-        int sum = calculator.calculateInput(input);
-        outputView.printResult(sum);
+        if (input == null || input.trim().isBlank()) {
+            outputView.printResult(BigDecimal.valueOf(0));
+        } else {
+            BigDecimal sum = calculator.calculateInput(input);
+            outputView.printResult(sum);
+        }
+
     }
 }
