10cb1df20a66610bd50df4fb37d80b713ca97feb
Juyeon Kim <airin00100@gmail.com>
2024-10-21 22:42:29 +0900

    feat: 쉼표 및 콜론 구분자를 사용해 숫자를 분리하고 합계 구하기 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,10 +6,28 @@ public class StringCalculator {
             return 0;
         }
 
-        throw new UnsupportedOperationException("기능이 아직 구현되지 않았습니다.");
+        String[] tokens = split(text);
+        return sum(tokens);
     }
 
     private static boolean isNullOrEmpty(String s) {
         return s == null || s.isEmpty();
+    }
+
+    private static String[] split(String text) {
+        String delimiter = ",|:"; // 기본 구분자
+        return text.split(delimiter);
+    }
+
+    private static int sum(String[] tokens) {
+        int total = 0;
+        for (String token : tokens) {
+            if (token.isEmpty()) {
+                continue;
+            }
+            int num = Integer.parseInt(token);
+            total += num;
+        }
+        return total;
     }
 }
