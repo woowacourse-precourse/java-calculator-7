5f16beb3962aaee56fd049ed0b3932a1172e66b5
daxxn <dahyeonhan4529@gmail.com>
2024-10-21 15:14:45 +0900

    fix(controller): update to align with refactored model
    - Adjusted controller methods to work with the new CalculatorModel structure
    - Improved code consistency by integrating changes from model refactor

--- src/main/java/controller/CalculatorController.java
+++ src/main/java/controller/CalculatorController.java
@@ -4,8 +4,8 @@ import model.CalculatorModel;
 import view.CalculatorView;
 
 public class CalculatorController {
-    private CalculatorView view;
-    private CalculatorModel model;
+    private final CalculatorView view;
+    private final CalculatorModel model;
 
     public CalculatorController(CalculatorModel model, CalculatorView view) {
         this.model = model;
@@ -13,22 +13,15 @@ public class CalculatorController {
     }
 
     public void runController() {
-        //입력받기
         String input = view.getInput();
-
-        //커스텀 구분자 설정 및 문자열 가공
         String new_input = model.checkCustomDelimiter(input);
 
-        //빈 문자열인지 체크
-        if (new_input.isEmpty()) {
-            view.displayResult(0);
-        } else {
-            //숫자와 구분자 필터링
+        int result = 0;
+        if (!new_input.isEmpty()) {
             model.filterInput(new_input);
-            //숫자들의 합 계산
-            int result = model.addNumber();
-            //결과 출력하기
-            view.displayResult(result);
+            result = model.sumNumber();
         }
+
+        view.displayResult(result);
     }
 }
