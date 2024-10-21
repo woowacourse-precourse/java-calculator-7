deae61b4ebc96bd298810e2d14d4396ee42b9d1a
ykmxxi <ykmxxi97@gmail.com>
2024-10-21 15:03:45 +0900

    feat: 사용자 입력 null 검증을 진행한다

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -9,11 +9,22 @@ public class InputView {
         try {
             System.out.println("덧셈할 문자열을 입력해 주세요.");
             input = Console.readLine();
-            validateWhiteSpaces(input);
+            validate(input);
         } finally {
             Console.close();
         }
         return input;
+    }
+
+    private void validate(final String input) {
+        validateNull(input);
+        validateWhiteSpaces(input);
+    }
+
+    private void validateNull(final String input) {
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private void validateWhiteSpaces(final String input) {
