d513f8b8078bbce9d52af1c1e630041c142591b7
706com <ehdgusjdbswl@gmail.com>
2024-10-22 01:06:51 +0900

    [Feat] 예외처리 구현

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,5 +1,7 @@
 package calculator.service;
 
+import java.util.Arrays;
+
 public class CalculatorService {
     public String[] splitInputString(String input) throws IllegalArgumentException{
 
@@ -7,30 +9,31 @@ public class CalculatorService {
         int endIndex = -1;
         String numberInput = input;
 
-        String separator = "";
+        String separator = ",|:";
         if(input.startsWith("//")){
             endIndex = input.indexOf("\\n");
 
             for(int i=2; i<endIndex; i++){
-                separator += input.charAt(i); // 구분자 값 추출
-                if(i == endIndex-1){
-                    break;
+                if(i==2){
+                    separator += '|';
                 }
                 // 추후 정규식 적용을 위한 OR 연산자 추가
-                separator += "|";
+                char c = input.charAt(i);
+                if(c == '[' || c == ']'){
+                    separator += "\\\\";
+                }
+                separator += input.charAt(i); // 구분자 값 추출
             }
             numberInput = input.substring(endIndex+2);
         } else{
             separator = ",|:";
         }
-
         number = numberInput.split(separator);
 
         return number;
     }
 
     public int calculateSum(String[] numbers) {
-
         int sum = 0;
         if(numbers.length == 1 && numbers[0].equals("")){
             return sum;
