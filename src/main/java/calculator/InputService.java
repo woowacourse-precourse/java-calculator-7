c159cfdd3d77ad6a523ab29d19554f56a05eaf02
Cori1304-Seong <andyjm7739@gmail.com>
2024-10-21 17:06:15 +0900

    feat(InputService): change changeStrNlToNl() 기능 & 이름 변경
    - changeStrNlToNl() => parseCustomDelimiter
    
    changeStrNlToNl()에서 사용하는 replace방식은 예로가 있을 것으로 추정되어서 변경
    "//\\n\\n1\\n2\\n3"과 같은 문자열 같은 이상을 잡을 수 있을지 의문이기에 로직 변경, 구분자를 치환하지 않기에 원본에 손상이 없을 것으로 추정

--- src/main/java/calculator/InputService.java
+++ src/main/java/calculator/InputService.java
@@ -1,16 +1,30 @@
 package calculator;
 
+import calculator.model.InputData;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputService {
+    static String DEFAULT_DELIMITER = ",|:";
 
-    public static String readLine() {
+    public static InputData readLine() {
         System.out.println("숫자가 포함된 문자열을 입력하세요: ");
+        String customDelimiter = "";
+
         try {
             String input = Console.readLine();
+            if (input.startsWith("//")) {
+                int newlineIndex = input.indexOf("\\n");
 
-            String result = changeStrNlToNl(input);
-            return result;
+                if (newlineIndex == -1) {
+                    throw new IllegalArgumentException();
+                }
+                return parseCustomDelimiter(input);
+
+            }
+
+            return new InputData(input, customDelimiter.isEmpty() ? DEFAULT_DELIMITER : customDelimiter);
 
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
@@ -19,23 +33,20 @@ public class InputService {
 
     }
 
-    private static String changeStrNlToNl(String input) {
+    private static InputData parseCustomDelimiter(String input) {
         if (input.startsWith("//")) {
-            int newlineIndex = input.indexOf("\\n");
+            Matcher matcher = Pattern.compile("//(.)\\\\n(.*)").matcher(input);
+            if (matcher.find()) {
+                String delimiter = Pattern.quote(matcher.group(1)); // 커스텀 구분자 설정
+                String body = matcher.group(2); // 실제 숫자 부분 추출
 
-            if (newlineIndex == -1) {
-                throw new IllegalArgumentException();
+                return new InputData(body, delimiter);
+            } else {
+                throw new IllegalArgumentException("잘못된 입력 형식입니다.");
             }
-
-            String delimiter = input.substring(2, newlineIndex);
-
-            if (delimiter.isEmpty()) {
-                throw new IllegalArgumentException();
-            }
-
         }
-        // '\\n' new line 으로 인식하기 위해 변환;
-        return input.replace("\\n", "\n");
+        throw new IllegalArgumentException("custom delimiter 형식이 아닙니다. ");
+
     }
 
 }
