40accbbaf27efdb23d645e414dec1c02e164e00b
wlsdlf2 <wlsdlfzg@naver.com>
2024-10-21 17:13:03 +0900

    test: @ParameterizedTest 사용으로 예외 테스트 통합

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -5,61 +5,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ApplicationTest extends NsTest {
     @Test
-    void 커스텀_구분자_사용() {
+    @DisplayName("커스텀 구분자를 사용해서 계산했을 때 올바른 값을 도출한다.")
+    void 계산_성공_커스텀_구분자() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
         });
     }
 
-    @Test
-    void 예외_테스트1() {
+    @ParameterizedTest
+    @ValueSource(strings = {"-1,2,3", "//\\n1", "//;1;2", "1,2:", "1,a,2"})
+    @DisplayName("잘못된 입력값에 대해 IllegalArgumentException이 발생해야 한다.")
+    void 예외_발생_잘못된_입력(String input) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("-1,2,3"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 예외_테스트2() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("//\\n1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 예외_테스트3() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("//;1;2"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 예외_테스트4() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1,2:"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 예외_테스트5() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1,a,2"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 예외_테스트6() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("-1,2;3"))
+                assertThatThrownBy(() -> runException(input))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
