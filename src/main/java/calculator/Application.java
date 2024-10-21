b8abc2e25c49858be9f4d17c38776dcd3a95df65
hunko <rhwlgns4386@naver.com>
2024-10-21 15:19:17 +0900

    refactor : 입력 로직 InputView으로 이동

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,7 +5,7 @@ import calculator.splitter.AbstractDelimiterSplitter;
 import calculator.splitter.CustomDelimiterSplitter;
 import calculator.splitter.DefaultDelimiterSplitter;
 import calculator.splitter.DelegateDelimiterSplitter;
-import camp.nextstep.edu.missionutils.Console;
+import calculator.view.InputView;
 import java.util.List;
 
 public class Application {
@@ -14,13 +14,12 @@ public class Application {
         DelegateDelimiterSplitter splitter = new DelegateDelimiterSplitter(splitters());
         Accumulator accumulator = new Accumulator(splitter);
 
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
-        int sum = accumulator.sum(input);
+        int sum = accumulator.sum(InputView.readInput());
+
+
     }
 
     private static List<AbstractDelimiterSplitter> splitters() {
-        return List.of(new CustomDelimiterSplitter(0),
-                new DefaultDelimiterSplitter(1));
+        return List.of(new CustomDelimiterSplitter(0), new DefaultDelimiterSplitter(1));
     }
 }
