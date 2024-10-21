9e3a1731a3a71330e84f96fa4986d21f10709ded
rootTiket <l20078829@gmail.com>
2024-10-21 13:35:32 +0900

    feat: 커스텀 구분자 위치 검증 구현 커스텀 구분자가 숫자 앞이 아닌 다른 위치에 있을때 예외처리

--- src/main/java/calculator/domain/validation/DelimiterValidator.java
+++ src/main/java/calculator/domain/validation/DelimiterValidator.java
@@ -1,23 +1,27 @@
 package calculator.domain.validation;
 
-import static calculator.domain.errorMessage.parseError.INVALID_FORMAT;
+import static calculator.domain.errorMessage.ParseError.INCORRECT_POSITION;
+import static calculator.domain.errorMessage.ParseError.INVALID_FORMAT;
 
 import calculator.global.exception.CalculatorException;
 
 public class DelimiterValidator {
-    private String inputString;
+    ;
 
-    public DelimiterValidator(String inputString) {
-        this.inputString = inputString;
+    public void validateFormat(String inputString) {
+        checkDelimiterPosition(inputString);
+        checkStartDelimiter(inputString);
     }
 
-    public void validateFormat() {
-        checkStartDelimiter();
-    }
-
-    private void checkStartDelimiter() {
-        if (inputString.startsWith("//") && !inputString.contains("\\n")) {
+    private void checkStartDelimiter(String inputString) {
+        if (inputString.startsWith("//") && inputString.indexOf("\\n") != 3) {
             throw new CalculatorException(INVALID_FORMAT);
+        }
+    }
+
+    private void checkDelimiterPosition(String inputString) {
+        if (inputString.contains("//") && !inputString.startsWith("//")) {
+            throw new CalculatorException(INCORRECT_POSITION);
         }
     }
 }
