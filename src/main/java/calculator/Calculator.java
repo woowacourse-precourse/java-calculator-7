7cc26915db974986e78157d64139c9395259bd49
이원태 <dldnjsxo00524@gmail.com>
2024-10-21 20:25:36 +0900

    feat(requirement_2): add a feature processing custom delimiter
    Add a feature to allow the calculator class to process even if a custom delimiter is added to the input.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,12 +2,17 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
     String delimiter = ",|:";
 
     public void run() {
         String input = getInput();
+
+        input = addDelimiter(input);
 
         String[] delimitedInput = delimitString(input);
         int sumOfInput = sumOfList(delimitedInput);
@@ -18,6 +23,18 @@ public class Calculator {
     public String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
+    }
+
+    public String addDelimiter(String input) {
+
+        Pattern pattern = Pattern.compile("//(\\S)\\\\n");
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            this.delimiter += "|" + matcher.group(1);
+            input = matcher.replaceFirst("");
+        }
+        return input;
     }
 
     public String[] delimitString(String input) {
