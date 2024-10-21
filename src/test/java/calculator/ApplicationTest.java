de70adcfbcb7315a5cc1c7805755d8134644cc41
na0th <105775683+na0th@users.noreply.github.com>
2024-10-21 14:57:00 +0900

    test : filter 클래스가 제대로 검증하는지 테스트 케이스 추가
    추가한 내용 :
    테스트 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,13 +1,22 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+
+import static calculator.Filter.customDelimiterList;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+    @BeforeEach
+    void setUp() {
+        customDelimiterList.clear();
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -17,12 +26,166 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_구분자가_숫자인_경우_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//1\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자의_길이가_1이_아닌_경우_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//abc\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자가_공백이_아닌_경우_Input에_공백이_포함되면_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//a\\n1, 2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자가_공백이_아닌_경우_Input에_공백이_포함되면_예외_발생2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1 2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+    @Test
+    void 커스텀_구분자가_공백인_정상적인_경우() {
+        assertSimpleTest(() -> {
+            run("// \\n1 2 3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자가_마이너스_부호가_아닌_경우_Input에_음수가_포함되면_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//a\\-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자가_마이너스_부호인_정상적인_경우() {
+        assertSimpleTest(() -> {
+            run("//-\\n1-2-3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 입력_값이_없는_경우_0이_나와야_한다() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+
+    @Test
+    void 구분자가_연속으로_나오면_예외가_발생해야_한다() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_양식이_맨_처음에_나오지_않으면_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,3//a\\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 소수점이_포함된_덧셈도_계산이_되어야_한다() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("1.1,2.2"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Nested
+    class 정상적으로_입력된_경우_합_계산이_제대로_돼야_한다 {
+        @Test
+        void 정상적인_경우_1() {
+            assertSimpleTest(() -> {
+                run("//a\\n10a20a30");
+                assertThat(output()).contains("결과 : 60");
+            });
+        }
+
+        @Test
+        void 정상적인_경우_2() {
+            assertSimpleTest(() -> {
+                run("1,2,3");
+                assertThat(output()).contains("결과 : 6");
+            });
+        }
+
+        @Test
+        void 정상적인_경우_3() {
+            assertSimpleTest(() -> {
+                run("11,22:33");
+                assertThat(output()).contains("결과 : 66");
+            });
+        }
+
+    }
+
+    @Nested
+    class 구분자를_기준으로_양_옆으로_숫자가_없다면_예외가_발생해야_함 {
+        @Test
+        void 구분자를_기준으로_양_옆으로_숫자가_없다면_예외가_발생해야_한다1() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("1,2,3,"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        void 구분자를_기준으로_양_옆으로_숫자가_없다면_예외가_발생해야_한다2() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(","))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        void 구분자를_기준으로_양_옆으로_숫자가_없다면_예외가_발생해야_한다3() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(",2"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+    }
+
+    @Test
+    void 구분자_형식만_존재하는_경우_0이_나와야_한다() {
+        assertSimpleTest(() -> {
+            run("//a\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀구분자가_마이너스_부호가_아닌데_음수가_나오는_경우_예외_발생() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
 
     @Override
     public void runMain() {
