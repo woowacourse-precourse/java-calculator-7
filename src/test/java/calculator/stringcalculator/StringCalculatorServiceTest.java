d77953fecd0336558d6e419ac3f5c045c5670b66
woong2e <tlswodnd12345@naver.com>
2024-10-21 17:23:22 +0900

    test: 입력값 형식에대한 검증 테스트

---
+++ src/test/java/calculator/stringcalculator/StringCalculatorServiceTest.java
@@ -0,0 +1,37 @@
+package calculator.stringcalculator;
+
+import calculator.AppConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StringCalculatorServiceTest {
+
+    StringCalculatorService stringCalculatorService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        stringCalculatorService = appConfig.stringCalculatorService();
+    }
+
+    @Test
+    void validateInputFormTest1() {
+        String input = "/?+\\n123";
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> stringCalculatorService.validateInputForm(input));
+    }
+
+    @Test
+    void validateInputFormTest2() {
+        String input = "//+\\n\\n123";
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> stringCalculatorService.validateInputForm(input));
+    }
+
+    @Test
+    void validateInputFormTest3() {
+        String input = "k12;;3";
+        Assertions.assertDoesNotThrow(() -> stringCalculatorService.validateInputForm(input));
+    }
+}
