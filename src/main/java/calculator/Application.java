abb30d2813c509d4c7bd99ea2cc89cd8b06613d3
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-10-21 11:36:55 +0900

    feat: 구분자를 기준으로 분리한 각 숫자의 합 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import java.util.Arrays;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요");
+        String input=readLine();
+        int sum=calculateSum(input);
+        System.out.println("결과 : "+sum);
+    }
+    private static int calculateSum(String input){
+        if(input.isEmpty()) return 0;
+        String[] numbers=input.split("[,:]");
+        return sumNumbers(numbers);
+    }
+
+    private static int sumNumbers(String[] numbers) {
+        int sum=0;
+        for (String num : numbers) {
+            sum += Integer.parseInt(num.trim());
+        }
+        return sum;
     }
 }
