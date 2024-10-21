55a11df1746175c6440bc5935070bf7abb8dd3a1
euni <kongeuni1004@gmail.com>
2024-10-21 12:51:54 +0900

    test: 다양한 입력과 예외 상황에 대한 테스트 코드 추가
    - 커스텀 구분자 및 정상 입력에 대한 테스트 추가
    - 빈 문자열, 음수 입력, 유효하지 않은 구분자 등 예외 상황에 대한 테스트 작성
    - 각 테스트에서 예상되는 예외 메시지 검증 추가
    - 중복 테스트 로직 개선 및 명명 규칙 통일

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -19,10 +19,98 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 정상적인_입력테스트() {
+        assertSimpleTest(() -> {
+            run("1,2;3,4,5");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
+    @Test
+    void 쉼표만_표함된_경우_입력테스트() {
+        assertSimpleTest(() -> {
+            run("1,2,4,5");
+            assertThat(output()).contains("결과 : 12");
+        });
+    }
+
+    @Test
+    void 세미콜론만_표함된_경우_입력테스트() {
+        assertSimpleTest(() -> {
+            run("1;2;3;4;5");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
+    @Test
+    void 구분자_예외_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1/2/3/4"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 빈문자_예외_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(()-> runException("\n"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 음수인_경우_예외_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(()-> runException("1,2;-3,4;5"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_정상_입력_테스트(){
+        assertSimpleTest(() -> {
+            run("//*\\n1*2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자가_엾는_경우_예외_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(()-> runException("//\\n1234"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자가_숫자인_경우_예외_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(()-> runException("//3\\n1323334"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자가_아닌_다른_구분자를_사용할_경우_예외_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(()-> runException("//*\\n1;2;3;4"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_빈문자일_경우_예외_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(()-> runException("//*\\n"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_숫자가_음수일_경우_예외_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(()-> runException("//*\\n1*-3*1*2"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );    }
+
 
     @Override
     public void runMain() {
