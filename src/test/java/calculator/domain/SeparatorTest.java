551e75a259ad81aef593276c62b2ebd2d4023bf1
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-10-21 18:28:26 +0900

    test: 입력값의 수식 부분만 분리 기능 테스트
    - 커스텀 구분자 등록 부분이 있는 경우
    - 커스텀 구분자 등록 부분이 없는 경우

--- src/test/java/calculator/domain/SeparatorTest.java
+++ src/test/java/calculator/domain/SeparatorTest.java
@@ -24,4 +24,32 @@ class SeparatorTest {
         //then
         Assertions.assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    @DisplayName("입력값의 수식 부분만 분리 - 커스텀 구분자 등록 부분이 있는 경우")
+    void separateToFormulaPartWithCustomDelimiter() {
+        //given
+        String input = "//.\\n1,2:3;4";
+        String expected = "1,2:3;4";
+
+        //when
+        String actual = separator.separateToFormulaPart(input);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("입력값의 수식 부분만 분리 - 커스텀 구분자 등록 부분이 없는 경우")
+    void separateToFormulaPartWithNoCustomDelimiter() {
+        //given
+        String input = "1,2:3;4";
+        String expected = "1,2:3;4";
+
+        //when
+        String actual = separator.separateToFormulaPart(input);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
 }
