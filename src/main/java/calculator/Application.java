04a7f62a71be54d30c1326b47aeaaf21d1f55798
dayaelee <dayae.dev@gmail.com>
2024-10-21 17:53:22 +0900

    feat: 2-2. 커스텀 구분자를 지정한 경우를 판단하는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,30 +2,48 @@ package calculator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // 1. 사용자에게서 입력값 받기
-        // Scanner 클래스의 인스턴스 생성하기
-        Scanner scanner = new Scanner(System.in);
-        // nextLine() 메서드를 통해 입력 값 변수에 저장하기
-        String inputValue = scanner.nextLine();
+        String inputValue = Console.readLine();
+        System.out.println("inputValue: "+ inputValue);
 
-        // 2. "입력값에서 기본 구분자(쉼표, 콜론)를 통해 숫자를 추출하는 기능"
+        // 2. 입력값에서 기본 구분자(쉼표, 콜론)를 통해 숫자를 추출하는 기능
         ArrayList<Integer> result = new ArrayList<Integer>();
         String[] nums={};
-        if (inputValue.length()>1) {
-            nums = inputValue.split(",|;");
-        }
 
-        // 2-1. 입력값이 비어있을 경우 0만 추출
-        if (nums.length==0){
-            result.add(0);
-        } else{
-            for (String num: nums) {
-                result.add(Integer.parseInt(num));
+
+        if (inputValue.length()>1) {
+            // 2-2. 커스텀 구분자를 지정한 경우를 판단하는 기능
+            if (inputValue.startsWith("//")) {
+                if (inputValue.contains("\\n")){
+                    inputValue=inputValue.substring(2);
+                    String[] parts = inputValue.split("\\\\n");
+//
+//                    parts[0]
+                }else{
+                    System.out.println("??2");
+                    nums = inputValue.split(",|:");
+                }
+            }else{
+                nums = inputValue.split(",|:");
             }
         }
+
+
+
+//        // 2-1. 입력값이 비어있을 경우 0만 추출
+//        if (nums.length==0){
+//            result.add(0);
+//        } else{
+//            for (String num: nums) {
+//                result.add(Integer.parseInt(num));
+//            }
+//        }
 
 
 
