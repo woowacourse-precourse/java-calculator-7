e1d8a671fe84f7ae54f0b5968d980b8b4576b7a0
jjw0071 <jjw0071@naver.com>
2024-10-21 16:44:55 +0900

    feat:문자를 숫자로 바꾸고 에러처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,19 +5,30 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String delimiter = ",|:";
+        String customDelimiter = "";
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
         String[] s = input.split("\\\\n");
-        String customDelimiter = "";
-        if(s[0].startsWith("//")){
-            customDelimiter = s[0].substring(2);
-        }else{
-            throw new IllegalArgumentException("커스텀 구분자를 사용하려면 //로 시작해야 합니다.");
+        if(s.length>1){
+            if(s[0].startsWith("//")){
+                customDelimiter = s[0].substring(2);
+                delimiter += "|"+customDelimiter;
+            }else{
+                throw new IllegalArgumentException("커스텀 구분자를 사용하려면 //로 시작해야 합니다.");
+            }
         }
-        String totalDelimiter = delimiter+"|"+customDelimiter;
-        String[] inputNumbers = s[1].split(totalDelimiter);
+        String[] inputNumbers = s[s.length-1].split(delimiter);
+        int[] nums = new int[inputNumbers.length];
+
+        try{
+            for(int i = 0 ; i < nums.length ; i++){
+                nums[i] = Integer.parseInt(inputNumbers[i]);
+            }
+        }catch(Exception e){
+            throw new IllegalArgumentException("구분자 사이에 정상적인 숫자를 입력해야 합니다.");
+        }
         
 
 
