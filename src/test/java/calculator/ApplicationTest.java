60a05845e948f5e88207a1969100313923d16965
clickang <clickang_929@naver.com>
2024-10-21 19:29:29 +0900

    feat : 구분자 분리 및 계산 결과 도출

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,11 +8,57 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    public void 잘못된_입력_테스트() {
+        // 잘못된 입력 'a,2,3'에 대해 NumberFormatException을 기대
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("a,2,3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+
+        // 잘못된 입력 '1,,2'에 대해 NumberFormatException을 기대
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("1,,2"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    public void null_또는_빈문자() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException(""))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+
+        // 빈 문자열 입력 테스트
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException(null))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    public void 숫자하나() throws Exception {
+        assertSimpleTest(() -> {
+            run("1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    public void 쉼표구분자() throws Exception {
+        assertSimpleTest(() -> {
+            run("1,2,3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("//;\n1:2;3");
+            assertThat(output()).contains("결과 : 6");
         });
     }
 
