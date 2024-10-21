dd2daf2528c6952155fc8cbe1547a0d949323872
yallo428 <yallo428@naver.com>
2024-10-21 16:25:09 +0900

    feat(Output) 시작 메시지 및 결과 출력 메시지 구현

---
+++ src/main/java/calculator/io/Ouput.java
@@ -0,0 +1,12 @@
+package calculator.io;
+
+public class Ouput {
+
+    public void run(){
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+    }
+
+    public void result(int result){
+        System.out.println(String.format("결과 : %d", result));
+    }
+}
