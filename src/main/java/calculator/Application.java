a71485cd01d5b5f6f9b428eeb9f0539c85e706b4
bongj9 <lollypark501@naver.com>
2024-10-21 22:46:32 +0900

    feat:사용자 console을 활용하여 입력받기 & 예외처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요:");
+        String input = Console.readLine(); // 사용자 입력 받기
+
+        try {
+            int result = calculateSum(input); // 숫자 합산 수행
+            System.out.println("결과 : " + result); // 결과 출력
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
     }
 }
