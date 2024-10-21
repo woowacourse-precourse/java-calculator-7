f09b837b2aa41ede5e65071a3df5f2126ff839ce
kaswhy <sxshx818@naver.com>
2024-10-21 20:34:11 +0900

    refactor(CalculatorView): 매직 리터럴 상수화
    변경 전
    - getUserInput()의 안내 메시지를 상수화하지 않고 출력
    - displayResult()의 결과 메시지를 상수화하지 않고 출력
    
    변경 후
    - 각각을 상수화하여 출력

--- src/main/java/calculator/view/CalculateView.java
+++ src/main/java/calculator/view/CalculateView.java
@@ -3,12 +3,15 @@ package calculator.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class CalculateView {
+    private static final String INPUT_GUIDE = "덧셈할 문자열을 입력해 주세요.";
+    private static final String RESULT_MESSAGE = "결과 : ";
+
     public String getUserInput() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println(INPUT_GUIDE);
         return Console.readLine();
     }
 
     public void displayResult(int cnt) {
-        System.out.println("결과 : " + cnt);
+        System.out.println(RESULT_MESSAGE + cnt);
     }
 }
