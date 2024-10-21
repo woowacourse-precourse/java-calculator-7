2fc724ff2855a9923b2850a3a20526f43a9bf8c7
hunko <rhwlgns4386@naver.com>
2024-10-21 15:11:32 +0900

    feat : 문자열 입력 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+
+import calculator.splitter.AbstractDelimiterSplitter;
+import calculator.splitter.CustomDelimiterSplitter;
+import calculator.splitter.DefaultDelimiterSplitter;
+import calculator.splitter.DelegateDelimiterSplitter;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        DelegateDelimiterSplitter splitter = new DelegateDelimiterSplitter(splitters());
+        Accumulator accumulator = new Accumulator(splitter);
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        int sum = accumulator.sum(input);
+    }
+
+    private static List<AbstractDelimiterSplitter> splitters() {
+        return List.of(new CustomDelimiterSplitter(0),
+                new DefaultDelimiterSplitter(1));
     }
 }
