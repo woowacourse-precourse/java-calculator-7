37637eec7bfc0c281b7bc8d6bb73e2d5e5ba9ba0
zzioozzioo <zzioo1006@naver.com>
2024-10-21 16:59:31 +0900

    test: 추가적인 테스트 케이스 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,55 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 구분자가_없는_경우() {
+        assertSimpleTest(() -> {
+            run("12345");
+            assertThat(output()).contains("결과 : 12345");
+        });
+    }
+
+    @Test
+    void 기본구분자만_존재하는_경우() {
+        assertSimpleTest(() -> {
+            run(",1::2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀구분자만_존재하는_경우() {
+        assertSimpleTest(() -> {
+            run("//\"\\n0\"3");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 기본구분자와_커스텀구분자가_모두_사용된_경우() {
+        assertSimpleTest(() -> {
+            run("//#\\n3#2:,0");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+    @Test
+    void 구분자로_선언되지_않은_문자가_입력된_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//&\\n1%2:3&2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 계산가능한_범위에서_벗어난_값을_입력한_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("11223344556677889900"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
