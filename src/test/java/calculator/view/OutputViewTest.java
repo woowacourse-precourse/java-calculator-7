93f10a874c2183e907f57859f7a43b60299d3fde
Leegeonwoo7 <workgeon07@naver.com>
2024-10-21 12:01:25 +0900

    style(OutputView): 메서드 이름 명확하게 수정
    inputMessage() -> printInputMessage()
    resultMessage -> printResultMessage()

---
+++ src/test/java/calculator/view/OutputViewTest.java
@@ -0,0 +1,17 @@
+package calculator.view;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+class OutputViewTest {
+
+    @Test
+    @DisplayName("문자열을 입력받기위한 안내문구를 출력한다")
+    void printInputMessage() {
+        //given
+
+        //when
+
+        //then
+    }
+}
