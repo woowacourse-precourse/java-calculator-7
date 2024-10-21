4f2640fb22f453b8d7bb362de92bf0b1bbd82030
gyuoo <yeomky98@naver.com>
2024-10-21 22:51:13 +0900

    feat: implement calculation logic and input validation

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,11 +1,43 @@
 package calculator.service;
 
+import static calculator.exception.ErrorMessage.*;
+
 public class CalculatorService {
 
+	private static final int ZERO = 0;
 	private final InputService inputService;
 
 	public CalculatorService(InputService inputService) {
 		this.inputService = inputService;
+	}
+
+	public long calculate(String input) {
+		if (input == null || input.isEmpty()) {
+			throw new IllegalArgumentException(NULL_OR_EMPTY_INPUT.getMessage());
+		}
+		String[] numbers = inputService.splitInput(input);
+		return sum(numbers);
+	}
+
+	private long sum(String[] numbers) {
+		long total = ZERO;
+		for (String number : numbers) {
+			long parsedNumber = toLong(number);
+			total += parsedNumber;
+		}
+		return total;
+	}
+
+	private long toLong(String number) {
+		try {
+			long parsedNumber = Long.parseLong(number);
+			if (parsedNumber < ZERO) {
+				throw new IllegalArgumentException(NEGATIVE_NUMBER.getMessage()); // 음수 체크
+			}
+			return parsedNumber;
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(INVALID_INPUT.getMessage());
+		}
 	}
 
 }
