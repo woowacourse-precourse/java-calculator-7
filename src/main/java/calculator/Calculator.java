c544aa2abe12ca9ed32082f98291bf42a862c130
rkwkalrms <140880709+rkwkalrms@users.noreply.github.com>
2024-10-21 17:01:00 +0900

    Refactor: 전반적인 코드 정리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,23 +2,22 @@ package calculator;
 
 public class Calculator {
 
-    public int sumResult(String[] filteredString) {
+
+    public int result(String[] s) {
         int sum = 0;
 
-        try {
-            for(int i = 0; i<=filteredString.length-1; i++) {
-                if(Integer.parseInt(filteredString[i])< 0) {
-                    throw new IllegalArgumentException("음수는 사용할 수 없습니다.");
+        for(String number : s) {
+            try{
+
+
+                if(Integer.parseInt(number) < 0) {
+                    throw new IllegalArgumentException("음수입니다.");
                 }
-                sum += Integer.parseInt(filteredString[i]);
+                sum += Integer.parseInt(number);
+            }catch(NumberFormatException e) {
+                throw new IllegalArgumentException("잘못된 형식의 값입니다." + Integer.parseInt(number));
             }
-        } catch(NumberFormatException e) {
-            throw new IllegalArgumentException("잘못된 숫자 형식입니다.");
         }
         return sum;
     }
-
-
-
-
 }
