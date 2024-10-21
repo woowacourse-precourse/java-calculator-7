0bbd26430933e7a12248e25caadd373c1d19cccb
naraenar <hnar6ee@gmail.com>
2024-10-21 19:45:52 +0900

    fix: 문자열 커스텀 구분자 오류 수정
    커스텀 구분자를 인식하지 못하는 오류 수정

--- src/main/java/calculator/TextCalculator.java
+++ src/main/java/calculator/TextCalculator.java
@@ -16,7 +16,16 @@ public class TextCalculator {
         }
 
         // 커스텀 구분자가 형식에 맞게 있는지 검사
-        String customDelimiterRegex = "^//([^0-9\\\\]?)\\\\n[0-9,:]+$";
+        String customDelimiterDefineRegex = "^//([^0-9\\\\]?)\\\\n";
+        Pattern customDelimiterPattern = Pattern.compile(customDelimiterDefineRegex);
+        Matcher customDelimiterMatcher = customDelimiterPattern.matcher(inputText);
+        // 커스텀 구분자의 선언이 올바르지 못할 경우
+        if(!customDelimiterMatcher.find()) {
+            throw new IllegalArgumentException("문자열의 형식이 올바르지 않습니다.");
+        }
+
+        String customDelimiterRegex = "^//([^0-9\\\\]?)\\\\n[0-9,:" + customDelimiterMatcher.group(1) + "]+$";
+        // 기본 구분자, 커스텀구분자, 숫자 외 다른 문자가 있을 경우
         if(!inputText.matches(customDelimiterRegex)) {
             throw new IllegalArgumentException("문자열의 형식이 올바르지 않습니다.");
         }
