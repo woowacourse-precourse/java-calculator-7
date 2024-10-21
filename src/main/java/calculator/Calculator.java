4d8b7b7fe807aa833890db6c8b6206ec3c12a3ea
yebrong <officeguider@naver.com>
2024-10-21 22:59:20 +0900

    feat : 문자열 입력

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,22 @@
 package calculator;
+import java.util.Arrays;
 
 public class Calculator {
     private String inputData;
+    private String separator;
+    private boolean isDefault;
 
-    //
+    // 숫자 입력받았을 때 IllegalArgumentException까지 예외 처리
     public Calculator(String inputData){
         if(inputData.equals("") || inputData == null){
             this.inputData="0";
-        } else{
+            return;
+        } else if(inputData.startsWith("-")){
+            throw new IllegalArgumentException();
+        }else{
             this.inputData=inputData;
+//            getSeparator();
+
         }
     }
 
