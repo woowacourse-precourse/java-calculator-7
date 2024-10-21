a2f9a5e1b06e0fc416f8d7877ced36a9cbb9d134
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-10-21 15:19:19 +0900

    feat : 상ì사용ì 1. 입력 처리 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,13 @@
 package calculator;
 
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 1. 사용자의 입력 처리
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
     }
 }
