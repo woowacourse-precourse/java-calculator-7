6529c27071daa9b339d0f16a40563e42ffb4e05d
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-10-21 15:21:48 +0900

    feat(calculator): 각종 예외 처리 추가
    각 상황에 맞는 예외 코드 추가
    - 커스텀 입력이 잘못된 형식인 경우`IllegalArgumentException`을 발생시킨다.
    - 입력에 음수가 포함된 경우 `IllegalArgumentException`을 발생시킨다.
    - 숫자가 아닌 잘못된 값이 입력된 경우 `IllegalArgumentException`을 발생시킨다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,8 +8,13 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        int result = Calculator.add(input);
-        System.out.println("결과 : " + result);
+
+        try {
+            int result = Calculator.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
