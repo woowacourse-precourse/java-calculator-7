cba5e757cbffd86bd79a9633ad5dfaf7fd995dc8
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-10-21 21:35:04 +0900

    fix: 출력 형식 공백 수정으로 테스트 통과

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,13 +7,13 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();  // 사용자 입력 받기
 
-        System.out.println("입력값: " + input);  // 입력된 값을 출력해서 확인
+        // System.out.println("입력값: " + input);  // 입력된 값을 출력해서 확인
 
         try {
             int result = StringCalculator.add(input);  // StringCalculator의 add 메서드 호출
-            System.out.println("결과: " + result);  // 결과 출력
+            System.out.println("결과 : " + result);  // 결과 출력
         } catch (IllegalArgumentException e) {
-            System.out.println("오류: " + e.getMessage());  // 잘못된 입력일 경우 예외 메시지 출력
+            System.out.println("오류 : " + e.getMessage());  // 잘못된 입력일 경우 예외 메시지 출력
         }
     }
 }
