7347563684ce60da6594e5f7546f23683beacd66
임채연 <dlacodus0407@gmail.com>
2024-10-21 13:22:06 +0900

    refactor : 커스텀 구분자로 숫자를 포함할 수 없도록 합니다
    - 커스텀 구분자 판별 정규식에 숫자 외 문자와 매칭하도록 하여, replace가 되지 않도록 하여, 후에 isContainInvalidChar에서 거를 수 있도록 합니다

--- src/main/java/calculator/common/constant/RegexPatterns.java
+++ src/main/java/calculator/common/constant/RegexPatterns.java
@@ -3,5 +3,5 @@ package calculator.common.constant;
 public class RegexPatterns {
     public static final String START_WITH_WORD = "^[^\\d]";
     public static final String END_WITH_WORD = "[^\\d]$";
-    public static final String CUSTOM_DELIMITER = "^//(.*?)\\\\n";
+    public static final String CUSTOM_DELIMITER = "^//(\\D*?)\\\\n";
 }
