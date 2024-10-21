79039ae2d02d1d0ceb26a31046f0efa42a7c3458
Leegeonwoo7 <workgeon07@naver.com>
2024-10-21 12:35:41 +0900

    test(OutputViewTest): OutputView 테스트 추가
    -@BeforeEach, @AfterEach로 기존 Stream자원 관리
    - OutputView의 INPUT_MESSAGE 출력 테스트
    - OutputView의 RESULT_MESSAGE + 결과 출력 테스트

--- src/test/java/calculator/view/OutputViewTest.java
+++ src/test/java/calculator/view/OutputViewTest.java
@@ -1,17 +1,61 @@
 package calculator.view;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 class OutputViewTest {
+
+    OutputView outputView = new OutputView();
+    PrintStream originStream;
+
+    @BeforeEach
+    void setUp() {
+        originStream = System.out;
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(originStream);
+    }
 
     @Test
     @DisplayName("문자열을 입력받기위한 안내문구를 출력한다")
     void printInputMessage() {
         //given
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
 
         //when
+        outputView.printInputMessage();
 
         //then
+        Assertions.assertThat(outputStream.toString().trim())
+                .isEqualTo("덧셈할 문자열을 입력해 주세요.");
+
+        System.setOut(originStream);
     }
+
+    @Test
+    @DisplayName("연산 결과를 출력한다")
+    void printResultMessage() {
+        //given
+        String number = "15";
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        //when
+        outputView.printResultMessage(number);
+
+        //then
+        Assertions.assertThat(outputStream.toString().trim())
+                .isEqualTo("결과 : " + number);
+    }
+
 }
