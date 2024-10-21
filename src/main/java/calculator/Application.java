aae9d444179787f86b155b74514d9b8d1121e158
Lee Jongmin <ljm0813b@gmail.com>
2024-10-21 22:12:59 +0900

    <feat><Application.java>: ,와 :을 구분자를 처리하여 덧셈 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,31 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = sc.nextLine();
+
+        int result = calc(input);
+        System.out.println("결과 : " + result);
+
+        sc.close();
+    }
+
+    public static int calc(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        int result = 0;
+        String[] numbers = input.split("[,:]");
+
+        for (String num : numbers) {
+            result += Integer.parseInt(num.trim());
+        }
+        return result;
     }
 }
