010302bb51f4cd10af8d4129a85b28ef3d230830
leesh <leesh7807@gmail.com>
2024-10-21 19:13:50 +0900

    refactor: delete getInput method and do preprocess in constructor
    - To remove risk make StringCalculator's expression field null

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,19 +2,18 @@ package calculator;
 
 import java.util.Arrays;
 
+/**
+ * 문자열을 숫자 배열로 변환하여 계산을 수행하는 클래스.
+ *
+ * <p>문자열로 초기화되며 생성된다.
+ * 문자열의 배열 변환은 {@link DelimiterParser#parseInput(String)}
+ * 을 통해 이루어진다.</p>
+ */
 public class StringCalculator {
     private final DelimiterParser delimiterParser = new DelimiterParser();
-
     private int[] expression;
 
-    /**
-     * 계산을 위한 문자열을 받아들인다.
-     *
-     * <p>{@link DelimiterParser#parseInput(String)}을 이용하여 문자열을 분리하고
-     * {@code int[]}로 변환하여 저장한다.</p>
-     * @param input 계산을 원하는 문자열.
-     */
-    public void getInput(String input) {
+    public StringCalculator(String input) {
         preprocessOnInput(input);
     }
 
