dd4367ea8e72cb243fa33ec8baeb7e4e7285b38b
조윤재 <devilishvoice@inha.edu>
2024-10-21 17:16:41 +0900

    docs :요구사항명세 기입(2)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,6 @@
 package calculator;
 
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -9,49 +10,52 @@ public class Application {
         //2. 커스텀 구분자 //과 \n 사이에 있는 문자는 구분자로 인식.
         //3. 잘못된 값이 입력될 경우, illegalArgumentException 발생 후 애플리케이션 종료.
 
-//        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String str = readLine(); //"1,2,3" or //;\\n1;2;3 input = separator and positive integer
-        //String str ="//;\\n1";
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = readLine();
 
-        // (\n -> \\n) -> \\\\n -> \\\\\\\\n
-
-        try{
-            if(str.length() == 0){
+        try {
+            if (str.length() == 0) {
                 System.out.println("결과 : 0");
-            }else if(str.startsWith("//")){
-            String[] parts = str.split("//|\\\\n"); // "//" 또는 "\n"로 나누기
+            } else if (str.startsWith("//")) {
+                String[] parts = str.split("//|\\\\n"); // "//" 또는 "\n"로 나누기
+                String output = str.split("\\\\n")[1]; //\n 이후의 문자열을 커스텀 구분자와 기본 구분자 ,와 |로 구분 (문자열 인덱스)
 
-            //String separator = str.split("//|\\\\n")[1]; //Divider = ; 지정 (문자열 스플릿 > 인덱스)
-            String output = str.split("\\\\n")[1]; //\n 이후의 문자열을 커스텀 구분자와 기본 구분자 ,와 |로 구분 (문자열 인덱스)
-//;\\n1
-            String arr[] = output.split("["+parts[1] + "|,|:"+"]");
-            //String p = output.replaceAll(separator,",");
-            //String arr[] = p.split(",|:");
-
-            int sum = 0;
-            for (String cut : arr) {
-                int num = Integer.valueOf(cut);
-                if (num < 0){
-                    throw new IllegalArgumentException("negative value");
+                if (parts[1].equals("")) {
+                    throw new IllegalArgumentException("invaild value");
                 }
-                sum += num;
-            }
-            System.out.println("결과 : " + sum);
 
-        }else{
+                String arr[] = output.split("[" + parts[1] + "|,|:" + "]");
+                //String p = output.replaceAll(separator,",");
+                //String arr[] = p.split(",|:");
+
+                int sum = 0;
+
+                for (String cut : arr) {
+                    if (!Validate.isInteger(cut)) {
+                        throw new IllegalArgumentException("invaild character");
+                    }
+                    int num = Integer.valueOf(cut);
+                    if (num < 0) {
+                        throw new IllegalArgumentException("negative value");
+                    }
+                    sum += num;
+                }
+                System.out.println("결과 : " + sum);
+
+            } else {
                 String arr[] = str.split(",|:");
                 int sum = 0;
 
                 for (String cut : arr) {
                     int num = Integer.valueOf(cut);
-                    if (num < 0){
+                    if (num < 0) {
                         throw new IllegalArgumentException("negative value");
                     }
                     sum += num;
                 }
                 System.out.println("결과 : " + sum);
             }
-        }catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             System.out.println("잘못된 입력 : " + e.getMessage());
             throw e;
         }
