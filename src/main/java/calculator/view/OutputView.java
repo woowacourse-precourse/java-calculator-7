aabdadfd266e04b4c3bd664ce9eb6bfaede9d8ec
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-21 20:45:55 +0900

    Feat: DTO 적용으로 인한 출력 메서드 파라미터 변경

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,5 +1,7 @@
 package calculator.view;
 
+import calculator.service.dto.SumDto;
+
 public class OutputView {
 
     private static final String MESSAGE = "덧셈할 문자열을 입력해 주세요.";
@@ -9,7 +11,7 @@ public class OutputView {
         System.out.println(MESSAGE);
     }
 
-    public static void printResult(final int sum) {
-        System.out.println(RESULT + sum);
+    public static void printResult(SumDto sumDto) {
+        System.out.println(RESULT + sumDto.value());
     }
 }
