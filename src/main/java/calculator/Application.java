ac80cbe95090185d161e8dc6b8726e79bb673ce9
Devheun <dudwo5600@korea.ac.kr>
2024-10-21 19:41:57 +0900

    refactor: refactor original code
    - refactor existing code into separate classes
    - divide responsibilities

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,53 +1,14 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
-        if (input.isEmpty()) { // 빈 문자열 입력 시 0 출력
-            System.out.println("결과 : 0");
-            return;
-        }
+        InputHandler inputHandler = new InputHandler();
+        String input = inputHandler.getInput();
 
-        String delimiter = ",|:";
-        String values = "";
+        Calculator calculator = new Calculator(new StringSpliter());
+        int result = calculator.calculate(input);
 
-        Pattern customDelimiterPattern = Pattern.compile("//(.)\\\\n(.*)");
-        Matcher matcher = customDelimiterPattern.matcher(input);
-        if (matcher.matches()) { // 입력값이 해당 정규표현식과 완벽하게 일치하는지 확인
-            String customDelimiter = matcher.group(1);
-            if (!"-".equals(customDelimiter) && !customDelimiter.matches("\\d")) { // 음수기호 및 숫자 제외
-                delimiter += "|" + customDelimiter;
-                values = matcher.group(2);
-            }
-        }
-
-        String[] splitValues = values.split(delimiter);
-
-        int sum = 0;
-        for (String value : splitValues) {
-            if (!isPositiveInteger(value)) {
-                throw new IllegalArgumentException();
-            }
-            sum += Integer.parseInt(value);
-        }
-
-        System.out.println("결과 : " + sum);
-    }
-
-    // 문자열을 양수로 변환할 수 있는지 체크
-    private static boolean isPositiveInteger(String s) {
-        try {
-            // parseInt 안되면 NumberFormatException 발생
-            int number = Integer.parseInt(s);
-            return number > 0;  // 양수만 통과
-        } catch (NumberFormatException e) {
-            return false;
-        }
+        System.out.println("결과 : " + result);
     }
 }
