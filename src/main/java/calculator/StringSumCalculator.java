9daf1ff16496f27bddb78cdd81e07c9d4fa5590f
유윤지 <ll.o.llwhkr@gmail.com>
2024-10-21 20:17:12 +0900

    refactor: 커스텀구분자 if-else문 수정

--- src/main/java/calculator/StringSumCalculator.java
+++ src/main/java/calculator/StringSumCalculator.java
@@ -32,12 +32,12 @@ public class StringSumCalculator {
         String customDelimiters = input.substring(2, delimiterIdx);
         String numbersWithDelimiter = input.substring(delimiterIdx + 1);
 
-        if (customDelimiters.startsWith("[") && customDelimiters.endsWith("]")) {
+        if (!customDelimiters.startsWith("[") || !customDelimiters.endsWith("]")) {
+            return numbersWithDelimiter.split(Pattern.quote(customDelimiters));
+        } else {
             String[] delimiters = customDelimiters.substring(1, customDelimiters.length() - 1).split("]\\[");
             String delimiterPattern = String.join("|", Pattern.quote(delimiters[0]), Pattern.quote(delimiters[1]));
             return numbersWithDelimiter.split(delimiterPattern);
-        } else {
-            return numbersWithDelimiter.split(Pattern.quote(customDelimiters));
         }
     }
 
