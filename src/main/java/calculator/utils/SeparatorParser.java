f8767748575b2cc04f48f05a432cfde337db6f5b
ppparkta <pppppark2@gmail.com>
2024-10-21 19:27:04 +0900

    feat(SeparatorParser): 구분자 파싱 class getter 추가
    변경 사항
    - SeparatorParser로 나누는 과정에서 Parser에게 필요한 getter 추가
    - Parser가 담당하던 parseCustomSeparator, addBasicSeparator 메서드 SeparatorParser로 이동

--- src/main/java/calculator/utils/SeparatorParser.java
+++ src/main/java/calculator/utils/SeparatorParser.java
@@ -2,10 +2,55 @@ package calculator.utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class SeparatorParser {
     private final List<String> separator = new ArrayList<>();
     private String removedString = "";
 
-    
+    private void addBasicSeparator() {
+        separator.add(",");
+        separator.add(":");
+    }
+
+    private void parseCustomSeparator(String inputString) {
+        Stack<String> buffer = new Stack<>();
+        boolean flag = false;
+        int idx = 0;
+
+        for (int i = 0; i <= inputString.length() - 1; i++) {
+            if (!flag && (inputString.charAt(i) == '/' && inputString.charAt(i + 1) == '/')) {
+                buffer.push(inputString.substring(i, i + 2));
+                separator.add("");
+                flag = true;
+                i += 1;
+            } else if (inputString.charAt(i) == '\\' && inputString.charAt(i + 1) == 'n') {
+                if (buffer.isEmpty() || !flag) {
+                    throw new IllegalArgumentException("커스텀 구분자의 짝이 맞지 않습니다.");
+                }
+                buffer.pop();
+                idx++;
+                flag = false;
+                i += 1;
+            } else {
+                if (flag) {
+                    separator.set(idx, separator.get(idx) + inputString.charAt(i));
+                } else {
+                    removedString += inputString.charAt(i);
+                }
+            }
+        }
+
+        if (!buffer.isEmpty()) {
+            throw new IllegalArgumentException("커스텀 구분자의 짝이 맞지 않습니다.");
+        }
+    }
+
+    public List<String> getSeparator() {
+        return separator;
+    }
+
+    public String getRemovedString() {
+        return removedString;
+    }
 }
