203848b1faf79282679cc83648d825085b8230fc
yokxim <yooksi53@gmail.com>
2024-10-21 21:50:49 +0900

    [feat] initial commit
    main: Scanner를 사용해서 문자열 입력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import calculator.tool.JavaCalculator;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        JavaCalculator calculator = new JavaCalculator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = scanner.nextLine();
+        try {
+            int result = calculator.solution(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+
---
+++ src/main/java/calculator/tool/JavaCalculator.java
@@ -0,0 +1,7 @@
+package calculator.tool;
+
+public class JavaCalculator {
+    public int solution(String input) {
+        return 0;
+    }
+}
