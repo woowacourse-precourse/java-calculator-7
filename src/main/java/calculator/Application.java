eb78eeb05fdbf1eff6dfcd0960f3a4bcafc8fb24
Jhsysng <ysng1118@naver.com>
2024-10-21 21:16:05 +0900

    feat: System out 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,11 +7,12 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = readLine();
 
         try{
             int result = add(input);
-            System.out.println(result);
+            System.out.println("결과 : "+result);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
