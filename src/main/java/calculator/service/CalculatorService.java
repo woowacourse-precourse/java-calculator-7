9b2bf559b82ee25befe4dd2a67603bb0ccccba8a
Jo Myeong-geun <happy63590@gmail.com>
2024-10-21 18:40:42 +0900

    feat: 구분한 숫자를 더하는 기능 추가

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -6,9 +6,24 @@ import java.util.List;
 public class CalculatorService {
 
     public List<String> splitByDelimiters(String input) {
-        String delimiter = ",|:"; // 기본 구분자
+        String delimiter = ",|:";  //기본 구분자
+
+        //커스텀 구분자 탐색
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\n");
+            String customDelimiter = input.substring(2, delimiterIndex);
+            delimiter = delimiter + "|" + customDelimiter;  //기본 구분자에 커스텀 구분자 추가
+            input = input.substring(delimiterIndex + 1);  //문자열에서 커스텀 구분자 부분 제거
+        }
+
         return Arrays.stream(input.split(delimiter))
                 .toList();
+    }
+
+    public long sum(List<String> splitInput) {
+        return splitInput.stream()
+                .mapToLong(Long::parseLong)
+                .sum();
     }
 }
 
