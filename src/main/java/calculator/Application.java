21d222a8286294a2d13f4ad8ab22d618df8e96fa
hdsjjw <bisha0821@gmail.com>
2024-10-21 18:45:52 +0900

    feat(string-utils): added function to split strings by delimiter
    Added a new function to split strings based on a given delimiter:
    - Handles cases with multiple delimiters
    - Returns an array of substrings
    - Supports both single and multi-character delimiters
    
    BREAKING CHANGE: The previous `splitByComma` function has been removed. Use the new generic `splitByDelimiter` function instead.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+    public static List<String> splitByCommaOrColon(String input) {
+        // 정규식을 사용하여 , 혹은 : 로 문자열을 분리
+        String[] parts = input.split("[,:]");
+        return Arrays.asList(parts);
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String string;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요");
+        string= sc.nextLine();;
+
+        List<String> result = splitByCommaOrColon(string);
+        System.out.println(result);
     }
 }
