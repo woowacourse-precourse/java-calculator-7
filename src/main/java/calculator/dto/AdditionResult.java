feec45c658333ba62cb57e8427b796f2b7f7c809
DongHoon Lee <dhl1924@naver.com>
2024-10-21 15:48:41 +0900

    refactor(AdditionResult): 더 큰수를 계산하도록 하기 위해 long 타입으로 형변환 하도록 변경

--- src/main/java/calculator/dto/AdditionResult.java
+++ src/main/java/calculator/dto/AdditionResult.java
@@ -3,7 +3,7 @@ package calculator.dto;
 public record AdditionResult(String result) {
     public static AdditionResult from(double result) {
         if (result % 1 == 0) {
-            return new AdditionResult(String.valueOf((int) result));
+            return new AdditionResult(String.valueOf((long) result));
         }
         return new AdditionResult(String.valueOf(result));
     }
