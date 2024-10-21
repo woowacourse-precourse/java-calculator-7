1cd5ad276cfdcc503d7512e599f9d123f2984b90
imjaehyeong <dlawogud517@gmail.com>
2024-10-21 15:25:16 +0900

    feat: 예외처리
    입력값이 음수 인 경우에 대한 예외처리 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,34 +1,40 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        String input = Console.readLine();
         String[] splitedNumber;
-        int sum = 0;
         //  //;\n1;2;3
-        try {
-            if (input.startsWith("//") && input.startsWith("\\n", 3)) {
-                char sperator = input.charAt(2);
-                String sparatorString = String.valueOf(sperator);
-                splitedNumber = input.substring(5).split(sparatorString);
 
-            } else {
-                splitedNumber = input.split("[:,]");
-
-            }
-        } catch (NumberFormatException e) {
-            throw new RuntimeException(e);
+        if (input.startsWith("//") && input.startsWith("\\n", 3)) {
+            char sperator = input.charAt(2);
+            String sparatorString = String.valueOf(sperator);
+            splitedNumber = input.substring(5).split(sparatorString);
+            checkNegative(splitedNumber);
+        } else {
+            splitedNumber = input.split("[:,]");
+            checkNegative(splitedNumber);
         }
-        System.out.println(getSum(splitedNumber));
-
+        System.out.println("결과 : " + getSum(splitedNumber));
     }
-    static int getSum(String[] array){
-        int sum=0;
+
+    private static void checkNegative(String[] splitedNumber) {
+        Boolean hasNegative = Arrays.stream(splitedNumber).anyMatch(n -> Integer.valueOf(n) < 0);
+        if (hasNegative) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    static int getSum(String[] array) {
+        int sum = 0;
         for (String number : array) {
-            int num = Integer.valueOf(number);
+            int num = Integer.parseInt(number);
             sum += num;
         }
         return sum;
