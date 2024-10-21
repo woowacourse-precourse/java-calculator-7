0e93ee077091a00ffb65e2e28d75c5dd71e98a37
minjcho <117441212+minjcho@users.noreply.github.com>
2024-10-21 18:12:22 +0900

    feat: 사용자 입력 받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        // 이후 단계에서 처리할 메서드 호출 예정
     }
 }
