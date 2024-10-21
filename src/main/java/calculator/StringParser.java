19949cd523032aae5c4a6e597e0969aeefe47ed9
SECHANG1412 <binaryblitz1412@gmail.com>
2024-10-21 18:33:18 +0900

    git add src/main/java/calculator/StringParser.java git commit -m "feat: 구분자를 기준으로 문자열을 파싱하는 StringParser 클래스 추가"

--- src/main/java/calculator/StringParser.java
+++ src/main/java/calculator/StringParser.java
@@ -11,7 +11,7 @@ public class StringParser {
     public String[] parse(String input) {
         if (input.startsWith("//")) {
             int delimiterIndex = input.indexOf("\n");
-            input = input.substring(delimiterIndex + 1);  // 구분자 이후의 문자열로 변환
+            input = input.substring(delimiterIndex + 1);  // "\n" 이후 숫자 추출
         }
         return input.split(delimiter.getDelimiter());
     }
