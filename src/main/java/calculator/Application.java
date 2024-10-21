ad6284168d583323359bb82786a03043c6cc7091
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-10-21 18:54:01 +0900

    feat: 문자열 계산기 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.domain.Calculator;
+import calculator.domain.DelimiterExtractor;
+import calculator.domain.Separator;
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.ResultView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+
+        DelimiterExtractor delimiterExtractor = new DelimiterExtractor();
+        Separator separator = new Separator();
+        Calculator calculator = new Calculator();
+
+        CalculatorService calculatorService = new CalculatorService(calculator, delimiterExtractor, separator);
+
+        CalculatorController controller = new CalculatorController(inputView, resultView, calculatorService);
+        controller.run();
     }
 }
