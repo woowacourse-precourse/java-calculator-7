c92a52caeabb2d975bbbde8cc8f5021d7211d467
diaperi0901@gmail.com <diaperi0901@gmail.com>
2024-10-21 11:03:50 +0900

    feat(CalculatorService): 빈 문자열 또는 null 입력 처리 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -2,7 +2,7 @@ package calculator.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class CalculatorView {
+public class InputView {
     public String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
