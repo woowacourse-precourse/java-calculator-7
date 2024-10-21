df841dad3c00bc8966b18660c5abe590c9d3a15a
chaeshee0908 <chaeshee0908@naver.com>
2024-10-21 23:50:30 +0900

    fix: 최종구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,52 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import calculator.controller.CalculatorController;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
 //        CalculatorController c = new CalculatorController();
 //        c.calculateString();
 
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = readLine();
+
+        // 입력이 비어있을 경우 처리
+        if (input == null || input.isEmpty()) {
+            System.out.println("결과 : 0");
+            return;
+        }
+
+        // 기본 구분자
+        String delimiter = ",|:";
+
+        // 커스텀 구분자 확인
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\n");
+
+            if (delimiterIndex == -1) {
+                throw new IllegalArgumentException("[ERROR] 구분자를 찾을 수 없습니다.");
+            }
+
+            delimiter = Pattern.quote(input.substring(2, delimiterIndex));
+            input = input.substring(delimiterIndex + 1);
+        }
+
+        String[] numbers = input.split(delimiter);
+        int sum = Arrays.stream(numbers)
+                .mapToInt(num -> {
+                    int parsedNumber = Integer.parseInt(num.trim());
+                    if (parsedNumber < 0) {
+                        throw new IllegalArgumentException("[ERROR] 양수를 입력해야 합니다." + parsedNumber);
+                    }
+                    return parsedNumber;
+                })
+                .sum();
+
+        System.out.println("결과 : " + sum);
 
     }
 }
--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,9 +1,13 @@
 package calculator.controller;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import calculator.domain.Calculator;
 import calculator.domain.Extractor;
 import calculator.view.InputView;
 import calculator.view.OutputView;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class CalculatorController {
 
