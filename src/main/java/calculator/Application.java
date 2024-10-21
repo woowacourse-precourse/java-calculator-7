5e79b3fa4cbad7850992ebb79f426761cc17d134
Yook Halim <yookhalim@Yookui-MacBookPro.local>
2024-10-21 23:38:46 +0900

    feat: ste3- custom  sum

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,27 +9,53 @@ public class Application {
         int arrIndex = 0;
         String intStr = "";
 
+        //빈문자열일 경우 0 반환
         if(str.isEmpty()){
             return 0;
-        }
-        for (int i = 0; i < str.length(); i++){
-            if(str.charAt(i)==',' || str.charAt(i)==':'){
-                //구분자로 나눈 값 숫자로 변환하여 더하기
-                intStr = str.substring(arrIndex, i);
-                if(intStr.isEmpty() || strToNum(intStr)== -1){
-                    return -1;
-                }else{
-                    sum += strToNum(intStr);
-                }
-                arrIndex = i + 1;
-            }
-        }
-        intStr = str.substring(arrIndex);
-        sum += strToNum(intStr);
+        //커스텀 구분자일 경우
+        }else if(str.startsWith("//")){
+            char sep = str.charAt(2);
+            str = str.substring(5);
 
+            for (int i = 0; i < str.length(); i++){
+                if(str.charAt(i)==sep){
+                    //구분자로 나눈 값 숫자로 변환하여 더하기
+                    intStr = str.substring(arrIndex, i);
+                    if(intStr.isEmpty() || strToNum(intStr)== -1){
+                        //잘못된 입력일 경우 -1 반환
+                        return -1;
+                    }else{
+                        sum += strToNum(intStr);
+                    }
+                    arrIndex = i + 1;
+                }
+            }
+            //구분된 문자열 중 맨 마지막 문자열 포함
+            intStr = str.substring(arrIndex);
+            sum += strToNum(intStr);
+        // 쉼표 또는 세미콜론일 경우
+        }else{
+            for (int i = 0; i < str.length(); i++){
+                if(str.charAt(i)==',' || str.charAt(i)==':'){
+                    //구분자로 나눈 값 숫자로 변환하여 더하기
+                    intStr = str.substring(arrIndex, i);
+                    if(intStr.isEmpty() || strToNum(intStr)== -1){
+                        //잘못된 입력일 경우 -1 반환
+                        return -1;
+                    }else{
+                        sum += strToNum(intStr);
+                    }
+                    arrIndex = i + 1;
+                }
+            }
+            //구분된 문자열 중 맨 마지막 문자열 포함
+            intStr = str.substring(arrIndex);
+            sum += strToNum(intStr);
+        }
         return sum;
     }
 
+    //문자열 -> 숫자 변환
     public static int strToNum(String str){
         String intStr = "";
         for (int i = 0; i < str.length(); i++){
@@ -37,9 +63,11 @@ public class Application {
             if(48 <= ch && ch <=57){
                 intStr += ch;
             }else{
+                //숫자가 아닐 경우 -1 반환
                 return -1;
             }
         }
+        //숫자로 반환
         return Integer.parseInt(intStr);
     }
 
@@ -47,9 +75,11 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         //IllegalArgumentException 예외처리를 위한 try~catch 구문
         try{
+            //문자열 입력
             String str = Console.readLine();
-
+            //SplitSum 메서드 사용하여 result에 값 저장
             int result = SplitSum(str);
+            //-1 값 반환한 경우
             if(result == -1){
                 throw new IllegalArgumentException("잘못된 입력");
             }else{
@@ -57,8 +87,6 @@ public class Application {
             }
         }catch(IllegalArgumentException e){
             System.out.println(e.getMessage());
-        }finally {
-            System.out.println("애플리케이션 종료");
         }
     }
 }
