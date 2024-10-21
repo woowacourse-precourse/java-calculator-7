a1125b2581e1626664bd55f0487d27bd24ec3407
sytTest <syt1128@naver.com>
2024-10-21 21:47:36 +0900

    :sparkles: (TotalController) add input()
    입력 받는 inputController 를 작동시키는 input()

--- src/main/java/calculator/controller/TotalController.java
+++ src/main/java/calculator/controller/TotalController.java
@@ -1,4 +1,20 @@
 package calculator.controller;
 
+import calculator.domain.UserExpression;
+
 public class TotalController {
+    private static InputController inputController;
+    private static ExpressionController expressionController;
+    private static CalcController calcController;
+    private static UserExpression userExpression;
+
+    public TotalController() {
+        inputController = new InputController();
+        expressionController = new ExpressionController();
+        calcController = new CalcController();
+    }
+
+    private static void input() {
+        userExpression = inputController.input();
+    }
 }
