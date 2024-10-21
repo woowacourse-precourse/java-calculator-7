0cb0652ccfe88f637a6d54fe985f1deb5364f0ad
ChrisP <jspark4151@gmail.com>
2024-10-21 15:30:01 +0900

    feat(viewer) : add console viewer for input and result display

---
+++ src/main/java/calculator/ConsoleView.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class ConsoleView {
+    public void DisplayInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public void DisplayResult(int result) {
+        System.out.println("결과: " + result);
+    }
+}
