887b01a8b7064388737f63f289ffa294297cf009
sytTest <syt1128@naver.com>
2024-10-21 21:05:28 +0900

    :sparkle: (Validation) add undefinedStrMixedInExpression()
    구분자와 구분자가 통일된 입력식을 입력받아 숫자 혹은 통일된 구분자 이외의 문자열 값이 있다면 `IllegalArgumentException()` 을 발생시키는 undefinedStrMixedInExpression() 추가

--- src/main/java/calculator/domain/Validation.java
+++ src/main/java/calculator/domain/Validation.java
@@ -1,4 +1,16 @@
 package calculator.domain;
 
+import java.util.List;
+
 public class Validation {
+
+    public static void undefinedStrMixedInExpression (String unified, List<String> delimiters) {
+        List<String> chars = List.of(unified.split(""));
+        if(chars.stream()
+                .filter(s -> s.matches("[0-9]")).count()
+                + chars.stream()
+                .filter(s -> s.matches(delimiters.getFirst())).count()
+                != chars.size())
+            throw new IllegalArgumentException();
+    }
 }
