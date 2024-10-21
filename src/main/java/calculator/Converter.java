52208169bdb7f608f4a9ea1cb9c16ffe750a6c6b
pil-go <pilbongx@naver.com>
2024-10-21 11:13:23 +0900

    feat(Converter): 유효하지 않은 문자 예외처리 추가

--- src/main/java/calculator/Converter.java
+++ src/main/java/calculator/Converter.java
@@ -8,7 +8,11 @@ public class Converter {
         List<Integer> convertedList = new ArrayList<>();
 
         for (String element : input) {
-            convertedList.add(Integer.parseInt(element));
+            try {
+                convertedList.add(Integer.parseInt(element));
+            }catch (NumberFormatException e) {
+                throw new IllegalArgumentException("양수와 구분자만 입력 가능합니다.");
+            }
         }
 
         return convertedList;
