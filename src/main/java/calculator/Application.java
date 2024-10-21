3ad5d55b4d1da0d2b3a62faa50b331f10a7ba53b
euni <kongeuni1004@gmail.com>
2024-10-21 11:08:51 +0900

    style: 자바 코드 컨벤션에 맞게 코드 리팩토링
    - 메서드, 변수명, 들여쓰기 및 공백을 자바 코드 컨벤션에 맞게 수정
    - 일관된 코드 스타일 유지 및 가독성 향상

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,9 +10,9 @@ public class Application {
         String input = app.getInputString();
 
         String[] slice;
-        if(input.startsWith("/")){
+        if (input.startsWith("/")) {
             slice = app.customSliceString(input);
-        }else {
+        } else {
             slice = app.sliceString(input);
         }
 
@@ -21,41 +21,41 @@ public class Application {
         app.printResult(sum);
     }
 
-    public String getInputString(){
+    public String getInputString() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        if(input == null){
+        if (input == null) {
             throw new IllegalArgumentException("빈 문자열입니다");
         }
         return input;
     }
 
-    public String[] sliceString(String input){
+    public String[] sliceString(String input) {
         if (input.matches(".*[^0-9,;].*")) {
             throw new IllegalArgumentException("유효하지 않은 문자가 포함되어 있습니다.");
         }
         return input.split("[,;]");
     }
 
-    public int[] stringArrayToIntArray(String[] slice){
+    public int[] stringArrayToIntArray(String[] slice) {
         int[] numList = new int[slice.length];
 
         for (int i = 0; i < slice.length; i++) {
-            try{
+            try {
                 int number = Integer.parseInt(slice[i]);
-                if(number < 0){
+                if (number < 0) {
                     throw new IllegalArgumentException("음수가 있습니다");
                 }
-            }catch (NumberFormatException e){
-                throw new IllegalArgumentException(slice[i]+"는 유효하지 않은 숫자 형식입니다.");
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(slice[i] + "는 유효하지 않은 숫자 형식입니다.");
             }
             numList[i] = Integer.parseInt(slice[i]);
         }
         return numList;
     }
 
-    public int sumList(int[] numList){
+    public int sumList(int[] numList) {
         int sum = 0;
         for (int j : numList) {
             sum += j;
@@ -63,21 +63,21 @@ public class Application {
         return sum;
     }
 
-    public String[] customSliceString(String input){
-        input = input.replace("\\n","\n");
+    public String[] customSliceString(String input) {
+        input = input.replace("\\n", "\n");
         String[] s = input.split("\n");
-        if(s.length != 2){
+        if (s.length != 2) {
             throw new IllegalArgumentException("유효한 형식이 아닙니다");
         }
         String custom = s[0].substring(2);
-        if(custom.length() != 1){
+        if (custom.length() != 1) {
             throw new IllegalArgumentException("구분자가 유효한 형식이 아닙니다");
         }
         String[] slice = s[1].split(Pattern.quote(custom));
         return slice;
     }
 
-    public void printResult(int sum){
-        System.out.println("결과 : "+sum);
+    public void printResult(int sum) {
+        System.out.println("결과 : " + sum);
     }
 }
