47c01a31648a390a227aed401b2f1ae1bf61ea4e
Leegeonwoo7 <workgeon07@naver.com>
2024-10-21 18:23:49 +0900

    chore(OutputView): 메서드이름 수정 / Console 자원관리
    inputMessage() -> printInputMessage()
    resultMessage() -> printResultMessage()
    결과를 출력하는 시점에 Console.close()호출

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,15 +1,18 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class OutputView {
 
     private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
     private static final String RESULT_MESSAGE = "결과 : ";
 
-    public void inputMessage() {
+    public void printInputMessage() {
         System.out.println(INPUT_MESSAGE);
     }
 
-    public void resultMessage(String number) {
+    public void printResultMessage(String number) {
         System.out.println(RESULT_MESSAGE + number);
+        Console.close();
     }
 }
