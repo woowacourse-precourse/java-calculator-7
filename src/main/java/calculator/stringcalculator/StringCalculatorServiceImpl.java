2a1bab1b8f4d5e3b692897e66116b13f4fbc35f3
woong2e <tlswodnd12345@naver.com>
2024-10-21 22:09:40 +0900

    refactor: 구분자, 숫자문자열 저장 방법 변경

--- src/main/java/calculator/stringcalculator/StringCalculatorServiceImpl.java
+++ src/main/java/calculator/stringcalculator/StringCalculatorServiceImpl.java
@@ -1,55 +1,59 @@
 package calculator.stringcalculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculatorServiceImpl implements StringCalculatorService {
 
-    private final StringCalculatorRepository stringCalculatorRepository;
-
-    public StringCalculatorServiceImpl(StringCalculatorRepository stringCalculatorRepository) {
-        this.stringCalculatorRepository = stringCalculatorRepository;
-    }
-
     @Override
     public void validateInputForm(String input) {
-        if (!input.matches("//.+\\n.*") && !input.matches("^[0-9:,]*$")) {
-            throw new IllegalArgumentException();
+        if (input.matches("//(.*)\\n(.*)$") || input.matches("^[0-9:,]*$")) {
+            return;
         }
+        throw new IllegalArgumentException();
     }
 
     @Override
     public String calculate(String input) {
-        String delimiter = extractDelimiter(input);
+        List<String> delimiters = new ArrayList<>(Arrays.asList(";", ","));
+
+        String delimiter =  extractDelimiter(input);
+
 
         return "";
     }
 
     @Override
     public String extractDelimiter(String input) {
-        if(input.matches("//.+\\n.*")) {
-            Pattern pattern = Pattern.compile("//(.+)\\n(.*)");
+        String delimiter = "";
+        if(input.matches("//(.*)\\n(.*)$")) {
+            Pattern pattern = Pattern.compile("//(.*)\\n(.*)$");
             Matcher matcher = pattern.matcher(input);
-            String delimiter = "";
-            if (matcher.find()) {
-                delimiter = matcher.group(1);
-            }
 
-            return delimiter;
+            delimiter = matcher.group(1);
         }
 
-        return "";
+        return delimiter;
     }
 
     @Override
-    public void validateInputDelimiter(String delimiter) {
+    public String[] extractNumber(String input, List<String> delimiters) {
+        return new String[]{""};
+    }
 
+    @Override
+    public void validateInputDelimiter(String[] stringNumbers) {
 
     }
 
     @Override
-    public void extractNumber(String input) {
+    public String addStringNumbers(String[] stringNumbers) {
+        String result = "";
 
+        return result;
     }
 
 }
