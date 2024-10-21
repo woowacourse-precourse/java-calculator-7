552c6afc3e94b6563761bec50ef21c18d5794805
sinji <tobeabestns@naver.com>
2024-10-21 14:21:16 +0900

    "refactor: Scanner -> Console(라이브러리 참고사항 놓침)""

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,13 +1,13 @@
 package view;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
 
     public static String inputString() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        final String inputString = scanner.nextLine();
+        final String inputString = Console.readLine();
         return inputString;
     }
 }
+
