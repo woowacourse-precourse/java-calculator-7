1ad8d90b00bdd595ee9344e6c4e703b14da4e756
kryptonite43 <sonyj0430@gmail.com>
2024-10-21 17:40:44 +0900

    feat: 커스텀 구분자 지정 확인해 구분자 업데이트

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.String;
 
 public class Application {
     public static void main(String[] args) {
@@ -16,7 +17,10 @@ public class Application {
     public static int checkString(String input) {
         int answer = 0;
         //구분자 파악하여 저장, 구분자 목록 배열 리턴
-        getDelim();
+        char[] delims = getDelim(input);
+        for (char c:delims) {
+            System.out.println(c);
+        }
         // 문자열에서 숫자들 분류해내서 배열에 저장하기
         getNumbers();
         // 배열에 저장된 숫자들 모두 더하기
@@ -24,9 +28,23 @@ public class Application {
         return answer;
     }
 
-    public static char[] getDelim() {
-        char[] delim = new char[3];
+    public static char[] getDelim(String input) {
+        char[] delim;
+        String c1 = "//";
+        String c2 = "\\n";
 
+        String customChecker1 = input.substring(0,2);
+        String customChecker2 = input.substring(3,5);
+
+        if (customChecker1.equals(c1) && (customChecker2.equals(c2))) {
+            delim = new char[3];
+            delim[2] = input.charAt(2);
+        }
+        else {
+            delim = new char[2];
+        }
+        delim[0] = ',';
+        delim[1] = ':';
         return delim;
     }
 
