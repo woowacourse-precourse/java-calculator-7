333b56644062ea24f3fe5e8523b978700feb406b
SeongJunChun <csj991210@naver.com>
2024-10-21 13:01:55 +0900

    refactor: 구분자를 관리하는 기능 분리

---
+++ src/main/java/calculator/model/Delimiters.java
@@ -0,0 +1,35 @@
+package calculator.model;
+
+import static java.util.Collections.addAll;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Delimiters {
+    private final Set<String> delimiters = new HashSet<>();
+
+    public Delimiters(String[] delimiters) {
+        this.delimiters.addAll(Arrays.asList(delimiters));
+    }
+
+    public void addDelimiter(String delimiter) {
+        checkDelimiter(delimiter);
+        delimiters.add(delimiter);
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (String delimiter : delimiters) {
+            builder.append(delimiter).append("|");
+        }
+        builder.setLength(builder.length() - 1);
+        return builder.toString();
+    }
+
+    private void checkDelimiter(String delimiter) {
+        if (delimiter.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
--- src/main/java/calculator/service/SplitService.java
+++ src/main/java/calculator/service/SplitService.java
@@ -1,48 +1,34 @@
 package calculator.service;
 
+import calculator.model.Delimiters;
 import java.util.*;
 
 public class SplitService {
     private static final String START_POINT = "//";
     private static final String END_POINT = "\\n";
     private static final String[] BASIC_DELIMITERS = {",", ":"};
-    private final Set<String> delimiters = new HashSet<>();
+    private final Delimiters delimiters;
 
     public SplitService() {
-        delimiters.addAll(Arrays.asList(BASIC_DELIMITERS));
+        this.delimiters = new Delimiters(BASIC_DELIMITERS);
     }
 
     public List<String> split(String input) {
         String expression = extractExpression(input);
         checkStringValidity(expression);
-        String[] numbers = expression.split(delimiterSetToString());
+        String[] numbers = expression.split(delimiters.toString());
         return Arrays.stream(numbers).toList();
     }
 
     private String extractExpression(String input) {
         if (input.startsWith(START_POINT) && input.contains(END_POINT)) {
-            addDelimiter(input);
+            int startIndex = START_POINT.length();
             int endIndex = input.indexOf(END_POINT);
+            String customDelimiter = input.substring(startIndex, endIndex);
+            delimiters.addDelimiter(customDelimiter);
             return input.substring(endIndex + END_POINT.length());
         }
         return input;
-    }
-
-    private void addDelimiter(String input) {
-        int startIndex = START_POINT.length();
-        int endIndex = input.indexOf(END_POINT);
-        String customDelimiter = input.substring(startIndex, endIndex);
-        checkStringValidity(customDelimiter);
-        delimiters.add(customDelimiter);
-    }
-
-    private String delimiterSetToString() {
-        StringBuilder builder = new StringBuilder();
-        for (String delimiter : delimiters) {
-            builder.append(delimiter).append("|");
-        }
-        builder.setLength(builder.length() - 1);
-        return builder.toString();
     }
 
     private void checkStringValidity(String input) {
