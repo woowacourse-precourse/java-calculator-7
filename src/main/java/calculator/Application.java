66d5e2527ab28f6db414477a00a6aa707c662708
kim-su-mi <ksmm9607@naver.com>
2024-10-21 12:42:20 +0900

    feat: 문자열 입력처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	// 1. 사용자 입력 받기 (Console API 사용)
+        String userInput = Console.readLine();
     }
 }
