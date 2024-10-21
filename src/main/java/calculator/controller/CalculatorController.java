8f48e8b2e0b820e6f10ecf4c6a562a5ae56dc861
COBI-98 <tkdgus968@naver.com>
2024-10-21 23:42:43 +0900

    feat: 커스텀 문자열을 지정할 수 있다.

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -2,6 +2,34 @@ package calculator.controller;
 
 public class CalculatorController {
 
+    private static final String SETUP_SYMBOL_COMMA = ",";
+    private static final String SETUP_SYMBOL_COLON = ":";
+
+    private static final Pattern DELIMITER_PATTERN = Pattern.compile("//(.*?)\\n");
+    private final InputView inputView = new InputView();
+    private final DelimiterManager delimiterManager;
+
+    public CalculatorController() {
+        delimiterManager = new DelimiterManager(List.of(
+                new Delimiter(SETUP_SYMBOL_COMMA)
+                ,new Delimiter(SETUP_SYMBOL_COLON)
+        ));
+    }
     public void startCalculator(){
+        String inputString = inputView.createString();
+        DelimiterManager delimiterManager = extractCustomDelimiters(inputString);
+    }
+
+    private DelimiterManager extractCustomDelimiters(String input) {
+        input = input.replace("\\n", "\n");
+        Matcher matcher = DELIMITER_PATTERN.matcher(input);
+
+        while (matcher.find()) {
+            String delimiterBlock = matcher.group(1);
+            delimiterManager.addDelimiter(delimiterBlock);
+        }
+
+        return delimiterManager;
+    }
     }
 }
