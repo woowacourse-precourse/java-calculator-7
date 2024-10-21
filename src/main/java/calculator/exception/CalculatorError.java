1b9db954db8e3f78e03175b7d12413aa4548cce3
sanghee0820 <lsh277604@gmail.com>
2024-10-21 22:02:00 +0900

    feat: delimiter가 비어있을 경우 오류 추가

--- src/main/java/calculator/exception/CalculatorError.java
+++ src/main/java/calculator/exception/CalculatorError.java
@@ -2,7 +2,8 @@ package calculator.exception;
 
 public enum CalculatorError {
     INPUT_IS_NULL_OR_EMPTY("[Error] 입력 값이 존재하지 않습니다."),
-    DELIMITER_IS_NUMBER("[Error] 구분자는 숫자가 될 수 없습니다.");
+    DELIMITER_IS_NUMBER("[Error] 구분자는 숫자가 될 수 없습니다."),
+    DELIMITER_IS_EMPTY("[Error] 구분자는 비어 있거나 공백만 포함될 수 없습니다.");
 
     private final String message;
 
