3ca7ff0004c0ca816a2476d45c42fc473ac80f95
이준범 <jach1206@naver.com>
2024-10-21 18:48:58 +0900

    feat(service): 정수인지 유리수인지 판별하는 기능 추가
    service에 새 기능 구현
    - Calculator 클래스: 정수인지 유리수인지 판별하여, 적절하게 더한 값을 반환하는 기능 구현

--- src/main/java/calculator/service/Calculator.java
+++ src/main/java/calculator/service/Calculator.java
@@ -1,15 +1,48 @@
 package calculator.service;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 // 숫자들의 합을 구해 반환하기 위한 클래스
 public class Calculator {
 
     // 숫자들의 합을 구해 반환하는 메소드
-    public static Integer calculateSum(List<Integer> numbers) {
-        Integer sum = 0;
-        for (Integer number : numbers) {
-            sum += number;
+    public static String calculateSum(String[] numbers) {
+        if(checkInteger(numbers)) {
+            return calculateInteger(numbers);
         }
-        return sum;
+        return calculateRationalNumber(numbers);
+    }
+
+    // 정수인지 아닌지 판별하는 메소드
+    public static boolean checkInteger(String[] numbers){
+        for (String number : numbers) {
+            if(number.contains(".")){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 정수의 합을 구해 반환하는 메소드
+    public static String calculateInteger(String[] numbers){
+        BigInteger sum = BigInteger.ZERO;
+
+        for (String number : numbers) {
+            sum = sum.add(new BigInteger(number));
+        }
+
+        return sum.toString();
+    }
+
+    // 정수가 아닌 실수가 포함돼 있을 때, 합을 구해 반환하는 메소드
+    public static String calculateRationalNumber(String[] numbers){
+        BigDecimal sum = BigDecimal.ZERO;
+
+        for (String number : numbers) {
+            sum = sum.add(new BigDecimal(number));
+        }
+
+        return sum.toString();
     }
 }
