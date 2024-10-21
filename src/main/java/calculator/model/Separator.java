384d0186bc0f5c6ec5c344b2104d19439d971f86
youjae <youjae0803@gmail.com>
2024-10-21 15:33:35 +0900

    feat: Separator 생성 기능

---
+++ src/main/java/calculator/model/Separator.java
@@ -0,0 +1,39 @@
+package calculator.model;
+
+import calculator.util.ErrorCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Separator {
+    private final String regex;
+    private final List<Character> chars = new ArrayList<>(List.of('.', ','));
+    public Separator() {
+        this.regex = buildRegex();
+    }
+    public Separator(char customChar) {
+        validate(customChar);
+        chars.add(customChar);
+        this.regex = buildRegex();
+    }
+
+    private void validate(char ch){
+        if (Character.isDigit(ch)) {
+            throw new IllegalArgumentException(ErrorCode.SEPARATOR_ERROR.getMessage());
+        }
+    }
+    private String buildRegex() {
+        final String regex;
+        StringBuilder tmpRegex = new StringBuilder("[");
+        for (Character ch : chars) {
+            tmpRegex.append(ch);
+
+            if (ch == '\\') {
+                tmpRegex.append(ch);
+            }
+        }
+        tmpRegex.append("]");
+
+        return tmpRegex.toString();
+    }
+}
--- src/main/java/calculator/util/ErrorCode.java
+++ src/main/java/calculator/util/ErrorCode.java
@@ -1,7 +1,10 @@
 package calculator.util;
 
+import calculator.model.Separator;
+
 public enum ErrorCode {
-    NUMBER_ERROR("숫자는 양수여야됩니다.");
+    NUMBER_ERROR("숫자는 양수여야됩니다."),
+    SEPARATOR_ERROR("구분자는 숫자일 수 없습니다.");
     private final String message;
 
     ErrorCode(String message) {
