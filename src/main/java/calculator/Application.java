68995f4c1e9feb2819ba7069b6e547cee79dfc5a
dayaelee <dayae.dev@gmail.com>
2024-10-21 13:45:36 +0900

    feat: 2. 입력값에서 기본 구분자(쉼표, 콜론)를 통해 숫자를 추출하는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,4 +1,6 @@
 package calculator;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
@@ -8,6 +10,15 @@ public class Application {
         Scanner scanner = new Scanner(System.in);
         // nextLine() 메서드를 통해 입력 값 변수에 저장하기
         String inputValue = scanner.nextLine();
+
+        // 2. "입력값에서 기본 구분자(쉼표, 콜론)를 통해 숫자를 추출하는 기능"
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        String[] nums={};
+        if (inputValue.length()>1) {
+            nums = inputValue.split(",|;");
+        }
+
+
 
     }
 }
