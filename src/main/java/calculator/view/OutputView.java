35907ffb10d8b534b6a5b0929712328e1fc5977c
jinhyeon <tiemo0708@naver.com>
2024-10-21 18:47:52 +0900

    fix(view): OutputView 클래스에서 상수를 잘못 사용한 부분을 수정하여, 상수가 의도대로 출력되도록 수정

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,7 +2,12 @@ package calculator.view;
 
 public class OutputView {
     private static final String RESULT_MESSAGE = "결과 : ";
+
     public void printResult(int result) {
-        System.out.println("RESULT_MESSAGE " + result);
+        System.out.println(RESULT_MESSAGE + result);
+    }
+
+    public void printError(String message) {
+        System.out.println("오류: " + message);
     }
 }
