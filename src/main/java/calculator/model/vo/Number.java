a706bee5e630862de9295afd6b585bf1336b47a9
Park - Jeonghwan <990706leo@gmail.com>
2024-10-21 15:33:26 +0900

    feat(Number): 숫자는 반드시 양수여야 한다

--- src/main/java/calculator/model/vo/Number.java
+++ src/main/java/calculator/model/vo/Number.java
@@ -7,10 +7,17 @@ public final class Number {
     private final BigInteger value;
 
     private Number(BigInteger number) {
+        validatePositive(number);
         value = number;
     }
 
     public static Number of(String number) {
         return new Number(new BigInteger(number));
+    }
+
+    private void validatePositive(BigInteger number) {
+        if (number.compareTo(BigInteger.ZERO) == -1) {
+            throw new IllegalArgumentException("양수만 입력하여야 합니다.");
+        }
     }
 }
