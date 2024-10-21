834b8b8cf4ded0d96d9f36ae2d0cd18aaf912fbe
yunho_yun <yunho_yun@naver.com>
2024-10-21 22:32:39 +0900

    feat: 결과 출력 함수 구현
    printResult

--- src/main/java/calculator/view/CalculatorView.java
+++ src/main/java/calculator/view/CalculatorView.java
@@ -7,4 +7,8 @@ public class CalculatorView {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine().trim();
     }
+
+    public static void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
 }
