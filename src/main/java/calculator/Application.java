b3e737325cbf509ec5970c03c5871ba55f3742c9
Dongjun Lee <dongjle2@student.42heilbronn.de>
2024-10-21 05:32:56 +0200

    feat(Application): implement main function

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,34 @@
 package calculator;
 
+import calculator.DelimiterIdentifier;
+import calculator.CustomInputValidator;
+import calculator.DefaultInputValidator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        DelimiterIdentifier delimiterIdentifier = new DelimiterIdentifier();
+        String delimiter = delimiterIdentifier.getDelimiter(input);
+        DelimiterRegexBuilder delimiterRegexBuilder = new DelimiterRegexBuilder(delimiter);
+        SumCalculator sumCalculator = new SumCalculator();
+        InputValidator inputValidator = getInputValidator(input, delimiter);
+
+        if (inputValidator.isValidInput()) {
+            Parser parser = new Parser(input, delimiter, delimiterRegexBuilder.buildRegexPattern());
+            System.out.println("결과 : " + sumCalculator.sum(parser.parse()));
+        }
     }
+
+    private static InputValidator getInputValidator(String input, String delimiter) {
+        if (isDefaultDelimiter(delimiter)) {
+            return new DefaultInputValidator(input, delimiter);
+        }
+        return new CustomInputValidator(input, delimiter);
+    }
+
+    private static boolean isDefaultDelimiter(String delimiter) {
+        return delimiter.equals(DelimiterIdentifier.DEFAULT_DELIMITERS);
+    }
+
 }
