7dc0e804b36854bd8849b7892a231bcc42211e98
GunsanHaribo <hth99131@gmail.com>
2024-10-21 13:29:18 +0900

    fix(ErrorMessages.java): ERROR_NOT_CONTAIN_DELIMITER 메시지를 "구분자가 없습니다"로 수정
    기존 메시지가 의미를 정확하게 전달하지 못해 수정했습니다.

--- src/main/java/calculator/ErrorMessages.java
+++ src/main/java/calculator/ErrorMessages.java
@@ -6,7 +6,7 @@ public class ErrorMessages {
     protected static final String ERROR_CUSTOM_DELIMITER_NOT_START_WITH_FORMAT = "커스텀 구분자의 형식인 //로 시작하지 않습니다";
     protected static final String ERROR_CUSTOM_DELIMITER_OPERANDS_CONTAIN_OTHER_CHAR = "피연산자에 커스텀 구분자가 아닌 문자가 있습니다";
     protected static final String ERROR_CUSTOM_DELIMITER_CONTAIN_NUMBER = "구분자에 숫자가 들어있습니다";
-    protected static final String ERROR_NOT_CONTAIN_DELIMITER = "구분자에 숫자가 들어있습니다";
+    protected static final String ERROR_NOT_CONTAIN_DELIMITER = "구분자가 없습니다";
     protected static final String ERROR_BASIC_DELIMITER_OPERAND_CONTAIN_OTHER_CHAR = "피연산자에 숫자가 아닌 다른 문자가 있습니다";
     protected static final String ERROR_BASIC_DELIMITER_PARSING_PROBLEM = "파싱시 알 수 없는 에러가 있습니다";
     protected static final String ERROR_BASIC_DELIMITER_CONTAIN_ZERO = "0을 포함합니다";
