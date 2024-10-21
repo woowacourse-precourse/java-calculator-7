0768bbba14c522c9c05aa689b4df40551f746f52
huncozyboy <leejh5838@naver.com>
2024-10-22 08:54:21 +0900

    feat: 문자열 입력시 예외 메세지 추가

--- src/main/java/calculator/common/ErrorMessage.java
+++ src/main/java/calculator/common/ErrorMessage.java
@@ -2,16 +2,20 @@ package calculator.common;
 
 public enum ErrorMessage {
     NULL_INPUT("문자열이 입력되지 않았습니다."),
-    NEGATIVE_NUMBER("음수는 허용되지 않습니다."),
-    INVALID_NUMBER("숫자만 입력할 수 있습니다."),
+    NULL_INPUT_ERROR("문자열에 0은 입력할 수 없습니다."),
+    // 문자열 입력시
     EMPTY_CUSTOM_DELIMITER("커스텀 구분자 사이 값이 비어있습니다."),
     MULTIPLE_CUSTOM_DELIMITERS("커스텀 구분자는 한번만 입력할 수 있습니다."),
-    CONSECUTIVE_DELIMITERS("구분자가 연속해서 나타났습니다."),
-    NUMBER_TOO_LARGE("숫자의 범위가 너무 큽니다."),
-    MISSING_NUMBERS("숫자가 포함되지 않았습니다."),
-    OUT_OF_RANGE_RESULT("결과가 범위를 초과합니다."),
+    CONSECUTIVE_DELIMITERS("커스텀 구분자는 연속해서 입력할 수 없습니다."),
     INVALID_FORMAT("// 뒤에는 반드시 \\n이 와야합니다."),
-    INCORRECT_POSITION("//는 반드시 숫자 앞에 와야합니다.");
+    INCORRECT_POSITION("//는 반드시 숫자 앞에 와야합니다."),
+    // 커스텀 구분자
+    NUMBER_TOO_LARGE("숫자의 범위가 너무 큽니다."),
+    MISSING_NUMBERS("숫자만 입력할 수 있습니다."),
+    NEGATIVE_NUMBER("음수의 값은 입력할 수 없습니다."),
+    INVALID_NUMBER("숫자만 입력할 수 있습니다."),
+    OUT_OF_RANGE_RESULT("결과가 너무 큽니다.");
+    // 숫자 관련
     private final String message;
     ErrorMessage(String message) {
         this.message = message;
