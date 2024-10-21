02e3adcea1565a001c9009074b2ebe5fbd893bc6
박채린 <bewithbomi@gmail.com>
2024-10-21 22:42:25 +0900

    feat(parser):문자열 파싱 구현
    - 기본 구분자(쉼표, 콜론) 추가
    - 커스텀 구분자 파싱 구현 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,6 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -10,5 +11,19 @@ public class Application {
     private static String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
+    }
+
+    private static String[] parseInput(String input) {
+        String delimiter = ",|:";
+        String number = input;
+
+        if (input.startsWith("//")) {
+            int delimiterIdx = input.indexOf("\n");
+            if (delimiterIdx != -1) {
+                delimiter = Pattern.quote(input.substring(2, delimiterIdx));
+                number = input.substring(delimiterIdx + 1);
+            }
+        }
+        return number.split(delimiter);
     }
 }
