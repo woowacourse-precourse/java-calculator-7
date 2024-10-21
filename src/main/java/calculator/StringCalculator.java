c3e782aa808fa94edf40bc1d3e725d08541bd769
chung <souffle1903@gmail.com>
2024-10-21 21:56:31 +0900

    feat: 커스텀 구분자로 문자열 분리 커스텀 구분자로문자열 분리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,13 +2,20 @@ package calculator;
 
 public class StringCalculator {
     public static int addition(String input) {
-        String seperator = ",|:";
+        String seperator = ",:";
 
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException();
         }
 
-        String[] numbers = input.split(seperator);
+
+        if (input.startsWith("//") && input.contains("\\n")) {
+            int idx = input.indexOf("\\n");
+            seperator += input.substring(2, idx);
+            input = input.substring(idx + 2);
+        }
+
+        String[] numbers = input.split("[" + seperator + "]");
 
         int sum = 0;
         for (String number : numbers) {
