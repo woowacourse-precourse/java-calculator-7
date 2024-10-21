b302fa8e18a61f9c6fe755726731a902d89651ed
sytTest <syt1128@naver.com>
2024-10-21 21:38:08 +0900

    :sparkle: (CalcController) add createNewCalc()
    새로운 계산 객체를 생성하고 새로운 숫자 저장소를 만들어 필드에 저장하는 createNewCalc() 추가

--- src/main/java/calculator/controller/CalcController.java
+++ src/main/java/calculator/controller/CalcController.java
@@ -4,6 +4,12 @@ import calculator.domain.Calc;
 import calculator.repository.NumberRepository;
 
 public class CalcController {
+
     private Calc calc;
     private NumberRepository numberRepository;
+
+    private void createNewCalc() {
+        calc = new Calc();
+        numberRepository = new NumberRepository();
+    }
 }
