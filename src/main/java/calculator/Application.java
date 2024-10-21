fb43026ce3b3eab2ee72cf6b7a8d4c50379ca03a
jaehwannnnnn <seanjung0915@gmail.com>
2024-10-21 18:45:23 +0900

    fix: 예외 처리 부분 코드 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,7 +11,7 @@ public class Application {
             int result = StringCalculator.calculate(input);
             System.out.println("결과 : " + result);
         } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 입력입니다: " + e.getMessage());
+            throw new IllegalArgumentException("잘못된 입력입니다.");
         }
     }
 }
