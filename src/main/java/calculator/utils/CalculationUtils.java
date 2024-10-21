0c1dc2665bc5380198d389eaa5304f982d496e28
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-21 17:42:23 +0900

    feat: [기능 구현 3번] CalculationUtils 클래스 추가
    구분자를 통해 배열값 반환

---
+++ src/main/java/calculator/utils/CalculationUtils.java
@@ -0,0 +1,32 @@
+package calculator.utils;
+
+public class CalculationUtils {
+  private static String separator = ",|;";
+
+  public static String[] inputDataToArray(String inputData) {
+
+    inputData = inputData.trim();
+
+    if (inputData.startsWith("//")) {
+      inputData = inputDataToArrayByCustomSeparator(inputData);
+    }
+
+    return inputData.split(separator);
+  }
+
+  private static String inputDataToArrayByCustomSeparator(String inputData) {
+
+    int separatorEndIndex = inputData.indexOf("\\n");
+
+    String customSeparator = inputData.substring(2, separatorEndIndex);
+    
+    separator = escapeSpecialCharacters(customSeparator) + "|" + separator;
+    inputData = inputData.substring(separatorEndIndex + 2);
+
+    return inputData;
+  }
+
+  public static String escapeSpecialCharacters(String separator) {
+    return separator.replaceAll("([\\[\\]{}()\\\\*+?.^$|])", "\\\\$1");
+  }
+}
