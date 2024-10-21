ef5432fe7ec4e390e6c449af1708c87b8138449d
이준범 <jach1206@naver.com>
2024-10-21 16:31:08 +0900

    refactor(view): 메소드를 static으로 변환
    view의 메소드 코드 개선
    - InputView 클래스: getUserInput 메소드를 static으로 변환
    - OutputView 클래스: printOutput 메소드를 static으로 변환

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     // 유저의 입력을 받는 메소드
-    public String getUserInput() {
+    public static String getUserInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class OutputView {
 
     // 계산 결과를 출력하는 메소드
-    public void printOutput(String output) {
+    public static void printOutput(Integer output) {
         System.out.println("결과 : " + output);
         Console.close();
     }
