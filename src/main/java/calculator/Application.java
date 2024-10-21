31b788d9412826a7440a6a8ab04f1ab9bdf98135
JYP <john9803@likelion.org>
2024-10-21 23:10:30 +0900

    feat(calculator): 입력기능 구현
    주어진 라이브러리로 입력받는 기능추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,17 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        // 각종 flag
+        Boolean hasCustomDivider = false;
+        String divider = null;
+        Integer result = 0;
+
+        // 입력기능 - getInput() 차후 리팩토링
+        String input_str = camp.nextstep.edu.missionutils.Console.readLine().trim();
+
     }
 }
+
