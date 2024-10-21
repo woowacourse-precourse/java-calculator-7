abf8cc56d489d66bfa4d1aad803b99a1bfb7b308
20160295 <dorem96@icloud.com>
2024-10-21 15:15:58 +0900

    fix: 계산 입력 수정
    CalculationRequestDTO
    - 계산 요청 데이터 DTO
    - 입력 문자열을 저장하는 클래스
    
    CalculatorService
    - calculate메소드
        - 입력을 DTO 클래스로 수정
        - CalculationRequestDTO을 입력받아 구분자와 숫자를 구분
        - 계산

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,6 +1,20 @@
 package calculator.service;
 
+import calculator.model.CalculationRequestDTO;
+import calculator.model.DelimiterExtractionResult;
+import calculator.model.DelimiterExtractor;
+
 public class CalculatorService {
+    public static int calculate(CalculationRequestDTO requestDTO) {
+        String input = requestDTO.getInput();
+
+        DelimiterExtractionResult extractionResult = DelimiterExtractor.extractDelimiterAndNumbers(input);
+        String delimiter = extractionResult.getDelimiter();
+        String numbers = extractionResult.getNumbers();
+
+        return sumNumbers(numbers, delimiter);
+    }
+
     private static int sumNumbers(String input, String delimiter) {
         String[] numbers = input.split(delimiter);
         int sum = 0;
