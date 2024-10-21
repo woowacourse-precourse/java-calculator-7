c88eebe813f3757e18f4cf8477d8d607ca0282fc
tnsrma <tlsgudwns010@naver.com>
2024-10-21 23:11:17 +0900

    feat: 클린코드 수행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -20,48 +20,51 @@ public class Application {
         if (input.isEmpty()) {
             return 0;  // 빈 문자열 처리
         }
-        String delimiter = getDelimiter(input);  // 구분자 결정
-        String numbersString = getNumbersString(input, delimiter);  // 숫자 문자열 추출
-        List<Integer> numbers = parseNumbers(numbersString, delimiter);  // 숫자 분리 및 리스트 변환
-        return sumNumbers(numbers);  // 숫자 합 계산
+        String delimiter = getDelimiter(input);
+        String numbersString = getNumbersString(input);
+        List<Integer> numbers = parseNumbers(numbersString, delimiter);
+        return sumNumbers(numbers);
     }
 
     private static String getDelimiter(String input) {
         if (input.startsWith("//")) {
-            return extractCustomDelimiter(input);  // 커스텀 구분자 추출
+            return extractCustomDelimiter(input);
         }
-        return ",|:";  // 기본 구분자
+        return ",|:";
     }
 
     private static String extractCustomDelimiter(String input) {
         Matcher matcher = Pattern.compile("//(.*?)\\\\n").matcher(input);
         if (matcher.find()) {
-            return Pattern.quote(matcher.group(1));  // 커스텀 구분자 반환
+            return Pattern.quote(matcher.group(1));
         }
         throw new IllegalArgumentException("커스텀 구분자가 잘못되었습니다.");
     }
 
-    private static String getNumbersString(String input, String delimiter) {
-        return input.replaceFirst("//.*?\\\\n", "");  // 숫자 문자열 추출
+    private static String getNumbersString(String input) {
+        return input.replaceFirst("//.*?\\\\n", "");
     }
 
     private static List<Integer> parseNumbers(String input, String delimiter) {
-        return Arrays.stream(input.split(delimiter))  // 구분자로 문자열 분리
-                .map(Application::parseInt)  // 정수로 변환
+        return Arrays.stream(input.split(delimiter))
+                .map(Application::parseInt)
                 .collect(Collectors.toList());
     }
 
     private static int sumNumbers(List<Integer> numbers) {
-        return numbers.stream()  // 리스트의 모든 숫자 합산
+        return numbers.stream()
                 .reduce(0, Integer::sum);
     }
 
     private static int parseInt(String s) {
-        validateNumber(s);  // 숫자 유효성 검사
+        validateNumber(s);
         return Integer.parseInt(s);
     }
 
     private static void validateNumber(String s) {
+        if (s.isEmpty()) {
+            throw new IllegalArgumentException("빈 문자열은 허용되지 않습니다.");
+        }
         try {
             int number = Integer.parseInt(s);
             if (number < 0) {
