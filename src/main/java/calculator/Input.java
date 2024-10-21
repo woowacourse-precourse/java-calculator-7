f45f1ea837c8914f6640e0f6d2900b2d3a325d9e
Seongmo Ahn <djgood780@gmail.com>
2024-10-21 21:16:08 +0900

    refactor(Input): 구글 자바 가이드에 따른 코드 수정
    - else 예약어 제거
    - 패키지와 import문 사이 빈 줄 추가
    - 오버로드한 메서드들 순차적으로 표현
    - 검증 메서드 명 동사 단어로 변경 (valid->validate)
    - 예외 메시지에 유용한 정보 넣기
    - 내용들을 프로그램 흐름 순서로 정렬

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -1,76 +1,51 @@
 package calculator;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     public static String getUserInput() {
         String input = Console.readLine();
-        validUserInput(input);
+        validateUserInput(input);
         return input;
     }
 
-
-    public static void validUserInput(String input){
-        if(input.isEmpty() || validGeneralUserInput(input) || validCustomUserInput(input)){
+    public static void validateUserInput(String input) {
+        if (input.isEmpty() || validateGeneralUserInput(input) || validateCustomUserInput(input)) {
             return;
-        } else {
-            throw new IllegalArgumentException();
         }
+        throw new IllegalArgumentException("연속된 구분자, 공백, 커스텀 구분자 등 잘못된 입력 입니다.");
     }
 
     //사용자가 기본 구분자를 사용하였을 경우
-    public static boolean validGeneralUserInput(String input){
-        return validSeparator(input)
-                && validGeneralSequenceSeparator(input)
-                && validLastSeparator(input);
+    public static boolean validateGeneralUserInput(String input) {
+        return validateSeparator(input)
+                && validateGeneralSequenceSeparator(input)
+                && validateLastSeparator(input);
     }
 
     //사용자가 커스텀 구분자를 사용하였을 경우
-    public static boolean validCustomUserInput(String input){
-        return validCustomFrontAndCharacter(input)
-                && validSeparator(input, input.charAt(2))
-                && validCustomSequenceSeparator(input)
-                && validLastSeparator(input);
-    }
-
-    //사용자가 기본 구분자를 연달아 쓰지 않았는지 검증
-    public static boolean validGeneralSequenceSeparator(String input){
-        for(char c : input.toCharArray()){
-            if(!Character.isDigit(c) && input.indexOf(c) != input.length()-1){
-                if(!Character.isDigit(input.charAt(input.indexOf(c)+1))){
-                    return false;
-                }
-            }
-        }
-        return true;
+    public static boolean validateCustomUserInput(String input) {
+        return validateCustomFrontAndCharacter(input)
+                && validateSeparator(input, input.charAt(2))
+                && validateCustomSequenceSeparator(input)
+                && validateLastSeparator(input);
     }
 
     //사용자가 커스텀 구분자를 사용하였을 경우, 앞 열 위치인지, 구분자가 문자인지 검증
-    public static boolean validCustomFrontAndCharacter(String input){
-        if(input.length() > 1){
+    public static boolean validateCustomFrontAndCharacter(String input) {
+        if (input.length() > 1) {
+            char inputChar = input.charAt(2);
             return input.indexOf("//") == 0
-                    && !Character.isDigit(input.charAt(2))
+                    && !Character.isDigit(inputChar)
                     && input.indexOf("\\n") == 3;
-        }else{
-            return false;
         }
-    }
-
-    //사용자가 커스텀 구분자를 연달아 쓰지 않았는지 검증
-    public static boolean validCustomSequenceSeparator(String input){
-        for(int i=5; i<input.length(); i++){
-            if(!Character.isDigit(input.charAt(i)) && i != input.length()-1){
-                if(!Character.isDigit(input.charAt(i+1))){
-                    return false;
-                }
-            }
-        }
-        return true;
+        return false;
     }
 
     //사용자가 입력한 기본 구분자 검증
-    public static boolean validSeparator(String input){
-        for(char inputChar : input.toCharArray()){
-            if(!validIncludeSeparator(inputChar)){
+    public static boolean validateSeparator(String input) {
+        for (char inputChar : input.toCharArray()) {
+            if(!validateIncludeSeparator(inputChar)) {
                 return false;
             }
         }
@@ -78,10 +53,10 @@ public class Input {
     }
 
     //사용자가 입력한 커스텀 구분자 검증
-    public static boolean validSeparator(String input, char customSeparator){
-        for(int i=5; i<input.length(); i++){
+    public static boolean validateSeparator(String input, char customSeparator) {
+        for (int i = 5; i < input.length(); i++) {
             char inputChar = input.charAt(i);
-            if(!validIncludeSeparator(inputChar, customSeparator)){
+            if (!validateIncludeSeparator(inputChar, customSeparator)) {
                 return false;
             }
         }
@@ -89,24 +64,54 @@ public class Input {
     }
 
     //사용자가 입력한 기본 구분자만 들어갔는지 검증
-    public static boolean validIncludeSeparator(char inputChar){
-        if(!Character.isDigit(inputChar)){
+    public static boolean validateIncludeSeparator(char inputChar) {
+        if (!Character.isDigit(inputChar)) {
             return inputChar == ',' || inputChar == ':';
         }
         return true;
     }
 
     //사용자가 입력한 커스텀 구분자와 기본 구분자만 들어갔는지 검증
-    public static boolean validIncludeSeparator(char inputChar, char customSeparator){
-        if(!Character.isDigit(inputChar)){
+    public static boolean validateIncludeSeparator(char inputChar, char customSeparator) {
+        if (!Character.isDigit(inputChar)) {
             return inputChar == ',' || inputChar == ':' || inputChar == customSeparator;
         }
         return true;
     }
 
+    //사용자가 기본 구분자를 연달아 쓰지 않았는지 검증
+    public static boolean validateGeneralSequenceSeparator(String input) {
+        for (char inputChar : input.toCharArray()) {
+            if (!Character.isDigit(inputChar) && input.indexOf(inputChar) != (input.length() - 1)) {
+                int idx = input.indexOf(inputChar) + 1;
+                char inputCharNext = input.charAt(idx);
+                if (!Character.isDigit(inputCharNext)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    //사용자가 커스텀 구분자를 연달아 쓰지 않았는지 검증
+    public static boolean validateCustomSequenceSeparator(String input) {
+        for (int i = 5; i < input.length(); i++) {
+            char inputChar = input.charAt(i);
+            if (!Character.isDigit(inputChar) && i != (input.length() - 1)) {
+                int idx = i + 1;
+                char inputCharNext = input.charAt(idx);
+                if (!Character.isDigit(inputCharNext)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     //사용자가 문자열 마지막에 구분자를 사용했는지 검증
-    public static boolean validLastSeparator(String input){
-        return Character.isDigit(input.charAt(input.length() - 1));
+    public static boolean validateLastSeparator(String input) {
+        char inputChar = input.charAt(input.length() - 1);
+        return Character.isDigit(inputChar);
     }
 
 }
