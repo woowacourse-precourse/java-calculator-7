7c0918aedb941ac1cfea6e9bae78f37e206e42fb
Sehee Hong <seaheehong@gmail.com>
2024-10-21 22:18:36 +0900

    feat: 입력한 문자열에서 숫차 추출 기능 및 커스텀 구분자 지정 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,38 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine();
+
+            if (input == null || input.isEmpty()) {
+                System.out.println("결과 : 0");
+                return;
+            }
+
+            String delimiter = ",|:";
+
+            if (input.startsWith("//")) {
+                int delimiterIndex = input.indexOf("\n");
+                delimiter = input.substring(2, delimiterIndex);
+                input = input.substring(delimiterIndex + 1);
+            }
+
+            String[] numbers = input.split(delimiter);
+            int sum = 0;
+
+            for (String number : numbers) {
+                int num = Integer.parseInt(number);
+                sum += num;
+            }
+
+            System.out.println("결과 : " + sum);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
