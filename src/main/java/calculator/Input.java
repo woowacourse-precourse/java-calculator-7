b3f9201139fcf632e31356302828813e5dd5624f
kimYejin <kimphoby0125@gmail.com>
2024-10-21 16:33:15 +0900

    refactor : input 예외 처리 및 input 값에 따른 로직 처리

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -7,16 +7,17 @@ import java.util.NoSuchElementException;
 public class Input {
     private static String input;
     public static void readLine(){
-            try{
-                input = Console.readLine(); // 입력 읽기
-            }catch(NoSuchElementException e){
-                input = "0";
-            }
 
-            if(input.isEmpty() || input == null){
-                System.out.println("try");
-                input = "0";
-            }
+        try{
+            input = Console.readLine(); // 입력 읽기
+        } catch(NoSuchElementException e){
+            input = "0";
+        }
+
+        if(input.isEmpty() || input == null){
+            System.out.println("try");
+            input = "0";
+        }
     }
 
     public static String getInput(){
