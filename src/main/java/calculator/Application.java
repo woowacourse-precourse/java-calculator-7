0d7d85bc523b60d9e3016897bb23c4cadad76c2a
rainyday1367 <rainyday1367@naver.com>
2024-10-21 18:36:07 +0900

    틀 작성완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = Console.readLine();
+        try{
+            int result = add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    static int add(String input) {
+        int sum = 0;
+
+        return sum;
     }
 }
