52676b352e1969e53432e13dab230375d09abd6e
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-10-21 13:52:44 +0900

    feat(calculator): 문자열 계산기 기본 구조 및 입력 처리 추가
    문자열 덧셈 계산기를 위한 기본 구조
    - main 클래스는 사용자로부터 입력을 받음
    - Calculator 클래스는 입력 처리, null이나 빈 문자열일 경우 0을 반환

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package calculator;
+
+public class Calculator {
+
+    public static int add(String input) {
+
+        // 빈 입력에 대한 처리
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        
+        // TODO : 계산기 구현
+
+        return 0;
+    }
+
+}
