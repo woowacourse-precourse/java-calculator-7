9da1e65ee14612c105fe1f52eff30146ab51dfc6
31n5ang <31n5ang@gmail.com>
2024-10-21 21:35:48 +0900

    feat: 커스텀 구분자를 추출하는 기능 구현
    커스텀 구분자를 추출할 수 있을 경우, 분리되어야할 문자열의 시작 위치(startOffset)를 갱신합니다. 또한, 의존중인 delimiterManager에 구분자로 추가합니다.

--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -1,11 +1,29 @@
 package calculator;
 
+import java.util.Optional;
+
 public class Separator {
     private final String target;
     private final DelimiterManager delimiterManager;
+    private int startOffset = 0;
 
     Separator(String target, DelimiterManager delimiterManager) {
         this.target = target;
         this.delimiterManager = delimiterManager;
+    }
+
+    public Optional<Character> addCustomDelimiter(String prefix, String suffix) {
+        int prefixPosition = target.indexOf(prefix);
+        int suffixPosition = target.indexOf(suffix);
+        int customDelimiterPosition = prefixPosition + prefix.length();
+
+        if (prefixPosition != 0 || customDelimiterPosition + 1 != suffixPosition) {
+            return Optional.empty();
+        } else {
+            startOffset = suffixPosition + suffix.length();
+            char customDelimiter = target.charAt(customDelimiterPosition);
+            delimiterManager.add(customDelimiter);
+            return Optional.of(customDelimiter);
+        }
     }
 }
