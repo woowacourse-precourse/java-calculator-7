0fce3a6dc56deaad1d90b0eabad047efd81042cf
Rachel <cofla226@naver.com>
2024-10-21 23:27:15 +0900

    feat: 커스텀 구분자 처리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,9 +3,13 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
+
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String NEW_LINE_INDICATOR = "\\n";
 
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
@@ -15,8 +19,34 @@ public class Application {
 
     public static void processInput(String input) {
         List<Long> numbers = new ArrayList<>();
-        String delimiter = determineDelimiter(input);
+        String delimiter;
+
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            int idx = input.indexOf(NEW_LINE_INDICATOR);
+            if (idx > 0) {
+                delimiter = getCustomDelimiter(input);
+                input = input.substring(idx + 2).trim();
+            } else {
+                throw new IllegalArgumentException("커스텀 구분자가 잘못 지정되었습니다.");
+            }
+        } else {
+            delimiter = determineDelimiter(input);
+        }
+
         String[] inputArray = input.split(delimiter);
+        Arrays.stream(inputArray).forEach(s -> {
+            try {
+                long number = Long.valueOf(s.trim());
+                numbers.add(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("입력된 값이 숫자가 아닙니다: '" + s.trim() + "'");
+            }
+        });
+    }
+
+    private static String getCustomDelimiter(String input) {
+        int idx = input.indexOf(NEW_LINE_INDICATOR);
+        return input.substring(2, idx);
     }
 
     public static String determineDelimiter(String input) {
