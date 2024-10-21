18b449ccffb2f2ec22ba11156ea6b183a63aa3fe
dk7648 <dk7648@korea.ac.kr>
2024-10-21 21:24:31 +0900

    feat: 프로그램 실행 기능 추가

--- src/main/java/calculator/control/Calculator.java
+++ src/main/java/calculator/control/Calculator.java
@@ -1,8 +1,25 @@
 package calculator.control;
 
+import calculator.global.SentenceUtil;
+import calculator.model.Sentence;
+import calculator.model.Separator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 import java.util.List;
 
 public class Calculator {
+    public void start() {
+        System.out.println(SentenceUtil.extractSentence("1,2,3"));
+        OutputView.printNotice();
+        String input = InputView.readInput();
+
+        Sentence sentence = new Sentence(SentenceUtil.extractSentence(input));
+        Separator separator = new Separator();
+        separator.extractCustomCharacter(input);
+        List<Integer> numbers = sentence.extractNumbers(separator.getCharacters());
+        OutputView.printResult(addNumbers(numbers));
+    }
     public int addNumbers(List<Integer> numbers) {
         int result = 0;
         for (int number : numbers) {
