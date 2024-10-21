19948ccf627f9e49a7b62a68464e5b67ba2ad8f1
AntsThatWander <anubspider@gmail.com>
2024-10-21 12:09:21 +0900

    feat(기능2): change return types and parameters, and add a method
    Change some variables to instance variables:
    - input: String
    - delimeter: String = ":|,"
    
    Change some methods' return types and parameters:
    - getInput(): void
    - split(): String[]
    
    Add a method to split a custom delimeter and values to add:
    - splitInputDelimeter(): void

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,27 +1,40 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
+    private String input;
+    private String delimeter = ":|,";
+
     public void run() {
-        String input = getInput();
-        String[] splitted = split(input);
+        getInput();
+
+        String[] splitted = split();
         int result = add(splitted);
 
         System.out.println("결과 : " + result);
     } 
 
-    private String getInput() {
-        String input = "";
-
+    private void getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        input = Console.readLine();
+        this.input = Console.readLine();
 
-        return input;
+        splitInputDelimeter();
     }
 
-    private String[] split(String input) {
-        String delimeter = ":|,";
+    private void splitInputDelimeter () {
+        Pattern pattern = Pattern.compile("//(\\S)\\\\n");
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            this.delimeter += "|" + matcher.group(1);
+            this.input = matcher.replaceAll("");
+        } 
+    }
+
+    private String[] split() {
         String[] splitted = input.split(delimeter);
 
         return splitted;
