921e51d929097a6d93e73f748ab9666663678c36
백승우 <swthewhite@gmail.com>
2024-10-21 23:06:49 +0900

    feat: 빈 문자열 또는 null 입력 시 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,8 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // 사용자로부터 입력을 받음
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine(); // 사용자 입력을 받음
+        String input = Console.readLine();
+
+        // 입력이 null이거나 빈 문자열이면 예외 발생
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("빈 값은 허용되지 않습니다.");
+        }
     }
 }
+
