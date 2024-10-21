894777b78b8b18fff43fab7618aa40ecbf379ed1
wnsmir <163817041+wnsmir@users.noreply.github.com>
2024-10-21 22:07:59 +0900

    feat: add default delimiters and custom delimiter regex

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,4 +1,7 @@
 package calculator;
 
 public class StringCalculator {
+
+    private static final String DEFAULT_DELIMITERS = ",|:";
+    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\\\\n(.*)";
 }
