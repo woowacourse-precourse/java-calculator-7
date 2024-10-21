b057ac02056543fe5a9569d8911a7dd858a19f50
seohee-P <p03061180@gmail.com>
2024-10-21 18:17:10 +0900

    feat: 결과 출력하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,7 @@ import calculator.domain.Calculator;
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.run();
+        int result = calculator.run();
+        System.out.println("결과 : " + result);
     }
 }
--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -4,13 +4,19 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
 
-    public void run() {
+    public int run() {
         String input = getInput();
+        return calculate(input);
     }
 
+    // 문자열을 입력받는 함수
     private String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         return input;
+    }
+
+    private int calculate(String input) {
+        return 0;
     }
 }
