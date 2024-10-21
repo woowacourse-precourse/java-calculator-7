7d3ea97cc74eedd25df24ca467ee7025eba7ee17
kimch <rlacogk6577@daum.net>
2024-10-21 16:28:34 +0900

    feat(main): 사용자 입력을 받아 양수 리스트로 생성하고 계산 결과 출력 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+        Calculator calculator = new Calculator();
+        OutputHandler outputHandler = new OutputHandler();
+
+        ArrayList<Double> positiveList = inputHandler.generatePositiveList();
+        calculator.Add(positiveList);
+        double result = calculator.result;
+        outputHandler.printResult(result);
     }
 }
