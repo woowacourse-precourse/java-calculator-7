19e27b1c28592cf785e242844df51b04e9cd1c4b
kwon <rnjsdndud00@naver.com>
2024-10-21 12:31:46 +0900

    refactor(): 변경되지 않는 값 -> 상수 처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,17 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
 
+    // 입력값이 없는 경우 리턴 되는 값
+    private static final int EMPTY_RESULT = 0;
+    // 입력값이 1자리인 경우를 체크하기 위한 값
+    private static final int SINGLE_DIGIT_LENGTH = 1;
+
     // intSum, parseInt 메서드를 사용하기 위한 Utils 객체 생성
     CalculatorUtils calculatorUtils = new CalculatorUtils();
 
     public int calculate(String input) {
         // 입력값이 없는 경우에 대한 예외 처리
         if (input.isEmpty()) {
-            return 0;
+            return EMPTY_RESULT;
         }
 
         // 입력값이 1자리인 경우 그 숫자 그대로 반환
-        if (input.length() == 1) {
+        if (input.length() == SINGLE_DIGIT_LENGTH) {
             return calculatorUtils.parseInt(input);
         }
 
