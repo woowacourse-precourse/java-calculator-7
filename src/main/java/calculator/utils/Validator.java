2975255ccb2e84fb8e9976fd2f862a55b809c252
gahyeon <1000ji1@naver.com>
2024-10-21 22:11:11 +0900

    refactor : private -> public 바꿈

--- src/main/java/calculator/utils/Validator.java
+++ src/main/java/calculator/utils/Validator.java
@@ -1,10 +1,10 @@
 package calculator.utils;
 
 public class Validator {
-    private static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "숫자이여야 합니다.";
-    private static final String POSITIVE_NUMBER_EXCEPTION_MESSAGE = "양수이여야 합니다.";
-    private static final String CUSTOM_SEPARATOR_EXCEPTION_MESSAGE = "커스텀 구분자는 한 글자여야 합니다.";
-    private static final String START_POSITIVE_NUMBER_EXCEPTION_MESSAGE = "양수부터 입력하셔야 합니다.";
+    public static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "숫자이여야 합니다.";
+    public static final String POSITIVE_NUMBER_EXCEPTION_MESSAGE = "양수이여야 합니다.";
+    public static final String CUSTOM_SEPARATOR_EXCEPTION_MESSAGE = "커스텀 구분자는 한 글자여야 합니다.";
+    public static final String START_POSITIVE_NUMBER_EXCEPTION_MESSAGE = "양수부터 입력하셔야 합니다.";
 
     public static void isNumber(String[] tokens) {
         for (String token : tokens) {
