1a39ce4790f94142ffef3536533605db28d58a0f
kangsan <devstar1223@gmail.com>
2024-10-21 19:48:22 +0900

    feat: 오류 발생시 다시 입력

--- src/main/java/calculator/InputString.java
+++ src/main/java/calculator/InputString.java
@@ -6,7 +6,6 @@ public class InputString {
     public static String input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String stringValue = Console.readLine();
-        Console.close();
         return stringValue;
     }
 }
