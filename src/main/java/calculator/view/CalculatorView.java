ca8e992fe2e54dc9f235e3a32a15694175772ac2
Hun0914 <bkh010914@naver.com>
2024-10-21 14:13:16 +0900

    feat: 덧셈할 문자열 입력을 위한 CalculatorView 추가
    사용자가 덧셈할 문자열을 입력하도록 CalculatorView 클래스에 getInput 메서드를 추가했습니다.

---
+++ src/main/java/calculator/view/CalculatorView.java
@@ -0,0 +1,22 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CalculatorView {
+
+    public String getInput() {
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String input = Console.readLine();
+
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("빈 문자열입니다.");
+        }
+        ;
+
+        return input;
+    }
+
+
+}
