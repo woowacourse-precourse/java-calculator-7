4b7df2946cc6f3b5cc3174c6aeb7bb3251989206
smyeongL <sangmyeonglee123@gmail.com>
2024-10-21 19:45:40 +1100

    fix(application): remove debugging line

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,22 +1,20 @@
 package calculator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import calculator.context.Calculator;
+import calculator.strategy.Add;
+import calculator.parser.InputHandler;
 
 public class Application {
-
     public static void main(String[] args) {
         // Declare Strategy Context Class, Calculator
         Calculator addCalculator = new Calculator(new Add());
         InputHandler inputHandler = new InputHandler();
         inputHandler.getUserInput();
-        // inputHandler.setUserInput("//m\n1,2:3m2");
         inputHandler.setCustomSeparators();
         inputHandler.removeCustomSeparators();
         inputHandler.parseInput();
         addCalculator.setNumbers(inputHandler.getNumbers());
-        System.out.println(addCalculator.calculate());
+        addCalculator.calculate();
     }
 
 }
