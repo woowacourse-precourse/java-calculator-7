8239709b30a369c71930ebb4b4b81d19cf745551
woorimj <forestwr06@gmail.com>
2024-10-21 14:45:40 +0900

    Feat: 사용자 입출력 처리 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = sc.nextLine();
+
+        System.out.println("결과 : " );
     }
 }
