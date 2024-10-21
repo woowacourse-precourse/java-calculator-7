bcd92357831ab25cf7c4611abf74d8c2c5512c6f
minjeeki <minjeeki@student.42seoul.kr>
2024-10-21 23:14:34 +0900

    feat(calculator): implement operator set generation

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,6 +10,33 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
+        try {
+            // 기능 1. 연산자 세트 생성 (만약 //로 시작하고 \n으로 종료되지 않을 경우 에러처리)
+            Set<Character> setOperator = generateOperatorSet(input);
+            if (setOperator == null) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 입력입니다.");
+        }
+    }
+
+    // 기능 1: 연산자 세트 생성
+    public static Set<Character> generateOperatorSet(String word) {
+        Set<Character> setOperator = new HashSet<>();
+        setOperator.add(',');
+        setOperator.add(':');
+
+        if (word.startsWith("//")) {
+            int idxNextLine = word.indexOf("\\n");
+            if (idxNextLine == -1) {
+                return null;
+            }
+            for (int i = 2; i < idxNextLine; i++) {
+                setOperator.add(word.charAt(i));
+            }
+        }
+        return setOperator;
     }
 }
 
