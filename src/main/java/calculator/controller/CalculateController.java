b7bdaf605de02f6aaf8e9733f5d7f4b5cdc151d0
이준범 <jach1206@naver.com>
2024-10-21 16:26:52 +0900

    feat(controller): 애플리케이션이 실행 됐을 때, 적절히 처리하는 컨트롤러 구현
    controller에 새 기능 구현
    - CalculateController 클래스: 애플리케이션이 실행되면, 사용자에게 값을 입력받아 적절히 처리하고, 결과를 출력하는 기능 구현

---
+++ src/main/java/calculator/controller/CalculateController.java
@@ -0,0 +1,39 @@
+package calculator.controller;
+
+import calculator.service.Calculator;
+import calculator.service.CheckDelimiterType;
+import calculator.service.DelimiterExtractor;
+import calculator.service.NumExtractor;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+import java.util.List;
+
+public class CalculateController {
+
+    // 사용자가 애플리케이션을 실행하면 적절히 처리하는 메소드
+    public static void run(){
+        String userInput = InputView.getUserInput();
+
+        // 사용자가 아무것도 입력하지 않았다면 0 출력
+        if(userInput.isEmpty()){
+            OutputView.printOutput(0);
+        }
+
+        // 사용자의 입력값이 기본 구분자 형식인 경우
+        else if (CheckDelimiterType.isBasicDelimiter(userInput)) {
+            // 기본 구분자를 기반으로 숫차 추출
+            List<Integer> integers = NumExtractor.extractNumbersWithBasicDelimiters(userInput);
+            OutputView.printOutput(Calculator.calculateSum(integers));
+        }
+
+        // 사용자의 입력값이 커스텀 구분자 형식인 경우
+        else {
+            // 커스텀 구분자를 먼저 추출
+            String customDelimiter = DelimiterExtractor.extractDelimiter(userInput);
+            // 커스텀 구분자를 기반으로 숫자 추출
+            List<Integer> integers = NumExtractor.extractNumbersWithCustomDelimiter(userInput, customDelimiter);
+            OutputView.printOutput(Calculator.calculateSum(integers));
+        }
+    }
+}
