4a5199be9a7b5da80d06b027549cc03bfa5e4917
Hannana <hancrystal@sookmyung.ac.kr>
2024-10-21 20:46:53 +0900

    feat: 사용자로부터 문자열 입력 받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputStr = Console.readLine();
+        int answer = 0;
+        if(isNull(inputStr)){
+            displayResult(answer);
+        }
+        Console.close();
+    }
+
+    private static void displayResult(int result) {
+        System.out.println("결과: " + result);
+    }
+    private static boolean isNull(String s) {
+        return s == null || s.isEmpty();
     }
 }
