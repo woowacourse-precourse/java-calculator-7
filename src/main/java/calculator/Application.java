e20ba587742287146654c7e36ef4e2eeb235df10
LouiIII3 <jikdsunghuilee@naver.com>
2024-10-21 19:48:11 +0900

    feat(calculator): 빈 문자열 또는 null 입력 시 0 반환 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -16,7 +16,16 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = scanner.nextLine();
 
+        if (isEmptyOrNull(input)) {
+            scanner.close();
+            return "결과 : 0";
+        }
+
         scanner.close();
         return "결과 : " + answer;
+    }
+
+    private boolean isEmptyOrNull(String input) {
+        return input == null || input.isEmpty();
     }
 }
