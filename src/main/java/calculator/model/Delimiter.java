3b31b4031f2cc133104f7966e297dad1e21c82b6
Chan531 <chan657312@gmail.com>
2024-10-21 18:07:37 +0900

    chore(Delimiter): equals  & hasCode 오버라이딩

--- src/main/java/calculator/model/Delimiter.java
+++ src/main/java/calculator/model/Delimiter.java
@@ -1,5 +1,7 @@
 package calculator.model;
 
+import java.util.Objects;
+
 public class Delimiter {
 
 	private final String separator;
@@ -10,5 +12,20 @@ public class Delimiter {
 
 	public boolean compare(String input) {
 		return this.separator.equals(input);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Delimiter delimiter = (Delimiter)o;
+		return Objects.equals(separator, delimiter.separator);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(separator);
 	}
 }
