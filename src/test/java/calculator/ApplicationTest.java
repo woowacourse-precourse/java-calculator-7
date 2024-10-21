fbc109af014a1faff8392f3430efd56856f3dc99
leestana01 <leestana01@naver.com>
2024-10-21 10:25:01 +0900

    test: 통합 테스트 추가
    통합 테스트 추가
    - 기본 구분자 사용
    - 커스텀 구분자 사용
    - 혼합 구분자 사용

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -66,6 +66,22 @@ class ApplicationTest extends NsTest {
 
     // 통합 테스트 ----
     @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용2() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
@@ -74,9 +90,33 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_사용2() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 혼합_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1,2:3;4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_문자포함() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("a,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
