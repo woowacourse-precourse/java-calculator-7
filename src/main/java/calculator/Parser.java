baa51d6d3d9294d0338ba2e88cf9649014a84cab
peanut990 <peanut1998@naver.com>
2024-10-21 22:25:43 +0900

    feat : add exception handling for using separator between digit
    added function checkUsingSeparator() checks whether there are numbers before or after the separator.

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -32,10 +32,22 @@ public class Parser {
             }
 
             if (separators.contains(curChar)) {
-
+                checkUsingSeparator(index, inputArray);
                 index++;
                 continue;
             }
+        }
+    }
+
+    private void checkUsingSeparator(int index, char[] inputArray) {
+        int front = index - 1;
+        int back = index + 1;
+        if (front < 0 || back > inputArray.length - 1) {
+            throw new IllegalArgumentException("분리자는 숫자 사이에 사용해야 한다(문자열 끝에 사용)");
+        }
+
+        if (!(Character.isDigit(inputArray[front]) && Character.isDigit(inputArray[back]))) {
+            throw new IllegalArgumentException("분리자는 숫자 사이에 사용해야 한다(연속적인 분리자 사용)");
         }
     }
 
--- src/test/java/calculator/ExceptionTest.java
+++ src/test/java/calculator/ExceptionTest.java
@@ -93,31 +93,29 @@ public class ExceptionTest {
                 .hasMessage("숫자와 알파벳 n, 문자 '/', 문자 '\\' 는 구분자가 될 수 없다");
 
     }
-    //
-//    @Test
-//    public void 커스텀구분자예외처리테스트6() throws Exception {
-//        //given
-//        String inputString = "//;$\\n1;2,3$4";
-//        //when
-//        Parser parser = new Parser();
-//        parser.parse(inputString);
-//        List<Integer> nums = parser.getNums();
-//        int sum = Calculator.sum(nums);
-//        //then
-//        Assertions.assertThat(sum).isEqualTo(10);
-//    }
-//
-//
-//    @Test
-//    public void 커스텀구분자예외처리테스트8() throws Exception {
-//        //given
-//        String inputString = "//$\\1;2,3";
-//        Parser parser = new Parser();
-//        //when
-//        Throwable thrown = catchThrowable(() -> parser.parse(inputString));
-//        //then
-//        Assertions.assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
-//                .hasMessage("'\\'가 입력된 후에는 'n'이 입력되어야 한다");
-//    }
+
+    @Test
+    public void 커스텀구분자예외처리테스트7() throws Exception {
+        //given
+        String inputString = "//;\\n1;;2,3;4";
+        Parser parser = new Parser();
+        //when
+        Throwable thrown = catchThrowable(() -> parser.parse(inputString));
+        //then
+        Assertions.assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("분리자는 숫자 사이에 사용해야 한다(연속적인 분리자 사용)");
+    }
+
+    @Test
+    public void 커스텀구분자예외처리테스트8() throws Exception {
+        //given
+        String inputString = "//;\\n1;2,3;";
+        Parser parser = new Parser();
+        //when
+        Throwable thrown = catchThrowable(() -> parser.parse(inputString));
+        //then
+        Assertions.assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("분리자는 숫자 사이에 사용해야 한다(문자열 끝에 사용)");
+    }
 
 }
