0369f494147b2a36c34253aae7faf416f8ad2202
sjchae <soojch12@gmail.com>
2024-10-21 16:35:11 +0900

    feat: 쉼표(,) 콜론(:)으로 문자열 분리해서 숫자 추출해서 숫자 합 반환 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input;
+        try {
+            input = br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] inputArray = input.split("[,;]");
+        int num = 0;
+        for (String s : inputArray) {
+            num += Integer.parseInt(s);
+        }
+
+        System.out.println(num);
     }
 }
