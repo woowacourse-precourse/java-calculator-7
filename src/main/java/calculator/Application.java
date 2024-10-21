76c3195d5183be540df3f53fa23a795eeeb3a02b
dayeondayeon <dayeon2438@gmail.com>
2024-10-21 18:51:51 +0900

    feat : 사용자 입력으로부터 커스텀 구분자를 판단하고 배열로 변환하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,4 +1,5 @@
 package calculator;
+import static calculator.Converter.*;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -6,6 +7,10 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
+
         String customDelimiter = Validator.findCustomDelimiter(input);
+        input = removeCustomDelimiterAndSpace(input, customDelimiter);
+        String[] strings = convertToArray(input);
+
     }
 }
