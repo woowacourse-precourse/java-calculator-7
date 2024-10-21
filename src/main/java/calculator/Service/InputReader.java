257bcade8791358fefa63487b9d89daf5267e5ff
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-21 18:00:03 +0900

    feat(InputReader): 빈 문자열 예외처리 관련 코드 추가
    - 빈 문자열 "" 입력 시에는, 해당 문자열을 " " 공백 문자열로 변환해서 반환
    - 빈 문자열 "" 에 대한 출력은 과제에 0이라고 명시되어 있음
    - 해당 클래스에서는 그 과정 중 빈 문자열을 공백 문자열로 반환하는 로직을 포함
    - 또한 Console.close()를 추가해서 입력 버퍼를 닫아줌

--- src/main/java/calculator/Service/InputReader.java
+++ src/main/java/calculator/Service/InputReader.java
@@ -9,6 +9,12 @@ public class InputReader {
     public StringCalculatorDTO read() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();      // 사용자로부터 입력 받기
+
+        if (input.isEmpty()) {
+            input = " ";                        // 빈 문자열(\n) 입력시에는 " " 를 반환
+        }
+
+        Console.close();                        // 입력 후 콘솛 객체 제거
         return new StringCalculatorDTO(input);  // 입력값을 DTO로 감싸서 반환
     }
 }
