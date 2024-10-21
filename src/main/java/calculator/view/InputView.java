4e99f6ccd6b40bccc0b73db5d40a5d0452528c0e
daeyeon <kdy4410121@naver.com>
2024-10-21 16:36:13 +0900

    fix: 출력 순서 변경

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public String input() {
-        String inputString = Console.readLine();
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputString = Console.readLine();
         return inputString;
     }
 
