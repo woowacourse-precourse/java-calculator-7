68aaedfdc388f8ff66645ff961693a75ba68e228
jjw0071 <jjw0071@naver.com>
2024-10-21 14:05:38 +0900

    feat: get user input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -7,6 +9,9 @@ public class Application {
         String delimiter2 = ":";
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+
 
     }
 }
