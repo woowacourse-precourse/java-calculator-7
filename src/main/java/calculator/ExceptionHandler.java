d77efacfa80aa4253522fb4534cca300c7ab3a64
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-21 20:14:40 +0900

    docs: 클래스와 메서드에 주석 추가
    - Application, InputHandler, StringSplitter, Accumulator, OutputHandler 클래스에 주석을 추가하여 코드 설명 보강
    - 코드 이해와 유지보수를 쉽게 하기 위해 주석 작성

--- src/main/java/calculator/ExceptionHandler.java
+++ src/main/java/calculator/ExceptionHandler.java
@@ -1,12 +1,15 @@
 package calculator;
 
+// 예외처리를 담당하는 클래스
 public class ExceptionHandler {
+    // 커스텀 구분자가 비어있을 때 예외처리 <- //\n로 입력되었을 경우
     public static void handleInvalidDelmiter(String delmiter) {
         if (delmiter.isEmpty()) {
             throw createIllegalArgumentException("커스텀 구분자가 비어있습니다.");
         }
     }
 
+    // 기본, 커스텀 구분자로 등록되지 않은 구분자를 사용할 경우 예외처리
     public static void handleNonNumbericInput(String input) {
         for (char c : input.toCharArray()) {
             if (c < '0' || c > '9') {
@@ -15,6 +18,7 @@ public class ExceptionHandler {
         }
     }
 
+    // 입력된 숫자가 int의 범위를 넘어설 경우 예외처리
     public static void handleNumberRange(String input) {
         try {
             Integer.parseInt(input);
--- src/main/java/calculator/OutputHandler.java
+++ src/main/java/calculator/OutputHandler.java
@@ -1,6 +1,7 @@
 package calculator;
 
 public class OutputHandler {
+    // 결과 메시지 상수
     private final static String OUTPUT_MESSAGE = "결과 : ";
 
     public void showOutput(int result) {
