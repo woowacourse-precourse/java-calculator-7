860823ae106666f3ea07fbb82971cdf32fd6b17d
gyuoo <yeomky98@naver.com>
2024-10-21 23:39:24 +0900

    test: add tests for long range overflow

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -42,6 +42,15 @@ class ApplicationTest extends NsTest {
 	}
 
 	@Test
+	void long_범위_초과_테스트() {
+		assertSimpleTest(() -> {
+			assertThatThrownBy(() -> runException("9223372036854775808,1")) // Long.MAX_VALUE + 1
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("입력을 숫자로 변환할 수 없거나, 변환된 값이 long 범위를 초과합니다.");
+		});
+	}
+
+	@Test
 	void 음수_입력_테스트() {
 		assertSimpleTest(() -> {
 			assertThatThrownBy(() -> runException("-1,2,3"))
@@ -54,6 +63,14 @@ class ApplicationTest extends NsTest {
 	void 여러_커스텀_구분자_사용() {
 		assertSimpleTest(() -> {
 			run("//|\\n1;2|3//;\\n");
+			assertThat(output()).contains("결과 : 6");
+		});
+	}
+
+	@Test
+	void 공백_제거_후_결과() {
+		assertSimpleTest(() -> {
+			run("   1: 2: 3   ");
 			assertThat(output()).contains("결과 : 6");
 		});
 	}
