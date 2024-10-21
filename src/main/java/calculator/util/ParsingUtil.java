2cce09ff023ece1142c4d66fa7938647a1341f47
sanghee0820 <lsh277604@gmail.com>
2024-10-21 22:03:55 +0900

    feat: delimiter 유효성 검사 추가 및, input이 비어있는지 확인하는 매서드 변경

--- src/main/java/calculator/util/ParsingUtil.java
+++ src/main/java/calculator/util/ParsingUtil.java
@@ -11,12 +11,14 @@ public final class ParsingUtil {
     private static final String DEFAULT_DELIMITER_1 = ",";
     private static final String DEFAULT_DELIMITER_2 = ":";
     private static final String DEFAULT_DELIMITER_REGEX = ",|:";
-    private static final String CUSTOM_DELIMITER_REGEX = "//\\s*(.)\\s*\\n";
+    private static final String CUSTOM_DELIMITER_REGEX = "//\\s*(.+)\\s*\\n";
 
     private ParsingUtil() {
     }
 
     public static List<String> extractNumbers(String input) {
+        checkNullOrEmpty(input);
+
         if (containsCustomDelimiter(input)) {
             return parseWithCustomDelimiter(input);
         }
@@ -24,14 +26,14 @@ public final class ParsingUtil {
     }
 
     public static List<String> parseWithDefault(String input) {
-        if (isInputIsNullOrEmpty(input)) {
-            throw new IllegalArgumentException(CalculatorError.INPUT_IS_NULL_OR_EMPTY.getMessage());
-        }
+        checkNullOrEmpty(input);
 
         return Arrays.asList(input.split(DEFAULT_DELIMITER_REGEX));
     }
 
     public static List<String> parseWithCustomDelimiter(String input) {
+        checkNullOrEmpty(input);
+
         List<String> delimiters = extractCustomDelimiter(input);
         String[] split = input.split("\n");
         String numbers = split[split.length - 1];
@@ -45,12 +47,16 @@ public final class ParsingUtil {
 
 
     public static List<String> extractCustomDelimiter(String input) {
+        checkNullOrEmpty(input);
+
         Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
         Matcher matcher = pattern.matcher(input);
         List<String> delimiters = new ArrayList<>();
 
         while (matcher.find()) {
-            delimiters.add(matcher.group(1));
+            var customDelimiter = matcher.group(1).trim();
+            checkDelimiter(customDelimiter);
+            delimiters.add(customDelimiter.trim());
         }
         return delimiters;
     }
@@ -62,7 +68,26 @@ public final class ParsingUtil {
         return matcher.find();
     }
 
-    private static boolean isInputIsNullOrEmpty(String input) {
-        return input == null || input.isEmpty() || input.isBlank();
+    public static void checkDelimiter(String delimiter) {
+        checkDelimiterFormat(delimiter);
+        checkDelimiterEmpty(delimiter);
+    }
+
+    public static void checkDelimiterFormat(String delimiter) {
+        if (delimiter.matches("\\d+")) {
+            throw new IllegalArgumentException(CalculatorError.DELIMITER_IS_NUMBER.getMessage());
+        }
+    }
+
+    public static void checkDelimiterEmpty(String delimiter) {
+        if (delimiter == null || delimiter.isEmpty() || delimiter.isBlank()) {
+            throw new IllegalArgumentException(CalculatorError.DELIMITER_IS_EMPTY.getMessage());
+        }
+    }
+
+    private static void checkNullOrEmpty(String input) {
+        if (input == null || input.isEmpty() || input.isBlank()) {
+            throw new IllegalArgumentException(CalculatorError.INPUT_IS_NULL_OR_EMPTY.getMessage());
+        }
     }
 }
