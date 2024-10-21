40daa6d86f81bbd386126a8815e1691936e72326
joyes0ng <128571665+joyes0ng@users.noreply.github.com>
2024-10-21 20:39:38 +0900

    feat : Application 구현 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,5 +7,13 @@ public class Application {
         Scanner scanner = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String text = scanner.nextLine();
+
+        StringCalculator calculator = new StringCalculator();
+        try {
+            int result = calculator.add(text);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
