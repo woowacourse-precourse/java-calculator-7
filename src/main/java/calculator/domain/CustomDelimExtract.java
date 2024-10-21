267178a987157056b2a47c6e9cc72315a348be1b
sytTest <syt1128@naver.com>
2024-10-21 20:18:35 +0900

    :sparkle: (CustomDelimExtract) add getEssence()
    입력 받은 커스텀 구분자 입력식에서 구분자를 추출하는 getEssence() 추가

--- src/main/java/calculator/domain/CustomDelimExtract.java
+++ src/main/java/calculator/domain/CustomDelimExtract.java
@@ -1,4 +1,19 @@
 package calculator.domain;
 
+import calculator.domain.constant.CustomDelimBracket;
+
 public class CustomDelimExtract {
+    private static String lining;
+
+    public static String getEssence(String coveredCustomDelim) {
+        lining = coveredCustomDelim;
+        hatchBracket(CustomDelimBracket.OPEN.get());
+        hatchBracket(CustomDelimBracket.CLOSE.get());
+        return lining;
+    }
+
+    private static void hatchBracket(String bracket) {
+        lining = lining.replace(bracket, "");
+    }
+
 }
