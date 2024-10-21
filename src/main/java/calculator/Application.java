76416207dd910eeea01d74642165cb4120fa6663
AndBlack99 <jwoo1687@icloud.com>
2024-10-21 22:58:15 +0900

    feat : 콜론,쉼표 구분자 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+    	String input = Console.readLine();
+    	
+        String delimiter = ",|:";
+
+        String[] numbers = input.split(delimiter);
+        int sum = 0;
+        for (String number : numbers) {
+            if (number.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            try {
+            int num = Integer.parseInt(number);
+
+            }
+        }
+
+        System.out.println("결과 : " + sum);}
 }
