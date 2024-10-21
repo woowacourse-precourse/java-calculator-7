450c7bef9939d3ba079ce43ad378c0df57679710
tdddt <doto1116@ewhain.net>
2024-10-21 17:22:07 +0900

    feat: 기본 구분자를 사용해 숫자 추출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,19 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 1. 문자열 입력
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        // System.out.println(input);
+
+        // 2. 숫자 추출
+        // 기본 구분자
+        String[] num = input.split("[,:]");
+        // System.out.println(Arrays.toString(num));
     }
 }
