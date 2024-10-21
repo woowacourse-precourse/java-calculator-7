c095746f32f4dcd981f9f21891563dcc0440f1c8
chair00 <pk1029@yu.ac.kr>
2024-10-21 15:45:11 +0900

    feat: 결과 출력 기능 추가
    숫자 합 결과를 형식에 맞게 출력하는 메소드를 Calc 클래스에 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,12 @@ package calculator;
 
 
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -12,7 +18,87 @@ public class Application {
 
         System.out.println("input: " + input);
 
+        Calc calc = new Calc(input);
+
+        int result = calc.calcSum();
+
+        calc.printResult(result);
+
+
     }
+}
+
+class Calc {
+
+    private static Set<String> sep = new HashSet<String>();
+    private String input;
+    private int[] numArr;
+
+    Calc(String input) {
+        this.input = input;
+        setSepAndInput();
+    }
+
+    // 1. 구분자 저장 및 input에서 커스텀 구분자 분리하기
+    private void setSepAndInput() {
+
+        sep.add(",");
+        sep.add(":");
+
+        // "//" "\n" 사이 단어 추출
+        Pattern pattern = Pattern.compile("//(.*?)(?=\\\\n)");
+        Matcher matcher = pattern.matcher(input);
+
+        if(matcher.find()) {
+
+            String customSep = matcher.group(1).trim();
+
+            sep.add(customSep);
+
+
+            this.input = input.replace(matcher.group(0), "").replace("\\n", "").trim(); // 첫 번째 매칭된 문자열 제거
+
+        }
+
+//        System.out.println("sep: " + sep.toString());
+//        System.out.println("input: " + this.input);
+
+    }
+
+    // 2. 숫자 구분하기
+    private int[] getNumArr() {
+
+        String regex = String.join("|", sep);
+
+        try {
+            numArr = Arrays.stream(input.split(regex))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        } catch (Exception e) {
+
+            numArr = new int[0];
+        }
+
+        return numArr;
+    }
+
+    // 3. 숫자 합 구하기
+    public int calcSum() {
+        int[] numArr = getNumArr();
+
+        if (numArr != null) {
+
+            return Arrays.stream(numArr).sum();
+        }
+
+        return 0;
+    }
+
+    // 결과 출력하기
+    public void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
+
 }
 
 
