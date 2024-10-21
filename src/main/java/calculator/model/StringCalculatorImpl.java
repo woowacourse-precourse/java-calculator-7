b509e9d2cab76b9454ebff4aa5bfd08926bef503
LSM <leesung2925@gmail.com>
2024-10-21 23:13:36 +0900

    feat: 예외 처리 추가 및 잘못된 커스텀 구분자 형식 처리

--- src/main/java/calculator/model/StringCalculatorImpl.java
+++ src/main/java/calculator/model/StringCalculatorImpl.java
@@ -14,12 +14,17 @@ public class StringCalculatorImpl implements StringCalculator{
 
     @Override
     public String[] parse(String input) {
-        String delimiter = ",|:";
+        String delimiter = ":|,";
         String numbers = input;
 
         // 커스텀 구분자 처리
         if (input.startsWith("//")) {
             int delimiterIndex = input.indexOf("\\n");
+
+            if(delimiterIndex == -1){
+                throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다");
+            }
+
             delimiter = input.substring(2, delimiterIndex);
             numbers = input.substring(delimiterIndex + 2);
         }
@@ -27,14 +32,29 @@ public class StringCalculatorImpl implements StringCalculator{
         return numbers.split(delimiter);
     }
 
+
+
     @Override
     public int sum(String[] numbers) {
         int total = 0;
 
         for(String number : numbers){
-            total += Integer.parseInt(number);
+            total += validate(number);
         }
 
         return total;
+    }
+
+    public int validate(String number){
+        try{
+            int num = Integer.parseInt(number);
+            if(num < 0){
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다 : " + number);
+            }
+            return num;
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException("유효하지 않은 숫자입니다 : " + number);
+        }
     }
 }
