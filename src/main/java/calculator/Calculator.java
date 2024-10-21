ddedb76dc2019e261eebd2cd1ebc386c3c3c60a5
동원 <ai06021@gmail.com>
2024-10-21 19:55:34 +0900

    feat : Calculator inner numbers

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    private final List<Integer> numbers = new ArrayList<>();
+
+    public void run(String inputWord, Division division) {
+        String[] numberWords = inputWord.split(division.regularExpression());
+        try {
+            for (String numberWord : numberWords) {
+                numbers.add(Integer.parseInt(numberWord));
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 문자열 정수 입니다");
+
+        } catch (Exception e) {
+            System.out.println("예기치 않은 문제가 발생했습니다. 해결해야 합니다");
+        }
+    }
+}
