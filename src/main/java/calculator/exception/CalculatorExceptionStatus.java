6702638f01afc6df107c15195510354079f0b32e
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-21 18:11:12 +0900

    [chore] : . 구분자에 대한 에외처리
    기본 구분자 사용 시 : 예외처리 하지 않음
    커스텀 구분자 사용시 : . 구분자 사용 시 수식에 소수가 있다면 해석이 모호해져 사용하지 못하도록 예외처리

--- src/main/java/calculator/exception/CalculatorExceptionStatus.java
+++ src/main/java/calculator/exception/CalculatorExceptionStatus.java
@@ -5,7 +5,8 @@ public enum CalculatorExceptionStatus {
     INVALID_DEFAULT_DELIMITER_EXPRESSION("잘못된 입력 형식입니다"),
     INVALID_CUSTOM_DELIMITER_EXPRESSION("수식에 올바르지 않은 커스텀 구분자가 들어갔습니다"),
     INVALID_NEGATIVE_INTEGER("양수만 입력 가능합니다"),
-    INVALID_CUSTOM_DELIMITER_INTEGER("커스텀 구분자로 숫자를 사용할 수 없습니다.");
+    INVALID_CUSTOM_DELIMITER_INTEGER("커스텀 구분자로 숫자를 사용할 수 없습니다."),
+    INVALID_CUSTOM_DELIMITER_DOT("커스텀 구분자로 점을 사용할 수 없습니다.");
 
     private final String errorMessage;
 
--- src/main/java/calculator/utils/InputValidator.java
+++ src/main/java/calculator/utils/InputValidator.java
@@ -7,7 +7,7 @@ public class InputValidator {
 
     private static final String CUSTOM_DELIMITER_FORMAT = "^//.\\\\n.*";
     private static final String CHECK_NEGATIVE_INTEGER_FORMAT = ".*-\\d+.*";
-    private static final String CHECK_DEFAULT_DELIMITER_FORMAT = "[0-9,|:]+";
+    private static final String CHECK_DEFAULT_DELIMITER_FORMAT = "[0-9,|:\\.]+";
     private static final int CUSTOM_DELIMITER_IDX = 2;
     private static final int CUSTOM_DELIMITER_EXPRESSION_START_IDX = 5;
 
@@ -18,9 +18,13 @@ public class InputValidator {
                     CalculatorExceptionStatus.INVALID_NEGATIVE_INTEGER);
         }
 
+        // 커스텀 구분자 사용하는 경우
         if (input.matches(CUSTOM_DELIMITER_FORMAT)) {
             String delimiter = String.valueOf(input.charAt(CUSTOM_DELIMITER_IDX));
             String expression = input.substring(CUSTOM_DELIMITER_EXPRESSION_START_IDX);
+
+            if(delimiter.equals("."))
+                throw new CalculatorException(CalculatorExceptionStatus.INVALID_CUSTOM_DELIMITER_DOT);
 
             if (Character.isDigit(delimiter.charAt(0))) {
                 throw new CalculatorException(
@@ -28,7 +32,7 @@ public class InputValidator {
             }
 
             for (char c : expression.toCharArray()) {
-                if (!Character.isDigit(c) && !String.valueOf(c).equals(delimiter)) {
+                if (!Character.isDigit(c) && !String.valueOf(c).equals(delimiter) && !String.valueOf(c).equals(".")) {
                     throw new CalculatorException(
                             CalculatorExceptionStatus.INVALID_CUSTOM_DELIMITER_EXPRESSION);
                 }
@@ -36,9 +40,12 @@ public class InputValidator {
             return;
         }
 
+        // 기본 구분자를 사용하는 경우
         if (!input.isEmpty() && !input.matches(CHECK_DEFAULT_DELIMITER_FORMAT)) {
             throw new CalculatorException(
                     CalculatorExceptionStatus.INVALID_DEFAULT_DELIMITER_EXPRESSION);
         }
+
+
     }
 }
