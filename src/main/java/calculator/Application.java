38cfee6d928d0fb612befb1f14b29a9d2ed8d923
yejin923 <yejin923@naver.com>
2024-10-21 19:52:00 +0900

    fix: 예외처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,8 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static java.lang.Character.isDigit;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -13,38 +11,44 @@ public class Application {
             int total = 0;
             String separator = null;
             String num = null;
-            String[] str= null;
+            String[] str;
 
             System.out.println("덧셈할 문자열을 입력해주세요. ");
-            String line = Console.readLine();
+            String line = Console.readLine().trim();
 
-            if(line.isEmpty()|| line==null){
-                str[0] = String.valueOf(0);
+            if(line.matches(".*[A-Z가-힣]+.*"))
+                throw new IllegalArgumentException("한글과 알파벳은 입력할 수 없습니다.");
+
+            if (line.isEmpty()||line==null) {
+                throw new IllegalArgumentException("입력이 비어 있습니다.");
             }
-
-            if(line.matches(".*[가-힣]+.*")|| line.contains("-"))
-                throw new IllegalArgumentException("한글은 입력할 수 없습니다.");
 
             Pattern pattern = Pattern.compile("//(.)\\\\n(.*)");
             Matcher sep = pattern.matcher(line);
 
-            if (sep.find()) {
+            if (sep.find()&&!sep.group(1).isEmpty()&&!sep.group(1).equals(" ")) {
                 separator = sep.group(1);
-                if(isDigit(Integer.parseInt(separator))) throw new IllegalArgumentException("구분자 오류");
-                else{num = sep.group(2);
-                str = num.split(separator + "|,|:");}
+                num = sep.group(2);
+                str = num.split(separator + "|,|:");
 
             } else {
                 str = line.split(":|,");
             }
 
+
             for (int i = 0; i < str.length; i++) {
                 try {
                     if(Integer.parseInt(str[i])<=0) throw new IllegalArgumentException("양수를 입력하세요.");
-                    else total += Integer.parseInt(str[i]);
+                    if (str[i].isEmpty()) {
+                        str[i]="0";
+                    }
 
+
+                    total += Integer.parseInt(str[i]);
                 } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("숫자를 입력하세요.");
+
+                        throw new IllegalArgumentException("숫자가 아닙니다.");
+
                 }
             }
 
