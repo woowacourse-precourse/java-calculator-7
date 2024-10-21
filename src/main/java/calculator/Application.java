74fb0237a000f30ebf3d5f2b6bfe09868eb9db8c
chaeyeon0130 <k84292330@gmail.com>
2024-10-21 20:32:50 +0900

    feat(Application): 커스텀 구분자로 숫자 피연산자 추출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -17,6 +17,12 @@ public class Application {
         }
 
         String delimiter = ",|:";
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\\n");
+            delimiter = input.substring(2, delimiterIndex);
+            input = input.substring(delimiterIndex + 2);
+        }
+
         String[] tokens = input.split(delimiter);
     }
 }
