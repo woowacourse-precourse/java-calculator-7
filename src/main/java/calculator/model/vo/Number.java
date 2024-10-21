e2c99949b7e84bd5a2a273188f40e911d36fa8cc
Park - Jeonghwan <990706leo@gmail.com>
2024-10-21 15:46:08 +0900

    feat(Number): equals, hashCode 재정의

--- src/main/java/calculator/model/vo/Number.java
+++ src/main/java/calculator/model/vo/Number.java
@@ -1,6 +1,7 @@
 package calculator.model.vo;
 
 import java.math.BigInteger;
+import java.util.Objects;
 
 public final class Number {
 
@@ -27,5 +28,22 @@ public final class Number {
     @Override
     public String toString() {
         return value.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Number number = (Number) o;
+        return Objects.equals(value, number.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
