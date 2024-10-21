74d0642971b0d56927393c388c3a2c3d7a1a4419
Jihyun3478 <leejihyun3478@gmail.com>
2024-10-21 16:59:49 +0900

    feat : 구분자와 숫자 추출하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
 
@@ -8,6 +10,19 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         validateIsInputEmpty(input);
+
+        getSeparatorAndNumbers(input);
+    }
+
+    public static void getSeparatorAndNumbers(String input) {
+        String delimiter = ",|:";
+        String numbers = input;
+
+        Matcher matcher = Pattern.compile("//(.)\\\\n(.*)").matcher(input);
+        if (matcher.matches()) {
+            delimiter = Pattern.quote(matcher.group(1));
+            numbers = matcher.group(2);
+        }
     }
 
     private static void validateIsInputEmpty(String input) {
