397a7b56d665ea148fa6c10b040118694d3b89f9
chooh1010 <chooh1010@gmail.com>
2024-10-21 22:04:40 +0900

    feat: 문자열이 비었는지 확인

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class StringCalculator {
+    public StringCalculator() {
+    }
+
+    public int calculate(String string) {
+        if (string.isEmpty()) {
+            return 0;
+        }
+
+
+    }
+}
