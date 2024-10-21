8495cbc685a6e04be27cfb9399de707b2e54fdc6
hanseounggyun <hanseu9839@gmail.com>
2024-10-21 22:53:57 +0900

    refactor: Scanner -> BufferedReader로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,13 +4,16 @@ import calculator.domain.Calculator;
 import calculator.ui.InputView;
 import calculator.ui.ResultView;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         // TODO: 프로그램 구현
-        InputView inputView = new InputView(new Scanner(System.in));
+        InputView inputView = new InputView(new BufferedReader(new InputStreamReader(System.in)));
         Calculator calculator = new Calculator(inputView.inputView());
         ResultView.calculatorResult(calculator.sum());
+
+
     }
 }
--- src/main/java/calculator/ui/InputView.java
+++ src/main/java/calculator/ui/InputView.java
@@ -1,17 +1,27 @@
 package calculator.ui;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_SENTENCE = "덧셈할 문자열을 입력해 주세요.";
-    private final Scanner scanner;
+    private static final String ERROR_MESSAGE = "잘못 입력하였습니다.";
+    private final BufferedReader bufferedReader;
 
-    public InputView(Scanner scanner) {
-        this.scanner = scanner;
+    public InputView(BufferedReader bufferedReader) {
+        this.bufferedReader = bufferedReader;
     }
 
-    public String inputView() {
+    public String inputView()  {
         System.out.println(INPUT_SENTENCE);
-        return scanner.nextLine();
+        String str = null;
+        try{
+            str = bufferedReader.readLine();
+        } catch(Exception e) {
+            System.out.println(ERROR_MESSAGE);
+        }
+
+        return str;
     }
 }
