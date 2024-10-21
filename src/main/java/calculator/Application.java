30b1aba47835fb76d70d94d7504b6f11c377018f
GwonDooHyeon <secure3141@gmail.com>
2024-10-21 19:42:44 +0900

    feat: 문자열 덧셈기 계산기 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,34 +2,42 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-
 public class Application {
     
-    private static final String DELIMITER = "[,:]";
+    static int result = 0;
     
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         
         String input = Console.readLine();
+        
         if (input.isEmpty()) {
             return;
         }
         
-        int result = calculateSum(input);
-        System.out.println("결과 : " + result);
-    }
-    
-    private static int calculateSum(String input) {
-        String[] numbers = input.split(DELIMITER);
+        String delimiter = "[.:]";
         
-        try {
-            return Arrays.stream(numbers)
-                .mapToInt(Integer::parseInt)
-                .sum();
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
+        if (input.startsWith("//")) {
+            int customDelimiterIndex = input.indexOf("\\n");
+            
+            if (customDelimiterIndex != -1) {
+                delimiter = input.substring(2, customDelimiterIndex);
+                input = input.substring(customDelimiterIndex + 2);
+            }
         }
+        
+        String[] numbers = input.split(delimiter);
+        
+        for (String number : numbers) {
+            int parseNumber = Integer.parseInt(number);
+            if (parseNumber < 0) {
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+            }
+            
+            result += parseNumber;
+        }
+        
+        System.out.println("결과 : " + result);
     }
     
 }
