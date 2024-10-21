ec021dcfe4a45b1365d1107a0576cab15b63c76f
chair00 <pk1029@yu.ac.kr>
2024-10-21 20:49:21 +0900

    fix: 예제 테스트를 위한 임시코드

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,19 +12,19 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        try{
-            // 0. 문자열 입력받기
-            System.out.println("덧셈할 문자열을 입력해 주세요.");
-            String input = readLine();
-
-            Calc calc = new Calc(input);
-
-            int result = calc.calcSum();
-
-            System.out.println("결과 : " + result);
-
-        } catch (IllegalArgumentException e) {
-        }
+//        try{
+//            // 0. 문자열 입력받기
+//            System.out.println("덧셈할 문자열을 입력해 주세요.");
+//            String input = readLine();
+//
+//            Calc calc = new Calc(input);
+//
+//            int result = calc.calcSum();
+//
+//            System.out.println("결과 : " + result);
+//
+//        } catch (IllegalArgumentException e) {
+//        }
     }
 }
 
