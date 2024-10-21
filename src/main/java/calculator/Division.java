507040e603d1dc9a68e50cf2e0c3ba0981a30e73
동원 <ai06021@gmail.com>
2024-10-21 18:58:14 +0900

    feat : division class make

---
+++ src/main/java/calculator/Division.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Division {
+    private final List<String> word = new ArrayList<>();
+
+    public Division() {
+        word.add(";");
+        word.add(",");
+    }
+}
