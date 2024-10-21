8eb0c6f4b98c03904fb23f21050381c283612d95
mirolis6636 <mirolis6636@gmail.com>
2024-10-21 13:45:26 +0900

    fix: handle an exception when calling readUserInput

--- src/main/java/calculator/viewer/CalculatorViewer.java
+++ src/main/java/calculator/viewer/CalculatorViewer.java
@@ -1,10 +1,17 @@
 package calculator.viewer;
 
+import java.util.NoSuchElementException;
+
 public class CalculatorViewer {
     public String readUserInput(){
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String userInput = camp.nextstep.edu.missionutils.Console.readLine();
-        return userInput;
+        try {
+            String userInput = camp.nextstep.edu.missionutils.Console.readLine();
+            return userInput;
+        }
+        catch (NoSuchElementException e){
+            return "";
+        }
     }
 
     public void printResult(long result){
