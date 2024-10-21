1271c693a5c079ddd343467273fade65338fdff6
정미경 <wjdalrud5516@gmail.com>
2024-10-21 14:50:29 +0900

    Application.java nullCheck commit

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        int sum;
+        if (input.isEmpty() || input.trim().isEmpty()){
+            sum = 0;
+            System.out.println("결과 : " + sum);
+        } else {
+            System.out.println("공백이 아닙니다.");
+        }//if end
     }
 }
