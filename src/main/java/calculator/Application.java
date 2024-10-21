057e100daabe325a3f429ad8d1946c5f054dad41
CBAAabced <nmrkmj0108@kyonggi.ac.kr>
2024-10-21 10:14:49 +0900

    feat: implement user input handling

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
+import java.lang.String;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int sum = 0;
+        String inputString = getUserInput();
+        System.out.println("입력된 문자열: " + inputString);  //확인용 출력
+    }
+    public static String getUserInput(){
+        System.out.print("덧셈할 문자열을 입력하세요: ");
+        String inputString = Console.readLine();
+        return inputString;
     }
 }
