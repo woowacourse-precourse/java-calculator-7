685318409a13951bc9ebfcd17f1c0bc934c2a21e
Son JunHyung <kingjh1125@gmail.com>
2024-10-21 18:37:35 +0900

    test(controller): add integration test of real number
    실수 덧셈에 대한 테스트 추가

--- src/test/java/calculator/controller/calculatorControllerTest.java
+++ src/test/java/calculator/controller/calculatorControllerTest.java
@@ -12,8 +12,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class calculatorControllerTest extends NsTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"///\\n1/2/3", "//\\\\n1\\2\\3", "//n\\n1n2n3", "// \\n1 2 3", "1,2:3", "//^\\n1^2^3"})
-    void 문자열_덧셈_성공_테스트(String input) {
+    @ValueSource(strings = {"///\\n1/2/3", "//\\\\n1\\2\\3", "//n\\n1n2n3", "// \\n1 2 3", "1,2:3", "//.\\n1.2,3",
+            "//^\\n1^2^3"})
+    void 정수_문자열_덧셈_성공_테스트(String input) {
         assertSimpleTest(() -> {
             run(input);
             assertThat(output()).contains("결과 : 6");
@@ -21,8 +22,17 @@ public class calculatorControllerTest extends NsTest {
     }
 
     @ParameterizedTest
+    @ValueSource(strings = {"3.3", "1.12,1.13:1.05", "3,0.3", "1.3,2"})
+    void 실수_문자열_덧셈_성공_테스트(String input) {
+        assertSimpleTest(() -> {
+            run(input);
+            assertThat(output()).contains("결과 : 3.3");
+        });
+    }
+
+    @ParameterizedTest
     @ValueSource(strings = {"//]\\n1[2[3", "//1\\n1,2,3", "HelloWorld", "/\\n1", "//]]\\n1", "1,2,3,", ",1,2,3",
-            "1,2,a", "-1,2,3", "1,,2"})
+            "1,2,a", "-1,2,3", "1,,2", "0,1", "0.0,1", "//.\\n1.0,1"})
     void 문자열_덧셈_실패_테스트(String input) {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException(input))
