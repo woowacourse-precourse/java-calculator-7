86ddf089008b0f22366acdffb69ee45de8e9cc65
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-10-21 19:15:51 +0900

    feat: 일반 구분자에 대한 문자열 분할 및 Integer 변환

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,9 +2,12 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+
 public class Calculator {
     private String inputValue;
     private Integer result = 0;
+    private String delimiters = ":|,";
 
     Calculator() {
     }
@@ -12,5 +15,19 @@ public class Calculator {
     public void createInputValue() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         this.inputValue = Console.readLine();
+    }
+
+    private String[] splitNums() {
+        return this.inputValue.split(delimiters);
+    }
+
+    private ArrayList<Integer> toInteger(String[] stringNums) {
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        for (String n : stringNums) {
+            nums.add(Integer.parseInt(n));
+        }
+
+        return nums;
     }
 }
