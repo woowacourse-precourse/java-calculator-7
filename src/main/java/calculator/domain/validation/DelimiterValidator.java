cf27b38d5d585729ad7e5c9595b43a7725dc5214
rootTiket <l20078829@gmail.com>
2024-10-21 16:07:17 +0900

    fix: //와 \n의 위치가 바뀌어도 작동하도록 변경

--- src/main/java/calculator/domain/validation/DelimiterValidator.java
+++ src/main/java/calculator/domain/validation/DelimiterValidator.java
@@ -6,22 +6,40 @@ import static calculator.domain.constant.errorMessage.ParseError.INVALID_FORMAT;
 import calculator.global.exception.CalculatorException;
 
 public class DelimiterValidator {
-    ;
+
+    private static final String CUSTOM_DELIMITER_START = "//";
+    private static final String CUSTOM_DELIMITER_END = "\\n";
 
     public void validateFormat(String inputString) {
-        checkDelimiterPosition(inputString);
         checkStartDelimiter(inputString);
+        checkDelimiterPosition(inputString);
     }
 
     private void checkStartDelimiter(String inputString) {
-        if (inputString.startsWith("//") && inputString.indexOf("\\n") != 3) {
+
+        int startDelimiterIndex = inputString.indexOf(CUSTOM_DELIMITER_START);
+        int endDelimiterIndex = inputString.indexOf(CUSTOM_DELIMITER_END);
+
+        if (startDelimiterIndex == 0 && (endDelimiterIndex != CUSTOM_DELIMITER_START.length() + 1)) {
+            throw new CalculatorException(INVALID_FORMAT);
+        }
+
+        if (endDelimiterIndex == 0 && (startDelimiterIndex != CUSTOM_DELIMITER_START.length() + 1)) {
             throw new CalculatorException(INVALID_FORMAT);
         }
     }
 
     private void checkDelimiterPosition(String inputString) {
-        if (inputString.contains("//") && !inputString.startsWith("//")) {
+        int startDelimiterIndex = inputString.indexOf(CUSTOM_DELIMITER_START);
+        int endDelimiterIndex = inputString.indexOf(CUSTOM_DELIMITER_END);
+
+        if ((startDelimiterIndex > 1 && endDelimiterIndex == -1) || (endDelimiterIndex > 1
+                && startDelimiterIndex == -1)) {
             throw new CalculatorException(INCORRECT_POSITION);
+        }
+
+        if (startDelimiterIndex == -1 && endDelimiterIndex == -1) {
+            return;
         }
     }
 }
