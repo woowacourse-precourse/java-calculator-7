a789a597fbc1b5d35b9c67139890270ec2520dea
LouiIII3 <jikdsunghuilee@naver.com>
2024-10-21 19:49:21 +0900

    feat(calculator): 쉼표 또는 콜론 구분자 처리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -21,11 +21,27 @@ public class Application {
             return "결과 : 0";
         }
 
+        addWithDefaultDelimiters(input);
+
         scanner.close();
         return "결과 : " + answer;
     }
 
     private boolean isEmptyOrNull(String input) {
         return input == null || input.isEmpty();
+    }
+
+    private void addWithDefaultDelimiters(String input) {
+        String[] numbers = input.split("[,:]");
+        addNumbers(numbers);
+    }
+
+    private void addNumbers(String[] numbers) {
+        Stream.of(numbers).forEach(this::addNumber);
+    }
+
+
+    private void addNumber(String number) {
+        answer += Integer.parseInt(number);
     }
 }
