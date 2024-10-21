010170033ccace83c9f56bb431514c83b2cc0631
dbdb456 <adad568@naver.com>
2024-10-21 09:34:17 +0900

    test(ValidatorTest): validateDelimiterDeclaration 메서드 테스트 추가

--- src/test/java/calculator/ValidatorTest.java
+++ src/test/java/calculator/ValidatorTest.java
@@ -1,8 +1,13 @@
 package calculator;
 
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import calculator.validators.Validators;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 
 public class ValidatorTest {
@@ -13,4 +18,21 @@ public class ValidatorTest {
     void setUp() {
         validators = new Validators();
     }
+
+    @Test
+    void testValidateDelimiterDeclaration_Success_True() {
+        assertTrue(validators.validateDelimiterDeclaration("//;\\n1;2"));
+    }
+
+    @Test
+    void testValidateDelimiterDeclaration_Success_False() {
+        assertFalse(validators.validateDelimiterDeclaration("1:2"));
+    }
+
+    @Test
+    void testValidateDelimiterDeclaration_Fail_NoNewLine() {
+        assertThrows(IllegalArgumentException.class, () -> validators.validateDelimiterDeclaration("//;1;2"));
+    }
+
+    
 }
