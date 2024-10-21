ba7a7bfabd3ec752505cb5fb4990de4c3c0d894e
gayoung228 <103436798+gayoung228@users.noreply.github.com>
2024-10-21 23:45:28 +0900

    메시지

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = scanner.nextLine();
+
+        try {
+            int result = StringCalculator.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.err.println("에러: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
     }
 }
