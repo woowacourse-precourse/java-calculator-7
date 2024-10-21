12a6269babab4bf64c69cdee605bd10f5ad5175d
chaeyeon0130 <k84292330@gmail.com>
2024-10-21 20:59:33 +0900

    refactor(StringCalculator): StringExtractor와 NumberParser를 사용하는 StringCalculator 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,45 +3,13 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        int result = calculate(input);
+        StringCalculator calculator = new StringCalculator(new StringExtractor(), new NumberParser());
+        int result = calculator.sum(input);
         System.out.println("결과 : " + result);
-    }
-
-    public static int calculate(String input) {
-        if (input == null || input.isEmpty()) {
-            return 0;
-        }
-
-        String delimiter = ",|:";
-        if (input.startsWith("//")) {
-            int delimiterIndex = input.indexOf("\\n");
-            delimiter = input.substring(2, delimiterIndex);
-            input = input.substring(delimiterIndex + 2);
-        }
-
-        String[] tokens = input.split(delimiter);
-        int sum = 0;
-        for (String token : tokens) {
-            int number = parsePositiveNumber(token);
-            sum += number;
-        }
-
-        return sum;
-    }
-
-    public static int parsePositiveNumber(String token) {
-        try {
-            int number = Integer.parseInt(token);
-            if (number < 0) {
-                throw new IllegalArgumentException("음수는 허용되지 않습니다.");
-            }
-            return number;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자 형식이 잘못되었습니다.");
-        }
     }
 }
