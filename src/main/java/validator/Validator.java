cd2438fc10e4d96b5623a47f1230bd498ad5c8a2
xeimun <hinote444@naver.com>
2024-10-21 21:27:58 +0900

    refactor: 에러 메시지 수정

--- src/main/java/validator/Validator.java
+++ src/main/java/validator/Validator.java
@@ -31,14 +31,17 @@ public class Validator {
         }
     }
 
-    // "//"과 "\n" 중 하나만 사용한 경우 예외 발생
+    // 올바르지 않은 커스텀 구분자 선언을 한 경우 예외 발생
     public static void validateDelimiterDeclaration(String userInput, String declarationPrefix,
                                                     String declarationSuffix) {
+        // ["//"만 입력된 경우] 예외 발생
         if (userInput.startsWith(declarationPrefix) && !userInput.contains(declarationSuffix)) {
-            throw new IllegalArgumentException("커스텀 구분자를 사용할 경우 반드시 '//'와 '\\n'을 함께 사용해야 합니다.");
+            throw new IllegalArgumentException("유효한 커스텀 구분자 선언 형식으로 입력해주세요. : //{커스텀 구분자}\\n{문자열}");
         }
+
+        // ["\n"만 입력된 경우] 또는 [선언 순서가 뒤바뀐 경우] 또는 [선언이 문자열 앞부분이 아닌 경우] 예외 발생
         if (userInput.contains(declarationSuffix) && !userInput.startsWith(declarationPrefix)) {
-            throw new IllegalArgumentException("커스텀 구분자를 사용할 경우 반드시 '//'와 '\\n'을 함께 사용해야 합니다.");
+            throw new IllegalArgumentException("유효한 커스텀 구분자 선언 형식으로 입력해주세요. : //{커스텀 구분자}\\n{문자열}");
         }
     }
 }
