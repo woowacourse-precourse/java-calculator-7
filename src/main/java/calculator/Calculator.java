4a6ede7b3e159de0ba12dfcd1db7457d2400eb8e
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-10-21 19:06:48 +0900

    feat: 문자열 입력 메서드 생성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Calculator {
     private String inputValue;
     private Integer result = 0;
 
     Calculator() {
+    }
+
+    public void createInputValue() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        this.inputValue = Console.readLine();
     }
 }
