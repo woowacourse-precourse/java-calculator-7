b81c71a05d1de6b1e1d3873c176262acbcd0f631
spiky-haired-Pyramid-head <qns0147@gmail.com>
2024-10-21 16:17:23 +0900

    feat: Manager 클래스 View, Service 기능 추가

--- src/main/java/calculator/Manager/CalculatorManager.java
+++ src/main/java/calculator/Manager/CalculatorManager.java
@@ -1,5 +1,11 @@
 package calculator.Manager;
 
+import calculator.service.CalculatorService;
+import calculator.service.PlusCalculator;
+import calculator.view.CalculatorView;
+
+import java.util.List;
+
 public class CalculatorManager {
 
     private static CalculatorManager instance;
@@ -12,8 +18,13 @@ public class CalculatorManager {
         return instance;
     }
 
+    static CalculatorView view = CalculatorView.getInstance();
+    static CalculatorService service = PlusCalculator.getInstance();
 
-    public void run(){
 
+    public void run() {
+        String rexNumber = view.printInputString();
+        List<String> numList = service.splitDigits(rexNumber);
+        view.printResult(service.operation(numList));
     }
 }
