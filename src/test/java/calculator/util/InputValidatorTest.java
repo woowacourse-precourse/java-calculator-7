b9853c2877ba315b9ea2e74de21f32fab18e9c4f
sso9594 <sso9594@naver.com>
2024-10-21 17:48:24 +0900

    test(InputValidatorTest): 잘못된 테스트 케이스 삭제

--- src/test/java/calculator/util/InputValidatorTest.java
+++ src/test/java/calculator/util/InputValidatorTest.java
@@ -4,19 +4,10 @@ import calculator.domain.CalcTarget;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputValidatorTest {
-
-    @Test
-    @DisplayName("구분자 없이 입력되는 값에 대한 검증")
-    void validOnlyStringInput() {
-        // given
-        String input = "1234321";
-        CalcTarget calcTarget = CalcTarget.input(input);
-        // when&then
-        assertThrows(IllegalArgumentException.class, () -> InputValidator.validate(calcTarget));
-    }
 
     @Test
     @DisplayName("구분자 뒤에 숫자가 없는 값에 대한 검증")
