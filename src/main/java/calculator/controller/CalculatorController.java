a5e41b9db114e3b61500afd696ad499b897effc6
jae woo Jung <awfjol2008@gmail.com>
2024-10-21 20:02:04 +0900

    refactor: CalculatorController 클래스 내부 로직을 가독성있게 메서드로 분리

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -22,17 +22,44 @@ public class CalculatorController {
 	}
 
 	public void run() {
+		String input = readInput();
+
+		Delimiter delimiter = new Delimiter();
+		addCustomDelimiter(input, delimiter);
+
+		List<Integer> numbers = extractNumbers(input, delimiter);
+
+		int result = calculate(numbers);
+		printResult(result);
+	}
+
+	private String readInput() {
+		outputView.printReadCommand();
 		String input = inputView.readInput();
 		inputView.closeRead();
 
-		Delimiter delimiter = new Delimiter();
+		return input;
+	}
+
+	private void addCustomDelimiter(String input, Delimiter delimiter) {
 		Optional<String> customDelimiter = customDelimiterProcessor.extractCustomDelimiter(input);
 		customDelimiter.ifPresent(delimiter::addCustomDelimiter);
-
-		String processedInput = customDelimiterProcessor.removeCustomDelimiterPattern(input);
-		List<Integer> numbers = InputFilter.extractNumbers(processedInput, delimiter);
-
-		Calculator calculator = new Calculator();
-		outputView.printResult(calculator.sum(numbers));
 	}
+
+	private List<Integer> extractNumbers(String input, Delimiter delimiter) {
+		String processedInput = customDelimiterProcessor.removeCustomDelimiterPattern(input);
+
+		return InputFilter.extractNumbers(processedInput, delimiter);
+	}
+
+	private int calculate(List<Integer> numbers) {
+		Calculator calculator = new Calculator();
+
+		return calculator.sum(numbers);
+	}
+
+	private void printResult(int result) {
+		outputView.printResult(result);
+	}
+
 }
