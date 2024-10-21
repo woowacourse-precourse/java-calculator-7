1bd13685e7f83bce4298e8cd31a02aca5041be65
reindeer002 <lsj000424@gmail.com>
2024-10-21 10:47:00 +0900

    feat(model): change int to big integer for process large number

--- src/main/java/calculator/model/Operand.java
+++ src/main/java/calculator/model/Operand.java
@@ -1,18 +1,20 @@
 package calculator.model;
 
+import java.math.BigInteger;
+
 public class Operand {
-    private final int[] numbers;
+    private final BigInteger[] numbers;
 
     private Operand(String delimiter, String expression) {
         if (expression.isEmpty()) {
-            this.numbers = new int[] {0};
+            this.numbers = new BigInteger[] { BigInteger.ZERO };
             return;
         }
 
         String[] numbers = expression.split(delimiter);
-        this.numbers = new int[numbers.length];
+        this.numbers = new BigInteger[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            this.numbers[i] = Integer.parseInt(numbers[i]);
+            this.numbers[i] = new BigInteger(numbers[i]);
         }
     }
 
@@ -20,7 +22,7 @@ public class Operand {
         return new Operand(delimiter, expression);
     }
 
-    public int[] getNumbers() {
+    public BigInteger[] getNumbers() {
         return numbers;
     }
 }
