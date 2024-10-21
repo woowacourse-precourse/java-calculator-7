e241358bd17236dffb90a099c5e75b08e59a9252
hekim52 <acrossnightsky@gmail.com>
2024-10-21 19:28:51 +0900

    fix: 반복을 삭제함
    잘못된 값이나 'exit'을 넣기 전까지 반복됨을 삭제한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,72 +7,59 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try {
-            System.out.println("덧셈할 문자열을 입력해 주세요.");
-            String str = readLine();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = readLine();
 
-            ArrayList<Integer> tempList = new ArrayList<>();
-            ArrayList<Integer> wholeList = new ArrayList<>();
+        ArrayList<Integer> tempList = new ArrayList<>();
+        ArrayList<Integer> wholeList = new ArrayList<>();
 
-            while(!str.equals("exit")) {
-
-                boolean isCustom;
-                if(str.length() == 0) {
-                    isCustom = false;
-                } else {
-                    isCustom = (str.charAt(0) == '/') && (str.charAt(1) == '/') && (str.charAt(3) == '\\') && (str.charAt(4) == 'n');
-                }
-
-                char custom = '0';
-                if(isCustom) {
-                    custom = str.charAt(2);
-                    str = str.substring(5);
-                }
-
-                for(int i = 0; i < str.length(); i++) {
-
-                    char tempChar = str.charAt(i);
-
-                    if(Character.isDigit(tempChar)) {
-                        int tempInt = tempChar - '0';
-                        tempList.add(tempInt);
-                    } else {
-                        boolean plusCustom;
-                        if(custom != '0') {
-                            plusCustom = (tempChar == ',') || (tempChar == ':') || (tempChar == custom);
-                        } else {
-                            plusCustom = (tempChar == ',') || (tempChar == ':');
-                        }
-
-                        if(plusCustom) {
-                            tempListToWholeList(tempList, wholeList);
-                        } else {
-                            throw new IllegalArgumentException("잘못된 값이 입력되었습니다.");
-                        }
-                    }
-                }
-
-                if (tempList.size() != 0) {
-                    tempListToWholeList(tempList, wholeList);
-                }
-
-                int finalSum = 0;
-                for(int i = 0; i < wholeList.size(); i++) {
-                    finalSum += wholeList.get(i);
-                }
-                System.out.println("결과 : " + finalSum);
-                System.out.println();
-
-                wholeList.clear();
-
-                System.out.println("덧셈할 문자열을 입력해 주세요.");
-                str = readLine();
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        boolean isCustom;
+        if(str.length() == 0) {
+            isCustom = false;
+        } else {
+            isCustom = (str.charAt(0) == '/') && (str.charAt(1) == '/') && (str.charAt(3) == '\\') && (str.charAt(4) == 'n');
         }
 
-        System.out.println("계산기가 종료되었습니다.");
+        char custom = '0';
+        if(isCustom) {
+            custom = str.charAt(2);
+            str = str.substring(5);
+        }
+
+        for(int i = 0; i < str.length(); i++) {
+
+            char tempChar = str.charAt(i);
+
+            if(Character.isDigit(tempChar)) {
+                int tempInt = tempChar - '0';
+                tempList.add(tempInt);
+            } else {
+                boolean plusCustom;
+                if(custom != '0') {
+                    plusCustom = (tempChar == ',') || (tempChar == ':') || (tempChar == custom);
+                } else {
+                    plusCustom = (tempChar == ',') || (tempChar == ':');
+                }
+
+                if(plusCustom) {
+                    tempListToWholeList(tempList, wholeList);
+                } else {
+                    throw new IllegalArgumentException("잘못된 값이 입력되었습니다.");
+                }
+            }
+        }
+
+        if (tempList.size() != 0) {
+            tempListToWholeList(tempList, wholeList);
+        }
+
+        int finalSum = 0;
+        for(int i = 0; i < wholeList.size(); i++) {
+            finalSum += wholeList.get(i);
+        }
+        System.out.println("결과 : " + finalSum);
+
+        wholeList.clear();
     }
 
     public static void tempListToWholeList(ArrayList<Integer> tempList, ArrayList<Integer> wholeList) {
