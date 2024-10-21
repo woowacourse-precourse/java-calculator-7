1607e9733c3f51f4305d6648d5cfa82efd7958f8
githyj-jang <hoo9268@gmail.com>
2024-10-21 19:46:19 +0900

    * feat: Added the ability to extract and sum numbers from strings using commas and colons as separators. - Extract numbers by separating the input string with commas (,) and colons (:) - Convert the extracted numbers to integers and add them up

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,5 +8,14 @@ public class Application {
         // 1. 입력
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        // 2. 기본 구분자로 문자열 분리, 결과 합산
+        String[] numbers = input.split("[,:]");
+
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+
     }
 }
