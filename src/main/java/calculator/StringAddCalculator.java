9d273d0f9fb18347670081cfe823034ef20ca449
yanghyen <sk0310803@naver.com>
2024-10-21 20:36:53 +0900

    feat(calculator): 입력 문자열에서 구분자 추출 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,22 +2,31 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
-  public static void main(String[] args) {
-    // 사용자 입력 받기
-    System.out.println("덧셈할 문자열을 입력해 주세요.");
-    String userInput = Console.readLine();
+  public void run() {
+    String input = readInput();
   }
 
-  // 커스텀 구분자 형식 유효성 검사
-  private static void validateInput(String userInput) throws IllegalAccessException {
+  // 사용자 입력 받기
+  private String readInput() {
+    System.out.println("덧셈할 문자열을 입력해 주세요.");
+    return Console.readLine();
+  }
+
+  // 입력 문자열에서 구분자를 추출하기
+  private String getDelimiter(String userInput) {
     if (userInput.startsWith("//")) {
-      int delimiterIndex = userInput.indexOf('\n');
-      String numbersPart = userInput.substring(delimiterIndex + 1);
-      if (numbersPart.isEmpty()) {
-        throw new IllegalAccessException("잘못된 구분자 형식입니다.");
+      int delimiterIndex = userInput.indexOf("\\n");
+      if (delimiterIndex == -1) {
+        throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다.");
       }
+      String customDelimiter = Pattern.quote(userInput.substring(2, delimiterIndex));
+      return customDelimiter + "|,|:"; // 커스텀 구분자와 기본 구분자 모두 포함
     }
+    return ",|:"; // 기본 구분자만 반환
   }
 }
