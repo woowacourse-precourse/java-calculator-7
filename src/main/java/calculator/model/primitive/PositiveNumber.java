467136a0a172b433435e436669d7fb74eef7dea1
DongYeop <s01054830455@gmail.com>
2024-10-21 19:17:18 +0900

    feat: 계산결과 오버플로우에 대한 예외처리

--- src/main/java/calculator/model/primitive/PositiveNumber.java
+++ src/main/java/calculator/model/primitive/PositiveNumber.java
@@ -4,6 +4,7 @@ public class PositiveNumber {
 
     private static final long MIN_RANGE = 1L;
     private static final String POSITIVE_NUMBER_EXCEPTION_MESSAGE = "양수를 입력해야 합니다.";
+    private static final String SUM_OVERFLOW_EXCEPTION_MESSAGE = "계산 결과의 범위를 초과했습니다.";
 
     private final long number;
 
@@ -27,6 +28,21 @@ public class PositiveNumber {
     }
 
     public long addNumber(long sum) {
+        validateOverflow(sum);
         return sum + this.number;
+    }
+
+    private void validateOverflow(long sum) {
+        if (isSumOverflow(sum)) {
+            throw new ArithmeticException(SUM_OVERFLOW_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean isSumOverflow(long sum) {
+        return sum > calculateSafeOverFlowNumber();
+    }
+
+    private long calculateSafeOverFlowNumber() {
+        return Long.MAX_VALUE - this.number;
     }
 }
