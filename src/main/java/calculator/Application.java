28879f3a9359374b05b3c2395b6dd42375d20dc1
[CLOUD4] 백승주 <bsj5967@naver.com>
2024-10-21 15:40:20 +0900

    feat: 구분자 감지 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,5 +8,17 @@ public class Application {
 
         // Console.readLine()으로 사용자로부터 입력 받기
         String input = Console.readLine();
+
+        String separator = "[,:]";
+        String numbers = input;
+
+        // 커스텀 구분자가 있는지 확인
+        if (input.startsWith("//")) {
+            int newlineIndex = input.indexOf("\n"); // 첫 번째 줄바꿈 문자의 인덱스 찾기
+            if (newlineIndex != -1) {  // 줄바꿈 문자가 있는 경우에만
+                // 커스텀 구분자 추출 및 숫자 부분 분리
+                separator = input.substring(2, newlineIndex); //줄바꿈 전까지의 문자열을 구분자로 설정
+                numbers = input.substring(newlineIndex + 1);
+            }
     }
 }
