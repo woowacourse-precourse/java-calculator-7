1695feeb6cd1b5f2407c9b77d0e367786decaf7e
ssuukko <tlstpstpdl11@gmail.com>
2024-10-21 19:42:39 +0900

    Refactor: NumberParser 클래스를 추상화된 NumberValidator로 리팩토링하였습니다. - 숫자 검증 기능을 static 메서드로 변경하여 재사용성을 높였습니다. - 음수 검증과 숫자 판별 로직을 별도의 메서드로 분리하여 가독성을 개선하였습니다. - 클래스명을 NumberParser에서 NumberValidator로 변경하였습니다.

--- src/main/java/calculator/NumberValidator.java
+++ src/main/java/calculator/NumberValidator.java
@@ -1,11 +1,14 @@
 package calculator;
 
-public class NumberParser {
-    public int parse(String str) {
+public abstract class NumberValidator {
+    public static int validate(String str) {
         if (!isNumeric(str)) {
             throw new IllegalArgumentException("잘못된 입력입니다: " + str);
         }
+        return validateIfNegative(str);
+    }
 
+    private static int validateIfNegative(String str) {
         int number = Integer.parseInt(str);
         if (number < 0) {
             throw new IllegalArgumentException("음수는 허용되지 않습니다.");
@@ -13,10 +16,7 @@ public class NumberParser {
         return number;
     }
 
-    private boolean isNumeric(String str) {
-        if (str == null || str.isEmpty()) {
-            return false;
-        }
+    private static boolean isNumeric(String str) {
         for (char c : str.toCharArray()) {
             if (!Character.isDigit(c)) {
                 return false;
