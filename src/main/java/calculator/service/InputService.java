b958c239b06eab69de04f1cdc2a2e06558229dcf
gyuoo <yeomky98@naver.com>
2024-10-21 22:46:18 +0900

    feat: add constants for delimiters

--- src/main/java/calculator/service/InputService.java
+++ src/main/java/calculator/service/InputService.java
@@ -1,8 +1,16 @@
 package calculator.service;
 
+import java.util.ArrayList;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputService {
+
+	private static final int ZERO = 0;
+	private static final String DEFAULT_DELIMITERS = "[,:]";
+	private static final String CUSTOM_DELIMITER_PREFIX = "//";
+	private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+	private static final ArrayList<String> CUSTOM_DELIMITERS = new ArrayList<>();
 
 	public String readLine() {
 		return Console.readLine();
