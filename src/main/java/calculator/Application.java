7c2a3d68a800bfbbeef0f4640e0774fc7ba4c17b
mingdodev <mingdo.dev@gmail.com>
2024-10-21 22:03:59 +0900

    feat: save the result using sum of extracted numbers
    Set the code to handle numbers at even index of string, the separators to odd index of string. Calculating the sum is only occurs at the code of handling numbers.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,6 +8,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int sum = 0;
+        char c;
+        char customSep;
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputStr = readLine();
@@ -18,13 +20,41 @@ public class Application {
         boolean isCustomForm = m.find();
 
         if (isCustomForm) {
-            // 정상적인 커스텀 구분자 입력인 경우
-            // 문자와 숫자의 입력을 받고 각각 예외 처리 적용
+            // 커스텀 구분자 추출 및 커스텀 구분자 지정 형식 제거하기
+            customSep = inputStr.charAt(5);
+            inputStr = inputStr.substring(5);
+
+            for (int i = 0; i < inputStr.length(); i++) {
+                c = inputStr.charAt(i);
+                // 짝수 번째 인덱스는 숫자 입력
+                if (i % 2 == 0) {
+                    // 숫자 관련 예외 처리: 숫자가 아닌지 확인, 음수인지 확인
+                    // 결과값에 더하기
+                    sum += Character.getNumericValue(c);
+                }
+
+                // 홀수 번째 인덱스는 문자 입력
+                if (i % 2 ==1) {
+                    // 문자 관련 예외 처리: 지정된 구분자를 사용했는지 확인
+                }
+            }
         }
 
         if (!isCustomForm) {
-            // 비정상적인 커스텀 구분자 입력도 여기로 들어오지만, 숫자 또는 문자 입력 확인에서 예외 처리가 된다.
-            // 문자와 숫자의 입력을 받고 각각 예외 처리 적용
+            for (int i = 0; i < inputStr.length(); i++) {
+                c = inputStr.charAt(i);
+                // 짝수 번째 인덱스는 숫자 입력
+                if (i % 2 == 0) {
+                    // 숫자 관련 예외 처리: 숫자가 아닌지 확인, 음수인지 확인
+                    // 결과값에 더하기
+                    sum += Character.getNumericValue(c);
+                }
+
+                // 홀수 번째 인덱스는 문자 입력
+                if (i % 2 ==1) {
+                    // 문자 관련 예외 처리: 지정된 구분자를 사용했는지 확인
+                }
+            }
         }
 
         System.out.println("결과 : " + sum);
