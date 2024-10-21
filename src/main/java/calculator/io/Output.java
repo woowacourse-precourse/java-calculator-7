920d5f8474340f97aa2883b9d17038f0b367b00a
장시영 <sysmile77@gmail.com>
2024-10-21 10:55:06 +0900

    fix(Output): 결과메시지 출력부분 띄어쓰기

--- src/main/java/calculator/io/Output.java
+++ src/main/java/calculator/io/Output.java
@@ -3,6 +3,6 @@ package calculator.io;
 public class Output {
     public static void printOutMessage(int sumVal) {
 
-        System.out.print("결과: "+sumVal);
+        System.out.print("결과 : "+sumVal);
     }
 }
