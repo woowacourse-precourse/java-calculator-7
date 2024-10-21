6213d35d6abb33159a8aac0666e5bb59bdb949d7
woorimj <forestwr06@gmail.com>
2024-10-21 14:56:08 +0900

    Feat: 기본구분자 처리 후 숫자더하기 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,6 +9,19 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = sc.nextLine();
 
-        System.out.println("결과 : " );
+        System.out.println("결과 : " + sum(input));
+    }
+
+    public static int sum(String input) {
+        String[] nums = input.split("[,:]");
+
+        int total = 0;
+        for (String n : nums) {
+            if (!n.isEmpty()) {
+                int num = Integer.parseInt(n);
+                total += num;
+            }
+        }
+        return total;
     }
 }
