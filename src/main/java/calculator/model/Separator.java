32a87616b6f717ef08d84ad0c3ce096ae45c845d
coolman0123 <socool4m@naver.com>
2024-10-21 12:10:05 +0900

    feat(Separator): 예외처리 코드 적용
    - 사용자 입력에 대한 유효성 검사를 강화하기 위해 예외처리 로직 추가
    - 잘못된 분리자 처리시 적절한 예외를 발생시키도록 수정
    - handleCustomSeparator, pattern.quote 삭제

--- src/main/java/calculator/model/Separator.java
+++ src/main/java/calculator/model/Separator.java
@@ -7,15 +7,12 @@ public class Separator {
     public static final String DEFAULT_SEPARATOR = ",|:";
     public static String activeSeparator;
 
-
-
     public static String checkCustomSeparator(String input) {
         if (input.startsWith("//")) {
-            System.out.println("커스텀 있음");
             handleCustomSeparator(input);
+            SeparatorValidator.validateCustomDelimiter(input);
             return cleanInput(input);
     }
-
         return input;
     }
 
@@ -23,25 +20,26 @@ public class Separator {
     private static void handleCustomSeparator(String input) {
         Matcher matcher = Pattern.compile("//(.)\\\\n").matcher(input);
         if (matcher.find()) {
-            String customSeparator = Pattern.quote(matcher.group(1));
-            System.out.println("커스텀 구분자 : " + customSeparator);
+            String customSeparator = matcher.group(1);
+            SeparatorValidator.validateCustomDelimiterLength(customSeparator);
             activeSeparator = DEFAULT_SEPARATOR + "|" + customSeparator;
-            System.out.println("활성 구분자: " + activeSeparator);
+            SeparatorValidator.validateOnlyCustomDelimiterUsed(input);
         }
     }
 
     public static String cleanInput(String input) {
-        System.out.println("현재 input 상태 : " + input);
         String[] parts = input.split("\\\\n", 2);
 
-        return parts[1];
+        if (parts[1].isEmpty()) {
+            parts[1] = "0";
 
+            return parts[1];
+        }
+
+        return parts[1];
     }
 
     public static String[] splitInput(String token){
-        String[] tokens = token.split(activeSeparator);
-        return tokens;
+        return token.split(activeSeparator);
     }
-
-
 }
