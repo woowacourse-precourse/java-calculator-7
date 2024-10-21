cc520f4d964cb461861c2a2bc981883ef212b884
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-10-21 19:16:18 +0900

    feat : 커스텀 구분자 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 
     private static final String NEGATIVE_NUMBER_EXCEPTION_MESSAGE = "숫자는 양수 값이어야 합니다.";
@@ -21,9 +23,25 @@ public class Application {
             numberList = new ArrayList<>();
 //            커스텀 구분자 여부 체크
             String delimiter;
+            if(input.startsWith(CUSTOM_DELIMITER_START_STR)) {
+                input = input.substring(2);
+                delimiter = customDelimiter(input);
+                int idx = input.indexOf(CUSTOM_DELIMITER_END_STR);
+                input = input.substring(idx + 2);
+            }
+            String[] inputArray = input.split(delimiter);
         }   catch (Exception e){
             throw new IllegalArgumentException(INPUT_VALIDATION_EXCEPTION_MESSAGE);
         }
+    }
+
+    public static String customDelimiter(String input){
+//        ||와 \n 사이에 커스텀 구분자가 있는 경우
+        int idx = input.indexOf(CUSTOM_DELIMITER_END_STR);
+        if(idx > 0){
+            return input.substring(0, idx);
+        }
+        throw new IllegalArgumentException(INPUT_VALIDATION_EXCEPTION_MESSAGE);
     }
 
     }
