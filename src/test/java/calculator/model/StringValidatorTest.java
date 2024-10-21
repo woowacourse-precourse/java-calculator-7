38830bbc36b65b6751b64516ae2a36a663a3b6a8
Chan531 <chan657312@gmail.com>
2024-10-21 19:22:52 +0900

    fix(StringValidatorTest): input 오타로 인한 오류 수정

--- src/test/java/calculator/model/StringValidatorTest.java
+++ src/test/java/calculator/model/StringValidatorTest.java
@@ -25,7 +25,7 @@ class StringValidatorTest {
 
 	@ParameterizedTest
 	@DisplayName("커스텀 구분자가 포함된 문자열이 입력되었을 때 에러 없이 이를 검증한다.")
-	@ValueSource(strings = {"//#\\n1,2:3#4", "//?\\n1?2?3", "//\\\n1\\2\\3"})
+	@ValueSource(strings = {"//#\\n1,2:3#4", "//?\\n1?2?3", "//\\\\n1\\2\\3"})
 	void 커스텀_구분자가_포함된_문자열이_입력되었을_때_검증한다(String input) {
 		// given
 		int customDelimiterIndex = 2;
