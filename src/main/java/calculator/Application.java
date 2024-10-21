6f043d54e4f33246567fd9cbe5e3581f1e764502
31n5ang <31n5ang@gmail.com>
2024-10-21 22:13:01 +0900

    feat: 입출력 기능 구현
    사용자의 입력을 받고, DelimiterManager와 Separator 클래스를 사용해 결과값을 출력합니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.Optional;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        DelimiterManager delimiterManager = new DelimiterManager(',', ':');
+        Separator separator = new Separator(input, delimiterManager);
+
+        separator.extractCustomDelimiter("//", "\\n");
+        List<String> separateList = separator.separate();
+        int result = Separator.getSum(separateList);
+
+        System.out.println("결과 : " + result);
     }
 }
