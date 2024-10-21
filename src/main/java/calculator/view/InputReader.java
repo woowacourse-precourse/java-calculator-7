d11e43067093b10043d5b320ee24a01af6ed7379
coolman0123 <socool4m@naver.com>
2024-10-21 12:10:48 +0900

    feat(InputReader): 빈 값일시 0, null은 예외처리

--- src/main/java/calculator/view/InputReader.java
+++ src/main/java/calculator/view/InputReader.java
@@ -1,11 +1,17 @@
 package calculator.view;
 
+import calculator.model.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputReader {
 
     public static String readInput(){
         String input = Console.readLine();
+        InputValidator.validateNotNull(input);
+        if (input.isEmpty()) {
+            input = "0";
+            return input;
+        }
 
         return input;
     }
