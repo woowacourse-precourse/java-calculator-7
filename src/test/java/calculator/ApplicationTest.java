a097b9e808f1368faff79fc0dcf95c369063d7ef
AppleMint98 <hong721816@konkuk.ac.kr>
2024-10-21 17:32:18 +0900

    test : 기본 구분자 중 단일 구분자만 사용 시

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -18,6 +18,14 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 기본_구분자_사용_단일_구분자() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
