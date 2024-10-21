88f29d3087e667c6ae30b93a968de2f073201cec
sytTest <syt1128@naver.com>
2024-10-21 21:19:40 +0900

    :sparkle: (ExpressionController) add divideUserExpression()
    표현식 객체에 커스텀 구분자 후보와 실제 식을 입력해 커스텀 구분자 검증을 진행할divideUserExpression() 추가

--- src/main/java/calculator/controller/ExpressionController.java
+++ src/main/java/calculator/controller/ExpressionController.java
@@ -1,6 +1,7 @@
 package calculator.controller;
 
 import calculator.domain.UserExpression;
+import calculator.domain.UserExpressionDivide;
 import calculator.repository.DelimiterRepository;
 
 public class ExpressionController {
@@ -11,5 +12,9 @@ public class ExpressionController {
     private void createExpression(UserExpression userExpression) {
         this.userExpression = userExpression;
         delimiterRepository = new DelimiterRepository();
+    }
+
+    private void divideUserExpression() {
+        UserExpressionDivide.dividePart(userExpression);
     }
 }
