3e607c6a2996a439a5d1ad5a91a02a2109f1cbb8
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-21 17:05:12 +0900

    feat: 공백 예외상황 제거를 위한 공백제거 함수 추가 구현
    getNoSpaceInput으로 문자열 전체 공백 제거

--- src/main/java/util/Calculator.java
+++ src/main/java/util/Calculator.java
@@ -11,18 +11,23 @@ public class Calculator {
     public void run() {
         String input = InputView.readLine();
         Validator.isEmpty(input);
-        String delimiterType = DelimiterHandler.detectDelimiterType(input);
+        String noSpaceInput = getNoSpaceInput(input);
+        String delimiterType = DelimiterHandler.detectDelimiterType(noSpaceInput);
         if (delimiterType.equals("Basic")) {
-            int result = Calculator.getSum(BasicDelimiter.getNumber(input));
+            int result = Calculator.getSum(BasicDelimiter.getNumber(noSpaceInput));
             OutputView.printResult(result);
             return;  // Basic 구분자가 처리되면 일찍 종료
         }
         // Custom 구분자 처리
-        int result = getSum(CustomDelimiter.getNumber(input));
+        int result = getSum(CustomDelimiter.getNumber(noSpaceInput));
         OutputView.printResult(result);
     }
 
-    public static int getSum(int[] numbers) {
+    private static String getNoSpaceInput(String input) {
+        return input.replaceAll("\\s+", "");
+    }
+
+    private static int getSum(int[] numbers) {
         int sum = 0;
         for (int i : numbers) {
             sum += i;
