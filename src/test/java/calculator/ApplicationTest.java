2b59e0f4fca38b1ffbab098b168cb86b6cd99ba9
이예은 <162159470+lye5615@users.noreply.github.com>
2024-10-21 22:57:20 +0900

    Update ApplicationTest.java

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,3 +1,9 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
