4b9c2b82f5314a03b43117aa3bbfb6d10bf4c015
BusanGukbap <dong2857210@gmail.com>
2024-10-21 18:24:25 +0900

    feat(calc) : 문자열 숫자 계산 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,8 +1,18 @@
 package calculator;
 
 import java.util.regex.Pattern;
+import java.lang.String;
 
 public class StringCalculator {
+
+    public int calc(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String[] numbers = splitInput(input);
+        return sumNumbers(numbers);
+    }
 
     private String[] splitInput(String input) {
         String delimiter = ",|:"; // 기본 구분자
@@ -13,5 +23,25 @@ public class StringCalculator {
         }
 
         return input.split(delimiter);
+    }
+
+    private int sumNumbers(String[] numbers) {
+        int sum = 0;
+        for (String number : numbers) {
+            int num;
+            try {
+                num = Integer.parseInt(number);
+            } catch(NumberFormatException e) {
+                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다: " + number);
+            }
+
+            if (num < 0) {
+                throw new IllegalArgumentException("음수는 허용되지 않습니다: " + num);
+            }
+
+            sum += num;
+        }
+
+        return sum;
     }
 }
