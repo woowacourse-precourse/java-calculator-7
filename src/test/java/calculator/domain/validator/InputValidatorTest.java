caf48d022a46f157086c91261b771a5a176d0d05
gladhee <gladhee@icloud.com>
2024-10-21 17:20:13 +0900

    Test(InputValidator): 네이밍 명확하게 수정

--- src/test/java/calculator/domain/validator/InputValidatorTest.java
+++ src/test/java/calculator/domain/validator/InputValidatorTest.java
@@ -9,19 +9,19 @@ public class InputValidatorTest {
     private final InputValidator inputValidator = new InputValidator();
 
     @Test
-    void isMinus() {
+    void 양수가_아닌_문자_테스트() {
         assertThat(inputValidator.isNonPositiveNumber("1")).isFalse();
         assertThat(inputValidator.isNonPositiveNumber("-1")).isTrue();
     }
 
     @Test
-    void checkInputEmpty() {
+    void 입력_아무것도_없을_때_테스트() {
         assertThat(inputValidator.checkInputEmpty("")).isTrue();
         assertThat(inputValidator.checkInputEmpty("1")).isFalse();
     }
 
     @Test
-    void isNumeric() {
+    void 숫자가_아닌_문자_들어왔을_때_테스트() {
         assertThat(inputValidator.isValidNumber("1")).isTrue();
         assertThat(inputValidator.isValidNumber("a")).isFalse();
         assertThat(inputValidator.isValidNumber("\\")).isFalse();
