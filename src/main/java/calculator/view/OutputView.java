0b9e460dc7fdc9ab999179be05e65af9e999b48f
Si Yeong Park <psy915@swu.ac.kr>
2024-10-21 21:58:27 +0900

    fix(OutputView): 예외 발생 시 오류 메시지 출력

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -4,4 +4,7 @@ public class OutputView {
     public static void printResult(int result) {
         System.out.println("결과 : " + result);
     }
+    public static void printError(String errorMessage) {
+        System.out.println("오류 : " + errorMessage);
+    }
 }
