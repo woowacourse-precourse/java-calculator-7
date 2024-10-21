1fcba0507ced5c78b6807f68ebd4713240abd102
kkonii <119216472+kkonii@users.noreply.github.com>
2024-10-21 20:50:08 +0900

    refactor(parser,calculatorService) : 객체명 및 메소드명 변경

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -4,23 +4,22 @@ import calculator.domain.CustomDelimiter;
 import calculator.domain.Numbers;
 import calculator.domain.validation.NumberInputValidator;
 import calculator.util.InputFilter;
-import calculator.util.Splitter;
+import calculator.util.Parser;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CalculatorService {
 
     private static final NumberInputValidator numberInputValidator = new NumberInputValidator();
-
     private static final int DEFAULT_SIZE = 1;
     private static final int CUSTOM_SIZE = 3;
 
     public Numbers extractNumbers(String input) {
         InputFilter.doOutWrong(input);
-        String[] splitValues = Splitter.splitOut(input);
+        String[] elements = Parser.splitOutByCustomStyle(input);
 
-        List<String> splitNumbers = splitWithDelimiter(splitValues);
-        Numbers numbers = validateAndParse(splitNumbers);
+        List<String> numberInputs = splitWithDelimiter(elements);
+        Numbers numbers = validateAndParse(numberInputs);
 
         return numbers;
     }
@@ -28,23 +27,23 @@ public class CalculatorService {
     private Numbers validateAndParse(List<String> splitNumbers) {
         List<Integer> numberList = splitNumbers
                 .stream()
-                .filter(str -> !str.trim().isEmpty())
+                .filter(input -> !input.trim().isEmpty())
                 .map(numberInputValidator::validateOnlyNumeric)
                 .toList();
 
         return new Numbers(numberList);
     }
 
-    private List<String> splitWithDelimiter(String[] splitValues) {
+    private List<String> splitWithDelimiter(String[] elements) {
         List<String> numbers = new ArrayList<>();
 
-        if (splitValues.length == CUSTOM_SIZE) {
-            CustomDelimiter delimiter = new CustomDelimiter(splitValues[1]);
-            numbers = Splitter.splitToCustom(delimiter, splitValues);
+        if (elements.length == CUSTOM_SIZE) {
+            CustomDelimiter delimiter = new CustomDelimiter(elements[1]);
+            numbers = Parser.parseToCustom(delimiter, elements);
         }
 
-        if (splitValues.length == DEFAULT_SIZE) {
-            numbers = Splitter.splitToDefault(splitValues);
+        if (elements.length == DEFAULT_SIZE) {
+            numbers = Parser.parseToDefault(elements);
         }
 
         return numbers;
