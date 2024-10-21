d759b89ed28f85658d73d52cdec161333bf922c5
sytTest <syt1128@naver.com>
2024-10-21 20:29:55 +0900

    :sparkle: (Prep) add dicedChunk()
    문자열인 입력식과 구분자를 입력받아 구분자 기준으로 리스트를 만들며 공백은 제거하여 문자열 리스트를 반환하는 dicedChunk() 추가

--- src/main/java/calculator/domain/Prep.java
+++ src/main/java/calculator/domain/Prep.java
@@ -1,4 +1,12 @@
 package calculator.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Prep {
+
+    public static List<String> dicedChunk(String input, String delimiter) {
+        return Arrays.stream(input.split(delimiter)).filter(s ->!s.isEmpty()).toList();
+    }
+
 }
