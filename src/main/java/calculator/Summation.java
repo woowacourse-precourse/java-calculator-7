2f695c5677c4d97590f468e984bfc5980ca04c6e
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2024-10-21 19:52:24 +0900

    feat(Summation): sum
    구현

---
+++ src/main/java/calculator/Summation.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class Summation {
+    Long sum;
+    Long[] numbers;
+    Summation(Long[] numbers) {
+        this.numbers = numbers;
+        this.sum = 0L;
+    }
+
+    void sum() {
+        for (Long number : this.numbers) {
+            sum += number;
+        }
+    }
+}
