cb76e0c6bef0c0555c219eb6f863b93516e4f5d2
naraenar <hnar6ee@gmail.com>
2024-10-21 19:17:03 +0900

    refactor: 문자열 검증 로직 변경
    문자열 검증 로직 변경

--- src/main/java/calculator/TextCalculator.java
+++ src/main/java/calculator/TextCalculator.java
@@ -9,22 +9,15 @@ public class TextCalculator {
 
     // 문자열이 유효한지 검사
     public void validateText(String inputText) {
-        String customDelimiterRegex = "^//([^0-9\\\\]*)\\\\n";
-        Pattern customDelimiterPattern = Pattern.compile(customDelimiterRegex);
-        Matcher customDelimiterMatcher = customDelimiterPattern.matcher(inputText);
-
-        // 문자열 앞에 커스텀 구분자 선언이 있을 경우
-        if(customDelimiterMatcher.find()) {
-            if(customDelimiterMatcher.group(1).length() > 1) {
-                throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다");
-            }
+        // 문자열에 기본 구분자와 숫자 외 다른 문자가 있는지 검사
+        String textRegex = "[0-9,:]+$";
+        if(inputText.matches(textRegex)) {
             return;
         }
 
-        String textRegex = "[0-9,:]+$";
-
-        // 문자열에 기본 구분자와 숫자 외 다른 문자가 있을 경우
-        if(!inputText.matches(textRegex)) {
+        // 커스텀 구분자가 형식에 맞게 있는지 검사
+        String customDelimiterRegex = "^//([^0-9\\\\]?)\\\\n[0-9,:]+$";
+        if(!inputText.matches(customDelimiterRegex)) {
             throw new IllegalArgumentException("문자열의 형식이 올바르지 않습니다.");
         }
     }
