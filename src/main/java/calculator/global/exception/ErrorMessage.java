f6d17336b3d6ee445a060e82c3d08b5f44da0b46
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-21 13:39:11 +0900

    style(ErrorMessage): 일정한 코드 포맷팅을 위해 개행 추가

--- src/main/java/calculator/global/exception/ErrorMessage.java
+++ src/main/java/calculator/global/exception/ErrorMessage.java
@@ -1,6 +1,7 @@
 package calculator.global.exception;
 
 public enum ErrorMessage {
+
     INVALID_DELIMITER_ERROR("올바르지 않은 구분자 입력입니다."),
     NUMBER_STRING_FORMAT_ERROR("구분자 사이에 숫자를 입력해주세요."),
     NEGATIVE_NUMBER_ERROR("음수는 입력할 수 없습니다."),
