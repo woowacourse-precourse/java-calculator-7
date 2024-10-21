b33db9cd25d3147eaf485d9a6c582e8f72cf7344
mikekks <thdalsrb79@naver.com>
2024-10-21 10:29:30 +0900

    feat(Numbers): 숫자 목록 일급컬렉션 구현
    1. 객체 생성할 때, 양수인지 검증
    2. 숫자 목록의 요소들을 모두 더하는 기능

---
+++ src/main/java/calculator/Numbers.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import java.util.List;
+
+public class Numbers {
+	private final List<Integer> numbers;
+
+	public Numbers(List<Integer> numbers) {
+		validateIsPositiveNumber(numbers);
+		this.numbers = numbers;
+	}
+
+	public void validateIsPositiveNumber(List<Integer> numbers){
+		boolean hasNonPositive = numbers.stream()
+			.anyMatch(number -> number <= 0);
+
+		if (hasNonPositive) {
+			throw new IllegalArgumentException("입력된 숫자 중에 음수 또는 0이 있습니다.");
+		}
+	}
+
+	public int sumNumbers() {
+		return numbers.stream()
+			.mapToInt(Integer::intValue)
+			.sum();
+	}
+}
