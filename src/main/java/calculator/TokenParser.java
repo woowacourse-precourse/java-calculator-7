2e7c98fed47a7df91b83c17c694ba238ecced87a
caboodles <sanghyup2000@gmail.com>
2024-10-21 20:39:46 +0900

    feat(calculator): add TokenParser class
    converts token strings to NumberTokens

---
+++ src/main/java/calculator/TokenParser.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TokenParser {
+    private TokenParser() {
+        //인스턴스 생성 안됨
+    }
+    
+    public NumberTokens parseTokens(String[] tokens, boolean decimalAllowed) {
+        List<NumberToken> numberTokens = new ArrayList<>();
+        for (String tokenStr : tokens) {
+            NumberToken token = new NumberToken(tokenStr, decimalAllowed);
+            numberTokens.add(token);
+        }
+        return new NumberTokens(numberTokens);
+    }
+}
