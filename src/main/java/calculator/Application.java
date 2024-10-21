d686c52df8fa5313fc11dfcdba9ebb98653db5c7
KF97 <aysul0@naver.com>
2024-10-21 13:25:32 +0900

    feat: 기본 구분자 처리 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,12 +5,20 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int result = 0;
+
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         try {
             String str = Console.readLine();
-            
+
             if (str.equals("")) {
                 System.out.println("결과 : 0");
+            } else {
+                String[] numbers = str.split(",|:");
+                for (String s : numbers) {
+                    result += Integer.parseInt(s);
+                }
+                System.out.println("결과 : " + result);
             }
 
         } catch (IllegalArgumentException e) {
