f66f1e6604d9cfd4e1d3bcf5fb9f616ba347ee93
sytTest <syt1128@naver.com>
2024-10-21 21:36:55 +0900

    :sparkle: create CalcController
    계산 비즈니스 로직을 다룰 CalcController 클래스 생성

--- src/main/java/calculator/controller/CalcController.java
+++ src/main/java/calculator/controller/CalcController.java
@@ -1,5 +1,8 @@
 package calculator.controller;
 
+import calculator.domain.Calc;
+import calculator.repository.NumberRepository;
+
 public class CalcController {
     private Calc calc;
     private NumberRepository numberRepository;
