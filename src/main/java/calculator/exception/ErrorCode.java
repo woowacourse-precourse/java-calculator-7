9e38d197c61d217e77ae0646c276d9c6723ff883
yunhacandy <yunhacandy@gmail.com>
2024-10-21 11:48:56 +0900

    style (ErrorCode): NON_POSITIVE_NUMBER 오류에 대한 설명 수정

--- src/main/java/calculator/exception/ErrorCode.java
+++ src/main/java/calculator/exception/ErrorCode.java
@@ -5,7 +5,7 @@ public enum ErrorCode {
     INVALID_CUSTOM_SEPARATOR_LENGTH("커스텀 구분자는 한 글자만 가능합니다."),
     BLANK_CUSTOM_SEPARATOR("커스텀 구분자는 공백이 될 수 없습니다."),
     NUMERIC_CUSTOM_SEPARATOR("커스텀 구분자는 숫자가 될 수 없습니다."),
-    NON_POSITIVE_NUMBER("숫자는 1 이상의 양수여야 합니다."),
+    NON_POSITIVE_NUMBER("음수가 입력되었습니다."),
     INVALID_NUMBER_FORMAT("문자열은 숫자와 구분자로만 이루어져야 합니다.");
 
     private final String message;
