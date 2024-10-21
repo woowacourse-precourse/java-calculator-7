eae343cec09ce7db8b5aca39eebf729033a1daaa
chaeyeon0130 <k84292330@gmail.com>
2024-10-21 20:28:07 +0900

    feat(Application): 입력값이 null이거나 비어 있는 경우 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,14 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        int result = calculate(input);
+        System.out.println("결과 : " + result);
+    }
+
+    public static int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
     }
 }
