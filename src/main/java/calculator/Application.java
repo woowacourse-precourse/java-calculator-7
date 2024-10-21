65504f19bcec3e72eeffd632f72fe377519dae12
mango606 <yuj08184@kangwon.ac.kr>
2024-10-21 18:16:19 +0900

    feat: 사용자 입력 처리 및 빈 문자열 또는 null일 경우 0 반환 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,35 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine().replace("\"", "").trim();
+
+        try {
+            StringAdditionCalculator calculator = new StringAdditionCalculator(input);
+            int result = calculator.calculateSum();
+            System.out.print("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+    }
+}
+
+class StringAdditionCalculator {
+    private final String input;
+    private String delimiter = ",|:";
+
+    public StringAdditionCalculator(String input) {
+        this.input = input;
+        extractDelimiter();
+    }
+
+    private void extractDelimiter() {
+    }
+
+    public int calculateSum() {
+        return 0;
     }
 }
