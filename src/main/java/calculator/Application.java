03face048480090af4776a5fc913408bbdd49a00
RedSunSmile <naver-or@hanmail.net>
2024-10-21 23:52:48 +0900

    사용자입력로직과 자동출력정의

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import java.util.*;
+
 public class Application {
+    private TextNumber number;
+    private int sum;
+
+    public Application(){
+        this.number=new TextNumber();
+    }
+
+    public String changingNumber(String insert) {
+
+        return number.calculatingNumberPlus(insert);
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application change = new Application();
+        System.out.println("뎃셈할 문자열을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        String insert = sc.nextLine();
+        String result=change.changingNumber(insert);
+        System.out.println("결과: "+result);
+
     }
 }
