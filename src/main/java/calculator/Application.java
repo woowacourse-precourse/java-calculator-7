0388c5054c0bdde36e6e84be3c3dd833af84bc93
mingyeongwon <wonmingy@gmail.com>
2024-10-21 20:51:36 +0900

    feat: 커스텀 구분자 지정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,18 +4,30 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
 
         // 문자열 분리
         String input = Console.readLine();
-        String[] inputNumbers = input.split(",|:");
         int sum = 0;
+        String delimiter = ",|:";
 
-        for(String number : inputNumbers) {
-            sum += Integer.parseInt(number);
+        if (input == null || input.isEmpty()) {
+            System.out.println("결과 : 0");
+            return;
         }
+
+        if(input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\n"); // \n 없으면 -1 출력됨
+            if(delimiterIndex != -1) {
+                // 커스텀 구분자
+                delimiter = input.substring(2, delimiterIndex);
+                // 커스텀 구분자 이후 숫자 문자열
+                input = input.substring(delimiterIndex + 1);
+            }
+        }
+
+        String[] inputNumbers = input.split(delimiter);
 
         System.out.println("결과 : " + sum);
     }
