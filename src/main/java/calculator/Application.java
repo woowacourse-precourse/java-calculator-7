a26a8c944c743e95f5d2e7dc9a0aba5a27206898
KangHyeonmin <rkdgusals369@naver.com>
2024-10-21 22:04:11 +0900

    덧셈을 수행하는 메서드드

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -21,6 +21,36 @@ public class Application {
             throw e;  
         }
     }
+    public static int add(String str) {
+        if (str == null || str.isEmpty()) {
+            return 0;
+        }
+
+        String symbol = ",|:"; 
+
+        
+        Matcher matcher = Pattern.compile("//(.)\\\\n(.*)").matcher(str);
+        if (matcher.find()) {
+        	symbol = Pattern.quote(matcher.group(1));  
+            str = matcher.group(2); 
+        }
+
+        String[] num = str.split(symbol);
+        return calculate(num);
+    }
+    
+    private static int calculate(String[] num) {
+        int sum = 0;
+        for (String n : num) {
+            if (!n.isEmpty()) {
+                int intNum = exceptionHandle(n);
+                sum += intNum;
+            }
+        }
+        return sum;
+    }
+
+
 
    
 }
