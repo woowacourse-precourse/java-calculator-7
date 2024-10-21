18001f1cad9c7add6bcb1ccd170f48b3e5d9ada0
sseopsseop <ponde119@outlook.com>
2024-10-21 17:01:04 +0900

    fix: StringValidator 클래스 수정 공백문자 입력 시 검증로직 수행하지 않도록 수정 검증할 문자열 대신 다른 문자열을 분할하도록 짜여진 코드를 수정

--- src/main/java/calculator/util/StringValidator.java
+++ src/main/java/calculator/util/StringValidator.java
@@ -6,11 +6,12 @@ import java.util.regex.Pattern;
 public class StringValidator {
     // 문자열 검증 메서드
     public static void stringValidate(String inputString) throws IllegalArgumentException{
-       checkNull(inputString);
-       checkNegativeValue(inputString);
-       if(inputString.contains("//")){
-           validateCustomDelimiter(inputString);
-       }else validateDelimiters(inputString, Constants.DEFAULT_DELIMITERS);
+        if(inputString.isBlank()) return;
+        checkNull(inputString);
+        checkNegativeValue(inputString);
+        if(inputString.contains("//")){
+            validateCustomDelimiter(inputString);
+        }else validateDelimiters(inputString, Constants.DEFAULT_DELIMITERS);
     }
 
     // 입력 문자열이 null인 경우
@@ -23,7 +24,7 @@ public class StringValidator {
     // 문자열 앞부분의 "//"와 "\n" 사이에 커스텀 구분자를 지정하지 않은 경우
     // "//"와 "\n" 사이에 2개 이상의 문자가 포함되어 있는 경우
     public static void validateCustomDelimiter(String inputString){
-        Pattern customDelimiterPattern = Pattern.compile("//(.)\n(.*)");
+        Pattern customDelimiterPattern = Pattern.compile("//(.)\\\\n(.*)");
         Matcher matcher = customDelimiterPattern.matcher(inputString);
         if(matcher.matches()){
             String customDelimiter = matcher.group(1);
@@ -37,7 +38,7 @@ public class StringValidator {
 
     // 기본 및 지정된 커스텀 구분자와 숫자 이외의 문자가 포함된 경우
     public static void validateDelimiters(String inputString, String delimiters){
-        String[] splitTexts = delimiters.split(delimiters);
+        String[] splitTexts = inputString.split(delimiters);
 
         for(String text : splitTexts){
             if(!text.matches("\\d+")) throw new IllegalArgumentException("지정하지 않은 구분자가 포함되어 있습니다.");
