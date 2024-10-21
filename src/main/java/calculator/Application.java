923c410d024aa6f6b47b3b8cc0db49409957212d
Seung Ik Lee <natwo98@gmail.com>
2024-10-21 19:06:52 +0900

    #4 feat: implement add number in user's input separated by custom separator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,18 +9,28 @@ public class Application {
         String input = Console.readLine();
 
         int result = 0;
-
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            if (ch == ',' || ch == ':') {
-                continue;
-            } else if (Character.isDigit(ch)) {
-                result += Character.getNumericValue(ch);
+        if (input.charAt(0) == '/' && input.charAt(1) == '/' && input.charAt(3) == '\\' && input.charAt(4) == 'n') {
+            char customSeparator = input.charAt(2);
+            for (int i = 5; i < input.length(); i++) {
+                char ch = input.charAt(i);
+                if (ch == customSeparator) {
+                    continue;
+                } else if (Character.isDigit(ch)) {
+                    result += Character.getNumericValue(ch);
+                }
             }
-
+        } else {
+            for (int i = 0; i < input.length(); i++) {
+                char ch = input.charAt(i);
+                if (ch == ',' || ch == ':') {
+                    continue;
+                } else if (Character.isDigit(ch)) {
+                    result += Character.getNumericValue(ch);
+                }
+            }
         }
 
         System.out.println("결과 : " + result);
-        
+
     }
 }
