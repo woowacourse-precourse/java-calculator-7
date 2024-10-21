711bddf41d73eefa2b98ed68902279feb4234ea1
Kyungtae Kim <ktaekim@Kyungtaeui-MacBookAir.local>
2024-10-21 12:51:55 +0900

    feat: Console 를 사용하여 입력을 받고 null 및 공백 체크

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 1. Console 를 사용하여 입력을 받고 null 및 공백 체크
+        String input = Console.readLine();
+        Console.close();
+
+        if (input == null || input.isEmpty()) {
+            System.out.println("sum: 0");
+            return;
+        }
+
     }
 }
