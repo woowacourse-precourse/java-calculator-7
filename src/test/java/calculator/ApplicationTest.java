9387a23b0a6c472f43289603fa043b6892bcb673
SECHANG1412 <binaryblitz1412@gmail.com>
2024-10-21 18:35:14 +0900

    git add src/test/java/calculator/ApplicationTest.java git commit -m "test: Application 클래스에 대한 JUnit 테스트 추가"

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -10,22 +10,35 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
 
     @Test
-    void customDelimiterTest() {
+    void 빈_문자열_테스트() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과: 0");
+        });
+    }
+
+    @Test
+    void 쉼표_구분자_테스트() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과: 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_테스트() {
         assertSimpleTest(() -> {
             run("//;\n1;2");
             assertThat(output()).contains("결과: 3");
         });
     }
 
-
-
-
     @Test
-    void exceptionTest() {
+    void 음수_입력_테스트() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("-1,2,3"))
+            assertThatThrownBy(() -> runException("-1,2"))
                     .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("음수는 입력할 수 없습니다");
+                    .hasMessageContaining("음수는 입력할 수 없습니다: -1");
         });
     }
 
