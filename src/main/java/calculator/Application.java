b3f9f42daf617532f3086f63bd17a9d1e918a613
chair00 <pk1029@yu.ac.kr>
2024-10-21 20:50:44 +0900

    예제 테스트를 위한 임시코드-2

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,6 +12,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("hahaha");
 //        try{
 //            // 0. 문자열 입력받기
 //            System.out.println("덧셈할 문자열을 입력해 주세요.");
