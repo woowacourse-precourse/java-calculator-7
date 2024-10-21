f3ac584cb04f116dac9a894be028d212f8a98c21
davemins <davemins4125@naver.com>
2024-10-21 09:48:10 +0900

    feat(View): 비어있는 숫자인지 검사

--- src/main/java/calculator/domain/NumberParser.java
+++ src/main/java/calculator/domain/NumberParser.java
@@ -1,6 +1,11 @@
 package calculator.domain;
 
 public class NumberParser {
+    public String[] parse(String input, String delimiters) {
+        Validator.validate(input);
+        String processedInput = getSplittedString(input);
+        return processedInput.split(delimiters);
+    }
 
     private String getSplittedString(String input) {
         if (input.startsWith("//")) {
@@ -8,5 +13,19 @@ public class NumberParser {
             return input.substring(delimiterEndIndex + 1);
         }
         return input;
+    }
+
+    /* Validator */
+    private static class Validator {
+
+        private static void validate(String message) {
+            if (isBlank(message)) {
+                throw new IllegalArgumentException("비어있는 숫자가 입력되었습니다.");
+            }
+        }
+
+        private static boolean isBlank(String message) {
+            return message == null || message.isBlank(); // null 체크 추가
+        }
     }
 }
