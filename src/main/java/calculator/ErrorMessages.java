4cd09673a4a2e678b5854a93d5c61bfd2dadbf3f
soonvro <soonvro@gmail.com>
2024-10-21 20:54:05 +0900

    feat: null 인자가 들어왔을 때 에러 메시지 정의

--- src/main/java/calculator/ErrorMessages.java
+++ src/main/java/calculator/ErrorMessages.java
@@ -4,7 +4,8 @@ public enum ErrorMessages {
     NOT_EXIST_CUSTOM_DELIMITER_SUFFIX("커스텀 구분자의 끝이 존재하지 않습니다."),
     NOT_EXIST_CUSTOM_DELIMITER("커스텀 구분자가 존재하지 않습니다."),
     MULTI_CUSTOM_DELIMITER("커스텀 구분자는 한 개만 정의할 수 있습니다."),
-    INVALID_CUSTOM_DELIMITER("구분자가 올바르지 않습니다.");
+    INVALID_CUSTOM_DELIMITER("구분자가 올바르지 않습니다."),
+    NULL_INPUT("입력 값이 null입니다.");
 
     private final String message;
 
