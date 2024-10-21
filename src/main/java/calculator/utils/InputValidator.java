b4021ee2fae2195ff5182bc113d2061aefb5da4f
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-21 21:13:20 +0900

    [chore] : 입력 유효성 검사 로직 일부 삭제

--- src/main/java/calculator/utils/InputValidator.java
+++ src/main/java/calculator/utils/InputValidator.java
@@ -9,7 +9,6 @@ public class InputValidator {
     private static final String CHECK_NEGATIVE_INTEGER_FORMAT = ".*-\\d+.*";
     private static final String CHECK_DEFAULT_DELIMITER_FORMAT = "[0-9,|:\\.]+";
     private static final int CUSTOM_DELIMITER_IDX = 2;
-    private static final int CUSTOM_DELIMITER_EXPRESSION_START_IDX = 5;
 
     public static void validateInput(String input) {
 
@@ -21,21 +20,14 @@ public class InputValidator {
         // 커스텀 구분자 사용하는 경우
         if (input.matches(CUSTOM_DELIMITER_FORMAT)) {
             String delimiter = String.valueOf(input.charAt(CUSTOM_DELIMITER_IDX));
-            String expression = input.substring(CUSTOM_DELIMITER_EXPRESSION_START_IDX);
 
             if(delimiter.equals("."))
-                throw new CalculatorException(CalculatorExceptionStatus.INVALID_CUSTOM_DELIMITER_DOT);
+                throw new CalculatorException(
+                        CalculatorExceptionStatus.INVALID_CUSTOM_DELIMITER_DOT);
 
             if (Character.isDigit(delimiter.charAt(0))) {
                 throw new CalculatorException(
                         CalculatorExceptionStatus.INVALID_CUSTOM_DELIMITER_INTEGER);
-            }
-
-            for (char c : expression.toCharArray()) {
-                if (!Character.isDigit(c) && !String.valueOf(c).equals(delimiter) && !String.valueOf(c).equals(".")) {
-                    throw new CalculatorException(
-                            CalculatorExceptionStatus.INVALID_CUSTOM_DELIMITER_EXPRESSION);
-                }
             }
             return;
         }
@@ -45,7 +37,5 @@ public class InputValidator {
             throw new CalculatorException(
                     CalculatorExceptionStatus.INVALID_DEFAULT_DELIMITER_EXPRESSION);
         }
-
-
     }
 }
