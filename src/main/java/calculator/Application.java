01806170d014faf96d451d0db17f2b9397c91c1d
Koeunsaem <koensm12@gmail.com>
2024-10-21 22:58:43 +0900

    feat: 사용자 입력 받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import java.util.NoSuchElementException;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input;
+        input = Console.readLine();
+        int result = add(input); // 숫자 더하는 함수
+        System.out.println("결과 : " + result);
 }
