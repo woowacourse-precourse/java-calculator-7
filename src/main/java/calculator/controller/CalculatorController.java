ea586f034067d7d4226b987fb1a325d899ce6759
이희수 <hslee9234@naver.com>
2024-10-21 22:09:23 +0900

    refactor: CalculatorController
    입출력의 역할을 InputView와 OutputView가 분담하게 분리함

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -3,23 +3,29 @@ package calculator.controller;
 import calculator.exception.CalculatorException;
 import calculator.service.CalculatorService;
 import calculator.service.ParsingService;
-import camp.nextstep.edu.missionutils.Console;
+import calculator.view.InputView;
+import calculator.view.OutputView;
 import java.util.List;
 
 public class CalculatorController {
 
     private final ParsingService parser;
     private final CalculatorService calculator;
+    private final InputView inputView;
+
+    private final OutputView outputView;
 
     public CalculatorController() {
         this.parser = new ParsingService();
         this.calculator = new CalculatorService();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
     }
 
     public void run() {
         try {
-            System.out.println("덧셈할 문자열을 입력해 주세요.");
-            String input = Console.readLine();
+            // 문자열 입력
+            String input = inputView.input();
 
             // 문자열 파싱
             List<String> numbers = parser.parse(input);
@@ -28,7 +34,8 @@ public class CalculatorController {
             String result = calculator.add(numbers);
 
             // 결과 출력
-            System.out.println("결과 : " + result);
+            outputView.printResult(result);
+
         } catch (CalculatorException e) {
             // 예외 발생 시 메시지 출력
             System.out.println(e.getMessage());
