28f85405c907fa8f1a0f5609e0c59239161bd178
srcho01 <seorim5306@gmail.com>
2024-10-21 18:33:40 +0900

    refactor: 정규식을 제작하고 사용하는 부분을 별도의 RegexUtil 클래스로 분리

--- src/main/java/calculator/NumberParser.java
+++ src/main/java/calculator/NumberParser.java
@@ -7,14 +7,14 @@ import java.util.Set;
 public class NumberParser {
 
     public List<Long> parse(String string, Set<Character> delimiters) {
-        String regex = delimitersToRegex(delimiters);
-        String[] splitted = string.split(regex);
+        String[] splitted = RegexUtil.splitByDelimiters(string, delimiters);
 
         // 숫자 개수가 10억보다 크면 에러
         if (splitted.length > 1e9) {
             throw new IllegalArgumentException("숫자는 10억개 이하여야 합니다");
         }
 
+        // 숫자가 10억보다 작은지 검사하고 long으로 바꾸어 저장
         List<Long> numbers = new ArrayList<Long>();
         for (String stringNum : splitted) {
             if (stringNum.isEmpty()) { continue; }
@@ -27,19 +27,6 @@ public class NumberParser {
         }
 
         return numbers;
-    }
-
-    private String delimitersToRegex(Set<Character> delimiters) {
-        StringBuilder regex = new StringBuilder("[");
-        for (Character delimiter : delimiters) {
-            if (!Character.isLetterOrDigit(delimiter)) {
-                regex.append("\\");
-            }
-            regex.append(delimiter);
-        }
-
-        regex.append("]");
-        return regex.toString();
     }
 
 }
--- src/main/java/calculator/StringValidator.java
+++ src/main/java/calculator/StringValidator.java
@@ -5,24 +5,9 @@ import java.util.Set;
 public class StringValidator {
 
     public void validate(String string, Set<Character> delimiters) {
-        String regex = regexPattern(delimiters);
-
-        if (!string.isEmpty() && !string.matches(regex)) {
+        if (!RegexUtil.isDigitsAndDelimitersOnly(string, delimiters)) {
             throw new IllegalArgumentException("구분자가 아닌 문자가 있습니다");
         }
-    }
-
-    private String regexPattern(Set<Character> delimiters) {
-        StringBuilder regex = new StringBuilder("[0-9");
-        for (Character delimiter : delimiters) {
-            if (!Character.isLetterOrDigit(delimiter)) {
-                regex.append("\\");
-            }
-            regex.append(delimiter);
-        }
-
-        regex.append("]+$");
-        return regex.toString();
     }
 
 }
