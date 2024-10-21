0595c29320f3255ac168b9f160da66746481a63b
jeoongu <ms235280@naver.com>
2024-10-21 21:01:23 +0900

    feat(Application) : Application 프로그램 구현 및 실행 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        CalculatorService calculatorService = new CalculatorService();
+        CalculatorController controller = new CalculatorController(inputView, outputView, calculatorService);
+
+        controller.run();
     }
 }
