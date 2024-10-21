8a1d549cdf0442f5a9763ca594a8d050d30fc41e
sanghee0820 <lsh277604@gmail.com>
2024-10-21 21:22:21 +0900

    feat: 커스텀 구분자가 있는지 확인하는 로직 추가

--- src/main/java/calculator/util/ParsingUtil.java
+++ src/main/java/calculator/util/ParsingUtil.java
@@ -3,11 +3,21 @@ package calculator.util;
 import calculator.exception.CalculatorError;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class ParsingUtil {
     private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER = "//\\s*(.)\\s*\\n";
 
     private ParsingUtil() {
+    }
+
+    public static boolean containsCustomDelimiter(String input) {
+        Pattern pattern = Pattern.compile(CUSTOM_DELIMITER);
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.find();
     }
 
     public static List<String> parseWithDefault(String input) {
