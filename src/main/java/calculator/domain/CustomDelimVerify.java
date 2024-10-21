4fb925c5bb2be4c672065844e1a44706d5c2a0bf
sytTest <syt1128@naver.com>
2024-10-21 20:08:50 +0900

    :sparkle: (CustomDelimVerify) add coveredBracket()
    커스텀 구분자의 시작과 끝이 올바른 형식인지를 반환하는 coveredBracket() 추가

--- src/main/java/calculator/domain/CustomDelimVerify.java
+++ src/main/java/calculator/domain/CustomDelimVerify.java
@@ -1,4 +1,19 @@
 package calculator.domain;
 
+import calculator.domain.constant.CustomDelimBracket;
+
 public class CustomDelimVerify {
+
+    public static Boolean coveredBracket(String candidate) {
+        return correctOpenBracket(candidate)
+                && CorrectCloseBracket(candidate);
+    }
+
+    private static boolean correctOpenBracket(String candidate) {
+        return candidate.startsWith(CustomDelimBracket.OPEN.get());
+    }
+
+    private static boolean CorrectCloseBracket(String candidate) {
+        return candidate.endsWith(CustomDelimBracket.CLOSE.get());
+    }
 }
