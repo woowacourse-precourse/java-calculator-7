a0741eaac807a4c66a106584fd6b38d3d7979a56
Yook Halim <yookhalim@Yookui-MacBookPro.local>
2024-10-21 16:19:27 +0900

    feat:문자열입력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        //IllegalArgumentException 예외처리를 위한 try~catch 구문
+        try{
+            String str = Console.readLine();
+
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println("애플리케이션 종료");
+        }
     }
 }
