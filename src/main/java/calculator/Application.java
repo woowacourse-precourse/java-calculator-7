48d3d4c604361bfc4acb00e3f9f6a1ef525882fb
fortune-man <wnguddl96@knou.ac.kr>
2024-10-21 23:10:12 +0900

    docs(README) : 고객 요구에 근거한 결과 100% 제공

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,13 +3,15 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String readLine = Console.readLine();
+
         // 문자열을 입력하면 정수 연산 결과를 출력
         Calculator calculator = new Calculator(readLine);
         int calculating = calculator.calculating();
         Frontend frontend = new Frontend(calculating);
-        frontend.printResult();
+        frontend.printCalculationResult();
     }
 }
