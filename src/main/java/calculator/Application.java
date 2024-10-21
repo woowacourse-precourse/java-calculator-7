d3e19cc15c7c17808b5cf700f22a5a388b2e4623
Cho seyun <joseyun94@naver.com>
2024-10-21 20:36:39 +0900

    feat: main 메서드로 문자열 계산기 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import calculator.operator.AdditionOperator;
+import calculator.operator.Operator;
+import calculator.parser.DelimiterConfig;
+import calculator.parser.StringDelimiterParser;
+import calculator.parser.Parser;
+import calculator.util.InputReceiver;
+import calculator.util.ResultPrinter;
+
+import java.math.BigDecimal;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = InputReceiver.readInput();
+
+        Parser parser = new StringDelimiterParser(DelimiterConfig.DEFAULT);
+        Operator operator = new AdditionOperator();
+        StringAdditionCalculator stringAdditionCalculator = new StringAdditionCalculator(operator, parser);
+
+        BigDecimal result = stringAdditionCalculator.calculate(input);
+        ResultPrinter.printResult(result);
     }
 }
