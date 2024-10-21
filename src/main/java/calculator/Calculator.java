86afd0d61fa2af62eb31462302ba4a98dbfb8d7d
leeseongsil <leegwichan@naver.com>
2024-10-21 22:39:11 +0900

    refactor(Calculator): 필요없는 stream 문 제거

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,16 +8,11 @@ public class Calculator {
     }
 
     public static int sum(List<Integer> numbers) {
-        validate(numbers);
+        numbers.forEach(Calculator::validate);
 
         return numbers.stream()
                 .reduce(Integer::sum)
                 .orElseThrow(() -> new RuntimeException("숫자가 존재하지 않습니다."));
-    }
-
-    private static void validate(List<Integer> numbers) {
-        numbers.stream()
-                .forEach(Calculator::validate);
     }
 
     private static void validate(int number) {
