93133fa3b64578c7642a3d4e6c2128044039e0f4
spiky-haired-Pyramid-head <qns0147@gmail.com>
2024-10-21 16:24:56 +0900

    feat: Application 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.Manager.CalculatorManager;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorManager manager = CalculatorManager.getInstance();
+        manager.run();
     }
 }
