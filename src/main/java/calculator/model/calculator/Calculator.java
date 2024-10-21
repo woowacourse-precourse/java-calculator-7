f9fc612a8542ced43310bb1818dcc33ec9589d17
jyoooooung <jyoooooung@naver.com>
2024-10-21 23:29:10 +0900

    feat : 구분자 예외 처리 추가

--- src/main/java/calculator/model/calculator/Calculator.java
+++ src/main/java/calculator/model/calculator/Calculator.java
@@ -28,6 +28,9 @@ public class Calculator {
     private Expression extractCustomDelimiter(String userInput) {
         if (userInput.startsWith("//")) {
             int index = userInput.indexOf("\\n");
+            if (index == -1) {
+                throw new IllegalArgumentException("커스텀 구분자의 규칙을 맞춰 입력해주세요.");
+            }
             String customDelimiter = userInput.substring(2, index);
             addDelimiter(customDelimiter);
             return new Expression(userInput.substring(index + 1 + 1));
@@ -53,7 +56,13 @@ public class Calculator {
         String[] expressionNumber = input.split(delimiterPattern.toString());
         List<Number> numbers = new ArrayList<>();
 
-        for (String e : expressionNumber) numbers.add(new Number(Long.parseLong(e)));
+        for (String n : expressionNumber) {
+            try {
+                numbers.add(new Number(Long.parseLong(n)));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("잘못된 구분자를 입력하였습니다.");
+            }
+        }
         return numbers;
     }
 }
