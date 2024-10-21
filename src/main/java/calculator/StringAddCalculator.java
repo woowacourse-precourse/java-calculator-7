c1e4c0626126478e618d1360ae55d4e8e6aa6ded
YujinJuhn <holsya@naver.com>
2024-10-21 10:00:48 +0900

    feat: Implement split and sum functionality for default delimiters

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,12 +1,19 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
 
     public static int splitAndSum(String input){
         if(input == null || input.isEmpty()){
             return 0;
         }
-        return 0;
+
+        String[] tokens = input.split(",|:");
+        return sumTokens(tokens);
     }
 
+    private static int sumTokens(String[] tokens){
+        return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
+    }
 }
