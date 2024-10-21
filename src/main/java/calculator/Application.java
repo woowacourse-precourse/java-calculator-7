bd525205ba05a05b6323655b630caf737beb3650
greensnapback0229 <smdmim@gmail.com>
2024-10-21 17:50:28 +0900

    feat: add calculate sum of formula

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        CalculationUtil calculationUtil = new CalculationUtil(input);
+        calculationUtil.validInput();
+
+        List<Integer> numbers = calculationUtil.getNumbers();
+
+        int sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
+        }
+
+        System.out.println("결과: " + sum);
     }
 }
