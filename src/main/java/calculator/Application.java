6c5773d82ea891803057fb40aa36c90cad2cf954
yoonjin <skyj217@naver.com>
2024-10-21 21:50:24 +0900

    feat: 입출력, 예외 상황 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+       try{ // 입력
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        // 출력
+        System.out.println("결과 : " + input);
+    }
+        catch (IllegalArgumentException e) {
+            // 예외 발생 시 프로그램 종료
+            System.err.println(e);
+        }
     }
 }
