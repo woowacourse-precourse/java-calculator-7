3412d1b2d117f14001cc385ce07ee8331690a716
EgPwl <egpwl07@gmail.com>
2024-10-21 21:14:37 +0900

    fix: 결과 출력 형식에 띄어쓰기  추가

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,7 +2,7 @@ package calculator.view;
 
 public class OutputView {
 
-    private static final String RESULT_MESSAGE = "결과: ";
+    private static final String RESULT_MESSAGE = "결과 : ";
 
     public static void printResult (int result) {
         System.out.println(RESULT_MESSAGE + result);
