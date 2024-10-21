68dfd89df96dcb201b813c8ecca26c4a6090cf64
gobchang7 <anchovyzzang@gmail.com>
2024-10-21 21:18:21 +0900

    feat: 구분자 설정 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,1 +1,23 @@
+package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator {
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String delimiter = ",|:";
+        String numbers = input;
+
+        Matcher m = Pattern.compile("//(.)\\\\n(.*)").matcher(input);
+        if (m.find()) {
+            delimiter = m.group(1);
+            numbers = m.group(2);
+        }
+
+
+    }
+}
