ec6582a0eafc647e2e0520575bc5e527eda56ec8
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-21 16:56:39 +0900

    [test]<testCustomDelimiterIntegerInput_ThrowsException> 정수를 커스텀 구분자로 사용하는 경우의 테스트

--- src/test/java/calculator/utilsTest/InputValidatorTest.java
+++ src/test/java/calculator/utilsTest/InputValidatorTest.java
@@ -13,7 +13,17 @@ public class InputValidatorTest {
         // 음수가 포함된 입력에 대한 테스트 (예외 발생)
         String input = "1,-2,3";
 
-        assertThrows(CalculatorException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.validateInput(input);
+        }, "INVALID_NEGATIVE_INTEGER 예외가 발생해야 합니다.");
+    }
+
+    @Test
+    public void testCustomDelimiterIntegerInput_ThrowsException() {
+        // 커스텀 구분자에 정수가 들어간 테스트 (예외 발생)
+        String input = "//0\\n10203";
+
+        assertThrows(IllegalArgumentException.class, () -> {
             InputValidator.validateInput(input);
         }, "INVALID_NEGATIVE_INTEGER 예외가 발생해야 합니다.");
     }
@@ -33,7 +43,7 @@ public class InputValidatorTest {
         // 커스텀 구분자 안에 음수 숫자가 있는 경우 (예외 발생)
         String input = "//;\n1;-2;3";
 
-        assertThrows(CalculatorException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             InputValidator.validateInput(input);
         }, "INVALID_CUSTOM_DELIMITER_INTEGER 예외가 발생해야 합니다.");
     }
@@ -43,7 +53,7 @@ public class InputValidatorTest {
         // 커스텀 구분자 외에 다른 구분자가 사용된 경우 (예외 발생)
         String input = "//;\\n1;2,3"; // 쉼표(,)는 잘못된 구분자
 
-        assertThrows(CalculatorException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             InputValidator.validateInput(input);
         }, "INVALID_CUSTOM_DELIMITER_EXPRESSION 예외가 발생해야 합니다.");
     }
@@ -63,7 +73,7 @@ public class InputValidatorTest {
         // 기본 구분자와 수식에 잘못된 문자가 포함된 경우 (예외 발생)
         String input = "1,2;3"; // 세미콜론은 기본 구분자 형식에 맞지 않음
 
-        assertThrows(CalculatorException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             InputValidator.validateInput(input);
         }, "INVALID_DEFAULT_DELIMITER_EXPRESSION 예외가 발생해야 합니다.");
     }
