5ec4cf2056c7da6de67588ed0f577e69cecf0237
Seung Ik Lee <natwo98@gmail.com>
2024-10-21 15:49:53 +0900

    refactor: remove print input
    Closes #1

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,8 +8,7 @@ public class Application {
         Scanner scan = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = scan.nextLine();
-        System.out.println(input);
-        
+
         scan.close();
     }
 }
