a3c2f93b3a71388da59df37ce25136a9eaf8c2a0
Seung Ik Lee <natwo98@gmail.com>
2024-10-21 18:19:54 +0900

    #3 refactor: modify string user's input method

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,12 @@
 package calculator;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Scanner scan = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = scan.nextLine();
+        String input = Console.readLine();
 
         int result = 0;
 
@@ -22,7 +21,6 @@ public class Application {
         }
 
         System.out.println("결과 : " + result);
-
-        scan.close();
+        
     }
 }
