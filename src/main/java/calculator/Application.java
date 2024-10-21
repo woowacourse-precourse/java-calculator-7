4c6bad59b34890f1beead22605227bda51fceef9
[CLOUD4] 한선규 <sunkyu821@gmail.com>
2024-10-21 18:23:17 +0900

    feat(Application): 결과 입력 처리 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,5 +7,15 @@ public class Application {
         // 입력 받기
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        try {
+            // 계산기 로직 실행
+            int result = StringCalculator.splitAndSum(input);
+
+            // 결과 출력
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("에러 발생: " + e.getMessage());
+        }
     }
 }
