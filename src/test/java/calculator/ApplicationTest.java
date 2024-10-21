dbbaab7ab49ed1138f544e7d6dcffc045a8bde9d
kkmdevel <kkm.devel@gmail.com>
2024-10-21 16:36:11 +0900

    test: ApplicationTest 테스트 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,12 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,10 +20,111 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    @DisplayName("커스텀 구분자 사용")
+    void testCustomDelimiterWithMultipleNumbers() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    @DisplayName("long형으로 받을 수 없는 큰 숫자 사용")
+    void testCustomDelimiterWithHugeNumbers() {
+        assertSimpleTest(() -> {
+            run("//;\\n100000000000000000000;200000000000000000000");
+            assertThat(output()).contains("결과 : 300000000000000000000");
+        });
+    }
+
+    @Test
+    @DisplayName("빈 문자열 사용")
+    void testEmpty() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 문자열만 사용")
+    void testCustomDelimiterWithoutNumbers() {
+        assertSimpleTest(() -> {
+            run("//;\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    @DisplayName("특수 문자 \"를 커스텀 구분자로 사용")
+    void testCustomDelimiterWithSpecialCharacters1() {
+        assertSimpleTest(() -> {
+            run("//\"\\n1\"2\"3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    @DisplayName("특수 문자 \\를 커스텀 구분자로 사용")
+    void testCustomDelimiterWithSpecialCharacters2() {
+        assertSimpleTest(() -> {
+            run("//\\\\n1\\2\\3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+
+    @Test
+    @DisplayName("커스텀 구분자 문자열 형식 오류")
+    void testExceptionWithInvalidCustomDelimiter() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;n1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("빈 커스텀 구분자 문자열 오류")
+    void testExceptionWithEmptyCustomSeparator() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("두개 이상의 커스텀 구분자 사용")
+    void testExceptionWithMultipleCustomSeparator() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//@;~\\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자로 숫자 사용")
+    void testExceptionWithNumberCustomSeparator() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//1\\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("구분자 뒤에 숫자가 오지않음")
+    void testExceptionWithMissingNumber() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
