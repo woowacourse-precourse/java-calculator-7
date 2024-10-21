356e3089a3d69c368d389dffd07e15f19d1349dc
parksaegyeol <dmsgkrkt1@naver.com>
2024-10-21 16:53:47 +0900

    구분자 변수에 커스텀 구분자 포함하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,25 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        String delimiters = ",|:";
+        delimiters = updateDelimiters(input, delimiters);
+
 
     }
+
+
+    public static String updateDelimiters(String input, String delimiters) {
+
+        String updateDelimiters = delimiters;
+
+        if (input.startsWith("//")) {
+            int start_index = input.indexOf('/');
+            if (start_index + 3 == input.indexOf('\\') && start_index + 4 == input.indexOf('n')) {
+                delimiters += "|" + input.charAt(start_index + 2);
+                input = input.substring(start_index + 5);
+            }
+        }
+        return updateDelimiters;
+    }
+
 }
