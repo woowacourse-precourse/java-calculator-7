6e7b9ed37ffd81a7b6c8fd9105ca61202a8e2831
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-10-21 22:09:52 +0900

    feat: 기본 구분자 덧셈 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,14 +3,24 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
+    // 문자열 구분자
+    StringBuilder delimiter = new StringBuilder(",|:");
+    int sum = 0;
+
     // 실행 함수
     public void run() {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
 
+        // 구분자로 문자열 분리, 저장
+        String[] splitNumbers = str.split(delimiter.toString());
 
+        // 하나씩 합산
+        for (int i = 0; i < splitNumbers.length; i++) {
+            sum += Integer.parseInt(splitNumbers[i]);
+        }
         // 출력
-        System.out.println("결과: " + str);
+        System.out.println("결과: " + sum);
     }
 }
