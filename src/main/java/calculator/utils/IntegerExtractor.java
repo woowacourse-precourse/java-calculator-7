ce010b24ce56e53ad34242ecd3657dc2c81385f1
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-21 18:07:33 +0900

    [chore]<extractIntegerFromExpression> regex 특수문자가 아닌 경우 Pattern 메서드 이용하지 않음

--- src/main/java/calculator/utils/IntegerExtractor.java
+++ src/main/java/calculator/utils/IntegerExtractor.java
@@ -6,8 +6,16 @@ import java.util.regex.Pattern;
 public class IntegerExtractor {
 
     public static void extractIntegerFromExpression(CalculatorValues calculatorValues) {
-        String[] tokens = calculatorValues.getExpression()
-                .split(Pattern.quote(calculatorValues.getDelimiter()));
+        String[] tokens;
+
+        if(calculatorValues.getDelimiter().equals("[,:]")){
+            tokens = calculatorValues.getExpression()
+                    .split(calculatorValues.getDelimiter());
+        }
+        else{
+            tokens = calculatorValues.getExpression()
+                    .split(Pattern.quote(calculatorValues.getDelimiter()));
+        }
         for(String token : tokens){
             calculatorValues.addNumbersFromInputByDelimiter(token);
         }
