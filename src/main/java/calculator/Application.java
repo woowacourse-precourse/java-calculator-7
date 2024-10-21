2a36fcf595e3b354c8ae795fa8c42bac5744ef30
Nestory <xofks777@naver.com>
2024-10-21 20:40:17 +0900

    feature(Application): main 메서드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IllegalArgumentException {
+        String inputString = Display.displayInput();
+        Display.displayInputResult(inputString);
+
+        SumCalculator sumCalculator = new SumCalculator();
+        int result = sumCalculator.sum(inputString);
+        Display.displayOutputResult(result);
     }
 }
