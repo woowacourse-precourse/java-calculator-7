0d9e39fe7db6c67549c6ab1283a27255fd03440e
young219257 <young219257@gmail.com>
2024-10-21 22:09:35 +0900

    feat : 구분자 추출 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,6 +7,18 @@ public class Application {
 
     }
 
+    private static String extractDelimiter(String[] parts) {
+        try {
+            if (isDelimiterFormatCorrect(parts)) {
+                throw new IllegalArgumentException("올바르지 않은 커스텀 구분자 형식입니다.");
+            }
+            return parts[0].substring(2); // "//" 이후의 구분자 추출
+        } catch (IllegalArgumentException e) {
+            throw e; // 예외를 상위로 던짐
+        }
+    }
+
+
     private static boolean isDelimiterFormatCorrect(String[] parts) {
         return parts.length < 2 || parts[0].length() < 3; // 유효하지 않은 경우 true 반환
     }
