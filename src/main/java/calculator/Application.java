69374d51d668d6d4faed9ea4a7ff96e834326ebf
wouldu <jhw664089@gmail.com>
2024-10-21 21:36:31 +0900

    feat: 커스텀 구분자 처리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class Application {
 
@@ -13,11 +14,28 @@ public class Application {
             return;
         }
 
-        String[] numbers = input.split("[,:]");
+        // 입력 문자열에서 "\\n"을 "\n"으로 변환
+        input = input.replace("\\n", "\n");
+
+        String splitter = "[,:]";
+
+        if (input.startsWith("//")) {
+            int index = input.indexOf("\n");
+            splitter = Pattern.quote(input.substring(2, index));
+            input = input.substring(index + 1);
+        }
+
+        String[] numbers = input.split(splitter);
         int sum = 0;
-        for (String number : numbers) {
-            int num = Integer.parseInt(number.trim());
-            sum += num;
+
+        try {
+            for (String number : numbers) {
+                int num = Integer.parseInt(number.trim());
+                sum += num;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("유효하지 않은 입력입니다. 숫자만 포함해야 합니다.");
+            return;
         }
 
         System.out.println("결과 : " + sum);
