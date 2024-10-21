805af7f263108309412c61ed7316902fddd0882b
김혜주 <sueelly@naver.com>
2024-10-21 23:28:08 +0900

    feat(Application): 입출력 클래스 호출 및 프로그램 실행하는 메인 메서드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import calculator.controller.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        String input = inputView.getInput();
+
+        StringCalculator calculator = new StringCalculator();
+        calculator.run(input);
+
+        OutputView outputView = new OutputView();
+        outputView.printResult(calculator.getResult());
     }
 }
