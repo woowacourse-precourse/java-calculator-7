8b039d276b42c4d3aefe62b49251086722e020cb
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-10-21 21:45:40 +0900

    FEAT(InputParser): 입력 문자열의 커스텀 구분자 추출 기능 구현

---
+++ src/main/java/calculator/InputParser.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputParser {
+
+    private List<Character> separators;
+
+    public InputParser() {
+        this.separators = new ArrayList<>(List.of(new Character[]{',', ':'}));
+    }
+
+    public List<Character> getSeparators() {
+        return separators;
+    }
+
+    public String findCustomSeparator(String input) {
+        int customSeparatorStart = input.indexOf("//");
+        int customSeparatorEnd = input.indexOf("\\n");
+        System.out.println("customSeparatorEnd = " + customSeparatorEnd);
+
+        //커스텀 구분자가 존재하는 경우
+        if (customSeparatorStart == 0 && customSeparatorEnd > 0) {
+            for (int i = customSeparatorStart + 2; i < customSeparatorEnd; i++) {
+                separators.add(input.charAt(i));
+            }
+        }
+        //커스텀 구분자가 존재하지 않는 경우
+        else if (customSeparatorStart == -1 && customSeparatorEnd == -1) {
+            return input;
+        }
+        //커스텀 구분자 입력이 올바르지 않은 경우
+        else {
+            throw new IllegalArgumentException("커스텀 구분자 입력이 잘못되었습니다.");
+        }
+
+        //커스텀 구분자를 제거한 출력값 계산을 위한 문자열 반환
+        return input.substring(customSeparatorEnd + 2);
+    }
+}
