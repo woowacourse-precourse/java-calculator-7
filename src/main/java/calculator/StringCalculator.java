7aeb4ceaf33d46939cfded80fff6bb6d54125f03
BusanGukbap <dong2857210@gmail.com>
2024-10-21 17:21:16 +0900

    feat(splitInput) : 구분자 목록에 커스텀 구분자 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,16 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
     private String[] splitInput(String input) {
         String delimiter = ",|:"; // 기본 구분자
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\n");
+            delimiter = Pattern.quote(input.substring(2, delimiterIndex));
+            input = input.substring(delimiterIndex + 1);
+        }
 
         return input.split(delimiter);
     }
