ee31bf386a82c1456b41c1ff43ab1bac9b3febc3
halfmoon-mind <5338095@gmail.com>
2024-10-21 23:11:02 +0900

    feat: 계산하는 부분 구현

--- src/main/java/calculator/Calculater.java
+++ src/main/java/calculator/Calculater.java
@@ -1,2 +1,34 @@
-package calculator;public class Calculater {
+package calculator;
+
+import java.util.List;
+
+public class Calculater {
+    static int calculateInput(String input, List<Character> divisionCharacter) {
+        int result = 0;
+        int currentDigit = 0;
+        for (int i=0; i<input.length(); i++) {
+            char c = input.charAt(i);
+            boolean isDigit = Character.isDigit(c);
+            if(isDigit) {
+                currentDigit *= 10;
+                currentDigit += convertCharacterToInt(c);
+            } else {
+                if(divisionCharacter.contains(c)) {
+                    result += currentDigit;
+                    currentDigit = 0;
+                } else {
+                    throw new IllegalArgumentException("invalid character, can't be parsed");
+                }
+            }
+        }
+
+        if(currentDigit != 0) {
+            result += currentDigit;
+        }
+        return result;
+    }
+
+    static int convertCharacterToInt(char input) {
+        return input - '0';
+    }
 }
