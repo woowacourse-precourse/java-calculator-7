9f7e842a805c3c467317ac593946054d4e2efe14
ratatuwee <pkqwaszx@gmail.com>
2024-10-21 14:44:40 +0900

    test(application): 전체 동작 테스트 및 예외 테스트 추가2
    입력값만을 가지고 전체 동작이 정상적으로 이뤄지는지 확인하는 테스트들을 추가하였습니다
    숫자, 소수, 구분자 상황에 따라 테스트를 나누어 추가하였습니다

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -22,7 +22,6 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains("결과 : 3");
         });
     }
-
     @Test
     void 커스텀_구분자_미사용() {
         assertSimpleTest(() -> {
@@ -51,6 +50,13 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains("결과 : 6.3");
         });
     }
+    @Test
+    void 커스텀_구분자_숫자사용_계산할숫자가없을경우() {
+        assertSimpleTest(() -> {
+            run("//7\\n7777");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
 
     @Test
     void 예외_테스트() {
@@ -70,6 +76,41 @@ class ApplicationTest extends NsTest {
     void 예외_테스트_소수사용_소수누락시() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("2.,0.1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_커스텀구분자문자사용_숫자누락시() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\;;;;;;"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_커스텀구분자미사용_숫자누락시() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",,,::,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_커스텀구분자미사용_문자누락시() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("123123"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_숫자0만입력시() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0,1,2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_소수0만입력시() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0.0,1,2"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
