3d1ee3437e5b92e9a240c5661fbbb17a766e1907
Park-Min-Jun <gomdole3963@gmail.com>
2024-10-21 23:34:00 +0900

    feat: 커스텀 구분자 찾는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,10 +7,24 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input;
+        String sep = "";
+        String str = "";
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         input = sc.nextLine();
+        String[] input_split = input.split("");
+        String[] str_num = input.split(",|:");
 
+        if (input_split[0].equals("/")) {
+            for (int i = 2; i < input_split.length; i++) {
+                if (input_split[0].equals("/") && input_split[1].equals("/")) {
+                    if (input_split[i].equals("\\") && input_split[i + 1].equals("n")) {
+                        break;
+                    }
+                    sep += input_split[i];
+                }
+            }
+        }
     }
 }
