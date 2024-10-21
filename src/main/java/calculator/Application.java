f8ec7b9b605eac3498a965bc0496540ba51fccd1
TwoJeong <jjeongee117@gmail.com>
2024-10-21 23:24:03 +0900

    feat:예외처리 및 커스텀 문자열 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,29 +1,51 @@
 package calculator;
+import java.util.regex.Pattern;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
+    public static int add(String input) {
+        input = input.replace("\\n","\n");
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        String defaultlimiter = ",|:";
+        String arrnumbers = input;
+
+        if(input.startsWith("//")){
+            int EndlimieteIndex = input.indexOf("\n");
+            if(EndlimieteIndex ==-1){
+                throw new IllegalArgumentException("커스텀 구분자 지정 이후 줄바꿈이 필요");
+            }
+            defaultlimiter = Pattern.quote(input.substring(2,EndlimieteIndex));
+            arrnumbers = input.substring(EndlimieteIndex+1);
+        }
+        String[] splitarrs = arrnumbers.split(defaultlimiter);
+        int sumresult=0;
+
+        for(String splitarr : splitarrs){
+            try{
+                splitarr = splitarr.trim();
+                if(!splitarr.isEmpty()){
+                    int num = Integer.parseInt(splitarr);
+                    sumresult+=num;
+                }
+            }catch(NumberFormatException e){
+                throw new IllegalArgumentException("유효하지 않은 숫자입니다");
+            }
+        } return sumresult;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         //입력받은 String 배열을 쉼표 또는 세미콜론 기준으로 자르고 누적합을 계산
-        System.out.println("덧셈할 문자열을 입력해주세요.");
-        String inputstr = readLine();
-        int sumresult =0;
+        try{
+            System.out.println("덧셈할 문자열을 입력해주세요.");
+            String inputstr = readLine();
 
-        //아무것도 입력하지 않으면 덧셈의 결과가 0
-        if(inputstr.trim().isEmpty()){
-            System.out.println("결과: "+ 0);
-        } else{
-            args = inputstr.split("[,:]");
-            for(String arg:args){
-                try{
-                    if(!arg.trim().isEmpty()){ //입력받은 문자열이 공백요소가 아니라면
-                        sumresult +=Integer.parseInt(arg.trim());
-                    }
-                }catch (NumberFormatException e){
-                    System.out.println("잘못된 입력값입니다" + inputstr); //숫자,구분 연산자 외의 기호가 들어갔을때
-                }
-        }
-            System.out.println("결과 : " + sumresult);
+            int result = add(inputstr);
+            System.out.println("결과:"+ result);
+        } catch (IllegalArgumentException e){
+            System.err.println(e.getMessage());
         }
     }
 }
