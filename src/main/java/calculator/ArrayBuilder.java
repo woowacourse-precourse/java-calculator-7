9075fcb247842af6fe0369ce982ac2b04c40f7f6
ssuukko <tlstpstpdl11@gmail.com>
2024-10-21 19:39:29 +0900

    Refactor: 하드코딩된 substring 시작 인덱스를 동적으로 처리하도록 수정하였습니다. - substring 인덱스를 고정된 5에서 delimiterEndIndex를 기반으로 유연하게 변경

---
+++ src/main/java/calculator/ArrayBuilder.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class ArrayBuilder {
+
+    public String getStrNumbers(String numbers, int delimiterEndIndex) {
+        return numbers.substring(delimiterEndIndex + 2);
+    }
+}
