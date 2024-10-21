2e1458dd76dbea34eb136cc2cb9d829a312ce3f8
masee1994 <masee1994@gmail.com>
2024-10-21 19:29:45 +0900

    feat(application): 기본 문자열 구분 덧셈 기능 구현
    입력받은 문자열을 쉼표(,) 또는 콜론(:)으로 구분한 후 각 숫자를 합산하여 반환하는 기능을 추가했습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+        String num[] = input.split("[,:]");
+        int sum = 0;
+        for (String number : num) {
+            sum += Integer.parseInt(number);
+        }
+        System.out.println("결과 : "+sum);
     }
 }
