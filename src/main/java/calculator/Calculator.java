5d47857c1f37290d2ea558c700c8911969b9bf43
고성태 <v12rhtjdxo@naver.com>
2024-10-21 18:45:46 +0900

    feat : 새로운 구분자 찾고 저장하는 기능

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,7 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+//리스트를 저장하기 위한 클래스
+class ListNode {
+    //시작 인덱스 저장 리스트
+    List<Integer> startDTIndexList;
+    //시작 인덱스 저장 리스트
+    List<Integer> endDTIndexList;
 
+    public ListNode(List<Integer> startDTIndexList, List<Integer> endDTIndexList) {
+        this.startDTIndexList = startDTIndexList;
+        this.endDTIndexList = endDTIndexList;
+    }
+}
 
 public class Calculator {
     //입력받은 데이터
@@ -16,5 +27,26 @@ public class Calculator {
     public Calculator(String inputData) {
         this.inputData = inputData;
         this.dTList = new ArrayList<>(Arrays.asList(",", ":"));
+    }
+
+    private ListNode findCustomDTIdx(String inputData) {
+        //시작 인덱스를 저장할 리스트
+        List<Integer> startDTIdxList = new ArrayList<>();
+        //끝 인덱스를 저장할 리스트
+        List<Integer> endDTIdxList = new ArrayList<>();
+
+        //조건을 통해 알맞은 리스트에 값 저장
+        for (int i = 0; i < inputData.length() - 1; i++) {
+            if (inputData.charAt(i) == '/' && inputData.charAt(i + 1) == '/') {
+                startDTIdxList.add(i + 2);
+                i++;
+            } else if (inputData.charAt(i) == '\\' && inputData.charAt(i + 1) == 'n') {
+                endDTIdxList.add(i - 1);
+                i++;
+            }
+        }
+
+        //ListNode 형식으로 return
+        return new ListNode(startDTIdxList, endDTIdxList);
     }
 }
