0fc5ee3784d13bd4f2e25511b8199500bde0105e
mikekks <thdalsrb79@naver.com>
2024-10-21 10:32:16 +0900

    feat(Output): 모든 출력에 관한 기능 구현
    1. 입력 전, 메세지 출력
    2. 결과값을 출력

---
+++ src/main/java/calculator/Output.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class Output {
+
+	private static final String RESULT_MESSAGE = "결과 : ";
+	private static final String INPUT_PROMPT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+	public static void printResult(int result){
+		System.out.println(RESULT_MESSAGE + result);
+	}
+
+	public static void printInputPrompt(){
+		System.out.println(INPUT_PROMPT_MESSAGE);
+	}
+}
