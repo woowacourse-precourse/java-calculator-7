18c0cb09e93140e71c182d2f4281edcb686d1f57
gimogibumo <wolfbone0304@naver.com>
2024-10-21 19:45:07 +0900

    feat: 빈 문자열 또는 null 입력 시 0을 반환하는 기능 추가

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,20 @@
+package calculator;
+
+/**
+ * StringCalculator 클래스는 주어진 문자열 입력에서 숫자를 더하는 기능을 수행합니다.
+ */
+public class StringCalculator {
+    /**
+     * 입력된 문자열에서 숫자를 더하는 메서드입니다.
+     * 입력이 null이거나 빈 문자열일 경우 0을 반환합니다.
+     *
+     * @param input 쉼표(,) 또는 콜론(:)으로 구분된 숫자 문자열
+     * @return 숫자의 합 또는 입력이 비어 있을 경우 0
+     */
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+}
