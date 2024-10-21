663b267e99af9663867d2aaa966c8c723c0e31aa
Seung Ik Lee <natwo98@gmail.com>
2024-10-21 16:06:19 +0900

    feat: implement add number in user's input separated by default separator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,6 +9,20 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = scan.nextLine();
 
+        int result = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch == ',' || ch == ':') {
+                continue;
+            } else if (Character.isDigit(ch)) {
+                result += Character.getNumericValue(ch);
+            }
+
+        }
+
+        System.out.println(result);
+
         scan.close();
     }
 }
