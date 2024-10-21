51be10b5e10ab9338a93be16dc988843020f01e6
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-10-21 22:13:20 +0900

    feat: 커스텀 구분자 설정 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -7,6 +7,29 @@ public class Calculator {
     StringBuilder delimiter = new StringBuilder(",|:");
     int sum = 0;
 
+    // 커스텀 구분자 설정 메소드
+    public String customDelimiter(String str) {
+        //커스텀 구분자로 시작하는경우
+        if (str.startsWith("//")) {
+
+            // 구분자 설정 인덱스 찾기
+            int endIndex = str.indexOf("\\n");
+            if (endIndex == -1) {
+                throw new IllegalArgumentException("구분자 입력이 잘못되었습니다.");
+            }
+
+            // 커스텀 구분자를 구분자 리스트에 추가
+            String customDelimiter = str.substring(2, endIndex);
+            for (char c : customDelimiter.toCharArray()) {
+                delimiter.append("|").append(c);
+            }
+
+            // 구분자 설정문 이후의 문자열처리
+            str = str.substring(endIndex + 2);
+        }
+        return str;
+    }
+
     // 실행 함수
     public void run() {
 
@@ -19,6 +42,8 @@ public class Calculator {
         }
 
         try {
+            // 커스텀 구분자 있는지 확인 후 있으면 구분자에 추가
+            str = customDelimiter(str);
 
             // 구분자로 문자열 분리, 저장
             String[] splitNumbers = str.split(delimiter.toString());
