92c623e43af8a74c03119ad570b96490cd559416
kckc0608 <kckc0608@naver.com>
2024-10-21 17:03:26 +0900

    fix: 입력 문자열 개행 문자 제거

--- src/main/java/calculator/stringCalculator/calculatorInterface/CalculatorInterface.java
+++ src/main/java/calculator/stringCalculator/calculatorInterface/CalculatorInterface.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class CalculatorInterface {
 
-    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.\n";
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
     private static final String RESULT_MESSAGE = "결과 : ";
 
     public void printResult(long result) {
