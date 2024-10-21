043f58bebdf5ad611a4bf353fb1e57ed20329a2b
chosunghyun18 <krismon@naver.com>
2024-10-21 09:32:08 +0900

    feat : 사용자 입력 및 메모리 확인

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,11 @@
 package calculator;
-import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
-        String userInput = Console.readLine();
-        Calculator calculator = new Calculator();
         try{
-            if(calculator.checkInput(userInput)) {
-                Integer calculateOutput = calculator.getSum(userInput);
+            Calculator calculator = new Calculator();
+            calculator.setUserInputByCustomConsole();
+            if(calculator.checkInputByRule()) {
+                Integer calculateOutput = calculator.getSumByCalculate();
                 System.out.println(calculateOutput);
             }
         }catch (Exception e) {
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,36 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Calculator {
-    public Boolean checkInput(String userInput) {
+    private String userInput;
+
+    public Calculator() {
+
+    }
+
+    public Boolean checkInputByRule() {
+        return checkInputByRule(this.userInput);
+    }
+    public Boolean checkInputByRule(String userInput) {
         return false;
     }
 
-    public Integer getSum(String userInput) {
+    public Integer getSumByCalculate() {
+        return this.getSumByCalculate(this.userInput);
+    }
+    public Integer getSumByCalculate(String userInput) {
         return 0;
     }
+
+    public String setUserInputByCustomConsole() {
+        try{
+            this.userInput = Console.readLine();
+            return this.userInput;
+        } catch (OutOfMemoryError e) {
+            throw new IllegalArgumentException(e);
+        }
+
+    }
+
 }
