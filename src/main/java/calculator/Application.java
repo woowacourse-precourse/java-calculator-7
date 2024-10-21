aaafea196171947cc6a2e2ba01a499caa5954ed6
MINSEO PARK <miinseo0324@gmail.com>
2024-10-21 20:31:15 +0900

    fix: print 출력 메세지 수정
    :이라고 작성했는데, 문제에서는 .로 끝났다.
    문제의 출력 형식을 맞추기 위해 수정함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -54,7 +54,7 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요 : ");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         try {
             int result = add(input);
