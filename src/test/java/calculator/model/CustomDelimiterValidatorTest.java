bb8110187b4410591f64e55bab619f682e0137af
Chan531 <chan657312@gmail.com>
2024-10-21 18:08:46 +0900

    test(CustomDelimiterValidatorTest): 커스텀 구분자가 포함된 문자열 테스트

--- src/test/java/calculator/model/CustomDelimiterValidatorTest.java
+++ src/test/java/calculator/model/CustomDelimiterValidatorTest.java
@@ -28,4 +28,24 @@ class CustomDelimiterValidatorTest {
 		assertThat(stringValidator.getInput()).isEqualTo(inputExpectedResult);
 		assertThat(stringValidator.getDelimiters()).isEqualTo(delimitersExpectedResult);
 	}
+
+	@Test
+	@DisplayName("커스텀 구분자가 포함된 문자열이 입력되었을 때 커스텀 구분자를 포함하고 커스텀 구분자 지정 문자열이 제거된 StringValidator를 반환한다.")
+	void 커스텀_구분자가_포함된_문자열이_입력되었을_때_이를_반영한_문자열_유효성_검사_객체를_반환한다() {
+		// given
+		CustomDelimiterValidator customDelimiterValidator = new CustomDelimiterValidator();
+		String customDelimiter = "#";
+		String input = "//#\\n1,2:3#4";
+		String inputExpectedResult = "1,2:3#4";
+		List<Delimiter> delimitersExpectedResult = Stream.of(",", ":", customDelimiter)
+			    .map(Delimiter::new)
+			    .toList();
+
+		// when
+		StringValidator stringValidator = customDelimiterValidator.validCustomDelimiter(input);
+
+		// then
+		assertThat(stringValidator.getInput()).isEqualTo(inputExpectedResult);
+		assertThat(stringValidator.getDelimiters()).isEqualTo(delimitersExpectedResult);
+	}
 }
