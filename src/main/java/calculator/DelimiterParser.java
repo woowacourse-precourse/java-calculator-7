c028822c6bd4ad5980a2ef675cb9170c985f7cf2
문혜교 <3hilney@gmail.com>
2024-10-21 12:37:17 +0900

    feat: 커스텀 구분자 외 나머지 문자열 반환

--- src/main/java/calculator/DelimiterParser.java
+++ src/main/java/calculator/DelimiterParser.java
@@ -12,4 +12,14 @@ public class DelimiterParser {
         return null;
     }
 
+    // 나머지 문자열 반환
+    public String getRamainString(String input) {
+        if (input.startsWith("//") && input.contains("\n")) {
+            int remainStart = input.indexOf("\n") + 1;
+            return input.substring(remainStart);
+        }
+
+        return input;
+    }
+
 }
