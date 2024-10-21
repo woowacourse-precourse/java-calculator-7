627b9993ce96fdd2c3305c27ed7f3ad09adb5d88
fortune-man <wnguddl96@knou.ac.kr>
2024-10-21 21:12:12 +0900

    refactor(FrontendTest) : NS Test 클래스에 대해 감각을 익혀가면서 출력 테스트 코드 개선

--- src/test/java/calculator/FrontendTest.java
+++ src/test/java/calculator/FrontendTest.java
@@ -2,6 +2,8 @@ package calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,42 +11,63 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class FrontendTest {
+  private PrintStream standardOut;
+  private OutputStream captor;
+
+
+
+  @BeforeEach
+  protected final void init() {
+    this.standardOut = System.out;
+    this.captor = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(this.captor));
+  }
+
+
+
+  @AfterEach
+  protected final void printOutput() {
+    System.setOut(this.standardOut);
+    System.out.println(this.output());
+  }
+  private String output() { return this.captor.toString().trim(); }
 
 
 
   //  java.lang.NullPointerException at FrontendTest.java:28
   // java.lang.NullPointerException: Cannot invoke "calculator.Frontend.accessTarget()" because "this.frontend" is null
-  @DisplayName("계산된 결과에 접근해서 값을 가져온다")
+//  @Test
+//  public void accessResult() throws Exception{
+//      //given
+//      int expect = 15;
+//      String readLine = "1, 2, 3, 4, 5";
+//
+//      //when
+//      Calculator calculator = new Calculator(readLine);
+//      int target = calculator.calculating();
+//
+//      Frontend frontend = new Frontend(target);
+//      int actual = frontend.getTarget();
+//
+//      //then
+//      assertEquals(expect, actual);
+//  }
+
+
   @Test
-  public void accessResult() throws Exception{
+  public void 계산_결과를_전달받으면_정상_출력되는지_확인한다() throws Exception{
       //given
       int expect = 15;
       String readLine = "1, 2, 3, 4, 5";
+      Calculator calculator = new Calculator(readLine);
 
-      //when
-    Calculator calculator = new Calculator(readLine);
-    int target = calculator.calculating();
+      // when
+      int target = calculator.calculating();
+      Frontend frontend = new Frontend(target);
 
-    Frontend frontend = new Frontend(target);
-    int actual = frontend.getTarget();
+      // then
+      frontend.printResult();
 
-    //then
-      assertEquals(expect, actual);
   }
-
-//  @DisplayName("조회된 계산결과를 출력한다")
-//  @Test
-//  public void testPrintResult() throws Exception{
-//      //given
-//      int given = 10;
-//
-//      //when
-//      String expect = "결과 : 10";
-//      frontend.printResult();
-//
-//      //then
-//      assertEquals();
-//
-//  }
 
 }
