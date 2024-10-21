80d29b623a00efef2a541e9a3cfb3fe266012836
hyvnsevng <yh189487@gmail.com>
2024-10-21 23:03:15 +0900

    fix(Calculator): Calculator 클래스의 변수, 생성자, 실행 메서드 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,28 @@
 package calculator;
 
 public class Calculator {
-
-    private Input input;
+    private final Input input;
+    private final InputValidation validator;
+    private final CustomDelimiter delimiter;
+    private final Adder adder;
+    private final Output output;
 
     public Calculator() {
+        this.adder = new Adder();
         this.input = new Input();
+        this.validator = new InputValidation();
+        this.delimiter = new CustomDelimiter();
+        this.output = new Output();
     }
 
     public void run() {
         String inputString = input.input();
+        String[] numbers = delimiter.extractNumbers(inputString);
+        int sum = adder.calculateSum(numbers);
+        output.displayResult(sum);
+    }
+
+    private void validateInput(String input) {
+        validator.validate(input);
     }
 }
