e866d53fb861753a781b83d2f7e2b67bf4b71b58
sonshn <sonshn@naver.com>
2024-10-21 19:28:34 +0900

    feat(OutputView): 결과 출력 구현

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,14 @@
+package calculator.view;
+
+public class OutputView {
+
+    /**
+     * 결과 출력
+     *
+     * @return 연산 결과
+     */
+    public void getOutput(int result) {
+        System.out.println("결과 : " + result);
+    }
+
+}
