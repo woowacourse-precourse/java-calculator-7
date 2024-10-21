eea795b156cc892cbcd8ad9957e60d3193c18af7
Leegeonwoo7 <workgeon07@naver.com>
2024-10-21 17:53:25 +0900

    chore(addNumber()): 중복되는 메서드 제거

--- src/main/java/calculator/model/Numbers.java
+++ src/main/java/calculator/model/Numbers.java
@@ -11,7 +11,6 @@ public class Numbers {
         return numbers;
     }
 
-    //TODO 사용하지 않을 경우 삭제 현재 NumbersTest에서만 사용중
     public void addNumber(Integer... numbers) {
         for (Integer number : numbers) {
             if (number == null || number < 0) {
@@ -19,13 +18,6 @@ public class Numbers {
             }
             this.numbers.add(number);
         }
-    }
-
-    public void addNumber(Integer number) {
-        if (number == null || number < 0) {
-            throw new IllegalArgumentException("숫자가 0이거나 NULL이어서는 안됩니다.");
-        }
-        numbers.add(number);
     }
 
     public Integer sumNumbers() {
