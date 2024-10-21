7639c2ba5abe2d5d3bb5b9975c3cabb6f4f1aea5
imjaehyeong <dlawogud517@gmail.com>
2024-10-21 15:50:19 +0900

    style: 주석 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,7 +3,6 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
