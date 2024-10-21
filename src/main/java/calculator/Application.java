7ce95ffca56aacedb815509c5eb894372c2fe974
himinqq <alsrud0966@naver.com>
2024-10-21 12:32:09 +0900

    refractor: 매개변수 타입 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,14 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
 
-        //입력
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
         StringCalculator calculator = new StringCalculator();
         int resultSum = calculator.calculate(input);
 
-        //출력
         System.out.println("결과 : " + resultSum);
         Console.close();
 
--- src/main/java/calculator/InputValidator.java
+++ src/main/java/calculator/InputValidator.java
@@ -2,21 +2,23 @@ package calculator;
 
 public class InputValidator {
 
-    //사용자 입력 검증 : 구분자
-    public void isDelimiter(char[] charArray) {
-        for (int i = 1; i < charArray.length; i = i + 2) {
-            char delimiter = charArray[i];
-            if (delimiter != '|' || delimiter != ',') {
+    public void validate(String input) {
+        validateNumber(input);
+        validateDelimiter(input);
+    }
+
+    public void validateDelimiter(String input) {
+        for (int i = 1; i < input.length(); i += 2) {
+            char delimiter = input.charAt(i);
+            if (delimiter != ':' && delimiter != ',') {
                 throw new IllegalArgumentException("구분자 입력이 올바르지 않습니다.");
             }
         }
     }
 
-
-    //사용자 입력 검증 : 양수
-    public void isPositiveNumber(char[] charArray) {
-        for (int i = 0; i < charArray.length; i = i + 2) {
-            char number = charArray[i];
+    public void validateNumber(String input) {
+        for (int i = 0; i < input.length(); i += 2) {
+            char number = input.charAt(i);
             if (!Character.isDigit(number)) {
                 throw new IllegalArgumentException("숫자를 입력하세요.");
             }
