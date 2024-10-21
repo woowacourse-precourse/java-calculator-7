4ea844d7a3e9c3e9e4d71e23ef59d669234c579c
SeuneKeun <eiziong1@gmail.com>
2024-10-21 16:57:00 +0900

    refactor: 입력 출력 메시지 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println(Message.getInputMessage());
         String inputString = Console.readLine();
+
         StringAddCalculator stringAddCalculator = new StringAddCalculator();
 
-        System.out.println(stringAddCalculator.calculate(inputString));
+        System.out.println(Message.getOutputMessage() + stringAddCalculator.calculate(inputString));
     }
 }
