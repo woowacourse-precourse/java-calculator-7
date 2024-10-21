481f15cf45f87802b9f04afa154840c16f9d0045
김희망 <gmlakd990514@naver.com>
2024-10-21 21:22:37 +0900

    feat: 파서 및 계산기 구현

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # java-calculator-precourse
+## 구현 기능 목록
+
+1. 문자열 파서 
+2. 커스텀 구분자 등록
--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        //기본 구분자 등록
+        Parser.setSeparator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println("결과 : " +Parser.parseAndCalculate(scanner.nextLine()));
+
     }
+
+
 }
