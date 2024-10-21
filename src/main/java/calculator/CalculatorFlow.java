3811d17571b916e3ec46176a786db0e474e9fb20
maedachaeva <parkchaeyeon.b@gmail.com>
2024-10-21 11:39:23 +0900

    refactor: 입력 안내 메시지 출력 로직 이동
    CalculatorFlow에서 UserInput로 입력 안내 메시지 출력 로직 이동

--- src/main/java/calculator/CalculatorFlow.java
+++ src/main/java/calculator/CalculatorFlow.java
@@ -2,6 +2,6 @@ package calculator;
 
 public class CalculatorFlow {
     public void start() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        UserInput.showInputPrompt();
     }
 }
---
+++ src/main/java/calculator/UserInput.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class UserInput {
+    private static final String INPUT_PROMPT = "덧셈할 문자열을 입력해 주세요.";
+
+    public static void showInputPrompt() {
+        System.out.println(INPUT_PROMPT);
+    }
+}
