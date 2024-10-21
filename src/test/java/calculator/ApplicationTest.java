8900f3858714a55bafeeeced37fd92c1900f1950
huhdy32 <mong3268@gmail.com>
2024-10-21 14:22:14 +0900

    test(all): 통합 예외처리 테스트 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,7 +1,11 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,6 +25,28 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("사용자입력_예외케이스")
+    void 커스텀_예외(final String userInput) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(userInput))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    static Stream<Arguments> 사용자입력_예외케이스() {
+        return Stream.of(
+                Arguments.of("//\\n"),
+                Arguments.of("//\\\n"),
+                Arguments.of("//1\\n\\n"),
+                Arguments.of("//12"),
+                Arguments.of("\\n12"),
+                Arguments.of("////\n"),
+                Arguments.of("//12\\n1,2,3"),
+                Arguments.of("//\\n1,2,3:4")
         );
     }
 
