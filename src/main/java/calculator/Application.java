90a9f3624f672642fc9f585a8001a59c9325586b
bk <naturalbb360@gmail.com>
2024-10-21 14:05:56 +0900

    refactor(Application): 메소드 및 변수명을 더 명확하게 수정 - 'main' 메소드를 최상단으로 이동 - 코드 전체의 'seperator'를 'separator'로 오타 수정 - 'getCustomSeperator'를 'getNumberTextFromInputDividedBySeparators'로 변경 - 'getNumber'를 'getNumberTexts'로 변경 - 'addNum'을 'getSumOfNumberTexts'로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,31 +7,48 @@ import java.util.List;
 
 public class Application {
 
-    public static String getCustomSeperator(String input, ArrayList<String> seperatorList) {
-        String customSeperator;
+    public static void main(String[] args) {
+        ArrayList<String> separatorList = new ArrayList<>(List.of(".", ":"));
+        String userInput, cleanInput;
+        ArrayList<String> userNumList = new ArrayList<String>();
+
+        System.out.println("덧셈할 문자열을 입력해주세요");
+        userInput = getUserInput();
+        cleanInput = getNumberTextFromInputDividedBySeparators(userInput, separatorList);
+        getNumberTexts(cleanInput, separatorList, userNumList);
+        String answer = getSumOfNumberTexts(userNumList);
+        System.out.println("결과 : " + answer);
+
+    }
+
+    public static String getNumberTextFromInputDividedBySeparators(String input, ArrayList<String> separatorList) {
+        checkCustomSeparatorCount(input, "//");
+        checkCustomSeparatorCount(input, "\\n");
+
+        String customSeparator;
         if (input.contains("\\n") && input.contains("//")) {
-            customSeperator = input.substring(input.indexOf("//") + 2, input.indexOf("\\n"));
-            if (customSeperator.length() > 1) {
+            customSeparator = input.substring(input.indexOf("//") + 2, input.indexOf("\\n"));
+            if (customSeparator.length() > 1) {
                 throw new IllegalArgumentException("한개의 커스텀 구분자만 추가할 수 있습니다.");
             }
             if (input.indexOf("//") != 0) {
                 throw new IllegalArgumentException("커스텀 구분자는 문자열 처음에서만 추가할 수 있습니다.");
             }
-            seperatorList.add(customSeperator);
+            separatorList.add(customSeparator);
             return input.substring((input.indexOf("\\n") + 2));
         }
         return input;
     }
 
-    public static void checkCustomSeparatorCount(String input, String customSeperatorKeyword) {
+    public static void checkCustomSeparatorCount(String input, String customSeparatorKeyword) {
         int index = 0;
         int keyworkdCount = 0;
-        while ((index = input.indexOf(customSeperatorKeyword, index)) != -1) {
+        while ((index = input.indexOf(customSeparatorKeyword, index)) != -1) {
             keyworkdCount++;
             if (keyworkdCount > 1) {
                 throw new IllegalArgumentException("//와 \\n는 한 번만 사용될 수 있습니다.");
             }
-            index += customSeperatorKeyword.length();
+            index += customSeparatorKeyword.length();
         }
     }
 
@@ -45,11 +62,11 @@ public class Application {
         return input.matches("\\d");
     }
 
-    public static void getNumber(String cleanInput, ArrayList<String> seperatorList, ArrayList<String> userNumList) {
+    public static void getNumberTexts(String input, ArrayList<String> separatorList, ArrayList<String> userNumList) {
         StringBuilder numberBuffer = new StringBuilder();
-        for (int i = 0; i < cleanInput.length(); i++) {
-            String curChar = cleanInput.substring(i, i + 1);
-            if (seperatorList.contains(curChar)) {
+        for (int i = 0; i < input.length(); i++) {
+            String curChar = input.substring(i, i + 1);
+            if (separatorList.contains(curChar)) {
                 if (!numberBuffer.isEmpty()) {
                     userNumList.add(numberBuffer.toString());
                     numberBuffer = new StringBuilder();
@@ -70,25 +87,11 @@ public class Application {
         return new BigInteger(numberString);
     }
 
-    public static void addNum(ArrayList<String> userNumList) {
+    public static String getSumOfNumberTexts(ArrayList<String> userNumList) {
         BigInteger answer = BigInteger.ZERO;
-        for (String s : userNumList) {
-            answer = answer.add(convertStringToBigInt(s));
+        for (String numberText : userNumList) {
+            answer = answer.add(convertStringToBigInt(numberText));
         }
-        System.out.println("결과 : " + answer);
-    }
-
-    public static void main(String[] args) {
-        ArrayList<String> seperatorList = new ArrayList<>(List.of(".", ":"));
-        String userInput, cleanInput;
-        ArrayList<String> userNumList = new ArrayList<String>();
-
-        System.out.println("덧셈할 문자열을 입력해주세요");
-        userInput = getUserInput();
-        checkCustomSeparatorCount(userInput, "//");
-        checkCustomSeparatorCount(userInput, "\\n");
-        cleanInput = getCustomSeperator(userInput, seperatorList);
-        getNumber(cleanInput, seperatorList, userNumList);
-        addNum(userNumList);
+        return answer.toString();
     }
 }
