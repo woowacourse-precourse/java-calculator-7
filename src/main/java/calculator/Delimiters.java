ccd4ebaab24cfa9155f4409d6f07255f9d9fec2c
Park, Sojeong <sojeong2101@gmail.com>
2024-10-21 20:16:53 +0900

    refactor($Delimiters): 매직 넘버 상수화

--- src/main/java/calculator/Delimiters.java
+++ src/main/java/calculator/Delimiters.java
@@ -7,31 +7,45 @@ import java.util.regex.Pattern;
 
 public class Delimiters {
 
+    private static final String BASIC_DELIMITERS_COMMA = ",";
+    private static final String BASIC_DELIMITERS_COLON = ":";
+    private static final String CUSTOM_DELIMITERS_PREFIX = "//";
+    private static final String CUSTOM_DELIMITERS_SUFFIX = "\\\\n";
+    private static final String VALID_STRING_REGEX_TEMPLATE = "[^0-9%s]+";
+    private static final String CUSTOM_SEPARATOR_REGEX = "//(.*?)\\\\n";
+
     private List<String> separators;
 
     public Delimiters(String rawString) {
-        this.separators = new ArrayList<>();
-        separators.add(",");
-        separators.add(":");
-        separators.add("//");
-        separators.add("\\\\n");
+        this.separators = initializeDefaultSeparators();
         findCustomSeparator(rawString);
         checkNotDelimiter(rawString);
     }
 
-    private void checkNotDelimiter(String rawString) {
-        String regex = "[^0-9" + String.join("", separators) + "]+";
-        Matcher matcher = Pattern.compile(regex).matcher(rawString);
+    private List<String> initializeDefaultSeparators() {
+        List<String> defaultSeparators = new ArrayList<>();
+        defaultSeparators.add(BASIC_DELIMITERS_COMMA);
+        defaultSeparators.add(BASIC_DELIMITERS_COLON);
+        defaultSeparators.add(CUSTOM_DELIMITERS_PREFIX);
+        defaultSeparators.add(CUSTOM_DELIMITERS_SUFFIX);
 
-        while (matcher.find()) {
-            throw new IllegalArgumentException("구분자 외 문자는 입력할 수 없습니다.");
-        }
+        return defaultSeparators;
     }
 
     private void findCustomSeparator(String rawString) {
-        Matcher matcher = Pattern.compile("//(.*?)\\\\n").matcher(rawString);
+        Matcher matcher = Pattern.compile(CUSTOM_SEPARATOR_REGEX).matcher(rawString);
+
         while (matcher.find()) {
             separators.add(matcher.group(1));
+        }
+    }
+
+    private void checkNotDelimiter(String rawString) {
+        String validCharactersRegex = String.format(VALID_STRING_REGEX_TEMPLATE, String.join("", separators));
+        Matcher matcher = Pattern.compile(validCharactersRegex).matcher(rawString);
+
+        while (matcher.find()) {
+            throw new IllegalArgumentException("구분자 외 문자는 입력할 수 없습니다.");
         }
     }
 }
