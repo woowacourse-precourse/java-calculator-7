d40f93e2d0e1a7f977294e16da918ea0294028d2
gracepec <gracepec0621@gmail.com>
2024-10-21 18:59:18 +0900

    feat(Calculator): 구분자를 기준이으로 분리한 각 숫자의 합을 반환 기능 추가
    - Calculator 클래스 추가
    - 구분자 사이 숫자가 아닌 입력에 대한 예외 처리 구현

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -2,14 +2,17 @@ package calculator;
 
 import calculator.service.InputHandler;
 import calculator.service.SeparatorHandler;
+import calculator.service.Calculator;
 
 public class Controller {
     private final InputHandler inputHandler;
     private final SeparatorHandler separatorHandler;
+    private final Calculator calculator;
 
     public Controller() {
         this.inputHandler = new InputHandler();
         this.separatorHandler = new SeparatorHandler();
+        this.calculator = new Calculator();
     }
 
     public void run(){
@@ -18,5 +21,7 @@ public class Controller {
         input = separatorHandler.checkCustomSeparator(input);
 
         inputHandler.validateInput(input);
+
+        int result = calculator.calculate(input);
     }
 }
---
+++ src/main/java/calculator/service/Calculator.java
@@ -0,0 +1,27 @@
+package calculator.service;
+
+import com.sun.source.tree.BreakTree;
+
+public class Calculator {
+    public int calculate(String input) {
+        int sum = 0;
+
+        if (input.contains(",")) {
+            String[] nums_str = input.split(",");
+
+            for (String s : nums_str) {
+                try {
+                    sum = sum + Integer.parseInt(s);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("구분자 사이 입력이 숫자가 아닙니다.");
+                }
+            }
+        }
+
+        if (!input.contains(",")) {
+            sum = Integer.parseInt(input);
+        }
+
+        return sum;
+    }
+}
