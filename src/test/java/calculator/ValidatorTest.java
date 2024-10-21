1e231fbb4b51f2ee4ecbdd2c90ee3fda40432bf0
dbdb456 <adad568@naver.com>
2024-10-21 09:30:54 +0900

    test(ValidatorTest): 테스트 클래스 초기화

--- src/test/java/calculator/ValidatorTest.java
+++ src/test/java/calculator/ValidatorTest.java
@@ -1,4 +1,16 @@
 package calculator;
 
+
+import calculator.validators.Validators;
+import org.junit.jupiter.api.BeforeEach;
+
+
 public class ValidatorTest {
+
+    private Validators validators;
+
+    @BeforeEach
+    void setUp() {
+        validators = new Validators();
+    }
 }
