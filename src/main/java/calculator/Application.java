9d72fdef85774af51e9fad89225506c4e4c09302
son <hh001204@naver.com>
2024-10-21 21:15:51 +0900

    feat: 연산 기능 추가 - 구분자와 숫자를 분리 - 숫자들의 합을 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,7 +9,19 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputText = Console.readLine();
 
+        // 2. 연산 과정
+        String separator = ",|:";
+        String[] tokens = inputText.split(separator);
+
         int sum = 0;
+
+        for (String token : tokens) {
+            int number = Integer.parseInt(token);
+            if (number < 0) {
+                System.out.println("양수만 입력해 주세요.");
+            }
+            sum += number;
+        }
 
         // 3. 출력
         System.out.println("결과 : " + sum);
