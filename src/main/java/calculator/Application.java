e67402bb57e427d90ffe333813dbec745f11fa67
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2024-10-21 22:38:30 +0900

    feat(Application.java): Application 클래스 작성
    사용자의 입력값을 받고, 합 계산기를 실행합니다.
    
    readLine, sumCalculator 메소드 호출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.stream.Stream;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        SumCalculator calculator = new SumCalculator();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String userInput = Console.readLine();
+        calculator.startCalculator(userInput);
+
+
+
+
     }
 }
