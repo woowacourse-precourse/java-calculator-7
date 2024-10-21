d2214ef9a145fc143a416248c31d6c976a131f1f
KimJongMin <whdals011203@naver.com>
2024-10-21 17:40:16 +0900

    test(ApplicationTest.java): 테스트 케이스 추가
    테스트 케이스 추가
    
    커스텀_구분자_온점() : 커스텀 구분자로 온점이 입력 될 경우, 소수는 모두 정수로 인식
    정수만_입력() : 커스텀 구분자 없이 정수만 입력됐을 경우
    소수_정수_섞어서_입력() : 소수, 정수 섞어서 입력됐을 경우
    소수_입력() : 소수만 입력됐을 경우

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,10 +17,42 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_온점() {
+        assertSimpleTest(() -> {
+            run("//.\\n1.3.1:2");
+            assertThat(output()).contains("결과 : 7");
+        });
+    }
+
+    @Test
+    void 정수만_입력() {
+        assertSimpleTest(() -> {
+            run("1,2,3:4");
+            assertThat(output()).contains("결과 : 10.0");
+        });
+    }
+
+    @Test
+    void 소수_정수_섞어서_입력() {
+        assertSimpleTest(() -> {
+            run("1.3:2,3.1:5.7");
+            assertThat(output()).contains("결과 : 12.1");
+        });
+    }
+
+    @Test
+    void 소수_입력() {
+        assertSimpleTest(() -> {
+            run("1.2,1.3,1.7:8");
+            assertThat(output()).contains("결과 : 12.2");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
