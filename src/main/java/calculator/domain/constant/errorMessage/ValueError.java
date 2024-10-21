871a9937a0eef05dcf64c85f1454b84a6815e30f
rootTiket <l20078829@gmail.com>
2024-10-21 19:05:02 +0900

    test: 구분자 여러개일 경우 예외처리

--- src/main/java/calculator/domain/constant/errorMessage/ValueError.java
+++ src/main/java/calculator/domain/constant/errorMessage/ValueError.java
@@ -6,7 +6,8 @@ public enum ValueError implements Message {
     MINUS_VALUE("음수가 올 수 없습니다."),
     INVALID_VALUE_FORMAT("올바르지 않은 형식입니다."),
     OUT_OF_RANGE_VALUE("Int 범위 외로는 입력할 수 없습니다."),
-    OUT_OF_RANGE_RESULT("결과가 범위를 초과합니다.");
+    OUT_OF_RANGE_RESULT("결과가 범위를 초과합니다."),
+    MULTIPLE_CHARACTERS("구분자는 하나만 지정할 수 있습니다.");
 
 
     private final String message;
