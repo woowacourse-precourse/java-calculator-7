90b1604766c76640be052b4607e5978e50f41d65
davemins <davemins4125@naver.com>
2024-10-21 10:16:36 +0900

    feat(Controller): 추출한 숫자들의 합계를 계산하는 함수 호출

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,5 +1,6 @@
 package calculator.controller;
 
+import calculator.domain.Calculator;
 import calculator.domain.DelimiterExtractor;
 import calculator.domain.NumberParser;
 import calculator.view.View;
@@ -7,11 +8,13 @@ import calculator.view.View;
 public class CalculatorController {
     private final DelimiterExtractor delimiterExtractor;
     private final NumberParser numberParser;
+    private final Calculator calculator;
 
 
     public CalculatorController() {
         this.delimiterExtractor = new DelimiterExtractor();
         this.numberParser = new NumberParser();
+        this.calculator = new Calculator();
     }
 
     public void run() {
@@ -19,5 +22,8 @@ public class CalculatorController {
 
         String delimiters = delimiterExtractor.extract(input);
         String[] numbers = numberParser.parse(input, delimiters);
+
+        int result = calculator.sum(numbers);
+
     }
 }
