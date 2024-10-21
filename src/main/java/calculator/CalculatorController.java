3a4757dc6756b7b56fe7512fc400975053b5f65c
ffineapple <imffineapple@gmail.com>
2024-10-21 14:26:28 +0900

    feat(CalculatorController): 입력값에서 숫자 추출을 위한 split 함수와 parse 함수 구현

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -2,6 +2,10 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CalculatorController {
@@ -17,5 +21,37 @@ public class CalculatorController {
 
     private String removeWhiteSpace(String input) {
         return input.replaceAll("\\s", "");
+    }
+
+    private List<String> splitUserInput(String input) {
+        if (Validator.isCustomFormat(input)) {
+            return customSplit(input);
+        }
+        return defaultSplit(input);
+    }
+
+    private List<String> defaultSplit(String input) {
+        return Arrays.asList(input.split(Constants.REGEX_DEFAULT_SEPARATOR, -1));
+    }
+
+    private List<String> customSplit(String input) {
+        Matcher matcher = customPattern.matcher(input);
+        if (matcher.find()) {
+            String customSeparator = matcher.group(1);
+            String content = matcher.group(2);
+            return Arrays.asList(content.split(Pattern.quote(customSeparator), -1));
+        }
+        throw new IllegalArgumentException(Constants.ERROR_INVALID_CUSTOM_FORMAT);
+    }
+
+    private List<Integer> parseInputNumbers(List<String> inputNumbers) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : inputNumbers) {
+            if (number.equals("")) {
+                number = number.replace("", "0");
+            }
+            numbers.add(Integer.parseInt(number));
+        }
+        return numbers;
     }
 }
