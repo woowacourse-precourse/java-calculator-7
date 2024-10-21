1ad2ef519016eab2ed4751ae68b36448c5d46cbf
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-21 19:41:15 +0900

    refactor(input): 사용자에게 보일 입력 메세지 final로 선언

--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -4,13 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 // 사용자의 문자열 입력을 책임지는 클래스
 public class InputHandler {
+    private final static String INPUT_MESSAGE = "덧셈할 문자열을 입력해주세요.";
+
     public String getInput() {
-        System.out.println("덧셈할 문자열을 입력해주세요.");
-        String input = Console.readLine();
-
-        // 예외처리 추가하기
-
-        return input;
+        System.out.println(INPUT_MESSAGE);
+        return Console.readLine();
     }
 
 }
