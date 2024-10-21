02d17bf386a672fed3c8756d53a2b5061a788dea
YooSejeong <nived3@naver.com>
2024-10-21 15:10:10 +0900

    feat(Application): 1. 문자열 입력받는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 사용자에게 문자열 입력 받기
+        System.out.println("덧셈할 문자열을 입력해 주세요. ");
+        String input = Console.readLine();
+
+        try {
+            //TODO : InputValidator.validateInput(input);  // 올바른 값인지 입력값 검증
+            System.out.println("결과 : " + input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 입력입니다: " + e.getMessage());
+        }
+
+
     }
 }
+
