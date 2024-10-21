6f2add119e462f0c13708eb1a31e6f3801fe440e
na0th <105775683+na0th@users.noreply.github.com>
2024-10-21 15:56:38 +0900

    refactor : MainController로 Main 코드를 넣음
    추가한 내용 :
    MainController를 통해 Main 메서드는 run() 메서드만 실행하면 계산 로직이 controller가 처리하도록하여 Main 메서드가 깔끔해지게 함

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,20 +1,11 @@
 package calculator;
 
-import calculator.view.InputView;
-import calculator.view.OutputView;
-
-import java.util.List;
+import calculator.controller.MainController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        String inputString = InputView.printInputView();
-        List<Integer> integers = Filter.doFilter(inputString);
-        // TODO: 검증? 필터?를 거쳐서 InputString 에서 List<Integer> 형태로 변환하여 calculate 의 파라미터로 넣기
-        String output = Calculator.calculate(integers);
-        OutputView.printOutputView(output);
-
-
-
+       // TODO: 프로그램 구현
+        MainController mainController = new MainController();
+        mainController.run();
     }
 }
