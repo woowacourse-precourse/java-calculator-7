5ea19313215c356e758c1244ed033020e21f076f
zelatndnjs <zelatndnjs@naver.com>
2024-10-21 18:20:54 +0900

    new

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,7 +7,6 @@ import static camp.nextstep.edu.missionutils.Console.*;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         String input = readLine();
         try {
             int result = StringCalculator.add(input);
