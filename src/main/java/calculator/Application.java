0342f82fa3f0f0386bee2fad9745b8bc342551ce
nsb329 <nsb329@naver.com>
2024-10-21 21:13:13 +0900

    양수가 아닌 문자열의 덧셈 예외처리, 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,9 +4,14 @@ public class Application {
     public static void main(String[] args) {
        System.out.println("덧셈할 문자열을 입력하세요.");
        String input = Console.readLine();
+       try{
           calc(input);
        System.out.println("결과:"+calc(input));
        }
+       catch(IllegalArgumentException e) {
+          System.out.println(e.getMessage());
+       }
+    }
     public static int calc(String input){
           if (input == null || input.isEmpty()) {
                return 0;
@@ -15,13 +20,16 @@ public class Application {
           String custom=input.substring(2,input.indexOf("\\n"));
           input=input.substring(input.indexOf("\\n")+2);
           input=input.replaceAll(custom,",");
+       }else if(input.startsWith("/")) {
+          throw new IllegalArgumentException("제대로 된 구분자를 입력해주세요.");
        }
        input=input.replaceAll(":",",");
        String arr[]=input.split(",");
        int sum=0;
        for(int i=0; i<arr.length; i++){
-    	   if(Integer.parseInt(arr[i])<0) {
-    	   }
+          if(Integer.parseInt(arr[i])<0) {
+             throw new IllegalArgumentException("음수를 입력하였습니다. 양수를 입력해주세요.");
+          }
           sum+=Integer.parseInt(arr[i]);
        }
        return sum;
