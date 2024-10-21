7afdcb8e2663d036cb15df21541e15be6902481e
박채린 <bewithbomi@gmail.com>
2024-10-21 22:29:39 +0900

    feat(input) : 사용자 입력 기능 추가
    - 사용자에게 입력 요청하는 getInput() 메소드 구현
    - camp.nextstep.edu.missionutils.Console을 import 하여 Console.readLine() 으로 사용자 입력 읽기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = getInput();
+    }
+
+    private static String getInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
     }
 }
