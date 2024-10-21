3ba8d25bb6b9959fced7cc4e189aebe9dc97ff0c
고성태 <v12rhtjdxo@naver.com>
2024-10-21 18:51:04 +0900

    feat : test코드 + 덧셈작업 구현 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        //str값을 input 받는다.
+        String input = Console.readLine();
+        //Array or List를 활용해서 더하기 후 결과값 출력
+        System.out.println("결과 : " + new Calculator(input).convertStringToIntArrayAndSum());
     }
 }
