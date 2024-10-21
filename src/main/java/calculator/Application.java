cef3212b22158dc3a858ffd9504c37f844151fd8
AndBlack99 <jwoo1687@icloud.com>
2024-10-21 23:19:23 +0900

    feat : 잘못된 문자열에 대한 처리기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,27 +5,35 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
     	String input = Console.readLine();
-    	
         String delimiter = ",|:";
         
         if (input.startsWith("//")) {
             int customDelimiter = input.indexOf("\\n");
+            if (customDelimiter == -1) {
 
+                throw new IllegalArgumentException();
+            }
             delimiter = input.substring(2, customDelimiter);
             input = input.substring(customDelimiter + 2);
         }
 
         String[] numbers = input.split(delimiter);
-        
         int sum = 0;
-        
-        for (String number : numbers) {       
+
+        for (String number : numbers) {
+            if (number.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            try {
             int num = Integer.parseInt(number);
-            
+            if (num < 0) {
+                throw new IllegalArgumentException();
+            }
             sum += num;
-            } 
-            
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException();
+            }
         }
 
-        System.out.println("결과 : " + sum);
+        System.out.println("결과 : " + sum);}
 }
