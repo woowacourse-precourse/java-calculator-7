82bb7e7a897413b87983bd6d8f4c90da3b7cbe9e
Chan Mi <rkdcksal02@gmail.com>
2024-10-21 18:04:19 +0900

    fix(view): 입력 출력 변경

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,10 +2,10 @@ package calculator.view;
 
 public class OutputView {
     public void displayResult(int result) {
-        System.out.println("결과: " + result);
+        System.out.println("결과 : " + result);
     }
 
     public void displayErrorMessage(String message) {
-        System.out.println("오류: " + message);
+        System.out.println("오류 : " + message);
     }
 }
