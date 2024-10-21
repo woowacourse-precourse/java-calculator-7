64fa3ea598ca3dca526d1f114e8d13fccc6e2b39
yokxim <yooksi53@gmail.com>
2024-10-21 22:22:37 +0900

    [fix] 잘못된 문자열을 입력한 경우 예외 처리를 하지 않고 바로 프로그램 종료
    메시지 반환(예외처리) 로직 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,12 +10,8 @@ public class Application {
         Scanner scanner = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = scanner.nextLine();
-        try {
-            int result = calculator.solution(input);
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        int result = calculator.solution(input);
+        System.out.println("결과 : " + result);
     }
 }
 
