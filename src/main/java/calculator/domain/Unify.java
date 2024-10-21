be2037e71b7e33d0b9fd7936725421fb0066e82f
sytTest <syt1128@naver.com>
2024-10-21 20:26:29 +0900

    :sparkle: (Unify) add allDelimiters()
    입력식의 모든 구분자를 통일하는 allDelimiters() 추가

--- src/main/java/calculator/domain/Unify.java
+++ src/main/java/calculator/domain/Unify.java
@@ -12,4 +12,16 @@ public class Unify {
         expression = essential;
         delimiters = new ArrayList<>(elements);
     }
+
+    public static String allDelimiters (String essential, List<String> delimiters) {
+        setting(essential, delimiters);
+        for (String delimiter : delimiters) {
+            expression = unifyDelimiter(delimiter);
+        }
+        return expression;
+    }
+
+    private static String unifyDelimiter (String delimiter) {
+        return expression.replaceAll(delimiter,delimiters.getFirst());
+    }
 }
