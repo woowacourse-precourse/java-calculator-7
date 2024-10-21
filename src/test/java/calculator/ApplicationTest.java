27a70daf24a82607976d5453e9271298f28ff049
dohee-jin <doheeeq@gmail.com>
2024-10-21 22:45:26 +0900

    git commit -m feat: 프리코스 1주차 과제
    프리코스 1주차 과제

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -7,7 +7,9 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class ApplicationTest extends NsTest {
+import java.util.Scanner;
+
+/*class ApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -28,4 +30,27 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+}*/
+public class ApplicationTest {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("덧셈할 문자열을 입력해주세요: ");
+		String str = sc.nextLine();
+		String[] arraystr = str.split("[;,]");
+		int sum = 0;
+
+		for(int i=0;i<arraystr.length;i++){
+				int num = Integer.parseInt(arraystr[i]);
+				if(num < 0) {
+					throw new IllegalArgumentException(); 
+				}
+				else {
+
+					sum += num;
+				}
+				
+		}
+		System.out.println(sum);
+ 	}
 }
