80abc36029689a90edbf7197220630018fe7331c
sytTest <syt1128@naver.com>
2024-10-21 20:25:37 +0900

    :sparkle: (Unify) add setting()
    입력식과 구분자 리스트를 입력 후 필드에 셋팅하는 setting() 추가

--- src/main/java/calculator/domain/Unify.java
+++ src/main/java/calculator/domain/Unify.java
@@ -1,4 +1,15 @@
 package calculator.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Unify {
+
+    private static String expression;
+    private static List<String> delimiters;
+
+    public static void setting (String essential, List<String> elements) {
+        expression = essential;
+        delimiters = new ArrayList<>(elements);
+    }
 }
