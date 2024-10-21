3b76b9dfa7df74e3c0f1a4cc68337050a4ce183b
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-10-21 15:28:26 +0900

    feat : 4.커스텀 구분자로 문장 분리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -12,7 +12,22 @@ public class Calculator {
         // 3. 기본 구분자(쉼표, 콜론)로 문자열 분리
         String delimiter = ",|:";
         String input = str;
+
+        //4. 커스텀 구분자로 문장 분리
+        int customIndex = isCustom(str);
+        if (customIndex != -1) {
+            String customDelimiter = input.substring(2, customIndex);
+            delimiter += "|" + customDelimiter;
+            input = input.substring(customIndex + 2);
+        }
         String[] numbers = input.split(delimiter);
     }
 
+    // 4. 커스텀 구분자로 문장 분리
+    private int isCustom(String str) {
+        if (str.startsWith("//")) {
+            return str.indexOf("\\n");
+        }
+        return -1;
+    }
 }
