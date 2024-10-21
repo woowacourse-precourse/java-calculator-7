175e57cbdec964815fde78e04bc7ba2d3f7f87f2
Park - Jeonghwan <990706leo@gmail.com>
2024-10-21 16:22:01 +0900

    feat(StringProcessor): 커스텀 구분자를 설정하고 식별할 수 있다.

--- src/main/java/calculator/model/StringProcessor.java
+++ src/main/java/calculator/model/StringProcessor.java
@@ -6,8 +6,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StringProcessor {
+
     private static final String COLON = ":";
     private static final String COMMA = ",";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
     private List<String> delimiters;
 
     public StringProcessor() {
@@ -15,10 +18,33 @@ public class StringProcessor {
     }
 
     public List<Number> interpret(String expression) {
+        if (isCustomDelimiterExist(expression)) {
+            createCustomDelimiter(expression);
+            String removed = removeAffix(expression);
+            return Arrays.stream(removed.split(concatDelimiters()))
+                    .map(Number::of)
+                    .toList();
+        }
         delimiters = List.of(COLON, COMMA);
         return Arrays.stream(expression.split(concatDelimiters()))
                 .map(Number::of)
                 .toList();
+    }
+
+    private boolean isCustomDelimiterExist(String expression) {
+        return expression.startsWith(CUSTOM_DELIMITER_PREFIX);
+    }
+
+    private void createCustomDelimiter(String expression) {
+        int end = expression.indexOf(CUSTOM_DELIMITER_SUFFIX);
+
+        delimiters.add(expression.substring(CUSTOM_DELIMITER_PREFIX.length(), end));
+    }
+
+    private String removeAffix(String original) {
+        int end = original.indexOf(CUSTOM_DELIMITER_SUFFIX);
+
+        return original.substring(end + CUSTOM_DELIMITER_SUFFIX.length());
     }
 
     private String concatDelimiters() {
