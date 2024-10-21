1b453218e375810f86a1c3720ad5ad6fdfc20d2e
spspd <hsst104@gmail.com>
2024-10-21 14:28:55 +0900

    [feature] 1 input/output string

---
+++ woowa1/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // action
+        String input_sen = br.readLine();
+
+        // output
+        System.out.println(input_sen);
+
+    }
+}
