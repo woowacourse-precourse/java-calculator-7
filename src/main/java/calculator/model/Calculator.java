3907386df8380f3a452f67d95e752c752d8da0be
Chan Mi <rkdcksal02@gmail.com>
2024-10-21 17:27:44 +0900

    fix(model): Calculator 빈 문자열 또는 null인 경우 0 반환

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -4,6 +4,11 @@ import calculator.utils.StringParser;
 
 public class Calculator {
     public int add(String input) {
+        // 빈 문자열 또는 null인 경우 0 반환
+        if (input == null || input.trim().isEmpty()) {
+            return 0;
+        }
+
         String[] numbers = StringParser.parse(input);
         int sum = 0;
 
