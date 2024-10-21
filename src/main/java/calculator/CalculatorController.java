335b8aebc215fc45ba22972e88efde0f7a002789
ffineapple <imffineapple@gmail.com>
2024-10-21 12:35:55 +0900

    feat(CalculatorController): 입력값에서 whitespace를 제거하는 함수 구현

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -2,12 +2,20 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class CalculatorController {
+    private final Pattern customPattern = Pattern.compile(Constants.REGEX_CUSTOM_FORMAT);
+
     private void printStartText() {
         System.out.println(Constants.MESSAGE_START_INPUT);
     }
 
     private String inputData() {
         return Console.readLine();
+    }
+
+    private String removeWhiteSpace(String input) {
+        return input.replaceAll("\\s", "");
     }
 }
