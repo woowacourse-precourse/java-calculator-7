0311946a6c1d18e0fede7eeac8b13fa6cd9c5373
sseopsseop <ponde119@outlook.com>
2024-10-21 20:25:07 +0900

    fix(CalculateController) : try ~ catch 구문 제거 Test코드에서 Exception이 발생할 상황에서 발생하지 않는 문제를 해결하기 위해서 try~catch구문 제거

--- src/main/java/calculator/controller/CalculateController.java
+++ src/main/java/calculator/controller/CalculateController.java
@@ -7,13 +7,9 @@ import calculator.view.OutputView;
 
 public class CalculateController {
     public static void run(){
-        try{
-            String inputString = InputView.inputString();
-            StringValidator.stringValidate(inputString);
-            int sum = StringCalculator.calculateSum(inputString);
-            OutputView.printOutput(sum);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        String inputString = InputView.inputString();
+        StringValidator.stringValidate(inputString);
+        int sum = StringCalculator.calculateSum(inputString);
+        OutputView.printOutput(sum);
     }
 }
