76b5bd2cde577e742f681ce39e788d9430392b65
gracepec <gracepec0621@gmail.com>
2024-10-21 21:27:47 +0900

    fix(getUserInput): 사용자 입력을 받기 전 \n 제거

--- src/main/java/calculator/service/InputHandler.java
+++ src/main/java/calculator/service/InputHandler.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
     public String getUserInput() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.\n");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
 
         return Console.readLine();
     }
