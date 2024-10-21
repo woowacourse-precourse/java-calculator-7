d8f4c2157909621248f6242861c0d9623423935c
kangsan <devstar1223@gmail.com>
2024-10-21 17:58:19 +0900

    feat: 커스텀 문자열 지정 부분과 계산식 두 부분으로 나누기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,9 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         String stringValue  = InputString.input();
+        // 커스텀 문자열 지정 방법 확인
+        StringValueDivide.divide(stringValue); // 커스텀 구분자 선언 부분과 계산식 부분 으로 나눔
+        String customDelimiterAssign = StringValueDivide.getCustomDelimiterAssign(); // 커스텀 구분자 선언 부분
+        String expression = StringValueDivide.getExpression(); // 계산식 부분
     }
 }
