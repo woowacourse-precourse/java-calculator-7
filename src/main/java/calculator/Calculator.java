948013c9553f05886157bb455c1cf3f6dc846d85
inti0 <142914095+inti0@users.noreply.github.com>
2024-10-21 20:32:23 +0900

    feat : 컨트롤러 역할의 Calculator 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,2 +1,24 @@
-package calculator;public class Calculator {
+package calculator;
+
+import java.util.List;
+
+public class Calculator {
+
+    private final CalculatorView view = new CalculatorView();
+    private final StringSplitterManager splitterManager = new StringSplitterManager(
+            List.of(
+                    new DefaultStringSplitter(),
+                    new CustomStringSplitter()
+            )
+    );
+
+    public void run() {
+        String input = view.readInput();
+
+        List<String> splitStrings = splitterManager.getSplitStrings(input);
+        SumCalculator sumCalculator = new SumCalculator(splitStrings);
+        String result = sumCalculator.calculateSum();
+
+        view.printOutput(result);
+    }
 }
