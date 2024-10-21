44a313a6d9298240b612d558312961cf67cf9ae1
706com <ehdgusjdbswl@gmail.com>
2024-10-21 23:51:33 +0900

    [Fix] 콘솔 출력 수정

--- src/main/java/calculator/console/OutputConsole.java
+++ src/main/java/calculator/console/OutputConsole.java
@@ -2,6 +2,6 @@ package calculator.console;
 
 public class OutputConsole {
     public static void outputConsole(int number){
-        System.out.println("결과:  " + number);
+        System.out.println("결과 : " + number);
     }
 }
