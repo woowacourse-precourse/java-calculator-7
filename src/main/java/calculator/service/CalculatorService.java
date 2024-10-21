0154b8ba47de3b83cdc53410fb26175cc4a48885
gyuoo <yeomky98@naver.com>
2024-10-21 22:50:27 +0900

    feat: add CalculatorService class

---
+++ src/main/java/calculator/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package calculator.service;
+
+public class CalculatorService {
+
+	private final InputService inputService;
+
+	public CalculatorService(InputService inputService) {
+		this.inputService = inputService;
+	}
+
+}
