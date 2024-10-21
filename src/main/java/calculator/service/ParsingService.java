136b88878352f41500e7c0d7dc50cdcfdf4c9a9b
choiyoung69 <choiyoung609@gmail.com>
2024-10-21 11:41:31 +0900

    refactor(ParsingService): ParsingService에서 직접 ArrayList를 사용하여 delimiters를 저장하도록 하는 것이 아니라 일급 컬렉션을 사용하도록 변경

--- src/main/java/calculator/service/ParsingService.java
+++ src/main/java/calculator/service/ParsingService.java
@@ -31,18 +31,18 @@ public class ParsingService {
     }
 
     //구분자 관련해서 해결해야 할 듯;; ㅜㅜ 이거 해결해야만 돼. ㅜ
-    private static String parseCustomDelimiter(String operandStr, ArrayList<Character> delimiters){
+    private static String parseCustomDelimiter(String operandStr, Delimiters delimiters){
         //custom 구분자 추출
         if(operandStr.startsWith("//")){
-            delimiters.add(operandStr.charAt(CUSTOM_DELIMITER_INDEX));
+            delimiters.addCustomDelimiter(operandStr.charAt(CUSTOM_DELIMITER_INDEX));
             operandStr = operandStr.substring(EXCEPT_CUSTOM_DELIMITER_INDEX);
         }
         return operandStr;
     }
 
     //구분자에 따라 문자열을 분리하는 기능
-    private static void parse(String operandStr, ArrayList<Character> delimiters, ArrayList<Integer> operandList){
-        String regex = createRegexFromDelimiters(delimiters);
+    private static void parse(String operandStr, Delimiters delimiters, ArrayList<Integer> operandList){
+        String regex = delimiters.createRegexFromDelimiters();
 
         String[] tokens = operandStr.split(regex);
         for (String token : tokens) {
