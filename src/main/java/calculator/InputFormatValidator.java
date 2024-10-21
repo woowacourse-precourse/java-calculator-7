7fce952b197b9ce79c215cffd3f5fdfa0696813c
= <hyeonhc328@gmail.com>
2024-10-21 16:44:34 +0900

    refactor(validator): 입력 검증 로직을 별도 클래스로 분리
    InputFormatValidator.java 로 입력 형식 검증을 별도의 클래스로 분리해 책임을 구분함

---
+++ src/main/java/calculator/InputFormatValidator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class InputFormatValidator {
+    private static final String ERROR_NEGATIVE_NUMBER = "음수는 허용되지 않습니다.";
+    private static final String ERROR_STARTS_WITH_CHAR = "커스텀 문자 지정 외에는 문자로 시작할 수 없습니다.";
+
+    public static void validateFormat(String input) throws IllegalArgumentException {
+        if (input.startsWith("-")) {
+            throw new IllegalArgumentException(ERROR_NEGATIVE_NUMBER);
+        }
+
+        if (!Character.isDigit(input.charAt(0))) {
+            throw new IllegalArgumentException(ERROR_STARTS_WITH_CHAR);
+        }
+    }
+}
--- src/main/java/calculator/InputValidator.java
+++ src/main/java/calculator/InputValidator.java
@@ -1,9 +1,6 @@
 package calculator;
 
 public class InputValidator {
-    private static final String ERROR_NEGATIVE_NUMBER = "음수는 허용되지 않습니다.";
-    private static final String ERROR_STARTS_WITH_CHAR = "커스텀 문자 지정 외에는 문자로 시작할 수 없습니다.";
-
     private final Delimiter delimiter;
     private final Parser parser;
 
@@ -16,27 +13,11 @@ public class InputValidator {
         if (input.startsWith("//")) {
             input = delimiter.parseDelimiter(input);
         }
-
         if (input.isEmpty()) {
             return "0";
         }
-
-        validateNotStartsWithChar(input);
-        validateNotStartsWithNegativeNumber(input);
-
+        InputFormatValidator.validateFormat(input);
         return this.parser.parse(input, delimiter);
-    }
-
-    private void validateNotStartsWithNegativeNumber(String input) {
-        if (input.startsWith("-")) {
-            throw new IllegalArgumentException(ERROR_NEGATIVE_NUMBER);
-        }
-    }
-
-    private void validateNotStartsWithChar(String input) {
-        if (!Character.isDigit(input.charAt(0))) {
-            throw new IllegalArgumentException(ERROR_STARTS_WITH_CHAR);
-        }
     }
 
 }
