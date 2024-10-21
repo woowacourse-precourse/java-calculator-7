1247f8008b683fbff2d21169d95fe37790ab31b8
SECHANG1412 <binaryblitz1412@gmail.com>
2024-10-21 18:34:04 +0900

    git add src/main/java/calculator/Delimiter.java git commit -m "feat: 구분자를 처리하는 Delimiter 클래스 추가"

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,20 +8,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
     void customDelimiterTest() {
         assertSimpleTest(() -> {
             run("//;\n1;2");
-            assertThat(output()).contains("Result: 3");
+            assertThat(output()).contains("결과: 3");
         });
     }
 
+
+
+
     @Test
     void exceptionTest() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("-1,2,3"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("-1,2,3"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("음수는 입력할 수 없습니다");
+        });
     }
 
     @Override
