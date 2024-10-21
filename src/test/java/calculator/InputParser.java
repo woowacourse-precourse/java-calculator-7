0350e3b913b07b0a271dc87e2c7261fd755f6c2e
SinnoLn <sintmtn69@gmail.com>
2024-10-21 12:17:39 +0900

    feat: null 처리 기능 추가

--- src/test/java/calculator/InputParser.java
+++ src/test/java/calculator/InputParser.java
@@ -2,6 +2,10 @@ package calculator;
 
 public class InputParser {
     public int[] parse(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("입력 값이 null 일 수 없습니다.");
+        }
+
         String delimiter = "[,|:]"; // 기본 구분자
         String[] tokens = input.split(delimiter);
         int[] numbers = new int[tokens.length];
