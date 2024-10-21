1a729062168ccf587af4fa2c8e73d3f4cc6b2fde
Park - Jeonghwan <990706leo@gmail.com>
2024-10-21 15:21:15 +0900

    feat(OutputView): 결과값을 출력할 수 있다

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,6 +1,9 @@
 package calculator.view;
 
 public class OutputView {
+    public void printResult(int number) {
+        System.out.println("결과 : " + number);
+    }
 
     public void printStart() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
