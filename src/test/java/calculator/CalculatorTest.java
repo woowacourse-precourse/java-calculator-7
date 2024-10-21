5936346c5ebd329ab0b3581477173b424afd7137
chosunghyun18 <krismon@naver.com>
2024-10-21 10:53:59 +0900

    test : 커스텀 구분자 슬레쉬 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -18,6 +18,17 @@ class CalculatorTest extends NsTest {
         });
     }
 
+    @DisplayName("커스텀 구분자 사용")
+    @Test
+    void 커스텀_구분자_공백_슬래쉬_다수_포함_사용() {
+        assertSimpleTest(() -> {
+            String customDelimiter = "/; 3ㅇ ㄹㄹ f  \\t\\ t\\ t\\ \\ \\n";
+            run("//"+customDelimiter+"\\n"+"3"+customDelimiter+"2:6");
+            assertThat(output()).contains("결과 : 11");
+        });
+    }
+
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
