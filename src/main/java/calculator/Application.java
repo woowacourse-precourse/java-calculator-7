00b627218483c0a35d1ae71cbc4dcf3b171621d4
euni <kongeuni1004@gmail.com>
2024-10-21 10:59:53 +0900

    feat: 입력 값에 대한 예외 처리 추가
    - 유효하지 않은 문자가 포함된 경우 IllegalArgumentException 발생 처리
    - 쉼표(,) 및 세미콜론(;) 외의 구분자를 사용할 경우 예외 처리
    - 빈 문자열 또는 공백 입력 시 예외 처리
    - 음수 및 잘못된 숫자 형식에 대한 예외 처리 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -25,18 +25,31 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        if(input == null){
+            throw new IllegalArgumentException("빈 문자열입니다");
+        }
         return input;
     }
 
     public String[] sliceString(String input){
-        String[] slice = input.split("[,;]");
-        return slice;
+        if (input.matches(".*[^0-9,;].*")) {
+            throw new IllegalArgumentException("유효하지 않은 문자가 포함되어 있습니다.");
+        }
+        return input.split("[,;]");
     }
 
     public int[] stringArrayToIntArray(String[] slice){
         int[] numList = new int[slice.length];
 
         for (int i = 0; i < slice.length; i++) {
+            try{
+                int number = Integer.parseInt(slice[i]);
+                if(number < 0){
+                    throw new IllegalArgumentException("음수가 있습니다");
+                }
+            }catch (NumberFormatException e){
+                throw new IllegalArgumentException(slice[i]+"는 유효하지 않은 숫자 형식입니다.");
+            }
             numList[i] = Integer.parseInt(slice[i]);
         }
         return numList;
@@ -53,7 +66,13 @@ public class Application {
     public String[] customSliceString(String input){
         input = input.replace("\\n","\n");
         String[] s = input.split("\n");
+        if(s.length != 2){
+            throw new IllegalArgumentException("유효한 형식이 아닙니다");
+        }
         String custom = s[0].substring(2);
+        if(custom.length() != 1){
+            throw new IllegalArgumentException("구분자가 유효한 형식이 아닙니다");
+        }
         String[] slice = s[1].split(Pattern.quote(custom));
         return slice;
     }
