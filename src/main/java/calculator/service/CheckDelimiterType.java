4512fdeee8a8ef2a1ca4a807e5339e6dac54298a
이준범 <jach1206@naver.com>
2024-10-21 19:02:37 +0900

    refactor(service): 유리수 판별을 위한 정규식 수정
    service의 코드 리팩토링
    - CheckDelimiterType 클래스: 유리수를 고려하여 정규식 수정

--- src/main/java/calculator/service/CheckDelimiterType.java
+++ src/main/java/calculator/service/CheckDelimiterType.java
@@ -3,7 +3,7 @@ package calculator.service;
 public class CheckDelimiterType {
 
     // 기본 구분자 형식인지 판별하기 위한 정규식
-    private static final String BASIC_DELIMITER_PATTERN = "(-)*([0-9])+([,|:])(.*)";
+    private static final String BASIC_DELIMITER_PATTERN = "(-)?([0-9])+(\\.[0-9]+)?([,|:])?(.*)";
 
     // 기본 구분자 형식인지 판별하기 위한 메소드
     public static Boolean isBasicDelimiter(String input) {
