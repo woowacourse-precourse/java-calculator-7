a8598503844958cd8b834b70d28468a229d671c3
tdddt <doto1116@ewhain.net>
2024-10-21 17:57:31 +0900

    feat: 커스텀 구분자를 사용해 숫자 추출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,11 +9,22 @@ public class Application {
 
         // 1. 문자열 입력
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
+        String text = Console.readLine();
 
         // 2. 숫자 추출
         // 기본 구분자
-        String[] num = input.split("[,:]");
-        // System.out.println(Arrays.toString(num));
+        String sep = ",|:";
+
+        // 커스텀 구분자
+        if(text.startsWith("//")){
+            int sepIdx = text.indexOf("\\n"); // 줄바꿈으로 인식하지 않기 위해
+            if (sepIdx != -1) {
+                sep = text.substring(2, sepIdx);  // 커스텀 구분자 추출
+                text = text.substring(sepIdx + 2); // 문자열에서 구분자 부분 제거
+                System.out.println("사용된 구분자: " + sep);
+            }
+        }
+        String[] num = text.split("["+sep+"]");
+        System.out.println(Arrays.toString(num));
     }
 }
