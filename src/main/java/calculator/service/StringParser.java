b12dfb17f397f0ddd9a4d829dc0622c487db9714
cherry031 <20203093@kookmin.ac.kr>
2024-10-21 14:28:52 +0900

    feat: 커스텀 구분자로 구분

--- src/main/java/calculator/service/StringParser.java
+++ src/main/java/calculator/service/StringParser.java
@@ -1,8 +1,24 @@
 package calculator.service;
 
 public class StringParser {
+    private static final String DEFAULT_DELIMITERS = ",|:";
+
     public String[] parseString(String input) {
-        String[] parsed = input.split(", | :");
-        return parsed;
+        if (input.startsWith("//")) {
+            String customDelimiter = findCustomDelimiter(input);
+            String delimiters = DEFAULT_DELIMITERS + "|" + customDelimiter;
+            return splitString(input.substring(input.indexOf("\n") + 1), delimiters);
+        }
+
+        return splitString(input, DEFAULT_DELIMITERS);
+    }
+
+    private String findCustomDelimiter(String input) {
+        int endIndex = input.indexOf("\n");
+        return input.substring(2, endIndex);
+    }
+
+    private String[] splitString(String input, String delimiters) {
+        return input.split(delimiters);
     }
 }
