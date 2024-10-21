f5b77c9fce77b09641c4c8fa167deacebb30760d
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-21 14:35:38 +0900

    feat: [기능 구현 2번] CalculatorService DTO 변환 로직 추가

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,5 +1,15 @@
 package calculator.service;
 
+import calculator.dto.CalculationDTO;
+import calculator.model.CalculationResult;
+
 public class CalculatorService {
 
+  public CalculationResult calculate(String inputData) {
+
+    CalculationDTO calculationDTO = new CalculationDTO();
+    calculationDTO.calculate(inputData);
+
+    return calculationDTO.toModel();
+  }
 }
