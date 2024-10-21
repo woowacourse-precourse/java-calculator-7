5f06079ba2d89f0342248b34e92096a9f6147688
YOON JAEHYEON <yoonjaehyeon@YOONui-MacBookPro.local>
2024-10-21 19:48:30 +0900

    기본 입출력 구성 및 빈 문자열 처리 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static int calculator_7(String str){
+        if (str.trim().isEmpty()) return 0;
+
+        return 0;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        int result = calculator_7(input);
+        System.out.println("결과 : " + result);
     }
 }
