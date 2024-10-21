7c14fb0a3aeb691703ad3565f5ffc10b18208a78
parksaegyeol <dmsgkrkt1@naver.com>
2024-10-21 17:05:42 +0900

    테스트용 최종 코드 재제출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        //테스트용 최종 코드 재제출
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         String delimiters = ",|:";
