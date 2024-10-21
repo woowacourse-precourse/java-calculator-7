1e132a3488f2cb37a4bc4ac720d92b8845853d66
wouldu <jhw664089@gmail.com>
2024-10-21 22:47:19 +0900

    fix: 예외 테스트 통과를 위한 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -15,6 +15,7 @@ public class Application {
             System.out.println("결과 : " + result);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+            throw e;
         }
     }
 }
