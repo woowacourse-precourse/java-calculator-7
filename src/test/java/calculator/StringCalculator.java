498973b99feb1ee1735b9860d90dab6cb8862761
SinnoLn <sintmtn69@gmail.com>
2024-10-21 12:37:24 +0900

    feat: 빈 배열 처리 기능 추가

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -6,6 +6,10 @@ public class StringCalculator {
             throw new IllegalArgumentException("입력 값이 null 일 수 없습니다");
         }
 
+        if (numbers.length == 0) {
+            return 0;  // 빈 배열일 경우 0 반환
+        }
+
         int sum = 0;
         for (int num : numbers) {
             sum += num;
