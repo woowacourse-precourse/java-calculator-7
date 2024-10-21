1f465edfa21b6e3ee054479c4f9622e63b4138b8
Cho seyun <joseyun94@naver.com>
2024-10-21 20:39:40 +0900

    test: 문자열 덧셈 계산기 종합 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -20,6 +21,87 @@ class ApplicationTest extends NsTest {
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("커스텀, 기본 구분자 혼합 사용시 정상 반환")
+    void success_mix() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;1,1:1");
+            assertThat(output()).contains("결과 : 4");
+        });
+    }
+
+    @Test
+    @DisplayName("커스텀, 기본 구분자 혼합 사용시 예외 - 숫자로 치환할 수 없음")
+    void fail_mix2() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//;\\na;1,1:1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName(" \"\"입력 시 0반환 ")
+    void success_blank() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    @DisplayName(" \"\"입력 시 0반환 ")
+    void success_manyBlank() {
+        assertSimpleTest(() -> {
+            run("        ");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    @DisplayName("커스텀(' '), 기본 구분자 혼합 사용시 정상 반환")
+    void success_spaceDelimiter() {
+        assertSimpleTest(() -> {
+            run("// \\n1 1 1:1");
+            assertThat(output()).contains("결과 : 4");
+        });
+    }
+
+    @Test
+    @DisplayName("양쪽에 공백이 있는 커스텀(' '), 기본 구분자 혼합 사용시 정상 반환")
+    void success_spaceDelimiterAndSideBlank() {
+        assertSimpleTest(() -> {
+            run("    // \\n1 1 1:1  ");
+            assertThat(output()).contains("결과 : 4");
+        });
+    }
+
+    @Test
+    @DisplayName("소숫점자리 정상 출력")
+    void success_print_decimal() {
+        assertSimpleTest(() -> {
+            run("1,1,1.1");
+            assertThat(output()).contains("결과 : 3.1");
+        });
+    }
+
+    @Test
+    @DisplayName("소숫점자리 정상 출력")
+    void success_print() {
+        assertSimpleTest(() -> {
+            run("1.5,1.3,1.2");
+            assertThat(output()).contains("결과 : 4");
+        });
+    }
+
+    @Test
+    @DisplayName("empty 커스텀 구분자 사용시 예외처리")
+    void fail_emptyCustomDelimiter() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//\\n1,1:1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
