6f37b6abc2dc16a0051c1f73d3841d96466acaac
KangHyeonmin <rkdgusals369@naver.com>
2024-10-21 22:00:56 +0900

    결과를 출력하는 메서드

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	runCalculator();
     }
+
+    public static void runCalculator() {
+        try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String str = Console.readLine();
+
+            int sum = add(str);
+            System.out.println("결과 : " + sum);
+        } catch (IllegalArgumentException e) {
+            System.out.println("에러: " + e.getMessage());
+            throw e;  
+        }
+    }
+
+   
 }
