3696ef36431a7e2b093a3752fdaa872a926e08e7
Shin1012 <83046264+Eunoia24@users.noreply.github.com>
2024-10-21 23:15:37 +0900

    Function 1

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 1. 사용자로부터 입력을 받는 기능
+        String input = Console.readLine();
+        System.out.println("입력받은 문자열: " + input);
+
+        // TODO: 나머지 기능 구현
     }
 }
