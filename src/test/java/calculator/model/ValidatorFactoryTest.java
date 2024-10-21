222e8a0105a3b53222776b6efe089dca016daca2
kimchanho97 <nh0903@pusan.ac.kr>
2024-10-21 21:00:25 +0900

    test(ValidatorFactoryTest): 커스텀 구분자 문자열이 아닌 경우 DefaultValidator 생성 테스트

--- src/test/java/calculator/model/ValidatorFactoryTest.java
+++ src/test/java/calculator/model/ValidatorFactoryTest.java
@@ -19,4 +19,24 @@ class ValidatorFactoryTest {
         // then
         assertInstanceOf(CustomValidator.class, validator);
     }
+
+    @Test
+    @DisplayName("기본 구분자 문자열 입력시 DefaultValidator를 생성한다.")
+    void 기본_구분자_Validator_생성() {
+        String defaultDelimiterInput = "1,2,3";
+
+        Validator validator = ValidatorFactory.createValidator(defaultDelimiterInput);
+
+        assertInstanceOf(DefaultValidator.class, validator);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 문자열 정규표현식을 만족하지 않는 경우 DefaultValidator를 생성한다.")
+    void 커스텀_구분자_이외의_입력_Validator_생성() {
+        String notCustomDelimiterInput = "//*n1*2*3";
+
+        Validator validator = ValidatorFactory.createValidator(notCustomDelimiterInput);
+
+        assertInstanceOf(DefaultValidator.class, validator);
+    }
 }
