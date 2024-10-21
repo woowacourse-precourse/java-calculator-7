2a325c04fac2e222eb2df1d9bf396a3d165700f0
jehyuck <wpgur07@gmail.com>
2024-10-21 16:05:02 +0900

    feat: 문자열 계산 class Calculator 기초 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+    public Integer calculate(String input) {
+        int rtn = 0;
+        if (input.length() == 0) {
+            return rtn;
+        }
+        return 0;
+    }
+}
