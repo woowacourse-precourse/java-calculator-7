71c6775c8e43609df92aceaf18db4de050ef7f7c
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-21 18:22:09 +0900

    feat(InputReader): 공백 문자열 예외처리 추가
    - 공백 문자열과 "" 빈 문자열은 서로 다르고, 입력 조건에서 문자열과 양수만 입력할 수 있다고 했으니 공백 문자열 입력시에는 예외로 처리
    - 또한 빈 문자열은 0으로 출력해야함을 문제의 예시에서 언급했기 때문에, 빈 문자열을 공백 문자열로 변환해서 넘겨줌
    - 사용자 최초 입력 시에 공백 문자열만 아니면 IllegalArgumentException이 발생하지 않음
    - 빈 문자열을 공백 문자로 만들어서 컨트롤러에 전달하는 것은 IllegalArgumentException이 아님

--- src/main/java/calculator/Service/InputReader.java
+++ src/main/java/calculator/Service/InputReader.java
@@ -8,13 +8,20 @@ public class InputReader {
     // 입력을 받아 DTO로 반환하는 메서드
     public StringCalculatorDTO read() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();      // 사용자로부터 입력 받기
+        String input = Console.readLine();
 
         if (input.isEmpty()) {
-            input = " ";                        // 빈 문자열(\n) 입력시에는 " " 를 반환
+            // 빈 문자열을 " "로 바꿨지만 해당 부분 이외에는 공백에 대한 예외 처리를 하지 않음
+            input = " ";
+        } else if (input.isBlank()) {
+            // 공백 문자는 입력 조건에 위배되기 때문에 예외 처리
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력 형식 (공백):" + input);
         }
 
-        Console.close();                        // 입력 후 콘솛 객체 제거
-        return new StringCalculatorDTO(input);  // 입력값을 DTO로 감싸서 반환
+        // 입력 후 콘솛 객체 제거
+        Console.close();
+
+        // 입력값을 DTO로 감싸서 반환
+        return new StringCalculatorDTO(input);
     }
 }
