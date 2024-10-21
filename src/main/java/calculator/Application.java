fc101a640b5daf2ad6e191ac03017997271a89c4
KIMB0B <axzsd96301@gmail.com>
2024-10-21 16:50:23 +0900

    feat(Application): 문자열 분리하여 덧셈 할 수로 인식

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,11 +10,23 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         ArrayList<String> sep = new ArrayList<>(Arrays.asList(",", ":"));
+        ArrayList<Integer> numbers = new ArrayList<>();
 
         if (input.startsWith("//")) {
             int customSepIndexEnd = input.indexOf("\n");
             String newSep = input.substring(2, customSepIndexEnd).trim();
             sep.add(newSep);
         }
+
+        String[] strNumbers = input.split(String.join("|", sep));
+
+        for (String number : strNumbers) {
+            if (number.isEmpty()) { // 빈 문자열 체크
+                numbers.add(0);
+            } else {
+                numbers.add(Integer.parseInt(number.trim()));
+            }
+        }
+
     }
 }
