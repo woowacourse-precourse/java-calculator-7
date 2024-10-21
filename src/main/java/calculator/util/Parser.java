769cf571806f5253a1b5768bd21d53da28104919
young-in <hardworkingini@gmail.com>
2024-10-21 20:05:11 +0900

    refactor : method 1개가 하나의 역할만 하도록 메소드 분리

--- src/main/java/calculator/util/Parser.java
+++ src/main/java/calculator/util/Parser.java
@@ -1,6 +1,7 @@
 package calculator.util;
 
 import calculator.domain.Calculator;
+import calculator.domain.Tokens;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,22 +9,34 @@ import java.util.regex.Pattern;
 public class Parser {
     private static final String DEFAULT_DELIMITERS = ",|:";
 
-    public Calculator parse(String input) {
-        String delimiter = DEFAULT_DELIMITERS;
+    public Tokens parse(String input) {
         String numbers = input;
 
         if(input.startsWith("//")){
-            Matcher matcher = Pattern.compile("//(.*?)\\\\n(.*)").matcher(input);
-            if(matcher.matches()){
-                String customDelimiter = matcher.group(1);
-                delimiter =  DEFAULT_DELIMITERS + "|" + customDelimiter;
-                numbers = matcher.group(2);
-            }else{
-                throw new IllegalArgumentException("잘못된 구분자 형식입니다.");
-            }
+            return parseCustomDelimiter(input);
         }
 
-        String[] tokens = numbers.split(delimiter);
-        return new Calculator(delimiter, tokens);
+        return parseTokens(new Calculator(input));
+    }
+
+    private Tokens parseCustomDelimiter(String input) {
+        String delimiter =  DEFAULT_DELIMITERS;
+        String numbers = input;
+
+        Matcher matcher = Pattern.compile("//(.*?)\\\\n(.*)").matcher(input);
+        if(matcher.matches()){
+            String customDelimiter = matcher.group(1);
+            delimiter =  DEFAULT_DELIMITERS + "|" + customDelimiter;
+            numbers = matcher.group(2);
+
+            return parseTokens(new Calculator(delimiter, numbers));
+        }else{
+            throw new IllegalArgumentException("잘못된 구분자 형식입니다.");
+        }
+    }
+
+    private Tokens parseTokens(Calculator calculator) {
+        String[] tokens = calculator.getNumbers().split(calculator.getDelimiter());
+        return new Tokens(tokens);
     }
 }
