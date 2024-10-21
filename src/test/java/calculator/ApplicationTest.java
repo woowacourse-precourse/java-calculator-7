cd959c55233344d0c1dba20c4508c2933efb8809
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-10-21 22:16:29 +0900

    test(ApplicationTest): calculator Integration Test 구현 및 완료
    string을 list로 변환하여 sum을 계산해 반환하는 Calculator의 Integration test를 구현하고 성공하였습니다. ApplicationTest에도 반영하였습니다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,19 +9,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
-    void 커스텀_구분자_사용() {
+    void 커스텀_구분자_사용_테스트() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
         });
     }
     @Test
-    void 커스텀_구분자_긴_배열() {
+    void 커스텀_구분자_긴_배열_테스트() {
         assertSimpleTest(() -> {
             run("//;\\n1;3;56;6;78;3;1;");
             assertThat(output()).contains("결과 : 148");
         });
     }
+
     @Test
     void 커스텀_구분자_복잡한_형태() {
         assertSimpleTest(() -> {
@@ -39,7 +40,15 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트_잘못된_구분자_입력_형식() {
+    void 커스텀_구분자_테스트_예외_양수가_아닌_경우() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//;\\n1;3;56;6;78;3;-1;"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_테스트_예외_잘못된_형식() {
         assertSimpleTest(() -> {
             assertThatThrownBy(() -> runException("//*@#$%@#$&%@#$%\\4*@#$%@#$&%@#$%4*@#$%@#$&%@#$%5"))
                     .isInstanceOf(IllegalArgumentException.class);
