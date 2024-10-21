50f7ff61390cf1ed230b569abd15b2fda891a72a
smk412 <whu1477@naver.com>
2024-10-21 16:45:57 +0900

    feat : 기본 구분자로 문자열 분리 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,10 +7,12 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        String newSep = str;
 
-
-        System.out.println(str);
-
+        String[] sep = str.split("[,:]");
+        for( int i = 0; i < sep.length; i++ ) {
+            System.out.println(sep[i]);
+        }
 
     }
 }
