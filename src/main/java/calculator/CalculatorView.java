986d1eb4de5f1eb886be46a73fbf0455473884e0
ChanghyunNam (PC) <tabloni0719@ajou.ac.kr>
2024-10-21 11:18:52 +0900

    fix : Test 케이스 "" 시 결과 : 0을 반환하도록 예외사항 수정

--- src/main/java/calculator/CalculatorView.java
+++ src/main/java/calculator/CalculatorView.java
@@ -2,13 +2,18 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.math.BigInteger;
+import java.util.NoSuchElementException;
 
 public class CalculatorView {
 
-    public String getInput() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+public String getInput() {
+    System.out.println("덧셈할 문자열을 입력해 주세요.");
+    try {
         return Console.readLine();
+    } catch (NoSuchElementException e) {
+        return "";
     }
+}
     
     public void displayResult(BigInteger result) {
         System.out.println("결과 : " + result);
