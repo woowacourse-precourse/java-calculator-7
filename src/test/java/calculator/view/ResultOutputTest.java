2a0392d32ba33522ebfb7a10692b455d2638d461
ratatuwee <pkqwaszx@gmail.com>
2024-10-21 16:30:16 +0900

    test(result output): 결과값 출력 클래스 테스트 추가
    결과값 출력 클래스의 테스트를 추가 하였습니다
    출력을 캡쳐하여 결과값과 비교하는 테스트와 사용하는 상수의 값을 확인하는 테스트를 추가하였습니다

---
+++ src/test/java/calculator/view/ResultOutputTest.java
@@ -0,0 +1,44 @@
+package calculator.view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ResultOutputTest {
+    @DisplayName("출력시 사용되는 상수 확인")
+    @Test
+    void checkViewOutputMessage(){
+        assertThat(ResultOutput.OutputMessage.OUTPUT_PROMPT.getMessage()).isEqualTo("결과 : ");
+    }
+    @DisplayName("출력 결과물 확인_정수")
+    @Test
+    void checkViewOutputInteger(){
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        ResultOutput resultOutput = new ResultOutput();
+        resultOutput.promptResultOutput("3");
+
+        assertEquals("결과 : 3\n", outputStream.toString());
+
+        System.setOut(originalOut);
+    }
+    @DisplayName("출력 결과물 확인_소수")
+    @Test
+    void checkViewOutputDecimal(){
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        ResultOutput resultOutput = new ResultOutput();
+        resultOutput.promptResultOutput("3.3");
+
+        assertEquals("결과 : 3.3\n", outputStream.toString());
+
+        System.setOut(originalOut);
+    }
+}
