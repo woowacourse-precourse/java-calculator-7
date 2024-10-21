35ee064559881aa591bde20e3b68a46982cca93a
Chan531 <chan657312@gmail.com>
2024-10-21 17:54:32 +0900

    test(NumberExtractorTest): n자리 수를 포함한 문자열 테스트

--- src/test/java/calculator/model/NumberExtractorTest.java
+++ src/test/java/calculator/model/NumberExtractorTest.java
@@ -26,10 +26,31 @@ class NumberExtractorTest {
 		assertEquals(expectedResult, result);
 	}
 
+	@ParameterizedTest
+	@DisplayName("숫자가 한 자리 수가 아니라면 그 수의 길이만큼 수를 추출한 후 수의 리스트를 반환한다.")
+	@MethodSource("N자리_수_예제_케이스")
+	void N자리_수가_포함된_유효한_문자열이_입력되었을_때_수의_리스트를_반환한다(String input, List<Integer> expectedResult) {
+		// given
+		NumberExtractor numberExtractor = new NumberExtractor();
+
+		// when
+		List<Integer> result = numberExtractor.extractNumbers(input);
+
+		// then
+		assertEquals(expectedResult, result);
+	}
+
 	private static Stream<Arguments> 유효한_문자열_예제_케이스() {
 		return Stream.of(
 			    Arguments.of("1,2:3", List.of(1, 2, 3)),
 			    Arguments.of("1:2,3#4", List.of(1, 2, 3, 4))
+		);
+	}
+
+	private static Stream<Arguments> N자리_수_예제_케이스() {
+		return Stream.of(
+			Arguments.of("1,22:333", List.of(1, 22, 333)),
+			Arguments.of("1:22,333#4444", List.of(1, 22, 333, 4444))
 		);
 	}
 }
