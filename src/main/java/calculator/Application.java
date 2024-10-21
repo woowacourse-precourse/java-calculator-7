cb381cfa3bea6092df2be8c0c2d2f32e65d94ea9
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-10-21 18:17:36 +0900

    Feat: 구분자 추출 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,13 +7,42 @@ import java.io.OutputStreamWriter;
 
 public class Application {
 
+    //문자열이 비었거나 null인 경우 0을 반환하고 프로그램을 끝내기 위해 생성
     public static boolean isBlankOrNull(String sentence){
         return sentence.isBlank() || sentence == null;
+    }
+
+    public static String extractLetter(String str){
+        char firstLetter = str.charAt(0);
+        //:와 ,로 구분되는 경우
+        if(firstLetter == ':' || firstLetter == ',' || Character.isDigit(firstLetter)){
+            return ":|,";
+        }
+        //커스텀 구분자를 쓰는 경우
+        else if(firstLetter == '/') {
+            return extractCustomLetter(str);
+        }
+        //잘못된 입력
+        throw new IllegalArgumentException("[extractLetter] 잘못된 입력");
+    }
+
+    public static String extractCustomLetter(String str){
+        if(str.length() > 2){
+            return str.substring(2);
+        }
+
+        throw new IllegalArgumentException("[extractCustomLetter] 잘못된 입력");
     }
 
     public static void main(String[] args) {
         String str = Console.readLine();
         int result = 0;
+
+        if(!isBlankOrNull(str)){
+            String letter = extractLetter(str);
+            System.out.println(letter);
+        }
+
 
     }
 }
