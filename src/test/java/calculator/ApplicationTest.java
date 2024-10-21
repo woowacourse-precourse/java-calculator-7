227346971a01a9ad9d8388fa39c582fff965c702
leestana01 <leestana01@naver.com>
2024-10-21 10:23:17 +0900

    test: 단위 테스트 추가
    단위 테스트 추가
    - 구분자 파싱
    - 합계 계산
    - 문자 to 숫자 변환

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,6 +8,63 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    // 단위 테스트 ----
+    Application app = new Application();
+
+    @Test
+    public void 기본_구분자_파싱() {
+        String input = "1,2:3";
+        String[] result = app.splitInput(input);
+        assertThat(result[0]).contains("[,:]");
+        assertThat(result[1]).contains("1,2:3");
+    }
+
+    @Test
+    public void 커스텀_구분자_파싱() {
+        String[] result = app.splitInput("//;\\n1;2;3");
+        assertThat(result[0]).contains("[,:;]");
+        assertThat(result[1]).contains("1;2;3");
+    }
+
+
+    @Test
+    public void 합계_계산() {
+        Integer result = app.getSum(new String[]{"[,:;]", "1;2;3"});
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void 합계_계산_음수포함() {
+        String[] splitInput = new String[]{"[,;]", "1;-2;3"};
+        assertThatThrownBy(() -> app.getSum(splitInput))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 문자_숫자_변환() {
+        assertThat(app.convertToNum("1")).isEqualTo(1);
+    }
+
+    @Test
+    public void 문자_숫자_변환_0입력() {
+        assertThatThrownBy(() -> app.convertToNum("0"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 문자_숫자_변환_음수입력() {
+        assertThatThrownBy(() -> app.convertToNum("-1"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 문자_숫자_변환_문자입력() {
+        assertThatThrownBy(() -> app.convertToNum("a"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    // 통합 테스트 ----
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
