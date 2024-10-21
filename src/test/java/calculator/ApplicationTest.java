756e75b6d3c078b1cbee45b56fb2c06d645856b9
YeaChan05 <qkenrdl05@gmail.com>
2024-10-21 17:19:24 +0900

    test(ApplicationTest): 추가 통합 테스트 작성
    요구사항에서 발생 가능한 예외 케이스를 추가해 테스트코드를 작성 및 검토

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,16 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -16,16 +21,102 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    public static String formatStringWithDelimiter(String template, String delimiter) {
+        Pattern pattern = Pattern.compile("%s");
+        Matcher matcher = pattern.matcher(template);
+        return matcher.replaceAll(Matcher.quoteReplacement(delimiter));
+    }
+
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    @DisplayName("빈 문자열인 경우 0 반환")
+    void assertBlankStringToZero() {
+        assertSimpleTest(() -> {
+            run("", "\n");
+            assertThat(output()).contains("0");
+        });
+    }
+
+    @Test
+    @DisplayName("여러 숫자 입력 (쉼표와 콜론 혼용)")
+    void assertMultipleNumbersWithCommaAndColon() {
+        assertSimpleTest(() -> {
+            run("1,2:3,4:5");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
+    @Test
+    @DisplayName("`//`만 있고 `\n`이 없는 경우 예외를 던짐")
+    void assertThrowExceptionWhenNoEndCommand() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//1,2,3"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("missing newline after header");
+        });
+    }
+
+    @Test
+    @DisplayName("구분자가 한글자 이상인 경우")
+    void assertSingleCharacterDelimiter() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//kk\\n1kk2kk3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("invalid length"));
+    }
+
+    @Test
+    @DisplayName("구분자가 공백인 경우")
+    void assertNotBeBlankDelimiter() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n123"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("delimiter cannot be blank")
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {",", ":"})
+    @DisplayName("기존 구분자를 다시 커스텀 구분자로 등록하는 경우")
+    void assertAlsoAbleToCustomizeDefaultDelimiter(String delimiter) {
+        String arg = formatStringWithDelimiter("//%s\\n1%s2%s3", delimiter);
+        assertSimpleTest(() -> {
+            run(arg);
+            assertThat(output()).contains("6");
+        });
+    }
+
+    @Test
+    @DisplayName("개행문자가 사용자 지정 구분자인 경우")
+    void assertNotAllowNewlineCommandAsDelimiter() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\n\n123"))
+                        .isInstanceOf(IllegalArgumentException.class));
     }
 
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1:2:3","1,2,3","//\n123","//n\\n1n2n3","//kk\\n1kk2kk3","//-\\n-1-2-3"})
+    @DisplayName("시스템 문자열은 항상 출력됨")
+    void assertMustContainSystemMessage(String argument) {
+        assertSimpleTest(() -> {
+            try {
+                run(argument);
+                assertThat(output()).contains("덧셈할 문자열을 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                //예외에 대한 테스트 X
+            }
+        });
     }
 }
