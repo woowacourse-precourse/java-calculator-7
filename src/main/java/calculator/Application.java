0306d527be10b94d5948afafec11decd5a609c09
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-10-21 15:42:44 +0900

    chore: 주석 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,8 +4,6 @@ import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
         Calculator calculator = new Calculator();
         CalculatorIO calculatorIO = new CalculatorIO();
 
--- src/main/java/calculator/CalculatorIO.java
+++ src/main/java/calculator/CalculatorIO.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class CalculatorIO {
     public String input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine(); // 입력 받은 문자열 반환
+        return Console.readLine();
     }
 
     public void output(int result) {
