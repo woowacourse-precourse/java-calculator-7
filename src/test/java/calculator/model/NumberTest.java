ecd9269822d84680081fb792db87613993561f23
Park - Jeonghwan <990706leo@gmail.com>
2024-10-21 15:49:17 +0900

    test(Number): 동등성을 보장한다

--- src/test/java/calculator/model/NumberTest.java
+++ src/test/java/calculator/model/NumberTest.java
@@ -2,6 +2,7 @@ package calculator.model;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import calculator.model.vo.Number;
 import org.junit.jupiter.api.Test;
@@ -22,6 +23,14 @@ public class NumberTest {
         Number test = Number.of(empty);
 
         assertEquals("0", test.toString());
+    }
+
+    @Test
+    void Number는_동등성을_보장한다() {
+        Number one = Number.of("10");
+        Number another = Number.of("10");
+
+        assertTrue(one.equals(another));
     }
 
 }
