936095af9511e58a03cbbabde34e991ccb29861d
davemins <davemins4125@naver.com>
2024-10-21 10:12:27 +0900

    feat(Controller): 입력 요청 메시지 출력 함수 호출

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,10 @@
+package calculator.controller;
+
+import calculator.view.View;
+
+public class CalculatorController {
+
+    public void run() {
+        String input = View.requestString();
+    }
+}
