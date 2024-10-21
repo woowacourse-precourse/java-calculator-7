20a25f5797f90ae374777cbb4ef9553f9416d067
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-21 20:52:30 +0900

    style(SumCalculatorTest): 코드 컨벤션 적용

--- src/test/java/calculator/model/SumCalculatorTest.java
+++ src/test/java/calculator/model/SumCalculatorTest.java
@@ -12,8 +12,9 @@ public class SumCalculatorTest extends NsTest {
 
     @Test
     void 합계가_long_범위를_넘어가면_예외발생() {
-        assertSimpleTest(() -> assertThatThrownBy(() -> runException(Long.MAX_VALUE + ",1")).isInstanceOf(
-                IllegalArgumentException.class).hasMessage(Message.SUM_LONG_OVERFLOW.getMessage()));
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException(Long.MAX_VALUE + ",1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Message.SUM_LONG_OVERFLOW.getMessage()));
     }
 
     @Override
