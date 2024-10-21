13810c99574390ae4338263fd2c4eafd2e6bc16c
rkwkalrms <140880709+rkwkalrms@users.noreply.github.com>
2024-10-21 16:59:46 +0900

    Refactor: Input 메서드 호출 추가 및 코드 전반 개선

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,21 +1,15 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
-        //문자열 받기
-        StringFilter strF = new StringFilter(Console.readLine());       //문자 필터링
+        String input = Input.inputString();
 
+        StringFilter strF = new StringFilter();
         Calculator cal = new Calculator();
 
-        String[] numbers = strF.getFilteredString();
-        System.out.println("결과 : " + cal.sumResult(numbers));
+        String[] numbers = strF.checkString(input);
 
-
-
-
-
+        System.out.println("결과 : " + cal.result(numbers));
 
     }
 }
