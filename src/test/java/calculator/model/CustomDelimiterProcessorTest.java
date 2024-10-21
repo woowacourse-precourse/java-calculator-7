f4807e855aa240ff0123fde5fee36c78233391e9
unh6unh6 <ruddbs3308@naver.com>
2024-10-21 15:31:12 +0900

    test: 커스텀 구분자를 지정하지 않는 경우 테스트

--- src/test/java/calculator/model/CustomDelimiterProcessorTest.java
+++ src/test/java/calculator/model/CustomDelimiterProcessorTest.java
@@ -20,6 +20,15 @@ public class CustomDelimiterProcessorTest {
     }
 
     @Test
+    void 커스텀_구분자_지정하지_않는_경우() {
+        String inputString = "5,3:2";
+        assertThat(inputString)
+                .isEqualTo(customDelimiterProcessor.addCustomDelimiters("5,3:2", delimiters));
+        assertThat(delimiters.getDelimiters()).
+                isEqualTo(Arrays.asList(new Character[]{',', ':'}));
+    }
+
+    @Test
     void 남아있는_문자열이_충분하지_않으면_예외() {
         assertThatThrownBy(() ->
                 customDelimiterProcessor.addCustomDelimiters("//$n", delimiters))
