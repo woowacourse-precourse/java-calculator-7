4f2e593bd914afb1818111ffcebf4d2f95cc68db
KimJongMin <whdals011203@naver.com>
2024-10-21 17:41:49 +0900

    refactor(Calculator.java): 덧셈 로직 수정
    sumOfString 메소드 로직 수정, Number sum(Number, Number) 메소드 추가
    
    - 덧셈 연산 전, validator를 이용한 유효성 검증 로직 추가
    - NumberParser 인터페이스의, static 메소드인 parse 사용
    - private 메소드인 sum(Number, Number) 메소드는 마지막 최종 덧셈 연산에서 호출
    - String[] 타입을 stream API를 이용하여, 함수형으로 간단히 처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,41 +1,28 @@
 package calculator;
 
-import calculator.parser.ExpressionParser;
-import calculator.parser.Parser;
-import camp.nextstep.edu.missionutils.Console;
+import calculator.Validator.Validator;
+import calculator.parser.NumberParser;
 import java.util.Arrays;
 
 public class Calculator {
 
-    private final Parser parser;
-
     public Calculator() {
-        parser = new ExpressionParser();
-    }
-
-    public String input() {
-        return Console.readLine();
     }
 
     //think: 스트림의 강력함을 사용하면 좋겠지만, 이렇게 쓰는 방식이 좋을지 모르겠다.
-    public double sumOfString(String[] value) {
+    public Number sumOfString(String[] value) {
+        Validator.validate(value); // 계산 하기 전 유효성 검사
         return Arrays.stream(value)
-                .map(v -> {
-                    try {
-                        return Double.parseDouble(v);
-                    } catch (NumberFormatException e) {
-                        throw new IllegalArgumentException("숫자만 입력해주세요."); //think: 람다식 안에서 예외를 던지는 방식이 맞을까?
-                    }
-                })
-                .reduce(Double::sum)
-                .orElse(0.0);
+                .map(NumberParser::parse) // 스트림에서 parse를 계속 호출하고 있는 문제점
+                .reduce(this::sum)
+                .orElse(0);
     }
 
-    private static void findDelimiter(Parser parser, String text) {
-        parser.setDelimiters(text);
-    }
-
-    private static String[] split(Parser parser, String text) {
-        return parser.split(text);
+    private Number sum(Number n1, Number n2) {
+        if (n1.getClass().equals(Double.class) && n2.getClass().equals(Double.class)) {
+            return Double.sum(n1.doubleValue(), n2.doubleValue());
+        } else {
+            return Integer.sum(n1.intValue(), n2.intValue());
+        }
     }
 }
