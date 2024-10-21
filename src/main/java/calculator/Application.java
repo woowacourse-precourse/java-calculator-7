449fd51aecd78609932eb4190f1c206b9184c7f6
Jewoo Lee <jaewoo0254@gmail.com>
2024-10-21 11:55:36 +0900

    refactor: remove unnecessary code

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,8 +4,6 @@ import calculator.controller.InputController;
 import calculator.parser.InputParser;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Scanner;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -14,7 +12,7 @@ public class Application {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        
+
         inputController.processInput(input);
     }
 }
