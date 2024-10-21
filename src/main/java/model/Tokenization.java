8ae06687acfcde5c17f0fe00d79a498589a73196
aaahyunseo <hs030608@naver.com>
2024-10-21 23:12:53 +0900

    refactor(Tokenization): 문자열 분리 기능 역할 분리

--- src/main/java/model/Tokenization.java
+++ src/main/java/model/Tokenization.java
@@ -11,28 +11,25 @@ public class Tokenization {
     private static final Pattern CUSTOM_OPERATION_PATTERN = Pattern.compile("^//(.*)\\\\n(.*)");
     private static final String DEFAULT_OPERATION = ",|:";
 
+
     /**
-     * 구분자에 따라 문자열 분리하는 함수
+     * 구분자에 따라 문자열 분리하는 메인 함수
      * **/
     public ArrayList<Integer> tokenize(String str){
         ArrayList<Integer> numArr = new ArrayList<>();
-        String operation = DEFAULT_OPERATION;
-        Matcher matcher = CUSTOM_OPERATION_PATTERN.matcher(str);
+        String delimiter = extractDelimiter(str);
 
-        // 구분자 확인
-        if(matcher.find()){
-            operation = matcher.group(1);
-            str = matcher.group(2);
-            operation = Pattern.quote(operation);
-        }
+        str = extractContent(str);
 
-        // 구분자에 따라 문자열 분리
-        for(String i:str.split(operation)){
-            if (!i.matches(NUMBER_PATTERN)) {
+        String[] tokens = splitString(str, delimiter);
+
+        // 문자열 정수형으로 변환
+        for(String token : tokens){
+            if (!token.matches(NUMBER_PATTERN)) {
                 throw new IllegalArgumentException(ErrorMessage.INVALID_TYPE.getErrorMessage());
             }
             try{
-                int num = Integer.parseInt(i);
+                int num = Integer.parseInt(token);
                 if (num<=0){
                     throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER.getErrorMessage());
                 }
@@ -42,5 +39,34 @@ public class Tokenization {
             }
         }
         return numArr;
+    }
+
+    /**
+     * 구분자 추출 메서드
+     */
+    private String extractDelimiter(String str) {
+        Matcher matcher = CUSTOM_OPERATION_PATTERN.matcher(str);
+        if (matcher.find()) {
+            return Pattern.quote(matcher.group(1));
+        }
+        return DEFAULT_OPERATION;
+    }
+
+    /**
+     * 구분자에 따라 분리한 내용 추출 메서드
+     */
+    private String extractContent(String str) {
+        Matcher matcher = CUSTOM_OPERATION_PATTERN.matcher(str);
+        if (matcher.find()) {
+            return matcher.group(2);
+        }
+        return str;
+    }
+
+    /**
+     * 구분자에 따라 문자열 분리 메서드
+     */
+    private String[] splitString(String str, String delimiter) {
+        return str.split(delimiter);
     }
 }
