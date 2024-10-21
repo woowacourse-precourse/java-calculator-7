39f723af9fe4884eeb8b7dba638ece64b16e90de
이준범 <jach1206@naver.com>
2024-10-21 16:37:11 +0900

    refactor(service): 사용자가 입력한 숫자가 음수인 경우에 대한 예외 처리 추가
    service에 예외 처리 추가
    - NumExtractor 클래스: 사용자가 입력한 숫자에 음수가 들어있는 경우 예외 처리 코드 추가

--- src/main/java/calculator/exception/ErrorMessage.java
+++ src/main/java/calculator/exception/ErrorMessage.java
@@ -3,7 +3,8 @@ package calculator.exception;
 public enum ErrorMessage {
 
     IS_NOT_NUMBER("구분자 사이에 숫자가 들어있지 않습니다."),
-    IS_NOT_CUSTOM_DELIMITER_PATTERN("커스텀 구분자 입력 형식과 일치하지 않습니다.");
+    IS_NOT_CUSTOM_DELIMITER_PATTERN("커스텀 구분자 입력 형식과 일치하지 않습니다."),
+    IS_NOT_POSITIVE("양수 이외의 값은 입력될 수 없습니다.");
 
     private final String message;
 
--- src/main/java/calculator/service/NumExtractor.java
+++ src/main/java/calculator/service/NumExtractor.java
@@ -10,16 +10,15 @@ import java.util.regex.Pattern;
 // 사용자의 입력값에서 숫자를 추출해내는 클래스
 public class NumExtractor {
     // 숫자를 판별하기 위한 정규식
-    private static final String NUMBER_VALID_PATTERN = "^[0-9]*$";
+    private static final String NUMBER_VALID_PATTERN = "^(-)*([0-9])*$";
     // 커스텀 구분자를 지정하는 부분을 제거하기 위한 정규식
     private static final String REMOVE_CUSTOM_DELIMITER_PATTERN = "(//)(.*)(\\\\n)(.*)";
 
     // 기본 구분자를 활용한 숫자 추출 메소드
     public static List<Integer> extractNumbersWithBasicDelimiters(String userInput){
-
         List<Integer> result = new ArrayList<>();
         String[] extractedNums = userInput.split(",|:");
-        //구분자 사이에 숫자 이외에 다른 것이 들어있는지 판별한다.
+        //구분자 사이에 숫자 이외에 다른 것이 들어있는지 판별
         numValid(extractedNums);
 
         for (String eachExtractedNum : extractedNums) {
@@ -36,8 +35,7 @@ public class NumExtractor {
         String extractedNumsWithCustomDelimiter = removeCustomDelimiter(userInput);
         //커스텀 구분자를 기준으로 숫자 추출
         String[] extractedNums = extractedNumsWithCustomDelimiter.split(delimiter);
-
-        // 구분자 사이에 숫자가 아닌 값이 들어가 있는지 체크
+        // 구분자 사이에 숫자가 아닌 값이 들어가 있는지 판별
         numValid(extractedNums);
 
         for (String eachExtractedNum : extractedNums) {
@@ -47,12 +45,18 @@ public class NumExtractor {
         return result;
     }
 
-    // 구분자 사이에 숫자 이외에 다른 것이 들어있다면 예외 발생
+    // 구분자 사이에 숫자 이외에 다른 것이 들어있거나 음수가 들어있다면 예외 발생
     public static void numValid(String[] extractedNums){
-
         for (String eachExtractedNum : extractedNums) {
+
+            //구분자 사이에 숫자 이외에 다른 것이 들어있다면 예외 처리
             if(!eachExtractedNum.matches(NUMBER_VALID_PATTERN)){
                 throw new IllegalArgumentException(ErrorMessage.IS_NOT_NUMBER.getMessage());
+            }
+
+            // 사용자가 입력한 숫자가 음수인 경우 예외 처리
+            if(eachExtractedNum.contains("-")){
+                throw new IllegalArgumentException(ErrorMessage.IS_NOT_POSITIVE.getMessage());
             }
         }
     }
