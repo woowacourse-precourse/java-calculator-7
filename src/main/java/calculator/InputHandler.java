f2884bf61fb6e9c278728b74e6f26520327cbbbf
Jihyun3478 <leejihyun3478@gmail.com>
2024-10-21 18:28:53 +0900

    refactor(InputHandler) : InputHandler 클래스에 입력 기능 분리

---
+++ src/main/java/calculator/InputHandler.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+	public String getInput() {
+		System.out.println("덧셈할 문자열을 입력해 주세요.");
+		String input = Console.readLine();
+		validateIsInputEmpty(input);
+
+		return input;
+	}
+
+	private void validateIsInputEmpty(String input) {
+		if (input == null || input.isEmpty()) {
+			throw new IllegalArgumentException("올바르지 않은 입력값 입니다.");
+		}
+	}
+}
