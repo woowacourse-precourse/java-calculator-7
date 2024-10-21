02dad11956cf7f062e121fc98c2320bce65c0bca
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-10-21 10:42:47 +0900

    refactor : 콘솔로 테스트한 코드 삭제

--- src/main/java/calculator/service/StringParser.java
+++ src/main/java/calculator/service/StringParser.java
@@ -17,7 +17,6 @@ public class StringParser {
         Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
         String customDelimiter= "";
         List<String> result = new ArrayList<>();
-        System.out.println(matcher.matches());
 
         // 추출한 커스텀 구분자와 숫자부분을 리스트에 담음
         if (matcher.matches()){
@@ -34,6 +33,10 @@ public class StringParser {
     //문자 나누기기
     public List<String> splitNumber(String input){
         String delimiter = DEFAULT_DELIMITER;
+
+        InputValidator validator = new InputValidator();
+        validator.validate(input);
+
         //커스텀 구분자가 존재하면  커스텀 구분자 파싱 실행
         if (input.startsWith("//")){
 
@@ -42,7 +45,6 @@ public class StringParser {
             delimiter += customDelimiterParse.get(0);
             input = customDelimiterParse.get(1);
         }
-
         String[] tokens = input.split(delimiter);
 
         List <String> result =new ArrayList<>();
