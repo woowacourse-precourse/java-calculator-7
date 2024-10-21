bfd4d0c6e2848c20ee6d942af7bd9c322d958e3a
liebigbear <jtw10155@naver.com>
2024-10-21 19:30:56 +0900

    feat: 예외처리 : 잘못된 값을 입력할 경우 IllegalArgumentException 발생

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -19,8 +19,21 @@ public class Calculator {
     private long performAddition(String[] numbers) {
         long result = 0;
         for (String number : numbers) {
+            if(!isPositive(number)) {
+                throw new IllegalArgumentException();
+            }
             result += Integer.parseInt(number);
         }
         return result;
+    }
+    private boolean isPositive(String number) {
+        if(number == null) {
+            return false;
+        }
+        try {
+            return Integer.parseInt(number) >= 0;
+        } catch(NumberFormatException e) {
+            return false;
+        }
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,44 +8,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-
     @Test
-    void 기본_구분자_사용_1() {
+    void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
-           run("");
-           assertThat(output()).contains("0");
-        });
-    }
-
-    @Test
-    void 기본_구분자_사용_2() {
-        assertSimpleTest(() -> {
-            run("1,2:3");
-            assertThat(output()).contains("6");
-        });
-    }
-
-    @Test
-    void 커스텀_구분자_사용_1() {
-        assertSimpleTest(() -> {
-            run("//;\\n1;2;3");
-            assertThat(output()).contains("6");
-        });
-    }
-
-    @Test
-    void 커스텀_구분자_사용_2() {
-        assertSimpleTest(() -> {
-            run("//tapo\\n1tapo2tapo3:4,5");
-            assertThat(output()).contains("15");
+            run("//;\\n1");
+            assertThat(output()).contains("결과 : 1");
         });
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
