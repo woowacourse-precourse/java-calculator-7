0123b73d789cc2650f6e871bbcd4ecb36c7daa53
waffle <nate.crema@gmail.com>
2024-10-21 16:43:04 +0900

    fix: 입력조건 판별대응 - n자리 수 미대응문제 수정

--- src/main/java/calculator/validation/IOValidation.java
+++ src/main/java/calculator/validation/IOValidation.java
@@ -1,7 +1,7 @@
 package calculator.validation;
 
 public class IOValidation {
-    private static String inputFormatCheck = "((\\d.){0,}(\\d)|^$)";
+    private static String inputFormatCheck = "((\\d){1,}.{0,}|^$)";
 
     public static void checkInput( String inputStr ) {
         if ( !inputStr.matches( inputFormatCheck ) )
