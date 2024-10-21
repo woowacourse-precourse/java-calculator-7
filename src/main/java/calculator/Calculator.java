04bc96406771a62aae8e026c63d51ab46ca6b69e
gibumgo <gibumgo@gmail.com>
2024-10-21 09:41:49 +0900

    feat : 숫자 더하기 기능 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,29 @@
+package calculator;
+
+import java.util.Arrays;
+
+public class Calculator {
+    private static final int ZERO = 0;
+    private static final String DELIMITER = ",";
+
+    public static int calculate(String input) {
+        if (isBlank(input)) {
+            return ZERO;
+        }
+        return sum(input.split(DELIMITER));
+    }
+
+    private static boolean isBlank(String input) {
+        return input == null || input.isBlank();
+    }
+
+    private static int sum(String[] values) {
+        return Arrays.stream(values)
+                .mapToInt(Calculator::toInt)
+                .sum();
+    }
+
+    private static int toInt(String input) {
+        return Integer.parseInt(input);
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class CalculatorTest {
+    @ParameterizedTest
+    @DisplayName("빈 문자열 또는 null 값 입력시 0 출력 테스트")
+    @NullAndEmptySource
+    void null_또는_빈문자(String text) {
+        int result = Calculator.calculate(text);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환 테스트")
+    void 숫자_하나_입력_테스트() {
+        int result = Calculator.calculate("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("숫자 합 구하는 기능 테스트")
+    void 더하기_기능_테스트() {
+        int result = Calculator.calculate("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+}
