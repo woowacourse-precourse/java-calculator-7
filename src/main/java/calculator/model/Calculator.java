e475735b8f51a2a61bc85e449f36c0a8601c8e83
Si Yeong Park <psy915@swu.ac.kr>
2024-10-21 22:38:54 +0900

    refactor(Calculator): 음수 입력 검증 로직을 InputValidator로 분리

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -4,6 +4,8 @@ public class Calculator {
     public static int calculate(String input) {
         String[] numbers = StringDelimiter.parse(input);
 
+        InputValidator.validateNegativeNumbers(Arrays.asList(numbers));
+
         int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
             sum += Integer.parseInt(numbers[i]);
