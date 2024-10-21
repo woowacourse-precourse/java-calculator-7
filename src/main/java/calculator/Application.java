ec44ee524f070008487978c34e09e4a97eea8276
hyunseon <dbsgustjs730@naver.com>
2024-10-21 20:49:32 +0900

    1st precourse 결과값 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         int sum = 0;
@@ -14,7 +15,7 @@ public class Application {
                 sum += Integer.parseInt(String.valueOf(str.charAt(i)));
             }
         }
-        System.out.println(sum);
+        System.out.println("결과 : " + sum);
     }
     public static boolean isNumber(char str){
         try{
