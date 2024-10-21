10c4f5ca04a64c7692498771b3fab0be57764853
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-21 11:13:55 +0900

    fix(inputNumber): 기본 구분자를 입력하였을 때 생기는 오류 수정
    커스텀 구분자 형식일때만 동작하는 검증 메서드를 만들어서 오류를 수정하였음

--- src/main/java/calculator/domain/InputNumber.java
+++ src/main/java/calculator/domain/InputNumber.java
@@ -5,6 +5,7 @@ public class InputNumber {
     private static final String EXCEPTION_EMPTY_STRING = "빈 값을 입력하셨습니다.";
     private static final String EXCEPTION_CUSTOM_STRING = "올바른 형식으로 입력하세요.";
     private static final String CUSTOM_NUMBER_PART_DELIMITER = "\\";
+    private static final String FIRST_CUSTOM_STRING = "//";
 
     private final String inputStrings;
 
@@ -26,6 +27,12 @@ public class InputNumber {
     }
 
     private void validateCustomStrings(String inputStrings) {
+        if (inputStrings.startsWith(FIRST_CUSTOM_STRING)) {
+            validateCustomDelimiter(inputStrings);
+        }
+    }
+
+    private void validateCustomDelimiter(String inputStrings) {
         if (!inputStrings.contains(CUSTOM_NUMBER_PART_DELIMITER)) {
             throw new IllegalArgumentException(EXCEPTION_CUSTOM_STRING);
         }
