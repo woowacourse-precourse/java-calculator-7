a8260cbffc8177072f7cfb2d6d0ba6183aef42fe
nsb329 <nsb329@naver.com>
2024-10-21 21:10:08 +0900

    덧셈 계산기 메서드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,4 +5,18 @@ public class Application {
        System.out.println("덧셈할 문자열을 입력하세요.");
        String input = Console.readLine();
     }
+    public static int calc(String input){
+          if (input == null || input.isEmpty()) {
+               return 0;
+          }
+       input=input.replaceAll(":",",");
+       String arr[]=input.split(",");
+       int sum=0;
+       for(int i=0; i<arr.length; i++){
+          if(Integer.parseInt(arr[i])<0) {
+          }
+          sum+=Integer.parseInt(arr[i]);
+       }
+       return sum;
+    }
 }
