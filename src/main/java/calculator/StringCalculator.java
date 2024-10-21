6772a21acd15ceb809bd0f15ebee35107cb0614d
leesh <leesh7807@gmail.com>
2024-10-21 18:32:33 +0900

    feat: add method making string array divided by delimiter

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,4 +1,16 @@
 package calculator;
 
 public class StringCalculator {
+    private final DelimiterParser delimiterParser = new DelimiterParser();
+
+    private String[] makeStringToExpression(String input) {
+        DelimiterContentPair parsed = delimiterParser.parseInput(input);
+        return makeStrArray(parsed);
+    }
+
+    private String[] makeStrArray(DelimiterContentPair parsedInput) {
+        String delimiter = parsedInput.getDelimiter();
+        String content = parsedInput.getContent();
+        return content.split(delimiter);
+    }
 }
