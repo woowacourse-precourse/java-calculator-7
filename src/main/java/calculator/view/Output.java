6fed8ed4c47403cc3dba23aff526fe8ebaec792e
Chan531 <chan657312@gmail.com>
2024-10-21 16:11:05 +0900

    chore(Output): 상수명 변경

--- src/main/java/calculator/view/Output.java
+++ src/main/java/calculator/view/Output.java
@@ -3,13 +3,13 @@ package calculator.view;
 public class Output {
 
 	private static final String INITIAL_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
-	private static final String RESULT = "결과 : ";
+	private static final String RESULT_MESSAGE = "결과 : ";
 
 	public void printInitialMessage() {
 		System.out.println(INITIAL_MESSAGE);
 	}
 
 	public void printAnswerMessage(int answer) {
-		System.out.println(RESULT + answer);
+		System.out.println(RESULT_MESSAGE + answer);
 	}
 }
