d43896fe89e85ab5a24efc43d6748993167e35be
sodychoe <sodychoe@icloud.com>
2024-10-21 21:48:35 +0900

    test : 소숫점 출력 테스트

--- src/test/java/calculator/io/OutputConsoleHandlerTest.java
+++ src/test/java/calculator/io/OutputConsoleHandlerTest.java
@@ -28,4 +28,23 @@ class OutputConsoleHandlerTest {
 
     }
 
+    @Test
+    @DisplayName("소숫점 결과를 반환한다")
+    void showDoubleResult() {
+        // given
+        OutputConsoleHandler outputConsoleHandler = new OutputConsoleHandler();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        // when
+        double result = 3.52;
+
+        // then
+        outputConsoleHandler.showCalculatedResult(result);
+        assertThat(byteArrayOutputStream.toString()).contains("결과 : 3.52");
+
+        System.setOut(System.out);
+
+    }
+
 }
