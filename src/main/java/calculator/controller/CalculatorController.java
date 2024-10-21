1395f3a14961c49dbb1fbf65374f8e41e47e7778
jimnyy <dojimny1004@gmail.com>
2024-10-21 23:58:44 +0900

    feat: 결과 제출

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,19 @@
 package calculator.controller;
 
+import calculator.calculator.Calculator;
+import calculator.input.InputView;
+import calculator.output.OutputView;
+
 public class CalculatorController {
+    private final Calculator calculator = new Calculator();
+
+    public void run() {
+        String input = InputView.getInput();
+        try {
+            int result = calculator.add(input);
+            OutputView.printResult(result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/main/java/calculator/input/InputView.java
+++ src/main/java/calculator/input/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String getInput() {
+    public static String getInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
