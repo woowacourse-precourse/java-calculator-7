aeef526cc64e3cf0b94192ec8ff310d3c2a5e40d
davemins <davemins4125@naver.com>
2024-10-21 10:13:39 +0900

    feat(Controller): 구분자를 추출하는 함수 호출

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,10 +1,19 @@
 package calculator.controller;
 
+import calculator.domain.DelimiterExtractor;
 import calculator.view.View;
 
 public class CalculatorController {
+    private final DelimiterExtractor delimiterExtractor;
+
+
+    public CalculatorController() {
+        this.delimiterExtractor = new DelimiterExtractor();
+    }
 
     public void run() {
         String input = View.requestString();
+
+        String delimiters = delimiterExtractor.extract(input);
     }
 }
