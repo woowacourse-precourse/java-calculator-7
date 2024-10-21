15ac5b0922874c8ccb7b838cdf4d3e9883dc7baf
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-10-21 15:26:04 +0900

    feat : 3.기본 구분자(쉼표,콜론)로 문자열 분리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,6 +8,11 @@ public class Calculator {
     public int cal(String str) {
         // 2. 빈 문자열 입력 처리
         if (str.isEmpty()) return 0;
+
+        // 3. 기본 구분자(쉼표, 콜론)로 문자열 분리
+        String delimiter = ",|:";
+        String input = str;
+        String[] numbers = input.split(delimiter);
     }
 
 }
