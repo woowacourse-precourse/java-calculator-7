c8bbede6c13fe876fb0334c298513a758d49b605
unknown <saree98@naver.com>
2024-10-21 15:34:11 +0900

    feat(ConsoleInputHandler): 입력 처리 클래스 구현
    ConsoleInputHandler에 사용자 입력을 받는 기능 추가
    - 사용자 입력을 받음
    - 검증기를 이용해 입력을 검증

--- src/main/java/calculator/ConsoleInputHandler.java
+++ src/main/java/calculator/ConsoleInputHandler.java
@@ -3,7 +3,19 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class ConsoleInputHandler {
-    public String getUserNumberInput(){
-        return Console.readLine();
+
+    public static String getUserInput() throws IllegalArgumentException{
+
+        String userInput = Console.readLine();
+
+        if(InputValidator.validate(userInput)){
+
+            return userInput;
+
+        }
+
+        throw new IllegalArgumentException();
     }
+
+
 }
