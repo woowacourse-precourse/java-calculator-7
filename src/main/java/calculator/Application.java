1b6f99acaabb8953e40073681a6f5490f0fadaf9
EunSeo <gmjea023@naver.com>
2024-10-21 15:15:59 +0900

    fix(main): 전체기능 추가
    main에 구현된 기능들 추가함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,34 @@
 package calculator;
 
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 입력
+        String request = UserInterface.getUserInput();
+
+        if (request.trim().isEmpty()) {
+            System.out.println("결과 : 0");
+            return;
+        }
+
+        // 커스텀 구분자와 문자열 분리
+        String customDelimiter = DelimiterExtractor.extractCustomDelimiter(request);
+        String subString = DelimiterExtractor.extractSubString(request, customDelimiter);
+
+        // 구분자로 숫자분리
+        String[] speratedStrings;
+
+        if (!customDelimiter.isEmpty()) {
+            speratedStrings = StringParser.splitString(subString, customDelimiter);
+        } else {
+            speratedStrings = StringParser.splitString(subString);
+        }
+
+
+        // 덧셈, 결과출력
+        int answer = Calculate.add(speratedStrings);
+        System.out.println("결과 : " + answer);
+
     }
+
 }
