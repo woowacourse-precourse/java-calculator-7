d884e35fcfb0bf1c5be6e378adc37f98c44b1ca9
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-21 14:24:40 +0900

    refactor(Application): main에서 객체를 생성하고 Controller에 인자로 전달하여 동작하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,19 +1,20 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.model.StringCalculator;
+import calculator.validator.StringCalculatorValidator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        StringCalculatorValidator validator = new StringCalculatorValidator();
+        StringCalculator model = new StringCalculator();
+        OutputView outputView = new OutputView();
 
-        CalculatorController calculatorController = new CalculatorController();
-
-        // 입력 받기
-        calculatorController.displayInputView();
-        calculatorController.getUserInput();
-
-        // 계산 하기
-        calculatorController.calculate();
-
-        // 출력하기
-        calculatorController.displayOutputView();
+        CalculatorController controller = new CalculatorController(inputView, validator, model, outputView);
+        controller.run();
     }
 }
