2832681d7290f17b46d1355bced833693178916f
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-10-21 17:14:17 +0900

    feat(Calculator): 커스텀 구분자 가능 추가
    // \n 사이에 있는 문자를 구문자로 추가.
    길이가 2 이상인 구분자는, 1개씩 짤라서 1글자 구분자로 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,22 +5,53 @@ import java.util.Set;
 
 public class Calculator {
 
-    private Set<String> separateSet = new HashSet<>();
+    private Set<Character> separators = new HashSet<>();
 
-    public Calculator(){
-        separateSet.add(",");
-        separateSet.add(":");
+    public  void addSeparator(String str, int start, int end) {
+        for(int i = start+2; i < end; i++) {
+            separators.add(str.charAt(i));
+        }
     }
 
-    public int sumNumbers(String numbers){
-        String[] splitNumbers = numbers.split(separateSet.toString());
-
-        int sum = 0;
-        for (String num : splitNumbers) {
-            sum += Integer.parseInt(num);
+    public int calculateSum(String str) {
+        int result = 0;
+        String numberBuffer = "";
+        for(int i = 0; i < str.length(); i++) {
+            char charValue = str.charAt(i);
+            if(separators.contains(charValue)) {
+                if(numberBuffer.equals(""))
+                    return -1;
+                result += Integer.parseInt(numberBuffer);
+                numberBuffer = "";
+            }
+            else if(Character.isDigit(charValue)) {
+                numberBuffer += charValue;
+            }
+            else {
+                return -1;
+            }
         }
+        result+= Integer.parseInt(numberBuffer);
+        return result;
+    }
 
-        return sum;
+    public int sumNumbers(String str){
+        separators.add(':');
+        separators.add(',');
+        int result = 0;
+        if(!Character.isDigit(str.charAt(0))){
+            int idx1 = str.indexOf("//");
+            int idx2 = str.indexOf("\\n");
+            if(idx1 == -1|| idx2 == -1 ||idx1 > 0|| idx2+2 == str.length())
+                throw new IllegalArgumentException("Error");
+
+            else {
+                addSeparator(str.substring(idx1, idx2+2),idx1, idx2);
+                str = str.substring(idx2+2, str.length());
+
+            }
+        }
+        return calculateSum(str);
     }
 }
 
