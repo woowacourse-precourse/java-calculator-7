3701fdb8e144f112338aced2dd86fd4203768b3d
문준일 <answnsdlfwkd@naver.com>
2024-10-21 23:13:58 +0900

    fix: 커스텀 구분자를 처리하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,8 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
@@ -11,11 +13,34 @@ public class Application {
             throw new IllegalArgumentException("입력 문자열이 비어 있습니다.");
         }
 
-        String delimiter = "[,:]";  // 쉼표 또는 콜론
+        String delimiter = "[,:]";  // 기본 구분자
+
+        if (str.startsWith("//")) {
+            int delimiterIndex1 = str.indexOf('\\');
+            int delimiterIndex2 = str.indexOf('n');
+            if (delimiterIndex1 == -1 || delimiterIndex2 == -1) {
+                throw new IllegalArgumentException("구분자가 잘못 입력되었습니다.");
+            }
+            try {
+                if (str.charAt(3) == '\\' && str.charAt(4) == 'n') {
+                    String delimiterCustom = str.substring(2, delimiterIndex1);
+                    if (delimiterCustom.length() > 1) {
+                        throw new IllegalArgumentException("구분자는 한 글자여야 합니다.");
+                    }
+                    if (Character.isDigit(delimiterCustom.charAt(0))) {
+                        throw new IllegalArgumentException("구분자로 숫자가 입력되었습니다.");
+                    }
+                    delimiter = Pattern.quote(delimiterCustom);
+                }
+                str = str.substring(delimiterIndex2 + 1);  // 커스텀 구분자 이후 문자열
+            } catch (Exception e) {
+                throw new IllegalArgumentException("잘못된 구분자가 입력되었습니다.");
+            }
+        }
 
         String[] strArr = str.split(delimiter);
         int sum = 0;
-        
+
         for (String token : strArr) {
             try {
                 int number = Integer.parseInt(token);
