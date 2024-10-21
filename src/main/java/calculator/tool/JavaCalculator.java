bbc3d51c08ac2e59d324b4ffd7e4725d58881d64
yokxim <yooksi53@gmail.com>
2024-10-21 22:01:37 +0900

    [feat] 기능구현 2,3,4
    쉼표, 콜론이 입력된 문자열을 입력하면 각 숫자를 더한 결과를 반환한다.

--- src/main/java/calculator/tool/JavaCalculator.java
+++ src/main/java/calculator/tool/JavaCalculator.java
@@ -17,5 +17,43 @@ public class JavaCalculator {
         if (input == null || input.isEmpty()) {
             return 0;
         }
+
+        List<Integer> numbers = new ArrayList<>();
+        splitAndAddNumbers(input, numbers);
+
+        return numbers.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    private void splitAndAddNumbers(String input, List<Integer> numbers) {
+        boolean fullyProcessed = false;
+        for (String partition : partitions) {
+            String[] rawData = input.split(partition);
+
+            if (rawData.length > 1) {
+                fullyProcessed = true;
+
+                for (String rawDatum : rawData) {
+                    if (isNumeric(rawDatum)) {
+                        numbers.add(Integer.parseInt(rawDatum));
+                    } else {
+                        splitAndAddNumbers(rawDatum, numbers);
+                    }
+                }
+                break;
+            }
+        }
+
+        if (!fullyProcessed && !isNumeric(input)) {
+            throw new IllegalArgumentException("Inappropriate type");
+        }
+    }
+
+    private boolean isNumeric(String rawDatum) {
+        try {
+            Integer.parseInt(rawDatum);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
