7ae8993871cca5c2e7543e28d3e7bfe3c1ad6ab6
smk412 <whu1477@naver.com>
2024-10-21 20:44:06 +0900

    fix : 기본 구분자 작동 안됨 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,25 +6,20 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
-        String sep="";
-        String str = sc.nextLine();
-        String [] newSep = str.split("");
-        String[] newStr = new String[]{""};
+        String input = sc.nextLine();
+        String result = "";
 
-        for (int i=2; i < newSep.length;i++){
-            if (newSep[i].equals("\\") && newSep[i + 1].equals("n")) {
-                break;
-            }
-            sep += newSep[i];
+        if (input.startsWith("//")) {
+            String sep = input.substring(2, input.indexOf("\\n"));
+            String[] str = input.substring(input.indexOf("\\n") + 2).split("[,:" + sep + "]");
+            result =  String.join(", ", str);
+        } else {
+            String[] str = input.split(",|:");
+            result =  String.join(", ", str);
         }
-        if(!sep.isEmpty()){
-            newSep = str.split("[,:n"+sep+"]");
-            newStr = newSep;
-        }else{
-            newStr = str.split("[,:]");
-        }
-        for (String s : newStr) {
-            System.out.println(s);
-        }
+
+        System.out.println("결과 : " + result);
     }
+
+
 }
