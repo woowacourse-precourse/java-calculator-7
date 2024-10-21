46a92a994304e49b4a1de9e785bc929439846f13
dayeondayeon <dayeon2438@gmail.com>
2024-10-21 18:47:20 +0900

    test : 커스텀 및 기본 구분자를 통해 입력 문자열을 배열로 변환하는 기능 테스트

--- src/test/java/calculator/ConverterTest.java
+++ src/test/java/calculator/ConverterTest.java
@@ -14,4 +14,19 @@ class ConverterTest {
         assert answer.equals(target);
     }
 
+    @Test
+    void testConverter() {
+        String target = "//_ :,: \n1_4:6,5";
+        String delimiter = findCustomDelimiter(target);
+        target = removeCustomDelimiterAndSpace(target, delimiter);
+
+        String[] strings = Converter.convertToArray(target);
+        String[] answer = {"1", "4", "6", "5"};
+
+        assert strings.length == answer.length;
+        for (int i = 0; i < answer.length; i++) {
+            assert answer[i].equals(strings[i]);
+        }
+    }
+
 }
