5c830bcc77842b0a6915047957f6c0c7e4c5ae2e
siwol025 <cyk99115695@gmail.com>
2024-10-21 09:46:14 +0900

    test: 더하기 기능 테스트

--- src/test/java/calculator/domain/SumCalculatorTest.java
+++ src/test/java/calculator/domain/SumCalculatorTest.java
@@ -1,10 +1,9 @@
 package calculator.domain;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class SumCalculatorTest {
@@ -16,6 +15,7 @@ class SumCalculatorTest {
     }
 
     @Test
+    @DisplayName("빈문자를 입력했을 때 0을 반환하는지 테스트")
     void sum_isEmpty() {
         String input = "";
 
@@ -24,4 +24,63 @@ class SumCalculatorTest {
         assertThat(result).isEqualTo(0);
     }
 
+    @Test
+    @DisplayName("숫자를 하나만 입력했을 경우")
+    void sum_One_number() {
+        String input = "1";
+
+        int result = sumCalculator.sum(input);
+
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자를 사용하면서 숫자를 하나만 입력했을 경우")
+    void sumByCustomDelimiter_One_number() {
+        String input = "//;\\n1";
+
+        int result = sumCalculator.sum(input);
+
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("쉼표 구분자 작동 여부 테스트")
+    void sumByDefaultDelimiter_쉼표() {
+        String input = "1,2,3";
+
+        int result = sumCalculator.sum(input);
+
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("세미콜론 구분자 작동 여부 테스트")
+    void sumByDefaultDelimiter_세미콜론() {
+        String input = "1;2;3";
+
+        int result = sumCalculator.sum(input);
+
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("쉼표 구분자와 세미콜론 구분자 동시 사용 테스트")
+    void sumByDefaultDelimiter() {
+        String input = "1,2;3";
+
+        int result = sumCalculator.sum(input);
+
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 테스트")
+    void sumByCustomDelimiter() {
+        String input = "//;\\n1;2;3";
+
+        int result = sumCalculator.sum(input);
+
+        assertThat(result).isEqualTo(6);
+    }
 }
