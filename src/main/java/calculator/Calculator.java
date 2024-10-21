7f80871c078e18b40cf4692f27ecfe540a475fea
LeeJungBum <jungbum05114@gmail.com>
2024-10-21 22:24:26 +0900

    docs(Calculator): 자바독 추가
    StringCalculator 클래스에 자바독을 추가하여 클래스의 기능과 사용법을 명세화 하였습니다

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,19 +3,26 @@ package calculator;
 import java.util.Arrays;
 
 /**
- * class: Calculator.
+ * Calculator 클래스는 숫자 배열을 처리하여 계산 기능을 제공하는 추상 클래스입니다.
+ * <p>
+ * 이 클래스는 하위 클래스에서 상속받아 사용할 수 있으며, 배열에 포함된 숫자를 모두 더하는 기능을 제공합니다.
+ * </p>
  *
  * @author JBumLee
  * @version 2024/10/19
  */
 public abstract class Calculator {
     /**
-     * 배열에 들어오는 숫자를 모두 더하는 add 메소드. 내부적으로는 Stream을 사용한다.
+     * 주어진 int 배열에 포함된 모든 숫자를 더합니다.
      *
-     * @param numbers int범위의 숫자
-     * @return 숫자
+     * <p>
+     * 이 메소드는 내부적으로 Java Stream API를 사용하여 배열의 모든 숫자를 합산합니다.
+     * </p>
+     *
+     * @param numbers 더할 숫자들의 배열 (int 범위)
+     * @return 주어진 숫자들의 합계
      */
-    public long add(int... numbers) {
+    protected long sum(int... numbers) {
         return Arrays.stream(numbers).sum();
     }
 }
