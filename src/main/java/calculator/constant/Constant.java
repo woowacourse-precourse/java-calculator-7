c5f9db68ba15bbc584981fd5f373030b2cbbf2c5
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-10-21 18:07:25 +0900

    feat: 모든 로직에서 사용할 공용 상수 선언
    - 입력값 검증 정규식
    - 커스텀 구분자 등록 prefix, suffix

---
+++ src/main/java/calculator/constant/Constant.java
@@ -0,0 +1,7 @@
+package calculator.constant;
+
+public class Constant {
+    public static final String VALIDATION_REGEX = "//(.)\\\\n(\\d+)(.*?)";
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\\\\n";
+}
