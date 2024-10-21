bf2784184f63c20418fcf32e98f3869741f84f1e
EoN <jaegyun1011@naver.com>
2024-10-21 18:05:12 +0900

    feat : 수식 문자열 parsing 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,21 +6,31 @@ public class Calculator {
     public void run() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         try {
-            input();
-            int result = add();
+            String expr = input();
+            String[] numstr = parse(expr);
+            int result = add(numstr);
             System.out.println("결과 : " + result);
         } catch(IllegalArgumentException e) {
             System.out.println("올바르지 않은 입력입니다.");
         }
     }
 
-    public void input() {
-        String expr = Console.readLine();
-
+    public String input() {
+        return Console.readLine();
     }
 
-    public int add() {
+    public String[] parse(String expr) {
+        String del = ",|:";
+
+        return expr.split(del);
+    }
+
+    public int add(String[] numstr) {
         int sum = 0;
+
+        for(String x : numstr) {
+            sum += 1;
+        }
 
         return sum;
     }
