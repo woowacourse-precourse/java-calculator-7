7a6133d3ad9f4dace6dfb5d799b23e33c0f0590a
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-10-21 19:38:04 +0900

    fix(core): fix main class
    add try catch exception
    - in console.readline() for empty input string

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,10 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String input = Console.readLine();
+        String input;
+        try {
+            input = Console.readLine();
+        } catch (java.util.NoSuchElementException e) {
+            // 빈 문자열을 직접 입력받는 경우
+            System.out.println("결과 : 0");
+            return;
+        }
         InputHandler inputHandler = new InputHandler();
 
         int result = inputHandler.processInput(input);
         System.out.println("결과 : " + result);
+
     }
 }
