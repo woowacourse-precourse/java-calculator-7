69676bc9143f109b69c9baf358db2f13d34e19ab
heeyeop Kim <ssucgoo@gmail.com>
2024-10-21 21:38:29 +0900

    feat: 커스텀 구분자 추출 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,16 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = Console.readLine();
+        String customDelimiter = getCustomDelimiter(input);
+    }
+
+    public static String getCustomDelimiter(String input) {
+        if (input.startsWith("//")) {
+            if (input.contains("\\n")) {
+                return input.substring(input.indexOf("//") + 2, input.indexOf("\\n") - 1);
+            }
+            throw new IllegalArgumentException();
+        }
+        return null;
     }
 }
