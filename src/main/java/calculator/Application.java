24e4855d5e662ad9551b67f0ae19b8fc012dc24b
mingyeongwon <wonmingy@gmail.com>
2024-10-21 19:13:41 +0900

    feat: 쉼표(,) 또는 콜론(:)을 구분자를 기준으로 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        // 문자열 분리
+        String input = Console.readLine();
+        String[] inputNumbers = input.split(",|:");
+        for(String number : inputNumbers) {
+            System.out.print(number + " ");
+        }
     }
 }
