9c6edc7eef560459146d4ec5727456ebbfc6a6d2
hyeonjeong9911 <knk990101@naver.com>
2024-10-21 19:22:16 +0900

    feat: 구분자를 기준으로 숫자를 파싱하는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,9 +2,26 @@ package calculator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = readLine();
+        String sep = ",|;";
+        int sum = 0;
+
+        if (input.startsWith("//")) {
+            sep = input.split("\n")[0].substring(1);
+        }
+
+        String[] strArr = input.split(sep);
+        int[] intArr = new int[strArr.length];  // strArr int 형으로 변환해서 넣을 배열
+        for (String s : strArr) {
+            sum += Integer.parseInt(s);
+        }
+
+        System.out.println("결과 : " + sum);
+
     }
 }
