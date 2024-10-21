9e5eb68755fadb4ae1d540893e0c4b7bba77f9e4
dia <dia19dev@gmail.com>
2024-10-21 16:14:16 +0900

    feat(Application): sum calculation
    Implemented logic to calculate the sum of numbers and added functionality to print the result. Additionally, fixed the exception handling during the input process to ensure better error management.
    
    Closes

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,17 +5,30 @@ public class Application {
         String separator = "[,;]";
         String str;
         String[] operand;
+        int sum = 0;
 
         //문자열 입력받기
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
             str = reader.readLine();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new IllegalArgumentException(e);
         }
 
         //구분자 분리
         operand = str.split(separator);
 
+        //숫자 합 구하기
+        try {
+            for(String s : operand) {
+                if (Integer.parseInt(s) < 0) throw new IllegalArgumentException();
+                else sum += Integer.parseInt(s);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        //결과 출력하기
+        System.out.println("결과: " + sum);
     }
 }
