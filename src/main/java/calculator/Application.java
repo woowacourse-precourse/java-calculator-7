a4c4d494f3f05c2e531331202b9bd86b1e836e85
KIM MINSEOK <mskim.glenn@gmail.com>
2024-10-21 14:29:59 +0900

    feat(Application): 메인 함수에서 입력, 계산, 출력 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,21 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputReader inputReader = new InputReader();
+        StringCalculator calculator = new StringCalculator();
+        OutputPrinter outputPrinter = new OutputPrinter();
+
+        try {
+            // 사용자의 입력을 받는 기능
+            String input = inputReader.promptForInput();
+
+            // 계산하는 기능
+            int result = calculator.calculateSum(input);
+
+            // 결과를 출력하는 기능
+            outputPrinter.displayResult(result);
+        } catch (IllegalArgumentException e) {
+            outputPrinter.displayError(e.getMessage());
+        }
     }
 }
