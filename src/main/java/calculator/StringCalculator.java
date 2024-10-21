1a644befb8cbb45bc641a505d62bba51c6815d0b
hyeblee <hyeb1224@naver.com>
2024-10-21 09:07:36 +0900

    StringCalculator 호출 간소화

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -20,7 +20,7 @@ public class StringCalculator {
     public long addString(String[] numbers){
         long sum=0;
         for(String number:numbers){
-            if(number.isEmpty()) continue;
+            if(number.trim().isEmpty()) continue;
             int num = stringToInt(number);
             if(num < 0)
                 throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
@@ -34,11 +34,10 @@ public class StringCalculator {
             return 0;
 
         String delimiter = ",|:";
-        String specialChars = ".^$*+?()[]{}\\|/";
 
         if(input.startsWith("//")){
-            if(input.charAt(3)=='\\'&&input.charAt(4)=='n'){
-                String customDelimiter = Pattern.quote(String.valueOf(input.charAt(2)));
+            if(input.charAt(3) == '\\' && input.charAt(4) == 'n'){
+                String customDelimiter = Pattern.quote(String.valueOf(String.valueOf(input.charAt(2))));
                 if(input.charAt(2)>='0'&&input.charAt(2)<='9')
                     throw new IllegalArgumentException("숫자는 커스텀 구분자가 될 수 없습니다.");
                 delimiter = delimiter + "|" + customDelimiter;
