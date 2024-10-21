10448823c8f3ea9b589c6bd26418c77e48ca22af
seoys0910 <seoys0910@naver.com>
2024-10-21 18:30:16 +0900

    refactor : 숫자 검증 로직을 하나의 메서드로 통합하여 간소화

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,24 +6,15 @@ public class Calculator {
     public static int numberSum(List<String> numbers) {
         int sum = 0;
         for (String value : numbers) {
-            validateNumber(value);
+            validatePositiveNumber(value);
             sum += Integer.parseInt(value);
         }
         return sum;
     }
 
-    private static void validateNumber(String num) {
-        try {
-            int number = Integer.parseInt(num);
-            validateNegative(number);
-        } catch (NumberFormatException exception) {
-            throw new IllegalArgumentException("잘못된 값이 입력되었습니다.");
-        }
-    }
-
-    private static void validateNegative(int num) {
-        if (num < 0) {
-            throw new IllegalArgumentException("양수를 입력해주세요.");
+    private static void validatePositiveNumber(String num) {
+        if (!num.matches("\\d+")) {
+            throw new IllegalArgumentException("올바른 양수와 구분자를 입력해 주세요.");
         }
     }
 }
