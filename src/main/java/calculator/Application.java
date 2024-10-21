4a3bf1b10c19b825f921856124576cf1654a8386
swhyeon98 <smreo75@gmail.com>
2024-10-21 17:21:24 +0900

    feat: 커스텀 구분자 지원 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,15 +5,31 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Application {
-    private static final String delimiter = "[,:]";
+    private static final String DEFAULT_DELIMITER = "[,:]";
+
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String numbers = Console.readLine();
-        if (numbers.isEmpty()) {
+        String input = Console.readLine();
+        if (input.isEmpty()) {
             System.out.println("결과: 0");
             return;
         }
-        List<String> split = List.of(numbers.split(delimiter));
-        System.out.println("결과: " + split.stream().mapToInt(Integer::parseInt).sum());
+
+        input = input.replace("\\n", "\n");
+        String delimiter = DEFAULT_DELIMITER;
+
+        if (input.startsWith("//")) {
+            int delimiterEndIndex = input.indexOf("\n");
+            delimiter = "[" + input.substring(2, delimiterEndIndex) + "]";
+            input = input.substring(delimiterEndIndex + 1);
+        }
+
+        List<String> numbers = List.of(input.split(delimiter));
+
+        int result = numbers.stream()
+                .mapToInt(Integer::parseInt)
+                .sum();
+
+        System.out.println("결과: " + result);
     }
 }
