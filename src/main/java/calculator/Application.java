941a90f541209623d93c049debcbc6833c938257
JEONGHUNJEON <jjhoon06@gmail.com>
2024-10-21 20:52:57 +0900

    chore: 간단한 주석 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,7 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    // 들어온 인자들의 검증 메서드들을 관장하는 메서드
     public static String[] checkInput (String input, StringBuilder delimiter) {
         String processedInput = extractCustomDelimiter(input, delimiter);
         if (!validateInput(processedInput, delimiter)) {
@@ -11,6 +12,7 @@ public class Application {
         return checkDuplicateDelimiter(processedInput.split(delimiter.toString()));
     }
 
+    // 커스텀 구분를 확인하는 메서드
     public static String extractCustomDelimiter(String input, StringBuilder delimiter) {
         String processedInput = input;
         if (input.startsWith("//")) {
@@ -25,11 +27,13 @@ public class Application {
         return processedInput;
     }
 
+    // 들어온 인자가 맞는 형식을 지켰는지 확인하는 메서드
     public static boolean validateInput(String input, StringBuilder delimiter) {
         String regex = "^[0-9" + delimiter.substring(1) + "+$";
         return input.matches(regex);
     }
 
+    // 구분자가 숫자 구분자 숫자 순서가 아닌 숫자 구분자 구분자 숫자 식으로 들어왔을 때를 걸러주는 메서드
     public static String[] checkDuplicateDelimiter (String[] splitInput) {
         for (String str : splitInput) {
             if (str.equals("")) {
@@ -39,6 +43,7 @@ public class Application {
         return splitInput;
     }
 
+    // 인자들을 더해주는 메서드
     public static int calculateSum(String[] splitInput) {
         int sum = 0;
         for (String input : splitInput) {
