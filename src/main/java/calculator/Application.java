6f92d362d079bcba75eb642b5d21c6405ffc8deb
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-10-21 19:16:58 +0900

    feat : 입력값 체크 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -50,6 +50,12 @@ public class Application {
         return input.contains(SEMICOLON)?SEMICOLON:COMMA;
     }
 
+    public static String inputCheck(String input){
+//        잘 못 된 입력 체크
+        if(!input.contains(SEMICOLON) && !input.contains(COMMA)) throw new IllegalArgumentException(INPUT_VALIDATION_EXCEPTION_MESSAGE);
+        return input;
+    }
+
     }
 
     public static void main(String[] args) {
