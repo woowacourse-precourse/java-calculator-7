29c4e7eeb8dc3ac856f9f37cdfc4de46a0466ee9
kimsehw <seunghwan9703@gmail.com>
2024-10-21 16:23:48 +0900

    test(ApplicationTest): 빈 커스텀 구분자(null) 테스트
    커스텀 구분자로 빈값(null)이 들어올 경우 알맞는 예외가 발생하는지 테스트 합니다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,13 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import calculator.model.Calculator;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,9 +21,16 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void nullInputTest() {
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> Calculator.calculate("//\\n1,2"));
+        Assertions.assertEquals(e.getMessage(), "잘못된 입력입니다. 빈칸(\"\")은 구분자가 될 수 없습니다.");
     }
 
     @Override
