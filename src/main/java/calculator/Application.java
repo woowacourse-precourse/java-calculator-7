6b56480f8ed4407c05ac28b3a6674bf0efc5bb87
Hannana <hancrystal@sookmyung.ac.kr>
2024-10-21 21:00:03 +0900

    feat(split): 사용자 입력 문자열 분리하기 with 기본 구분자

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,15 +5,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        //사용자로부터 문자열 입력 받기
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputStr = Console.readLine();
         int answer = 0;
         if(isNull(inputStr)){
             displayResult(answer);
+        }else{
+            //사용자 입력 문자열 분리하기 with 기본 구분자
+            splitBasic(inputStr);
         }
         Console.close();
     }
-
+    private static String[] splitBasic(String inputStr) {
+        return inputStr.split(",|:");
+    }
     private static void displayResult(int result) {
         System.out.println("결과: " + result);
     }
