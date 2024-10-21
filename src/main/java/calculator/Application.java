393851d02b85a9aa59b64656b5bfb181d310917d
gladhee <gladhee@icloud.com>
2024-10-21 11:25:31 +0900

    feat(Application): 진입점 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorController calculatorController = new CalculatorController();
+
+        calculatorController.run();
     }
 }
