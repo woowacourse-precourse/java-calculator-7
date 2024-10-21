59d93cb42494816cc6fb71d87f25e7b0f04305a0
Timothy <timothy@jungnanum.com>
2024-10-21 21:58:06 +0900

    feat(#2): 입력받은 값을 비교하는 테스트 코드를 작성
    - 스파게티 코드가 되지 않기 위해서 MVC 형태로 구조를 작성한다.
        - 입력받은 값을 비교하는 테스트 코드를 작성한다.
          - 문자열에 기본 구분자들(쉼표(,)와 콜론(:))을 구분자가 포함되어 있는지 Boolean으로 반환하는 메소드 작성

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -20,13 +20,25 @@ public class CalculatorService {
     /**
      * 사용자로부터 입력받은 문자열 연산
      * @param userInput
-     * @return
+     * @return Integer
      */
     public Integer calcFromString(String userInput) {
         if (userInput.isEmpty()) {
             return 0;
         }
+
+
+
         return Integer.parseInt(userInput);
+    }
+
+    /**
+     * 기본 구분자 쉼표(,) 또는 콜론(:)이 사용자 입력 문자열에 있는지 확인하는 메소드
+     * @param userInput
+     * @return Boolean
+     */
+    private static Boolean containsDefaultDelimiters(String userInput) {
+        return userInput.contains(",") || userInput.contains(":");
     }
 
 
