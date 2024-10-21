40be46eafb53c1016c5e02b0c1b5957cbbd24f9e
이태구 <alexlee9841@gmail.com>
2024-10-21 22:58:12 +0900

    sumFromString

---
+++ sumFromString.java
@@ -0,0 +1,7 @@
+import camp.nextstep.edu.missionutils.Console;
+
+public static int sumFromString(String input) {
+    if (input == null || input.isEmpty()) {
+        return 0;  // 입력이 비었을 경우 0 반환
+    }
+}
