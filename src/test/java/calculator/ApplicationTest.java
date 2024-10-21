f27085c23cb500b30426df8114c225be4dd64800
songsunkook <songsunkook@gmail.com>
2024-10-21 21:18:50 +0900

    docs: 테스트 주석 추가 및 줄바꿈

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -28,16 +28,22 @@ class ApplicationTest extends NsTest {
     @Test
     void 정상_케이스() {
         assertSimpleTest(() -> {
+            // 일반적인 입력
             run("1,2,3");
             assertThat(output()).contains("결과 : 6");
+
             // 구분자가 연속으로 주어지는 경우
             run("1::2::3");
             assertThat(output()).contains("결과 : 6");
+
+            // 숫자 없이 구분자만 연속으로 주어지는 경우
             run(",,,,");
             assertThat(output()).contains("결과 : 0");
+
             // 커스텀 구분자와 기본 구분자를 혼용하는 경우
             run("//;\\n1,2:3;4");
             assertThat(output()).contains("결과 : 10");
+
             // 숫자를 커스텀 구분자로 사용하는 경우
             run("//1\\n21212");
             assertThat(output()).contains("결과 : 6");
@@ -50,6 +56,7 @@ class ApplicationTest extends NsTest {
                 // 커스텀 구분자가 2글자 이상인 경우
                 assertThatThrownBy(() -> runException("//;;\\n1;2;3"))
                     .isInstanceOf(IllegalArgumentException.class);
+
                 // 숫자로 0이 들어오는 경우
                 assertThatThrownBy(() -> runException("0,1,2"))
                     .isInstanceOf(IllegalArgumentException.class);
