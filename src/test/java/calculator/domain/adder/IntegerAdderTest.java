26def2ae32aa8b664ae1729db7a04175ddc27768
Mint <audwl03071@gmail.com>
2024-10-21 14:55:00 +0900

    test: Integer 타입 정수 테스트일 경우 정수 타입 명시

--- src/test/java/calculator/domain/adder/IntegerAdderTest.java
+++ src/test/java/calculator/domain/adder/IntegerAdderTest.java
@@ -11,12 +11,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-@DisplayName("정수 가산기 테스트")
+@DisplayName("Integer 타입 정수 가산기 테스트")
 class IntegerAdderTest {
 
     @Test
-    @DisplayName("정수들을 더한다")
-    void 성공_정수가산_오버플로우X() {
+    @DisplayName("Integer 타입 정수들을 더한다")
+    void 성공_Integer타입정수가산_오버플로우X() {
         // Given
         List<Integer> numbers = List.of(1, 2, 3);
         IntegerAdder integerAdder = new IntegerAdder();
@@ -30,8 +30,8 @@ class IntegerAdderTest {
 
     @ParameterizedTest
     @MethodSource
-    @DisplayName("정수들을 더할 때, 오버플로우가 발생할 경우 예외가 발생한다")
-    void 성공_정수가산_오버플로우O(List<Integer> numbers) {
+    @DisplayName("Integer 타입 정수들을 더할 때, 오버플로우가 발생할 경우 예외가 발생한다")
+    void 성공_Integer타입정수가산_오버플로우O(List<Integer> numbers) {
         // Given
         IntegerAdder integerAdder = new IntegerAdder();
 
@@ -41,7 +41,7 @@ class IntegerAdderTest {
                 .hasMessage("오버플로우가 발생했습니다.");
     }
 
-    private static Stream<Arguments> 성공_정수가산_오버플로우O() {
+    private static Stream<Arguments> 성공_Integer타입정수가산_오버플로우O() {
         return Stream.of(
                 Arguments.of(List.of((int) (Math.pow(2, 31) - 1), 2, 3)),
                 Arguments.of(List.of((int) -Math.pow(2, 31), -2, -3))
--- src/test/java/calculator/util/converter/IntegerConvertibleTest.java
+++ src/test/java/calculator/util/converter/IntegerConvertibleTest.java
@@ -7,12 +7,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("정수 변환기 테스트")
+@DisplayName("Integer 타입 정수 변환기 테스트")
 class IntegerConvertibleTest {
 
     @Test
     @DisplayName("유효한 수를 포함하는 문자열 리스트가 주어질 때, 정수 리스트로 변환한다")
-    void 성공_정수리스트변환_유효한수() {
+    void 성공_Integer타입정수리스트변환_유효한수() {
         // Given
         List<String> splitInput = List.of("1", "2", "3");
         IntegerConvertible integerConvertible = new IntegerConvertible();
@@ -25,8 +25,8 @@ class IntegerConvertibleTest {
     }
 
     @Test
-    @DisplayName("정수 범위에 벗어나는 수를 포함하는 문자열 리스트가 주어질 때, 예외가 발생한다")
-    void 성공_정수리스트변환_범위에벗어나는수() {
+    @DisplayName("Integer 타입 정수 범위에 벗어나는 수를 포함하는 문자열 리스트가 주어질 때, 예외가 발생한다")
+    void 성공_Integer타입정수리스트변환_범위에벗어나는수() {
         // Given
         List<String> splitInput = List.of((long) Math.pow(2, 32) + ",2,3");
         IntegerConvertible integerConvertible = new IntegerConvertible();
@@ -38,8 +38,8 @@ class IntegerConvertibleTest {
     }
 
     @Test
-    @DisplayName("정수로 변환할 수 없는 문자를 포함하는 문자열 리스트가 주어질 때, 예외가 발생한다")
-    void 성공_정수리스트변환_변환불가문자() {
+    @DisplayName("Integer 타입 정수로 변환할 수 없는 문자를 포함하는 문자열 리스트가 주어질 때, 예외가 발생한다")
+    void 성공_Integer타입정수리스트변환_변환불가문자() {
         // Given
         List<String> splitInput = List.of("a", "2", "3");
         IntegerConvertible integerConvertible = new IntegerConvertible();
@@ -52,7 +52,7 @@ class IntegerConvertibleTest {
 
     @Test
     @DisplayName("소수점을 포함하는 문자열 리스트가 주어질 때, 예외가 발생한다")
-    void 성공_정수리스트변환_소수점() {
+    void 성공_Integer타입정수리스트변환_소수점() {
         // Given
         List<String> splitInput = List.of("1.5", "2", "3");
         IntegerConvertible integerConvertible = new IntegerConvertible();
@@ -65,7 +65,7 @@ class IntegerConvertibleTest {
 
     @Test
     @DisplayName("음수를 포함하는 문자열 리스트가 주어질 때, 예외가 발생한다")
-    void 성공_정수리스트변환_음수() {
+    void 성공_Integer타입정수리스트변환_음수() {
         // Given
         List<String> splitInput = List.of("-1", "2", "3");
         IntegerConvertible integerConvertible = new IntegerConvertible();
