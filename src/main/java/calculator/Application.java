b6c96e5455f8074a7367bfb12dbe3f6c7828e19e
백승우 <swthewhite@gmail.com>
2024-10-21 23:22:07 +0900

    feat: 커스텀 구분자 파싱 및 처리 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,12 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class Application {
     private static final String DEFAULT_DELIMITERS = ",|:";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_POSTFIX = "\n";
 
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
@@ -13,8 +16,30 @@ public class Application {
             throw new IllegalArgumentException("빈 값은 허용되지 않습니다.");
         }
 
-        int result = sumOfNumbers(input, DEFAULT_DELIMITERS);
-        System.out.println("결과 : " + result);
+        input = input.replace("\\n", "\n");
+
+        String[] result = processCustomDelimiter(input);
+        String delimiter = result[0];
+        String numbers = result[1];
+
+        int sum = sumOfNumbers(numbers, delimiter);
+        System.out.println("결과 : " + sum);
+    }
+
+    private static String[] processCustomDelimiter(String input) {
+        String delimiter = DEFAULT_DELIMITERS;
+        String numbers = input;
+
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            int delimiterIndex = input.indexOf(CUSTOM_DELIMITER_POSTFIX);
+            if (delimiterIndex == -1) {
+                throw new IllegalArgumentException("잘못된 형식입니다.");
+            }
+            delimiter = Pattern.quote(input.substring(2, delimiterIndex));
+            numbers = input.substring(delimiterIndex + 1);
+        }
+
+        return new String[] { delimiter, numbers };
     }
 
     private static int sumOfNumbers(String numbers, String delimiter) {
@@ -26,5 +51,6 @@ public class Application {
         return sum;
     }
 }
+
 
 
