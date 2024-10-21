af8b6ffdd5699a6a16a3839481e15e3dc49e9dc5
pjy2163 <pjy2163@naver.com>
2024-10-21 16:58:57 +0900

    refactor: CalculatorLogic.java에서 예외처리 부분을 클래스로 분리

--- src/main/java/calculator/CalculatorLogic.java
+++ src/main/java/calculator/CalculatorLogic.java
@@ -3,17 +3,12 @@ import java.util.List;
 
 public class CalculatorLogic {
     public static int calculateSum(List<String> numbers) {
-        if (numbers.isEmpty()) {
-            throw new IllegalArgumentException("빈 문자열은 허용되지 않습니다.");
-        }
+        ExceptionLogic.validateInput(numbers);
 
         int sum = 0;
         for (String number : numbers) {
             if (!number.trim().isEmpty()) {
                 int num = Integer.parseInt(number.trim());
-                if (num < 0) {
-                    throw new IllegalArgumentException("음수는 허용되지 않습니다.");
-                }
                 sum += num;
             }
         }
