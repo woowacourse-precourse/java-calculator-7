510155703471b7a52a6568bd189510ce4dcfb6b2
zeus <jj99526@naver.com>
2024-10-21 22:16:33 +0900

    feat : 입력받기 메소드 및 구분자 확인 메소드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,14 +1,43 @@
 package calculator;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Application {
+    public String getInput(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = scanner.nextLine();
 
-    public static void main(String[] args) {
+        return input;
+    }
+
+    public Set<String> separatorCheck(String input){
         Set<String> separator=new HashSet<>();
         separator.add(",");
         separator.add(":");
+        if (input.startsWith("//")){
+            int delimiterIndex = input.indexOf("\\n");
+            String customDelimiter = input.substring(2, delimiterIndex);
+            separator.add(customDelimiter);
+        }
+        return separator;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Application application=new Application();
+        String input= application.getInput();
+        Set<String>separator=application.separatorCheck(input);
+//        int result=application.sum(input,separator);
+
+
+
+
+
 
 
         // TODO: 프로그램 구현
