741badbb0df5a2b1493d7f3c4b257ed75e8a0830
이하진 <glgkwls11@labradorlabs.ai>
2024-10-21 18:08:08 +0900

    feat: 커스텀 구분자 추출 함수

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,27 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private static String CUSTOM_SEPARATOR_FRONT = "//";
+    private static String CUSTOM_SEPARATOR_END = "\\n";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        String customSeparator = getCustomSeparator(input);
+    }
+
+
+    private static String getCustomSeparator(String input) {
+        if (Character.isDigit(input.toCharArray()[0])) {
+            return null;
+        }
+
+        if (!input.startsWith(CUSTOM_SEPARATOR_FRONT) || !input.contains(CUSTOM_SEPARATOR_END)) {
+            throw new IllegalArgumentException();
+        }
+
+        return input.split(CUSTOM_SEPARATOR_END)[0];
     }
 }
