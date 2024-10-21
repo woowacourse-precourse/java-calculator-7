88b50aa1cbb9ad751335e4e323a2fb63c0ada5bd
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-10-21 21:50:16 +0900

    fix : 구분자로 숫자가 들어오는 경우 예외 처리

--- src/main/java/calculator/separator/DelimiterStorage.java
+++ src/main/java/calculator/separator/DelimiterStorage.java
@@ -6,6 +6,11 @@ import java.util.List;
 public class DelimiterStorage {
     private static final List<String> delimiters = new ArrayList<>();
     public void addDelimiter(String delimiter){
+
+        if(delimiter.matches("\\d")){
+            throw new IllegalArgumentException("숫자는 구분자가 될 수 없다");
+        }
+
         delimiters.add(delimiter);
     }
     public List<String> getDelimiters() {
