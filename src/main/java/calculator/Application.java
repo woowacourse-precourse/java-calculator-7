d42804ba3940a6319d4b453b2d5a947e3caf1f24
이하진 <glgkwls11@labradorlabs.ai>
2024-10-21 18:15:30 +0900

    feat : 구분자 목록 추출 함수

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,14 +2,19 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     private static String CUSTOM_SEPARATOR_FRONT = "//";
     private static String CUSTOM_SEPARATOR_END = "\\n";
+    private static List<String> DEFAULT_SEPARATORS = List.of(",", ":");
 
     public static void main(String[] args) {
         String input = Console.readLine();
         String customSeparator = getCustomSeparator(input);
+        List<String> separators = getSeparators(customSeparator);
     }
 
 
@@ -23,5 +28,16 @@ public class Application {
         }
 
         return input.split(CUSTOM_SEPARATOR_END)[0];
+    }
+
+    private static List<String> getSeparators(String customSeparator) {
+        List<String> separators = new ArrayList<>();
+
+        if (customSeparator != null) {
+            separators.addAll(DEFAULT_SEPARATORS);
+            separators.add(customSeparator);
+        }
+
+        return separators;
     }
 }
