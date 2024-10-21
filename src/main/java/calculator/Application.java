1d2c7ffec3d18065f59d19f33cd99a7580cfd333
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-10-21 20:18:01 +0900

    Feat: 문자열에서 양수를 추출하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Application {
 
@@ -12,15 +15,15 @@ public class Application {
         return sentence.isBlank() || sentence == null;
     }
 
-    public static String extractLetter(String str){
+    public static boolean isCustomLetter(String str){
         char firstLetter = str.charAt(0);
         //:와 ,로 구분되는 경우
         if(firstLetter == ':' || firstLetter == ',' || Character.isDigit(firstLetter)){
-            return ":|,";
+            return false;
         }
         //커스텀 구분자를 쓰는 경우
         else if(firstLetter == '/') {
-            return extractCustomLetter(str);
+            return true;
         }
         //잘못된 입력
         throw new IllegalArgumentException("[extractLetter] 잘못된 입력");
@@ -34,13 +37,30 @@ public class Application {
         throw new IllegalArgumentException("[extractCustomLetter] 잘못된 입력");
     }
 
+    public static List<Integer> extractNum(String str){
+        String letter = ":|,";
+        String numStr = str;
+        List<Integer> nums = new ArrayList<>();
+
+        if(isCustomLetter(str)){
+            letter = extractCustomLetter(str);
+            numStr = Console.readLine();
+        }
+        StringTokenizer st = new StringTokenizer(numStr, letter);
+
+        while(st.hasMoreTokens()){
+            nums.add(Integer.parseInt(st.nextToken()));
+        }
+
+        return nums;
+    }
+
     public static void main(String[] args) {
         String str = Console.readLine();
         int result = 0;
-
+        List<Integer> nums;
         if(!isBlankOrNull(str)){
-            String letter = extractLetter(str);
-            System.out.println(letter);
+            nums = extractNum(str);
         }
 
 
