565da0a423fd2dd488a17aa5039d89fe31a02dc7
KIMB0B <axzsd96301@gmail.com>
2024-10-21 16:38:10 +0900

    feat(Application): 커스텀 구분자 지정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,9 +2,19 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        ArrayList<String> sep = new ArrayList<>(Arrays.asList(",", ":"));
+
+        if (input.startsWith("//")) {
+            int customSepIndexEnd = input.indexOf("\n");
+            String newSep = input.substring(2, customSepIndexEnd).trim();
+            sep.add(newSep);
+        }
     }
 }
