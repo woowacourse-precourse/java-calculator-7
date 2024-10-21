1352e3304734c137f1bb88f9627b168d7e8a8a0e
dabeann <dabin4078@daum.net>
2024-10-21 14:11:36 +0900

    ♻️  Refactor : exception 메시지 삭제

--- src/main/java/calculator/calculator/InputParser.java
+++ src/main/java/calculator/calculator/InputParser.java
@@ -7,7 +7,7 @@ public class InputParser {
         if (input.startsWith("//")) {
             int delimiterEndIndex = input.indexOf("\\n");
             if (delimiterEndIndex == -1) {
-                throw new IllegalArgumentException("잘못된 형식입니다. 구분자 다음에 줄바꿈이 필요합니다.");
+                throw new IllegalArgumentException();
             }
             delimiter = input.substring(2, delimiterEndIndex);
             delimiter = escapeSpecialCharacters(delimiter);
