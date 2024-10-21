8c8481750ff3e99eed82971037d5b6a814092d99
sodychoe <sodychoe@icloud.com>
2024-10-21 21:20:34 +0900

    test : 길이가 긴 입력에 대한 테스트 추가

--- src/test/java/calculator/io/InputConsoleHandlerTest.java
+++ src/test/java/calculator/io/InputConsoleHandlerTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,6 +25,36 @@ class InputConsoleHandlerTest {
         String[] inputSplit = INPUT_CONSOLE_HANDLER.getUserInput();
 
         assertThat(inputSplit).containsExactly("1", "2", "3");
+
+        System.setIn(System.in);
+        Console.close();
+    }
+
+    @Test
+    @DisplayName("긴 길이의 입력도 정상적으로 반환한다.")
+    void inputSplitLong() {
+        // given
+        StringBuilder inputBuilder = new StringBuilder();
+        List<String> expected = new ArrayList<>();
+
+        int numData = 100;
+        for (int i = 1; i <= numData; i++) {
+            inputBuilder.append(i);
+            if (i < numData) {
+                inputBuilder.append(":");
+            }
+            expected.add(String.valueOf(i));
+        }
+        String input = inputBuilder.toString();
+
+        // when
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        // then
+        String[] inputSplit = INPUT_CONSOLE_HANDLER.getUserInput();
+
+        assertThat(inputSplit).containsExactlyElementsOf(
+                expected
+        );
 
         System.setIn(System.in);
         Console.close();
