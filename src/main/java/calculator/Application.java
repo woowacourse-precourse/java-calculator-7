d11bc88dffa2a061f28a5e349f6d9b683f3d7d99
YoonYn9915 <jsy440720@gmail.com>
2024-10-21 21:09:54 +0900

    feat: 문자열 계산기 클래스를 사용해 덧셈 계산하고 결과 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,14 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
+        StringCalculator stringCalculator = new StringCalculator();
+        int result = stringCalculator.calculate(input);
+        System.out.println("결과 : " + result);
     }
 }
