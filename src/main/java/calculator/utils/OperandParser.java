598a17022982e60aacb9e48d81553cbc33f48c7a
ppparkta <pppppark2@gmail.com>
2024-10-21 19:30:14 +0900

    feat(OperandParser): 피연산자 파싱 class 추가
    변경 사항
    - Parser가 담당하던 parseOperand를 OperandParser로 이동

--- src/main/java/calculator/utils/OperandParser.java
+++ src/main/java/calculator/utils/OperandParser.java
@@ -5,4 +5,43 @@ import java.util.List;
 
 public class OperandParser {
     private final List<Integer> operand = new ArrayList<>();
+
+    private int isSeparator(String tmpString, List<String> separator) {
+        for (int i = 0; i < separator.size(); i++) {
+            if (tmpString.length() < separator.get(i).length()) {
+                continue;
+            }
+            if (separator.get(i).equals(tmpString.substring(0, separator.get(i).length()))) {
+                return separator.get(i).length();
+            }
+        }
+        return -1;
+    }
+
+    private void parseOperand(String removedString, List<String> separator) {
+        String tmpOperand = "";
+
+        for (int i = 0; i < removedString.length(); i++) {
+            int separatorLength = isSeparator(removedString.substring(i), separator);
+
+            if (separatorLength != -1) {
+                if (!tmpOperand.equals("")) {
+                    operand.add(Integer.parseInt(tmpOperand));
+                    tmpOperand = "";
+                }
+                i += separatorLength - 1;
+            } else if (Character.isDigit(removedString.charAt(i))) {
+                tmpOperand += removedString.charAt(i);
+            } else {
+                throw new IllegalArgumentException("등록되지 않은 구분자가 포함되어 있습니다.");
+            }
+        }
+        if (!tmpOperand.equals("")) {
+            operand.add(Integer.parseInt(tmpOperand));
+        }
+    }
+
+    public List<Integer> getOperand() {
+        return operand;
+    }
 }
