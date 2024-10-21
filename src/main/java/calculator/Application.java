d6e845e800d3e0c8e2382d19a7bd65c909032179
yuntasha <jaljayo85@gmail.com>
2024-10-21 09:34:02 +0900

    refactor(Application.java): remove import

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,9 +2,7 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
