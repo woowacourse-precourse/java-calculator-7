e15ee1e6c3d1e59d844fe7cfba2ce63ae9a57d17
rootTiket <l20078829@gmail.com>
2024-10-21 18:25:50 +0900

    test: 숫자 사이 공백 입력시 예외

--- src/test/java/calculator/domain/ParserTest.java
+++ src/test/java/calculator/domain/ParserTest.java
@@ -1,6 +1,8 @@
 package calculator.domain;
 
+import static calculator.domain.constant.errorMessage.ValueError.INVALID_VALUE_FORMAT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -17,4 +19,17 @@ class ParserTest {
         List<Integer> result = parser.parseInput(input);
         assertEquals(List.of(0), result);
     }
+
+    @Test
+    @DisplayName("숫자 사이에 공백이 있으면 예외를 발생시킨다")
+    void throwsExceptionForWhitespaceInNumbers() {
+        Parser parser = new Parser();
+        String input = "1 2,3";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> parser.parseInput(input));
+        assertEquals(INVALID_VALUE_FORMAT.getMessage(), exception.getMessage());
+    }
+
+
 }
