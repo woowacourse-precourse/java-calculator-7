f01e345dd6c44f8eac2f6f4d5c1ccf30b8e7bec2
Seyoung Goo <0917sy@gmail.com>
2024-10-21 20:19:27 +0900

    feat: 커스텀 구분자 차리
    - 문자열 맨 앞의 `//`와 `\n` 사이에 위치한 구분자 추출
    - 커스텀 구분자가 있을 경우 이를 기준으로 문자열 분리

--- src/main/java/calculator/model/StringParser.java
+++ src/main/java/calculator/model/StringParser.java
@@ -3,13 +3,22 @@ package calculator.model;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class StringParser {
     private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
 
 
     public List<BigDecimal> parse(String input) {
-        String[] splitInput = input.split(DEFAULT_DELIMITER_REGEX);
+        String[] splitInput;
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            splitInput = splitByCustomDelimiter(input);
+        } else {
+            splitInput = input.split(DEFAULT_DELIMITER_REGEX);
+        }
+
         return convertToNumbers(splitInput);
     }
 
@@ -25,7 +34,21 @@ public class StringParser {
                     })
                     .toList();
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");   // TODO
+            throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
         }
+    }
+
+    private String[] splitByCustomDelimiter(String input) {
+        int delimiterEndIdx = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+        if (delimiterEndIdx == -1) {
+            throw new IllegalArgumentException("커스텀 구분자 형식 오류입니다.");
+        }
+
+        String customDelimiter = input.substring(CUSTOM_DELIMITER_PREFIX.length(), delimiterEndIdx);
+        customDelimiter = Pattern.quote(customDelimiter);
+
+        String numberPart = input.substring(delimiterEndIdx + CUSTOM_DELIMITER_SUFFIX.length());
+
+        return numberPart.split(DEFAULT_DELIMITER_REGEX + '|' + customDelimiter);
     }
 }
