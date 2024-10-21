8384a7898f7127f9ecc67740f6349b3c145d3b1a
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-21 23:48:12 +0900

    style : 주석 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -22,9 +22,12 @@ public class Application {
                 e.printStackTrace();
                 return;
             }
+            // 현재 입력된 문자열 한 줄에 대한 덧셈 연산 후 결과 출력
             currentSum = ApplicationController.run(new ExpressionDTO(input), view);
-            totalSum = totalSum.add(currentSum.get());
-        } while (!currentSum.get().equals(BigDecimal.ZERO));
+            totalSum = totalSum.add(currentSum.get());  // 덧셈 결과 누적
+        } while (!currentSum.get().equals(BigDecimal.ZERO));    // 빈 문자열 입력 시 종료
+
+        // 누적합 출력
         view.displayOutput(new CalculatorResultDTO<>(totalSum));
     }
 }
