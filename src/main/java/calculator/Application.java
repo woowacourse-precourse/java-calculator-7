ef2d7da5005064ae008a3d03745c35937859fd1e
apple-mint <174938449+apple-mint@users.noreply.github.com>
2024-10-21 12:59:50 +0900

    feat: 빈 문자열 및 공백 포함 문자열 여부 확인 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import org.junit.platform.commons.util.StringUtils;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,5 +9,11 @@ public class Application {
         // 1. 문자열 입력받기
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String words = Console.readLine();
+
+        // 2. 공백이 없고 값이 하나 이상 있는지 확인
+        // 아니라면 유효한 값이 아니므로 IllegalArgumentException 발생시킴
+        if (words == null || words.isEmpty() || StringUtils.containsWhitespace(words)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
