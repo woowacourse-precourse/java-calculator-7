d993dc8da4350476bb7192a196aad5441f21d186
Cori1304-Seong <andyjm7739@gmail.com>
2024-10-21 11:51:12 +0900

    feat(Application): String input int로 바뀌는지 확인 로직

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,14 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+      String input = InputService.readLine();
+
+      // TODO: int로 바뀐 숫자 sum 기능 구현, class나 함수로 분리
+        int number = Integer.parseInt(input);
+        System.out.println("number의 타입; : " + ((Object) number).getClass().getName());
+        number += 10;
+        System.out.println("number + 10 = " + (number));
+        System.out.println("number의 타입; : " + ((Object) number).getClass().getName());
     }
 }
