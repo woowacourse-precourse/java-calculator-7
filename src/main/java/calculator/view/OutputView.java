0220004463aa226aa7d4a01631a1ed78729078c1
WonJun Park <tngkrt2004@gmail.com>
2024-10-21 18:51:07 +0900

    feat(view): 에러 메세지 출력 구현

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,11 +1,15 @@
 package calculator.view;
 
 public class OutputView {
-    public void printInput(){
+    public void printInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
     public void printResult(int result) {
         System.out.println("결과 : " + result);
+    }
+
+    public void printError(String message) {
+        System.out.println(message);
     }
 }
