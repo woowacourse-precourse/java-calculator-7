5f06008f2868ffcee21b4171fa3abef97d232b52
leestana01 <leestana01@naver.com>
2024-10-21 09:09:27 +0900

    feat(Application): 커스텀 구분자 추가
    문제 상황 해결 로직 추가
    - 정규식을 통한 커스텀 구분자 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,10 +10,14 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        String delimiter = "[,:]";
+        Matcher matcher = Pattern.compile("//(.*)\\\\n(.*)").matcher(input);
+
+        boolean isMatched = matcher.find();
+        String delimiter = "[,:" + (isMatched ? matcher.group(1) : "") + "]";
+        String numbers = isMatched ? matcher.group(2) : input;
 
         System.out.println("결과 : " +
-            Arrays.stream(input.split(delimiter))
+            Arrays.stream(numbers.split(delimiter))
                 .map(Integer::parseInt)
                 .reduce(0, Integer::sum));
     }
