dd5f06b8f0cf53ac58a0b603b2b2969a177b1cd3
SeungMin Park <tonydragon@naver.com>
2024-10-21 23:41:48 +0900

    feat: 커스텀 구분자 처리 기능 구현

--- src/main/java/model/StringCalculator.java
+++ src/main/java/model/StringCalculator.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     private static final String DEFAULT_DELIMITER = ",|:";
 
@@ -8,7 +11,16 @@ public class StringCalculator {
             return 0;
         }
 
-        String[] tokens = input.split(DEFAULT_DELIMITER);
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (m.matches()) {
+            String customDelimiter = Pattern.quote(m.group(1));
+            String numbers = m.group(2);
+            return sumNumbers(numbers.split(customDelimiter));
+        }
+        return sumNumbers(input.split(DEFAULT_DELIMITER));
+    }
+
+    private static int sumNumbers(String[] tokens) {
         int sum = 0;
         for (String token : tokens) {
             sum += Integer.parseInt(token);
