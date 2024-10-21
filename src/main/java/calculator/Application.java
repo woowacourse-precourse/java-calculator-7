a9d0eedf6ee5dda63d0c79cdee0420e0ae1c1b11
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-10-21 21:51:08 +0900

    feat(Application): 구분자로 분리한 값들을 검사하고 통과하면 더한 뒤 반환한다
    getSum함수를 만들어서 값들에 대한 검사를 한다. 만약에 비어있는 경우에는 0을 반환한다.
    숫자가 아니거나, 음수인 경우도 모두 IllegalArgumentException를 출력한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,7 +2,29 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void calculate(String input) {
+
+    public static int getSum(String[] numberArray){
+        int sum=0;
+        for (String number : numberArray) {
+            String trimmed = number.trim();
+            if (trimmed.isEmpty()) {
+                return sum;
+                // throw new IllegalArgumentException("빈 숫자가 있습니다.");
+            }
+            // 숫자 형식 검증
+            if (!trimmed.matches("\\d+")) {
+                throw new IllegalArgumentException("숫자 형식이 올바르지 않습니다.");
+            }
+
+            int num = Integer.parseInt(trimmed);
+            if (num < 0) {
+                throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+            }
+            sum+=num;
+        }
+        return sum;
+    }
+    public static int calculate(String input) {
         String numbers;
         if (input.length() >= 2 && input.substring(0, 2).equals("//")) {
             int slashIndex = input.indexOf("\\n");
@@ -11,6 +33,7 @@ public class Application {
             if (slashIndex != -1) {
                 String customDivider = input.substring(2, slashIndex);
                 String[] numberArray = numbers.split(String.valueOf(customDivider));
+                return getSum(numberArray);
             }
             else{
                 throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다.");
@@ -20,15 +43,16 @@ public class Application {
             numbers = input;
             // 쉼표와 콜론을 기준으로 분리
             String[] numberArray = numbers.split("[,;]");
+            return getSum(numberArray);
         }
-        return;
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         try {
             System.out.println("숫자를 입력하세요");
             String input = Console.readLine();
-            calculate(input);
+            int result = calculate(input);
+            System.out.printf("결과 : %d",result);
         }
         finally {
             Console.close();
