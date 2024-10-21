c4f2d63bd3d92329dfbeb3bf4e323a5edba3d72c
leesh <leesh7807@gmail.com>
2024-10-21 18:43:31 +0900

    feat: add string array to integer array method in StringCalculator class

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,8 +1,14 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     private final DelimiterParser delimiterParser = new DelimiterParser();
 
+    private int[] StringToIntegerArray(String[] arr) {
+        return Arrays.stream(arr).mapToInt(this::parseInt).toArray();
+    }
+    
     private String[] makeStringToExpression(String input) {
         DelimiterContentPair parsed = delimiterParser.parseInput(input);
         return makeStrArray(parsed);
@@ -12,5 +18,17 @@ public class StringCalculator {
         String delimiter = parsedInput.getDelimiter();
         String content = parsedInput.getContent();
         return content.split(delimiter);
+    }
+
+    private Integer parseInt(String str) {
+        try {
+            // 비어 있는 문자열 처리
+            if (str.isEmpty()) {
+                return 0;
+            }
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid input: should be Integer.");
+        }
     }
 }
