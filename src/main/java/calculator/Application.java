1505201d951057ee58c1835278a0e8a10a818abc
jarvis0720 <gktkddms9631@gmail.com>
2024-10-21 20:10:12 +0900

    refactor(Application): 메인 로직 개선 및 분리된 기능 활용
    - Separator와 Calculator 클래스를 활용하여 메인 로직 단순화
    - 입력에 따른 커스텀 구분자 처리 및 기본 구분자 처리 로직 통합

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,34 +1,27 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        String input;
-        input = readLine();
-
+        Separator separator = new Separator();
+        Calculator calculator = new Calculator();
+        String input = readLine();
         int result = 0;
-        String[] numbers;
-        if (input.isEmpty()) {
-            System.out.println("결과 : 0");
-            return;
-        }
-        StringBuilder seperator = new StringBuilder("[,:]");
-        if (input.contains("//") && input.contains("\\n")) {
-            String[] dividedInput = input.split("\\\\n");
-            String customSeparator = dividedInput[0].substring(2);
-            seperator.insert(1, customSeparator);
-            numbers = dividedInput[1].split(seperator.toString());
-        }
-        else{
-            numbers = input.split(seperator.toString());
-        }
+        if (!input.isEmpty()) result = start(input, separator, calculator);
+        System.out.println("결과 : " + result);
+        Console.close();
+    }
 
-        int[] intArray = new int[numbers.length];
-        for (int i = 0; i < numbers.length; i++) intArray[i] = Integer.parseInt(numbers[i].trim());
-
-       for (int num : intArray) result += num;
-       System.out.println("결과 : " + result);
-
+    public static int start(String input, Separator separator, Calculator calculator) {
+        if (input.startsWith("//")) {
+            separator.divideInput(input);
+            separator.insertCustomSeparator();
+            separator.splitWithCustomSeparater();
+        } else separator.splitWIthSeparater(input);
+        calculator.convertStringArrayToIntArray(separator.getNumbers());
+        return calculator.calculateResult();
     }
 }
