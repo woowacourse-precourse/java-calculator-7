563ace7284cca819a2de4655abe1a742dd1edcbb
unknown <wkdwoehd29@gmail.com>
2024-10-21 17:06:21 +0900

    feat(Application.java): 커스텀 구분자 지원 추가
    입력 문자열에서 커스텀 구분자의 존재 여부를 판단하는 기능을 구현했습니다. 사용자가 원하는 구분자를 지정하여파싱할 수 있습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+
+    }
+    private static boolean check_delimeter(String input) {
+        if(input.charAt(0)=='/' && input.charAt(1)=='/') {
+            return true;
+        }
+        return false;
     }
 }
+
