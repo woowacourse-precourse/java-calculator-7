107145c5100e640c812104463ece2f32ff3cc21f
chaeshee0908 <chaeshee0908@naver.com>
2024-10-21 21:12:51 +0900

    feat(Number): 숫자가 양수인지 유효성 검사

--- src/main/java/calculator/domain/Number.java
+++ src/main/java/calculator/domain/Number.java
@@ -5,10 +5,17 @@ public class Number {
     private Integer number;
 
     public Number(Integer number) {
+        validatePositiveNumber(number);
         this.number = number;
     }
 
     public Integer getNumber() {
         return number;
+    }
+
+    public void validatePositiveNumber(Integer n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("[ERROR] 양수를 입력해야 합니다.");
+        }
     }
 }
