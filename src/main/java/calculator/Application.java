2b5f889395f81fa11937292200e2658802ba8d58
kyungsoo <yksoo00@naver.com>
2024-10-21 15:14:52 +0900

    feat : 문자열 덧셈 계산기 실행 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import calculator.domain.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView input = new InputView();
+        Calculator calculator = new Calculator();
+        new OutputView(calculator.add(input.getUserInput()));
     }
 }
