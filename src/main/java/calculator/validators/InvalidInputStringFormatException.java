333e5a22c4f655d1956c6253b767292bd14de2b1
choiyoung69 <choiyoung609@gmail.com>
2024-10-21 13:52:13 +0900

    feat(InvalidOperandFormatException): 음수인지 체크하는 함수 생성

--- src/main/java/calculator/validators/InvalidInputStringFormatException.java
+++ src/main/java/calculator/validators/InvalidInputStringFormatException.java
@@ -11,25 +11,27 @@ public class InvalidInputStringFormatException {
         checkFrontAndBackOfCustomDelimiter(operandStr);
     }
 
+    //스페이스만 입력되었는지 확인
     public static void checkIfStringWhiteSpace(String operandStr){
         if(!operandStr.isEmpty() && operandStr.trim().isEmpty()){
             throw new IllegalArgumentException();
         }
     }
 
-    // 문자열의 처음이 숫자이거나 // 가 아니라면 에러 발생
+    // 문자열의 처음이 숫자이거나 //인지 확인
     public static void checkStartingPoint(String operandStr){
         if(!(operandStr.startsWith(CUSTOM_DELIMITER_START_WRAPPER) || Character.isDigit(operandStr.charAt(0))))
             throw new IllegalArgumentException();
     }
 
-    //문자열의 마지막이 숫자로 끝나지 않으면 에러 발생
+    //문자열의 마지막이 숫자로 끝나는지 확인
     public static void checkEndPoint(String operandStr){
         char endOfString = operandStr.charAt(operandStr.length() - 1);
         if(!Character.isDigit(endOfString))
             throw new IllegalArgumentException();
     }
 
+    // 커스텀 문자를 구분하는 //뒤에 \\n이 오는지 확인
     public static void checkFrontAndBackOfCustomDelimiter(String operandStr){
         if(operandStr.startsWith(CUSTOM_DELIMITER_START_WRAPPER) && !operandStr.startsWith(CUSTOM_DELIMITER_END_WRAPPER, 3)){
             throw new IllegalArgumentException();
--- src/main/java/calculator/validators/InvalidOperandFormatException.java
+++ src/main/java/calculator/validators/InvalidOperandFormatException.java
@@ -4,7 +4,9 @@ public class InvalidOperandFormatException {
     // 빈 값 및 정수 체크를 위한 함수
     public static int validateToken(String token) {
         checkEmpty(token);
-        return checkInteger(token);
+        int integerToken = checkInteger(token);
+        checkNegativeInteger(integerToken);
+        return integerToken;
     }
     private static void checkEmpty(String str){
         if (str.isEmpty()) {
@@ -18,5 +20,10 @@ public class InvalidOperandFormatException {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("정수로 변환할 수 없는 값입니다");
         }
+    }
+
+    private static void checkNegativeInteger(int integerToken){
+        if(integerToken < 0)
+            throw new IllegalArgumentException("음수 값이 입력되었습니다");
     }
 }
