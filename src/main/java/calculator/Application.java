63f74414b606bc5fb69607f385b4aac8dda37d3f
Hyejeong <thswjd0609@gmail.com>
2024-10-21 10:58:43 +0900

    refactor: 클래스 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,26 +4,51 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String line = Console.readLine();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
 
-        String[] nums;
-        int sum = 0;
-        String delimiter = ",|:";
+        StringCalculator sc = new StringCalculator(input);
+        sc.findCustomDelimiter();
+        sc.splitByDelimiter();
+        sc.calculateSum();
+        int sum = sc.getSum();
 
-        // 커스텀 구분자
-        if (line.startsWith("//")) {
-            nums = line.split("\\\\n", 2);
+        System.out.println("결과 : " + sum);
+    }
+}
+
+class StringCalculator {
+    private String input;
+    private String delimiter;
+    private String[] nums;
+    private int sum;
+
+    public StringCalculator(String input) {
+        this.input = input;
+        this.delimiter = ",|:";
+        this.sum = 0;
+    }
+
+    // 커스텀 구분자
+    public void findCustomDelimiter() {
+        if (input.startsWith("//")) {
+            nums = input.split("\\\\n", 2);
             if (nums.length == 1) {
                 throw new IllegalArgumentException("Invalid custom delimiter format");
             } else {
                 delimiter += "|" + nums[0].substring(2);
-                line = nums[1];
+                input = nums[1];
             }
         }
+    }
 
-        // 구분자를 기준으로 분리
-        nums = line.split(delimiter);
+    // 구분자를 기준으로 분리
+    public void splitByDelimiter() {
+        nums = input.split(delimiter);
+    }
 
+    // 숫자들의 합 계산
+    public void calculateSum() {
         for (String num : nums) {
             if (num.isEmpty()) {
                 continue;
@@ -37,7 +62,10 @@ public class Application {
                 throw new IllegalArgumentException("Invalid number format");
             }
         }
+    }
 
-        System.out.println("결과 : " + sum);
+    // 숫자들의 합 반환
+    public int getSum() {
+        return sum;
     }
 }
