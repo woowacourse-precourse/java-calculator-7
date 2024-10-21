9b4cf34ab222af1273915bf3e48125c3d5a68f50
rhxfkg <rhxfkg@gmail.com>
2024-10-21 18:17:46 +0900

    main

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,7 @@ public class Application {
         String str = camp.nextstep.edu.missionutils.Console.readLine();
         Calculator calculator = new Calculator();
         int result = calculator.calculate(str);
+
         System.out.println("결과 : " + result);
     }
 }
