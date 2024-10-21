88f1a493ae1936305386dbab816b910c649b3463
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-21 22:24:49 +0900

    refactor(Calculator.java): Calculator 싱글톤 패턴으로 변경
    Calculator 클래스의 디자인 패턴을 싱글톤 패턴으로 변경

--- src/main/java/calculator/utils/Calculator.java
+++ src/main/java/calculator/utils/Calculator.java
@@ -3,6 +3,18 @@ package calculator.utils;
 import java.util.List;
 
 public class Calculator {
+    private static Calculator instance;
+
+    private Calculator() {
+    }
+
+    public static Calculator getInstance() {
+        if (instance == null) {
+            instance = new Calculator();
+        }
+        return instance;
+    }
+
     public int calculateResult(List<Integer> nums) {
         return nums.stream()
                 .mapToInt(Integer::intValue)
