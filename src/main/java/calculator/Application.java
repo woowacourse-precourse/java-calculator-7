9f3e7b5925b94c72c872efcb2f5d920f09335e5c
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-10-21 16:15:59 +0900

    feat: input, output screen

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,20 @@
 package calculator;
 
-public class Application {
+import static calculator.CalculatorService.add;
 
+import camp.nextstep.edu.missionutils.Console;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String input = Console.readLine();
+
+        try {
+            int result = add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
