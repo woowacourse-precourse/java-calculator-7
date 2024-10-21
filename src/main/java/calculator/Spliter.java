2f1faa0b758678ed040258d1e838afa23a3f2274
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2024-10-21 16:11:05 +0900

    feat: 결과 값을 출력하는 메서드 구현

--- src/main/java/calculator/Spliter.java
+++ src/main/java/calculator/Spliter.java
@@ -1,18 +1,18 @@
 package calculator;
 
-import java.util.Arrays;
-
 public class Spliter {
     private String inputString;
-    private char customSeparator1;
+    private char customSeparator;
     private int resultValue = 0;
 
+    // 생성자
     public Spliter(String inputString){
         this.inputString = inputString;
     }
 
-    public int getResultValue() {
-        return resultValue;
+    // 결과 값 출력
+    public void printResult() {
+        System.out.println("결과 : " + resultValue);
     }
 
     public void calculate() {
@@ -24,19 +24,16 @@ public class Spliter {
         } else { // 문자열 시작이 문자이면
             generateCustomSeparator();
 
-            if(customSeparator1 >= 65 && customSeparator1 <= 90 || customSeparator1 >= 97 && customSeparator1 <= 122) {
-                result = inputString.split(":|,|" + customSeparator1);
+            if(customSeparator >= 65 && customSeparator <= 90 || customSeparator >= 97 && customSeparator <= 122) {
+                result = inputString.split(":|,|" + customSeparator);
             } else {
-                result = inputString.split(":|,|" + "\\" + customSeparator1);
+                result = inputString.split(":|,|" + "\\" + customSeparator);
             }
         }
 
-        //System.out.println(Arrays.toString(result));
-
-        if(inspectionValue(result) == true) {
+        if(inspectionValue(result)) {
             sum(result);
         } else {
-            //System.out.println("[result문자열]잘못된 값을 입력하였습니다.");
             throw new IllegalArgumentException();
         }
 
@@ -50,31 +47,24 @@ public class Spliter {
         }
     }
 
-    // 작성 필요
-    //*\n1:2,3*4*-4
-    //s\n1:2,3s4s5
-    //s\n1:2,3s4s-5
     public void generateCustomSeparator() {
         if(inputString.length() >= 5) {
             char[] inputArray = inputString.toCharArray();
 
             // 커스텀 구분자 등록 후 문자열 시작위치 변경
             if(inputArray[0] == '/' && inputArray[1] == '/' && inputArray[3] == '\\' && inputArray[4] == 'n') {
-                customSeparator1 = inputArray[2];
+                customSeparator = inputArray[2];
             } else {
-                //System.out.println("[커스텀 구분자 생성]잘못된 값을 입력하였습니다!");
                 throw new IllegalArgumentException();
             }
             this.inputString = inputString.substring(5);
         }
     }
 
-    // 작성 필요
     public boolean inspectionValue(String[] result) {
         for(int i=0; i<result.length; i++) {
             for(int j=0; j<result[i].length(); j++) {
                 if(result[i].charAt(j) < 48 || result[i].charAt(j) > 57) {
-                    //System.out.println("result[" + i + "][" + j + "] = " + result[i].charAt(j));
                     return false;
                 }
             }
@@ -83,7 +73,6 @@ public class Spliter {
     }
 
     public void sum(String[] result) {
-        //System.out.println("커스텀 구분자 = " + customSeparator1 + ", result = " + Arrays.toString(result));
         for(int i=0; i<result.length; i++) {
             this.resultValue += Integer.parseInt(result[i]);
         }
