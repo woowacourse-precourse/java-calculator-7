dcb716f5e8d29d78a9fa10e4ebf5809bcd6ed2bd
SinnoLn <sintmtn69@gmail.com>
2024-10-21 12:46:28 +0900

    feat: 메인 함수 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        InputParser inputParser = new InputParser();
+        int[] numbers = inputParser.parse(input);
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.calculate(numbers);
+        System.out.println("결과 : " + result);
     }
 }
