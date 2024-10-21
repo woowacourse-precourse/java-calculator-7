a126c92b40cc4a67888372316f3b91adef9630a8
davemins <davemins4125@naver.com>
2024-10-21 16:58:25 +0900

    refactor(Controller): 불필요한 의존성 제거
    - NumberParser에서 구분자 추출 및 숫자 파싱 로직을 통합하여 Controller 불필요한 의존성 제거

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,28 +1,21 @@
 package calculator.controller;
 
 import calculator.domain.Calculator;
-import calculator.domain.DelimiterExtractor;
 import calculator.domain.NumberParser;
 import calculator.view.View;
 
 public class CalculatorController {
-    private final DelimiterExtractor delimiterExtractor;
     private final NumberParser numberParser;
     private final Calculator calculator;
 
-
     public CalculatorController() {
-        this.delimiterExtractor = new DelimiterExtractor();
         this.numberParser = new NumberParser();
         this.calculator = new Calculator();
     }
 
     public void run() {
         String input = View.requestString();
-
-        String delimiters = delimiterExtractor.extract(input);
-        String[] numbers = numberParser.parse(input, delimiters);
-
+        String[] numbers = numberParser.parse(input);
         int result = calculator.sum(numbers);
         View.printResult(result);
     }
