b759077a50ac636d951ae96a814b9e272f28d700
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-21 18:23:01 +0900

    test(ApplicationTest): 다양한 입력에 대한 테스트 추가
    - 0 입력 시 IllegalArgumentException 발생 테스트
    - 빈 문자열 입력 시 결과 0 반환 테스트
    - 공백 문자열 입력 시 IllegalArgumentException 발생 테스트
    - 숫자로 된 커스텀 구분자 사용 시 정상 동작 테스트
    - 숫자만 입력 시 결과 동일하게 반환 테스트
    - 기본 구분자만 입력 시 결과 0 반환 테스트
    - 여러 기본 구분자 입력 시 결과 0 반환 테스트
    - 커스텀 구분자만 지정한 경우 결과 0 반환 테스트
    - 커스텀 구분자만 여러 번 입력한 경우 결과 0 반환 테스트
    - 기본 및 커스텀 구분자 조합 입력 시 결과 0 반환 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,9 +19,142 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    // Given: "-1, -2, -3"
+    // When: SumCalculator.sum(inputDTO)
+    // Then: IllegalArgumentException
+    @Test
+    void 예외_테스트_음수만_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,-2,-3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    // Given: 0
+    // When: SumCalculator.sum(inputDTO)
+    // Then: IllegalArgumentException
+    @Test
+    void 예외_테스트_문자열_0_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    // Given: "\n"
+    // When: SumCalculator.sum(inputDTO)
+    // Then: "결과 : 0"
+    // 문제 입력 조건 중 "" => 0 에 해당하는 부분
+    @Test
+    void 동작_테스트_빈_문자열_입력() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    // Given: " "
+    // When: SumCalculator.sum(inputDTO)
+    // Then: "결과 : 0"
+    @Test
+    void 동작_테스트_공백_문자열_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" "))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    // Given: "//1\n21212,212"
+    // When: SumCalculator.sum(inputDTO)
+    // Then: "결과 : 10"
+    @Test
+    void 동작_테스트_커스텀_구분자를_숫자로_사용() {
+        assertSimpleTest(() -> {
+            run("//1\\n21212,212");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    // Given: "123"
+    // When: SumCalculator.sum(inputDTO)
+    // Then: "결과 : 123"
+    @Test
+    void 동작_테스트_숫자만_입력() {
+        assertSimpleTest(() -> {
+            run("123");
+            assertThat(output()).contains("결과 : 123");
+        });
+    }
+
+    // Given: ","
+    // When: SumCalculator.sum(inputDTO)
+    // Then: "결과 : 0"
+    @Test
+    void 동작_테스트_구분자만_입력() {
+        assertSimpleTest(() -> {
+            run(",");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    // Given: ",:,"
+    // When: SumCalculator.sum(inputDTO)
+    // Then: "결과 : 0"
+    @Test
+    void 동작_테스트_구분자만_다수_입력() {
+        assertSimpleTest(() -> {
+            run(",:,");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    // Given: "//;\\n"
+    // When: SumCalculator.sum(inputDTO)
+    // Then: "결과 : 0"
+    @Test
+    void 동작_테스트_커스텀_구분자_지정만() {
+        assertSimpleTest(() -> {
+            run("//;\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    // Given: "//;\\n;"
+    // When: SumCalculator.sum(inputDTO)
+    // Then: "결과 : 0"
+    @Test
+    void 동작_테스트_커스텀_구분자만_입력() {
+        assertSimpleTest(() -> {
+            run("//;\\n;");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    // Given: "//;\\n;;;"
+    // When: SumCalculator.sum(inputDTO)
+    // Then: "결과 : 0"
+    @Test
+    void 동작_테스트_커스텀_구분자만_다수_입력() {
+        assertSimpleTest(() -> {
+            run("//;\\n;;;");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    // Given: "//;\\n;,:;"
+    // When: SumCalculator.sum(inputDTO)
+    // Then: "결과 : 0"
+    @Test
+    void 동작_테스트_기본과_커스텀_구분자들만_입력() {
+        assertSimpleTest(() -> {
+            run("//;\\n;,:;");
+            assertThat(output()).contains("결과 : 0");
+        });
     }
 
     @Override
