8981ea47baf3f61771fa23f7230e2f983c895215
SinnoLn <sintmtn69@gmail.com>
2024-10-21 15:30:08 +0900

    refactor: 공백 처리 최적화 및 커스텀 구분자 검사 강화

--- src/main/java/calculator/InputParser.java
+++ src/main/java/calculator/InputParser.java
@@ -1,30 +1,30 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class InputParser {
     public int[] parse(String input) {
         if (input == null) {
             throw new IllegalArgumentException("입력 값이 null 일 수 없습니다.");
         }
 
+        input = input.trim();
+
         if (input.isEmpty()) {
             return new int[]{0};  // 빈 문자열일 경우 0 반환
         }
 
-        input = input.trim();
-
-        if (input.contains(" ")) {
-            input = input.replaceAll("\\s+", "");
-        }
+        input = input.replaceAll("\\s+", "");
 
         String delimiter = "[,|:]"; // 기본 구분자
         if (input.startsWith("//")) {
             int delimiterEnd = input.indexOf("\\n");
             if (delimiterEnd != -1) {
                 String customDelimiter = input.substring(2, delimiterEnd);
-                if (customDelimiter.isEmpty()) {
-                    throw new IllegalArgumentException("커스텀 구분자 안에 빈 문자열일 수 없습니다.");
+                if (customDelimiter.length() != 1) {
+                    throw new IllegalArgumentException("커스텀 구분자는 한 글자만 가능합니다.");
                 }
-                delimiter = customDelimiter;
+                delimiter = Pattern.quote(customDelimiter);
                 input = input.substring(delimiterEnd + 2);
             } else {
                 throw new IllegalArgumentException("잘못된 입력형식입니다: " + input);
@@ -34,7 +34,7 @@ public class InputParser {
         if (input.matches(".*[,|:]{2,}.*")) {
             throw new IllegalArgumentException("잘못된 구분자가 연속으로 사용되었습니다.");
         }
-        
+
         String[] tokens = input.split(delimiter);
 
         for (String token : tokens) {
@@ -44,7 +44,6 @@ public class InputParser {
         }
 
         int[] numbers = new int[tokens.length];
-
         for (int i = 0; i < tokens.length; i++) {
             numbers[i] = Integer.parseInt(tokens[i]);  // 각각의 토큰을 int 로 변환
         }
