3c7c6eea8ab9a9644987c5d112fb9446bef81de6
hanseounggyun <hanseu9839@gmail.com>
2024-10-22 10:17:04 +0900

    refactor: input Console.readline()으로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,13 +4,11 @@ import calculator.domain.Calculator;
 import calculator.ui.InputView;
 import calculator.ui.ResultView;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 public class Application {
     public static void main(String[] args)  {
         // TODO: 프로그램 구현
-        InputView inputView = new InputView(new BufferedReader(new InputStreamReader(System.in)));
+        InputView inputView = new InputView();
         Calculator calculator = new Calculator(inputView.inputView());
         ResultView.calculatorResult(calculator.sum());
 
--- src/main/java/calculator/ui/InputView.java
+++ src/main/java/calculator/ui/InputView.java
@@ -1,23 +1,17 @@
 package calculator.ui;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_SENTENCE = "덧셈할 문자열을 입력해 주세요.";
     private static final String ERROR_MESSAGE = "잘못 입력하였습니다.";
-    private final BufferedReader bufferedReader;
 
-    public InputView(BufferedReader bufferedReader) {
-        this.bufferedReader = bufferedReader;
-    }
 
     public String inputView()  {
         System.out.println(INPUT_SENTENCE);
         String str = null;
         try{
-            str = bufferedReader.readLine();
+            str = Console.readLine();
         } catch(Exception e) {
             System.out.println(ERROR_MESSAGE);
         }
