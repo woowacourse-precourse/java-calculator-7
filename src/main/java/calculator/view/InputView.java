6415d4e685eb7298d75413e3b548c364ab6c10c0
huncozyboy <leejh5838@naver.com>
2024-10-22 09:27:24 +0900

    feat: InputView에 음수와 숫자 범위 검증 로직 추가

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -2,9 +2,13 @@ package calculator.view;
 
 import calculator.common.ErrorMessage;
 import calculator.exception.CalculatorException;
+import calculator.model.validator.NegativeNumberValidator;
+import calculator.model.validator.NumberRangeValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private final NegativeNumberValidator negativeNumberValidator = new NegativeNumberValidator();
+    private final NumberRangeValidator numberRangeValidator = new NumberRangeValidator();
     public String inputString() {
         String input = Console.readLine();
 
@@ -16,5 +20,8 @@ public class InputView {
         if (input.isEmpty()) {
             throw new CalculatorException(ErrorMessage.NULL_INPUT);
         }
+        int number = Integer.parseInt(input);
+        negativeNumberValidator.validate(number);
+        numberRangeValidator.validate(number);
     }
 }
