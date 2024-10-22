52b2978e3fb7d5dc4f52c19d85dc3be596067a56
huncozyboy <leejh5838@naver.com>
2024-10-22 10:10:50 +0900

    fix: 출력메세지 오타 수정

--- src/main/java/calculator/common/OutputMessage.java
+++ src/main/java/calculator/common/OutputMessage.java
@@ -1,7 +1,7 @@
 package calculator.common;
 
 public enum OutputMessage {
-    START_MESSEAGE("덧셈할 문자열을 입력해 주세요."),
+    START_MESSAGE("덧셈할 문자열을 입력해 주세요."),
     RESULT_MESSAGE("결과 : "),
     ;
     private final String message;
