5159b0ba6c3688720b957fbae1e1e994baa1042a
huncozyboy <leejh5838@naver.com>
2024-10-22 10:20:20 +0900

    feat: 입력 값이 비어있을시 0 반환 기능 추가

--- src/main/java/calculator/model/calculator/StringCalculator.java
+++ src/main/java/calculator/model/calculator/StringCalculator.java
@@ -1,3 +1,12 @@
 package calculator.model.calculator;
 public class StringCalculator {
+    public int add(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+        return calculateSum(input);
+    }
+    private int calculateSum(String input) {
+        return 0;
+    }
 }
