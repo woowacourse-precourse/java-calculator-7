9b3d307900e9474144f6e0b08125a133844912c3
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-22 11:38:20 +0900

    feat(IOService): add 빈 문자열 입력 처리
    빈 입력 시 발생할 수 있는 NoSuchElementException을 처리하여 안전하게 빈 문자열로 변환.

--- src/main/java/calculator/IOService.java
+++ src/main/java/calculator/IOService.java
@@ -3,17 +3,28 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.math.BigDecimal;
+import java.util.NoSuchElementException;
 
 public class IOService {
 
     public String getInput() {
         try {
             System.out.println("덧셈할 문자열을 입력해 주세요.");
-            String input = Console.readLine();
+            String input = getSafeInput();
+
             validateInput(input);
+
             return input;
         } finally {
             Console.close();
+        }
+    }
+
+    private String getSafeInput() {
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            return ""; // 빈 문자열로 처리
         }
     }
 
