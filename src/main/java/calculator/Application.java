c7922165582aba998a69dabbfaf48504d044ba95
Lanvizu <ghzm888@gmail.com>
2024-10-22 11:35:35 +0900

    feat: main 메서드 정리 및 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import calculator.domain.DelimiterParser;
+import calculator.domain.InputParser;
+import calculator.domain.StringSplitter;
+import calculator.domain.SumCalculator;
+import calculator.domain.controller.CalculatorController;
+import calculator.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputParser inputParser = new InputParser();
+        DelimiterParser delimiterParser = new DelimiterParser();
+        StringSplitter stringSplitter = new StringSplitter();
+        SumCalculator sumCalculator = new SumCalculator();
+        CalculatorController calculatorController = new CalculatorController(inputParser, delimiterParser,
+                stringSplitter, sumCalculator);
+        String input = InputView.input();
+        calculatorController.run(input);
     }
 }
