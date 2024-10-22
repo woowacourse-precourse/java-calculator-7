58645e8236c6501eef5a8ab169e9dad89b54e9cf
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-22 15:16:23 +0900

    test(InputTest): add @DisplayName 애너테이션, 테스트 설명 개선
    - 각 테스트 메서드에 @DisplayName 애너테이션을 적용하여 테스트의 목적을 명확히 표현
    - 테스트 실행 시 가독성을 향상

--- src/test/java/calculator/InputTest.java
+++ src/test/java/calculator/InputTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -9,8 +10,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputTest extends NsTest {
 
-    // 빈 문자열 입력 처리
     @Test
+    @DisplayName("빈 문자열 입력 시 0을 반환해야 한다")
     void 빈_문자열_입력시_0반환() {
         assertSimpleTest(() -> {
             run("");
@@ -18,8 +19,8 @@ class InputTest extends NsTest {
         });
     }
 
-    // 기본 구분자(쉼표, 콜론)를 사용하는 경우
     @Test
+    @DisplayName("기본 구분자로 입력된 숫자를 계산해야 한다")
     void 기본_구분자로_입력된_숫자_계산() {
         assertSimpleTest(() -> {
             run("1,2:3");
@@ -27,8 +28,8 @@ class InputTest extends NsTest {
         });
     }
 
-    // 커스텀 구분자 ";"를 사용하는 경우
     @Test
+    @DisplayName("커스텀 구분자로 세미콜론을 사용할 경우 숫자를 계산해야 한다")
     void 커스텀_구분자로_세미콜론_사용한_숫자_계산() {
         assertSimpleTest(() -> {
             run("//;\\n1;2;3");
@@ -36,8 +37,8 @@ class InputTest extends NsTest {
         });
     }
 
-    // 커스텀 구분자 "a"를 사용하는 경우
     @Test
+    @DisplayName("커스텀 구분자로 a를 사용할 경우 숫자를 계산해야 한다")
     void 커스텀_구분자로_a_사용한_숫자_계산() {
         assertSimpleTest(() -> {
             run("//a\\n1a2a3");
@@ -45,8 +46,8 @@ class InputTest extends NsTest {
         });
     }
 
-    // 잘못된 입력: 여러 개의 커스텀 구분자를 사용한 경우
     @Test
+    @DisplayName("여러 개의 커스텀 구분자를 사용할 경우 예외가 발생해야 한다")
     void 여러개의_커스텀_구분자_사용시_예외처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("//;,&\\n1;2&3"))
@@ -54,8 +55,8 @@ class InputTest extends NsTest {
         );
     }
 
-    // 잘못된 입력: 커스텀 구분자가 숫자인 경우
     @Test
+    @DisplayName("커스텀 구분자가 숫자인 경우 예외가 발생해야 한다")
     void 커스텀_구분자로_숫자를_사용한_경우_예외처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("//1\\n11213"))
@@ -63,8 +64,8 @@ class InputTest extends NsTest {
         );
     }
 
-    // 잘못된 입력: 공백이 포함된 경우
     @Test
+    @DisplayName("입력값에 공백이 포함된 경우 예외가 발생해야 한다")
     void 입력값에_공백이_포함된_경우_예외처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1, 2, 3, 45 67 89"))
@@ -72,8 +73,8 @@ class InputTest extends NsTest {
         );
     }
 
-    // 음수가 포함된 경우 예외 처리
     @Test
+    @DisplayName("음수가 포함된 경우 예외가 발생해야 한다")
     void 음수가_포함된_경우_예외처리() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("//;\\n-1;2;3"))
