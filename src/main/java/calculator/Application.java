40b715bb649b811fcd08eb347a2a7fd108ce36b1
KangrimChoi <rkdfla100@naver.com>
2024-10-23 12:00:52 +0900

    Code format:'Ctrl+Shift+L' in IntelliJ

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,4 +1,5 @@
 package calculator;
+
 import java.util.Scanner;
 
 public class Application {
@@ -7,7 +8,7 @@ public class Application {
         Scanner scanner = new Scanner(System.in);
         System.out.print("덧셈할 문자열을 입력해 주세요.\n");
 
-        try{
+        try {
             final String input = scanner.nextLine();
             int result = makeCalculation(input);
             printResult(result);
@@ -22,38 +23,38 @@ public class Application {
     public static int makeCalculation(final String input) {
         int i = 0;
         int sum = 0;
-        String Num = "0";
+        String num = "0";
         char custom = '\u0000';
 
-        if(input.length() >= 5) {
-            String subS = input.substring(0,2);
-            String subN = input.substring(3,5);
+        if (input.length() >= 5) {
+            String subS = input.substring(0, 2);
+            String subN = input.substring(3, 5);
             if (subS.equals("//") && subN.equals("\\n")) {
                 custom = input.charAt(2);
-                i=5;
+                i = 5;
             }
         }
 
         while (i != input.length()) {
             char curVal = input.charAt(i);
 
-            if(curVal >= '0' && curVal <= '9') {
-               Num += Character.toString(curVal);
-            } else if(curVal == ',' || curVal == ':' || curVal == custom) {
-                sum += Integer.parseInt(Num);
-                Num = "0";
-            } else{
+            if (curVal >= '0' && curVal <= '9') {
+                num += Character.toString(curVal);
+            } else if (curVal == ',' || curVal == ':' || curVal == custom) {
+                sum += Integer.parseInt(num);
+                num = "0";
+            } else {
                 throw new IllegalArgumentException();
             }
             ++i;
         }
 
-        sum += Integer.parseInt(Num); //마지막 숫자
+        sum += Integer.parseInt(num); //마지막 숫자
         return sum;
     }
 
 
-    public static void printResult(int result){
+    public static void printResult(int result) {
         System.out.println("결과 : " + result);
     }
 
