c890a56806a55c7a01420fbfd2e9cbb9a74c795f
Local_Hwang <amongzamong@gmail.com>
2024-12-04 20:38:52 +0900

    feat : Add domain layer

---
+++ src/main/java/calculator/domain/CalculatorInput.java
@@ -0,0 +1,13 @@
+package calculator.domain;
+
+public class CalculatorInput {
+    private String input;
+
+    public CalculatorInput(String input) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return input;
+    }
+}
