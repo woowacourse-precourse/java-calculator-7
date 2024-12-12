f8d3c75a90caf4664b0f182079deb1129bdd7fb1
slay <palgalow@gmail.com>
2024-12-12 16:08:53 +0900

    feat(Recommendation): 카테고리 추가, 추천메뉴 추가 기능 추가

---
+++ src/main/java/menu/domain/Recommendation.java
@@ -0,0 +1,19 @@
+package menu.domain;
+
+public class Recommendation {
+    private Coachs coachs;
+    private Categories categories;
+
+    public Recommendation(Coachs coachs, Categories categories) {
+        this.coachs = coachs;
+        this.categories = categories;
+    }
+
+    public void addCategory(String category) {
+        categories.addCategory(category);
+    }
+
+    public void addRecommendMenu(String category) {
+        coachs.recommendMenuOneCycle(category);
+    }
+}
