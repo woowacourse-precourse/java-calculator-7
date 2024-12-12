44430953da1688566044d31aa98c8b2215a33007
slay <palgalow@gmail.com>
2024-12-12 14:55:13 +0900

    feat(InputView): 못먹는 메뉴 입력받는 기능 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -5,19 +5,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
-    private static final String RECOMMENDATION_START_MSG = "점심 메뉴 추천을 시작합니다.";
     private static final String COACH_NAME_INPUT_MSG = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String INEDIBLE_MENU_INPUT_MSG = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     public List<String> readCoachs() {
+        System.out.println(COACH_NAME_INPUT_MSG);
         String input = Console.readLine();
-        return parseCoachs(input.split(","));
+        return parseInput(input.split(","));
     }
 
-    private List<String> parseCoachs(String[] input) {
-        List<String> coachs = new ArrayList<>();
-        for (String coach : input) {
-            coachs.add(coach);
+    public List<String> readInedibles() {
+        System.out.println(INEDIBLE_MENU_INPUT_MSG);
+        String input = Console.readLine();
+        return parseInput(input.split(","));
+    }
+
+    private List<String> parseInput(String[] input) {
+        List<String> listInput = new ArrayList<>();
+        for (String s : input) {
+            listInput.add(s);
         }
-        return coachs;
+        return listInput;
     }
 }
