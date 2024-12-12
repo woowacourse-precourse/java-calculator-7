f74802337deb48cec98bfed429bcf10a8283911f
slay <palgalow@gmail.com>
2024-12-12 14:50:57 +0900

    feat(InputView): 코치 이름 입력받는 기능 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,40 +1,23 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
-import menu.util.Parser;
-import menu.util.Spliter;
-import menu.util.Validator;
 
 public class InputView {
-    private static final String INPUT_COACH_MSG = "코치의 이름을 입력해 주세요. (, 로 구분)";
-    private static final String INPUT_INEDIBLE = "\n%s(이)가 못 먹는 메뉴를 입력해 주세요.\n";
+    private static final String RECOMMENDATION_START_MSG = "점심 메뉴 추천을 시작합니다.";
+    private static final String COACH_NAME_INPUT_MSG = "코치의 이름을 입력해 주세요. (, 로 구분)";
 
-    public List<String> readCoach() {
-        while (true) {
-            try {
-                System.out.println(INPUT_COACH_MSG);
-                String input = Console.readLine();
-                String[] splitInput = Spliter.splitInput(input);
-                Validator.validateCoach(splitInput);
-                return Parser.parseInputToList(splitInput);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+    public List<String> readCoachs() {
+        String input = Console.readLine();
+        return parseCoachs(input.split(","));
     }
 
-    public List<String> readInedible(String name) {
-        while (true) {
-            try {
-                System.out.printf(INPUT_INEDIBLE, name);
-                String input = Console.readLine();
-                String[] splitInput = Spliter.splitInput(input);
-                Validator.validateInedible(splitInput);
-                return Parser.parseInputToList(splitInput);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+    private List<String> parseCoachs(String[] input) {
+        List<String> coachs = new ArrayList<>();
+        for (String coach : input) {
+            coachs.add(coach);
         }
+        return coachs;
     }
 }
