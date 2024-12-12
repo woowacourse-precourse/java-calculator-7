ad7d30a3481599e37f5b12585debab6472a6ede4
HarryBae1011 <06harry@naver.com>
2024-12-12 14:24:48 +0900

    String calculator

---
+++ src/main/java/calculator/io/CalculatorInput.java
@@ -0,0 +1,17 @@
+package calculator.io;
+
+import calculator.domain.Strings;
+import camp.nextstep.edu.missionutils.Console;
+
+public class CalculatorInput {
+
+    public CalculatorInput() {
+    }
+
+    public Strings getString() {
+        String input = Console.readLine();
+
+        return new Strings(input);
+    }
+
+}
