a43a35355b1ddcb6451daee6b2b72d77cadd45c1
slay <palgalow@gmail.com>
2024-12-12 15:14:53 +0900

    feat(RecommendMenu): 추천메뉴 중복 검증하는 기능 추가

---
+++ src/main/java/menu/domain/RecommendMenu.java
@@ -0,0 +1,21 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import menu.util.Exception;
+
+public class RecommendMenu {
+    private List<String> menus = new ArrayList<>();
+
+    public RecommendMenu() {
+    }
+
+    public void adRecommendMenu() {
+    }
+
+    private void validateRecommendMenu(String menu) {
+        if (menus.contains(menu)) {
+            throw new IllegalArgumentException(Exception.ERROR + Exception.DUPLICATE_RECOMMEND_MENU);
+        }
+    }
+}
