6839f418e2c143a859f13bf2a56795ad53357de7
slay <palgalow@gmail.com>
2024-12-12 14:59:44 +0900

    feat(Exception): 코치 수 오류 메세지 추가

--- src/main/java/menu/util/Exception.java
+++ src/main/java/menu/util/Exception.java
@@ -1,6 +1,8 @@
 package menu.util;
 
 public class Exception {
-    private static final String ERROR = "[ERROR] ";
-    private static final String COACH_NAME_LENGTH = "코치의 이름은 최소 2글자, 최대 4글자여야 합니다.";
+    public static final String ERROR = "[ERROR] ";
+    public static final String COACH_NAME_LENGTH = "코치의 이름은 최소 2글자, 최대 4글자여야 합니다.";
+    public static final String COACH_AMOUNT_UNDER_RANGE = "코치는 최소 2명 이상 입력해야 합니다.";
+    public static final String COACH_AMOUNT_OVER_RANGE = "코치는 최대 5명 이하 입력해야 합니다.";
 }
