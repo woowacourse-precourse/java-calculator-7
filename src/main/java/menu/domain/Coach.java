7cf8f4689439156c19c8535ee64dfeaa61881a05
slay <palgalow@gmail.com>
2024-12-12 15:17:04 +0900

    refactor(Coach): 자료구조 수정

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,16 +1,18 @@
 package menu.domain;
 
-import java.util.List;
 import menu.util.Exception;
 
 public class Coach {
     private final String name;
-    private List<String> menus;
-    private List<String> inedibles;
+    private InedibleMenu inedibleMenu;
+    private RecommendMenu recommendMenu;
 
-    public Coach(String name, List<String> menus, List<String> inedibles) {
+
+    public Coach(String name, InedibleMenu inedibleMenu, RecommendMenu recommendMenu) {
         validateNameLength(name);
         this.name = name;
+        this.inedibleMenu = inedibleMenu;
+        this.recommendMenu = recommendMenu;
     }
 
     public void validateNameLength(String name) {
