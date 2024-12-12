2f73cc5d5348daf21363fe6439d17aa95add5275
slay <palgalow@gmail.com>
2024-12-12 16:16:01 +0900

    feat(RecommendationService): 한 사이클 추천 기능, 카테고리 랜덤 선정, 모든 추천사이클 순환 기능 추가

---
+++ src/main/java/menu/service/RecommendationService.java
@@ -0,0 +1,35 @@
+package menu.service;
+
+import menu.domain.Menu;
+import menu.domain.Recommendation;
+
+public class RecommendationService {
+    private Recommendation recommendation;
+
+    public RecommendationService(Recommendation recommendation) {
+        this.recommendation = recommendation;
+    }
+
+    public void recommendationMenu() {
+        while (!recommendation.isCompleteRecommendation()) {
+            recommendationOneCycle();
+        }
+    }
+
+    private void recommendationOneCycle() {
+        String category = randomCategory();
+        recommendation.addRecommendMenu(category);
+    }
+
+    private String randomCategory() {
+        while (true) {
+            try {
+                String category = Menu.randomCategory();
+                recommendation.addCategory(category);
+                return category;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
