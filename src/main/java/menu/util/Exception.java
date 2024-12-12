a8d33b1986410d79816e634d4e817a42ba5e5790
slay <palgalow@gmail.com>
2024-12-12 14:44:59 +0900

    feat(Exception): 오류 메세지 관리하는 클래스 생성

---
+++ src/main/java/menu/util/Exception.java
@@ -0,0 +1,6 @@
+package menu.util;
+
+public class Exception {
+    private static final String ERROR = "[ERROR] ";
+    private static final String COACH_NAME_LENGTH = "코치의 이름은 최소 2글자, 최대 4글자여야 합니다.";
+}
