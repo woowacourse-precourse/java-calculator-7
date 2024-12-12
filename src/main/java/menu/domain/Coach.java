f5c568c2a4825ab060e43dc2f1b1b6ee5a44282c
slay <palgalow@gmail.com>
2024-12-12 14:45:51 +0900

    feat(Coach): 코치 이름 길이 검증하는 기능 추가

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,6 +1,7 @@
 package menu.domain;
 
 import java.util.List;
+import menu.util.Exception;
 
 public class Coach {
     private final String name;
@@ -8,6 +9,13 @@ public class Coach {
     private List<String> inedibles;
 
     public Coach(String name, List<String> menus, List<String> inedibles) {
+        validateNameLength(name);
         this.name = name;
+    }
+
+    public void validateNameLength(String name) {
+        if (name.length() < 2 || name.length() > 4) {
+            throw new IllegalArgumentException(Exception.ERROR + Exception.COACH_NAME_LENGTH);
+        }
     }
 }
