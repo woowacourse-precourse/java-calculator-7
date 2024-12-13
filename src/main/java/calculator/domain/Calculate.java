3f36a4acedea0cfb087b41f3c1a7135230cb24a4
HarryBae1011 <06harry@naver.com>
2024-12-13 19:43:30 +0900

    String calculator

--- src/main/java/calculator/domain/Calculate.java
+++ src/main/java/calculator/domain/Calculate.java
@@ -1,1 +1,25 @@
+package calculator.domain;
 
+public class Calculate {
+
+    private final String[] numbers;
+    private int sum = 0;
+
+    public Calculate(Strings strings) {
+        this.numbers = strings.distinguisher();
+    }
+
+    public int addNumbers() {
+
+        for (String number : numbers) {
+            int i = Integer.parseInt(number);
+
+            if (i <= 0) {
+                throw new IllegalArgumentException("올바른 값을 입력해주세요");
+            }
+
+            sum += i;
+        }
+        return sum;
+    }
+}
---
+++ src/main/java/calculator/io/CalculatorOutput.java
@@ -0,0 +1,12 @@
+package calculator.io;
+
+public class CalculatorOutput {
+
+    public void printAskingString() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public void printResult() {
+        System.out.print("결과 : ");
+    }
+}
